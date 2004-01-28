bd1a071e518b371ba0f41d57fcc803efafe1fe8f
nobody <nobody@3dd8ee02-672a-0410-8165-a85d635670bd>
2004-01-28 23:26:47 +0000

    This commit was manufactured by cvs2svn to create branch 'rjl'.

---
+++ branches/rjl/emf/src/edu/colorado/phet/waves/model/WavefrontType.java
@@ -0,0 +1,12 @@
+/**
+ * Class: WavefrontType
+ * Package: edu.colorado.phet.emf.model
+ * Author: Another Guy
+ * Date: May 23, 2003
+ */
+package edu.colorado.phet.waves.model;
+
+
+public interface WavefrontType {
+    float computeAmplitudeAtDistance( Wavefront wavefront, float amplitude, float distance );
+}
