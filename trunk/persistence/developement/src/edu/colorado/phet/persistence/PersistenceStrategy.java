4fe2940dd532e8552743d174f8c2a73ec33cc7a0
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2004-02-06 00:08:22 +0000

    Initial revision

---
+++ trunk/persistence/developement/src/edu/colorado/phet/persistence/PersistenceStrategy.java
@@ -0,0 +1,11 @@
+/**
+ * Class: PersistenceStrategy
+ * Package: edu.colorado.phet.persistence
+ * Author: Another Guy
+ * Date: Feb 5, 2004
+ */
+package edu.colorado.phet.persistence;
+
+public interface PersistenceStrategy {
+    void store( String text );
+}
