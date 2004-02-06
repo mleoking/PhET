7cd4bd3e5aff8ccc6190e749d17e102eea2c4a74
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2004-02-06 00:08:22 +0000

    Initial revision

---
+++ lasers/src/edu/colorado/phet/lasers/view/TwoEnergyLevelApparatusPanel.java
@@ -0,0 +1,17 @@
+/**
+ * Class: BaseLaserApparatusPanel
+ * Package: edu.colorado.phet.lasers.view
+ * Author: Another Guy
+ * Date: Mar 21, 2003
+ */
+package edu.colorado.phet.lasers.view;
+
+import edu.colorado.phet.controller.PhetApplication;
+import edu.colorado.phet.graphics.ApparatusPanel;
+
+public class TwoEnergyLevelApparatusPanel extends ApparatusPanel {
+
+    public TwoEnergyLevelApparatusPanel( PhetApplication application ) {
+        super( "Atom With Two Energy Levels" );
+    }
+}
