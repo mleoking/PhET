5629d83b22ba90cef3090275e493ab5432fd14ea
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2004-02-11 00:48:53 +0000

    Initial revision

---
+++ trunk/greenhouse/src/edu/colorado/phet/coreadditions/InvModelCoordsGraphic.java
@@ -0,0 +1,11 @@
+/**
+ * Class: InvModelCoordsGraphic
+ * Class: edu.colorado.phet.coreadditions
+ * User: Ron LeMaster
+ * Date: Oct 15, 2003
+ * Time: 7:40:02 AM
+ */
+package edu.colorado.phet.coreadditions;
+
+public interface InvModelCoordsGraphic {
+}
---
+++ trunk/idealgas/development/common-src/src/edu/colorado/phet/physics/TimeStep.java
@@ -0,0 +1,12 @@
+/**
+ * Class: TimeStep
+ * Package: edu.colorado.phet.physics
+ * Author: Another Guy
+ * Date: Apr 22, 2003
+ */
+package edu.colorado.phet.physics;
+
+public interface TimeStep {
+
+    public void stepInTime( float dt );
+}
