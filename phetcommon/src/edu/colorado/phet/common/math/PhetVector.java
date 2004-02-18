901492e375f83bee2fce9464144598fc657db905
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2004-02-18 16:54:42 +0000

    Removing invalid comments.

--- phetcommon/src/edu/colorado/phet/common/math/PhetVector.java
+++ phetcommon/src/edu/colorado/phet/common/math/PhetVector.java
@@ -8,7 +8,6 @@ import java.awt.geom.Point2D;
  * Date: Jun 24, 2003
  * Time: 10:24:31 AM
  * Copyright (c) Jun 24, 2003 by Sam Reid
- * Testing commit.
  */
 public class PhetVector {
     double x;
