7106f3691e71c2012cff9d468f27be69a85b57f8
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2004-03-06 17:39:32 +0000

    no message

---
+++ trunk/src/edu/colorado/phet/nuclearphysics/Config.java
@@ -0,0 +1,16 @@
+/**
+ * Class: Config
+ * Package: edu.colorado.phet.nuclearphysics
+ * Author: Another Guy
+ * Date: Mar 6, 2004
+ */
+package edu.colorado.phet.nuclearphysics;
+
+import edu.colorado.phet.nuclearphysics.model.PotentialProfile;
+
+public class Config {
+
+    public static PotentialProfile u235PotentialProfile = new PotentialProfile( 300, 400, 75 );
+    public static double AlphaLocationUncertaintySigmaFactor = 0.333;
+
+}
