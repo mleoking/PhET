e6e4cfafeb381c56b7c36ae891eaf8288f6efa9b
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2004-03-18 16:41:46 +0000

    no message

---
+++ trunk/lostinspace/src/edu/colorado/games4education/lostinspace/model/NormalStar.java
@@ -0,0 +1,15 @@
+/**
+ * Class: NormalStar
+ * Package: edu.colorado.games4education.lostinspace.model
+ * Author: Another Guy
+ * Date: Mar 16, 2004
+ */
+package edu.colorado.games4education.lostinspace.model;
+
+import java.awt.geom.Point2D;
+
+public class NormalStar extends Star {
+    public NormalStar( double luminance, Point2D.Double location, double syntheticZBound ) {
+        super( luminance, location, syntheticZBound );
+    }
+}
