46ad24cf183a8ba9b3fafba00b4ffa0723d20722
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2004-03-19 16:14:19 +0000

    Changed parameter names and changed the first g.setTransform() to g.transform.

--- trunk/phetcommon/src/edu/colorado/phet/common/view/graphics/TxGraphic.java
+++ trunk/phetcommon/src/edu/colorado/phet/common/view/graphics/TxGraphic.java
@@ -10,19 +10,23 @@ package edu.colorado.phet.common.view.graphics;
 import java.awt.*;
 import java.awt.geom.AffineTransform;
 
+/**
+ * A decorator for Graphic that applies an AffineTransform before
+ * the Graphic is painted.
+ */
 public class TxGraphic implements Graphic {
     private Graphic graphic;
-    private AffineTransform modelToViewTx;
+    private AffineTransform transform;
 
-    public TxGraphic(Graphic circleGraphic, AffineTransform modelToViewTx) {
-        this.graphic = circleGraphic;
-        this.modelToViewTx = modelToViewTx;
+    public TxGraphic( Graphic graphic, AffineTransform transform ) {
+        this.graphic = graphic;
+        this.transform = transform;
     }
 
-    public void paint(Graphics2D g) {
+    public void paint( Graphics2D g ) {
         AffineTransform orgTx = g.getTransform();
-        g.setTransform(modelToViewTx);
-        graphic.paint(g);
-        g.setTransform(orgTx);
+        g.transform( transform );
+        graphic.paint( g );
+        g.setTransform( orgTx );
     }
 }
