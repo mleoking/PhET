fd1c663617e4c485d7385dc17368f08d14344060
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2004-03-25 03:27:30 +0000

    Reformatted with PhetStyle

--- trunk/phetcommon/src/edu/colorado/phet/common/view/util/DoubleGeneralPath.java
+++ trunk/phetcommon/src/edu/colorado/phet/common/view/util/DoubleGeneralPath.java
@@ -3,9 +3,9 @@ package edu.colorado.phet.common.view.util;
 
 import edu.colorado.phet.common.math.PhetVector;
 
+import java.awt.*;
 import java.awt.geom.GeneralPath;
 import java.awt.geom.Point2D;
-import java.awt.*;
 
 /**
  * This adapter class for GeneralPath allows provides an interface in double coordinates.
@@ -13,43 +13,45 @@ import java.awt.*;
 public class DoubleGeneralPath {
     GeneralPath path;
 
-    public DoubleGeneralPath(Shape shape){
-        path=new GeneralPath(shape);
-    }
-    public DoubleGeneralPath(PhetVector pt) {
-        this(pt.getX(), pt.getY());
+    public DoubleGeneralPath( Shape shape ) {
+        path = new GeneralPath( shape );
     }
 
-    public DoubleGeneralPath(Point2D.Double pt) {
-        this(pt.x, pt.y);
+    public DoubleGeneralPath( PhetVector pt ) {
+        this( pt.getX(), pt.getY() );
     }
 
-    public DoubleGeneralPath(double x, double y) {
+    public DoubleGeneralPath( Point2D.Double pt ) {
+        this( pt.x, pt.y );
+    }
+
+    public DoubleGeneralPath( double x, double y ) {
         path = new GeneralPath();
-        path.moveTo((float) x, (float) y);
+        path.moveTo( (float)x, (float)y );
     }
 
-    public void moveTo(double x, double y) {
-        path.moveTo((float) x, (float) y);
+    public void moveTo( double x, double y ) {
+        path.moveTo( (float)x, (float)y );
     }
 
-    public void lineTo(double x, double y) {
-        path.lineTo((float) x, (float) y);
+    public void lineTo( double x, double y ) {
+        path.lineTo( (float)x, (float)y );
     }
 
-    public void lineTo(Point2D.Double pt) {
-        lineTo(pt.x, pt.y);
+    public void lineTo( Point2D.Double pt ) {
+        lineTo( pt.x, pt.y );
     }
-    public void lineToRelative(double dx,double dy){
-        Point2D cur=path.getCurrentPoint();
-        lineTo(cur.getX()+dx,cur.getY()+dy);
+
+    public void lineToRelative( double dx, double dy ) {
+        Point2D cur = path.getCurrentPoint();
+        lineTo( cur.getX() + dx, cur.getY() + dy );
     }
 
     public GeneralPath getGeneralPath() {
         return path;
     }
 
-    public void lineTo(PhetVector loc) {
-        lineTo(loc.getX(), loc.getY());
+    public void lineTo( PhetVector loc ) {
+        lineTo( loc.getX(), loc.getY() );
     }
 }
