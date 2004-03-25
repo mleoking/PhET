70563d22eb58d71f7d4e34f312288a556ee783c2
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2004-03-25 19:33:42 +0000

    Made nucleons radially shaded GIFs

--- trunk/nuclearphysics/src/edu/colorado/phet/nuclearphysics/view/NeutronGraphic.java
+++ trunk/nuclearphysics/src/edu/colorado/phet/nuclearphysics/view/NeutronGraphic.java
@@ -6,15 +6,33 @@
  */
 package edu.colorado.phet.nuclearphysics.view;
 
+import edu.colorado.phet.common.view.util.graphics.ImageLoader;
 import edu.colorado.phet.nuclearphysics.model.Neutron;
 import edu.colorado.phet.nuclearphysics.model.NuclearParticle;
 
 import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+import java.awt.image.ImageObserver;
+import java.io.IOException;
 import java.util.HashMap;
 
-public class NeutronGraphic extends ParticleGraphic {
+public class NeutronGraphic extends ParticleGraphic implements ImageObserver {
 
     private static Color color = Color.gray;
+    private static BufferedImage neutronImage;
+    private static AffineTransform atx = new AffineTransform();
+
+    static {
+        ImageLoader imgLoader = new ImageLoader();
+        try {
+            neutronImage = imgLoader.loadImage( "images/gray-xsml.gif" );
+        }
+        catch( IOException e ) {
+            e.printStackTrace();
+        }
+    }
+
     private static HashMap graphicToModelMap = new HashMap();
 
     public static NeutronGraphic getGraphicForNeutron( Neutron neutron ) {
@@ -30,16 +48,17 @@ public class NeutronGraphic extends ParticleGraphic {
         graphicToModelMap.put( particle, this );
     }
 
-    public void paint( Graphics2D g ) {
-        super.paint( g );
-    }
-
     public void paint( Graphics2D g, double x, double y ) {
-        super.paint( g, x, y );    //To change body of overridden methods use File | Settings | File Templates.
+        atx.setToTranslation( x, y );
+        g.drawImage( neutronImage, atx, this );
     }
 
 
     public void update() {
         super.update();
+    }
+
+    public boolean imageUpdate( Image img, int infoflags, int x, int y, int width, int height ) {
+        return false;
     }
 }
--- trunk/nuclearphysics/src/edu/colorado/phet/nuclearphysics/view/ProtonGraphic.java
+++ trunk/nuclearphysics/src/edu/colorado/phet/nuclearphysics/view/ProtonGraphic.java
@@ -6,13 +6,30 @@
  */
 package edu.colorado.phet.nuclearphysics.view;
 
+import edu.colorado.phet.common.view.util.graphics.ImageLoader;
 import edu.colorado.phet.nuclearphysics.model.NuclearParticle;
 
 import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+import java.awt.image.ImageObserver;
+import java.io.IOException;
 
-public class ProtonGraphic extends ParticleGraphic {
+public class ProtonGraphic extends ParticleGraphic implements ImageObserver {
 
     private static Color color = Color.red;
+    private static AffineTransform atx = new AffineTransform();
+    private static BufferedImage protonImage;
+
+    static {
+        ImageLoader imgLoader = new ImageLoader();
+        try {
+            protonImage = imgLoader.loadImage( "images/red-xsml.gif" );
+        }
+        catch( IOException e ) {
+            e.printStackTrace();
+        }
+    }
 
     public ProtonGraphic() {
         super( color );
@@ -20,5 +37,14 @@ public class ProtonGraphic extends ParticleGraphic {
 
     public ProtonGraphic( NuclearParticle particle ) {
         super( particle, ProtonGraphic.color );
+    }
+
+    public void paint( Graphics2D g, double x, double y ) {
+        atx.setToTranslation( x, y );
+        g.drawImage( protonImage, atx, this );
+    }
+
+    public boolean imageUpdate( Image img, int infoflags, int x, int y, int width, int height ) {
+        return false;
     }
 }
