607c30581b80bf13bc2e0370766aeeebfa173dd3
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2004-04-26 03:16:50 +0000

    no message

--- trunk/distanceladder/src/edu/colorado/phet/distanceladder/DistanceLadderGame.java
+++ trunk/distanceladder/src/edu/colorado/phet/distanceladder/DistanceLadderGame.java
@@ -14,6 +14,7 @@ import edu.colorado.phet.distanceladder.exercise.Message;
 import edu.colorado.phet.distanceladder.levels.Level1;
 import edu.colorado.phet.distanceladder.levels.Level1A;
 import edu.colorado.phet.distanceladder.levels.Level2;
+import edu.colorado.phet.distanceladder.levels.Level2A;
 import edu.colorado.phet.distanceladder.model.*;
 
 import javax.swing.*;
@@ -66,12 +67,13 @@ public class DistanceLadderGame {
 //        displayMessage( new HtmlMessage( "messages/intro-1.html" ) );
 
 //        displayMessage( new HtmlMessage( "messages/level1-intro.html" ) );
-        doLevel( new Level1( app.getApplicationView().getPhetFrame(), model ) );
-        doLevel( new Level1A( app.getApplicationView().getPhetFrame(), model ) );
+//        doLevel( new Level1( app.getApplicationView().getPhetFrame(), model ) );
+//        doLevel( new Level1A( app.getApplicationView().getPhetFrame(), model ) );
 
-        displayMessage( new HtmlMessage( "messages/level2-intro.html" ) );
-        doLevel( new Level2( app.getApplicationView().getPhetFrame(), model ) );
+//        displayMessage( new HtmlMessage( "messages/level2-intro.html" ) );
+//        doLevel( new Level2( app.getApplicationView().getPhetFrame(), model ) );
         displayMessage( new HtmlMessage( "messages/level2A-intro.html" ) );
+        doLevel( new Level2A( app.getApplicationView().getPhetFrame(), model ) );
 
         cockpitModule.activate( null );
     }
