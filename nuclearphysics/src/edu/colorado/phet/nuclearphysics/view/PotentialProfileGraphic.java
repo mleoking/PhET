61d458d63a09a2594fa0f4eac2a0b2e9420391a3
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2004-05-05 22:56:46 +0000

    U235 nuceleus jiggles when as it starts to fission Nucleus no longer jumps lateraly when first drawn.

--- nuclearphysics/src/edu/colorado/phet/nuclearphysics/view/PotentialProfileGraphic.java
+++ nuclearphysics/src/edu/colorado/phet/nuclearphysics/view/PotentialProfileGraphic.java
@@ -64,7 +64,17 @@ public class PotentialProfileGraphic implements Graphic, SimpleObserver {
 
     public void paint( Graphics2D g ) {
         profileTx.setToIdentity();
-        profileTx.translate( nucleus.getLocation().getX(), 0 );
+
+        /**
+         *  @note: This line now puts the x location at 0, so it won't jiggle while a
+         * nucleus that is fissioning jiggles. This will not work if we want to go
+         * back to having two profiles that move away with the daughter nuclei after
+         * a fission event. To make that happen, inable the commented line of code
+         */
+        profileTx.translate( 0, 0 );
+        //        profileTx.translate( nucleus.getLocation().getX(), 0 );
+
+
         AffineTransform orgTx = g.getTransform();
         g.transform( profileTx );
         g.drawImage( image, -image.getWidth( imgObs ) / 2,
