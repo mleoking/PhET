a260ed3b395e6a53e4666b922d7b991d781045f0
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2004-05-13 21:19:35 +0000

    no message

---
+++ trunk/phetcommon/src/edu/colorado/phet/common/view/graphics/BoundedGraphic.java
@@ -0,0 +1,12 @@
+/**
+ * Class: BoundedGraphic
+ * Package: edu.colorado.phet.common.view.graphics
+ * Author: Another Guy
+ * Date: May 11, 2004
+ */
+package edu.colorado.phet.common.view.graphics;
+
+import edu.colorado.phet.common.view.graphics.bounds.Boundary;
+
+public interface BoundedGraphic extends Graphic, Boundary {
+}
