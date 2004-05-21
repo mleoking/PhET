2c2b536d105f99030fa6e0ed94cd78494e85fd5c
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2004-05-21 19:55:54 +0000

    Made vector class hierarchy better

--- trunk/phetcommon/tests-src/edu/colorado/phet/common/examples/TestVectors.java
+++ trunk/phetcommon/tests-src/edu/colorado/phet/common/examples/TestVectors.java
@@ -6,12 +6,13 @@
  */
 package edu.colorado.phet.common.examples;
 
+import edu.colorado.phet.common.math.AbstractVector2D;
 import edu.colorado.phet.common.math.ImmutableVector2D;
 import edu.colorado.phet.common.math.Vector2D;
 
 public class TestVectors {
     public static void main( String[] args ) {
-        ImmutableVector2D.Double v = new ImmutableVector2D.Double( 3, 4 );
+        AbstractVector2D.Double v = new ImmutableVector2D.Double( 3, 4 );
         System.out.println( "v = " + v );
 
         Vector2D.Double dir = new Vector2D.Double( 7, 8 );
