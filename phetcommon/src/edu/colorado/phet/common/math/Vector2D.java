52ba5f9e32a48bc5072ac8159156ec041e479fb6
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2004-05-21 19:25:36 +0000

    Implemented Float versions of the Vector2D sub-interfaces

--- phetcommon/src/edu/colorado/phet/common/math/Vector2D.java
+++ phetcommon/src/edu/colorado/phet/common/math/Vector2D.java
@@ -17,7 +17,7 @@ public interface Vector2D extends ImmutableVector2D {
 
     void setY( double y );
 
-    void setCoordinates( double x, double y );
+    void setComponents( double x, double y );
 
     Vector2D normalize();
 
@@ -54,7 +54,74 @@ public interface Vector2D extends ImmutableVector2D {
             super.setY( y );
         }
 
-        public void setCoordinates( double x, double y ) {
+        public void setComponents( double x, double y ) {
+            setX( x );
+            setY( y );
+        }
+
+        public Vector2D subtract( ImmutableVector2D that ) {
+            setX( getX() - that.getX() );
+            setY( getY() - that.getY() );
+            return this;
+        }
+
+        public String toString() {
+            return "Vector2D.Double[" + getX() + ", " + getY() + "]";
+        }
+    }
+
+    public static class Float extends ImmutableVector2D.Float implements Vector2D {
+
+        public Float() {
+        }
+
+        public Float( float x, float y ) {
+            super( x, y );
+        }
+
+        public Float( double x, double y ) {
+            super( (float)x, (float)y );
+        }
+
+        public Vector2D add( ImmutableVector2D v ) {
+            setX( getX() + v.getX() );
+            setY( getY() + v.getY() );
+            return this;
+        }
+
+        public Vector2D normalize() {
+            double length = getMagnitude();
+            return scale( 1.0 / length );
+        }
+
+        public Vector2D scale( double scale ) {
+            setX( getX() * scale );
+            setY( getY() * scale );
+            return this;
+        }
+
+        public void setX( float x ) {
+            super.setX( x );
+        }
+
+        public void setY( float y ) {
+            super.setY( y );
+        }
+
+        public void setX( double x ) {
+            super.setX( x );
+        }
+
+        public void setY( double y ) {
+            super.setY( y );
+        }
+
+        public void setComponents( float x, float y ) {
+            setX( x );
+            setY( y );
+        }
+
+        public void setComponents( double x, double y ) {
             setX( x );
             setY( y );
         }
