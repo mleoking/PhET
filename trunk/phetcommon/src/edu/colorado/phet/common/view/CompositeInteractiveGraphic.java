50b93b846eee9c160c8f1db4623950909b633716
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2004-06-18 15:58:15 +0000

    Added DoubleGeneralPath methods, Fixed the border in ApparatusPanel, Added documentation, reformatted. Added debug information to CompositeInteractiveGraphic.

--- trunk/phetcommon/src/edu/colorado/phet/common/view/CompositeInteractiveGraphic.java
+++ trunk/phetcommon/src/edu/colorado/phet/common/view/CompositeInteractiveGraphic.java
@@ -23,17 +23,28 @@ public class CompositeInteractiveGraphic implements InteractiveGraphic {
 
     private MultiMap graphicMap = new MultiMap();
     private MouseManager mouseManager;
+    private boolean visible = true;
 
     public CompositeInteractiveGraphic() {
         mouseManager = new MouseManager();
     }
 
     public void paint( Graphics2D g ) {
-        Iterator it = graphicMap.iterator();
-        while( it.hasNext() ) {
-            Graphic graphic = (Graphic)it.next();
-            graphic.paint( g );
+        if( visible ) {
+            Iterator it = graphicMap.iterator();
+            while( it.hasNext() ) {
+                Graphic graphic = (Graphic)it.next();
+                graphic.paint( g );
+            }
         }
+    }
+
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public void setVisible( boolean visible ) {
+        this.visible = visible;
     }
 
     /**
@@ -44,13 +55,15 @@ public class CompositeInteractiveGraphic implements InteractiveGraphic {
      * @return
      */
     public boolean contains( int x, int y ) {
-        Iterator it = this.graphicMap.iterator();
-        while( it.hasNext() ) {
-            Object o = it.next();
-            if( o instanceof Boundary ) {
-                Boundary boundary = (Boundary)o;
-                if( boundary.contains( x, y ) ) {
-                    return true;
+        if( visible ) {
+            Iterator it = this.graphicMap.iterator();
+            while( it.hasNext() ) {
+                Object o = it.next();
+                if( o instanceof Boundary ) {
+                    Boundary boundary = (Boundary)o;
+                    if( boundary.contains( x, y ) ) {
+                        return true;
+                    }
                 }
             }
         }
@@ -100,31 +113,45 @@ public class CompositeInteractiveGraphic implements InteractiveGraphic {
 
     // Mouse-related behaviors
     public void mouseClicked( MouseEvent e ) {
-        mouseManager.mouseClicked( e );
+        if( visible ) {
+            mouseManager.mouseClicked( e );
+        }
     }
 
     public void mousePressed( MouseEvent e ) {
-        mouseManager.mousePressed( e );
+        if( visible ) {
+            mouseManager.mousePressed( e );
+        }
     }
 
     public void mouseReleased( MouseEvent e ) {
-        mouseManager.mouseReleased( e );
+        if( visible ) {
+            mouseManager.mouseReleased( e );
+        }
     }
 
     public void mouseEntered( MouseEvent e ) {
-        mouseManager.mouseEntered( e );
+        if( visible ) {
+            mouseManager.mouseEntered( e );
+        }
     }
 
     public void mouseExited( MouseEvent e ) {
-        mouseManager.mouseExited( e );
+        if( visible ) {
+            mouseManager.mouseExited( e );
+        }
     }
 
     public void mouseDragged( MouseEvent e ) {
-        mouseManager.mouseDragged( e );
+        if( visible ) {
+            mouseManager.mouseDragged( e );
+        }
     }
 
     public void mouseMoved( MouseEvent e ) {
-        mouseManager.mouseMoved( e );
+        if( visible ) {
+            mouseManager.mouseMoved( e );
+        }
     }
 
     public void startDragging( InteractiveGraphic graphic, MouseEvent e ) {
@@ -205,15 +232,40 @@ public class CompositeInteractiveGraphic implements InteractiveGraphic {
             return null;
         }
 
+        /*
+
+•	I think I’ve found the trouble spot.  The tree is like this:
+•	TotalGraphic ? Circuit Graphic ? Junction graphics and branch graphics.
+•	The totalGraphic :
+1.	gets the mousemoved event
+2.	Discovers that no graphic contains the new location.
+3.	Calls a no-op on the CircuitGraphic.
+4.	Sets activeUnit to null.
+•	In step 3, the no-op on CircuitGraphic should be a call to CircuitGraphic.activeUnit.mouseExited if active unit is not null.  This suggests we need a different mouse listener to add to components than to leave in the tree.  Not sure what the best design is for this.  It is satisfactory to add CompositeInteractiveGraphics directly to components.
+        */
+
         //Does nothing if we're already over the right handler.
         private void handleEntranceAndExit( final MouseEvent e ) {
+            //What is this?  Why are we doing invoke later?  We should already be in the swing thread.
+            //I verified this in one runtime.
+
             SwingUtilities.invokeLater( new Runnable() {
                 public void run() {
                     MouseInputListener unit = getHandler( e );
                     if( unit == null ) {
                         // If the mouse isn't over anything contained in the
                         // CompositeInteractiveGraphic...
+//                        System.out.println( "Handler is null." + System.currentTimeMillis() );
+                        getHandler( e );
                         if( activeUnit != null ) {
+//                    if( activeUnit instanceof CompositeInteractiveGraphic ) {
+//                        //traverse the tree.
+//                        CompositeInteractiveGraphic cig = (CompositeInteractiveGraphic)activeUnit;
+//                        if( cig.mouseManager.activeUnit != null ) {
+//                            cig.mouseManager.activeUnit.mouseExited( e );
+//                            cig.mouseManager.activeUnit=null;
+//                        }
+//                    }
                             activeUnit.mouseExited( e );
                             activeUnit = null;
                         }
@@ -236,6 +288,7 @@ public class CompositeInteractiveGraphic implements InteractiveGraphic {
                     }
                 }
             } );
+
         }
 
         public void mouseMoved( MouseEvent e ) {
@@ -246,8 +299,12 @@ public class CompositeInteractiveGraphic implements InteractiveGraphic {
             }
         }
 
-        //temporarily transfer control to the specified graphic.
-        //May not be safe to give control to a mouseinputlistener not in our multimap...
+        /**
+         * temporarily transfer control to the specified graphic.
+         *
+         * @param inputListener
+         * @param event
+         */
         public void startDragging( MouseInputListener inputListener, MouseEvent event ) {
             if( activeUnit != null ) {
                 activeUnit.mouseReleased( event );//could be problems if expected event==RELEASE_EVENT
