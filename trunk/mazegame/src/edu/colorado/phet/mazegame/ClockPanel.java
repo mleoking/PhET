8a69e1e1620656c1d6202d8ed85012fed786417a
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2004-08-08 22:02:05 +0000

    Smoothed readout.

--- trunk/mazegame/src/edu/colorado/phet/mazegame/ClockPanel.java
+++ trunk/mazegame/src/edu/colorado/phet/mazegame/ClockPanel.java
@@ -82,5 +82,10 @@ public class ClockPanel extends JPanel {
         return clockTime;
     }
 
+    protected void paintComponent( Graphics g ) {
+        Graphics2D g2 = (Graphics2D)g;
+        g2.setRenderingHint( RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON );
+        super.paintComponent( g );
+    }
 }//end of public class
 
