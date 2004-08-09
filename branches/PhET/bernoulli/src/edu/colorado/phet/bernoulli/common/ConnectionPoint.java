24cb44a193c4f518cdf7adebc53bcfc9d92aea9e
nobody <nobody@3dd8ee02-672a-0410-8165-a85d635670bd>
2004-08-09 18:17:19 +0000

    This commit was manufactured by cvs2svn to create branch 'PhET'.

---
+++ branches/PhET/bernoulli/src/edu/colorado/phet/bernoulli/common/ConnectionPoint.java
@@ -0,0 +1,18 @@
+/**
+ * Class: ConnectionPoint
+ * Package: edu.colorado.phet.bernoulli.common
+ * Author: Another Guy
+ * Date: Sep 26, 2003
+ */
+package edu.colorado.phet.bernoulli.common;
+
+
+public class ConnectionPoint {
+    double x;
+    double y;
+
+    public ConnectionPoint( double x, double y ) {
+        this.x = x;
+        this.y = y;
+    }
+}
