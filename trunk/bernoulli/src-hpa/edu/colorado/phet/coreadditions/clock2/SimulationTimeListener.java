65a54b71b35cc8c962ff9550e72e1204d3f4dc1e
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2004-08-09 18:17:18 +0000

    Initial revision

---
+++ trunk/bernoulli/src-hpa/edu/colorado/phet/coreadditions/clock2/SimulationTimeListener.java
@@ -0,0 +1,9 @@
+/*Copyright, Sam Reid, 2003.*/
+package edu.colorado.phet.coreadditions.clock2;
+
+/**
+ * Listens for time simulation events.
+ */
+public interface SimulationTimeListener {
+    public void simulationTimeIncreased(double dt);
+}
