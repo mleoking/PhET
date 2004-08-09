11b54da437fc7d980846f985051bd8868aa757d8
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2004-08-09 23:57:18 +0000

    Initial revision

---
+++ trunk/ohm1d/src-pc/phet/paint/Painter.java
@@ -0,0 +1,7 @@
+package phet.paint;
+
+import java.awt.*;
+
+public interface Painter {
+    public void paint( Graphics2D g );
+}
---
+++ trunk/ohm1d/src-pc/phet/phys2d/propagators/NullPropagator.java
@@ -0,0 +1,9 @@
+package phet.phys2d.propagators;
+
+import phet.phys2d.Particle;
+import phet.phys2d.Propagator;
+
+public class NullPropagator implements Propagator {
+    public void propagate( double time, Particle p ) {
+    }
+}
