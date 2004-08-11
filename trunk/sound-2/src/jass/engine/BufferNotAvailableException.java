52957ce9934df3315bf50e04d661807b94c7b118
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2004-08-11 21:46:40 +0000

    Display now working.

---
+++ trunk/sound-2/src/jass/engine/BufferNotAvailableException.java
@@ -0,0 +1,9 @@
+package jass.engine;
+
+/**
+ Thrown when a buffer is not available.
+ @author Kees van den Doel (kvdoel@cs.ubc.ca)
+ */
+public class BufferNotAvailableException extends Exception {
+}
+
---
+++ trunk/sound-2/src/jass/engine/UnsupportedAudioFileFormatException.java
@@ -0,0 +1,15 @@
+package jass.engine;
+
+/**
+ Thrown when encountering unsupported file format.
+ @author Kees van den Doel (kvdoel@cs.ubc.ca)
+ */
+public class UnsupportedAudioFileFormatException extends Exception {
+    public UnsupportedAudioFileFormatException(String s) {
+        super(s);
+    }
+
+    public UnsupportedAudioFileFormatException() {
+        super();
+    }
+}
