cbcbf8d9236d0124aef8857366f28cad2bbcdbd7
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2004-09-02 17:38:21 +0000

    Initial revision

---
+++ trunk/javadetector/src/edu/colorado/phet/javadetector/JarTest.java
@@ -0,0 +1,17 @@
+/**
+ * Class: JarTest
+ * Package: edu.colorado.phet.javadetector
+ * Author: Another Guy
+ * Date: Sep 1, 2004
+ */
+package edu.colorado.phet.javadetector;
+
+import javax.swing.*;
+
+public class JarTest {
+
+
+    public static void main( String[] args ) {
+        JOptionPane.showMessageDialog( null, "Testing..." );
+    }
+}
