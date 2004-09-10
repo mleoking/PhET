a887f863e06dacf2a5847fb5c73526bc190f1bcd
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2004-09-10 22:15:10 +0000

    Initial revision

---
+++ trunk/ideal-gas-v3/src/edu/colorado/phet/idealgas/Strings.java
@@ -0,0 +1,14 @@
+/**
+ * Class: Strings
+ * Package: edu.colorado.phet.idealgas
+ * Author: Another Guy
+ * Date: Sep 10, 2004
+ */
+package edu.colorado.phet.idealgas;
+
+public class Strings {
+
+    public static String title = "Ideal Gases & Buoyancy";
+    public static String description = "Ideal Gases & Buoyancy";
+    public static String idealGasModuleName = "Ideal Gas";
+}
