867a72600ceae176fada123d4ded0b3ab2ca4abd
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2004-09-14 23:12:50 +0000

    Molecule-molecule collisions working

--- trunk/ideal-gas-v3/src/edu/colorado/phet/collision/CollisionFactory.java
+++ trunk/ideal-gas-v3/src/edu/colorado/phet/collision/CollisionFactory.java
@@ -7,25 +7,24 @@
 package edu.colorado.phet.collision;
 
 import edu.colorado.phet.common.model.Particle;
+import edu.colorado.phet.idealgas.model.IdealGasModel;
 
 import java.util.ArrayList;
 
 public class CollisionFactory {
 
-    //
-    // Static fields and methods
-    //
     static private ArrayList collisionPrototypes = new ArrayList();
 
     static public void addPrototype( Collision collision ) {
         collisionPrototypes.add( collision );
     }
 
-    static public Collision create( Particle particleA, Particle particleB ) {
+    static public Collision create( Particle particleA, Particle particleB,
+                                    IdealGasModel model, double dt ) {
         Collision result = null;
         for( int i = 0; i < collisionPrototypes.size() && result == null; i++ ) {
             Collision collisionPrototype = (Collision)collisionPrototypes.get( i );
-            result = collisionPrototype.createIfApplicable( particleA, particleB );
+            result = collisionPrototype.createIfApplicable( particleA, particleB, model, dt );
         }
         return result;
     }
