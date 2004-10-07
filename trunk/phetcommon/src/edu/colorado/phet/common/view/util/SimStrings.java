a55aa528604eae25d6effc33fc8cc07f10823a18
joetreat <joetreat@3dd8ee02-672a-0410-8165-a85d635670bd>
2004-10-07 17:00:54 +0000

    Added support for multiple resource bundles

--- trunk/phetcommon/src/edu/colorado/phet/common/view/util/SimStrings.java
+++ trunk/phetcommon/src/edu/colorado/phet/common/view/util/SimStrings.java
@@ -6,20 +6,45 @@
  */
 package edu.colorado.phet.common.view.util;
 
+import java.util.Iterator;
 import java.util.ResourceBundle;
+import java.util.Vector;
 
 public class SimStrings {
 
-    private static ResourceBundle localizedStrings;
+    private static Vector localizedStrings;
 
     public static void setStrings( String stringsPath ) {
-        localizedStrings = ResourceBundle.getBundle( stringsPath );
+        if ( localizedStrings == null ) {
+            localizedStrings = new Vector();
+        }
+        try {
+            ResourceBundle rb = ResourceBundle.getBundle( stringsPath );
+            if ( rb != null ) {
+                localizedStrings.add( rb );
+            }
+        } catch (Exception x) {
+            System.out.println( "SimStrings.setStrings: " + x );
+        }
     }
 
     public static String get( String key ) {
         if( localizedStrings == null ) {
             throw new RuntimeException( "Strings not initialized" );
         }
-        return localizedStrings.getString( key );
+        
+        String value = null;
+        
+        for ( Iterator i = localizedStrings.iterator(); value == null && i.hasNext(); ) {
+            try {
+                ResourceBundle rb = (ResourceBundle) i.next();
+
+                value = rb.getString( key );
+            } catch (Exception x) {
+                value = null;
+            }
+        }
+        
+        return value;
     }
 }
