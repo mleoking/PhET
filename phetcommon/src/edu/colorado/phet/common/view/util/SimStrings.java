001f30c6e16983cb0db2eee0c4c097c4253ed8c9
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2004-10-08 22:56:51 +0000

    Short term fix to load srtings for PhetCommon when simulation using PhetCommon is not yet internationalized.

--- phetcommon/src/edu/colorado/phet/common/view/util/SimStrings.java
+++ phetcommon/src/edu/colorado/phet/common/view/util/SimStrings.java
@@ -14,37 +14,44 @@ public class SimStrings {
 
     private static Vector localizedStrings;
 
+    static {
+        SimStrings.setStrings( "localization/CommonStrings" );
+    }
+
     public static void setStrings( String stringsPath ) {
-        if ( localizedStrings == null ) {
+        if( localizedStrings == null ) {
             localizedStrings = new Vector();
         }
         try {
             ResourceBundle rb = ResourceBundle.getBundle( stringsPath );
-            if ( rb != null ) {
+            if( rb != null ) {
                 localizedStrings.add( rb );
             }
-        } catch (Exception x) {
+        }
+        catch( Exception x ) {
             System.out.println( "SimStrings.setStrings: " + x );
         }
     }
 
     public static String get( String key ) {
         if( localizedStrings == null ) {
+
             throw new RuntimeException( "Strings not initialized" );
         }
-        
+
         String value = null;
-        
-        for ( Iterator i = localizedStrings.iterator(); value == null && i.hasNext(); ) {
+
+        for( Iterator i = localizedStrings.iterator(); value == null && i.hasNext(); ) {
             try {
-                ResourceBundle rb = (ResourceBundle) i.next();
+                ResourceBundle rb = (ResourceBundle)i.next();
 
                 value = rb.getString( key );
-            } catch (Exception x) {
+            }
+            catch( Exception x ) {
                 value = null;
             }
         }
-        
+
         return value;
     }
 }
