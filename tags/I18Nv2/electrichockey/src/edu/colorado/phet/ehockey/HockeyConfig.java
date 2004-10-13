c9b8f3778d9d29a640978a511c588e2cc6285793
nobody <nobody@3dd8ee02-672a-0410-8165-a85d635670bd>
2004-10-13 16:13:10 +0000

    This commit was manufactured by cvs2svn to create tag 'I18Nv2'.

---
+++ tags/I18Nv2/electrichockey/src/edu/colorado/phet/ehockey/HockeyConfig.java
@@ -0,0 +1,8 @@
+
+package edu.colorado.phet.ehockey;
+
+public class HockeyConfig {
+
+    public static final String localizedStringPath = "localization/HockeyStrings";
+
+} // end of public class HockeyConfig
