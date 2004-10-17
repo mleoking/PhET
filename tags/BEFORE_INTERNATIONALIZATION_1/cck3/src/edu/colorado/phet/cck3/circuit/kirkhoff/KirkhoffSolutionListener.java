64d72e2836dcfde104c0c2118808ac6db3d665ec
nobody <nobody@3dd8ee02-672a-0410-8165-a85d635670bd>
2004-10-17 17:42:26 +0000

    This commit was manufactured by cvs2svn to create tag 'BEFORE_INTERNATIONALIZATION_1'.

---
+++ tags/BEFORE_INTERNATIONALIZATION_1/cck3/src/edu/colorado/phet/cck3/circuit/kirkhoff/KirkhoffSolutionListener.java
@@ -0,0 +1,11 @@
+package edu.colorado.phet.cck3.circuit.kirkhoff;
+
+/**
+ * User: Sam Reid
+ * Date: Jun 14, 2004
+ * Time: 8:20:04 PM
+ * Copyright (c) Jun 14, 2004 by Sam Reid
+ */
+public interface KirkhoffSolutionListener {
+    void finishedKirkhoff();
+}
