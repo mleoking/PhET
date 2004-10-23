80c085c0c43da977a70eb7fbfc58fe281f97b50a
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2004-10-23 15:05:40 +0000

    no message

---
+++ trunk/collision/src/edu/colorado/phet/collision/SphereWallExpert.java
@@ -0,0 +1,15 @@
+/**
+ * Class: SphereWallExpert
+ * Class: edu.colorado.phet.collision
+ * User: Ron LeMaster
+ * Date: Oct 23, 2004
+ * Time: 7:40:08 AM
+ * $Author$
+ */
+package edu.colorado.phet.collision;
+
+public class SphereWallExpert implements CollisionExpert {
+    public boolean detectAndDoCollision( Collidable bodyA, Collidable bodyB ) {
+        throw new RuntimeException( "TBI" );
+    }
+}
