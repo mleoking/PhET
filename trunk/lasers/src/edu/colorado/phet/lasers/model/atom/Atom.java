584b5236e8673dd7b16d53ecac7cded858ee7d6c
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2004-10-25 04:39:56 +0000

    Moved all modules to a module package so they will be easier to find in the IDE.

--- trunk/lasers/src/edu/colorado/phet/lasers/model/atom/Atom.java
+++ trunk/lasers/src/edu/colorado/phet/lasers/model/atom/Atom.java
@@ -53,6 +53,7 @@ public class Atom extends SphericalBody {
     public interface Listener {
         void photonEmitted( Atom atom, Photon photon );
         void leftSystem( Atom atom );
+        void stateChanged( Atom atom, AtomicState oldState, AtomicState newState );
     }
 
     public Atom() {
@@ -79,9 +80,16 @@ public class Atom extends SphericalBody {
         return state;
     }
 
-    public void setState( AtomicState newState ) {
+    public void setState( final AtomicState newState ) {
+        final AtomicState oldState = this.state;
         this.state = newState;
         notifyObservers();
+
+        this.subscriptionService.notifyListeners( new SubscriptionService.Notifier() {
+            public void doNotify( Object obj ) {
+                ((Listener)obj).stateChanged( Atom.this, oldState, newState );
+            }
+        } );
     }
 
     public void stepInTime( double dt ) {
