b0a86e2b9d02d193addf846d436960c691ace5ff
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2004-10-26 22:08:39 +0000

    no message

---
+++ trunk/lasers/src/edu/colorado/phet/lasers/LaserSimulation.java
@@ -0,0 +1,94 @@
+/**
+ * Class: LaserSimulation
+ * Package: edu.colorado.phet.lasers.controller
+ * Author: Another Guy
+ * Date: Mar 21, 2003
+ * Latest Change:
+ *      $Author$
+ *      $Date$
+ *      $Name$
+ *      $Revision$
+ */
+package edu.colorado.phet.lasers;
+
+import edu.colorado.phet.common.application.ApplicationModel;
+import edu.colorado.phet.common.application.Module;
+import edu.colorado.phet.common.application.PhetApplication;
+import edu.colorado.phet.common.model.clock.AbstractClock;
+import edu.colorado.phet.common.model.clock.SwingTimerClock;
+import edu.colorado.phet.lasers.controller.module.MultipleAtomThreeLevelModule;
+import edu.colorado.phet.lasers.controller.module.MultipleAtomTwoLevelModule;
+import edu.colorado.phet.lasers.controller.module.OneAtomThreeLevelsModule;
+import edu.colorado.phet.lasers.controller.module.OneAtomTwoLevelsModule;
+
+public class LaserSimulation extends PhetApplication {
+
+    static class LaserAppModel extends ApplicationModel {
+        public LaserAppModel() {
+            super( "Lasers", "Lasers", "0.1" );
+
+            AbstractClock clock = new SwingTimerClock( 10, 20 );
+            setClock( clock );
+            setFrameCenteredSize( 1000, 800 );
+
+            Module singleAtomModule = new OneAtomTwoLevelsModule( clock );
+            Module oneAtomThreeLevelsModule = new OneAtomThreeLevelsModule( clock );
+            Module multipleAtomTwoLevelModule = new MultipleAtomTwoLevelModule( clock );
+            Module multipleAtomThreeLevelModule = new MultipleAtomThreeLevelModule( clock );
+            //            Module testApparatusModule = new TestApparatusModule();
+            Module[] modules = new Module[]{
+                singleAtomModule,
+                oneAtomThreeLevelsModule,
+                multipleAtomTwoLevelModule,
+                multipleAtomThreeLevelModule,
+                //                testApparatusModule
+            };
+            setModules( modules );
+            setInitialModule( singleAtomModule );
+        }
+    }
+
+    public LaserSimulation() {
+        super( new LaserAppModel() );
+    }
+
+
+    //    protected PhetMainPanel createMainPanel() {
+    //        return new LaserMainPanel( this );
+    //    }
+    //
+    //    protected JMenu createControlsMenu( PhetFrame phetFrame ) {
+    //        return new ControlsMenu( phetFrame, this );
+    //    }
+    //
+    //
+    //    protected JMenu createTestMenu() {
+    //        return null;
+    //    }
+    //
+    //    public GraphicFactory getGraphicFactory() {
+    //        return LaserGraphicFactory.instance();
+    //    }
+    //
+    //    protected PhetAboutDialog getAboutDialog( PhetFrame phetFrame ) {
+    //        return null;
+    //    }
+
+    //    protected Config getConfig() {
+    //        return LaserConfig.instance();
+    //    }
+
+
+    public void displayHighToMidEmission( boolean selected ) {
+        throw new RuntimeException( "TBI" );
+        //        LaserGraphicFactory.instance().setHighToMidEmissionsVisible( selected );
+    }
+
+    //
+    // Static fields and methods
+    //
+    public static void main( String[] args ) {
+        LaserSimulation simulation = new LaserSimulation();
+        simulation.startApplication();
+    }
+}
