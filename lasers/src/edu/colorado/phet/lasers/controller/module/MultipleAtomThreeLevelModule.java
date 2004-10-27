f3617714697242d45e1f3a26d80a7fdf1a13f4d9
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2004-10-27 21:18:04 +0000

    Consolidated control panels

--- lasers/src/edu/colorado/phet/lasers/controller/module/MultipleAtomThreeLevelModule.java
+++ lasers/src/edu/colorado/phet/lasers/controller/module/MultipleAtomThreeLevelModule.java
@@ -13,8 +13,7 @@
 package edu.colorado.phet.lasers.controller.module;
 
 import edu.colorado.phet.common.model.clock.AbstractClock;
-import edu.colorado.phet.common.view.PhetControlPanel;
-import edu.colorado.phet.lasers.controller.ThreeLevelControlPanel;
+import edu.colorado.phet.lasers.controller.LaserControlPanel;
 import edu.colorado.phet.lasers.model.LaserModel;
 import edu.colorado.phet.lasers.view.ThreeEnergyLevelMonitorPanel;
 
@@ -25,15 +24,17 @@ public class MultipleAtomThreeLevelModule extends MultipleAtomBaseModule {
     private double s_maxSpeed = .1;
 
     private ArrayList atoms;
-    private PhetControlPanel controlPanel;
+    private LaserControlPanel controlPanel;
+    //    private PhetControlPanel controlPanel;
 
     /**
      *
      */
     public MultipleAtomThreeLevelModule( AbstractClock clock ) {
         super( "Multiple Atoms / Three Levels" );
-        controlPanel = new ThreeLevelControlPanel( this, clock );
+        controlPanel = new LaserControlPanel( this, clock );
         setControlPanel( controlPanel );
+        controlPanel.setThreeEnergyLevels( true );
         setEnergyMonitorPanel( new ThreeEnergyLevelMonitorPanel( (LaserModel)getModel() ) );
     }
 }
--- lasers/src/edu/colorado/phet/lasers/controller/module/OneAtomThreeLevelsModule.java
+++ lasers/src/edu/colorado/phet/lasers/controller/module/OneAtomThreeLevelsModule.java
@@ -13,36 +13,19 @@
 package edu.colorado.phet.lasers.controller.module;
 
 import edu.colorado.phet.common.model.clock.AbstractClock;
-import edu.colorado.phet.lasers.controller.ThreeLevelControlPanel;
+import edu.colorado.phet.lasers.controller.LaserControlPanel;
 import edu.colorado.phet.lasers.model.LaserModel;
-import edu.colorado.phet.lasers.model.atom.Atom;
 import edu.colorado.phet.lasers.view.ThreeEnergyLevelMonitorPanel;
 
 public class OneAtomThreeLevelsModule extends SingleAtomBaseModule {
-
-    private Atom atom;
 
     public OneAtomThreeLevelsModule( AbstractClock clock ) {
         super( "One Atom / Three Energy Levels" );
 
         setEnergyMonitorPanel( new ThreeEnergyLevelMonitorPanel( (LaserModel)getModel() ) );
-        //        setMonitorPanel( monitorPanel  );
-        setControlPanel( new ThreeLevelControlPanel( this, clock ) );
+        LaserControlPanel controlPanel = new LaserControlPanel( this, clock );
+        controlPanel.setThreeEnergyLevels( true );
+        controlPanel.setMaxPhotonRate( 5 );
+        setControlPanel( controlPanel );
     }
-
-    //    public void activate( PhetApplication app ) {
-    //        super.activate( app );
-    //
-    //        atom = new Atom();
-    //        atom.setPosition( getLaserOrigin().getX() + s_boxWidth / 2,
-    //                          getLaserOrigin().getY() + s_boxHeight / 2  );
-    //        atom.setVelocity( 0, 0 );
-    //        addAtom( atom );
-    //    }
-    //
-    //    public void deactivate( PhetApplication app ) {
-    //        super.deactivate( app );
-    //        getLaserModel().removeModelElement( atom );
-    //        atom.removeFromSystem();
-    //    }
 }
