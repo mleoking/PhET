e326ddec5cc6584dd4e75c86f991f1d39ce369ec
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2004-10-29 21:18:31 +0000

    Factored out DelayStrategy.

--- trunk/phetcommon/src/edu/colorado/phet/common/model/clock/SwingTimerClock.java
+++ trunk/phetcommon/src/edu/colorado/phet/common/model/clock/SwingTimerClock.java
@@ -12,13 +12,12 @@ import java.awt.event.ActionListener;
 
 public class SwingTimerClock extends AbstractClock {
 
-    Timer timer;
+    private Timer timer;
     private long lastTickTime;
-    private int nominalWaitTime;
+    private DelayStrategy delayStrategy;
 
     public SwingTimerClock( double dt, int delay ) {
         this( dt, delay, true );
-        nominalWaitTime = delay;
     }
 
     /**
@@ -27,6 +26,7 @@ public class SwingTimerClock extends AbstractClock {
     public SwingTimerClock( double dt, int delay, boolean isFixed ) {
         super( dt, delay, isFixed );
         timer = new Timer( delay, new Ticker() );
+        delayStrategy = new DynamicDelay( delay );
     }
 
     public void doStart() {
@@ -37,6 +37,7 @@ public class SwingTimerClock extends AbstractClock {
     public void setDelay( int delay ) {
         super.setDelay( delay );
         timer.setDelay( delay );
+        this.delayStrategy = new ConstantDelay( delay );
     }
 
     protected void doStop() {
@@ -60,13 +61,49 @@ public class SwingTimerClock extends AbstractClock {
         }
     }
 
+    private static interface DelayStrategy {
+        int getDelay( long actualWaitTime );
+    }
+
+    private static class ConstantDelay implements DelayStrategy {
+        private int delay;
+
+        public ConstantDelay( int delay ) {
+            this.delay = delay;
+        }
+
+        public int getDelay( long actualWaitTime ) {
+            return delay;
+        }
+    }
+
+    private static class DynamicDelay implements DelayStrategy {
+        private int delay;
+        private int min;
+
+        public DynamicDelay( int delay ) {
+            this( delay, 5 );
+        }
+
+        public DynamicDelay( int delay, int min ) {
+            this.delay = delay;
+            this.min = min;
+        }
+
+        public int getDelay( long actualWaitTime ) {
+            int result = Math.max( delay - (int)actualWaitTime, min );
+            return result;
+        }
+    }
+
     private class Ticker implements ActionListener {
 
         public void actionPerformed( ActionEvent e ) {
             if( isRunning() ) {
                 long tickTime = System.currentTimeMillis();
                 long actualWaitTime = tickTime - lastTickTime;
-                                timer.setDelay( Math.max( nominalWaitTime - (int)actualWaitTime, 5 ));
+                int delay = delayStrategy.getDelay( actualWaitTime );
+                timer.setDelay( delay );
                 lastTickTime = tickTime;
                 if( isRunning() ) {
                     clockTicked( getSimulationTime( actualWaitTime ) );
