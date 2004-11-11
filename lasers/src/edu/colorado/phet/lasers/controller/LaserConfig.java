8ccc66325b3604334b18c6c828d56f10c65e177a
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2004-11-11 04:04:47 +0000

    Moved liftime sliders to the right of the energy levels dialog, and they now move up and down with the levels. Removed all static structures from AtomicState and its subclasses.

--- lasers/src/edu/colorado/phet/lasers/controller/LaserConfig.java
+++ lasers/src/edu/colorado/phet/lasers/controller/LaserConfig.java
@@ -63,8 +63,8 @@ public class LaserConfig {
     public static final String GROUND_STATE_IMAGE_FILE = IMAGE_DIRECTORY + "particle-gray-med.gif";
     public static final String HIGH_ENERGY_STATE_IMAGE_FILE = IMAGE_DIRECTORY + "particle-blue-med.gif";
     public static final String MIDDLE_ENERGY_STATE_IMAGE_FILE = IMAGE_DIRECTORY + "particle-red-med.gif";
-    public static final String PHOTON_IMAGE_FILE = IMAGE_DIRECTORY + "photon-comet.png";
-    //    public static final String PHOTON_IMAGE_FILE = IMAGE_DIRECTORY + "particle-red-xsml.gif";
+//    public static final String PHOTON_IMAGE_FILE = IMAGE_DIRECTORY + "photon-comet.png";
+        public static final String PHOTON_IMAGE_FILE = IMAGE_DIRECTORY + "particle-red-med.gif";
     public static final String MID_HIGH_ENERGY_PHOTON_IMAGE_FILE = IMAGE_DIRECTORY + "particle-deep-red-xsml.gif";
     public static final String HIGH_ENERGY_PHOTON_IMAGE_FILE = IMAGE_DIRECTORY + "particle-blue-xsml.gif";
     public static final String LOW_ENERGY_PHOTON_IMAGE_FILE = IMAGE_DIRECTORY + "particle-red-xsml.gif";
