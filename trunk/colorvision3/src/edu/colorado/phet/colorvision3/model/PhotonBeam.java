fbe9c7f21cd39ad9ae78779d9be8f0624b61eadc
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2004-11-13 23:33:34 +0000

    Major changes to stepInTime to handle filtering and white light source properly. This is the first version that appears to work correctly.

--- trunk/colorvision3/src/edu/colorado/phet/colorvision3/model/PhotonBeam.java
+++ trunk/colorvision3/src/edu/colorado/phet/colorvision3/model/PhotonBeam.java
@@ -35,6 +35,8 @@ public class PhotonBeam extends SimpleObservable implements SimpleObserver, Mode
   private static final int FILTERED_PHOTON_ADVANCE = 20;
   // Photon delta.
   public static final int PHOTON_DS = 10;
+  // White photons are culled by this amount when hitting a filter.
+  private static final double WHITE_CULL_FACTOR = 0.5;
 
 	//----------------------------------------------------------------------------
 	// Instance data
@@ -130,7 +132,7 @@ public class PhotonBeam extends SimpleObservable implements SimpleObserver, Mode
   }
   
   /**
-   * Gets the base color perceived by the user.
+   * Gets the photon color perceived by the user.
    * This is the color of the most recent photon to leave the bounds.
    * 
    * @return the color
@@ -141,7 +143,7 @@ public class PhotonBeam extends SimpleObservable implements SimpleObserver, Mode
   }
   
   /**
-   * Gets the intensity perceived by the user.
+   * Gets the light source intensity perceived by the user.
    * This is the intensity of the most recent photon to leave the bounds.
    * 
    * @return the perceived intensity
@@ -153,7 +155,7 @@ public class PhotonBeam extends SimpleObservable implements SimpleObserver, Mode
   
   /**
    * Enables or disables the beam.
-   * A disabled beam does not send ColorChangeEvents.
+   * A disabled beam does not send notify observers or send ColorChangeEvents to listeners.
    * 
    * @param enabled true to enable, false to disable
    */
@@ -199,37 +201,88 @@ public class PhotonBeam extends SimpleObservable implements SimpleObserver, Mode
 
   /**
    * Called each time the simulation clock ticks.
+   * <p>
+   * THIS IS THE MOST COMPLICATED METHOD IN THE ENTIRE COLOR VISION SIMULATION.
+   * READ THIS DOCUMENTATION THOROUGHLY BEFORE ATTEMPTING ANY MODIFICATIONS!  
+   * <p>
    * Walks the photon list exactly once, and advances or prunes photons
    * based on their location and optional color filtering.  Pruned photons 
-   * are available for reuse. The current intensity determines how many 
-   * photons must be initialized, either via reuse or by creating new photons.
+   * are available for reuse. The current light intensity determines how many 
+   * photons must be emitted, either via reuse or by creating new photons.
    * <p>
-   * Note that this algorithm assumes that the photons are traveling 
-   * left-to-right in its treatment of filtering and coordinate generation.
+   * If the photon beam is enabled, observers are notified each time that
+   * the method is called and at least one photon moves.
+   * <p>
+   * If the photon beam is enabled and at least one photon leaves the 
+   * beam's bounds, listeners are sent a ColorChangeEvent.  If more than
+   * one photon leaves the bounds, only one ColorChangeEvent is sent,
+   * containing the information about the last photon to leave.
+   * <p>
+   * Note that handling of filtering is problematic.  When a collection
+   * of photons hits the filter, the collection has to be culled based 
+   * on how the filter passes the photons.  The only way to cull the 
+   * collection is to assume that all photons in the collection have 
+   * the same color. So we use the attributes of the last photon to
+   * be passed by the filter to determine the effect of the filter 
+   * on the entire collection.
+   * <p>
+   * HACK: Technically, a photon cannot have a color of white. A white 
+   * light source should emit a collection of photons whose color is
+   * distributed over the visible spectrum.  But if we did this, we 
+   * would have no way of properly setting the perceived color when 
+   * one of these photons leaves the beam bounds.  So we allow photons
+   * to have a color of white, and leave it up to whatever is visually
+   * representing white photons to decide how to render them.
+   * (For example, PhotonBeamGraphic selects a random wavelength
+   * for painting a white photon.)
+   * <p>
+   * HACK: This algorithm's handling of filtering and motion assumes
+   * that photons are traveling left-to-right.
    * 
    * @param dt time delta, currently ignored
    */
   public void stepInTime( double dt )
   {
     Photon photon;
-    int passedCount = 0;
-    int stepCount = 0;
-    double lastFilteredX = 0.0;
-    double lastFilteredY = 0.0;
-    VisibleColor newPerceivedColor = _perceivedColor;
-    double newPerceivedIntensity = _perceivedIntensity;
     
-    // Determine how many photons to emit.
-    // If the intensity is changing to from non-zero to zero, emit one 
-    // photon with zero intensity to ensure that zero intensity is 
-    // perceived by the viewer.
-    int allocCount = (int) ((_spotlightModel.getIntensity() / 100) * _maxPhotons);
-    if ( allocCount == 0 && _perceivedIntensity != 0.0 )
+    // Number of photons that advanced.
+    int stepCount = 0;
+    // Number of photons that passed through the filter.
+    int passedCount = 0;
+    // X location of the last photon to be blocked by the filter.
+    double lastBlockedX = 0.0;
+    // Y location of the last photon to be blocked by the filter.
+    double lastBlockedY = 0.0;
+    // Color of the last photon to leave the bounds.
+    VisibleColor newPerceivedColor = _perceivedColor;
+    // Intensity of the last photon to leave the bounds.
+    double newPerceivedIntensity = _perceivedIntensity;
+    // Photons that passed through the filter, array of Photon.
+    ArrayList filteredPhotons = new ArrayList();
+    // Percentage of color passed by the filter for the last photon to pass through the filter.
+    double lastPercentPassed = 0;
+    
+    // Does this photon beam have a filter?
+    boolean hasFilter = ( _filterModel != null );
+    // Does this photon beam have an enabled filter?
+    boolean hasEnabledFilter = ( _filterModel != null && _filterModel.isEnabled() );
+    
+    // Determine how many photons to emit from the light source. 
+    int allocCount = 0;
+    if ( _spotlightModel.getIntensity() == 0 && _perceivedIntensity != 0 )
     {
+      // If the light intensity is changing to from non-zero to zero, 
+      // emit one photon with zero intensity to ensure that zero intensity 
+      // is perceived by the viewer.
       allocCount = 1;
     }
+    else
+    {
+      // Number of photons is based on the light source intensity.
+      allocCount = (int) ((_spotlightModel.getIntensity() / 100) * _maxPhotons);
+    }
     
-    // Walk the photon array and paint/prune/filter/reinitialize as needed.
+    // For each photon...
     for ( int i = 0; i < _photons.size(); i++ )
     {
       photon = (Photon)_photons.get(i);
@@ -248,37 +301,46 @@ public class PhotonBeam extends SimpleObservable implements SimpleObserver, Mode
           newPerceivedColor = photon.getColor();
           newPerceivedIntensity = photon.getIntensity();
         }
-        else if ( _filterModel != null && ! photon.isFiltered() )
+        else if ( hasFilter && ! photon.isFiltered() && 
+                  photon.getX() > _filterModel.getX() + FILTER_CENTER_OFFSET )
         {
-          // If we have an active filter and the photon has not been previously
-          // filtered, then determine whether the photon should pass the filter
-          // and what its new color should be.
-          // Note! This assumes the photon is traveling left-to-right horizontally.
-          if ( photon.getX() > _filterModel.getX() + FILTER_CENTER_OFFSET )
+          // HACK: Above expression assumes the photon is traveling left-to-right.      
+          // If the photon has just passed the filter, mark it as filtered,
+          // regardless of whether the filter is enabled.  A photon is subject 
+          // to filtering only at the moment that it passed the filter, and
+          // this prevents it from being filtered if the filter is enabled.
+          photon.setFiltered( true );
+          
+          // If the filter is enabled, determine its effect on the photon.
+          if ( hasEnabledFilter )
           {
-            VisibleColor color = _filterModel.colorPassed( photon.getColor() );
-            if ( color.equals( VisibleColor.INVISIBLE ) )
+            // Determine what percentage of the photon's wavelength is passed by the filter.
+            double percentPassed = _filterModel.percentPassed( photon.getColor() );
+            if ( percentPassed == 0 )
             {
-              // If the filter passes no color, then mark the photon as available.
+              // If the photon's color doesn't pass the filter, 
+              // then mark the photon as available.
               photon.setInUse( false );
-              lastFilteredX = photon.getX();
-              lastFilteredY = photon.getY();
+              lastBlockedX = photon.getX();
+              lastBlockedY = photon.getY();
             }
-            else
+            else 
             {
-              // Photon's color is the filtered color.
-              photon.setColor( color );
-              photon.setFiltered( true );
-              if ( _filterModel.isEnabled() )
-              {
-                // Advance it a bit so it looks like it goes through the filter.
-                photon.setLocation( photon.getX() + FILTERED_PHOTON_ADVANCE, photon.getY() );
-                passedCount++;
-              }
+              // Some of the photon's color is passed by the filter.
+              filteredPhotons.add( photon );
+              
+              // HACK: Advance the photon so it looks like it goes through the filter.
+              photon.setLocation( photon.getX() + FILTERED_PHOTON_ADVANCE, photon.getY() );
+              
+              // Adjust the photon's intensity.
+              photon.setIntensity( percentPassed );
+              
+              passedCount++;
+              lastPercentPassed = percentPassed;
             }
-          }
-        }
-      }
+          } // if the filter is enabled
+        } // if photon has hit the filter
+      } // if the photon is in use
       
       // If the photon is not in use and we need to allocate photons,
       // then reuse the photon.
@@ -290,46 +352,97 @@ public class PhotonBeam extends SimpleObservable implements SimpleObserver, Mode
  
         photon.setLocation( x, y );
         photon.setDirection( direction );
-        photon.setColor( _spotlightModel.getColor() );
         photon.setIntensity( _spotlightModel.getIntensity() );
         photon.setInUse( true );
         photon.setFiltered( false );
-       
+        photon.setColor( _spotlightModel.getColor() );
+        
         allocCount--;
       }
     } // for each photon
 
+    // Cull filtered photons.
+    if ( filteredPhotons.size() > 0 )
+    {
+      int count = filteredPhotons.size();
+      
+      // White photons are handled specially.
+      // HACK: Use the last photon passed by the filter to decide whether they are all white.
+      Photon lastPhotonPassed = (Photon)filteredPhotons.get(count-1);
+      double wavelength = lastPhotonPassed.getColor().getWavelength();
+      boolean isWhite = (wavelength == VisibleColor.WHITE_WAVELENGTH );
+
+      // Determine how many to cull.
+      int cull = 0;
+      if ( isWhite )
+      {
+        // White photons are always culled by the same factor.
+        cull = (int)(count * WHITE_CULL_FACTOR);
+      }
+      else
+      {
+        // Monochrome photons are culled based on the how much of the
+        // last filtered photon was passed.
+        cull = (int) (count - (count * lastPercentPassed / 100));
+      }
+      
+      // Cull the photons by marking them as available.
+      for ( int i = 0; i < cull; i++ )
+      {
+        ((Photon)filteredPhotons.get(i)).setInUse( false );
+        passedCount--;
+      }
+      
+      // Convert white photons to the filter's color.
+      if ( isWhite )
+      {
+        VisibleColor filterColor = _filterModel.getTransmissionPeak();
+        for ( int j = cull; j < count; j++ )
+        {
+          ((Photon)filteredPhotons.get(j)).setColor( filterColor );
+        }
+      }
+    }
+    
+    // NOTE: Culling may have marked some photons as available by this point.
+    // Rather than make another trip through the photons array, we'll choose
+    // to allocate new photons if needed.  The available photons will be 
+    // reused the next time this method is called.
+    
     // If we didn't find enough available photons, allocate some new ones.
     while ( allocCount > 0 )
     {
-      double direction = genDirection( _spotlightModel.getDirection() );
-      double x = genX( _spotlightModel.getX() );
-      photon = new Photon( _spotlightModel.getColor(),
-                                  _spotlightModel.getIntensity(), 
-                                  x, _spotlightModel.getY(),
-                                  direction );
+      photon = new Photon( _spotlightModel.getColor(), 
+                           _spotlightModel.getIntensity(), 
+                           genX( _spotlightModel.getX() ), 
+                           _spotlightModel.getY(),
+                           genDirection( _spotlightModel.getDirection() ) );
       _photons.add( photon );
       
       allocCount--;
     }
     
-    // If filtering is enabled and we passed no photons, then send a photon
-    // with "no color", starting at the location of the last photon that the 
-    // filter blocked.
-    if ( _filterModel != null && _filterModel.isEnabled() && passedCount == 0 && 
-        _perceivedColor != null && !_perceivedColor.equals(VisibleColor.INVISIBLE) )
+    // If filtering is enabled and we passed no photons, and there is still 
+    // a visible perceived color, then send a photon with "no color", starting 
+    // at the location of the last photon that the filter blocked.
+    if ( hasEnabledFilter && 
+         passedCount == 0 &&
+         _perceivedColor != null && 
+         _perceivedColor.getWavelength() != VisibleColor.INVISIBLE_WAVELENGTH )
     {
-      
-      photon = new Photon( VisibleColor.INVISIBLE, 0, lastFilteredX, lastFilteredY, _spotlightModel.getDirection() );
+      // NOTE: It would be better, but more costly, to use an available photon.
+      photon = new Photon( VisibleColor.INVISIBLE, 0, lastBlockedX, lastBlockedY, _spotlightModel.getDirection() );
       _photons.add( photon );
     }
     
-    // If the perceived color or intensity has changed, then fire a VisibleColorChangeEvent.
-    if ( newPerceivedIntensity != _perceivedIntensity || !newPerceivedColor.equals(_perceivedColor) )
+    // If the perceived color or intensity has changed...
+    if ( newPerceivedIntensity != _perceivedIntensity || 
+         newPerceivedColor.getWavelength() != _perceivedColor.getWavelength() )
     {
       _perceivedColor = newPerceivedColor;
       _perceivedIntensity = newPerceivedIntensity;
       
+      // Fire a VisibleColorChangeEvent if the beam is enabled.
       if ( _enabled )
       {
         // Calculate the new color.
@@ -346,8 +459,7 @@ public class PhotonBeam extends SimpleObservable implements SimpleObserver, Mode
       }
     }
     
-    // If the photon beam is enabled and at least one photon moved, 
-    // notify observers that the photon beam has changed.
+    // Notify observers if the beam is enabled and at least one photon moved.
     if ( _enabled && stepCount > 0 )
     {
       notifyObservers();
@@ -382,7 +494,7 @@ public class PhotonBeam extends SimpleObservable implements SimpleObserver, Mode
     // Add the delta to the original direction.
     return (direction + delta);
   }
-
+  
 	//----------------------------------------------------------------------------
 	// SimpleObserver implementation
   //----------------------------------------------------------------------------
