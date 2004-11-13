f03ab5212e8eedf396ba46686a5388a4841a5650
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2004-11-13 05:28:50 +0000

    Forces1D init.

---
+++ forces1d/src/edu/colorado/phet/forces1d/Force1DUtil.java
@@ -0,0 +1,14 @@
+/** Sam Reid*/
+package edu.colorado.phet.forces1d;
+
+/**
+ * User: Sam Reid
+ * Date: Nov 12, 2004
+ * Time: 10:27:37 PM
+ * Copyright (c) Nov 12, 2004 by Sam Reid
+ */
+public class Force1DUtil {
+    public static void main( String[] args ) {
+        
+    }
+}
