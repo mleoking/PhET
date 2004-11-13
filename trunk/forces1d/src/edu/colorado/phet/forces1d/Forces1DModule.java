b0507cd0822e0055ea652b3471f00046deab4425
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2004-11-13 05:28:50 +0000

    Forces1D init.

--- trunk/forces1d/src/edu/colorado/phet/forces1d/Forces1DModule.java
+++ trunk/forces1d/src/edu/colorado/phet/forces1d/Forces1DModule.java
@@ -4,9 +4,13 @@ package edu.colorado.phet.forces1d;
 import edu.colorado.phet.common.application.ApplicationModel;
 import edu.colorado.phet.common.application.Module;
 import edu.colorado.phet.common.application.PhetApplication;
+import edu.colorado.phet.common.model.BaseModel;
 import edu.colorado.phet.common.model.clock.AbstractClock;
 import edu.colorado.phet.common.model.clock.SwingTimerClock;
 import edu.colorado.phet.common.view.util.FrameSetup;
+import edu.colorado.phet.forces1d.common.PhetLookAndFeel;
+
+import javax.swing.*;
 
 /**
  * User: Sam Reid
@@ -18,9 +22,13 @@ public class Forces1DModule extends Module {
 
     public Forces1DModule() {
         super( "Forces1D" );
+        Force1DPanel apparatusPanel = new Force1DPanel( this );
+        setApparatusPanel( apparatusPanel );
+        setModel( new BaseModel() );
     }
 
-    public static void main( String[] args ) {
+    public static void main( String[] args ) throws UnsupportedLookAndFeelException {
+        UIManager.setLookAndFeel( new PhetLookAndFeel() );
         Forces1DModule module = new Forces1DModule();
         AbstractClock clock = new SwingTimerClock( 1, 30 );
         ApplicationModel model = new ApplicationModel( "Forces 1D", "Force1d applet", "1.0Alpha", new FrameSetup.CenteredWithSize( 800, 800 ), module, clock );
