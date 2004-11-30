64c81d7268c1d20b4ab96689fa0ffc22510132d4
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2004-11-30 02:28:37 +0000

    fix CVS Info header that got mangled when code was reformatted

--- trunk/colorvision3/src/edu/colorado/phet/colorvision3/SingleBulbModule.java
+++ trunk/colorvision3/src/edu/colorado/phet/colorvision3/SingleBulbModule.java
@@ -1,8 +1,11 @@
 /* Copyright 2004, University of Colorado */
 
 /*
- * CVS Info - Filename : $Source$
- * Branch : $Name$ Modified by : $Author$ Revision : $Revision$
+ * CVS Info - 
+ * Filename : $Source$
+ * Branch : $Name$ 
+ * Modified by : $Author$ 
+ * Revision : $Revision$
  * Date modified : $Date$
  */
 
