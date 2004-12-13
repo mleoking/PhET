a4aa359c08412b556a2b78781c591b3c99bb377f
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2004-12-13 04:15:56 +0000

    Deleted AspectRatio code, moved main() code to test dir.

--- phetcommon/src/edu/colorado/phet/common/math/MedianFilter.java
+++ phetcommon/src/edu/colorado/phet/common/math/MedianFilter.java
@@ -68,12 +68,4 @@ public class MedianFilter {
         return source;
     }
 
-    public static void main( String[] args ) {
-        double[] d = new double[]{9, 8, 7, 6, 5, 6, 7, 4, 5, 8, 9};
-        MedianFilter mf = new MedianFilter( d );
-        d = mf.filter( 3 );
-        for( int i = 0; i < d.length; i++ ) {
-            System.out.println( d[i] );
-        }
-    }
 }
