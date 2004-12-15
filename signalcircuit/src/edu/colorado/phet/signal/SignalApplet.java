3f652a13b795e78b70b4a6bbec5d550dff23b18c
joetreat <joetreat@3dd8ee02-672a-0410-8165-a85d635670bd>
2004-12-15 00:35:53 +0000

    modified code for updated SimStrings.

--- signalcircuit/src/edu/colorado/phet/signal/SignalApplet.java
+++ signalcircuit/src/edu/colorado/phet/signal/SignalApplet.java
@@ -62,12 +62,12 @@ public class SignalApplet extends JApplet {
     public static void main( String[] args ) {
         String applicationLocale = System.getProperty( "javaws.locale" );
         if( applicationLocale != null && !applicationLocale.equals( "" ) ) {
-            Locale.setDefault( new Locale( applicationLocale ) );
+            SimStrings.setLocale( new Locale( applicationLocale ) );
         }
         String argsKey = "user.language=";
         if( args.length > 0 && args[0].startsWith( argsKey )) {
             String locale = args[0].substring( argsKey.length(), args[0].length() );
-            Locale.setDefault( new Locale( locale ));
+            SimStrings.setLocale( new Locale( locale ));
         }
 
         SimStrings.setStrings( localizedStringsPath );
