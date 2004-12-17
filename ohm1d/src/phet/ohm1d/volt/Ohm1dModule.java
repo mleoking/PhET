b551a2bc181a55b4a6dcc0bc964af9a5b3c88cc7
joetreat <joetreat@3dd8ee02-672a-0410-8165-a85d635670bd>
2004-12-17 15:45:35 +0000

    Changes to call SimStrings.setLocale.

--- ohm1d/src/phet/ohm1d/volt/Ohm1dModule.java
+++ ohm1d/src/phet/ohm1d/volt/Ohm1dModule.java
@@ -72,7 +72,7 @@ public class Ohm1dModule extends JApplet {
         //System.err.println("HI");
         String applicationLocale = Toolkit.getDefaultToolkit().getProperty( "javaws.locale", null );
         if( applicationLocale != null && !applicationLocale.equals( "" ) ) {
-            Locale.setDefault( new Locale( applicationLocale ) );
+            SimStrings.setLocale( new Locale( applicationLocale ) );
         }
         SimStrings.setStrings( localizedStringsPath );
         JButton jb = new JButton( SimStrings.get( "Ohm1dModule.StartButton" ) );
@@ -621,12 +621,12 @@ public class Ohm1dModule extends JApplet {
     public static void main( String[] args ) throws Throwable {
         String applicationLocale = System.getProperty( "javaws.locale" );
         if( applicationLocale != null && !applicationLocale.equals( "" ) ) {
-            Locale.setDefault( new Locale( applicationLocale ) );
+            SimStrings.setLocale( new Locale( applicationLocale ) );
         }
         String argsKey = "user.language=";
         if( args.length > 0 && args[0].startsWith( argsKey )) {
             String locale = args[0].substring( argsKey.length(), args[0].length() );
-            Locale.setDefault( new Locale( locale ));
+            SimStrings.setLocale( new Locale( locale ));
         }
 
         SimStrings.setStrings( localizedStringsPath );
