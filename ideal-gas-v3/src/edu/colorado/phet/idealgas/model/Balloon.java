975e16e66b33b966777beadae0bbc39a7086901d
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2004-12-25 19:46:57 +0000

    Did a lot of work on the helium ballon model. It seems to work better now. Changed the requested frame rate of the simulation to 25 fps.

--- ideal-gas-v3/src/edu/colorado/phet/idealgas/model/Balloon.java
+++ ideal-gas-v3/src/edu/colorado/phet/idealgas/model/Balloon.java
@@ -8,9 +8,8 @@
 package edu.colorado.phet.idealgas.model;
 
 import edu.colorado.phet.collision.CollidableBody;
+import edu.colorado.phet.common.math.MathUtil;
 import edu.colorado.phet.common.math.Vector2D;
-import edu.colorado.phet.common.model.clock.AbstractClock;
-import edu.colorado.phet.idealgas.util.ScalarDataRecorder;
 import edu.colorado.phet.mechanics.Body;
 
 import java.awt.geom.Point2D;
@@ -19,26 +18,18 @@ public class Balloon extends HollowSphere {
 
     public static final double MIN_RADIUS = 10;
 
-    private ScalarDataRecorder insidePressureRecorder;
-    private ScalarDataRecorder outsidePressureRecorder;
-
     // Attributes for adjusting the size of the balloon
     private int timeStepsSinceLastRadiusAdjustment = 0;
     private int timeStepsBetweenRadiusAdjustments = 5;
-//    private int timeStepsBetweenRadiusAdjustments = 20;
-    private double aveInOutPressureRatio = 0;
     // Exponent for the power function that adjusts the size of the balloon when
     // the internal or external pressure changes
-//    private double dampingExponent = 015;
     private double dampingExponent = 0.02;
     // Temporary varaibles, pre-allocated for performance
     private Vector2D momentumPre = new Vector2D.Double();
     private Vector2D momentumPost = new Vector2D.Double();
     private Box2D box;
-    private double aDouble;
+    private double accumulatedImpact = 0;
 
-
-    public static Balloon instance;
 
     /**
      * @param center
@@ -52,46 +43,15 @@ public class Balloon extends HollowSphere {
                    Vector2D acceleration,
                    double mass,
                    double radius,
-                   Box2D box,
-                   AbstractClock clock) {
+                   Box2D box ) {
         super(center, velocity, acceleration, mass, radius);
         this.box = box;
-        insidePressureRecorder = new ScalarDataRecorder(clock);
-        outsidePressureRecorder = new ScalarDataRecorder(clock);
-
-        instance = this;
     }
-
-//    public void reInitialize() {
-//        super.reInitialize();
-//        insidePressureRecorder.clear();
-//        outsidePressureRecorder.clear();
-//    }
 
     /**
      * Records the impact on the inside or outside of the balloon
      */
-//    SphericalBody sphericalBody = new SphericalBody( new Point2D.Double(),
-//                                                     new Vector2D.Double(),
-//                                                     new Vector2D.Double(),
-//                                                     0,
-//                                                     GasMolecule.s_defaultRadius );
-
     public void collideWithParticle(CollidableBody particle) {
-
-        // Get the momentum of the balloon before the collision
-//        momentumPre.setX( getVelocity().getX() );
-//        momentumPre.setY( getVelocity().getY() );
-//        momentumPre = momentumPre.scale( this.getMass() );
-
-        // This bizarre copying from one object to another is a total hack that
-        // was made neccessary by the creation of the CollisionGod class, and the
-        // fact that some of the system uses Particles from the common code, and
-        // some of it uses Particles from the ideal gas code. It is an embarassing
-        // mess that ought to be straightened out.
-//        sphericalBody.setPosition( particle.getPosition() );
-//        sphericalBody.setVelocity( particle.getVelocity() );
-//        super.collideWithParticle( sphericalBody );
 
         // Get the new momentum of the balloon
         momentumPost.setX(this.getVelocity().getX());
@@ -102,11 +62,30 @@ public class Balloon extends HollowSphere {
         Vector2D momentumChange = momentumPost.subtract(momentumPre);
         double impact = momentumChange.getMagnitude();
         // todo: change this to a test that relies on containsBody, when that is correctly implemented
-        ScalarDataRecorder recorder = this.contains(particle)
-                ? insidePressureRecorder
-                : outsidePressureRecorder;
-        recorder.addDataRecordEntry(impact);
+        int sign = this.contains(particle) ? 1 : -1;
+        accumulatedImpact += impact * sign;
         momentumPre.setComponents(momentumPost.getX(), momentumPost.getY());
+
+        // Adjust the size of the balloon
+        if (timeStepsSinceLastRadiusAdjustment >= timeStepsBetweenRadiusAdjustments) {
+            adjustRadius();
+            // Reset accumulators
+            accumulatedImpact = 0;
+            timeStepsSinceLastRadiusAdjustment = 0;
+        }
+    }
+
+    private void adjustRadius() {
+        // Adjust the radius of the balloon
+        //Make sure the balloon doesn't expand beyond the box
+        double maxRadius = Math.min((box.getMaxX() - box.getMinX()) / 2,
+                (box.getMaxY() - box.getMinY()) / 2);
+        double dr = Math.pow(Math.abs(accumulatedImpact), dampingExponent) * MathUtil.getSign(accumulatedImpact);
+        double newRadius = Math.min(this.getRadius() + dr, maxRadius);
+        if (!Double.isNaN(newRadius)) {
+            newRadius = Math.min(maxRadius, Math.max(newRadius, MIN_RADIUS));
+            this.setRadius(newRadius);
+        }
     }
 
     private boolean contains(Body body) {
@@ -118,38 +97,8 @@ public class Balloon extends HollowSphere {
      * @param dt
      */
     public void stepInTime(double dt) {
-
         super.stepInTime(dt);
-
-        // Compute average pressure differential
-        insidePressureRecorder.computeDataStatistics();
-        outsidePressureRecorder.computeDataStatistics();
-        double outsidePressure = Math.max(outsidePressureRecorder.getDataTotal(), 1);
-        double currInOutPressureRatio = insidePressureRecorder.getDataTotal() / outsidePressure;
-//        double currInOutPressureRatio = insidePressureRecorder.getDataTotal() / outsidePressureRecorder.getDataTotal();
-        if (!Double.isNaN(currInOutPressureRatio)
-                && currInOutPressureRatio != 0
-                && currInOutPressureRatio != Double.POSITIVE_INFINITY
-                && currInOutPressureRatio != Double.NEGATIVE_INFINITY) {
-            aveInOutPressureRatio = ((aveInOutPressureRatio * timeStepsSinceLastRadiusAdjustment)
-                    + (insidePressureRecorder.getDataTotal() / outsidePressureRecorder.getDataTotal()))
-                    / (++timeStepsSinceLastRadiusAdjustment);
-        }
-
-        // Adjust the radius of the balloon
-        //Make sure the balloon doesn't expand beyond the box
-        double maxRadius = Math.min((box.getMaxX() - box.getMinX()) / 2,
-                (box.getMaxY() - box.getMinY()) / 2);
         timeStepsSinceLastRadiusAdjustment++;
-        if (timeStepsSinceLastRadiusAdjustment >= timeStepsBetweenRadiusAdjustments) {
-            timeStepsSinceLastRadiusAdjustment = 0;
-
-            double newRadius = Math.min(this.getRadius() * Math.pow(aveInOutPressureRatio, dampingExponent), maxRadius);
-            if (!Double.isNaN(newRadius)) {
-                aDouble = newRadius = Math.min(maxRadius, Math.max(newRadius, MIN_RADIUS));
-                this.setRadius(newRadius);
-            }
-        }
     }
 }
 
