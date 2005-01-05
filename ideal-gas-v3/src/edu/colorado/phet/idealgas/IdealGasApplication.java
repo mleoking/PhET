97b5a392226f035cfa807e761879487dbe63d1d3
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-01-05 22:17:23 +0000

    no message

--- ideal-gas-v3/src/edu/colorado/phet/idealgas/IdealGasApplication.java
+++ ideal-gas-v3/src/edu/colorado/phet/idealgas/IdealGasApplication.java
@@ -10,6 +10,7 @@ import edu.colorado.phet.common.application.ApplicationModel;
 import edu.colorado.phet.common.application.Module;
 import edu.colorado.phet.common.application.PhetApplication;
 import edu.colorado.phet.common.model.clock.SwingTimerClock;
+import edu.colorado.phet.common.util.FrameRateReporter;
 import edu.colorado.phet.common.view.util.SimStrings;
 import edu.colorado.phet.idealgas.controller.HeliumBalloonModule;
 import edu.colorado.phet.idealgas.controller.HotAirBalloonModule;
@@ -24,67 +25,68 @@ public class IdealGasApplication extends PhetApplication {
 
     static class IdealGasApplicationModel extends ApplicationModel {
         public IdealGasApplicationModel() {
-            super(SimStrings.get("IdealGasApplication.title"),
-                    SimStrings.get("IdealGasApplication.description"),
-                    IdealGasConfig.VERSION,
-                    IdealGasConfig.FRAME_SETUP);
+            super( SimStrings.get( "IdealGasApplication.title" ),
+                   SimStrings.get( "IdealGasApplication.description" ),
+                   IdealGasConfig.VERSION,
+                   IdealGasConfig.FRAME_SETUP );
 
             // Create the clock
-            SwingTimerClock clock = new SwingTimerClock(IdealGasConfig.s_timeStep, IdealGasConfig.s_waitTime, true);
-            setClock(clock);
+            SwingTimerClock clock = new SwingTimerClock( IdealGasConfig.s_timeStep, IdealGasConfig.s_waitTime, true );
+            setClock( clock );
 
             // Create the modules
-            Module idealGasModule = new IdealGasModule(getClock());
-            Module rigidSphereModule = new RigidHollowSphereModule(getClock());
-            Module heliumBalloonModule = new HeliumBalloonModule(getClock());
-            Module hotAirBalloonModule = new HotAirBalloonModule(getClock());
+            Module idealGasModule = new IdealGasModule( getClock() );
+            Module rigidSphereModule = new RigidHollowSphereModule( getClock() );
+            Module heliumBalloonModule = new HeliumBalloonModule( getClock() );
+            Module hotAirBalloonModule = new HotAirBalloonModule( getClock() );
             Module[] modules = new Module[]{
                 idealGasModule,
                 rigidSphereModule,
                 heliumBalloonModule,
                 hotAirBalloonModule
             };
-            setModules(modules);
+            setModules( modules );
             //                        setInitialModule( rigidSphereModule );
-            setInitialModule(idealGasModule);
+            setInitialModule( idealGasModule );
 
-//            new FrameRateReporter( clock );
+            new FrameRateReporter( clock );
         }
     }
 
     public IdealGasApplication() {
-        super(new IdealGasApplicationModel());
+        super( new IdealGasApplicationModel() );
 //        this.getApplicationView().getPhetFrame().setExtendedState(JFrame.MAXIMIZED_BOTH);
         this.startApplication();
     }
 
-    public static void main(String[] args) {
+    public static void main( String[] args ) {
 
         try {
-            UIManager.setLookAndFeel(new IdealGasLandF());
-        } catch (UnsupportedLookAndFeelException e) {
+            UIManager.setLookAndFeel( new IdealGasLandF() );
+        }
+        catch( UnsupportedLookAndFeelException e ) {
             e.printStackTrace();
         }
 
-        String test1 = System.getProperty("java.vm.version");
-        System.out.println("test1 = " + test1);
-        String s = System.getProperty("javaws.locale");
-        System.out.println("s = " + s);
-        String applicationLocale = System.getProperty("javaws.locale");
-        System.out.println("applicationLocale = " + applicationLocale);
+        String test1 = System.getProperty( "java.vm.version" );
+        System.out.println( "test1 = " + test1 );
+        String s = System.getProperty( "javaws.locale" );
+        System.out.println( "s = " + s );
+        String applicationLocale = System.getProperty( "javaws.locale" );
+        System.out.println( "applicationLocale = " + applicationLocale );
 
-        if (applicationLocale != null && !applicationLocale.equals("")) {
-            Locale.setDefault(new Locale(applicationLocale));
+        if( applicationLocale != null && !applicationLocale.equals( "" ) ) {
+            Locale.setDefault( new Locale( applicationLocale ) );
         }
         String argsKey = "user.language=";
-        System.out.println("args.length = " + args.length);
-        if (args.length > 0 && args[0].startsWith(argsKey)) {
-            String locale = args[0].substring(argsKey.length(), args[0].length());
-            Locale.setDefault(new Locale(locale));
-            System.out.println("locale = " + locale);
+        System.out.println( "args.length = " + args.length );
+        if( args.length > 0 && args[0].startsWith( argsKey ) ) {
+            String locale = args[0].substring( argsKey.length(), args[0].length() );
+            Locale.setDefault( new Locale( locale ) );
+            System.out.println( "locale = " + locale );
         }
 
-        SimStrings.setStrings(IdealGasConfig.localizedStringsPath);
+        SimStrings.setStrings( IdealGasConfig.localizedStringsPath );
         new IdealGasApplication();
     }
 }
