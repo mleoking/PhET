0cf2e389fbcf1a347f8a6677b1fa6dd49fd9c4f3
nobody <nobody@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-01-12 16:17:17 +0000

    This commit was manufactured by cvs2svn to create tag 'phetcommon-pre-NWO-1-18-05'.

---
+++ tags/phetcommon-pre-NWO-1-18-05/phetcommon/src/edu/colorado/phet/common/view/graphics/InteractiveGraphic.java
@@ -0,0 +1,22 @@
+/* Copyright 2003-2004, University of Colorado */
+
+/*
+ * CVS Info -
+ * Filename : $Source$
+ * Branch : $Name$
+ * Modified by : $Author$
+ * Revision : $Revision$
+ * Date modified : $Date$
+ */
+package edu.colorado.phet.common.view.graphics;
+
+import javax.swing.event.MouseInputListener;
+
+/**
+ * The coupling of Graphic and Controller.
+ * 
+ * @author ?
+ * @version $Revision$
+ */
+public interface InteractiveGraphic extends BoundedGraphic, MouseInputListener {
+}
