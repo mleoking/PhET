f2a7c023d6849c610b66c9e27c54851d140b6f95
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-01-20 00:22:00 +0000

    no message

--- ideal-gas-v3/src/edu/colorado/phet/idealgas/controller/IdealGasControlPanel.java
+++ ideal-gas-v3/src/edu/colorado/phet/idealgas/controller/IdealGasControlPanel.java
@@ -6,16 +6,12 @@
  */
 package edu.colorado.phet.idealgas.controller;
 
-import edu.colorado.phet.common.util.SimpleObserver;
-import edu.colorado.phet.common.view.util.ImageLoader;
 import edu.colorado.phet.common.view.util.SimStrings;
 import edu.colorado.phet.coreadditions.ToggleButton;
-import edu.colorado.phet.idealgas.IdealGasConfig;
 import edu.colorado.phet.idealgas.model.IdealGasModel;
 
 import javax.swing.*;
 import javax.swing.border.Border;
-import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
@@ -24,7 +20,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.io.IOException;
 import java.text.NumberFormat;
 import java.util.Hashtable;
 
@@ -42,8 +37,11 @@ public class IdealGasControlPanel extends JPanel {
     private IdealGasModule module;
     private IdealGasModel idealGasModel;
     private GridBagConstraints gbc;
-
-    private JPanel buttonPanel = new JPanel();
+    // Separate panels for miscellaneous controls, particle controls, and buttons
+    private JPanel miscPanel;
+    private JPanel particleControlsPanel;
+    private JPanel buttonPanel;
+    private GridBagConstraints particleControlsGbc;
 
     public IdealGasControlPanel( IdealGasModule module ) {
         super();
@@ -53,52 +51,88 @@ public class IdealGasControlPanel extends JPanel {
     }
 
     private void init() {
+
+        makeMiscControls();
+        makeButtonPanel();
+        makeParticlesControls();
+
         this.setLayout( new GridBagLayout() );
         gbc = new GridBagConstraints( 0, 0, 1, 1, 1, 1,
                                       GridBagConstraints.CENTER,
                                       GridBagConstraints.HORIZONTAL,
-                                      new Insets( 4, 4, 4, 4 ), 0, 0 );
+                                      new Insets( 0,0,0,0 ), 0, 0 );
+//                                      new Insets( 4, 4, 4, 4 ), 0, 0 );
+
+        // Add control for selecting the constant parameter
+        gbc.gridy = 0;
         add( constantParamControls(), gbc );
+
+        // Add controls for the number and type of molecules to put in the box
         gbc.gridy++;
+        add( particleControlsPanel, gbc );
 
-        add( gravityControls(), gbc );
+        // Add miscellaneous controls (gravity, particle interactions, etc.
+        gbc.gridy++;
+        this.add( miscPanel, gbc );
 
-        JPanel speciesButtonPanel = new SpeciesSelectionPanel( module.getPump() );
+        // Add the reset and measurement panel buttons
+        gbc.gridy++;
+        this.add( buttonPanel, gbc );
+
+        Border border = BorderFactory.createEtchedBorder();
+        this.setBorder( border );
+    }
+
+    /**
+     * Creates the panel that holds controls for the particles in the box
+     */
+    private void makeParticlesControls() {
+        particleControlsPanel = new JPanel( new GridBagLayout() );
+
+        particleControlsGbc = new GridBagConstraints( 0, 0, 1, 1, 1, 1,
+                                                                 GridBagConstraints.CENTER,
+                                                                 GridBagConstraints.HORIZONTAL,
+                                                                 new Insets( 0, 0, 0, 0 ), 0, 0 );
+
+        // Add controls for the number and type of molecules to put in the box
+        JPanel speciesButtonPanel = new PumpControlPanel( module, module.getPump() );
         speciesButtonPanel.setBorder( new TitledBorder( SimStrings.get( "IdealGasControlPanel.Gas_In_Pump" ) ) );
-        gbc.gridy++;
-        gbc.fill = GridBagConstraints.NONE;
-        this.add( speciesButtonPanel, gbc );
-        gbc.gridy++;
-        this.add( new NumParticlesControls(), gbc );
+        particleControlsGbc.gridy++;
+        particleControlsGbc.fill = GridBagConstraints.NONE;
+        particleControlsPanel.add( speciesButtonPanel, particleControlsGbc );
+        particleControlsGbc.gridy++;
+//        particleControlsPanel.add( new NumParticlesControls2(), particleControlsGbc );
+//        particleControlsPanel.add( new NumParticlesControls(), particleControlsGbc );
+    }
+
+    /**
+     * Creates the panel that contains controls for things that don't appear as objects on the screen
+     */
+    private void makeMiscControls() {
+        miscPanel = new JPanel( new GridBagLayout() );
+        GridBagConstraints gbc = new GridBagConstraints( 0, 0, 1, 1, 1, 1,
+                                                         GridBagConstraints.CENTER,
+                                                         GridBagConstraints.HORIZONTAL,
+                                                         new Insets( 0, 0, 0, 0 ), 0, 0 );
+        JPanel gravityControls = gravityControls();
+        miscPanel.add( gravityControls, gbc );
 
         gbc.gridy++;
         ParticleInteractionControl pic = new ParticleInteractionControl();
         gbc.fill = GridBagConstraints.NONE;
-        this.add( pic, gbc );
+        miscPanel.add( pic, gbc );
 
-        gbc.gridy++;
-        this.add( makeButtonPanel(), gbc );
+        Border gravityBorder = new TitledBorder( SimStrings.get( "IdealGasControlPanel.MiscPanelTitle" ) );
+        miscPanel.setBorder( gravityBorder );
 
-        Border border = BorderFactory.createEtchedBorder();
-        this.setBorder( border );
-
-        // Add button for submitting a screen shot
-        //        JButton screenShotBtn = new JButton( "Screen Shot" );
-        //        this.add( screenShotBtn );
-        //        screenShotBtn.addActionListener( new ActionListener() {
-        //            public void actionPerformed( ActionEvent e ) {
-        //                makeScreenShot();
-        //            }
-        //        } );
     }
 
     /**
      * Make buttons for Reset and Measurement Tools
      */
-    private JPanel makeButtonPanel() {
+    private void makeButtonPanel() {
 
         // Reset button
-        JPanel panel = new JPanel( new GridBagLayout() );
         JButton resetBtn = new JButton( SimStrings.get( "IdealGasControlPanel.Reset" ) );
         resetBtn.setBackground( new Color( 180, 255, 180 ) );
         resetBtn.addActionListener( new ActionListener() {
@@ -112,26 +146,25 @@ public class IdealGasControlPanel extends JPanel {
         measurementDlgBtn.setAlignmentX( JButton.CENTER_ALIGNMENT );
         measurementDlgBtn.setBackground( new Color( 255, 255, 120 ) );
 
+        // Put them on the button panel
+        buttonPanel = new JPanel( new GridBagLayout() );
         GridBagConstraints gbc = new GridBagConstraints( 0, 0, 1, 1, 1, 1,
                                                          GridBagConstraints.CENTER, GridBagConstraints.NONE,
                                                          new Insets( 2, 2, 2, 2 ), 0, 0 );
-        panel.add( resetBtn, gbc );
-        gbc.gridy++;
-        panel.add( measurementDlgBtn, gbc );
 
-        return panel;
+        buttonPanel.add( resetBtn, gbc );
+        gbc.gridy++;
+        buttonPanel.add( measurementDlgBtn, gbc );
+        buttonPanel.revalidate();
     }
 
     /**
-     *
+     * Returns a panel with selections for whether volume or pressure is to be held constant
      */
     private JPanel constantParamControls() {
         JPanel constantParamButtonPanel = new JPanel( new FlowLayout( FlowLayout.LEFT ) );
-        constantParamButtonPanel.setPreferredSize( new Dimension( IdealGasConfig.CONTROL_PANEL_WIDTH, 80 ) );
         final JRadioButton constantVolumeRB = new JRadioButton( SimStrings.get( "Common.Volume" ) );
-        constantVolumeRB.setPreferredSize( new Dimension( 80, 15 ) );
         final JRadioButton constantPressureRB = new JRadioButton( SimStrings.get( "Common.Pressure" ) );
-        constantPressureRB.setPreferredSize( new Dimension( 80, 15 ) );
         final ButtonGroup constantParameterGroup = new ButtonGroup();
         constantParameterGroup.add( constantVolumeRB );
         constantParameterGroup.add( constantPressureRB );
@@ -208,46 +241,6 @@ public class IdealGasControlPanel extends JPanel {
     }
 
 
-    private class NumParticlesControls extends JPanel {
-        NumParticlesControls() {
-
-            super( new GridBagLayout() );
-            GridBagConstraints gbc = new GridBagConstraints( 0, 0, 1, 1, 1, 1,
-                                                             GridBagConstraints.EAST,
-                                                             GridBagConstraints.NONE,
-                                                             new Insets( 0, 0, 0, 0 ), 0, 0 );
-            JLabel label = new JLabel( SimStrings.get( "MeasurementControlPanel.Number_of_particles" ) );
-            this.add( label, gbc );
-            // Set up the spinner for controlling the number of particles in
-            // the hollow sphere
-            Integer value = new Integer( 0 );
-            Integer min = new Integer( 0 );
-            Integer max = new Integer( 1000 );
-            Integer step = new Integer( 1 );
-            SpinnerNumberModel model = new SpinnerNumberModel( value, min, max, step );
-            final JSpinner particleSpinner = new JSpinner( model );
-            particleSpinner.setPreferredSize( new Dimension( 50, 20 ) );
-            gbc.gridx = 1;
-            gbc.anchor = GridBagConstraints.WEST;
-            this.add( particleSpinner, gbc );
-
-            particleSpinner.addChangeListener( new ChangeListener() {
-                public void stateChanged( ChangeEvent e ) {
-                    setNumParticlesInBox( ( (Integer)particleSpinner.getValue() ).intValue() );
-                }
-            } );
-
-            // Hook the spinner up so it will track molecules put in the box by the pump
-            getModule().getModel().addObserver( new SimpleObserver() {
-                public void update() {
-                    int h = getModule().getIdealGasModel().getHeavySpeciesCnt();
-                    int l = getModule().getIdealGasModel().getLightSpeciesCnt();
-                    particleSpinner.setValue( new Integer( l + h ) );
-                }
-            } );
-        }
-    }
-
     private void updateGravity( boolean isEnabled, int value ) {
         gravityTF.setText( gravityFormat.format( value ) );
         if( !isEnabled ) {
@@ -264,10 +257,6 @@ public class IdealGasControlPanel extends JPanel {
 
     public void setGravity( double amt ) {
         this.gravitySlider.setValue( (int)amt );
-    }
-
-    protected IdealGasModule getModule() {
-        return module;
     }
 
     private void setNumParticlesInBox( int numParticles ) {
@@ -290,6 +279,23 @@ public class IdealGasControlPanel extends JPanel {
         this.add( component, gbc );
     }
 
+    public void addParticleControl( Component component ) {
+        particleControlsGbc.gridy++;
+        particleControlsPanel.add( component, particleControlsGbc );
+    }
+
+    //--------------------------------------------------------------------------
+    // Utility methods
+    //--------------------------------------------------------------------------
+
+    protected IdealGasModule getModule() {
+        return module;
+    }
+
+    //--------------------------------------------------------------------------
+    // Inner classes
+    //--------------------------------------------------------------------------
+
     private class MeasurementDialogButton extends ToggleButton {
 
         public MeasurementDialogButton() {
@@ -311,34 +317,134 @@ public class IdealGasControlPanel extends JPanel {
         }
     }
 
-    //    private void makeScreenShot() {
-    //        Window w = SwingUtilities.getWindowAncestor( this );
-    //        BufferedImage bi = new BufferedImage( w.getWidth(), w.getHeight(),
-    //                                              BufferedImage.TYPE_INT_RGB );
-    //        w.paint( bi.createGraphics() );
-    //        try {
-    //            // Save as PNG
-    ////            File file = new File("newimage.png");
-    ////            ImageIO.write(bi, "png", file);
-    //
-    //            // Save as JPEG
-    ////            file = new File("newimage.jpg");
-    ////            ImageIO.write(bi, "jpg", file);
-    //            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-    //            ImageIO.write( bi, "jpg", baos );
-    //            String s = new sun.misc.BASE64Encoder().encode( baos.toByteArray() );
-    ////            String s = baos.toString();
-    //            int a = baos.size();
-    //            int b = s.length();
-    //            RemotePersistence rp = new RemotePersistence( "http://localhost/phptest/remote-jpg-pst.php", "test.jpg" );
-    //            rp.store( s );
-    //        }
-    //        catch( IOException e ) {
-    //            System.out.println( "Exception sending screen shot.\n" + e );
-    //        }
-    //
-    //    }
 
+//    private class NumParticlesControls extends JPanel {
+//        NumParticlesControls() {
+//
+//            super( new GridBagLayout() );
+//            GridBagConstraints gbc = new GridBagConstraints( 0, 0, 1, 1, 1, 1,
+//                                                             GridBagConstraints.EAST,
+//                                                             GridBagConstraints.NONE,
+//                                                             new Insets( 0, 0, 0, 0 ), 0, 0 );
+//            JLabel label = new JLabel( SimStrings.get( "MeasurementControlPanel.Number_of_particles" ) );
+//            this.add( label, gbc );
+//            // Set up the spinner for controlling the number of particles in
+//            // the hollow sphere
+//            Integer value = new Integer( 0 );
+//            Integer min = new Integer( 0 );
+//            Integer max = new Integer( 1000 );
+//            Integer step = new Integer( 1 );
+//            SpinnerNumberModel model = new SpinnerNumberModel( value, min, max, step );
+//            final JSpinner particleSpinner = new JSpinner( model );
+//            particleSpinner.setPreferredSize( new Dimension( 50, 20 ) );
+//            gbc.gridx = 1;
+//            gbc.anchor = GridBagConstraints.WEST;
+//            this.add( particleSpinner, gbc );
+//
+//            particleSpinner.addChangeListener( new ChangeListener() {
+//                public void stateChanged( ChangeEvent e ) {
+//                    setNumParticlesInBox( ( (Integer)particleSpinner.getValue() ).intValue() );
+//                }
+//            } );
+//
+//            // Hook the spinner up so it will track molecules put in the box by the pump
+//            getModule().getModel().addObserver( new SimpleObserver() {
+//                public void update() {
+//                    int h = getModule().getIdealGasModel().getHeavySpeciesCnt();
+//                    int l = getModule().getIdealGasModel().getLightSpeciesCnt();
+//                    particleSpinner.setValue( new Integer( l + h ) );
+//                }
+//            } );
+//        }
+//    }
+//
+//    private class NumParticlesControls2 extends JPanel {
+//        NumParticlesControls2() {
+//
+//            super( new GridBagLayout() );
+//            // Set up the spinner for controlling the number of particles in
+//            // the hollow sphere
+//            Integer value = new Integer( 0 );
+//            Integer min = new Integer( 0 );
+//            Integer max = new Integer( 1000 );
+//            Integer step = new Integer( 1 );
+//
+//            // Spinner for heavy species
+//            SpinnerNumberModel heavySpinnerModel = new SpinnerNumberModel( value, min, max, step );
+//            final JSpinner heavySpinner = new JSpinner( heavySpinnerModel );
+//            heavySpinner.setPreferredSize( new Dimension( 50, 20 ) );
+//            heavySpinner.addChangeListener( new ChangeListener() {
+//                public void stateChanged( ChangeEvent e ) {
+//                    int dn = ((Integer)heavySpinner.getValue()).intValue()  - ( getModule().getIdealGasModel().getHeavySpeciesCnt() );
+//                    if( dn > 0 ) {
+//                        for( int i = 0; i < dn; i++ ) {
+//                            Pump pump = getModule().getPump();
+//                            pump.setCurrentGasSpecies( HeavySpecies.class );
+//                            getModule().pumpGasMolecules( 1 );
+//                        }
+//                    }
+//                    else if( dn < 0 ) {
+//                        for( int i = 0; i < -dn; i++ ) {
+//                            getModule().removeGasMolecule( HeavySpecies.class );
+//                        }
+//                    }
+//                }
+//            } );
+//
+//            // Hook the spinner up so it will track molecules put in the box by the pump
+//            getModule().getModel().addObserver( new SimpleObserver() {
+//                public void update() {
+//                    int h = getModule().getIdealGasModel().getHeavySpeciesCnt();
+//                    heavySpinner.setValue( new Integer( h ) );
+//                }
+//            } );
+//
+//
+//            // Spinner for light species
+//            SpinnerNumberModel lightSpinnerModel = new SpinnerNumberModel( value, min, max, step );
+//            final JSpinner lightSpinner = new JSpinner( lightSpinnerModel );
+//            lightSpinner.setPreferredSize( new Dimension( 50, 20 ) );
+//            lightSpinner.addChangeListener( new ChangeListener() {
+//                public void stateChanged( ChangeEvent e ) {
+//                    int dn = ((Integer)lightSpinner.getValue()).intValue()  - ( getModule().getIdealGasModel().getLightSpeciesCnt());
+//                    if( dn > 0 ) {
+//                        for( int i = 0; i < dn; i++ ) {
+//                            Pump pump = getModule().getPump();
+//                            pump.setCurrentGasSpecies( LightSpecies.class );
+//                            getModule().pumpGasMolecules( 1 );
+//                        }
+//                    }
+//                    else if( dn < 0 ) {
+//                        for( int i = 0; i < -dn; i++ ) {
+//                            getModule().removeGasMolecule( LightSpecies.class );
+//                        }
+//                    }
+//                }
+//            } );
+//
+//            // Hook the spinner up so it will track molecules put in the box by the pump
+//            getModule().getModel().addObserver( new SimpleObserver() {
+//                public void update() {
+//                    int h = getModule().getIdealGasModel().getLightSpeciesCnt();
+//                    lightSpinner.setValue( new Integer( h ) );
+//                }
+//            } );
+//
+//
+//            GridBagConstraints gbc = new GridBagConstraints( 0, 0, 1, 1, 1, 1,
+//                                                             GridBagConstraints.EAST,
+//                                                             GridBagConstraints.NONE,
+//                                                             new Insets( 0, 0, 0, 0 ), 0, 0 );
+//            JLabel label = new JLabel( SimStrings.get( "MeasurementControlPanel.Number_of_particles" ) );
+//            this.add( label, gbc );
+//            gbc.gridx = 1;
+//            gbc.anchor = GridBagConstraints.WEST;
+//            this.add( heavySpinner, gbc );
+//
+//            gbc.gridy++;
+//            this.add( lightSpinner, gbc );
+//        }
+//    }
 }
 
 
--- ideal-gas-v3/src/edu/colorado/phet/idealgas/controller/SpeciesSelectionPanel.java
+++ ideal-gas-v3/src/edu/colorado/phet/idealgas/controller/SpeciesSelectionPanel.java
@@ -6,37 +6,65 @@
  */
 package edu.colorado.phet.idealgas.controller;
 
+import edu.colorado.phet.common.util.SimpleObserver;
 import edu.colorado.phet.common.view.util.SimStrings;
 import edu.colorado.phet.idealgas.model.HeavySpecies;
 import edu.colorado.phet.idealgas.model.LightSpecies;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 
-public class SpeciesSelectionPanel extends JPanel {
+public abstract class SpeciesSelectionPanel extends JPanel {
+    private IdealGasModule module;
+    private GasSource gasSource;
+    private JSpinner heavySpinner;
+    private JSpinner lightSpinner;
+    private JRadioButton heavySpeciesRB;
+    private JRadioButton lightSpeciesRB;
 
 
-    public SpeciesSelectionPanel( final GasSource gasSource ) {
-//        this.gasSource = gasSource;
+    public SpeciesSelectionPanel( final IdealGasModule module, final GasSource gasSource ) {
+        this.module = module;
+        this.gasSource = gasSource;
+
+        // Radio buttons
+        makeRadioButtons();
+
+        // Spinners for the species
+        makeSpinners();
+
+        // Lay out the panel
         setLayout( new GridBagLayout() );
-        final JRadioButton heavySpeciesRB = new JRadioButton( SimStrings.get( "Common.Heavy_Species" ) );
-        heavySpeciesRB.setForeground( Color.blue );
-        final JRadioButton lightSpeciesRB = new JRadioButton( SimStrings.get( "Common.Light_Species" ) );
-        lightSpeciesRB.setForeground( Color.red );
-        final ButtonGroup speciesGroup = new ButtonGroup();
-        speciesGroup.add( heavySpeciesRB );
-        speciesGroup.add( lightSpeciesRB );
-
         Insets insets = new Insets( 0, 0, 0, 0 );
         GridBagConstraints gbc = new GridBagConstraints( 0, 0, 1, 1, 1, 1,
                                                          GridBagConstraints.WEST, GridBagConstraints.NONE,
                                                          insets, 0, 0 );
         add( heavySpeciesRB, gbc );
+        gbc.gridx = 1;
+        add( heavySpinner, gbc );
+        gbc.gridx = 0;
         gbc.gridy = 1;
         add( lightSpeciesRB, gbc );
+        gbc.gridx = 1;
+        add( lightSpinner, gbc );
+    }
+
+    /**
+     * Sets up the radio buttons for selecting a species
+     */
+    private void makeRadioButtons() {
+        heavySpeciesRB = new JRadioButton( SimStrings.get( "Common.Heavy_Species" ) );
+        heavySpeciesRB.setForeground( Color.blue );
+        lightSpeciesRB = new JRadioButton( SimStrings.get( "Common.Light_Species" ) );
+        lightSpeciesRB.setForeground( Color.red );
+        final ButtonGroup speciesGroup = new ButtonGroup();
+        speciesGroup.add( heavySpeciesRB );
+        speciesGroup.add( lightSpeciesRB );
 
         heavySpeciesRB.setSelected( true );
         heavySpeciesRB.addActionListener( new ActionListener() {
@@ -57,4 +85,86 @@ public class SpeciesSelectionPanel extends JPanel {
             }
         } );
     }
+
+    /**
+     * Sets up the spinners
+     */
+    private void makeSpinners() {
+        // Set up the spinner for controlling the number of particles in
+        // the hollow sphere
+        Integer value = new Integer( 0 );
+        Integer min = new Integer( 0 );
+        Integer max = new Integer( 1000 );
+        Integer step = new Integer( 1 );
+
+        // Spinner for heavy species
+        SpinnerNumberModel heavySpinnerModel = new SpinnerNumberModel( value, min, max, step );
+        heavySpinner = new JSpinner( heavySpinnerModel );
+        heavySpinner.setPreferredSize( new Dimension( 50, 20 ) );
+        heavySpinner.addChangeListener( new ChangeListener() {
+            public void stateChanged( ChangeEvent e ) {
+                int dn = ((Integer)heavySpinner.getValue()).intValue()  - ( module.getIdealGasModel().getHeavySpeciesCnt() );
+                if( dn > 0 ) {
+                    for( int i = 0; i < dn; i++ ) {
+                        createMolecule( HeavySpecies.class );
+                    }
+                }
+                else if( dn < 0 ) {
+                    for( int i = 0; i < -dn; i++ ) {
+                        removeMolecule( HeavySpecies.class );
+                    }
+                }
+            }
+        } );
+
+        // Hook the spinner up so it will track molecules put in the box by the pump
+        module.getModel().addObserver( new SimpleObserver() {
+            public void update() {
+                int h = module.getIdealGasModel().getHeavySpeciesCnt();
+                heavySpinner.setValue( new Integer( h ) );
+            }
+        } );
+
+
+        // Spinner for light species
+        SpinnerNumberModel lightSpinnerModel = new SpinnerNumberModel( value, min, max, step );
+        lightSpinner = new JSpinner( lightSpinnerModel );
+        lightSpinner.setPreferredSize( new Dimension( 50, 20 ) );
+        lightSpinner.addChangeListener( new ChangeListener() {
+            public void stateChanged( ChangeEvent e ) {
+                int dn = ((Integer)lightSpinner.getValue()).intValue()  - ( module.getIdealGasModel().getLightSpeciesCnt());
+                if( dn > 0 ) {
+                    for( int i = 0; i < dn; i++ ) {
+                        createMolecule( LightSpecies.class );
+                    }
+                }
+                else if( dn < 0 ) {
+                    for( int i = 0; i < -dn; i++ ) {
+                        removeMolecule( LightSpecies.class );
+                    }
+                }
+            }
+        } );
+
+        // Hook the spinner up so it will track molecules put in the box by the pump
+        module.getModel().addObserver( new SimpleObserver() {
+            public void update() {
+                int h = module.getIdealGasModel().getLightSpeciesCnt();
+                lightSpinner.setValue( new Integer( h ) );
+            }
+        } );
+    }
+
+    //----------------------------------------------------------------------------------
+    // Setters and getters
+    //----------------------------------------------------------------------------------
+    protected IdealGasModule getModule() {
+        return module;
+    }
+
+    //----------------------------------------------------------------------------------
+    // Abstract methods
+    //----------------------------------------------------------------------------------
+    protected abstract void createMolecule( Class moleculeClass );
+    protected abstract void removeMolecule( Class moleculeClass );
 }
