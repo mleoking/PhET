746f0c3ffbc7835451e2e9f54a80c368ad618379
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-01-25 23:38:16 +0000

    bug fix: use absolute value of voltage to set light intensity

--- faraday/src/edu/colorado/phet/faraday/model/Attic/LightBulb.java
+++ faraday/src/edu/colorado/phet/faraday/model/Attic/LightBulb.java
@@ -70,7 +70,8 @@ public class LightBulb extends SimpleObservable implements SimpleObserver {
      * @return the intensity (0.0 - 1.0)
      */
     public double getIntensity() {
-        double intensity = _coilModel.getVoltage() / FaradayConfig.MAX_EMF;
+        double voltage = Math.abs( _coilModel.getVoltage() );
+        double intensity = voltage / FaradayConfig.MAX_EMF;
         intensity = MathUtil.clamp( 0, intensity, 1 );
         if ( intensity == Double.NaN ) {
             System.out.println( "WARNING: LightBulb.stepInTime: intensity=NaN" );
