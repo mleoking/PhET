948e014b8b1503de83338d96943f1da5399cd05d
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-01-25 23:39:46 +0000

    remove "spacing" property, not something that we're dealing with in the model

--- trunk/faraday/src/edu/colorado/phet/faraday/model/AbstractCoil.java
+++ trunk/faraday/src/edu/colorado/phet/faraday/model/AbstractCoil.java
@@ -26,12 +26,10 @@ public class AbstractCoil extends SpacialObservable {
     // Instance data
     //----------------------------------------------------------------------------
     
-    // Radius of the coil
-    private double _radius;
-    // Spacing between the loops
-    private double _spacing;
     // Number of loops in the coil.
     private int _numberOfLoops;
+    // Radius of all loops in the coil.
+    private double _radius;
 
     //----------------------------------------------------------------------------
     // Constructors
@@ -39,7 +37,7 @@ public class AbstractCoil extends SpacialObservable {
     
     /**
      * Zero-argument constructor.
-     * Creates a default coil with one loop, radius=10.0, location=(0,0), direction=0.0, spacing=1.0
+     * Creates a default coil with one loop, radius=10.0, location=(0,0), direction=0.0
      */
     public AbstractCoil() {
         this( 1, 10.0 );
@@ -47,30 +45,28 @@ public class AbstractCoil extends SpacialObservable {
     
     /**
      * Partially-specified constructor.
-     * Creates the specified coil with location=(0,0), direction=0.0, spacing=1.0
+     * Creates the specified coil with location=(0,0), direction=0.0
      * 
-     * @param numberOfLoops number of wire loops in the coil
-     * @param radius radius used for all wire loops
+     * @param numberOfLoops number of loops in the coil
+     * @param radius radius used for all loops
      */
     public AbstractCoil( int numberOfLoops, double radius ) {
-        this( numberOfLoops, radius, new Point2D.Double(0,0), 0.0, 1.0 );
+        this( numberOfLoops, radius, new Point2D.Double(0,0), 0.0 );
     }
     
     /**
      * Fully-specified constructor.
      * 
-     * @param numberOfLoops number of wire loops in the coil
-     * @param radius radius used for all wire loops
+     * @param numberOfLoops number of loops in the coil
+     * @param radius radius used for all loops
      * @param location location of the coil
      * @param direction direction in degrees (see setDirection)
-     * @param spacing spacing between the loops
      */
-    public AbstractCoil( int numberOfLoops, double radius, Point2D location, double direction, double spacing ) {
+    public AbstractCoil( int numberOfLoops, double radius, Point2D location, double direction ) {
         super( location, direction );
         assert( location != null );
         setNumberOfLoops( numberOfLoops );
         setRadius( radius );
-        setSpacing( spacing );
     }
     
     //----------------------------------------------------------------------------
@@ -139,31 +135,5 @@ public class AbstractCoil extends SpacialObservable {
      */
     public double getArea() {
         return _numberOfLoops * ( Math.PI * _radius * _radius );
-    }
-    
-    /**
-     * Sets the spacing between loops in the coil.
-     * 
-     * @param spacing the spacing
-     * @throws IllegalArgumentException if spacing is not > 0
-     */
-    public void setSpacing( double spacing ) {
-        if ( ! (spacing > 0) ) {
-            throw new IllegalArgumentException( "spacing must be > 0: " + spacing );
-        }
-        if ( spacing != _spacing ) {
-            _spacing = spacing;
-            updateSelf();
-            notifyObservers();
-        }
-    }
-    
-    /**
-     * Gets the spacing between the loops.
-     * 
-     * @return the spacing
-     */
-    public double getSpacing() {
-        return _spacing;
     }
 }
