6b279a5c660ce5df3a2336e4b0442084f98c7377
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-01-28 20:04:56 +0000

    rewrite Compass graphic using solely Shapes

--- trunk/faraday/src/edu/colorado/phet/faraday/view/CompassGraphic.java
+++ trunk/faraday/src/edu/colorado/phet/faraday/view/CompassGraphic.java
@@ -11,29 +11,48 @@
 
 package edu.colorado.phet.faraday.view;
 
-import java.awt.Color;
-import java.awt.Component;
-import java.awt.RenderingHints;
+import java.awt.*;
 import java.awt.geom.Ellipse2D;
 
+import edu.colorado.phet.common.math.AbstractVector2D;
+import edu.colorado.phet.common.math.ImmutableVector2D;
 import edu.colorado.phet.common.util.SimpleObserver;
 import edu.colorado.phet.common.view.graphics.mousecontrols.TranslationEvent;
 import edu.colorado.phet.common.view.graphics.mousecontrols.TranslationListener;
 import edu.colorado.phet.common.view.phetgraphics.CompositePhetGraphic;
-import edu.colorado.phet.common.view.phetgraphics.PhetImageGraphic;
 import edu.colorado.phet.common.view.phetgraphics.PhetShapeGraphic;
-import edu.colorado.phet.faraday.FaradayConfig;
 import edu.colorado.phet.faraday.model.AbstractCompass;
 
 /**
  * CompassGraphic is the graphical representation of a compass.
  * It can be dragged around, and will indicate the direction of the magnetic field.
+ * Its registration point is at the center of the needle.
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  * @version $Revision$
  */
 public class CompassGraphic extends CompositePhetGraphic implements SimpleObserver {
-      
+  
+    //----------------------------------------------------------------------------
+    // Class data
+    //----------------------------------------------------------------------------
+    
+    // Sizes
+    private static final int RING_DIAMETER = 70;
+    private static final int RING_WIDTH = 10;
+    private static final int INDICATOR_DIAMETER = 6;
+    private static final int ANCHOR_DIAMETER = 6;
+    private static final Dimension NEEDLE_SIZE = new Dimension( 55, 15 );
+    
+    // Colors
+    private static final Color RING_COLOR = new Color( 153, 153, 153 );
+    private static final Color LENS_COLOR = new Color( 255, 255, 255, 15 ); // alpha!
+    private static final Color INDICATOR_COLOR = Color.BLACK;
+    private static final Color ANCHOR_COLOR = Color.BLACK;
+    
+    // misc.
+    private static final int INDICATOR_INCREMENT = 45; // degrees
+
     //----------------------------------------------------------------------------
     // Instance data
     //----------------------------------------------------------------------------
@@ -60,25 +79,44 @@ public class CompassGraphic extends CompositePhetGraphic implements SimpleObserv
         _compassModel = compassModel;
         _compassModel.addObserver( this );
         
-        // Compass body
-        PhetImageGraphic body = new PhetImageGraphic( component, FaradayConfig.COMPASS_IMAGE );
-        int rx = body.getImage().getWidth() / 2;
-        int ry = body.getImage().getHeight() / 2;
-        body.setRegistrationPoint( rx, ry );
+        setRenderingHints(new RenderingHints( RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON ) );
         
+        // Ring & lens, center at (0,0)
+        Shape ringShape = new Ellipse2D.Double( -( RING_DIAMETER/2), -( RING_DIAMETER/2), RING_DIAMETER, RING_DIAMETER );
+        PhetShapeGraphic ring = new PhetShapeGraphic( component );
+        ring.setShape( ringShape );
+        ring.setPaint( LENS_COLOR );
+        ring.setBorderColor( RING_COLOR );
+        ring.setStroke( new BasicStroke( RING_WIDTH ) );
+        addGraphic( ring );
+        
+        // Indicators at evenly-spaced increments around the ring.
+        int angle = 0;
+        PhetShapeGraphic indicatorGraphic = null;
+        Shape indicatorShape = new Ellipse2D.Double( -(INDICATOR_DIAMETER/2), -(INDICATOR_DIAMETER/2), INDICATOR_DIAMETER, INDICATOR_DIAMETER ); 
+        while ( angle < 360 ) {
+            indicatorGraphic = new PhetShapeGraphic( component );
+            indicatorGraphic.setShape( indicatorShape );
+            indicatorGraphic.setPaint( INDICATOR_COLOR );
+            AbstractVector2D v = ImmutableVector2D.Double.parseAngleAndMagnitude( RING_DIAMETER/2, Math.toRadians( angle ) );
+            int rx = (int) v.getX();
+            int ry = (int) v.getY();
+            indicatorGraphic.setRegistrationPoint( rx, ry );
+            addGraphic( indicatorGraphic );
+            angle += INDICATOR_INCREMENT;
+        }
+ 
         // Needle
         _needle = new CompassNeedleGraphic( component );
-        _needle.setSize( 55, 15 );
+        _needle.setSize( NEEDLE_SIZE.width, NEEDLE_SIZE.height );
         _needle.setDirection( _compassModel.getDirection() );
+        addGraphic( _needle );
         
         // Thing that anchors the needle to the body.
+        Shape anchorShape = new Ellipse2D.Double( -(ANCHOR_DIAMETER/2), -(ANCHOR_DIAMETER/2), ANCHOR_DIAMETER, ANCHOR_DIAMETER );
         PhetShapeGraphic anchor = new PhetShapeGraphic( component);
-        anchor.setShape( new Ellipse2D.Double( -3, -3, 6, 6 ) );
-        anchor.setPaint( Color.BLACK );
-        anchor.setRenderingHints(new RenderingHints( RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON ) );
-        
-        addGraphic( body );
-        addGraphic( _needle );
+        anchor.setShape( anchorShape );
+        anchor.setPaint( ANCHOR_COLOR );
         addGraphic( anchor );
         
         // Setup interactivity.
