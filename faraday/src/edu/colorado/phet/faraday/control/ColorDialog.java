73d834d23f0878eac18c474507a40dd86f5f3f17
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-01-28 06:29:29 +0000

    tweaks to background color chooser

--- faraday/src/edu/colorado/phet/faraday/control/ColorDialog.java
+++ faraday/src/edu/colorado/phet/faraday/control/ColorDialog.java
@@ -21,44 +21,65 @@ import javax.swing.JDialog;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
+import edu.colorado.phet.common.view.util.SwingUtils;
+
 
 /**
  * ColorDialog is a color chooser and associated listener.
+ * Implementation courtesy of Sam Reid.
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  * @version $Revision$
  */
 public class ColorDialog {
 
+    /**
+     * ColorDialog.Listener is the interface that all client must implement.
+     *
+     * @author Chris Malley (cmalley@pixelzoom.com)
+     * @version $Revision$
+     */
     public interface Listener {
-
+        /** Called when the user selects a color. */
         void colorChanged( Color color );
-
-        void cancelled( Color orig );
-
+        /** Called when the user presses the OK button. */
         void ok( Color color );
+        /** Called when the user pressed the Cancel button. */
+        void cancelled( Color orig );
     }
     
-    public static void showDialog( String title, Component component, final Color initialColor, final Listener listener ) {
+    /**
+     * Shows a color chooser dialog.
+     * 
+     * @param title
+     * @param parent
+     * @param initialColor
+     * @param listener
+     */
+    public static void showDialog( String title, Component parent, 
+            final Color initialColor, final Listener listener ) {
+        
         final JColorChooser jcc = new JColorChooser( initialColor );
+        
         jcc.getSelectionModel().addChangeListener( new ChangeListener() {
-
             public void stateChanged( ChangeEvent e ) {
                 listener.colorChanged( jcc.getColor() );
             }
         } );
-        JDialog dialog = JColorChooser.createDialog( component, title, false, jcc, new ActionListener() {
-
-            public void actionPerformed( ActionEvent e ) {
-                listener.ok( jcc.getColor() );
-            }
-        }, new ActionListener() {
-
-            public void actionPerformed( ActionEvent e ) {
-                listener.cancelled( initialColor );
-            }
-        } );
-       // SwingUtils.centerDialogInParent( dialog );//XXX
+        
+        JDialog dialog = JColorChooser.createDialog( parent, title, false, jcc, 
+            new ActionListener() {
+                public void actionPerformed( ActionEvent e ) {
+                    listener.ok( jcc.getColor() );
+                }
+            },
+            new ActionListener() {
+                public void actionPerformed( ActionEvent e ) {
+                    listener.cancelled( initialColor );
+                }
+            } );
+        
+        SwingUtils.centerDialogInParent( dialog );
         dialog.setVisible( true );
     }
 }
