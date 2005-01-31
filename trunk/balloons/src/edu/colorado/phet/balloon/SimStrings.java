d43698394dc157c054d68b299d82415322481f83
joetreat <joetreat@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-01-31 14:46:13 +0000

    Updated to more recent version of SimStrings.

--- trunk/balloons/src/edu/colorado/phet/balloon/SimStrings.java
+++ trunk/balloons/src/edu/colorado/phet/balloon/SimStrings.java
@@ -1,32 +1,77 @@
 package edu.colorado.phet.balloon;
 
+import java.util.Iterator;
 import java.util.Locale;
 import java.util.ResourceBundle;
+import java.util.Vector;
 
 public class SimStrings {
 
-    private static ResourceBundle localizedStrings = null;
+    private static Vector localizedStrings;
+    private static Vector stringsPaths;
+    private static Locale localizedLocale;
 
-    public static void setStrings( String stringsPath ) {
-
-        try {
-            localizedStrings = ResourceBundle.getBundle( stringsPath );
-        }
-        catch ( java.util.MissingResourceException e ) {
-
-            throw new RuntimeException( "localizedStrings resource bundle not found" );
+    public static void setLocale( Locale locale ) {
+        localizedLocale = locale;
+        // Reload all existing string resources with the new locale
+        Vector priorPaths = stringsPaths;
+        stringsPaths = null;
+        localizedStrings = null;
+        if( priorPaths != null ) {
+            for( Iterator i = priorPaths.iterator(); i.hasNext(); ) {
+                String path = (String) i.next();
+                setStrings( path );
+            }
         }
     }
 
+    public static void setStrings( String stringsPath ) {
+        if( localizedStrings == null ) {
+            localizedStrings = new Vector();
+            stringsPaths = new Vector();
+        }
+        if( stringsPaths.contains( stringsPath ) ) {
+            return;
+        }
+        try {
+            if( localizedLocale == null ) {
+                localizedLocale = Locale.getDefault();
+            }
+            ResourceBundle rb = ResourceBundle.getBundle( stringsPath, localizedLocale );
+            if( rb != null ) {
+                localizedStrings.add( rb );
+                stringsPaths.add( stringsPath );
+            }
+        }
+        catch( Exception x ) {
+            System.out.println( "SimStrings.setStrings: " + x );
+        }
+    }
 
     public static String get( String key ) {
-
-        if ( localizedStrings == null ) {
+        if( localizedStrings == null ) {
 
             throw new RuntimeException( "Strings not initialized" );
         }
 
-        return localizedStrings.getString( key );
-    }
+        String value = null;
 
-} // end of public class SimStrings
+        for( Iterator i = localizedStrings.iterator(); value == null && i.hasNext(); ) {
+            try {
+                ResourceBundle rb = (ResourceBundle)i.next();
+
+                value = rb.getString( key );
+            }
+            catch( Exception x ) {
+                value = null;
+            }
+        }
+        
+        if ( value == null ) {
+            System.err.println( "SimStrings: key not found, key = \"" + key + "\"" );
+            value = key;
+        }
+
+        return value;
+    }
+}
