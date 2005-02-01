632ae340533bd64f78a4c0f28222e1c827435847
nobody <nobody@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-02-01 07:51:04 +0000

    This commit was manufactured by cvs2svn to create tag 'energyconservation-v2r7-jan05'.

---
+++ tags/energyconservation-v2r7-jan05/energyconservation/src-hpa-22/edu/colorado/phet/coreadditions/plaf/PlafTest.java
@@ -0,0 +1,6 @@
+package edu.colorado.phet.coreadditions.plaf;
+
+
+public class PlafTest {
+
+}
---
+++ tags/energyconservation-v2r7-jan05/energyconservation/src/edu/colorado/phet/ec2/elements/car/StateChangeTest.java
@@ -0,0 +1,11 @@
+package edu.colorado.phet.ec2.elements.car;
+
+/**
+ * User: Sam Reid
+ * Date: Jul 27, 2003
+ * Time: 12:41:48 AM
+ * Copyright (c) Jul 27, 2003 by Sam Reid
+ */
+public interface StateChangeTest {
+    public boolean shouldChangeState();
+}
