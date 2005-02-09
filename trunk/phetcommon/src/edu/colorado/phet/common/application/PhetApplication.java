d8a6686e42df1eb0f92d8adc42ce6d1cb38361e5
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-02-09 23:34:16 +0000

    move ubiquitous locale initialization out of apps and into initLocalization method

--- trunk/phetcommon/src/edu/colorado/phet/common/application/PhetApplication.java
+++ trunk/phetcommon/src/edu/colorado/phet/common/application/PhetApplication.java
@@ -11,10 +11,12 @@
 
 package edu.colorado.phet.common.application;
 
+import java.io.IOException;
+import java.util.Locale;
+
 import edu.colorado.phet.common.model.clock.ClockTickListener;
 import edu.colorado.phet.common.view.PhetFrame;
-
-import java.io.IOException;
+import edu.colorado.phet.common.view.util.SimStrings;
 
 /**
  * The top-level class for all PhET applications.
@@ -56,6 +58,33 @@ public class PhetApplication {
         moduleManager.setActiveModule( applicationModel.getInitialModule() );
         applicationModel.start();
         phetFrame.setVisible( true );
+    }
+    
+    /**
+     * Initialize application localization.
+     * 
+     * @param args the commandline arguments that were passed to main
+     * @param bundleName the base name of the resource bundle containing localized strings
+     */
+    public static void initLocalization( String[] args, String bundleName ) {
+        // Get the default locale from property javaws.locale.
+        String applicationLocale = System.getProperty( "javaws.locale" );
+        if ( applicationLocale != null && !applicationLocale.equals( "" ) ) {
+            SimStrings.setLocale( new Locale( applicationLocale ) );
+        }
+
+        // Override default locale using "user.language=" command line argument.
+        String argsKey = "user.language=";
+        for ( int i = 0; i < args.length; i++ ) {
+            if ( args[i].startsWith( argsKey ) ) {
+                String locale = args[i].substring( argsKey.length(), args[i].length() );
+                SimStrings.setLocale( new Locale( locale ) );
+                break;
+            }
+        }
+
+        // Initialize simulation strings using resource bundle for the locale.
+        SimStrings.setStrings( bundleName );
     }
 
     public PhetFrame getPhetFrame() {
