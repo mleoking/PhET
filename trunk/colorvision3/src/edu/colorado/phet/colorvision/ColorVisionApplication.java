afdb1f5dea2da8cf07cf8c6c1bb59ee0c2f917c7
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-02-09 23:48:24 +0000

    move localization init into PhetApplication

--- trunk/colorvision3/src/edu/colorado/phet/colorvision/ColorVisionApplication.java
+++ trunk/colorvision3/src/edu/colorado/phet/colorvision/ColorVisionApplication.java
@@ -54,25 +54,7 @@ public class ColorVisionApplication extends PhetApplication {
         BoundsOutliner.setEnabled( BOUNDS_OUTLINE_ENABLED ); // DEBUG
 
         // Initialize localization.
-        {
-            // Get the default locale from property javaws.locale.
-            String applicationLocale = System.getProperty( "javaws.locale" );
-            if( applicationLocale != null && !applicationLocale.equals( "" ) ) {
-                SimStrings.setLocale( new Locale( applicationLocale ) );
-            }
-
-            // Override default locale using "user.language=" command line
-            // argument.
-            String argsKey = "user.language=";
-            if( args.length > 0 && args[0].startsWith( argsKey ) ) {
-                String locale = args[0].substring( argsKey.length(), args[0].length() );
-                SimStrings.setLocale( new Locale( locale ) );
-            }
-
-            // Initialize simulation strings using resource bundle for the
-            // locale.
-            SimStrings.setStrings( ColorVisionConfig.LOCALIZATION_BUNDLE_BASENAME );
-        }
+        PhetApplication.initLocalization( args, ColorVisionConfig.LOCALIZATION_BUNDLE_BASENAME );
 
         // Get stuff needed to initialize the application model.
         String title = SimStrings.get( "ColorVisionApplication.title" );
