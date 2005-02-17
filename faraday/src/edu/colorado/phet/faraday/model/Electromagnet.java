4189f2990c6505256d04861f6724f1a4ac5a300e
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-02-17 16:51:12 +0000

    add skelton for "Electromagnet" module

--- faraday/src/edu/colorado/phet/faraday/model/Electromagnet.java
+++ faraday/src/edu/colorado/phet/faraday/model/Electromagnet.java
@@ -14,21 +14,22 @@ package edu.colorado.phet.faraday.model;
 import java.awt.geom.Point2D;
 
 import edu.colorado.phet.common.math.AbstractVector2D;
+import edu.colorado.phet.common.math.ImmutableVector2D;
 
 
 /**
- * ElectroMagnet is the model of an electro-magnet.
+ * Electromagnet is the model of an electro-magnet.
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  * @version $Revision$
  */
-public class ElectroMagnet extends AbstractMagnet {
+public class Electromagnet extends AbstractMagnet {
 
     //----------------------------------------------------------------------------
     // Constructors
     //----------------------------------------------------------------------------
     
-    public ElectroMagnet() {
+    public Electromagnet() {
         super();
     }
     
@@ -41,7 +42,6 @@ public class ElectroMagnet extends AbstractMagnet {
      */
     public AbstractVector2D getStrength( Point2D p ) {
         assert( p != null );
-        // TODO Auto-generated method stub
-        return null;
+        return new ImmutableVector2D.Double( 0, 0 ); // XXX
     }
 }
