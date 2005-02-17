ac0c41b1b64ab79702f33cbefabdb007bfd9dc74
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-02-17 16:34:58 +0000

    lower field values by 1 pixel

--- faraday/src/edu/colorado/phet/faraday/view/FieldMeterGraphic.java
+++ faraday/src/edu/colorado/phet/faraday/view/FieldMeterGraphic.java
@@ -52,7 +52,7 @@ public class FieldMeterGraphic extends CompositePhetGraphic implements SimpleObs
     private static final Point TITLE_LOCATION = new Point( 0, 84 );
     
     // Bottom right of the top-most field (MUST MATCH IMAGE FILE!)
-    private static final Point FIELD_LOCATION = new Point( 35, 111 );
+    private static final Point FIELD_LOCATION = new Point( 35, 112 );
     
     // Horizontal spacing between fields (MUST MATCH IMAGE FILE!)
     private static final int FIELD_SPACING = 24;
