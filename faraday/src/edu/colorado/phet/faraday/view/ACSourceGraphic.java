6c35bc8a7f2fcab71e08dd126923cecfa9a0e453
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-03-01 23:42:22 +0000

    convert to a CompositePhetGraphic in preparation for putting controls (sliders) on this graphic

--- faraday/src/edu/colorado/phet/faraday/view/ACSourceGraphic.java
+++ faraday/src/edu/colorado/phet/faraday/view/ACSourceGraphic.java
@@ -14,6 +14,7 @@ package edu.colorado.phet.faraday.view;
 import java.awt.Component;
 
 import edu.colorado.phet.common.util.SimpleObserver;
+import edu.colorado.phet.common.view.phetgraphics.CompositePhetGraphic;
 import edu.colorado.phet.common.view.phetgraphics.PhetImageGraphic;
 import edu.colorado.phet.faraday.FaradayConfig;
 import edu.colorado.phet.faraday.model.ACSource;
@@ -25,32 +26,64 @@ import edu.colorado.phet.faraday.model.ACSource;
  * @author Chris Malley (cmalley@pixelzoom.com)
  * @version $Revision$
  */
-public class ACSourceGraphic extends PhetImageGraphic implements SimpleObserver {
+public class ACSourceGraphic extends CompositePhetGraphic implements SimpleObserver {
 
-    private ACSource _acSourceModel;
+    //----------------------------------------------------------------------------
+    // Instance data
+    //----------------------------------------------------------------------------
     
+    private ACSource _acSourceModel;
+    private PhetImageGraphic _acSourceGraphic;
+    
+    //----------------------------------------------------------------------------
+    // Constructors and finalizers
+    //----------------------------------------------------------------------------
+    
+    /**
+     * Sole constructor.
+     * 
+     * @param component
+     * @param acSourceModel
+     */
     public ACSourceGraphic( Component component, ACSource acSourceModel ) {
         
-        super( component, FaradayConfig.AC_SOURCE_IMAGE );
+        super( component );
+        
         assert( component != null );
         assert( acSourceModel != null );
         
         _acSourceModel = acSourceModel;
         _acSourceModel.addObserver( this );
         
-        // Registration point is the bottom center of the image.
-        setRegistrationPoint( getImage().getWidth() / 2, getImage().getHeight() );
-        
-        scale( 0.40 ); // XXX
+        // AC panel
+        {
+            _acSourceGraphic = new PhetImageGraphic( component, FaradayConfig.AC_SOURCE_IMAGE );
+            addGraphic( _acSourceGraphic );
+
+            // Registration point is the bottom center of the image.
+            int rx = _acSourceGraphic.getImage().getWidth() / 2;
+            int ry = _acSourceGraphic.getImage().getHeight();
+            _acSourceGraphic.setRegistrationPoint( rx, ry );
+
+            _acSourceGraphic.scale( 0.40 ); // XXX
+        }
         
         update();
     }
     
+    /**
+     * Finalizes an instance of this type.
+     * Call this method prior to releasing all references to an object of this type.
+     */
     public void finalize() {
         _acSourceModel.removeObserver( this );
         _acSourceModel = null;
     }
 
+    //----------------------------------------------------------------------------
+    // SimpleObserver implementation
+    //----------------------------------------------------------------------------
+    
     /*
      * @see edu.colorado.phet.common.util.SimpleObserver#update()
      */
