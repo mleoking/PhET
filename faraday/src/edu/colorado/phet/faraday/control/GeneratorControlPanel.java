8310a3e0949b318ef0bc626f961e44264324a076
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-03-09 01:57:53 +0000

    first pass at Generator module, consisting of a Turbine (bar magnet) and a Pickup Coil

--- faraday/src/edu/colorado/phet/faraday/control/GeneratorControlPanel.java
+++ faraday/src/edu/colorado/phet/faraday/control/GeneratorControlPanel.java
@@ -11,7 +11,14 @@
 
 package edu.colorado.phet.faraday.control;
 
+import edu.colorado.phet.faraday.control.panel.BarMagnetPanel;
+import edu.colorado.phet.faraday.control.panel.PickupCoilPanel;
+import edu.colorado.phet.faraday.model.*;
 import edu.colorado.phet.faraday.module.GeneratorModule;
+import edu.colorado.phet.faraday.view.CoilGraphic;
+import edu.colorado.phet.faraday.view.CompassGridGraphic;
+import edu.colorado.phet.faraday.view.FieldMeterGraphic;
+import edu.colorado.phet.faraday.view.TurbineGraphic;
 
 /**
  * GeneratorControlPanel is the control panel for the "Generator" module.
@@ -33,10 +40,28 @@ public class GeneratorControlPanel extends FaradayControlPanel {
      * 
      * @param module the module that this control panel is associated with.
      */
-    public GeneratorControlPanel( GeneratorModule module ) {
-        
-        super( module );
-        
-        //XXX add control panels here
+    public GeneratorControlPanel( GeneratorModule module,
+            /* Generator stuff */
+            Turbine turbineModel,
+            Compass compassModel,
+            TurbineGraphic turbineGraphic,
+            CompassGridGraphic gridGraphic,
+            FieldMeterGraphic fieldMeterGraphic,
+            /* Pickup Coil stuff */
+            PickupCoil pickupCoilModel,
+            CoilGraphic coilGraphic,
+            Lightbulb lightbulbModel,
+            Voltmeter voltmeterModel ) {
+
+            super( module );
+            
+            BarMagnetPanel barMagnetPanel = new BarMagnetPanel( turbineModel, compassModel, turbineGraphic.getBarMagnetGraphic(), gridGraphic, fieldMeterGraphic );
+            barMagnetPanel.setFlipPolarityVisible( false );
+            barMagnetPanel.setSeeInsideVisible( false );
+            barMagnetPanel.setFieldMeterEnabled( false );
+            addFullWidth( barMagnetPanel );
+            
+            PickupCoilPanel pickupCoilPanel = new PickupCoilPanel( pickupCoilModel, coilGraphic, lightbulbModel, voltmeterModel );
+            addFullWidth( pickupCoilPanel );
     }
 }
--- faraday/src/edu/colorado/phet/faraday/module/GeneratorModule.java
+++ faraday/src/edu/colorado/phet/faraday/module/GeneratorModule.java
@@ -12,16 +12,20 @@
 package edu.colorado.phet.faraday.module;
 
 import java.awt.Color;
+import java.awt.Point;
 
 import edu.colorado.phet.common.application.ApplicationModel;
-import edu.colorado.phet.common.application.Module;
 import edu.colorado.phet.common.model.BaseModel;
 import edu.colorado.phet.common.model.clock.AbstractClock;
 import edu.colorado.phet.common.view.ApparatusPanel2;
 import edu.colorado.phet.common.view.util.SimStrings;
 import edu.colorado.phet.faraday.FaradayConfig;
+import edu.colorado.phet.faraday.control.FaradaySlider;
 import edu.colorado.phet.faraday.control.GeneratorControlPanel;
-import edu.colorado.phet.faraday.control.TransformerControlPanel;
+import edu.colorado.phet.faraday.model.*;
+import edu.colorado.phet.faraday.util.IRescaler;
+import edu.colorado.phet.faraday.util.MagneticFieldRescaler;
+import edu.colorado.phet.faraday.view.*;
 
 
 /**
@@ -30,21 +34,34 @@ import edu.colorado.phet.faraday.control.TransformerControlPanel;
  * @author Chris Malley (cmalley@pixelzoom.com)
  * @version $Revision$
  */
-public class GeneratorModule extends Module {
+public class GeneratorModule extends FaradayModule {
 
     //----------------------------------------------------------------------------
     // Class data
     //----------------------------------------------------------------------------
 
-    // Rendering layers
+//  Rendering layers
     private static final double GRID_LAYER = 1;
+    private static final double PICKUP_COIL_BACK_LAYER = 2;
+    private static final double TURBINE_LAYER = 3;
+    private static final double COMPASS_LAYER = 4;
+    private static final double PICKUP_COIL_FRONT_LAYER = 5;
+    private static final double FIELD_METER_LAYER = 6;
     private static final double DEBUG_LAYER = FaradayConfig.DEBUG_LAYER;
     private static final double HELP_LAYER = FaradayConfig.HELP_LAYER;
 
     // Locations
+    private static final Point TURBINE_LOCATION = new Point( 200, 400 );
+    private static final Point PICKUP_COIL_LOCATION = new Point( 500, 400 );
+    private static final Point COMPASS_LOCATION = new Point( 150, 200 );
+    private static final Point FIELD_METER_LOCATION = new Point( 150, 400 );
 
     // Colors
     private static final Color APPARATUS_BACKGROUND = Color.BLACK;
+    
+    // Pickup Coil parameters
+    private static final int NUMBER_OF_LOOPS = 2;
+    private static final double LOOP_RADIUS = 0.75 * FaradayConfig.MAX_PICKUP_RADIUS;
     
     //----------------------------------------------------------------------------
     // Constructors
@@ -71,6 +88,46 @@ public class GeneratorModule extends Module {
         BaseModel model = new BaseModel();
         this.setModel( model );
         
+        // Turbine
+        Turbine turbineModel = new Turbine();
+        turbineModel.setMaxStrength( FaradayConfig.BAR_MAGNET_STRENGTH_MAX );
+        turbineModel.setMinStrength( FaradayConfig.BAR_MAGNET_STRENGTH_MIN );
+        turbineModel.setStrength( 0.75 * FaradayConfig.BAR_MAGNET_STRENGTH_MAX );
+        turbineModel.setLocation( TURBINE_LOCATION );
+        turbineModel.setDirection( 0 /* radians */ );
+        turbineModel.setSize( FaradayConfig.BAR_MAGNET_SIZE );
+        model.addModelElement( turbineModel );
+        
+        // Rescaler
+        IRescaler rescaler = new MagneticFieldRescaler( turbineModel );
+        
+        // Compass
+        Compass compassModel = new Compass( turbineModel ); 
+        compassModel.setLocation( COMPASS_LOCATION );
+        compassModel.setRotationalKinematicsEnabled( false );
+        compassModel.setEnabled( false );
+        model.addModelElement( compassModel );
+        
+        // Pickup Coil
+        PickupCoil pickupCoilModel = new PickupCoil( turbineModel );
+        pickupCoilModel.setNumberOfLoops( NUMBER_OF_LOOPS );
+        pickupCoilModel.setRadius( LOOP_RADIUS );
+        pickupCoilModel.setDirection( 0 /* radians */ );
+        pickupCoilModel.setMaxVoltage( FaradayConfig.MAX_PICKUP_EMF );
+        pickupCoilModel.setLocation( PICKUP_COIL_LOCATION);
+        model.addModelElement( pickupCoilModel );
+       
+        // Lightbulb
+        Lightbulb lightbulbModel = new Lightbulb( pickupCoilModel );
+        lightbulbModel.setEnabled( true );
+        
+        // Volt Meter
+        Voltmeter voltmeterModel = new Voltmeter( pickupCoilModel );
+        voltmeterModel.setRescaler( rescaler );
+        voltmeterModel.setRotationalKinematicsEnabled( true );
+        voltmeterModel.setEnabled( false );
+        model.addModelElement( voltmeterModel );
+        
         //----------------------------------------------------------------------------
         // View
         //----------------------------------------------------------------------------
@@ -79,21 +136,70 @@ public class GeneratorModule extends Module {
         ApparatusPanel2 apparatusPanel = new ApparatusPanel2( model, clock );
         apparatusPanel.setBackground( APPARATUS_BACKGROUND );
         this.setApparatusPanel( apparatusPanel );
+
+        // Turbine
+        TurbineGraphic generatorGraphic = new TurbineGraphic( apparatusPanel, turbineModel );
+        apparatusPanel.addChangeListener( generatorGraphic );
+        apparatusPanel.addGraphic( generatorGraphic, TURBINE_LAYER );
+        
+        // Pickup Coil
+        PickupCoilGraphic pickupCoilGraphic = 
+            new PickupCoilGraphic( apparatusPanel, model, pickupCoilModel, lightbulbModel, voltmeterModel, rescaler );
+        apparatusPanel.addChangeListener( pickupCoilGraphic );
+        apparatusPanel.addGraphic( pickupCoilGraphic.getForeground(), PICKUP_COIL_FRONT_LAYER );
+        apparatusPanel.addGraphic( pickupCoilGraphic.getBackground(), PICKUP_COIL_BACK_LAYER );
+        
+        // Grid
+        CompassGridGraphic gridGraphic = new CompassGridGraphic( apparatusPanel, turbineModel, FaradayConfig.GRID_SPACING, FaradayConfig.GRID_SPACING );
+        gridGraphic.setRescaler( rescaler );
+        gridGraphic.setNeedleSize( FaradayConfig.GRID_NEEDLE_SIZE );
+        gridGraphic.setAlphaEnabled( ! APPARATUS_BACKGROUND.equals( Color.BLACK ) );
+        gridGraphic.setVisible( false );
+        apparatusPanel.addChangeListener( gridGraphic );
+        apparatusPanel.addGraphic( gridGraphic, GRID_LAYER );
+        super.setCompassGridGraphic( gridGraphic );
+        
+        // CompassGraphic
+        CompassGraphic compassGraphic = new CompassGraphic( apparatusPanel, compassModel );
+        compassGraphic.setLocation( COMPASS_LOCATION );
+        apparatusPanel.addChangeListener( compassGraphic );
+        apparatusPanel.addGraphic( compassGraphic, COMPASS_LAYER );
+
+        // Field Meter
+        FieldMeterGraphic fieldMeterGraphic = new FieldMeterGraphic( apparatusPanel, turbineModel );
+        fieldMeterGraphic.setLocation( FIELD_METER_LOCATION );
+        fieldMeterGraphic.setVisible( false );
+        apparatusPanel.addChangeListener( fieldMeterGraphic );
+        apparatusPanel.addGraphic( fieldMeterGraphic, FIELD_METER_LAYER );
+        
+//        // XXX test bounds & registration point of FaradaySlider
+//        FaradaySlider _speedSlider = new FaradaySlider( apparatusPanel, 100 );
+//        addGraphic( _speedSlider, DEBUG_LAYER );
+//        _speedSlider.setMinimum( -100 );
+//        _speedSlider.setMaximum( 100 );
+//        _speedSlider.setValue( 0 );
+//        _speedSlider.addTick( -100 );
+//        _speedSlider.addTick( 100 );
+//        _speedSlider.addTick( 0 );
+//        _speedSlider.setLocation( 100, 100 );
         
         // Debugger
-//      DebuggerGraphic debugger = new DebuggerGraphic( apparatusPanel );
-//      debugger.setLocationColor( Color.GREEN );
-//      debugger.add( fieldMeterGraphic );
-//      apparatusPanel.addGraphic( debugger, DEBUG_LAYER );
-        
+//        DebuggerGraphic debugger = new DebuggerGraphic( apparatusPanel );
+//        debugger.add( _speedSlider );
+//        apparatusPanel.addGraphic( debugger, DEBUG_LAYER );
+
         // Collision detection
+        compassGraphic.getCollisionDetector().add( pickupCoilGraphic );
+        pickupCoilGraphic.getCollisionDetector().add( compassGraphic );
         
         //----------------------------------------------------------------------------
         // Control
         //----------------------------------------------------------------------------
-
+        
         // Control Panel
-        GeneratorControlPanel controlPanel = new GeneratorControlPanel( this );
+        GeneratorControlPanel controlPanel = new GeneratorControlPanel( this, 
+            turbineModel, compassModel, generatorGraphic, gridGraphic, fieldMeterGraphic, 
+            pickupCoilModel, pickupCoilGraphic.getCoilGraphic(), lightbulbModel, voltmeterModel );
         this.setControlPanel( controlPanel );
         
         //----------------------------------------------------------------------------
