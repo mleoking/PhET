8e5704a94a999fffa3d7795a482e041ee6059e2b
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-03-11 02:16:08 +0000

    First attempt at a PhetJPanel. It works with the example in BasicPhetJComponent. Mouse clicks come through properly, and some of the layout is correct, but the border on the outer JPanel doesn't display properly.

---
+++ trunk/phetcommon/tests-src/edu/colorado/phet/common/tests/phetjcomponents/PhetJPanel.java
@@ -0,0 +1,62 @@
+/* Copyright 2004, University of Colorado */
+
+/*
+ * CVS Info -
+ * Filename : $Source: 
+ * Branch : $Name:  
+ * Modified by : $Author: 
+ * Revision : $Revision: 
+ * Date modified : $Date: 
+ */
+
+package edu.colorado.phet.common.tests.phetjcomponents;
+
+import edu.colorado.phet.common.view.phetgraphics.GraphicLayerSet;
+import edu.colorado.phet.common.view.phetgraphics.PhetGraphic;
+import edu.colorado.phet.common.view.ApparatusPanel;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ *
+ */
+public class PhetJPanel extends GraphicLayerSet {
+
+    /**
+     *
+     * @param ap
+     * @param jp
+     */
+    public PhetJPanel( ApparatusPanel ap, JPanel jp ) {
+        super( ap );
+
+        // Get Swing to set the locations of the components of the panel
+        JFrame dummyFrame = new JFrame();
+        Container contentPane = dummyFrame.getContentPane();
+        contentPane.add( jp );
+        dummyFrame.pack();
+
+        // Add a PhetJComponent for the the JPanel itself
+        this.addGraphic( new PhetJComponent( ap, jp ) );
+
+        // Go through the components in the JPanel and add aPhetJComponent for
+        // each of them. Use the location that Swing determined for the JComponent
+        // for the PhetJComponent
+        Component[] components = jp.getComponents();
+        PhetGraphic pjc = null;
+        for( int i = 0; i < components.length; i++ ) {
+            Component component = components[i];
+            Point location = new Point( (int)component.getLocation().getX(),
+                                        (int)component.getLocation().getY() );
+            if( component instanceof JComponent ) {
+                pjc = new PhetJComponent( ap, (JComponent)component );
+            }
+            if( component instanceof JPanel ) {
+                pjc = new PhetJPanel( ap, (JPanel)component );
+            }
+            this.addGraphic( pjc );
+            pjc.setLocation( location );
+        }
+    }
+}
