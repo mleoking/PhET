ed5ccf645368de17fff44e44444e5963fbe6904d
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-03-11 19:07:57 +0000

    Added modelToViewDifferential() and viewToModelDifferential()

--- phetcommon/src/edu/colorado/phet/common/math/ModelViewTransform1D.java
+++ phetcommon/src/edu/colorado/phet/common/math/ModelViewTransform1D.java
@@ -13,6 +13,8 @@ import java.util.ArrayList;
 public class ModelViewTransform1D {
     private Function.LinearFunction modelToView;
     private ArrayList transformListeners = new ArrayList();
+    private double minModel;
+    private double maxModel;
 
     public interface Observer {
         void transformChanged( ModelViewTransform1D transform );
@@ -20,10 +22,20 @@ public class ModelViewTransform1D {
 
     public ModelViewTransform1D( double minModel, double maxModel, int minView, int maxView ) {
         this.modelToView = new Function.LinearFunction( minModel, maxModel, minView, maxView );
+        this.minModel = minModel;
+        this.maxModel = maxModel;
     }
 
     public int modelToView( double x ) {
         return (int)modelToView.evaluate( x );
+    }
+
+    public int modelToViewDifferential( double dModel ) {
+        return modelToView( dModel ) - modelToView( 0 );
+    }
+
+    public double viewToModelDifferential( int dView ) {
+        return viewToModel( dView ) - viewToModel( 0 );            
     }
 
     public double viewToModel( int x ) {
