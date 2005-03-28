70e381b32ea0d9d968b82ac42e358ef5390fdec7
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-03-28 23:18:53 +0000

    Added curveTo()

--- branches/frame-layout-and-alpha-3-24-05/phetcommon/src/edu/colorado/phet/common/view/util/DoubleGeneralPath.java
+++ branches/frame-layout-and-alpha-3-24-05/phetcommon/src/edu/colorado/phet/common/view/util/DoubleGeneralPath.java
@@ -62,6 +62,10 @@ public class DoubleGeneralPath {
         path.quadTo( (float)x1, (float)y1, (float)x2, (float)y2 );
     }
 
+    public void curveTo( double x1, double y1, double x2, double y2, double x3, double y3 ) {
+        path.curveTo( (float)x1, (float)y1, (float)x2, (float)y2, (float)x3, (float)y3 );
+    }
+
     public void lineTo( double x, double y ) {
         path.lineTo( (float)x, (float)y );
     }
