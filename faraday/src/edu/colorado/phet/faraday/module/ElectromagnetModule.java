e2dda0c43a5f7e6e724440488a8a9ba6224eb946
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-04-11 17:03:24 +0000

    - make Eletromagnet wiggle me go away if location changes

--- faraday/src/edu/colorado/phet/faraday/module/ElectromagnetModule.java
+++ faraday/src/edu/colorado/phet/faraday/module/ElectromagnetModule.java
@@ -14,6 +14,7 @@ package edu.colorado.phet.faraday.module;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Point;
+import java.awt.geom.Point2D;
 
 import edu.colorado.phet.common.model.BaseModel;
 import edu.colorado.phet.common.model.clock.AbstractClock;
@@ -192,13 +193,14 @@ public class ElectromagnetModule extends FaradayModule {
         // Help
         //----------------------------------------------------------------------------
         
-        ThisWiggleMeGraphic wiggleMe = new ThisWiggleMeGraphic( apparatusPanel, model, batteryModel );
+        ThisWiggleMeGraphic wiggleMe = new ThisWiggleMeGraphic( apparatusPanel, model, batteryModel, electromagnetModel );
         apparatusPanel.addGraphic( wiggleMe, HELP_LAYER );
     }
     
     /**
      * ThisWiggleMeGraphic is the wiggle me for this module.
-     * It disappears when the electromagnet battery's voltage is changed, or the battery is disabled.
+     * It disappears when the electromagnet battery's voltage is changed, 
+     * or the battery is disabled, or the electromagnet is moved.
      *
      * @author Chris Malley (cmalley@pixelzoom.com)
      * @version $Revision$
@@ -207,6 +209,8 @@ public class ElectromagnetModule extends FaradayModule {
 
         private Battery _batteryModel;
         private double _batteryVoltage;
+        private Electromagnet _electromagnetModel;
+        private Point2D _electromagnetLocation;
 
         /**
          * Sole constructor.
@@ -214,13 +218,18 @@ public class ElectromagnetModule extends FaradayModule {
          * @param component
          * @param model
          * @param batteryModel
+         * @param electromagnetModel
          */
-        public ThisWiggleMeGraphic( Component component, BaseModel model, Battery batteryModel ) {
+        public ThisWiggleMeGraphic( Component component, BaseModel model, 
+                Battery batteryModel, Electromagnet electromagnetModel ) {
             super( component, model );
 
             _batteryModel = batteryModel;
-            _batteryVoltage = batteryModel.getVoltage();
-            batteryModel.addObserver( this );
+            _batteryVoltage = _batteryModel.getVoltage();
+            _batteryModel.addObserver( this );
+            _electromagnetModel = electromagnetModel;
+            _electromagnetLocation = _electromagnetModel.getLocation();
+            _electromagnetModel.addObserver( this );
             
             setText( SimStrings.get( "ElectromagnetModule.wiggleMe" ) );
             addArrow( WiggleMeGraphic.BOTTOM_LEFT, new Vector2D( -50, 50 ) );
@@ -236,11 +245,14 @@ public class ElectromagnetModule extends FaradayModule {
          * If the battery voltage changes or the battery is disabled, disable and unwire the wiggle me.
          */
         public void update() {
-            if ( _batteryVoltage != _batteryModel.getVoltage() || ! _batteryModel.isEnabled() ) {
+            if ( _batteryVoltage != _batteryModel.getVoltage() || 
+                    ! _batteryModel.isEnabled() ||
+                    ! _electromagnetLocation.equals( _electromagnetModel.getLocation() )  ) {
                 // Disable
                 setEnabled( false );
                 // Unwire
                 _batteryModel.removeObserver( this );
+                _electromagnetModel.removeObserver( this );
             }
         }
     }
