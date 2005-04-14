9a7dc7019b7ed08257f48125dd9451e0778b1bf4
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-04-14 17:50:51 +0000

    Only runs frame rate counter when the option is enabled.

--- phetcommon/src/edu/colorado/phet/common/util/DebugMenu.java
+++ phetcommon/src/edu/colorado/phet/common/util/DebugMenu.java
@@ -150,7 +150,7 @@ public class DebugMenu extends JMenu {
             super( "Show frame rate" );
             this.setMnemonic( 'f' );
             frameRateDlg = new JDialog( app.getPhetFrame(), "Frame Rate", false );
-            JTextArea textArea = new JTextArea( 10, 5 );
+            final JTextArea textArea = new JTextArea( 10, 5 );
             frameRateDlg.getContentPane().add( new JScrollPane( textArea,
                                                                 JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
                                                                 JScrollPane.HORIZONTAL_SCROLLBAR_NEVER ) );
@@ -161,10 +161,11 @@ public class DebugMenu extends JMenu {
             this.addActionListener( new ActionListener() {
                 public void actionPerformed( ActionEvent e ) {
                     frameRateDlg.setVisible( isSelected() );
+                    startRecording( app.getClock(), textArea );
                 }
             } );
 
-            startRecording( app.getApplicationModel().getClock(), textArea );
+//            startRecording( app.getApplicationModel().getClock(), textArea );
         }
 
         void startRecording( AbstractClock clock, final JTextArea textArea ) {
