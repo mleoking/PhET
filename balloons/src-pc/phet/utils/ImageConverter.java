c163c5e9d32fc150e5f66d00aa818e9209dd815c
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-04-22 16:31:07 +0000

    Changes from long long ago.

--- balloons/src-pc/phet/utils/ImageConverter.java
+++ balloons/src-pc/phet/utils/ImageConverter.java
@@ -19,13 +19,15 @@ public class ImageConverter {
 
     public static final BufferedImage toBufferedImageARGB( Image im, Component c )
             throws InterruptedException {
-        //helper.ThreadHelper.nap(1000);
         MediaTracker mt = new MediaTracker( c );
         mt.addImage( im, 0 );
         mt.waitForAll();
         BufferedImage bi = new BufferedImage( im.getWidth( null ), im.getHeight( null ), BufferedImage.TYPE_INT_ARGB );//watch out for -1.
+
         Graphics2D g = (Graphics2D)bi.getGraphics();
-        g.drawImage( im, new AffineTransform(), null );
+//        g.setBackground( new Color( 0, 0, 0, 1.0f ) );
+//        g.clearRect( 0, 0, im.getWidth( c ), im.getHeight( c ) );
+        g.drawImage( im, new AffineTransform(), c );
         return bi;
     }
 }
