2565dfc91904e06a5a33ed7296e2f7f7451d42c2
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-04-22 18:06:51 +0000

    turn off rendering of pickup coil sample points

--- trunk/faraday/src/edu/colorado/phet/faraday/FaradayConfig.java
+++ trunk/faraday/src/edu/colorado/phet/faraday/FaradayConfig.java
@@ -32,7 +32,7 @@ public class FaradayConfig {
     public static final boolean DEBUG_ENABLE_SCALE_PANEL = false;
     public static final boolean DEBUG_ENABLE_DEVELOPER_MENU = false;
     public static final boolean DEBUG_DRAW_ELECTROMAGNET_MODEL_SHAPE = false;
-    public static final boolean DEBUG_DRAW_PICKUP_SAMPLE_POINTS = true;
+    public static final boolean DEBUG_DRAW_PICKUP_SAMPLE_POINTS = false;
     
     //----------------------------------------------------------------------------
     // Application
