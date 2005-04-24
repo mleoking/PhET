a754898ee9a18bd98ffbc0ad09242dc51cd87cfa
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-04-24 18:45:55 +0000

    remove "critical angle" code, since its behavior did not yeild a physically accurate result

--- faraday/src/edu/colorado/phet/faraday/model/Turbine.java
+++ faraday/src/edu/colorado/phet/faraday/model/Turbine.java
@@ -16,19 +16,13 @@ import edu.colorado.phet.faraday.FaradayConfig;
 
 
 /**
- * Turbine is the model of a simple turbine.
+ * Turbine is the model of a simple turbine. It rotates at some speed, and its 
+ * rotation is measured in RPMs (rotations per minute).
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  * @version $Revision$
  */
 public class Turbine extends DipoleMagnet implements ModelElement {
-
-    //----------------------------------------------------------------------------
-    // Class data
-    //----------------------------------------------------------------------------
-    
-    // Determines whether all critical angles will be hit.
-    private static boolean _criticalAnglesEnabled = false;
     
     //----------------------------------------------------------------------------
     // Instance data
@@ -36,7 +30,7 @@ public class Turbine extends DipoleMagnet implements ModelElement {
     
     private double _speed; // -1...+1 (see setSpeed)
     private double _maxRPM; // rotations per minute at full speed
-    private double _maxDelta; // radians
+    private double _maxDelta; // change in angle at full speed, in radians
     
     //----------------------------------------------------------------------------
     // Constructors
@@ -89,7 +83,7 @@ public class Turbine extends DipoleMagnet implements ModelElement {
         // Pre-compute the maximum change in angle per clock tick.
         double framesPerSecond = FaradayConfig.CLOCK_FRAME_RATE;
         double framesPerMinute = 60 * framesPerSecond;
-        _maxDelta = ( 2 * Math.PI) * ( maxRPM / framesPerMinute );
+        _maxDelta = ( 2 * Math.PI ) * ( maxRPM / framesPerMinute );
     }
     
     /**
@@ -110,55 +104,20 @@ public class Turbine extends DipoleMagnet implements ModelElement {
         return Math.abs( _speed * _maxRPM );
     }
     
-    /**
-     * Enables or disables critical angles.
-     * When enabled, the delta angle may be adjusted so that the rotation
-     * hits the points where it is parallel and perpendicular to the pickup coil.
-     * 
-     * @param criticalAnglesEnabled true or false
-     */
-    public static void setCriticalAnglesEnabled( boolean criticalAnglesEnabled ) {
-        _criticalAnglesEnabled = criticalAnglesEnabled;
-    }
-    
-    /**
-     * Determines whether critical angles are enabled.
-     * See etCriticalAnglesEnabled.
-     * 
-     * @return true or false
-     */
-    public static boolean isCriticalAnglesEnabled() {
-        return _criticalAnglesEnabled;
-    }
-    
     //----------------------------------------------------------------------------
     // ModelElement implementation
     //----------------------------------------------------------------------------
     
     /*
-     * @see edu.colorado.phet.common.model.ModelElement#stepInTime(double)
+     * Update the turbine's direction, based on its speed.
      */
     public void stepInTime( double dt ) {
         
         if ( _speed != 0 ) {
             
-            double previousDirection = getDirection();
-            double newDirection = getDirection() + ( dt * _speed * _maxDelta );
-
-            // Adjust the angle so that we hit all peaks and zero crossings.
-            if ( _criticalAnglesEnabled ) {
-                for ( int i = 0; i < 5; i++ ) {
-                    double criticalAngle = i * ( Math.PI / 2 ); // ...at 90 degree intervals
-                    if ( _speed > 0 && newDirection > criticalAngle && previousDirection < criticalAngle ) {
-                        newDirection = criticalAngle;
-                        break;
-                    }
-                    else if ( _speed < 0 && newDirection < -criticalAngle && previousDirection > -criticalAngle ) {
-                        newDirection = -criticalAngle;
-                        break;
-                    }
-                }
-            }
+            // Determine the new direction
+            double delta = dt * _speed * _maxDelta;
+            double newDirection = getDirection() + delta;
             
             // Limit direction to -360...+360 degrees.
             int sign = ( newDirection < 0 ) ? -1 : +1;
