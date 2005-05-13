3e06df9e259fd11e38485f95cefaefdb11679f02
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-05-13 19:57:19 +0000

    Beginning work on squiggles

--- trunk/fluorescent-lights/src/edu/colorado/phet/dischargelamps/DischargeLampsApp.java
+++ trunk/fluorescent-lights/src/edu/colorado/phet/dischargelamps/DischargeLampsApp.java
@@ -33,33 +33,35 @@ public class DischargeLampsApp extends PhetApplication {
         super( descriptor, args );
     }
 
-//    public FluorescentLightsApp( String args[] ) {
-//        super( args,
-//               new SwingTimerClock( 12, 25, AbstractClock.FRAMES_PER_SECOND ),
-//               "Fluorescent Lights",
-//               "Fluorescent Lights",
-//               "0.01" );
-//
-//        // Determine the resolution of the screen
-//        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-//        FrameSetup frameSetup = new FrameSetup.CenteredWithSize( 1024, 750 );
-//        if( dim.getWidth() == 1024 || dim.getHeight() == 768 ) {
-//            frameSetup = new FrameSetup.MaxExtent( new FrameSetup.CenteredWithSize( 1024, 750 ) );
-//        }
-//        setFrameSetup( frameSetup );
-//
-//        DischargeLampModule singleAtomModule = new DischargeLampModule( SimStrings.get( "ModuleTitle.SingleAtomModule" ),
-//                                                                        getClock() );
-//
-//        double maxSpeed = 0.1;
-//        DischargeLampModule multipleAtomModule = new DischargeLampModule( SimStrings.get( "ModuleTitle.MultipleAtomModule" ),
-//                                                                          getClock() );
-//        addModule( singleAtomModule );
-//        addModule( multipleAtomModule );
-//        setInitialModule( singleAtomModule );
-//
-//        getPhetFrame().pack();
-//    }
+    public DischargeLampsApp( String[] args ) {
+        super( args, SimStrings.get( "DischargeLampsApplication.title" ),
+               SimStrings.get( "DischargeLampsApplication.title" ),
+               "0.01",
+               new SwingTimerClock( DischargeLampsConfig.DT, DischargeLampsConfig.FPS, AbstractClock.FRAMES_PER_SECOND ),
+               true );
+
+        // Determine the resolution of the screen
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        FrameSetup frameSetup = new FrameSetup.CenteredWithSize( 1024, 768 );
+        if( dim.getWidth() == 1024 || dim.getHeight() == 768 ) {
+            frameSetup = new FrameSetup.MaxExtent( new FrameSetup.CenteredWithSize( 1024, 768 ) );
+        }
+        setFrameSetup( frameSetup );
+
+        DischargeLampModule singleAtomModule = new SingleAtomModule( SimStrings.get( "ModuleTitle.SingleAtomModule" ),
+                                                                     getClock(),
+                                                                     DischargeLampsConfig.NUM_ENERGY_LEVELS );
+
+        double maxSpeed = 0.1;
+        DischargeLampModule multipleAtomModule = new MultipleAtomModule( SimStrings.get( "ModuleTitle.MultipleAtomModule" ),
+                                                                         getClock(), 30,
+                                                                         DischargeLampsConfig.NUM_ENERGY_LEVELS,
+                                                                         maxSpeed );
+        setModules( new Module[]{singleAtomModule,
+                                 multipleAtomModule} );
+        setInitialModule( singleAtomModule );
+    }
+
 
     private static class AppDesc extends ApplicationModel {
         public AppDesc() {
@@ -89,16 +91,22 @@ public class DischargeLampsApp extends PhetApplication {
             setModules( new Module[]{
                 singleAtomModule,
                 multipleAtomModule} );
-            setInitialModule( multipleAtomModule );
+//            setInitialModule( multipleAtomModule );
             setInitialModule( singleAtomModule );
         }
     }
 
+    /**
+     * @param args
+     */
     public static void main( String[] args ) {
+
+        // Tell SimStrings where the simulations-specific strings are
         SimStrings.setStrings( DischargeLampsConfig.localizedStringsPath );
         SimStrings.setStrings( LaserConfig.localizedStringsPath );
 
-        DischargeLampsApp app = new DischargeLampsApp( new AppDesc(), args );
+        DischargeLampsApp app = new DischargeLampsApp( args );
+//        DischargeLampsApp app = new DischargeLampsApp( new AppDesc(), args );
         app.startApplication();
     }
 
