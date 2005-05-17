17a58602790dffdd033961fe483ae7f94d630918
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-05-17 15:05:56 +0000

    no message

--- fluorescent-lights/src/edu/colorado/phet/dischargelamps/DischargeLampsApp.java
+++ fluorescent-lights/src/edu/colorado/phet/dischargelamps/DischargeLampsApp.java
@@ -29,10 +29,10 @@ import java.awt.*;
  */
 public class DischargeLampsApp extends PhetApplication {
 
-    public DischargeLampsApp( ApplicationModel descriptor, String args[] ) {
-        super( descriptor, args );
-    }
-
+    /**
+     *
+     * @param args
+     */
     public DischargeLampsApp( String[] args ) {
         super( args, SimStrings.get( "DischargeLampsApplication.title" ),
                SimStrings.get( "DischargeLampsApplication.title" ),
@@ -62,40 +62,6 @@ public class DischargeLampsApp extends PhetApplication {
         setInitialModule( singleAtomModule );
     }
 
-
-    private static class AppDesc extends ApplicationModel {
-        public AppDesc() {
-            super( SimStrings.get( "DischargeLampsApplication.title" ),
-                   SimStrings.get( "DischargeLampsApplication.title" ),
-                   "0.01" );
-
-            setClock( new SwingTimerClock( DischargeLampsConfig.DT, DischargeLampsConfig.FPS, AbstractClock.FRAMES_PER_SECOND ) );
-
-            // Determine the resolution of the screen
-            Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-            FrameSetup frameSetup = new FrameSetup.CenteredWithSize( 1024, 768 );
-            if( dim.getWidth() == 1024 || dim.getHeight() == 768 ) {
-                frameSetup = new FrameSetup.MaxExtent( new FrameSetup.CenteredWithSize( 1024, 768 ) );
-            }
-            setFrameSetup( frameSetup );
-
-            DischargeLampModule singleAtomModule = new SingleAtomModule( SimStrings.get( "ModuleTitle.SingleAtomModule" ),
-                                                                         getClock(),
-                                                                         DischargeLampsConfig.NUM_ENERGY_LEVELS );
-
-            double maxSpeed = 0.1;
-            DischargeLampModule multipleAtomModule = new MultipleAtomModule( SimStrings.get( "ModuleTitle.MultipleAtomModule" ),
-                                                                             getClock(), 30,
-                                                                             DischargeLampsConfig.NUM_ENERGY_LEVELS,
-                                                                             maxSpeed );
-            setModules( new Module[]{
-                singleAtomModule,
-                multipleAtomModule} );
-//            setInitialModule( multipleAtomModule );
-            setInitialModule( singleAtomModule );
-        }
-    }
-
     /**
      * @param args
      */
@@ -106,7 +72,6 @@ public class DischargeLampsApp extends PhetApplication {
         SimStrings.setStrings( LaserConfig.localizedStringsPath );
 
         DischargeLampsApp app = new DischargeLampsApp( args );
-//        DischargeLampsApp app = new DischargeLampsApp( new AppDesc(), args );
         app.startApplication();
     }
 
