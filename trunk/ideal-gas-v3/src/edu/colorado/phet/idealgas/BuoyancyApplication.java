bf7541fa77bf4b9126292807fdf6b652c55e92ef
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-05-18 20:07:26 +0000

    Switched to new startup code

--- trunk/ideal-gas-v3/src/edu/colorado/phet/idealgas/BuoyancyApplication.java
+++ trunk/ideal-gas-v3/src/edu/colorado/phet/idealgas/BuoyancyApplication.java
@@ -11,59 +11,43 @@ import edu.colorado.phet.common.application.Module;
 import edu.colorado.phet.common.application.ModuleManager;
 import edu.colorado.phet.common.application.PhetApplication;
 import edu.colorado.phet.common.model.clock.SwingTimerClock;
+import edu.colorado.phet.common.model.clock.AbstractClock;
 import edu.colorado.phet.common.view.ApparatusPanel;
 import edu.colorado.phet.common.view.PhetFrame;
 import edu.colorado.phet.common.view.util.SimStrings;
+import edu.colorado.phet.common.view.util.FrameSetup;
 import edu.colorado.phet.idealgas.controller.HeliumBalloonModule;
 import edu.colorado.phet.idealgas.controller.HotAirBalloonModule;
 import edu.colorado.phet.idealgas.controller.IdealGasModule;
 import edu.colorado.phet.idealgas.controller.RigidHollowSphereModule;
 import edu.colorado.phet.idealgas.view.IdealGasLandF;
+import edu.colorado.phet.idealgas.model.IdealGasModel;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class BuoyancyApplication extends PhetApplication {
 
-    static class IdealGasApplicationModel extends ApplicationModel {
-        public IdealGasApplicationModel() {
-            super( SimStrings.get( "BuoyancyApplication.title" ),
-                   SimStrings.get( "BuoyancyApplication.description" ),
-                   IdealGasConfig.VERSION,
-                   IdealGasConfig.FRAME_SETUP );
+    public BuoyancyApplication( String[] args ) {
+        super( args, SimStrings.get( "BuoyancyApplication.title" ),
+               SimStrings.get( "BuoyancyApplication.description" ),
+               IdealGasConfig.VERSION,
+               new SwingTimerClock( IdealGasConfig.TIME_STEP, IdealGasConfig.WAIT_TIME, true ),
+               true,
+               IdealGasConfig.FRAME_SETUP );
 
-            // Create the clock
-            SwingTimerClock clock = new SwingTimerClock( IdealGasConfig.TIME_STEP, IdealGasConfig.WAIT_TIME, true );
-            setClock( clock );
-
-            // Create the modules
-            Module idealgasModule = new IdealGasModule( getClock() );
-            Module rigidSphereModule = new RigidHollowSphereModule( getClock() );
-            Module heliumBalloonModule = new HeliumBalloonModule( getClock() );
-            Module hotAirBalloonModule = new HotAirBalloonModule( getClock() );
-            Module[] modules = new Module[]{
-                hotAirBalloonModule,
-                rigidSphereModule,
-                heliumBalloonModule,
-                idealgasModule,
-            };
-            setModules( modules );
-            setInitialModule( hotAirBalloonModule );
-//            setInitialModule( rigidSphereModule );
-
-            // Set the initial size
-            setFrameCenteredSize( 920, 700 );
-//            setFrameCenteredSize( 1020, 700 );
-        }
-    }
-
-    public BuoyancyApplication( String[] args) {
-        super( new IdealGasApplicationModel(), args );
-
-        // Add some menus
-        PhetFrame frame = getPhetFrame();
-//        frame.addMenu( new OptionsMenu( this ) );
-        this.startApplication();
+        // Create the modules
+        Module idealgasModule = new IdealGasModule( getClock() );
+        Module rigidSphereModule = new RigidHollowSphereModule( getClock() );
+        Module heliumBalloonModule = new HeliumBalloonModule( getClock() );
+        Module hotAirBalloonModule = new HotAirBalloonModule( getClock() );
+        Module[] modules = new Module[]{
+            hotAirBalloonModule,
+            rigidSphereModule,
+            heliumBalloonModule,
+            idealgasModule,
+        };
+        setModules( modules );
     }
 
     protected void parseArgs( String[] args ) {
@@ -71,11 +55,11 @@ public class BuoyancyApplication extends PhetApplication {
 
         for( int i = 0; i < args.length; i++ ) {
             String arg = args[i];
-            if( arg.startsWith( "-B")) {
+            if( arg.startsWith( "-B" ) ) {
                 ModuleManager mm = this.getModuleManager();
                 for( int j = 0; j < mm.numModules(); j++ ) {
                     ApparatusPanel ap = mm.moduleAt( j ).getApparatusPanel();
-                    ap.setBackground( Color.black);
+                    ap.setBackground( Color.black );
                     ap.paintImmediately( ap.getBounds() );
                 }
             }
@@ -92,6 +76,6 @@ public class BuoyancyApplication extends PhetApplication {
         }
 
         SimStrings.setStrings( IdealGasConfig.localizedStringsPath );
-        new BuoyancyApplication( args );
+        new BuoyancyApplication( args ).startApplication();
     }
 }
