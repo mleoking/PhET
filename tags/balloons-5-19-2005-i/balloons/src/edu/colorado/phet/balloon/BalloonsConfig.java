6822f756455c8b38039d94427d93328c4177452c
nobody <nobody@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-05-19 16:22:11 +0000

    This commit was manufactured by cvs2svn to create tag 'balloons-5-19-2005-i'.

---
+++ tags/balloons-5-19-2005-i/balloons/src/edu/colorado/phet/balloon/BalloonsConfig.java
@@ -0,0 +1,8 @@
+
+package edu.colorado.phet.balloon;
+
+public class BalloonsConfig {
+
+    public static final String localizedStringsPath = "localization/SimStrings";
+
+} // end of public class BalloonsConfig
