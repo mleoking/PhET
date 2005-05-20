91fdd02c4890b8c89ef6f9fa81a86f7c2d36b37f
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-05-20 16:02:10 +0000

    convert to new PhetApplication startup sequence

--- trunk/fourier/src/edu/colorado/phet/fourier/FourierApplication.java
+++ trunk/fourier/src/edu/colorado/phet/fourier/FourierApplication.java
@@ -15,9 +15,17 @@ import java.io.IOException;
 
 import javax.swing.JMenu;
 
+import edu.colorado.phet.common.application.Module;
 import edu.colorado.phet.common.application.PhetApplication;
+import edu.colorado.phet.common.model.clock.AbstractClock;
+import edu.colorado.phet.common.model.clock.SwingTimerClock;
+import edu.colorado.phet.common.view.PhetLookAndFeel;
 import edu.colorado.phet.common.view.util.FrameSetup;
 import edu.colorado.phet.common.view.util.SimStrings;
+import edu.colorado.phet.fourier.module.ContinuousModule;
+import edu.colorado.phet.fourier.module.DiscreteModule;
+import edu.colorado.phet.fourier.module.DiscreteToContinousModule;
+import edu.colorado.phet.fourier.module.WavePulseShaperModule;
 
 
 /**
@@ -28,12 +36,13 @@ import edu.colorado.phet.common.view.util.SimStrings;
  */
 public class FourierApplication extends PhetApplication {
 
-
     //----------------------------------------------------------------------------
     // Class data
     //----------------------------------------------------------------------------
     
     private static final boolean ENABLE_DEVELOPER_MENU = true;
+    
+    private static final boolean TEST_ONE_MODULE = false;
     
     //----------------------------------------------------------------------------
     // Constructors
@@ -42,15 +51,22 @@ public class FourierApplication extends PhetApplication {
     /**
      * Sole constructor.
      * 
-     * @param appModel the application model
-     * @throws IOException
+     * @param args command line arguments
+     * @param title
+     * @param description
+     * @param version
+     * @param clock
+     * @param useClockControlPanel
+     * @param frameSetup
      */
-    public FourierApplication( FourierApplicationModel appModel, String[] args ) throws IOException {
-        super( appModel, args );
-        assert( appModel != null );
+    public FourierApplication( String[] args, 
+            String title, String description, String version, AbstractClock clock,
+            boolean useClockControlPanel, FrameSetup frameSetup )
+    {
+        super( args, title, description, version, clock, useClockControlPanel, frameSetup );
         initMenubar();
     }
-
+    
     //----------------------------------------------------------------------------
     // Menubar
     //----------------------------------------------------------------------------
@@ -86,9 +102,9 @@ public class FourierApplication extends PhetApplication {
         SimStrings.init( args, FourierConfig.LOCALIZATION_BUNDLE_BASENAME );
         
         // Initialize Look-&-Feel
-//        PhetLookAndFeel.setLookAndFeel();
-//        PhetLookAndFeel laf = new PhetLookAndFeel();
-//        laf.apply();
+        PhetLookAndFeel.setLookAndFeel();
+        PhetLookAndFeel laf = new PhetLookAndFeel();
+        laf.apply();
         
         // Get stuff needed to initialize the application model.
         String title = SimStrings.get( "FourierApplication.title" );
@@ -98,11 +114,31 @@ public class FourierApplication extends PhetApplication {
         int height = FourierConfig.APP_FRAME_HEIGHT;
         FrameSetup frameSetup = new FrameSetup.CenteredWithSize( width, height );
 
-        // Create the application model.
-        FourierApplicationModel appModel = new FourierApplicationModel( title, description, version, frameSetup );
-
+        // Clock
+        double timeStep = FourierConfig.CLOCK_TIME_STEP;
+        int waitTime = ( 1000 / FourierConfig.CLOCK_FRAME_RATE ); // milliseconds
+        boolean isFixed = FourierConfig.CLOCK_TIME_STEP_IS_CONSTANT;
+        AbstractClock clock = new SwingTimerClock( timeStep, waitTime, isFixed );
+        boolean useClockControlPanel = true;
+        
         // Create the application.
-        PhetApplication app = new FourierApplication( appModel, args );
+        FourierApplication app = new FourierApplication( args,
+                 title, description, version, clock, useClockControlPanel, frameSetup );
+        
+        // Simulation Modules
+        if ( TEST_ONE_MODULE ) {
+            Module module = new DiscreteModule( clock );
+            app.setModules( new Module[] { module } );
+            app.setInitialModule( module );
+        }
+        else {
+            DiscreteModule discreteModule = new DiscreteModule( clock );
+            DiscreteToContinousModule discreteToContinuousModule = new DiscreteToContinousModule( clock );
+            ContinuousModule continuousModule = new ContinuousModule( clock );
+            WavePulseShaperModule wavePulseShapeModule = new WavePulseShaperModule( clock );
+            app.setModules( new Module[] { discreteModule, discreteToContinuousModule, continuousModule, wavePulseShapeModule } );
+            app.setInitialModule( discreteModule );
+        }
         
         // Start the application.
         app.startApplication();
