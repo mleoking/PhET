de5eb5c485341d31d79a6b92a0d8b0dd8f4e7063
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-05-20 20:55:32 +0000

    more readable grid computation code

--- trunk/charts/development/src/edu/colorado/phet/chart/AbstractGrid.java
+++ trunk/charts/development/src/edu/colorado/phet/chart/AbstractGrid.java
@@ -13,24 +13,23 @@ import java.util.ArrayList;
  * Copyright (c) Sep 21, 2004 by Sam Reid
  */
 public abstract class AbstractGrid extends CompositePhetGraphic {
-    private double[] lines;
     public final static int HORIZONTAL = 1;
     public final static int VERTICAL = 2;
 
-//    private boolean visible = true;
     private Chart chart;
     private int orientation;
     private Stroke stroke;
     private Color color;
     private double spacing;
     private double crossesOtherAxisAt;
+    private double[] lines;
 
-    protected AbstractGrid( Chart chart, int orientation, Stroke stroke, Color color, double tickSpacing, double crossesOtherAxisAt ) {
+    protected AbstractGrid( Chart chart, int orientation, Stroke stroke, Color color, double spacing, double crossesOtherAxisAt ) {
         this.chart = chart;
         this.orientation = orientation;
         this.stroke = stroke;
         this.color = color;
-        this.spacing = tickSpacing;
+        this.spacing = spacing;
         this.crossesOtherAxisAt = crossesOtherAxisAt;
     }
 
@@ -47,8 +46,9 @@ public abstract class AbstractGrid extends CompositePhetGraphic {
     protected abstract void update();
 
     public void setSpacing( double spacing ) {
+//        System.out.println( "spacing = " + spacing );
         this.spacing = spacing;
-        lines = null;
+//        lines = null;
         update();
     }
 
@@ -111,24 +111,52 @@ public abstract class AbstractGrid extends CompositePhetGraphic {
 //        return output;
 //    }
 
-    public double[] getGridLines( double origin, double min, double max, double spacing ) {
-        if( lines != null ) {
-            return lines;
-        }
+    private static double[] getGridLines( double origin, double min, double max, double spacing ) {
+//        return new double[]{0,10};
+//        System.out.println( "getting gridlines: spacing = " + spacing );
         int n = (int)Math.ceil( ( min - origin ) / spacing );
+        if( n > 100 ) {
+            new RuntimeException( "Lots of gridlines coming up, n=" + n ).printStackTrace();
+        }
         ArrayList results = new ArrayList();
         for( double currentPoint = origin + n * spacing; currentPoint <= max; currentPoint += spacing ) {
             results.add( new Double( currentPoint ) );
+//            System.out.println( "results.size() = " + results.size() );
         }
         double[] output = new double[results.size()];
         for( int i = 0; i < output.length; i++ ) {
             output[i] = ( (Double)results.get( i ) ).doubleValue();
         }
+
+//        System.out.println( "output.length = " + output.length );
+
         return output;
     }
 
     public void setGridlines( double[] lines ) {
+        if( lines.length > 100 ) {
+            System.out.println( "AbstractGrid.lines.length = " + lines.length );
+            new RuntimeException( "Lots of lines: lines.length=" + lines.length ).printStackTrace();
+        }
         this.lines = lines;
         update();
     }
+
+    public double[] getGridlines() {
+        if( lines == null ) {
+            setGridlines( createDefaultLines() );
+        }
+        return lines;
+    }
+
+    private double[] createDefaultLines() {
+        if( getOrientation() == HORIZONTAL ) {
+            return ( getGridLines( getCrossesOtherAxisAt(), chart.getRange().getMinX(), chart.getRange().getMaxX(), getSpacing() ) );
+        }
+        else {
+            return ( getGridLines( getCrossesOtherAxisAt(), chart.getRange().getMinY(), chart.getRange().getMaxY(), getSpacing() ) );
+        }
+
+    }
+
 }
