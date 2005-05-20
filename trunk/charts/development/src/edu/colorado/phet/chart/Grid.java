0bafb5a8dee95436311008c8c8115d31373c1dff
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-05-20 20:53:45 +0000

    bugfix on bounds computation

--- trunk/charts/development/src/edu/colorado/phet/chart/Grid.java
+++ trunk/charts/development/src/edu/colorado/phet/chart/Grid.java
@@ -28,26 +28,38 @@ public class Grid extends AbstractGrid {
         clear();
         int orientation = super.getOrientation();
         Chart chart = super.getChart();
+        double[] gridLines = getGridlines();
+//        System.out.println( "gridLines.length = " + gridLines.length );
         if( orientation == VERTICAL ) {
-            double[] gridLines = getGridLines( getCrossesOtherAxisAt(), chart.getRange().getMinX(), chart.getRange().getMaxX(), getSpacing() );
+
+//            double[] gridLines = getGridLines( getCrossesOtherAxisAt(), chart.getRange().getMinX(), chart.getRange().getMaxX(), getSpacing() );
             for( int i = 0; i < gridLines.length; i++ ) {
                 double gridLineX = gridLines[i];
-                Point src = chart.transform( gridLineX, chart.getRange().getMinY() );
-                Point dst = chart.transform( gridLineX, chart.getRange().getMaxY() );
-                Line2D.Double line = new Line2D.Double( src.x, src.y, dst.x, dst.y );
-                PhetShapeGraphic lineGraphic = new PhetShapeGraphic( chart.getComponent(), line, getStroke(), getColor() );
-                addGraphic( lineGraphic );
+                if( gridLineX >= chart.getRange().getMinX() && gridLineX <= chart.getRange().getMaxX() ) {//TODO this is a big bug.
+//                    System.out.println( "gridLineX = " + gridLineX );
+                    Point src = chart.transform( gridLineX, chart.getRange().getMinY() );
+//                    System.out.println( "chart.getModelViewTransform() = " + chart.getModelViewTransform() );
+//                    System.out.println( "src = " + src );
+                    Point dst = chart.transform( gridLineX, chart.getRange().getMaxY() );
+//                    System.out.println( "dst = " + dst );
+                    Line2D.Double line = new Line2D.Double( src.x, src.y, dst.x, dst.y );
+                    PhetShapeGraphic lineGraphic = new PhetShapeGraphic( chart.getComponent(), line, getStroke(), getColor() );
+//                    System.out.println( "lineGraphic.getBounds() = " + lineGraphic.getBounds() );
+                    addGraphic( lineGraphic );
+                }
             }
         }
         else if( orientation == HORIZONTAL ) {
-            double[] gridLines = getGridLines( getCrossesOtherAxisAt(), chart.getRange().getMinY(), chart.getRange().getMaxY(), getSpacing() );
-            for( int i = 0; i < gridLines.length; i++ ) {
+//            double[] gridLines = getGridLines( getCrossesOtherAxisAt(), chart.getRange().getMinY(), chart.getRange().getMaxY(), getSpacing() );
+            for( int i = 0; i < gridLines.length; i++ ) {//TODO this is a big bug.
                 double gridLineY = gridLines[i];
-                Point src = chart.transform( chart.getRange().getMinX(), gridLineY );
-                Point dst = chart.transform( chart.getRange().getMaxX(), gridLineY );
-                Line2D.Double line = new Line2D.Double( src.x, src.y, dst.x, dst.y );
-                PhetShapeGraphic lineGraphic = new PhetShapeGraphic( chart.getComponent(), line, getStroke(), getColor() );
-                addGraphic( lineGraphic );
+                if( gridLineY >= chart.getRange().getMinY() && gridLineY <= chart.getRange().getMaxY() ) {
+                    Point src = chart.transform( chart.getRange().getMinX(), gridLineY );
+                    Point dst = chart.transform( chart.getRange().getMaxX(), gridLineY );
+                    Line2D.Double line = new Line2D.Double( src.x, src.y, dst.x, dst.y );
+                    PhetShapeGraphic lineGraphic = new PhetShapeGraphic( chart.getComponent(), line, getStroke(), getColor() );
+                    addGraphic( lineGraphic );
+                }
             }
         }
     }
