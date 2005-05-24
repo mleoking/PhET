80bf20cb115a21543759c29af29d4530d87578fa
nobody <nobody@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-05-24 23:36:19 +0000

    This commit was manufactured by cvs2svn to create tag 'sound-pe-merge-with-recalibrated-clock-5-26-05'.

---
+++ tags/sound-pe-merge-with-recalibrated-clock-5-26-05/sound-2/src/edu/colorado/phet/sound/view/RgbReporter.java
@@ -0,0 +1,12 @@
+/**
+ * Class: RgbReporter
+ * Class: edu.colorado.phet.sound.view
+ * User: Ron LeMaster
+ * Date: Aug 20, 2004
+ * Time: 4:07:27 PM
+ */
+package edu.colorado.phet.sound.view;
+
+public interface RgbReporter {
+    int rgbAt( int x, int y );
+}
