44bbbcb4a4b3f069a1541bf91ee31d2503e81d61
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-06-01 20:24:26 +0000

    add methods to control visibility, label visibility, font and stroke of major and minor ticks

--- charts/development/src/edu/colorado/phet/chart/Chart.java
+++ charts/development/src/edu/colorado/phet/chart/Chart.java
@@ -177,6 +177,22 @@ public class Chart extends GraphicLayerSet {
             addGraphic( majorTicks );
         }
 
+        public void setMajorTicksVisible( boolean visible ) {
+           majorTicks.setVisible( visible );    
+        }
+        
+        public void setMinorTicksVisible( boolean visible ) {
+            minorTicks.setVisible( visible );
+        }
+        
+        public void setMajorTickLabelsVisible( boolean visible ) {
+            majorTicks.setLabelsVisible( visible );
+        }
+        
+        public void setMinorTickLabelsVisible( boolean visible ) {
+            minorTicks.setLabelsVisible( visible );
+        }
+        
         public void setMajorTickSpacing( double majorTickSpacing ) {
             majorTicks.setSpacing( majorTickSpacing );
         }
@@ -193,6 +209,21 @@ public class Chart extends GraphicLayerSet {
             majorTicks.setOffset( dx, dy );
         }
 
+        public void setMajorTickFont( Font font ) {
+            majorTicks.setFont( font );
+        }
+        
+        public void setMinorTickFont( Font font ) {
+            minorTicks.setFont( font );
+        }
+        
+        public void setMajorTickStroke( Stroke stroke ) {
+            majorTicks.setStroke( stroke );
+        }
+        
+        public void setMinorTickStroke( Stroke stroke ) {
+            minorTicks.setStroke( stroke );
+        }
     }
 
     public TickMarkSet getVerticalTicks() {
