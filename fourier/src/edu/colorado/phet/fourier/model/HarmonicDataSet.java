911c7cef70ecd6194322294df0122d2eaf731a76
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-06-08 18:21:15 +0000

    - optimize memory management of the points array - restore my formatting

--- fourier/src/edu/colorado/phet/fourier/model/HarmonicDataSet.java
+++ fourier/src/edu/colorado/phet/fourier/model/HarmonicDataSet.java
@@ -11,13 +11,13 @@
 
 package edu.colorado.phet.fourier.model;
 
+import java.awt.geom.Point2D;
+import java.lang.reflect.Array;
+
 import edu.colorado.phet.chart.DataSet;
 import edu.colorado.phet.common.util.SimpleObserver;
 import edu.colorado.phet.fourier.FourierConstants;
 import edu.colorado.phet.fourier.util.FourierUtils;
-
-import java.awt.geom.Point2D;
-import java.util.ArrayList;
 
 
 /**
@@ -32,17 +32,18 @@ public class HarmonicDataSet extends DataSet implements SimpleObserver {
     //----------------------------------------------------------------------------
     // Instance data
     //----------------------------------------------------------------------------
-    
+
     private Harmonic _harmonic;
     private int _waveType;
     private double _fundamentalCycle;;
     private int _numberOfPoints;
     private int _numberOfFundamentalCycles;
-    
+    private Point2D[] _points;
+
     //----------------------------------------------------------------------------
     // Constructors & finalizers
     //----------------------------------------------------------------------------
-    
+
     /**
      * Sole constructor.
      *
@@ -51,10 +52,9 @@ public class HarmonicDataSet extends DataSet implements SimpleObserver {
      * @param fundamentalCycle          the width of a one cycle of the fundamental harmonic
      * @param numberOfFundamentalCycles the number of fundamental cycles
      */
-    public HarmonicDataSet( Harmonic harmonic, int numberOfPoints,
-                            double fundamentalCycle, int numberOfFundamentalCycles ) {
+    public HarmonicDataSet( Harmonic harmonic, int numberOfPoints, double fundamentalCycle, int numberOfFundamentalCycles ) {
 
-        assert( harmonic != null );
+        assert ( harmonic != null );
         _harmonic = harmonic;
         _harmonic.addObserver( this );
 
@@ -64,6 +64,11 @@ public class HarmonicDataSet extends DataSet implements SimpleObserver {
 
         _waveType = FourierConstants.WAVE_TYPE_SINE;
 
+        _points = new Point2D[ numberOfPoints + 1 ];
+        for ( int i = 0; i < _points.length; i++ ) {
+            _points[ i ] = new Point2D.Double();
+        }
+        
         update();
     }
 
@@ -75,19 +80,19 @@ public class HarmonicDataSet extends DataSet implements SimpleObserver {
         _harmonic.removeObserver( this );
         _harmonic = null;
     }
-    
+
     //----------------------------------------------------------------------------
     // Accessors
     //----------------------------------------------------------------------------
-    
+
     /**
      * Sets the wave type (sine or cosine).
      *
      * @param waveType WAVE_TYPE_SINE or WAVE_TYPE_COSINE
      */
     public void setWaveType( int waveType ) {
-        assert( waveType == FourierConstants.WAVE_TYPE_SINE || waveType == FourierConstants.WAVE_TYPE_COSINE );
-        if( waveType != _waveType ) {
+        assert ( waveType == FourierConstants.WAVE_TYPE_SINE || waveType == FourierConstants.WAVE_TYPE_COSINE );
+        if ( waveType != _waveType ) {
             _waveType = waveType;
             update();
         }
@@ -105,7 +110,7 @@ public class HarmonicDataSet extends DataSet implements SimpleObserver {
     //----------------------------------------------------------------------------
     // SimpleObserver implementation
     //----------------------------------------------------------------------------
-    
+
     /**
      * Updates this data set to match its associated Harmonic model.
      */
@@ -115,20 +120,19 @@ public class HarmonicDataSet extends DataSet implements SimpleObserver {
 
         double amplitude = _harmonic.getAmplitude();
 
-        if( amplitude != 0 ) {
+        if ( amplitude != 0 ) {
 
             int numberOfCycles = _numberOfFundamentalCycles * ( _harmonic.getOrder() + 1 );
-            double pointsPerCycle = _numberOfPoints / (double)numberOfCycles;
+            double pointsPerCycle = _numberOfPoints / (double) numberOfCycles;
             double deltaX = ( _numberOfFundamentalCycles * _fundamentalCycle ) / _numberOfPoints;
             double deltaAngle = ( 2.0 * Math.PI ) / pointsPerCycle;
-
             double startX = -2 * _fundamentalCycle;
             double startAngle = 0.0;
-            ArrayList ppoint = new ArrayList();
-            for( int i = 0; i <= _numberOfPoints; i++ ) {
+
+            for ( int i = 0; i < _points.length; i++ ) {
                 double angle = startAngle + ( i * deltaAngle );
                 double radians;
-                if( _waveType == FourierConstants.WAVE_TYPE_SINE ) {
+                if ( _waveType == FourierConstants.WAVE_TYPE_SINE ) {
                     radians = FourierUtils.sin( angle );
                 }
                 else { /* cosines */
@@ -136,10 +140,10 @@ public class HarmonicDataSet extends DataSet implements SimpleObserver {
                 }
                 double x = startX + ( i * deltaX );
                 double y = amplitude * radians;
-                ppoint.add( new Point2D.Double( x, y ) );
+                _points[ i ].setLocation( x, y );
             }
-            super.addAllPoints( (Point2D[])ppoint.toArray( new Point2D[0] ) );
-
+            
+            addAllPoints( _points );
         }
     }
 }
