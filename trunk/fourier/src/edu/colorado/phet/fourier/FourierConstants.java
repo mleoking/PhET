aed3ad58c7612f8034ab74fa699fcd8b3927a7c4
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-06-14 16:17:52 +0000

    add static validator methods for constants

--- trunk/fourier/src/edu/colorado/phet/fourier/FourierConstants.java
+++ trunk/fourier/src/edu/colorado/phet/fourier/FourierConstants.java
@@ -49,4 +49,66 @@ public class FourierConstants {
     
     /* Not intended for instantiation. */
     private FourierConstants() {}
+    
+    public static boolean isValidDomain( int domain ) {
+        boolean isValid = false;
+        switch ( domain ) {
+            case DOMAIN_SPACE:
+            case DOMAIN_TIME:
+            case DOMAIN_SPACE_AND_TIME:
+                 isValid = true;
+                 break;
+            default:
+                 isValid = false;
+        }
+        return isValid;
+    }
+    
+    public static boolean isValidPreset( int preset ) {
+        boolean isValid = false;
+        switch ( preset ) {
+            case PRESET_SINE_COSINE:
+            case PRESET_SAWTOOTH:
+            case PRESET_TRIANGLE:
+            case PRESET_WAVE_PACKET:
+            case PRESET_CUSTOM:
+                 isValid = true;
+                 break;
+            default:
+                 isValid = false;
+        }
+        return isValid;
+    }
+    
+    public static boolean isValidWaveType( int waveType ) {
+        boolean isValid = false;
+        switch ( waveType ) {
+            case WAVE_TYPE_SINE:
+            case WAVE_TYPE_COSINE:
+                 isValid = true;
+                 break;
+            default:
+                 isValid = false;
+        }
+        return isValid;
+    }
+    
+    public static boolean isValidMathForm( int mathForm ) {
+        boolean isValid = false;
+        switch ( mathForm ) {
+            case MATH_FORM_WAVE_NUMBER:
+            case MATH_FORM_WAVELENGTH:
+            case MATH_FORM_MODE:
+            case MATH_FORM_ANGULAR_FREQUENCY:
+            case MATH_FORM_FREQUENCY:
+            case MATH_FORM_PERIOD:
+            case MATH_FORM_WAVE_NUMBER_AND_ANGULAR_FREQUENCY:
+            case MATH_FORM_WAVELENGTH_AND_PERIOD:
+                 isValid = true;
+                 break;
+            default:
+                 isValid = false;
+        }
+        return isValid;
+    }
 }
