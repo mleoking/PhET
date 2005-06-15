c332104674fa6d1572dd545c164c1a22e7e0bfd2
nobody <nobody@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-06-15 23:43:29 +0000

    This commit was manufactured by cvs2svn to create tag 'ideal-gas-v3-r23-6-15-05'.

---
+++ tags/ideal-gas-v3-r23-6-15-05/ideal-gas-v3/src/edu/colorado/phet/idealgas/model/Force.java
@@ -0,0 +1,13 @@
+/**
+ * Class: Force
+ * Package: edu.colorado.phet.idealgas.model
+ * Author: Another Guy
+ * Date: Jul 22, 2004
+ */
+package edu.colorado.phet.idealgas.model;
+
+import edu.colorado.phet.common.model.Particle;
+
+public interface Force {
+    public void act( Particle p );
+}
