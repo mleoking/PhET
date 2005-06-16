9b36744a89054fc88e2da9ccd34103dd6a176aed
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-06-16 09:32:21 +0000

    more readable 1d example

--- trunk/schrodinger/src/edu/colorado/phet/qm/model/RichardsonPropagator.java
+++ trunk/schrodinger/src/edu/colorado/phet/qm/model/RichardsonPropagator.java
@@ -1,7 +1,5 @@
 package edu.colorado.phet.qm.model;
 
-import edu.colorado.phet.qm.model.operators.ProbabilityValue;
-
 
 /*********************************************************/
 /* Two-dimensional Time dependent Schrodinger Equation.  */
@@ -35,14 +33,17 @@ public class RichardsonPropagator implements Propagator {
         simulationTime = 0.0;
         timeStep = 0;
         hbar = 1;
-        mass = 50;
+        mass = 0.0020;
 
         double dx = 1.0;
         dt = 0.8 * mass * dx * dx / hbar;
         epsilon = hbar * dt / ( mass * dx * dx );
 
-        alpha = new Complex( ( 1 + Math.cos( epsilon ) ) / 2.0, -Math.sin( epsilon ) / 2.0 );
-        beta = new Complex( ( 1 - Math.cos( epsilon ) ) / 2.0, Math.sin( epsilon ) / 2.0 );
+//        alpha = new Complex( ( 1 + Math.cos( epsilon ) ) / 2.0, -Math.sin( epsilon ) / 2.0 );
+//        beta = new Complex( ( 1 - Math.cos( epsilon ) ) / 2.0, Math.sin( epsilon ) / 2.0 );
+
+        alpha = new Complex( 0.5 + 0.5 * Math.cos( epsilon / 2 ), -0.5 * Math.sin( epsilon / 2 ) );
+        beta = new Complex( ( Math.sin( epsilon / 4 ) ) * Math.sin( epsilon / 4 ), 0.5 * Math.sin( epsilon / 2 ) );
         System.out.println( "dt = " + dt );
         System.out.println( "epsilon = " + epsilon );
         System.out.println( "alpha = " + alpha );
@@ -51,8 +52,8 @@ public class RichardsonPropagator implements Propagator {
 
     public void propagate( Complex[][] w ) {
         int nx = w.length;
-        double norm = new ProbabilityValue().compute( w );
-        System.out.println( "pre: norm = " + norm );
+//        double norm = new ProbabilityValue().compute( w );
+//        System.out.println( "pre: norm = " + norm );
         betaeven = new Complex[nx][nx];
         betaodd = new Complex[nx][nx];
         for( int i = 0; i < betaeven.length; i++ ) {
@@ -68,17 +69,23 @@ public class RichardsonPropagator implements Propagator {
             }
         }
         prop2D( w );
-        norm = new ProbabilityValue().compute( w );
-        System.out.println( "post: norm = " + norm );
+//        norm = new ProbabilityValue().compute( w );
+//        System.out.println( "post: norm = " + norm );
         simulationTime += deltaTime;
         timeStep++;
     }
 
     private void prop2D( Complex[][] w ) {
-        applyPotential( w );
+
+
+        stepIt( w, 0, -1 );
         stepIt( w, 1, 0 );
+        stepIt( w, 0, 1 );
+        stepIt( w, -1, 0 );
+        applyPotential( w );
         stepIt( w, -1, 0 );
         stepIt( w, 0, 1 );
+        stepIt( w, 1, 0 );
         stepIt( w, 0, -1 );
     }
 
@@ -87,8 +94,8 @@ public class RichardsonPropagator implements Propagator {
         for( int i = 1; i < w.length - 1; i++ ) {
             for( int j = 1; j < w[0].length - 1; j++ ) {
                 Complex alphaTerm = alpha.times( copy[i][j] );
-                Complex evenTerm = betaeven[i][j].times( copy[i - dx][j - dy] );
-                Complex oddTerm = betaodd[i][j].times( copy[i + dx][j + dy] );
+                Complex evenTerm = betaeven[i][j].times( copy[i + dx][j + dy] );
+                Complex oddTerm = betaodd[i][j].times( copy[i - dx][j - dy] );
                 w[i][j].setValue( alphaTerm.plus( evenTerm ).plus( oddTerm ) );
             }
         }
