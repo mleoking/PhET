a3d6be18451f69ff542fe5971dac588c5d0b25a6
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-06-23 15:27:22 +0000

    added timing code to display how long it takes

--- trunk/phetcommon/tests-src/edu/colorado/phet/common/tests/phetjcomponents/TestManyPhetJComponents.java
+++ trunk/phetcommon/tests-src/edu/colorado/phet/common/tests/phetjcomponents/TestManyPhetJComponents.java
@@ -6,6 +6,7 @@ import edu.colorado.phet.common.application.PhetApplication;
 import edu.colorado.phet.common.model.BaseModel;
 import edu.colorado.phet.common.model.clock.AbstractClock;
 import edu.colorado.phet.common.model.clock.SwingTimerClock;
+import edu.colorado.phet.common.util.QuickTimer;
 import edu.colorado.phet.common.view.ApparatusPanel2;
 import edu.colorado.phet.common.view.phetcomponents.PhetJComponent;
 import edu.colorado.phet.common.view.phetgraphics.PhetGraphic;
@@ -22,7 +23,7 @@ import java.util.Random;
  */
 
 public class TestManyPhetJComponents extends Module {
-    private int numComponents = 500;
+    private static final int numComponents = 500;
     private static Random random = new Random();
 
     /**
@@ -42,10 +43,28 @@ public class TestManyPhetJComponents extends Module {
     }
 
     public static void main( String[] args ) {
+        QuickTimer main = new QuickTimer();
         SwingTimerClock clock = new SwingTimerClock( 1, 30 );
         PhetApplication phetApplication = new PhetApplication( args, "title", "desc", "version", clock, true, new FrameSetup.CenteredWithSize( 600, 600 ) );
         TestManyPhetJComponents module = new TestManyPhetJComponents( "name", clock );
         phetApplication.setModules( new Module[]{module} );
         phetApplication.startApplication();
+        System.out.println( "time to make & display " + numComponents + ", phetJComponents= " + main + " ms" );
+
+        QuickTimer swing = new QuickTimer();
+        JFrame f = new JFrame();
+        JPanel conten = new JPanel();
+        conten.setLayout( null );
+        for( int i = 0; i < numComponents; i++ ) {
+            JButton but = new JButton( "button_" + i );
+
+            but.setLocation( random.nextInt( 400 ), random.nextInt( 400 ) );
+            but.reshape( but.getX(), but.getY(), but.getPreferredSize().width, but.getPreferredSize().height );
+            conten.add( but );
+        }
+        f.setContentPane( conten );
+        f.setSize( 600, 600 );
+        f.setVisible( true );
+        System.out.println( "time to make & display " + numComponents + ", normal JButtons= " + swing + " ms" );
     }
 }
