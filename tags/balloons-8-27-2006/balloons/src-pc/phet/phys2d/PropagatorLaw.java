80a9ce2b30409a8c6e4138190fe62b2d6af375c1
nobody <nobody@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-06-27 15:18:29 +0000

    This commit was manufactured by cvs2svn to create tag 'balloons-8-27-2006'.

---
+++ tags/balloons-8-27-2006/balloons/src-pc/phet/phys2d/PropagatorLaw.java
@@ -0,0 +1,19 @@
+package phet.phys2d;
+
+public class PropagatorLaw implements Law {
+    Propagator p;
+
+    public PropagatorLaw( Propagator p ) {
+        this.p = p;
+    }
+
+    public Propagator getPropagator() {
+        return p;
+    }
+
+    public void iterate( double dt, System2D sys ) {
+        for( int i = 0; i < sys.numParticles(); i++ ) {
+            p.propagate( dt, sys.particleAt( i ) );
+        }
+    }
+}
