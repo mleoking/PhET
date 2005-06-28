c3a1904cef84b6b9ee0d1c2ab7afa20b3ee08001
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-06-28 23:09:58 +0000

    remove constants that were migrated to MathStrings

--- trunk/fourier/src/edu/colorado/phet/fourier/view/HarmonicsEquation.java
+++ trunk/fourier/src/edu/colorado/phet/fourier/view/HarmonicsEquation.java
@@ -37,32 +37,6 @@ public class HarmonicsEquation extends HTMLGraphic {
     private static final Font DEFAULT_FONT = new Font( FourierConfig.FONT_NAME, Font.PLAIN, 20 );
     private static final Color DEFAULT_COLOR = Color.BLACK;
     
-    // Math forms for "space" domain
-    private static final String TEXT_SPACE_WAVE_NUMBER = 
-        "<html>A<sub>n</sub> sin( k<sub>n</sub>x )</html>";
-    private static final String TEXT_SPACE_WAVELENGTH = 
-        "<html>A<sub>n</sub> sin( 2\u03c0x / \u03BB<sub>n</sub> )</html>";
-    private static final String TEXT_SPACE_MODE = 
-        "<html>A<sub>n</sub> sin( 2\u03c0nx / L )</html>";
-    
-    // Math forms for "time" domain
-    private static final String TEXT_TIME_ANGULAR_FREQUENCY = 
-        "<html>A<sub>n</sub> sin( \u03C9<sub>n</sub>t )</html>";
-    private static final String TEXT_TIME_FREQUENCY = 
-        "<html>A<sub>n</sub> sin( 2\u03c0f<sub>n</sub>t )</html>";
-    private static final String TEXT_TIME_PERIOD = 
-        "<html>A<sub>n</sub> sin( 2\u03c0t / T<sub>n</sub> )</html>";
-    private static final String TEXT_TIME_MODE = 
-        "<html>A<sub>n</sub> sin( 2\u03c0nt / T )</html>";
-    
-    // Math forms for "space & time" domain
-    private static final String TEXT_SPACE_AND_TIME_WAVENUMBER_AND_ANGULAR_FREQUENCY = 
-        "<html>A<sub>n</sub> sin( k<sub>n</sub>x - \u03C9<sub>n</sub>t )</html>";
-    private static final String TEXT_SPACE_AND_TIME_WAVELENGTH_AND_PERIOD =
-        "<html>A<sub>n</sub> sin( 2\u03c0( x/\u03BB<sub>n</sub> - t/T<sub>n</sub> ) )</html>";
-    private static final String TEXT_SPACE_AND_TIME_MODE = 
-        "<html>A<sub>n</sub> sin( 2\u03c0n( x/L - t/T ) )</html>";
-    
     //----------------------------------------------------------------------------
     // Constructors
     //----------------------------------------------------------------------------
