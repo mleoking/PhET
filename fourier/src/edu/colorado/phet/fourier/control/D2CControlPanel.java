a621e4fcb1f73114632d33b4703db1647609ea3b
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-07-14 19:32:04 +0000

    add preliminary labels to sliders in D2C control panel

--- fourier/src/edu/colorado/phet/fourier/control/D2CControlPanel.java
+++ fourier/src/edu/colorado/phet/fourier/control/D2CControlPanel.java
@@ -16,17 +16,20 @@ import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.util.ArrayList;
+import java.util.Hashtable;
 
-import javax.swing.Box;
-import javax.swing.BoxLayout;
 import javax.swing.JCheckBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.border.TitledBorder;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
 import edu.colorado.phet.common.view.util.SimStrings;
 import edu.colorado.phet.fourier.FourierConstants;
+import edu.colorado.phet.fourier.MathStrings;
 import edu.colorado.phet.fourier.module.FourierModule;
+import edu.colorado.phet.fourier.util.EasyGridBagLayout;
 
 
 
@@ -43,6 +46,13 @@ public class D2CControlPanel extends FourierControlPanel {
     //----------------------------------------------------------------------------
     
     private static final int TITLED_BORDER_WIDTH = 1;
+    
+    private static final int MIN_SPACING = 0;
+    private static final int MAX_SPACING = 100;
+    private static final int MIN_K_WIDTH = 0;
+    private static final int MAX_K_WIDTH = 100;
+    private static final int MIN_X_WIDTH = 0;
+    private static final int MAX_X_WIDTH = 100;
     
     //----------------------------------------------------------------------------
     // Instance data
@@ -80,64 +90,132 @@ public class D2CControlPanel extends FourierControlPanel {
         int width = Integer.parseInt( widthString );
         setMinumumWidth( width );
 
-        // Domain
+        JPanel miscPanel = new JPanel();
         {
-            // Label
-            String label = SimStrings.get( "D2CControlPanel.domain" );
+            // Domain
+            {
+                // Label
+                String label = SimStrings.get( "D2CControlPanel.domain" );
 
-            // Choices
-            _domainChoices = new ArrayList();
-            _domainChoices.add( new FourierComboBox.Choice( FourierConstants.DOMAIN_SPACE, SimStrings.get( "domain.space" ) ) );
-            _domainChoices.add( new FourierComboBox.Choice( FourierConstants.DOMAIN_TIME, SimStrings.get( "domain.time" ) ) );
-            _domainChoices.add( new FourierComboBox.Choice( FourierConstants.DOMAIN_SPACE_AND_TIME, SimStrings.get( "domain.spaceAndTime" ) ) );
+                // Choices
+                _domainChoices = new ArrayList();
+                _domainChoices.add( new FourierComboBox.Choice( FourierConstants.DOMAIN_SPACE, SimStrings.get( "domain.space" ) ) );
+                _domainChoices.add( new FourierComboBox.Choice( FourierConstants.DOMAIN_TIME, SimStrings.get( "domain.time" ) ) );
+ 
+                // Function combo box
+                _domainComboBox = new FourierComboBox( label, _domainChoices );
+            }
 
-            // Function combo box
-            _domainComboBox = new FourierComboBox( label, _domainChoices );
-        }
-        
-        // Spacing
-        {
-            String format = SimStrings.get( "D2CControlPanel.spacing" );
-            _spacingSlider = new FourierSlider( format );
-        }
-        
-        // Continuous checkbox
-        _continuousCheckBox = new JCheckBox( SimStrings.get( "D2CControlPanel.continuous" ) );
-        
-        // k-space width
-        {
-            String format = SimStrings.get( "D2CControlPanel.kWidth" );
-            _kWidthSlider = new FourierSlider( format );
-        }
-        
-        // x-space width
-        {
-            String format = SimStrings.get( "D2CControlPanel.xWidth" );
-            _xWidthSlider = new FourierSlider( format ); 
-        }
-        
-        // Wave Type
-        {
-            // Label
-            String label = SimStrings.get( "D2CControlPanel.waveType" );
+            // Wave Type
+            {
+                // Label
+                String label = SimStrings.get( "D2CControlPanel.waveType" );
+
+                // Choices
+                _waveTypeChoices = new ArrayList();
+                _waveTypeChoices.add( new FourierComboBox.Choice( FourierConstants.WAVE_TYPE_SINE, SimStrings.get( "waveType.sines" ) ) );
+                _waveTypeChoices.add( new FourierComboBox.Choice( FourierConstants.WAVE_TYPE_COSINE, SimStrings.get( "waveType.cosines" ) ) );
+
+                // Wave Type combo box
+                _waveTypeComboBox = new FourierComboBox( label, _waveTypeChoices );
+            }
             
-            // Choices
-            _waveTypeChoices = new ArrayList();
-            _waveTypeChoices.add( new FourierComboBox.Choice( FourierConstants.WAVE_TYPE_SINE, SimStrings.get( "waveType.sines" ) ) );
-            _waveTypeChoices.add( new FourierComboBox.Choice( FourierConstants.WAVE_TYPE_COSINE, SimStrings.get( "waveType.cosines" ) ) );
-            
-            // Wave Type combo box
-            _waveTypeComboBox = new FourierComboBox( label, _waveTypeChoices ); 
+            // Layout
+            EasyGridBagLayout layout = new EasyGridBagLayout( miscPanel );
+            miscPanel.setLayout( layout );
+            int row = 0;
+            layout.addComponent( _domainComboBox, row++, 0 );
+            layout.addComponent( _waveTypeComboBox, row++, 0 );
         }
+        
+        JPanel harmonicsPanel = new JPanel();
+        {
+            String title = SimStrings.get( "D2CControlPanel.harmonics" );
+            harmonicsPanel.setBorder( new TitledBorder( title ) );
+            
+            // Spacing
+            {
+                String format = SimStrings.get( "D2CControlPanel.spacing" );
+                _spacingSlider = new FourierSlider( format );
+                _spacingSlider.setMinimum( MIN_SPACING );
+                _spacingSlider.setMaximum( MAX_SPACING );
+                
+                Hashtable labelTable = new Hashtable();
+                labelTable.put( new Integer( 0 ), new JLabel( "0" ) );
+                labelTable.put( new Integer( 20 ), new JLabel( MathStrings.C_PI + "/8" ) );
+                labelTable.put( new Integer( 40 ), new JLabel( MathStrings.C_PI + "/4" ) );
+                labelTable.put( new Integer( 60 ), new JLabel( MathStrings.C_PI + "/2" ) );
+                labelTable.put( new Integer( 80 ), new JLabel( "" + MathStrings.C_PI ) );
+                labelTable.put( new Integer( 100 ), new JLabel( "2" + MathStrings.C_PI ) );
+                _spacingSlider.setLabelTable( labelTable );
+                _spacingSlider.setMajorTickSpacing( 20 );
+            }
+
+            // Continuous checkbox
+            _continuousCheckBox = new JCheckBox( SimStrings.get( "D2CControlPanel.continuous" ) );
+            
+            // Layout
+            EasyGridBagLayout layout = new EasyGridBagLayout( harmonicsPanel );
+            harmonicsPanel.setLayout( layout );
+            int row = 0;
+            layout.addComponent( _spacingSlider, row++, 0 );
+            layout.addComponent( _continuousCheckBox, row++, 0 );
+        }
+        
+        // Packet width panel
+        JPanel packetWidthPanel = new JPanel();
+        {
+            String title = SimStrings.get( "D2CControlPanel.packetWidth" );
+            packetWidthPanel.setBorder( new TitledBorder( title ) );
+            
+            // k-space width
+            {
+                String format = SimStrings.get( "D2CControlPanel.kWidth" );
+                _kWidthSlider = new FourierSlider( format );
+                _kWidthSlider.setMinimum( MIN_K_WIDTH );
+                _kWidthSlider.setMaximum( MAX_K_WIDTH );
+                
+                Hashtable labelTable = new Hashtable();
+                labelTable.put( new Integer( 0 ), new JLabel( "1" ) );
+                labelTable.put( new Integer( 20 ), new JLabel( "" + MathStrings.C_PI ) );
+                labelTable.put( new Integer( 40 ), new JLabel( "2" + MathStrings.C_PI ) );
+                labelTable.put( new Integer( 60 ), new JLabel( "3" + MathStrings.C_PI ) );
+                labelTable.put( new Integer( 80 ), new JLabel( "4" + MathStrings.C_PI ) );
+                labelTable.put( new Integer( 100 ), new JLabel( "5" + MathStrings.C_PI ) );
+                _kWidthSlider.setLabelTable( labelTable );
+                _kWidthSlider.setMajorTickSpacing( 20 );
+            }
+
+            // x-space width
+            {
+                String format = SimStrings.get( "D2CControlPanel.xWidth" );
+                _xWidthSlider = new FourierSlider( format );
+                _xWidthSlider.setMinimum( MIN_X_WIDTH );
+                _xWidthSlider.setMaximum( MAX_X_WIDTH );
+                
+                Hashtable labelTable = new Hashtable();
+                labelTable.put( new Integer( 0 ), new JLabel( MathStrings.C_PI + "/5" ) );
+                labelTable.put( new Integer( 100 ), new JLabel( "1" ) );
+                _xWidthSlider.setLabelTable( labelTable );
+                _xWidthSlider.setMajorTickSpacing( 20 );
+            }
+            
+            // Layout
+            EasyGridBagLayout layout = new EasyGridBagLayout( packetWidthPanel );
+            packetWidthPanel.setLayout( layout );
+            int row = 0;
+            layout.addComponent( _kWidthSlider, row++, 0 );
+            layout.addComponent( _xWidthSlider, row++, 0 );
+        }
+
 
         // Layout
-        addFullWidth( _domainComboBox );
-        addFullWidth( _continuousCheckBox );
-        addFullWidth( _spacingSlider );
-        addFullWidth( _kWidthSlider );
-        addFullWidth( _xWidthSlider );
-        addFullWidth( _waveTypeComboBox );
-        
+        addFullWidth( miscPanel );
+        addVerticalSpace( 15 );
+        addFullWidth( harmonicsPanel );
+        addVerticalSpace( 15 );
+        addFullWidth( packetWidthPanel );
+  
         // Set the state of the controls.
         reset();
         
