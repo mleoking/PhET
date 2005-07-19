faca7ebd6c3fa57b3e58f8d2f5248bda5e4e18ce
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-07-19 21:44:01 +0000

    disable File>Load and File>Save menu items, not yet implemented

--- trunk/fourier/src/edu/colorado/phet/fourier/FourierApplication.java
+++ trunk/fourier/src/edu/colorado/phet/fourier/FourierApplication.java
@@ -78,15 +78,15 @@ public class FourierApplication extends PhetApplication {
         
         // File menu
         {
-            JMenuItem saveItem = new JMenuItem( SimStrings.get( "FileMenu.save" ) );
-            saveItem.setMnemonic( SimStrings.get( "FileMenu.save.mnemonic" ).charAt(0) );
-            
-            JMenuItem loadItem = new JMenuItem( SimStrings.get( "FileMenu.load" ) );
-            loadItem.setMnemonic( SimStrings.get( "FileMenu.load.mnemonic" ).charAt(0) );
-
-            frame.addFileMenuItem( saveItem );
-            frame.addFileMenuItem( loadItem );
-            frame.addFileMenuSeparator();
+//            JMenuItem saveItem = new JMenuItem( SimStrings.get( "FileMenu.save" ) );
+//            saveItem.setMnemonic( SimStrings.get( "FileMenu.save.mnemonic" ).charAt(0) );
+//            
+//            JMenuItem loadItem = new JMenuItem( SimStrings.get( "FileMenu.load" ) );
+//            loadItem.setMnemonic( SimStrings.get( "FileMenu.load.mnemonic" ).charAt(0) );
+//
+//            frame.addFileMenuItem( saveItem );
+//            frame.addFileMenuItem( loadItem );
+//            frame.addFileMenuSeparator();
         }
         
         // Options menu
