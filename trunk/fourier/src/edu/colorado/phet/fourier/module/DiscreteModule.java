66d9a193ab29106866424d4e8fded7366c32b401
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-07-20 22:10:56 +0000

    redo layout of SumGraph so that origin is at top-left corner

--- trunk/fourier/src/edu/colorado/phet/fourier/module/DiscreteModule.java
+++ trunk/fourier/src/edu/colorado/phet/fourier/module/DiscreteModule.java
@@ -53,7 +53,7 @@ public class DiscreteModule extends FourierModule {
     // Locations
     private static final Point AMPLITUDES_LOCATION = new Point( 0, 0 );
     private static final Point HARMONICS_LOCATION = new Point( 0, 195 );
-    private static final Point SUM_LOCATION = new Point( 60, 510 );
+    private static final Point SUM_LOCATION = new Point( 0, 195 + 200 );
     private static final Point WAVELENGTH_TOOL_LOCATION = new Point( 590, 230 );
     private static final Point PERIOD_TOOL_LOCATION = WAVELENGTH_TOOL_LOCATION;
     private static final Point PERIOD_DISPLAY_LOCATION = new Point( 655, 345 );
