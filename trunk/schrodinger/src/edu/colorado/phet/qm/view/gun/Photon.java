9cd61dfc6cb5282fd65f9ff9879a813622f78afa
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-07-27 21:58:50 +0000

    schrodinger fixes

--- trunk/schrodinger/src/edu/colorado/phet/qm/view/gun/Photon.java
+++ trunk/schrodinger/src/edu/colorado/phet/qm/view/gun/Photon.java
@@ -35,7 +35,8 @@ public class Photon extends GunParticle {
         getGunGraphic().getSchrodingerPanel().setDisplayPhotonColor( this );
         abstractGun.getSchrodingerModule().getDiscreteModel().setPropagatorClassical();
         abstractGun.addGraphic( wavelengthSliderGraphic );
-        wavelengthSliderGraphic.setLocation( -wavelengthSliderGraphic.getWidth() - 2, abstractGun.getControlOffsetY() + abstractGun.getComboBox().getPreferredSize().height + 2 );
+        wavelengthSliderGraphic.setLocation( -wavelengthSliderGraphic.getWidth() - 2,
+                                             abstractGun.getControlOffsetY() + abstractGun.getComboBox().getPreferredSize().height + 2 + 20 );
     }
 
     public void deactivate( AbstractGun abstractGun ) {
