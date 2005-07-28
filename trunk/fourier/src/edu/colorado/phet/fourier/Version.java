286c01fefe1bfa0cc508846552b6c157aed8e7b1
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-07-28 16:37:33 +0000

    (7/29) - move all version information to Version class (7/29) - remove build tag from version in FourierApplication, will need to get it in there some other way

---
+++ trunk/fourier/src/edu/colorado/phet/fourier/Version.java
@@ -0,0 +1,28 @@
+/* Copyright 2005, University of Colorado */
+
+/*
+ * CVS Info -
+ * Filename : $Source$
+ * Branch : $Name$
+ * Modified by : $Author$
+ * Revision : $Revision$
+ * Date modified : $Date$
+ */
+
+package edu.colorado.phet.fourier;
+
+
+/**
+ * Version
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ * @version $Revision$
+ */
+public class Version {
+  
+    public static final String NUMBER = "1.0";
+    public static final String BUILD = "fourier-v1r0-2005-07-27";
+    
+    /* This class is not intended for instantiation. */
+    private Version() {}
+}
