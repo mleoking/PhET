e3a1674180447051b0361447e52cc9e52d2f0e2a
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-08-12 19:17:13 +0000

    *** empty log message ***

--- trunk/fourier/src/edu/colorado/phet/fourier/Version.java
+++ trunk/fourier/src/edu/colorado/phet/fourier/Version.java
@@ -21,7 +21,7 @@ package edu.colorado.phet.fourier;
 public class Version {
   
     public static final String NUMBER = "1.0";
-    public static final String BUILD = "fourier-sandbox-2005-08-11";
+    public static final String BUILD = "fourier-sandbox-2005-08-12";
     
     /* This class is not intended for instantiation. */
     private Version() {}
