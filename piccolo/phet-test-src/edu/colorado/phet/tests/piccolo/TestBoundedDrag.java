b9aa726211a65e4f83a6561dfe1339bbb4e53ced
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-08-27 03:27:32 +0000

    added test

---
+++ piccolo/phet-test-src/edu/colorado/phet/tests/piccolo/TestBoundedDrag.java
@@ -0,0 +1,15 @@
+/* Copyright 2004, Sam Reid */
+package edu.colorado.phet.tests.piccolo;
+
+/**
+ * User: Sam Reid
+ * Date: Aug 26, 2005
+ * Time: 9:16:29 PM
+ * Copyright (c) Aug 26, 2005 by Sam Reid
+ */
+
+public class TestBoundedDrag {
+    public static void main( String[] args ) {
+        
+    }
+}
