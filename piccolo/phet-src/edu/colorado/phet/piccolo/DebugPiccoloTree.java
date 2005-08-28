2b519a20617f487e0e96b740eccf70c2de6600c2
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-08-28 02:46:34 +0000

    tidied up demo code

--- piccolo/phet-src/edu/colorado/phet/piccolo/DebugPiccoloTree.java
+++ piccolo/phet-src/edu/colorado/phet/piccolo/DebugPiccoloTree.java
@@ -1,37 +1,44 @@
 /* Copyright 2004, Sam Reid */
 package edu.colorado.phet.piccolo;
 
-import edu.umd.cs.piccolo.PCanvas;
 import edu.umd.cs.piccolo.PNode;
-import edu.umd.cs.piccolo.nodes.PPath;
 import edu.umd.cs.piccolo.nodes.PText;
-
-import java.awt.geom.Rectangle2D;
 
 /**
  * Prints the piccolo tree.
  */
 
 public class DebugPiccoloTree {
-    private PCanvas pCanvas;
-
     public DebugPiccoloTree() {
-        pCanvas = new PCanvas();
-        pCanvas.getLayer().addChild( new PText( "Hello" ) );
-        PPath child = new PPath( new Rectangle2D.Double( 0, 0, 10, 10 ) );
-        child.addChild( new PText( "Child of rect" ) );
-        pCanvas.getLayer().addChild( child );
     }
 
-    private void printTree( PNode root, String pre ) {
+    public void printTree( PNode root ) {
+        printTree( root, "" );
+    }
+
+    public void printTree( PNode root, String pre ) {
         System.out.print( pre );
-        String className = root.getClass().getName().substring( root.getClass().getName().lastIndexOf( '.' ) + 1 );
+
+        String className = getClassName( root );
         String additionalInfo = getAdditionalInfo( root );
-        System.out.println( className + " " + additionalInfo );
-        //todo fails for unpackaged nodes.
+        printAll( className, additionalInfo );
         for( int i = 0; i < root.getChildrenReference().size(); i++ ) {
             PNode child = (PNode)root.getChildrenReference().get( i );
             printTree( child, pre + "\t" );
+        }
+    }
+
+    private void printAll( String className, String additionalInfo ) {
+        String total = ( className + " " + additionalInfo );
+        System.out.println( total );
+    }
+
+    private String getClassName( PNode root ) {
+        if( root.getClass().getName().indexOf( '.' ) >= 0 ) {
+            return root.getClass().getName().substring( root.getClass().getName().lastIndexOf( '.' ) + 1 );
+        }
+        else {
+            return root.getClass().getName();
         }
     }
 
@@ -45,20 +52,4 @@ public class DebugPiccoloTree {
         return "";
     }
 
-    public static void main( String[] args ) {
-        new DebugPiccoloTree().start();
-    }
-
-    private void start() {
-        PNode trueRoot = getTrueRoot();
-        printTree( trueRoot, "" );
-    }
-
-    private PNode getTrueRoot() {
-        PNode trueRoot = pCanvas.getRoot();
-        while( trueRoot.getParent() != null ) {
-            trueRoot = trueRoot.getParent();
-        }
-        return trueRoot;
-    }
 }
