8de9cbebd8ee604f2841ebba3044c93faa5c8713
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-09-01 22:50:06 +0000

    no message

--- trunk/fluorescent-lights/src/edu/colorado/phet/dischargelamps/view/DischargeLampEnergyMonitorPanel2.java
+++ trunk/fluorescent-lights/src/edu/colorado/phet/dischargelamps/view/DischargeLampEnergyMonitorPanel2.java
@@ -19,6 +19,7 @@ import edu.colorado.phet.dischargelamps.DischargeLampModule;
 import edu.colorado.phet.dischargelamps.model.Electron;
 import edu.colorado.phet.dischargelamps.model.AtomicStateFactory;
 import edu.colorado.phet.dischargelamps.model.DischargeLampModel;
+import edu.colorado.phet.dischargelamps.model.DefaultElementProperties;
 
 import javax.swing.*;
 import javax.swing.event.ChangeListener;
@@ -28,7 +29,7 @@ import java.awt.*;
 
 /**
  * DischargeLampEnergyMonitorPanel2
- * <p>
+ * <p/>
  * Wrapper for EnergyLevelMonitorPanel that adds a spinner to set the number of energy levels.
  *
  * @author Ron LeMaster
@@ -37,9 +38,9 @@ import java.awt.*;
 public class DischargeLampEnergyMonitorPanel2 extends JPanel {
     private DischargeLampEnergyLevelMonitorPanel elmp;
     private DischargeLampModel model;
+    private JSpinner numLevelsSpinner;
 
     /**
-     *
      * @param model
      * @param clock
      * @param atomicStates
@@ -51,6 +52,7 @@ public class DischargeLampEnergyMonitorPanel2 extends JPanel {
                                              int panelWidth, int panelHeight ) {
         super( new GridBagLayout() );
         this.model = model;
+        model.addChangeListener( new SpinnerManager() );
         elmp = new DischargeLampEnergyLevelMonitorPanel( model, clock, atomicStates, panelWidth, panelHeight );
         elmp.setBorder( new EtchedBorder() );
         GridBagConstraints gbc = new GridBagConstraints( 0, 0, 1, 1, 0, 0,
@@ -60,19 +62,18 @@ public class DischargeLampEnergyMonitorPanel2 extends JPanel {
         this.add( elmp, gbc );
 
         // Add the spinner that controls the number of energy levels
-        final JSpinner numLevelsSpinner = new JSpinner( new SpinnerNumberModel( DischargeLampsConfig.NUM_ENERGY_LEVELS, 2,
-                                                                                DischargeLampsConfig.MAX_NUM_ENERGY_LEVELS,
-                                                                                1 ) );
+        numLevelsSpinner = new JSpinner( new SpinnerNumberModel( DischargeLampsConfig.NUM_ENERGY_LEVELS, 2,
+                                                                 DischargeLampsConfig.MAX_NUM_ENERGY_LEVELS,
+                                                                 1 ) );
 
         // Add a listener that will create the number of atomic states specified by the spinner, and apply them
         // to all the existing atoms
         numLevelsSpinner.addChangeListener( new ChangeListener() {
             public void stateChanged( ChangeEvent e ) {
-                model.setNumAtomicEnergyLevels( ((Integer)numLevelsSpinner.getValue() ).intValue() );
+                model.setNumAtomicEnergyLevels( ( (Integer)numLevelsSpinner.getValue() ).intValue() );
             }
         } );
-//        AtomicState[] defaultAtomicStates = new AtomicStateFactory().createAtomicStates( ( (Integer)numLevelsSpinner.getValue() ).intValue() );
-//        elmp.setEnergyLevels( defaultAtomicStates );
+        setSpinnerVisibility( model );
         gbc.gridy = 1;
         gbc.anchor = GridBagConstraints.CENTER;
         this.add( numLevelsSpinner, gbc );
@@ -100,5 +101,24 @@ public class DischargeLampEnergyMonitorPanel2 extends JPanel {
 
     public DischargeLampEnergyLevelMonitorPanel getElmp() {
         return elmp;
+    }
+
+    private void setSpinnerVisibility( DischargeLampModel model ) {
+        if( model.getElementProperties() instanceof DefaultElementProperties ) {
+            numLevelsSpinner.setVisible( true );
+        }
+        else {
+            numLevelsSpinner.setVisible( false );
+        }
+    }
+
+    //----------------------------------------------------------------
+    // Inner classes
+    //----------------------------------------------------------------
+
+    private class SpinnerManager extends DischargeLampModel.ChangeListenerAdapter {
+        public void energyLevelsChanged( DischargeLampModel.ChangeEvent event ) {
+            setSpinnerVisibility( event.getDischargeLampModelDischargeLampModel() );
+        }
     }
 }
