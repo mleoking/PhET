6be5db3a15605a2221c3a89e983ebbe84f714d7c
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-09-13 07:59:49 +0000

    removed knowledge of cameras for the transform

--- piccolo/phet-src/edu/colorado/phet/piccolo/ConnectorGraphic.java
+++ piccolo/phet-src/edu/colorado/phet/piccolo/ConnectorGraphic.java
@@ -30,45 +30,24 @@ public class ConnectorGraphic extends PPath {
         src.addPropertyChangeListener( PNode.PROPERTY_FULL_BOUNDS, changeHandler );
         dst.addPropertyChangeListener( PNode.PROPERTY_FULL_BOUNDS, changeHandler );
         update();
-//        setPaint( Color.black );
         setStrokePaint( Color.black );
     }
 
     private void update() {
         connectRectsWithLine();
     }
-    // This method connects the centers of two
-// rectangle nodes with a line node. If you know that two nodes
-// exist in the same coordinate system then you don't need to make
-// all these conversions. This example assumes the most general case where
-// they all exist in different coordinate systems.
 
     public void connectRectsWithLine() {
-
-        // First get the center of each rectangle in the
-        // local coordinate system of each rectangle.
         if( source == null || source.getFullBounds() == null ||
             destination == null || destination.getFullBounds() == null ||
             source.getParent() == null || destination.getParent() == null ) {
             return;
         }
-        Point2D r1c = source.getFullBounds().getCenter2D();
-        Point2D r2c = destination.getFullBounds().getCenter2D();
 
-        // Next convert that center point for each rectangle
-        // into global coordinate system.
-        source.getParent().localToGlobal( r1c );
-        destination.getParent().localToGlobal( r2c );
-
-        // Now that the centers are in global coordinates they
-        // can be converted into the local coordinate system
-        // of the line node.
+        Point2D r1c = source.getGlobalFullBounds().getCenter2D();
+        Point2D r2c = destination.getGlobalFullBounds().getCenter2D();
         globalToLocal( r1c );
         globalToLocal( r2c );
-
-        // Finish by setting the endpoints of the line to
-        // the center points of the rectangles, now that those
-        // center points are in the local coordinate system of the line.
         updateShape( r1c, r2c );
         repaint();
     }
