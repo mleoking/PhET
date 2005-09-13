7ce2c38570c71d73b91e2220f7c1118827bcdb05
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-09-13 00:11:30 +0000

    1.0.12

--- trunk/fourier/src/edu/colorado/phet/fourier/Version.java
+++ trunk/fourier/src/edu/colorado/phet/fourier/Version.java
@@ -20,8 +20,8 @@ package edu.colorado.phet.fourier;
  */
 public class Version {
   
-    public static final String NUMBER = "1.0.11";
-    public static final String BUILD = "fourier-1-0-11-2005-09-09";
+    public static final String NUMBER = "1.0.12";
+    public static final String BUILD = "fourier-1-0-12-2005-09-12";
     
     /* This class is not intended for instantiation. */
     private Version() {}
