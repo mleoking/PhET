98c019b2d6292bc6ead690f5d887d6863a012017
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-09-22 20:11:16 +0000

    (9/22) - make border on FourierTitledPanel 10% darker (9/22) - override FourierTitledPanel.paintComponent to enable anti-aliasing of title borders on Windows

--- fourier/src/edu/colorado/phet/fourier/control/FourierTitledPanel.java
+++ fourier/src/edu/colorado/phet/fourier/control/FourierTitledPanel.java
@@ -11,8 +11,11 @@
 
 package edu.colorado.phet.fourier.control;
 
+import java.awt.*;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
@@ -31,12 +34,26 @@ import edu.colorado.phet.fourier.FourierConfig;
 public class FourierTitledPanel extends JPanel {
 
     private static final Font TITLE_FONT = new Font( FourierConfig.FONT_NAME, Font.ITALIC, 13 );
+    private static final Color BORDER_COLOR = new Color( 175, 175, 175 );
     
     public FourierTitledPanel( String title ) {
         TitledBorder titledBorder = new TitledBorder( title );
         Font font = titledBorder.getTitleFont();
         titledBorder.setTitleFont( TITLE_FONT );
-        titledBorder.setBorder( BorderFactory.createLineBorder( Color.LIGHT_GRAY, 1 ) );
+        titledBorder.setBorder( BorderFactory.createLineBorder( BORDER_COLOR, 1 ) );
         setBorder( titledBorder );
+    }
+    
+    /**
+     * Windows doesn't anti-alias Swing components.
+     * Since we're using an italic font, it looks particularly ugly.
+     * So override paintComponent and enabled anti-aliasing.
+     */
+    public void paintComponent( Graphics g ) {
+        if ( g instanceof Graphics2D ) {
+            Graphics2D g2 = (Graphics2D) g;
+            g2.setRenderingHint( RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON );
+        }
+        super.paintComponent( g );
     }
 }
