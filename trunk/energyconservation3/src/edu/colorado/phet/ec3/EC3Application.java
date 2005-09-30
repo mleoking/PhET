f1a33aac50c55d0ea591752b1081693b6663c680
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-09-30 21:14:38 +0000

    added global key handling

--- trunk/energyconservation3/src/edu/colorado/phet/ec3/EC3Application.java
+++ trunk/energyconservation3/src/edu/colorado/phet/ec3/EC3Application.java
@@ -5,6 +5,9 @@ import edu.colorado.phet.common.application.Module;
 import edu.colorado.phet.common.application.PhetApplication;
 import edu.colorado.phet.common.model.clock.SwingTimerClock;
 
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
 /**
  * User: Sam Reid
  * Date: Sep 21, 2005
@@ -28,5 +31,33 @@ public class EC3Application extends PhetApplication {
     private void start() {
         super.startApplication();
         module.getPhetPCanvas().requestFocus();
+//        getPhetFrame().setGlassPane( new InterceptingGlassPane( module.getEnergyConservationCanvas() ) );
+        final EC3Canvas c = module.getEnergyConservationCanvas();
+        KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher( new KeyEventDispatcher() {
+            public boolean dispatchKeyEvent( KeyEvent e ) {
+                if( !c.hasFocus() ) {
+                    int id = e.getID();
+                    switch( id ) {
+                        case KeyEvent.KEY_PRESSED:
+                            System.out.println( "KEY_PRESSED" );
+                            c.keyPressed( e );
+                            break;
+                        case KeyEvent.KEY_RELEASED:
+                            System.out.println( "KEY_RELEASED" );
+                            c.keyReleased( e );
+                            break;
+                        case KeyEvent.KEY_TYPED:
+                            System.out.println( "KEY_TYPED" );
+                            c.keyTyped( e );
+                            break;
+                        default:
+                            System.out.println( "unknown type" );
+                            break;
+                    }
+
+                }
+                return false;
+            }
+        } );
     }
 }
