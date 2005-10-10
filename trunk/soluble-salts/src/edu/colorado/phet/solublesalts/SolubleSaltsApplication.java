3460052e119df23d9c21a92031edb254102f5b85
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-10-10 19:18:22 +0000

    no message

---
+++ trunk/soluble-salts/src/edu/colorado/phet/solublesalts/SolubleSaltsApplication.java
@@ -0,0 +1,42 @@
+/* Copyright 2003-2004, University of Colorado */
+
+/*
+ * CVS Info -
+ * Filename : $Source$
+ * Branch : $Name$
+ * Modified by : $Author$
+ * Revision : $Revision$
+ * Date modified : $Date$
+ */
+package edu.colorado.phet.solublesalts;
+
+import edu.colorado.phet.common.application.PhetApplication;
+import edu.colorado.phet.common.application.Module;
+import edu.colorado.phet.common.model.clock.AbstractClock;
+import edu.colorado.phet.common.model.clock.SwingTimerClock;
+import edu.colorado.phet.common.view.util.FrameSetup;
+import edu.colorado.phet.solublesalts.module.SolubleSaltsModule;
+
+/**
+ * SolubleSaltsApplication
+ *
+ * @author Ron LeMaster
+ * @version $Revision$
+ */
+public class SolubleSaltsApplication extends PhetApplication {
+
+    private static AbstractClock CLOCK = new SwingTimerClock( SolubleSaltsConfig.DT, SolubleSaltsConfig.FPS, AbstractClock.FRAMES_PER_SECOND );
+
+    public SolubleSaltsApplication( String[] args) {
+        super( args,
+               SolubleSaltsConfig.TITLE,
+               SolubleSaltsConfig.DESCRIPTION,
+               SolubleSaltsConfig.VERSION,
+               CLOCK,
+               true,
+               new FrameSetup.CenteredWithSize( 1000, 740 ) );
+
+        Module module = new SolubleSaltsModule( CLOCK );
+    }
+
+}
