2575d684ae67393e099021551c781f9d1ba35868
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-10-12 00:05:36 +0000

    no message

---
+++ trunk/soluble-salts/src/edu/colorado/phet/solublesalts/control/IonGraphicManager.java
@@ -0,0 +1,51 @@
+/* Copyright 2003-2004, University of Colorado */
+
+/*
+ * CVS Info -
+ * Filename : $Source$
+ * Branch : $Name$
+ * Modified by : $Author$
+ * Revision : $Revision$
+ * Date modified : $Date$
+ */
+package edu.colorado.phet.solublesalts.control;
+
+import edu.colorado.phet.solublesalts.model.SolubleSaltsModel;
+import edu.colorado.phet.solublesalts.model.Ion;
+import edu.colorado.phet.solublesalts.view.IonGraphic;
+import edu.colorado.phet.solublesalts.SolubleSaltsConfig;
+import edu.colorado.phet.piccolo.PhetPCanvas;
+import edu.umd.cs.piccolo.PNode;
+
+import java.util.HashMap;
+
+/**
+ * IonGraphicManager
+ * <p>
+ * Creates graphics for ions when they are added to a model and adds the graphics to a canvas,
+ * and removes the graphics from the canvas when the ions leave the model
+ *
+ * @author Ron LeMaster
+ * @version $Revision$
+ */
+public class IonGraphicManager implements SolubleSaltsModel.IonListener {
+
+    private PhetPCanvas graphicContainer;
+    private HashMap ionToGraphicMap = new HashMap();
+
+    public IonGraphicManager( PhetPCanvas graphicContainer ) {
+        this.graphicContainer = graphicContainer;
+    }
+
+    public void ionAdded( SolubleSaltsModel.IonEvent event ) {
+        IonGraphic ig = new IonGraphic( event.getIon(), SolubleSaltsConfig.BLUE_ION_IMAGE_NAME );
+        graphicContainer.addWorldChild( ig );
+        ionToGraphicMap.put( event.getIon(), ig );
+    }
+
+    public void ionRemoved( SolubleSaltsModel.IonEvent event ) {
+        IonGraphic ig = (IonGraphic)ionToGraphicMap.get( event.getIon() );
+        graphicContainer.removeWorldChild( ig );
+        ionToGraphicMap.remove( event.getIon() );
+    }
+}
--- trunk/soluble-salts/src/edu/colorado/phet/solublesalts/model/SolubleSaltsModel.java
+++ trunk/soluble-salts/src/edu/colorado/phet/solublesalts/model/SolubleSaltsModel.java
@@ -11,9 +11,13 @@
 package edu.colorado.phet.solublesalts.model;
 
 import edu.colorado.phet.common.model.BaseModel;
+import edu.colorado.phet.common.model.ModelElement;
+import edu.colorado.phet.common.util.EventChannel;
 
 import java.awt.geom.Point2D;
 import java.awt.*;
+import java.util.EventObject;
+import java.util.EventListener;
 
 /**
  * SolubleSaltsModel
@@ -32,8 +36,61 @@ public class SolubleSaltsModel extends BaseModel {
         vessel = new Vessel( vesselWidth, vesselDepth, vesselLoc );
     }
 
+    public void addModelElement( ModelElement modelElement ) {
+        super.addModelElement( modelElement );
+
+        if( modelElement instanceof Ion ) {
+            ionListenerProxy.ionAdded( new IonEvent( modelElement ) );
+        }
+    }
+
+    public void removeModelElement( ModelElement modelElement ) {
+        super.removeModelElement( modelElement );
+
+        if( modelElement instanceof Ion ) {
+            ionListenerProxy.ionRemoved( new IonEvent( modelElement ) );
+        }
+    }
+
     public Vessel getVessel() {
         return vessel;
     }
 
+    //----------------------------------------------------------------
+    // Events and listeners for Ions
+    //----------------------------------------------------------------
+
+    private EventChannel ionEventChannel = new EventChannel( IonListener.class );
+    private IonListener ionListenerProxy = (IonListener)ionEventChannel.getListenerProxy();
+
+    public void addIonListener( IonListener listener ) {
+        ionEventChannel.addListener( listener );
+    }
+
+    public void removeIonListener( IonListener listener ) {
+        ionEventChannel.removeListener( listener );
+    }
+
+    public class IonEvent extends EventObject {
+        public IonEvent( Object source ) {
+            super( source );
+            if( !( source instanceof Ion )) {
+                throw new RuntimeException( "source of wrong type");
+            }
+        }
+
+        public Ion getIon() {
+            return (Ion)getSource();
+        }
+    }
+
+    public interface IonListener extends EventListener {
+        void ionAdded( IonEvent event );
+        void ionRemoved( IonEvent event );
+    }
+
+    public static class IonListenerAdapter implements IonListener {
+        public void ionAdded( IonEvent event ) {}
+        public void ionRemoved( IonEvent event ) {}
+    }
 }
