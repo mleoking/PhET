b667767bcfef61a7ce983cccfc6154d8e327b302
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-10-13 19:29:33 +0000

    no message

--- soluble-salts/src/edu/colorado/phet/solublesalts/model/Ion.java
+++ soluble-salts/src/edu/colorado/phet/solublesalts/model/Ion.java
@@ -10,8 +10,8 @@
  */
 package edu.colorado.phet.solublesalts.model;
 
+import edu.colorado.phet.collision.SphericalBody;
 import edu.colorado.phet.common.math.Vector2D;
-import edu.colorado.phet.common.model.Particle;
 
 import java.awt.geom.Point2D;
 
@@ -21,20 +21,23 @@ import java.awt.geom.Point2D;
  * @author Ron LeMaster
  * @version $Revision$
  */
-public class Ion extends Particle {
+public class Ion extends SphericalBody {
 
-    private double charge;
+    private IonProperties ionProperties;
 
-    public Ion( double charge ) {
-        this.charge = charge;
+    public Ion( IonProperties ionProperties ) {
+        this( new Point2D.Double(),
+              new Vector2D.Double(),
+              new Vector2D.Double(),
+              ionProperties );
     }
 
-    public Ion( Point2D position, Vector2D velocity, Vector2D acceleration, double charge ) {
-        super( position, velocity, acceleration );
-        this.charge = charge;
+    public Ion( Point2D position, Vector2D velocity, Vector2D acceleration, IonProperties ionProperties ) {
+        super( position, velocity, acceleration, ionProperties.getMass(), ionProperties.getRadius() );
+        this.ionProperties = ionProperties;
     }
 
     public double getCharge() {
-        return charge;
+        return ionProperties.getCharge();
     }
 }
