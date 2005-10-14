e6071032befb953b22d1981856feda703f766ec0
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-10-14 16:24:23 +0000

    no message

--- soluble-salts/src/edu/colorado/phet/solublesalts/module/SolubleSaltsModule.java
+++ soluble-salts/src/edu/colorado/phet/solublesalts/module/SolubleSaltsModule.java
@@ -12,12 +12,13 @@ package edu.colorado.phet.solublesalts.module;
 
 import edu.colorado.phet.common.math.Vector2D;
 import edu.colorado.phet.common.model.clock.AbstractClock;
+import edu.colorado.phet.common.view.ControlPanel;
 import edu.colorado.phet.common.view.util.SimStrings;
 import edu.colorado.phet.piccolo.CursorHandler;
 import edu.colorado.phet.piccolo.PhetPCanvas;
 import edu.colorado.phet.piccolo.PiccoloModule;
-import edu.colorado.phet.solublesalts.control.IonGraphicManager;
 import edu.colorado.phet.solublesalts.model.*;
+import edu.colorado.phet.solublesalts.view.IonGraphicManager;
 import edu.colorado.phet.solublesalts.view.VesselGraphic;
 import edu.umd.cs.piccolo.event.PDragEventHandler;
 import edu.umd.cs.piccolo.nodes.PPath;
@@ -35,6 +36,7 @@ import java.util.Random;
 public class SolubleSaltsModule extends PiccoloModule {
 
     Random random = new Random( System.currentTimeMillis() );
+    private PhetPCanvas simPanel;
 
     public SolubleSaltsModule( AbstractClock clock ) {
         super( SimStrings.get( "Module.title" ), clock );
@@ -42,19 +44,15 @@ public class SolubleSaltsModule extends PiccoloModule {
         // Set up the basics
         final SolubleSaltsModel model = new SolubleSaltsModel();
         setModel( model );
-        final PhetPCanvas simPanel = new PhetPCanvas();
+        simPanel = new PhetPCanvas();
         setPhetPCanvas( simPanel );
 
-        // Add a graphic manager to the model that will manage IonGraphics
+        // Add a graphic manager to the model that will create and remove IonGraphics
+        // when Ions are added to and removed from the model
         model.addIonListener( new IonGraphicManager( simPanel ) );
 
-        // A test graphic
-        Rectangle r = new Rectangle( 100, 150, 20, 70 );
-        PPath pp = new PPath( r );
-        pp.setPaint( Color.red );
-        simPanel.addWorldChild( pp );
-        pp.addInputEventListener( new CursorHandler( Cursor.HAND_CURSOR ) );
-        pp.addInputEventListener( new PDragEventHandler() );
+        // Test code
+        test();
 
         // Create a graphic for the vessel
         VesselGraphic vesselGraphic = new VesselGraphic( model.getVessel() );
@@ -65,12 +63,6 @@ public class SolubleSaltsModule extends PiccoloModule {
         // Create an ion and add it to the model
         {
             Vessel vessel = model.getVessel();
-            Ion ion = new Sodium( new Point2D.Double( vessel.getLocation().getX() + 100,
-                                                      vessel.getLocation().getY() + vessel.getDepth() - 50 ),
-                                  new Vector2D.Double( 2, -1 ),
-                                  new Vector2D.Double() );
-//            model.addModelElement( ion );
-
             Ion ion2 = new Chloride( new Point2D.Double( vessel.getLocation().getX() + 100,
                                                          vessel.getLocation().getY() - 50 ),
                                      new Vector2D.Double( 0, 5 ),
@@ -88,8 +80,18 @@ public class SolubleSaltsModule extends PiccoloModule {
             }
         }
 
+        // Set up the control panel
+//        setControlPanel( new SolubleSaltsControlPanel( this ) );
+        setControlPanel( new ControlPanel( this ) );
+
     }
 
+    /**
+     * Generate a random position for an ion
+     *
+     * @param ion
+     * @return
+     */
     private Point2D genIonPosition( Ion ion ) {
         SolubleSaltsModel model = (SolubleSaltsModel)getModel();
         Vessel vessel = model.getVessel();
@@ -100,8 +102,24 @@ public class SolubleSaltsModule extends PiccoloModule {
         return new Point2D.Double( x, y );
     }
 
+    /**
+     * Generate a random velocity for an aion
+     *
+     * @return
+     */
     private Vector2D genVelocity() {
         double vMax = 4;
         return new Vector2D.Double( random.nextDouble() * vMax, random.nextDouble() * vMax );
     }
+
+    private void test() {
+        // A test graphic
+        Rectangle r = new Rectangle( 100, 150, 20, 70 );
+        PPath pp = new PPath( r );
+        pp.setPaint( Color.red );
+        simPanel.addWorldChild( pp );
+        pp.addInputEventListener( new CursorHandler( Cursor.HAND_CURSOR ) );
+        pp.addInputEventListener( new PDragEventHandler() );
+    }
+
 }
