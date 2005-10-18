d917168734a185646b892d1b3464a5a51c262641
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-10-18 20:09:57 +0000

    set up application framework for "Optical Pulse Shaper" simulation

---
+++ trunk/shaper/src/edu/colorado/phet/shaper/Version.java
@@ -0,0 +1,28 @@
+/* Copyright 2005, University of Colorado */
+
+/*
+ * CVS Info -
+ * Filename : $Source$
+ * Branch : $Name$
+ * Modified by : $Author$
+ * Revision : $Revision$
+ * Date modified : $Date$
+ */
+
+package edu.colorado.phet.shaper;
+
+
+/**
+ * Version
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ * @version $Revision$
+ */
+public class Version {
+  
+    public static final String NUMBER = "0.0.1";
+    public static final String BUILD = "shaper-0-0-1-2005-10-18";
+    
+    /* This class is not intended for instantiation. */
+    private Version() {}
+}
