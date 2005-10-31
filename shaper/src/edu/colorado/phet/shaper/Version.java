3bd6265784828c5fd3b612821cf1531881500b5a
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-10-31 21:32:57 +0000

    0.0.5

--- shaper/src/edu/colorado/phet/shaper/Version.java
+++ shaper/src/edu/colorado/phet/shaper/Version.java
@@ -20,9 +20,9 @@ package edu.colorado.phet.shaper;
  */
 public class Version {
   
-    public static final String NUMBER = "0.0.4";
+    public static final String NUMBER = "0.0.5";
     
-    public static final String CVS_TAG = "shaper-0-0-4-2005-10-28";
+    public static final String CVS_TAG = "shaper-0-0-5-2005-10-31";
     
     /* This class is not intended for instantiation. */
     private Version() {}
