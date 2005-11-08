0aff0c89e96b745c7a2095058b7003ba1a554152
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-11-08 21:31:54 +0000

    updates

--- energyconservation3/src/edu/colorado/phet/ec3/EnergyPositionPlotPanel.java
+++ energyconservation3/src/edu/colorado/phet/ec3/EnergyPositionPlotPanel.java
@@ -5,8 +5,11 @@ import edu.colorado.phet.common.model.clock.ClockTickEvent;
 import edu.colorado.phet.common.model.clock.ClockTickListener;
 import edu.colorado.phet.ec3.model.Body;
 import edu.colorado.phet.ec3.plots.Range2D;
+import edu.colorado.phet.piccolo.PhetPCanvas;
+import edu.umd.cs.piccolo.nodes.PImage;
+import edu.umd.cs.piccolo.nodes.PPath;
 import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartPanel;
+import org.jfree.chart.ChartRenderingInfo;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.axis.NumberAxis;
 import org.jfree.chart.plot.PlotOrientation;
@@ -19,6 +22,13 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 
 /**
  * User: Sam Reid
@@ -27,14 +37,16 @@ import java.awt.event.ActionListener;
  * Copyright (c) Nov 6, 2005 by Sam Reid
  */
 
-public class EnergyPositionPlotPanel extends JPanel {
+public class EnergyPositionPlotPanel extends PhetPCanvas {
     private JFreeChart chart;
     private XYSeriesCollection dataset;
-    private ChartPanel chartPanel;
-    private XYSeries peSeries;
     private EC3Module module;
+
+    private XYSeries peSeries;
     private XYSeries keSeries;
     private XYSeries totSeries;
+    private PImage image;
+    private ChartRenderingInfo info = new ChartRenderingInfo();
 
     public EnergyPositionPlotPanel( EC3Module ec3Module ) {
         this.module = ec3Module;
@@ -45,17 +57,15 @@ public class EnergyPositionPlotPanel extends JPanel {
         } );
         dataset = createDataset();
         chart = createChart( new Range2D( 0, 0, 800, 400000 ), dataset, "Title" );
-        chartPanel = new ChartPanel( chart );
+//        chartPanel = new ChartPanel( chart );
         setLayout( new BorderLayout() );
-        add( chartPanel, BorderLayout.CENTER );
+//        add( chartPanel, BorderLayout.CENTER );
         peSeries = new XYSeries( "Potential" );
-        dataset.addSeries( peSeries );
-
         keSeries = new XYSeries( "Kinetic" );
-        dataset.addSeries( keSeries );
-
         totSeries = new XYSeries( "Total" );
-        dataset.addSeries( totSeries );
+//        dataset.addSeries( peSeries );
+//        dataset.addSeries( keSeries );
+//        dataset.addSeries( totSeries );
         JButton clear = new JButton( "Clear" );
         clear.addActionListener( new ActionListener() {
             public void actionPerformed( ActionEvent e ) {
@@ -64,6 +74,26 @@ public class EnergyPositionPlotPanel extends JPanel {
         } );
         add( clear, BorderLayout.SOUTH );
         chart.setAntiAlias( true );
+
+        image = new PImage( new BufferedImage( 10, 10, BufferedImage.TYPE_INT_RGB ) );
+        addScreenChild( image );
+        updateImage();
+        addComponentListener( new ComponentAdapter() {
+            public void componentResized( ComponentEvent e ) {
+                updateImage();
+            }
+
+            public void componentShown( ComponentEvent e ) {
+                updateImage();
+            }
+        } );
+    }
+
+    private void updateImage() {
+        if( getWidth() > 0 && getHeight() > 0 ) {
+            image.setImage( chart.createBufferedImage( getWidth(), getHeight(), info ) );
+        }
+        reset();
     }
 
     private static JFreeChart createChart( Range2D range, XYDataset dataset, String title ) {
@@ -75,7 +105,7 @@ public class EnergyPositionPlotPanel extends JPanel {
 
         chart.setBackgroundPaint( new Color( 240, 220, 210 ) );
 
-        XYPlot plot = (XYPlot)chart.getPlot();
+        XYPlot plot = chart.getXYPlot();
         plot.setBackgroundPaint( Color.white );
         plot.setDomainGridlinesVisible( true );
         plot.setRangeGridlinesVisible( false );
@@ -108,14 +138,81 @@ public class EnergyPositionPlotPanel extends JPanel {
         peSeries.clear();
         keSeries.clear();
         totSeries.clear();
+        getScreenNode().removeAllChildren();
+        addScreenChild( image );
+        peDots.clear();
     }
+
+    ArrayList peDots = new ArrayList();
 
     private void update() {
         if( module.getEnergyConservationModel().numBodies() > 0 ) {
             Body body = module.getEnergyConservationModel().bodyAt( 0 );
-            peSeries.add( body.getX(), module.getEnergyConservationModel().getPotentialEnergy( body ) );
-            keSeries.add( body.getX(), body.getKineticEnergy() );
-            totSeries.add( body.getX(), module.getEnergyConservationModel().getTotalEnergy( body ) );
+//            peSeries.add( body.getX(), module.getEnergyConservationModel().getPotentialEnergy( body ) );
+//            keSeries.add( body.getX(), body.getKineticEnergy() );
+//            totSeries.add( body.getX(), module.getEnergyConservationModel().getTotalEnergy( body ) );
+
+            addFadeDot( body.getX(), module.getEnergyConservationModel().getPotentialEnergy( body ), module.getEnergyLookAndFeel().getPEColor() );
+//            addFadeDot( body.getX(), module.getEnergyConservationModel().getTotalEnergy( body ), module.getEnergyLookAndFeel().getTotalEnergyColor() );
+            addFadeDot( body.getX(), body.getKineticEnergy(), module.getEnergyLookAndFeel().getKEColor() );
         }
+        for( int i = 0; i < peDots.size(); i++ ) {
+            FadeDot fadeDot = (FadeDot)peDots.get( i );
+            fadeDot.fade();
+            if( fadeDot.isFullyFaded() ) {
+                peDots.remove( i );
+                getScreenNode().removeChild( fadeDot );
+                i--;
+            }
+        }
+
+    }
+
+    private void addFadeDot( double x, double y, Color peColor ) {
+        FadeDot path = new FadeDot( peColor, toImageLocation( x, y ) );
+        addScreenChild( path );
+        peDots.add( path );
+    }
+
+    static class FadeDot extends PPath {
+        private Color origColor;
+        private int age;
+        private int dAge = 2;
+        private Color fadeColor;
+
+        public FadeDot( Color color, Point2D loc ) {
+            super( new Ellipse2D.Double( -3, -3, 6, 6 ), null );
+            setPaint( color );
+            setOffset( loc );
+            this.origColor = color;
+        }
+
+        public void fade() {
+            age += dAge;
+            int fadeAlpha = 255 - age;
+            if( fadeAlpha < 0 ) {
+                fadeAlpha = 0;
+            }
+            Color fadeColor = new Color( origColor.getRed(), origColor.getGreen(), origColor.getBlue(),
+                                         fadeAlpha );
+            this.fadeColor = fadeColor;
+            setPaint( fadeColor );
+        }
+
+        public boolean isFullyFaded() {
+            return fadeColor.getAlpha() <= 0;
+        }
+    }
+
+    public Point2D toImageLocation( double x, double y ) {
+        Rectangle2D dataArea = info.getPlotInfo().getDataArea();
+        if( dataArea == null ) {
+            throw new RuntimeException( "Null data area" );
+        }
+
+        double transX1 = chart.getXYPlot().getDomainAxisForDataset( 0 ).valueToJava2D( x, dataArea, chart.getXYPlot().getDomainAxisEdge() );
+        double transY1 = chart.getXYPlot().getRangeAxisForDataset( 0 ).valueToJava2D( y, dataArea, chart.getXYPlot().getRangeAxisEdge() );
+        Point2D.Double pt = new Point2D.Double( transX1, transY1 );
+        return pt;
     }
 }
