71a7d30621190b29f0a9559881936e58d826d8b6
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-11-14 22:57:05 +0000

    package name was wrong, this class apparently was moved from a "tools" package

--- trunk/piccolo-phet/src/edu/colorado/phet/piccolo/MeasuringTape.java
+++ trunk/piccolo-phet/src/edu/colorado/phet/piccolo/MeasuringTape.java
@@ -1,5 +1,5 @@
 /* Copyright 2004, Sam Reid */
-package edu.colorado.phet.piccolo.tools;
+package edu.colorado.phet.piccolo;
 
 import edu.colorado.phet.common.math.Vector2D;
 import edu.colorado.phet.common.view.graphics.transforms.ModelViewTransform2D;
