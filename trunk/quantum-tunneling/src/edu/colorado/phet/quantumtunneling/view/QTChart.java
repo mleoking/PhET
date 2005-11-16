98c42903973d8eab1e453b53392e9c95c8be409f
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-11-16 22:29:04 +0000

    javadoc

--- trunk/quantum-tunneling/src/edu/colorado/phet/quantumtunneling/view/QTChart.java
+++ trunk/quantum-tunneling/src/edu/colorado/phet/quantumtunneling/view/QTChart.java
@@ -28,19 +28,36 @@ import edu.umd.cs.piccolo.PNode;
 
 
 /**
- * QTChart
+ * QTChart is a PNode wrapper around a JFreeChart.
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  * @version $Revision$
  */
 public class QTChart extends PNode {
 
+    //----------------------------------------------------------------------------
+    // Instance data
+    //----------------------------------------------------------------------------
+    
     private JFreeChart _chart;
     private ChartPanel _chartPanel;
     private PSwing _pSwing;
     
+    //----------------------------------------------------------------------------
+    // Constructors
+    //----------------------------------------------------------------------------
+    
+    /**
+     * Sole constructor.
+     * 
+     * @param canvas
+     * @param title
+     * @param xAxisLabel
+     * @param yAxisLabel
+     */
     public QTChart( PSwingCanvas canvas, String title, String xAxisLabel, String yAxisLabel ) {
         
+        // Create a JFreeChart
         _chart = ChartFactory.createXYLineChart( 
                 title,            // title
                 xAxisLabel,       // x-axis label
@@ -52,33 +69,68 @@ public class QTChart extends PNode {
                 false             // generate URLs?
         );
         _chart.setBackgroundPaint( QTConstants.CANVAS_COLOR );
+        
+        // Put the JFreeChart inside a ChartPanel (a JPanel)
         _chartPanel = new ChartPanel( _chart );
         _chartPanel.setOpaque( false );
+        
+        // Wrap the ChartPanel with PSwing
         _pSwing = new PSwing( canvas, _chartPanel );
         
+        // Wrap the PSwing with this PNode
         addChild( _pSwing );
     }
     
+    //----------------------------------------------------------------------------
+    // Accessors
+    //----------------------------------------------------------------------------
+    
+    /**
+     * Sets the font used to draw the axes labels.
+     * 
+     * @param font
+     */
     public void setAxisLabelFont( Font font ) {
         XYPlot xyPlot = _chart.getXYPlot();
         xyPlot.getDomainAxis().setLabelFont( font ); // x axis
         xyPlot.getRangeAxis().setLabelFont( font ); // y axis
     }
     
+    /**
+     * Sets the range of the X axis.
+     * 
+     * @param range
+     */
     public void setXRange( Range range ) {
         XYPlot xyPlot = _chart.getXYPlot();
         xyPlot.getDomainAxis().setRange( range );
     }
     
+    /**
+     * Sets the range of the Y axis.
+     * 
+     * @param range
+     */
     public void setYRange( Range range ) {
         XYPlot xyPlot = _chart.getXYPlot();
         xyPlot.getRangeAxis().setRange( range );
     }
     
+    /**
+     * Sets the dimensions of the chart.
+     * 
+     * @param width
+     * @param height
+     */
     public void setSize( int width, int height ) {
         setSize( new Dimension( width, height ) );
     }
     
+    /**
+     * Sets the dimensions of the chart.
+     * 
+     * @param size
+     */
     public void setSize( Dimension size ) {
         if ( size.getWidth() > 0 && size.getHeight() > 0 ) {
             _chartPanel.setPreferredSize( size );
