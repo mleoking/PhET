1d57941d5d72be7d3897d1db5be4ca05f5a2150f
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-11-29 16:20:23 +0000

    - properly restore the cursor - add javadoc

--- trunk/piccolo-phet/src/edu/colorado/phet/piccolo/CursorHandler.java
+++ trunk/piccolo-phet/src/edu/colorado/phet/piccolo/CursorHandler.java
@@ -8,73 +8,118 @@ import java.awt.*;
 import java.awt.event.MouseEvent;
 
 /**
- * Changes the mouse to a different cursor on entrance.
- * //todo should this save the state of the mouse before entrance, for restore on exit?
+ * CursorHandler handles cursor behavior for interactive PNodes.
+ * It changes the mouse to a different cursor on entrance,
+ * and restores the cursor on exit.  If the mouse was pressed
+ * (to drag, for example) then the cursor is not restored until
+ * after the mouse has been released.
+ * <p>
+ * The default cursor is the "hand" cursor, but you can specify your
+ * own cursor in one of the constructors.
+ * <p>
+ * None of the events received by this handler are marked as "handled",
+ * so this hanlder can co-exist with other event handlers.  Other event
+ * handlers should be careful not to mark events as "handled" that this
+ * handler needs to see.
  */
 
 public class CursorHandler extends PBasicInputEventHandler {
     
-    public static final Cursor HAND = Cursor.getPredefinedCursor( Cursor.HAND_CURSOR );
-    public static final Cursor DEFAULT = Cursor.getPredefinedCursor( Cursor.DEFAULT_CURSOR );
-    public static final Cursor CROSSHAIR = Cursor.getPredefinedCursor( Cursor.CROSSHAIR_CURSOR );
+    //----------------------------------------------------------------------------
+    // Class data
+    //----------------------------------------------------------------------------
     
+    // Some common cursors...
+    public static final Cursor CROSSHAIR = Cursor.getPredefinedCursor( Cursor.CROSSHAIR_CURSOR );
+    public static final Cursor DEFAULT = Cursor.getPredefinedCursor( Cursor.DEFAULT_CURSOR );
+    public static final Cursor HAND = Cursor.getPredefinedCursor( Cursor.HAND_CURSOR );
+    
+    //----------------------------------------------------------------------------
+    // Instance data
+    //----------------------------------------------------------------------------
+    
+    /* cursor to change to */
     private Cursor cursor;
-    private Cursor defaultCursor;//the cursor to return to on exit.
-    private boolean dragging = false;
-    private boolean entered = false;
+    /* cursor to restore */
+    private Cursor restoreCursor;
+    /* is the mouse pressed? */
+    private boolean mousePressed;
+    /* Is the mouse inside the node? */
+    private boolean mouseInside;
 
+    //----------------------------------------------------------------------------
+    // Constructors
+    //----------------------------------------------------------------------------
+    
+    /**
+     * Creates a handler that uses the HAND cursor.
+     */
     public CursorHandler() {
         this( HAND );
     }
     
+    /**
+     * Creates a handler using one of the cursor types predefined by Cursor.
+     * 
+     * @param cursorType
+     */
     public CursorHandler( int cursorType ) {
         this( Cursor.getPredefinedCursor( cursorType ) );
     }
 
+    /**
+     * Creates a handler using a specified cursor.
+     * 
+     * @param cursor
+     */
     public CursorHandler( Cursor cursor ) {
-        this( cursor, Cursor.getPredefinedCursor( Cursor.DEFAULT_CURSOR ) );
-    }
-
-    public CursorHandler( Cursor cursor, Cursor defaultCursor ) {
         this.cursor = cursor;
-        this.defaultCursor = defaultCursor;
+        mousePressed = false;
+        mouseInside = false;
     }
 
+    //----------------------------------------------------------------------------
+    // PBasicInputEventHandler overrides
+    //----------------------------------------------------------------------------
+    
     public void mouseEntered( PInputEvent event ) {
-        entered = true;
-        getComponent( event ).setCursor( cursor );
+        mouseInside = true;
+        if( !mousePressed ) {
+            restoreCursor = getComponent( event ).getCursor();
+            getComponent( event ).setCursor( cursor ); 
+        }
     }
 
     public void mouseExited( PInputEvent event ) {
-        entered = false;
-        if( !dragging ) {
-            getComponent( event ).setCursor( defaultCursor );
+        mouseInside = false;
+        if( !mousePressed ) {
+            getComponent( event ).setCursor( restoreCursor );
         }
     }
     
-    public void mouseDragged( PInputEvent event ) {
-        dragging = true;
-        getComponent( event ).setCursor( cursor );
+    public void mousePressed( PInputEvent event ) {
+        mousePressed = true;
+        if( !mouseInside ) {
+            restoreCursor = getComponent( event ).getCursor();
+            getComponent( event ).setCursor( cursor );
+        }
     }
 
     public void mouseReleased( PInputEvent event ) {
-        dragging = false;
-        if( !entered ) {
-            getComponent( event ).setCursor( defaultCursor );
+        mousePressed = false;
+        if( !mouseInside ) {
+            getComponent( event ).setCursor( restoreCursor );
         }
-        super.mouseReleased( event );
     }
 
+    /*
+     * Workaround for a Piccolo problem.
+     */
     private Component getComponent( PInputEvent aEvent ) {
         if( !( EventQueue.getCurrentEvent() instanceof MouseEvent ) ) {
             new Exception("EventQueue.getCurrentEvent was not a MouseEvent, consider making PInputEvent.getSourceSwingEvent public.  Actual event: "+aEvent+", class="+aEvent.getClass().getName()).printStackTrace( );
         }
-        if( EventQueue.getCurrentEvent() instanceof MouseEvent ) {
-            MouseEvent mouseEvent = (MouseEvent)EventQueue.getCurrentEvent();
-            return mouseEvent.getComponent();
-        }
-        else {
-            throw new RuntimeException( "No component for event." );
-        }
+        MouseEvent mouseEvent = (MouseEvent)EventQueue.getCurrentEvent();
+        return mouseEvent.getComponent();
     }
 }
