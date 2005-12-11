2979265f465c8196a4038f61866ae2116d94274c
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-12-11 02:12:18 +0000

    Bugfix: detectors & barriers only work north of the slits, or if slit absorption is disabled.

---
+++ schrodinger/src/edu/colorado/phet/qm/model/WaveModel.java
@@ -0,0 +1,31 @@
+/* Copyright 2004, Sam Reid */
+package edu.colorado.phet.qm.model;
+
+/**
+ * User: Sam Reid
+ * Date: Dec 10, 2005
+ * Time: 7:05:02 PM
+ * Copyright (c) Dec 10, 2005 by Sam Reid
+ */
+
+public class WaveModel {
+    private Wavefunction wavefunction;
+    private Propagator propagator;
+
+    public WaveModel( Wavefunction wavefunction, Propagator propagator ) {
+        this.wavefunction = wavefunction;
+        this.propagator = propagator;
+    }
+
+    public Wavefunction getWavefunction() {
+        return wavefunction;
+    }
+
+    public Propagator getPropagator() {
+        return propagator;
+    }
+
+    public void setPropagator( Propagator propagator ) {
+        this.propagator = propagator;
+    }
+}
