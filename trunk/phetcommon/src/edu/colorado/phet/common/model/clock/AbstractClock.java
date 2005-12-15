d0098260d46143d8c3abbde61c452ccbc588a16c
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-12-15 19:03:49 +0000

    remove previous changes to setRunningTime and resetRunningTime, it breaks some older simulations

--- trunk/phetcommon/src/edu/colorado/phet/common/model/clock/AbstractClock.java
+++ trunk/phetcommon/src/edu/colorado/phet/common/model/clock/AbstractClock.java
@@ -162,7 +162,6 @@ public abstract class AbstractClock {
 
     public void resetRunningTime() {
         this.runningTime = 0;
-        clockTicked( 0 );
     }
 
     protected void clockTicked( double dt ) {
@@ -177,7 +176,6 @@ public abstract class AbstractClock {
 
     protected void setRunningTime( double runningTime ) {
         this.runningTime = runningTime;
-        clockTicked( 0 );
     }
 
     protected double getSimulationTime( long actualDelay ) {
