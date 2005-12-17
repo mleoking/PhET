8e57de6bc847bb4bd3b0fef4bda36e7fc146470c
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-12-17 16:29:03 +0000

    Upgraded to work with packages.

--- energyconservation3/src/edu/colorado/phet/ec3/view/bargraphs/BarGraphSet.java
+++ energyconservation3/src/edu/colorado/phet/ec3/view/bargraphs/BarGraphSet.java
@@ -5,7 +5,7 @@ import edu.colorado.phet.common.math.ModelViewTransform1D;
 import edu.colorado.phet.common.model.clock.ClockAdapter;
 import edu.colorado.phet.common.model.clock.ClockEvent;
 import edu.colorado.phet.common.model.clock.ClockListener;
-import edu.colorado.phet.common.view.graphics.shapes.Arrow;
+import edu.colorado.phet.common.view.graphics.Arrow;
 import edu.colorado.phet.common.view.util.ImageLoader;
 import edu.colorado.phet.ec3.EC3Canvas;
 import edu.colorado.phet.ec3.EnergyLookAndFeel;
