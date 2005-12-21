6f64a1ba1cf2d95d78c0785e7abf58d1b35cdf96
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-12-21 21:24:37 +0000

    added control panel code

--- branches/pc-for-cck-branch-feb05/phetcommon/src/edu/colorado/phet/common/view/components/VerticalLayoutPanel.java
+++ branches/pc-for-cck-branch-feb05/phetcommon/src/edu/colorado/phet/common/view/components/VerticalLayoutPanel.java
@@ -1,21 +1,31 @@
-/** Sam Reid*/
+/* Copyright 2004, University of Colorado */
+
+/*
+ * CVS Info -
+ * Filename : $Source$
+ * Branch : $Name$
+ * Modified by : $Author$
+ * Revision : $Revision$
+ * Date modified : $Date$
+ */
 package edu.colorado.phet.common.view.components;
 
 import javax.swing.*;
 import java.awt.*;
 
 /**
- * User: Sam Reid
- * Date: Aug 13, 2004
- * Time: 10:10:30 AM
- * Copyright (c) Aug 13, 2004 by Sam Reid
+ * VerticalLayoutPanel
+ *
+ * @author ?
+ * @version $Revision$
  */
 public class VerticalLayoutPanel extends JPanel {
     private GridBagConstraints gridBagConstraints;
 
     public VerticalLayoutPanel() {
         setLayout( new GridBagLayout() );
-        gridBagConstraints = new GridBagConstraints( 0, 0, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets( 0, 0, 0, 0 ), 0, 0 );
+        gridBagConstraints = new GridBagConstraints( 0, 0, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets( 0, 0, 0, 0 ), 0, 0 );
+        gridBagConstraints.weighty = 0.5;
     }
 
     public GridBagConstraints getGridBagConstraints() {
@@ -28,8 +38,20 @@ public class VerticalLayoutPanel extends JPanel {
         return null;
     }
 
+    public void setGridY( int gridy ) {
+        this.gridBagConstraints.gridy = gridy;
+    }
+
     public void setFill( int fill ) {
         gridBagConstraints.fill = fill;
+    }
+
+    public void setFillHorizontal() {
+        setFill( GridBagConstraints.HORIZONTAL );
+    }
+
+    public void setFillNone() {
+        setFill( GridBagConstraints.NONE );
     }
 
     public void setAnchor( int anchor ) {
@@ -38,5 +60,17 @@ public class VerticalLayoutPanel extends JPanel {
 
     public void setInsets( Insets insets ) {
         gridBagConstraints.insets = insets;
+    }
+
+    public int getFill() {
+        return gridBagConstraints.fill;
+    }
+
+    public Component addFullWidth( Component component ) {
+        int fill = getFill();
+        setFill( GridBagConstraints.HORIZONTAL );
+        Component c = add( component );
+        setFill( fill );
+        return c;
     }
 }
