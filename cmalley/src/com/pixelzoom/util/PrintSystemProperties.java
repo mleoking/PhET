2ca551b3a1bdc448adfe4cc144b7a7a6e9e4423d
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-12-23 18:15:17 +0000

    sort

--- cmalley/src/com/pixelzoom/util/PrintSystemProperties.java
+++ cmalley/src/com/pixelzoom/util/PrintSystemProperties.java
@@ -11,9 +11,14 @@
 
 package com.pixelzoom.util;
 
+import java.util.*;
+import java.util.Collections;
+import java.util.Properties;
+import java.util.Set;
+
 
 /**
- * PrintSystemProperties prints the System properties to System.out.
+ * PrintSystemProperties prints the System properties (sorted) to System.out.
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  * @version $Revision$
@@ -21,6 +26,20 @@ package com.pixelzoom.util;
 public class PrintSystemProperties {
 
     public static void main( String[] args ) {
-        System.getProperties().list(System.out);
+        
+        // Get the system properties
+        Properties properties = System.getProperties();
+        
+        // Sort 'em
+        Object[] keySet = properties.keySet().toArray();
+        List keys = Arrays.asList( keySet );
+        Collections.sort( keys );
+
+        // Print out each key/value pair
+        for( int i = 0; i < keys.size(); i++ ) {
+            Object key = keys.get( i );
+            Object value = properties.get( key );
+            System.out.println( key + ": " + value );
+        }
     }
 }
