2c8250cf5275fdd934bafc330844029c0aed8f72
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2005-12-27 22:17:57 +0000

    (12/27/05) - changes to support phetcommon's new Module and Clock architecture

--- test-location/src/edu/colorado/phet/testlocation/TestApplication.java
+++ test-location/src/edu/colorado/phet/testlocation/TestApplication.java
@@ -2,11 +2,7 @@ package edu.colorado.phet.testlocation;
 
 import java.io.IOException;
 
-import edu.colorado.phet.common.application.ApplicationModel;
-import edu.colorado.phet.common.application.Module;
 import edu.colorado.phet.common.application.PhetApplication;
-import edu.colorado.phet.common.model.clock.AbstractClock;
-import edu.colorado.phet.common.model.clock.SwingTimerClock;
 import edu.colorado.phet.common.view.util.FrameSetup;
 
 /**
@@ -18,31 +14,20 @@ import edu.colorado.phet.common.view.util.FrameSetup;
  */
 public class TestApplication extends PhetApplication {
 
-    public TestApplication() throws IOException {
-        super( new LocationApplicationModel() );
+    public TestApplication( String[] args, String title, String description, String version, FrameSetup frameSetup ) throws IOException {
+        super( args, title, description, version, frameSetup );
+        TestModule module = new TestModule();
+        addModule( module );
     }
     
-    private static class LocationApplicationModel extends ApplicationModel {
-        public LocationApplicationModel()  {
-            super( "Test Location", "Tests changes to semantics of PhetGraphic location", "0.1" );
-            FrameSetup frameSetup = new FrameSetup.CenteredWithSize( 1024, 768 );
-            setFrameSetup( frameSetup );
-            AbstractClock clock = new SwingTimerClock( 1, 16 );
-            setClock( clock );
-            TestModule module = new TestModule( this, clock );
-            setModules( new Module[] { module } );
-            setInitialModule( module );
-        }
-    }
-    
-    public static void main( String[] args ) {
-        try {
-            PhetApplication app = new TestApplication();
-            app.startApplication();
-        }
-        catch( IOException ioe ) {
-            ioe.printStackTrace();
-            System.exit( 1 );
-        }
+    public static void main( String[] args ) throws IOException {
+
+        String title = "Test Location";
+        String description = "Tests changes to semantics of PhetGraphic location";
+        String version = "0.1";
+        FrameSetup frameSetup = new FrameSetup.CenteredWithSize( 1024, 768 );
+
+        PhetApplication app = new TestApplication( args, title, description, version, frameSetup );
+        app.startApplication();
     }
 }
