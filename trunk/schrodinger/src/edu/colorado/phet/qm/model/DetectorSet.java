88e237f77a19461c4b370785d13baecd5551ddae
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-01-05 08:42:05 +0000

    add functions

--- trunk/schrodinger/src/edu/colorado/phet/qm/model/DetectorSet.java
+++ trunk/schrodinger/src/edu/colorado/phet/qm/model/DetectorSet.java
@@ -1,8 +1,6 @@
 /* Copyright 2004, Sam Reid */
 package edu.colorado.phet.qm.model;
 
-import edu.colorado.phet.qm.model.math.Complex;
-
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.Random;
@@ -99,40 +97,8 @@ public class DetectorSet {
 
     }
 
-    public Point getCollapsePoint( Rectangle bounds ) {
-        //compute a probability model for each dA
-        Wavefunction copy = getWavefunction().copy();
-        for( int i = 0; i < copy.getWidth(); i++ ) {
-            for( int j = 0; j < copy.getHeight(); j++ ) {
-                if( !bounds.contains( i, j ) ) {
-                    copy.valueAt( i, j ).zero();
-                }
-            }
-        }
-
-        copy.normalize();//in case we care
-        Complex runningSum = new Complex();
-        double rnd = random.nextDouble();
-
-        for( int i = 0; i < copy.getWidth(); i++ ) {
-            for( int j = 0; j < copy.getHeight(); j++ ) {
-                Complex psiStar = copy.valueAt( i, j ).complexConjugate();
-                Complex psi = copy.valueAt( i, j );
-                Complex term = psiStar.times( psi );
-                double pre = runningSum.abs();
-                runningSum = runningSum.plus( term );
-                double post = runningSum.abs();
-                if( pre <= rnd && rnd <= post ) {
-                    return new Point( i, j );
-                }
-            }
-        }
-        new RuntimeException( "No collapse point." ).printStackTrace();
-        return new Point( 0, 0 );
-    }
-
     public Point getCollapsePoint() {//todo call getCollapsePoint with the internal bounds.
-        return getCollapsePoint( getWavefunction().getBounds() );
+        return new CollapseComputation().getCollapsePoint( wavefunction, wavefunction.getBounds() );
     }
 
     private Wavefunction getWavefunction() {
