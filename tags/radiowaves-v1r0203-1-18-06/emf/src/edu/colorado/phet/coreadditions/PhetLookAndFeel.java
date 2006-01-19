a47d74ad341213c8e3ddaba06ced6ff2e79a53e2
nobody <nobody@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-01-19 00:00:59 +0000

    This commit was manufactured by cvs2svn to create tag 'radiowaves-v1r0203-1-18-06'.

---
+++ tags/radiowaves-v1r0203-1-18-06/emf/src/edu/colorado/phet/coreadditions/PhetLookAndFeel.java
@@ -0,0 +1,13 @@
+/**
+ * Class: PhetLookAndFeel
+ * Package: edu.colorado.phet.coreadditions
+ * Author: Another Guy
+ * Date: Aug 5, 2003
+ */
+package edu.colorado.phet.coreadditions;
+
+import java.awt.*;
+
+public interface PhetLookAndFeel {
+    public Image getSmallIconImage();
+}
