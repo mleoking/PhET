fb31d8b9dd257aa51720d6f8c1d79fe14fa2d4c4
nobody <nobody@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-01-25 01:43:13 +0000

    This commit was manufactured by cvs2svn to create tag 'qwi-1-24-2006-i'.

---
+++ tags/qwi-1-24-2006-i/schrodinger/src/edu/colorado/phet/qm/view/colorgrid/ColorMap.java
@@ -0,0 +1,13 @@
+package edu.colorado.phet.qm.view.colorgrid;
+
+import java.awt.*;
+
+/**
+ * User: Sam Reid
+ * Date: Jun 9, 2005
+ * Time: 2:55:30 PM
+ * Copyright (c) Jun 9, 2005 by Sam Reid
+ */
+public interface ColorMap {
+    Paint getColor( int i, int k );
+}
