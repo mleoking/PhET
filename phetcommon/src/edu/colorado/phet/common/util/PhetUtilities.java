6f93e18d3c33c10ea7726979ed635c825e9914c6
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-02-02 18:41:45 +0000

    Added code to invokeLater() to avoid startup sequence problems.

--- phetcommon/src/edu/colorado/phet/common/util/PhetUtilities.java
+++ phetcommon/src/edu/colorado/phet/common/util/PhetUtilities.java
@@ -18,36 +18,46 @@ import edu.colorado.phet.common.model.clock.SwingClock;
 import edu.colorado.phet.common.view.PhetFrame;
 
 import javax.swing.*;
+import java.util.ArrayList;
 
 /**
  * PhetModelUtilities
- * <p>
+ * <p/>
  * Static methods of general utility
  *
  * @author Ron LeMaster
  * @version $Revision$
  */
 public class PhetUtilities {
+    private static ArrayList pendingRunnables = new ArrayList();
 
     /**
      * Requests that a Runnable be executed by the model.
-     * <p>
+     * <p/>
      * This provides thread-safe execution of a method by the model, whether its clock is running
      * in the Swing dispatch queue thread or a model-specific thread.
      *
      * @param runnable
      */
     public static void invokeLater( Runnable runnable ) {
-        IClock clock = PhetApplication.instance().getActiveModule().getClock();
-        if( clock instanceof ModelClock ) {
-            ModelClock modelClock = (ModelClock)clock;
-            modelClock.invokeLater( runnable );
-        }
-        else if( clock instanceof SwingClock ) {
-            SwingUtilities.invokeLater( runnable );
-        }
-        else {
-            runnable.run();
+        pendingRunnables.add( runnable );
+        Module activeModule = getActiveModule();
+        if( activeModule != null ){
+            IClock clock = activeModule.getClock();
+            for( int i = 0; i < pendingRunnables.size(); i++ ) {
+                Runnable target = (Runnable)pendingRunnables.get( i );
+                if( clock instanceof ModelClock ) {
+                    ModelClock modelClock = (ModelClock)clock;
+                    modelClock.invokeLater( target );
+                }
+                else if( clock instanceof SwingClock ) {
+                    SwingUtilities.invokeLater( target );
+                }
+                else {
+                    target.run();
+                }
+            }
+            pendingRunnables.clear();
         }
     }
 
