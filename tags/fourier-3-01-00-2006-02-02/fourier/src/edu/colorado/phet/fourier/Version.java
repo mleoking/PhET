20321b99b375b624aa4eabe529c15cb7a0c0a79e
nobody <nobody@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-02-02 21:21:19 +0000

    This commit was manufactured by cvs2svn to create tag 'fourier-3-01-00-2006-02-02'.

---
+++ tags/fourier-3-01-00-2006-02-02/fourier/src/edu/colorado/phet/fourier/Version.java
@@ -0,0 +1,28 @@
+/* Copyright 2005, University of Colorado */
+
+/*
+ * CVS Info -
+ * Filename : $Source$
+ * Branch : $Name$
+ * Modified by : $Author$
+ * Revision : $Revision$
+ * Date modified : $Date$
+ */
+
+package edu.colorado.phet.fourier;
+
+
+/**
+ * Version
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ * @version $Revision$
+ */
+public class Version {
+  
+    public static final String NUMBER = "3.01";
+    public static final String BUILD = "fourier-3-01-00-2006-02-02";
+    
+    /* This class is not intended for instantiation. */
+    private Version() {}
+}
