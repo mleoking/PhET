c8b8fb6c7d9812c13c7c4bd881ccaf1d3e1c5e46
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-02-09 19:13:26 +0000

    bugfix: need a different lower bound for each zoom level of Prob Density plot

--- trunk/quantum-tunneling/src/edu/colorado/phet/quantumtunneling/QTConstants.java
+++ trunk/quantum-tunneling/src/edu/colorado/phet/quantumtunneling/QTConstants.java
@@ -142,15 +142,14 @@ public class QTConstants {
     public static final Range DEFAULT_WAVE_FUNCTION_RANGE = WAVE_FUNCTION_ZOOM_SPECS[ DEFAULT_WAVE_FUNCTION_ZOOM_INDEX ].getRange();
     
     // Probability Density
-    private static final double PROBABILITY_DENSITY_MIN = -0.2;
     public static final ZoomSpec[] PROBABILITY_DENSITY_ZOOM_SPECS = {
                          /* range, tickSpacing, tickPattern */
-            new ZoomSpec( new Range( PROBABILITY_DENSITY_MIN, 0.25 ), 0.05, "0.00" ),
-            new ZoomSpec( new Range( PROBABILITY_DENSITY_MIN, 1 ),    0.25, "0.00" ),
-            new ZoomSpec( new Range( PROBABILITY_DENSITY_MIN, 2.25 ), 0.5,  "0.0" ),
-            new ZoomSpec( new Range( PROBABILITY_DENSITY_MIN, 4 ),    1,    "0" ),
-            new ZoomSpec( new Range( PROBABILITY_DENSITY_MIN, 6.25 ), 1,    "0" ),
-            new ZoomSpec( new Range( PROBABILITY_DENSITY_MIN, 9 ),    1,    "0" )
+            new ZoomSpec( new Range( -0.0125, 0.25 ), 0.05, "0.00" ),
+            new ZoomSpec( new Range( -0.05, 1 ),      0.25, "0.00" ),
+            new ZoomSpec( new Range( -0.1125, 2.25 ), 0.5,  "0.0" ),
+            new ZoomSpec( new Range( -0.2, 4 ),       1,    "0" ),
+            new ZoomSpec( new Range( -0.3125, 6.25 ), 1,    "0" ),
+            new ZoomSpec( new Range( -0.45, 9 ),      1,    "0" )
     };
     public static final int DEFAULT_PROBABILTY_DENSITY_ZOOM_INDEX = 3;
     public static final Range DEFAULT_PROBABILITY_DENSITY_RANGE = PROBABILITY_DENSITY_ZOOM_SPECS[ DEFAULT_PROBABILTY_DENSITY_ZOOM_INDEX ].getRange();
