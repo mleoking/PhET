f98b20197aa5baca691c02e608361d33de958df7
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-02-10 19:25:32 +0000

    (2/10/06) - replace method calls with duplicate code in RichardsonSolver.propagate to improve performance (2/10/06) - use a lightweight complex number implementation in RichardsonSolver to reduce method calls

--- trunk/quantum-tunneling/src/edu/colorado/phet/quantumtunneling/model/RichardsonSolver.java
+++ trunk/quantum-tunneling/src/edu/colorado/phet/quantumtunneling/model/RichardsonSolver.java
@@ -38,7 +38,7 @@ import edu.colorado.phet.quantumtunneling.util.MutableComplex;
  * @version $Revision$
  */
 public class RichardsonSolver {
-    
+
     //----------------------------------------------------------------------
     // Class data
     //----------------------------------------------------------------------
@@ -59,17 +59,17 @@ public class RichardsonSolver {
     private double _dx;
     private double _dt;
     private double _positions[]; // position at each sample point
-    private MutableComplex _Psi[]; // wave function values at each sample point
-    private Complex _EtoV[]; // potential energy propogator = exp(-i*V(x)*dt/hbar)
+    private RComplex _Psi[]; // wave function values at each sample point
+    private RComplex _EtoV[]; // potential energy propagator = exp(-i*V(x)*dt/hbar)
     
-    private Complex _alpha; // special parameter for Richardson algorithm
-    private Complex _beta; // special parameter for Richardson algorithm
+    private RComplex _alpha; // special parameter for Richardson algorithm
+    private RComplex _beta; // special parameter for Richardson algorithm
     
     // Reusable complex numbers
-    private MutableComplex _c1;
-    private MutableComplex _c2;
-    private MutableComplex _c3;
-    private MutableComplex _c4;
+    private RComplex _c1;
+    private RComplex _c2;
+    private RComplex _c3;
+    private RComplex _c4;
     
     //----------------------------------------------------------------------
     // Constructors
@@ -79,8 +79,6 @@ public class RichardsonSolver {
      * Sole constructor.
      * 
      * @param wavePacket
-     * @param dx
-     * @param dt
      */
     public RichardsonSolver( WavePacket wavePacket ) {
         
@@ -88,10 +86,10 @@ public class RichardsonSolver {
         _dx = 1;
         _dt = QTConstants.CLOCK_STEP;
                 
-        _c1 = new MutableComplex();
-        _c2 = new MutableComplex();
-        _c3 = new MutableComplex();
-        _c4 = new MutableComplex();
+        _c1 = new RComplex();
+        _c2 = new RComplex();
+        _c3 = new RComplex();
+        _c4 = new RComplex();
         
         update();
     }
@@ -114,7 +112,7 @@ public class RichardsonSolver {
      * 
      * @return Complex[]
      */
-    public Complex[] getEnergies() {
+    public RComplex[] getEnergies() {
         return _Psi;
     }
     
@@ -170,29 +168,29 @@ public class RichardsonSolver {
         // Calculate the number of sample points.
         final int numberOfPoints = (int)( ( maxX - minX ) / _dx ) + 1;
         
-        // Initialize constants used by the propogator.
+        // Initialize constants used by the propagator.
         final double epsilon = HBAR * _dt / ( MASS * _dx * _dx );
-        _alpha = new Complex( 0.5 * ( 1.0 + Math.cos( epsilon / 2 ) ), -0.5 * Math.sin( epsilon / 2 ) );
-        _beta = new Complex( ( Math.sin( epsilon / 4 ) ) * Math.sin( epsilon / 4 ), 0.5 * Math.sin( epsilon / 2 ) );
+        _alpha = new RComplex( 0.5 * ( 1.0 + Math.cos( epsilon / 2 ) ), -0.5 * Math.sin( epsilon / 2 ) );
+        _beta = new RComplex( ( Math.sin( epsilon / 4 ) ) * Math.sin( epsilon / 4 ), 0.5 * Math.sin( epsilon / 2 ) );
 
-        // Initialize the data arrays used by the propogator.
+        // Initialize the data arrays used by the propagator.
         _positions = new double[numberOfPoints];
-        _Psi = new MutableComplex[numberOfPoints];
-        _EtoV = new Complex[numberOfPoints];
-        final double A = 1 / ( Math.pow( Math.PI, 0.25 ) * Math.sqrt( width ) ); // normalization constant
+        _Psi = new RComplex[numberOfPoints];
+        _EtoV = new RComplex[numberOfPoints];
+        RComplex A = new RComplex( 1 / ( Math.pow( Math.PI, 0.25 ) * Math.sqrt( width ) ), 0 ); // normalization constant
         for ( int i = 0; i < numberOfPoints; i++ ) {
             final double position = minX + ( i * _dx );
             _positions[i] = position;
             if ( zero ) {
-                _Psi[i] = new MutableComplex( 0, 0 );
-                _EtoV[i] = Complex.ZERO;
+                _Psi[i] = new RComplex( 0, 0 );
+                _EtoV[i] = new RComplex( 0, 0 );
             }
             else {
                 final double r = Math.exp( -( ( position - center ) / width ) * ( ( position - center ) / width ) / 2 );
-                _Psi[i] = new MutableComplex( r * Math.cos( MASS * vx * ( position - center ) / HBAR ), r * Math.sin( MASS * vx * ( position - center ) / HBAR ) );
+                _Psi[i] = new RComplex( r * Math.cos( MASS * vx * ( position - center ) / HBAR ), r * Math.sin( MASS * vx * ( position - center ) / HBAR ) );
                 _Psi[i].multiply( A );
                 final double s = getPotentialEnergy( position ) * _dt / HBAR;
-                _EtoV[i] = new Complex( Math.cos( s ), -Math.sin( s ) );
+                _EtoV[i] = new RComplex( Math.cos( s ), -Math.sin( s ) );
             }
         }
     }
@@ -209,78 +207,186 @@ public class RichardsonSolver {
     }
     
     /**
-     * Propogates the solution by 1 step.
+     * Propagates the solution by 1 step.
+     * <p>
+     * Note that thre's a lot of duplicate code here.
+     * The body of each for loop could easily be made into a method; ditto for the damping code.
+     * (In fact, that's the way the I originally implemented this.)
+     * But this method is such a bottleneck, that reducing method calls results
+     * in big performance savings.  So 9sadly) we're trading off good code design for performance.
      */
-    public void propogate() {
+    public void propagate() {
         int numberOfPoints = _Psi.length;
 
+        // Starting at 0...
         for ( int i = 0; i < numberOfPoints - 1; i += 2 ) {
-            propogate( i, i + 1 );
+            // A = Psi[i]
+            _c1.setValue( _Psi[i] );
+            
+            // B = Psi[i+1]
+            _c2.setValue( _Psi[i+1] );
+            
+            // Psi[i] = (alpha * A) + (beta * B)
+            _c3.setValue( _alpha );
+            _c3.multiply( _c1 );
+            _c4.setValue( _beta );
+            _c4.multiply( _c2 );
+            _Psi[i].setValue( _c3 );
+            _Psi[i].add( _c4 );
+            
+            // Psi[i+1] = (alpha * B) + (beta * A)
+            _c3.setValue( _alpha );
+            _c3.multiply( _c2 );
+            _c4.setValue( _beta );
+            _c4.multiply( _c1 );
+            _Psi[i+1].setValue( _c3 );
+            _Psi[i+1].add( _c4 );
         }
 
+        // Starting at 1...
         for ( int i = 1; i < numberOfPoints - 1; i += 2 ) {
-            propogate( i, i + 1 );
+            // A = Psi[i]
+            _c1.setValue( _Psi[i] );
+            
+            // B = Psi[i+1]
+            _c2.setValue( _Psi[i+1] );
+            
+            // Psi[i] = (alpha * A) + (beta * B)
+            _c3.setValue( _alpha );
+            _c3.multiply( _c1 );
+            _c4.setValue( _beta );
+            _c4.multiply( _c2 );
+            _Psi[i].setValue( _c3 );
+            _Psi[i].add( _c4 );
+            
+            // Psi[i+1] = (alpha * B) + (beta * A)
+            _c3.setValue( _alpha );
+            _c3.multiply( _c2 );
+            _c4.setValue( _beta );
+            _c4.multiply( _c1 );
+            _Psi[i+1].setValue( _c3 );
+            _Psi[i+1].add( _c4 );
         }
+        
+        // Wrap around...
+        {
+            int i = numberOfPoints - 1;
+            int j = 0;
+            
+            // A = Psi[i]
+            _c1.setValue( _Psi[i] );
 
-        propogate( numberOfPoints - 1, 0 );
+            // B = Psi[j]
+            _c2.setValue( _Psi[j] );
 
+            // Psi[i] = (alpha * A) + (beta * B)
+            _c3.setValue( _alpha );
+            _c3.multiply( _c1 );
+            _c4.setValue( _beta );
+            _c4.multiply( _c2 );
+            _Psi[i].setValue( _c3 );
+            _Psi[i].add( _c4 );
+
+            // Psi[j] = (alpha * B) + (beta * A)
+            _c3.setValue( _alpha );
+            _c3.multiply( _c2 );
+            _c4.setValue( _beta );
+            _c4.multiply( _c1 );
+            _Psi[j].setValue( _c3 );
+            _Psi[j].add( _c4 );
+        }
+        
+        // Apply propagator values...
         for ( int i = 0; i < numberOfPoints; i++ ) {
             _Psi[i].multiply( _EtoV[i] );
         }
 
-        // now do what we did above, but in the reverse order...
+        // Now do what we did above, but in the reverse order...
         
-        propogate( numberOfPoints - 1, 0 );
+        // Wrap around...
+        {
+            int i = numberOfPoints - 1;
+            int j = 0;
+            
+            // A = Psi[i]
+            _c1.setValue( _Psi[i] );
 
+            // B = Psi[j]
+            _c2.setValue( _Psi[j] );
+
+            // Psi[i] = (alpha * A) + (beta * B)
+            _c3.setValue( _alpha );
+            _c3.multiply( _c1 );
+            _c4.setValue( _beta );
+            _c4.multiply( _c2 );
+            _Psi[i].setValue( _c3 );
+            _Psi[i].add( _c4 );
+
+            // Psi[j] = (alpha * B) + (beta * A)
+            _c3.setValue( _alpha );
+            _c3.multiply( _c2 );
+            _c4.setValue( _beta );
+            _c4.multiply( _c1 );
+            _Psi[j].setValue( _c3 );
+            _Psi[j].add( _c4 );
+        }
+
+        // Starting at 1...
         for ( int i = 1; i < numberOfPoints - 1; i += 2 ) {
-            propogate( i, i + 1 );
+            // A = Psi[i]
+            _c1.setValue( _Psi[i] );
+            
+            // B = Psi[i+1]
+            _c2.setValue( _Psi[i+1] );
+            
+            // Psi[i] = (alpha * A) + (beta * B)
+            _c3.setValue( _alpha );
+            _c3.multiply( _c1 );
+            _c4.setValue( _beta );
+            _c4.multiply( _c2 );
+            _Psi[i].setValue( _c3 );
+            _Psi[i].add( _c4 );
+            
+            // Psi[i+1] = (alpha * B) + (beta * A)
+            _c3.setValue( _alpha );
+            _c3.multiply( _c2 );
+            _c4.setValue( _beta );
+            _c4.multiply( _c1 );
+            _Psi[i+1].setValue( _c3 );
+            _Psi[i+1].add( _c4 );
         }
 
+        // Starting at 0...
         for ( int i = 0; i < numberOfPoints - 1; i += 2 ) {
-            propogate( i, i + 1 );
+            // A = Psi[i]
+            _c1.setValue( _Psi[i] );
+            
+            // B = Psi[i+1]
+            _c2.setValue( _Psi[i+1] );
+            
+            // Psi[i] = (alpha * A) + (beta * B)
+            _c3.setValue( _alpha );
+            _c3.multiply( _c1 );
+            _c4.setValue( _beta );
+            _c4.multiply( _c2 );
+            _Psi[i].setValue( _c3 );
+            _Psi[i].add( _c4 );
+            
+            // Psi[i+1] = (alpha * B) + (beta * A)
+            _c3.setValue( _alpha );
+            _c3.multiply( _c2 );
+            _c4.setValue( _beta );
+            _c4.multiply( _c1 );
+            _Psi[i+1].setValue( _c3 );
+            _Psi[i+1].add( _c4 );
         }
 
-        // apply damping to prevent periodic boundary condition...
-        damp();
-    }
-    
-    /*
-     * Performs propogation on two of the Psi entries.
-     * 
-     * @param i
-     * @param j
-     */
-    private void propogate( int i, int j ) {
-        // A = Psi[i]
-        _c1.setValue( _Psi[i] );
-        
-        // B = Psi[j]
-        _c2.setValue( _Psi[j] );
-        
-        // Psi[i] = (alpha * A) + (beta * B)
-        _c3.setValue( _alpha );
-        _c3.multiply( _c1 );
-        _c4.setValue( _beta );
-        _c4.multiply( _c2 );
-        _Psi[i].setValue( _c3 );
-        _Psi[i].add( _c4 );
-        
-        // Psi[j] = (alpha * B) + (beta * A)
-        _c3.setValue( _alpha );
-        _c3.multiply( _c2 );
-        _c4.setValue( _beta );
-        _c4.multiply( _c1 );
-        _Psi[j].setValue( _c3 );
-        _Psi[j].add( _c4 );
-    }
-    
-    /*
-     * Damps the values near the min and max positions
-     * to prevent periodic boundary conditions.
-     * Otherwise, the wave will appear to exit from one
-     * edge of the display and enter on the other edge.
-     */
-    private void damp() {
+        /*
+         * Damps the values near the min and max positions
+         * to prevent periodic boundary conditions.
+         * Otherwise, the wave will appear to exit from one
+         * edge of the display and enter on the other edge.
+         */
         if ( _Psi.length < DAMPING_FACTORS.length ) {
             return;
         }
@@ -315,5 +421,63 @@ public class RichardsonSolver {
         }
         
         return isZero;
+    }
+     
+    /**
+     * RComplex provides the bare minimum of complex number functionality
+     * needed by the Richardson algorithm. The goal is to minimize method
+     * calls that occur in the Richardson propagate method.
+     */
+    public static class RComplex {
+        
+        private double _real;
+        private double _imaginary;
+        
+        public RComplex() {
+            this( 0, 0 );
+        }
+        
+        public RComplex( double real, double imaginary ) {
+            _real = real;
+            _imaginary = imaginary;
+        }
+        
+        public double getReal() {
+            return _real;
+        }
+        
+        public double getImaginary() {
+            return _imaginary;
+        }
+        
+        public void setValue( RComplex c ) {
+            _real = c._real;
+            _imaginary = c._imaginary;
+        }
+        
+        public void add( RComplex c ) {
+            _real += c._real;
+            _imaginary += c._imaginary;
+        }
+        
+        public void multiply( RComplex c ) {
+            double newReal = _real * c._real - _imaginary * c._imaginary;
+            double newImaginary = _real * c._imaginary + _imaginary * c._real;
+            _real = newReal;
+            _imaginary = newImaginary;
+        }
+        
+        public void scale( double scale ) {
+            _real *= scale;
+            _imaginary *= scale;
+        }
+        
+        public double getAbs() {
+            return ( Math.sqrt( ( _real * _real ) + ( _imaginary * _imaginary ) ) );
+        }
+        
+        public double getPhase() {
+            return Math.atan2( _imaginary, _real );
+        }
     }
 }
