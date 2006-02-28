639a1bfcb18ca270909a423f184edbd12bc06115
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-02-28 20:38:37 +0000

    added initial version of t_d_quant

---
+++ schrodinger/benfold/source/EditableWells.java
@@ -0,0 +1,11 @@
+class EditableWells implements Function {
+    public EditableWells() {
+    }
+
+    public double evaluate( double x ) {
+        return x;
+    }
+
+    public void update( double x, double y, boolean mousePressed ) {
+    }
+}
