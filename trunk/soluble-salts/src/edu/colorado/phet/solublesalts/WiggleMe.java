56fac6c9b7baa3375acba359f445b9cb1d371c71
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-03-07 22:15:58 +0000

    no message

--- trunk/soluble-salts/src/edu/colorado/phet/solublesalts/WiggleMe.java
+++ trunk/soluble-salts/src/edu/colorado/phet/solublesalts/WiggleMe.java
@@ -52,7 +52,7 @@ public class WiggleMe extends PNode implements ModelElement {
     public void stepInTime( double dt ) {
         double dy = 0;
         if( direction == DOWN ) {
-            dy = 10;
+            dy = 15;
         }
         else if( direction == UP ) {
             dy = -5;
