d71e9696cbe3b90b1a8f540e06efa30eb05f0de4
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-03-10 00:44:13 +0000

    added bug test

---
+++ branches/phettabbedpane-03-08-2006/piccolo-phet/src/edu/colorado/phet/piccolo/TestTabGraphic.java
@@ -0,0 +1,43 @@
+package edu.colorado.phet.piccolo;
+
+import edu.umd.cs.piccolo.PCanvas;
+import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.nodes.PPath;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: reids
+ * Date: Mar 9, 2006
+ * Time: 5:21:00 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestTabGraphic {
+    public static void main( String[] args ) {
+        JFrame frame = new JFrame();
+        PCanvas pCanvas = new PCanvas();
+//        BrokenTab tabNode = new BrokenTab( );
+        PPath tabNode=new PPath( new Rectangle( 20,20));
+        pCanvas.getLayer().addChild( tabNode );
+        frame.setContentPane( pCanvas );
+        frame.setSize( 200, 200 );
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+
+        Color selectedTabColor = Color.blue;
+        GradientPaint paint = new GradientPaint( 0, (float)tabNode.getFullBounds().getY() - 2, selectedTabColor.brighter(), 0, (float)( tabNode.getFullBounds().getY() + 6 ), selectedTabColor );
+        tabNode.setPaint( paint );
+
+        frame.setVisible( true );
+    }
+
+    static class BrokenTab extends PNode {
+        PPath background;
+
+        public BrokenTab() {
+            background = new PPath( new Rectangle( 0, 0, 20, 20 ) );
+            addChild( background );
+        }
+    }
+}
