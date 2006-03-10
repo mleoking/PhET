6041485570948c4f48fda21649790d9b3f599869
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-03-10 00:52:15 +0000

    added bug test

--- branches/phettabbedpane-03-08-2006/piccolo-phet/src/edu/colorado/phet/piccolo/TestTabGraphic.java
+++ branches/phettabbedpane-03-08-2006/piccolo-phet/src/edu/colorado/phet/piccolo/TestTabGraphic.java
@@ -1,7 +1,6 @@
 package edu.colorado.phet.piccolo;
 
 import edu.umd.cs.piccolo.PCanvas;
-import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.nodes.PPath;
 
 import javax.swing.*;
@@ -18,20 +17,14 @@ public class TestTabGraphic {
     public static void main( String[] args ) {
         JFrame frame = new JFrame();
         PCanvas pCanvas = new PCanvas();
-        PPath tabNode=new PPath( new Rectangle( 20,20));
+
+        PPath tabNode = new PPath( new Rectangle( 20, 20 ) );
+        tabNode.setPaint( new GradientPaint( 0, 0, Color.blue, 0, 10, Color.blue ) );
+
         pCanvas.getLayer().addChild( tabNode );
         frame.setContentPane( pCanvas );
         frame.setSize( 200, 200 );
         frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-
-        Color selectedTabColor = Color.blue;
-
-//        float y1 = (float)tabNode.getFullBounds().getY() - 2;
-//        float y2 = (float)( tabNode.getFullBounds().getY() + 6 );
-        float y1=-2;
-        float y2= 6;
-        GradientPaint paint = new GradientPaint( 0, y1, selectedTabColor.brighter(), 0, y2, selectedTabColor );
-        tabNode.setPaint( paint );
 
         frame.setVisible( true );
     }
