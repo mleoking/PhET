76e180c3ad01adbee877102ba8205181fa12a59f
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-03-15 23:46:57 +0000

    Removed text box from "screen brightness" control. Increased window width by 12 pixels to ensure 2-lasers controls fits onscreen. Made the gun controls "velocity" label color white, a bit larger and easier to see.

--- trunk/schrodinger/src/edu/colorado/phet/qm/controls/DetectorSheetControlPanel.java
+++ trunk/schrodinger/src/edu/colorado/phet/qm/controls/DetectorSheetControlPanel.java
@@ -54,6 +54,7 @@ public class DetectorSheetControlPanel extends VerticalLayoutPanel {
         } );
 
         brightnessModelSlider = new ModelSlider( "Screen Brightness", "", 0, 1.0, 0.2, new DecimalFormat( "0.0" ) );
+        brightnessModelSlider.setTextFieldVisible( false );
         brightnessModelSlider.setModelTicks( new double[]{0, 0.25, 0.5, 0.75, 1.0} );
         this.brightnessModelSlider.addChangeListener( new ChangeListener() {
             public void stateChanged( ChangeEvent e ) {
@@ -98,12 +99,12 @@ public class DetectorSheetControlPanel extends VerticalLayoutPanel {
         displayPanel.add( showAverage );
         displayPanel.add( showHits );
 
-        HorizontalLayoutPanel saveClear = new HorizontalLayoutPanel();
-        saveClear.setBorder( BorderFactory.createTitledBorder( "Screen" ) );
-        saveClear.add( fadeCheckbox );
-        saveClear.add( clearButton );
-        saveClear.add( saveScreenJButton );
-        add( saveClear );
+        HorizontalLayoutPanel screenPanel = new HorizontalLayoutPanel();
+        screenPanel.setBorder( BorderFactory.createTitledBorder( "Screen" ) );
+        screenPanel.add( fadeCheckbox );
+        screenPanel.add( clearButton );
+        screenPanel.add( saveScreenJButton );
+        add( screenPanel );
         add( brightnessModelSlider );
         add( displayPanel );
 //        saveClear.setForeground( Color.blue);
