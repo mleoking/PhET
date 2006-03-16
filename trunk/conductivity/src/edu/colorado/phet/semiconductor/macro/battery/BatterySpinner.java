3dcbe8a707b2acdf84fe9dbcca7ef0307ed97b18
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-03-16 16:56:43 +0000

    Added an error message when typing an invalid voltage.

--- trunk/conductivity/src/edu/colorado/phet/semiconductor/macro/battery/BatterySpinner.java
+++ trunk/conductivity/src/edu/colorado/phet/semiconductor/macro/battery/BatterySpinner.java
@@ -11,16 +11,41 @@ import javax.swing.border.TitledBorder;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 // Referenced classes of package edu.colorado.phet.semiconductor.macro.battery:
 //            Battery
 
 public class BatterySpinner {
+    private double lastValue;
+    JSpinner spinner;
+    double min;
+    double max;
 
     public BatterySpinner( final Battery battery ) {
         min = 0.0D;
         max = 2D;
-        spinner = new JSpinner( new SpinnerNumberModel( battery.getVoltage(), min, max, 0.10000000000000001D ) );
+        spinner = new JSpinner( new SpinnerNumberModel( battery.getVoltage(), Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, 0.10000000000000001D ) );
+
+        final JTextField tf = ( (JSpinner.DefaultEditor)spinner.getEditor() ).getTextField();
+        tf.addKeyListener( new KeyAdapter() {
+            public void keyPressed( KeyEvent ke ) {
+                if( ke.getKeyCode() == KeyEvent.VK_ENTER ) {
+                    try {
+                        double value = Double.parseDouble( tf.getText() );
+                        if( value < min || value > max ) {
+                            showErrorMessage();
+                        }
+                    }
+                    catch( RuntimeException e ) {
+                        e.printStackTrace();
+                        showErrorMessage();
+                    }
+                }
+            }
+        } );
+
         TitledBorder titledborder = BorderFactory.createTitledBorder( SimStrings.get( "BatterySpinner.BorderTitle" ) );
         titledborder.setTitleFont( new Font( "Lucida Sans", 0, 18 ) );
         spinner.setBorder( titledborder );
@@ -28,20 +53,32 @@ public class BatterySpinner {
         spinner.addChangeListener( new ChangeListener() {
 
             public void stateChanged( ChangeEvent changeevent ) {
-                Object obj = spinner.getValue();
-                Double double1 = (Double)obj;
-                double d = double1.doubleValue();
-                battery.setVoltage( d );
+                double d = getSpinnerValue();
+                if( d >= min && d <= max ) {
+                    battery.setVoltage( d );
+                    lastValue = d;
+                }
+                else {
+                    spinner.setValue( new Double( lastValue ) );
+                }
             }
 
         } );
+
+    }
+
+    private void showErrorMessage() {
+        JOptionPane.showMessageDialog( spinner, "Value out of range: Voltage should be betwen 0 and 2 Volts.", "Invalid Voltage", JOptionPane.ERROR_MESSAGE );
+    }
+
+    private double getSpinnerValue() {
+        Object obj = spinner.getValue();
+        Double double1 = (Double)obj;
+        return double1.doubleValue();
     }
 
     public JSpinner getSpinner() {
         return spinner;
     }
 
-    JSpinner spinner;
-    double min;
-    double max;
 }
