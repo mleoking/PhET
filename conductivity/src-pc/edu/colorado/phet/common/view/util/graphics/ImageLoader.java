300ed0a69c956c149b7a97cd7706607207bb5541
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-03-17 16:43:29 +0000

    converted image loader

--- conductivity/src-pc/edu/colorado/phet/common/view/util/graphics/ImageLoader.java
+++ conductivity/src-pc/edu/colorado/phet/common/view/util/graphics/ImageLoader.java
@@ -1,7 +1,16 @@
-/*Copyright, Sam Reid, 2003.*/
+/* Copyright 2003-2004, University of Colorado */
+
+/*
+ * CVS Info -
+ * Filename : $Source$
+ * Branch : $Name$
+ * Modified by : $Author$
+ * Revision : $Revision$
+ * Date modified : $Date$
+ */
 package edu.colorado.phet.common.view.util.graphics;
 
-import edu.colorado.phet.common.view.util.GraphicsUtil;
+import edu.colorado.phet.semiconductor.common.ImageDebugger;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
@@ -11,15 +20,17 @@ import java.io.IOException;
 import java.net.URL;
 
 /**
- * User: Sam Reid
- * Date: Apr 16, 2003
- * Time: 9:34:20 AM
- * Copyright (c) Apr 16, 2003 by Sam Reid
+ * Utility class for loading images.
+ * <p/>
+ * Copyright (c) Apr 16, 2003 by University of Colorado, PhET
  * This level of granularity is required because different loaders return different
  * pixel data, and the final result is highly dependent on that
  * and the conversion mechanism.
  * <p/>
  * All combinations do not return the same results.
+ *
+ * @author ?
+ * @version $Revision$
  */
 public class ImageLoader {
 
@@ -37,11 +48,15 @@ public class ImageLoader {
         return instance.loadImage( str );
     }
 
+    public static BufferedImage loadBufferedImage( URL url ) throws IOException {
+        return instance.loadImage( url );
+    }
+
     public ImageLoader() {
-        setImageIOLoader();
+        setPhetLoader();
         conversionStrategy = new ConversionStrategy() {
             public BufferedImage toBufferedImage( Image image ) {
-                return GraphicsUtil.toBufferedImage( image );
+                return BufferedImageUtils.toBufferedImage( image );
             }
         };
     }
@@ -51,13 +66,25 @@ public class ImageLoader {
      * using the current conversion strategy.
      *
      * @param name
-     * @return
+     * @return the loaded image.
      */
     public BufferedImage loadImage( String name ) throws IOException {
         ClassLoader cl = this.getClass().getClassLoader();
         URL imageUrl = cl.getResource( name );
-        Image image = loadStrategy.loadImage( imageUrl );
+        if( imageUrl == null ) {
+            throw new IOException( "Null image URL for resource name=" + name );
+        }
+        return loadImage( imageUrl );
+    }
+
+    public BufferedImage loadImage( URL imageURL ) throws IOException {
+        if( imageURL == null ) {
+            throw new IOException( "Null image URL." );
+        }
+        Image image = loadStrategy.loadImage( imageURL );
+        ImageDebugger.show( "imload", image );
         BufferedImage buffy = conversionStrategy.toBufferedImage( image );
+        ImageDebugger.show( "buf", buffy );
         return buffy;
     }
 
@@ -67,6 +94,28 @@ public class ImageLoader {
 
     public void setPhetLoader() {
         this.loadStrategy = new PhetResourceLoader();
+    }
+
+    public void setToolkitLoader() {
+        this.loadStrategy = new ToolkitLoader();
+    }
+
+    /**
+     * So you can add your own strategy.
+     */
+    public void setLoadStrategy( LoadStrategy loadStrategy ) {
+        this.loadStrategy = loadStrategy;
+    }
+
+    /**
+     * So you can add your own strategy.
+     */
+    public void setConversionStrategy( ConversionStrategy conversionStrategy ) {
+        this.conversionStrategy = conversionStrategy;
+    }
+
+    public void setIconLoader() {
+        this.loadStrategy = new IconLoader();
     }
 
     static interface LoadStrategy {
@@ -83,14 +132,27 @@ public class ImageLoader {
         }
     }
 
+    static class IconLoader implements LoadStrategy {
+        public Image loadImage( URL location ) throws IOException {
+            ImageIcon ic = new ImageIcon( location );
+            return ic.getImage();
+        }
+
+    }
+
+    static class ToolkitLoader implements LoadStrategy {
+        public Image loadImage( URL location ) throws IOException {
+            return Toolkit.getDefaultToolkit().getImage( location );
+        }
+    }
+
     static class PhetResourceLoader implements LoadStrategy {
         private static class ResourceLoader extends Container {
             public Image fetchImage( URL imageLocation ) throws IOException {
                 Image image = null;
                 try {
                     if( imageLocation == null ) {
-                        System.out.println( "Image resource not found: " + imageLocation );
-                        throw new IOException( "Image resource not found: " + imageLocation );
+                        throw new IOException( "Image resource not found: Null imagelocation URL" );
                     }
                     else {
                         Toolkit toolkit = Toolkit.getDefaultToolkit();
@@ -107,6 +169,9 @@ public class ImageLoader {
         }
 
         public Image loadImage( URL location ) throws IOException {
+            if( location == null ) {
+                throw new IOException( "Null URL Location" );
+            }
             Image im = null;
             try {
                 ResourceLoader r = new ResourceLoader();
@@ -119,4 +184,6 @@ public class ImageLoader {
 
         }
     }
+
+
 }
