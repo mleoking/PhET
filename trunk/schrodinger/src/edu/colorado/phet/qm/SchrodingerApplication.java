4a271667de545da3f863a61298626750b1119de6
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-03-20 20:07:55 +0000

    Ensure the tabs appear in the z-order in which they are listed, rather than dependent on time-sequence of selection.

--- trunk/schrodinger/src/edu/colorado/phet/qm/SchrodingerApplication.java
+++ trunk/schrodinger/src/edu/colorado/phet/qm/SchrodingerApplication.java
@@ -36,7 +36,7 @@ import java.io.IOException;
 public class SchrodingerApplication extends PhetApplication {
     public static String TITLE = "Quantum Wave Interference";
     public static String DESCRIPTION = "Quantum Wave Interference";
-    public static String VERSION = "0.48";
+    public static String VERSION = "0.49";
     private String[] args;
     private IntensityModule intensityModule;
 
