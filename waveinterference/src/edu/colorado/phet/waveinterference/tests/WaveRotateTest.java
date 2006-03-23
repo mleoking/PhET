f91cb3937fc2a2cdbbdcc0d7e4ecdc5bce6ed455
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-03-23 07:00:44 +0000

    Added view rotation test.

--- waveinterference/src/edu/colorado/phet/waveinterference/tests/WaveRotateTest.java
+++ waveinterference/src/edu/colorado/phet/waveinterference/tests/WaveRotateTest.java
@@ -3,12 +3,12 @@ package edu.colorado.phet.waveinterference.tests;
 
 import edu.colorado.phet.common.application.PhetApplication;
 import edu.colorado.phet.common.view.ModelSlider;
-import edu.colorado.phet.waveinterference.view.IndexColorMap;
-import edu.colorado.phet.waveinterference.view.SimpleWavefunctionGraphic;
-import edu.colorado.phet.waveinterference.view.WaveSideView;
+import edu.colorado.phet.waveinterference.view.*;
 
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 /**
  * User: Sam Reid
@@ -24,20 +24,24 @@ public class WaveRotateTest extends PhetApplication {
         addModule( new WaveRotateTestModule() );
     }
 
-    static class WaveRotateTestModule extends BasicWaveTestModule {
+    class WaveRotateTestModule extends BasicWaveTestModule {
         private WaveSideView waveSideView;
         private SimpleWavefunctionGraphic simpleWavefunctionGraphic;
+        private RotationGlyph rotationGlyph;
 
         public WaveRotateTestModule() {
             super( "Wave Rotate Test" );
 
-            waveSideView = new WaveSideView( getLattice() );
-            waveSideView.setOffset( 0, 300 );
+            waveSideView = new WaveSideViewFull( getLattice() );
+            waveSideView.setOffset( 75, 300 );
 
             simpleWavefunctionGraphic = new SimpleWavefunctionGraphic( super.getLattice(), 10, 10, new IndexColorMap( super.getLattice() ) );
-
-            super.getPhetPCanvas().addScreenChild( simpleWavefunctionGraphic );
+            simpleWavefunctionGraphic.setOffset( 75, 0 );
+            rotationGlyph = new RotationGlyph();
+            getPhetPCanvas().addScreenChild( rotationGlyph );
+            getPhetPCanvas().addScreenChild( simpleWavefunctionGraphic );
             getPhetPCanvas().addScreenChild( waveSideView );
+
             waveSideView.setSpaceBetweenCells( simpleWavefunctionGraphic.getCellDimensions().width );
 
             BasicWaveTestControlPanel controlPanel = new BasicWaveTestControlPanel( this );
@@ -49,9 +53,66 @@ public class WaveRotateTest extends PhetApplication {
                     simpleWavefunctionGraphic.setCellDimensions( dim, dim );
                 }
             } );
+            final ModelSlider rotate = new ModelSlider( "View Angle", "radians", 0, Math.PI / 2, 0 );
+            rotate.addChangeListener( new ChangeListener() {
+                public void stateChanged( ChangeEvent e ) {
+                    setRotation( rotate.getValue() );
+                }
+            } );
+            rotate.getSlider().addMouseListener( new MouseListener() {
+                public void mouseClicked( MouseEvent e ) {
+                }
+
+                public void mouseEntered( MouseEvent e ) {
+                }
+
+                public void mouseExited( MouseEvent e ) {
+                }
+
+                public void mousePressed( MouseEvent e ) {
+                    setGlyphVisible( true );
+                }
+
+                public void mouseReleased( MouseEvent e ) {
+                    setGlyphVisible( false );
+                }
+            } );
+
             controlPanel.addControl( cellDim );
+            controlPanel.addControl( rotate );
             setControlPanel( controlPanel );
             updateLocations();
+            setRotation( 0 );
+        }
+
+        void setGlyphVisible( boolean vis ) {
+            if( vis ) {
+                rotationGlyph.setPrimaryHeight( simpleWavefunctionGraphic.getFullBounds().getHeight() );
+                rotationGlyph.setPrimaryWidth( simpleWavefunctionGraphic.getFullBounds().getWidth() );
+                rotationGlyph.setOffset( 75, simpleWavefunctionGraphic.getFullBounds().getCenterY() - rotationGlyph.getFullBounds().getHeight() );
+            }
+            rotationGlyph.setVisible( vis );
+//            waveSideView.setVisible( !vis );
+//            simpleWavefunctionGraphic.setVisible( !vis );
+        }
+
+        private void setRotation( double value ) {
+            setGlyphVisible( true );
+            rotationGlyph.setAngle( value );
+            rotationGlyph.setOffset( 75, simpleWavefunctionGraphic.getFullBounds().getCenterY() - rotationGlyph.getSurfaceHeight() );
+            if( value == 0 ) {
+                waveSideView.setVisible( false );
+                simpleWavefunctionGraphic.setVisible( true );
+            }
+            else if( value >= Math.PI / 2 - 0.02 ) {
+                setGlyphVisible( false );
+                waveSideView.setVisible( true );
+                simpleWavefunctionGraphic.setVisible( false );
+            }
+            else {
+                waveSideView.setVisible( false );
+                simpleWavefunctionGraphic.setVisible( false );
+            }
         }
 
         private void updateLocations() {
---
+++ waveinterference/src/edu/colorado/phet/waveinterference/view/RotationGlyph.java
@@ -0,0 +1,98 @@
+/* Copyright 2004, Sam Reid */
+package edu.colorado.phet.waveinterference.view;
+
+import edu.colorado.phet.common.math.Function;
+import edu.colorado.phet.common.view.util.DoubleGeneralPath;
+import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.nodes.PPath;
+
+import java.awt.*;
+import java.awt.geom.Point2D;
+
+/**
+ * User: Sam Reid
+ * Date: Mar 22, 2006
+ * Time: 11:11:38 PM
+ * Copyright (c) Mar 22, 2006 by Sam Reid
+ */
+
+public class RotationGlyph extends PNode {
+    private PPath surface;
+    private double angle;
+    private double primaryHeight = 100;
+    private double primaryWidth = 100;
+    private double expansionWidth = 50;
+    private PPath depth;
+
+    public RotationGlyph() {
+        surface = new PPath();
+        surface.setPaint( new Color( 128, 128, 255 ) );
+        surface.setStroke( new BasicStroke( 3 ) );
+
+        depth = new PPath();
+        depth.setPaint( new Color( 100, 100, 255 ) );
+        depth.setStroke( new BasicStroke( 3 ) );
+        addChild( depth );
+        addChild( surface );
+
+        update();
+        setRotation( 0.0 );
+    }
+
+    public double getPrimaryHeight() {
+        return primaryHeight;
+    }
+
+    public void setPrimaryHeight( double primaryHeight ) {
+        this.primaryHeight = primaryHeight;
+        update();
+    }
+
+    public double getPrimaryWidth() {
+        return primaryWidth;
+    }
+
+    public void setPrimaryWidth( double primaryWidth ) {
+        this.primaryWidth = primaryWidth;
+        update();
+    }
+
+    public void setAngle( double angle ) {
+        this.angle = angle;
+        assert angle >= 0 && angle <= Math.PI;
+        update();
+    }
+
+    public void update() {
+
+        Function.LinearFunction heightFunction = new Function.LinearFunction( 0, Math.PI / 2, 1, 0 );
+        Function.LinearFunction widthFunction = new Function.LinearFunction( 0, Math.PI / 2, 0, expansionWidth );
+        double sy = heightFunction.evaluate( angle );
+        double dx = widthFunction.evaluate( angle );
+        double h = primaryHeight * sy / 2;
+
+        DoubleGeneralPath generalPath = new DoubleGeneralPath();
+        generalPath.moveTo( dx, h );
+        generalPath.lineToRelative( primaryWidth - dx, 0 );
+        generalPath.lineToRelative( 2 * dx, h * 2 );
+        Point2D pin1 = generalPath.getCurrentPoint();
+        generalPath.lineToRelative( -primaryWidth - 4 * dx, 0 );
+        Point2D pin2 = generalPath.getCurrentPoint();
+        generalPath.lineTo( dx, h );
+        generalPath.closePath();
+        surface.setPathTo( generalPath.getGeneralPath() );
+
+        DoubleGeneralPath depthPath = new DoubleGeneralPath( pin1 );
+        depthPath.lineToRelative( 0, 200 );
+        depthPath.lineToRelative( pin2.getX() - pin1.getX(), 0 );
+        depthPath.lineTo( pin2 );
+        depthPath.closePath();
+        depth.setPathTo( depthPath.getGeneralPath() );
+//        GeneralPath path = new GeneralPath();
+//        path.moveTo( 0, 0 );
+    }
+
+    public double getSurfaceHeight() {
+        return surface.getFullBounds().getHeight();
+    }
+}
