ac958fcc82463a3831e833a4f77ec569435ee037
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-03-27 16:15:27 +0000

    Added cabaility to set number format

--- trunk/phetcommon/src/edu/colorado/phet/common/view/OrderOfMagnitudeSpinner.java
+++ trunk/phetcommon/src/edu/colorado/phet/common/view/OrderOfMagnitudeSpinner.java
@@ -11,6 +11,7 @@
 package edu.colorado.phet.common.view;
 
 import javax.swing.*;
+import java.text.NumberFormat;
 
 /**
  * A subclass of JSpinner that moves up and down through orders of magnitude
@@ -20,10 +21,20 @@ import javax.swing.*;
  */
 public class OrderOfMagnitudeSpinner extends JSpinner {
 
-    public OrderOfMagnitudeSpinner( float minFactor, float maxFactor ) {
+    public OrderOfMagnitudeSpinner( double initValue, double minFactor, double maxFactor, String numberFormat ) {
+        this( initValue, minFactor, maxFactor );
+        JSpinner.NumberEditor numberEditor = new NumberEditor( this, numberFormat );
+        this.setEditor( numberEditor );
+    }
+
+    public OrderOfMagnitudeSpinner( double initValue, double minFactor, double maxFactor ) {
         super();
-        final SpinnerModel model = new OrderOfMagnitudeListModel( minFactor, maxFactor );
+        final SpinnerModel model = new OrderOfMagnitudeListModel( initValue, minFactor, maxFactor );
         setModel( model );
+    }
+
+    public OrderOfMagnitudeSpinner( double minFactor, double maxFactor ) {
+        this( 1, minFactor, maxFactor );
     }
 
     //
@@ -35,32 +46,32 @@ public class OrderOfMagnitudeSpinner extends JSpinner {
      */
     private class OrderOfMagnitudeListModel extends SpinnerNumberModel {
 
-        private float maxFactor;
-        private float minFactor;
+        private double maxFactor;
+        private double minFactor;
 
-        OrderOfMagnitudeListModel( float minFactor, float maxFactor ) {
-            super( 1.0, minFactor, maxFactor, 1 );
+        OrderOfMagnitudeListModel( double initValue,double minFactor, double maxFactor ) {
+            super( initValue, minFactor, maxFactor, 1 );
             this.maxFactor = maxFactor;
             this.minFactor = minFactor;
         }
 
         public Object getNextValue() {
             Double currValue = (Double)getValue();
-            if( currValue.floatValue() >= maxFactor ) {
+            if( currValue.doubleValue() >= maxFactor ) {
                 return currValue;
             }
             else {
-                return new Double( ( (Double)getValue() ).floatValue() * 10 );
+                return new Double( ( (Double)getValue() ).doubleValue() * 10 );
             }
         }
 
         public Object getPreviousValue() {
             Double currValue = (Double)getValue();
-            if( currValue.floatValue() <= minFactor ) {
+            if( currValue.doubleValue() <= minFactor ) {
                 return currValue;
             }
             else {
-                return new Double( ( (Double)getValue() ).floatValue() / 10 );
+                return new Double( ( (Double)getValue() ).doubleValue() / 10 );
             }
         }
     }
