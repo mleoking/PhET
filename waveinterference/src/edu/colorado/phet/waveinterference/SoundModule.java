ca1e4eb39b48efe7fadedd2b3d404786a0ebc972
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-03-28 19:39:57 +0000

    Added sound

--- waveinterference/src/edu/colorado/phet/waveinterference/SoundModule.java
+++ waveinterference/src/edu/colorado/phet/waveinterference/SoundModule.java
@@ -1,15 +1,82 @@
 /* Copyright 2004, Sam Reid */
 package edu.colorado.phet.waveinterference;
 
+import edu.colorado.phet.waveinterference.model.Oscillator;
+import edu.colorado.phet.waveinterference.model.SlitPotential;
+import edu.colorado.phet.waveinterference.model.WaveModel;
+import edu.colorado.phet.waveinterference.tests.ModuleApplication;
+import edu.colorado.phet.waveinterference.view.*;
+
 /**
  * User: Sam Reid
  * Date: Mar 21, 2006
- * Time: 11:12:01 PM
+ * Time: 11:07:30 PM
  * Copyright (c) Mar 21, 2006 by Sam Reid
  */
 
 public class SoundModule extends WaveInterferenceModule {
+    private SoundSimulationPanel soundSimulationPanel;
+    private WaveInterferenceModel waveInterferenceModel;
+    private SoundControlPanel waterControlPanel;
+
     public SoundModule() {
         super( "Sound" );
+        waveInterferenceModel = new WaveInterferenceModel();
+        soundSimulationPanel = new SoundSimulationPanel( this );
+        waterControlPanel = new SoundControlPanel( this );
+
+        addModelElement( waveInterferenceModel );
+        addModelElement( soundSimulationPanel );
+
+        setSimulationPanel( soundSimulationPanel );
+        setControlPanel( waterControlPanel );
+    }
+
+    public WaveModel getWaveModel() {
+        return waveInterferenceModel.getWaveModel();
+    }
+
+    public SlitPotential getSlitPotential() {
+        return waveInterferenceModel.getSlitPotential();
+    }
+
+    public Oscillator getSecondaryOscillator() {
+        return waveInterferenceModel.getSecondaryOscillator();
+    }
+
+    public Oscillator getPrimaryOscillator() {
+        return waveInterferenceModel.getPrimaryOscillator();
+    }
+
+    public IntensityReaderSet getIntensityReaderSet() {
+        return soundSimulationPanel.getIntensityReaderSet();
+    }
+
+    public MeasurementToolSet getMeasurementToolSet() {
+        return soundSimulationPanel.getMeasurementToolSet();
+    }
+
+    public FaucetGraphic getPrimaryFaucetGraphic() {
+        return soundSimulationPanel.getPrimaryFaucetGraphic();
+    }
+
+    public SoundSimulationPanel getSoundSimulationPanel() {
+        return soundSimulationPanel;
+    }
+
+    public RotationWaveGraphic getRotationWaveGraphic() {
+        return soundSimulationPanel.getRotationWaveGraphic();
+    }
+
+    public MultiDrip getMultiDrip() {
+        return soundSimulationPanel.getMultiDrip();
+    }
+
+    public LatticeScreenCoordinates getLatticeScreenCoordinates() {
+        return soundSimulationPanel.getLatticeScreenCoordinates();
+    }
+
+    public static void main( String[] args ) {
+        new ModuleApplication().startApplication( args, new SoundModule() );
     }
 }
---
+++ waveinterference/src/edu/colorado/phet/waveinterference/SoundSimulationPanel.java
@@ -0,0 +1,110 @@
+/* Copyright 2004, Sam Reid */
+package edu.colorado.phet.waveinterference;
+
+import edu.colorado.phet.common.model.ModelElement;
+import edu.colorado.phet.waveinterference.model.Lattice2D;
+import edu.colorado.phet.waveinterference.model.WaveModel;
+import edu.colorado.phet.waveinterference.view.*;
+
+/**
+ * User: Sam Reid
+ * Date: Mar 26, 2006
+ * Time: 5:31:39 PM
+ * Copyright (c) Mar 26, 2006 by Sam Reid
+ */
+
+public class SoundSimulationPanel extends WaveInterferenceCanvas implements ModelElement {
+    private SoundModule soundModule;
+    private RotationWaveGraphic rotationWaveGraphic;
+    private IntensityReaderSet intensityReaderSet;
+    private SlitPotentialGraphic slitPotentialGraphic;
+    private MeasurementToolSet measurementToolSet;
+    private FaucetGraphic primaryFaucetGraphic;
+    private FaucetGraphic secondaryFaucetGraphic;
+    private MultiDrip multiDrip;
+
+    public SoundSimulationPanel( SoundModule soundModule ) {
+        this.soundModule = soundModule;
+
+        WaveModelGraphic waveModelGraphic = new WaveModelGraphic( getWaveModel(), 8, 8, new IndexColorMap( getLattice() ) );
+        WaveSideViewFull waveSideView = new WaveSideViewFull( getLattice(), waveModelGraphic.getLatticeScreenCoordinates() );
+        RotationGlyph rotationGlyph = new RotationGlyph();
+        rotationWaveGraphic = new RotationWaveGraphic( waveModelGraphic, waveSideView, rotationGlyph );
+        rotationWaveGraphic.setOffset( 300, 50 );
+        rotationWaveGraphic.addListener( new RotationWaveGraphic.Listener() {
+            public void rotationChanged() {
+                angleChanged();
+            }
+        } );
+        addScreenChild( rotationWaveGraphic );
+
+        primaryFaucetGraphic = new FaucetGraphic( getWaveModel(), soundModule.getPrimaryOscillator(), getLatticeScreenCoordinates() );
+        addScreenChild( primaryFaucetGraphic );
+
+        secondaryFaucetGraphic = new FaucetGraphic( getWaveModel(), soundModule.getSecondaryOscillator(), getLatticeScreenCoordinates() );
+        secondaryFaucetGraphic.setEnabled( false );
+        addScreenChild( secondaryFaucetGraphic );
+
+        slitPotentialGraphic = new SlitPotentialGraphic( soundModule.getSlitPotential(), getLatticeScreenCoordinates() );
+        addScreenChild( slitPotentialGraphic );
+
+        intensityReaderSet = new IntensityReaderSet();
+        addScreenChild( intensityReaderSet );
+
+        measurementToolSet = new MeasurementToolSet( this, soundModule.getClock() );
+        addScreenChild( measurementToolSet );
+
+        multiDrip = new MultiDrip( getWaveModel(), primaryFaucetGraphic, secondaryFaucetGraphic );
+
+        FaucetControlPanelPNode faucetControlPanelPNode = new FaucetControlPanelPNode( this, new FaucetControlPanel( soundModule.getPrimaryOscillator(), getPrimaryFaucetGraphic() ), getPrimaryFaucetGraphic(), waveModelGraphic );
+        addScreenChild( faucetControlPanelPNode );
+    }
+
+    private void angleChanged() {
+        if( rotationWaveGraphic.isTopView() ) {
+            slitPotentialGraphic.setVisible( true );
+        }
+        else {
+            slitPotentialGraphic.setVisible( false );
+        }
+    }
+
+    public MultiDrip getMultiDrip() {
+        return multiDrip;
+    }
+
+    private Lattice2D getLattice() {
+        return getWaveModel().getLattice();
+    }
+
+    private WaveModel getWaveModel() {
+        return soundModule.getWaveModel();
+    }
+
+    public RotationWaveGraphic getRotationWaveGraphic() {
+        return rotationWaveGraphic;
+    }
+
+    public LatticeScreenCoordinates getLatticeScreenCoordinates() {
+        return rotationWaveGraphic.getLatticeScreenCoordinates();
+    }
+
+    public IntensityReaderSet getIntensityReaderSet() {
+        return intensityReaderSet;
+    }
+
+    public MeasurementToolSet getMeasurementToolSet() {
+        return measurementToolSet;
+    }
+
+    public FaucetGraphic getPrimaryFaucetGraphic() {
+        return primaryFaucetGraphic;
+    }
+
+    public void stepInTime( double dt ) {
+        rotationWaveGraphic.update();
+        primaryFaucetGraphic.step();
+        secondaryFaucetGraphic.step();
+        intensityReaderSet.update();
+    }
+}
