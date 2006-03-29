f38a41e26138bb2d7f3885f61d3b6d226343e7af
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-03-29 01:48:34 +0000

    Added sound

---
+++ trunk/waveinterference/src/edu/colorado/phet/waveinterference/view/SpeakerGraphic.java
@@ -0,0 +1,39 @@
+/* Copyright 2004, Sam Reid */
+package edu.colorado.phet.waveinterference.view;
+
+import edu.colorado.phet.piccolo.util.PImageFactory;
+import edu.colorado.phet.waveinterference.model.Oscillator;
+import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.nodes.PImage;
+
+import java.awt.geom.Point2D;
+
+/**
+ * User: Sam Reid
+ * Date: Mar 28, 2006
+ * Time: 6:32:34 PM
+ * Copyright (c) Mar 28, 2006 by Sam Reid
+ */
+
+public class SpeakerGraphic extends PNode {
+    private Oscillator oscillator;
+    private LatticeScreenCoordinates latticeScreenCoordinates;
+
+    public SpeakerGraphic( Oscillator oscillator, LatticeScreenCoordinates latticeScreenCoordinates ) {
+        this.oscillator = oscillator;
+        this.latticeScreenCoordinates = latticeScreenCoordinates;
+        PImage image = PImageFactory.create( "images/speaker.gif" );
+        addChild( image );
+        latticeScreenCoordinates.addListener( new LatticeScreenCoordinates.Listener() {
+            public void mappingChanged() {
+                updateLocation();
+            }
+        } );
+        updateLocation();
+    }
+
+    private void updateLocation() {
+        Point2D coord = latticeScreenCoordinates.toScreenCoordinates( oscillator.getCenterX(), oscillator.getCenterY() );
+        setOffset( coord.getX() - getFullBounds().getWidth(), coord.getY() - getFullBounds().getHeight() / 2.0 );
+    }
+}
