58e7a7db85d713c703b34086107b7998586c0ff8
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-04-05 18:37:30 +0000

    sandbox

--- quantum-tunneling/src/edu/colorado/phet/quantumtunneling/QTVersion.java
+++ quantum-tunneling/src/edu/colorado/phet/quantumtunneling/QTVersion.java
@@ -20,11 +20,11 @@ package edu.colorado.phet.quantumtunneling;
  */
 public class QTVersion {
   
-//    public static final String NUMBER = "sandbox";
-//    public static final String CVS_TAG = "quantumtunneling-sandbox";
+    public static final String NUMBER = "sandbox";
+    public static final String CVS_TAG = "quantumtunneling-sandbox";
     
-    public static final String NUMBER = "0.00.23";
-    public static final String CVS_TAG = "quantumtunneling-0-00-23-2006-04-05";
+//    public static final String NUMBER = "0.00.23";
+//    public static final String CVS_TAG = "quantumtunneling-0-00-23-2006-04-05";
     
     /* This class is not intended for instantiation. */
     private QTVersion() {}
