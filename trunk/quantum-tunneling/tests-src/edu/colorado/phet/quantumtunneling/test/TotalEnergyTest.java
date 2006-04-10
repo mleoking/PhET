253199f15b38d9fb766f094c04b988e359690a58
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-04-10 23:00:22 +0000

    composite gradient and mask into a buffer so that it can then be composited on top of a chart with gridlines

--- trunk/quantum-tunneling/tests-src/edu/colorado/phet/quantumtunneling/test/TotalEnergyTest.java
+++ trunk/quantum-tunneling/tests-src/edu/colorado/phet/quantumtunneling/test/TotalEnergyTest.java
@@ -43,8 +43,12 @@ public class TotalEnergyTest {
     private static final Color EDGE_COLOR = new Color( 0, 255, 0, 0 ); // transparent green
     
     private static final Color BACKGROUND_COLOR = Color.BLACK;
+    private static final Color GRIDLINES_COLOR = Color.LIGHT_GRAY;
     private static final Paint TOP_PAINT = new GradientPaint( 0, 0, EDGE_COLOR, 0, H/2, CENTER_COLOR );
     private static final Paint BOTTOM_PAINT = new GradientPaint( 0, H, EDGE_COLOR, 0, H/2, CENTER_COLOR );
+    
+    public static final Stroke GRIDLINES_STROKE = 
+        new BasicStroke( 1f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[] {3,3}, 0 );
     
     private static final Rectangle2D BACKGROUND_RECT = new Rectangle2D.Double( 0, 0, W, H );
     private static final Rectangle2D TOP_RECT = new Rectangle2D.Double( 0, 0, W, H/2 );
@@ -88,15 +92,33 @@ public class TotalEnergyTest {
                 
                 // Apply the mask to the gradient band.
                 // The gradient shows through the opaque part of the mask.
+                BufferedImage compositeImage = new BufferedImage( W, H, BufferedImage.TYPE_INT_ARGB );
+                {
+                    Graphics2D g2 = compositeImage.createGraphics();
+                    AffineTransform xform = new AffineTransform();
+                    g2.setRenderingHint( RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR );
+                    g2.setPaint( new Color( 255, 0, 0, 0 ) ); // transparent
+                    g2.fill( BACKGROUND_RECT );
+                    g2.drawRenderedImage( bi, xform );
+                    g2.setComposite( AlphaComposite.DstIn );
+                    g2.drawRenderedImage( mask, xform );
+                }
+                
+                // Draw the composite image on top of a "chart" that has vertical gridlines...
                 {
                     Graphics2D g2 = (Graphics2D)g;
                     AffineTransform xform = new AffineTransform();
                     g2.setRenderingHint( RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR );
                     g2.setPaint( BACKGROUND_COLOR );
                     g2.fill( BACKGROUND_RECT );
-                    g2.drawRenderedImage( bi, xform );
-                    g2.setComposite( AlphaComposite.DstIn );
-                    g2.drawRenderedImage( mask, xform );
+                    g2.setPaint( GRIDLINES_COLOR );
+                    g2.setStroke( GRIDLINES_STROKE );
+                    final int numberOfGridlines = 10;
+                    for ( int i = 0; i < numberOfGridlines; i++ ) {
+                        int x = i * W / numberOfGridlines;
+                        g2.drawLine( x, 0, x, H );
+                    }
+                    g2.drawRenderedImage( compositeImage, xform );
                 }
             }
         };
