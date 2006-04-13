67f0faf1d4e3ba6a005ba3a20e28dd08b39ee7a1
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-04-13 06:39:18 +0000

    Tried to address the 'jitters' bug with manual double buffering.  It works, but introduces performance problems while interacting with the simulation.

---
+++ waveinterference/src/edu/colorado/phet/waveinterference/DoubleBufferedPhetPCanvas.java
@@ -0,0 +1,68 @@
+/* Copyright 2004, Sam Reid */
+package edu.colorado.phet.waveinterference;
+
+import edu.colorado.phet.piccolo.PhetPCanvas;
+
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+
+/**
+ * User: Sam Reid
+ * Date: Apr 13, 2006
+ * Time: 12:28:18 AM
+ * Copyright (c) Apr 13, 2006 by Sam Reid
+ */
+
+public class DoubleBufferedPhetPCanvas extends PhetPCanvas {
+    private BufferedImage buffer;
+    private boolean pressed;
+
+    public DoubleBufferedPhetPCanvas() {
+        addMouseListener( new MouseListener() {
+            public void mouseClicked( MouseEvent e ) {
+            }
+
+            public void mouseEntered( MouseEvent e ) {
+            }
+
+            public void mouseExited( MouseEvent e ) {
+            }
+
+            public void mousePressed( MouseEvent e ) {
+                pressed = true;
+            }
+
+            public void mouseReleased( MouseEvent e ) {
+                pressed = false;
+            }
+        } );
+    }
+
+    public void paintComponent( Graphics g ) {
+//        if( pressed ) {
+//            paintNormal( g );
+//        }
+//        else {
+        paintBuffered( g );
+//        }
+    }
+
+    private void paintNormal( Graphics g ) {
+        super.paintComponent( g );
+    }
+
+    private void paintBuffered( Graphics g ) {
+        synchronizeImage();
+        super.paintComponent( buffer.createGraphics() );
+        g.drawImage( buffer, 0, 0, null );
+    }
+
+    private void synchronizeImage() {
+        if( buffer == null || buffer.getWidth() != getWidth() || buffer.getHeight() != getHeight() ) {
+            buffer = new BufferedImage( getWidth(), getHeight(), BufferedImage.TYPE_INT_RGB );
+        }
+    }
+
+}
