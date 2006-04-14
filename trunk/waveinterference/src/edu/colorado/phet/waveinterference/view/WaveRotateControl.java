9d201f214a1563ccdafb3ed13ab6bbf2d00b94d4
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-04-14 17:20:02 +0000

    added getters for model range

--- trunk/waveinterference/src/edu/colorado/phet/waveinterference/view/WaveRotateControl.java
+++ trunk/waveinterference/src/edu/colorado/phet/waveinterference/view/WaveRotateControl.java
@@ -1,11 +1,13 @@
 /* Copyright 2004, Sam Reid */
 package edu.colorado.phet.waveinterference.view;
 
+import edu.colorado.phet.common.view.HorizontalLayoutPanel;
 import edu.colorado.phet.common.view.ModelSlider;
-import edu.colorado.phet.common.view.VerticalLayoutPanel;
 
+import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+import java.util.Hashtable;
 
 /**
  * User: Sam Reid
@@ -14,7 +16,7 @@ import javax.swing.event.ChangeListener;
  * Copyright (c) Mar 26, 2006 by Sam Reid
  */
 
-public class WaveRotateControl extends VerticalLayoutPanel {
+public class WaveRotateControl extends HorizontalLayoutPanel {
     public WaveRotateControl( final RotationWaveGraphic rotationWaveGraphic ) {
         final ModelSlider rotate = new ModelSlider( "View Angle", "radians", 0, Math.PI / 2, rotationWaveGraphic.getRotation() );
         rotate.addChangeListener( new ChangeListener() {
@@ -22,6 +24,43 @@ public class WaveRotateControl extends VerticalLayoutPanel {
                 rotationWaveGraphic.setViewAngle( rotate.getValue() );
             }
         } );
+        rotate.setPaintLabels( true );
+        Hashtable modelLabels = new Hashtable();
+        modelLabels.put( new Double( rotate.getMaximumModelValue() ), new JLabel( "Side" ) );
+        modelLabels.put( new Double( rotate.getMinimumModelValue() ), new JLabel( "Top" ) );
+        rotate.setPaintTicks( false );
+        rotate.setTextFieldVisible( false );
+        rotate.setModelLabels( modelLabels );
+
+        rotate.getSlider().addChangeListener( new ChangeListener() {
+            public void stateChanged( ChangeEvent e ) {
+                System.out.println( "rotate.getSlider().getValue() = " + rotate.getSlider().getValue() );
+                System.out.println( "WaveRotateControl.stateChanged" );
+                if( !rotate.getSlider().getValueIsAdjusting() ) {
+                    SwingUtilities.invokeLater( new Runnable() {
+                        public void run() {
+                            System.out.println( "WaveRotateControl.stateChangedVAJ" );
+                            double halfway = ( rotate.getMaximumModelValue() + rotate.getMinimumModelValue() ) / 2;
+                            if( rotate.getValue() < halfway ) {
+                                rotate.setValue( rotate.getMinimumModelValue() );
+                            }
+                            else {
+                                rotate.setValue( rotate.getMaximumModelValue() );
+                            }
+                        }
+                    } );
+                }
+            }
+        } );
+//        rotate.addMouseListener( new MouseAdapter() {
+//            public void mouseReleased( MouseEvent e ) {
+//                System.out.println( "WaveRotateControl.mouseReleased" );
+//            }
+//        } );
+//        add(Box.createRigidArea( new Dimension( 30,0)));
         add( rotate );
+//        add(Box.createRigidArea( new Dimension( 10,10 )));
+//        setMaximumSize( new Dimension( 50,500) );
+//        rotate.setPreferredSize( new Dimension( 75,getPreferredSize().height+3) );
     }
 }
