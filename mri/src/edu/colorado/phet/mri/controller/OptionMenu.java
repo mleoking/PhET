ed64c32db58f176b7c930e90829e882eb5af2066
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-04-14 17:51:02 +0000

    no message

---
+++ mri/src/edu/colorado/phet/mri/controller/OptionMenu.java
@@ -0,0 +1,26 @@
+/* Copyright 2003-2004, University of Colorado */
+
+/*
+ * CVS Info -
+ * Filename : $Source$
+ * Branch : $Name$
+ * Modified by : $Author$
+ * Revision : $Revision$
+ * Date modified : $Date$
+ */
+package edu.colorado.phet.mri.controller;
+
+import javax.swing.*;
+
+/**
+ * OptionMenu
+ *
+ * @author Ron LeMaster
+ * @version $Revision$
+ */
+public class OptionMenu extends JMenu {
+
+    public OptionMenu() {
+        super( "Options" );
+    }
+}
