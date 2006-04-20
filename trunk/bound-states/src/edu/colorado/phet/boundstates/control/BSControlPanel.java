55dc42aa056f8eb4d09c329eb11bd3b8d393d290
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-04-20 15:01:53 +0000

    (4/20/06) - bugfix: mass wasn't initialized properly, made harmonic oscillator look too wide

--- trunk/bound-states/src/edu/colorado/phet/boundstates/control/BSControlPanel.java
+++ trunk/bound-states/src/edu/colorado/phet/boundstates/control/BSControlPanel.java
@@ -277,7 +277,7 @@ public class BSControlPanel extends BSAbstractControlPanel {
             String massLabel = SimStrings.get( "label.particleMass" );
             String massUnits = "<html>m<sub>e</sub></html>";
             _massSlider = new SliderControl( 
-                    BSConstants.DEFAULT_MASS, BSConstants.MIN_MASS, BSConstants.MAX_MASS, 5,
+                    BSConstants.DEFAULT_MASS_MULTIPLIER, BSConstants.MIN_MASS_MULTIPLIER, BSConstants.MAX_MASS_MULTIPLIER, 5,
                     1, 1, massLabel, massUnits, 3, SLIDER_INSETS );
             _massSlider.setNotifyWhileDragging( NOTIFY_WHILE_DRAGGING );
             _massSlider.setTextEditable( true );
