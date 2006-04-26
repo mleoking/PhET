67274fd4cb556f949f139f90c4607c22d6a1c9e3
nobody <nobody@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-04-26 16:47:36 +0000

    This commit was manufactured by cvs2svn to create branch 'mri-phet-tabbed-pane-branch-v0r0104-5-4-2006'.

---
+++ branches/mri-phet-tabbed-pane-branch-v0r0104-5-4-2006/collision/src/edu/colorado/phet/collision/Collision.java
@@ -0,0 +1,11 @@
+/**
+ * Class: Collision
+ * Package: edu.colorado.phet.model.collision
+ * Author: Another Guy
+ * Date: Mar 24, 2003
+ */
+package edu.colorado.phet.collision;
+
+public interface Collision {
+    void collide();
+}
