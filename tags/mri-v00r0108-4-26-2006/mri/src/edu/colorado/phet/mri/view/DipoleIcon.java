583710a620fd7a7da0bafb11dca6e977d770f033
nobody <nobody@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-04-26 20:04:03 +0000

    This commit was manufactured by cvs2svn to create tag 'mri-v00r0108-4-26-2006'.

---
+++ tags/mri-v00r0108-4-26-2006/mri/src/edu/colorado/phet/mri/view/DipoleIcon.java
@@ -0,0 +1,28 @@
+/* Copyright 2003-2004, University of Colorado */
+
+/*
+ * CVS Info -
+ * Filename : $Source$
+ * Branch : $Name$
+ * Modified by : $Author$
+ * Revision : $Revision$
+ * Date modified : $Date$
+ */
+package edu.colorado.phet.mri.view;
+
+import edu.colorado.phet.mri.MriConfig;
+
+import javax.swing.*;
+
+/**
+ * DipoleIcon
+ *
+ * @author Ron LeMaster
+ * @version $Revision$
+ */
+public class DipoleIcon extends ImageIcon {
+
+    public DipoleIcon() {
+        super( MriConfig.DIPOLE_IMAGE );
+    }
+}
