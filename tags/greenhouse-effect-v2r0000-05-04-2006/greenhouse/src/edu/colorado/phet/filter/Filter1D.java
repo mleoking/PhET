b3de02a291fae2b489e4ef11b161caca4ed695a3
nobody <nobody@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-05-04 23:18:20 +0000

    This commit was manufactured by cvs2svn to create tag 'greenhouse-effect-v2r0000-05-04-2006'.

---
+++ tags/greenhouse-effect-v2r0000-05-04-2006/greenhouse/src/edu/colorado/phet/filter/Filter1D.java
@@ -0,0 +1,12 @@
+/**
+ * Class: Filter1D
+ * Class: edu.colorado.phet.filter
+ * User: Ron LeMaster
+ * Date: Oct 12, 2003
+ * Time: 4:51:43 PM
+ */
+package edu.colorado.phet.filter;
+
+public interface Filter1D {
+    boolean passes( double value );
+}
