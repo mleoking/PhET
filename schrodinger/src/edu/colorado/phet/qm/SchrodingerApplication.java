a40bbd68bf4c258e752647ab88ab68577e6c83f9
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-05-14 07:07:33 +0000

    update build scripts, post 0.00.62

--- schrodinger/src/edu/colorado/phet/qm/SchrodingerApplication.java
+++ schrodinger/src/edu/colorado/phet/qm/SchrodingerApplication.java
@@ -37,7 +37,7 @@ import java.lang.reflect.InvocationTargetException;
 public class SchrodingerApplication extends PhetApplication {
     public static String TITLE = "Quantum Wave Interference";
     public static String DESCRIPTION = "Quantum Wave Interference";
-    public static String VERSION = "0.00.61";
+    public static String VERSION = "0.00.62";
     private String[] args;
     private IntensityModule intensityModule;
 
