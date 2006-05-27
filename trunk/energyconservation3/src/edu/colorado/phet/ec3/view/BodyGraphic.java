67c971b004a551352d2adf8c34708a8d19747bd2
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-05-27 00:48:58 +0000

    added jet pack, transform test

--- trunk/energyconservation3/src/edu/colorado/phet/ec3/view/BodyGraphic.java
+++ trunk/energyconservation3/src/edu/colorado/phet/ec3/view/BodyGraphic.java
@@ -15,16 +15,11 @@ import edu.umd.cs.piccolo.nodes.PPath;
 import edu.umd.cs.piccolo.util.PBounds;
 import edu.umd.cs.piccolo.util.PDimension;
 
-import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.geom.AffineTransform;
-import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.util.ArrayList;
 
 /**
  * User: Sam Reid
@@ -37,14 +32,9 @@ public class BodyGraphic extends PNode {
     private Body body;
     private EC3Module ec3Module;
     private PPath boundsDebugPPath;
-    private ArrayList dragHistory = new ArrayList();
-    private Point2D mouseLocation;
-    private long t0 = System.currentTimeMillis();
-    private int numHistoryPointsForThrow = 5;
     private PImage skater;
     private PPath centerDebugger;
     private JetPackGraphic jetPackGraphic;
-//    private boolean debugCenter = false;
     private boolean debugCenter = true;
 
     public BodyGraphic( final EC3Module ec3Module, Body body ) {
@@ -52,14 +42,10 @@ public class BodyGraphic extends PNode {
         this.body = body;
         boundsDebugPPath = new PPath( body.getShape() );
         boundsDebugPPath.setStroke( null );
-//        boundsDebugPPath.setPaint( Color.blue );
         boundsDebugPPath.setPaint( new Color( 0, 0, 255, 128 ) );
         jetPackGraphic = new JetPackGraphic( this );
-
+        addChild( jetPackGraphic );
         try {
-//            BufferedImage image = ImageLoader.loadBufferedImage( "images/skater-67.png" );
-//            BufferedImage image = ImageLoader.loadBufferedImage( "images/skater-phet2_0032.gif" );
-//            BufferedImage image = ImageLoader.loadBufferedImage( "images/skater-phet2_0032-b.gif" );
             BufferedImage image = ImageLoader.loadBufferedImage( "images/skater3.png" );
             skater = new PImage( image );
             addChild( skater );
@@ -75,7 +61,7 @@ public class BodyGraphic extends PNode {
             e.printStackTrace();
         }
 
-        update();
+
         ec3Module.getModel().addModelElement( new ModelElement() {
             public void stepInTime( double dt ) {
                 update();
@@ -84,19 +70,13 @@ public class BodyGraphic extends PNode {
         addInputEventListener( new PBasicInputEventHandler() {
             public void mouseDragged( PInputEvent event ) {
                 PDimension delta = event.getDeltaRelativeTo( BodyGraphic.this );
-//                System.out.println( "delta = " + delta );
                 boolean okToTranslate = true;
                 if( getBody().getLocatedShape().getBounds2D().getMinY() < 0 && delta.getHeight() < 0 ) {
                     okToTranslate = false;
                 }
-//                if( getFullBounds().getMinX() < 0 && delta.getWidth() < 0 ) {
-//                    okToTranslate = false;
-//                }
                 PBounds b = getFullBounds();
                 localToGlobal( b );
                 ec3Module.getEnergyConservationCanvas().getLayer().globalToLocal( b );
-//                System.out.println( "b = " + b );
-//                System.out.println( "getFullBounds().getMaxX() = " + getFullBounds().getMaxX() +", canWidth="+ec3Module.getEnergyConservationCanvas().getWidth());
                 if( b.getMaxX() > ec3Module.getEnergyConservationCanvas().getWidth() && delta.getWidth() > 0 ) {
                     okToTranslate = false;
                 }
@@ -111,36 +91,6 @@ public class BodyGraphic extends PNode {
         } );
         addInputEventListener( new CursorHandler( Cursor.HAND_CURSOR ) );
 
-//        PActivity recordActivity = new PActivity( 0, 30, -1 ){
-//            protected void activityStep( long elapsedTime ) {
-//                super.activityStep( elapsedTime );
-//                addHistoryPoint();
-//            }
-//        };
-        final Timer timer = new Timer( 30, new ActionListener() {
-            public void actionPerformed( ActionEvent e ) {
-//                System.out.println( "BodyGraphic.actionPerformed" );
-                addHistoryPoint();
-            }
-        } );
-        addInputEventListener( new PBasicInputEventHandler() {
-            public void mousePressed( PInputEvent event ) {
-//                t0 = System.currentTimeMillis();
-                mouseLocation = event.getPositionRelativeTo( BodyGraphic.this );
-                dragHistory.clear();
-                timer.start();
-            }
-
-            public void mouseDragged( PInputEvent event ) {
-                mouseLocation = event.getPositionRelativeTo( BodyGraphic.this );
-                addHistoryPoint();
-            }
-
-            public void mouseReleased( PInputEvent event ) {
-                timer.stop();
-                throwObject();
-            }
-        } );
         addInputEventListener( new PBasicInputEventHandler() {
             public void mousePressed( PInputEvent event ) {
                 getBody().setUserControlled( true );
@@ -156,65 +106,8 @@ public class BodyGraphic extends PNode {
                 getBody().setVelocity( 0, 0 );
             }
         } );
-        addChild( jetPackGraphic );
-    }
 
-    private double getTime() {
-        return ( System.currentTimeMillis() - t0 ) / 1000.0;
-    }
-
-    private void throwObject() {
-//        XYSeries xSeries = new XYSeries( "x" );
-//        XYSeries ySeries = new XYSeries( "y" );
-//        XYSeriesCollection dataset = new XYSeriesCollection();
-//        dataset.addSeries( xSeries );
-//        dataset.addSeries( ySeries );
-//
-////        Regression.getOLSRegression()
-//        double currentTime = getTime();
-//        System.out.println( "currentTime = " + currentTime );
-//        List history = dragHistory.subList( dragHistory.size() - numHistoryPointsForThrow, dragHistory.size() );
-//
-//        for( int i = 0; i < history.size(); i++ ) {
-//            DataPoint dataPoint = (DataPoint)history.get( i );
-//            if( ( currentTime - dataPoint.time ) < 0.1 ) {
-//                xSeries.add( dataPoint.time, dataPoint.x );
-//                ySeries.add( dataPoint.time, dataPoint.y );
-//            }
-//        }
-//        System.out.println( "dragHistory = " + dragHistory );
-//        System.out.println( "history = " + history );
-//        System.out.println( "xSeries = " + toString( xSeries ) );
-//        System.out.println( "ySeries = " + toString( ySeries ) );
-//        double[] bmX = Regression.getOLSRegression( dataset, 0 );//y=a+bx
-//        double[] bmY = Regression.getOLSRegression( dataset, 1 );
-//        double vx = bmX[1];
-//        double vy = bmY[1];
-//        System.out.println( "vx=" + vx + ", vy = " + vy );
-//        vx /= 50;
-//        vy /= 50;
-//        Vector2D.Double vecADouble = new Vector2D.Double( vx, vy );
-//        double threshold = 0.27;
-//        System.out.println( "vecADouble.getMagnitude() = " + vecADouble.getMagnitude() );
-//        if( vecADouble.getMagnitude() > threshold ) {
-//            body.setVelocity( vx, vy );
-//        }
-        dragHistory.clear();
-    }
-
-//    private String toString( XYSeries xSeries ) {
-//        String s = "Series=" + xSeries.getKey() + ": numItems=" + xSeries.getItemCount() + " ";
-//        ArrayList pt = new ArrayList();
-//        for( int i = 0; i < xSeries.getItemCount(); i++ ) {
-//            pt.add( new Point2D.Double( xSeries.getDataItem( i ).getX().doubleValue(), xSeries.getDataItem( i ).getY().doubleValue() ) );
-//        }
-//        return s + pt;
-//    }
-
-    private void addHistoryPoint() {
-        DataPoint o = new DataPoint( getTime(), mouseLocation.getX(), mouseLocation.getY() );
-//        System.out.println( "Adding history point: o=" + o );
-        dragHistory.add( o );
+        update();
     }
 
     public Body getBody() {
@@ -226,22 +119,6 @@ public class BodyGraphic extends PNode {
         update();
     }
 
-    private class DataPoint {
-        double time;
-        double x;
-        double y;
-
-        public DataPoint( double time, double x, double y ) {
-            this.time = time;
-            this.x = x;
-            this.y = y;
-        }
-
-        public String toString() {
-            return "time=" + time + ", point=" + new Point2D.Double( x, y );
-        }
-    }
-
     public PImage getSkater() {
         return skater;
     }
@@ -249,20 +126,7 @@ public class BodyGraphic extends PNode {
     public void update() {
         boundsDebugPPath.setPathTo( body.getLocatedShape() );
 
-        skater.setTransform( new AffineTransform() );
-        double dw = body.getShape().getBounds2D().getWidth() / skater.getImage().getWidth( null );
-        double dh = body.getShape().getBounds2D().getHeight() / skater.getImage().getHeight( null );
-
-        skater.transformBy( body.getTransform() );
-        skater.translate( 0, -AbstractSpline.SPLINE_THICKNESS / 2.0 );
-        skater.transformBy( AffineTransform.getScaleInstance( dw, dh ) );
-
-        boolean facingRight = body.isFacingRight();
-        if( facingRight ) {
-            skater.transformBy( AffineTransform.getScaleInstance( -1, 1 ) );
-            skater.translate( -skater.getImage().getWidth( null ), 0 );
-        }
-
+        skater.setTransform( createSkaterTransform() );
         centerDebugger.setPathTo( new Rectangle2D.Double( body.getAttachPoint().getX(), body.getAttachPoint().getY(), 0.1, 0.1 ) );
         if( body.getThrust().getMagnitude() != 0 ) {
             setFlamesVisible( true );
@@ -271,6 +135,43 @@ public class BodyGraphic extends PNode {
             setFlamesVisible( false );
         }
         updateFlames();
+    }
+
+    public AffineTransform createTransform( double objWidth, double objHeight, int imageWidth, int imageHeight ) {
+        AffineTransform t = new AffineTransform();
+        t.concatenate( body.getTransform() );
+        t.translate( -getBodyModelWidth() / 2, 0 );
+        t.translate( 0, -AbstractSpline.SPLINE_THICKNESS / 2.0 );
+        t.scale( objWidth / imageWidth, objHeight / imageHeight );
+
+        if( body.isFacingRight() ) {
+            t.concatenate( AffineTransform.getScaleInstance( -1, 1 ) );
+            t.translate( -imageWidth * 3 / 2.0, 0 );
+        }
+        else {
+//            t.concatenate( AffineTransform.getScaleInstance( -1, 1 ) );
+            t.translate( imageWidth / 2, 0 );
+        }
+        return t;
+    }
+
+    public boolean isFacingRight() {
+        return body.isFacingRight();
+    }
+
+    public AffineTransform createSkaterTransform() {
+        return createTransform( getBodyModelWidth(),
+                                getBodyModelHeight(),
+                                skater.getImage().getWidth( null ),
+                                skater.getImage().getHeight( null ) );
+    }
+
+    public double getBodyModelHeight() {
+        return body.getShape().getBounds2D().getHeight();
+    }
+
+    public double getBodyModelWidth() {
+        return body.getShape().getBounds2D().getWidth();
     }
 
     private void updateFlames() {
