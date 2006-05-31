fa5a5fdd3c7c20a92f451e84267dbb497b01a0e6
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-05-31 22:42:42 +0000

    Repackaged classes.

--- trunk/simlauncher/src/edu/colorado/phet/simlauncher/util/DebugStringFile.java
+++ trunk/simlauncher/src/edu/colorado/phet/simlauncher/util/DebugStringFile.java
@@ -8,31 +8,31 @@
  * Revision : $Revision$
  * Date modified : $Date$
  */
-package edu.colorado.phet.simlauncher;
+package edu.colorado.phet.simlauncher.util;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
 /**
- * JnlpFile
+ * DebugStringFile
+ * <p>
+ * A simple File that can return its contents as a String
  *
  * @author Ron LeMaster
  * @version $Revision$
  */
-public class JnlpFile2 {
+public class DebugStringFile {
     String path;
 
-    public JnlpFile2( String path ) {
+    public DebugStringFile( String path ) {
         this.path = path;
     }
-
-
 
     /**
      * For debugging
      *
-     * @return
+     * @return the contents of the file as a String
      */
     public String getContents() {
         StringBuffer sb = new StringBuffer();
