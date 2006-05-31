460671003492125217a2f58da26b06917a46da70
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-05-31 16:53:45 +0000

    Added javadoc.

--- branches/piccolo-phet-module-with-graphics-specific-tabbed-panes-branch-4-29-2006/piccolo-phet/src/edu/colorado/phet/piccolo/PiccoloPhetApplication.java
+++ branches/piccolo-phet-module-with-graphics-specific-tabbed-panes-branch-4-29-2006/piccolo-phet/src/edu/colorado/phet/piccolo/PiccoloPhetApplication.java
@@ -10,46 +10,102 @@
  */
 package edu.colorado.phet.piccolo;
 
-import edu.colorado.phet.common.application.PhetApplication;
 import edu.colorado.phet.common.application.Module;
-import edu.colorado.phet.common.view.util.FrameSetup;
-import edu.colorado.phet.common.view.TabbedModulePanePhetGraphics;
+import edu.colorado.phet.common.application.PhetApplication;
 import edu.colorado.phet.common.view.ITabbedModulePane;
+import edu.colorado.phet.common.view.TabbedModulePanePhetGraphics;
+import edu.colorado.phet.common.view.util.FrameSetup;
 
 import javax.swing.*;
 
 /**
  * PiccoloPhetApplication
+ * <p/>
+ * Allows for the use of components and graphics that require Piccolo support.
+ * <p/>
+ * Piccolo-dependent items that can be specified:
+ * <ul>
+ * <li>PhetTabbedPane is used in TabbedModulePane instances. (JTabbedPane can be specified in the constructor, if
+ * desired.)
+ * </ul>
  *
  * @author Ron LeMaster
  * @version $Revision$
  */
 public class PiccoloPhetApplication extends PhetApplication {
 
+    //--------------------------------------------------------------------------------------------------
+    // Class fields and methods and inner classes
+    //--------------------------------------------------------------------------------------------------
+
+    /**
+     * Enumeration class used to specify the type of tabbed panes the application is to use in
+     * its TabbedModulePane instances
+     */
     public static class TabbedModulePaneType {
         private TabbedModulePaneType() {
         }
     }
 
+    // Standard Swing JTabbedPanes
     public static final TabbedModulePaneType JTABBED_PANE = new TabbedModulePaneType();
+    // Graphical PhetTabbedPanes
     public static final TabbedModulePaneType PHET_TABBED_PANE = new TabbedModulePaneType();
+    // The default TabbedModulePaneType
+    private static final TabbedModulePaneType DEFAULT_TABBED_PANE_TYPE = PHET_TABBED_PANE;
+
+    //--------------------------------------------------------------------------------------------------
+    // Instance fields and methods
+    //--------------------------------------------------------------------------------------------------
 
     // The type to be used for tabbed module panes
-    private TabbedModulePaneType tabbedModulePaneType = JTABBED_PANE;
+    private TabbedModulePaneType tabbedModulePaneType = DEFAULT_TABBED_PANE_TYPE;
 
+    /**
+     * @param args
+     * @param title
+     * @param description
+     * @param version
+     */
     public PiccoloPhetApplication( String[] args, String title, String description, String version ) {
         super( args, title, description, version );
     }
 
+    /**
+     * Allows for the specification of the type of tabbed panes to be used.
+     *
+     * @param args
+     * @param title
+     * @param description
+     * @param version
+     * @param tabbedModulePaneType The type of tabbed panes to be used
+     */
     public PiccoloPhetApplication( String[] args, String title, String description, String version, TabbedModulePaneType tabbedModulePaneType ) {
         super( args, title, description, version );
         this.tabbedModulePaneType = tabbedModulePaneType;
     }
 
+    /**
+     * @param args
+     * @param title
+     * @param description
+     * @param version
+     * @param frameSetup
+     */
     public PiccoloPhetApplication( String[] args, String title, String description, String version, FrameSetup frameSetup ) {
         super( args, title, description, version, frameSetup );
     }
 
+    /**
+     * Allows for the specification of the type of tabbed panes to be used.
+     *
+     * @param args
+     * @param title
+     * @param description
+     * @param version
+     * @param frameSetup
+     * @param tabbedModulePaneType
+     */
     public PiccoloPhetApplication( String[] args, String title, String description, String version, FrameSetup frameSetup, TabbedModulePaneType tabbedModulePaneType ) {
         super( args, title, description, version, frameSetup );
         this.tabbedModulePaneType = tabbedModulePaneType;
