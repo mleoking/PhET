6f9fcff6bf686e154d743424e46a2667dd18cfac
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-06-02 19:44:17 +0000

    removed flicker from awt splash window

--- trunk/phetcommon/src/edu/colorado/phet/common/application/AWTSplashWindow.java
+++ trunk/phetcommon/src/edu/colorado/phet/common/application/AWTSplashWindow.java
@@ -11,6 +11,11 @@
 
 package edu.colorado.phet.common.application;
 
+import edu.colorado.phet.common.view.util.ImageLoader;
+import edu.colorado.phet.common.view.util.SimStrings;
+import edu.colorado.phet.common.view.util.SwingUtils;
+
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -19,22 +24,16 @@ import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.text.MessageFormat;
 
-import javax.swing.SwingUtilities;
-
-import edu.colorado.phet.common.view.util.ImageLoader;
-import edu.colorado.phet.common.view.util.SimStrings;
-import edu.colorado.phet.common.view.util.SwingUtils;
-
 /**
  * AWTSplashWindow is a splash window based on AWT, that allows
  * us to show some animation outside of the event dispatch thread.
- * <p>
- * To overcome some deadlock problems at startup, we've started using 
+ * <p/>
+ * To overcome some deadlock problems at startup, we've started using
  * SwingUtilities.invokeLater to do all of our initialization of PhET
- * sims. An unfortunate consequence of this is that an indeterminate 
+ * sims. An unfortunate consequence of this is that an indeterminate
  * progress bar will not animate in the event dispatch thread.
  * AWTSplashWindow does an animation that is similar to an
- * indeterminate progress bar, but does its painting in its 
+ * indeterminate progress bar, but does its painting in its
  * own thread.
  *
  * @author Sam Reid
@@ -43,27 +42,27 @@ import edu.colorado.phet.common.view.util.SwingUtils;
 public class AWTSplashWindow extends Window {
 
     private static final String LOGO_RESOURCE_NAME = "images/Phet-Flatirons-logo-3-small.gif";
-    
+
     private boolean done = false; // flag that tells the animation thread to stop
     private Panel panel; // panel that contains all the UI components
     private AnimationComponent animationComponent;
-    
+
     /**
      * Constructor.
-     * 
+     *
      * @param owner the Frame to act as owner
      * @param title title of the simulation, will be inserted into a standard message format
      */
     public AWTSplashWindow( Frame owner, String title ) {
         super( owner );
-        
+
         addMouseListener( new MouseAdapter() {
             public void mousePressed( MouseEvent e ) {
                 hide();
             }
         } );
         setCursor( Cursor.getPredefinedCursor( Cursor.WAIT_CURSOR ) );
-        
+
         // UI components
         String labelString = MessageFormat.format( SimStrings.get( "PhetApplication.StartupDialog.message" ), new Object[]{title} );
         BufferedImage image = getLogoImage();
@@ -77,16 +76,27 @@ public class AWTSplashWindow extends Window {
         animationComponent = new AnimationComponent();
 
         // Panel to hold all of the components
-        panel = new Panel( new GridBagLayout() ) {         
+        panel = new Panel( new GridBagLayout() ) {
             public void paint( Graphics g ) {
                 super.paint( g );
                 drawBorder( this, g );
             }
+
+            /**
+             * This is necessary to reduce flicker.
+             *
+             * @param g
+             */
+            public void update( Graphics g ) {
+                if( g != null ) {
+                    paint( g );
+                }
+            }
         };
-        
+
         // Layout
-        final GridBagConstraints gbc = new GridBagConstraints( 0, 0, 1, 1, 1, 1, 
-                GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets( 0, 20, 0, 10 ), 0, 0 );
+        final GridBagConstraints gbc = new GridBagConstraints( 0, 0, 1, 1, 1, 1,
+                                                               GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets( 0, 20, 0, 10 ), 0, 0 );
         add( panel );
         gbc.gridheight = 2;
         panel.add( imageComponent, gbc );
@@ -102,7 +112,7 @@ public class AWTSplashWindow extends Window {
         // Paint in a separate thread.
         // We need to paint the entire panel or some things don't paint.
         startPaintThread( panel );
-        
+
         invalidate();
         pack();
         SwingUtils.centerWindowOnScreen( this );
@@ -120,7 +130,7 @@ public class AWTSplashWindow extends Window {
             throw new RuntimeException( "failed to load logo image: " + LOGO_RESOURCE_NAME );
         }
     }
-    
+
     /**
      * Hides the window and stops the animation.
      */
@@ -128,16 +138,17 @@ public class AWTSplashWindow extends Window {
         super.hide();
         done = true; // stop the animation
     }
-    
+
     /**
      * Sets the background color for the window and main panel.
+     *
      * @param color
      */
     public void setBackground( Color color ) {
         super.setBackground( color );
         panel.setBackground( color );
     }
-    
+
     /*
      * Paints a component in a separate thread.
      */
@@ -159,7 +170,7 @@ public class AWTSplashWindow extends Window {
         } );
         t.start();
     }
-    
+
     /*
      * Draws a 1-pixel border around a Component.
      */
@@ -173,19 +184,20 @@ public class AWTSplashWindow extends Window {
      * A black "block" moves sinusoidally in a "track".
      */
     private static class AnimationComponent extends Component {
-        
+
         private static final int BLOCK_WIDTH = 20;
         private static final int BLOCK_HEIGHT = 14;
         private static final Color BLOCK_COLOR = Color.BLACK;
-        
+
         private static final int TRACK_WIDTH = 250;
         private static final int TRACK_HEIGHT = BLOCK_HEIGHT;
         private static final Color TRACK_COLOR = Color.LIGHT_GRAY;
 
         private static final double FREQUENCY = 0.25; // Hz
-        
-        public AnimationComponent() {}
-        
+
+        public AnimationComponent() {
+        }
+
         public Dimension getPreferredSize() {
             return getMinimumSize();
         }
@@ -246,13 +258,13 @@ public class AWTSplashWindow extends Window {
                 try {
                     runTest();
                 }
-                catch ( Exception e ) {
+                catch( Exception e ) {
                     e.printStackTrace();
                 }
             }
         } );
     }
-    
+
     private static void runTest() throws InterruptedException {
         Frame owner = new Frame();
         AWTSplashWindow awtSplashWindow = new AWTSplashWindow( owner, "Test AWTSplashWindow" );
