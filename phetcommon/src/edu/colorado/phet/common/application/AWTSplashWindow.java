b0ab9fd751ba8cae9aecf95ddd2168096cb51a9d
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-06-02 17:46:16 +0000

    - removed unnecessary member data - use show/hide interface since this is a Window - remove buggy management of self-created owner, make client provide window owner in constructor - add class data members for constants - remove "light green" default background, add setBackground method - remove Panel.update override (unnecessary) - encapsulate paint thread in startPaintThread method, which can be applied to any Component - add javadoc - add PhET standard CVS header

--- phetcommon/src/edu/colorado/phet/common/application/AWTSplashWindow.java
+++ phetcommon/src/edu/colorado/phet/common/application/AWTSplashWindow.java
@@ -1,11 +1,16 @@
+/* Copyright 2006, University of Colorado */
+
+/*
+ * CVS Info -
+ * Filename : $Source$
+ * Branch : $Name$
+ * Modified by : $Author$
+ * Revision : $Revision$
+ * Date modified : $Date$
+ */
+
 package edu.colorado.phet.common.application;
 
-import edu.colorado.phet.common.view.PhetFrame;
-import edu.colorado.phet.common.view.util.ImageLoader;
-import edu.colorado.phet.common.view.util.SimStrings;
-import edu.colorado.phet.common.view.util.SwingUtils;
-
-import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -14,57 +19,74 @@ import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.text.MessageFormat;
 
-/**
- * User: Sam Reid
- * Date: May 31, 2006
- * Time: 8:57:09 AM
- * Copyright (c) May 31, 2006 by Sam Reid
- */
+import javax.swing.SwingUtilities;
 
+import edu.colorado.phet.common.view.util.ImageLoader;
+import edu.colorado.phet.common.view.util.SimStrings;
+import edu.colorado.phet.common.view.util.SwingUtils;
+
+/**
+ * AWTSplashWindow is a splash window based on AWT, that allows
+ * us to show some animation outside of the event dispatch thread.
+ * <p>
+ * To overcome some deadlock problems at startup, we've started using 
+ * SwingUtilities.invokeLater to do all of our initialization of PhET
+ * sims. An unfortunate consequence of this is that an indeterminate 
+ * progress bar will not animate in the event dispatch thread.
+ * AWTSplashWindow does an animation that is similar to an
+ * indeterminate progress bar, but does its painting in its 
+ * own thread.
+ *
+ * @author Sam Reid
+ * @version $Revision$
+ */
 public class AWTSplashWindow extends Window {
 
-    private static Frame owner;
-    private boolean done = false;
-    private String labelString;
-
-    private Component imageComponent;
-    private Component textComponent;
-    private Component animationComponent;
-
-    public AWTSplashWindow( final Image image, String title ) {
-        super( initOwner() );
+    private static final String LOGO_RESOURCE_NAME = "images/Phet-Flatirons-logo-3-small.gif";
+    
+    private boolean done = false; // flag that tells the animation thread to stop
+    private Panel panel; // panel that contains all the UI components
+    private AnimationComponent animationComponent;
+    
+    /**
+     * Constructor.
+     * 
+     * @param owner the Frame to act as owner
+     * @param title title of the simulation, will be inserted into a standard message format
+     */
+    public AWTSplashWindow( Frame owner, String title ) {
+        super( owner );
+        
         addMouseListener( new MouseAdapter() {
             public void mousePressed( MouseEvent e ) {
-                hideSplash();
+                hide();
             }
         } );
         setCursor( Cursor.getPredefinedCursor( Cursor.WAIT_CURSOR ) );
-        labelString = MessageFormat.format( SimStrings.get( "PhetApplication.StartupDialog.message" ), new Object[]{title} );
-        imageComponent = new ImageComponent( image ) {
+        
+        // UI components
+        String labelString = MessageFormat.format( SimStrings.get( "PhetApplication.StartupDialog.message" ), new Object[]{title} );
+        BufferedImage image = getLogoImage();
+        Component imageComponent = new ImageComponent( image ) {
             public void paint( Graphics g ) {
                 super.paint( g );
                 drawBorder( this, g );
             }
         };
-        textComponent = new Label( labelString );
+        Component textComponent = new Label( labelString );
         animationComponent = new AnimationComponent();
 
-        final GridBagConstraints gbc = new GridBagConstraints( 0, 0, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets( 0, 20, 0, 10 ), 0, 0 );
-
-        final Panel panel = new Panel( new GridBagLayout() ) {
+        // Panel to hold all of the components
+        panel = new Panel( new GridBagLayout() ) {         
             public void paint( Graphics g ) {
-                if( g != null ) {
-                    super.paint( g );
-                    drawBorder( this, g );
-                }
-            }
-
-            public void update( Graphics g ) {
-                if( g != null ) {
-                    paint( g );
-                }
+                super.paint( g );
+                drawBorder( this, g );
             }
         };
+        
+        // Layout
+        final GridBagConstraints gbc = new GridBagConstraints( 0, 0, 1, 1, 1, 1, 
+                GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets( 0, 20, 0, 10 ), 0, 0 );
         add( panel );
         gbc.gridheight = 2;
         panel.add( imageComponent, gbc );
@@ -76,16 +98,58 @@ public class AWTSplashWindow extends Window {
         gbc.fill = GridBagConstraints.REMAINDER;
         gbc.insets = new Insets( 10, 10, 20, 20 );
         panel.add( animationComponent, gbc );
-        panel.setBackground( new Color( 200, 240, 200 ) );  // light green );
 
+        // Paint in a separate thread.
+        // We need to paint the entire panel or some things don't paint.
+        startPaintThread( panel );
+        
+        invalidate();
+        pack();
+        SwingUtils.centerWindowOnScreen( this );
+    }
+
+    /*
+     * Gets the PhET logo image.
+     */
+    private static BufferedImage getLogoImage() {
+        try {
+            return ImageLoader.loadBufferedImage( LOGO_RESOURCE_NAME );
+        }
+        catch( IOException e ) {
+            e.printStackTrace();
+            throw new RuntimeException( "failed to load logo image: " + LOGO_RESOURCE_NAME );
+        }
+    }
+    
+    /**
+     * Hides the window and stops the animation.
+     */
+    public void hide() {
+        super.hide();
+        done = true; // stop the animation
+    }
+    
+    /**
+     * Sets the background color for the window and main panel.
+     * @param color
+     */
+    public void setBackground( Color color ) {
+        super.setBackground( color );
+        panel.setBackground( color );
+    }
+    
+    /*
+     * Paints a component in a separate thread.
+     */
+    private void startPaintThread( final Component component ) {
         Thread t = new Thread( new Runnable() {
             public void run() {
                 while( !done ) {
                     try {
                         Thread.sleep( 30 );
-                        panel.invalidate();
-                        panel.repaint();
-                        panel.update( panel.getGraphics() );
+                        component.invalidate();
+                        component.repaint();
+                        component.update( component.getGraphics() );
                     }
                     catch( InterruptedException e ) {
                         e.printStackTrace();
@@ -94,38 +158,40 @@ public class AWTSplashWindow extends Window {
             }
         } );
         t.start();
-
-        invalidate();
-        pack();
-        SwingUtils.centerWindowOnScreen( this );
     }
-
-    public AWTSplashWindow( PhetFrame phetFrame, String title ) {
-        this( getPhetImage(), title );
-    }
-
-    private static Frame initOwner() {
-        if( owner == null ) {
-            owner = new Frame();
-        }
-        return owner;
-    }
-
+    
+    /*
+     * Draws a 1-pixel border around a Component.
+     */
     private static void drawBorder( Component component, Graphics g ) {
         g.setColor( Color.black );
         g.drawRect( 0, 0, component.getWidth() - 1, component.getHeight() - 1 );
     }
 
-    private class AnimationComponent extends Component {
-        int blockWidth = 20;
-        int blockHeight = 14;
+    /*
+     * AWT component that provides something similar to Swing's indeterminate progress bar.
+     * A black "block" moves sinusoidally in a "track".
+     */
+    private static class AnimationComponent extends Component {
+        
+        private static final int BLOCK_WIDTH = 20;
+        private static final int BLOCK_HEIGHT = 14;
+        private static final Color BLOCK_COLOR = Color.BLACK;
+        
+        private static final int TRACK_WIDTH = 250;
+        private static final int TRACK_HEIGHT = BLOCK_HEIGHT;
+        private static final Color TRACK_COLOR = Color.LIGHT_GRAY;
 
+        private static final double FREQUENCY = 0.25; // Hz
+        
+        public AnimationComponent() {}
+        
         public Dimension getPreferredSize() {
             return getMinimumSize();
         }
 
         public Dimension getMinimumSize() {
-            return new Dimension( 250, blockHeight );
+            return new Dimension( TRACK_WIDTH, TRACK_HEIGHT );
         }
 
         public void update( Graphics g ) {
@@ -133,25 +199,24 @@ public class AWTSplashWindow extends Window {
         }
 
         public void paint( Graphics g ) {
-//            System.out.println( "getSize( ) = " + getSize() );
             if( g == null ) {
                 return;
             }
-//            System.out.println( "AWTSplashWindow$AnimationComponent.paint" );
-            g.setColor( Color.lightGray );
+            g.setColor( TRACK_COLOR );
             g.fillRect( 0, 0, getWidth(), getHeight() );
-            g.setColor( Color.black );
-//            double freqHZ = 0.5;
-            double freqHZ = 0.25;
-            double angularfrequency = freqHZ * 2 * Math.PI;
+            g.setColor( BLOCK_COLOR );
+            double angularfrequency = FREQUENCY * 2 * Math.PI;
             int x = (int)( Math.sin( System.currentTimeMillis() / 1000.0 * angularfrequency ) * getWidth() / 2.0 + getWidth() / 2.0 );
-            int a = x - blockWidth / 2;
+            int a = x - BLOCK_WIDTH / 2;
 
-            g.fillRect( a, 0, blockWidth, blockHeight );
+            g.fillRect( a, 0, BLOCK_WIDTH, BLOCK_HEIGHT );
             g.drawRect( 0, 0, getWidth() - 1, getHeight() - 1 );
         }
     }
 
+    /*
+     * AWT component that draws an image.
+     */
     private static class ImageComponent extends Component {
         Image image;
 
@@ -169,69 +234,32 @@ public class AWTSplashWindow extends Window {
 
         public void paint( Graphics g ) {
             super.paint( g );
-            g.drawImage( image, 0, 0, null );
+            g.drawImage( image, 0, 0, this );
         }
     }
 
-    public void hideSplash() {
-        if( !done ) {
-//            System.out.println( "AWTSplashScreen2.hideSplash" );
-            super.setVisible( false );
-            super.hide();
-            super.dispose();
-            owner.dispose();
-            owner = null;
-            done = true;
-        }
-    }
-
-    private void showSplashScreen() {
-        setVisible( true );
-    }
-
-    private static BufferedImage getPhetImage() {
-        try {
-            return ImageLoader.loadBufferedImage( "images/Phet-Flatirons-logo-3-small.gif" );
-        }
-        catch( IOException e ) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
+    // Test harness
     public static void main( String[] args ) throws InterruptedException, InvocationTargetException {
-        SwingUtilities.invokeAndWait( new Runnable() {
+        // Use invokeLater like we would in our simulation's main.
+        SwingUtilities.invokeLater( new Runnable() {
             public void run() {
                 try {
                     runTest();
                 }
-                catch( InterruptedException e ) {
-                    e.printStackTrace();
-                }
-                catch( InvocationTargetException e ) {
+                catch ( Exception e ) {
                     e.printStackTrace();
                 }
             }
         } );
-//        runTest();
     }
-
-    private static void runTest() throws InterruptedException, InvocationTargetException {
-        AWTSplashWindow awtSplashWindow = new AWTSplashWindow( getPhetImage(), "Energy Skate Park" );
-        awtSplashWindow.showSplashScreen();
-        Thread.sleep( 1000 );
-//        SwingUtilities.invokeAndWait( new Runnable() {
-//            public void run() {
-//                try {
+    
+    private static void runTest() throws InterruptedException {
+        Frame owner = new Frame();
+        AWTSplashWindow awtSplashWindow = new AWTSplashWindow( owner, "Test AWTSplashWindow" );
+        awtSplashWindow.setBackground( new Color( 200, 240, 200 ) ); // light green
+        awtSplashWindow.show();
         Thread.sleep( 5000 );
-//                }
-//                catch( InterruptedException e ) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        } );
-        Thread.sleep( 1000 );
-        awtSplashWindow.hideSplash();
+        awtSplashWindow.hide();
+        owner.dispose(); // clean up the owner so the program exits
     }
-
 }
