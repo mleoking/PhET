8fdbf700c8231b551ac419165a0e38ea2e8ce416
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-06-02 06:26:51 +0000

    post 0.00.22

--- trunk/energyconservation3/src/edu/colorado/phet/ec3/EC3Application.java
+++ trunk/energyconservation3/src/edu/colorado/phet/ec3/EC3Application.java
@@ -17,7 +17,7 @@ import java.awt.event.KeyEvent;
 
 public class EC3Application extends PhetApplication {
     private EC3Module module;
-    private static final String VERSION = "0.00.21-test1";
+    private static final String VERSION = "0.00.22";
 
     public EC3Application( String[] args ) {
         super( args, "Energy Skate Park", "Energy Conservation", VERSION, new EC3FrameSetup() );
