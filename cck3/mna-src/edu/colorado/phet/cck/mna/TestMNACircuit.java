ed898d7391028ef4106e161193628c2911b89df3
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-06-18 20:03:09 +0000

    writing mna

--- cck3/mna-src/edu/colorado/phet/cck/mna/TestMNACircuit.java
+++ cck3/mna-src/edu/colorado/phet/cck/mna/TestMNACircuit.java
@@ -10,19 +10,17 @@ package edu.colorado.phet.cck.mna;
 
 public class TestMNACircuit {
     public static void main( String[] args ) {
-        String[]netList = new String[]{
-                "i1 0 1 1.0",
-                "r2 1 0 1.0",
-                "r3 1 2 1.0",
-                "r4 2 0 1.0",
-                "r5 2 3 1.0",
-                "r6 3 0 1.0",
-                "r7 3 4 1.0",
-                "r8 4 0 1.0",
-                "i9 0 4 1.0",
-        };
+        String netlist = "i1 0 1 1.0\n" +
+                         "r2 1 0 1.0\n" +
+                         "r3 1 2 1.0\n" +
+                         "r4 2 0 1.0\n" +
+                         "r5 2 3 1.0\n" +
+                         "r6 3 0 1.0\n" +
+                         "r7 3 4 1.0\n" +
+                         "r8 4 0 1.0\n" +
+                         "i9 0 4 1.0";
         MNACircuit circuit = new MNACircuit();
-        circuit.parseNetList( netList );
+        circuit.parseNetList( netlist );
         System.out.println( "circuit = " + circuit );
     }
 }
