f5174a17dcf8a7114189e3b09b349faa522c5ee3
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-06-20 03:20:33 +0000

    3.01.01

--- trunk/cck3/src/edu/colorado/phet/cck3/CCK3Module.java
+++ trunk/cck3/src/edu/colorado/phet/cck3/CCK3Module.java
@@ -68,7 +68,7 @@ import java.util.Arrays;
  * Copyright (c) May 24, 2004 by Sam Reid
  */
 public class CCK3Module extends Module {
-    private static final String VERSION = "3.01.00";
+    private static final String VERSION = "3.01.01";
 
     private SetupParameters parameters;
     private Circuit circuit;
