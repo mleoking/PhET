a37631b5f808f018eef407c700459091d16bb082
nobody <nobody@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-06-22 20:39:14 +0000

    This commit was manufactured by cvs2svn to create tag 'nukes-v1r0808-6-22-2006'.

---
+++ tags/nukes-v1r0808-6-22-2006/nuclearphysics/src/edu/colorado/phet/nuclearphysics/util/Translatable.java
@@ -0,0 +1,20 @@
+/* Copyright 2003-2004, University of Colorado */
+
+/*
+ * CVS Info -
+ * Filename : $Source$
+ * Branch : $Name$
+ * Modified by : $Author$
+ * Revision : $Revision$
+ * Date modified : $Date$
+ */
+package edu.colorado.phet.nuclearphysics.util;
+
+/**
+ * Translatable
+ *
+ * @author Ron LeMaster
+ * @version $Revision$
+ */
+public interface Translatable {
+}
