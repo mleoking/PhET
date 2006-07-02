6f81cd37ec531be08daaddeeb3becc392b10cf1c
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-07-02 06:29:09 +0000

    added spark

--- travoltage/src/edu/colorado/phet/travoltage/ArmNode.java
+++ travoltage/src/edu/colorado/phet/travoltage/ArmNode.java
@@ -1,7 +1,11 @@
 /* Copyright 2004, Sam Reid */
 package edu.colorado.phet.travoltage;
 
+import edu.colorado.phet.common.math.AbstractVector2D;
+import edu.colorado.phet.common.math.Vector2D;
+
 import java.awt.*;
+import java.awt.geom.Point2D;
 
 /**
  * User: Sam Reid
@@ -11,9 +15,15 @@ import java.awt.*;
  */
 
 public class ArmNode extends LimbNode {
+    private double insetAngle = Math.PI / 16 + Math.PI / 32;
 
     public ArmNode() {
         super( "images/arm2.gif", new Point( 8, 42 ) );
     }
 
+    public Point2D getGlobalFingertipPoint() {
+        Point2D globalPivot = localToGlobal( getPivot() );
+        AbstractVector2D v = Vector2D.Double.parseAngleAndMagnitude( getImageNode().getWidth() * 0.95, getAngle() - insetAngle );
+        return v.getDestination( globalPivot );
+    }
 }
