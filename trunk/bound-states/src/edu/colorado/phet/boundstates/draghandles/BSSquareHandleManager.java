a84a3098a221e0f68fe1e349d1dc5c08b5043876
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-07-03 19:55:30 +0000

    (7/3/06) - first attempt at offset and height drag handles for square potential

---
+++ trunk/bound-states/src/edu/colorado/phet/boundstates/draghandles/BSSquareHandleManager.java
@@ -0,0 +1,71 @@
+/* Copyright 2006, University of Colorado */
+
+/*
+ * CVS Info -
+ * Filename : $Source$
+ * Branch : $Name$
+ * Modified by : $Author$
+ * Revision : $Revision$
+ * Date modified : $Date$
+ */
+
+package edu.colorado.phet.boundstates.draghandles;
+
+import edu.colorado.phet.boundstates.color.BSColorScheme;
+import edu.colorado.phet.boundstates.model.BSSquarePotential;
+import edu.colorado.phet.boundstates.view.BSCombinedChartNode;
+import edu.umd.cs.piccolo.PNode;
+
+
+public class BSSquareHandleManager extends PNode {
+    
+    //----------------------------------------------------------------------------
+    // Class data
+    //----------------------------------------------------------------------------
+    
+    private static final boolean DEBUG_SHOW_VALUES = true;
+    
+    //----------------------------------------------------------------------------
+    // Instance data
+    //----------------------------------------------------------------------------
+    
+    private BSCombinedChartNode _chartNode;
+    
+    private BSSquareOffsetHandle _offsetHandle;
+    private BSSquareHeightHandle _heightHandle;
+    
+    //----------------------------------------------------------------------------
+    // Constructors
+    //----------------------------------------------------------------------------
+    
+    public BSSquareHandleManager( BSCombinedChartNode chartNode ) {
+        super();
+        _chartNode = chartNode;
+    }
+    
+    //----------------------------------------------------------------------------
+    // Accessors
+    //----------------------------------------------------------------------------
+    
+    public void setPotential( BSSquarePotential potential ) {
+
+        removeAllChildren();
+
+        _offsetHandle = new BSSquareOffsetHandle( potential, _chartNode );
+        _offsetHandle.setValueVisible( DEBUG_SHOW_VALUES );
+        addChild( _offsetHandle );
+
+        _heightHandle = new BSSquareHeightHandle( potential, _chartNode );
+        _heightHandle.setValueVisible( DEBUG_SHOW_VALUES );
+        addChild( _heightHandle );
+    }
+    
+    public void setColorScheme( BSColorScheme colorScheme ) {
+        //XXX
+    }
+       
+    public void updateDragBounds() {
+        _offsetHandle.updateDragBounds();
+        _heightHandle.updateDragBounds();
+    }
+}
