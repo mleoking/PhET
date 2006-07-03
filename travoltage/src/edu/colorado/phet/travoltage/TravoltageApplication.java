6464215b9a54c2f8083da2e66b030b02c2e83edf
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-07-03 23:59:38 +0000

    added build/deploy system

--- travoltage/src/edu/colorado/phet/travoltage/TravoltageApplication.java
+++ travoltage/src/edu/colorado/phet/travoltage/TravoltageApplication.java
@@ -16,7 +16,7 @@ import javax.swing.*;
 public class TravoltageApplication extends PhetApplication {
     private static final String TITLE = "Travoltage";
     private static final String DESCRIPTION = "The John Travoltage Simulation";
-    private static final String VERSION = "2.00.00";
+    private static final String VERSION = "1.00.01";
 
     public TravoltageApplication( String[] args ) {
         super( args, TITLE, DESCRIPTION, VERSION, new TravoltageFrameSetup() );
