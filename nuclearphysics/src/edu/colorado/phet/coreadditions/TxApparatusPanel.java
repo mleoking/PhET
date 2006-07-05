36303e21a9ca521112ac6be4f4fd12f0bf9f3fc8
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-07-05 23:03:29 +0000

    no message

--- nuclearphysics/src/edu/colorado/phet/coreadditions/TxApparatusPanel.java
+++ nuclearphysics/src/edu/colorado/phet/coreadditions/TxApparatusPanel.java
@@ -6,29 +6,29 @@
  */
 package edu.colorado.phet.coreadditions;
 
-import edu.colorado.phet.common.model.BaseModel;
 import edu.colorado.phet.common.model.clock.IClock;
 import edu.colorado.phet.common.view.ApparatusPanel2;
 import edu.colorado.phet.common.view.phetgraphics.PhetGraphic;
 
 import java.awt.geom.AffineTransform;
-import java.awt.geom.Point2D;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.*;
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 
 /**
  * An ApparatusPanel that handles TxGraphics
  */
 public class TxApparatusPanel extends ApparatusPanel2 {
+
+    static ArrayList instances = new ArrayList( );
+    static boolean init = false;
+
+
     private HashMap txGraphicMap = new HashMap();
     private AffineTransform atx = new AffineTransform();
 
     public TxApparatusPanel( IClock clock ) {
         super( clock );
+        instances.add( this );
     }
 
     public AffineTransform getTransform() {
