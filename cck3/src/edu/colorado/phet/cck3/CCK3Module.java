eb5026edfe1f251d3e6ce294ca456e46810bb54d
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-07-05 06:19:15 +0000

    no message

--- cck3/src/edu/colorado/phet/cck3/CCK3Module.java
+++ cck3/src/edu/colorado/phet/cck3/CCK3Module.java
@@ -71,7 +71,7 @@ import java.util.Arrays;
  * Copyright (c) May 24, 2004 by Sam Reid
  */
 public class CCK3Module extends Module {
-    private static final String VERSION = "3.01.04";
+    private static final String VERSION = "3.01.05";
 
     private SetupParameters parameters;
     private Circuit circuit;
