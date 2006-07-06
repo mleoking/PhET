456dae4ef8ac40dbbad78e0bc06f1da39cbbddb6
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-07-06 20:52:34 +0000

    change some Spanish translations, rebuild 1.00.00

--- trunk/bound-states/src/edu/colorado/phet/boundstates/BSVersion.java
+++ trunk/bound-states/src/edu/colorado/phet/boundstates/BSVersion.java
@@ -20,11 +20,11 @@ package edu.colorado.phet.boundstates;
  */
 public class BSVersion {
   
-    public static final String NUMBER = "sandbox";
-    public static final String CVS_TAG = "boundstates-sandbox";
+//    public static final String NUMBER = "sandbox";
+//    public static final String CVS_TAG = "boundstates-sandbox";
     
-//    public static final String NUMBER = "1.00";
-//    public static final String CVS_TAG = "boundstates-1-00-00-2006-07-06";
+    public static final String NUMBER = "1.00";
+    public static final String CVS_TAG = "boundstates-1-00-00-2006-07-06";
     
     /* This class is not intended for instantiation. */
     private BSVersion() {}
