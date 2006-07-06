9fe908210ece768b4ed57cc809282d43ecc54269
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-07-06 18:59:07 +0000

    Sliders on MultipleFissionControlPanel put up warning when more nuclei can't be added.

--- trunk/nuclearphysics/src/edu/colorado/phet/nuclearphysics/controller/MultipleNucleusFissionControlPanel.java
+++ trunk/nuclearphysics/src/edu/colorado/phet/nuclearphysics/controller/MultipleNucleusFissionControlPanel.java
@@ -9,8 +9,6 @@ package edu.colorado.phet.nuclearphysics.controller;
 import edu.colorado.phet.common.model.ModelElement;
 import edu.colorado.phet.common.view.util.SimStrings;
 import edu.colorado.phet.common.view.ModelSlider;
-import edu.colorado.phet.common.util.PhetUtilities;
-import edu.colorado.phet.nuclearphysics.model.Nucleus;
 import edu.colorado.phet.nuclearphysics.model.Uranium235;
 import edu.colorado.phet.nuclearphysics.model.Uranium238;
 
@@ -26,6 +24,7 @@ import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.util.Random;
 import java.text.DecimalFormat;
+import java.text.NumberFormat;
 
 public class MultipleNucleusFissionControlPanel extends JPanel {
 
@@ -40,13 +39,10 @@ public class MultipleNucleusFissionControlPanel extends JPanel {
     //--------------------------------------------------------------------------------------------------
 
     private MultipleNucleusFissionModule module;
-    private JSpinner numU235Spinner;
-    private JSpinner numU238Spinner;
-    private ModelSlider numU235Slider;
-    private ModelSlider numU238Slider;
+    private MyModelSlider numU235Slider;
+    private MyModelSlider numU238Slider;
     private JTextField percentDecayTF;
     private JButton resetBtn;
-    private Boolean blockAddingNuclei = Boolean.FALSE;
 
     /**
      * Constructor
@@ -72,8 +68,6 @@ public class MultipleNucleusFissionControlPanel extends JPanel {
                 int modelNum = module.getU235Nuclei().size();
                 int viewNum = (int)numU235Slider.getValue();
                 if( modelNum != viewNum ) {
-                    numU235Spinner.setValue( new Integer( module.getU235Nuclei().size() ) );
-
                     numU235Slider.setValue( module.getU235Nuclei().size() );
                 }
 
@@ -87,56 +81,18 @@ public class MultipleNucleusFissionControlPanel extends JPanel {
                 percentDecayTF.setText( percentFormat.format( percent ) );
 
                 modelNum = module.getU238Nuclei().size();
-                viewNum = ( (Integer)numU238Spinner.getValue() ).intValue();
                 viewNum = (int)numU238Slider.getValue();
                 if( modelNum != viewNum ) {
-                    numU238Spinner.setValue( new Integer( module.getU238Nuclei().size() ) );
-
                     numU238Slider.setValue( module.getU238Nuclei().size() );
                 }
             }
         } );
 
         //--------------------------------------------------------------------------------------------------
-        // Spinners
+        // Sliders that add and remove nuclei
         //--------------------------------------------------------------------------------------------------
 
-        numU235Spinner = new JSpinner( new SpinnerNumberModel( 1, 0, 200, 1 ) );
-        numU235Spinner.addChangeListener( new ChangeListener() {
-            public void stateChanged( ChangeEvent e ) {
-                int numU235 = ( (Integer)numU235Spinner.getValue() ).intValue();
-                setNumU235Nuclei( numU235 );
-            }
-        } );
-
-        numU238Spinner = new JSpinner( new SpinnerNumberModel( 0, 0, 200, 1 ) );
-        numU238Spinner.addChangeListener( new ChangeListener() {
-            public void stateChanged( ChangeEvent e ) {
-                setNumU238Nuclei( ( (Integer)numU238Spinner.getValue() ).intValue() );
-            }
-        } );
-
-        DecimalFormat sliderFormat = new DecimalFormat( "#0" );
-        numU235Slider = new ModelSlider( "U235", "", 0, 300, 0, sliderFormat );
-        numU235Slider.setPreferredSliderWidth( 150 );
-        numU235Slider.addChangeListener( new ChangeListener() {
-            public void stateChanged( ChangeEvent e ) {
-                synchronized( MultipleNucleusFissionControlPanel.this ) {
-                    int numU235 = (int)numU235Slider.getValue();
-                    setNumU235Nuclei( numU235 );
-                }
-            }
-        } );
-        numU238Slider = new ModelSlider( "U238", "", 0, 300, 0, sliderFormat );
-        numU238Slider.setPreferredSliderWidth( 150 );
-        numU238Slider.addChangeListener( new ChangeListener() {
-            public void stateChanged( ChangeEvent e ) {
-                synchronized( MultipleNucleusFissionControlPanel.this ) {
-                    int numU238 = (int)numU238Slider.getValue();
-                    setNumU238Nuclei( numU238 );
-                }
-            }
-        } );
+        makeNewNucleusSliders();
 
         //--------------------------------------------------------------------------------------------------
         // Buttons
@@ -148,13 +104,8 @@ public class MultipleNucleusFissionControlPanel extends JPanel {
                 module.stop();
                 module.start();
                 percentDecayTF.setText( "0" );
-                numU235Spinner.setValue( new Integer( 1 ) );
-                numU238Spinner.setValue( new Integer( 0 ) );
-
                 numU235Slider.setValue( 1 );
                 numU238Slider.setValue( 0 );
-                numU235Spinner.setEnabled( true );
-                numU238Spinner.setEnabled( true );
             }
         } );
         resetBtn.setEnabled( true );
@@ -195,44 +146,98 @@ public class MultipleNucleusFissionControlPanel extends JPanel {
         this.setBorder( titledBorder );
     }
 
-    private synchronized void setNumU235Nuclei( final int num ) {
-        int delta = num - module.getU235Nuclei().size();
-        for( int i = 0; i < delta; i++ ) {
-            module.addU235Nucleus();
+    private void makeNewNucleusSliders() {
+        if( numU235Slider != null ) {
+            remove( numU235Slider );
         }
-        for( int i = 0; i < -delta; i++ ) {
-            int numNuclei = module.getU235Nuclei().size();
-            Uranium235 nucleus = (Uranium235)module.getU235Nuclei().get( random.nextInt( numNuclei ) );
-            module.removeU235Nucleus( nucleus );
+        if( numU238Slider != null ) {
+            remove( numU238Slider );
         }
 
-        if( num != module.getU235Nuclei().size() ) {
-            numU235Slider.setValue( module.getU235Nuclei().size() );
+        DecimalFormat sliderFormat = new DecimalFormat( "#0" );
+        numU235Slider = new MyModelSlider( "U235", "", 0, 100, 0, sliderFormat );
+        numU235Slider.setPreferredSliderWidth( 150 );
+        numU235Slider.addChangeListener( new ChangeListener() {
+            public void stateChanged( ChangeEvent e ) {
+                    int numU235 = (int)numU235Slider.getValue();
+                    setNumU235Nuclei( numU235 );
+                }
+        } );
+        numU238Slider = new MyModelSlider( "U238", "", 0, 100, 0, sliderFormat );
+        numU238Slider.setPreferredSliderWidth( 150 );
+        numU238Slider.addChangeListener( new ChangeListener() {
+            public void stateChanged( ChangeEvent e ) {
+                    int numU238 = (int)numU238Slider.getValue();
+                    setNumU238Nuclei( numU238 );
+            }
+        } );
+    }
+
+    private void addNucleusSliders() {
+        GridBagConstraints gbc = new GridBagConstraints( 0, 0, 1, 1, 1, 1, GridBagConstraints.EAST,
+                                                         GridBagConstraints.NONE,
+                                                         new Insets( 5, 5, 5, 5 ), 5, 5 );
+        gbc.gridwidth = 2;
+        add( numU235Slider, gbc );
+        gbc.gridy++;
+        add( numU238Slider, gbc );
+    }
+
+    private void setNumU235Nuclei( final int num ) {
+            int delta = num - module.getU235Nuclei().size();
+            for( int i = 0; i < delta; i++ ) {
+                module.addU235Nucleus();
+            }
+            for( int i = 0; i < -delta; i++ ) {
+                int numNuclei = module.getU235Nuclei().size();
+                Uranium235 nucleus = (Uranium235)module.getU235Nuclei().get( random.nextInt( numNuclei ) );
+                module.removeU235Nucleus( nucleus );
+            }
+
+            if( num != module.getU235Nuclei().size() ) {
+                makeNewNucleusSliders();
+                addNucleusSliders();
+                numU235Slider.setValue( module.getU235Nuclei().size() );
+                JOptionPane.showMessageDialog( MultipleNucleusFissionControlPanel.this,
+                                               SimStrings.get( "MultipleNucleusFissionControlPanel.NucleusPlacementFailed" ) );
+            }
+    }
+
+    private void setNumU238Nuclei( final int num ) {
+        int delta = num - module.getU238Nuclei().size();
+        for( int i = 0; i < delta; i++ ) {
+            module.addU238Nucleus();
+        }
+        for( int i = 0; i < -delta; i++ ) {
+            int numNuclei = module.getU238Nuclei().size();
+            Uranium238 nucleus = (Uranium238)module.getU238Nuclei().get( random.nextInt( numNuclei ) );
+            module.removeU238Nucleus( nucleus );
+        }
+        if( num != module.getU238Nuclei().size() ) {
+            makeNewNucleusSliders();
+            addNucleusSliders();
+            numU238Slider.setValue( module.getU238Nuclei().size() );
             JOptionPane.showMessageDialog( MultipleNucleusFissionControlPanel.this,
                                            SimStrings.get( "MultipleNucleusFissionControlPanel.NucleusPlacementFailed" ) );
         }
     }
 
-    private synchronized void setNumU238Nuclei( final int num ) {
-        SwingUtilities.invokeLater( new Runnable() {
-            public void run() {
-                int delta = num - module.getU238Nuclei().size();
-                for( int i = 0; i < delta; i++ ) {
-                    module.addU238Nucleus();
-                }
-                for( int i = 0; i < -delta; i++ ) {
-                    int numNuclei = module.getU238Nuclei().size();
-                    Uranium238 nucleus = (Uranium238)module.getU238Nuclei().get( random.nextInt( numNuclei ) );
-                    module.removeU238Nucleus( nucleus );
-                }
-                if( num != module.getU238Nuclei().size() ) {
-                    numU238Slider.setEnabled( false );
-                    JOptionPane.showMessageDialog( MultipleNucleusFissionControlPanel.this,
-                                                   SimStrings.get( "MultipleNucleusFissionControlPanel.NucleusPlacementFailed" ) );
-                    numU238Slider.setValue( module.getU238Nuclei().size() );
-                    numU238Slider.setEnabled( true );
-                }
-            }
-        } );
+    /**
+     * For debugging
+     */
+    class MyModelSlider extends ModelSlider {
+
+        public MyModelSlider( String title, String units, final double min, final double max, double initial ) {
+            super( title, units, min, max, initial );
+        }
+
+        public MyModelSlider( String title, String units, final double min, final double max, double initialValue, NumberFormat textFieldFormat ) {
+            super( title, units, min, max, initialValue, textFieldFormat );
+        }
+
+        public MyModelSlider( String title, String units, final double min, final double max, double initialValue, NumberFormat textFieldFormat, NumberFormat sliderLabelFormat ) {
+            super( title, units, min, max, initialValue, textFieldFormat, sliderLabelFormat );
+        }
+
     }
 }
