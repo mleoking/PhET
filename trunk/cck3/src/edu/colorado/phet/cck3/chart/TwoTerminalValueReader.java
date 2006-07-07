e63864f50d5aab1ae67273ad04fc85a34876ea4d
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-07-07 02:49:48 +0000

    factor out crosshairs

---
+++ trunk/cck3/src/edu/colorado/phet/cck3/chart/TwoTerminalValueReader.java
@@ -0,0 +1,11 @@
+package edu.colorado.phet.cck3.chart;
+
+/**
+ * User: Sam Reid
+ * Date: Jul 6, 2006
+ * Time: 8:47:52 PM
+ * Copyright (c) Jul 6, 2006 by Sam Reid
+ */
+public interface TwoTerminalValueReader {
+    double getValue( double x, double y, double x1, double y1 );
+}
