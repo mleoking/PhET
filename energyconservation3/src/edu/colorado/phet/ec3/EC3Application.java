897686071435bed1e2ffac1c5a52020b77a47a20
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-07-10 22:31:13 +0000

    1.01

--- energyconservation3/src/edu/colorado/phet/ec3/EC3Application.java
+++ energyconservation3/src/edu/colorado/phet/ec3/EC3Application.java
@@ -17,7 +17,7 @@ import java.awt.event.KeyEvent;
 
 public class EC3Application extends PhetApplication {
     private EC3Module module;
-    private static final String VERSION = "0.00.28";
+    private static final String VERSION = "1.01";
 
     public EC3Application( String[] args ) {
         super( args, "Energy Skate Park", "Energy Conservation", VERSION, new EC3FrameSetup() );
