27979a66213a8e7e9ba37b121b0113dda99d92ed
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-07-10 21:04:09 +0000

    travoltage 1.02

--- trunk/travoltage/src/edu/colorado/phet/travoltage/TravoltageApplication.java
+++ trunk/travoltage/src/edu/colorado/phet/travoltage/TravoltageApplication.java
@@ -15,7 +15,7 @@ import javax.swing.*;
  */
 
 public class TravoltageApplication extends PhetApplication {
-    private static final String VERSION = "1.01.01";
+    private static final String VERSION = "1.02";
     private static final String TITLE = "Travoltage";
     private static final String DESCRIPTION = "The John Travoltage Simulation";
     private JDialog dialog;
