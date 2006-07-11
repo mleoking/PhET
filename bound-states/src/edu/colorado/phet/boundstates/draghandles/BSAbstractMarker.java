73051c509f31831730698722f6fb86c949d58d03
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-07-11 22:55:33 +0000

    javadoc

--- bound-states/src/edu/colorado/phet/boundstates/draghandles/BSAbstractMarker.java
+++ bound-states/src/edu/colorado/phet/boundstates/draghandles/BSAbstractMarker.java
@@ -19,19 +19,39 @@ import edu.colorado.phet.boundstates.model.BSAbstractPotential;
 import edu.colorado.phet.boundstates.model.BSCoulomb1DPotential;
 import edu.umd.cs.piccolox.nodes.PComposite;
 
-
+/**
+ * BSAbstractMarker is the base class for all drag handle markers.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ * @version $Revision$
+ */
 public abstract class BSAbstractMarker extends PComposite {
     
+    /**
+     * Constructor.
+     */
     public BSAbstractMarker() {
         super();
     }
     
+    /**
+     * Updates the marker when it's made visible.
+     * @param visible
+     */
     public void setVisible( boolean visible ) {
         super.setVisible( visible );
         updateView();
     }
     
+    /**
+     * Updates the marker to match the model. 
+     */
     public abstract void updateView();
     
+    /**
+     * Sets the color scheme used to draw the marker.
+     * 
+     * @param colorScheme
+     */
     public abstract void setColorScheme( BSColorScheme colorScheme );
 }
