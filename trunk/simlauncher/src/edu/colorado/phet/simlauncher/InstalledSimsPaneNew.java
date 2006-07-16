03a97d2cb8c40480726267a6f78dcbcc2536b210
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-07-16 17:17:16 +0000

    Added category list to installed sims pane

--- trunk/simlauncher/src/edu/colorado/phet/simlauncher/InstalledSimsPaneNew.java
+++ trunk/simlauncher/src/edu/colorado/phet/simlauncher/InstalledSimsPaneNew.java
@@ -10,8 +10,8 @@
  */
 package edu.colorado.phet.simlauncher;
 
-import edu.colorado.phet.simlauncher.actions.LaunchSimAction;
-import edu.colorado.phet.simlauncher.menus.InstalledSimPopupMenu;
+import edu.colorado.phet.simlauncher.actions.InstallOrUpdateSimAction;
+import edu.colorado.phet.simlauncher.menus.CatalogPopupMenu;
 import edu.colorado.phet.simlauncher.util.ChangeEventChannel;
 
 import javax.swing.*;
@@ -29,23 +29,22 @@ import java.util.ArrayList;
  * @author Ron LeMaster
  * @version $Revision$
  */
-public class InstalledSimsPaneNew extends JSplitPane implements Catalog.ChangeListener, SimContainer {
-//public class InstalledSimsPane extends JPanel implements Catalog.ChangeListener,
-//                                                         SimContainer {
+public class InstalledSimsPaneNew extends JSplitPane implements SimContainer, Catalog.ChangeListener {
 
-
-    private CategoryPanelNew categoryPanel;
-    private InstalledSimsPaneNew.MySimPane simulationPanel;
+    private CategoryPanel categoryPanel;
+    private InstalledSimsPane simulationPanel;
+//    private InstalledSimsPaneNew.SimPanel simulationPanel;
     private ChangeEventChannel changeEventChannel = new ChangeEventChannel();
 
-
+    /**
+     * Constructor
+     */
     public InstalledSimsPaneNew() {
         super( JSplitPane.HORIZONTAL_SPLIT, true );
 //        super( JSplitPane.HORIZONTAL_SPLIT, null, null );
 
-        simulationPanel = new InstalledSimsPaneNew.MySimPane();
-        categoryPanel = new CategoryPanelNew( simulationPanel );
-//    simulationPanel = new MySimPane();
+        categoryPanel = new CategoryPanel();
+        simulationPanel = new InstalledSimsPane();
 
         setLeftComponent( categoryPanel );
         JPanel rightPanel = new JPanel();
@@ -71,135 +70,164 @@ public class InstalledSimsPaneNew extends JSplitPane implements Catalog.ChangeLi
         changeEventChannel.removeListener( listener );
     }
 
-    public Simulation getSimulation() {
-        return simulationPanel.getSimulation();
-    }
-
-    public Simulation[] getSimulations() {
-        return simulationPanel.getSimulations();
-    }
+    //--------------------------------------------------------------------------------------------------
+    // Implementation of Catalog.ChangeListener
+    //--------------------------------------------------------------------------------------------------
 
     public void catalogChanged( Catalog.ChangeEvent event ) {
         simulationPanel.catalogChanged( event );
     }
 
-
     //--------------------------------------------------------------------------------------------------
     // Inner classes
     //--------------------------------------------------------------------------------------------------
 
-    private class MySimPane extends AbstractSimPanel {
+    /**
+     *
+     */
+    private class CategoryPanel extends JPanel {
+        private JList categoryJList;
+
+        public CategoryPanel() {
+            setBorder( BorderFactory.createTitledBorder( BorderFactory.createEtchedBorder(), "Categories" ) );
+            Category allSims = new Category( "All simulations", Catalog.instance().getAllSimulations() );
+            ArrayList categories = new ArrayList( );
+            categories.add( allSims );
+            categories.addAll( Catalog.instance().getCategories());
+            categoryJList = new JList( (Category[])( categories.toArray( new Category[ categories.size()] ) ) );
+            categoryJList.setSelectedValue( allSims, true );
+            add( categoryJList, BorderLayout.CENTER );
+
+            categoryJList.addMouseListener( new MouseAdapter() {
+                public void mouseClicked( MouseEvent e ) {
+                    // Get the simulations in the selected category
+                    Category category = categoryPanel.getSelectedCategory();
+                    java.util.List simListA = null;
+                    if( category == null ) {
+                        simListA = new ArrayList( Catalog.instance().getAllSimulations() );
+                    }
+                    else {
+                        simListA = new ArrayList( category.getSimulations() );
+                    }
+                    java.util.List installedSims = Catalog.instance().getInstalledSimulations();
+                    for( int i =simListA.size()-1; i >= 0; i-- ) {
+                        Object o = simListA.get( i );
+                        if( !installedSims.contains( o )) {
+                            simListA.remove( o );
+                        }
+                    }
+                    simulationPanel.updateSimTable( simListA );
+                }
+            } );
+        }
+
+        public Category getSelectedCategory() {
+            Category category = (Category)categoryJList.getSelectedValue();
+            return category;
+        }
+    }
+
+    /**
+     * The panel that contains the SimTable
+     */
+    private class SimPanel extends JPanel implements Catalog.ChangeListener, SimContainer {
         private SimTable simTable;
-        private SimTable.SimComparator simTableSortType = Options.instance().getInstalledSimulationsSortType();
+        private SimTable.SimComparator simTableSortType = SimTable.NAME_SORT;
         private JScrollPane simTableScrollPane;
-        private ChangeEventChannel changeEventChannel = new ChangeEventChannel();
-        private JButton launchBtn;
-        private GridBagConstraints tableGbc = new GridBagConstraints( 0, 1, 2, 1, 1, 1,
-                                                                      GridBagConstraints.CENTER,
-                                                                      GridBagConstraints.VERTICAL,
-                                                                      new Insets( 0, 0, 0, 0 ), 0, 0 );
+        private JButton installBtn;
         private GridBagConstraints headerGbc = new GridBagConstraints( 0, 0, 1, 1, 1, 0.1,
                                                                        GridBagConstraints.CENTER,
                                                                        GridBagConstraints.NONE,
-                                                                       new Insets( 10, 0, 0, 0 ), 0, 0 );
+                                                                       new Insets( 10, 0, 20, 0 ), 0, 0 );
+        private GridBagConstraints tableGbc = new GridBagConstraints( 0, 1, 2, 1, 1, 1,
+                                                                      GridBagConstraints.CENTER,
+                                                                      GridBagConstraints.BOTH,
+                                                                      new Insets( 0, 0, 0, 0 ), 0, 0 );
 
         /**
          * Constructor
          */
-        public MySimPane() {
+        public SimPanel() {
             super( new GridBagLayout() );
 
-            // Launch button
-            launchBtn = new JButton( "Launch" );
-            // Add an extension to the Launch action that resorts the table if the sort order is
-            // most-recently-used
-            launchBtn.addActionListener( new LaunchSimAction( this ) {
-                public void actionPerformed( ActionEvent e ) {
-                    super.actionPerformed( e );
-                    if( Options.instance().getInstalledSimulationsSortType().equals( SimTable.MOST_RECENTLY_USED_SORT ) )
-                    {
-                        updateSimTable();
-                    }
-                }
-            } );
-            launchBtn.setEnabled( false );
+            setBorder( BorderFactory.createTitledBorder( BorderFactory.createEtchedBorder(), "Simulatons" ) );
+
+            JPanel headerPanel = new JPanel( new BorderLayout() );
+            // Install button
+            installBtn = new JButton( "Install / Update" );
+            installBtn.addActionListener( new InstallOrUpdateSimAction( this, this ) );
+            installBtn.setEnabled( false );
             headerGbc.weightx = 1;
-            add( launchBtn, headerGbc );
+            add( installBtn, headerGbc );
 
             // "Show Thumbnails" checkbox
             final JCheckBox showThumbnailsCB = new JCheckBox( "Show thumbnails" );
             showThumbnailsCB.addActionListener( new AbstractAction() {
                 public void actionPerformed( ActionEvent e ) {
-                    Options.instance().setShowInstalledThumbnails( showThumbnailsCB.isSelected() );
+                    Options.instance().setShowCatalogThumbnails( showThumbnailsCB.isSelected() );
                 }
             } );
-            showThumbnailsCB.setSelected( Options.instance().isShowInstalledThumbnails() );
+            showThumbnailsCB.setSelected( Options.instance().isShowCatalogThumbnails() );
             headerGbc.gridx++;
-            headerGbc.weightx = 0.01;
+            headerPanel.add( showThumbnailsCB, BorderLayout.EAST );
+            headerGbc.weightx = .01;
             add( showThumbnailsCB, headerGbc );
 
-            // Creates the SimTable
-            updateSimTable();
-
-            // Listen for changes in Options
+            // Add a listener to the catalog that will update the sim table if the catalog changes
+            Catalog.instance().addChangeListener( this );
             Options.instance().addListener( new Options.ChangeListener() {
                 public void optionsChanged( Options.ChangeEvent event ) {
-                    if( simTableSortType != event.getOptions().getInstalledSimulationsSortType() ) {
-                        simTableSortType = event.getOptions().getInstalledSimulationsSortType();
-                    }
                     updateSimTable();
                 }
             } );
+            updateSimTable();
         }
 
         /**
-         * Creates the SimTable
+         *
          */
-        protected void updateSimTable() {
+        private void updateSimTable() {
             if( simTable != null ) {
-                simTableScrollPane.remove( simTable );
                 remove( simTableScrollPane );
+                simTableScrollPane.remove( simTable );
             }
 
+            // Get the simulations in the selected category
+            Category category = categoryPanel.getSelectedCategory();
+            java.util.List simListA = null;
+            if( category == null ) {
+                simListA = new ArrayList( Catalog.instance().getAllSimulations() );
+            }
+            else {
+                simListA = new ArrayList( category.getSimulations() );
+            }
+
+            // Create the SimulationTable
             ArrayList columns = new ArrayList();
+            columns.add( SimTable.SELECTION_CHECKBOX );
             columns.add( SimTable.NAME );
-            if( Options.instance().isShowInstalledThumbnails() ) {
+            if( Options.instance().isShowCatalogThumbnails() ) {
                 columns.add( SimTable.THUMBNAIL );
             }
+            columns.add( SimTable.IS_INSTALLED );
             columns.add( SimTable.IS_UP_TO_DATE );
-            simTable = new SimTable( Catalog.instance().getInstalledSimulations(),
+            simTable = new SimTable( simListA,
                                      simTableSortType,
                                      ListSelectionModel.SINGLE_SELECTION,
                                      columns );
 
-            // Add mouse handler
-            simTable.addMouseListener( new InstalledSimsPaneNew.MySimPane.MouseHandler() );
+            // Add a mouse handler to the table
+            simTable.addMouseListener( new MouseHandler() );
 
             simTableScrollPane = new JScrollPane( simTable );
             add( simTableScrollPane, tableGbc );
 
-            // Disable the lauch button. Since the table is new, there can't be any simulation selected
-            launchBtn.setEnabled( false );
+            // Disable the install button, since there is no selected simulation anymore
+            installBtn.setEnabled( false );
 
             // This is required to get rid of screen turds if the old table had a scrollbar and the
             // new one doesn't
             revalidate();
-            repaint();
-        }
-
-
-        /**
-         *
-         */
-        public void addChangeListener( ChangeEventChannel.ChangeListener listener ) {
-            changeEventChannel.addListener( listener );
-        }
-
-        /**
-         *
-         */
-        public void removeChangeListener( ChangeEventChannel.ChangeListener listener ) {
-            changeEventChannel.removeListener( listener );
         }
 
         //--------------------------------------------------------------------------------------------------
@@ -208,7 +236,7 @@ public class InstalledSimsPaneNew extends JSplitPane implements Catalog.ChangeLi
 
         public void catalogChanged( Catalog.ChangeEvent event ) {
             updateSimTable();
-            changeEventChannel.notifyChangeListeners( InstalledSimsPaneNew.this );
+            changeEventChannel.notifyChangeListeners( this );
         }
 
         //--------------------------------------------------------------------------------------------------
@@ -227,15 +255,32 @@ public class InstalledSimsPaneNew extends JSplitPane implements Catalog.ChangeLi
         // Handles mouse clicks on the simulation table
         //--------------------------------------------------------------------------------------------------
 
+        /**
+         * Handles enabling/disabling the intall button, checking and unchecking the check box, and right clicks
+         * popping up a context menu
+         */
         private class MouseHandler extends MouseAdapter {
             public void mouseClicked( MouseEvent event ) {
-                Simulation sim = simTable.getSelection();
-                launchBtn.setEnabled( sim != null );
+                // If a double left click, offer to install the simulation
+//                Simulation sim = simTable.getSelection();
+//                installBtn.setEnabled( sim != null );
+//                if( !event.isPopupTrigger() && event.getClickCount() == 2 ) {
+//                    int choice = JOptionPane.showConfirmDialog( UninstalledSimsPane.this,
+//                                                                "Do you want to install the simulation?",
+//                                                                "Confirm",
+//                                                                JOptionPane.OK_CANCEL_OPTION );
+//                    if( choice == JOptionPane.OK_OPTION ) {
+//                        try {
+//                            sim.install();
+//                        }
+//                        catch( SimResourceException e ) {
+//                            e.printStackTrace();
+//                        }
+//                    }
+//                }
 
-                // If a double left click, launch the simulation
-                if( !event.isPopupTrigger() && event.getClickCount() == 2 ) {
-                    sim.launch();
-                }
+                // The Install button should be enabled if any check boxes are checked
+                installBtn.setEnabled( simTable.getSelections().length > 0 );
 
                 // Notify change listeners
                 changeEventChannel.notifyChangeListeners( InstalledSimsPaneNew.this );
@@ -244,16 +289,37 @@ public class InstalledSimsPaneNew extends JSplitPane implements Catalog.ChangeLi
             // Required to get e.isPopupTrigger() to return true on right-click
             public void mouseReleased( MouseEvent event ) {
                 Simulation sim = simTable.getSelection();
-                launchBtn.setEnabled( sim != null );
 
-                // If it's a right click and a simulation is selected, pop up the context menu
+                // If right click, pop up context menu
                 if( event.isPopupTrigger() && sim != null ) {
-                    new InstalledSimPopupMenu( sim ).show( event.getComponent(), event.getX(), event.getY() );
+                    new CatalogPopupMenu( sim ).show( event.getComponent(), event.getX(), event.getY() );
                 }
 
                 // Notify change listeners
                 changeEventChannel.notifyChangeListeners( InstalledSimsPaneNew.this );
             }
         }
-}
+
+        private class SelectionFlipper extends MouseAdapter {
+            public void mouseClicked( MouseEvent e ) {
+                simTable.getSelection();
+                int selectedRow = simTable.getSelectedRow();
+                boolean oldValue = ( (Boolean)simTable.getValueAt( selectedRow, 0 ) ).booleanValue();
+                simTable.setValueAt( new Boolean( !oldValue ), selectedRow, 0 );
+            }
+        }
+    }
+
+    //--------------------------------------------------------------------------------------------------
+    // Implementation of SimulationContainer
+    //--------------------------------------------------------------------------------------------------
+
+    public Simulation getSimulation() {
+        return simulationPanel.getSimulation();
+    }
+
+    public Simulation[] getSimulations() {
+        return simulationPanel.getSimulations();
+    }
+
 }
