6e00d93687ae6bae4733487ed1740b0c022fa4f3
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-07-19 08:40:46 +0000

    moved value readouts north

--- trunk/schrodinger/src/edu/colorado/phet/qm/davissongermer/DGApplication.java
+++ trunk/schrodinger/src/edu/colorado/phet/qm/davissongermer/DGApplication.java
@@ -20,7 +20,7 @@ import edu.colorado.phet.qm.QWIPhetLookAndFeel;
  */
 
 public class DGApplication extends PiccoloPhetApplication {
-    public static String VERSION = "1.00.00";
+    public static String VERSION = "1.00";
     public static String TITLE = "Davisson-Germer: Electron Diffraction";
     public static String DESCRIPTION = "Simulate the original experiment that proved that electrons can behave as waves.  \n" +
                                        "Watch electrons diffract off a crystal of atoms, interfering with themselves \n" +
