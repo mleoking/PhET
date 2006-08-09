785fc7710b80b024d963025e1e64658f4f48ff7f
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-08-09 22:30:55 +0000

    sandbox

--- bound-states/src/edu/colorado/phet/boundstates/BSVersion.java
+++ bound-states/src/edu/colorado/phet/boundstates/BSVersion.java
@@ -20,11 +20,11 @@ package edu.colorado.phet.boundstates;
  */
 public class BSVersion {
   
-//    public static final String NUMBER = "sandbox";
-//    public static final String CVS_TAG = "boundstates-sandbox";
+    public static final String NUMBER = "sandbox";
+    public static final String CVS_TAG = "boundstates-sandbox";
     
-    public static final String NUMBER = "1.01.02";
-    public static final String CVS_TAG = "boundstates-1-01-02-2006-08-09";
+//    public static final String NUMBER = "1.01.02";
+//    public static final String CVS_TAG = "boundstates-1-01-02-2006-08-09";
     
     /* This class is not intended for instantiation. */
     private BSVersion() {}
