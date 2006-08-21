5feb0f5bd864696615df9639970eca521dc3e717
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-08-21 03:08:42 +0000

    il8n media controls

--- energyconservation3/src/edu/colorado/phet/ec3/EnergySkateParkStrings.java
+++ energyconservation3/src/edu/colorado/phet/ec3/EnergySkateParkStrings.java
@@ -15,6 +15,7 @@ public class EnergySkateParkStrings {
     }
 
     public static String getString( String s ) {
-        return "$" + SimStrings.get( s ) + "$";
+//        return "$" + SimStrings.get( s ) + "$";
+        return SimStrings.get( s );
     }
 }
