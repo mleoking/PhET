dfbffe90a7167acc751b44c0938d8de88e89112b
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-09-06 18:25:36 +0000

    cosmetics

--- molecular-reactions/src/edu/colorado/phet/molecularreactions/view/MoleculeInstanceControlPanel.java
+++ molecular-reactions/src/edu/colorado/phet/molecularreactions/view/MoleculeInstanceControlPanel.java
@@ -32,14 +32,16 @@ public class MoleculeInstanceControlPanel extends JPanel {
     public MoleculeInstanceControlPanel( MRModel model ) {
         super( new GridBagLayout() );
 
+        Insets insets = new Insets( 2, 2, 2, 2 );
+
         JLabel aLabel = new JLabel( "A" );
         JLabel bLabel = new JLabel( "B" );
         JLabel cLabel = new JLabel( "C" );
         JLabel abLabel = new JLabel( "AB" );
 
-        JTextField aTF = new MoleculeCounter( 8, MoleculeA.class, model );
-        JTextField bTF = new MoleculeCounter( 8, MoleculeB.class, model );
-        JTextField cTF = new MoleculeCounter( 8, MoleculeC.class, model );
+        JTextField aTF = new MoleculeCounter( 2, MoleculeA.class, model );
+        JTextField bTF = new MoleculeCounter( 2, MoleculeB.class, model );
+        JTextField cTF = new MoleculeCounter( 2, MoleculeC.class, model );
 //        JTextField abTF = new MoleculeCounter( 8, MoleculeAB.class, model );
 
         GridBagConstraints gbc = new GridBagConstraints( 0,
@@ -47,7 +49,8 @@ public class MoleculeInstanceControlPanel extends JPanel {
                                                          1, 1, 1, 1,
                                                          GridBagConstraints.EAST,
                                                          GridBagConstraints.NONE,
-                                                         new Insets( 0, 0, 0, 0 ), 0, 0 );
+                                                         insets,
+                                                         0, 0 );
         add( aLabel, gbc );
         add( bLabel, gbc );
         add( cLabel, gbc );
@@ -59,74 +62,4 @@ public class MoleculeInstanceControlPanel extends JPanel {
         add( bTF, gbc );
         add( cTF, gbc );
     }
-
-//    public class MoleculeCounter extends JTextField implements PublishingModel.ModelListener {
-//        private Class moleculeClass;
-//        private int cnt;
-//        // Flag to mark that we are adding or removing molecules from the model,
-//        // so that we don't respond to add/remove messages from the model
-//        private boolean selfUpdating;
-//
-//        public MoleculeCounter( int columns, final Class moleculeClass, final MRModel model ) {
-//            super( columns );
-//            this.moleculeClass = moleculeClass;
-//            model.addListener( this );
-//            setText( "0" );
-//
-//            this.addActionListener( new ActionListener() {
-//                public void actionPerformed( ActionEvent e ) {
-//                    selfUpdating = true;
-//                    int diff = Integer.parseInt( getText() ) - cnt;
-//
-//                    for( int i = 0; i < Math.abs( diff ); i++ ) {
-//
-//                        // Do we need to add molecules?
-//                        if( diff > 0 ) {
-//                            Point2D p = new Point2D.Double( model.getBox().getMinX() + 20,
-//                                                            model.getBox().getMinY() + 20 );
-//                            Vector2D v = new Vector2D.Double( 2, 2 );
-//                            Molecule m = MoleculeFactory.createMolecule( moleculeClass,
-//                                                                         p,
-//                                                                         v );
-//                            model.addModelElement( m );
-//                            cnt++;
-//                        }
-//
-//                        // Do we need to remove molecules?
-//                        else if( diff < 0 ) {
-//                            List modelElements = model.getModelElements();
-//                            for( int j = modelElements.size() - 1; j >= 0; j-- ) {
-//                                Object o = modelElements.get( j );
-//                                if( moleculeClass.isInstance( o ) && !((Molecule)o).isPartOfComposite() ) {
-//                                    Molecule molecule = (Molecule)o;
-//                                    model.removeModelElement( molecule );
-//                                    cnt--;
-//                                    break;
-//                                }
-//                            }
-//                            // We need to set the value in the text field in case we were asked to remove a
-//                            // molecule that couldn't be removed
-//                            setText( Integer.toString( cnt ));
-//                        }
-//                    }
-//
-//                    selfUpdating = false;
-//                }
-//            } );
-//        }
-//
-//        public void modelElementAdded( ModelElement element ) {
-//            if( !selfUpdating && moleculeClass.isInstance( element ) ) {
-//                cnt = Integer.parseInt( getText() );
-//                setText( Integer.toString( ++cnt ) );
-//            }
-//        }
-//
-//        public void modelElementRemoved( ModelElement element ) {
-//            if( !selfUpdating && moleculeClass.isInstance( element ) ) {
-//                int cnt = Integer.parseInt( getText() );
-//                setText( Integer.toString( --cnt ) );
-//            }
-//        }
-//    }
 }
