f842db949bd63f63f0a9a3788e0c2c6d2a53eed4
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-09-06 17:50:21 +0000

    - rename HAGunNode -> GunNode - show/hide atomic model selector based on mode switch

--- trunk/hydrogen-atom/src/edu/colorado/phet/hydrogenatom/module/HAModule.java
+++ trunk/hydrogen-atom/src/edu/colorado/phet/hydrogenatom/module/HAModule.java
@@ -15,13 +15,16 @@ import java.awt.Dimension;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
 import edu.colorado.phet.common.view.util.SimStrings;
 import edu.colorado.phet.hydrogenatom.HAConstants;
 import edu.colorado.phet.hydrogenatom.control.AtomicModelSelector;
 import edu.colorado.phet.hydrogenatom.control.HAClockControlPanel;
 import edu.colorado.phet.hydrogenatom.control.ModeSwitch;
 import edu.colorado.phet.hydrogenatom.model.HAClock;
-import edu.colorado.phet.hydrogenatom.view.HAGunNode;
+import edu.colorado.phet.hydrogenatom.view.GunNode;
 import edu.colorado.phet.piccolo.PhetPCanvas;
 import edu.colorado.phet.piccolo.PiccoloModule;
 import edu.colorado.phet.piccolo.help.HelpPane;
@@ -42,6 +45,10 @@ public class HAModule extends PiccoloModule {
     
     private PhetPCanvas _canvas;
     private PNode _rootNode;
+    
+    private ModeSwitch _modeSwitch;
+    private AtomicModelSelector _atomicModelSelector;
+    private GunNode _gunNode;
     
     private HAClockControlPanel _clockControlPanel;
     
@@ -76,25 +83,32 @@ public class HAModule extends PiccoloModule {
             _canvas.addWorldChild( _rootNode );
         }
         
-        // Mode switch
+        // Mode switch (experiment/prediction)
         {
-            ModeSwitch modeSwitch = new ModeSwitch( _canvas );
-            modeSwitch.setOffset( 30, 10 );
-            _rootNode.addChild( modeSwitch );
+            _modeSwitch = new ModeSwitch( _canvas );
+            _modeSwitch.setOffset( 30, 10 );
+            _rootNode.addChild( _modeSwitch );
+            _modeSwitch.addChangeListener( new ChangeListener() {
+               public void stateChanged( ChangeEvent event ) {
+                   _atomicModelSelector.setVisible( _modeSwitch.isPredictionSelected() );
+                   _atomicModelSelector.setPickable( _modeSwitch.isPredictionSelected() );
+                   _atomicModelSelector.setChildrenPickable( _modeSwitch.isPredictionSelected() );
+               }
+            });
         }
         
         // Atomic Model selector
         {
-            AtomicModelSelector atomicModelSelector = new AtomicModelSelector( _canvas );
-            atomicModelSelector.setOffset( 300, 10 );
-            _rootNode.addChild( atomicModelSelector );
+            _atomicModelSelector = new AtomicModelSelector( _canvas );
+            _atomicModelSelector.setOffset( 300, 10 );
+            _rootNode.addChild( _atomicModelSelector );
         }
         
         // Gun
         {
-            HAGunNode gunNode = new HAGunNode( _canvas );
-            gunNode.setOffset( 30, 200 );
-            _rootNode.addChild( gunNode );
+            _gunNode = new GunNode( _canvas );
+            _gunNode.setOffset( 30, 200 );
+            _rootNode.addChild( _gunNode );
         }
         
         //----------------------------------------------------------------------------
