9a8ad3fa60e5b8e1ce90b363d88094c480cdf928
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-09-06 19:08:51 +0000

    (9/6/06) - add placeholders for energy diagrams (9/6/06) - add "Show electron energy level diagram" checkbox (9/6/06) - add "Show spectrometer" checkbox

---
+++ hydrogen-atom/src/edu/colorado/phet/hydrogenatom/energydiagrams/AbstractEnergyDiagram.java
@@ -0,0 +1,27 @@
+/* Copyright 2006, University of Colorado */
+
+/*
+ * CVS Info -
+ * Filename : $Source$
+ * Branch : $Name$
+ * Modified by : $Author$
+ * Revision : $Revision$
+ * Date modified : $Date$
+ */
+
+package edu.colorado.phet.hydrogenatom.energydiagrams;
+
+import edu.colorado.phet.piccolo.util.PImageFactory;
+import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.nodes.PImage;
+
+
+public abstract class AbstractEnergyDiagram extends PNode {
+
+    public AbstractEnergyDiagram( String imageName ) {
+        super();
+        
+        PImage image = PImageFactory.create( imageName );
+        addChild( image );
+    }
+}
