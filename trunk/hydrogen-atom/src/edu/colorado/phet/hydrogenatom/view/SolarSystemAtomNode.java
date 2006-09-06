5e3cabb8aa221fb1dcb7b096066ac7c6c8b0c2a2
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-09-06 21:55:41 +0000

    (9/6/06) - add placeholders for visual representation of atomic models

---
+++ trunk/hydrogen-atom/src/edu/colorado/phet/hydrogenatom/view/SolarSystemAtomNode.java
@@ -0,0 +1,25 @@
+/* Copyright 2006, University of Colorado */
+
+/*
+ * CVS Info -
+ * Filename : $Source$
+ * Branch : $Name$
+ * Modified by : $Author$
+ * Revision : $Revision$
+ * Date modified : $Date$
+ */
+
+package edu.colorado.phet.hydrogenatom.view;
+
+import edu.colorado.phet.hydrogenatom.HAConstants;
+import edu.colorado.phet.piccolo.util.PImageFactory;
+import edu.umd.cs.piccolo.nodes.PImage;
+
+
+public class SolarSystemAtomNode extends HANode {
+
+    public SolarSystemAtomNode() {
+        PImage image = PImageFactory.create( HAConstants.IMAGE_SOLAR_SYSTEM_ATOM );
+        addChild( image );
+    }
+}
