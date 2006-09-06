9e7e229929d1013b9d3d94aa5ecd39b31e144fa0
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-09-06 16:04:43 +0000

    *** empty log message ***

--- hydrogen-atom/src/edu/colorado/phet/hydrogenatom/view/HAGunNode.java
+++ hydrogen-atom/src/edu/colorado/phet/hydrogenatom/view/HAGunNode.java
@@ -67,7 +67,7 @@ public class HAGunNode extends PNode {
             }
         });
        
-        // Cable that connects gun to control panels
+        // Cable that physically connects gun to control panels
         PImage cableNode = PImageFactory.create( HAConstants.IMAGE_GUN_CONTROL_CABLE );
         
         // Add nodes in background-to-foreground order
