46d3e90d41cbac433441e9cb0ac7b67b8b65a370
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-09-07 18:45:04 +0000

    *** empty log message ***

--- trunk/cmalley/src/com/pixelzoom/examples/JFreeAboutDialog.java
+++ trunk/cmalley/src/com/pixelzoom/examples/JFreeAboutDialog.java
@@ -1,13 +1,3 @@
-/* Copyright 2006, University of Colorado */
-
-/*
- * CVS Info -
- * Filename : $Source$
- * Branch : $Name$
- * Modified by : $Author$
- * Revision : $Revision$
- * Date modified : $Date$
- */
 
 package com.pixelzoom.examples;
 
