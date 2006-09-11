f94b55e8fab0542592136642053b343661b83d9b
nobody <nobody@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-09-11 16:29:43 +0000

    This commit was manufactured by cvs2svn to create tag 'cck-9-12-2006'.

---
+++ tags/cck-9-12-2006/cck3/src/edu/colorado/phet/cck3/HasCapacitorClip.java
@@ -0,0 +1,13 @@
+package edu.colorado.phet.cck3;
+
+import java.awt.*;
+
+/**
+ * User: Sam Reid
+ * Date: Jul 8, 2006
+ * Time: 8:36:14 PM
+ * Copyright (c) Jul 8, 2006 by Sam Reid
+ */
+public interface HasCapacitorClip {
+    Shape getCapacitorClip();
+}
