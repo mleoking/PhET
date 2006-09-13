3a961103e81d089e22afe762ee66d629ceba5cf0
nobody <nobody@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-09-13 23:16:43 +0000

    This commit was manufactured by cvs2svn to create tag 'molecular-reactions-hard-collisions-9-13-06'.

---
+++ tags/molecular-reactions-hard-collisions-9-13-06/molecular-reactions/src/edu/colorado/phet/molecularreactions/view/EnergySimpleMoleculeGraphic.java
@@ -0,0 +1,30 @@
+/* Copyright 2003-2004, University of Colorado */
+
+/*
+ * CVS Info -
+ * Filename : $Source$
+ * Branch : $Name$
+ * Modified by : $Author$
+ * Revision : $Revision$
+ * Date modified : $Date$
+ */
+package edu.colorado.phet.molecularreactions.view;
+
+import edu.colorado.phet.molecularreactions.model.SimpleMolecule;
+
+/**
+ * SimpleMoleculeGraphic
+ *
+ * @author Ron LeMaster
+ * @version $Revision$
+ */
+public class EnergySimpleMoleculeGraphic extends AbstractSimpleMoleculeGraphic {
+
+    public EnergySimpleMoleculeGraphic( SimpleMolecule molecule ) {
+        super( molecule );
+    }
+
+    public void update() {
+        super.update();
+    }
+}
