425f50ac420a8a1a8068b51faede8bef9351edaa
nobody <nobody@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-09-13 19:00:44 +0000

    This commit was manufactured by cvs2svn to create tag 'cck-9-13-2006-i'.

---
+++ tags/cck-9-13-2006-i/cck3/phetcommon-cck/src/edu/colorado/phet/common_cck/view/graphics/mousecontrols/Translatable.java
@@ -0,0 +1,11 @@
+package edu.colorado.phet.common_cck.view.graphics.mousecontrols;
+
+/**
+ * User: Sam Reid
+ * Date: Oct 9, 2003
+ * Time: 12:29:00 AM
+ * Copyright (c) Oct 9, 2003 by Sam Reid
+ */
+public interface Translatable {
+    public void translate( double dx, double dy );
+}
