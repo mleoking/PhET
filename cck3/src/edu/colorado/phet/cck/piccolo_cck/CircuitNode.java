4e553f119bf32d85eb04622758f0365744231248
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-09-16 05:16:01 +0000

    added junction code

--- cck3/src/edu/colorado/phet/cck/piccolo_cck/CircuitNode.java
+++ cck3/src/edu/colorado/phet/cck/piccolo_cck/CircuitNode.java
@@ -9,6 +9,9 @@ import edu.colorado.phet.cck.model.components.Wire;
 import edu.colorado.phet.piccolo.PhetPNode;
 import edu.umd.cs.piccolo.PNode;
 
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+
 /**
  * User: Sam Reid
  * Date: Sep 14, 2006
@@ -19,6 +22,7 @@ import edu.umd.cs.piccolo.PNode;
 public class CircuitNode extends PhetPNode {
     private CCKModel cckModel;
     private Circuit circuit;
+    private ArrayList junctionGraphics = new ArrayList();
 
     public CircuitNode( CCKModel cckModel, Circuit circuit ) {
         this.cckModel = cckModel;
@@ -29,22 +33,60 @@ public class CircuitNode extends PhetPNode {
             }
 
             public void junctionAdded( Junction junction ) {
-                addChild( createNode( junction ) );
+                JunctionNode node = createNode( junction );
+                junctionGraphics.add( node );
+                addChild( node );
+            }
+
+            public void junctionRemoved( Junction junction ) {
+                for( int i = 0; i < junctionGraphics.size(); i++ ) {
+                    JunctionNode junctionNode = (JunctionNode)junctionGraphics.get( i );
+                    if( junctionNode.getJunction() == junction ) {
+                        removeJunctionGraphic( junctionNode );
+                    }
+                }
             }
         } );
     }
 
-    public PNode createNode( Junction junction ) {
-        return new JunctionNode( cckModel, junction );
+    private void removeJunctionGraphic( JunctionNode junctionNode ) {
+        junctionGraphics.remove( junctionNode );
+        removeChild( junctionNode );
+    }
+
+    public JunctionNode createNode( Junction junction ) {
+        return new JunctionNode( cckModel, junction, this );
     }
 
     public PNode createNode( Branch branch ) {
         if( branch instanceof Wire ) {
-            return new WireNode( (Wire)branch );
+            return new WireNode( cckModel, (Wire)branch );
         }
         else {
             throw new RuntimeException( "Unrecognized branch type: " + branch.getClass() );
         }
     }
 
+    public Junction getStickyTarget( Junction junction, double x, double y ) {
+        double bestDist = Double.POSITIVE_INFINITY;
+        double STICKY_DIST = 2;
+        Junction best = null;
+        for( int i = 0; i < junctionGraphics.size(); i++ ) {
+            //todo see circuitGraphic line 502 for handling strong components
+            JunctionNode junctionNode = (JunctionNode)junctionGraphics.get( i );
+            if( junctionNode.getJunction() != junction && !getCircuit().hasBranch( junction, junctionNode.getJunction() ) && !getCircuit().wouldConnectionCauseOverlappingBranches( junction, junctionNode.getJunction() ) )
+            {
+                double dist = new Point2D.Double( x, y ).distance( junctionNode.getJunction().getX(), junctionNode.getJunction().getY() );
+                if( dist < STICKY_DIST && dist < bestDist ) {
+                    best = junctionNode.getJunction();
+                    bestDist = dist;
+                }
+            }
+        }
+        return best;
+    }
+
+    private Circuit getCircuit() {
+        return cckModel.getCircuit();
+    }
 }
