52025e8bc61620f9e30d610fbd79459914f5fe4f
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-09-18 17:50:03 +0000

    *** empty log message ***

--- hydrogen-atom/test-src/edu/colorado/phet/hydrogenatom/test/TestWavelengthControl.java
+++ hydrogen-atom/test-src/edu/colorado/phet/hydrogenatom/test/TestWavelengthControl.java
@@ -16,6 +16,8 @@ import java.awt.Font;
 
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import edu.colorado.phet.common.view.util.VisibleColor;
 import edu.colorado.phet.hydrogenatom.control.WavelengthControl;
@@ -55,38 +57,43 @@ public class TestWavelengthControl extends JFrame {
         c2.setOffset( xOffset, yOffset );
         yOffset += ySpacing;
         
-        // UV & IR with default colors
+        // UV & IR
         WavelengthControl c3 = new WavelengthControl( canvas, 90, 900 );
         canvas.getLayer().addChild( c3 );
         c3.setOffset( xOffset, yOffset );
         yOffset += ySpacing;
         
-        // UV & IR with custom colors, fonts, etc
-        WavelengthControl c4 = new WavelengthControl( canvas, 90, 900, uvTrackColor, uvLabelColor, irTrackColor, irLabelColor );
-        c4.setWavelength( 300 );
-        c4.setTextFieldColors( Color.RED, Color.BLACK );
-        c4.setUnitsForeground( Color.BLUE );
-        c4.setTextFieldFont( new Font( "Lucida Sans", Font.PLAIN, 20 ) );
-        c4.setUnitsFont( new Font( "Lucida Sans", Font.ITALIC, 14 ) );
-        c4.setTextFieldColumns( 5 );
+        // UV only
+        WavelengthControl c4 = new WavelengthControl( canvas, 90, VisibleColor.MAX_WAVELENGTH );
         canvas.getLayer().addChild( c4 );
         c4.setOffset( xOffset, yOffset );
         yOffset += ySpacing;
         
-        // UV & IR with no room for labels
-        WavelengthControl c5 = new WavelengthControl( canvas, VisibleColor.MIN_WAVELENGTH - 10, VisibleColor.MAX_WAVELENGTH + 10 );
+        // IR only
+        WavelengthControl c5 = new WavelengthControl( canvas, VisibleColor.MIN_WAVELENGTH, 900 );
         canvas.getLayer().addChild( c5 );
         c5.setOffset( xOffset, yOffset );
         yOffset += ySpacing;
         
-        // UV only
-        WavelengthControl c6 = new WavelengthControl( canvas, 90, VisibleColor.MAX_WAVELENGTH );
+        // UV & IR with no room for labels
+        WavelengthControl c6 = new WavelengthControl( canvas, VisibleColor.MIN_WAVELENGTH - 15, VisibleColor.MAX_WAVELENGTH + 15 );
         canvas.getLayer().addChild( c6 );
         c6.setOffset( xOffset, yOffset );
         yOffset += ySpacing;
         
-        // IR only
-        WavelengthControl c7 = new WavelengthControl( canvas, VisibleColor.MIN_WAVELENGTH, 900 );
+        // UV & IR with custom colors, fonts, etc
+        final WavelengthControl c7 = new WavelengthControl( canvas, 90, 900, uvTrackColor, uvLabelColor, irTrackColor, irLabelColor );
+        c7.setWavelength( 300 );
+        c7.setTextFieldColors( c7.getWavelengthColor(), Color.BLACK );
+        c7.addChangeListener( new ChangeListener() { 
+            public void stateChanged( ChangeEvent event ) {
+                c7.setTextFieldColors( c7.getWavelengthColor(), Color.WHITE );
+            }
+        } );
+        c7.setUnitsForeground( Color.BLUE );
+        c7.setTextFieldFont( new Font( "Lucida Sans", Font.PLAIN, 20 ) );
+        c7.setUnitsFont( new Font( "Lucida Sans", Font.ITALIC, 14 ) );
+        c7.setTextFieldColumns( 5 );
         canvas.getLayer().addChild( c7 );
         c7.setOffset( xOffset, yOffset );
         yOffset += ySpacing;
