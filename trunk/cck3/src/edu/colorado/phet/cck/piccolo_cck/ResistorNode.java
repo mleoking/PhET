a2b3cb25aefea51d4776ecc8c1b80b671900af43
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-09-18 02:37:23 +0000

    started other circuit component graphics

--- trunk/cck3/src/edu/colorado/phet/cck/piccolo_cck/ResistorNode.java
+++ trunk/cck3/src/edu/colorado/phet/cck/piccolo_cck/ResistorNode.java
@@ -3,6 +3,7 @@ package edu.colorado.phet.cck.piccolo_cck;
 import edu.colorado.phet.cck.CCKImageSuite;
 import edu.colorado.phet.cck.model.CCKModel;
 import edu.colorado.phet.cck.model.components.Resistor;
+import edu.colorado.phet.common.math.Vector2D;
 import edu.colorado.phet.common_cck.util.SimpleObserver;
 import edu.colorado.phet.piccolo.PhetPNode;
 import edu.colorado.phet.piccolo.event.CursorHandler;
@@ -11,7 +12,7 @@ import edu.umd.cs.piccolo.event.PInputEvent;
 import edu.umd.cs.piccolo.nodes.PImage;
 import edu.umd.cs.piccolo.util.PDimension;
 
-import java.awt.geom.Line2D;
+import java.awt.geom.AffineTransform;
 
 /**
  * User: Sam Reid
@@ -29,7 +30,6 @@ public class ResistorNode extends PhetPNode {
         this.model = model;
         this.resistor = resistor;
         pImage = new PImage( CCKImageSuite.getInstance().getLifelikeSuite().getResistorImage() );
-        pImage.scale( 0.01 );
         addChild( pImage );
         update();
         resistor.addObserver( new SimpleObserver() {
@@ -40,7 +40,7 @@ public class ResistorNode extends PhetPNode {
         addInputEventListener( new CursorHandler() );
         addInputEventListener( new PBasicInputEventHandler() {
             public void mouseDragged( PInputEvent event ) {
-                PDimension delta = event.getDeltaRelativeTo( ResistorNode.this );
+                PDimension delta = event.getDeltaRelativeTo( ResistorNode.this.getParent() );
                 resistor.translate( delta.width, delta.height );
             }
 
@@ -51,14 +51,15 @@ public class ResistorNode extends PhetPNode {
     }
 
     private void update() {
-        Line2D.Double wireLine = new Line2D.Double( resistor.getStartPoint(), resistor.getEndPoint() );
         double resistorLength = resistor.getStartPoint().distance( resistor.getEndPoint() );
         double imageLength = pImage.getFullBounds().getWidth();
         double scale = resistorLength / imageLength;
+        double angle = new Vector2D.Double( resistor.getStartPoint(), resistor.getEndPoint() ).getAngle();
+        setTransform( new AffineTransform() );
         setScale( scale );
         setOffset( resistor.getStartPoint() );
-        translate( 0, -getFullBounds().getHeight() / 4 );
-//        setOffset( Math.random() * 1, Math.random() * 1 );
+        rotate( angle );
+        translate( 0, -pImage.getFullBounds().getHeight() / 2 );
     }
 
 }
