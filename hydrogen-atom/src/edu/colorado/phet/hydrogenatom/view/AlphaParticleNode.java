7827e43ba81a67bc5e98677b0b72b412348289a1
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-09-25 17:28:23 +0000

    (9/22/06) - create proton, neutron, electron images programmatically

--- hydrogen-atom/src/edu/colorado/phet/hydrogenatom/view/AlphaParticleNode.java
+++ hydrogen-atom/src/edu/colorado/phet/hydrogenatom/view/AlphaParticleNode.java
@@ -11,20 +11,55 @@
 
 package edu.colorado.phet.hydrogenatom.view;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Stroke;
+
 import edu.colorado.phet.hydrogenatom.HAConstants;
 import edu.colorado.phet.piccolo.PhetPNode;
-import edu.colorado.phet.piccolo.util.PImageFactory;
-import edu.umd.cs.piccolo.nodes.PImage;
+import edu.umd.cs.piccolo.PNode;
 
 
 public class AlphaParticleNode extends PhetPNode {
 
-    public AlphaParticleNode() {
+    private static final double OVERLAP = 0.333;
+    private static final Stroke STROKE = new BasicStroke( 0.5f );
+    private static final Color STROKE_COLOR = Color.BLACK;
+    
+    public AlphaParticleNode( boolean outline ) {
         super();
-
-        PImage imageNode = PImageFactory.create( HAConstants.IMAGE_ALPHA_PARTICLE );
-        addChild( imageNode );
         
-        imageNode.setOffset( -imageNode.getFullBounds().getWidth()/2, -imageNode.getFullBounds().getHeight()/2 );
+        PNode parent = new PNode();
+        ProtonNode p1 = new ProtonNode();
+        p1.setStroke( STROKE, STROKE_COLOR );
+        ProtonNode p2 = new ProtonNode();
+        p2.setStroke( STROKE, STROKE_COLOR );
+        NeutronNode n1 = new NeutronNode();
+        n1.setStroke( STROKE, STROKE_COLOR );
+        NeutronNode n2 = new NeutronNode();
+        n2.setStroke( STROKE, STROKE_COLOR );
+        
+        addChild( parent );
+        parent.addChild( p2 );
+        parent.addChild( n2 );
+        parent.addChild( p1 );
+        parent.addChild( n1 );
+        
+        final double xOffset = ( 1 - OVERLAP ) * p1.getFullBounds().getWidth();
+        final double yOffset = ( 1 - OVERLAP ) * p1.getFullBounds().getHeight();
+        p1.setOffset( 0, 0 );
+        p2.setOffset( xOffset, yOffset );
+        n1.setOffset( xOffset, 0 );
+        n2.setOffset( 0, yOffset );
+        
+        // move the origin to the center
+        double x = -( getFullBounds().getWidth() / 2 ) + ( getBounds().getX() - getFullBounds().getX() );
+        double y = -( getFullBounds().getHeight() / 2 ) + ( getBounds().getY() - getFullBounds().getY() );
+        parent.setOffset( x, y );
+        
+        if ( HAConstants.SHOW_ORIGIN_NODES ) {
+            OriginNode originNode = new OriginNode( Color.GREEN );
+            addChild( originNode );
+        }
     }
 }
--- hydrogen-atom/src/edu/colorado/phet/hydrogenatom/view/ProtonNode.java
+++ hydrogen-atom/src/edu/colorado/phet/hydrogenatom/view/ProtonNode.java
@@ -12,28 +12,39 @@
 package edu.colorado.phet.hydrogenatom.view;
 
 import java.awt.Color;
+import java.awt.Paint;
+import java.awt.Shape;
+import java.awt.Stroke;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Point2D;
 
-import edu.colorado.phet.hydrogenatom.HAConstants;
+import edu.colorado.phet.hydrogenatom.util.RoundGradientPaint;
 import edu.colorado.phet.piccolo.PhetPNode;
-import edu.colorado.phet.piccolo.util.PImageFactory;
-import edu.umd.cs.piccolo.nodes.PImage;
+import edu.umd.cs.piccolo.nodes.PPath;
 
 
 public class ProtonNode extends PhetPNode {
 
-    private static final double DESIRED_DIAMETER = 11;
+    private static final double DIAMETER = 11;
+    private static final Color COLOR = new Color( 255, 0, 0 );
+    private static final Color HILITE_COLOR = new Color( 255, 130, 130 );
+    private static final Paint ROUND_GRADIENT = new RoundGradientPaint( 0, DIAMETER/6, HILITE_COLOR, new Point2D.Double( DIAMETER/4, DIAMETER/4 ), COLOR );
+    
+    private PPath _pathNode;
     
     public ProtonNode() {
         super();
 
-        PImage imageNode = PImageFactory.create( HAConstants.IMAGE_PROTON );
-        addChild( imageNode );
+        Shape shape = new Ellipse2D.Double( -DIAMETER/2, -DIAMETER/2, DIAMETER, DIAMETER );
+        _pathNode = new PPath( shape );
+        _pathNode.setPaint( ROUND_GRADIENT );
+        _pathNode.setStroke( null );   
         
-        double imageDiameter = imageNode.getFullBounds().getWidth();
-        double scale = DESIRED_DIAMETER / imageDiameter;
-        double x = scale * -( imageNode.getFullBounds().getWidth() / 2 );
-        double y = scale * -( imageNode.getFullBounds().getHeight() / 2 );
-        imageNode.translate( x, y );
-        imageNode.scale( scale );
+        addChild( _pathNode );
+    }
+    
+    public void setStroke( Stroke stroke, Color color ) {
+        _pathNode.setStroke( stroke );
+        _pathNode.setStrokePaint( color );
     }
 }
