7333ab005f29250c48d77e17cfadfcd561b48967
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-09-25 18:43:06 +0000

    no message

--- trunk/cck3/src/edu/colorado/phet/cck/piccolo_cck/VoltmeterModel.java
+++ trunk/cck3/src/edu/colorado/phet/cck/piccolo_cck/VoltmeterModel.java
@@ -12,10 +12,15 @@ import java.util.ArrayList;
 
 public class VoltmeterModel {
     private boolean visible = false;
-    private Point2D.Double unit = new Point2D.Double();
+    private UnitModel unitModel;
     private ArrayList listeners = new ArrayList();
+    private LeadModel redLead;
+    private LeadModel blackLead;
 
     public VoltmeterModel() {
+        redLead = new LeadModel( new Point2D.Double( 0, 0 ) );
+        blackLead = new LeadModel( new Point2D.Double( 1, 0 ) );
+        unitModel = new UnitModel();
     }
 
     public boolean isVisible() {
@@ -28,14 +33,16 @@ public class VoltmeterModel {
         notifyListeners();
     }
 
-    public Point2D getUnitOffset() {
-        return new Point2D.Double( unit.x, unit.y );
+    public LeadModel getBlackLeadModel() {
+        return blackLead;
     }
 
-    public void translateBody( double dx, double dy ) {
-        unit.x += dx;
-        unit.y += dy;
-        notifyListeners();
+    public LeadModel getRedLeadModel() {
+        return redLead;
+    }
+
+    public UnitModel getUnitModel() {
+        return unitModel;
     }
 
     public static interface Listener {
@@ -50,6 +57,77 @@ public class VoltmeterModel {
         for( int i = 0; i < listeners.size(); i++ ) {
             Listener listener = (Listener)listeners.get( i );
             listener.voltmeterChanged();
+        }
+    }
+
+    public static class LeadModel {
+        private Point2D.Double tipLocation;
+        private Point2D.Double tailLocation;
+
+        public LeadModel() {
+            this( new Point2D.Double() );
+        }
+
+        public LeadModel( Point2D.Double tipLocation ) {
+            this.tipLocation = new Point2D.Double( tipLocation.getX(), tipLocation.getY() );
+        }
+
+        public void translate( double dx, double dy ) {
+            tipLocation.x += dx;
+            tipLocation.y += dy;
+            notifyListeners();
+        }
+
+        public Point2D getTipLocation() {
+            return tipLocation;
+        }
+
+        static interface Listener {
+            void leadModelChanged();
+        }
+
+        ArrayList listeners = new ArrayList();
+
+        public void addListener( Listener listener ) {
+            listeners.add( listener );
+        }
+
+        public void notifyListeners() {
+            for( int i = 0; i < listeners.size(); i++ ) {
+                Listener listener = (Listener)listeners.get( i );
+                listener.leadModelChanged();
+            }
+        }
+    }
+
+    public static class UnitModel {
+        private Point2D.Double location = new Point2D.Double();
+
+        public Point2D getLocation() {
+            return new Point2D.Double( location.x, location.y );
+        }
+
+        public void translateBody( double dx, double dy ) {
+            location.x += dx;
+            location.y += dy;
+            notifyListeners();
+        }
+
+        static interface Listener {
+            void unitModelChanged();
+        }
+
+        ArrayList listeners = new ArrayList();
+
+        public void addListener( Listener listener ) {
+            listeners.add( listener );
+        }
+
+        public void notifyListeners() {
+            for( int i = 0; i < listeners.size(); i++ ) {
+                Listener listener = (Listener)listeners.get( i );
+                listener.unitModelChanged();
+            }
         }
     }
 }
--- trunk/cck3/src/edu/colorado/phet/cck/piccolo_cck/VoltmeterNode.java
+++ trunk/cck3/src/edu/colorado/phet/cck/piccolo_cck/VoltmeterNode.java
@@ -1,12 +1,17 @@
 package edu.colorado.phet.cck.piccolo_cck;
 
 import edu.colorado.phet.piccolo.PhetPNode;
+import edu.colorado.phet.piccolo.nodes.RegisterablePNode;
 import edu.colorado.phet.piccolo.util.PImageFactory;
 import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
 import edu.umd.cs.piccolo.event.PInputEvent;
 import edu.umd.cs.piccolo.nodes.PImage;
 import edu.umd.cs.piccolo.nodes.PPath;
 import edu.umd.cs.piccolo.util.PDimension;
+
+import java.awt.*;
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
 
 /**
  * User: Sam Reid
@@ -17,35 +22,153 @@ import edu.umd.cs.piccolo.util.PDimension;
 
 public class VoltmeterNode extends PhetPNode {
     private VoltmeterModel voltmeterModel;
-    private PImage unitImageNode;
-    private PImage blackProbe;
-    private PImage redProbe;
-    private PPath redCable;
-    private PPath blackCable;
+    private UnitNode unitImageNode;
+    private LeadNode blackProbe;
+    private LeadNode redProbe;
+    private CableNode redCable;
+    private CableNode blackCable;
+    private static final double SCALE = 1.0 / 80.0 * 1.2;
 
     public VoltmeterNode( final VoltmeterModel voltmeterModel ) {
         this.voltmeterModel = voltmeterModel;
-        unitImageNode = PImageFactory.create( "images/vm3.gif" );
-        redProbe = PImageFactory.create( "images/probeRed.gif" );
-        blackProbe = PImageFactory.create( "images/probeBlack.gif" );
-        unitImageNode.scale( 1.0 / 80.0 );
+        unitImageNode = new UnitNode( voltmeterModel.getUnitModel() );
+        redProbe = new LeadNode( "images/probeRed.gif", voltmeterModel.getRedLeadModel() );
+        blackProbe = new LeadNode( "images/probeBlack.gif", voltmeterModel.getBlackLeadModel() );
+
         addChild( unitImageNode );
+
+        Point2D redUnitConnectionPt = new Point2D.Double( 12 * SCALE, 218 * SCALE );
+        Point2D blackUnitConnectionPt = new Point2D.Double( 88 * SCALE, 218 * SCALE );
+        redCable = new CableNode( Color.red, redProbe,
+                                  voltmeterModel.getUnitModel(),
+                                  voltmeterModel.getRedLeadModel(),
+                                  redUnitConnectionPt );
+        blackCable = new CableNode( Color.black, blackProbe,
+                                    voltmeterModel.getUnitModel(),
+                                    voltmeterModel.getBlackLeadModel(),
+                                    blackUnitConnectionPt );
+
+        addChild( redCable );
+        addChild( redProbe );
+
+        addChild( blackCable );
+        addChild( blackProbe );
         voltmeterModel.addListener( new VoltmeterModel.Listener() {
             public void voltmeterChanged() {
                 update();
             }
         } );
         update();
-        addInputEventListener( new PBasicInputEventHandler() {
-            public void mouseDragged( PInputEvent event ) {
-                PDimension pt = event.getDeltaRelativeTo( VoltmeterNode.this.getParent() );
-                voltmeterModel.translateBody( pt.width, pt.height );
-            }
-        } );
     }
 
     private void update() {
         setVisible( voltmeterModel.isVisible() );
-        setOffset( voltmeterModel.getUnitOffset() );
+    }
+
+    static class UnitNode extends PhetPNode {
+        private VoltmeterModel.UnitModel unitModel;
+
+        public UnitNode( final VoltmeterModel.UnitModel unitModel ) {
+            this.unitModel = unitModel;
+            scale( SCALE );
+            unitModel.addListener( new VoltmeterModel.UnitModel.Listener() {
+                public void unitModelChanged() {
+                    update();
+                }
+            } );
+            addChild( PImageFactory.create( "images/vm3.gif" ) );
+
+            addInputEventListener( new PBasicInputEventHandler() {
+                public void mouseDragged( PInputEvent event ) {
+                    PDimension pt = event.getDeltaRelativeTo( UnitNode.this.getParent() );
+                    unitModel.translateBody( pt.width, pt.height );
+                }
+            } );
+
+            update();
+        }
+
+        private void update() {
+            setOffset( unitModel.getLocation() );
+        }
+    }
+
+    private class CableNode extends PhetPNode {
+        private PPath path;
+        private final BasicStroke cableStroke = new BasicStroke( (float)( 3 * SCALE ) );
+        private LeadNode leadNode;
+        private VoltmeterModel.UnitModel unitModel;
+        private VoltmeterModel.LeadModel leadModel;
+        private Point2D unitConnectionOffset;
+
+        public CableNode( Color color,
+                          LeadNode leadNode,
+                          VoltmeterModel.UnitModel unitModel,
+                          VoltmeterModel.LeadModel leadModel,
+                          Point2D unitConnectionOffset ) {
+            this.leadNode = leadNode;
+            this.unitModel = unitModel;
+            this.leadModel = leadModel;
+            this.unitConnectionOffset = unitConnectionOffset;
+            path = new PPath();
+            path.setStroke( cableStroke );
+            path.setStrokePaint( color );
+            addChild( path );
+            unitModel.addListener( new VoltmeterModel.UnitModel.Listener() {
+                public void unitModelChanged() {
+                    update();
+                }
+            } );
+            leadModel.addListener( new VoltmeterModel.LeadModel.Listener() {
+                public void leadModelChanged() {
+                    update();
+                }
+            } );
+            update();
+        }
+
+        public void update() {
+            Point2D leadConnectionPt = leadNode.getTailLocation();
+            Point2D unitConnectionPt = new Point2D.Double( unitModel.getLocation().getX() + unitConnectionOffset.getX(),
+                                                           unitModel.getLocation().getY() + unitConnectionOffset.getY() );
+            path.setPathTo( new Line2D.Double( leadConnectionPt,
+                                               unitConnectionPt ) );
+        }
+    }
+
+    private class LeadNode extends RegisterablePNode {
+        private VoltmeterModel.LeadModel leadModel;
+
+        public LeadNode( String imageLocation, final VoltmeterModel.LeadModel leadModel ) {
+            this.leadModel = leadModel;
+            scale( SCALE );
+            PImage imageNode = PImageFactory.create( imageLocation );
+            addChild( imageNode );
+            setRegistrationPoint( imageNode.getWidth() / 2, 0 );
+            leadModel.addListener( new VoltmeterModel.LeadModel.Listener() {
+                public void leadModelChanged() {
+                    updateLead();
+                }
+            } );
+
+            addInputEventListener( new PBasicInputEventHandler() {
+                public void mouseDragged( PInputEvent event ) {
+                    PDimension pt = event.getDeltaRelativeTo( LeadNode.this.getParent() );
+                    leadModel.translate( pt.width, pt.height );
+                }
+            } );
+
+            updateLead();
+        }
+
+        private void updateLead() {
+            setOffset( leadModel.getTipLocation() );
+        }
+
+        public Point2D getTailLocation() {
+            System.out.println( "getFullBounds().getHeight() = " + getFullBounds().getHeight() );
+            return new Point2D.Double( getOffset().getX(),
+                                       getOffset().getY() + getFullBounds().getHeight() );
+        }
     }
 }
