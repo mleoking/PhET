a143919c4520322fc0cbec48cbabbf95dfd430b0
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-09-26 17:47:53 +0000

    (9/26/06) - distort orbits so that 6 orbits fit in animation region (9/26/06) - factor common code out of Bohr and deBroglie nodes

--- hydrogen-atom/src/edu/colorado/phet/hydrogenatom/view/BohrAtomNode.java
+++ hydrogen-atom/src/edu/colorado/phet/hydrogenatom/view/BohrAtomNode.java
@@ -11,60 +11,50 @@
 
 package edu.colorado.phet.hydrogenatom.view;
 
-import java.awt.*;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Shape;
 import java.awt.Stroke;
 import java.awt.geom.Ellipse2D;
+import java.util.ArrayList;
 
 import edu.colorado.phet.hydrogenatom.HAConstants;
-import edu.colorado.phet.piccolo.util.PImageFactory;
-import edu.umd.cs.piccolo.nodes.PImage;
+import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.nodes.PPath;
 
 
-public class BohrAtomNode extends AbstractAtomNode {
+public class BohrAtomNode extends AbstractOrbitAtomNode {
 
-    private static final double GROUND_ORBIT_RADIUS = 20;
-    private static final Color ORBIT_COLOR = Color.WHITE;
-    private static final Stroke ORBIT_STROKE = 
-        new BasicStroke( 1f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[] {3,3}, 0 );
+    private static final int DEFAULT_SELECTED_ORBIT = 1; //XXX hack, for demo purposes
+    private static final int NUMBER_OF_ORBITS = 6;
+    
+    private ArrayList _orbitNodes; // array of PNode
+    private ProtonNode _protonNode;
+    private ElectronNode _electronNode;
     
     public BohrAtomNode() {
         super();
         
-        ProtonNode protonNode = new ProtonNode();
-        ElectronNode electronNode = new ElectronNode(); 
-        PPath o1 = createOrbitNode( 1 * GROUND_ORBIT_RADIUS );
-        PPath o2 = createOrbitNode( 4 * GROUND_ORBIT_RADIUS );
-        PPath o3 = createOrbitNode( 9 * GROUND_ORBIT_RADIUS );
-        PPath o4 = createOrbitNode( 16 * GROUND_ORBIT_RADIUS );
-        PPath o5 = createOrbitNode( 25 * GROUND_ORBIT_RADIUS );
-       
-        addChild( o1 );
-        addChild( o2 );
-        addChild( o3 );
-        addChild( o4 );
-        addChild( o5 );
-        addChild( protonNode );
-        addChild( electronNode );
+        _orbitNodes = new ArrayList();
+        for ( int orbit = 1; orbit <= NUMBER_OF_ORBITS; orbit++ ) {
+            PNode orbitNode = createOrbitNode( orbit );
+            addChild( orbitNode );
+            _orbitNodes.add( orbitNode );
+        }
+        
+        _protonNode = new ProtonNode();
+        addChild( _protonNode );
+        
+        _electronNode = new ElectronNode();
+        addChild( _electronNode );
         
         OriginNode originNode = new OriginNode( Color.GREEN );
         if ( HAConstants.SHOW_ORIGIN_NODES ) {
             addChild( originNode );
         }
         
-        protonNode.setOffset( 0, 0 );
-        electronNode.setOffset( o2.getFullBounds().getWidth() / 2, 0 );
-    }
-    
-    private PPath createOrbitNode( double diameter ) {
-        Shape shape = new Ellipse2D.Double( -diameter/2, -diameter/2, diameter, diameter );
-        PPath orbitNode = new PPath();
-        orbitNode.setPathTo( shape );
-        orbitNode.setStroke( ORBIT_STROKE );
-        orbitNode.setStrokePaint( ORBIT_COLOR );
-        return orbitNode;
+        _protonNode.setOffset( 0, 0 );
+        PNode orbitNode = (PNode)_orbitNodes.get( DEFAULT_SELECTED_ORBIT - 1 );
+        _electronNode.setOffset( orbitNode.getFullBounds().getWidth() / 2, 0 );
     }
 }
--- hydrogen-atom/src/edu/colorado/phet/hydrogenatom/view/DeBroglieAtomNode.java
+++ hydrogen-atom/src/edu/colorado/phet/hydrogenatom/view/DeBroglieAtomNode.java
@@ -13,60 +13,47 @@ package edu.colorado.phet.hydrogenatom.view;
 
 import java.awt.*;
 import java.awt.geom.Ellipse2D;
+import java.util.ArrayList;
 
 import edu.colorado.phet.hydrogenatom.HAConstants;
+import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.nodes.PPath;
 
 
-public class DeBroglieAtomNode extends AbstractAtomNode {
+public class DeBroglieAtomNode extends AbstractOrbitAtomNode {
     
-    private static final double GROUND_ORBIT_RADIUS = 20;
-    private static final Color ORBIT_COLOR = Color.WHITE;
-    private static final Stroke ORBIT_STROKE = 
-        new BasicStroke( 1f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[] {3,3}, 0 );
-    private static final Paint EXCITED_PAINT = Color.BLUE;
-    private static final Stroke EXCITED_STROKE = new BasicStroke( 8f );
+    private static final int DEFAULT_SELECTED_ORBIT = 1; //XXX hack, for demo purposes
+    private static final int NUMBER_OF_ORBITS = 6;
+    
+    private ArrayList _orbitNodes; // array of PNode
+    private ProtonNode _protonNode;
     
     public DeBroglieAtomNode() {
         super();
         
-        ProtonNode protonNode = new ProtonNode();
-        PPath o1 = createOrbitNode( 1 * GROUND_ORBIT_RADIUS );
-        PPath o2 = createExcitedNode( 4 * GROUND_ORBIT_RADIUS );
-        PPath o3 = createOrbitNode( 9 * GROUND_ORBIT_RADIUS );
-        PPath o4 = createOrbitNode( 16 * GROUND_ORBIT_RADIUS );
-        PPath o5 = createOrbitNode( 25 * GROUND_ORBIT_RADIUS );
-       
-        addChild( o1 );
-        addChild( o2 );
-        addChild( o3 );
-        addChild( o4 );
-        addChild( o5 );
-        addChild( protonNode );
+        _orbitNodes = new ArrayList();
+        for ( int orbit = 1; orbit <= NUMBER_OF_ORBITS; orbit++ ) {
+            PNode orbitNode = null;
+            if ( orbit == DEFAULT_SELECTED_ORBIT ) {
+                orbitNode = createExcitedNode( orbit );
+            }
+            else {
+                orbitNode = createOrbitNode( orbit );
+            }
+            addChild( orbitNode );
+            _orbitNodes.add( orbitNode );
+        }
+        
+        _protonNode = new ProtonNode();
+        addChild( _protonNode );
         
         OriginNode originNode = new OriginNode( Color.GREEN );
         if ( HAConstants.SHOW_ORIGIN_NODES ) {
             addChild( originNode );
         }
         
-        protonNode.setOffset( 0, 0 );
+        _protonNode.setOffset( 0, 0 );
     }
     
-    private PPath createOrbitNode( double diameter ) {
-        Shape shape = new Ellipse2D.Double( -diameter/2, -diameter/2, diameter, diameter );
-        PPath orbitNode = new PPath();
-        orbitNode.setPathTo( shape );
-        orbitNode.setStroke( ORBIT_STROKE );
-        orbitNode.setStrokePaint( ORBIT_COLOR );
-        return orbitNode;
-    }
-    
-    private PPath createExcitedNode( double diameter ) {
-        Shape shape = new Ellipse2D.Double( -diameter/2, -diameter/2, diameter, diameter );
-        PPath orbitNode = new PPath();
-        orbitNode.setPathTo( shape );
-        orbitNode.setStroke( EXCITED_STROKE );
-        orbitNode.setStrokePaint( EXCITED_PAINT );
-        return orbitNode;
-    }
+
 }
