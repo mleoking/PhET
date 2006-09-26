4ed32854b745250b31df6644e2fffd564deb866a
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-09-26 23:27:34 +0000

    CompositeMolecule no longer contains Bond instances

--- molecular-reactions/src/edu/colorado/phet/molecularreactions/view/MoleculeCounter.java
+++ molecular-reactions/src/edu/colorado/phet/molecularreactions/view/MoleculeCounter.java
@@ -132,6 +132,7 @@ public class MoleculeCounter extends JSpinner implements PublishingModel.ModelLi
                 SimpleMolecule component = components[j];
                 model.addModelElement( component );
             }
+            Bond[] bonds = ((CompositeMolecule)m).getBonds();
         }
     }
 
