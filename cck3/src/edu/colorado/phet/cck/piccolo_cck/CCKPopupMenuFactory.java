b6e3fa9690f92554e13deffd00614dda10279bda
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-10-06 14:09:36 +0000

    menu refactors

--- cck3/src/edu/colorado/phet/cck/piccolo_cck/CCKPopupMenuFactory.java
+++ cck3/src/edu/colorado/phet/cck/piccolo_cck/CCKPopupMenuFactory.java
@@ -1,6 +1,7 @@
 package edu.colorado.phet.cck.piccolo_cck;
 
 import edu.colorado.phet.cck.ICCKModule;
+import edu.colorado.phet.cck.grabbag.GrabBagResistor;
 import edu.colorado.phet.cck.model.components.*;
 
 import javax.swing.*;
@@ -31,6 +32,24 @@ public class CCKPopupMenuFactory {
         }
         else if( branch instanceof Battery ) {
             return new ComponentMenu.BatteryJMenu( (Battery)branch, module );
+        }
+        else if( branch instanceof Bulb ) {
+            return new ComponentMenu.BulbMenu( (Bulb)branch, module );
+        }
+        else if( branch instanceof Capacitor ) {
+            return new ComponentMenu.CapacitorMenu( (Capacitor)branch, module );
+        }
+        else if( branch instanceof GrabBagResistor ) {
+            return new ComponentMenu.GrabBagResistorMenu( (GrabBagResistor)branch, module );
+        }
+        else if( branch instanceof Inductor ) {
+            return new ComponentMenu.InductorMenu( (Inductor)branch, module );
+        }
+        else if( branch instanceof Resistor ) {
+            return new ComponentMenu.ResistorMenu( (Resistor)branch, module );
+        }
+        else if( branch instanceof Wire ) {
+            return new WirePopupMenu( module.getCCKModel(), branch );
         }
         else {
             System.out.println( "No popup menu found for component: " + branch );
