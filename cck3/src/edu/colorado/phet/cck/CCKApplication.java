24b7d078b41b4af9c46b3056b7103d9fdbb543d4
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-10-07 16:37:29 +0000

    added getTabbedModulePane to PhetFrame, see javadoc for usage here's example code to set whether the tabbed pane logo is visible or not (only supported for PhetTabbedPane): getPhetFrame().getTabbedModulePane().setLogoVisible( true );

--- cck3/src/edu/colorado/phet/cck/CCKApplication.java
+++ cck3/src/edu/colorado/phet/cck/CCKApplication.java
@@ -51,7 +51,7 @@ public class CCKApplication extends PiccoloPhetApplication {
 //        Module[] modules = new Module[]{phetGraphicsCCKModule, cckPiccoloModule};
         Module[] modules = new Module[]{cckPiccoloModule, phetGraphicsCCKModule};
         setModules( modules );
-
+        getPhetFrame().getTabbedModulePane().setLogoVisible( true );
         getPhetFrame().addMenu( new LookAndFeelMenu() );
 //        getPhetFrame().addMenu( new OptionsMenu( th, cckModule ) );//todo options menu
 
