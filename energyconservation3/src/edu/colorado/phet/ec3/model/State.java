40d45063b2fc1cc3448d0f72414df24c3e203075
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-10-11 11:13:15 +0000

    added 2 rotation modes for body to accommodate spline traversal and freefall

---
+++ energyconservation3/src/edu/colorado/phet/ec3/model/State.java
@@ -0,0 +1,43 @@
+package edu.colorado.phet.ec3.model;
+
+import java.awt.geom.Point2D;
+
+/**
+ * User: Sam Reid
+ * Date: Oct 11, 2006
+ * Time: 4:52:36 AM
+ * Copyright (c) Oct 11, 2006 by Sam Reid
+ */
+public class State {
+    private EnergyConservationModel model;
+    private Body body;
+
+    public State( EnergyConservationModel model, Body body ) {
+        this.model = model.copyState();
+        this.body = body.copyState();
+    }
+
+    public EnergyConservationModel getModel() {
+        return model;
+    }
+
+    public Body getBody() {
+        return body;
+    }
+
+    public Point2D.Double getPosition() {
+        return body.getPosition();
+    }
+
+    public double getMechanicalEnergy() {
+        return model.getMechanicalEnergy( body );
+    }
+
+    public double getTotalEnergy() {
+        return model.getTotalMechanicalEnergy( body ) + model.getThermalEnergy();
+    }
+
+    public double getHeat() {
+        return model.getThermalEnergy();
+    }
+}
