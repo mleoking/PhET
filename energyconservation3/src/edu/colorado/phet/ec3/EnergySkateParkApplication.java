312dc082213c6f95da1e3379d64d61d948cc5b29
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-10-11 19:32:58 +0000

    bugfixes

--- energyconservation3/src/edu/colorado/phet/ec3/EnergySkateParkApplication.java
+++ energyconservation3/src/edu/colorado/phet/ec3/EnergySkateParkApplication.java
@@ -18,7 +18,7 @@ import java.awt.event.KeyEvent;
 
 public class EnergySkateParkApplication extends PhetApplication {
     private EnergySkateParkModule module;
-    private static final String VERSION = "1.02.01";
+    private static final String VERSION = "1.02.02";
 
     public EnergySkateParkApplication( String[] args ) {
         super( args, EnergySkateParkStrings.getString( "energy.skate.park" ), EnergySkateParkStrings.getString( "energy.conservation" ), VERSION, new EC3FrameSetup() );
