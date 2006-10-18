6bac80171ab2998a3dac666c24ed58ba96624c73
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-10-18 00:20:54 +0000

    improve interface for EnergyConserver

--- trunk/energyconservation3/src/edu/colorado/phet/ec3/model/FreeSplineMode2.java
+++ trunk/energyconservation3/src/edu/colorado/phet/ec3/model/FreeSplineMode2.java
@@ -4,7 +4,6 @@ import edu.colorado.phet.common.math.AbstractVector2D;
 import edu.colorado.phet.common.math.ImmutableVector2D;
 import edu.colorado.phet.common.math.Vector2D;
 import edu.colorado.phet.ec3.model.spline.AbstractSpline;
-import edu.colorado.phet.ec3.model.spline.Segment;
 
 import java.awt.geom.Point2D;
 
@@ -18,7 +17,6 @@ import java.awt.geom.Point2D;
 public class FreeSplineMode2 implements UpdateMode {
     private EnergyConservationModel model;
     private AbstractSpline spline;
-//    private double x = 0;
 
     public FreeSplineMode2( EnergyConservationModel model, AbstractSpline spline ) {
         this.model = model;
@@ -26,32 +24,27 @@ public class FreeSplineMode2 implements UpdateMode {
     }
 
     public void stepInTime( Body body, double dt ) {
-        ForceMode forceMode = new ForceMode() {
-            public void init( Body body ) {
-            }
-        };
-        AbstractVector2D netForce = createNetForce( body );
-        System.out.println( "netForce = " + netForce );
-        forceMode.setNetForce( netForce );
-        forceMode.stepInTime( body, dt );
-        Point2D splineLocation = spline.getSegmentPath().evaluate( getDistAlongSpline( body ) );
+        Body origState = body.copyState();
+
+        double x = getDistAlongSpline( body );
+        System.out.println( "x = " + x );
+        new ForceMode( createNetForce( body, x ) ).stepInTime( body, dt );
+        //move along the spline until we have a good match
+//        double epsilon = 1;
+//        double deltaT = 0.01;
+//        for( double t = x - epsilon; t <= x + epsilon; t += deltaT ) {
+//
+//        }
+        double x2 = getDistAlongSpline( body );
+
+        Point2D splineLocation = spline.getSegmentPath().evaluate( x2 );
         body.setAttachmentPointPosition( splineLocation );
-//        System.out.println( "FreeSplineMode2.stepInTime" );
-
-//        spline.evaluate();
-//        System.out.println( "x = " + x );
-//        setDistAlongSpline( body, x );
-//        x += 0.01;
-    }
-
-    private void setDistAlongSpline( Body body, double distAlongSpline ) {
-        body.setAttachmentPointPosition( spline.evaluate( distAlongSpline ) );
+        new EnergyConserver().fixEnergy( body, origState.getTotalEnergy() );
+//        System.out.println( "origKE="+origState.getKineticEnergy()+", finalKE="+body.getKineticEnergy() );
     }
 
     public void init( Body body ) {
         body.convertToSpline();
-//        x = getDistAlongSpline( body );
-        body.setVelocity( 0, 0 );
     }
 
     private double getDistAlongSpline( Body body ) {//todo: binary search?
@@ -61,6 +54,7 @@ public class FreeSplineMode2 implements UpdateMode {
         for( int i = 0; i < N; i++ ) {
             double splineLength = spline.getSegmentPath().getLength();
             double alongSpline = i / ( (double)N ) * splineLength;
+//            System.out.println( "alongSpline = " + alongSpline );
             Point2D loc = spline.evaluate( alongSpline );
             double dist = body.getPosition().distance( loc );
 //            System.out.println( "i = " + i + ", scalar=" + alongSpline + ", score=" + dist );
@@ -72,14 +66,36 @@ public class FreeSplineMode2 implements UpdateMode {
         System.out.println( "found best=" + scalar + ", score=" + best );
         return scalar;
     }
+//    private double getDistAlongSpline( Body body ) {
+//        double x = getDistAlongSpline( body, 0, spline.getLength() );
+//        return x;
+//    }
+//
+//    private double getDistAlongSpline( Body body, double min, double max ) {//todo: binary search?
+//        double bestScore = Double.POSITIVE_INFINITY;
+//        double bestDist = 0.0;
+//        int N = 100;
+//        for( double alongSpline = min; alongSpline <= max; alongSpline += ( max - min ) / N ) {
+//
+////            double alongSpline = i / ( (double)N ) * splineLength;
+//            Point2D loc = spline.evaluate( alongSpline );
+//            double dist = body.getPosition().distance( loc );
+//            System.out.println( "alongSpline = " + alongSpline + ", dist=" + dist );
+//            if( dist < bestScore ) {
+//                bestScore = dist;
+//                bestDist = alongSpline;
+//            }
+//        }
+////        System.out.println( "found best=" + scalar + ", score=" + best );
+//        return bestDist;
+//    }
 
-    private AbstractVector2D createNetForce( Body body ) {
-        double x = getDistAlongSpline( body );
+    private AbstractVector2D createNetForce( Body body, double x ) {
         AbstractVector2D[] forces = new AbstractVector2D[]{
                 body.getGravityForce(),
-                getNormalForce( model, spline.getSegmentPath().getSegmentAtPosition( x ), body ),
+                getNormalForce( body, x ),
                 body.getThrust(),
-                getFrictionForce( model, spline.getSegmentPath().getSegmentAtPosition( x ), body )
+                getFrictionForce( body, x )
         };
         Vector2D.Double sum = new Vector2D.Double();
         for( int i = 0; i < forces.length; i++ ) {
@@ -92,29 +108,17 @@ public class FreeSplineMode2 implements UpdateMode {
         return sum;
     }
 
-    private AbstractVector2D getFrictionForce( EnergyConservationModel model, Segment segment, Body body ) {
-        if( segment == null ) {
-            return new Vector2D.Double();
-        }
-        double fricMag = body.getFrictionCoefficient() * getNormalForce( model, segment, body ).getMagnitude();
+    private AbstractVector2D getFrictionForce( Body body, double x ) {
+        double fricMag = body.getFrictionCoefficient() * getNormalForce( body, x ).getMagnitude();
         if( body.getVelocity().getMagnitude() > 0 ) {
-            return body.getVelocity().getInstanceOfMagnitude( -fricMag * 5 );//todo fudge factor of five
+            return body.getVelocity().getInstanceOfMagnitude( -fricMag );
         }
         else {
             return new ImmutableVector2D.Double( 0, 0 );
         }
     }
 
-    private AbstractVector2D getNormalForce( EnergyConservationModel model, Segment segment, Body body ) {
-        if( segment == null ) {
-            return new Vector2D.Double();
-        }
-        if( body.getVelocity().dot( segment.getUnitNormalVector() ) <= 0.1 ) {
-//        if( segment.getUnitNormalVector().dot( getGravityForce( model ) ) < 0 ) {//todo is this correct?
-            return segment.getUnitNormalVector().getScaledInstance( -body.getGravityForce().getMagnitude() * Math.cos( segment.getAngle() ) );
-        }
-        else {
-            return new ImmutableVector2D.Double();
-        }
+    private AbstractVector2D getNormalForce( Body body, double x ) {
+        return spline.getUnitNormalVector( x ).getScaledInstance( -body.getGravityForce().getMagnitude() * Math.cos( spline.getUnitNormalVector( x ).getNormalVector().getAngle() ) );
     }
 }
