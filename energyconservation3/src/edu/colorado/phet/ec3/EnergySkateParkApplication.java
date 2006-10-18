def9817163b227568fb1e5d46e202d237a917df6
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-10-18 14:27:07 +0000

    added improved frame sizes for deployment

--- energyconservation3/src/edu/colorado/phet/ec3/EnergySkateParkApplication.java
+++ energyconservation3/src/edu/colorado/phet/ec3/EnergySkateParkApplication.java
@@ -21,7 +21,10 @@ public class EnergySkateParkApplication extends PhetApplication {
     private static final String VERSION = "1.02.05";
 
     public EnergySkateParkApplication( String[] args ) {
-        super( args, EnergySkateParkStrings.getString( "energy.skate.park" ), EnergySkateParkStrings.getString( "energy.conservation" ), VERSION, new EC3FrameSetup() );
+        super( args, EnergySkateParkStrings.getString( "energy.skate.park" ), EnergySkateParkStrings.getString( "energy.conservation" ),
+               VERSION,
+//               new EnergySkateParkDebugFrameSetup() );
+new EnergySkateParkFrameSetup() );
         module = new EnergySkateParkModule( "Module", new SwingClock( 30, 0.03 ), getPhetFrame() );
         setModules( new Module[]{module} );
     }
