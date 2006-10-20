b88cccbbe6c4ef5d04217041c6a83812d562aae1
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-10-20 20:36:58 +0000

    Pie and bar charts have striped paints for AB and BC molecules

--- trunk/molecular-reactions/src/edu/colorado/phet/molecularreactions/view/MoleculePaints.java
+++ trunk/molecular-reactions/src/edu/colorado/phet/molecularreactions/view/MoleculePaints.java
@@ -14,10 +14,13 @@ import edu.colorado.phet.molecularreactions.model.MoleculeA;
 import edu.colorado.phet.molecularreactions.model.MoleculeBC;
 import edu.colorado.phet.molecularreactions.model.MoleculeAB;
 import edu.colorado.phet.molecularreactions.model.MoleculeC;
+import edu.colorado.phet.common.view.util.ImageLoader;
 
 import java.util.Map;
 import java.util.HashMap;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 /**
  * MoleculePaints
@@ -27,9 +30,38 @@ import java.awt.*;
  */
 public class MoleculePaints {
     private static Map moleculeTypeToPaint = new HashMap();
+
+    private static BufferedImage moleculeBCPaintImg;
+    private static Paint moleculeBCPaint;
+    static {
+        try {
+            moleculeBCPaintImg = ImageLoader.loadBufferedImage( "images/molecule-bc-paint.gif");
+            moleculeBCPaint = new TexturePaint( moleculeBCPaintImg, new Rectangle( 0,0,
+                                                                                   moleculeBCPaintImg.getWidth(),
+                                                                                   moleculeBCPaintImg.getHeight()) ) ;
+        }
+        catch( IOException e ) {
+            e.printStackTrace();
+        }
+    }
+
+    private static BufferedImage moleculeABPaintImg;
+    private static Paint moleculeABPaint;
+    static {
+        try {
+            moleculeABPaintImg = ImageLoader.loadBufferedImage( "images/molecule-AB-paint.gif");
+            moleculeABPaint = new TexturePaint( moleculeABPaintImg, new Rectangle( 0,0,
+                                                                                   moleculeABPaintImg.getWidth(),
+                                                                                   moleculeABPaintImg.getHeight()) ) ;
+        }
+        catch( IOException e ) {
+            e.printStackTrace();
+        }
+    }
+
     private static Paint moleculeAPaint = new Color( 180, 180, 0 );
-    private static Paint moleculeBCPaint = new Color( 180, 80, 100 ) ;
-    private static Paint moleculeABPaint = new Color( 60, 180, 120 );
+//    private static Paint moleculeBCPaint = new Color( 180, 80, 100 ) ;
+//    private static Paint moleculeABPaint = new Color( 60, 180, 120 );
     private static Paint moleculeCPaint = new Color( 60, 10, 180 );
     static {
         moleculeTypeToPaint.put( MoleculeA.class, moleculeAPaint );
