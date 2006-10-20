724f3e32da91a9386130584c2c308d660094fc0c
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-10-20 06:08:58 +0000

    added a Test to automatically display a PhetAboutDialog

---
+++ trunk/phetcommon/tests-src/edu/colorado/phet/common/tests/TestPhetAboutDialog.java
@@ -0,0 +1,53 @@
+/* Copyright 2006, University of Colorado */
+
+/*
+ * CVS Info -
+ * Filename : $Source$
+ * Branch : $Name$
+ * Modified by : $Author$
+ * Revision : $Revision$
+ * Date modified : $Date$
+ */
+package edu.colorado.phet.common.tests;
+
+import edu.colorado.phet.common.application.PhetAboutDialog;
+import edu.colorado.phet.common.application.PhetApplication;
+import edu.colorado.phet.common.model.BaseModel;
+import edu.colorado.phet.common.model.clock.IClock;
+import edu.colorado.phet.common.model.clock.SwingClock;
+import edu.colorado.phet.common.view.util.FrameSetup;
+import edu.colorado.phet.piccolo.PiccoloModule;
+
+import javax.swing.*;
+
+/**
+ * Test to automatically display a PhetAboutDialog
+ */
+public class TestPhetAboutDialog {
+    private PhetApplication phetApplication;
+    private PhetAboutDialog phetAboutDialog;
+
+    public TestPhetAboutDialog( String[] args ) {
+        phetApplication = new PhetApplication( args, "Test Phet About Dialog", "This simulation is used to test the about dialog", "Version 0.01", new FrameSetup.CenteredWithSize( 400, 400 ) );
+        PiccoloModule module = new TestModule( "example module", new SwingClock( 30, 1 ) );
+        module.setModel( new BaseModel() );
+        phetApplication.addModule( module );
+        phetAboutDialog = new PhetAboutDialog( phetApplication );
+    }
+
+    public static void main( String[] args ) {
+        new TestPhetAboutDialog( args ).start();
+    }
+
+    private void start() {
+        phetApplication.startApplication();
+        phetAboutDialog.show();
+    }
+
+    static class TestModule extends PiccoloModule {
+        public TestModule( String name, IClock clock ) {
+            super( name, clock );
+            setSimulationPanel( new JPanel() );
+        }
+    }
+}
