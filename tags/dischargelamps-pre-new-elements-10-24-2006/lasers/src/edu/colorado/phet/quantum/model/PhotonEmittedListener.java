bf457e0552b7312ae406ed7662456fe27c788d59
nobody <nobody@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-10-24 17:43:11 +0000

    This commit was manufactured by cvs2svn to create tag 'dischargelamps-pre-new-elements-10-24-2006'.

---
+++ tags/dischargelamps-pre-new-elements-10-24-2006/lasers/src/edu/colorado/phet/quantum/model/PhotonEmittedListener.java
@@ -0,0 +1,17 @@
+/* Copyright 2004, University of Colorado */
+
+/*
+ * CVS Info -
+ * Filename : $Source$
+ * Branch : $Name$
+ * Modified by : $Author$
+ * Revision : $Revision$
+ * Date modified : $Date$
+ */
+package edu.colorado.phet.quantum.model;
+
+import java.util.EventListener;
+
+public interface PhotonEmittedListener extends EventListener {
+    public void photonEmitted( PhotonEmittedEvent event );
+}
