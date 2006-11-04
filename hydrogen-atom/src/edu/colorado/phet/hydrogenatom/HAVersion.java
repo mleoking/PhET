110523c7b0eab159960d0be69aa43b43ca6f2242
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-11-04 16:18:05 +0000

    *** empty log message ***

--- hydrogen-atom/src/edu/colorado/phet/hydrogenatom/HAVersion.java
+++ hydrogen-atom/src/edu/colorado/phet/hydrogenatom/HAVersion.java
@@ -23,7 +23,7 @@ public class HAVersion {
 //    public static final String NUMBER = "sandbox";
 //    public static final String CVS_TAG = "hydrogenatom-sandbox";
     
-    public static final String NUMBER = "0.00.06";
+    public static final String NUMBER = "0.00.07";
     public static final String CVS_TAG = "hydrogenatom-0-00-07-2006-11-04";
     
     /* This class is not intended for instantiation. */
