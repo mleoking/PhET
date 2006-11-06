985acfb6ad0eaac63046888fc54f978d8a9bc747
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-11-06 23:06:14 +0000

    implement move method to test Rutherford Scattering for Bohr

--- hydrogen-atom/src/edu/colorado/phet/hydrogenatom/model/BohrModel.java
+++ hydrogen-atom/src/edu/colorado/phet/hydrogenatom/model/BohrModel.java
@@ -13,10 +13,24 @@ package edu.colorado.phet.hydrogenatom.model;
 
 import java.awt.geom.Point2D;
 
+import edu.colorado.phet.hydrogenatom.HAConstants;
+
 
 public class BohrModel extends AbstractHydrogenAtom {
 
     public BohrModel( Point2D position ) {
         super( position, 0 /* orientation */ );
+    }
+    
+    /**
+     * Move an alpha particle using a Rutherford Scattering algorithm.
+     * 
+     * @param alphaParticle
+     * @param dt
+     */
+    public void move( AlphaParticle alphaParticle, final double dt ) {
+        final double L = HAConstants.ANIMATION_BOX_SIZE.height;
+        final double D = L / 4;
+        RutherfordScattering.move( this, alphaParticle, dt, D );
     }
 }
