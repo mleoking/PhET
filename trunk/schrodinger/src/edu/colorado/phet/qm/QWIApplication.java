03d1e77dbc62d4cec0374ac5d7985ab0aacab6d2
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-11-06 20:56:49 +0000

    1.04

--- trunk/schrodinger/src/edu/colorado/phet/qm/QWIApplication.java
+++ trunk/schrodinger/src/edu/colorado/phet/qm/QWIApplication.java
@@ -29,7 +29,7 @@ import java.io.IOException;
  */
 
 public class QWIApplication extends PiccoloPhetApplication {
-    public static String VERSION = "1.03.01";
+    public static String VERSION = "1.04";
     private IntensityModule intensityModule;
 
     public QWIApplication( String[] args ) {
