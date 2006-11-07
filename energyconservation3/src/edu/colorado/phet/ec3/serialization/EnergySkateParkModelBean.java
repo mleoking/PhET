914300bd9e10beb68a7720c87e78d9a7a5c12738
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-11-07 20:03:14 +0000

    Bugfix: When I pulled up energy vs time graph, and messed around with pausing, using cursor, and trying to go again ... it got confused. (Right now I can't get the guy to start moving again ... even when I hit Reset.)

--- energyconservation3/src/edu/colorado/phet/ec3/serialization/EnergySkateParkModelBean.java
+++ energyconservation3/src/edu/colorado/phet/ec3/serialization/EnergySkateParkModelBean.java
@@ -1,5 +1,10 @@
 package edu.colorado.phet.ec3.serialization;
 
+import edu.colorado.phet.ec3.EnergySkateParkModule;
+import edu.colorado.phet.ec3.model.Body;
+
+import java.awt.geom.Point2D;
+
 /**
  * User: Sam Reid
  * Date: Nov 7, 2006
@@ -8,4 +13,25 @@ package edu.colorado.phet.ec3.serialization;
  */
 
 public class EnergySkateParkModelBean {
+    private Point2D.Double bodyZeroPosition;
+
+    public EnergySkateParkModelBean() {
+    }
+
+    public EnergySkateParkModelBean( EnergySkateParkModule module ) {
+        Body body = module.getEnergyConservationModel().bodyAt( 0 );
+        bodyZeroPosition = body.getPosition();
+    }
+
+    public Point2D.Double getBodyZeroPosition() {
+        return bodyZeroPosition;
+    }
+
+    public void setBodyZeroPosition( Point2D.Double bodyZeroPosition ) {
+        this.bodyZeroPosition = bodyZeroPosition;
+    }
+
+    public void apply( EnergySkateParkModule module ) {
+        module.getEnergyConservationModel().bodyAt( 0 ).setAttachmentPointPosition( bodyZeroPosition );
+    }
 }
