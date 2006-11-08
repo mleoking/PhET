7ccd6335f49bc6349771db8cbb122a518ecb45fa
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-11-08 09:21:43 +0000

    added roller-coaster mode (sticky tracks)

--- energyconservation3/src/edu/colorado/phet/ec3/model/FreeFall.java
+++ energyconservation3/src/edu/colorado/phet/ec3/model/FreeFall.java
@@ -2,6 +2,9 @@
 package edu.colorado.phet.ec3.model;
 
 import edu.colorado.phet.common.math.Vector2D;
+import edu.colorado.phet.ec3.model.spline.AbstractSpline;
+
+import java.util.ArrayList;
 
 
 /**
@@ -19,24 +22,64 @@ public class FreeFall extends ForceMode implements Derivable {
     }
 
     public void stepInTime( Body body, double dt ) {
-        boolean bodyCollidingWithSpline = body.getCollisionState().getSplineCount() > 0;
-        boolean topOrig = body.getCollisionState().isTop();
+        Body.StateRecord origState = body.getCollisionState();
         stepIgnoreSplines( body, dt );
-        boolean topFinal = body.createCollisionState().isTop();
-        if( topFinal != topOrig && bodyCollidingWithSpline ) {
-            System.out.println( "Passed through spline" );
-            new SplineInteraction( energyConservationModel ).doCollision( body.getCollisionState().getSpline( 0 ), body );
-            int maxTries = 5;
+        Body.StateRecord newState = body.createCollisionState();
+        AbstractSpline passedThrough = getCrossedSpline( origState, newState );
+        if( passedThrough != null ) {
+//            System.out.println( "Passed through spline, at first, top=" + topOrig + ", but now top=" + topFinal );
+            System.out.println( "Passed through spline: " + passedThrough );
+            new SplineInteraction( energyConservationModel ).doCollision( passedThrough, body );
+            int maxTries = 2;
             int count = 0;
-            while( topOrig != body.createCollisionState().isTop() && count < maxTries ) {
+            while( getCrossedSpline( origState, body.createCollisionState() ) != null && count < maxTries )
+            {                  //todo: should we set it so this spline can't be grabbed soon?
                 stepIgnoreSplines( body, dt );
                 count++;
                 System.out.println( "Restoration count = " + count );
             }
+//            body.clearCollisionHistory();
         }
         else {
             new SplineInteraction( energyConservationModel ).interactWithSplines( body );
         }
+    }
+
+    private AbstractSpline getCrossedSpline( Body.StateRecord a, Body.StateRecord b ) {
+        ArrayList splines = new ArrayList();
+        for( int i = 0; i < a. getSplineCount(); i++ ) {
+            AbstractSpline spline = a.getSpline( i );
+            if( b.containsSpline( spline ) ) {
+                splines.add( spline );
+            }
+        }
+        for( int i = 0; i < splines.size(); i++ ) {
+            AbstractSpline spline = (AbstractSpline)splines.get( i );
+            if( isCrossed( a.getTraversalState( spline ), b.getTraversalState( spline ) ) ) {
+                return spline;
+            }
+        }
+        return null;
+    }
+
+    private boolean isCrossed( Body.TraversalState x, Body.TraversalState y ) {
+//        if( x.isTop() && y.isTop() ) {
+        if( x.isTop() == y.isTop() ) {
+            //not a crossing, since they were both on the same side.
+            return false;
+        }
+        double epsilon = 0.02;
+        if( x.getScalarAlongSpline() <= 0 || y.getScalarAlongSpline() <= 0 || x.getScalarAlongSpline() >= x.getSpline().getLength() - epsilon || y.getScalarAlongSpline() >= y.getSpline().getLength() - epsilon )
+        {
+            //bogus result because we passed the end of the spline
+            return false;
+        }
+        if( x.getClosestSplineLocation().distance( y.getClosestSplineLocation() ) > 2 ) {
+            //bogus because the spline locations didn't match.
+            return false;
+        }
+
+        return true;
     }
 
     public void stepIgnoreSplines( Body body, double dt ) {
