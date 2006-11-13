8345d4f49ba5ee05317d6cc4d015a092a5e5bc51
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-11-13 20:52:18 +0000

    Changed electrons to blue, and made both the same size.

--- trunk/emf/src/edu/colorado/phet/emf/EmfConfig.java
+++ trunk/emf/src/edu/colorado/phet/emf/EmfConfig.java
@@ -14,9 +14,9 @@ public class EmfConfig {
     public static double SINGLE_VECTOR_ROW_OFFSET = 0.5;
 
     // Images
-    public final static String bigElectronImg = "images/yellow-electron.gif";
-    public final static String smallElectronImg = "images/small-yellow-electron.gif";
-    
+    public final static String bigElectronImg = "images/blue-sml.gif";
+    public final static String smallElectronImg = "images/blue-sml.gif";
+
     // Localization
     public static final String localizedStringsPath = "localization/RadioWavesStrings";
 
