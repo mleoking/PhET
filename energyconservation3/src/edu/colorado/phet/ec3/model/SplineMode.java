f7670dec2da0c790d173b6a442768f3e756d94df
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-11-14 08:26:40 +0000

    Bugfix: Skater rotates back and forth quickly when at the bottom of a potential well (with friction).

--- energyconservation3/src/edu/colorado/phet/ec3/model/SplineMode.java
+++ energyconservation3/src/edu/colorado/phet/ec3/model/SplineMode.java
@@ -74,9 +74,9 @@ public class SplineMode implements UpdateMode {
 
     private void fixEnergy( Body origState, AbstractVector2D netForce, double x2, final Body body, double dt ) {
         boolean fixed = false;
-        if( !fixed && body.getSpeed() >= 0.001 ) {
+        if( !fixed && body.getSpeed() >= 0.0001 ) {
             //increasing the speed threshold from 0.001 to 0.1 causes the moon-sticking problem to go away.
-            fixed = fixed || new EnergyConserver().fixEnergyWithVelocity( body, origState.getTotalEnergy(), 15, 0.001 );
+            fixed = fixed || new EnergyConserver().fixEnergyWithVelocity( body, origState.getTotalEnergy(), 15, 0.0001 );
         }
         if( !fixed && Math.abs( spline.getUnitNormalVector( x2 ).getY() ) < 0.9 ) {
             double epsilon = 0.001;//1E-8     
