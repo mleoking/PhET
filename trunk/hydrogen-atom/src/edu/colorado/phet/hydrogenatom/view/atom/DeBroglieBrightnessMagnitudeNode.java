4032fb958647da4cb92e589efa865ca3fa628fbd
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-11-20 22:43:03 +0000

    (11/20/06) - first pass at  DeBroglie "brightness" and "brightness magnitude" representations (approx a ring using circles)

--- trunk/hydrogen-atom/src/edu/colorado/phet/hydrogenatom/view/atom/DeBroglieBrightnessMagnitudeNode.java
+++ trunk/hydrogen-atom/src/edu/colorado/phet/hydrogenatom/view/atom/DeBroglieBrightnessMagnitudeNode.java
@@ -11,19 +11,51 @@
 
 package edu.colorado.phet.hydrogenatom.view.atom;
 
+import java.awt.Color;
+
 import edu.colorado.phet.hydrogenatom.model.DeBroglieModel;
-import edu.colorado.phet.hydrogenatom.view.atom.DeBroglieNode.AbstractDeBroglie2DViewStrategy;
 
+/**
+ * DeBroglieBrightnessMagnitudeNode represents the deBroglie model
+ * as a standing wave. The magnitude of the amplitude of the standing
+ * wave is represented by the brightness of color in a ring that 
+ * is positioned at the electron's orbit.
+ * <p>
+ * Note that this representation is identical to DeBroglieBrightnessNode,
+ * except that we use the magnitude of the amplitude (instead of the 
+ * actual value of the amplitude) to determine color.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ * @version $Revision$
+ */
+class DeBroglieBrightnessMagnitudeNode extends DeBroglieBrightnessNode {
 
-class DeBroglieBrightnessMagnitudeNode extends AbstractDeBroglie2DViewStrategy {
-
+    //----------------------------------------------------------------------------
+    // Constructors
+    //----------------------------------------------------------------------------
+    
     public DeBroglieBrightnessMagnitudeNode( DeBroglieModel atom ) {
         super( atom );
-        update();
     }
     
-    public void update() {
-        //XXX
-    }
+    //----------------------------------------------------------------------------
+    // DeBroglieBrightnessNode overrides
+    //----------------------------------------------------------------------------
     
+    /**
+     * Maps the magnitude of the specified amplitude to a color.
+     * @param amplitude
+     * @return Color
+     */
+    protected Color amplitudeToColor( double amplitude ) {
+        assert( amplitude >= -1 && amplitude <= 1 );
+        double magnitude = Math.abs( amplitude );
+        double f = 0.7;
+        int r = (int)( ( f * 255 ) - ( f * 255 * magnitude ) );
+        int g = (int)( ( f * 255 ) - ( f * 255 * magnitude ) );
+        int b = 255;
+        Color color = new Color( r, g, b );
+        return color;
+    }
+
 }
--- trunk/hydrogen-atom/src/edu/colorado/phet/hydrogenatom/view/atom/DeBroglieBrightnessNode.java
+++ trunk/hydrogen-atom/src/edu/colorado/phet/hydrogenatom/view/atom/DeBroglieBrightnessNode.java
@@ -12,24 +12,95 @@
 package edu.colorado.phet.hydrogenatom.view.atom;
 
 import java.awt.Color;
+import java.awt.Shape;
+import java.awt.geom.Ellipse2D;
 
-import edu.colorado.phet.hydrogenatom.HAConstants;
 import edu.colorado.phet.hydrogenatom.model.DeBroglieModel;
-import edu.colorado.phet.hydrogenatom.view.OriginNode;
-import edu.colorado.phet.hydrogenatom.view.atom.AbstractHydrogenAtomNode.OrbitFactory;
 import edu.colorado.phet.hydrogenatom.view.atom.DeBroglieNode.AbstractDeBroglie2DViewStrategy;
-import edu.colorado.phet.hydrogenatom.view.particle.ProtonNode;
 import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.nodes.PPath;
 
-
+/**
+ * DeBroglieBrightnessNode represents the deBroglie model
+ * as a standing wave. The amplitude (-1...+1) of the standing
+ * wave is represented by the brightness of color in a ring that 
+ * is positioned at the electron's orbit.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ * @version $Revision$
+ */
 class DeBroglieBrightnessNode extends AbstractDeBroglie2DViewStrategy {
-
+    
+    //----------------------------------------------------------------------------
+    // Class data
+    //----------------------------------------------------------------------------
+    
+    private static final double RING_WIDTH = 10;
+    
+    //----------------------------------------------------------------------------
+    // Instance data
+    //----------------------------------------------------------------------------
+    
+    private PNode _ringNode;
+    
+    //----------------------------------------------------------------------------
+    // Constructors
+    //----------------------------------------------------------------------------
+    
     public DeBroglieBrightnessNode( DeBroglieModel atom ) {
         super( atom );
+        _ringNode = new PNode();
+        addChild( _ringNode );
         update();
     }
     
+    //----------------------------------------------------------------------------
+    // AbstractDeBroglie2DViewStrategy implementation
+    //----------------------------------------------------------------------------
+    
+    /**
+     * Updates the view to match the model.
+     * In this case, the view is a ring, located at the electron's orbit.
+     * The ring is colored by mapping amplitudes of the standing wave
+     * at points on the ring to colors.
+     */
     public void update() {
-        //XXX
+
+        _ringNode.removeAllChildren();
+
+        DeBroglieModel atom = getAtom();
+        int steps = 36;
+        double radius = atom.getElectronOrbitRadius();
+
+        for ( int i = 0; i < steps; i++ ) {
+
+            double angle = ( 2 * Math.PI ) * ( (double) i / steps );
+            double amplitude = atom.getAmplitude( angle );
+            Color color = amplitudeToColor( amplitude );
+
+            double diameter = 15;
+            Shape shape = new Ellipse2D.Double( -diameter/2, -diameter/2, diameter, diameter );
+
+            PPath path = new PPath( shape );
+            path.setPaint( color );
+            path.setStroke( null );
+            _ringNode.addChild( path );
+
+            double x = radius * Math.cos( angle );
+            double y = radius * Math.sin( angle );
+            path.setOffset( x, y );
+        }
+    }
+    
+    /**
+     * Maps the magnitude of the specified amplitude to a color.
+     * May be overridden by subclasses to provide different representations.
+     * 
+     * @param amplitude
+     * @return Color
+     */
+    protected Color amplitudeToColor( double amplitude ) {
+        assert( amplitude >= -1 && amplitude <= 1 );
+        return Color.RED;//XXX
     }
 }
