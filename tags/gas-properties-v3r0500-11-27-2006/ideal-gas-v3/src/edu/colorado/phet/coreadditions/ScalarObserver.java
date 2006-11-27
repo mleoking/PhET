e0d2f824134eb94cf1e90ac5d9ea0406be5e6fed
nobody <nobody@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-11-27 22:53:40 +0000

    This commit was manufactured by cvs2svn to create tag 'gas-properties-v3r0500-11-27-2006'.

---
+++ tags/gas-properties-v3r0500-11-27-2006/ideal-gas-v3/src/edu/colorado/phet/coreadditions/ScalarObserver.java
@@ -0,0 +1,12 @@
+/**
+ * Class: ScalarObserver
+ * Package: edu.colorado.phet.coreadditions
+ * Author: Another Guy
+ * Date: Sep 28, 2004
+ */
+package edu.colorado.phet.coreadditions;
+
+import edu.colorado.phet.common.util.SimpleObserver;
+
+public interface ScalarObserver extends SimpleObserver {
+}
