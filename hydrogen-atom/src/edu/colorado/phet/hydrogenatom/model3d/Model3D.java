06a03890bf3aff717c002d4d2d8e9102814ba3b9
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-11-28 18:36:29 +0000

    (11/28/06) - first pass at 3D deBroglie view using Sun's Wireframe code

---
+++ hydrogen-atom/src/edu/colorado/phet/hydrogenatom/model3d/Model3D.java
@@ -0,0 +1,758 @@
+/*
+
+ * Copyright (c) 2003 Sun Microsystems, Inc. All  Rights Reserved.
+
+ * 
+
+ * Redistribution and use in source and binary forms, with or without
+
+ * modification, are permitted provided that the following conditions
+
+ * are met:
+
+ * 
+
+ * -Redistributions of source code must retain the above copyright
+
+ *  notice, this list of conditions and the following disclaimer.
+
+ * 
+
+ * -Redistribution in binary form must reproduct the above copyright
+
+ *  notice, this list of conditions and the following disclaimer in
+
+ *  the documentation and/or other materials provided with the distribution.
+
+ * 
+
+ * Neither the name of Sun Microsystems, Inc. or the names of contributors
+
+ * may be used to endorse or promote products derived from this software
+
+ * without specific prior written permission.
+
+ * 
+
+ * This software is provided "AS IS," without a warranty of any kind. ALL
+
+ * EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND WARRANTIES, INCLUDING
+
+ * ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+
+ * OR NON-INFRINGEMENT, ARE HEREBY EXCLUDED. SUN AND ITS LICENSORS SHALL NOT
+
+ * BE LIABLE FOR ANY DAMAGES OR LIABILITIES SUFFERED BY LICENSEE AS A RESULT
+
+ * OF OR RELATING TO USE, MODIFICATION OR DISTRIBUTION OF THE SOFTWARE OR ITS
+
+ * DERIVATIVES. IN NO EVENT WILL SUN OR ITS LICENSORS BE LIABLE FOR ANY LOST
+
+ * REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT, SPECIAL, CONSEQUENTIAL,
+
+ * INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER CAUSED AND REGARDLESS OF THE THEORY
+
+ * OF LIABILITY, ARISING OUT OF THE USE OF OR INABILITY TO USE SOFTWARE, EVEN
+
+ * IF SUN HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+
+ * 
+
+ * You acknowledge that Software is not designed, licensed or intended for
+
+ * use in the design, construction, operation or maintenance of any nuclear
+
+ * facility.
+
+ */
+
+
+
+/*
+
+ * CHANGE LIST:
+
+ * 
+
+ * 11/27/06 - obtained code from http://www.cs.cf.ac.uk/Dave/JAVA/3d/3d.html
+
+ * 11/27/06 - discovered code was distributed by Sun with JDK 1.4.2
+
+ * 11/28/06 - added missing copyright header
+
+ * 11/28/06 - formatted code to PhET standards
+
+ * 11/28/06 - organized imports to PhET standards
+
+ * 11/28/06 - made all member data private, added mutators and accessors where needed
+
+ * 11/28/06 - added missing public modifiers to methods
+
+ * 11/28/06 - added some comments
+
+ */
+
+
+
+package edu.colorado.phet.hydrogenatom.model3d;
+
+
+
+import java.awt.Color;
+
+import java.awt.Graphics;
+
+import java.io.*;
+
+
+
+/**
+
+ * Model3D is the representation of a 3D model.
+
+ * <p>
+
+ * This code was distributed with JDK 1.4.2, as the Wireframe example applet.
+
+ */
+
+public class Model3D {
+
+
+
+    private float vert[];
+
+    private int tvert[];
+
+    private int nvert, maxvert;
+
+    private int con[];
+
+    private int ncon, maxcon;
+
+    private boolean transformed;
+
+    private Matrix3D mat;
+
+    private float xmin, xmax, ymin, ymax, zmin, zmax;
+
+
+
+    /*
+
+     * Create a 3D model by parsing an input stream
+
+     */
+
+    public Model3D( InputStream is ) throws IOException {
+
+        mat = new Matrix3D();
+
+        mat.xrot( 20 );
+
+        mat.yrot( 30 );
+
+        Reader r = new BufferedReader( new InputStreamReader( is ) );
+
+        StreamTokenizer st = new StreamTokenizer( r );
+
+        st.eolIsSignificant( true );
+
+        st.commentChar( '#' );
+
+        scan: while ( true ) {
+
+            switch ( st.nextToken() ) {
+
+            default:
+
+                break scan;
+
+            case StreamTokenizer.TT_EOL:
+
+                break;
+
+            case StreamTokenizer.TT_WORD:
+
+                if ( "v".equals( st.sval ) ) {
+
+                    double x = 0, y = 0, z = 0;
+
+                    if ( st.nextToken() == StreamTokenizer.TT_NUMBER ) {
+
+                        x = st.nval;
+
+                        if ( st.nextToken() == StreamTokenizer.TT_NUMBER ) {
+
+                            y = st.nval;
+
+                            if ( st.nextToken() == StreamTokenizer.TT_NUMBER ) {
+
+                                z = st.nval;
+
+                            }
+
+                        }
+
+                    }
+
+                    addVert( (float) x, (float) y, (float) z );
+
+                    while ( st.ttype != StreamTokenizer.TT_EOL && st.ttype != StreamTokenizer.TT_EOF ) {
+
+                        st.nextToken();
+
+                    }
+
+                }
+
+                else if ( "f".equals( st.sval ) || "fo".equals( st.sval ) || "l".equals( st.sval ) ) {
+
+                    int start = -1;
+
+                    int prev = -1;
+
+                    int n = -1;
+
+                    while ( true ) {
+
+                        if ( st.nextToken() == StreamTokenizer.TT_NUMBER ) {
+
+                            n = (int) st.nval;
+
+                            if ( prev >= 0 ) {
+
+                                add( prev - 1, n - 1 );
+
+                            }
+
+                            if ( start < 0 ) {
+
+                                start = n;
+
+                            }
+
+                            prev = n;
+
+                        }
+
+                        else if ( st.ttype == '/' ) {
+
+                            st.nextToken();
+
+                        }
+
+                        else {
+
+                            break;
+
+                        }
+
+                    }
+
+                    if ( start >= 0 ) {
+
+                        add( start - 1, prev - 1 );
+
+                    }
+
+                    if ( st.ttype != StreamTokenizer.TT_EOL ) {
+
+                        break scan;
+
+                    }
+
+                }
+
+                else {
+
+                    while ( st.nextToken() != StreamTokenizer.TT_EOL && st.ttype != StreamTokenizer.TT_EOF ) {
+
+                        ;
+
+                    }
+
+                }
+
+            }
+
+        }
+
+        is.close();
+
+    }
+
+
+
+    public Matrix3D getMatrix() {
+
+        return mat;
+
+    }
+
+    
+
+    public void setTransformed( boolean transformed ) {
+
+        this.transformed = transformed;
+
+    }
+
+    
+
+    /*
+
+     * Add a vertex to this model
+
+     */
+
+    private int addVert( float x, float y, float z ) {
+
+        int i = nvert;
+
+        if ( i >= maxvert ) {
+
+            if ( vert == null ) {
+
+                maxvert = 100;
+
+                vert = new float[maxvert * 3];
+
+            }
+
+            else {
+
+                maxvert *= 2;
+
+                float nv[] = new float[maxvert * 3];
+
+                System.arraycopy( vert, 0, nv, 0, vert.length );
+
+                vert = nv;
+
+            }
+
+        }
+
+        i *= 3;
+
+        vert[i] = x;
+
+        vert[i + 1] = y;
+
+        vert[i + 2] = z;
+
+        return nvert++;
+
+    }
+
+
+
+    /*
+
+     * Add a line from vertex p1 to vertex p2
+
+     */
+
+    private void add( int p1, int p2 ) {
+
+        int i = ncon;
+
+        if ( p1 >= nvert || p2 >= nvert ) {
+
+            return;
+
+        }
+
+        if ( i >= maxcon ) {
+
+            if ( con == null ) {
+
+                maxcon = 100;
+
+                con = new int[maxcon];
+
+            }
+
+            else {
+
+                maxcon *= 2;
+
+                int nv[] = new int[maxcon];
+
+                System.arraycopy( con, 0, nv, 0, con.length );
+
+                con = nv;
+
+            }
+
+        }
+
+        if ( p1 > p2 ) {
+
+            int t = p1;
+
+            p1 = p2;
+
+            p2 = t;
+
+        }
+
+        con[i] = ( p1 << 16 ) | p2;
+
+        ncon = i + 1;
+
+    }
+
+
+
+    /**
+
+     * Transform all the points in this model
+
+     */
+
+    public void transform() {
+
+        if ( transformed || nvert <= 0 ) {
+
+            return;
+
+        }
+
+        if ( tvert == null || tvert.length < nvert * 3 ) {
+
+            tvert = new int[nvert * 3];
+
+        }
+
+        mat.transform( vert, tvert, nvert );
+
+        transformed = true;
+
+    }
+
+
+
+    private void sort( int lo0, int hi0 ) {
+
+        int a[] = con;
+
+        int lo = lo0;
+
+        int hi = hi0;
+
+        if ( lo >= hi ) {
+
+            return;
+
+        }
+
+        int mid = a[( lo + hi ) / 2];
+
+        while ( lo < hi ) {
+
+            while ( lo < hi && a[lo] < mid ) {
+
+                lo++;
+
+            }
+
+            while ( lo < hi && a[hi] >= mid ) {
+
+                hi--;
+
+            }
+
+            if ( lo < hi ) {
+
+                int T = a[lo];
+
+                a[lo] = a[hi];
+
+                a[hi] = T;
+
+            }
+
+        }
+
+        if ( hi < lo ) {
+
+            int T = hi;
+
+            hi = lo;
+
+            lo = T;
+
+        }
+
+        sort( lo0, lo );
+
+        sort( lo == lo0 ? lo + 1 : lo, hi0 );
+
+    }
+
+
+
+    /**
+
+     * eliminate duplicate lines
+
+     */
+
+    public void compress() {
+
+        int limit = ncon;
+
+        int c[] = con;
+
+        sort( 0, ncon - 1 );
+
+        int d = 0;
+
+        int pp1 = -1;
+
+        for ( int i = 0; i < limit; i++ ) {
+
+            int p1 = c[i];
+
+            if ( pp1 != p1 ) {
+
+                c[d] = p1;
+
+                d++;
+
+            }
+
+            pp1 = p1;
+
+        }
+
+        ncon = d;
+
+    }
+
+
+
+    static Color gr[];
+
+
+
+    /**
+
+     * Paint this model to a graphics context.  It uses the matrix associated
+
+     * with this model to map from model space to screen space.
+
+     */
+
+    public void paint( Graphics g ) {
+
+        if ( vert == null || nvert <= 0 ) {
+
+            return;
+
+        }
+
+        transform();
+
+        if ( gr == null ) {
+
+            gr = new Color[16];
+
+            for ( int i = 0; i < 16; i++ ) {
+
+                int grey = (int) ( 170 * ( 1 - Math.pow( i / 15.0, 2.3 ) ) );
+
+                gr[i] = new Color( grey, grey, grey );
+
+                gr[i] = Color.BLUE; //XXX
+
+            }
+
+        }
+
+        int lg = 0;
+
+        int lim = ncon;
+
+        int c[] = con;
+
+        int v[] = tvert;
+
+        if ( lim <= 0 || nvert <= 0 ) {
+
+            return;
+
+        }
+
+        for ( int i = 0; i < lim; i++ ) {
+
+            int T = c[i];
+
+            int p1 = ( ( T >> 16 ) & 0xFFFF ) * 3;
+
+            int p2 = ( T & 0xFFFF ) * 3;
+
+            int grey = v[p1 + 2] + v[p2 + 2];
+
+            if ( grey < 0 ) {
+
+                grey = 0;
+
+            }
+
+            if ( grey > 15 ) {
+
+                grey = 15;
+
+            }
+
+            if ( grey != lg ) {
+
+                lg = grey;
+
+                g.setColor( gr[grey] );
+
+            }
+
+            g.drawLine( v[p1], v[p1 + 1], v[p2], v[p2 + 1] );
+
+        }
+
+    }
+
+
+
+    /**
+
+     * Find the bounding box of this model
+
+     */
+
+    public void findBB() {
+
+        if ( nvert <= 0 ) {
+
+            return;
+
+        }
+
+        float v[] = vert;
+
+        float xmin = v[0], xmax = xmin;
+
+        float ymin = v[1], ymax = ymin;
+
+        float zmin = v[2], zmax = zmin;
+
+        for ( int i = nvert * 3; ( i -= 3 ) > 0; ) {
+
+            float x = v[i];
+
+            if ( x < xmin ) {
+
+                xmin = x;
+
+            }
+
+            if ( x > xmax ) {
+
+                xmax = x;
+
+            }
+
+            float y = v[i + 1];
+
+            if ( y < ymin ) {
+
+                ymin = y;
+
+            }
+
+            if ( y > ymax ) {
+
+                ymax = y;
+
+            }
+
+            float z = v[i + 2];
+
+            if ( z < zmin ) {
+
+                zmin = z;
+
+            }
+
+            if ( z > zmax ) {
+
+                zmax = z;
+
+            }
+
+        }
+
+        this.xmax = xmax;
+
+        this.xmin = xmin;
+
+        this.ymax = ymax;
+
+        this.ymin = ymin;
+
+        this.zmax = zmax;
+
+        this.zmin = zmin;
+
+    }
+
+    
+
+    public float getXMax() {
+
+        return xmax;
+
+    }
+
+    
+
+    public float getXMin() {
+
+        return xmin;
+
+    }
+
+    
+
+    public float getYMax() {
+
+        return ymax;
+
+    }
+
+    
+
+    public float getYMin() {
+
+        return ymin;
+
+    }
+
+    
+
+    public float getZMax() {
+
+        return zmax;
+
+    }
+
+    
+
+    public float getZMin() {
+
+        return zmin;
+
+    }
+
+}
+
--- hydrogen-atom/src/edu/colorado/phet/hydrogenatom/view/atom/DeBroglieHeight3DNode.java
+++ hydrogen-atom/src/edu/colorado/phet/hydrogenatom/view/atom/DeBroglieHeight3DNode.java
@@ -11,28 +11,159 @@
 
 package edu.colorado.phet.hydrogenatom.view.atom;
 
-import java.awt.Color;
-import java.awt.Font;
+import java.awt.BasicStroke;
+import java.awt.Stroke;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.DecimalFormat;
 
-import edu.colorado.phet.hydrogenatom.HAConstants;
 import edu.colorado.phet.hydrogenatom.model.DeBroglieModel;
+import edu.colorado.phet.hydrogenatom.model3d.Matrix3D;
+import edu.colorado.phet.hydrogenatom.model3d.Model3D;
+import edu.colorado.phet.hydrogenatom.model3d.PNode3D;
 import edu.colorado.phet.hydrogenatom.view.atom.DeBroglieNode.AbstractDeBroglieViewStrategy;
-import edu.umd.cs.piccolo.nodes.PText;
+import edu.colorado.phet.hydrogenatom.view.particle.ProtonNode;
+import edu.umd.cs.piccolo.PNode;
 
 
 public class DeBroglieHeight3DNode extends AbstractDeBroglieViewStrategy {
     
+    private static final int WAVE_POINTS = 200;
+    private static final int WAVE_FREQUENCY = 6;
+    private static final Stroke WAVE_STROKE = new BasicStroke( 2f );
+    
+    private PNode3D _waveNode;
+    private int count = 0;
+    private Matrix3D _amat;
+    
     public DeBroglieHeight3DNode( DeBroglieModel atom ) {
         super( atom );
+        _amat = new Matrix3D();
+        _amat.xrot( 60 );
+        initStaticNodes();
+        update();
+    }
+    
+    /*
+     * Creates nodes for the proton and orbits.
+     */
+    private void initStaticNodes() {
+
+        // Orbits (pseudo-3D perspective)
+        int groundState = DeBroglieModel.getGroundState();
+        int numberOfStates = DeBroglieModel.getNumberOfStates();
+        for ( int state = groundState; state < ( groundState + numberOfStates ); state++ ) {
+            double radius = getAtom().getOrbitRadius( state );
+            PNode orbitNode = OrbitNodeFactory.createPerspectiveOrbitNode( radius, 2 );
+            addChild( orbitNode );
+        }
         
-        PText text = new PText( "\"3D height\" view is not implemented");
-        text.setFont( new Font( HAConstants.DEFAULT_FONT_NAME, Font.PLAIN, 18 ) );
-        text.setOffset( -text.getWidth()/2, -text.getHeight()/2 );
-        text.setTextPaint( Color.WHITE );
-        addChild( text );
+        // Proton
+        ProtonNode protonNode = new ProtonNode();
+        protonNode.setOffset( 0, 0 );
+        addChild( protonNode );
     }
     
     public void update() {
-        //XXX
+        
+        double amplitude = 6 * Math.sin( count / 10.0 );
+        double phase = count / 100.0;
+        double radius = getAtom().getElectronOrbitRadius();
+
+        Point3D[] points = getPoints( WAVE_POINTS, radius, WAVE_FREQUENCY, amplitude, phase );
+        InputStream stream = pointsToStream( points );
+        Model3D model3d = null;
+        try {
+            model3d = new Model3D( stream );
+        }
+        catch ( IOException ioe ) {
+            throw new RuntimeException( ioe );
+        }
+        
+        model3d.compress();
+        model3d.findBB();
+        
+        Matrix3D matrix = model3d.getMatrix();
+        matrix.unit();
+        float xt = -( model3d.getXMin() + model3d.getXMax() ) / 2;
+        float yt = -( model3d.getYMin() + model3d.getYMax() ) / 2;
+        float zt = -( model3d.getZMin() + model3d.getZMax() ) / 2;
+        matrix.translate( xt, yt, zt );
+        matrix.mult( _amat );
+        model3d.setTransformed( false );
+        
+        if ( _waveNode != null ) {
+            removeChild( _waveNode );
+        }
+        _waveNode = new PNode3D( model3d, WAVE_STROKE );
+        addChild( _waveNode );
+        
+        count++;
+    }
+    
+    /*
+     * Gets the points that approximate a standing wave.
+     */
+    private static Point3D[] getPoints( int numberOfPoints, double radius, double zFreq, double zAmp, double zPhase ) {
+        Point3D[] points = new Point3D[numberOfPoints];
+        for ( int i = 0; i < numberOfPoints; i++ ) {
+            double frac = i / ( (double) numberOfPoints - 1 );
+            double angle = Math.PI * 2 * frac;
+            double x = radius * Math.cos( angle );
+            double y = radius * Math.sin( angle );
+            double z = zAmp * Math.sin( angle * zFreq + zPhase );
+            Point3D pt = new Point3D( x, y, z );
+            points[i] = pt;
+        }
+        return points;
+    }
+    
+    /*
+     * Converts an array of 3D points to an input stream.
+     * The Model3D constructor is expecting to read an input stream,
+     * most likely from a file that contains info about the model.
+     * The input stream format is reportedly the same as the Wavefront OBJ file format.
+     * Tokens found in Dave's sample data files:
+     * v = vertex
+     * l = line
+     * f = face
+     * fo = ?
+     * g = ?
+     */
+    private static InputStream pointsToStream( Point3D[] points ) {
+        StringBuffer buf = new StringBuffer();
+        DecimalFormat formatter = new DecimalFormat( "0.0000" );
+        for ( int i = 0; i < points.length; i++ ) {
+            Point3D p = points[i];
+            buf.append( "v " );
+            buf.append( formatter.format( p.x ) );
+            buf.append( " " );
+            buf.append( formatter.format( p.y ) );
+            buf.append( " " );
+            buf.append( formatter.format( p.z ) );
+            buf.append( System.getProperty( "line.separator" ) );
+        }
+        for ( int i = 1; i <= points.length; i++ ) {
+            buf.append( "l " );
+            buf.append( i );
+            buf.append( " " );
+            buf.append( i + 1 );
+            buf.append( System.getProperty( "line.separator" ) );
+        }
+        buf.append( "l " );
+        buf.append( points.length - 1 );
+        buf.append( " 1" );
+        InputStream is = new ByteArrayInputStream( buf.toString().getBytes() );
+        return is;
+    }
+    
+    private static class Point3D {
+        double x, y, z;
+        public Point3D( double x, double y, double z ) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
     }
 }
