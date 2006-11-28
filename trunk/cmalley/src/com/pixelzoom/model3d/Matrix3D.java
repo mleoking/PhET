de6309419bdd60604a2b6913a089bef0949a6771
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-11-28 17:46:33 +0000

    change "Dave 3D" stuff to indicate that it's the stuff that Sun distributes with it's Wireframe applet example. Add Sun's copyright headers that were missing from Dave's stuff.

--- trunk/cmalley/src/com/pixelzoom/model3d/Matrix3D.java
+++ trunk/cmalley/src/com/pixelzoom/model3d/Matrix3D.java
@@ -1,12 +1,43 @@
+/*
+ * Copyright (c) 2003 Sun Microsystems, Inc. All  Rights Reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ * -Redistributions of source code must retain the above copyright
+ *  notice, this list of conditions and the following disclaimer.
+ * 
+ * -Redistribution in binary form must reproduct the above copyright
+ *  notice, this list of conditions and the following disclaimer in
+ *  the documentation and/or other materials provided with the distribution.
+ * 
+ * Neither the name of Sun Microsystems, Inc. or the names of contributors
+ * may be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ * 
+ * This software is provided "AS IS," without a warranty of any kind. ALL
+ * EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND WARRANTIES, INCLUDING
+ * ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+ * OR NON-INFRINGEMENT, ARE HEREBY EXCLUDED. SUN AND ITS LICENSORS SHALL NOT
+ * BE LIABLE FOR ANY DAMAGES OR LIABILITIES SUFFERED BY LICENSEE AS A RESULT
+ * OF OR RELATING TO USE, MODIFICATION OR DISTRIBUTION OF THE SOFTWARE OR ITS
+ * DERIVATIVES. IN NO EVENT WILL SUN OR ITS LICENSORS BE LIABLE FOR ANY LOST
+ * REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT, SPECIAL, CONSEQUENTIAL,
+ * INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER CAUSED AND REGARDLESS OF THE THEORY
+ * OF LIABILITY, ARISING OUT OF THE USE OF OR INABILITY TO USE SOFTWARE, EVEN
+ * IF SUN HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+ * 
+ * You acknowledge that Software is not designed, licensed or intended for
+ * use in the design, construction, operation or maintenance of any nuclear
+ * facility.
+ */
 
-package com.pixelzoom.dave3d;
+package com.pixelzoom.model3d;
 
 /**
  * Matrix3D is a fairly conventional 3D matrix that can transform sets of
  * 3D points and perform a variety of manipulations on the transform.
- * <p>
- * This code came from http://www.cs.cf.ac.uk/Dave/JAVA/3d/3d.html
- * Copyright Dr. A. D. Marshall, Cardiff School of Computer Science
  */
 public class Matrix3D {
 
