92a9dad148360e9543c8e233dce841bac7936efe
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-11-29 20:50:32 +0000

    (11/29/06) - add workaround for selecting all text when a DoubleSpinner gains focus

--- quantum-tunneling/src/edu/colorado/phet/quantumtunneling/control/DoubleSpinner.java
+++ quantum-tunneling/src/edu/colorado/phet/quantumtunneling/control/DoubleSpinner.java
@@ -18,42 +18,64 @@ import java.awt.event.FocusListener;
 import javax.swing.JFormattedTextField;
 import javax.swing.JSpinner;
 import javax.swing.SpinnerNumberModel;
-import javax.swing.JSpinner.NumberEditor;
+import javax.swing.SwingUtilities;
 
 
-public class DoubleSpinner extends JSpinner implements FocusListener { 
-    
+/**
+ * DoubleSpinner is a spinner that contains a double precision value.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ * @version $Revision$
+ */
+public class DoubleSpinner extends JSpinner implements FocusListener {
+
     private JFormattedTextField _textField;
-    
+
     public DoubleSpinner( double value, double min, double max, double step, String format, Dimension size ) {
-        super( );
-        // model
-        SpinnerNumberModel model = new SpinnerNumberModel( value, min, max, step );
-        setModel( model );
-        // editor
-        NumberEditor numberEditor = new NumberEditor( this, format );
-        setEditor( numberEditor );
-        _textField = numberEditor.getTextField();
-        _textField.addFocusListener( this );
-        // size
-        setPreferredSize( size );
-        setMinimumSize( size );
+        super();
+
+        // Spinner 
+        {
+            // model
+            SpinnerNumberModel model = new SpinnerNumberModel( value, min, max, step );
+            setModel( model );
+            // editor
+            NumberEditor numberEditor = new NumberEditor( this, format );
+            setEditor( numberEditor );
+            _textField = numberEditor.getTextField();
+            _textField.addFocusListener( this );
+            // size
+            setPreferredSize( size );
+            setMinimumSize( size );
+        }
     }
 
     public double getDoubleValue() {
-        return ((Double) getValue()).doubleValue();
+        return ( (Double) getValue() ).doubleValue();
     }
-    
+
     public void setDoubleValue( double value ) {
         setValue( new Double( value ) );
     }
-    
+
+    public JFormattedTextField getFormattedTextField() {
+        return _textField;
+    }
+
     /*
      * When the spinner gains focus, select its contents.
-     * NOTE: This currently does not work; see Bug ID 4699955 at bugs.sun.com
+     * <p>
+     * NOTE: 
+     * This currently does not work; see Bug ID 4699955 at bugs.sun.com
+     * A workaround is to call selectAll in invokeLater.
      */
     public void focusGained( FocusEvent e ) {
-        _textField.selectAll();
+        SwingUtilities.invokeLater( new Runnable() {
+
+            public void run() {
+                _textField.selectAll();
+            }
+        } );
     }
 
     public void focusLost( FocusEvent e ) {}
