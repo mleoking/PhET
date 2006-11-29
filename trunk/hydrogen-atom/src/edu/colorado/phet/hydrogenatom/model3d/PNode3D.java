c81943f631f8bb8f89aa0e44c537ce9dd17b1b8d
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-11-29 01:06:56 +0000

    document hack

--- trunk/hydrogen-atom/src/edu/colorado/phet/hydrogenatom/model3d/PNode3D.java
+++ trunk/hydrogen-atom/src/edu/colorado/phet/hydrogenatom/model3d/PNode3D.java
@@ -12,9 +12,6 @@
 package edu.colorado.phet.hydrogenatom.model3d;
 
 import java.awt.Color;
-import java.awt.Graphics2D;
-import java.awt.RenderingHints;
-import java.awt.Stroke;
 import java.awt.geom.Rectangle2D;
 
 import edu.umd.cs.piccolo.PNode;
@@ -31,16 +28,18 @@ public class PNode3D extends PNode {
         
         _wireframe = wireframe;
         
-        //HACK to get the bounds to refresh
-        PPath boundsPath = new PPath();
-        double x = _wireframe.getXMin();
-        double y = _wireframe.getYMin();
-        double w = _wireframe.getXMax() - x;
-        double h = _wireframe.getYMax() - y;
-        boundsPath.setPathTo( new Rectangle2D.Double( x, y, w, h ) );
-        boundsPath.setStroke( null );
-        boundsPath.setPaint( new Color( 0, 0, 0, 0 ) );
-        addChild( boundsPath );
+        //XXX HACK to get the bounds to refresh
+        {
+            PPath boundsPath = new PPath();
+            double x = _wireframe.getXMin();
+            double y = _wireframe.getYMin();
+            double w = _wireframe.getXMax() - x;
+            double h = _wireframe.getYMax() - y;
+            boundsPath.setPathTo( new Rectangle2D.Double( x, y, w, h ) );
+            boundsPath.setStroke( null );
+            boundsPath.setPaint( new Color( 0, 0, 0, 0 ) );
+            addChild( boundsPath );
+        }
     }
     
     protected void paint( PPaintContext paintContext ) {
