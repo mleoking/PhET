b6a0ba1e1d0e9d75f826a7bdbf111a5cf8d9e594
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-12-16 18:33:11 +0000

    Fixed: Virtual Ammeter doesn't update unless dragged.

--- trunk/cck3/src/edu/colorado/phet/cck/piccolo_cck/VirtualAmmeterNode.java
+++ trunk/cck3/src/edu/colorado/phet/cck/piccolo_cck/VirtualAmmeterNode.java
@@ -2,6 +2,9 @@ package edu.colorado.phet.cck.piccolo_cck;
 
 import edu.colorado.phet.cck.ICCKModule;
 import edu.colorado.phet.cck.model.Circuit;
+import edu.colorado.phet.cck.model.CircuitListenerAdapter;
+import edu.colorado.phet.cck.model.Junction;
+import edu.colorado.phet.cck.model.analysis.CircuitSolutionListener;
 import edu.colorado.phet.cck.model.components.Branch;
 import edu.colorado.phet.common.view.util.SimStrings;
 import edu.colorado.phet.piccolo.PhetPNode;
@@ -11,9 +14,7 @@ import edu.umd.cs.piccolo.event.PInputEvent;
 import edu.umd.cs.piccolo.util.PDimension;
 
 import java.awt.*;
-import java.awt.geom.AffineTransform;
 import java.awt.geom.Point2D;
-import java.awt.geom.Rectangle2D;
 import java.text.DecimalFormat;
 
 /**
@@ -44,15 +45,51 @@ public class VirtualAmmeterNode extends PhetPNode {
         addInputEventListener( new PBasicInputEventHandler() {
             public void mouseDragged( PInputEvent event ) {
                 PDimension pt = event.getDeltaRelativeTo( VirtualAmmeterNode.this );
-                Rectangle2D.Double rect = getFullBounds();
-                Rectangle2D proposedBounds = AffineTransform.getTranslateInstance( pt.width, pt.height ).createTransformedShape( rect ).getBounds2D();
-//                if( module.getCCKModel().getModelBounds().contains( proposedBounds ) ) {
                 translate( pt.width, pt.height );
                 update();
-//                }
             }
         } );
         addInputEventListener( new CursorHandler() );
+        module.getCCKModel().getCircuitSolver().addSolutionListener( new CircuitSolutionListener() {
+            public void circuitSolverFinished() {
+                update();
+            }
+        } );
+        circuit.addCircuitListener( new CircuitListenerAdapter() {
+
+            public void junctionRemoved( Junction junction ) {
+                update();
+            }
+
+            public void branchRemoved( Branch branch ) {
+                update();
+            }
+
+            public void junctionsMoved() {
+                update();
+            }
+
+            public void branchesMoved( Branch[] branches ) {
+                update();
+            }
+
+            public void junctionAdded( Junction junction ) {
+                update();
+            }
+
+            public void junctionsConnected( Junction a, Junction b, Junction newTarget ) {
+                update();
+            }
+
+            public void junctionsSplit( Junction old, Junction[] j ) {
+                update();
+            }
+
+            public void branchAdded( Branch branch ) {
+                update();
+            }
+
+        } );
         update();
     }
 
