86602abc74887166e81065485fd4cf5fb7ec18e9
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-12-18 19:17:20 +0000

    no message

--- trunk/mri/src/edu/colorado/phet/mri/MriConfig.java
+++ trunk/mri/src/edu/colorado/phet/mri/MriConfig.java
@@ -34,7 +34,7 @@ public class MriConfig {
     // Descriptive information
     public static final String TITLE = SimStrings.get( "Application.Title" );
     public static final String DESCRIPTION = SimStrings.get( "Application.Description" );
-    public static final String VERSION = "0.01";
+    public static final String VERSION = "1.01";
 
     public final static double scale = 1;
 
