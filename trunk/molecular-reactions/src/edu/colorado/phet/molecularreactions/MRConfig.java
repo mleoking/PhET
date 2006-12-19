77437593256caefa5eb702ff8669904bc6239c13
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-12-19 22:10:11 +0000

    no message

--- trunk/molecular-reactions/src/edu/colorado/phet/molecularreactions/MRConfig.java
+++ trunk/molecular-reactions/src/edu/colorado/phet/molecularreactions/MRConfig.java
@@ -27,8 +27,7 @@ public class MRConfig {
     // Misc
     //--------------------------------------------------------------------------------------------------
     // Version
-    public static final String VERSION = "0.01";
-    public static final String SUB_VERSION = "0.03";
+    public static final String VERSION = "1.01";
     // Prefix of the strings bundles
     public static final String LOCALIZATION_BUNDLE = "localization/MRStrings";
     // Debug flag
