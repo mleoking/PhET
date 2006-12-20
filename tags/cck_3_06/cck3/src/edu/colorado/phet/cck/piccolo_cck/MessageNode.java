3a8de0b79fd9727896ebae54c5fcd2e557fb2cd9
nobody <nobody@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-12-20 22:39:23 +0000

    This commit was manufactured by cvs2svn to create tag 'cck_3_06'.

---
+++ tags/cck_3_06/cck3/src/edu/colorado/phet/cck/piccolo_cck/MessageNode.java
@@ -0,0 +1,13 @@
+package edu.colorado.phet.cck.piccolo_cck;
+
+import edu.colorado.phet.piccolo.PhetPNode;
+
+/**
+ * User: Sam Reid
+ * Date: Sep 14, 2006
+ * Time: 5:12:43 PM
+ * Copyright (c) Sep 14, 2006 by Sam Reid
+ */
+
+public class MessageNode extends PhetPNode {
+}
