9f7f6f669b01b018e9162bb216c9b70ddc74568b
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-12-22 03:19:32 +0000

    (12/21/06) - move SpectrometerNode to view package

--- hydrogen-atom/src/edu/colorado/phet/hydrogenatom/view/SpectrometerNode.java
+++ hydrogen-atom/src/edu/colorado/phet/hydrogenatom/view/SpectrometerNode.java
@@ -9,7 +9,7 @@
  * Date modified : $Date$
  */
 
-package edu.colorado.phet.hydrogenatom.spectrometer;
+package edu.colorado.phet.hydrogenatom.view;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
