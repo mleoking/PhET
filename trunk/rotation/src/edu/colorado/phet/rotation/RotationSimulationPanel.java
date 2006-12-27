76c755a456e0c04b983f209bf4efae765d682ebd
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-12-27 18:45:31 +0000

    Initial revision

---
+++ trunk/rotation/src/edu/colorado/phet/rotation/RotationSimulationPanel.java
@@ -0,0 +1,13 @@
+package edu.colorado.phet.rotation;
+
+import edu.colorado.phet.piccolo.PhetPCanvas;
+
+/**
+ * User: Sam Reid
+ * Date: Dec 27, 2006
+ * Time: 11:34:12 AM
+ * Copyright (c) Dec 27, 2006 by Sam Reid
+ */
+
+public class RotationSimulationPanel extends PhetPCanvas {
+}
