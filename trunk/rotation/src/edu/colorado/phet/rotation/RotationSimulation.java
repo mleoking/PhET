61b488f3c19ee121b4d0611bf7e42ee22e211a76
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-12-28 15:13:13 +0000

    no message

--- trunk/rotation/src/edu/colorado/phet/rotation/RotationSimulation.java
+++ trunk/rotation/src/edu/colorado/phet/rotation/RotationSimulation.java
@@ -6,6 +6,10 @@ import edu.colorado.phet.common.view.util.FrameSetup;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 
 /**
  * User: Sam Reid
@@ -17,11 +21,22 @@ import java.awt.*;
 public class RotationSimulation extends PhetApplication {
     public static final String TITLE = "Rotational Motion";
     public static final String DESCRIPTION = "Rotational Motion Simulation";
-    public static final String VERSION = "0.00.01";
 
     public RotationSimulation( String[] args ) {
-        super( args, TITLE, DESCRIPTION, VERSION, createFrameSetup() );
+        super( args, TITLE, DESCRIPTION, readVersion(), createFrameSetup() );
         addModule( new RotationModule() );
+    }
+
+    private static String readVersion() {
+        URL url = Thread.currentThread().getContextClassLoader().getResource( "cck.version" );
+        try {
+            BufferedReader br = new BufferedReader( new InputStreamReader( url.openStream() ) );
+            return br.readLine();
+        }
+        catch( IOException e ) {
+            e.printStackTrace();
+            return "Version Not Found";
+        }
     }
 
     private static FrameSetup createFrameSetup() {
