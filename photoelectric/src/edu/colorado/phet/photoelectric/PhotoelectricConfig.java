ea91b8bf78f4ab43523137d46b11ab2f1dab36aa
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-12-28 23:04:56 +0000

    updated version numbers

--- photoelectric/src/edu/colorado/phet/photoelectric/PhotoelectricConfig.java
+++ photoelectric/src/edu/colorado/phet/photoelectric/PhotoelectricConfig.java
@@ -21,7 +21,7 @@ import java.awt.*;
 public class PhotoelectricConfig {
 
     // Version
-    public static final String VERSION = "1.02";
+    public static final String VERSION = "1.02.01";
 
     // Images
     public static final String IMAGE_DIRECTORY = "images/";
