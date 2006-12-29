86bffe1e16330293a517fdc8f3db31de0a63a157
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-12-29 05:43:38 +0000

    added controls & greek characters

---
+++ trunk/rotation/src/edu/colorado/phet/rotation/graphs/ControlGraph.java
@@ -0,0 +1,16 @@
+package edu.colorado.phet.rotation.graphs;
+
+import edu.umd.cs.piccolo.PNode;
+
+/**
+ * User: Sam Reid
+ * Date: Dec 28, 2006
+ * Time: 10:38:00 PM
+ * Copyright (c) Dec 28, 2006 by Sam Reid
+ */
+
+public class ControlGraph extends PNode {
+
+    public ControlGraph() {
+    }
+}
