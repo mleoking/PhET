0dcee1611a11217d09cb187c54768a595c5730b8
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2006-12-30 07:58:30 +0000

    added graph controls

--- trunk/rotation/tests-src/edu/colorado/phet/rotation/tests/TestTimeDerivative.java
+++ trunk/rotation/tests-src/edu/colorado/phet/rotation/tests/TestTimeDerivative.java
@@ -1,8 +1,7 @@
 package edu.colorado.phet.rotation.tests;
 
-import JSci.maths.LinearMath;
-import JSci.maths.polynomials.RealPolynomial;
-import JSci.maths.vectors.AbstractDoubleVector;
+import edu.colorado.phet.rotation.model.RotationMath;
+import edu.colorado.phet.rotation.model.TimeData;
 
 /**
  * User: Sam Reid
@@ -11,7 +10,7 @@ import JSci.maths.vectors.AbstractDoubleVector;
  * Copyright (c) Dec 30, 2006 by Sam Reid
  */
 
-public class TestLinearRegression {
+public class TestTimeDerivative {
 
     /**
      * public static AbstractDoubleVector linearRegression(double[][] data)
@@ -27,15 +26,13 @@ public class TestLinearRegression {
      */
     public static void main( String[] args ) {
         double[] x = new double[]{0, 1, 2, 3, 4};
-        double[] y = new double[]{1, 3, 5, 7, 9.01};
-        int n = 2;
-        double[][] data = new double[n][x.length];
-        data[0] = x;
-        data[1] = y;
-        AbstractDoubleVector out = LinearMath.linearRegression( data );
-        System.out.println( "out = " + out );
+        double[] y = new double[]{1, 3, 5, 7, 9};
 
-        RealPolynomial lsf = LinearMath.leastSquaresFit( 2, data );
-        System.out.println( "lsf = " + lsf );
+        TimeData[] timeData = new TimeData[x.length];
+        for( int i = 0; i < timeData.length; i++ ) {
+            timeData[i] = new TimeData( y[i], x[i] );
+        }
+        double slope = RotationMath.estimateDerivative( timeData );
+        System.out.println( "slope = " + slope );
     }
 }
