e7b70f8e2e2ad8b2e10d676daac284ba94535b92
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-01-01 18:17:59 +0000

    added chart slider controls

--- rotation/src/edu/colorado/phet/rotation/graphs/BasicChartSlider.java
+++ rotation/src/edu/colorado/phet/rotation/graphs/BasicChartSlider.java
@@ -4,8 +4,6 @@ import edu.colorado.phet.jfreechart.piccolo.JFreeChartNode;
 import edu.umd.cs.piccolo.PNode;
 import org.jfree.chart.event.ChartChangeEvent;
 import org.jfree.chart.event.ChartChangeListener;
-import org.jfree.chart.plot.CombinedDomainXYPlot;
-import org.jfree.chart.plot.XYPlot;
 
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
@@ -18,21 +16,18 @@ import java.beans.PropertyChangeListener;
  *
  * @author Sam Reid
  */
-public class CombinedChartSlider extends AbstractChartSlider {
+public class BasicChartSlider extends AbstractChartSlider {
     private JFreeChartNode jFreeChartNode;
-    private int index;
 
     /**
      * Constructs a VerticalChartControl to use the data area of the specified JFreeChartNode and the specified graphic for the thumb control.
      *
      * @param jFreeChartNode the chart to control.
      * @param sliderThumb    the PNode that displays the thumb of the slider.
-     * @param index          the index of the subplot
      */
-    public CombinedChartSlider( final JFreeChartNode jFreeChartNode, final PNode sliderThumb, int index ) {
+    public BasicChartSlider( final JFreeChartNode jFreeChartNode, final PNode sliderThumb ) {
         super( sliderThumb );
         this.jFreeChartNode = jFreeChartNode;
-        this.index = index;
         jFreeChartNode.addPropertyChangeListener( new PropertyChangeListener() {
             public void propertyChange( PropertyChangeEvent evt ) {
                 updateLayout();
@@ -47,44 +42,39 @@ public class CombinedChartSlider extends AbstractChartSlider {
         updateLayout();
     }
 
-    protected double getMinRangeValue() {
-        if( jFreeChartNode == null ) {
-            return 0;
-        }
-        return getSubplot().getRangeAxis().getLowerBound();
-    }
-
-    private XYPlot getSubplot() {
-        CombinedDomainXYPlot combinedDomainXYPlot = (CombinedDomainXYPlot)jFreeChartNode.getChart().getXYPlot();
-        return (XYPlot)combinedDomainXYPlot.getSubplots().get( index );
-    }
-
     protected double getMaxRangeValue() {
         if( jFreeChartNode == null ) {
             return 0;
         }
-        return getSubplot().getRangeAxis().getUpperBound();
+        return jFreeChartNode.getChart().getXYPlot().getRangeAxis().getRange().getUpperBound();
+    }
+
+    protected double getMinRangeValue() {
+        if( jFreeChartNode == null ) {
+            return 0;
+        }
+        return jFreeChartNode.getChart().getXYPlot().getRangeAxis().getRange().getLowerBound();
     }
 
     protected Rectangle2D getDataArea() {
         if( jFreeChartNode == null ) {
             return new Rectangle2D.Double( 0, 0, 0, 0 );
         }
-        return jFreeChartNode.getDataArea( index );
+        return jFreeChartNode.getDataArea();
     }
 
     protected Point2D plotToNode( Point2D.Double aDouble ) {
         if( jFreeChartNode == null ) {
             return new Point2D.Double();
         }
-        return jFreeChartNode.subplotToNode( aDouble, index );
+        return jFreeChartNode.plotToNode( aDouble );
     }
 
     protected Point2D nodeToPlot( Point2D.Double pt ) {
         if( jFreeChartNode == null ) {
             return new Point2D.Double();
         }
-        return jFreeChartNode.nodeToSubplot( pt, index );
+        return jFreeChartNode.nodeToPlot( pt );
     }
 
 }
---
+++ rotation/tests-src/edu/colorado/phet/rotation/tests/TestBasicVerticalSliderChart.java
@@ -0,0 +1,58 @@
+package edu.colorado.phet.rotation.tests;
+
+/**
+ * User: Sam Reid
+ * Date: Jan 1, 2007
+ * Time: 11:00:07 AM
+ * Copyright (c) Jan 1, 2007 by Sam Reid
+ */
+
+import edu.colorado.phet.jfreechart.piccolo.JFreeChartNode;
+import edu.colorado.phet.piccolo.PhetPCanvas;
+import edu.colorado.phet.rotation.graphs.BasicVerticalSliderChart;
+import edu.umd.cs.piccolo.nodes.PText;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+import javax.swing.*;
+
+public class TestBasicVerticalSliderChart {
+    private JFrame frame;
+
+    public TestBasicVerticalSliderChart() {
+        frame = new JFrame();
+        frame.setSize( 600, 600 );
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+
+        PhetPCanvas phetPCanvas = new PhetPCanvas();
+        frame.setContentPane( phetPCanvas );
+
+        int xIndex = 0;
+        final XYSeries series = new XYSeries( "series_1" );
+        for( xIndex = 0; xIndex < 100; xIndex++ ) {
+            series.add( xIndex, Math.sin( xIndex / 100.0 * Math.PI * 2 ) );
+        }
+        XYDataset dataset = new XYSeriesCollection( series );
+        JFreeChart jFreeChart = ChartFactory.createXYLineChart( "title", "x", "y", dataset, PlotOrientation.VERTICAL, false, false, false );
+        jFreeChart.getXYPlot().getRangeAxis().setAutoRange( false );
+        jFreeChart.getXYPlot().getRangeAxis().setRange( -2, 2 );
+        JFreeChartNode jFreeChartNode = new JFreeChartNode( jFreeChart );
+        jFreeChartNode.setBounds( 0, 0, 500, 400 );
+
+        BasicVerticalSliderChart chart = new BasicVerticalSliderChart( jFreeChartNode, new PText( "Hello" ) );
+        chart.setOffset( 100, 100 );
+        phetPCanvas.addScreenChild( chart );
+    }
+
+    public static void main( String[] args ) {
+        new TestBasicVerticalSliderChart().start();
+    }
+
+    private void start() {
+        frame.setVisible( true );
+    }
+}
