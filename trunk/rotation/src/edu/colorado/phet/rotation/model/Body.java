aae9291d29f10db510fd01766ad55de6e070bf58
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-01-02 19:02:11 +0000

    rotation graph set

--- trunk/rotation/src/edu/colorado/phet/rotation/model/Body.java
+++ trunk/rotation/src/edu/colorado/phet/rotation/model/Body.java
@@ -1,5 +1,10 @@
 package edu.colorado.phet.rotation.model;
 
+import edu.colorado.phet.common.math.AbstractVector2D;
+import edu.colorado.phet.common.math.Vector2D;
+
+import java.awt.geom.Point2D;
+
 /**
  * User: Sam Reid
  * Date: Dec 28, 2006
@@ -25,5 +30,32 @@ public class Body {
         catch( CloneNotSupportedException e ) {
             throw new RuntimeException( e );
         }
+    }
+
+    public double getX( RotationModelState rotationModelState ) {
+        return distance * Math.cos( angleOffset + rotationModelState.getAngle() );
+    }
+
+    public double getY( RotationModelState rotationModelState ) {
+        return distance * Math.sin( angleOffset + rotationModelState.getAngle() );
+    }
+
+    public double getVelocityX( RotationModelState rotationModelState ) {
+        return getVelocity( rotationModelState ).getX();
+    }
+
+    public double getVelocityY( RotationModelState rotationModelState ) {
+        return getVelocity( rotationModelState ).getY();
+    }
+
+    public Point2D getPosition( RotationModelState rotationModelState ) {
+        return new Point2D.Double( getX( rotationModelState ), getY( rotationModelState ) );
+    }
+
+    private AbstractVector2D getVelocity( RotationModelState rotationModelState ) {
+        double v = distance * rotationModelState.getAngularVelocity();//v=r*omega
+        Point2D position = getPosition( rotationModelState );
+        double angle = new Vector2D.Double( position ).getAngle();
+        return Vector2D.Double.parseAngleAndMagnitude( v, angle );
     }
 }
