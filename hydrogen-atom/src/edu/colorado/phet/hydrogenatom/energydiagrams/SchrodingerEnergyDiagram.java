0a0e53243db78356276249e2a6007e4680ed2a5b
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-01-02 22:17:43 +0000

    (1/2/06) - implement Schrodinger energy diagram with no squiggles, no m=

--- hydrogen-atom/src/edu/colorado/phet/hydrogenatom/energydiagrams/SchrodingerEnergyDiagram.java
+++ hydrogen-atom/src/edu/colorado/phet/hydrogenatom/energydiagrams/SchrodingerEnergyDiagram.java
@@ -35,20 +35,27 @@ import edu.umd.cs.piccolox.nodes.PComposite;
 public class SchrodingerEnergyDiagram extends AbstractEnergyDiagram implements Observer {
     
     private static final double X_MARGIN = 10;
-    private static final double Y_MARGIN = 20;
+    private static final double Y_MARGIN = 10;
     
-    private static final double LINE_LABEL_SPACING = 6;
+    private static final double LINE_LINE_SPACING = 10;
+    private static final double LINE_LABEL_SPACING = 10;
+
+    private double _lHeight;
     
     public SchrodingerEnergyDiagram() {
         super( SchrodingerModel.getNumberOfStates() );
         
         assert( SchrodingerModel.getGroundState() == 1 ); // n=1 must be ground state
 
-        for ( int i = 0; i < BohrModel.getNumberOfStates(); i++ ) { 
-            int n = i + 1;
-            PNode levelNode = createLevelNode( n );
-            double x = getXOffset( n ) + X_MARGIN;
-            double y = getYOffset( n ) + Y_MARGIN;
+        PNode lNode = createLNode();
+        lNode.setOffset( getDrawingArea().getX() + X_MARGIN, Y_MARGIN );
+        addChild( lNode );
+        _lHeight = lNode.getFullBounds().getHeight();
+        
+        for ( int n = 1; n <= SchrodingerModel.getNumberOfStates(); n++ ) { 
+            PNode levelNode = createNNode( n );
+            double x = getXOffset( 0 );
+            double y = getYOffset( n );
             levelNode.setOffset( x, y );
             addChild( levelNode );
         }
@@ -56,42 +63,77 @@ public class SchrodingerEnergyDiagram extends AbstractEnergyDiagram implements O
     
     public void update( Observable o, Object arg ) {
         if ( o instanceof BohrModel ) {
-            BohrModel atom = (BohrModel) o;
+            SchrodingerModel atom = (SchrodingerModel) o;
             if ( arg == AbstractHydrogenAtom.PROPERTY_ELECTRON_STATE ) {
                 ElectronNode electronNode = getElectronNode();
                 final int n = atom.getElectronState();
-                final double x = getXOffset( n ) + ( getLineLength() / 2 ) + X_MARGIN;
-                final double y = getYOffset( n ) - ( electronNode.getFullBounds().getHeight() / 2 ) + Y_MARGIN;
+                final int l = atom.getSecondaryElectronState();
+                final double x = getXOffset( l ) + ( LINE_LENGTH / 2 );
+                final double y = getYOffset( n ) - ( electronNode.getFullBounds().getHeight() / 2 );
                 electronNode.setOffset( x, y );
             }
         }
     }
     
-    protected double getXOffset( int state ) {
-        return getDrawingArea().getX() + X_MARGIN;
+    protected double getXOffset( int l ) {
+        return getDrawingArea().getX() + X_MARGIN + ( l * LINE_LENGTH ) + ( l * LINE_LINE_SPACING );
     }
     
-    protected double getYOffset( int state ) {
+    protected double getYOffset( int n ) {
         final double minE = getEnergy( 1 );
         final double maxE = getEnergy( BohrModel.getNumberOfStates() );
         final double rangeE = maxE - minE;
         Rectangle2D drawingArea = getDrawingArea();
-        final double height = drawingArea.getHeight() - ( 2 * Y_MARGIN );
-        double y = drawingArea.getY() + ( height * ( maxE - getEnergy( state ) ) / rangeE );
+        final double electronHeight = getElectronNode().getFullBounds().getHeight();
+        final double height = drawingArea.getHeight() - ( 2 * Y_MARGIN ) - _lHeight - electronHeight;
+        double y = drawingArea.getY() + Y_MARGIN + _lHeight + electronHeight + ( height * ( maxE - getEnergy( n ) ) / rangeE );
         return y;
     }
     
-    private static PNode createLevelNode( int state ) {
+    private static PNode createLNode() {
         
-        PNode lineNode = createLineNode();
+        final int numberOfStates = SchrodingerModel.getNumberOfStates();
+        
+        PComposite parentNode = new PComposite();
+        
+        PText labelNode = new PText( "l=");
+        labelNode.setFont( LABEL_FONT );
+        labelNode.setTextPaint( LABEL_COLOR );
+        parentNode.addChild( labelNode );
+        labelNode.setOffset( -labelNode.getWidth(), 0 );
+        
+        for ( int l = 0; l < numberOfStates; l++ ) {
+            PText valueNode = new PText( String.valueOf( l ) );
+            valueNode.setFont( LABEL_FONT );
+            valueNode.setTextPaint( LABEL_COLOR );
+            valueNode.setOffset( ( l * LINE_LENGTH ) + ( l * LINE_LINE_SPACING ) + ( LINE_LENGTH / 2 ) - ( valueNode.getWidth() / 2 ), 0 );
+            parentNode.addChild( valueNode );
+        }
+        
+        return parentNode;
+    }
+    
+    private static PNode createNNode( int state ) {
+        
+        final int numberOfStates = SchrodingerModel.getNumberOfStates();
+
+        PComposite linesParentNode = new PComposite();
+        for ( int i = 0; i < state; i++ ) {
+            PNode lineNode = createLineNode();
+            lineNode.setOffset( i * ( LINE_LENGTH + LINE_LINE_SPACING ), 0 );
+            linesParentNode.addChild( lineNode );
+        }
+
         PNode labelNode = createLabelNode( state );
         
         PComposite parentNode = new PComposite();
-        parentNode.addChild( lineNode );
+        parentNode.addChild( linesParentNode );
         parentNode.addChild( labelNode );
         
-        lineNode.setOffset( 0, 0 );
-        labelNode.setOffset( lineNode.getWidth() + LINE_LABEL_SPACING, -( ( lineNode.getHeight() / 2 ) + ( labelNode.getHeight() / 2 ) ) );
+        linesParentNode.setOffset( 0, 0 );
+        double x = ( numberOfStates * LINE_LENGTH ) + ( ( numberOfStates - 1 ) * LINE_LINE_SPACING ) + LINE_LABEL_SPACING;
+        double y = -( ( linesParentNode.getHeight() / 2 ) + ( labelNode.getHeight() / 2 ) );
+        labelNode.setOffset( x, y );
         
         return parentNode;
     }
