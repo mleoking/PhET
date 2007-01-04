e8734245d051f64fd12048f5767c949c40ffea7f
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-01-04 02:32:49 +0000

    javadoc

--- hydrogen-atom/src/edu/colorado/phet/hydrogenatom/energydiagrams/DeBroglieEnergyDiagram.java
+++ hydrogen-atom/src/edu/colorado/phet/hydrogenatom/energydiagrams/DeBroglieEnergyDiagram.java
@@ -13,8 +13,14 @@ package edu.colorado.phet.hydrogenatom.energydiagrams;
 
 import edu.umd.cs.piccolox.pswing.PSwingCanvas;
 
-
-
+/**
+ * DeBroglieEnergyDiagram is the energy level diagram that corresponds
+ * to a hydrogen atom based on the deBroglie model. it is identical 
+ * to the diagram for the Bohr model.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ * @version $Revision$
+ */
 public class DeBroglieEnergyDiagram extends BohrEnergyDiagram {
 
     public DeBroglieEnergyDiagram( PSwingCanvas canvas ) {
