254893a50f1510a5f888dc10d8e47dbb26834fbd
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-01-04 18:23:16 +0000

    Renamed class

--- trunk/photoelectric/src/edu/colorado/phet/photoelectric/view/CompositeGraphPanel.java
+++ trunk/photoelectric/src/edu/colorado/phet/photoelectric/view/CompositeGraphPanel.java
@@ -24,12 +24,14 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 /**
- * GraphPanel2
+ * CompositeGraphPanel
+ * <p>
+ * A JPanel that holds all the individual GraphPanels
  *
  * @author Ron LeMaster
  * @version $Revision$
  */
-public class GraphPanel2 extends JPanel {
+public class CompositeGraphPanel extends JPanel {
 
     private ControlPanel controlPanel;
     private int rowIdx;
@@ -37,31 +39,31 @@ public class GraphPanel2 extends JPanel {
     private Module module;
 
 
-    public GraphPanel2( Module module ) {
+    public CompositeGraphPanel( Module module ) {
         super( new GridBagLayout() );
         PhotoelectricModel model = (PhotoelectricModel)module.getModel();
         controlPanel = module.getControlPanel();
         this.module = module;
         Insets graphInsets = new Insets( 5, 20, 17, 15 );
 
-        GraphPanel cvgPanel2 = new GraphPanel( module.getClock() );
-        cvgPanel2.setGraph( new CurrentVsVoltageGraph( cvgPanel2, model ), graphInsets );
+        GraphPanel currentVsVoltagePanel = new GraphPanel( module.getClock() );
+        currentVsVoltagePanel.setGraph( new CurrentVsVoltageGraph( currentVsVoltagePanel, model ), graphInsets );
         addGraph( SimStrings.get( "GraphTitle.CurrentVsVoltage" ),
-                  cvgPanel2,
+                  currentVsVoltagePanel,
                   SimStrings.get( "Voltage" ),
                   SimStrings.get( "Current" ));
 
-        GraphPanel panel2 = new GraphPanel( module.getClock() );
-        panel2.setGraph( new CurrentVsIntensityGraph( panel2, model ), graphInsets );
+        GraphPanel currentVsIntensityPanel = new GraphPanel( module.getClock() );
+        currentVsIntensityPanel.setGraph( new CurrentVsIntensityGraph( currentVsIntensityPanel, model ), graphInsets );
         addGraph( SimStrings.get( "GraphTitle.CurrentVsIntensity" ),
-                  panel2,
+                  currentVsIntensityPanel,
                   SimStrings.get( "GraphLabel.Intensity" ),
                   SimStrings.get( "GraphLabel.Current" ) );
 
-        GraphPanel panel3 = new GraphPanel( module.getClock() );
-        panel3.setGraph( new EnergyVsFrequencyGraph( panel2, model ), graphInsets );
+        GraphPanel energyVsFreqPanel = new GraphPanel( module.getClock() );
+        energyVsFreqPanel.setGraph( new EnergyVsFrequencyGraph( currentVsIntensityPanel, model ), graphInsets );
         addGraph( SimStrings.get( "GraphTitle.EnergyVsFrequency" ),
-                  panel3,
+                  energyVsFreqPanel,
                   SimStrings.get( "GraphLabel.Frequency"),
                   SimStrings.get( "GraphLabel.Energy" ));
 
@@ -80,45 +82,58 @@ public class GraphPanel2 extends JPanel {
     }
 
     private void addGraph( String title, final GraphPanel graphPanel, String xLabel, String yLabel ) {
+
+        // The checkbox that controls visibility of the graph
+        final JCheckBox cb = new JCheckBox( title );
+        checkBoxes.add( cb );
+        final RotatedTextLabel yAxisLabel = new RotatedTextLabel( yLabel );
+        final JLabel xAxisLabel = new JLabel( xLabel );
+        final JButton zoomInBtn = new JButton( new ZoomInAction( graphPanel.getGraph() ) );
+        final JButton zoomOutBtn = new JButton( new ZoomOutAction( graphPanel.getGraph() ) );
+        cb.addActionListener( new ActionListener() {
+            public void actionPerformed( ActionEvent e ) {
+                graphPanel.setVisible( cb.isSelected() );
+                graphPanel.clearGraph();
+                yAxisLabel.setVisible( cb.isSelected() );
+                xAxisLabel.setVisible( cb.isSelected() );
+                zoomInBtn.setVisible( cb.isSelected() );
+                zoomOutBtn.setVisible( cb.isSelected() );
+                setLogoVisibility( controlPanel );
+            }
+        } );
+        graphPanel.setVisible( cb.isSelected() );
+        yAxisLabel.setVisible( cb.isSelected() );
+        xAxisLabel.setVisible( cb.isSelected() );
+        zoomInBtn.setVisible( cb.isSelected() );
+        zoomOutBtn.setVisible( cb.isSelected() );
+
+
+        // Lay out the panel
         GridBagConstraints checkBoxGbc = new GridBagConstraints( 0, 0,
                                                                  2, 1, 0, 0,
                                                                  GridBagConstraints.NORTHWEST,
                                                                  GridBagConstraints.NONE,
                                                                  new Insets( 0, 0, 0, 25 ), 0, 0 );
         GridBagConstraints graphGbc = new GridBagConstraints( 1, 0,
-                                                              1, 1, 0, 0,
+                                                              1, 2, 0, 0,
                                                               GridBagConstraints.NORTHWEST,
                                                               GridBagConstraints.NONE,
                                                               new Insets( 0, 0, 0, 0 ), 0, 0 );
-
         GridBagConstraints xLabelGbc = new GridBagConstraints( 1, 0,
                                                                1, 1, 0, 0,
                                                                GridBagConstraints.NORTH,
                                                                GridBagConstraints.NONE,
                                                                new Insets( 0, 0, 0, 0 ), 0, 0 );
-
         GridBagConstraints yLabelGbc = new GridBagConstraints( 0, 0,
-                                                               1, 1, 0, 0,
+                                                               1, 2, 0, 0,
                                                                GridBagConstraints.WEST,
                                                                GridBagConstraints.NONE,
                                                                new Insets( 0, 0, 0, 0 ), 0, 0 );
-
-        final JCheckBox cb = new JCheckBox( title );
-        checkBoxes.add( cb );
-        final RotatedTextLabel currentVsVoltageYLabel = new RotatedTextLabel( yLabel );
-        final JLabel currentVsVoltageXLabel = new JLabel( xLabel );
-        cb.addActionListener( new ActionListener() {
-            public void actionPerformed( ActionEvent e ) {
-                graphPanel.setVisible( cb.isSelected() );
-                graphPanel.clearGraph();
-                currentVsVoltageYLabel.setVisible( cb.isSelected() );
-                currentVsVoltageXLabel.setVisible( cb.isSelected() );
-                setLogoVisibility( controlPanel );
-            }
-        } );
-        graphPanel.setVisible( cb.isSelected() );
-        currentVsVoltageYLabel.setVisible( cb.isSelected() );
-        currentVsVoltageXLabel.setVisible( cb.isSelected() );
+        GridBagConstraints zoomBtnGbc = new GridBagConstraints( 2, 0,
+                                                               1, 1, 0, 0,
+                                                               GridBagConstraints.NORTH,
+                                                               GridBagConstraints.NONE,
+                                                               new Insets( 0, 0, 0, 0 ), 0, 0 );
 
         checkBoxGbc.gridy = rowIdx;
         add( cb, checkBoxGbc );
@@ -126,12 +141,46 @@ public class GraphPanel2 extends JPanel {
         rowIdx++;
         graphGbc.gridy = rowIdx;
         yLabelGbc.gridy = rowIdx;
-        add( currentVsVoltageYLabel, yLabelGbc );
+        add( yAxisLabel, yLabelGbc );
         add( graphPanel, graphGbc );
+
+        zoomBtnGbc.gridy = rowIdx;
+        add( zoomInBtn,  zoomBtnGbc );
+        rowIdx++;
+        zoomBtnGbc.gridy = rowIdx;
+        add( zoomOutBtn,  zoomBtnGbc );
+
         rowIdx++;
         xLabelGbc.gridy = rowIdx;
-        add( currentVsVoltageXLabel, xLabelGbc );
+        add( xAxisLabel, xLabelGbc );
+
+
         rowIdx++;
     }
 
+    private static class ZoomInAction extends AbstractAction {
+        private PhotoelectricGraph graph;
+
+        public ZoomInAction( PhotoelectricGraph graph ) {
+            super( "+" );
+            this.graph = graph;
+        }
+
+        public void actionPerformed( ActionEvent e ) {
+            graph.zoomIn();
+        }
+    }
+
+    private static class ZoomOutAction extends AbstractAction {
+        private PhotoelectricGraph graph;
+
+        public ZoomOutAction( PhotoelectricGraph graph ) {
+            super( "-" );
+            this.graph = graph;
+        }
+
+        public void actionPerformed( ActionEvent e ) {
+            graph.zoomOut();
+        }
+    }
 }
