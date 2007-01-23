3f98faf268810434ace28090fddd195b52dc3b61
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-01-23 07:42:23 +0000

    added buffered rendering strategy

--- trunk/rotation/src/edu/colorado/phet/rotation/graphs/DynamicJFreeChartNode.java
+++ trunk/rotation/src/edu/colorado/phet/rotation/graphs/DynamicJFreeChartNode.java
@@ -176,6 +176,7 @@ public class DynamicJFreeChartNode extends JFreeChartNode {
         }
 
         public void dataAdded() {
+            dynamicJFreeChartNode
         }
 
         public void uninstall() {
