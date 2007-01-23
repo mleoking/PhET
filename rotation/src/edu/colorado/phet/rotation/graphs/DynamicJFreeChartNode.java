f720e40dfbc763af9dfe259d6e61be1d599ae175
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-01-23 19:37:06 +0000

    added tests, improved state transitions

--- rotation/src/edu/colorado/phet/rotation/graphs/DynamicJFreeChartNode.java
+++ rotation/src/edu/colorado/phet/rotation/graphs/DynamicJFreeChartNode.java
@@ -24,6 +24,17 @@ import java.util.ArrayList;
  * have our own rendering strategies here.
  * <p/>
  * Data is added to the chart through addValue() methods, not through the underlying XYSeriesCollection dataset.
+ * <p/>
+ * The 3 rendering styles are:
+ * 1. JFreeChart renderer: This uses the JFreeChart subsystem to do all the data rendering.
+ * This looks beautiful and comes built-in to jfreechart but has performance problems during dynamic data display, since the entire region
+ * is repainted whenever a single point changes.
+ * <p/>
+ * 2. Piccolo renderer: This uses a PPath to render the path as a PNode child of the JFreeChartNode
+ * This looks fine and reduces the clip region necessary for painting.  When combined with a buffered jfreechartnode, this can improve computation substantially.
+ * This renderer is combined with a PClip to ensure no data is drawn outside the chart's data area (which could otherwise change the fullbounds of the jfreechartnode..
+ * <p/>
+ * 3. Buffered renderer: This draws directly to the buffer in the JFreeChartNode, and only repaints the changed region of the screen.
  */
 public class DynamicJFreeChartNode extends JFreeChartNode {
     private ArrayList seriesDataList = new ArrayList();
