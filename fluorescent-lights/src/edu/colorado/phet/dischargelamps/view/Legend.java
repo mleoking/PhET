9da7180c529c219b0cc6ebfd36ee87f36363ade6
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-01-27 23:30:02 +0000

    no message

--- fluorescent-lights/src/edu/colorado/phet/dischargelamps/view/Legend.java
+++ fluorescent-lights/src/edu/colorado/phet/dischargelamps/view/Legend.java
@@ -13,9 +13,14 @@ package edu.colorado.phet.dischargelamps.view;
 import edu.colorado.phet.common.view.util.ImageLoader;
 import edu.colorado.phet.common.view.util.SimStrings;
 import edu.colorado.phet.common.math.Vector2D;
+import edu.colorado.phet.common.util.PhetUtilities;
 import edu.colorado.phet.dischargelamps.DischargeLampsConfig;
+import edu.colorado.phet.dischargelamps.model.DischargeLampAtom;
+import edu.colorado.phet.dischargelamps.model.DischargeLampModel;
+import edu.colorado.phet.dischargelamps.model.HydrogenProperties;
 import edu.colorado.phet.lasers.view.PhotonGraphic;
 import edu.colorado.phet.quantum.model.Photon;
+import edu.colorado.phet.quantum.view.AnnotatedAtomGraphic;
 
 import javax.swing.*;
 import javax.swing.border.TitledBorder;
@@ -33,6 +38,7 @@ import java.io.IOException;
 public class Legend extends JPanel {
     Icon electronIcon = null;
     Icon photonIcon = null;
+    Icon atomIcon = null;
 
     public Legend() {
         super( new GridBagLayout() );
@@ -44,16 +50,18 @@ public class Legend extends JPanel {
     private void layoutPanel() {
         GridBagConstraints iconGbc = new GridBagConstraints( 0, GridBagConstraints.RELATIVE,
                                                              1, 1, 1, 1,
-                                                             GridBagConstraints.EAST,
+                                                             GridBagConstraints.CENTER,
                                                              GridBagConstraints.NONE,
-                                                             new Insets( 0, 10, 5, 10 ),
+                                                             new Insets( 0, 60, 5, 10 ),
                                                              0, 0 );
         GridBagConstraints labelGbc = new GridBagConstraints( 1, GridBagConstraints.RELATIVE,
                                                               1, 1, 1, 1,
-                                                              GridBagConstraints.NORTHWEST,
+                                                              GridBagConstraints.WEST,
                                                               GridBagConstraints.NONE,
-                                                              new Insets( 0, 10, 5, 10 ),
+                                                              new Insets( 0, 0, 5, 10 ),
                                                               0, 0 );
+        add( new JLabel( atomIcon ), iconGbc );
+        add( new JLabel( SimStrings.get( "Legend.atom" ) ), labelGbc );
         add( new JLabel( electronIcon ), iconGbc );
         add( new JLabel( SimStrings.get( "Legend.electron" ) ), labelGbc );
         add( new JLabel( photonIcon ), iconGbc );
@@ -61,6 +69,8 @@ public class Legend extends JPanel {
     }
 
     private void createIcons() {
+
+        // Make the electron icon
         try {
             BufferedImage electronImage = ImageLoader.loadBufferedImage( DischargeLampsConfig.ELECTRON_IMAGE_FILE_NAME );
             electronIcon = new ImageIcon( electronImage );
@@ -68,8 +78,22 @@ public class Legend extends JPanel {
         catch( IOException e ) {
             e.printStackTrace();
         }
+
+        // Make the photon icon
         Photon photon = Photon.create( 400, new Point2D.Double(), new Vector2D.Double() );
         BufferedImage photonImage = PhotonGraphic.getInstance( this, photon ).getImage();
         photonIcon = new ImageIcon( photonImage );
+
+        // Make the atom icon
+        DischargeLampAtom atom = new DischargeLampAtom( new DischargeLampModel(),
+                                                        new HydrogenProperties() );
+        AnnotatedAtomGraphic atomGraphic = new AnnotatedAtomGraphic( this, atom );
+        BufferedImage atomBI = new BufferedImage( atomGraphic.getWidth(), atomGraphic.getHeight(),
+                                                  BufferedImage.TYPE_INT_ARGB_PRE );
+        Graphics2D g2BI = (Graphics2D)atomBI.getGraphics();
+        g2BI.translate( atomGraphic.getWidth() / 2, atomGraphic.getHeight() / 2);
+        g2BI.scale( .8, .8 );
+        atomGraphic.paint( g2BI );
+        atomIcon = new ImageIcon( atomBI );
     }
 }
