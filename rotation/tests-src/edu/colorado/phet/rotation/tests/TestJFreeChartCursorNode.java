8a7fa966a6cba8bf2d1194a71a3b5e1bdb1558d0
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-01-29 16:01:43 +0000

    added cursor

---
+++ rotation/tests-src/edu/colorado/phet/rotation/tests/TestJFreeChartCursorNode.java
@@ -0,0 +1,23 @@
+package edu.colorado.phet.rotation.tests;
+
+import edu.colorado.phet.rotation.graphs.JFreeChartCursorNode;
+
+/**
+ * User: Sam Reid
+ * Date: Jan 29, 2007
+ * Time: 8:35:28 AM
+ * Copyright (c) Jan 29, 2007 by Sam Reid
+ */
+
+public class TestJFreeChartCursorNode extends TestDynamicJFreeChartNodeTree {
+
+    public TestJFreeChartCursorNode() {
+        JFreeChartCursorNode jFreeChartCursorNode = new JFreeChartCursorNode( getDynamicJFreeChartNode() );
+        getPhetPCanvas().addScreenChild( jFreeChartCursorNode );
+    }
+
+    public static void main( String[] args ) {
+        new TestJFreeChartCursorNode().start();
+    }
+
+}
