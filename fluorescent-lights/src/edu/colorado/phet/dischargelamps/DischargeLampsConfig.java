5df1c142961e3614926854ac59b53bc97bd0ced8
ronlemaster <ronlemaster@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-02-05 18:29:52 +0000

    no message

--- fluorescent-lights/src/edu/colorado/phet/dischargelamps/DischargeLampsConfig.java
+++ fluorescent-lights/src/edu/colorado/phet/dischargelamps/DischargeLampsConfig.java
@@ -23,7 +23,7 @@ import java.text.DecimalFormat;
 public class DischargeLampsConfig {
 
     // Version
-    public static String version = "1.02.13";
+    public static String version = "1.02.14";
 
     // Localization
     public static final String localizedStringsPath = "localization/DischargeLampsStrings";
