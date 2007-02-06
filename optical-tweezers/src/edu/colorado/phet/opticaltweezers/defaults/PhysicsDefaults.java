43742746ee6faddd9cf08505f26237e61dfba9bc
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-02-06 17:50:56 +0000

    standardize names for things related to fluid control panel

--- optical-tweezers/src/edu/colorado/phet/opticaltweezers/defaults/PhysicsDefaults.java
+++ optical-tweezers/src/edu/colorado/phet/opticaltweezers/defaults/PhysicsDefaults.java
@@ -49,7 +49,7 @@ public class PhysicsDefaults {
     public static final boolean RULER_SELECTED = false;
     public static final boolean POSITION_HISTOGRAM_SELECTED = false;
     public static final boolean ADVANCED_VISIBLE = false;
-    public static final boolean FLUID_AND_FLOW_CONTROLS_SELECTED = false;
+    public static final boolean FLUID_CONTROLS_SELECTED = false;
     public static final boolean MOMENTUM_CHANGE_MODEL_SELECTED = false;
     public static final boolean POTENTIAL_ENERGY_CHART_SELECTED = false;
     
