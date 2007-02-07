0ebfdb44e4af206033c4926cc14b6ada3451c1ad
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-02-07 00:06:02 +0000

    (2/6/07) - add warning sign to laser control panel (2/6/07) - toggle laser start/stop button

--- optical-tweezers/src/edu/colorado/phet/opticaltweezers/control/LaserControlPanel.java
+++ optical-tweezers/src/edu/colorado/phet/opticaltweezers/control/LaserControlPanel.java
@@ -15,13 +15,15 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
-import javax.swing.BorderFactory;
-import javax.swing.Box;
-import javax.swing.JButton;
-import javax.swing.JLabel;
+import javax.swing.*;
 
 import edu.colorado.phet.common.view.HorizontalLayoutPanel;
+import edu.colorado.phet.common.view.util.ImageLoader;
 import edu.colorado.phet.common.view.util.SimStrings;
 import edu.colorado.phet.opticaltweezers.OTConstants;
 import edu.colorado.phet.opticaltweezers.util.DoubleRange;
@@ -29,16 +31,41 @@ import edu.colorado.phet.opticaltweezers.util.DoubleRange;
 
 public class LaserControlPanel extends HorizontalLayoutPanel {
 
+    private boolean _laserRunning;//XXX won't need this when we can check the model state
     private JButton _startStopButton;
     private LaserPowerControl _powerControl;
     
     public LaserControlPanel( Font font ) {
         super();
         
-        JLabel cautionLabel = new JLabel( "CAUTION!" );
+        JLabel laserSign = null;
+        try {
+            BufferedImage image = ImageLoader.loadBufferedImage( OTConstants.IMAGE_LASER_SIGN );
+            Icon icon = new ImageIcon( image );
+            laserSign = new JLabel( icon );
+        }
+        catch ( IOException e ) {
+            e.printStackTrace();
+            laserSign = new JLabel( "Caution!" );
+        }
         
+        _laserRunning = false;
         _startStopButton = new JButton( SimStrings.get( "label.startLaser" ) );
         _startStopButton.setOpaque( false );
+        _startStopButton.addActionListener( new ActionListener() {
+            public void actionPerformed( ActionEvent e ) {
+                if ( _laserRunning ) {
+                    //XXX stop laser
+                    _startStopButton.setText( SimStrings.get( "label.startLaser" ) );
+                    _laserRunning = false;
+                }
+                else {
+                    //XXX start laser
+                    _startStopButton.setText( SimStrings.get( "label.stopLaser" ) );
+                    _laserRunning = true;
+                }
+            }
+        } );
         
         DoubleRange range = OTConstants.LASER_POWER_RANGE;
         String label = SimStrings.get( "label.power" );
@@ -53,7 +80,7 @@ public class LaserControlPanel extends HorizontalLayoutPanel {
         setBorder( BorderFactory.createLineBorder( Color.BLACK ) );
         setBackground( Color.DARK_GRAY );
         setInsets( new Insets( 5, 5, 5, 5 ) );
-        add( cautionLabel );
+        add( laserSign );
         add( Box.createHorizontalStrut( 20 ) );
         add( _startStopButton );
         add( Box.createHorizontalStrut( 20 ) );
