268acd3af1d7fd14ec6e03076bfdd2fa4b747d62
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-02-14 10:39:08 +0000

    added spline code & jama package

--- energyconservation3/tests-src/edu/colorado/phet/ec3/test/phys1d/Test2.java
+++ energyconservation3/tests-src/edu/colorado/phet/ec3/test/phys1d/Test2.java
@@ -1,5 +1,6 @@
 package edu.colorado.phet.ec3.test.phys1d;
 
+import edu.colorado.phet.common.view.util.DoubleGeneralPath;
 import edu.colorado.phet.ec3.model.spline.CubicSpline;
 import edu.colorado.phet.ec3.model.spline.SplineSurface;
 import edu.colorado.phet.piccolo.nodes.PhetPPath;
@@ -25,97 +26,126 @@ public class Test2 extends JFrame {
 
     public Test2() {
         PSwingCanvas pSwingCanvas = new PSwingCanvas();
-        setContentPane(pSwingCanvas);
-        CubicSpline cubicSpline = new CubicSpline();
+        setContentPane( pSwingCanvas );
+
+        CubicSpline2D cubicSpline = CubicSpline2D.interpolate( new Point2D[]{
+                new Point2D.Double( 100, 50 ),
+                new Point2D.Double( 200, 100 ),
+                new Point2D.Double( 300, 50 )
+        } );
+
+//        CubicSpline cubicSpline = new CubicSpline();
 //        cubicSpline.addControlPoint(10, 10);
 //        cubicSpline.addControlPoint(200, 200);
 //        cubicSpline.addControlPoint(400, 10);
 
-        cubicSpline.addControlPoint(50, 50);
-        cubicSpline.addControlPoint(225, 50);
-        cubicSpline.addControlPoint(250, 50);
+//        cubicSpline.addControlPoint(50, 50);
+//        cubicSpline.addControlPoint(225, 50);
+//        cubicSpline.addControlPoint(250, 50);
 
-        SplineSurface splineSurface = new SplineSurface(cubicSpline);
-        MySplineGraphic mySplineGraphic = new MySplineGraphic(splineSurface);
-        pSwingCanvas.getLayer().addChild(mySplineGraphic);
-        setSize(800, 600);
+//        SplineSurface splineSurface = new SplineSurface(cubicSpline);
+        MyCubicCurve2DGraphic mySplineGraphic = new MyCubicCurve2DGraphic( cubicSpline );
+        pSwingCanvas.getLayer().addChild( mySplineGraphic );
+        setSize( 800, 600 );
 
-        final Particle1D particle1d = new Particle1D(cubicSpline);
-        ParticleGraphic particleGraphic = new ParticleGraphic(particle1d);
-        pSwingCanvas.getLayer().addChild(particleGraphic);
+        final Particle1D particle1d = new Particle1D( cubicSpline );
+        ParticleGraphic particleGraphic = new ParticleGraphic( particle1d );
+        pSwingCanvas.getLayer().addChild( particleGraphic );
 
-        Timer timer = new Timer(30, new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
+        Timer timer = new Timer( 30, new ActionListener() {
+            public void actionPerformed( ActionEvent e ) {
                 Point2D origLoc = particle1d.getLocation();
                 double origS = particle1d.s;
-                particle1d.stepInTime(1.0);
+                particle1d.stepInTime( 1.0 );
 
 
                 Point2D newLoc = particle1d.getLocation();
-                double dist = newLoc.distance(origLoc);
+                double dist = newLoc.distance( origLoc );
                 double ds = particle1d.s - origS;
-                System.out.println("ds = " + ds + " root(dx^2+dy^2)=" + dist);
+                System.out.println( "ds = " + ds + " root(dx^2+dy^2)=" + dist );
             }
-        });
+        } );
         timer.start();
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
     }
 
     static class ParticleGraphic extends PNode {
         private Particle1D particle1d;
         private PhetPPath phetPPath;
 
-        public ParticleGraphic(Particle1D particle1d) {
+        public ParticleGraphic( Particle1D particle1d ) {
             //To change body of created methods use File | Settings | File Templates.
             this.particle1d = particle1d;
-            phetPPath = new PhetPPath(new BasicStroke(1), Color.red);
-            phetPPath.setPathTo(new Ellipse2D.Double(0, 0, 10, 10));
-            addChild(phetPPath);
-            particle1d.addListener(this);
+            phetPPath = new PhetPPath( new BasicStroke( 1 ), Color.red );
+            phetPPath.setPathTo( new Ellipse2D.Double( 0, 0, 10, 10 ) );
+            addChild( phetPPath );
+            particle1d.addListener( this );
             update();
         }
 
         private void update() {
-            phetPPath.setOffset(particle1d.getX() - phetPPath.getWidth() / 2, particle1d.getY() - phetPPath.getHeight() / 2);
+            phetPPath.setOffset( particle1d.getX() - phetPPath.getWidth() / 2, particle1d.getY() - phetPPath.getHeight() / 2 );
         }
     }
 
     static class Particle1D {
 
         double s = 0;
-        private CubicSpline cubicSpline;
+        private CubicSpline2D cubicSpline;
         private double velocity = 0.005;
         private ArrayList listeners = new ArrayList();
 
-        public Particle1D(CubicSpline cubicSpline) {
+        public Particle1D( CubicSpline2D cubicSpline ) {
             //To change body of created methods use File | Settings | File Templates.
             this.cubicSpline = cubicSpline;
         }
 
         public double getX() {
-            return cubicSpline.evaluateAnalytical(s).getX();
+            return cubicSpline.evaluate( s ).getX();
 //            return 0;  //To change body of created methods use File | Settings | File Templates.
         }
 
         public double getY() {
-            return cubicSpline.evaluateAnalytical(s).getY();
+            return cubicSpline.evaluate( s ).getY();
 //            return 0;  //To change body of created methods use File | Settings | File Templates.
         }
 
-        public void stepInTime(double dt) {
+        public void stepInTime( double dt ) {
             s += velocity * dt;
-            for (int i = 0; i < listeners.size(); i++) {
-                ParticleGraphic particleGraphic = (ParticleGraphic) listeners.get(i);
+            for( int i = 0; i < listeners.size(); i++ ) {
+                ParticleGraphic particleGraphic = (ParticleGraphic)listeners.get( i );
                 particleGraphic.update();
             }
         }
 
-        public void addListener(ParticleGraphic particleGraphic) {
-            listeners.add(particleGraphic);
+        public void addListener( ParticleGraphic particleGraphic ) {
+            listeners.add( particleGraphic );
         }
 
         public Point2D getLocation() {
-            return new Point2D.Double(getX(), getY());
+            return new Point2D.Double( getX(), getY() );
+        }
+    }
+
+    static class MyCubicCurve2DGraphic extends PNode {
+        private CubicSpline2D cubicSpline2D;
+        private PhetPPath phetPPath;
+
+        public MyCubicCurve2DGraphic( CubicSpline2D splineSurface ) {
+            this.cubicSpline2D = splineSurface;
+            //To change body of created methods use File | Settings | File Templates.
+            phetPPath = new PhetPPath( new BasicStroke( 1 ), Color.blue );
+            addChild( phetPPath );
+            update();
+        }
+
+        private void update() {
+            DoubleGeneralPath doubleGeneralPath = new DoubleGeneralPath( cubicSpline2D.evaluate( 0 ) );
+            double ds = 0.01;
+            for( double s = ds; s <= 2.0; s += ds ) {
+                doubleGeneralPath.lineTo( cubicSpline2D.evaluate( s ) );
+            }
+            phetPPath.setPathTo( doubleGeneralPath.getGeneralPath() );
         }
     }
 
@@ -123,25 +153,25 @@ public class Test2 extends JFrame {
         private SplineSurface splineSurface;
         private PhetPPath phetPPath;
 
-        public MySplineGraphic(SplineSurface splineSurface) {
+        public MySplineGraphic( SplineSurface splineSurface ) {
             this.splineSurface = splineSurface;
             //To change body of created methods use File | Settings | File Templates.
-            phetPPath = new PhetPPath(new BasicStroke(1), Color.blue);
-            addChild(phetPPath);
+            phetPPath = new PhetPPath( new BasicStroke( 1 ), Color.blue );
+            addChild( phetPPath );
             update();
         }
 
         private void update() {
-            phetPPath.setPathTo(splineSurface.getSpline().getInterpolationPath());
+            phetPPath.setPathTo( splineSurface.getSpline().getInterpolationPath() );
         }
     }
 
-    public static void main(String[] args) {
+    public static void main( String[] args ) {
         new Test2().start();
     }
 
     private void start() {
         //To change body of created methods use File | Settings | File Templates.
-        setVisible(true);
+        setVisible( true );
     }
 }
