8b4d7de9a7b48649aa650614b22d2fe15cefbabc
nobody <nobody@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-02-16 17:14:11 +0000

    This commit was manufactured by cvs2svn to create tag 'hydrogenatom-1-01-00-2007-02-16'.

---
+++ tags/hydrogenatom-1-01-00-2007-02-16/hydrogen-atom/src/edu/colorado/phet/hydrogenatom/event/GunFiredAdapter.java
@@ -0,0 +1,20 @@
+/* Copyright 2006, University of Colorado */
+
+/*
+ * CVS Info -
+ * Filename : $Source$
+ * Branch : $Name$
+ * Modified by : $Author$
+ * Revision : $Revision$
+ * Date modified : $Date$
+ */
+
+package edu.colorado.phet.hydrogenatom.event;
+
+/**
+ * GunFiredAdapter is a default implementation of GunFiredListener.
+ */
+public class GunFiredAdapter {
+    public void photonFired( GunFiredEvent event ) {}
+    public void alphaParticleFired( GunFiredEvent event ) {}
+}
