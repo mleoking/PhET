ddb4dc93fbc3fa7aa6c640e10a6e2085d288d722
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-02-23 18:14:50 +0000

    add simplified constructor and argument validation

--- trunk/piccolo-phet/src/edu/colorado/phet/piccolo/nodes/HandleNode.java
+++ trunk/piccolo-phet/src/edu/colorado/phet/piccolo/nodes/HandleNode.java
@@ -31,6 +31,8 @@
 
 package edu.colorado.phet.piccolo.nodes;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
 import java.awt.Paint;
 import java.awt.Stroke;
 import java.awt.geom.Area;
@@ -59,16 +61,61 @@ import edu.umd.cs.piccolo.nodes.PPath;
  */
 public class HandleNode extends PhetPNode {
     
-    public HandleNode( double width, double height, double thickness, double cornerRadius, Paint fillPaint, Paint strokePaint, Stroke stroke ) {
+    // Defaults
+    private static final double DEFAULT_THICKNESS_RATIO = 0.4;
+    private static final double DEFAULT_CORNER_WIDTH_RATIO = 0.4;
+    private static final Paint DEFAULT_STROKE_PAINT = Color.BLACK;
+    private static final Stroke DEFAULT_STROKE = new BasicStroke();
+    
+    /**
+     * Simplified constructor, uses default values for many properties.
+     * 
+     * @param width
+     * @param height
+     * @param fillPaint
+     */
+    public HandleNode( double width, double height, Paint fillPaint ) {
+        this( width, height, 
+                DEFAULT_THICKNESS_RATIO * Math.min( width, height ), 
+                DEFAULT_CORNER_WIDTH_RATIO * Math.min( width, height), 
+                fillPaint, DEFAULT_STROKE_PAINT, DEFAULT_STROKE );
+    }
+    
+    /**
+     * Fully-specified constructor.
+     * 
+     * @param width
+     * @param height
+     * @param thickness
+     * @param cornerWidth
+     * @param fillPaint
+     * @param strokePaint
+     * @param stroke
+     */
+    public HandleNode( double width, double height, double thickness, double cornerWidth, Paint fillPaint, Paint strokePaint, Stroke stroke ) {
         super();
+        
+        // Validate arguments
+        if ( width <= 0 ) { 
+            throw new IllegalArgumentException( "width must be > 0" );
+        }
+        if ( height <= 0 ) {
+            throw new IllegalArgumentException( "height must be > 0" );
+        }
+        if ( thickness >= Math.min( width, height ) ) {
+            throw new IllegalArgumentException( "thickness is too large: " + thickness );
+        }
+        if ( cornerWidth < 0 ) {
+            throw new IllegalArgumentException( "cornerWidth must be >= 0" );
+        }
         
         /*
          * Create the handle shape using constructive geometry.
          * To get rounded corners on the handle, create a rounded rectangle 
          * that is twice as wide as needed, then cut it in half.
          */
-        RoundRectangle2D outerRect = new RoundRectangle2D.Double( 0, 0, 2 * width, height, cornerRadius, cornerRadius );
-        RoundRectangle2D innerRect = new RoundRectangle2D.Double( thickness, thickness, 2 * ( width - thickness ), height - ( 2 * thickness ), cornerRadius, cornerRadius );
+        RoundRectangle2D outerRect = new RoundRectangle2D.Double( 0, 0, 2 * width, height, cornerWidth, cornerWidth );
+        RoundRectangle2D innerRect = new RoundRectangle2D.Double( thickness, thickness, 2 * ( width - thickness ), height - ( 2 * thickness ), cornerWidth, cornerWidth );
         Rectangle2D boundsRect = new Rectangle2D.Double( 0, 0, width, height );
         Area handleArea = new Area( outerRect );
         handleArea.exclusiveOr( new Area( innerRect ) );
