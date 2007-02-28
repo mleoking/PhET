1994b68160a01a92d6fa2cd89aedc45556c85ded
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-02-28 20:59:51 +0000

    updated project files after refactor added bernoulli common classes (these were dropped)

---
+++ simulations-java/bernoulli/common/src-hpa/edu/colorado/phet/coreadditions/controllers/AbstractShape.java
@@ -0,0 +1,10 @@
+package edu.colorado.phet.coreadditions.controllers;
+
+import java.awt.*;
+
+/**
+ * Used by some MouseHandlers to determine whether a mouse can handle an event.
+ */
+public interface AbstractShape {
+    boolean containsPoint(Point pt);
+}
