387d5af81098b130a2495dcb8913c6ff0284d977
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-02-28 07:08:10 +0000

    (2-27-2007) Deleted: energyconservation, was deprecated version of energy skate park.

--- trunk/phet/simulations/energyconservation/src-hpa-22/edu/colorado/phet/coreadditions/clock2/SimulationTimeListener.java
+++
@@ -1,9 +0,0 @@
-/*Copyright, Sam Reid, 2003.*/
-package edu.colorado.phet.coreadditions.clock2;
-
-/**
- * Listens for time simulation events.
- */
-public interface SimulationTimeListener {
-    public void simulationTimeIncreased( double dt );
-}
--- trunk/phet/simulations/readme.txt
+++ trunk/phet/simulations/readme.txt
@@ -3,3 +3,6 @@ This directory contains the software for PhET simulations.
 Sam Reid
 PhET
 2-27-2007
+
+Changes:
+(2-27-2007) Deleted: energyconservation, was deprecated version of energy skate park.
