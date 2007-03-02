a9354d1d9c70e082aaf5f097744dd48c468b152b
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-03-02 21:20:41 +0000

    redo slider controls, show current value and provide text entry

--- trunk/simulations-java/rutherford-scattering/src/edu/colorado/phet/rutherfordscattering/control/RutherfordAtomControlPanel.java
+++ trunk/simulations-java/rutherford-scattering/src/edu/colorado/phet/rutherfordscattering/control/RutherfordAtomControlPanel.java
@@ -2,6 +2,7 @@
 
 package edu.colorado.phet.rutherfordscattering.control;
 
+import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
@@ -15,9 +16,9 @@ public class RutherfordAtomControlPanel extends AbstractControlPanel {
 
     private static final double CLOCK_STEP_TO_ENERGY_MULTIPLER = 100;
     
-    private EnergyControl _energyControl;
-    private IntegerSliderControl _protonsControl;
-    private IntegerSliderControl _neutronsControl;
+    private SliderControl _clockControl;
+    private SliderControl _protonsControl;
+    private SliderControl _neutronsControl;
     
     public RutherfordAtomControlPanel( RutherfordAtomModule module ) {
         super( module );
@@ -25,54 +26,77 @@ public class RutherfordAtomControlPanel extends AbstractControlPanel {
         JLabel alphaParticlePropertiesLabel = new JLabel( SimStrings.get( "label.alphaParticleProperties" ) );
         alphaParticlePropertiesLabel.setFont( RSConstants.TITLE_FONT );
         
-        // Energy
-        int minEnergy = clockStepToEnergy( RSConstants.MIN_CLOCK_STEP );
-        int maxEnergy = clockStepToEnergy( RSConstants.MAX_CLOCK_STEP );
-        int defaultEnergy = clockStepToEnergy( RSConstants.DEFAULT_CLOCK_STEP );
-        _energyControl = new EnergyControl( minEnergy, maxEnergy, defaultEnergy );
-        _energyControl.setTitleFont( RSConstants.CONTROL_FONT );
-        _energyControl.setSliderFont( RSConstants.CONTROL_FONT );
-        _energyControl.addChangeListener( new ChangeListener() {
-            public void stateChanged( ChangeEvent event ) {
-                handleEnergyChange();
-            }
-        });
+        // Clock (Energy) control
+        {
+            double value = RSConstants.DEFAULT_CLOCK_STEP;
+            double min = RSConstants.MIN_CLOCK_STEP;
+            double max = RSConstants.MAX_CLOCK_STEP;
+            double tickSpacing = max - min;
+            int tickDecimalPlaces = 0;
+            int valueDecimalPlaces = 1;
+            String label = SimStrings.get( "label.energy" ); // labeled "Energy" !
+            String units = "";
+            int columns = 1;
+            _clockControl = new SliderControl( value, min, max, tickSpacing, tickDecimalPlaces, valueDecimalPlaces, label, units, columns );
+            _clockControl.setBorder( BorderFactory.createEtchedBorder() );
+            _clockControl.setTextFieldVisible( false );
+            _clockControl.setMinMaxLabels( SimStrings.get( "label.minEnergy" ), SimStrings.get( "label.maxEnergy" ) );
+            _clockControl.addChangeListener( new ChangeListener() {
+
+                public void stateChanged( ChangeEvent event ) {
+                    handleClockChange();
+                }
+            } );
+        }
         
         JLabel atomPropertiesLabel = new JLabel( SimStrings.get( "label.atomProperties" ) );
         atomPropertiesLabel.setFont( RSConstants.TITLE_FONT );
         
         // Number of protons
-        String labelProtons = SimStrings.get( "label.numberOfProtons" );
-        int minProtons = RSConstants.MIN_PROTONS;
-        int maxProtons = RSConstants.MAX_PROTONS;
-        int defaultProtons = RSConstants.DEFAULT_PROTONS;
-        _protonsControl = new IntegerSliderControl( labelProtons, minProtons, maxProtons, defaultProtons );
-        _protonsControl.setTitleFont( RSConstants.CONTROL_FONT );
-        _protonsControl.setSliderFont( RSConstants.CONTROL_FONT );
-        _protonsControl.addChangeListener( new ChangeListener() {
-            public void stateChanged( ChangeEvent event ) {
-                handleProtonsChange();
-            }
-        });
+        {
+            int value = RSConstants.DEFAULT_PROTONS;
+            int min = RSConstants.MIN_PROTONS;
+            int max = RSConstants.MAX_PROTONS;
+            int tickSpacing = max - min;
+            int tickDecimalPlaces = 0;
+            int valueDecimalPlaces = 0;
+            String label = SimStrings.get( "label.numberOfProtons" );
+            String units = "";
+            int columns = 3;
+            _protonsControl = new SliderControl( value, min, max, tickSpacing, tickDecimalPlaces, valueDecimalPlaces, label, units, columns );
+            _protonsControl.setBorder( BorderFactory.createEtchedBorder() );
+            _protonsControl.addChangeListener( new ChangeListener() {
+                public void stateChanged( ChangeEvent event ) {
+                    handleProtonsChange();
+                }
+            } );
+        }
         
         // Number of neutrons
-        String labelNeutrons = SimStrings.get( "label.numberOfNeutrons" );
-        int minNeutrons = RSConstants.MIN_NEUTRONS;
-        int maxNeutrons = RSConstants.MAX_NEUTRONS;
-        int defaultNeutrons = RSConstants.DEFAULT_NEUTRONS;
-        _neutronsControl = new IntegerSliderControl( labelNeutrons, minNeutrons, maxNeutrons, defaultNeutrons );
-        _neutronsControl.setTitleFont( RSConstants.CONTROL_FONT );
-        _neutronsControl.setSliderFont( RSConstants.CONTROL_FONT );
-        _neutronsControl.addChangeListener( new ChangeListener() {
-            public void stateChanged( ChangeEvent event ) {
-                handleNeutronsChange();
-            }
-        });
+        {
+
+            int value = RSConstants.DEFAULT_NEUTRONS;
+            int min = RSConstants.MIN_NEUTRONS;
+            int max = RSConstants.MAX_NEUTRONS;
+            int tickSpacing = max - min;
+            int tickDecimalPlaces = 0;
+            int valueDecimalPlaces = 0;
+            String label = SimStrings.get( "label.numberOfNeutrons" );
+            String units = "";
+            int columns = 3;
+            _neutronsControl = new SliderControl( value, min, max, tickSpacing, tickDecimalPlaces, valueDecimalPlaces, label, units, columns );
+            _neutronsControl.setBorder( BorderFactory.createEtchedBorder() );
+            _neutronsControl.addChangeListener( new ChangeListener() {
+                public void stateChanged( ChangeEvent event ) {
+                    handleNeutronsChange();
+                }
+            } );
+        }
         
         addVerticalSpace( 20 );
         addControlFullWidth( alphaParticlePropertiesLabel );
         addVerticalSpace( 20 );
-        addControlFullWidth( _energyControl );
+        addControlFullWidth( _clockControl );
         addVerticalSpace( 20 );
         addSeparator();
         addVerticalSpace( 20 );
@@ -80,29 +104,25 @@ public class RutherfordAtomControlPanel extends AbstractControlPanel {
         addVerticalSpace( 20 );
         addControlFullWidth( _protonsControl );
         addControlFullWidth( _neutronsControl );
+        addVerticalSpace( 20 );
+        addSeparator();
     }
 
-    private static double energyToClockStep( int energy ) {
-        return energy / (double) CLOCK_STEP_TO_ENERGY_MULTIPLER;
-    }
-
-    private static int clockStepToEnergy( double clockStep ) {
-        return (int) ( clockStep * CLOCK_STEP_TO_ENERGY_MULTIPLER );
-    }
-    
-    private void handleEnergyChange() {
-        int energy = _energyControl.getValue();
-        double clockStep = energyToClockStep( energy );
-        getModule().setDt( clockStep );
+    private void handleClockChange() {
+        double dt = _clockControl.getValue();
+        System.out.println( "RutherfordAtomControlPanel.handleClockChange dt=" + dt );//XXX
+        getModule().setDt( dt );
     }
     
     private void handleProtonsChange() {
-        int numberOfProtons = _protonsControl.getValue();
+        int numberOfProtons = (int) _protonsControl.getValue();
+        System.out.println( "RutherfordAtomControlPanel.handleProtonsChange numberOfProtons=" + numberOfProtons );//XXX
         //XXX
     }
-    
+
     private void handleNeutronsChange() {
-        int numberOfNeutrons = _neutronsControl.getValue();
+        int numberOfNeutrons = (int) _neutronsControl.getValue();
+        System.out.println( "RutherfordAtomControlPanel.handleNeutronsChange numberOfNeutrons=" + numberOfNeutrons );//XXX
         //XXX
     }
 }
