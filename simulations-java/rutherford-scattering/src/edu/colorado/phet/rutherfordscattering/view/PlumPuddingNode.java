604e748f2be8334845ad863cf309fc4ed340a0fd
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-03-02 22:01:09 +0000

    first attempt at randomly distributing atoms in plum pudding atom

--- simulations-java/rutherford-scattering/src/edu/colorado/phet/rutherfordscattering/view/PlumPuddingNode.java
+++ simulations-java/rutherford-scattering/src/edu/colorado/phet/rutherfordscattering/view/PlumPuddingNode.java
@@ -15,14 +15,13 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Point2D;
-import java.util.Observable;
-import java.util.Observer;
+import java.util.Random;
 
 import edu.colorado.phet.piccolo.PhetPNode;
 import edu.colorado.phet.piccolo.util.PImageFactory;
 import edu.colorado.phet.rutherfordscattering.RSConstants;
-import edu.colorado.phet.rutherfordscattering.model.AbstractHydrogenAtom;
 import edu.colorado.phet.rutherfordscattering.model.PlumPuddingModel;
+import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.nodes.PImage;
 import edu.umd.cs.piccolo.nodes.PPath;
 import edu.umd.cs.piccolo.util.PBounds;
@@ -42,6 +41,8 @@ public class PlumPuddingNode extends PhetPNode {
     
     private static final boolean SHOW_MODEL_SHAPE = false;
     
+    private static final int NUMBER_OF_ELECTRONS = 79;
+    
     //----------------------------------------------------------------------------
     // Constructors
     //----------------------------------------------------------------------------
@@ -56,26 +57,37 @@ public class PlumPuddingNode extends PhetPNode {
         setPickable( false );
         setChildrenPickable( false );
         
+        // Read the pudding image and scale it to match the atom model
         PImage puddingNode = PImageFactory.create( RSConstants.IMAGE_PLUM_PUDDING );
-        double imageRadius = Math.max( puddingNode.getWidth(), puddingNode.getHeight() );
-        double atomRadius = 2 * ModelViewTransform.transform( atom.getRadius() );
-        double imageScale = atomRadius / imageRadius;
+        double imageDiameter = Math.max( puddingNode.getWidth(), puddingNode.getHeight() );
+        double atomDiameter = 2 * ModelViewTransform.transform( atom.getRadius() );
+        double imageScale = atomDiameter / imageDiameter;
         puddingNode.scale( imageScale );
-        
+        puddingNode.setOffset( -puddingNode.getFullBounds().getWidth()/2, -puddingNode.getFullBounds().getHeight()/2 );
         addChild( puddingNode );
-
-        if ( SHOW_MODEL_SHAPE ) {
-            PPath shapeNode = new PPath();
-            double r = atom.getRadius();
-            double d = ( 2 * r );
-            shapeNode.setPathTo( new Ellipse2D.Double( -r, -r, d, d ) );
-            shapeNode.setStroke( new BasicStroke( 1f ) );
-            shapeNode.setStrokePaint( Color.GREEN );
-            addChild( shapeNode );
+      
+        // Create electrons at random locations throughout the pudding
+        Random randomDistance = new Random();
+        Random randomAngle = new Random();
+        double maxDistance = atomDiameter / 2;
+        for ( int i = 0; i < NUMBER_OF_ELECTRONS; i++ ) {
+            ElectronNode electronNode = new ElectronNode();
+            // choose a random location in polar coordinates
+            double distance = randomDistance.nextDouble() * maxDistance;
+            double angle = randomAngle.nextDouble() * ( 2 * Math.PI );
+            // convert to Cartesian coordinates
+            double x = distance * Math.cos( angle );
+            double y = distance * Math.sin( angle );
+            System.out.println( "adding electron at (" + x + "," + y + ")" );//XXX
+            electronNode.setOffset( x, y );
+            addChild( electronNode );
         }
         
-        PBounds pb = puddingNode.getFullBounds();
-        puddingNode.setOffset( -pb.getWidth()/2, -pb.getHeight()/2 );
+        // Flatten everything to an image
+        PImage imageNode = new PImage( this.toImage() );
+        imageNode.setOffset( -imageNode.getFullBounds().getWidth()/2, -imageNode.getFullBounds().getHeight()/2 );
+        removeAllChildren();
+        addChild( imageNode );
         
         Point2D atomPosition = atom.getPositionRef();
         Point2D nodePosition = ModelViewTransform.transform( atomPosition );
