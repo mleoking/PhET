74820bd87e9f09b18a625ab07e695453b60c1718
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-03-08 02:18:53 +0000

    do a deep copy of the phetcommon branch that colorvision3 requires

---
+++ trunk/simulations-java/colorvision3/phetcommon/src/edu/colorado/phet/common/view/graphics/BoundedGraphic.java
@@ -0,0 +1,20 @@
+/* Copyright 2004, University of Colorado */
+
+/*
+ * CVS Info -
+ * Filename : $Source$
+ * Branch : $Name$
+ * Modified by : $Author$
+ * Revision : $Revision$
+ * Date modified : $Date$
+ */
+package edu.colorado.phet.common.view.graphics;
+
+/**
+ * BoundedGraphic
+ *
+ * @author ?
+ * @version $Revision$
+ */
+public interface BoundedGraphic extends Graphic, Boundary {
+}
