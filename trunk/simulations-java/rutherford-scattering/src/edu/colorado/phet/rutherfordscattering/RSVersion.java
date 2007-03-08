2f04c843edc02385e47e65680b54a1c229c01f80
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-03-08 00:55:47 +0000

    version 0.00.02

--- trunk/simulations-java/rutherford-scattering/src/edu/colorado/phet/rutherfordscattering/RSVersion.java
+++ trunk/simulations-java/rutherford-scattering/src/edu/colorado/phet/rutherfordscattering/RSVersion.java
@@ -10,8 +10,8 @@ package edu.colorado.phet.rutherfordscattering;
  */
 public class RSVersion {
   
-    public static final String NUMBER = "sandbox";
-//    public static final String NUMBER = "0.00.01 (13476)";
+//    public static final String NUMBER = "sandbox";
+    public static final String NUMBER = "0.00.02 (13613)";
     
     /* This class is not intended for instantiation. */
     private RSVersion() {}
