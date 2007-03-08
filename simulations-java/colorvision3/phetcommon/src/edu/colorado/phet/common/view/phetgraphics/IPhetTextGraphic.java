f567865b4089a103ea169177ee886677c2f9b4de
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-03-08 02:18:53 +0000

    do a deep copy of the phetcommon branch that colorvision3 requires

---
+++ simulations-java/colorvision3/phetcommon/src/edu/colorado/phet/common/view/phetgraphics/IPhetTextGraphic.java
@@ -0,0 +1,24 @@
+/* Copyright 2004, University of Colorado */
+
+/*
+ * CVS Info -
+ * Filename : $Source$
+ * Branch : $Name$
+ * Modified by : $Author$
+ * Revision : $Revision$
+ * Date modified : $Date$
+ */
+package edu.colorado.phet.common.view.phetgraphics;
+
+
+/**
+ * IPhetTextGraphic
+ *
+ * @author Sam Reid
+ * @version $Revision$
+ */
+public interface IPhetTextGraphic {
+    void setPosition( int x, int y );
+
+    void setVisible( boolean visible );
+}
