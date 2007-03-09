217f8590ae23af58583f244398115f5180d5baca
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-03-09 18:50:55 +0000

    add a flag that determines whether failure in the algorithm are reported to System.err

--- trunk/simulations-java/rutherford-scattering/src/edu/colorado/phet/rutherfordscattering/model/RutherfordScattering.java
+++ trunk/simulations-java/rutherford-scattering/src/edu/colorado/phet/rutherfordscattering/model/RutherfordScattering.java
@@ -19,8 +19,8 @@ import edu.colorado.phet.rutherfordscattering.RSConstants;
  */
 public class RutherfordScattering {
 
-    // Prints debugging output
-    public static boolean DEBUG_OUTPUT_ENABLED = true;
+    // Reports algorithm failures
+    public static boolean REPORT_FAILURES = true;
     
     // Formatter, for debug output
     private static final DecimalFormat F = new DecimalFormat( "0.00" );
@@ -142,49 +142,50 @@ public class RutherfordScattering {
         }
         
         double yNew = -rNew * Math.cos( phiNew );
-        System.out.println( " y=" + y );//XXX
         yNew *= -1; // flip y sign from algorithm to model
         
         //-------------------------------------------------------------------------------
-        // detect failures in the algorithm
+        // report failures in the algorithm
         //-------------------------------------------------------------------------------
         
-        boolean error = false;
+        if ( REPORT_FAILURES ) {
+            
+            boolean error = false;
 
-        if ( !( b > 0 ) ) {
-            System.err.println( "ERROR: RutherfordScattering.moveParticle, b=" + b );
-            error = true;
-        }
+            if ( !( b > 0 ) ) {
+                System.err.println( "ERROR: RutherfordScattering.moveParticle, b=" + b );
+                error = true;
+            }
 
-        if ( !( sNew > 0 ) ) {
-            System.err.println( "ERROR: RutherfordScattering.moveParticle, newSpeed=" + sNew + ", reverting to " + s );
-            sNew = s;
-            error = true;
-        }
+            if ( !( sNew > 0 ) ) {
+                System.err.println( "ERROR: RutherfordScattering.moveParticle, newSpeed=" + sNew );
+                error = true;
+            }
 
-        // Debugging output, in coordinates relative to the atom's center
-        if ( DEBUG_OUTPUT_ENABLED && error ) {
-            System.err.println( "DEBUG: RutherfordScattering.moveParticle [" );
-            System.err.println( "  particle id=" + alphaParticle.getId() );
-            System.err.println( "  constants:" );
-            System.err.println( "    dt=" + F.format( dt ) );
-            System.err.println( "    b=" + b );
-            System.err.println( "    L=" + F.format( L ) );
-            System.err.println( "    D=" + D );
-            System.err.println( "    (x0,y0)=(" + F.format( x0 ) + "," + F.format( y0 ) + ")" );
-            System.err.println( "    s0=" + F.format( s0 ) );
-            System.err.println( "    sd=" + F.format( sd ) );
-            System.err.println( "    p=" + p );
-            System.err.println( "    pd=" + pd );
-            System.err.println( "  current state:" );
-            System.err.println( "    (x,y)=(" + F.format( x ) + "," + F.format( y ) + ")" );
-            System.err.println( "    (r,phi)=(" + F.format( r ) + "," + F.format( Math.toDegrees( phi ) ) + ")" );
-            System.err.println( "    s=" + F.format( s ) );
-            System.err.println( "  new state:" );
-            System.err.println( "    (x,y)=(" + F.format( xNew ) + "," + F.format( yNew ) + ")" );
-            System.err.println( "    (r,phi)=(" + F.format( rNew ) + "," + F.format( Math.toDegrees( phiNew ) ) + ")" );
-            System.err.println( "    s=" + sNew );
-            System.err.println( "]" );
+            // Debugging output, in coordinates relative to the atom's center
+            if ( error ) {
+                System.err.println( "DEBUG: RutherfordScattering.moveParticle [" );
+                System.err.println( "  particle id=" + alphaParticle.getId() );
+                System.err.println( "  constants:" );
+                System.err.println( "    dt=" + F.format( dt ) );
+                System.err.println( "    b=" + b );
+                System.err.println( "    L=" + F.format( L ) );
+                System.err.println( "    D=" + D );
+                System.err.println( "    (x0,y0)=(" + F.format( x0 ) + "," + F.format( y0 ) + ")" );
+                System.err.println( "    s0=" + F.format( s0 ) );
+                System.err.println( "    sd=" + F.format( sd ) );
+                System.err.println( "    p=" + p );
+                System.err.println( "    pd=" + pd );
+                System.err.println( "  current state:" );
+                System.err.println( "    (x,y)=(" + F.format( x ) + "," + F.format( y ) + ")" );
+                System.err.println( "    (r,phi)=(" + F.format( r ) + "," + F.format( Math.toDegrees( phi ) ) + ")" );
+                System.err.println( "    s=" + F.format( s ) );
+                System.err.println( "  new state:" );
+                System.err.println( "    (x,y)=(" + F.format( xNew ) + "," + F.format( yNew ) + ")" );
+                System.err.println( "    (r,phi)=(" + F.format( rNew ) + "," + F.format( Math.toDegrees( phiNew ) ) + ")" );
+                System.err.println( "    s=" + sNew );
+                System.err.println( "]" );
+            }
         }
 
         //-------------------------------------------------------------------------------
