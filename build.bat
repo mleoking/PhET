d70839f4a07fef3f0a6b8871733a32d986fe0fa5
jdegoes <jdegoes@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-03-16 22:58:44 +0000

    Ant Master Build File: Common targets implemented and build file refactored. There are still a few targets left to be implemented and more extensive testing remaining.

--- build.bat
+++ build.bat
@@ -8,7 +8,7 @@ set ANT_HOME=%ROOT_DIR%\build-tools\apache-ant-1.7.0
 set PATH=%ANT_HOME%\bin;%PATH%
 
 if "%JAVA_HOME%"=="" (echo The environment variable JAVA_HOME must be set to the location of a valid JDK.) else (
-    if "%1"=="" (ant.bat) else (if "%2"=="" (ant.bat %1) else (ant.bat %1 -Dsim.name=%2))
+    if "%1"=="" (ant.bat) else (if "%2"=="" (ant.bat %1) else (ant.bat %1 -Dname.sim=%2))
 )
 
 cd "%OLD_DIR%
