0af9a61b5742735b9b77635863aac29b6377cdaa
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-03-19 23:11:47 +0000

    test application to isolate drag problem with ruler and laser apparatus

--- simulations-java/optical-tweezers/test-src/TestDragProblem.java
+++ simulations-java/optical-tweezers/test-src/TestDragProblem.java
@@ -4,7 +4,6 @@ import java.awt.Dimension;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.awt.geom.Dimension2D;
-import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 
 import javax.swing.*;
@@ -16,35 +15,63 @@ import edu.colorado.phet.piccolo.event.BoundedDragHandler;
 import edu.colorado.phet.piccolo.event.CursorHandler;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.nodes.PPath;
-import edu.umd.cs.piccolo.nodes.PText;
 import edu.umd.cs.piccolo.util.PDimension;
 
-
-public class TestDynamicResize extends JFrame {
+/**
+ * TestDragProblem isolates a problem I'm having in the Optical Tweezers simulations
+ * when using a BoundedDragHandler and PhetPCanvas.addWorldChild.
+ * <p>
+ * This example has a horizontal bar that is similar to the ruler in Optical Tweezers.
+ * When the canvas size is changed, the bar dynamically resizes so that its width is
+ * always 75% of the canvas width. The bar can be dragged vertically, but it's 
+ * horizontal position is locked. Horizontal position can only be adjusted using
+ * the slider at the bottom of the frame.
+ * <p>
+ * A position (x,y) determines the location of the bar's upper left corner.
+ * For x>=0, dragging works fine. But for x<0, it becomes possible to move
+ * the bar horizontally.  As -x becomes larger, the amount of horizontal 
+ * movement increases.
+ * <p>
+ * This problem seems to be unique to using PhetPCanvas.addWorldChild.
+ * The problem does not occur when adding nodes using PhetPCanvas.getLayer().addChild;
+ * to demonstrate, set USE_WORLD_CHILD=false.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
+public class TestDragProblem extends JFrame {
+    
+    public static final boolean USE_WORLD_CHILD = true;
 
     private static final Dimension FRAME_SIZE = new Dimension( 1024, 768 ); // screen coordinates
     private static final Dimension WORLD_SIZE = new Dimension( 750, 750 ); // world coordinates
-    private static final Point2D HORIZONTAL_BAR_POSITION = new Point2D.Double( 400, 100 ); // world coordinates
+    private static final double MIN_X_POSITION = -700; // world coordinates
+    private static final double MAX_X_POSITION = +700; // world coordinates
+    private static final double INITIAL_X_POSITION = 100; // world coordinates
     
     public static void main( String[] args ) {
-        TestDynamicResize frame = new TestDynamicResize();
+        TestDragProblem frame = new TestDragProblem();
         frame.show();
     }
     
-    public TestDynamicResize() {
-        super( "TestRulerResize" );
+    public TestDragProblem() {
+        super( "TestDragProblem" );
         
         /*--------------------------- Canvas ---------------------------*/
         
-        PPath dragBoundsNode = new PPath();
-        dragBoundsNode.setStrokePaint( Color.RED );
+        PPath dragBoundsNode = new PPath(); // shape will be determined by HorizontalBarNode
+        dragBoundsNode.setStrokePaint( Color.RED ); // so we can see the bounds
         
-        final HorizontalBarNode horizontalBarNode = new HorizontalBarNode( dragBoundsNode );
-        horizontalBarNode.setPosition( HORIZONTAL_BAR_POSITION );
+        final HorizontalBarNode horizontalBarNode = new HorizontalBarNode( INITIAL_X_POSITION, dragBoundsNode );
 
         final PhetPCanvas canvas = new PhetPCanvas( WORLD_SIZE );
-        canvas.addWorldChild( horizontalBarNode );
-        canvas.addWorldChild( dragBoundsNode );
+        if ( USE_WORLD_CHILD ) {
+            canvas.addWorldChild( horizontalBarNode );
+            canvas.addWorldChild( dragBoundsNode );
+        }
+        else {
+            canvas.getLayer().addChild( horizontalBarNode );
+            canvas.getLayer().addChild( dragBoundsNode );
+        }
         
         // when the canvas size changes, update the horizontal bar
         canvas.addComponentListener( new ComponentAdapter() {
@@ -58,27 +85,26 @@ public class TestDynamicResize extends JFrame {
         
         /*--------------------------- Control Panel ---------------------------*/
         
-        final JLabel positionLabel = new JLabel( String.valueOf( HORIZONTAL_BAR_POSITION.getX() ) );
+        final JLabel xPositionLabel = new JLabel( String.valueOf( INITIAL_X_POSITION ) );
         
-        final JSlider positionSlider = new JSlider();
-        positionSlider.setMinimum( 0 );
-        positionSlider.setMaximum( 750 );
-        positionSlider.setValue( (int) HORIZONTAL_BAR_POSITION.getX() );
-        positionSlider.addChangeListener( new ChangeListener() {
+        final JSlider xPositionSlider = new JSlider();
+        xPositionSlider.setMinimum( (int) MIN_X_POSITION );
+        xPositionSlider.setMaximum( (int) MAX_X_POSITION);
+        xPositionSlider.setValue( (int) INITIAL_X_POSITION );
+        xPositionSlider.addChangeListener( new ChangeListener() {
             // when the slider is changed, update the horizontal bar's position
             public void stateChanged( ChangeEvent event ) {
-                double x = positionSlider.getValue();
-                double y = horizontalBarNode.getFullBounds().getY();
-                positionLabel.setText( String.valueOf( x ) );
-                horizontalBarNode.setPosition( new Point2D.Double( x, y ) );
+                double x = xPositionSlider.getValue();
+                xPositionLabel.setText( String.valueOf( x ) );
+                horizontalBarNode.setHorizontalPosition( x );
             }
         } );
         
         JPanel controlPanel = new JPanel();
         controlPanel.setBorder( BorderFactory.createEmptyBorder( 15, 15, 15, 15 ) );
         controlPanel.setLayout( new BoxLayout( controlPanel, BoxLayout.X_AXIS ) );
-        controlPanel.add( positionSlider );
-        controlPanel.add( positionLabel );
+        controlPanel.add( xPositionSlider );
+        controlPanel.add( xPositionLabel );
         
         /*--------------------------- Frame ---------------------------*/
         
@@ -94,8 +120,7 @@ public class TestDynamicResize extends JFrame {
     
     /*
      * HorizontalBarNode is a horizontal bar with a dynamic width and fixed height.
-     * The word "CENTER" is printed at the center of the bar.
-     * The width is adjusted to match the width of the canvas.
+     * The width is adjusted dynamically so that it is always 75% of the canvas width.
      * The bar can be vertically dragged, but dragging cannot change it's horizontal position.
      * Horizontal position can be changed only via the slider in the control panel.
      */
@@ -103,22 +128,22 @@ public class TestDynamicResize extends JFrame {
         
         private static final double HEIGHT = 20;
 
-        private Point2D _position;
+        private double _xPosition;
         private Dimension2D _worldSize;
         private PPath _dragBoundsNode;
         
-        public HorizontalBarNode( PPath dragBoundsNode ) {
+        public HorizontalBarNode( double x, PPath dragBoundsNode ) {
             super();
-            _position = new Point2D.Double();
             _worldSize = new PDimension();
             _dragBoundsNode = dragBoundsNode;
             addInputEventListener( new BoundedDragHandler( this, _dragBoundsNode ) );
             addInputEventListener( new CursorHandler() );
+            setHorizontalPosition( x );
         }
         
-        public void setPosition( Point2D position ) {
-            _position.setLocation( position );
-            updatePosition();
+        public void setHorizontalPosition( double x ) {
+            _xPosition = x;
+            updateOffset();
         }
         
         public void setWorldSize( Dimension2D worldSize ) {
@@ -126,43 +151,30 @@ public class TestDynamicResize extends JFrame {
             updateWidth();
         }
         
-        /* Adjusts the horizontal offset so that the horizontal center is at x=0 */
-        private void updatePosition() {
-            final double x = _position.getX() - ( getFullBounds().getWidth() / 2 );
-            final double y = getOffset().getY();
-            setOffset( x, y );
+        private void updateOffset() {
+            setOffset( _xPosition, getOffset().getY() );
             updateDragBounds();
         }
         
-        /* Adjusts the drag bound for the position and world size */
         private void updateDragBounds() {
-            Rectangle2D dragBounds = new Rectangle2D.Double( getFullBounds().getX(), 0, getFullBounds().getWidth(), _worldSize.getHeight() );
-            _dragBoundsNode.setPathTo( dragBounds );
+            Rectangle2D rect = new Rectangle2D.Double( getFullBounds().getX(), 0, getFullBounds().getWidth(), _worldSize.getHeight() );
+            _dragBoundsNode.setPathTo( rect );
+            _dragBoundsNode.setOffset( 0, 0 );
         }
         
-        /* Adjusts the width to match the world width */
         private void updateWidth() {
             removeAllChildren();
             
             // gray rectangle that is as wide as the canvas
-            Rectangle2D rect = new Rectangle2D.Double( 0, 0, _worldSize.getWidth(), HEIGHT );
+            Rectangle2D rect = new Rectangle2D.Double( 0, 0, 0.75 * _worldSize.getWidth(), HEIGHT );
             PPath pathNode = new PPath( rect );
             pathNode.setStroke( new BasicStroke( 1f ) );
             pathNode.setStrokePaint( Color.BLACK );
             pathNode.setPaint( Color.GRAY );
             pathNode.setOffset( 0, 0 );
-            
-            // center "CENTER" in the rectangle
-            PText textNode = new PText( "CENTER" );
-            textNode.setTextPaint( Color.BLACK );
-            double x = ( pathNode.getFullBounds().getWidth() / 2 ) - ( textNode.getFullBounds().getWidth() / 2 );
-            double y = ( pathNode.getFullBounds().getHeight() / 2 ) - ( textNode.getFullBounds().getHeight() / 2 );
-            textNode.setOffset( x, y );
-            
             addChild( pathNode );
-            addChild( textNode );
             
-            updatePosition();
+            updateOffset();
         }
     }
     
