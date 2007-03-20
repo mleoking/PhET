f976964a9ce90b8eb947fbae11c231af01a2e8cf
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-03-20 16:46:19 +0000

    consolidate 5 copies of ColorChooserFactory into phetcommon

--- trunk/simulations-java/fourier/src/edu/colorado/phet/fourier/control/HarmonicColorsDialog.java
+++ trunk/simulations-java/fourier/src/edu/colorado/phet/fourier/control/HarmonicColorsDialog.java
@@ -22,6 +22,7 @@ import javax.swing.*;
 import javax.swing.event.MouseInputAdapter;
 
 import edu.colorado.phet.common.application.PhetApplication;
+import edu.colorado.phet.common.view.util.ColorChooserFactory;
 import edu.colorado.phet.common.view.util.EasyGridBagLayout;
 import edu.colorado.phet.common.view.util.SimStrings;
 import edu.colorado.phet.fourier.view.HarmonicColors;
