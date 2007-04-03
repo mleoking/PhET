5e1c756d28ea3c89a64d7221b7f81593bd9fef39
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-04-03 18:09:24 +0000

    cleanup balloons project

--- simulations-java/balloons/src/edu/colorado/phet/balloon/HelpPanel.java
+++ simulations-java/balloons/src/edu/colorado/phet/balloon/HelpPanel.java
@@ -8,7 +8,7 @@
  * Revision : $Revision$
  * Date modified : $Date$
  */
-package edu.colorado.phet.common;
+package edu.colorado.phet.balloon;
 
 import edu.colorado.phet.common.view.util.SimStrings;
 
