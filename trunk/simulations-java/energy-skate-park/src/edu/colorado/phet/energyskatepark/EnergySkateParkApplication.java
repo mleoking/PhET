3d77abc5e4d006a50964fb7fb4308aa7bbee61c4
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-04-04 00:29:35 +0000

    added docs

--- trunk/simulations-java/energy-skate-park/src/edu/colorado/phet/energyskatepark/EnergySkateParkApplication.java
+++ trunk/simulations-java/energy-skate-park/src/edu/colorado/phet/energyskatepark/EnergySkateParkApplication.java
@@ -49,7 +49,7 @@ import java.awt.event.KeyEvent;
 import java.io.IOException;
 
 public class EnergySkateParkApplication extends PhetApplication {
-    private static final String VERSION = "1.05.16";
+    private static final String VERSION = "1.05.16 <14170>";
     private EnergySkateParkModule module;
     public static double SIMULATION_TIME_DT = 0.03;
 
