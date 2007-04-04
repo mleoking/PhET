44b96a388b92143f1c661de1426e8a9d0fccaa67
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-04-04 21:05:51 +0000

    moved test code

--- simulations-java/energy-skate-park/tests-src/edu/colorado/phet/energyskatepark/test/transforms/WorldNode.java
+++ simulations-java/energy-skate-park/tests-src/edu/colorado/phet/energyskatepark/test/transforms/WorldNode.java
@@ -1,5 +1,5 @@
 /* Copyright 2007, University of Colorado */
-package edu.colorado.phet.energyskatepark.test;
+package edu.colorado.phet.energyskatepark.test.transforms;
 
 import edu.umd.cs.piccolo.PCanvas;
 import edu.umd.cs.piccolo.PNode;
