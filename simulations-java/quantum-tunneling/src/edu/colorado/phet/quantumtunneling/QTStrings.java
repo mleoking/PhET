1e34eb29499d421e72927b4a4481b3db81df2706
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-04-06 16:02:40 +0000

    add QTStrings.getVersion

--- simulations-java/quantum-tunneling/src/edu/colorado/phet/quantumtunneling/QTStrings.java
+++ simulations-java/quantum-tunneling/src/edu/colorado/phet/quantumtunneling/QTStrings.java
@@ -23,4 +23,8 @@ public class QTStrings {
     public static final int getInt( String name, int defaultValue ) {
         return CONFIG.getInt( name, defaultValue );
     }
+    
+    public static final String getVersion() {
+        return CONFIG.getVersion().toString();
+    }
 }
