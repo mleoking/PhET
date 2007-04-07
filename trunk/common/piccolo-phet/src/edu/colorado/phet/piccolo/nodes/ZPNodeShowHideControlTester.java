07e6f08d3a11e0516557b49d3246a5f0cd8523b2
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-04-07 20:46:36 +0000

    add tester for PNodeShowHideControl

---
+++ trunk/common/piccolo-phet/src/edu/colorado/phet/piccolo/nodes/ZPNodeShowHideControlTester.java
@@ -0,0 +1,46 @@
+/* Copyright 2007, University of Colorado */
+
+package edu.colorado.phet.piccolo.nodes;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.geom.Rectangle2D;
+
+import javax.swing.JFrame;
+
+import edu.colorado.phet.piccolo.PhetPCanvas;
+import edu.umd.cs.piccolo.nodes.PPath;
+import edu.umd.cs.piccolo.nodes.PText;
+import edu.umd.cs.piccolox.nodes.PComposite;
+
+
+public class ZPNodeShowHideControlTester {
+
+    public static void main( String[] args ) {
+        
+        PhetPCanvas canvas = new PhetPCanvas();
+        
+        PPath pathNode = new PPath( new Rectangle2D.Double( 0, 0, 200, 200 ) );
+        pathNode.setPaint( null );
+        pathNode.setStrokePaint( Color.RED );
+        pathNode.setStroke( new BasicStroke( 3f ) );
+        
+        PText textNode = new PText( "Blah blah blah" );
+        
+        PComposite parentNode = new PComposite();
+        parentNode.addChild( pathNode );
+        parentNode.addChild( textNode );
+        textNode.setOffset( 0, 100 );
+        parentNode.setOffset( 100, 100 );
+        canvas.getLayer().addChild( parentNode );
+        
+        PNodeShowHideControl showHideControl = new PNodeShowHideControl( parentNode, "Press me" );
+        
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        frame.setContentPane( canvas );
+        frame.setSize( 800, 600 );
+        frame.setVisible( true );
+    }
+
+}
