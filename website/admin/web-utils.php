3ed43a5df98900f596940f1cc2212203121b5357
jdegoes <jdegoes@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-04-11 18:06:21 +0000

    website: More bug fixes to PHP code.

--- website/admin/web-utils.php
+++ website/admin/web-utils.php
@@ -1,9 +1,18 @@
 <?php
 
-function generate_check_status($item_num, $checked_item_num) {
-    if ($checked_item_num == null && $item_num == "0" || $item_num == $checked_item_num) return "checked";
+    function generate_check_status($item_num, $checked_item_num) {
+        if ($checked_item_num == null && $item_num == "0" || $item_num == $checked_item_num) return "checked";
     
-    return " ";
-}
+        return " ";
+    }
+    
+    function url_exists($url) {
+        if (is_array(get_headers($url))) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 
 ?>
