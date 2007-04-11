b37aa6e265b87852b2161097ac590568dfd89226
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-04-11 02:40:02 +0000

    posting 1.05.18

--- simulations-java/energy-skate-park/src/edu/colorado/phet/energyskatepark/EnergySkateParkApplication.java
+++ simulations-java/energy-skate-park/src/edu/colorado/phet/energyskatepark/EnergySkateParkApplication.java
@@ -48,7 +48,7 @@ import java.awt.event.KeyEvent;
 import java.io.IOException;
 
 public class EnergySkateParkApplication extends PhetApplication {
-    private static final String VERSION = "1.05.17 <14176>";
+    private static final String VERSION = "1.05.18 <14306>";
     private EnergySkateParkModule module;
     public static double SIMULATION_TIME_DT = 0.03;
 
