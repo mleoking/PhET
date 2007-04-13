eb7f3c3f452dbd44d4145c2bc49399a6d7905130
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-04-13 21:57:32 +0000

    initial checkin of java-based build process (prototype candidate for replacing pure ant build process)

--- trunk/simulations-java/build-tools/phet-build/src/edu/colorado/phet/build/PhetBuildTask.java
+++ trunk/simulations-java/build-tools/phet-build/src/edu/colorado/phet/build/PhetBuildTask.java
@@ -1,17 +1,34 @@
 package edu.colorado.phet.build;
 
 import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Main;
 import org.apache.tools.ant.Task;
+import org.apache.tools.ant.taskdefs.Echo;
+
+import java.util.Properties;
 
 public class PhetBuildTask extends Task {
-    private String msg;
+    private String projectName;
 
     // The method executing the task
     public void execute() throws BuildException {
-        System.out.println(msg);
+        System.out.println( "Executing: " + projectName );
+
+        Echo echo = new Echo();
+        echo.setProject( getProject() );
+        echo.setLocation( getLocation() );
+        echo.setOwningTarget( getOwningTarget() );
+        echo.setTaskName( getTaskName() );
+        echo.setMessage( "Started building: " + projectName );
+        echo.init();
+        echo.execute();
     }
 
-    public void setProject(String projectName){
+    public void setProject( String projectName ) {
+        this.projectName = projectName;
+    }
+
+    public static void main( String[] args ) {
         
     }
 }
