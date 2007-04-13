5539e55c69933672178a87f3ed772255e7b626ad
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-04-13 22:24:05 +0000

    added ability to launch ant task from java main for debugging

--- simulations-java/build-tools/phet-build/src/edu/colorado/phet/build/PhetBuildTask.java
+++ simulations-java/build-tools/phet-build/src/edu/colorado/phet/build/PhetBuildTask.java
@@ -1,34 +1,65 @@
 package edu.colorado.phet.build;
 
 import org.apache.tools.ant.BuildException;
-import org.apache.tools.ant.Main;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.ProjectHelper;
 import org.apache.tools.ant.Task;
 import org.apache.tools.ant.taskdefs.Echo;
 
-import java.util.Properties;
+import java.io.File;
+import java.util.ArrayList;
 
 public class PhetBuildTask extends Task {
     private String projectName;
 
     // The method executing the task
     public void execute() throws BuildException {
-        System.out.println( "Executing: " + projectName );
+        output( "Executing: " + projectName );
+        buildProject();
+    }
 
+    private void buildProject() {
+        output( "in build project" );
+    }
+
+    public void output( String string ) {
+        System.out.println( string );
+        echo( string );
+    }
+
+    private void echo( String message ) {
         Echo echo = new Echo();
         echo.setProject( getProject() );
         echo.setLocation( getLocation() );
         echo.setOwningTarget( getOwningTarget() );
         echo.setTaskName( getTaskName() );
-        echo.setMessage( "Started building: " + projectName );
+        echo.setMessage( message );
         echo.init();
-        echo.execute();
+        try {
+            echo.execute();
+        }
+        catch( NullPointerException np ) {
+
+        }
     }
 
     public void setProject( String projectName ) {
         this.projectName = projectName;
     }
 
+    public static class PhetProject {
+        ArrayList sourcePaths = new ArrayList();
+        ArrayList libPaths = new ArrayList();
+        ArrayList dataPaths = new ArrayList();
+        ArrayList projectDependencies = new ArrayList();
+    }
+
     public static void main( String[] args ) {
-        
+        File buildFile = new File( "build.xml" );
+        Project p = new Project();
+        p.setUserProperty( "ant.file", buildFile.getAbsolutePath() );
+        p.init();
+        ProjectHelper.configureProject( p, buildFile );
+        p.executeTarget( p.getDefaultTarget() );
     }
 }
