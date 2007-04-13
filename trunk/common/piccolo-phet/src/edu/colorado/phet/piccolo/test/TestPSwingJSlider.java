f4339dfc38efd1b29976d5d8d91f7dced7005407
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-04-13 00:28:36 +0000

    moved piccolo-phet tests-src/ to src/../test

---
+++ trunk/common/piccolo-phet/src/edu/colorado/phet/piccolo/test/TestPSwingJSlider.java
@@ -0,0 +1,37 @@
+package edu.colorado.phet.piccolo.test;
+
+import edu.umd.cs.piccolox.pswing.PSwing;
+import edu.umd.cs.piccolox.pswing.PSwingCanvas;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Author: Sam Reid
+ * Mar 20, 2007, 9:12:27 PM
+ */
+public class TestPSwingJSlider {
+    private JFrame frame;
+
+    public TestPSwingJSlider() {
+        frame = new JFrame( "TestPSwingJSlider" );
+
+        PSwingCanvas pCanvas = new PSwingCanvas();
+        frame.setContentPane( pCanvas );
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        frame.setSize( 600, 600 );
+
+        JComponent c2 = new JScrollBar();
+        c2.setPreferredSize(new Dimension( c2.getPreferredSize().width,400));
+        pCanvas.getLayer().addChild( new PSwing(c2 ) );
+        pCanvas.setPanEventHandler( null );
+    }
+
+    public static void main( String[] args ) {
+        new TestPSwingJSlider().start();
+    }
+
+    private void start() {
+        frame.setVisible( true );
+    }
+}
