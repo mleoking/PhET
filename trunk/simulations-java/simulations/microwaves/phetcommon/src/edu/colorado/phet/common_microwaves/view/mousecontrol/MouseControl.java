f09d58a581beb95b9952324a7c76bc3bbf54b56a
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-04-13 07:04:08 +0000

    moved common lib for microwaves

---
+++ trunk/simulations-java/simulations/microwaves/phetcommon/src/edu/colorado/phet/common_microwaves/view/mousecontrol/MouseControl.java
@@ -0,0 +1,25 @@
+/*Copyright, Sam Reid, 2003.*/
+package edu.colorado.phet.common_microwaves.view.mousecontrol;
+
+import java.awt.event.MouseEvent;
+import java.awt.geom.Point2D;
+
+/**
+ * User: Sam Reid
+ * Date: Oct 9, 2003
+ * Time: 12:44:00 AM
+ * Copyright (c) Oct 9, 2003 by Sam Reid
+ */
+public interface MouseControl {
+    public boolean canHandleMousePress(MouseEvent event, Point2D.Double modelLoc);
+
+    public void mouseDragged(MouseEvent event, Point2D.Double modelLoc);
+
+    public void mousePressed(MouseEvent event, Point2D.Double modelLoc);
+
+    public void mouseReleased(MouseEvent event, Point2D.Double modelLoc);
+
+    public void mouseEntered(MouseEvent event, Point2D.Double modelLoc);
+
+    public void mouseExited(MouseEvent event, Point2D.Double modelLoc);
+}
