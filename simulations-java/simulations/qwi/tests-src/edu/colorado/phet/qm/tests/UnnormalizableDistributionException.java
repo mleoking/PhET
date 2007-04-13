7f8a167d833f124e1e1974ffb231f13b78542939
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-04-13 07:53:25 +0000

    cleanup qwi

--- simulations-java/simulations/qwi/tests-src/edu/colorado/phet/qm/tests/UnnormalizableDistributionException.java
+++ simulations-java/simulations/qwi/tests-src/edu/colorado/phet/qm/tests/UnnormalizableDistributionException.java
@@ -3,7 +3,7 @@ package edu.colorado.phet.qm.tests;
 import java.io.Serializable;
 import java.util.*;
 
-class UnnormalizableDistributionException extends Exception {
+public class UnnormalizableDistributionException extends Exception {
     public UnnormalizableDistributionException( String s ) {
         super( s );
     }
