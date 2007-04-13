7ac0be0ee15506f851a70b9fed5987baea21e6bb
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-04-13 04:22:17 +0000

    standardize balloons

--- simulations-java/simulations/balloons/src/edu/colorado/phet/balloon/BalloonHelpPainter.java
+++ simulations-java/simulations/balloons/src/edu/colorado/phet/balloon/BalloonHelpPainter.java
@@ -13,22 +13,22 @@ import java.awt.*;
  */
 
 public class BalloonHelpPainter implements Painter {
-    private BalloonApplet balloonApplet;
+    private BalloonsApplication balloonsApplication;
 
-    public BalloonHelpPainter( BalloonApplet balloonApplet ) {
-        this.balloonApplet = balloonApplet;
+    public BalloonHelpPainter( BalloonsApplication balloonsApplication ) {
+        this.balloonsApplication = balloonsApplication;
     }
 
     public void paint( Graphics2D g ) {
         g.setFont( new Font( "Lucida Sans", Font.BOLD, 14 ) );
         g.setColor( Color.blue );
-        g.drawString( "Rub the balloon", balloonApplet.getSweaterMaxX(), 50 );
+        g.drawString( "Rub the balloon", balloonsApplication.getSweaterMaxX(), 50 );
         double height = g.getFont().getStringBounds( "Rub", g.getFontRenderContext() ).getHeight();
-        g.drawString( "on the sweater", balloonApplet.getSweaterMaxX(), (int)( 50 + height ) );
+        g.drawString( "on the sweater", balloonsApplication.getSweaterMaxX(), (int)( 50 + height ) );
 
         double w = g.getFont().getStringBounds( "Bring the balloon", g.getFontRenderContext() ).getWidth();
-        int x = (int)( balloonApplet.getWallX() - w - 10 );
-        int y = (int)( balloonApplet.getWallHeight() - height * 5 );
+        int x = (int)( balloonsApplication.getWallX() - w - 10 );
+        int y = (int)( balloonsApplication.getWallHeight() - height * 5 );
         g.drawString( "Bring the balloon", x, y );
         g.drawString( "near the wall", x, (int)( y + height ) );
     }
