64dfa2d1756b827ef0bf2b1315956461f10a896f
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-04-17 04:36:12 +0000

    added initial support for new PhetAboutDialog in Balloons and Sound

--- trunk/simulations-java/simulations/sound/phetcommon/src/edu/colorado/phet/common_sound/view/components/menu/HelpMenu.java
+++ trunk/simulations-java/simulations/sound/phetcommon/src/edu/colorado/phet/common_sound/view/components/menu/HelpMenu.java
@@ -10,15 +10,16 @@
  */
 package edu.colorado.phet.common_sound.view.components.menu;
 
+import edu.colorado.phet.common_sound.application.*;
+import edu.colorado.phet.common_sound.util.VersionUtils;
+import edu.colorado.phet.common_sound.view.PhetFrame;
+import edu.colorado.phet.common_sound.view.util.SimStrings;
+import edu.colorado.phet.common.application.PhetAboutDialog;
+
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
-
-import javax.swing.*;
-
-import edu.colorado.phet.common_sound.application.*;
-import edu.colorado.phet.common_sound.util.VersionUtils;
-import edu.colorado.phet.common_sound.view.util.SimStrings;
 
 /**
  * HelpMenu
@@ -29,15 +30,23 @@ import edu.colorado.phet.common_sound.view.util.SimStrings;
 public class HelpMenu extends JMenu implements ModuleObserver {
     private ImageIcon icon;
     private JMenuItem onscreenHelp;
+    private PhetFrame phetFrame;
+    private String title;
+    private String description;
+    private String version;
 
     public HelpMenu( PhetApplication application ) {
-        this( application.getModuleManager(), application.getApplicationModel().getName(),
+        this( application.getPhetFrame(), application.getModuleManager(), application.getApplicationModel().getName(),
               application.getApplicationModel().getDescription(), application.getApplicationModel().getVersion() );
     }
 
-    public HelpMenu( final ModuleManager moduleManager, final String title,
+    public HelpMenu( PhetFrame phetFrame, final ModuleManager moduleManager, final String title,
                      String description, String version ) {
         super( SimStrings.get( "Common.HelpMenu.Title" ) );
+        this.phetFrame = phetFrame;
+        this.title = title;
+        this.description = description;
+        this.version = version;
         this.setMnemonic( SimStrings.get( "Common.HelpMenu.TitleMnemonic" ).charAt( 0 ) );
         moduleManager.addModuleObserver( this );
 
@@ -52,7 +61,7 @@ public class HelpMenu extends JMenu implements ModuleObserver {
         } );
         onscreenHelp.setEnabled( moduleManager.getActiveModule() != null && moduleManager.getActiveModule().hasHelp() );
         add( onscreenHelp );
-        
+
         //----------------------------------------------------------------------
         // "MegaHelp" menu item
         final JMenuItem megaHelpItem = new JMenuItem( SimStrings.get( "Common.HelpMenu.MegaHelp" ) );
@@ -85,28 +94,41 @@ public class HelpMenu extends JMenu implements ModuleObserver {
         //----------------------------------------------------------------------
         // Separator
         addSeparator();
-        
+
         //----------------------------------------------------------------------
         // "About" menu item
         final JMenuItem about = new JMenuItem( SimStrings.get( "Common.HelpMenu.About" ) );
 
-        String javaVersion = SimStrings.get( "Common.HelpMenu.JavaVersion" ) + ": " + System.getProperty( "java.version" );
+
         about.setMnemonic( SimStrings.get( "Common.HelpMenu.AboutMnemonic" ).charAt( 0 ) );
-        String message = title + "\n" + description + "\n" + SimStrings.get( "Common.HelpMenu.VersionLabel" ) + ": " + version + "\n\n" + javaVersion + "\n";
-        final String msg = message;
+
         about.addActionListener( new ActionListener() {
             public void actionPerformed( ActionEvent e ) {
-                JOptionPane.showMessageDialog( about, msg, SimStrings.get( "Common.HelpMenu.AboutTitle" ) + " " + title, JOptionPane.INFORMATION_MESSAGE, icon );
+//                showAboutDialogOrig();
+                showAboutDialogPhet();
             }
         } );
         add( about );
+    }
+
+    private void showAboutDialogPhet() {
+        String credits = "";
+        PhetAboutDialog phetAboutDialog = new PhetAboutDialog( phetFrame, new PhetAboutDialog.SimpleDialogConfig( title, description, version, credits ) );
+        phetAboutDialog.show( );
+    }
+
+    private void showAboutDialogOrig() {
+        String javaVersion = SimStrings.get( "Common.HelpMenu.JavaVersion" ) + ": " + System.getProperty( "java.version" );
+        String message = title + "\n" + description + "\n" + SimStrings.get( "Common.HelpMenu.VersionLabel" ) + ": " + version + "\n\n" + javaVersion + "\n";
+        final String msg = message;
+        JOptionPane.showMessageDialog( this, msg, SimStrings.get( "Common.HelpMenu.AboutTitle" ) + " " + title, JOptionPane.INFORMATION_MESSAGE, icon );
     }
 
     /**
      * Sets the state of the Help menu item.
      * This is used to keep the menubar's Help menu item
      * in sync with the control panel's Help button.
-     * 
+     *
      * @param selected
      */
     public void setHelpSelected( boolean selected ) {
@@ -153,7 +175,7 @@ public class HelpMenu extends JMenu implements ModuleObserver {
 
     public void activeModuleChanged( ModuleEvent event ) {
         Module module = event.getModule();
-        if ( module != null ) {
+        if( module != null ) {
             onscreenHelp.setEnabled( module.hasHelp() );
             onscreenHelp.setSelected( module.isHelpEnabled() );
         }
