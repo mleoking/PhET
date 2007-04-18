79ce3df2ba3dc8de96e37b36f2c86164b751b3d9
jdegoes <jdegoes@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-04-18 20:25:11 +0000

    website: Updated control panel to point to new code.

--- trunk/website/admin/choose-sim.php
+++ trunk/website/admin/choose-sim.php
@@ -4,20 +4,19 @@
 	
 	//start drop down menu & form
 	
-	print ("<form action=updateexistingsim.php method=post><select name=sim_id>");
+	print ("<form action=\"edit-sim.php\" method=post><select name=\"sim_id\">");
 	
-    $sim_id              = $_REQUEST['sim_id'];
     $select_simulations_st = "SELECT * FROM `simulation` ORDER BY `sim_name` ASC ";
     $simulation_table      = mysql_query($select_simulations_st);
 	
     while ($sim = mysql_fetch_row($simulation_table)) {      
-        $sim_id    = $sim[0];
+        $sim_id   = $sim[0];
         $simtitle = $sim[1];
 	
         //print drop down menu
-        print "<OPTION VALUE=\"$sim_id\">$simtitle";
+        print "<option value=\"$sim_id\">$simtitle";
     }
 
     // close drop down menu and form
-    print "</select><input type=submit value=\"EDIT\"></form>";
+    print "</select><input type=\"submit\" value=\"edit\"></form>";
 ?>
