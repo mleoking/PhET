5c9c65e5d289538534c8e57f643a9ca7c3213a4d
jdegoes <jdegoes@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-04-19 20:42:23 +0000

    phet-build: Deleted old phet proguard object; deprecated action methods of phet project and made them delegate to new phet project manager class.

---
+++ simulations-java/build-tools/phet-build/src/edu/colorado/phet/build/PhetProjectManager.java
@@ -0,0 +1,94 @@
+/* Copyright 2007, University of Colorado */
+package edu.colorado.phet.build;
+
+import org.apache.tools.ant.taskdefs.*;
+import org.apache.tools.ant.types.Path;
+import org.apache.tools.ant.types.FileSet;
+
+import java.io.File;
+
+import edu.colorado.phet.build.proguard.ProguardCommand;
+
+
+public class PhetProjectManager {
+    private final PhetProject project;
+    private final AntTaskRunner antTaskRunner;
+
+    public PhetProjectManager( PhetProject project, AntTaskRunner taskRunner ) {
+        this.project = project;
+        this.antTaskRunner = taskRunner;
+    }
+
+    public void compile( File[] src, File[] classpath, File dst ) {
+        output( "compiling " + project.getName() );
+        Javac javac = new Javac();
+        javac.setSource( "1.4" );
+        javac.setSrcdir( new Path( antTaskRunner.getProject(), toString( src ) ) );
+        javac.setDestdir( project.getClassesDirectory() );
+        javac.setClasspath( new Path( antTaskRunner.getProject(), toString( classpath ) ) );
+        antTaskRunner.runTask( javac );
+        output( "Finished compiling " + project.getName() + "." );
+    }
+
+    public void jar() {
+        Jar jar = new Jar();
+        File[] dataDirectories = project.getAllDataDirectories();
+        for( int i = 0; i < dataDirectories.length; i++ ) {
+            FileSet set = new FileSet();
+            set.setDir( dataDirectories[i] );
+            jar.addFileset( set );
+        }
+        jar.setBasedir( project.getClassesDirectory() );
+        jar.setJarfile( project.getJarFile() );
+        Manifest manifest = new Manifest();
+        try {
+            Manifest.Attribute attribute = new Manifest.Attribute();
+            attribute.setName( "Main-Class" );
+            attribute.setValue( project.getMainClass() );
+            manifest.addConfiguredAttribute( attribute );
+            jar.addConfiguredManifest( manifest );
+        }
+        catch( ManifestException e ) {
+            e.printStackTrace();
+        }
+        
+        antTaskRunner.runTask( jar );
+    }
+
+    public void buildAll( boolean shrink ) {
+        compile( project.getAllSourceRoots(), project.getAllJarFiles(), project.getClassesDirectory() );
+        jar();
+
+        PhetProguardConfigBuilder builder = new PhetProguardConfigBuilder();
+
+        builder.setPhetProject( project );
+        builder.setShrink( shrink );
+
+        try {
+            new ProguardCommand( builder.build(), antTaskRunner ).execute();
+        }
+        catch( Exception e ) {
+            // TODO
+            e.printStackTrace();
+        }
+    }
+
+
+    private void output( String s ) {
+        Echo echo = new Echo();
+        echo.setMessage( s );
+        antTaskRunner.runTask( echo );
+    }
+
+    private String toString( File[] files ) {
+        String string = "";
+        for( int i = 0; i < files.length; i++ ) {
+            File file = files[i];
+            string += file.getAbsolutePath();
+            if( i < files.length - 1 ) {
+                string += " : ";
+            }
+        }
+        return string;
+    }
+}
