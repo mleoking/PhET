5e4200808c16d46446a8c8bec13c8484d1b69145
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-04-20 00:16:38 +0000

    javadoc

--- simulations-java/simulations/optical-tweezers/src/edu/colorado/phet/opticaltweezers/control/slider/LinearSliderStrategy.java
+++ simulations-java/simulations/optical-tweezers/src/edu/colorado/phet/opticaltweezers/control/slider/LinearSliderStrategy.java
@@ -10,15 +10,35 @@ package edu.colorado.phet.opticaltweezers.control.slider;
  */
 public class LinearSliderStrategy extends AbstractSliderStrategy {
     
+    /**
+     * Constructor.
+     * 
+     * @param sliderMin
+     * @param sliderMax
+     * @param modelMin
+     * @param modelMax
+     */
     public LinearSliderStrategy( int sliderMin, int sliderMax, double modelMin, double modelMax ) {
         super( sliderMin, sliderMax, modelMin, modelMax );
     }
 
+    /**
+     * Converts from slider to model coordinates.
+     * 
+     * @param sliderValue slider value
+     * @return model value
+     */
     public double sliderToModel( int sliderValue ) {
         double ratio = ( sliderValue - getSliderMin() ) / (double)( getSliderMax() - getSliderMin() );
         return getModelMin() + ( ratio * ( getModelMax() - getModelMin() ) );
     }
 
+    /**
+     * Converts from model to slider coordinates.
+     * 
+     * @param modelValue model value
+     * @return slider value
+     */
     public int modelToSlider( double modelValue ) {
         double ratio = ( modelValue - getModelMin() ) / ( getModelMax() - getModelMin() );
         return getSliderMin() + (int)( ratio * ( getSliderMax() - getSliderMin() ) );
