192bcd258ce66d1fa7b262588055c34abb898bd8
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-04-23 17:29:40 +0000

    update data directories

--- simulations-java/simulations/energy-skate-park/src/edu/colorado/phet/energyskatepark/SkaterCharacterSet.java
+++ simulations-java/simulations/energy-skate-park/src/edu/colorado/phet/energyskatepark/SkaterCharacterSet.java
@@ -12,12 +12,7 @@ public class SkaterCharacterSet {
     public SkaterCharacterSet() {
         addSkaterCharacter( new SkaterCharacter( "images/skater3.png", "PhET Skater", 75.0, 1.8 ) );
         addSkaterCharacter( new SkaterCharacter( "images/starskater2.gif", "Star Skater", 60.0, 1.5 ) );
-//        addSkaterCharacter( new SkaterCharacter( "images/ferrari-side2.gif", "Ferarri", 2000.0, 1.0 ) );
-//        addSkaterCharacter( new SkaterCharacter( "images/Lunar-Rover.gif", "Lunar Rover", 210.0, 1.2 ) );
-//        addSkaterCharacter( new SkaterCharacter( "images/Mars-Lander.gif", "Mars Lander", 180.0, 1.3 ) );
-//        addSkaterCharacter( new SkaterCharacter( "images/motorcycle.gif", "Motorcycle", 227.0, 1.0 ) );
         addSkaterCharacter( new SkaterCharacter( "images/bulldog.gif", "Bulldog", 20.0, 1.125 ) );
-//        addSkaterCharacter( new SkaterCharacter( "images/bugboard.gif", "Bug", 0.2, 0.75 ) );
         addSkaterCharacter( new SkaterCharacter( "images/bluebug.gif", "Bug", 0.2, 0.75 ) );
         addSkaterCharacter( new SkaterCharacter( "images/red-ball.png", "Ball", 5.0, 0.3, 2.0 ) );
     }
