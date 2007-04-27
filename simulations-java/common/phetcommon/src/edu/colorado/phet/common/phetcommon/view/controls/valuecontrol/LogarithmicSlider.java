327c57d83901e6da5a54a39a2f29ae9295df63a4
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-04-27 17:17:36 +0000

    add javadoc note about the challenges of setting tick marks & labels for a logarithmic control when the underlying JSlider only supports linear spacing of ticks

--- simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/view/controls/valuecontrol/LogarithmicSlider.java
+++ simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/view/controls/valuecontrol/LogarithmicSlider.java
@@ -4,6 +4,12 @@ package edu.colorado.phet.common.phetcommon.view.controls.valuecontrol;
 
 /**
  * LogarithmicSlider is a JSlider with that provides a logarithmic mapping to model values.
+ * <p>
+ * Setting tick marks for this control can be challenging and (unfortunately)
+ * requires an understanding of the underlying slider implementation.
+ * The underlying slider is based on JSlider, which only supports 
+ * linearly-spaced tick marks.  So you are limited to using tick marks
+ * and labels that are linearly spaced.
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
