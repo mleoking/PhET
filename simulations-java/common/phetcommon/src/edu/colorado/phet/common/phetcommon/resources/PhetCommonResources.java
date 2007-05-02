6d19d7394f34438c5d5997fceb1fe12a79250cb8
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-05-02 18:03:38 +0000

    - move CloseButtonNode back to optical-tweezers, too trivial for piccolo-phet - add constants to PhetCommonResources for image resource names, use throughout common code - add StandardIconButton, base class for all standard iconic PhET buttons (eg, close, minimize, maximize)

--- simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/resources/PhetCommonResources.java
+++ simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/resources/PhetCommonResources.java
@@ -7,6 +7,17 @@ package edu.colorado.phet.common.phetcommon.resources;
  */
 public class PhetCommonResources {
     
+    // Symbolic names for image resources
+    public static final String IMAGE_CLOSE_BUTTON = "buttons/closeButton.png";
+    public static final String IMAGE_MINIMIZE_BUTTON = "buttons/minimizeButton.png";
+    public static final String IMAGE_MAXIMIZE_BUTTON = "buttons/maximizeButton.png";
+    public static final String IMAGE_FAST_FORWARD = "clock/FastForward24.gif";
+    public static final String IMAGE_PAUSE = "clock/Pause24.gif";
+    public static final String IMAGE_PLAY = "clock/Play24.gif";
+    public static final String IMAGE_REWIND = "clock/Rewind.gif";
+    public static final String IMAGE_STEP_FORWARD = "clock/StepForward24.gif";
+    public static final String IMAGE_STOP = "clock/Stop24.gif";
+    
     private static PhetResources INSTANCE = PhetResources.forProject( "phetcommon" );
 
     /* not intended for instantiation */
