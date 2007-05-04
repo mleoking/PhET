eb75bd2c095f4fb313f5abc89387d941d306a36f
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-05-04 04:31:34 +0000

    Switched gravity slider to use LinearValueControl, changed looks slightly.

--- simulations-java/simulations/energy-skate-park/src/edu/colorado/phet/energyskatepark/view/GravitySlider.java
+++ simulations-java/simulations/energy-skate-park/src/edu/colorado/phet/energyskatepark/view/GravitySlider.java
@@ -3,8 +3,7 @@ package edu.colorado.phet.energyskatepark.view;
 
 import edu.colorado.phet.common.phetcommon.model.clock.ClockAdapter;
 import edu.colorado.phet.common.phetcommon.model.clock.ClockEvent;
-import edu.colorado.phet.common.phetcommon.util.DefaultDecimalFormat;
-import edu.colorado.phet.common.phetcommon.view.ModelSlider;
+import edu.colorado.phet.common.phetcommon.view.controls.valuecontrol.LinearValueControl;
 import edu.colorado.phet.energyskatepark.EnergySkateParkModule;
 import edu.colorado.phet.energyskatepark.EnergySkateParkStrings;
 import edu.colorado.phet.energyskatepark.model.EnergySkateParkModel;
@@ -18,30 +17,21 @@ import java.util.Hashtable;
  * User: Sam Reid
  * Date: Nov 8, 2005
  * Time: 11:11:41 AM
- *
  */
 
-public class GravitySlider extends ModelSlider {
+//public class GravitySlider extends ModelSlider {
+public class GravitySlider extends LinearValueControl {
 
     public GravitySlider( final EnergySkateParkModule module ) {
-//        super( "Gravity", "N/kg", 0, -EnergyConservationModel.G_JUPITER * 1.2, -EnergyConservationModel.G_EARTH, new DecimalFormat( "0.00" ), new DecimalFormat( "0" ) );
-        super( EnergySkateParkStrings.getString( "gravity" ), EnergySkateParkStrings.getString( "n.kg" ), 0, 30, -EnergySkateParkModel.G_EARTH, new DefaultDecimalFormat( "0.00" ) );
+        super( 0, 30, EnergySkateParkStrings.getString( "gravity" ), "0.00", EnergySkateParkStrings.getString( "n.kg" ) );
         Hashtable modelTicks = new Hashtable();
         modelTicks.put( new Double( 0 ), new JLabel( EnergySkateParkStrings.getString( "space" ) ) );
         modelTicks.put( new Double( -EnergySkateParkModel.G_EARTH ), new JLabel( EnergySkateParkStrings.getString( "earth" ) ) );
-//        modelTicks.put( new Double( -EnergyConservationModel.G_MOON ), new JLabel( "Moon" ) );
         modelTicks.put( new Double( -EnergySkateParkModel.G_JUPITER ), new JLabel( EnergySkateParkStrings.getString( "jupiter" ) ) );
-//        setModelTicks( new double[]{-EnergyConservationModel.G_MOON, -EnergyConservationModel.G_EARTH, -EnergyConservationModel.G_JUPITER} );
-//        setModelTicks( new double[]{0, -EnergyConservationModel.G_EARTH, -EnergyConservationModel.G_JUPITER} );
-//        setModelTicks( new double[]{0, 5,10,15,20,25,30} );
         setMajorTickSpacing( 10 );
-        setNumMinorTicksPerMajorTick( 2 );
-//        setNumMinorTicks( 0);
-        setModelLabels( modelTicks );
+        setMinorTickSpacing( 10 / 2.0 );
+        setTickLabels( modelTicks );
 
-//        setModelTicks( new double[]{0, 10, 20, 30, 40} );
-//        setNumMajorTicks( 4 );
-//        setNumMinorTicksPerMajorTick( 2 );
         addChangeListener( new ChangeListener() {
             public void stateChanged( ChangeEvent e ) {
                 module.getEnergySkateParkModel().setGravity( -getValue() );
@@ -54,17 +44,8 @@ public class GravitySlider extends ModelSlider {
 
             }
         } );
-        module.getClock().addClockListener( new ClockAdapter() {
-            public void clockTicked( ClockEvent clockEvent ) {
-                if( getUnitsReadout().getBorder() != null ) {
-                    getUnitsReadout().setBorder( null );
-                }
-            }
-        } );
-
-        getUnitsReadout().setBackground( EC3LookAndFeel.backgroundColor );
-        getUnitsReadout().setBorder( null );
         setFocusable( false );
         getSlider().setFocusable( false );
+        setBorder( BorderFactory.createEtchedBorder() );
     }
 }
