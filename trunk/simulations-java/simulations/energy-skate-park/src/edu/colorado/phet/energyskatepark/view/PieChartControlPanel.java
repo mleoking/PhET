d55549609aa3693897534ba753cf93fb1c94f584
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-05-05 18:44:35 +0000

    Added "Clear Heat" button in pie chart control panel. Made "Clear Heat" buttons become disabled when the system's thermal energy is zero.

--- trunk/simulations-java/simulations/energy-skate-park/src/edu/colorado/phet/energyskatepark/view/PieChartControlPanel.java
+++ trunk/simulations-java/simulations/energy-skate-park/src/edu/colorado/phet/energyskatepark/view/PieChartControlPanel.java
@@ -9,20 +9,20 @@ import edu.colorado.phet.energyskatepark.model.EnergySkateParkModel;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.*;
 
 /**
  * User: Sam Reid
  * Date: Oct 31, 2005
  * Time: 12:56:48 AM
- *
  */
 
 public class PieChartControlPanel extends VerticalLayoutPanel {
     private EnergySkateParkModule module;
     private EnergySkateParkControlPanel energySkateParkControlPanel;
-    //    private JCheckBox ignoreThermal;
     private JCheckBox showThermal;
-    public JCheckBox showPieChartCheckBox;
+    private JCheckBox showPieChartCheckBox;
+    private ClearHeatButton clearHeat;
 
     public PieChartControlPanel( final EnergySkateParkModule module, EnergySkateParkControlPanel energySkateParkControlPanel ) {
         this.module = module;
@@ -31,7 +31,6 @@ public class PieChartControlPanel extends VerticalLayoutPanel {
         showPieChartCheckBox = new JCheckBox( EnergySkateParkStrings.getString( "show.pie.chart" ), module.isPieChartVisible() );
         showPieChartCheckBox.addActionListener( new ActionListener() {
             public void actionPerformed( ActionEvent e ) {
-//                ignoreThermal.setEnabled( pieChart.isSelected() );
                 showThermal.setEnabled( showPieChartCheckBox.isSelected() );
                 module.setPieChartVisible( showPieChartCheckBox.isSelected() );
             }
@@ -45,6 +44,11 @@ public class PieChartControlPanel extends VerticalLayoutPanel {
             }
         } );
         add( showThermal );
+
+        clearHeat = new ClearHeatButton( module );
+        setFillNone();
+        setAnchor( GridBagConstraints.WEST );
+        add( clearHeat );
         showThermal.setEnabled( showPieChartCheckBox.isSelected() );
     }
 
