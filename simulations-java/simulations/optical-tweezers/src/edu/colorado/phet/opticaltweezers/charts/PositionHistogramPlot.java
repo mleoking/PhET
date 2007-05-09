10b4a56623e0a2040cd2da15db4915134ff6ffc7
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-05-09 23:50:09 +0000

    5/9/07 - integrate PhetHistogramDataset into PositionHistogramPlot

--- simulations-java/simulations/optical-tweezers/src/edu/colorado/phet/opticaltweezers/charts/PositionHistogramPlot.java
+++ simulations-java/simulations/optical-tweezers/src/edu/colorado/phet/opticaltweezers/charts/PositionHistogramPlot.java
@@ -6,13 +6,11 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Stroke;
-import java.util.Random;
 
 import org.jfree.chart.axis.AxisLocation;
 import org.jfree.chart.axis.NumberAxis;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.renderer.xy.XYBarRenderer;
-import org.jfree.data.Range;
 import org.jfree.data.statistics.HistogramDataset;
 import org.jfree.data.statistics.HistogramType;
 
@@ -21,7 +19,7 @@ import edu.colorado.phet.opticaltweezers.OTConstants;
 
 public class PositionHistogramPlot extends XYPlot {
 
-    private static final String SERIES_KEY = "positionFrequencySeries";
+    private static final String SERIES_KEY = "position";
     private static final Color BACKGROUND_COLOR = new Color( 0, 0, 0, 0 ); // transparent
     private static final Color BAR_FILL_COLOR = Color.YELLOW;
     private static final Color BAR_OUTLINE_COLOR = Color.BLACK;
@@ -29,18 +27,39 @@ public class PositionHistogramPlot extends XYPlot {
     private static final Font AXIS_LABEL_FONT = new Font( OTConstants.DEFAULT_FONT_NAME, Font.PLAIN, 14 );
     private static final Stroke AXIS_STROKE = new BasicStroke( 1f );
     private static final Color AXIS_COLOR = Color.BLACK;
-    private static final int NUMBER_OF_HISTOGRAM_BINS = 100;
     
+    //XXX this stuff should be in constructor
+    private static final double MIN_POSITION = 0; // nm
+    private static final double MAX_POSITION = 10000; // nm
+    private static final double BIN_WIDTH = 20; // nm
+    
+    private PhetHistogramDataset _dataset;
+    private int _seriesIndex;
     private NumberAxis _xAxis, _yAxis;
-    private double[] _positions;
     
     public PositionHistogramPlot() {
         super();
+        
+        // dataset
+        _dataset = new PhetHistogramDataset();
+        setDataset( _dataset );
+        
+        // series
+        int numberOfBins = (int) ( ( MAX_POSITION - MIN_POSITION ) / BIN_WIDTH );
+        _seriesIndex = _dataset.addSeries( SERIES_KEY, numberOfBins, MIN_POSITION, MAX_POSITION );
+        
+        // renderer
+        XYBarRenderer renderer = new XYBarRenderer();
+        renderer.setPaint( BAR_FILL_COLOR );
+        renderer.setOutlinePaint( BAR_OUTLINE_COLOR );
+        renderer.setDrawBarOutline( true );
+        setRenderer( renderer );
         
         // axis labels
         String positonLabel = "";
         String frequencyLabel = "";
         
+        // x-axis configuration
         _xAxis = new NumberAxis();
         _xAxis.setLabel( positonLabel );
         _xAxis.setLabelFont( AXIS_LABEL_FONT );
@@ -49,6 +68,7 @@ public class PositionHistogramPlot extends XYPlot {
         _xAxis.setAxisLineStroke( AXIS_STROKE );
         _xAxis.setAxisLinePaint( AXIS_COLOR );
         
+        // y-axis configuration
         _yAxis = new NumberAxis();
         _yAxis.setLabel( frequencyLabel );
         _yAxis.setLabelFont( AXIS_LABEL_FONT );
@@ -56,7 +76,9 @@ public class PositionHistogramPlot extends XYPlot {
         _yAxis.setTickMarksVisible( false );
         _yAxis.setAxisLineStroke( AXIS_STROKE );
         _yAxis.setAxisLinePaint( AXIS_COLOR );
+        // If we don't set a range for the y-axis, the data will appear to be normalized.
         
+        // plot configuration
         setRangeAxisLocation( AxisLocation.BOTTOM_OR_LEFT );
         setBackgroundPaint( BACKGROUND_COLOR );
         setDomainGridlinesVisible( false );
@@ -65,49 +87,19 @@ public class PositionHistogramPlot extends XYPlot {
         setRangeGridlinePaint( GRIDLINES_COLOR );
         setDomainAxis( _xAxis );
         setRangeAxis( _yAxis );
-        
-        // frequency will be normalized, so set range to 1
-        _yAxis.setRange( new Range( 0, 1 ) );
+//        setDomainZeroBaselineVisible( false ); // 1.0.5 feature
+//        setRangeZeroBaselineVisible( false ); // 1.0.5 feature
     }
 
     public void setPositionRange( double minPosition, double maxPosition ) {
         _xAxis.setRange( minPosition, maxPosition );
-        setPositions( _positions );
     }
     
     public void addPosition( double position ) {
-        double[] newPositions = null;
-        if ( _positions == null ) {
-            newPositions = new double[1];
-            newPositions[0] = position;
-        }
-        else {
-            newPositions = new double[ _positions.length + 1 ];
-            System.arraycopy( _positions, 0, newPositions, 0, _positions.length );
-            newPositions[ newPositions.length - 1 ] = position;
-        }
-        setPositions( newPositions );
+        _dataset.addObservation( _seriesIndex, position );
     }
 
     public void clear() {
-        setPositions( null );
-    }
-    
-    private void setPositions( double[] positions ) {
-        
-        _positions = positions;
-        
-        HistogramDataset dataset = new HistogramDataset();
-        dataset.setType( HistogramType.RELATIVE_FREQUENCY );
-        if ( positions != null ) {
-            dataset.addSeries( SERIES_KEY, positions, NUMBER_OF_HISTOGRAM_BINS );
-        }
-        setDataset( dataset );
-        
-        XYBarRenderer renderer = new XYBarRenderer();
-        renderer.setPaint( BAR_FILL_COLOR );
-        renderer.setOutlinePaint( BAR_OUTLINE_COLOR );
-        renderer.setDrawBarOutline( true );
-        setRenderer( renderer );
+        _dataset.clearObservations( _seriesIndex );
     }
 }
