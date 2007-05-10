fe1af948073da5f3f1f51a53702e66a7c225cfd6
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-05-10 04:42:44 +0000

    demonstration of pdf output

--- simulations-java/simulations/cck/src/edu/colorado/phet/cck/controls/OptionsMenu.java
+++ simulations-java/simulations/cck/src/edu/colorado/phet/cck/controls/OptionsMenu.java
@@ -6,19 +6,35 @@ import edu.colorado.phet.common.phetcommon.view.util.SimStrings;
 
 import javax.swing.*;
 
+//import com.lowagie.text.DocumentException;
+
 /**
  * User: Sam Reid
  * Date: Jul 11, 2006
  * Time: 1:05:36 AM
- *
  */
 
 public class OptionsMenu extends JMenu {
-    public OptionsMenu( PhetApplication application, ICCKModule cck ) {
+    public OptionsMenu( PhetApplication application, final ICCKModule cck ) {
         super( SimStrings.getInstance().getString( "OptionsMenu.Title" ) );
         setMnemonic( SimStrings.getInstance().getString( "OptionsMenu.TitleMnemonic" ).charAt( 0 ) );
 //        cck.setFrame( application.getApplicationView().getPhetFrame() );
         add( new BackgroundColorMenuItem( application, cck ) );
         add( new ToolboxColorMenuItem( application, cck ) );
+//        JMenuItem item = new JMenuItem( "Screenshot (PDF)" );
+//        item.addActionListener( new ActionListener() {
+//            public void actionPerformed( ActionEvent e ) {
+//                try {
+//                    PDFUtil.exportPDF( new File( "C:\\Users\\Sam\\Desktop\\phet-pdf_out.pdf"),cck.getSimulationPanel() );
+//                }
+//                catch( DocumentException e1 ) {
+//                    e1.printStackTrace();
+//                }
+//                catch( FileNotFoundException e1 ) {
+//                    e1.printStackTrace();
+//                }
+//            }
+//        } );
+//        add( item );
     }
 }
