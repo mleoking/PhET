892c8a8055eb583559cea5be8d4f8f362ceb5583
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-05-11 03:40:58 +0000

    cleanup tests & startup

--- simulations-java/simulations/rotation/src/edu/colorado/phet/rotation/RotationApplication.java
+++ simulations-java/simulations/rotation/src/edu/colorado/phet/rotation/RotationApplication.java
@@ -1,16 +1,11 @@
 package edu.colorado.phet.rotation;
 
-import edu.colorado.phet.common.jfreechartphet.test.TestDynamicJFreeChartNode;
-import edu.colorado.phet.common.jfreechartphet.test.TestDynamicJFreeChartNodeTree;
 import edu.colorado.phet.common.phetcommon.application.PhetApplication;
+import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
 import edu.colorado.phet.common.phetcommon.view.util.FrameSetup;
-import edu.colorado.phet.rotation.tests.*;
-import edu.colorado.phet.rotation.tests.combined.TestCombinedModelPlot2;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -23,94 +18,19 @@ import java.net.URL;
  */
 
 public class RotationApplication extends PhetApplication {
-    public static final String TITLE = "Rotational Motion";
-    public static final String DESCRIPTION = "Rotational Motion Simulation";
-    private JMenu testMenu;
     private RotationModule rotationModule;
 
     public RotationApplication( String[] args ) {
-        super( args, TITLE, DESCRIPTION, readVersion(), createFrameSetup() );
+        super( new RotationApplicationConfig( args ) );
         rotationModule = new RotationModule();
         addModule( rotationModule );
 
-        addTests();
+        getPhetFrame().addMenu( new RotationTestMenu() );
     }
 
-    private void addTests() {
-        testMenu = new JMenu( "Tests" );
-        testMenu.setMnemonic( 't' );
-        getPhetFrame().addMenu( testMenu );
-        addTest( "Dynamic Plot", new Runnable() {
-            public void run() {
-                TestDynamicJFreeChartNode.main( new String[0] );
-            }
-        } );
-        addTest( "Dynamic Plot 2", new Runnable() {
-            public void run() {
-                TestDynamicJFreeChartNodeTree.main( new String[0] );
-            }
-        } );
-        addTest( "Platform Graphics", new Runnable() {
-            public void run() {
-                TestPlatformNode.main( new String[0] );
-            }
-        } );
-        addTest( "Graphs", new Runnable() {
-            public void run() {
-                TestGraphs.main( new String[0] );
-            }
-        } );
-        addTest( "Combined Model Plot", new Runnable() {
-            public void run() {
-                TestCombinedModelPlot2.main( new String[0] );
-            }
-        } );
-        addTest( "Graph Control", new Runnable() {
-            public void run() {
-                TestControlGraph.main( new String[0] );
-            }
-        } );
-        addTest( "Graph Selection", new Runnable() {
-            public void run() {
-                TestGraphSelectionControl.main( new String[0] );
-            }
-        } );
-        addTest( "Graph View", new Runnable() {
-            public void run() {
-                TestGraphSelectionView.main( new String[0] );
-            }
-        } );
-        addTest( "Vector Control", new Runnable() {
-            public void run() {
-                TestShowVectorsControl.main( new String[0] );
-            }
-        } );
-        addTest( "Symbol Key", new Runnable() {
-            public void run() {
-                TestSymbolKey.main( new String[0] );
-            }
-        } );
-    }
-
-    private void addTest( String name, final Runnable runnable ) {
-        JMenuItem item = new JMenuItem( name );
-        item.addActionListener( new ActionListener() {
-            public void actionPerformed( ActionEvent e ) {
-                runnable.run();
-            }
-        } );
-        testMenu.add( item );
-    }
-
-    private static String readVersion() {
-        URL url = Thread.currentThread().getContextClassLoader().getResource( "sim.version" );
-        try {
-            BufferedReader br = new BufferedReader( new InputStreamReader( url.openStream() ) );
-            return br.readLine();
-        }
-        catch( IOException e ) {
-            e.printStackTrace();
-            return "Version Not Found";
+    static class RotationApplicationConfig extends PhetApplicationConfig {
+        public RotationApplicationConfig( String[] commandLineArgs ) {
+            super( commandLineArgs, createFrameSetup(), RotationResources.getInstance() );
         }
     }
 
---
+++ simulations-java/simulations/rotation/src/edu/colorado/phet/rotation/RotationTestMenu.java
@@ -0,0 +1,83 @@
+package edu.colorado.phet.rotation;
+
+import edu.colorado.phet.common.jfreechartphet.test.TestDynamicJFreeChartNode;
+import edu.colorado.phet.common.jfreechartphet.test.TestDynamicJFreeChartNodeTree;
+import edu.colorado.phet.rotation.tests.*;
+import edu.colorado.phet.rotation.tests.combined.TestCombinedModelPlot2;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Author: Sam Reid
+ * May 10, 2007, 10:34:39 PM
+ */
+public class RotationTestMenu extends JMenu {
+    public RotationTestMenu() {
+        super( "Tests" );
+        setMnemonic( 't' );
+
+        addTest( "Dynamic Plot", new Runnable() {
+            public void run() {
+                TestDynamicJFreeChartNode.main( new String[0] );
+            }
+        } );
+        addTest( "Dynamic Plot 2", new Runnable() {
+            public void run() {
+                TestDynamicJFreeChartNodeTree.main( new String[0] );
+            }
+        } );
+        addTest( "Platform Graphics", new Runnable() {
+            public void run() {
+                TestPlatformNode.main( new String[0] );
+            }
+        } );
+        addTest( "Graphs", new Runnable() {
+            public void run() {
+                TestGraphs.main( new String[0] );
+            }
+        } );
+        addTest( "Combined Model Plot", new Runnable() {
+            public void run() {
+                TestCombinedModelPlot2.main( new String[0] );
+            }
+        } );
+        addTest( "Graph Control", new Runnable() {
+            public void run() {
+                TestControlGraph.main( new String[0] );
+            }
+        } );
+        addTest( "Graph Selection", new Runnable() {
+            public void run() {
+                TestGraphSelectionControl.main( new String[0] );
+            }
+        } );
+        addTest( "Graph View", new Runnable() {
+            public void run() {
+                TestGraphSelectionView.main( new String[0] );
+            }
+        } );
+        addTest( "Vector Control", new Runnable() {
+            public void run() {
+                TestShowVectorsControl.main( new String[0] );
+            }
+        } );
+        addTest( "Symbol Key", new Runnable() {
+            public void run() {
+                TestSymbolKey.main( new String[0] );
+            }
+        } );
+    }
+
+    private void addTest( String name, final Runnable runnable ) {
+        JMenuItem item = new JMenuItem( name );
+        item.addActionListener( new ActionListener() {
+            public void actionPerformed( ActionEvent e ) {
+                runnable.run();
+            }
+        } );
+        add( item );
+    }
+}
+
