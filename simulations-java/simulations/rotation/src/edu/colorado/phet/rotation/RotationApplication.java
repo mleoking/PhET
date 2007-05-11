1d81e711e5f8a056961775cb7273a7153a4aabe8
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-05-11 03:42:05 +0000

    cleanup tests & startup

--- simulations-java/simulations/rotation/src/edu/colorado/phet/rotation/RotationApplication.java
+++ simulations-java/simulations/rotation/src/edu/colorado/phet/rotation/RotationApplication.java
@@ -6,10 +6,6 @@ import edu.colorado.phet.common.phetcommon.view.util.FrameSetup;
 
 import javax.swing.*;
 import java.awt.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
 
 /**
  * User: Sam Reid
@@ -21,25 +17,20 @@ public class RotationApplication extends PhetApplication {
     private RotationModule rotationModule;
 
     public RotationApplication( String[] args ) {
-        super( new RotationApplicationConfig( args ) );
+        super( new PhetApplicationConfig( args, createFrameSetup(), RotationResources.getInstance() ) );
         rotationModule = new RotationModule();
         addModule( rotationModule );
 
         getPhetFrame().addMenu( new RotationTestMenu() );
     }
 
-    static class RotationApplicationConfig extends PhetApplicationConfig {
-        public RotationApplicationConfig( String[] commandLineArgs ) {
-            super( commandLineArgs, createFrameSetup(), RotationResources.getInstance() );
-        }
-    }
-
     private static FrameSetup createFrameSetup() {
-        if( Toolkit.getDefaultToolkit().getScreenSize().height <= 768 ) {
-            return new FrameSetup.MaxExtent( new FrameSetup.TopCenter( Toolkit.getDefaultToolkit().getScreenSize().width, 700 ) );
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        if( screenSize.height <= 768 ) {
+            return new FrameSetup.MaxExtent( new FrameSetup.TopCenter( screenSize.width, 700 ) );
         }
         else {
-            return new FrameSetup.TopCenter( Toolkit.getDefaultToolkit().getScreenSize().width, Toolkit.getDefaultToolkit().getScreenSize().height - 100 );
+            return new FrameSetup.TopCenter( screenSize.width, screenSize.height - 100 );
         }
     }
 
