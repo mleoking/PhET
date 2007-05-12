d1c1ec16fd15a924d6c136905d81c880f30eb679
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-05-12 06:59:48 +0000

    added batik test

--- simulations-java/simulations/rotation/src/edu/colorado/phet/rotation/util/TestSVG.java
+++ simulations-java/simulations/rotation/src/edu/colorado/phet/rotation/util/TestSVG.java
@@ -1,13 +1,13 @@
-//package edu.colorado.phet.rotation.util;
-//
-//import com.elementengine.xml.svg.svgImage;
-//
-///**
-// * Author: Sam Reid
-// * May 11, 2007, 2:53:08 PM
-// */
-//public class TestSVG {
-//    public static void main( String[] args ) {
-//        svgImage.main(new String[]{"C:\\Users\\Sam\\Desktop\\svgout.svg"} );
-//    }
-//}
+package edu.colorado.phet.rotation.util;
+
+import com.elementengine.xml.svg.svgImage;
+
+/**
+ * Author: Sam Reid
+ * May 11, 2007, 2:53:08 PM
+ */
+public class TestSVG {
+    public static void main( String[] args ) {
+        svgImage.main(new String[]{"C:\\Users\\Sam\\Desktop\\svgout.svg"} );
+    }
+}
