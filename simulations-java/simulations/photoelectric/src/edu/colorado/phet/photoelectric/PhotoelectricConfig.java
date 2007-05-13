f72bdb4eca5ed73646aed0fff27190f58e028679
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-05-13 04:29:24 +0000

    prepare to publish

--- simulations-java/simulations/photoelectric/src/edu/colorado/phet/photoelectric/PhotoelectricConfig.java
+++ simulations-java/simulations/photoelectric/src/edu/colorado/phet/photoelectric/PhotoelectricConfig.java
@@ -26,7 +26,7 @@ public class PhotoelectricConfig {
     public static final String PROJECT_NAME = "photoelectric";
     
     // Version
-    public static final String VERSION = "1.05.00, ";
+    public static final String VERSION = "1.05.00, ?";
     
     public static final String LOCALIZATION_RESOURCE_NAME = PROJECT_NAME + "/localization/" + PROJECT_NAME + "-strings";
 
