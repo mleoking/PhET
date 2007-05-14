ddf8b6520a1d401bdcb04ddf6c0ba113a7e1a756
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-05-14 18:38:34 +0000

    get clock.png from phetcommon

--- trunk/simulations-java/simulations/hydrogen-atom/src/edu/colorado/phet/hydrogenatom/HAConstants.java
+++ trunk/simulations-java/simulations/hydrogen-atom/src/edu/colorado/phet/hydrogenatom/HAConstants.java
@@ -135,7 +135,6 @@ public class HAConstants {
     public static final String IMAGE_BILLIARD_BALL_BUTTON = "billiardBallButton.png";
     public static final String IMAGE_BOHR_BUTTON = "bohrButton.png";
     public static final String IMAGE_CAMERA = "camera.png";
-    public static final String IMAGE_CLOCK = "clock.png";
     public static final String IMAGE_CLOSE_BUTTON = "closeButton.png";
     public static final String IMAGE_DEBROGLIE_BUTTON = "deBroglieButton.png";
     public static final String IMAGE_GUN = "gun.png";
