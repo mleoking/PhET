250bffb30e9df39d3ceaaa67f1bb8247fce2cdf0
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-05-17 13:32:17 +0000

    added clean tasks

--- trunk/simulations-java/build.bat
+++ trunk/simulations-java/build.bat
@@ -8,7 +8,7 @@ set ANT_OPTS=-Xmx640m
 set PATH=%ANT_HOME%\bin;%PATH%
 
 if "%JAVA_HOME%"=="" (echo The environment variable JAVA_HOME must be set to the location of a valid JDK.) else (
-    if "%1"=="" (ant.bat) else (if "%2"=="" (ant.bat %1) else (ant.bat %1 -Dname.sim=%2))
+    if "%1"=="" (ant.bat) else (if "%2"=="" (ant.bat %1) else (ant.bat %1 -Dsim.name=%2))
 )
 
 cd "%OLD_DIR%
