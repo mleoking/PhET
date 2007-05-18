e5c2813ac3071544088920df5ba89e7824b98483
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-05-18 21:28:17 +0000

    initial support for locales

---
+++ simulations-java/build-tools/phet-build/src/edu/colorado/phet/build/PhetListLocalesTask.java
@@ -0,0 +1,17 @@
+package edu.colorado.phet.build;
+
+/**
+ * Constructs an iterable list of simulations for use in ant-contrib.
+ */
+public class PhetListLocalesTask extends AbstractPhetBuildTask {
+    private String property = "sim.locales";
+
+    protected void executeImpl( PhetProject phetProject ) throws Exception {
+        String flavorsList = PhetBuildUtils.convertArrayToList( phetProject.getLocales() );
+        getProject().setProperty( property, flavorsList );
+    }
+
+    public void setProperty( String property ) {
+        this.property = property;
+    }
+}
