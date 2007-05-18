910b984b1cb83bb4d54f392893be6ca545373640
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-05-18 08:13:38 +0000

    added cursor, synchronized to single model

---
+++ trunk/simulations-java/simulations/rotation/src/edu/colorado/phet/rotation/graphs/CursorModel.java
@@ -0,0 +1,41 @@
+package edu.colorado.phet.rotation.graphs;
+
+import java.util.ArrayList;
+
+/**
+ * Author: Sam Reid
+ * May 18, 2007, 3:01:50 AM
+ */
+public class CursorModel {
+    private ArrayList listeners = new ArrayList();
+    private double time;
+
+    public double getTime() {
+        return time;
+    }
+
+    public void setTime( double time ) {
+        if( this.time != time ) {
+            this.time = time;
+            notifyListeners();
+        }
+    }
+
+    public static interface Listener {
+        void changed();
+    }
+
+    public void addListener( Listener listener ) {
+        listeners.add( listener );
+    }
+
+    public void removeListener( Listener listener ) {
+        listeners.remove( listener );
+    }
+
+    public void notifyListeners() {
+        for( int i = 0; i < listeners.size(); i++ ) {
+            ( (Listener)listeners.get( i ) ).changed();
+        }
+    }
+}
