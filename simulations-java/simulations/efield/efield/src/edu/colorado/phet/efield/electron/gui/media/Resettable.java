6f5eb8c41b8b00b174133aa51c7e6e212e58c926
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-05-21 04:58:06 +0000

    update efield data

---
+++ simulations-java/simulations/efield/efield/src/edu/colorado/phet/efield/electron/gui/media/Resettable.java
@@ -0,0 +1,14 @@
+// Decompiled by Jad v1.5.8f. Copyright 2001 Pavel Kouznetsov.
+// Jad home page: http://www.kpdus.com/jad.html
+// Decompiler options: packimports(3) 
+
+package edu.colorado.phet.efield.electron.gui.media;
+
+import edu.colorado.phet.efield.electron.gui.ParticlePanel;
+import edu.colorado.phet.efield.electron.phys2d_efield.System2D;
+
+public interface Resettable
+{
+
+    public abstract void fireResetAction(System2D system2d, ParticlePanel particlepanel);
+}
