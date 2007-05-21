3b14b8169109a1740e6cc36109ed0228d0af0180
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-05-21 04:58:06 +0000

    update efield data

---
+++ trunk/simulations-java/simulations/efield/efield/src/edu/colorado/phet/efield/electron/particleFactory/CustomizableParticleFactory.java
@@ -0,0 +1,54 @@
+// Decompiled by Jad v1.5.8f. Copyright 2001 Pavel Kouznetsov.
+// Jad home page: http://www.kpdus.com/jad.html
+// Decompiler options: packimports(3) 
+
+package edu.colorado.phet.efield.electron.particleFactory;
+
+import edu.colorado.phet.efield.electron.core.ParticleFactory;
+import edu.colorado.phet.efield.electron.utils.Debug;
+
+import java.awt.Rectangle;
+import java.util.Random;
+import edu.colorado.phet.efield.electron.phys2d_efield.DoublePoint;
+import edu.colorado.phet.efield.electron.phys2d_efield.Particle;
+
+// Referenced classes of package edu.colorado.phet.efield.electron.particleFactory:
+//            ParticlePropertyListener
+
+public class CustomizableParticleFactory
+    implements ParticleFactory, ParticlePropertyListener
+{
+
+    public CustomizableParticleFactory(int i, int j, int k, int l, Particle particle)
+    {
+        this(new Rectangle(i, j, k, l), particle);
+    }
+
+    public CustomizableParticleFactory(Rectangle rectangle, Particle particle)
+    {
+        bounds = rectangle;
+        properties = particle;
+    }
+
+    public void propertiesChanged(Particle particle)
+    {
+        properties = particle;
+    }
+
+    public Particle newParticle()
+    {
+        Particle particle = new Particle();
+        int i = rand.nextInt(bounds.width) + bounds.x;
+        int j = rand.nextInt(bounds.height) + bounds.y;
+        particle.setPosition(new DoublePoint(i, j));
+        particle.setCharge(properties.getCharge());
+        particle.setMass(properties.getMass());
+        Debug.traceln("created: " + particle);
+        return particle;
+    }
+
+    static Random rand = new Random();
+    Rectangle bounds;
+    Particle properties;
+
+}
