16341db6c5d96e64b13d9679c2a7244ed0552365
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-05-24 20:56:12 +0000

    remove duplicate strings

--- simulations-java/simulations/mri/src/edu/colorado/phet/mri/MriConfig.java
+++ simulations-java/simulations/mri/src/edu/colorado/phet/mri/MriConfig.java
@@ -32,8 +32,8 @@ public class MriConfig {
     public static final String STRINGS_BUNDLE_NAME = "mri/localization/mri-strings";
 
     // Descriptive information
-    public static final String TITLE = SimStrings.getInstance().getString( "Application.Title" );
-    public static final String DESCRIPTION = SimStrings.getInstance().getString( "Application.Description" );
+    public static final String TITLE = SimStrings.getInstance().getString( "mri.name" );
+    public static final String DESCRIPTION = SimStrings.getInstance().getString( "mri.description" );
     public static final String VERSION = "1.03.00, 14975";
 
     public final static double scale = 1;
