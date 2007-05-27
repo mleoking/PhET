6a1ac268264aff7f53699a39ad8bd10c341f8498
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-05-27 06:04:36 +0000

    added an origin graphic

---
+++ simulations-java/simulations/rotation/src/edu/colorado/phet/rotation/view/RotationOriginNode.java
@@ -0,0 +1,30 @@
+package edu.colorado.phet.rotation.view;
+
+import edu.colorado.phet.common.piccolophet.nodes.HTMLNode;
+import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
+import edu.colorado.phet.rotation.model.RotationPlatform;
+import edu.colorado.phet.rotation.util.LucidaSansFont;
+import edu.colorado.phet.rotation.util.UnicodeUtil;
+import edu.umd.cs.piccolo.PNode;
+
+import java.awt.*;
+import java.awt.geom.Line2D;
+
+/**
+ * Author: Sam Reid
+ * May 27, 2007, 12:56:07 AM
+ */
+public class RotationOriginNode extends PNode {
+
+    public RotationOriginNode( RotationPlatform rotationPlatform ) {
+        PhetPPath path = new PhetPPath( new Line2D.Double( 0, 0, 100, 0 ), new BasicStroke( 2.0f ), Color.black );
+        double offsetX = 20;
+        addChild( path );
+
+        HTMLNode htmlNode = new HTMLNode( "<html>" + UnicodeUtil.THETA + "=0<sup>o</sup></html>" );
+        htmlNode.setFont( new LucidaSansFont( 18, true ) );
+        addChild( htmlNode );
+
+        translate( rotationPlatform.getCenter().getX() + rotationPlatform.getRadius() + offsetX, rotationPlatform.getCenter().getY() );
+    }
+}
--- simulations-java/simulations/rotation/src/edu/colorado/phet/rotation/view/RotationPlayAreaNode.java
+++ simulations-java/simulations/rotation/src/edu/colorado/phet/rotation/view/RotationPlayAreaNode.java
@@ -1,8 +1,8 @@
 package edu.colorado.phet.rotation.view;
 
+import edu.colorado.phet.rotation.controls.VectorViewModel;
 import edu.colorado.phet.rotation.model.RotationBody;
 import edu.colorado.phet.rotation.model.RotationModel;
-import edu.colorado.phet.rotation.controls.VectorViewModel;
 import edu.umd.cs.piccolo.PNode;
 
 /**
@@ -16,27 +16,30 @@ public class RotationPlayAreaNode extends PNode {
     private PNode rotationBodyLayer = new PNode();
     private RotationModel rotationModel;
     private PNode vectorLayer = new PNode();
+    private RotationOriginNode originNode;
 
     public RotationPlayAreaNode( final RotationModel rotationModel, VectorViewModel vectiorViewModel ) {
         this.rotationModel = rotationModel;
         platformNode = new PlatformNode( rotationModel, rotationModel.getRotationPlatform() );
+        originNode = new RotationOriginNode( rotationModel.getRotationPlatform() );
 
         addChild( platformNode );
         addChild( rotationBodyLayer );
         addChild( vectorLayer );
+        addChild( originNode );
 
         for( int i = 0; i < rotationModel.getNumRotationBodies(); i++ ) {
             addRotationBodyNode( rotationModel.getRotationBody( i ) );
         }
         for( int i = 0; i < rotationModel.getNumRotationBodies(); i++ ) {
-            addVectorNode( rotationModel.getRotationBody( i ),vectiorViewModel);
+            addVectorNode( rotationModel.getRotationBody( i ), vectiorViewModel );
         }
 
 
     }
 
     private void addVectorNode( RotationBody rotationBody, VectorViewModel vectorViewModel ) {
-        vectorLayer.addChild( new BodyVectorLayer(rotationModel,rotationBody,vectorViewModel));
+        vectorLayer.addChild( new BodyVectorLayer( rotationModel, rotationBody, vectorViewModel ) );
     }
 
     public PlatformNode getPlatformNode() {
