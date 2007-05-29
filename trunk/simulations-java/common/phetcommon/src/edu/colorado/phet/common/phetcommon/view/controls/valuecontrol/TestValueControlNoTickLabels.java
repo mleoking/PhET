99cbd55021ea9d476a766736154a719cd88a6b3b
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-05-29 07:11:18 +0000

    Demonstration of failure of setting both Minor & Major tick labels both invisible on a LinearValueControl. Workaround is to use getSlider().setPaintTicks(false)

---
+++ trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/view/controls/valuecontrol/TestValueControlNoTickLabels.java
@@ -0,0 +1,40 @@
+package edu.colorado.phet.common.phetcommon.view.controls.valuecontrol;
+
+import edu.colorado.phet.common.phetcommon.view.VerticalLayoutPanel;
+
+import javax.swing.*;
+
+/**
+ * Demonstration of failure of setting both Minor & Major tick labels both invisible on a LinearValueControl.
+ * Workaround is to use getSlider().setPaintTicks(false)
+ *
+ * @author Sam Reid
+ */
+public class TestValueControlNoTickLabels extends JFrame {
+
+    public TestValueControlNoTickLabels() {
+        final LinearValueControl protonsControl = new LinearValueControl( 0, 10, "label", "0.00", "units" );
+        //these lines fail
+        protonsControl.setMinorTickLabelsVisible( false );
+        protonsControl.setMajorTickLabelsVisible( false );
+
+        //but this line works
+        //protonsControl.getSlider().setPaintTicks( false );
+
+        //and slider works properly
+        JSlider slider = new JSlider( 0, 100 );
+        slider.setPaintTicks( false );
+
+        JPanel contentPane = new VerticalLayoutPanel();
+        contentPane.add( protonsControl );
+        contentPane.add( slider );
+
+        setContentPane( contentPane );
+        pack();
+        setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+    }
+
+    public static void main( String[] args ) {
+        new TestValueControlNoTickLabels().show();
+    }
+}
