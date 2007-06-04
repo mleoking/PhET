a5e3215807c88ccbf91bf4f06ff838e59b87c2fb
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-06-04 15:08:25 +0000

    add more typical constructor, ala Font

--- trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/view/util/LucidaSansFont.java
+++ trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/view/util/LucidaSansFont.java
@@ -1,14 +1,28 @@
 /* Copyright 2007, University of Colorado */
 package edu.colorado.phet.common.phetcommon.view.util;
 
-import java.awt.*;
+import java.awt.Font;
 
 /**
  * Simplified interface for using a platform-independent (JRE bundled) font.
  */
 public class LucidaSansFont extends Font {
+    
+    private static final String FONT_NAME = "Lucida Sans";
+    
     /**
-     * Constructs a LucidaSansFont with a specified point size
+     * This constructor is more similar to the Font constructor, 
+     * but supplies the font name.
+     * 
+     * @param style
+     * @param size
+     */
+    public LucidaSansFont( int style, int size ) {
+        super( FONT_NAME, style, size );
+    }
+    
+    /**
+     * Constructs a LucidaSansFont with a specified point size.
      *
      * @param size the size of the font.
      */
@@ -34,6 +48,6 @@ public class LucidaSansFont extends Font {
      * @param italics whether it is italicized.
      */
     public LucidaSansFont( int size, boolean bold, boolean italics ) {
-        super( "Lucida Sans", ( bold ? Font.BOLD : Font.PLAIN ) | ( italics ? Font.ITALIC : Font.PLAIN ), size );
+        this( ( bold ? Font.BOLD : Font.PLAIN ) | ( italics ? Font.ITALIC : Font.PLAIN ), size );
     }
 }
