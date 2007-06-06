7d783f3c2393b06c7364ff5cd10bb55bec7a9b22
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-06-06 23:07:25 +0000

    added support for high-speed rendering of incrementalppath given correct usage

--- trunk/simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/nodes/IncrementalPPath.java
+++ trunk/simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/nodes/IncrementalPPath.java
@@ -6,14 +6,19 @@ import edu.umd.cs.piccolo.event.PZoomEventHandler;
 import edu.umd.cs.piccolo.nodes.PPath;
 import edu.umd.cs.piccolo.util.PBounds;
 import edu.umd.cs.piccolo.util.PDebug;
+import edu.umd.cs.piccolo.util.PPaintContext;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.geom.GeneralPath;
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.ArrayList;
 
 /**
  * Author: Sam Reid
@@ -22,19 +27,37 @@ import java.awt.geom.Rectangle2D;
 public class IncrementalPPath extends PPath {
     private PCanvas pCanvas;
     private boolean doingLineTo = false;
+//    private Point2D prevPt;
+    //    private Point2D currPt;
+    ArrayList pts = new ArrayList();
 
     public IncrementalPPath( PCanvas pCanvas ) {
         this.pCanvas = pCanvas;
+        PropertyChangeListener pcl = new PropertyChangeListener() {
+            public void propertyChange( PropertyChangeEvent evt ) {
+                pts.clear();
+                repaint();
+            }
+        };
+//        addPropertyChangeListener( PROPERTY_BOUNDS, pcl);
+//        addPropertyChangeListener( PROPERTY_FULL_BOUNDS, pcl);
+        addPropertyChangeListener( PROPERTY_TRANSFORM, pcl );
+        addPropertyChangeListener( PROPERTY_STROKE, pcl );
+//        addPropertyChangeListener( pcl);
+
     }
 
     public void lineTo( float x, float y ) {
-        Point2D currentPoint = getPathReference().getCurrentPoint();
+        pts.add( new Point2D.Double( x, y ) );
+//        this.prevPt = getPathReference().getCurrentPoint();
+//        this.currPt = ;
+        Point2D prevPoint = getPathReference().getCurrentPoint();
         doingLineTo = true;
-        super.lineTo( x, y );
+        localLineTo( x, y, prevPoint );
         doingLineTo = false;
         Point2D newPoint = getPathReference().getCurrentPoint();
 
-        Line2D.Double line = new Line2D.Double( currentPoint, newPoint );
+        Line2D.Double line = new Line2D.Double( prevPoint, newPoint );
         Rectangle2D bounds = getStroke().createStrokedShape( line ).getBounds2D();
 //        Rectangle2D.Double localBounds = new Rectangle2D.Double( bounds.getX(), bounds.getY(), bounds.getWidth(), bounds.getHeight() );
         localToGlobal( bounds );
@@ -42,6 +65,81 @@ public class IncrementalPPath extends PPath {
 //        System.out.println( "line=" + toString(line) + ", stroke=" + toString( getStroke() ) + ", localBounds=" + localBounds + ", globalBounds=" + bounds );
 //        System.out.println( "line=" + toString(line) + ", stroke=" + toString( getStroke() ) + ", globalBounds=" + bounds );
         globalBoundsChanged( bounds );
+
+    }
+
+    private void localLineTo( float x, float y, Point2D prevPoint ) {
+        getPathReference().lineTo( x, y );
+        firePropertyChange( PROPERTY_CODE_PATH, PROPERTY_PATH, null, getPathReference() );
+        localUpdateBoundsFromPath( prevPoint );
+        invalidatePaint();
+    }
+
+    protected void paint( PPaintContext paintContext ) {
+        Paint p = getPaint();
+        Graphics2D g2 = paintContext.getGraphics();
+
+        if( p != null ) {
+            g2.setPaint( p );
+            g2.fill( getPathReference() );
+        }
+
+        if( getStroke() != null && getStrokePaint() != null ) {
+            g2.setPaint( getStrokePaint() );
+            g2.setStroke( getStroke() );
+            int numPtsToUse = 30;
+            if( pts.size() < numPtsToUse ) {
+                g2.draw( getPathReference() );
+            }
+            else {
+                GeneralPath path = new GeneralPath();
+                path.moveTo( (float)getPreviousPoint( 0 ).getX(), (float)getPreviousPoint( 0 ).getY() );
+                for( int i = 1; i < numPtsToUse; i++ ) {
+                    path.lineTo( (float)getPreviousPoint( i ).getX(), (float)getPreviousPoint( i ).getY() );
+                }
+                g2.draw( path );
+            }
+        }
+    }
+
+    private Point2D getPreviousPoint( int i ) {
+        return (Point2D)pts.get( pts.size() - 1 - i );
+    }
+
+    private boolean updatingBoundsFromPath = false;
+
+    protected void internalUpdateBounds( double x, double y, double width, double height ) {
+        if( updatingBoundsFromPath ) {
+            return;
+        }
+        else {
+            super.internalUpdateBounds( x, y, width, height );
+        }
+    }
+
+    private void localUpdateBoundsFromPath( Point2D prevPoint ) {
+        updatingBoundsFromPath = true;
+        if( getPathReference() == null ) {
+            resetBounds();
+        }
+        else {
+//            Rectangle2D b = getPathBoundsWithStroke();
+            Rectangle2D b = getBounds().createUnion( getLastSegmentBoundsWithStroke( prevPoint ) );
+            setBounds( b.getX(), b.getY(), b.getWidth(), b.getHeight() );
+        }
+        updatingBoundsFromPath = false;
+    }
+
+    private Rectangle2D getLastSegmentBoundsWithStroke( Point2D prevPoint ) {
+        if( getStroke() != null && prevPoint != null ) {
+            Line2D.Double line = new Line2D.Double( getPathReference().getCurrentPoint(), prevPoint );
+            return getStroke().createStrokedShape( line ).getBounds2D();
+
+//            return newBounds;
+        }
+        else {
+            return getPathReference().getBounds2D();
+        }
     }
 
     protected void globalBoundsChanged( Rectangle2D bounds ) {
@@ -51,12 +149,13 @@ public class IncrementalPPath extends PPath {
     protected void repaintGlobalBounds( Rectangle2D bounds ) {
         pCanvas.repaint( new PBounds( bounds ) );
     }
+
     protected void repaintGlobalBoundsImmediately( Rectangle2D bounds ) {
         pCanvas.paintImmediately( bounds.getBounds() );
     }
 
     private String toString( Line2D.Double line ) {
-        return line.getP1()+" to "+line.getP2();
+        return line.getP1() + " to " + line.getP2();
     }
 
     private String toString( Stroke stroke ) {
@@ -77,6 +176,7 @@ public class IncrementalPPath extends PPath {
 
     /**
      * Demonstration for IncrementalPPath.
+     *
      * @param args
      */
     public static void main( String[] args ) {
