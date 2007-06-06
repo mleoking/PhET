2d183a27864b724aaeb7833b3892b4f5b4f94c7d
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-06-06 09:23:58 +0000

    refactored

--- simulations-java/common/jfreechart-phet/src/edu/colorado/phet/common/jfreechartphet/piccolo/dynamic/IncrementalPPathSeriesView.java
+++ simulations-java/common/jfreechart-phet/src/edu/colorado/phet/common/jfreechartphet/piccolo/dynamic/IncrementalPPathSeriesView.java
@@ -7,8 +7,8 @@ import edu.umd.cs.piccolo.nodes.PPath;
  * Author: Sam Reid
  * Jun 5, 2007, 6:03:59 PM
  */
-public class PiccoloSeriesView extends PPathSeriesView {
-    public PiccoloSeriesView( DynamicJFreeChartNode dynamicJFreeChartNode, SeriesData seriesData ) {
+public class IncrementalPPathSeriesView extends PPathSeriesView {
+    public IncrementalPPathSeriesView( DynamicJFreeChartNode dynamicJFreeChartNode, SeriesData seriesData ) {
         super( dynamicJFreeChartNode, seriesData );
     }
 
