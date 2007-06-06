4815febd937a0878b14d30e67baba359205a2dd3
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-06-06 16:44:01 +0000

    removed unused library copies, added test for broken slider

--- simulations-java/simulations/self-driven-particle-model/src/org/reid/particles/view/RandomnessSlider.java
+++ simulations-java/simulations/self-driven-particle-model/src/org/reid/particles/view/RandomnessSlider.java
@@ -2,10 +2,14 @@
 package org.reid.particles.view;
 
 import edu.colorado.phet.common.phetcommon.view.ModelSlider;
+import edu.colorado.phet.common.piccolophet.PhetPCanvas;
+import edu.umd.cs.piccolox.pswing.PSwing;
 import org.reid.particles.model.ParticleModel;
 
+import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+import java.awt.*;
 import java.text.DecimalFormat;
 
 /**
@@ -38,6 +42,18 @@ public class RandomnessSlider extends ModelSlider {
         return particleModel;
     }
 
-//    final ModelSlider randomnessSlider = new ModelSlider( "Randomness", "radians", 0, Math.PI * 2, model.getAngleRandomness(), new DecimalFormat( "0.00" ) );
+    //    final ModelSlider randomnessSlider = new ModelSlider( "Randomness", "radians", 0, Math.PI * 2, model.getAngleRandomness(), new DecimalFormat( "0.00" ) );
+    public static void main( String[] args ) {
+        JFrame frame = new JFrame();
+//        JPanel contentPane = new JPanel();
+        PhetPCanvas pane = new PhetPCanvas();
+        pane.addScreenChild( new PSwing( pane, new RandomnessSlider( new ParticleModel( 400, 400 ) ) ) );
+        frame.setContentPane( pane );
+        frame.pack();
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        frame.setLocation( Toolkit.getDefaultToolkit().getScreenSize().width / 2 - frame.getWidth() / 2,
+                           Toolkit.getDefaultToolkit().getScreenSize().height / 2 - frame.getHeight() / 2 );
+        frame.show();
 
+    }
 }
