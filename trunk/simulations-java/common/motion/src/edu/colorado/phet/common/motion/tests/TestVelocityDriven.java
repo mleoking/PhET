78cd902223458305d234b17a2600e5a479b83abe
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-06-07 05:05:33 +0000

    posted tests in rotation project

--- trunk/simulations-java/common/motion/src/edu/colorado/phet/common/motion/tests/TestVelocityDriven.java
+++ trunk/simulations-java/common/motion/src/edu/colorado/phet/common/motion/tests/TestVelocityDriven.java
@@ -8,6 +8,7 @@ package edu.colorado.phet.common.motion.tests;
  */
 
 import edu.colorado.phet.common.phetcommon.view.ModelSlider;
+import edu.colorado.phet.common.phetcommon.model.clock.SwingClock;
 import edu.colorado.phet.common.motion.model.MotionModel;
 import edu.colorado.phet.common.motion.model.VelocityDriven;
 
@@ -28,7 +29,7 @@ public class TestVelocityDriven {
         frame.setSize( 600, 600 );
         frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
 
-        rotationModel = new MotionModel();
+        rotationModel = new MotionModel(new SwingClock( 30,1) );
         final VelocityDriven updateStrategy = new VelocityDriven();
         rotationModel.setUpdateStrategy( updateStrategy );
         final ModelSlider modelSlider = new ModelSlider( "Velocity", "m/s", -10, 10, rotationModel.getVelocity() );
