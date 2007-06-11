c853895b860e104629bd4f56840c7e4b77e03296
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-06-11 15:33:40 +0000

    6/11/07 - change objective view, make it an on-edge view of the lens

--- simulations-java/simulations/optical-tweezers/src/edu/colorado/phet/opticaltweezers/view/ObjectiveNode.java
+++ simulations-java/simulations/optical-tweezers/src/edu/colorado/phet/opticaltweezers/view/ObjectiveNode.java
@@ -6,23 +6,27 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Stroke;
 import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
 
 import edu.umd.cs.piccolo.nodes.PPath;
+import edu.umd.cs.piccolox.nodes.PComposite;
 
 /**
  * ObjectiveNode is the visual representation of the microscope objective (lens).
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
-public class ObjectiveNode extends PPath {
+public class ObjectiveNode extends PComposite {
 
     //----------------------------------------------------------------------------
     // Class data
     //----------------------------------------------------------------------------
     
-    private static final Color FILL_COLOR = new Color( 217, 240, 255, 100 ); // transparent light blue
+    private static final Color LENS_COLOR = new Color( 217, 240, 255, 100 ); // transparent light blue
+    private static final Color CENTER_COLOR = new Color( 200, 220, 255, 180 ); // slightly darker, more opaque
     private static final Color STROKE_COLOR = Color.BLACK;
     private static final Stroke STROKE = new BasicStroke( 1f );
+    private static final double THICKNESS_PERCENT = 0.14; // thickness of center rectangle, % of objective height
     
     //----------------------------------------------------------------------------
     // Constructor
@@ -37,10 +41,18 @@ public class ObjectiveNode extends PPath {
     public ObjectiveNode( double width, double height ) {
         super();
         
-        // (0,0) is at center
-        setPathTo( new Ellipse2D.Double( -width/2, -height/2, width, height ) );
-        setPaint( FILL_COLOR );
-        setStroke( STROKE );
-        setStrokePaint( STROKE_COLOR );
+        PPath ellipseNode = new PPath();
+        ellipseNode.setPathTo( new Ellipse2D.Double( -width/2, -height/2, width, height ) ); // origin at center
+        ellipseNode.setPaint( LENS_COLOR );
+        ellipseNode.setStroke( STROKE );
+        ellipseNode.setStrokePaint( STROKE_COLOR );
+        addChild( ellipseNode );
+        
+        PPath rectangleNode = new PPath();
+        rectangleNode.setPathTo( new Rectangle2D.Double( -width/2, -( THICKNESS_PERCENT * height ) /2, width, ( THICKNESS_PERCENT * height ) ) ); // origin at center
+        rectangleNode.setPaint( CENTER_COLOR );
+        rectangleNode.setStroke( STROKE );
+        rectangleNode.setStrokePaint( STROKE_COLOR );
+        addChild( rectangleNode );
     }
 }
