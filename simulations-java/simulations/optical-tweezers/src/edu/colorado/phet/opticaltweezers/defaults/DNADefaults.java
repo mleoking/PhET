e3a04c162566a6c8b752057d2b28d5f4a5e42038
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-06-11 02:22:12 +0000

    6/7/07 - add fluid drag force to acceleration in DNA strand evolution model 6/7/07 - add fluid drag coefficient to DNA model and developer controls

--- simulations-java/simulations/optical-tweezers/src/edu/colorado/phet/opticaltweezers/defaults/DNADefaults.java
+++ simulations-java/simulations/optical-tweezers/src/edu/colorado/phet/opticaltweezers/defaults/DNADefaults.java
@@ -79,6 +79,7 @@ public class DNADefaults {
     public static final DoubleRange DNA_EVOLUTION_DT_RANGE = new DoubleRange( 0.05, 0.2, 0.1 );
     public static final DoubleRange DNA_KICK_CONSTANT_RANGE = new DoubleRange( 10, 100, 50, 0 );
     public static final IntegerRange DNA_NUMBER_OF_EVOLUTIONS_PER_CLOCK_STEP_RANGE = new IntegerRange( 1, 100, 30 );
+    public static final DoubleRange DNA_FLUID_DRAG_COEFFICIENT_RANGE = new DoubleRange( 0, 0.00020, 0.00005 );
     public static final boolean DNA_PIVOTS_VISIBLE = true;
     public static final boolean DNA_EXTENSION_VISIBLE = true;
     
