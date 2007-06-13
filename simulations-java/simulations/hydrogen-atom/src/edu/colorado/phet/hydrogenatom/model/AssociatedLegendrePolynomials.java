9c807914a4c1fc0bce6bcdf6646db1b2eb12c111
jdegoes <jdegoes@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-06-13 22:38:59 +0000

    hydrogen-atom: Factored out term class & added corresponding polynomial class; made class immutable & added associated test suite. Both term & polynomial class now located in common area.

--- simulations-java/simulations/hydrogen-atom/src/edu/colorado/phet/hydrogenatom/model/AssociatedLegendrePolynomials.java
+++ simulations-java/simulations/hydrogen-atom/src/edu/colorado/phet/hydrogenatom/model/AssociatedLegendrePolynomials.java
@@ -3,6 +3,8 @@
 package edu.colorado.phet.hydrogenatom.model;
 
 
+import edu.colorado.phet.common.phetcommon.math.PolynomialTerm;
+
 import java.util.ArrayList;
 
 /**
@@ -17,58 +19,58 @@ public class AssociatedLegendrePolynomials {
      * <p>
      * This solution uses Wolfram's definition of the associated Legendre polynomial.
      * See http://mathworld.wolfram.com/LegendrePolynomial.html.
-     * When l > 6, this implemention starts to differ from Wolfram and "Numerical Recipes in C".
+     * When L > 6, this implemention starts to differ from Wolfram and "Numerical Recipes in C".
      * To compare with Mathematica online, use: x^2*(3)*( LegendreP[7,3,-0.99])
      * as the input to The Integrator at http://integrals.wolfram.com/index.jsp
      * 
-     * @param l electron's secondary state
-     * @param m electron's tertiary state
+     * @param L electron's secondary state
+     * @param M electron's tertiary state
      * @param x coordinate on horizontal axis
      * @return double
-     * @throws IllegalArgumentException if l > 6
+     * @throws IllegalArgumentException if L > 6
      */
-    public static double solve( int l, int m, double x ) {
+    public static double solve( int L, int M, double x ) {
 
         // validate arguments
-        if ( l > 6 ) {
-            throw new IllegalArgumentException( "untested for l > 6" );
+        if ( L > 6 ) {
+            throw new IllegalArgumentException( "untested for L > 6" );
         }
-        if ( l < 0 ) {
-            throw new IllegalArgumentException( "l out of bounds: " + l );
+        if ( L < 0 ) {
+            throw new IllegalArgumentException( "L out of bounds: " + L );
         }
-        if ( m < 0 || m > l ) {
-            throw new IllegalArgumentException( "m out of bounds: " + m );
+        if ( M < 0 || M > L ) {
+            throw new IllegalArgumentException( "M out of bounds: " + M );
         }
         if ( Math.abs( x ) > 1.0 ) {
             throw new IllegalArgumentException( "x out of bounds: " + x );
         }
 
         ArrayList productTerms = new ArrayList();
-        productTerms.add( new Term( 0, 1 ) );
-        for ( int i = 0; i < l; i++ ) {
+        productTerms.add( new PolynomialTerm( 0, 1 ) );
+        for ( int i = 0; i < L; i++ ) {
             //x^2-1 times each term on left side
             ArrayList terms = new ArrayList();
             for ( int k = 0; k < productTerms.size(); k++ ) {
-                Term term = (Term) productTerms.get( k );
-                terms.add( new Term( term.getPower() + 2, term.getCoeff() ) );
-                terms.add( new Term( term.getPower(), term.getCoeff() * -1 ) );
+                PolynomialTerm term = (PolynomialTerm) productTerms.get( k );
+                terms.add( new PolynomialTerm( term.getPower() + 2, term.getCoeff() ) );
+                terms.add( new PolynomialTerm( term.getPower(), term.getCoeff() * -1 ) );
             }
             productTerms = terms;
         }
         for ( int k = 0; k < productTerms.size(); k++ ) {
-            Term t = (Term) productTerms.get( k );
-            t.derive( l + m );
+            PolynomialTerm t = (PolynomialTerm) productTerms.get( k );
+            productTerms.set( k, t.derive( L + M ) );
         }
 
         // Wolfram says there is a sign convention difference here
-        double legendre = Math.pow( -1, m ) * Math.pow( 2, -l ) / fact( l ) * Math.pow( 1 - x * x, m / 2.0 ) * eval( productTerms, x );
+        double legendre = Math.pow( -1, M ) / (Math.pow( 2, L ) * fact( L )) * Math.pow( 1 - x * x, M / 2.0 ) * eval( productTerms, x );
         return legendre;
     }
 
     private static double eval( ArrayList productTerms, double x ) {
         double sum = 0;
         for ( int i = 0; i < productTerms.size(); i++ ) {
-            Term term = (Term) productTerms.get( i );
+            PolynomialTerm term = (PolynomialTerm) productTerms.get( i );
             sum += term.eval( x );
         }
         return sum;
@@ -78,45 +80,4 @@ public class AssociatedLegendrePolynomials {
         return ( a == 0 || a == 1 ) ? 1 : a * fact( a - 1 );
     }
 
-    private static class Term {
-
-        int power;
-        int coeff;
-
-        public Term( int power, int coeff ) {
-            this.power = power;
-            this.coeff = coeff;
-        }
-
-        public int getPower() {
-            return power;
-        }
-
-        public int getCoeff() {
-            return coeff;
-        }
-
-        public void derive( int numDer ) {
-            for ( int i = 0; i < numDer; i++ ) {
-                derive();
-            }
-        }
-
-        private void derive() {
-            if ( power == 0 ) {
-                power = 0;
-                coeff = 0;
-            }
-            else {
-                int newCoeff = coeff * power;
-                int newPower = power - 1;
-                this.coeff = newCoeff;
-                this.power = newPower;
-            }
-        }
-
-        public double eval( double x ) {
-            return Math.pow( x, power ) * coeff;
-        }
-    }
 }
