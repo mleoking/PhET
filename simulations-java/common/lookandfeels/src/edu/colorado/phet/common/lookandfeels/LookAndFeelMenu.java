b14684c0603b0adf9c4408f8e9e2f80456cb5a2b
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-06-14 06:46:44 +0000

    removed dependencies on look and feels

--- simulations-java/common/lookandfeels/src/edu/colorado/phet/common/lookandfeels/LookAndFeelMenu.java
+++ simulations-java/common/lookandfeels/src/edu/colorado/phet/common/lookandfeels/LookAndFeelMenu.java
@@ -1,25 +1,22 @@
 package edu.colorado.phet.common.lookandfeels;
 
-import com.oyoaha.swing.plaf.oyoaha.OyoahaLookAndFeel;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.net.URL;
 
 /**
  * User: Sam Reid
  * Date: Feb 18, 2007
  * Time: 3:58:03 PM
- *
  */
 
 public class LookAndFeelMenu extends JMenu {
 
     public LookAndFeelMenu() {
         super( "Look & Feel" );
-        UIManager.installLookAndFeel( "Oyoaha Default", OyoahaLookAndFeel.class.getName() );
+//        UIManager.installLookAndFeel( "Oyoaha Default", OyoahaLookAndFeel.class.getName() );
         setMnemonic( 'l' );
         UIManager.LookAndFeelInfo[] infos = UIManager.getInstalledLookAndFeels();
         for( int i = 0; i < infos.length; i++ ) {
@@ -49,14 +46,14 @@ public class LookAndFeelMenu extends JMenu {
             add( jMenuItem );
         }
 
-        LookAndFeelMenuItem liquid = new LookAndFeelMenuItem( "Liquid", "com.birosoft.liquid.LiquidLookAndFeel" );
-        add( liquid );
+//        LookAndFeelMenuItem liquid = new LookAndFeelMenuItem( "Liquid", "com.birosoft.liquid.LiquidLookAndFeel" );
+//        add( liquid );
 
 //        add( new ThemePackMenuItem( "Daisy", "Oyoaha.themepack.14.04.05/daisy2.zotm" ) );
-        String[] names = new String[]{"anidaisy matrix.zotm", "anidaisy.zotm", "daisy2.zotm", "flat1.zotm", "flat2.zotm", "flat3.zotm", "flat4.zotm", "flat5.zotm", "flat6.zotm", "flat7.zotm", "flat8.zotm", "gang.zotm", "gradient.zotm", "slushy.zotm", "slushy10.zotm", "slushy2.zotm", "slushy3.zotm", "slushy4.zotm", "slushy5.zotm", "slushy6.zotm", "slushy7.zotm", "slushy8.zotm", "slushy9.zotm", "tfiberr.zotm", "tfiberr2.zotm", "tflat1.zotm", "tflat2.zotm", "tflat3.zotm", "tflat4.zotm", "tflat5.zotm", "tflat6.zotm", "tflat7.zotm", "tgang.zotm", "tgang2.zotm", "tgang3.zotm", "tzipper.zotm", "zipper.zotm"};
-        for( int i = 0; i < names.length; i++ ) {
-            add( new ThemePackMenuItem( names[i], "Oyoaha.themepack.14.04.05/" + names[i] ) );
-        }
+//        String[] names = new String[]{"anidaisy matrix.zotm", "anidaisy.zotm", "daisy2.zotm", "flat1.zotm", "flat2.zotm", "flat3.zotm", "flat4.zotm", "flat5.zotm", "flat6.zotm", "flat7.zotm", "flat8.zotm", "gang.zotm", "gradient.zotm", "slushy.zotm", "slushy10.zotm", "slushy2.zotm", "slushy3.zotm", "slushy4.zotm", "slushy5.zotm", "slushy6.zotm", "slushy7.zotm", "slushy8.zotm", "slushy9.zotm", "tfiberr.zotm", "tfiberr2.zotm", "tflat1.zotm", "tflat2.zotm", "tflat3.zotm", "tflat4.zotm", "tflat5.zotm", "tflat6.zotm", "tflat7.zotm", "tgang.zotm", "tgang2.zotm", "tgang3.zotm", "tzipper.zotm", "zipper.zotm"};
+//        for( int i = 0; i < names.length; i++ ) {
+//            add( new ThemePackMenuItem( names[i], "Oyoaha.themepack.14.04.05/" + names[i] ) );
+//        }
     }
 
     public class LookAndFeelMenuItem extends JMenuItem {
@@ -86,32 +83,32 @@ public class LookAndFeelMenu extends JMenu {
         }
     }
 
-    public class ThemePackMenuItem extends JMenuItem {
-        public ThemePackMenuItem( String res ) {
-            this( res, res );
-        }
-
-        public ThemePackMenuItem( String name, final String resource ) {
-            super( name );
-            addActionListener( new ActionListener() {
-                public void actionPerformed( ActionEvent e ) {
-                    OyoahaLookAndFeel look = new OyoahaLookAndFeel();
-//                    URL url = getClass().getClassLoader().getResource( resource );
-                    System.out.println( "Trying to load resource: " + resource );
-                    URL url = Thread.currentThread().getContextClassLoader().getResource( resource );
-                    look.setOyoahaTheme( url );
-
-                    try {
-                        UIManager.setLookAndFeel( look );
-                    }
-                    catch( UnsupportedLookAndFeelException e1 ) {
-                        e1.printStackTrace();
-                    }
-                    refreshApp();
-                }
-            } );
-        }
-    }
+//    public class ThemePackMenuItem extends JMenuItem {
+//        public ThemePackMenuItem( String res ) {
+//            this( res, res );
+//        }
+//
+//        public ThemePackMenuItem( String name, final String resource ) {
+//            super( name );
+//            addActionListener( new ActionListener() {
+//                public void actionPerformed( ActionEvent e ) {
+//                    OyoahaLookAndFeel look = new OyoahaLookAndFeel();
+////                    URL url = getClass().getClassLoader().getResource( resource );
+//                    System.out.println( "Trying to load resource: " + resource );
+//                    URL url = Thread.currentThread().getContextClassLoader().getResource( resource );
+//                    look.setOyoahaTheme( url );
+//
+//                    try {
+//                        UIManager.setLookAndFeel( look );
+//                    }
+//                    catch( UnsupportedLookAndFeelException e1 ) {
+//                        e1.printStackTrace();
+//                    }
+//                    refreshApp();
+//                }
+//            } );
+//        }
+//    }
 
     /**
      * Makes sure the component tree UI of all frames are updated.
