99379834c2988db455057a56165af3c0ab718845
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-06-15 09:01:37 +0000

    update audio manager

--- trunk/simulations-java/misc/phet-media/src/edu/colorado/phet/mm/MediaSoundApplication.java
+++ trunk/simulations-java/misc/phet-media/src/edu/colorado/phet/mm/MediaSoundApplication.java
@@ -4,8 +4,11 @@ import edu.colorado.phet.common.phetcommon.view.util.PhetAudioClip;
 import edu.colorado.phet.mm.util.FileUtils;
 
 import javax.swing.*;
+import javax.swing.event.MouseInputAdapter;
+import java.applet.Applet;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.MouseEvent;
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -16,7 +19,9 @@ import java.util.Vector;
  * Author: Sam Reid
  * Jun 15, 2007, 3:38:43 AM
  */
-public class SoundApplication {
+public class MediaSoundApplication {
+    private static JList list;
+
     public static void main( String[] args ) {
         File[] soundFiles = MediaFinder.getSoundFiles();
         ArrayList singleCopies = new ArrayList();
@@ -28,28 +33,50 @@ public class SoundApplication {
         }
         System.out.println( "original = " + soundFiles.length );
         System.out.println( "without duplicates = " + singleCopies.size() );
-        final JList list = new JList( new Vector( singleCopies ) );
+        list = new JList( new Vector( singleCopies ) );
         list.setSelectionMode( ListSelectionModel.SINGLE_SELECTION );
         JFrame frame = new JFrame();
         JPanel panel = new JPanel( new BorderLayout() );
         frame.setContentPane( panel );
         panel.add( list, BorderLayout.CENTER );
+        list.addMouseListener( new MouseInputAdapter() {
+            // implements java.awt.event.MouseListener
+            public void mousePressed( MouseEvent e ) {
+                if( e.getClickCount() >= 2 ) {
+                    playSelected();
+                }
+            }
+        } );
         JButton play = new JButton( new AbstractAction( "Play" ) {
             public void actionPerformed( ActionEvent e ) {
-                File file = (File)list.getSelectedValue();
-                try {
-                    PhetAudioClip phetAudioClip = new PhetAudioClip( file.toURL() );
-                    phetAudioClip.play();
-                }
-                catch( MalformedURLException e1 ) {
-                    e1.printStackTrace();
-                }
+                playSelected();
             }
         } );
         panel.add( play, BorderLayout.SOUTH );
         frame.setSize( 800, 600 );
         frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
         frame.show();
+    }
+
+    private static void playSelected() {
+        File file = (File)list.getSelectedValue();
+        if( file.getAbsolutePath().toLowerCase().endsWith( ".au" ) ) {
+            try {
+                Applet.newAudioClip( file.toURL() ).play();
+            }
+            catch( MalformedURLException e ) {
+                e.printStackTrace();
+            }
+        }
+        else {
+            try {
+                PhetAudioClip phetAudioClip = new PhetAudioClip( file.toURL() );
+                phetAudioClip.play();
+            }
+            catch( MalformedURLException e1 ) {
+                e1.printStackTrace();
+            }
+        }
     }
 
     private static boolean contains( ArrayList fileList, File b ) {
