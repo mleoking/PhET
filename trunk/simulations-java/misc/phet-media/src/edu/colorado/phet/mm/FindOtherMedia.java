69d560b32bb045527e687676c305e132f52c0f66
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-06-15 08:24:27 +0000

    refactor, start search for non-images

---
+++ trunk/simulations-java/misc/phet-media/src/edu/colorado/phet/mm/FindOtherMedia.java
@@ -0,0 +1,17 @@
+package edu.colorado.phet.mm;
+
+import java.io.File;
+
+/**
+ * Author: Sam Reid
+ * Jun 15, 2007, 3:11:22 AM
+ */
+public class FindOtherMedia {
+    public static void main( String[] args ) {
+        File[]dataDir= MediaFinder.getDataDirectories();
+        for( int i = 0; i < dataDir.length; i++ ) {
+            File file = dataDir[i];
+            
+        }
+    }
+}
