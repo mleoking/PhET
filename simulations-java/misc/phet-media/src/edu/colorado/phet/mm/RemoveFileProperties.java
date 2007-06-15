c7eb41f1d7bf5a82fdfa5740356c6104c1db3378
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-06-15 07:29:45 +0000

    removing filename key

---
+++ simulations-java/misc/phet-media/src/edu/colorado/phet/mm/RemoveFileProperties.java
@@ -0,0 +1,25 @@
+package edu.colorado.phet.mm;
+
+import java.io.*;
+import java.util.Properties;
+
+/**
+ * Author: Sam Reid
+ * Jun 15, 2007, 2:25:27 AM
+ */
+public class RemoveFileProperties {
+    public static void main( String[] args ) throws IOException {
+        File[] file = new File( "annotated-data" ).listFiles( new FilenameFilter() {
+            public boolean accept( File dir, String name ) {
+                return name.toLowerCase().endsWith( "properties" );
+            }
+        } );
+        for( int i = 0; i < file.length; i++ ) {
+            File file1 = file[i];
+            Properties properties = new Properties();
+            properties.load( new FileInputStream( file1 ) );
+            properties.remove( "filename" );
+            properties.store( new FileOutputStream( file1 ), null );
+        }
+    }
+}
