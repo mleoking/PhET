53d2c6fa23ca0f68c460bc7c72a8198ee086c0cb
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-06-20 05:34:43 +0000

    added a test to demonstrate failure of time series derivative alignment

---
+++ simulations-java/common/motion/src/edu/colorado/phet/common/motion/tests/TestDerivativeOffsets.java
@@ -0,0 +1,42 @@
+package edu.colorado.phet.common.motion.tests;
+
+import edu.colorado.phet.common.motion.model.MotionModel;
+import edu.colorado.phet.common.phetcommon.model.clock.SwingClock;
+
+/**
+ * Author: Sam Reid
+ * Jun 20, 2007, 12:26:53 AM
+ */
+public class TestDerivativeOffsets {
+    private MotionModel motionModel;
+    private SwingClock clock;
+
+    public TestDerivativeOffsets() {
+        clock = new SwingClock( 30, 1 );
+        motionModel = new MotionModel( clock );
+        motionModel.setPositionDriven();
+    }
+
+    private void step( int i ) {
+        for( int k = 0; k < i; k++ ) {
+            clock.stepClockWhilePaused();
+            showState( motionModel );
+        }
+    }
+
+    private void showState( MotionModel motionModel ) {
+        System.out.println( "t=" + motionModel.getTime() + ", x=" + motionModel.getPosition() + ", v=" + motionModel.getVelocity() + ", a=" + motionModel.getAcceleration() );
+    }
+
+    private void start() {
+        showState( motionModel );
+        step( 100 );
+        motionModel.setPosition( 1.0 );
+        step( 100 );
+    }
+
+    public static void main( String[] args ) {
+        new TestDerivativeOffsets().start();
+    }
+
+}
