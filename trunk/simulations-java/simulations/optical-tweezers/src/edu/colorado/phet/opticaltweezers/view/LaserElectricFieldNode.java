b008948b119123fcaca2c64ff7ac84f1cdbd7e05
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-06-20 19:01:44 +0000

    add dummy vector at laser center

--- trunk/simulations-java/simulations/optical-tweezers/src/edu/colorado/phet/opticaltweezers/view/LaserElectricFieldNode.java
+++ trunk/simulations-java/simulations/optical-tweezers/src/edu/colorado/phet/opticaltweezers/view/LaserElectricFieldNode.java
@@ -2,6 +2,7 @@
 
 package edu.colorado.phet.opticaltweezers.view;
 
+import java.awt.geom.Point2D;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -29,17 +30,34 @@ public class LaserElectricFieldNode extends PhetPNode implements Observer {
     private Laser _laser;
     private ModelViewTransform _modelViewTransform;
     
+    private Point2D[] _samplePoints; // sample points, relative to laser origin
+    private Vector2DNode[] _vectorNodes; // vector display for each element in _samplePoints
+    private final double _referenceMagnitude;
+    private final double _referenceLength;
+    
     //----------------------------------------------------------------------------
     // Constructors
     //----------------------------------------------------------------------------
     
     public LaserElectricFieldNode( Laser laser, ModelViewTransform modelViewTransform ) {
         super();
+        setPickable( false );
+        setChildrenPickable( false );
         
         _laser = laser;
         _laser.addObserver( this );
         
         _modelViewTransform = modelViewTransform;
+        
+        _referenceMagnitude = 200;//XXX
+        _referenceLength = 200;//XXX
+        
+        Vector2DNode vectorNode = new Vector2DNode( 100, 0, _referenceMagnitude, _referenceLength );
+        addChild( vectorNode );
+        // move vector to center of trap
+        final double x = _modelViewTransform.modelToView( laser.getDiameterAtObjective() / 2 );
+        final double y = _modelViewTransform.modelToView( laser.getY() );
+        vectorNode.setOffset( x, y );
     }
 
     public void cleanup() {
