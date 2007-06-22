1627ccfcbd6e2d466f8914b09527808a4733007b
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-06-22 20:25:59 +0000

    6/22/07 - E-field vectors should be green outline with no fill color, alpha based on E-field magnitude

--- simulations-java/simulations/optical-tweezers/src/edu/colorado/phet/opticaltweezers/view/LaserElectricFieldNode.java
+++ simulations-java/simulations/optical-tweezers/src/edu/colorado/phet/opticaltweezers/view/LaserElectricFieldNode.java
@@ -2,12 +2,16 @@
 
 package edu.colorado.phet.opticaltweezers.view;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Stroke;
 import java.awt.geom.Point2D;
 import java.util.*;
 
 import edu.colorado.phet.common.piccolophet.PhetPNode;
 import edu.colorado.phet.opticaltweezers.model.Laser;
 import edu.colorado.phet.opticaltweezers.model.ModelViewTransform;
+import edu.colorado.phet.opticaltweezers.util.ColorUtils;
 import edu.colorado.phet.opticaltweezers.util.Vector2D;
 
 
@@ -24,6 +28,11 @@ public class LaserElectricFieldNode extends PhetPNode implements Observer {
     //----------------------------------------------------------------------------
     
     private static final double REFERENCE_LENGTH = 100; //XXX this should be based on # of vectors across laser waist
+    private static final String ELECTRIC_FIELD_UNITS = "V/nm";
+    
+    // vector color and stroke
+    private static final Color VECTOR_STROKE_COLOR = Color.GREEN.darker();
+    private static final Stroke VECTOR_STROKE = new BasicStroke( 1f );
     
     // layout of vectors
     private static final double X_MARGIN = 20; // nm
@@ -139,12 +148,18 @@ public class LaserElectricFieldNode extends PhetPNode implements Observer {
     }
     
     private void updateVectors() {
+        final Vector2D maxElectricField = _laser.getMaxElectricField();
         Iterator i = _vectorNodes.iterator();
         while ( i.hasNext() ) {
             ElectricFieldVectorNode vectorNode = (ElectricFieldVectorNode) i.next();
+            // set the vector's magnitude
             Point2D offsetFromLaser = vectorNode.getOffsetFromLaser();
             Vector2D electricField = _laser.getElectricField( offsetFromLaser );
             vectorNode.setXY( electricField.getX(), electricField.getY() );
+            // set the vector's color
+            int alpha = (int)( 255 * electricField.getMagnitude() / maxElectricField.getMagnitude() );
+            Color c = ColorUtils.addAlpha( VECTOR_STROKE_COLOR, alpha );
+            vectorNode.setArrowStrokePaint( c );
         }
     }
     
@@ -159,6 +174,9 @@ public class LaserElectricFieldNode extends PhetPNode implements Observer {
         public ElectricFieldVectorNode( Point2D offsetFromLaser, double xMagnitude, double yMagnitude, double referenceMagnitude, double referenceLength ) {
             super( xMagnitude, yMagnitude, referenceMagnitude, referenceLength );
             _offsetFromLaser = new Point2D.Double( offsetFromLaser.getX(), offsetFromLaser.getY() );
+            setArrowStroke( VECTOR_STROKE );
+            setArrowFillPaint( null );
+            setUnits( ELECTRIC_FIELD_UNITS );
         }
         
         public Point2D getOffsetFromLaser() {
