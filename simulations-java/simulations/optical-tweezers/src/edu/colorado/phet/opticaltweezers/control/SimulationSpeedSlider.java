97b778f07d1f595a63399c3744934e2007e29977
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-06-27 23:21:19 +0000

    6/27/07 - add log mapping strategies to convert simulation speed sliders value to model (time) values

--- simulations-java/simulations/optical-tweezers/src/edu/colorado/phet/opticaltweezers/control/SimulationSpeedSlider.java
+++ simulations-java/simulations/optical-tweezers/src/edu/colorado/phet/opticaltweezers/control/SimulationSpeedSlider.java
@@ -2,7 +2,10 @@
 
 package edu.colorado.phet.opticaltweezers.control;
 
-import java.awt.*;
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Paint;
+import java.awt.Rectangle;
 import java.awt.geom.GeneralPath;
 import java.awt.geom.Line2D;
 import java.awt.geom.Rectangle2D;
@@ -15,6 +18,7 @@ import javax.swing.event.ChangeListener;
 import javax.swing.event.EventListenerList;
 
 import edu.colorado.phet.common.phetcommon.util.DoubleRange;
+import edu.colorado.phet.common.phetcommon.view.controls.valuecontrol.LogarithmicMappingStrategy;
 import edu.colorado.phet.common.phetcommon.view.util.PhetDefaultFont;
 import edu.colorado.phet.common.piccolophet.event.BoundedDragHandler;
 import edu.colorado.phet.common.piccolophet.event.CursorHandler;
@@ -44,6 +48,17 @@ public class SimulationSpeedSlider extends PNode {
     // Class data
     //----------------------------------------------------------------------------
     
+    // Background
+    private static final int BACKGROUND_HEIGHT = 30;
+    private static final int SLOW_BACKGROUND_WIDTH = 80;
+    private static final int BETWEEN_BACKGROUND_WIDTH = 40;
+    private static final int FAST_BACKGROUND_WIDTH = 80;
+    private static final Color BACKGROUND_STROKE_COLOR = Color.BLACK;
+    private static final int BACKGROUND_STROKE_WIDTH = 1;
+    private static final Color SLOW_FILL_COLOR = Color.GREEN;
+    private static final Color FAST_FILL_COLOR = Color.RED;
+    private static final Color BETWEEN_FILL_COLOR = Color.DARK_GRAY;
+    
     // Track
     private static final int TRACK_HEIGHT = 2;
     private static final Color TRACK_FILL_COLOR = Color.BLACK;
@@ -52,27 +67,19 @@ public class SimulationSpeedSlider extends PNode {
     
     // Knob
     private static final int KNOB_WIDTH = 15;
-    private static final int KNOB_HEIGHT = 40;
+    private static final int KNOB_HEIGHT = (int)( 0.8 * BACKGROUND_HEIGHT );
     private static final Color KNOB_FILL_COLOR = Color.WHITE;
     private static final Color KNOB_STROKE_COLOR = Color.BLACK;
     private static final int KNOB_STROKE_WIDTH = 1;
     
-    // Background
-    private static final int BACKGROUND_HEIGHT = 50;
-    private static final int SLOW_BACKGROUND_WIDTH = 160;
-    private static final int BETWEEN_BACKGROUND_WIDTH = 80;
-    private static final int FAST_BACKGROUND_WIDTH = 160;
-    private static final Color BACKGROUND_STROKE_COLOR = Color.BLACK;
-    private static final int BACKGROUND_STROKE_WIDTH = 1;
-    private static final Color SLOW_FILL_COLOR = Color.BLUE;
-    private static final Color FAST_FILL_COLOR = Color.RED;
-    private static final Color BETWEEN_FILL_COLOR = Color.DARK_GRAY;
-    
     // Labels
     private static final int LABEL_FONT_SIZE = 14;
     private static final int LABEL_Y_SPACING = 5;
-    private static final Color SLOW_LABEL_FONT_COLOR = SLOW_FILL_COLOR;
-    private static final Color FAST_LABEL_FONT_COLOR = FAST_FILL_COLOR;
+    private static final Color SLOW_LABEL_FONT_COLOR = Color.BLACK;
+    private static final Color FAST_LABEL_FONT_COLOR = Color.BLACK;
+    
+    // Value
+    private static final DecimalFormat VALUE_FORMAT = new DecimalFormat( "0.0E00" );
     
     // Tick marks
     private static final int TICK_MARK_LENGTH = 10;
@@ -80,7 +87,7 @@ public class SimulationSpeedSlider extends PNode {
     private static final int TICK_MARK_STROKE_WIDTH = 1;
     
     // Tick labels
-    private static final int TICK_LABEL_FONT_SIZE = 14;
+    private static final int TICK_LABEL_FONT_SIZE = 10;
     private static final Color TICK_LABEL_COLOR = Color.BLACK;
     private static final DecimalFormat TICK_LABEL_FORMAT = new DecimalFormat( "0E00" );
     private static final int TICK_LABEL_Y_SPACING = 2;
@@ -98,6 +105,10 @@ public class SimulationSpeedSlider extends PNode {
     private EventListenerList _listenerList;
     private boolean _isAdjusting;
     
+    private LogarithmicMappingStrategy _slowMappingStrategy;
+    private LogarithmicMappingStrategy _fastMappingStrategy;
+    private LogarithmicMappingStrategy _betweenMappingStrategy;
+    
     //----------------------------------------------------------------------------
     // Constructors & initializers
     //----------------------------------------------------------------------------
@@ -105,8 +116,12 @@ public class SimulationSpeedSlider extends PNode {
     public SimulationSpeedSlider( DoubleRange slowRange, DoubleRange fastRange, double value ) {
         super();
         
-        assert( slowRange.getMax() < fastRange.getMin() );
-        assert( slowRange.contains( value ) || fastRange.contains( value ) );
+        if ( !( slowRange.getMax() < fastRange.getMin() ) ) {
+            throw new IllegalArgumentException( "slowRange and fastRange overlap" );
+        }
+        if ( !( slowRange.contains( value ) || fastRange.contains( value ) ) ) {
+            throw new IllegalArgumentException( "value out of range: " + value );
+        }
         
         _slowRange = slowRange;
         _fastRange = fastRange;
@@ -114,10 +129,20 @@ public class SimulationSpeedSlider extends PNode {
         
         _listenerList = new EventListenerList();
         
+        // strategies for mapping between model and view coordinates
+        _slowMappingStrategy = new LogarithmicMappingStrategy( _slowRange.getMin(), _slowRange.getMax(), 0, SLOW_BACKGROUND_WIDTH );
+        _fastMappingStrategy = new LogarithmicMappingStrategy( _fastRange.getMin(), _fastRange.getMax(), 0, FAST_BACKGROUND_WIDTH );
+        _betweenMappingStrategy = new LogarithmicMappingStrategy( _slowRange.getMax(), _fastRange.getMin(), 0, BETWEEN_BACKGROUND_WIDTH );
+        
         initNodes();
         initInteractivity();
+        
+        updateKnobPosition();
     }
     
+    /*
+     * Creates and performs layout of all nodes.
+     */
     private void initNodes() {
         
         String slowString = OTResources.getString( "label.slow" );
@@ -187,7 +212,6 @@ public class SimulationSpeedSlider extends PNode {
             slowTickMarkMinNode.setOffset( x, y );
             
             x = slowBackgroundNode.getFullBounds().getMaxX();
-            y = slowTickMarkMinNode.getYOffset() - slowTickMarkMaxNode.getFullBounds().getWidth();
             slowTickMarkMaxNode.setOffset( x, y );
             
             x = fastBackgroundNode.getXOffset();
@@ -195,7 +219,6 @@ public class SimulationSpeedSlider extends PNode {
             fastTickMarkMinNode.setOffset( x, y );
             
             x = fastBackgroundNode.getFullBounds().getMaxX() - fastTickMarkMaxNode.getFullBounds().getWidth();
-            y = fastTickMarkMinNode.getYOffset();
             fastTickMarkMaxNode.setOffset( x, y );
             
             x = slowTickMarkMinNode.getXOffset() - ( slowTickLabelMinNode.getFullBounds().getWidth() / 2 );
@@ -250,6 +273,9 @@ public class SimulationSpeedSlider extends PNode {
         fastTickLabelMaxNode.setPickable( false );
     }
     
+    /*
+     * Creates one of the background sections (slow, fast, in-between).
+     */
     private static PPath createBackgroundNode( int width, Paint fillPaint ) {
         int w = width - BACKGROUND_STROKE_WIDTH;
         int h = BACKGROUND_HEIGHT - BACKGROUND_STROKE_WIDTH;
@@ -261,6 +287,9 @@ public class SimulationSpeedSlider extends PNode {
         return node;
     }
     
+    /*
+     * Creates the slider track node.
+     */
     private static PPath createTrackNode() {
         int width = ( SLOW_BACKGROUND_WIDTH + BETWEEN_BACKGROUND_WIDTH + FAST_BACKGROUND_WIDTH ) - TRACK_STROKE_WIDTH;
         int height = Math.max( 1, TRACK_HEIGHT - TRACK_STROKE_WIDTH );
@@ -272,6 +301,9 @@ public class SimulationSpeedSlider extends PNode {
         return trackNode;
     }
     
+    /*
+     * Creates the slider knob node.
+     */
     private static PPath createKnobNode() {
         float w = KNOB_WIDTH - KNOB_STROKE_WIDTH;
         float h = KNOB_HEIGHT - KNOB_STROKE_WIDTH;
@@ -290,6 +322,9 @@ public class SimulationSpeedSlider extends PNode {
         return knob;
     }
     
+    /*
+     * Create a tick mark node.
+     */
     private static PPath createTickMarkNode() {
         Line2D tickPath = new Line2D.Double( 0, 0, 0, TICK_MARK_LENGTH );
         PPath tickNode = new PPath( tickPath );
@@ -298,6 +333,9 @@ public class SimulationSpeedSlider extends PNode {
         return tickNode;
     }
     
+    /*
+     * Creates a tick label node.
+     */
     private static PText createTickLabelNode( double value ) {
         PText textNode = new PText();
         textNode.setText( TICK_LABEL_FORMAT.format( value ) );
@@ -306,22 +344,25 @@ public class SimulationSpeedSlider extends PNode {
         return textNode;
     }
     
+    /*
+     * Adds interactivity to the knob.
+     */
     private void initInteractivity() {
         
-        // constrain knob to be dragged horizontally within the track
+        // Constrain the knob to be dragged horizontally within the track
         double xMin = _trackNode.getFullBounds().getX() - ( _knobNode.getFullBounds().getWidth() / 2 );
         double xMax = _trackNode.getFullBounds().getMaxX() + ( _knobNode.getFullBounds().getWidth() / 2 );
         double yMin = _trackNode.getFullBounds().getY() + ( _trackNode.getFullBounds().getHeight() / 2 ) - ( _knobNode.getFullBounds().getHeight() / 2 );
         double yMax = _trackNode.getFullBounds().getY() + ( _trackNode.getFullBounds().getHeight() / 2 ) + ( _knobNode.getFullBounds().getHeight() / 2 );
         Rectangle2D dragBounds = new Rectangle2D.Double( xMin, yMin, xMax - xMin, yMax - yMin );
-        
         PPath dragBoundsNode = new PPath( dragBounds );
         dragBoundsNode.setStroke( null );
         addChild( dragBoundsNode );
-        
         BoundedDragHandler dragHandler = new BoundedDragHandler( _knobNode, dragBoundsNode );
         _knobNode.addInputEventListener( dragHandler );
         _knobNode.addInputEventListener( new CursorHandler() );
+        
+        // Update the value when the knob is moved.
         _knobNode.addPropertyChangeListener( new PropertyChangeListener() {
             public void propertyChange( PropertyChangeEvent event ) {
                 if ( event.getPropertyName().equals( PNode.PROPERTY_TRANSFORM ) ) {
@@ -330,13 +371,14 @@ public class SimulationSpeedSlider extends PNode {
             }
         } );
         
+        // Change the "adjusting" start on mouse press & release.
         _knobNode.addInputEventListener( new PBasicInputEventHandler() {
             public void mousePressed( PInputEvent event ) {
                 _isAdjusting = true;
             }
-
             public void mouseReleased( PInputEvent event ) {
                 _isAdjusting = false;
+                snapToRange();
                 fireChangeEvent( new ChangeEvent( this ) );
             }
         } );
@@ -346,8 +388,13 @@ public class SimulationSpeedSlider extends PNode {
     // Setters and getters
     //----------------------------------------------------------------------------
     
+    /**
+     * Sets the slider value.
+     * 
+     * @param value the value, in model coordinates
+     */
     public void setValue( double value ) {
-        if ( !isValidValue( value ) ) {
+        if ( ! (_slowRange.contains( value ) || _fastRange.contains( value ) ) ) {
             throw new IllegalArgumentException( "value out of range: " + value );
         }
         if ( value != _value ) {
@@ -357,33 +404,143 @@ public class SimulationSpeedSlider extends PNode {
         }
     }
     
+    /**
+     * Gets the slider value
+     * 
+     * @return the value, in model coordinates
+     */
     public double getValue() { 
         return _value;
     }
     
+    /**
+     * Gets the slider value, formatted as a string.
+     * 
+     * @return
+     */
     public String getFormattedValue() {
-        return TICK_LABEL_FORMAT.format( _value );
+        return VALUE_FORMAT.format( _value );
     }
     
+    /**
+     * Is the value currently being adjusted?
+     * Returns true while the knob is being dragged. 
+     * 
+     * @return true or false
+     */
     public boolean isAdjusting() {
         return _isAdjusting;
     }
     
-    private boolean isValidValue( double value ) {
-        return ( _slowRange.contains( value ) || _fastRange.contains( value ) );
+    /*
+     * Sets the slider's value in view coordinates.
+     * This is x offset of the knob with respect to the track.
+     */
+    private void setSliderValue( int sliderValue ) {
+        _knobNode.setOffset( sliderValue - ( _knobNode.getFullBounds().getWidth() / 2 ), _knobNode.getYOffset() );
+    }
+    
+    /*
+     * Gets the slider's value in view coordinates.
+     * This is x offset of the knob with respect to the track.
+     */
+    private int getSliderValue() {
+        // Read the knob offset in slider coordinates
+        int xOffset = (int)( _knobNode.getXOffset() + ( _knobNode.getFullBounds().getWidth() / 2 ) );
+        if ( xOffset < 0 ) {
+            xOffset = 0;
+        }
+        else if ( xOffset > SLOW_BACKGROUND_WIDTH + BETWEEN_BACKGROUND_WIDTH + FAST_BACKGROUND_WIDTH ) {
+            xOffset = SLOW_BACKGROUND_WIDTH + BETWEEN_BACKGROUND_WIDTH + FAST_BACKGROUND_WIDTH;
+        }
+        return xOffset;
+    }
+    
+    /*
+     * Is the slider knob in the "slow" range?
+     */
+    private boolean isKnobInSlowRange( int sliderValue ) {
+        return ( sliderValue >= 0 && 
+                ( sliderValue <= SLOW_BACKGROUND_WIDTH ) );
+    }
+    
+    /*
+     * Is the slider knob in the range between "slow" and "fast"?
+     */
+    private boolean isKnobInBetweenRange( int sliderValue ) {
+        return ( ( sliderValue > SLOW_BACKGROUND_WIDTH ) && 
+                ( sliderValue <= SLOW_BACKGROUND_WIDTH + BETWEEN_BACKGROUND_WIDTH ) );
+    }
+    
+    /*
+     * Is the slider knob in the "fast" range?
+     */
+    private boolean isKnobInFastRange( int sliderValue ) {
+        return ( ( sliderValue > SLOW_BACKGROUND_WIDTH + BETWEEN_BACKGROUND_WIDTH ) && 
+                ( sliderValue <= SLOW_BACKGROUND_WIDTH + BETWEEN_BACKGROUND_WIDTH + FAST_BACKGROUND_WIDTH ) );
     }
     
     //----------------------------------------------------------------------------
     // Updaters
     //----------------------------------------------------------------------------
     
+    /*
+     * Updates the model value to match the position of the knob.
+     */
     private void updateValue() {
-        //XXX
+        
+        // Read the knob offset in slider coordinates
+        int sliderValue = getSliderValue();
+        
+        // Convert to a value in model coordinates
+        if ( isKnobInSlowRange( sliderValue ) ) {
+            _value = _slowMappingStrategy.sliderToModel( sliderValue );
+        }
+        else if ( isKnobInBetweenRange( sliderValue ) ) {
+            _value = _betweenMappingStrategy.sliderToModel( sliderValue - SLOW_BACKGROUND_WIDTH );
+        }
+        else if ( isKnobInFastRange( sliderValue ) ) {
+            _value = _fastMappingStrategy.sliderToModel( sliderValue - SLOW_BACKGROUND_WIDTH - BETWEEN_BACKGROUND_WIDTH );
+        }
+        else {
+            throw new IllegalStateException( "knob is out of range, programming error?" );
+        }
+        
+        // Notify listeners that the value has changed
         fireChangeEvent( new ChangeEvent( this ) );
     }
     
+    /*
+     * Updates the knob position to match the current model value.
+     */
     private void updateKnobPosition() {
-        //XXX
+        int sliderValue = 0;
+        if ( _value <= _slowRange.getMax() ) {
+            sliderValue = _slowMappingStrategy.modelToSlider( _value );
+        }
+        else if ( _value < _fastRange.getMin() ) {
+            sliderValue = SLOW_BACKGROUND_WIDTH + _betweenMappingStrategy.modelToSlider( _value );
+        }
+        else {
+            sliderValue = SLOW_BACKGROUND_WIDTH + BETWEEN_BACKGROUND_WIDTH + _fastMappingStrategy.modelToSlider( _value );
+        }
+        setSliderValue( sliderValue );
+    }
+    
+    /*
+     * If the slider is in the range between "slow" and "fast",
+     * snap it to the closest of slow.max or fast.min.
+     */
+    private void snapToRange() {
+        int sliderValue = getSliderValue();
+        if ( isKnobInBetweenRange( sliderValue ) ) {
+            if ( ( sliderValue - SLOW_BACKGROUND_WIDTH < ( BETWEEN_BACKGROUND_WIDTH / 2 ) ) ) {
+                setValue( _slowRange.getMax() );
+            }
+            else {
+                setValue( _fastRange.getMin() );
+            }  
+        }
     }
     
     //----------------------------------------------------------------------------
