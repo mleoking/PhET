bd315a7a2462da4dada4fa9ed2fd9f32f9fddb07
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-06-27 18:29:34 +0000

    6/27/07 - resolve roundoff problems in simulation speed slider by using integer precision for layout

--- trunk/simulations-java/simulations/optical-tweezers/src/edu/colorado/phet/opticaltweezers/control/SimulationSpeedSlider.java
+++ trunk/simulations-java/simulations/optical-tweezers/src/edu/colorado/phet/opticaltweezers/control/SimulationSpeedSlider.java
@@ -32,45 +32,46 @@ public class SimulationSpeedSlider extends PNode {
     // Class data
     //----------------------------------------------------------------------------
     
-    private static final Dimension BACKGROUND_SIZE = new Dimension( 400, 50 );
-    private static final double SLOW_BACKGROUND_PERCENT = 0.4;
-    private static final double FAST_BACKGROUND_PERCENT = 0.4;
-    private static final double TRACK_HEIGHT = 2;
-    private static final Dimension KNOB_SIZE = new Dimension( 15, 40 );
-    private static final double TICK_LENGTH = 10;
-    
-    private static final int FONT_SIZE = 14;
-    private static final int SLOW_FAST_Y_SPACING = 5;
-    private static final int TICK_LABEL_Y_SPACING = 2;
-    
+    // Track
+    private static final int TRACK_HEIGHT = 2;
     private static final Color TRACK_FILL_COLOR = Color.BLACK;
     private static final Color TRACK_STROKE_COLOR = Color.BLACK;
-    private static final BasicStroke TRACK_STROKE = new BasicStroke( 1f );
+    private static final int TRACK_STROKE_WIDTH = 1;
     
+    // Knob
+    private static final int KNOB_WIDTH = 15;
+    private static final int KNOB_HEIGHT = 40;
     private static final Color KNOB_FILL_COLOR = Color.WHITE;
     private static final Color KNOB_STROKE_COLOR = Color.BLACK;
-    private static final BasicStroke KNOB_STROKE = new BasicStroke( 1f );
+    private static final int KNOB_STROKE_WIDTH = 1;
     
-    private static final Font SLOW_FONT = new PhetDefaultFont( FONT_SIZE );
-    private static final Color SLOW_FONT_COLOR = Color.BLUE;
+    // Background
+    private static final int BACKGROUND_HEIGHT = 50;
+    private static final int SLOW_BACKGROUND_WIDTH = 160;
+    private static final int BETWEEN_BACKGROUND_WIDTH = 80;
+    private static final int FAST_BACKGROUND_WIDTH = 160;
+    private static final Color BACKGROUND_STROKE_COLOR = Color.BLACK;
+    private static final int BACKGROUND_STROKE_WIDTH = 1;
     private static final Color SLOW_FILL_COLOR = Color.BLUE;
-    private static final Color SLOW_STROKE_COLOR = Color.BLACK;
-    private static final BasicStroke SLOW_STROKE = new BasicStroke( 1f );
-    
-    private static final Font FAST_FONT = new PhetDefaultFont( FONT_SIZE );
-    private static final Color FAST_FONT_COLOR = Color.RED;
     private static final Color FAST_FILL_COLOR = Color.RED;
-    private static final Color FAST_STROKE_COLOR = Color.BLACK;
-    private static final BasicStroke FAST_STROKE = new BasicStroke( 1f );
-    
     private static final Color BETWEEN_FILL_COLOR = Color.DARK_GRAY;
-    private static final Color BETWEEN_STROKE_COLOR = Color.BLACK;
-    private static final BasicStroke BETWEEN_STROKE = new BasicStroke( 1f );
     
-    private static final Color TICK_COLOR = Color.BLACK;
-    private static final BasicStroke TICK_STROKE = new BasicStroke( 1f );
-    private static final Font TICK_FONT = new PhetDefaultFont( FONT_SIZE );
-    private static final DecimalFormat TICK_FORMAT = new DecimalFormat( "0E00" );
+    // Labels
+    private static final int LABEL_FONT_SIZE = 14;
+    private static final int LABEL_Y_SPACING = 5;
+    private static final Color SLOW_LABEL_FONT_COLOR = SLOW_FILL_COLOR;
+    private static final Color FAST_LABEL_FONT_COLOR = FAST_FILL_COLOR;
+    
+    // Tick marks
+    private static final int TICK_MARK_LENGTH = 10;
+    private static final Color TICK_MARK_COLOR = Color.BLACK;
+    private static final int TICK_MARK_STROKE_WIDTH = 1;
+    
+    // Tick labels
+    private static final int TICK_LABEL_FONT_SIZE = 14;
+    private static final Color TICK_LABEL_COLOR = Color.BLACK;
+    private static final DecimalFormat TICK_LABEL_FORMAT = new DecimalFormat( "0E00" );
+    private static final int TICK_LABEL_Y_SPACING = 2;
 
     //----------------------------------------------------------------------------
     // Instance data
@@ -109,37 +110,17 @@ public class SimulationSpeedSlider extends PNode {
         String fastString = OTResources.getString( "label.fast" );
         
         PText slowTextNode = new PText( slowString );
-        slowTextNode.setFont( SLOW_FONT );
-        slowTextNode.setTextPaint( SLOW_FONT_COLOR );
+        slowTextNode.setFont( new PhetDefaultFont( LABEL_FONT_SIZE ) );
+        slowTextNode.setTextPaint( SLOW_LABEL_FONT_COLOR );
         
         PText fastTextNode = new PText( fastString );
-        fastTextNode.setFont( FAST_FONT );
-        fastTextNode.setTextPaint( FAST_FONT_COLOR );
+        fastTextNode.setFont( new PhetDefaultFont( LABEL_FONT_SIZE ) );
+        fastTextNode.setTextPaint( FAST_LABEL_FONT_COLOR );
         
-        double width = SLOW_BACKGROUND_PERCENT * ( BACKGROUND_SIZE.getWidth() - ( 2 * SLOW_STROKE.getLineWidth() ) );
-        double height = BACKGROUND_SIZE.getHeight() - ( 2 * SLOW_STROKE.getLineWidth() );
-        Rectangle2D slowBackgroundPath = new Rectangle2D.Double( 0, 0, width, height );
-        PPath slowBackgroundNode = new PPath( slowBackgroundPath);
-        slowBackgroundNode.setPaint( SLOW_FILL_COLOR );
-        slowBackgroundNode.setStroke( SLOW_STROKE );
-        slowBackgroundNode.setStrokePaint( SLOW_STROKE_COLOR );
-        
-        width = FAST_BACKGROUND_PERCENT * ( BACKGROUND_SIZE.getWidth() - ( 2 * SLOW_STROKE.getLineWidth() ) );
-        height = BACKGROUND_SIZE.getHeight() - ( 2 * FAST_STROKE.getLineWidth() );
-        Rectangle2D fastBackgroundPath = new Rectangle2D.Double( 0, 0, width, height );
-        PPath fastBackgroundNode = new PPath( fastBackgroundPath );
-        fastBackgroundNode.setPaint( FAST_FILL_COLOR );
-        fastBackgroundNode.setStroke( FAST_STROKE );
-        fastBackgroundNode.setStrokePaint( FAST_STROKE_COLOR );
-        
-        width = BACKGROUND_SIZE.getWidth() - slowBackgroundNode.getFullBounds().getWidth() - fastBackgroundNode.getFullBounds().getWidth();
-        height = BACKGROUND_SIZE.getHeight() - ( 2 * BETWEEN_STROKE.getLineWidth() );
-        PPath betweenBackgroundNode = new PPath();
-        Rectangle2D betweenBackgroundPath = new Rectangle2D.Double( 0, 0, width, height );
-        betweenBackgroundNode.setPathTo( betweenBackgroundPath );
-        betweenBackgroundNode.setPaint( BETWEEN_FILL_COLOR );
-        betweenBackgroundNode.setStroke( BETWEEN_STROKE );
-        betweenBackgroundNode.setStrokePaint( BETWEEN_STROKE_COLOR );
+        // Background sections
+        PPath slowBackgroundNode = createBackgroundNode( SLOW_BACKGROUND_WIDTH, SLOW_FILL_COLOR );
+        PPath fastBackgroundNode = createBackgroundNode( FAST_BACKGROUND_WIDTH, FAST_FILL_COLOR );
+        PPath betweenBackgroundNode = createBackgroundNode( BETWEEN_BACKGROUND_WIDTH, BETWEEN_FILL_COLOR );
         
         _trackNode = createTrackNode();
         
@@ -158,51 +139,67 @@ public class SimulationSpeedSlider extends PNode {
         PNode fastTickLabelMaxNode = createTickLabelNode( _fastRange.getMax() );
         
         // Positioning
-        double x = 0;
-        double y = 0;
-        slowBackgroundNode.setOffset( x, y );
-        x = slowBackgroundNode.getFullBounds().getMaxX();
-        y = slowBackgroundNode.getYOffset();
-        betweenBackgroundNode.setOffset( x, y );
-        x = betweenBackgroundNode.getFullBounds().getMaxX();
-        y = betweenBackgroundNode.getYOffset();
-        fastBackgroundNode.setOffset( x, y );
-        x = slowBackgroundNode.getXOffset() + ( ( slowBackgroundNode.getFullBounds().getWidth() - slowTextNode.getFullBounds().getWidth() ) / 2 );
-        y = -( slowTextNode.getFullBounds().getHeight() + SLOW_FAST_Y_SPACING );
-        slowTextNode.setOffset( x, y );
-        x = fastBackgroundNode.getXOffset() + ( ( fastBackgroundNode.getFullBounds().getWidth() - fastTextNode.getFullBounds().getWidth() ) / 2 );
-        y = -( fastTextNode.getFullBounds().getHeight() + SLOW_FAST_Y_SPACING );
-        fastTextNode.setOffset( x, y );
-        x = slowBackgroundNode.getXOffset();
-        y = slowBackgroundNode.getYOffset() + ( ( BACKGROUND_SIZE.getHeight() - _trackNode.getFullBounds().getHeight() ) / 2 );
-        _trackNode.setOffset( x, y );
-        x = slowBackgroundNode.getXOffset() - ( _knobNode.getFullBounds().getWidth() / 2 );
-        y = slowBackgroundNode.getYOffset() + ( ( BACKGROUND_SIZE.getHeight() - _knobNode.getFullBounds().getHeight() ) / 2 );
-        _knobNode.setOffset( x, y );
-        x = slowBackgroundNode.getXOffset();
-        y = slowBackgroundNode.getFullBounds().getMaxY();
-        slowTickMarkMinNode.setOffset( x, y );
-        x = slowBackgroundNode.getFullBounds().getMaxX();
-        y = slowTickMarkMinNode.getYOffset() - slowTickMarkMaxNode.getFullBounds().getWidth();
-        slowTickMarkMaxNode.setOffset( x, y );
-        x = fastBackgroundNode.getXOffset();
-        y = fastBackgroundNode.getFullBounds().getMaxY();
-        fastTickMarkMinNode.setOffset( x, y );
-        x = fastBackgroundNode.getFullBounds().getMaxX() - fastTickMarkMaxNode.getFullBounds().getWidth();
-        y = fastTickMarkMinNode.getYOffset();
-        fastTickMarkMaxNode.setOffset( x, y );
-        x = slowTickMarkMinNode.getXOffset() - ( slowTickLabelMinNode.getFullBounds().getWidth() / 2 );
-        y = slowTickMarkMinNode.getFullBounds().getMaxY() + TICK_LABEL_Y_SPACING;
-        slowTickLabelMinNode.setOffset( x, y );
-        x = slowTickMarkMaxNode.getXOffset() - ( slowTickLabelMaxNode.getFullBounds().getWidth() / 2 );
-        y = slowTickMarkMaxNode.getFullBounds().getMaxY() + TICK_LABEL_Y_SPACING;
-        slowTickLabelMaxNode.setOffset( x, y );
-        x = fastTickMarkMinNode.getXOffset() - ( fastTickLabelMinNode.getFullBounds().getWidth() / 2 );
-        y = fastTickMarkMinNode.getFullBounds().getMaxY() + TICK_LABEL_Y_SPACING;
-        fastTickLabelMinNode.setOffset( x, y );
-        x = fastTickMarkMaxNode.getXOffset() - ( fastTickLabelMaxNode.getFullBounds().getWidth() / 2 );
-        y = fastTickMarkMaxNode.getFullBounds().getMaxY() + TICK_LABEL_Y_SPACING;
-        fastTickLabelMaxNode.setOffset( x, y );
+        {
+            double x = 0;
+            double y = 0;
+            slowBackgroundNode.setOffset( x, y );
+            
+            x = SLOW_BACKGROUND_WIDTH;
+            y = slowBackgroundNode.getYOffset();
+            betweenBackgroundNode.setOffset( x, y );
+            
+            x = SLOW_BACKGROUND_WIDTH + BETWEEN_BACKGROUND_WIDTH;
+            y = betweenBackgroundNode.getYOffset();
+            fastBackgroundNode.setOffset( x, y );
+            
+            x = slowBackgroundNode.getXOffset() + ( ( slowBackgroundNode.getFullBounds().getWidth() - slowTextNode.getFullBounds().getWidth() ) / 2 );
+            y = -( slowTextNode.getFullBounds().getHeight() + LABEL_Y_SPACING );
+            slowTextNode.setOffset( x, y );
+            
+            x = fastBackgroundNode.getXOffset() + ( ( fastBackgroundNode.getFullBounds().getWidth() - fastTextNode.getFullBounds().getWidth() ) / 2 );
+            y = -( fastTextNode.getFullBounds().getHeight() + LABEL_Y_SPACING );
+            fastTextNode.setOffset( x, y );
+            
+            x = slowBackgroundNode.getXOffset();
+            y = slowBackgroundNode.getYOffset() + ( ( BACKGROUND_HEIGHT - _trackNode.getFullBounds().getHeight() ) / 2 );
+            _trackNode.setOffset( x, y );
+            
+            x = slowBackgroundNode.getXOffset() - ( _knobNode.getFullBounds().getWidth() / 2 );
+            y = slowBackgroundNode.getYOffset() + ( ( BACKGROUND_HEIGHT - _knobNode.getFullBounds().getHeight() ) / 2 );
+            _knobNode.setOffset( x, y );
+            
+            x = slowBackgroundNode.getXOffset();
+            y = slowBackgroundNode.getFullBounds().getMaxY();
+            slowTickMarkMinNode.setOffset( x, y );
+            
+            x = slowBackgroundNode.getFullBounds().getMaxX();
+            y = slowTickMarkMinNode.getYOffset() - slowTickMarkMaxNode.getFullBounds().getWidth();
+            slowTickMarkMaxNode.setOffset( x, y );
+            
+            x = fastBackgroundNode.getXOffset();
+            y = fastBackgroundNode.getFullBounds().getMaxY();
+            fastTickMarkMinNode.setOffset( x, y );
+            
+            x = fastBackgroundNode.getFullBounds().getMaxX() - fastTickMarkMaxNode.getFullBounds().getWidth();
+            y = fastTickMarkMinNode.getYOffset();
+            fastTickMarkMaxNode.setOffset( x, y );
+            
+            x = slowTickMarkMinNode.getXOffset() - ( slowTickLabelMinNode.getFullBounds().getWidth() / 2 );
+            y = slowTickMarkMinNode.getFullBounds().getMaxY() + TICK_LABEL_Y_SPACING;
+            slowTickLabelMinNode.setOffset( x, y );
+            
+            x = slowTickMarkMaxNode.getXOffset() - ( slowTickLabelMaxNode.getFullBounds().getWidth() / 2 );
+            y = slowTickMarkMaxNode.getFullBounds().getMaxY() + TICK_LABEL_Y_SPACING;
+            slowTickLabelMaxNode.setOffset( x, y );
+            
+            x = fastTickMarkMinNode.getXOffset() - ( fastTickLabelMinNode.getFullBounds().getWidth() / 2 );
+            y = fastTickMarkMinNode.getFullBounds().getMaxY() + TICK_LABEL_Y_SPACING;
+            fastTickLabelMinNode.setOffset( x, y );
+            
+            x = fastTickMarkMaxNode.getXOffset() - ( fastTickLabelMaxNode.getFullBounds().getWidth() / 2 );
+            y = fastTickMarkMaxNode.getFullBounds().getMaxY() + TICK_LABEL_Y_SPACING;
+            fastTickLabelMaxNode.setOffset( x, y );
+        }
         
         // Layering
         addChild( slowTickMarkMinNode );
@@ -239,46 +236,59 @@ public class SimulationSpeedSlider extends PNode {
         fastTickLabelMaxNode.setPickable( false );
     }
     
+    private static PPath createBackgroundNode( int width, Paint fillPaint ) {
+        int w = width - BACKGROUND_STROKE_WIDTH;
+        int h = BACKGROUND_HEIGHT - BACKGROUND_STROKE_WIDTH;
+        Rectangle path = new Rectangle( 0, 0, w, h );
+        PPath node = new PPath( path);
+        node.setPaint( fillPaint );
+        node.setStroke( new BasicStroke( BACKGROUND_STROKE_WIDTH ) );
+        node.setStrokePaint( BACKGROUND_STROKE_COLOR );
+        return node;
+    }
+    
     private static PPath createTrackNode() {
-        double width = BACKGROUND_SIZE.getWidth() - ( 2 * TRACK_STROKE.getLineWidth() );
-        double height = TRACK_HEIGHT;
-        Rectangle2D trackPath = new Rectangle2D.Double( 0, 0, width, height );
+        int width = ( SLOW_BACKGROUND_WIDTH + BETWEEN_BACKGROUND_WIDTH + FAST_BACKGROUND_WIDTH ) - TRACK_STROKE_WIDTH;
+        int height = Math.max( 1, TRACK_HEIGHT - TRACK_STROKE_WIDTH );
+        Rectangle trackPath = new Rectangle( 0, 0, width, height );
         PPath trackNode = new PPath( trackPath );
         trackNode.setPaint( TRACK_FILL_COLOR );
-        trackNode.setStroke( TRACK_STROKE );
+        trackNode.setStroke( new BasicStroke( TRACK_STROKE_WIDTH ) );
         trackNode.setStrokePaint( TRACK_STROKE_COLOR );
         return trackNode;
     }
     
     private static PPath createKnobNode() {
+        float w = KNOB_WIDTH - KNOB_STROKE_WIDTH;
+        float h = KNOB_HEIGHT - KNOB_STROKE_WIDTH;
         GeneralPath knobPath = new GeneralPath();
         knobPath.moveTo( 0f, 0f );
-        knobPath.lineTo( (float) KNOB_SIZE.getWidth(), 0f );
-        knobPath.lineTo( (float) KNOB_SIZE.getWidth(), (float) ( 0.65 * KNOB_SIZE.getHeight() ) );
-        knobPath.lineTo( (float) ( KNOB_SIZE.getWidth() / 2 ), (float) KNOB_SIZE.getHeight() );
-        knobPath.lineTo( 0f, (float) ( 0.65 * KNOB_SIZE.getHeight() ) );
+        knobPath.lineTo( w, 0f );
+        knobPath.lineTo( w, 0.65f * h );
+        knobPath.lineTo( w/2f, h );
+        knobPath.lineTo( 0f, 0.65f * h );
         knobPath.closePath();
         
         PPath knob = new PPath( knobPath );
         knob.setPaint( KNOB_FILL_COLOR );
-        knob.setStroke( KNOB_STROKE );
+        knob.setStroke( new BasicStroke( KNOB_STROKE_WIDTH ) );
         knob.setStrokePaint( KNOB_STROKE_COLOR );
         return knob;
     }
     
     private static PPath createTickMarkNode() {
-        Line2D tickPath = new Line2D.Double( 0, 0, 0, TICK_LENGTH );
+        Line2D tickPath = new Line2D.Double( 0, 0, 0, TICK_MARK_LENGTH );
         PPath tickNode = new PPath( tickPath );
-        tickNode.setStroke( TICK_STROKE );
-        tickNode.setStrokePaint( TICK_COLOR );
+        tickNode.setStroke( new BasicStroke( TICK_MARK_STROKE_WIDTH ) );
+        tickNode.setStrokePaint( TICK_MARK_COLOR );
         return tickNode;
     }
     
     private static PText createTickLabelNode( double value ) {
         PText textNode = new PText();
-        textNode.setText( TICK_FORMAT.format( value ) );
-        textNode.setFont( TICK_FONT );
-        textNode.setTextPaint( TICK_COLOR );
+        textNode.setText( TICK_LABEL_FORMAT.format( value ) );
+        textNode.setFont( new PhetDefaultFont( TICK_LABEL_FONT_SIZE ) );
+        textNode.setTextPaint( TICK_LABEL_COLOR );
         return textNode;
     }
     
@@ -301,7 +311,7 @@ public class SimulationSpeedSlider extends PNode {
     }
     
     public String getFormattedValue() {
-        return TICK_FORMAT.format( _value );
+        return TICK_LABEL_FORMAT.format( _value );
     }
     
     private boolean isValidValue( double value ) {
