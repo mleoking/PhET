c0b7d62bdd92cc4c4f8cae9693083868df04f1b4
jdegoes <jdegoes@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-06-28 21:15:15 +0000

    website: Removed XML encoding info from header of each page, for the benefit of IE6.

--- trunk/website/admin/site-utils.php
+++ trunk/website/admin/site-utils.php
@@ -227,9 +227,16 @@ EOT;
         $prefix = "..";
         
         expire_page_immediately();
-        
+
+/*
+
+    TODO: 
+    
+        Reinsert <?xml version="1.0" encoding="UTF-8"?>    
+            after IE6 is dead
+
+*/
         print <<<EOT
-<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
             <head>
@@ -603,9 +610,15 @@ EOT;
      do_authentication(false);
      
      expire_page_immediately();
-     
+     /*
+
+         TODO: 
+
+             Reinsert <?xml version="1.0" encoding="UTF-8"?>    
+                 after IE6 is dead
+
+     */     
         print <<<EOT
-<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
         <html xmlns="http://www.w3.org/1999/xhtml">
             <head>
