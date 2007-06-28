59a40e132bcd0eb59c77a877e5e49aab5e131233
jdegoes <jdegoes@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-06-28 21:15:15 +0000

    website: Removed XML encoding info from header of each page, for the benefit of IE6.

--- website/index.php
+++ website/index.php
@@ -6,8 +6,16 @@
     include_once("admin/sim-utils.php");
     include_once("admin/web-utils.php");
     
+    /*
+
+        TODO: 
+
+            Reinsert <?xml version="1.0" encoding="UTF-8"?>    
+                after IE6 is dead
+
+    */
+    
     print <<<EOT
-        <?xml version="1.0" encoding="UTF-8"?>
 EOT;
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
