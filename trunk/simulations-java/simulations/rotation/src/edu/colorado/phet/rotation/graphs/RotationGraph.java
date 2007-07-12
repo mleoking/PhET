1e57e2675d797e8075d86fbacc25a054316d3379
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-07-12 17:40:41 +0000

    Added: a vertical axis label on these graphs as in force 1 D.

--- trunk/simulations-java/simulations/rotation/src/edu/colorado/phet/rotation/graphs/RotationGraph.java
+++ trunk/simulations-java/simulations/rotation/src/edu/colorado/phet/rotation/graphs/RotationGraph.java
@@ -4,6 +4,7 @@ import edu.colorado.phet.common.motion.graphs.MotionControlGraph;
 import edu.colorado.phet.common.motion.model.ISimulationVariable;
 import edu.colorado.phet.common.motion.model.ITimeSeries;
 import edu.colorado.phet.common.motion.model.UpdateStrategy;
+import edu.colorado.phet.common.phetcommon.view.util.PhetDefaultFont;
 import edu.colorado.phet.common.piccolophet.PhetPCanvas;
 import edu.colorado.phet.common.timeseries.model.TimeSeriesModel;
 import edu.colorado.phet.rotation.model.RotationModel;
@@ -23,16 +24,14 @@ import java.awt.geom.Rectangle2D;
  */
 public class RotationGraph extends MotionControlGraph {
     public RotationGraph( PhetPCanvas pSwingCanvas, ISimulationVariable simulationVariable, ITimeSeries timeSeries,
-                          String label, String title, double min, double max, Color color, PImage thumb,
+                          String label, String title, String units, double min, double max, Color color, PImage thumb,
                           RotationModel motionModel, boolean editable, TimeSeriesModel timeSeriesModel,
                           UpdateStrategy updateStrategy, double maxDomainValue, RotationPlatform iPositionDriven ) {
         super( pSwingCanvas, simulationVariable, timeSeries,
                label, title, min, max, color, thumb,
                motionModel, editable, timeSeriesModel, updateStrategy, maxDomainValue, iPositionDriven );
         ValueAxis oldRangeAxis = getJFreeChartNode().getChart().getXYPlot().getRangeAxis();
-//        NumberAxis numberAxis = (NumberAxis)getJFreeChartNode().getChart().getXYPlot().getRangeAxis();
-//        numberAxis.setTickUnit( new NumberTickUnit( numberAxis.getRange().getLength() / 3 ) );
-        RotationGraphNumberAxis verticalAxis = new RotationGraphNumberAxis( title );
+        RotationGraphNumberAxis verticalAxis = new RotationGraphNumberAxis( title+" ("+units+")" );
         verticalAxis.setRange( oldRangeAxis.getRange() );
         getJFreeChartNode().getChart().getXYPlot().setRangeAxis( verticalAxis );
     }
@@ -40,44 +39,15 @@ public class RotationGraph extends MotionControlGraph {
     public static class RotationGraphNumberAxis extends NumberAxis {
         public RotationGraphNumberAxis( String title ) {
             super( title );
+            setLabelFont( new PhetDefaultFont( 14, true ) );
         }
 
-        /**
-         * Selects an appropriate tick value for the axis.  The strategy is to
-         * display as many ticks as possible (selected from an array of 'standard'
-         * tick units) without the labels overlapping.
-         *
-         * @param g2       the graphics device.
-         * @param dataArea the area in which the plot should be drawn.
-         * @param edge     the axis location.
-         */
         protected void selectVerticalAutoTickUnit( Graphics2D g2,
                                                    Rectangle2D dataArea,
                                                    RectangleEdge edge ) {
+            //modify the value chosen by the superclass
             super.selectVerticalAutoTickUnit( g2, dataArea, edge );
-            NumberTickUnit unit = getTickUnit();
-            setTickUnit( new NumberTickUnit( unit.getSize() * 2 ), false, false );
-//            double tickLabelHeight = estimateMaximumTickLabelHeight( g2 );
-//
-//            // start with the current tick unit...
-//            TickUnitSource tickUnits = getStandardTickUnits();
-//            TickUnit unit1 = tickUnits.getCeilingTickUnit( getTickUnit() );
-//            double unitHeight = lengthToJava2D( unit1.getSize(), dataArea, edge );
-//
-//            // then extrapolate...
-//            double guess = ( tickLabelHeight / unitHeight ) * unit1.getSize();
-//
-//            NumberTickUnit unit2
-//                    = (NumberTickUnit)tickUnits.getCeilingTickUnit( guess );
-//            double unit2Height = lengthToJava2D( unit2.getSize(), dataArea, edge );
-//
-//            tickLabelHeight = estimateMaximumTickLabelHeight( g2 );
-//            if( tickLabelHeight > unit2Height ) {
-//                unit2 = (NumberTickUnit)tickUnits.getLargerTickUnit( unit2 );
-//            }
-//
-//            setTickUnit( unit2, false, false );
-
+            setTickUnit( new NumberTickUnit( getTickUnit().getSize() * 2 ), false, false );
         }
     }
 }
--- trunk/simulations-java/simulations/rotation/src/edu/colorado/phet/rotation/graphs/RotationGraphSet.java
+++ trunk/simulations-java/simulations/rotation/src/edu/colorado/phet/rotation/graphs/RotationGraphSet.java
@@ -2,7 +2,6 @@ package edu.colorado.phet.rotation.graphs;
 
 import edu.colorado.phet.common.motion.graphs.GraphSuiteSet;
 import edu.colorado.phet.common.motion.graphs.MinimizableControlGraph;
-import edu.colorado.phet.common.motion.graphs.MotionControlGraph;
 import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
 import edu.colorado.phet.common.piccolophet.PhetPCanvas;
 import edu.colorado.phet.rotation.model.RotationBody;
@@ -25,33 +24,33 @@ public class RotationGraphSet extends GraphSuiteSet {
         double maxDomainValue = 30.0;
         MinimizableControlGraph angleGraph = new MinimizableControlGraph( UnicodeUtil.THETA, new RotationGraph(
                 pSwingCanvas, rotationModel.getPlatformAngleVariable(), rotationModel.getPlatformAngleTimeSeries(),
-                UnicodeUtil.THETA, "Angular Position", -Math.PI * 3, Math.PI * 3, Color.blue, new PImage( loadArrow( "blue-arrow.png" ) ),
+                UnicodeUtil.THETA, "Angular Position", "radians", -Math.PI * 3, Math.PI * 3, Color.blue, new PImage( loadArrow( "blue-arrow.png" ) ),
                 rotationModel, true, rotationModel.getTimeSeriesModel(), rotationModel.getPositionDriven(), maxDomainValue, rotationModel.getRotationPlatform() ) );
 
         MinimizableControlGraph velocityGraph = new MinimizableControlGraph( UnicodeUtil.OMEGA, new RotationGraph(
                 pSwingCanvas, rotationModel.getPlatformVelocityVariable(), rotationModel.getPlatformVelocityTimeSeries(),
-                UnicodeUtil.OMEGA, "Angular Velocity", -5, 5, Color.red, new PImage( loadArrow( "red-arrow.png" ) ),
+                UnicodeUtil.OMEGA, "Angular Velocity", "radians/sec", -5, 5, Color.red, new PImage( loadArrow( "red-arrow.png" ) ),
                 rotationModel, true, rotationModel.getTimeSeriesModel(), rotationModel.getVelocityDriven(), maxDomainValue, rotationModel.getRotationPlatform() ) );
 
         MinimizableControlGraph accelGraph = new MinimizableControlGraph( UnicodeUtil.ALPHA, new RotationGraph(
                 pSwingCanvas, rotationModel.getPlatformAccelVariable(), rotationModel.getPlatformAccelTimeSeries(),
-                UnicodeUtil.ALPHA, "Angular Acceleration", -0.001/0.03/0.03, 0.001/0.03/0.03, Color.green, new PImage( loadArrow( "green-arrow.png" ) ),
+                UnicodeUtil.ALPHA, "Angular Acceleration", "radians/sec^2", -0.001/0.03/0.03, 0.001/0.03/0.03, Color.green, new PImage( loadArrow( "green-arrow.png" ) ),
                 rotationModel, true, rotationModel.getTimeSeriesModel(), rotationModel.getAccelDriven(), maxDomainValue, rotationModel.getRotationPlatform() ) );
 
         RotationBody b = rotationModel.getRotationBody( 0 );
         MinimizableControlGraph linearPositionGraph = new MinimizableControlGraph( "x,y", new RotationGraph(
-                pSwingCanvas, b.getXPositionVariable(), b.getXPositionTimeSeries(), "x", "Position", 0, 500, Color.blue,
+                pSwingCanvas, b.getXPositionVariable(), b.getXPositionTimeSeries(), "x", "Position", "m", 0, 500, Color.blue,
                 new PImage( loadArrow( "blue-arrow.png" ) ), rotationModel, false, rotationModel.getTimeSeriesModel(), null, maxDomainValue, null ) );
         linearPositionGraph.getControlGraph().addSeries( "Position", Color.red, "y", b.getYPositionVariable(), b.getYPositionTimeSeries() );
 
         MinimizableControlGraph linearVelocityGraph = new MinimizableControlGraph( "v<sub>x</sub>,v<sub>y</sub>", new RotationGraph(
-                pSwingCanvas, b.getXVelocityVariable(), b.getXVelocityTimeSeries(), "vx", "Velocity (x)", -10/0.03*3, 10/0.03*3, Color.blue,
+                pSwingCanvas, b.getXVelocityVariable(), b.getXVelocityTimeSeries(), "vx", "Velocity (x)", "m/s", -10/0.03*3, 10/0.03*3, Color.blue,
                 new PImage( loadArrow( "blue-arrow.png" ) ), rotationModel, false, rotationModel.getTimeSeriesModel(), null, maxDomainValue, null ) );
         linearVelocityGraph.getControlGraph().addSeries( "Velocity (y)", Color.red, "vy", b.getYVelocityVariable(), b.getYVelocityTimeSeries() );
         linearVelocityGraph.getControlGraph().addSeries( "|Velocity|", Color.green, "|v|", b.getSpeedVariable(), b.getSpeedSeries() );
 
         MinimizableControlGraph centripetalAccelGraph = new MinimizableControlGraph( "a<sub>x</sub>,a<sub>y</sub>", new RotationGraph(
-                pSwingCanvas, b.getXAccelVariable(), b.getXAccelTimeSeries(), "ax", "Acceleration (x)", -1/0.03/0.03, 1/0.03/0.03, Color.green,
+                pSwingCanvas, b.getXAccelVariable(), b.getXAccelTimeSeries(), "ax", "Acceleration (x)", "m/s^2", -1/0.03/0.03, 1/0.03/0.03, Color.green,
                 new PImage( loadArrow( "green-arrow.png" ) ), rotationModel, false, rotationModel.getTimeSeriesModel(), null, maxDomainValue, null ) );
         centripetalAccelGraph.getControlGraph().addSeries( "Acceleration (y)", Color.red, "ay", b.getYAccelVariable(), b.getYAccelTimeSeries() );
         centripetalAccelGraph.getControlGraph().addSeries( "|Acceleration|", Color.blue, "a", b.getAccelMagnitudeVariable(), b.getAccelMagnitudeSeries() );
