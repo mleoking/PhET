5518e0b81689dc75ff8a488ae20b0e1c55670d34
jdegoes <jdegoes@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-07-12 16:20:00 +0000

    installer: Changed installer builder to use new JNLP code. Fixed some bugs in JNLP library.

--- trunk/phet-installer/new-automated/global.php
+++ trunk/phet-installer/new-automated/global.php
@@ -1,21 +1,20 @@
 <?php
 
-function flushing_echo($text) {
-    echo "$text\n<br/>";
-    flush();
-    ob_flush();
-}
+	function flushing_echo($text) {
+	    echo "$text\n<br/>";
+	    flush();
+	}
 
-function is_cmd_line_option_enabled($attr) {
-    $args = $_SERVER['argv'];
+	function is_cmd_line_option_enabled($attr) {
+	    $args = $_SERVER['argv'];
     
-    foreach ($args as $arg) {
-        if (preg_match('/\-+'.$attr.'/', $arg)) {
-            return true;
-        }
-    }
+	    foreach ($args as $arg) {
+	        if (preg_match('/\-+'.preg_quote($attr).'\b/', $arg)) {
+	            return true;
+	        }
+	    }
     
-    return false;
-}
+	    return false;
+	}
 
 ?>
