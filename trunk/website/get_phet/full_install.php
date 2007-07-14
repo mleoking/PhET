e3c87d31c5b8180291d8f1900a68d6c450f7c211
jdegoes <jdegoes@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-07-14 03:52:53 +0000

    website: Conversion script completed. Installer pages updated.

--- trunk/website/get_phet/full_install.php
+++ trunk/website/get_phet/full_install.php
@@ -6,55 +6,19 @@
         ?>
             <h1>Full PhET Installation</h1>
 
-            <table id="get_phet" cellspacing="0" summary="">
-                <caption>
-                    These installers of the PhET Simulations was released on: 1/2/2007. For changes, see <a href="/">What's New</a>. **We suggest uninstalling your earlier versions of PhET before installing.
-                </caption>
+            <p>
+            	These installers of the PhET Simulations are updated weekly. We suggest uninstalling your earlier versions of PhET before installing.
+            </p>
 
-                <tr>
-                    <th scope="row" abbr="" class="specalt-none">
-                        <center>
-                            Choose your computer platform
-                        </center>
-                    </th>
-
-                    <th scope="row" abbr="" class="specalt-none">
-                        <center>
-                            Full PhET Installation
-                        </center>
-                    </th>
-                </tr>
-
-                <tr>
-                    <th scope="row" abbr="" class="spec">
-                        <ul>
-                            <li>PC with Windows and Java 1.4 or later</li>
-
-                            <li>Mac with OS 10.3.9 or later</li>
-
-                            <li>Linux</li>
-
-                            <li>Other UNIX</li>
-                        </ul>
-                    </th>
-
-                    <th scope="row" abbr="" class="spec"><a href="/"><img src="../images/checkbox.jpg" />&nbsp;download (approx. 40MB)</a></th>
-                </tr>
-
-                <tr>
-                    <th scope="row" abbr="" class="spec">
-                        <ul>
-                            <li>PC with Windows that does NOT have Java 1.4 or later on it</li>
-                        </ul>
-                    </th>
-
-                    <th scope="row" abbr="" class="spec"><a href="/"><img src="../images/checkbox.jpg" />&nbsp;download (approx. 65MB)</a></th>
-                </tr>
-            </table>
+			<ul>
+				<li><a href="../../phet-dist/PhET-1.0-windows.exe">Download installer for Windows</a> - 55 MB</li>
+				
+				<li><a href="../../phet-dist/PhET-1.0-osx.zip">Download installer for Mac OS X</a> - 39 MB</li>
+				
+				<li><a href="../../phet-dist/PhET-1.0-linux.bin">Download installer for Linux</a> - 39 MB</li>
+			</ul>
             
-            <br/>
-
-            <p>For instructions on creating your own installation CD, <a href="/"><u>click here</u></a>.</p>
+            <hr/>
 
             <p>If you have an Internet connection that is too slow, we can send you a CD with the full installation package. Click <a href="../about/contact.php"><u>here to contact us</u></a>.</p>
 
@@ -70,46 +34,14 @@
 
             <p>You can make a CD-ROM for doing the full PhET installation on computers without Internet access. You will need a computer equipped with a CD writer, a blank CD, and an Internet connection. Please <a href="../about/contact.php">contact PhET</a> help with any questions.</p>
 
-            <p>Follow the instructions for your computer type below:</p>
-
-            <h3>For Windows Computers</h3>
+            <p>Follow the instructions below:</p>
 
             <ul>
-                <li>Identify whether you need the installer with or without Java - if in doubt, use the installer with Java.</li>
+                <li>Download the <a href="../../phet-dist/PhET-1.0-CD-ROM.zip">PhET CD-ROM Distribution archive</a> onto your computer (140 MB).</li>
 
-                <li>From the downloads page (click on the "Click Here to Download" button, above), right click on the appropriate link and select "Save Target As..." (your web browser may vary) to save the "install.exe" file to your computer.</li>
+                <li>Extract the contents of the archive (on most operating systems, this is done by double-clicking the downloaded file).</li>
 
-                <li>Using the CD-writing software on your computer, write the "install.exe" file you have just downloaded onto the blank CD-ROM and label it "PhET Install Disk" along with the installer's date (noted on the PhET web site front page).</li>
-
-                <li>You can now use this CD-ROM to install PhET by inserting the CD-ROM and double-clicking on the "install.exe" file.</li>
-
-                <li>Please remember to periodically check for updated versions of the PhET simulations.</li>
-            </ul>
-
-            <h3>For Macintosh Computers</h3>
-
-            <ul>
-                <li>Please note that PhET requires Mac OS X 10.2 or later</li>
-
-                <li>From the downloads page (click on the "Click Here to Download" button, above), right click on the link to the Macintosh installer and select "Save Target As..." (your web browser may vary) to save the "install.zip" file to your computer.</li>
-
-                <li>Using the CD-writing software on your computer, write the "install.zip" file you have just downloaded onto the blank CD-ROM and label it "PhET Install Disk" along with the installer's date (noted on the PhET web site front page).</li>
-
-                <li>You can now use this CD-ROM to install PhET by inserting the CD-ROM and double-clicking on the "install" file. The compressed installer should be recognized by StuffIt Expander and should automatically be expanded after downloading. If it is not expanded, you can expand it manually using StuffIt Expander 6.0 or later.</li>
-
-                <li>If you have any problems launching the installer once it has been expanded, make sure that the compressed installer was expanded using StuffIt Expander. If you continue to have problems, please contact technical support.</li>
-
-                <li>Please remember to periodically check for updated versions of the PhET simulations.</li>
-            </ul>
-
-            <h3>For UNIX/Linux Computers</h3>
-
-            <ul>
-                <li>From the downloads page (click on the "Click Here to Download" button, above), right click on the link to the appropriate Linux or UNIX installer and select "Save Target As..." (your web browser may vary) to save the "install.bin" file to your computer.</li>
-
-                <li>Using the CD-writing software on your computer, write the "install.bin" file you have just downloaded onto the blank CD-ROM and label it "PhET Install Disk" along with the installer's date (noted on the PhET web site front page).</li>
-
-                <li>You can now use this CD-ROM to install PhET by opening a shell, changing to the CD-ROM directory and typing: <strong>sh ./install.bin</strong></li>
+                <li>Copy all the files to the CD-ROM.</li>
 
                 <li>Please remember to periodically check for updated versions of the PhET simulations.</li>
             </ul>
--- trunk/website/get_phet/simlauncher.php
+++ trunk/website/get_phet/simlauncher.php
@@ -6,53 +6,7 @@
         ?>
             <h1>Individual Simulation Installers</h1>
 
-            <table id="get_phet" cellspacing="0" summary="">
-                <caption>
-                    These installers of the PhET Simulations were released on: 1/2/2007. For changes, see <a href="/">What's New</a>. **We suggest uninstalling your earlier versions of PhET before installing.
-                </caption>
-
-                <tr>
-                    <th scope="row" abbr="" class="specalt-none">
-                        <center>
-                            Choose your computer platform
-                        </center>
-                    </th>
-
-                    <th scope="row" abbr="" class="specalt-none">
-                        <center>
-                            Full PhET Installation
-                        </center>
-                    </th>
-                </tr>
-
-                <tr>
-                    <th scope="row" abbr="" class="spec">
-                        <ul>
-                            <li>PC with Windows and Java 1.4 or later</li>
-
-                            <li>Mac with OS 10.3.9 or later</li>
-
-                            <li>Linux</li>
-
-                            <li>Other UNIX</li>
-                        </ul>
-                    </th>
-
-                    <th scope="row" abbr="" class="spec"><a href="/"><img src="../images/checkbox.jpg" />&nbsp;download (approx. 40MB)</a></th>
-                </tr>
-
-                <tr>
-                    <th scope="row" abbr="" class="spec">
-                        <ul>
-                            <li>PC with Windows that does NOT have Java 1.4 or later on it</li>
-                        </ul>
-                    </th>
-
-                    <th scope="row" abbr="" class="spec"><a href="/"><img src="../images/checkbox.jpg" />&nbsp;download (approx. 65MB)</a></th>
-                </tr>
-            </table>
-
-            <p>For instructions on creating your own installation CD, <a href="simlauncher.php"><u>click here</u></a>.</p>
+            <p>TO BE POSTED SHORTLY.</p>
 
             <p>If you have an Internet connection that is too slow, we can send you a CD with the full installation package. Click <a href="../about/contact.php"><u>here to contact us</u></a>.</p>
         <?php
