9d63cee861c99c0739b14256c81a1e1631005465
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-07-17 18:47:59 +0000

    “Show symbol key” button switches to “Hide symbol key” if symbol key is showing.

--- trunk/simulations-java/simulations/rotation/src/edu/colorado/phet/rotation/controls/SymbolKeyButton.java
+++ trunk/simulations-java/simulations/rotation/src/edu/colorado/phet/rotation/controls/SymbolKeyButton.java
@@ -1,10 +1,13 @@
 package edu.colorado.phet.rotation.controls;
 
+import edu.colorado.phet.common.phetcommon.view.MultiStateButton;
 import edu.colorado.phet.common.phetcommon.view.util.SwingUtils;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 /**
  * Author: Sam Reid
@@ -12,11 +15,16 @@ import java.awt.event.ActionListener;
  */
 public class SymbolKeyButton extends JPanel {
     private JDialog dialog;
+    private String SHOW_MODE = "SHOW";
+    private String HIDE_MODE = "HIDE";
 
     public SymbolKeyButton( JFrame parentFrame ) {
         dialog = new JDialog( parentFrame, "Symbol Key", false );
-        final JButton button = new JButton( "Show Symbol Key" );
-        button.addActionListener( new ActionListener() {
+        final MultiStateButton button = new MultiStateButton();
+        button.addMode( SHOW_MODE, "Symbol Key", null );
+        button.addMode( HIDE_MODE, "Hide Symbol Key", null );
+
+        button.addActionListener( SHOW_MODE, new ActionListener() {
             public void actionPerformed( ActionEvent e ) {
                 SwingUtils.centerDialogInParent( dialog );
                 dialog.show();
@@ -24,6 +32,17 @@ public class SymbolKeyButton extends JPanel {
                     JComponent jComponent = (JComponent)dialog.getContentPane();
                     jComponent.paintImmediately( 0, 0, jComponent.getWidth(), jComponent.getHeight() );
                 }
+            }
+        } );
+
+        dialog.addWindowListener( new WindowAdapter() {
+            public void windowClosing( WindowEvent e ) {
+                button.setMode( SHOW_MODE );
+            }
+        } );
+        button.addActionListener( HIDE_MODE, new ActionListener() {
+            public void actionPerformed( ActionEvent e ) {
+                dialog.hide();
             }
         } );
         dialog.setContentPane( new SymbolKey() );
