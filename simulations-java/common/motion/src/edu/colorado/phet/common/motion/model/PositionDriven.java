84b06a814c1a23cdbb938d31e076d9fd44ce21d8
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-07-17 04:24:30 +0000

    rotation: added smoothing options

--- simulations-java/common/motion/src/edu/colorado/phet/common/motion/model/PositionDriven.java
+++ simulations-java/common/motion/src/edu/colorado/phet/common/motion/model/PositionDriven.java
@@ -14,13 +14,34 @@ public class PositionDriven implements UpdateStrategy {
 
     //todo: try 2nd order derivative directly from position data?
     public void update( MotionBodySeries motionBodySeries, double dt, MotionBodyState state, double time ) {
-        TimeData v = MotionMath.getDerivative( motionBodySeries.getRecentPositionTimeSeries( Math.min( velocityWindow, motionBodySeries.getPositionSampleCount() ) ) );
-        TimeData a = MotionMath.getDerivative( motionBodySeries.getRecentVelocityTimeSeries( Math.min( accelerationWindow, motionBodySeries.getVelocitySampleCount() ) ) );
-        //TimeData a = MotionMath.getSecondDerivative( motionBodySeries.getRecentPositionTimeSeries( Math.min( accelerationWindow, motionBodySeries.getPositionSampleCount() ) ) );
+        TimeData v = MotionMath.getDerivative( smooth( motionBodySeries.getRecentPositionTimeSeries( Math.min( velocityWindow, motionBodySeries.getPositionSampleCount() ) ), 1 ) );
+        TimeData a = MotionMath.getDerivative( smooth( motionBodySeries.getRecentVelocityTimeSeries( Math.min( accelerationWindow, motionBodySeries.getVelocitySampleCount() ) ), 1 ) );
+//        TimeData a = MotionMath.getSecondDerivative( smooth( motionBodySeries.getRecentPositionTimeSeries( Math.min( accelerationWindow, motionBodySeries.getPositionSampleCount() ) ), 20 ) );
 
         motionBodySeries.addPositionData( state.getPosition(), time );
         motionBodySeries.addVelocityData( v.getValue(), v.getTime() );
         motionBodySeries.addAccelerationData( a.getValue(), a.getTime() );
+    }
+
+    public TimeData[] smooth( TimeData[] series, int numSmooth ) {
+        for( int i = 0; i < numSmooth; i++ ) {
+            series = smooth( series );
+        }
+        return series;
+    }
+
+    private TimeData[] smooth( TimeData[] datas ) {
+        TimeData[] smooth = new TimeData[datas.length];
+        for( int i = 0; i < smooth.length; i++ ) {
+            if( i > 0 && i < smooth.length - 1 ) {
+                smooth[i] = new TimeData( ( datas[i - 1].getValue() + datas[i].getValue() + datas[i + 1].getValue() ) / 3.0, datas[i].getTime() );
+            }
+            else {
+                smooth[i] = new TimeData( datas[i].getValue(), datas[i].getTime() );
+            }
+        }
+
+        return smooth;
     }
 
     public double getAccelerationWindow() {
