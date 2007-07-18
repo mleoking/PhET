3cd8ff61da13d159d09851da163ce9fd0f96a62d
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-07-18 01:56:27 +0000

    removed duplicated state creation

--- trunk/simulations-java/common/motion/src/edu/colorado/phet/common/motion/model/MotionBody.java
+++ trunk/simulations-java/common/motion/src/edu/colorado/phet/common/motion/model/MotionBody.java
@@ -10,6 +10,47 @@ public class MotionBody implements IUpdateStrategy {
 
     private MotionBodySeries motionBodySeries = new MotionBodySeries();
     private MotionBodyState motionBodyState = new MotionBodyState();//current state
+    private DefaultSimulationVariable x;
+    private DefaultSimulationVariable v;
+    private DefaultSimulationVariable a;
+
+    public MotionBody() {
+        x = new DefaultSimulationVariable();
+        motionBodyState.addListener( new MotionBodyState.Adapter() {
+            public void positionChanged( double dx ) {
+                x.setValue( motionBodyState.getPosition() );
+            }
+        } );
+        x.addListener( new ISimulationVariable.Listener() {
+            public void valueChanged() {
+                motionBodyState.setPosition( x.getValue() );
+            }
+        } );
+
+        v = new DefaultSimulationVariable();
+        motionBodyState.addListener( new MotionBodyState.Adapter() {
+            public void velocityChanged() {
+                v.setValue( motionBodyState.getVelocity() );
+            }
+        } );
+        v.addListener( new ISimulationVariable.Listener() {
+            public void valueChanged() {
+                motionBodyState.setVelocity( v.getValue() );
+            }
+        } );
+
+        a = new DefaultSimulationVariable();
+        motionBodyState.addListener( new MotionBodyState.Adapter() {
+            public void accelerationChanged() {
+                a.setValue( motionBodyState.getAcceleration() );
+            }
+        } );
+        a.addListener( new ISimulationVariable.Listener() {
+            public void valueChanged() {
+                motionBodyState.setAcceleration( a.getValue() );
+            }
+        } );
+    }
 
     public MotionBodySeries getMotionBodySeries() {
         return motionBodySeries;
@@ -41,17 +82,6 @@ public class MotionBody implements IUpdateStrategy {
     }
 
     public ISimulationVariable getXVariable() {
-        final DefaultSimulationVariable x = new DefaultSimulationVariable();
-        motionBodyState.addListener( new MotionBodyState.Adapter() {
-            public void positionChanged( double dx ) {
-                x.setValue( motionBodyState.getPosition() );
-            }
-        } );
-        x.addListener( new ISimulationVariable.Listener() {
-            public void valueChanged() {
-                motionBodyState.setPosition( x.getValue() );
-            }
-        } );
         return x;
     }
 
@@ -72,32 +102,10 @@ public class MotionBody implements IUpdateStrategy {
     }
 
     public ISimulationVariable getVVariable() {
-        final DefaultSimulationVariable v = new DefaultSimulationVariable();
-        motionBodyState.addListener( new MotionBodyState.Adapter() {
-            public void velocityChanged() {
-                v.setValue( motionBodyState.getVelocity() );
-            }
-        } );
-        v.addListener( new ISimulationVariable.Listener() {
-            public void valueChanged() {
-                motionBodyState.setVelocity( v.getValue() );
-            }
-        } );
         return v;
     }
 
     public ISimulationVariable getAVariable() {
-        final DefaultSimulationVariable a = new DefaultSimulationVariable();
-        motionBodyState.addListener( new MotionBodyState.Adapter() {
-            public void accelerationChanged() {
-                a.setValue( motionBodyState.getAcceleration() );
-            }
-        } );
-        a.addListener( new ISimulationVariable.Listener() {
-            public void valueChanged() {
-                motionBodyState.setAcceleration( a.getValue() );
-            }
-        } );
         return a;
     }
 
