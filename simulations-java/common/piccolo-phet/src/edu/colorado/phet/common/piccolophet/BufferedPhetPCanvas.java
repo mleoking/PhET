2fcd9131b1364ed6fca6e594ce01a668855ed066
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-07-18 23:12:22 +0000

    //Using INT_RGB on a mac doesn't allow any transparency to appear //INT_ARGB_PRE seems to resolve this issue

--- simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/BufferedPhetPCanvas.java
+++ simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/BufferedPhetPCanvas.java
@@ -1,6 +1,6 @@
 package edu.colorado.phet.common.piccolophet;
 
-import edu.colorado.phet.common.piccolophet.PhetPCanvas;
+import edu.colorado.phet.common.phetcommon.util.PhetUtilities;
 
 import java.awt.*;
 import java.awt.geom.AffineTransform;
@@ -8,6 +8,7 @@ import java.awt.image.BufferedImage;
 
 /**
  * This class explicitly buffers the graphics to resolve some issues related to piccolo (pswing) scene graph rendering.
+ *
  * @author Sam Reid
  */
 public class BufferedPhetPCanvas extends PhetPCanvas {
@@ -15,7 +16,8 @@ public class BufferedPhetPCanvas extends PhetPCanvas {
 
     public void paintComponent( Graphics g ) {
         if( bufferedImage == null || bufferedImage.getWidth() != getWidth() || bufferedImage.getHeight() != getHeight() ) {
-            bufferedImage = new BufferedImage( getWidth(), getHeight(), BufferedImage.TYPE_INT_RGB );
+//            bufferedImage = new BufferedImage( getWidth(), getHeight(),  );
+            bufferedImage = new BufferedImage( getWidth(), getHeight(), getBufferedImageType() );
         }
 
         Graphics2D bufferedGraphics = bufferedImage.createGraphics();
@@ -23,5 +25,12 @@ public class BufferedPhetPCanvas extends PhetPCanvas {
         super.paintComponent( bufferedGraphics );
         ( (Graphics2D)g ).drawRenderedImage( bufferedImage, new AffineTransform() );
         bufferedGraphics.dispose();
+    }
+
+    //Using INT_RGB on a mac doesn't allow any transparency to appear
+    //INT_ARGB_PRE seems to resolve this issue
+    private int getBufferedImageType() {
+        return PhetUtilities.isMacintosh() ? BufferedImage.TYPE_INT_ARGB_PRE
+               : BufferedImage.TYPE_INT_RGB;
     }
 }
