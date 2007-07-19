3cb0419ebcfdda8a29b5773b1c2f59e4e136701b
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-07-19 21:14:54 +0000

    Fixed: Set the magnitude color to be the correct color (not x-component) for linear components, choose others as we see fit.;  Fixed: Colors for angular variables should be variants of linear colors (set to pastel variants)

--- trunk/simulations-java/simulations/rotation/src/edu/colorado/phet/rotation/RotationColorScheme.java
+++ trunk/simulations-java/simulations/rotation/src/edu/colorado/phet/rotation/RotationColorScheme.java
@@ -7,23 +7,25 @@ import java.awt.*;
  * Jul 14, 2007, 12:39:46 AM
  */
 public class RotationColorScheme {
+    public static final Color TEAL = new Color( 0, 128, 128 );
+
     public static final Color POSITION_COLOR = Color.blue;
     public static final Color VELOCITY_COLOR = Color.red;
     public static final Color ACCELERATION_COLOR = Color.green;
 
-    public static final Color ANGLE_COLOR = POSITION_COLOR;
-    public static final Color ANGULAR_VELOCITY_COLOR = VELOCITY_COLOR;
-    public static final Color ANGULAR_ACCELERATION_COLOR = ACCELERATION_COLOR;
+    public static final Color ANGLE_COLOR = new Color( 127, 217, 255 );//light blue
+    public static final Color ANGULAR_VELOCITY_COLOR = new Color( 255, 127, 181 );//light red
+    public static final Color ANGULAR_ACCELERATION_COLOR = new Color( 168, 232, 68 );//light green
 
     public static final Color X_COLOR = Color.green;
     public static final Color Y_COLOR = Color.red;
     public static final Color R_COLOR = Color.blue;
 
-    public static final Color VX_COLOR = Color.red;
+    public static final Color VX_COLOR = Color.blue;
     public static final Color VY_COLOR = Color.green;
-    public static final Color VM_COLOR = Color.blue;
+    public static final Color VM_COLOR = VELOCITY_COLOR;
 
-    public static final Color AX_COLOR = Color.green;
-    public static final Color AY_COLOR = Color.blue;
-    public static final Color AM_COLOR = Color.red;
+    public static final Color AX_COLOR = Color.blue;
+    public static final Color AY_COLOR = Color.red;
+    public static final Color AM_COLOR = ACCELERATION_COLOR;
 }
