4ed8bfd8406939f36534e15cf25429bd7a6fded9
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-07-20 15:56:24 +0000

    added initial support for switching angular coordinate units

--- simulations-java/simulations/rotation/src/edu/colorado/phet/rotation/tests/TestRotationControlPanel.java
+++ simulations-java/simulations/rotation/src/edu/colorado/phet/rotation/tests/TestRotationControlPanel.java
@@ -15,6 +15,7 @@ import edu.colorado.phet.common.piccolophet.PhetPCanvas;
 import edu.colorado.phet.common.piccolophet.nodes.RulerNode;
 import edu.colorado.phet.rotation.RotationControlPanel;
 import edu.colorado.phet.rotation.RotationModule;
+import edu.colorado.phet.rotation.AngleUnitModel;
 import edu.colorado.phet.rotation.controls.VectorViewModel;
 import edu.colorado.phet.rotation.graphs.RotationGraphSet;
 import edu.colorado.phet.rotation.model.RotationModel;
@@ -35,7 +36,7 @@ public class TestRotationControlPanel {
         GraphSuite graphSuite = new RotationGraphSet( phetPCanvas, rotationModel ).getGraphSuite( 0 );
         GraphSetModel graphSetModel = new GraphSetModel( graphSuite );
         VectorViewModel vectorViewModel = new VectorViewModel();
-        frame.setContentPane( new RotationControlPanel( new RulerNode( 10, 10, new String[0], "units", 3, 14 ), rotationGraphSet, graphSetModel, vectorViewModel, frame, rotationModel.getRotationBody( 1 ), new RotationModule( frame ) ) );
+        frame.setContentPane( new RotationControlPanel( new RulerNode( 10, 10, new String[0], "units", 3, 14 ), rotationGraphSet, graphSetModel, vectorViewModel, frame, rotationModel.getRotationBody( 1 ), new RotationModule( frame ) ,new AngleUnitModel( true ) ) );
     }
 
     public static void main( String[] args ) {
