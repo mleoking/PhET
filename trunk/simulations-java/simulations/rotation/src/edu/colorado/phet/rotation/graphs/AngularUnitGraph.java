0edb21c99d1723679c1c1e2d9ca97e947bdcb2d2
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-07-21 01:38:39 +0000

    improved support for units on graph data

--- trunk/simulations-java/simulations/rotation/src/edu/colorado/phet/rotation/graphs/AngularUnitGraph.java
+++ trunk/simulations-java/simulations/rotation/src/edu/colorado/phet/rotation/graphs/AngularUnitGraph.java
@@ -2,6 +2,7 @@ package edu.colorado.phet.rotation.graphs;
 
 import edu.colorado.phet.common.motion.graphs.ControlGraphSeries;
 import edu.colorado.phet.common.motion.model.ISimulationVariable;
+import edu.colorado.phet.common.motion.model.TimeData;
 import edu.colorado.phet.common.motion.model.UpdateStrategy;
 import edu.colorado.phet.common.piccolophet.PhetPCanvas;
 import edu.colorado.phet.common.timeseries.model.TimeSeriesModel;
@@ -43,13 +44,36 @@ public class AngularUnitGraph extends RotationGraph {
 
     private void updateUnits() {
         getVerticalAxis().setLabel( getTitle() + " (" + getUnitsString() + ")" );
-        setVerticalRange( angleUnitModel.isRadians() ? minRad : toDegrees( minRad ),
-                          angleUnitModel.isRadians() ? maxRad : toDegrees( maxRad ) );
+        setVerticalRange( isRadians() ? minRad : toDegrees( minRad ),
+                          isRadians() ? maxRad : toDegrees( maxRad ) );
         for( int i = 0; i < getSeriesCount(); i++ ) {
             ControlGraphSeries series = getControlGraphSeries( i );
             series.setUnits( getUnitsString() );
         }
+        getDynamicJFreeChartNode().clear();
+        //add back all existing data in the right units
+        for( int i = 0; i < getSeriesCount(); i++ ) {
+            ControlGraphSeries series = getControlGraphSeries( i );
+            for( int k = 0; k < series.getObservableTimeSeries().getSampleCount(); k++ ) {
+                TimeData data = series.getObservableTimeSeries().getData( k );
+                getDynamicJFreeChartNode().addValue( getSeriesIndex( series ), data.getTime(), getValue( data ) );
+            }
+        }
+
         forceUpdateAll();
+    }
+
+    private double getValue( TimeData data ) {
+        return isRadians() ? data.getValue() : toDegrees( data.getValue() );
+    }
+
+    protected void handleDataAdded( int seriesIndex, TimeData timeData ) {
+//        super.handleDataAdded( seriesIndex, timeData );
+        addValue( seriesIndex, timeData.getTime(), getValue( timeData ) );
+    }
+
+    private boolean isRadians() {
+        return angleUnitModel.isRadians();
     }
 
     private double toDegrees( double rad ) {
@@ -61,7 +85,7 @@ public class AngularUnitGraph extends RotationGraph {
     }
 
     private String getUnitsString() {
-        return angleUnitModel.isRadians() ? unitsRad : unitsDeg;
+        return isRadians() ? unitsRad : unitsDeg;
     }
 
     protected void handleValueChanged() {
@@ -69,11 +93,10 @@ public class AngularUnitGraph extends RotationGraph {
     }
 
     private double viewToModel( double sliderValue ) {
-        return angleUnitModel.isRadians() ? sliderValue : toRadians( sliderValue );
+        return isRadians() ? sliderValue : toRadians( sliderValue );
     }
 
     protected void updateSliderValue() {
-        getJFreeChartSliderNode().setValue( angleUnitModel.isRadians() ? getSimulationVariable().getData().getValue() :
-                                            toDegrees( getSimulationVariable().getData().getValue() ) );
+        getJFreeChartSliderNode().setValue( getValue( getSimulationVariable().getData() ) );
     }
 }
