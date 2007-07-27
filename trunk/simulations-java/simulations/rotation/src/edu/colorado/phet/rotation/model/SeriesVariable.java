a94e142d312e26ed39bc00bd5caf6c5567804786
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-07-27 01:11:44 +0000

    factored out SeriesVariable

---
+++ trunk/simulations-java/simulations/rotation/src/edu/colorado/phet/rotation/model/SeriesVariable.java
@@ -0,0 +1,51 @@
+package edu.colorado.phet.rotation.model;
+
+import edu.colorado.phet.common.motion.model.DefaultSimulationVariable;
+import edu.colorado.phet.common.motion.model.DefaultTimeSeries;
+import edu.colorado.phet.common.motion.model.ISimulationVariable;
+import edu.colorado.phet.common.motion.model.ITimeSeries;
+
+/**
+ * Author: Sam Reid
+ * Jul 26, 2007, 8:04:10 PM
+ */
+public class SeriesVariable {
+    private ISimulationVariable variable = new DefaultSimulationVariable();
+    private ITimeSeries series = new DefaultTimeSeries();
+
+    public SeriesVariable() {
+    }
+
+    public void clear() {
+        series.clear();
+    }
+
+    public void updateSeriesAndState( double value, double time ) {
+        variable.setValue( value );
+        series.addValue( value, time );
+    }
+
+    public int getSampleCount() {
+        return series.getSampleCount();
+    }
+
+    public void setValue( double value ) {
+        variable.setValue( value );
+    }
+
+    public double getValueForTime( double time ) {
+        return series.getValueForTime( time );
+    }
+
+    public ISimulationVariable getVariable() {
+        return variable;
+    }
+
+    public ITimeSeries getSeries() {
+        return series;
+    }
+
+    public void setValueForTime( double time ) {
+        setValue( getValueForTime( time ) );
+    }
+}
