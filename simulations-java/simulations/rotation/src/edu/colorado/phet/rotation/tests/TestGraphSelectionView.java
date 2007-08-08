5a10458b8e89079ff1402a9ce332b0bc633ca7f3
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-08-08 22:41:41 +0000

    refactored RotationGraphSet to support torque sim

--- simulations-java/simulations/rotation/src/edu/colorado/phet/rotation/tests/TestGraphSelectionView.java
+++ simulations-java/simulations/rotation/src/edu/colorado/phet/rotation/tests/TestGraphSelectionView.java
@@ -10,6 +10,7 @@ import edu.colorado.phet.common.piccolophet.BufferedPhetPCanvas;
 import edu.colorado.phet.common.piccolophet.PhetPCanvas;
 import edu.colorado.phet.rotation.AngleUnitModel;
 import edu.colorado.phet.rotation.graphs.AbstractRotationGraphSet;
+import edu.colorado.phet.rotation.graphs.RotationGraphSet;
 import edu.colorado.phet.rotation.model.RotationModel;
 
 import javax.swing.*;
@@ -31,7 +32,7 @@ public class TestGraphSelectionView {
 
         phetPCanvas = new BufferedPhetPCanvas();
         phetPCanvas.setBackground( new Color( 200, 240, 200 ) );
-        GraphSuiteSet rotationGraphSet = new AbstractRotationGraphSet( phetPCanvas, new RotationModel( new ConstantDtClock( 30, 1 ) ), new AngleUnitModel( true ) );
+        GraphSuiteSet rotationGraphSet = new RotationGraphSet( phetPCanvas, new RotationModel( new ConstantDtClock( 30, 1 ) ), new AngleUnitModel( true ) );
         GraphSetModel graphSetModel = new GraphSetModel( rotationGraphSet.getGraphSuite( 0 ) );
 
         GraphSelectionControl graphSelectionControl = new GraphSelectionControl( rotationGraphSet, graphSetModel );
