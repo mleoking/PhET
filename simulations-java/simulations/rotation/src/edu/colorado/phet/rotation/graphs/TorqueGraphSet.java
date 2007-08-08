addc2f1b3b3b5d3e2fff90f58bab8c5842d3ccc0
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-08-08 23:20:09 +0000

    fixed for torque graph series

--- simulations-java/simulations/rotation/src/edu/colorado/phet/rotation/graphs/TorqueGraphSet.java
+++ simulations-java/simulations/rotation/src/edu/colorado/phet/rotation/graphs/TorqueGraphSet.java
@@ -7,6 +7,8 @@ import edu.colorado.phet.rotation.torque.TorqueModel;
 import edu.colorado.phet.rotation.util.UnicodeUtil;
 import edu.umd.cs.piccolo.nodes.PImage;
 
+import java.awt.*;
+
 /**
  * User: Sam Reid
  * Date: Dec 28, 2006
@@ -20,36 +22,31 @@ public class TorqueGraphSet extends AbstractRotationGraphSet {
 
         RotationMinimizableControlGraph torqueGraph = new RotationMinimizableControlGraph( UnicodeUtil.TAU, new RotationGraph(
                 pSwingCanvas, tm.getTorqueVariable(),
-                UnicodeUtil.TAU, "torque", "units", -0.001, 0.001, new PImage( loadArrow( "green-arrow.png" ) ),
+                UnicodeUtil.TAU, "torque", "units", -0.1, 0.1, new PImage( loadArrow( "green-arrow.png" ) ),
                 tm, true, tm.getTimeSeriesModel(), tm.getTorqueDriven(), RotationModel.MAX_TIME, tm.getRotationPlatform() ) );
-//
+        torqueGraph.addSeries( "Torque", Color.blue, UnicodeUtil.TAU, "units", tm.getTorqueVariable(), tm.getTorqueTimeSeries(), new BasicStroke( 2 ) );
+
         RotationMinimizableControlGraph forceGraph = new RotationMinimizableControlGraph( "F", new RotationGraph(
                 pSwingCanvas, tm.getForceVariable(),
                 "F", "force", "units", -0.001 / 200.0, 0.001 / 200.0, new PImage( loadArrow( "green-arrow.png" ) ),
                 tm, true, tm.getTimeSeriesModel(), tm.getForceDriven(), RotationModel.MAX_TIME, tm.getRotationPlatform() ) );
-//
+        forceGraph.addSeries( "Force", Color.blue, "force", "units", tm.getForceVariable(), tm.getForceTimeSeries(), new BasicStroke( 2 ) );
+
         RotationMinimizableControlGraph momentOfInertiaGraph = new RotationMinimizableControlGraph( "I", new RotationGraph(
                 pSwingCanvas, tm.getMomentOfInertiaVariable(),
                 "I", "Moment of Inertia", "units", -5, 5, new PImage( loadArrow( "green-arrow.png" ) ),
                 tm, false, tm.getTimeSeriesModel(), null, RotationModel.MAX_TIME, tm.getRotationPlatform() ) );
+        momentOfInertiaGraph.addSeries( "Moment of Inertia", Color.green, "I", "kg*m^2", tm.getMomentOfInertiaVariable(), tm.getMomentOfInertiaTimeSeries(), new BasicStroke( 2 ) );
 
         RotationMinimizableControlGraph angularMomentumGraph = new RotationMinimizableControlGraph( "L", new RotationGraph(
                 pSwingCanvas, tm.getAngularMomentumVariable(),
                 "L", "Angular Momentum", "units", -0.1, 0.1, new PImage( loadArrow( "green-arrow.png" ) ),
                 tm, false, tm.getTimeSeriesModel(), null, RotationModel.MAX_TIME, tm.getRotationPlatform() ) );
-
+        angularMomentumGraph.addSeries( "Angular Momentum", Color.red, "L", "units", tm.getAngularMomentumVariable(), tm.getAngularMomentumTimeSeries(), new BasicStroke( 2 ) );
 
         RotationMinimizableControlGraph angleGraph = createAngleGraph();
         RotationMinimizableControlGraph angVelGraph = createAngVelGraph();
         RotationMinimizableControlGraph angAccelGraph = createAngAccelGraph();
-//        RotationMinimizableControlGraph xGraph = createXGraph();
-//        RotationMinimizableControlGraph vGraph = createVGraph();
-//        RotationMinimizableControlGraph aGraph = createAGraph();
-
-//        addGraphSuite( new RotationMinimizableControlGraph[]{angleGraph, angVelGraph, xGraph} );
-//        addGraphSuite( new RotationMinimizableControlGraph[]{angleGraph, angVelGraph, angAccelGraph} );
-//        addGraphSuite( new RotationMinimizableControlGraph[]{angleGraph, angVelGraph, vGraph} );
-//        addGraphSuite( new RotationMinimizableControlGraph[]{angleGraph, angVelGraph, aGraph} );
 
         addGraphSuite( new RotationMinimizableControlGraph[]{forceGraph, torqueGraph} );
         addGraphSuite( new RotationMinimizableControlGraph[]{torqueGraph, angAccelGraph, angVelGraph, angleGraph} );
