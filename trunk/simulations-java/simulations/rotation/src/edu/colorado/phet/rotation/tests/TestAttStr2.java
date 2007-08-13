a61b5c84423b84280c77de3ccd596d412892332f
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-08-13 05:58:59 +0000

    added tests for attributed strings

---
+++ trunk/simulations-java/simulations/rotation/src/edu/colorado/phet/rotation/tests/TestAttStr2.java
@@ -0,0 +1,55 @@
+package edu.colorado.phet.rotation.tests;
+import java.awt.*;
+import java.awt.font.*;
+import java.text.*;
+import javax.swing.*;
+
+/** From:
+ * http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=4156394
+ */
+public class TestAttStr2 extends JPanel {
+    AttributedString as;
+
+    public static void main(String[] args) {
+        JFrame jf = new JFrame( TestAttStr2.class.getName());
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jf.getContentPane().setLayout(new GridLayout(1,0));
+        jf.getContentPane().add(new TestAttStr2());
+        jf.setSize(300, 300);
+        jf.setVisible(true);
+    }
+
+    TestAttStr2() {
+        String s = "attributed string of a length long enough to wrap a few times.";
+        as = new AttributedString(s);
+        as.addAttribute(TextAttribute.SIZE,
+                        new Float(24));
+        as.addAttribute(TextAttribute.SUPERSCRIPT,
+                        TextAttribute.SUPERSCRIPT_SUB, 0, 3);
+        as.addAttribute(TextAttribute.SUPERSCRIPT,
+                        TextAttribute.SUPERSCRIPT_SUPER, 5, 8);
+        as.addAttribute(TextAttribute.SUPERSCRIPT,
+                        TextAttribute.SUPERSCRIPT_SUB,
+                        s.length()-5, s.length()-2);
+    }
+
+    public void paintComponent(Graphics g) {
+
+        Graphics2D g2d = (Graphics2D)g;
+        g2d.setColor( Color.blue);
+        AttributedCharacterIterator aci = as.getIterator();
+        g2d.drawString(aci, 10, 100);
+
+        FontRenderContext frc = g2d.getFontRenderContext();
+        LineBreakMeasurer lbm = new LineBreakMeasurer(aci, frc);
+        final float width = 200;
+        float y = 150;
+        for (TextLayout tl = lbm.nextLayout(width); tl != null; tl = lbm.nextLayout(width)) {
+            y += tl.getAscent();
+            tl.draw(g2d, 10, y);
+            g2d.drawLine(0, (int)y, 200, (int)y);
+            y += tl.getDescent() + tl.getLeading();
+        }
+    }
+}
+
