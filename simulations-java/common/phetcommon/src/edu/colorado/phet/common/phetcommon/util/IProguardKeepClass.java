59b42b16458c5383bd8e3ff09b8eb8890e080554
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-08-16 22:44:05 +0000

    document the Proguard config file entry in the javadoc

--- simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/util/IProguardKeepClass.java
+++ simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/util/IProguardKeepClass.java
@@ -7,11 +7,19 @@ import java.io.Serializable;
 /**
  * IProguardKeepClass is a marker interface related to PhET's use of Proguard.
  * When applying Proguard to JAR files, we will tell Proguard to keep
- * any class that implements this interface.  If you have a class that 
- * is dynamically loaded, you'll want to implement this interface so 
- * that Proguard keeps your class.
+ * any class that implements this interface.  All fields and methods of 
+ * the class will be kept. If you have a class that is dynamically loaded,
+ * you'll want to implement this interface so that Proguard keeps your class.
  * <p>
- * See the "-keep class" option in the Proguard manual.
+ * The relevant entry in the Proguard configuration file will look like this:
+ * <code>
+ * -keep class * extends edu.colorado.phet.common.phetcommon.util.IProguardKeepClass {
+ *    <fields>;
+ *    <methods>;
+ * }
+ * </code>
+ * <p>
+ * For more details, see the "-keep" option in the Proguard manual.
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
