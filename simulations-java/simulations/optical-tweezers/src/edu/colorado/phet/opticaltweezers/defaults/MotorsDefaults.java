868b49ad1cfc6060498a63a470233315368306e9
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-08-17 16:36:16 +0000

    8/17/07 - start with Motors module being identical to DNA module

--- simulations-java/simulations/optical-tweezers/src/edu/colorado/phet/opticaltweezers/defaults/MotorsDefaults.java
+++ simulations-java/simulations/optical-tweezers/src/edu/colorado/phet/opticaltweezers/defaults/MotorsDefaults.java
@@ -3,7 +3,11 @@
 package edu.colorado.phet.opticaltweezers.defaults;
 
 import java.awt.Dimension;
+import java.awt.geom.Point2D;
 
+import edu.colorado.phet.common.phetcommon.util.DoubleRange;
+import edu.colorado.phet.common.phetcommon.util.IntegerRange;
+import edu.colorado.phet.opticaltweezers.control.ForcesControlPanel;
 import edu.colorado.phet.opticaltweezers.model.OTClock;
 
 
@@ -17,11 +21,61 @@ public class MotorsDefaults {
     /* Not intended for instantiation */
     private MotorsDefaults() {}
     
-    // Model-view transform
-    public static final Dimension VIEW_SIZE = new Dimension( 750, 750 );
-    public static final double MODEL_TO_VIEW_SCALE = 0.5;
-    
     // Clock
     public static final OTClock CLOCK = new OTClock( GlobalDefaults.FRAME_RATE, 
             GlobalDefaults.SLOW_DT_RANGE, GlobalDefaults.FAST_DT_RANGE, GlobalDefaults.DEFAULT_DT );
+    
+    // Model-view transform
+    public static final Dimension VIEW_SIZE = new Dimension( 750, 750 );
+    public static final double MODEL_TO_VIEW_SCALE = 0.25;
+
+    // Microscope Slide model, local origin at slide's geometric center
+    public static final double MICROSCOPE_SLIDE_CENTER_HEIGHT = 2000; // nm
+    public static final double MICROSCOPE_SLIDE_EDGE_HEIGHT = 0.025 * MICROSCOPE_SLIDE_CENTER_HEIGHT; // nm
+    private static final double MICROSCOPE_SLIDE_HEIGHT = MICROSCOPE_SLIDE_CENTER_HEIGHT + ( 2 * MICROSCOPE_SLIDE_EDGE_HEIGHT );
+    private static final double MICROSCOPE_SLIDE_Y_OFFSET = 150; // nm
+    public static final Point2D MICROSCOPE_SLIDE_POSITION = new Point2D.Double( 0, MICROSCOPE_SLIDE_Y_OFFSET + ( MICROSCOPE_SLIDE_HEIGHT /2 ) ); // nm
+    public static final double MICROSCOPE_SLIDE_ORIENTATION = Math.toRadians( 0 );
+
+    // Laser model
+    public static final double LASER_DIAMETER_AT_OBJECTIVE = 1800; // nm, chosen so that beam shape is similar to PhysicsDefaults
+    public static final double LASER_DIAMETER_AT_WAIST = 500; // nm
+    public static final double LASER_DISTANCE_FROM_OBJECTIVE_TO_WAIST = ( MICROSCOPE_SLIDE_HEIGHT / 2 ) + MICROSCOPE_SLIDE_Y_OFFSET; // nm
+    public static final double LASER_DISTANCE_FROM_OBJECTIVE_TO_CONTROL_PANEL = 200; // nm
+    public static final Point2D LASER_POSITION = new Point2D.Double( 2400, MICROSCOPE_SLIDE_POSITION.getY() ); // nm
+    
+    // Bead model
+    public static final Point2D BEAD_POSITION = new Point2D.Double( LASER_POSITION.getX(), LASER_POSITION.getY() ); // nm
+    
+    // Ruler
+    public static final double RULER_Y_POSITION = LASER_POSITION.getY() + ( GlobalDefaults.BEAD_DIAMETER / 2 ) + 30; // nm, just below center of trap
+    public static final int RULER_MAJOR_TICK_INTERVAL = 200; // nm
+    public static final int RULER_MINOR_TICKS_BETWEEN_MAJORS = 3;
+
+    // DNA Strand model
+    public static final double DNA_CONTOUR_LENGTH = 2413; // nm
+    public static final double DNA_PERSISTENCE_LENGTH = 50; // nm, double strand
+    public static final int DNA_NUMBER_OF_SPRINGS = 39; // nm
+    public static final DoubleRange DNA_SPRING_CONSTANT_RANGE = new DoubleRange( 2, 20, 10 );
+    public static final DoubleRange DNA_DRAG_COEFFICIENT_RANGE = new DoubleRange( 0.1, 2, 0.5 );
+    public static final DoubleRange DNA_EVOLUTION_DT_RANGE = new DoubleRange( 0.05, 0.2, 0.1 );
+    public static final DoubleRange DNA_KICK_CONSTANT_RANGE = new DoubleRange( 10, 100, 50, 0 );
+    public static final IntegerRange DNA_NUMBER_OF_EVOLUTIONS_PER_CLOCK_STEP_RANGE = new IntegerRange( 1, 100, 30 );
+    public static final DoubleRange DNA_FLUID_DRAG_COEFFICIENT_RANGE = new DoubleRange( 0, 0.00020, 0.000015 );
+    public static final boolean DNA_PIVOTS_VISIBLE = false;
+    public static final boolean DNA_EXTENSION_VISIBLE = false;
+    
+    // Control panel settings
+    public static final boolean LASER_BEAM_VISIBLE = true;
+    public static final boolean LASER_ELECTRIC_FIELD_VISIBLE = false;
+    public static final boolean TRAP_FORCE_SELECTED = false;
+    public static final boolean FLUID_DRAG_FORCE_SELECTED = false;
+    public static final boolean DNA_FORCE_SELECTED = false;
+    public static final boolean BROWNIAN_MOTION_ENABLED = true;
+    public static final boolean POSITION_HISTOGRAM_SELECTED = false;
+    public static final boolean POTENTIAL_ENERGY_CHART_SELECTED = false;
+    public static final boolean RULER_SELECTED = false;
+    public static final boolean FLUID_CONTROLS_SELECTED = false;
+    public static final boolean VECTOR_VALUES_VISIBLE = false;
+    public static final boolean VECTOR_COMPONENTS_VISIBLE = false;
 }
--- simulations-java/simulations/optical-tweezers/src/edu/colorado/phet/opticaltweezers/module/motors/MotorsControlPanel.java
+++ simulations-java/simulations/optical-tweezers/src/edu/colorado/phet/opticaltweezers/module/motors/MotorsControlPanel.java
@@ -2,9 +2,19 @@
 
 package edu.colorado.phet.opticaltweezers.module.motors;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import edu.colorado.phet.opticaltweezers.OTResources;
+import edu.colorado.phet.opticaltweezers.OpticalTweezersApplication;
+import edu.colorado.phet.opticaltweezers.control.ChartsControlPanel;
+import edu.colorado.phet.opticaltweezers.control.ForcesControlPanel;
+import edu.colorado.phet.opticaltweezers.control.MiscControlPanel;
+import edu.colorado.phet.opticaltweezers.control.SimulationSpeedControlPanel;
+import edu.colorado.phet.opticaltweezers.control.developer.DeveloperControlPanel;
+import edu.colorado.phet.opticaltweezers.model.MotorsModel;
+import edu.colorado.phet.opticaltweezers.model.OTClock;
 import edu.colorado.phet.opticaltweezers.module.AbstractControlPanel;
-import edu.colorado.phet.opticaltweezers.module.AbstractModule;
 
 /**
  * MotorsControlPanel is the control panel for MotorsModule.
@@ -17,6 +27,14 @@ public class MotorsControlPanel extends AbstractControlPanel {
     // Instance data
     //----------------------------------------------------------------------------
     
+    private MotorsCanvas _canvas;
+    
+    private SimulationSpeedControlPanel _simulationSpeedControlPanel;
+    private ForcesControlPanel _forcesControlPanel;
+    private ChartsControlPanel _chartsControlPanel;
+    private MiscControlPanel _miscControlPanel;
+    private DeveloperControlPanel _developerControlPanel;
+
     //----------------------------------------------------------------------------
     // Constructors
     //----------------------------------------------------------------------------
@@ -26,16 +44,88 @@ public class MotorsControlPanel extends AbstractControlPanel {
      * 
      * @param module
      */
-    public MotorsControlPanel( AbstractModule module ) {
+    public MotorsControlPanel( MotorsModule module) {
         super( module );
+
+        _canvas = module.getMotorsCanvas();
 
         // Set the control panel's minimum width.
         int minimumWidth = OTResources.getInt( "int.minControlPanelWidth", 215 );
         setMinumumWidth( minimumWidth );
         
+        // Sub-panels
+        MotorsModel model = module.getMotorsModel();
+        _simulationSpeedControlPanel = new SimulationSpeedControlPanel( TITLE_FONT, CONTROL_FONT, model.getClock() );
+        _forcesControlPanel = new ForcesControlPanel( TITLE_FONT, CONTROL_FONT, 
+                model.getBead(), model.getFluid(),
+                _canvas.getTrapForceNode(), _canvas.getFluidDragForceNode(), _canvas.getDNAForceNode() );
+        _chartsControlPanel = new ChartsControlPanel( TITLE_FONT, CONTROL_FONT, module.getFrame(),
+                model.getClock(), model.getBead(), model.getLaser(),
+                _canvas.getPotentialEnergyChartNode(), _canvas.getLaserNode() );
+        _miscControlPanel = new MiscControlPanel( TITLE_FONT, CONTROL_FONT, module.getFrame(), _canvas.getRulerNode(), model.getFluid() );
+        List forceVectorNodes = new ArrayList();
+        forceVectorNodes.add( _canvas.getTrapForceNode() );
+        forceVectorNodes.add( _canvas.getFluidDragForceNode() );
+        forceVectorNodes.add( _canvas.getDNAForceNode() );
+        _developerControlPanel = new DeveloperControlPanel( TITLE_FONT, CONTROL_FONT, module.getFrame(),
+                (OTClock)module.getClock(), model.getBead(), model.getLaser(), 
+                model.getDNAStrand(), _canvas.getDNAStrandNode(),
+                _canvas.getTrapForceNode(), _canvas.getFluidDragForceNode(), _canvas.getDNAForceNode(),
+                null /* electricFieldNode */, null /* chargeDistributionNode */ );
+        
+        // Turn off some features
+        _forcesControlPanel.setBrownianMotionCheckBoxVisible( false );
+        _miscControlPanel.setFluidVacuumPanelVisible( false );
+        
         // Layout
         {
+            addControlFullWidth( _simulationSpeedControlPanel );
+            addSeparator();
+            addControlFullWidth( _forcesControlPanel );
+            addSeparator();
+            addControlFullWidth( _chartsControlPanel );
+            addSeparator();
+            addControlFullWidth( _miscControlPanel );
+            addSeparator();
+            if ( OpticalTweezersApplication.isDeveloperControlsEnabled() ) {
+                addControlFullWidth( _developerControlPanel );
+                addSeparator();
+            }
             addResetButton();
         }
     }
+    
+    //----------------------------------------------------------------------------
+    // Setters and getters
+    //----------------------------------------------------------------------------
+    
+    public void closeAllDialogs() {
+        _chartsControlPanel.setPositionHistogramSelected( false );
+        _miscControlPanel.setFluidControlsSelected( false );
+    }
+    
+    //----------------------------------------------------------------------------
+    // Access to subpanels
+    //----------------------------------------------------------------------------
+    
+    public DeveloperControlPanel getDeveloperControlPanel() {
+        return _developerControlPanel;
+    }
+    
+    public SimulationSpeedControlPanel getSimulationSpeedControlPanel() {
+        return _simulationSpeedControlPanel;
+    }
+    
+    public ForcesControlPanel getForcesControlPanel() {
+        return _forcesControlPanel;
+    }
+    
+    public ChartsControlPanel getChartsControlPanel() {
+        return _chartsControlPanel;
+    }
+    
+    public MiscControlPanel getMiscControlPanel() {
+        return _miscControlPanel;
+    }
 }
+
