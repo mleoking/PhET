94279c920eb32db7390379ab71d4c2def07c2609
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-08-24 17:37:24 +0000

    8/23/07 - first pass at enzyme speed model, rotation varies with ATP and DNA force

--- trunk/simulations-java/simulations/optical-tweezers/src/edu/colorado/phet/opticaltweezers/model/AbstractEnzyme.java
+++ trunk/simulations-java/simulations/optical-tweezers/src/edu/colorado/phet/opticaltweezers/model/AbstractEnzyme.java
@@ -8,7 +8,12 @@ import edu.colorado.phet.common.phetcommon.model.ModelElement;
 
 /**
  * AbstractEnzyme is base class for all enzymes.
- * An enzyme that feeds on ATP in the fluid and pulls on the DNA.
+ * An enzyme feeds on ATP in the fluid and pulls on the DNA strand,
+ * converting chemical energy into work.
+ * <p>
+ * The model herein is based on:
+ * "Force production by single kinesin motors", Schnitzer et. al.,
+ * Nature Cell Biology, Vol 2, October 2000, p. 718-723.
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
@@ -21,9 +26,18 @@ public abstract class AbstractEnzyme extends FixedObject implements ModelElement
     public static final String PROPERTY_ENABLED = "enabled";
     public static final String PROPERTY_INNER_ORIENTATION = "innerOrientation";
     
+    public static final double MAX_ROTATION_PER_CLOCK_STEP = Math.toRadians( 20 );
+    
     //----------------------------------------------------------------------------
     // Instance data
     //----------------------------------------------------------------------------
+    
+    private DNAStrand _dnaStrand;
+    private Fluid _fluid;
+    
+    // constants used in the velocity model
+    private final double _c1, _c2, _c3, _c4, _c5, _c6, _c7, _c8, _d1, _d2;
+    private final double _maxDNAStrandSpeed;
     
     private final double _outerDiameter, _innerDiameter;
     private double _innerOrientation;
@@ -33,12 +47,30 @@ public abstract class AbstractEnzyme extends FixedObject implements ModelElement
     // Constructors
     //----------------------------------------------------------------------------
     
-    public AbstractEnzyme( Point2D position, double outerDiameter, double innerDiameter ) {
+    public AbstractEnzyme( Point2D position, double outerDiameter, double innerDiameter, DNAStrand dnaStrand, Fluid fluid,
+            double c1, double c2, double c3, double c4, double c5, double c6, double c7, double c8, double d1, double d2 ) {
         super( position, 0 /* orientation */ );
+        
         _outerDiameter = outerDiameter;
         _innerDiameter = innerDiameter;
         _innerOrientation = 0;
         _enabled = false;
+        
+        _dnaStrand = dnaStrand;
+        _fluid = fluid;
+        
+        _c1 = c1;
+        _c2 = c2;
+        _c3 = c3;
+        _c4 = c4;
+        _c5 = c5;
+        _c6 = c6;
+        _c7 = c7;
+        _c8 = c8;
+        _d1 = d1;
+        _d2 = d2;
+        
+        _maxDNAStrandSpeed = getMaxDNAStrandSpeed();
     }
     
     //----------------------------------------------------------------------------
@@ -69,12 +101,78 @@ public abstract class AbstractEnzyme extends FixedObject implements ModelElement
     }
     
     //----------------------------------------------------------------------------
+    // Model of DNA strand moving through enzyme
+    //----------------------------------------------------------------------------
+    
+    /**
+     * Gets the speed with which the DNA strand is moving through the enzyme.
+     * <p>
+     * This is erroneously refered to as velocity in the design document.
+     * It is a function of the DNA force magnitude, so it has no orientation
+     * and should be referred to as speed (the magnitude component of velocity).
+     * 
+     * @return speed (nm/s)
+     */
+    public double getDNAStrandSpeed() {
+        final double atp = _fluid.getATPConcentration();
+        final double fDNA = _dnaStrand.getForce().getMagnitude();
+        return getDNAStrandSpeed( atp, fDNA );
+    }
+    
+    /*
+     * Gets the maximum speed with which the DNA strand is moving through the enzyme.
+     * The speed will be at a maximum when the ATP concentration is at its maximum
+     * and the DNA force is zero.
+     */
+    private double getMaxDNAStrandSpeed() {
+        final double maxATP = _fluid.getATPConcentrationRange().getMax();
+        return getDNAStrandSpeed( maxATP, 0 );
+    }
+    
+    /*
+     * Gets the speed with which the DNA strand is moving through the enzyme
+     * for specific ATP and DNA force values.
+     * 
+     * @param atp ATP concentration
+     * @param fDNA DNA force magnitude (pN)
+     * @return speed (nm/s)
+     */
+    public double getDNAStrandSpeed( final double atp, final double fDNA ) {
+        final double sMax = calcMaxSpeed( fDNA ); // V_Max in design doc
+        final double km = calcRateConstant( fDNA ); // KM in design doc
+        double speed = sMax * atp / ( atp + km ); // Velocity in design doc
+        return speed;
+    }
+    
+    /*
+     * Max speed, referred to erroneously as V_Max in the design document.
+     * 
+     * @param fDNA DNA force magnitude
+     * @return max speed (nm/s)
+     */
+    private double calcMaxSpeed( final double fDNA ) {
+        return ( _d2 * _c1 ) / ( _c2 + _c3 * Math.exp( fDNA * _c4 ) );
+    }
+    
+    /*
+     * Rate constant, referred to as KM in the design document.
+     * 
+     * @param fDNA DNA force magnitude
+     * @return KM
+     */
+    private double calcRateConstant( final double fDNA ) {
+        return _d1 * ( _c6 + _c7 * Math.exp( fDNA * _c8 ) ) / ( _c2 + _c3 * Math.exp( fDNA * _c4 ) );
+    }
+    
+    //----------------------------------------------------------------------------
     // ModelElement implementation
     //----------------------------------------------------------------------------
 
     public void stepInTime( double dt ) {
         if ( _enabled ) {
-            _innerOrientation += Math.toRadians( 10 );//XXX rotate
+            final double dnaStrandSpeed = getDNAStrandSpeed();
+//            System.out.println( "AbstractEnzyme.stepInTime dnaStrandSpeed=" + dnaStrandSpeed );//XXX
+            _innerOrientation += MAX_ROTATION_PER_CLOCK_STEP * ( dnaStrandSpeed / _maxDNAStrandSpeed );
             notifyObservers( PROPERTY_INNER_ORIENTATION );
         }
     }
