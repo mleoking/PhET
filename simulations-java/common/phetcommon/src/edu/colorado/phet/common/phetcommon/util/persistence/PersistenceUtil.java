324fc68682549e0125013ac774c6cace2089e7b9
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-08-24 03:30:37 +0000

    removed unused/bogus serialization implementation

--- simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/util/persistence/PersistenceUtil.java
+++ simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/util/persistence/PersistenceUtil.java
@@ -26,32 +26,6 @@ import java.io.*;
 public class PersistenceUtil {
 
     /**
-     * Adds Save and Restore items to the File menu of a PhetApplication
-     *
-     * @param application
-     * @deprecated
-     */
-    public static void addMenuItems( PhetApplication application ) {
-
-        JMenuItem mi2 = new JMenuItem( "Restore state" );
-        mi2.addActionListener( new ActionListener() {
-            public void actionPerformed( ActionEvent e ) {
-                PhetApplication.instance().restoreState( "/temp/ttt.xml" );
-            }
-        } );
-        application.getPhetFrame().addFileMenuItem( mi2 );
-
-        JMenuItem mi = new JMenuItem( "Save state" );
-        mi.addActionListener( new ActionListener() {
-            public void actionPerformed( ActionEvent e ) {
-                PhetApplication.instance().saveState( "/temp/ttt.xml" );
-            }
-        } );
-        application.getPhetFrame().addFileMenuItem( mi );
-        application.getPhetFrame().addFileMenuSeparatorAfter( mi2 );
-    }
-
-    /**
      * Perform a deep copy of a Serializable object graph using serialization.
      *
      * @param object the object to be copied.
