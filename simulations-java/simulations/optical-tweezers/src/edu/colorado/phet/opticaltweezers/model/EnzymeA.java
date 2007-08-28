0f0ba28e91b9f611f8d662bd7625da0344a08722
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-08-28 17:44:46 +0000

    8/28/07 - use d2 as maxSpeed in enzyme speed model 8/28/07 - replace d1 with c1/c5 in enzyme speed model

--- simulations-java/simulations/optical-tweezers/src/edu/colorado/phet/opticaltweezers/model/EnzymeA.java
+++ simulations-java/simulations/optical-tweezers/src/edu/colorado/phet/opticaltweezers/model/EnzymeA.java
@@ -11,7 +11,10 @@ import java.awt.geom.Point2D;
  */
 public class EnzymeA extends AbstractEnzyme {
     
-    // parameters for velocity model
+    // speed when DNA force=0 and ATP concentration=infinite
+    private static final double MAX_DNA_SPEED = 5000; // nm/s
+    
+    // calibration constants for DNA speed model
     private static final double C1 = 4.79;
     private static final double C2 = 4.7;
     private static final double C3 = 0.09;
@@ -20,11 +23,9 @@ public class EnzymeA extends AbstractEnzyme {
     private static final double C6 = 2;
     private static final double C7 = 0.1;
     private static final double C8 = 1.2;
-    private static final double D1 = 2.281;
-    private static final double D2 = 5000; // nm/s
 
     public EnzymeA( Point2D position, double outerDiameter, double innerDiameter, DNAStrand dnaStrand, Fluid fluid, double maxDt ) {
-        super( position, outerDiameter, innerDiameter, dnaStrand, fluid, maxDt, C1, C2, C3, C4, C5, C6, C7, C8, D1, D2 );
+        super( position, outerDiameter, innerDiameter, dnaStrand, fluid, maxDt, MAX_DNA_SPEED, C1, C2, C3, C4, C5, C6, C7, C8 );
     }
 
 }
