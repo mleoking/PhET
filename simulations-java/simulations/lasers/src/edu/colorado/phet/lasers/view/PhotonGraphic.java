1e2805ef42a539e00d55150a6558a9ea203366aa
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-09-01 06:58:28 +0000

    use built-in image caching

--- simulations-java/simulations/lasers/src/edu/colorado/phet/lasers/view/PhotonGraphic.java
+++ simulations-java/simulations/lasers/src/edu/colorado/phet/lasers/view/PhotonGraphic.java
@@ -230,7 +230,8 @@ public class PhotonGraphic extends PhetImageGraphic implements SimpleObserver,
     }
 
     private static BufferedImage createTwinkleImage( double wavelength ) {
-        return BufferedImageUtils.toBufferedImage( PhotonImageFactory.createPhotonImage( wavelength, PHOTON_SIZE ) );
+        return BufferedImageUtils.toBufferedImage( PhotonImageFactory.lookupPhotonImage( wavelength, PHOTON_SIZE ) );
+//        return BufferedImageUtils.toBufferedImage( PhotonImageFactory.createPhotonImage( wavelength, PHOTON_SIZE ) );
     }
 
     public void update() {
