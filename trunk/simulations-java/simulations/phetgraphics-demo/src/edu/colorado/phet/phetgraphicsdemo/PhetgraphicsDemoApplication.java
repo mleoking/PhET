7d338a9c22fd518c45d4e81fa34285710f661ab7
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-09-12 16:39:07 +0000

    change name of main class to PhetgraphicsDemoApplication, fix build file for new mainclass and package names

--- trunk/simulations-java/simulations/phetgraphics-demo/src/edu/colorado/phet/phetgraphicsdemo/PhetgraphicsDemoApplication.java
+++ trunk/simulations-java/simulations/phetgraphics-demo/src/edu/colorado/phet/phetgraphicsdemo/PhetgraphicsDemoApplication.java
@@ -6,15 +6,16 @@ import edu.colorado.phet.common.phetcommon.view.util.FrameSetup;
 import java.io.IOException;
 
 /**
- * TestApplication is a test application that demonstrates proposed changes
- * to the semantics of PhetGraphic's location.
+ * PhetgraphicsDemoApplication demonstrates how registration point, location 
+ * and transforms can be combined to create "self-contained behaviors" that
+ * can be combined in composite graphics.
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  * @version $Revision$
  */
-public class TestApplication extends NonPiccoloPhetApplication {
+public class PhetgraphicsDemoApplication extends NonPiccoloPhetApplication {
 
-    public TestApplication( String[] args, String title, String description, String version, FrameSetup frameSetup ) throws IOException {
+    public PhetgraphicsDemoApplication( String[] args, String title, String description, String version, FrameSetup frameSetup ) throws IOException {
         super( args, title, description, version, frameSetup );
         TestModule module = new TestModule();
         addModule( module );
@@ -23,11 +24,11 @@ public class TestApplication extends NonPiccoloPhetApplication {
     public static void main( String[] args ) throws IOException {
 
         String title = "Test Location";
-        String description = "Tests changes to semantics of PhetGraphic location";
+        String description = "Demonstrates semantics of phetgraphics location";
         String version = "0.1";
         FrameSetup frameSetup = new FrameSetup.CenteredWithSize( 1024, 768 );
 
-        NonPiccoloPhetApplication app = new TestApplication( args, title, description, version, frameSetup );
+        NonPiccoloPhetApplication app = new PhetgraphicsDemoApplication( args, title, description, version, frameSetup );
         app.startApplication();
     }
 }
