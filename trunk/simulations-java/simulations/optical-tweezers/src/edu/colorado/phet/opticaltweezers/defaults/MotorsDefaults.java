e5bc79ab7ec06cf889a8335d6cea7d7a9ac64e2d
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-09-19 16:43:44 +0000

    9/19/07 - use a different color for DNA free strand, so it's easier to distinguish the 2 strand 9/19/07 - make enzyme stop rotating when DNA strand is fully reeled in 9/19/07 - first attempt at making enzyme pull DNA strand 9/19/07 - move enzyme node behind DNA strand 9/19/07 - move bead node behind DNA strand and enzyme

--- trunk/simulations-java/simulations/optical-tweezers/src/edu/colorado/phet/opticaltweezers/defaults/MotorsDefaults.java
+++ trunk/simulations-java/simulations/optical-tweezers/src/edu/colorado/phet/opticaltweezers/defaults/MotorsDefaults.java
@@ -2,6 +2,7 @@
 
 package edu.colorado.phet.opticaltweezers.defaults;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Point;
 import java.awt.geom.Point2D;
@@ -96,6 +97,7 @@ public class MotorsDefaults {
     public static final DoubleRange DNA_FLUID_DRAG_COEFFICIENT_RANGE = GlobalDefaults.DNA_FLUID_DRAG_COEFFICIENT_RANGE;
     public static final boolean DNA_PIVOTS_VISIBLE = GlobalDefaults.DNA_PIVOTS_VISIBLE;
     public static final boolean DNA_EXTENSIONS_VISIBLE = GlobalDefaults.DNA_EXTENSIONS_VISIBLE;
+    public static final Color DNA_FREE_STRAND_COLOR = Color.GREEN;
     
     // Invisible bead, attached to free end of DNA
     public static final Point2D INVISIBLE_BEAD_POSITION = 
