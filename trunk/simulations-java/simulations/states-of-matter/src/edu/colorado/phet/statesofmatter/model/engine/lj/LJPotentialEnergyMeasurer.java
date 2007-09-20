9affd35f141350d96c5c0c33d99bba74c15cafaf
jdegoes <jdegoes@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-09-20 17:55:03 +0000

    states-of-matter: partial rewrite of model, continuation

---
+++ trunk/simulations-java/simulations/states-of-matter/src/edu/colorado/phet/statesofmatter/model/engine/lj/LJPotentialEnergyMeasurer.java
@@ -0,0 +1,37 @@
+package edu.colorado.phet.statesofmatter.model.engine.lj;
+
+import edu.colorado.phet.statesofmatter.model.particle.StatesOfMatterParticle;
+
+import java.util.List;
+
+public class LJPotentialEnergyMeasurer {
+    private final List particles;
+    private final LennardJonesPotential ljp;
+
+    public LJPotentialEnergyMeasurer(List particles, LennardJonesPotential potential) {
+        this.particles = particles;
+        this.ljp       = potential;
+    }
+
+    private StatesOfMatterParticle p(int i) {
+        return (StatesOfMatterParticle)particles.get(i);
+    }
+
+    public double measure() {
+        double potential = 0.0;
+
+        for (int i = 0; i < particles.size() - 1; i++) {
+            for (int j = i + 1; j < particles.size(); j++) {
+                StatesOfMatterParticle p1 = p(i);
+                StatesOfMatterParticle p2 = p(j);
+
+                double dx = p2.getX() - p1.getX();
+                double dy = p2.getY() - p1.getY();
+
+                potential += ljp.evaluate(dx, dy);
+            }
+        }
+
+        return potential;
+    }
+}
