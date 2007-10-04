275b006812a2514230735ffb12648508c19f11c3
jdegoes <jdegoes@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-10-04 17:51:11 +0000

    website: updates to deployment process, front page, and static html

--- website/index.php
+++ website/index.php
@@ -87,7 +87,7 @@
 				<table>
 					<tr>
 						<td class="warning-notice">
-							<strong>Warning:</strong> Our web traffic has increased dramatically. We are working to accommodate this increase.
+							<strong>Update:<strong> Thanks to CU's ITS team for supporting switch to new, faster server that accommodates our increase in traffic. Email phethelp@colorado.edu with any further web issues.
 						</td>
 						
 						<td class="links">
