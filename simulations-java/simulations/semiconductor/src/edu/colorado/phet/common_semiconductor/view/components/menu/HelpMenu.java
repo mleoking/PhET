baaea2005e6b170d9bc380a844102c1d7e974b2a
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-10-06 04:27:41 +0000

    10-5-2007: Updated about dialog to standard version

--- simulations-java/simulations/semiconductor/src/edu/colorado/phet/common_semiconductor/view/components/menu/HelpMenu.java
+++ simulations-java/simulations/semiconductor/src/edu/colorado/phet/common_semiconductor/view/components/menu/HelpMenu.java
@@ -7,8 +7,10 @@
 package edu.colorado.phet.common_semiconductor.view.components.menu;
 
 import edu.colorado.phet.common.phetcommon.view.util.SimStrings;
+import edu.colorado.phet.common.phetcommon.application.PhetAboutDialog;
 import edu.colorado.phet.common_semiconductor.application.PhetApplication;
 import edu.colorado.phet.common_semiconductor.util.VersionUtils;
+import edu.colorado.phet.common_semiconductor.view.PhetFrame;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
@@ -20,23 +22,15 @@ public class HelpMenu extends JMenu {
         SimStrings.setStrings( "localization/SemiConductorPCStrings" );
     }
 
-    public HelpMenu( final PhetApplication app ) {
+    public HelpMenu( final PhetFrame phetFrame ) {
         super( SimStrings.get( "HelpMenu.HelpMenu" ) );
         this.setMnemonic( SimStrings.get( "HelpMenu.HelpMnemonic" ).charAt( 0 ) );
 
         final JMenuItem about = new JMenuItem( SimStrings.get( "HelpMenu.AboutMenuItem" ) );
         about.setMnemonic( SimStrings.get( "HelpMenu.AboutMnemonic" ).charAt( 0 ) );
-        final String name = app.getApplicationDescriptor().getWindowTitle();
-        String desc = app.getApplicationDescriptor().getDescription();
-        String version = app.getApplicationDescriptor().getVersion();
-        String message = name + "\n" + desc + "\n" + SimStrings.get( "HelpMenu.VersionLabel" ) + ": " + version;
-        VersionUtils.VersionInfo inf = VersionUtils.readVersionInfo( app );
-        message += "\n" + SimStrings.get( "HelpMenu.BuilderNumberLabel" ) + ": " + inf.getBuildNumber();
-        message += "\n" + SimStrings.get( "HelpMenu.BuilderTimeLabel" ) + ": " + inf.getBuildTime();
-        final String msg = message;
         about.addActionListener( new ActionListener() {
             public void actionPerformed( ActionEvent e ) {
-                JOptionPane.showMessageDialog( about, msg, SimStrings.get( "HelpMenu.AboutLabel" ) + " " + name, JOptionPane.INFORMATION_MESSAGE );
+                new PhetAboutDialog( phetFrame, "semiconductor").show( );
             }
         } );
         add( about );
