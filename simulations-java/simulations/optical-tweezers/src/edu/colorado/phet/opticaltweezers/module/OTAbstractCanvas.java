c4909b222b6c2cf7c7083e723546beea9332df6d
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-10-09 20:04:34 +0000

    10/9/07 - migrate updateLayout to PhetPCanvas

--- simulations-java/simulations/optical-tweezers/src/edu/colorado/phet/opticaltweezers/module/OTAbstractCanvas.java
+++ simulations-java/simulations/optical-tweezers/src/edu/colorado/phet/opticaltweezers/module/OTAbstractCanvas.java
@@ -3,8 +3,6 @@
 package edu.colorado.phet.opticaltweezers.module;
 
 import java.awt.Dimension;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
 
 import edu.colorado.phet.common.piccolophet.PhetPCanvas;
 import edu.umd.cs.piccolo.PNode;
@@ -29,20 +27,7 @@ public abstract class OTAbstractCanvas extends PhetPCanvas {
         // Root of our scene graph
         _rootNode = new PNode();
         addWorldChild( _rootNode );
-        
-        // When the canvas is resized...
-        addComponentListener( new ComponentAdapter() {
-            public void componentResized( ComponentEvent e ) {
-                // update the layout
-                updateLayout();
-            }
-        } );
     }
-    
-    /**
-     * Updates the layout when the canvas size is changed.
-     */
-    public abstract void updateLayout();
     
     /**
      * Adds a node to the canvas' root node.
