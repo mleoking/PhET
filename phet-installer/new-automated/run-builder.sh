55ddcdf42ff2189712f8e3c2248e29f89e68a1b2
jdegoes <jdegoes@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-10-12 20:56:52 +0000

    installer: added preliminary script to run installer, cleanup, & do post-build copy to phet distribution directory (not tested).

---
+++ phet-installer/new-automated/run-builder.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+/usr/local/php/bin/php build-install.php --full | tee installer-builder-log.txt
+
+rm -rf ./website/* | tee installer-builder-log.txt
+cp ./installer-output/*.* ../phet-dist/ | tee installer-builder-log.txt
