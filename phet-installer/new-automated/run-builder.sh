526e14006299eea9e1808f45ac60649038342ebc
jdegoes <jdegoes@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-10-15 20:23:26 +0000

    installer: bug fixes to installer script

--- phet-installer/new-automated/run-builder.sh
+++ phet-installer/new-automated/run-builder.sh
@@ -1,36 +1,36 @@
 #!/bin/sh
-echo "Performing SVN update" | tee installer-builder-log.txt
+echo "Performing SVN update" |& tee --append installer-builder-log.txt
 
 svn update
 
 if [ "$?" -ne "0" ]; then
-  echo "Error performing SVN update" | tee installer-builder-log.txt
+  echo "Error performing SVN update" |& tee --append installer-builder-log.txt
   exit 1
 fi
 
-echo "Building all installers" | tee installer-builder-log.txt
+echo "Building all installers" |& tee --append installer-builder-log.txt
 
-/usr/local/php/bin/php build-install.php --full | tee installer-builder-log.txt
+/usr/local/php/bin/php build-install.php --full |& tee --append installer-builder-log.txt
 
 if [ "$?" -ne "0" ]; then
-  echo "Error building installer" | tee installer-builder-log.txt
+  echo "Error building installer" |& tee --append installer-builder-log.txt
   exit 1
 fi
 
-echo "Removing temporary files" | tee installer-builder-log.txt
+echo "Removing temporary files" |& tee --append installer-builder-log.txt
 
-rm -rf ./temp/website/* | tee installer-builder-log.txt
+rm -rf ./temp/website/* |& tee --append installer-builder-log.txt
 
 if [ "$?" -ne "0" ]; then
-  echo "Error removing temporary files" | tee installer-builder-log.txt
+  echo "Error removing temporary files" |& tee --append installer-builder-log.txt
   exit 1
 fi
 
-echo "Copying new installers to distribution directory" | tee installer-builder-log.txt
+echo "Copying new installers to distribution directory" |& tee --append installer-builder-log.txt
 
-cp ./temp/installer-output/*.* ../phet-dist/ | tee installer-builder-log.txt
+cp ./temp/installer-output/*.* ../phet-dist/ |& tee --append installer-builder-log.txt
 
 if [ "$?" -ne "0" ]; then
-  echo "Error copying new installers to distribution directory" | tee installer-builder-log.txt
+  echo "Error copying new installers to distribution directory" |& tee --append installer-builder-log.txt
   exit 1
 fi
