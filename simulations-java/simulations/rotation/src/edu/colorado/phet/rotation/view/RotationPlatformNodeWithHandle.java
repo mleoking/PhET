e3b459c098b30a7547ed01eda3ead4504b1efb1f
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-10-28 22:34:17 +0000

    cleanup

---
+++ simulations-java/simulations/rotation/src/edu/colorado/phet/rotation/view/RotationPlatformNodeWithHandle.java
@@ -0,0 +1,23 @@
+package edu.colorado.phet.rotation.view;
+
+import java.awt.*;
+
+import edu.colorado.phet.rotation.model.RotationPlatform;
+import edu.colorado.phet.common.piccolophet.nodes.HandleNode;
+
+/**
+ * Created by: Sam
+* Oct 28, 2007 at 4:34:09 PM
+*/
+public class RotationPlatformNodeWithHandle extends RotationPlatformNode {
+    private double handleHeight = 10 * RotationPlayAreaNode.SCALE;
+
+    public RotationPlatformNodeWithHandle( final RotationPlatform rotationPlatform ) {
+        super( rotationPlatform );
+        HandleNode handleNode2 = new HandleNode( handleHeight / 2 * 7, handleHeight * 7, Color.gray );
+        handleNode2.setStroke( new BasicStroke( 1.0f / 50.0f ) );
+        handleNode2.setOffset( rotationPlatform.getRadius() + handleNode2.getFullBounds().getWidth() * 0.9, handleNode2.getFullBounds().getHeight() / 2 );
+        handleNode2.rotate( Math.PI );
+        super.addContentNode( handleNode2 );
+    }
+}
