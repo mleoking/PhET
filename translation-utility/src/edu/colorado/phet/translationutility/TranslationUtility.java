d8505ba45e6c54b3757a51f5ad633597e8acd65c
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-11-01 17:32:49 +0000

    publish

--- translation-utility/src/edu/colorado/phet/translationutility/TranslationUtility.java
+++ translation-utility/src/edu/colorado/phet/translationutility/TranslationUtility.java
@@ -18,8 +18,8 @@ import edu.colorado.phet.common.phetcommon.view.util.SwingUtils;
 public class TranslationUtility extends JFrame {
     
     private static final String NAME = "PhET Simulation Translator";
-    private static final String VERSION = "0.00.01";
-    private static final String SVN_REVISION = "18366";
+    private static final String VERSION = "0.00.02";
+    private static final String SVN_REVISION = "18434";
     
     private static final String TITLE = NAME + " : " + VERSION + " (" + SVN_REVISION + ")";
     
