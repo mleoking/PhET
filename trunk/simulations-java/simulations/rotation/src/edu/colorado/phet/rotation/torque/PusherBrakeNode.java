9015d4a0458c82fee471949b1bab19c667cf4d47
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-11-07 07:29:39 +0000

    changed brake graphic from pusher to brake pad

--- trunk/simulations-java/simulations/rotation/src/edu/colorado/phet/rotation/torque/PusherBrakeNode.java
+++ trunk/simulations-java/simulations/rotation/src/edu/colorado/phet/rotation/torque/PusherBrakeNode.java
@@ -1,16 +1,17 @@
 package edu.colorado.phet.rotation.torque;
 
 import java.awt.*;
-import java.awt.geom.AffineTransform;
 import java.awt.geom.Point2D;
+import java.awt.geom.RoundRectangle2D;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 import edu.colorado.phet.common.phetcommon.math.AbstractVector2D;
-import edu.colorado.phet.common.phetcommon.math.Function;
 import edu.colorado.phet.common.phetcommon.math.MathUtil;
 import edu.colorado.phet.common.phetcommon.math.Vector2D;
+import edu.colorado.phet.common.phetcommon.view.util.BufferedImageUtils;
 import edu.colorado.phet.common.piccolophet.event.CursorHandler;
-import edu.colorado.phet.common.piccolophet.nodes.HandleNode;
+import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.colorado.phet.rotation.RotationResources;
 import edu.colorado.phet.rotation.model.RotationPlatform;
 import edu.colorado.phet.rotation.view.RotationPlayAreaNode;
@@ -23,12 +24,13 @@ import edu.umd.cs.piccolo.nodes.PImage;
  * Author: Sam Reid
  * Aug 21, 2007, 3:40:17 AM
  */
-public class BrakeNode extends PNode {
+public class PusherBrakeNode extends PNode {
     private RotationPlatform rotationPlatform;
     private TorqueModel torqueModel;
-    private PNode im;
+    private PhetPPath block;
+    private PImage im = new PImage();
 
-    public BrakeNode( final RotationPlatform rotationPlatform, final TorqueModel torqueModel ) {
+    public PusherBrakeNode( final RotationPlatform rotationPlatform, final TorqueModel torqueModel ) {
         this.rotationPlatform = rotationPlatform;
         this.torqueModel = torqueModel;
         rotationPlatform.addListener( new RotationPlatform.Adapter() {
@@ -38,22 +40,22 @@ public class BrakeNode extends PNode {
         } );
         torqueModel.addListener( new TorqueModel.Adapter() {
             public void brakePressureChanged() {
+                updateImage();
                 updateTransform();
             }
         } );
+//        Color blockColor = new Color( 207, 187, 108 );
+        Color blockColor = new Color( 20, 18, 10 );
+//        block = new PhetPPath( new Rectangle2D.Double( 0, 0, 0.5, 0.5 ), blockColor, new BasicStroke( (float) ( 1 * RotationPlayAreaNode.SCALE ) ), Color.darkGray );
+        block = new PhetPPath( new RoundRectangle2D.Double( 0, 0, 0.5, 0.5, 0.1, 0.1 ), blockColor, new BasicStroke( (float) ( 1 * RotationPlayAreaNode.SCALE ) ), Color.darkGray );
+        block.translate( 0, -block.getFullBounds().getHeight() / 2.0 );
+        addChild( block );
 
-        try {
-            im = createImageWithHandle();
-        }
-        catch( IOException e ) {
-            e.printStackTrace();
-        }
-        double imageScale = RotationPlayAreaNode.SCALE * 0.6;
-        im.transformBy( AffineTransform.getScaleInstance( -imageScale, imageScale ) );
+        updateImage();
 
-        im.rotate( Math.PI / 2 );
-//        im.translate( -im.getFullBounds().getWidth() / imageScale * 2.0, 0 );
-        im.translate( -im.getFullBounds().getWidth() / imageScale , 0 );
+        double imageScale = RotationPlayAreaNode.SCALE * 0.8;
+        im.scale( imageScale );
+        im.translate( block.getFullBounds().getWidth() / imageScale * 0.9, -im.getFullBounds().getHeight() / imageScale / 2.0 * 0.7 );
 
         addChild( im );
         addInputEventListener( new CursorHandler() );
@@ -84,32 +86,40 @@ public class BrakeNode extends PNode {
             }
 
             private Point2D getPoint( PInputEvent event ) {
-                return event.getPositionRelativeTo( BrakeNode.this.getParent() );
+                return event.getPositionRelativeTo( PusherBrakeNode.this.getParent() );
             }
         } );
 
         updateTransform();
+        updateImage();
     }
 
-    private PNode createImageWithHandle() throws IOException {
-        final PImage image = new PImage( RotationResources.loadBufferedImage( "brake-pad.gif" ) );
-        return image;
+    private void updateImage() {
+        double numImages = 15;
+//        int image = (int) ( ( torqueModel.getBrakeForceMagnitude() / ( FullTorqueControlPanel.MAX_BRAKE - FullTorqueControlPanel.MIN_BRAKE ) ) * ( numImages - 1.0 ) + 1.0 );
+        int image = (int) ( ( torqueModel.getBrakePressure() / ( FullTorqueControlPanel.MAX_BRAKE - FullTorqueControlPanel.MIN_BRAKE ) ) * ( numImages - 1.0 ) + 1.0 );
+        if ( image == 0 ) {//todo: resolve this workaround
+            image = 1;
+        }
+//        System.out.println( "torqueModel.getBRakeForce= " + torqueModel.getBrakeForceMagnitude() + ", image=" + image );
+        String imageString = "pusher-leaning_00" + ( ( "" + image ).length() == 1 ? "0" : "" ) + image + ".gif";
+        try {
+            BufferedImage i = RotationResources.loadBufferedImage( "animations/" + imageString );
+            i = BufferedImageUtils.flipX( i );
+            i = BufferedImageUtils.flipY( i );
+            im.setImage( i );
+        }
+        catch( IOException e ) {
+            e.printStackTrace();
+        }
     }
 
     private void updateTransform() {
         double angle = -Math.PI / 4;
-//        boolean awayFromPlatform = torqueModel.getBrakePressure() == 0;
-        final double farAway = 0.00;
-        final double close = -0.09;
-        Function.LinearFunction linearFunction = new Function.LinearFunction( 0, 1, farAway, close );
-        double x = linearFunction.evaluate( torqueModel.getBrakePressure() );
-        x = MathUtil.clamp( close, x, farAway );
+//        boolean awayFromPlatform = torqueModel.getBrakeForceMagnitude() == 0;
+        boolean awayFromPlatform = torqueModel.getBrakePressure() == 0;
 
-        if (torqueModel.getBrakePressure()==0){
-            x=0.05;
-        }
-
-        AbstractVector2D vec = Vector2D.Double.parseAngleAndMagnitude( rotationPlatform.getRadius() + x, angle );
+        AbstractVector2D vec = Vector2D.Double.parseAngleAndMagnitude( rotationPlatform.getRadius() + ( awayFromPlatform ? 0.08 : 0.00 ), angle );
         setOffset( vec.getDestination( rotationPlatform.getCenter() ) );
         setRotation( angle );
     }
