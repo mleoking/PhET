458ce4c90c33e110b9ef5c861752f37258e52f57
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-11-12 22:43:40 +0000

    added debug info

--- trunk/simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/util/DebugPiccoloTree.java
+++ trunk/simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/util/DebugPiccoloTree.java
@@ -10,6 +10,7 @@
  */
 package edu.colorado.phet.common.piccolophet.util;
 
+import edu.umd.cs.piccolo.PCamera;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.nodes.PText;
 
@@ -32,9 +33,29 @@ public class DebugPiccoloTree {
         String additionalInfo = getAdditionalInfo( root );
         printAll( className, additionalInfo );
         for ( int i = 0; i < root.getChildrenReference().size(); i++ ) {
-            PNode child = (PNode) root.getChildrenReference().get( i );
-            printTree( child, pre + "\t" );
+            printTree( (PNode) root.getChildrenReference().get( i ), pre + "\t" );
         }
+        if ( root instanceof PCamera ) {
+            PCamera r = (PCamera) root;
+            for ( int i = 0; i < r.getLayerCount(); i++ ) {
+                printTree( r.getLayer( i ), pre + "\t" );
+            }
+        }
+    }
+
+    public static int piccoloCount( PNode camera ) {
+        int sum = 1;
+        if ( camera instanceof PCamera ) {
+            PCamera cam = (PCamera) camera;
+            for ( int i = 0; i < cam.getLayerCount(); i++ ) {
+                PNode layer = cam.getLayer( i );
+                sum += piccoloCount( layer );
+            }
+        }
+        for ( int i = 0; i < camera.getChildrenCount(); i++ ) {
+            sum += piccoloCount( camera.getChild( i ) );
+        }
+        return sum;
     }
 
     private void printAll( String className, String additionalInfo ) {
