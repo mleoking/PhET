91d13ddf51bcc7076da4eb4487233c04a8ca6b0d
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-11-13 18:34:43 +0000

    11/12/07 - TUMenuBar

--- translation-utility/src/edu/colorado/phet/translationutility/TranslationUtility.java
+++ translation-utility/src/edu/colorado/phet/translationutility/TranslationUtility.java
@@ -26,47 +26,32 @@ public class TranslationUtility extends JFrame {
     
     public static void main( String[] args ) {
         
-        String title =  ProjectProperties.getTitle();
-        
+        String title = ProjectProperties.getTitle();
+
         // prompt the user for initialization info
         InitializationDialog initDialog = new InitializationDialog( title );
         SwingUtils.centerWindowOnScreen( initDialog );
         initDialog.show();
-        
-        // open the primary user interface
-        if ( initDialog.isContinue() ) {
-            
-            String jarFileName = initDialog.getJarFileName();
-            String targetCountryCode = initDialog.getTargetCountryCode();
-            String[] commonProjectNames = ProjectProperties.getCommonProjectNames();
-            JarFileManager jarFileManager = new JarFileManager( jarFileName, commonProjectNames );
-            
-            boolean autoTranslate = initDialog.isAutoTranslateEnabled();
-            TranslationPanel translationPanel = new TranslationPanel( jarFileManager, SOURCE_COUNTRY_CODE, targetCountryCode, autoTranslate );
-            
-            JMenuBar menuBar = new JMenuBar();
-            JMenu fileMenu = new JMenu( TUResources.getString( "menu.file" ) );
-            fileMenu.setMnemonic( TUResources.getChar( "menu.file.mnemonic", 'F' ) );
-            menuBar.add( fileMenu );
-            JMenuItem exitMenuItem = new JMenuItem( TUResources.getString( "menu.item.exit" ), TUResources.getChar( "menu.item.exit.mnemonic", 'x' ) );
-            fileMenu.add( exitMenuItem );
-            exitMenuItem.addActionListener( new ActionListener() {
-                public void actionPerformed( ActionEvent e ) {
-                    System.exit( 0 );
-                }
-            });
-            
-            JFrame frame = new JFrame( title );
-            frame.setJMenuBar( menuBar );
-            frame.getContentPane().add( translationPanel );
-            frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-            frame.pack();
-            //WORKAROUND: increase the width so we don't get a horizontal scrollbar
-            frame.setBounds( (int)frame.getBounds().getX(), (int)frame.getBounds().getY(), (int)frame.getBounds().getWidth() + 30, (int)frame.getBounds().getHeight() );
-            frame.show();
-        }
-        else {
+        if ( !initDialog.isContinue() ) {
             System.exit( 0 );
         }
+
+        // open the primary user interface
+        String jarFileName = initDialog.getJarFileName();
+        String targetCountryCode = initDialog.getTargetCountryCode();
+        String[] commonProjectNames = ProjectProperties.getCommonProjectNames();
+        JarFileManager jarFileManager = new JarFileManager( jarFileName, commonProjectNames );
+
+        boolean autoTranslate = initDialog.isAutoTranslateEnabled();
+        TranslationPanel translationPanel = new TranslationPanel( jarFileManager, SOURCE_COUNTRY_CODE, targetCountryCode, autoTranslate );
+
+        JFrame frame = new JFrame( title );
+        frame.setJMenuBar( new TUMenuBar() );
+        frame.getContentPane().add( translationPanel );
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        frame.pack();
+        //WORKAROUND: increase the width so we don't get a horizontal scrollbar
+        frame.setBounds( (int) frame.getBounds().getX(), (int) frame.getBounds().getY(), (int) frame.getBounds().getWidth() + 30, (int) frame.getBounds().getHeight() );
+        frame.show();
     }
 }
