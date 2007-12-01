f1edbf2c9c6a304e2cc1f7bed1b1ead139725d5e
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-12-01 22:22:35 +0000

    graphics workaround for intro panel

--- simulations-java/simulations/rotation/src/edu/colorado/phet/rotation/RotationIntroSimulationPanel.java
+++ simulations-java/simulations/rotation/src/edu/colorado/phet/rotation/RotationIntroSimulationPanel.java
@@ -1,5 +1,15 @@
 package edu.colorado.phet.rotation;
 
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import javax.swing.*;
+
+import edu.colorado.phet.common.phetcommon.model.clock.ClockAdapter;
+import edu.colorado.phet.common.phetcommon.model.clock.ClockEvent;
+import edu.colorado.phet.common.phetcommon.application.Module;
+import edu.colorado.phet.common.piccolophet.event.PDebugKeyHandler;
 import edu.colorado.phet.rotation.view.AbstractRotationSimulationPanel;
 import edu.colorado.phet.rotation.view.RotationSimPlayAreaNode;
 import edu.umd.cs.piccolox.pswing.PSwing;
@@ -16,7 +26,8 @@ public class RotationIntroSimulationPanel extends AbstractIntroSimulationPanel {
     private RotationIntroControlPanel introSimControlPanel;
     private PSwing introSimControlPanelPSwing;
 
-    public RotationIntroSimulationPanel( RotationIntroModule introModule ) {
+    public RotationIntroSimulationPanel( RotationIntroModule introModule,JFrame frame ) {
+        super( (JComponent) frame.getContentPane(),introModule);
         this.introModule = introModule;
         playAreaNode = new RotationSimPlayAreaNode( introModule.getRotationModel(), introModule.getVectorViewModel(), introModule.getAngleUnitModel() );
         playAreaNode.setOriginNodeVisible( false );
@@ -30,5 +41,38 @@ public class RotationIntroSimulationPanel extends AbstractIntroSimulationPanel {
 
         updateLayout();
         setBackground( AbstractRotationSimulationPanel.PLAY_AREA_BACKGROUND_COLOR );
+        addKeyListener( new PDebugKeyHandler() );
+        requestFocus();
+        final Timer timer=new Timer( introModule.getRotationClock().getDelay(), new ActionListener() {
+            public void actionPerformed( ActionEvent e ) {
+                doPaintImm();
+            }
+        } );
+        introModule.addListener( new Module.Listener() {
+            public void activated() {
+                timer.start();
+            }
+
+            public void deactivated() {
+                timer.stop();
+            }
+        } );
     }
+    public void doPaintImm(){
+        super.paintImmediately( 0,0,getWidth(), getHeight() );
+//        System.out.println( "RotationIntroSimulationPanel.doPaintImm" );
+    }
+
+    public void paintImmediately() {
+//        super.paintImmediately();
+    }
+
+    public void paintImmediately( int x, int y, int w, int h ) {
+//        super.paintImmediately( x, y, w, h );
+    }
+
+    public void paintImmediately( Rectangle r ) {
+//        super.paintImmediately( r );
+    }
+
 }
