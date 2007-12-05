248c8435ba1cdfddf8c503250a71dfbbfd627b30
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-12-05 23:43:37 +0000

    added audio

--- simulations-java/simulations/moving-man/src/edu/colorado/phet/movingman/model/CollisionAudioEffects.java
+++ simulations-java/simulations/moving-man/src/edu/colorado/phet/movingman/model/CollisionAudioEffects.java
@@ -47,8 +47,8 @@ public class CollisionAudioEffects extends Man.Adapter {
     public void collided( Man man ) {
         if( lastCollisionLocation.equals( FREE ) ) {
             if( soundEnabled ) {
-                URL crash = CollisionAudioEffects.class.getClassLoader().getResource( "moving-man/audio/smash0.wav" );
-                JSAudioPlayer.playNoBlock( crash );
+//                URL crash = CollisionAudioEffects.class.getClassLoader().getResource( "moving-man/audio/smash0.wav" );
+                JSAudioPlayer.playNoBlock( Thread.currentThread().getContextClassLoader().getResource( "moving-man/audio/smash0.wav" ));
                 lastCollisionLocation = man.isMaximum() ? RIGHT : LEFT;
             }
         }
