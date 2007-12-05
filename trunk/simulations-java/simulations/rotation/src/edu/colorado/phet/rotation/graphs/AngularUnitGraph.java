de9bd8bfcb6aad08bf8ef370d3082634bda59da0
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-12-05 11:11:44 +0000

    removed unused label parameter

--- trunk/simulations-java/simulations/rotation/src/edu/colorado/phet/rotation/graphs/AngularUnitGraph.java
+++ trunk/simulations-java/simulations/rotation/src/edu/colorado/phet/rotation/graphs/AngularUnitGraph.java
@@ -20,7 +20,7 @@ public class AngularUnitGraph extends RotationGraph {
     private double minRad;
     private double maxRad;
 
-    public AngularUnitGraph( PhetPCanvas pSwingCanvas, ControlGraphSeries series, String label, String title, AngleUnitModel angleUnitModel, String unitsRad, String unitsDeg, double minRad, double maxRad, RotationModel motionModel, boolean editable, TimeSeriesModel timeSeriesModel, UpdateStrategy updateStrategy, double maxDomainValue, RotationPlatform iPositionDriven ) {
+    public AngularUnitGraph( PhetPCanvas pSwingCanvas, ControlGraphSeries series, String title, AngleUnitModel angleUnitModel, String unitsRad, String unitsDeg, double minRad, double maxRad, RotationModel motionModel, boolean editable, TimeSeriesModel timeSeriesModel, UpdateStrategy updateStrategy, double maxDomainValue, RotationPlatform iPositionDriven ) {
         super( pSwingCanvas, series, title, unitsRad, minRad, maxRad, motionModel, editable, timeSeriesModel, updateStrategy, maxDomainValue, iPositionDriven );
         this.angleUnitModel = angleUnitModel;
         this.unitsRad = unitsRad;
