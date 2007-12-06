97fb1714db3a9908ece764306b926495594a4347
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-12-06 06:52:18 +0000

    fixed: graph x-axis should be time (not ticks)

--- simulations-java/simulations/moving-man/src/edu/colorado/phet/movingman/motion/movingman/MovingManMotionModel.java
+++ simulations-java/simulations/moving-man/src/edu/colorado/phet/movingman/motion/movingman/MovingManMotionModel.java
@@ -24,7 +24,7 @@ public class MovingManMotionModel extends MotionModel implements UpdateableObjec
     private ControlGraphSeries vSeries = new ControlGraphSeries( "V", Color.red, "v", "m/s", new BasicStroke( 2 ), true, null, v );
     private ControlGraphSeries aSeries = new ControlGraphSeries( "A", Color.green, "a", "m/s^2", new BasicStroke( 2 ), true, null, a );
 
-    public static final int MAX_T = 500;
+    public static final int MAX_T = 20;
 
     private double min = -10;
     private double max = 10;
