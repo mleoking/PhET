71f3e54c8885dd264927a3dfc6b8a341d066ec04
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-12-10 20:46:05 +0000

    12/10/07 - show language names at top of columns in TranslationPanel 12/10/07 - revise help for language selection 12/10/07 - select language from a combo box, with option to enter a PhET-assigned language code for a non-ISO language 12/10/07 - add ISO language codes and names to project properties file

---
+++ trunk/translation-utility/src/edu/colorado/phet/translationutility/control/LanguageComboBox.java
@@ -0,0 +1,105 @@
+/* Copyright 2007, University of Colorado */
+
+package edu.colorado.phet.translationutility.control;
+
+import java.awt.Color;
+import java.awt.Component;
+
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.ListCellRenderer;
+
+import edu.colorado.phet.translationutility.TUResources;
+import edu.colorado.phet.translationutility.util.LanguageCodes;
+
+
+public class LanguageComboBox extends JComboBox {
+    
+    public static final String CUSTOM_NAME = TUResources.getString( "label.custom" );
+
+    private static class LanguageChoice {
+
+        private final String _name;
+        private final String _code;
+
+        public LanguageChoice( String name, String code ) {
+            _name = name;
+            _code = code;
+        }
+
+        public String getName() {
+            return _name;
+        }
+
+        public String getCode() {
+            return _code;
+        }
+    }
+    
+    private static class LanguageRenderer extends JLabel implements ListCellRenderer {
+        
+        public Component getListCellRendererComponent( JList list, Object value, int index, boolean isSelected, boolean cellHasFocus ) {
+            
+            if ( isSelected ) {
+                setForeground( Color.RED );
+            }
+            else {
+                setBackground( list.getBackground() );
+                setForeground( list.getForeground() );
+            }
+
+            // Set text.
+            LanguageChoice choice = (LanguageChoice) value;
+            String name = choice.getName();
+            String code = choice.getCode();
+            String text = name;
+            if ( code != null ) {
+               text = text + " (" + code + ")";
+            }
+            setText( text );
+
+            return this;
+        }
+    }
+    
+    public LanguageComboBox() {
+        super();
+        
+        setRenderer( new LanguageRenderer() );
+        
+        LanguageCodes lc = LanguageCodes.getInstance();
+        String[] names = lc.getSortedNames();
+        for ( int i = 0; i < names.length; i++ ) {
+            String name = names[i];
+            String code = lc.getCode( name );
+            addItem( new LanguageChoice( name, code ) );
+        }
+        
+        addItem( new LanguageChoice( CUSTOM_NAME, null ) );
+    }
+    
+    public String getSelectedCode() {
+        String code = null;
+        LanguageChoice choice = (LanguageChoice) getSelectedItem();
+        if ( !choice.getName().equals( CUSTOM_NAME ) ) {
+            code = choice.getCode();
+        }
+        return code;
+    }
+
+    public String getSelectedName() {
+        LanguageChoice choice = (LanguageChoice) getSelectedItem();
+        String name = choice.getName();
+        if ( name.equals( CUSTOM_NAME ) ) {
+            name = null;
+        }
+        return name;
+    }
+    
+    public boolean isCustomSelected() {
+        LanguageChoice choice = (LanguageChoice) getSelectedItem();
+        String name = choice.getName();
+        return ( name.equals( CUSTOM_NAME ) );
+    }
+}
