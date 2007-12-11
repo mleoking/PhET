0f1d984d7061f6e650cf1baa220e94b2f196ece1
jdegoes <jdegoes@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-12-11 00:31:13 +0000

    added rezip for discrepancies

--- simulations-java/build-tools/phet-build/src/edu/colorado/phet/build/FileUtils.java
+++ simulations-java/build-tools/phet-build/src/edu/colorado/phet/build/FileUtils.java
@@ -1,21 +1,28 @@
 /* Copyright 2007, University of Colorado */
 package edu.colorado.phet.build;
 
+import junit.framework.TestCase;
+
 import java.io.*;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.regex.Pattern;
+import java.util.zip.CRC32;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipOutputStream;
 
-public class FileUtils {
+public class FileUtils extends TestCase {
     private static String DEFAULT_ENCODING = "utf-8";
 
-    public static String loadFileAsString( File file ) throws IOException {
-        return loadFileAsString( file, DEFAULT_ENCODING );
+    public static String loadFileAsString(File file) throws IOException {
+        return loadFileAsString(file, DEFAULT_ENCODING);
     }
 
-    public static String loadFileAsString( File file, String encoding ) throws IOException {
-        InputStream inStream = new FileInputStream( file );
+    public static String loadFileAsString(File file, String encoding) throws IOException {
+        InputStream inStream = new FileInputStream(file);
 
         ByteArrayOutputStream outStream;
 
@@ -23,8 +30,8 @@ public class FileUtils {
             outStream = new ByteArrayOutputStream();
 
             int c;
-            while( ( c = inStream.read() ) >= 0 ) {
-                outStream.write( c );
+            while ((c = inStream.read()) >= 0) {
+                outStream.write(c);
             }
             outStream.flush();
         }
@@ -32,27 +39,27 @@ public class FileUtils {
             inStream.close();
         }
 
-        return new String( outStream.toByteArray(), encoding );
+        return new String(outStream.toByteArray(), encoding);
     }
 
 
-    public static void filter( File inputFile, File outputFile, HashMap map ) throws IOException {
-        filter( inputFile, outputFile, map, DEFAULT_ENCODING );
+    public static void filter(File inputFile, File outputFile, HashMap map) throws IOException {
+        filter(inputFile, outputFile, map, DEFAULT_ENCODING);
     }
 
-    public static void writeString( File outputFile, String text, String encoding ) throws IOException {
-        writeBytes( outputFile, text.getBytes( encoding ) );
+    public static void writeString(File outputFile, String text, String encoding) throws IOException {
+        writeBytes(outputFile, text.getBytes(encoding));
     }
 
-    public static void writeString( File outputFile, String text ) throws IOException {
-        writeString( outputFile, text, DEFAULT_ENCODING );
+    public static void writeString(File outputFile, String text) throws IOException {
+        writeString(outputFile, text, DEFAULT_ENCODING);
     }
 
-    public static void writeBytes( File outputFile, byte[] bytes ) throws IOException {
+    public static void writeBytes(File outputFile, byte[] bytes) throws IOException {
         outputFile.getParentFile().mkdirs();
-        FileOutputStream outputStream = new FileOutputStream( outputFile );
+        FileOutputStream outputStream = new FileOutputStream(outputFile);
         try {
-            outputStream.write( bytes );
+            outputStream.write(bytes);
         }
         finally {
             outputStream.close();
@@ -71,7 +78,7 @@ public class FileUtils {
                 changed = true;
 
                 String before = body.substring(0, indexOfFindText);
-                String after  = body.substring(indexOfFindText + find.length());
+                String after = body.substring(indexOfFindText + find.length());
 
                 body = before + replacement + after;
             }
@@ -82,59 +89,157 @@ public class FileUtils {
         return body;
     }
 
-    public static String filter( HashMap map, String file ) {
+    public static String filter(HashMap map, String file) {
         Set set = map.keySet();
-        for( Iterator iterator = set.iterator(); iterator.hasNext(); ) {
+        for (Iterator iterator = set.iterator(); iterator.hasNext();) {
             String key = (String)iterator.next();
-            String value = (String)map.get( key );
+            String value = (String)map.get(key);
 
             //echo( "key = " + key + ", value=" + value );
 
-            file = replaceAll(file, "@" + key + "@", value );
+            file = replaceAll(file, "@" + key + "@", value);
         }
         return file;
     }
 
-    public static void filter( File source, File destFile, HashMap filterMap, String encoding ) throws IOException {
-        String text = loadFileAsString( source, encoding );
-        String result = filter( filterMap, text );
-        writeString( destFile, result, encoding );
+    public static void filter(File source, File destFile, HashMap filterMap, String encoding) throws IOException {
+        String text = loadFileAsString(source, encoding);
+        String result = filter(filterMap, text);
+        writeString(destFile, result, encoding);
     }
 
-    public static void delete( File file ) {
-        if ( file.isDirectory() ) {
+    public static void delete(File file) {
+        if (file.isDirectory()) {
             File[] children = file.listFiles();
 
-            for ( int i = 0; i < children.length; i++ ) {
-                delete( children[i] );
+            for (int i = 0; i < children.length; i++) {
+                delete(children[i]);
             }
         }
 
         file.delete();
     }
 
-    public static void copyTo( File source, File dest ) throws IOException {
-        InputStream in = new BufferedInputStream( new FileInputStream( source ) );
+    public static void copyTo(File source, File dest) throws IOException {
+        copyAndClose(new FileInputStream(source), new FileOutputStream(dest),true);
+    }
 
-        try {
-            OutputStream out = new BufferedOutputStream( new FileOutputStream( dest ) );
-
-            try {
-                int bytesRead;
-
-                byte[] buffer = new byte[1024];
-
-                while ( ( bytesRead = in.read( buffer) ) >= 0 ) {
-                    out.write( buffer, 0, bytesRead );
-                }
-            }
-            finally {
-                out.close();
-            }
-
+    public static void copy(InputStream source, OutputStream dest, boolean buffered) throws IOException {
+        if (buffered) {
+            source = new BufferedInputStream(source);
+            dest = new BufferedOutputStream(dest);
         }
-        finally {
-            in.close();
+
+        int bytesRead;
+
+        byte[] buffer = new byte[1024];
+
+        while ((bytesRead = source.read(buffer)) >= 0) {
+            dest.write(buffer, 0, bytesRead);
         }
     }
+
+    public static void copyAndClose(InputStream source, OutputStream dest,boolean buffered) throws IOException {
+        copy(source, dest,buffered);
+        source.close();
+        dest.close();
+    }
+
+    public static void unzip(File zipFileName, File targetDir) throws IOException {
+        unzip(zipFileName, targetDir, new FileFilter() {
+            public boolean accept(File file) {
+                return true;
+            }
+        });
+    }
+
+    public static void unzip(File zipFileName, File targetDir, FileFilter filter) throws IOException {
+        ZipFile zipFile = new ZipFile(zipFileName);
+
+        Enumeration enumeration = zipFile.entries();
+
+        while (enumeration.hasMoreElements()) {
+            ZipEntry entry = (ZipEntry)enumeration.nextElement();
+
+            String name = entry.getName();
+
+            if (filter.accept(new File(targetDir, name))) {
+                if (entry.isDirectory()) {
+                    new File(targetDir, name).mkdirs();
+                }
+                else {
+                    File targetFile = new File(targetDir, name);
+
+                    targetFile.getParentFile().mkdirs();
+
+                    InputStream source = zipFile.getInputStream(entry);
+                    FileOutputStream fileOutputStream = new FileOutputStream(targetFile);
+
+                    copyAndClose(source, fileOutputStream,false);
+
+                    source.close();
+                    fileOutputStream.close();
+                }
+            }
+        }
+        zipFile.close();
+    }
+
+    public static void testUnzip() throws IOException {
+        final Pattern excludePattern = Pattern.compile(Pattern.quote("quantum-tunneling") + "[\\\\/]localization[\\\\/]" + Pattern.quote("quantum-tunneling") + ".*\\.properties");
+
+        unzip(new File("/Users/jdegoes/Desktop/quantum-tunneling.jar"), new File("/Users/jdegoes/Desktop/temp-dir"), new FileFilter() {
+            public boolean accept(File file) {
+                if (excludePattern.matcher(file.getAbsolutePath()).find()) {
+                    return false;
+                }
+
+                return true;
+            }
+        });
+    }
+
+    public static void zipSingleFile(CRC32 crc, File rootDir, File file, ZipOutputStream zipOutputStream) throws IOException {
+        if (file.isDirectory()) {
+            File[] f = file.listFiles();
+            for (int i = 0; i < f.length; i++) {
+                zipSingleFile(crc, rootDir, f[i], zipOutputStream);
+            }
+        }
+        else {
+            String path = file.getAbsolutePath().substring(rootDir.getAbsolutePath().length());
+            path = path.replace('\\', '/');
+            if (!path.startsWith("/")) {
+                path = "/" + path;
+            }
+            ZipEntry zipEntry = new ZipEntry(path);
+//            zipEntry.setMethod(ZipEntry.STORED);
+//            zipEntry.setCompressedSize(file.length());
+//            zipEntry.setSize(file.length());
+//            zipEntry.setCrc(crc.getValue());
+
+
+            zipOutputStream.putNextEntry(zipEntry);
+
+            FileInputStream inputStream = new FileInputStream(file);
+            System.out.println("file.getAbsolutePath() = " + file.getAbsolutePath() + ", path=" + path);
+            copy(inputStream, zipOutputStream,false);
+
+
+            zipOutputStream.closeEntry();
+            inputStream.close();
+        }
+    }
+
+    public static void zip(CRC32 crc, File dir, File dest) throws IOException {
+        ZipOutputStream zipOutputStream = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(dest)));
+        zipSingleFile(crc, dir, dir, zipOutputStream);
+        zipOutputStream.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        zip(new CRC32(), new File("/Users/jdegoes/Desktop/temp-dir"), new File("/Users/jdegoes/Desktop/temp-dir-rezipped.zip"));
+    }
+
+
 }
--- simulations-java/build-tools/phet-build/src/edu/colorado/phet/build/translate/TranslationDiscrepancy.java
+++ simulations-java/build-tools/phet-build/src/edu/colorado/phet/build/translate/TranslationDiscrepancy.java
@@ -1,8 +1,16 @@
 package edu.colorado.phet.build.translate;
 
+import edu.colorado.phet.build.FileUtils;
 import edu.colorado.phet.build.PhetProject;
 
+import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Locale;
 import java.util.Set;
+import java.util.regex.Pattern;
+import java.util.zip.CRC32;
 
 public class TranslationDiscrepancy {
     private final Set extraLocal;
@@ -10,7 +18,7 @@ public class TranslationDiscrepancy {
     private final PhetProject phetProject;
     private final String flavor;
 
-    public TranslationDiscrepancy(Set extraLocal, Set extraRemote, PhetProject phetProject, String flavor) {
+    TranslationDiscrepancy(Set extraLocal, Set extraRemote, PhetProject phetProject, String flavor) {
         this.extraLocal  = extraLocal;
         this.extraRemote = extraRemote;
         this.phetProject = phetProject;
@@ -35,5 +43,39 @@ public class TranslationDiscrepancy {
 
     public String toString() {
         return "need to be removed from remote jar: " + extraRemote + ", " + "need to be added to remote jar: " + extraLocal + " ";
+    }
+
+    public void resolve(File resolveJAR) throws IOException {
+        final Pattern excludePattern = Pattern.compile(Pattern.quote(phetProject.getName()) + "[\\\\/]localization[\\\\/]" + Pattern.quote(phetProject.getName()) + ".*\\.properties");
+
+        String deployUrl = phetProject.getDeployedFlavorJarURL(flavor);
+
+        File jarFile = File.createTempFile(flavor, ".jar");
+
+        FileDownload.download(deployUrl, jarFile);
+
+        File tempUnzipDir = File.createTempFile(flavor + "-dir", "");
+
+        FileUtils.unzip(jarFile, tempUnzipDir, new FileFilter() {
+            public boolean accept(File file) {
+                if (excludePattern.matcher(file.getAbsolutePath()).find()) {
+                    return false;
+                }
+
+                return true;
+            }
+        });
+
+        File localizationDir = new File(tempUnzipDir, phetProject.getName() + File.separator + "localization");
+
+        localizationDir.mkdir();
+
+        for (Iterator iterator = extraLocal.iterator(); iterator.hasNext();) {
+            Locale locale = (Locale)iterator.next();
+
+            File source = phetProject.getTranslationFile(locale);
+            FileUtils.copyTo(source, new File(localizationDir, source.getName()));
+        }
+        FileUtils.zip(new CRC32(), localizationDir,resolveJAR);
     }
 }
