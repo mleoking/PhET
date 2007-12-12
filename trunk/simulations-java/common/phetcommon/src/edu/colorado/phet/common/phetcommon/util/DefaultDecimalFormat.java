681f00b5b390ddffbc686b05809ee54f91be22d0
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-12-12 05:21:59 +0000

    replaced manual conversion of ',' to '.' in number parsing with usage of DecimalFormat.getNumberInstance().parse(), using the default locale for older sims and the declared locale (in PhetResources) for newer sims)

--- trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/util/DefaultDecimalFormat.java
+++ trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/util/DefaultDecimalFormat.java
@@ -12,6 +12,9 @@ package edu.colorado.phet.common.phetcommon.util;
 
 import java.text.DecimalFormat;
 import java.text.FieldPosition;
+import java.text.ParseException;
+
+import edu.colorado.phet.common.phetcommon.resources.PhetResources;
 
 /**
  * This formatter never returns zero-values with a minus
@@ -35,10 +38,13 @@ public class DefaultDecimalFormat extends DecimalFormat {
         StringBuffer formattedText = decimalFormat.format( number, new StringBuffer(), fieldPosition );
         double parsed = 0;
         try {
-            parsed = Double.parseDouble( formattedText.toString().replace( ',', '.' ) );//to handle european 
+            parsed = DecimalFormat.getNumberInstance( PhetResources.readLocale() ).parse( formattedText.toString() ).doubleValue();//to handle european
         }
         catch( NumberFormatException numberFormatException ) {
             return decimalFormat.format( number, result, fieldPosition );
+        }
+        catch( ParseException e ) {
+            e.printStackTrace();
         }
         if ( parsed == 0 && formattedText.indexOf( "-" ) == 0 ) {
             result.append( formattedText.substring( 1 ) );
@@ -47,6 +53,11 @@ public class DefaultDecimalFormat extends DecimalFormat {
             result.append( formattedText );
         }
         return result;
+    }
+
+    public static void main( String[] args ) {
+        System.out.println( "new DecimalFormat( \"0.00\" ).format( -0.00001 )= " + new DecimalFormat( "0.00" ).format( -0.00001 ) );
+        System.out.println( "new DefaultDecimalFormat( \"0.00\" ).format( -0.00001 )= " + new DefaultDecimalFormat( "0.00" ).format( -0.00001 ) );
     }
 
 }
