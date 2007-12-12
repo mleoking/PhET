572cc616d7e692d5d019a949a44f76ee528a459f
jdegoes <jdegoes@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-12-12 18:28:36 +0000

    states-of-matter: completely refactored particle creation strategy as its creation method's parameters were multiplying beyond measure; new design enables cleaner PackedHexagonalParticleCreationStrategy

--- simulations-java/simulations/states-of-matter/src/edu/colorado/phet/statesofmatter/model/particle/RandomParticleCreationStrategy.java
+++ simulations-java/simulations/states-of-matter/src/edu/colorado/phet/statesofmatter/model/particle/RandomParticleCreationStrategy.java
@@ -1,9 +1,15 @@
 package edu.colorado.phet.statesofmatter.model.particle;
 
-import java.util.List;
-
 public class RandomParticleCreationStrategy implements ParticleCreationStrategy {
-    public StatesOfMatterParticle createNewParticle(List particles, double radius, double mass) {
+    private final double radius;
+    private final double mass;
+
+    public RandomParticleCreationStrategy(double radius, double mass) {
+        this.radius = radius;
+        this.mass   = mass;
+    }
+
+    public StatesOfMatterParticle createNewParticle() {
         return new StatesOfMatterParticle(Math.random(), Math.random(), radius, mass);
     }
 }
--- simulations-java/simulations/states-of-matter/src/edu/colorado/phet/statesofmatter/model/particle/ZBoundedParticleCreationStrategyTester.java
+++ simulations-java/simulations/states-of-matter/src/edu/colorado/phet/statesofmatter/model/particle/ZBoundedParticleCreationStrategyTester.java
@@ -4,23 +4,25 @@ import edu.colorado.phet.statesofmatter.StatesOfMatterConfig;
 import junit.framework.TestCase;
 
 import java.awt.geom.Rectangle2D;
-import java.util.ArrayList;
 
 public class ZBoundedParticleCreationStrategyTester extends TestCase {
     protected static final int NUM_PARTICLES_TO_TEST = 300;
+
     protected static final double PARTICLE_RADIUS = 0.1;
+    protected static final double PARTICLE_MASS = 1.0;
 
     protected volatile ParticleCreationStrategy strategy;
 
+
     public void setUp() {
-         this.strategy = new BoundedParticleCreationStrategy(StatesOfMatterConfig.CONTAINER_BOUNDS);
+         this.strategy = new BoundedParticleCreationStrategy(StatesOfMatterConfig.CONTAINER_BOUNDS, PARTICLE_RADIUS, PARTICLE_MASS);
     }
 
     public void testThatNoParticleCenterIsOutsideBounds() {
         Rectangle2D.Double bounds = StatesOfMatterConfig.CONTAINER_BOUNDS;
 
         for (int i = 0; i < NUM_PARTICLES_TO_TEST; i++) {
-            StatesOfMatterParticle particle = strategy.createNewParticle(new ArrayList(), PARTICLE_RADIUS, 1.0);
+            StatesOfMatterParticle particle = strategy.createNewParticle();
 
             assertTrue(bounds.contains(particle.getX(), particle.getY()));
         }
@@ -35,7 +37,7 @@ public class ZBoundedParticleCreationStrategyTester extends TestCase {
                                                            bounds.height - 2 * PARTICLE_RADIUS);
 
         for (int i = 0; i < NUM_PARTICLES_TO_TEST; i++) {
-            StatesOfMatterParticle particle = strategy.createNewParticle(new ArrayList(), PARTICLE_RADIUS, 1.0);
+            StatesOfMatterParticle particle = strategy.createNewParticle();
 
             assertTrue(narrow.contains(particle.getX(), particle.getY()));
         }
