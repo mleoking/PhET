8a9db48402f63bfdce9d84291f0ea7f48999ebf4
jdegoes <jdegoes@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-12-12 22:24:26 +0000

    states-of-matter: TDD'd packed hexagonal particle creation strategy, and integrated it into the simulation

--- trunk/simulations-java/simulations/states-of-matter/src/edu/colorado/phet/statesofmatter/StatesOfMatterConfig.java
+++ trunk/simulations-java/simulations/states-of-matter/src/edu/colorado/phet/statesofmatter/StatesOfMatterConfig.java
@@ -12,13 +12,14 @@ public class StatesOfMatterConfig extends PhetApplicationConfig {
     public static final PhetResources RESOURCES = PhetResources.forProject(PROJECT_NAME);
     public static final int WINDOW_WIDTH  = 800;
     public static final int WINDOW_HEIGHT = 600;
-    public static final int INITIAL_PARTICLE_COUNT = 100;
+    public static final int INITIAL_MAX_PARTICLE_COUNT = 600;
     public static final Rectangle2D.Double CONTAINER_BOUNDS = new Rectangle2D.Double(-3, -4, 6, 8);
-    public static final double INITIAL_TOTAL_ENERGY = 1000000;
+    public static final Rectangle2D.Double ICE_CUBE_BOUNDS = new Rectangle2D.Double(-1.5, 1, 3, 3);
+    public static final double INITIAL_TOTAL_ENERGY = 60000;
     public static final double PARTICLE_RADIUS = 0.1;
     public static final double PARTICLE_MASS = 1.0;
     public static final double PARTICLE_MAX_KE = 100000;
-    public static final double PARTICLE_CREATION_CUSHION = 0.05;
+    public static final double PARTICLE_CREATION_CUSHION = 0.005;
 
     public static final int COMPUTATIONS_PER_RENDER = 5;
 
@@ -27,6 +28,7 @@ public class StatesOfMatterConfig extends PhetApplicationConfig {
 
     public static final double EPSILON = 1.0;
     public static final double RMIN    = 2.0 * PARTICLE_RADIUS;
+
 
     public StatesOfMatterConfig(String[] commandLineArgs, FrameSetup frameSetup) {
         super(commandLineArgs, frameSetup, RESOURCES);
