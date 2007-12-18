e0bcccd94116ce0fed102169f7062215101fcdf3
jdegoes <jdegoes@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-12-18 20:39:12 +0000

    moving-man: refactored common logic in graph creation

--- trunk/simulations-java/simulations/states-of-matter/src/edu/colorado/phet/statesofmatter/model/container/AbstractParticleContainer.java
+++ trunk/simulations-java/simulations/states-of-matter/src/edu/colorado/phet/statesofmatter/model/container/AbstractParticleContainer.java
@@ -1,6 +1,17 @@
 package edu.colorado.phet.statesofmatter.model.container;
 
 public abstract class AbstractParticleContainer implements ParticleContainer {
+    private double rotation;
 
+    public final double getRotation() {
+        return rotation;
+    }
 
+    public final void setRotation(double rotation) {
+        this.rotation = rotation;
+
+        rotateImpl(rotation);
+    }
+
+    protected abstract void rotateImpl(double rotation);
 }
