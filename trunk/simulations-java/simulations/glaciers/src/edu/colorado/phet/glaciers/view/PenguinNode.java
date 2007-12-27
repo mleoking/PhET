cedc7481ee0bd0b8e5f4e6b40d0d8d1f415d75db
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-12-27 22:13:53 +0000

    12/27/07 - add penguin, constrain dragging (not connected to viewport) 12/27/07 - convert penguin back to PImage

--- trunk/simulations-java/simulations/glaciers/src/edu/colorado/phet/glaciers/view/PenguinNode.java
+++ trunk/simulations-java/simulations/glaciers/src/edu/colorado/phet/glaciers/view/PenguinNode.java
@@ -2,20 +2,17 @@
 
 package edu.colorado.phet.glaciers.view;
 
-import java.io.IOException;
-
-import edu.colorado.phet.common.piccolophet.PhetPCanvas;
 import edu.colorado.phet.common.piccolophet.event.BoundedDragHandler;
 import edu.colorado.phet.common.piccolophet.event.CursorHandler;
-import edu.colorado.phet.common.piccolophet.nodes.SVGNode;
-import edu.colorado.phet.glaciers.GlaciersResources;
+import edu.colorado.phet.glaciers.GlaciersImages;
 import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.nodes.PImage;
 
 
-public class PenguinNode extends SVGNode {
+public class PenguinNode extends PImage {
 
-    public PenguinNode( PhetPCanvas canvas, PNode dragBoundsNode ) throws IOException {
-        super( canvas, GlaciersResources.getResourceAsStream( "images/penguin.svg" ), 50, 50 );
+    public PenguinNode( PNode dragBoundsNode ) {
+        super( GlaciersImages.PENGUIN );
         BoundedDragHandler dragHandler = new BoundedDragHandler( this, dragBoundsNode );
         addInputEventListener( dragHandler );
         addInputEventListener( new CursorHandler() );
