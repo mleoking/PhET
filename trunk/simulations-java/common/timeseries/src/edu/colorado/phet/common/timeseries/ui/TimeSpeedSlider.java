0d2b6acaf36fb9585661591ebbab6dc7459eb539
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2007-12-30 18:31:10 +0000

    +Since vertical space is a precious commodity here, I put a "sim speed" label between slow and fast.

--- trunk/simulations-java/common/timeseries/src/edu/colorado/phet/common/timeseries/ui/TimeSpeedSlider.java
+++ trunk/simulations-java/common/timeseries/src/edu/colorado/phet/common/timeseries/ui/TimeSpeedSlider.java
@@ -1,36 +1,59 @@
 package edu.colorado.phet.common.timeseries.ui;
 
 import java.util.Hashtable;
+import java.awt.*;
 
 import javax.swing.*;
+import javax.swing.event.ChangeListener;
 
 import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
+import edu.colorado.phet.common.phetcommon.view.VerticalLayoutPanel;
+import edu.colorado.phet.common.phetcommon.view.util.PhetDefaultFont;
 import edu.colorado.phet.common.phetcommon.view.controls.valuecontrol.LinearValueControl;
 
 /**
  * Author: Sam Reid
  * Jun 1, 2007, 2:27:44 PM
  */
-public class TimeSpeedSlider extends LinearValueControl {
+public class TimeSpeedSlider extends VerticalLayoutPanel {
     private ConstantDtClock energySkateParkClock;
+    private LinearValueControl linearSlider;
 
     public TimeSpeedSlider( double min, double max, String textFieldPattern, final ConstantDtClock defaultClock ) {
-        super( min, max, "", textFieldPattern, "" );
+        linearSlider = new LinearValueControl( min, max, "", textFieldPattern, "" );
         this.energySkateParkClock = defaultClock;
-        setTextFieldVisible( false );
+        linearSlider.setTextFieldVisible( false );
         Hashtable table = new Hashtable();
         table.put( new Double( min ), new JLabel( TimeseriesResources.getString( "time.slow" ) ) );
         table.put( new Double( max ), new JLabel( TimeseriesResources.getString( "time.fast" ) ) );
-        setTickLabels( table );
+        final JLabel value = new JLabel( TimeseriesResources.getString( "sim.speed" ) );
+        value.setFont( new PhetDefaultFont( Font.ITALIC, PhetDefaultFont.getDefaultFontSize()) );
+        table.put( new Double( ( max + min ) / 2 ), value );
+        linearSlider.setTickLabels( table );
         defaultClock.addConstantDtClockListener( new ConstantDtClock.ConstantDtClockAdapter() {
             public void dtChanged( ConstantDtClock.ConstantDtClockEvent event ) {
                 update( defaultClock );
             }
         } );
         update( defaultClock );
+//        setBorder( BorderFactory.createTitledBorder( "sim speed" ) );
+//        add( new JLabel( "simulation speed" ) );
+        add( linearSlider );
+    }
+
+    public void addChangeListener( ChangeListener ch ) {
+        linearSlider.addChangeListener( ch );
     }
 
     private void update( ConstantDtClock defaultClock ) {
-        setValue( defaultClock.getTimingStrategy().getSimulationTimeChangeForPausedClock() );
+        linearSlider.setValue( defaultClock.getTimingStrategy().getSimulationTimeChangeForPausedClock() );
+    }
+
+    public double getValue() {
+        return linearSlider.getValue();
+    }
+
+    public void setValue( double dt ) {
+        linearSlider.setValue( dt );
     }
 }
