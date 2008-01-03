1c524a176d20eeddc846f4dd6f57764fe55fc082
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-01-03 20:28:56 +0000

    1/3/08 - quick pass at glacier and atmosphere models

---
+++ trunk/simulations-java/simulations/glaciers/src/edu/colorado/phet/glaciers/model/Atmosphere.java
@@ -0,0 +1,21 @@
+package edu.colorado.phet.glaciers.model;
+
+import java.awt.geom.Point2D;
+
+import edu.colorado.phet.common.phetcommon.model.ModelElement;
+
+
+public class Atmosphere implements ModelElement {
+
+    public Atmosphere() {
+        //XXX
+    }
+    
+    public double getTemperature( Point2D position ) {
+        return 0; //XXX
+    }
+
+    public void stepInTime( double dt ) {
+        // TODO Auto-generated method stub
+    }
+}
