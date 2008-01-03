75b5c53389f6d79121d7b9c731b461de92932396
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-01-03 00:12:44 +0000

    1/2/08 - constrain viewport dragging to be horizontal 1/2/08 - use penguin for viewport control, put dashed vertical lines L&R of penguin to denote viewport bounds 1/2/08 - resize world based on size of top canvas

---
+++ simulations-java/simulations/glaciers/src/edu/colorado/phet/glaciers/model/World.java
@@ -0,0 +1,68 @@
+/* Copyright 2008, University of Colorado */
+
+package edu.colorado.phet.glaciers.model;
+
+import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
+
+/**
+ * World describes general characteristics of the world.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
+public class World {
+    
+    private Rectangle2D _bounds;
+    private ArrayList _listeners;
+
+    public World() {
+        this( new Rectangle2D.Double() );
+    }
+    
+    public World( Rectangle2D bounds ) {
+        _bounds = new Rectangle2D.Double();
+        _bounds.setRect( bounds );
+        _listeners = new ArrayList();
+    }
+    
+    public void setBounds( Rectangle2D bounds ) {
+        if ( !bounds.equals( _bounds ) ) {
+            _bounds.setRect( bounds );
+            System.out.println( "World.setBounds bounds=" + bounds );//XXX
+            notifyBoundsChanged();
+        }
+    }
+    
+    public Rectangle2D getBounds() {
+        return new Rectangle2D.Double( _bounds.getX(), _bounds.getY(), _bounds.getWidth(), _bounds.getHeight() );
+    }
+    
+    public Rectangle2D getBoundsReference() {
+        return _bounds;
+    }
+    
+    public boolean contains( Rectangle2D r ) {
+        return _bounds.contains( r );
+    }
+    
+    public interface WorldListener {
+        public void boundsChanged();
+    }
+    
+    public void addListener( WorldListener listener ) {
+        _listeners.add( listener );
+    }
+
+    public void removeListener( WorldListener listener ) {
+        _listeners.remove( listener );
+    }
+    
+    private void notifyBoundsChanged() {
+        for ( int i = 0; i < _listeners.size(); i++ ) {
+            Object listener = _listeners.get( i );
+            if ( listener instanceof WorldListener ) {
+                ( (WorldListener) listener ).boundsChanged();
+            }
+        }
+    }
+}
