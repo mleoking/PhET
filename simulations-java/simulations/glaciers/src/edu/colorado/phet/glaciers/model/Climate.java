c69c6a9847a2cefd85a23f21841806ee948cdf7f
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-01-05 01:33:19 +0000

    1/4/07 - flesh out basic Climate model

---
+++ simulations-java/simulations/glaciers/src/edu/colorado/phet/glaciers/model/Climate.java
@@ -0,0 +1,106 @@
+/* Copyright 2008, University of Colorado */
+
+package edu.colorado.phet.glaciers.model;
+
+import java.util.ArrayList;
+
+import edu.colorado.phet.common.phetcommon.model.ModelElement;
+
+
+public class Climate implements ModelElement {
+    
+    private static final double DELTA_TEMPERATURE = -6.6E-3; // degrees C per meter
+    private static final double DELTA_PRECIPITATION = 2; // (meters per year) per meter
+    
+    private final double _referenceAltitude; // meters
+    private double _referenceTemperature; // degrees C
+    private double _referencePrecipitation; // meters per years
+    private ArrayList _listeners;
+    
+    public Climate( double referenceAltitude ) {
+        _referenceAltitude = referenceAltitude;
+        _referenceTemperature = 20;
+        _referencePrecipitation = 0;
+        _listeners = new ArrayList();
+    }
+    
+    public void cleanup() {}
+    
+    public double getReferenceAltitude() {
+        return _referenceAltitude;
+    }
+    
+    public void setReferenceTemperature( double referenceTemperature ) {
+        if ( referenceTemperature != _referenceTemperature ) {
+            _referenceTemperature = referenceTemperature;
+            notifyReferenceTemperatureChanged();
+        }
+    }
+    
+    public double getReferenceTemperature() {
+        return _referenceTemperature;
+    }
+    
+    public void setReferencePrecipitation( double referencePrecipitation ) {
+        if ( referencePrecipitation < 0 ) {
+            throw new IllegalArgumentException( "referencePrecipition must be > 0: " + referencePrecipitation );
+        }
+        if ( referencePrecipitation != _referencePrecipitation ) {
+            _referencePrecipitation = referencePrecipitation;
+            notifyReferencePrecipitationChanged();
+        }
+    }
+    
+    public double getReferencePrecipition() {
+        return _referencePrecipitation;
+    }
+    
+    public double getTemperature( double altitude ) {
+        return _referenceTemperature + ( ( altitude - _referenceAltitude ) * DELTA_TEMPERATURE );
+    }
+    
+    public double getPrecipitation( double altitude ) {
+        return _referencePrecipitation + ( ( altitude - _referenceAltitude ) * DELTA_PRECIPITATION );
+    }
+
+    public void stepInTime( double dt ) {
+        // do nothing
+    }
+    
+    public interface ClimateListener {
+        public void referenceTemperatureChanged();
+        public void referencePrecipitationChanged();
+    }
+    
+    public static class ClimateAdapter implements ClimateListener {
+        public void referenceTemperatureChanged() {};
+        public void referencePrecipitationChanged() {};
+    }
+    
+    public void addListener( ClimateListener listener ) {
+        _listeners.add( listener );
+    }
+    
+    public void removeListener( ClimateListener listener ) {
+        _listeners.remove( listener );
+    }
+    
+    private void notifyReferenceTemperatureChanged() {
+        for ( int i = 0; i < _listeners.size(); i++ ) {
+            Object listener = _listeners.get( i );
+            if ( listener instanceof ClimateListener ) {
+                ( (ClimateListener) listener ).referenceTemperatureChanged();
+            }
+        }
+    }
+    
+    private void notifyReferencePrecipitationChanged() {
+        for ( int i = 0; i < _listeners.size(); i++ ) {
+            Object listener = _listeners.get( i );
+            if ( listener instanceof ClimateListener ) {
+                ( (ClimateListener) listener ).referencePrecipitationChanged();
+            }
+        }
+    }
+}
+
---
+++ simulations-java/simulations/glaciers/src/edu/colorado/phet/glaciers/model/ModelViewTransform.java
@@ -0,0 +1,141 @@
+/* Copyright 2008, University of Colorado */
+
+package edu.colorado.phet.glaciers.model;
+
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Point2D;
+
+/**
+ * ModelViewTransform provides the transforms between model and view coordinate systems.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
+public class ModelViewTransform {
+
+    //----------------------------------------------------------------------------
+    // Instance data
+    //----------------------------------------------------------------------------
+    
+    private AffineTransform _modelToViewTransform;
+    private AffineTransform _viewToModelTransform;
+    
+    private Point2D _pModelDistance, _pViewDistance; // reusable points for transforming distances
+    
+    //----------------------------------------------------------------------------
+    // Constructors
+    //----------------------------------------------------------------------------
+    
+    /**
+     * Constructor.
+     * 
+     * @param xScale x scale for mapping from model to view
+     * @param yScale y scale for mapping from model to view
+     */
+    public ModelViewTransform( double xScale, double yScale ) {
+        this( xScale, yScale, 0, 0 );
+    }
+    
+    /**
+     * Constructor.
+     * 
+     * @param xScale x scale for mapping from model to view
+     * @param yScale y scale for mapping from model to view
+     * @param xOffset x offset for mapping from model to view
+     * @param yOffset y offset for mapping from model to view
+     */
+    public ModelViewTransform( double xScale, double yScale, double xOffset, double yOffset ) {
+        
+        _modelToViewTransform = new AffineTransform();
+        _modelToViewTransform.scale( xScale, yScale );
+        _modelToViewTransform.translate( xOffset, yOffset );
+        
+        _viewToModelTransform = new AffineTransform();
+        _viewToModelTransform.translate( -xOffset, -yOffset );
+        _viewToModelTransform.scale( 1d/xScale, 1d/yScale );
+        
+        _pModelDistance = new Point2D.Double();
+        _pViewDistance = new Point2D.Double();
+    }
+    
+    //----------------------------------------------------------------------------
+    // Model-to-view transforms
+    //----------------------------------------------------------------------------
+    
+    /**
+     * Maps a point from model to view coordinates.
+     * 
+     * @param pModel point in model coordinates
+     * @return point in view coordinates
+     */
+    public Point2D modelToView( Point2D pModel ) {
+        return modelToView( pModel, null );
+    }
+    
+    public Point2D modelToView( double x, double y ) {
+        return modelToView( new Point2D.Double( x, y ) );
+    }
+    
+    /**
+     * Maps a point from model to view coordinates.
+     * 
+     * @param pModel point in model coordinates
+     * @param pView point in view coordinates, possibly null
+     * @return point in view coordinates
+     */
+    public Point2D modelToView( Point2D pModel, Point2D pView ) {
+        return _modelToViewTransform.transform( pModel, pView );
+    }
+    
+    /**
+     * Maps a distance from model to view coordinates.
+     * 
+     * @param distance distance in model coordinates
+     * @return distance in view coordinates
+     */
+    public double modelToView( double distance ) {
+        _pModelDistance.setLocation( distance, 0 );
+        modelToView( _pModelDistance, _pViewDistance );
+        return _pViewDistance.getX();
+    }
+    
+    //----------------------------------------------------------------------------
+    // View-to-model transforms
+    //----------------------------------------------------------------------------
+    
+    /**
+     * Maps a point from view to model coordinates.
+     * 
+     * @param pView point in view coordinates
+     * @return point in model coordinates
+     */
+    public Point2D viewToModel( Point2D pView ) {
+        return viewToModel( pView, null );
+    }
+    
+    public Point2D viewToModel( double x, double y ) {
+        return viewToModel( new Point2D.Double( x, y ) );
+    }
+    
+    /**
+     * Maps a point from view to model coordinates.
+     * 
+     * @param pView point in view coordinates
+     * @param pModel point in model coordinates, possibly null
+     * @return point in model coordinates
+     */
+    public Point2D viewToModel( Point2D pView, Point2D pModel ) {
+        return _viewToModelTransform.transform( pView, pModel );
+    }
+    
+    /**
+     * Maps a distance from view to model coordinates.
+     * 
+     * @param distance distance in view coordinates
+     * @return distance in model coordinates
+     */
+    public double viewToModel( double distance ) {
+        _pViewDistance.setLocation( distance, 0 );
+        viewToModel( _pViewDistance, _pModelDistance );
+        return _pModelDistance.getX();
+    }
+}
