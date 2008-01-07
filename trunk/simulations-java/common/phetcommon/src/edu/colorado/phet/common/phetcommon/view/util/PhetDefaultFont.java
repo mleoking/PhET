a24b65768bdef8a6e1aec1f7688210f08a000e18
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-01-07 21:26:35 +0000

    PhetDefaultFont overhaul: - delete LUCIDA_SANS constant - delete constructor that takes a font name; use Font if you need to do this - make public methods private that deal with locale, preferred fonts, and default font creation - add getDefaultStyle - clean up creation of default font

--- trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/view/util/PhetDefaultFont.java
+++ trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/view/util/PhetDefaultFont.java
@@ -1,4 +1,5 @@
-/* Copyright 2007, University of Colorado */
+/* Copyright 2007-2008, University of Colorado */
+
 package edu.colorado.phet.common.phetcommon.view.util;
 
 import edu.colorado.phet.common.phetcommon.resources.PhetResources;
@@ -9,73 +10,49 @@ import java.util.Arrays;
 import javax.swing.*;
 
 /**
- * Simplified interface for instantiating the default font used in PhET simulations.
+ * PhetDefaultFont provides an interface for instantiating the default font used in PhET simulations.
  */
 public class PhetDefaultFont extends Font {
-    private static final Font REFERENCE_FONT = getReferenceFont();
-    private static final String FONT_NAME = REFERENCE_FONT.getFontName();
-    public static final String LUCIDA_SANS = "Lucida Sans";
-
-    public PhetDefaultFont() {
-        this( getReferenceFont().getSize() );
-    }
-
-    private static Font getReferenceFont() {
-        Font referenceFont = new JLabel().getFont();
-
-        if ( isJapaneseLocale() ) {
-            referenceFont = getPreferredFont( new String[]{"MS Mincho", "MS Gothic", "Osaka"} );
-        }
-        else if ( isLocale( "ar" ) ) {
-            referenceFont = getPreferredFont(new String[]{"Lucida Sans Regular"} );
-        }
-
-        return referenceFont;
-    }
-
-    private static String getPreferredFontName( String fontName ) {
-        String[] names = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
-
-        if (isJapaneseLocale()||isLocale( "ar" )) {
-             return REFERENCE_FONT.getFontName();
-        }
-        else if ( !Arrays.asList( names ).contains( fontName ) ) {
-            return REFERENCE_FONT.getFontName();
-        }
-
-        return fontName;
-    }
-
-    public PhetDefaultFont( String fontName ) {
-        this( fontName, REFERENCE_FONT.getStyle(), REFERENCE_FONT.getSize() );
-    }
-
-    public PhetDefaultFont( String fontName, int style, float size ) {
-        super( getPreferredFontName( fontName ), style, (int)size );
-    }
+    
+    // preferred physical font names for various ISO language codes
+    //TODO: read these from a properties file
+    private static final String[] JA_PREFERRED_FONTS = { "MS Mincho", "MS Gothic", "Osaka" }; // Japanese
+    private static final String[] AR_PREFERRED_FONTS = { "Lucida Sans Regular" }; // Arabic
+    
+    // we'll use this font if we have no font preference, or if no preferred font is installed
+    private static final Font FALLBACK_FONT = new JLabel().getFont();
+    
+    // the font used to create instances of PhetDefaultFont
+    private static final Font DEFAULT_FONT = createDefaultFont();
 
     /**
-     * Constructs a PhetDefaultFont with a specified point size.
+     * Constructs a PhetDefaultFont with a default style and point size.
+     */
+    public PhetDefaultFont() {
+        this( getDefaultFontSize() );
+    }
+    
+    /**
+     * Constructs a PhetDefaultFont with a default style and specified point size.
      *
      * @param size the size of the font.
      */
     public PhetDefaultFont( int size ) {
-        this( Font.PLAIN, size );
+        this( getDefaultFontStyle(), size );
     }
 
     /**
-     * This constructor is more similar to the Font constructor,
-     * but supplies the font name.
+     * Constructs a PhetDefaultFont with a specified style and point size.
      *
      * @param style
      * @param size
      */
     public PhetDefaultFont( int style, int size ) {
-        super( FONT_NAME, style, size );
+        super( getDefaultFontName(), style, size );
     }
 
     /**
-     * Constructs a PhetDefaultFont with the specified font size, and whether it is bold.
+     * Constructs a PhetDefaultFont with a specified font size, and whether it is bold.
      *
      * @param size the font size
      * @param bold whether it is bold.
@@ -85,7 +62,7 @@ public class PhetDefaultFont extends Font {
     }
 
     /**
-     * Constructs a Lucida Sans font with the specified size, and whether it is bold and/or italicized.
+     * Constructs a PhetDefaultFont font with a specified size, and whether it is bold and/or italicized.
      *
      * @param size    the font size.
      * @param bold    whether it is bold
@@ -96,12 +73,12 @@ public class PhetDefaultFont extends Font {
     }
 
     /**
-     * Returns the font family name for the default font.
+     * Returns the font face name for the default font.
      *
-     * @return the font family name for the default font
+     * @return the font face name for the default font
      */
     public static String getDefaultFontName() {
-        return FONT_NAME;
+        return DEFAULT_FONT.getFontName();
     }
 
     /**
@@ -110,34 +87,65 @@ public class PhetDefaultFont extends Font {
      * @return the font size for the default font
      */
     public static int getDefaultFontSize() {
-        return getReferenceFont().getSize();
+        return DEFAULT_FONT.getSize();
+    }
+    
+    /**
+     * Returns the font style for the default font.
+     *
+     * @return the font style for the default font
+     */
+    public static int getDefaultFontStyle() {
+        return DEFAULT_FONT.getStyle();
     }
 
-    public static Font getPreferredFont( String[] preferredJAFonts ) {
-        float defaultSize = new JLabel().getFont().getSize();
+    /*
+     * Creates the font that will be used to create all instances of PhetDefaultFont.
+     */
+    private static Font createDefaultFont() {
 
-        for ( int i = 0; i < preferredJAFonts.length; i++ ) {
-            String preferredJAFont = preferredJAFonts[i];
+        Font defaultFont = FALLBACK_FONT;
+
+        //TODO: read the list of preferred fonts for a local from a properties file
+        if ( isLocale( "ja" ) ) {
+            defaultFont = getPreferredFont( JA_PREFERRED_FONTS, FALLBACK_FONT );
+        }
+        else if ( isLocale( "ar" ) ) {
+            defaultFont = getPreferredFont( AR_PREFERRED_FONTS, FALLBACK_FONT );
+        }
+
+        System.out.println( "PhetDefaultFont.createDefaultFont defaultFont=" + defaultFont.toString() );
+        return defaultFont;
+    }
+    
+    /*
+     * Looks through a list of preferred physical font names, ordered by decreasing preference.
+     * The first match with a font installed on the system is returned.
+     * If there is no match, then the defaultFont is returned.
+     */
+    private static Font getPreferredFont( String[] preferredFontNames, Font defaultFont ) {
+        
+        Font preferredFont = defaultFont;
+
+        for ( int i = 0; i < preferredFontNames.length; i++ ) {
+            String preferredFontName = preferredFontNames[i];
             ArrayList fonts = new ArrayList( Arrays.asList( GraphicsEnvironment.getLocalGraphicsEnvironment().getAllFonts() ) );
-//            System.out.println( "fonts = " + fonts );
+            System.out.println( "PhetDefaultFonts.getPreferredFont fonts=" + fonts );
             for ( int k = 0; k < fonts.size(); k++ ) {
                 Font o = (Font) fonts.get( k );
-                if ( o.getName().equals( preferredJAFont )
-//                     ||o.getFamily(  ).equals( preferredJAFont )
-                        ) {
-                    System.out.println( "Chose Font: " + o );
-                    return o.deriveFont(defaultSize);
+                if ( o.getName().equals( preferredFontName ) ) {
+                    preferredFont = o.deriveFont( preferredFont.getSize() );
                 }
             }
         }
-        return new JLabel().getFont();
+
+        return preferredFont;
     }
 
-    public static boolean isJapaneseLocale() {
-        return isLocale( "ja" );
-    }
-
-    public static boolean isLocale(String locale){
-        return PhetResources.readLocale().getLanguage().equalsIgnoreCase( locale );
+    /*
+     * Determines if the locale matches a specified language code.
+     */
+    private static boolean isLocale( String language ) {
+        return PhetResources.readLocale().getLanguage().equalsIgnoreCase( language );
     }
 }
