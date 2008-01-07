988f89c491804f4651e907109cf866e6ea8e4be9
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-01-07 19:03:14 +0000

    ohm-1d: delete uses of PhetDefaultFont.LUCIDA_SANS, test

--- simulations-java/simulations/ohm-1d/src/edu/colorado/phet/ohm1d/Ohm1DApplication.java
+++ simulations-java/simulations/ohm-1d/src/edu/colorado/phet/ohm1d/Ohm1DApplication.java
@@ -564,7 +564,7 @@ public class Ohm1DApplication extends JApplet {
 //        URL fontLocation = loader.getResource( "fonts/SYLFAEN.TTF" );
         //URL fontLocation=loader.getResource("fonts/PAPYRUS.TTF");
 
-        Font sylfFont = new Font( PhetDefaultFont.LUCIDA_SANS, Font.PLAIN, 18 );
+        Font sylfFont = new PhetDefaultFont( Font.PLAIN, 18 );
         sylfFont = font.deriveFont( Font.PLAIN, 44.2f );
 
         //System.err.println("Battx="+battX+", batty="+battY);
