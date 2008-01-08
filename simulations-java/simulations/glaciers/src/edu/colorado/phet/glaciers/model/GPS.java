0380fa65e88a74c468536784c152693e9170cc41
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-01-08 00:34:06 +0000

    Add GPS model element, for taking a reading on where we are in model coordinates. This will be used primarily as a development/debugging tool.

---
+++ simulations-java/simulations/glaciers/src/edu/colorado/phet/glaciers/model/GPS.java
@@ -0,0 +1,11 @@
+package edu.colorado.phet.glaciers.model;
+
+import java.awt.geom.Point2D;
+
+
+public class GPS extends Movable {
+
+    public GPS( Point2D position ) {
+        super( position );
+    }
+}
