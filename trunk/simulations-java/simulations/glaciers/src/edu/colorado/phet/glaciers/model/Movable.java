a378c3130500b4df3e910519ea982acd8b07b57c
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-01-08 22:17:57 +0000

    improve performance of setPosition(double,double)

--- trunk/simulations-java/simulations/glaciers/src/edu/colorado/phet/glaciers/model/Movable.java
+++ trunk/simulations-java/simulations/glaciers/src/edu/colorado/phet/glaciers/model/Movable.java
@@ -36,7 +36,7 @@ public abstract class Movable {
     }
     
     public void setPosition( double x, double y ) {
-        if ( x != getX() || y != getY() ) {
+        if ( x != _position.getX() || y != _position.getY() ) {
             _position.setLocation( x, y );
             notifyPositionChanged();
         }
