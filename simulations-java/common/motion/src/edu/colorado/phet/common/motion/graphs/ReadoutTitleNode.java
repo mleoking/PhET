03520d245cff89e6be883f6449d885628c3b3654
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-01-09 20:37:25 +0000

    fix text offset

--- simulations-java/common/motion/src/edu/colorado/phet/common/motion/graphs/ReadoutTitleNode.java
+++ simulations-java/common/motion/src/edu/colorado/phet/common/motion/graphs/ReadoutTitleNode.java
@@ -129,11 +129,9 @@ public class ReadoutTitleNode extends PNode {
             titleNode.setHtml( series.getAbbr() + "= " );
         }
 
-//        ((PNode)valueNode).setOffset( titleNode.getFullBounds().getWidth() + 3, 3 );
-//        ((PNode)valueNode).setOffset( titleNode.getFullBounds().getWidth() + 3, ((PNode)valueNode).getHeight()*2 );
-//        System.out.println( "((PNode)valueNode).getHeight()*2  = " + ( (PNode) valueNode ).getFullBounds().getHeight() );
-//        ((PNode)valueNode).setOffset( titleNode.getFullBounds().getWidth() + 3, 100);
-        ((PNode)valueNode).setOffset( titleNode.getFullBounds().getWidth() + 3, ( (PNode) valueNode ).getFullBounds().getHeight()*1.5);
+        ((PNode)valueNode).setOffset( titleNode.getFullBounds().getWidth() + 3, 3 );
+        
+//        ((PNode)valueNode).setOffset( titleNode.getFullBounds().getWidth() + 3, ( (PNode) valueNode ).getFullBounds().getHeight()*1.5);
         updateText();
     }
 
