ca956051928668033e70aabbc267b7ebb4c03e9c
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-01-09 16:27:08 +0000

    1/9/08 - implement Valley elevation and width functions

--- simulations-java/simulations/glaciers/src/edu/colorado/phet/glaciers/model/Valley.java
+++ simulations-java/simulations/glaciers/src/edu/colorado/phet/glaciers/model/Valley.java
@@ -1,12 +1,52 @@
+/* Copyright 2008, University of Colorado */
+
 package edu.colorado.phet.glaciers.model;
 
-
+/**
+ * Valley is the model of the valley where the glacier forms.
+ * For the purposes of this simulation, the valley is a static entity that does not change over time.
+ * <p>
+ * The coordinate system is:
+ * <ul>
+ * <li>x : position down the valley floor (meters)
+ * <li>y : width across the valley floor (meters)
+ * <li>z: elevation above sea level (meters)
+ * </ul>
+ * <p>
+ * "Down the valley" indicates increasing x and decreasing z.
+ * "Up the valley" indicates decreasing x and increasing z.
+ * <p>
+ * x=0 is at the point of highest elevation (maximum z), and
+ * is the point where the glacier begins to form.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
 public class Valley {
 
     public Valley() {
     }
     
-    public double getAltitude( double x ) {
-        return 0;//XXX
+    /**
+     * Gets the elevation at a position along the valley floor.
+     * Slope starts off steep, then levels out as we go down the valley.
+     * 
+     * @param x position (meters)
+     * @return elevation (meters)
+     */
+    public double getElevation( double x ) {
+        return 4e3 - ( x / 30. ) + Math.exp( -( x - 5e3 ) / 1e3 );
+    }
+    
+    /**
+     * Gets the width at a position along the valley floor.
+     * Width is largest at the highest points of the valley, 
+     * then tapers down to constant further down the valley.
+     * 
+     * @param x position (meters)
+     * @return width (meters)
+     */
+    public double getWidth( double x ) {
+        final double term = ( x - 5e3 ) / 2e3;
+        return 1e3 + ( 5e3 * Math.exp( -( term * term ) ) );
     }
 }
