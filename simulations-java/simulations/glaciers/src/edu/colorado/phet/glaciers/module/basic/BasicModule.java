560ae0cae25bfca067e2e6db581071e93699c55a
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-01-14 02:43:28 +0000

    1/13/08 - fix penguin scaling and layout to handle MVT y translation 1/13/08 - refactor PlayArea.updateLayout, break into smaller methods 1/13/08 - remove MVT distance transforms, they don't work unless xScale=yScale

--- simulations-java/simulations/glaciers/src/edu/colorado/phet/glaciers/module/basic/BasicModule.java
+++ simulations-java/simulations/glaciers/src/edu/colorado/phet/glaciers/module/basic/BasicModule.java
@@ -30,8 +30,8 @@ public class BasicModule extends PiccoloModule {
     // Class data
     //----------------------------------------------------------------------------
     
-    private static final double VIEW_X_SCALE = 1.0;
-    private static final double VIEW_Y_SCALE = 1.0;
+    private static final double VIEW_X_SCALE = 0.5;
+    private static final double VIEW_Y_SCALE = 0.1;
     private static final Point2D VIEW_ORIGIN = new Point2D.Double( 500, 100 );
     
     //----------------------------------------------------------------------------
