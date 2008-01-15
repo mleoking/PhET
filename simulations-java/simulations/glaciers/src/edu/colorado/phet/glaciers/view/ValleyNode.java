71b8acd357192bbe54ea5eee44a3cd407bfe42e1
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-01-15 05:48:37 +0000

    1/14/08 - place ValleyNode at x=0, y=Valley.getElevation(0)

--- simulations-java/simulations/glaciers/src/edu/colorado/phet/glaciers/view/ValleyNode.java
+++ simulations-java/simulations/glaciers/src/edu/colorado/phet/glaciers/view/ValleyNode.java
@@ -2,6 +2,8 @@
 
 package edu.colorado.phet.glaciers.view;
 
+import java.awt.geom.Point2D;
+
 import edu.colorado.phet.glaciers.GlaciersImages;
 import edu.colorado.phet.glaciers.model.Valley;
 import edu.umd.cs.piccolo.nodes.PImage;
@@ -22,6 +24,13 @@ public class ValleyNode extends PComposite {
         PImage imageNode = new PImage( GlaciersImages.VALLEY );
         addChild( imageNode );
         setPickable( false );
+        
+        //XXX position this node's upper-left corner at the highest point in the valley, at x=0
+        double x = 0;
+        double y = _valley.getElevation( x );
+        Point2D highPoint =  new Point2D.Double( x, y );
+        Point2D pView = _mvt.modelToView( highPoint );
+        setOffset( pView );
     }
     
     public void cleanup() {}
