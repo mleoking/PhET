e38e71f23f1066d9dc18e27157b8b85ee3d96ebd
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-01-24 02:49:20 +0000

    1/23/08 - stub out most of "Advanced" module, it will come later after some redesign 1/23/08 - delete AdvanceClimateControlPanel, this needs to be redesigned

---
+++ simulations-java/simulations/glaciers/src/edu/colorado/phet/glaciers/module/advanced/AdvancedController.java
@@ -0,0 +1,18 @@
+/* Copyright 2008, University of Colorado */
+
+package edu.colorado.phet.glaciers.module.advanced;
+
+
+public class AdvancedController {
+
+    private AdvancedModel _model;
+    private AdvancedControlPanel _controlPanel;
+    
+    public AdvancedController( AdvancedModel model, AdvancedControlPanel controlPanel ) {
+        
+        _model = model;
+        _controlPanel = controlPanel;
+        
+        //XXX fill this in, it will be some variation of BasicController
+    }
+}
