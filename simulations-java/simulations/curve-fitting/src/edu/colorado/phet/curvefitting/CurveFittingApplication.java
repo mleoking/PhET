c671b3cc2f27d661e77139973ecffc1a5ffefbb9
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-02-04 18:29:51 +0000

    added curve-fitting

---
+++ simulations-java/simulations/curve-fitting/src/edu/colorado/phet/curvefitting/CurveFittingApplication.java
@@ -0,0 +1,17 @@
+package edu.colorado.phet.curvefitting;
+
+import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
+import edu.colorado.phet.common.phetcommon.resources.PhetResources;
+import edu.colorado.phet.common.phetcommon.view.util.FrameSetup;
+import edu.colorado.phet.common.piccolophet.PhetApplication;
+
+public class CurveFittingApplication extends PhetApplication {
+    public CurveFittingApplication( PhetApplicationConfig config ) {
+        super( config );
+        addModule( new CurveFittingModule() );
+    }
+
+    public static void main( String[] args ) {
+        new CurveFittingApplication( new PhetApplicationConfig( args, new FrameSetup.CenteredWithInsets( 100,100), PhetResources.forProject( "curve-fitting" ), "curve-fitting") ).startApplication();
+    }
+}
