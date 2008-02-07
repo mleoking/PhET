760df4f9b6489e14abe4a0be9e1894aa97b39d45
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-02-07 05:31:07 +0000

    disable drag handler

--- simulations-java/simulations/waveinterference/src/edu/colorado/phet/waveinterference/view/CrossSectionGraphic.java
+++ simulations-java/simulations/waveinterference/src/edu/colorado/phet/waveinterference/view/CrossSectionGraphic.java
@@ -66,7 +66,7 @@ public class CrossSectionGraphic extends PhetPNode {
                 notifyListeners();
             }
         };
-        addInputEventListener( dragHandler );
+//        addInputEventListener( dragHandler );
         addInputEventListener( new CursorHandler() );
     }
 
