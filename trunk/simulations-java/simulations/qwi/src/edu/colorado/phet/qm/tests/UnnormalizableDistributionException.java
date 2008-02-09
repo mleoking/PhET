18189e99cfe54df92c4aef3d064056e2df864a41
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-02-09 20:19:58 +0000

    rename package qwi to quantumwaveinterference

--- trunk/simulations-java/simulations/qwi/src/edu/colorado/phet/qm/tests/UnnormalizableDistributionException.java
+++
@@ -1,7 +0,0 @@
-package edu.colorado.phet.qm.tests;
-
-public class UnnormalizableDistributionException extends Exception {
-    public UnnormalizableDistributionException( String s ) {
-        super( s );
-    }
-}
---
+++ trunk/simulations-java/simulations/qwi/src/edu/colorado/phet/quantumwaveinterference/persistence/TestQWIPersistence.java
@@ -0,0 +1,21 @@
+package edu.colorado.phet.quantumwaveinterference.persistence;
+
+import edu.colorado.phet.common.phetcommon.util.persistence.PersistenceUtil;
+import edu.colorado.phet.quantumwaveinterference.QWIApplication;
+import edu.colorado.phet.quantumwaveinterference.davissongermer.QWIStrings;
+import junit.framework.TestCase;
+
+/**
+ * Author: Sam Reid
+ * Jul 5, 2007, 11:53:13 PM
+ */
+public class TestQWIPersistence extends TestCase {
+    public void testQWIPersistenceEmptyState() throws PersistenceUtil.CopyFailedException {
+        QWIStrings.init( new String[0] );
+        QWIApplication qwiApplication = new QWIApplication( new String[0] );
+        QWIState state = new QWIState( qwiApplication.getIntensityModule() );
+        QWIState copy = (QWIState)PersistenceUtil.copy( state );
+        System.out.println( "copy = " + copy );
+        assertEquals( "qwi state copy should be the same", copy, state );
+    }
+}
