b07f1c175a63986212bd32e6dd508d61321d825c
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-02-15 16:24:29 +0000

    added conversion from excel csv format in batch

---
+++ team/reids/admin/src/edu/colorado/phet/reids/admin/util/ConvertBatch.java
@@ -0,0 +1,31 @@
+package edu.colorado.phet.reids.admin.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.ParseException;
+
+import edu.colorado.phet.build.FileUtils;
+import edu.colorado.phet.reids.admin.TimesheetData;
+
+/**
+ * Created by: Sam
+ * Feb 15, 2008 at 9:18:44 AM
+ */
+public class ConvertBatch {
+    public static void main( String[] args ) throws IOException, ParseException {
+        String[] files = new String[]{
+                "C:\\Users\\Sam\\Desktop\\csvimport\\timecard-1-1-2008.csv",
+                "C:\\Users\\Sam\\Desktop\\csvimport\\timecard-1-7-2008.csv",
+                "C:\\Users\\Sam\\Desktop\\csvimport\\timecard-1-14-2008.csv",
+                "C:\\Users\\Sam\\Desktop\\csvimport\\timecard-1-21-2008.csv",
+                "C:\\Users\\Sam\\Desktop\\csvimport\\timecard-1-28-2008.csv",
+                "C:\\Users\\Sam\\Desktop\\csvimport\\timecard-2-4-2008.csv"
+        };
+        TimesheetData timesheetData = new TimesheetData();
+        for ( int i = 0; i < files.length; i++ ) {
+            TimesheetData d = ConvertExcelCSV.load( new File( files[i] ) );
+            timesheetData.addAll( d );
+        }
+        FileUtils.writeString( new File( "C:\\Users\\Sam\\Desktop\\batch.csv" ), timesheetData.toCSV() );
+    }
+}
