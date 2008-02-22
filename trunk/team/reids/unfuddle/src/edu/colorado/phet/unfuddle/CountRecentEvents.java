97a4494cb6a593c81d34d51bb87c1032dff744e8
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-02-22 14:36:41 +0000

    added debug utility for unfuddle limit paramter

---
+++ trunk/team/reids/unfuddle/src/edu/colorado/phet/unfuddle/CountRecentEvents.java
@@ -0,0 +1,23 @@
+package edu.colorado.phet.unfuddle;
+
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
+
+/**
+ * Created by: Sam
+ * Feb 22, 2008 at 7:25:45 AM
+ */
+public class CountRecentEvents {
+    public static void main( String[] args ) throws IOException, SAXException, ParserConfigurationException {
+        UnfuddleCurl unfuddleCurl = new UnfuddleCurl( args[0], args[1], UnfuddleCurl.PHET_PROJECT_ID );
+        System.out.println( "limit\tevents" );
+        for ( int limit = 1; limit < 50; limit++ ) {
+            XMLObject events = new XMLObject( unfuddleCurl.readString( "activity.xml?limit=" + limit ) );
+            int e = events.getNodeCount( "event" );
+            System.out.println( limit + "\t" + e );
+        }
+    }
+}
