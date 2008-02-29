65b72131f8c4ec21fd1cd6a1e880b29239327106
flydanofly <flydanofly@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-02-29 17:01:09 +0000

    website: updating clearing out the sim cache

--- trunk/website/admin/test.php
+++ trunk/website/admin/test.php
@@ -1,17 +1,10 @@
 <?php
-	include_once("../admin/global.php");
 
-	include_once(SITE_ROOT."admin/db.inc");
-	include_once(SITE_ROOT."admin/sim-utils.php");
-	include_once(SITE_ROOT."admin/web-utils.php");
-	include_once(SITE_ROOT."admin/db-utils.php");
-	include_once(SITE_ROOT."admin/site-utils.php");
-	include_once(SITE_ROOT."admin/cache-utils.php");
-	
-	foreach (glob('../*/cached-*') as $dir) {
-		print "Deleting $dir<br/>";
-		
-		exec("rm -rf $dir");
-		flush();
-	}
+    foreach (glob('../*/cached-*') as $dir) {
+        print "Deleting $dir<br/>";
+
+        exec("rm -rf $dir");
+        flush();
+    }
+
 ?>
