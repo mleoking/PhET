6ab920546c6b590009cbb8fdfedb9da382dd4515
flydanofly <flydanofly@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-03-05 20:47:55 +0000

    website: Improved ability to clear the various caches and added the ability to the admin control panel.  Not pretty, would be nice to have slicker interface for use in admin panel.
    resolves: unfuddle ticket #190

---
+++ trunk/website/admin/cache-clear-utils.php
@@ -0,0 +1,61 @@
+<?php
+
+    /**
+     * Clear the cache, optionally printing information is it goes.
+     *
+     * @param string $base_directory name of the base directory to clear
+     * @param bool $verbose true if result should be printed
+     * @param string $print_header (optional) human readable header, such as "Simulations"
+     */
+    function cache_clear($base_directory, $verbose = true, $print_header = "") {
+        // Print the header, if necessary
+        if ($verbose) {
+            print "<p>\n";
+
+            if ($header != "") {
+                print "  <strong>$print_header</strong><br />\n";
+            }
+
+            print "  Clearing caches in directory: $base_directory<br />\n";
+        }
+
+        // Get the cache directories to clear
+        $clear_dirs = glob($base_directory.'/cached-*');
+
+        // Nothing to do?
+        if (count($clear_dirs) == 0) {
+            if ($verbose) {
+                print "  No directories found\n";
+                print "</p>\n";
+            }
+
+            return;
+        }
+
+        if ($verbose) {
+            // Prepare to print the list of directories affected
+            print "  <ul>\n";
+        }
+
+        foreach ($clear_dirs as $dir) {
+            if ($verbose) {
+                // Print the command used
+                print "    <li>rm -rf $dir</li>\n";
+            }
+
+            // Do the deed
+            exec("rm -rf $dir");
+        }
+
+        if ($verbose) {
+            // print wrap-up info
+            print "  </ul>\n";
+            print "  Finished clearing caches in directory: $base_directory\n";
+            print "</p>\n";
+
+            // Flush the output buffer
+            flush();
+        }
+    }
+
+?>
