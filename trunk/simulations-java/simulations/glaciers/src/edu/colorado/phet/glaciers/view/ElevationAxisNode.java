cac004c14df7a38d10c91e41cd5dd89673233562
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-03-06 04:04:32 +0000

    3/5/08 - add elevation axes on left and right edges of zoomed viewport

--- trunk/simulations-java/simulations/glaciers/src/edu/colorado/phet/glaciers/view/ElevationAxisNode.java
+++ trunk/simulations-java/simulations/glaciers/src/edu/colorado/phet/glaciers/view/ElevationAxisNode.java
@@ -27,11 +27,11 @@ public class ElevationAxisNode extends PComposite {
     private static final Color TICK_LABEL_COLOR = TICK_COLOR;
     private static final Font TICK_LABEL_FONT = new PhetDefaultFont( 12 );
     private static final DecimalFormat TICK_LABEL_FORMAT = new DecimalFormat( "0" );
-    private static final boolean TICK_ON_LEFT = true; // true=label to left of tick, false=label to right of tick
     
     private final PComposite _parentNode;
     private final ModelViewTransform _mvt;
     private final double _minElevation, _maxElevation, _majorTickSpacing;
+    private final boolean _tickLabelOnLeft;
     
     /**
      * Constructor.
@@ -41,8 +41,9 @@ public class ElevationAxisNode extends PComposite {
      * @param minElevation meters
      * @param maxElevation meters
      * @param majorTickSpacing meters
+     * @param tickLabelOnLeft  true for label to left of tick, false for label to right of tick
      */
-    public ElevationAxisNode( ModelViewTransform mvt, DoubleRange elevationRange, double majorTickSpacing ) {
+    public ElevationAxisNode( ModelViewTransform mvt, DoubleRange elevationRange, double majorTickSpacing, boolean tickLabelOnLeft ) {
         super();
         setPickable( false );
         setChildrenPickable( false );
@@ -51,13 +52,18 @@ public class ElevationAxisNode extends PComposite {
         _minElevation = elevationRange.getMin();
         _maxElevation = elevationRange.getMax();
         _majorTickSpacing = majorTickSpacing;
+        _tickLabelOnLeft = tickLabelOnLeft;
+        
         _parentNode = new PComposite();
         addChild( _parentNode );
         
-        setDownvalleyLocation( 0 );
+        createNodes( 0 /* x location */ );
     }
     
-    public void setDownvalleyLocation( double x ) {
+    /*
+     * Creates all nodes at some downvalley (horizontal) location.
+     */
+    private void createNodes( double x ) {
         
         _parentNode.removeAllChildren();
         
@@ -68,7 +74,7 @@ public class ElevationAxisNode extends PComposite {
         // ticks & labels
         double elevation = _minElevation;
         while ( elevation <= _maxElevation ) {
-            PNode tickNode = createLabeledTickNode( x, elevation, _mvt );
+            PNode tickNode = createLabeledTickNode( x, elevation, _tickLabelOnLeft, _mvt );
             elevation += _majorTickSpacing;
             _parentNode.addChild( tickNode );
         }
@@ -104,12 +110,12 @@ public class ElevationAxisNode extends PComposite {
     /*
      * Creates a labeled tick, a horizontal line with a label to the right of the line.
      */
-    private static PNode createLabeledTickNode( double x, double elevation, ModelViewTransform mvt ) {
+    private static PNode createLabeledTickNode( double x, double elevation, boolean tickLabelOnLeft, ModelViewTransform mvt ) {
         
         PComposite parentNode = new PComposite();
         
         // tick
-        PNode tickNode = createTickNode( x, elevation, mvt );
+        PNode tickNode = createTickNode( x, elevation, tickLabelOnLeft, mvt );
         parentNode.addChild( tickNode );
         
         // label
@@ -120,7 +126,7 @@ public class ElevationAxisNode extends PComposite {
         tickNode.setOffset( 0, 0 );
         PBounds tb = tickNode.getFullBoundsReference();
         PBounds lb = labelNode.getFullBoundsReference();
-        if ( TICK_ON_LEFT ) {
+        if ( tickLabelOnLeft ) {
             labelNode.setOffset( tb.getX() - lb.getWidth() - 2, tb.getY() + ( tb.getHeight() / 2 ) - ( lb.getHeight() / 2 ) );
         }
         else {
@@ -133,14 +139,14 @@ public class ElevationAxisNode extends PComposite {
     /*
      * Creates a tick, a horizontal line.
      */
-    private static PNode createTickNode( double x, double elevation, ModelViewTransform mvt ) {
+    private static PNode createTickNode( double x, double elevation, boolean tickLabelOnLeft, ModelViewTransform mvt ) {
         
         // right edge of tick
-        Point2D pLeftModel = new Point2D.Double( ( TICK_ON_LEFT ? x : x + TICK_LENGTH ), elevation );
+        Point2D pLeftModel = new Point2D.Double( ( tickLabelOnLeft ? x : x + TICK_LENGTH ), elevation );
         Point2D pLeftView = mvt.modelToView( pLeftModel );
         
         // left edge of tick
-        Point2D pRightModel = new Point2D.Double( ( TICK_ON_LEFT ? x - TICK_LENGTH : x ), elevation );
+        Point2D pRightModel = new Point2D.Double( ( tickLabelOnLeft ? x - TICK_LENGTH : x ), elevation );
         Point2D pRightView = mvt.modelToView( pRightModel );
         
         // path
