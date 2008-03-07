adbc47650e45b98a9cd727ac7dc8350f47d46681
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-03-07 19:40:31 +0000

    3/6/08 - visualization of ice velocity at stationary (x,z) locations 3/6/08 - bugfix in implementation of ice velocity model

--- trunk/simulations-java/simulations/glaciers/src/edu/colorado/phet/glaciers/view/IceVelocitiesNode.java
+++ trunk/simulations-java/simulations/glaciers/src/edu/colorado/phet/glaciers/view/IceVelocitiesNode.java
@@ -2,12 +2,18 @@
 
 package edu.colorado.phet.glaciers.view;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Paint;
+import java.awt.Stroke;
+import java.awt.geom.Point2D;
+
 import edu.colorado.phet.common.phetcommon.math.Vector2D;
+import edu.colorado.phet.common.piccolophet.nodes.Vector2DNode;
 import edu.colorado.phet.glaciers.model.Glacier;
 import edu.colorado.phet.glaciers.model.Glacier.GlacierAdapter;
 import edu.colorado.phet.glaciers.model.Glacier.GlacierListener;
 import edu.umd.cs.piccolo.PNode;
-import edu.umd.cs.piccolo.nodes.PText;
 import edu.umd.cs.piccolox.nodes.PComposite;
 
 
@@ -17,9 +23,18 @@ public class IceVelocitiesNode extends PComposite {
     // Class data
     //----------------------------------------------------------------------------
     
-    private static final double DX = 100;
-    private static final double DZ = 10;
-    private static final double UNIT_VECTOR_LENGTH = 1; // length (in meters) of a 1 meter/year velocity
+    private static final double DX = 400;
+    private static final double DZ = 50;
+    private static final double ICE_ROCK_MARGIN = 20; // meters
+    private static final double ICE_AIR_MARGIN = 0; // meters
+    
+    private static final double VELOCITY_VECTOR_SCALE = 5; // scale vectors by this much
+    private static final double VELOCITY_VECTOR_HEAD_HEIGHT = 4;
+    private static final double VELOCITY_VECTOR_HEAD_WIDTH = 4;
+    private static final double VELOCITY_VECTOR_TAIL_WIDTH = 1;
+    private static final Stroke VELOCITY_VECTOR_STROKE = new BasicStroke( 1f );
+    private static final Paint VELOCITY_VECTOR_STROKE_PAINT = null;
+    private static final Paint VELOCITY_VECTOR_FILL_PAINT = Color.BLACK;
     
     //----------------------------------------------------------------------------
     // Instance data
@@ -52,6 +67,9 @@ public class IceVelocitiesNode extends PComposite {
         _mvt = mvt;
         
         _parentNode = new PComposite();
+        _parentNode.setOffset( 0, 0 );
+        addChild( _parentNode );
+        
         _isDirty = true;
         
         updateVectors();
@@ -70,29 +88,40 @@ public class IceVelocitiesNode extends PComposite {
             _isDirty = true;
         }
         else {
-//            _parentNode.removeAllChildren();
-//            final double xTerminus = _glacier.getTerminusX();
-//            double x = Glacier.getX0();
-//            while ( x <= xTerminus ) {
-//                double valleyFloorElevation = _glacier.getValley().getElevation( x );
-//                double iceSurfaceElevation = valleyFloorElevation + _glacier.getIceThickness( x );
-//                double z = valleyFloorElevation;
-//                while ( z <= iceSurfaceElevation ) {
-//                    Vector2D velocity = _glacier.getIceVelocity( x, z );
-//                    PNode velocityNode = createVector( x, z, velocity, _mvt );
-//                    _parentNode.addChild( velocityNode );
-//                    z += DZ;
-//                }
-//                x += DX;
-//            }
+            _parentNode.removeAllChildren();
+            final double xTerminus = _glacier.getTerminusX();
+            double x = Glacier.getX0();
+            while ( x <= xTerminus ) {
+                double valleyFloorElevation = _glacier.getValley().getElevation( x );
+                double iceSurfaceElevation = valleyFloorElevation + _glacier.getIceThickness( x );
+                double z = valleyFloorElevation + ICE_ROCK_MARGIN;
+                while ( z <= iceSurfaceElevation - ICE_AIR_MARGIN ) {
+                    Vector2D vModel = _glacier.getIceVelocity( x, z );
+                    Point2D vView = _mvt.modelToView( vModel.getX(), vModel.getY() );
+                    PNode velocityNode = new VelocityVectorNode( vView.getX(), vView.getY(), VELOCITY_VECTOR_SCALE );
+                    _parentNode.addChild( velocityNode );
+                    velocityNode.setOffset( _mvt.modelToView( x, z ) );
+                    z += DZ;
+                }
+                x += DX;
+            }
             _isDirty = false;
         }
     }
     
-    private PNode createVector( double x, double z, Vector2D v, ModelViewTransform mvt ) {
-        //XXX put an 'x' at the point where the vector will be
-        PText textNode = new PText( "x" );
-        textNode.setOffset( _mvt.modelToView( x, z ) );
-        return textNode;
+    /*
+     * VelocityVectorNode encapsulates the "look" of a fluid velocity vector.
+     */
+    private class VelocityVectorNode extends Vector2DNode {
+        public VelocityVectorNode( double x, double y, double scale ) {
+            super( x, y, 1, scale );
+            setHeadSize( VELOCITY_VECTOR_HEAD_WIDTH, VELOCITY_VECTOR_HEAD_HEIGHT );
+            setTailWidth( VELOCITY_VECTOR_TAIL_WIDTH );
+            setArrowFillPaint( VELOCITY_VECTOR_FILL_PAINT );
+            setArrowStroke( VELOCITY_VECTOR_STROKE );
+            setArrowStrokePaint( VELOCITY_VECTOR_STROKE_PAINT );
+            setFractionalHeadHeight( 0.5 );
+            setValueVisible( false );
+        }
     }
 }
