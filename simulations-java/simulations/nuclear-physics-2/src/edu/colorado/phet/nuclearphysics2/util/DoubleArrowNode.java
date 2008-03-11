8dcc964117f79fa52e4e62111d819337054af883
jxb147 <jxb147@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-03-11 17:50:53 +0000

    Changed the axes of the alpha radiation chart to scale properly when the window is resized.  Also cleaned up the potential energy curve.  Finally, the string resources for Spanish and Portuguese were imported from the old Nuclear Physics project.

--- simulations-java/simulations/nuclear-physics-2/src/edu/colorado/phet/nuclearphysics2/util/DoubleArrowNode.java
+++ simulations-java/simulations/nuclear-physics-2/src/edu/colorado/phet/nuclearphysics2/util/DoubleArrowNode.java
@@ -14,6 +14,22 @@ import edu.umd.cs.piccolo.nodes.PPath;
  */
 public class DoubleArrowNode extends PPath {
 
+    //------------------------------------------------------------------------
+    // Class data
+    //------------------------------------------------------------------------
+    
+    // The points that describe the location of the tip and tail of the arrow.
+    Point2D m_tipLocation;
+    Point2D m_tailLocation;
+    
+    // The two "sub-arrows" that comprise this arrow node.
+    Arrow m_midToTip;
+    Arrow m_midToTail;
+    
+    // Overall shape of the two combined arrows that comprise this double-ended
+    // arrow.
+    GeneralPath m_overallShape;
+    
     /**
      * Constructor.
      *
@@ -32,13 +48,20 @@ public class DoubleArrowNode extends PPath {
         
         // Create two single-ended arrows, one from the midpoint to the tail
         // and one from the midpoint to the tip.
-        Arrow midToTip  = new Arrow( midpoint, tipLocation, headHeight, headWidth, tailWidth );
-        Arrow midToTail = new Arrow( midpoint, tailLocation, headHeight, headWidth, tailWidth );
-        GeneralPath overallShape = midToTip.getShape();
-        overallShape.append( midToTail.getShape(), false );
+        m_midToTip  = new Arrow( midpoint, tipLocation, headHeight, headWidth, tailWidth );
+        m_midToTail = new Arrow( midpoint, tailLocation, headHeight, headWidth, tailWidth );
+        m_overallShape = m_midToTip.getShape();
+        m_overallShape.append( m_midToTail.getShape(), false );       
+        setPathTo( m_overallShape );
         
-        setPathTo( overallShape );
+        // Save the tip and tail points for later use.
+        m_tailLocation = tailLocation;
+        m_tipLocation = tipLocation;
     }
+    
+    //------------------------------------------------------------------------
+    // Constructor
+    //------------------------------------------------------------------------
 
     /**
      * Constructor.
@@ -61,17 +84,72 @@ public class DoubleArrowNode extends PPath {
         
         // Create two single-ended arrows, one from the midpoint to the tail
         // and one from the midpoint to the tip.
-        Arrow midToTip  = new Arrow( midpoint, tipLocation, headHeight, headWidth, tailWidth, fractionalHeadHeight, scaleTailToo );
-        Arrow midToTail = new Arrow( midpoint, tailLocation, headHeight, headWidth, tailWidth, fractionalHeadHeight, scaleTailToo );
-        GeneralPath overallShape = midToTip.getShape();
-        overallShape.append( midToTail.getShape(), false );
-        
-        setPathTo( overallShape );
+        m_midToTip  = new Arrow( midpoint, tipLocation, headHeight, headWidth, tailWidth, fractionalHeadHeight, scaleTailToo );
+        m_midToTail = new Arrow( midpoint, tailLocation, headHeight, headWidth, tailWidth, fractionalHeadHeight, scaleTailToo );
+        m_overallShape = m_midToTip.getShape();
+        m_overallShape.append( m_midToTail.getShape(), false );       
+        setPathTo( m_overallShape );        
     }
-    
+
+    //------------------------------------------------------------------------
+    // Methods
+    //------------------------------------------------------------------------
+
+    /**
+     * Method to set a new tip location, effectively moving the arrow as a
+     * result.
+     */
+    public void setTipLocation(Point2D newTipLocation){
+        m_tipLocation = newTipLocation;
+        
+        Point2D midpoint = midPoint(m_tailLocation, m_tipLocation);
+        
+        m_midToTip.setTipAndTailLocations( m_tipLocation, midpoint );
+        m_midToTail.setTipAndTailLocations( m_tailLocation, midpoint );
+     
+        m_overallShape = m_midToTip.getShape();
+        m_overallShape.append( m_midToTail.getShape(), false );       
+        setPathTo( m_overallShape );
+    }
+
+    /**
+     * Method to set a new tail location, effectively moving the arrow as a
+     * result.
+     */
+    public void setTailLocation(Point2D newTailLocation){
+        m_tailLocation = newTailLocation;
+        
+        Point2D midpoint = midPoint(m_tailLocation, m_tipLocation);
+        
+        m_midToTip.setTipAndTailLocations( m_tipLocation, midpoint );
+        m_midToTail.setTipAndTailLocations( m_tailLocation, midpoint );
+     
+        m_overallShape = m_midToTip.getShape();
+        m_overallShape.append( m_midToTail.getShape(), false );       
+        setPathTo( m_overallShape );
+    }
+
+    /**
+     * Method to set new locations for both the tail and the tip of the arrow.
+     * This method can be used to translate or rotate the arrow.
+     */
+    public void setTipAndTailLocations(Point2D newTipLocation, Point2D newTailLocation){
+        m_tailLocation = newTailLocation;
+        m_tipLocation = newTipLocation;
+        
+        Point2D midpoint = midPoint(m_tailLocation, m_tipLocation);
+        
+        m_midToTip.setTipAndTailLocations( m_tipLocation, midpoint );
+        m_midToTail.setTipAndTailLocations( m_tailLocation, midpoint );
+     
+        m_overallShape = m_midToTip.getShape();
+        m_overallShape.append( m_midToTail.getShape(), false );       
+        setPathTo( m_overallShape );
+    }
+
     private Point2D midPoint(Point2D point1, Point2D point2){ 
         
         return (new Point2D.Double( point1.getX() +( (point2.getX()-point1.getX())/2 ), point1.getY() +( (point2.getY()-point1.getY())/2 ) ));
     }
-
+    
 }
