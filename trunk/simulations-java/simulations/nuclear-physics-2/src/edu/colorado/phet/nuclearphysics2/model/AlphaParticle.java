9eb209e4944b126294213c55e86eb210f1a24eeb
jxb147 <jxb147@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-03-19 20:18:44 +0000

    Cleaned up the routines that control the quantum tunneling behavior.

--- trunk/simulations-java/simulations/nuclear-physics-2/src/edu/colorado/phet/nuclearphysics2/model/AlphaParticle.java
+++ trunk/simulations-java/simulations/nuclear-physics-2/src/edu/colorado/phet/nuclearphysics2/model/AlphaParticle.java
@@ -88,6 +88,10 @@ public class AlphaParticle implements AtomicNucleusConstituent {
         }
     }
     
+    // TODO: jblanco 03-19-2008 - The most recent feedback is that the
+    // particles should just tunnel and not translate.  Based on that feedback,
+    // the following method is no longer used.  Remove it if this decision is
+    // ratified.
     /**
      * Tell the particle to move itself based on its current velocity.
      */
@@ -217,28 +221,22 @@ public class AlphaParticle implements AtomicNucleusConstituent {
      * @param maxDistance - Maximum distance from origin (0,0).
      */
     public void tunnel(double minDistance, double maxDistance)
-    {
-        /*
-        double guassian = Math.abs( _rand.nextGaussian() / 1.0 );
-        if (guassian > 1.0){
-            // Hard limit guassian to a max of 1.0.
-            guassian = 1.0;
-        }
-        double newRadius = minDistance + (guassian * (maxDistance - minDistance));
-        */
-        //double newRadius = minDistance + (_rand.nextDouble() * (maxDistance - minDistance));
-        //double newRadius = minDistance + (_rand.nextDouble() * _rand.nextDouble() * (maxDistance - minDistance));
+    {       
+        // Create a probability distribution that will cause the particles to
+        // be fairly evenly spread around the core of the nucleus and appear
+        // occasionally at the outer reaches.
         
         double multiplier = _rand.nextDouble();
+        
         if (multiplier > 0.8)
         {
             // Cause the distribution to tail off in the outer regions of the
             // nucleus.
             multiplier = _rand.nextDouble() * _rand.nextDouble();
         }
-        double newRadius = minDistance + (multiplier * (maxDistance - minDistance));
-
         
+        double newRadius = minDistance + (multiplier * (maxDistance - minDistance));
+       
         // Calculate the new angle, in radians, from the origin.
         double newAngle = _rand.nextDouble() * 2 * Math.PI;
         
