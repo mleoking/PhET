858a72fee64b4b7e14d25f65e1d5249d426ab188
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-04-04 17:10:43 +0000

    Rewrote sections of AnimateClockJComponent to improve readability and future maintenance. Changes include: - all sizing (componet, clock face, hands) is now specified in terms of desired radius of the clock face - origin of shapes is the center of the clock face - clock face is a first-class object, like clock hands - taking clock face stroke width into account eliminates need for insets - use CAP_BUTT on hand strokes so that their length is not extended - convert a bunch of things into constants that were previously instance data or hardcoded

--- simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/view/AnimatedClockJComponent.java
+++ simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/view/AnimatedClockJComponent.java
@@ -1,50 +1,67 @@
+/* Copyright 2008, University of Colorado */
+
 package edu.colorado.phet.common.phetcommon.view;
 
 import java.awt.*;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
 
-import javax.swing.*;
+import javax.swing.JComponent;
 
 import edu.colorado.phet.common.phetcommon.model.clock.ClockAdapter;
 import edu.colorado.phet.common.phetcommon.model.clock.ClockEvent;
 import edu.colorado.phet.common.phetcommon.model.clock.IClock;
 
 /**
- * Created by: Sam
- * Apr 3, 2008 at 3:15:34 PM
+ * Animated clock icon, hands move when the clock ticks.
+ * 
+ * @author Sam Reid
  */
 public class AnimatedClockJComponent extends JComponent {
-    private IClock clock;  //the clock we observe
 
-    private int componentWidth = 26;
-    private double inset = 2;
+    // Face properties
+    private static final double FACE_RADIUS = 11;
+    private static final float FACE_STROKE_WIDTH = 2.5f;
+    private static final Color FACE_FILL_COLOR = Color.WHITE;
+    private static final Color FACE_STROKE_COLOR = Color.BLACK;
+    private static final BasicStroke FACE_STROKE = new BasicStroke( FACE_STROKE_WIDTH, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER );
+    
+    // Hand properties
+    private static final BasicStroke HAND_STROKE = new BasicStroke( 2f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER );
+    private static final Color HAND_STROKE_COLOR = Color.BLACK;
+    private static final double FAST_HAND_LENGTH = 0.70 * FACE_RADIUS;
+    private static final double SLOW_HAND_LENGTH = 0.55 * FACE_RADIUS;
+    private static final double FAST_HAND_START_ANGLE = -Math.PI / 2; // starting angle of the fast hand (radians)
+    private static final double SLOW_HAND_START_ANGLE = FAST_HAND_START_ANGLE; // starting angle of the slow hand (radians)
+    private static final double FAST_HAND_DELTA_ANGLE = 0.09; // change in angle per clock tick (radians), needs to be fast enough that tick results in a visible move
+    private static final double SLOW_HAND_DELTA_ANGLE = FAST_HAND_DELTA_ANGLE / 12; // change in angle per clock tick (radians)
 
-    private BasicStroke clockShapeStroke = new BasicStroke( 2.5f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_ROUND );
-    private Ellipse2D.Double clockShape = new Ellipse2D.Double();
+    // Parts of a clock instance
+    private final Face face;
+    private final Hand fastHand, slowHand;
+    
+    public AnimatedClockJComponent( final IClock clock ) {
+        
+        // size the component to fit the clock face, account for stroke width
+        int componentSize = (int)( ( 2 * FACE_RADIUS ) + ( FACE_STROKE_WIDTH ) ) + 1; // +1 to compensate for cast to int
+        setPreferredSize( new Dimension( componentSize, componentSize ) );
 
-    private Hand fastHand = new Hand( DEFAULT_ANGLE, ( componentWidth - inset * 2 ) / 2 * FAST_HAND_SCALE, FAST_SPEED, Color.black, new BasicStroke( 2.5f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_MITER ) );
-    private Hand slowHand = new Hand( DEFAULT_ANGLE, ( componentWidth - inset * 2 ) / 2 * SLOW_HAND_SCALE, FAST_SPEED / 60, Color.black, new BasicStroke( 2.5f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_MITER ) );
+        face = new Face( FACE_RADIUS );
+        fastHand = new Hand( FAST_HAND_LENGTH, FAST_HAND_START_ANGLE, FAST_HAND_DELTA_ANGLE );
+        slowHand = new Hand( SLOW_HAND_LENGTH, SLOW_HAND_START_ANGLE, SLOW_HAND_DELTA_ANGLE );
 
-    //Defaults
-    private static final double FAST_SPEED = 0.09;//needs to be fast enough that STEP makes a visible move
-    private static final double DEFAULT_ANGLE = -Math.PI / 2;
-    private static final double FAST_HAND_SCALE = 0.65;
-    private static final double SLOW_HAND_SCALE = 0.55;
-
-    public AnimatedClockJComponent( IClock clock ) {
-        this.clock = clock;
-        setPreferredSize( new Dimension( componentWidth, componentWidth ) );
         clock.addClockListener( new ClockAdapter() {
+            /* When the clock ticks, update the hands */
             public void clockTicked( ClockEvent clockEvent ) {
                 fastHand.update();
                 slowHand.update();
                 repaint( 0, 0, getWidth(), getHeight() );
             }
 
+            /* When the clock is reset, move the hands to midnight */
             public void simulationTimeReset( ClockEvent clockEvent ) {
-                fastHand.setAngle( DEFAULT_ANGLE );
-                slowHand.setAngle( DEFAULT_ANGLE );
+                fastHand.setAngle( FAST_HAND_START_ANGLE );
+                slowHand.setAngle( SLOW_HAND_START_ANGLE );
                 repaint( 0, 0, getWidth(), getHeight() );
             }
         } );
@@ -54,61 +71,89 @@ public class AnimatedClockJComponent extends JComponent {
         super.paintComponent( g );
 
         Graphics2D g2 = (Graphics2D) g;
-        RenderingHints renderingHints = g2.getRenderingHints();
+        
+        // save graphics state
+        RenderingHints origRenderingHints = g2.getRenderingHints();
         Stroke origStroke = g2.getStroke();
         Paint origPaint = g2.getPaint();
 
         g2.setRenderingHint( RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON );
 
-        g2.setStroke( clockShapeStroke );
-
-        clockShape.setFrame( inset, inset, componentWidth - inset * 2, componentWidth - inset * 2 );
-
-        g2.setPaint( Color.white );
-        g2.fill( clockShape );
-
-        g2.setPaint( Color.black );
-        g2.draw( clockShape );
-
+        // translate origin to center of component
+        final double tx = getWidth() / 2;
+        final double ty = tx;
+        g2.translate( tx, ty );
+        
+        // draw the parts of the clock
+        face.draw( g2 );
         fastHand.draw( g2 );
         slowHand.draw( g2 );
 
-        g2.setRenderingHints( renderingHints );
+        // restore graphics state
+        g2.translate( -tx, -ty );
+        g2.setRenderingHints( origRenderingHints );
         g2.setStroke( origStroke );
         g2.setPaint( origPaint );
     }
 
-    private class Hand {
-        private double angle;
-        private double radius;
-        private double speed;
-        private Color color;
-        private Stroke stroke;
-        private Line2D.Double s=new Line2D.Double( );
-
-        Hand( double angle, double radius, double speed, Color color, Stroke stroke ) {
-            this.angle = angle;
-            this.radius = radius;
-            this.speed = speed;
-            this.color = color;
-            this.stroke = stroke;
+    /**
+     * Clock face, origin is at the center of the face, where the hands pivot.
+     */
+    private static class Face {
+        private final Shape shape;
+        
+        public Face( double radius ) {
+            this.shape = new Ellipse2D.Double( -radius, -radius, 2 * radius, 2 * radius ); // origin at center of clock face
         }
-
-        public void update() {
-            angle += speed;
-        }
-
+        
+        /*
+         * Draws the clock face, with origin at the center of the face.
+         * Assumes the caller is saving and restoring graphics state
+         */
         public void draw( Graphics2D g2 ) {
-            g2.setColor( color );
-            g2.setStroke( stroke );
-            double x = componentWidth / 2.0 + inset / 4;//todo: i don't understand why this is off by a pixel or so
-            double y = componentWidth / 2.0 + inset / 4;
-            s.setLine( x, y, x + radius * Math.cos( angle ), y + radius * Math.sin( angle ) );
-            g2.draw( s );
+            g2.setStroke( FACE_STROKE );
+            g2.setPaint( FACE_FILL_COLOR );
+            g2.fill( shape );
+            g2.setPaint( FACE_STROKE_COLOR );
+            g2.draw( shape );
+        }
+        
+    }
+    
+    /**
+     * Clock hand, origin is at the point where the hand pivots.
+     */
+    private static class Hand {
+
+        private final double length;
+        private double angle;
+        private final double deltaAngle;
+        private final Line2D.Double line;
+
+        public Hand( double length, double angle, double deltaAngle ) {
+            this.length = length;
+            this.angle = angle;
+            this.deltaAngle = deltaAngle;
+            this.line = new Line2D.Double();
         }
 
         public void setAngle( double angle ) {
             this.angle = angle;
+        }
+        
+        public void update() {
+            angle += deltaAngle;
+        }
+
+        /*
+         * Draws the clock hand, starting at the center of the clock face.
+         * Assumes the caller is saving and restoring graphics state
+         */
+        public void draw( Graphics2D g2 ) {
+            g2.setColor( HAND_STROKE_COLOR );
+            g2.setStroke( HAND_STROKE );
+            line.setLine( 0, 0, length * Math.cos( angle ), length * Math.sin( angle ) ); // origin at center of clock face
+            g2.draw( line );
         }
     }
 
