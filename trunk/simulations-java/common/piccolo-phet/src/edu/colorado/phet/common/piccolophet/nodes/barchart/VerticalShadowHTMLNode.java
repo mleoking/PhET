1f49733a26a144b35015d9e6982d9626279fd1cf
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-04-10 19:37:46 +0000

    cleanup

--- trunk/simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/nodes/barchart/VerticalShadowHTMLNode.java
+++ trunk/simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/nodes/barchart/VerticalShadowHTMLNode.java
@@ -7,25 +7,22 @@ import edu.colorado.phet.common.piccolophet.nodes.ShadowHTMLNode;
 import edu.umd.cs.piccolo.PNode;
 
 /**
- * This class is used by Energy Skate Park (and possibly other sims). 
+ * This class is used to draw the vertical axis label of a BarChartNode.
  * It is still under development and subject to change.
+ *
  * @author Sam Reid
  */
 
-public class VerticalTextNode extends PNode {
-    private Font font;
+public class VerticalShadowHTMLNode extends PNode {
     private String text;
 
-    public VerticalTextNode( Font font, String text, Color color, Color outline ) {
-        this.font = font;
+    public VerticalShadowHTMLNode( Font font, String text, Color color, Color outline ) {
         this.text = text;
         ShadowHTMLNode phetTextNode = new ShadowHTMLNode( text );//, font, color, 1, 1, outline );
         phetTextNode.setColor( color );
         phetTextNode.setShadowColor( outline );
         phetTextNode.setFont( font );
 
-        double h = phetTextNode.getFullBounds().getHeight();
-//        System.out.println( "h = " + h );
         phetTextNode.translate( -3, -10 );
         phetTextNode.rotate( -Math.PI / 2 );
 
