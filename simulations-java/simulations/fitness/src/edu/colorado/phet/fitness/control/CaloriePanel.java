d61d59808ec270968d51806d08013fd369e62588
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-04-10 19:08:52 +0000

    added a method for doing multistep scaling, much better graphics resolution for downscaling

--- simulations-java/simulations/fitness/src/edu/colorado/phet/fitness/control/CaloriePanel.java
+++ simulations-java/simulations/fitness/src/edu/colorado/phet/fitness/control/CaloriePanel.java
@@ -204,7 +204,7 @@ public class CaloriePanel extends PNode {
     }
 
     private static class IconStrip extends PNode {
-        double height = 60;
+        double height = 30;
 
 
         private IconStrip( IconItem[] iconItems ) {
@@ -240,8 +240,12 @@ public class CaloriePanel extends PNode {
         }
 
         private BoxedImage createNode( IconItem foodItem, int index, BoxedImage[] others ) {
-            BoxedImage imx = new BoxedImage( FitnessResources.getImage( foodItem.getImageName() ) );
-            imx.scale( height / imx.getFullBounds().getHeight() );
+            BufferedImage image = FitnessResources.getImage( foodItem.getImageName() );
+            double sy = height / image.getHeight();
+            int width = (int) ( sy * image.getWidth() );
+            BoxedImage imx = new BoxedImage( getScaledInstance( image, width, (int) height, RenderingHints.VALUE_INTERPOLATION_BILINEAR, true ) );
+
+//            imx.scale( height / imx.getFullBounds().getHeight() );
             addChild( imx );
             imx.addInputEventListener( new CursorHandler() );
             if ( index > 0 ) {
@@ -263,4 +267,82 @@ public class CaloriePanel extends PNode {
             }
         }
     }
+
+    /**
+     * 
+     * Convenience method that returns a scaled instance of the
+     * provided {@code BufferedImage}.
+     *
+     * @param img           the original image to be scaled
+     * @param targetWidth   the desired width of the scaled instance,
+     *                      in pixels
+     * @param targetHeight  the desired height of the scaled instance,
+     *                      in pixels
+     * @param hint          one of the rendering hints that corresponds to
+     *                      {@code RenderingHints.KEY_INTERPOLATION} (e.g.
+     *                      {@code RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR},
+     *                      {@code RenderingHints.VALUE_INTERPOLATION_BILINEAR},
+     *                      {@code RenderingHints.VALUE_INTERPOLATION_BICUBIC})
+     * @param higherQuality if true, this method will use a multi-step
+     *                      scaling technique that provides higher quality than the usual
+     *                      one-step technique (only useful in downscaling cases, where
+     *                      {@code targetWidth} or {@code targetHeight} is
+     *                      smaller than the original dimensions, and generally only when
+     *                      the {@code BILINEAR} hint is specified)
+     * @return a scaled version of the original {@code BufferedImage}
+     *         <p/>
+     *
+     * see http://today.java.net/pub/a/today/2007/04/03/perils-of-image-getscaledinstance.html
+     * see also SwingLabs GraphicsUtilities
+     */
+    public static BufferedImage getScaledInstance( BufferedImage img,
+                                                   int targetWidth,
+                                                   int targetHeight,
+                                                   Object hint,
+                                                   boolean higherQuality ) {
+        int type = BufferedImage.TYPE_INT_ARGB;
+        BufferedImage ret = (BufferedImage) img;
+        int w, h;
+        if ( higherQuality ) {
+            // Use multi-step technique: start with original size, then
+            // scale down in multiple passes with drawImage()
+            // until the target size is reached
+            w = img.getWidth();
+            h = img.getHeight();
+        }
+        else {
+            // Use one-step technique: scale directly from original
+            // size to target size with a single drawImage() call
+            w = targetWidth;
+            h = targetHeight;
+        }
+
+        do {
+            if ( higherQuality && w > targetWidth ) {
+                w /= 2;
+                if ( w < targetWidth ) {
+                    w = targetWidth;
+                }
+            }
+
+            if ( higherQuality && h > targetHeight ) {
+                h /= 2;
+                if ( h < targetHeight ) {
+                    h = targetHeight;
+                }
+            }
+
+            BufferedImage tmp = new BufferedImage( w, h, type );
+            Graphics2D g2 = tmp.createGraphics();
+            g2.setRenderingHint( RenderingHints.KEY_INTERPOLATION, hint );
+            g2.drawImage( ret, 0, 0, w, h, null );
+            g2.dispose();
+
+            ret = tmp;
+        } while ( w != targetWidth || h != targetHeight );
+
+        return ret;
+    }
+
+
 }
