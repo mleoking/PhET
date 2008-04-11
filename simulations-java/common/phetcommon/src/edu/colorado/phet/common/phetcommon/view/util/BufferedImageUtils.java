9c84a264e9b4c86f98b9c99a3d15428d2148fd80
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-04-11 03:43:00 +0000

    added a Convenience method for getScaledInstance with typical default values, see below

--- simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/view/util/BufferedImageUtils.java
+++ simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/view/util/BufferedImageUtils.java
@@ -255,28 +255,37 @@ public class BufferedImageUtils {
     }
 
     /**
+     * Convenience method for getScaledInstance with typical default values, see below
+     */
+    public static BufferedImage multiScale( BufferedImage img, double scale ) {
+        int w = (int) ( img.getWidth() * scale );
+        int h = (int) ( img.getHeight() * scale );
+        return getScaledInstance( img, w, h, RenderingHints.VALUE_INTERPOLATION_BILINEAR, true );
+    }
+
+    /**
      * This method uses multi-step scaling techniques for downscaling for better image quality.
      * see http://today.java.net/pub/a/today/2007/04/03/perils-of-image-getscaledinstance.html
-     * see also SwingLabs GraphicsUtilities 
-     *
+     * see also SwingLabs GraphicsUtilities
+     * <p/>
      * Convenience method that returns a scaled instance of the provided {@code BufferedImage}.
      *
-     * @param img           the original image to be scaled
-     * @param targetWidth   the desired width of the scaled instance,
-     *                      in pixels
-     * @param targetHeight  the desired height of the scaled instance,
-     *                      in pixels
-     * @param interpolationHintValue          one of the rendering hints that corresponds to
-     *                      {@code RenderingHints.KEY_INTERPOLATION} (e.g.
-     *                      {@code RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR},
-     *                      {@code RenderingHints.VALUE_INTERPOLATION_BILINEAR},
-     *                      {@code RenderingHints.VALUE_INTERPOLATION_BICUBIC})
-     * @param higherQuality if true, this method will use a multi-step
-     *                      scaling technique that provides higher quality than the usual
-     *                      one-step technique (only useful in downscaling cases, where
-     *                      {@code targetWidth} or {@code targetHeight} is
-     *                      smaller than the original dimensions, and generally only when
-     *                      the {@code BILINEAR} hint is specified)
+     * @param img                    the original image to be scaled
+     * @param targetWidth            the desired width of the scaled instance,
+     *                               in pixels
+     * @param targetHeight           the desired height of the scaled instance,
+     *                               in pixels
+     * @param interpolationHintValue one of the rendering hints that corresponds to
+     *                               {@code RenderingHints.KEY_INTERPOLATION} (e.g.
+     *                               {@code RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR},
+     *                               {@code RenderingHints.VALUE_INTERPOLATION_BILINEAR},
+     *                               {@code RenderingHints.VALUE_INTERPOLATION_BICUBIC})
+     * @param higherQuality          if true, this method will use a multi-step
+     *                               scaling technique that provides higher quality than the usual
+     *                               one-step technique (only useful in downscaling cases, where
+     *                               {@code targetWidth} or {@code targetHeight} is
+     *                               smaller than the original dimensions, and generally only when
+     *                               the {@code BILINEAR} hint is specified)
      * @return a scaled version of the original {@code BufferedImage}
      */
     public static BufferedImage getScaledInstance( BufferedImage img,
