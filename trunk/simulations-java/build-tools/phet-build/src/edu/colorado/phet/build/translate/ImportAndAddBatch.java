b83be582cc26f3e8b2f6595e2df54f3e70dc0237
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-04-16 17:19:59 +0000

    add some messages/directions in dialogs

--- trunk/simulations-java/build-tools/phet-build/src/edu/colorado/phet/build/translate/ImportAndAddBatch.java
+++ trunk/simulations-java/build-tools/phet-build/src/edu/colorado/phet/build/translate/ImportAndAddBatch.java
@@ -2,6 +2,8 @@ package edu.colorado.phet.build.translate;
 
 import java.io.File;
 
+import javax.swing.JOptionPane;
+
 /**
  * Created by: Sam
  * Feb 28, 2008 at 11:55:38 PM
@@ -18,13 +20,24 @@ public class ImportAndAddBatch {
             System.out.println( "usage: ImportAndAddBatch path-to-simulations-java-in-your-workspace" );
             System.exit( 1 );
         }
-        new ImportAndAddBatch( new File( args[0] ) ).importAndAddBatch( AddTranslation.prompt( "Which Directory to import and batch add?" ) );
+        JOptionPane.showMessageDialog( null,
+                "<html>Put the localization files that you wish to deploy in a directory.<br>" +
+                "When you have finished this step, press OK to continue.<br>" +
+                "You will be prompted for the directory name.</html>" );
+        String dirname = AddTranslation.prompt( "Enter the name of the directory where your localization files are:" );
+        new ImportAndAddBatch( new File( args[0] ) ).importAndAddBatch( dirname );
     }
 
     private void importAndAddBatch( String dir ) throws Exception {
-        System.out.println( "Importing" );
+        System.out.println( "Importing..." );
         new ImportTranslations( baseDir ).importTranslations( new File( dir ) );
         System.out.println( "Finished Importing." );
+        JOptionPane.showMessageDialog( null, 
+                "<html>Localization files have been imported into your IDE workspace.<br>" +
+        		"Please refresh your workspace, examine the files,<br>" +
+        		"and manually commit them to the SVN repository.<br><br>" +
+        		"Press OK when you are ready to integrate the files into<br>" +
+        		"the PHET production server." );
 
         System.out.println( "Adding to tigercat" );
         String username = AddTranslation.prompt( "Production Server username:" );
