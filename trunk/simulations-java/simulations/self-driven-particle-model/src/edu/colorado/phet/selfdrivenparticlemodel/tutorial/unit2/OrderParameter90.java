738344ba3f66caa6c62c3b8134223378c11ede75
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-04-18 04:50:11 +0000

    removed lucidasansfont class

--- trunk/simulations-java/simulations/self-driven-particle-model/src/edu/colorado/phet/selfdrivenparticlemodel/tutorial/unit2/OrderParameter90.java
+++ trunk/simulations-java/simulations/self-driven-particle-model/src/edu/colorado/phet/selfdrivenparticlemodel/tutorial/unit2/OrderParameter90.java
@@ -2,8 +2,8 @@
 package edu.colorado.phet.selfdrivenparticlemodel.tutorial.unit2;
 
 import edu.colorado.phet.selfdrivenparticlemodel.tutorial.BasicTutorialCanvas;
-import edu.colorado.phet.selfdrivenparticlemodel.tutorial.LucidaSansFont;
 import edu.colorado.phet.selfdrivenparticlemodel.tutorial.unit1.FullFeatureBaseClass;
+import edu.colorado.phet.common.phetcommon.view.util.PhetDefaultFont;
 import edu.umd.cs.piccolo.activities.PActivity;
 import edu.umd.cs.piccolo.nodes.PText;
 
@@ -32,7 +32,7 @@ public class OrderParameter90 extends FullFeatureBaseClass {
 //        artificialAdvance();
         orderParamText = new PText();
         orderParamText.setTextPaint( Color.blue );
-        orderParamText.setFont( new LucidaSansFont( 16, true ) );
+        orderParamText.setFont( new PhetDefaultFont( 16, true ) );
         decimalFormat = new DecimalFormat( "0.00" );
         activity = new PActivity( -1 ) {
             protected void activityStep( long elapsedTime ) {
