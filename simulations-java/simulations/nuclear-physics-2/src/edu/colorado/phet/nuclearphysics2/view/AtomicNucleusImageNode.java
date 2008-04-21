fd6ff3e6bd47745ead62fc2fe82b73d75f889e27
jxb147 <jxb147@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-04-21 22:06:32 +0000

    Created a factory object for generating images of atomic nuclei based on their atomic weights.  This includes a caching mechanism so that once a few images have been generated they can be used instead of always regenerating.

--- simulations-java/simulations/nuclear-physics-2/src/edu/colorado/phet/nuclearphysics2/view/AtomicNucleusImageNode.java
+++ simulations-java/simulations/nuclear-physics-2/src/edu/colorado/phet/nuclearphysics2/view/AtomicNucleusImageNode.java
@@ -1,5 +1,7 @@
 package edu.colorado.phet.nuclearphysics2.view;
 
+import java.util.ArrayList;
+
 import edu.colorado.phet.nuclearphysics2.NuclearPhysics2Resources;
 import edu.colorado.phet.nuclearphysics2.model.AtomicNucleus;
 import edu.umd.cs.piccolo.PNode;
@@ -15,11 +17,29 @@ public class AtomicNucleusImageNode extends AtomicNucleusNode {
         
         // Create a graphical image that will represent this nucleus in
         // the view.
-        _displayImage = NuclearPhysics2Resources.getImageNode("Uranium Nucleus Small.png");
+//        _displayImage = NuclearPhysics2Resources.getImageNode("Uranium Nucleus Small.png");
 //        _displayImage = NucleusImageFactory.generateNucleusImage( atomicNucleus.getNumNeutrons(), 
-//                atomicNucleus.getNumProtons() );
+//                atomicNucleus.getNumProtons(), 20 );
+        _displayImage = NucleusImageFactory.getInstance().generateNucleusImage( atomicNucleus.getNumNeutrons(), 
+                atomicNucleus.getNumProtons(), 20 );
         
-        _displayImage.scale( (atomicNucleus.getDiameter()/1.5)/((_displayImage.getWidth() + _displayImage.getHeight()) / 2));
+        _displayImage.scale( (atomicNucleus.getDiameter()/1.2)/((_displayImage.getWidth() + _displayImage.getHeight()) / 2));
+        addChild(0, _displayImage);
+        
+        update();
+    }
+    
+    protected void handleAtomicWeightChanged(AtomicNucleus atomicNucleus, int numProtons, int numNeutrons, 
+            ArrayList byProducts){
+        
+        super.handleAtomicWeightChanged( atomicNucleus, numProtons, numNeutrons, byProducts );
+        
+        // Generate a new image for this node, since the weight has changed.
+        removeChild( _displayImage );
+
+        _displayImage = NucleusImageFactory.getInstance().generateNucleusImage( atomicNucleus.getNumNeutrons(), 
+                atomicNucleus.getNumProtons(), 20 );
+        _displayImage.scale( (atomicNucleus.getDiameter()/1.2)/((_displayImage.getWidth() + _displayImage.getHeight()) / 2));
         addChild(0, _displayImage);
         
         update();
--- simulations-java/simulations/nuclear-physics-2/src/edu/colorado/phet/nuclearphysics2/view/NucleusImageFactory.java
+++ simulations-java/simulations/nuclear-physics-2/src/edu/colorado/phet/nuclearphysics2/view/NucleusImageFactory.java
@@ -1,5 +1,8 @@
 package edu.colorado.phet.nuclearphysics2.view;
 
+import java.awt.Image;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Random;
 
 import javax.swing.JFrame;
@@ -11,9 +14,12 @@ import edu.umd.cs.piccolo.nodes.PImage;
 
 
 /**
- * This class generates a nucleus image from the constituent proton and
- * neutron images based on the size of the nucleus.  This is being done for
- * several reasons:
+ * This singleton class generates a nucleus image from the constituent proton
+ * and neutron images based on the size of the nucleus.  Since it would be too
+ * computationally intensive to do this every time a new nucleus is needed,
+ * images are cached and returned after once several have been created for a
+ * given atomic weight.
+ * 
  * 1. To provide images of nuclei that are proportionate in size to their
  *    atomic weight.
  * 2. To make it so that changing the way the nucleons look will also change
@@ -23,27 +29,72 @@ import edu.umd.cs.piccolo.nodes.PImage;
  * @author John Blanco
  */
 public class NucleusImageFactory {
+ 
+    //------------------------------------------------------------------------
+    // Class Data
+    //------------------------------------------------------------------------
+ 
+    private static final int IMAGES_TO_CACHE = 4;
     
     private static final double NUCLEON_DIAMETER = 1.6; // Femtometers.
     private static final double ALPHA_PARTICLE_DIAMTER = 3.2; // Femtometers.
-    
+    private static NucleusImageFactory _instance = null;
     private static Random _rand = new Random();
     
-    // Not intended for instantiation.
-    private NucleusImageFactory(){}
+    private static final int MAX_ATOMIC_WEIGHT = 250;
+
+    //------------------------------------------------------------------------
+    // Instance Data
+    //------------------------------------------------------------------------
+    
+    private ArrayList [] imageMap = new ArrayList[MAX_ATOMIC_WEIGHT];
+    
+    //------------------------------------------------------------------------
+    // Constructor
+    //------------------------------------------------------------------------
+    
+    /**
+     * Constructor for this singleton class, exists to defeat direct instantiation.
+     */
+    private NucleusImageFactory(){
+    }
+    
+    //------------------------------------------------------------------------
+    // Public Methods
+    //------------------------------------------------------------------------
+    
+    /**
+     * Get the one and only instance of this class.
+     */
+    public static NucleusImageFactory getInstance(){
+        if (_instance == null){
+            _instance = new NucleusImageFactory();
+        }
+        return _instance;
+    }
     
     /**
      * Generates an image of an atomic nucleus out of images of protons,
      * neutrons, and alpha particles that are available as resources for this
-     * simulation.  Note that this does NOT generate a scaled image - the
-     * responsibility for scaling rests with the caller.
+     * simulation.
      * 
      * @param numProtons
      * @param numNeutrons
+     * @param pixelsPerFm - The desired pixels per femtometer of the supplied image.
      * @return
      */
-    public static PImage generateNucleusImage(int numProtons, int numNeutrons){
+    public PImage generateNucleusImage(int numProtons, int numNeutrons, double pixelsPerFm){
         
+        Image nucleusImage;
+        
+        // If we have enough cached images at this atomic weight, use one
+        // of them.
+        nucleusImage = getCachedImage( numProtons + numNeutrons );
+        if (nucleusImage != null){
+            return new PImage(nucleusImage);
+        }
+        
+        // Start creating a new image.
         PNode nucleus = new PNode();
         
         // Decide on the proportion of free protons and neutrons versus those
@@ -72,22 +123,27 @@ public class NucleusImageFactory {
         for ( int i = 0; i < maxParticleType; i++ ) {
             if (numAlphas == numFreeNeutrons){
                 // Add an alpha particle.
-                addAlpha(nucleus, nucleusRadius);
+                addAlpha(nucleus, nucleusRadius, pixelsPerFm);
                 numAlphas--;
             }
             if (numFreeProtons == numFreeNeutrons){
-                addProton(nucleus, nucleusRadius);
+                addProton(nucleus, nucleusRadius, pixelsPerFm);
                 numFreeProtons--;
             }
             
-            addNeutron(nucleus, nucleusRadius);
+            addNeutron(nucleus, nucleusRadius, pixelsPerFm);
             numFreeNeutrons--;
         }
         
+        // Cache the newly created image.
+        nucleusImage = nucleus.toImage();
+        addCachedImage( numNeutrons + numProtons, nucleusImage );
+        
+        // Return the image;
         return new PImage(nucleus.toImage());
     }
     
-    private static void addAlpha(PNode nucleus, double radius){
+    private void addAlpha(PNode nucleus, double radius, double pixelsPerFm){
 
         // Randomly select from one of the available images.
         PImage alphaImage;
@@ -97,59 +153,115 @@ public class NucleusImageFactory {
         else{
             alphaImage = NuclearPhysics2Resources.getImageNode("Alpha Particle 002.png");            
         }
-        double adjustedRadius = alphaImage.getWidth() * NUCLEON_DIAMETER;
         
-        // Add the image to the node and position it.
-        setParticlePosition(adjustedRadius, alphaImage);
+        // Scale and position the image.
+        alphaImage.scale( pixelsPerFm * ALPHA_PARTICLE_DIAMTER / ((alphaImage.getWidth() + alphaImage.getHeight()) / 2) );
+        setParticlePosition(radius, alphaImage, pixelsPerFm);
+        
+        // Add the image to the main node.
         nucleus.addChild( alphaImage );
     }
     
-    private static void addNeutron(PNode nucleus, double radius){
+    private void addNeutron(PNode nucleus, double radius, double pixelsPerFm){
         
         // Load the image.
         PImage neutronImage = NuclearPhysics2Resources.getImageNode("Neutron.png");
-        double adjustedRadius = neutronImage.getWidth() * NUCLEON_DIAMETER;
         
-        // Add the image to the node and position it.
-        setParticlePosition(adjustedRadius, neutronImage);
+        // Scale and position the image.
+        neutronImage.scale( pixelsPerFm * NUCLEON_DIAMETER / neutronImage.getWidth());
+        setParticlePosition(radius, neutronImage, pixelsPerFm);
+        
+        // Add the image to the main node.
         nucleus.addChild( neutronImage );
     }   
 
-    private static void addProton(PNode nucleus, double radius){
+    private void addProton(PNode nucleus, double radius, double pixelsPerFm){
         
         // Load the image.
         PImage protonImage = NuclearPhysics2Resources.getImageNode("Proton.png");
-        double adjustedRadius = protonImage.getWidth() * NUCLEON_DIAMETER;
         
-        // Add the image to the node and position it.
+        // Scale and position the image.
+        protonImage.scale( pixelsPerFm * NUCLEON_DIAMETER / protonImage.getWidth());
+        setParticlePosition(radius, protonImage, pixelsPerFm);
+        
+        // Add the image to the main node.
         nucleus.addChild( protonImage );
-        setParticlePosition(adjustedRadius, protonImage);
     }   
     
-    private static void setParticlePosition(double nucleusRadius, PNode particle){
+    private void setParticlePosition(double nucleusRadius, PNode particle, double pixelsPerFm){
         double angle = (_rand.nextDouble() * Math.PI * 2);
         double multiplier = _rand.nextDouble();
         if (multiplier > 0.8){
             // Cause the distribution to tail off in the outer regions
             // of the nucleus.  This makes the center of the nucleus
             // look more concentrated, which is what we want.
-            multiplier = multiplier * _rand.nextDouble();
+            multiplier = multiplier * _rand.nextDouble() / 2;
         }
-        double radius = nucleusRadius * multiplier;
+        double radius = nucleusRadius * multiplier * pixelsPerFm;
         double xPos = Math.sin( angle ) * radius;
         double yPos = Math.cos( angle ) * radius;
         particle.setOffset( xPos, yPos );
     }
     
-    private static double getDiameter(int atomicWeight){
+    private double getDiameter(int atomicWeight){
         // This calculation is based on an empirically derived function that
         // seems to give pretty reasonable values.
         return (1.6 * Math.pow( (double)atomicWeight, 0.362));
     }
+    
+    /**
+     * Search for a cached image and return one if there are enough already
+     * generated.
+     * 
+     * @param atomicWeight
+     * @return - Image if the cache is full, null if not.
+     */
+    private Image getCachedImage(int atomicWeight){
+        
+        if (atomicWeight > MAX_ATOMIC_WEIGHT){
+            System.err.println("Warning: Requested image larger than max atomic weight allowed.");
+            return null;
+        }
+        
+        ArrayList imageList = imageMap[atomicWeight];
+        
+        if (imageList == null){
+            // No cached images exist for this atomic weight.
+            return null;
+        }
+        
+        if (imageList.size() < IMAGES_TO_CACHE){
+            // Not enough cached images exist for this atomic weight.
+            return null;
+        }
+        
+        // We have the needed number of cached images, so return one.
+        return (Image)imageList.get( _rand.nextInt( IMAGES_TO_CACHE ) );
+    }
+    
+    private void addCachedImage(int atomicWeight, Image newImage){
 
+        if (atomicWeight > MAX_ATOMIC_WEIGHT){
+            System.err.println("Warning: New image has atomic weight above allowed maximum.");
+            return;
+        }
+        
+        ArrayList imageList = imageMap[atomicWeight];
+        
+        if (imageList == null){
+            // This is the first image for this particular atomic weight, so
+            // we need to create an ArrayList to hold the images.
+            imageList = new ArrayList(IMAGES_TO_CACHE);
+            imageMap[atomicWeight] = imageList;
+        }
+        
+        // Add the image to the list.
+        imageList.add( newImage );
+    }
+    
     public static void main( String[] args ) {
         
-        PImage testImage = generateNucleusImage(120, 100);
+        PImage testImage = getInstance().generateNucleusImage(120, 100, 20);
         
         JFrame frame = new JFrame();
         PhetPCanvas canvas = new PhetPCanvas();
