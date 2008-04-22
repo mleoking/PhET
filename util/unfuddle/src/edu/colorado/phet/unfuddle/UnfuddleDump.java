b3413ff88185ba75b83ced1c59b51d8741cf8370
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-04-22 22:30:42 +0000

    doc UnfuddleDump, create constants file

--- util/unfuddle/src/edu/colorado/phet/unfuddle/UnfuddleDump.java
+++ util/unfuddle/src/edu/colorado/phet/unfuddle/UnfuddleDump.java
@@ -3,12 +3,22 @@ package edu.colorado.phet.unfuddle;
 import java.io.IOException;
 
 /**
+ * This doesn't work.
+ * <p>
+ * Anytime we have a new person join, a change of email address, a new component added,
+ * and possibly other things, you must manually get a new dump from the Unfuddle website.
+ * Go to the PhET project's Setting page, and select "Download an XML representation 
+ * of this project only" from the right column.
+ * 
  * Created by: Sam
  * Feb 19, 2008 at 8:37:29 AM
  */
 public class UnfuddleDump {
     public static void main( String[] args ) throws IOException {
-        UnfuddleCurl curl = new UnfuddleCurl( args[0], args[1], 9404 );
+        if ( args.length != 2 ) {
+            System.out.println( "usage: UnfuddleDump username password" );
+        }
+        UnfuddleCurl curl = new UnfuddleCurl( args[0], args[1], UnfuddleNotifierConstants.PHET_ACCOUNT_ID );
 //        String dump = curl.readString( "tickets" );
         String dump = curl.readString( "dump" );
         System.out.println( "dump.length = " + dump.length() );
