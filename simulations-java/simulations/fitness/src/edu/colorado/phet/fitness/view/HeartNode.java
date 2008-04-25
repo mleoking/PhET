eda4b0156c397ffa92b1f51523d73cfad67687df
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-04-25 21:05:36 +0000

    Added better behavior for heart/health indicator, based on BMI

--- simulations-java/simulations/fitness/src/edu/colorado/phet/fitness/view/HeartNode.java
+++ simulations-java/simulations/fitness/src/edu/colorado/phet/fitness/view/HeartNode.java
@@ -4,6 +4,8 @@ import java.awt.*;
 
 import edu.colorado.phet.common.phetcommon.view.util.BufferedImageUtils;
 import edu.colorado.phet.common.phetcommon.view.util.DoubleGeneralPath;
+import edu.colorado.phet.common.phetcommon.math.Function;
+import edu.colorado.phet.common.phetcommon.math.MathUtil;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.colorado.phet.fitness.FitnessResources;
 import edu.colorado.phet.fitness.model.Human;
@@ -38,7 +40,7 @@ public class HeartNode extends PImage {
         smilePath = new PhetPPath( new BasicStroke( 14f ), Color.black );
         addChild( smilePath );
         human.addListener( new Human.Adapter() {
-            public void fatPercentChanged() {
+            public void bmiChanged() {
                 updateSmile();
             }
         } );
@@ -47,11 +49,15 @@ public class HeartNode extends PImage {
     }
 
     private void updateSmile() {
+        double optimalBMI = 21.5;//median of WHO values according to: http://en.wikipedia.org/wiki/Body_mass_index
+        double distFromOptimal = Math.abs( human.getBMI() - optimalBMI );
+        Function.LinearFunction linearFunction=new Function.LinearFunction( 0,10,1,0);
+
         DoubleGeneralPath smile = new DoubleGeneralPath();
         double smileInsetScaleX = 0.3;
         double smileYFrac = 0.6;
 //        double happiness = ( human.getLeanMuscleMass() / 100.0 - 0.5 ) * 2;//between 0 and 1
-        double happiness = human.getHeartHealth();
+        double happiness = MathUtil.clamp( -0.5,linearFunction.evaluate( distFromOptimal ),1);
         double controlPointDY = 50 * happiness;
         smile.moveTo( heart.getFullBounds().getWidth() * smileInsetScaleX, heart.getFullBounds().getHeight() * smileYFrac );
         smile.curveTo( heart.getFullBounds().getWidth() * 0.4, heart.getFullBounds().getHeight() * smileYFrac + controlPointDY,
