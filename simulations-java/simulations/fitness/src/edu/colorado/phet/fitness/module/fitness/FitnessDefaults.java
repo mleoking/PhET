534f102250f9c1c2880baa37bb9412e774a7e421
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-04-29 14:23:54 +0000

    Fixed: Sim should not be running on startup

--- simulations-java/simulations/fitness/src/edu/colorado/phet/fitness/module/fitness/FitnessDefaults.java
+++ simulations-java/simulations/fitness/src/edu/colorado/phet/fitness/module/fitness/FitnessDefaults.java
@@ -5,7 +5,8 @@ package edu.colorado.phet.fitness.module.fitness;
  * Apr 18, 2008 at 12:41:19 AM
  */
 public class FitnessDefaults {
-    public static final double CLOCK_DT = 1000.0 * 250 ;
+    public static final double CLOCK_DT = 1000.0 * 250;
     //    public static final int CLOCK_DELAY = 30;
     public static final int CLOCK_DELAY = 30 * 3;
+    public static final boolean STARTS_PAUSED = true;
 }
