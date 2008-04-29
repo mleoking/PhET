c3a8540edbf929da5fa3f27ae2a97a243687bd4c
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-04-29 13:18:48 +0000

    Renamed "activity" to "lifestyle" in the stacked bar chart Removed confusing "professional athlete" option

--- trunk/simulations-java/simulations/fitness/src/edu/colorado/phet/fitness/control/ExerciseSelectionPanel.java
+++ trunk/simulations-java/simulations/fitness/src/edu/colorado/phet/fitness/control/ExerciseSelectionPanel.java
@@ -14,31 +14,44 @@ import edu.colorado.phet.fitness.model.Human;
  * Created by: Sam
  * Apr 24, 2008 at 2:39:39 AM
  */
-public class ExerciseSelectionPanel extends CalorieSelectionPanel {
+public class ExerciseSelectionPanel extends JPanel implements ICalorieSelectionPanel {
     private Human human;
+    private CalorieSelectionPanel calorieSelectionPanel;
 
     public ExerciseSelectionPanel( final Human human, final CalorieSet available, final CalorieSet selected, String availableTitle, String selectedTitle ) {
-        super( available, selected, availableTitle, selectedTitle );
+        setLayout( new GridBagLayout() );
+        calorieSelectionPanel = new CalorieSelectionPanel( available, selected, availableTitle, selectedTitle );
         this.human = human;
 
-//        JPanel activityLevels = new MyVerticalLayoutPanel();
-        JPanel activityLevels = new JPanel();
-        activityLevels.setBorder( CalorieSelectionPanel.createTitledBorder( "Lifestyle" ) );
-        ButtonGroup bg = new ButtonGroup();
-        for ( int i = 0; i < Activity.DEFAULT_ACTIVITY_LEVELS.length; i++ ) {
-            JRadioButton jRadioButton = new JRadioButton( Activity.DEFAULT_ACTIVITY_LEVELS[i].toString(),
-                                                          Activity.DEFAULT_ACTIVITY_LEVELS[i].getValue() == human.getActivityLevel() );
-            final int i1 = i;
-            jRadioButton.addActionListener( new ActionListener() {
-                public void actionPerformed( ActionEvent e ) {
-                    human.setActivityLevel( Activity.DEFAULT_ACTIVITY_LEVELS[i1].getValue() );
-                }
-            } );
-            jRadioButton.setFont( new PhetDefaultFont( 13, true ) );
-            bg.add( jRadioButton );
-            activityLevels.add( jRadioButton );
-        }
+        JPanel activityLevels = new ActivityLevelsPanel();
         //todo: fix layout with respect to parent, breaks encapsulation
         add( activityLevels, new GridBagConstraints( 0, 0, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets( 1, 1, 1, 1 ), 0, 0 ), 0 );
+        add( calorieSelectionPanel, new GridBagConstraints( 0, 1, 1, 1, 1, 1E6, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets( 1, 1, 1, 1 ), 0, 0 ) );
+//        add(activityLevels);
+//        add(calorieSelectionPanel);
+    }
+
+    public void addListener( CalorieSelectionPanel.Listener listener ) {
+        calorieSelectionPanel.addListener( listener );
+    }
+
+    private class ActivityLevelsPanel extends JPanel {
+        private ActivityLevelsPanel() {
+            setBorder( CalorieSelectionPanel.createTitledBorder( "Lifestyle" ) );
+            ButtonGroup bg = new ButtonGroup();
+            for ( int i = 0; i < Activity.DEFAULT_ACTIVITY_LEVELS.length; i++ ) {
+                JRadioButton jRadioButton = new JRadioButton( Activity.DEFAULT_ACTIVITY_LEVELS[i].toString(),
+                                                              Activity.DEFAULT_ACTIVITY_LEVELS[i].getValue() == human.getActivityLevel() );
+                final int i1 = i;
+                jRadioButton.addActionListener( new ActionListener() {
+                    public void actionPerformed( ActionEvent e ) {
+                        human.setActivityLevel( Activity.DEFAULT_ACTIVITY_LEVELS[i1].getValue() );
+                    }
+                } );
+                jRadioButton.setFont( new PhetDefaultFont( 13, true ) );
+                bg.add( jRadioButton );
+                add( jRadioButton );
+            }
+        }
     }
 }
