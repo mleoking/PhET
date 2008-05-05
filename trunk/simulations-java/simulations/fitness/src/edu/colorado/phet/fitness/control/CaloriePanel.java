ebac2dbe9af79eea54f12fd5295a1f62bd1848b2
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-05-05 06:49:58 +0000

    start improving graphs

--- trunk/simulations-java/simulations/fitness/src/edu/colorado/phet/fitness/control/CaloriePanel.java
+++ trunk/simulations-java/simulations/fitness/src/edu/colorado/phet/fitness/control/CaloriePanel.java
@@ -9,6 +9,7 @@ import edu.colorado.phet.common.motion.graphs.ControlGraphSeries;
 import edu.colorado.phet.common.motion.graphs.GraphSuiteSet;
 import edu.colorado.phet.common.motion.graphs.MinimizableControlGraph;
 import edu.colorado.phet.common.motion.model.DefaultTemporalVariable;
+import edu.colorado.phet.common.motion.model.ITemporalVariable;
 import edu.colorado.phet.common.motion.model.IVariable;
 import edu.colorado.phet.common.motion.model.MotionTimeSeriesModel;
 import edu.colorado.phet.common.phetcommon.math.Function;
@@ -38,20 +39,46 @@ public class CaloriePanel extends PNode {
     private CalorieNode foodNode;
     private CalorieNode exerciseNode;
 
+    private DefaultTemporalVariable massVar;
+    private DefaultTemporalVariable calIntakeVar = new DefaultTemporalVariable();
+    private DefaultTemporalVariable calBurnVar = new DefaultTemporalVariable();
+
     public CaloriePanel( final FitnessModel model, PhetPCanvas phetPCanvas, Frame parentFrame ) {
         this.phetPCanvas = phetPCanvas;
         GraphSuiteSet graphSuiteSet = new GraphSuiteSet();
+
+        //todo: remove the following bogus line
         TimeSeriesModel tsm = new MotionTimeSeriesModel( new TestTimeSeries.MyRecordableModel(), new ConstantDtClock( 30, 1 ) );
 
-        final ControlGraph weightGraph = new ControlGraph( phetPCanvas, new ControlGraphSeries( "Weight", Color.blue, "weight", "lbs", "", model.getHuman().getMassVariable() ), "Weight", 0, 100, tsm, 7.37E8 );
-        weightGraph.setHorizontalRange( Human.DEFAULT_VALUE.getAgeSeconds(), Human.DEFAULT_VALUE.getAgeSeconds() + FitnessUnits.yearsToSeconds( 20 ) );
+        massVar = new DefaultTemporalVariable();
+        model.addListener( new FitnessModel.Adapter() {
+            public void simulationTimeChanged() {
+                super.simulationTimeChanged();
+                massVar.addValue( model.getUnits().modelToViewMass( model.getHuman().getMass() ), FitnessUnits.secondsToYears( model.getHuman().getAge() ) );
+            }
+        } );
+        massVar.setValue( model.getUnits().modelToViewMass( model.getHuman().getMass() ) );
+        model.addListener( new FitnessModel.Adapter() {
+            public void unitsChanged() {
+                massVar.clear();
+                ITemporalVariable itv = model.getHuman().getMassVariable();
+                for ( int i = 0; i < itv.getSampleCount(); i++ ) {
+                    massVar.addValue( model.getUnits().modelToViewMass( itv.getData( i ).getValue() ), itv.getData( i ).getTime() );
+                }
+            }
+        } );
+
+        final ControlGraph weightGraph = new ControlGraph( phetPCanvas, new ControlGraphSeries( "Weight", Color.blue, "weight", "lbs", "", massVar ), "Weight", 0, 250, tsm, 7.37E8 );
+        weightGraph.setHorizontalRange( FitnessUnits.secondsToYears( Human.DEFAULT_VALUE.getAgeSeconds() ),
+                                        FitnessUnits.secondsToYears( Human.DEFAULT_VALUE.getAgeSeconds() + FitnessUnits.yearsToSeconds( 10 ) ) );
         weightGraph.setEditable( false );
         weightChart = new MinimizableControlGraph( "Weight", weightGraph );
         weightChart.setAvailableBounds( 600, 125 );
 
         final ControlGraph calorieGraph = new ControlGraph( phetPCanvas, new ControlGraphSeries( "Calories", Color.green, "Cal", "Cal", new BasicStroke( 4, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER ), "", model.getHuman().getCaloricIntakeVariable() ), "Calories", 0, 4000, tsm );
         calorieGraph.addSeries( new ControlGraphSeries( "Exercise Calories", Color.red, "cal", FitnessStrings.KCAL, new BasicStroke( 2, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER ), "", model.getHuman().getCaloricBurnVariable() ) );
-        calorieGraph.setHorizontalRange( Human.DEFAULT_VALUE.getAgeSeconds(), Human.DEFAULT_VALUE.getAgeSeconds() + FitnessUnits.yearsToSeconds( 20 ) );
+        calorieGraph.setHorizontalRange( FitnessUnits.secondsToYears( Human.DEFAULT_VALUE.getAgeSeconds() ),
+                                         FitnessUnits.secondsToYears( Human.DEFAULT_VALUE.getAgeSeconds() + FitnessUnits.yearsToSeconds( 10 ) ) );
         calorieGraph.setEditable( false );
         model.addListener( new FitnessModel.Adapter() {
             public void simulationTimeChanged() {
