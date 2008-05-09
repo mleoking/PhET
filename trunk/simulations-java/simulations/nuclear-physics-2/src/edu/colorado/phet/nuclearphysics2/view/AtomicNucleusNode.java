1eab3d6472f3fc3d733cf4989658f89add91c938
jxb147 <jxb147@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-05-09 23:20:00 +0000

    Make the graphic representation of the explosion energy slightly larger and tweaked the temperature-to-color realtionship in the reactor.

--- trunk/simulations-java/simulations/nuclear-physics-2/src/edu/colorado/phet/nuclearphysics2/view/AtomicNucleusNode.java
+++ trunk/simulations-java/simulations/nuclear-physics-2/src/edu/colorado/phet/nuclearphysics2/view/AtomicNucleusNode.java
@@ -41,7 +41,7 @@ public class AtomicNucleusNode extends PNode {
     private static final double LABEL_SCALING_FACTOR = 0.30;
     
     // Constants that control the nature of the explosion graphic.
-    private static final int   EXPLOSION_COUNTER_RESET_VAL = 10;
+    private static final int   EXPLOSION_COUNTER_RESET_VAL = 15;
     private static final Color EXPLOSION_STROKE_COLOR = new Color(0xffff33);
     private static final Color EXPLOSION_FILL_COLOR = new Color(0xffff33);
     private static final float EXPLOSION_MIN_TRANSPARENCY = 0.4f;
@@ -304,7 +304,7 @@ public class AtomicNucleusNode extends PNode {
 
         if (_explosionCounter > 0){
             // Step the explosion graphic.
-            double explosionRadius = (double)(EXPLOSION_COUNTER_RESET_VAL - _explosionCounter + 1) * 3;
+            double explosionRadius = (double)(EXPLOSION_COUNTER_RESET_VAL - _explosionCounter + 1) * 3.5;
             _explosionShape.setFrameFromCenter( 0, 0, explosionRadius, explosionRadius );
             _explosion.setPathTo( _explosionShape );
             _explosion.setTransparency( 
