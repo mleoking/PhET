97ab2e807f4375fb6d0fceb2b10e7a38437d3f78
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-05-12 22:45:05 +0000

    5/12/08 - first pass at resizable ice thickness tool, a little clunky looking

--- simulations-java/simulations/glaciers/src/edu/colorado/phet/glaciers/view/tools/IceThicknessToolNode.java
+++ simulations-java/simulations/glaciers/src/edu/colorado/phet/glaciers/view/tools/IceThicknessToolNode.java
@@ -2,8 +2,12 @@
 
 package edu.colorado.phet.glaciers.view.tools;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.Stroke;
+import java.awt.geom.GeneralPath;
+import java.awt.geom.RoundRectangle2D;
 import java.text.NumberFormat;
 
 import javax.swing.BorderFactory;
@@ -13,7 +17,6 @@ import javax.swing.border.Border;
 
 import edu.colorado.phet.common.phetcommon.util.DefaultDecimalFormat;
 import edu.colorado.phet.common.phetcommon.view.util.PhetDefaultFont;
-import edu.colorado.phet.glaciers.GlaciersImages;
 import edu.colorado.phet.glaciers.GlaciersStrings;
 import edu.colorado.phet.glaciers.model.IceThicknessTool;
 import edu.colorado.phet.glaciers.model.AbstractTool.ToolListener;
@@ -21,9 +24,10 @@ import edu.colorado.phet.glaciers.model.IceThicknessTool.IceThicknessToolListene
 import edu.colorado.phet.glaciers.model.Movable.MovableAdapter;
 import edu.colorado.phet.glaciers.model.Movable.MovableListener;
 import edu.colorado.phet.glaciers.view.ModelViewTransform;
-import edu.colorado.phet.glaciers.view.tools.AbstractToolOriginNode.DownToolOriginNode;
 import edu.umd.cs.piccolo.PNode;
-import edu.umd.cs.piccolo.nodes.PImage;
+import edu.umd.cs.piccolo.nodes.PPath;
+import edu.umd.cs.piccolo.util.PDimension;
+import edu.umd.cs.piccolox.nodes.PComposite;
 import edu.umd.cs.piccolox.pswing.PSwing;
 
 /**
@@ -40,6 +44,8 @@ public class IceThicknessToolNode extends AbstractToolNode {
     private static final Font FONT = new PhetDefaultFont( 10 );
     private static final Border BORDER = BorderFactory.createLineBorder( Color.BLACK, 1 );
     private static final NumberFormat ICE_THICKNESS_FORMAT = new DefaultDecimalFormat( "0.0" );
+    private static final PDimension HANDLE_SIZE = new PDimension( 5, 20 );
+    private static final PDimension CALIPERS_CLOSED_SIZE = new PDimension( 25, 20 );
     
     //----------------------------------------------------------------------------
     // Class data
@@ -50,6 +56,7 @@ public class IceThicknessToolNode extends AbstractToolNode {
     private MovableListener _movableListener;
     private ToolListener _toolListener;
     private JLabel _iceThicknessDisplay;
+    private CalipersNode _calipersNode;
 
     //----------------------------------------------------------------------------
     // Constructors
@@ -80,22 +87,22 @@ public class IceThicknessToolNode extends AbstractToolNode {
         };
         _iceThicknessTool.addToolListener( _toolListener );
         
-        PNode arrowNode = new DownToolOriginNode();
-        addChild( arrowNode );
-        arrowNode.setOffset( 0, 0 ); // this node identifies the origin
+        _calipersNode = new CalipersNode( CALIPERS_CLOSED_SIZE );
+        addChild( _calipersNode );
+        _calipersNode.setOffset( 0, 0 );
         
-        PImage imageNode = new PImage( GlaciersImages.ICE_THICKNESS_TOOL );
-        addChild( imageNode );
-        imageNode.setOffset( -imageNode.getFullBoundsReference().getWidth() + 3, arrowNode.getFullBoundsReference().getMinY() - imageNode.getFullBoundsReference().getHeight() );
+        PNode handleNode = new HandleNode( HANDLE_SIZE );
+        addChild( handleNode );
+        handleNode.setOffset( -handleNode.getFullBoundsReference().getWidth(), _calipersNode.getFullBoundsReference().getMaxY() );
         
-        _iceThicknessDisplay = new JLabel();
+        _iceThicknessDisplay = new JLabel( "?" );
         _iceThicknessDisplay.setFont( FONT );
         JPanel panel = new JPanel();
         panel.setBorder( BORDER );
         panel.add( _iceThicknessDisplay );
         PSwing panelNode = new PSwing( panel );
         addChild( panelNode );
-        panelNode.setOffset( imageNode.getFullBoundsReference().getMaxX() + 2, imageNode.getFullBoundsReference().getMinY() );
+        panelNode.setOffset( 2, -panelNode.getFullBoundsReference().getHeight() + _calipersNode.getJawsHeight() );
         
         // initial state
         update();
@@ -109,6 +116,101 @@ public class IceThicknessToolNode extends AbstractToolNode {
     }
     
     //----------------------------------------------------------------------------
+    // Geometry
+    //----------------------------------------------------------------------------
+    
+    /**
+     * Handle on the tool.
+     */
+    private static class HandleNode extends PComposite {
+        
+        private static final Stroke STROKE = new BasicStroke( 1f );
+        private static final Color STROKE_COLOR = Color.BLACK;
+        private static final Color FILL_COLOR = new Color( 85, 14, 14 ); // reddish brown
+        
+        public HandleNode( PDimension size ) {
+            super();
+            
+            final double w = size.getWidth();
+            final double h = size.getHeight();
+            
+            PPath topPartNode = new PPath( new RoundRectangle2D.Double( 0, 0, w, 0.25 * h, w / 2, w / 2 ) );
+            topPartNode.setStroke( STROKE );
+            topPartNode.setStrokePaint( STROKE_COLOR );
+            topPartNode.setPaint( FILL_COLOR );
+            topPartNode.setOffset( 0, 0 );
+            
+            PPath bottomPartNode = new PPath( new RoundRectangle2D.Double( 0, 0, w, 0.75 * h, w / 2, w / 2 ) );
+            bottomPartNode.setStroke( STROKE );
+            bottomPartNode.setStrokePaint( STROKE_COLOR );
+            bottomPartNode.setPaint( FILL_COLOR );
+            bottomPartNode.setOffset( 0, topPartNode.getFullBoundsReference().getHeight() );
+            
+            addChild( topPartNode );
+            addChild( bottomPartNode );
+        }
+    }
+    
+    /**
+     * Resizable calipers.
+     */
+    private static class CalipersNode extends PComposite {
+        
+        private static final Stroke STROKE = new BasicStroke( 1f );
+        private static final Color STROKE_COLOR = Color.BLACK;
+        private static final Color FILL_COLOR = new Color( 140, 136, 120 ); // metal gray
+        private static final double FRAME_THICKNESS = 4;
+        
+        private PDimension _closedSize;
+        private GeneralPath _path;
+        private PPath _pathNode;
+
+        public CalipersNode( PDimension closedSize ) {
+            super();
+            _closedSize = new PDimension( closedSize );
+            _path = new GeneralPath();
+            _pathNode = new PPath();
+            _pathNode.setStroke( STROKE );
+            _pathNode.setStrokePaint( STROKE_COLOR );
+            _pathNode.setPaint( FILL_COLOR );
+            addChild( _pathNode );
+            open( 20 );
+        }
+        
+        public double getJawsHeight() {
+            return _closedSize.getHeight() / 2;
+        }
+
+        public void open( double height ) {
+            
+            final double w = _closedSize.getWidth();
+            final double h = _closedSize.getHeight() + height;
+            final double jawsHeight = getJawsHeight();
+            final double jawsWidth = jawsHeight / 5;
+
+            _path.reset();
+            _path.moveTo( 0f, 0f );
+            _path.lineTo( 0f, (float)jawsHeight );
+            _path.lineTo( (float)-w, (float)jawsHeight );
+            _path.lineTo( (float)-w, (float)( -h + jawsHeight ) );
+            _path.lineTo( 0f, (float)( -h + jawsHeight ) );
+            _path.lineTo( 0f, (float)( -h + jawsHeight + jawsHeight ) );
+            _path.lineTo( (float)-jawsWidth, (float)( -h + jawsHeight + jawsHeight ) );
+            _path.lineTo( (float)( -jawsWidth ), (float)( -h + jawsHeight + FRAME_THICKNESS ) );
+            _path.lineTo( (float)( -w + FRAME_THICKNESS ), (float)( -h + jawsHeight + FRAME_THICKNESS ) );
+            _path.lineTo( (float)( -w + FRAME_THICKNESS ), (float)( jawsHeight - FRAME_THICKNESS ) );
+            _path.lineTo( (float)-jawsWidth, (float)( jawsHeight - FRAME_THICKNESS ) );
+            _path.lineTo( (float)-jawsWidth, 0f );
+            _path.closePath();
+            _pathNode.setPathTo( _path );
+        }
+        
+        public void close() {
+            open( 0 );
+        }
+    }
+    
+    //----------------------------------------------------------------------------
     // Updaters
     //----------------------------------------------------------------------------
     
@@ -118,17 +220,16 @@ public class IceThicknessToolNode extends AbstractToolNode {
     private void update() {
         
         if ( _iceThicknessTool.isDragging() ) {
-            String text = "? " + GlaciersStrings.UNITS_ICE_THICKNESS;
+            String text = "? " + GlaciersStrings.UNITS_ICE_THICKNESS +  "    ";
             _iceThicknessDisplay.setText( text );
-            
-            //TODO: calipers should be in neutral state while dragging
+            _calipersNode.open( 20 );
         }
         else {
             double value = _iceThicknessTool.getThickness();
             String text = ICE_THICKNESS_FORMAT.format( value ) + " " + GlaciersStrings.UNITS_ICE_THICKNESS;
             _iceThicknessDisplay.setText( text );
-            
-            //TODO: open/close the calipers to match the ice thickness, align with ice
+            double viewDistance = Math.abs( getModelViewTransform().modelToView( 0, value ).getY() );
+            _calipersNode.open( viewDistance );
         }
 
     }
