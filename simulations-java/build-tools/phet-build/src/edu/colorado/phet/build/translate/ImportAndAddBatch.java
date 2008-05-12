51b52259d836a81b86450593d91262b4e9f1e85e
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-05-12 15:33:42 +0000

    added docs

--- simulations-java/build-tools/phet-build/src/edu/colorado/phet/build/translate/ImportAndAddBatch.java
+++ simulations-java/build-tools/phet-build/src/edu/colorado/phet/build/translate/ImportAndAddBatch.java
@@ -2,13 +2,15 @@ package edu.colorado.phet.build.translate;
 
 import java.io.File;
 
-import javax.swing.JOptionPane;
+import javax.swing.*;
 
 import edu.colorado.phet.build.translate.AddTranslation.AddTranslationReturnValue;
 
 /**
- * Created by: Sam
- * Feb 28, 2008 at 11:55:38 PM
+ * This is the main entry point for importing translations into subversion and deploying them to tigercat.
+ *
+ * @author Sam Reid
+ * @author Chris Malley
  */
 public class ImportAndAddBatch {
     private File baseDir;
@@ -23,23 +25,23 @@ public class ImportAndAddBatch {
             System.exit( 1 );
         }
         JOptionPane.showMessageDialog( null,
-                "<html>Put the localization files that you wish to deploy in a directory.<br>" +
-                "When you have finished this step, press OK to continue.<br>" +
-                "You will be prompted for the directory name.</html>" );
+                                       "<html>Put the localization files that you wish to deploy in a directory.<br>" +
+                                       "When you have finished this step, press OK to continue.<br>" +
+                                       "You will be prompted for the directory name.</html>" );
         String dirname = AddTranslation.prompt( "Enter the name of the directory where your localization files are:" );
         new ImportAndAddBatch( new File( args[0] ) ).importAndAddBatch( dirname );
     }
 
     private void importAndAddBatch( String dir ) throws Exception {
-        
+
         // import the translations into the IDE workspace
         new ImportTranslations( baseDir ).importTranslations( new File( dir ) );
-        JOptionPane.showMessageDialog( null, 
-                "<html>Localization files have been imported into your IDE workspace.<br>" +
-        		"Please refresh your workspace, examine the files,<br>" +
-        		"and manually commit them to the SVN repository.<br><br>" +
-        		"Press OK when you are ready to integrate the files into<br>" +
-        		"the PHET production server." );
+        JOptionPane.showMessageDialog( null,
+                                       "<html>Localization files have been imported into your IDE workspace.<br>" +
+                                       "Please refresh your workspace, examine the files,<br>" +
+                                       "and manually commit them to the SVN repository.<br><br>" +
+                                       "Press OK when you are ready to integrate the files into<br>" +
+                                       "the PHET production server." );
 
         // deploy the translations to the PhET productions server
         String username = AddTranslation.prompt( "Production Server username:" );
@@ -47,7 +49,7 @@ public class ImportAndAddBatch {
         AddTranslationBatch addTranslationBatch = new AddTranslationBatch( baseDir, new File( dir ), username, password );
         AddTranslationReturnValue[] returnValues = addTranslationBatch.runBatch();
         System.out.println( "Finished deploying" );
-        
+
         String results = "<html>Results:<br><br>";
         for ( int i = 0; i < returnValues.length; i++ ) {
             AddTranslationReturnValue returnValue = returnValues[i];
