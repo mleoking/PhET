8af72f9503fb09ddae1c1680f1ed6a10a261df51
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-05-13 07:17:53 +0000

    cleanup types

--- trunk/util/unfuddle/src/edu/colorado/phet/unfuddle/CompositeMessageHandler.java
+++ trunk/util/unfuddle/src/edu/colorado/phet/unfuddle/CompositeMessageHandler.java
@@ -4,22 +4,16 @@ import java.util.ArrayList;
 
 import javax.mail.MessagingException;
 
-/**
- * Created by: Sam
- * Feb 21, 2008 at 1:58:42 PM
- * test comment
- */
 public class CompositeMessageHandler implements IMessageHandler {
-    private ArrayList list = new ArrayList();
+    private ArrayList<IMessageHandler> list = new ArrayList<IMessageHandler>();
 
     public void addMessageHandler( IMessageHandler m ) {
         list.add( m );
     }
 
     public void handleMessage( IMessage m ) throws MessagingException {
-        for ( int i = 0; i < list.size(); i++ ) {
-            IMessageHandler messageHandler = (IMessageHandler) list.get( i );
-            messageHandler.handleMessage( m );
+        for ( IMessageHandler aList : list ) {
+            aList.handleMessage( m );
         }
     }
 }
