f0ec495bc9eeeb361251a210827e99de7438521e
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-05-14 04:27:49 +0000

    improved threading model

--- simulations-java/build-tools/phet-build/src/edu/colorado/phet/build/translate/TestSim.java
+++ simulations-java/build-tools/phet-build/src/edu/colorado/phet/build/translate/TestSim.java
@@ -20,8 +20,8 @@ import edu.colorado.phet.common.phetcommon.resources.DummyConstantStringTester;
  */
 public class TestSim {
     public static void main( String[] args ) throws IOException, ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException, InterruptedException, AWTException {
-        String project = args[0];
-        String sim = args[1];
+        final String project = args[0];
+        final String sim = args[1];
 
         PhetProject phetProject = new PhetProject( new File( "C:\\reid\\phet\\svn\\trunk\\simulations-java\\simulations" ), project );
         PhetProjectFlavor flavor = phetProject.getFlavor( sim );
@@ -33,19 +33,39 @@ public class TestSim {
         Method m = c.getMethod( "main", new Class[]{new String[0].getClass()} );
         m.invoke( null, new Object[]{new String[0]} );
 
-        Thread.sleep( 5000 );//todo: sleep until the main frame is available
-        Frame[] allFrames = Frame.getFrames();
-        Robot robot = new Robot();
-        for ( int i = 0; i < allFrames.length; i++ ) {
-            Frame allFrame = allFrames[i];
-            if ( allFrame.isShowing() ) {
-                System.out.println( "i=" + i + ", allFrame.getBounds() = " + allFrame.getBounds() );
-                BufferedImage im = robot.createScreenCapture( allFrame.getBounds() );
-                File output = new File( "C:/Users/Sam/Desktop/sim-out-test-1/" + project + "_" + sim + "_frame_" + i + ".png" );
-                output.getParentFile().mkdirs();
-                ImageIO.write( im, "PNG", output );
+        new Thread( new Runnable() {
+            public void run() {
+                try {
+                    Thread.sleep( 5000 );//todo: sleep until the main frame is available
+                }
+                catch( InterruptedException e ) {
+                    e.printStackTrace();
+                }
+                Frame[] allFrames = Frame.getFrames();
+                Robot robot;
+                try {
+                    robot = new Robot();
+                    for ( int i = 0; i < allFrames.length; i++ ) {
+                        Frame allFrame = allFrames[i];
+                        if ( allFrame.isShowing() ) {
+                            System.out.println( "i=" + i + ", allFrame.getBounds() = " + allFrame.getBounds() );
+                            BufferedImage im = robot.createScreenCapture( allFrame.getBounds() );
+                            File output = new File( "C:/Users/Sam/Desktop/sim-out-test-1/" + project + "_" + sim + "_frame_" + i + ".png" );
+                            output.getParentFile().mkdirs();
+                            ImageIO.write( im, "PNG", output );
+
+                        }
+                    }
+                }
+                catch( AWTException e ) {
+                    e.printStackTrace();
+                }
+                catch( IOException e ) {
+                    e.printStackTrace();
+                }
+                System.exit( 0 );
             }
-        }
-        System.exit( 0 );
+        } ).start();
+
     }
 }
