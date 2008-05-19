1af336c85155c47333df1e9887296f32cb716031
jxb147 <jxb147@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-05-19 21:20:08 +0000

    Added email notification.

--- trunk/util/phet-installer/new-automated/run-builder.sh
+++ trunk/util/phet-installer/new-automated/run-builder.sh
@@ -8,6 +8,18 @@
 # Make sure we're in the proper directory
 cd /web/htdocs/phet/installer-builder/
 
+# Subroutine for sending email notification of results.
+function send_email_notification {
+   EMAIL_ADDR="john.blanco@colorado.edu"
+   EMAIL_MSG="/tmp/phet_build_email_msg.txt"
+   LINES_TO_SEND=30
+   echo "Below are the final $LINES_TO_SEND lines excerpted from the log file:"> $EMAIL_MSG
+   echo "">> $EMAIL_MSG
+   tail -n $LINES_TO_SEND ./installer-builder-log.txt >> $EMAIL_MSG
+   EMAIL_SUBJECT="Result of nightly installer build: $1"
+   /bin/mail -s "$EMAIL_SUBJECT" "$EMAIL_ADDR" < $EMAIL_MSG
+}
+
 echo "================================================================" | tee --append installer-builder-log.txt
 echo " Installer opreation performed on: " | tee --append installer-builder-log.txt
 date  | tee --append installer-builder-log.txt
@@ -19,6 +31,7 @@ svn update
 
 if [ "$?" -ne "0" ]; then
   echo "Error performing SVN update" | tee --append installer-builder-log.txt
+  send_email_notification FAILURE
   exit 1
 fi
 
@@ -28,6 +41,7 @@ echo "Building all installers" | tee --append installer-builder-log.txt
 
 if [ "$?" -ne "0" ]; then
   echo "Error building installer" | tee --append installer-builder-log.txt
+  send_email_notification FAILURE
   exit 1
 fi
 
@@ -37,6 +51,7 @@ rm -rf ./temp/website/* | tee --append installer-builder-log.txt
 
 if [ "$?" -ne "0" ]; then
   echo "Error removing temporary files" | tee --append installer-builder-log.txt
+  send_email_notification FAILURE
   exit 1
 fi
 
@@ -46,5 +61,8 @@ cp ./temp/installer-output/*.* ../phet-dist/ | tee --append installer-builder-lo
 
 if [ "$?" -ne "0" ]; then
   echo "Error copying new installers to distribution directory" | tee --append installer-builder-log.txt
+  send_email_notification FAILURE
   exit 1
 fi
+
+send_email_notification SUCCESS
