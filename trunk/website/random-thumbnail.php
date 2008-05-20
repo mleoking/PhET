fdd035fbb59ea693b414ffea229ed6d159638a1f
flydanofly <flydanofly@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-05-20 18:16:38 +0000

    website: Whitespace correction, 2 statements on the same line

--- trunk/website/random-thumbnail.php
+++ trunk/website/random-thumbnail.php
@@ -3,7 +3,8 @@
     $original_dir = getcwd();
     chdir("./admin");
 
-    if (!defined("SITE_ROOT")) define("SITE_ROOT", "../"); include_once(SITE_ROOT."admin/global.php");
+    if (!defined("SITE_ROOT")) define("SITE_ROOT", "../");
+    include_once(SITE_ROOT."admin/global.php");
     include_once(SITE_ROOT."admin/sim-utils.php");
     include_once(SITE_ROOT."admin/sys-utils.php");
     include_once(SITE_ROOT."admin/web-utils.php");
