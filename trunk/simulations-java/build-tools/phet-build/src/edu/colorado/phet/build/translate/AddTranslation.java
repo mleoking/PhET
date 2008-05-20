46653d13d5b4ffa240e8a41b048220c54a99422c
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-05-20 18:44:39 +0000

    reverted to old cache-clear location; this change will be made around June 1

--- trunk/simulations-java/build-tools/phet-build/src/edu/colorado/phet/build/translate/AddTranslation.java
+++ trunk/simulations-java/build-tools/phet-build/src/edu/colorado/phet/build/translate/AddTranslation.java
@@ -120,7 +120,8 @@ public class AddTranslation {
                     //Line #118
                     //the web file referenced should be "cache-clear.php?cache=all" instead of "cache-clear-all.php"
 
-                    FileUtils.download( "http://phet.colorado.edu/new/admin/cache-clear.php?cache=all", new File( getTempProjectDir( phetProject ), "cache-clear-all.php" ) );
+//                    FileUtils.download( "http://phet.colorado.edu/new/admin/cache-clear.php?cache=all", new File( getTempProjectDir( phetProject ), "cache-clear-all.php" ) );
+                    FileUtils.download( "http://phet.colorado.edu/new/admin/cache-clear-all.php", new File( getTempProjectDir( phetProject ), "cache-clear-all.php" ) );
                     System.out.println( "Deployed: " + phetProject.getName() + " in language " + language + ", please test it to make sure it works correctly." );
                     System.out.println( "Finished deploy" );
                 }
