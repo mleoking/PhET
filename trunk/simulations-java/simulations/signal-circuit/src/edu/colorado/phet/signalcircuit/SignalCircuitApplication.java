98e5ba37409e05ac80991f736cc03a701205342b
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-05-20 16:12:29 +0000

    converted to use phetlookandfeel

--- trunk/simulations-java/simulations/signal-circuit/src/edu/colorado/phet/signalcircuit/SignalCircuitApplication.java
+++ trunk/simulations-java/simulations/signal-circuit/src/edu/colorado/phet/signalcircuit/SignalCircuitApplication.java
@@ -6,19 +6,20 @@
 
 package edu.colorado.phet.signalcircuit;
 
-import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
-import edu.colorado.phet.common.phetcommon.view.util.SimStrings;
-import edu.colorado.phet.phys2d.laws.Validate;
-
-import javax.swing.*;
 import java.awt.*;
 import java.util.Locale;
+
+import javax.swing.*;
+
+import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
+import edu.colorado.phet.common.phetcommon.view.util.SimStrings;
+import edu.colorado.phet.common.phetcommon.view.PhetLookAndFeel;
+import edu.colorado.phet.common.phetcommon.resources.DummyConstantStringTester;
+import edu.colorado.phet.phys2d.laws.Validate;
 
 //import edu.colorado.phet.util.ExitOnClose;
 
 public class SignalCircuitApplication extends JApplet {
-    public static boolean applet = true;
-
     // Localization
     public static final String localizedStringsPath = "signal-circuit/localization/signal-circuit-strings";
     private static final String VERSION = PhetApplicationConfig.getVersion( "signal-circuit" ).formatForTitleBar();
@@ -27,15 +28,7 @@ public class SignalCircuitApplication extends JApplet {
         int width = 600;
         int height = 300;
 
-        if( applet ) {
-            String applicationLocale = Toolkit.getDefaultToolkit().getProperty( "javaws.phet.locale", null );
-            if( applicationLocale != null && !applicationLocale.equals( "" ) ) {
-                Locale.setDefault( new Locale( applicationLocale ) );
-            }
-            SimStrings.getInstance().addStrings( localizedStringsPath );
-        }
-
-        Signal s = new Signal( width, height, applet );
+        Signal s = new Signal( width, height, false );
         s.getSystem().addLaw( new Validate( this ) );
 
         getContentPane().setLayout( new BorderLayout() );
@@ -48,19 +41,15 @@ public class SignalCircuitApplication extends JApplet {
     public static void main( final String[] args ) {
         SwingUtilities.invokeLater( new Runnable() {
             public void run() {
-                runMain( args );
+                new PhetLookAndFeel().initLookAndFeel();
+                SimStrings.getInstance().init( args, localizedStringsPath );
+                JFrame f = new JFrame( SimStrings.getInstance().getString( "SignalCircuitApplication.title" ) + " (" + VERSION + ")" );
+                f.setContentPane( new SignalCircuitApplication() );
+                f.setSize( new Dimension( 850, 435 ) );
+                f.setVisible( true );
+                f.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
             }
         } );
     }
 
-    private static void runMain( String[] args ) {
-        SimStrings.getInstance().init( args, localizedStringsPath );
-
-        applet = false;
-        JFrame f = new JFrame( SimStrings.getInstance().getString( "SignalCircuitApplication.title" ) + " (" + VERSION + ")" );
-        f.setContentPane( new SignalCircuitApplication() );
-        f.setSize( new Dimension( 850, 435 ) );
-        f.setVisible( true );
-        f.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-    }
 }
