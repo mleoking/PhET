3163b59fd516eb541fa30f879de294161f4a2c21
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-05-20 00:34:04 +0000

    removed phet logo from distance ladder, moved main to new thread

--- trunk/simulations-java/simulations/distance-ladder/phetcommon/src/edu/colorado/phet/distanceladder/common/view/BasicPhetPanel.java
+++ trunk/simulations-java/simulations/distance-ladder/phetcommon/src/edu/colorado/phet/distanceladder/common/view/BasicPhetPanel.java
@@ -18,18 +18,8 @@ public class BasicPhetPanel extends JPanel {
     private JComponent east;
     private JComponent north;
     private JComponent south;
-    private static Image phetLogo;
     private JDialog buttonDlg;
     private boolean fullScreen = false;
-
-    static {
-        try {
-            phetLogo = new ImageLoader().loadImage( "images/Phet-logo-48x48.gif" );
-        }
-        catch( IOException e ) {
-            e.printStackTrace();  //To change body of catch statement use Options | File Templates.
-        }
-    }
 
     public BasicPhetPanel( JComponent apparatusPanelContainer, JComponent controlPanel, JComponent monitorPanel, JComponent appControl ) {
         this.setLayout( new BorderLayout() );
@@ -119,18 +109,7 @@ public class BasicPhetPanel extends JPanel {
             buttonDlg = new JDialog();
             buttonDlg.setTitle( "Return" );
             buttonDlg.setDefaultCloseOperation( JDialog.DO_NOTHING_ON_CLOSE );
-            ImageIcon logo = new ImageIcon( phetLogo );
-            JButton logoButton = new JButton( logo );
-            logoButton.setPreferredSize( new Dimension( logo.getIconWidth() + 12, logo.getIconHeight() + 12 ) );
-            logoButton.addActionListener( new ActionListener() {
-                public void actionPerformed( ActionEvent e ) {
-                    setFullScreen( false );
-                    buttonDlg.setVisible( false );
-                }
-            } );
-            logoButton.setToolTipText( "Disable Full Frame" );
             buttonDlg.getContentPane().setLayout( new FlowLayout( FlowLayout.CENTER ) );
-            buttonDlg.getContentPane().add( logoButton );
             Rectangle thisBounds = this.getBounds();
             buttonDlg.pack();
             buttonDlg.setLocation( (int)( this.getLocationOnScreen().getX() + thisBounds.getMaxX() - buttonDlg.getWidth() ),
