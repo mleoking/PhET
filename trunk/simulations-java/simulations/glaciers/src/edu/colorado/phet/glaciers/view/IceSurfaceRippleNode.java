3fbf806f3493628d34a25533221adc9c4878292e
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-05-23 22:50:12 +0000

    5/23/08 - improve "look" of ripples, patterned after ripples in design doc

--- trunk/simulations-java/simulations/glaciers/src/edu/colorado/phet/glaciers/view/IceSurfaceRippleNode.java
+++ trunk/simulations-java/simulations/glaciers/src/edu/colorado/phet/glaciers/view/IceSurfaceRippleNode.java
@@ -6,7 +6,7 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Shape;
 import java.awt.Stroke;
-import java.awt.geom.Line2D;
+import java.awt.geom.Arc2D;
 import java.awt.geom.Point2D;
 
 import edu.colorado.phet.glaciers.model.Glacier;
@@ -16,17 +16,20 @@ import edu.colorado.phet.glaciers.model.Glacier.GlacierListener;
 import edu.colorado.phet.glaciers.model.IceSurfaceRipple.IceSurfaceRippleAdapter;
 import edu.colorado.phet.glaciers.model.IceSurfaceRipple.IceSurfaceRippleListener;
 import edu.umd.cs.piccolo.nodes.PPath;
+import edu.umd.cs.piccolox.nodes.PComposite;
 
 /**
  * IceSurfaceRippleNode is the visual representation of a "ripple" on the surface of the ice.
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
-public class IceSurfaceRippleNode extends PPath {
+public class IceSurfaceRippleNode extends PComposite {
     
     private static final Stroke STROKE = new BasicStroke( 1f );
     private static final Color COLOR_ABOVE_ELA = new Color( 230, 230, 230 );
     private static final Color COLOR_BELOW_ELA = new Color( 200, 200, 200 );
+    private static final double RIPPLE_WIDTH = 25; // meters
+    private static final double MARGIN_PERCENTAGE = 0.2;
     
     private final Glacier _glacier;
     private final IceSurfaceRipple _ripple;
@@ -34,6 +37,7 @@ public class IceSurfaceRippleNode extends PPath {
     private final IceSurfaceRippleListener _rippleListener;
     private final ModelViewTransform _mvt;
     private final Point2D _pView;
+    private final PPath _topArcNode, _bottomArcNode;
     
     public IceSurfaceRippleNode( IceSurfaceRipple ripple, Glacier glacier, ModelViewTransform mvt ) {
         super();
@@ -59,12 +63,27 @@ public class IceSurfaceRippleNode extends PPath {
         _ripple.addIceSurfaceRippleListener( _rippleListener );
         
         _pView = new Point2D.Double();
-        
-        Point2D pModel = new Point2D.Double( 0, MountainsAndValleyNode.getPerspectiveHeight() );
+       
+        Point2D pModel = new Point2D.Double( RIPPLE_WIDTH, 0 );
         mvt.modelToView( pModel, _pView );
-        final double height = _pView.getY();
-        setPathTo( createPath( height ) );
-        setStroke( STROKE );
+        final double rippleWidth = Math.abs( _pView.getX() );
+        final double arcWidth = 2 * rippleWidth;
+        pModel = new Point2D.Double( 0, MountainsAndValleyNode.getPerspectiveHeight() );
+        mvt.modelToView( pModel, _pView );
+        final double surfaceHeight = Math.abs( _pView.getY() );
+        final double margin = MARGIN_PERCENTAGE * surfaceHeight;
+        final double rippleHeight = surfaceHeight - ( 2 * margin );
+        Shape topArc = new Arc2D.Double( 0, 0, arcWidth, 0.35 * rippleHeight, 270, 180, Arc2D.OPEN ); // angles specified in degrees!
+        Shape bottomArc = new Arc2D.Double( 0, 0, arcWidth, 0.65 * rippleHeight, 270, 180, Arc2D.OPEN );
+        _topArcNode = new PPath( topArc );
+        _topArcNode.setStroke( STROKE );
+        _bottomArcNode = new PPath( bottomArc );
+        _bottomArcNode.setStroke( STROKE );
+        
+        _bottomArcNode.setOffset( 0, -( _bottomArcNode.getFullBoundsReference().getHeight() + margin ) );
+        _topArcNode.setOffset( 0, -( _topArcNode.getFullBoundsReference().getHeight() + _bottomArcNode.getFullBoundsReference().getHeight() + margin ) );
+        addChild( _topArcNode );
+        addChild( _bottomArcNode );
         
         update();
     }
@@ -74,20 +93,17 @@ public class IceSurfaceRippleNode extends PPath {
         _ripple.removeIceSurfaceRippleListener( _rippleListener );
     }
     
-    private static Shape createPath( double height ) {
-        double margin = 0.2 * height;
-        return new Line2D.Double( 0, margin, 0, height - margin );
-    }
-    
     private void update() {
         
         // use different colors above and below ELA
         final Point2D surfaceAtELA = _glacier.getSurfaceAtSteadyStateELAReference();
         if ( surfaceAtELA != null && _ripple.getX() >= surfaceAtELA.getX() ) {
-            setStrokePaint( COLOR_BELOW_ELA );
+            _topArcNode.setStrokePaint( COLOR_BELOW_ELA );
+            _bottomArcNode.setStrokePaint( COLOR_BELOW_ELA );
         }
         else {
-            setStrokePaint( COLOR_ABOVE_ELA );
+            _topArcNode.setStrokePaint( COLOR_ABOVE_ELA );
+            _bottomArcNode.setStrokePaint( COLOR_ABOVE_ELA );
         }
         
         // position
