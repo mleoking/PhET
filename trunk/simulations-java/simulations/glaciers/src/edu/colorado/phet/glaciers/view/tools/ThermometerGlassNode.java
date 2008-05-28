2fe44a534d5476b24096984e5579032a570f4889
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-05-28 17:35:19 +0000

    5/28/08 - started working on thermometer node with dynamic mercury

---
+++ trunk/simulations-java/simulations/glaciers/src/edu/colorado/phet/glaciers/view/tools/ThermometerGlassNode.java
@@ -0,0 +1,84 @@
+package edu.colorado.phet.glaciers.view.tools;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Shape;
+import java.awt.Stroke;
+import java.awt.geom.Area;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.RoundRectangle2D;
+
+import edu.umd.cs.piccolo.nodes.PPath;
+import edu.umd.cs.piccolo.util.PDimension;
+import edu.umd.cs.piccolox.nodes.PClip;
+import edu.umd.cs.piccolox.nodes.PComposite;
+
+
+public class ThermometerGlassNode extends PComposite {
+
+    private static final double SHAFT_WIDTH_PROPORTION = 0.5; 
+    
+    private static final Color LIQUID_COLOR = Color.RED; 
+    private static final Stroke GLASS_STROKE = new BasicStroke( 2f );
+    private static final Color GLASS_STROKE_COLOR = Color.BLACK;
+    private static final Color GLASS_FILL_COLOR = Color.WHITE;
+    
+    private final PDimension _size;
+    private final Rectangle2D _liquidShape;
+    private final PPath _liquidNode;
+    
+    public ThermometerGlassNode( PDimension size ) {
+        super();
+        
+        if ( size.getWidth() >= size.getHeight() ) {
+            throw new IllegalArgumentException( "height must be > width :" + size );
+        }
+        
+        _size = new PDimension( size );
+        
+        final double bulbDiameter = size.getWidth();
+        final double shaftWidth = size.getWidth() * SHAFT_WIDTH_PROPORTION;
+        final double shaftHeight = size.getHeight() - bulbDiameter;
+        RoundRectangle2D shaftShape = new RoundRectangle2D.Double( -shaftWidth/2, -shaftHeight, shaftWidth, shaftHeight + bulbDiameter, bulbDiameter/2, bulbDiameter/2 );
+        Shape bulbShape = new Ellipse2D.Double( -bulbDiameter/2, 0, bulbDiameter, bulbDiameter );
+        Area area = new Area( shaftShape );
+        area.add( new Area( bulbShape ) );
+        
+        PPath glassFillNode = new PPath( area );
+        glassFillNode.setStroke( null );
+        glassFillNode.setPaint( GLASS_FILL_COLOR );
+        
+        PPath glassOutlineNode = new PPath( area );
+        glassOutlineNode.setStroke( GLASS_STROKE );
+        glassOutlineNode.setStrokePaint( GLASS_STROKE_COLOR );
+        glassOutlineNode.setPaint( null );
+        
+        _liquidShape = new Rectangle2D.Double();
+        _liquidNode = new PPath();
+        _liquidNode.setPaint( LIQUID_COLOR );
+        _liquidNode.setStroke( null );
+        
+        PClip clipNode = new PClip();
+        clipNode.setPathTo( area );
+        clipNode.setStroke( null );
+        clipNode.addChild( _liquidNode );
+        
+        addChild( glassFillNode );
+        addChild( clipNode );
+        addChild( glassOutlineNode );
+        
+        setTemperature( 0.5 );
+    }
+    
+    public void setTemperature( double percent ) {
+        if ( percent < 0 || percent > 1 ) {
+            throw new IllegalArgumentException( "percent out of range: " + percent );
+        }
+        final double bulbDiameter = _size.getWidth();
+        final double liquidWidth = bulbDiameter;
+        final double liquidHeight = bulbDiameter + ( percent * ( _size.getHeight() - bulbDiameter ) );
+        _liquidShape.setRect( -bulbDiameter/2, -liquidHeight + bulbDiameter, liquidWidth, liquidHeight );
+        _liquidNode.setPathTo( _liquidShape );
+    }
+}
