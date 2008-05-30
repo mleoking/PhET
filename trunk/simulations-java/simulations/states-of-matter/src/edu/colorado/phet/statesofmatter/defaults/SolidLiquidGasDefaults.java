527be73e6980fe2858c2623eb2d278032bfe95ed
jxb147 <jxb147@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-05-30 21:29:38 +0000

    Added and removed classes and performed various refactorings to make this sim more consistent with the currently accepted simulation design.

---
+++ trunk/simulations-java/simulations/states-of-matter/src/edu/colorado/phet/statesofmatter/defaults/SolidLiquidGasDefaults.java
@@ -0,0 +1,22 @@
+package edu.colorado.phet.statesofmatter.defaults;
+
+import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
+
+/**
+ * This class contains the default settings for the "Solid, Liquid, Gas" Module.
+ *
+ * @author John Blanco
+ */
+public class SolidLiquidGasDefaults {
+    
+    /* Not intended for instantiation */
+    private SolidLiquidGasDefaults() {}
+    
+    // Clock
+    public static final boolean CLOCK_RUNNING = GlobalDefaults.CLOCK_RUNNING;
+    public static final int CLOCK_FRAME_RATE = 25; // Frames per second.
+    public static final double CLOCK_DT = 5; // Milliseconds per tick.
+    public static final int CLOCK_TIME_COLUMNS = GlobalDefaults.CLOCK_TIME_COLUMNS;
+    public static final ConstantDtClock CLOCK = new ConstantDtClock( CLOCK_FRAME_RATE, CLOCK_DT );
+
+}
