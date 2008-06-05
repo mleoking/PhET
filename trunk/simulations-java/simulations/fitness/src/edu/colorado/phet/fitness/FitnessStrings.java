14e73113ae497abaf4b0ce38d0b0d387041778cc
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-06-05 23:33:54 +0000

    removed example module fixed missing strings

--- trunk/simulations-java/simulations/fitness/src/edu/colorado/phet/fitness/FitnessStrings.java
+++ trunk/simulations-java/simulations/fitness/src/edu/colorado/phet/fitness/FitnessStrings.java
@@ -9,27 +9,19 @@ import java.text.DecimalFormat;
  * We load all strings as statics so that we will be warned at startup time of any missing strings.
  */
 public class FitnessStrings {
-    public static String TITLE_FITNESS_MODULE = FitnessResources.getString( "fitness.module.title" );
+    public static String TITLE_FITNESS_MODULE = FitnessResources.getString( "fitness.name" );
 
 
     /* not intended for instantiation */
     private FitnessStrings() {
     }
 
-    public static final String LABEL_POSITION = FitnessResources.getString( "label.position" );
-    public static final String LABEL_ORIENTATION = FitnessResources.getString( "label.orientation" );
-
-    public static final String TITLE_EXAMPLE_CONTROL_PANEL = FitnessResources.getString( "title.exampleControlPanel" );
-    public static final String TITLE_EXAMPLE_MODULE = FitnessResources.getString( "title.exampleModule" );
-
-    public static final String UNITS_DISTANCE = FitnessResources.getString( "units.distance" );
-    public static final String UNITS_ORIENTATION = FitnessResources.getString( "units.orientation" );
     public static final String UNITS_TIME = FitnessResources.getString( "units.years" );
 
     public static final String KCAL = FitnessResources.getString( "units.cal" );
     public static final String day= FitnessResources.getString( "units.day" );
     public static final String KCAL_PER_DAY = KCAL + "/"+day;
-    public static final String FATS = FitnessResources.getString( "fats" );
+    public static final String FATS = FitnessResources.getString( "food.fats" );
 
     public static final DecimalFormat KCAL_PER_DAY_FORMAT = new DecimalFormat( "0" );
     public static final DecimalFormat WEIGHT_FORMAT = new DecimalFormat( "0" );
