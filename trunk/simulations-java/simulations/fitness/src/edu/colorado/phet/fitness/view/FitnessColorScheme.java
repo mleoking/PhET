5969447710e7c3ddce7bc6e410450d2e42ab1294
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-06-06 17:13:50 +0000

    Lightened up proteins, fats, lipids for projector

--- trunk/simulations-java/simulations/fitness/src/edu/colorado/phet/fitness/view/FitnessColorScheme.java
+++ trunk/simulations-java/simulations/fitness/src/edu/colorado/phet/fitness/view/FitnessColorScheme.java
@@ -2,8 +2,6 @@ package edu.colorado.phet.fitness.view;
 
 import java.awt.*;
 
-import edu.colorado.phet.fitness.util.FitnessUtil;
-
 /**
  * Created by: Sam
  * Apr 17, 2008 at 8:32:19 PM
@@ -13,13 +11,9 @@ public class FitnessColorScheme {
     public static final Color ACTIVITY = new Color( 248, 125, 17 );
     public static final Color EXERCISE = new Color( 240, 248, 10 );
 
-//    public static final Color PROTEIN = FitnessUtil.brighter( new Color( 17, 93, 24 ), 20 );
-//    public static final Color CARBS = FitnessUtil.brighter( new Color( 29, 32, 175 ), 20 );
-//    public static final Color FATS = FitnessUtil.brighter( new Color( 75, 3, 75 ), 20 );
-
-    public static final Color PROTEIN = FitnessUtil.brighter( new Color( 17, 93, 24 ), 40 );
-    public static final Color CARBS = FitnessUtil.brighter( new Color( 29, 32, 175 ), 40 );
-    public static final Color FATS = FitnessUtil.brighter( new Color( 75, 3, 75 ), 40 );
+    public static final Color PROTEIN = new Color( 79, 171, 87 );
+    public static final Color CARBS = new Color( 93, 93, 238 );
+    public static final Color FATS = new Color( 171, 73, 171 );
 
     public static Color getBackgroundColor() {
         return new Color( 200, 240, 200 );
