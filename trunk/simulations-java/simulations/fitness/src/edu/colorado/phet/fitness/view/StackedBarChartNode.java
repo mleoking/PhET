70fcda31497fa108a586cf7f4ef228574b803d9b
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-06-07 05:30:46 +0000

    added stub for zoom

--- trunk/simulations-java/simulations/fitness/src/edu/colorado/phet/fitness/view/StackedBarChartNode.java
+++ trunk/simulations-java/simulations/fitness/src/edu/colorado/phet/fitness/view/StackedBarChartNode.java
@@ -1,10 +1,15 @@
 package edu.colorado.phet.fitness.view;
 
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
 import edu.colorado.phet.common.phetcommon.math.Function;
 import edu.colorado.phet.common.piccolophet.BufferedPhetPCanvas;
 import edu.colorado.phet.common.piccolophet.PhetPCanvas;
+import edu.colorado.phet.common.piccolophet.nodes.GradientButtonNode;
 import edu.colorado.phet.fitness.FitnessStrings;
 import edu.umd.cs.piccolo.PNode;
 
@@ -17,6 +22,7 @@ public class StackedBarChartNode extends PNode {
     private Function function;
     private int spacing;
     private StackedBarChartAxisNode axisNode;
+    private GradientButtonNode zoomOut;
 
     public StackedBarChartNode( Function function, String title, int horizontalInset, double minorTickSpacing, double majorTickSpacing, double maxYValue ) {
         this.function = function;
@@ -25,6 +31,15 @@ public class StackedBarChartNode extends PNode {
 
         axisNode = new StackedBarChartAxisNode( title, function, minorTickSpacing, majorTickSpacing, maxYValue );
         addChild( axisNode );
+
+        zoomOut = new GradientButtonNode( "-", 14, Color.green );
+        zoomOut.addActionListener( new ActionListener() {
+            public void actionPerformed( ActionEvent e ) {
+            }
+        } );
+        addChild( zoomOut );
+
+        updateLayout();
     }
 
     public void addStackedBarNode( StackedBarNode node ) {
@@ -39,7 +54,7 @@ public class StackedBarChartNode extends PNode {
             node.setOffset( 0, 0 );
             double dx = node.getFullBounds().getMaxX() - axisNode.getFullBounds().getX();
             axisNode.offset( dx + 2, 0 );
-//            axisNode.setOffset( node.getFullBounds().getMaxX() + axisNode.getFullBounds().getWidth(), 0 );
+            zoomOut.setOffset( axisNode.getFullBounds().getCenterX() + zoomOut.getFullBounds().getWidth() / 2, axisNode.getFullBounds().getMaxY() - zoomOut.getFullBounds().getHeight() );
 
             double xOffset = axisNode.getFullBounds().getMaxX() + 2;
             for ( int i = 1; i < barLayer.getChildrenCount(); i++ ) {
