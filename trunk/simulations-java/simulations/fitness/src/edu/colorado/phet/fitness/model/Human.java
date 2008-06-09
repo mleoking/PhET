2b2a1a50bf50a239a765dbf6e25805838479a1ed
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-06-09 16:00:16 +0000

    documentation

--- trunk/simulations-java/simulations/fitness/src/edu/colorado/phet/fitness/model/Human.java
+++ trunk/simulations-java/simulations/fitness/src/edu/colorado/phet/fitness/model/Human.java
@@ -209,9 +209,15 @@ public class Human {
         }
     }
 
+    /*
+     * SR – According to Exercise Physiology 4th Ed. By McArdle, Katch & Katch, 1996, p154, the Resting Daily Energy Expenditure is:
+     RDEE (kcal) =370+21.6 * FFM (kg)
+     This equation is said to apply to males and females over a wide range of body weights, and is the equation recommended to us by the CU Integrated Physiologists
+     */
     private void updateBMR() {
 //        bmr.setValue( BasalMetabolicRate.getBasalMetabolicRateHarrisBenedict( getMass(), getHeight(), getAge(), gender ) );
 //        System.out.println( "value = " + value +", FFMP="+getFatFreeMassPercent());
+        //
         bmr.setValue( 392 + 21.8 * getFatFreeMassKG() );
         updateActivity();
     }
