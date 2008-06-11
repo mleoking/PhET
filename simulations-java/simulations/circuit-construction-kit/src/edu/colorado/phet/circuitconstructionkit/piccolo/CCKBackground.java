181bd2222954244cbfba0bb567d4650ba18a45a1
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-06-11 17:44:25 +0000

    repackage, cleanup

---
+++ simulations-java/simulations/circuit-construction-kit/src/edu/colorado/phet/circuitconstructionkit/piccolo/CCKBackground.java
@@ -0,0 +1,51 @@
+package edu.colorado.phet.circuitconstructionkit.piccolo;
+
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+
+import edu.colorado.phet.circuitconstructionkit.ICCKModule;
+import edu.colorado.phet.circuitconstructionkit.model.CCKModel;
+import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
+import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
+import edu.umd.cs.piccolo.event.PInputEvent;
+import edu.umd.cs.piccolo.nodes.PPath;
+
+/**
+ * User: Sam Reid
+ * Date: Dec 13, 2006
+ * Time: 7:20:46 AM
+ */
+
+public class CCKBackground extends PNode {
+    private CCKModel model;
+    private CCKSimulationPanel cckSimulationPanel;
+    private PPath path;
+
+    public CCKBackground( final CCKModel model, CCKSimulationPanel cckSimulationPanel ) {
+        this.model = model;
+        this.cckSimulationPanel = cckSimulationPanel;
+        path = new PhetPPath( ICCKModule.BACKGROUND_COLOR );
+        addChild( path );
+        cckSimulationPanel.addComponentListener( new ComponentAdapter() {
+            public void componentResized( ComponentEvent e ) {
+                update();
+            }
+
+            public void componentShown( ComponentEvent e ) {
+                update();
+            }
+        } );
+        update();
+        addInputEventListener( new PBasicInputEventHandler() {
+            public void mousePressed( PInputEvent event ) {
+                System.out.println( "CCKBackground.mousePressed" );
+                model.clearSelection();
+            }
+        } );
+    }
+
+    private void update() {
+        path.setPathToRectangle( 0, 0, cckSimulationPanel.getWidth(), cckSimulationPanel.getHeight() );
+    }
+}
