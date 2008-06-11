92a7cf83bf8a9bf32f8bc466b7ea1bb5db779e1e
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-06-11 17:44:25 +0000

    repackage, cleanup

---
+++ trunk/simulations-java/simulations/circuit-construction-kit/src/edu/colorado/phet/circuitconstructionkit/piccolo/lifelike/InductorNode.java
@@ -0,0 +1,27 @@
+package edu.colorado.phet.circuitconstructionkit.piccolo_cck.lifelike;
+
+import javax.swing.*;
+
+import edu.colorado.phet.circuitconstructionkit.CCKImageSuite;
+import edu.colorado.phet.circuitconstructionkit.ICCKModule;
+import edu.colorado.phet.circuitconstructionkit.model.CCKModel;
+import edu.colorado.phet.circuitconstructionkit.model.components.Inductor;
+import edu.colorado.phet.circuitconstructionkit.piccolo_cck.ComponentImageNode;
+
+/**
+ * User: Sam Reid
+ * Date: Sep 20, 2006
+ * Time: 10:49:26 AM
+ */
+
+public class InductorNode extends ComponentImageNode {
+    private ICCKModule module;
+    private Inductor inductor;
+
+    public InductorNode( CCKModel model, Inductor inductor, JComponent component, ICCKModule module ) {
+        super( model, inductor, CCKImageSuite.getInstance().getInductorImage(), component, module );
+        this.module = module;
+        this.inductor = inductor;
+    }
+
+}
