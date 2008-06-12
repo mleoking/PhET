3fa6a48543bc33d5cd3fea41725ab1ab5d720ca4
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-06-12 17:58:51 +0000

    refactor

--- trunk/simulations-java/simulations/ph-scale/src/edu/colorado/phet/phscale/control/PHControlNode.java
+++ trunk/simulations-java/simulations/ph-scale/src/edu/colorado/phet/phscale/control/PHControlNode.java
@@ -1,3 +1,5 @@
+/* Copyright 2008, University of Colorado */
+
 package edu.colorado.phet.phscale.control;
 
 import java.awt.*;
@@ -25,14 +27,10 @@ public class PHControlNode extends PNode {
     
     private static final Font LABEL_FONT = new PhetFont( Font.BOLD, 18 );
     private static final Font VALUE_FONT = new PhetFont( 18 );
-    private static final Font SLIDER_FONT = new PhetFont( 16 );
     private static final int VALUE_COLUMNS = 4;
-    private static final Color ACID_COLOR = PHScaleConstants.H3O_COLOR;
-    private static final Color BASE_COLOR = PHScaleConstants.OH_COLOR;
-    private static final Stroke SLIDER_TRACK_STROKE = new BasicStroke( 1f );
-    private static final Color SLIDER_TRACK_STROKE_COLOR = Color.BLACK;
     
     private JFormattedTextField _valueTextField;
+    private PHSliderNode _sliderNode;
     
     public PHControlNode( double width, double height ) {
         super();
@@ -55,54 +53,16 @@ public class PHControlNode extends PNode {
         valuePanel.setLayout( valuePanelLayout );
         valuePanelLayout.addComponent( phLabel, 0, 0 );
         valuePanelLayout.addComponent( _valueTextField, 0, 1 );
-        
         PSwing valuePanelWrapper = new PSwing( valuePanel );
         
         PPath outlineNode = new PPath( new Rectangle2D.Double( 0, 0, width, height ) );
         outlineNode.setStroke( new BasicStroke( 2f ) );
         outlineNode.setStrokePaint( Color.BLACK );
         
-        JSlider slider = new JSlider( JSlider.VERTICAL, -100, 1500, 500 );
-        slider.setMajorTickSpacing( 100 );
-        slider.setPaintTicks( true );
-        slider.setPaintLabels( true );
-        slider.setPaintTrack( false );
-        slider.setPreferredSize( new Dimension( slider.getPreferredSize().width, 500 ) );
-        Hashtable sliderLabelTable = new Hashtable();
-        sliderLabelTable.put( new Integer( 0 ), new JLabel( "0" ) );
-        sliderLabelTable.put( new Integer( 200 ), new JLabel( "2" ) );
-        sliderLabelTable.put( new Integer( 400 ), new JLabel( "4" ) );
-        sliderLabelTable.put( new Integer( 600 ), new JLabel( "6" ) );
-        sliderLabelTable.put( new Integer( 800 ), new JLabel( "8" ) );
-        sliderLabelTable.put( new Integer( 1000 ), new JLabel( "10" ) );
-        sliderLabelTable.put( new Integer( 1200 ), new JLabel( "12" ) );
-        sliderLabelTable.put( new Integer( 1400 ), new JLabel( "14" ) );
-        slider.setLabelTable( sliderLabelTable );
-        PSwing sliderWrapper = new PSwing( slider );
-        
-        double w = 10;
-        double h = slider.getPreferredSize().getHeight();
-        PPath sliderTrackNode = new PPath( new Rectangle2D.Double( 0, 0, w, h ) );
-        Paint trackPaint = new GradientPaint( 0f, 0f, BASE_COLOR, 0f, (float)h, ACID_COLOR );
-        sliderTrackNode.setPaint( trackPaint );
-        sliderTrackNode.setStroke( SLIDER_TRACK_STROKE );
-        sliderTrackNode.setStrokePaint( SLIDER_TRACK_STROKE_COLOR );
-        
-        PText acidLabelNode = new PText( PHScaleStrings.LABEL_ACID );
-        acidLabelNode.setFont( SLIDER_FONT );
-        acidLabelNode.rotate( -Math.PI / 2 );
-        sliderTrackNode.addChild( acidLabelNode );
-        acidLabelNode.setOffset( -( acidLabelNode.getFullBoundsReference().getWidth() + 4 ), sliderTrackNode.getFullBoundsReference().getHeight() - acidLabelNode.getFullBoundsReference().getHeight() ); // bottom left of the track
-        
-        PText baseLabelNode = new PText( PHScaleStrings.LABEL_BASE );
-        baseLabelNode.setFont( SLIDER_FONT );
-        baseLabelNode.rotate( -Math.PI / 2 );
-        sliderTrackNode.addChild( baseLabelNode );
-        baseLabelNode.setOffset( -( baseLabelNode.getFullBoundsReference().getWidth() + 4 ), baseLabelNode.getFullBoundsReference().getHeight() ); // top left of the track
+        _sliderNode = new PHSliderNode( 10, 500 );
         
         addChild( valuePanelWrapper );
-        addChild( sliderTrackNode );
-        addChild( sliderWrapper );
+        addChild( _sliderNode );
         addChild( outlineNode );
         
         outlineNode.setOffset( 0, 0 );
@@ -110,10 +70,8 @@ public class PHControlNode extends PNode {
         PBounds vb = valuePanelWrapper.getFullBoundsReference();
         valuePanelWrapper.setOffset( ob.getX() + ( ob.getWidth() - vb.getWidth() ) / 2, ob.getY() + 10 );
         vb = valuePanelWrapper.getFullBoundsReference();
-        PBounds sb = sliderWrapper.getFullBoundsReference();
-        sliderWrapper.setOffset( ob.getX() + ( ob.getWidth() - sb.getWidth() ) / 2, vb.getMaxY() + 10 );
-        sb = sliderWrapper.getFullBoundsReference();
-        sliderTrackNode.setOffset( sb.getX(), sb.getY() );
+        PBounds sb = _sliderNode.getFullBoundsReference();
+        _sliderNode.setOffset( ob.getX() + ( ob.getWidth() - sb.getWidth() ) / 2, vb.getMaxY() + 10 );
     }
     
     private void handleSliderChanged() {
