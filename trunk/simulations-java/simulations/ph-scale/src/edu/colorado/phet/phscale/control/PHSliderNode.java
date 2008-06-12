2d98725f297691ad7d213a9d69f50f5e6b82ab50
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-06-12 19:31:35 +0000

    6/12/08 - custom Piccolo slider for setting pH

--- trunk/simulations-java/simulations/ph-scale/src/edu/colorado/phet/phscale/control/PHSliderNode.java
+++ trunk/simulations-java/simulations/ph-scale/src/edu/colorado/phet/phscale/control/PHSliderNode.java
@@ -3,11 +3,9 @@
 package edu.colorado.phet.phscale.control;
 
 import java.awt.*;
+import java.awt.geom.GeneralPath;
+import java.awt.geom.Line2D;
 import java.awt.geom.Rectangle2D;
-import java.util.Hashtable;
-
-import javax.swing.JLabel;
-import javax.swing.JSlider;
 
 import edu.colorado.phet.common.phetcommon.util.IntegerRange;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
@@ -17,79 +15,203 @@ import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.nodes.PPath;
 import edu.umd.cs.piccolo.nodes.PText;
 import edu.umd.cs.piccolo.util.PDimension;
-import edu.umd.cs.piccolox.pswing.PSwing;
+import edu.umd.cs.piccolox.nodes.PComposite;
 
 
 public class PHSliderNode extends PNode {
     
-    private static final int SIGNIFICANT_DECIMALS = 2;
-    private static final IntegerRange RANGE = new IntegerRange( -1, 15, 7 );
-    private static final int FIRST_TICK = 0;
-    private static final int TICK_SPACING = 2;
-    private static final int MODEL_TO_VIEW_SCALE = (int) Math.pow( 10, SIGNIFICANT_DECIMALS );
+    // whether the max is on top or bottom
+    private static final boolean MAX_AT_TOP = true;
     
-    private static final Color ACID_COLOR = PHScaleConstants.H3O_COLOR;
-    private static final Color BASE_COLOR = PHScaleConstants.OH_COLOR;
+    // Range of the slider
+    private static final IntegerRange RANGE = new IntegerRange( -1, 15, 7 );
+
+    // Track
     private static final Stroke TRACK_STROKE = new BasicStroke( 1f );
     private static final Color TRACK_COLOR = Color.BLACK;
-    private static final Font RANGE_FONT = new PhetFont( 16 );
-    private static final Font TICK_FONT = new PhetFont( 16 );
+    private static final Color ACID_COLOR = PHScaleConstants.H3O_COLOR;
+    private static final Color BASE_COLOR = PHScaleConstants.OH_COLOR;
     
-    public PHSliderNode( PDimension trackSize ) {
-        this( trackSize.getWidth(), trackSize.getHeight() );
-    }
+    // Knob
+    private static final Color KNOB_FILL_COLOR = Color.WHITE;
+    private static final Color KNOB_STROKE_COLOR = Color.BLACK;
+    private static final int KNOB_STROKE_WIDTH = 1;
+    
+    // Ticks
+    private static final double TICK_LENGTH = 10;
+    private static final int TICK_SPACING = 2;
+    private static final Font TICK_FONT = new PhetFont( 16 );
+    private static final Stroke TICK_STROKE = new BasicStroke( 1f );
+    private static final Color TICK_COLOR = Color.BLACK;
+    private static final double TICK_LABEL_SPACING = 2;
 
-    public PHSliderNode( double trackWidth, double trackHeight ) {
+    // Labels
+    private static final Font ACID_BASE_FONT = new PhetFont( 16 );
+
+    private final TrackNode _trackNode;
+    private final KnobNode _knobNode;
+    private double _pH;
+    
+    public PHSliderNode( PDimension trackSize, PDimension knobSize ) {
         super();
         
-        TrackNode trackNode = new TrackNode( trackWidth, trackHeight );
+        _trackNode = new TrackNode( trackSize );
+        _knobNode = new KnobNode( knobSize );
         
-        int min = RANGE.getMin() * MODEL_TO_VIEW_SCALE;
-        int max = RANGE.getMax() * MODEL_TO_VIEW_SCALE;
-        int defaultValue = RANGE.getDefault() * MODEL_TO_VIEW_SCALE;
-        JSlider slider = new JSlider( JSlider.VERTICAL, min, max, defaultValue );
-        slider.setMajorTickSpacing( 1 * MODEL_TO_VIEW_SCALE );
-        slider.setPaintTicks( true );
-        slider.setPaintLabels( true );
-        slider.setPaintTrack( false );
-        slider.setPreferredSize( new Dimension( slider.getPreferredSize().width, (int)trackHeight ) );
-        Hashtable sliderLabelTable = new Hashtable();
-        for ( int i = 0; i <= 14; i += 2 ) {
-            sliderLabelTable.put( new Integer( i * MODEL_TO_VIEW_SCALE ), new JLabel( String.valueOf( i ) ) );
+        PComposite tickNodes = new PComposite();
+        TickMarkNode tickMarkNode = null;
+        final double xOffset = 0;
+        double yOffset = 0;
+        final double yDelta = trackSize.getHeight() / ( RANGE.getMax() - RANGE.getMin() );
+        if ( MAX_AT_TOP ) {
+            for ( int i = RANGE.getMax(); i >= RANGE.getMin(); i-- ) {
+                if ( i % TICK_SPACING == 0 ) {
+                    tickMarkNode = new TickMarkNode( String.valueOf( i ) );
+                }
+                else {
+                    tickMarkNode = new TickMarkNode();
+                }
+                tickNodes.addChild( tickMarkNode );
+                tickMarkNode.setOffset( xOffset, yOffset );
+                yOffset += yDelta;
+            }
         }
-        slider.setLabelTable( sliderLabelTable );
-        PSwing sliderWrapper = new PSwing( slider );
+        else {
+            for ( int i = RANGE.getMin(); i <= RANGE.getMax(); i++ ) {
+                if ( i % TICK_SPACING == 0 ) {
+                    tickMarkNode = new TickMarkNode( String.valueOf( i ) );
+                }
+                else {
+                    tickMarkNode = new TickMarkNode();
+                }
+                tickNodes.addChild( tickMarkNode );
+                tickMarkNode.setOffset( xOffset, yOffset );
+                yOffset += yDelta;
+            }
+        }
         
         PText acidLabelNode = new PText( PHScaleStrings.LABEL_ACID );
-        acidLabelNode.setFont( RANGE_FONT );
+        acidLabelNode.setFont( ACID_BASE_FONT );
         acidLabelNode.rotate( -Math.PI / 2 );
         
         PText baseLabelNode = new PText( PHScaleStrings.LABEL_BASE );
-        baseLabelNode.setFont( RANGE_FONT );
+        baseLabelNode.setFont( ACID_BASE_FONT );
         baseLabelNode.rotate( -Math.PI / 2 );
 
-        addChild( sliderWrapper );
-        addChild( trackNode );
+        addChild( _trackNode );
         addChild( acidLabelNode );
         addChild( baseLabelNode );
+        addChild( tickNodes );
+        addChild( _knobNode );
         
-        sliderWrapper.setOffset( 0, 0 );
-        trackNode.setOffset( sliderWrapper.getOffset() );
-        acidLabelNode.setOffset( -( acidLabelNode.getFullBoundsReference().getWidth() + 4 ), trackNode.getFullBoundsReference().getHeight() - 5 ); // bottom left of the track
-        baseLabelNode.setOffset( -( baseLabelNode.getFullBoundsReference().getWidth() + 4 ), baseLabelNode.getFullBoundsReference().getHeight() + 5 ); // top left of the track
+        _trackNode.setOffset( 0, 0 ); // origin at the upper-left corner of the track
+        tickNodes.setOffset( _trackNode.getFullBoundsReference().getMaxX() + 5, _trackNode.getYOffset() );
+        _knobNode.setOffset( _trackNode.getFullBoundsReference().getMaxX() - ( 0.75 * _knobNode.getFullBoundsReference().getWidth() ), 0 );
+        final int xSpacing = 4; // space between range labels and track
+        final int yMargin = 5; // range labels are offset this amount from ends of track
+        if ( MAX_AT_TOP ) {
+            acidLabelNode.setOffset( -( acidLabelNode.getFullBoundsReference().getWidth() + xSpacing ), _trackNode.getFullBoundsReference().getHeight() - yMargin ); // bottom left of the track
+            baseLabelNode.setOffset( -( baseLabelNode.getFullBoundsReference().getWidth() + xSpacing ), baseLabelNode.getFullBoundsReference().getHeight() + yMargin ); // top left of the track
+        }
+        else {
+            baseLabelNode.setOffset( -( baseLabelNode.getFullBoundsReference().getWidth() + xSpacing ), _trackNode.getFullBoundsReference().getHeight() - yMargin ); // bottom left of the track
+            acidLabelNode.setOffset( -( acidLabelNode.getFullBoundsReference().getWidth() + xSpacing ), acidLabelNode.getFullBoundsReference().getHeight() + yMargin ); // top left of the track    
+        }
+        
+        // initialize
+        setPH( RANGE.getDefault() );
+    }
+    
+    public double getPH() {
+        return _pH;
+    }
+    
+    public void setPH( double pH ) {
+        if ( pH < RANGE.getMin() || pH > RANGE.getMax() ) {
+            throw new IllegalArgumentException( "pH is out of range: " + pH );
+        }
+        if ( pH != _pH ) {
+            double xOffset = _knobNode.getXOffset();
+            double yOffset = ( ( RANGE.getMax() - pH ) / ( RANGE.getMax() - RANGE.getMin() ) ) * _trackNode.getFullBoundsReference().getHeight();
+            _knobNode.setOffset( xOffset, yOffset );
+        }
     }
     
     /*
      * The slider track, filled with a gradient that indicates the transition from acid to base.
      */
-    private static class TrackNode extends PPath {
-        public TrackNode( double width, double height ) {
+    private static class TrackNode extends PNode {
+        public TrackNode( PDimension size ) {
             super();
-            setPathTo( new Rectangle2D.Double( 0, 0, width, height ) );
-            Paint trackPaint = new GradientPaint( 0f, 0f, BASE_COLOR, 0f, (float)height, ACID_COLOR );
-            setPaint( trackPaint );
-            setStroke( TRACK_STROKE );
-            setStrokePaint( TRACK_COLOR );
+            PPath pathNode = new PPath();
+            pathNode.setPathTo( new Rectangle2D.Double( 0, 0, size.getWidth(), size.getHeight() ) );
+            Paint trackPaint = null;
+            if ( MAX_AT_TOP ) {
+                trackPaint = new GradientPaint( 0f, 0f, BASE_COLOR, 0f, (float)size.getHeight(), ACID_COLOR );
+            }
+            else {
+                trackPaint = new GradientPaint( 0f, 0f, ACID_COLOR, 0f, (float)size.getHeight(), BASE_COLOR );
+            }
+            pathNode.setPaint( trackPaint );
+            pathNode.setStroke( TRACK_STROKE );
+            pathNode.setStrokePaint( TRACK_COLOR );
+            addChild( pathNode );
+        }
+    }
+    
+    /*
+     * The slider knob, origin at tip.
+     */
+    private static class KnobNode extends PNode {
+        public KnobNode( PDimension size ) {
+
+            // tip of knob points down
+            float w = (float) size.getWidth();
+            float h = (float) size.getHeight();
+            GeneralPath knobPath = new GeneralPath();
+            knobPath.moveTo( 0f, 0f );
+            knobPath.lineTo( w, 0f );
+            knobPath.lineTo( w, 0.65f * h );
+            knobPath.lineTo( w / 2f, h );
+            knobPath.lineTo( 0f, 0.65f * h );
+            knobPath.closePath();
+
+            PPath pathNode = new PPath();
+            pathNode.setPathTo( knobPath );
+            pathNode.setPaint( KNOB_FILL_COLOR );
+            pathNode.setStroke( new BasicStroke( KNOB_STROKE_WIDTH ) );
+            pathNode.setStrokePaint( KNOB_STROKE_COLOR );
+            addChild( pathNode );
+            
+            // tip of knob points to the right
+            rotate( -Math.PI/2 );
+        }
+    }
+    
+    private static class TickMarkNode extends PComposite {
+        
+        public TickMarkNode() {
+            this( null );
+        }
+        
+        public TickMarkNode( String label ) {
+            super();
+            setPickable( false );
+            setChildrenPickable( false );
+            
+            PPath tickNode = new PPath( new Line2D.Double( 0, 0, TICK_LENGTH, 0 ) );
+            tickNode.setStroke( TICK_STROKE );
+            tickNode.setStrokePaint( TICK_COLOR );
+            tickNode.setOffset( 0, 0 );
+            addChild( tickNode );
+            
+            PText labelNode = null;
+            if ( label != null ) {
+                labelNode = new PText( label );
+                labelNode.setFont( TICK_FONT );
+                labelNode.setOffset( tickNode.getFullBoundsReference().getMaxX() + TICK_LABEL_SPACING, -labelNode.getFullBoundsReference().getHeight() / 2 );
+                addChild( labelNode );
+            }
         }
     }
 }
