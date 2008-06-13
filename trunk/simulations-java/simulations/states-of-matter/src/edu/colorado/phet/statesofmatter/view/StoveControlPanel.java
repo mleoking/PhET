4dcf0ed40d3a2b9568d005dc1cf24bfca9a780e7
jxb147 <jxb147@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-06-13 23:19:57 +0000

    added control panel for heat source/sink

--- trunk/simulations-java/simulations/states-of-matter/src/edu/colorado/phet/statesofmatter/view/StoveControlPanel.java
+++ trunk/simulations-java/simulations/states-of-matter/src/edu/colorado/phet/statesofmatter/view/StoveControlPanel.java
@@ -2,20 +2,27 @@ package edu.colorado.phet.statesofmatter.view;
 
 import edu.colorado.phet.idealgas.IdealGasResources;
 import edu.colorado.phet.statesofmatter.StatesOfMatterStrings;
+import edu.colorado.phet.phetgraphicsdemo.view.DebuggerGraphic;
 
 import javax.swing.*;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.ChangeEvent;
 import javax.swing.border.Border;
 import javax.swing.border.TitledBorder;
 import javax.swing.border.EtchedBorder;
 import javax.swing.border.BevelBorder;
 import java.util.Hashtable;
+import java.util.ArrayList;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 public class StoveControlPanel extends JPanel {
     private static final int s_stoveSliderHeight = 60;
-    private Color controlColor = new Color( 240, 230, 255 );
+    private Color controlColor = new Color(240, 230, 255);
+    private ArrayList listeners = new ArrayList();
+    private JSlider stoveSlider;
+    private int maxStoveSliderValue;
 
     public StoveControlPanel() {
 
@@ -25,62 +32,61 @@ public class StoveControlPanel extends JPanel {
         // it must be put into an intermediate JPanel with a simple layout manager (in this case
         // we use the default), and that intermediate panel is then added to the ApparatusPanel.
         JPanel stovePanel = this;
-        int maxStoveSliderValue = 40;
-        final JSlider stoveSlider = new JSlider( JSlider.VERTICAL, -maxStoveSliderValue,
-                                                 maxStoveSliderValue, 0 );
-        stoveSlider.setMajorTickSpacing( maxStoveSliderValue );
-        stoveSlider.setMinorTickSpacing( 10 );
-        stoveSlider.setSnapToTicks( true );
+        maxStoveSliderValue = 40;
+        stoveSlider = new JSlider(JSlider.VERTICAL, -maxStoveSliderValue,
+                maxStoveSliderValue, 0);
+        stoveSlider.setMajorTickSpacing(maxStoveSliderValue);
+        stoveSlider.setMinorTickSpacing(10);
+        stoveSlider.setSnapToTicks(true);
         Hashtable labelTable = new Hashtable();
-        labelTable.put( new Integer( 40 ), new JLabel(StatesOfMatterStrings.STOVE_CONTROL_PANEL_ADD_LABEL));
-        labelTable.put( new Integer( 0 ), new JLabel(StatesOfMatterStrings.STOVE_CONTROL_PANEL_ZERO_LABEL));
-        labelTable.put( new Integer( -40 ), new JLabel(StatesOfMatterStrings.STOVE_CONTROL_PANEL_REMOVE_LABEL));
-        stoveSlider.setLabelTable( labelTable );
-        stoveSlider.setPaintTicks( true );
-        stoveSlider.setSnapToTicks( true );
-        stoveSlider.setPaintLabels( true );
-        stoveSlider.setPreferredSize( new Dimension( 100, s_stoveSliderHeight ) );
-//        stoveSlider.addChangeListener( new ChangeListener() {
-//            public void stateChanged( ChangeEvent event ) {
-//                module.setStove( stoveSlider.getValue() );
-//            }
-//        } );
-        stoveSlider.addMouseListener( new MouseAdapter() {
-            public void mouseReleased( MouseEvent e ) {
-                stoveSlider.setValue( 0 );
+        labelTable.put(new Integer(40), new JLabel(StatesOfMatterStrings.STOVE_CONTROL_PANEL_ADD_LABEL));
+        labelTable.put(new Integer(0), new JLabel(StatesOfMatterStrings.STOVE_CONTROL_PANEL_ZERO_LABEL));
+        labelTable.put(new Integer(-40), new JLabel(StatesOfMatterStrings.STOVE_CONTROL_PANEL_REMOVE_LABEL));
+        stoveSlider.setLabelTable(labelTable);
+        stoveSlider.setPaintTicks(true);
+        stoveSlider.setSnapToTicks(true);
+        stoveSlider.setPaintLabels(true);
+        stoveSlider.setPreferredSize(new Dimension(100, s_stoveSliderHeight));
+
+        stoveSlider.addMouseListener(new MouseAdapter() {
+            public void mouseReleased(MouseEvent e) {
+                stoveSlider.setValue(0);
             }
-        } );
+        });
 
-        Border border = new TitledBorder( new EtchedBorder( BevelBorder.RAISED,
-                                                            new Color( 40, 20, 255 ),
-                                                            Color.black ),
-                                          IdealGasResources.getString( "IdealGasControlPanel.Heat_Control" ) );
-        stovePanel.setBorder( border );
+        Border border = new TitledBorder(new EtchedBorder(BevelBorder.RAISED,
+                new Color(40, 20, 255),
+                Color.black),
+                IdealGasResources.getString("IdealGasControlPanel.Heat_Control"));
+        stovePanel.setBorder(border);
         stoveSlider.setBackground(controlColor);
-//        stovePanel.setPreferredSize( new Dimension( 115, 85 ) );
         stovePanel.setBackground(controlColor);
-//        PhetGraphic panelPJC = PhetJComponent.newInstance( module.getApparatusPanel(), stovePanel );
-//        this.addGraphic( panelPJC );
-
-//        stoveSlider.setBackground( controlColor );
-//        PhetGraphic sliderPJC = PhetJComponent.newInstance( module.getApparatusPanel(), stoveSlider );
-//        sliderPJC.setCursorHand();
-//        this.addGraphic( sliderPJC );
-//        sliderPJC.setLocation( 10, 20 );
-
-        // Add a listener to the model that will disable the slider when the model is in contant temperature mode
-//        final IdealGasModel model = module.getIdealGasModel();
-//        model.addChangeListener( new IdealGasModel.ChangeListener() {
-//            public void stateChanged( IdealGasModel.ChangeEvent event ) {
-//                if( model.getConstantProperty() == IdealGasModel.CONSTANT_TEMPERATURE ) {
-//                    stoveSlider.setEnabled( false );
-//                }
-//                else {
-//                    stoveSlider.setEnabled( true );
-//                }
-//            }
-//        } );
         add(stoveSlider);
+
+        stoveSlider.addChangeListener(new ChangeListener() {
+            public void stateChanged(ChangeEvent e) {
+                notifyListeners();
+            }
+        });
     }
 
+    private void notifyListeners() {
+        for (int i = 0; i < listeners.size(); i++) {
+            Listener listener = (Listener) listeners.get(i);
+            listener.valueChanged(getSliderValue());
+        }
+    }
+
+    private double getSliderValue() {
+        //assumes symmetric range on slider
+        return stoveSlider.getValue() / (double)stoveSlider.getMaximum();
+    }
+
+    public static interface Listener {
+        void valueChanged(double value);
+    }
+
+    public void addListener(Listener listener) {
+        listeners.add(listener);
+    }
 }
--- trunk/simulations-java/simulations/states-of-matter/src/edu/colorado/phet/statesofmatter/view/StoveNode.java
+++ trunk/simulations-java/simulations/states-of-matter/src/edu/colorado/phet/statesofmatter/view/StoveNode.java
@@ -12,33 +12,63 @@ import edu.umd.cs.piccolox.pswing.PSwing;
 
 
 public class StoveNode extends PNode {
-    
-    public StoveNode (){
-        
-        PImage fireImage = StatesOfMatterResources.getImageNode( "flames.gif" );
+
+
+    // Heat value, ranges from -1 to +1.
+    private double heat;
+    private PImage fireImage;
+    private PImage iceImage;
+    private PImage stoveImage;
+    private StoveControlPanel stoveControlPanel;
+
+    public StoveNode() {
+
+        fireImage = StatesOfMatterResources.getImageNode("flames.gif");
         addChild(fireImage);
 
-        PImage iceImage = StatesOfMatterResources.getImageNode( "ice.gif" );
+        iceImage = StatesOfMatterResources.getImageNode("ice.gif");
         addChild(iceImage);
 
-        PImage stoveImage = StatesOfMatterResources.getImageNode( "stove.png" );
+        stoveImage = StatesOfMatterResources.getImageNode("stove.png");
         addChild(stoveImage);
 
-        fireImage.setOffset(stoveImage.getFullBoundsReference().width/2 - fireImage.getFullBoundsReference().width/2, 0);
-        iceImage.setOffset(stoveImage.getFullBoundsReference().width/2 - iceImage.getFullBoundsReference().width/2, 0);
-
-        StoveControlPanel stoveControlPanel = new StoveControlPanel();
+        stoveControlPanel = new StoveControlPanel();
+        stoveControlPanel.addListener(new StoveControlPanel.Listener() {
+            public void valueChanged(double value) {
+                heat = value;
+                update();
+            }
+        });
         PSwing stoveControlPanelNode = new PSwing(stoveControlPanel);
         addChild(stoveControlPanelNode);
         stoveControlPanelNode.setOffset(stoveImage.getFullBoundsReference().getWidth() + 10, 0);
+
+        update();
     }
-    
-    public static void main( String[] args ) {
-        SwingUtilities.invokeLater( new Runnable(){
-            public void run(){
+
+    private void update() {
+
+        if (heat > 0) {
+            fireImage.setOffset(stoveImage.getFullBoundsReference().width / 2 - fireImage.getFullBoundsReference().width / 2,
+                    -heat * stoveImage.getFullBoundsReference().height);
+            iceImage.setOffset(stoveImage.getFullBoundsReference().width / 2 - iceImage.getFullBoundsReference().width / 2, 0);
+        } else if (heat <= 0) {
+            iceImage.setOffset(stoveImage.getFullBoundsReference().width / 2 - iceImage.getFullBoundsReference().width / 2,
+                    heat * stoveImage.getFullBoundsReference().height);
+            fireImage.setOffset(stoveImage.getFullBoundsReference().width / 2 - fireImage.getFullBoundsReference().width / 2, 0);
+        }
+        iceImage.setVisible(heat<0);
+        fireImage.setVisible(heat>0);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
                 PiccoloTestFrame testFrame = new PiccoloTestFrame("Stove Node Test");
-                testFrame.addNode( new StoveNode() );
-                testFrame.setVisible( true );
+                StoveNode stoveNode = new StoveNode();
+                stoveNode.setOffset(100, 200);
+                testFrame.addNode(stoveNode);
+                testFrame.setVisible(true);
             }
         });
     }
