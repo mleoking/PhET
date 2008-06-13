1ddc3d8b56950fe2b098dc824799f7765fde66f6
jxb147 <jxb147@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-06-13 22:55:32 +0000

    updates for strings

--- trunk/simulations-java/simulations/states-of-matter/src/edu/colorado/phet/statesofmatter/StatesOfMatterStrings.java
+++ trunk/simulations-java/simulations/states-of-matter/src/edu/colorado/phet/statesofmatter/StatesOfMatterStrings.java
@@ -10,15 +10,18 @@ package edu.colorado.phet.statesofmatter;
  * @author John Blanco
  */
 public class StatesOfMatterStrings {
-    
     /* Not intended for instantiation. */
     private StatesOfMatterStrings() {}
     
+    public static final String STOVE_CONTROL_PANEL_ADD_LABEL = StatesOfMatterResources.getString( "Stove.Add" );
+    public static final String STOVE_CONTROL_PANEL_REMOVE_LABEL = StatesOfMatterResources.getString( "Stove.Remove" );
+    public static final String STOVE_CONTROL_PANEL_ZERO_LABEL = StatesOfMatterResources.getString( "Stove.Zero" );
+
     public static final String TITLE_SOLID_LIQUID_GAS_MODULE = StatesOfMatterResources.getString( "ModuleTitle.SolidLiquidGasModule" );
 
-    public static final String MOLECULE_TYPE_SELECT_LABEL = StatesOfMatterResources.getString( "SolidLiquidGasControl.MoleculeSelectionLabel" );
-    public static final String OXYGEN_SELECTION_LABEL = StatesOfMatterResources.getString( "SolidLiquidGasControl.OxygenLabel" );
-    public static final String NITROGEN_SELECTION_LABEL = StatesOfMatterResources.getString( "SolidLiquidGasControl.NitrogenLabel" );
-    public static final String CARBON_DIOXIDE_SELECTION_LABEL = StatesOfMatterResources.getString( "SolidLiquidGasControl.CarbonDioxideLabel" );
+    public static final String MOLECULE_TYPE_SELECT_LABEL = StatesOfMatterResources.getString("SolidLiquidGasControl.MoleculeSelection");
+    public static final String OXYGEN_SELECTION_LABEL = StatesOfMatterResources.getString("SolidLiquidGasControl.Oxygen");
+    public static final String NITROGEN_SELECTION_LABEL = StatesOfMatterResources.getString("SolidLiquidGasControl.Nitrogen");
+    public static final String CARBON_DIOXIDE_SELECTION_LABEL = StatesOfMatterResources.getString("SolidLiquidGasControl.CarbonDioxide");
 
 }
