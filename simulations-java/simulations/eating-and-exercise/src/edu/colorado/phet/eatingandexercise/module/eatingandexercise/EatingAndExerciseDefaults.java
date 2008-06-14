f2af377863079158bf5efea16839f7b1a54cf645
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-06-14 14:03:02 +0000

    reformat

--- simulations-java/simulations/eating-and-exercise/src/edu/colorado/phet/eatingandexercise/module/eatingandexercise/EatingAndExerciseDefaults.java
+++ simulations-java/simulations/eating-and-exercise/src/edu/colorado/phet/eatingandexercise/module/eatingandexercise/EatingAndExerciseDefaults.java
@@ -7,9 +7,9 @@ import edu.colorado.phet.eatingandexercise.model.EatingAndExerciseUnits;
  * Apr 18, 2008 at 12:41:19 AM
  */
 public class EatingAndExerciseDefaults {
-    public static final double CLOCK_DT = 1000.0 * 250/2;
+    public static final double CLOCK_DT = 1000.0 * 250 / 2;
     //    public static final int CLOCK_DELAY = 30;
-    public static final int CLOCK_DELAY = 30 * 3*2;
+    public static final int CLOCK_DELAY = 30 * 3 * 2;
     public static final boolean STARTS_PAUSED = true;
     public static final double SINGLE_STEP_TIME = EatingAndExerciseUnits.monthsToSeconds( 1 );
 }
