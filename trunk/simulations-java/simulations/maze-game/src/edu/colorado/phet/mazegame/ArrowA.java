6dd430992142c66e080bbf647ca781e0046b4bdf
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-06-16 17:28:01 +0000

    minor cleanup

--- trunk/simulations-java/simulations/maze-game/src/edu/colorado/phet/mazegame/ArrowA.java
+++ trunk/simulations-java/simulations/maze-game/src/edu/colorado/phet/mazegame/ArrowA.java
@@ -5,58 +5,55 @@ import java.awt.*;
 //M.Dubson 5/30/02  Draws a nice arrow.
 public class ArrowA {
 
-    private double L;      //length of arrow
-    private int[] xInt = new int[8];    //positions of arrow corners for polygon()
-    private int[] yInt = new int[8];
+    private double arrowLength;      //length of arrow
+    private int[] arrowCornersX = new int[8];    //positions of arrow corners for polygon()
+    private int[] arrowCornersY = new int[8];
 
     //set arrow position arrow to head:(xFinal, yFinal), tail:(x0, y0)
 
     //formula for width of arrow line
 
     public double computeWidth() {
-        return ( this.L / 8.0 );
+        return ( this.arrowLength / 8.0 );
     }
 
     public void setPosition( int x0, int y0, int xFinal, int yFinal ) {
         double x1 = xFinal - x0;      //x-component of arrow
         double y1 = yFinal - y0;    //y-component of arrow
-        this.L = Math.pow( ( x1 * x1 + y1 * y1 ), 0.5 );
+        this.arrowLength = Math.pow( ( x1 * x1 + y1 * y1 ), 0.5 );
         //this.thta = Math.asin((yFinal - y0)/L);  //not necessary
         double w = this.computeWidth();
         double h = 4 * w * Math.sqrt( 3 ) / 2.0;
 
-        double[] x = new double[]{( w / 2 ) * ( -y1 / L ),
-                ( w / 2 ) * ( -y1 / L ) + ( L - h ) * ( x1 / L ),
-                ( w ) * ( -y1 / L ) + ( L - h ) * ( x1 / L ),
-                ( L ) * ( x1 / L ),
-                ( w ) * ( y1 / L ) + ( L - h ) * ( x1 / L ),
-                ( w / 2 ) * ( y1 / L ) + ( L - h ) * ( x1 / L ),
-                ( w / 2 ) * ( y1 / L ),
-                ( w / 2 ) * ( -y1 / L )};
+        double[] x = new double[]{( w / 2 ) * ( -y1 / arrowLength ),
+                ( w / 2 ) * ( -y1 / arrowLength ) + ( arrowLength - h ) * ( x1 / arrowLength ),
+                ( w ) * ( -y1 / arrowLength ) + ( arrowLength - h ) * ( x1 / arrowLength ),
+                ( arrowLength ) * ( x1 / arrowLength ),
+                ( w ) * ( y1 / arrowLength ) + ( arrowLength - h ) * ( x1 / arrowLength ),
+                ( w / 2 ) * ( y1 / arrowLength ) + ( arrowLength - h ) * ( x1 / arrowLength ),
+                ( w / 2 ) * ( y1 / arrowLength ),
+                ( w / 2 ) * ( -y1 / arrowLength )};
 
-        double[] y = new double[]{( w / 2 ) * x1 / L,
-                ( w / 2 ) * ( x1 / L ) + ( L - h ) * ( y1 / L ),
-                ( w ) * ( x1 / L ) + ( L - h ) * ( y1 / L ),
-                ( L ) * ( y1 / L ),
-                ( w ) * ( -x1 / L ) + ( L - h ) * ( y1 / L ),
-                ( w / 2 ) * ( -x1 / L ) + ( L - h ) * ( y1 / L ),
-                ( w / 2 ) * ( -x1 / L ),
-                ( w / 2 ) * ( x1 / L )};
+        double[] y = new double[]{( w / 2 ) * x1 / arrowLength,
+                ( w / 2 ) * ( x1 / arrowLength ) + ( arrowLength - h ) * ( y1 / arrowLength ),
+                ( w ) * ( x1 / arrowLength ) + ( arrowLength - h ) * ( y1 / arrowLength ),
+                ( arrowLength ) * ( y1 / arrowLength ),
+                ( w ) * ( -x1 / arrowLength ) + ( arrowLength - h ) * ( y1 / arrowLength ),
+                ( w / 2 ) * ( -x1 / arrowLength ) + ( arrowLength - h ) * ( y1 / arrowLength ),
+                ( w / 2 ) * ( -x1 / arrowLength ),
+                ( w / 2 ) * ( x1 / arrowLength )};
 
         for ( int i = 0; i < x.length; i++ ) {
-            xInt[i] = x0 + (int) ( x[i] );
-            yInt[i] = y0 + (int) ( y[i] );
+            arrowCornersX[i] = x0 + (int) ( x[i] );
+            arrowCornersY[i] = y0 + (int) ( y[i] );
         }
-
-    }//end of position()
-
+    }
 
     public void paint( Graphics g ) {
-        //System.out.println("deltaX = " + (xFinal - x0) + "  deltaY = " + (yFinal - y0));
         g.setColor( Color.green );
-        g.fillPolygon( xInt, yInt, xInt.length );
+        g.fillPolygon( arrowCornersX, arrowCornersY, arrowCornersX.length );
         g.setColor( Color.red );
-        g.drawPolygon( xInt, yInt, xInt.length );
-    }//end of paint method
+        g.drawPolygon( arrowCornersX, arrowCornersY, arrowCornersX.length );
+    }
 
-}//end of public class
+}
