5d848b67ec1f553595cfaecd521d3fbea4f16d94
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-06-17 03:59:55 +0000

    6/16/08 - discover project name from jar files named curve-fit.jar or curve-fit_fr.jar 6/16/08 - wrap main in SwingUtility.invokeLater

--- trunk/util/translation-utility/src/edu/colorado/phet/translationutility/TranslationUtility.java
+++ trunk/util/translation-utility/src/edu/colorado/phet/translationutility/TranslationUtility.java
@@ -5,6 +5,7 @@ package edu.colorado.phet.translationutility;
 import java.io.File;
 
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
 import edu.colorado.phet.common.phetcommon.view.util.SwingUtils;
 import edu.colorado.phet.translationutility.simulations.ISimulation;
@@ -24,14 +25,9 @@ public class TranslationUtility extends JFrame {
     //NOTE: not tested with any source language code except "en"
     private static final String SOURCE_LANGUAGE_CODE = "en";
 
-    /* not intended for instantiation */
     private TranslationUtility() {}
     
-    /**
-     * main
-     * @param args
-     */
-    public static void main( String[] args ) {
+    public static void start() {
         
         // check for a more recent version on the server
         UpdateManager.checkForUpdate();
@@ -65,5 +61,18 @@ public class TranslationUtility extends JFrame {
         JFrame mainFrame = new MainFrame( simulation, SOURCE_LANGUAGE_CODE, targetLanguageCode, saveDirName );
         mainFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
         mainFrame.setVisible( true );
+    }
+
+    public static void main( String[] args ) {
+        /*
+         * Wrap the body of main in invokeLater, so that all initialization occurs
+         * in the event dispatch thread. Sun now recommends doing all Swing init in
+         * the event dispatch thread.
+         */
+        SwingUtilities.invokeLater( new Runnable() {
+            public void run() {
+                TranslationUtility.start();
+            }
+        } );
     }
 }
