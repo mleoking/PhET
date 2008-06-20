b7e0dae790fd1ce9bb5267a4692002b690d622b2
flydanofly <flydanofly@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-06-20 04:08:14 +0000

    website: Eliminated translations table with simple list of languages with translations, # of translations, and links to language sections that have all of a lanugage's translated sims

--- website/simulations/translations.php
+++ website/simulations/translations.php
@@ -3,6 +3,7 @@
 if (!defined("SITE_ROOT")) define("SITE_ROOT", "../");
 include_once(SITE_ROOT."admin/global.php");
 include_once(SITE_ROOT."page_templates/SitePage.php");
+include_once(SITE_ROOT."admin/sim-utils.php");
 
 class TranslationsPage extends SitePage {
 
@@ -31,15 +32,80 @@ class TranslationsPage extends SitePage {
 
 EOT;
 
-         $sim_to_translations = sim_get_all_translated_language_names();
+        $sim_to_translations = sim_get_all_translated_language_names();
 
-         $languages = array();
+        // TODO: make own function to reshuffle data
+        $sim_lang_map = array();
+        foreach ($sim_to_translations as $sim_name => $map) {
+            foreach ($map as $language_name => $language_launch_url) {
+                if (!array_key_exists($language_name, $sim_lang_map)) {
+                    $sim_lang_map[$language_name] = array();
+                }
 
-         foreach ($sim_to_translations as $sim_name => $map) {
-             foreach ($map as $language_name => $launch_url) {
-                 $languages["$language_name"] = "$language_name";
-             }
-         }
+                $sim_lang_map[$language_name][$sim_name] = $language_launch_url;
+            }
+        }
+
+        function mycountcmp($a, $b) {
+            return count($a) < count($b);
+        }
+
+        // ON HOLD: Google uses a cached version of the webpage to translate, and it doesn't have all the translations we do
+        global $LANGUAGE_CODE_TO_LANGUAGE_NAME;
+        $reverse_lanugage_lookup = array_flip($LANGUAGE_CODE_TO_LANGUAGE_NAME);
+        function mk_google_translate_link($page, $to, $from = 'en') {
+            //mk_google_translate_link("/new/index.php", 'de');
+            // http://translate.google.com/translate?u=http%3A%2F%2Fphet.colorado.edu%2Fnew%2Findex.php&hl=en&ie=UTF8&sl=en&tl=de
+            $link = "http://translate.google.com/translate?u=";
+            $link .= urlencode("http://".PHET_DOMAIN_NAME.$page);
+            $link .= "&amp;hl=en";
+            $link .= "&amp;ie=UTF8";
+            $link .= "&amp;sl={$from}";
+            $link .= "&amp;tl={$to}";
+            return $link;
+            //var_dump($link);
+            //var_dump("http://translate.google.com/translate?u=http%3A%2F%2Fphet2.colorado.edu%2Fnew%2Findex.php&amp;hl=en&amp;ie=UTF8&amp;sl=en&amp;tl=de");
+        }
+        function mk_google_translate_link2($page, $to, $from = 'en') {
+            // http://209.85.171.104/translate_c?hl=en&sl=en&tl=pt&u=http://phet2.colorado.edu/simulations/translations.php#Portuguese
+            //$link = "http://translate.google.com/translate?";
+            $link = "http://209.85.171.104/translate_c?";
+            $link .= "hl=en";
+            $link .= "&amp;sl={$from}";
+            $link .= "&amp;tl={$to}";
+            $link .= "&amp;u=http://".PHET_DOMAIN_NAME.$page;// urlencode("http://".PHET_DOMAIN_NAME.$page);
+            return $link;
+        }
+
+        uasort($sim_lang_map, "mycountcmp");
+        print "<ul>\n";
+        foreach ($sim_lang_map as $language_name => $sim_info) {
+            //$google_translation_link = mk_google_translate_link2("/simulations/translations.php#{$language_name}", $reverse_lanugage_lookup[$language_name]);
+            //print "<li><a href=\"#{$language_name}\">{$language_name}</a> - (".count($sim_info)." translations) - <a href=\"{$google_translation_link}\">Translation in Google</a></li>\n";
+            print "<li><a href=\"#{$language_name}\">{$language_name}</a> - (".count($sim_info)." translations)</li>\n";
+        }
+        print "</ul>\n";
+
+        foreach ($sim_lang_map as $language_name => $sim_info) {
+            $language_icon_src = sim_get_language_icon_url_from_language_name($language_name);
+
+            print "<h1 id=\"{$language_name}\"><img src=\"{$language_icon_src}\" alt=\"\" title=\"{$language_name}\"/></h1>\n";
+            print "<ul>\n";
+            foreach ($sim_info as $sim_name => $sim_url) {
+                $formatted_sim_name = format_string_for_html($sim_name);
+                print "<li><a href=\"{$sim_url}\">$formatted_sim_name</a></li>\n";
+            }
+            print "</ul>\n";
+        }
+
+    if(false) {
+        $languages = array();
+
+        foreach ($sim_to_translations as $sim_name => $map) {
+            foreach ($map as $language_name => $launch_url) {
+                $languages["$language_name"] = "$language_name";
+            }
+        }
 
         $columns = count($languages) + 1;
 
@@ -161,7 +227,7 @@ EOT;
 
             print "</ul>\n";
         }
-
+    }
         flush();
     }
 
