1c908d8af3daf414f567fba9a64502800cf30998
jxb147 <jxb147@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-06-23 22:49:09 +0000

    Another experiment: Changed the images that were being used for constituent particles to instead be simple changes.  This doesn't look good, and is just an experiment for now.

--- trunk/simulations-java/simulations/nuclear-physics/src/edu/colorado/phet/nuclearphysics/view/AlphaParticleNode.java
+++ trunk/simulations-java/simulations/nuclear-physics/src/edu/colorado/phet/nuclearphysics/view/AlphaParticleNode.java
@@ -2,11 +2,15 @@
 
 package edu.colorado.phet.nuclearphysics.view;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.geom.Ellipse2D;
 import java.util.Random;
 
 import edu.colorado.phet.nuclearphysics.NuclearPhysicsResources;
 import edu.colorado.phet.nuclearphysics.model.AlphaParticle;
 import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.nodes.PPath;
 
 /**
  * AlphaParticleNode - This class is used to represent an alpha particle in
@@ -18,7 +22,7 @@ public class AlphaParticleNode extends PNode {
     
     private final static double PARTICLE_DIAMETER = 3.2d;  // Femto meters.
     
-    private PNode _displayImage;
+    private PPath _displayImage;
     private AlphaParticle _alphaParticle;
     
     public AlphaParticleNode(AlphaParticle alphaParticle)
@@ -29,13 +33,17 @@ public class AlphaParticleNode extends PNode {
         // Randomly choose an image for this particle.  This is done to give
         // the nucleus a more random and thus realistic look.
         if (rand.nextDouble() > 0.5){
-           _displayImage = NuclearPhysicsResources.getImageNode("Alpha Particle 001.jpg");
+//           _displayImage = NuclearPhysicsResources.getImageNode("Alpha Particle 001.jpg");
+            _displayImage = new PPath(new Ellipse2D.Double(0,0,PARTICLE_DIAMETER * 1.5, PARTICLE_DIAMETER));
         }
         else {
-            _displayImage = NuclearPhysicsResources.getImageNode("Alpha Particle 002.jpg");            
+//            _displayImage = NuclearPhysicsResources.getImageNode("Alpha Particle 002.jpg");            
+            _displayImage = new PPath(new Ellipse2D.Double(0,0,PARTICLE_DIAMETER, PARTICLE_DIAMETER * 1.5));
         }
         
-        _displayImage.scale( PARTICLE_DIAMETER/((_displayImage.getWidth() + _displayImage.getHeight()) / 2));
+//        _displayImage.scale( PARTICLE_DIAMETER/((_displayImage.getWidth() + _displayImage.getHeight()) / 2));
+        _displayImage.setPaint( Color.BLACK );
+        _displayImage.setStroke( new BasicStroke(0.1f) );
         addChild(_displayImage);
         alphaParticle.addListener(new AlphaParticle.Listener(){
             public void positionChanged(AlphaParticle alpha)
