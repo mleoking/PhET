2d0c068ddf79bc0a05057cc9be4ae9da76df3fd8
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-06-25 19:26:21 +0000

    - set alpha the same for all liquid colors - update todo list

--- trunk/simulations-java/simulations/ph-scale/src/edu/colorado/phet/phscale/model/LiquidDescriptor.java
+++ trunk/simulations-java/simulations/ph-scale/src/edu/colorado/phet/phscale/model/LiquidDescriptor.java
@@ -11,19 +11,21 @@ import edu.colorado.phet.phscale.PHScaleStrings;
 
 public class LiquidDescriptor {
     
+    private static final int ALPHA = 127;
+    
     // water is a special singleton
     public static class WaterDescriptor extends LiquidDescriptor {
         private WaterDescriptor() {
-            super(  PHScaleStrings.CHOICE_WATER, 7, new Color( 200, 200, 255, 150 ) );
+            super(  PHScaleStrings.CHOICE_WATER, 7, new Color( 200, 200, 255, ALPHA ) );
         }
     }
     public static final WaterDescriptor WATER = new WaterDescriptor();
     
     // singletons for each liquid type
-    public static final LiquidDescriptor MILK = new LiquidDescriptor( PHScaleStrings.CHOICE_MILK, 6.5, new Color( 255, 255, 255, 150 ) );
-    public static final LiquidDescriptor BEER = new LiquidDescriptor( PHScaleStrings.CHOICE_BEER, 4.5, new Color( 185, 79, 5, 150 ) );
-    public static final LiquidDescriptor COLA = new LiquidDescriptor( PHScaleStrings.CHOICE_COLA, 2.5, new Color( 122, 60, 35, 150 ) );
-    public static final LiquidDescriptor LEMON_JUICE = new LiquidDescriptor( PHScaleStrings.CHOICE_LEMON_JUICE, 2.4, new Color(255, 255, 0, 150 ) );
+    public static final LiquidDescriptor MILK = new LiquidDescriptor( PHScaleStrings.CHOICE_MILK, 6.5, new Color( 255, 255, 255, ALPHA ) );
+    public static final LiquidDescriptor BEER = new LiquidDescriptor( PHScaleStrings.CHOICE_BEER, 4.5, new Color( 185, 79, 5, ALPHA ) );
+    public static final LiquidDescriptor COLA = new LiquidDescriptor( PHScaleStrings.CHOICE_COLA, 2.5, new Color( 122, 60, 35, ALPHA ) );
+    public static final LiquidDescriptor LEMON_JUICE = new LiquidDescriptor( PHScaleStrings.CHOICE_LEMON_JUICE, 2.4, new Color(255, 255, 0, ALPHA ) );
     
     // all choices except water
     private static final LiquidDescriptor[] CHOICES = new LiquidDescriptor[] { MILK, BEER, COLA, LEMON_JUICE };
@@ -37,13 +39,11 @@ public class LiquidDescriptor {
     private final String _name;
     private final double _pH;
     private final Color _color;
-    private final java.util.ArrayList _listeners;
 
     protected LiquidDescriptor( String name, double pH, Color color ) {
         _name = name;
         _pH = pH;
         _color = color;
-        _listeners = new ArrayList();
     }
     
     public String getName() {
