320b9ca076571582271a59c6b08c0862a323a1aa
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-07-01 18:43:12 +0000

    layout fixes

--- simulations-java/simulations/eating-and-exercise/src/edu/colorado/phet/eatingandexercise/module/eatingandexercise/EatingAndExerciseRenderingSizeStrategy.java
+++ simulations-java/simulations/eating-and-exercise/src/edu/colorado/phet/eatingandexercise/module/eatingandexercise/EatingAndExerciseRenderingSizeStrategy.java
@@ -19,10 +19,19 @@ public class EatingAndExerciseRenderingSizeStrategy implements PhetPCanvas.Trans
     //todo: remove magic numbers in layout
     public AffineTransform getTransform() {
         double availableHeight = canvas.getAvailableWorldHeight();
+        double availableWidth = canvas.getAvailableWorldWidth();
         double maxVisibleHeight = EatingAndExerciseUnits.feetToMeters( 7.5 );//extra padding for scale node
-        double scale = availableHeight / maxVisibleHeight;
+        double maxVisibleWidth = EatingAndExerciseUnits.feetToMeters( 6 );//extra padding for scale node
+        double scaleVert = availableHeight / maxVisibleHeight;
+        double scaleHoriz = availableWidth / maxVisibleWidth;
+        System.out.println( "scaleVert = " + scaleVert );
+        System.out.println( "scaleHoriz = " + scaleHoriz );
+
+        double scale = Math.min( scaleHoriz, scaleVert );
+        System.out.println( "scale = " + scale );
+
         AffineTransform transform = AffineTransform.getScaleInstance( scale, scale );
-        transform.translate( maxVisibleHeight * 0.4,//center of human graphic is at x=0, translate onscreen
+        transform.translate( maxVisibleHeight * 0.36,//center of human graphic is at x=0, translate onscreen
                              EatingAndExerciseUnits.feetToMeters( 6.5 ) );//translate down a bit to keep the scale onscreen
         return transform;
     }
