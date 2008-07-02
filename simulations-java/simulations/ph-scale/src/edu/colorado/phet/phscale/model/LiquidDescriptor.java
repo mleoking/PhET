05b646f7ab6a0ec95071f9a8d07a262a503f750c
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-07-02 17:30:56 +0000

    7/2/08 - sort liquids by decreasing pH in liquid combo box 7/2/08 - use on color for all of the liquids that don't have an assigned color

--- simulations-java/simulations/ph-scale/src/edu/colorado/phet/phscale/model/LiquidDescriptor.java
+++ simulations-java/simulations/ph-scale/src/edu/colorado/phet/phscale/model/LiquidDescriptor.java
@@ -24,30 +24,33 @@ public class LiquidDescriptor {
     
     private static final DecimalFormat PH_FORMAT = new DecimalFormat( "0.0" );
     
+    private static final Color UNASSIGNED_ACID_COLOR = ColorUtils.createColor( PHScaleConstants.H3O_COLOR, ALPHA );
+    private static final Color UNASSIGNED_BASE_COLOR = ColorUtils.createColor( PHScaleConstants.OH_COLOR, ALPHA );
+    
     // instances for each liquid type
-    public static final LiquidDescriptor BATTERY_ACID = new LiquidDescriptor( PHScaleStrings.CHOICE_BATTERY_ACID, 1, new Color( 255, 0, 0, ALPHA ) );
-    public static final LiquidDescriptor BILE = new LiquidDescriptor( PHScaleStrings.CHOICE_BILE, 2, new Color( 0, 255, 0, ALPHA ) );
+    public static final LiquidDescriptor BATTERY_ACID = new LiquidDescriptor( PHScaleStrings.CHOICE_BATTERY_ACID, 1, UNASSIGNED_ACID_COLOR );
+    public static final LiquidDescriptor BILE = new LiquidDescriptor( PHScaleStrings.CHOICE_BILE, 2, UNASSIGNED_ACID_COLOR );
     public static final LiquidDescriptor LEMON_JUICE = new LiquidDescriptor( PHScaleStrings.CHOICE_LEMON_JUICE, 2.4, new Color( 255, 255, 0, ALPHA ) );
-    public static final LiquidDescriptor COLA = new LiquidDescriptor( PHScaleStrings.CHOICE_COLA, 2.5, new Color( 122, 60, 35, ALPHA ) );
-    public static final LiquidDescriptor VINEGAR = new LiquidDescriptor( PHScaleStrings.CHOICE_VINEGAR, 2.9, new Color( 185, 79, 5, ALPHA ) );
-    public static final LiquidDescriptor ORANGE_JUICE = new LiquidDescriptor( PHScaleStrings.CHOICE_ORANGE_JUICE, 3.5, new Color( 185, 79, 5, ALPHA ) );
+    public static final LiquidDescriptor COLA = new LiquidDescriptor( PHScaleStrings.CHOICE_COLA, 2.5, UNASSIGNED_ACID_COLOR );
+    public static final LiquidDescriptor VINEGAR = new LiquidDescriptor( PHScaleStrings.CHOICE_VINEGAR, 2.9, UNASSIGNED_ACID_COLOR );
+    public static final LiquidDescriptor ORANGE_JUICE = new LiquidDescriptor( PHScaleStrings.CHOICE_ORANGE_JUICE, 3.5, new Color( 255, 186, 0, ALPHA ) );
     public static final LiquidDescriptor BEER = new LiquidDescriptor( PHScaleStrings.CHOICE_BEER, 4.5, new Color( 185, 79, 5, ALPHA ) );
-    public static final LiquidDescriptor COFFEE = new LiquidDescriptor( PHScaleStrings.CHOICE_COFFEE, 5.0, new Color( 185, 79, 5, ALPHA ) );
-    public static final LiquidDescriptor TEA = new LiquidDescriptor( PHScaleStrings.CHOICE_TEA, 5.5, new Color( 255, 255, 255, ALPHA ) );
-    public static final LiquidDescriptor ACID_RAIN = new LiquidDescriptor( PHScaleStrings.CHOICE_ACID_RAIN, 5.6, new Color( 255, 255, 255, ALPHA ) );
+    public static final LiquidDescriptor COFFEE = new LiquidDescriptor( PHScaleStrings.CHOICE_COFFEE, 5.0, UNASSIGNED_ACID_COLOR );
+    public static final LiquidDescriptor TEA = new LiquidDescriptor( PHScaleStrings.CHOICE_TEA, 5.5, UNASSIGNED_ACID_COLOR );
+    public static final LiquidDescriptor ACID_RAIN = new LiquidDescriptor( PHScaleStrings.CHOICE_ACID_RAIN, 5.6, UNASSIGNED_ACID_COLOR );
     public static final LiquidDescriptor MILK = new LiquidDescriptor( PHScaleStrings.CHOICE_MILK, 6.5, new Color( 255, 255, 255, ALPHA ) );
     public static final LiquidDescriptor WATER = new LiquidDescriptor( PHScaleStrings.CHOICE_WATER, 7, ColorUtils.createColor( PHScaleConstants.H2O_COLOR, ALPHA ) );
-    public static final LiquidDescriptor SALIVA = new LiquidDescriptor( PHScaleStrings.CHOICE_SALIVA, 6.5, new Color( 255, 255, 255, ALPHA ) );
+    public static final LiquidDescriptor SALIVA = new LiquidDescriptor( PHScaleStrings.CHOICE_SALIVA, 7.4, UNASSIGNED_BASE_COLOR );
     public static final LiquidDescriptor BLOOD = new LiquidDescriptor( PHScaleStrings.CHOICE_MILK, 7.4, new Color( 255, 0, 0, ALPHA ) );
-    public static final LiquidDescriptor SEA_WATER = new LiquidDescriptor( PHScaleStrings.CHOICE_SEA_WATER, 7.4, new Color( 255, 0, 0, ALPHA ) );
-    public static final LiquidDescriptor HAND_SOAP = new LiquidDescriptor( PHScaleStrings.CHOICE_HAND_SOAP, 10, new Color( 255, 255, 255, ALPHA ) );
-    public static final LiquidDescriptor AMMONIA = new LiquidDescriptor( PHScaleStrings.CHOICE_AMMONIA, 11.5, new Color( 255, 255, 255, ALPHA ) );
-    public static final LiquidDescriptor BLEACH = new LiquidDescriptor( PHScaleStrings.CHOICE_BLEACH, 12.5, new Color( 255, 255, 255, ALPHA ) );
-    public static final LiquidDescriptor DRAIN_CLEANER = new LiquidDescriptor( PHScaleStrings.CHOICE_DRAIN_CLEANER, 13, new Color( 255, 255, 255, ALPHA ) );
-    public static final LiquidDescriptor LYE = new LiquidDescriptor( PHScaleStrings.CHOICE_LYE, 13.5, new Color( 255, 255, 255, ALPHA ) );
+    public static final LiquidDescriptor SEA_WATER = new LiquidDescriptor( PHScaleStrings.CHOICE_SEA_WATER, 7.4, UNASSIGNED_BASE_COLOR );
+    public static final LiquidDescriptor HAND_SOAP = new LiquidDescriptor( PHScaleStrings.CHOICE_HAND_SOAP, 10, UNASSIGNED_BASE_COLOR );
+    public static final LiquidDescriptor AMMONIA = new LiquidDescriptor( PHScaleStrings.CHOICE_AMMONIA, 11.5, UNASSIGNED_BASE_COLOR );
+    public static final LiquidDescriptor BLEACH = new LiquidDescriptor( PHScaleStrings.CHOICE_BLEACH, 12.5, UNASSIGNED_BASE_COLOR );
+    public static final LiquidDescriptor DRAIN_CLEANER = new LiquidDescriptor( PHScaleStrings.CHOICE_DRAIN_CLEANER, 13, UNASSIGNED_BASE_COLOR );
+    public static final LiquidDescriptor LYE = new LiquidDescriptor( PHScaleStrings.CHOICE_LYE, 13.5, UNASSIGNED_BASE_COLOR );
     
-    // all instances except water
-    private static final LiquidDescriptor[] ALL_INSTANCES = new LiquidDescriptor[] { 
+    // all instances
+    private static final LiquidDescriptor[] ALL_INSTANCES = new LiquidDescriptor[] {
         BATTERY_ACID, BILE, LEMON_JUICE, COLA, VINEGAR, ORANGE_JUICE, BEER, COFFEE,
         TEA, ACID_RAIN, MILK, WATER, SALIVA, BLOOD, SEA_WATER, HAND_SOAP, AMMONIA,
         BLEACH, DRAIN_CLEANER, LYE
