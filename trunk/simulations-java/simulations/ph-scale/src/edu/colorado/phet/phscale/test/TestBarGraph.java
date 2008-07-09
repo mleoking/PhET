dccf30b863360df32a97dc7b92b549cea06a5383
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-07-09 22:14:41 +0000

    add sample chart

--- trunk/simulations-java/simulations/ph-scale/src/edu/colorado/phet/phscale/test/TestBarGraph.java
+++ trunk/simulations-java/simulations/ph-scale/src/edu/colorado/phet/phscale/test/TestBarGraph.java
@@ -12,6 +12,17 @@ import javax.swing.JSlider;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.ClusteredXYBarRenderer;
+import org.jfree.data.time.Day;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.time.TimeSeriesCollection;
+import org.jfree.data.xy.IntervalXYDataset;
+
 import edu.colorado.phet.common.phetcommon.view.util.EasyGridBagLayout;
 
 /**
@@ -24,7 +35,9 @@ public class TestBarGraph extends JFrame {
     public TestBarGraph() {
         super( "TestBarGraph" );
         
-        JPanel chartPanel = new JPanel();
+        IntervalXYDataset dataset = createDataset();
+        JFreeChart chart = createChart( dataset );
+        ChartPanel chartPanel = new ChartPanel( chart );
         
         JLabel pHLabel = new JLabel( "pH:" );
         
@@ -53,6 +66,53 @@ public class TestBarGraph extends JFrame {
         panel.add( controlPanel, BorderLayout.SOUTH );
         
         getContentPane().add( panel );
+    }
+    
+    private static IntervalXYDataset createDataset() {
+        
+        TimeSeries series1 = new TimeSeries("Series 1", Day.class);
+        series1.add(new Day(1, 1, 2003), 54.3);
+        series1.add(new Day(2, 1, 2003), 20.3);
+        series1.add(new Day(3, 1, 2003), 43.4);
+        series1.add(new Day(4, 1, 2003), -12.0);
+
+        TimeSeries series2 = new TimeSeries("Series 2", Day.class);
+        series2.add(new Day(1, 1, 2003), 8.0);
+        series2.add(new Day(2, 1, 2003), 16.0);
+        series2.add(new Day(3, 1, 2003), 21.0);
+        series2.add(new Day(4, 1, 2003), 5.0);
+
+        TimeSeriesCollection dataset = new TimeSeriesCollection();
+        dataset.addSeries(series1);
+        dataset.addSeries(series2);
+        return dataset;
+    }
+    
+    /** 
+     * Creates a chart.
+     * 
+     * @param dataset  the dataset.
+     * 
+     * @return The chart.
+     */
+    private static JFreeChart createChart( IntervalXYDataset dataset ) {
+        
+        JFreeChart chart = ChartFactory.createXYBarChart(
+            "",      // chart title
+            "X",  // domain (x) axis label
+            true,
+            "Y",     // range (y) axis label
+            dataset,
+            PlotOrientation.VERTICAL,
+            false,  // legend
+            false, // tooltips
+            false // urls
+        );
+
+        XYPlot plot = (XYPlot) chart.getPlot(); 
+        ClusteredXYBarRenderer r = new ClusteredXYBarRenderer();
+        plot.setRenderer(r);
+        return chart;        
     }
     
     public static void main( String args[] ) {
