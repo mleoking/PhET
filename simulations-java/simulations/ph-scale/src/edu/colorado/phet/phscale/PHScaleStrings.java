abf29be9d38caa00fab025416fb541eb72b0bed2
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-07-10 03:36:40 +0000

    7/9/08 - add Shapes and nodes for bars, but shapes are not updated yet 7/9/08 - add zoom buttons to bar graph, wire them up to tick labels

--- simulations-java/simulations/ph-scale/src/edu/colorado/phet/phscale/PHScaleStrings.java
+++ simulations-java/simulations/ph-scale/src/edu/colorado/phet/phscale/PHScaleStrings.java
@@ -23,6 +23,8 @@ public class PHScaleStrings {
     public static final String BUTTON_YES = PHScaleResources.getCommonString( PhetCommonResources.STRING_YES );
     public static final String BUTTON_NO = PHScaleResources.getCommonString( PhetCommonResources.STRING_NO );
     public static final String BUTTON_RESET_ALL = PHScaleResources.getCommonString( PhetCommonResources.STRING_RESET_ALL );
+    public static final String BUTTON_ZOOM_IN = PHScaleResources.getString( "button.zoomIn" );
+    public static final String BUTTON_ZOOM_OUT = PHScaleResources.getString( "button.zoomOut" );
 
     public static final String CHECK_BOX_MOLECULE_COUNT = PHScaleResources.getString( "checkBox.moleculeCount" );
     private static final String CHECK_BOX_H3O_OH_RATIO = PHScaleResources.getString( "checkBox.H3O_OH_ratio" );
