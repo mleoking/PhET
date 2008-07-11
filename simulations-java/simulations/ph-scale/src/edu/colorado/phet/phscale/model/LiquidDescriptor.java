913cbfb6c51425db83e7434de0585e0b152d999b
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-07-11 16:12:57 +0000

    7/11/08 - change liquid colors 7/11/08 - replace cola with lime soda

--- simulations-java/simulations/ph-scale/src/edu/colorado/phet/phscale/model/LiquidDescriptor.java
+++ simulations-java/simulations/ph-scale/src/edu/colorado/phet/phscale/model/LiquidDescriptor.java
@@ -20,29 +20,27 @@ public class LiquidDescriptor {
     // Class data
     //----------------------------------------------------------------------------
     
-    private static final int ALPHA = 127;
-    
     private static final DecimalFormat PH_FORMAT = new DecimalFormat( "0.0" );
     
     // instances for each liquid type
-    private static final LiquidDescriptor DRAIN_CLEANER = new LiquidDescriptor( PHScaleStrings.CHOICE_DRAIN_CLEANER, 13, new Color( 255, 255, 0, ALPHA ) );
-    private static final LiquidDescriptor HAND_SOAP = new LiquidDescriptor( PHScaleStrings.CHOICE_HAND_SOAP, 10, new Color( 204, 0, 204, ALPHA ) );
-    private static final LiquidDescriptor BLOOD = new LiquidDescriptor( PHScaleStrings.CHOICE_BLOOD, 7.4, new Color( 255, 0, 0, ALPHA ) );
-    private static final LiquidDescriptor SPIT = new LiquidDescriptor( PHScaleStrings.CHOICE_SPIT, 7.4, new Color( 255, 255, 255, ALPHA ) );
+    private static final LiquidDescriptor DRAIN_CLEANER = new LiquidDescriptor( PHScaleStrings.CHOICE_DRAIN_CLEANER, 13, new Color( 255, 255, 0, 150 ) );
+    private static final LiquidDescriptor HAND_SOAP = new LiquidDescriptor( PHScaleStrings.CHOICE_HAND_SOAP, 10, new Color( 204, 0, 204, 90 ) );
+    private static final LiquidDescriptor BLOOD = new LiquidDescriptor( PHScaleStrings.CHOICE_BLOOD, 7.4, new Color( 185, 12, 0, 150 ) );
+    private static final LiquidDescriptor SPIT = new LiquidDescriptor( PHScaleStrings.CHOICE_SPIT, 7.4, new Color( 204, 204, 198, 73 ) );
     private static final LiquidDescriptor WATER = new LiquidDescriptor( PHScaleStrings.CHOICE_WATER, 7, new Color( 193, 222, 227, 127 ) );
-    private static final LiquidDescriptor MILK = new LiquidDescriptor( PHScaleStrings.CHOICE_MILK, 6.5, new Color( 255, 255, 255, ALPHA ) );
-    private static final LiquidDescriptor COFFEE = new LiquidDescriptor( PHScaleStrings.CHOICE_COFFEE, 5.0, new Color( 74, 44, 30, ALPHA ) );
-    private static final LiquidDescriptor BEER = new LiquidDescriptor( PHScaleStrings.CHOICE_BEER, 4.5, new Color( 185, 79, 5, ALPHA ) );
-    private static final LiquidDescriptor COLA = new LiquidDescriptor( PHScaleStrings.CHOICE_COLA, 2.5, new Color( 179, 119, 87, ALPHA ) );
-    private static final LiquidDescriptor VOMIT = new LiquidDescriptor( PHScaleStrings.CHOICE_VOMIT, 2, new Color( 0, 255, 0, ALPHA ) );
-    private static final LiquidDescriptor BATTERY_ACID = new LiquidDescriptor( PHScaleStrings.CHOICE_BATTERY_ACID, 1, new Color( 255, 255, 0, ALPHA ) );
+    private static final LiquidDescriptor MILK = new LiquidDescriptor( PHScaleStrings.CHOICE_MILK, 6.5, new Color( 255, 255, 255, 180 ) );
+    private static final LiquidDescriptor COFFEE = new LiquidDescriptor( PHScaleStrings.CHOICE_COFFEE, 5.0, new Color( 164, 99, 7, 127 ) );
+    private static final LiquidDescriptor BEER = new LiquidDescriptor( PHScaleStrings.CHOICE_BEER, 4.5, new Color( 255, 200, 0, 127 ) );
+    private static final LiquidDescriptor LIME_SODA = new LiquidDescriptor( PHScaleStrings.LIME_SODA, 2.5, new Color( 204, 255, 102, 162 ) );
+    private static final LiquidDescriptor VOMIT = new LiquidDescriptor( PHScaleStrings.CHOICE_VOMIT, 2, new Color( 255, 171, 120, 183 ) );
+    private static final LiquidDescriptor BATTERY_ACID = new LiquidDescriptor( PHScaleStrings.CHOICE_BATTERY_ACID, 1, new Color( 255, 255, 0, 127 ) );
     
     public static class CustomLiquidDescriptor extends LiquidDescriptor {
         
         private final double _defaultPH;
         
         private CustomLiquidDescriptor( ) {
-            super( PHScaleStrings.CHOICE_CUSTOM, 7, new Color( 255, 255, 156, ALPHA )  );
+            super( PHScaleStrings.CHOICE_CUSTOM, 7, new Color( 255, 255, 156, 127 )  );
             _defaultPH = getPH();
         }
         
@@ -65,7 +63,7 @@ public class LiquidDescriptor {
     
     // all instances
     private static final LiquidDescriptor[] ALL_INSTANCES = new LiquidDescriptor[] {
-        DRAIN_CLEANER, HAND_SOAP, BLOOD, SPIT, WATER, MILK, COFFEE, BEER, COLA, VOMIT, BATTERY_ACID, CUSTOM /* put Custom last! */
+        DRAIN_CLEANER, HAND_SOAP, BLOOD, SPIT, WATER, MILK, COFFEE, BEER, LIME_SODA, VOMIT, BATTERY_ACID, CUSTOM /* put Custom last! */
     };
     
     //----------------------------------------------------------------------------
