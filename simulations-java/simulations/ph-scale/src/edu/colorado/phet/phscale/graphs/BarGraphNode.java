7c6de7d1755214f47d9bc9411ca0dfa31b3c9225
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-07-12 22:20:10 +0000

    7/12/08 - move origin to the geometric center of drag handle 7/12/08 - fix horizontal positioning of bars and their drag handles 7/12/08 - if a bar is taller than the chart, give it an arrowhead and hide its drag handle 7/12/08 - add stubs to determine bar heights from model and graph settings

--- simulations-java/simulations/ph-scale/src/edu/colorado/phet/phscale/graphs/BarGraphNode.java
+++ simulations-java/simulations/ph-scale/src/edu/colorado/phet/phscale/graphs/BarGraphNode.java
@@ -42,8 +42,12 @@ public class BarGraphNode extends PNode {
     private static final Color OUTLINE_FILL_COLOR = Color.WHITE;
     
     // bars
+    private static final double BAR_WIDTH = 50;
     private static final Stroke BAR_STROKE = null;
     private static final Color BAR_STROKE_COLOR = Color.BLACK;
+    private static final double BAR_ARROW_HEAD_WIDTH = BAR_WIDTH + 15;
+    private static final double BAR_ARROW_HEAD_HEIGHT = 60;
+    private static final double BAR_ARROW_PERCENT_ABOVE_GRAPH = 0.15;
     
     // numeric values
     private static final Font VALUE_FONT = new PhetFont( Font.BOLD, 16 );
@@ -90,7 +94,7 @@ public class BarGraphNode extends PNode {
     private final Liquid _liquid;
     private final LiquidListener _liquidListener;
     
-    private final PPath _graphOutlineNode;
+    private final PDimension _graphOutlineSize;
     private final FormattedNumberNode _h3oNumberNode, _ohNumberNode, _h2oNumberNode;
     private final LogYAxisNode _logYAxisNode;
     private final LinearYAxisNode _linearYAxisNode;
@@ -111,6 +115,7 @@ public class BarGraphNode extends PNode {
     
     public BarGraphNode( PDimension graphOutlineSize, Liquid liquid ) {
         
+        _graphOutlineSize = new PDimension( graphOutlineSize );
         _logScale = true;
         _concentrationUnits = true;
         _linearTicksExponent = BIGGEST_LINEAR_TICK_EXPONENT;
@@ -124,25 +129,16 @@ public class BarGraphNode extends PNode {
         };
         _liquid.addLiquidListener( _liquidListener );
         
+        // graphOutlineNode is not instance data because we do NOT want to use its bounds for calculations.
+        // It's stroke width will cause calculation errors.  Use _graphOutlineSize instead.
         Rectangle2D r = new Rectangle2D.Double( 0, 0, graphOutlineSize.getWidth(), graphOutlineSize.getHeight() );
-        _graphOutlineNode = new PPath( r );
-        _graphOutlineNode.setStroke( OUTLINE_STROKE );
-        _graphOutlineNode.setStrokePaint( OUTLINE_STROKE_COLOR );
-        _graphOutlineNode.setPaint( OUTLINE_FILL_COLOR );
-        _graphOutlineNode.setPickable( false );
-        _graphOutlineNode.setChildrenPickable( false );
-        addChild( _graphOutlineNode );
-        
-        _h3oNumberNode = createNumberNode( H3O_FORMAT );
-        addChild( _h3oNumberNode );
-        
-        _ohNumberNode = createNumberNode( OH_FORMAT );
-        addChild( _ohNumberNode );
-        
-        _h2oNumberNode = createNumberNode( H2O_FORMAT );
-        addChild( _h2oNumberNode );
-        
-        updateValues(); // do this before setting offsets so that bounds are reasonable
+        PPath graphOutlineNode = new PPath( r );
+        graphOutlineNode.setStroke( OUTLINE_STROKE );
+        graphOutlineNode.setStrokePaint( OUTLINE_STROKE_COLOR );
+        graphOutlineNode.setPaint( OUTLINE_FILL_COLOR );
+        graphOutlineNode.setPickable( false );
+        graphOutlineNode.setChildrenPickable( false );
+        addChild( graphOutlineNode );
         
         _logYAxisNode = new LogYAxisNode( graphOutlineSize, NUMBER_OF_LOG_TICKS, TICKS_TOP_MARGIN, 
                 BIGGEST_LOG_TICK_EXPONENT,  LOG_TICK_EXPONENT_SPACING, TICK_LENGTH,
@@ -182,6 +178,17 @@ public class BarGraphNode extends PNode {
         _h2oBarNode.setStrokePaint( BAR_STROKE_COLOR );
         addChild( _h2oBarNode );
         
+        _h3oNumberNode = createNumberNode( H3O_FORMAT );
+        addChild( _h3oNumberNode );
+        
+        _ohNumberNode = createNumberNode( OH_FORMAT );
+        addChild( _ohNumberNode );
+        
+        _h2oNumberNode = createNumberNode( H2O_FORMAT );
+        addChild( _h2oNumberNode );
+        
+        updateValues(); // do this before setting offsets so that bounds are reasonable
+        
         // Zoom controls
         {
             _zoomInButton = new JButton( PHScaleStrings.BUTTON_ZOOM_IN );
@@ -218,20 +225,25 @@ public class BarGraphNode extends PNode {
         _ohDragHandleNode = new BarDragHandleNode();
         addChild( _ohDragHandleNode );
         
-        _graphOutlineNode.setOffset( 0, 0 );
-        PBounds gob = _graphOutlineNode.getFullBoundsReference();
-        _logYAxisNode.setOffset( _graphOutlineNode.getOffset() );
-        _linearYAxisNode.setOffset( _graphOutlineNode.getOffset() );
-        final double xH3O = 0.25 * gob.getWidth();
-        final double xOH = 0.5 * gob.getWidth();
-        final double xH2O = 0.75 * gob.getWidth();
+        // layout
+        graphOutlineNode.setOffset( 0, 0 );
+        PBounds gob = graphOutlineNode.getFullBoundsReference();
+        _logYAxisNode.setOffset( graphOutlineNode.getOffset() );
+        _linearYAxisNode.setOffset( graphOutlineNode.getOffset() );
+        final double xMargin = ( graphOutlineNode.getWidth() - ( 3 * BAR_WIDTH ) ) / 12;
+        assert( xMargin > 0 );
+        final double xH3O = ( 1./6.) * gob.getWidth() + xMargin;
+        final double xOH = ( 3./6. ) * gob.getWidth();
+        final double xH2O = ( 5./6.) * gob.getWidth() - xMargin;
         _h3oNumberNode.setOffset( xH3O - _h3oNumberNode.getFullBoundsReference().getWidth()/2, gob.getMaxY() - _h3oNumberNode.getFullBoundsReference().getHeight() - VALUE_Y_MARGIN );
         _ohNumberNode.setOffset( xOH - _ohNumberNode.getFullBoundsReference().getWidth()/2, gob.getMaxY() - _ohNumberNode.getFullBoundsReference().getHeight() - VALUE_Y_MARGIN );
         _h2oNumberNode.setOffset( xH2O - _h2oNumberNode.getFullBoundsReference().getWidth()/2, gob.getMaxY() - _h2oNumberNode.getFullBoundsReference().getHeight() - VALUE_Y_MARGIN );
-        //XXX set offsets for bars
+        _h3oBarNode.setOffset( xH3O, graphOutlineSize.getHeight() );
+        _ohBarNode.setOffset( xOH, graphOutlineSize.getHeight() );
+        _h2oBarNode.setOffset( xH2O, graphOutlineSize.getHeight() );
         _zoomPanelWrapper.setOffset( gob.getCenterX() - _zoomPanelWrapper.getFullBoundsReference().getWidth()/2, 30 );
-        _h3oDragHandleNode.setOffset( xH3O - _h3oDragHandleNode.getFullBoundsReference().getWidth() / 2, graphOutlineSize.getHeight() - _h3oDragHandleNode.getFullBoundsReference().getHeight() / 2 );
-        _ohDragHandleNode.setOffset( xOH - _ohDragHandleNode.getFullBoundsReference().getWidth() / 2, graphOutlineSize.getHeight() - _ohDragHandleNode.getFullBoundsReference().getHeight() / 2 );
+        _h3oDragHandleNode.setOffset( xH3O, graphOutlineSize.getHeight() - _h3oDragHandleNode.getFullBoundsReference().getHeight() / 2 );
+        _ohDragHandleNode.setOffset( xOH, graphOutlineSize.getHeight() - _ohDragHandleNode.getFullBoundsReference().getHeight() / 2 );
 
         updateYAxis();
         updateBars();
@@ -350,40 +362,71 @@ public class BarGraphNode extends PNode {
     
     private void updateBars() {
         
-        _h3oBarShape.reset();
-        _ohBarShape.reset();
-        _h2oBarShape.reset();
+        final double h3oBarLength = getH3OBarLength();
+        final double ohBarLength = getOHBarLength();
+        final double h2oBarLength = getH2OLength();
         
+        final double graphHeight = _graphOutlineSize.getHeight();
+        
+        updateBar( _h3oBarNode, _h3oBarShape, h3oBarLength, graphHeight );
+        updateBar( _ohBarNode, _ohBarShape, ohBarLength, graphHeight );
+        updateBar( _h2oBarNode, _h2oBarShape, h2oBarLength, graphHeight );
+        
+        updateDragHandle( _h3oDragHandleNode, h3oBarLength, graphHeight );
+        updateDragHandle( _ohDragHandleNode, ohBarLength, graphHeight );
+    }
+    
+    private double getH3OBarLength() {
+        double length = 0;
         if ( !_liquid.isEmpty() ) {
-            if ( _logScale ) {
-                updateBarShapesLog();
-            }
-            else { /* linear */
-                updateBarsShapesLinear();
-            }
+            length = 150; //XXX
         }
-        
-        _h3oBarNode.setPathTo( _h3oBarShape );
-        _ohBarNode.setPathTo( _ohBarShape );
-        _h2oBarNode.setPathTo( _h2oBarShape );
+        return length;
     }
     
-    private void updateBarShapesLog() {
-        if ( _concentrationUnits ) {
-            //XXX
+    private double getOHBarLength() {
+        double length = 0;
+        if ( !_liquid.isEmpty() ) {
+            length = 250; //XXX
         }
-        else {
-            //XXX
-        }
+        return length;
     }
     
-    private void updateBarsShapesLinear() {
-        if ( _concentrationUnits ) {
-            //XXX
+    private double getH2OLength() {
+        double length = 0;
+        if ( !_liquid.isEmpty() ) {
+            length = _graphOutlineSize.getHeight() + 10; //XXX
         }
-        else {
-            //XXX
+        return length;
+    }
+    
+    private static void updateBar( PPath barNode, GeneralPath shape, final double barLength, final double graphHeight ) {
+        shape.reset();
+        if ( barLength > graphHeight ) {
+            // if barLength taller than graph, draw bar with arrow at top
+            final double adjustedBarLength = graphHeight - ( ( 1 - BAR_ARROW_PERCENT_ABOVE_GRAPH ) * BAR_ARROW_HEAD_HEIGHT );
+            shape.moveTo( (float) -( BAR_WIDTH / 2 ), 0f );
+            shape.lineTo( (float) ( BAR_WIDTH / 2 ), 0f );
+            shape.lineTo( (float) ( BAR_WIDTH / 2 ), (float) -adjustedBarLength );
+            shape.lineTo( (float) ( BAR_ARROW_HEAD_WIDTH / 2 ), (float) -adjustedBarLength );
+            shape.lineTo( 0f, (float) -( adjustedBarLength + BAR_ARROW_HEAD_HEIGHT ) );
+            shape.lineTo( (float) -( BAR_ARROW_HEAD_WIDTH / 2 ), (float) -adjustedBarLength );
+            shape.lineTo( (float) -( BAR_WIDTH / 2 ), (float) -adjustedBarLength );
+            shape.closePath();
         }
+        else if ( barLength > 0 ) {
+            shape.moveTo( (float) -( BAR_WIDTH / 2 ), 0f );
+            shape.lineTo( (float) ( BAR_WIDTH / 2 ), 0f );
+            shape.lineTo( (float) ( BAR_WIDTH / 2 ), (float) -barLength );
+            shape.lineTo( (float) -( BAR_WIDTH / 2 ), (float) -barLength );
+            shape.closePath();
+        }
+        barNode.setPathTo( shape );
+    }
+    
+    private static void updateDragHandle( PNode dragHandleNode, final double barLength, final double graphHeight ) {
+        dragHandleNode.setVisible( barLength <= graphHeight  );
+        dragHandleNode.setOffset( dragHandleNode.getXOffset(), graphHeight - barLength );
     }
     
     private void updateControls() {
