e8ab1a79af6079dada3c3980282ba9c5b982cd63
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-07-14 04:04:00 +0000

    7/13/08 - move zoom controls to their own class

--- trunk/simulations-java/simulations/ph-scale/src/edu/colorado/phet/phscale/graphs/BarNode.java
+++ trunk/simulations-java/simulations/ph-scale/src/edu/colorado/phet/phscale/graphs/BarNode.java
@@ -10,21 +10,36 @@ import edu.umd.cs.piccolo.nodes.PPath;
 
 /**
  * BarNode is a bar in the bar graph.
+ * The origin is at the bottom center.
+ * If the bar's height exceeds the height of the graph, an arrow head is drawn
+ * on the top of the bar.
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
 public class BarNode extends PPath {
+    
+    //----------------------------------------------------------------------------
+    // Class data
+    //----------------------------------------------------------------------------
     
     private static final Stroke STROKE = null;
     private static final Color STROKE_COLOR = Color.BLACK;
     private static final double ARROW_HEAD_HEIGHT = 60;
     private static final double ARROW_PERCENT_ABOVE_GRAPH = 0.15;
     
+    //----------------------------------------------------------------------------
+    // Instance data
+    //----------------------------------------------------------------------------
+    
     private final double _barWidth;
     private final double _graphHeight;
     private final double _arrowHeadWidth;
     private final GeneralPath _barShape;
 
+    //----------------------------------------------------------------------------
+    // Constructors
+    //----------------------------------------------------------------------------
+    
     public BarNode( double barWidth, Color color, double graphHeight ) {
         
         setPickable( false );
@@ -43,10 +58,16 @@ public class BarNode extends PPath {
         setBarHeight( 1 );
     }
     
+    //----------------------------------------------------------------------------
+    // Setters and getters
+    //----------------------------------------------------------------------------
+    
     public void setBarHeight( final double barHeight ) {
+        
         _barShape.reset();
+        
         if ( barHeight > _graphHeight ) {
-            // if barHeight is taller than graph, draw bar with an arrow at the top
+            // rectangle with arrowhead at the top, origin at bottom center
             final double adjustedBarLength = _graphHeight - ( ( 1 - ARROW_PERCENT_ABOVE_GRAPH ) * ARROW_HEAD_HEIGHT );
             _barShape.moveTo( (float) -( _barWidth / 2 ), 0f );
             _barShape.lineTo( (float) ( _barWidth / 2 ), 0f );
@@ -58,12 +79,14 @@ public class BarNode extends PPath {
             _barShape.closePath();
         }
         else if ( barHeight > 0 ) {
+            // rectangle, origin at bottom center
             _barShape.moveTo( (float) -( _barWidth / 2 ), 0f );
             _barShape.lineTo( (float) ( _barWidth / 2 ), 0f );
             _barShape.lineTo( (float) ( _barWidth / 2 ), (float) -barHeight );
             _barShape.lineTo( (float) -( _barWidth / 2 ), (float) -barHeight );
             _barShape.closePath();
         }
+        
         setPathTo( _barShape );
     }
 }
