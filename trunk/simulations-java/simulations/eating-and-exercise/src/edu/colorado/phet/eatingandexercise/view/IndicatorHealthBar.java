b870486d5750f4c4f314d541af1492c0a9f4dbff
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-07-15 17:46:13 +0000

    cleanup health indicators

--- trunk/simulations-java/simulations/eating-and-exercise/src/edu/colorado/phet/eatingandexercise/view/IndicatorHealthBar.java
+++ trunk/simulations-java/simulations/eating-and-exercise/src/edu/colorado/phet/eatingandexercise/view/IndicatorHealthBar.java
@@ -1,5 +1,7 @@
 package edu.colorado.phet.eatingandexercise.view;
 
+import java.awt.*;
+
 /**
  * Created by: Sam
  * Jun 27, 2008 at 12:16:16 PM
@@ -7,8 +9,8 @@ package edu.colorado.phet.eatingandexercise.view;
 public class IndicatorHealthBar extends HealthBar {
     private HealthLevel healthLevel;
 
-    public IndicatorHealthBar( String name, double min, double max, double minOptimal, double maxOptimal,double viewHeight ) {
-        super( name, min, max, minOptimal, maxOptimal, viewHeight );
+    public IndicatorHealthBar( String name, double min, double max, double minOptimal, double maxOptimal, double viewHeight, Color minColor, Color maxColor ) {
+        super( name, min, max, minOptimal, maxOptimal, viewHeight, minColor, maxColor );
         healthLevel = new HealthLevel( this );
         addChild( healthLevel );
     }
