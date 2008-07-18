8c3c87dee2305c3477888fb5e2319f5ed58b45cc
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-07-18 15:54:33 +0000

    post version with muscle mass conversion

--- simulations-java/simulations/eating-and-exercise/src/edu/colorado/phet/eatingandexercise/model/DefaultHumanUpdate.java
+++ simulations-java/simulations/eating-and-exercise/src/edu/colorado/phet/eatingandexercise/model/DefaultHumanUpdate.java
@@ -9,7 +9,7 @@ public class DefaultHumanUpdate implements HumanUpdate {
 
     public DefaultHumanUpdate() {
         compositeHumanUpdate.add( new MuscleAndFatMassLoss2() );
-//        compositeHumanUpdate.add( new MuscleGainedFromExercising() );
+        compositeHumanUpdate.add( new MuscleGainedFromExercising() );
     }
 
     public void update( Human human, double dt ) {
