735d00834beebe7f176f596afa5e3311add3da1d
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-07-19 03:37:44 +0000

    7/19/08 - move more constants to GlaciersConstants

--- simulations-java/simulations/glaciers/src/edu/colorado/phet/glaciers/view/GlaciersModelViewTransform.java
+++ simulations-java/simulations/glaciers/src/edu/colorado/phet/glaciers/view/GlaciersModelViewTransform.java
@@ -2,18 +2,14 @@
 
 package edu.colorado.phet.glaciers.view;
 
+import edu.colorado.phet.glaciers.GlaciersConstants;
+
 
 public class GlaciersModelViewTransform extends ModelViewTransform {
 
-    // ModelViewTransform (MVT) parameters
-    private static final double MVT_X_SCALE = 0.062; // scale x by this amount when going from model to view
-    private static final double MVT_Y_SCALE = 0.1; // scale y by this amount when going from model to view
-    private static final double MVT_X_OFFSET = 0; // translate x by this amount when going from model to view
-    private static final double MVT_Y_OFFSET = 0; // translate y by this amount when going from model to view
-    private static final boolean MVT_FLIP_SIGN_X = false;
-    private static final boolean MVT_FLIP_SIGN_Y = true;
-    
     public GlaciersModelViewTransform() {
-        super( MVT_X_SCALE, MVT_Y_SCALE, MVT_X_OFFSET, MVT_Y_OFFSET, MVT_FLIP_SIGN_X, MVT_FLIP_SIGN_Y );
+        super( GlaciersConstants.MVT_X_SCALE, GlaciersConstants.MVT_Y_SCALE, 
+                GlaciersConstants.MVT_X_OFFSET, GlaciersConstants.MVT_Y_OFFSET, 
+                GlaciersConstants.MVT_FLIP_SIGN_X, GlaciersConstants.MVT_FLIP_SIGN_Y );
     }
 }
