f47e6c3ffdd4028c9bee2b4f99bdf4c6b46c6ae6
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-07-22 23:36:05 +0000

    7/22/08 - simulate snowfall using a rectangle that covers the valley, filled with a gradient paint, vary alpha based on snowfall (not clipped to region above ice surface)

--- simulations-java/simulations/glaciers/src/edu/colorado/phet/glaciers/view/SnowfallNode.java
+++ simulations-java/simulations/glaciers/src/edu/colorado/phet/glaciers/view/SnowfallNode.java
@@ -2,58 +2,167 @@
 
 package edu.colorado.phet.glaciers.view;
 
-import java.awt.geom.Point2D;
+import java.awt.Color;
+import java.awt.GradientPaint;
+import java.awt.geom.Rectangle2D;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 
+import edu.colorado.phet.common.phetcommon.view.util.ColorUtils;
 import edu.colorado.phet.common.piccolophet.PhetPNode;
 import edu.colorado.phet.glaciers.GlaciersConstants;
 import edu.colorado.phet.glaciers.GlaciersImages;
-import edu.colorado.phet.glaciers.model.Climate;
+import edu.colorado.phet.glaciers.model.Glacier;
 import edu.colorado.phet.glaciers.model.Climate.ClimateAdapter;
-import edu.umd.cs.piccolo.nodes.PImage;
+import edu.colorado.phet.glaciers.model.Climate.ClimateListener;
+import edu.colorado.phet.glaciers.model.Glacier.GlacierAdapter;
+import edu.colorado.phet.glaciers.model.Glacier.GlacierListener;
+import edu.umd.cs.piccolo.nodes.PPath;
 
-
+/**
+ * SnowfallNode is the visual representation of snowfall.
+ * A white gradient is drawn in a rectangle that covers the entire world.
+ * This rectangle is clipped so that only the portion above the surface of
+ * the ice or valley floor (whichever is higher) is drawn.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
 public class SnowfallNode extends PhetPNode {
     
-    private static final double MIN_SCALE = 1;
-    private static final double MAX_SCALE = MIN_SCALE * 5;
+    //----------------------------------------------------------------------------
+    // Class data
+    //----------------------------------------------------------------------------
     
-    private final Climate _climate;
+    private static final Color BASE_COLOR = new Color( 255, 255, 255 );
+    private static final Color NO_SNOW_COLOR = ColorUtils.createColor( BASE_COLOR, 0 );
+    private static final int MAX_ALPHA = 255; // opaque to simulate "white out" conditions at highest elevation
+    private static final double ELEVATION_WHERE_FADE_BEGINS = 5000; // elevation where gradient paint starts fading out (meters)
+    private static final double ELEVATION_WHERE_FADE_ENDS = 1500; // elevation where gradient paint has fully faded out (meters)
+    
+    //----------------------------------------------------------------------------
+    // Instance data
+    //----------------------------------------------------------------------------
+    
+    private final Glacier _glacier;
+    private final GlacierListener _glacierListener;
+    private final ClimateListener _climateListener;
     private final ModelViewTransform _mvt;
-    private PImage _imageNode;
+    private final Rectangle2D _worldBounds; // world bounds in model coordinates (meters x meters)
+    private final PPath _pathNode;
     
-    public SnowfallNode( Climate climate, ModelViewTransform mvt ) {
+    //----------------------------------------------------------------------------
+    // Constructors
+    //----------------------------------------------------------------------------
+    
+    public SnowfallNode( Glacier glacier, ModelViewTransform mvt ) {
+        super();
+        setPickable( false );
+        setChildrenPickable( false );
         
-        _climate = climate;
-        _climate.addClimateListener( new ClimateAdapter() {
-            public void snowfallChanged() {
-                update();
-            }
-        });
-        
+        _glacier = glacier;
         _mvt = mvt;
+        _worldBounds = new Rectangle2D.Double( 0, 0, 1, 1 );
         
-        _imageNode = new PImage( GlaciersImages.SNOWFLAKE );
-        addChild( _imageNode );
+        _pathNode = new PPath();
+        _pathNode.setStroke( null );
+        addChild( _pathNode );
         
-        Point2D p = _mvt.modelToView( 2500, 5000 );//XXX
-        _imageNode.setOffset( p );//XXX shouldn't apply offset to imageNode
+        _glacierListener = new GlacierAdapter() {
+            // ice thickness determines the shape used to draw the snowfall
+            public void iceThicknessChanged() {
+                updateShape();
+            }
+        };
+        _glacier.addGlacierListener( _glacierListener );
         
-        update();
+        _climateListener = new ClimateAdapter() {
+            // climate determines the color used to draw the snowfall
+            public void snowfallChanged() {
+                updatePaint();
+            }
+        };
+        _glacier.getClimate().addClimateListener( _climateListener );
+        
+        updateShape();
+        updatePaint();
     }
     
-    private void update() {
-        final double snowfall = _climate.getSnowfall();
+    public void cleanup() {
+        _glacier.removeGlacierListener( _glacierListener );
+        _glacier.getClimate().removeClimateListener( _climateListener );
+    }
+    
+    //----------------------------------------------------------------------------
+    // Setters and getters
+    //----------------------------------------------------------------------------
+    
+    /**
+     * Sets the world bounds.
+     * <p>
+     * The semantics of this rectangle are a bit odd, and match those of Viewport.
+     * (x,y) is the upper-left corner of the rectangle, and width and height 
+     * are distances to the right and down respectively.
+     * 
+     * @param worldBounds
+     */
+    public void setWorldBounds( Rectangle2D worldBounds ) {
+        if ( !worldBounds.equals( _worldBounds ) ) {
+            // height is adjusted to match where the gradient paint fades to NO_SNOW_COLOR.
+            // This ensures that we don't see an abrupt edge at the bottom of the gradient (if our bounds are too short),
+            // and we don't draw more than we need to (if our bounds are too tall).
+            final double height = worldBounds.getY() - ELEVATION_WHERE_FADE_ENDS;
+            _worldBounds.setFrame( worldBounds.getX(), worldBounds.getY(), worldBounds.getWidth(), height );
+            updateShape();
+            updatePaint();
+        }
+    }
+    
+    //----------------------------------------------------------------------------
+    // Updaters
+    //----------------------------------------------------------------------------
+    
+    /*
+     * Updates the shape to match the world bounds, clipped to the region above the ice and valley floor.
+     */
+    private void updateShape() {
+        Rectangle2D viewBounds = _mvt.modelToView( _worldBounds ); 
+        _pathNode.setPathTo( viewBounds );
+    }
+    
+    /*
+     * Updates the gradient paint to match the climate's snowfall.
+     */
+    private void updatePaint() {
+        
+        assert( ELEVATION_WHERE_FADE_BEGINS > ELEVATION_WHERE_FADE_ENDS );
+        
+        // get the snowfall value
+        final double snowfall = _glacier.getClimate().getSnowfall();
+        assert( GlaciersConstants.SNOWFALL_RANGE.contains( snowfall ) );
+        
+        // convert snowfall to alpha channel value.
+        // alpha channel is varies linearly.
         final double min = GlaciersConstants.SNOWFALL_RANGE.getMin();
         final double max = GlaciersConstants.SNOWFALL_RANGE.getMax();
-        final double scale = MIN_SCALE + ( MAX_SCALE - MIN_SCALE ) * ( snowfall - min ) / ( max - min );
-        Point2D offset = _imageNode.getOffset();
-        _imageNode.getTransformReference( true ).setToScale( scale, scale );
-        _imageNode.setOffset( offset );
+        final double scale = ( snowfall - min ) / ( max - min );
+        final int alpha = (int) ( scale * MAX_ALPHA );
+        
+        // create gradient paint.
+        // The gradient starts to fade at ELEVATION_WHERE_FADE_BEGINS, and is fully transparent at ELEVATION_WHERE_FADE_ENDS.
+        Rectangle2D modelBounds = new Rectangle2D.Double( _worldBounds.getX(), ELEVATION_WHERE_FADE_BEGINS, 
+                                                          _worldBounds.getWidth(), ELEVATION_WHERE_FADE_BEGINS - ELEVATION_WHERE_FADE_ENDS );
+        Rectangle2D viewBounds = _mvt.modelToView( modelBounds );
+        Color maxSnowColor = ColorUtils.createColor( BASE_COLOR, alpha );
+        GradientPaint paint = new GradientPaint( (float)viewBounds.getX(), (float)viewBounds.getMinY(), maxSnowColor, 
+                                                 (float)viewBounds.getX(), (float)viewBounds.getMaxY(), NO_SNOW_COLOR );
+        _pathNode.setPaint( paint );
     }
-
+    
+    //----------------------------------------------------------------------------
+    // Utilities
+    //----------------------------------------------------------------------------
+    
     public static Icon createIcon() {
         return new ImageIcon( GlaciersImages.SNOWFLAKE );
     }
