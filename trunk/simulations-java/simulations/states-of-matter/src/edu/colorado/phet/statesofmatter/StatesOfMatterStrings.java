180071f8b0e4d210f6872933209cb0183bdb8530
jxb147 <jxb147@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-07-22 19:00:17 +0000

    Added the buttons that show/hide the phase and interaction diagrams.

--- trunk/simulations-java/simulations/states-of-matter/src/edu/colorado/phet/statesofmatter/StatesOfMatterStrings.java
+++ trunk/simulations-java/simulations/states-of-matter/src/edu/colorado/phet/statesofmatter/StatesOfMatterStrings.java
@@ -42,9 +42,11 @@ public class StatesOfMatterStrings {
     public static final String PHASE_DIAGRAM_TRIPLE = StatesOfMatterResources.getString("PhaseChanges.PhaseDiagram.Triple");
     public static final String PHASE_DIAGRAM_CRITICAL = StatesOfMatterResources.getString("PhaseChanges.PhaseDiagram.Critical");
     public static final String PHASE_DIAGRAM_POINT = StatesOfMatterResources.getString("PhaseChanges.PhaseDiagram.Point");
+    public static final String PHASE_DIAGRAM_BUTTON_LABEL = StatesOfMatterResources.getString("PhaseChanges.PhaseDiagram.ButtonLabel");
     
     public static final String INTERACTION_POTENTIAL_GRAPH_X_AXIS_LABEL = StatesOfMatterResources.getString("PhaseChanges.InteractionPotentialGraph.xAxisLabel");
     public static final String INTERACTION_POTENTIAL_GRAPH_Y_AXIS_LABEL = StatesOfMatterResources.getString("PhaseChanges.InteractionPotentialGraph.yAxisLabel");
+    public static final String INTERACTION_POTENTIAL_BUTTON_LABEL = StatesOfMatterResources.getString("PhaseChanges.InteractionPotentialGraph.ButtonLabel");
 
     public static final String GRAVITY_CONTROL_TITLE = StatesOfMatterResources.getString("GravityControl.Title");
     public static final String GRAVITY_CONTROL_NONE = StatesOfMatterResources.getString("GravityControl.None");
