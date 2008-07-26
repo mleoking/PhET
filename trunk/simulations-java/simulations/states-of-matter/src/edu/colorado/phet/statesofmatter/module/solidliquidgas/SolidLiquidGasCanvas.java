4ac98c7f892271919031a5ae247c2642b42701d4
jxb147 <jxb147@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-07-26 01:37:54 +0000

    Changed the container type to the simpler version so that I can deploy it as an example.

--- trunk/simulations-java/simulations/states-of-matter/src/edu/colorado/phet/statesofmatter/module/solidliquidgas/SolidLiquidGasCanvas.java
+++ trunk/simulations-java/simulations/states-of-matter/src/edu/colorado/phet/statesofmatter/module/solidliquidgas/SolidLiquidGasCanvas.java
@@ -79,7 +79,7 @@ public class SolidLiquidGasCanvas extends PhetPCanvas {
         setBackground( StatesOfMatterConstants.CANVAS_BACKGROUND );
         
         // Create and add the particle container.
-        m_particleContainer = new ParticleContainerNode3(m_model, m_mvt, true);
+        m_particleContainer = new ParticleContainerNode3(m_model, m_mvt, false);
         addWorldChild(m_particleContainer);
         
         // Add a thermometer for displaying temperature.
