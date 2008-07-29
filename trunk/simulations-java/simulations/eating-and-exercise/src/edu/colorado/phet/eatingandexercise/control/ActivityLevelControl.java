f785e3e7014a83bd03ed821b69e9f20579bbf8d2
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-07-29 03:46:28 +0000

    Added descriptions for the activity levels (from Noah)

--- trunk/simulations-java/simulations/eating-and-exercise/src/edu/colorado/phet/eatingandexercise/control/ActivityLevelControl.java
+++ trunk/simulations-java/simulations/eating-and-exercise/src/edu/colorado/phet/eatingandexercise/control/ActivityLevelControl.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
+import edu.colorado.phet.eatingandexercise.EatingAndExerciseResources;
 import edu.colorado.phet.eatingandexercise.model.Human;
 import edu.colorado.phet.eatingandexercise.module.eatingandexercise.EatingAndExerciseCanvas;
 
@@ -13,15 +14,31 @@ import edu.colorado.phet.eatingandexercise.module.eatingandexercise.EatingAndExe
  * Jul 1, 2008 at 2:18:21 PM
  */
 public class ActivityLevelControl extends JPanel {
-    public ActivityLevelControl( EatingAndExerciseCanvas canvas, Human human ) {
-//        add( new JLabel( "Activity Level" ) );
+    public ActivityLevelControl( final EatingAndExerciseCanvas canvas, Human human ) {
         add( new ActivityLevelComboBox( canvas, human ) );
         final JButton button = new JButton( "?" );
         add( button );
         button.addActionListener( new ActionListener() {
             public void actionPerformed( ActionEvent e ) {
-                JOptionPane.showMessageDialog( button, "Explanation of activity levels goes here" );
+                JOptionPane.showMessageDialog( canvas, getExplanationText() );
             }
         } );
+    }
+
+    String[] activityLevelKeys = new String[]{
+            "very-sedentary",
+            "sedentary",
+            "moderate",
+            "active",
+    };
+
+    private String getExplanationText() {
+        String string = "<html>";
+        for ( int i = 0; i < activityLevelKeys.length; i++ ) {
+            String key = EatingAndExerciseResources.getString( "activity." + activityLevelKeys[i] );
+            String desc = EatingAndExerciseResources.getString( "activity." + activityLevelKeys[i] + ".description" );
+            string += key + ": " + desc + "<br><br><br>";
+        }
+        return string + "</html>";
     }
 }
