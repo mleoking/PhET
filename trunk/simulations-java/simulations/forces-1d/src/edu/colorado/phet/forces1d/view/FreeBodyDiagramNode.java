d2b52b270d55abdcdd110b838115816ff7bc81a7
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-08-24 00:58:40 +0000

    Fixed: mouse events pass through the "close fbd" buttons and create force

--- trunk/simulations-java/simulations/forces-1d/src/edu/colorado/phet/forces1d/view/FreeBodyDiagramNode.java
+++ trunk/simulations-java/simulations/forces-1d/src/edu/colorado/phet/forces1d/view/FreeBodyDiagramNode.java
@@ -5,6 +5,7 @@ import java.awt.*;
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
 
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.event.CursorHandler;
