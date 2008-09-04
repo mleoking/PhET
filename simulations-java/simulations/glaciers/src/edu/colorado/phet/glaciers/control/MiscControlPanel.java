573ef3f5633e56e0d7eea7050d7a1d70b54102ec
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-09-04 22:27:25 +0000

    9/4/08 - replace "glacier picture" check box with a button (icon on button, to left of steady state button)

--- simulations-java/simulations/glaciers/src/edu/colorado/phet/glaciers/control/MiscControlPanel.java
+++ simulations-java/simulations/glaciers/src/edu/colorado/phet/glaciers/control/MiscControlPanel.java
@@ -3,17 +3,21 @@
 package edu.colorado.phet.glaciers.control;
 
 import java.awt.Frame;
+import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
-import javax.swing.JButton;
-import javax.swing.JComponent;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 import edu.colorado.phet.common.phetcommon.application.Module;
 import edu.colorado.phet.common.phetcommon.view.ResetAllButton;
 import edu.colorado.phet.common.phetcommon.view.util.EasyGridBagLayout;
+import edu.colorado.phet.common.phetcommon.view.util.SwingUtils;
+import edu.colorado.phet.glaciers.GlaciersImages;
 import edu.colorado.phet.glaciers.GlaciersStrings;
+import edu.colorado.phet.glaciers.dialog.GlacierPictureDialog;
 import edu.colorado.phet.glaciers.model.Glacier;
 import edu.colorado.phet.glaciers.model.Glacier.GlacierAdapter;
 
@@ -31,9 +35,14 @@ public class MiscControlPanel extends JPanel {
     private final Glacier _glacier;
     private final Frame _dialogOwner;
     
+    private final JButton _pictureButton;
     private final JButton _equilibriumButton;
     private final ResetAllButton _resetAllButton;
     private final HelpButton _helpButton;
+    
+    private JDialog _glacierPictureDialog;
+    private boolean _glacierPictureDialogWasOpen;
+    private Point _glacierPictureDialogLocation;
     
     //----------------------------------------------------------------------------
     // Constructors
@@ -51,6 +60,13 @@ public class MiscControlPanel extends JPanel {
             }
         } );
         
+        _pictureButton = new JButton( new ImageIcon( GlaciersImages.GLACIER_THUMBNAIL ) );
+        _pictureButton.addActionListener( new ActionListener() {
+            public void actionPerformed( ActionEvent e ) {
+                handlePictureButton();
+            }
+        } );
+        
         _equilibriumButton = new JButton( GlaciersStrings.BUTTON_STEADY_STATE );
         _equilibriumButton.addActionListener( new ActionListener() {
             public void actionPerformed( ActionEvent e ) {
@@ -65,6 +81,7 @@ public class MiscControlPanel extends JPanel {
         EasyGridBagLayout layout = new EasyGridBagLayout( this );
         setLayout( layout );
         int column = 0;
+        layout.addComponent( _pictureButton, 0, column++ );
         layout.addComponent( _equilibriumButton, 0, column++ );
         layout.addComponent( _resetAllButton, 0, column++ );
         if ( module.hasHelp() ) {
@@ -89,5 +106,49 @@ public class MiscControlPanel extends JPanel {
      */
     public JComponent getEquilibriumButton() {
         return _equilibriumButton;
+    }
+    
+    public void activate() {
+        if ( _glacierPictureDialogWasOpen ) {
+            handlePictureButton();
+        }
+    }
+    
+    public void deactivate() {
+        _glacierPictureDialogWasOpen = ( _glacierPictureDialog != null );
+        if ( _glacierPictureDialog != null ) {
+            _glacierPictureDialog.dispose();
+        }
+    }
+    
+    //----------------------------------------------------------------------------
+    // Event handlers
+    //----------------------------------------------------------------------------
+    
+    private void handlePictureButton() {
+        if ( _glacierPictureDialog == null ) {
+            
+            _glacierPictureDialog = new GlacierPictureDialog( _dialogOwner );
+            
+            if ( _glacierPictureDialogLocation != null ) {
+                // open the dialog where it was last
+                _glacierPictureDialog.setLocation( _glacierPictureDialogLocation );
+            }
+            else {
+                // center in parent
+                SwingUtils.centerDialogInParent( _glacierPictureDialog );
+            }
+            
+            _glacierPictureDialog.addWindowListener( new WindowAdapter() {
+                // called when the close button in the dialog's window dressing is clicked
+                public void windowClosing( WindowEvent e ) {
+                    _glacierPictureDialogLocation = _glacierPictureDialog.getLocation();
+                    _glacierPictureDialog.dispose();
+                    _glacierPictureDialog = null;
+                }
+            } );
+            
+            _glacierPictureDialog.setVisible( true );
+        }
     }
 }
