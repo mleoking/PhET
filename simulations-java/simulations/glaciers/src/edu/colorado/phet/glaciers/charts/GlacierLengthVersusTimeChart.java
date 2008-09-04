1251901f5581e1262e5af68509c26a1a2ace6117
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-09-04 21:36:55 +0000

    9/4/08 - strip chart implementation for "ELA versus time" chart

--- simulations-java/simulations/glaciers/src/edu/colorado/phet/glaciers/charts/GlacierLengthVersusTimeChart.java
+++ simulations-java/simulations/glaciers/src/edu/colorado/phet/glaciers/charts/GlacierLengthVersusTimeChart.java
@@ -86,6 +86,7 @@ public class GlacierLengthVersusTimeChart extends JDialog {
         
         _domainAxis = (NumberAxis) plot.getDomainAxis();
         _domainAxis.setStandardTickUnits( NumberAxis.createIntegerTickUnits() );
+        // x-axis (time) range will be set dynamically
         
         NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
         rangeAxis.setRange( LENGTH_RANGE );
@@ -111,10 +112,6 @@ public class GlacierLengthVersusTimeChart extends JDialog {
     
     private void cleanup() {
         _clock.removeClockListener( _clockListener );
-    }
-    
-    public void clear() {
-        _series.clear();
     }
     
     private void update() {
