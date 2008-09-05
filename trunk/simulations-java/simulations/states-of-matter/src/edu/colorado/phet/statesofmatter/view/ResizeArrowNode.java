31f30c68498f39117737bb564af1234469b70987
jxb147 <jxb147@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-09-05 22:08:02 +0000

    Added the handler that makes the arrows highlight when moused over.

--- trunk/simulations-java/simulations/states-of-matter/src/edu/colorado/phet/statesofmatter/view/ResizeArrowNode.java
+++ trunk/simulations-java/simulations/states-of-matter/src/edu/colorado/phet/statesofmatter/view/ResizeArrowNode.java
@@ -8,6 +8,8 @@ import java.awt.geom.Point2D;
 import edu.colorado.phet.common.piccolophet.event.CursorHandler;
 import edu.colorado.phet.common.piccolophet.nodes.DoubleArrowNode;
 import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
+import edu.umd.cs.piccolo.event.PInputEvent;
 
 /**
  * This node represents a double-headed arrow that can be grabbed by the user
@@ -17,7 +19,7 @@ import edu.umd.cs.piccolo.PNode;
  */
 public class ResizeArrowNode extends PNode{
     
-    private static final Color NON_HIGHLIGHTED_FILL_COLOR = Color.ORANGE; 
+    private static final Color NORMAL_FILL_COLOR = Color.ORANGE; 
     private static final Color HIGHLIGHTED_FILL_COLOR = Color.YELLOW; 
 
     DoubleArrowNode m_adjusterArrow;
@@ -33,14 +35,47 @@ public class ResizeArrowNode extends PNode{
      */
     public ResizeArrowNode( double size, double angle ){
         
+        // Create and add the child node that will represent the double-
+        // headed arrow.
         m_adjusterArrow = new DoubleArrowNode(new Point2D.Double(-size/2, 0), new Point2D.Double(size/2, 0), size/4,
                 size/2, size/4);
         m_adjusterArrow.rotate( angle );
-        m_adjusterArrow.setPaint( NON_HIGHLIGHTED_FILL_COLOR );
-        addChild( m_adjusterArrow );
-        
+        m_adjusterArrow.setPaint( NORMAL_FILL_COLOR );
         m_adjusterArrow.setPickable( true );
-        
+        addChild( m_adjusterArrow );
+
+        // Add the handler that will change the cursor when the user moves
+        // the mouse over this node.
         m_adjusterArrow.addInputEventListener( new CursorHandler() );
+        
+        // Add the handler that will highlight the node when the user moves
+        // the mouse over.
+        m_adjusterArrow.addInputEventListener( new PBasicInputEventHandler(){
+            private boolean m_mouseIsPressed;
+            private boolean m_mouseIsInside;
+            public void mousePressed( PInputEvent event ) {
+                m_mouseIsPressed = true;
+                m_adjusterArrow.setPaint( HIGHLIGHTED_FILL_COLOR );
+            }
+
+            public void mouseReleased( PInputEvent event ) {
+                m_mouseIsPressed = false;
+                if ( !m_mouseIsInside ) {
+                    m_adjusterArrow.setPaint( NORMAL_FILL_COLOR );
+                }
+            }
+
+            public void mouseEntered( PInputEvent event ) {
+                m_mouseIsInside = true;
+                m_adjusterArrow.setPaint( HIGHLIGHTED_FILL_COLOR );
+            }
+
+            public void mouseExited( PInputEvent event ) {
+                m_mouseIsInside = false;
+                if ( !m_mouseIsPressed ) {
+                    m_adjusterArrow.setPaint( NORMAL_FILL_COLOR );
+                }
+            }
+        });
     }
 }
