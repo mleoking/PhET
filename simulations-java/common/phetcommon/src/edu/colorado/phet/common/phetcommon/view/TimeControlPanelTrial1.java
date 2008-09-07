35c4e8522ab0b2cc8ccd9d2e181c470d541f4eef
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-09-07 00:36:51 +0000

    added trial for connecting clock controls to base of play area

---
+++ simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/view/TimeControlPanelTrial1.java
@@ -0,0 +1,469 @@
+///* Copyright 2004-2008, University of Colorado */
+//
+//package edu.colorado.phet.common.phetcommon.view;
+//
+//import java.awt.*;
+//import java.awt.event.ActionEvent;
+//import java.awt.event.ActionListener;
+//import java.awt.event.ComponentAdapter;
+//import java.awt.event.ComponentEvent;
+//import java.awt.image.BufferedImage;
+//import java.text.DecimalFormat;
+//import java.text.NumberFormat;
+//import java.util.ArrayList;
+//
+//import javax.swing.*;
+//
+//import edu.colorado.phet.common.phetcommon.application.PhetApplication;
+//import edu.colorado.phet.common.phetcommon.application.Module;
+//import edu.colorado.phet.common.phetcommon.resources.PhetCommonResources;
+//import edu.colorado.phet.common.phetcommon.view.util.SwingUtils;
+//import edu.colorado.phet.common.phetcommon.model.clock.IClock;
+//import edu.colorado.phet.common.phetcommon.model.clock.SwingClock;
+//import edu.colorado.phet.common.piccolophet.PhetPCanvas;
+//import edu.colorado.phet.common.piccolophet.PiccoloModule;
+//
+///**
+// * TimeControlPanel implements a Swing component for controlling the clock in PhET simulations.
+// *
+// * @author Chris Malley, Sam Reid
+// */
+//public class TimeControlPanel extends PhetPCanvas {
+//
+//    public static final NumberFormat DEFAULT_TIME_FORMAT = new DecimalFormat( "0" );
+//    public static final int DEFAULT_TIME_COLUMNS = 8;
+//
+//    private JButton playPauseButton;
+//    private JButton stepButton;
+//    private JButton restartButton;
+//    private ImageIcon playIcon;
+//    private ImageIcon pauseIcon;
+//    private String playString;
+//    private String pauseString;
+//    private JTextField timeTextField;
+//    private JLabel unitsLabel;
+//    private JPanel buttonPanel;
+//    private JPanel timeDisplayPanel;
+//    private AnimatedClockJComponent animatedClockIcon;
+//
+//    private NumberFormat timeFormat;
+//    private double time;
+//    private boolean paused;
+//    private JPanel userPanel;
+//
+//    private ArrayList listeners = new ArrayList();
+//    private PanelNode panelNode;
+//
+//    public TimeControlPanel( Module module) {
+//
+//        time = 0;
+//        paused = false;
+//        timeFormat = DEFAULT_TIME_FORMAT;
+//
+//        // Play/Pause
+//        playString = PhetCommonResources.getInstance().getLocalizedString( PhetCommonResources.STRING_CLOCK_PLAY );
+//        pauseString = PhetCommonResources.getInstance().getLocalizedString( PhetCommonResources.STRING_CLOCK_PAUSE );
+//        BufferedImage playImage = PhetCommonResources.getInstance().getImage( PhetCommonResources.IMAGE_PLAY );
+//        BufferedImage pauseImage = PhetCommonResources.getInstance().getImage( PhetCommonResources.IMAGE_PAUSE );
+//        playIcon = new ImageIcon( playImage );
+//        pauseIcon = new ImageIcon( pauseImage );
+//        playPauseButton = new JButton();
+//        Dimension maxSize = SwingUtils.getMaxDimension( playPauseButton, playString, playIcon, pauseString, pauseIcon );
+//        playPauseButton.setPreferredSize( maxSize );
+//
+//        // Step
+//        String stepString = PhetCommonResources.getInstance().getLocalizedString( PhetCommonResources.STRING_CLOCK_STEP );
+//        BufferedImage stepImage = PhetCommonResources.getInstance().getImage( PhetCommonResources.IMAGE_STEP_FORWARD );
+//        ImageIcon stepIcon = new ImageIcon( stepImage );
+//        stepButton = new JButton( stepString, stepIcon );
+//
+//        // Restart
+//        String restartString = PhetCommonResources.getInstance().getLocalizedString( PhetCommonResources.STRING_CLOCK_RESTART );
+//        BufferedImage restartImage = PhetCommonResources.getInstance().getImage( PhetCommonResources.IMAGE_RESTART );
+//        ImageIcon restartIcon = new ImageIcon( restartImage );
+//        restartButton = new JButton( restartString, restartIcon );
+//
+//        // Put all the buttons in a button panel
+//        buttonPanel = new JPanel( new FlowLayout( FlowLayout.CENTER ) );
+//        buttonPanel.add( restartButton );
+//        buttonPanel.add( playPauseButton );
+//        buttonPanel.add( stepButton );
+//
+//
+//        setBorder( null );
+//        setBackground( new Color( 255,242,173) );
+//
+//        panelNode = new PanelNode(module);
+//        updateLayout();
+//
+//        SwingUtilities.invokeLater( new Runnable() {
+//            public void run() {
+//                updateLayout();//update after simulation panel has been set to module
+//            }
+//        } );
+//        setPreferredSize( new Dimension( 1024, (int) panelNode.getFullBounds().getMaxY()+15 ) );
+//        addScreenChild( panelNode );
+//        addComponentListener( new ComponentAdapter() {
+//            public void componentResized( ComponentEvent e ) {
+//                updateLayout();
+//            }
+//        } );
+//        updateLayout();
+//
+//        // Time display, time value & units
+//        timeTextField = new JTextField();
+//        timeTextField.setColumns( DEFAULT_TIME_COLUMNS );
+//        timeTextField.setEditable( false );
+//        timeTextField.setHorizontalAlignment( JTextField.RIGHT );
+//        unitsLabel = new JLabel();
+//        timeDisplayPanel = new JPanel( new FlowLayout( FlowLayout.CENTER ) );
+//        timeDisplayPanel.add( timeTextField );
+//        timeDisplayPanel.add( unitsLabel );
+//
+//        // Animated clock icon
+//        animatedClockIcon = new AnimatedClockJComponent();
+//
+//        // User panel, for stuff between the time display and buttons
+//        userPanel = new JPanel( new FlowLayout( FlowLayout.CENTER ) );
+//
+//        // for backward compatibility with existing sims
+//        restartButton.setVisible( false );
+//        timeTextField.setVisible( false );
+//        unitsLabel.setVisible( false );
+//
+//        // Workaround for Macintosh
+//        SwingUtils.fixButtonOpacity( playPauseButton );
+//        SwingUtils.fixButtonOpacity( stepButton );
+//
+//        // Layout the button panel
+//        setLayout( new FlowLayout( FlowLayout.CENTER ) );
+//        if ( PhetApplication.instance().isDeveloperControlsEnabled() ) { //TODO: only in dev versions until we finish this feature
+//            add( animatedClockIcon );
+//        }
+//        add( timeDisplayPanel );
+//        add( userPanel );
+//        add( buttonPanel );
+//
+//        // Adapter methods for event dispatch
+//        playPauseButton.addActionListener( new ActionListener() {
+//            public void actionPerformed( ActionEvent e ) {
+//                paused = !paused;
+//                updateButtons();
+//                if ( paused ) {
+//                    notifyPausePressed();
+//                }
+//                else {
+//                    notifyPlayPressed();
+//                }
+//            }
+//        } );
+//        stepButton.addActionListener( new ActionListener() {
+//            public void actionPerformed( ActionEvent e ) {
+//                notifyStepPressed();
+//            }
+//        } );
+//        restartButton.addActionListener( new ActionListener() {
+//            public void actionPerformed( ActionEvent e ) {
+//                notifyRestartPressed();
+//            }
+//        } );
+//
+//        updateButtons();
+//    }
+//
+//    public Component add( Component comp ) {
+//        return null;
+//    }
+//
+//    /**
+//     * Sets the text for the step button to the specified value.
+//     *
+//     * @param text the label text to display on the step button
+//     */
+//    public void setStepButtonText( String text ) {
+//        stepButton.setText( text );
+//    }
+//
+//    /**
+//     * Advances the animated clock icon by one step.
+//     */
+//    public void advanceAnimatedClockIcon() {
+//        animatedClockIcon.advance();
+//    }
+//
+//    protected void updateLayout() {
+//        super.updateLayout();
+//        panelNode.updateLayout( getWidth(), getHeight() );
+//    }
+//
+//    /**
+//     * Resets the animated clock icon to its initial state.
+//     */
+//    public void resetAnimatedClockIcon() {
+//        animatedClockIcon.reset();
+//    }
+//
+//    /**
+//     * Sets the visibility of the Restart button.
+//     * This button is invisible by default for backward compatibility with existing sims.
+//     *
+//     * @param visible
+//     */
+//    public void setRestartButtonVisible( boolean visible ) {
+//        restartButton.setVisible( visible );
+//    }
+//
+//    /**
+//     * Sets the visibility of the time display.
+//     * This display is invisible by default for backward compatibility with existing sims.
+//     *
+//     * @param visible
+//     */
+//    public void setTimeDisplayVisible( boolean visible ) {
+//        timeTextField.setVisible( visible );
+//        unitsLabel.setVisible( visible );
+//        if ( visible ) {
+//            updateTimeDisplay();
+//        }
+//    }
+//
+//    /**
+//     * Convenience method for adding a component to the left of this panel.
+//     *
+//     * @param component
+//     */
+//    public void addToLeft( JComponent component ) {
+////        add( component, 0 );
+//    }
+//
+//    /**
+//     * Adds component between the time display and the buttons.
+//     * <p/>
+//     * TODO: This is a hack, currently used by some sims to add a clock speed control.
+//     * We should figure out a better way to add components to the layout, or
+//     * add a standard clock speed control to this control panel.
+//     *
+//     * @param component
+//     */
+//    public void addBetweenTimeDisplayAndButtons( JComponent component ) {
+////        userPanel.add( component );
+//    }
+//
+//    /**
+//     * Sets whether this TimeControlPanel should treat time as paused.
+//     *
+//     * @param paused
+//     */
+//    public void setPaused( boolean paused ) {
+//        this.paused = paused;
+//        updateButtons();
+//    }
+//
+//    /**
+//     * Enables or disables the clock control panel.
+//     * When disabled, all buttons (play/pause/step) are also disabled.
+//     * When enabled, the buttons are enabled to correspond to the clock state.
+//     *
+//     * @param enabled true or false
+//     */
+//    public void setEnabled( boolean enabled ) {
+//        super.setEnabled( enabled );
+//        updateButtons();
+//    }
+//
+//    /**
+//     * Gets the "Restart" component, used for attaching help items.
+//     *
+//     * @return
+//     */
+//    public JComponent getRestartComponent() {
+//        return restartButton;
+//    }
+//
+//    /**
+//     * Sets the format of the time display.
+//     * See DecimalFormat for specification of pattern syntax.
+//     *
+//     * @param formatPattern
+//     */
+//    public void setTimeFormat( String formatPattern ) {
+//        setTimeFormat( new DecimalFormat( formatPattern ) );
+//    }
+//
+//    /**
+//     * Sets the format of the time display.
+//     *
+//     * @param format
+//     */
+//    public void setTimeFormat( NumberFormat format ) {
+//        timeFormat = format;
+//        updateTimeDisplay();
+//    }
+//
+//    /**
+//     * Sets the font used to display the time.
+//     *
+//     * @param font
+//     */
+//    public void setTimeFont( Font font ) {
+//        timeTextField.setFont( font );
+//    }
+//
+//    /**
+//     * Sets the number of columns used to display the time.
+//     *
+//     * @param columns
+//     */
+//    public void setTimeColumns( int columns ) {
+//        timeTextField.setColumns( columns );
+//    }
+//
+//    /**
+//     * Sets the time units.
+//     *
+//     * @param units
+//     */
+//    public void setUnits( String units ) {
+//        unitsLabel.setText( units );
+//    }
+//
+//    /**
+//     * Sets the font for the time units.
+//     *
+//     * @param font
+//     */
+//    public void setUnitsFont( Font font ) {
+//        unitsLabel.setFont( font );
+//    }
+//
+//    /**
+//     * Sets the time displayed.
+//     */
+//    public void setTimeDisplay( double time ) {
+//        if ( time != this.time ) {
+//            this.time = time;
+//            updateTimeDisplay();
+//        }
+//    }
+//
+//    /*
+//    * Updates the time display.
+//    */
+//    private void updateTimeDisplay() {
+//        if ( timeTextField.isVisible() ) {
+//            String sValue = timeFormat.format( time );
+//            timeTextField.setText( sValue );
+//        }
+//    }
+//
+//    /*
+//     * Updates the state of the play/pause and step buttons to reflect whether the control is paused and/or enabled.
+//     */
+//    private void updateButtons() {
+//        playPauseButton.setText( paused ? playString : pauseString );
+//        playPauseButton.setIcon( paused ? playIcon : pauseIcon );
+//        playPauseButton.setEnabled( isEnabled() );
+//        stepButton.setEnabled( isEnabled() && paused );
+//        restartButton.setEnabled( isEnabled() );
+//    }
+//
+//    /**
+//     * Listener interface for receiving events from TimeControlPanel
+//     * This interface may be revised to pass a TimeControlEvent.
+//     */
+//    public static interface TimeControlListener {
+//        void stepPressed();
+//
+//        void playPressed();
+//
+//        void pausePressed();
+//
+//        void restartPressed();
+//    }
+//
+//    /**
+//     * Convenience adapter class for TimeControlListener
+//     */
+//    public static class TimeControlAdapter implements TimeControlListener {
+//        public void stepPressed() {
+//        }
+//
+//        public void playPressed() {
+//        }
+//
+//        public void pausePressed() {
+//        }
+//
+//        public void restartPressed() {
+//        }
+//    }
+//
+//    public void addTimeControlListener( TimeControlListener listener ) {
+//        listeners.add( listener );
+//    }
+//
+//    public void removeTimeControlListener( TimeControlListener listener ) {
+//        listeners.remove( listener );
+//    }
+//
+//    private void notifyStepPressed() {
+//        for ( int i = 0; i < listeners.size(); i++ ) {
+//            ( (TimeControlListener) listeners.get( i ) ).stepPressed();
+//        }
+//    }
+//
+//    private void notifyPlayPressed() {
+//        for ( int i = 0; i < listeners.size(); i++ ) {
+//            ( (TimeControlListener) listeners.get( i ) ).playPressed();
+//        }
+//    }
+//
+//    private void notifyPausePressed() {
+//        for ( int i = 0; i < listeners.size(); i++ ) {
+//            ( (TimeControlListener) listeners.get( i ) ).pausePressed();
+//        }
+//    }
+//
+//    private void notifyRestartPressed() {
+//        for ( int i = 0; i < listeners.size(); i++ ) {
+//            ( (TimeControlListener) listeners.get( i ) ).restartPressed();
+//        }
+//    }
+//
+//    /**
+//     * Returns the component responsible for handling play/pause button presses.
+//     *
+//     * @return the play/pause button
+//     */
+//    public JComponent getPlayPauseButton() {
+//        return playPauseButton;
+//    }
+//
+//    public static void main( String[] args ) {
+//        JFrame frame = new JFrame();
+//        TimeControlPanel pane = new TimeControlPanel(new PiccoloModule("test",new SwingClock( 30,1)));
+//        pane.setRestartButtonVisible( true );
+//        pane.addTimeControlListener( new TimeControlListener() {
+//            public void stepPressed() {
+//                System.out.println( "TimeControlPanel.stepPressed" );
+//            }
+//
+//            public void playPressed() {
+//                System.out.println( "TimeControlPanel.playPressed" );
+//            }
+//
+//            public void pausePressed() {
+//                System.out.println( "TimeControlPanel.pausePressed" );
+//            }
+//
+//            public void restartPressed() {
+//                System.out.println( "TimeControlPanel.restartPressed" );
+//            }
+//        } );
+//        frame.setContentPane( pane );
+//        frame.pack();
+//        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+//        SwingUtils.centerWindowOnScreen( frame );
+//        frame.setVisible( true );
+//    }
+//}
