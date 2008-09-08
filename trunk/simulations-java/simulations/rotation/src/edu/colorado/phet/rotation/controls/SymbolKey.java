ac71494faecb2d4b6679f19efb30d410aa0d7517
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-09-08 18:05:10 +0000

    improved il8n

--- trunk/simulations-java/simulations/rotation/src/edu/colorado/phet/rotation/controls/SymbolKey.java
+++ trunk/simulations-java/simulations/rotation/src/edu/colorado/phet/rotation/controls/SymbolKey.java
@@ -4,6 +4,7 @@ import java.awt.*;
 
 import javax.swing.*;
 
+import edu.colorado.phet.rotation.RotationStrings;
 import edu.colorado.phet.rotation.util.UnicodeUtil;
 import edu.colorado.phet.rotation.view.RotationLookAndFeel;
 
@@ -19,28 +20,21 @@ public class SymbolKey extends JPanel {
     public SymbolKey() {
         setLayout( new GridBagLayout() );
         gridBagConstraints = new GridBagConstraints( 0, GridBagConstraints.RELATIVE, 1, 1, 0, 0, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets( 0, 0, 0, 0 ), 0, 0 );
-        JLabel label = new JLabel( "Symbol Key:" );
+        JLabel label = new JLabel( RotationStrings.getString( "controls.symbol.key" ) );
         label.setFont( RotationLookAndFeel.getControlPanelTitleFont() );
         add( label, gridBagConstraints );
 
-        addItem( UnicodeUtil.THETA, "Angle" );
-        addItem( UnicodeUtil.OMEGA, "Angular Velocity" );
-        addItem( UnicodeUtil.ALPHA, "Angular Acceleration" );
-        addItem( "x, y", "Position" );
-        addItem( "variable.v", "Velocity" );
-        addItem( "variable.a", "Acceleration" );
+        addItem( UnicodeUtil.THETA, RotationStrings.getString( "variable.angle" ) );
+        addItem( UnicodeUtil.OMEGA, RotationStrings.getString( "variable.angular.velocity" ) );
+        addItem( UnicodeUtil.ALPHA, RotationStrings.getString( "variable.angular.acceleration" ) );
+        addItem( RotationStrings.getString( "variable.x.and.y" ), RotationStrings.getString( "variable.position" ) );
+        addItem( RotationStrings.getString( "variable.v" ), RotationStrings.getString( "variable.velocity" ) );
+        addItem( RotationStrings.getString( "variable.a" ), RotationStrings.getString( "variable.acceleration" ) );
         setBorder( BorderFactory.createLineBorder( Color.lightGray ) );
     }
 
-    private void addItem( String theta, String s ) {
-        JLabel label = new JLabel( theta + " = " + s );
-//        {
-//            protected void paintComponent( Graphics g ) {
-//                boolean aa = GraphicsUtil.antialias( g, true );
-//                super.paintComponent( g );
-//                GraphicsUtil.antialias( g, aa );
-//            }
-//        };
+    private void addItem( String key, String s ) {
+        JLabel label = new JLabel( key + " = " + s );
         label.setFont( RotationLookAndFeel.getLegendItemFont() );
         add( label, gridBagConstraints );
     }
