338037efbb4a02fffc6e10b989985bb157b9162d
jxb147 <jxb147@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-09-08 16:09:09 +0000

    Added the ability to only have the interactivity controls visible when the user has selected the adjustable molecule.

--- trunk/simulations-java/simulations/states-of-matter/src/edu/colorado/phet/statesofmatter/module/phasechanges/InteractionPotentialNodeWithInteraction.java
+++ trunk/simulations-java/simulations/states-of-matter/src/edu/colorado/phet/statesofmatter/module/phasechanges/InteractionPotentialNodeWithInteraction.java
@@ -1,3 +1,5 @@
+/* Copyright 2008, University of Colorado */
+
 package edu.colorado.phet.statesofmatter.module.phasechanges;
 
 import java.awt.geom.Point2D;
@@ -10,7 +12,12 @@ import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
 import edu.umd.cs.piccolo.event.PInputEvent;
 import edu.umd.cs.piccolo.util.PDimension;
 
-
+/**
+ * This class extends the Interaction Potential diagram to allow the user to
+ * change the curve through direct interaction with it.
+ *
+ * @author John Blanco
+ */
 public class InteractionPotentialNodeWithInteraction extends InteractionPotentialDiagramNode {
 
     private static final double RESIZE_HANDLE_SIZE_PROPORTION = 0.05;    // Size of handles as function of node width.
@@ -20,6 +27,7 @@ public class InteractionPotentialNodeWithInteraction extends InteractionPotentia
     private DualParticleModel m_model;
     private ResizeArrowNode m_sigmaResizeHandle;
     private ResizeArrowNode m_epsilonResizeHandle;
+    private boolean m_interactionEnabled;
 
     /**
      * Constructor.
@@ -37,6 +45,10 @@ public class InteractionPotentialNodeWithInteraction extends InteractionPotentia
         model.addListener(new DualParticleModel.Adapter(){
             public void interactionPotentialChanged() {
                 setLjPotentialParameters( model.getSigma(), model.getEpsilon() );
+            }
+            public void moleculeTypeChanged() {
+                updateInteractifyState();
+                drawPotentialCurve();
             }
         });
         
@@ -65,8 +77,28 @@ public class InteractionPotentialNodeWithInteraction extends InteractionPotentia
                 m_model.setSigma( m_model.getSigma() + d.getWidth() * scaleFactor );
             }
         });
+        
+        // Update interactivity state.
+        updateInteractifyState();
     }
 
+    /**
+     * 
+     */
+    private void updateInteractifyState() {
+        if (m_model.getMoleculeType() != StatesOfMatterConstants.USER_DEFINED_MOLECULE){
+            m_interactionEnabled = false;
+        }
+        else{
+            m_interactionEnabled = true;
+        }
+    }
+
+    /**
+     * This is an override of the method in the base class that draws the
+     * curve on the graph, and this override draws the controls that allow
+     * the user to interact with the graph.
+     */
     protected void drawPotentialCurve() {
         
         // The bulk of the drawing is done by the base class.
@@ -77,11 +109,17 @@ public class InteractionPotentialNodeWithInteraction extends InteractionPotentia
             Point2D graphMin = getGraphMin();
             m_epsilonResizeHandle.setOffset( 
                     graphMin.getX() + getGraphOffsetX() + (m_width * EPSILON_HANDLE_OFFSET_PROPORTION), graphMin.getY() );
+            m_epsilonResizeHandle.setVisible( m_interactionEnabled );
+            m_epsilonResizeHandle.setPickable( m_interactionEnabled );
+            m_epsilonResizeHandle.setChildrenPickable( m_interactionEnabled );
         }
         if (m_sigmaResizeHandle != null){
             Point2D zeroCrossingPoint = getZeroCrossingPoint();
             m_sigmaResizeHandle.setOffset( zeroCrossingPoint.getX() + getGraphOffsetX(), 
                     (getGraphHeight() / 2) - SIGMA_HANDLE_OFFSET_PROPORTION * m_height );
+            m_sigmaResizeHandle.setVisible( m_interactionEnabled );
+            m_sigmaResizeHandle.setPickable( m_interactionEnabled );
+            m_sigmaResizeHandle.setChildrenPickable( m_interactionEnabled );
         }
     }
 }
