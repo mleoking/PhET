9f313f5035e948c4383d16e8f9beb0b5563b00bb
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-09-08 17:50:00 +0000

    document the workaround for JComponents and PNodes

--- simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/test/testmacbug/TestGradientPaint.java
+++ simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/test/testmacbug/TestGradientPaint.java
@@ -8,8 +8,14 @@ import javax.swing.*;
  * Gradient Paint causes this program to crash on Mac OS 10.4.11 with Java 1.5.0_13.
  * The same program runs fine on Mac OS 10.5.4 with Java 1.5.0_13, so the problem
  * must be in native code on Mac OS 10.4.
+ * <p>
+ * The workaround is to render with VALUE_RENDER_SPEED instead of VALUE_RENDER_QUALITY.
  */
 public class TestGradientPaint {
+    
+    private static final Object VALUE_RENDER = RenderingHints.VALUE_RENDER_QUALITY; // causes a crash
+//    private static final Object VALUE_RENDER = RenderingHints.VALUE_RENDER_SPEED; // works fine
+    
     public static void main( String[] args ) {
         final GradientPaint gradientPaint = new GradientPaint( 0, 0, Color.blue, 0, 10, Color.blue );
         JFrame frame = new JFrame();
@@ -17,9 +23,9 @@ public class TestGradientPaint {
         final Rectangle rectangle = new Rectangle( 20, 20 );
         frame.setContentPane( new JPanel() {
             protected void paintComponent( Graphics graphics ) {
-                super.paintComponent( graphics );    //To change body of overridden methods use File | Settings | File Templates.
+                super.paintComponent( graphics );
                 Graphics2D g2 = (Graphics2D) graphics;
-                g2.setRenderingHint( RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY );
+                g2.setRenderingHint( RenderingHints.KEY_RENDERING, VALUE_RENDER );
                 g2.setPaint( gradientPaint );
                 g2.fill( rectangle );
             }
