1cb1133c4f1d0443ce848af3542447c20b1abc9b
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-09-08 17:27:48 +0000

    add comments about what we're testing

--- simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/test/testmacbug/TestTabGraphic.java
+++ simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/test/testmacbug/TestTabGraphic.java
@@ -8,11 +8,9 @@ import edu.umd.cs.piccolo.PCanvas;
 import edu.umd.cs.piccolo.nodes.PPath;
 
 /**
- * Created by IntelliJ IDEA.
- * User: reids
- * Date: Mar 9, 2006
- * Time: 5:21:00 PM
- * To change this template use File | Settings | File Templates.
+ * Gradient Paint causes this program to crash on Mac OS 10.4.11 with Java 1.5.0_13.
+ * The same program runs fine on Mac OS 10.5.4 with Java 1.5.0_13, so the problem
+ * must be in native code on Mac OS 10.4.
  */
 public class TestTabGraphic {
     public static void main( String[] args ) {
