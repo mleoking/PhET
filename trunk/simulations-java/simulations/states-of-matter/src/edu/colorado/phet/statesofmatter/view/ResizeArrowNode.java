b3ee3c51e07b6325313c0136ab02d9a674a8badb
jxb147 <jxb147@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-09-08 18:16:08 +0000

    Tweaked the colors for the adjuster arrows.

--- trunk/simulations-java/simulations/states-of-matter/src/edu/colorado/phet/statesofmatter/view/ResizeArrowNode.java
+++ trunk/simulations-java/simulations/states-of-matter/src/edu/colorado/phet/statesofmatter/view/ResizeArrowNode.java
@@ -19,8 +19,8 @@ import edu.umd.cs.piccolo.event.PInputEvent;
  */
 public class ResizeArrowNode extends PNode{
     
-    private static final Color NORMAL_FILL_COLOR = Color.LIGHT_GRAY; 
-    private static final Color HIGHLIGHTED_FILL_COLOR = Color.ORANGE; 
+    private static final Color NORMAL_FILL_COLOR = new Color(0x00cc66); 
+    private static final Color HIGHLIGHTED_FILL_COLOR = new Color(0x00ff66); 
 
     DoubleArrowNode m_adjusterArrow;
     
