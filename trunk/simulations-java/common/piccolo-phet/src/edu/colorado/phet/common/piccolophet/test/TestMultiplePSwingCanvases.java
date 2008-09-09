3e94c00938528e011c85ce56c377a026ee84ff53
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-09-09 02:04:38 +0000

    updated to work with new piccolo package

--- trunk/simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/test/TestMultiplePSwingCanvases.java
+++ trunk/simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/test/TestMultiplePSwingCanvases.java
@@ -1,15 +1,14 @@
 /* Copyright 2007, University of Colorado */
 package edu.colorado.phet.common.piccolophet.test;
 
-import edu.umd.cs.piccolox.pswing.tests.TestPSwingFull;
+import edu.umd.cs.piccolo.examples.PSwingExample;
 
 public class TestMultiplePSwingCanvases {
     public static void main( String[] args ) {
-        TestPSwingFull.main( args );
-        TestPSwingFull.main( args );
-        TestPSwingFull.main( args );
-        TestPSwingFull.main( args );
-        TestPSwingFull.main( args );
-        TestPSwingFull.main( args );
+        PSwingExample.main( args );
+        PSwingExample.main( args );
+        PSwingExample.main( args );
+        PSwingExample.main( args );
+        PSwingExample.main( args );
     }
 }
