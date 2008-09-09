53cd454bf5d90cad57a52a80e72b5d65bc4cbfef
jxb147 <jxb147@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-09-09 20:56:29 +0000

    Added a push pin for holding the fixed atom in place on the interaction potential tab.  This caused some refactoring since I also removed references to the coffee cup image while I was updating the list of images.

---
+++ simulations-java/simulations/states-of-matter/src/edu/colorado/phet/statesofmatter/view/PushpinNode.java
@@ -0,0 +1,33 @@
+/* Copyright 2007, University of Colorado */
+
+package edu.colorado.phet.statesofmatter.view;
+
+import edu.colorado.phet.opticaltweezers.OTConstants;
+import edu.colorado.phet.opticaltweezers.OTResources;
+import edu.colorado.phet.statesofmatter.StatesOfMatterConstants;
+import edu.colorado.phet.statesofmatter.StatesOfMatterResources;
+import edu.umd.cs.piccolo.nodes.PImage;
+import edu.umd.cs.piccolo.util.PBounds;
+import edu.umd.cs.piccolox.nodes.PComposite;
+
+/**
+ * PushpinNode represents the pushpin used to "pin" an atom in one location.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
+public class PushpinNode extends PComposite {
+
+    public PushpinNode() {
+        super();
+        
+        setPickable( false );
+        setChildrenPickable( false );
+        
+        PImage imageNode = new PImage( StatesOfMatterResources.getImage( StatesOfMatterConstants.PUSH_PIN_IMAGE ) );
+        // move origin to the point of the pin, at lower right
+        PBounds b = imageNode.getFullBoundsReference();
+        imageNode.setOffset( -b.getWidth(), -b.getHeight() );
+        
+        addChild( imageNode );
+    }
+}
