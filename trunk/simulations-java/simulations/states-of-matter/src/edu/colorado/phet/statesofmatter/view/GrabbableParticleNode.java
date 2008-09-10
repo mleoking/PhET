4ae2a6c55b4b279faf7df5cd20dc4edb0609f746
jxb147 <jxb147@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-09-10 17:10:36 +0000

    Implemented limits to the placement of the movable particle on the Interaction Potential canvas.

--- trunk/simulations-java/simulations/states-of-matter/src/edu/colorado/phet/statesofmatter/view/GrabbableParticleNode.java
+++ trunk/simulations-java/simulations/states-of-matter/src/edu/colorado/phet/statesofmatter/view/GrabbableParticleNode.java
@@ -29,24 +29,30 @@ public class GrabbableParticleNode extends ParticleForceNode {
     //----------------------------------------------------------------------------
 
     DualParticleModel m_model;
+    double m_minX; // The minimum value allowed in the x direction.
+    double m_maxX; // The minimum value allowed in the x direction.
     
     //----------------------------------------------------------------------------
     // Constructor(s)
     //----------------------------------------------------------------------------
     
     /**
-     * Constructor
+     * Constructor.
      * 
      * @param particle - The particle within the model.
      * @param mvt - Model-view transform.
      * @param useGradient - A boolean that controls whether to use a gradient.
+     * @param minX - Minimum value in the X direction to which the particle can be moved.
+     * @param maxX - Maximum value in the X direction to which the particle can be moved.
      */
     public GrabbableParticleNode( DualParticleModel model, StatesOfMatterAtom particle, ModelViewTransform mvt, 
-            boolean useGradient ) {
+            boolean useGradient, double minX, double maxX ) {
         
         super( particle, mvt, useGradient );
 
         m_model = model;
+        m_minX = minX;
+        m_maxX = maxX;
         
         // This node will need to be pickable so the user can grab it.
         setPickable( true );
@@ -73,6 +79,38 @@ public class GrabbableParticleNode extends ParticleForceNode {
         });
     }
     
+    /**
+     * Constructor
+     */
+    public GrabbableParticleNode( DualParticleModel model, StatesOfMatterAtom particle, ModelViewTransform mvt, 
+            boolean useGradient ) {
+
+        this(model, particle, mvt, useGradient, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
+    }
+    
+    //----------------------------------------------------------------------------
+    // Accessor Methods
+    //----------------------------------------------------------------------------
+    
+    public double getMinX() {
+        return m_minX;
+    }
+
+    
+    public void setMinX( double minX ) {
+        m_minX = minX;
+    }
+
+    
+    public double getMaxX() {
+        return m_maxX;
+    }
+
+    
+    public void setMaxX( double maxX ) {
+        m_maxX = maxX;
+    }
+
     //----------------------------------------------------------------------------
     // Private Methods
     //----------------------------------------------------------------------------
@@ -89,8 +127,18 @@ public class GrabbableParticleNode extends ParticleForceNode {
         PDimension d = event.getDeltaRelativeTo(draggedNode);
         draggedNode.localToParent(d);
 
+        // Make sure we don't exceed the positional limits.
+        double newPosX = m_particle.getX() + d.width;
+        if (newPosX > m_maxX) {
+            newPosX = m_maxX;
+        }
+        else if (newPosX < m_minX) {
+            newPosX = m_minX;
+        }
+        System.out.println("newPosX = " + newPosX);
+        
         // Move the particle based on the amount of mouse movement.
-        m_particle.setPosition( m_particle.getX() + d.width, m_particle.getY() );
+        m_particle.setPosition( newPosX, m_particle.getY() );
     }
     
     private void handleMouseEndDragEvent(PInputEvent event){
@@ -98,4 +146,6 @@ public class GrabbableParticleNode extends ParticleForceNode {
         // even if the motion was paused by some other means.
         m_model.setParticleMotionPaused( false );
     }
+
+    
 }
