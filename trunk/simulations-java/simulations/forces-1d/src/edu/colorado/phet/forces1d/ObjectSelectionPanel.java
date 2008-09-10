9067ca8ed25cb1fadd5de967a96e03d9503dfb72
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-09-10 19:59:02 +0000

    Control panel no longer resizes on item change: this was done by highlighting the item instead of changing font.

--- trunk/simulations-java/simulations/forces-1d/src/edu/colorado/phet/forces1d/ObjectSelectionPanel.java
+++ trunk/simulations-java/simulations/forces-1d/src/edu/colorado/phet/forces1d/ObjectSelectionPanel.java
@@ -1,4 +1,3 @@
-/*  */
 package edu.colorado.phet.forces1d;
 
 import java.awt.*;
@@ -9,10 +8,10 @@ import java.io.IOException;
 
 import javax.swing.*;
 
-import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
-import edu.colorado.phet.forces1d.phetcommon.view.util.BufferedImageUtils;
-import edu.colorado.phet.forces1d.phetcommon.view.util.ImageLoader;
+import edu.colorado.phet.common.phetcommon.view.VerticalLayoutPanel;
+import edu.colorado.phet.common.phetcommon.view.util.BufferedImageUtils;
 import edu.colorado.phet.forces1d.model.Force1dObject;
+import edu.colorado.phet.forces1d.phetcommon.view.util.ImageLoader;
 
 /**
  * User: Sam Reid
@@ -20,13 +19,11 @@ import edu.colorado.phet.forces1d.model.Force1dObject;
  * Time: 8:12:57 AM
  */
 
-public class ObjectSelectionPanel extends JPanel {
-    private Font selectedFont = new Font( PhetFont.getDefaultFontName(), Font.BOLD, 13 );
-    private Font normalFont = new Font( PhetFont.getDefaultFontName(), Font.PLAIN, 13 );
+public class ObjectSelectionPanel extends VerticalLayoutPanel {
+    private Color SELECTION_COLOR = Color.yellow;
 
     public ObjectSelectionPanel( final Force1dObject[] imageElements, final Forces1DControlPanel simpleControlPanel ) {
-        setLayout( new BoxLayout( this, BoxLayout.Y_AXIS ) );
-        ButtonGroup bg = new ButtonGroup();
+        ButtonGroup buttonGroup = new ButtonGroup();
         final JRadioButton[] jRadioButtons = new JRadioButton[imageElements.length];
         for ( int i = 0; i < imageElements.length; i++ ) {
             final Force1dObject imageElement = imageElements[i];
@@ -37,18 +34,19 @@ public class ObjectSelectionPanel extends JPanel {
             catch( IOException e ) {
                 e.printStackTrace();
             }
-            image = BufferedImageUtils.rescaleYMaintainAspectRatio( simpleControlPanel, image, 35 );
+
+            image = BufferedImageUtils.multiScaleToHeight( image, 35 );
             ImageIcon icon = new ImageIcon( image );
             JRadioButton jRadioButton = new JRadioButton( imageElement.getName() + " (" + imageElement.getMass() + " kg)", icon );
             jRadioButtons[i] = jRadioButton;
             if ( i == 0 ) {
                 jRadioButton.setSelected( true );
-                jRadioButton.setFont( selectedFont );
+                jRadioButton.setBackground( SELECTION_COLOR );
             }
             else {
-                jRadioButton.setFont( normalFont );
+                jRadioButton.setBackground( Forces1DApplication.FORCES_1D_BACKGROUND_COLOR );
             }
-            bg.add( jRadioButton );
+            buttonGroup.add( jRadioButton );
             final int i1 = i;
             jRadioButton.addActionListener( new ActionListener() {
                 public void actionPerformed( ActionEvent e ) {
@@ -56,10 +54,10 @@ public class ObjectSelectionPanel extends JPanel {
                     for ( int j = 0; j < jRadioButtons.length; j++ ) {
                         JRadioButton radioButton = jRadioButtons[j];
                         if ( j == i1 ) {
-                            radioButton.setFont( selectedFont );
+                            radioButton.setBackground( SELECTION_COLOR );
                         }
                         else {
-                            radioButton.setFont( normalFont );
+                            radioButton.setBackground( Forces1DApplication.FORCES_1D_BACKGROUND_COLOR );
                         }
                     }
                 }
