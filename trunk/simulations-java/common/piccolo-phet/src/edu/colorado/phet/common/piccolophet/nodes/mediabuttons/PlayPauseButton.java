1d08fa72a6dfc460817e8382d0fd06b7fc0f9a36
jxb147 <jxb147@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-09-11 20:39:12 +0000

    Initial creation of the revised buttons for play/pause of sims.

---
+++ trunk/simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/nodes/mediabuttons/PlayPauseButton.java
@@ -0,0 +1,64 @@
+/* Copyright 2008, University of Colorado */
+
+package edu.colorado.phet.common.piccolophet.nodes.mediabuttons;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Shape;
+
+import edu.colorado.phet.common.phetcommon.view.util.DoubleGeneralPath;
+import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
+import edu.colorado.phet.common.piccolophet.test.PiccoloTestFrame;
+import edu.umd.cs.piccolo.nodes.PPath;
+
+
+public class PlayPauseButton extends AbstractMediaButton {
+
+    double dx;
+    double dy;
+    
+    public PlayPauseButton( int buttonHeight ) {
+        super( buttonHeight );
+        
+        dx = buttonHeight / 6;
+        dy = buttonHeight / 6;
+        PPath playButton = new PhetPPath( createPauseIconShape(), Color.BLACK, new BasicStroke(1), Color.LIGHT_GRAY );
+        addChild( playButton );
+    }
+    
+    private Shape createPlayIconShape() {
+        
+        DoubleGeneralPath shape = new DoubleGeneralPath();
+        double width = getButtonDimension().width;
+        double height = getButtonDimension().height;
+        shape.moveTo( width / 2 + dx, height / 2 );
+        shape.lineToRelative( -dx * 2, dy );
+        shape.lineToRelative( 0, -2 * dy );
+        shape.lineToRelative( 2 * dx, dy );
+        return shape.getGeneralPath();
+    }
+    
+    private Shape createPauseIconShape() {
+        
+        DoubleGeneralPath shape = new DoubleGeneralPath();
+        double width = getButtonDimension().width;
+        double height = getButtonDimension().height;
+        shape.moveTo( width / 2 - dx / 3, height / 2 - dy );
+        shape.lineToRelative( 0, 2 * dy );
+        shape.lineToRelative( -dx, 0 );
+        shape.lineToRelative( 0, -2 * dy );
+        shape.lineToRelative( dx, 0 );
+        shape.moveTo( width / 2 + 1.33 * dx, height / 2 - dy );
+        shape.lineToRelative( 0, 2 * dy );
+        shape.lineToRelative( -dx, 0 );
+        shape.lineToRelative( 0, -2 * dy );
+        shape.lineToRelative( dx, 0 );
+        return shape.getGeneralPath();
+    }
+    
+    public static void main( String[] args ) {
+        PiccoloTestFrame testFrame = new PiccoloTestFrame("Button Test");
+        testFrame.addNode( new PlayPauseButton(200) );
+        testFrame.setVisible( true );
+    }
+}
