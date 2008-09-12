91abad35ac2b1942e07f52f285c9bcef2e93627a
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-09-12 03:26:50 +0000

    refactor

---
+++ trunk/simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/nodes/mediabuttons/ButtonIconSet.java
@@ -0,0 +1,54 @@
+package edu.colorado.phet.common.piccolophet.nodes.mediabuttons;
+
+import java.awt.*;
+
+import edu.colorado.phet.common.phetcommon.view.util.DoubleGeneralPath;
+
+/**
+ * Factory for creating shapes to use in a media button set.
+ */
+public class ButtonIconSet {
+    private double dx;
+    private double dy;
+    private int buttonWidth;
+    private int buttonHeight;
+
+    public ButtonIconSet( int buttonWidth, int buttonHeight ) {
+        this.buttonWidth = buttonWidth;
+        this.buttonHeight = buttonHeight;
+        dx = buttonHeight / 6;
+        dy = buttonHeight / 6;
+    }
+
+    public Shape createPlayIconShape() {
+        DoubleGeneralPath shape = new DoubleGeneralPath();
+        double width = buttonWidth;
+        double height = buttonHeight;
+        shape.moveTo( width / 2 + dx + dx / 4, height / 2 );
+        shape.lineToRelative( -dx * 2, dy );
+        shape.lineToRelative( 0, -2 * dy );
+        shape.lineToRelative( 2 * dx, dy );
+        return shape.getGeneralPath();
+    }
+
+    public Shape createPauseIconShape() {
+        DoubleGeneralPath shape = new DoubleGeneralPath();
+        double width = buttonWidth;
+        double height = buttonHeight;
+
+        double buttonSpacing = dx / 4;
+        double buttonWidth = dx * 0.8;
+        shape.moveTo( width / 2 - buttonSpacing, height / 2 - dy );
+        shape.lineToRelative( 0, 2 * dy );
+        shape.lineToRelative( -buttonWidth, 0 );
+        shape.lineToRelative( 0, -2 * dy );
+        shape.lineToRelative( buttonWidth, 0 );
+
+        shape.moveTo( width / 2 + buttonWidth + buttonSpacing, height / 2 - dy );
+        shape.lineToRelative( 0, 2 * dy );
+        shape.lineToRelative( -buttonWidth, 0 );
+        shape.lineToRelative( 0, -2 * dy );
+        shape.lineToRelative( buttonWidth, 0 );
+        return shape.getGeneralPath();
+    }
+}
--- trunk/simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/nodes/mediabuttons/PlayPauseButton.java
+++ trunk/simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/nodes/mediabuttons/PlayPauseButton.java
@@ -4,7 +4,6 @@ package edu.colorado.phet.common.piccolophet.nodes.mediabuttons;
 
 import java.awt.*;
 
-import edu.colorado.phet.common.phetcommon.view.util.DoubleGeneralPath;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.colorado.phet.common.piccolophet.test.PiccoloTestFrame;
 import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
@@ -13,18 +12,15 @@ import edu.umd.cs.piccolo.nodes.PPath;
 
 
 public class PlayPauseButton extends AbstractMediaButton {
-
-    private double dx;
-    private double dy;
     private boolean playing;
     private PPath iconNode;
+    private ButtonIconSet buttonIconSet;
 
     public PlayPauseButton( int buttonHeight ) {
         super( buttonHeight );
 
-        dx = buttonHeight / 6;
-        dy = buttonHeight / 6;
-        iconNode = new PhetPPath( createPlayIconShape(), Color.BLACK, new BasicStroke( 1 ), Color.LIGHT_GRAY );
+        buttonIconSet = new ButtonIconSet( buttonHeight, buttonHeight );
+        iconNode = new PhetPPath( buttonIconSet.createPlayIconShape(), Color.BLACK, new BasicStroke( 1 ), Color.LIGHT_GRAY );
         addChild( iconNode );
         addInputEventListener( new PBasicInputEventHandler() {
             public void mouseReleased( PInputEvent event ) {
@@ -40,43 +36,11 @@ public class PlayPauseButton extends AbstractMediaButton {
     }
 
     private void updateShape() {
-        iconNode.setPathTo( isPlaying() ? createPauseIconShape() : createPlayIconShape() );
+        iconNode.setPathTo( isPlaying() ? buttonIconSet.createPauseIconShape() : buttonIconSet.createPlayIconShape() );
     }
 
     private boolean isPlaying() {
         return playing;
-    }
-
-    private Shape createPlayIconShape() {
-        DoubleGeneralPath shape = new DoubleGeneralPath();
-        double width = getButtonDimension().width;
-        double height = getButtonDimension().height;
-        shape.moveTo( width / 2 + dx+dx/4, height / 2 );
-        shape.lineToRelative( -dx * 2, dy );
-        shape.lineToRelative( 0, -2 * dy );
-        shape.lineToRelative( 2 * dx, dy );
-        return shape.getGeneralPath();
-    }
-
-    private Shape createPauseIconShape() {
-        DoubleGeneralPath shape = new DoubleGeneralPath();
-        double width = getButtonDimension().width;
-        double height = getButtonDimension().height;
-
-        double buttonSpacing=dx/4;
-        double buttonWidth=dx*0.8;
-        shape.moveTo( width / 2 - buttonSpacing, height / 2 - dy );
-        shape.lineToRelative( 0, 2 * dy );
-        shape.lineToRelative( -buttonWidth, 0 );
-        shape.lineToRelative( 0, -2 * dy );
-        shape.lineToRelative( buttonWidth, 0 );
-
-        shape.moveTo( width / 2 + buttonWidth + buttonSpacing, height / 2 - dy );
-        shape.lineToRelative( 0, 2 * dy );
-        shape.lineToRelative( -buttonWidth, 0 );
-        shape.lineToRelative( 0, -2 * dy );
-        shape.lineToRelative( buttonWidth, 0 );
-        return shape.getGeneralPath();
     }
 
     public static void main( String[] args ) {
