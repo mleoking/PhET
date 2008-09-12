22cfcde39267f982b07d8c5f63eb8b21f0a61005
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-09-12 14:26:17 +0000

    specify outer class to facilitate pluggable parent class (no change in behavior)

--- trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/view/ClockControlPanel.java
+++ trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/view/ClockControlPanel.java
@@ -29,7 +29,7 @@ public class ClockControlPanel extends TimeControlPanel {
         this.clock = clock;
 
         // Update the clock in response to control panel events
-        addTimeControlListener( new TimeControlAdapter() {
+        addTimeControlListener( new TimeControlPanel.TimeControlAdapter() {
             public void stepPressed() {
                 clock.stepClockWhilePaused();
             }
