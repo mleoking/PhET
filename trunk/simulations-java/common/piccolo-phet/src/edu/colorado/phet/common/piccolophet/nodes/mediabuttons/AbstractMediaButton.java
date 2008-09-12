5477c5a207b2eee8538e0172b198c71bca55185a
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-09-12 00:50:01 +0000

    added highlights for mouse-over and mouse-press

--- trunk/simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/nodes/mediabuttons/AbstractMediaButton.java
+++ trunk/simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/nodes/mediabuttons/AbstractMediaButton.java
@@ -2,37 +2,73 @@
 
 package edu.colorado.phet.common.piccolophet.nodes.mediabuttons;
 
-import java.awt.Color;
-import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.awt.image.LookupOp;
+import java.awt.image.LookupTable;
 
-import edu.colorado.phet.common.phetcommon.resources.PhetCommonResources;
+import edu.colorado.phet.common.phetcommon.math.MathUtil;
 import edu.colorado.phet.common.phetcommon.resources.PhetResources;
 import edu.colorado.phet.common.phetcommon.view.util.BufferedImageUtils;
-import edu.colorado.phet.common.piccolophet.test.PiccoloTestFrame;
 import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
+import edu.umd.cs.piccolo.event.PInputEvent;
 import edu.umd.cs.piccolo.nodes.PImage;
 import edu.umd.cs.piccolo.util.PDimension;
 
 
 public class AbstractMediaButton extends PNode {
-    
-    PImage buttonImageNode;
-    
+
+    private PImage buttonImageNode;
+    private int buttonHeight;
+
     public AbstractMediaButton( int buttonHeight ) {
-        
-        BufferedImage image = new PhetResources( "piccolo-phet" ).getImage( "button-template.png" );
+        this.buttonHeight = buttonHeight;
+        BufferedImage image = getImage();
         image = BufferedImageUtils.multiScaleToHeight( image, buttonHeight );
-//        BufferedImage image = new BufferedImage(buttonHeight, buttonHeight, BufferedImage.TYPE_INT_ARGB);
-//        Graphics2D g = image.createGraphics();
-//        g.setPaint( Color.ORANGE );
-//        g.fillRect( 0, 0, buttonHeight, buttonHeight );
         buttonImageNode = new PImage( image );
-        
         addChild( buttonImageNode );
+
+        addInputEventListener( new PBasicInputEventHandler() {
+
+            public void mouseEntered( PInputEvent event ) {
+                buttonImageNode.setImage( new MyRescaleOp( 1.2, 0 ).filter( getImage(), null ) );
+            }
+
+            public void mouseExited( PInputEvent event ) {
+                buttonImageNode.setImage( getImage() );
+            }
+
+            public void mousePressed( PInputEvent event ) {
+                buttonImageNode.setImage( new MyRescaleOp( 0.9, 0 ).filter( getImage(), null ) );
+            }
+
+            public void mouseReleased( PInputEvent event ) {
+                buttonImageNode.setImage( getImage() );
+            }
+        } );
     }
-    
+
+    static class MyRescaleOp extends LookupOp {
+
+        public MyRescaleOp( final double scale, final double offset ) {
+            super( new LookupTable( 0, 4 ) {
+                public int[] lookupPixel( int[] src, int[] dest ) {
+                    for ( int i = 0; i < src.length; i++ ) {
+                        dest[i] = (int) MathUtil.clamp( 0, src[i] * scale + offset, 255 );
+                    }
+                    return dest;
+                }
+            }, null );
+        }
+    }
+
+    protected BufferedImage getImage() {
+        BufferedImage image = new PhetResources( "piccolo-phet" ).getImage( "button-template.png" );
+        return BufferedImageUtils.multiScaleToHeight( image, buttonHeight );
+    }
+
     public PDimension getButtonDimension() {
-        return new PDimension(buttonImageNode.getFullBounds().width, buttonImageNode.getFullBounds().height);
+        return new PDimension( buttonImageNode.getFullBounds().width, buttonImageNode.getFullBounds().height );
     }
+
 }
