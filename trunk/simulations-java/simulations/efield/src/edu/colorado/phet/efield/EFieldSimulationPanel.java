9a9800478dec5ec287e825a2d7256680e2e04262
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-09-16 04:20:33 +0000

    ported main & launcher to phetcommon

--- trunk/simulations-java/simulations/efield/src/edu/colorado/phet/efield/EFieldSimulationPanel.java
+++ trunk/simulations-java/simulations/efield/src/edu/colorado/phet/efield/EFieldSimulationPanel.java
@@ -8,7 +8,7 @@ import java.util.Vector;
 import javax.swing.*;
 import javax.swing.border.Border;
 
-import edu.colorado.phet.common.phetcommon.view.PhetLookAndFeel;
+import edu.colorado.phet.common.phetcommon.model.clock.IClock;
 import edu.colorado.phet.common.phetcommon.view.util.SimStrings;
 import edu.colorado.phet.efield.electron.core.ParticleContainer;
 import edu.colorado.phet.efield.electron.core.RandomSystemFactory;
@@ -35,10 +35,16 @@ import edu.colorado.phet.efield.electron.phys2d_efield.System2D;
 import edu.colorado.phet.efield.electron.phys2d_efield.SystemRunner;
 import edu.colorado.phet.efield.electron.utils.ResourceLoader;
 
-public class EFieldApplication extends JApplet {
-    // Localization
+public class EFieldSimulationPanel extends JPanel {
     public static final String localizedStringsPath = "efield/localization/efield-strings";
     private String applicationLocale = null;
+    private ElectricFieldPainter electricFieldPainter;
+    private ParticlePanel particlePanel;
+    private IClock clock;
+
+    public EFieldSimulationPanel( IClock clock ) {
+        this.clock = clock;
+    }
 
     public void init() {
         if ( applicationLocale == null ) {
@@ -49,9 +55,6 @@ public class EFieldApplication extends JApplet {
         }
         SimStrings.setStrings( localizedStringsPath );
 
-        if ( args == null ) {
-            args = new String[]{"10"};
-        }
         int num = 0;
         int x = 50;
         int y = 50;
@@ -63,7 +66,6 @@ public class EFieldApplication extends JApplet {
         CustomizableParticleFactory ef = new CustomizableParticleFactory( x, y, width, height, def );
         sppd.getDialog().addParticlePropertyListener( ef );
 
-        //ElectronFactory ef=new ElectronFactory(x,y,width,height);
         RandomSystemFactory randFact = new RandomSystemFactory( 10, num, x, y, width, height );
         ElectricForceLaw fieldLaw = new ElectricForceLaw( new DoublePoint( 0, 0 ) );
         randFact.addForceLaw( fieldLaw );
@@ -79,35 +81,33 @@ public class EFieldApplication extends JApplet {
         SystemRunner sr = new SystemRunner( sys, dt, wait );
         new Thread( sr ).start();
 
-        ParticlePanel pp = new ParticlePanel();
+        particlePanel = new ParticlePanel();
         //ParticlePainter painter=new DotPainter(Color.blue,16);
         BufferedImage bi = null;
         try {
-//            bi = ResourceLoader.loadBufferedImage("images/Electron3.GIF", pp, true);
-            bi = ResourceLoader.loadBufferedImage( "efield/images/electron9.gif", pp, true );
+//            bi = ResourceLoader.loadBufferedImage("images/Electron3.GIF", particlePanel, true);
+            bi = ResourceLoader.loadBufferedImage( "efield/images/electron9.gif", particlePanel, true );
         }
         catch( InterruptedException e ) {
             e.printStackTrace();
         }
-//        bi = electron.utils.AlphaFixer.patchAlpha(bi);
         ParticlePainter painter = new ImagePainter( bi );
 
-        randFact.updatePanel( pp, sys, painter );
-        //ParticleGrabber pg=new ParticleGrabber(pp,sys,sr);
-        ParticleGrabber pg = new ParticleThrower( pp, sys, sr, 5, 18 );
-        pp.addMouseListener( pg );
-        pp.addMouseMotionListener( pg );
+        randFact.updatePanel( particlePanel, sys, painter );
+        ParticleGrabber pg = new ParticleThrower( particlePanel, sys, sr, 5, 18 );
+        particlePanel.addMouseListener( pg );
+        particlePanel.addMouseMotionListener( pg );
 
-        getContentPane().setLayout( new BorderLayout() );
-        getContentPane().add( pp, BorderLayout.CENTER );
+        setLayout( new BorderLayout() );
+        add( particlePanel, BorderLayout.CENTER );
         validate();
 
         MediaControl mc = null;
         try {
-            mc = new MediaControl( sr, randFact, pp, painter,
-                                   ResourceLoader.loadBufferedImage( "efield/icons/media/Play24.gif", pp, false ),
-                                   ResourceLoader.loadBufferedImage( "efield/icons/media/Pause24.gif", pp, false ),
-                                   ResourceLoader.loadBufferedImage( "efield/icons/media/Stop24.gif", pp, false ) );
+            mc = new MediaControl( sr, randFact, particlePanel, painter,
+                                   ResourceLoader.loadBufferedImage( "efield/icons/media/Play24.gif", particlePanel, false ),
+                                   ResourceLoader.loadBufferedImage( "efield/icons/media/Pause24.gif", particlePanel, false ),
+                                   ResourceLoader.loadBufferedImage( "efield/icons/media/Stop24.gif", particlePanel, false ) );
         }
         catch( InterruptedException e ) {
             e.printStackTrace();
@@ -115,8 +115,8 @@ public class EFieldApplication extends JApplet {
         JPanel controlPanel = ( mc.getPanel() );
 
 
-        AddRemove ar = new AddRemove( new Vector(), ef, pp, painter );
-        ar.add( new PanelAdapter( pp, painter ) );
+        AddRemove ar = new AddRemove( new Vector(), ef, particlePanel, painter );
+        ar.add( new PanelAdapter( particlePanel, painter ) );
         SystemAdapter sa = new SystemAdapter( sys );
         ar.add( sa );
 
@@ -146,84 +146,36 @@ public class EFieldApplication extends JApplet {
         Border etched = BorderFactory.createEtchedBorder();
         Border b = BorderFactory.createTitledBorder( etched, SimStrings.get( "FieldNode2.ExternalFieldBorder" ) );
         fieldPanel.setBorder( b );
-        getContentPane().add( southPanel, BorderLayout.SOUTH );
+        add( southPanel, BorderLayout.SOUTH );
 
         mc.add( ar );/*Notify of reset actions.*/
         mc.add( pg );
 
         Painter wally = new WallPainter( x - 15, y - 15, width + 25, height + 25, new BasicStroke( 8, BasicStroke.CAP_ROUND, BasicStroke.JOIN_MITER ), Color.blue );
-        pp.add( wally );
+        particlePanel.add( wally );
 
         int numFieldDots = 6;
-//        if( args.length > 0 ) {
-//            numFieldDots = Integer.parseInt( args[0] );
-//        }
 
         VectorPainter vp2 = new DefaultVectorPainter( Color.blue, new BasicStroke( 2 ), Math.PI / 8, 4 );
-        ElectricFieldPainter electricFieldPainter = new ElectricFieldPainter( x, y, width, height, numFieldDots, numFieldDots, vp2 );
+        electricFieldPainter = new ElectricFieldPainter( x, y, width, height, numFieldDots, numFieldDots, vp2 );
         electricFieldPainter.addSource( fieldLaw );
         int maxArrowLength = 25;
         ChargeFieldSource cs = new ChargeFieldSource( cla, 120000, maxArrowLength ); //sa
         electricFieldPainter.addSource( cs );
 
-        pp.add( electricFieldPainter );
+        particlePanel.add( electricFieldPainter );
 
-        MenuConstructor emcee = new FieldMenuConstructor( cs, pp );
-        ParticlePopupListener ml = new ParticlePopupListener( pp, emcee );
-        pp.addMouseListener( ml );
+        MenuConstructor emcee = new FieldMenuConstructor( cs, particlePanel );
+        ParticlePopupListener ml = new ParticlePopupListener( particlePanel, emcee );
+        particlePanel.addMouseListener( ml );
+    }
 
-        JMenuBar jj = new JMenuBar();
-        setJMenuBar( jj );
-        //jj.add(emcee.getMenu(new Particle()));
-        DiscreteFieldSlider dfs = new DiscreteFieldSlider( electricFieldPainter, pp );
+    public JMenu getMenu() {
+        DiscreteFieldSlider dfs = new DiscreteFieldSlider( electricFieldPainter, particlePanel );
         JMenuItem fieldDiscretionItem = new JMenuItem( SimStrings.get( "FieldNode2.SetFieldDiscretenessMenuItem" ) );
         fieldDiscretionItem.addActionListener( dfs );
         JMenu fieldMenu = new JMenu( SimStrings.get( "FieldNode2.ElectricFieldMenuTitle" ) );
         fieldMenu.add( fieldDiscretionItem );
-        jj.add( fieldMenu );
-
-    }
-
-    static String[] args = null;
-
-    public static void main( final String[] argx ) {
-        SwingUtilities.invokeLater( new Runnable() {
-            public void run() {
-                PhetLookAndFeel phetLookAndFeel = new PhetLookAndFeel();
-                phetLookAndFeel.initLookAndFeel();
-                SimStrings.getInstance().init( argx, localizedStringsPath );
-
-                EFieldApplication j = new EFieldApplication();
-                j.setSize( new Dimension( 600, 600 ) );
-
-                String argsKey = "user.language=";
-                for ( int i = 0; i < argx.length; i++ ) {
-                    if ( argx[i].startsWith( argsKey ) ) {
-                        args = new String[argx.length - 1];
-                        break;
-                    }
-                }
-                if ( args == null ) {
-                    args = argx;
-                }
-                else {
-                    int k = 0;
-                    for ( int i = 0; i < argx.length; i++ ) {
-                        if ( !argx[i].startsWith( argsKey ) ) {
-                            args[k++] = argx[i];
-                        }
-                    }
-                }
-
-                j.init();
-
-                JFrame f = new JFrame( SimStrings.get( "EFieldApplication.title" ) );
-                f.setContentPane( j );
-                f.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-                f.setSize( new Dimension( 500, 600 ) );
-                //f.pack();
-                f.setVisible( true );
-            }
-        } );
+        return fieldMenu;
     }
 }
