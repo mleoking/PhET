79565fc40018407ea112e4f9415d181eddf87097
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-09-16 01:16:38 +0000

    cleanup

--- trunk/simulations-java/simulations/balloons/src/edu/colorado/phet/balloons/BalloonsSimulationPanel.java
+++ trunk/simulations-java/simulations/balloons/src/edu/colorado/phet/balloons/BalloonsSimulationPanel.java
@@ -24,13 +24,9 @@ import edu.colorado.phet.balloons.common.phys2d.Repaint;
 import edu.colorado.phet.balloons.common.phys2d.System2D;
 import edu.colorado.phet.common.phetcommon.model.clock.ClockAdapter;
 import edu.colorado.phet.common.phetcommon.model.clock.ClockEvent;
-import edu.colorado.phet.common.phetcommon.view.util.BufferedImageUtils;
 import edu.colorado.phet.common.phetcommon.view.HelpPanel;
+import edu.colorado.phet.common.phetcommon.view.util.BufferedImageUtils;
 
-
-/**
- * Test comment.
- */
 public class BalloonsSimulationPanel extends JPanel {
     private PainterPanel painterPanel;
     private LayeredPainter layeredPainter;
