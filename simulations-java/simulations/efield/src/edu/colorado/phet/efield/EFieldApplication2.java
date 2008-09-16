5230476d56a0546cac3209a642aa51f2dca2df8d
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-09-16 04:20:33 +0000

    ported main & launcher to phetcommon

---
+++ simulations-java/simulations/efield/src/edu/colorado/phet/efield/EFieldApplication2.java
@@ -0,0 +1,59 @@
+package edu.colorado.phet.efield;
+
+import java.awt.*;
+import java.io.IOException;
+
+import javax.swing.*;
+
+import edu.colorado.phet.common.phetcommon.application.Module;
+import edu.colorado.phet.common.phetcommon.application.PhetApplication;
+import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
+import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
+import edu.colorado.phet.common.phetcommon.resources.PhetResources;
+import edu.colorado.phet.common.phetcommon.view.PhetLookAndFeel;
+import edu.colorado.phet.common.phetcommon.view.util.FrameSetup;
+import edu.colorado.phet.common.phetcommon.view.util.SimStrings;
+import edu.colorado.phet.efield.electron.electricField.DiscreteFieldSlider;
+
+public class EFieldApplication2 extends PhetApplication {
+    private EFieldModule module;
+
+    public EFieldApplication2( PhetApplicationConfig config ) {
+        super( config );
+        module = new EFieldModule( config );
+        addModule( module );
+        getPhetFrame().addMenu(  module.getMenu());
+    }
+
+    public static class EFieldApplicationConfig extends PhetApplicationConfig {
+        public EFieldApplicationConfig( String[] commandLineArgs ) {
+            super( commandLineArgs, new FrameSetup.CenteredWithSize( 600,600), new PhetResources( "efield" ) );
+            super.setApplicationConstructor( new ApplicationConstructor() {
+                public PhetApplication getApplication( PhetApplicationConfig config ) {
+                    return new EFieldApplication2( config );
+                }
+            } );
+            super.setLookAndFeel( new PhetLookAndFeel() );
+        }
+    }
+
+    private class EFieldModule extends Module {
+        private EFieldSimulationPanel simulationPanel = new EFieldSimulationPanel( getClock() );
+
+        public EFieldModule( PhetApplicationConfig config ) {
+            super( config.getName(), new ConstantDtClock( 30, 1 ) );
+            simulationPanel.init();
+            setSimulationPanel( simulationPanel );
+            setClockControlPanel( null );
+            setLogoPanelVisible( false );
+        }
+
+        public JMenu getMenu() {
+            return simulationPanel.getMenu(); 
+        }
+    }
+
+    public static void main( String[] args ) {
+        new EFieldApplicationConfig( args ).launchSim();
+    }
+}
