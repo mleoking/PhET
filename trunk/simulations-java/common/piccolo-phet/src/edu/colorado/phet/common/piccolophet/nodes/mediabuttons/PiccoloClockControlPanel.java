cac14835daf50b865c6b77be1d103423eb6409f2
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-09-19 12:37:18 +0000

    fix comment about duplicated code

--- trunk/simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/nodes/mediabuttons/PiccoloClockControlPanel.java
+++ trunk/simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/nodes/mediabuttons/PiccoloClockControlPanel.java
@@ -6,7 +6,7 @@ import edu.colorado.phet.common.phetcommon.model.clock.IClock;
 import edu.colorado.phet.common.phetcommon.view.TimeControlPanel;
 
 
-//todo: this duplicates code with TimeControlPanel
+//todo: this duplicates code with ClockControlPanel
 public class PiccoloClockControlPanel extends PiccoloTimeControlPanel{
     private IClock clock;
     private ClockAdapter clockListener;
