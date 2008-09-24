23e27d0ed9637a6ddb6b47811e2cb278053c43cf
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-09-24 15:29:27 +0000

    switch to phetcommon clock

--- trunk/simulations-java/simulations/motion-2d/src/edu/colorado/phet/motion2d/Motion2DSimulationPanel.java
+++ trunk/simulations-java/simulations/motion-2d/src/edu/colorado/phet/motion2d/Motion2DSimulationPanel.java
@@ -1,28 +1,29 @@
 package edu.colorado.phet.motion2d;
 
-import edu.colorado.phet.common.phetcommon.view.util.ImageLoader;
-import edu.colorado.phet.common.phetcommon.view.util.SimStrings;
-import edu.colorado.phet.common.phetcommon.model.clock.IClock;
-
-import javax.swing.*;
 import java.awt.*;
 import java.io.IOException;
 import java.util.Locale;
 
-public class Motion2DSimulationPanel extends JPanel{
+import javax.swing.*;
+
+import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
+import edu.colorado.phet.common.phetcommon.view.util.ImageLoader;
+import edu.colorado.phet.common.phetcommon.view.util.SimStrings;
+
+public class Motion2DSimulationPanel extends JPanel {
 
     private Motion2DPanel motion2DPanel;
     private Image ballImage, emptyImage;
     Cursor hide, show;        //Invisible and visible mouse cursors
-    private IClock clock;
+    private ConstantDtClock clock;
 
-    public Motion2DSimulationPanel( IClock clock ) {
-        this.clock=clock;
+    public Motion2DSimulationPanel( ConstantDtClock clock ) {
+        this.clock = clock;
     }
 
     public void init() {
         String applicationLocale = Toolkit.getDefaultToolkit().getProperty( "javaws.phet.locale", null );
-        if( applicationLocale != null && !applicationLocale.equals( "" ) ) {
+        if ( applicationLocale != null && !applicationLocale.equals( "" ) ) {
             SimStrings.getInstance().setLocale( new Locale( applicationLocale ) );
         }
         SimStrings.getInstance().addStrings( Motion2DApplication.localizedStringsPath );
@@ -47,5 +48,9 @@ public class Motion2DSimulationPanel extends JPanel{
 
     public Image getBallImage() {
         return ballImage;
+    }
+
+    public ConstantDtClock getClock() {
+        return clock;
     }
 }
