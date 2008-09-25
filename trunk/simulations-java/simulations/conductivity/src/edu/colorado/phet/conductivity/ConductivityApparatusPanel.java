41c3d4d9011f164d29ba49b601e053c08a7e9b4d
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-09-25 15:50:27 +0000

    intermediate progress bringing forward to trunk phetcommon

---
+++ trunk/simulations-java/simulations/conductivity/src/edu/colorado/phet/conductivity/ConductivityApparatusPanel.java
@@ -0,0 +1,12 @@
+package edu.colorado.phet.conductivity;
+
+import edu.colorado.phet.common.conductivity.view.ApparatusPanel;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Sam
+ * Date: Sep 25, 2008
+ * Time: 9:31:54 AM
+ */
+public class ConductivityApparatusPanel extends ApparatusPanel {
+}
