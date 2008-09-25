83e64575d00d07e8b08142c84c8c1da70606a2af
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-09-25 17:04:09 +0000

    remove explicit create of PhetLookAndFeel

--- trunk/simulations-java/simulations/radio-waves/src/edu/colorado/phet/radiowaves/RadioWavesApplication.java
+++ trunk/simulations-java/simulations/radio-waves/src/edu/colorado/phet/radiowaves/RadioWavesApplication.java
@@ -14,7 +14,6 @@ import javax.swing.JMenu;
 import edu.colorado.phet.common.phetcommon.application.PhetApplication;
 import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
 import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
-import edu.colorado.phet.common.phetcommon.view.PhetLookAndFeel;
 import edu.colorado.phet.common.phetcommon.view.util.FrameSetup;
 import edu.colorado.phet.radiowaves.view.WaveMediumGraphic;
 
@@ -50,7 +49,6 @@ public class RadioWavesApplication extends PhetApplication {
                     return new RadioWavesApplication( config );
                 }
             } );
-            super.setLookAndFeel( new PhetLookAndFeel() );//XXX delete this, not needed
         }
     }
     
