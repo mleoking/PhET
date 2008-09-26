76882c7f9e8a73730560077b9973e4ddd098971b
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-09-26 18:32:19 +0000

    improvements for Java Version Checker

--- simulations-java/common/java-version-checker/src/edu/colorado/phet/javaversionchecker/JavaVersionChecker.java
+++ simulations-java/common/java-version-checker/src/edu/colorado/phet/javaversionchecker/JavaVersionChecker.java
@@ -2,17 +2,16 @@ package edu.colorado.phet.javaversionchecker;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 import javax.swing.*;
-
-import edu.colorado.phet.common.phetcommon.application.JARLauncher;
 
 public class JavaVersionChecker {
     public static void main( String[] args ) throws IOException, InvocationTargetException, ClassNotFoundException, NoSuchMethodException, IllegalAccessException {
         String javaVersion = System.getProperty( "java.version" );
-        System.out.println( "javaVersion = " + javaVersion );
-        if ( javaVersion.startsWith( "1.4" ) || javaVersion.startsWith( "1.3" ) || true ) {
+        if ( javaVersion.startsWith( "1.4" ) || javaVersion.startsWith( "1.3" ) || javaVersion.startsWith( "1.2" ) || javaVersion.startsWith( "1.1" ) ) {
             showJavaVersionErrorDialog();
+            System.exit( 0 );//daemon threads leftover
         }
         else {
             launchApplication( args );
@@ -20,10 +19,15 @@ public class JavaVersionChecker {
     }
 
     private static void launchApplication( String[] args ) throws ClassNotFoundException, InvocationTargetException, IOException, NoSuchMethodException, IllegalAccessException {
-        JARLauncher.main( args );
+
+        //access through reflection so this class can be compiled independently of anything
+        Class c = Class.forName( "edu.colorado.phet.common.phetcommon.application.JARLauncher" );
+
+        Method method = c.getMethod( "main", new Class[]{args.getClass()} );
+        method.invoke( null, new Object[]{args} );
     }
 
     private static void showJavaVersionErrorDialog() {
-        JOptionPane.showMessageDialog( null, "PhET requires Java 1.5+.\nDetected version: " + System.getProperty( "java.version" ) + "\n\nPlease visit java.com to get the latest version of Java" );
+        JOptionPane.showMessageDialog( null, "PhET requires Java 1.5+.\nDetected version: " + System.getProperty( "java.version" ) + "\n\nPlease visit java.com to get the latest version of Java." );
     }
 }
