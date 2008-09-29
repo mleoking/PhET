ae839ce2ea379badd2f10d0b78b79e8c72dfe4ab
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-09-29 23:47:50 +0000

    javadoc

--- simulations-java/simulations/glaciers/src/edu/colorado/phet/glaciers/view/UnitsChangeListener.java
+++ simulations-java/simulations/glaciers/src/edu/colorado/phet/glaciers/view/UnitsChangeListener.java
@@ -2,7 +2,19 @@
 
 package edu.colorado.phet.glaciers.view;
 
+/**
+ * UnitsChangeListener is the interface implemented by all listeners who
+ * are interested in switching between English and metric units.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
 public interface UnitsChangeListener {
+    
+    /**
+     * Called when units are changed.
+     * 
+     * @param englishUnits true if units were changed to English, false if changed to metric
+     */
     public void unitsChanged( boolean englishUnits );
 }
 
