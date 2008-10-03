3fd0dbbeecd29cfbf00ae2026b6d29675052bcd8
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-10-03 03:41:08 +0000

    add locale info

--- simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/tracking/TrackingInfo.java
+++ simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/tracking/TrackingInfo.java
@@ -2,8 +2,10 @@ package edu.colorado.phet.common.phetcommon.tracking;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
+import edu.colorado.phet.common.phetcommon.resources.PhetResources;
 
 public class TrackingInfo {
     private TrackingEntry[] entries;
@@ -13,6 +15,7 @@ public class TrackingInfo {
                 new TrackingEntry( "version", config.getVersion().toString() ),
                 new TrackingEntry( "project", config.getProjectName() ),
                 new TrackingEntry( "sim", config.getFlavor() ),
+                new TrackingEntry( "sim-locale", PhetResources.readLocale().toString() ),
 
                 new TrackingEntry.SystemProperty( "os.name" ),
                 new TrackingEntry.SystemProperty( "os.version" ),
@@ -24,6 +27,7 @@ public class TrackingInfo {
 
                 new TrackingEntry.SystemProperty( "user.country" ),
                 new TrackingEntry.SystemProperty( "user.timezone" ),
+                new TrackingEntry( "default-locale", Locale.getDefault().toString() ),
 
                 //for some reason, user.timezone only appears if the next line is used (otherwise user.timezone is empty or null)
                 new TrackingEntry( "time", new SimpleDateFormat( "yyyy-MM-dd_HH:mm:ss" ).format( new Date() ) )
