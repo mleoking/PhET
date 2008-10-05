b42e1b9f1228e695804773bf6ef92f7b493e3083
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-10-05 17:06:11 +0000

    added UI for updates

--- trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/preferences/UpdatesPreferencesPanel.java
+++ trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/preferences/UpdatesPreferencesPanel.java
@@ -1,6 +1,23 @@
 package edu.colorado.phet.common.phetcommon.preferences;
 
+import java.awt.*;
+
 import javax.swing.*;
 
 public class UpdatesPreferencesPanel extends JPanel {
+    public UpdatesPreferencesPanel() {
+        setLayout( new GridBagLayout() );
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.gridy = GridBagConstraints.RELATIVE;
+        constraints.gridx = 0;
+        constraints.gridwidth = 1;
+
+        JCheckBox autoCheck = new JCheckBox( "Automatically check for updates", true );
+        add( Box.createRigidArea( new Dimension( 50, 20 ) ), constraints );
+        add( autoCheck, constraints );
+        add( Box.createRigidArea( new Dimension( 50, 10 ) ), constraints );
+        add( new PreferencesScopePanel(), constraints );
+        add( Box.createRigidArea( new Dimension( 50, 10 ) ), constraints );
+        add( new JButton( "Check for update..." ), constraints );
+    }
 }
