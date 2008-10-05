322cf55d26bf1c772b73b07ab2d0e11117e2bb9f
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-10-05 17:44:40 +0000

    started implementation of manual update check

---
+++ trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/preferences/IManuallyCheckForUpdates.java
@@ -0,0 +1,5 @@
+package edu.colorado.phet.common.phetcommon.preferences;
+
+public interface IManuallyCheckForUpdates {
+    void checkForUpdates();
+}
--- trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/preferences/PreferencesDialog.java
+++ trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/preferences/PreferencesDialog.java
@@ -12,17 +12,18 @@ import edu.colorado.phet.common.phetcommon.tracking.Tracker;
 import edu.colorado.phet.common.phetcommon.view.util.FrameSetup;
 
 public class PreferencesDialog extends JDialog {
-    public PreferencesDialog( Frame owner, Tracker tracker ) {
+    public PreferencesDialog( Frame owner, Tracker tracker, IManuallyCheckForUpdates iCheckForUpdates ) {
         super( owner, "Preferences", true );
-        setContentPane( new PreferencesPanel( tracker ) );
+        setContentPane( new PreferencesPanel( iCheckForUpdates, tracker ) );
         pack();
     }
 
     public static void main( String[] args ) {
-        PhetApplicationConfig config = new PhetApplicationConfig( args, new FrameSetup.CenteredWithSize( 1024, 768 ), new PhetResources( "nuclear-physics" ), "alpha-radiation" );
+//        PhetApplicationConfig config = new PhetApplicationConfig( args, new FrameSetup.CenteredWithSize( 1024, 768 ), new PhetResources( "nuclear-physics" ), "alpha-radiation" );
+        PhetApplicationConfig config = new PhetApplicationConfig( args, new FrameSetup.CenteredWithSize( 1024, 768 ), new PhetResources( "balloons" ), "balloons" );
         Tracker tracker = config.getTracker();
         tracker.startTracking();
-        PreferencesDialog preferencesDialog = new PreferencesDialog( null, tracker );
+        PreferencesDialog preferencesDialog = new PreferencesDialog( null, tracker, new DefaultManualCheckForUpdates( config.getProjectName(), config.getVersion(), config.getName() ) );
         preferencesDialog.addWindowListener( new WindowAdapter() {
             public void windowClosing( WindowEvent e ) {
                 System.exit( 0 );
