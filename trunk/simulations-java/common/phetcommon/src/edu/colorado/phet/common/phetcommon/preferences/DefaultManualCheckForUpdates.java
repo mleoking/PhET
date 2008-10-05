6f80258f913de1ae70fb699bf196bbc538769e06
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-10-05 20:35:28 +0000

    refactor message dialogs

--- trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/preferences/DefaultManualCheckForUpdates.java
+++ trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/preferences/DefaultManualCheckForUpdates.java
@@ -1,7 +1,11 @@
 package edu.colorado.phet.common.phetcommon.preferences;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 import javax.swing.*;
 
@@ -31,22 +35,29 @@ public class DefaultManualCheckForUpdates implements IManuallyCheckForUpdates {
                 String title = "New version available";
                 String text = "<html>Your current version of " + humanReadableSimName + " is " + currentVersion.formatForTitleBar() + ".  A newer version (" + remoteVersion.formatForTitleBar() + ") is available.<br>" +
                               "A web browser is being opened to the PhET website, where you can get the new version.<html>";
-                if ( window instanceof Frame || window == null ) {
-                    new UpdateResultDialog( (Frame) window, title, text ).setVisible( true );
-                }
-                else if ( window instanceof Dialog ) {
-                    new UpdateResultDialog( (Dialog) window, title, text ).setVisible( true );
-                }
+                UpdateResultDialog.showDialog( window, title, text );
             }
 
-            public void exceptionInUpdateCheck( IOException e ) {
-                JOptionPane.showMessageDialog( null, "An error was encountered while trying to access the PhET website. \n" +
-                                                     "Please try again later, or visit http://phet.colorado.edu. \n" +
-                                                     "If the problem persists, please contact phethelp@colorado.edu." );
+            public void exceptionInUpdateCheck( final IOException e ) {
+                String title = "Error during update check";
+                String text = "<html>" + "An error was encountered while trying to access the PhET website.<br>Please try again later, or visit <a href=\"http://phet.colorado.edu\">http://phet.colorado.edu</a>.<br>If the problem persists, please contact <a href=\"mailto:phethelp@colorado.edu\">phethelp@colorado.edu</a>." + "<html>";
+                final JButton details = new JButton( "Details..." );
+                details.addActionListener( new ActionListener() {
+                    public void actionPerformed( ActionEvent event ) {
+                        final StringWriter result = new StringWriter();
+                        final PrintWriter printWriter = new PrintWriter( result );
+                        e.printStackTrace( printWriter );
+                        JOptionPane.showMessageDialog( details, result.getBuffer() );
+                    }
+                } );
+                UpdateResultDialog updateResultDialog = UpdateResultDialog.createDialog( window, title, text );
+                updateResultDialog.addComponent( details );
+                updateResultDialog.pack();
+                updateResultDialog.setVisible( true );
             }
 
             public void noNewVersionAvailable( PhetVersion currentVersion, PhetVersion remoteVersion ) {
-                JOptionPane.showMessageDialog( null, "You have the current version (" + currentVersion.formatForTitleBar() + ") of " + humanReadableSimName + "." );
+                UpdateResultDialog.showDialog( window, "Up to date", "<html>" + "You have the current version (" + currentVersion.formatForTitleBar() + ") of " + humanReadableSimName + "." + "<html>" );
             }
         };
         updateManager.addListener( listener );
--- trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/preferences/UpdateResultDialog.java
+++ trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/preferences/UpdateResultDialog.java
@@ -12,6 +12,9 @@ import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.phetcommon.view.util.SwingUtils;
 
 public class UpdateResultDialog extends JDialog {
+    private JPanel contentPane;
+    private GridBagConstraints constraints;
+
     public UpdateResultDialog( Frame parent, String title, String html ) {
         super( parent, title, true );
         init( html );
@@ -23,9 +26,9 @@ public class UpdateResultDialog extends JDialog {
     }
 
     private void init( String html ) {
-        JPanel contentPane = new JPanel();
+        contentPane = new JPanel();
         contentPane.setLayout( new GridBagLayout() );
-        GridBagConstraints constraints = new GridBagConstraints();
+        constraints = new GridBagConstraints();
         constraints.gridy = GridBagConstraints.RELATIVE;
         constraints.gridx = 0;
         constraints.gridwidth = 1;
@@ -47,6 +50,26 @@ public class UpdateResultDialog extends JDialog {
         logoPanel.add( copyrightLabel );
 
         return logoPanel;
+    }
+
+    public static UpdateResultDialog createDialog( Window window, String title, String text ) {
+        if ( window instanceof Frame || window == null ) {
+            return new UpdateResultDialog( (Frame) window, title, text );
+        }
+        else if ( window instanceof Dialog ) {
+            return new UpdateResultDialog( (Dialog) window, title, text );
+        }
+        else {
+            throw new RuntimeException( "Illegal window type: " + window.getClass() );
+        }
+    }
+
+    public static void showDialog( Window window, String title, String text ) {
+        createDialog( window, title, text ).setVisible( true );
+    }
+
+    public void addComponent( JComponent component ) {
+        contentPane.add( component, constraints );
     }
 
     private class OKButton extends JButton {
