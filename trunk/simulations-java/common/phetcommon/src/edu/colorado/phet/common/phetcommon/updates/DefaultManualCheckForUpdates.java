971a9caf5992545d87edf4b9f0085903abe15803
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-10-06 05:43:57 +0000

    minor fixes (font family, layout, preferences link)

--- trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/updates/DefaultManualCheckForUpdates.java
+++ trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/updates/DefaultManualCheckForUpdates.java
@@ -9,6 +9,7 @@ import java.io.StringWriter;
 
 import javax.swing.*;
 
+import edu.colorado.phet.common.phetcommon.application.PhetAboutDialog;
 import edu.colorado.phet.common.phetcommon.preferences.IManualUpdateChecker;
 import edu.colorado.phet.common.phetcommon.resources.PhetVersion;
 
@@ -33,9 +34,9 @@ public class DefaultManualCheckForUpdates implements IManualUpdateChecker {
 
             public void newVersionAvailable( PhetVersion currentVersion, PhetVersion remoteVersion ) {
                 String title = "New version available";
-                String text = "<html>Your current version of " + humanReadableSimName + " is " + currentVersion.formatForTitleBar() + ".  A newer version (" + remoteVersion.formatForTitleBar() + ") is available.<br>" +
+                String html = "<html>" + PhetAboutDialog.HTML_CUSTOM_STYLE + "Your current version of " + humanReadableSimName + " is " + currentVersion.formatForTitleBar() + ".  A newer version (" + remoteVersion.formatForTitleBar() + ") is available.<br>" +
                               "A web browser will be opened to the PhET website, where you can get the new version.<html>";
-                UpdateResultDialog dialog = UpdateResultDialog.createDialog( window, title, text );
+                UpdateResultDialog dialog = UpdateResultDialog.createDialog( window, title, html );
                 dialog.addOKButton();
                 dialog.addListener( new UpdateResultDialog.Listener() {
                     public void dialogFinished() {
@@ -48,7 +49,7 @@ public class DefaultManualCheckForUpdates implements IManualUpdateChecker {
 
             public void exceptionInUpdateCheck( final IOException e ) {
                 String title = "Error during update check";
-                String text = "<html>" + "An error was encountered while trying to access the PhET website.<br>Please try again later, or visit <a href=\"http://phet.colorado.edu\">http://phet.colorado.edu</a>.<br>If the problem persists, please contact <a href=\"mailto:phethelp@colorado.edu\">phethelp@colorado.edu</a>." + "<html>";
+                String html = "<html>" + PhetAboutDialog.HTML_CUSTOM_STYLE+"An error was encountered while trying to access the PhET website.<br>Please try again later, or visit <a href=\"http://phet.colorado.edu\">http://phet.colorado.edu</a>.<br>If the problem persists, please contact <a href=\"mailto:phethelp@colorado.edu\">phethelp@colorado.edu</a>." + "<html>";
                 final JButton details = new JButton( "Details..." );
                 details.addActionListener( new ActionListener() {
                     public void actionPerformed( ActionEvent event ) {
@@ -58,7 +59,7 @@ public class DefaultManualCheckForUpdates implements IManualUpdateChecker {
                         JOptionPane.showMessageDialog( details, result.getBuffer() );
                     }
                 } );
-                UpdateResultDialog updateResultDialog = UpdateResultDialog.createDialog( window, title, text );
+                UpdateResultDialog updateResultDialog = UpdateResultDialog.createDialog( window, title, html );
                 JPanel buttonPanel = new JPanel();
                 buttonPanel.add( updateResultDialog.createOKButton() );
                 buttonPanel.add( details );
@@ -68,7 +69,7 @@ public class DefaultManualCheckForUpdates implements IManualUpdateChecker {
             }
 
             public void noNewVersionAvailable( PhetVersion currentVersion, PhetVersion remoteVersion ) {
-                UpdateResultDialog.showDialog( window, "Up to date", "<html>" + "You have the current version (" + currentVersion.formatForTitleBar() + ") of " + humanReadableSimName + "." + "<html>" );
+                UpdateResultDialog.showDialog( window, "Up to date", "<html>" + PhetAboutDialog.HTML_CUSTOM_STYLE+"You have the current version (" + currentVersion.formatForTitleBar() + ") of " + humanReadableSimName + "." + "<html>" );
             }
         };
         updateManager.addListener( listener );
