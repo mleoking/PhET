74fd48f330f6a3769ab4964cf31d3e62a528314d
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-10-08 07:27:38 +0000

    converted demo to application (not applet)

--- simulations-java/contrib/perlin-2004/render/srr/Demo2.java
+++ simulations-java/contrib/perlin-2004/render/srr/Demo2.java
@@ -10,69 +10,96 @@
  * 
  */
 package srr;
-import java.awt.Graphics;
-import java.awt.Panel;
+
+import render.*;
+
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import java.awt.geom.Point2D;
 
 import javax.swing.*;
-import render.*;
-import render.demo.PointApp;
 
 public class Demo2 extends JFrame implements Renderable {
 
-   //the renderer
-   RenderablePanel render;
+    //the renderer
+    RenderablePanel render;
 
-	public Demo2() {
-		super("simple example");
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setSize(600, 600);
+    public Demo2() {
+        super( "simple example" );
+        this.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        this.setSize( 600, 600 );
 
-		render = new RenderPanel();
+        render = new RenderPanel();
 //		render = new RenderPanelGL(600,600);
 
-		render.setRenderable(this);
+        render.setRenderable( this );
 
-		this.setBounds(0, 0, 600, 600);
-		this.getContentPane().setBounds(0, 0, 600, 600);
-		this.getContentPane().add((Panel) render);
+        this.setBounds( 0, 0, 600, 600 );
+        this.getContentPane().setBounds( 0, 0, 600, 600 );
+        this.getContentPane().add( (Panel) render );
 
-		setVisible(true);
-	}
+        setVisible( true );
+    }
 
-	public void initialize() {
-		render.addLight(1, 1, 1, 1, 1, 1); // LIGHTS
-		render.addLight(0, 1, 0, 1, 1, 1);
-		render.setBgColor(.2, .2, .8); // BACKGROUND COLOR
-		render.push();
-		render.transform(render.getWorld()); // INITIAL VIEW ANGLE
-		render.pop();
-		Material red = new Material();
-		red.setDiffuse(1, 0, 0);
-		Geometry cube1 = render.getWorld().add().cube().setMaterial(red);
+    public void initialize() {
+        render.addLight( 1, 1, 1, 1, 1, 1 ); // LIGHTS
+        render.addLight( 0, 1, 0, 1, 1, 1 );
+        render.setBgColor( .2, .2, .8 ); // BACKGROUND COLOR
+        render.push();
+        render.transform( render.getWorld() ); // INITIAL VIEW ANGLE
+        render.pop();
+        Material red = new Material();
+        red.setDiffuse( 1, 0, 0 );
 
-		render.push();
-		render.rotateY(-Math.PI/12.);
-		render.rotateX(-Math.PI/2.4);
-		render.scale(2,3,1);
-		render.transform(cube1);
-		render.pop();
+        render.getWorld().add().cube().setMaterial( red );    // GOLD BALL
+        render.getWorld().add().cube().setMaterial( red );    // GOLD BALL
+        render.getWorld().child[1].getMatrix().translate( 2.5, 0, 0 );
 
-	}
+        render.addMouseMotionListener( new MouseMotionListener() {
+            Point2D lastPoint;
 
-	public void animate(double time) {
-	   render.pause();
-		//no need to keep re-rendering if nothing is changing
-	}
+            public void mouseDragged( MouseEvent e ) {
+                if ( lastPoint != null ) {
+                    final double dx = e.getX() - lastPoint.getX();
+                    final double dy = e.getY() - lastPoint.getY();
+
+                    Geometry geometry = render.getGeometry( e.getX(), e.getY() );
+                    if ( geometry != null ) {
+                        geometry.getMatrix().translate( dx / 50, -dy / 50, 0 );
+                        repaint();
+                    }
+                }
+                lastPoint = e.getPoint();
+            }
+
+            public void mouseMoved( MouseEvent e ) {
+                lastPoint = e.getPoint();
+                if ( render.getGeometry( e.getX(), e.getY() ) != null ) {
+                    setCursor( Cursor.getPredefinedCursor( Cursor.HAND_CURSOR ) );
+                }
+                else {
+                    setCursor( Cursor.getDefaultCursor() );
+                }
+            }
+        } );
+    }
+
+    public void animate( double time ) {
+        render.pause();
+        //no need to keep re-rendering if nothing is changing
+    }
 
 
-	public static void main(String[] args) {
-		Demo2 pointApp = new Demo2();
-		pointApp.render.init();
-		pointApp.render.start();
-	}
+    public static void main( String[] args ) {
+        Demo2 pointApp = new Demo2();
+        pointApp.render.init();
+        pointApp.render.start();
+    }
 
-   /**
-    * @see render.Renderable#drawOverlay(java.awt.Graphics)
-    */
-   public void drawOverlay(Graphics g) { }
+    /**
+     * @see render.Renderable#drawOverlay(java.awt.Graphics)
+     */
+    public void drawOverlay( Graphics g ) {
+    }
 }
