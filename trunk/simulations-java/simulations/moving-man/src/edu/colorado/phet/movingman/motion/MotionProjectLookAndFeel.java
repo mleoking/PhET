c91566d72d6fda69438c9a5fedc02a7decf43adb
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-10-09 23:28:38 +0000

    10-08-2008: use PhetApplicationConfig.launchSim

--- trunk/simulations-java/simulations/moving-man/src/edu/colorado/phet/movingman/motion/MotionProjectLookAndFeel.java
+++ trunk/simulations-java/simulations/moving-man/src/edu/colorado/phet/movingman/motion/MotionProjectLookAndFeel.java
@@ -1,6 +1,6 @@
 package edu.colorado.phet.movingman.motion;
 
-import java.awt.*;
+import java.awt.Color;
 
 import edu.colorado.phet.common.phetcommon.view.PhetLookAndFeel;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
@@ -9,14 +9,14 @@ import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
  * Created by: Sam
  * Dec 4, 2007 at 1:27:26 PM
  */
-public class MotionProjectLookAndFeel {
+public class MotionProjectLookAndFeel extends PhetLookAndFeel {
+    
     public static final Color BACKGROUND_COLOR = new Color( 200, 240, 200 );
     public static final Color CHART_BACKGROUND_COLOR = new Color( 250, 247, 224 );
 
-    public static void init() {
-        PhetLookAndFeel plaf = new PhetLookAndFeel();
-        plaf.setBackgroundColor( BACKGROUND_COLOR );
-        plaf.setFont( new PhetFont( 14, true ) );
-        plaf.initLookAndFeel();
+    public MotionProjectLookAndFeel() {
+        super();
+        setBackgroundColor( BACKGROUND_COLOR );
+        setFont( new PhetFont( 14, true ) );
     }
 }
