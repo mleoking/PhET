bc25d8b1624318be61328b8e7400b9d5621a76c4
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-10-09 18:32:24 +0000

    delete bernoulli project, horribly out of data, unpublished (Sam made me do it)

--- simulations-java/simulations/bernoulli/phetcommon/src/edu/colorado/phet/common/bernoulli/bernoulli/clock2/SimulationTimeListener.java
+++
@@ -1,9 +0,0 @@
-/*, 2003.*/
-package edu.colorado.phet.common.bernoulli.bernoulli.clock2;
-
-/**
- * Listens for time simulation events.
- */
-public interface SimulationTimeListener {
-    public void simulationTimeIncreased(double dt);
-}
