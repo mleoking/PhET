37ce72cd8f5fd0d7ed72f85c32beafbaecc97a3e
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-10-09 19:42:10 +0000

    added sourceforge page with license info

---
+++ trunk/simulations-java/contrib/JGV/jgv_files/projects_003.js
@@ -0,0 +1,19 @@
+document.write('');
+
+function FusionToMR(sitezone,attribs){
+
+document.write('<scr'+'ipt language="Javascr'+'ipt" src="http://ad.doubleclick.net/adj/ostg.'+sitezone+'3_medrec;'+attribs+'?">');
+document.write('</scr'+'ipt>');
+
+}
+
+document.write('\n');
+
+var sitezone="sourceforge/multimed_p8";
+var attribs="pg=/projects;psrch=0;logged_in=0;tile=1;tpc=jgv;tpc=java;tpc=visualization;tpc=viewers;tpc=3drendering;tpc=graphics;tpc=multimedia;tpc=mathematics;tpc=scientific;ord=8397276571151984";
+// var tile="1";
+// var ord="8397276571151984";
+
+FusionToMR(sitezone,attribs);
+
+document.write('');
---
+++ trunk/simulations-java/contrib/JGV/jgv_files/projects_004.js
@@ -0,0 +1,8 @@
+document.write('');
+
+google_ad_client = 'ca-ostg_js';
+google_ad_channel = 'sourceforge_test';
+google_ad_type = 'text_image_flash_html';
+ostg_style_type = 'clean_blue';
+
+document.write('\n<script type=\"text/javascript\" src=\"http://s.fsdn.com/adops/google/ads/imu.js\"><\/script>');document.write('\n<script type=\"text/javascript\" src=\"http://pagead2.googlesyndication.com/pagead/show_ads.js\"><\/script>');document.write('');
