f2bfec1f170dd4fc2fff85f6ffac6540a034f43f
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-10-09 18:32:24 +0000

    delete bernoulli project, horribly out of data, unpublished (Sam made me do it)

--- trunk/simulations-java/simulations/bernoulli/phetcommon/src/edu/colorado/phet/common/bernoulli/bernoulli/clock2/TickListener.java
+++
@@ -1,8 +0,0 @@
-package edu.colorado.phet.common.bernoulli.bernoulli.clock2;
-
-/**
- * Signifies that a class listenens for tick events.
- */
-public interface TickListener {
-    public void clockTicked(AbstractClock source);
-}
--- trunk/simulations-java/simulations/bernoulli/phetcommon/src/edu/colorado/phet/common/bernoulli/bernoulli/graphics/transform/TransformListener.java
+++
@@ -1,8 +0,0 @@
-package edu.colorado.phet.common.bernoulli.bernoulli.graphics.transform;
-
-/**
- * Listens for changes in the model or view viewport.
- */
-public interface TransformListener {
-    public void transformChanged(ModelViewTransform2d mvt);
-}
