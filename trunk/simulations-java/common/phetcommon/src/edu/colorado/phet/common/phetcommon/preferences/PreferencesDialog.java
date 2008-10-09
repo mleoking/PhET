64746727920522c8aaffc5e3c4bd1818b6e2d993
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-10-09 05:47:42 +0000

    add OK button to Preferences dialog

--- trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/preferences/PreferencesDialog.java
+++ trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/preferences/PreferencesDialog.java
@@ -1,22 +1,44 @@
 package edu.colorado.phet.common.phetcommon.preferences;
 
-import java.awt.*;
+import java.awt.FlowLayout;
+import java.awt.Frame;
+import java.awt.GridBagConstraints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-import javax.swing.*;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
 
 import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
+import edu.colorado.phet.common.phetcommon.resources.PhetCommonResources;
 import edu.colorado.phet.common.phetcommon.resources.PhetResources;
+import edu.colorado.phet.common.phetcommon.updates.ApplicationConfigManualCheckForUpdates;
+import edu.colorado.phet.common.phetcommon.view.util.EasyGridBagLayout;
 import edu.colorado.phet.common.phetcommon.view.util.FrameSetup;
 import edu.colorado.phet.common.phetcommon.view.util.SwingUtils;
-import edu.colorado.phet.common.phetcommon.updates.ApplicationConfigManualCheckForUpdates;
 
 public class PreferencesDialog extends JDialog {
     public PreferencesDialog( Frame owner, ITrackingInfo tracker, IManualUpdateChecker iCheckForUpdates, IPreferences updatePreferences, IPreferences trackingPreferences ) {
         super( owner, "Preferences", true );
         setResizable( false );
-        setContentPane( new PreferencesPanel( iCheckForUpdates, tracker, updatePreferences, trackingPreferences ) );
+        JPanel userInputPanel = new PreferencesPanel( iCheckForUpdates, tracker, updatePreferences, trackingPreferences );
+        JButton okButton = new JButton( PhetCommonResources.getString( "Common.choice.ok" ) );
+        okButton.addActionListener( new ActionListener() {
+            public void actionPerformed( ActionEvent e ) {
+                dispose();
+            }
+        } );
+        JPanel buttonPanel = new JPanel( new FlowLayout( FlowLayout.CENTER ) );
+        buttonPanel.add( okButton );
+        JPanel panel = new JPanel();
+        EasyGridBagLayout layout = new EasyGridBagLayout( panel );
+        panel.setLayout( layout );
+        layout.addComponent( userInputPanel, 0, 0 );
+        layout.addAnchoredComponent( buttonPanel, 2, 0, GridBagConstraints.CENTER );
+        setContentPane( panel );
         pack();
         if ( owner != null ) {
             SwingUtils.centerDialogInParent( this );
