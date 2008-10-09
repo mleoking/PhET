0e305f60b13a1561206ce663c5c8f9bf1c143752
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-10-09 00:26:55 +0000

    >Removed unnecessary help items from reversible reactions sim

--- simulations-java/simulations/ideal-gas/src/edu/colorado/phet/idealgas/controller/IdealGasModule.java
+++ simulations-java/simulations/ideal-gas/src/edu/colorado/phet/idealgas/controller/IdealGasModule.java
@@ -10,6 +10,26 @@
  */
 package edu.colorado.phet.idealgas.controller;
 
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.awt.geom.Point2D;
+import java.awt.image.BufferedImage;
+import java.awt.image.BufferedImageOp;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.EventListener;
+import java.util.EventObject;
+import java.util.Random;
+
+import javax.swing.*;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.Border;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
+
 import edu.colorado.phet.common.phetcommon.application.PhetApplication;
 import edu.colorado.phet.common.phetcommon.util.EventChannel;
 import edu.colorado.phet.common.phetcommon.view.ControlPanel;
@@ -27,25 +47,6 @@ import edu.colorado.phet.idealgas.instrumentation.Thermometer;
 import edu.colorado.phet.idealgas.model.*;
 import edu.colorado.phet.idealgas.view.*;
 import edu.colorado.phet.idealgas.view.monitors.*;
-
-import javax.swing.*;
-import javax.swing.border.BevelBorder;
-import javax.swing.border.Border;
-import javax.swing.border.EtchedBorder;
-import javax.swing.border.TitledBorder;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
-import java.awt.geom.Point2D;
-import java.awt.image.BufferedImage;
-import java.awt.image.BufferedImageOp;
-import java.text.DecimalFormat;
-import java.util.ArrayList;
-import java.util.EventListener;
-import java.util.EventObject;
-import java.util.Random;
 
 /**
  *
@@ -196,7 +197,7 @@ public class IdealGasModule extends PhetGraphicsModule {
      * @return the model
      */
     protected IdealGasModel getIdealGasModel() {
-        return (IdealGasModel)getModel();
+        return (IdealGasModel) getModel();
     }
 
     /**
@@ -222,8 +223,8 @@ public class IdealGasModule extends PhetGraphicsModule {
         box.setGaugeSlice( gaugeSlice );
         idealGasModel.addModelElement( gaugeSlice );
         pressureGauge = new PressureDialGauge( box, getApparatusPanel(),
-                                               new Point( (int)box.getMaxX(),
-                                                          (int)gaugeSlice.getY() ) );
+                                               new Point( (int) box.getMaxX(),
+                                                          (int) gaugeSlice.getY() ) );
         addGraphic( pressureGauge, IdealGasConfig.MOLECULE_LAYER );
 
         // Add the thermometer
@@ -271,8 +272,8 @@ public class IdealGasModule extends PhetGraphicsModule {
         // Add buttons for selecting the species that the pump will produce
         pumpSelectorPanel = new PumpSpeciesSelectorPanel( this );
         pumpSelectorPanel.setLocation( IdealGasConfig.X_BASE_OFFSET + 630, IdealGasConfig.Y_BASE_OFFSET + 300 );
-        pumpSelectorPanel.setLocation( (int)( pumpGraphic.getLocation().getX() + pumpGraphic.getWidth() - pumpSelectorPanel.getWidth() + 10 ),
-                                       (int)( pumpGraphic.getLocation().getY() + pumpGraphic.getHeight() + 32 ) );
+        pumpSelectorPanel.setLocation( (int) ( pumpGraphic.getLocation().getX() + pumpGraphic.getWidth() - pumpSelectorPanel.getWidth() + 10 ),
+                                       (int) ( pumpGraphic.getLocation().getY() + pumpGraphic.getHeight() + 32 ) );
         getApparatusPanel().addGraphic( pumpSelectorPanel );
         getApparatusPanel().revalidate();
     }
@@ -315,18 +316,13 @@ public class IdealGasModule extends PhetGraphicsModule {
         return new Pump.ConstantEnergyStrategy( idealGasModel );
     }
 
-    private void addHelp() {
-        HelpItem helpItem1 = new HelpItem( getApparatusPanel(),
-                                           IdealGasResources.getString( "help.wall" ), 
-                                           box.getPosition().getX(), box.getPosition().getY(),
-                                           HelpItem.BELOW, HelpItem.LEFT );
-        helpItem1.setForegroundColor( IdealGasConfig.HELP_COLOR );
-        addHelpItem( helpItem1 );
-        HelpItem helpItem2 = new HelpItem( getApparatusPanel(),
-                                           IdealGasResources.getString( "help.door" ),
-                                           box.getPosition().getX() + 100, box.getPosition().getY() - 50 );
-        helpItem2.setForegroundColor( IdealGasConfig.HELP_COLOR );
-        addHelpItem( helpItem2 );
+    protected void addHelp() {
+        addWallHelp();
+        addDoorHelp();
+        addStoveHelp();
+    }
+
+    protected void addStoveHelp() {
         HelpItem helpItem3 = new HelpItem( getApparatusPanel(),
                                            IdealGasResources.getString( "help.stove" ),
                                            box.getPosition().getX() + 50, box.getMaxY() + 50 );
@@ -334,11 +330,28 @@ public class IdealGasModule extends PhetGraphicsModule {
         addHelpItem( helpItem3 );
     }
 
+    protected void addDoorHelp() {
+        HelpItem helpItem2 = new HelpItem( getApparatusPanel(),
+                                           IdealGasResources.getString( "help.door" ),
+                                           box.getPosition().getX() + 100, box.getPosition().getY() - 50 );
+        helpItem2.setForegroundColor( IdealGasConfig.HELP_COLOR );
+        addHelpItem( helpItem2 );
+    }
+
+    protected void addWallHelp() {
+        HelpItem helpItem1 = new HelpItem( getApparatusPanel(),
+                                           IdealGasResources.getString( "help.wall" ),
+                                           box.getPosition().getX(), box.getPosition().getY(),
+                                           HelpItem.BELOW, HelpItem.LEFT );
+        helpItem1.setForegroundColor( IdealGasConfig.HELP_COLOR );
+        addHelpItem( helpItem1 );
+    }
+
     public void activate( PhetApplication app ) {
         super.activate();
 //        super.activate( app );
-        for( int i = 0; i < visibleInstruments.size(); i++ ) {
-            Component component = (Component)visibleInstruments.get( i );
+        for ( int i = 0; i < visibleInstruments.size(); i++ ) {
+            Component component = (Component) visibleInstruments.get( i );
             component.setVisible( true );
         }
 
@@ -350,8 +363,8 @@ public class IdealGasModule extends PhetGraphicsModule {
     public void deactivate( PhetApplication app ) {
         super.deactivate();
 //        super.deactivate( app );
-        for( int i = 0; i < visibleInstruments.size(); i++ ) {
-            Component component = (Component)visibleInstruments.get( i );
+        for ( int i = 0; i < visibleInstruments.size(); i++ ) {
+            Component component = (Component) visibleInstruments.get( i );
             component.setVisible( false );
         }
     }
@@ -376,15 +389,15 @@ public class IdealGasModule extends PhetGraphicsModule {
     //-----------------------------------------------------------------
 
     public JDialog setMeasurementDlgVisible( boolean isVisible ) {
-        if( measurementDlg == null ) {
+        if ( measurementDlg == null ) {
             measurementDlg = new MeasurementDialog( PhetApplication.instance().getPhetFrame(), this );
             JFrame frame = PhetApplication.instance().getPhetFrame();
             measurementDlg.setLocationRelativeTo( frame );
-            measurementDlg.setLocation( (int)( frame.getLocation().getX() + frame.getWidth() * 3 / 5 ),
-                                        (int)frame.getLocation().getY() + 20 );
+            measurementDlg.setLocation( (int) ( frame.getLocation().getX() + frame.getWidth() * 3 / 5 ),
+                                        (int) frame.getLocation().getY() + 20 );
         }
         measurementDlg.setVisible( isVisible );
-        if( isVisible ) {
+        if ( isVisible ) {
             visibleInstruments.add( measurementDlg );
         }
         else {
@@ -394,12 +407,12 @@ public class IdealGasModule extends PhetGraphicsModule {
     }
 
     public JDialog setSpeciesMonitorDlgEnabled( boolean isEnabled ) {
-        if( speciesMonitorDlg == null ) {
+        if ( speciesMonitorDlg == null ) {
             speciesMonitorDlg = new SpeciesMonitorDialog( PhetApplication.instance().getPhetFrame(),
                                                           idealGasModel );
         }
         speciesMonitorDlg.setVisible( isEnabled );
-        if( isEnabled ) {
+        if ( isEnabled ) {
             visibleInstruments.add( speciesMonitorDlg );
         }
         else {
@@ -413,10 +426,10 @@ public class IdealGasModule extends PhetGraphicsModule {
     }
 
     public void setCmLinesOn( boolean cmLinesOn ) {
-        if( cmLines == null ) {
+        if ( cmLines == null ) {
             cmLines = new CmLines( getApparatusPanel(), idealGasModel );
         }
-        if( cmLinesOn ) {
+        if ( cmLinesOn ) {
             addGraphic( cmLines, IdealGasConfig.MOLECULE_LAYER + 1 );
         }
         else {
@@ -431,8 +444,8 @@ public class IdealGasModule extends PhetGraphicsModule {
      * @param value
      */
     public void setStove( int value ) {
-        idealGasModel.setHeatSource( (double)value );
-        ( (BaseIdealGasApparatusPanel)getApparatusPanel() ).setStove( value );
+        idealGasModel.setHeatSource( (double) value );
+        ( (BaseIdealGasApparatusPanel) getApparatusPanel() ).setStove( value );
     }
 
     /**
@@ -443,7 +456,7 @@ public class IdealGasModule extends PhetGraphicsModule {
      */
     public void setGravity( double value ) {
         gravity.setAmt( value );
-        if( value != 0 ) {
+        if ( value != 0 ) {
             pump.setPumpingEnergyStrategy( new Pump.FixedEnergyStrategy() );
             box.setMultipleSlicesEnabled( false );
         }
@@ -468,27 +481,27 @@ public class IdealGasModule extends PhetGraphicsModule {
         // just to make sure there is no non-random effect on the temperature
         // of the system
         Object obj = null;
-        while( obj == null ) {
+        while ( obj == null ) {
             boolean randomB = random.nextBoolean();
-            if( randomB ) {
-                for( int i = 0; i < bodies.size(); i++ ) {
+            if ( randomB ) {
+                for ( int i = 0; i < bodies.size(); i++ ) {
                     obj = bodies.get( i );
-                    if( species.isInstance( obj ) ) {
+                    if ( species.isInstance( obj ) ) {
                         break;
                     }
                 }
             }
             else {
-                for( int i = bodies.size() - 1; i >= 0; i-- ) {
+                for ( int i = bodies.size() - 1; i >= 0; i-- ) {
                     obj = bodies.get( i );
-                    if( species.isInstance( obj ) ) {
+                    if ( species.isInstance( obj ) ) {
                         break;
                     }
                 }
             }
         }
-        if( obj instanceof GasMolecule ) {
-            GasMolecule molecule = (GasMolecule)obj;
+        if ( obj instanceof GasMolecule ) {
+            GasMolecule molecule = (GasMolecule) obj;
             idealGasModel.removeModelElement( molecule );
         }
     }
@@ -540,8 +553,8 @@ public class IdealGasModule extends PhetGraphicsModule {
      */
     public void setPressureSliceEnabled( boolean pressureSliceEnabled ) {
 
-        if( pressureSlice == null ) {
-            pressureSlice = new PressureSlice( getBox(), (IdealGasModel)getModel(), clock );
+        if ( pressureSlice == null ) {
+            pressureSlice = new PressureSlice( getBox(), (IdealGasModel) getModel(), clock );
             pressureSlice.setUpdateContinuously( false );
             pressureSliceGraphic = new PressureSliceGraphic( getApparatusPanel(),
                                                              pressureSlice,
@@ -597,7 +610,7 @@ public class IdealGasModule extends PhetGraphicsModule {
                                                     200, 100 );
             getApparatusPanel().add( pressureSlideTimeAveCtrlPane );
         }
-        if( pressureSliceEnabled ) {
+        if ( pressureSliceEnabled ) {
             getModel().addModelElement( pressureSlice );
             addGraphic( pressureSliceGraphic, 20 );
             pressureSlideTimeAveCtrlPane.setVisible( true );
@@ -606,7 +619,7 @@ public class IdealGasModule extends PhetGraphicsModule {
         else {
             getApparatusPanel().removeGraphic( pressureSliceGraphic );
             getModel().removeModelElement( pressureSlice );
-            if( pressureSlideTimeAveCtrlPane != null ) {
+            if ( pressureSlideTimeAveCtrlPane != null ) {
                 pressureSlideTimeAveCtrlPane.setVisible( false );
             }
         }
@@ -616,10 +629,10 @@ public class IdealGasModule extends PhetGraphicsModule {
      * @param rulerEnabled
      */
     public void setRulerEnabed( boolean rulerEnabled ) {
-        if( rulerGraphic == null ) {
+        if ( rulerGraphic == null ) {
             rulerGraphic = new RulerGraphic( getApparatusPanel() );
         }
-        if( rulerEnabled ) {
+        if ( rulerEnabled ) {
             getApparatusPanel().addGraphic( rulerGraphic, Integer.MAX_VALUE );
         }
         else {
@@ -627,8 +640,8 @@ public class IdealGasModule extends PhetGraphicsModule {
         }
         getApparatusPanel().revalidate();
         getApparatusPanel().paintImmediately( 0, 0,
-                                              (int)getApparatusPanel().getBounds().getWidth(),
-                                              (int)getApparatusPanel().getBounds().getHeight() );
+                                              (int) getApparatusPanel().getBounds().getWidth(),
+                                              (int) getApparatusPanel().getBounds().getHeight() );
     }
 
     /**
@@ -638,10 +651,10 @@ public class IdealGasModule extends PhetGraphicsModule {
      * @return the dialog
      */
     public JDialog setHistogramDlgEnabled( boolean histogramDlgEnabled ) {
-        if( histogramDlgEnabled ) {
+        if ( histogramDlgEnabled ) {
             visibleInstruments.add( histogramDlg );
             histogramDlg = new EnergyHistogramDialog( PhetApplication.instance().getPhetFrame(),
-                                                      (IdealGasModel)getModel() );
+                                                      (IdealGasModel) getModel() );
             histogramDlg.setVisible( true );
         }
         else {
@@ -653,7 +666,7 @@ public class IdealGasModule extends PhetGraphicsModule {
     }
 
     public void setStopwatchEnabled( boolean stopwatchEnabled ) {
-        if( stopwatchEnabled ) {
+        if ( stopwatchEnabled ) {
             stopwatchPanel = new StopwatchPanel( getModel(), IdealGasResources.getString( "stopwatch.units" ), IdealGasConfig.TIME_SCALE_FACTOR );
             getClockControlPanel().add( stopwatchPanel, BorderLayout.WEST );
             getClockControlPanel().revalidate();
@@ -670,7 +683,7 @@ public class IdealGasModule extends PhetGraphicsModule {
      * Enable/disable the pressure gauge
      */
     public void setPressureGaugeVisible( boolean isVisible ) {
-        if( isVisible ) {
+        if ( isVisible ) {
             getApparatusPanel().addGraphic( pressureGauge );
         }
         else {
@@ -682,7 +695,7 @@ public class IdealGasModule extends PhetGraphicsModule {
     // Event handling
     //-----------------------------------------------------
     EventChannel resetEventChannel = new EventChannel( ResetListener.class );
-    ResetListener resetListenersProxy = (ResetListener)resetEventChannel.getListenerProxy();
+    ResetListener resetListenersProxy = (ResetListener) resetEventChannel.getListenerProxy();
 
     public void reset() {
         getIdealGasModel().removeAllMolecules();
@@ -691,7 +704,7 @@ public class IdealGasModule extends PhetGraphicsModule {
         box.setOpening( new Point2D[]{new Point2D.Double(), new Point2D.Double()} );
         box.clearData();
 
-        if( stopwatchPanel != null ) {
+        if ( stopwatchPanel != null ) {
             stopwatchPanel.reset();
         }
     }
@@ -709,10 +722,10 @@ public class IdealGasModule extends PhetGraphicsModule {
     }
 
     public void setCurrentPumpImage( Color color ) {
-        if( color.equals( Color.blue ) ) {
+        if ( color.equals( Color.blue ) ) {
             pumpGraphic.setImage( bluePumpImg );
         }
-        if( color.equals( Color.red ) ) {
+        if ( color.equals( Color.red ) ) {
             pumpGraphic.setImage( redPumpImg );
         }
     }
