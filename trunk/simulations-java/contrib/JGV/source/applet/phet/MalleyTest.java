d2b4e5be2b4114399258d3a5a3ac89f13776a5b3
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-10-09 20:26:53 +0000

    update

--- trunk/simulations-java/contrib/JGV/source/applet/phet/MalleyTest.java
+++ trunk/simulations-java/contrib/JGV/source/applet/phet/MalleyTest.java
@@ -2,10 +2,14 @@ package phet;
 
 import geom.jgv.gui.JGVPanel;
 
+import java.io.*;
+
 import javax.swing.JFrame;
 
 
 public class MalleyTest extends JFrame {
+    
+    private static final String GEOMETRY_FILENAME = "/tmp/cube.xml";
     
     public MalleyTest() {
         super( "Malley Test" );
@@ -13,11 +17,36 @@ public class MalleyTest extends JFrame {
         setSize( 640, 480 );
         
         JGVPanel jgvPanel = new JGVPanel( this );
-        jgvPanel.addXMLFile( "/tmp/cube.xml" );
         add( jgvPanel );
+       
+        jgvPanel.addXMLFile( GEOMETRY_FILENAME );
+//        jgvPanel.addGeom( readFile( GEOMETRY_FILENAME ) );
     }
 
     public static void main( String[] args ) {
         new MalleyTest().setVisible( true );
+    }
+    
+    private static String readFile( String filename ) {
+        String text = new String();
+        try {
+            InputStream inputStream = new FileInputStream( filename );
+            BufferedReader bufferedReader = new BufferedReader( new InputStreamReader( inputStream ) );
+            String line = bufferedReader.readLine();
+            while ( line != null ) {
+                text += line;
+                line = bufferedReader.readLine();
+                if ( line != null ) {
+                    text += System.getProperty( "line.separator" );
+                }
+            }
+        }
+        catch( FileNotFoundException e ) {
+            e.printStackTrace();
+        }
+        catch( IOException e ) {
+            e.printStackTrace();
+        }
+        return text;
     }
 }
