ff0d0b31f4fdd282dfa23ae97db10952daccee03
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-10-09 19:21:29 +0000

    fixed version getter to not delegate to phetapplicationconfig

--- simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/application/PhetApplicationConfig.java
+++ simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/application/PhetApplicationConfig.java
@@ -222,7 +222,7 @@ public class PhetApplicationConfig implements Trackable, ITrackingInfo {
      * @deprecated Simulations should use PhetApplicationConfig properly.
      */
     public static PhetVersion getVersion( String simName ) {
-        return new PhetApplicationConfig( new String[0], new FrameSetup.NoOp(), new PhetResources( simName ) ).getVersion();
+        return new PhetResources( simName ).getVersion();
     }
 
     /**
