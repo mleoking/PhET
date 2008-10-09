b31aced6e0f149bd232cdbd2a618e497cd4bfaa2
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-10-09 19:08:32 +0000

    moved forward to non-deprecated PhetApplicationConfig

--- trunk/simulations-java/simulations/states-of-matter/src/edu/colorado/phet/statesofmatter/InteractionPotentialApplication.java
+++ trunk/simulations-java/simulations/states-of-matter/src/edu/colorado/phet/statesofmatter/InteractionPotentialApplication.java
@@ -9,6 +9,7 @@ import javax.swing.SwingUtilities;
 
 import edu.colorado.phet.common.phetcommon.application.Module;
 import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
+import edu.colorado.phet.common.phetcommon.application.PhetApplication;
 import edu.colorado.phet.common.phetcommon.util.IProguardKeepClass;
 import edu.colorado.phet.common.phetcommon.view.ITabbedModulePane;
 import edu.colorado.phet.common.phetcommon.view.PhetFrame;
@@ -122,33 +123,24 @@ public class InteractionPotentialApplication extends PiccoloPhetApplication impl
     // main
     //----------------------------------------------------------------------------
 
-    public static void main(final String[] args) {
-        /*
-         * Wrap the body of main in invokeLater, so that all initialization occurs
-         * in the event dispatch thread. Sun now recommends doing all Swing init in
-         * the event dispatch thread. And the Piccolo-based tabs in TabbedModulePanePiccolo
-         * seem to cause startup deadlock problems if they aren't initialized in the
-         * event dispatch thread. Since we don't have an easy way to separate Swing and
-         * non-Swing init, we're stuck doing everything in invokeLater.
-         */
-        SwingUtilities.invokeLater( new Runnable() {
+    public static void main(final String[] args ) {
+        PhetApplicationConfig config = new PhetApplicationConfig( args, new PhetApplicationConfig.ApplicationConstructor() {
+                    public PhetApplication getApplication( PhetApplicationConfig config ) {
 
-            public void run() {
+                        // Creakte the application.
+                        InteractionPotentialApplication app = new InteractionPotentialApplication( config );
 
-                PhetApplicationConfig config = 
-                    new PhetApplicationConfig( args, StatesOfMatterConstants.FRAME_SETUP, 
-                            StatesOfMatterResources.getResourceLoader(), "interaction-potential" );
-                
-                PhetLookAndFeel p = new PhetLookAndFeel();
-                p.setBackgroundColor( StatesOfMatterConstants.CONTROL_PANEL_COLOR );
-                p.initLookAndFeel();
+                        // Start the application.
+                        app.startApplication();
+                        return app;
+                    }
+                }, "states-of-matter", "interaction-potential" );
+        config.setFrameSetup( StatesOfMatterConstants.FRAME_SETUP );
 
-                // Create the application.
-                InteractionPotentialApplication app = new InteractionPotentialApplication( config );
+        PhetLookAndFeel p = new PhetLookAndFeel();
+        p.setBackgroundColor( StatesOfMatterConstants.CONTROL_PANEL_COLOR );
+        config.setLookAndFeel( p );
+        config.launchSim();
 
-                // Start the application.
-                app.startApplication();
-            }
-        } );
     }
 }
