419fc2b455c09a3be3cf861746cb5c968bfc23eb
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-10-10 03:54:55 +0000

    added stub for cube creation and comments

--- trunk/simulations-java/contrib/JGV/source/applet/phet/ReidTest.java
+++ trunk/simulations-java/contrib/JGV/source/applet/phet/ReidTest.java
@@ -5,6 +5,7 @@ import geom.jgv.gui.CameraCanvas;
 import geom.jgv.model.Geom;
 import geom.jgv.view.BSPTree;
 
+import java.awt.*;
 import java.io.ByteArrayInputStream;
 
 import javax.swing.*;
@@ -12,7 +13,11 @@ import javax.swing.*;
 import org.sjg.xml.Document;
 import org.sjg.xml.Parser;
 
+//main concerns:
+//spheres?
+//data structures for geometry?
 
+//textures?
 public class ReidTest extends JFrame {
     private CameraCanvas cameraCanvas;
 
@@ -47,58 +52,88 @@ public class ReidTest extends JFrame {
 
     public static void main( String[] args ) throws Exception {
         new ReidTest().setVisible( true );
+        System.out.println( "cubeXML = " + cubeXML );
+
+        MyGeomOff g = new MyGeomOff();
+        g.addVertex( -0.5, -0.5, -0.5 );
+        g.addVertex( 0.5, -0.5, -0.5 );
+        g.addVertex( 0.5, 0.5, -0.5 );
+        g.addVertex( -0.5, 0.5, -0.5 );
+        g.addVertex( -0.5, -0.5, 0.5 );
+        g.addVertex( 0.5, -0.5, 0.5 );
+        g.addVertex( 0.5, 0.5, 0.5 );
+        g.addVertex( -0.5, 0.5, 0.5 );
+
+        g.addFace( new double[]{3, 2, 1, 0}, Color.green );
+        g.addFace( new double[]{4, 5, 6, 7}, Color.blue );
+        g.addFace( new double[]{2, 3, 7, 6}, Color.red );
+        g.addFace( new double[]{0, 1, 5, 4}, Color.orange );
+        g.addFace( new double[]{0, 4, 7, 3}, Color.gray );
+        g.addFace( new double[]{1, 2, 6, 5}, Color.yellow );
     }
 
-    String cubeXML = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n" +
-                     "<geom>\n" +
-                     "  <geom-inst node=\"1cube\">\n" +
-                     "    <pickable>\n" +
-                     "      <action type=\"URL\">http://www.msnbc.com/</action>\n" +
-                     "    </pickable>\n" +
-                     "    <transform>\n" +
-                     "       .3   0   0  0\n" +
-                     "        0  .3   0  0\n" +
-                     "        0   0  .3  0\n" +
-                     "        0   0  -1  1\n" +
-                     "    </transform>\n" +
-                     "  <geom-off vertices=\"8\" faces=\"6\" edges=\"12\">\n" +
-                     "    <vertices>\n" +
-                     "      <vertex index=\"0\" x=\"-0.5\" y=\"-0.5\" z=\"-0.5\"/>\n" +
-                     "      <vertex index=\"1\" x=\"0.5\" y=\"-0.5\" z=\"-0.5\"/>\n" +
-                     "      <vertex index=\"2\" x=\"0.5\" y=\"0.5\" z=\"-0.5\"/>\n" +
-                     "      <vertex index=\"3\" x=\"-0.5\" y=\"0.5\" z=\"-0.5\"/>\n" +
-                     "      <vertex index=\"4\" x=\"-0.5\" y=\"-0.5\" z=\"0.5\"/>\n" +
-                     "      <vertex index=\"5\" x=\"0.5\" y=\"-0.5\" z=\"0.5\"/>\n" +
-                     "      <vertex index=\"6\" x=\"0.5\" y=\"0.5\" z=\"0.5\"/>\n" +
-                     "      <vertex index=\"7\" x=\"-0.5\" y=\"0.5\" z=\"0.5\"/>\n" +
-                     "    </vertices>\n" +
-                     "    <faces>\n" +
-                     "      <face>\n" +
-                     "        <vertices>3 2 1 0</vertices>\n" +
-                     "\t<color r=\"0.800\" g=\"0.098\" b=\"0.098\"/>\n" +
-                     "      </face>\n" +
-                     "      <face>\n" +
-                     "        <vertices>4 5 6 7</vertices>\n" +
-                     "\t<color r=\"0.098\" g=\"0.647\" b=\"0.400\"/>\n" +
-                     "      </face>\n" +
-                     "      <face>\n" +
-                     "        <vertices>2 3 7 6</vertices>\n" +
-                     "\t<color r=\"0.098\" g=\"0.098\" b=\"0.800\"/>\n" +
-                     "      </face>\n" +
-                     "      <face>\n" +
-                     "        <vertices>0 1 5 4</vertices>\n" +
-                     "\t<color r=\"0.898\" g=\"0.600\" b=\"0.000\"/>\n" +
-                     "      </face>\n" +
-                     "      <face>\n" +
-                     "        <vertices>0 4 7 3</vertices>\n" +
-                     "\t<color r=\"0.000\" g=\"0.600\" b=\"0.800\"/>\n" +
-                     "      </face>\n" +
-                     "      <face>\n" +
-                     "        <vertices>1 2 6 5</vertices>\n" +
-                     "\t<color r=\"0.498\" g=\"0.000\" b=\"0.898\"/>\n" +
-                     "      </face>\n" +
-                     "    </faces>\n" +
-                     "  </geom-off>\n" +
-                     "  </geom-inst>\n" +
-                     "</geom>";
+    static String cubeXML = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n" +
+                            "<geom>\n" +
+                            "  <geom-inst node=\"1cube\">\n" +
+                            "    <pickable>\n" +
+                            "      <action type=\"URL\">http://www.msnbc.com/</action>\n" +
+                            "    </pickable>\n" +
+                            "    <transform>\n" +
+                            "       1   0   0  0\n" +
+                            "        0  1   0  0\n" +
+                            "        0   0  1  0\n" +
+                            "        0   0  0  1\n" +
+                            "    </transform>\n" +
+                            "  <geom-off vertices=\"8\" faces=\"6\" edges=\"12\">\n" +
+                            "    <vertices>\n" +
+                            "      <vertex index=\"0\" x=\"-0.5\" y=\"-0.5\" z=\"-0.5\"/>\n" +
+                            "      <vertex index=\"1\" x=\"0.5\" y=\"-0.5\" z=\"-0.5\"/>\n" +
+                            "      <vertex index=\"2\" x=\"0.5\" y=\"0.5\" z=\"-0.5\"/>\n" +
+                            "      <vertex index=\"3\" x=\"-0.5\" y=\"0.5\" z=\"-0.5\"/>\n" +
+                            "      <vertex index=\"4\" x=\"-0.5\" y=\"-0.5\" z=\"0.5\"/>\n" +
+                            "      <vertex index=\"5\" x=\"0.5\" y=\"-0.5\" z=\"0.5\"/>\n" +
+                            "      <vertex index=\"6\" x=\"0.5\" y=\"0.5\" z=\"0.5\"/>\n" +
+                            "      <vertex index=\"7\" x=\"-0.5\" y=\"0.5\" z=\"0.5\"/>\n" +
+                            "    </vertices>\n" +
+                            "    <faces>\n" +
+                            "      <face>\n" +
+                            "        <vertices>3 2 1 0</vertices>\n" +
+                            "\t<color r=\"0.800\" g=\"0.098\" b=\"0.098\"/>\n" +
+                            "      </face>\n" +
+                            "      <face>\n" +
+                            "        <vertices>4 5 6 7</vertices>\n" +
+                            "\t<color r=\"0.098\" g=\"0.647\" b=\"0.400\"/>\n" +
+                            "      </face>\n" +
+                            "      <face>\n" +
+                            "        <vertices>2 3 7 6</vertices>\n" +
+                            "\t<color r=\"0.098\" g=\"0.098\" b=\"0.800\"/>\n" +
+                            "      </face>\n" +
+                            "      <face>\n" +
+                            "        <vertices>0 1 5 4</vertices>\n" +
+                            "\t<color r=\"0.898\" g=\"0.600\" b=\"0.000\"/>\n" +
+                            "      </face>\n" +
+                            "      <face>\n" +
+                            "        <vertices>0 4 7 3</vertices>\n" +
+                            "\t<color r=\"0.000\" g=\"0.600\" b=\"0.800\"/>\n" +
+                            "      </face>\n" +
+                            "      <face>\n" +
+                            "        <vertices>1 2 6 5</vertices>\n" +
+                            "\t<color r=\"0.498\" g=\"0.000\" b=\"0.898\"/>\n" +
+                            "      </face>\n" +
+                            "    </faces>\n" +
+                            "  </geom-off>\n" +
+                            "  </geom-inst>\n" +
+                            "</geom>";
+
+    private static class MyGeomOff {
+        public void addVertex( double x, double y, double z ) {
+        }
+
+        public void addFace( double[] doubles, Color green ) {
+        }
+
+        public String toXML() {
+            return null;
+        }
+    }
 }
