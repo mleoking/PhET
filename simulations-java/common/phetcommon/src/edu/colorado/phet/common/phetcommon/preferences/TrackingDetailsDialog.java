aef4a85b033640496f3c16db26fa0a3f9cab90d7
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-10-10 03:25:03 +0000

    rename TrackingDetailsDialog to match its title, add an OK button to dismiss it

--- simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/preferences/TrackingDetailsDialog.java
+++ simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/preferences/TrackingDetailsDialog.java
@@ -1,6 +1,8 @@
 package edu.colorado.phet.common.phetcommon.preferences;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 
@@ -15,15 +17,18 @@ import edu.colorado.phet.common.phetcommon.view.VerticalLayoutPanel;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.phetcommon.view.util.SwingUtils;
 
-public class FullTrackingInfoDialog extends JDialog {
+/**
+ * Dialog that appears when you press the "Details" button in the Tracking preferences panel.
+ */
+public class TrackingDetailsDialog extends JDialog {
     private ITrackingInfo iTrackingInfo;
 
-    public FullTrackingInfoDialog( Dialog owner, ITrackingInfo iTrackingInfo ) {
-        super( owner );
+    public TrackingDetailsDialog( Dialog owner, ITrackingInfo iTrackingInfo ) {
+        super( owner, "Tracking Details" );
         init( iTrackingInfo );
     }
 
-    public FullTrackingInfoDialog( Frame owner, ITrackingInfo iTrackingInfo ) {
+    public TrackingDetailsDialog( Frame owner, ITrackingInfo iTrackingInfo ) {
         super( owner );
         init( iTrackingInfo );
     }
@@ -39,6 +44,7 @@ public class FullTrackingInfoDialog extends JDialog {
         panel.setBorder( BorderFactory.createEmptyBorder( 8, 2, 8, 2 ) );
         panel.add( createLogoPanel(), constraints );
         panel.add( createReportPanel(), constraints );
+        panel.add( createButtonPanel(), constraints );
         getContentPane().add( panel );
         pack();
         SwingUtils.centerDialogInParent( this );
@@ -78,6 +84,18 @@ public class FullTrackingInfoDialog extends JDialog {
         logoPanel.add( copyrightLabel );
 
         return logoPanel;
+    }
+    
+    private JPanel createButtonPanel() {
+        JPanel panel = new JPanel();
+        JButton okButton = new JButton( getLocalizedString( "Common.choice.ok" ) );
+        okButton.addActionListener( new ActionListener() {
+            public void actionPerformed( ActionEvent e ) {
+                dispose();
+            }
+        });
+        panel.add( okButton );
+        return panel;
     }
 
     private String getLocalizedString( String propertyName ) {
