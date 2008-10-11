53a6e6c19e99b18238d28138051c6f8b4a76e657
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-10-11 22:11:23 +0000

    - create PhetCommonConstants, add PhET URL and email address - create HTMLUtils, add static methods to create styled HTML with active links - cleanup TrackingDetailsDialog

---
+++ simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/PhetCommonConstants.java
@@ -0,0 +1,9 @@
+package edu.colorado.phet.common.phetcommon;
+
+
+public class PhetCommonConstants {
+
+    public static final String PHET_HOME_URL = "http://phet.colorado.edu";
+    public static final String PHET_EMAIL = "phethelp@colorado.edu";
+
+}
