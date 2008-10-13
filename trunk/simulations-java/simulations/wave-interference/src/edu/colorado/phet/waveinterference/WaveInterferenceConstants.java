afb74f7f1ee4ebedc26a83f307bf63363dc7f2e5
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-10-13 23:44:52 +0000

    10-13-2008: [811] use PhetApplicationConfig

---
+++ trunk/simulations-java/simulations/wave-interference/src/edu/colorado/phet/waveinterference/WaveInterferenceConstants.java
@@ -0,0 +1,11 @@
+package edu.colorado.phet.waveinterference;
+
+import edu.colorado.phet.common.phetcommon.view.util.FrameSetup;
+
+
+public class WaveInterferenceConstants {
+    
+    public static final String PROJECT_NAME = "wave-interference";
+    public static final FrameSetup FRAME_SETUP = new FrameSetup.MaxExtent( new FrameSetup.CenteredWithInsets( 50, 50 ) );
+
+}
