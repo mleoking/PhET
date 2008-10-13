0c3ea87a4ef0125a509278ad61df81a9aa9cdcfe
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-10-13 17:52:05 +0000

    10-13-2008: hide clock controls, they don't have any value for this sim 10-13-2008: use framework support for PhetAboutDialog 10-13-2008: remove obsolete entries from strings files 10-13-2008: replace SimStrings with PhetResources 10-13-2008: use PhetApplicationConfig

--- simulations-java/simulations/travoltage/src/edu/colorado/phet/travoltage/TravoltageApplication.java
+++ simulations-java/simulations/travoltage/src/edu/colorado/phet/travoltage/TravoltageApplication.java
@@ -1,14 +1,12 @@
 /*  */
 package edu.colorado.phet.travoltage;
 
-import edu.colorado.phet.common.phetcommon.application.PhetApplication;
-import edu.colorado.phet.common.phetcommon.application.PhetAboutDialog;
-import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
-import edu.colorado.phet.common.phetcommon.view.util.FrameSetup;
-import edu.colorado.phet.common.phetcommon.view.util.SimStrings;
-import edu.colorado.phet.common.phetcommon.view.util.SwingUtils;
+import javax.swing.JFrame;
 
-import javax.swing.*;
+import edu.colorado.phet.common.phetcommon.application.PhetApplication;
+import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
+import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig.ApplicationConstructor;
+import edu.colorado.phet.common.phetcommon.view.util.FrameSetup;
 
 /**
  * User: Sam Reid
@@ -17,38 +15,28 @@ import javax.swing.*;
  */
 
 public class TravoltageApplication extends PhetApplication {
-    //    private static final String VERSION = "1.04";
-    private static final String VERSION = PhetApplicationConfig.getVersion( "travoltage" ).formatForTitleBar();
-    private JDialog dialog;
-
-    public TravoltageApplication( String[] args ) {
-        super( args, SimStrings.getInstance().getString( "TravoltageApplication.title" ), SimStrings.getInstance().getString( "TravoltageApplication.description" ), VERSION, new TravoltageFrameSetup() );
+    
+    public TravoltageApplication( PhetApplicationConfig config ) {
+        super( config );
         addModule( new TravoltageModule() );
-    }
-
-    public void showAboutDialog() {
-        if( dialog == null ) {
-            //dialog = new TravoltageAboutDialog( this );
-            dialog = new PhetAboutDialog( this );
-
-            SwingUtils.centerWindowOnScreen( dialog );
-        }
-        dialog.show();
     }
 
     public static class TravoltageFrameSetup implements FrameSetup {
         public void initialize( JFrame frame ) {
-            new FrameSetup.CenteredWithSize( 800, 700 ).initialize( frame );
+            new FrameSetup.CenteredWithSize( 800, 600 ).initialize( frame );
         }
     }
 
     public static void main( final String[] args ) {
-        SwingUtilities.invokeLater( new Runnable() {
-            public void run() {
-                SimStrings.getInstance().init( args, "travoltage/localization/travoltage-strings" );
-                new TravoltageLookAndFeel().initLookAndFeel();
-                new TravoltageApplication( args ).startApplication();
+        
+        ApplicationConstructor appConstructor = new ApplicationConstructor() {
+            public PhetApplication getApplication( PhetApplicationConfig config ) {
+                return new TravoltageApplication( config );
             }
-        } );
+        };
+        
+        PhetApplicationConfig appConfig = new PhetApplicationConfig( args, appConstructor, TravoltageConstants.PROJECT_NAME );
+        appConfig.setFrameSetup( new TravoltageFrameSetup() );
+        appConfig.launchSim();
     }
 }
