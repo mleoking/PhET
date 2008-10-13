715d8e20ea1a8d09229a8e85f8e46a67a1f06f2a
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-10-13 17:38:27 +0000

    format

--- simulations-java/simulations/phetgraphics-demo/src/edu/colorado/phet/phetgraphicsdemo/PhetGraphicsDemoResources.java
+++ simulations-java/simulations/phetgraphics-demo/src/edu/colorado/phet/phetgraphicsdemo/PhetGraphicsDemoResources.java
@@ -1,3 +1,4 @@
+
 package edu.colorado.phet.phetgraphicsdemo;
 
 import java.awt.image.BufferedImage;
@@ -8,27 +9,27 @@ import edu.colorado.phet.common.phetcommon.resources.PhetResources;
 
 public class PhetGraphicsDemoResources {
 
-private static final PhetResources RESOURCES = new PhetResources( PhetGraphicsDemoConstants.PROJECT_NAME );
-    
+    private static final PhetResources RESOURCES = new PhetResources( PhetGraphicsDemoConstants.PROJECT_NAME );
+
     /* not intended for instantiation */
     private PhetGraphicsDemoResources() {}
-    
+
     public static final PhetResources getResourceLoader() {
         return RESOURCES;
     }
-    
+
     public static final String getString( String name ) {
-        return RESOURCES.getLocalizedString( name  );
+        return RESOURCES.getLocalizedString( name );
     }
-    
+
     public static final BufferedImage getImage( String name ) {
         return RESOURCES.getImage( name );
     }
-    
+
     public static final String getCommonString( String name ) {
         return PhetCommonResources.getInstance().getLocalizedString( name );
     }
-    
+
     public static final BufferedImage getCommonImage( String name ) {
         return PhetCommonResources.getInstance().getImage( name );
     }
