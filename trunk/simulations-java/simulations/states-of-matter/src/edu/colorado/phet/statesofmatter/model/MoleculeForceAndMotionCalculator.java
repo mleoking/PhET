e3df47080600f47698ace022faf7d170b61fd567
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-10-15 23:08:33 +0000

    New interface that will be used to abstract out the force and motion calculations.

---
+++ trunk/simulations-java/simulations/states-of-matter/src/edu/colorado/phet/statesofmatter/model/MoleculeForceAndMotionCalculator.java
@@ -0,0 +1,19 @@
+/* Copyright 2008, University of Colorado */
+
+package edu.colorado.phet.statesofmatter.model;
+
+/**
+ * This interface is used to control an object that simulates the interaction
+ * of a set of molecules.  Through this interface the simulation can be
+ * stepped, reset, etc.
+ * 
+ * @author John Blanco
+ *
+ */
+public interface MoleculeForceAndMotionCalculator {
+
+	/**
+	 * Do the next set of calculations of force and motion.
+	 */
+	public void updateForcesAndMotion();
+}
