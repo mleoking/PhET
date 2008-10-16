dbbf5bfe439a8956bbe9151b940b3908d650235b
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-10-16 02:50:00 +0000

    moved ClockFactory to ZAbstractClockTester, since it is only used in these tests

--- simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/model/clock/ClockFactory.java
+++
@@ -1,7 +0,0 @@
-/* Copyright 2007, University of Colorado */
-
-package edu.colorado.phet.common.phetcommon.model.clock;
-
-public interface ClockFactory {
-    Clock createInstance(int defaultDelay, double v);
-}
