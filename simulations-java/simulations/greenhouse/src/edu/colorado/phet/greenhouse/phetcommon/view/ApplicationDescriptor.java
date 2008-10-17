14bc82df3ecb5573501e21a47027fcc8f6d02985
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-10-17 01:07:16 +0000

    562 - move to trunk FrameSetup

--- simulations-java/simulations/greenhouse/src/edu/colorado/phet/greenhouse/phetcommon/view/ApplicationDescriptor.java
+++ simulations-java/simulations/greenhouse/src/edu/colorado/phet/greenhouse/phetcommon/view/ApplicationDescriptor.java
@@ -1,10 +1,7 @@
 /*, 2003.*/
 package edu.colorado.phet.greenhouse.phetcommon.view;
 
-import edu.colorado.phet.greenhouse.phetcommon.view.util.framesetup.AbsoluteFrameSetup;
-import edu.colorado.phet.greenhouse.phetcommon.view.util.framesetup.FrameCenterer;
-import edu.colorado.phet.greenhouse.phetcommon.view.util.framesetup.FrameSetup;
-import edu.colorado.phet.greenhouse.phetcommon.view.util.framesetup.FullScreen;
+import edu.colorado.phet.common.phetcommon.view.util.FrameSetup;
 
 /**
  * User: Sam Reid
@@ -17,25 +14,6 @@ public class ApplicationDescriptor {
     String description;
     String version;
     FrameSetup frameSetup;
-
-    public ApplicationDescriptor(String windowTitle, String description, String version, int width, int height) {
-        this(windowTitle, description, version, width, height, false);
-    }
-
-    public ApplicationDescriptor(String windowTitle, String description, String version, int x, int y, boolean isInsetSpecified) {
-        this(windowTitle, description, version, getFrameSetup(isInsetSpecified, x, y));
-    }
-
-    private static FrameSetup getFrameSetup(boolean inset, int x, int y) {
-        if (inset)
-            return new FrameCenterer(x, y);
-        else
-            return new AbsoluteFrameSetup(x, y);
-    }
-
-    public ApplicationDescriptor(String windowTitle, String description, String version) {
-        this(windowTitle, description, version, new FullScreen());
-    }
 
     public ApplicationDescriptor(String windowTitle, String description, String version, FrameSetup frameSetup) {
         this.windowTitle = windowTitle;
