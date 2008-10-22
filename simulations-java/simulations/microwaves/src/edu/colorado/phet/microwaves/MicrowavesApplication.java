cfc04c3519e5db708498fcb2bbf6c290d535fd2a
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-10-22 19:16:32 +0000

    startup application with PhetApplicationLauncher

--- simulations-java/simulations/microwaves/src/edu/colorado/phet/microwaves/MicrowavesApplication.java
+++ simulations-java/simulations/microwaves/src/edu/colorado/phet/microwaves/MicrowavesApplication.java
@@ -12,15 +12,17 @@ import javax.swing.*;
 import edu.colorado.phet.common.phetcommon.resources.PhetResources;
 import edu.colorado.phet.common.phetcommon.view.PhetLookAndFeel;
 import edu.colorado.phet.common.phetcommon.view.util.FrameSetup;
+import edu.colorado.phet.common.phetcommon.application.PhetApplicationLauncher;
+import edu.colorado.phet.common.phetcommon.application.PhetApplication;
+import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
 import edu.colorado.phet.common_microwaves.application.Module;
-import edu.colorado.phet.common_microwaves.application.PhetApplication;
 import edu.colorado.phet.common_microwaves.view.ApplicationDescriptor;
 import edu.colorado.phet.coreadditions_microwaves.clock.DynamicClockModel;
 import edu.colorado.phet.coreadditions_microwaves.clock.SwingTimerClock;
 import edu.colorado.phet.coreadditions_microwaves.components.PhetFrame;
 import edu.colorado.phet.microwaves.coreadditions.MessageFormatter;
 
-public class MicrowavesApplication {
+public class MicrowavesApplication extends PhetApplication {
 
 
     //
@@ -28,25 +30,16 @@ public class MicrowavesApplication {
     //
     public static double s_speedOfLight = 10;
 
-    public static PhetApplication s_application;
+    public static edu.colorado.phet.common_microwaves.application.PhetApplication s_application;
 
     // Localization
     public static final String localizedStringsPath = "microwaves/localization/microwaves-strings";
     //todo: convert to proper use of PhetApplicationConfig for getting version
     private static final String VERSION = new PhetResources( "microwaves" ).getVersion().formatForTitleBar();
 
-    public static void main( final String[] args ) {
-        SwingUtilities.invokeLater( new Runnable() {
-            public void run() {
-                runApplication( args );
-            }
-        } );
-    }
-
-    private static void runApplication( String[] args ) {
-        new PhetLookAndFeel().initLookAndFeel();
-//        MicrowavesResources.getStringInstance().init( args, localizedStringsPath );
-
+    public MicrowavesApplication( PhetApplicationConfig config ) {
+        super( config );
+        
         Module oneMoleculesModule = new OneMoleculeModule();
         Module singleLineOfMoleculesModule2 = new SingleLineOfMoleculesModule2();
         Module manyMoleculesModule = new ManyMoleculesModule();
@@ -64,16 +57,21 @@ public class MicrowavesApplication {
                 + ")",
                 MessageFormatter.format( MicrowavesResources.getString( "MicrowavesApplication.description" ) ),
                 VERSION, new FrameSetup.CenteredWithSize( 1024, 768 ) );
-        s_application = new PhetApplication( appDescriptor, modules,
-                                             new SwingTimerClock( new DynamicClockModel( 20, 50 ) ) );
+        s_application = new edu.colorado.phet.common_microwaves.application.PhetApplication( appDescriptor, modules, new SwingTimerClock( new DynamicClockModel( 20, 50 ) ) );
         PhetFrame frame = s_application.getApplicationView().getPhetFrame();
 //        frame.addMenu( new ViewMenu() );
         frame.addMenu( new MicrowaveModule.ControlMenu() );
-        frame.setDefaultLookAndFeelDecorated( true );
 //        frame.setIconImage( lookAndFeel.getSmallIconImage() );
 
 //        s_application.startApplication( manyMoleculesModule );
         s_application.startApplication( singleLineOfMoleculesModule2 );
     }
 
+    public void startApplication() {
+//        super.startApplication();
+    }
+
+    public static void main( String[] args ) {
+        new PhetApplicationLauncher().launchSim( args, "microwaves",MicrowavesApplication.class);
+    }
 }
