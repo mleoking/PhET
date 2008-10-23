937faaf0ccd4af84355845f9ad79724882a49ff9
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-10-23 22:02:26 +0000

    Added simple radio buttons to AlphaDecayNucleusSelectionPanel.

--- simulations-java/simulations/nuclear-physics/src/edu/colorado/phet/nuclearphysics/module/alphadecay/AlphaDecayNucleusSelectionPanel.java
+++ simulations-java/simulations/nuclear-physics/src/edu/colorado/phet/nuclearphysics/module/alphadecay/AlphaDecayNucleusSelectionPanel.java
@@ -8,9 +8,11 @@ import java.awt.GridLayout;
 import java.awt.Image;
 
 import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.border.BevelBorder;
 import javax.swing.border.TitledBorder;
 
@@ -40,6 +42,13 @@ public class AlphaDecayNucleusSelectionPanel extends JPanel {
     private static final double PARTICLE_SCALE_FACTOR = 8;
     
     //------------------------------------------------------------------------
+    // Instance Data
+    //------------------------------------------------------------------------
+    
+    private JRadioButton m_poloniumRadioButton;
+    private JRadioButton m_customNucleusRadioButton;
+
+    //------------------------------------------------------------------------
     // Constructor
     //------------------------------------------------------------------------
     
@@ -57,19 +66,21 @@ public class AlphaDecayNucleusSelectionPanel extends JPanel {
         setBorder( titledBorder );
         
         // Set the layout.
-        setLayout( new GridLayout(0, 2) );
-
-        // Add the images and labels for the simple portion of the legend.
+        setLayout( new GridLayout(2, 1) );
         
-        PNode neutron = new NeutronNode();
-        neutron.scale( PARTICLE_SCALE_FACTOR );
-        addLegendItem( neutron.toImage(), NuclearPhysicsStrings.NEUTRON_LEGEND_LABEL ); 
-        PNode proton = new ProtonNode();
-        proton.scale( PARTICLE_SCALE_FACTOR );
-        addLegendItem( proton.toImage(), NuclearPhysicsStrings.PROTON_LEGEND_LABEL ); 
-        PNode alphaParticle = new AlphaParticleNode();
-        alphaParticle.scale( PARTICLE_SCALE_FACTOR );
-        addLegendItem( alphaParticle.toImage(), NuclearPhysicsStrings.ALPHA_PARTICLE_LEGEND_LABEL );
+        // Create the radio buttons.
+        m_poloniumRadioButton = new JRadioButton("Polonium");
+        m_customNucleusRadioButton = new JRadioButton("Custom");
+        
+        // Group the buttons together and set initial state.
+        ButtonGroup buttonGroup = new ButtonGroup();
+        buttonGroup.add( m_poloniumRadioButton );
+        buttonGroup.add( m_customNucleusRadioButton );
+        m_poloniumRadioButton.setSelected( true );
+        
+        // Add the buttons to the panel.
+        add( m_poloniumRadioButton );
+        add( m_customNucleusRadioButton );
     }
     
     /**
