e7510ea9243346fd1252677cf654ff319173a280
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-10-23 06:44:34 +0000

    reformat

--- simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/updates/dialogs/AutomaticUpdateDialog.java
+++ simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/updates/dialogs/AutomaticUpdateDialog.java
@@ -1,9 +1,6 @@
 package edu.colorado.phet.common.phetcommon.updates.dialogs;
 
-import java.awt.Color;
-import java.awt.Cursor;
-import java.awt.Frame;
-import java.awt.GridBagConstraints;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
@@ -11,8 +8,8 @@ import java.awt.event.MouseEvent;
 
 import javax.swing.*;
 
-import edu.colorado.phet.common.phetcommon.application.PhetApplication;
 import edu.colorado.phet.common.phetcommon.application.ISimInfo;
+import edu.colorado.phet.common.phetcommon.application.PhetApplication;
 import edu.colorado.phet.common.phetcommon.preferences.*;
 import edu.colorado.phet.common.phetcommon.resources.PhetCommonResources;
 import edu.colorado.phet.common.phetcommon.resources.PhetVersion;
@@ -25,44 +22,44 @@ import edu.colorado.phet.common.phetcommon.view.util.HTMLUtils.InteractiveHTMLPa
  * The dialog that used to automatically notify the user that an update is available.
  */
 public class AutomaticUpdateDialog extends AbstractUpdateDialog {
-    
+
     private static final String TITLE = PhetCommonResources.getString( "Common.updates.updateAvailable" );
     private static final String UPDATE_NOW_BUTTON = PhetCommonResources.getString( "Common.updates.updateNow" );
     private static final String ASK_ME_LATER_BUTTON = PhetCommonResources.getString( "Common.updates.askMeLater" );
     private static final String SKIP_UPDATE_BUTTON = PhetCommonResources.getString( "Common.updates.skipThisUpdate" );
     private static final String EDIT_PREFERENCES_LINK = PhetCommonResources.getString( "Common.updates.editPreferences" );
-    
+
     public AutomaticUpdateDialog( PhetApplication application, PhetVersion newVersion ) {
-        this( application.getPhetFrame(), 
+        this( application.getPhetFrame(),
               application.getSimInfo().getProjectName(),
               application.getSimInfo().getFlavor(),
               application.getSimInfo().getName(),
               application.getSimInfo().getVersion(),
               newVersion,
-              application.getTrackingInfo(), 
+              application.getTrackingInfo(),
               new ApplicationConfigManualCheckForUpdates( application.getPhetFrame(), application.getSimInfo() ),
               application.getSimInfo(),
-              new DefaultUpdateTimer(), 
+              new DefaultUpdateTimer(),
               new DefaultVersionSkipper() );
     }
 
-    private AutomaticUpdateDialog( final Frame owner, 
-            final String project, final String sim, final String simName, 
-            final PhetVersion currentVersion, final PhetVersion newVersion,
-            final ITrackingInfo trackingInfo, final IManualUpdateChecker iManuallyCheckForUpdates, 
-            final ISimInfo config, 
-            final IUpdateTimer updateTimer, final IVersionSkipper versionSkipper ) {
+    private AutomaticUpdateDialog( final Frame owner,
+                                   final String project, final String sim, final String simName,
+                                   final PhetVersion currentVersion, final PhetVersion newVersion,
+                                   final ITrackingInfo trackingInfo, final IManualUpdateChecker iManuallyCheckForUpdates,
+                                   final ISimInfo config,
+                                   final IUpdateTimer updateTimer, final IVersionSkipper versionSkipper ) {
         super( owner, TITLE );
         setResizable( false );
         setModal( true );
-        
+
         // information about the update that was found
         String html = getAutomaticUpdateMessageHTML( simName, currentVersion.formatForTitleBar(), newVersion.formatForTitleBar() );
         JComponent htmlPane = new InteractiveHTMLPane( html );
         JPanel messagePanel = new JPanel();
         messagePanel.setBorder( BorderFactory.createEmptyBorder( 5, 5, 5, 5 ) );
         messagePanel.add( htmlPane );
-        
+
         // opens a web browser to the sim's web page
         JButton updateNowButton = new JButton( UPDATE_NOW_BUTTON );
         updateNowButton.addActionListener( new ActionListener() {
@@ -79,6 +76,7 @@ public class AutomaticUpdateDialog extends AbstractUpdateDialog {
             public void actionPerformed( ActionEvent e ) {
                 updateTimer.setLastAskMeLaterTime( project, sim, System.currentTimeMillis() );
                 dispose();
+
             }
         } );
 
@@ -90,7 +88,7 @@ public class AutomaticUpdateDialog extends AbstractUpdateDialog {
                 dispose();
             }
         } );
-        
+
         JPanel buttonPanel = new JPanel();
         buttonPanel.add( updateNowButton );
         buttonPanel.add( askMeLater );
