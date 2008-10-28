de86a1b7b81c20b7f971073da48a674c919661a5
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-10-28 22:51:23 +0000

    removed copy of timestamp (handled in parent)

--- trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/tracking/SessionEndedMessage.java
+++ trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/tracking/SessionEndedMessage.java
@@ -1,8 +1,7 @@
 package edu.colorado.phet.common.phetcommon.tracking;
 
 public class SessionEndedMessage extends TrackingMessage {
-    public SessionEndedMessage( SessionID sessionID, long currentTimeMillis ) {
+    public SessionEndedMessage( SessionID sessionID) {
         super( sessionID, "session-ended" );
-        addEntry( new TrackingEntry( "timestamp", String.valueOf( currentTimeMillis ) ) );
     }
 }
