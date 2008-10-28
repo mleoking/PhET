a5e3b172fa241a20c47031b3f7a35be63606280a
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-10-28 23:52:50 +0000

    864, comment 1:  fix this problem by using '/' as the path separator for JAR resources on all platforms, not System.getProperty("file.separator")

--- trunk/util/translation-utility/src/edu/colorado/phet/translationutility/TUConstants.java
+++ trunk/util/translation-utility/src/edu/colorado/phet/translationutility/TUConstants.java
@@ -9,4 +9,10 @@ public class TUConstants {
     private TUConstants() {}
     
     public static final String ENGLISH_LANGUAGE_CODE = "en";
+    
+    // use this separator for creating paths to files in the file system
+    public static final String FILE_PATH_SEPARATOR = System.getProperty( "file.separator" );
+    
+    // use this separator for creating paths to resources in the JAR file
+    public static final String RESOURCE_PATH_SEPARATOR = "/";
 }
