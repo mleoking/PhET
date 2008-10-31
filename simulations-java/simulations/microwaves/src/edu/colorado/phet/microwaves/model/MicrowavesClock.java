7a09b57e9179c9cec9d12a8f1a2babae0968b38c
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-10-31 19:06:09 +0000

    #562, create clock subclass, since we'll need an instance in each module instead on one clock per application

---
+++ simulations-java/simulations/microwaves/src/edu/colorado/phet/microwaves/model/MicrowavesClock.java
@@ -0,0 +1,11 @@
+package edu.colorado.phet.microwaves.model;
+
+import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
+
+public class MicrowavesClock extends ConstantDtClock {
+    
+    public MicrowavesClock() {
+        super( 50, 20 );
+    }
+
+}
