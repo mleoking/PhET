2862ed6fda594cf2ebcbdf6874eefbd7414c2848
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-10-31 14:32:14 +0000

    minor cleanup, reorganize, reformat

--- trunk/simulations-flash/flash-launcher/src/edu/colorado/phet/flashlauncher/FlashLauncher.java
+++ trunk/simulations-flash/flash-launcher/src/edu/colorado/phet/flashlauncher/FlashLauncher.java
@@ -10,9 +10,7 @@ import java.text.MessageFormat;
 import java.util.Properties;
 import java.util.StringTokenizer;
 
-import javax.swing.JFrame;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
+import javax.swing.*;
 
 import edu.colorado.phet.flashlauncher.util.BareBonesBrowserLaunch;
 import edu.colorado.phet.flashlauncher.util.FileUtils;
@@ -24,21 +22,21 @@ import edu.colorado.phet.flashlauncher.util.FileUtils;
  * which are used to fill in an HTML template.
  * The JAR file is unzipped to a temporary directory.
  * A web browser is launched, and pointed at the HTML.
- * 
+ *
  * @author Sam Reid
  */
 public class FlashLauncher {
-    
+
     private static final String ARGS_FILENAME = "flash-launcher-args.txt";
     private static final String HTML_TEMPLATE = "flash-launcher-template.html";
     private static final String VERSION_FORMAT = "{0}.{1}.{2} ({3})";
-    
+
     private String sim;
     private String language;
     private static JTextArea jTextArea;
 
     public FlashLauncher() throws IOException {
-        
+
         // read sim and language from args file (JAR resource)
         InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream( ARGS_FILENAME );
         BufferedReader bu = new BufferedReader( new InputStreamReader( inputStream ) );
@@ -47,7 +45,7 @@ public class FlashLauncher {
         println( "line = " + line );
         this.sim = stringTokenizer.nextToken();
         this.language = stringTokenizer.nextToken();
-        
+
         // if the developer flag is specified in args file, open a window to show debug output
         if ( stringTokenizer.hasMoreTokens() && stringTokenizer.nextToken().equals( "-dev" ) ) {
             println( "FlashLauncher.FlashLauncher dev" );
@@ -70,37 +68,32 @@ public class FlashLauncher {
         }
     }
 
-    public static void main( String[] args ) throws IOException {
-//        JOptionPane.showMessageDialog( null, System.getProperty( "java.class.path" ) );
-        new FlashLauncher().start();
-    }
-
     /*
      * Launches the simulation in a web browser.
      */
     private void start() throws IOException {
-        
+
         println( "FlashLauncher.start" );
         String unzipDirName = System.getProperty( "java.io.tmpdir" ) + System.getProperty( "file.separator" ) + "phet-" + sim;
         println( "unzipping to directory = " + unzipDirName );
         File unzipDir = new File( unzipDirName );
-        
+
         // unzip the JAR into temp directory
         File jarfile = getJARFile();
         println( "jarfile = " + jarfile );
         println( "Starting unzip jarfile=" + jarfile + ", unzipDir=" + unzipDir );
         FileUtils.unzip( jarfile, unzipDir );
         println( "Finished unzip" );
-        
+
         // read the properties file
         Properties properties = readProperties( sim );
-       
+
         // read the version properties
         String version = readVersion( properties );
-        
+
         // read the background color property
         String bgcolor = readBackgroundColor( properties );
-        
+
         // dynamically generate an HTML file
         String html = generateHTML( sim, language, version, bgcolor );
         File htmlFile = new File( unzipDir, sim + "_" + language + ".html" );
@@ -112,7 +105,7 @@ public class FlashLauncher {
         println( "Starting openurl" );
         BareBonesBrowserLaunch.openURL( "file://" + htmlFile.getAbsolutePath() );
     }
-    
+
     private static Properties readProperties( String sim ) {
         String propertiesFileName = sim + ".properties";
         InputStream inStream = Thread.currentThread().getContextClassLoader().getResourceAsStream( propertiesFileName );
@@ -120,15 +113,13 @@ public class FlashLauncher {
         try {
             properties.load( inStream );
         }
-        catch ( IOException e ) {
+        catch( IOException e ) {
             e.printStackTrace(); //TODO handle this better
         }
         return properties;
     }
-    
+
     private static String readVersion( Properties properties ) {
-        String version = null;
-        
         // read the version properties
         String major = properties.getProperty( "version.major" );
         String minor = properties.getProperty( "version.minor" );
@@ -136,12 +127,11 @@ public class FlashLauncher {
         String revision = properties.getProperty( "version.revision" );
 
         // format the version string
-        Object[] args = { major, minor, dev, revision };
-        version = MessageFormat.format( VERSION_FORMAT, args );
+        Object[] args = {major, minor, dev, revision};
 
-        return version;
+        return MessageFormat.format( VERSION_FORMAT, args );
     }
-    
+
     private static String readBackgroundColor( Properties properties ) {
         String bgcolor = properties.getProperty( "bgcolor" );
         if ( bgcolor == null ) {
@@ -186,5 +176,10 @@ public class FlashLauncher {
             e.printStackTrace();
             throw new RuntimeException( e );
         }
+    }
+
+    public static void main( String[] args ) throws IOException {
+//        JOptionPane.showMessageDialog( null, System.getProperty( "java.class.path" ) );
+        new FlashLauncher().start();
     }
 }
