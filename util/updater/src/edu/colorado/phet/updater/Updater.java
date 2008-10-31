e8690f7f787f271a8b695e8f4232cae68fd8db92
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-10-31 04:26:27 +0000

    added prototype for updater

--- util/updater/src/edu/colorado/phet/updater/Updater.java
+++ util/updater/src/edu/colorado/phet/updater/Updater.java
@@ -1,42 +1,43 @@
 package edu.colorado.phet.updater;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Updater {
 
-	private void update(String project, String sim, String locale, File targetLocation) {
-		
-		// Download the new, updated version of the sim.
-		download(project, targetLocation);
-		
-		// Execute the newly downloaded sim.
-		launchSimulation( sim, locale, targetLocation );
-		
-	}
+    private void update( String project, String sim, String locale, File targetLocation ) throws FileNotFoundException {
 
-	private void launchSimulation(String sim, String locale, File targetLocation) {
+        // Download the new, updated version of the sim.
+        download( project, sim, targetLocation );
 
-		try {
-			Runtime.getRuntime().exec("java -jar " + targetLocation.getAbsolutePath());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	}
+        // Execute the newly downloaded sim.
+        launchSimulation( sim, locale, targetLocation );
 
-	private void download(String project, File targetLocation) {
-		// TODO Auto-generated method stub
-		
-	}
+    }
 
-	public static void main(String[] args) {
-		String project = args[0];
-		String sim = args[1];
-		String locale = args[2];
-		File targetLocation = new File(args[3]);
-		
-		new Updater().update(project, sim, locale, targetLocation);
-	}
+    private void launchSimulation( String sim, String locale, File targetLocation ) {
+        String sep = System.getProperty( "file.separator" );
+        String javaPath = System.getProperty( "java.home" ) + sep + "bin" + sep + "java";
+        try {
+            Process p = Runtime.getRuntime().exec( javaPath + " -jar " + targetLocation.getAbsolutePath() );
+        }
+        catch( IOException e ) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void download( String project, String flavor, File targetLocation ) throws FileNotFoundException {
+        Util.download( "http://phet.colorado.edu/sims/" + project + "/" + flavor + ".jar", targetLocation );
+    }
+
+    public static void main( String[] args ) throws FileNotFoundException {
+        String project = args[0];
+        String sim = args[1];
+        String locale = args[2];
+        File targetLocation = new File( args[3] );
+
+        new Updater().update( project, sim, locale, targetLocation );
+    }
 }
