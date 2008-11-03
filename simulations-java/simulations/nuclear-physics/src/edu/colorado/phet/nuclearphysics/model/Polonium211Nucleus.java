0044d29c372199d70526ef5e696dc9d843ca1c7f
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-11-03 23:56:28 +0000

    Created a model component to represent a nucleus of Polonium 211.  This is a non-composite version.

---
+++ simulations-java/simulations/nuclear-physics/src/edu/colorado/phet/nuclearphysics/model/Polonium211Nucleus.java
@@ -0,0 +1,115 @@
+/* Copyright 2008, University of Colorado */
+
+package edu.colorado.phet.nuclearphysics.model;
+
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.Random;
+
+import edu.colorado.phet.common.phetcommon.model.clock.ClockEvent;
+
+/**
+ * This class represents a non-composite Polonium 211 nucleus.  In other words,
+ * this nucleus does not create or keep track of any constituent nucleons.
+ *
+ * @author John Blanco
+ */
+public class Polonium211Nucleus extends AtomicNucleus {
+    
+    //------------------------------------------------------------------------
+    // Class Data
+    //------------------------------------------------------------------------
+
+    // Number of neutrons and protons in the nucleus upon construction.  The
+    // values below are for Uranium-235.
+    public static final int ORIGINAL_NUM_PROTONS = 84;
+    public static final int ORIGINAL_NUM_NEUTRONS = 127;
+    
+    // Random number generator used for calculating decay time based on half life.
+    private static final Random RAND = new Random();
+    
+    //------------------------------------------------------------------------
+    // Instance Data
+    //------------------------------------------------------------------------
+
+    // Time at which fission will occur.
+    private double _decayTime = 0;
+
+    //------------------------------------------------------------------------
+    // Constructor(s)
+    //------------------------------------------------------------------------
+    
+    public Polonium211Nucleus(NuclearPhysicsClock clock, Point2D position, double fissionInterval){
+
+        super(clock, position, ORIGINAL_NUM_PROTONS, ORIGINAL_NUM_NEUTRONS);
+    }
+    
+    //------------------------------------------------------------------------
+    // Accessor Methods
+    //------------------------------------------------------------------------
+    
+    public double getDecayTime(){
+        return _decayTime;
+    }
+    
+    //------------------------------------------------------------------------
+    // Other Public Methods
+    //------------------------------------------------------------------------
+    
+    /**
+     * Resets the nucleus to its original state, before any fission has
+     * occurred.
+     */
+    public void reset(){
+        
+        // Reset the decay time to 0, indicating that it shouldn't occur
+        // until something changes.
+        _decayTime = 0;
+
+        // Set acceleration, velocity, and position back to 0.
+        setPosition( new Point2D.Double(0, 0) );
+        setVelocity( 0, 0 );
+        setAcceleration( 0, 0 );
+        
+        if ((_numNeutrons != ORIGINAL_NUM_NEUTRONS) || (_numProtons != ORIGINAL_NUM_PROTONS)){
+            // Decay has occurred.
+            _numNeutrons = ORIGINAL_NUM_NEUTRONS;
+            _numProtons = ORIGINAL_NUM_PROTONS;
+            
+            // Notify all listeners of the change to our atomic weight.
+            notifyAtomicWeightChanged(null);
+        }
+
+        // Notify all listeners of the potential position change.
+        notifyPositionChanged();
+    }
+    
+    //------------------------------------------------------------------------
+    // Private and Protected Methods
+    //------------------------------------------------------------------------
+    /**
+     * This method lets this model element know that the clock has ticked.  In
+     * response we check if it is time to decay.
+     */
+    protected void handleClockTicked(ClockEvent clockEvent)
+    {
+        super.handleClockTicked( clockEvent );
+        
+        // See if alpha decay should occur.
+        if ((_decayTime != 0) && (clockEvent.getSimulationTime() >= _decayTime ))
+        {
+            // Cause alpha decay by generating an alpha particle and reducing our atomic weight.
+            ArrayList byProducts = new ArrayList();
+            byProducts.add( new AlphaParticle(_position.getX(), _position.getY()));
+            _numNeutrons -= 2;
+            _numProtons -= 2;
+
+            // Send out the decay event to all listeners.
+            notifyAtomicWeightChanged(byProducts);
+            
+            // Set the decay time to 0 to indicate that decay has occurred and
+            // should not occur again.
+            _decayTime = 0;
+        }
+    }
+}
