0ccd02492551e6a2752558b864a7c5a76cda5713
dano <dano@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-11-04 07:22:16 +0000

    website: Added a temp fix to remove work around the magic_quotes_gpc being turned on

--- website/admin/global.php
+++ website/admin/global.php
@@ -1,5 +1,29 @@
 <?php
 
+// Temp hack to get around magic_quotes_gpc until admins can set it
+//
+if (get_magic_quotes_gpc()) {
+    function undoMagicQuotes($array, $topLevel=true) {
+        $newArray = array();
+        foreach($array as $key => $value) {
+            if (!$topLevel) {
+                $key = stripslashes($key);
+            }
+            if (is_array($value)) {
+                $newArray[$key] = undoMagicQuotes($value, false);
+            }
+            else {
+                $newArray[$key] = stripslashes($value);
+            }
+        }
+        return $newArray;
+    }
+    $_GET = undoMagicQuotes($_GET);
+    $_POST = undoMagicQuotes($_POST);
+    $_COOKIE = undoMagicQuotes($_COOKIE);
+    $_REQUEST = undoMagicQuotes($_REQUEST);
+}
+
     assert(defined("SITE_ROOT"));
 
     @include_once("local-debug-settings.php");
