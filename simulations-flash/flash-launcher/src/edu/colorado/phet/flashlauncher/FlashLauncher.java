da04134202c573ff864fbc92b81ac1a658712780
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-11-05 06:01:42 +0000

    comment out tracking & updates for redeploy under 1.4

--- simulations-flash/flash-launcher/src/edu/colorado/phet/flashlauncher/FlashLauncher.java
+++ simulations-flash/flash-launcher/src/edu/colorado/phet/flashlauncher/FlashLauncher.java
@@ -2,7 +2,6 @@
 
 package edu.colorado.phet.flashlauncher;
 
-import java.awt.*;
 import java.io.*;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -13,10 +12,6 @@ import java.util.StringTokenizer;
 
 import javax.swing.*;
 
-import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
-import edu.colorado.phet.common.phetcommon.application.UpdateApplicationManager;
-import edu.colorado.phet.common.phetcommon.tracking.SessionStartedMessage;
-import edu.colorado.phet.common.phetcommon.tracking.TrackingManager;
 import edu.colorado.phet.flashlauncher.util.BareBonesBrowserLaunch;
 import edu.colorado.phet.flashlauncher.util.FileUtils;
 
@@ -77,7 +72,7 @@ public class FlashLauncher {
      * Launches the simulation in a web browser.
      */
     private void start() throws IOException {
-        handleTrackingAndUpdates();
+//        handleTrackingAndUpdates();
         println( "FlashLauncher.start" );
         String unzipDirName = System.getProperty( "java.io.tmpdir" ) + System.getProperty( "file.separator" ) + "phet-" + sim;
         println( "unzipping to directory = " + unzipDirName );
@@ -111,19 +106,19 @@ public class FlashLauncher {
         BareBonesBrowserLaunch.openURL( "file://" + htmlFile.getAbsolutePath() );
     }
 
-    private void handleTrackingAndUpdates() {
-        System.out.println( "FlashLauncher.handleTrackingAndUpdates" );
-        PhetApplicationConfig config = new PhetApplicationConfig( new String[]{"-tracking", "-updates"}, sim );
-        long applicationLaunchFinishedAt = System.currentTimeMillis();
-        config.setApplicationLaunchFinishedAt( applicationLaunchFinishedAt );
-
-        TrackingManager.initInstance( config );
-        TrackingManager.postMessage( new SessionStartedMessage( config ) );
-        Frame hiddenParent = new JFrame();
-        hiddenParent.setLocation( Toolkit.getDefaultToolkit().getScreenSize().width / 2,
-                                  Toolkit.getDefaultToolkit().getScreenSize().height / 2 );
-        new UpdateApplicationManager( config ).applicationStarted( hiddenParent, config, config );
-    }
+//    private void handleTrackingAndUpdates() {
+//        System.out.println( "FlashLauncher.handleTrackingAndUpdates" );
+//        PhetApplicationConfig config = new PhetApplicationConfig( new String[]{"-tracking", "-updates"}, sim );
+//        long applicationLaunchFinishedAt = System.currentTimeMillis();
+//        config.setApplicationLaunchFinishedAt( applicationLaunchFinishedAt );
+//
+//        TrackingManager.initInstance( config );
+//        TrackingManager.postMessage( new SessionStartedMessage( config ) );
+//        Frame hiddenParent = new JFrame();
+//        hiddenParent.setLocation( Toolkit.getDefaultToolkit().getScreenSize().width / 2,
+//                                  Toolkit.getDefaultToolkit().getScreenSize().height / 2 );
+//        new UpdateApplicationManager( config ).applicationStarted( hiddenParent, config, config );
+//    }
 
     private static Properties readProperties( String sim ) {
         String propertiesFileName = sim + ".properties";
