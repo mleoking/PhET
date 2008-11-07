e3c0181817f664c47545809719b9b5a97f20aea4
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-11-07 05:59:19 +0000

    refactor packages, see #881

--- simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/updates/dialogs/UpdateButton.java
+++ simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/updates/dialogs/UpdateButton.java
@@ -2,18 +2,11 @@ package edu.colorado.phet.common.phetcommon.updates.dialogs;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.*;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.Arrays;
 
 import javax.swing.JButton;
 
 import edu.colorado.phet.common.phetcommon.resources.PhetCommonResources;
-import edu.colorado.phet.common.phetcommon.tracking.TrackingManager;
-import edu.colorado.phet.common.phetcommon.tracking.TrackingMessage;
+import edu.colorado.phet.common.phetcommon.updates.SimUpdater;
 
 /**
  * Updates the simulations by running the PhET "updater", which downloads the new version
