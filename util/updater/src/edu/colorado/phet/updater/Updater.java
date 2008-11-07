8023efb7a8259bd06a8ec539ae65e4f7471e26f3
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-11-07 05:10:31 +0000

    Document the updater class, identify todo's for if/when problems occur.  See #881

--- util/updater/src/edu/colorado/phet/updater/Updater.java
+++ util/updater/src/edu/colorado/phet/updater/Updater.java
@@ -7,6 +7,9 @@ import java.util.Arrays;
 
 public class Updater {
 
+    /*
+     * Downloads and launches the jar for the specified simulation.
+     */
     private void update( String project, String sim, String locale, File targetLocation ) {
         // Download the new, updated version of the sim.
         try {
@@ -23,24 +26,40 @@ public class Updater {
         println( "finished launch" );
     }
 
+    /*
+     * Launches the simulation (as specified by a JAR file) in the specified locale.
+     */
     private void launchSimulation( String locale, File targetLocation ) {
+        //todo: add support for locale when we have added support for non-english offline JARs
         String javaPath = System.getProperty( "java.home" ) + System.getProperty( "file.separator" ) + "bin" + System.getProperty( "file.separator" ) + "java";
         try {
             String[] cmd = new String[]{javaPath, "-jar", targetLocation.getAbsolutePath()};
             println( "exec'ing command=" + Arrays.asList( cmd ) );
             Process p = Runtime.getRuntime().exec( cmd );
+            //todo: display output from this process in case any errors occur
         }
         catch( IOException e ) {
             e.printStackTrace();
             println( e.toString() );
         }
-
     }
 
+    /*
+     * Downloads the JAR for the specified simulation to the specified location.
+     */
     private void download( String project, String flavor, File targetLocation ) throws FileNotFoundException {
         Util.download( "http://phet.colorado.edu/sims/" + project + "/" + flavor + ".jar", targetLocation );
     }
 
+    private static void println( String ms ) {
+        DebugLogger.println( Updater.class.getName() + "> " + ms );
+    }
+
+    /*
+    This main is invoked by the Updater in UpdateButton or equivalent.
+    Arguments must be as in this example:
+    java -jar updater.jar nuclear-physics alpha-decay en C:/temp/alpha-decay.jar
+    */
     public static void main( String[] args ) {
         String project = args[0];
         String sim = args[1];
@@ -49,9 +68,5 @@ public class Updater {
 
         println( "started updater, project=" + project + ", sim=" + sim + ", locale=" + locale + ", targetlocation=" + targetLocation );
         new Updater().update( project, sim, locale, targetLocation );
-    }
-
-    private static void println( String ms ) {
-        DebugLogger.println( Updater.class.getName() + "> " + ms );
     }
 }
