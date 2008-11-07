f9812482f7c4fc652bf7fd76d7acb5e60f4b1bd0
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-11-07 05:10:31 +0000

    Document the updater class, identify todo's for if/when problems occur.  See #881

--- trunk/util/updater/src/edu/colorado/phet/updater/DebugLogger.java
+++ trunk/util/updater/src/edu/colorado/phet/updater/DebugLogger.java
@@ -4,15 +4,17 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
+/**
+ * This class is responsible for producing a log file to help debug issues with the update process.
+ */
 public class DebugLogger {
-    
-    public static File logFile = new File( System.getProperty( "java.io.tmpdir" ) + System.getProperty( "file.separator" ) + "updater-log.txt" );
+    private static File logFile = new File( System.getProperty( "java.io.tmpdir" ) + System.getProperty( "file.separator" ) + "updater-log.txt" );
     private static FileWriter fileWriter;
 
     static {
         System.out.println( "Inited data file: " + logFile.getAbsolutePath() );
         try {
-            logFile.createNewFile();
+            boolean a = logFile.createNewFile();
             fileWriter = new FileWriter( logFile, true );
         }
         catch( IOException e ) {
@@ -20,6 +22,11 @@ public class DebugLogger {
         }
     }
 
+    /**
+     * Write a message to the console and the log file.
+     *
+     * @param message
+     */
     public static void println( String message ) {
         try {
             fileWriter.write( message + "\n" );
