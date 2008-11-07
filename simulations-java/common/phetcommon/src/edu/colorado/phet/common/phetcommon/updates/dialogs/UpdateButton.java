c60d0d09b9e26a7c9b264050d8ea2b6f97bf31b5
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-11-07 06:18:27 +0000

    cleanup docs

--- simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/updates/dialogs/UpdateButton.java
+++ simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/updates/dialogs/UpdateButton.java
@@ -8,12 +8,6 @@ import javax.swing.JButton;
 import edu.colorado.phet.common.phetcommon.resources.PhetCommonResources;
 import edu.colorado.phet.common.phetcommon.updates.SimUpdater;
 
-/**
- * Updates the simulations by running the PhET "updater", which downloads the new version
- * of the sim, replaces the running version, and restarts the new version.
- *
- * @author Sam Reid
- */
 public class UpdateButton extends JButton {
     public UpdateButton( final String project, final String sim, final String locale ) {
         super( PhetCommonResources.getString( "Common.updates.updateNow" ) );
