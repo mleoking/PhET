027822468fdfb6cf3f6d1d10eb5672b27c2701bd
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-11-09 07:04:17 +0000

    replace website root with {$this->prefix}, as in previous version

--- website/about/index.php
+++ website/about/index.php
@@ -16,11 +16,11 @@ class AboutPage extends SitePage {
         <p>PhET Interactive Simulations is an ongoing effort to provide
 an extensive suite of simulations to improve the way that physics, chemistry,
 biology, earth science and math are taught and learned.   The <a
-href="http://phet.colorado.edu/simulations/index.php">simulations</a> are
+href="{$this->prefix}simulations/index.php">simulations</a> are
 interactive tools that enable students to make connections between real life
 phenomena and the underlying science which explains such phenomena.  Our team
 of scientists, software engineers and science educators use a <a
-href="http://phet.colorado.edu/research/index.php">research-based approach</a> –
+href="{$this->prefix}research/index.php">research-based approach</a> –
 incorporating findings from prior research and our own testing – to create
 simulations that support student engagement with and understanding of
 scientific concepts. </p>
@@ -39,17 +39,17 @@ cause-and-effects relationships as well as multiple linked representations
 simulations are extensively tested and evaluated.  These tests include student
 interviews in addition to actual utilization of the simulations in a variety of
 settings, including lectures, group work, homework and lab work.   Our <a
-href="http://phet.colorado.edu/about/legend.php">rating system</a> indicates
+href="{$this->prefix}about/legend.php">rating system</a> indicates
 what level of testing has been completed on each simulation. </p>
 
 <p>All PhET simulations are freely available from the <a
-href="http://phet.colorado.edu/index.php">PhET website</a> and are easy to use
+href="{$this->prefix}index.php">PhET website</a> and are easy to use
 and incorpate into the classroom. They are written in <a
-href="http://phet.colorado.edu/tech_support/support-java.php">Java</a> and <a
-href="http://phet.colorado.edu/tech_support/support-flash.php">Flash</a>, and
+href="{$this->prefix}tech_support/support-java.php">Java</a> and <a
+href="{$this->prefix}tech_support/support-flash.php">Flash</a>, and
 can be run using a standard web browser as long as <a
-href="http://phet.colorado.edu/tech_support/support-flash.php">Flash</a> and <a
-href="http://phet.colorado.edu/tech_support/support-java.php">Java</a> are
+href="{$this->prefix}tech_support/support-flash.php">Flash</a> and <a
+href="{$this->prefix}tech_support/support-java.php">Java</a> are
 installed. </p>
 
 EOT;
