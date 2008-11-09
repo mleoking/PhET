331235dc624a7bac32730a69b7138e49ebc90d63
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-11-09 04:30:06 +0000

    add new about text from Wendy

--- website/about/index.php
+++ website/about/index.php
@@ -13,26 +13,44 @@ class AboutPage extends SitePage {
         }
 
         print <<<EOT
-        <p>
-        The Physics Education Technology (PhET) project is an ongoing effort to provide an extensive suite of simulations for teaching and learning physics and chemistry and to make these resources both freely available from the <a href="{$this->prefix}index.php">PhET website</a> and easy to incorporate into classrooms.
-        </p>
+        <p>PhET Interactive Simulations is an ongoing effort to provide
+an extensive suite of simulations to improve the way that physics, chemistry,
+biology, earth science and math are taught and learned.   The <a
+href="http://phet.colorado.edu/simulations/index.php">simulations</a> are
+interactive tools that enable students to make connections between real life
+phenomena and the underlying science which explains such phenomena.  Our team
+of scientists, software engineers and science educators use a <a
+href="http://phet.colorado.edu/research/index.php">research-based approach</a> –
+incorporating findings from prior research and our own testing – to create
+simulations that support student engagement with and understanding of
+scientific concepts. </p>
 
-        <p>The <a href="{$this->prefix}simulations/index.php">simulations</a> are animated, interactive, and game-like environments in which students learn through exploration. In these simulations, we emphasize the connections between real life phenomena and the underlying science and seek to make the visual and conceptual models that expert physicists use accessible to students.
-         </p>
+<p>In order to help students visually comprehend thse concepts,
+PhET simulations animate what is invisible to the eye through the use of
+graphics and intuitive controls such as click-and-drag manipulation, sliders
+and radio buttons.  In order to further encourage quantitative exploration, the
+simulations also offer measurement instruments including rulers, stop-watchs,
+voltmeters and thermometers.  As the user manipulates these interactive tools,
+responses are immediately animated thus effectively illustrating
+cause-and-effects relationships as well as multiple linked representations
+(motion of the objects, graphs, number readouts, etc…).</p>
 
+<p>To ensure eduational effectiveness and usability, all of the
+simulations are extensively tested and evaluated.  These tests include student
+interviews in addition to actual utilization of the simulations in a variety of
+settings, including lectures, group work, homework and lab work.   Our <a
+href="http://phet.colorado.edu/about/legend.php">rating system</a> indicates
+what level of testing has been completed on each simulation. </p>
 
-        <p>Our team of scientists, software engineers and science educators uses a <a href="{$this->prefix}research/index.php">research-based approach</a> in our design – incorporating findings from prior research and our own testing – to create simulations that support student engagement with and understanding of physics concepts.
-        </p>
-
-        <p>
-        PhET simulations animate what is invisible to the eye, such as atoms, electrons, photons and electric fields. User interaction is encouraged by engaging graphics and intuitive controls that include click-and-drag manipulation, sliders and radio buttons. By immediately animating the response to any user interaction, the simulations are particularly good at establishing cause-and-effect and at linking multiple representations.
-        </p>
-
-        <p>For quantitative exploration, the simulations have measurement instruments available, such as a ruler, stop-watch, voltmeter and thermometer. All the simulations are extensively tested for usability and educational effectiveness, and a <a href="{$this->prefix}about/legend.php">rating system</a> is used to indicate what level of testing they have received. The tests involve student interviews and use of the simulations in a variety of settings, including lectures, group work, homework and lab work.
-        </p>
-
-        <p>The PhET simulations are easy to use. They are written in <a href="{$this->prefix}tech_support/support-java.php">Java</a> and <a href="{$this->prefix}tech_support/support-flash.php">Flash</a>, and can be run using a standard web browser as long as the latest Flash and Java plug-ins are installed.
-        </p>
+<p>All PhET simulations are freely available from the <a
+href="http://phet.colorado.edu/index.php">PhET website</a> and are easy to use
+and incorpate into the classroom. They are written in <a
+href="http://phet.colorado.edu/tech_support/support-java.php">Java</a> and <a
+href="http://phet.colorado.edu/tech_support/support-flash.php">Flash</a>, and
+can be run using a standard web browser as long as <a
+href="http://phet.colorado.edu/tech_support/support-flash.php">Flash</a> and <a
+href="http://phet.colorado.edu/tech_support/support-java.php">Java</a> are
+installed. </p>
 
 EOT;
         return true;
