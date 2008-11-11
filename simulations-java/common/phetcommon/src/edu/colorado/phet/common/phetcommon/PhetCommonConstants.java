07a9654f93a45a7a2153dc0281d265bc5add25da
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-11-11 18:31:23 +0000

    11-11-08: add constant for .phet directory

--- simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/PhetCommonConstants.java
+++ simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/PhetCommonConstants.java
@@ -6,5 +6,7 @@ public class PhetCommonConstants {
     public static final String PHET_NAME = "PhET";  // user-visible name, does not require translation
     public static final String PHET_HOME_URL = "http://phet.colorado.edu";
     public static final String PHET_EMAIL = "phethelp@colorado.edu";
+    
+    public static final String PERSISTENCE_DIRNAME = System.getProperty( "user.home" ) + System.getProperty( "file.separator" ) + ".phet";
 
 }
