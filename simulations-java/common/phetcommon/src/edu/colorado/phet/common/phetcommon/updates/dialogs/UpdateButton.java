4517cef4e7a1c774e7126622f47f528a0e924d8b
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-11-11 19:12:26 +0000

    see #924, "Update Now" button needs to do something different for online sims

--- simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/updates/dialogs/UpdateButton.java
+++ simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/updates/dialogs/UpdateButton.java
@@ -6,14 +6,21 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 
 import edu.colorado.phet.common.phetcommon.resources.PhetCommonResources;
+import edu.colorado.phet.common.phetcommon.updates.OpenWebPageToNewVersion;
 import edu.colorado.phet.common.phetcommon.updates.SimUpdater;
+import edu.colorado.phet.common.phetcommon.util.PhetUtilities;
 
 public class UpdateButton extends JButton {
     public UpdateButton( final String project, final String sim, final String locale ) {
         super( PhetCommonResources.getString( "Common.updates.updateNow" ) );
         addActionListener( new ActionListener() {
             public void actionPerformed( ActionEvent e ) {
-                new SimUpdater().updateSim( project, sim, locale );
+                if ( PhetUtilities.isRunningFromWebsite() ) {
+                    OpenWebPageToNewVersion.openWebPageToNewVersion( project, sim );
+                }
+                else {
+                    new SimUpdater().updateSim( project, sim, locale );
+                }
             }
         } );
     }
