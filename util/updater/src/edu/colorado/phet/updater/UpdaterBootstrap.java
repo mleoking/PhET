40a54a24fb931987242a156a02b6010be5df1f8f
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-11-12 18:47:09 +0000

    see #881 - throw exceptions all the way up to main so that we stop executing when an exception occurs - change "copy" to "replace", so that src file is deleted on exit - add flag to turn off debugging output for production releases - add getJavaPath method

--- util/updater/src/edu/colorado/phet/updater/UpdaterBootstrap.java
+++ util/updater/src/edu/colorado/phet/updater/UpdaterBootstrap.java
@@ -8,66 +8,67 @@ import java.util.Arrays;
  * Copies and launches specified JAR, see SimUpdater.
  */
 public class UpdaterBootstrap {
-    private File src;
-    private File dst;
+    
+    // do not enabled debug output for public releases, the log file will grow indefinitely!
+    private static final boolean DEBUG_OUTPUT_ENABLED = true;
+    
+    private final File src;
+    private final File dst;
 
     public UpdaterBootstrap( File src, File dst ) {
         this.src = src;
         this.dst = dst;
     }
 
-    private void copyAndLaunch() {
-        //todo: updater may need to wait explicitly, since the original JAR presumably must be exited before it can be overwritten
-        // Download the new, updated version of the sim.
-        copy();
-        launchSimulation();
+    private void replaceAndLaunch() throws IOException {
+        //TODO: updater may need to wait explicitly, since the original JAR presumably must be exited before it can be overwritten
+        replace();
+        launch();
         println( "finished launch" );
     }
 
-    private void copy() {
-        try {
-            FileUtils.copyTo( src, dst );
-        }
-        catch( IOException e ) {
-            e.printStackTrace();
-            println( e.getMessage() );
-        }
+    private void replace() throws IOException {
+        FileUtils.copyTo( src, dst );
+        src.deleteOnExit();
     }
 
     /*
-     * Launches the simulation (as specified by a JAR file) in the specified locale.
+     * Launches the simulation (as specified by a JAR file) with the specified language code.
      */
-    private void launchSimulation() {
-        //todo: add support for locale when we have added support for non-english offline JARs
-        String javaPath = System.getProperty( "java.home" ) + System.getProperty( "file.separator" ) + "bin" + System.getProperty( "file.separator" ) + "java";
-        try {
-            String[] cmd = new String[]{javaPath, "-jar", dst.getAbsolutePath()};
-            println( "exec'ing command=" + Arrays.asList( cmd ) );
-            Process p = Runtime.getRuntime().exec( cmd );
-            //todo: display output from this process in case any errors occur
-        }
-        catch( IOException e ) {
-            e.printStackTrace();
-            println( e.toString() );
-        }
+    private void launch() throws IOException {
+        //TODO: add support for language code when we have added support for non-English offline JARs
+        String[] cmdArray = new String[] { getJavaPath(), "-jar", dst.getAbsolutePath() };
+        println( "restarting sim with cmdArray=" + Arrays.asList( cmdArray ) );
+        Process p = Runtime.getRuntime().exec( cmdArray );
+        //TODO: display output from this process in case any errors occur
+    }
+    
+    public static String getJavaPath() {
+        return System.getProperty( "java.home" ) + System.getProperty( "file.separator" ) + "bin" + System.getProperty( "file.separator" ) + "java";
     }
 
     private static void println( String ms ) {
-        DebugLogger.println( UpdaterBootstrap.class.getName() + "> " + ms );
+        if ( DEBUG_OUTPUT_ENABLED ) {
+            DebugLogger.println( UpdaterBootstrap.class.getName() + "> " + ms );
+        }
     }
 
     /*
-    This main is invoked by the Updater in UpdateButton or equivalent.
-    Arguments must be as in this example:
-    java -jar updater.jar C:/temp/alpha-decay0123.jar C:/user/phet/alpha-decay.jar
-    */
+     * This main is invoked by the Updater in UpdateButton or equivalent.
+     * Arguments must be as in this example:
+     * java -jar updater.jar C:/temp/alpha-decay0123.jar C:/user/phet/alpha-decay.jar
+     */
     public static void main( String[] args ) {
         String src = args[0];
         String dst = args[1];
-
-        println( "started updater, src=" + src + ", target=" + dst );
-        new UpdaterBootstrap( new File( src ), new File( dst ) ).copyAndLaunch();
+        println( "starting updater, src=" + src + ", target=" + dst );
+        try {
+            new UpdaterBootstrap( new File( src ), new File( dst ) ).replaceAndLaunch();
+        }
+        catch ( IOException e ) {
+            e.printStackTrace();
+            println( e.getMessage() );
+            System.exit( 1 ); // indicate abnormal exit
+        }
     }
-
-
 }
