0a24395db40d005e2e61b0f7dd931d77a2d773d2
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-11-14 17:33:45 +0000

    see #956, use "Reset All" string from common strings

--- trunk/simulations-java/simulations/balloons/src/edu/colorado/phet/balloons/BalloonsSimulationPanel.java
+++ trunk/simulations-java/simulations/balloons/src/edu/colorado/phet/balloons/BalloonsSimulationPanel.java
@@ -24,6 +24,7 @@ import edu.colorado.phet.balloons.common.phys2d.Repaint;
 import edu.colorado.phet.balloons.common.phys2d.System2D;
 import edu.colorado.phet.common.phetcommon.model.clock.ClockAdapter;
 import edu.colorado.phet.common.phetcommon.model.clock.ClockEvent;
+import edu.colorado.phet.common.phetcommon.resources.PhetCommonResources;
 import edu.colorado.phet.common.phetcommon.view.HelpPanel;
 import edu.colorado.phet.common.phetcommon.view.util.BufferedImageUtils;
 
@@ -179,7 +180,7 @@ public class BalloonsSimulationPanel extends JPanel {
         buttonPanel.add( showNoCharges );
         buttonPanel.add( showDiff );
         buttonPanel.setBorder( createSmoothBorder( BalloonsResources.getString( "BalloonApplet.ChargeDisplay" ) ) );
-        JButton resetBtn = new JButton( BalloonsResources.getString( "BalloonApplet.Reset" ) );
+        JButton resetBtn = new JButton( PhetCommonResources.getString( PhetCommonResources.STRING_RESET_ALL ) );
         controlPanel.add( resetBtn );
         controlPanel.add( buttonPanel );
 
