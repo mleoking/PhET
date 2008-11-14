bfc91300219615e82e231f0e06dfe8f2043d699a
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-11-14 17:53:36 +0000

    see #949, can updater.jar delete itself on exit?

--- trunk/util/updater/src/edu/colorado/phet/updater/FileUtils.java
+++ trunk/util/updater/src/edu/colorado/phet/updater/FileUtils.java
@@ -1,13 +1,23 @@
 package edu.colorado.phet.updater;
 
 import java.io.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 public class FileUtils {
+    
     public static void copyTo( File source, File dest ) throws IOException {
         copyAndClose( new FileInputStream( source ), new FileOutputStream( dest ), true );
     }
+    
+    private static void copyAndClose( InputStream source, OutputStream dest, boolean buffered ) throws IOException {
+        copy( source, dest, buffered );
+        source.close();
+        dest.close();
+    }
 
-    public static void copy( InputStream source, OutputStream dest, boolean buffered ) throws IOException {
+    private static void copy( InputStream source, OutputStream dest, boolean buffered ) throws IOException {
         //todo: buffering is disabled until file truncation issue is resolved
         buffered = false;
         if ( buffered ) {
@@ -24,9 +34,37 @@ public class FileUtils {
         }
     }
 
-    public static void copyAndClose( InputStream source, OutputStream dest, boolean buffered ) throws IOException {
-        copy( source, dest, buffered );
-        source.close();
-        dest.close();
+    // copied from phetcommon FileUtils
+    /**
+     * Determines if a file has a specified suffix.
+     * The suffix is case insensitive.
+     * You can specify either "xyz" or ".xyz" and this will do the right thing.
+     * 
+     * @param file
+     * @param suffix
+     * @return
+     */
+    public static boolean hasSuffix( File file, String suffix ) {
+        if ( !suffix.startsWith( "." ) ) {
+            suffix = "." + suffix;
+        }
+        return file.getName().toLowerCase().endsWith( suffix );
+    }
+    
+    // copied from phetcommon FileUtils
+    /**
+     * Gets the JAR file that this class was launched from.
+     */
+    public static File getCodeSource() {
+        URL url = FileUtils.class.getProtectionDomain().getCodeSource().getLocation();
+        try {
+            URI uri = new URI( url.toString() );
+            return new File( uri.getPath() );
+        }
+        catch( URISyntaxException e ) {
+            System.out.println( e.getMessage() );
+            e.printStackTrace();
+            throw new RuntimeException( e );
+        }
     }
 }
