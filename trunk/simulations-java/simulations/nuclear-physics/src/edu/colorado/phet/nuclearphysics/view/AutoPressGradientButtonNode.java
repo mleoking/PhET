a634127fc2ccbc7ca51e8cac69dea7593966d7b4
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-11-21 18:12:08 +0000

    Made the "autopress" happen on the canvas when the half life is adjusted via the time chart.

--- trunk/simulations-java/simulations/nuclear-physics/src/edu/colorado/phet/nuclearphysics/view/AutoPressGradientButtonNode.java
+++ trunk/simulations-java/simulations/nuclear-physics/src/edu/colorado/phet/nuclearphysics/view/AutoPressGradientButtonNode.java
@@ -18,17 +18,19 @@ import edu.umd.cs.piccolo.nodes.PPath;
  */
 public class AutoPressGradientButtonNode extends GradientButtonNode{
 
-	private static final int HIGHLIGHT_TIME = 500; // In milliseconds.
+	private static final int HIGHLIGHT_TIME = 250; // In milliseconds.
 	private static final int PRESS_TIME = 500; // In milliseconds.
 	
 	// Possible states for the button.
 	private static final int BUTTON_IDLE = 1;
-	private static final int BUTTON_HIGHLIGHTED = 2;
+	private static final int BUTTON_PRE_PRESS_HIGHLIGHTED = 2;
 	private static final int BUTTON_PRESSED = 3;
+	private static final int BUTTON_POST_PRESS_HIGHLIGHTED = 3;
 	
 	// Timers
-    private static final Timer HIGHLIGHT_TIMER = new Timer( HIGHLIGHT_TIME, null );
+    private static final Timer PRE_PRESS_HIGHLIGHT_TIMER = new Timer( HIGHLIGHT_TIME, null );
     private static final Timer PRESS_TIMER = new Timer( PRESS_TIME, null );
+    private static final Timer POST_PRESS_HIGHLIGHT_TIMER = new Timer( HIGHLIGHT_TIME, null );
     
     // State variable for button state.
 	private int buttonState;
@@ -41,29 +43,40 @@ public class AutoPressGradientButtonNode extends GradientButtonNode{
 		super(label, fontSize, buttonColor);
 		buttonState = BUTTON_IDLE;
 
-		HIGHLIGHT_TIMER.addActionListener( new ActionListener() {
+		PRE_PRESS_HIGHLIGHT_TIMER.addActionListener( new ActionListener() {
             public void actionPerformed( ActionEvent e ) {
             	// Make the button look like it is pressed.
     			PPath button = getButton();
         		button.setPaint( getArmedGradient() );
     			button.setOffset(SHADOW_OFFSET, SHADOW_OFFSET);
         		buttonState = BUTTON_PRESSED;
-        		HIGHLIGHT_TIMER.stop();
+        		PRE_PRESS_HIGHLIGHT_TIMER.stop();
         		PRESS_TIMER.start();
             }
         } );
 
 		PRESS_TIMER.addActionListener( new ActionListener() {
             public void actionPerformed( ActionEvent e ) {
+            	// Make the button look like it is highlighted but not pressed.
+    			PPath button = getButton();
+        		button.setPaint( getMouseOverGradient() );
+    			button.setOffset(0, 0);
+        		buttonState = BUTTON_POST_PRESS_HIGHLIGHTED;
+        		PRESS_TIMER.stop();
+        		POST_PRESS_HIGHLIGHT_TIMER.start();
+            }
+        } );
+
+		POST_PRESS_HIGHLIGHT_TIMER.addActionListener( new ActionListener() {
+            public void actionPerformed( ActionEvent e ) {
             	// Make the button look like it is neither pressed nor highlighted.
     			PPath button = getButton();
         		button.setPaint( getMouseNotOverGradient() );
     			button.setOffset(0, 0);
         		buttonState = BUTTON_IDLE;
-        		PRESS_TIMER.stop();
+        		POST_PRESS_HIGHLIGHT_TIMER.stop();
             }
         } );
-
 	}
 
 	public void autoPress(){
@@ -73,8 +86,8 @@ public class AutoPressGradientButtonNode extends GradientButtonNode{
 			button.setPaint( getMouseOverGradient() );
 			
 			// Start the timer for the next step.
-	        HIGHLIGHT_TIMER.start();
-	        buttonState = BUTTON_HIGHLIGHTED;
+	        PRE_PRESS_HIGHLIGHT_TIMER.start();
+	        buttonState = BUTTON_PRE_PRESS_HIGHLIGHTED;
 		}
 	}
 }
