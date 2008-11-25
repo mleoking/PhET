a2ebd0d17b075317516a2cc8fbd8debca1f96bbb
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-11-25 20:27:46 +0000

    Added the ability to automatically delete a backup that was created earlier on the same day.

--- website/phet-dist/installers/create-backup.sh
+++ website/phet-dist/installers/create-backup.sh
@@ -12,9 +12,20 @@ BACKUP_DIR_STEM_NAME=backup
 BACKUP_DIR=$BACKUP_DIR_STEM_NAME-$(date +%Y-%m-%d)
 let NUM_BACKUPS_TO_KEEP=2
 
+echo ""
 echo "Creating backup of installers..."
 
-# Make the backup directory.
+# See if the backup directory already exists and delete it if so.
+
+if [ -d $BACKUP_DIR ]; then
+    echo ""
+    echo "Warning: A backup directory already exists for today, indicating that a"
+    echo "backup has already been performed.  This directory will be deleted."
+    echo ""
+    rm -rf $BACKUP_DIR
+fi
+
+# Create the backup directory.
 
 mkdir $BACKUP_DIR
 if [ "$?" -ne "0" ]; then
@@ -24,8 +35,10 @@ fi
 
 # Copy the files into the backup directory.
 
+echo ""
 cp -v *installer* ./$BACKUP_DIR
 cp -v *CD* ./$BACKUP_DIR
+echo ""
 
 if [ "$?" -ne "0" ]; then
   echo "Error copying files into backup directory."
@@ -42,5 +55,6 @@ for DIR in $EXCESS_BACKUP_DIRS
 do
    echo Removing old backup directory $DIR
    rm -rf $DIR
+   echo ""
 done
 
