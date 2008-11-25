ffdd5051a366ed1101dd6d3d9c36fb1477956be4
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-11-25 17:03:50 +0000

    Completed the function for adding the marker file and now call it from the function that downloads the Java and Flash sims.

--- util/phet-installer/new-automated/build-install.php
+++ util/phet-installer/new-automated/build-install.php
@@ -202,6 +202,9 @@
 
         // Get the resources for the Flash sims.
         builder_download_flash_rsrcs();
+
+        // Add the marker file, needed for sim usage tracking.
+        create_marker_file();
     }
 
     //--------------------------------------------------------------------------
@@ -286,9 +289,10 @@
     // See unfuddle ticket #875 for more information.
     //--------------------------------------------------------------------------
     function create_marker_file(){
-        $local_file_name = ".phet_installer";
-	$contents = "DO NOT DELETE THIS FILE. This file is used by the PhET tracking feature to detect that these sims were installed using the PhET Installer."
-        file_put_contents_anywhere($local_file_name, $contents);
+        $base_file_name = ".phet_installer";
+        $marker_file_name = RIPPED_WEBSITE_TOP.PHET_SIMS_SUBDIR.$base_file_name;
+	$contents = "DO NOT DELETE THIS FILE. This file is used by the PhET tracking feature to detect that these sims were installed using the PhET Installer.";
+        file_put_contents_anywhere($marker_file_name, $contents);
     }
 
     //--------------------------------------------------------------------------
