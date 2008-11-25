0355a463d1dfd424f8782c8a7b1f7bd231bf4c18
dano <dano@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-11-25 01:29:45 +0000

    website: Added logging utilities

--- trunk/website/admin/get-run-offline.php
+++ trunk/website/admin/get-run-offline.php
@@ -7,6 +7,7 @@
     include_once(SITE_ROOT."admin/sim-utils.php");
     include_once(SITE_ROOT."admin/sys-utils.php");
     include_once(SITE_ROOT."admin/web-utils.php");
+    include_once(SITE_ROOT."admin/log-utils.php");
 
     // Get the simulation id
     $sim_id = $_REQUEST['sim_id'];
@@ -19,6 +20,11 @@
 
     // Get the simulation data
     $simulation = sim_get_sim_by_id($sim_id);
+
+    // Log this info
+    $datefmt = "y/m/d h:i:s A";
+    $log_string = join("\t", array(date($datefmt), time(), $simulation['sim_name'], $language_code, $_SERVER['REMOTE_ADDR']))."\n";
+    log_message('download-java-sim.log', $log_string);
 
     // Get the filename and content
     $download_data = sim_get_run_offline($simulation, $language_code);
