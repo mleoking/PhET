8db08b9e63e4f6224769cd3aecbff5777b51972d
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-12-01 23:10:16 +0000

    see #945, generalize error dialog

--- simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/updates/dialogs/UpdateErrorDialog.java
+++ simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/updates/dialogs/UpdateErrorDialog.java
@@ -1,92 +1,44 @@
+/* Copyright 2008, University of Colorado */
+
 package edu.colorado.phet.common.phetcommon.updates.dialogs;
 
 import java.awt.Frame;
-import java.awt.GridBagConstraints;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.text.MessageFormat;
 
-import javax.swing.*;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
 
+import edu.colorado.phet.common.phetcommon.dialogs.ErrorDialog;
 import edu.colorado.phet.common.phetcommon.resources.PhetCommonResources;
-import edu.colorado.phet.common.phetcommon.view.util.EasyGridBagLayout;
 import edu.colorado.phet.common.phetcommon.view.util.HTMLUtils;
 import edu.colorado.phet.common.phetcommon.view.util.SwingUtils;
-import edu.colorado.phet.common.phetcommon.view.util.HTMLUtils.InteractiveHTMLPane;
 
 /**
- * Error dialog, with a summary and "Details" button to get more info.
- * The "Details" button opens a Details dialog.
+ * This error dialog indicates that something went wrong with an update.
+ * It includes a summary message and "Details" button to get more info.
+ * The "Details" button opens another dialog that shows a stack trace.
  */
-public class UpdateErrorDialog extends JDialog {
+public class UpdateErrorDialog extends ErrorDialog {
     
     private static final String TITLE = PhetCommonResources.getString( "Common.updates.error.title" );
-    private static final String OK_BUTTON = PhetCommonResources.getString( "Common.choice.ok" );
-    private static final String DETAILS_BUTTON = PhetCommonResources.getString( "Common.updates.error.detailsButton" );
-    private static final String DETAILS_TITLE = PhetCommonResources.getString( "Common.updates.error.detailsTitle" );
+    private static final String ERROR_MESSAGE = PhetCommonResources.getString( "Common.updates.errorMessage" );
 
     public UpdateErrorDialog( Frame owner, final Exception exception ) {
-        super( owner, TITLE );
+        super( owner, TITLE, getErrorMessageHTML(), exception );
         setModal( true );
-        setResizable( false );
-        
-        // notification that an error occurred
-        String html = getErrorMessageHTML();
-        JComponent htmlPane = new InteractiveHTMLPane( html );
-        JPanel messagePanel = new JPanel();
-        messagePanel.setBorder( BorderFactory.createEmptyBorder( 5, 5, 5, 5 ) );
-        messagePanel.add( htmlPane );
-        
-        // closes the dialog
-        JButton okButton = new JButton( OK_BUTTON );
-        okButton.addActionListener( new ActionListener() {
-            public void actionPerformed( ActionEvent e ) {
-                dispose();
-            }
-        } );
-        
-        // shows the stack trace
-        //TODO: make it easy to copy-&-paste this, for emailing to phethelp
-        final JButton detailsButton = new JButton( DETAILS_BUTTON );
-        detailsButton.addActionListener( new ActionListener() {
-            public void actionPerformed( ActionEvent event ) {
-                final StringWriter result = new StringWriter();
-                final PrintWriter printWriter = new PrintWriter( result );
-                exception.printStackTrace( printWriter );
-                JOptionPane.showMessageDialog( detailsButton, result.getBuffer(), DETAILS_TITLE, JOptionPane.INFORMATION_MESSAGE );
-            }
-        } );
-        
-        JPanel buttonPanel = new JPanel();
-        buttonPanel.add( okButton );
-        buttonPanel.add( detailsButton );
-        
-        // main panel layout
-        JPanel panel = new JPanel();
-        EasyGridBagLayout layout = new EasyGridBagLayout( panel );
-        panel.setLayout( layout );
-        int row = 0;
-        int column = 0;
-        layout.addComponent( messagePanel, row++, column );
-        layout.addFilledComponent( new JSeparator(), row++, column, GridBagConstraints.HORIZONTAL );
-        layout.addAnchoredComponent( buttonPanel, row++, column, GridBagConstraints.CENTER );
-        
-        setContentPane( panel );
-        pack();
         SwingUtils.centerDialogInParent( this );
     }
     
     protected static String getErrorMessageHTML() {
-        Object[] args = { HTMLUtils.getPhetHomeHref(), HTMLUtils.getPhetMailtoHref() };
-        String htmlFragment = MessageFormat.format( PhetCommonResources.getString( "Common.updates.errorMessage" ), args );
+        Object[] args = { HTMLUtils.getPhetHomeHref() };
+        String htmlFragment = MessageFormat.format( ERROR_MESSAGE, args );
         return HTMLUtils.createStyledHTMLFromFragment( htmlFragment );
     }
     
+    // test
     public static void main( String[] args ) {
         // dialog must have an owner if you want cursor to change over hyperlinks
         JFrame frame = new JFrame();
@@ -95,6 +47,9 @@ public class UpdateErrorDialog extends JDialog {
         frame.setVisible( true );
         JDialog dialog = new UpdateErrorDialog( frame, new IOException() );
         dialog.addWindowListener( new WindowAdapter() {
+            public void windowClosing( WindowEvent e ) {
+                System.exit( 0 );
+            }
             public void windowClosed( WindowEvent e ) {
                 System.exit( 0 );
             }
