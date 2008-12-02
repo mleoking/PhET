3b248dd10f82828bc4f6aee338f31b543ba64ccd
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-12-02 18:56:26 +0000

    cleanup and generalize ILogger hierarchy

--- simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/util/logging/ConsoleLogger.java
+++ simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/util/logging/ConsoleLogger.java
@@ -1,8 +1,22 @@
 /* Copyright 2008, University of Colorado */
+
 package edu.colorado.phet.common.phetcommon.util.logging;
 
 public class ConsoleLogger implements ILogger{
+    
     public void log( String text ) {
         System.out.println( text );
+        System.out.flush();
+    }
+    
+    public void logError( String text ) {
+        System.err.println( text );
+        System.err.flush();
+    }
+    
+    public static void main( String[] args ) {
+        ILogger logger = new ConsoleLogger();
+        logger.log( "good news" );
+        logger.logError( "bad news" );
     }
 }
