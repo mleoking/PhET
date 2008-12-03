cd67cea38051609fe257086d01c26c736db98e7f
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-12-03 19:36:42 +0000

    Removed packages that are now empty.

--- simulations-java/simulations/states-of-matter/src/edu/colorado/phet/statesofmatter/model/engine/integration/Integrator1D.java
+++
@@ -1,7 +0,0 @@
-package edu.colorado.phet.statesofmatter.model.engine.integration;
-
-public interface Integrator1D {
-    double nextPosition(double lastX, double lastV, double lastA);
-
-    double nextVelocity(double lastV, double lastA, double curA);
-}
