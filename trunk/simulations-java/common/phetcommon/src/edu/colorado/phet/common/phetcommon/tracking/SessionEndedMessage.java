d55dfc6ab8f490a5444ceb4db50c54dd4a7f8279
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-12-16 01:09:07 +0000

    see #862, add tracking message fields to match database design spec, change tracking message names and field to use "_" instead of "-" see #1041, each message_type should have a separate message_version value, message_version should be sent with every message see #1042, tracker_version should be sent with every message

--- trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/tracking/SessionEndedMessage.java
+++ trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/tracking/SessionEndedMessage.java
@@ -6,7 +6,12 @@ package edu.colorado.phet.common.phetcommon.tracking;
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
 public class SessionEndedMessage extends TrackingMessage {
-    public SessionEndedMessage( SessionID sessionID) {
-        super( sessionID, "session-ended" );
+    
+    // Versioning the messages allows us to manage data after changing message content.
+    // If the content of this message is changed, you'll need to increment the version number.
+    public static final String MESSAGE_VERSION = "1";
+    
+    public SessionEndedMessage( SessionID sessionID ) {
+        super( sessionID, "session_ended", MESSAGE_VERSION );
     }
 }
