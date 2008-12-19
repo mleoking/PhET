629c5ed5fad571cb0242214048d324299e371d8f
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-12-19 18:48:59 +0000

    layout fixes, see #984 -reduce width to less than 1280 -reduce pad around project list

---
+++ trunk/simulations-java/build-tools/phet-build/src/edu/colorado/phet/build/java/Boxer.java
@@ -0,0 +1,21 @@
+package edu.colorado.phet.build.java;
+
+import javax.swing.*;
+
+public class Boxer {
+    public static JComponent horizontalBox( JComponent a, JComponent b ) {
+        return box( a, b, BoxLayout.X_AXIS );
+    }
+
+    public static JComponent verticalBox( JComponent a, JComponent b ) {
+        return box( a, b, BoxLayout.Y_AXIS );
+    }
+
+    private static JComponent box( JComponent a, JComponent b, int axis ) {
+        JPanel panel = new JPanel();
+        panel.setLayout( new BoxLayout( panel, axis ) );
+        panel.add( a );
+        panel.add( b );
+        return panel;
+    }
+}
