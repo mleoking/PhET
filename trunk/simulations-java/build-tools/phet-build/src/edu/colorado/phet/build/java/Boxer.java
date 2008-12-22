fd3cfceb0948c72aac7ff02c88db2a6e0f946860
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-12-22 18:37:03 +0000

    see #984. Removed a dependency on VerticalLayoutPanel.  This was causing build failure, because build-tools did not previously depend on phetcommon.  The dependency was in unused code, so I remove the unused code.

--- trunk/simulations-java/build-tools/phet-build/src/edu/colorado/phet/build/java/Boxer.java
+++ trunk/simulations-java/build-tools/phet-build/src/edu/colorado/phet/build/java/Boxer.java
@@ -1,10 +1,8 @@
 package edu.colorado.phet.build.java;
 
-import java.awt.*;
-
-import javax.swing.*;
-
-import edu.colorado.phet.common.phetcommon.view.VerticalLayoutPanel;
+import javax.swing.BoxLayout;
+import javax.swing.JComponent;
+import javax.swing.JPanel;
 
 public class Boxer {
     public static JComponent horizontalBox( JComponent a, JComponent b ) {
@@ -17,34 +15,6 @@ public class Boxer {
 
     private static JComponent box( JComponent a, JComponent b, int axis ) {
         return useBoxLayout( a, b, axis );
-//        return useVFLayout( a, b, axis );
-//        return useBorderLayout( a, b, axis );
-    }
-
-    private static JComponent useBorderLayout( JComponent a, JComponent b, int axis ) {
-        JPanel panel = new JPanel(new BorderLayout( ));
-        if ( axis == BoxLayout.Y_AXIS ) {
-            panel.add(a,BorderLayout.NORTH);
-            panel.add(b,BorderLayout.SOUTH);
-        }
-        else {
-            panel.add(a,BorderLayout.WEST);
-            panel.add(b,BorderLayout.EAST);
-        }
-        return panel;
-    }
-
-    private static JComponent useVFLayout( JComponent a, JComponent b, int axis ) {
-        JPanel panel = new JPanel();
-        if ( axis == BoxLayout.Y_AXIS ) {
-            panel = new VerticalLayoutPanel();
-        }
-        else {
-            panel.setLayout( new FlowLayout() );
-        }
-        panel.add( a );
-        panel.add( b );
-        return panel;
     }
 
     private static JComponent useBoxLayout( JComponent a, JComponent b, int axis ) {
