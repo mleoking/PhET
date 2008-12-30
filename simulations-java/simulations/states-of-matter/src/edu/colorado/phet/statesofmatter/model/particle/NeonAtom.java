250dc8ad42a00a3d7b9806d99634cf88728cbc99
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2008-12-30 20:13:16 +0000

    Made the epsilon value for neon smaller based on input from Paul Beale.

--- simulations-java/simulations/states-of-matter/src/edu/colorado/phet/statesofmatter/model/particle/NeonAtom.java
+++ simulations-java/simulations/states-of-matter/src/edu/colorado/phet/statesofmatter/model/particle/NeonAtom.java
@@ -12,7 +12,7 @@ public class NeonAtom extends StatesOfMatterAtom {
     public static final double RADIUS = 154;    // In picometers.
     private static final double MASS = 20.1797; // In atomic mass units.
     private static final double SIGMA = 308;    // In picometers.
-    private static final double EPSILON = 65;   // epsilon/k-Boltzmann is in Kelvin.
+    private static final double EPSILON = 32.8; // epsilon/k-Boltzmann is in Kelvin.
 
     
     public NeonAtom(double xPos, double yPos){
