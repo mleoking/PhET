986660ff918457e4f0c1d41108ad8d8017f1b752
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-01-03 18:32:24 +0000

    added test for interaction model

--- trunk/simulations-java/simulations/the-ramp/src/edu/colorado/phet/theramp/v2/model/RampObject.java
+++ trunk/simulations-java/simulations/the-ramp/src/edu/colorado/phet/theramp/v2/model/RampObject.java
@@ -10,8 +10,21 @@ public class RampObject {
     private double staticFrictionCoefficient;
     private double kineticFrictionCoefficient;
     private double angle;
+    private boolean interacting;
 
+
+    private static boolean started=false;
     public RampObject( double mass, ImmutableVector2D.Double position, ImmutableVector2D.Double v, ImmutableVector2D.Double a, double staticFrictionCoefficient, double kineticFrictionCoefficient, double angle ) {
+        this( mass, position, v, a, staticFrictionCoefficient, kineticFrictionCoefficient, angle, false );
+    }
+
+    public RampObject( double mass, ImmutableVector2D.Double position, ImmutableVector2D.Double v, ImmutableVector2D.Double a, double staticFrictionCoefficient, double kineticFrictionCoefficient, double angle, boolean interacting ) {
+        if (position.getMagnitude()>0){
+            started=true;
+        }
+        if (started&&position.getMagnitude()==0){
+            System.out.println( "" );
+        }
         this.mass = mass;
         this.position = position;
         this.v = v;
@@ -19,17 +32,46 @@ public class RampObject {
         this.staticFrictionCoefficient = staticFrictionCoefficient;
         this.kineticFrictionCoefficient = kineticFrictionCoefficient;
         this.angle = angle;
+        this.interacting = interacting;
     }
 
     public RampObject translate( double dx, double dy ) {
-        return new RampObject( mass,
-                               new ImmutableVector2D.Double( position.getX() + dx, position.getY() + dy ),
-                               new ImmutableVector2D.Double( v.getX(), v.getY() ),
-                               new ImmutableVector2D.Double( a.getX(), a.getY() ),
-                               staticFrictionCoefficient, kineticFrictionCoefficient, angle );
+        return setPosition( position.getX() + dx, position.getY() + dy );
     }
 
     public ImmutableVector2D.Double getPosition() {
         return position;
+    }
+
+    public RampObject setPosition( double x, double y ) {
+        return new RampObject( mass,
+                               new ImmutableVector2D.Double( x, y ),
+                               new ImmutableVector2D.Double( v.getX(), v.getY() ),
+                               new ImmutableVector2D.Double( a.getX(), a.getY() ),
+                               staticFrictionCoefficient, kineticFrictionCoefficient, angle, interacting );
+    }
+
+    public String toString() {
+        return "RampObject{" +
+               "mass=" + mass +
+               ", position=" + position +
+               ", v=" + v +
+               ", a=" + a +
+               ", staticFrictionCoefficient=" + staticFrictionCoefficient +
+               ", kineticFrictionCoefficient=" + kineticFrictionCoefficient +
+               ", angle=" + angle +
+               '}';
+    }
+
+    public RampObject setInteracting( boolean interacting ) {
+        return new RampObject( mass,
+                               new ImmutableVector2D.Double( position.getX(), position.getY() ),
+                               new ImmutableVector2D.Double( v.getX(), v.getY() ),
+                               new ImmutableVector2D.Double( a.getX(), a.getY() ),
+                               staticFrictionCoefficient, kineticFrictionCoefficient, angle, interacting );
+    }
+
+    public boolean isInteracting() {
+        return interacting;
     }
 }
