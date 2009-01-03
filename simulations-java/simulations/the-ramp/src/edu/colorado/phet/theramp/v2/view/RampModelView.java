a3de1d0ff872f4089ca92edac42ca7dd44ef1c40
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-01-03 18:32:24 +0000

    added test for interaction model

--- simulations-java/simulations/the-ramp/src/edu/colorado/phet/theramp/v2/view/RampModelView.java
+++ simulations-java/simulations/the-ramp/src/edu/colorado/phet/theramp/v2/view/RampModelView.java
@@ -1,10 +1,14 @@
 package edu.colorado.phet.theramp.v2.view;
 
+import java.awt.*;
 import java.awt.geom.Rectangle2D;
 
+import edu.colorado.phet.common.piccolophet.event.CursorHandler;
 import edu.colorado.phet.theramp.v2.model.RampModel;
 import edu.colorado.phet.theramp.v2.model.RampObject;
 import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
+import edu.umd.cs.piccolo.event.PInputEvent;
 import edu.umd.cs.piccolo.nodes.PPath;
 
 public class RampModelView extends PNode {
@@ -41,7 +45,6 @@ public class RampModelView extends PNode {
     }
 
     private void updateOldNodes() {
-
         if ( getChildrenCount() == 0 ) {
             updateNewNodes();
         }
@@ -50,9 +53,35 @@ public class RampModelView extends PNode {
         }
     }
 
-    private PNode createNode( RampObject object ) {
-        Rectangle2D.Double aDouble = new Rectangle2D.Double( object.getPosition().getX(), object.getPosition().getY(), 10, 10 );
+    private PNode createNode( final RampObject object ) {
+        Rectangle2D.Double aDouble = new Rectangle2D.Double( object.getPosition().getX(), object.getPosition().getY(), 20, 20 );
 //        System.out.println( "aDouble = " + aDouble );
-        return new PPath( aDouble );
+        final PPath pPath = new PPath( aDouble );
+        pPath.setPaint( Color.blue );
+        PBasicInputEventHandler listener = new PBasicInputEventHandler() {
+            public void mousePressed( PInputEvent event ) {
+                super.mousePressed( event );
+                module.updateCurrentState( object, object.setInteracting( true ) );
+//                pPath.removeInputEventListener( this );
+            }
+
+            public void mouseDragged( PInputEvent event ) {
+                super.mouseDragged( event );
+                RampObject newObject = object.setPosition( event.getCanvasPosition().getX(), event.getCanvasPosition().getY() );
+                module.updateCurrentState( object, newObject );
+//                pPath.removeInputEventListener( this );
+            }
+
+            public void mouseReleased( PInputEvent event ) {
+                super.mouseReleased( event );
+                module.updateCurrentState( object, object.setInteracting( false ) );
+//                pPath.removeInputEventListener( this );
+            }
+        };
+        //with immutable model object and new scene graph constructed at each instance,
+        //need to handle transfer of mouse handling state
+        pPath.addInputEventListener( listener );
+        pPath.addInputEventListener( new CursorHandler() );
+        return pPath;
     }
 }
