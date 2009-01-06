04fd596412d0dd2b6eb3c773b9b9f98cd6e6a75a
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-01-06 14:57:00 +0000

    added input handlers

--- team/reids/admin/scala-src/testing/RampObjectNode.scala
+++ team/reids/admin/scala-src/testing/RampObjectNode.scala
@@ -1,5 +1,7 @@
 package testing
 
+import edu.colorado.phet.common.piccolophet.event.CursorHandler
+import edu.umd.cs.piccolo.event.{PBasicInputEventHandler, PInputEvent}
 import edu.umd.cs.piccolo.nodes.PPath
 import edu.umd.cs.piccolo.PNode
 import java.awt.Color
@@ -11,4 +13,9 @@ class RampObjectNode(rampObject: RampObject) extends PNode {
 
   rampObject.addListener((r: RampObject) => x.setOffset(r.getPosition.x, r.getPosition.y))
   addChild(x)
+
+  addInputEventListener(new CursorHandler)
+  addInputEventListener(new PBasicInputEventHandler() {
+    override def mouseDragged(event: PInputEvent) = rampObject.translate(event.getCanvasDelta.width, event.getCanvasDelta.height)
+  })
 }
