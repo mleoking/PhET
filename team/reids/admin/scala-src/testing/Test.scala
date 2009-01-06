a53eed38768f92c2b0178e921dd427254122b17c
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-01-06 03:38:58 +0000

    cleanup adapters

--- team/reids/admin/scala-src/testing/Test.scala
+++ team/reids/admin/scala-src/testing/Test.scala
@@ -14,6 +14,8 @@ import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock
 import edu.colorado.phet.common.piccolophet.PhetPCanvas
 import edu.umd.cs.piccolo.nodes.PText
 import edu.umd.cs.piccolo.PNode
+import java.awt.Color
+import java.awt.Font
 import javax.swing.JLabel
 
 object MyTest {
@@ -21,7 +23,7 @@ object MyTest {
     println("started")
     class ScalaModelElement(element: Double => Unit) extends ClockAdapter {
       override def simulationTimeChanged(clockEvent: ClockEvent) = {
-        element(clockEvent.getSimulationTimeChange);
+        element(clockEvent.getSimulationTimeChange)
       }
     }
     class ScalaModule extends Module("my module", new ConstantDtClock(30, 1)) {
@@ -29,21 +31,21 @@ object MyTest {
       setSimulationPanel(canvas)
 
       val ptext = new PText("hello")
+      ptext setFont new Font("Lucida Sans",Font.BOLD,30)
       ptext.setOffset(300, 200)
       canvas addScreenChild ptext
-      getClock.addClockListener(new ScalaModelElement((dt: Double) => ptext.translate(1, 0)));
-      getClock.addClockListener(new ScalaModelElement((dt: Double) => ptext.translate(0, 2)));
+      canvas setBackground new Color(200,255,240)
+      getClock.addClockListener(new ScalaModelElement((dt: Double) => ptext.translate(1, 0)))
+      getClock.addClockListener(new ScalaModelElement((dt: Double) => ptext.translate(0, 2)))
     }
 
-    val config = new PhetApplicationConfig(args, "moving-man")
-    object ScalaApp extends PhetApplication(config) {
-      addModule(new ScalaModule)
-    }
-    object ScalaAppConstructor extends ApplicationConstructor {
-      override def getApplication(a: PhetApplicationConfig): PhetApplication = ScalaApp
-    }
-
-    new PhetApplicationLauncher().launchSim(config, ScalaAppConstructor)
+    new PhetApplicationLauncher().launchSim(
+      new PhetApplicationConfig(args, "moving-man"),
+      new ApplicationConstructor() {
+        override def getApplication(a: PhetApplicationConfig): PhetApplication = new PhetApplication(a) {
+          addModule(new ScalaModule)
+        }
+      })
     println("finished")
   }
 }
