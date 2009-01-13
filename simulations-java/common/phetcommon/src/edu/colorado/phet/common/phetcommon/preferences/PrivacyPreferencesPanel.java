c144fc8f91fde2e4f322828f45d9c33ca2b8e6ff
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-01-13 19:49:08 +0000

    #1073, replace "Details" button in Privacy Preferences with a hyperlink

--- simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/preferences/PrivacyPreferencesPanel.java
+++ simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/preferences/PrivacyPreferencesPanel.java
@@ -1,14 +1,19 @@
 package edu.colorado.phet.common.phetcommon.preferences;
 
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.Dialog;
+import java.awt.Frame;
+import java.awt.Insets;
+import java.awt.Window;
 
 import javax.swing.*;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
+import javax.swing.text.html.HTMLEditorKit;
 
 import edu.colorado.phet.common.phetcommon.resources.PhetCommonResources;
 import edu.colorado.phet.common.phetcommon.tracking.ITrackingInfo;
 import edu.colorado.phet.common.phetcommon.view.util.EasyGridBagLayout;
+import edu.colorado.phet.common.phetcommon.view.util.HTMLUtils;
 
 /**
  * Panel for displaying preferences related to privacy.
@@ -17,56 +22,50 @@ public class PrivacyPreferencesPanel extends JPanel {
 
     private static final String DESCRIPTION_STRING = PhetCommonResources.getString( "Common.tracking.description" );
     private static final String TRACKING_ENABLED = PhetCommonResources.getString( "Common.tracking.enabled" );
-    private static final String DETAILS_BUTTON = PhetCommonResources.getString( "Common.tracking.details.button" );
     
     private final JCheckBox trackingEnabledCheckBox;
     
     public PrivacyPreferencesPanel( ITrackingInfo trackingInfo, boolean trackingEnabled ) {
         
         // feature description
-        JComponent oneLiner = createDescription();
+        String html = HTMLUtils.createStyledHTMLFromFragment( DESCRIPTION_STRING );
+        JComponent description = new DescriptionPane( html, trackingInfo );
         
         // enable check box
         trackingEnabledCheckBox = new JCheckBox( TRACKING_ENABLED, trackingEnabled );
         
-        // details button
-        DetailsButton detailsButton = new DetailsButton( trackingInfo );
-
         // layout
         EasyGridBagLayout layout = new EasyGridBagLayout( this );
         this.setLayout( layout );
         layout.setInsets( new Insets( 5, 5, 5, 5 ) );
         int row = 0;
         int column = 0;
-        layout.addAnchoredComponent( oneLiner, row++, column, GridBagConstraints.WEST);
-        layout.addAnchoredComponent( trackingEnabledCheckBox, row++, column, GridBagConstraints.WEST );
-        layout.addAnchoredComponent( detailsButton, row++, column, GridBagConstraints.EAST );
+        layout.addComponent( description, row++, column );
+        layout.addComponent( trackingEnabledCheckBox, row++, column );
     }
     
     public boolean isTrackingEnabled() {
         return trackingEnabledCheckBox.isSelected();
     }
 
-    /* One-line description of tracking */
-    private static JComponent createDescription() {
-        return new JLabel( DESCRIPTION_STRING );
-    }
-
-    /*
-     * Details button opens a dialog that allows the user to view 
-     * the tracking info that is sent to PhET.
-     */
-    private static class DetailsButton extends JButton {
-        private DetailsButton( final ITrackingInfo trackingInfo ) {
-            super( DETAILS_BUTTON );
-            addActionListener( new ActionListener() {
-                public void actionPerformed( ActionEvent e ) {
-                    Window window = SwingUtilities.getWindowAncestor( DetailsButton.this );
-                    if ( window instanceof Frame ) {
-                        new TrackingDetailsDialog( (Frame) window, trackingInfo ).setVisible( true );
-                    }
-                    else if ( window instanceof Dialog ) {
-                        new TrackingDetailsDialog( (Dialog) window, trackingInfo ).setVisible( true );
+    private static class DescriptionPane extends JEditorPane {
+        public DescriptionPane( String html, final ITrackingInfo trackingInfo ) {
+            setEditorKit( new HTMLEditorKit() );
+            setText( html );
+            setEditable( false );
+            setBackground( new JPanel().getBackground() );
+            addHyperlinkListener( new HyperlinkListener() {
+                public void hyperlinkUpdate( HyperlinkEvent e ) {
+                    if ( e.getEventType() == HyperlinkEvent.EventType.ACTIVATED ) {
+                        // This assumes there's only one hyperlink in the description text.
+                        // If more links are added, you'll need to check the link in the event.
+                        Window window = SwingUtilities.getWindowAncestor( DescriptionPane.this );
+                        if ( window instanceof Frame ) {
+                            new TrackingDetailsDialog( (Frame) window, trackingInfo ).setVisible( true );
+                        }
+                        else if ( window instanceof Dialog ) {
+                            new TrackingDetailsDialog( (Dialog) window, trackingInfo ).setVisible( true );
+                        }
                     }
                 }
             } );
