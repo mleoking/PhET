152ef89e2fa82d575f1a2b1bda5f0f3ee526240c
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-01-14 01:07:58 +0000

    see #1095, use MessageFormat to insert hyperlink descriptions (href) so that translators can't mess these up and break links.

--- simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/preferences/PrivacyPreferencesPanel.java
+++ simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/preferences/PrivacyPreferencesPanel.java
@@ -4,6 +4,7 @@ import java.awt.Dialog;
 import java.awt.Frame;
 import java.awt.Insets;
 import java.awt.Window;
+import java.text.MessageFormat;
 
 import javax.swing.JCheckBox;
 import javax.swing.JComponent;
@@ -23,7 +24,6 @@ import edu.colorado.phet.common.phetcommon.view.util.HTMLUtils.HTMLEditorPane;
  */
 public class PrivacyPreferencesPanel extends JPanel {
 
-    private static final String DESCRIPTION_STRING = PhetCommonResources.getString( "Common.tracking.description" );
     private static final String TRACKING_ENABLED = PhetCommonResources.getString( "Common.tracking.enabled" );
     
     private final JCheckBox trackingEnabledCheckBox;
@@ -31,8 +31,7 @@ public class PrivacyPreferencesPanel extends JPanel {
     public PrivacyPreferencesPanel( ITrackingInfo trackingInfo, boolean trackingEnabled ) {
         
         // feature description
-        String html = HTMLUtils.createStyledHTMLFromFragment( DESCRIPTION_STRING );
-        JComponent description = new DescriptionPane( html, trackingInfo );
+        JComponent description = new DescriptionPane( trackingInfo );
         
         // enable check box
         trackingEnabledCheckBox = new JCheckBox( TRACKING_ENABLED, trackingEnabled );
@@ -56,28 +55,41 @@ public class PrivacyPreferencesPanel extends JPanel {
      * But instead of opening a web browser, it opens a Swing dialog.
      */
     private static class DescriptionPane extends HTMLEditorPane {
-        public DescriptionPane( String html, final ITrackingInfo trackingInfo ) {
-            super( html );
+        
+        private static final String DESCRIPTION_PATTERN = PhetCommonResources.getString( "Common.tracking.description" );
+
+        // identifiers for hyperlink actions
+        private static final String LINK_SHOW_TRACKING_DETAILS = "showTrackingDetails";
+
+        public DescriptionPane( final ITrackingInfo trackingInfo ) {
+            super( "" );
+            
+            // insert our own hyperlink descriptions into the message, so translators can't mess them up
+            Object[] args = { LINK_SHOW_TRACKING_DETAILS };
+            String htmlFragment = MessageFormat.format( DESCRIPTION_PATTERN, args );
+            setText( HTMLUtils.createStyledHTMLFromFragment( htmlFragment ) );
+            
             addHyperlinkListener( new HyperlinkListener() {
                 public void hyperlinkUpdate( HyperlinkEvent e ) {
-                    /*
-                     * This assumes there's only one hyperlink in the description text,
-                     * and that hyperlink opens a dialog that shows tracking details.
-                     * If more links are added, you'll need to check the link in the event,
-                     * and do something specific based on which link it is.
-                     * But beware! Translators might change the href in the translated text.
-                     */
                     if ( e.getEventType() == HyperlinkEvent.EventType.ACTIVATED ) {
-                        Window window = SwingUtilities.getWindowAncestor( DescriptionPane.this );
-                        if ( window instanceof Frame ) {
-                            new TrackingDetailsDialog( (Frame) window, trackingInfo ).setVisible( true );
+                        if ( e.getDescription().equals( LINK_SHOW_TRACKING_DETAILS ) ) {
+                            showTrackingDetails( SwingUtilities.getWindowAncestor( DescriptionPane.this ), trackingInfo );
                         }
-                        else if ( window instanceof Dialog ) {
-                            new TrackingDetailsDialog( (Dialog) window, trackingInfo ).setVisible( true );
+                        else {
+                            System.err.println( "PrivacyPreferencesPanel.DescriptionPane.hyperlinkUpdate: unsupported hyperlink, description=" + e.getDescription() );
                         }
                     }
                 }
             } );
+        }
+        
+        private static void showTrackingDetails( Window owner, ITrackingInfo trackingInfo ) {
+            if ( owner instanceof Frame ) {
+                new TrackingDetailsDialog( (Frame) owner, trackingInfo ).setVisible( true );
+            }
+            else if ( owner instanceof Dialog ) {
+                new TrackingDetailsDialog( (Dialog) owner, trackingInfo ).setVisible( true );
+            }
         }
     }
 }
