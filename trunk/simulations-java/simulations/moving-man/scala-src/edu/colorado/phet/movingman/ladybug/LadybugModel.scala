81213c35a57989f2a16f67cf102d4d55bf07b9ef
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-01-14 04:47:15 +0000

    add smoothing

--- trunk/simulations-java/simulations/moving-man/scala-src/edu/colorado/phet/movingman/ladybug/LadybugModel.scala
+++ trunk/simulations-java/simulations/moving-man/scala-src/edu/colorado/phet/movingman/ladybug/LadybugModel.scala
@@ -11,37 +11,36 @@ class LadybugModel {
     time += dt;
     history += (time, ladybug.getState)
 
-    if (estimateVelocity.magnitude > 1E-6)
-      ladybug.setAngle(estimateAngle)
+    if (history.length > 5) {
+      if (estimateVelocity.magnitude > 1E-6)
+        ladybug.setAngle(estimateAngle())
 
-    ladybug.setVelocity(estimateVelocity)
+      ladybug.setVelocity(estimateVelocity)
+    }
   }
 
-  def estimateAngle: Double = estimateVelocity.getAngle
+  def estimateAngle(): Double = estimateVelocity.getAngle
 
-  def estimateVelocity: Vector2D = {
-    val dtList = for (elm <- history) yield elm._1;
-    val pos = for (elm <- history) yield elm._2.position
-
-    val posX = for (p <- pos) yield p.x
-    val posY = for (p <- pos) yield p.y
-
-    val size = posX.size
-    if (size > 2) {
-      val x2 = posX(size - 1)
-      val x1 = posX(size - 2)
-
-      val y2 = posY(size - 1)
-      val y1 = posY(size - 2)
-
-      val t2 = dtList(size - 1)
-      val t1 = dtList(size - 2)
-
-      val derX = (x2 - x1) / (t2 - t1)
-      val derY = (y2 - y1) / (t2 - t1)
-      new Vector2D(derX, derY)
-
-    } else
-      new Vector2D
+  def estimateVelocity(): Vector2D = {
+    val v1 = estimateVelocity(history.length - 1);
+    val v2 = estimateVelocity(history.length - 2);
+    val v3 = estimateVelocity(history.length - 3);
+    return (v1 + v2 + v3) / 3;
   }
+
+  def estimateVelocity(index: Int): Vector2D = {
+    val x2 = history(index)._2.position.x
+    val x1 = history(index - 1)._2.position.x
+
+    val y2 = history(index)._2.position.y
+    val y1 = history(index - 1)._2.position.y
+
+    val t2 = history(index)._1
+    val t1 = history(index - 1)._1
+
+    val derX = (x2 - x1) / (t2 - t1)
+    val derY = (y2 - y1) / (t2 - t1)
+    new Vector2D(derX, derY)
+  }
+
 }
