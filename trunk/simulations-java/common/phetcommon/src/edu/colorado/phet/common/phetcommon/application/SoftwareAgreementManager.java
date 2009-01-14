9c991ceb5cca2450c39ca21c87a005c17fed49d6
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-01-14 00:57:51 +0000

    see #1095, make hyperlinks work in Software & Privacy Agreements dialog

--- trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/application/SoftwareAgreementManager.java
+++ trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/application/SoftwareAgreementManager.java
@@ -1,22 +1,26 @@
 
 package edu.colorado.phet.common.phetcommon.application;
 
-import java.awt.FlowLayout;
-import java.awt.GridBagConstraints;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.text.MessageFormat;
 
 import javax.swing.*;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
 
 import edu.colorado.phet.common.phetcommon.preferences.PhetPreferences;
+import edu.colorado.phet.common.phetcommon.preferences.TrackingDetailsDialog;
 import edu.colorado.phet.common.phetcommon.resources.PhetCommonResources;
+import edu.colorado.phet.common.phetcommon.tracking.ITrackingInfo;
 import edu.colorado.phet.common.phetcommon.view.PhetExit;
 import edu.colorado.phet.common.phetcommon.view.util.EasyGridBagLayout;
 import edu.colorado.phet.common.phetcommon.view.util.HTMLUtils;
 import edu.colorado.phet.common.phetcommon.view.util.SwingUtils;
-import edu.colorado.phet.common.phetcommon.view.util.HTMLUtils.InteractiveHTMLPane;
+import edu.colorado.phet.common.phetcommon.view.util.HTMLUtils.HTMLEditorPane;
 
 /**
  * Manages policy related to PhET's software and privacy agreements.
@@ -31,18 +35,18 @@ public class SoftwareAgreementManager {
     /**
      * Ensures that the user has accepted the agreements that pertain to this software.
      */
-    public static void validate() {
+    public static void validate( ITrackingInfo trackingInfo ) {
         int acceptedVersion = PhetPreferences.getInstance().getSoftwareAgreementVersion();
         if ( acceptedVersion < SOFTWARE_AGREEMENT_VERSION ) {
-            negotiate();
+            negotiate( trackingInfo );
         }
     }
     
     /*
      * Negotiates the agreement with the user.
      */
-    private static void negotiate() {
-        new SoftwareAgreementDialog().setVisible( true );
+    private static void negotiate( ITrackingInfo trackingInfo ) {
+        new SoftwareAgreementDialog( trackingInfo ).setVisible( true );
     }
     
     /*
@@ -53,15 +57,14 @@ public class SoftwareAgreementManager {
         private static final String TITLE = PhetCommonResources.getString( "Common.softwareAgreement.title" );
         private static final String ACCEPT_BUTTON = PhetCommonResources.getString( "Common.softwareAgreement.accept" );
         private static final String CANCEL_BUTTON = PhetCommonResources.getString( "Common.softwareAgreement.cancel" );
-        private static final String MESSAGE = PhetCommonResources.getString( "Common.softwareAgreement.message" );
         
-        public SoftwareAgreementDialog() {
+        public SoftwareAgreementDialog( ITrackingInfo trackingInfo ) {
             super();
             setTitle( TITLE );
             setModal( true );
             setResizable( false );
 
-            JComponent message = createMessagePanel();
+            JComponent message = createMessagePanel( trackingInfo );
             JComponent buttonPanel = createButtonPanel();
 
             JPanel panel = new JPanel();
@@ -86,9 +89,8 @@ public class SoftwareAgreementManager {
             SwingUtils.centerWindowOnScreen( this );
         }
 
-        private JComponent createMessagePanel() {
-            String html = HTMLUtils.createStyledHTMLFromFragment( MESSAGE );
-            JComponent htmlPane = new InteractiveHTMLPane( html );
+        private JComponent createMessagePanel( ITrackingInfo trackingInfo ) {
+            JComponent htmlPane = new MessagePane( trackingInfo );
             JPanel panel = new JPanel();
             panel.setBorder( BorderFactory.createEmptyBorder( 5, 5, 5, 5 ) );
             panel.add( htmlPane );
@@ -133,9 +135,62 @@ public class SoftwareAgreementManager {
             PhetExit.exit();
         }
     }
+    
+    /*
+     * This is an HTML editor pane interactive hyperlinks.
+     * But instead of opening a web browser, it opens a Swing dialog.
+     */
+    private static class MessagePane extends HTMLEditorPane {
+       
+        private static final String MESSAGE_PATTERN = PhetCommonResources.getString( "Common.softwareAgreement.message" );
+        private static final String INFORMATION_LINK = "information";
+        private static final String AGREEMENTS_LINK = "agreements";
+        
+        public MessagePane( final ITrackingInfo trackingInfo ) {
+            super( "" );
+            
+            // insert our own hyperlink descriptions into the message
+            Object[] args = { INFORMATION_LINK, AGREEMENTS_LINK };
+            String htmlFragment = MessageFormat.format( MESSAGE_PATTERN, args );
+            System.out.println( "htmlFragment" + htmlFragment );//XXX
+            setText( HTMLUtils.createStyledHTMLFromFragment( htmlFragment ) );
+            
+            addHyperlinkListener( new HyperlinkListener() {
+                public void hyperlinkUpdate( HyperlinkEvent e ) {
+                    if ( e.getEventType() == HyperlinkEvent.EventType.ACTIVATED ) {
+                        Window owner = SwingUtilities.getWindowAncestor( MessagePane.this );
+                        if ( e.getDescription().equals( INFORMATION_LINK ) ) {
+                            showTrackingDetails( owner, trackingInfo );
+                        }
+                        else if ( e.getDescription().equals( AGREEMENTS_LINK ) ) {
+                            showAgreements( owner );
+                        }
+                        else {
+                            System.err.println( "SoftwareAgreementManager.MessagePane.hyperlinkUpdate: unsupported hyperlink, description=" + e.getDescription() );
+                        }
+                    }
+                }
+            } );
+        }
+        
+        private static void showTrackingDetails( Window owner, ITrackingInfo trackingInfo ) {
+            if ( owner instanceof Frame ) {
+                new TrackingDetailsDialog( (Frame) owner, trackingInfo ).setVisible( true );
+            }
+            else if ( owner instanceof Dialog ) {
+                new TrackingDetailsDialog( (Dialog) owner, trackingInfo ).setVisible( true );
+            }
+        }
+        
+        private static void showAgreements( Window owner ) {
+            //TODO: read agreements, display in a dialog with a scrollpane and Close button
+            JOptionPane.showMessageDialog( owner, "agreements go here", "Software & Privacy Agreements", JOptionPane.PLAIN_MESSAGE );
+        }
+    }
 
     public static void main( String[] args ) {
-        new SoftwareAgreementDialog().setVisible( true );
+        PhetApplicationConfig config = new PhetApplicationConfig( args, "balloons" );
+        new SoftwareAgreementDialog( config ).setVisible( true );
         System.out.println( "continuing" );
     }
 }
