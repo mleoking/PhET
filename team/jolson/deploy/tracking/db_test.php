0a3a9d853eeebf7ae8e53083c2261b9b75c886cd
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-01-16 23:33:20 +0000

    Tracking Database: Created Flash section of random message generation

--- team/jolson/deploy/tracking/db_test.php
+++ team/jolson/deploy/tracking/db_test.php
@@ -1,4 +1,5 @@
 <?php
+/*
 	include("db_util.php");
 	$link = setup_mysql();
 	
@@ -21,49 +22,7 @@
 		. $session_id . ", 'Windows Vista', '6.0', 'x86', 'Sun Microsystems Inc.', 1, 6, 0, NULL, 'America/Denver');";
 	mysql_query($query2);
 	echo mysql_errno($link) . ": " . mysql_error($link). "\n";
-	
-?>
-<p>
-This is a test of your local emergency database system.
-		
-		host_java_os_name=Windows Vista
-		host_java_os_version=6.0
-		host_java_os_arch=x86
-		host_java_vendor=Sun Microsystems Inc.
-		host_java_version_major=1
-		host_java_version_minor=6
-		host_java_version_maintenance=0
-		host_java_webstart_version=null
-		host_java_timezone=America/Denver
+*/
 
-	message_type=session
-	message_version=1
-	sim_type=java
-	sim_project=balloons
-	sim_name=balloons
-	sim_sessions=14
-	sim_major_version=1
-	sim_minor_version=07
-	sim_dev_version=00
-	sim_svn_revision=26532
-	sim_distribution_id=general
-	sim_locale_language=en
-	sim_locale_country=US
-	sim_scenario=standalone-jar
-	sim_dev=true
-	host_os_name=Windows Vista
-	host_os_version=6.0
-	host_os_arch=x86
-	host_java_vendor=Sun Microsystems Inc.
-	host_java_version_major=1
-	host_java_version_minor=6
-	host_java_version_maintenance=0
-	host_java_webstart_version=null
-	host_locale_language=en
-	host_locale_country=US
-	host_timezone=America/Denver
-	user_preference_file_creation_time=1229711605402
-	user_total_sessions=394
-	debug_sim_version=1.07.00 (26532)
-	debug_host_java_version=1.6.0_10-rc2-b31
-</p>
+echo date("YmdHis", time());
+?>
