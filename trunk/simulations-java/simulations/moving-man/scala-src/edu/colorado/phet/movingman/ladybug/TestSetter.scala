3d6878187b6afab3b7d663b89ba3f7dd0e3c6933
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-01-16 19:52:59 +0000

    added line count utility

--- trunk/simulations-java/simulations/moving-man/scala-src/edu/colorado/phet/movingman/ladybug/TestSetter.scala
+++ trunk/simulations-java/simulations/moving-man/scala-src/edu/colorado/phet/movingman/ladybug/TestSetter.scala
@@ -1,13 +1,34 @@
 package edu.colorado.phet.movingman.ladybug
 
+import _root_.scala.io.Source
+import java.io.File
+
 object TestSetter {
   def main(args: Array[String]) = {
-    class Time{
-      var h=12
-      def hour:Int=h
-      def hour_=(x:Int)={h=x}
+
+    class Time {
+      var h = 12
+
+      def hour: Int = h
+
+      def hour_=(x: Int) = {h = x}
     }
-    val t=new Time
+    val t = new Time
     t.hour = 123
+
+    val file = new File("C:/workingcopy/phet/svn/trunk/simulations-java/simulations/moving-man/scala-src/edu/colorado/phet/movingman/ladybug");
+    val children = file.listFiles();
+
+    var count = 0;
+    children.foreach((f: File) => {
+      if (f.isFile) {
+        val s: Source = Source.fromFile(f)
+        val c=s.getLines.toList.length
+        println(f.getAbsolutePath + ": " + c)
+        count = count + c
+//        print("running count="+count)
+      }
+    })
+    println("Total count: " + count);
   }
 }
