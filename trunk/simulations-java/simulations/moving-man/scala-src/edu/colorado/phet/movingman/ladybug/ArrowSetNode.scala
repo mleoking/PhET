a0527ba613acf962262e42e26a8b9eaf37756821
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-01-16 14:54:36 +0000

    add labels to arrows

--- trunk/simulations-java/simulations/moving-man/scala-src/edu/colorado/phet/movingman/ladybug/ArrowSetNode.scala
+++ trunk/simulations-java/simulations/moving-man/scala-src/edu/colorado/phet/movingman/ladybug/ArrowSetNode.scala
@@ -1,23 +1,37 @@
 package edu.colorado.phet.movingman.ladybug
 
+import _root_.edu.colorado.phet.common.phetcommon.view.util.PhetFont
+import _root_.edu.colorado.phet.common.piccolophet.nodes.ArrowNode
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform2D
 import edu.colorado.phet.common.piccolophet.nodes.ArrowNode
 import java.awt.geom.Point2D
 import java.awt.{BasicStroke, Color}
+import umd.cs.piccolo.nodes.PText
 import umd.cs.piccolo.PNode
 import LadybugUtil._
 
 class ArrowSetNode(ladybug: Ladybug, transform: ModelViewTransform2D) extends PNode {
-  def createNode(color: Color) = {
-    val velocityNode = new ArrowNode(new Point2D.Double(0, 0), new Point2D.Double(200, 200), 90, 90, 60, 2, true)
-    velocityNode setPaint color
-    velocityNode setStroke new BasicStroke(0.5f)
-    velocityNode setStrokePaint Color.gray
-    velocityNode
+  class LabeledArrowNode(color: Color, name: String) extends PNode {
+    val arrowNode = new ArrowNode(new Point2D.Double(0, 0), new Point2D.Double(200, 200), 90, 90, 60, 2, true)
+    arrowNode setPaint color
+    arrowNode setStroke new BasicStroke(0.5f)
+    arrowNode setStrokePaint Color.gray
+
+    val labelNode = new PText(name)
+    labelNode.setFont(new PhetFont(24))
+
+    addChild(arrowNode)
+    addChild(labelNode)
+
+    def setTipAndTailLocations(a: Vector2D, b: Vector2D) = {
+      arrowNode.setTipAndTailLocations(a, b)
+      labelNode.setOffset(a)
+      labelNode.setVisible(labelNode.getFullBounds.width < (a - b).magnitude)
+    }
   }
 
-  val velocityNode = createNode(LadybugColorSet.velocity)
-  val accelNode = createNode(LadybugColorSet.acceleration)
+  val velocityNode = new LabeledArrowNode(LadybugColorSet.velocity, "Velocity")
+  val accelNode = new LabeledArrowNode(LadybugColorSet.acceleration, "Acceleration")
   addChild(velocityNode)
   addChild(accelNode)
 
