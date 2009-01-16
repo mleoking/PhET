7e20dd3cb4b11ca0309a1650a2355453fc5bd215
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-01-16 05:44:13 +0000

    create stubs for control panel

--- trunk/simulations-java/simulations/moving-man/scala-src/edu/colorado/phet/movingman/ladybug/Ladybug2DApplication.scala
+++ trunk/simulations-java/simulations/moving-man/scala-src/edu/colorado/phet/movingman/ladybug/Ladybug2DApplication.scala
@@ -22,32 +22,16 @@ object Ladybug2DApplication {
       element(clockEvent.getSimulationTimeChange)
     }
   }
-  class ScalaClock(delay: Int, dt: Double) extends ConstantDtClock(delay, dt) {
-    def addClockListener(exp: Double => Unit) {
-      super.addClockListener(new ClockAdapter() {
-        override def simulationTimeChanged(clockEvent: ClockEvent) = exp(clockEvent.getSimulationTimeChange)
-      })
-    }
-  }
+
   def main(args: Array[String]) = {
     println("started")
 
     val clock = new ScalaClock(30, 1)
-    class ScalaModule extends Module("my module", clock) {
-      val model = new LadybugModel
-      val canvas = new LadybugCanvas
-      setSimulationPanel(canvas)
-
-      canvas setBackground new Color(200, 255, 240)
-      clock.addClockListener(model.update(_))
-      canvas.addNode(new LadybugNode(model.ladybug, canvas.transform))
-    }
-
     new PhetApplicationLauncher().launchSim(
       new PhetApplicationConfig(args, "moving-man", "ladybug-2d"),
       new ApplicationConstructor() {
         override def getApplication(a: PhetApplicationConfig): PhetApplication = new PhetApplication(a) {
-          addModule(new ScalaModule)
+          addModule(new LadybugModule(clock))
         }
       })
     println("finished")
