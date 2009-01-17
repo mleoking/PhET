913180afbd80c6e64d99bebc1b62f5c3f7376f52
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-01-17 05:46:40 +0000

    added timeline

--- simulations-java/simulations/moving-man/scala-src/edu/colorado/phet/movingman/ladybug/RecordingControl.scala
+++ simulations-java/simulations/moving-man/scala-src/edu/colorado/phet/movingman/ladybug/RecordingControl.scala
@@ -1,13 +1,15 @@
 package edu.colorado.phet.movingman.ladybug
 
 import _root_.edu.colorado.phet.common.phetcommon.view.util.PhetFont
+import _root_.edu.colorado.phet.common.piccolophet.nodes.PhetPPath
 import edu.colorado.phet.common.piccolophet.PhetPCanvas
-import java.awt.Dimension
+import java.awt.geom.Ellipse2D
+import java.awt.{Rectangle, Dimension, BasicStroke, Color}
 import java.text.DecimalFormat
 import javax.swing.JPanel
-import umd.cs.piccolo.nodes.PText
 import LadybugUtil._
-import java.awt.Color
+import umd.cs.piccolo.nodes.{PPath, PText}
+import umd.cs.piccolo.PNode
 
 class RecordingControl(model: LadybugModel) extends PhetPCanvas {
   val text = new PText("0.00")
@@ -32,6 +34,9 @@ class RecordingControl(model: LadybugModel) extends PhetPCanvas {
   playbackIndicator.setOffset(_width - playbackIndicator.getFullBounds.getWidth - 2, _height - playbackIndicator.getFullBounds.getHeight)
   recordIndicator.setOffset(playbackIndicator.getFullBounds.getX, playbackIndicator.getFullBounds.getY - recordIndicator.getFullBounds.getHeight)
 
+  val timeline = new Timeline()
+  addScreenChild(timeline)
+
   updateReadouts
   def updateReadouts = {
     text.setText(model.getTime())
@@ -39,5 +44,27 @@ class RecordingControl(model: LadybugModel) extends PhetPCanvas {
 
     playbackIndicator.setTextPaint(if (model.isPlayback) Color.red else Color.lightGray)
     recordIndicator.setTextPaint(if (model.isRecord) Color.red else Color.lightGray)
+  }
+
+  class Timeline extends PNode {
+    val pathOffsetY = 4
+    val pathHeight = 10
+    val ellipseWidth = 8
+    val ellipseHeight = 12
+    val background = new PhetPPath(new Rectangle(0, pathOffsetY, _width, pathHeight), Color.lightGray)
+    val shaded = new PhetPPath(Color.orange)
+    val handle = new PhetPPath(Color.blue, new BasicStroke(1), Color.black)
+    addChild(background)
+    addChild(shaded)
+    addChild(handle)
+    model.addListener((model: LadybugModel) => {
+      updateSelf()
+    })
+    updateSelf
+    def updateSelf() = {
+      shaded.setPathTo(new Rectangle(0, pathOffsetY, (model.history.length / 4.0).toInt, pathHeight))
+      handle.setVisible(model.isPlayback)
+      handle.setPathTo(new Ellipse2D.Double((model.getPlaybackIndex / 4.0).toInt - ellipseWidth / 2, pathOffsetY, ellipseWidth, ellipseHeight))
+    }
   }
 }
