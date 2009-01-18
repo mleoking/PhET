32d5b68959842636ba7309e6f05207457fc3c6e6
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-01-18 07:48:00 +0000

    Tracking Database normalized with views

--- trunk/team/jolson/deploy/tracking/db_util.php
+++ trunk/team/jolson/deploy/tracking/db_util.php
@@ -5,5 +5,8 @@
 		mysql_select_db("phet_stats_test_1") or die(mysql_error());
 		return $link;
 	}
+	function insert_session($simType, $simProject, $simName, $simMajorVersion, $simMinorVersion, $simDevVersion, $simSvnRevision, $simLocaleLanguage, $simLocaleCountry, $simSessionsSince, $simSessionsEver, $simUsageType, $simDistributionTag, $simDev, $simScenario, $hostLocaleLanguage, $hostLocaleCountry, $hostSimplifiedOS) {
+		
+	}
 ?>
 
