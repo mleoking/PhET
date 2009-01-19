48729466e7103dfaee487425cda29277e4569e07
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-01-19 22:25:46 +0000

    Removed some code that was initially added in order to better map the phase indicator when the adjustable attraction atom was selected.  Since we now just hide the diagram when this atom is selected, the code is no longer needed.

--- trunk/simulations-java/simulations/states-of-matter/src/edu/colorado/phet/statesofmatter/module/phasechanges/PhaseChangesControlPanel.java
+++ trunk/simulations-java/simulations/states-of-matter/src/edu/colorado/phet/statesofmatter/module/phasechanges/PhaseChangesControlPanel.java
@@ -286,14 +286,6 @@ public class PhaseChangesControlPanel extends ControlPanel {
             mappedTemperature = modelTemperature * SLOPE_IN_2ND_REGION + OFFSET_IN_2ND_REGION;    		
     	}
 
-    	if (m_model.getMoleculeType() == StatesOfMatterConstants.USER_DEFINED_MOLECULE){
-    		// If the molecule is the user-defined molecule, it means that
-    		// the epsilon is changeable, which means that the phase will be
-    		// different for the same temperature.  We account for that here
-    		// by adjusting the temperature based on the epsilon value.
-    		mappedTemperature = mappedTemperature / (m_model.getEpsilon() / (StatesOfMatterConstants.MAX_EPSILON / 2));
-    	}
-
     	return Math.min(mappedTemperature, 1);
     }
     
@@ -302,18 +294,11 @@ public class PhaseChangesControlPanel extends ControlPanel {
     private double mapModelTempAndPressureToPhaseDiagramPressure(double modelPressure, double modelTemperature){
     	double mappedTemperature = mapModelTemperatureToPhaseDiagramTemperature(modelTemperature);
     	double mappedPressure;
-    	double adjustmentFactor = 1;
     	
-    	// Set the adjustment factor based on whether the user-defined nucleus
-    	// is in use.
-    	if (m_model.getMoleculeType() == StatesOfMatterConstants.USER_DEFINED_MOLECULE){
-        	adjustmentFactor = StatesOfMatterConstants.MAX_EPSILON / (2 * m_model.getEpsilon());
-    	}
-    	
-    	if (modelTemperature * adjustmentFactor < TRIPLE_POINT_TEMPERATURE_IN_MODEL){
+    	if (modelTemperature  < TRIPLE_POINT_TEMPERATURE_IN_MODEL){
     		mappedPressure = 1.4 * (Math.pow(mappedTemperature, 2)) + PRESSURE_FACTOR * Math.pow(modelPressure, 2);
     	}
-    	else if (modelTemperature * adjustmentFactor < CRITICAL_POINT_TEMPERATURE_IN_MODEL){
+    	else if (modelTemperature  < CRITICAL_POINT_TEMPERATURE_IN_MODEL){
             mappedPressure = 0.19 + 1.2 * (Math.pow(mappedTemperature - TRIPLE_POINT_TEMPERATURE_ON_DIAGRAM, 2)) + 
             	PRESSURE_FACTOR * Math.pow(modelPressure, 2);    		
     	}
