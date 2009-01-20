76c74ba1b4457cad924e41aed27d78d71b064c0f
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-01-20 21:11:36 +0000

    > Changed velocity color to green > Changed acceleration color to pink

--- trunk/simulations-java/simulations/moving-man/scala-src/edu/colorado/phet/movingman/ladybug/LadybugColorSet.scala
+++ trunk/simulations-java/simulations/moving-man/scala-src/edu/colorado/phet/movingman/ladybug/LadybugColorSet.scala
@@ -3,7 +3,7 @@ package edu.colorado.phet.movingman.ladybug
 import java.awt.Color
 
 object LadybugColorSet {
-  val acceleration = new Color(250, 246, 75)
-  val velocity = new Color(69, 248, 207)
+  val acceleration = Color.pink
+  val velocity = Color.green
   val position = new Color(35, 248, 20)
 }
