ce3a789873dc23071806b7a839e287b60aaa1f92
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-01-20 20:33:55 +0000

    delete example MVC components

--- simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/module/solutions/SolutionsModel.java
+++ simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/module/solutions/SolutionsModel.java
@@ -3,7 +3,6 @@
 package edu.colorado.phet.acidbasesolutions.module.solutions;
 
 import edu.colorado.phet.acidbasesolutions.model.ABSClock;
-import edu.colorado.phet.acidbasesolutions.model.ExampleModelElement;
 import edu.colorado.phet.acidbasesolutions.model.Solution;
 import edu.colorado.phet.acidbasesolutions.module.ABSModel;
 
@@ -18,7 +17,6 @@ public class SolutionsModel extends ABSModel {
     // Instance data
     //----------------------------------------------------------------------------
     
-    private final ExampleModelElement _exampleModelElement;//XXX delete
     private final Solution _solution;
 
     //----------------------------------------------------------------------------
@@ -28,24 +26,12 @@ public class SolutionsModel extends ABSModel {
     public SolutionsModel( ABSClock clock ) {
         super( clock );
         
-        _exampleModelElement = new ExampleModelElement( 
-                SolutionsDefaults.EXAMPLE_MODEL_ELEMENT_WIDTH,
-                SolutionsDefaults.EXAMPLE_MODEL_ELEMENT_HEIGHT,
-                SolutionsDefaults.EXAMPLE_MODEL_ELEMENT_POSITION, 
-                SolutionsDefaults.EXAMPLE_MODEL_ELEMENT_ORIENTATION );
-        addClockListener( _exampleModelElement );
-        
         _solution = new Solution();
     }
     
     //----------------------------------------------------------------------------
     // Accessors
     //----------------------------------------------------------------------------
-    
-    //XXX delete
-    public ExampleModelElement getExampleModelElement() {
-        return _exampleModelElement;
-    }
     
     public Solution getSolution() {
         return _solution;
