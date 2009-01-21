f7da28e05c04d6b042a4c95bb43c318495fed7c3
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-01-21 20:10:15 +0000

    Made menus, menuitems, menucheckboxes all have the default system color instead the the PhetLookAndFeel background color.  Sam worked with me on this an approved.

--- trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/view/PhetLookAndFeel.java
+++ trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/view/PhetLookAndFeel.java
@@ -65,7 +65,8 @@ public class PhetLookAndFeel {
             "ScrollBar", "Viewport"
     };
 
-    private ArrayList ignoreBackgroundList=new ArrayList( Arrays.asList( new String[]{"TextArea","TextField","TextPane","FormattedTextField","MenuBar"} ));
+    private ArrayList ignoreBackgroundList=new ArrayList( Arrays.asList( new String[]{"TextArea","TextField","TextPane","FormattedTextField",
+    		"MenuBar","Menu","MenuItem","CheckBoxMenuItem"} ));
 
     //----------------------------------------------------------------------------
     // Instance data
