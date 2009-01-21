4cb67d22ef03fd18fd400135faa2951401876fe8
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-01-21 02:16:29 +0000

    factor out common class DefaultIconButton

--- simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/nodes/mediabuttons/RewindButton.java
+++ simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/nodes/mediabuttons/RewindButton.java
@@ -1,61 +1,15 @@
 package edu.colorado.phet.common.piccolophet.nodes.mediabuttons;
 
-import java.awt.BasicStroke;
-import java.awt.Color;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.ArrayList;
-
-import edu.colorado.phet.common.piccolophet.event.ButtonEventHandler;
-import edu.colorado.phet.common.piccolophet.event.ButtonEventHandler.ButtonEventAdapter;
-import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.colorado.phet.common.piccolophet.test.PiccoloTestFrame;
-import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
-import edu.umd.cs.piccolo.event.PInputEvent;
 
 /**
  * Created by: Sam
  * Sep 12, 2008 at 7:13:02 AM
  */
-public class RewindButton extends IconButton {
-
-    private PhetPPath iconNode;
-    private ArrayList listeners = new ArrayList();
+public class RewindButton extends DefaultIconButton {
 
     public RewindButton( int buttonHeight ) {
-        super( buttonHeight );
-
-        ButtonIconSet buttonIconSet = new ButtonIconSet( buttonHeight, buttonHeight );
-        iconNode = new PhetPPath( buttonIconSet.createRewindIconShape(), Color.BLACK, new BasicStroke( 1 ), Color.LIGHT_GRAY );
-        addChild( iconNode );
-
-        // this handler ensures that the button won't fire unless the mouse is released while inside the button
-        ButtonEventHandler handler = new ButtonEventHandler();
-        addInputEventListener( handler );
-        handler.addButtonEventListener( new ButtonEventAdapter() {
-            public void fire() {
-                notifyListeners();
-            }
-        } );
-    }
-
-    protected void updateImage() {
-        super.updateImage();
-        iconNode.setPaint( isEnabled() ? Color.black : Color.gray );
-    }
-
-    public static interface Listener {
-        void buttonPressed();
-    }
-
-    public void addListener( Listener listener ) {
-        listeners.add( listener );
-    }
-
-    public void notifyListeners() {
-        for ( int i = 0; i < listeners.size(); i++ ) {
-            ( (Listener) listeners.get( i ) ).buttonPressed();
-        }
+        super( buttonHeight, new ButtonIconSet( buttonHeight, buttonHeight ).createRewindIconShape() );
     }
 
     public static void main( String[] args ) {
--- simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/nodes/mediabuttons/StepButton.java
+++ simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/nodes/mediabuttons/StepButton.java
@@ -1,63 +1,18 @@
 package edu.colorado.phet.common.piccolophet.nodes.mediabuttons;
 
-import java.awt.BasicStroke;
-import java.awt.Color;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.ArrayList;
-
-import edu.colorado.phet.common.piccolophet.event.ButtonEventHandler;
-import edu.colorado.phet.common.piccolophet.event.ButtonEventHandler.ButtonEventAdapter;
-import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.colorado.phet.common.piccolophet.test.PiccoloTestFrame;
-import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
-import edu.umd.cs.piccolo.event.PInputEvent;
 
 /**
  * Created by: Sam
  * Sep 12, 2008 at 7:13:02 AM
  */
-public class StepButton extends IconButton {
-    
-    private PhetPPath iconNode;
-    private ArrayList listeners = new ArrayList();
+public class StepButton extends DefaultIconButton {
 
     public StepButton( int buttonHeight ) {
-        super( buttonHeight );
-        
-        ButtonIconSet buttonIconSet = new ButtonIconSet( buttonHeight, buttonHeight );
-        iconNode = new PhetPPath( buttonIconSet.createStepIconShape(), Color.BLACK, new BasicStroke( 1 ), Color.LIGHT_GRAY );
-        addChild( iconNode );
-        
-        // this handler ensures that the button won't fire unless the mouse is released while inside the button
-        ButtonEventHandler handler = new ButtonEventHandler();
-        addInputEventListener( handler );
-        handler.addButtonEventListener( new ButtonEventAdapter() {
-            public void fire() {
-                notifyListeners();
-            }
-        } );
+        super( buttonHeight, new ButtonIconSet( buttonHeight, buttonHeight ).createStepIconShape() );
     }
 
-    protected void updateImage() {
-        super.updateImage();
-        iconNode.setPaint( isEnabled() ? Color.black : Color.gray );
-    }
 
-    public static interface Listener {
-        void buttonPressed();
-    }
-
-    public void addListener( Listener listener ) {
-        listeners.add( listener );
-    }
-
-    public void notifyListeners() {
-        for ( int i = 0; i < listeners.size(); i++ ) {
-            ( (Listener) listeners.get( i ) ).buttonPressed();
-        }
-    }
-    
     public static void main( String[] args ) {
         PiccoloTestFrame testFrame = new PiccoloTestFrame( "Button Test" );
         PlayPauseButton playPauseButton = new PlayPauseButton( 75 );
@@ -68,4 +23,5 @@ public class StepButton extends IconButton {
         testFrame.addNode( button );
         testFrame.setVisible( true );
     }
+
 }
