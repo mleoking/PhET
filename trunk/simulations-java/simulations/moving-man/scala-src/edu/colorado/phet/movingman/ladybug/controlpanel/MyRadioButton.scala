21c78d37b8e2bedaff7de020158352232bdb9eec
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-01-23 06:22:20 +0000

    added auxiliary constructor and made main constructor more general

--- trunk/simulations-java/simulations/moving-man/scala-src/edu/colorado/phet/movingman/ladybug/controlpanel/MyRadioButton.scala
+++ trunk/simulations-java/simulations/moving-man/scala-src/edu/colorado/phet/movingman/ladybug/controlpanel/MyRadioButton.scala
@@ -4,11 +4,13 @@ import model.ObservableS
 import scala.swing.RadioButton
 import java.awt.event.{ActionEvent, ActionListener}
 
-class MyRadioButton(text: String, actionListener: => Unit, getter: => Boolean, observable: ObservableS) extends RadioButton(text) {
-  observable.addListener(update)
+class MyRadioButton(text: String, actionListener: => Unit, getter: => Boolean, addListener: (() => Unit) => Unit) extends RadioButton(text) {
+  addListener(update)
   update()
   peer.addActionListener(new ActionListener() {
     def actionPerformed(ae: ActionEvent) = actionListener
   });
   def update() = peer.setSelected(getter)
+
+  def this(text: String, actionListener: => Unit, getter: => Boolean, model: ObservableS) = this (text, actionListener, getter, model.addListener _)
 }
