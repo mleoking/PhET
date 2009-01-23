77f39dd7a9ae97cf5526c2c4d98a64d296b3db15
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-01-23 00:47:49 +0000

    removed Sunbeam threads

--- trunk/simulations-java/simulations/greenhouse/src/edu/colorado/phet/greenhouse/view/StarGraphic.java
+++ trunk/simulations-java/simulations/greenhouse/src/edu/colorado/phet/greenhouse/view/StarGraphic.java
@@ -12,7 +12,6 @@ import java.awt.geom.Rectangle2D;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
-import java.util.Random;
 
 import edu.colorado.phet.greenhouse.model.Disk;
 import edu.colorado.phet.greenhouse.model.Star;
@@ -23,23 +22,17 @@ public class StarGraphic extends DiskGraphic {
     private static Paint paint = Color.YELLOW;
     private java.util.List sunbeams = Collections.synchronizedList( new LinkedList() );
     private long timeBetweenSunbeams = 30;
-    private SunbeamGenerator sunbeamGenerator;
-    private SunbeamAnimator sunbeamAnimator;
 
     public StarGraphic( Star star, Rectangle2D.Double modelBounds ) {
         super( new Disk( star.getLocation(), star.getRadius() ), paint );
         this.star = star;
         this.modelBounds = modelBounds;
-        sunbeamGenerator = new SunbeamGenerator( sunbeams, timeBetweenSunbeams );
 //        sunbeamGenerator.start();
-        sunbeamAnimator = new SunbeamAnimator( sunbeams );
 //        sunbeamAnimator.start();
         Arrays.fill( dR, 1.3 );
     }
 
     public void stopAnimation() {
-        sunbeamGenerator.setRunning( false );
-        sunbeamAnimator.setRunning( false );
     }
 
     public void removeDistantSunbeams() {
@@ -127,81 +120,4 @@ public class StarGraphic extends DiskGraphic {
         }
     }
 
-    class SunbeamGenerator extends Thread {
-        java.util.List sunbeams;
-        private long timeBetweenSunbeams;
-        double t = System.currentTimeMillis();
-        private boolean isRunning;
-        int cnt;
-
-        SunbeamGenerator( java.util.List sunbeams, long timeBetweenSunbeams ) {
-            this.sunbeams = sunbeams;
-            this.timeBetweenSunbeams = timeBetweenSunbeams;
-        }
-
-        public void run() {
-            isRunning = true;
-            while ( isRunning ) {
-                synchronized( this ) {
-                    try {
-                        Thread.sleep( timeBetweenSunbeams );
-                        removeDistantSunbeams();
-                        for ( int i = 0; i < 2; i++ ) {
-                            double theta = Math.random() * Math.PI * 2;
-                            sunbeams.add( new Sunbeam( theta ) );
-                        }
-                    }
-                    catch( InterruptedException e ) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }
-
-        public synchronized void setRunning( boolean isRunning ) {
-            this.isRunning = isRunning;
-        }
-    }
-
-
-    class SunbeamAnimator extends Thread {
-        java.util.List sunbeams;
-        double t = System.currentTimeMillis();
-        double dt = 30;
-        boolean isRunning;
-        Random randomGenerator = new Random();
-
-        SunbeamAnimator( java.util.List sunbeams ) {
-            this.sunbeams = sunbeams;
-        }
-
-        int cnt = 0;
-
-        public void run() {
-            isRunning = true;
-            while ( isRunning ) {
-                try {
-                    Thread.sleep( (long) dt );
-                    t += dt;
-                    removeDistantSunbeams();
-                    for ( int i = 0; i < sunbeams.size(); i++ ) {
-                        Sunbeam sunbeam = (Sunbeam) sunbeams.get( i );
-                        sunbeam.computeLine( t );
-                    }
-                    cnt++;
-                    for ( int j = 0; cnt % 5 == 0 && j < numFlares; j++ ) {
-//                        dR[j] = 1.3 * ( randomGenerator.nextGaussian() * 0.1 + 1.0 );
-                        dR[j] = 1.3 * ( Math.random() * 0.2 + 1.0 );
-                    }
-                }
-                catch( InterruptedException e ) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
-        public void setRunning( boolean isRunning ) {
-            this.isRunning = isRunning;
-        }
-    }
 }
