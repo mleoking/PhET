867af4089d7f5d61a943ead1f4a1a9d553efc186
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-01-23 21:07:36 +0000

    first whack at model, not right, but checking it in for posterity

---
+++ simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/module/IAcid.java
@@ -0,0 +1,15 @@
+package edu.colorado.phet.acidbasesolutions.module;
+
+
+public interface IAcid {
+
+    public double getConcentrationHA();
+
+    public double getConcentrationA();
+
+    public double getConcentrationH3O();
+
+    public double getConcentrationOH();
+
+    public double getConcentrationH2O();
+}
---
+++ simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/module/IWeakBase.java
@@ -0,0 +1,15 @@
+package edu.colorado.phet.acidbasesolutions.module;
+
+
+public interface IWeakBase {
+    
+    public double getConcentrationB();
+
+    public double getConcentrationBH();
+
+    public double getConcentrationH3O();
+
+    public double getConcentrationOH();
+
+    public double getConcentrationH2O();
+}
