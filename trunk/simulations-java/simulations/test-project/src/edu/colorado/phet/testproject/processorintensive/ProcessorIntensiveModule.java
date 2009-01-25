db1d4525171c571acebefcf0dbb33579b0d21514
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-01-25 03:55:02 +0000

    move test code out of sim-template and into test-project

--- trunk/simulations-java/simulations/test-project/src/edu/colorado/phet/testproject/processorintensive/ProcessorIntensiveModule.java
+++ trunk/simulations-java/simulations/test-project/src/edu/colorado/phet/testproject/processorintensive/ProcessorIntensiveModule.java
@@ -1,22 +1,17 @@
-package edu.colorado.phet.simtemplate.module.intensive;
+package edu.colorado.phet.testproject.processorintensive;
 
-import java.awt.*;
+import java.awt.Graphics;
 
-import edu.colorado.phet.simtemplate.module.example.ExampleModule;
-import edu.colorado.phet.common.piccolophet.PhetPCanvas;
 import edu.colorado.phet.common.phetcommon.model.BaseModel;
 import edu.colorado.phet.common.phetcommon.model.ModelElement;
+import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
+import edu.colorado.phet.common.piccolophet.PhetPCanvas;
+import edu.colorado.phet.common.piccolophet.PiccoloModule;
 import edu.umd.cs.piccolo.nodes.PText;
 
-/**
- * Created by IntelliJ IDEA.
- * User: Sam
- * Date: Jan 15, 2009
- * Time: 2:25:41 PM
- */
-public class ProcessorIntensiveModule extends ExampleModule {
-    public ProcessorIntensiveModule( Frame parentFrame ) {
-        super( parentFrame );
+public class ProcessorIntensiveModule extends PiccoloModule {
+    public ProcessorIntensiveModule() {
+        super( "Process Intensive Test", new ConstantDtClock( 30, 1 ) );
         PhetPCanvas panel = new PhetPCanvas(){
             public void paintComponent( Graphics g ) {
                 super.paintComponent( g );
@@ -32,6 +27,7 @@ public class ProcessorIntensiveModule extends ExampleModule {
         setModel( new BaseModel() );
 
         final PText text = new PText( "hello" );
+        text.setOffset( 50, 50 );
         panel.addScreenChild( text );
         addModelElement( new ModelElement() {
             public void stepInTime( double dt ) {
