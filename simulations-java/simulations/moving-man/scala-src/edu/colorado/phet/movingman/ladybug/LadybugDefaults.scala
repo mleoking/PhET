c2d6f35a0159ff8a58f4733e49d6a65ebe5e2678
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-01-26 05:13:58 +0000

    bugfixes

--- simulations-java/simulations/moving-man/scala-src/edu/colorado/phet/movingman/ladybug/LadybugDefaults.scala
+++ simulations-java/simulations/moving-man/scala-src/edu/colorado/phet/movingman/ladybug/LadybugDefaults.scala
@@ -15,5 +15,5 @@ object LadybugDefaults {
   var pauseAtEndOfPlayback = true
   var recordAtEndOfPlayback = false
   val defaultLocation = new Vector2D(5, 1)
-  val ACCEL_VECTOR_SCALE = 2.454545
+  val ACCEL_VECTOR_SCALE = 0.2454545
 }
