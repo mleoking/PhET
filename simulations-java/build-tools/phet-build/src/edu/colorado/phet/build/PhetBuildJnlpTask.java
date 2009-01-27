8a3567b1a0a01b2bf7cae287ce92d7d075e95dd0
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-01-27 18:19:13 +0000

    see #1057, make it easier to delete javaws.phet.locale after IOM

--- simulations-java/build-tools/phet-build/src/edu/colorado/phet/build/PhetBuildJnlpTask.java
+++ simulations-java/build-tools/phet-build/src/edu/colorado/phet/build/PhetBuildJnlpTask.java
@@ -83,8 +83,8 @@ public class PhetBuildJnlpTask extends AbstractPhetBuildTask {
     private String getJNLPProperties() {//todo: locale support
         String properties = "";
         //explicitly request English for the default JNLP file
+        properties += "<property name=\"javaws.phet.locale\" value=\"" + ( locale == null ? "en" : locale ) + "\" />\n"; //XXX #1057, backward compatibility, delete after IOM
         properties += "<property name=\"javaws.user.language\" value=\"" + ( locale == null ? "en" : locale ) + "\" />";
-        properties += "<property name=\"javaws.phet.locale\" value=\"" + ( locale == null ? "en" : locale ) + "\" />"; //XXX #1057, backward compatibility, delete after IOM
         return properties;
     }
     
