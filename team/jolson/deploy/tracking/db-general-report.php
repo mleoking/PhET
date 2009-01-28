df38e89739b896622157baece81380430c8049bf
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-01-28 05:28:39 +0000

    Tracking database: Some code reformatting and clean up

--- team/jolson/deploy/tracking/db-general-report.php
+++ team/jolson/deploy/tracking/db-general-report.php
@@ -16,7 +16,7 @@
 		$result = mysql_query($query) or die("SELECT ERROR: " . mysql_error());
 		$end_time = microtime(true);
 		$num_rows = mysql_num_rows($result);
-		print "<h6>$query</h6>\n";
+		print "<h6>{$query}</h6>\n";
 		print "Running time: " . ($end_time - $start_time) . " seconds\n";
 		if($explain) {
 			display_explain($query);
@@ -36,7 +36,7 @@
 		while($get_info = mysql_fetch_row($result)) {
 			print "<tr>\n";
 			foreach($get_info as $field) {
-				print "\t<td><font face=arial size=2/>$field</font></td>\n";
+				print "\t<td><font face=arial size=2/>{$field}</font></td>\n";
 			}
 			print "</tr>\n";
 		}
@@ -59,7 +59,7 @@
 		while($get_info = mysql_fetch_row($result)) {
 			print "<tr>\n";
 			foreach($get_info as $field) {
-				print "\t<td><font face=arial size=2/>$field</font></td>\n";
+				print "\t<td><font face=arial size=2/>{$field}</font></td>\n";
 			}
 			print "</tr>\n";
 		}
