aa3db5ee90021796d8da1317362b34b9ffc69d6a
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-01-28 22:40:19 +0000

    Tracking database: ability to hide data on report

--- trunk/team/jolson/deploy/tracking/db-sim-report.php
+++ trunk/team/jolson/deploy/tracking/db-sim-report.php
@@ -4,14 +4,20 @@
 	
 	set_time_limit(60 * 20);
 	
-	function display_desc($desc) {
-		print "\n<h4>{$desc}</h4>\n";
-	}
+	$query_counter = 0;
 	
-	function display_query($query) {
+	function display_query($desc, $query) {
+		global $query_counter;
+		
+		$query_counter++;
+		
+		$id = 'id' . $query_counter;
+		
+		print "\n<h4 style='padding-bottom: 0px; margin-top: 5px; margin-bottom: 5px;'>{$desc}</h4><a href='#' style='color: #FF0000; font-size: 10px; cursor: hand; padding: 0px; margin: 0px' onclick=\"var ob = document.getElementById('{$id}'); ob.style.display = 'none';\">hide data</a>\n";
+		
 		$result = mysql_query($query) or die("SELECT ERROR: " . mysql_error());
 		$num_rows = mysql_num_rows($result);
-		print "<table border=1>\n";
+		print "<table border=1 id='{$id}'>\n";
 		$fields_num = mysql_num_fields($result);
 		for($i=0; $i<$fields_num; $i++) {
 			$field = mysql_fetch_field($result);
@@ -43,28 +49,20 @@
 	$row = mysql_fetch_row($result);
 	$simType = $row[0];
 	
-	display_desc("java or flash?");
-	display_query("SELECT DISTINCT IF(sim_type = 0, 'java', 'flash') AS sim_type FROM session WHERE sim_name = @sid;");
+	display_query("java or flash?", "SELECT DISTINCT IF(sim_type = 0, 'java', 'flash') AS sim_type FROM session WHERE sim_name = @sid;");
 	
-	display_desc("total number of messages");
-	display_query("SELECT COUNT(*) AS total_messages FROM session WHERE (session.sim_name = @sid)");
+	display_query("total number of messages", "SELECT COUNT(*) AS total_messages FROM session WHERE (session.sim_name = @sid)");
 	
-	display_desc("total number of messages (non-dev)");
-	display_query("SELECT COUNT(*) AS total_messages FROM session WHERE (session.sim_name = @sid AND session.sim_dev = false)");
+	display_query("total number of messages (non-dev)", "SELECT COUNT(*) AS total_messages FROM session WHERE (session.sim_name = @sid AND session.sim_dev = false)");
 	
-	display_desc("total number of sim runs (non-dev)");
-	display_query("SELECT SUM(session.sim_sessions_since) AS total_sessions FROM session WHERE (session.sim_name = @sid AND session.sim_dev = false)");
+	display_query("total number of sim runs (non-dev)", "SELECT SUM(session.sim_sessions_since) AS total_sessions FROM session WHERE (session.sim_name = @sid AND session.sim_dev = false)");
 	
-	display_desc("number of times sim has been run by week");
-	display_query("SELECT YEARWEEK(timestamp) AS year_week, SUM(session.sim_sessions_since) AS sessions FROM session WHERE (session.sim_name = @sid) GROUP BY YEARWEEK(timestamp);");
+	display_query("number of times sim has been run by week", "SELECT YEARWEEK(timestamp) AS year_week, SUM(session.sim_sessions_since) AS sessions FROM session WHERE (session.sim_name = @sid) GROUP BY YEARWEEK(timestamp);");
 	
-	display_desc("number of times each simplified OS has been seen (non-dev)");
-	display_query("SELECT simplified_os.name, SUM(session.sim_sessions_since) AS sessions FROM session, simplified_os WHERE (session.host_simplified_os = simplified_os.id AND session.sim_dev = false AND session.sim_name = @sid) GROUP BY simplified_os.name ORDER BY COUNT(session.id) DESC;");
+	display_query("number of times each simplified OS has been seen (non-dev)", "SELECT simplified_os.name, SUM(session.sim_sessions_since) AS sessions FROM session, simplified_os WHERE (session.host_simplified_os = simplified_os.id AND session.sim_dev = false AND session.sim_name = @sid) GROUP BY simplified_os.name ORDER BY COUNT(session.id) DESC;");
 	
-	display_desc("number of times each deployment type has been seen (non-dev)");
-	display_query("SELECT deployment.name, SUM(session.sim_sessions_since) AS sessions FROM session, deployment WHERE (session.sim_deployment = deployment.id AND session.sim_dev = false AND session.sim_name = @sid) GROUP BY deployment.name ORDER BY COUNT(session.id) DESC;");
+	display_query("number of times each deployment type has been seen (non-dev)", "SELECT deployment.name, SUM(session.sim_sessions_since) AS sessions FROM session, deployment WHERE (session.sim_deployment = deployment.id AND session.sim_dev = false AND session.sim_name = @sid) GROUP BY deployment.name ORDER BY COUNT(session.id) DESC;");
 	
-	display_desc("number of times each (non-null) distribution tag has been seen (non-dev)");
 	$query = <<<DIST
 SELECT
 	distribution_tag.name,
@@ -79,12 +77,10 @@ WHERE (
 GROUP BY distribution_tag.name
 ORDER BY COUNT(session.id) DESC;
 DIST;
-	display_query($query);
+	display_query("number of times each (non-null) distribution tag has been seen (non-dev)", $query);
 	
-	display_desc("number of times sim has been run, by language");
-	display_query("SELECT session.sim_locale_language, SUM(session.sim_sessions_since) FROM session WHERE (session.sim_name = @sid) GROUP BY session.sim_locale_language ORDER BY SUM(session.sim_sessions_since) DESC;");
+	display_query("number of times sim has been run, by language", "SELECT session.sim_locale_language, SUM(session.sim_sessions_since) FROM session WHERE (session.sim_name = @sid) GROUP BY session.sim_locale_language ORDER BY SUM(session.sim_sessions_since) DESC;");
 	
-	display_desc("sim runs by locale");
 	$query = <<<LOC
 SELECT
 	IF(y.country IS NULL, y.language, CONCAT(y.language, '_', y.country)) AS locale,
@@ -101,9 +97,8 @@ FROM (
 ) as y
 ORDER BY sim_runs DESC;
 LOC;
-	display_query($query);
+	display_query("sim runs by locale", $query);
 	
-	display_desc("number of times the sim has been run, by year month and day (within the last year) (non-dev)");
 	$query = <<<RUN
 SELECT DISTINCT
 	YEAR(timestamp) AS year,
@@ -118,10 +113,9 @@ WHERE (
 )
 GROUP BY YEAR(timestamp), MONTH(timestamp), DAY(timestamp);
 RUN;
-	display_query($query);
+	display_query("number of times the sim has been run, by year month and day (within the last year) (non-dev)", $query);
 	
 	if($simType == "flash") {
-		display_desc("number of times each (non-null) Flash OS has been seen (non-dev)");
 		$query = <<<DIST
 SELECT
 	flash_os.name,
@@ -137,9 +131,8 @@ WHERE (
 GROUP BY flash_os.name
 ORDER BY COUNT(session.id) DESC;
 DIST;
-		display_query($query);
+		display_query("number of times each (non-null) Flash OS has been seen (non-dev)", $query);
 		
-		display_desc("number of times each (non-null) Flash domain has been seen (non-dev)");
 		$query = <<<DIST
 SELECT
 	flash_domain.name,
@@ -155,9 +148,8 @@ WHERE (
 GROUP BY flash_domain.name
 ORDER BY COUNT(session.id) DESC;
 DIST;
-		display_query($query);
+		display_query("number of times each (non-null) Flash domain has been seen (non-dev)", $query);
 		
-		display_desc("number of times each Flash major version has been seen (non-dev)");
 		$query = <<<DIST
 SELECT
 	session_flash_info.host_flash_version_major,
@@ -171,9 +163,8 @@ WHERE (
 GROUP BY session_flash_info.host_flash_version_major
 ORDER BY session_flash_info.host_flash_version_major;
 DIST;
-		display_query($query);
+		display_query("number of times each Flash major version has been seen (non-dev)", $query);
 		
-		display_desc("number of times each Flash version has been seen (non-dev)");
 		$query = <<<EOT
 SELECT
 	CONCAT(
@@ -201,7 +192,7 @@ GROUP BY
 	session_flash_info.host_flash_version_build
 ORDER BY sessions DESC;
 EOT;
-		display_query($query);
+		display_query("number of times each Flash version has been seen (non-dev)", $query);
 	}
 	
 	$total_end_time = microtime(true);
