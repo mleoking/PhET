2881db80f1bfbd8eda8c1fff7117046b8841a962
dano <dano@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-01-30 01:33:01 +0000

    website: Updated the Flash tech support page

--- website/tech_support/support-flash.php
+++ website/tech_support/support-flash.php
@@ -19,18 +19,41 @@ class FlashSupportPage extends SitePage {
         $portal_root = PORTAL_ROOT;
         $phet_help_email = PHET_HELP_EMAIL;
 
+        $flash_version = FLASH_MIN_VERSION_FULL;
+
         print <<<EOT
-            <p>This page will help you solve some of the problems people commonly have running our programs. If you can't solve your problem here, please notify us by email at the following address: <a href="mailto:{$phet_help_email}"><span class="red">{$phet_help_email}</span></a>.</p>
+            <p>
+            This page will help you solve some of the problems people
+            commonly have running our programs. If you can't solve your 
+            problem here, please notify us by email at the following address:
+            <a href="mailto:{$phet_help_email}">
+              <span class="red">{$phet_help_email}</span>
+            </a>.
+            </p>
 
-            <p>To run the Flash-based simulations you must have Flash 8 (available free) or newer installed on your computer.</p><a href="http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash"><img src="{$this->prefix}images/get-flash.gif" alt="Get Flash" /></a>
+            <p>
+            To run the Flash-based simulations you must have
+            {$flash_version} (available free) or newer installed on your
+            computer.
+            </p>
 
-            <p>If you get a blank window when you try to launch a Flash simulation, you probably need a new version of the Flash player.</p>
+            <a href="http://www.adobe.com/go/getflashplayer">
+              <img class="getflashbutton centered" src="{$this->prefix}images/160x41_Get_Flash_Player.jpg" alt="Get Adobe Flash Player" />
+            </a>
 
-            <p>Older versions of the Flash Player can cause problems. Updating your flash player is recommended if you receive an error similar to:<br />
+            <p>
+            If you get a blank window when you try to launch a Flash 
+            simulation, you probably need a new version of the Flash player.
+            </p>
+
+            <p>
+            Older versions of the Flash Player can cause problems.
+            Updating your flash player is recommended if you receive
+            an error similar to:
             <br />
-            <a href="/"><img src="{$this->prefix}images/flash-error.gif" alt="Image of a Flash error message" /></a></p>
-
-            <p>If you are unsure if you currently have a version of Flash, we can check for you if you <a href="flash_detect_v7.html">click here.</a></p>
+            <br />
+            <img class="centered" src="{$this->prefix}images/flash-error.gif" alt="Image of a Flash error message" />
+            </p>
 
             <p><a href="#top"><img src="{$this->prefix}images/top.gif" alt="Back to the Top" /></a></p>
 
