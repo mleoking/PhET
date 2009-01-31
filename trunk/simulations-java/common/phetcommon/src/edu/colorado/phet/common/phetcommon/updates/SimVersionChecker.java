254475a8e496efa8e6fcf9b10bb7df73be09f959
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-01-31 00:47:03 +0000

    delete debug output

--- trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/updates/SimVersionChecker.java
+++ trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/updates/SimVersionChecker.java
@@ -31,7 +31,6 @@ public class SimVersionChecker {
      */
     public PhetVersion getVersion( String project, String simulation ) throws IOException {
         String xmlString = getXMLString( project, simulation );
-         System.out.println( "SimVersionChecker xmlString=" + xmlString );
         try {
             Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse( new ByteArrayInputStream( xmlString.getBytes() ) );
             String version = document.getDocumentElement().getAttribute( "version" );
