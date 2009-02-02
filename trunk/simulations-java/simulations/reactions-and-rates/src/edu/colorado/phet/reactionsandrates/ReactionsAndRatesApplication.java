74270fd5409b0b6a6c4c0face3f54b88cb7bd7aa
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-02-02 17:38:36 +0000

    bugfix in initializer (was calling swing thread from swing thread, and crashing)

--- trunk/simulations-java/simulations/reactions-and-rates/src/edu/colorado/phet/reactionsandrates/ReactionsAndRatesApplication.java
+++ trunk/simulations-java/simulations/reactions-and-rates/src/edu/colorado/phet/reactionsandrates/ReactionsAndRatesApplication.java
@@ -12,11 +12,9 @@ package edu.colorado.phet.reactionsandrates;
 
 import java.lang.reflect.InvocationTargetException;
 
-import javax.swing.*;
-
+import edu.colorado.phet.common.phetcommon.application.ApplicationConstructor;
 import edu.colorado.phet.common.phetcommon.application.PhetApplication;
 import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
-import edu.colorado.phet.common.phetcommon.application.ApplicationConstructor;
 import edu.colorado.phet.common.phetcommon.application.PhetApplicationLauncher;
 import edu.colorado.phet.common.phetcommon.view.PhetLookAndFeel;
 import edu.colorado.phet.common.phetcommon.view.util.FrameSetup;
@@ -41,31 +39,13 @@ public class ReactionsAndRatesApplication extends PiccoloPhetApplication {
 
 
     public static void main( final String[] args ) throws InvocationTargetException, InterruptedException {
-        SwingUtilities.invokeLater( new Runnable() {
-            public void run() {
-                // Standard initializations
-                PhetLookAndFeel phetLookAndFeel = new PhetLookAndFeel();
-                phetLookAndFeel.setFont( MRConfig.CONTROL_FONT );
-                phetLookAndFeel.setTitledBorderFont( MRConfig.CONTROL_FONT );
+        PhetLookAndFeel phetLookAndFeel = new PhetLookAndFeel();
+        phetLookAndFeel.setFont( MRConfig.CONTROL_FONT );
+        phetLookAndFeel.setTitledBorderFont( MRConfig.CONTROL_FONT );
 
-
-//                PhetApplicationConfig config = new PhetApplicationConfig( args, frameSetup, MRConfig.RESOURCES );
-                ApplicationConstructor applicationConstructor = new ApplicationConstructor() {
-                    public PhetApplication getApplication( PhetApplicationConfig config ) {
-
-                        ReactionsAndRatesApplication application = new ReactionsAndRatesApplication( config );
-
-                        // Let 'er rip
-                        application.startApplication();
-                        return application;
-                    }
-                };
-                PhetApplicationConfig config = new PhetApplicationConfig( args, "reactions-and-rates" );
-                config.setFrameSetup( new FrameSetup.CenteredWithSize( 1000, 700 ) );
-                config.setLookAndFeel( phetLookAndFeel );
-                new PhetApplicationLauncher().launchSim( config, applicationConstructor );
-            }
-        } );
-
+        PhetApplicationConfig config = new PhetApplicationConfig( args, "reactions-and-rates" );
+        config.setFrameSetup( new FrameSetup.CenteredWithSize( 1000, 700 ) );
+        config.setLookAndFeel( phetLookAndFeel );
+        new PhetApplicationLauncher().launchSim( config, ReactionsAndRatesApplication.class );
     }
 }
