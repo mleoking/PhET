90a80a5ab166d64e1598084608c14d6edd743ad9
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-02-04 18:11:16 +0000

    Statistics-database: submit_message changes to return the XML status

--- trunk/statistics/submit_message.php
+++ trunk/statistics/submit_message.php
@@ -13,11 +13,15 @@
 	
 	
 	function fail_me($str) {
-		print "<p>Failure: {$str}</p></html>";
+		print "<error-message>{$str}</error-message><success>false</success></statistics-result></xml>";
 		exit;
 	}
 	
-	print "<html>";
+	function warn_me($str) {
+		print "<warning-message>{$str}</warning-message>";
+	}
+	
+	print "<xml><statistics-result>";
 	
 	// load the xml from postdata
 	$xml = simplexml_load_string($HTTP_RAW_POST_DATA);
@@ -56,33 +60,25 @@
 		fclose($file);
 	}
 	
-	// determine whether a string should be represented as NULL
-	// inside the statistics database
-	function is_int_field_null($value) {
-		if(
-			$value === null
-			|| $value === false
-			|| $value === ""
-			|| $value === "null"
-			|| $value === "undefined"
-			|| $value === "none"
-		) {
-			return true;
-		}
-		return false;
-	}
-	
 	function int_decode($field) {
 		global $xml;
 		
 		// turn %20 => ' ', etc.
 		$decoded = urldecode($xml[$field]);
 		
-		// if it represents a null value, return NULL
-		if(is_int_field_null($decoded)) {
+		if($decoded === null) {
+			warn_me("field {$field} was not specified, replacing with null");
 			if($field == "sim_version_timestamp") {
-				// TODO: try to coerce, or just ignore this?
-				fail_me("sim_version_timestamp cannot be null");
+				return "0";
+				warn_me("sim_version_timestamp cannot be null, replacing with '0'");
+			}
+			return "NULL";
+		}
+		
+		if($decoded === "null") {
+			if($field == "sim_version_timestamp") {
+				return "0";
+				warn_me("sim_version_timestamp cannot be null, replacing with '0'");
 			}
 			return "NULL";
 		}
@@ -91,11 +87,60 @@
 			// is a numeric string, this is OK
 			return $decoded;
 		} else {
-			fail_me("value for {$field} is non-null and non-numeric");
+			fail_me("value for {$field} ({$decoded}) is non-null and non-numeric");
 		}
 	}
 	
-	// TODO: bool_decode?
+	function int_verify($value, $name) {
+		if(ctype_digit($value)) {
+			// string is numeric
+			return $value;
+		} else {
+			fail_me("field {$name} ({$value}) failed to parse as a number");
+		}
+	}
+	
+	function string_decode($field) {
+		global $xml;
+		
+		// turn %20 => ' ', etc.
+		$decoded = urldecode($xml[$field]);
+		
+		if($decoded === null) {
+			warn_me("field {$field} was not specified, replacing with null");
+			return "NULL";
+		}
+		
+		return $decoded;
+	}
+	
+	function bool_decode($field) {
+		global $xml;
+		
+		// turn %20 => ' ', etc.
+		$decoded = urldecode($xml[$field]);
+		
+		if($decoded === null) {
+			warn_me("boolean field {$field} was not specified, replacing with null");
+			return "NULL";
+		}
+		
+		if($decoded === "null") {
+			return "NULL";
+		}
+		
+		if(
+			$decoded === "true"
+			|| $decoded === "false"
+			|| $decoded === "0"
+			|| $decoded === "1"
+		) {
+			// we are OK, this is a good boolean
+			return $decoded;
+		}
+		
+		fail_me("field {$field} value {$decoded} is not boolean");
+	}
 	
 	if($xml["sim_type"] == "flash") {
 	    // message from a flash simulation
@@ -123,46 +168,45 @@
 			
 			// extract everything after the first space ("9,0,124,0")
 			$version_right = substr($version_string, $space_position + 1);
-			if(empty($version_right)) { die("Error: 725378"); }
+			if(empty($version_right)) { fail_me("Bad Flash version information"); }
 			
 			// split right side on commas (array("9", "0", "124", "0"))
 			$version_numbers = explode(",", $version_right);
-			if(count($version_numbers) != 4) { die("Error: 981403"); }
+			if(count($version_numbers) != 4) { fail_me("Bad Flash version information"); }
 			
 			$sessionID = insert_flash_message(
 				array (
 					"message_version" => 1,
-					"sim_project" => urldecode($xml["sim_project"]),
-					"sim_name" => urldecode($xml["sim_name"]),
+					"sim_project" => string_decode("sim_project"),
+					"sim_name" => string_decode("sim_name"),
 					"sim_major_version" => int_decode("sim_major_version"),
 					"sim_minor_version" => int_decode("sim_minor_version"),
 					"sim_dev_version" => int_decode("sim_dev_version"),
 					"sim_svn_revision" => int_decode("sim_svn_revision"),
 					"sim_version_timestamp" => int_decode("sim_version_timestamp"),
-					"sim_locale_language" => urldecode($xml["sim_locale_language"]),
-					"sim_locale_country" => urldecode($xml["sim_locale_country"]),
+					"sim_locale_language" => string_decode("sim_locale_language"),
+					"sim_locale_country" => string_decode("sim_locale_country"),
 					"sim_sessions_since" => int_decode("sim_sessions_since"),
 					"sim_total_sessions" => int_decode("sim_total_sessions"),
-					"sim_deployment" => urldecode($xml["sim_deployment"]),
-					"sim_distribution_tag" => urldecode($xml["sim_distribution_tag"]),
-					"sim_dev" => urldecode($xml["sim_dev"]),
-					"host_locale_language" => urldecode($xml["host_locale_language"]),
-					"host_locale_country" => "none",
+					"sim_deployment" => string_decode("sim_deployment"),
+					"sim_distribution_tag" => string_decode("sim_distribution_tag"),
+					"sim_dev" => bool_decode("sim_dev"),
+					"host_locale_language" => string_decode("host_locale_language"),
+					"host_locale_country" => "null",
 					"host_flash_version_type" => $version_left,
-					"host_flash_version_major" => $version_numbers[0],
-					"host_flash_version_minor" => $version_numbers[1],
-					"host_flash_version_revision" => $version_numbers[2],
-					"host_flash_version_build" => $version_numbers[3],
-					"host_flash_time_offset" => urldecode($xml["host_flash_time_offset"]),
-					"host_flash_accessibility" => urldecode($xml["host_flash_accessibility"]),
-					"host_flash_domain" => urldecode($xml["host_flash_domain"]),
-					"host_flash_os" => urldecode($xml["host_flash_os"])
+					"host_flash_version_major" => int_verify($version_numbers[0], "host_flash_version_major"),
+					"host_flash_version_minor" => int_verify($version_numbers[1], "host_flash_version_minor"),
+					"host_flash_version_revision" => int_verify($version_numbers[2], "host_flash_version_revision"),
+					"host_flash_version_build" => int_verify($version_numbers[3], "host_flash_version_build"),
+					"host_flash_time_offset" => string_decode("host_flash_time_offset"),
+					"host_flash_accessibility" => string_decode("host_flash_accessibility"),
+					"host_flash_domain" => string_decode("host_flash_domain"),
+					"host_flash_os" => string_decode("host_flash_os")
 				)
 			);
 			
 			if(empty($sessionID)) {
-				$success = false;
-				$fail_reason = "message error";
+				fail_me("flash insertion error");
 			}
 			
 		}
@@ -182,45 +226,44 @@
 			$sessionID = insert_java_message(
 				array (
 					"message_version" => 1,
-					"sim_project" => urldecode($xml["sim_project"]),
-					"sim_name" => urldecode($xml["sim_name"]),
+					"sim_project" => string_decode("sim_project"),
+					"sim_name" => string_decode("sim_name"),
 					"sim_major_version" => int_decode("sim_major_version"),
 					"sim_minor_version" => int_decode("sim_minor_version"),
 					"sim_dev_version" => int_decode("sim_dev_version"),
 					"sim_svn_revision" => int_decode("sim_svn_revision"),
 					"sim_version_timestamp" => int_decode("sim_version_timestamp"),
-					"sim_locale_language" => urldecode($xml["sim_locale_language"]),
-					"sim_locale_country" => urldecode($xml["sim_locale_country"]),
+					"sim_locale_language" => string_decode("sim_locale_language"),
+					"sim_locale_country" => string_decode("sim_locale_country"),
 					"sim_sessions_since" => int_decode("sim_sessions_since"),
 					"sim_total_sessions" => int_decode("sim_total_sessions"),
-					"sim_deployment" => urldecode($xml["sim_deployment"]),
-					"sim_distribution_tag" => urldecode($xml["sim_distribution_tag"]),
-					"sim_dev" => urldecode($xml["sim_dev"]),
-					"host_locale_language" => urldecode($xml["host_locale_language"]),
-					"host_locale_country" => urldecode($xml["host_locale_country"]),
-					"host_java_os_name" => urldecode($xml["host_os_name"]),
-					"host_java_os_version" => urldecode($xml["host_os_version"]),
-					"host_java_os_arch" => urldecode($xml["host_os_arch"]),
-					"host_java_vendor" => urldecode($xml["host_java_vendor"]),
+					"sim_deployment" => string_decode("sim_deployment"),
+					"sim_distribution_tag" => string_decode("sim_distribution_tag"),
+					"sim_dev" => bool_decode("sim_dev"),
+					"host_locale_language" => string_decode("host_locale_language"),
+					"host_locale_country" => string_decode("host_locale_country"),
+					"host_java_os_name" => string_decode("host_os_name"),
+					"host_java_os_version" => string_decode("host_os_version"),
+					"host_java_os_arch" => string_decode("host_os_arch"),
+					"host_java_vendor" => string_decode("host_java_vendor"),
 					"host_java_version_major" => int_decode("host_java_version_major"),
 					"host_java_version_minor" => int_decode("host_java_version_minor"),
 					"host_java_version_maintenance" => int_decode("host_java_version_maintenance"),
-					"host_java_webstart_version" => urldecode($xml["host_java_webstart_version"]),
-					"host_java_timezone" => urldecode($xml["host_java_timezone"])
+					"host_java_webstart_version" => string_decode("host_java_webstart_version"),
+					"host_java_timezone" => string_decode("host_java_timezone")
 				)
 			);
 			
 			if(empty($sessionID)) {
-				$success = false;
-				$fail_reason = "message error";
+				fail_me("java insertion error");
 			}
 			
 		}
 		
 	}
 	
-	print "<p>Received Successfully</p>";
+	print "<success>true</success>";
 	
-	print "</html>";
+	print "</statistics-result></xml>";
 	
 ?>
