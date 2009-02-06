45f910d6bb0c7c1f968bcc9b3a54cbb800db0bc2
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-02-06 21:22:18 +0000

    > Increased size of return ladybug button

--- trunk/simulations-java/simulations/moving-man/scala-src/edu/colorado/phet/movingman/ladybug/canvas/ReturnLadybugButton.scala
+++ trunk/simulations-java/simulations/moving-man/scala-src/edu/colorado/phet/movingman/ladybug/canvas/ReturnLadybugButton.scala
@@ -1,16 +1,18 @@
 package edu.colorado.phet.movingman.ladybug.canvas
 
+import _root_.edu.colorado.phet.common.phetcommon.view.util.PhetFont
 import _root_.edu.colorado.phet.common.piccolophet.PhetPNode
 import _root_.scala.swing.Button
 import java.awt.event.{ActionEvent, ComponentAdapter, ComponentEvent, ActionListener}
 import java.awt.geom.Point2D
-import java.awt.Rectangle
+import java.awt.{Rectangle, Graphics2D, Graphics}
 import javax.swing.JButton
 import model.LadybugModel
 import umd.cs.piccolo.nodes.PText
 import umd.cs.piccolo.PNode
 import edu.colorado.phet.movingman.ladybug.LadybugUtil._
 import umd.cs.piccolox.pswing.PSwing
+import java.awt.Color._
 
 class ReturnLadybugButton(model: LadybugModel, canvas: LadybugCanvas) extends PhetPNode {
     val b = new JButton("Return Ladybug")
@@ -19,6 +21,7 @@ class ReturnLadybugButton(model: LadybugModel, canvas: LadybugCanvas) extends Ph
     model.addListener(updateVisible)
     updateVisible()
     updateLocation()
+    b.setFont(new PhetFont(20))
     canvas.addComponentListener(new ComponentAdapter {
         override def componentResized(e: ComponentEvent) = {
             updateLocation
