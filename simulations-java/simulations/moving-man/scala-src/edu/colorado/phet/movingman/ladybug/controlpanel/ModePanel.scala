931db445acefa5b4b7d9d91c6dd0704451eb7d4d
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-02-06 00:29:26 +0000

    added color highlighting to show pause mode

--- simulations-java/simulations/moving-man/scala-src/edu/colorado/phet/movingman/ladybug/controlpanel/ModePanel.scala
+++ simulations-java/simulations/moving-man/scala-src/edu/colorado/phet/movingman/ladybug/controlpanel/ModePanel.scala
@@ -5,12 +5,22 @@ import javax.swing.{BoxLayout, JPanel}
 import javax.swing.BoxLayout._
 import model.LadybugModel
 import edu.colorado.phet.movingman.ladybug.swing.MyRadioButton
+import java.awt.Color._
 
 class ModePanel(model: LadybugModel) extends JPanel {
     setLayout(new BoxLayout(this, Y_AXIS))
-    val recordingButton = new MyRadioButton("Recording", model.setRecord(true), model.isRecord, model.addListener)
+    def color(b: Boolean) = if (b) red else black
+
+    val recordingButton = new MyRadioButton("Recording", model.setRecord(true), {model.isRecord}, model.addListener)
     add(recordingButton.peer)
 
-    val playbackButton = new MyRadioButton("Playback", model.setRecord(false), model.isPlayback, model.addListener)
+    val playbackButton = new MyRadioButton("Playback", model.setRecord(false), {model.isPlayback}, model.addListener)
     add(playbackButton.peer)
+
+    model.addListener(() => updateColors)
+    def updateColors = {
+        recordingButton.peer.setForeground(color(recordingButton.peer.isSelected && !model.isPaused))
+        playbackButton.peer.setForeground(color(playbackButton.peer.isSelected && !model.isPaused))
+    }
+    updateColors
 }
