d62a043e7425798de704a6e1c0537ff11c4039fb
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-02-06 00:39:01 +0000

    refactoring

--- simulations-java/simulations/moving-man/scala-src/edu/colorado/phet/movingman/ladybug/controlpanel/ModePanel.scala
+++ simulations-java/simulations/moving-man/scala-src/edu/colorado/phet/movingman/ladybug/controlpanel/ModePanel.scala
@@ -1,8 +1,8 @@
 package edu.colorado.phet.movingman.ladybug.controlpanel
 
 import _root_.scala.swing.Panel
-import javax.swing.{BoxLayout, JPanel}
 import javax.swing.BoxLayout._
+import javax.swing.{BoxLayout, JPanel, JComponent}
 import model.LadybugModel
 import edu.colorado.phet.movingman.ladybug.swing.MyRadioButton
 import java.awt.Color._
@@ -11,11 +11,8 @@ class ModePanel(model: LadybugModel) extends JPanel {
     setLayout(new BoxLayout(this, Y_AXIS))
     def color(b: Boolean) = if (b) red else black
 
-    val recordingButton = new MyRadioButton("Recording", model.setRecord(true), {model.isRecord}, model.addListener)
-    add(recordingButton.peer)
-
-    val playbackButton = new MyRadioButton("Playback", model.setRecord(false), {model.isPlayback}, model.addListener)
-    add(playbackButton.peer)
+    val recordingButton = addNewControl{new MyRadioButton("Recording", model.setRecord(true), {model.isRecord}, model.addListener)}
+    val playbackButton = addNewControl{new MyRadioButton("Playback", model.setRecord(false), {model.isPlayback}, model.addListener)}
 
     model.addListener(() => updateColors)
     def updateColors = {
@@ -23,4 +20,11 @@ class ModePanel(model: LadybugModel) extends JPanel {
         playbackButton.peer.setForeground(color(playbackButton.peer.isSelected && !model.isPaused))
     }
     updateColors
+
+    //a control structure that (1) creates a swing component and (2) automatically adds it
+    def addNewControl(m: => MyRadioButton): MyRadioButton = {
+        val component = m
+        add(component.peer)
+        component
+    }
 }
