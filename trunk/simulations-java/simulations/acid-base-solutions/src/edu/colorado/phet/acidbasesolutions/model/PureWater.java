d338773a8ccadbd2e657dc6d66ce896ea12a029e
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-02-10 00:16:28 +0000

    pure water is not a solution

--- trunk/simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/model/PureWater.java
+++ trunk/simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/model/PureWater.java
@@ -3,7 +3,14 @@ package edu.colorado.phet.acidbasesolutions.model;
 import edu.colorado.phet.acidbasesolutions.ABSStrings;
 import edu.colorado.phet.acidbasesolutions.ABSSymbols;
 
-
+/**
+ * Pure water is not an aqueous solution.
+ * A solution is formed when a solvent dissolves some substance.
+ * In an aqueous solution, water is the solvent.
+ * In pure water, there is no substance that is dissolves, and thus no solution.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
 public class PureWater {
     
     private static final double W = 55.6; // concentration, mol/L
@@ -26,5 +33,12 @@ public class PureWater {
     public double getEquilibriumConstant() {
         return Kw;
     }
+    
+    public double getHydroniumConcentration() {
+        return 1E-7;
+    }
 
+    public double getHydroxideConcentration() {
+        return 1E-7;
+    }
 }
