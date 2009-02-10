5fb2a982aacee0c35f64c183f59bc11f53c0a9ec
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-02-10 01:20:08 +0000

    basic sketch of controls for solution

---
+++ trunk/simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/control/MoleculeComboBox.java
@@ -0,0 +1,12 @@
+package edu.colorado.phet.acidbasesolutions.control;
+
+import javax.swing.JComboBox;
+
+
+public class MoleculeComboBox extends JComboBox {
+    
+    public MoleculeComboBox() {
+        super();
+    }
+
+}
