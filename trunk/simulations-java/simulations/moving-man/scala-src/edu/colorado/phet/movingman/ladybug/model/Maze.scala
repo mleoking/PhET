6c5a3939f9bf304c9c70ce5f760880e2eb7b22c0
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-02-11 07:03:20 +0000

    added basic collision detection

--- trunk/simulations-java/simulations/moving-man/scala-src/edu/colorado/phet/movingman/ladybug/model/Maze.scala
+++ trunk/simulations-java/simulations/moving-man/scala-src/edu/colorado/phet/movingman/ladybug/model/Maze.scala
@@ -2,9 +2,20 @@ package edu.colorado.phet.movingman.ladybug.model
 
 import _root_.scala.collection.mutable.ArrayBuffer
 import java.awt.geom.Rectangle2D
+import edu.colorado.phet.movingman.ladybug.LadybugUtil._
 
 class Maze {
   val rectangles = new ArrayBuffer[Rectangle2D]
 
-  rectangles += new Rectangle2D.Double(0, 0, 10, 10)
+  rectangles += new Rectangle2D.Double(-10, -9, 20, 1)
+  rectangles += new Rectangle2D.Double(-10, 9, 20, 1)
+  rectangles += new Rectangle2D.Double(-9, -10, 1, 20)
+  rectangles += new Rectangle2D.Double(9, -10, 1, 20)
+
+  def update(ladybug: Ladybug) = {}
+
+  def containsPoint(pt: Vector2D):Boolean = {
+    rectangles.foldLeft(false)((value:Boolean,cur:Rectangle2D)=>cur.contains(pt)||value)
+  }
+
 }
