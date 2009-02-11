ff0602b963de6b6e1ed7d8ee094617637029bab0
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-02-11 15:50:51 +0000

    improvements for maze generation

--- trunk/simulations-java/simulations/moving-man/scala-src/edu/colorado/phet/movingman/ladybug/aphidmaze/MazeGenerator.scala
+++ trunk/simulations-java/simulations/moving-man/scala-src/edu/colorado/phet/movingman/ladybug/aphidmaze/MazeGenerator.scala
@@ -1,30 +1,50 @@
 package edu.colorado.phet.movingman.ladybug.aphidmaze
 
-import _root_.scala.collection.mutable.ArrayBuffer
+import _root_.scala.collection.mutable.{HashSet, ArrayBuffer}
 
 class MazeGenerator {
   //see http://home.att.net/~srschmitt/script_maze_generator.html#the%20source%20code
 
+  case class Wall(x: Int, y: Int, dx: Int, dy: Int)
   case class Location(x: Int, y: int)
-  class Cell {
-    var top = true
-    var bottom = true
-    var left = true
-    var right = true
-
-    override def toString = "top=" + top + ", bottom=" + bottom + ", left=" + left + ", right=" + right
-  }
-  val grid = new Array[Array[Cell]](10, 10)
-  for (i <- 0 until 10)
+  val walls = new ArrayBuffer[Wall]
+  val allLocations = new HashSet[Location]
+  for (i <- 0 until 10) {
     for (j <- 0 until 10) {
-      grid(i)(j) = new Cell
-      println("i=" + i + ", j=" + j + ", cell=" + grid(i)(j))
+      walls += Wall(i, j, 1, 0)
+      walls += Wall(i, j, 0, 1)
+      allLocations += Location(i, j)
     }
-
+  }
+  val path = new ArrayBuffer[Location]
   val current = Location(0, 0)
+  val visited = new HashSet[Location]
 
-  def getAdjacent(location: Location) = {
+  while (visited != allLocations) {
+    visit()
+  }
 
+  def visit() = {
+    val neighbors = getAdjacent(current)
+    val remaining = neighbors -- visited
+
+    if (remaining.size > 0) {
+      //todo: pick at random
+      val nextLoc = remaining.toSeq(0)
+      walls -= getWallBetween(current, nextLoc)
+    } else {
+      backtrack()
+    }
+  }
+
+  def getWallBetween(a: Location, b: Location): Wall = {
+    new Wall(0, 0, 1, 0)
+  }
+
+  def backtrack() = {}
+
+  def getAdjacent(location: Location): HashSet[Location] = {
+    null
   }
 
   val adjacent = getAdjacent(current)
