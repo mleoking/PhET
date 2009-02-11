a0e639a91d4e9612ca772b83d3a108c7ca2e4bad
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-02-11 06:41:50 +0000

    add subtyping for model types

--- simulations-java/simulations/moving-man/scala-src/edu/colorado/phet/movingman/ladybug/AphidMazeModule.scala
+++ simulations-java/simulations/moving-man/scala-src/edu/colorado/phet/movingman/ladybug/AphidMazeModule.scala
@@ -3,8 +3,8 @@ package edu.colorado.phet.movingman.ladybug
 import canvas.LadybugCanvas
 import model.{LadybugModel, ScalaClock}
 
-class AphidMazeModule(clock: ScalaClock) extends LadybugModule(clock: ScalaClock, () => new AphidMazeModel,
-  (m: LadybugModule) => new AphidMazeCanvas(m.model, m.getVectorVisibilityModel, m.getPathVisibilityModel)) {
-
+class AphidMazeModule(clock: ScalaClock) extends LadybugModule[AphidMazeModel](clock: ScalaClock, () => new AphidMazeModel,
+  (m: LadybugModule[AphidMazeModel]) => new AphidMazeCanvas(m.model, m.getVectorVisibilityModel, m.getPathVisibilityModel)) {
+  println(model)
   setLadybugDraggable(false)
 }
