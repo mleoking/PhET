881ce21d1379acc9974b20e9d32fa40beefd7ce0
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-02-11 17:12:12 +0000

    improve maze generation

--- trunk/simulations-java/simulations/moving-man/scala-src/edu/colorado/phet/movingman/ladybug/aphidmaze/MazeGenerator.scala
+++ trunk/simulations-java/simulations/moving-man/scala-src/edu/colorado/phet/movingman/ladybug/aphidmaze/MazeGenerator.scala
@@ -1,28 +1,46 @@
 package edu.colorado.phet.movingman.ladybug.aphidmaze
 
 import _root_.scala.collection.mutable.{HashSet, ArrayBuffer}
+
 case class Wall(x: Int, y: Int, dx: Int, dy: Int)
 class MazeGenerator {
   //see http://home.att.net/~srschmitt/script_maze_generator.html#the%20source%20code
 
-
-  case class Location(x: Int, y: int)
+  case class Location(x: Int, y: int) {
+    def distance(other: Location) = {
+      Math.abs(x - other.x) + Math.abs(y - other.y)
+    }
+  }
   val walls = new ArrayBuffer[Wall]
   val allLocations = new HashSet[Location]
-  for (i <- 0 until 10) {
-    for (j <- 0 until 10) {
+  val maxX = 8;
+  val maxY = 8;
+  for (i <- -maxX to maxX) {
+    for (j <- -maxY to maxY) {
       walls += Wall(i, j, 1, 0)
       walls += Wall(i, j, 0, 1)
       allLocations += Location(i, j)
     }
   }
+
   val path = new ArrayBuffer[Location]
-  val current = Location(0, 0)
+  var current = Location(0, 0)
   val visited = new HashSet[Location]
 
-//  while (visited != allLocations) {
-//    visit()
-//  }
+  while (visited != allLocations) {
+    visit()
+  }
+
+  for (i <- -maxX to maxX) {
+    for (j <- -maxY to maxY) {
+      if (i == -maxX) {
+        walls += Wall(i, j, 0, 1)
+      }
+      if (i == maxX) {
+        walls += Wall(i + 1, j, 0, 1)
+      }
+    }
+  }
 
   def visit() = {
     val neighbors = getAdjacent(current)
@@ -32,19 +50,28 @@ class MazeGenerator {
       //todo: pick at random
       val nextLoc = remaining.toSeq(0)
       walls -= getWallBetween(current, nextLoc)
+      current = nextLoc
+      path += current
+      visited += current
     } else {
       backtrack()
     }
   }
 
   def getWallBetween(a: Location, b: Location): Wall = {
-    new Wall(0, 0, 1, 0)
+    new Wall(Math.min(a.x, b.x), Math.min(a.y, b.y), Math.abs(a.x - b.x), Math.abs(a.y - b.y))
   }
 
-  def backtrack() = {}
+  def backtrack() = {
+    path.remove(path.length - 1)
+    current = path(path.length - 1)
+  }
 
   def getAdjacent(location: Location): HashSet[Location] = {
-    null
+    val iterable = allLocations.filter(_.distance(location) <= 1.01)
+    val set = new HashSet[Location]
+    set ++ iterable
+    set
   }
 
   val adjacent = getAdjacent(current)
