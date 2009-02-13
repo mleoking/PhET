1153d4282ecb702c5329d9c88104943172106720
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-02-13 20:59:05 +0000

    Statistics-database: added recent messages to general-query

--- trunk/statistics/report/general-query.js
+++ trunk/statistics/report/general-query.js
@@ -72,9 +72,6 @@ var simCountsConstraints = ["sim_project", "sim_name", "sim_dev", "sim_type", "s
 
 function setupSimCounts() {
 	
-	fid("results").innerHTML = "";
-	fid("debug").innerHTML = "";
-	
 	var opts = fid("query_options");
 	var str = "";
 	
@@ -132,6 +129,25 @@ function setupSimCounts() {
 	build_order();
 }
 
+function setupRecentMessages() {
+	var str = "";
+	
+	str += "<div class='constraint'>";
+	str += "Sim type: <select name='recent_sim_type' id='recent_sim_type'>";
+	str += "<option value='all'>All</option>";
+	str += "<option value='java'>Java</option>";
+	str += "<option value='flash'>Flash</option>";
+	str += "</select>";
+	str += "</div>";
+	
+	str += "<div class='constraint'>";
+	str += "Quantity: <input type='text' name='count' size='5' maxlength='10' id='count' value='10'>";
+	str += "</input>";
+	str += "</div>";
+	
+	fid("query_options").innerHTML = str;
+}
+
 function build_order() {
 	var str = "";
 	str += "<span class='field'>Order by: </span><select name='ordercolumn' id='ordercolumn'>";
@@ -141,8 +157,6 @@ function build_order() {
 		str += "<option value='" + getValue("group") + "'>" + getValue("group") + "</option>";
 	}
 	str += "</select>";
-	
-	
 	
 	var old_order = getValue("order");
 	
@@ -164,11 +178,17 @@ function setupMessageCounts() {
 
 
 function query_change() {
+	
+	fid("results").innerHTML = "";
+	fid("debug").innerHTML = "";
+	
 	switch(getValue('query')) {
 		case "session_count":
 			setupSessionCounts(); break;
 		case "message_count":
 			setupMessageCounts(); break;
+		case "recent_messages":
+			setupRecentMessages(); break;
 	}
 }
 
@@ -194,6 +214,9 @@ function query_string() {
 			str += "desc:";
 		}
 		str += getValue("ordercolumn");
+	} else if(getValue("query") == "recent_messages") {
+		str += "&recent_sim_type=" + getValue('recent_sim_type');
+		str += "&count=" + getValue('count');
 	}
 	
 	return str;
@@ -221,147 +244,12 @@ onload = function() {
 		show_csv();
 	}
 	
-	setupSessionCounts();
+	//setupSessionCounts();
+	query_change();
 }
 
-
-
-
-/*
-
-function order_string() {
-	var col = document.query_info.ordercol.value;
-	var ord = document.query_info.order.value;
-	
-	if(col == "none") { return ""; }
-	
-	var str = "&order=";
-	
-	if(ord == "descending") {
-		str += "desc:";
-	}
-	
-	if(col == "type") {
-		return str + document.query_info.query.value;
-	} else if(col == "group" && document.query_info.group.value != "none") {
-		return str + document.query_info.group.value;
-	} else {
-		return "";
+onkeydown = function(code) {
+	if(code.which == 13) {
+		show_table();
 	}
 }
-
-function query_string() {
-	var str = "";
-	str += "query=" + document.query_info.query.value;
-	if(document.query_info.sim_project && document.query_info.sim_project.value != "any") {
-		str += "&sim_project=" + document.query_info.sim_project.value;
-	}
-	if(document.query_info.group.value != "none") {
-		str += "&group=" + document.query_info.group.value;
-	}
-	str += order_string();
-	document.getElementById("debug").innerHTML = str;
-	return str;
-}
-
-function show_below() {
-	loadHTML("query-table.php?" + query_string());
-	alert();
-}
-
-function download_csv() {
-	window.location.href = ("query-csv.php?" + query_string());
-}
-
-onload = function() {
-	//ahah("select-projects.php", "projects");
-}
-
-*/
-
-
-/*
-<?php
-	include("queries.php");
-	include("../db-auth.php");
-	include("../db-stats.php");
-	$link = setup_mysql();
-	
-?>
-<html>
-	<head>
-		<title>PhET Statistics - General Query</title>
-		<script type="text/javascript" src="general-query.js" />
-	</head>
-	<body>
-		<form name="query_info">
-			<p>
-				Query type:
-				<select name="query">
-					<optgroup label="Counts">
-						<option value="session_count">Session Counts</option>
-						<option value="message_count">Message Counts</option>
-					</optgroup>
-					<optgroup label="Other">
-					</optgroup>
-				</select>
-			</p>
-			<p>
-				Project
-				<span id='projects'>
-					(<a href="javascript:ahah('select-projects.php', 'projects')">specify</a>)
-				</span>
-			</p>
-			<p>
-				Group by:
-				<select name="group">
-					<option value="none">none</option>
-					<optgroup label="All sims">
-						<option value="week">Week</option>
-						<option value="month">Month</option>
-						<option value="version">Sim Version</option>
-						<option value="sim_locale">Sim Locale</option>
-						<option value="host_locale">Host Locale</option>
-						<option value="host_simplified_os">OS</option>
-						<option value="sim_type">Java / Flash</option>
-						<option value="sim_project">Project</option>
-						<option value="sim_name">Name</option>
-						<option value="project_name">Project, Name</option>
-						<option value="sim_deployment">Deployment</option>
-						<option value="sim_distribution_tag">Distribution Tag</option>
-					</optgroup>
-					<optgroup label="Java sims">
-					</optgroup>
-					<optgroup label="Flash sims">
-						<option value="host_flash_domain">Domain</option>
-						<option value="host_flash_os">OS</option>
-						<option value="host_flash_version">Flash Player</option>
-					</optgroup>
-				</select>
-			</p>
-			<p>
-				Order by:
-				<select name="ordercol">
-					<option value="none">none</option>
-					<option value="type">type</option>
-					<option value="group">group</option>
-				</select>
-				<select name="order">
-					<option value="ascending">ascending</option>
-					<option value="descending">descending</option>
-				</select>
-			</p>
-			
-			<p>
-				<button name="show_table" type="button" onclick="javascript:show_below()">show below</button>
-				<button name="show_csv" type="button" onclick="javascript:download_csv()">download csv</button>
-			</p>
-		</form>
-		<div id="debug">
-		</div>
-		<div id="content">
-		</div>
-	</body>
-</html>
-*/
-
--- trunk/statistics/report/queries.php
+++ trunk/statistics/report/queries.php
@@ -393,6 +393,143 @@
 					array_push($query, "SELECT DISTINCT IF(sim_type = 0, 'java', 'flash') AS sim_type FROM session WHERE sim_name = @sid; ");
 				} else { die("cannot have sim_type query without sim_name"); }
 				break;
+			case "recent_messages":
+				$count = ( $arr['count'] ? $arr['count'] : '10' );
+				if($arr['recent_sim_type'] == 'all') {
+					$querytext = <<<SES
+SELECT
+	session.id,
+	session.timestamp,
+	session.message_version,
+	session.server_svn_revision,
+	IF(session.sim_type = 0, 'java', 'flash') AS sim_type,
+	sim_project.name AS sim_project,
+	sim_name.name AS sim_name,
+	session.sim_major_version,
+	session.sim_minor_version,
+	session.sim_dev_version,
+	session.sim_svn_revision,
+	session.sim_version_timestamp,
+	session.sim_locale_language,
+	session.sim_locale_country,
+	session.sim_sessions_since,
+	session.sim_total_sessions,
+	deployment.name AS sim_deployment,
+	distribution_tag.name AS sim_distribution_tag,
+	session.sim_dev,
+	session.host_locale_language,
+	session.host_locale_country,
+	simplified_os.name AS host_simplified_os
+FROM session, sim_project, sim_name, deployment, distribution_tag, simplified_os
+WHERE (
+	session.sim_project = sim_project.id
+	AND session.sim_name = sim_name.id
+	AND session.sim_deployment = deployment.id
+	AND session.sim_distribution_tag = distribution_tag.id
+	AND session.host_simplified_os = simplified_os.id
+)
+ORDER BY session.id DESC LIMIT {$count};
+SES;
+				} else if($arr['recent_sim_type'] == 'flash') {
+					$querytext = <<<FLA
+SELECT
+	session.id,
+	session.timestamp,
+	session.message_version,
+	session.server_svn_revision,
+	sim_project.name AS sim_project,
+	sim_name.name AS sim_name,
+	session.sim_major_version,
+	session.sim_minor_version,
+	session.sim_dev_version,
+	session.sim_svn_revision,
+	session.sim_version_timestamp,
+	session.sim_locale_language,
+	session.sim_locale_country,
+	session.sim_sessions_since,
+	session.sim_total_sessions,
+	deployment.name AS sim_deployment,
+	distribution_tag.name AS sim_distribution_tag,
+	session.sim_dev,
+	session.host_locale_language,
+	session.host_locale_country,
+	simplified_os.name AS host_simplified_os,
+	flash_version_type.name AS host_flash_version_type,
+	session_flash_info.host_flash_version_major,
+	session_flash_info.host_flash_version_minor,
+	session_flash_info.host_flash_version_revision,
+	session_flash_info.host_flash_version_build,
+	session_flash_info.host_flash_time_offset,
+	session_flash_info.host_flash_accessibility,
+	flash_domain.name AS host_flash_domain,
+	flash_os.name AS host_flash_os
+FROM session, sim_project, sim_name, deployment, distribution_tag, simplified_os, session_flash_info, flash_version_type, flash_domain, flash_os
+WHERE (
+	session.sim_project = sim_project.id
+	AND session.sim_name = sim_name.id
+	AND session.sim_deployment = deployment.id
+	AND session.sim_distribution_tag = distribution_tag.id
+	AND session.host_simplified_os = simplified_os.id
+	AND session.id = session_flash_info.session_id
+	AND session_flash_info.host_flash_version_type = flash_version_type.id
+	AND session_flash_info.host_flash_domain = flash_domain.id
+	AND session_flash_info.host_flash_os = flash_os.id
+)
+ORDER BY session.id DESC LIMIT {$count};
+FLA;
+				} else if($arr['recent_sim_type'] == 'java') {
+					$querytext = <<<JAV
+SELECT
+	session.id,
+	session.timestamp,
+	session.message_version,
+	session.server_svn_revision,
+	sim_project.name AS sim_project,
+	sim_name.name AS sim_name,
+	session.sim_major_version,
+	session.sim_minor_version,
+	session.sim_dev_version,
+	session.sim_svn_revision,
+	session.sim_version_timestamp,
+	session.sim_locale_language,
+	session.sim_locale_country,
+	session.sim_sessions_since,
+	session.sim_total_sessions,
+	deployment.name AS sim_deployment,
+	distribution_tag.name AS sim_distribution_tag,
+	session.sim_dev,
+	session.host_locale_language,
+	session.host_locale_country,
+	simplified_os.name AS host_simplified_os,
+	java_os_name.name AS host_java_os_name,
+	java_os_version.name AS host_java_os_version,
+	java_os_arch.name AS host_java_os_arch,
+	java_vendor.name AS host_java_vendor,
+	session_java_info.host_java_version_major,
+	session_java_info.host_java_version_minor,
+	session_java_info.host_java_version_maintenance,
+	java_webstart_version.name AS host_java_webstart_version,
+	java_timezone.name AS host_java_timezone
+FROM session, sim_project, sim_name, deployment, distribution_tag, simplified_os, session_java_info, java_os_name, java_os_version, java_os_arch, java_vendor, java_webstart_version, java_timezone
+WHERE (
+	session.sim_project = sim_project.id
+	AND session.sim_name = sim_name.id
+	AND session.sim_deployment = deployment.id
+	AND session.sim_distribution_tag = distribution_tag.id
+	AND session.host_simplified_os = simplified_os.id
+	AND session.id = session_java_info.session_id
+	AND session_java_info.host_java_os_name = java_os_name.id
+	AND session_java_info.host_java_os_version = java_os_version.id
+	AND session_java_info.host_java_os_arch = java_os_arch.id
+	AND session_java_info.host_java_vendor = java_vendor.id
+	AND session_java_info.host_java_webstart_version = java_webstart_version.id
+	AND session_java_info.host_java_timezone = java_timezone.id
+)
+ORDER BY session.id DESC LIMIT {$count};
+JAV;
+				}
+				array_push($query, $querytext);
+				break;
 		}
 		
 		return $query;
