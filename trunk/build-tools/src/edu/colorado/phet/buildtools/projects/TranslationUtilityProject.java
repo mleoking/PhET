c27ddb9dbd58fd762714a99e5a8697049ca9cb17
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-02-13 22:50:27 +0000

    fix bug with updater project add untested translation-utility project

---
+++ trunk/build-tools/src/edu/colorado/phet/buildtools/projects/TranslationUtilityProject.java
@@ -0,0 +1,33 @@
+package edu.colorado.phet.buildtools.projects;
+
+import java.io.File;
+import java.io.IOException;
+
+import edu.colorado.phet.buildtools.projects.JavaProject;
+
+public class TranslationUtilityProject extends JavaProject {
+    public TranslationUtilityProject( File file ) throws IOException {
+        super( file );
+    }
+
+    public File getTrunk() {
+        // directory structure is trunk/util/translation-utility
+        return getProjectDir().getParentFile().getParentFile();
+    }
+
+    public void buildLaunchFiles( String URL, boolean dev ) {
+        System.out.println( "No launch files (JNLP) for " + getClass().getName() );
+    }
+
+    public String getAlternateMainClass() {
+        return "edu.colorado.phet.translationutility.TranslationUtility";
+    }
+
+    public File getDefaultDeployJar() {
+        return new File( getDeployDir(), "translation-utility.jar" );
+    }
+
+    public String getProdServerDeployPath() {
+        return "/web/htdocs/phet/phet-dist/translation-utility";
+    }
+}
