ac900833f1f280e3b5158e36546e4e973b1b26ea
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-02-13 06:59:08 +0000

    added a control structure for defineInvokeAndPass

--- simulations-java/simulations/moving-man/scala-src/edu/colorado/phet/movingman/ladybug/controlpanel/DigitalTimer.scala
+++ simulations-java/simulations/moving-man/scala-src/edu/colorado/phet/movingman/ladybug/controlpanel/DigitalTimer.scala
@@ -7,18 +7,28 @@ import java.text.DecimalFormat
 import model.LadybugModel
 import umd.cs.piccolo.nodes.{PPath, PText}
 import umd.cs.piccolo.PNode
+import LadybugUtil._
 
-class DigitalTimer(m:LadybugModel) extends PNode {
+class DigitalTimer(m: LadybugModel) extends PNode {
   val text = new PText("123.27 sec")
   text.setFont(new PhetFont(42))
 
-  val background = new PhetPPath(text.getFullBounds, Color.lightGray,new BasicStroke(1),Color.darkGray)
+  val background = new PhetPPath(text.getFullBounds, Color.lightGray, new BasicStroke(1), Color.darkGray)
   addChild(background)
   addChild(text)
 
+  /*
+    Non-Control Structure way of doing this, we are prone to forget:
+     (a) to call the first update or (b) to attach listener to the model
+     Using a control structure ensures everything will happen
   m.addListenerByName(update())
   update
-  def update()={
-    text.setText(new DecimalFormat("0.00").format(m.getTime)+" sec")
+  def update()= {
+  text.setText(new DecimalFormat("0.00").format(m.getTime)+" sec")
+   */
+
+  val update = defineInvokeAndPass(m.addListenerByName){
+    text.setText(new DecimalFormat("0.00").format(m.getTime) + " sec")
   }
+
 }
