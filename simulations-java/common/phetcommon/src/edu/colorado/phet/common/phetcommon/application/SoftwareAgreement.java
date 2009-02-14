de0648ebaf3fac905a0e9128fb5e1104ca2b3f06
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-02-14 16:46:06 +0000

    see #1140, read software-agreement.properties

--- simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/application/SoftwareAgreement.java
+++ simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/application/SoftwareAgreement.java
@@ -1,5 +1,10 @@
 package edu.colorado.phet.common.phetcommon.application;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import edu.colorado.phet.common.phetcommon.resources.PhetProperties;
+
 /**
  * Encapsulation of the "Software Use Agreement".
  *
@@ -7,10 +12,23 @@ package edu.colorado.phet.common.phetcommon.application;
  */
 public class SoftwareAgreement {
     
+    private static final String RESOURCE_NAME = "software-agreement.properties";
+    private static final String KEY_VERSION = "version";
+    private static final String KEY_CONTENT = "content";
+    private static final int DEFAULT_VERSION = -1;
+    
+    // singleton instance
     private static SoftwareAgreement instance;
     
+    private final int version;
+    private final String content;
+    
     // singleton
-    private SoftwareAgreement() {}
+    private SoftwareAgreement() {
+        PhetProperties p = readProperties();
+        version = p.getInt( KEY_VERSION, DEFAULT_VERSION );
+        content = p.getProperty( KEY_CONTENT );
+    }
     
     public static SoftwareAgreement getInstance() {
         if ( instance == null ) {
@@ -18,63 +36,39 @@ public class SoftwareAgreement {
         }
         return instance;
     }
-
+    
     /**
-     * Version numbers are monotonically increasing.
+     * Version number is a monotonically-increasing integer.
      * @return
      */
     public int getVersion() {
-        return 1; //TODO this should be read from the agreement file (property version= ?)
+        return version;
     }
     
     /**
-     * Content of the agreement is String that is in HTML format.
+     * Content of the agreement is an HTML string.
      * @return
      */
     public String getContent() {
-        // TODO this should be read from the agreement file (property content= ?)
-        return "<html>" +
-        "<b>PhET Software Agreement</b><br> " +
-        "<br>" +
-        "This agreement contains:<br>" +
-        "<ul>" +
-        "<li>License" +
-        "<li>Privacy" +
-        "<li>Disclaimers" +
-        "</ul>" +
-        "<br>" +
-        "<b>License</b><br>" +
-        "<br>" +
-        "blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah<br> " +
-        "blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah<br> " +
-        "blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah<br> " +
-        "blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah<br> " +
-        "blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah<br> " +
-        "blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah<br> " +
-        "blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah<br> " +
-        "<br>" +
-        "<br>" +
-        "<b>Privacy</b><br>" +
-        "<br>" +
-        "blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah<br> " +
-        "blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah<br> " +
-        "blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah<br> " +
-        "blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah<br> " +
-        "blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah<br> " +
-        "blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah<br> " +
-        "blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah<br> " +
-        "<br>" +
-        "<br>" +
-        "<b>Disclaimers</b><br>" +
-        "<br>" +
-        "blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah<br> " +
-        "blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah<br> " +
-        "blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah<br> " +
-        "blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah<br> " +
-        "blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah<br> " +
-        "blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah<br> " +
-        "blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah<br> " +
-        "<br>" +
-        "</html>";
+       return content;
+    }
+    
+    // TODO: use a standard resource loader
+    private PhetProperties readProperties() {
+        PhetProperties p = new PhetProperties();
+        InputStream inStream = Thread.currentThread().getContextClassLoader().getResourceAsStream( RESOURCE_NAME );
+        if ( inStream != null ) {
+            try {
+                p.load( inStream );
+            }
+            catch ( IOException e ) {
+                System.err.println( "exception reading software agreement: " + RESOURCE_NAME );
+                e.printStackTrace();
+            }
+        }
+        else {
+            System.err.println( "missing software agreement: " + RESOURCE_NAME );
+        }
+        return p;
     }
 }
