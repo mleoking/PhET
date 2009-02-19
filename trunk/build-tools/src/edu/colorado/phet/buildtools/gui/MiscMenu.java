002d3f4bf72f5648674b37896302077f092c4291
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-02-19 03:25:50 +0000

    improve output for batch

--- trunk/build-tools/src/edu/colorado/phet/buildtools/gui/MiscMenu.java
+++ trunk/build-tools/src/edu/colorado/phet/buildtools/gui/MiscMenu.java
@@ -2,7 +2,10 @@ package edu.colorado.phet.buildtools.gui;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashSet;
 
@@ -48,12 +51,35 @@ public class MiscMenu extends JMenu {
             public void actionPerformed( ActionEvent e ) {
                 LocalProperties localProperties = new LocalProperties( new File( trunk, "build-tools/build-local.properties" ) );
                 PhetProject[] projects = PhetProject.getAllProjects( trunk );
+                BufferedWriter bufferedWriter = null;
+                try {
+                    bufferedWriter = new BufferedWriter( new FileWriter( new File( trunk, "build-tools/report.txt" ) ) ){
+                        public void write( String str ) throws IOException {
+                            super.write( str );
+                            flush();
+                            System.out.println(str);
+                        }
+                    };
+                }
+                catch( IOException e1 ) {
+                    e1.printStackTrace();
+                }
                 for ( int i = 0; i < projects.length; i++ ) {
                     if ( projects[i].getName().startsWith( "test" ) ) {
                         BuildScript buildScript = new BuildScript( trunk, projects[i], new AuthenticationInfo( localProperties.getProperty( "svn.username" ), localProperties.getProperty( "svn.password" ) ), localProperties.getProperty( "browser" ) );
+                        final BufferedWriter bufferedWriter1 = bufferedWriter;
                         buildScript.addListener( new BuildScript.Listener() {
                             public void deployFinished( BuildScript buildScript, PhetProject project, String codebase ) {
-                                System.out.println( ">>>Deploy finished, project=" + project.getName() + ", codebase=" + codebase );
+//                                System.out.println( ">>>Deploy finished, project=" + project.getName() + ", codebase=" + codebase );
+                                try {
+                                    bufferedWriter1.write( project.getName() + ": " + codebase + "\n" );
+                                    for ( int k = 0; k < project.getSimulationNames().length; k++ ) {
+                                        bufferedWriter1.write( "\t" + project.getSimulation( project.getSimulationNames()[k] ).getTitle() + "\n" );
+                                    }
+                                }
+                                catch( IOException e1 ) {
+                                    e1.printStackTrace();
+                                }
                             }
                         } );
                         buildScript.deployDev( new AuthenticationInfo( localProperties.getProperty( "deploy." + "dev" + ".username" ), localProperties.getProperty( "deploy." + "dev" + ".password" ) ) );
