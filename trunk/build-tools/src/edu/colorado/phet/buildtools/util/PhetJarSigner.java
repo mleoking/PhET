248a221650838287f32169e479ee3a2737f46f83
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-02-23 22:29:35 +0000

    Initial working version of jar signer, tested on Windows XP only at this point.

--- trunk/build-tools/src/edu/colorado/phet/buildtools/util/PhetJarSigner.java
+++ trunk/build-tools/src/edu/colorado/phet/buildtools/util/PhetJarSigner.java
@@ -3,10 +3,11 @@
 package edu.colorado.phet.buildtools.util;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
-import org.apache.tools.ant.taskdefs.SignJar;
+import java.util.Properties;
 
 /**
  * This class can be used to sign JAR files, and is intended to be used within
@@ -15,41 +16,85 @@ import org.apache.tools.ant.taskdefs.SignJar;
  * @author John Blanco
  */
 public class PhetJarSigner {
+	
+    public static final String KEYSTORE_PROPERTY_NAME = "signing-config.keystore";
+    public static final String KEYSTORE_PASSWORD_PROPERTY_NAME = "signing-config.keystore-password";
+    public static final String CERTIFICATE_ALIAS_PROPERTY_NAME = "signing-config.alias";
 
-	String pathToJarsigner;
+	String pathToJarSigner;
 	String pathToConfigFile;
 	String pathToJarFile;
+	Properties signingConfigProperties;
 	
-	
-	
-
 	public PhetJarSigner( String pathToJarsigner, String pathToConfigFile, String pathToJarFile ) {
-		this.pathToJarsigner = pathToJarsigner;
+		this.pathToJarSigner = pathToJarsigner;
 		this.pathToConfigFile = pathToConfigFile;
 		this.pathToJarFile = pathToJarFile;
+		
 	}
 
 	public boolean signJar(){
 
-		System.out.println("signJar called.");
-        String s = null;
+		// Verify that the specified properties file exists and can be loaded.
+        
+		File signingConfigFile = new File( pathToConfigFile );
+		signingConfigProperties = new Properties();
+        if ( signingConfigFile.exists() ) {
+            try {
+            	signingConfigProperties.load( new FileInputStream( signingConfigFile ) );
+            }
+            catch( IOException e ) {
+            	System.err.println("Error: Unable to load signing configuration file, aborting.");
+                e.printStackTrace();
+                return false;
+            }
+        }
+        else{
+        	System.err.println("Error: Signing config file does not exist: " + pathToConfigFile);
+        }
+        
+        // Make sure the needed properties are present.
+        String keystoreFileName = signingConfigProperties.getProperty( KEYSTORE_PROPERTY_NAME );
+        String keystorePassword = signingConfigProperties.getProperty( KEYSTORE_PASSWORD_PROPERTY_NAME );
+        String alias = signingConfigProperties.getProperty( CERTIFICATE_ALIAS_PROPERTY_NAME );
+        
+        if ((keystoreFileName == null) ||
+        	(keystorePassword == null) ||
+        	(alias == null)){
+        	
+        	System.err.println("Error: Missing one or more properties needed for signing, aborting.");
+        	return false;
+        }
+        
+        // Make sure that the specified JAR file can be located.
+		File jarFile = new File( pathToJarFile );
+		if ( !jarFile.exists() ){
+			System.err.println("Error: Could not locate specified JAR file, aborting.");
+			return false;
+		}
+        
+        // Create and execute the signing command.
 
+        String signingCommand = pathToJarSigner + " -keystore " + keystoreFileName + " -storetype pkcs12 " +
+ 	    	" -storepass " + keystorePassword + " \"" + pathToJarFile + "\" " + alias;
+        
+        System.out.println("About to execute signing command:");
+        System.out.println(signingCommand);
+        
         try {
+        	// Execute the signing command.
+            Process p = Runtime.getRuntime().exec( signingCommand );
             
-            Process p = Runtime.getRuntime().exec( pathToJarsigner );
-            
+            // Obtain the standard and error output.
             BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
-
             BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
 
-            // read the output from the command
-            
-            System.out.println("Standard output from signing:\n");
+            // Output the results echoed by the execution of the command.
+            String s = null;
+            System.out.println("Standard output from signing (if any):\n");
             while ((s = stdInput.readLine()) != null) {
                 System.out.println(s);
             }
-            
-            // read any errors from the attempted command
 
             System.out.println("Standard error from signing (if any):\n");
             while ((s = stdError.readLine()) != null) {
@@ -62,12 +107,46 @@ public class PhetJarSigner {
             return false;
         }
         
+        // Create and execute the verification command.
+        
+        String verifyCommand = pathToJarSigner + " -verify " + " -certs " + pathToJarFile;
+    
+        System.out.println("About to execute the verification command:");
+        System.out.println(verifyCommand);
+    
+	    try {
+	    	// Execute the signing command.
+	        Process p = Runtime.getRuntime().exec( verifyCommand );
+	        
+	        // Obtain the standard and error output.
+	        BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+	        BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+	
+	        // Output the results echoed by the execution of the command.
+	        String s = null;
+	        System.out.println("Standard output from verify:\n");
+	        while ((s = stdInput.readLine()) != null) {
+	            System.out.println(s);
+	        }
+	
+	        System.out.println("Standard error from verify (if any):\n");
+	        while ((s = stdError.readLine()) != null) {
+	            System.out.println(s);
+	        }
+	    }
+	    catch (IOException e) {
+	        System.out.println("Exception while attempting to sign JAR:");
+	        e.printStackTrace();
+	        return false;
+	    }
+        
+        
         return true;
 	}
 
 	public static void main(String[] args) {
 		if (args.length != 3){
-			System.err.println("JAR Signer Test: Not enough arguments, aborting.");
+			System.err.println("PhET JAR Signer: Not enough arguments, aborting.");
 			System.exit( -1 );
 		}
 		
