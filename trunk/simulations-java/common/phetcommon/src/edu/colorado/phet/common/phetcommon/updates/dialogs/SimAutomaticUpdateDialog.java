66a3edd62f715137b966d6c78464b4bc25172733
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-02-26 18:00:00 +0000

    Added main to SimAutomaticUpdateDialog

--- trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/updates/dialogs/SimAutomaticUpdateDialog.java
+++ trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/updates/dialogs/SimAutomaticUpdateDialog.java
@@ -2,12 +2,10 @@ package edu.colorado.phet.common.phetcommon.updates.dialogs;
 
 import java.awt.Frame;
 
-import javax.swing.Box;
-import javax.swing.JComponent;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 import edu.colorado.phet.common.phetcommon.application.ISimInfo;
+import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
 import edu.colorado.phet.common.phetcommon.resources.PhetCommonResources;
 import edu.colorado.phet.common.phetcommon.resources.PhetVersion;
 import edu.colorado.phet.common.phetcommon.updates.IAskMeLaterStrategy;
@@ -50,4 +48,51 @@ public class SimAutomaticUpdateDialog extends SimAbstractUpdateDialog {
         return new JLabel( preferencesHTML );
     }
 
+    public static void main(String[] args) {
+        System.out.println( "I'm running!" );
+        PhetApplicationConfig config = new PhetApplicationConfig( args, "moving-man" );
+        final SimAutomaticUpdateDialog dialog = new SimAutomaticUpdateDialog( new JFrame(), config, new PhetVersion("1", "00", "05", "30000", "1234567890"), new IAskMeLaterStrategy() {
+            public void setStartTime(long time) {
+                //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            public long getStartTime() {
+                return 0;  //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            public void setDuration(long duration) {
+                //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            public long getDuration() {
+                return 0;  //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            public boolean isDurationExceeded() {
+                return true;  //To change body of implemented methods use File | Settings | File Templates.
+            }
+        }, new IVersionSkipper() {
+            public void setSkippedVersion(int skipVersion) {
+                //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            public int getSkippedVersion() {
+                return 0;  //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            public boolean isSkipped(int version) {
+                return false;  //To change body of implemented methods use File | Settings | File Templates.
+            }
+        });
+
+        dialog.setVisible( true );
+
+        SwingUtilities.invokeLater( new Runnable() {
+            public void run() {
+                dialog.setSize( 800, 800 );
+            }
+        });
+
+
+    }
 }
