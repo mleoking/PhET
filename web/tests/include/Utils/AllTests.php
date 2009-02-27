585c2baa19c2f451f26b27ae4b7608b211b30f02
dano <dano@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-02-27 01:24:08 +0000

    Updated tests supporting locale fixes and refactor.  Goes with R29080

--- web/tests/include/Utils/AllTests.php
+++ web/tests/include/Utils/AllTests.php
@@ -1,17 +1,19 @@
 <?php
 
-  //error_reporting(E_ALL | E_STRICT);
+require_once(dirname(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR . 'test_global.php');
 
-require_once 'PHPUnit/Framework/TestSuite.php';
-require_once 'PHPUnit/Extensions/PhptTestSuite.php';
+require_once('PHPUnit/Framework/TestSuite.php');
+require_once('PHPUnit/Extensions/PhptTestSuite.php');
 
-require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'UpdateUtilsTest.php';
-require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'ValidateTest.php';
+require_once(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'LocaleTest.php');
+require_once(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'UpdateUtilsTest.php');
+require_once(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'ValidateTest.php');
 
 class include_utils_AllTests {
   public static function suite() {
     $suite = new PHPUnit_Framework_TestSuite('include_utils');
     
+    $suite->addTestSuite('LocaleTest');
     $suite->addTestSuite('UpdateUtilsTest');
     $suite->addTestSuite('ValidateTest');
     
