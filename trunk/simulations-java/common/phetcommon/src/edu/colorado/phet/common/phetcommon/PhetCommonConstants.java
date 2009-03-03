1355e7a6b8fec6de64dc035c86bc1947d6675e0d
dano <dano@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-03-03 20:41:18 +0000

    Moved service type scripts to the services/ directory.  Renamed sim-redirect.php to sim-website-redirect.php.  Updated PhetCommonConstants.java to reflect these changes.

--- trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/PhetCommonConstants.java
+++ trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/PhetCommonConstants.java
@@ -13,7 +13,7 @@ public class PhetCommonConstants {
     public static final String PHET_LICENSE_URL = PHET_HOME_URL + "/about/licensing.php";
     
     // services
-    public static final String SIM_REDIRECT_URL = PHET_HOME_URL + "/simulations/sim-redirect.php";
+    public static final String SIM_REDIRECT_URL = PHET_HOME_URL + "/services/sim-redirect.php";
     public static final String SIM_JAR_REDIRECT_URL = PHET_HOME_URL + "/services/sim-jar-redirect.php";
     public static final String PHET_INFO_URL = PHET_HOME_URL + "/services/phet-info.php";
     public static final String STATISTICS_SERVICE_URL = PHET_HOME_URL + "/statistics/submit_message.php";
