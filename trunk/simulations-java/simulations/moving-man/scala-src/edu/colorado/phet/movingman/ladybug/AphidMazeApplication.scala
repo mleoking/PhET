3facbd9b91d9a7872774eea4d043db7792be30e4
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-03-04 05:59:29 +0000

    cleanup imports

--- trunk/simulations-java/simulations/moving-man/scala-src/edu/colorado/phet/movingman/ladybug/AphidMazeApplication.scala
+++ trunk/simulations-java/simulations/moving-man/scala-src/edu/colorado/phet/movingman/ladybug/AphidMazeApplication.scala
@@ -1,24 +1,8 @@
 package edu.colorado.phet.movingman.ladybug
 
 import aphidmaze.AphidMazeModule
-import edu.colorado.phet.common.phetcommon.application.ApplicationConstructor
-import edu.colorado.phet.common.phetcommon.application.Module
-import edu.colorado.phet.common.phetcommon.application.PhetApplication
-import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig
-import edu.colorado.phet.common.phetcommon.application.PhetApplicationLauncher
-import edu.colorado.phet.common.phetcommon.model.clock.ClockAdapter
-import edu.colorado.phet.common.phetcommon.model.clock.ClockEvent
-import edu.colorado.phet.common.phetcommon.model.clock.ClockListener
-import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock
-import edu.colorado.phet.common.piccolophet.PhetPCanvas
-import edu.umd.cs.piccolo.nodes.PText
-import edu.umd.cs.piccolo.PNode
-import java.awt.Color
-import java.awt.Font
-import java.util.Date
-import javax.swing.JLabel
-import model.ScalaClock
 import scalacommon.ScalaApplicationLauncher
+import model.ScalaClock
 
 //aphid maze is a tab in ladybug 2d, just an application during development to facilitate deployment and testing
 object AphidMazeApplication {
