12a39f7da9ae0a8f8a4c72eb7d67f7c3e445e395
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-03-04 05:57:59 +0000

    add scala-common and generalize ScalaApplicationLauncher

--- trunk/simulations-java/simulations/moving-man/scala-src/edu/colorado/phet/movingman/ladybug/AphidMazeApplication.scala
+++ trunk/simulations-java/simulations/moving-man/scala-src/edu/colorado/phet/movingman/ladybug/AphidMazeApplication.scala
@@ -18,10 +18,11 @@ import java.awt.Font
 import java.util.Date
 import javax.swing.JLabel
 import model.ScalaClock
+import scalacommon.ScalaApplicationLauncher
 
 //aphid maze is a tab in ladybug 2d, just an application during development to facilitate deployment and testing
 object AphidMazeApplication {
   def main(args: Array[String]) = {
-    ScalaApplication.main(args, "moving-man", "ladybug-2d", new AphidMazeModule(new ScalaClock(30, 30 / 1000.0)))
+    ScalaApplicationLauncher.launchApplication(args, "moving-man", "ladybug-2d", ()=>new AphidMazeModule(new ScalaClock(30, 30 / 1000.0)))
   }
 }
---
+++ trunk/simulations-java/simulations/the-ramp/scala-src/edu/colorado/phet/therampscala/RampApplication.scala
@@ -0,0 +1,5 @@
+package edu.colorado.phet.therampscala
+
+class RampApplication{
+  
+}
