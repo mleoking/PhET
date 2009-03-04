f9d2f9801cbd5274a0f985536fb4870a2a612b5d
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-03-04 06:11:16 +0000

    add module skeleton

--- simulations-java/simulations/the-ramp/scala-src/edu/colorado/phet/therampscala/RampApplication.scala
+++ simulations-java/simulations/the-ramp/scala-src/edu/colorado/phet/therampscala/RampApplication.scala
@@ -1,10 +1,20 @@
 package edu.colorado.phet.therampscala
 
+import common.phetcommon.application.Module
+import common.phetcommon.model.BaseModel
+import common.piccolophet.PhetPCanvas
 import movingman.ladybug.LadybugModule
 import scalacommon.{ScalaApplicationLauncher, ScalaClock}
 import movingman.ladybug.model.LadybugModel
-class RampApplication{
+
+class RampModel
+class RampModule(clock: ScalaClock) extends Module("Ramp", clock) {
+  val model = new RampModel
+  setSimulationPanel(new PhetPCanvas)
+  setModel(new BaseModel)
+}
+object RampApplication {
   def main(args: Array[String]) = {
-    ScalaApplicationLauncher.launchApplication(args, "moving-man", "ladybug-2d", () => new LadybugModule[LadybugModel](new ScalaClock(30, 30 / 1000.0)))
+    ScalaApplicationLauncher.launchApplication(args, "the-ramp", "the-ramp", () => new RampModule(new ScalaClock(30, 30 / 1000.0)))
   }
 }
