f37df1a22ff0af409e5405092df2641c176ec37d
dano <dano@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-03-05 18:20:29 +0000

    Updated and moved sim-jar-redirect.php tests

--- web/tests/simulations/AllTests.php
+++ web/tests/simulations/AllTests.php
@@ -5,15 +5,13 @@
 require_once 'PHPUnit/Framework/TestSuite.php';
 require_once 'PHPUnit/Extensions/PhptTestSuite.php';
 
+
 $root = dirname(__FILE__) . DIRECTORY_SEPARATOR;
-require_once($root . 'simJarRedirectTest.php');
 $root = NULL;
 
 class simulation_AllTests {
   public static function suite() {
     $suite = new PHPUnit_Framework_TestSuite('simulation');
-
-    $suite->addTestSuite('simJarRedirectTest');
 
     return $suite;
   }
