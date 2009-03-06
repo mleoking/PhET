e5c7c5ac5ffd8b342e1f1168a4b42ec582a937b3
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-03-06 22:29:21 +0000

    Initial checkin of utility for looking for orphan strings (i.e. strings that exist in the localization properties files but are not found in the java code).  Parts of this are not yet functional.

---
+++ trunk/util/localization-strings/src/edu/colorado/phet/localizationstrings/util/FileFinder.java
@@ -0,0 +1,21 @@
+package edu.colorado.phet.localizationstrings.util;
+
+import java.io.File;
+import java.util.*;
+
+public class FileFinder {
+	
+	public static void findFiles(File directory, WildCardFileFilter filter, ArrayList fileList) {
+		directory.setReadOnly();  // Safety first.
+		File[] filesToAdd = directory.listFiles( filter );
+		for (int i=0; i<filesToAdd.length; i++){
+			fileList.add(filesToAdd[i]);
+		}
+		File[] files = directory.listFiles();
+		for(int i = 0; i < files.length; i++) {
+			if(files[i].isDirectory()){
+				findFiles(files[i], filter, fileList);
+			}
+		}
+	}
+}
