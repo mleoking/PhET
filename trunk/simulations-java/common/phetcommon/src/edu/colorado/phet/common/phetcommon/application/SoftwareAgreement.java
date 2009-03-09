87783c8f3c063a40f796ee63b986e1ad45467dd8
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-03-09 16:17:05 +0000

    read license info from file generated by build process, see #1407

--- trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/application/SoftwareAgreement.java
+++ trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/application/SoftwareAgreement.java
@@ -2,6 +2,8 @@ package edu.colorado.phet.common.phetcommon.application;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 import edu.colorado.phet.common.phetcommon.resources.PhetProperties;
 
@@ -11,32 +13,50 @@ import edu.colorado.phet.common.phetcommon.resources.PhetProperties;
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
 public class SoftwareAgreement {
-    
-    private static final String RESOURCE_NAME = "software-agreement.properties";
+
+    private static final String RESOURCE_NAME = "phetcommon/software-agreement/software-agreement.properties";
+    private static final String HTML_RESOURCE_NAME = "phetcommon/software-agreement/software-agreement.htm";
     private static final String KEY_VERSION = "version";
-    private static final String KEY_CONTENT = "content";
     private static final int DEFAULT_VERSION = -1;
-    
+
     // singleton instance
     private static SoftwareAgreement instance;
-    
+
     private final int version;
-    private final String content;
-    
+    private String content;
+
     // singleton
     private SoftwareAgreement() {
         PhetProperties p = readProperties();
         version = p.getInt( KEY_VERSION, DEFAULT_VERSION );
-        content = p.getProperty( KEY_CONTENT );
+//        content = p.getProperty( KEY_CONTENT );
+        try {
+            content=readContent();
+        }
+        catch( IOException e ) {
+            e.printStackTrace();
+        }
     }
-    
+
+    private String readContent() throws IOException {
+        InputStream inStream = Thread.currentThread().getContextClassLoader().getResourceAsStream( HTML_RESOURCE_NAME );
+        BufferedReader bufferedReader=new BufferedReader( new InputStreamReader( inStream));
+        String s="";
+        String line=bufferedReader.readLine();
+        while(line!=null){
+            s+=line+"\n";
+            line=bufferedReader.readLine();
+        }
+        return s;
+    }
+
     public static SoftwareAgreement getInstance() {
         if ( instance == null ) {
             instance = new SoftwareAgreement();
         }
         return instance;
     }
-    
+
     /**
      * Version number is a monotonically-increasing integer.
      * @return
@@ -44,7 +64,7 @@ public class SoftwareAgreement {
     public int getVersion() {
         return version;
     }
-    
+
     /**
      * Content of the agreement is an HTML string.
      * @return
