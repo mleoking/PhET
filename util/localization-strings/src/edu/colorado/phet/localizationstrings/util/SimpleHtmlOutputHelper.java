62cfb0135f1f2a0ae73372368bd4c15bcf62f07e
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-03-11 21:28:37 +0000

    Simple class for helping with generation of HTML reports.

---
+++ util/localization-strings/src/edu/colorado/phet/localizationstrings/util/SimpleHtmlOutputHelper.java
@@ -0,0 +1,38 @@
+package edu.colorado.phet.localizationstrings.util;
+
+public class SimpleHtmlOutputHelper {
+	
+	public static void printHtmlHeader(){
+		System.out.println("<html>");
+		System.out.println("<body>");
+	}
+	
+	public static void printHtmlClose(){
+		System.out.println("</body>");
+		System.out.println("</html>");
+	}
+	
+	public static void printHtmlHeading(String text, int level){
+		if ((level > 5) || (level < 1)){
+			System.err.println("Invalid header level: " + level);
+			level = 1;
+		}
+		System.out.println("<h" + level + ">" + text + "</h" + level + ">");
+	}
+	
+	public static void printHtmlStartList(){
+		System.out.println("<ul>");
+	}
+
+	public static void printHtmlListItem(String text){
+		System.out.println("<li>" + text + "</li>");
+	}
+	
+	public static void printHtmlEndList(){
+		System.out.println("</ul>");
+	}
+
+	public static void printHtmlBreak(){
+		System.out.println("<br>");
+	}
+}
