3abe6a346d3f2fa5f63a581d3cc939f21789920f
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-03-13 17:36:05 +0000

    improve reporting

--- util/testing/src/edu/colorado/phet/testing/CheckTestCoverage.java
+++ util/testing/src/edu/colorado/phet/testing/CheckTestCoverage.java
@@ -2,9 +2,10 @@ package edu.colorado.phet.testing;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
 
 public class CheckTestCoverage {
 
@@ -16,25 +17,33 @@ public class CheckTestCoverage {
         //for now, just download manually
 
         Spreadsheet spreadsheet = Spreadsheet.load( new File( "C:\\Users\\Owner\\Desktop\\iom.csv" ) );
-        System.out.println( "Test IDs = " + Arrays.asList( spreadsheet.listValues( "Test ID" ) ) );
+        List testIDs = Arrays.asList( spreadsheet.listValues( "Test ID" ) );
+        HashSet testIDSet = new HashSet( testIDs );
+        System.out.println( "Test IDs = " + testIDs );
         System.out.println( "Testers = " + spreadsheet.getUniqueValues( "Tester" ) );
         System.out.println( "Scenarios = " + spreadsheet.getUniqueValues( "Scenario" ) );
 
-        showData( "Test Results", "PASS", spreadsheet );
-        showData( "Test Results", "FAIL", spreadsheet );
-
-    }
-
-    private static void showData( String key, String value, Spreadsheet spreadsheet ) {
-        Entry[] matches = spreadsheet.getMatches( key, value );
-        System.out.println( value + " [" + matches.length + "]: " + Arrays.asList( getValues( matches, "Test ID" ) ) );
-    }
-
-    private static String[] getValues( Entry[] matches, String key ) {
-        HashSet collection = new HashSet();
-        for ( int i = 0; i < matches.length; i++ ) {
-            collection.add( matches[i].getValue( key ) );
+        for ( Iterator iterator = testIDSet.iterator(); iterator.hasNext(); ) {
+            String s = (String) iterator.next();
+            analyzeTest( spreadsheet, s );
         }
-        return (String[]) collection.toArray( new String[collection.size()] );
     }
+
+    private static void analyzeTest( Spreadsheet spreadsheet, final String testID ) {
+        Spreadsheet entries = spreadsheet.getMatches( new Spreadsheet.Matcher() {
+            public boolean matches( Entry e ) {
+                return e.getValue( "Test ID" ).equals( testID );
+            }
+        } );
+        System.out.println( testID + "\n\tnumTests=" + entries.size() );
+        Spreadsheet macTests = entries.getMatches( new Spreadsheet.Matcher() {
+            public boolean matches( Entry e ) {
+                return e.getValue( "OS" ).toLowerCase().indexOf( "mac" ) >= 0;
+            }
+        } );
+        System.out.println( "\tMac tests: " + macTests.size() );
+
+    }
+
+
 }
