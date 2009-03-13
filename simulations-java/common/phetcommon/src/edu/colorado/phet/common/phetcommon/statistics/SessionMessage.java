469590b18eb6145fa3cbd4c28f33544faba02ae6
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-03-13 02:08:56 +0000

    see #1466, Common.statistics.* localization keys appear in orphaned-string report

--- simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/statistics/SessionMessage.java
+++ simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/statistics/SessionMessage.java
@@ -53,43 +53,43 @@ public class SessionMessage extends StatisticsMessage {
         StatisticsMessageField[] fields = new StatisticsMessageField[]{
                 
                 // Sim data
-                new StatisticsMessageField( "sim_project", simInfo.getProjectName() ),
-                new StatisticsMessageField( "sim_name", simInfo.getFlavor() ),
-                new StatisticsMessageField( "sim_major_version", simInfo.getVersion().getMajor() ),
-                new StatisticsMessageField( "sim_minor_version", simInfo.getVersion().getMinor() ),
-                new StatisticsMessageField( "sim_dev_version", simInfo.getVersion().getDev() ),
-                new StatisticsMessageField( "sim_svn_revision", simInfo.getVersion().getRevision() ),
-                new StatisticsMessageField( "sim_version_timestamp", simInfo.getVersion().getTimestampSeconds() ),
-                new StatisticsMessageField( "sim_distribution_tag", simInfo.getDistributionTag() ),
-                new StatisticsMessageField( "sim_locale_language", PhetResources.readLocale().getLanguage() ),
-                new StatisticsMessageField( "sim_locale_country", PhetResources.readLocale().getCountry() ),
-                new StatisticsMessageField( "sim_deployment", DeploymentScenario.getInstance().toString() ),
-                new StatisticsMessageField( "sim_dev", simInfo.isDev() + "" ),
-                new StatisticsMessageField( "sim_total_sessions", SessionCounter.getInstance().getCount() ),
-                new StatisticsMessageField( "sim_sessions_since", SessionCounter.getInstance().getCountSince() ),
+                new StatisticsMessageField( "Common.statistics.sim_project", "sim_project", simInfo.getProjectName() ),
+                new StatisticsMessageField( "Common.statistics.sim_name", "sim_name", simInfo.getFlavor() ),
+                new StatisticsMessageField( "Common.statistics.sim_major_version", "sim_major_version", simInfo.getVersion().getMajor() ),
+                new StatisticsMessageField( "Common.statistics.sim_minor_version", "sim_minor_version", simInfo.getVersion().getMinor() ),
+                new StatisticsMessageField( "Common.statistics.sim_dev_version", "sim_dev_version", simInfo.getVersion().getDev() ),
+                new StatisticsMessageField( "Common.statistics.sim_svn_revision", "sim_svn_revision", simInfo.getVersion().getRevision() ),
+                new StatisticsMessageField( "Common.statistics.sim_version_timestamp", "sim_version_timestamp", simInfo.getVersion().getTimestampSeconds() ),
+                new StatisticsMessageField( "Common.statistics.sim_distribution_tag", "sim_distribution_tag", simInfo.getDistributionTag() ),
+                new StatisticsMessageField( "Common.statistics.sim_locale_language", "sim_locale_language", PhetResources.readLocale().getLanguage() ),
+                new StatisticsMessageField( "Common.statistics.sim_locale_country", "sim_locale_country", PhetResources.readLocale().getCountry() ),
+                new StatisticsMessageField( "Common.statistics.sim_deployment", "sim_deployment", DeploymentScenario.getInstance().toString() ),
+                new StatisticsMessageField( "Common.statistics.sim_dev", "sim_dev", simInfo.isDev() + "" ),
+                new StatisticsMessageField( "Common.statistics.sim_total_sessions", "sim_total_sessions", SessionCounter.getInstance().getCount() ),
+                new StatisticsMessageField( "Common.statistics.sim_sessions_since", "sim_sessions_since", SessionCounter.getInstance().getCountSince() ),
                 
                 // Host data
-                new StatisticsMessageField.SystemProperty( "host_os_name", "os.name" ),
-                new StatisticsMessageField.SystemProperty( "host_os_version", "os.version" ),
-                new StatisticsMessageField.SystemProperty( "host_os_arch", "os.arch" ),
-                new StatisticsMessageField.SystemProperty( "host_java_vendor", "java.vendor" ),
-                new StatisticsMessageField( "host_java_version_major", jre.getMajorNumber() ),
-                new StatisticsMessageField( "host_java_version_minor", jre.getMinorNumber() ),
-                new StatisticsMessageField( "host_java_version_maintenance", jre.getMaintenanceNumber() ),
-                new StatisticsMessageField.SystemProperty( "host_java_webstart_version", "javawebstart.version" ),
-                new StatisticsMessageField.SystemProperty( "host_locale_language", "user.language" ),
-                new StatisticsMessageField.SystemProperty( "host_locale_country", "user.country" ),
-                new StatisticsMessageField.SystemProperty( "host_java_timezone", "user.timezone" ),
+                new StatisticsMessageField.SystemProperty( "Common.statistics.host_os_name", "host_os_name", "os.name" ),
+                new StatisticsMessageField.SystemProperty( "Common.statistics.host_os_version", "host_os_version", "os.version" ),
+                new StatisticsMessageField.SystemProperty( "Common.statistics.host_os_arch", "host_os_arch", "os.arch" ),
+                new StatisticsMessageField.SystemProperty( "Common.statistics.host_java_vendor", "host_java_vendor", "java.vendor" ),
+                new StatisticsMessageField( "Common.statistics.host_java_version_major", "host_java_version_major", jre.getMajorNumber() ),
+                new StatisticsMessageField( "Common.statistics.host_java_version_minor", "host_java_version_minor", jre.getMinorNumber() ),
+                new StatisticsMessageField( "Common.statistics.host_java_version_maintenance", "host_java_version_maintenance", jre.getMaintenanceNumber() ),
+                new StatisticsMessageField.SystemProperty( "Common.statistics.host_java_webstart_version", "host_java_webstart_version", "javawebstart.version" ),
+                new StatisticsMessageField.SystemProperty( "Common.statistics.host_locale_language", "host_locale_language", "user.language" ),
+                new StatisticsMessageField.SystemProperty( "Common.statistics.host_locale_country", "host_locale_country", "user.country" ),
+                new StatisticsMessageField.SystemProperty( "Common.statistics.host_java_timezone", "host_java_timezone", "user.timezone" ),
                 
                 // User data
-                new StatisticsMessageField( "user_preference_file_creation_time", PhetPreferences.getInstance().getPreferencesFileCreationTime() ),
-                new StatisticsMessageField( "user_total_sessions", SessionCounter.getInstance().getTotal() ),
+                new StatisticsMessageField( "Common.statistics.user_preference_file_creation_time", "user_preference_file_creation_time", PhetPreferences.getInstance().getPreferencesFileCreationTime() ),
+                new StatisticsMessageField( "Common.statistics.user_total_sessions", "user_total_sessions", SessionCounter.getInstance().getTotal() ),
         };
         super.addFields( fields );
         
         if ( DeploymentScenario.getInstance() == DeploymentScenario.PHET_INSTALLATION ) {
             PhetInstallation p = PhetInstallation.getInstance();
-            addField( new StatisticsMessageField( "user_installation_timestamp", p.getInstallationTimestamp() ) );
+            addField( new StatisticsMessageField( "Common.statistics.user_installation_timestamp", "user_installation_timestamp", p.getInstallationTimestamp() ) );
         }
     }
 
