157323a2c099d3e92bd18a7d2a0fa419d196537d
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-03-19 19:58:45 +0000

    see #1372, test app refinements

--- trunk/simulations-java/simulations/ideal-gas/src/edu/colorado/phet/idealgas/test/TestJSpinnerApplication.java
+++ trunk/simulations-java/simulations/ideal-gas/src/edu/colorado/phet/idealgas/test/TestJSpinnerApplication.java
@@ -1,12 +1,10 @@
 
 package edu.colorado.phet.idealgas.test;
 
-import javax.swing.JFrame;
-import javax.swing.JSpinner;
-import javax.swing.JTextField;
-import javax.swing.WindowConstants;
+import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+import javax.swing.plaf.metal.MetalLookAndFeel;
 
 /**
  * This test isolates problem #1372, JSpinners are broken on Mac OS.
@@ -14,12 +12,13 @@ import javax.swing.event.ChangeListener;
  */
 public class TestJSpinnerApplication {
 
-    public static void main( final String[] args ) {
-//        new JSpinner();// if this is done first, no problem
+    public static void main( final String[] args ) throws UnsupportedLookAndFeelException {
+//        UIManager.setLookAndFeel( new MetalLookAndFeel() ); // workaround: problem is in Aqua L&F
+//        new JSpinner();// workaround
         new JTextField() {
             public String getUIClassID() {
                 return super.getUIClassID();
-//                return "TextAreaUI"; // if we use this UI class id, no problem
+//                return "TextAreaUI"; // workaround
             }
         };
         final JSpinner spinner = new JSpinner();
