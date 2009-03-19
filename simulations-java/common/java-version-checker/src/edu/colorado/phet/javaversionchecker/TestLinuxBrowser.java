a062439ca4a9117d7d769d4ef7f475ff69f5d045
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-03-19 19:33:06 +0000

    removed main class test for linux browser

--- simulations-java/common/java-version-checker/src/edu/colorado/phet/javaversionchecker/TestLinuxBrowser.java
+++
@@ -1,7 +0,0 @@
-package edu.colorado.phet.javaversionchecker;
-
-public class TestLinuxBrowser {
-    public static void main( String[] args ) throws Exception {
-        JavaVersionChecker.launchBrowserOnLinux( "http://www.google.com" );
-    }
-}
