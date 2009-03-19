953c0e0064269fb263d274b7e653b01193f6524a
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-03-19 17:23:52 +0000

    added test for linux

---
+++ trunk/simulations-java/common/java-version-checker/src/edu/colorado/phet/javaversionchecker/TestLinuxBrowser.java
@@ -0,0 +1,7 @@
+package edu.colorado.phet.javaversionchecker;
+
+public class TestLinuxBrowser {
+    public static void main( String[] args ) throws Exception {
+        JavaVersionChecker.launchBrowserOnLinux( "http://www.google.com" );
+    }
+}
