f7e53bd1a5a406cdfff8cf387721255c32c8060a
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-03-19 02:40:25 +0000

    Create window at 1280x960 if screen is big enough, otherwise start maximized, see #1474

--- simulations-java/simulations/wave-interference/src/edu/colorado/phet/waveinterference/WaveInterferenceConstants.java
+++ simulations-java/simulations/wave-interference/src/edu/colorado/phet/waveinterference/WaveInterferenceConstants.java
@@ -1,11 +1,34 @@
 package edu.colorado.phet.waveinterference;
 
+import java.awt.*;
+
+import javax.swing.*;
+
 import edu.colorado.phet.common.phetcommon.view.util.FrameSetup;
 
 
 public class WaveInterferenceConstants {
-    
+
     public static final String PROJECT_NAME = "wave-interference";
-    public static final FrameSetup FRAME_SETUP = new FrameSetup.MaxExtent( new FrameSetup.CenteredWithInsets( 50, 50 ) );
+    public static final FrameSetup FRAME_SETUP = new FrameSetup() {
+        int width = 1280;
+        int height = 960;
+
+        public void initialize( JFrame frame ) {
+            Toolkit tk = Toolkit.getDefaultToolkit();
+            Dimension d = tk.getScreenSize();
+            int x = ( d.width - width ) / 2;
+            int y = ( d.height - height ) / 2;
+            frame.setLocation( x, y );
+
+            //test to see that the requested dimensions aren't bigger than the screen
+            if ( d.width < width || d.height < height ) {
+                new MaxExtent( new CenteredWithInsets( 10, 10 ) ).initialize( frame );
+            }
+            else {
+                frame.setSize( width, height );
+            }
+        }
+    };
 
 }
