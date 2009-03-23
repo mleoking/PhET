1191b4c7f3b330b75713731a2f12a9b129613942
dano <dano@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-03-23 23:57:07 +0000

    Changed the thumbnail scheme so that it will check the project directory before trying to generate it.  After IOM, it will just be read and not checked or generated.  See ticket #1505.

--- web/website/include/Simulation/PreIomJavaSimulation.class.php
+++ web/website/include/Simulation/PreIomJavaSimulation.class.php
@@ -23,6 +23,50 @@ class PreIomJavaSimulation extends JavaSimulation {
         return (int) (filesize($filename) / 1024);
     }
 
+    public function getThumbnailFilename() {
+        $thumbnail_filename = parent::getThumbnailFilename();
+        if (file_exists($thumbnail_filename)) {
+            return $thumbnail_filename;
+        }
+
+        // TODO: too much in 1 function, refactor
+        // TODO: abstract the file_name_hash, there is a naming convention between this and a 'resounce name'
+        $image_url = $this->getScreenshotUrl();
+        $file_name_hash = md5($image_url).'.jpg';
+        if (cache_has_valid_page(self::thumbnail_cache, $file_name_hash, self::thumbnail_cache_lifespan)) {
+            return cache_get_file_location(self::thumbnail_cache, $file_name_hash);
+        }
+
+        // Load image -- assume image is in png format (for now):
+        @$img = imagecreatefrompng($image_url);
+        if ($img == false) {
+            return;
+        }
+
+        $existing_width  = imagesx($img);
+        $existing_height = imagesy($img);
+
+        // Scale to thumbnail width, preserving aspect ratio:
+        $new_width  = self::thumbnail_width;
+        $new_height = self::thumbnail_height;
+        //old method is proportional: floor($existing_height * ( thumbnail_width / $existing_width ));
+
+        $tmp_img = imagecreatetruecolor($new_width, $new_height);
+
+        imagecopyresampled($tmp_img, $img, 0, 0, 0, 0, $new_width, $new_height, $existing_width, $existing_height);
+
+        $temp_file = tempnam('/tmp', 'sim-thumbnail');
+
+        // Output image to cached image location:
+        imagejpeg($tmp_img, $temp_file);
+
+        cache_put(self::thumbnail_cache, $file_name_hash, file_get_contents($temp_file));
+
+        unlink($temp_file);
+
+        return cache_get_file_location(self::thumbnail_cache, $file_name_hash);
+    }
+
     private function getPreIomLaunchFilename($locale = Locale::DEFAULT_LOCALE) {
         if (Locale::inst()->isDefault($locale)) {
             return self::sim_root."{$this->project_name}/{$this->sim_name}.jnlp";
