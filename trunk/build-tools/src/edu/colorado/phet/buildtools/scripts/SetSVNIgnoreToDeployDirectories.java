0907123d918a7816d44b2cd34b5d2d8307af45f1
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-03-23 15:38:39 +0000

    add SWF and HTML to deploy ignores, see #1505

--- trunk/build-tools/src/edu/colorado/phet/buildtools/scripts/SetSVNIgnoreToDeployDirectories.java
+++ trunk/build-tools/src/edu/colorado/phet/buildtools/scripts/SetSVNIgnoreToDeployDirectories.java
@@ -9,17 +9,18 @@ import java.io.IOException;
 import java.util.Arrays;
 
 import edu.colorado.phet.buildtools.PhetProject;
+import edu.colorado.phet.common.phetcommon.util.StreamReaderThread;
 
 /**
  * AddSVNIgnoreToDeployDirectories is a utility for adding the svn:ignore property to all deploy directories.
- * The PhET build process generates JAR and JNLP files, and puts them in the deploy directories.
- * We do not want to check in these JAR and JNLP files, so we need to tell SVN to ignore them.
+ * The PhET build process generates JAR, JNLP, SWF, JPG, GIF, PNG, HEADER files, and puts them in the deploy directories.
+ * We do not want to check in these files, so we need to tell SVN to ignore them.
  * This is done via the svn:ignore property, and that property is set using the "svn propset" command.
  * See "svn help propset" for details.
  */
 public class SetSVNIgnoreToDeployDirectories {
 
-    public static void main( String[] args ) throws IOException {
+    public static void main( String[] args ) throws IOException, InterruptedException {
 
         // trunk must be specified on the command line
         if ( args.length != 1 ) {
@@ -36,12 +37,13 @@ public class SetSVNIgnoreToDeployDirectories {
         PhetProject[] projects = PhetProject.getAllProjects( trunk );
         for ( int k = 0; k < projects.length; k++ ) {
             File dir = projects[k].getDeployDir();
-            String[] ignorePatterns = new String[]{"*.jar", "*.jnlp", "*.properties", "HEADER","*.png","*.jpg","*.gif"};
+            String[] ignorePatterns = new String[]{"*.jar", "*.jnlp", "*.properties", "HEADER","*.png","*.jpg","*.gif","*.swf","*.html"};
             setIgnorePatternsOnDir( dir, ignorePatterns );
+            System.out.println( "Update properties on: "+dir.getAbsolutePath() );
         }
     }
 
-    public static void setIgnorePatternsOnDir( File dir, String[] ignorePatterns ) throws IOException {
+    public static void setIgnorePatternsOnDir( File dir, String[] ignorePatterns ) throws IOException, InterruptedException {
         // Write the svn:ignore property value to the temporary file
         // Create a temporary file
         File propFile = File.createTempFile( "deploy-svn-ignore.", ".tmp" );
@@ -60,6 +62,9 @@ public class SetSVNIgnoreToDeployDirectories {
         //use a command array for non-windows platforms
         String[] svnCommand = new String[]{"svn", "propset", "svn:ignore", "--file", propFilename, dir.getAbsolutePath()};
         System.out.println( Arrays.asList( svnCommand ) );
-        Runtime.getRuntime().exec( svnCommand );
+        Process p=Runtime.getRuntime().exec( svnCommand );
+        new StreamReaderThread( p.getErrorStream(), "err>" ).start();
+        new StreamReaderThread( p.getInputStream(), "" ).start();
+        p.waitFor();
     }
 }
