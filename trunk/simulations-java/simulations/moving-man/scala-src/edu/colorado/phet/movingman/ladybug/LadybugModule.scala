faa89e224bba2689fc79a00b7fd450690e30f45b
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-03-25 17:21:27 +0000

    generalize names

--- trunk/simulations-java/simulations/moving-man/scala-src/edu/colorado/phet/movingman/ladybug/LadybugModule.scala
+++ trunk/simulations-java/simulations/moving-man/scala-src/edu/colorado/phet/movingman/ladybug/LadybugModule.scala
@@ -38,7 +38,7 @@ class LadybugModule[ModelType <: LadybugModel](clock: ScalaClock,
 
   clock.addClockListener(model.update(_))
 
-  setClockControlPanel(new LadybugClockControlPanel(model,canvas, () => {createRightControl(this)}))
+  setClockControlPanel(new RecordModelControlPanel(model,canvas, () => {createRightControl(this)}))
 
   def getLadybugMotionModel = model.getLadybugMotionModel()
 
