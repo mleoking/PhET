6c4940c06e09c7ab6a3e6ea1190dad2ab533e667
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-03-25 17:21:27 +0000

    generalize names

--- simulations-java/simulations/moving-man/scala-src/edu/colorado/phet/movingman/ladybug/controlpanel/RecordModelControlPanel.scala
+++ simulations-java/simulations/moving-man/scala-src/edu/colorado/phet/movingman/ladybug/controlpanel/RecordModelControlPanel.scala
@@ -33,7 +33,7 @@ import umd.cs.piccolox.pswing.PSwing
 import edu.colorado.phet.common.piccolophet.nodes.mediabuttons.PiccoloTimeControlPanel.BackgroundNode
 import edu.colorado.phet.scalacommon.Predef._
 
-class LadybugClockControlPanel[T](model: RecordModel[T], simPanel: JComponent, createRightControl: () => PNode) extends PhetPCanvas {
+class RecordModelControlPanel[T](model: RecordModel[T], simPanel: JComponent, createRightControl: () => PNode) extends PhetPCanvas {
   private class MyButtonNode(text: String, icon: Icon, action: () => Unit) extends PText(text) {
     addInputEventListener(new PBasicInputEventHandler() {
       override def mousePressed(event: PInputEvent) = {action()}
