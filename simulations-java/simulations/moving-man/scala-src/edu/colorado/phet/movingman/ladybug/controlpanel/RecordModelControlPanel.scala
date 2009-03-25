78c7d0ea338bef34cb9d0f55969f8e9f23824c4d
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-03-25 17:22:12 +0000

    minor fixes

--- simulations-java/simulations/moving-man/scala-src/edu/colorado/phet/movingman/ladybug/controlpanel/RecordModelControlPanel.scala
+++ simulations-java/simulations/moving-man/scala-src/edu/colorado/phet/movingman/ladybug/controlpanel/RecordModelControlPanel.scala
@@ -14,7 +14,6 @@ import edu.colorado.phet.common.piccolophet.nodes.mediabuttons.PlayPauseButton
 import edu.colorado.phet.common.piccolophet.nodes.mediabuttons.RewindButton
 import edu.colorado.phet.common.piccolophet.nodes.mediabuttons.StepButton
 import edu.colorado.phet.common.piccolophet.PhetPCanvas
-import model.LadybugState
 import scala.collection.mutable.ArrayBuffer
 import scalacommon.record.{DataPoint, RecordModel}
 import edu.colorado.phet.common.phetcommon.resources.PhetCommonResources
