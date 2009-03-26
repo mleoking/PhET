05cf637922de74a82b239659cfe30f683b9f9c56
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-03-26 21:22:15 +0000

    see #1497, fixed and rewrote InputTemperatureControl, it was a mess

--- simulations-java/simulations/ideal-gas/src/edu/colorado/phet/idealgas/controller/InputTemperatureControlPanel.java
+++ simulations-java/simulations/ideal-gas/src/edu/colorado/phet/idealgas/controller/InputTemperatureControlPanel.java
@@ -1,13 +1,5 @@
-/* Copyright 2003-2004, University of Colorado */
+/* Copyright 2003-2009, University of Colorado */
 
-/*
- * CVS Info -
- * Filename : $Source$
- * Branch : $Name$
- * Modified by : $Author$
- * Revision : $Revision$
- * Date modified : $Date$
- */
 package edu.colorado.phet.idealgas.controller;
 
 import edu.colorado.phet.idealgas.IdealGasConfig;
@@ -26,74 +18,85 @@ import java.awt.event.ActionListener;
  * InputTemperatureControlPanel
  *
  * @author Ron LeMaster
- * @version $Revision$
  */
 public class InputTemperatureControlPanel extends JPanel {
-    private static double maxTemperature = 10000;
+    
+    // TODO: figure out where the 2.5 comes from!!!!!
+    private static final double HACK_CONSTANT = 2.5;
+    
+    private static final double MIN_TEMPERATURE = 50;
+    private static final double MAX_TEMPERATURE = 10000;
+    private static final double DEFAULT_TEMPERATURE = IdealGasModel.DEFAULT_ENERGY / IdealGasConfig.TEMPERATURE_SCALE_FACTOR / HACK_CONSTANT;
+    
+    private final IdealGasModule module;
+    private final Pump[] pumps;
+    private final JCheckBox temperatureCheckBox;
+    private final JSpinner temperatureSpinner;
 
-    private GridBagConstraints gbc;
-    private IdealGasModule module;
-
-    public InputTemperatureControlPanel( IdealGasModule module, Pump[] pumps ) {
+    public InputTemperatureControlPanel( IdealGasModule module, final Pump[] pumps ) {
         super( new GridBagLayout() );
+        
         this.module = module;
-        makeParticlesControls( pumps );
-    }
-
-    /**
-     * Creates the panel that holds controls for the particles in the box
-     */
-    private void makeParticlesControls( final Pump[] pumps ) {
-        gbc = new GridBagConstraints( 0, 0,
-                                      1, 1, 1, 1,
-                                      GridBagConstraints.CENTER,
-                                      GridBagConstraints.NONE,
-                                      new Insets( 0, 0, 0, 0 ), 0, 0 );
-
-        // Add control for temperature at which particles are introduced
-        // todo: figure out where the 2.5 comes from, and don't use a hard-coded constant here!!!!!
-        final double hackConst = 2.5;
-        final JSpinner tempSpinner = new JSpinner( new SpinnerNumberModel( IdealGasModel.DEFAULT_ENERGY / IdealGasConfig.TEMPERATURE_SCALE_FACTOR / hackConst,
-                                                                           50, maxTemperature, 1 ) );
-        tempSpinner.setEnabled( false );
-        final JCheckBox tempLbl = new JCheckBox( IdealGasResources.getString( "AdvancedControlPanel.Particle_Temperature" ), false );
-        tempLbl.addActionListener( new ActionListener() {
-            Pump.PumpingEnergyStrategy orgEnergyStrategy = module.getPumpingEnergyStrategy();
-            Pump.PumpingEnergyStrategy energyStrategy = null;
-
+        this.pumps = pumps;
+        
+        // check box
+        temperatureCheckBox = new JCheckBox( IdealGasResources.getString( "AdvancedControlPanel.Particle_Temperature" ), false );
+        temperatureCheckBox.addActionListener( new ActionListener() {
             public void actionPerformed( ActionEvent e ) {
-                tempSpinner.setEnabled( tempLbl.isSelected() );
-                if( !tempLbl.isSelected() ) {
-                    energyStrategy = orgEnergyStrategy;
-                }
-                else {
-                    orgEnergyStrategy = module.getPumpingEnergyStrategy();
-                    double temp = ( (Double)tempSpinner.getValue() ).doubleValue() * IdealGasConfig.TEMPERATURE_SCALE_FACTOR * hackConst;
-                    energyStrategy = new Pump.FixedEnergyStrategy( temp );
-                }
-                for( int i = 0; i < pumps.length; i++ ) {
-                    pumps[i].setPumpingEnergyStrategy( energyStrategy );
-                }
+                temperatureSpinner.setEnabled( temperatureCheckBox.isSelected() );
+                updatePumps();
+            }
+        } );
+        
+        // spinner
+        temperatureSpinner = new JSpinner( new SpinnerNumberModel( DEFAULT_TEMPERATURE, MIN_TEMPERATURE, MAX_TEMPERATURE, 1 /* stepSize */ ) );
+        temperatureSpinner.setEnabled( false );
+        temperatureSpinner.addChangeListener( new ChangeListener() {
+            public void stateChanged( ChangeEvent e ) {
+                updatePumps();
             }
         } );
 
-        gbc.insets = new Insets( 0, 0, 0, 0 );
-//        gbc.insets = new Insets( 0, 10, 0, 0 );
+        // layout
+        GridBagConstraints gbc = new GridBagConstraints( 0, 0,
+                1, 1, 1, 1,
+                GridBagConstraints.CENTER,
+                GridBagConstraints.NONE,
+                new Insets( 0, 0, 0, 0 ), 0, 0 );
         gbc.anchor = GridBagConstraints.NORTHWEST;
-        this.add( tempLbl, gbc );
-
+        this.add( temperatureCheckBox, gbc );
         gbc.gridy = 1;
         gbc.anchor = GridBagConstraints.CENTER;
-        this.add( tempSpinner, gbc );
-        // Changes the temperature at which particles are pumped into the box
-        tempSpinner.addChangeListener( new ChangeListener() {
-            public void stateChanged( ChangeEvent e ) {
-                double temp = ( (Double)tempSpinner.getValue() ).doubleValue() * IdealGasConfig.TEMPERATURE_SCALE_FACTOR * hackConst;
-                for( int i = 0; i < pumps.length; i++ ) {
-                    pumps[i].setPumpingEnergyStrategy( new Pump.FixedEnergyStrategy( temp ) );
-                }
-            }
-        } );
-
+        this.add( temperatureSpinner, gbc );
+    }
+    
+    private double getTemperature() {
+        return ( (Double) temperatureSpinner.getValue() ).doubleValue() * IdealGasConfig.TEMPERATURE_SCALE_FACTOR * HACK_CONSTANT;
+    }
+    
+    /*
+     * If the check box is checked, use a fixed-energy strategy based on temperature.
+     * Otherwise get the strategy from the module.
+     */
+    private Pump.PumpingEnergyStrategy getPumpingStrategy() {
+        Pump.PumpingEnergyStrategy strategy = null;
+        if ( temperatureCheckBox.isSelected() ) {
+            strategy = new Pump.FixedEnergyStrategy( getTemperature() );
+        }
+        else {
+            strategy = module.getPumpingEnergyStrategy();
+        }
+        return strategy;
+    }
+    
+    /*
+     * Updates the energy strategy used by all pumps.
+     */
+    private void updatePumps() {
+        Pump.PumpingEnergyStrategy strategy = getPumpingStrategy();
+        System.out.println( "updating " + pumps.length + " pumps, energy=" + strategy.getMoleculeEnergy() + " type=" + strategy.getClass().getName() );//XXX
+        for( int i = 0; i < pumps.length; i++ ) {
+            pumps[i].setPumpingEnergyStrategy( strategy );
+        }
     }
 }
