6ec3505ed317b3a36e6ad09254d12e5303db6dbc
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-03-26 20:45:16 +0000

    Removed the UserDefinedAtom class and consolidated its functionality with the ConfigurableAttractionAtom.  They were serving essentially the same purpose, so this simplifies things.

--- simulations-java/simulations/states-of-matter/src/edu/colorado/phet/statesofmatter/model/AtomFactory.java
+++ simulations-java/simulations/states-of-matter/src/edu/colorado/phet/statesofmatter/model/AtomFactory.java
@@ -3,10 +3,10 @@
 package edu.colorado.phet.statesofmatter.model;
 
 import edu.colorado.phet.statesofmatter.model.particle.ArgonAtom;
+import edu.colorado.phet.statesofmatter.model.particle.ConfigurableAttractionAtom;
 import edu.colorado.phet.statesofmatter.model.particle.NeonAtom;
 import edu.colorado.phet.statesofmatter.model.particle.OxygenAtom;
 import edu.colorado.phet.statesofmatter.model.particle.StatesOfMatterAtom;
-import edu.colorado.phet.statesofmatter.model.particle.UserDefinedAtom;
 
 /**
  * This class is used to create an instance of the desired type of atom.
@@ -19,7 +19,7 @@ public class AtomFactory {
 		StatesOfMatterAtom atom = null;
 		
         if (atomType == AtomType.ADJUSTABLE){
-            atom = new UserDefinedAtom(0, 0);
+            atom = new ConfigurableAttractionAtom(0, 0);
         }
         else if (atomType == AtomType.ARGON){
             atom = new ArgonAtom(0, 0);
