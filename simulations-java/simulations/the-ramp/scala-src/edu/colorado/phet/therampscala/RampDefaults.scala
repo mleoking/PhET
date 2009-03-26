dfaf5affd03ddc5a81e01db80c3b12c5a860d007
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-03-26 02:26:34 +0000

    added object properties dialog

--- simulations-java/simulations/the-ramp/scala-src/edu/colorado/phet/therampscala/RampDefaults.scala
+++ simulations-java/simulations/the-ramp/scala-src/edu/colorado/phet/therampscala/RampDefaults.scala
@@ -1,24 +1,31 @@
 package edu.colorado.phet.therampscala
 
-case class ScalaRampObject(name: String, mass: Double, imageFilename: String) {
+class ScalaRampObject(_name: String, _mass: Double, _imageFilename: String, _customizable: Boolean) {
+  val customizable = _customizable
+  val name=_name
+  val mass=_mass
+  val imageFilename=_imageFilename
+
   def getDisplayText = name + " (" + mass + " kg)"
+
+  def this(name: String, mass: Double, imageFilename: String) = this (name, mass, imageFilename, false)
 }
 
-class CustomTextRampObject(name: String, mass: Double, imageFilename: String) extends ScalaRampObject(name, mass, imageFilename) {
+class CustomTextRampObject(name: String, mass: Double, imageFilename: String, customizable: Boolean) extends ScalaRampObject(name, mass, imageFilename, customizable) {
   override def getDisplayText = name
 }
 
 object RampDefaults {
   val MIN_X = -10
   val MAX_X = 10
-  val objects = ScalaRampObject("File Cabinet", 200.0, "cabinet.gif") ::
-          ScalaRampObject("Sleepy Dog", 25.0, "ollie.gif") ::
-          ScalaRampObject("Small Crate", 150, "crate.gif") ::
-          new CustomTextRampObject("Custom Crate", 150, "crate.gif") ::
-          ScalaRampObject("Refrigerator", 400, "fridge.gif") ::
-          ScalaRampObject("Textboox", 10, "phetbook.gif") ::
-          ScalaRampObject("Big Crate", 300, "crate.gif") ::
-          new CustomTextRampObject("Mystery Object", 300, "crate.gif") ::
+  val objects = new ScalaRampObject("File Cabinet", 200.0, "cabinet.gif") ::
+          new ScalaRampObject("Sleepy Dog", 25.0, "ollie.gif") ::
+          new ScalaRampObject("Small Crate", 150, "crate.gif") ::
+          new CustomTextRampObject("Custom Crate", 150, "crate.gif", true) ::
+          new ScalaRampObject("Refrigerator", 400, "fridge.gif") ::
+          new ScalaRampObject("Textboox", 10, "phetbook.gif") ::
+          new ScalaRampObject("Big Crate", 300, "crate.gif") ::
+          new CustomTextRampObject("Mystery Object", 300, "crate.gif", false) ::
           Nil
   val objectsPerRow = 4
 
