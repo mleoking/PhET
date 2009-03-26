2b469005e7019b5665d848be662dba3edf580f4d
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-03-26 07:16:57 +0000

    Added accessibility and optimization to flex-build, and lowered targeted Flash Player to >= 9

--- build-tools/src/edu/colorado/phet/buildtools/flex/PhetFlexProject.java
+++ build-tools/src/edu/colorado/phet/buildtools/flex/PhetFlexProject.java
@@ -50,7 +50,7 @@ public class PhetFlexProject extends PhetProject {
         boolean b = swfFile.delete();
         System.out.println( "Delete SWFFile=" + b );
 //        Process p = Runtime.getRuntime().exec( "mxmlc -output deploy/" + getName() + ".swf -compiler.source-path=src " + getMXML(), null, getProjectDir() );
-        Process p = Runtime.getRuntime().exec( new String[]{"mxmlc", "-output", "deploy/" + getName() + ".swf", "-compiler.source-path=src:../../common/src", "-target-player", "10", getMXML()}, null, getProjectDir() );
+        Process p = Runtime.getRuntime().exec( new String[]{"mxmlc", "-output", "deploy/" + getName() + ".swf", "-compiler.source-path=src:../../common/src", "-compiler.accessible", "-compiler.optimize", "-target-player", "9", getMXML()}, null, getProjectDir() );
         new StreamReaderThread( p.getErrorStream(), "err>" ).start();
         new StreamReaderThread( p.getInputStream(), "" ).start();
         p.waitFor();
