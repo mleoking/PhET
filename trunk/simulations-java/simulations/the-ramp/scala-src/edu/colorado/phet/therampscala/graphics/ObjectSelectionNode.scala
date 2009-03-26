a176e4e5d36bed0c1e8de786082794067b619269
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-03-26 02:26:34 +0000

    added object properties dialog

--- trunk/simulations-java/simulations/the-ramp/scala-src/edu/colorado/phet/therampscala/graphics/ObjectSelectionNode.scala
+++ trunk/simulations-java/simulations/the-ramp/scala-src/edu/colorado/phet/therampscala/graphics/ObjectSelectionNode.scala
@@ -4,12 +4,17 @@ package edu.colorado.phet.therampscala.graphics
 import collection.mutable.ArrayBuffer
 import common.phetcommon.view.graphics.transforms.ModelViewTransform2D
 import common.phetcommon.view.util.{BufferedImageUtils, PhetFont}
+import common.phetcommon.view.VerticalLayoutPanel
+import java.awt.{Rectangle, BasicStroke, Color}
+import javax.swing.{JButton, Timer}
+import scalacommon.util.Observable
+import swing.ScalaValueControl
+import umd.cs.piccolox.nodes.PClip
+import umd.cs.piccolox.pswing.PSwing
 import umd.cs.piccolo.PNode
 import umd.cs.piccolo.nodes.{PText, PImage}
 
 import common.piccolophet.nodes.PhetPPath
-import java.awt.{BasicStroke, Color}
-
 import java.awt.geom.Rectangle2D
 import umd.cs.piccolo.event.{PBasicInputEventHandler, PInputEvent}
 
@@ -43,6 +48,9 @@ class ObjectSelectionNode(transform: ModelViewTransform2D, model: ObjectModel) e
     addChild(textNode)
 
     defineInvokeAndPass(model.addListenerByName) {
+      update()
+    }
+    def update() {
       if (model.selectedObject == o) {
         backgroundNode.setPaint(new Color(0, 0, 255, 50))
         textNode.setFont(new PhetFont(14, true))
@@ -57,10 +65,82 @@ class ObjectSelectionNode(transform: ModelViewTransform2D, model: ObjectModel) e
       }
     })
   }
+  class CustomObjectSelectionIcon(o: ScalaRampObject) extends ObjectSelectionIcon(o: ScalaRampObject) {
+    override def update() = {
+      if (model.selectedObject == o) {
+        backgroundNode.setPaint(ccp.getBackground)
+        textNode.setFont(new PhetFont(14, true))
+      } else {
+        backgroundNode.setPaint(new Color(0, 0, 0, 0))
+        textNode.setFont(new PhetFont(14, false))
+      }
+    }
 
-  val nodes = for (o <- objects) yield {
-    new ObjectSelectionIcon(o)
+    private var expand = false
+    private val timer = new Timer(20, () => {})
+    private var added = false
+
+    val obj = new Object with Observable
+    class CustomControlPanel extends VerticalLayoutPanel {
+      add(new ScalaValueControl(0, 100, "mass", "0.0", "kg", 3, (x: Double) => {}, obj.addListener))
+      add(new ScalaValueControl(0, 100, "Coefficient of Static Friction", "0.0", "", 3, (x: Double) => {}, obj.addListener))
+      add(new ScalaValueControl(0, 100, "Coefficient of Kinetic Friction", "0.0", "", 3, (x: Double) => {}, obj.addListener))
+    }
+
+    val ccp=new CustomControlPanel
+    val controlPanel = new PSwing(ccp)
+    val clip = new PClip
+    clip.setStrokePaint(null)
+    clip.setPathTo(new Rectangle2D.Double(0, 0, controlPanel.getFullBounds.getWidth, controlPanel.getFullBounds.getHeight))
+    clip.addChild(controlPanel)
+
+    clip.setVisible(false)
+    timer.addActionListener(() => {
+      if (expand && !added) {
+        addChild(0, clip) //so it skips initial layout code
+        added = true
+      }
+
+      val dst = if (expand) -controlPanel.getFullBounds.getHeight-0.5 else 0.0
+      val cur = clip.getOffset.getY
+
+      val dy = dst - cur
+      val speed = 14
+      clip.setOffset(0, cur + (if (dy > 0) speed else -speed))
+      clip.setPathTo(new Rectangle2D.Double(0, 0, controlPanel.getFullBounds.getWidth, -clip.getOffset.getY))
+      if (abs(dy) <= speed * 2) {
+        clip.setOffset(0, dst)
+
+        clip.setVisible(expand)
+        clip.setPickable(expand)
+        clip.setChildrenPickable(expand)
+        timer.stop()
+      }
+    })
+    defineInvokeAndPass(model.addListenerByName) {
+      if (model.selectedObject == o)
+        expandControls()
+      else
+        collapseControls()
+    }
+    def expandControls() {
+      expand = true
+
+      clip.setVisible(expand)
+      clip.setPickable(expand)
+      clip.setChildrenPickable(expand)
+      timer.start()
+    }
+
+    def collapseControls() {
+      expand = false
+
+      timer.start()
+    }
   }
+
+  val nodes = for (o <- objects) yield
+    if (o.customizable) new CustomObjectSelectionIcon(o) else new ObjectSelectionIcon(o)
 
   val cellDim = nodes.foldLeft(new PDimension)((a, b) => new PDimension(max(a.width, b.getFullBounds.width), max(a.height, b.getFullBounds.height)))
 
