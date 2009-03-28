816561946bd16b0db4ecba6faa9fca0308ce4dc5
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-03-28 21:32:06 +0000

    add generation of JNLP files, see #1368

--- trunk/build-tools/src/edu/colorado/phet/buildtools/translate/TranslationDeployPublisher.java
+++ trunk/build-tools/src/edu/colorado/phet/buildtools/translate/TranslationDeployPublisher.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import edu.colorado.phet.buildtools.JARGenerator;
 import edu.colorado.phet.buildtools.PhetServer;
 import edu.colorado.phet.buildtools.util.FileUtils;
 
@@ -44,11 +45,27 @@ public class TranslationDeployPublisher {
             File jar = new File( translationDir, project + "_" + locales[i] + ".jar" );
             FileUtils.copyToDir( jar, new File( sims, project ) );
         }
+        FileUtils.copyToDir( getAllJAR( translationDir, project ), new File( sims, project ) );
+    }
+
+    private File getAllJAR( File translationDir, String project ) {
+        return new File( translationDir, project + "_all.jar" );
     }
 
     //generate new JNLPs in sims directory
-    private void generateJNLPs( File translationDir, String project, String[] locales ) {
-//        JNLP
+    private void generateJNLPs( File translationDir, String project, String[] locales ) throws IOException {
+        //TODO generate fresh JNLPs instead?
+        //might be safer to copy existing JNLP to make sure main class is right, etc.
+
+        String[] flavors = JARGenerator.getFlavors( getAllJAR( translationDir, project ) );
+        for ( int i = 0; i < flavors.length; i++ ) {
+            //for now, copy english JNLP and replace value="en" with value="locale_STR"
+            String englishJNLP = FileUtils.loadFileAsString( new File( sims, project + "/" + flavors[i] + "_en.jnlp" ), "UTF-16" );
+            for ( int j = 0; j < locales.length; j++ ) {
+                String out = FileUtils.replaceAll( englishJNLP, "value=\"en\"", "value=\"" + locales[j] + "\"" );
+                FileUtils.writeString( new File( sims, project + "/" + flavors[i] + "_" + locales[j] + ".jnlp" ), out, "UTF-16" );
+            }
+        }
     }
 
     //regenerate server HTML caches so new translations will appear
