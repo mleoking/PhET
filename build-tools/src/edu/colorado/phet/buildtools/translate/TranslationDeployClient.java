f5c1ff458e39f34b2191206d4f414e99ceee9084
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-03-28 04:38:51 +0000

    started implementing client side for translation deploy, see #1386

--- build-tools/src/edu/colorado/phet/buildtools/translate/TranslationDeployClient.java
+++ build-tools/src/edu/colorado/phet/buildtools/translate/TranslationDeployClient.java
@@ -1,6 +1,21 @@
 package edu.colorado.phet.buildtools.translate;
 
-/**
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.rev6.scf.SshCommand;
+import org.rev6.scf.SshConnection;
+import org.rev6.scf.SshException;
+
+import edu.colorado.phet.buildtools.AuthenticationInfo;
+import edu.colorado.phet.buildtools.BuildLocalProperties;
+import edu.colorado.phet.buildtools.PhetServer;
+
+import com.jcraft.jsch.JSchException;
+
+/*
  * This is the 3-27-2009 rewrite of translation deploying.
  * Here's the basic technique:
  * 1. Client identifies new localization files to integrate, by putting them in a directory and telling the program which directory.
@@ -21,4 +36,100 @@ package edu.colorado.phet.buildtools.translate;
  * 7. User signifies to server that sims can be copied to the sim directory.
  */
 public class TranslationDeployClient {
+    private File trunk;
+
+    public TranslationDeployClient( File trunk ) {
+        this.trunk = trunk;
+    }
+
+    public static void main( String[] args ) throws IOException {
+        new TranslationDeployClient( new File( args[0] ) ).startClient();
+    }
+
+    private void startClient() throws IOException {
+        BuildLocalProperties.initRelativeToTrunk( trunk );
+//        JOptionPane.showMessageDialog( null,
+//                                       "<html>Put the localization files that you wish to deploy in a directory.<br>" +
+//                                       "When you have finished this step, press OK to continue.<br>" +
+//                                       "You will be prompted for the directory name.</html>" );
+//        String dirname = AddTranslation.prompt( "Enter the name of the directory where your localization files are:" );
+//        // import the translations into the IDE workspace
+//        new ImportTranslations( new File( trunk, "simulations-java" ) ).importTranslations( new File( dirname ) );
+//        JOptionPane.showMessageDialog( null,
+//                                       "<html>Localization files have been imported into your IDE workspace.<br>" +
+//                                       "Please refresh your workspace, examine the files,<br>" +
+//                                       "and manually commit them to the SVN repository.<br><br>" +
+//                                       "Press OK when you are ready to integrate the files into<br>" +
+//                                       "the PHET production server." );
+
+        File srcDir = new File( "C:\\Users\\Owner\\Desktop\\txdir" );
+        String deployDirName = new SimpleDateFormat( "M-d-yyyy_h-ma" ).format( new Date() );
+        System.out.println( "Deploying to: " + deployDirName );
+        String deployPath = "/web/chroot/phet/usr/local/apache/htdocs/staging/translations/" + deployDirName;
+
+        mkdir( PhetServer.PRODUCTION, BuildLocalProperties.getInstance().getProdAuthenticationInfo(), deployPath );
+        transfer( PhetServer.PRODUCTION, BuildLocalProperties.getInstance().getProdAuthenticationInfo(), srcDir, deployPath );
+        openBrowser( "http://phet.colorado.edu/staging/translations/" + deployDirName );
+    }
+
+    private void openBrowser( String deployPath ) {
+        String browser = BuildLocalProperties.getInstance().getBrowser();
+        if ( browser != null ) {
+            try {
+                Runtime.getRuntime().exec( new String[]{browser, deployPath} );
+            }
+            catch( IOException e ) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void mkdir( PhetServer server, AuthenticationInfo authenticationInfo, String serverDir ) {
+        SshConnection sshConnection = new SshConnection( server.getHost(), authenticationInfo.getUsername(), authenticationInfo.getPassword() );
+        try {
+            sshConnection.connect();
+
+            //TODO: how can we detect failure of this command, e.g. due to permissions errors?  See #1164
+            sshConnection.executeTask( new SshCommand( "mkdir -p -m 775 " + serverDir ) );//TODO: would it be worthwhile to skip this task when possible?
+        }
+        catch( SshException e ) {
+            if ( e.toString().toLowerCase().indexOf( "auth fail" ) != -1 ) {
+                // TODO: check if authentication fails, don't try logging in again
+                // on tigercat, 3 (9?) unsuccessful login attepts will lock you out
+                System.out.println( "Authentication on '" + server.getHost() + "' has failed, is your username and password correct?  Exiting..." );
+                System.exit( 0 );
+            }
+            e.printStackTrace();
+        }
+        finally {
+            sshConnection.disconnect();
+        }
+    }
+
+    public static void transfer( PhetServer server, AuthenticationInfo authenticationInfo, File srcDir, String remotePathDir ) {
+
+        //for some reason, the securechannelfacade fails with a "server didn't expect this file" error
+        //the failure is on tigercat, but scf works properly on spot
+        //but our code works on both; therefore there is probably a problem with the handshaking in securechannelfacade
+        File[] f = srcDir.listFiles(); //TODO: should handle recursive for future use (if we ever want to support nested directories)
+        for ( int i = 0; i < f.length; i++ ) {
+            if ( f[i].getName().startsWith( "." ) ) {
+                //ignore
+            }
+            else {
+                //server.getHost(), authenticationInfo.getUsername(), authenticationInfo.getPassword()
+                try {
+                    ScpTo.uploadFile( f[i], authenticationInfo.getUsername(), server.getHost(), remotePathDir + "/" + f[i].getName(), authenticationInfo.getPassword() );
+                }
+                catch( JSchException e ) {
+                    e.printStackTrace();
+                }
+                catch( IOException e ) {
+                    e.printStackTrace();
+                }
+//                    sshConnection.executeTask( new ScpUpload( new ScpFile( f[i],  ) ) );
+            }
+        }
+    }
+
 }
