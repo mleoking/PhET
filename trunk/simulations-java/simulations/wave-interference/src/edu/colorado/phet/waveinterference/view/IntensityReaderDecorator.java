bc03c9f81dc509c4406e8b3e32da469a7cc32704
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-03-30 18:13:49 +0000

    see #1404, clean up IntensityReaderDecorator

--- trunk/simulations-java/simulations/wave-interference/src/edu/colorado/phet/waveinterference/view/IntensityReaderDecorator.java
+++ trunk/simulations-java/simulations/wave-interference/src/edu/colorado/phet/waveinterference/view/IntensityReaderDecorator.java
@@ -1,11 +1,8 @@
 /*  */
 package edu.colorado.phet.waveinterference.view;
 
-import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionListener;
 import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.beans.PropertyChangeEvent;
@@ -26,42 +23,20 @@ import edu.umd.cs.piccolox.pswing.PSwing;
 import edu.umd.cs.piccolox.pswing.PSwingCanvas;
 
 /**
- * User: Sam Reid
- * Date: Mar 23, 2006
- * Time: 9:58:29 PM
- *
- */
-
-/**
- * Decorates with buttons and controls.
+ * Adds controls to an intensity reader detector.
+ * 
+ * @author Sam Reid
  */
 public class IntensityReaderDecorator extends PhetPNode {
     
-    private ArrayList listeners = new ArrayList();
-    private IntensityReader intensityReader;
-    private PSwing buttonPSwing;
+    private final ArrayList listeners = new ArrayList();
+    private final IntensityReader intensityReader;
+    private final PSwing buttonPSwing;
 
     public IntensityReaderDecorator( String title, final PSwingCanvas pSwingCanvas, WaveModel waveModel, LatticeScreenCoordinates latticeScreenCoordinates, IClock clock ) {
-        this.intensityReader = new IntensityReader( title, waveModel, latticeScreenCoordinates, clock );
-        JButton close = null;
-        try {
-            BufferedImage image = ImageLoader.loadBufferedImage( "wave-interference/images/x-20.png" );
-            image = BufferedImageUtils.rescaleYMaintainAspectRatio( image, (int) ( image.getHeight() * 0.6 ) );
-            close = new JButton( new ImageIcon( image ) );
-            close.setOpaque( false );
-            close.addActionListener( new ActionListener() {
-                public void actionPerformed( ActionEvent e ) {
-                    doDelete();
-                }
-            } );
-        }
-        catch( IOException e ) {
-            e.printStackTrace();
-        }
         
-        buttonPSwing = new PSwing( close );
-        addChild( intensityReader );
-        addChild( buttonPSwing );
+        // detector
+        this.intensityReader = new IntensityReader( title, waveModel, latticeScreenCoordinates, clock );
         intensityReader.addPropertyChangeListener( PNode.PROPERTY_FULL_BOUNDS, new PropertyChangeListener() {
             public void propertyChange( PropertyChangeEvent evt ) {
                 updateLocation();
@@ -72,13 +47,35 @@ public class IntensityReaderDecorator extends PhetPNode {
                 updateLocation();
             }
         } );
+        
+        // close button
+        JButton close = null;
+        try {
+            BufferedImage image = ImageLoader.loadBufferedImage( "wave-interference/images/x-20.png" );
+            image = BufferedImageUtils.rescaleYMaintainAspectRatio( image, (int) ( image.getHeight() * 0.6 ) );
+            close = new JButton( new ImageIcon( image ) );
+            close.setOpaque( false );
+            close.addActionListener( new ActionListener() {
+                public void actionPerformed( ActionEvent e ) {
+                    delete();
+                }
+            } );
+        }
+        catch( IOException e ) {
+            e.printStackTrace();
+        }
+        buttonPSwing = new PSwing( close );
+        
+        // rendering order
+        addChild( intensityReader );
+        addChild( buttonPSwing );
     }
 
     public void delete() {
-        doDelete();
+        notifyDeleted();
     }
 
-    private void doDelete() {
+    private void notifyDeleted() {
         for ( int i = 0; i < listeners.size(); i++ ) {
             Listener listener = (Listener) listeners.get( i );
             listener.deleted();
