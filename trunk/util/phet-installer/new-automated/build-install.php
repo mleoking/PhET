dcddb1790bf883591350a942d6adfab4333648a2
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-03-30 17:57:47 +0000

    Fixed typo in comment, no functional change.

--- trunk/util/phet-installer/new-automated/build-install.php
+++ trunk/util/phet-installer/new-automated/build-install.php
@@ -2,7 +2,7 @@
 
     //============================================================================
     // This file contains PHP functions for a number of the steps that are
-    // necessary for creating the insaller, such as ripping the web site, creating
+    // necessary for creating the installer, such as ripping the web site, creating
     // the marker file, etc.  It is through the functions in this file that the
     // building of the installers is controlled, either from a web interface or
     // from shell scripts.
