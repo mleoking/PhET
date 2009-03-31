f18029f34676110c7d1c00450b9c2183d0580917
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-03-31 07:49:53 +0000

    Translation cleanup and documentation. see #1368

--- build-tools/src/edu/colorado/phet/buildtools/translate/Translation.java
+++ build-tools/src/edu/colorado/phet/buildtools/translate/Translation.java
@@ -9,27 +9,32 @@ import edu.colorado.phet.buildtools.flash.FlashSimulationProject;
 import edu.colorado.phet.buildtools.java.projects.JavaSimulationProject;
 import edu.colorado.phet.common.phetcommon.util.LocaleUtils;
 
+// Handles information pertaining to a particular translation
+
 public class Translation {
 
+    // the file where the translation is stored
     private File file;
 
+    // types of translations
     public static final String TRANSLATION_JAVA = "java";
     public static final String TRANSLATION_FLASH = "flash";
 
     public Translation( File translationFile ) {
         this.file = translationFile;
-
-        System.out.println( "Created " + toString() );
     }
 
+    // does this appear to be an actual translation?
     public boolean isValid() {
         return ( getSimName() != null && getType() != null );
     }
 
+    // get the translation file
     public File getFile() {
         return file;
     }
 
+    // sim name for the translation
     public String getSimName() {
         String simname = null;
         final int index = file.getName().indexOf( "-strings_" );
@@ -37,6 +42,7 @@ public class Translation {
             simname = file.getName().substring( 0, index );
         }
 
+        // if strings for these sims somehow appear, they are not to be thought of as valid!!!
         if ( simname != null && ( simname.equals( "java-common-strings" ) || simname.equals( "flash-common-strings" ) ) ) {
             simname = null;
         }
@@ -44,10 +50,10 @@ public class Translation {
         return simname;
     }
 
+    // is this a common-strings translation? (IE not a simulation translation)
     public boolean isCommonTranslation() {
         boolean ret = false;
 
-        String type = getType();
         String name = file.getName();
 
         if ( isJavaTranslation() ) {
@@ -60,10 +66,12 @@ public class Translation {
         return ret;
     }
 
+    // is this a sim translation? (IE not a common-string translation)
     public boolean isSimulationTranslation() {
         return isValid() && !isCommonTranslation();
     }
 
+    // get the locale of the translation
     public Locale getLocale() {
         Locale ret = null;
         String search = "-strings_";
@@ -78,18 +86,23 @@ public class Translation {
         return ret;
     }
 
+    // is it a translation for Java (sim or common)?
     public boolean isJavaTranslation() {
         String type = getType();
         if ( type == null ) { return false; }
         return type.equals( TRANSLATION_JAVA );
     }
 
+    // is it a translation for Flash (sim or common)?
     public boolean isFlashTranslation() {
         String type = getType();
         if ( type == null ) { return false; }
         return type.equals( TRANSLATION_FLASH );
     }
 
+    // return the type of the translation.
+    // generally, use isJavaTranslation() or isFlashTranslation() instead.
+    // this may return null!
     public String getType() {
         String filename = file.getName();
 
@@ -103,7 +116,8 @@ public class Translation {
         return null;
     }
 
-    public File getSimulationsDir( File trunk ) {
+    // convenience function for where simulations are stored
+    private File getSimulationsDir( File trunk ) {
         if ( isJavaTranslation() ) {
             return new File( trunk, "simulations-java/simulations" );
         }
@@ -114,6 +128,7 @@ public class Translation {
         return null;
     }
 
+    // return a new copy of the project corresponding to this translation
     public PhetProject getProject( File trunk ) throws IOException {
         String simName = getSimName();
 
@@ -127,8 +142,14 @@ public class Translation {
         return null;
     }
 
+    // return a string about the translation
     public String toString() {
-        return file.getName() + " (" + getSimName() + ", " + getType() + ", " + LocaleUtils.localeToString( getLocale() ) + ")";
+        if ( isValid() ) {
+            return file.getName() + " (" + getSimName() + ", " + getType() + ", " + LocaleUtils.localeToString( getLocale() ) + ")";
+        }
+        else {
+            return file.getName() + " (possibly invalid)";
+        }
     }
 
 }
