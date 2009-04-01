b0d6811a7549aa8639ef553546fab3e8914619d2
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-04-01 19:38:46 +0000

    Moved strings from moving-man to ladybug-2d, fixed to use correct resource loader.

--- trunk/simulations-java/simulations/ladybug-2d/scala-src/edu/colorado/phet/ladybug2d/Ladybug2DApplication.scala
+++ trunk/simulations-java/simulations/ladybug-2d/scala-src/edu/colorado/phet/ladybug2d/Ladybug2DApplication.scala
@@ -5,7 +5,7 @@ import scalacommon.{ScalaApplicationLauncher, ScalaClock}
 
 object Ladybug2DApplication {
   def main(args: Array[String]) = {
-    ScalaApplicationLauncher.launchApplication(args, "moving-man", "ladybug-2d",
+    ScalaApplicationLauncher.launchApplication(args, "ladybug-2d", "ladybug-2d",
       () => new LadybugModule[LadybugModel](new ScalaClock(30, 30 / 1000.0)))
   }
 }
