3ab73e7943ebbe76a6361430d4f41aea9e159c2d
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-04-02 04:25:04 +0000

    rename ladybug-2d => ladybug-motion-2d

---
+++ simulations-java/simulations/ladybug-motion-2d/scala-src/edu/colorado/phet/ladybugmotion2d/controlpanel/PathVisibilityModel.scala
@@ -0,0 +1,52 @@
+package edu.colorado.phet.ladybugmotion2d.controlpanel
+
+import scalacommon.util.Observable
+
+class PathVisibilityModel extends Observable {
+  private var _lineVisible = false
+  private var _dotsVisible = false
+  private var _fadeVisible = true
+  private var _fadeFullVisible = false
+
+  def lineVisible: Boolean = _lineVisible
+
+  def dotsVisible: Boolean = _dotsVisible
+
+  def fadeVisible: Boolean = _fadeVisible
+
+  def fadeFullVisible: Boolean = _fadeFullVisible
+
+  def lineVisible_=(x: Boolean) = {
+    _lineVisible = x
+    notifyListeners
+  }
+
+  def allOff() = {
+    lineVisible = false
+    dotsVisible = false
+    fadeVisible = false
+    fadeFullVisible = false
+  }
+
+  def dotsVisible_=(x: Boolean) = {
+    _dotsVisible = x
+    notifyListeners
+  }
+
+  def fadeVisible_=(x: Boolean) = {
+    _fadeVisible = x
+    notifyListeners
+  }
+
+  def fadeFullVisible_=(x: Boolean) = {
+    _fadeFullVisible = x
+    notifyListeners
+  }
+
+  def resetAll() = {
+    lineVisible = false
+    dotsVisible = false
+    fadeVisible = true
+    fadeFullVisible = false
+  }
+}
