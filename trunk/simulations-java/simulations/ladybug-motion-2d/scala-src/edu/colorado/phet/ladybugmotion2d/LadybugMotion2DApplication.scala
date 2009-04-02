fae5dc88fface5e667e2facd89e15fc3172c9e17
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-04-02 04:37:32 +0000

    rename ladybug-2d => ladybug-motion-2d

--- trunk/simulations-java/simulations/ladybug-motion-2d/scala-src/edu/colorado/phet/ladybugmotion2d/LadybugMotion2DApplication.scala
+++ trunk/simulations-java/simulations/ladybug-motion-2d/scala-src/edu/colorado/phet/ladybugmotion2d/LadybugMotion2DApplication.scala
@@ -3,7 +3,7 @@ package edu.colorado.phet.ladybugmotion2d
 import model.LadybugModel
 import scalacommon.{ScalaApplicationLauncher, ScalaClock}
 
-object Ladybug2DApplication {
+object LadybugMotion2DApplication {
   def main(args: Array[String]) = {
     ScalaApplicationLauncher.launchApplication(args, "ladybug-motion-2d", "ladybug-motion-2d",
       () => new LadybugModule[LadybugModel](new ScalaClock(30, 30 / 1000.0)))
