e04fe7f6bae2bdda3a0b6a9ae175c7abb9f3df48
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-04-06 20:22:58 +0000

    Added a function for finding all primary JAR files, as well as one for testing if a JAR is signed.

--- util/phet-installer/new-automated/jar-util.php
+++ util/phet-installer/new-automated/jar-util.php
@@ -13,6 +13,7 @@
     require_once("file-util.php");
 
     define('CONFIG_FILE', 'keystore-config.ini');
+    define('JARSIGNER', '/usr/local/java/bin/jarsigner'); // Full path to jarsigner util.
 
     //------------------------------------------------------------------------
     // Function to sign the specified JAR file.
@@ -26,7 +27,7 @@
     }
 
     //------------------------------------------------------------------------
-    // Function to each of the JAR files in the provided list.
+    // Function to sign each of the JAR files in the provided list.
     //------------------------------------------------------------------------
     function sign_multiple_jars( $list_of_jar_files ) {
         flushing_echo( "function sign_multiple_jars called" );
@@ -46,17 +47,56 @@
     }
 
     //------------------------------------------------------------------------
+    // Function to locate the primary JAR files for the various projects that
+    // exist below the provided directory path.  The "primary JAR file" is the
+    // one that contains all the supported languages and is accessed via the
+    // JNLP file when the user launches the simulation directly from the web
+    // site.
+    //
+    // Note that this file is located by matching the naming convention that
+    // came into use in March 2009, which is <project>_all.jar.  If this
+    // convention ever changes, this function will need to be modified.
+    //------------------------------------------------------------------------
+    function find_primary_project_jar_files( $initial_directory ) {
+        flushing_echo( "function find_primary_project_jar_files called, directory = ".$initial_directory );
+        $file_list = file_list_in_directory( $initial_directory, "*_all.jar" );
+        return $file_list;
+    }
+
+    //------------------------------------------------------------------------
+    // Function to determine whether a given JAR file is signed.  Returns
+    // true if so and false if not.
+    //------------------------------------------------------------------------
+    function is_jar_signed( $path_to_jar_file ) {
+        flushing_echo( "function is_jar_signed called, file = ".$path_to_jar_file );
+        $verify_command = JARSIGNER." -verify ".$path_to_jar_file;
+        $result = system( $verify_command, $retval );
+
+        if ( ( $retval == 0 ) && ( stristr( $result, "verified" ) ) ){
+            echo "JAR is signed.\n";
+            return true;
+        }
+        else {
+            echo "JAR is unsigned.\n";
+            return false;
+        }
+    }
+
+    //------------------------------------------------------------------------
     // Test harness - change this as needed when adding and verifying new
     // functions.
     //------------------------------------------------------------------------
-    function test_functions(){
-        $jar_files = find_jar_files( "./temp/website/phet.colorado.edu/sims" );
-        sign_multiple_jars( $jar_files );
+    function test_harness(){
+        $primary_project_jar_files = find_primary_project_jar_files( "./temp/website/phet.colorado.edu/sims" );
+        foreach ( $primary_project_jar_files as $primary_project_jar_file ) {
+            echo "File: ".$primary_project_jar_file."\n";
+            is_jar_signed( $primary_project_jar_file );
+        }
     }
 
     //------------------------------------------------------------------------
     // Entry point when run directly.  This intended primarily for testing
     // of the functions within this file.
     //------------------------------------------------------------------------
-    test_functions();
+    test_harness();
 ?>
