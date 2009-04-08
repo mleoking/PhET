acc6d45216ea6a8493854525dd801e327b395d83
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-04-08 09:14:03 +0000

    initial checkin for cavendish-experiment

---
+++ simulations-java/simulations/cavendish-experiment/scala-src/edu/colorado/phet/cavendishexperiment/CavendishExperimentResources.scala
@@ -0,0 +1,5 @@
+package edu.colorado.phet.cavendishexperiment
+
+import common.phetcommon.resources.PhetResources
+
+object CavendishExperimentResources extends PhetResources("cavendish-experiment")
