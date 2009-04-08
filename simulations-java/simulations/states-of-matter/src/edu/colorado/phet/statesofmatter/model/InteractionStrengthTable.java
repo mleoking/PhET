0faffeef6b90ae124c242a12dd77d9ce8e8e071f
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-04-08 20:22:08 +0000

    Added some more "real" information about interaction strength to the InteractionStrengthTable class.

--- simulations-java/simulations/states-of-matter/src/edu/colorado/phet/statesofmatter/model/InteractionStrengthTable.java
+++ simulations-java/simulations/states-of-matter/src/edu/colorado/phet/statesofmatter/model/InteractionStrengthTable.java
@@ -13,11 +13,10 @@ import edu.colorado.phet.statesofmatter.model.particle.ConfigurableStatesOfMatte
  * At the time of this writing, it is my understanding that the interaction
  * potential is not a property of an individual atom or molecule, but of two
  * interacting atoms or molecules.  That is why this is being created as a
- * separate class.  If this turns out to be a misunderstanding, it would make
- * sense to consolidate this information into the individual atom objects.
+ * separate class.  I talked with Mike Dubson, and he confirmed this
+ * understanding.
  * 
  * @author John Blanco
- *
  */
 public class InteractionStrengthTable {
 
@@ -27,36 +26,56 @@ public class InteractionStrengthTable {
 	 * it is generally done.  Note that this value is used as the "epsilon"
 	 * parameter in Lennard-Jones potential calculations.
 	 * 
-	 * @param atom1
-	 * @param atom2
+	 * @param atomType1
+	 * @param atomType2
 	 * @return
 	 */
-	static double getInteractionPotential( AtomType atom1, AtomType atom2 ){
-		if (atom1 == atom2){
+	static double getInteractionPotential( AtomType atomType1, AtomType atomType2 ){
+		if (atomType1 == atomType2){
 			// Heterogeneous pair of atoms.
-			if (atom1 == AtomType.NEON){
-				return 32.8;
+			if (atomType1 == AtomType.NEON){
+				// Source: Hansen & McDouald, Theory of Simple Liquids, obtained from the web
+				return 35.8;
 			}
-			else if (atom1 == AtomType.ARGON){
+			else if (atomType1 == AtomType.ARGON){
+				// Source: F. Cuadros, I. Cachadina, and W. Ahamuda, Molc. Engineering, 6, 319 (1996), provided
+				// in the original spec for the SOM simulation.
 				return 111.84;
 			}
-			else if (atom1 == AtomType.OXYGEN){
-				return 200;  // TODO: Waiting for actual value from Noah P for this.
+			else if (atomType1 == AtomType.OXYGEN){
+				// Source: Noah P, who got it from Robert Parsons.
+				return 423.3;
 			}
-			else if (atom1 == AtomType.ADJUSTABLE){
+			else if (atomType1 == AtomType.ADJUSTABLE){
 				return ConfigurableStatesOfMatterAtom.DEFAULT_INTERACTION_POTENTIAL;
 			}
 			else{
-				System.err.println("Error: Interaction potential not available for requested atom: " + atom1);
+				System.err.println("Error: Interaction potential not available for requested atom: " + atomType1);
 				assert(false);
 				return StatesOfMatterConstants.MAX_EPSILON / 2;  // In the real world, default to an arbitrary value.
 			}
 		}
 		else{
-			// Heterogeneous situation.  I think that in reality each pair of
-			// atoms would have a unique interaction potential, but for now we
-			// just use the average of the two atoms.
-			return (getInteractionPotential(atom1, atom1) + getInteractionPotential(atom2, atom2)) / 2;
+			if ( ( ( atomType1 == AtomType.NEON ) && ( atomType2 == AtomType.ARGON ) ) ||
+				   ( atomType1 == AtomType.ARGON ) && ( atomType2 == AtomType.NEON ) ){
+				// Source: Noah P, who got it from Robert Parsons.
+				return 59.5;
+			}
+			else if ( ( ( atomType1 == AtomType.NEON ) && ( atomType2 == AtomType.OXYGEN ) ) ||
+                        ( atomType1 == AtomType.OXYGEN ) && ( atomType2 == AtomType.NEON ) ){
+				// Source: Noah P, who got it from Robert Parsons.
+				return 51;
+			}
+			else if ( ( ( atomType1 == AtomType.ARGON ) && ( atomType2 == AtomType.OXYGEN ) ) ||
+                        ( atomType1 == AtomType.OXYGEN ) && ( atomType2 == AtomType.ARGON ) ){
+				// Source: Noah P, who got it from Robert Parsons.
+				return 85;
+			}
+			else {
+				System.err.println("Error: Do not have data for this combination of molecules, using default.");
+				System.err.println("       atomType1 = " + atomType1 + ", atomType2 = " + atomType2);
+				return ( StatesOfMatterConstants.MAX_EPSILON - StatesOfMatterConstants.MIN_EPSILON ) / 2; 
+			}
 		}
 	}
 }
