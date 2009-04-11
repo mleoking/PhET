174a8d09aeeb88518a155f4d688d8652881d8ab1
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-04-11 19:02:46 +0000

    Adding sim structure for natural-selection

---
+++ trunk/simulations-java/simulations/sim-template/src/edu/colorado/phet/simtemplate/NaturalSelectionApplication.java
@@ -0,0 +1,10 @@
+package edu.colorado.phet.naturalselection;
+
+import edu.colorado.phet.common.piccolophet.PiccoloPhetApplication;
+import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
+
+public class NaturalSelectionApplication extends PiccoloPhetApplication {
+    public NaturalSelectionApplication( PhetApplicationConfig config ) {
+        super( config );
+    }
+}
