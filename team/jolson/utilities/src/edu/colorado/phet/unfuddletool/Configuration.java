1085a0f0b6bc3a85459d3e0c16f756164d42f4dd
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-04-11 20:54:40 +0000

    Better HTML font display

---
+++ team/jolson/utilities/src/edu/colorado/phet/unfuddletool/Configuration.java
@@ -0,0 +1,16 @@
+package edu.colorado.phet.unfuddletool;
+
+public class Configuration {
+
+    public String getAccountName() {
+        return "phet";
+    }
+
+    public int getProjectId() {
+        return 9404;
+    }
+
+    public String getDefaultHTMLFont() {
+        return "BitStream Vera Serif";
+    }
+}
