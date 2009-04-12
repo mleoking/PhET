b541ac9cb39c29387bfece6c03122a1562c3814b
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-04-12 19:20:59 +0000

    Hopefully fixing sim-template

---
+++ trunk/simulations-java/simulations/sim-template/src/edu/colorado/phet/simtemplate/module/example/ExampleModule.java
@@ -0,0 +1,147 @@
+/* Copyright 2007-2008, University of Colorado */
+
+package edu.colorado.phet.simtemplate.module.example;
+
+import java.awt.Frame;
+
+import edu.colorado.phet.common.phetcommon.view.ClockControlPanel;
+import edu.colorado.phet.common.piccolophet.PiccoloModule;
+import edu.colorado.phet.common.piccolophet.nodes.mediabuttons.PiccoloClockControlPanel;
+import edu.colorado.phet.simtemplate.SimTemplateApplication;
+import edu.colorado.phet.simtemplate.SimTemplateStrings;
+import edu.colorado.phet.simtemplate.defaults.ExampleDefaults;
+import edu.colorado.phet.simtemplate.model.ExampleModelElement;
+import edu.colorado.phet.simtemplate.model.SimTemplateClock;
+import edu.colorado.phet.simtemplate.persistence.ExampleConfig;
+import edu.colorado.phet.simtemplate.view.ExampleNode;
+
+/**
+ * ExampleModule is the "Example" module.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
+public class ExampleModule extends PiccoloModule {
+
+    //----------------------------------------------------------------------------
+    // Instance data
+    //----------------------------------------------------------------------------
+
+    private ExampleModel _model;
+    private ExampleCanvas _canvas;
+    private ExampleControlPanel _controlPanel;
+    private PiccoloClockControlPanel _clockControlPanel;
+
+    //----------------------------------------------------------------------------
+    // Constructors
+    //----------------------------------------------------------------------------
+
+    public ExampleModule( Frame parentFrame ) {
+        super( SimTemplateStrings.TITLE_EXAMPLE_MODULE, new SimTemplateClock( ExampleDefaults.CLOCK_FRAME_RATE, ExampleDefaults.CLOCK_DT ) );
+
+        // Model
+        SimTemplateClock clock = (SimTemplateClock) getClock();
+        _model = new ExampleModel( clock );
+
+        // Canvas
+        _canvas = new ExampleCanvas( _model );
+        setSimulationPanel( _canvas );
+
+        // Control Panel
+        _controlPanel = new ExampleControlPanel( this, parentFrame );
+        setControlPanel( _controlPanel );
+        
+        // Clock controls
+        _clockControlPanel = new PiccoloClockControlPanel( getClock() );
+        _clockControlPanel.setRewindButtonVisible( true );
+        _clockControlPanel.setTimeDisplayVisible( true );
+        _clockControlPanel.setUnits( SimTemplateStrings.UNITS_TIME );
+        _clockControlPanel.setTimeColumns( ExampleDefaults.CLOCK_TIME_COLUMNS );
+        setClockControlPanel( _clockControlPanel );
+
+        // Controller
+        ExampleController controller = new ExampleController( _model, _canvas, _controlPanel );
+        
+        // Help
+        if ( hasHelp() ) {
+            //XXX add help items
+        }
+
+        // Set initial state
+        reset();
+    }
+
+    //----------------------------------------------------------------------------
+    // Module overrides
+    //----------------------------------------------------------------------------
+
+    /**
+     * Resets the module.
+     */
+    public void reset() {
+
+        // Clock
+        SimTemplateClock clock = _model.getClock();
+        clock.resetSimulationTime();
+        clock.setDt( ExampleDefaults.CLOCK_DT );
+        setClockRunningWhenActive( ExampleDefaults.CLOCK_RUNNING );
+
+        // ExampleModelElement
+        ExampleModelElement exampleModelElement = _model.getExampleModelElement();
+        exampleModelElement.setPosition( ExampleDefaults.EXAMPLE_MODEL_ELEMENT_POSITION );
+        exampleModelElement.setOrientation( ExampleDefaults.EXAMPLE_MODEL_ELEMENT_ORIENTATION );
+        
+        // ExampleNode
+        ExampleNode exampleNode = _canvas.getExampleNode();
+        exampleNode.setSize( exampleModelElement.getWidth(), exampleModelElement.getHeight() );
+        exampleNode.setPosition( exampleModelElement.getPosition() );
+        exampleNode.setOrientation( exampleModelElement.getOrientation() );
+    }
+    
+    //----------------------------------------------------------------------------
+    // Persistence
+    //----------------------------------------------------------------------------
+
+    public ExampleConfig save() {
+
+        ExampleConfig config = new ExampleConfig();
+
+        // Module
+        config.setActive( isActive() );
+
+        // Clock
+        SimTemplateClock clock = _model.getClock();
+        config.setClockDt( clock.getDt() );
+        config.setClockRunning( getClockRunningWhenActive() );
+
+        // ExampleModelElement
+        ExampleModelElement exampleModelElement = _model.getExampleModelElement();
+        config.setExampleModelElementPosition( exampleModelElement.getPositionReference() );
+        config.setExampleModelElementOrientation( exampleModelElement.getOrientation() );
+
+        // Control panel settings that are specific to the view
+        //XXX
+        
+        return config;
+    }
+
+    public void load( ExampleConfig config ) {
+
+        // Module
+        if ( config.isActive() ) {
+            SimTemplateApplication.instance().setActiveModule( this );
+        }
+
+        // Clock
+        SimTemplateClock clock = _model.getClock();
+        clock.setDt( config.getClockDt() );
+        setClockRunningWhenActive( config.isClockRunning() );
+
+        // ExampleModelElement
+        ExampleModelElement exampleModelElement = _model.getExampleModelElement();
+        exampleModelElement.setPosition( config.getExampleModelElementPosition() );
+        exampleModelElement.setOrientation( config.getExampleModelElementOrientation() );
+
+        // Control panel settings that are specific to the view
+        //XXX
+    }
+}
