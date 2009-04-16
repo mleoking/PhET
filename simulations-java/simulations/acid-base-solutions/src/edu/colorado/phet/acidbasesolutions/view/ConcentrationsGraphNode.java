5deb4bdfefd00b667e593a58a3858dfee9e3ad1e
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-04-16 00:48:59 +0000

    more placeholders

--- simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/view/ConcentrationsGraphNode.java
+++ simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/view/ConcentrationsGraphNode.java
@@ -4,8 +4,10 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.geom.Rectangle2D;
 
+import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.PhetPNode;
 import edu.umd.cs.piccolo.nodes.PPath;
+import edu.umd.cs.piccolo.nodes.PText;
 
 
 public class ConcentrationsGraphNode extends PhetPNode {
@@ -14,10 +16,16 @@ public class ConcentrationsGraphNode extends PhetPNode {
         super();
         
         //XXX placeholder
-        PPath path = new PPath();
-        path.setPathTo( new Rectangle2D.Double( 0, 0, 400, 600 ) );
-        path.setStroke( new BasicStroke( 2f ) );
-        path.setStrokePaint( Color.BLACK );
-        addChild( path );
+        PPath outlineNode = new PPath();
+        outlineNode.setPathTo( new Rectangle2D.Double( 0, 0, 400, 600 ) );
+        outlineNode.setStroke( new BasicStroke( 2f ) );
+        outlineNode.setStrokePaint( Color.BLACK );
+        addChild( outlineNode );
+        
+        //XXX placeholder
+        PText textNode = new PText( "Concentration Graph lives here" );
+        textNode.setFont( new PhetFont( 16 ) );
+        addChild( textNode );
+        textNode.setOffset( ( outlineNode.getWidth() - textNode.getWidth() ) / 2, 50 );
     }
 }
