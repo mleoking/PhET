7f32ad49e8af23160f7e58bf373d9eb7d00d0866
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-04-16 22:03:38 +0000

    see #1404, reformat, delete dead code

--- simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/BufferedPhetPCanvas.java
+++ simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/BufferedPhetPCanvas.java
@@ -1,6 +1,8 @@
+
 package edu.colorado.phet.common.piccolophet;
 
-import java.awt.*;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Dimension2D;
 import java.awt.image.BufferedImage;
@@ -8,15 +10,16 @@ import java.awt.image.BufferedImage;
 import edu.colorado.phet.common.phetcommon.util.PhetUtilities;
 
 /**
- * This class explicitly buffers the graphics to resolve some issues related to piccolo (pswing) scene graph rendering.
+ * This class explicitly buffers the graphics to resolve some issues 
+ * related to Piccolo (PSwing) scene graph rendering.
  *
  * @author Sam Reid
  */
 public class BufferedPhetPCanvas extends PhetPCanvas {
+
     private BufferedImage bufferedImage;
 
-    public BufferedPhetPCanvas() {
-    }
+    public BufferedPhetPCanvas() {}
 
     public BufferedPhetPCanvas( Dimension2D pDimension ) {
         super( pDimension );
@@ -24,22 +27,17 @@ public class BufferedPhetPCanvas extends PhetPCanvas {
 
     public void paintComponent( Graphics g ) {
         if ( bufferedImage == null || bufferedImage.getWidth() != getWidth() || bufferedImage.getHeight() != getHeight() ) {
-//            bufferedImage = new BufferedImage( getWidth(), getHeight(),  );
-            //todo: consider getting a hardware-accelerated offscreen buffer through createImage
             bufferedImage = new BufferedImage( getWidth(), getHeight(), getBufferedImageType() );
         }
-
         Graphics2D bufferedGraphics = bufferedImage.createGraphics();
-        bufferedGraphics.setClip( g.getClipBounds() );//todo is this correct?
+        bufferedGraphics.setClip( g.getClipBounds() );//TODO is this correct?
         super.paintComponent( bufferedGraphics );
         ( (Graphics2D) g ).drawRenderedImage( bufferedImage, new AffineTransform() );
         bufferedGraphics.dispose();
     }
 
-    //Using INT_RGB on a mac doesn't allow any transparency to appear
-    //INT_ARGB_PRE seems to resolve this issue
+    // Using INT_RGB on a Mac doesn't allow any transparency to appear, INT_ARGB_PRE seems to resolve this issue.
     private int getBufferedImageType() {
-        return PhetUtilities.isMacintosh() ? BufferedImage.TYPE_INT_ARGB_PRE
-               : BufferedImage.TYPE_INT_RGB;
+        return PhetUtilities.isMacintosh() ? BufferedImage.TYPE_INT_ARGB_PRE : BufferedImage.TYPE_INT_RGB;
     }
 }
