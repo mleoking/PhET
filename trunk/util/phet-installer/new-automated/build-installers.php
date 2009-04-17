b4a4ea154296e68dfcb91fa2bd6bf142d9cdab0a
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-04-17 22:04:57 +0000

    Stripped out what little functionality is left in here so that all this does now is build the installers from the locally ripped version of the web site.  This is part of the effort to make the installer builder easier to integrate into the build process, see #1155.

--- trunk/util/phet-installer/new-automated/build-installers.php
+++ trunk/util/phet-installer/new-automated/build-installers.php
@@ -25,74 +25,23 @@
     require_once("rip-util.php");
 
     //--------------------------------------------------------------------------
-    // Print the usage information.
-    //--------------------------------------------------------------------------
-    function print_help() {
-        flushing_echo("Usage: build-install [--full]\n".
-                      "                     [--remove-website-copy]\n".
-                      "                     [--rip-website]\n".
-                      "                     [--download-sims]\n".
-                      "                     [--create-marker-file]\n".
-                      "                     [--build-all]\n".
-                      "                     [--help]\n");
-
-    }
-
-    //--------------------------------------------------------------------------
-    // Function for analyzing the command line options.
-    //--------------------------------------------------------------------------
-    function is_checked($attr) {
-        if (is_cmd_line_option_enabled($attr)) {
-            return true;
-        }
-        else if (is_cmd_line_option_enabled("full")) {
-            return true;
-        }
-
-        return isset($_REQUEST[$attr]);
-    }
-
-    //--------------------------------------------------------------------------
     // Main routine - interprets command line options and executes the
     // indicated requests.
     //--------------------------------------------------------------------------
-    function main() {
-        if (is_cmd_line_option_enabled("help")) {
-            print_help();
+    function build_installers() {
+        if (file_lock("install-builder")) {
+            builder_build_all();
         }
         else {
-            if (file_lock("install-builder")) {
-
-                // Note that the order of operations below is important in the
-                // case where all operations are performed.
-
-                if (is_checked('remove-website-copy'))
-                    builder_remove_website_copy();
-
-                if (is_checked('rip-website'))
-                    builder_rip_website();
-
-                if (is_checked('download-sims'))
-                    builder_download_sims();
-
-                if (is_checked('insert-installer-creation-time'))
-                    builder_insert_installer_creation_time();
-
-                if (is_checked('build-all'))
-                    builder_build_all();
+            flushing_echo("The PhET AutoInstallBuilder appears to be completing a previous build.");
+            flushing_echo("Refresh this page to ignore this warning and build anyway.");
         }
-        else {
-                flushing_echo("The PhET AutoInstallBuilder appears to be completing a previous build. Refresh this page to ignore this warning and build anyway.");
-        }
-
         file_unlock("install-builder");
-
-        }
     }
 
     //--------------------------------------------------------------------------
     // Entry point for this PHP file.
     //--------------------------------------------------------------------------
-    main();
+    build_installers();
 
 ?>
