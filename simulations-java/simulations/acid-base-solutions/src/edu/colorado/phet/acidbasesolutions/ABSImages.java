245bde7e862073b34eafd2e02143cc3d6fc72958
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-04-18 00:50:16 +0000

    static images

---
+++ simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/ABSImages.java
@@ -0,0 +1,24 @@
+package edu.colorado.phet.acidbasesolutions;
+
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+
+
+public class ABSImages {
+
+    private ABSImages() {}
+    
+    public static final Image A_MINUS_MOLECULE = getImage( "A_minus.png" );
+    public static final Image B_MOLECULE = getImage( "B.png" );
+    public static final Image BH_PLUS_MOLECULE = getImage( "BH_plus.png" );
+    public static final Image H2O_MOLECULE = getImage( "H2O.png" );
+    public static final Image H3O_MOLECULE = getImage( "H3O.png" );
+    public static final Image HA_MOLECULE = getImage( "HA.png" );
+    public static final Image M_PLUS_MOLECULE = getImage( "M_plus.png" );
+    public static final Image MOH_PLUS_MOLECULE = getImage( "MOH.png" );
+    public static final Image OH_MINUS_MOLECULE = getImage( "OH_minus.png" );
+    
+    private static final BufferedImage getImage( String resourceName ) {
+        return ABSResources.getImage( resourceName );
+    }
+}
