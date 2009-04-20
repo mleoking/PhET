67cf5af5239592063d7330db338dee25e6c76472
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-04-20 21:08:53 +0000

    improved support for free body diagram

--- trunk/simulations-java/simulations/the-ramp/scala-src/edu/colorado/phet/therampscala/graphics/FreeBodyDiagramNode.scala
+++ trunk/simulations-java/simulations/the-ramp/scala-src/edu/colorado/phet/therampscala/graphics/FreeBodyDiagramNode.scala
@@ -1,5 +1,6 @@
 package edu.colorado.phet.therampscala.graphics
 
+import common.phetcommon.view.graphics.transforms.ModelViewTransform2D
 import common.phetcommon.view.util.PhetFont
 import common.piccolophet.nodes.ArrowNode
 import common.piccolophet.PhetPCanvas
@@ -10,11 +11,16 @@ import scalacommon.math.Vector2D
 import scalacommon.util.Observable
 import umd.cs.piccolo.nodes.{PText, PPath}
 import umd.cs.piccolo.PNode
+import scalacommon.Predef._
 
-abstract class Vector extends Observable {
+trait Vector extends Observable {
   def getValue: Vector2D
+
+  def getColor: Color
 }
-class FreeBodyDiagramNode(val width: Int, val height: Int, val vectors: Vector*) extends PNode {
+class FreeBodyDiagramNode(val width: Int, val height: Int, val modelWidth: Double, val modelHeight: Double, val vectors: Vector*) extends PNode {
+  val transformT = new ModelViewTransform2D(new Rectangle2D.Double(-width / 2, -height / 2, width, height),
+    new Rectangle2D.Double(-modelWidth / 2, -modelHeight / 2, modelWidth, modelHeight))
   val background = new PPath(new Rectangle2D.Double(0, 0, width, height))
   addChild(background)
   val arrowInset = 4
@@ -31,12 +37,26 @@ class FreeBodyDiagramNode(val width: Int, val height: Int, val vectors: Vector*)
   }
   addChild(new AxisNode(0 + arrowInset, height / 2, width - arrowInset, height / 2, "x"))
   addChild(new AxisNode(width / 2, height - arrowInset, width / 2, 0 + arrowInset, "y"))
+
+  for (vector <- vectors) {
+    val node = new VectorNode(vector)
+    addChild(node)
+  }
+  class VectorNode(val vector: Vector) extends PNode {
+    val arrowNode = new ArrowNode(new Point2D.Double(width / 2, height / 2), transformT.modelToView(vector.getValue), 20, 20, 10)
+    arrowNode.setPaint(vector.getColor)
+    addChild(arrowNode)
+  }
 }
 
 object TestFBD extends Application {
   val frame = new JFrame
   val canvas = new PhetPCanvas
-  canvas.addScreenChild(new FreeBodyDiagramNode(200, 200))
+  canvas.addScreenChild(new FreeBodyDiagramNode(200, 200, 20, 20, new Vector() {
+    def getValue = new Vector2D(10, 10)
+
+    def getColor = Color.blue
+  }))
   frame.setContentPane(canvas)
   frame.setSize(800, 600)
   frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
