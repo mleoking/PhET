45e9abf4fbf74d550ef3c2738e5af4f8bffc743f
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-04-20 23:43:15 +0000

    demo of BoxLayout exception when container is not parented

--- simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/view/SwingLayoutNode.java
+++ simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/view/SwingLayoutNode.java
@@ -1,6 +1,7 @@
 package edu.colorado.phet.acidbasesolutions.view;
 
 import java.awt.*;
+import java.awt.geom.Rectangle2D;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.util.HashMap;
@@ -9,14 +10,17 @@ import javax.swing.*;
 
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.PhetPCanvas;
-import edu.colorado.phet.common.piccolophet.nodes.ShadowHTMLNode;
+import edu.colorado.phet.common.piccolophet.nodes.HTMLNode;
 import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.nodes.PPath;
 import edu.umd.cs.piccolo.nodes.PText;
 import edu.umd.cs.piccolox.pswing.PSwing;
 
 /**
- * This class uses a swing layout manager to position PNodes.
- * TODO: This implementation doesn't handle PNode resizes
+ * Uses Swing layout facilities to position PNodes.
+ * 
+ * @author Sam Reid
+ * @author Chris Malley (cmalley@pixelzoom.com)
  */
 public class SwingLayoutNode extends PNode {
     
@@ -24,10 +28,17 @@ public class SwingLayoutNode extends PNode {
     private final HashMap nodeComponentMap; // PNode -> NodeComponent
     private final PropertyChangeListener propertyChangeListener;
 
+    /**
+     * Uses a default FlowLayout.
+     */
     public SwingLayoutNode() {
         this( new FlowLayout() );
     }
     
+    /**
+     * Uses a specific layout.
+     * @param layoutManager
+     */
     public SwingLayoutNode( LayoutManager layoutManager ) {
         this.container = new JPanel( layoutManager );
         this.nodeComponentMap = new HashMap();
@@ -40,25 +51,68 @@ public class SwingLayoutNode extends PNode {
         };
     }
     
+    public Container getContainer() {
+        return container;
+    }
+    
+    /**
+     * Sets the layout. Like Swing, if you call this after adding nodes,
+     * the results can sometimes be a bit unpredictable.
+     * 
+     * @param layoutManager
+     */
     public void setLayout( LayoutManager layoutManager ) {
         container.setLayout( layoutManager );
         updateLayout();
     }
     
+    /**
+     * Adds a child with some layout constraints.
+     * Like Swing, bad things can happen if the type of the constraints
+     * isn't compatible with the layout manager.
+     * 
+     * @param child
+     * @param constraints
+     */
     public void addChild( PNode child, Object constraints ) {
         super.addChild( child );
         addNodeComponent( child, constraints );
     }
     
+    /**
+     * Adds a child with no layout constraints.
+     * Like Swing, bad things can happen the layout manager 
+     * is expecting to receive constraints.
+     * 
+     * @param child
+     */
     public void addChild( PNode child ) {
         addChild( child, null );
     }
     
+    /**
+     * Adds a child at the specified index.
+     * Like Swing, bad things can happen if the type of the constraints
+     * isn't compatible with the layout manager.
+     * 
+     * @param index
+     * @param child
+     * @param constraints
+     */
     public void addChild( int index, PNode child, Object constraints ) {
         super.addChild( index, child  );
         addNodeComponent( child, constraints );
     }
     
+    /**
+     * Adds a child at the specified index with no layout constraints.
+     * Like Swing, bad things can happen the layout manager 
+     * is expecting to receive constraints.
+     * 
+     * @param index
+     * @param child
+     * @param constraints
+     */
     public void addChild( int index, PNode node ) {
         super.addChild( index, node  );
     }
@@ -75,15 +129,26 @@ public class SwingLayoutNode extends PNode {
         return node;
     }
     
+    /*
+     * Adds a proxy component for a node.
+     */
     private void addNodeComponent( PNode node, Object constraints ) {
         NodeComponent component = new NodeComponent( node );
-        container.add( component, constraints );
+        if ( constraints == null ) {
+            container.add( component );
+        }
+        else {
+            container.add( component, constraints );
+        }
         container.setSize( container.getPreferredSize() );
         node.addPropertyChangeListener( propertyChangeListener );
         nodeComponentMap.put( node, component );
         updateLayout();
     }
     
+    /*
+     * Removes a proxy component for a node.
+     */
     private void removeNodeComponent( PNode node ) {
         if ( node != null ) {
             NodeComponent component = (NodeComponent) nodeComponentMap.get( node );
@@ -119,10 +184,6 @@ public class SwingLayoutNode extends PNode {
             this.node = node;
         }
         
-        public PNode getNode() {
-            return node;
-        }
-
         public Dimension getPreferredSize() {
             return new Dimension( (int) node.getFullBoundsReference().getWidth(), (int) node.getFullBoundsReference().getHeight() );
         }
@@ -133,51 +194,72 @@ public class SwingLayoutNode extends PNode {
         }
     }
 
+    // test cases
     public static void main( String[] args ) {
         
-        PhetPCanvas contentPane = new PhetPCanvas();
-        SwingLayoutNode borderLayoutNode = new SwingLayoutNode( new BorderLayout() );
+        Dimension canvasSize = new Dimension( 800, 600 );
+        PhetPCanvas canvas = new PhetPCanvas( canvasSize );
+        canvas.setPreferredSize( canvasSize );
+        PNode rootNode = new PNode();
+        canvas.addWorldChild( rootNode );
+        
+        BorderLayout borderLayout = new BorderLayout();
+        borderLayout.setHgap( 10 );
+        borderLayout.setVgap( 5 );
+        SwingLayoutNode borderLayoutNode = new SwingLayoutNode( borderLayout );
         borderLayoutNode.addChild( new PText( "West" ), BorderLayout.WEST );
         borderLayoutNode.addChild( new PText( "CENTER" ), BorderLayout.CENTER );
         borderLayoutNode.addChild( new PText( "South" ), BorderLayout.SOUTH );
         borderLayoutNode.addChild( new PText( "North" ), BorderLayout.NORTH );
         borderLayoutNode.addChild( new PText( "East" ), BorderLayout.EAST );
         borderLayoutNode.setOffset( 100, 100 );
-        contentPane.addScreenChild( borderLayoutNode );
+        rootNode.addChild( borderLayoutNode );
 
         SwingLayoutNode flowLayoutNode = new SwingLayoutNode( new FlowLayout() );
-        flowLayoutNode.addChild( new PText( "First Node" ) );
-        flowLayoutNode.addChild( new PText( "Second Node" ) );
+        flowLayoutNode.addChild( new PText( "1+1" ) );
+        flowLayoutNode.addChild( new PText( "2+2" ) );
         flowLayoutNode.setOffset( 200, 200 );
-        contentPane.addScreenChild( flowLayoutNode );
+        rootNode.addChild( flowLayoutNode );
 
-        SwingLayoutNode gridBagConstraintNode = new SwingLayoutNode( new GridBagLayout() );
+        SwingLayoutNode gridBagLayoutNode = new SwingLayoutNode( new GridBagLayout() );
         GridBagConstraints gridBagConstraints = new GridBagConstraints();
         gridBagConstraints.gridx = GridBagConstraints.RELATIVE;
-        gridBagConstraintNode.addChild( new PText( "First Node" ), gridBagConstraints );
-        gridBagConstraintNode.addChild( new PText( "Second Node" ), gridBagConstraints );
+        gridBagLayoutNode.addChild( new PText( "FirstNode" ), gridBagConstraints );
+        gridBagLayoutNode.addChild( new PText( "SecondNode" ), gridBagConstraints );
         gridBagConstraints.insets = new Insets( 50, 50, 50, 50 );
-        gridBagConstraintNode.addChild( new PText( "Third Node" ), gridBagConstraints );
-        gridBagConstraintNode.setOffset( 400, 400 );
-        contentPane.addScreenChild( gridBagConstraintNode );
+        gridBagLayoutNode.addChild( new PText( "ThirdNode" ), gridBagConstraints );
+        gridBagLayoutNode.setOffset( 400, 250 );
+        rootNode.addChild( gridBagLayoutNode );
 
-        SwingLayoutNode verticalLayout = new SwingLayoutNode( new GridBagLayout() );
-        verticalLayout.addChild( new PSwing( new JButton( "Midnight" ) ) );
-        verticalLayout.addChild( new PSwing( new JButton( "One O'Clock" ) ) );
-        verticalLayout.addChild( new PSwing( new JButton( "Two" ) ) );
-        verticalLayout.addChild( new PSwing( new JLabel( "Three" ) ) );
-        verticalLayout.addChild( new PSwing( new JSlider() ) );
-        verticalLayout.addChild( new PSwing( new JTextField( "Four" ) ) );
-        ShadowHTMLNode child = new ShadowHTMLNode( "<html> Five </html>", Color.blue );
-        child.setFont( new PhetFont( 15, true ) );
-        verticalLayout.addChild( child );
-        verticalLayout.setOffset( 100, 400 );
-        contentPane.addScreenChild( verticalLayout );
-
+        SwingLayoutNode horizontalLayoutNode = new SwingLayoutNode( new GridBagLayout() );
+        horizontalLayoutNode.addChild( new PSwing( new JButton( "Zero" ) ) );
+        horizontalLayoutNode.addChild( new PSwing( new JButton( "One" ) ) );
+        horizontalLayoutNode.addChild( new PSwing( new JButton( "Two" ) ) );
+        horizontalLayoutNode.addChild( new PSwing( new JLabel( "Three" ) ) );
+        horizontalLayoutNode.addChild( new PSwing( new JSlider() ) );
+        horizontalLayoutNode.addChild( new PSwing( new JTextField( "Four" ) ) );
+        HTMLNode htmlNode = new HTMLNode( "<html>Five</html>", new PhetFont( 15, true ), Color.blue );
+        htmlNode.scale( 3 );
+        horizontalLayoutNode.addChild( htmlNode );
+        horizontalLayoutNode.setOffset( 100, 400 );
+        rootNode.addChild( horizontalLayoutNode );
+        
+        JPanel p = new JPanel();
+        p.setLayout( new BoxLayout( p, BoxLayout.Y_AXIS ) );
+        p.add( new JLabel( "foo" ) );
+        p.doLayout();
+        
+        SwingLayoutNode boxLayoutNode = new SwingLayoutNode();
+        boxLayoutNode.setLayout( new BoxLayout( boxLayoutNode.getContainer(), BoxLayout.Y_AXIS ) );
+        boxLayoutNode.addChild( new PPath( new Rectangle2D.Double( 0, 0, 50, 50 ) ) );
+        boxLayoutNode.addChild( new PPath( new Rectangle2D.Double( 0, 0, 100, 50 ) ) );
+        boxLayoutNode.setOffset( 300, 300 );
+        rootNode.addChild( boxLayoutNode );
+        
         JFrame frame = new JFrame();
-        frame.setContentPane( contentPane );
+        frame.setContentPane( canvas );
+        frame.pack();
         frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-        frame.setSize( 800, 600 );
         frame.setVisible( true );
     }
 }
