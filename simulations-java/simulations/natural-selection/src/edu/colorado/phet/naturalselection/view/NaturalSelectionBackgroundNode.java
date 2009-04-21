2b93923a8e7850a948428c9751bc2578ed5229be
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-04-21 11:27:42 +0000

    Natural Selection: refactoring and documentation

--- simulations-java/simulations/natural-selection/src/edu/colorado/phet/naturalselection/view/NaturalSelectionBackgroundNode.java
+++ simulations-java/simulations/natural-selection/src/edu/colorado/phet/naturalselection/view/NaturalSelectionBackgroundNode.java
@@ -3,20 +3,31 @@
 package edu.colorado.phet.naturalselection.view;
 
 import edu.colorado.phet.naturalselection.NaturalSelectionResources;
+import edu.colorado.phet.naturalselection.NaturalSelectionConstants;
 import edu.colorado.phet.naturalselection.model.Bunny;
 import edu.colorado.phet.naturalselection.module.naturalselection.NaturalSelectionModel;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.nodes.PImage;
 
+/**
+ * Displays the background for the environment
+ *
+ * @author Jonathan Olson
+ */
 public class NaturalSelectionBackgroundNode extends PNode implements NaturalSelectionModel.NaturalSelectionModelListener {
 
     private int climate;
     private PImage equatorImage;
     private PImage arcticImage;
 
+    /**
+     * Constructor
+     * @param initialClimate The initial climate value
+     */
     public NaturalSelectionBackgroundNode( int initialClimate ) {
-        equatorImage = NaturalSelectionResources.getImageNode( "natural_selection_background_equator_2.png" );
-        arcticImage = NaturalSelectionResources.getImageNode( "natural_selection_background_arctic_2.png" );
+        // initialize images
+        equatorImage = NaturalSelectionResources.getImageNode( NaturalSelectionConstants.IMAGE_BACKGROUND_EQUATOR );
+        arcticImage = NaturalSelectionResources.getImageNode( NaturalSelectionConstants.IMAGE_BACKGROUND_ARCTIC );
 
         climate = initialClimate;
 
@@ -48,6 +59,10 @@ public class NaturalSelectionBackgroundNode extends PNode implements NaturalSele
             addChild( arcticImage );
         }
     }
+
+    //----------------------------------------------------------------------------
+    // Event handlers
+    //----------------------------------------------------------------------------
 
     public void onMonthChange( String monthName ) {
 
