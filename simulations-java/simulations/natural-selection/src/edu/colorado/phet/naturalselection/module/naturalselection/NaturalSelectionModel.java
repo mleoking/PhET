542b25c8ac299c5bf63a298930818f22b6bcd440
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-04-21 10:37:52 +0000

    Natural Selection: refactoring and documentation

--- simulations-java/simulations/natural-selection/src/edu/colorado/phet/naturalselection/module/naturalselection/NaturalSelectionModel.java
+++ simulations-java/simulations/natural-selection/src/edu/colorado/phet/naturalselection/module/naturalselection/NaturalSelectionModel.java
@@ -10,46 +10,83 @@ import edu.colorado.phet.common.phetcommon.model.clock.ClockEvent;
 import edu.colorado.phet.naturalselection.defaults.NaturalSelectionDefaults;
 import edu.colorado.phet.naturalselection.model.*;
 
+/**
+ * The model itself for the Natural Selection simulation
+ *
+ * @author Jonathan Olson
+ */
 public class NaturalSelectionModel extends ClockAdapter {
 
+    // types of climates
     public static final int CLIMATE_EQUATOR = 0;
     public static final int CLIMATE_ARCTIC = 1;
 
+    // types of selection factors
     public static final int SELECTION_NONE = 0;
     public static final int SELECTION_FOOD = 1;
     public static final int SELECTION_WOLVES = 2;
 
+    /**
+     * The simulation clock
+     */
     private NaturalSelectionClock clock;
 
+    /**
+     * A list of all of the (model) bunnies.
+     * WARNING: do NOT change the order of this list 
+     */
     private ArrayList bunnies;
+
+    // starting bunnies
     private Bunny rootFather;
     private Bunny rootMother;
 
     private ArrayList listeners;
 
-    public static double TICKS_PER_MONTH = 25.0;
-
+    /**
+     * the last tick at which a month changed. When the difference between this and the actual tick is large enough,
+     * another month will change
+     */
     private double lastMonthTick = 0;
 
+    /**
+     * The current month name, as an index into MONTH_NAMES
+     */
     private int currentMonth = 0;
     public static final String[] MONTH_NAMES = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 
+    /**
+     * The current generation
+     */
     private int generation = 0;
 
+    /**
+     * The current climate (CLIMATE_EQUATOR or CLIMATE_ARCTIC) 
+     */
     private int climate = CLIMATE_EQUATOR;
+
+    /**
+     * The current selection factor (SELECTION_NONE, SELECTION_FOOD, or SELECTION_WOLVES) 
+     */
     private int selectionFactor = SELECTION_NONE;
 
-    public NaturalSelectionModel( NaturalSelectionClock _clock ) {
+    /**
+     * Constructor
+     * @param clock The simulation clock
+     */
+    public NaturalSelectionModel( NaturalSelectionClock clock ) {
 
-        clock = _clock;
+        this.clock = clock;
 
         bunnies = new ArrayList();
         listeners = new ArrayList();
 
+        // set up the genes to this module
         ColorGene.getInstance().setModel( this );
         TeethGene.getInstance().setModel( this );
         TailGene.getInstance().setModel( this );
 
+        // create the starting bunnies
         rootFather = new Bunny( null, null, 0 );
         rootMother = new Bunny( null, null, 0 );
         rootFather.setPotentialMate( rootMother );
@@ -57,9 +94,12 @@ public class NaturalSelectionModel extends ClockAdapter {
         bunnies.add( rootFather );
         bunnies.add( rootMother );
 
-        clock.addClockListener( this );
+        this.clock.addClockListener( this );
     }
 
+    /**
+     * Reset the entire module
+     */
     public void reset() {
 
         climate = CLIMATE_EQUATOR;
@@ -100,22 +140,11 @@ public class NaturalSelectionModel extends ClockAdapter {
         notifyNewBunny( rootMother );
     }
 
-    public int getPopulation() {
-        // TODO: easier way? maybe count a filtered ArrayList?
-        int ret = 0;
-        for ( int i = 0; i < bunnies.size(); i++ ) {
-            Bunny bunny = (Bunny) bunnies.get( i );
-            if ( bunny.isAlive() ) {
-                ret++;
-            }
-        }
-        return ret;
-    }
 
-    public int getGeneration() {
-        return generation;
-    }
 
+    /**
+     * Causes another generation to be born (and everything else that happens when a new generation occurs)
+     */
     private void nextGeneration() {
         System.out.println( "***** Mating season, creating next generation" );
 
@@ -132,9 +161,13 @@ public class NaturalSelectionModel extends ClockAdapter {
         System.out.println( "\tPopulation: " + getPopulation() );
     }
 
+    /**
+     * Causes all bunnies that can reproduce to do so
+     */
     private void mateBunnies() {
         Iterator iter = bunnies.iterator();
 
+        // temporarily store the new bunnies that we are creating
         ArrayList newBunnies = new ArrayList();
 
         while ( iter.hasNext() ) {
@@ -157,6 +190,9 @@ public class NaturalSelectionModel extends ClockAdapter {
         }
     }
 
+    /**
+     * Make all of the bunnies older
+     */
     private void ageBunnies() {
         Iterator iter = bunnies.iterator();
         while ( iter.hasNext() ) {
@@ -164,23 +200,38 @@ public class NaturalSelectionModel extends ClockAdapter {
         }
     }
 
+    /**
+     * Increment a month. If applicable, start a new generation.
+     */
     private void nextMonth() {
         currentMonth++;
         if ( currentMonth >= 12 ) {
             currentMonth = 0;
         }
         notifyMonthChange();
-        if ( getCurrentMonth().equals( "August" ) ) {
+        if ( currentMonth == NaturalSelectionDefaults.GENERATION_MONTH ) {
             nextGeneration();
         }
     }
 
-    public void simulationTimeChanged( ClockEvent event ) {
-        //System.out.println( event.getSimulationTime() - lastMonthTick );
-        while ( event.getSimulationTime() - lastMonthTick > TICKS_PER_MONTH ) {
-            lastMonthTick += TICKS_PER_MONTH;
-            nextMonth();
+    //----------------------------------------------------------------------------
+    // Getters and setters
+    //----------------------------------------------------------------------------
+
+    public int getPopulation() {
+        // TODO: easier way? maybe count a filtered ArrayList?
+        int ret = 0;
+        for ( int i = 0; i < bunnies.size(); i++ ) {
+            Bunny bunny = (Bunny) bunnies.get( i );
+            if ( bunny.isAlive() ) {
+                ret++;
+            }
         }
+        return ret;
+    }
+
+    public int getGeneration() {
+        return generation;
     }
 
     public String getCurrentMonth() {
@@ -191,6 +242,10 @@ public class NaturalSelectionModel extends ClockAdapter {
         return bunnies;
     }
 
+    /**
+     * Get only alive bunnies
+     * @return A list of bunnies that are alive
+     */
     public ArrayList getAliveBunnyList() {
         ArrayList ret = new ArrayList();
         Iterator iter = bunnies.iterator();
@@ -203,6 +258,11 @@ public class NaturalSelectionModel extends ClockAdapter {
         return ret;
     }
 
+    /**
+     * Get a list of all bunnies that were born in the desired generation
+     * @param desiredGeneration The generation that bunnies were born in.
+     * @return A list of all bunnies (alive or dead) born in desiredGeneration
+     */
     public ArrayList getBunnyGenerationList( int desiredGeneration ) {
         ArrayList ret = new ArrayList();
         Iterator iter = bunnies.iterator();
@@ -244,7 +304,21 @@ public class NaturalSelectionModel extends ClockAdapter {
     }
 
 
-    // notification
+    //----------------------------------------------------------------------------
+    // Event handlers
+    //----------------------------------------------------------------------------
+
+    public void simulationTimeChanged( ClockEvent event ) {
+        while ( event.getSimulationTime() - lastMonthTick > NaturalSelectionDefaults.TICKS_PER_MONTH ) {
+            lastMonthTick += NaturalSelectionDefaults.TICKS_PER_MONTH;
+            nextMonth();
+        }
+    }
+
+
+    //----------------------------------------------------------------------------
+    // Notifications
+    //----------------------------------------------------------------------------
 
     private void notifyMonthChange() {
         Iterator iter = listeners.iterator();
@@ -281,7 +355,9 @@ public class NaturalSelectionModel extends ClockAdapter {
         }
     }
 
-    // listeners
+    //----------------------------------------------------------------------------
+    // Listeners
+    //----------------------------------------------------------------------------
 
     public void addListener( NaturalSelectionModelListener listener ) {
         listeners.add( listener );
@@ -291,17 +367,40 @@ public class NaturalSelectionModel extends ClockAdapter {
         listeners.remove( listener );
     }
 
+    /**
+     * The interface for objects that wish to receive model change events
+     */
     public interface NaturalSelectionModelListener {
         // shortcut: implemented in PopulationCanvas, TimeDisplayPanel, SpritesNode, NaturalSelectionBackgroundNode
 
+        /**
+         * Called when the month changes
+         * @param monthName The new month name
+         */
         public void onMonthChange( String monthName );
 
+        /**
+         * Called when the generation changes
+         * @param generation The new generation number
+         */
         public void onGenerationChange( int generation );
 
+        /**
+         * Called when a new bunny is born
+         * @param bunny The bunny
+         */
         public void onNewBunny( Bunny bunny );
 
+        /**
+         * Called when the climate changes
+         * @param climate The new climate
+         */
         public void onClimateChange( int climate );
 
+        /**
+         * Called when the selection factor changes
+         * @param selectionFactor The new selection factor
+         */
         public void onSelectionFactorChange( int selectionFactor );
     }
 
