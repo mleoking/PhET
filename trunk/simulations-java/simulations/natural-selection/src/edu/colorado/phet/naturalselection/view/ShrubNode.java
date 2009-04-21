fa30506cf6449da6abb18db2fec19d8c73367c67
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-04-21 18:49:14 +0000

    Natural Selection: refactoring and documentation

--- trunk/simulations-java/simulations/natural-selection/src/edu/colorado/phet/naturalselection/view/ShrubNode.java
+++ trunk/simulations-java/simulations/natural-selection/src/edu/colorado/phet/naturalselection/view/ShrubNode.java
@@ -2,16 +2,35 @@
 
 package edu.colorado.phet.naturalselection.view;
 
+import java.awt.geom.Point2D;
+
+import edu.colorado.phet.naturalselection.NaturalSelectionConstants;
 import edu.colorado.phet.naturalselection.NaturalSelectionResources;
 import edu.umd.cs.piccolo.nodes.PImage;
 
+/**
+ * Displays a shrub in piccolo
+ *
+ * @author Jonathan Olson
+ */
 public class ShrubNode extends NaturalSelectionSprite {
 
-    public PImage shrubImage;
-
+    /**
+     * Constructor. Creates a shrub at the specified canvas location with the specified scale, with the correct depth
+     * so that bunnies display correctly in front and behind the shrub
+     *
+     * @param baseX Canvas X location (center of shrub)
+     * @param baseY Canvas y location (bottom of shrub)
+     * @param scale Scale
+     */
     public ShrubNode( double baseX, double baseY, double scale ) {
-        shrubImage = NaturalSelectionResources.getImageNode( "shrub.png" );
-        shrubImage.setOffset( 0, 15 );
+        // load the image
+        PImage shrubImage = NaturalSelectionResources.getImageNode( NaturalSelectionConstants.IMAGE_SHRUB );
+
+        // offset the shrub image so it looks like the base is where the bottom of the image was before
+        Point2D offset = NaturalSelectionConstants.IMAGE_SHRUB_OFFSET;
+        shrubImage.setOffset( offset.getX(), offset.getY() );
+
         addChild( shrubImage );
         setSpriteLocation( baseX, 0, getInverseGroundZDepth( baseY ) );
         setScale( scale );
