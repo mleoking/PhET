1a327cbfffc88cb7cc342db4a796c9953beaa78a
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-04-21 01:47:44 +0000

    fix image resource name

--- trunk/simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/ABSImages.java
+++ trunk/simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/ABSImages.java
@@ -12,7 +12,7 @@ public class ABSImages {
     public static final Image B_MOLECULE = getImage( "B.png" );
     public static final Image BH_PLUS_MOLECULE = getImage( "BH_plus.png" );
     public static final Image H2O_MOLECULE = getImage( "H2O.png" );
-    public static final Image H3O_MOLECULE = getImage( "H3O.png" );
+    public static final Image H3O_PLUS_MOLECULE = getImage( "H3O_plus.png" );
     public static final Image HA_MOLECULE = getImage( "HA.png" );
     public static final Image M_PLUS_MOLECULE = getImage( "M_plus.png" );
     public static final Image MOH_PLUS_MOLECULE = getImage( "MOH.png" );
