92bcd593e4d364e4432dfadc10b4f7b10cffed3a
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-04-21 10:41:12 +0000

    Natural Selection: refactoring and documentation

--- simulations-java/simulations/natural-selection/src/edu/colorado/phet/naturalselection/util/ImagePanel.java
+++ simulations-java/simulations/natural-selection/src/edu/colorado/phet/naturalselection/util/ImagePanel.java
@@ -9,15 +9,28 @@ import javax.swing.*;
 
 import edu.colorado.phet.naturalselection.NaturalSelectionResources;
 
+/**
+ * Creates a panel that contains the desired image
+ *
+ * @author Jonathan Olson
+ */
 public class ImagePanel extends JPanel {
 
     private BufferedImage image;
 
+    /**
+     * Constructor
+     * @param imageName The filename of the image
+     */
     public ImagePanel( String imageName ) {
         image = NaturalSelectionResources.getImage( imageName );
         setPreferredSize( new Dimension( image.getWidth(), image.getHeight() ) );
     }
 
+    /**
+     * Constructor
+     * @param actualImage The image
+     */
     public ImagePanel( BufferedImage actualImage ) {
         image = actualImage;
         setPreferredSize( new Dimension( image.getWidth(), image.getHeight() ) );
