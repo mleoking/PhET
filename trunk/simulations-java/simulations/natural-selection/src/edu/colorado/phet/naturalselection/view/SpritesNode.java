206437be552ef22c64f70ad7839cd9b860bc7f0d
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-04-21 18:54:04 +0000

    Natural Selection: refactoring and documentation

--- trunk/simulations-java/simulations/natural-selection/src/edu/colorado/phet/naturalselection/view/SpritesNode.java
+++ trunk/simulations-java/simulations/natural-selection/src/edu/colorado/phet/naturalselection/view/SpritesNode.java
@@ -12,16 +12,29 @@ import edu.colorado.phet.naturalselection.model.Bunny;
 import edu.colorado.phet.naturalselection.module.naturalselection.NaturalSelectionModel;
 import edu.umd.cs.piccolo.PNode;
 
+/**
+ * Handles all of the sprites on the simulation canvas (bunnies, wolves, trees, shrubs, etc.)
+ *
+ * @author Jonathan Olson
+ */
 public class SpritesNode extends PNode implements NaturalSelectionModel.NaturalSelectionModelListener {
 
+    /**
+     * A list of the variable (bunny and wolf) sprites
+     */
     private ArrayList sprites;
 
+    // trees and shrubs lists
     private ArrayList trees;
     private ArrayList shrubs;
 
+    // cached climate and selection factor
     private int oldClimate;
     private int oldSelection;
 
+    /**
+     * Constructor
+     */
     public SpritesNode() {
         sprites = new ArrayList();
         trees = new ArrayList();
@@ -29,6 +42,9 @@ public class SpritesNode extends PNode implements NaturalSelectionModel.NaturalS
 
         oldClimate = NaturalSelectionModel.CLIMATE_EQUATOR;
         oldSelection = NaturalSelectionModel.SELECTION_NONE;
+
+
+        // create the trees (manually positioned and sized)
 
         TreeNode bigTree = new TreeNode( 125, 138, 1 );
         addChildSprite( bigTree );
@@ -41,6 +57,9 @@ public class SpritesNode extends PNode implements NaturalSelectionModel.NaturalS
         TreeNode smallTree = new TreeNode( 635, 90, 0.2 );
         addChildSprite( smallTree );
         trees.add( smallTree );
+
+
+        // create the shrubs (manually positioned and sized)
 
         ShrubNode shrubA = new ShrubNode( 80, 330, 1 );
         addChildSprite( shrubA );
@@ -59,6 +78,9 @@ public class SpritesNode extends PNode implements NaturalSelectionModel.NaturalS
 
     }
 
+    //----------------------------------------------------------------------------
+    // Event handlers
+    //----------------------------------------------------------------------------
 
     public void onMonthChange( String monthName ) {
 
@@ -69,6 +91,8 @@ public class SpritesNode extends PNode implements NaturalSelectionModel.NaturalS
     }
 
     public void onClimateChange( int climate ) {
+        // turn the trees on and off
+
         if ( climate == oldClimate ) {
             return;
         }
@@ -89,6 +113,8 @@ public class SpritesNode extends PNode implements NaturalSelectionModel.NaturalS
     }
 
     public void onSelectionFactorChange( int selectionFactor ) {
+        // turn the shrubs on and off
+
         if ( selectionFactor == oldSelection ) {
             return;
         }
@@ -119,19 +145,32 @@ public class SpritesNode extends PNode implements NaturalSelectionModel.NaturalS
         sprites.clear();
     }
 
+    /**
+     * Called from the model when a new bunny occurs. Used here to create the bunny sprite, and to randomly position it
+     * @param bunny The bunny
+     */
     public void onNewBunny( Bunny bunny ) {
+        // create a bunny node with the correct visual appearance
         BunnyNode bunnyNode = new BunnyNode( bunny.getColorGenotype().getPhenotype(), bunny.getTeethGenotype().getPhenotype(), bunny.getTailGenotype().getPhenotype() );
+
+        // randomly position the bunny
         double bunnyDepth = Math.random();
         double sidePadding = 25;
         double locationX = ( NaturalSelectionDefaults.VIEW_SIZE.getWidth() - sidePadding * 2 ) * Math.random() + sidePadding;
         double locationY = 0;
         double locationZ = 1 + bunnyDepth * 1.5;
         bunnyNode.setSpriteLocation( locationX, locationY, locationZ );
+
+        // add the bunny
         addChildSprite( bunnyNode );
         bunny.addListener( bunnyNode );
         sprites.add( bunnyNode );
     }
 
+    /**
+     * Add a sprite. Inserted into the correct Z depth 
+     * @param sprite The sprite to add
+     */
     public void addChildSprite( NaturalSelectionSprite sprite ) {
         List displayList = getChildrenReference();
 
