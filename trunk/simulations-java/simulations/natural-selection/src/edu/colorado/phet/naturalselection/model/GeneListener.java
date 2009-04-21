ab90f2ba4c1b9a6986d7bf2113b2f5629147255d
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-04-21 10:04:50 +0000

    Natural Selection: refactoring and documentation

--- trunk/simulations-java/simulations/natural-selection/src/edu/colorado/phet/naturalselection/model/GeneListener.java
+++ trunk/simulations-java/simulations/natural-selection/src/edu/colorado/phet/naturalselection/model/GeneListener.java
@@ -2,8 +2,22 @@
 
 package edu.colorado.phet.naturalselection.model;
 
+/**
+ * The interface for objects that want to receive events from Genes
+ *
+ * @author Jonathan Olson
+ */
 public interface GeneListener {
-    public void onChangeDominantAllele( boolean primary );
+    /**
+     * Event sent when the dominant allele is changed
+     * @param primary Whether the new dominant allele is the primary or secondary allele
+     */
+    public void onChangeDominantAllele( boolean primary ); // TODO: change to the allele, AND send primary?
 
+    /**
+     * Event sent when the distribution of traits in bunnies changes
+     * @param primary Number of bunnies with the primary phenotype
+     * @param secondary Number of bunnies with the second phenotype
+     */
     public void onChangeDistribution( int primary, int secondary );
 }
