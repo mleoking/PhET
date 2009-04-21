0736797aac67ce0a82ffdcfd4a4c15b7ac1155b5
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-04-21 11:32:25 +0000

    Natural Selection: refactoring and documentation

--- trunk/simulations-java/simulations/natural-selection/src/edu/colorado/phet/naturalselection/view/NaturalSelectionSprite.java
+++ trunk/simulations-java/simulations/natural-selection/src/edu/colorado/phet/naturalselection/view/NaturalSelectionSprite.java
@@ -8,9 +8,14 @@ import edu.colorado.phet.naturalselection.defaults.NaturalSelectionDefaults;
 import edu.colorado.phet.naturalselection.module.naturalselection.NaturalSelectionCanvas;
 import edu.umd.cs.piccolo.PNode;
 
+/**
+ * Abstract class for all piccolo nodes to be displayed in and around the bunnies
+ *
+ * @author Jonathan Olson
+ */
 public abstract class NaturalSelectionSprite extends PNode implements Comparable {
 
-
+    // position
     private double spriteX = 0;
     private double spriteY = 0;
     private double spriteZ = 0;
@@ -18,6 +23,10 @@ public abstract class NaturalSelectionSprite extends PNode implements Comparable
     public NaturalSelectionSprite() {
 
     }
+
+    //----------------------------------------------------------------------------
+    // Getters and setters
+    //----------------------------------------------------------------------------
 
     public double getSpriteX() {
         return spriteX;
@@ -49,19 +58,38 @@ public abstract class NaturalSelectionSprite extends PNode implements Comparable
         setSpriteZ( z );
     }
 
+    /**
+     * Layout function to determine 2D position from the 3D coordinates
+     * @return 2D canvas position
+     */
     public Point2D getCanvasLocation() {
+        // assuming for now that we are on a hill, so 0 y == on hill. spriteY will show above and below that
         double locationY = ( -NaturalSelectionDefaults.VIEW_SIZE.getHeight() + NaturalSelectionCanvas.HORIZON ) * ( 1 - ( spriteZ - 1 ) / 1.5 ) + spriteY;
         return new Point2D.Double( spriteX, NaturalSelectionCanvas.HORIZON - ( locationY / spriteZ ) );
     }
 
+    /**
+     * Compute the necessary Z depth for a particular canvas height y (used for placing trees and shrubbery!)
+     * @param y The canvas location y
+     * @return The necessary spriteZ depth that with spriteY == 0 will return the canvas location y
+     */
     public double getInverseGroundZDepth( double y ) {
         return ( ( 5 * NaturalSelectionCanvas.HORIZON - 5 * NaturalSelectionDefaults.VIEW_SIZE.getHeight() ) / ( -3 * y + 5 * NaturalSelectionCanvas.HORIZON - 2 * NaturalSelectionDefaults.VIEW_SIZE.getHeight() ) );
     }
 
+    /**
+     * Determine the scale that should be applied
+     * @return The scale
+     */
     public double getCanvasScale() {
         return 1.0 / spriteZ;
     }
 
+    /**
+     * Allow the sprites to be sorted quickly for display depth
+     * @param otherObject Another sprite to be compared to
+     * @return The usual compare function return values
+     */
     public int compareTo( Object otherObject ) {
         NaturalSelectionSprite other = (NaturalSelectionSprite) otherObject;
         if ( spriteZ == other.spriteZ ) {
