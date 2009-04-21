be22880d315a3f2912f472e0cea2f5aeae631ade
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-04-21 18:40:10 +0000

    Natural Selection: refactoring and documentation

--- simulations-java/simulations/natural-selection/src/edu/colorado/phet/naturalselection/view/PopulationGraphNode.java
+++ simulations-java/simulations/natural-selection/src/edu/colorado/phet/naturalselection/view/PopulationGraphNode.java
@@ -6,34 +6,58 @@ import java.awt.*;
 import java.awt.geom.GeneralPath;
 
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
+import edu.colorado.phet.naturalselection.defaults.NaturalSelectionDefaults;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.nodes.PPath;
 import edu.umd.cs.piccolo.nodes.PText;
 
+/**
+ * Draws a simple population bar graph with labels
+ *
+ * @author Jonathan Olson
+ */
 public class PopulationGraphNode extends PNode {
 
-    public PPath bar;
-    public PPath axis;
-    public PText label;
-    public PText quantity;
+    // piccolo nodes used for display
+    private PPath bar;
+    private PPath axis;
+    private PText label;
+    private PText quantity;
 
+    // current displayed population
     private int population;
 
+    // vertical distance from the origin to the base of the bar chart
     private static float BASELINE = 170;
+
+    // x offset to the bar's left side
     private static float BAR_LEFT = 25;
+
+    // x offset to the bar's right side
     private static float BAR_RIGHT = 55;
+
+    // same with the axis
     private static float AXIS_LEFT = 15;
     private static float AXIS_RIGHT = 65;
 
+    // total available width
     private static float TOTAL_WIDTH = 80;
 
+    // stroke widths
     private static float AXIS_STROKE_WIDTH = 3f;
     private static float BAR_STROKE_WIDTH = 1f;
 
+    // pixels per 1 bunny, can be fractional
     private static float POPULATION_MULTIPLIER = 1f;
 
+    // the maximum top of the bar
     private static float GRAPH_TOP = 20;
 
+    /**
+     * Constructor
+     *
+     * @param startPopulation The initial population
+     */
     public PopulationGraphNode( int startPopulation ) {
         axis = new PPath();
         bar = new PPath();
@@ -45,13 +69,18 @@ public class PopulationGraphNode extends PNode {
     }
 
     public void reset() {
-        updatePopulation( 2 );
+        updatePopulation( NaturalSelectionDefaults.DEFAULT_NUMBER_OF_BUNNIES );
     }
 
     private float getGraphHeight() {
         return ( (float) population ) * POPULATION_MULTIPLIER;
     }
 
+    /**
+     * Cap the graph height so it won't display over other componenets.
+     *
+     * @return The (possibly) capped height
+     */
     private float getTopPosition() {
         float top = BASELINE - getGraphHeight();
         if ( top < GRAPH_TOP ) {
@@ -60,6 +89,11 @@ public class PopulationGraphNode extends PNode {
         return top;
     }
 
+    /**
+     * Refresh the display with the new population
+     *
+     * @param newPopulation New population
+     */
     public void updatePopulation( int newPopulation ) {
         population = newPopulation;
 
@@ -70,6 +104,9 @@ public class PopulationGraphNode extends PNode {
         drawLabels();
     }
 
+    /**
+     * Draws the axis
+     */
     private void drawAxis() {
         axis.setStroke( new BasicStroke( AXIS_STROKE_WIDTH ) );
         axis.setStrokePaint( Color.BLACK );
@@ -82,6 +119,9 @@ public class PopulationGraphNode extends PNode {
         conditionalAdd( axis );
     }
 
+    /**
+     * Draws the labels (pop above bar)
+     */
     private void drawLabels() {
         label.setFont( new PhetFont() );
         label.setOffset( ( TOTAL_WIDTH - label.getWidth() ) / 2, BASELINE + 10 );
@@ -92,6 +132,9 @@ public class PopulationGraphNode extends PNode {
         conditionalAdd( quantity );
     }
 
+    /**
+     * Draws the bar of the bar chart
+     */
     private void drawBar() {
         bar.setStroke( new BasicStroke( BAR_STROKE_WIDTH ) );
         bar.setStrokePaint( Color.BLACK );
@@ -104,15 +147,23 @@ public class PopulationGraphNode extends PNode {
         path.lineTo( BAR_LEFT, BASELINE );
         path.lineTo( BAR_RIGHT, BASELINE );
         path.lineTo( BAR_RIGHT, top );
+
+        // if capped, use a dashed line instead
         if ( top == GRAPH_TOP ) {
             bar.setStroke( new BasicStroke( BAR_STROKE_WIDTH, BasicStroke.CAP_SQUARE, BasicStroke.JOIN_BEVEL, 10.0f, new float[]{2.0f, 2.0f}, 0.0f ) );
         }
+
         path.closePath();
         bar.setPathTo( path );
 
         conditionalAdd( bar );
     }
 
+    /**
+     * Helper function that only adds the node if it is not already a child
+     *
+     * @param node The node to possibly add
+     */
     private void conditionalAdd( PNode node ) {
         if ( node.getParent() == null ) {
             addChild( node );
