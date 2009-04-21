40626b8b55dc5b771fd24311caabba7a35cd35cc
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-04-21 18:32:15 +0000

    Added the ability to insert the timestamp of the installers' creation into the database.

--- trunk/util/phet-installer/new-automated/deploy-installers.php
+++ trunk/util/phet-installer/new-automated/deploy-installers.php
@@ -13,10 +13,38 @@
     require_once("config.php");
     require_once("global.php");
     require_once("installer-util.php");
+    require_once("file-util.php");
+
+    //--------------------------------------------------------------------------
+    // Function to insert the timestamp for the creation of the installers into
+    // the database that will be read by the deployed sims in order to
+    // determine whether a new version of the installer is available.
+    //--------------------------------------------------------------------------
+    function insert_timestamp_into_db(){
+        if ( !file_exists( CREATION_TIMESTAMP_FILE_NAME ) ){
+            flushing_echo( "Error: Unable to locate timestamp file ".CREATION_TIMESTAMP_FILE_NAME.", time stamp not inserted into DB." );
+        }
+
+        // Read the timestamp, which should be the only thing in the file.
+        $timestamp = file_get_contents( CREATION_TIMESTAMP_FILE_NAME );
+
+        // Execute the PHP script that inserts the value into the DB.
+        //exec( "/web/htdocs/phet/cl_utils/add-installer-timestamp.php $TIMESTAMP" );
+        flushing_echo( "!!!!! Timestamp insertion stubbed, would execute: /web/htdocs/phet/cl_utils/add-installer-timestamp.php $timestamp" );
+    }
+
+    //--------------------------------------------------------------------------
+    // Function to deploy the installers and insert the creation date into the
+    // database.
+    //--------------------------------------------------------------------------
+    function deploy_all(){
+        deploy_installers();
+        insert_timestamp_into_db();
+    }
 
     //--------------------------------------------------------------------------
     // Entry point for this PHP file.
     //--------------------------------------------------------------------------
-    deploy_installers();
+    deploy_all();
 
 ?>
