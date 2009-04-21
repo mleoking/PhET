5d71452d90895f6779563aa112cd73bea9cc48d5
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-04-21 09:51:08 +0000

    Natural Selection: refactoring and documentation

--- trunk/simulations-java/simulations/natural-selection/src/edu/colorado/phet/naturalselection/model/ColorGene.java
+++ trunk/simulations-java/simulations/natural-selection/src/edu/colorado/phet/naturalselection/model/ColorGene.java
@@ -2,8 +2,13 @@
 
 package edu.colorado.phet.naturalselection.model;
 
+/**
+ * The gene for different colors of fur (singleton)
+ *
+ * @author Jonathan Olson
+ */
 public class ColorGene extends Gene {
-
+    // fur alleles (also used for phenotypes)
     public static final Allele WHITE_ALLELE = new Allele( "White-Colored Fur" );
     public static final Allele BROWN_ALLELE = new Allele( "Brown-Colored Fur" );
 
@@ -20,14 +25,27 @@ public class ColorGene extends Gene {
         return instance;
     }
 
+    /**
+     * Mutation probability
+     * @return The fraction of bunnies that have a mutation when enabled.
+     */
     public double getMutationFraction() {
         return 0.2;
     }
 
+    /**
+     * Extracts the correct genotype from a bunny
+     * @param bunny The bunny you need the DNA from
+     * @return The bunny's Genotype for this gene
+     */
     public Genotype getBunnyGenotype( Bunny bunny ) {
         return bunny.getColorGenotype();
     }
 
+    /**
+     * The name of the gene
+     * @return The name of the gene
+     */
     public String getName() {
         return "Color";
     }
