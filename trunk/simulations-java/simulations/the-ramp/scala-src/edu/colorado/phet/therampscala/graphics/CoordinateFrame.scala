176e59606be707578252179dce01051c6b655a00
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-04-22 14:40:27 +0000

    started coordinate frame

--- trunk/simulations-java/simulations/the-ramp/scala-src/edu/colorado/phet/therampscala/graphics/CoordinateFrame.scala
+++ trunk/simulations-java/simulations/the-ramp/scala-src/edu/colorado/phet/therampscala/graphics/CoordinateFrame.scala
@@ -1,10 +1,19 @@
 package edu.colorado.phet.therampscala.graphics
 
 import common.phetcommon.view.graphics.transforms.ModelViewTransform2D
+import common.piccolophet.nodes.PhetPPath
+import java.awt.geom.Line2D
+import java.awt.{BasicStroke, Color}
 import model.RampModel
 import umd.cs.piccolo.nodes.PText
 import umd.cs.piccolo.PNode
 
 class CoordinateFrame(val model: RampModel, val transform: ModelViewTransform2D) extends PNode {
   addChild(new PText("Coordinate Frame"))
+  val path = new PhetPPath(new BasicStroke(2), Color.black)
+
+  def update() = {
+    path.setPathTo(new Line2D.Double(transform.modelToView(0, 0), transform.modelToView(0, 10)))
+  }
+  update()
 }
