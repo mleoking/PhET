6bdde574e6befd494e1abdb4dccf1f4885449537
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-04-23 22:09:49 +0000

    Added gradient paint to representation of carbon atom to make it appear 3D.

--- trunk/simulations-java/simulations/nuclear-physics/src/edu/colorado/phet/nuclearphysics/common/view/AtomicNucleusImageNode.java
+++ trunk/simulations-java/simulations/nuclear-physics/src/edu/colorado/phet/nuclearphysics/common/view/AtomicNucleusImageNode.java
@@ -3,9 +3,12 @@
 package edu.colorado.phet.nuclearphysics.common.view;
 
 import java.awt.Color;
+import java.awt.Paint;
 import java.awt.geom.Ellipse2D;
+import java.awt.geom.Point2D;
 import java.util.ArrayList;
 
+import edu.colorado.phet.common.phetcommon.view.graphics.RoundGradientPaint;
 import edu.colorado.phet.common.piccolophet.nodes.SphericalNode;
 import edu.colorado.phet.nuclearphysics.common.model.AtomicNucleus;
 import edu.colorado.phet.nuclearphysics.view.NucleusImageFactory;
@@ -32,7 +35,14 @@ public class AtomicNucleusImageNode extends AtomicNucleusNode {
         addChild(0, _displayImage);
     }
 
+    /**
+     * Create the image that will be used to visually represent this nucleus.
+     * 
+     * @param atomicNucleus
+     * @param imageType
+     */
 	private void createImage( AtomicNucleus atomicNucleus, AtomicNucleusImageType imageType ) {
+		
 		// Create a graphical image that will represent this nucleus in the view.
 		if ( imageType == AtomicNucleusImageType.NUCLEONS_VISIBLE ){
 	        _displayImage = NucleusImageFactory.getInstance().getNucleusImage( atomicNucleus.getNumNeutrons(), 
@@ -42,7 +52,11 @@ public class AtomicNucleusImageNode extends AtomicNucleusNode {
 	        _displayImage.scale( (atomicNucleus.getDiameter()/1.2)/((_displayImage.getWidth() + _displayImage.getHeight()) / 2));
 		}
 		else if ( imageType == AtomicNucleusImageType.GRADIENT_SPHERE ){
-	        _displayImage = new SphericalNode( atomicNucleus.getDiameter(), Color.RED, false );
+			double radius = atomicNucleus.getDiameter() / 2;
+    		Paint spherePaint = new RoundGradientPaint( radius, -radius, Color.WHITE,
+                    new Point2D.Double( -radius, radius ), Color.RED );
+
+	        _displayImage = new SphericalNode( atomicNucleus.getDiameter(), spherePaint, false );
 		}
 		else if ( imageType == AtomicNucleusImageType.CIRCLE_WITH_HIGHLIGHT ){
 	        _displayImage = new PPath( new Ellipse2D.Double( 0, 0, atomicNucleus.getDiameter(),
