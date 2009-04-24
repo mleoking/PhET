308f4350fb18ff96aa393f1f933f54ad66115c46
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-04-24 23:03:53 +0000

    add optional vector offsets

--- simulations-java/simulations/the-ramp/scala-src/edu/colorado/phet/therampscala/graphics/RampCanvas.scala
+++ simulations-java/simulations/the-ramp/scala-src/edu/colorado/phet/therampscala/graphics/RampCanvas.scala
@@ -4,13 +4,14 @@ package edu.colorado.phet.therampscala.graphics
 import common.phetcommon.view.graphics.transforms.ModelViewTransform2D
 import java.awt.Color
 import java.awt.geom.Rectangle2D
-import model.{Bead, RampModel}
+import model.{BeadVector, Bead, RampModel}
 import scalacommon.math.Vector2D
 import scalacommon.Predef._
 import umd.cs.piccolo.PNode
 import java.lang.Math._
 
-class RampCanvas(model: RampModel, coordinateSystemModel: CoordinateSystemModel, freeBodyDiagramModel: FreeBodyDiagramModel) extends DefaultCanvas(22, 20) {
+class RampCanvas(model: RampModel, coordinateSystemModel: CoordinateSystemModel, freeBodyDiagramModel: FreeBodyDiagramModel,
+                 vectorViewModel: VectorViewModel) extends DefaultCanvas(22, 20) {
   setBackground(new Color(200, 255, 240))
 
   addNode(new SkyNode(transform))
@@ -60,12 +61,21 @@ class RampCanvas(model: RampModel, coordinateSystemModel: CoordinateSystemModel,
   val vectorNode = new VectorSetNode(transform, model.beads(0))
   addNode(vectorNode)
 
-  def addVector(a: Vector, offsetFBD: VectorValue, offsetPlayArea: Double) = {
+  def addVector(a: Vector with PointOfOriginVector, offsetFBD: VectorValue, offsetPlayArea: Double) = {
     fbdNode.addVector(a, offsetFBD)
-    val tailLocationInPlayArea = new VectorValue() {
-      def addListenerByName(listener: => Unit) = model.beads(0).addListenerByName(listener)
 
-      def getValue = model.beads(0).position2D + new Vector2D(model.beads(0).getAngle + PI / 2) * (offsetPlayArea + model.beads(0).height / 3)
+
+    val tailLocationInPlayArea = new VectorValue() {
+      def addListenerByName(listener: => Unit) = {
+        model.beads(0).addListenerByName(listener)
+        vectorViewModel.addListenerByName(listener)
+      }
+
+      def getValue = {
+        val defaultCenter = model.beads(0).height / 2.0
+        model.beads(0).position2D + new Vector2D(model.beads(0).getAngle + PI / 2) *
+                (offsetPlayArea + (if (vectorViewModel.centered) defaultCenter else a.getPointOfOriginOffset(defaultCenter)))
+      }
     }
     val playAreaAdapter = new Vector(a.color, a.name, a.abbreviation) {
       def getValue = a.getValue * RampDefaults.PLAY_AREA_VECTOR_SCALE
@@ -73,11 +83,16 @@ class RampCanvas(model: RampModel, coordinateSystemModel: CoordinateSystemModel,
     vectorNode.addVector(playAreaAdapter, tailLocationInPlayArea)
   }
 
-  def addVector(a: Vector): Unit = addVector(a, new ConstantVectorValue, 0)
+  def addVector(a: BeadVector): Unit = addVector(a, new ConstantVectorValue, 0)
   addVector(model.beads(0).appliedForceVector)
   addVector(model.beads(0).gravityForceVector)
   addVector(model.beads(0).normalForceVector)
   addVector(model.beads(0).frictionForceVector)
   addVector(model.beads(0).wallForceVector)
   addVector(model.beads(0).totalForceVector, new ConstantVectorValue(new Vector2D(0, fbdWidth / 4)), 2)
+
+
 }
+trait PointOfOriginVector {
+    def getPointOfOriginOffset(defaultCenter:Double):Double
+  }
