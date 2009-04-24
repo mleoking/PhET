42a789fed5c0754a9d4dcbafcb2454d70a024151
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-04-24 15:25:09 +0000

    factor out defaults

---
+++ trunk/simulations-java/simulations/the-ramp/scala-src/edu/colorado/phet/therampscala/RampDefaultValues.scala
@@ -0,0 +1,25 @@
+package edu.colorado.phet.therampscala
+
+import java.awt.Color
+
+object RampDefaults {
+  val MIN_X = -10
+  val MAX_X = 10
+  val objects = new ScalaRampObject("File Cabinet", 5.0, 0.5, 0.5, "cabinet.gif") ::
+          new ScalaRampObject("Sleepy Dog", 25.0, 0.5, 0.5, "ollie.gif") ::
+          new ScalaRampObject("Small Crate", 150, 0.5, 0.5, "crate.gif") ::
+          new MutableRampObject("Custom Crate", 150, 0.5, 0.5, "crate.gif", true) ::
+          new ScalaRampObject("Refrigerator", 400, 0.5, 0.5, "fridge.gif") ::
+          new ScalaRampObject("Textboox", 10, 0.5, 0.5, "phetbook.gif") ::
+          new ScalaRampObject("Big Crate", 300, 0.5, 0.5, "crate.gif") ::
+          new CustomTextRampObject("Mystery Object", 300, 0.5, 0.5, "mystery-box.png", false) ::
+          Nil
+  val iconsPerRow = 4
+
+  val appliedForceColor = Color.orange
+  val gravityForceColor = Color.blue
+  val normalForceColor = Color.yellow
+  val frictionForceColor = Color.red
+  val totalForceColor = Color.pink
+
+}
