dc1f5a52d6d6823d2d59e9f053cac87f1fb09f36
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-04-25 23:42:49 +0000

    improve scales and default values

--- trunk/simulations-java/simulations/the-ramp/scala-src/edu/colorado/phet/therampscala/ScalaRampObject.scala
+++ trunk/simulations-java/simulations/the-ramp/scala-src/edu/colorado/phet/therampscala/ScalaRampObject.scala
@@ -4,7 +4,7 @@ package edu.colorado.phet.therampscala
 import java.awt.Color
 import scalacommon.util.Observable
 
-class ScalaRampObject(_name: String, _mass: Double, val kineticFriction: Double, val staticFriction: Double, _imageFilename: String, _customizable: Boolean) {
+class ScalaRampObject(_name: String, _mass: Double, val kineticFriction: Double, val staticFriction: Double, _height: Double, _imageFilename: String, _customizable: Boolean) {
   val customizable = _customizable
   val name = _name
 
@@ -14,12 +14,13 @@ class ScalaRampObject(_name: String, _mass: Double, val kineticFriction: Double,
 
   def getDisplayText = name + " (" + mass + " kg)"
 
-  def this(name: String, mass: Double, kineticFriction: Double, staticFriction: Double, imageFilename: String) = this (name, mass, kineticFriction, staticFriction, imageFilename, false)
+  def this(name: String, mass: Double, kineticFriction: Double, staticFriction: Double, height: Double, imageFilename: String) = this (name, mass, kineticFriction, staticFriction, height,imageFilename, false)
 
-  def height = mass / 2/4
+  //  def height = mass / 2 / 4
+  def height = _height
 }
 
-class CustomTextRampObject(name: String, mass: Double, kineticFriction: Double, staticFriction: Double, imageFilename: String, customizable: Boolean) extends ScalaRampObject(name, mass, kineticFriction, staticFriction, imageFilename, customizable) {
+class CustomTextRampObject(name: String, mass: Double, kineticFriction: Double, staticFriction: Double, imageFilename: String, customizable: Boolean) extends ScalaRampObject(name, mass, kineticFriction, staticFriction, 1,imageFilename, customizable) {
   override def getDisplayText = name
 }
 
@@ -34,5 +35,5 @@ class MutableRampObject(name: String, _mass: Double, kineticFriction: Double, st
     notifyListeners()
   }
 
-  //  def height = m_mass / 100.0
+ override def height = m_mass / 2  / 10
 }
