9885930aff8892e4a95d075c38f8c25e6d8be3c9
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-04-25 09:58:03 +0000

    refactor, start adding snap to coordinate frames

--- trunk/simulations-java/simulations/the-ramp/scala-src/edu/colorado/phet/therampscala/model/RampModel.scala
+++ trunk/simulations-java/simulations/the-ramp/scala-src/edu/colorado/phet/therampscala/model/RampModel.scala
@@ -9,32 +9,57 @@ import scalacommon.util.Observable
 import scalacommon.record.RecordModel
 import java.lang.Math._
 
-class CoordinateFrameModel extends Observable {
+class CoordinateFrameModel(snapToAngles: List[() => Double]) extends Observable {
   private var _angle = 0.0
 
   def angle = _angle
 
   def angle_=(ang: Double) = {
-    _angle = ang
+
+    var snapChoice = _angle
+//    for (a <- snapToAngles) {
+//      val snapToAngle = a()
+//      if (abs(snapToAngle - ang) < 5.0.toRadians) {
+//        snapChoice = snapToAngle
+//      }
+//    }
+
+    _angle = snapChoice
     notifyListeners()
   }
 }
 
 class RampModel extends RecordModel[String] with ObjectModel {
   setPaused(false)
-  val coordinateFrameModel = new CoordinateFrameModel
-
-  def setPlaybackState(state: String) {}
-
-  def handleRecordStartedDuringPlayback() {}
-
-  def getMaxRecordPoints = 100
 
   val rampSegments = new ArrayBuffer[RampSegment]
   val beads = new ArrayBuffer[Bead]
   private var _walls = true
   private var _frictionless = false
   private var _selectedObject = RampDefaults.objects(0)
+
+  val rampLength = 10
+  rampSegments += new RampSegment(new Point2D.Double(-rampLength, 0), new Point2D.Double(0, 0))
+  val initialAngle = 30.0.toRadians
+  rampSegments += new RampSegment(new Point2D.Double(0, 0), new Point2D.Double(rampLength * cos(initialAngle), rampLength * sin(initialAngle)))
+
+  val coordinateFrameModel = new CoordinateFrameModel( (()=>0.0) :: Nil)
+
+  //Sends notification when any ramp segment changes
+  object rampChangeAdapter extends Observable //todo: perhaps we should just pass the addListener method to the beads
+  rampSegments(0).addListenerByName {rampChangeAdapter.notifyListeners}
+  rampSegments(1).addListenerByName {rampChangeAdapter.notifyListeners}
+  beads += new Bead(new BeadState(5, 0, _selectedObject.mass, _selectedObject.staticFriction, _selectedObject.kineticFriction), 3, positionMapper, rampSegmentAccessor, rampChangeAdapter)
+  val tree = new Bead(new BeadState(-9, 0, 10, 0, 0), 3, positionMapper, rampSegmentAccessor, rampChangeAdapter)
+  val leftWall = new Bead(new BeadState(-10, 0, 10, 0, 0), 3, positionMapper, rampSegmentAccessor, rampChangeAdapter)
+  val rightWall = new Bead(new BeadState(10, 0, 10, 0, 0), 3, positionMapper, rampSegmentAccessor, rampChangeAdapter)
+  val manBead = new Bead(new BeadState(2, 0, 10, 0, 0), 3, positionMapper, rampSegmentAccessor, rampChangeAdapter)
+
+  def setPlaybackState(state: String) {}
+
+  def handleRecordStartedDuringPlayback() {}
+
+  def getMaxRecordPoints = 100
 
   def selectedObject = _selectedObject
 
@@ -68,11 +93,6 @@ class RampModel extends RecordModel[String] with ObjectModel {
     notifyListeners()
   }
 
-  val rampLength = 10
-  rampSegments += new RampSegment(new Point2D.Double(-rampLength, 0), new Point2D.Double(0, 0))
-  val initialAngle = 30.0.toRadians
-  rampSegments += new RampSegment(new Point2D.Double(0, 0), new Point2D.Double(rampLength * cos(initialAngle), rampLength * sin(initialAngle)))
-
   def setRampAngle(angle: Double) = {
     rampSegments(1).setAngle(angle)
   }
@@ -85,22 +105,13 @@ class RampModel extends RecordModel[String] with ObjectModel {
 
   def rampSegmentAccessor(particleLocation: Double) = if (particleLocation <= 0) rampSegments(0) else rampSegments(1)
 
-  //Sends notification when any ramp segment changes
-  object rampChangeAdapter extends Observable //todo: perhaps we should just pass the addListener method to the beads
-  rampSegments(0).addListenerByName {rampChangeAdapter.notifyListeners}
-  rampSegments(1).addListenerByName {rampChangeAdapter.notifyListeners}
-  beads += new Bead(new BeadState(5, 0, _selectedObject.mass, _selectedObject.staticFriction, _selectedObject.kineticFriction), 3, positionMapper, rampSegmentAccessor, rampChangeAdapter)
-  val tree = new Bead(new BeadState(-9, 0, 10, 0, 0), 3, positionMapper, rampSegmentAccessor, rampChangeAdapter)
-  val leftWall = new Bead(new BeadState(-10, 0, 10, 0, 0), 3, positionMapper, rampSegmentAccessor, rampChangeAdapter)
-  val rightWall = new Bead(new BeadState(10, 0, 10, 0, 0), 3, positionMapper, rampSegmentAccessor, rampChangeAdapter)
-  val manBead = new Bead(new BeadState(2, 0, 10, 0, 0), 3, positionMapper, rampSegmentAccessor, rampChangeAdapter)
-
   def update(dt: Double) = {
     if (!isPaused) {
       beads.foreach(_.newStepCode(dt))
     }
   }
-  def stepRecord(dt:Double)={
+
+  def stepRecord(dt: Double) = {
     beads.foreach(_.newStepCode(dt))
   }
 }
