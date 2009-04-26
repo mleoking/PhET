d55c7d2f0b273f2ba8a985e180782bc353e146b2
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-04-26 05:09:54 +0000

    fixes for icons and tooltips

--- simulations-java/simulations/the-ramp/scala-src/edu/colorado/phet/therampscala/ScalaRampObject.scala
+++ simulations-java/simulations/the-ramp/scala-src/edu/colorado/phet/therampscala/ScalaRampObject.scala
@@ -14,14 +14,18 @@ class ScalaRampObject(_name: String, _mass: Double, val kineticFriction: Double,
 
   def getDisplayText = name + " (" + mass + " kg)"
 
-  def this(name: String, mass: Double, kineticFriction: Double, staticFriction: Double, height: Double, imageFilename: String) = this (name, mass, kineticFriction, staticFriction, height,imageFilename, false)
+  def this(name: String, mass: Double, kineticFriction: Double, staticFriction: Double, height: Double, imageFilename: String) = this (name, mass, kineticFriction, staticFriction, height, imageFilename, false)
 
   //  def height = mass / 2 / 4
   def height = _height
+
+  def displayTooltip = true
 }
 
-class CustomTextRampObject(name: String, mass: Double, kineticFriction: Double, staticFriction: Double, imageFilename: String, customizable: Boolean) extends ScalaRampObject(name, mass, kineticFriction, staticFriction, 1,imageFilename, customizable) {
+class CustomTextRampObject(name: String, mass: Double, kineticFriction: Double, staticFriction: Double, imageFilename: String, customizable: Boolean) extends ScalaRampObject(name, mass, kineticFriction, staticFriction, 1, imageFilename, customizable) {
   override def getDisplayText = name
+
+  override def displayTooltip = false
 }
 
 class MutableRampObject(name: String, _mass: Double, kineticFriction: Double, staticFriction: Double, imageFilename: String, customizable: Boolean)
@@ -35,5 +39,5 @@ class MutableRampObject(name: String, _mass: Double, kineticFriction: Double, st
     notifyListeners()
   }
 
- override def height = m_mass / 2  / 10
+  override def height = m_mass / 2 / 10
 }
