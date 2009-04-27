38bd11f256d66b5c669dcca5ae4e29c0d976183e
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-04-27 04:26:19 +0000

    increased stack space to avoid stack overflow exception during scala compile, see #1635

--- trunk/build-tools/build.bat
+++ trunk/build-tools/build.bat
@@ -4,7 +4,7 @@ set OLD_DIR=%CD%
 cd /d %~dp0
 set ROOT_DIR=%CD%
 set ANT_HOME=%ROOT_DIR%\contrib\apache-ant
-set ANT_OPTS=-Xmx640m
+set ANT_OPTS=-Xmx640m -Xss1040K
 set PATH=%ANT_HOME%\bin;%PATH%
 
 if "%JAVA_HOME%"=="" (echo The environment variable JAVA_HOME must be set to the location of a valid JDK.) else (
