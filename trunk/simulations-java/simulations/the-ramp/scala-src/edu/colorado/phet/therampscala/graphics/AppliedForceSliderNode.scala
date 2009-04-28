88800b07889b05acc1286db979364e707f2e3dca
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-04-28 04:48:35 +0000

    improve module layout

--- trunk/simulations-java/simulations/the-ramp/scala-src/edu/colorado/phet/therampscala/graphics/AppliedForceSliderNode.scala
+++ trunk/simulations-java/simulations/the-ramp/scala-src/edu/colorado/phet/therampscala/graphics/AppliedForceSliderNode.scala
@@ -2,6 +2,7 @@ package edu.colorado.phet.therampscala.graphics
 
 
 import common.phetcommon.view.graphics.transforms.ModelViewTransform2D
+import java.awt.Dimension
 import java.awt.geom.Point2D
 import model.{Bead}
 import umd.cs.piccolo.PNode
@@ -14,14 +15,15 @@ class AppliedForceSliderNode(bead: Bead, transform: ModelViewTransform2D) extend
   val max = 500
   val control = new ScalaValueControl(-max, max, "Applied Force X", "0.0", "N",
     bead.parallelAppliedForce, value => bead.parallelAppliedForce = value, bead.addListener)
-
+  control.setSize(new Dimension(control.getPreferredSize.width, (control.getPreferredSize.height * 1.45).toInt))
   val pswing = new PSwing(control)
   addChild(pswing)
   def updatePosition() = {
     val viewLoc = transform.modelToView(new Point2D.Double(0, -1))
-    val scale = 1.2f
-    pswing.setOffset(viewLoc - new Vector2D(pswing.getFullBounds.getWidth * scale, 0))
+    val scale = 1.2f * 1.2f
+    val x = transform.getViewBounds
     pswing.setScale(scale)
+    pswing.setOffset(x.getCenterX - pswing.getFullBounds.getWidth / 2, viewLoc.y)
   }
   updatePosition()
 }
