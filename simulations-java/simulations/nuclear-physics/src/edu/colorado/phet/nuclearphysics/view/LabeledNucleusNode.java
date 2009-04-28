71c427e57037d66c7399548f71edfaacabda6d03
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-04-28 15:53:28 +0000

    Added support for creating a spherical node for the nucleus.

--- simulations-java/simulations/nuclear-physics/src/edu/colorado/phet/nuclearphysics/view/LabeledNucleusNode.java
+++ simulations-java/simulations/nuclear-physics/src/edu/colorado/phet/nuclearphysics/view/LabeledNucleusNode.java
@@ -3,12 +3,17 @@
 package edu.colorado.phet.nuclearphysics.view;
 
 import java.awt.Color;
+import java.awt.GradientPaint;
+import java.awt.Paint;
+import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
 
 import javax.swing.JFrame;
 
+import edu.colorado.phet.common.phetcommon.view.graphics.RoundGradientPaint;
 import edu.colorado.phet.common.piccolophet.PhetPCanvas;
 import edu.colorado.phet.common.piccolophet.nodes.ShadowHTMLNode;
+import edu.colorado.phet.common.piccolophet.nodes.SphericalNode;
 import edu.colorado.phet.nuclearphysics.NuclearPhysicsConstants;
 import edu.colorado.phet.nuclearphysics.NuclearPhysicsResources;
 import edu.colorado.phet.nuclearphysics.NuclearPhysicsStrings;
@@ -19,7 +24,8 @@ import edu.umd.cs.piccolox.nodes.PComposite;
 
 /**
  * This class creates a PNode that is a composite of an image (generally
- * a nucleus) and a shaded label.
+ * a nucleus) and a shaded label.  To date, this type of node has been used
+ * primarily on control panels.
  * 
  * @author John Blanco
  */
@@ -32,6 +38,7 @@ public class LabeledNucleusNode extends PComposite {
     private static final double IMAGE_SCALING_FACTOR = 0.20;
     private static final double SMALLER_FONT_SCALING_FACTOR = IMAGE_SCALING_FACTOR * 9;
     private static final double LARGER_FONT_SCALING_FACTOR = IMAGE_SCALING_FACTOR * 12;
+    private static final double SPHERE_DIAMETER = 50;
 
     //------------------------------------------------------------------------
     // Instance Data
@@ -42,14 +49,14 @@ public class LabeledNucleusNode extends PComposite {
     //------------------------------------------------------------------------
 
     /**
-     * Constructor.
+     * Constructor that takes the name of an image resource and loads it.
      * 
      * @param imageName - Name of the image resource that will provide the background.
      * @param isotopeNumber - Numerical isotope number, which will be displayed as a pre-script.
      * @param chemicalSymbol - Chemical symbol for the nucleus.
      * @param labelColor - Color that will be used to display the label.
      */
-    public LabeledNucleusNode(String imageName, String isotopeNumber, String chemicalSymbol, Color labelColor){
+    public LabeledNucleusNode( String imageName, String isotopeNumber, String chemicalSymbol, Color labelColor ){
         
         // Get the image for the nucleus.
         BufferedImage im = NuclearPhysicsResources.getImage( imageName );
@@ -85,6 +92,45 @@ public class LabeledNucleusNode extends PComposite {
         setChildrenPickable(false);
     }
     
+    /**
+     * Constructor that takes a color instead of an image name and creates a
+     * sphere to represent the nucleus.
+     * 
+     * @param sphereColor - Desired color of the sphere the will represent the nucleus.
+     * @param isotopeNumber - Numerical isotope number, which will be displayed as a pre-script.
+     * @param chemicalSymbol - Chemical symbol for the nucleus.
+     * @param labelColor - Color that will be used to display the label.
+     */
+    public LabeledNucleusNode( Color sphereColor, String isotopeNumber, String chemicalSymbol, Color labelColor ){
+
+    	// Create the gradient paint for the sphere in order to give it a 3D look.
+		Paint spherePaint = new RoundGradientPaint( SPHERE_DIAMETER / 4, -SPHERE_DIAMETER / 4, Color.WHITE,
+                new Point2D.Double( SPHERE_DIAMETER / 2, SPHERE_DIAMETER / 2 ), sphereColor );
+
+    	// Create and add the sphere node.
+    	SphericalNode sphere = new SphericalNode(SPHERE_DIAMETER, spherePaint, false);
+    	sphere.setOffset(SPHERE_DIAMETER / 2, SPHERE_DIAMETER / 2);
+        addChild( sphere );
+        
+        // Create and add the shadowed label.
+        String labelText = "<html><sup><font size=-2>" + isotopeNumber +
+            " </font></sup>" + chemicalSymbol + "</html>";
+        ShadowHTMLNode label = new ShadowHTMLNode( labelText );
+        label.setColor( labelColor );
+        label.setShadowColor( labelColor == Color.BLACK ? Color.WHITE : Color.BLACK );
+        double scale = (SPHERE_DIAMETER / label.getFullBoundsReference().getWidth()) * 0.9;
+        label.setScale( scale );
+        label.setShadowOffset( 0.5, 0.5 );
+        // Center the label over the nucleus.
+        label.setOffset( ( sphere.getFullBoundsReference().getWidth() - label.getFullBoundsReference().getWidth() ) / 2, 
+        		( sphere.getFullBoundsReference().getHeight() - label.getFullBoundsReference().getHeight() ) / 2);
+        addChild(label);
+        
+        // Make sure we aren't pickable since we don't handle any mouse events.
+        setPickable(false);
+        setChildrenPickable(false);
+    }
+    
     //------------------------------------------------------------------------
     // Public Methods
     //------------------------------------------------------------------------
@@ -95,14 +141,19 @@ public class LabeledNucleusNode extends PComposite {
      * @param args - Unused.
      */
     public static void main(String [] args){
-        LabeledNucleusNode testNode = new LabeledNucleusNode("Uranium Nucleus Small.png",
+        LabeledNucleusNode imageTestNode = new LabeledNucleusNode("Uranium Nucleus Small.png",
+                NuclearPhysicsStrings.URANIUM_235_ISOTOPE_NUMBER, NuclearPhysicsStrings.URANIUM_235_CHEMICAL_SYMBOL, 
+                NuclearPhysicsConstants.URANIUM_235_LABEL_COLOR );
+        LabeledNucleusNode sphereTestNode = new LabeledNucleusNode(Color.DARK_GRAY,
                 NuclearPhysicsStrings.URANIUM_235_ISOTOPE_NUMBER, NuclearPhysicsStrings.URANIUM_235_CHEMICAL_SYMBOL, 
                 NuclearPhysicsConstants.URANIUM_235_LABEL_COLOR );
         
         JFrame frame = new JFrame();
         PhetPCanvas canvas = new PhetPCanvas();
         frame.setContentPane( canvas );
-        canvas.addScreenChild( testNode );
+        canvas.addScreenChild( imageTestNode );
+        imageTestNode.setOffset(200, 200);
+        canvas.addScreenChild(sphereTestNode);
         frame.setSize( 800, 600 );
         frame.setVisible( true );
     }
