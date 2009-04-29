d8efaa237594f40f3ddd44e22ce0e5eb528d0069
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-04-29 05:28:31 +0000

    improvements for surface chooser

--- simulations-java/simulations/the-ramp/scala-src/edu/colorado/phet/therampscala/graphics/SurfaceChooser.scala
+++ simulations-java/simulations/the-ramp/scala-src/edu/colorado/phet/therampscala/graphics/SurfaceChooser.scala
@@ -1,8 +1,15 @@
 package edu.colorado.phet.therampscala.graphics
 
 
+import common.phetcommon.view.controls.valuecontrol.LinearValueControl
+import eatingandexercise.view.SliderNode
+import java.awt.Dimension
+import java.util.{Hashtable, Dictionary}
+import javax.swing.{SwingConstants, ImageIcon, JLabel, JSlider}
+import swing.ScalaValueControl
 import umd.cs.piccolo.nodes.PText
 import umd.cs.piccolo.PNode
+import umd.cs.piccolox.pswing.PSwing
 
 /**
  * Created by IntelliJ IDEA.
@@ -14,4 +21,12 @@ import umd.cs.piccolo.PNode
 
 class SurfaceChooser(surfaceModel: SurfaceModel) extends PNode {
   addChild(new PText("Choose Surface"))
+  val control = new ScalaValueControl(0, 6, "friction", "0.00", "", surfaceModel.friction, surfaceModel.friction = _, surfaceModel.addListener)
+  control.addTickLabel(0.0, toLabel("Ice", "ice.gif"))
+  control.addTickLabel(3.0, toLabel("Concrete", "concrete.gif"))
+  control.addTickLabel(6.0, toLabel("Carpet", "carpet.gif"))
+  control.getSlider.setPreferredSize(new Dimension(control.getSlider.getPreferredSize.width * 2, control.getSlider.getPreferredSize.height))
+  addChild(new PSwing(control))
+
+  def toLabel(s: String, filename: String) = new JLabel(s, new ImageIcon(RampResources.getImage("robotmovingcompany/" + filename)), SwingConstants.LEADING)
 }
