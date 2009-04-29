0ea9f611105033f008dade86f04f2eccd02ee264
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-04-29 20:31:42 +0000

    standardize text style for A, B, M symbols

--- trunk/simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/ABSSymbols.java
+++ trunk/simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/ABSSymbols.java
@@ -7,26 +7,26 @@ package edu.colorado.phet.acidbasesolutions;
  * These do not require localization, their use is universal.
  * <p>
  * HTML is used to handle superscripts and subscripts.
- * May symbols are HTML fragments, so they can be combined into more 
- * complex HTML documents.
+ * Many symbols are HTML fragments, so they can be combined into more complex HTML documents.
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
 public class ABSSymbols {
     
+    // generic acid/base symbols, HTM fragments
+    public static final String A = "<i>A</i>";
+    public static final String B = "<i>B</i>";
+    public static final String M = "<i>M</i>";
+    
     // general symbols, HTML fragments
     public static final String H2O = "H<sub>2</sub>O";
     public static final String H3O_PLUS = "H<sub>3</sub>O<sup>+</sup>";
     public static final String OH_MINUS = "OH<sup>-</sup>";
-    public static final String HA = "HA";
-    public static final String HA_EMPHASIS = "H<i>A</i>";
-    public static final String A_MINUS = "A<sup>-</sup>";
-    public static final String B = "B";
-    public static final String B_EMPHASIS = "<i>B</i>";
-    public static final String BH_PLUS = "BH<sup>+</sup>";
-    public static final String MOH = "MOH";
-    public static final String MOH_EMPHASIS = "<i>M</i>OH";
-    public static final String M_PLUS = "M<sup>+</sup>";
+    public static final String HA = "H" + A;
+    public static final String A_MINUS = A + "<sup>-</sup>";
+    public static final String BH_PLUS = B + "H<sup>+</sup>";
+    public static final String MOH = M + "OH";
+    public static final String M_PLUS = M + "<sup>+</sup>";
     public static final String Ka = "K<sub>a</sub>";
     public static final String Kb = "K<sub>b</sub>";
     public static final String Kw = "K<sub>w</sub>";
