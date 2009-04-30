98ccf774629acec04fcad1e9e5519064d6323579
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-04-30 17:03:27 +0000

    Unfuddletool: link fix

--- util/unfuddletool/src/edu/colorado/phet/unfuddletool/LinkHandler.java
+++ util/unfuddletool/src/edu/colorado/phet/unfuddletool/LinkHandler.java
@@ -1,7 +1,9 @@
 package edu.colorado.phet.unfuddletool;
 
+import java.awt.*;
 import java.io.IOException;
 
+import javax.swing.*;
 import javax.swing.event.HyperlinkEvent;
 import javax.swing.event.HyperlinkListener;
 
@@ -14,11 +16,56 @@ public class LinkHandler implements HyperlinkListener {
 
         System.out.println( "Opening " + hyperlinkEvent.getDescription() );
 
+        showInBrowser( hyperlinkEvent.getDescription(), null );
+    }
+
+    // from http://forums.sun.com/thread.jspa?threadID=679673
+    static boolean showInBrowser( String url, Frame frame ) {
+        //minimizes the app
+        if ( frame != null ) {
+            frame.setExtendedState( JFrame.ICONIFIED );
+        }
+
+        String os = System.getProperty( "os.name" ).toLowerCase();
+        Runtime rt = Runtime.getRuntime();
         try {
-            Runtime.getRuntime().exec( new String[]{Configuration.browser(), hyperlinkEvent.getDescription()} );
+            if ( os.indexOf( "win" ) >= 0 ) {
+                String[] cmd = new String[4];
+                cmd[0] = "cmd.exe";
+                cmd[1] = "/C";
+                cmd[2] = "start";
+                cmd[3] = url;
+                rt.exec( cmd );
+            }
+            else if ( os.indexOf( "mac" ) >= 0 ) {
+                rt.exec( "open " + url );
+            }
+            else {
+                //prioritized 'guess' of users' preference
+                String[] browsers = {"firefox", "epiphany", "mozilla", "konqueror",
+                        "netscape", "opera", "links", "lynx"};
+
+                StringBuffer cmd = new StringBuffer();
+                for ( int i = 0; i < browsers.length; i++ ) {
+                    cmd.append( ( i == 0 ? "" : " || " ) + browsers[i] + " \"" + url + "\" " );
+                }
+
+                rt.exec( new String[]{"sh", "-c", cmd.toString()} );
+                //rt.exec("firefox http://www.google.com");
+                //System.out.println(cmd.toString());
+
+            }
         }
         catch( IOException e ) {
             e.printStackTrace();
+            JOptionPane.showMessageDialog( frame,
+                                           "\n\n The system failed to invoke your default web browser while attempting to access: \n\n " + url + "\n\n",
+                                           "Browser Error",
+                                           JOptionPane.WARNING_MESSAGE );
+
+            return false;
         }
+        return true;
     }
+
 }
