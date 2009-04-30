036de6283f15da06cd6fe2f2ebccc52ee8a09413
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-04-30 01:02:41 +0000

    added workaround for eclipse main launching

---
+++ trunk/simulations-java/simulations/the-ramp/scala-src/edu/colorado/phet/therampscala/RampMain2.scala
@@ -0,0 +1,10 @@
+package edu.colorado.phet.therampscala
+
+import common.phetcommon.application.{PhetApplicationConfig, PhetApplicationLauncher, Module}
+
+//Current Eclipse plugin has trouble finding main for objects defined in a file with other classes, so we use a separate file 
+object RampMain2 {
+  def main(args: Array[String]) = {
+    new PhetApplicationLauncher().launchSim(args, "the-ramp", classOf[RampApplication])
+  }
+}
