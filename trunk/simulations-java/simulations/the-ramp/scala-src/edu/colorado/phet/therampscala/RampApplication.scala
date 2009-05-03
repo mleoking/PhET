b780f6d621abbc0c127086cdf418eb57988b8dc1
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-05-03 19:36:14 +0000

    reformat, optimize imports

--- trunk/simulations-java/simulations/the-ramp/scala-src/edu/colorado/phet/therampscala/RampApplication.scala
+++ trunk/simulations-java/simulations/the-ramp/scala-src/edu/colorado/phet/therampscala/RampApplication.scala
@@ -1,44 +1,16 @@
 package edu.colorado.phet.therampscala
 
-import _root_.scala.swing.CheckBox
-import collection.mutable.{HashMap, ArrayBuffer}
-import common.phetcommon.application.{PhetApplicationConfig, PhetApplicationLauncher, Module}
-import common.phetcommon.math.MathUtil
-import common.phetcommon.model.BaseModel
-import common.phetcommon.view.controls.valuecontrol.LinearValueControl
-import common.phetcommon.view.graphics.transforms.{TransformListener, ModelViewTransform2D}
-import common.phetcommon.view.util.{PhetFont, BufferedImageUtils}
-import common.phetcommon.view.{VerticalLayoutPanel, ResetAllButton}
-import common.piccolophet.event.CursorHandler
-import common.piccolophet.nodes.PhetPPath
-import common.piccolophet.{PiccoloPhetApplication, PhetPCanvas}
-import controls.RampControlPanel
-import edu.colorado.phet.scalacommon.Predef._
-import graphics._
-import java.awt._
-import java.awt.event.{ActionEvent, ActionListener}
-
-import java.awt.geom._
-import java.awt.image.BufferedImage
-import java.text.DecimalFormat
-import javax.sound.sampled.AudioSystem
-import javax.swing._
-import javax.swing.event.{ChangeListener, ChangeEvent}
+import common.phetcommon.application.{PhetApplicationLauncher, Module, PhetApplicationConfig}
 import model._
+import graphics.{RampCanvas, RMCCanvas}
+import controls.RampControlPanel
+import scalacommon.record.{RecordModelControlPanel, PlaybackSpeedSlider}
 
+import java.awt.Color
+import javax.swing.JFrame
 
-import scalacommon.math.Vector2D
-import scalacommon.record.{PlaybackSpeedSlider, RecordModel, RecordModelControlPanel}
-import scalacommon.swing.MyRadioButton
-import scalacommon.util.Observable
-import swing.ScalaValueControl
-import umd.cs.piccolo.event.{PBasicInputEventHandler, PInputEvent}
-import umd.cs.piccolo.nodes.{PPath, PImage, PText}
-import umd.cs.piccolo.PNode
-import scalacommon.{ScalaApplicationLauncher, ScalaClock}
-import java.lang.Math._
-import umd.cs.piccolo.util.PDimension
-import umd.cs.piccolox.pswing.PSwing
+import common.piccolophet.PiccoloPhetApplication
+import scalacommon.ScalaClock
 
 class AbstractRampModule(frame: JFrame, clock: ScalaClock, name: String) extends Module(name, clock) {
   val model = new RampModel
@@ -64,7 +36,7 @@ class RampModule(frame: JFrame, clock: ScalaClock) extends AbstractRampModule(fr
 }
 
 class RobotMovingCompanyModule(frame: JFrame, clock: ScalaClock) extends AbstractRampModule(frame, clock, "Robot Moving Company") {
-  val gameModel = new robotmovingcompany.RobotMovingCompanyGameModel(model, clock)//todo: fix this with imports
+  val gameModel = new robotmovingcompany.RobotMovingCompanyGameModel(model, clock) //todo: fix this with imports
   val canvas = new RMCCanvas(model, coordinateSystemModel, fbdModel, vectorViewModel, frame, gameModel)
 
   setSimulationPanel(canvas)
