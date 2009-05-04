dee3510ca118c6d30ee5bf246406f1abd959e2c9
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-05-04 03:18:21 +0000

    see #689, add test cases

--- trunk/simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/test/TestPSwingJSlider.java
+++ trunk/simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/test/TestPSwingJSlider.java
@@ -3,6 +3,7 @@
 package edu.colorado.phet.common.piccolophet.test;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -23,7 +24,8 @@ import edu.umd.cs.piccolox.pswing.PSwing;
  * If the JSlider is inside a JPanel, and the JPanel is wrapped by a PSwing,
  * then the JSlider's knob doesn't render properly.  The rendering is 
  * unacceptable when the JPanel has a titled border, and just a little off 
- * if the JPanel has no titled border.
+ * if the JPanel has no titled border.  The rendering is also OK if the 
+ * slider has no ticks.
  * <p>
  * This program creates 3 instances of the JPanel. Two of them are wrapped
  * with PSwing (one with a titled border, one without), while the third is 
@@ -37,22 +39,56 @@ public class TestPSwingJSlider {
 
     public static void main( String[] args ) {
         
-        JPanel swingControlPanel = new TestPanel( "title" );
-        
-        PSwing titledControlPanelWrapper = new PSwing( new TestPanel( "title" ) );
-        titledControlPanelWrapper.setOffset( 100, 10 );
-        
-        PSwing untitledControlPanelWrapper = new PSwing( new TestPanel() );
-        untitledControlPanelWrapper.setOffset( 200, 10 );
-        
-        JSlider slider = new TestSlider();
-        PSwing sliderWrapper = new PSwing( slider );
-        sliderWrapper.setOffset( 300, 10 );
+        double xOffset = 10;
+        double yOffset = 10;
         
         PhetPCanvas canvas = new PhetPCanvas();
-        canvas.getLayer().addChild( titledControlPanelWrapper );
-        canvas.getLayer().addChild( untitledControlPanelWrapper );
-        canvas.getLayer().addChild( sliderWrapper );
+        canvas.setPreferredSize( new Dimension( 600, 300 ) );
+        
+        // OK, demonstrated workaround
+        {
+            PSwing pswing = new PSwing( new SliderWithTicks() );
+            canvas.getLayer().addChild( pswing );
+            pswing.setOffset( xOffset, yOffset );
+            xOffset += 100;
+        }
+        
+        // Very broken, border
+        {
+            JPanel panel = new JPanel();
+            panel.setBorder( new TitledBorder( "broken" ) );
+            panel.add( new SliderWithTicks() );
+            PSwing pswing = new PSwing( panel );
+            canvas.getLayer().addChild( pswing );
+            pswing.setOffset( xOffset, yOffset );
+            xOffset += 100;
+        }
+        
+        // Less broken, no border
+        {
+            JPanel panel = new JPanel();
+            panel.add( new SliderWithTicks() );
+            PSwing pswing = new PSwing( panel );
+            canvas.getLayer().addChild( pswing );
+            pswing.setOffset( xOffset, yOffset );
+            xOffset += 100;
+        }
+        
+        // OK, no ticks
+        {
+            JPanel panel = new JPanel();
+            panel.setBorder( new TitledBorder( "OK" ) );
+            panel.add( new SliderNoTicks() );
+            PSwing pswing = new PSwing( panel );
+            canvas.getLayer().addChild( pswing );
+            pswing.setOffset( xOffset, yOffset );
+            xOffset += 100;
+        }
+        
+        // OK
+        JPanel swingControlPanel = new JPanel();
+        swingControlPanel.setBorder( new TitledBorder( "OK" ) );
+        swingControlPanel.add( new SliderWithTicks() );
         
         JPanel mainPanel = new JPanel( new BorderLayout() );
         mainPanel.add( swingControlPanel, BorderLayout.EAST );
@@ -60,39 +96,29 @@ public class TestPSwingJSlider {
         
         JFrame frame = new JFrame();
         frame.setContentPane( mainPanel );
-        frame.setSize( 500, 300 );
+        frame.pack();
         frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
         SwingUtils.centerWindowOnScreen( frame );
         frame.setVisible( true );
     }
-
-    private static class TestPanel extends JPanel {
-
-        public TestPanel() {
-            this( null );
-        }
-        
-        public TestPanel( String title) {
-            if ( title != null ) {
-                TitledBorder border = new TitledBorder( title );
-                setBorder( border );
-            }
-            add( new TestSlider() );
-        }
-    }
     
-    private static class TestSlider extends JSlider {
+    private static class SliderNoTicks extends JSlider {
         
-        public TestSlider() {
+        public SliderNoTicks() {
             setOrientation( JSlider.VERTICAL );
             setMinimum( 0 );
             setMaximum( 100 );
             setValue( 0 );
+        }
+    }
+    
+    private static class SliderWithTicks extends SliderNoTicks {
+        
+        public SliderWithTicks() {
             setMajorTickSpacing( 50 );
             setMinorTickSpacing( 10 );
             setPaintTicks( true );
             setPaintLabels( true );
         }
     }
-
 }
