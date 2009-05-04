98103e16064efb4cfc4d63a58d69f98a39f3f9c6
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-05-04 22:27:42 +0000

    create Equilibrium Expression dialog, add developer controls, wire up to check box in sim

--- trunk/simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/control/MiscControls.java
+++ trunk/simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/control/MiscControls.java
@@ -15,8 +15,8 @@ import javax.swing.JPanel;
 import javax.swing.border.LineBorder;
 import javax.swing.border.TitledBorder;
 
-import edu.colorado.phet.acidbasesolutions.dialog.EquilibriumExpressionsDialog;
 import edu.colorado.phet.acidbasesolutions.dialog.SymbolLegendDialog;
+import edu.colorado.phet.acidbasesolutions.view.equations.EquilibriumExpressionsDialog;
 import edu.colorado.phet.acidbasesolutions.view.equations.ReactionEquationsDialog;
 import edu.colorado.phet.common.phetcommon.application.PhetApplication;
 import edu.colorado.phet.common.phetcommon.view.util.EasyGridBagLayout;
