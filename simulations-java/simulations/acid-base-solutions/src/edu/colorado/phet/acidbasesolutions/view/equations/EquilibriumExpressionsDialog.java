df9174c9040172f47d8e743aa04f4791fbe4f604
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-05-04 22:27:42 +0000

    create Equilibrium Expression dialog, add developer controls, wire up to check box in sim

---
+++ simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/view/equations/EquilibriumExpressionsDialog.java
@@ -0,0 +1,377 @@
+package edu.colorado.phet.acidbasesolutions.view.equations;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.TitledBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import edu.colorado.phet.acidbasesolutions.view.equations.AbstractEquilibriumExpressionNode.*;
+import edu.colorado.phet.common.phetcommon.application.PaintImmediateDialog;
+import edu.colorado.phet.common.phetcommon.view.controls.valuecontrol.AbstractValueControl;
+import edu.colorado.phet.common.phetcommon.view.controls.valuecontrol.ILayoutStrategy;
+import edu.colorado.phet.common.phetcommon.view.controls.valuecontrol.LinearValueControl;
+import edu.colorado.phet.common.phetcommon.view.util.EasyGridBagLayout;
+import edu.colorado.phet.common.piccolophet.PhetPCanvas;
+import edu.umd.cs.piccolo.nodes.PText;
+
+
+public class EquilibriumExpressionsDialog extends PaintImmediateDialog {
+    
+    //TODO localize
+    private static final String TITLE = "Reaction Equations";
+    private static final String ON = "on";
+    private static final String OFF = "off";
+    private static final String SYMBOL_SIZES_CHANGE = "<html>Symbol sizes change<br>with concentration:";
+    
+    private static final int DEFAULT_SCALE = 100; // percent
+    private static final int MAX_SCALE = 500; // percent
+    private static final Dimension TOP_CANVAS_SIZE = new Dimension( 600, 175 );
+    private static final Dimension BOTTOM_CANVAS_SIZE = TOP_CANVAS_SIZE;
+    
+    private final PhetPCanvas topCanvas, bottomCanvas;
+    private AbstractEquilibriumExpressionNode topExpression;
+    private final AbstractEquilibriumExpressionNode bottomExpression;
+    
+    // developer controls
+    private EquationComboBox equationComboBox;
+    private GlobalScaleSlider globalScaleSlider;
+    private TermScaleSlider topKSlider, topLeftNumeratorSlider, topRightNumeratorSlider, topDenominatorSlider;
+    private TermScaleSlider bottomKSlider, bottomLeftNumeratorSlider, bottomRightNumeratorSlider;
+    private final JRadioButton scaleOnRadioButton, scaleOffRadioButton;
+    private final PText topCanvasSizeNode, bottomCanvasSizeNode;
+    
+    public EquilibriumExpressionsDialog( Frame owner ) {
+        this( owner, true ); //XXX default should be dev=false
+    }
+    
+    public EquilibriumExpressionsDialog( Frame owner, boolean dev ) {
+        super( owner, TITLE );
+        setResizable( dev ); // resizable only if in dev mode
+        
+        JComponent devControls = createDevControls();
+        
+        // scale on/off
+        JLabel scaleOnOffLabel = new JLabel( SYMBOL_SIZES_CHANGE );
+        ActionListener scaleOnOffActionListener = new ActionListener() {
+            public void actionPerformed( ActionEvent e ) {
+                updateScale();
+            }
+        };
+        scaleOnRadioButton = new JRadioButton( ON );
+        scaleOnRadioButton.addActionListener( scaleOnOffActionListener );
+        scaleOffRadioButton = new JRadioButton( OFF );
+        scaleOffRadioButton.addActionListener( scaleOnOffActionListener );
+        ButtonGroup buttonGroup = new ButtonGroup();
+        buttonGroup.add( scaleOffRadioButton );
+        buttonGroup.add( scaleOnRadioButton );
+        scaleOnRadioButton.setSelected( true );
+        JPanel scaleOnOffPanel = new JPanel( new GridBagLayout() );
+        scaleOnOffPanel.setBorder( new EmptyBorder( 5, 5, 5, 5 ) );
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.gridx = GridBagConstraints.RELATIVE;
+        scaleOnOffPanel.add( scaleOnOffLabel );
+        scaleOnOffPanel.add( scaleOnRadioButton );
+        scaleOnOffPanel.add( scaleOffRadioButton );
+        
+        // top canvas
+        topCanvas = new PhetPCanvas() {
+            protected void updateLayout() {
+                updateTopLayout();
+            }
+        };
+        topCanvas.setPreferredSize( TOP_CANVAS_SIZE );
+        topCanvasSizeNode = new PText();
+        topCanvasSizeNode.setOffset( 5, 5 );
+        if ( dev ) {
+            topCanvas.getLayer().addChild( topCanvasSizeNode );
+        }
+        
+        // bottom canvas
+        bottomCanvas = new PhetPCanvas() {
+            protected void updateLayout() {
+                updateBottomLayout();
+            }
+        };
+        bottomCanvas.setPreferredSize( BOTTOM_CANVAS_SIZE );
+        bottomExpression = new WaterEquilibriumExpressionNode();
+        bottomCanvas.getLayer().addChild( bottomExpression );
+        bottomCanvasSizeNode = new PText();
+        bottomCanvasSizeNode.setOffset( 5, 5 );
+        if ( dev ) {
+            bottomCanvas.getLayer().addChild( bottomCanvasSizeNode );
+        }
+        
+        // layout
+        JPanel canvasPanel = new JPanel( new GridLayout( 0, 1 ) );
+        canvasPanel.add( topCanvas );
+        canvasPanel.add( bottomCanvas);
+        JPanel userPanel = new JPanel( new BorderLayout() );
+        userPanel.add( scaleOnOffPanel, BorderLayout.NORTH );
+        userPanel.add( canvasPanel, BorderLayout.CENTER );
+        JPanel mainPanel = new JPanel( new BorderLayout() );
+        mainPanel.add( userPanel, BorderLayout.CENTER );
+        if ( dev ) {
+            mainPanel.add( devControls, BorderLayout.EAST );
+        }
+            
+        updateTopEquation();
+        getContentPane().add( mainPanel, BorderLayout.CENTER );
+        pack();
+    }
+    
+    private JComponent createDevControls() {
+        
+        // global scale slider
+        globalScaleSlider = new GlobalScaleSlider( "global scale:" );
+        globalScaleSlider.addChangeListener( new ChangeListener() {
+            public void stateChanged( ChangeEvent e ) {
+                updateNominalScale();
+            }
+        });
+        
+        // top equations controls
+        JPanel topControls = new JPanel();
+        {
+            topControls.setLayout( new BoxLayout( topControls, BoxLayout.Y_AXIS ) );
+            topControls.setBorder( new TitledBorder( "top equation" ) );
+            
+            // equation combo box
+            equationComboBox = new EquationComboBox();
+            topControls.add( equationComboBox );
+            equationComboBox.addActionListener( new ActionListener() {
+                public void actionPerformed( ActionEvent e ) {
+                    updateTopEquation();
+                }
+            } );
+            
+            topKSlider = new TermScaleSlider( "K:" );
+            topControls.add( topKSlider );
+            topKSlider.addChangeListener( new ChangeListener() {
+                public void stateChanged( ChangeEvent e ) {
+                    topExpression.setKScale( topKSlider.getValue() / 100.0 );
+                }
+            });
+            
+            topLeftNumeratorSlider = new TermScaleSlider( "numerator left:" );
+            topControls.add( topLeftNumeratorSlider );
+            topLeftNumeratorSlider.addChangeListener( new ChangeListener() {
+                public void stateChanged( ChangeEvent e ) {
+                    topExpression.setLeftNumeratorScale( topLeftNumeratorSlider.getValue() / 100.0 );
+                }
+            });
+            
+            topRightNumeratorSlider = new TermScaleSlider( "numerator right:" );
+            topControls.add( topRightNumeratorSlider );
+            topRightNumeratorSlider.addChangeListener( new ChangeListener() {
+                public void stateChanged( ChangeEvent e ) {
+                    topExpression.setRightNumeratorScale( topRightNumeratorSlider.getValue() / 100.0 );
+                }
+            });
+            
+            topDenominatorSlider = new TermScaleSlider( "denominator" );
+            topControls.add( topDenominatorSlider );
+            topDenominatorSlider.addChangeListener( new ChangeListener() {
+                public void stateChanged( ChangeEvent e ) {
+                    topExpression.setDenominatorScale( topDenominatorSlider.getValue() / 100.0 );
+                }
+            });
+        }
+
+        // bottom equation controls
+        JPanel bottomControls = new JPanel();
+        {
+            bottomControls.setLayout( new BoxLayout( bottomControls, BoxLayout.Y_AXIS ) );
+            bottomControls.setBorder( new TitledBorder( "bottom equation" ) );
+            
+            bottomKSlider = new TermScaleSlider( "K:" );
+            bottomControls.add( bottomKSlider );
+            bottomKSlider.addChangeListener( new ChangeListener() {
+                public void stateChanged( ChangeEvent e ) {
+                    bottomExpression.setKScale( bottomKSlider.getValue() / 100.0 );
+                }
+            });
+            
+            bottomLeftNumeratorSlider = new TermScaleSlider( "numerator left:" );
+            bottomControls.add( bottomLeftNumeratorSlider );
+            bottomLeftNumeratorSlider.addChangeListener( new ChangeListener() {
+                public void stateChanged( ChangeEvent e ) {
+                    bottomExpression.setLeftNumeratorScale( bottomLeftNumeratorSlider.getValue() / 100.0 );
+                }
+            });
+            
+            bottomRightNumeratorSlider = new TermScaleSlider( "numerator right:" );
+            bottomControls.add( bottomRightNumeratorSlider );
+            bottomRightNumeratorSlider.addChangeListener( new ChangeListener() {
+                public void stateChanged( ChangeEvent e ) {
+                    bottomExpression.setRightNumeratorScale( bottomRightNumeratorSlider.getValue() / 100.0 );
+                }
+            });
+        }
+        
+        // layout
+        JPanel panel = new JPanel();
+        panel.setLayout( new BoxLayout( panel, BoxLayout.Y_AXIS ) );
+        panel.setBorder( new TitledBorder( "Developer Controls" ) );
+        panel.add( globalScaleSlider );
+        panel.add( topControls );
+        panel.add( bottomControls );
+        JScrollPane scrollPane = new JScrollPane( panel );
+        
+        return scrollPane;
+    }
+    
+    private void updateTopLayout() {
+        double xOffset = ( topCanvas.getWidth() - topExpression.getFullBoundsReference().getWidth() ) / 2;
+        double yOffset = 20 + ( topCanvas.getHeight() - topExpression.getFullBoundsReference().getHeight() ) / 2;
+        topExpression.setOffset( xOffset, yOffset );
+        Dimension canvasSize = topCanvas.getSize();
+        topCanvasSizeNode.setText( "canvas size: " + canvasSize.width + "x" + canvasSize.height );
+    }
+    
+    private void updateBottomLayout() {
+        double xOffset = ( bottomCanvas.getWidth() - bottomExpression.getFullBoundsReference().getWidth() ) / 2;
+        double yOffset = 20 + ( bottomCanvas.getHeight() - bottomExpression.getFullBoundsReference().getHeight() ) / 2;
+        bottomExpression.setOffset( xOffset, yOffset );
+        Dimension canvasSize = bottomCanvas.getSize();
+        bottomCanvasSizeNode.setText( "canvas size: " + canvasSize.width + "x" + canvasSize.height );
+    }
+    
+    private void updateNominalScale() {
+        topExpression.setScale( globalScaleSlider.getValue() / 100.0 );
+        updateTopLayout();
+        bottomExpression.setScale( globalScaleSlider.getValue() / 100.0 );
+        updateBottomLayout();
+    }
+    
+    private void updateScale() {
+        final boolean enabled = scaleOnRadioButton.isSelected();
+        topKSlider.setEnabled( enabled );
+        topLeftNumeratorSlider.setEnabled( enabled );
+        topRightNumeratorSlider.setEnabled( enabled );
+        topDenominatorSlider.setEnabled( enabled );
+        bottomKSlider.setEnabled( enabled );
+        bottomLeftNumeratorSlider.setEnabled( enabled );
+        bottomRightNumeratorSlider.setEnabled( enabled );
+        if ( enabled ) {
+            // top
+            topExpression.setKScale( topKSlider.getValue() / 100.0 );
+            topExpression.setLeftNumeratorScale( topLeftNumeratorSlider.getValue() / 100.0 );
+            topExpression.setRightNumeratorScale( topRightNumeratorSlider.getValue() / 100.0 );
+            topExpression.setDenominatorScale( topDenominatorSlider.getValue() / 100.0 );
+            // bottom
+            bottomExpression.setKScale( bottomKSlider.getValue() / 100.0 );
+            bottomExpression.setLeftNumeratorScale( bottomLeftNumeratorSlider.getValue() / 100.0 );
+            bottomExpression.setRightNumeratorScale( bottomRightNumeratorSlider.getValue() / 100.0 );
+        }
+        else {
+            topExpression.setScaleAll( 1 );
+            bottomExpression.setScaleAll( 1 );
+        }
+    }
+    
+    private void updateTopEquation() {
+        // clean up existing top equation
+        double scale = 1.0;
+        if ( topExpression != null ) {
+            scale = topExpression.getScale();
+            topCanvas.getLayer().removeChild( topExpression );
+        }
+        // set the new equation
+        topExpression = equationComboBox.getSelectedNode();
+        topExpression.setScale( scale );
+        topExpression.setScaleAll( 1 );
+        topCanvas.getLayer().addChild( topExpression );
+        // update layouts
+        updateTopLayout();
+        updateScale();
+    }
+    
+    private static class EquationChoice {
+
+        private final String name;
+        private final AbstractEquilibriumExpressionNode node;
+        
+        public EquationChoice( String name, AbstractEquilibriumExpressionNode node ) {
+            this.name = name;
+            this.node = node;
+        }
+        
+        public String toString() {
+            return name;
+        }
+        
+        public AbstractEquilibriumExpressionNode getNode() {
+            return node;
+        }
+    }
+    
+    private static class EquationComboBox extends JComboBox {
+        
+        public EquationComboBox() {
+            addItem( new EquationChoice( "weak acid", new WeakAcidEquilibriumExpressionNode() ) );
+            addItem( new EquationChoice( "strong acid", new StrongAcidEquilibriumExpressionNode() ) );
+            addItem( new EquationChoice( "weak base", new WeakBaseEquilibriumExpressionNode() ) );
+            addItem( new EquationChoice( "strong base", new StrongBaseEquilibriumExpressionNode() ) );
+            addItem( new EquationChoice( "acetic acid", new WeakAcidEquilibriumExpressionNode( "CH<sub>3</sub>COO<sup>-</sup>", "CH<sub>3</sub>COOH" ) ) );
+            addItem( new EquationChoice( "hydrochloric acid", new StrongAcidEquilibriumExpressionNode( "Cl<sup>-</sup>", "HCl" ) ) );
+            addItem( new EquationChoice( "pyridine", new WeakBaseEquilibriumExpressionNode( "C<sub>5</sub>H<sub>5</sub>NH<sup>+</sup>", "C<sub>5</sub>H<sub>5</sub>N" ) ) );
+            addItem( new EquationChoice( "sodium hydroxide", new StrongBaseEquilibriumExpressionNode( "Na<sup>+</sup>", "NaOH" ) ) );
+        }
+        
+        public AbstractEquilibriumExpressionNode getSelectedNode() {
+            return ((EquationChoice) getSelectedItem()).getNode();
+        }
+    }
+    
+    private static class HorizontalLayoutStrategy implements ILayoutStrategy {
+
+        public HorizontalLayoutStrategy() {}
+        
+        public void doLayout( AbstractValueControl valueControl ) {
+
+            // Get the components that will be part of the layout
+            JComponent slider = valueControl.getSlider();
+            JComponent textField = valueControl.getTextField();
+            JComponent valueLabel = valueControl.getValueLabel();
+            JComponent unitsLabel = valueControl.getUnitsLabel();
+
+            // Label - slider - textfield - units.
+            EasyGridBagLayout layout = new EasyGridBagLayout( valueControl );
+            valueControl.setLayout( layout );
+            layout.addComponent( valueLabel, 0, 0 );
+            layout.addFilledComponent( slider, 0, 1, GridBagConstraints.HORIZONTAL );
+            layout.addFilledComponent( textField, 0, 2, GridBagConstraints.HORIZONTAL );
+            layout.addComponent( unitsLabel, 0, 3 );
+        }
+    }
+    
+    private static class GlobalScaleSlider extends LinearValueControl {
+
+        public GlobalScaleSlider( String label ) {
+            super( 50, 200, label, "##0", "%", new HorizontalLayoutStrategy() );
+            setValue( DEFAULT_SCALE );
+            getSlider().setPaintTicks( false );
+            getSlider().setPaintLabels( false );
+        }
+    }
+    
+    private static class TermScaleSlider extends LinearValueControl {
+
+        public TermScaleSlider( String label ) {
+            super( 1, MAX_SCALE, label, "##0", "%", new HorizontalLayoutStrategy() );
+            setValue( DEFAULT_SCALE );
+            getSlider().setPaintTicks( false );
+            getSlider().setPaintLabels( false );
+        }
+    }
+    
+    public static void main( String[] args ) {
+        JDialog frame = new EquilibriumExpressionsDialog( null, true );
+        frame.setVisible( true );
+    }
+}
