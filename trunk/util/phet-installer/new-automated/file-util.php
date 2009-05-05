e7ce3043fbf1ec9a62b7302531e5aa8c345821e0
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-05-05 17:55:54 +0000

    Added the ability to recursively update permissions in order to resolve problems with other phet developers building installers.  This fix was necessary because HTTrack seems to use something other than the umask for setting permissions on ripped files.  See #1154.

--- trunk/util/phet-installer/new-automated/file-util.php
+++ trunk/util/phet-installer/new-automated/file-util.php
@@ -301,4 +301,36 @@
 		}
 	}
 
+    function file_chmod_recursive($path, $filePerm=0644, $dirPerm=0755) {
+
+        // Check if the path exists
+        if(!file_exists($path)) {
+             return(FALSE);
+        }      
+
+        if(is_file($path))
+        {
+            // This is a file - chmod the file with the given permissions
+            chmod($path, $filePerm);
+        } elseif(is_dir($path)) {
+            // This is a directory...
+            // Get an array of the contents
+            $foldersAndFiles = scandir($path);
+            // Remove "." and ".." from the list
+            $entries = array_slice($foldersAndFiles, 2);
+            // Parse every result...
+            foreach($entries as $entry)
+            {
+               if ( ( $entry != '.' ) && ( $entry != '..' ) ){
+                   // And call this function again recursively, with the same permissions
+                   file_chmod_recursive($path."/".$entry, $filePerm, $dirPerm);
+               }
+            }
+            // When we are done with the contents of the directory, we chmod the directory itself
+            chmod($path, $dirPerm);
+        }
+        // Everything seemed to work out well, return TRUE
+        return(TRUE);
+    }
+
 ?>
