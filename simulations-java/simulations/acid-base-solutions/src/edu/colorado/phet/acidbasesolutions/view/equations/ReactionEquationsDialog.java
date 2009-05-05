1d8b1df357d5ce89a687ede8a08e7ef6a974fcaf
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-05-05 22:08:00 +0000

    organize imports

--- simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/view/equations/ReactionEquationsDialog.java
+++ simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/view/equations/ReactionEquationsDialog.java
@@ -5,7 +5,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.*;
-import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
 import javax.swing.border.TitledBorder;
