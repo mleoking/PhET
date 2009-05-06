ebd5b4273b44da7767094162234063c19c02ca3f
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-05-06 22:36:01 +0000

    util to show all System properties and default values

--- trunk/team/cmalley/src/com/pixelzoom/util/PrintSystemProperties.java
+++ trunk/team/cmalley/src/com/pixelzoom/util/PrintSystemProperties.java
@@ -1,10 +1,17 @@
 
 package com.pixelzoom.util;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.Window;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Properties;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 
 /**
@@ -16,6 +23,8 @@ import java.util.Properties;
  */
 public class PrintSystemProperties {
 
+    private static final boolean PRINT_TO_STDOUT = false;
+    
     public static void main( String[] args ) {
         
         // Get the system properties
@@ -26,11 +35,40 @@ public class PrintSystemProperties {
         List keys = Arrays.asList( keySet );
         Collections.sort( keys );
 
-        // Print out each key/value pair
+        // table data
+        String[][] rowData = new String[keys.size()][2];
         for( int i = 0; i < keys.size(); i++ ) {
             Object key = keys.get( i );
             Object value = properties.get( key );
-            System.out.println( key + ": " + value );
+            if ( PRINT_TO_STDOUT ) {
+                System.out.println( key + ": " + value );
+            }
+            rowData[i][0] = key.toString();
+            rowData[i][1] = "" + value.toString();
+            if ( PRINT_TO_STDOUT ) {
+                System.out.println( rowData[i][0] + "=" + rowData[i][1] );
+            }
         }
+        
+        // table in a scroll pane
+        String[] colName = { "Key", "Value" };
+        JTable table = new JTable( rowData, colName );
+        JScrollPane pane = new JScrollPane( table );
+
+        // display in a frame
+        JFrame frame = new JFrame( "System properties & default values" );
+        frame.setContentPane( pane );
+        frame.pack();
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        centerOnScreen( frame );
+        frame.setVisible( true );
+    }
+
+    private static void centerOnScreen( Window window ) {
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        Dimension screenSize = tk.getScreenSize();
+        int x = (int) ( screenSize.getWidth() / 2 - window.getWidth() / 2 );
+        int y = (int) ( screenSize.getHeight() / 2 - window.getHeight() / 2 );
+        window.setLocation( x, y );
     }
 }
