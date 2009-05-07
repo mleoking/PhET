0f4f834f606323bf2e9bfe5919e1982f6d1fad45
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-05-07 15:54:51 +0000

    start chart implementation

---
+++ trunk/simulations-java/simulations/the-ramp/scala-src/edu/colorado/phet/therampscala/charts/Chart.scala
@@ -0,0 +1,51 @@
+package edu.colorado.phet.therampscala.charts
+
+
+import common.jfreechartphet.piccolo.JFreeChartNode
+import common.motion.graphs.JFreeChartDecorator.{DottedZeroLine, InChartTickMarks}
+import common.motion.graphs.{JFreeChartDecorator, ControlGraph}
+import common.piccolophet.PhetPCanvas
+import javax.swing.JFrame
+import org.jfree.chart.axis.{NumberAxis, ValueAxis}
+import org.jfree.chart.JFreeChart
+import org.jfree.chart.plot.XYPlot
+import org.jfree.chart.renderer.xy.{XYLineAndShapeRenderer, XYItemRenderer}
+import org.jfree.data.xy.{DefaultXYDataset, XYDataset}
+import umd.cs.piccolo.nodes.PText
+import umd.cs.piccolo.PNode
+
+class Chart extends PNode {
+//  val dataset = new DefaultXYDataset
+//  val domainAxis = new NumberAxis
+//  domainAxis.setRange(0, 20)
+//  val rangeAxis = new NumberAxis
+//  rangeAxis.setRange(-10, 10)
+//  val plot = new XYPlot(dataset, domainAxis, rangeAxis, new XYLineAndShapeRenderer)
+//  val chart = new JFreeChart(plot)
+  val chart = ControlGraph.createDefaultChart("Title")
+  chart.getXYPlot.getDomainAxis.setRange(0,20)
+  chart.getXYPlot.getRangeAxis.setRange(-10,10)
+  chart.addJFreeChartNodeGraphic(new InChartTickMarks(2,2,10))
+  chart.addJFreeChartNodeGraphic(new DottedZeroLine)
+  val chartNode = new JFreeChartNode(chart)
+
+  chartNode.setBounds(0, 0, 700, 400)
+  addChild(chartNode)
+}
+
+class TestPNode(nodes: PNode*) {
+  val frame = new JFrame
+  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
+  frame.setSize(800, 600)
+  val canvas = new PhetPCanvas()
+  for (node <- nodes)
+    canvas.addScreenChild(node)
+  frame.setContentPane(canvas)
+  frame.setVisible(true)
+}
+
+object TestChart {
+  def main(args: Array[String]) {
+    new TestPNode(new PText("hello"), new Chart)
+  }
+}
