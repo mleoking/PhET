638ca2d89205eda1f8a037d1cfa57e920f80bbd9
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-05-11 15:19:21 +0000

    these are the constants that we should be using to configure Java.  Feel free to rename, but consolidate them here.

---
+++ trunk/build-tools/src/edu/colorado/phet/buildtools/BuildToolsConstants.java
@@ -0,0 +1,15 @@
+package edu.colorado.phet.buildtools;
+
+/**
+ * Configuration constants for build-tools.
+ */
+public class BuildToolsConstants {
+    
+    /* not intended for instantiation */
+    private BuildToolsConstants() {}
+    
+    public static final String SIM_JAVA_VERSION="1.5";
+    public static final String SIM_SCALA_VERSION="jvm-1.5";
+    public static final String BOOTSTRAP_JAVA_VERSION="1.4";
+
+}
