314a20bd5ab6563eb6bf422a170315ce40216628
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-05-12 06:09:54 +0000

    unzip library files and add java.library.path dynamically, see #1655

--- trunk/simulations-java/simulations/density/src/edu/colorado/phet/densityjava/DensityApplication.java
+++ trunk/simulations-java/simulations/density/src/edu/colorado/phet/densityjava/DensityApplication.java
@@ -27,6 +27,7 @@ import edu.colorado.phet.common.phetcommon.application.Module;
 import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
 import edu.colorado.phet.common.phetcommon.application.PhetApplicationLauncher;
 import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
+import edu.colorado.phet.common.phetcommon.util.FileUtils;
 import edu.colorado.phet.common.piccolophet.PiccoloPhetApplication;
 import jmetest.intersection.TestTrianglePick;
 import jmetest.util.JMESwingTest;
@@ -34,10 +35,15 @@ import jmetest.util.JMESwingTest;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.*;
+import java.lang.reflect.Field;
 import java.net.URL;
 import java.nio.FloatBuffer;
+import java.util.Enumeration;
 import java.util.concurrent.Callable;
 import java.util.logging.Logger;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 public class DensityApplication extends PiccoloPhetApplication {
     private static final Logger logger = Logger.getLogger(JMESwingTest.class
@@ -59,8 +65,110 @@ public class DensityApplication extends PiccoloPhetApplication {
 
     }
 
-    public static void main(String[] args) {
+    public static void addDir(String s) throws IOException {
+        try {
+            Field field = ClassLoader.class.getDeclaredField("usr_paths");
+            field.setAccessible(true);
+            String[] paths = (String[]) field.get(null);
+            for (int i = 0; i < paths.length; i++) {
+                if (s.equals(paths[i])) {
+                    return;
+                }
+            }
+            String[] tmp = new String[paths.length + 1];
+            System.arraycopy(paths, 0, tmp, 0, paths.length);
+            tmp[paths.length] = s;
+            field.set(null, tmp);
+        } catch (IllegalAccessException e) {
+            throw new IOException("Failed to get permissions to set library path");
+        } catch (NoSuchFieldException e) {
+            throw new IOException("Failed to get field handle to set library path");
+        }
+    }
+
+
+    public static void unzip(File zipFileName, File targetDir) throws IOException {
+        unzip(zipFileName, targetDir, new FileFilter() {
+            public boolean accept(File file) {
+                return true;
+            }
+        });
+    }
+
+    public static void copy(InputStream source, OutputStream dest, boolean buffered) throws IOException {
+        //TODO: buffering is disabled until file truncation issue is resolved
+        buffered = false;
+        if (buffered) {
+            source = new BufferedInputStream(source);
+            dest = new BufferedOutputStream(dest);
+        }
+
+        int bytesRead;
+
+        byte[] buffer = new byte[1024];
+
+        while ((bytesRead = source.read(buffer)) >= 0) {
+            dest.write(buffer, 0, bytesRead);
+        }
+    }
+
+    public static void copyAndClose(InputStream source, OutputStream dest, boolean buffered) throws IOException {
+        copy(source, dest, buffered);
+        source.close();
+        dest.close();
+    }
+
+    public static void unzip(File zipFileName, File targetDir, FileFilter filter) throws IOException {
+        ZipFile zipFile = new ZipFile(zipFileName);
+
+        Enumeration enumeration = zipFile.entries();
+
+        while (enumeration.hasMoreElements()) {
+            ZipEntry entry = (ZipEntry) enumeration.nextElement();
+
+            String name = entry.getName();
+
+            if (filter.accept(new File(targetDir, name))) {
+                if (entry.isDirectory()) {
+                    new File(targetDir, name).mkdirs();
+                } else {
+                    File targetFile = new File(targetDir, name);
+
+                    targetFile.getParentFile().mkdirs();
+
+                    InputStream source = zipFile.getInputStream(entry);
+                    FileOutputStream fileOutputStream = new FileOutputStream(targetFile);
+
+                    copyAndClose(source, fileOutputStream, false);
+                }
+            }
+        }
+        zipFile.close();
+    }
+
+    public static void copyTo(File source, File dest) throws IOException {
+        dest.getParentFile().mkdirs();
+        copyAndClose(new FileInputStream(source), new FileOutputStream(dest), true);
+    }
+
+    public static void main(String[] args) throws IOException {
+//        if (!Arrays.asList(args).contains("withlib")) {
+//            //extract natives to user's .phet-natives directory.
+//            //copy this jar file so it can be unzipped while it's being read?
+//            String[] cmdArray = new String[]{PhetUtilities.getJavaPath(), "-jar", updaterBootstrap.getAbsolutePath(), src.getAbsolutePath(), dst.getAbsolutePath()};
+////        log("Starting updater bootstrap with cmdArray=" + Arrays.asList(cmdArray).toString());
+//            Runtime.getRuntime().exec(cmdArray);
+//        } else {
+        File codeSource = FileUtils.getCodeSource();
+        File copy = new File(System.getProperty("java.io.tmpdir"), codeSource.getName());
+        copyTo(codeSource, copy);
+        File dir = new File(copy.getParentFile(), "unzipped");
+        unzip(copy, dir);
+        System.out.println("DensityApplication.main, unzip dir=" + dir.getAbsolutePath());
+
+        addDir(new File(dir, "natives").getAbsolutePath());
         new PhetApplicationLauncher().launchSim(args, "density", DensityApplication.class);
+//        }
     }
 
     class SwingFrame {
