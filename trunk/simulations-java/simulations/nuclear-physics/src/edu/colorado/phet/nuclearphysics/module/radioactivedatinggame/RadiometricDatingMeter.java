44524f10c597d68aaed8d6f1c5eac07a98ae2c72
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-05-15 22:24:39 +0000

    Added initialization of the meter and the meter node so that the initial percentage shown is correct.

--- trunk/simulations-java/simulations/nuclear-physics/src/edu/colorado/phet/nuclearphysics/module/radioactivedatinggame/RadiometricDatingMeter.java
+++ trunk/simulations-java/simulations/nuclear-physics/src/edu/colorado/phet/nuclearphysics/module/radioactivedatinggame/RadiometricDatingMeter.java
@@ -38,6 +38,8 @@ public class RadiometricDatingMeter {
 				updateReading();
 			}
 		});
+		
+		updateReading();
 	}
 
 	//----------------------------------------------------------------------------
