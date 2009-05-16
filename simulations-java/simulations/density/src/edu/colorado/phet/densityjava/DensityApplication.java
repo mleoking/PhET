5df37be1bb0689ec8f36db8a903027ac10927efc
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-05-16 20:34:00 +0000

    inital draft for dynamics

--- simulations-java/simulations/density/src/edu/colorado/phet/densityjava/DensityApplication.java
+++ simulations-java/simulations/density/src/edu/colorado/phet/densityjava/DensityApplication.java
@@ -4,6 +4,8 @@ import edu.colorado.phet.common.phetcommon.application.Module;
 import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
 import edu.colorado.phet.common.phetcommon.application.PhetApplicationLauncher;
 import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
+import edu.colorado.phet.common.phetcommon.model.clock.ClockAdapter;
+import edu.colorado.phet.common.phetcommon.model.clock.ClockEvent;
 import edu.colorado.phet.common.piccolophet.PiccoloPhetApplication;
 import edu.colorado.phet.densityjava.model.DensityModel;
 
@@ -29,6 +31,11 @@ public class DensityApplication extends PiccoloPhetApplication {
             super("density", new ConstantDtClock(30, 30 / 1000.0));
             panel = new DensityJMECanvas(frame, model);
             setSimulationPanel(panel);
+            getClock().addClockListener(new ClockAdapter(){
+                public void simulationTimeChanged(ClockEvent clockEvent) {
+                    model.stepInTime(clockEvent.getSimulationTimeChange());
+                }
+            });
         }
 
         public void activate() {
