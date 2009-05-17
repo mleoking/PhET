64eeab1f054f206b3d248cd15de7b5df4d38e89d
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-05-17 22:53:13 +0000

    Resource deploy work. see #692

--- trunk/build-tools/src/edu/colorado/phet/buildtools/translate/ResourceDeployServer.java
+++ trunk/build-tools/src/edu/colorado/phet/buildtools/translate/ResourceDeployServer.java
@@ -1,6 +1,8 @@
 package edu.colorado.phet.buildtools.translate;
 
 import java.io.File;
+import java.io.PrintStream;
+import java.io.FileOutputStream;
 
 public class ResourceDeployServer {
 
@@ -12,11 +14,32 @@ public class ResourceDeployServer {
         this.jarCommand = jarCommand;
         this.buildLocalProperties = buildLocalProperties;
         this.resourceDir = resourceDir;
+
+        FileOutputStream out; // declare a file output object
+        PrintStream p; // declare a print stream object
+
+        try {
+            // Create a new file output stream
+            // connected to "myfile.txt"
+            out = new FileOutputStream( new File( resourceDir, "status.txt")  );
+
+            // Connect print stream to the output stream
+            p = new PrintStream( out );
+
+            p.println( "Success" );
+
+            p.close();
+        }
+        catch( Exception e ) {
+            System.err.println( "Error writing to file" );
+        }
     }
 
     public static void main( String[] args ) {
         System.out.println( "Running ResourceDeployServer" );
+
         new ResourceDeployServer( args[0], new File( args[1] ), new File( args[2] ) );
+        
     }
 
 }
