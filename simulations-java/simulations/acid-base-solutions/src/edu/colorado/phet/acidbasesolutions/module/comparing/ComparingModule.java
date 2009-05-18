54ac79c343c9bde703b3ab3cfdfc8c1f50ae5c02
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-05-18 17:38:44 +0000

    disable debug output

--- simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/module/comparing/ComparingModule.java
+++ simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/module/comparing/ComparingModule.java
@@ -68,7 +68,7 @@ public class ComparingModule extends ABSAbstractModule {
      * Resets the module.
      */
     public void reset() {
-        System.out.println( getClass().getName() + ".reset" );//XXX
+//        System.out.println( getClass().getName() + ".reset" );//XXX
     }
     
     //----------------------------------------------------------------------------
