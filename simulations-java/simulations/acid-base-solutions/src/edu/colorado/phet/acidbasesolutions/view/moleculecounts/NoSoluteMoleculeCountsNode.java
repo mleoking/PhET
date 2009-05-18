89d1cdb925a8eb7ecbb32f8b532fe7af12aecf0d
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-05-18 20:55:09 +0000

    refactor and repackage molecule counts class hierarchy

---
+++ simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/view/moleculecounts/NoSoluteMoleculeCountsNode.java
@@ -0,0 +1,12 @@
+/* Copyright 2009, University of Colorado */
+package edu.colorado.phet.acidbasesolutions.view.moleculecounts;
+
+
+public class NoSoluteMoleculeCountsNode extends AbstractMoleculeCountsNode {
+
+    public NoSoluteMoleculeCountsNode() {
+        super();
+        setVisible( 0, false );
+        setVisible( 1, false );
+    }
+}
