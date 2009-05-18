4ef6a5cacaa3282719bdce6a33a062feacaa144f
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-05-18 11:22:35 +0000

    Common translation deploy process: initial publisher work

--- build-tools/src/edu/colorado/phet/buildtools/translate/ResourceDeployPublisher.java
+++ build-tools/src/edu/colorado/phet/buildtools/translate/ResourceDeployPublisher.java
@@ -1,6 +1,33 @@
 package edu.colorado.phet.buildtools.translate;
 
+import java.io.File;
+
 import edu.colorado.phet.common.phetcommon.util.IProguardKeepClass;
 
 public class ResourceDeployPublisher implements IProguardKeepClass {
+
+    private File resourceDir;
+    private File testDir;
+    private File liveSimDir;
+
+    public ResourceDeployPublisher( File resourceDir ) {
+        this.resourceDir = resourceDir;
+
+        testDir = new File( resourceDir, "test" );
+        liveSimDir = new File( resourceDir, "../../" );
+
+        File[] testDirs = testDir.listFiles();
+
+        for ( int i = 0; i < testDirs.length; i++ ) {
+            File dir = testDirs[i];
+
+            String sim = dir.getName();
+
+            
+        }
+    }
+
+    public static void main( String[] args ) {
+        new ResourceDeployPublisher( new File( args[0] ) );
+    }
 }
