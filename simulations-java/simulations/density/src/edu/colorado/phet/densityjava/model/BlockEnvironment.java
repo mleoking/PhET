39509f7138a5d52d6f6c79eecf7f1adc49bb573c
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-05-18 14:51:03 +0000

    organize and document

--- simulations-java/simulations/density/src/edu/colorado/phet/densityjava/model/BlockEnvironment.java
+++ simulations-java/simulations/density/src/edu/colorado/phet/densityjava/model/BlockEnvironment.java
@@ -1,12 +1,6 @@
 package edu.colorado.phet.densityjava.model;
 
-/**
- * Created by IntelliJ IDEA.
- * User: Owner
- * Date: May 18, 2009
- * Time: 8:24:46 AM
- * To change this template use File | Settings | File Templates.
- */
+//environment for computing applied forces and collisions
 public interface BlockEnvironment {
     double getFloorY(Block block);
 
--- simulations-java/simulations/density/src/edu/colorado/phet/densityjava/model/DensityModelBlockEnvironment.java
+++ simulations-java/simulations/density/src/edu/colorado/phet/densityjava/model/DensityModelBlockEnvironment.java
@@ -1,5 +1,6 @@
 package edu.colorado.phet.densityjava.model;
 
+//environment for computing applied forces and collisions
 public class DensityModelBlockEnvironment implements BlockEnvironment {
     private DensityModel model;
 
@@ -7,18 +8,23 @@ public class DensityModelBlockEnvironment implements BlockEnvironment {
         this.model = model;
     }
 
+    //Find the Y value of whatever the block could eventually settle on (i.e. the ground, the pool bottom, another block)
     public double getFloorY(Block block) {
         DensityModel.RectangularObject target = model.getHighestObjectBelow(block);
         if (target != null) {
             return target.getMaxY();
         }
-        Water water = model.getWater();
-        if (water.getWidthRange().contains(block.getWidthRange()))
-            return water.getBottomY();
+        if (getWater().getWidthRange().contains(block.getWidthRange()))
+            return getWater().getBottomY();
         else
-            return water.getSwimmingPoolSurfaceY();
+            return getWater().getSwimmingPoolSurfaceY();
     }
 
+    public Water getWater() {
+        return model.getWater();
+    }
+
+    //Determine the applied force on a block (e.g. due to other blocks resting on it)
     public double getAppliedForce(Block block) {
         //if there is a block sitting on top of this block, the applied force
         //due to that block is equal to -N, since F12=-F21
@@ -29,6 +35,8 @@ public class DensityModelBlockEnvironment implements BlockEnvironment {
             Block above = (Block) justAbove;
             sum += -above.getNormalForce();
         }
+        //commented out because can cause cycles:
+        // F_normal_1=f(F_applied_2) and F_applied_2=f(F_normal_1)
 //            if (justBeneath != null && justBeneath instanceof Block && justBeneath.getDistanceY(block) < 0.01) {
 //                Block beneath = (Block) justBeneath;
 //                sum += beneath.getNormalForce();
