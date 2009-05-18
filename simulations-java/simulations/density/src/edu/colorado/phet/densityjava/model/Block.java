a1381197d1a47154a0bbe422202519dfe407e856
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-05-18 14:30:14 +0000

    refactor model

---
+++ simulations-java/simulations/density/src/edu/colorado/phet/densityjava/model/Block.java
@@ -0,0 +1,81 @@
+package edu.colorado.phet.densityjava.model;
+
+import java.awt.*;
+
+public class Block extends DensityModel.RectangularObject {
+    private BlockEnvironment blockEnvironment;
+    private double mass;
+    private double velocity = 0;
+    private DensityModel.Water water;
+    private boolean userDragging = false;
+
+    public Block(String name, double dim, DensityModel.Water water, double x, double y, Color color, double mass, BlockEnvironment blockEnvironment) {
+        super(name, x, y, dim, dim, dim, color);
+        this.water = water;
+        this.mass = mass;
+        this.blockEnvironment = blockEnvironment;
+    }
+
+    public void stepInTime(double simulationTimeChange) {
+        if (!userDragging) {
+            double force = getGravityForce() + getBuoyancyForce() + getAppliedForce() + getNormalForce();
+            double accel = force / mass;
+            velocity += accel * simulationTimeChange;
+            velocity = velocity * getDragCoefficient();
+            setPosition2D(getX(), getY() + velocity * simulationTimeChange);
+            if (getY() <= getFloorY()) {
+                setPosition2D(getX(), getFloorY());
+                velocity = 0;
+            }
+        }
+    }
+
+    public double getAppliedForce() {
+        return blockEnvironment.getAppliedForce(this);
+    }
+
+    public double getDragCoefficient() {
+        return 0.95;
+    }
+
+    //the bottom of the pool if over the pool, otherwise the ground level
+    //todo: generalize for block stacking
+    private double getFloorY() {
+        return blockEnvironment.getFloorY(this);
+    }
+
+    public double getNormalForce() {
+        if (getY() <= getFloorY() && getGravityForce() + getBuoyancyForce() + getAppliedForce() < 0) {
+            return -getGravityForce() - getBuoyancyForce() - getAppliedForce();
+        } else return 0;
+    }
+
+
+    public void setVelocity(double v) {
+        this.velocity = v;
+        notifyListeners();
+    }
+
+    //According to Archimedes' principle,
+    // "Any object, wholly or partly immersed in a fluid,
+    // is buoyed up by a force equal to the weight of the fluid displaced by the object."
+    private double getBuoyancyForce() {
+        double volumeDisplaced = getSubmergedVolume(water.getHeight());
+        double massDisplaced = water.getDensity() * volumeDisplaced / 500;
+        double weightDisplaced = massDisplaced * 9.8;
+        return weightDisplaced;
+    }
+
+    private double getGravityForce() {
+        return -9.8 * mass;
+    }
+
+    public void setUserDragging(boolean userDragging) {
+        this.userDragging = userDragging;
+    }
+
+    public double getSpeed() {
+        return Math.abs(velocity);
+    }
+
+}
