894fe163b7d4aeb18bf81bb0d7a987a43e97f792
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-05-19 11:44:07 +0000

    Build-tools: fixed Java version for flash-launcher. see #1667

--- trunk/build-tools/src/edu/colorado/phet/buildtools/BuildToolsConstants.java
+++ trunk/build-tools/src/edu/colorado/phet/buildtools/BuildToolsConstants.java
@@ -11,6 +11,7 @@ public class BuildToolsConstants {
     // Java version config
     public static final String SIM_JAVA_VERSION="1.5"; // see javac
     public static final String SIM_SCALA_VERSION="jvm-1.4"; // see Scalac.Target
+    public static final String FLASH_LAUNCHER_JAVA_VERSION="1.4"; // see javac
     public static final String BOOTSTRAP_JAVA_VERSION="1.4"; // see javac
 
 }
