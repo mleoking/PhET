dc9e8b66605358c0d1b834a8d46138a94bbc27dd
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-05-26 18:37:59 +0000

    Moved the "edge of the world" into the model and added code in the view to set and monitor it.  Still needs to be made to look correct, but the positioning is nearly correct.

--- simulations-java/simulations/nuclear-physics/src/edu/colorado/phet/nuclearphysics/module/radioactivedatinggame/EdgeOfWorldNode.java
+++ simulations-java/simulations/nuclear-physics/src/edu/colorado/phet/nuclearphysics/module/radioactivedatinggame/EdgeOfWorldNode.java
@@ -8,21 +8,34 @@ import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTra
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.umd.cs.piccolo.PNode;
 
+/**
+ * This class represents a Piccolo node that is intended to create a 3D-like
+ * view of the edge of the world.
+ * 
+ * @author John Blanco
+ */
 public class EdgeOfWorldNode extends PNode {
 	
 	private RadioactiveDatingGameModel _model;
 	private ModelViewTransform2D _mvt;
 	private PhetPPath _testShape;
 
+	/**
+	 * Constructor.
+	 * 
+	 * @param model
+	 * @param canvas
+	 * @param mvt
+	 */
 	public EdgeOfWorldNode( RadioactiveDatingGameModel model, ModelViewTransform2D mvt){
+		
 		_model = model;
 		_mvt = mvt;
 		_testShape = new PhetPPath(Color.red,new BasicStroke(2), Color.red);
-		_testShape.setPathTo(_mvt.createTransformedShape(_model.getEdgeOfWorld()));
 		addChild( _testShape );
 		_model.addObserver(new SimpleObserver(){
 			public void update() {
-				_testShape.setPathTo(_mvt.createTransformedShape(_model.getEdgeOfWorld()));
+				_testShape.setPathTo(_mvt.createTransformedShape(_model.getEdgeOfWorldRect()));
 			}
 		});
 	}
