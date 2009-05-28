708e51ee5bab23b9c10b957283545a978c008152
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-05-28 03:35:41 +0000

    started xith feasibility test

---
+++ simulations-java/simulations/density/src-xith/edu/colorado/phet/density/view/xith/MySC.java
@@ -0,0 +1,9 @@
+package edu.colorado.phet.density.view.xith;
+
+import org.xith3d.render.Canvas3DJPanel;
+
+public class MySC  {
+    public MySC(Canvas3DJPanel canvasPanel) {
+        super(canvasPanel);
+    }
+}
