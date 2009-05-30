cfde22b221fd5ea54b884e426ef27d48ee4ff4e4
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-05-30 06:00:57 +0000

    Natural selection: jfreechart tests

--- simulations-java/simulations/natural-selection/src/edu/colorado/phet/naturalselection/test/TestCanvas.java
+++ simulations-java/simulations/natural-selection/src/edu/colorado/phet/naturalselection/test/TestCanvas.java
@@ -6,6 +6,7 @@ import java.awt.geom.Rectangle2D;
 
 import org.jfree.chart.ChartRenderingInfo;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.title.LegendTitle;
 import org.jfree.chart.axis.NumberAxis;
 import org.jfree.chart.axis.ValueAxis;
 import org.jfree.chart.plot.PlotRenderingInfo;
@@ -23,7 +24,8 @@ import edu.umd.cs.piccolo.PNode;
 public class TestCanvas extends PhetPCanvas {
     private JFreeChartNode chartNode;
     private XYPlotNode plotNode;
-    private XYSeries series;
+    private XYSeries totalSeries;
+    private XYSeries whiteSeries;
     private XYPlot mainPlot;
 
     private static final int RANGE = 300;
@@ -36,20 +38,31 @@ public class TestCanvas extends PhetPCanvas {
 
         XYPlot emptyPlot = createPlot();
         JFreeChart chart = new JFreeChart( emptyPlot );
+
         chartNode = new JFreeChartNode( chart );
         root.addChild( chartNode );
 
-        final int seriesIndex = 0;
-        series = new XYSeries( "Data" );
-        XYSeriesCollection dataset = new XYSeriesCollection();
-        dataset.addSeries( series );
+        final int totalIndex = 0;
+        totalSeries = new XYSeries( "Total" );
+        XYSeriesCollection totalDataset = new XYSeriesCollection();
+        totalDataset.addSeries( totalSeries );
+
+        final int whiteIndex = 1;
+        whiteSeries = new XYSeries( "White Fur" );
+        XYSeriesCollection whiteDataset = new XYSeriesCollection();
+        whiteDataset.addSeries( whiteSeries );
 
         mainPlot = createPlot();
-        mainPlot.setDataset( seriesIndex, dataset );
-        XYItemRenderer renderer = new StandardXYItemRenderer();
-        renderer.setPaint( Color.RED );
-        renderer.setStroke( new BasicStroke( 2f ) );
-        mainPlot.setRenderer( seriesIndex, renderer );
+        mainPlot.setDataset( totalIndex, totalDataset );
+        mainPlot.setDataset( whiteIndex, whiteDataset );
+        XYItemRenderer totalRenderer = new StandardXYItemRenderer();
+        totalRenderer.setPaint( Color.RED );
+        totalRenderer.setStroke( new BasicStroke( 2f ) );
+        mainPlot.setRenderer( totalIndex, totalRenderer );
+        XYItemRenderer whiteRenderer = new StandardXYItemRenderer();
+        whiteRenderer.setPaint( Color.BLUE );
+        whiteRenderer.setStroke( new BasicStroke( 2f ) );
+        mainPlot.setRenderer( whiteIndex, whiteRenderer );
 
         plotNode = new XYPlotNode( mainPlot );
         root.addChild( plotNode );
@@ -61,7 +74,6 @@ public class TestCanvas extends PhetPCanvas {
         XYPlot plot = new XYPlot();
 
         ValueAxis domainAxis = new NumberAxis( "Time" );
-        //domainAxis.setVisible( false );
         domainAxis.setTickLabelsVisible( false );
         domainAxis.setRange( 0, RANGE );
         plot.setDomainAxis( domainAxis );
@@ -108,25 +120,37 @@ public class TestCanvas extends PhetPCanvas {
     private int low = 0;
     private int MIN_Y = 0;
     private int MAX_Y = 50;
-    private double lastY = 25;
+    private double lastTotalY = 25;
+    private double lastWhiteY = 20;
 
     public void addDataPoint() {
-        series.setNotify( false );
+        totalSeries.setNotify( false );
+        whiteSeries.setNotify( false );
 
-        lastY += Math.random() * 4 - 2;
-        if ( lastY > MAX_Y ) { lastY = MAX_Y; }
-        if ( lastY < MIN_Y ) { lastY = MIN_Y; }
+        lastTotalY += Math.random() * 4 - 2;
+        if ( lastTotalY > MAX_Y ) { lastTotalY = MAX_Y; }
+        if ( lastTotalY < MIN_Y ) { lastTotalY = MIN_Y; }
 
-        series.add( pos++, (int) lastY );
+        totalSeries.add( pos, lastTotalY );
+
+        lastWhiteY += Math.random() * 4 - 2;
+        if ( lastWhiteY > MAX_Y ) { lastWhiteY = MAX_Y; }
+        if ( lastWhiteY < MIN_Y ) { lastWhiteY = MIN_Y; }
+
+        whiteSeries.add( pos, lastWhiteY );
+
+        pos++;
 
         if ( pos >= RANGE + 1 ) {
-            series.remove( 0 );
+            totalSeries.remove( 0 );
+            whiteSeries.remove( 0 );
             low++;
 
             mainPlot.getDomainAxis().setRange( low, low + RANGE );
         }
 
-        series.setNotify( true );
+        totalSeries.setNotify( true );
+        whiteSeries.setNotify( true );
     }
 
 }
