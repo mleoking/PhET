230e01e0f61623c72417ec1fa004d0a9f6beaa47
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-05-31 21:48:31 +0000

    Natural selection: added time until next generation progress bar

---
+++ trunk/simulations-java/simulations/natural-selection/src/edu/colorado/phet/naturalselection/control/GenerationProgressPanel.java
@@ -0,0 +1,38 @@
+package edu.colorado.phet.naturalselection.control;
+
+import java.awt.*;
+
+import javax.swing.*;
+
+import edu.colorado.phet.common.phetcommon.model.clock.ClockAdapter;
+import edu.colorado.phet.common.phetcommon.model.clock.ClockEvent;
+import edu.colorado.phet.naturalselection.module.naturalselection.NaturalSelectionModel;
+
+public class GenerationProgressPanel extends JPanel {
+
+    private JProgressBar generationProgressBar;
+
+    public GenerationProgressPanel( final NaturalSelectionModel model ) {
+        super( new GridLayout( 2, 1 ) );
+
+        setOpaque( false );
+
+        add( new JLabel( "Time until next generation" ) );
+
+        generationProgressBar = new JProgressBar( SwingConstants.HORIZONTAL );
+        generationProgressBar.setValue( 0 );
+
+        add( generationProgressBar );
+
+        //generationProgressBar.setPreferredSize( new Dimension( (int) showGenerationChartButton.getPreferredSize().getWidth(), (int) generationProgressBar.getPreferredSize().getHeight() ) );
+        //generationProgressBar.setMaximumSize( generationProgressBar.getPreferredSize() );
+
+        model.getClock().addClockListener( new ClockAdapter() {
+            @Override
+            public void simulationTimeChanged( ClockEvent event ) {
+                generationProgressBar.setValue( model.getGenerationProgressPercent() );
+            }
+        } );
+    }
+
+}
