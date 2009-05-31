5f575a306c660acee2a0e7a49519641a210a2425
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-05-31 22:36:24 +0000

    Natural selection: added game over handling

---
+++ trunk/simulations-java/simulations/natural-selection/src/edu/colorado/phet/naturalselection/dialog/GameOverDialog.java
@@ -0,0 +1,43 @@
+package edu.colorado.phet.naturalselection.dialog;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+import edu.colorado.phet.common.phetcommon.view.VerticalLayoutPanel;
+import edu.colorado.phet.naturalselection.module.naturalselection.NaturalSelectionModule;
+
+public class GameOverDialog extends JDialog {
+
+    public GameOverDialog( Frame frame, final NaturalSelectionModule module ) {
+        super( frame );
+
+        setTitle( "Game Over" );
+
+        VerticalLayoutPanel panel = new VerticalLayoutPanel();
+
+        panel.setInsets( new Insets( 10, 10, 0, 10 ) );
+
+        JLabel label = new JLabel( "All of the bunnies died!" );
+        panel.add( label );
+
+        panel.setInsets( new Insets( 10, 10, 10, 10 ) );
+
+        JButton playAgainButton = new JButton( "Play Again" );
+        panel.add( playAgainButton );
+
+        playAgainButton.addActionListener( new ActionListener() {
+            public void actionPerformed( ActionEvent actionEvent ) {
+                module.reset();
+                GameOverDialog.this.dispose();
+            }
+        } );
+
+        setContentPane( panel );
+
+        pack();
+    }
+
+}
