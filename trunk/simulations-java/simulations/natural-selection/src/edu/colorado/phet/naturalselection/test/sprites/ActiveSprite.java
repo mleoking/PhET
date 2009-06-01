b44374175e3ec9973453a50df264e1269af89e93
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-06-01 09:22:45 +0000

    Natural selection: depth test performance in piccolo

---
+++ trunk/simulations-java/simulations/natural-selection/src/edu/colorado/phet/naturalselection/test/sprites/ActiveSprite.java
@@ -0,0 +1,58 @@
+package edu.colorado.phet.naturalselection.test.sprites;
+
+import java.awt.*;
+
+import edu.colorado.phet.common.phetcommon.math.Point3D;
+import edu.colorado.phet.common.phetcommon.view.util.DoubleGeneralPath;
+import edu.umd.cs.piccolo.nodes.PPath;
+
+public class ActiveSprite extends Sprite {
+
+    private PPath graphics;
+
+    public ActiveSprite( Landscape landscape, Point3D position ) {
+        super( landscape, position );
+
+        graphics = new PPath();
+
+        graphics.setStroke( new BasicStroke( 1 ) );
+        graphics.setPaint( new Color( (int) ( Math.random() * 256 ), (int) ( Math.random() * 256 ), (int) ( Math.random() * 256 ) ) );
+        graphics.setStrokePaint( Color.WHITE );
+
+        final double M = 10.0;
+
+        DoubleGeneralPath path = new DoubleGeneralPath();
+        path.moveTo( -M, 0 );
+        path.lineTo( M, 0 );
+        path.lineTo( M, -2 * M );
+        path.lineTo( -M, -2 * M );
+        path.lineTo( -M, 0 );
+
+        graphics.setPathTo( path.getGeneralPath() );
+
+        addChild( graphics );
+
+        graphics.setScale( 200 / position.getZ() );
+    }
+
+    public void perturb() {
+        Point3D cur = getPosition();
+        double x = cur.getX() + Math.random() * 4 - 2;
+        double z = cur.getZ() + Math.random() * 4 - 2;
+        if ( z > Landscape.LANDSCAPE_FARPLANE ) {
+            z = Landscape.LANDSCAPE_FARPLANE;
+        }
+        if ( z < Landscape.LANDSCAPE_NEARPLANE ) {
+            z = Landscape.LANDSCAPE_NEARPLANE;
+        }
+        double mx = landscape.getMaximumX( z );
+        if ( x > mx ) {
+            x = mx;
+        }
+        if ( x < -mx ) {
+            x = -mx;
+        }
+        double y = landscape.getGroundY( x, z );
+        setPosition( new Point3D.Double( x, y, z ) );
+    }
+}
--- trunk/simulations-java/simulations/natural-selection/src/edu/colorado/phet/naturalselection/test/sprites/SpriteCanvas.java
+++ trunk/simulations-java/simulations/natural-selection/src/edu/colorado/phet/naturalselection/test/sprites/SpriteCanvas.java
@@ -1,15 +1,42 @@
 package edu.colorado.phet.naturalselection.test.sprites;
 
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+
 import edu.colorado.phet.common.piccolophet.PhetPCanvas;
+import edu.umd.cs.piccolo.PNode;
 
 public class SpriteCanvas extends PhetPCanvas {
 
-    public SpriteCanvas() {
+    public static final Dimension BASE = new Dimension( 640, 480 );
+    private Background back;
+    private Landscape landscape;
 
+    public SpriteCanvas() {
+        super( BASE );
+
+        setWorldTransformStrategy( new ConstantTransformStrategy( new AffineTransform() ) );
+
+        PNode root = new PNode();
+
+        addWorldChild( root );
+
+        back = new Background( 640, 480 );
+        root.addChild( back );
+
+        landscape = new Landscape();
+        root.addChild( landscape );
+
+
+    }
+
+    public Landscape getLandscape() {
+        return landscape;
     }
 
     @Override
     protected void updateLayout() {
-
+        back.updateLayout( getWidth(), getHeight() );
+        landscape.updateLayout( getWidth(), getHeight() );
     }
 }
