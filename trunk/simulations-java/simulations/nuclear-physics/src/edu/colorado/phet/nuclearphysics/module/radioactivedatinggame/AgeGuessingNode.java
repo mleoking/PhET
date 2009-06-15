774670a78fdbdbac1a4af024e02560879a3f8650
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-06-15 18:53:36 +0000

    Added the button for checking the age to the AgeGuessingNode.

--- trunk/simulations-java/simulations/nuclear-physics/src/edu/colorado/phet/nuclearphysics/module/radioactivedatinggame/AgeGuessingNode.java
+++ trunk/simulations-java/simulations/nuclear-physics/src/edu/colorado/phet/nuclearphysics/module/radioactivedatinggame/AgeGuessingNode.java
@@ -1,10 +1,17 @@
 package edu.colorado.phet.nuclearphysics.module.radioactivedatinggame;
 
-import java.awt.Color;
-import java.awt.geom.Rectangle2D;
+import java.awt.Font;
 
-import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import edu.colorado.phet.common.phetcommon.view.VerticalLayoutPanel;
+import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
+import edu.colorado.phet.nuclearphysics.NuclearPhysicsStrings;
 import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolox.pswing.PSwing;
 
 /**
  * This class presents a dialog to the user and allows them to guess the age
@@ -14,13 +21,42 @@ import edu.umd.cs.piccolo.PNode;
  */
 public class AgeGuessingNode extends PNode {
 	
-	private static double WIDTH = 100;
-	private static double HEIGHT = WIDTH / 2;
+	private static final double WIDTH = 100;
+	private static final double HEIGHT = WIDTH / 2;
+	private static final Font TEXT_FONT = new PhetFont(18);
 	
-	public PhetPPath _boundingRect = new PhetPPath(Color.PINK);
 
+	/**
+	 * Constructor.
+	 */
 	public AgeGuessingNode() {
-		_boundingRect.setPathTo(new Rectangle2D.Double(0, 0, WIDTH, HEIGHT));
-		addChild(_boundingRect);
+		
+		// Create the panel that will contain all the components.
+		JPanel ageGuessingNodePanel = new VerticalLayoutPanel();
+		
+		// Create the sub-panel that will contain the text field for entering
+		// the age and the units label.
+		JPanel ageEntryPanel = new JPanel();
+		JTextField ageEntryField = new JTextField(15);
+		ageEntryField.setFont(TEXT_FONT);
+		JLabel textEntryFieldLabel = new JLabel(NuclearPhysicsStrings.READOUT_UNITS_YRS);
+		textEntryFieldLabel.setFont(TEXT_FONT);
+		ageEntryPanel.add(ageEntryField);
+		ageEntryPanel.add(textEntryFieldLabel);
+		
+		// Create the sub-panel that contains the button. 
+		JPanel checkAgeButtonPanel = new JPanel();
+		JButton checkAgeButton = new JButton(NuclearPhysicsStrings.CHECK_AGE);
+		checkAgeButton.setFont(TEXT_FONT);
+		checkAgeButtonPanel.add(checkAgeButton);
+		
+		// Add the sub-panels to the overall panel.
+		ageGuessingNodePanel.add(ageEntryPanel);
+		ageGuessingNodePanel.add(checkAgeButtonPanel);
+		
+		// Wrap the whole thing in a PSwing and add it to the node.
+		PSwing ageGuessingNodePanelPSwing = new PSwing(ageGuessingNodePanel);
+		addChild(ageGuessingNodePanelPSwing);
+		
 	}
 }
