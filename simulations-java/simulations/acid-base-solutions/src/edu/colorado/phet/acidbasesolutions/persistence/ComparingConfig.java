6968384b2c5e80d3f987d491b53f606d7d7209ac
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-06-15 19:28:43 +0000

    save/load for Comparing module

--- simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/persistence/ComparingConfig.java
+++ simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/persistence/ComparingConfig.java
@@ -14,7 +14,29 @@ public class ComparingConfig extends AbstractModuleConfig {
     // Instance data
     //----------------------------------------------------------------------------
     
-    //XXX other stuff
+    // left solution
+    private String soluteNameLeft;
+    private double concentrationLeft;
+    private double strengthLeft;
+    
+    // right solution
+    private String soluteNameRight;
+    private double concentrationRight;
+    private double strengthRight;
+    
+    // view controls
+    private boolean beakersSelected;
+    private boolean graphsSelected;
+    private boolean equationsSelected;
+    
+    // beaker view controls
+    private boolean disassociatedComponentsRatioVisible;
+    private boolean hydroniumHydroxideRatioVisible;
+    private boolean moleculeCountsVisible;
+    private boolean beakerLabelVisible;
+    
+    // equations controls
+    private boolean equationsScalingEnabled;
     
     //----------------------------------------------------------------------------
     // Constructors
@@ -24,12 +46,148 @@ public class ComparingConfig extends AbstractModuleConfig {
      * Zero-argument constructor for Java Bean compliance.
      */
     public ComparingConfig() {}
-    
+
     //----------------------------------------------------------------------------
     // Setters and getters
     //----------------------------------------------------------------------------
     
-    //----------------------------------------------------------------------------
-    // Convenience methods
-    //----------------------------------------------------------------------------
+    public String getSoluteNameLeft() {
+        return soluteNameLeft;
+    }
+
+    
+    public void setSoluteNameLeft( String soluteNameLeft ) {
+        this.soluteNameLeft = soluteNameLeft;
+    }
+
+    
+    public double getConcentrationLeft() {
+        return concentrationLeft;
+    }
+
+    
+    public void setConcentrationLeft( double concentrationLeft ) {
+        this.concentrationLeft = concentrationLeft;
+    }
+
+    
+    public double getStrengthLeft() {
+        return strengthLeft;
+    }
+
+    
+    public void setStrengthLeft( double strengthLeft ) {
+        this.strengthLeft = strengthLeft;
+    }
+
+    
+    public String getSoluteNameRight() {
+        return soluteNameRight;
+    }
+
+    
+    public void setSoluteNameRight( String soluteNameRight ) {
+        this.soluteNameRight = soluteNameRight;
+    }
+
+    
+    public double getConcentrationRight() {
+        return concentrationRight;
+    }
+
+    
+    public void setConcentrationRight( double concentrationRight ) {
+        this.concentrationRight = concentrationRight;
+    }
+
+    
+    public double getStrengthRight() {
+        return strengthRight;
+    }
+
+    
+    public void setStrengthRight( double strengthRight ) {
+        this.strengthRight = strengthRight;
+    }
+
+    
+    public boolean isBeakersSelected() {
+        return beakersSelected;
+    }
+
+    
+    public void setBeakersSelected( boolean beakersSelected ) {
+        this.beakersSelected = beakersSelected;
+    }
+
+    
+    public boolean isGraphsSelected() {
+        return graphsSelected;
+    }
+
+    
+    public void setGraphsSelected( boolean graphsSelected ) {
+        this.graphsSelected = graphsSelected;
+    }
+
+    
+    public boolean isEquationsSelected() {
+        return equationsSelected;
+    }
+
+    
+    public void setEquationsSelected( boolean equationsSelected ) {
+        this.equationsSelected = equationsSelected;
+    }
+
+    
+    public boolean isDisassociatedComponentsRatioVisible() {
+        return disassociatedComponentsRatioVisible;
+    }
+
+    
+    public void setDisassociatedComponentsRatioVisible( boolean disassociatedComponentsRatioVisible ) {
+        this.disassociatedComponentsRatioVisible = disassociatedComponentsRatioVisible;
+    }
+
+    
+    public boolean isHydroniumHydroxideRatioVisible() {
+        return hydroniumHydroxideRatioVisible;
+    }
+
+    
+    public void setHydroniumHydroxideRatioVisible( boolean hydroniumHydroxideRatioVisible ) {
+        this.hydroniumHydroxideRatioVisible = hydroniumHydroxideRatioVisible;
+    }
+
+    
+    public boolean isMoleculeCountsVisible() {
+        return moleculeCountsVisible;
+    }
+
+    
+    public void setMoleculeCountsVisible( boolean moleculeCountsVisible ) {
+        this.moleculeCountsVisible = moleculeCountsVisible;
+    }
+
+    
+    public boolean isBeakerLabelVisible() {
+        return beakerLabelVisible;
+    }
+
+    
+    public void setBeakerLabelVisible( boolean beakerLabelVisible ) {
+        this.beakerLabelVisible = beakerLabelVisible;
+    }
+
+    
+    public boolean isEquationsScalingEnabled() {
+        return equationsScalingEnabled;
+    }
+
+    
+    public void setEquationsScalingEnabled( boolean equationsSalingEnabled ) {
+        this.equationsScalingEnabled = equationsSalingEnabled;
+    }
+
 }
