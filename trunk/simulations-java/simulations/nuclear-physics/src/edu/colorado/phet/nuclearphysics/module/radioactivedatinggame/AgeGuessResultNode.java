8f356db00718859c1bce18983528b04bc8b4131b
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-06-15 21:21:04 +0000

    Added icons to the nodes that indicate correctness of the age guess.

--- trunk/simulations-java/simulations/nuclear-physics/src/edu/colorado/phet/nuclearphysics/module/radioactivedatinggame/AgeGuessResultNode.java
+++ trunk/simulations-java/simulations/nuclear-physics/src/edu/colorado/phet/nuclearphysics/module/radioactivedatinggame/AgeGuessResultNode.java
@@ -6,20 +6,23 @@ import java.awt.geom.Rectangle2D;
 import java.awt.geom.Line2D.Double;
 
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
+import edu.colorado.phet.nuclearphysics.NuclearPhysicsResources;
 import edu.colorado.phet.nuclearphysics.NuclearPhysicsStrings;
 import edu.colorado.phet.nuclearphysics.module.alphadecay.multinucleus.MultiNucleusDecayModel;
 import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.nodes.PImage;
 import edu.umd.cs.piccolo.nodes.PPath;
 import edu.umd.cs.piccolo.nodes.PText;
 
 public class AgeGuessResultNode extends PNode {
 
-	private static final Font TEXT_FONT = new PhetFont(18);
+	private static final Font TEXT_FONT = new PhetFont(20);
 	private static final Color FILL_COLOR_BAD_GUESS = Color.RED;
 	private static final Color FILL_COLOR_GOOD_GUESS = Color.GREEN;
 	
 	private final boolean _guessIsGood;
 	private final PPath _backgoundRect;
+	private final PImage _icon;
 
 	/**
 	 * Contructor.
@@ -39,7 +42,7 @@ public class AgeGuessResultNode extends PNode {
 		
 		// Create the bounding rectangle so that it encloses the text.
 		_backgoundRect = new PPath( new Rectangle2D.Double( 0, 0, ageText.getFullBoundsReference().width * 1.2,
-				ageText.getFullBoundsReference().height * 1.5 ) );
+				ageText.getFullBoundsReference().height * 1.3 ) );
 		_backgoundRect.setPaint(_guessIsGood ? FILL_COLOR_GOOD_GUESS : FILL_COLOR_BAD_GUESS);
 		addChild(_backgoundRect);
 		
@@ -49,6 +52,18 @@ public class AgeGuessResultNode extends PNode {
 		ageText.setOffset(
 				_backgoundRect.getFullBoundsReference().width / 2 - ageText.getFullBoundsReference().width / 2,
 				_backgoundRect.getFullBoundsReference().height / 2 - ageText.getFullBoundsReference().height / 2 );
+		
+		// Add the icon that graphically indicates whether the guess is good or bad.
+		if (_guessIsGood){
+			_icon = NuclearPhysicsResources.getImageNode("gold_star.png");
+		}
+		else {
+			_icon = NuclearPhysicsResources.getImageNode("red_x.png");
+		}
+		
+		_icon.setScale( _backgoundRect.getFullBoundsReference().height / _icon.getFullBoundsReference().height );
+		_icon.setOffset(_backgoundRect.getFullBoundsReference().getMaxX() + 3, 0);
+		addChild(_icon);
 	}
 
 	public boolean isGuessGood() {
