734b4a02e124b2045836ad642dc543c107266bd8
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-06-25 20:03:20 +0000

    consolidate duplicate code for ratio check boxes

---
+++ trunk/simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/control/RatioCheckBox.java
@@ -0,0 +1,39 @@
+package edu.colorado.phet.acidbasesolutions.control;
+
+import java.awt.Color;
+import java.text.MessageFormat;
+
+import javax.swing.JCheckBox;
+
+import edu.colorado.phet.acidbasesolutions.ABSColors;
+import edu.colorado.phet.acidbasesolutions.ABSStrings;
+import edu.colorado.phet.acidbasesolutions.ABSSymbols;
+import edu.colorado.phet.common.phetcommon.view.util.HTMLUtils;
+
+
+public class RatioCheckBox extends JCheckBox {
+    
+    public RatioCheckBox() {
+        super();
+    }
+    
+    public RatioCheckBox( String symbol1, Color color1, String symbol2, Color color2 ) {
+        super();
+        setComponents( symbol1, color1, symbol2, color2 );
+    }
+    
+    public void setComponents( String symbol1, Color color1, String symbol2, Color color2 ) {
+        String s1 = HTMLUtils.createColoredFragment( symbol1, color1 );
+        String s2 = HTMLUtils.createColoredFragment( symbol2, color2 );
+        Object[] args = { s1, s2 };
+        String html = HTMLUtils.toHTMLString( MessageFormat.format( ABSStrings.CHECK_BOX_RATIO, args ) );
+        setText( html );
+    }
+    
+    public static class HydroniumHydroxideRatioCheckBox extends RatioCheckBox {
+        
+        public HydroniumHydroxideRatioCheckBox() {
+            super( ABSSymbols.H3O_PLUS, ABSColors.H3O_PLUS, ABSSymbols.OH_MINUS, ABSColors.OH_MINUS );
+        }
+    }
+}
