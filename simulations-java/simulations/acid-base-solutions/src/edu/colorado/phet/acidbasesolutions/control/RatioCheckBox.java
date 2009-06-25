3c778f47c78258f34c9b04da36e287ac6846a3bf
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-06-25 21:19:46 +0000

    javadoc, default color support

--- simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/control/RatioCheckBox.java
+++ simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/control/RatioCheckBox.java
@@ -10,16 +10,31 @@ import edu.colorado.phet.acidbasesolutions.ABSStrings;
 import edu.colorado.phet.acidbasesolutions.ABSSymbols;
 import edu.colorado.phet.common.phetcommon.view.util.HTMLUtils;
 
-
+/**
+ * Check box for component ratios.
+ * Components are displayed in specified colors.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
 public class RatioCheckBox extends JCheckBox {
+    
+    private static final Color DEFAULT_COLOR = Color.BLACK;
     
     public RatioCheckBox() {
         super();
     }
     
+    public RatioCheckBox( String symbol1, String symbol2 ) { 
+        this( symbol1, DEFAULT_COLOR, symbol2, DEFAULT_COLOR );
+    }
+    
     public RatioCheckBox( String symbol1, Color color1, String symbol2, Color color2 ) {
         super();
         setComponents( symbol1, color1, symbol2, color2 );
+    }
+    
+    public void setComponent( String symbol1, String symbol2 ) {
+        setComponents( symbol1, DEFAULT_COLOR, symbol2, DEFAULT_COLOR );
     }
     
     public void setComponents( String symbol1, Color color1, String symbol2, Color color2 ) {
@@ -30,6 +45,11 @@ public class RatioCheckBox extends JCheckBox {
         setText( html );
     }
     
+    /**
+     * Check box for H3O/OH ratio.
+     *
+     * @author Chris Malley (cmalley@pixelzoom.com)
+     */
     public static class HydroniumHydroxideRatioCheckBox extends RatioCheckBox {
         
         public HydroniumHydroxideRatioCheckBox() {
