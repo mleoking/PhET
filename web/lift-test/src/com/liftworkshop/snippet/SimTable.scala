9f5281be41041b360b4d472f2af95924a6016c85
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-06-28 22:15:10 +0000

    added thumbnails for simulations and usage of CSS from tomcattest

---
+++ web/lift-test/src/com/liftworkshop/snippet/SimTable.scala
@@ -0,0 +1,33 @@
+package com.liftworkshop.snippet
+
+case class Simulation(title:String,simname:String)
+
+class SimTable{
+  //TODO load simulations from database/disk
+  def simulations =
+    new Simulation("Circuit Construction Kit", "circuit-construction-kit-dc") ::
+            new Simulation("Glaciers", "glaciers") ::
+            new Simulation("Projectile Motion", "projectile-motion") :: Nil
+
+  //Generate HTML For the simulation table
+  def display=
+    <table id="mini_sim_table">
+        <tr>
+        {getEntries}
+        </tr>
+    </table>
+
+  //Generate a sequence of sim entries
+  def getEntries=for (sim<-simulations) yield {
+    println("hello")
+    val thumbnail="data/images/"+sim.simname+"-thumbnail.jpg"
+    <td>
+        <div class="mini_sim_group">
+            <a href="index.html">
+                <img src={thumbnail}/>
+                <div class="mini_sim_title">{sim.title}</div>
+            </a>
+        </div>
+    </td>
+  }
+}
