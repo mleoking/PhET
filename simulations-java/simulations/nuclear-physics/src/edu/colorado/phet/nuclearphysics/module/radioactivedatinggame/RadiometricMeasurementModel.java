b9c844677ec954b0e77579a61ca652639fb662b2
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-06-29 18:07:26 +0000

    Added the ability to set/get the simulation mode.

--- simulations-java/simulations/nuclear-physics/src/edu/colorado/phet/nuclearphysics/module/radioactivedatinggame/RadiometricMeasurementModel.java
+++ simulations-java/simulations/nuclear-physics/src/edu/colorado/phet/nuclearphysics/module/radioactivedatinggame/RadiometricMeasurementModel.java
@@ -4,7 +4,9 @@ package edu.colorado.phet.nuclearphysics.module.radioactivedatinggame;
 
 import java.awt.geom.Point2D;
 
+import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
+import edu.colorado.phet.nuclearphysics.common.NuclearPhysicsClock;
 
 /**
  * This class defines a model (in the model-view-controller paradigm) that
@@ -19,27 +21,33 @@ public class RadiometricMeasurementModel implements ModelContainingDatableItems 
     // Class data
     //------------------------------------------------------------------------
 	
+	// Enumerations that defines the main modes for this model.
+	public enum SIMULATION_MODE { TREE, ROCK };
+	private SIMULATION_MODE DEFAULT_MODE = SIMULATION_MODE.TREE;
+	
     //------------------------------------------------------------------------
     // Instance data
     //------------------------------------------------------------------------
 
 	private DatableItem _datableItem;
 	private RadiometricDatingMeter _meter;
+	private SIMULATION_MODE _simulationMode = DEFAULT_MODE;
+	private ConstantDtClock _clock;
 
     //------------------------------------------------------------------------
     // Constructor
     //------------------------------------------------------------------------
     
-    public RadiometricMeasurementModel()
+    public RadiometricMeasurementModel(NuclearPhysicsClock clock)
     {
-
+    	_clock = clock;
+    	
     	// Add the meter and register for user-initiated movements.
     	_meter = new RadiometricDatingMeter( this, new Point2D.Double(-5, 3) );
     	
     	_meter.getProbeModel().addObserver(new SimpleObserver(){
 			public void update() {
 				getDatableItemAtLocation( _meter.getProbeModel().getTipLocation() );
-				
 			}
     	});
     }
@@ -62,11 +70,32 @@ public class RadiometricMeasurementModel implements ModelContainingDatableItems 
     	return 0;
     }
     
+    public SIMULATION_MODE getSimulationMode() {
+		return _simulationMode;
+	}
+
+    /**
+     * Set the mode (e.g. "TREE") that defines what the model will simulate.
+     * 
+     * @param mode
+     */
+	public void setSimulationMode(SIMULATION_MODE mode) {
+		_simulationMode = mode;
+	}
+	
+	public ConstantDtClock getClock() {
+		return _clock;
+	}
+	
+	public void setClock(ConstantDtClock clock) {
+		_clock = clock;
+	}
+	
     //------------------------------------------------------------------------
     // Other Methods
     //------------------------------------------------------------------------
 
-    /**
+	/**
      * Get the datable item at the specified model location, or null if there
      * isn't anything there.
      */
