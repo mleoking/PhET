72880ae7bcdee6423112725965c3344e81c78093
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-06-30 21:02:15 +0000

    Added the ability to listen to the clock and "age" as simulation time goes by.

--- trunk/simulations-java/simulations/nuclear-physics/src/edu/colorado/phet/nuclearphysics/module/radioactivedatinggame/AgingTree.java
+++ trunk/simulations-java/simulations/nuclear-physics/src/edu/colorado/phet/nuclearphysics/module/radioactivedatinggame/AgingTree.java
@@ -4,8 +4,11 @@ package edu.colorado.phet.nuclearphysics.module.radioactivedatinggame;
 
 import java.awt.geom.Point2D;
 
+import edu.colorado.phet.common.phetcommon.model.clock.ClockAdapter;
+import edu.colorado.phet.common.phetcommon.model.clock.ClockEvent;
 import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
 import edu.colorado.phet.nuclearphysics.common.Cleanupable;
+import edu.colorado.phet.nuclearphysics.module.alphadecay.multinucleus.MultiNucleusDecayModel;
 
 /**
  * This class implements the behavior of a model element that represents a
@@ -24,11 +27,18 @@ public class AgingTree extends DatableItem implements Cleanupable {
 	private static final String LIVING_TREE_IMAGE_NAME = "tree_1.png";
 	private static final String DEAD_TREE_IMAGE_NAME = "dead_tree.png";
 	
+	// Age adjustment factor - used to convert the amount of simulation time
+	// into the age of the item so that users don't have to wait around for
+	// thousands of years for anything to happen.
+	private static final double AGE_ADJUSTMENT_FACTOR = MultiNucleusDecayModel.convertYearsToMs(1000) / 5000;
+	
     //------------------------------------------------------------------------
     // Instance Data
     //------------------------------------------------------------------------
 	
 	private final ConstantDtClock _clock;
+	private final ClockAdapter _clockAdapter;
+	private double age = 0; // Age in milliseconds of this datable item.
 	
     //------------------------------------------------------------------------
     // Constructor
@@ -37,6 +47,17 @@ public class AgingTree extends DatableItem implements Cleanupable {
 	public AgingTree(ConstantDtClock clock, Point2D center, double width) {
 		super(NAME, LIVING_TREE_IMAGE_NAME, center, width, 0, 0);
 		_clock = clock;
+		
+		// Create the adapter that will listen to the clock.
+		_clockAdapter = new ClockAdapter(){
+		    public void clockTicked( ClockEvent clockEvent ) {
+		    	handleClockTicked();
+		    }
+		    public void simulationTimeReset( ClockEvent clockEvent ) {
+		    	handleSimulationTimeReset();
+		    }
+		};
+		_clock.addClockListener(_clockAdapter);
 	}
 
     //------------------------------------------------------------------------
@@ -44,6 +65,19 @@ public class AgingTree extends DatableItem implements Cleanupable {
     //------------------------------------------------------------------------
 
 	public void cleanup() {
-		// TODO Auto-generated method stub
+		_clock.removeClockListener(_clockAdapter);
+	}
+	
+	private void handleClockTicked(){
+		age = _clock.getSimulationTime() * AGE_ADJUSTMENT_FACTOR;
+	}
+
+	private void handleSimulationTimeReset(){
+		age = 0;
+	}
+
+	@Override
+	public double getAge() {
+		return age;
 	}
 }
