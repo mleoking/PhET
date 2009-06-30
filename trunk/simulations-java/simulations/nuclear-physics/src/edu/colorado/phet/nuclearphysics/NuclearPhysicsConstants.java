b54c5a8aad532b5d8cac1428739403bcb802f426
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-06-30 22:31:24 +0000

    Adjusted color of bottom layer of strata based on feedback.

--- trunk/simulations-java/simulations/nuclear-physics/src/edu/colorado/phet/nuclearphysics/NuclearPhysicsConstants.java
+++ trunk/simulations-java/simulations/nuclear-physics/src/edu/colorado/phet/nuclearphysics/NuclearPhysicsConstants.java
@@ -149,7 +149,7 @@ public class NuclearPhysicsConstants {
 		strataColors.add( new Color( 216, 175, 208 ) );
 		strataColors.add( new Color( 198, 218, 119 ) );
 		strataColors.add( new Color( 179, 179, 179 ) );
-		strataColors.add( new Color( 150, 22, 22 ) );
+		strataColors.add( Color.DARK_GRAY );
 	}
 
     //----------------------------------------------------------------------------
