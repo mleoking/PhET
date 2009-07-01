025405c45bf49132b45934641ea4cb37b50b329a
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-07-01 19:46:17 +0000

    improve tests for text rendering order

--- simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/tests/TestTextRenderOrder.java
+++ simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/tests/TestTextRenderOrder.java
@@ -1,11 +1,10 @@
 package edu.colorado.phet.common.phetcommon.tests;
 
-import java.awt.*;
-import java.text.MessageFormat;
+import edu.colorado.phet.common.phetcommon.view.VerticalLayoutPanel;
 
 import javax.swing.*;
-
-import edu.colorado.phet.common.phetcommon.view.VerticalLayoutPanel;
+import java.awt.*;
+import java.text.MessageFormat;
 
 /**
  * Created by IntelliJ IDEA.
@@ -14,39 +13,43 @@ import edu.colorado.phet.common.phetcommon.view.VerticalLayoutPanel;
  * Time: 10:42:04 AM
  */
 public class TestTextRenderOrder {
-    public static void main( String[] args ) {
-        /*
+    public static void main(String[] args) {
         JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         VerticalLayoutPanel verticalLayoutPanel = new VerticalLayoutPanel();
-        frame.setContentPane( verticalLayoutPanel );
-        verticalLayoutPanel.add( new JLabel( "\u062D" ) );
-        verticalLayoutPanel.add( new JLabel( "\u0635" ) );
-        verticalLayoutPanel.add( new JLabel( "\u062D \u0635" ) );
-        verticalLayoutPanel.add( new JLabel( "\u062Dhello\u0635" ) );
-        verticalLayoutPanel.add( new JLabel( "\u062D hello \u0635" ) );
-        verticalLayoutPanel.add(new JLabel(MessageFormat.format( "testing {0} and {1}","value.0","value.1" )));
-        verticalLayoutPanel.add(new JLabel(MessageFormat.format( "testing {0} and {1}","\u062D","\u0635" )));
-        verticalLayoutPanel.add(new JLabel(MessageFormat.format( "{0}{1}","\u062D","\u0635" )));
-        verticalLayoutPanel.add(new JLabel(MessageFormat.format( "{0} {1}","\u062D","\u0635" )));
-        verticalLayoutPanel.add(new JLabel(MessageFormat.format( "{0} hello {1}","\u062D","\u0635" )));
+        frame.setContentPane(verticalLayoutPanel);
+
+        testJLabels(verticalLayoutPanel,"","");
+        verticalLayoutPanel.add(new JLabel("Separator"));
+        testJLabels(verticalLayoutPanel,"<html>","</html>");
         JPanel jPanel = new JPanel() {
-            //            @Override
-            protected void paintComponent( Graphics g ) {
-                super.paintComponent( g );
-                Graphics2D g2= (Graphics2D) g;
-                g2.drawString( "\u062D",50,50 );
-                g2.drawString( "\u0635",50,100);
-                g2.drawString( "\u062D\u0635",50,150);
-                g2.drawString( "\u062Dhello\u0635",50,200);
-                g2.drawString( "\u062D hello \u0635",50,250);
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                Graphics2D g2 = (Graphics2D) g;
+                g2.drawString("\u062D", 50, 50);
+                g2.drawString("\u0635", 50, 100);
+                g2.drawString("\u062D\u0635", 50, 150);
+                g2.drawString("\u062Dhello\u0635", 50, 200);
+                g2.drawString("\u062D hello \u0635", 50, 250);
             }
         };
-        jPanel.setPreferredSize( new Dimension( 400, 400 ) );
+        jPanel.setPreferredSize(new Dimension(400, 400));
         verticalLayoutPanel.add(jPanel);
 
         frame.pack();
-        frame.setVisible( true );
-        */
+        frame.setVisible(true);
+    }
+
+    private static void testJLabels(VerticalLayoutPanel verticalLayoutPanel,String prefix,String suffix) {
+        verticalLayoutPanel.add(new JLabel(prefix+"\u062D"+suffix));
+        verticalLayoutPanel.add(new JLabel(prefix+"\u0635"+suffix));
+        verticalLayoutPanel.add(new JLabel(prefix+"\u062D \u0635"+suffix));
+        verticalLayoutPanel.add(new JLabel(prefix+"\u062Dhello\u0635"+suffix));
+        verticalLayoutPanel.add(new JLabel(prefix+"\u062D hello \u0635"+suffix));
+        verticalLayoutPanel.add(new JLabel(prefix+MessageFormat.format("testing {0} and {1}", "value.0", "value.1")+suffix));
+        verticalLayoutPanel.add(new JLabel(prefix+MessageFormat.format("testing {0} and {1}", "\u062D", "\u0635")+suffix));
+        verticalLayoutPanel.add(new JLabel(prefix+MessageFormat.format("{0}{1}", "\u062D", "\u0635")+suffix));
+        verticalLayoutPanel.add(new JLabel(prefix+MessageFormat.format("{0} {1}", "\u062D", "\u0635")+suffix));
+        verticalLayoutPanel.add(new JLabel(prefix+MessageFormat.format("{0} hello {1}", "\u062D", "\u0635")+suffix));
     }
 }
