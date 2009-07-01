1494db74ddd5f06b3c3791702163e015bae7590a
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-07-01 21:41:47 +0000

    Added stubbed routines for handling all motion/animation events from the model, filled in the one for handling size changes.

--- trunk/simulations-java/simulations/nuclear-physics/src/edu/colorado/phet/nuclearphysics/module/radioactivedatinggame/DatableItemNode.java
+++ trunk/simulations-java/simulations/nuclear-physics/src/edu/colorado/phet/nuclearphysics/module/radioactivedatinggame/DatableItemNode.java
@@ -12,7 +12,9 @@ import edu.umd.cs.piccolo.nodes.PText;
 
 /**
  * A node that represents an item that can be dated through radiometric
- * method, i.e. and item that is "datable".
+ * method, i.e. and item that is "datable".  Note that this items can move
+ * around, rotate, and change their appearance in other ways, so this node
+ * must be able to handle such occurrences.
  * 
  * @author John Blanco
  */
@@ -25,9 +27,38 @@ public class DatableItemNode extends PNode {
 	// For debugging of placement, turns on a name so users can tell what's what.
 	private final boolean SHOW_NAME = false;
 
+	/**
+	 * Constructor
+	 * 
+	 * @param datableItem
+	 * @param mvt
+	 */
 	public DatableItemNode(DatableItem datableItem, ModelViewTransform2D mvt) {
+		
 		this.datableItem = datableItem;
 		this.mvt = mvt;
+		
+		// Register with the datable items for changes in its position, image, etc.
+		datableItem.addAnimationListener(new ModelAnimationListener(){
+
+			public void imageChanged() {
+				// TODO Auto-generated method stub
+			}
+
+			public void positionChanged() {
+				// TODO Auto-generated method stub
+			}
+
+			public void rotationalAngleChanged() {
+				// TODO Auto-generated method stub
+			}
+
+			public void sizeChanged() {
+				handleSizeChanged();
+			}
+		});
+		
+		// Load up the initial image.
 		image = new PImage( datableItem.getImage() );
 		Point2D desiredSize = mvt.modelToViewDifferentialDouble(datableItem.getWidth(), datableItem.getHeight());
 		image.scale(desiredSize.getX() / image.getFullBoundsReference().getWidth());
@@ -44,5 +75,13 @@ public class DatableItemNode extends PNode {
 		Point2D centerCanvasPosition = mvt.modelToViewDouble(datableItem.getPosition());
 		image.setOffset(centerCanvasPosition.getX() - ( image.getFullBoundsReference().width / 2 ),
 				centerCanvasPosition.getY() - ( image.getFullBoundsReference().height / 2 ) );
+	}
+	
+	public void handleSizeChanged(){
+		Point2D desiredSize = mvt.modelToViewDifferentialDouble(datableItem.getWidth(), datableItem.getHeight());
+		// We ignore the height here, since we can't scale in each direction
+		// due to limitations of Piccolo.
+		image.scale(desiredSize.getX() / image.getFullBoundsReference().getWidth());
+		updatePosition();
 	}
 }
