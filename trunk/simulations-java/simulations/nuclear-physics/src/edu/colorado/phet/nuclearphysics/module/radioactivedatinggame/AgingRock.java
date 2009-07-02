e919abf58ead7c77553788efa09d42a9cb2232fb
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-07-02 22:01:26 +0000

    factor out superclass AnimatedDatableItem from Volcano and Rock

--- trunk/simulations-java/simulations/nuclear-physics/src/edu/colorado/phet/nuclearphysics/module/radioactivedatinggame/AgingRock.java
+++ trunk/simulations-java/simulations/nuclear-physics/src/edu/colorado/phet/nuclearphysics/module/radioactivedatinggame/AgingRock.java
@@ -4,13 +4,9 @@ package edu.colorado.phet.nuclearphysics.module.radioactivedatinggame;
 
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
+import java.util.Arrays;
 
-import javax.swing.JFrame;
-
-import edu.colorado.phet.common.phetcommon.model.clock.ClockAdapter;
-import edu.colorado.phet.common.phetcommon.model.clock.ClockEvent;
 import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
-import edu.colorado.phet.common.piccolophet.PhetPCanvas;
 import edu.colorado.phet.nuclearphysics.common.Cleanupable;
 import edu.colorado.phet.nuclearphysics.module.alphadecay.multinucleus.MultiNucleusDecayModel;
 
@@ -21,114 +17,55 @@ import edu.colorado.phet.nuclearphysics.module.alphadecay.multinucleus.MultiNucl
  * 
  * @author John Blanco
  */
-public class AgingRock extends DatableItem implements Cleanupable {
+public class AgingRock extends AnimatedDatableItem implements Cleanupable {
 
-    //------------------------------------------------------------------------
-    // Class Data
-    //------------------------------------------------------------------------
-	
-	private static final String NAME = "Aging Rock"; // For debugging, no need to translate. 
-	private static final String HOT_ROCK_IMAGE_NAME = "rock_1.png";
-	private static final String COOL_ROCK_IMAGE_NAME = "rock_1.png";
-	
-	// Age adjustment factor - used to convert the amount of simulation time
-	// into the age of the item so that users don't have to wait around for
-	// thousands of years for anything to happen.
-	private static final double AGE_ADJUSTMENT_FACTOR = MultiNucleusDecayModel.convertYearsToMs(1E9) / 5000;
-	
-	// Animation sequence.
-	private static ArrayList<ModelAnimationDelta> ANIMATION_SEQUENCE = new ArrayList<ModelAnimationDelta>();
-	
-    //------------------------------------------------------------------------
-    // Instance Data
-    //------------------------------------------------------------------------
-	
-	private final ConstantDtClock _clock;
-	private final ModelAnimationDeltaInterpreter animationIterpreter;
-	private final ClockAdapter _clockAdapter;
-	private double age = 0; // Age in milliseconds of this datable item.
-	
     //------------------------------------------------------------------------
     // Constructor
     //------------------------------------------------------------------------
 	
 	public AgingRock(ConstantDtClock clock, Point2D center, double width) {
-		super(NAME, HOT_ROCK_IMAGE_NAME, center, width, 0, 0);
-		_clock = clock;
-		
-		// Create the adapter that will listen to the clock.
-		_clockAdapter = new ClockAdapter(){
-		    public void clockTicked( ClockEvent clockEvent ) {
-		    	handleClockTicked();
-		    }
-		    public void simulationTimeReset( ClockEvent clockEvent ) {
-		    	handleSimulationTimeReset();
-		    }
-		};
-		_clock.addClockListener(_clockAdapter);
-		
-		// Create the animation interpreter that will execute the animation.
-		animationIterpreter = new ModelAnimationDeltaInterpreter(this, ANIMATION_SEQUENCE);
+		super("Aging Rock", Arrays.asList( "rock_1.png" ), center, width, 0, 0,clock,EruptingVolcano.VOLCANO_AGE_FACTOR );
+        System.out.println( "clock = " + clock );
 	}
 
     //------------------------------------------------------------------------
-    // Methods
-    //------------------------------------------------------------------------
-
-	public void cleanup() {
-		_clock.removeClockListener(_clockAdapter);
-	}
-	
-	private void handleClockTicked(){
-		age = _clock.getSimulationTime() * AGE_ADJUSTMENT_FACTOR;
-		animationIterpreter.setTime(age);
-	}
-
-	private void handleSimulationTimeReset(){
-		age = 0;
-	}
-
-	@Override
-	public double getAge() {
-		return age;
-	}
-	
-	//------------------------------------------------------------------------
     // The animation sequence that defines how the appearance of the tree
 	// will change as it ages.
     //------------------------------------------------------------------------
-	static{
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(120E6),  new Point2D.Double(-0.35, .6), 0, 1.07, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(130E6),  new Point2D.Double(-0.35, .6), 0, 1.07, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(140E6),  new Point2D.Double(-0.35, .6), 0, 1.07, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(150E6),  new Point2D.Double(-0.35, 0.5), 0, 1.07, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(160E6),  new Point2D.Double(-0.35, 0.5), 0, 1.07, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(170E6),  new Point2D.Double(-0.35, 0.5), 0, 1.07, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(180E6),  new Point2D.Double(-0.35, 0.5), 0, 1.07, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(190E6),  new Point2D.Double(-0.35, 0.5), 0, 1.07, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(200E6),  new Point2D.Double(-0.35, 0.5), 0, 1.07, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(210E6),  new Point2D.Double(-0.35, 0.5), 0, 1.07, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(220E6),  new Point2D.Double(-0.35, 0.5), 0, 1.07, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(230E6),  new Point2D.Double(-0.35, 0.4), 0, 1.07, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(240E6),  new Point2D.Double(-0.35, 0.3), 0, 1.07, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(250E6),  new Point2D.Double(-0.35, 0.2), 0, 1.07, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(260E6),  new Point2D.Double(-0.35, 0.1), 0, 1.07, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(270E6),  new Point2D.Double(-0.35, 0), 0, 1.07, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(280E6),  new Point2D.Double(-0.35, -0.1), 0, 1.07, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(290E6),  new Point2D.Double(-0.35, -0.1), 0, 1.07, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(300E6),  new Point2D.Double(-0.35, -0.4), 0, 1.07, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(310E6),  new Point2D.Double(-0.35, -0.5), 0, 1.07, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(320E6),  new Point2D.Double(-0.35, -0.6), 0, 1.07, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(330E6),  new Point2D.Double(-0.35, -.9), 0, 1.07, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(340E6),  new Point2D.Double(-0.35, -1), 0, 1.07, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(350E6),  new Point2D.Double(-0.35, -1.1), 0, 1.07, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(360E6),  new Point2D.Double(-0.35, -1.2), 0, 1.07, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(370E6),  new Point2D.Double(-0.35, -1.2), 0, 1.07, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(380E6),  new Point2D.Double(-0.35, -1.3), 0, 1.07, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(390E6),  new Point2D.Double(-0.35, -1.3), 0, 1.07, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(300E6),  new Point2D.Double(-0.35, -1.5), 0, 1.07, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(310E6),  new Point2D.Double(-0.35, -1.5), 0, 1.07, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(320E6),  new Point2D.Double(-0.35, -1.5), 0, 1.07, 0, 0, 0));
-		
-	}
+    protected ArrayList<ModelAnimationDelta> getAnimationSequence() {
+        TimeUpdater timeUpdater=new TimeUpdater(MultiNucleusDecayModel.convertYearsToMs( 20*10E6), MultiNucleusDecayModel.convertYearsToMs( 10E6 ));
+        ArrayList<ModelAnimationDelta> ANIMATION_SEQUENCE=new ArrayList<ModelAnimationDelta>( );
+        ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(-0.35, .6), 0, 1.07, 0, 0, 0));
+		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(-0.35, .6), 0, 1.07, 0, 0, 0));
+		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(-0.35, .6), 0, 1.07, 0, 0, 0));
+		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(-0.35, 0.5), 0, 1.07, 0, 0, 0));
+		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(-0.35, 0.5), 0, 1.07, 0, 0, 0));
+		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(-0.35, 0.5), 0, 1.07, 0, 0, 0));
+		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(-0.35, 0.5), 0, 1.07, 0, 0, 0));
+		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(-0.35, 0.5), 0, 1.07, 0, 0, 0));
+		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(-0.35, 0.5), 0, 1.07, 0, 0, 0));
+		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(-0.35, 0.5), 0, 1.07, 0, 0, 0));
+		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(-0.35, 0.5), 0, 1.07, 0, 0, 0));
+		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(-0.35, 0.4), 0, 1.07, 0, 0, 0));
+		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(-0.35, 0.3), 0, 1.07, 0, 0, 0));
+		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(-0.35, 0.2), 0, 1.07, 0, 0, 0));
+		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(-0.35, 0.1), 0, 1.07, 0, 0, 0));
+		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(-0.35, 0), 0, 1.07, 0, 0, 0));
+		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(-0.35, -0.1), 0, 1.07, 0, 0, 0));
+		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(-0.35, -0.1), 0, 1.07, 0, 0, 0));
+		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(-0.35, -0.4), 0, 1.07, 0, 0, 0));
+		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(-0.35, -0.5), 0, 1.07, 0, 0, 0));
+		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(-0.35, -0.6), 0, 1.07, 0, 0, 0));
+		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(-0.35, -.9), 0, 1.07, 0, 0, 0));
+		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(-0.35, -1), 0, 1.07, 0, 0, 0));
+		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(-0.35, -1.1), 0, 1.07, 0, 0, 0));
+		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(-0.35, -1.2), 0, 1.07, 0, 0, 0));
+		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(-0.35, -1.2), 0, 1.07, 0, 0, 0));
+		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(-0.35, -1.3), 0, 1.07, 0, 0, 0));
+		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(-0.35, -1.3), 0, 1.07, 0, 0, 0));
+		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(-0.35, -1.5), 0, 1.07, 0, 0, 0));
+		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(-0.35, -1.5), 0, 1.07, 0, 0, 0));
+		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(-0.35, -1.5), 0, 1.07, 0, 0, 0));
+        return ANIMATION_SEQUENCE;
+    }
 }
--- trunk/simulations-java/simulations/nuclear-physics/src/edu/colorado/phet/nuclearphysics/module/radioactivedatinggame/EruptingVolcano.java
+++ trunk/simulations-java/simulations/nuclear-physics/src/edu/colorado/phet/nuclearphysics/module/radioactivedatinggame/EruptingVolcano.java
@@ -4,9 +4,8 @@ package edu.colorado.phet.nuclearphysics.module.radioactivedatinggame;
 
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
+import java.util.Arrays;
 
-import edu.colorado.phet.common.phetcommon.model.clock.ClockAdapter;
-import edu.colorado.phet.common.phetcommon.model.clock.ClockEvent;
 import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
 import edu.colorado.phet.nuclearphysics.module.alphadecay.multinucleus.MultiNucleusDecayModel;
 
@@ -17,117 +16,50 @@ import edu.colorado.phet.nuclearphysics.module.alphadecay.multinucleus.MultiNucl
  * 
  * @author John Blanco
  */
-public class EruptingVolcano extends DatableItem {
+public class EruptingVolcano extends AnimatedDatableItem {
+    public static final double VOLCANO_AGE_FACTOR = MultiNucleusDecayModel.convertYearsToMs(1E9) / 5000;
 
-    //------------------------------------------------------------------------
-    // Class Data
-    //------------------------------------------------------------------------
-	
-	private static final String NAME = "Volcano"; // For debugging, no need to translate.
-	
-	// Images for the volcano.
-	private static final String HOT_VOLCANO_IMAGE_NAME = "volcano_hot.png";
-	private static final String DORMANT_VOLCANO_IMAGE_NAME = "volcano_cool.png";
-	private static final ArrayList<String> VOLCANO_IMAGES = new ArrayList<String>();
-	
-	static{
-		VOLCANO_IMAGES.add(DORMANT_VOLCANO_IMAGE_NAME);
-		VOLCANO_IMAGES.add(HOT_VOLCANO_IMAGE_NAME);
-	}
-	
-	// Age adjustment factor - used to convert the amount of simulation time
-	// into the age of the item so that users don't have to wait around for
-	// thousands of years for anything to happen.
-	private static final double AGE_ADJUSTMENT_FACTOR = MultiNucleusDecayModel.convertYearsToMs(1E9) / 5000;
-	
-	// Animation sequence.
-	private static ArrayList<ModelAnimationDelta> ANIMATION_SEQUENCE = new ArrayList<ModelAnimationDelta>();
-	
-    //------------------------------------------------------------------------
-    // Instance Data
-    //------------------------------------------------------------------------
-	
-	private final ConstantDtClock _clock;
-	private final ClockAdapter _clockAdapter;
-	private double age = 0; // Age in milliseconds of this datable item.
-	private final ModelAnimationDeltaInterpreter animationIterpreter;
-	
     //------------------------------------------------------------------------
     // Constructor
     //------------------------------------------------------------------------
 	
 	public EruptingVolcano(ConstantDtClock clock, Point2D center, double width) {
-		super(NAME, VOLCANO_IMAGES, center, width, 0, 0);
-		_clock = clock;
-		
-		// Create the adapter that will listen to the clock.
-		_clockAdapter = new ClockAdapter(){
-		    public void clockTicked( ClockEvent clockEvent ) {
-		    	handleClockTicked();
-		    }
-		    public void simulationTimeReset( ClockEvent clockEvent ) {
-		    	handleSimulationTimeReset();
-		    }
-		};
-		_clock.addClockListener(_clockAdapter);
-		
-		// Create the animation interpreter that will execute the animation.
-		animationIterpreter = new ModelAnimationDeltaInterpreter(this, ANIMATION_SEQUENCE);
-	}
-	
-    //------------------------------------------------------------------------
-    // Methods
-    //------------------------------------------------------------------------
-	
-	private void handleClockTicked(){
-		age = _clock.getSimulationTime() * AGE_ADJUSTMENT_FACTOR;
-		animationIterpreter.setTime(age);
+		super("Volcano", Arrays.asList( "volcano_cool.png","volcano_hot.png" ), center, width, 0, 0, clock , VOLCANO_AGE_FACTOR );
 	}
 
-	private void handleSimulationTimeReset(){
-		age = 0;
-	}
+    protected ArrayList<ModelAnimationDelta> getAnimationSequence() {
+        TimeUpdater timeUpdater=new TimeUpdater(0, MultiNucleusDecayModel.convertYearsToMs( 10E6 ));
+        ArrayList<ModelAnimationDelta> ANIMATION_SEQUENCE = new ArrayList<ModelAnimationDelta>();
+        ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(-0.25, 0), 0, 1.0, 0, 0, 0));
+        ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(0.25, 0), 0, 1.0, 0, 0, 0));
+        ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(-0.3, 0.1), 0, 1.0, 0, 0, 0));
+        ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(0.3, -0.10), 0, 1.0, 0, 0, 0));
+        ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(0, 0.1), 0, 1.0, 0, 0, 0));
+        ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(0, -0.1), 0, 1.0, 0, 0, 0));
+        ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(-0.2, -0.1), 0, 1.0, 0, 0, 0));
+        ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(0.2, 0.1), 0, 1.0, 0, 0, 0));
+        ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(-0.25, 0), 0, 1.0, 0, 0, 0));
+        ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(0.25, 0), 0, 1.0, 0, 0, 0));
 
-	@Override
-	public double getAge() {
-		return age;
-	}
-	
-	//------------------------------------------------------------------------
-    // The animation sequence that defines how the appearance of the tree
-	// will change as it ages.
-    //------------------------------------------------------------------------
-	static{
-		// Shake.
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(10E6),  new Point2D.Double(-0.25, 0), 0, 1.0, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(20E6),  new Point2D.Double(0.25, 0), 0, 1.0, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(30E6),  new Point2D.Double(-0.3, 0.1), 0, 1.0, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(40E6),  new Point2D.Double(0.3, -0.10), 0, 1.0, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(50E6),  new Point2D.Double(0, 0.1), 0, 1.0, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(60E6),  new Point2D.Double(0, -0.1), 0, 1.0, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(70E6),  new Point2D.Double(-0.2, -0.1), 0, 1.0, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(80E6),  new Point2D.Double(0.2, 0.1), 0, 1.0, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(90E6),  new Point2D.Double(-0.25, 0), 0, 1.0, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(100E6),  new Point2D.Double(0.25, 0), 0, 1.0, 0, 0, 0));
-		
-		// Fade image to hot volcano.
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(110E6), null, 0, 1.0, 0, 0, 0.2));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(120E6), null, 0, 1.0, 0, 0, 0.2));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(130E6), null, 0, 1.0, 0, 0, 0.2));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(140E6), null, 0, 1.0, 0, 0, 0.2));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(150E6), null, 0, 1.0, 0, 0, 0.2));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(160E6), null, 0, 1.0, 0, 0, 0.2));
-		
-		// More shaking.
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(170E6),  new Point2D.Double(-0.25, 0), 0, 1.0, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(180E6),  new Point2D.Double(0.25, 0), 0, 1.0, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(190E6),  new Point2D.Double(-0.3, 0.1), 0, 1.0, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(200E6),  new Point2D.Double(0.3, -0.10), 0, 1.0, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(210E6),  new Point2D.Double(0, 0.1), 0, 1.0, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(220E6),  new Point2D.Double(0, -0.1), 0, 1.0, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(230E6),  new Point2D.Double(-0.2, -0.1), 0, 1.0, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(240E6),  new Point2D.Double(0.2, 0.1), 0, 1.0, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(250E6),  new Point2D.Double(-0.25, 0), 0, 1.0, 0, 0, 0));
-		ANIMATION_SEQUENCE.add(new ModelAnimationDelta(MultiNucleusDecayModel.convertYearsToMs(260E6),  new Point2D.Double(0.25, 0), 0, 1.0, 0, 0, 0));
-	}
+        // Fade image to hot volcano.
+        ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(), null, 0, 1.0, 0, 0, 0.2));
+        ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(), null, 0, 1.0, 0, 0, 0.2));
+        ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(), null, 0, 1.0, 0, 0, 0.2));
+        ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(), null, 0, 1.0, 0, 0, 0.2));
+        ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(), null, 0, 1.0, 0, 0, 0.2));
+        ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(), null, 0, 1.0, 0, 0, 0.2));
+
+        // More shaking.
+        ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(-0.25, 0), 0, 1.0, 0, 0, 0));
+        ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(0.25, 0), 0, 1.0, 0, 0, 0));
+        ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(-0.3, 0.1), 0, 1.0, 0, 0, 0));
+        ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(0.3, -0.10), 0, 1.0, 0, 0, 0));
+        ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(0, 0.1), 0, 1.0, 0, 0, 0));
+        ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(0, -0.1), 0, 1.0, 0, 0, 0));
+        ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(-0.2, -0.1), 0, 1.0, 0, 0, 0));
+        ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(0.2, 0.1), 0, 1.0, 0, 0, 0));
+        ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(-0.25, 0), 0, 1.0, 0, 0, 0));
+        ANIMATION_SEQUENCE.add(new ModelAnimationDelta(timeUpdater.updateTime(),  new Point2D.Double(0.25, 0), 0, 1.0, 0, 0, 0));
+        return ANIMATION_SEQUENCE;
+    }
 }
