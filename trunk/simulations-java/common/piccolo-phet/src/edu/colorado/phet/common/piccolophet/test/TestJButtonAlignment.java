69332f7bc96656b0393f351bfd4a351de312848f
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-07-03 16:40:50 +0000

    test button alignment

---
+++ trunk/simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/test/TestJButtonAlignment.java
@@ -0,0 +1,69 @@
+package edu.colorado.phet.common.piccolophet.test;
+
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
+
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+
+import edu.colorado.phet.common.phetcommon.view.util.SwingUtils;
+import edu.colorado.phet.common.piccolophet.PhetPCanvas;
+import edu.umd.cs.piccolo.nodes.PPath;
+import edu.umd.cs.piccolox.pswing.PSwing;
+
+/**
+ * I noticed that it was impossible to left-align JButtons on Mac.
+ * There is always a bit of blank space to the left of the button.
+ * This program demonstrates that it's not a PSwing issue, it's a general Swing issue.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
+public class TestJButtonAlignment extends JFrame {
+
+    public TestJButtonAlignment() {
+        super();
+        
+        PhetPCanvas canvas = new PhetPCanvas();
+        canvas.setPreferredSize( new Dimension( 400, 300 ) );
+        
+        PPath rectNode = new PPath( new Rectangle2D.Double( 0, 0, 100, 50 ) );
+        rectNode.setPaint( Color.RED );
+        canvas.getLayer().addChild( rectNode );
+        rectNode.setOffset( 50, 50 );
+        
+        // JLabel left aligns OK
+        PSwing labelNode = new PSwing( new JLabel( "PSwing JLabel" ) );
+        canvas.getLayer().addChild( labelNode );
+        labelNode.setOffset( rectNode.getXOffset(), rectNode.getFullBoundsReference().getMaxY() + 10 );
+        
+        // JButton doesn't align perfectly, there is a bit of blank space to the left of the button (on Mac OS only?)
+        JButton button = new JButton( "PSwing JButton" );
+        PSwing buttonNode = new PSwing( button );
+        canvas.getLayer().addChild( buttonNode );
+        buttonNode.setOffset( rectNode.getXOffset(), labelNode.getFullBoundsReference().getMaxY() + 10 );
+        
+        // JPanel with a JLabel and JButton, same space is shown to left of JButton
+        JPanel panel = new JPanel();
+        panel.setBorder( new LineBorder( Color.BLACK ) );
+        panel.setLayout( new GridBagLayout() );
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.anchor = GridBagConstraints.WEST;
+        constraints.gridx = 0;
+        constraints.gridy = GridBagConstraints.RELATIVE;
+        panel.add( new JLabel( "JLabel" ), constraints );
+        panel.add( new JButton( "JButton" ), constraints );
+        PSwing pswing = new PSwing( panel );
+        canvas.getLayer().addChild( pswing );
+        pswing.setOffset( rectNode.getXOffset(), buttonNode.getFullBoundsReference().getMaxY() + 10 );
+        
+        getContentPane().add( canvas );
+        pack();
+    }
+    
+    public static void main( String[] args ) {
+        JFrame frame = new TestJButtonAlignment();
+        frame.setDefaultCloseOperation( WindowConstants.EXIT_ON_CLOSE );
+        SwingUtils.centerWindowOnScreen( frame );
+        frame.setVisible( true );
+    }
+}
