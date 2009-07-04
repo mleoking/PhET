adfff3f43f23229e25e00aff19bee37bd27f5419
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-07-04 08:39:22 +0000

    Wicket internationalization tests

--- trunk/web/wicket-test/src/java/edu/colorado/phet/wickettest/test/SubPage.java
+++ trunk/web/wicket-test/src/java/edu/colorado/phet/wickettest/test/SubPage.java
@@ -3,15 +3,15 @@ package edu.colorado.phet.wickettest.test;
 import org.apache.wicket.PageParameters;
 
 import edu.colorado.phet.common.phetcommon.util.LocaleUtils;
-import edu.colorado.phet.wickettest.panels.SimulationListPanel;
+import edu.colorado.phet.wickettest.panels.SimulationDisplayPanel;
 import edu.colorado.phet.wickettest.util.PhetPage;
 
 public class SubPage extends PhetPage {
     public SubPage( PageParameters parameters ) {
         super( parameters );
 
-        add( new SimulationListPanel( "page-1", LocaleUtils.stringToLocale( "en" ) ) );
-        add( new SimulationListPanel( "page-2", LocaleUtils.stringToLocale( "ar" ) ) );
+        add( new SimulationDisplayPanel( "page-1", LocaleUtils.stringToLocale( "en" ) ) );
+        add( new SimulationDisplayPanel( "page-2", LocaleUtils.stringToLocale( "ar" ) ) );
 
     }
 
