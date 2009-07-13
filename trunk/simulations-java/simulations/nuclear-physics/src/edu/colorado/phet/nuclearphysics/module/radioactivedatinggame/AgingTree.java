484061ee54f1e6ac7c086fa9c6fe3cba1f8d5715
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-07-13 21:50:04 +0000

    Moved the AgingTree class to descend directly from the AnimatedDatableItem so that a more adaptable animation can be created.  Also create part of the animation.

--- trunk/simulations-java/simulations/nuclear-physics/src/edu/colorado/phet/nuclearphysics/module/radioactivedatinggame/AgingTree.java
+++ trunk/simulations-java/simulations/nuclear-physics/src/edu/colorado/phet/nuclearphysics/module/radioactivedatinggame/AgingTree.java
@@ -2,12 +2,14 @@
 
 package edu.colorado.phet.nuclearphysics.module.radioactivedatinggame;
 
+import java.awt.geom.Dimension2D;
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
 import edu.colorado.phet.nuclearphysics.module.alphadecay.multinucleus.MultiNucleusDecayModel;
+import edu.umd.cs.piccolo.util.PDimension;
 
 /**
  * This class implements the behavior of a model element that represents a
@@ -16,24 +18,117 @@ import edu.colorado.phet.nuclearphysics.module.alphadecay.multinucleus.MultiNucl
  *
  * @author John Blanco
  */
-public class AgingTree extends StaticAnimatedDatableItem {
+public class AgingTree extends AnimatedDatableItem {
 
     //------------------------------------------------------------------------
     // Instance Data
     //------------------------------------------------------------------------
+	
+	private static final double FULL_GROWN_TREE_HEIGHT = 25; // Model units, roughly meters.
+	private static final double GROWTH_RATE = 1.03; // High number for faster growth.
+	private static final double AGE_OF_NATURAL_DEATH = MultiNucleusDecayModel.convertYearsToMs(500);
+	private static final int SWAY_COUNT = 75; // Controls how long tree sways before falling over.
+	private static final double MAX_SWAY_ANGLE = Math.PI/12; // Controls amount of sway.
 
+    //------------------------------------------------------------------------
+    // Class Data
+    //------------------------------------------------------------------------
+	
+	private boolean _closurePossibleSent = false;
+	private int _swayCounter = SWAY_COUNT;
+	
     //------------------------------------------------------------------------
     // Constructor
     //------------------------------------------------------------------------
 
     public AgingTree( ConstantDtClock clock, Point2D center, double width ) {
-        super( "Aging Tree", Arrays.asList( "tree_1.png", "tree_dead_2.png" ), center, width, 0, 0, clock, MultiNucleusDecayModel.convertYearsToMs( 1000 ) / 5000 );
+        super( "Aging Tree", Arrays.asList( "tree_1.png", "tree_dead_2.png" ), center, width, 0, 0, clock, 
+        		MultiNucleusDecayModel.convertYearsToMs( 500 ) / 5000 );
+    }
+    
+    //------------------------------------------------------------------------
+    // Methods.
+    //------------------------------------------------------------------------
+    
+    @Override
+    protected void handleClockTicked() {
+    	super.handleClockTicked();
+    	animate(getClock().getSimulationTime() * getTimeConversionFactor() - getBirthTime());
+    }
+    
+    /**
+     * Implement the next steps in the animation of the tree based on a
+     * number of factors, such as its age, whether it has reached full size,
+     * whether closure has occurred, etc.
+     * 
+     * @param time
+     */
+    private void animate(double time){
+    	
+    	if (!_closurePossibleSent){
+    		// At the moment of birth for the tree, closure is possible.  If
+    		// we haven't set the state to indicate this, do it now.
+    		setClosureState(RadiometricClosureState.CLOSURE_POSSIBLE);
+    	}
+    	
+    	// Handle growth.
+    	
+    	Dimension2D size = getSize();
+    	if (( size.getHeight() < FULL_GROWN_TREE_HEIGHT && getClosureState() != RadiometricClosureState.CLOSED )){
+    		
+    		// Grow a little bit.
+    		setSize(new PDimension(size.getWidth() * GROWTH_RATE, size.getHeight() * GROWTH_RATE));
+    		
+    		// Shift up a bit so that it looks like the tree is growing up out
+    		// of the ground.
+    		Point2D centerPos = getPosition();
+    		setPosition(centerPos.getX(), centerPos.getY() + size.getHeight() * 0.01);
+    	}
+    	
+    	// Handle death by natural causes.
+    	if (getClosureState() != RadiometricClosureState.CLOSED && time > AGE_OF_NATURAL_DEATH){
+    		// Time to die, a.k.a. to radiometrically "close".
+    		setClosureState(RadiometricClosureState.CLOSED);
+    	}
+
+    	// Handle the post-closure animation.
+    	if ( getClosureState() == RadiometricClosureState.CLOSED ){
+    		
+    		if (getFadeFactor() < 1.0){
+    			// Handle fading from live to dead image.
+	    		double currentFadeFactor = getFadeFactor();
+	    		
+	    		double fadeRate = 0.02;
+	    		if (time < AGE_OF_NATURAL_DEATH){
+	    			// Fade faster if closer was forced so that users don't get
+	    			// impatient.
+	    			fadeRate *= 1.5;
+	    		}
+				setFadeFactor(Math.min(currentFadeFactor + 0.02, 1.0));
+    		}
+    		else if (_swayCounter > 0){
+    			
+    			// Set the angle for the sway.
+    			double angleMultiplier = 
+    				-Math.sin(((double)(_swayCounter - SWAY_COUNT) / (double)SWAY_COUNT) * Math.PI * 2); 
+    			setRotationalAngle(angleMultiplier * MAX_SWAY_ANGLE);
+    			
+    			// Translate a bit so that it looks like the bottom of the
+    			// tree is the point around which the sway is occurring.
+    			double swayMultiplier = 
+    				Math.cos(((double)(_swayCounter - SWAY_COUNT) / (double)SWAY_COUNT) * Math.PI * 2); 
+    			setPosition(getPosition().getX() + swayMultiplier * getSize().getWidth() * 0.01, getPosition().getY());
+    			
+    			// Move to the next step in the cycle.
+    			_swayCounter--;
+    		}
+    	}
     }
 
     //------------------------------------------------------------------------
     // The animation sequence that defines how the appearance of the tree
     // will change as it ages.
-    //-----------------------------------------------------------------------
+    //------------------------------------------------------------------------
 
     protected AnimationSequence getAnimationSequence() {
     	
@@ -117,4 +212,5 @@ public class AgingTree extends StaticAnimatedDatableItem {
         
         return new StaticAnimationSequence(animationSequence);
     }
+
 }
