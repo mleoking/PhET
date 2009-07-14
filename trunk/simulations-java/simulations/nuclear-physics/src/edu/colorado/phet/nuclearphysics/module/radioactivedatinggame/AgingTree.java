c0342c5a9edb8ccddc1f82c1a07a3729a1243718
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-07-14 22:07:44 +0000

    Removed obsolete static animation sequence, did a minor tweak to the new dynamic sequence.

--- trunk/simulations-java/simulations/nuclear-physics/src/edu/colorado/phet/nuclearphysics/module/radioactivedatinggame/AgingTree.java
+++ trunk/simulations-java/simulations/nuclear-physics/src/edu/colorado/phet/nuclearphysics/module/radioactivedatinggame/AgingTree.java
@@ -31,7 +31,7 @@ public class AgingTree extends AnimatedDatableItem {
 	private static final int SWAY_COUNT = 30; // Controls how long tree sways before falling over.
 	private static final double MAX_SWAY_DEFLECTION = 0.01; // In radians, controls amount of sway.
 	private static final int FALL_COUNT = 40; // Controls how long it takes the tree to fall over.
-	private static final int BOUNCE_COUNT = 11; // Controls length of bounce after falling.
+	private static final int BOUNCE_COUNT = 9; // Controls length of bounce after falling.
 	private static final double BOUNCE_PROPORTION = 0.01; // Controls magnitude of bounds.
 	private static final Random RAND = new Random();
 
@@ -152,94 +152,6 @@ public class AgingTree extends AnimatedDatableItem {
     	}
     }
 
-    //------------------------------------------------------------------------
-    // The animation sequence that defines how the appearance of the tree
-    // will change as it ages.
-    //------------------------------------------------------------------------
-
-    protected AnimationSequence getAnimationSequence() {
-    	
-    	// Vars needed in the sequence.
-        double growthRate = 1.075;
-        double verticalGrowthCompensation = 0.035;  // Higher values move up more.
-        RadiometricClosureEvent closureOccurredEvent = 
-        	new RadiometricClosureEvent(this, RadiometricClosureState.CLOSED);
-        RadiometricClosureEvent closurePossibleEvent = 
-        	new RadiometricClosureEvent(this, RadiometricClosureState.CLOSURE_POSSIBLE);
-        TimeUpdater timeUpdater = new TimeUpdater( 0, MultiNucleusDecayModel.convertYearsToMs( 25 ) );
-
-        // Create the sequence.
-        ArrayList<ModelAnimationDelta> animationSequence = new ArrayList<ModelAnimationDelta>();
-        
-        //--------------------------------------------------------------------
-        // Add the individual animation deltas to the sequence.
-        //--------------------------------------------------------------------
-        
-        // Signal that closure is now possible.
-        animationSequence.add( new ModelAnimationDelta( timeUpdater.updateTime(), null, 0, growthRate, 0, 0, 0, 
-        		closurePossibleEvent ) );
-
-        // Grow.
-        for (int i = 0; i < 40; i++){
-            animationSequence.add( new ModelAnimationDelta( timeUpdater.updateTime(), 
-            		new Point2D.Double(0, verticalGrowthCompensation), 0, growthRate, 0, 0, 0, null ) );
-            verticalGrowthCompensation = verticalGrowthCompensation * growthRate;
-        }
-
-        // Signal that closure has occurred.
-        double tempTime = timeUpdater.updateTime();
-        setClosureAge(tempTime);
-        animationSequence.add( new ModelAnimationDelta( tempTime, null, 0, 1, 0, 0, 0, closureOccurredEvent ) );
-        
-        // Fade from the live to the dead tree.
-        int fadeSteps = 10;
-        double fadeAmountPerStep = 1 / (double)fadeSteps;
-        for (int i = 0; i < fadeSteps; i++){
-        	animationSequence.add( new ModelAnimationDelta( timeUpdater.updateTime(), null, 0, 1.0, 0, 0, fadeAmountPerStep, null ) );
-        }
-        
-        // Pause.
-        timeUpdater.updateTime();
-        timeUpdater.updateTime();
-        timeUpdater.updateTime();
-        timeUpdater.updateTime();
-
-        // Sway back and forth a bit.
-        Point2D swayRightTranslation = new Point2D.Double(0.5, 0);
-        Point2D swayLeftTranslation = new Point2D.Double(-0.5, 0);
-        double swayAngleChange = 0.05;
-        animationSequence.add( new ModelAnimationDelta( timeUpdater.updateTime(), swayRightTranslation, swayAngleChange, 1.0, 0, 0, 0, null ) );
-        animationSequence.add( new ModelAnimationDelta( timeUpdater.updateTime(), swayRightTranslation, swayAngleChange, 1.0, 0, 0, 0, null ) );
-        timeUpdater.updateTime();
-        timeUpdater.updateTime();
-        animationSequence.add( new ModelAnimationDelta( timeUpdater.updateTime(), swayLeftTranslation, -swayAngleChange, 1.0, 0, 0, 0, null ) );
-        animationSequence.add( new ModelAnimationDelta( timeUpdater.updateTime(), swayLeftTranslation, -swayAngleChange, 1.0, 0, 0, 0, null ) );
-        animationSequence.add( new ModelAnimationDelta( timeUpdater.updateTime(), swayLeftTranslation, -swayAngleChange, 1.0, 0, 0, 0, null ) );
-        animationSequence.add( new ModelAnimationDelta( timeUpdater.updateTime(), swayLeftTranslation, -swayAngleChange, 1.0, 0, 0, 0, null ) );
-        animationSequence.add( new ModelAnimationDelta( timeUpdater.updateTime(), swayRightTranslation, swayAngleChange, 1.0, 0, 0, 0, null ) );
-        animationSequence.add( new ModelAnimationDelta( timeUpdater.updateTime(), swayRightTranslation, swayAngleChange, 1.0, 0, 0, 0, null ) );
-        
-        // Fall over.
-        int fallSteps = 6;
-        Point2D fallTranslation = new Point2D.Double();
-        double fallRotationPerStep = Math.PI / 2 / (double)fallSteps;
-        double fallTranslationPerStep = 2;
-        for (int i = 0; i < fallSteps; i++){
-        	fallTranslation.setLocation(fallTranslationPerStep * Math.cos(fallRotationPerStep * i),
-        			-fallTranslationPerStep * Math.sin(fallRotationPerStep * i));
-        	animationSequence.add( new ModelAnimationDelta( timeUpdater.updateTime(), fallTranslation, fallRotationPerStep, 1.0, 0, 0, 0, null ) );
-        }
-        
-        // Sink a little.
-        Point2D sinkTranslation = new Point2D.Double(0, -0.3);
-        int sinkSteps = 5;
-        for (int i = 0; i < sinkSteps; i++){
-        	animationSequence.add( new ModelAnimationDelta( timeUpdater.updateTime(), sinkTranslation, 0, 1.0, 0, 0, 0, null ) );
-        }
-        
-        return new StaticAnimationSequence(animationSequence);
-    }
-    
     private void rotateAboutBottomCenter(double deltaTheta){
     	
     	double totalTranslation = getSize().getHeight() * Math.sin( deltaTheta / 2 );
