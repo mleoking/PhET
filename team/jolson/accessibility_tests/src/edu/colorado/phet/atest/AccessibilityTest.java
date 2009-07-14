516ef1a6820025f9df841bfd0f64032656253eea
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-07-14 03:05:38 +0000

    Swing accessibility test for customized colors

---
+++ team/jolson/accessibility_tests/src/edu/colorado/phet/atest/AccessibilityTest.java
@@ -0,0 +1,77 @@
+package edu.colorado.phet.atest;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.MessageFormat;
+
+import javax.swing.*;
+import javax.swing.border.TitledBorder;
+
+public class AccessibilityTest extends JFrame {
+
+
+    public AccessibilityTest() throws HeadlessException {
+        setTitle( "High Contrast Test" );
+        setSize( 700, 300 );
+
+        JPanel content = new JPanel( new GridLayout( 1, 2 ) );
+
+        JPanel regular = new JPanel( new GridLayout( 3, 1 ) );
+        final JPanel custom = new JPanel();
+
+        regular.setBorder( new TitledBorder( "Regular" ) );
+        custom.setBorder( new TitledBorder( "Custom colors" ) );
+
+        final JLabel regularLabel = new JLabel( "This is without color customization" );
+        regular.add( regularLabel );
+        JButton regularButton = new JButton( "Check contrast" );
+        regular.add( regularButton );
+        JTextArea textArea = new JTextArea( "If you load this sim when high-contrast is enabled on windows and press the above button, " +
+                                            "the right panel should switch back to normal colors." );
+        textArea.setLineWrap( true );
+        regular.add( textArea );
+
+
+        final JLabel customLabel = new JLabel( "This is with color customization" );
+        custom.add( customLabel );
+        final JButton customButton = new JButton( "This button does nothing" );
+        custom.add( customButton );
+
+        content.add( regular );
+        content.add( custom );
+
+        setContentPane( content );
+
+        custom.setBackground( Color.PINK );
+        customLabel.setBackground( Color.PINK );
+        customLabel.setForeground( Color.BLUE );
+        customButton.setForeground( Color.BLUE );
+
+        setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+
+        setVisible( true );
+
+        regularButton.addActionListener( new ActionListener() {
+            public void actionPerformed( ActionEvent actionEvent ) {
+                Toolkit toolkit = Toolkit.getDefaultToolkit();
+                Boolean highContrast = (Boolean) toolkit.getDesktopProperty( "win.highContrast.on" );
+
+                if ( highContrast != null && highContrast ) {
+                    custom.setBackground( null );
+                    customLabel.setBackground( null );
+                    customLabel.setForeground( null );
+                    customButton.setForeground( null );
+                    regularLabel.setText( MessageFormat.format( "Win contrast: {0}, colors should be high-contrast on the right", highContrast ) );
+                }
+                else {
+                    regularLabel.setText( MessageFormat.format( "Win contrast: {0}, colors unchanged", highContrast ) );
+                }
+            }
+        } );
+    }
+
+    public static void main( String[] args ) {
+        new AccessibilityTest();
+    }
+}
