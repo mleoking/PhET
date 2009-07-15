a9ca8a32a09022a6804e8d0c9416d76556bbc91f
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-07-15 06:14:52 +0000

    connected plot to parallel applied force

--- simulations-java/simulations/the-ramp/scala-src/edu/colorado/phet/therampscala/charts/ChartNode.scala
+++ simulations-java/simulations/the-ramp/scala-src/edu/colorado/phet/therampscala/charts/ChartNode.scala
@@ -6,11 +6,18 @@ import common.phetcommon.model.clock.ConstantDtClock
 import common.phetcommon.view.graphics.transforms.ModelViewTransform2D
 import common.piccolophet.{PhetPCanvas}
 import common.timeseries.model.{RecordableModel, TimeSeriesModel}
+import java.awt.Color
 import java.awt.geom.Point2D
+import model.RampModel
 import umd.cs.piccolo.PNode
 
-class ChartNode(transform: ModelViewTransform2D, canvas: PhetPCanvas) extends PNode {
-  val temp = new DefaultTemporalVariable()
+class ChartNode(transform: ModelViewTransform2D, canvas: PhetPCanvas, model: RampModel) extends PNode {
+
+  val temp = new DefaultTemporalVariable(){
+    override def setValue(value: Double) = model.bead.parallelAppliedForce = value
+  }
+  model.stepListeners += (() => temp.addValue(model.bead.parallelAppliedForce,model.getTime))
+//  model.addListener()
   val recordableModel = new RecordableModel() {
     def getState = "hello"
 
@@ -26,9 +33,13 @@ class ChartNode(transform: ModelViewTransform2D, canvas: PhetPCanvas) extends PN
   val updateableObject = new UpdateableObject {
     def setUpdateStrategy(updateStrategy: UpdateStrategy) = {}
   }
-  val controlGraphSeries = new ControlGraphSeries(temp)
-  val x = new MotionControlGraph(canvas, controlGraphSeries, "label", "title", 0, 10, true, timeseriesModel, updateableObject)
-  val y = new MotionControlGraph(canvas, controlGraphSeries, "label", "title", 0, 10, true, timeseriesModel, updateableObject)
+  val controlGraphSeries = new ControlGraphSeries("title",Color.blue,"abbr","N","character",temp)
+  val x = new MotionControlGraph(canvas, controlGraphSeries, "label", "title", -2000, 2000, true, timeseriesModel, updateableObject){
+    setDomainUpperBound(20)
+  }
+  val y = new MotionControlGraph(canvas, controlGraphSeries, "label", "title", 0, 10, true, timeseriesModel, updateableObject){
+    setDomainUpperBound(20)
+  }
   val set = new GraphSetNode(new GraphSetModel(new GraphSuite(Array(new MinimizableControlGraph("x", x), new MinimizableControlGraph("y", y)))))
   set.setAlignedLayout()
 
