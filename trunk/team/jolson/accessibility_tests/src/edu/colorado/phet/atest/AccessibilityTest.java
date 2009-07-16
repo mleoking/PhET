3509d2256c7a3d0e67b5a511a5b987ab61c6cd20
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-07-16 09:01:40 +0000

    Accessibility test font test

--- trunk/team/jolson/accessibility_tests/src/edu/colorado/phet/atest/AccessibilityTest.java
+++ trunk/team/jolson/accessibility_tests/src/edu/colorado/phet/atest/AccessibilityTest.java
@@ -1,23 +1,44 @@
 package edu.colorado.phet.atest;
 
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.text.MessageFormat;
 
 import javax.swing.*;
 import javax.swing.border.TitledBorder;
 
 public class AccessibilityTest extends JFrame {
 
+    private boolean isHighContrast() {
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Boolean ret = (Boolean) toolkit.getDesktopProperty( "win.highContrast.on" );
+        if ( ret == null ) {
+            return false;
+        }
+        return ret.booleanValue();
+    }
+
+    private Color accessibleColor( Color color ) {
+        if ( isHighContrast() ) {
+            return null;
+        }
+        return color;
+    }
+
+    private void setAccessibleFont( JComponent component ) {
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Font font = (Font) toolkit.getDesktopProperty( "win.messagebox.font" );
+        if ( font != null && font.getSize() > 14 ) {
+            component.setFont( font );
+        }
+    }
+
 
     public AccessibilityTest() throws HeadlessException {
-        setTitle( "High Contrast Test" );
+        setTitle( "High Contrast / Font Size Test" );
         setSize( 700, 300 );
 
         JPanel content = new JPanel( new GridLayout( 1, 2 ) );
 
-        JPanel regular = new JPanel( new GridLayout( 3, 1 ) );
+        JPanel regular = new JPanel();
         final JPanel custom = new JPanel();
 
         regular.setBorder( new TitledBorder( "Regular" ) );
@@ -25,18 +46,11 @@ public class AccessibilityTest extends JFrame {
 
         final JLabel regularLabel = new JLabel( "This is without color customization" );
         regular.add( regularLabel );
-        JButton regularButton = new JButton( "Check contrast / font" );
-        regular.add( regularButton );
-        JTextArea textArea = new JTextArea( "If you load this sim when high-contrast is enabled on windows and press the above button, " +
-                                            "the right panel should switch back to normal colors. (and the above label should use the " +
-                                            "correct windows system font" );
-        textArea.setLineWrap( true );
-        regular.add( textArea );
 
 
         final JLabel customLabel = new JLabel( "This is with color customization" );
         custom.add( customLabel );
-        final JButton customButton = new JButton( "This button does nothing" );
+        final JButton customButton = new JButton( "This button does nothing!" );
         custom.add( customButton );
 
         content.add( regular );
@@ -44,42 +58,20 @@ public class AccessibilityTest extends JFrame {
 
         setContentPane( content );
 
-        custom.setBackground( Color.PINK );
-        customLabel.setBackground( Color.PINK );
-        customLabel.setForeground( Color.BLUE );
-        customButton.setForeground( Color.BLUE );
+        custom.setBackground( accessibleColor( Color.PINK ) );
+        customLabel.setBackground( accessibleColor( Color.PINK ) );
+        customLabel.setForeground( accessibleColor( Color.BLUE ) );
+        customButton.setForeground( accessibleColor( Color.BLUE ) );
+
+        setAccessibleFont( regular );
+        setAccessibleFont( custom );
+        setAccessibleFont( regularLabel );
+        setAccessibleFont( customLabel );
+        setAccessibleFont( customButton );
 
         setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
 
-        
         setVisible( true );
-
-        regularButton.addActionListener( new ActionListener() {
-            public void actionPerformed( ActionEvent actionEvent ) {
-                Toolkit toolkit = Toolkit.getDefaultToolkit();
-                Boolean highContrast = (Boolean) toolkit.getDesktopProperty( "win.highContrast.on" );
-
-                if ( highContrast != null && highContrast ) {
-                    custom.setBackground( null );
-                    customLabel.setBackground( null );
-                    customLabel.setForeground( null );
-                    customButton.setForeground( null );
-                    regularLabel.setText( MessageFormat.format( "Win contrast: {0}, colors should be high-contrast on the right", highContrast ) );
-                }
-                else {
-                    regularLabel.setText( MessageFormat.format( "Win contrast: {0}, colors unchanged", highContrast ) );
-                }
-
-                Font font = (Font) toolkit.getDesktopProperty( "win.messagebox.font" );
-
-                if ( font != null ) {
-                    regularLabel.setFont( font );
-                    System.out.println( "Changing font" );
-                } else {
-                    System.out.println( "Not changing font, could not find win.messagebox.font" );
-                }
-            }
-        } );
     }
 
     public static void main( String[] args ) {
