9ed83a4c56f1e18f6e0052c7e81237ce12eee820
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-07-23 18:35:05 +0000

    organize imports

--- simulations-java/simulations/glaciers/src/edu/colorado/phet/glaciers/control/ModelConstantsPanel.java
+++ simulations-java/simulations/glaciers/src/edu/colorado/phet/glaciers/control/ModelConstantsPanel.java
@@ -11,7 +11,6 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JSeparator;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
