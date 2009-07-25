af928ed491c7e07f3a4aa35c7207b9317e85f827
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-07-25 21:48:32 +0000

    Wicket-test: changed hibernate session usage to the session-per-request model, with each panel having its own transaction (for now)

--- web/wicket-test/src/java/edu/colorado/phet/wickettest/panels/SimulationListPanel.java
+++ web/wicket-test/src/java/edu/colorado/phet/wickettest/panels/SimulationListPanel.java
@@ -2,7 +2,6 @@ package edu.colorado.phet.wickettest.panels;
 
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Locale;
 
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.list.ListItem;
@@ -14,41 +13,20 @@ import org.hibernate.Transaction;
 import edu.colorado.phet.wickettest.test.BasicLocalizedSimulation;
 import edu.colorado.phet.wickettest.util.HibernateUtils;
 import edu.colorado.phet.wickettest.util.PhetLink;
+import edu.colorado.phet.wickettest.util.PhetPage;
 
 public class SimulationListPanel extends PhetPanel {
 
-    public SimulationListPanel( String id, final Locale myLocale ) {
-        super( id, myLocale );
-
-        /*
-        List<SimulationModel> models = SqlUtils.getOrderedSimulationModels( getContext(), myLocale );
-
-        ListView simulationList = new ListView( "simulation-list", models ) {
-            protected void populateItem( ListItem item ) {
-                WebSimulation simulation = (WebSimulation) ( ( (SimulationModel) ( item.getModel().getObject() ) ).getObject() );
-
-                Label title = new Label( "title", simulation.getTitle() );
-                PhetLink link = SimulationPage.createLink( "simulation-link", getMyLocale(), simulation );
-                link.add( title );
-                item.add( link );
-            }
-        };
-        add( simulationList );
-        */
+    public SimulationListPanel( String id, PhetPage page ) {
+        super( id, page.getMyLocale() );
 
         List<BasicLocalizedSimulation> tsims = new LinkedList<BasicLocalizedSimulation>();
 
-        Session session = HibernateUtils.getInstance().getCurrentSession();
+        Session session = page.getHibernateSession();
         Transaction tx = null;
         try {
             tx = session.beginTransaction();
-            /*
-            List<BasicSimulation> simulations = HibernateUtils.getAllSimulationsT();
-            for ( BasicSimulation simulation : simulations ) {
-                tsims.add( simulation.getBestLocalizedSimulation( getMyLocale() ) );
-            }
-            */
-            tsims = HibernateUtils.getAllSimulationsTX( getMyLocale() );
+            tsims = HibernateUtils.getAllSimulationsS( session, getMyLocale() );
 
             tx.commit();
         }
@@ -66,12 +44,7 @@ public class SimulationListPanel extends PhetPanel {
 
         ListView simulationList = new ListView( "simulation-list", tsims ) {
             protected void populateItem( ListItem item ) {
-                System.out.println( "item " + item );
-                System.out.println( "item.getModel() " + item.getModel().getClass().getCanonicalName() );
-                System.out.println( "item.getModel().getObject() " + item.getModel().getObject().getClass().getCanonicalName() );
-                //System.out.println( "item.getModel().getObject().getObject() " + item.getModel().getObject().getObject().getClass().getCanonicalName() );
                 BasicLocalizedSimulation simulation = (BasicLocalizedSimulation) item.getModel().getObject();
-
                 Label title = new Label( "title", simulation.getTitle() );
                 PhetLink link = new PhetLink( "simulation-link", "#" );
                 link.add( title );
