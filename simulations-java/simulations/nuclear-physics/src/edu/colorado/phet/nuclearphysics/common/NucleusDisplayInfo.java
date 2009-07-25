5dbbe67f9adc8c188abb05e9a747db54858288a7
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-07-25 00:27:49 +0000

    Added display information for Hydrogen-3 and Helium-3, and also added an image name to the NucleusDisplayInfo class for cases where a nucleus is being displayed as a pre-fabricated image.

--- simulations-java/simulations/nuclear-physics/src/edu/colorado/phet/nuclearphysics/common/NucleusDisplayInfo.java
+++ simulations-java/simulations/nuclear-physics/src/edu/colorado/phet/nuclearphysics/common/NucleusDisplayInfo.java
@@ -23,89 +23,117 @@ public class NucleusDisplayInfo {
     // Class Data
     //------------------------------------------------------------------------
 
+	public static final NucleusDisplayInfo HYDROGEN_3_DISPLAY_INFO = new NucleusDisplayInfo(
+			NuclearPhysicsStrings.HYDROGEN_3_LEGEND_LABEL,
+			NuclearPhysicsStrings.HYDROGEN_3_CHEMICAL_SYMBOL,
+			NuclearPhysicsStrings.HYDROGEN_3_ISOTOPE_NUMBER,
+			NuclearPhysicsConstants.HYDROGEN_3_LABEL_COLOR,
+			NuclearPhysicsConstants.HYDROGEN_COLOR, 
+			"hydrogen-nucleus.png" );
+
+	public static final NucleusDisplayInfo HELIUM_3_DISPLAY_INFO = new NucleusDisplayInfo(
+			NuclearPhysicsStrings.HELIUM_3_LEGEND_LABEL,
+			NuclearPhysicsStrings.HELIUM_3_CHEMICAL_SYMBOL,
+			NuclearPhysicsStrings.HELIUM_3_ISOTOPE_NUMBER,
+			NuclearPhysicsConstants.HELIUM_3_LABEL_COLOR,
+			NuclearPhysicsConstants.HELIUM_COLOR, 
+			"helium-nucleus.png" );
+
 	public static final NucleusDisplayInfo CARBON_14_DISPLAY_INFO = new NucleusDisplayInfo(
 			NuclearPhysicsStrings.CARBON_14_LEGEND_LABEL,
 			NuclearPhysicsStrings.CARBON_14_CHEMICAL_SYMBOL,
 			NuclearPhysicsStrings.CARBON_14_ISOTOPE_NUMBER,
 			NuclearPhysicsConstants.CARBON_14_LABEL_COLOR,
-			NuclearPhysicsConstants.CARBON_COLOR );
+			NuclearPhysicsConstants.CARBON_COLOR, 
+			"Polonium Nucleus Small.png" );
 
 	public static final NucleusDisplayInfo NITROGEN_14_DISPLAY_INFO = new NucleusDisplayInfo(
 			NuclearPhysicsStrings.NITROGEN_14_LEGEND_LABEL,
 			NuclearPhysicsStrings.NITROGEN_14_CHEMICAL_SYMBOL,
 			NuclearPhysicsStrings.NITROGEN_14_ISOTOPE_NUMBER,
 			NuclearPhysicsConstants.NITROGEN_14_LABEL_COLOR,
-			NuclearPhysicsConstants.NITROGEN_COLOR );
+			NuclearPhysicsConstants.NITROGEN_COLOR, 
+			"Polonium Nucleus Small.png" );
 
 	public static final NucleusDisplayInfo URANIUM_235_DISPLAY_INFO = new NucleusDisplayInfo(
 			NuclearPhysicsStrings.URANIUM_235_LEGEND_LABEL,
 			NuclearPhysicsStrings.URANIUM_235_CHEMICAL_SYMBOL,
 			NuclearPhysicsStrings.URANIUM_235_ISOTOPE_NUMBER,
 			NuclearPhysicsConstants.URANIUM_235_LABEL_COLOR,
-			NuclearPhysicsConstants.URANIUM_COLOR );
+			NuclearPhysicsConstants.URANIUM_COLOR, 
+			"Polonium Nucleus Small.png" );
 	
 	public static final NucleusDisplayInfo URANIUM_236_DISPLAY_INFO = new NucleusDisplayInfo(
 			NuclearPhysicsStrings.URANIUM_236_LEGEND_LABEL,
 			NuclearPhysicsStrings.URANIUM_236_CHEMICAL_SYMBOL,
 			NuclearPhysicsStrings.URANIUM_236_ISOTOPE_NUMBER,
 			NuclearPhysicsConstants.URANIUM_236_LABEL_COLOR,
-			NuclearPhysicsConstants.URANIUM_COLOR );
+			NuclearPhysicsConstants.URANIUM_COLOR, 
+			"Polonium Nucleus Small.png" );
 	
 	public static final NucleusDisplayInfo URANIUM_238_DISPLAY_INFO = new NucleusDisplayInfo(
 			NuclearPhysicsStrings.URANIUM_238_LEGEND_LABEL,
 			NuclearPhysicsStrings.URANIUM_238_CHEMICAL_SYMBOL,
 			NuclearPhysicsStrings.URANIUM_238_ISOTOPE_NUMBER,
 			NuclearPhysicsConstants.URANIUM_238_LABEL_COLOR,
-			NuclearPhysicsConstants.URANIUM_COLOR );
+			NuclearPhysicsConstants.URANIUM_COLOR, 
+			"Polonium Nucleus Small.png" );
 	
 	public static final NucleusDisplayInfo URANIUM_239_DISPLAY_INFO = new NucleusDisplayInfo(
 			NuclearPhysicsStrings.URANIUM_239_LEGEND_LABEL,
 			NuclearPhysicsStrings.URANIUM_239_CHEMICAL_SYMBOL,
 			NuclearPhysicsStrings.URANIUM_239_ISOTOPE_NUMBER,
 			NuclearPhysicsConstants.URANIUM_239_LABEL_COLOR,
-			NuclearPhysicsConstants.URANIUM_COLOR );
+			NuclearPhysicsConstants.URANIUM_COLOR, 
+			"Polonium Nucleus Small.png" );
 	
 	public static final NucleusDisplayInfo POLONIUM_211_DISPLAY_INFO = new NucleusDisplayInfo(
 			NuclearPhysicsStrings.POLONIUM_211_LEGEND_LABEL,
 			NuclearPhysicsStrings.POLONIUM_211_CHEMICAL_SYMBOL,
 			NuclearPhysicsStrings.POLONIUM_211_ISOTOPE_NUMBER,
 			NuclearPhysicsConstants.POLONIUM_LABEL_COLOR,
-			NuclearPhysicsConstants.POLONIUM_COLOR );
+			NuclearPhysicsConstants.POLONIUM_COLOR, 
+			"Polonium Nucleus Small.png" );
 
 	public static final NucleusDisplayInfo LEAD_206_DISPLAY_INFO = new NucleusDisplayInfo(
 			NuclearPhysicsStrings.LEAD_206_LEGEND_LABEL,
 			NuclearPhysicsStrings.LEAD_206_CHEMICAL_SYMBOL,
 			NuclearPhysicsStrings.LEAD_206_ISOTOPE_NUMBER,
 			NuclearPhysicsConstants.LEAD_LABEL_COLOR,
-			NuclearPhysicsConstants.LEAD_COLOR );
+			NuclearPhysicsConstants.LEAD_COLOR, 
+			"Polonium Nucleus Small.png" );
 
 	public static final NucleusDisplayInfo LEAD_207_DISPLAY_INFO = new NucleusDisplayInfo(
 			NuclearPhysicsStrings.LEAD_207_LEGEND_LABEL,
 			NuclearPhysicsStrings.LEAD_207_CHEMICAL_SYMBOL,
 			NuclearPhysicsStrings.LEAD_207_ISOTOPE_NUMBER,
 			NuclearPhysicsConstants.LEAD_LABEL_COLOR,
-			NuclearPhysicsConstants.LEAD_COLOR );
+			NuclearPhysicsConstants.LEAD_COLOR, 
+			"Polonium Nucleus Small.png" );
 
 	public static final NucleusDisplayInfo CUSTOM_NUCLEUS_DISPLAY_INFO = new NucleusDisplayInfo(
 			NuclearPhysicsStrings.CUSTOM_NUCLEUS_LEGEND_LABEL,
 			NuclearPhysicsStrings.CUSTOM_NUCLEUS_CHEMICAL_SYMBOL,
 			"",  // No isotope number for the custom nucleus.
 			NuclearPhysicsConstants.CUSTOM_NUCLEUS_LABEL_COLOR,
-			NuclearPhysicsConstants.CUSTOM_NUCLEUS_PRE_DECAY_COLOR );
+			NuclearPhysicsConstants.CUSTOM_NUCLEUS_PRE_DECAY_COLOR, 
+			"Polonium Nucleus Small.png" );
 
 	public static final NucleusDisplayInfo DECAYED_CUSTOM_NUCLEUS_DISPLAY_INFO = new NucleusDisplayInfo(
 			NuclearPhysicsStrings.CUSTOM_NUCLEUS_LEGEND_LABEL,
 			NuclearPhysicsStrings.CUSTOM_NUCLEUS_CHEMICAL_SYMBOL,
 			"",  // No isotope number for the custom nucleus.
 			NuclearPhysicsConstants.DECAYED_CUSTOM_NUCLEUS_LABEL_COLOR,
-			NuclearPhysicsConstants.CUSTOM_NUCLEUS_POST_DECAY_COLOR );
+			NuclearPhysicsConstants.CUSTOM_NUCLEUS_POST_DECAY_COLOR, 
+			"Polonium Nucleus Small.png" );
 
 	public static final NucleusDisplayInfo DEFAULT_DISPLAY_INFO = new NucleusDisplayInfo(
 			"XX",
 			"XX",
 			"XX",
 			Color.WHITE,
-			Color.GRAY );
+			Color.GRAY, 
+			"Polonium Nucleus Small.png" );
 	
     //------------------------------------------------------------------------
     // Instance Data
@@ -116,6 +144,7 @@ public class NucleusDisplayInfo {
 	private final String isotopeNumberString; // Mostly used when labeling a nucleus.
 	private final Color labelColor;           // Mostly used when labeling a nucleus.
 	private final Color displayColor;         // Color used when portraying nucleus as a sphere.
+	private final String imageName;           // Used when representing this nucleus as an image.
 
     //------------------------------------------------------------------------
     // Constructor
@@ -125,18 +154,21 @@ public class NucleusDisplayInfo {
 	 * Constructor.  Not intended to be instantiated outside of this class,
 	 * but this could change if for some reason more dynamic display info is
 	 * needed in the future.
+	 * @param imageName TODO
 	 */
 	private NucleusDisplayInfo(
 			String name, 
 			String chemicalSymbol, 
 			String isotopeNumberString, 
 			Color labelColor,
-			Color displayColor) {
+			Color displayColor,
+			String imageName) {
 		this.longName = name;
 		this.chemicalSymbol = chemicalSymbol;
 		this.isotopeNumberString = isotopeNumberString;
 		this.labelColor = labelColor;
 		this.displayColor = displayColor;
+		this.imageName = imageName;
 	}
 
     //------------------------------------------------------------------------
@@ -162,12 +194,24 @@ public class NucleusDisplayInfo {
 	public Color getDisplayColor() {
 		return displayColor;
 	}
-	
+
+	public String getImageName() {
+		return imageName;
+	}
+
 	public static NucleusDisplayInfo getDisplayInfoForNucleusType(NucleusType nucleusType){
 		
 		NucleusDisplayInfo displayInfo = null;
 		
 		switch (nucleusType){
+		case HYDROGEN_3:
+			displayInfo = HYDROGEN_3_DISPLAY_INFO;
+			break;
+
+		case HELIUM_3:
+			displayInfo = HELIUM_3_DISPLAY_INFO;
+			break;
+		
 		case CARBON_14:
 			displayInfo = CARBON_14_DISPLAY_INFO;
 			break;
