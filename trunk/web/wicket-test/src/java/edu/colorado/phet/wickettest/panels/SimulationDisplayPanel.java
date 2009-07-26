612fa6fe87e4b2e843ed22f61ece9ec95cd14efe
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-07-26 22:35:46 +0000

    Wicket-test: changed simulation display list to wrapping-table format, with optional paging

--- trunk/web/wicket-test/src/java/edu/colorado/phet/wickettest/panels/SimulationDisplayPanel.java
+++ trunk/web/wicket-test/src/java/edu/colorado/phet/wickettest/panels/SimulationDisplayPanel.java
@@ -1,14 +1,16 @@
 package edu.colorado.phet.wickettest.panels;
 
 import java.text.MessageFormat;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.list.ListItem;
-import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.markup.repeater.Item;
+import org.apache.wicket.markup.repeater.data.GridView;
+import org.apache.wicket.markup.repeater.data.IDataProvider;
+import org.apache.wicket.model.IModel;
 
-import edu.colorado.phet.wickettest.SimulationModel;
 import edu.colorado.phet.wickettest.WebSimulation;
 import edu.colorado.phet.wickettest.content.SimulationPage;
 import static edu.colorado.phet.wickettest.util.HtmlUtils.encode;
@@ -21,19 +23,68 @@ public class SimulationDisplayPanel extends PhetPanel {
     public SimulationDisplayPanel( String id, final Locale myLocale ) {
         super( id, myLocale );
 
-        List<SimulationModel> models = SqlUtils.getOrderedSimulationModels( getContext(), getMyLocale() );
+        List<WebSimulation> simulations = SqlUtils.getOrderedSimulations( getContext(), getMyLocale() );
 
-        ListView simulationList = new ListView( "simulation-list", models ) {
-            protected void populateItem( ListItem item ) {
-                WebSimulation simulation = (WebSimulation) ( ( (SimulationModel) ( item.getModel().getObject() ) ).getObject() );
+        IDataProvider simData = new SimulationDataProvider( simulations );
+        GridView gridView = new GridView( "rows", simData ) {
+            @Override
+            protected void populateEmptyItem( Item item ) {
+                item.setVisible( false );
+            }
+
+            @Override
+            protected void populateItem( Item item ) {
+                WebSimulation simulation = (WebSimulation) item.getModelObject();
                 PhetLink link = SimulationPage.createLink( "simulation-link", getMyLocale(), simulation );
                 link.add( new Label( "title", simulation.getTitle() ) );
                 link.add( new StaticImage( "thumbnail", simulation.getThumbnailUrl(), MessageFormat.format( "Screenshot of the simulation {0}", encode( simulation.getTitle() ) ) ) );
                 item.add( link );
             }
         };
-        add( simulationList );
+        gridView.setColumns( 3 );
+        add( gridView );
 
+    }
+
+    private static class SimulationDataProvider implements IDataProvider {
+        private List<WebSimulation> simulations;
+
+        private SimulationDataProvider( List<WebSimulation> simulations ) {
+            this.simulations = simulations;
+        }
+
+
+        public Iterator iterator( int first, int count ) {
+            int endIndex = first + count;
+            if ( endIndex > simulations.size() ) {
+                endIndex = simulations.size();
+            }
+            return simulations.subList( first, endIndex ).iterator();
+        }
+
+        public int size() {
+            return simulations.size();
+        }
+
+        public IModel model( final Object o ) {
+            return new IModel() {
+                public Object getObject() {
+                    return o;
+                }
+
+                public void setObject( Object o ) {
+
+                }
+
+                public void detach() {
+
+                }
+            };
+        }
+
+        public void detach() {
+
+        }
     }
 
 }
