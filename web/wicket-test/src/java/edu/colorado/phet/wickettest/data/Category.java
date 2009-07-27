46f140bcb11fa1f3a1eca1511895f3b238fd3bb8
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-07-27 12:30:41 +0000

    Wicket-test: category data model implementation

---
+++ web/wicket-test/src/java/edu/colorado/phet/wickettest/data/Category.java
@@ -0,0 +1,94 @@
+package edu.colorado.phet.wickettest.data;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Category {
+    private int id;
+    private String name;
+    private boolean auto;
+    private boolean root;
+    private List subcategories = new LinkedList();
+    private List simulations = new LinkedList();
+    private Category parent;
+
+    public Category() {
+    }
+
+    public Category( Category parent, String name ) {
+        this.parent = parent;
+        this.name = name;
+        if ( parent == null ) {
+            auto = true;
+            root = true;
+        }
+        else if ( parent.isRoot() ) {
+            auto = true;
+            root = false;
+        }
+        else {
+            auto = false;
+            root = false;
+        }
+
+        if ( !root ) {
+            parent.subcategories.add( this );
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId( int id ) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName( String name ) {
+        this.name = name;
+    }
+
+    public boolean isAuto() {
+        return auto;
+    }
+
+    public void setAuto( boolean auto ) {
+        this.auto = auto;
+    }
+
+    public boolean isRoot() {
+        return root;
+    }
+
+    public void setRoot( boolean root ) {
+        this.root = root;
+    }
+
+    public List getSubcategories() {
+        return subcategories;
+    }
+
+    public void setSubcategories( List subcategories ) {
+        this.subcategories = subcategories;
+    }
+
+    public List getSimulations() {
+        return simulations;
+    }
+
+    public void setSimulations( List simulations ) {
+        this.simulations = simulations;
+    }
+
+    public Category getParent() {
+        return parent;
+    }
+
+    public void setParent( Category parent ) {
+        this.parent = parent;
+    }
+}
---
+++ web/wicket-test/src/java/edu/colorado/phet/wickettest/test/InitializeCategories.java
@@ -0,0 +1,84 @@
+package edu.colorado.phet.wickettest.test;
+
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import edu.colorado.phet.wickettest.data.Category;
+import edu.colorado.phet.wickettest.data.Simulation;
+import edu.colorado.phet.wickettest.util.HibernateUtils;
+
+public class InitializeCategories {
+
+    public static void addSimsToCategory( Session session, Category category, String[] names ) {
+        List list = category.getSimulations();
+        for ( String name : names ) {
+            Simulation sim = (Simulation) session.createQuery( "select s from Simulation as s where s.name = :name" ).setString( "name", name ).uniqueResult();
+            list.add( sim );
+        }
+    }
+
+    public static void main( String[] args ) {
+
+        Transaction tx = null;
+        Session session = HibernateUtils.getInstance().openSession();
+        try {
+            tx = session.beginTransaction();
+
+            Category root = new Category( null, "root" );
+            Category featured = new Category( root, "featured" );
+            addSimsToCategory( session, featured, new String[]{
+                    "circuit-construction-kit-dc", "stretching-dna", "glaciers", "moving-man", "soluble-salts",
+                    "equation-grapher", "projectile-motion", "lasers", "faraday", "quantum-tunneling", "wave-interference",
+                    "radio-waves"} );
+            Category physics = new Category( root, "physics" );
+            Category motion = new Category( physics, "motion" );
+            Category soundAndWaves = new Category( physics, "soundAndWaves" );
+
+            Simulation movingMan = (Simulation) session.createQuery( "select s from Simulation as s where s.name = :name" ).setString( "name", "moving-man" ).uniqueResult();
+            Simulation masses = (Simulation) session.createQuery( "select s from Simulation as s where s.name = :name" ).setString( "name", "mass-spring-lab" ).uniqueResult();
+            Simulation sound = (Simulation) session.createQuery( "select s from Simulation as s where s.name = :name" ).setString( "name", "sound" ).uniqueResult();
+
+            motion.getSimulations().add( movingMan );
+            motion.getSimulations().add( masses );
+            soundAndWaves.getSimulations().add( sound );
+            soundAndWaves.getSimulations().add( masses );
+
+            session.save( root );
+            session.save( featured );
+            session.save( physics );
+            session.save( motion );
+            session.save( soundAndWaves );
+
+            tx.commit();
+        }
+        catch( RuntimeException e ) {
+            if ( tx != null && tx.isActive() ) {
+                try {
+                    tx.rollback();
+                }
+                catch( HibernateException e1 ) {
+                    System.out.println( "Error rolling back transaction" );
+                }
+                throw e;
+            }
+        }
+
+    }
+
+    public static void traceCategory( Category category, String buf ) {
+        System.out.println( buf + "CAT name: " + category.getName() + " id: " + category.getId() );
+        for ( Object o : category.getSimulations() ) {
+            Simulation sim = (Simulation) o;
+            System.out.println( buf + "  " + sim.getName() );
+        }
+        for ( Object o : category.getSubcategories() ) {
+            Category cat = (Category) o;
+            traceCategory( cat, buf + "    " );
+        }
+    }
+
+
+}
