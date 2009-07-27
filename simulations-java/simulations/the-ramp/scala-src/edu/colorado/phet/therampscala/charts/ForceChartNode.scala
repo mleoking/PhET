b254d4439c24db1dc42be4a479a94392f1f96c15
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-07-27 14:22:56 +0000

    added a series for friction

--- simulations-java/simulations/the-ramp/scala-src/edu/colorado/phet/therampscala/charts/ForceChartNode.scala
+++ simulations-java/simulations/the-ramp/scala-src/edu/colorado/phet/therampscala/charts/ForceChartNode.scala
@@ -6,16 +6,20 @@ import common.phetcommon.model.clock.ConstantDtClock
 import common.phetcommon.view.graphics.transforms.ModelViewTransform2D
 import common.piccolophet.{PhetPCanvas}
 import common.timeseries.model.{RecordableModel, TimeSeriesModel}
-import java.awt.Color
 import java.awt.geom.Point2D
 import model.RampModel
 import umd.cs.piccolo.PNode
 
 class ForceChartNode(transform: ModelViewTransform2D, canvas: PhetPCanvas, model: RampModel) extends PNode {
-  val temp = new DefaultTemporalVariable() {
+  val parallelAppliedForceVariable = new DefaultTemporalVariable() {
     override def setValue(value: Double) = model.bead.parallelAppliedForce = value
   }
-  model.stepListeners += (() => temp.addValue(model.bead.parallelAppliedForce, model.getTime))
+  model.stepListeners += (() => parallelAppliedForceVariable.addValue(model.bead.parallelAppliedForce, model.getTime))
+
+  val parallelFriction = new DefaultTemporalVariable()
+  //todo: use ParallelComponent of the friction Vector
+  model.stepListeners += (() => parallelFriction.addValue(model.bead.frictionForce.magnitude, model.getTime))
+
   val recordableModel = new RecordableModel() {
     def getState = "hello"
 
@@ -31,33 +35,32 @@ class ForceChartNode(transform: ModelViewTransform2D, canvas: PhetPCanvas, model
   val updateableObject = new UpdateableObject {
     def setUpdateStrategy(updateStrategy: UpdateStrategy) = {}
   }
-  val controlGraphSeries = new ControlGraphSeries("title", Color.blue, "abbr", "N", "character", temp)
+  val controlGraphSeries = new ControlGraphSeries("Parallel Applied Force", RampDefaults.appliedForceColor, "Fa", "N", "", parallelAppliedForceVariable)
   val parallelForceChart = new MotionControlGraph(canvas, controlGraphSeries, "label", "title", -2000, 2000, true, timeseriesModel, updateableObject) {
     setDomainUpperBound(20)
     getJFreeChartNode.setBuffered(false)
     getJFreeChartNode.setPiccoloSeries() //works better on an unbuffered chart
+    addSeries(new ControlGraphSeries("Parallel Friction Force", RampDefaults.frictionForceColor, "Ff", "N", "", parallelFriction))
   }
+
   //  val y = new MotionControlGraph(canvas, controlGraphSeries, "label", "title", 0, 10, true, timeseriesModel, updateableObject) {
   //    setDomainUpperBound(20)
   //    getJFreeChartNode.setBuffered(false)
   //    getJFreeChartNode.setPiccoloSeries()
   //  }
   //  val set = new GraphSetNode(new GraphSetModel(new GraphSuite(Array(new MinimizableControlGraph("x", x), new MinimizableControlGraph("y", y)))))
-  val set = new GraphSetNode(new GraphSetModel(new GraphSuite(Array(new MinimizableControlGraph("Parallel Forces(N)", parallelForceChart))))){
+  val graphSetNode = new GraphSetNode(new GraphSetModel(new GraphSuite(Array(new MinimizableControlGraph("Parallel Forces(N)", parallelForceChart))))) {
     override def getMaxAvailableHeight(availableHeight: Double) = availableHeight
   }
-  set.setAlignedLayout()
+  graphSetNode.setAlignedLayout()
 
-  addChild(set)
+  addChild(graphSetNode)
 
   def updatePosition() = {
     val viewLoc = transform.modelToView(new Point2D.Double(0, -1))
     val viewBounds = transform.getViewBounds
-    println("viewBounds=" + viewBounds)
     val h = viewBounds.getHeight - viewLoc.y
-    println("using h=" + h)
-    set.setBounds(viewBounds.getX, viewLoc.y, viewBounds.getWidth, h)
-//    set.relayout()
+    graphSetNode.setBounds(viewBounds.getX, viewLoc.y, viewBounds.getWidth, h)
   }
   updatePosition()
 }
