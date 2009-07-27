5fa30422773c08824b9e92a0478b5fdf0eee5753
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-07-27 23:09:34 +0000

    Reformatted slightly and added a thumbnail of the newsletter that is linked to the actual newsletter.

--- trunk/web/website/about/news.php
+++ trunk/web/website/about/news.php
@@ -21,11 +21,16 @@ class NewsPage extends SitePage {
         $newsletters_dir = PHET_DIST_ROOT."newsletters/";
 
         print <<<EOT
+            <h2>Current Newsletter</h2>
+
+            <p><a href="{$newsletters_dir}phet_newsletter_sum09.pdf">Summer 2009</a></p>
+            <p><a href="{$newsletters_dir}phet_newsletter_sum09.pdf"><img src="phet_newsletter_summer_09_thumbnail.png" width="180" height="233" border="2" alt="Summer 2009 Newsletter" /></a></p>
+            <br>
+
             <h2>Newsletter Archive</h2>
 
             <ul>
-                <li><a href="{$newsletters_dir}phet_newsletter_sum09.pdf">Summer 2009</a></li>
-                <li><a href="{$newsletters_dir}phet_newsletter_july16_2008.pdf">Summer 2008 - PhET's first newsletter</a></li>
+            <li><a href="{$newsletters_dir}phet_newsletter_july16_2008.pdf">Summer 2008 - PhET's first newsletter</a></li>
             </ul>
 
             <h2>Website &amp; Simulation Updates</h2>
