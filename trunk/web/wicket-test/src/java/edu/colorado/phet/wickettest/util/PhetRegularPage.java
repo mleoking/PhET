e815f57a7a0c815bce4f41b4cbe528a79c27778c
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-07-28 09:02:43 +0000

    Wicket-test: todo doc

--- trunk/web/wicket-test/src/java/edu/colorado/phet/wickettest/util/PhetRegularPage.java
+++ trunk/web/wicket-test/src/java/edu/colorado/phet/wickettest/util/PhetRegularPage.java
@@ -8,7 +8,7 @@ public abstract class PhetRegularPage extends PhetPage {
     public PhetRegularPage( PageParameters parameters ) {
         super( parameters, true );
 
-        // TODO: add in RTL support? not the easiest thing in CSS for swapping panel layouts like this
+        // TODO: add in RTL support? not the easiest thing in CSS for swapping panel layouts like this (implement with a LTR or RTL panel option that specifies either float or direction
 
         add( new SideNavMenu( "side-navigation", getPageContext() ) );
     }
