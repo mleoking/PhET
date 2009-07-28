1a328ecbd15f7f2a94b16d9ddb62530696f438a6
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-07-28 08:58:05 +0000

    Wicket-test: initial reworking of index page (with many things to do)

--- web/wicket-test/src/java/edu/colorado/phet/wickettest/content/IndexPage.java
+++ web/wicket-test/src/java/edu/colorado/phet/wickettest/content/IndexPage.java
@@ -6,10 +6,7 @@ import org.apache.wicket.markup.html.link.Link;
 
 import edu.colorado.phet.common.phetcommon.util.LocaleUtils;
 import edu.colorado.phet.wickettest.translation.TranslationTestPage;
-import edu.colorado.phet.wickettest.util.Linkable;
-import edu.colorado.phet.wickettest.util.PageContext;
-import edu.colorado.phet.wickettest.util.PhetLink;
-import edu.colorado.phet.wickettest.util.PhetPage;
+import edu.colorado.phet.wickettest.util.*;
 
 public class IndexPage extends PhetPage {
     public IndexPage( PageParameters parameters ) {
@@ -17,7 +14,16 @@ public class IndexPage extends PhetPage {
 
         addTitle( "Wicket test index page" );
 
-        add( SimulationDisplay.createLink( "en-simulations", LocaleUtils.stringToLocale( "en" ) ) );
+        add( new StaticImage( "ksu-logo", "/images/ksu-logo.gif", null ) );
+        add( new StaticImage( "jila-logo", "/images/jila_logo_small.gif", null ) );
+        add( new StaticImage( "nsf-logo", "/images/nsf-logo-small.gif", null ) );
+        add( new StaticImage( "hewlett-logo", "/images/hewlett-logo-small.jpg", null ) );
+
+        PhetLink imageLink = SimulationDisplay.createLink( "image-link", LocaleUtils.stringToLocale( "en" ) );
+        add( imageLink );
+        imageLink.add( new StaticImage( "index-animated-screenshot", "/images/mass-spring-lab-animated-screenshot.gif", null ) );
+
+        add( SimulationDisplay.createLink( "play-sims-link", LocaleUtils.stringToLocale( "en" ) ) );
         add( SimulationDisplay.createLink( "es-simulations", LocaleUtils.stringToLocale( "es" ) ) );
         add( SimulationDisplay.createLink( "el-simulations", LocaleUtils.stringToLocale( "el" ) ) );
         add( SimulationDisplay.createLink( "ar-simulations", LocaleUtils.stringToLocale( "ar" ) ) );
