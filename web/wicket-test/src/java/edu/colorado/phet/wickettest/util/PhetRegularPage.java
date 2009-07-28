7f6edfb00101a9fceba075480d76583a11a21cb1
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-07-28 18:57:06 +0000

    removed comment

--- web/wicket-test/src/java/edu/colorado/phet/wickettest/util/PhetRegularPage.java
+++ web/wicket-test/src/java/edu/colorado/phet/wickettest/util/PhetRegularPage.java
@@ -8,8 +8,6 @@ public abstract class PhetRegularPage extends PhetPage {
     public PhetRegularPage( PageParameters parameters ) {
         super( parameters, true );
 
-        // TODO: add in RTL support? not the easiest thing in CSS for swapping panel layouts like this (implement with a LTR or RTL panel option that specifies either float or direction
-
         add( new SideNavMenu( "side-navigation", getPageContext() ) );
     }
 }
