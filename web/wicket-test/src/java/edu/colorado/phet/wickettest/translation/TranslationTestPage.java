a82670e9b14250103f025445148473085663d6b9
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-07-29 06:40:02 +0000

    Wicket-test: translation testing, added wicket-extensions

--- web/wicket-test/src/java/edu/colorado/phet/wickettest/translation/TranslationTestPage.java
+++ web/wicket-test/src/java/edu/colorado/phet/wickettest/translation/TranslationTestPage.java
@@ -1,15 +1,65 @@
 package edu.colorado.phet.wickettest.translation;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Locale;
 
 import org.apache.wicket.Component;
 import org.apache.wicket.PageParameters;
+import org.hibernate.HibernateException;
+import org.hibernate.Transaction;
 
 import edu.colorado.phet.common.phetcommon.util.LocaleUtils;
+import edu.colorado.phet.wickettest.content.SimulationDisplay;
+import edu.colorado.phet.wickettest.data.Category;
+import edu.colorado.phet.wickettest.data.LocalizedSimulation;
+import edu.colorado.phet.wickettest.panels.PhetPanel;
+import edu.colorado.phet.wickettest.panels.SimulationDisplayPanel;
+import edu.colorado.phet.wickettest.util.HibernateUtils;
+import edu.colorado.phet.wickettest.util.PageContext;
 
 public class TranslationTestPage extends TranslationPage {
+
+    private PhetPanel panel;
+
     public TranslationTestPage( PageParameters parameters ) {
         super( parameters, true );
+
+        Locale testLocale = LocaleUtils.stringToLocale( "zh_CN" );
+
+        Category category;
+        List<LocalizedSimulation> simulations = null;
+        PageContext context = getPageContext();
+
+        Transaction tx = null;
+        try {
+            tx = getHibernateSession().beginTransaction();
+            if ( parameters.containsKey( "categories" ) ) {
+                category = Category.getCategoryFromPath( context.getSession(), "featured" );
+
+                simulations = new LinkedList<LocalizedSimulation>();
+                SimulationDisplay.addSimulationsFromCategory( simulations, testLocale, category );
+            }
+            else {
+                simulations = HibernateUtils.getAllSimulationsWithLocale( context.getSession(), testLocale );
+                HibernateUtils.orderSimulations( simulations, testLocale );
+            }
+            tx.commit();
+        }
+        catch( RuntimeException e ) {
+            if ( tx != null && tx.isActive() ) {
+                try {
+                    tx.rollback();
+                }
+                catch( HibernateException e1 ) {
+                    System.out.println( "ERROR: Error rolling back transaction" );
+                }
+                throw e;
+            }
+        }
+
+        panel = new SimulationDisplayPanel( "panel", context, simulations );
+        add( panel );
 
         addTitle( "Translation test page" );
     }
@@ -18,6 +68,7 @@ public class TranslationTestPage extends TranslationPage {
         if ( locale == null && key.equals( "language.dir" ) ) {
             return "ltr";
         }
-        return LocaleUtils.localeToString( locale ) + " : " + key;
+        //return LocaleUtils.localeToString( locale ) + " : " + key;
+        return null;
     }
 }
