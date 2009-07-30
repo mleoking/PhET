6274022ad25c2f9eaca5a36df466ddb7b5759653
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-07-30 01:34:12 +0000

    Wicket-test: CSS tweaks, added breadcrumb system, other misc changes

--- trunk/web/wicket-test/src/java/edu/colorado/phet/wickettest/util/PhetRegularPage.java
+++ trunk/web/wicket-test/src/java/edu/colorado/phet/wickettest/util/PhetRegularPage.java
@@ -1,25 +1,18 @@
 package edu.colorado.phet.wickettest.util;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import org.apache.wicket.PageParameters;
 
 import edu.colorado.phet.wickettest.menu.NavLocation;
-import edu.colorado.phet.wickettest.panels.SideNavMenu;
+import edu.colorado.phet.wickettest.panels.NavBreadCrumbs;
 
-public abstract class PhetRegularPage extends PhetPage {
+public class PhetRegularPage extends PhetMenuPage {
     public PhetRegularPage( PageParameters parameters ) {
-        super( parameters, true );
+        super( parameters );
     }
 
-    public void initializeMenu( NavLocation currentLocation ) {
-        Set<NavLocation> currentLocations = new HashSet<NavLocation>();
-        currentLocations.add( currentLocation );
-        initializeMenuWithSet( currentLocations );
-    }
-
-    public void initializeMenuWithSet( Set<NavLocation> currentLocations ) {
-        add( new SideNavMenu( "side-navigation", getPageContext(), currentLocations ) );
+    @Override
+    public void initializeLocation( NavLocation currentLocation ) {
+        super.initializeLocation( currentLocation );
+        add( new NavBreadCrumbs( "breadcrumbs", getPageContext(), currentLocation ) );
     }
 }
