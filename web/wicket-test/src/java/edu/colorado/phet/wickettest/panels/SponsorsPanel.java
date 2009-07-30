2667ad700049e0f6d544bd8ed5cdb201cb732179
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-07-30 07:03:03 +0000

    Wicket-test: ugly but working translation test is up and running

---
+++ web/wicket-test/src/java/edu/colorado/phet/wickettest/panels/SponsorsPanel.java
@@ -0,0 +1,9 @@
+package edu.colorado.phet.wickettest.panels;
+
+import edu.colorado.phet.wickettest.util.PageContext;
+
+public class SponsorsPanel extends PhetPanel {
+    public SponsorsPanel( String id, PageContext context ) {
+        super( id, context );
+    }
+}
--- web/wicket-test/src/java/edu/colorado/phet/wickettest/translation/TranslationTestPage.java
+++ web/wicket-test/src/java/edu/colorado/phet/wickettest/translation/TranslationTestPage.java
@@ -1,92 +1,101 @@
 package edu.colorado.phet.wickettest.translation;
 
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Locale;
 
 import org.apache.wicket.Component;
 import org.apache.wicket.PageParameters;
 import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.extensions.ajax.markup.html.AjaxEditableLabel;
+import org.apache.wicket.extensions.ajax.markup.html.AjaxEditableMultiLineLabel;
 import org.apache.wicket.model.Model;
-import org.hibernate.HibernateException;
-import org.hibernate.Transaction;
 
 import edu.colorado.phet.common.phetcommon.util.LocaleUtils;
-import edu.colorado.phet.wickettest.content.SimulationDisplay;
-import edu.colorado.phet.wickettest.data.Category;
-import edu.colorado.phet.wickettest.data.LocalizedSimulation;
 import edu.colorado.phet.wickettest.panels.PhetPanel;
-import edu.colorado.phet.wickettest.panels.SimulationDisplayPanel;
-import edu.colorado.phet.wickettest.util.HibernateUtils;
+import edu.colorado.phet.wickettest.panels.SponsorsPanel;
 import edu.colorado.phet.wickettest.util.PageContext;
 
 public class TranslationTestPage extends TranslationPage {
 
     private PhetPanel panel;
-    private Model str = new Model( "Simulations" );
+    private Model modelDir = new Model( "ltr" );
+    private Model modelPrincipalSponsors = new Model( "Principal Sponsors" );
+    private Model modelHewlett = new Model( "Makes grants to address the most serious social and environmental problems facing society, where risk capital, responsibly invested, may make a difference over time." );
+    private Model modelNsf = new Model( "An independent federal agency created by Congress in 1950 to promote the progress of science." );
+    private Model modelKsu = new Model( "King Saud University seeks to become a leader in educational and technological innovation, scientific discovery and creativity through fostering an atmosphere of intellectual inspiration and partnership for the prosperity of society." );
 
     public TranslationTestPage( PageParameters parameters ) {
         super( parameters, true );
 
         Locale testLocale = LocaleUtils.stringToLocale( "zh_CN" );
 
-        Category category;
-        List<LocalizedSimulation> simulations = null;
-        PageContext context = getPageContext();
-
-        Transaction tx = null;
-        try {
-            tx = getHibernateSession().beginTransaction();
-            if ( parameters.containsKey( "categories" ) ) {
-                category = Category.getCategoryFromPath( getHibernateSession(), "featured" );
-
-                simulations = new LinkedList<LocalizedSimulation>();
-                SimulationDisplay.addSimulationsFromCategory( simulations, testLocale, category );
-            }
-            else {
-                simulations = HibernateUtils.getAllSimulationsWithLocale( getHibernateSession(), testLocale );
-                HibernateUtils.orderSimulations( simulations, testLocale );
-            }
-            tx.commit();
-        }
-        catch( RuntimeException e ) {
-            if ( tx != null && tx.isActive() ) {
-                try {
-                    tx.rollback();
-                }
-                catch( HibernateException e1 ) {
-                    System.out.println( "ERROR: Error rolling back transaction" );
-                }
-                throw e;
-            }
-        }
-
-        panel = new SimulationDisplayPanel( "panel", context, simulations );
+        panel = new SponsorsPanel( "panel", new PageContext( testLocale, this ) );
         panel.setOutputMarkupId( true );
         add( panel );
 
         addTitle( "Translation test page" );
 
 
-        add( new AjaxEditableLabel( "translation-label", str ) {
+        add( new AjaxEditableMultiLineLabel( "translation-dir", modelDir ) {
             @Override
             protected void onSubmit( AjaxRequestTarget target ) {
                 super.onSubmit( target );
-                System.out.println( "Edited" );
-                System.out.println( "model: " + getModel() );
-                System.out.println( "Label model: " + getLabel().getModel() );
                 target.addComponent( panel );
             }
         } );
+
+        add( new AjaxEditableMultiLineLabel( "translation-principalSponsors", modelPrincipalSponsors ) {
+            @Override
+            protected void onSubmit( AjaxRequestTarget target ) {
+                super.onSubmit( target );
+                target.addComponent( panel );
+            }
+        } );
+
+        add( new AjaxEditableMultiLineLabel( "translation-hewlett", modelHewlett ) {
+            @Override
+            protected void onSubmit( AjaxRequestTarget target ) {
+                super.onSubmit( target );
+                target.addComponent( panel );
+            }
+        } );
+
+        add( new AjaxEditableMultiLineLabel( "translation-nsf", modelNsf ) {
+            @Override
+            protected void onSubmit( AjaxRequestTarget target ) {
+                super.onSubmit( target );
+                target.addComponent( panel );
+            }
+        } );
+
+        add( new AjaxEditableMultiLineLabel( "translation-ksu", modelKsu ) {
+            @Override
+            protected void onSubmit( AjaxRequestTarget target ) {
+                super.onSubmit( target );
+                target.addComponent( panel );
+            }
+        } );
+
     }
 
     public String translateString( Component component, Locale locale, String key ) {
+        /*
         if ( locale == null && key.equals( "language.dir" ) ) {
             return "ltr";
         }
-        if ( key.equals( "simulationDisplay.simulations" ) ) {
-            return (String) str.getObject();
+        */
+        if ( key.equals( "language.dir" ) ) {
+            return (String) modelDir.getObject();
+        }
+        else if ( key.equals( "sponsors.principalSponsors" ) ) {
+            return (String) modelPrincipalSponsors.getObject();
+        }
+        else if ( key.equals( "sponsors.hewlett" ) ) {
+            return (String) modelHewlett.getObject();
+        }
+        else if ( key.equals( "sponsors.nsf" ) ) {
+            return (String) modelNsf.getObject();
+        }
+        else if ( key.equals( "sponsors.ksu" ) ) {
+            return (String) modelKsu.getObject();
         }
         return null;
     }
