114f8117e152af482cd5bb055d8948ceea32ba14
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-08-01 19:15:14 +0000

    Wicket-test: changed side-by-side translation handling data to be database-backed

---
+++ trunk/web/wicket-test/src/java/edu/colorado/phet/wickettest/data/Translation.java
@@ -0,0 +1,38 @@
+package edu.colorado.phet.wickettest.data;
+
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Set;
+
+public class Translation {
+    private int id;
+    private Locale locale;
+    private Set translatedStrings = new HashSet();
+
+    public Translation() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId( int id ) {
+        this.id = id;
+    }
+
+    public Locale getLocale() {
+        return locale;
+    }
+
+    public void setLocale( Locale locale ) {
+        this.locale = locale;
+    }
+
+    public Set getTranslatedStrings() {
+        return translatedStrings;
+    }
+
+    public void setTranslatedStrings( Set translatedStrings ) {
+        this.translatedStrings = translatedStrings;
+    }
+}
--- trunk/web/wicket-test/src/java/edu/colorado/phet/wickettest/translation/TranslationTestPage.java
+++ trunk/web/wicket-test/src/java/edu/colorado/phet/wickettest/translation/TranslationTestPage.java
@@ -2,19 +2,26 @@ package edu.colorado.phet.wickettest.translation;
 
 import java.util.Locale;
 
-import org.apache.wicket.Component;
 import org.apache.wicket.PageParameters;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.extensions.ajax.markup.html.AjaxEditableMultiLineLabel;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.model.Model;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import edu.colorado.phet.common.phetcommon.util.LocaleUtils;
+import edu.colorado.phet.wickettest.data.TranslatedString;
+import edu.colorado.phet.wickettest.data.Translation;
 import edu.colorado.phet.wickettest.panels.PhetPanel;
 import edu.colorado.phet.wickettest.panels.SponsorsPanel;
 import edu.colorado.phet.wickettest.util.PageContext;
+import edu.colorado.phet.wickettest.util.PhetPage;
 
-public class TranslationTestPage extends TranslationPage {
+public class TranslationTestPage extends PhetPage {
 
+    private int translationId;
     private PhetPanel panel;
     private Model modelDir = new Model( "ltr" );
     private Model modelPrincipalSponsors = new Model( "Principal Sponsors" );
@@ -26,6 +33,36 @@ public class TranslationTestPage extends TranslationPage {
         super( parameters, true );
 
         Locale testLocale = LocaleUtils.stringToLocale( "zh_CN" );
+        Session session = getHibernateSession();
+        Translation translation = null;
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+
+            translation = new Translation();
+            translation.setLocale( testLocale );
+
+            session.save( translation );
+
+            tx.commit();
+        }
+        catch( RuntimeException e ) {
+            System.out.println( "Exception: " + e );
+            if ( tx != null && tx.isActive() ) {
+                try {
+                    tx.rollback();
+                }
+                catch( HibernateException e1 ) {
+                    System.out.println( "ERROR: Error rolling back transaction" );
+                }
+                throw e;
+            }
+        }
+
+        if ( translation != null ) {
+            translationId = translation.getId();
+            add( new Label( "translation-id", String.valueOf( translationId ) ) );
+        }
 
         panel = new SponsorsPanel( "panel", new PageContext( testLocale, this ) );
         panel.setOutputMarkupId( true );
@@ -38,6 +75,7 @@ public class TranslationTestPage extends TranslationPage {
             @Override
             protected void onSubmit( AjaxRequestTarget target ) {
                 super.onSubmit( target );
+                setString( "language.dir", (String) modelDir.getObject() );
                 target.addComponent( panel );
             }
         } );
@@ -46,6 +84,7 @@ public class TranslationTestPage extends TranslationPage {
             @Override
             protected void onSubmit( AjaxRequestTarget target ) {
                 super.onSubmit( target );
+                setString( "sponsors.principalSponsors", (String) modelPrincipalSponsors.getObject() );
                 target.addComponent( panel );
             }
         } );
@@ -54,6 +93,7 @@ public class TranslationTestPage extends TranslationPage {
             @Override
             protected void onSubmit( AjaxRequestTarget target ) {
                 super.onSubmit( target );
+                setString( "sponsors.hewlett", (String) modelHewlett.getObject() );
                 target.addComponent( panel );
             }
         } );
@@ -62,6 +102,7 @@ public class TranslationTestPage extends TranslationPage {
             @Override
             protected void onSubmit( AjaxRequestTarget target ) {
                 super.onSubmit( target );
+                setString( "sponsors.nsf", (String) modelNsf.getObject() );
                 target.addComponent( panel );
             }
         } );
@@ -70,33 +111,59 @@ public class TranslationTestPage extends TranslationPage {
             @Override
             protected void onSubmit( AjaxRequestTarget target ) {
                 super.onSubmit( target );
+                setString( "sponsors.ksu", (String) modelKsu.getObject() );
                 target.addComponent( panel );
             }
         } );
 
     }
 
-    public String translateString( Component component, Locale locale, String key ) {
-        /*
-        if ( locale == null && key.equals( "language.dir" ) ) {
-            return "ltr";
+    public void setString( String key, String value ) {
+        Session session = getHibernateSession();
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+
+            Translation translation = (Translation) session.load( Translation.class, translationId );
+            TranslatedString tString = null;
+            for ( Object o : translation.getTranslatedStrings() ) {
+                TranslatedString ts = (TranslatedString) o;
+                if ( ts.getKey().equals( key ) ) {
+                    tString = ts;
+                    break;
+                }
+            }
+            if ( tString == null ) {
+                tString = new TranslatedString();
+                tString.setKey( key );
+                tString.setValue( value );
+                tString.setTranslation( translation );
+                translation.getTranslatedStrings().add( tString );
+                session.save( tString );
+            }
+            else {
+                tString.setValue( value );
+                session.update( tString );
+            }
+
+            tx.commit();
         }
-        */
-        if ( key.equals( "language.dir" ) ) {
-            return (String) modelDir.getObject();
+        catch( RuntimeException e ) {
+            System.out.println( "Exception: " + e );
+            if ( tx != null && tx.isActive() ) {
+                try {
+                    tx.rollback();
+                }
+                catch( HibernateException e1 ) {
+                    System.out.println( "ERROR: Error rolling back transaction" );
+                }
+                throw e;
+            }
         }
-        else if ( key.equals( "sponsors.principalSponsors" ) ) {
-            return (String) modelPrincipalSponsors.getObject();
-        }
-        else if ( key.equals( "sponsors.hewlett" ) ) {
-            return (String) modelHewlett.getObject();
-        }
-        else if ( key.equals( "sponsors.nsf" ) ) {
-            return (String) modelNsf.getObject();
-        }
-        else if ( key.equals( "sponsors.ksu" ) ) {
-            return (String) modelKsu.getObject();
-        }
-        return null;
+    }
+
+    @Override
+    public String getVariation() {
+        return new Integer( translationId ).toString();
     }
 }
