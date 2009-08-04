b048d76bbfccca7991f17ef9134552e8893ed316
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-08-04 15:38:56 +0000

    improve internationalization

--- trunk/simulations-java/simulations/the-ramp/scala-src/edu/colorado/phet/therampscala/RampResources.scala
+++ trunk/simulations-java/simulations/the-ramp/scala-src/edu/colorado/phet/therampscala/RampResources.scala
@@ -1,7 +1,18 @@
 package edu.colorado.phet.therampscala
 
 import common.phetcommon.resources.PhetResources
+import java.text.MessageFormat
 
-object RampResources extends PhetResources("the-ramp"){
-  def str(key:String)=getLocalizedString(key)
+object RampResources extends PhetResources("the-ramp") {
+  val forcePattern = str("force.pattern")
+  val energyPattern = str("energy.pattern")
+  val workPattern = str("work.pattern")
+
+  def str(key: String) = getLocalizedString(key)
+
+  def formatForce(force: String) = MessageFormat.format(forcePattern, force)
+
+  def formatEnergy(energy: String) = MessageFormat.format(energyPattern, energy)
+
+  def formatWork(work: String) = MessageFormat.format(workPattern, work)
 }
