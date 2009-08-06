03a87b7c31b122596b13a79a66e78f406fab291a
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-08-06 06:36:22 +0000

    Wicket-test: added html-embedding localization component, and fully localized the about PhET page.

--- web/wicket-test/src/java/edu/colorado/phet/wickettest/content/AboutPhetPanel.java
+++ web/wicket-test/src/java/edu/colorado/phet/wickettest/content/AboutPhetPanel.java
@@ -1,17 +1,20 @@
 package edu.colorado.phet.wickettest.content;
 
-import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.model.ResourceModel;
-
 import edu.colorado.phet.wickettest.panels.PhetPanel;
+import edu.colorado.phet.wickettest.util.LocalizedLabel;
 import edu.colorado.phet.wickettest.util.PageContext;
 
 public class AboutPhetPanel extends PhetPanel {
     public AboutPhetPanel( String id, PageContext context ) {
         super( id, context );
 
-        //add( new Label( "about-p1", new StringResourceModel( "about.p1", this, null, new String[]{"href=\"http://phet.colorado.edu/simulations/index.php\"", "href=\"http://phet.colorado.edu/research/index.php\""} ) ) );
-        ResourceModel resourceModel = new ResourceModel( "about.p1" );
-        add( new Label( "about-p1", resourceModel ) );
+        add( new LocalizedLabel( "about-p1", "about.p1", new String[]{"href=\"http://phet.colorado.edu/simulations/index.php\"", "href=\"http://phet.colorado.edu/research/index.php\""} ) );
+        add( new LocalizedLabel( "about-p2", "about.p2" ) );
+        add( new LocalizedLabel( "about-p3", "about.p3", new String[]{"href=\"http://phet.colorado.edu/about/legend.php\""} ) );
+        add( new LocalizedLabel( "about-p4", "about.p4", new String[]{
+                "href=\"http://phet.colorado.edu/index.php\"",
+                "href=\"http://phet.colorado.edu/tech_support/support-java.php\"",
+                "href=\"http://phet.colorado.edu/tech_support/support-flash.php\""
+        } ) );
     }
 }
