cb6d63836c40711733279a230f4ced4b89da1845
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-08-06 15:09:38 +0000

    Adjusted initial tree position so that it doesn't go under the air probe when it grows to full size.

--- trunk/simulations-java/simulations/nuclear-physics/src/edu/colorado/phet/nuclearphysics/module/radioactivedatinggame/RadiometricMeasurementModel.java
+++ trunk/simulations-java/simulations/nuclear-physics/src/edu/colorado/phet/nuclearphysics/module/radioactivedatinggame/RadiometricMeasurementModel.java
@@ -34,7 +34,7 @@ public class RadiometricMeasurementModel implements ModelContainingDatableItems 
 	
 	// Constants that control the initial size and position of some of the
 	// model elements.
-	private static final Point2D INITIAL_TREE_POSITION = new Point2D.Double(-12, -2);
+	private static final Point2D INITIAL_TREE_POSITION = new Point2D.Double(-12, -4);
 	private static final double  INITIAL_TREE_WIDTH = 1;
 	private static final Point2D INITIAL_VOLCANO_POSITION = new Point2D.Double(13, 0);
 	private static final double  INITIAL_VOLCANO_WIDTH = 12;
