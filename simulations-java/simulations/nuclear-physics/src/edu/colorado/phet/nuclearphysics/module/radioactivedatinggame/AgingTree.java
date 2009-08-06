e00d947e63ea5d8342fd15ab64249e66e0eab7d6
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-08-06 18:32:37 +0000

    Modified the rate at which the tree ages and the age at which it dies.  This was based on feedback from the most recent meeting.

--- simulations-java/simulations/nuclear-physics/src/edu/colorado/phet/nuclearphysics/module/radioactivedatinggame/AgingTree.java
+++ simulations-java/simulations/nuclear-physics/src/edu/colorado/phet/nuclearphysics/module/radioactivedatinggame/AgingTree.java
@@ -27,7 +27,7 @@ public class AgingTree extends AnimatedDatableItem {
 	
 	private static final double FULL_GROWN_TREE_HEIGHT = 22; // Model units, roughly meters.
 	private static final double GROWTH_RATE = 1.03; // High number for faster growth.
-	private static final double AGE_OF_NATURAL_DEATH = MultiNucleusDecayModel.convertYearsToMs(3000);
+	private static final double AGE_OF_NATURAL_DEATH = MultiNucleusDecayModel.convertYearsToMs(1000);
 	private static final int FADE_TO_DEAD_TREE_COUNT = 30; // Controls how long it takes for tree to die.
 	private static final int SWAY_COUNT = 30; // Controls how long tree sways before falling over.
 	private static final double MAX_SWAY_DEFLECTION = 0.01; // In radians, controls amount of sway.
