d5fffa0cbd81aec490669424bba40ec830577312
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-08-11 21:29:36 +0000

    Removed obsolete website tests. If you want to see these, you can always update them back.

--- trunk/web/django-test/testproject/djangotest/__init__.py
+++
@@ -1,3 +0,0 @@
-import sys, os
-tmp = os.getcwd().split('/')
-sys.path.append( "/".join( tmp[ 0 : len( tmp ) - 1 ] ) )
--- trunk/web/lift-test/src/com/liftworkshop/snippet/HelloWorld.scala
+++
@@ -1,6 +0,0 @@
-package com.liftworkshop.snippet
-
-class HelloWorld {
-  def howdy = <span>Welcome to todo at{new _root_.java.util.Date}</span>
-}
-
