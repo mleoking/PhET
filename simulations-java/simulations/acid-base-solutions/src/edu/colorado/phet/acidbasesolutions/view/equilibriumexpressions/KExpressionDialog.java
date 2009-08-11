f4d9395630a4b663707916719d3baf269e7c8d67
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-08-11 19:46:10 +0000

    change title of K expression dialog to "Equilibrium Expression" (singular)

--- simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/view/equilibriumexpressions/KExpressionDialog.java
+++ simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/view/equilibriumexpressions/KExpressionDialog.java
@@ -2,6 +2,7 @@ package edu.colorado.phet.acidbasesolutions.view.equilibriumexpressions;
 
 import java.awt.Frame;
 
+import edu.colorado.phet.acidbasesolutions.ABSStrings;
 import edu.colorado.phet.acidbasesolutions.model.AqueousSolution;
 
 /**
@@ -13,5 +14,6 @@ public class KExpressionDialog extends EquilibriumExpressionsDialog {
     
     public KExpressionDialog( Frame owner, final AqueousSolution solution ) {
         super( owner, solution, false /* showWaterExpression */ );
+        setTitle( ABSStrings.TITLE_EQUILIBRIUM_EXPRESSION );
     }
 }
