4747c65a719462d34c403e140d15bdb1a28cc4c6
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-08-11 17:37:01 +0000

    8/11/09 save/load state of K expression dialog (open/closed) 8/11/09 add button next to strength slider to show K expression with scaling control, first tab only 8/11/09 add dialog that shows K expression only

---
+++ simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/view/equilibriumexpressions/KExpressionDialog.java
@@ -0,0 +1,17 @@
+package edu.colorado.phet.acidbasesolutions.view.equilibriumexpressions;
+
+import java.awt.Frame;
+
+import edu.colorado.phet.acidbasesolutions.model.AqueousSolution;
+
+/**
+ * Dialog that displays the K equilibrium expression for a solution.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
+public class KExpressionDialog extends EquilibriumExpressionsDialog {
+    
+    public KExpressionDialog( Frame owner, final AqueousSolution solution ) {
+        super( owner, solution, false /* showWaterExpression */ );
+    }
+}
