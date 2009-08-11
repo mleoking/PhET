d9258093ccc5c8abc9259ae82005de6219259359
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-08-11 09:09:22 +0000

    Wicket-test: admin interface work

--- web/wicket-test/src/java/edu/colorado/phet/wickettest/admin/AdminMainPage.java
+++ web/wicket-test/src/java/edu/colorado/phet/wickettest/admin/AdminMainPage.java
@@ -1,13 +1,84 @@
 package edu.colorado.phet.wickettest.admin;
 
-import org.apache.wicket.PageParameters;
+import java.util.*;
 
+import org.apache.wicket.PageParameters;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import edu.colorado.phet.common.phetcommon.util.LocaleUtils;
 import edu.colorado.phet.wickettest.authentication.AuthenticatedPage;
+import edu.colorado.phet.wickettest.components.PhetLink;
+import edu.colorado.phet.wickettest.data.LocalizedSimulation;
+import edu.colorado.phet.wickettest.data.Simulation;
 
 public class AdminMainPage extends AuthenticatedPage {
     public AdminMainPage( PageParameters parameters ) {
         super( parameters );
 
         addTitle( "Administration section" );
+
+        Session session = getHibernateSession();
+        List<Simulation> simulations = new LinkedList();
+        final Map<Simulation, LocalizedSimulation> englishSims = new HashMap<Simulation, LocalizedSimulation>();
+
+        Locale english = LocaleUtils.stringToLocale( "en" );
+
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+
+            List sims = session.createQuery( "select s from Simulation as s" ).list();
+
+            for ( Object sim : sims ) {
+                Simulation simulation = (Simulation) sim;
+                simulations.add( simulation );
+                for ( Object o : simulation.getLocalizedSimulations() ) {
+                    LocalizedSimulation lsim = (LocalizedSimulation) o;
+                    if ( lsim.getLocale().equals( english ) ) {
+                        englishSims.put( simulation, lsim );
+                        break;
+                    }
+                }
+            }
+
+            tx.commit();
+        }
+        catch( RuntimeException e ) {
+            if ( tx != null && tx.isActive() ) {
+                try {
+                    tx.rollback();
+                }
+                catch( HibernateException e1 ) {
+                    System.out.println( "ERROR: Error rolling back transaction" );
+                }
+                throw e;
+            }
+        }
+
+        Collections.sort( simulations, new Comparator<Simulation>() {
+            public int compare( Simulation a, Simulation b ) {
+                return ( (Simulation) a ).getName().compareTo( ( (Simulation) b ).getName() );
+            }
+        } );
+
+        ListView simulationList = new ListView( "simulation-list", simulations ) {
+            protected void populateItem( ListItem item ) {
+                Simulation simulation = (Simulation) item.getModel().getObject();
+                LocalizedSimulation lsim = englishSims.get( simulation );
+                Link link = new PhetLink( "simulation-link", "#" );
+                link.add( new Label( "simulation-name", simulation.getName() ) );
+                item.add( link );
+                item.add( new Label( "simulation-project", simulation.getProject().getName() ) );
+                item.add( new Label( "simulation-title", lsim.getTitle() ) );
+            }
+        };
+
+        add( simulationList );
     }
 }
