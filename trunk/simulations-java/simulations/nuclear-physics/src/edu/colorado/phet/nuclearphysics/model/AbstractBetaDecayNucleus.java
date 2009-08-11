435907ded39eab13126cd7a1946e3d0fab16342c
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-08-11 22:17:41 +0000

    First shot at implementing the actual beta decay behavior.

--- trunk/simulations-java/simulations/nuclear-physics/src/edu/colorado/phet/nuclearphysics/model/AbstractBetaDecayNucleus.java
+++ trunk/simulations-java/simulations/nuclear-physics/src/edu/colorado/phet/nuclearphysics/model/AbstractBetaDecayNucleus.java
@@ -4,10 +4,13 @@ package edu.colorado.phet.nuclearphysics.model;
 
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
+import java.util.Random;
 
 import edu.colorado.phet.common.phetcommon.model.clock.ClockEvent;
 import edu.colorado.phet.nuclearphysics.common.NuclearPhysicsClock;
+import edu.colorado.phet.nuclearphysics.common.model.Antineutrino;
 import edu.colorado.phet.nuclearphysics.common.model.AtomicNucleus;
+import edu.colorado.phet.nuclearphysics.common.model.Electron;
 
 /**
  * This class contains much of the behavior that is common to all nuclei that
@@ -17,6 +20,9 @@ import edu.colorado.phet.nuclearphysics.common.model.AtomicNucleus;
  */
 public abstract class AbstractBetaDecayNucleus extends AtomicNucleus {
 
+	private static final Random RAND = new Random();
+	private static final double EMISSION_SPEED = 10;  // Femtometers per clock tick.  Weird, I know.
+	
 	public AbstractBetaDecayNucleus(NuclearPhysicsClock clock, Point2D position,
 			int numProtons, int numNeutrons, double decayTimeScalingFactor) {
 		super(clock, position, numProtons, numNeutrons, decayTimeScalingFactor);
@@ -26,11 +32,23 @@ public abstract class AbstractBetaDecayNucleus extends AtomicNucleus {
 	 * Take the actions that simulate alpha decay.
 	 */
 	protected void decay( ClockEvent clockEvent ){
+		
+		// Update the nucleus configuration.
+		_numNeutrons -= 1;
+		_numProtons += 1;
+		
+		// Create the emitted particles, which are an electron and an
+		// antineutrino.
+		double angle = RAND.nextDouble() * Math.PI * 2;
+		double xVel = Math.cos(angle) * EMISSION_SPEED; 
+		double yVel = Math.sin(angle) * EMISSION_SPEED; 
         ArrayList byProducts = new ArrayList();
-        // TODO: Need to implement the actual decay.
-        System.err.println(getClass().getName() + " - Warning: Beta decay not yet implemented.");
-        _numNeutrons -= 1;
-        _numProtons += 1;
+        Electron electron = new Electron(getPositionReference().getX(), getPositionReference().getY());
+        electron.setVelocity(xVel, yVel);
+        byProducts.add(electron);
+        Antineutrino antineutrino = new Antineutrino(getPositionReference().getX(), getPositionReference().getY());
+        antineutrino.setVelocity(xVel, yVel);
+        byProducts.add(antineutrino);
 
         // Set the final value of the time that this nucleus existed prior to
         // decaying.
