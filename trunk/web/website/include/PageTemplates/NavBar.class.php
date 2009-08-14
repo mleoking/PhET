fb3044e5f5cbc30ec0d710ce7adc161ab92cc9dc
dano <dano@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-08-14 23:04:56 +0000

    Made the ECSME/KSU combined logo bigger on the home page and the sponsors section of the NavBar, see #1755

--- trunk/web/website/include/PageTemplates/NavBar.class.php
+++ trunk/web/website/include/PageTemplates/NavBar.class.php
@@ -414,7 +414,7 @@ EOT;
 
                     <dt>
                         <a href="http://ecsme.ksu.edu.sa/en/index.cfm" title="Click here to visit the Excellence Center of Science and Mathematics Education at King Saud University">
-                            <img src="{$prefix}images/ECSME-combined-logo-small.jpg" height="28" alt="Excellence Center of Science and Mathematics Education at King Saud University Logo"/>Excellence Center of Science and Mathematics Education at King Saud University</a>
+                            <img src="{$prefix}images/ECSME-combined-logo-small.jpg" height="38" alt="Excellence Center of Science and Mathematics Education at King Saud University Logo"/>Excellence Center of Science and Mathematics Education at King Saud University</a>
                     </dt>
 
                     <dd>
