835a24f0f7d0c8e7f2d737d2af63fca2bde6ee37
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-08-17 23:10:12 +0000

    Removal of methods whose functionality has been promoted to the base classes.

--- trunk/simulations-java/simulations/nuclear-physics/src/edu/colorado/phet/nuclearphysics/model/Polonium211CompositeNucleus.java
+++ trunk/simulations-java/simulations/nuclear-physics/src/edu/colorado/phet/nuclearphysics/model/Polonium211CompositeNucleus.java
@@ -23,14 +23,11 @@ public class Polonium211CompositeNucleus extends AlphaDecayCompositeNucleus {
     public static final int ORIGINAL_NUM_PROTONS = 84;
     public static final int ORIGINAL_NUM_NEUTRONS = 127;
     
-    // Half life for this nucleus.
-    public static double HALF_LIFE = 516; // In milliseconds.
-
     // The "agitation factor" for the various types of nucleus.  The amount of
     // agitation controls how dynamic the nucleus looks on the canvas.  Values
     // must be in the range 0-9.
-    static final int POLONIUM_211_AGITATION_FACTOR = 5;
-    static final int LEAD_207_AGITATION_FACTOR = 1;
+    private static final int POLONIUM_211_AGITATION_FACTOR = 5;
+    private static final int LEAD_207_AGITATION_FACTOR = 1;
 
     //------------------------------------------------------------------------
     // Instance data
@@ -43,10 +40,6 @@ public class Polonium211CompositeNucleus extends AlphaDecayCompositeNucleus {
     //------------------------------------------------------------------------
     // Methods
     //------------------------------------------------------------------------
-    
-    public double getHalfLife(){
-    	return HALF_LIFE;
-    }
     
 	protected void updateAgitationFactor() {
 	    // Determine the amount of agitation that should be exhibited by this
