b97ad00ec27aecdf0a931903ee4c68d502978f99
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-08-18 16:40:31 +0000

    Removed unused variables and unneeded overrides (since much functionality has been moved into base classes).

--- simulations-java/simulations/nuclear-physics/src/edu/colorado/phet/nuclearphysics/model/BetaDecayCompositeNucleus.java
+++ simulations-java/simulations/nuclear-physics/src/edu/colorado/phet/nuclearphysics/model/BetaDecayCompositeNucleus.java
@@ -4,9 +4,12 @@ package edu.colorado.phet.nuclearphysics.model;
 
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
+import java.util.Random;
 
 import edu.colorado.phet.common.phetcommon.model.clock.ClockEvent;
 import edu.colorado.phet.nuclearphysics.common.NuclearPhysicsClock;
+import edu.colorado.phet.nuclearphysics.common.model.Antineutrino;
+import edu.colorado.phet.nuclearphysics.common.model.Electron;
 
 /**
  * Base class for composite nuclei (i.e. nuclei that are made up of multiple
@@ -17,15 +20,12 @@ import edu.colorado.phet.nuclearphysics.common.NuclearPhysicsClock;
 public abstract class BetaDecayCompositeNucleus extends CompositeAtomicNucleus {
 
     //------------------------------------------------------------------------
-    // Instance data
+    // Class Data
     //------------------------------------------------------------------------
 
-	protected double   _timeUntilDecay = 0;
-	protected double   _startTime;         // Simulation time at which this nucleus was created or last reset.
-	protected double   _pauseStartTime;    // Time at which this nucleus was paused.
-	protected double   _preDecayLifeTime;  // Milliseconds of existence prior to decay.
-	private boolean    _hasDecayed = false;
-	
+	private static final Random RAND = new Random();
+	private static final double EMISSION_SPEED = 1.5;  // Femtometers per clock tick.  Weird, I know.
+
     //------------------------------------------------------------------------
     // Constructor
     //------------------------------------------------------------------------
@@ -33,13 +33,6 @@ public abstract class BetaDecayCompositeNucleus extends CompositeAtomicNucleus {
 	public BetaDecayCompositeNucleus(NuclearPhysicsClock clock,
 			Point2D position, int numProtons, int numNeutrons) {
 		super(clock, position, numProtons, numNeutrons);
-		_startTime = clock.getSimulationTime();
-	}
-
-	public BetaDecayCompositeNucleus(NuclearPhysicsClock clock,
-			Point2D position, ArrayList constituents) {
-		super(clock, position, constituents);
-		_startTime = clock.getSimulationTime();
 	}
 
 	/**
@@ -48,13 +41,10 @@ public abstract class BetaDecayCompositeNucleus extends CompositeAtomicNucleus {
 	 */
 	public void reset() {
 	    
-	    // Reset the decay time.
-	    _timeUntilDecay = calculateDecayTime();
-	    _hasDecayed = false;
-	    _startTime = _clock.getSimulationTime();
+		super.reset();
 	    
 	    // Change one proton into a neutron.
-	    _numNeutrons++;
+	    _numNeutrons--;
 	    _numProtons++;
 	
         // Update our agitation level.
@@ -63,58 +53,36 @@ public abstract class BetaDecayCompositeNucleus extends CompositeAtomicNucleus {
         // Let the listeners know that the nucleus has changed.
         notifyNucleusChangeEvent(null);
 	}
-	
-	/**
-	 * This method lets this model element know that the clock has ticked.  In
-	 * response, the nucleus generally 'agitates' a bit, may also perform some
-	 * sort of decay, and may move.
-	 */
-	protected void handleClockTicked(ClockEvent clockEvent) {
-	    super.handleClockTicked( clockEvent );
-	    
-	    if (!_paused){
-		    // See if beta decay should occur.
-		    if ((_timeUntilDecay != Double.POSITIVE_INFINITY) && (getTimeOfExistence() >= _timeUntilDecay))
-		    {
-		    	// TODO: Beta decay not yet implemented.
-		    	System.err.println("Warning: Beta decay is not yet implemented.");
-		        
-		    	// Mark that decay has happened.
-		    	_hasDecayed = true;
-		    	_preDecayLifeTime = _timeUntilDecay;
-	
-		    	// Set the decay time to infinity to indicate that no more tunneling out
-		        // should occur.
-		        _timeUntilDecay = Double.POSITIVE_INFINITY;
-		    }
-	    }
-	}
-	
-	public void setPaused(boolean paused) {
-		if (_paused != paused){
-			super.setPaused(paused);
-			if (paused){
-				// Record the time at which the pause began.
-				_pauseStartTime = _clock.getSimulationTime();
-			}
-			else{
-				// Update the start time for this nucleus based on
-				// the amount of time that it was paused.
-				_startTime = _startTime + (_clock.getSimulationTime() - _pauseStartTime);
-			}
-		}
-	}
 
 	/**
-	 * Get the time for which this nucleus has existed.
-	 * 
-	 * @return - Existence time in milliseconds.
+	 * Take the actions that simulate beta decay.
 	 */
-	protected double getTimeOfExistence(){
-		return (_clock.getSimulationTime() - _startTime);
-	}
+	@Override
+	protected void decay(ClockEvent clockEvent) {
 
-	abstract protected void updateAgitationFactor();
-	abstract protected double calculateDecayTime();
-	abstract public double getHalfLife();
+		super.decay(clockEvent);
+
+		// Update the nucleus configuration.
+		_numNeutrons -= 1;
+		_numProtons += 1;
+
+		// Create the emitted particles, which are an electron and an
+		// antineutrino.
+		double angle = RAND.nextDouble() * Math.PI * 2;
+		double xVel = Math.cos(angle) * EMISSION_SPEED; 
+		double yVel = Math.sin(angle) * EMISSION_SPEED; 
+		ArrayList byProducts = new ArrayList();
+		Electron electron = new Electron(getPositionReference().getX(), getPositionReference().getY());
+		electron.setVelocity(xVel, yVel);
+		byProducts.add(electron);
+		angle = RAND.nextDouble() * Math.PI * 2;
+		xVel = Math.cos(angle) * EMISSION_SPEED; 
+		yVel = Math.sin(angle) * EMISSION_SPEED; 
+		Antineutrino antineutrino = new Antineutrino(getPositionReference().getX(), getPositionReference().getY());
+		antineutrino.setVelocity(xVel, yVel);
+		byProducts.add(antineutrino);
+
+		// Send out the decay event to all listeners.
+		notifyNucleusChangeEvent(byProducts);
+	}
 }
