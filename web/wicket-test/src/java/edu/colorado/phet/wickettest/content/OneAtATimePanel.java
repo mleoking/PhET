0d71cbc6f0f29afe7439b9c2c8e4c957c1c0e063
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-08-18 05:51:52 +0000

    Wicket-test: flushed out one at a time page

--- web/wicket-test/src/java/edu/colorado/phet/wickettest/content/OneAtATimePanel.java
+++ web/wicket-test/src/java/edu/colorado/phet/wickettest/content/OneAtATimePanel.java
@@ -1,15 +1,47 @@
 package edu.colorado.phet.wickettest.content;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
+import org.hibernate.Session;
 
 import edu.colorado.phet.wickettest.components.PhetLink;
+import edu.colorado.phet.wickettest.data.LocalizedSimulation;
 import edu.colorado.phet.wickettest.panels.PhetPanel;
+import edu.colorado.phet.wickettest.util.HibernateTask;
+import edu.colorado.phet.wickettest.util.HibernateUtils;
 import edu.colorado.phet.wickettest.util.Linkable;
 import edu.colorado.phet.wickettest.util.PageContext;
 
 public class OneAtATimePanel extends PhetPanel {
-    public OneAtATimePanel( String id, PageContext context ) {
+    public OneAtATimePanel( String id, final PageContext context ) {
         super( id, context );
+
+        final List<LocalizedSimulation> simulations = new LinkedList<LocalizedSimulation>();
+
+        HibernateUtils.wrapTransaction( getHibernateSession(), new HibernateTask() {
+            public boolean run( Session session ) {
+                List<LocalizedSimulation> sims = HibernateUtils.getAllSimulationsWithLocale( session, context.getLocale() );
+                HibernateUtils.orderSimulations( sims, context.getLocale() );
+                for ( LocalizedSimulation sim : sims ) {
+                    simulations.add( sim );
+                }
+                return true;
+            }
+        } );
+
+        add( new ListView( "simulation-list", simulations ) {
+            protected void populateItem( ListItem item ) {
+                LocalizedSimulation simulation = (LocalizedSimulation) item.getModel().getObject();
+                Link link = new PhetLink( "download-link", simulation.getDownloadUrl() );
+                link.add( new Label( "simulation-title", simulation.getTitle() ) );
+                item.add( link );
+            }
+        } );
 
     }
 
