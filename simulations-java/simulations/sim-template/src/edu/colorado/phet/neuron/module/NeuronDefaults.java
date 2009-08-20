bb618144428fdcbac74d469caa0804ec5092179f
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-08-20 19:34:07 +0000

    converting neuron template to sim template, incremental progress

---
+++ simulations-java/simulations/sim-template/src/edu/colorado/phet/neuron/module/NeuronDefaults.java
@@ -0,0 +1,32 @@
+/* Copyright 2007, University of Colorado */
+
+package edu.colorado.phet.neuron.module;
+
+import java.awt.Dimension;
+
+
+
+/**
+ * GlobalDefaults contains default settings that are common to 2 or more modules.
+ * 
+ * NOTE! This class is package private, and values herein should only be referenced
+ * by the "defaults" classes for each module.  Classes that are module-specific should
+ * use the class that corresponds to their module.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
+public class NeuronDefaults {
+
+    /* Not intended for instantiation */
+    private NeuronDefaults() {}
+    
+    // Clock
+    public static final boolean CLOCK_RUNNING = true;
+    public static final int CLOCK_FRAME_RATE = 25; // fps, frames per second (wall time)
+    public static final double CLOCK_DT = 1;
+    public static final int CLOCK_TIME_COLUMNS = 10;
+
+    // Model-view transform
+    public static final Dimension VIEW_SIZE = new Dimension( 1500, 1500 );
+
+}
