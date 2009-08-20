28b238af531af33d77e049043cb2d0a209836b9c
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-08-20 22:32:24 +0000

    start adding transforms

--- trunk/simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/view/NeuronCanvas.java
+++ trunk/simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/view/NeuronCanvas.java
@@ -3,8 +3,10 @@
 package edu.colorado.phet.neuron.view;
 
 import java.awt.geom.Dimension2D;
+import java.awt.geom.Rectangle2D;
 
 import edu.colorado.phet.common.piccolophet.PhetPCanvas;
+import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform2D;
 import edu.colorado.phet.neuron.NeuronConstants;
 import edu.colorado.phet.neuron.model.NeuronModel;
 import edu.colorado.phet.neuron.module.NeuronDefaults;
@@ -32,9 +34,15 @@ public class NeuronCanvas extends PhetPCanvas {
     //----------------------------------------------------------------------------
     
     public NeuronCanvas( NeuronModel model ) {
-        super( NeuronDefaults.VIEW_SIZE );
+        super( NeuronDefaults.INTERMEDIATE_RENDERING_SIZE );
+        setWorldTransformStrategy( new CenteringBoxStrategy( this, NeuronDefaults.INTERMEDIATE_RENDERING_SIZE ) );
+        ModelViewTransform2D transform = new ModelViewTransform2D(
+                new Rectangle2D.Double(-NeuronDefaults.CROSS_SECTION_RADIUS,-NeuronDefaults.CROSS_SECTION_RADIUS,NeuronDefaults.CROSS_SECTION_RADIUS*2,NeuronDefaults.CROSS_SECTION_RADIUS*2),
+                new Rectangle2D.Double( 0,0,NeuronDefaults.INTERMEDIATE_RENDERING_SIZE.width, NeuronDefaults.INTERMEDIATE_RENDERING_SIZE.height) );
         
         this.model = model;
+
+        System.out.println( "transform.modelToView = " + transform.modelToView(0,0) );
         
         setBackground( NeuronConstants.CANVAS_BACKGROUND );
         
@@ -42,7 +50,7 @@ public class NeuronCanvas extends PhetPCanvas {
         _rootNode = new PNode();
         addWorldChild( _rootNode );
         
-        ParticleNode particleNode = new ParticleNode( model.getParticle() );
+        ParticleNode particleNode = new ParticleNode( model.getParticle() ,transform);
         _rootNode.addChild(particleNode);
     }
     
@@ -65,6 +73,7 @@ public class NeuronCanvas extends PhetPCanvas {
             return;
         }
         else if ( NeuronConstants.DEBUG_CANVAS_UPDATE_LAYOUT ) {
+            System.out.println( "getSize() = " + getSize() );
             System.out.println( "ExampleCanvas.updateLayout worldSize=" + worldSize );//XXX
         }
         
