b6dae5d779828b7340c3e3eb3156dc0f04abb062
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-08-20 18:55:38 +0000

    Initial commit of the framework for the Neuron sim.

---
+++ trunk/simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/NeuronApplication.java
@@ -0,0 +1,153 @@
+/* Copyright 2007, University of Colorado */
+
+package edu.colorado.phet.neuron;
+
+import java.awt.Color;
+import java.awt.Frame;
+
+import edu.colorado.phet.common.phetcommon.application.Module;
+import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
+import edu.colorado.phet.common.phetcommon.application.PhetApplicationLauncher;
+import edu.colorado.phet.common.phetcommon.util.persistence.XMLPersistenceManager;
+import edu.colorado.phet.common.phetcommon.view.ITabbedModulePane;
+import edu.colorado.phet.common.phetcommon.view.PhetFrame;
+import edu.colorado.phet.common.phetcommon.view.menu.OptionsMenu;
+import edu.colorado.phet.common.piccolophet.PhetTabbedPane;
+import edu.colorado.phet.common.piccolophet.PiccoloPhetApplication;
+import edu.colorado.phet.common.piccolophet.TabbedModulePanePiccolo;
+import edu.colorado.phet.neuron.developer.DeveloperMenu;
+import edu.colorado.phet.neuron.module.NeuronModule;
+
+/**
+ * SimTemplateApplication is the main application for this simulation.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ * 
+ */
+public class NeuronApplication extends PiccoloPhetApplication {
+
+    //----------------------------------------------------------------------------
+    // Instance data
+    //----------------------------------------------------------------------------
+
+    private NeuronModule exampleModule;
+
+    // PersistanceManager is used to save/load simulation configurations.
+    private XMLPersistenceManager persistenceManager;
+
+    private static TabbedModulePanePiccolo tabbedModulePane;
+
+    //----------------------------------------------------------------------------
+    // Constructors
+    //----------------------------------------------------------------------------
+
+    /**
+     * Sole constructor.
+     *
+     * @param config the configuration for this application
+     */
+    public NeuronApplication( PhetApplicationConfig config )
+    {
+        super( config );
+        initTabbedPane();
+        initModules();
+        initMenubar( config.getCommandLineArgs() );
+    }
+
+    //----------------------------------------------------------------------------
+    // Initialization
+    //----------------------------------------------------------------------------
+
+    /*
+     * Initializes the tabbed pane.
+     */
+    private void initTabbedPane() {
+
+        // Create our own tabbed pane type so we can set the tab color
+        TabbedPaneType tabbedPaneType = new TabbedPaneType(){
+            public ITabbedModulePane createTabbedPane() {
+                tabbedModulePane = new TabbedModulePanePiccolo();
+                return tabbedModulePane;
+            }
+        };
+        setTabbedPaneType( tabbedPaneType );
+    }
+    
+    /*
+     * Initializes the modules.
+     */
+    private void initModules() {
+        
+        Frame parentFrame = getPhetFrame();
+
+        exampleModule = getFirstModule(parentFrame);
+        addModule( exampleModule );
+        
+        Module secondModule = new NeuronModule( parentFrame );
+        secondModule.setName( "Another Example" );
+        addModule( secondModule );
+    }
+
+    protected NeuronModule getFirstModule(Frame parentFrame) {
+        return new NeuronModule( parentFrame );
+    }
+
+    /*
+     * Initializes the menubar.
+     */
+    private void initMenubar( String[] args ) {
+
+        // File->Save/Load
+        final PhetFrame frame = getPhetFrame();
+        frame.addFileSaveLoadMenuItems();
+        if ( persistenceManager == null ) {
+            persistenceManager = new XMLPersistenceManager( frame );
+        }
+
+        // Options menu
+        OptionsMenu optionsMenu = new OptionsMenu();
+        // add menu items here, or in a subclass on OptionsMenu
+        if ( optionsMenu.getMenuComponentCount() > 0 ) {
+            frame.addMenu( optionsMenu );
+        }
+
+        // Developer menu
+        DeveloperMenu developerMenu = new DeveloperMenu( this );
+        if ( developerMenu.getMenuComponentCount() > 0 && isDeveloperControlsEnabled() ) {
+            frame.addMenu( developerMenu );
+        }
+    }
+
+    //----------------------------------------------------------------------------
+    // Setters & getters
+    //----------------------------------------------------------------------------
+
+    public void setControlPanelBackground( Color color ) {
+        Module[] modules = getModules();
+        for ( int i = 0; i < modules.length; i++ ) {
+            modules[i].setControlPanelBackground( color );
+            modules[i].setClockControlPanelBackground( color );
+            modules[i].setHelpPanelBackground( color );
+        }
+    }
+
+    public Color getControlPanelBackground() {
+        return getModule( 0 ).getControlPanel().getBackground();
+    }
+
+    public PhetTabbedPane getTabbedPane() {
+        return tabbedModulePane;
+    }
+
+    //----------------------------------------------------------------------------
+    // main
+    //----------------------------------------------------------------------------
+
+    public static void main( final String[] args ) throws ClassNotFoundException {
+        /* 
+         * If you want to customize your application (look-&-feel, window size, etc) 
+         * create your own PhetApplicationConfig and use one of the other launchSim methods
+         */
+        new PhetApplicationLauncher().launchSim( args, NeuronConstants.PROJECT_NAME, NeuronApplication.class );
+    }
+}
---
+++ trunk/simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/controlpanel/NeuronControlPanel.java
@@ -0,0 +1,58 @@
+/* Copyright 2007-2008, University of Colorado */
+
+package edu.colorado.phet.neuron.controlpanel;
+
+import java.awt.Frame;
+
+import edu.colorado.phet.common.phetcommon.view.ControlPanel;
+import edu.colorado.phet.neuron.NeuronResources;
+import edu.colorado.phet.neuron.model.NeuronModel;
+import edu.colorado.phet.neuron.module.NeuronModule;
+
+/**
+ * ExampleControlPanel is the control panel for ExampleModule.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
+public class NeuronControlPanel extends ControlPanel {
+
+    //----------------------------------------------------------------------------
+    // Instance data
+    //----------------------------------------------------------------------------
+    
+    //----------------------------------------------------------------------------
+    // Constructors
+    //----------------------------------------------------------------------------
+    
+    /**
+     * Constructor.
+     * 
+     * @param module
+     * @param parentFrame parent frame, for creating dialogs
+     */
+    public NeuronControlPanel( NeuronModule module, Frame parentFrame, NeuronModel model ) {
+        super();
+        
+        // Set the control panel's minimum width.
+        int minimumWidth = NeuronResources.getInt( "int.minControlPanelWidth", 215 );
+        setMinimumWidth( minimumWidth );
+        
+        // Layout
+        {
+            addResetAllButton( module );
+        }
+    }
+    
+    //----------------------------------------------------------------------------
+    // Setters and getters
+    //----------------------------------------------------------------------------
+    
+    public void closeAllDialogs() {
+        //XXX close any dialogs created via the control panel
+    }
+    
+    //----------------------------------------------------------------------------
+    // Access to subpanels
+    //----------------------------------------------------------------------------
+    
+}
