65a93f4b446361dfdcca21cee2c0af703423c14b
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-08-26 19:23:08 +0000

    add flags and parameters for various root-finding methods

--- simulations-java/simulations/titration/src/edu/colorado/phet/titration/prototype/TPModel.java
+++ simulations-java/simulations/titration/src/edu/colorado/phet/titration/prototype/TPModel.java
@@ -13,35 +13,18 @@ import edu.colorado.phet.titration.test.RootTest;
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
 public class TPModel {
-
+    
+    private static final boolean ROOTS_DURRAND_KERNER = false;
+    private static final boolean ROOTS_OPTIMIZED = false;
+    private static final int ROOTS_ITERATIONS = 300;
+    private static final double ROOTS_THRESHOLD = 1E-40;
+    
     private static final double Kw = TPConstants.Kw;
-    private static final int ROOTS_ITERATIONS = 30;
-    private static final double ROOTS_THRESHOLD = 0.00001;
 
     private TPModel() {}
 
-    /*
-    ## strong base/strong acid
-
-    function strongbase (Ca,Cb,Va,Vb)
-    Kw = 1e-14;
-
-    ## solve quadratic eqn for H: a*H^2 + b*H + c = 0;
-    a = -(Va + Vb);
-    b = Ca.*Va - Cb.*Vb;
-    c = Kw.*(Va + Vb);
-    #H1 = (-b + sqrt(b.^2 - 4.*a.*c))./(2.*a);
-    H2 = (-b - sqrt(b.^2 - 4.*a.*c))./(2.*a);
-
-    ## find pH at each Va value
-    #pH1 = -log10(H1);
-    pH2 = -log10(H2);
-
-    ## plot pH versus Va
-    #plot(Va,pH1);
-    plot(Va,pH2); axis ([0,75,0,14]); xlabel('Va'); ylabel('pH');
-
-    endfunction
+    /**
+     * Strong base titrated with a strong acid.
      */
     public static final double strongBase( double Ca, double Cb, double Va, double Vb ) {
         double a = -( Va + Vb );
@@ -52,37 +35,8 @@ public class TPModel {
         return pH2;
     }
 
-    /*
-     ## weak base/strong acid
-
-    function weakbase (Ca,Cb,Va,Vb,Kb)
-    Kw = 1e-14;
-
-    ## solve cubic eqn for H: a*H^3 + b*H^2 + c*H + d = 0
-    a = -(Va + Vb);
-    b = Ca.*Va - Cb.*Vb - (Kw./Kb).*(Va + Vb);
-    c = Kw.*(Va + Vb) + (Ca.*Va.*Kw)./Kb;
-    d = ((Kw.^2)./Kb).*(Va + Vb);
-
-    for indx = 1:length(Va)
-    x = roots([a(indx), b(indx), c(indx), d(indx)]);
-    x = sort(x);
-    H1(indx) = x(1);
-    H2(indx) = x(2);
-    H3(indx) = x(3);
-    end
-
-    ## find pH at each Va value
-    #pH1 = -log10(H1);
-    #pH2 = -log10(H2);
-    pH3 = -log10(H3);
-
-    ## plot pH versus Va
-    #plot(Va,pH1);
-    #plot(Va,pH2);
-    plot(Va,pH3); axis ([0,75,0,14]); xlabel('Va'); ylabel('pH');
-
-    endfunction
+    /**
+     * Weak base titrated with a strong acid.
      */
     public static double weakBase( double Ca, double Cb, double Va, double Vb, double Kb ) {
         double a = -( Va + Vb );
@@ -90,31 +44,12 @@ public class TPModel {
         double c = Kw * ( Va + Vb ) + ( Ca * Va * Kw ) / Kb;
         double d = ( ( Kw * Kw ) / Kb ) * ( Va + Vb );
         double[] coefficients = new double[] { a, b, c, d }; // high to low
-        return pH( coefficients );
+        double pH = pH( coefficients );
+        return pH;
     }
 
-    /*
-    ## strong acid/strong base
-
-    function strongacid (Ca,Cb,Va,Vb)
-    Kw = 1e-14;
-
-    ## solve quadratic eqn for H: a*H^2 + b*H + c = 0;
-    a = Va + Vb;
-    b = Cb.*Vb - Ca.*Va;
-    c = -Kw.*(Va + Vb);
-    H1 = (-b + sqrt(b.^2 - 4.*a.*c))./(2.*a);
-    #H2 = (-b - sqrt(b.^2 - 4.*a.*c))./(2.*a);
-
-    ## find pH at each Vb value
-    pH1 = -log10(H1);
-    #pH2 = -log10(H2);
-
-    ## plot pH versus Vb
-    plot(Vb,pH1); axis ([0,75,0,14]); xlabel('Vb'); ylabel('pH');
-    #plot(Vb,pH2);
-
-    endfunction
+    /**
+     * Strong acid titrated with a strong base.
      */
     public static final double strongAcid( double Ca, double Cb, double Va, double Vb ) {
         double a = Va + Vb;
@@ -125,45 +60,17 @@ public class TPModel {
         return pH1;
     }
 
-    /*
-    ## weak acid/strong base
-
-    function weakacid (Ca,Cb,Va,Vb,Ka)
-    Kw = 1e-14;
-
-    ## solve cubic eqn for H: a*H^3 + b*H^2 + c*H + d = 0
-    a = Va + Vb;
-    b = Cb.*Vb + Va.*Ka + Vb.*Ka;
-    c = Cb.*Vb.*Ka - Ca.*Va.*Ka - Va.*Kw - Vb.*Kw;
-    d = -Kw.*Ka.*(Va + Vb);
-
-    for indx = 1:length(Vb)
-    x = roots([a(indx), b(indx), c(indx), d(indx)]);
-    x = sort(x);
-    H1(indx) = x(1);
-    H2(indx) = x(2);
-    H3(indx) = x(3);
-    end
-
-    ## find pH at each Vb value
-    #pH1 = -log10(H1);
-    #pH2 = -log10(H2);
-    pH3 = -log10(H3);
-
-    ## plot pH versus Vb
-    #plot(Vb,pH1);
-    #plot(Vb,pH2);
-    plot(Vb,pH3); axis ([0,75,0,14]); xlabel('Vb'); ylabel('pH');
-
-    endfunction
-    */
+    /**
+     * Weak acid titrated with a strong base.
+     */
     public static final double weakAcid( double Ca, double Cb, double Va, double Vb, double Ka ) {
         double a = Va + Vb;
         double b = Cb * Vb + Va * Ka + Vb * Ka;
         double c = Cb * Vb * Ka - Ca * Va * Ka - Va * Kw - Vb * Kw;
         double d = -Kw * Ka * ( Va + Vb );
         double[] coefficients = new double[] { a, b, c, d }; // high to low
-        return pH( coefficients );
+        double pH = pH( coefficients );
+        return pH;
     }
 
     /*
@@ -180,24 +87,15 @@ public class TPModel {
     e = -(K1.*K2.*Kw).*(Va + Vb);
 
     for indx = 1:length(Vb)
-    x = roots([a(indx), b(indx), c(indx), d(indx), e(indx)]);
-    x = sort(x);
-    H1(indx) = x(1);
-    H2(indx) = x(2);
-    H3(indx) = x(3);
-    H4(indx) = x(4);
+        x = roots([a(indx), b(indx), c(indx), d(indx), e(indx)]);
+        x = sort(x);
+        H4(indx) = x(4);
     end
 
     ## find pH at each Vb value
-    #pH1 = -log10(H1);
-    #pH2 = -log10(H2);
-    #pH3 = -log10(H3);
     pH4 = -log10(H4);
 
     ## plot pH versus Vb
-    #plot(Vb,pH1);
-    #plot(Vb,pH2);
-    #plot(Vb,pH3);
     plot(Vb,pH4); axis ([0,75,0,14]); xlabel('Vb'); ylabel('pH');
 
     endfunction
@@ -227,13 +125,9 @@ public class TPModel {
     f = -Va.*K1.*K2.*K3.*Kw - Vb.*K1.*K2.*K3.*Kw;
 
     for indx = 1:length(Vb)
-    x = roots([a(indx), b(indx), c(indx), d(indx), e(indx), f(indx)]);
-    x = sort(x);
-    H1(indx) = x(1);
-    H2(indx) = x(2);
-    H3(indx) = x(3);
-    H4(indx) = x(4);
-    H5(indx) = x(5);
+        x = roots([a(indx), b(indx), c(indx), d(indx), e(indx), f(indx)]);
+        x = sort(x);
+        H5(indx) = x(5);
     end
 
     ## find pH at each Vb value
@@ -280,13 +174,51 @@ public class TPModel {
      * find the roots, sort them, and then calculate the pH based on the Nth root. 
      */
     private static final double pH( double[] coefficients ) {
+        Complex[] roots = roots( coefficients );
+        Double[] rootsSorted = sortReal( roots );
+        double H = rootsSorted[coefficients.length - 2];
+        return -Math.log10( H );
+    }
+    
+    private static Complex[] roots( double[] coefficients ) {
         Complex[] polynomial = new Complex[coefficients.length];
         for ( int i = 0; i < coefficients.length; i++ ) {
             polynomial[i] = new Complex( coefficients[i], 0 );
         }
-        Complex[] roots = RootTest.findRootsOptimizedDurandKerner( polynomial, ROOTS_ITERATIONS, ROOTS_THRESHOLD );
-        Double[] rootsSorted = sortReal( roots );
-        double H = rootsSorted[coefficients.length - 2];
-        return -Math.log10( H );
+        Complex[] roots = null;
+        if ( ROOTS_DURRAND_KERNER ) {
+            if ( ROOTS_OPTIMIZED ) {
+                roots = RootTest.findRootsOptimizedDurandKerner( polynomial, ROOTS_ITERATIONS, ROOTS_THRESHOLD );
+            }
+            else {
+                roots = RootTest.findRootsDurandKerner( polynomial, ROOTS_ITERATIONS );
+            }
+        }
+        else {
+            roots = RootTest.findRootsLaguerre( polynomial, ROOTS_ITERATIONS );
+        }
+        return roots;
+    }
+    
+    private static void debugPrintCoefficients( double[] coefficients ) {
+        System.out.print( "coefficients:" );
+        for ( int i = 0; i < coefficients.length; i++ ) {
+            System.out.print( " " + coefficients[i] );
+        }
+        System.out.println();
+    }
+    
+    private static void debugPrintRealRoots( Complex[] roots ) {
+        System.out.print( "roots:" );
+        for ( int i = 0; i < roots.length; i++ ) {
+            System.out.print( " " + roots[i].getReal() );
+        }
+        System.out.println();
+    }
+    
+    public static void main( String[] args ) {
+        TPModel.weakBase( 0.1, 0.1, 10, 25, 0.1 );
+        double[] coeff = new double[] { 4, 3,2,1 };
+        pH( coeff );
     }
 }
