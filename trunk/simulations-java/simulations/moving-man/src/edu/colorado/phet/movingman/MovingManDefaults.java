04ba72fe7b3c8b8d14047ef2f55b333ace5df47f
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-08-26 20:48:38 +0000

    Changed v-driven acceleration window from 4 to 3

--- trunk/simulations-java/simulations/moving-man/src/edu/colorado/phet/movingman/MovingManDefaults.java
+++ trunk/simulations-java/simulations/moving-man/src/edu/colorado/phet/movingman/MovingManDefaults.java
@@ -3,5 +3,5 @@ package edu.colorado.phet.movingman;
 public class MovingManDefaults {
     public static final int SMOOTH_XV = 4;
     public static final int SMOOTH_XA = 4;
-    public static final int SMOOTH_VA = 4;
+    public static final int SMOOTH_VA = 3;
 }
