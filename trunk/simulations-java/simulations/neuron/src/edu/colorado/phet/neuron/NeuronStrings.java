40812e717eb7bba9a36952f09e3cd5278868dfa2
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-08-27 19:43:37 +0000

    Cleanup.

--- trunk/simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/NeuronStrings.java
+++ trunk/simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/NeuronStrings.java
@@ -1,20 +1,21 @@
-/* Copyright 2008, University of Colorado */
+/* Copyright 2009, University of Colorado */
 
 package edu.colorado.phet.neuron;
 
-import edu.colorado.phet.common.phetcommon.resources.PhetCommonResources;
+import static edu.colorado.phet.neuron.NeuronResources.getString;
 
 /**
- * TemplateStrings is the collection of localized strings used by this simulations.
- * We load all strings as statics so that we will be warned at startup time of any missing strings.
+ * NeuronStrings is the collection of localized strings used by this
+ * simulation. * We load all strings as statics so that we will be warned at
+ * startup time of any missing strings.
  *
- * @author Chris Malley (cmalley@pixelzoom.com)
+ * @author John Blanco
  */
 public class NeuronStrings {
     
     /* not intended for instantiation */
     private NeuronStrings() {}
     
-    public static final String TITLE_MEMBRANE_DIFFUSION_MODULE = NeuronResources.getString( "ModuleTitle.MembraneDiffusionModule" );
-    public static final String TITLE_GATED_CHANNELS_MODULE = NeuronResources.getString( "ModuleTitle.GatedChannels" );
+    public static final String TITLE_MEMBRANE_DIFFUSION_MODULE = getString( "ModuleTitle.MembraneDiffusionModule" );
+    public static final String TITLE_GATED_CHANNELS_MODULE = getString( "ModuleTitle.GatedChannels" );
 }
