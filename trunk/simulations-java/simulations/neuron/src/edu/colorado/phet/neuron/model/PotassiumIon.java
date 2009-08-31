b2e06170bb7b20ebffb3fb09c1c6f7d1b8799e9f
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-08-31 18:56:00 +0000

    Modified to use translatable resources for name.

--- trunk/simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/model/PotassiumIon.java
+++ trunk/simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/model/PotassiumIon.java
@@ -2,17 +2,22 @@ package edu.colorado.phet.neuron.model;
 
 import java.awt.Color;
 
+import edu.colorado.phet.neuron.NeuronStrings;
+
 public class PotassiumIon extends Atom {
 
 	@Override
 	public String getChemicalSymbol() {
-		return null;
+		return NeuronStrings.POTASSIUM_CHEMICAL_SYMBOL;
 	}
 
 	@Override
 	public Color getRepresentationColor() {
-		// TODO Auto-generated method stub
-		return null;
+		return Color.RED;
 	}
 
+	@Override
+	public int getCharge() {
+		return -1;
+	}
 }
