3a2baee8986f5cdc10db6a036e88a89f8928cabe
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-08-31 18:47:26 +0000

    Added strings for Potassion and Sodium chemical symbols as well as ion symbols.

--- trunk/simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/NeuronStrings.java
+++ trunk/simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/NeuronStrings.java
@@ -18,4 +18,11 @@ public class NeuronStrings {
     
     public static final String TITLE_MEMBRANE_DIFFUSION_MODULE = getString( "ModuleTitle.MembraneDiffusionModule" );
     public static final String TITLE_GATED_CHANNELS_MODULE = getString( "ModuleTitle.GatedChannels" );
+
+    public static final String POTASSIUM_CHEMICAL_SYMBOL = getString( "PotassiumChemicalSymbol" );
+    public static final String SODIUM_CHEMICAL_SYMBOL = getString( "SodiumChemicalSymbol" );
+    public static final String POSITIVE_ION_SYMBOL = getString( "PositiveIonSymbol" );
+    public static final String NEGATIVE_ION_SYMBOL = getString( "NegativeIonSymbol" );
+    
+    
 }
