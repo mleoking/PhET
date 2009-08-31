aa8bafd7dd8d587ddd4186b0600596c8652816ee
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-08-31 21:17:43 +0000

    rename package therampscala => theramp

---
+++ simulations-java/simulations/the-ramp/scala-src/edu/colorado/phet/theramp/RampApplication.scala
@@ -0,0 +1,152 @@
+package edu.colorado.phet.theramp
+
+import charts.bargraphs.{WorkEnergyChartModel, WorkEnergyChart}
+import charts.RampChartNode
+import common.phetcommon.application.{PhetApplicationLauncher, Module, PhetApplicationConfig}
+import common.piccolophet.{PiccoloPhetApplication}
+import graphics.RampCanvas
+import java.awt.event.{ActionEvent, ActionListener}
+import java.awt.{Color}
+import javax.swing._
+import model._
+import controls.RampControlPanel
+import robotmovingcompany.{RobotMovingCompanyGameModel, Result, RobotMovingCompanyCanvas}
+import scalacommon.record.{RecordModelControlPanel, PlaybackSpeedSlider}
+
+import scalacommon.ScalaClock
+
+//TODO: improve inheritance/composition scheme for different applications/modules/canvases/models
+class AbstractRampModule(frame: JFrame, clock: ScalaClock, name: String, defaultBeadPosition: Double, pausedOnReset: Boolean,
+                         initialAngle: Double) extends Module(name, clock) {
+  val rampModel = new RampModel(defaultBeadPosition, pausedOnReset, initialAngle)
+  val wordModel = new WordModel
+  val fbdModel = new FreeBodyDiagramModel
+  val coordinateSystemModel = new AdjustableCoordinateModel
+  val vectorViewModel = new VectorViewModel
+  coordinateSystemModel.addListenerByName(if (coordinateSystemModel.fixed) rampModel.coordinateFrameModel.angle = 0)
+  private var lastTickTime = System.currentTimeMillis
+
+  //This clock is always running; pausing just pauses the physics
+  clock.addClockListener(dt => {
+    val paintAndInputTime = System.currentTimeMillis - lastTickTime
+
+    val startTime = System.currentTimeMillis
+    rampModel.update(dt)
+    RepaintManager.currentManager(getSimulationPanel).paintDirtyRegions()
+    val modelTime = System.currentTimeMillis - startTime
+
+    val elapsed = paintAndInputTime + modelTime
+    if (elapsed < 25) {
+      val toSleep = 25 - elapsed
+      //      println("had excess time, sleeping: " + toSleep)
+      Thread.sleep(toSleep) //todo: blocks swing event handler thread and paint thread, should run this clock loop in another thread
+    }
+    lastTickTime = System.currentTimeMillis
+  })
+
+  //pause on start/reset, and unpause (and start recording) when the user applies a force
+  rampModel.setPaused(true)
+
+  def resetRampModule(): Unit = {
+    rampModel.resetAll()
+    wordModel.resetAll()
+    fbdModel.resetAll()
+    coordinateSystemModel.resetAll()
+    vectorViewModel.resetAll()
+    //pause on startup/reset, and unpause (and start recording) when the user applies a force 
+    rampModel.setPaused(true)
+    resetAll()
+  }
+
+  def resetAll() = {}
+}
+
+class BasicRampModule(frame: JFrame, clock: ScalaClock, name: String,
+                      coordinateSystemFeaturesEnabled: Boolean, useObjectComboBox: Boolean, showAppliedForceSlider: Boolean,
+                      defaultBeadPosition: Double, pausedOnReset: Boolean, initialAngle: Double, modelOffsetY: Double)
+        extends AbstractRampModule(frame, clock, name, defaultBeadPosition, pausedOnReset, initialAngle) {
+  val rampCanvas = new RampCanvas(rampModel, coordinateSystemModel, fbdModel, vectorViewModel, frame, !useObjectComboBox, showAppliedForceSlider, initialAngle != 0.0, modelOffsetY)
+  setSimulationPanel(rampCanvas)
+  val rampControlPanel = new RampControlPanel(rampModel, wordModel, fbdModel, coordinateSystemModel, vectorViewModel,
+    resetRampModule, coordinateSystemFeaturesEnabled, useObjectComboBox, rampModel, true, true)
+  setControlPanel(rampControlPanel)
+  setClockControlPanel(new RecordModelControlPanel(rampModel, rampCanvas, () => new PlaybackSpeedSlider(rampModel), Color.blue, 20))
+}
+
+import RampResources._
+
+class IntroRampModule(frame: JFrame, clock: ScalaClock) extends BasicRampModule(frame, clock, "module.introduction".translate, false, false, true, -6, false, RampDefaults.defaultRampAngle, 0.0)
+
+class CoordinatesRampModule(frame: JFrame, clock: ScalaClock) extends BasicRampModule(frame, clock, "module.coordinates".translate, true, false, true, -6, false, RampDefaults.defaultRampAngle, 0.0) {
+  coordinateSystemModel.adjustable = true
+}
+
+class ForceGraphsModule(frame: JFrame, clock: ScalaClock) extends GraphingModule(frame, clock, "module.force-graphs".translate, false, 0.0)
+
+class GraphingModule(frame: JFrame, clock: ScalaClock, name: String, showEnergyGraph: Boolean, modelOffsetY: Double) extends BasicRampModule(frame, clock, name, false, true, false, -6, true, RampDefaults.defaultRampAngle, modelOffsetY) {
+  coordinateSystemModel.adjustable = false
+  rampCanvas.addNodeAfter(rampCanvas.earthNode, new RampChartNode(rampCanvas.transform, rampCanvas, rampModel, showEnergyGraph))
+}
+
+class WorkEnergyModule(frame: JFrame, clock: ScalaClock) extends GraphingModule(frame, clock, "module.work-energy".translate, true, 100.0) {
+  val workEnergyChartModel = new WorkEnergyChartModel
+  val workEnergyChartButton = new JButton("controls.showWorkEnergyCharts".translate)
+  workEnergyChartButton.addActionListener(new ActionListener() {
+    def actionPerformed(e: ActionEvent) = {workEnergyChartModel.visible = true}
+  })
+  rampControlPanel.addToBody(workEnergyChartButton)
+  val workEnergyChart = new WorkEnergyChart(workEnergyChartModel, rampModel, frame)
+
+  override def resetAll() = {super.reset(); workEnergyChartModel.reset()}
+}
+
+class RobotMovingCompanyModule(frame: JFrame, clock: ScalaClock)
+        extends AbstractRampModule(frame, clock, "module.robotMovingCompany".translate, 5, false, RampDefaults.defaultRampAngle) {
+  val gameModel = new RobotMovingCompanyGameModel(rampModel, clock)
+
+  gameModel.itemFinishedListeners += ((scalaRampObject, result) => {
+    val audioClip = result match {
+      case Result(_, true, _, _) => Some("smash0.wav".literal)
+      case Result(true, false, _, _) => Some("tintagel/DIAMOND.WAV".literal)
+      case Result(false, false, _, _) => Some("tintagel/PERSONAL.WAV".literal)
+      case _ => None
+    }
+    if (!audioClip.isEmpty) RampResources.getAudioClip(audioClip.get).play()
+  })
+
+  val canvas = new RobotMovingCompanyCanvas(rampModel, coordinateSystemModel, fbdModel, vectorViewModel, frame, gameModel)
+
+  setSimulationPanel(canvas)
+  setLogoPanelVisible(false)
+}
+
+class RampApplication(config: PhetApplicationConfig) extends PiccoloPhetApplication(config) {
+  def newClock = new ScalaClock(RampDefaults.DELAY, RampDefaults.DT_DEFAULT)
+  addModule(new IntroRampModule(getPhetFrame, newClock))
+  addModule(new CoordinatesRampModule(getPhetFrame, newClock))
+  addModule(new ForceGraphsModule(getPhetFrame, newClock))
+  addModule(new WorkEnergyModule(getPhetFrame, newClock))
+  addModule(new RobotMovingCompanyModule(getPhetFrame, newClock))
+}
+class RampWorkEnergyApplication(config: PhetApplicationConfig) extends PiccoloPhetApplication(config) {
+  def newClock = new ScalaClock(RampDefaults.DELAY, RampDefaults.DT_DEFAULT)
+  addModule(new WorkEnergyModule(getPhetFrame, newClock))
+}
+
+class RobotMovingCompanyApplication(config: PhetApplicationConfig) extends PiccoloPhetApplication(config) {
+  addModule(new RobotMovingCompanyModule(getPhetFrame, new ScalaClock(RampDefaults.DELAY, RampDefaults.DT_DEFAULT)))
+}
+
+//Current IntelliJ plugin has trouble finding main for classes with a companion object, so we use a different name 
+object RampApplicationMain {
+  def main(args: Array[String]) = new PhetApplicationLauncher().launchSim(args, "the-ramp".literal, classOf[RampApplication])
+}
+
+object RampWorkEnergyApplicationMain {
+  def main(args: Array[String]) = new PhetApplicationLauncher().launchSim(args, "the-ramp".literal, classOf[RampWorkEnergyApplication])
+}
+
+object RobotMovingCompanyApplicationMain {
+  def main(args: Array[String]) = new PhetApplicationLauncher().launchSim(args, "the-ramp".literal, "robot-moving-company".literal, classOf[RobotMovingCompanyApplication])
+}
+
---
+++ simulations-java/simulations/the-ramp/scala-src/edu/colorado/phet/theramp/graphics/FireDogView.scala
@@ -0,0 +1,24 @@
+package edu.colorado.phet.theramp.graphics
+
+import model.{FireDog, Raindrop, RampModel}
+import umd.cs.piccolo.PNode
+import RampResources._
+
+//todo: factor out common code
+class FireDogView(rampModel: RampModel, canvas: AbstractRampCanvas) extends PNode {
+  rampModel.fireDogAddedListeners += ((added: FireDog) => {
+    val node = new BeadNode(added.dogbead, canvas.transform, "firedog.gif".literal)
+    addChild(node)
+
+    added.removedListeners += (() => removeChild(node)) //eleganter than ever
+  })
+}
+
+class RaindropView(rampModel: RampModel, canvas: AbstractRampCanvas) extends PNode {
+  rampModel.raindropAddedListeners += ((added: Raindrop) => {
+    val node = new BeadNode(added.rainbead, canvas.transform, "raindrop.png".literal)
+    addChild(node)
+
+    added.removedListeners += (() => removeChild(node))
+  })
+}
