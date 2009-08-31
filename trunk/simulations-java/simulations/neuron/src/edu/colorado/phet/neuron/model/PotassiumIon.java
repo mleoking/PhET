3a7d2e93373a68d679d92424965a961d8a0b7850
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-08-31 18:48:37 +0000

    New model class for potassium ions.

---
+++ trunk/simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/model/PotassiumIon.java
@@ -0,0 +1,18 @@
+package edu.colorado.phet.neuron.model;
+
+import java.awt.Color;
+
+public class PotassiumIon extends Atom {
+
+	@Override
+	public String getChemicalSymbol() {
+		return null;
+	}
+
+	@Override
+	public Color getRepresentationColor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
