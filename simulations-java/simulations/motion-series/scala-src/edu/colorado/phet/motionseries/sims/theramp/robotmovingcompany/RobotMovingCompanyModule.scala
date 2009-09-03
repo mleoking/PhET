52d89a3685465aee6c92be352f0e0d3d4b6a105b
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-09-03 17:37:27 +0000

    refactor modules

---
+++ simulations-java/simulations/motion-series/scala-src/edu/colorado/phet/motionseries/sims/theramp/robotmovingcompany/RobotMovingCompanyModule.scala
@@ -0,0 +1,44 @@
+package edu.colorado.phet.motionseries.sims.theramp.robotmovingcompany
+
+
+import javax.swing.JFrame
+import model.MotionSeriesModel
+import scalacommon.ScalaClock
+
+class RobotMovingCompanyModule(frame: JFrame, clock: ScalaClock)
+        extends AbstractRampModule(frame, clock, "module.robotMovingCompany".translate, 5, false, MotionSeriesDefaults.defaultRampAngle) {
+  override def reset() = {
+    super.reset()
+    rampModel.frictionless = false
+  }
+
+  override def resetAll() = {
+    super.resetAll()
+    rampModel.frictionless = false
+  }
+
+  override def createRampModel(defaultBeadPosition: Double, pausedOnReset: Boolean, initialAngle: Double) = {
+    new MotionSeriesModel(defaultBeadPosition, pausedOnReset, initialAngle) {
+      override def updateSegmentLengths() = setSegmentLengths(rampLength, rampLength)
+      frictionless = false
+    }
+
+  }
+
+  val gameModel = new RobotMovingCompanyGameModel(rampModel, clock)
+
+  gameModel.itemFinishedListeners += ((scalaRampObject, result) => {
+    val audioClip = result match {
+      case Result(_, true, _, _) => Some("smash0.wav".literal)
+      case Result(true, false, _, _) => Some("tintagel/DIAMOND.WAV".literal)
+      case Result(false, false, _, _) => Some("tintagel/PERSONAL.WAV".literal)
+      case _ => None
+    }
+    if (!audioClip.isEmpty) MotionSeriesResources.getAudioClip(audioClip.get).play()
+  })
+
+  val canvas = new RobotMovingCompanyCanvas(rampModel, coordinateSystemModel, fbdModel, vectorViewModel, frame, gameModel)
+
+  setSimulationPanel(canvas)
+  setLogoPanelVisible(false)
+}
