2a4e9c7cfa566929104454749ddf6e88698ad756
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-09-03 16:19:03 +0000

    refactor conditional => subclasses

--- simulations-java/simulations/motion-series/scala-src/edu/colorado/phet/motionseries/sims/theramp/RampChartNode.scala
+++ simulations-java/simulations/motion-series/scala-src/edu/colorado/phet/motionseries/sims/theramp/RampChartNode.scala
@@ -6,13 +6,22 @@ import phet.common.motion.graphs._
 import model.{RampModel}
 import RampResources._
 
-class RampChartNode(canvas: MotionSeriesCanvas, model: RampModel, showEnergyGraph: Boolean)
-        extends MotionSeriesChartNode(canvas, model) {
+class RampForceEnergyChartNode(canvas: MotionSeriesCanvas, model: RampModel) extends MotionSeriesChartNode(canvas, model) {
   val parallelForcesString = "forces.parallel-title".translate
-  val graphs = if (showEnergyGraph) Array(new MinimizableControlGraph(parallelForcesString, forceGraph),
+  val graphs = Array(new MinimizableControlGraph(parallelForcesString, forceGraph),
     new MinimizableControlGraph("forces.work-energy-title".translate, energyGraph))
-  else Array(new MinimizableControlGraph(parallelForcesString, forceGraph))
+  val graphSetNode = new GraphSetNode(new GraphSetModel(new GraphSuite(graphs))) {
+    override def getMaxAvailableHeight(availableHeight: Double) = availableHeight
+    setAlignedLayout()
+  }
 
+  addChild(graphSetNode)
+  updateLayout()
+}
+
+class RampForceChartNode(canvas: MotionSeriesCanvas, model: RampModel) extends MotionSeriesChartNode(canvas, model) {
+  val parallelForcesString = "forces.parallel-title".translate
+  val graphs = Array(new MinimizableControlGraph(parallelForcesString, forceGraph))
   val graphSetNode = new GraphSetNode(new GraphSetModel(new GraphSuite(graphs))) {
     override def getMaxAvailableHeight(availableHeight: Double) = availableHeight
     setAlignedLayout()
