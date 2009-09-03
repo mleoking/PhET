b0a875505cd32d3fd36d90e0d0b0e78f3c3b0923
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-09-03 20:16:05 +0000

    Wicket-test: internationalized about - sponsors and contribute

--- trunk/web/wicket-test/src/java/edu/colorado/phet/wickettest/translation/entities/SponsorsEntity.java
+++ trunk/web/wicket-test/src/java/edu/colorado/phet/wickettest/translation/entities/SponsorsEntity.java
@@ -1,5 +1,6 @@
 package edu.colorado.phet.wickettest.translation.entities;
 
+import edu.colorado.phet.wickettest.content.about.AboutSponsors;
 import edu.colorado.phet.wickettest.panels.PhetPanel;
 import edu.colorado.phet.wickettest.panels.SponsorsPanel;
 import edu.colorado.phet.wickettest.translation.PhetPanelFactory;
@@ -13,11 +14,34 @@ public class SponsorsEntity extends TranslationEntity {
         addString( "sponsors.nsf" );
         addString( "sponsors.ksu" );
         addString( "sponsors.otherSponsors" );
+
+        addString( "sponsors.header" );
+        addString( "sponsors.financialSupport" );
+        addString( "sponsors.platinum" );
+        addString( "sponsors.gold" );
+        addString( "sponsors.bronze" );
+        addString( "sponsors.technicalSupport" );
+        addString( "sponsors.piccolo.desc" );
+        addString( "sponsors.jfreechart.desc" );
+        addString( "sponsors.jade.desc" );
+        addString( "sponsors.sourceforge.desc" );
+        addString( "sponsors.proguard.desc" );
+        addString( "sponsors.jetbrains.desc" );
+        addString( "sponsors.ej.desc" );
+        addString( "sponsors.bitrock.desc" );
+        addString( "sponsors.royalinteractive.desc" );
+        addString( "sponsors.dynamicalSystems.desc" );
+
         addPreview( new PhetPanelFactory() {
             public PhetPanel getNewPanel( String id, PageContext context, PhetRequestCycle requestCycle ) {
                 return new SponsorsPanel( id, context );
             }
         }, "Sponsors Panel" );
+        addPreview( new PhetPanelFactory() {
+            public PhetPanel getNewPanel( String id, PageContext context, PhetRequestCycle requestCycle ) {
+                return new AboutSponsors( id, context );
+            }
+        }, "Sponsors Page" );
     }
 
     public String getDisplayName() {
