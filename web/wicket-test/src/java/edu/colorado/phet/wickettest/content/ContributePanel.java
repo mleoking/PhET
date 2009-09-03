af322c3caf8e65bebd5f4487b2d18b9f21b2b12e
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-09-03 20:16:05 +0000

    Wicket-test: internationalized about - sponsors and contribute

--- web/wicket-test/src/java/edu/colorado/phet/wickettest/content/ContributePanel.java
+++ web/wicket-test/src/java/edu/colorado/phet/wickettest/content/ContributePanel.java
@@ -2,6 +2,7 @@ package edu.colorado.phet.wickettest.content;
 
 import org.apache.wicket.markup.html.link.Link;
 
+import edu.colorado.phet.wickettest.components.LocalizedText;
 import edu.colorado.phet.wickettest.components.PhetLink;
 import edu.colorado.phet.wickettest.panels.PhetPanel;
 import edu.colorado.phet.wickettest.util.Linkable;
@@ -11,6 +12,14 @@ public class ContributePanel extends PhetPanel {
     public ContributePanel( String id, PageContext context ) {
         super( id, context );
 
+        add( new LocalizedText( "contribute-main", "contribute.main", new Object[]{
+                "<a href=\"mailto:phethelp@colorado.edu?Subject=Financial%20Contribution&amp;Body=Dear%20Sir%20or%20Madam:%20I%20would%20like%20to%20make%20a%20generous%20donation%20to%20PhET.\">phethelp@colorado.edu</a>"
+        } ) );
+
+        add( new LocalizedText( "contribute-thanks", "contribute.thanks", new Object[]{
+                "href=\"/about/sponsors\"",
+                "href=\"http://www.royalinteractive.com/\""
+        }));
     }
 
     public static String getKey() {
---
+++ web/wicket-test/src/java/edu/colorado/phet/wickettest/translation/entities/ContributeEntity.java
@@ -0,0 +1,25 @@
+package edu.colorado.phet.wickettest.translation.entities;
+
+import edu.colorado.phet.wickettest.content.IndexPanel;
+import edu.colorado.phet.wickettest.content.ContributePanel;
+import edu.colorado.phet.wickettest.panels.PhetPanel;
+import edu.colorado.phet.wickettest.translation.PhetPanelFactory;
+import edu.colorado.phet.wickettest.util.PageContext;
+import edu.colorado.phet.wickettest.util.PhetRequestCycle;
+
+public class ContributeEntity extends TranslationEntity {
+    public ContributeEntity() {
+        addString( "contribute.financialContributions" );
+        addString( "contribute.main" );
+        addString( "contribute.thanks" );
+        addPreview( new PhetPanelFactory() {
+            public PhetPanel getNewPanel( String id, PageContext context, PhetRequestCycle requestCycle ) {
+                return new ContributePanel( id, context );
+            }
+        }, "Contribute" );
+    }
+
+    public String getDisplayName() {
+        return "Contribute";
+    }
+}
