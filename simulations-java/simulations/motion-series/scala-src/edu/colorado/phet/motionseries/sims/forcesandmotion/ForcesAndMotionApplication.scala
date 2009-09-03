d47ab49b81a597ff71c7fd45b5f25ba783aca3f2
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-09-03 20:54:53 +0000

    more renaming forces-and-friction => forces-and-motion

--- simulations-java/simulations/motion-series/scala-src/edu/colorado/phet/motionseries/sims/forcesandmotion/ForcesAndMotionApplication.scala
+++ simulations-java/simulations/motion-series/scala-src/edu/colorado/phet/motionseries/sims/forcesandmotion/ForcesAndMotionApplication.scala
@@ -43,21 +43,21 @@ class BasicForcesAndMotionCanvas(model: MotionSeriesModel, coordinateSystemModel
   override def addHeightAndAngleIndicators() = {}
 }
 
-class IntroModule(frame: JFrame, clock: ScalaClock) extends BasicForcesAndMotionModule(frame, clock, "forces-and-friction.module.intro.title".translate, false, true, false, true, -6, false, 0.0, true, MotionSeriesDefaults.forceMotionViewport)
+class IntroModule(frame: JFrame, clock: ScalaClock) extends BasicForcesAndMotionModule(frame, clock, "forces-and-motion.module.intro.title".translate, false, true, false, true, -6, false, 0.0, true, MotionSeriesDefaults.forceMotionViewport)
 
-class FrictionModule(frame: JFrame, clock: ScalaClock) extends BasicForcesAndMotionModule(frame, clock, "forces-and-friction.module.friction.title".translate, false, false, false, true, -6, false, 0.0, false, MotionSeriesDefaults.frictionViewport) {
+class FrictionModule(frame: JFrame, clock: ScalaClock) extends BasicForcesAndMotionModule(frame, clock, "forces-and-motion.module.friction.title".translate, false, false, false, true, -6, false, 0.0, false, MotionSeriesDefaults.frictionViewport) {
   val frictionPlayAreaControlPanel = new PSwing(new FrictionPlayAreaControlPanel(motionSeriesModel.bead))
   frictionPlayAreaControlPanel.setOffset(canvas.stage.width / 2 - frictionPlayAreaControlPanel.getFullBounds.getWidth / 2, canvas.stage.height - frictionPlayAreaControlPanel.getFullBounds.getHeight - 2)
   canvas.addStageNode(frictionPlayAreaControlPanel)
   motionSeriesModel.frictionless = false
 }
 
-class GraphingModule(frame: JFrame, clock: ScalaClock) extends BasicForcesAndMotionModule(frame, clock, "forces-and-friction.module.graphing.title".translate, false, false, true, false, -6, false, 0.0, true, MotionSeriesDefaults.forceEnergyGraphViewport) {
+class GraphingModule(frame: JFrame, clock: ScalaClock) extends BasicForcesAndMotionModule(frame, clock, "forces-and-motion.module.graphing.title".translate, false, false, true, false, -6, false, 0.0, true, MotionSeriesDefaults.forceEnergyGraphViewport) {
   coordinateSystemModel.adjustable = false
   canvas.addScreenNode(new ForcesAndMotionChartNode(canvas, motionSeriesModel))
 }
 
-class RobotMovingCompany1DModule(frame: JFrame, clock: ScalaClock) extends BasicForcesAndMotionModule(frame, clock, "forces-and-friction.module.robot-moving-company.title".translate, false, false, false, false, -6, false, 0.0, true, MotionSeriesDefaults.defaultViewport)
+class RobotMovingCompany1DModule(frame: JFrame, clock: ScalaClock) extends BasicForcesAndMotionModule(frame, clock, "forces-and-motion.module.robot-moving-company.title".translate, false, false, false, false, -6, false, 0.0, true, MotionSeriesDefaults.defaultViewport)
 
 class ForcesAndMotionApplication(config: PhetApplicationConfig) extends PiccoloPhetApplication(config) {
   def newClock = new ScalaClock(MotionSeriesDefaults.DELAY, MotionSeriesDefaults.DT_DEFAULT)
@@ -68,5 +68,5 @@ class ForcesAndMotionApplication(config: PhetApplicationConfig) extends PiccoloP
 }
 
 object ForcesAndMotionApplicationMain {
-  def main(args: Array[String]) = new PhetApplicationLauncher().launchSim(args, "motion-series".literal, "forces-and-friction".literal, classOf[ForcesAndMotionApplication])
+  def main(args: Array[String]) = new PhetApplicationLauncher().launchSim(args, "motion-series".literal, "forces-and-motion".literal, classOf[ForcesAndMotionApplication])
 }
