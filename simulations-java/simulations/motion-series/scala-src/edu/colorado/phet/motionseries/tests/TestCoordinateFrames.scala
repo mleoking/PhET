5484e489921ba89873918c6fd0c0a83b7dac6459
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-09-03 04:24:31 +0000

    added mutators for stage bounds

--- simulations-java/simulations/motion-series/scala-src/edu/colorado/phet/motionseries/tests/TestCoordinateFrames.scala
+++ simulations-java/simulations/motion-series/scala-src/edu/colorado/phet/motionseries/tests/TestCoordinateFrames.scala
@@ -7,6 +7,7 @@ import java.awt.event.{ComponentEvent, ComponentAdapter}
 import java.awt.geom.{Ellipse2D, Rectangle2D}
 import java.awt.{Color, BasicStroke, Component}
 import javax.swing.JFrame
+import scalacommon.util.Observable
 import umd.cs.piccolo.nodes.PText
 import umd.cs.piccolo.PNode
 
@@ -16,23 +17,24 @@ object TestCoordinateFrames {
   }
 }
 
-class StageNode(stageWidth: Int, stageHeight: Int, canvas: Component, node: PNode) extends PNode {
+class StageNode(stage: Stage, canvas: Component, node: PNode) extends PNode {
   addChild(node)
   canvas.addComponentListener(new ComponentAdapter() {
     override def componentResized(e: ComponentEvent) = updateLayout()
   })
+  stage.addListener(() => updateLayout())
   updateLayout()
   def updateLayout() = {
     val canvasWidth = canvas.getWidth
     val canvasHeight = canvas.getHeight
-    val widthScale = canvasWidth.toDouble / stageWidth
-    val heightScale = canvasHeight.toDouble / stageHeight
+    val widthScale = canvasWidth.toDouble / stage.width
+    val heightScale = canvasHeight.toDouble / stage.height
     val scale = Math.min(widthScale, heightScale)
     val patchedScale = if (scale > 0) scale else 1.0
     setScale(patchedScale)
 
-    val scaledWidth = patchedScale * stageWidth
-    val scaledHeight = patchedScale * stageHeight
+    val scaledWidth = patchedScale * stage.width
+    val scaledHeight = patchedScale * stage.height
     setOffset(canvasWidth / 2 - scaledWidth / 2, canvasHeight / 2 - scaledHeight / 2)
   }
 }
@@ -48,12 +50,30 @@ class MyPText(str: String, x: Double, y: Double, scale: Double) extends PText(st
   setOffset(x, y)
 }
 
+class Stage(private var _width: Double, private var _height: Double) extends Observable {
+  def width = _width
+
+  def height = _height
+
+  def setSize(w: Double, h: Double) = {
+    this._width = w
+    this._height = h
+    notifyListeners()
+  }
+}
+
 class MyCanvas(stageWidth: Int, stageHeight: Int, modelBounds: Rectangle2D.Double) extends PhetPCanvas {
+  val stage = new Stage(stageWidth, stageHeight)
   val transform = new ModelViewTransform2D(modelBounds, new Rectangle2D.Double(0, 0, stageWidth, stageHeight))
+
+  def setStageBounds(w: Double, h: Double) = {
+    stage.setSize(w, h)
+    transform.setViewBounds(new Rectangle2D.Double(0, 0, w, h))
+  }
 
   def addScreenNode(node: PNode) = getLayer.addChild(node)
 
-  def addStageNode(node: PNode) = addScreenNode(new StageNode(stageWidth, stageHeight, this, node))
+  def addStageNode(node: PNode) = addScreenNode(new StageNode(stage, this, node))
 
   def addModelNode(node: PNode) = addStageNode(new ModelNode(transform, node))
 }
@@ -63,18 +83,18 @@ class StartTest {
     val stageWidth = 200
     val stageHeight = 100
     val modelBounds = new Rectangle2D.Double(0, 0, 2E-6, 1E-6)
-    val phetCanvas = new MyCanvas(stageWidth, stageHeight, modelBounds)
-    phetCanvas.addScreenNode(new MyPText("Hello from screen at 50,50", 50, 50))
+    val canvas = new MyCanvas(stageWidth, stageHeight, modelBounds)
+    canvas.addScreenNode(new MyPText("Hello from screen at 50,50", 50, 50))
     val stageText = new MyPText("Hello from Stage at 100,50", 100, 50, 0.5)
-    phetCanvas.addStageNode(stageText)
-    phetCanvas.addStageNode(new PhetPPath(new Rectangle2D.Double(0, 0, stageWidth, stageHeight), new BasicStroke(2), Color.yellow))
-    phetCanvas.addScreenNode(new MyPText("Hello from screen at 100,100", 100, 100))
-    phetCanvas.addModelNode(new PhetPPath(new Ellipse2D.Double(0, 0, 0.5E-6, 0.5E-6), Color.blue))
-    phetCanvas.addModelNode(new MyPText("hello from left edge of world bounds", modelBounds.getMinX, modelBounds.getCenterY, 1E-6 / 100))
+    canvas.addStageNode(stageText)
+    canvas.addStageNode(new PhetPPath(new Rectangle2D.Double(0, 0, stageWidth, stageHeight), new BasicStroke(2), Color.yellow))
+    canvas.addScreenNode(new MyPText("Hello from screen at 100,100", 100, 100))
+    canvas.addModelNode(new PhetPPath(new Ellipse2D.Double(0, 0, 0.5E-6, 0.5E-6), Color.blue))
+    canvas.addModelNode(new MyPText("hello from left edge of world bounds", modelBounds.getMinX, modelBounds.getCenterY, 1E-6 / 100))
 
     //center one node beneath another, though they be in different coordinate frames
     val rectNode = new PhetPPath(new Rectangle2D.Double(0, 0, 50, 10), Color.red)
-    phetCanvas.addScreenNode(rectNode)
+    canvas.addScreenNode(rectNode)
     def updateRectNodeLocation() = {
       var rectNodeBounds = rectNode.globalToLocal(stageText.getGlobalFullBounds)
       rectNodeBounds = rectNode.localToParent(rectNodeBounds)
@@ -82,12 +102,17 @@ class StartTest {
     }
     updateRectNodeLocation()
     //coordinates can change, so need to update when they do
-    phetCanvas.addComponentListener(new ComponentAdapter() {override def componentResized(e: ComponentEvent) = {updateRectNodeLocation()}})
+    canvas.addComponentListener(new ComponentAdapter() {override def componentResized(e: ComponentEvent) = {updateRectNodeLocation()}})
 
     //todo: compute stage bounds dynamically, based on contents of the stage
+    //todo: maybe stage bounds should be mutable, since it is preferable to create the nodes as children of the canvas
+
+    //todo: how to implement pan/zoom with this paradigm
+
+    canvas.setStageBounds(200,200)
 
     val frame = new JFrame
-    frame.setContentPane(phetCanvas)
+    frame.setContentPane(canvas)
     frame.setSize(800, 600)
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
     frame.setVisible(true)
