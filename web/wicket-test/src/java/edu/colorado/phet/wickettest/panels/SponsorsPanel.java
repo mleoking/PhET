456fc7c2b06121e0ed49701d4bc648f4e2f31aad
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-09-04 05:24:52 +0000

    Wicket-test: fixed table align on RTL for simulation pages, fixed link on sponsors panel

--- web/wicket-test/src/java/edu/colorado/phet/wickettest/panels/SponsorsPanel.java
+++ web/wicket-test/src/java/edu/colorado/phet/wickettest/panels/SponsorsPanel.java
@@ -3,11 +3,14 @@ package edu.colorado.phet.wickettest.panels;
 import org.apache.wicket.behavior.HeaderContributor;
 
 import edu.colorado.phet.wickettest.util.PageContext;
+import edu.colorado.phet.wickettest.content.about.AboutSponsors;
 
 public class SponsorsPanel extends PhetPanel {
     public SponsorsPanel( String id, PageContext context ) {
         super( id, context );
 
         add( HeaderContributor.forCss( "/css/sponsors-v1.css" ) );
+
+        add( AboutSponsors.getLinker().getLink( "sponsors-link", context ));
     }
 }
