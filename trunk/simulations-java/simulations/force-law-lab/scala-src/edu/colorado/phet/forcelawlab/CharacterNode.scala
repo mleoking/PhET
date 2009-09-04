ea776e512da1efc2cb2c1c39f1aa65821e535bb8
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-09-04 15:51:21 +0000

    factored out CharacterNode class

---
+++ trunk/simulations-java/simulations/force-law-lab/scala-src/edu/colorado/phet/forcelawlab/CharacterNode.scala
@@ -0,0 +1,72 @@
+package edu.colorado.phet.forcelawlab
+
+import common.phetcommon.view.graphics.transforms.ModelViewTransform2D
+import umd.cs.piccolo.nodes.PImage
+import common.phetcommon.view.util.BufferedImageUtils
+import umd.cs.piccolo.PNode
+import common.piccolophet.nodes.PhetPPath
+import java.awt.{Color, BasicStroke, TexturePaint}
+import scalacommon.math.Vector2D
+import java.awt.geom.{Line2D, Rectangle2D}
+import scalacommon.Predef._
+
+class CharacterNode(mass: Mass, mass2: Mass, transform: ModelViewTransform2D, leftOfObject: Boolean, gravityForce: () => Double) extends PNode {
+  val ropeNode = new PhetPPath(new BasicStroke(5), Color.black)
+  addChild(ropeNode)
+  mass2.addListener(update)
+  mass.addListener(update)
+
+  def update() = {
+    updateRopeNode()
+    updateCharacterNode()
+  }
+
+  def ropeStart = transform.modelToView(mass.position)
+
+  lazy val sign = if (leftOfObject) -1 else 1
+
+  def ropeEnd = {
+    val length = transform.modelToViewDifferentialX(mass.radius) + 100
+    ropeStart + new Vector2D(length * sign, 0)
+  }
+
+  def updateRopeNode() = {
+    //    ropeNode.setPathTo(new Line2D.Double(ropeStart, ropeEnd))
+
+    ropeNode.setStroke(null)
+    ropeNode.setStrokePaint(null)
+    ropeNode.setPathTo(new BasicStroke(5).createStrokedShape(new Line2D.Double(ropeStart, ropeEnd)))
+    val im = BufferedImageUtils.multiScaleToHeight(ForceLawLabResources.getImage("rope-pattern.png"), 5)
+
+    val texturePaint = new TexturePaint(im, new Rectangle2D.Double(ropeEnd.x, 0, im.getWidth, im.getHeight))
+    ropeNode.setPaint(texturePaint)
+  }
+
+  def forceAmount = {
+    val minForceToShow = 0.00000000064
+    val maxForceToShow = 0.00000000989
+    val a = new common.phetcommon.math.Function.LinearFunction(minForceToShow, maxForceToShow, 0, 14).evaluate(gravityForce()).toInt
+    (a max 0) min 14
+  }
+
+  val scale = 0.5
+
+  def getImage = {
+    val im = ForceLawLabResources.getImage("pull-figure/pull_figure_" + forceAmount + ".png")
+    val flipIm = if (leftOfObject) BufferedImageUtils.flipX(im) else im
+    BufferedImageUtils.multiScale(flipIm, 0.5)
+  }
+
+  val ropeHeightFromImageBase = 103 * scale
+  val characterImageNode = new PImage(getImage)
+  addChild(characterImageNode)
+  update()
+  def updateCharacterNode() = {
+    characterImageNode.setImage(getImage)
+    characterImageNode.setOffset(ropeEnd)
+    characterImageNode.translate(0, ropeHeightFromImageBase - characterImageNode.getFullBounds.getHeight)
+    if (leftOfObject) characterImageNode.translate(-characterImageNode.getFullBounds.getWidth, 0)
+    characterImageNode.translate(-40 * sign * scale, 0) //move closer to rope, since graphic offset increases as force increases
+    characterImageNode.translate(-forceAmount * sign * 1.5 * scale, 0) //step closer to rope, to keep rope constant length
+  }
+}
--- trunk/simulations-java/simulations/force-law-lab/scala-src/edu/colorado/phet/forcelawlab/ForceLawLabApplication.scala
+++ trunk/simulations-java/simulations/force-law-lab/scala-src/edu/colorado/phet/forcelawlab/ForceLawLabApplication.scala
@@ -153,66 +153,6 @@ class MyDoubleGeneralPath(pt: Point2D) extends DoubleGeneralPath(pt) {
   def curveTo(control1: Vector2D, control2: Vector2D, dest: Vector2D) = super.curveTo(control1.x, control1.y, control2.x, control2.y, dest.x, dest.y)
 }
 
-class CharacterNode(mass: Mass, mass2: Mass, transform: ModelViewTransform2D, leftOfObject: Boolean, gravityForce: () => Double) extends PNode {
-  val ropeNode = new PhetPPath(new BasicStroke(5), Color.black)
-  addChild(ropeNode)
-  mass2.addListener(update)
-  mass.addListener(update)
-
-  def update() = {
-    updateRopeNode()
-    updateCharacterNode()
-  }
-
-  def ropeStart = transform.modelToView(mass.position)
-
-  lazy val sign = if (leftOfObject) -1 else 1
-
-  def ropeEnd = {
-    val length = transform.modelToViewDifferentialX(mass.radius) + 100
-    ropeStart + new Vector2D(length * sign, 0)
-  }
-
-  def updateRopeNode() = {
-    //    ropeNode.setPathTo(new Line2D.Double(ropeStart, ropeEnd))
-
-    ropeNode.setStroke(null)
-    ropeNode.setStrokePaint(null)
-    ropeNode.setPathTo(new BasicStroke(5).createStrokedShape(new Line2D.Double(ropeStart, ropeEnd)))
-    val im = BufferedImageUtils.multiScaleToHeight(ForceLawLabResources.getImage("rope-pattern.png"), 5)
-
-    val texturePaint = new TexturePaint(im, new Rectangle2D.Double(ropeEnd.x, 0, im.getWidth, im.getHeight))
-    ropeNode.setPaint(texturePaint)
-  }
-
-  def forceAmount = {
-    val minForceToShow = 0.00000000064
-    val maxForceToShow = 0.00000000989
-    val a = new common.phetcommon.math.Function.LinearFunction(minForceToShow, maxForceToShow, 0, 14).evaluate(gravityForce()).toInt
-    (a max 0) min 14
-  }
-
-  val scale = 0.5
-  def getImage = {
-    val im = ForceLawLabResources.getImage("pull-figure/pull_figure_" + forceAmount + ".png")
-    val flipIm = if (leftOfObject) BufferedImageUtils.flipX(im) else im
-    BufferedImageUtils.multiScale(flipIm,0.5)
-  }
-
-  val ropeHeightFromImageBase = 103 * scale
-  val characterImageNode = new PImage(getImage)
-  addChild(characterImageNode)
-  update()
-  def updateCharacterNode() = {
-    characterImageNode.setImage(getImage)
-    characterImageNode.setOffset(ropeEnd)
-    characterImageNode.translate(0, ropeHeightFromImageBase - characterImageNode.getFullBounds.getHeight)
-    if (leftOfObject) characterImageNode.translate(-characterImageNode.getFullBounds.getWidth, 0)
-    characterImageNode.translate(-40 * sign * scale , 0) //move closer to rope, since graphic offset increases as force increases
-    characterImageNode.translate(- forceAmount * sign * 1.5 * scale, 0)  //step closer to rope, to keep rope constant length
-  }
-}
-
 class ForceLawLabControlPanel(model: ForceLawLabModel, resetFunction: () => Unit) extends ControlPanel {
   import ForceLawLabResources._
   add(new ForceLawLabScalaValueControl(0.01, 100, model.m1.name, "0.00", getLocalizedString("units.kg"), model.m1.mass, model.m1.mass = _, model.m1.addListener))
