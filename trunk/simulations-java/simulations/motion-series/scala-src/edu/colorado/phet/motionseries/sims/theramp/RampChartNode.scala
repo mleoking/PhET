8b87f7bdf7617a6a2f56e5960057d9d7f125773b
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-09-04 20:38:26 +0000

    simplify graph subclasses

--- trunk/simulations-java/simulations/motion-series/scala-src/edu/colorado/phet/motionseries/sims/theramp/RampChartNode.scala
+++ trunk/simulations-java/simulations/motion-series/scala-src/edu/colorado/phet/motionseries/sims/theramp/RampChartNode.scala
@@ -1,32 +1,15 @@
 package edu.colorado.phet.motionseries.sims.theramp
 
-import charts.{MotionSeriesChartNode}
+import charts.{Graph, MotionSeriesChartNode}
 import graphics.MotionSeriesCanvas
-import phet.common.motion.graphs._
 import model.{MotionSeriesModel}
 import motionseries.MotionSeriesResources._
 
 class RampForceEnergyChartNode(canvas: MotionSeriesCanvas, model: MotionSeriesModel) extends MotionSeriesChartNode(canvas, model) {
-  val parallelForcesString = "forces.parallel-title".translate
-  val graphs = Array(new MinimizableControlGraph(parallelForcesString, forceGraph),
-    new MinimizableControlGraph("forces.work-energy-title".translate, energyGraph))
-  val graphSetNode = new GraphSetNode(new GraphSetModel(new GraphSuite(graphs))) {
-    override def getMaxAvailableHeight(availableHeight: Double) = availableHeight
-    setAlignedLayout()
-  }
-
-  addChild(graphSetNode)
-  updateLayout()
+  init(Graph("forces.parallel-title".translate, forceGraph, false) ::
+          Graph("forces.work-energy-title".translate, energyGraph, false) :: Nil)
 }
 
 class RampForceChartNode(canvas: MotionSeriesCanvas, model: MotionSeriesModel) extends MotionSeriesChartNode(canvas, model) {
-  val parallelForcesString = "forces.parallel-title".translate
-  val graphs = Array(new MinimizableControlGraph(parallelForcesString, forceGraph))
-  val graphSetNode = new GraphSetNode(new GraphSetModel(new GraphSuite(graphs))) {
-    override def getMaxAvailableHeight(availableHeight: Double) = availableHeight
-    setAlignedLayout()
-  }
-
-  addChild(graphSetNode)
-  updateLayout()
+  init(Graph("forces.parallel-title".translate, forceGraph, false) :: Nil)
 }
