2de4eded87f7c174f79b7ede20dd222eb84a709b
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-09-04 22:28:14 +0000

    Wicket-test: added internationalization to licensing panel, added software agreement html

--- web/wicket-test/src/java/edu/colorado/phet/wickettest/content/about/AboutLicensingPanel.java
+++ web/wicket-test/src/java/edu/colorado/phet/wickettest/content/about/AboutLicensingPanel.java
@@ -1,5 +1,6 @@
 package edu.colorado.phet.wickettest.content.about;
 
+import edu.colorado.phet.wickettest.components.LocalizedText;
 import edu.colorado.phet.wickettest.panels.PhetPanel;
 import edu.colorado.phet.wickettest.util.PageContext;
 import edu.colorado.phet.wickettest.util.links.AbstractLinker;
@@ -8,6 +9,47 @@ import edu.colorado.phet.wickettest.util.links.RawLinkable;
 public class AboutLicensingPanel extends PhetPanel {
     public AboutLicensingPanel( String id, PageContext context ) {
         super( id, context );
+
+        add( new LocalizedText( "about-licensing-contact-phet", "about.licensing.contactPhet", new Object[]{
+                "<a href=\"mailto:phethelp@colorado.edu\">phethelp@colorado.edu</a>"
+        } ) );
+
+        add( new LocalizedText( "about-licensing-cca-intro", "about.licensing.cca.intro", new Object[]{
+                "href=\"http://phet.colorado.edu\"",
+                "href=\"http://creativecommons.org/licenses/by/3.0/us/\""
+        } ) );
+
+        add( new LocalizedText( "about-licensing-gpl-sims", "about.licensing.gplSims" ) );
+
+        add( new LocalizedText( "about-licensing-cca-fulltext", "about.licensing.ccaFulltext", new Object[]{
+                "href=\"http://creativecommons.org/licenses/by/3.0/us/legalcode\""
+        }));
+
+        add( new LocalizedText( "about-licensing-gpl-intro", "about.licensing.gplIntro", new Object[]{
+                "href=\"http://phet.colorado.edu\"",
+                "href=\"http://creativecommons.org/licenses/GPL/2.0/\""
+                //"href=\"http://creativecommons.org/licenses/by/3.0/us/\""
+        } ) );
+
+        add( new LocalizedText( "about-licensing-gpl-fulltext", "about.licensing.gpl.fullText", new Object[]{
+                "href=\"http://www.gnu.org/licenses/old-licenses/gpl-2.0.html\""
+        }));
+
+        add( new LocalizedText( "about-licensing-gpl-source-code", "about.licensing.gpl.sourceCode", new Object[]{
+                AboutSourceCodePanel.getLinker().getHref( context )
+        }));
+
+        add( new LocalizedText( "about-licensing-alternative-license-options", "about.licensing.alternativeLicenseOptions", new Object[]{
+                "<a href=\"mailto:phethelp@colorado.edu\">phethelp@colorado.edu</a>"
+        }));
+
+        add( new LocalizedText( "about-licensing-source-code-link", "about.licensing.sourceCodeLink", new Object[]{
+                AboutSourceCodePanel.getLinker().getHref( context )
+        }));
+
+        add( new LocalizedText( "about-licensing-agreement-full-text", "about.licensing.softwareAgreementFullText", new Object[]{
+                "href=\"/about/software-agreement_v7.htm\""
+        }));
 
     }
 
