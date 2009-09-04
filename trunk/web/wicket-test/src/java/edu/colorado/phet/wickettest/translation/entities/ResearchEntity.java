6f41cbc42c97295f63d9359492cec152deb9cf78
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-09-04 07:40:21 +0000

    Wicket-test: internationalized research panel

---
+++ trunk/web/wicket-test/src/java/edu/colorado/phet/wickettest/translation/entities/ResearchEntity.java
@@ -0,0 +1,39 @@
+package edu.colorado.phet.wickettest.translation.entities;
+
+import edu.colorado.phet.wickettest.translation.PhetPanelFactory;
+import edu.colorado.phet.wickettest.panels.PhetPanel;
+import edu.colorado.phet.wickettest.util.PageContext;
+import edu.colorado.phet.wickettest.util.PhetRequestCycle;
+import edu.colorado.phet.wickettest.content.RunOurSimulationsPanel;
+import edu.colorado.phet.wickettest.content.ResearchPanel;
+
+public class ResearchEntity extends TranslationEntity {
+
+    public ResearchEntity() {
+        addString( "research.intro" );
+        addString( "research.additional" );
+        addString( "research.immediateInterests" );
+        addString( "research.simsAffectStudents" );
+        addString( "research.simsAffectStudents.point1" );
+        addString( "research.simsAffectStudents.point2" );
+        addString( "research.simsAffectStudents.point3" );
+        addString( "research.simsEnvironments" );
+        addString( "research.simsEnvironments.point1" );
+        addString( "research.simsEnvironments.point2" );
+        addString( "research.simsEnvironments.point3" );
+        addString( "research.publicationsAndPresentations" );
+        addString( "research.aboutPhet" );
+        addString( "research.effectivenessStudies" );
+        addString( "research.studentBeliefs" );
+
+        addPreview( new PhetPanelFactory() {
+            public PhetPanel getNewPanel( String id, PageContext context, PhetRequestCycle requestCycle ) {
+                return new ResearchPanel( id, context );
+            }
+        }, "Research page" );
+    }
+
+    public String getDisplayName() {
+        return "Research";
+    }
+}
