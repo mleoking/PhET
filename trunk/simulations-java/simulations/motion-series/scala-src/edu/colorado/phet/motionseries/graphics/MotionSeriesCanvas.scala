801e2a4ce2ee15230735f9b11f5a1c2e992416ed
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-09-06 04:33:52 +0000

    refactor vector graphics (incremental progress)

--- trunk/simulations-java/simulations/motion-series/scala-src/edu/colorado/phet/motionseries/graphics/MotionSeriesCanvas.scala
+++ trunk/simulations-java/simulations/motion-series/scala-src/edu/colorado/phet/motionseries/graphics/MotionSeriesCanvas.scala
@@ -8,12 +8,10 @@ import phet.common.phetcommon.view.graphics.transforms.ModelViewTransform2D
 import java.awt.event._
 import javax.swing.{JFrame}
 import model._
-import scalacommon.math.Vector2D
 import scalacommon.Predef._
 import motionseries.MotionSeriesResources
 import tests.MyCanvas
 import umd.cs.piccolo.PNode
-import java.lang.Math._
 import motionseries.MotionSeriesResources._
 
 abstract class MotionSeriesCanvas(model: MotionSeriesModel,
@@ -103,97 +101,14 @@ abstract class MotionSeriesCanvas(model: MotionSeriesModel,
   }
   //  addStageAreaDisplay()
 
-  val vectorNode = new PNode{
-    def addVector(a: Vector, offset: VectorValue) = addChild(new BodyVectorNode(transform, a, offset,model.bead))
-  }
-  playAreaNode.addChild(vectorNode)
-  def addVectorAllComponents(bead: Bead,
-                             beadVector: BeadVector with PointOfOriginVector, 
-                             offsetFBD: VectorValue,
-                             offsetPlayArea: Double,
-                             selectedVectorVisible: () => Boolean) = {
-    addVector(bead, beadVector, offsetFBD, offsetPlayArea)
-    val parallelComponent = new ParallelComponent(beadVector, bead)
-    val perpComponent = new PerpendicularComponent(beadVector, bead)
-    val xComponent = new XComponent(beadVector, bead, model.coordinateFrameModel)
-    val yComponent = new YComponent(beadVector, bead, model.coordinateFrameModel)
-    def update() = {
-      yComponent.visible = vectorViewModel.xyComponentsVisible && selectedVectorVisible()
-      xComponent.visible = vectorViewModel.xyComponentsVisible && selectedVectorVisible()
-      beadVector.visible = vectorViewModel.originalVectors && selectedVectorVisible()
-      parallelComponent.visible = vectorViewModel.parallelComponents && selectedVectorVisible()
-      perpComponent.visible = vectorViewModel.parallelComponents && selectedVectorVisible()
-    }
-    vectorViewModel.addListener(update)
-    update()
-
-    addVector(bead, xComponent, offsetFBD, offsetPlayArea)
-    addVector(bead, yComponent, offsetFBD, offsetPlayArea)
-    addVector(bead, parallelComponent, offsetFBD, offsetPlayArea)
-    addVector(bead, perpComponent, offsetFBD, offsetPlayArea)
-  }
-
-  def addVector(bead: Bead, vector: Vector with PointOfOriginVector, offsetFBD: VectorValue, offsetPlayArea: Double) = {
-    fbdNode.addVector(vector, offsetFBD, MotionSeriesDefaults.FBD_LABEL_MAX_OFFSET)
-    windowFBDNode.addVector(vector, offsetFBD, MotionSeriesDefaults.FBD_LABEL_MAX_OFFSET)
-
-    val tailLocationInPlayArea = new VectorValue() {
-      def addListener(listener: () => Unit) = {
-        bead.addListener(listener)
-        vectorViewModel.addListener(listener)
-      }
-
-      def getValue = {
-        val defaultCenter = bead.height / 2.0
-        bead.position2D + new Vector2D(bead.getAngle + PI / 2) *
-                (offsetPlayArea + (if (vectorViewModel.centered) defaultCenter else vector.getPointOfOriginOffset(defaultCenter)))
-      }
-
-
-      def removeListener(listener: () => Unit) = {
-        bead.removeListener(listener)
-        vectorViewModel.removeListener(listener)
-      }
-    }
-    //todo: make sure this adapter overrides other methods as well such as addListener
-    val playAreaAdapter = new Vector(vector.color, vector.name, vector.abbreviation, () => vector.getValue * MotionSeriesDefaults.PLAY_AREA_VECTOR_SCALE, vector.painter) {
-      vector.addListenerByName {
-        notifyListeners()
-      }
-      override def visible = vector.visible
-
-      override def visible_=(vis: Boolean) = vector.visible = vis
-
-      override def getPaint = vector.getPaint
-    }
-
-    if (useVectorNodeInPlayArea) {
-      vectorNode.addVector(playAreaAdapter, tailLocationInPlayArea)
-    }
-    bead.removalListeners += (() => {
-      fbdNode.removeVector(vector)
-      windowFBDNode.removeVector(vector)
-      //      vectorNode.removeVector(playAreaAdapter) //todo: don't use vectorNode for game module but remove it if non-game module
-    })
-  }
-
-  def addVectorAllComponents(bead: Bead, a: BeadVector): Unit = addVectorAllComponents(bead, a, new ConstantVectorValue, 0, () => true)
-
-  def addAllVectors(bead: Bead) = {
-    addVectorAllComponents(bead, bead.appliedForceVector)
-    addVectorAllComponents(bead, bead.gravityForceVector)
-    addVectorAllComponents(bead, bead.normalForceVector)
-    addVectorAllComponents(bead, bead.frictionForceVector)
-    addVectorAllComponents(bead, bead.wallForceVector)
-    addVectorAllComponents(bead, bead.totalForceVector, new ConstantVectorValue(new Vector2D(0, fbdWidth / 4)), 2, () => vectorViewModel.sumOfForcesVector) //no need to add a separate listener, since it is already contained in vectorviewmodel
-  }
-  addAllVectors(model.bead)
-
+  val vectorView = new VectorView(transform, model.bead, vectorViewModel, useVectorNodeInPlayArea, model.coordinateFrameModel,
+    fbdWidth, fbdNode, windowFBDNode)
+  playAreaNode.addChild(vectorView)
   playAreaNode.addChild(new RaindropView(model, this))
   playAreaNode.addChild(new FireDogView(model, this))
 
   val clearHeatButton = new ClearHeatButton(model)
-  val viewPt = transform.modelToView(5, 2)//show near the right side of the ramp, just above it so that it is visible in every sim+module
+  val viewPt = transform.modelToView(5, 2) //show near the right side of the ramp, just above it so that it is visible in every sim+module
   clearHeatButton.setOffset(viewPt.x - clearHeatButton.getFullBounds.getWidth / 2, viewPt.y)
   addStageNode(clearHeatButton)
 
---
+++ trunk/simulations-java/simulations/motion-series/scala-src/edu/colorado/phet/motionseries/graphics/VectorView.scala
@@ -0,0 +1,105 @@
+package edu.colorado.phet.motionseries.graphics
+
+import common.phetcommon.view.graphics.transforms.ModelViewTransform2D
+import model._
+import scalacommon.math.Vector2D
+import umd.cs.piccolo.PNode
+
+class VectorView(transform:ModelViewTransform2D,
+                 bead:Bead,
+                 vectorViewModel:VectorViewModel,
+                 useVectorNodeInPlayArea:Boolean,
+                 coordinateFrameModel:CoordinateFrameModel,
+                 fbdWidth:Int,
+                 fbdNode:FreeBodyDiagramNode,
+                 windowFBDNode:FBDDialog)
+        extends PNode{
+  val vectorNode = new PNode {
+    def addVector(a: Vector, offset: VectorValue) = addChild(new BodyVectorNode(transform, a, offset, bead))
+  }
+  addChild(vectorNode)
+
+  def addVectorAllComponents(bead: Bead,
+                             beadVector: BeadVector with PointOfOriginVector,
+                             offsetFBD: VectorValue,
+                             offsetPlayArea: Double,
+                             selectedVectorVisible: () => Boolean) = {
+    addVector(bead, beadVector, offsetFBD, offsetPlayArea)
+    val parallelComponent = new ParallelComponent(beadVector, bead)
+    val perpComponent = new PerpendicularComponent(beadVector, bead)
+    val xComponent = new XComponent(beadVector, bead, coordinateFrameModel)
+    val yComponent = new YComponent(beadVector, bead, coordinateFrameModel)
+    def update() = {
+      yComponent.visible = vectorViewModel.xyComponentsVisible && selectedVectorVisible()
+      xComponent.visible = vectorViewModel.xyComponentsVisible && selectedVectorVisible()
+      beadVector.visible = vectorViewModel.originalVectors && selectedVectorVisible()
+      parallelComponent.visible = vectorViewModel.parallelComponents && selectedVectorVisible()
+      perpComponent.visible = vectorViewModel.parallelComponents && selectedVectorVisible()
+    }
+    vectorViewModel.addListener(update)
+    update()
+
+    addVector(bead, xComponent, offsetFBD, offsetPlayArea)
+    addVector(bead, yComponent, offsetFBD, offsetPlayArea)
+    addVector(bead, parallelComponent, offsetFBD, offsetPlayArea)
+    addVector(bead, perpComponent, offsetFBD, offsetPlayArea)
+  }
+
+  def addVector(bead: Bead, vector: Vector with PointOfOriginVector, offsetFBD: VectorValue, offsetPlayArea: Double) = {
+    fbdNode.addVector(vector, offsetFBD, MotionSeriesDefaults.FBD_LABEL_MAX_OFFSET)
+    windowFBDNode.addVector(vector, offsetFBD, MotionSeriesDefaults.FBD_LABEL_MAX_OFFSET)
+
+    val tailLocationInPlayArea = new VectorValue() {
+      def addListener(listener: () => Unit) = {
+        bead.addListener(listener)
+        vectorViewModel.addListener(listener)
+      }
+
+      def getValue = {
+        val defaultCenter = bead.height / 2.0
+        bead.position2D + new Vector2D(bead.getAngle + java.lang.Math.PI / 2) *
+                (offsetPlayArea + (if (vectorViewModel.centered) defaultCenter else vector.getPointOfOriginOffset(defaultCenter)))
+      }
+
+
+      def removeListener(listener: () => Unit) = {
+        bead.removeListener(listener)
+        vectorViewModel.removeListener(listener)
+      }
+    }
+    //todo: make sure this adapter overrides other methods as well such as addListener
+    val playAreaAdapter = new Vector(vector.color, vector.name, vector.abbreviation,
+      () => vector.getValue * MotionSeriesDefaults.PLAY_AREA_VECTOR_SCALE, vector.painter) {
+      vector.addListenerByName {
+        notifyListeners()
+      }
+      override def visible = vector.visible
+
+      override def visible_=(vis: Boolean) = vector.visible = vis
+
+      override def getPaint = vector.getPaint
+    }
+
+    if (useVectorNodeInPlayArea) {
+      vectorNode.addVector(playAreaAdapter, tailLocationInPlayArea)
+    }
+    bead.removalListeners += (() => {
+      fbdNode.removeVector(vector)
+      windowFBDNode.removeVector(vector)
+      //      vectorNode.removeVector(playAreaAdapter) //todo: don't use vectorNode for game module but remove it if non-game module
+    })
+  }
+
+  def addVectorAllComponents(bead: Bead, a: BeadVector): Unit = addVectorAllComponents(bead, a, new ConstantVectorValue, 0, () => true)
+
+  def addAllVectors(bead: Bead) = {
+    addVectorAllComponents(bead, bead.appliedForceVector)
+    addVectorAllComponents(bead, bead.gravityForceVector)
+    addVectorAllComponents(bead, bead.normalForceVector)
+    addVectorAllComponents(bead, bead.frictionForceVector)
+    addVectorAllComponents(bead, bead.wallForceVector)
+    addVectorAllComponents(bead, bead.totalForceVector, new ConstantVectorValue(new Vector2D(0, fbdWidth / 4)), 2, () => vectorViewModel.sumOfForcesVector) //no need to add a separate listener, since it is already contained in vectorviewmodel
+  }
+  addAllVectors(bead)
+
+}
