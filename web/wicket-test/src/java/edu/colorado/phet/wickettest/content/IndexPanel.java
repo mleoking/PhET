67b3e840af8c21e94dbc07eb115b55000d16969a
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-09-06 05:19:53 +0000

    Wicket-test: translation link only available on English version

--- web/wicket-test/src/java/edu/colorado/phet/wickettest/content/IndexPanel.java
+++ web/wicket-test/src/java/edu/colorado/phet/wickettest/content/IndexPanel.java
@@ -4,6 +4,8 @@ import org.apache.wicket.behavior.HeaderContributor;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.link.Link;
 
+import edu.colorado.phet.wickettest.WicketApplication;
+import edu.colorado.phet.wickettest.components.InvisibleComponent;
 import edu.colorado.phet.wickettest.components.LocalizedText;
 import edu.colorado.phet.wickettest.components.PhetLink;
 import edu.colorado.phet.wickettest.components.StaticImage;
@@ -44,7 +46,12 @@ public class IndexPanel extends PhetPanel {
 
         add( SimulationDisplay.createLink( "below-simulations-link", context ) );
 
-        add( new BookmarkablePageLink( "test-translation", TranslationMainPage.class ) );
+        if ( context.getLocale().equals( WicketApplication.getDefaultLocale() ) ) {
+            add( new BookmarkablePageLink( "test-translation", TranslationMainPage.class ) );
+        }
+        else {
+            add( new InvisibleComponent( "test-translation" ) );
+        }
 
         add( new TranslationLinksPanel( "translation-links", context ) );
 
