ac5476a0cabc741d01d34fb6aa253c8e26324c25
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-09-10 18:21:10 +0000

    forces-and-motion: Removed gravity and wall force from chart

--- trunk/simulations-java/simulations/motion-series/scala-src/edu/colorado/phet/motionseries/sims/forcesandmotion/ForcesAndMotionChartNode.scala
+++ trunk/simulations-java/simulations/motion-series/scala-src/edu/colorado/phet/motionseries/sims/forcesandmotion/ForcesAndMotionChartNode.scala
@@ -6,7 +6,7 @@ import model.MotionSeriesModel
 import motionseries.MotionSeriesResources._
 
 class ForcesAndMotionChartNode(canvas: MotionSeriesCanvas, model: MotionSeriesModel) extends MotionSeriesChartNode(canvas, model) {
-  init(Graph("forces.parallel-title".translate, forceGraph, false) ::
+  init(Graph("forces.parallel-title".translate, forceGraph(false), false) ::
           Graph("acceleration", accelerationGraph, true) ::
           Graph("velocity", velocityGraph, true) ::
           Graph("position", positionGraph, true) :: Nil)
