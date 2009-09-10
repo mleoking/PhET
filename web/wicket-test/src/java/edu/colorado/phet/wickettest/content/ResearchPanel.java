ad23c23e1b22c576e62d6c493a90678218bfbe5f
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-09-10 17:59:54 +0000

    Wicket-test: rewrite of research page

--- web/wicket-test/src/java/edu/colorado/phet/wickettest/content/ResearchPanel.java
+++ web/wicket-test/src/java/edu/colorado/phet/wickettest/content/ResearchPanel.java
@@ -1,6 +1,7 @@
 package edu.colorado.phet.wickettest.content;
 
-import edu.colorado.phet.wickettest.components.LocalizedText;
+import org.apache.wicket.behavior.HeaderContributor;
+
 import edu.colorado.phet.wickettest.panels.PhetPanel;
 import edu.colorado.phet.wickettest.util.PageContext;
 import edu.colorado.phet.wickettest.util.links.AbstractLinker;
@@ -10,9 +11,11 @@ public class ResearchPanel extends PhetPanel {
     public ResearchPanel( String id, PageContext context ) {
         super( id, context );
 
-        add( new LocalizedText( "research-additional", "research.additional", new Object[]{
-                "href=\"/publications/PhET%20Look%20and%20Feel.pdf\""
-        } ) );
+//        add( new LocalizedText( "research-additional", "research.additional", new Object[]{
+//                "href=\"/publications/PhET%20Look%20and%20Feel.pdf\""
+//        } ) );
+
+        add( HeaderContributor.forCss( "/css/research-v1.css" ) );
 
     }
 
--- web/wicket-test/src/java/edu/colorado/phet/wickettest/translation/entities/ResearchEntity.java
+++ web/wicket-test/src/java/edu/colorado/phet/wickettest/translation/entities/ResearchEntity.java
@@ -9,21 +9,23 @@ import edu.colorado.phet.wickettest.util.PhetRequestCycle;
 public class ResearchEntity extends TranslationEntity {
 
     public ResearchEntity() {
-        addString( "research.intro" );
-        addString( "research.additional" );
-        addString( "research.immediateInterests" );
-        addString( "research.simsAffectStudents" );
-        addString( "research.simsAffectStudents.point1" );
-        addString( "research.simsAffectStudents.point2" );
-        addString( "research.simsAffectStudents.point3" );
-        addString( "research.simsEnvironments" );
-        addString( "research.simsEnvironments.point1" );
-        addString( "research.simsEnvironments.point2" );
-        addString( "research.simsEnvironments.point3" );
-        addString( "research.publicationsAndPresentations" );
-        addString( "research.aboutPhet" );
-        addString( "research.effectivenessStudies" );
-        addString( "research.studentBeliefs" );
+//        addString( "research.intro" );
+//        addString( "research.additional" );
+//        addString( "research.immediateInterests" );
+//        addString( "research.simsAffectStudents" );
+//        addString( "research.simsAffectStudents.point1" );
+//        addString( "research.simsAffectStudents.point2" );
+//        addString( "research.simsAffectStudents.point3" );
+//        addString( "research.simsEnvironments" );
+//        addString( "research.simsEnvironments.point1" );
+//        addString( "research.simsEnvironments.point2" );
+//        addString( "research.simsEnvironments.point3" );
+//        addString( "research.publicationsAndPresentations" );
+//        addString( "research.aboutPhet" );
+//        addString( "research.effectivenessStudies" );
+//        addString( "research.studentBeliefs" );
+
+        
 
         addPreview( new PhetPanelFactory() {
             public PhetPanel getNewPanel( String id, PageContext context, PhetRequestCycle requestCycle ) {
