3fd22fa32a622d175d818137100b4f8250f826da
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-09-16 18:07:31 +0000

    First (currently over simplified) visual representation.

--- trunk/simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/view/MembraneChannelNode.java
+++ trunk/simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/view/MembraneChannelNode.java
@@ -2,6 +2,9 @@
 
 package edu.colorado.phet.neuron.view;
 
+import java.awt.geom.Rectangle2D;
+
+import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform2D;
 import edu.colorado.phet.neuron.model.AbstractMembraneChannel;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.nodes.PPath;
@@ -12,12 +15,22 @@ import edu.umd.cs.piccolo.nodes.PPath;
  * @author John Blanco
  */
 public class MembraneChannelNode extends PNode{
-
-	private PPath channel;
-	private PPath leftSide;
-	private PPath rightSide;
 	
-	public MembraneChannelNode(AbstractMembraneChannel membraneChannelModel){
+    private PNode parentNode; // TODO: Use this to implement removal of nodes.
+	
+	public MembraneChannelNode(AbstractMembraneChannel membraneChannelModel, PNode parentNode, ModelViewTransform2D mvt){
+
+		this.parentNode = parentNode;
+		
+		PPath channel = new PPath(mvt.createTransformedShape(membraneChannelModel.getChannelRect()));
+		channel.setPaint(membraneChannelModel.getChannelColor());
+		addChild(channel);
+	}
+	
+	
+	private PPath createPillNode(Rectangle2D definingRect){
+		
+		return null;
 		
 	}
 }
