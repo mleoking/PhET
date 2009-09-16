2e6b7b4033dfb34c4751245299301385f549a094
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-09-16 19:45:03 +0000

    Added initial representations for the edges of the channel.

--- simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/view/MembraneChannelNode.java
+++ simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/view/MembraneChannelNode.java
@@ -12,6 +12,7 @@ import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.colorado.phet.neuron.model.AbstractMembraneChannel;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.nodes.PPath;
+import edu.umd.cs.piccolo.util.PDimension;
 
 /**
  * Node that represents a membrane channel in the view.
@@ -30,6 +31,8 @@ public class MembraneChannelNode extends PNode{
 
 		this.parentNode = parentNode;
 
+		PNode representation = new PNode();
+		
 		Dimension2D channelSize = membraneChannelModel.getChannelSize();
 		Rectangle2D transformedChannelRect = new Rectangle2D.Double(
 				-mvt.modelToViewDifferentialXDouble(channelSize.getWidth()) / 2,
@@ -38,21 +41,44 @@ public class MembraneChannelNode extends PNode{
 				-mvt.modelToViewDifferentialYDouble(channelSize.getHeight()));
 		PPath channel = new PPath(transformedChannelRect);
 		channel.setPaint(membraneChannelModel.getChannelColor());
-		channel.rotate(-membraneChannelModel.getRotationalAngle() + Math.PI / 2);
-		System.out.println("Angle = " + membraneChannelModel.getRotationalAngle() * 180 / Math.PI);
-		addChild(channel);
+		representation.addChild(channel);
 		
-		// Move the channel representation so that 
-		PPath positioningNode = new PhetPPath(new Ellipse2D.Double(-10, -10, 20, 20), Color.PINK);
-		addChild(positioningNode);
+		double edgeNodeWidth = (membraneChannelModel.getOverallSize().getWidth() - 
+				membraneChannelModel.getChannelSize().getWidth()) / 2;
+		double edgeNodeHeight = membraneChannelModel.getOverallSize().getHeight();
+		
+		Dimension2D transformedEdgeNodeSize = new PDimension(mvt.modelToViewDifferentialXDouble(edgeNodeWidth),
+				-mvt.modelToViewDifferentialYDouble(edgeNodeHeight));
+				
+		PNode leftEdgeNode = createEdgeNode(transformedEdgeNodeSize, membraneChannelModel.getEdgeColor());
+		leftEdgeNode.setOffset(channel.getFullBoundsReference().getMinX() - leftEdgeNode.getFullBoundsReference().width,
+				-leftEdgeNode.getFullBoundsReference().height / 2);
+		representation.addChild(leftEdgeNode);
+		
+		PNode rightEdgeNode = createEdgeNode(transformedEdgeNodeSize, membraneChannelModel.getEdgeColor());
+		rightEdgeNode.setOffset(channel.getFullBoundsReference().getMaxX(),
+				-rightEdgeNode.getFullBoundsReference().height / 2);
+		representation.addChild(rightEdgeNode);
+		
+		addChild(representation);
+		
+//		channel.rotate(-membraneChannelModel.getRotationalAngle() + Math.PI / 2);
+//		System.out.println("Angle = " + membraneChannelModel.getRotationalAngle() * 180 / Math.PI);
+		
+		// TODO: Positioning node, uncomment when needed, remove eventually.
+//		PPath positioningNode = new PhetPPath(new Ellipse2D.Double(-10, -10, 20, 20), Color.PINK);
+//		addChild(positioningNode);
 		
 		setOffset(mvt.modelToViewDouble(membraneChannelModel.getCenterLocation()));
 	}
 	
 	
-	private PPath createPillNode(Rectangle2D definingRect){
+	private PPath createEdgeNode(Dimension2D size, Color color){
 		
-		return null;
+		PPath edgeNode = new PPath();
+		edgeNode.setPathTo(new Rectangle2D.Double(0, 0, size.getWidth(), size.getHeight()));
+		edgeNode.setPaint(color);
 		
+		return edgeNode;
 	}
 }
