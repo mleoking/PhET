0122f0177bf35648c9a72e473e9da190b7b74fc0
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-09-16 04:07:34 +0000

    Initial creation of this class.

---
+++ simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/model/SodiumLeakageChannel.java
@@ -0,0 +1,37 @@
+/* Copyright 2009, University of Colorado */
+
+package edu.colorado.phet.neuron.model;
+
+import java.util.ArrayList;
+
+public class SodiumLeakageChannel extends AbstractMembraneChannel {
+
+    //----------------------------------------------------------------------------
+    // Class Data
+    //----------------------------------------------------------------------------
+	
+	private static final double CHANNEL_WIDTH = 100; // In nanometers.
+	
+    //----------------------------------------------------------------------------
+    // Instance Data
+    //----------------------------------------------------------------------------
+	
+    //----------------------------------------------------------------------------
+    // Constructor
+    //----------------------------------------------------------------------------
+	public SodiumLeakageChannel() {
+		super(CHANNEL_WIDTH, AxonMembrane.MEMBRANE_THICKNESS);
+	}
+
+	@Override
+	public void checkReleaseControlAtoms(ArrayList<Atom> freeAtoms) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void checkTakeControlAtoms(ArrayList<Atom> freeAtoms) {
+		// TODO Auto-generated method stub
+
+	}
+}
