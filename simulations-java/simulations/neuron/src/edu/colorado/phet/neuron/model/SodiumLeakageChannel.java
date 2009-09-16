2ab902bce2af45d6cd8cd2d1dd8502a96b415931
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-09-16 23:07:43 +0000

    Added the ability to "capture" a sodium atom that gets close to this channel.

--- simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/model/SodiumLeakageChannel.java
+++ simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/model/SodiumLeakageChannel.java
@@ -16,6 +16,8 @@ public class SodiumLeakageChannel extends AbstractMembraneChannel {
 	private static final double CHANNEL_HEIGHT = AxonMembrane.MEMBRANE_THICKNESS * 1.5; // In nanometers.
 	private static final double CHANNEL_WIDTH = AxonMembrane.MEMBRANE_THICKNESS * 0.75; // In nanometers.
 	
+	private static final double CAPTURE_DISTANCE = 4; // In nanometers.
+	
     //----------------------------------------------------------------------------
     // Instance Data
     //----------------------------------------------------------------------------
@@ -34,7 +36,28 @@ public class SodiumLeakageChannel extends AbstractMembraneChannel {
 
 	@Override
 	public void checkTakeControlAtoms(ArrayList<Atom> freeAtoms) {
-		// TODO Auto-generated method stub
+		
+		ArrayList<Atom> ownedAtoms = getOwnedAtomsRef();
+		
+		// Only move one atom at a time.
+		if (ownedAtoms.size() == 0){
+			boolean capture = false;
+			Atom atom = null;
+			for (int i = 0; i < freeAtoms.size(); i++){
+				// See if this atom is in the right place to be captured.
+				atom = freeAtoms.get(i);
+				if ( atom.getType() == AtomType.SODIUM && 
+					 atom.getPosition().distance(getCenterLocation()) < CAPTURE_DISTANCE ){
+					// Capture this guy.
+					capture = true;
+					break;
+				}
+			}
+			if (capture && atom != null){
+				freeAtoms.remove(atom);
+				ownedAtoms.add(atom);
+			}
+		}
 	}
 
 	@Override
@@ -46,6 +69,4 @@ public class SodiumLeakageChannel extends AbstractMembraneChannel {
 	public Color getEdgeColor() {
 		return Color.red;
 	}
-	
-	
 }
