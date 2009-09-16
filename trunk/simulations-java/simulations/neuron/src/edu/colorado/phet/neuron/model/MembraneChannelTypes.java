13779274c859c8cadaf8045462705a1d2de02307
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-09-16 19:22:02 +0000

    Enum the represents channel types.

---
+++ trunk/simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/model/MembraneChannelTypes.java
@@ -0,0 +1,13 @@
+/* Copyright 2009, University of Colorado */
+
+package edu.colorado.phet.neuron.model;
+
+/**
+ * Allowable types of membrane channels.
+ * 
+ * @author John Blanco
+ */
+public enum MembraneChannelTypes {
+	SODIUM_LEAKAGE_CHANNEL,
+	POTASSIUM_LEAKAGE_CHANNEL
+}
