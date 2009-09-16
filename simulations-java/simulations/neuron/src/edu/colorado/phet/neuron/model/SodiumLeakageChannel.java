ea517c1c3761cd886a46e0d71c8b2c7bd1eb5fce
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-09-16 21:14:17 +0000

    Overrode color-getters so that colors match the spec.

--- simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/model/SodiumLeakageChannel.java
+++ simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/model/SodiumLeakageChannel.java
@@ -2,7 +2,10 @@
 
 package edu.colorado.phet.neuron.model;
 
+import java.awt.Color;
 import java.util.ArrayList;
+
+import edu.colorado.phet.common.phetcommon.view.util.ColorUtils;
 
 public class SodiumLeakageChannel extends AbstractMembraneChannel {
 
@@ -27,12 +30,22 @@ public class SodiumLeakageChannel extends AbstractMembraneChannel {
 	@Override
 	public void checkReleaseControlAtoms(ArrayList<Atom> freeAtoms) {
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void checkTakeControlAtoms(ArrayList<Atom> freeAtoms) {
 		// TODO Auto-generated method stub
-
 	}
+
+	@Override
+	public Color getChannelColor() {
+		return ColorUtils.darkerColor(Color.red, 0.2);
+	}
+
+	@Override
+	public Color getEdgeColor() {
+		return Color.red;
+	}
+	
+	
 }
