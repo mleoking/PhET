8e000da73d12f4d5e542eb4dce28877bacac003a
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-09-17 18:49:05 +0000

    javadoc

--- trunk/util/translation-utility/src/edu/colorado/phet/translationutility/userinterface/TUTextArea.java
+++ trunk/util/translation-utility/src/edu/colorado/phet/translationutility/userinterface/TUTextArea.java
@@ -7,25 +7,25 @@ import javax.swing.JTextArea;
 import javax.swing.border.Border;
 
 /**
- * Base classes for text area in the main translation panel.
+ * Base class for text areas in the main translation panel.
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
 public abstract class TUTextArea extends JTextArea {
     
-    private static final int TEXT_AREA_COLUMNS = 20;
-    private static final Border TEXT_AREA_BORDER = BorderFactory.createCompoundBorder( 
+    private static final Color SELECTION_COLOR = Color.GREEN;
+    private static final int COLUMNS = 20;
+    private static final Border BORDER = BorderFactory.createCompoundBorder( 
             /* outside */ BorderFactory.createLineBorder( Color.BLACK, 1 ), 
             /* inside */ BorderFactory.createEmptyBorder( 2, 2, 2, 2 ) );
-    private static final Color SELECTION_COLOR = Color.GREEN;
     
     public TUTextArea( String value ) {
         super( value );
-        setColumns( TEXT_AREA_COLUMNS );
+        setColumns( COLUMNS );
         setLineWrap( true );
         setWrapStyleWord( true );
         setFocusable( true ); // must be true for Find selection to work
-        setBorder( TEXT_AREA_BORDER );
+        setBorder( BORDER );
         setSelectionColor( SELECTION_COLOR );
     }
 }
