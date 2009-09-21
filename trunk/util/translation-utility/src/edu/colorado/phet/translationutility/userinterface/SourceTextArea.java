83aadca07fe5a7c032d0d2c59033162bc62954a9
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-09-21 19:41:17 +0000

    see #1629, source text fields aren't editable, so don't request focus

--- trunk/util/translation-utility/src/edu/colorado/phet/translationutility/userinterface/SourceTextArea.java
+++ trunk/util/translation-utility/src/edu/colorado/phet/translationutility/userinterface/SourceTextArea.java
@@ -17,5 +17,6 @@ public class SourceTextArea extends TUTextArea {
         super( value );
         setEditable( false );
         setBackground( SOURCE_BACKGROUND );
+        setRequestFocusEnabled( false );
     }
 }
