adb44caade5f1c037f2b0d82708f2a74f2bcf0c9
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-09-21 18:07:57 +0000

    Integrated graphic for center of the axon cross section.

--- simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/view/AxonMembraneNode.java
+++ simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/view/AxonMembraneNode.java
@@ -4,13 +4,19 @@ package edu.colorado.phet.neuron.view;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Rectangle;
 import java.awt.Shape;
 import java.awt.geom.Ellipse2D;
+import java.awt.image.BufferedImage;
 
+import edu.colorado.phet.common.phetcommon.resources.PhetResources;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform2D;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
+import edu.colorado.phet.neuron.NeuronResources;
 import edu.colorado.phet.neuron.model.AxonMembrane;
+import edu.colorado.phet.nuclearphysics.NuclearPhysicsResources;
 import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.nodes.PImage;
 
 /**
  * Representation of the axon membrane in the view.
@@ -37,12 +43,23 @@ public class AxonMembraneNode extends PNode {
         double outerDiameter = axonMembraneModel.getCrossSectionDiameter() + axonMembraneModel.getMembraneThickness();
         double innerDiameter = axonMembraneModel.getCrossSectionDiameter() - axonMembraneModel.getMembraneThickness();
         
-        // Add the cross section.
-        Shape outerDiameterEllipse = mvt.createTransformedShape(new Ellipse2D.Double(-outerDiameter / 2, -outerDiameter / 2, outerDiameter, outerDiameter));
-        Shape innerDiameterEllipse = mvt.createTransformedShape(new Ellipse2D.Double(-innerDiameter / 2, -innerDiameter / 2, innerDiameter, innerDiameter));
-        outerMembrane = new PhetPPath( outerDiameterEllipse, Color.YELLOW, new BasicStroke(4), Color.BLACK);
+        // Create the cross section, which consists of an outer circle that
+        // represents the outer edge of the membrane, an inner graphic that
+        // is supposed to look like the fluid inside the axon, and an inner
+        // circle that represents the inner edge of the membrane.
+        Shape outerDiameterCircle = mvt.createTransformedShape(new Ellipse2D.Double(-outerDiameter / 2, -outerDiameter / 2, outerDiameter, outerDiameter));
+        Shape innerDiameterCircle = mvt.createTransformedShape(new Ellipse2D.Double(-innerDiameter / 2, -innerDiameter / 2, innerDiameter, innerDiameter));
+        outerMembrane = new PhetPPath( outerDiameterCircle, Color.YELLOW, new BasicStroke(4), Color.BLACK);
 		addChild( outerMembrane );
-        innerMembrane = new PhetPPath( innerDiameterEllipse, new Color(73, 210, 242),  new BasicStroke(4), Color.BLACK);
-		addChild( innerMembrane );
+        BufferedImage bufferedImage = NeuronResources.getImage( "water_circle.png" );
+        PImage centerImage = new PImage( bufferedImage );
+        Rectangle bounds = innerDiameterCircle.getBounds();
+        centerImage.setScale(bounds.getWidth() / centerImage.getFullBoundsReference().width);
+        centerImage.setOffset(bounds.getCenterX() - centerImage.getFullBoundsReference().width / 2,
+        		bounds.getCenterY() - centerImage.getFullBoundsReference().height / 2);
+        addChild(centerImage);
+        innerMembrane = new PhetPPath( innerDiameterCircle, new Color(73, 210, 242),  new BasicStroke(4), Color.BLACK);
+        innerMembrane = new PhetPPath( innerDiameterCircle, new BasicStroke(4), Color.BLACK );
+		addChild( innerMembrane );		
 	}
 }
