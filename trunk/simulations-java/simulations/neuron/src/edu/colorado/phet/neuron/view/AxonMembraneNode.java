34317394c4cda7fd6e4f42937afd10a1e658eeb0
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-09-23 18:39:07 +0000

    Removed the image from the center and went back to a solid color.  This was done based on feedback from Noah P.

--- trunk/simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/view/AxonMembraneNode.java
+++ trunk/simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/view/AxonMembraneNode.java
@@ -41,6 +41,14 @@ public class AxonMembraneNode extends PNode {
         double outerDiameter = axonMembraneModel.getCrossSectionDiameter() + axonMembraneModel.getMembraneThickness();
         double innerDiameter = axonMembraneModel.getCrossSectionDiameter() - axonMembraneModel.getMembraneThickness();
         
+        /*
+         * TODO:
+         * The cross section, for a while, had an image in the middle that was
+         * intended to create the appearance of depth.  After seeing it, Noah
+         * P thought that it could be misconstrued as some sort of charge
+         * gradient, so it was removed.  This code should be kept for a while
+         * until we are sure that having no image in the center (i.e. just a
+         * solid color) works reasonably well.
         // Create the cross section, which consists of an outer circle that
         // represents the outer edge of the membrane, an inner graphic that
         // is supposed to look like the fluid inside the axon, and an inner
@@ -56,8 +64,19 @@ public class AxonMembraneNode extends PNode {
         centerImage.setOffset(bounds.getCenterX() - centerImage.getFullBoundsReference().width / 2,
         		bounds.getCenterY() - centerImage.getFullBoundsReference().height / 2);
         addChild(centerImage);
-        innerMembrane = new PhetPPath( innerDiameterCircle, new Color(73, 210, 242),  new BasicStroke(4), Color.BLACK);
         innerMembrane = new PhetPPath( innerDiameterCircle, new BasicStroke(4), Color.BLACK );
+		addChild( innerMembrane );		
+         */
+        
+        // Create the cross section, which consists of an outer circle that
+        // represents the outer edge of the membrane and an inner circle that
+        // represents the inner edge of the membrane and the inner portion of
+        // the axon.
+        Shape outerDiameterCircle = mvt.createTransformedShape(new Ellipse2D.Double(-outerDiameter / 2, -outerDiameter / 2, outerDiameter, outerDiameter));
+        Shape innerDiameterCircle = mvt.createTransformedShape(new Ellipse2D.Double(-innerDiameter / 2, -innerDiameter / 2, innerDiameter, innerDiameter));
+        outerMembrane = new PhetPPath( outerDiameterCircle, Color.YELLOW, new BasicStroke(4), Color.BLACK);
+		addChild( outerMembrane );
+        innerMembrane = new PhetPPath( innerDiameterCircle, new Color(73, 210, 242),  new BasicStroke(4), Color.BLACK);
 		addChild( innerMembrane );		
 	}
 }
