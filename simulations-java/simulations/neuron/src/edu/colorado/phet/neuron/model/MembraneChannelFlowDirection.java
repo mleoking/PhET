4ac30c1ace90f11cdb84ce36eeb21a15e1ad625c
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-09-24 18:09:21 +0000

    New enum used to track/define the direction of flow of atoms through membrane channels.

---
+++ simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/model/MembraneChannelFlowDirection.java
@@ -0,0 +1,13 @@
+package edu.colorado.phet.neuron.model;
+
+/**
+ * Defines the direction of the flow for a membrane channel.
+ * 
+ * @author John Blanco
+ */
+public enum MembraneChannelFlowDirection {
+	NONE,
+	OUT,
+	IN,
+	BIDIRECTIONAL
+}
