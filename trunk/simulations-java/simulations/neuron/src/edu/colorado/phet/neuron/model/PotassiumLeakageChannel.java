08c50344ef87cb803dc6754754cb129e1661a0e6
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-09-24 18:50:01 +0000

    Inserted AbstractLeakChannel into the inheritance hierarchy for the leak channels in order to minimize redundant code.

--- trunk/simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/model/PotassiumLeakageChannel.java
+++ trunk/simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/model/PotassiumLeakageChannel.java
@@ -3,12 +3,10 @@
 package edu.colorado.phet.neuron.model;
 
 import java.awt.Color;
-import java.awt.geom.Point2D;
-import java.util.ArrayList;
 
 import edu.colorado.phet.common.phetcommon.view.util.ColorUtils;
 
-public class PotassiumLeakageChannel extends AbstractMembraneChannel {
+public class PotassiumLeakageChannel extends AbstractLeakChannel {
 
     //----------------------------------------------------------------------------
     // Class Data
@@ -16,8 +14,6 @@ public class PotassiumLeakageChannel extends AbstractMembraneChannel {
 	
 	private static final double CHANNEL_HEIGHT = AxonMembrane.MEMBRANE_THICKNESS * 1.5; // In nanometers.
 	private static final double CHANNEL_WIDTH = AxonMembrane.MEMBRANE_THICKNESS * 0.75; // In nanometers.
-	
-	private static final double CAPTURE_DISTANCE = 4.5; // In nanometers.
 	
     //----------------------------------------------------------------------------
     // Instance Data
@@ -27,50 +23,7 @@ public class PotassiumLeakageChannel extends AbstractMembraneChannel {
     // Constructor
     //----------------------------------------------------------------------------
 	public PotassiumLeakageChannel() {
-		super(CHANNEL_WIDTH, CHANNEL_HEIGHT);
-	}
-
-	@Override
-	public void checkReleaseControlAtoms(ArrayList<Atom> freeAtoms) {
-		Atom atom = null;
-		boolean release = false;
-		for (int i = 0; i < getOwnedAtomsRef().size(); i++){
-			atom = getOwnedAtomsRef().get(i);
-			if (atom.getPositionReference().distance(getCenterLocation()) > CAPTURE_DISTANCE * 1.3){
-				// Atom is far enough away that it can be released.
-				release = true;
-				break;
-			}
-		}
-		if (release){
-			getOwnedAtomsRef().remove(atom);
-			freeAtoms.add(atom);
-		}
-	}
-
-	@Override
-	public void checkTakeControlAtoms(ArrayList<Atom> freeAtoms) {
-		
-		ArrayList<Atom> ownedAtoms = getOwnedAtomsRef();
-		
-		// Only move one atom at a time.
-		if (ownedAtoms.size() == 0){
-			boolean capture = false;
-			Atom atom = null;
-			for (int i = 0; i < freeAtoms.size(); i++){
-				// See if this atom is in the right place to be captured.
-				atom = freeAtoms.get(i);
-				if ( atom.getType() == AtomType.POTASSIUM && 
-					 atom.getPosition().distance(getCenterLocation()) < CAPTURE_DISTANCE ){
-					// Capture this guy.
-					capture = true;
-					break;
-				}
-			}
-			if (capture && atom != null){
-				captureAtom(atom, freeAtoms, ownedAtoms);
-			}
-		}
+		super(CHANNEL_WIDTH, CHANNEL_HEIGHT, AtomType.POTASSIUM);
 	}
 
 	@Override
@@ -81,50 +34,6 @@ public class PotassiumLeakageChannel extends AbstractMembraneChannel {
 	@Override
 	public Color getEdgeColor() {
 		return Color.green;
-	}
-
-	@Override
-	public void stepInTime(double dt) {
-		if (!getOwnedAtomsRef().isEmpty()){
-			for (Atom atom : getOwnedAtomsRef()){
-				atom.stepInTime(dt);
-			}
-		}
-	}
-	
-	private void captureAtom(Atom atom, ArrayList<Atom> freeAtoms, ArrayList<Atom> ownedAtoms){
-		
-		// Transfer the atom to the list of atoms "owned" by this channel.
-		freeAtoms.remove(atom);
-		ownedAtoms.add(atom);
-		
-		double velocity = 100; // Arbitrary - working on this.
-		
-		// Move the atom so that it is exactly at the entrance of the channel.
-		if ( atom.getPositionReference().distance(new Point2D.Double(0, 0)) >
-		     getCenterLocation().distance(new Point2D.Double(0, 0)))
-		{
-			// Atom is outside of the membrane, so move it to the outer
-			// entrance of the channel.
-			double xPos = CAPTURE_DISTANCE * Math.cos(getRotationalAngle()) + getCenterLocation().getX();
-			double yPos = CAPTURE_DISTANCE * Math.sin(getRotationalAngle()) + getCenterLocation().getY();
-			atom.setPosition(xPos, yPos);
-			
-			// Set the velocity of the atom such that it is moving from the
-			// outside to the inside of the membrane.
-			atom.setVelocity(-velocity * Math.cos(getRotationalAngle()), -velocity * Math.sin(getRotationalAngle()));
-		}
-		else{
-			// Atom is inside of the membrane, so move it to the inner
-			// entrance of the channel.
-			double xPos = -CAPTURE_DISTANCE * Math.cos(getRotationalAngle()) + getCenterLocation().getX();
-			double yPos = -CAPTURE_DISTANCE * Math.sin(getRotationalAngle()) + getCenterLocation().getY();
-			atom.setPosition(xPos, yPos);
-			
-			// Set the velocity of the atom such that it is moving from the
-			// outside to the inside of the membrane.
-			atom.setVelocity(velocity * Math.cos(getRotationalAngle()), velocity * Math.sin(getRotationalAngle()));
-		}
 	}
 
 	@Override
