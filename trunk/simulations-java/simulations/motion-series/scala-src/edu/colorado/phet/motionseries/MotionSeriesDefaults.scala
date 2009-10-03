774bd5316f87c8ae9dc7f4ed2849581c886c528f
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-10-03 13:56:37 +0000

    robot-moving-company: Fixed: More force or lighter objects for F&M Mode, some objects immobile

--- trunk/simulations-java/simulations/motion-series/scala-src/edu/colorado/phet/motionseries/MotionSeriesDefaults.scala
+++ trunk/simulations-java/simulations/motion-series/scala-src/edu/colorado/phet/motionseries/MotionSeriesDefaults.scala
@@ -24,7 +24,7 @@ object MotionSeriesDefaults {
   val MAX_RECORD_TIME = MAX_CHART_DISPLAY_TIME
   val defaultRampAngle = 30.0.toRadians
   val rampRobotForce = 500
-  val forcesAndMotionRobotForce = 1000
+  val forcesAndMotionRobotForce = 2000
 
   val fullScreenArea = new StageContainerArea() {
     def getBounds(w: Double, h: Double) = new Rectangle2D.Double(0, 0, w, h)
