c8d49f8409984146e4e1f40305d8fda8eeb4d7f9
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-10-04 21:34:21 +0000

    Natural selection: reformat

--- simulations-java/simulations/natural-selection/src/edu/colorado/phet/naturalselection/test/ToolBarTest.java
+++ simulations-java/simulations/natural-selection/src/edu/colorado/phet/naturalselection/test/ToolBarTest.java
@@ -1,39 +1,6 @@
-/*
- * Copyright (c) 1995 - 2008 Sun Microsystems, Inc.  All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- *   - Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *
- *   - Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- *
- *   - Neither the name of Sun Microsystems nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
- * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package edu.colorado.phet.naturalselection.test;
 
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
@@ -50,18 +17,15 @@ public class ToolBarTest extends JPanel {
     private JToolBar toolBar;
 
     static private Component placeholder = new JLabel( "Placeholder" );
-    private static LayoutManager layout = new GridLayout( 1, 1);
+    private static LayoutManager layout = new GridLayout( 1, 1 );
     private PhetPCanvas canvas = new PhetPCanvas( new Dimension( 100, 100 ) );
     private static final Dimension CANVAS_SIZE = new Dimension( 200, 200 );
-    //static private Component content;
 
     public ToolBarTest() {
         super( layout );
 
         //Create the toolbar.
         toolBar = new JToolBar( "Pedigree Chart" );
-        //placeholder = Box.createRigidArea( new Dimension( 5, 5 ) );
-        //toolBar.add( placeholder );
 
         toolBar.addPropertyChangeListener( new PropertyChangeListener() {
             public void propertyChange( PropertyChangeEvent evt ) {
@@ -119,13 +83,10 @@ public class ToolBarTest extends JPanel {
     private void onDock() {
         System.out.println( "onDock()" );
         remove( placeholder );
-        //setLayout( new GridLayout( 1, 1 ) );
     }
 
     private void onUndock() {
         System.out.println( "onUndock()" );
-        //add( placeholder );
-        //setLayout( new GridLayout( 2, 1 ) );
         add( placeholder );
     }
 
