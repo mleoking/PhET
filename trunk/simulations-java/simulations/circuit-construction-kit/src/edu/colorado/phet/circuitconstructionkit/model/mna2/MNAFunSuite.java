b7cc839914a3a65e5ed4c9816259c25a19b302da
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-10-07 00:20:51 +0000

    factored out test

--- trunk/simulations-java/simulations/circuit-construction-kit/src/edu/colorado/phet/circuitconstructionkit/model/mna2/MNAFunSuite.java
+++ trunk/simulations-java/simulations/circuit-construction-kit/src/edu/colorado/phet/circuitconstructionkit/model/mna2/MNAFunSuite.java
@@ -184,33 +184,7 @@ public class MNAFunSuite extends TestCase {
         assertTrue( circuit.solve().approxEquals( desiredSolution ) );
     }
 
-    public void testAnalyticalSolution() {
-        double vBattery = 9;
-        double rResistor = 9;
-        double c = 1E-2;
-
-        double v = 0;
-        double i = vBattery / rResistor;
-
-        double dt = 1E-4;
-        System.out.println("i \t t \t vResistorAnalytical \t vResistorPredicted");
-        for (int j = 0; j < 1000; j++) {
-            double t = j *dt;
-            double vResistorAnalytical = -vBattery * Math.exp(-t / rResistor / c);
-            double vResistorPredicted = v -vBattery;
-
-//            System.out.println(i + "\t" + t + "\t" + vt + "\t" + it + "\t" + voltageDropAcrossResistor+"\t"+predictedVoltageDropAcrossResistor);
-            System.out.println(j + "\t" + t +"\t"+ vResistorAnalytical +"\t"+ vResistorPredicted);
-
-            double rTotal = dt / 2 / c + rResistor;//total equivalent resistance
-            double vTotal = vBattery - v - dt / 2 / c * i;
-            double iTotal = vTotal / rTotal;
-
-            i = iTotal;
-            v = vBattery - vTotal - dt / 2 / c * i + dt / 2 / c * i;
-        }
-    }
-
+    //this is for comparison with TestTheveninCapacitorRC
     public void testVRC991Eminus2(){
         testVRCCircuit(9,9,1E-2);
     }
