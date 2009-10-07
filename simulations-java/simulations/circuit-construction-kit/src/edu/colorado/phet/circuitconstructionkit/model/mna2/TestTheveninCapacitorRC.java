a653de72e70a5a861b709b33c1fa28cfed0e3358
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-10-07 03:22:06 +0000

    synchronized thevenin model with analytical result, see #1813

--- simulations-java/simulations/circuit-construction-kit/src/edu/colorado/phet/circuitconstructionkit/model/mna2/TestTheveninCapacitorRC.java
+++ simulations-java/simulations/circuit-construction-kit/src/edu/colorado/phet/circuitconstructionkit/model/mna2/TestTheveninCapacitorRC.java
@@ -4,7 +4,7 @@ import java.text.DecimalFormat;
 
 public class TestTheveninCapacitorRC {
     public static void main(String[] args) {
-        DecimalFormat f=new DecimalFormat("0.0000000000");
+        DecimalFormat f = new DecimalFormat("0.0000000000");
         double vBattery = 9;
         double rResistor = 9;
         double c = 1E-2;
@@ -16,19 +16,19 @@ public class TestTheveninCapacitorRC {
         System.out.println("i \t t \t vResistorAnalytical \t vResistorPredicted \t error");
         for (int j = 0; j < 1000; j++) {
             double t = j * dt;
-            double vResistorAnalytical = -vBattery * Math.exp(-t / rResistor / c);
-            double vResistorPredicted = v - vBattery;
+            double vResistorAnalytical = vBattery * Math.exp(-t / rResistor / c);
+            double vResistorPredicted = vBattery - v;
 
             double error = Math.abs(vResistorAnalytical - vResistorPredicted);
-//            System.out.println(i + "\t" + t + "\t" + vt + "\t" + it + "\t" + voltageDropAcrossResistor+"\t"+predictedVoltageDropAcrossResistor);
-            System.out.println(j + "\t" + f.format(t) + "\t" + f.format(vResistorAnalytical) + "\t" + f.format(vResistorPredicted)+"\t"+f.format(error));
+            System.out.println(j + "\t" + f.format(t) + "\t" + f.format(vResistorAnalytical) + "\t" + f.format(vResistorPredicted) + "\t" + f.format(error));
 
-            double rTotal = dt / 2 / c + rResistor;//total equivalent resistance
-            double vTotal = vBattery - v - dt / 2 / c * i;
-            double iTotal = vTotal / rTotal;
+            double vc = v + dt / 2 / c * i;
+            double rc = dt / 2 / c;
+            double newCurrent = (vBattery - vc) / (rc + rResistor);
+            double newVoltage = vBattery - newCurrent * rResistor;//signs may be wrong here
 
-            i = iTotal;
-            v = vBattery - vTotal - dt / 2 / c * i + dt / 2 / c * i;
+            i = newCurrent;
+            v = newVoltage;
         }
     }
 }
