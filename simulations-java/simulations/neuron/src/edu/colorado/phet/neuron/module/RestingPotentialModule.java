5ec6fba1d54a46d07cdb727aa26888f5a66f5ef2
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-10-09 21:25:44 +0000

    Created new module for Resting Potential.

---
+++ simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/module/RestingPotentialModule.java
@@ -0,0 +1,80 @@
+/* Copyright 2009, University of Colorado */
+
+package edu.colorado.phet.neuron.module;
+
+import java.awt.Frame;
+
+import edu.colorado.phet.common.piccolophet.PiccoloModule;
+import edu.colorado.phet.common.piccolophet.nodes.mediabuttons.PiccoloClockControlPanel;
+import edu.colorado.phet.neuron.NeuronStrings;
+import edu.colorado.phet.neuron.controlpanel.NeuronControlPanel;
+import edu.colorado.phet.neuron.model.NeuronClock;
+import edu.colorado.phet.neuron.model.AxonModel;
+import edu.colorado.phet.neuron.view.NeuronCanvas;
+
+/**
+ * Module that illustrates the concept of Resting Potential.
+ *
+ * @author John Blanco
+ */
+public class RestingPotentialModule extends PiccoloModule {
+
+    //----------------------------------------------------------------------------
+    // Instance data
+    //----------------------------------------------------------------------------
+
+    private AxonModel model;
+    private NeuronCanvas canvas;
+    private NeuronControlPanel controlPanel;
+    private PiccoloClockControlPanel clockControlPanel;
+
+    //----------------------------------------------------------------------------
+    // Constructors
+    //----------------------------------------------------------------------------
+
+    public RestingPotentialModule( Frame parentFrame ) {
+        super( NeuronStrings.TITLE_MEMBRANE_DIFFUSION_MODULE, new NeuronClock( NeuronDefaults.CLOCK_FRAME_RATE,
+        		NeuronDefaults.CLOCK_DT ) );
+
+        // Model
+        NeuronClock clock = (NeuronClock) getClock();
+        model = new AxonModel( clock );
+
+        // Canvas
+        canvas = new NeuronCanvas( model );
+        setSimulationPanel( canvas );
+
+        // Control Panel
+        controlPanel = new NeuronControlPanel( this, parentFrame, model, canvas );
+        setControlPanel( controlPanel );
+        
+        // Clock controls
+        clockControlPanel = new PiccoloClockControlPanel( getClock() );
+        setClockControlPanel( clockControlPanel );
+
+        // Help
+        if ( hasHelp() ) {
+            //XXX add help items
+        }
+
+        // Set initial state
+        reset();
+    }
+
+    //----------------------------------------------------------------------------
+    // Module overrides
+    //----------------------------------------------------------------------------
+
+    /**
+     * Resets the module.
+     */
+    public void reset() {
+
+        // Reset the clock
+        NeuronClock clock = model.getClock();
+        clock.resetSimulationTime();
+        
+        // Reset the model.
+        model.reset();
+    }    
+}
