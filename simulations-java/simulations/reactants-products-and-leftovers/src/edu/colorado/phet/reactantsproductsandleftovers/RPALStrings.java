2998c89c493e7f6a130be6b734cb1d79db2ed4f8
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-10-09 19:33:36 +0000

    i18n of all strings used in panels 1 & 3

--- simulations-java/simulations/reactants-products-and-leftovers/src/edu/colorado/phet/reactantsproductsandleftovers/RPALStrings.java
+++ simulations-java/simulations/reactants-products-and-leftovers/src/edu/colorado/phet/reactantsproductsandleftovers/RPALStrings.java
@@ -3,17 +3,31 @@
 package edu.colorado.phet.reactantsproductsandleftovers;
 
 
-/**
- * TemplateStrings is the collection of localized strings used by this simulations.
+/*
+ * Collection of localized strings used by this project. 
  * We load all strings as statics so that we will be warned at startup time of any missing strings.
- *
+ * 
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
 public class RPALStrings {
-    
+
     /* not intended for instantiation */
     private RPALStrings() {}
+
+    // labels
+    public static final String LABEL_SANDWICH_FORMULA = RPALResources.getString( "label.sandwichFormula" );
+    public static final String LABEL_REACTION_FORMULA = RPALResources.getString( "label.reactionFormula" );
+    public static final String LABEL_BEFORE_SANDWICH = RPALResources.getString( "label.beforeSandwich" );
+    public static final String LABEL_AFTER_SANDWICH = RPALResources.getString( "label.afterSwndwich" );
+    public static final String LABEL_BEFORE_REACTION = RPALResources.getString( "label.beforeReaction" );
+    public static final String LABEL_AFTER_REACTION = RPALResources.getString( "label.afterReaction" );
+
+    // radio buttons
+    public static final String RADIO_BUTTON_MAKE_WATER = RPALResources.getString( "radioButton.makeWater" );
+    public static final String RADIO_BUTTON_MAKE_AMMONIA = RPALResources.getString( "radioButton.makeAmmonia" );
+    public static final String RADIO_BUTTON_COMBUST_METHANE = RPALResources.getString( "radioButton.combustMethane" );
     
-    public static final String TITLE_SANDWICH_SHOP = RPALResources.getString( "title.sandwichShop");
-    public static final String TITLE_REAL_REACTION = RPALResources.getString( "title.realReaction");
+    // titles
+    public static final String TITLE_SANDWICH_SHOP = RPALResources.getString( "title.sandwichShop" );
+    public static final String TITLE_REAL_REACTION = RPALResources.getString( "title.realReaction" );
 }
