570ced3f25e17c1207b646b61db597c39604aa24
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-10-14 01:17:07 +0000

    adjust layout as sandwich image changes

--- trunk/simulations-java/simulations/reactants-products-and-leftovers/src/edu/colorado/phet/reactantsproductsandleftovers/controls/ProductQuantityDisplayNode.java
+++ trunk/simulations-java/simulations/reactants-products-and-leftovers/src/edu/colorado/phet/reactantsproductsandleftovers/controls/ProductQuantityDisplayNode.java
@@ -1,7 +1,11 @@
 package edu.colorado.phet.reactantsproductsandleftovers.controls;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
 import edu.colorado.phet.common.phetcommon.util.IntegerRange;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
+import edu.colorado.phet.common.piccolophet.util.PNodeLayoutUtils;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.nodes.PText;
 import edu.umd.cs.piccolo.util.PDimension;
@@ -14,6 +18,7 @@ public class ProductQuantityDisplayNode extends PComposite {
     
     private final IntegerHistogramBarNode barNode;
     private final PText valueNode;
+    private final PNode imageNode;
     
     public ProductQuantityDisplayNode( IntegerRange range, PNode imageNode, double imageScale ) {
         super();
@@ -21,6 +26,7 @@ public class ProductQuantityDisplayNode extends PComposite {
         barNode = new IntegerHistogramBarNode( range, BAR_SIZE );
         valueNode = new PText( String.valueOf( range.getMax() ) ); // use max for layout
         valueNode.setFont( new PhetFont( 22 ) );
+        this.imageNode = imageNode;
         imageNode.scale( imageScale );
         
         addChild( barNode );
@@ -37,12 +43,32 @@ public class ProductQuantityDisplayNode extends PComposite {
         y = barNode.getFullBoundsReference().getMaxY() + 15;
         imageNode.setOffset( x, y );
         
+        imageNode.addPropertyChangeListener( new PropertyChangeListener() {
+            public void propertyChange( PropertyChangeEvent event ) {
+                if ( event.getPropertyName().equals( PROPERTY_FULL_BOUNDS ) ) {
+                    updateLayout();
+                }
+            }
+        });
+        
         setValue( range.getDefault() );
     }
     
     public void setValue( int value ) {
         barNode.setValue( value );
         valueNode.setText( String.valueOf( value ) );
+    }
+    
+    private void updateLayout() {
+        double x = 0;
+        double y = 0;
+        barNode.setOffset( x, y );
+        x = barNode.getFullBoundsReference().getMinX() - valueNode.getFullBoundsReference().getWidth() - 2;
+        y = barNode.getFullBoundsReference().getMaxY() - valueNode.getFullBoundsReference().getHeight();
+        valueNode.setOffset( x, y );
+        x = barNode.getFullBoundsReference().getCenterX() - ( imageNode.getFullBoundsReference().getWidth() / 2 );
+        y = barNode.getFullBoundsReference().getMaxY() - PNodeLayoutUtils.getOriginYOffset( imageNode ) + 15;
+        imageNode.setOffset( x, y );
     }
 
 }
