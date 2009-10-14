59f8efcd4d9d1ca5c14219afdf7709098f3dd784
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-10-14 03:07:34 +0000

    improve camera values

--- simulations-java/simulations/density/src/edu/colorado/phet/densityjava/view/jpct/DensityCanvasJPCT.java
+++ simulations-java/simulations/density/src/edu/colorado/phet/densityjava/view/jpct/DensityCanvasJPCT.java
@@ -3,6 +3,8 @@ package edu.colorado.phet.densityjava.view.jpct;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
 import java.awt.geom.Point2D;
 
 import javax.swing.*;
@@ -21,6 +23,7 @@ import edu.umd.cs.piccolo.util.PBounds;
 import edu.umd.cs.piccolo.util.PDimension;
 
 import com.threed.jpct.*;
+import com.jme.math.Vector3f;
 
 public class DensityCanvasJPCT extends PhetPCanvas {
     private World world;
@@ -97,7 +100,7 @@ public class DensityCanvasJPCT extends PhetPCanvas {
 
         world.addObject( box );
 
-        world.getCamera().lookAt( box.getTransformedCenter() );
+//        world.getCamera().lookAt( box.getTransformedCenter() );
     }
 
     public DensityCanvasJPCT( DensityModel model ) {
@@ -113,13 +116,60 @@ public class DensityCanvasJPCT extends PhetPCanvas {
             addBlock(model.getBlock(i));
         }
 //        addBlock(new DraggableBlockJPCT(model.getWater(), modelViewTransform2D));
-        world.getCamera().rotateCameraZ((float) Math.PI);
+
+
+              //for use with camera steering, see below
+        Vector3f location = new Vector3f(4.2810082f, 7.02f, 10.8f), direction = new Vector3f(0.0f, -0.19007912f, -0.9817688f), up = new Vector3f(0.0f, 0.975561f, -0.21972877f);
+        world.getCamera().setPosition(location.x,location.y, location.z+10);
+//        world.getCamera().lookAt(new SimpleVector(direction.x,direction.y,direction.z));
+        world.getCamera().setOrientation(new SimpleVector(direction.x,direction.y,direction.z),new SimpleVector(up.x,up.y,up.z) );
+                world.getCamera().rotateCameraZ((float) Math.PI);
+        requestFocus();
+        addKeyListener(new KeyListener() {
+            public void keyTyped(KeyEvent e) {
+            }
+
+            public void keyPressed(KeyEvent e) {
+                System.out.println("e = " + e);
+                SimpleVector d = getDelta(e);
+                if (e.isControlDown()){
+                    d.add(world.getCamera().getDirection());
+                    world.getCamera().setOrientation(d,world.getCamera().getUpVector());
+                }
+                else{
+                    d.add(world.getCamera().getPosition());
+                    world.getCamera().setPosition(d);
+                }
+            }
+
+            private SimpleVector getDelta(KeyEvent e) {
+                float dx = (float) 0.3;
+                if (e.getKeyCode()==KeyEvent.VK_LEFT){
+                    return new SimpleVector(dx,0,0);
+                } else if (e.getKeyCode() == KeyEvent.VK_RIGHT){
+                    return new SimpleVector(-dx,0,0);
+                }else if (e.getKeyCode() == KeyEvent.VK_UP){
+                    return new SimpleVector(0,dx,0);
+                }
+                else if (e.getKeyCode() == KeyEvent.VK_DOWN){
+                    return new SimpleVector(0,-dx,0);
+                }
+                else return new SimpleVector(0,0,0);
+            }
+
+            public void keyReleased(KeyEvent e) {
+            }
+        });
+//        world.getCamera().
+//        cam.setDirection(direction);
+//        cam.setUp(up);
 
         setOpaque( false );
 
         Timer timer = new Timer( 30, new ActionListener() {
             public void actionPerformed( ActionEvent e ) {
 //                box.rotateY( 0.01f );
+                requestFocus();
                 repaint();
             }
         } );
