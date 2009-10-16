b0fe186969c9dd327a58006344fdc802095a760b
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-10-16 15:38:09 +0000

    update based on tests with Dubson, including logging, fixing time accumulation and clamping equivalent capacitance

--- trunk/simulations-java/simulations/circuit-construction-kit/src/edu/colorado/phet/circuitconstructionkit/model/mna2/TestTheveninCapacitorRC.java
+++ trunk/simulations-java/simulations/circuit-construction-kit/src/edu/colorado/phet/circuitconstructionkit/model/mna2/TestTheveninCapacitorRC.java
@@ -1,5 +1,11 @@
 package edu.colorado.phet.circuitconstructionkit.model.mna2;
 
+import edu.colorado.phet.common.phetcommon.math.MathUtil;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.DecimalFormat;
 
 public class TestTheveninCapacitorRC {
@@ -13,12 +19,13 @@ public class TestTheveninCapacitorRC {
         }
 
         public double distance(State state) {
-            return (Math.abs(state.v - v) + Math.abs(state.i - i))/2;//todo: when comparing entire circuits, important to divide by number of components
+//            return (Math.abs(state.v - v) + Math.abs(state.i - i))/2;//todo: when comparing entire circuits, important to divide by number of components
+            return Math.abs(state.i - i);//todo: when comparing entire circuits, important to divide by number of components
         }
     }
 
-    public static void main(String[] args) {
-        DecimalFormat f = new DecimalFormat("0.0000000000");
+    public static void main(String[] args) throws IOException {
+        DecimalFormat f = new DecimalFormat("0.000000000000000000");
         double vBattery = 9;
         double rResistor = 1E-6;
         double c = 1E-1;
@@ -26,21 +33,40 @@ public class TestTheveninCapacitorRC {
         State state = new State(0, vBattery / rResistor);
 
 //        double dt = 0.03;
-        System.out.println("iteration \t dt \t t \t v(t) \t i(t) \t vResistorAnalytical \t vResistorPredicted \t error");
+        String headers = "iteration \t dt \t t \t v(t) \t i(t) \t vResistorAnalytical \t vResistorPredicted \t error";
+        System.out.println(headers);
+
 //        double[] dtArray =new double[]{1E-6,1E-6,1E-5,1E-4,1E-3,1E-2,1E-2,1E-2};
-        for (int j = 0; j < 10000; j++) {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(new File("C:/Users/Sam/Desktop/cck-out" + System.currentTimeMillis() + ".txt")));
+        bufferedWriter.write(headers + "\n");
+        double t = 0;
+        double BASE_DT = 0.03;
+        for (int j = 0; j < 5000; j++) {
+
+            if (j > 2000) {
+//                System.out.println("");
+                double x = 3;
+                x = x*23;
+            }
 //            double dt = dtArray[j];
-            double dt = getTimestep(vBattery, rResistor, c, state, 0.03);
+//            double dt = Math.min(0.03, Math.max(rResistor* c, 0.2 * state.v * c / state.i));
+            double dt = getTimestep(vBattery, rResistor, c, state, BASE_DT);
 //            System.out.println("Chose dt = "+dt);
-            double t = j * dt;
+
+//            double t = j * dt;
+
             double vResistorAnalytical = vBattery * Math.exp(-t / rResistor / c);
             double vResistorPredicted = vBattery - state.v;
 
             double error = Math.abs(vResistorAnalytical - vResistorPredicted);
-            System.out.println(j + "\t" + f.format(dt)+"\t"+f.format(t) + "\t" + f.format(state.v) + "\t" + f.format(state.i) + "\t" + f.format(vResistorAnalytical) + "\t" + f.format(vResistorPredicted) + "\t" + f.format(error));
+            String str = j + "\t" + f.format(dt) + "\t" + f.format(t) + "\t" + f.format(state.v) + "\t" + f.format(state.i) + "\t" + f.format(vResistorAnalytical) + "\t" + f.format(vResistorPredicted) + "\t" + f.format(error);
+            System.out.println(str);
+            bufferedWriter.write(str + "\n");
 
             state = newState(vBattery, rResistor, c, state, dt);
+            t = t + dt;
         }
+        bufferedWriter.close();
     }
 
     private static double getTimestep(double vBattery, double rResistor, double c, State state, double dt) {
@@ -49,21 +75,20 @@ public class TestTheveninCapacitorRC {
         State b1 = newState(vBattery, rResistor, c, state, dt / 2);
         State b2 = newState(vBattery, rResistor, c, b1, dt / 2);
         double dist = a.distance(b2);
-        if (dist < 1E-2) return dt;
+        if (dist < 0.01) return dt;
         else return getTimestep(vBattery, rResistor, c, state, dt / 2);
     }
 
     private static State newState(double vBattery, double rResistor, double c, State state, double dt) {
-        double vc = state.v + dt / 2 / c * state.i;
+        double vc = MathUtil.clamp(-9,state.v + dt / 2 / c * state.i,9);
         double rc = dt / 2 / c;
         double newCurrent = (vBattery - vc) / (rc + rResistor);
         double newVoltage = vBattery - newCurrent * rResistor;//signs may be wrong here
 
-        state = new State(newVoltage, newCurrent);
-        return state;
+        return new State(newVoltage, newCurrent);
     }
 
-    private static double getTimestep(double v, double i) {
-        return 0;
-    }
+//    private static double getTimestep(double v, double i) {
+//        return 0;
+//    }
 }
