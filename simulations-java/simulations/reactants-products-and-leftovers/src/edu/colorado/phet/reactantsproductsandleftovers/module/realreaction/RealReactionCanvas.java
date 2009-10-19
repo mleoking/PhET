4a6d71ca0c55339c28160a05c0fe7f500c05d712
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-10-19 13:04:52 +0000

    delete unused constants

--- simulations-java/simulations/reactants-products-and-leftovers/src/edu/colorado/phet/reactantsproductsandleftovers/module/realreaction/RealReactionCanvas.java
+++ simulations-java/simulations/reactants-products-and-leftovers/src/edu/colorado/phet/reactantsproductsandleftovers/module/realreaction/RealReactionCanvas.java
@@ -4,7 +4,6 @@ package edu.colorado.phet.reactantsproductsandleftovers.module.realreaction;
 import java.awt.geom.Dimension2D;
 
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
-import edu.colorado.phet.reactantsproductsandleftovers.RPALConstants;
 import edu.colorado.phet.reactantsproductsandleftovers.RPALImages;
 import edu.colorado.phet.reactantsproductsandleftovers.view.RPALCanvas;
 import edu.umd.cs.piccolo.nodes.PImage;
@@ -43,9 +42,6 @@ public class RealReactionCanvas extends RPALCanvas {
         if ( worldSize.getWidth() <= 0 || worldSize.getHeight() <= 0 ) {
             // canvas hasn't been sized, blow off layout
             return;
-        }
-        else if ( RPALConstants.DEBUG_CANVAS_UPDATE_LAYOUT ) {
-            System.out.println( "SandwichShopCanvas.updateLayout worldSize=" + worldSize );//XXX
         }
 
         //XXX lay out nodes
