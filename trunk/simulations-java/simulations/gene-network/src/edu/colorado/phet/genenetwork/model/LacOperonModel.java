39406a248a6bccec4bcec04022de7ee655c3ba65
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-10-21 19:53:52 +0000

    Added listener support and initial handling of clock ticks.

--- trunk/simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/LacOperonModel.java
+++ trunk/simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/LacOperonModel.java
@@ -1,8 +1,11 @@
-/* Copyright 2007-2008, University of Colorado */
+/* Copyright 2009, University of Colorado */
 
 package edu.colorado.phet.genenetwork.model;
 
 import java.util.ArrayList;
+
+import edu.colorado.phet.common.phetcommon.model.clock.ClockAdapter;
+import edu.colorado.phet.common.phetcommon.model.clock.ClockEvent;
 
 
 
@@ -17,6 +20,7 @@ public class LacOperonModel {
     
     private final GeneNetworkClock clock;
     private ArrayList<SimpleModelElement> simpleModelElements = new ArrayList<SimpleModelElement>();
+    protected ArrayList<Listener> listeners = new ArrayList<Listener>();
 
     //----------------------------------------------------------------------------
     // Constructors
@@ -26,6 +30,15 @@ public class LacOperonModel {
         super();
         
         this.clock = clock;        
+        
+        clock.addClockListener(new ClockAdapter(){
+
+			@Override
+			public void clockTicked(ClockEvent clockEvent) {
+				handleClockTicked();
+			}
+        	
+        });
     }
 
     //----------------------------------------------------------------------------
@@ -42,5 +55,53 @@ public class LacOperonModel {
     
     private void addModelElement(SimpleModelElement modelElement){
     	simpleModelElements.add(modelElement);
+    }
+    
+    /**
+     * Get a list of all simple model elements in the model, including any
+     * that are currently bound up in composite model elements.
+     * @return
+     */
+    public ArrayList<SimpleModelElement> getAllSimpleModelElements(){
+    	return new ArrayList<SimpleModelElement>(simpleModelElements);
+    }
+    
+    private void handleClockTicked(){
+    	// Update the position of each of the simple model elements.
+    	for (SimpleModelElement modelElement : simpleModelElements){
+    		modelElement.updatePosition();
+    	}
+    }
+    
+    //------------------------------------------------------------------------
+    // Listener support
+    //------------------------------------------------------------------------
+	
+    protected void notifyModelElementAdded(SimpleModelElement modelElement){
+        // Notify all listeners of the addition of this model element.
+        for (Listener listener : listeners)
+        {
+            listener.modelElementAdded(modelElement); 
+        }        
+    }
+
+    public void addListener(Listener listener) {
+        if (listeners.contains( listener ))
+        {
+            // Don't bother re-adding.
+        	System.err.println(getClass().getName() + "- Warning: Attempting to re-add a listener that is already listening.");
+        	assert false;
+            return;
+        }
+        
+        listeners.add( listener );
+    }
+    
+    public void removeListener(Listener listener){
+    	listeners.remove(listener);
+    }
+	
+    public interface Listener {
+        void modelElementAdded(SimpleModelElement modelElement);
     }
 }
