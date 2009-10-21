16442f5b757b8af6ba3fcba88ce07309763f462f
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-10-21 21:55:27 +0000

    Initial creation of this model class.

---
+++ simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/Glucose.java
@@ -0,0 +1,14 @@
+/* Copyright 2009, University of Colorado */
+
+package edu.colorado.phet.genenetwork.model;
+
+import java.awt.Color;
+import java.awt.geom.Point2D;
+
+public class Glucose extends SimpleSugar {
+
+	public Glucose(Point2D initialPosition) {
+		super(initialPosition, Color.BLUE);
+	}
+
+}
