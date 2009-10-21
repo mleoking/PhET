59852b5ee38c85dd29b462c03582bb576a380a3e
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-10-21 21:53:04 +0000

    New class on from which the simple sugars will derive.

---
+++ simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/SimpleSugar.java
@@ -0,0 +1,46 @@
+/* Copyright 2009, University of Colorado */
+
+package edu.colorado.phet.genenetwork.model;
+
+import java.awt.Paint;
+import java.awt.Shape;
+import java.awt.geom.Point2D;
+
+import edu.colorado.phet.common.phetcommon.math.Vector2D;
+import edu.colorado.phet.common.phetcommon.view.util.DoubleGeneralPath;
+
+
+/**
+ * Class that represents LacZ, which is the model element that breaks up the
+ * lactose.
+ * 
+ * @author John Blanco
+ */
+public abstract class SimpleSugar extends SimpleModelElement {
+	
+	private static double HEIGHT = 3;
+	
+	public SimpleSugar(Point2D initialPosition, Paint paint) {
+		super(createShape(), initialPosition, paint);
+	}
+	
+	private static Shape createShape(){
+		// Create a hexagon shape.
+		DoubleGeneralPath path = new DoubleGeneralPath();
+		double length = HEIGHT / 2 / Math.sin(Math.PI/3);
+		path.moveTo(-length / 2, -HEIGHT / 2);
+		
+		double angle = Math.PI;
+		
+		for (int i = 0; i < 6; i++){
+			angle = lineToRelative(path, length, angle);
+		}
+		
+		return path.getGeneralPath();
+	}
+
+	private static double lineToRelative(DoubleGeneralPath path, double length, double angle) {
+		path.lineToRelative(Vector2D.Double.parseAngleAndMagnitude(length, angle));
+		return angle + Math.PI / 3;
+	}
+}
