71bf89d547a93fb694a7ad217510c9de8f587304
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-10-21 15:31:08 +0000

    doc

--- trunk/simulations-java/simulations/reactants-products-and-leftovers/src/edu/colorado/phet/reactantsproductsandleftovers/controls/IntegerSliderNode.java
+++ trunk/simulations-java/simulations/reactants-products-and-leftovers/src/edu/colorado/phet/reactantsproductsandleftovers/controls/IntegerSliderNode.java
@@ -31,6 +31,7 @@ import edu.umd.cs.piccolo.util.PDimension;
  */
 public class IntegerSliderNode extends PNode {
     
+    //XXX decide on 2D or 3D knob, then delete the other
     private static final boolean USE_3D_KNOB = false;
     
     //----------------------------------------------------------------------------
