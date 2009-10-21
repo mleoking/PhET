56379c0c2a36713584f956d08ff4390e32e24495
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-10-21 21:05:38 +0000

    Intermediate step towards reaching the real shape.

--- trunk/simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/LacZModelElement.java
+++ trunk/simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/LacZModelElement.java
@@ -5,6 +5,7 @@ package edu.colorado.phet.genenetwork.model;
 import java.awt.Color;
 import java.awt.Paint;
 import java.awt.Shape;
+import java.awt.geom.Area;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Point2D;
 
@@ -19,8 +20,21 @@ public class LacZModelElement extends SimpleModelElement {
 	
 	private static final Shape ELEMENT_SHAPE = new Ellipse2D.Double(-5, -10, 10, 20);
 	private static final Paint ELEMENT_PAINT = new Color(219, 198, 212);
+	private static double SIZE = 8;
 	
 	public LacZModelElement(Point2D initialPosition) {
-		super(ELEMENT_SHAPE, initialPosition, ELEMENT_PAINT);
+		super(createShape(), initialPosition, ELEMENT_PAINT);
+	}
+	
+	public LacZModelElement() {
+		this(new Point2D.Double());
+	}
+	
+	private static Shape createShape(){
+		Ellipse2D startingShape = new Ellipse2D.Double(-SIZE/2, -SIZE/2, SIZE, SIZE);
+		Area area = new Area(startingShape);
+		area.subtract(new Area(new LactoseModelElement().getShape()));
+		return area;
+		
 	}
 }
