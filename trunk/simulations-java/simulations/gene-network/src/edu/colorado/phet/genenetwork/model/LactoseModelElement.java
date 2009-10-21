d8e38d48d70bd4e331bf92f7729ccdf56596887e
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-10-21 21:54:40 +0000

    Cleaned up imports.

--- trunk/simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/LactoseModelElement.java
+++ trunk/simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/LactoseModelElement.java
@@ -3,7 +3,6 @@ package edu.colorado.phet.genenetwork.model;
 import java.awt.Color;
 import java.awt.Paint;
 import java.awt.Shape;
-import java.awt.geom.Ellipse2D;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 
