14356309b4b65fa1d4cdaf68ae57bafb1f37b96c
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-10-21 20:34:17 +0000

    Fixed a bug with the shape definition.

--- trunk/simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/LacZModelElement.java
+++ trunk/simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/LacZModelElement.java
@@ -17,7 +17,7 @@ import java.awt.geom.Point2D;
  */
 public class LacZModelElement extends SimpleModelElement {
 	
-	private static final Shape ELEMENT_SHAPE = new Ellipse2D.Double(-5, -10, 10,  10);
+	private static final Shape ELEMENT_SHAPE = new Ellipse2D.Double(-5, -10, 10, 20);
 	private static final Paint ELEMENT_PAINT = Color.GREEN;
 	
 	public LacZModelElement(Point2D initialPosition) {
