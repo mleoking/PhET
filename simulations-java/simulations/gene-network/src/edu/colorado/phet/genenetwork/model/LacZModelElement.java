6f1292565e0e9bff39df8ecb312316e3eb39c1f5
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-10-21 20:08:30 +0000

    Created a model element for the LacZ operon.

---
+++ simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/LacZModelElement.java
@@ -0,0 +1,26 @@
+/* Copyright 2009, University of Colorado */
+
+package edu.colorado.phet.genenetwork.model;
+
+import java.awt.Color;
+import java.awt.Paint;
+import java.awt.Shape;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Point2D;
+
+
+/**
+ * Class that represents LacZ, which is the model element that breaks up the
+ * lactose.
+ * 
+ * @author John Blanco
+ */
+public class LacZModelElement extends SimpleModelElement {
+	
+	private static final Shape ELEMENT_SHAPE = new Ellipse2D.Double(-5, -10, 10,  10);
+	private static final Paint ELEMENT_PAINT = Color.GREEN;
+	
+	public LacZModelElement(Point2D initialPosition) {
+		super(ELEMENT_SHAPE, initialPosition, ELEMENT_PAINT);
+	}
+}
