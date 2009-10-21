0dd191cb964f9bb7e4eeaa0779ce52a710b38603
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-10-21 20:35:41 +0000

    Added a centering dot so that we can better evaluate shapes (for debug purposes).

--- simulations-java/simulations/gene-network/src/edu/colorado/phet/genetwork/view/SimpleModelElementNode.java
+++ simulations-java/simulations/gene-network/src/edu/colorado/phet/genetwork/view/SimpleModelElementNode.java
@@ -3,6 +3,12 @@
 
 package edu.colorado.phet.genetwork.view;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.geom.Ellipse2D;
+
+import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform2D;
+import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.colorado.phet.genenetwork.model.SimpleModelElement;
 import edu.umd.cs.piccolo.nodes.PPath;
 
@@ -15,12 +21,14 @@ public class SimpleModelElementNode extends PPath {
 	
 	private final SimpleModelElement modelElement;
 	
-	public SimpleModelElementNode(SimpleModelElement modelElement){
+	private PhetPPath centerDot = new PhetPPath(Color.RED, new BasicStroke(2), Color.RED);
+	
+	public SimpleModelElementNode(SimpleModelElement modelElement, ModelViewTransform2D mvt){
 	
 		this.modelElement = modelElement;
 		
 		// Set up this node to look like it should.
-		setPathTo(modelElement.getShape());
+		setPathTo(mvt.createTransformedShape(modelElement.getShape()));
 		setPaint(modelElement.getPaint());
 		
 		// Register for important event notifications from the model.
@@ -31,5 +39,9 @@ public class SimpleModelElementNode extends PPath {
 				
 			}
 		});
+		
+		// Put a center dot on the node (for debug purposes).
+		centerDot.setPathTo(mvt.createTransformedShape(new Ellipse2D.Double(-1, -1, 2, 2)));
+		addChild(centerDot);
 	}
 }
