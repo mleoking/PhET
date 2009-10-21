43af955ea7d705453603c721a0db6b45a8e4aafb
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-10-21 22:18:25 +0000

    Corrected some issues with positioning of this mode based on shape and position within the model.

--- trunk/simulations-java/simulations/gene-network/src/edu/colorado/phet/genetwork/view/SimpleModelElementNode.java
+++ trunk/simulations-java/simulations/gene-network/src/edu/colorado/phet/genetwork/view/SimpleModelElementNode.java
@@ -5,6 +5,8 @@ package edu.colorado.phet.genetwork.view;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Shape;
+import java.awt.geom.AffineTransform;
 import java.awt.geom.Ellipse2D;
 
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform2D;
@@ -22,23 +24,33 @@ public class SimpleModelElementNode extends PPath {
 	private static final boolean SHOW_CENTER_DOT = false;
 	
 	private final SimpleModelElement modelElement;
+	private final ModelViewTransform2D mvt;
 	
 	private PhetPPath centerDot = new PhetPPath(Color.RED, new BasicStroke(2), Color.RED);
 	
 	public SimpleModelElementNode(SimpleModelElement modelElement, ModelViewTransform2D mvt){
 	
 		this.modelElement = modelElement;
+		this.mvt = mvt;
 		
 		// Set up this node to look like it should.
-		setPathTo(mvt.createTransformedShape(modelElement.getShape()));
+		Shape transformedShape = mvt.createTransformedShape(modelElement.getShape());
+		
+		// We only want the shape, and not any translation associated with the
+		// shape, so that we can move it in a reasonable way later.  For this,
+		// we have to subtract off the translation.
+		transformedShape.getBounds2D().getCenterX();
+		transformedShape = AffineTransform.getTranslateInstance(-transformedShape.getBounds2D().getCenterX(), 
+				-transformedShape.getBounds2D().getCenterY()).createTransformedShape(transformedShape);
+		
+		// Set the shape and color.
+		setPathTo(transformedShape);
 		setPaint(modelElement.getPaint());
 		
 		// Register for important event notifications from the model.
 		modelElement.addListener(new SimpleModelElement.Listener() {
-			
 			public void positionChanged() {
-				setOffset(SimpleModelElementNode.this.modelElement.getPosition());
-				
+				updateOffset();
 			}
 		});
 		
@@ -47,5 +59,12 @@ public class SimpleModelElementNode extends PPath {
 			centerDot.setPathTo(mvt.createTransformedShape(new Ellipse2D.Double(-1, -1, 2, 2)));
 			addChild(centerDot);
 		}
+		
+		// Set initial offset.
+		updateOffset();
 	}
+	
+    private void updateOffset() {
+        setOffset( mvt.modelToView( modelElement.getPosition() ));
+    }
 }
