447070007e2682da48a58b0a8a735aa018583f7a
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-10-22 21:38:09 +0000

    Added handler for clock ticks with initial (protype) movement for model elements.  Also added some additional elements.

--- trunk/simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/LacOperonModel.java
+++ trunk/simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/LacOperonModel.java
@@ -3,20 +3,29 @@
 package edu.colorado.phet.genenetwork.model;
 
 import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 
+import edu.colorado.phet.common.phetcommon.math.Vector2D;
 import edu.colorado.phet.common.phetcommon.model.clock.ClockAdapter;
 import edu.colorado.phet.common.phetcommon.model.clock.ClockEvent;
-
-
 
 /**
  * Model template.
  */
 public class LacOperonModel {
     
+	//----------------------------------------------------------------------------
+	// Class Data
+	//----------------------------------------------------------------------------
+	
+	private static final double MODEL_AREA_WIDTH = 100;
+	private static final double MODEL_AREA_HEIGHT = 100;
+	private static final Rectangle2D MODEL_BOUNDS = new Rectangle2D.Double(-MODEL_AREA_WIDTH / 2,
+			-MODEL_AREA_HEIGHT / 2, MODEL_AREA_WIDTH, MODEL_AREA_HEIGHT);
+	
     //----------------------------------------------------------------------------
-    // Instance data
+    // Instance Data
     //----------------------------------------------------------------------------
     
     private final GeneNetworkClock clock;
@@ -42,9 +51,24 @@ public class LacOperonModel {
         });
         
         // Add the initial model elements.
-        addModelElement(new LacZ(new Point2D.Double(0, 0)));
-        addModelElement(new Glucose(new Point2D.Double(20, 20)));
-        addModelElement(new Galactose(new Point2D.Double(-20, -35)));
+        SimpleModelElement modelElement = new LacZ(new Point2D.Double(0, 0));
+        modelElement.setVelocity(1, 1.5);
+        addModelElement(modelElement);
+        modelElement = new LacZ(new Point2D.Double(3, 3));
+        modelElement.setVelocity(-1, 1.5);
+        addModelElement(modelElement);
+        modelElement = new Glucose(new Point2D.Double(20, 20));
+        modelElement.setVelocity(1, 0.5);
+        addModelElement(modelElement);
+        modelElement = new Glucose(new Point2D.Double(20, 20));
+        modelElement.setVelocity(1, 0.5);
+        addModelElement(modelElement);
+        modelElement = new Galactose(new Point2D.Double(-20, -10));
+        modelElement.setVelocity(-1.5, 1);
+        addModelElement(modelElement);
+        modelElement = new Galactose(new Point2D.Double(15, -10));
+        modelElement.setVelocity(-1.5, 1);
+        addModelElement(modelElement);
     }
 
     //----------------------------------------------------------------------------
@@ -75,6 +99,22 @@ public class LacOperonModel {
     private void handleClockTicked(){
     	// Update the position of each of the simple model elements.
     	for (SimpleModelElement modelElement : simpleModelElements){
+    		
+    		Point2D position = modelElement.getPositionRef();
+    		Vector2D velocity = modelElement.getVelocityRef();
+    		
+    		if ((position.getX() > MODEL_BOUNDS.getMaxX() && velocity.getX() > 0) ||
+    			(position.getX() < MODEL_BOUNDS.getMinX() && velocity.getX() < 0))	{
+    			// Reverse direction in the X direction.
+    			modelElement.setVelocity(-velocity.getX(), velocity.getY());
+    		}
+    		if ((position.getY() > MODEL_BOUNDS.getMaxY() && velocity.getY() > 0) ||
+        		(position.getY() < MODEL_BOUNDS.getMinY() && velocity.getY() < 0))	{
+        		// Reverse direction in the Y direction.
+        		modelElement.setVelocity(velocity.getX(), -velocity.getY());
+        	}
+    		
+    		// Update the position based on the velocity.
     		modelElement.updatePosition();
     	}
     }
