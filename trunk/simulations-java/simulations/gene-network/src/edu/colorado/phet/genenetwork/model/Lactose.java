508c8f0e8a1ca4a5bfb3f4914bde85f1600184fb
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-10-23 17:42:26 +0000

    Added a method for getting shape, removed extension of composite class.

--- trunk/simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/Lactose.java
+++ trunk/simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/Lactose.java
@@ -1,19 +1,35 @@
 package edu.colorado.phet.genenetwork.model;
 
-import java.awt.Color;
-import java.awt.Paint;
 import java.awt.Shape;
-import java.awt.geom.Point2D;
-import java.awt.geom.Rectangle2D;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Area;
 
-public class Lactose extends CompositeModelElement{
-     public Galactose galactose;
-    public Glucose glucose;
+/**
+ * This class defines a molecule of Lactose, which is a combination of one
+ * molecule of Glucose and one of Galactose.  This class is intended for use
+ * primarily in constructing the visual representations of other molecules
+ * that bind to lactose within the sim, and is not intended to be directly
+ * used in the model.
+ * 
+ * @author John Blanco
+ */
+public class Lactose {
+	
+    public Area area;
 
-    public Lactose(double offsetX,double offsetY) {
-        glucose = new Glucose(new Point2D.Double(-Galactose.HEIGHT/2 + offsetX, offsetY));
-        add(glucose);
-        galactose = new Galactose(new Point2D.Double(Galactose.HEIGHT/2 + offsetX, offsetY));
-        add(galactose);
+    public Lactose() {
+		Shape glucoseShape = new Glucose().getShape();
+		AffineTransform transform = new AffineTransform();
+		transform.setToTranslation(	-glucoseShape.getBounds2D().getWidth()/2, 0 );
+		Shape glucoseShiftedLeft = transform.createTransformedShape(glucoseShape);
+		Shape galactoseShape = new Galactose().getShape();
+		transform.setToTranslation(	galactoseShape.getBounds2D().getWidth()/2, 0 );
+		Shape galactoseShiftedRight = transform.createTransformedShape(galactoseShape);
+		area = new Area(glucoseShiftedLeft);
+		area.add(new Area(galactoseShiftedRight));
 	}
+    
+    public Shape getShape(){
+    	return new Area(area);
+    }
 }
