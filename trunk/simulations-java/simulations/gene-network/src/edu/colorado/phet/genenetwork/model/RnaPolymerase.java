27320bff8bfb62d0e4246fe1848ee565856f9491
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-10-23 22:27:19 +0000

    Initial creation of this model element.

---
+++ trunk/simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/RnaPolymerase.java
@@ -0,0 +1,72 @@
+/* Copyright 2009, University of Colorado */
+
+package edu.colorado.phet.genenetwork.model;
+
+import java.awt.Color;
+import java.awt.GradientPaint;
+import java.awt.Paint;
+import java.awt.Shape;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Area;
+import java.awt.geom.GeneralPath;
+import java.awt.geom.Point2D;
+import java.awt.geom.Point2D.Double;
+
+
+/**
+ * Class that represents the CAP, which is an acronym for something, but I
+ * don't know what.
+ * 
+ * @author John Blanco
+ */
+public class RnaPolymerase extends SimpleModelElement {
+	
+//	private static final Paint ELEMENT_PAINT = new Color(124, 198, 240);
+	private static float WIDTH = CapBindingRegion.WIDTH + LacPromoter.WIDTH;
+	private static float HEIGHT = 8;  // In nanometers.
+	private static final Paint ELEMENT_PAINT = new GradientPaint(new Point2D.Double(-WIDTH, 0), 
+			new Color(17, 149, 210), new Point2D.Double(WIDTH * 5, 0), Color.WHITE);
+	
+	public RnaPolymerase(Point2D initialPosition) {
+		super(createActiveConformationShape(), initialPosition, ELEMENT_PAINT);
+	}
+	
+	public RnaPolymerase() {
+		this(new Point2D.Double());
+	}
+	
+	private static Shape createActiveConformationShape(){
+		
+		// Create the overall outline.
+		GeneralPath basicShape = new GeneralPath();
+		
+		basicShape.moveTo(WIDTH / 2, -HEIGHT/2);
+		basicShape.lineTo(0, -HEIGHT/2);
+		basicShape.lineTo(-WIDTH * 0.4f, 0);
+		basicShape.curveTo(0, HEIGHT * 0.6f, WIDTH/4, HEIGHT * 0.4f, WIDTH / 2, HEIGHT/4);
+		basicShape.closePath();
+		Area area = new Area(basicShape);
+		
+		// Get the shape of the promoter and shift it to the appropriate
+		// position.
+		Shape promoterShape = new LacPromoter().getShape();
+		AffineTransform transform = new AffineTransform();
+		transform.setToTranslation(	basicShape.getBounds2D().getMaxX() - promoterShape.getBounds().getMaxX(), -HEIGHT/2 );
+		promoterShape = transform.createTransformedShape(promoterShape);
+		
+		// Subtract off the shape of the binding region.
+		area.subtract(new Area(promoterShape));
+		
+		// Get the shape of the CAP and shift it to the appropriate location.
+		Shape capShape = new Cap().getShape();
+		transform = new AffineTransform();
+		transform.setToTranslation(	promoterShape.getBounds2D().getMinX() - capShape.getBounds2D().getWidth()/2, -2 );
+		capShape = transform.createTransformedShape(capShape);
+		
+		// Subtract off the shape of the camp.
+		area.subtract(new Area(capShape));
+		
+		return area;
+	}
+	
+}
