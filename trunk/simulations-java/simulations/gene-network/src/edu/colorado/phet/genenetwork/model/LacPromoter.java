73780c473ef81d45498f86b4bb56aba12d6d16dd
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-10-23 20:54:14 +0000

    Initial creation of this model element.

---
+++ trunk/simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/LacPromoter.java
@@ -0,0 +1,50 @@
+/* Copyright 2009, University of Colorado */
+
+package edu.colorado.phet.genenetwork.model;
+
+import java.awt.Color;
+import java.awt.Paint;
+import java.awt.Shape;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Area;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.GeneralPath;
+import java.awt.geom.Point2D;
+
+
+/**
+ * Class that represents lac promoter, which is the binding region on the DNA
+ * strand for the RNA polymerase.
+ * 
+ * @author John Blanco
+ */
+public class LacPromoter extends SimpleModelElement {
+	
+	private static final Paint ELEMENT_PAINT = new Color(0, 137, 225);
+	private static float HEIGHT = 3;
+	private static float WIDTH = 10;
+	
+	public LacPromoter(Point2D initialPosition) {
+		super(createShape(), initialPosition, ELEMENT_PAINT);
+	}
+	
+	public LacPromoter() {
+		this(new Point2D.Double());
+	}
+	
+	private static Shape createShape(){
+		
+		GeneralPath outline = new GeneralPath();
+		
+		outline.moveTo(WIDTH/2, HEIGHT/2);
+		outline.lineTo(WIDTH/2, -HEIGHT/2);
+		outline.lineTo(-WIDTH/2, -HEIGHT/2);
+		outline.lineTo(-WIDTH/2, HEIGHT/2);
+		outline.lineTo(-WIDTH/4, 0);
+		outline.lineTo(0, HEIGHT/2);
+		outline.lineTo(WIDTH/4, 0);
+		outline.closePath();
+		
+		return outline;
+	}
+}
