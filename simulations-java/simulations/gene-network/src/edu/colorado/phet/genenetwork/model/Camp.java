f9530aec61a9e28b315ec333a2eed1bb8ceacb7c
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-10-23 21:51:01 +0000

    Initial creation of this model element.

---
+++ simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/Camp.java
@@ -0,0 +1,34 @@
+/* Copyright 2009, University of Colorado */
+
+package edu.colorado.phet.genenetwork.model;
+
+import java.awt.Color;
+import java.awt.Paint;
+import java.awt.Shape;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Point2D;
+
+
+/**
+ * Class that represents the cAMP, which is an acronym for something, but I
+ * don't know what.
+ * 
+ * @author John Blanco
+ */
+public class Camp extends SimpleModelElement {
+	
+	private static final Paint ELEMENT_PAINT = Color.RED;
+	private static float DIAMETER = 2.0f;  // In nanometers.
+	
+	public Camp(Point2D initialPosition) {
+		super(createShape(), initialPosition, ELEMENT_PAINT);
+	}
+	
+	public Camp() {
+		this(new Point2D.Double());
+	}
+	
+	private static Shape createShape(){
+		return new Ellipse2D.Double(-DIAMETER/2, -DIAMETER/2, DIAMETER, DIAMETER);
+	}
+}
