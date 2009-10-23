5725f7bdadbb48b807c7c6a087f68d687c339c01
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-10-23 15:54:52 +0000

    Changed the apps used for testing this in order to enable Marj to run the test plan.

--- trunk/build-tools/src/edu/colorado/phet/buildtools/resource/ResourceDeployUtils.java
+++ trunk/build-tools/src/edu/colorado/phet/buildtools/resource/ResourceDeployUtils.java
@@ -76,7 +76,7 @@ public class ResourceDeployUtils {
      */
     public static File[] getJavaSimulationDirs( File trunk ) {
         if ( DEBUG ) {
-            return new File[]{new File( trunk, BuildToolsPaths.JAVA_SIMULATIONS_DIR + "/conductivity" )};
+            return new File[]{new File( trunk, BuildToolsPaths.JAVA_SIMULATIONS_DIR + "/bound-states" )};
         }
         else {
             File simsDir = new File( trunk, BuildToolsPaths.JAVA_SIMULATIONS_DIR );
@@ -100,7 +100,7 @@ public class ResourceDeployUtils {
 
     public static File[] getFlashSimulationDirs( File trunk ) {
         if ( DEBUG ) {
-            return new File[]{new File( trunk, BuildToolsPaths.FLASH_SIMULATIONS_DIR + "/curve-fitting" )};
+            return new File[]{new File( trunk, BuildToolsPaths.FLASH_SIMULATIONS_DIR + "/stern-gerlach" )};
         }
         else {
             File simsDir = new File( trunk, BuildToolsPaths.FLASH_SIMULATIONS_DIR );
