d01e3ffe7e4e41aa7af4b58d8170a36687be929f
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-10-23 19:42:56 +0000

    Initial creation of this class.

---
+++ trunk/simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/LacI.java
@@ -0,0 +1,60 @@
+/* Copyright 2009, University of Colorado */
+
+package edu.colorado.phet.genenetwork.model;
+
+import java.awt.Color;
+import java.awt.Paint;
+import java.awt.Shape;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Area;
+import java.awt.geom.GeneralPath;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+
+
+/**
+ * Class that represents LacI, which in real life is a protein that inhibits
+ * (hence the 'I' in the name) the expression of genes coding for proteins
+ * involved in lactose metabolism in bacteria.
+ * 
+ * @author John Blanco
+ */
+public class LacI extends SimpleModelElement {
+	
+	private static final Paint ELEMENT_PAINT = new Color(200, 200, 200);
+	private static double WIDTH = 6;   // In nanometers.
+	private static double HEIGHT = 4;  // In nanometers.
+	
+	public LacI(Point2D initialPosition) {
+		super(createActiveConformationShape(), initialPosition, ELEMENT_PAINT);
+	}
+	
+	public LacI() {
+		this(new Point2D.Double());
+	}
+	
+	private static Shape createActiveConformationShape(){
+		
+		// Create the overall outline.
+		GeneralPath outline = new GeneralPath();
+		
+		outline.moveTo(0, (float)HEIGHT/2);
+		outline.quadTo((float)WIDTH / 2, (float)HEIGHT / 2, (float)WIDTH/2, -(float)HEIGHT/2);
+		outline.lineTo((float)-WIDTH/2, (float)-HEIGHT/2);
+		outline.lineTo((float)-WIDTH/2, (float)(HEIGHT * 0.25));
+		outline.closePath();
+		Area area = new Area(outline);
+		
+		// Get the shape of a lactose molecule and shift it to the appropriate
+		// position.
+		Shape lactoseShape = new Lactose().getShape();
+		AffineTransform transform = new AffineTransform();
+		transform.setToTranslation(	0, HEIGHT/2 );
+		lactoseShape = transform.createTransformedShape(lactoseShape);
+		
+		// Subtract off the shape of the lactose molecule.
+		area.subtract(new Area(lactoseShape));
+		return area;
+	}
+	
+}
