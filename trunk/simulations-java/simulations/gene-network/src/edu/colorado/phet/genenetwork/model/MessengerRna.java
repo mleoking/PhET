862abd82382ee76be2bfe05b8701870db460bc20
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-10-26 17:30:59 +0000

    Improved the shape of RNA strand.

--- trunk/simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/MessengerRna.java
+++ trunk/simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/MessengerRna.java
@@ -4,9 +4,12 @@ package edu.colorado.phet.genenetwork.model;
 
 import java.awt.Color;
 import java.awt.Paint;
+import java.awt.Point;
 import java.awt.Shape;
 import java.awt.geom.GeneralPath;
 import java.awt.geom.Point2D;
+import java.awt.geom.Point2D.Double;
+import java.util.ArrayList;
 import java.util.Random;
 
 
@@ -57,6 +60,8 @@ public class MessengerRna extends SimpleModelElement {
 		return new QuadCurve2D.Double(-10, 0, 0, 5, 10, 0);
 		*/
 		
+		/*
+		 * Creates a simple curve.
 		GeneralPath path = new GeneralPath();
 		float curveHeight = 5;
 		path.moveTo(-APPROX_LENGTH/2, 0);
@@ -65,8 +70,38 @@ public class MessengerRna extends SimpleModelElement {
 		path.quadTo(0, curveHeight - THICKNESS, -APPROX_LENGTH/2, -THICKNESS);
 		path.closePath();
 		return path;
-		
+		 */
 
+		// Create the set of points that will define the curve.
+		ArrayList<Point2DFloat> curvePoints = new ArrayList<Point2DFloat>();
+		float curveHeight = 4;
+		curvePoints.add(new Point2DFloat(-APPROX_LENGTH/2, 0));
+		curvePoints.add(new Point2DFloat(-APPROX_LENGTH/3, curveHeight));
+		curvePoints.add(new Point2DFloat(-APPROX_LENGTH/6, -curveHeight));
+		curvePoints.add(new Point2DFloat(0, curveHeight/4));
+		curvePoints.add(new Point2DFloat(APPROX_LENGTH/6, curveHeight));
+		curvePoints.add(new Point2DFloat(APPROX_LENGTH/3, -curveHeight));
+		curvePoints.add(new Point2DFloat(APPROX_LENGTH/2, 0));
+		
+		// Create the path.  Note that in order to create a closed shape, the
+		// top line is drawn, then the line is essentially reversed but a
+		// little lower, and the two lines are connected.
+		GeneralPath path = new GeneralPath();
+		path.moveTo(curvePoints.get(0).getX(), curvePoints.get(0).getY());
+		path.curveTo(curvePoints.get(1).getX(), curvePoints.get(1).getY(), curvePoints.get(2).getX(),
+				curvePoints.get(2).getY(), curvePoints.get(3).getX(), curvePoints.get(3).getY());
+		path.curveTo(curvePoints.get(4).getX(), curvePoints.get(4).getY(), curvePoints.get(5).getX(),
+				curvePoints.get(5).getY(), curvePoints.get(6).getX(), curvePoints.get(6).getY());
+		
+		path.lineTo(curvePoints.get(6).getX(), curvePoints.get(6).getY() - THICKNESS);
+		path.curveTo(curvePoints.get(5).getX(), curvePoints.get(5).getY() - THICKNESS, curvePoints.get(4).getX(),
+				curvePoints.get(4).getY() - THICKNESS, curvePoints.get(3).getX(), 
+				curvePoints.get(3).getY() - THICKNESS);
+		path.curveTo(curvePoints.get(2).getX(), curvePoints.get(2).getY() - THICKNESS, curvePoints.get(1).getX(),
+				curvePoints.get(1).getY() - THICKNESS, curvePoints.get(0).getX(), 
+				curvePoints.get(0).getY() - THICKNESS);
+		path.closePath();
+		return path;
 		
 		/*
 		GeneralPath squiggly = new GeneralPath();
@@ -76,5 +111,32 @@ public class MessengerRna extends SimpleModelElement {
 		}
 		return squiggly;
 		*/
+	}
+	
+	private static class Point2DFloat{
+		private float x;
+		private float y;
+		
+		public Point2DFloat(float x, float y) {
+			super();
+			this.x = x;
+			this.y = y;
+		}
+
+		public float getX() {
+			return x;
+		}
+
+		public void setX(float x) {
+			this.x = x;
+		}
+
+		public float getY() {
+			return y;
+		}
+
+		public void setY(float y) {
+			this.y = y;
+		}
 	}
 }
