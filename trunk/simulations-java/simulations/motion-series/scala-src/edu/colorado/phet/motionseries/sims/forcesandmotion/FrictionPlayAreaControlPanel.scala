6e19f5abc45f39166f9eb5e134690cc0b31e793b
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-10-26 23:15:34 +0000

    forces-and-motion: Aligned the sliders for the friction controls in the 2nd tab.

--- trunk/simulations-java/simulations/motion-series/scala-src/edu/colorado/phet/motionseries/sims/forcesandmotion/FrictionPlayAreaControlPanel.scala
+++ trunk/simulations-java/simulations/motion-series/scala-src/edu/colorado/phet/motionseries/sims/forcesandmotion/FrictionPlayAreaControlPanel.scala
@@ -1,6 +1,5 @@
 package edu.colorado.phet.motionseries.sims.forcesandmotion
 
-import edu.colorado.phet.common.phetcommon.view.controls.valuecontrol.{HorizontalLayoutStrategy, AbstractValueControl}
 import edu.colorado.phet.common.phetcommon.view.VerticalLayoutPanel
 import java.awt.image.BufferedImage
 import java.util.Hashtable
@@ -9,7 +8,9 @@ import edu.colorado.phet.motionseries.MotionSeriesResources
 import edu.colorado.phet.motionseries.MotionSeriesResources._
 import edu.colorado.phet.motionseries.MotionSeriesDefaults._
 import edu.colorado.phet.motionseries.swing._
-import edu.colorado.phet.motionseries.model.{ForceBead, Bead}
+import edu.colorado.phet.motionseries.model.{MotionSeriesModel, ForceBead, Bead}
+import edu.colorado.phet.common.phetcommon.view.controls.valuecontrol.{AlignedSliderSetLayoutStrategy, HorizontalLayoutStrategy, AbstractValueControl}
+import java.awt.{GridBagConstraints, GridBagLayout, GridLayout}
 
 class MyValueControl(min: Double, max: Double, getter: () => Double, setter: Double => Unit, title: String, numberFormat: String, units: String, bead: Bead)
         extends ScalaValueControl(min, max, title, numberFormat, units, getter, setter, bead.addListener, new HorizontalLayoutStrategy) {
@@ -23,8 +24,9 @@ class FrictionPlayAreaControlPanel(bead: ForceBead) extends VerticalLayoutPanel 
   val kineticFriction = new MyValueControl(0.0, 2.0, () => bead.kineticFriction, bead.kineticFriction = _, "Coefficient of kinetic friction", "0.0".literal, "".literal, bead)
   val objectMass = new MyValueControl(1, 200, () => bead.mass, bead.mass = _, "Object Mass", "0.0".literal, "kg", bead)
   val gravity = new MyValueControl(0.1, sliderMaxGravity, () => bead.gravity.abs, x => bead.gravity = -x, "Gravity", "0.0".literal, "N/kg", bead)
+
   val sliderArray = Array[AbstractValueControl](staticFriction, kineticFriction, objectMass, gravity)
-  //  new AlignedSliderSetLayoutStrategy(sliderArray).doLayout()//fails horribly
+    new AlignedSliderSetLayoutStrategy(sliderArray).doLayout()//fails horribly
 
   val table = new Hashtable[Double, JComponent]
   class MyLabel(name: String, imageName: String) extends JLabel(name, SwingConstants.CENTER) {
@@ -41,7 +43,29 @@ class FrictionPlayAreaControlPanel(bead: ForceBead) extends VerticalLayoutPanel 
   gravity.getSlider.setPaintLabels(true)
   gravity.setTickLabels(table)
 
-  for (s <- sliderArray) add(s)
+  setLayout(new GridBagLayout)
+  val constraints = new GridBagConstraints
+  constraints.gridy = 0
+  constraints.gridx = GridBagConstraints.RELATIVE
+  for (s <- sliderArray) {
+    add(s.getValueLabel, constraints)
+    add(s.getSlider, constraints)
+    add(s.getTextField, constraints)
+    add(s.getUnitsLabel, constraints)
+    constraints.gridy = constraints.gridy + 1
+  }
+}
+
+object TestFrictionPlayAreaControlPanel{
+  def main(args: Array[String]) {
+    new JFrame{
+      setContentPane(new FrictionPlayAreaControlPanel(new MotionSeriesModel(-3.0,true,0.0).bead))
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
+      pack()
+      setVisible(true)
+    }
+
+  }
 }
 
 //object TestSurfaceControlPanel {
