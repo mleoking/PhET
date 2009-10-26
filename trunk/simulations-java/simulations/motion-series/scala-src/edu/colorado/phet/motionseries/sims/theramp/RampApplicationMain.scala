5c4b0f2b5eee88da8b7e0fac430b6a145e7fc440
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-10-26 20:46:28 +0000

    Removed separate robot moving company game entry point, they are available as final tab in each module.

--- trunk/simulations-java/simulations/motion-series/scala-src/edu/colorado/phet/motionseries/sims/theramp/RampApplicationMain.scala
+++ trunk/simulations-java/simulations/motion-series/scala-src/edu/colorado/phet/motionseries/sims/theramp/RampApplicationMain.scala
@@ -3,15 +3,6 @@ package edu.colorado.phet.motionseries.sims.theramp
 import edu.colorado.phet.common.phetcommon.application.PhetApplicationLauncher
 import edu.colorado.phet.motionseries.Predef._
 
-//Current IntelliJ plugin has trouble finding main for classes with a companion object, so we use a different name
 object RampApplicationMain {
   def main(args: Array[String]) = new PhetApplicationLauncher().launchSim(args, "motion-series".literal, "the-ramp".literal, classOf[RampApplication])
-}
-//
-//object RampWorkEnergyApplicationMain {
-//  def main(args: Array[String]) = new PhetApplicationLauncher().launchSim(args, "motion-series".literal, "the-ramp".literal, classOf[RampWorkEnergyApplication])
-//}
-//
-object RobotMovingCompanyApplicationMain {
-  def main(args: Array[String]) = new PhetApplicationLauncher().launchSim(args, "motion-series".literal, "robot-moving-company".literal, classOf[RobotMovingCompanyApplication])
 }
