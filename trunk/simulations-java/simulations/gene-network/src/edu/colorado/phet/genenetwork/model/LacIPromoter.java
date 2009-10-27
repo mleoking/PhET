6d665f274b2a3758d23dbfa6037fdfb8eef28e74
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-10-27 22:21:43 +0000

    First commit of this model element.

---
+++ trunk/simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/LacIPromoter.java
@@ -0,0 +1,31 @@
+/* Copyright 2009, University of Colorado */
+
+package edu.colorado.phet.genenetwork.model;
+
+import java.awt.Color;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+
+public class LacIPromoter extends SimpleModelElement {
+
+	private static final double WIDTH = 10;  // Nanometers. 
+	private static final double HEIGHT = 2;  // Nanometers.
+	
+	public LacIPromoter(Point2D initialPosition) {
+		super(new Rectangle2D.Double(-WIDTH/2, -HEIGHT/2, WIDTH, HEIGHT), new Point2D.Double(),
+				new Color(112, 190, 237));
+	}
+	
+    public LacIPromoter(double x,double y) {
+        this(new Point2D.Double(x,y));
+    }
+
+	public LacIPromoter(){
+		this(new Point2D.Double());
+	}
+	
+	@Override
+	SimpleElementType getType() {
+		return SimpleElementType.LAC_I_PROMOTER;
+	}
+}
