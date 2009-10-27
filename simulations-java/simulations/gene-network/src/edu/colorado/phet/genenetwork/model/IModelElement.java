722b68b45e8384b6ceabbd12d103d2c8d95346f1
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-10-27 02:17:17 +0000

    Added more methods to this interface.

--- simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/IModelElement.java
+++ simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/IModelElement.java
@@ -3,8 +3,14 @@ package edu.colorado.phet.genenetwork.model;
 import java.awt.*;
 import java.awt.geom.Point2D;
 
+import edu.colorado.phet.common.phetcommon.math.Vector2D;
+
 public interface IModelElement {
     Shape getShape();
-
     Point2D getPositionRef();
+    void setPosition(Point2D newPosition);
+    void setPosition(double xPos, double yPos);
+    Vector2D getVelocityRef();
+    void setVelocity(Vector2D newVelocity);
+    void setVelocity(double xVel, double yVel);
 }
