88fa255f3046a5748599b0a521a3e6551dc5099d
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-10-27 21:29:04 +0000

    Added the gene elements that will appear (at least as currently designed) on top of the DNA strand.

---
+++ simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/LacYGene.java
@@ -0,0 +1,31 @@
+/* Copyright 2009, University of Colorado */
+
+package edu.colorado.phet.genenetwork.model;
+
+import java.awt.Color;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+
+public class LacYGene extends SimpleModelElement {
+
+	private static final double WIDTH = 20;  // Nanometers. 
+	private static final double HEIGHT = 5;  // Nanometers.
+	
+	public LacYGene(Point2D initialPosition) {
+		super(new Rectangle2D.Double(-WIDTH/2, -HEIGHT/2, WIDTH, HEIGHT), new Point2D.Double(),
+				new Color(138, 198, 118));
+	}
+	
+    public LacYGene(double x,double y) {
+        this(new Point2D.Double(x,y));
+    }
+
+	public LacYGene(){
+		this(new Point2D.Double());
+	}
+	
+	@Override
+	SimpleElementType getType() {
+		return SimpleElementType.LAC_Y_GENE;
+	}
+}
