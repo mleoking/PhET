13595eff4cd5c2a5ced312a7afce1d30401ff815
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-10-27 22:41:29 +0000

    Cleaned up imports.

--- simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/LacZGene.java
+++ simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/LacZGene.java
@@ -4,7 +4,6 @@ package edu.colorado.phet.genenetwork.model;
 
 import java.awt.Color;
 import java.awt.geom.Point2D;
-import java.awt.geom.Rectangle2D;
 import java.awt.geom.RoundRectangle2D;
 
 public class LacZGene extends SimpleModelElement {
