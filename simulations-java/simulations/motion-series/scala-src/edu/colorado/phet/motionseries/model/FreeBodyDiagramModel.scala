e59c280132d33bb0c40949bd9921db9d23c3afbf
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-10-27 20:07:19 +0000

    Made it so the adjustable coordinate frame automatically moves with the ramp after snapping to it.

--- simulations-java/simulations/motion-series/scala-src/edu/colorado/phet/motionseries/model/FreeBodyDiagramModel.scala
+++ simulations-java/simulations/motion-series/scala-src/edu/colorado/phet/motionseries/model/FreeBodyDiagramModel.scala
@@ -140,12 +140,15 @@ class VectorViewModel extends Observable {
   }
 }
 
-class CoordinateFrameModel(snapAngles: () => List[Double]) extends Observable { //TODO: if snapped to the ramp, should rotate with ramp
+class CoordinateFrameModel(rampSegment:RampSegment) extends Observable { //TODO: if snapped to the ramp, should rotate with ramp
   private var _proposedAngle = 0.0 //the angle the user has tried to drag the coordinate frame to, not including snapping
-  private val snapRange = 10.0.toRadians
+  private val snapRange = 5.0.toRadians
+  private var snappedToRamp = false
 
+  rampSegment.addListener( ()=> if (snappedToRamp) proposedAngle = rampSegment.angle )
+  
   def angle = {
-    val angleList = snapAngles()
+    val angleList = rampSegment.angle :: 0.0::Nil
     val acceptedAngles = for (s <- angleList if (proposedAngle - s).abs < snapRange) yield s
     if (acceptedAngles.length == 0) proposedAngle
     else acceptedAngles(0) //take the first snap angle from the list
@@ -158,11 +161,7 @@ class CoordinateFrameModel(snapAngles: () => List[Double]) extends Observable { 
     notifyListeners()
   }
 
-  def dropped() = {
-//    var snapChoice = _proposedAngle
-//    for (snapAngle <- snapAngles() if ((snapAngle - _angle).abs < snapRange))
-//      snapChoice = snapAngle
-//    angle = snapChoice
+  def dropped() {
+    snappedToRamp = angle == rampSegment.angle 
   }
-
 }
