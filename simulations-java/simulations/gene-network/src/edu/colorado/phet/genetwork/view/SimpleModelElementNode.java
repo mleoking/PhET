d1392e1c2755e96a49db85593f34e7060df2aea9
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-10-28 22:00:26 +0000

    Added label that is shown if the corresponding element has a label.  This was done to support the gene segments on the DNA, and may need to be refined later.

--- simulations-java/simulations/gene-network/src/edu/colorado/phet/genetwork/view/SimpleModelElementNode.java
+++ simulations-java/simulations/gene-network/src/edu/colorado/phet/genetwork/view/SimpleModelElementNode.java
@@ -5,14 +5,17 @@ package edu.colorado.phet.genetwork.view;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Shape;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Ellipse2D;
 
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform2D;
+import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.colorado.phet.genenetwork.model.SimpleModelElement;
 import edu.umd.cs.piccolo.nodes.PPath;
+import edu.umd.cs.piccolo.nodes.PText;
 
 /**
  * A class that represents a simple model element in the view.
@@ -22,6 +25,7 @@ import edu.umd.cs.piccolo.nodes.PPath;
 public class SimpleModelElementNode extends PPath {
 	
 	private static final boolean SHOW_CENTER_DOT = false;
+	private static final Font LABEL_FONT = new PhetFont(16, true );
 	
 	private final SimpleModelElement modelElement;
 	private final ModelViewTransform2D mvt;
@@ -53,6 +57,15 @@ public class SimpleModelElementNode extends PPath {
 				updateOffset();
 			}
 		});
+		
+		// If there is a label text value, show it.
+		if (modelElement.getLabel() != null){
+			PText labelNode = new PText(modelElement.getLabel());
+			labelNode.setFont(LABEL_FONT);
+			labelNode.setOffset(getFullBoundsReference().getCenterX() - labelNode.getFullBoundsReference().width / 2, 
+					getFullBoundsReference().getCenterY() - labelNode.getFullBoundsReference().height / 2);
+			addChild(labelNode);
+		}
 		
 		// Put a center dot on the node (for debug purposes).
 		if (SHOW_CENTER_DOT){
