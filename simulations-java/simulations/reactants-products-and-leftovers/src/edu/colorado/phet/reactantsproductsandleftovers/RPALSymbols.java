820d4d23dcd427ef7b01b59a30d58025977f7e68
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-10-28 19:04:58 +0000

    add symbols

---
+++ simulations-java/simulations/reactants-products-and-leftovers/src/edu/colorado/phet/reactantsproductsandleftovers/RPALSymbols.java
@@ -0,0 +1,21 @@
+package edu.colorado.phet.reactantsproductsandleftovers;
+
+/**
+ * Collection of symbols used in this sim.
+ * These do not require localization, their use is universal.
+ * <p>
+ * HTML is used to handle superscripts and subscripts.
+ * Symbols that are HTML fragments can be combined into more complex HTML documents.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
+public class RPALSymbols {
+    
+    public static final String CH4 = "CH<sub>4</sub>";
+    public static final String CO2 = "CO<sub>2</sub>";
+    public static final String H2 = "H<sub>2</sub>";
+    public static final String H2O = "H<sub>2</sub>O";
+    public static final String O2 = "O<sub>2</sub>"; 
+    public static final String N2 = "N<sub>2</sub>";
+    public static final String NH3 = "NH<sub>3</sub>";
+}
