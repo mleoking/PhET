2e49e8d5d478ebe3a56ecfc649aea1ffc04a6bfb
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-10-30 20:50:24 +0000

    Fixed up some issues with the appearance of this panel.

--- trunk/simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/controlpanel/LacOperonControlPanel.java
+++ trunk/simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/controlpanel/LacOperonControlPanel.java
@@ -2,9 +2,18 @@
 
 package edu.colorado.phet.genenetwork.controlpanel;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Frame;
 
+import javax.swing.BorderFactory;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
+import javax.swing.border.TitledBorder;
+
 import edu.colorado.phet.common.phetcommon.view.ControlPanel;
+import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.genenetwork.GeneNetworkResources;
 import edu.colorado.phet.genenetwork.model.LacOperonModel;
 import edu.colorado.phet.genenetwork.module.LacOperonModule;
@@ -36,7 +45,18 @@ public class LacOperonControlPanel extends ControlPanel {
         setMinimumWidth( minimumWidth );
         
         // Add the legend panel.
-        addControlFullWidth(new LacOperonLegendPanel());
+        JPanel legendPanel = new JPanel();
+        legendPanel.setLayout(new BorderLayout());
+        legendPanel.add(new LacOperonLegendPanelContents(), BorderLayout.WEST);
+        Border baseBorder = BorderFactory.createRaisedBevelBorder();
+        TitledBorder titledBorder = BorderFactory.createTitledBorder( baseBorder,
+                "Legend",
+                TitledBorder.LEFT,
+                TitledBorder.TOP,
+                new PhetFont( Font.BOLD, 14 ),
+                Color.GRAY );
+        legendPanel.setBorder( titledBorder );
+        addControlFullWidth(legendPanel);
         
         // Layout
         {
