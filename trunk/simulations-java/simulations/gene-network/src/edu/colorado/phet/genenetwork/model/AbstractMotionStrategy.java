876eeb416dc201b363d8e47fbef5425b0378efe7
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-10-30 00:36:25 +0000

    Base class for motion strategies.

---
+++ trunk/simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/AbstractMotionStrategy.java
@@ -0,0 +1,13 @@
+/* Copyright 2009, University of Colorado */
+
+package edu.colorado.phet.genenetwork.model;
+
+/**
+ * Base class for the various motion strategies that can be exhibited by a
+ * model element.
+ * 
+ * @author John Blanco
+ */
+public abstract class AbstractMotionStrategy {
+	abstract public void updatePositionAndMotion();
+}
