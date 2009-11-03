68cea65ac774e91397240dfc66f7a57064be5d16
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-11-03 19:33:40 +0000

    chose to use spinners instead of sliders for reactant quantity controls

--- trunk/simulations-java/simulations/reactants-products-and-leftovers/src/edu/colorado/phet/reactantsproductsandleftovers/controls/ReactantQuantityControlNode.java
+++ trunk/simulations-java/simulations/reactants-products-and-leftovers/src/edu/colorado/phet/reactantsproductsandleftovers/controls/ReactantQuantityControlNode.java
@@ -7,7 +7,6 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
 import edu.colorado.phet.common.phetcommon.util.IntegerRange;
-import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.PhetPNode;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.util.PDimension;
@@ -15,17 +14,11 @@ import edu.umd.cs.piccolo.util.PDimension;
 
 public class ReactantQuantityControlNode extends PhetPNode {
     
-    private static final boolean USE_SPINNER = true;
-    
-    private static final PDimension SLIDER_TRACK_SIZE = new PDimension( 15, 75 );
-    private static final PDimension SLIDER_KNOB_SIZE = new PDimension( 30, 15 );
+    private static final PDimension HISTOGRAM_BAR_SIZE = new PDimension( 15, 75 );
 
     private final ArrayList<ChangeListener> listeners;
     private final IntegerSpinnerNode spinnerNode;
     private final IntegerHistogramBarNode histogramBar;
-    private final IntegerSliderNode sliderNode;
-    private final IntegerTextFieldNode textFieldNode;
-    private final ChangeListener sliderListener, textFieldListener;
 
     public ReactantQuantityControlNode( IntegerRange range, PNode imageNode, double imageScale ) {
         super();
@@ -33,87 +26,37 @@ public class ReactantQuantityControlNode extends PhetPNode {
         listeners = new ArrayList<ChangeListener>();
         spinnerNode = new IntegerSpinnerNode( range );
         spinnerNode.scale( 1.5 ); //XXX
-        histogramBar = new IntegerHistogramBarNode( range, SLIDER_TRACK_SIZE );
-        sliderNode = new IntegerSliderNode( range, SLIDER_TRACK_SIZE, SLIDER_KNOB_SIZE );
-        textFieldNode = new IntegerTextFieldNode( range, new PhetFont( 22 ) );
+        histogramBar = new IntegerHistogramBarNode( range, HISTOGRAM_BAR_SIZE );
         
         spinnerNode.addChangeListener( new ChangeListener() {
             public void stateChanged( ChangeEvent e ) {
                 histogramBar.setValue( spinnerNode.getValue() );
                 fireStateChanged();
             }
-        });
-        
-        sliderListener = new ChangeListener() {
-            public void stateChanged( ChangeEvent e ) {
-                textFieldNode.removeChangeListener( textFieldListener );
-                textFieldNode.setValue( sliderNode.getValue() );
-                textFieldNode.addChangeListener( textFieldListener );
-                fireStateChanged();
-            }
-        };
-        sliderNode.addChangeListener( sliderListener );
-        
-        textFieldListener = new ChangeListener() {
-            public void stateChanged( ChangeEvent e ) {
-                sliderNode.removeChangeListener( sliderListener );
-                sliderNode.setValue( textFieldNode.getValue() );
-                sliderNode.addChangeListener( sliderListener );
-                fireStateChanged();
-            }
-        };
-        textFieldNode.addChangeListener( textFieldListener );
-        
-        if ( USE_SPINNER ) {
-            addChild( spinnerNode );
-            addChild( histogramBar );
-        }
-        else {
-            addChild( sliderNode );
-            addChild( textFieldNode );
-        }
+        } );
+
+        addChild( spinnerNode );
+        addChild( histogramBar );
         addChild( imageNode );
         imageNode.scale( imageScale );
         
         // layout
-        if ( USE_SPINNER ) {
-            // origin at upper left of histogram bar
-            histogramBar.setOffset( 0, 0 );
-            double x = histogramBar.getFullBoundsReference().getMinX() - spinnerNode.getFullBoundsReference().getWidth() - 2;
-            double y = SLIDER_TRACK_SIZE.getHeight() - spinnerNode.getFullBoundsReference().getHeight();
-            spinnerNode.setOffset( x, y );
-            x = histogramBar.getFullBoundsReference().getCenterX() - ( imageNode.getFullBoundsReference().getWidth() / 2 );
-            y = histogramBar.getFullBoundsReference().getMaxY() + 15;
-            imageNode.setOffset( x, y );
-        }
-        else {
-            // origin at upper left of slider track
-            sliderNode.setOffset( 0, 0 );
-            double x = sliderNode.getFullBoundsReference().getMinX() - textFieldNode.getFullBoundsReference().getWidth() - 2;
-            double y = SLIDER_TRACK_SIZE.getHeight() - textFieldNode.getFullBoundsReference().getHeight();
-            textFieldNode.setOffset( x, y );
-            x = sliderNode.getFullBoundsReference().getCenterX() - ( imageNode.getFullBoundsReference().getWidth() / 2 );
-            y = sliderNode.getFullBoundsReference().getMaxY() + 15;
-            imageNode.setOffset( x, y );
-        }
+        // origin at upper left of histogram bar
+        histogramBar.setOffset( 0, 0 );
+        double x = histogramBar.getFullBoundsReference().getMinX() - spinnerNode.getFullBoundsReference().getWidth() - 2;
+        double y = HISTOGRAM_BAR_SIZE.getHeight() - spinnerNode.getFullBoundsReference().getHeight();
+        spinnerNode.setOffset( x, y );
+        x = histogramBar.getFullBoundsReference().getCenterX() - ( imageNode.getFullBoundsReference().getWidth() / 2 );
+        y = histogramBar.getFullBoundsReference().getMaxY() + 15;
+        imageNode.setOffset( x, y );
     }
     
     public void setValue( int value ) {
-        if ( USE_SPINNER ) {
-            spinnerNode.setValue( value );
-        }
-        else {
-            sliderNode.setValue( value );
-        }
+        spinnerNode.setValue( value );
     }
     
     public int getValue() {
-        if ( USE_SPINNER ) {
-            return spinnerNode.getValue();
-        }
-        else {
-            return sliderNode.getValue();
-        }
+        return spinnerNode.getValue();
     }
     
     //----------------------------------------------------------------------------
