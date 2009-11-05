b312a10b61e12c69d346ab69d2a0b20d9e77912a
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-11-05 19:05:29 +0000

    New motion strategy.

---
+++ trunk/simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/DeferToLeaderMotionStrategy.java
@@ -0,0 +1,22 @@
+/* Copyright 2009, University of Colorado */
+
+package edu.colorado.phet.genenetwork.model;
+
+/**
+ * This is a motion strategy that defers to a leader, meaning that it never
+ * decides to move on its own.
+ * 
+ * @author John Blanco
+ */
+public class DeferToLeaderMotionStrategy extends AbstractMotionStrategy {
+
+	public DeferToLeaderMotionStrategy(IModelElement modelElement) {
+		super(modelElement);
+	}
+
+	@Override
+	public void updatePositionAndMotion() {
+		// Does nothing, because it is assumed that the model element is
+		// monitoring the motion of the leader and following that.
+	}
+}
