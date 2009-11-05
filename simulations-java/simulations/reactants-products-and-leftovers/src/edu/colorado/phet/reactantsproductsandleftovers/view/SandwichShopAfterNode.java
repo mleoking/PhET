86656cd016299a5a7dfaeaafd9fe7b043ca914d3
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-11-05 03:28:27 +0000

    completed generalization of After box

--- simulations-java/simulations/reactants-products-and-leftovers/src/edu/colorado/phet/reactantsproductsandleftovers/view/SandwichShopAfterNode.java
+++ simulations-java/simulations/reactants-products-and-leftovers/src/edu/colorado/phet/reactantsproductsandleftovers/view/SandwichShopAfterNode.java
@@ -8,10 +8,11 @@ import javax.swing.event.ChangeListener;
 
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.PhetPNode;
-import edu.colorado.phet.common.piccolophet.util.PNodeLayoutUtils;
 import edu.colorado.phet.reactantsproductsandleftovers.RPALConstants;
 import edu.colorado.phet.reactantsproductsandleftovers.RPALStrings;
 import edu.colorado.phet.reactantsproductsandleftovers.controls.QuantityDisplayNode;
+import edu.colorado.phet.reactantsproductsandleftovers.model.Product;
+import edu.colorado.phet.reactantsproductsandleftovers.model.Reactant;
 import edu.colorado.phet.reactantsproductsandleftovers.module.sandwichshop.SandwichShopDefaults;
 import edu.colorado.phet.reactantsproductsandleftovers.module.sandwichshop.SandwichShopModel;
 import edu.umd.cs.piccolo.PNode;
@@ -23,19 +24,21 @@ import edu.umd.cs.piccolox.nodes.PComposite;
 public class SandwichShopAfterNode extends PhetPNode {
     
     private static final PDimension BOX_SIZE = RPALConstants.BEFORE_AFTER_BOX_SIZE;
-    private static final double DISPLAYS_X_SPACING = 40;
-    private static final double Y_MARGIN = 25;
+    private static final double TITLE_Y_SPACING = 10;
+    private static final double LEFT_MARGIN = 50;
+    private static final double RIGHT_MARGIN = LEFT_MARGIN;
+    private static final double CONTROLS_Y_SPACING = 15;
+    private static final double IMAGES_Y_MARGIN = 18;
+    private static final double IMAGES_Y_SPACING = 27;
+    private static final double LEFTOVERS_BRACKET_Y_SPACING = 3;
     private static final double IMAGE_SCALE = 0.25; //XXX
     
     private final SandwichShopModel model;
 
     private final BoxNode boxNode;
-    private final PComposite sandwichesParent, breadParent, meatParent, cheeseParent;
-    private final ArrayList<SubstanceNode> sandwichList;
-    private final ArrayList<SubstanceNode> breadList;
-    private final ArrayList<SubstanceNode> meatList;
-    private final ArrayList<SubstanceNode> cheeseList;
-    private final PNode sandwichQuantityDisplayNode;
+    private final ArrayList<PComposite> productNodeParents, reactantNodeParents; // parents for product and reactant images
+    private final ArrayList<ArrayList<SubstanceNode>> productNodeLists, reactantNodeLists; // one list of images per product and reactant
+    private final ArrayList<QuantityDisplayNode> productQuantityDisplayNodes, reactantQuantityDisplayNodes; // quantity displays for products and reactants
     
     public SandwichShopAfterNode( final SandwichShopModel model ) {
         super();
@@ -47,187 +50,195 @@ public class SandwichShopAfterNode extends PhetPNode {
             }
         });
         
-        sandwichList = new ArrayList<SubstanceNode>();
-        breadList = new ArrayList<SubstanceNode>();
-        meatList = new ArrayList<SubstanceNode>();
-        cheeseList = new ArrayList<SubstanceNode>();
+        productNodeParents = new ArrayList<PComposite>();
+        reactantNodeParents = new ArrayList<PComposite>();
+        productNodeLists = new ArrayList<ArrayList<SubstanceNode>>();
+        reactantNodeLists = new ArrayList<ArrayList<SubstanceNode>>();
+        productQuantityDisplayNodes = new ArrayList<QuantityDisplayNode>();
+        reactantQuantityDisplayNodes = new ArrayList<QuantityDisplayNode>();
         
+        // box
         boxNode = new BoxNode( BOX_SIZE );
         addChild( boxNode );
         
-        sandwichesParent = new PComposite();
-        addChild( sandwichesParent );
-        
-        breadParent = new PComposite();
-        addChild( breadParent );
-        
-        meatParent = new PComposite();
-        addChild( meatParent );
-        
-        cheeseParent = new PComposite();
-        addChild( cheeseParent );
-        
+        // title for the box
         PText titleNode = new PText( RPALStrings.LABEL_AFTER_SANDWICH );
         titleNode.setFont( new PhetFont( 30 ) );
         titleNode.setTextPaint( Color.BLACK );
         addChild( titleNode );
         
-        PNode valuesNode = new PNode();
-        addChild( valuesNode );
-        sandwichQuantityDisplayNode = new QuantityDisplayNode( model.getSandwich(), SandwichShopDefaults.QUANTITY_RANGE, IMAGE_SCALE );
-        PNode breadQuantityDisplayNode = new QuantityDisplayNode( model.getBread(), SandwichShopDefaults.QUANTITY_RANGE, IMAGE_SCALE );
-        PNode meatQuantityDisplayNode = new QuantityDisplayNode( model.getMeat(), SandwichShopDefaults.QUANTITY_RANGE, IMAGE_SCALE );
-        PNode cheeseQuantityDisplayNode = new QuantityDisplayNode( model.getCheese(), SandwichShopDefaults.QUANTITY_RANGE, IMAGE_SCALE );
-        valuesNode.addChild( sandwichQuantityDisplayNode );
-        valuesNode.addChild( breadQuantityDisplayNode );
-        valuesNode.addChild( meatQuantityDisplayNode );
-        valuesNode.addChild( cheeseQuantityDisplayNode );
-        double sandwichWidth = sandwichQuantityDisplayNode.getFullBoundsReference().getWidth();
-        double breadWidth = breadQuantityDisplayNode.getFullBoundsReference().getWidth();
-        double meatWidth = meatQuantityDisplayNode.getFullBoundsReference().getWidth();
-        double cheeseWidth = cheeseQuantityDisplayNode.getFullBoundsReference().getWidth();
-        double maxWidth = Math.max( sandwichWidth, Math.max( breadWidth, Math.max( meatWidth, cheeseWidth ) ) );
-        double deltaX = maxWidth + DISPLAYS_X_SPACING;
-        double xOffset = 0;
-        sandwichQuantityDisplayNode.setOffset( xOffset, 0 );
-        xOffset += deltaX;
-        breadQuantityDisplayNode.setOffset( xOffset, 0 );
-        xOffset += deltaX;
-        meatQuantityDisplayNode.setOffset( xOffset, 0 );
-        xOffset += deltaX;
-        cheeseQuantityDisplayNode.setOffset( xOffset, 0 );
+        // product images and quantity displays
+        ArrayList<Product> products = model.getReaction().getProductsReference();
+        for ( Product product : products ) {
+            
+            // one parent node for each product image
+            PComposite parent = new PComposite();
+            addChild( parent );
+            productNodeParents.add( parent );
+            
+            // one list of image nodes for each product 
+            productNodeLists.add( new ArrayList<SubstanceNode>() );
+            
+            // one quantity display for each product
+            QuantityDisplayNode controlNode = new QuantityDisplayNode( product, SandwichShopDefaults.QUANTITY_RANGE, IMAGE_SCALE );
+            addChild( controlNode );
+            productQuantityDisplayNodes.add( controlNode );
+        }
         
-        // leftovers label
-        double width = cheeseQuantityDisplayNode.getFullBoundsReference().getMaxX() - breadQuantityDisplayNode.getFullBoundsReference().getMinX();
-        PNode leftoversLabelNode = new LeftoversLabelNode( width );
-        addChild( leftoversLabelNode ); // add after using valueNodes bounds
+        // reactant images and quantity displays
+        ArrayList<Reactant> reactants = model.getReaction().getReactantsReference();
+        for ( Reactant reactant : reactants ) {
+            
+            // one parent node for each reactant image
+            PComposite parent = new PComposite();
+            addChild( parent );
+            reactantNodeParents.add( parent );
+            
+            // one list of image nodes for each reactant 
+            reactantNodeLists.add( new ArrayList<SubstanceNode>() );
+            
+            // one quantity display for each reactant
+            QuantityDisplayNode controlNode = new QuantityDisplayNode( reactant, SandwichShopDefaults.QUANTITY_RANGE, IMAGE_SCALE );
+            addChild( controlNode );
+            reactantQuantityDisplayNodes.add( controlNode );
+        }
         
         // layout, origin at upper-left corner of box
-        boxNode.setOffset( 0, 0 );
-        // title
-        double x = boxNode.getFullBoundsReference().getCenterX() - ( titleNode.getFullBoundsReference().getWidth() / 2 );
-        double y = boxNode.getFullBoundsReference().getMinY() - titleNode.getFullBoundsReference().getHeight() - 10;
+        final int numSubstances = products.size() + reactants.size();
+        assert( numSubstances > 2 ); // a reaction must have at least 1 product and 2 reactants, see ChemicalReaction
+        double x = 0;
+        double y = 0;
+        boxNode.setOffset( x, y );
+        // title centered above box
+        x = boxNode.getFullBoundsReference().getCenterX() - ( titleNode.getFullBoundsReference().getWidth() / 2 );
+        y = boxNode.getFullBoundsReference().getMinY() - titleNode.getFullBoundsReference().getHeight() - TITLE_Y_SPACING;
         titleNode.setOffset( x, y );
-        // value display
-        x = boxNode.getFullBoundsReference().getCenterX() - ( valuesNode.getFullBoundsReference().getWidth() / 2 ) - PNodeLayoutUtils.getOriginXOffset( valuesNode ) + 15; //XXX
-        y = boxNode.getFullBoundsReference().getMaxY() - PNodeLayoutUtils.getOriginYOffset( valuesNode ) + 15;
-        valuesNode.setOffset( x, y );
-        // sandwiches
-        x = 20; //XXX
-        y = boxNode.getFullBoundsReference().getHeight() - 50; //XXX
-        sandwichesParent.setOffset( x, y );
-        // bread
-        x =+ 125; //XXX
-        breadParent.setOffset( x, y );
-        // meat
-        x += 100; //XXX
-        meatParent.setOffset( x, y );
-        // cheese
-        x += 100; //XXX
-        cheeseParent.setOffset( x, y );
-        // leftovers label
-        x = 125; //XXX
-        y = valuesNode.getFullBoundsReference().getMaxY() + 2;
+        // product-specific nodes
+        x = boxNode.getFullBoundsReference().getMinX() + LEFT_MARGIN;
+        double deltaX = ( boxNode.getFullBoundsReference().getWidth() - LEFT_MARGIN - RIGHT_MARGIN ) / ( products.size() + reactants.size() - 1 );
+        for ( int i = 0; i < products.size(); i++ ) {
+            
+            // quantity displays
+            y = boxNode.getFullBoundsReference().getMaxY() + CONTROLS_Y_SPACING;
+            productQuantityDisplayNodes.get( i ).setOffset( x, y );
+            
+            // images, centered above quantity displays
+            y = boxNode.getFullBoundsReference().getMaxY() - IMAGES_Y_MARGIN;
+            productNodeParents.get( i ).setOffset( x, y );
+            
+            x += deltaX;
+        }
+        // reactant-specific nodes
+        for ( int i = 0; i < reactants.size(); i++ ) {
+            
+            // quantity displays
+            y = boxNode.getFullBoundsReference().getMaxY() + CONTROLS_Y_SPACING;
+            reactantQuantityDisplayNodes.get( i ).setOffset( x, y );
+            
+            // images, centered above quantity displays
+            y = boxNode.getFullBoundsReference().getMaxY() - IMAGES_Y_MARGIN;
+            reactantNodeParents.get( i ).setOffset( x, y );
+            
+            x += deltaX;
+        }
+        
+        // leftovers label, after doing layout of leftover quantity displays
+        double startX = reactantQuantityDisplayNodes.get( 0 ).getFullBoundsReference().getMinX();
+        double endX = reactantQuantityDisplayNodes.get( reactantQuantityDisplayNodes.size() - 1 ).getFullBoundsReference().getMaxX();
+        double width = endX - startX;
+        PNode leftoversLabelNode = new LeftoversLabelNode( width );
+        addChild( leftoversLabelNode );
+        x = startX;
+        y = reactantQuantityDisplayNodes.get( 0 ).getFullBoundsReference().getMaxY() + LEFTOVERS_BRACKET_Y_SPACING;
         leftoversLabelNode.setOffset( x, y );
-
+        
         update();
     }
     
+    /*
+     * For each product, update quantity display and number of images to match the quantity.
+     * For each reactant, update quantity display and number of images to match the leftovers.
+     * If we don't have a legitimate reaction, hide the product quantity displays.
+     */
     private void update() {
         
-        sandwichQuantityDisplayNode.setVisible( model.getReaction().isReaction() );
-        
-        // sandwiches
-        if ( model.getSandwich().getQuantity() < sandwichList.size() ) {
-            while ( model.getSandwich().getQuantity() < sandwichList.size() ) {
-                SubstanceNode node = sandwichList.get( sandwichList.size() - 1 );
-                node.cleanup();
-                sandwichesParent.removeChild( node );
-                sandwichList.remove( node );
+        // product quantities
+        ArrayList<Product> products = model.getReaction().getProductsReference();
+        for ( int i = 0; i < products.size(); i++ ) {
+            
+            // products are invisible if we don't have a legitimate reaction
+            productQuantityDisplayNodes.get(i).setVisible( model.getReaction().isReaction() );
+            
+            Product product = products.get( i );
+            PNode parent = productNodeParents.get( i );
+            ArrayList<SubstanceNode> images = productNodeLists.get( i );
+            
+            if ( product.getQuantity() < images.size() ) {
+                // remove images
+                while ( product.getQuantity() < images.size() ) {
+                    SubstanceNode node = images.get( images.size() - 1 );
+                    node.cleanup();
+                    parent.removeChild( node );
+                    images.remove( node );
+                }
             }
-        }
-        else {
-            while ( model.getSandwich().getQuantity() > sandwichList.size() ) {
-                SubstanceNode node = new SubstanceNode( model.getSandwich() );
-                sandwichesParent.addChild( node );
-                sandwichList.add( node );
-                node.scale( IMAGE_SCALE );
-                if ( sandwichesParent.getChildrenCount() > 1 ) {
-                    double x = 0;
-                    double y = sandwichesParent.getChild( sandwichesParent.getChildrenCount() - 2 ).getFullBoundsReference().getMinY() - PNodeLayoutUtils.getOriginYOffset( node )- Y_MARGIN;
-                    node.setOffset( x, y );
+            else {
+                // add images
+                while( product.getQuantity() > images.size() ) {
+                    SubstanceNode node = new SubstanceNode( product );
+                    node.scale( IMAGE_SCALE );
+                    parent.addChild( node );
+                    images.add( node );
+                    // images are vertically stacked
+                    double x = -node.getFullBoundsReference().getWidth() / 2;
+                    if ( parent.getChildrenCount() > 1 ) {
+                        double y = parent.getChild( parent.getChildrenCount() - 2 ).getFullBoundsReference().getMinY() - IMAGES_Y_SPACING;
+                        node.setOffset( x, y );
+                    }
+                    else {
+                        double y = -IMAGES_Y_SPACING;
+                        node.setOffset( x, y );
+                    }
                 }
             }
         }
-        
-        // bread
-        if ( model.getBread().getLeftovers() < breadList.size() ) {
-            while ( model.getBread().getLeftovers() < breadList.size() ) {
-                SubstanceNode node = breadList.get( breadList.size() - 1 );
-                node.cleanup();
-                breadParent.removeChild( node );
-                breadList.remove( node );
+
+        // reactant leftovers
+        ArrayList<Reactant> reactants = model.getReaction().getReactantsReference();
+        for ( int i = 0; i < reactants.size(); i++ ) {
+            
+            Reactant reactant = reactants.get( i );
+            PNode parent = reactantNodeParents.get( i );
+            ArrayList<SubstanceNode> images = reactantNodeLists.get( i );
+            
+            if ( reactant.getLeftovers() < images.size() ) {
+                // remove images
+                while ( reactant.getLeftovers() < images.size() ) {
+                    SubstanceNode node = images.get( images.size() - 1 );
+                    node.cleanup();
+                    parent.removeChild( node );
+                    images.remove( node );
+                }
             }
-        }
-        else {
-            while ( model.getBread().getLeftovers() > breadList.size() ) {
-                SubstanceNode node = new SubstanceNode( model.getBread() );
-                breadParent.addChild( node );
-                breadList.add( node );
-                node.scale( IMAGE_SCALE );
-                if ( breadParent.getChildrenCount() > 1 ) {
-                    double x = 0;
-                    double y = breadParent.getChild( breadParent.getChildrenCount() - 2 ).getFullBoundsReference().getMinY() - Y_MARGIN;
-                    node.setOffset( x, y );
+            else {
+                // add images
+                while( reactant.getLeftovers() > images.size() ) {
+                    SubstanceNode node = new SubstanceNode( reactant );
+                    node.scale( IMAGE_SCALE );
+                    parent.addChild( node );
+                    images.add( node );
+                    // images are vertically stacked
+                    double x = -node.getFullBoundsReference().getWidth() / 2;
+                    if ( parent.getChildrenCount() > 1 ) {
+                        double y = parent.getChild( parent.getChildrenCount() - 2 ).getFullBoundsReference().getMinY() - IMAGES_Y_SPACING;
+                        node.setOffset( x, y );
+                    }
+                    else {
+                        double y = -IMAGES_Y_SPACING;
+                        node.setOffset( x, y );
+                    }
                 }
             }
         }
-        
-        // meat
-        if ( model.getMeat().getLeftovers() < meatList.size() ) {
-            while ( model.getMeat().getLeftovers() < meatList.size() ) {
-                SubstanceNode node = meatList.get( meatList.size() - 1 );
-                node.cleanup();
-                meatParent.removeChild( node );
-                meatList.remove( node );
-            }
-        }
-        else {
-            while ( model.getMeat().getLeftovers() > meatList.size() ) {
-                SubstanceNode node = new SubstanceNode( model.getMeat() );
-                meatParent.addChild( node );
-                meatList.add( node );
-                node.scale( IMAGE_SCALE );
-                if ( meatParent.getChildrenCount() > 1 ) {
-                    double x = 0;
-                    double y = meatParent.getChild( meatParent.getChildrenCount() - 2 ).getFullBoundsReference().getMinY() - Y_MARGIN;
-                    node.setOffset( x, y );
-                }
-            }
-        }
-        
-        // cheese
-        if ( model.getCheese().getLeftovers() < cheeseList.size() ) {
-            while ( model.getCheese().getLeftovers() < cheeseList.size() ) {
-                SubstanceNode node = cheeseList.get( cheeseList.size() - 1 );
-                node.cleanup();
-                cheeseParent.removeChild( node );
-                cheeseList.remove( node );
-            }
-        }
-        else {
-            while ( model.getCheese().getLeftovers() > cheeseList.size() ) {
-                SubstanceNode node = new SubstanceNode( model.getCheese() );
-                cheeseParent.addChild( node );
-                cheeseList.add( node );
-                node.scale( IMAGE_SCALE );
-                if ( cheeseParent.getChildrenCount() > 1 ) {
-                    double x = 0;
-                    double y = cheeseParent.getChild( cheeseParent.getChildrenCount() - 2 ).getFullBoundsReference().getMinY() - Y_MARGIN;
-                    node.setOffset( x, y );
-                }
-            }
-        }
-        
     }
 }
