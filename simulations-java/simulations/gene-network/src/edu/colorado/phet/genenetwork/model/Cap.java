6b03d5b89994cccbd22e7c39ef6a293510555731
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-11-05 19:22:58 +0000

    Added the initial cut at getting Cap and CapBindingRegion to bond to one another.

--- simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/Cap.java
+++ simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/Cap.java
@@ -7,8 +7,11 @@ import java.awt.Paint;
 import java.awt.Shape;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Area;
+import java.awt.geom.Dimension2D;
 import java.awt.geom.GeneralPath;
 import java.awt.geom.Point2D;
+
+import edu.umd.cs.piccolo.util.PDimension;
 
 
 /**
@@ -23,8 +26,12 @@ public class Cap extends SimpleModelElement {
 	private static float WIDTH = CapBindingRegion.WIDTH;
 	private static float HEIGHT = 4;  // In nanometers.
 	
+	private CapBindingRegion capBindingRegionBondingPartner = null;
+	private boolean bound;
+	
 	public Cap(Point2D initialPosition) {
 		super(createActiveConformationShape(), initialPosition, ELEMENT_PAINT);
+		addBindingPoint(new BindingPoint(ModelElementType.CAP_BINDING_REGION, new PDimension(0, 0)));
 		setMotionStrategy(new WeightedRandomWalkMotionStrategy(this, LacOperonModel.getModelBounds()));
 	}
 	
@@ -72,4 +79,57 @@ public class Cap extends SimpleModelElement {
 		return area;
 	}
 	
+	@Override
+	public void updatePositionAndMotion() {
+		if (capBindingRegionBondingPartner != null){
+			// TODO: This needs refinement.  It needs to recognize when the
+			// bond is fully formed so that no motion is required, and it
+			// needs to position itself so the binding points align.  This is
+			// and initial rough attempt.
+			// Also, this should probably only be done in this case when bonds
+			// are formed and released, since the partner is known not to move.
+			if (!bound){
+				// We are moving towards forming a bond with a partner.
+				// Calculate the destination and make sure we are moving
+				// towards it.
+				Dimension2D partnerOffset = capBindingRegionBondingPartner.getBindingPointForElement(getType()).getOffset();
+				Dimension2D myOffset = getBindingPointForElement(capBindingRegionBondingPartner.getType()).getOffset();
+				double xDest = capBindingRegionBondingPartner.getPositionRef().getX() + partnerOffset.getWidth() - 
+					myOffset.getWidth();
+				double yDest = capBindingRegionBondingPartner.getPositionRef().getY() + partnerOffset.getHeight() - 
+					myOffset.getHeight();
+				if (getPositionRef().distance(xDest, yDest) < BOND_FORMING_DISTANCE){
+					// Close enough to form a bond.  Move to the location and
+					// then stop moving.
+					setPosition(xDest, yDest);
+					setMotionStrategy(new StillnessMotionStrategy(this));
+				}
+				else{
+					getMotionStrategyRef().setDestination(xDest, yDest);
+				}
+			}
+		}
+		super.updatePositionAndMotion();
+	}
+
+	@Override
+	public boolean availableForBonding(ModelElementType elementType) {
+		boolean available = false;
+		if (elementType == ModelElementType.CAP_BINDING_REGION && capBindingRegionBondingPartner == null){
+			available = true;
+		}
+		return available;
+	}
+
+	@Override
+	public boolean considerProposalFrom(IModelElement modelElement) {
+		boolean proposalAccepted = false;
+
+		if (modelElement instanceof CapBindingRegion && capBindingRegionBondingPartner == null){
+			capBindingRegionBondingPartner = (CapBindingRegion)modelElement;
+			proposalAccepted = true;
+		}
+		
+		return proposalAccepted;
+	}
 }
--- simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/CapBindingRegion.java
+++ simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/CapBindingRegion.java
@@ -7,6 +7,9 @@ import java.awt.Paint;
 import java.awt.Shape;
 import java.awt.geom.GeneralPath;
 import java.awt.geom.Point2D;
+import java.util.ArrayList;
+
+import edu.umd.cs.piccolo.util.PDimension;
 
 
 /**
@@ -20,8 +23,11 @@ public class CapBindingRegion extends SimpleModelElement {
 	private static float HEIGHT = 2.5f;
 	public static float WIDTH = 5;
 	
+	private Cap capBondingPartner = null;
+	
 	public CapBindingRegion(Point2D initialPosition) {
 		super(createShape(), initialPosition, ELEMENT_PAINT);
+		addBindingPoint(new BindingPoint(ModelElementType.CAP, new PDimension(0, HEIGHT)));
 	}
 	
 	public CapBindingRegion() {
@@ -47,5 +53,51 @@ public class CapBindingRegion extends SimpleModelElement {
 		outline.closePath();
 		
 		return outline;
+	}
+	
+	@Override
+	public boolean availableForBonding(ModelElementType elementType) {
+		boolean available = false;
+		if (elementType == ModelElementType.CAP && capBondingPartner == null){
+			available = true;
+		}
+		return available;
+	}
+
+	@Override
+	public boolean considerProposalFrom(IModelElement modelElement) {
+		boolean proposalAccepted = false;
+
+		if (modelElement instanceof Cap && capBondingPartner == null){
+			capBondingPartner = (Cap)modelElement;
+			proposalAccepted = true;
+		}
+		
+		return proposalAccepted;
+	}
+
+	@Override
+	public void updatePotentialBondingPartners( ArrayList<IModelElement> modelElements ) {
+		// Seek to bond with free elements that are within range and that
+		// match our needs.
+		if (capBondingPartner == null){
+			for (IModelElement modelElement : modelElements){
+				
+				// Look for a bond with Cap.
+				if (modelElement.getType() == ModelElementType.CAP &&
+					capBondingPartner == null &&
+					getPositionRef().distance(modelElement.getPositionRef()) <= BONDING_RANGE &&
+					modelElement.availableForBonding(getType())){
+					
+					// Propose a bond with this element
+					if (modelElement.considerProposalFrom(this)){
+						// Proposal accepted.  Note that the bond is only
+						// started at this point, and not really finalized
+						// until the binding points are in the same location.
+						capBondingPartner = (Cap)modelElement;
+					}
+				}
+			}
+		}
 	}
 }
