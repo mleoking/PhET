6da1c3a64bbdbf21a87109bd34216aa9e3786682
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-11-10 21:08:45 +0000

    Implemented the code to actually update the position based on the leader model element's motion.

--- simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/DeferToLeaderMotionStrategy.java
+++ simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/DeferToLeaderMotionStrategy.java
@@ -2,21 +2,51 @@
 
 package edu.colorado.phet.genenetwork.model;
 
+import java.awt.geom.Dimension2D;
+
 /**
  * This is a motion strategy that defers to a leader, meaning that it never
- * decides to move on its own.
+ * decides to move on its own, but instead watches the motion of the leader
+ * and updates the position every time the leader moves.
  * 
  * @author John Blanco
  */
 public class DeferToLeaderMotionStrategy extends AbstractMotionStrategy {
 
-	public DeferToLeaderMotionStrategy(IModelElement modelElement) {
-		super(modelElement);
+	private IModelElement leader;
+	private Dimension2D leaderToFollowerOffset;
+	private ModelElementListenerAdapter leaderMotionListener = new ModelElementListenerAdapter() {
+		@Override
+		public void positionChanged() {
+			updateFollowerPosition();
+		}
+	};
+	
+	public DeferToLeaderMotionStrategy(IModelElement follower, IModelElement leader, Dimension2D offset) {
+		super(follower);
+		this.leaderToFollowerOffset = offset;
+		
+		// Register for updates from the "leader".
+		leader.addListener(leaderMotionListener);
 	}
 
 	@Override
 	public void updatePositionAndMotion(double dt) {
 		// Does nothing, because it is assumed that the model element is
 		// monitoring the motion of the leader and following that.
+	}
+	
+	/**
+	 * Update the position of the model element for which this motion strategy
+	 * is responsible. 
+	 */
+	private void updateFollowerPosition(){
+		getModelElement().setPosition(leader.getPositionRef().getX() + leaderToFollowerOffset.getWidth(),
+				leader.getPositionRef().getY() + leaderToFollowerOffset.getHeight());
+	}
+
+	@Override
+	public void cleanup() {
+		leader.removeListener(leaderMotionListener);
 	}
 }
