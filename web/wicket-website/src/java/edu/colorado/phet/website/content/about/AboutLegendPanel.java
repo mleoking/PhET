59da3eb9afa61fe0bf17b9b00ff43bb07315a079
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-11-11 10:07:17 +0000

    Wicket-website: i18n of about => legend

--- web/wicket-website/src/java/edu/colorado/phet/website/content/about/AboutLegendPanel.java
+++ web/wicket-website/src/java/edu/colorado/phet/website/content/about/AboutLegendPanel.java
@@ -1,6 +1,8 @@
 package edu.colorado.phet.website.content.about;
 
 import edu.colorado.phet.website.DistributionHandler;
+import edu.colorado.phet.website.components.LocalizedText;
+import edu.colorado.phet.website.components.StaticImage;
 import edu.colorado.phet.website.panels.PhetPanel;
 import edu.colorado.phet.website.util.PageContext;
 import edu.colorado.phet.website.util.links.AbstractLinker;
@@ -9,6 +11,15 @@ import edu.colorado.phet.website.util.links.RawLinkable;
 public class AboutLegendPanel extends PhetPanel {
     public AboutLegendPanel( String id, PageContext context ) {
         super( id, context );
+
+        // TODO: consolidate image names
+        add( new StaticImage( "guidance-recommended-image", "/images/ratings/guidance-recommended.png", null ) );
+        add( new StaticImage( "under-construction-image", "/images/ratings/under-construction.png", null ) );
+        add( new StaticImage( "classroom-tested-image", "/images/ratings/classroom-tested.png", null ) );
+
+        add( new LocalizedText( "about-legend-guidance-recommended", "about.legend.guidance-recommended" ) );
+        add( new LocalizedText( "about-legend-under-construction", "about.legend.under-construction" ) );
+        add( new LocalizedText( "about-legend-classroom-tested", "about.legend.classroom-tested" ) );
 
     }
 
