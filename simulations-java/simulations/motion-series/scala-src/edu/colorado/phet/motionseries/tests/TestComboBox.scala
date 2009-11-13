efb13bfb4707975997d733b627f6cf5fa0b16ee2
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-11-13 07:07:39 +0000

    Added the object selection combo box to the test scenario, see #1880

--- simulations-java/simulations/motion-series/scala-src/edu/colorado/phet/motionseries/tests/TestComboBox.scala
+++ simulations-java/simulations/motion-series/scala-src/edu/colorado/phet/motionseries/tests/TestComboBox.scala
@@ -5,6 +5,8 @@ import edu.colorado.phet.common.piccolophet.PhetPCanvas
 import javax.swing.{JComboBox, JPanel, JFrame}
 import scala.Array
 import edu.umd.cs.piccolox.pswing.{PSwing, PComboBox}
+import edu.colorado.phet.motionseries.controls.ObjectSelectionComboBox
+import edu.colorado.phet.motionseries.model.MotionSeriesModel
 
 //This test is supposed to help us identify why warning signs appear next to some but not all comboboxes, see #1880
 object TestComboBox {
@@ -18,6 +20,13 @@ object TestComboBox {
           val swing = new PSwing(comboBox)
           comboBox.setEnvironment(swing,this)
           addScreenChild(swing)
+
+          val c2 = new ObjectSelectionComboBox(new MotionSeriesModel(0.0,true,0.0))
+          val s2 = new PSwing(c2)
+          c2.setEnvironment(s2,this)
+          s2.setOffset(200,50)
+          addScreenChild(s2)
+
         }, BorderLayout.CENTER)
         add(new JPanel {
           add(new JComboBox(objectArray("jcombobox")))
