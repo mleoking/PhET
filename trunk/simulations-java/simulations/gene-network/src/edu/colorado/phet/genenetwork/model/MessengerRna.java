ee100cfebc0b426607dac598287e5b436eaac621
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-11-13 23:16:25 +0000

    Got the grow routine to work reasonably well when just creating a line.  Next step is creating squiggly RNA.

--- trunk/simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/MessengerRna.java
+++ trunk/simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/MessengerRna.java
@@ -2,19 +2,17 @@
 
 package edu.colorado.phet.genenetwork.model;
 
-import edu.colorado.phet.common.phetcommon.view.util.DoubleGeneralPath;
-import edu.colorado.phet.genenetwork.test.TestRNA;
-
 import java.awt.Color;
 import java.awt.Paint;
 import java.awt.Shape;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Ellipse2D;
-import java.awt.geom.GeneralPath;
 import java.awt.geom.PathIterator;
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
 import java.util.Random;
+
+import edu.colorado.phet.common.phetcommon.view.util.DoubleGeneralPath;
 
 
 /**
@@ -37,6 +35,9 @@ public class MessengerRna extends SimpleModelElement {
 	
 	// Default initial length, used if none is specified.
 	private static float DEFAULT_LENGTH = 30;  // In nanometers.
+	
+	// Default length of the segments used to create the initial shape.
+	private static float DEFAULT_SEGMENT_LENGTH = 3;
 
 	// Used so that every strand looks a little different.
 	private static final Random RAND = new Random();
@@ -46,6 +47,7 @@ public class MessengerRna extends SimpleModelElement {
 	//----------------------------------------------------------------------------
 	
 	private double length = 0;
+	private ArrayList<Point2D> points = new ArrayList<Point2D>();
 	
 	//----------------------------------------------------------------------------
 	// Constructor(s)
@@ -68,6 +70,8 @@ public class MessengerRna extends SimpleModelElement {
 	//----------------------------------------------------------------------------
 	// Methods
 	//----------------------------------------------------------------------------
+	
+	
 	
 	/**
 	 * Grow the RNA strand by the specified length.
@@ -94,16 +98,49 @@ public class MessengerRna extends SimpleModelElement {
 //		}
 //		else{
 //            path.lineToRelative(growthAmount,0);
-
-            // Update the length.
-			length += growthAmount;
-
-			// Set the new new shape.
-			setShape(new TestRNA.RNA(0,0,length).getShape());
-
-//		}
+		
+		if (length == 0){
+			assert points.size() == 0; // Shouldn't have any points yet if length is zero.
+			points.add(new Point2D.Double(0, 0)); // Add the starting point.
+		}
+		else if (points.size() == 0){
+			// This is the case where the messenger RNA was created with an
+			// initial length and then asked to grow.  In this case, we need
+			// to deduce the points from the shape the first time through.
+			double [] coords = new double[6];
+			for (PathIterator it = getShape().getPathIterator(new AffineTransform()); !it.isDone(); it.next()){
+				it.currentSegment(coords);
+				points.add(new Point2D.Double(coords[0], coords[1]));
+			}
+			// At this point, we actually have twice as many points as we
+			// actually need, since we have points for the top and bottom
+			// of the closed path.  To correct this, we create a new array
+			// that has only the top points, correctly compensated.
+			assert points.size() % 2 == 0;
+			ArrayList<Point2D> adjustedPoints = new ArrayList<Point2D>();
+			for (int i = 0; i < points.size() / 2; i++){
+				adjustedPoints.add(new Point2D.Double(points.get(i).getX(), points.get(i).getY() - THICKNESS / 2));
+			}
+			points = adjustedPoints;
+		}
+		
+		// Add a new point to the list.
+		Point2D currentEndPoint = points.get(points.size() - 1);
+		points.add(new Point2D.Double(currentEndPoint.getX() + growthAmount, currentEndPoint.getY()));
+		
+		// Update the shape.
+		setShape(createPathFromPoints(points).getGeneralPath());
+		
+		// Update the length.
+		length += growthAmount;
 	}
 	
+	@Override
+	public void stepInTime(double dt) {
+		// TODO Auto-generated method stub
+		super.stepInTime(dt);
+	}
+
 	@Override
 	public ModelElementType getType() {
 		return ModelElementType.MESSENGER_RNA;
@@ -111,31 +148,20 @@ public class MessengerRna extends SimpleModelElement {
 	
 	private static Shape createInitialShape(double length){
 		
-		/*
-		return new Line2D.Double(0, 0, 20, 0);
-		*/
-		
-		/*
-		return new QuadCurve2D.Double(-10, 0, 0, 5, 10, 0);
-		*/
-		
-		/*
-		 * Creates a simple curve.
-		GeneralPath path = new GeneralPath();
-		float curveHeight = 5;
-		path.moveTo(-APPROX_LENGTH/2, 0);
-		path.quadTo(0, curveHeight, APPROX_LENGTH/2, 0);
-		path.lineTo(APPROX_LENGTH/2, -THICKNESS);
-		path.quadTo(0, curveHeight - THICKNESS, -APPROX_LENGTH/2, -THICKNESS);
-		path.closePath();
-		return path;
-		 */
-
-		if (length == 0){
-			// For a length of zero, return what is essentially a dot.
+		if (length < DEFAULT_LENGTH){
+			// For a length of essentially zero, return a dot.
 			return new Ellipse2D.Double(-THICKNESS / 2, -THICKNESS / 2, THICKNESS, THICKNESS);
 		}
-		
+		else{
+			ArrayList<Point2D> linePoints = new ArrayList<Point2D>();
+			for (int i = 0; i < Math.floor(length / DEFAULT_LENGTH); i++){
+				// Add a point.
+				linePoints.add(new Point2D.Double(-length / 2 + i * DEFAULT_SEGMENT_LENGTH, 0));
+			}
+			return createPathFromPoints(linePoints).getGeneralPath();
+		}
+
+		/*
 		// Create the set of points that will define the curve.
 		ArrayList<Point2DFloat> curvePoints = new ArrayList<Point2DFloat>();
 		float curveHeight = 4;
@@ -166,6 +192,9 @@ public class MessengerRna extends SimpleModelElement {
 				curvePoints.get(0).getY() - THICKNESS);
 		path.closePath();
 		return path;
+		*/
+		
+		
 		
 		/*
 		GeneralPath squiggly = new GeneralPath();
@@ -177,22 +206,30 @@ public class MessengerRna extends SimpleModelElement {
 		*/
 	}
 	
-	private static class Point2DFloat{
-		private final float x;
-		private final float y;
+	/**
+	 * Create a closed shape representing a curve from a set of points.  In
+	 * order for the shape to be closed, this draws a top line, then a bottom
+	 * line, and connects them.
+	 * 
+	 * @param points
+	 * @return
+	 */
+	private static DoubleGeneralPath createPathFromPoints(ArrayList<Point2D> points){
 		
-		public Point2DFloat(float x, float y) {
-			super();
-			this.x = x;
-			this.y = y;
+		DoubleGeneralPath path = new DoubleGeneralPath(points.get(0));
+		
+		// Draw the top line.
+		for (int i = 1; i < points.size(); i++){
+			path.lineTo(points.get(i).getX(), points.get(i).getY() + THICKNESS / 2);
 		}
-
-		public float getX() {
-			return x;
+		
+		// Draw the bottom line.
+		for (int i = points.size() - 1; i >= 0; i--){
+			path.lineTo(points.get(i).getX(), points.get(i).getY() - THICKNESS / 2);
 		}
-
-		public float getY() {
-			return y;
-		}
+		
+		path.closePath();
+		
+		return path;
 	}
 }
