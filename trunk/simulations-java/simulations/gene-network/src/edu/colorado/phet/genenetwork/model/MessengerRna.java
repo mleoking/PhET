fb1241e8c418576178d30c9655c5501b365940f9
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-11-13 23:18:48 +0000

    Cleanup: Removed some code that had been commented out.

--- trunk/simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/MessengerRna.java
+++ trunk/simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/MessengerRna.java
@@ -14,7 +14,6 @@ import java.util.Random;
 
 import edu.colorado.phet.common.phetcommon.view.util.DoubleGeneralPath;
 
-
 /**
  * Class that represents messenger RNA in the model.
  * 
@@ -71,8 +70,6 @@ public class MessengerRna extends SimpleModelElement {
 	// Methods
 	//----------------------------------------------------------------------------
 	
-	
-	
 	/**
 	 * Grow the RNA strand by the specified length.
 	 * 
@@ -82,22 +79,7 @@ public class MessengerRna extends SimpleModelElement {
 	 * possible to generalize this to grow in any direction, or to expand from
 	 * the center.  Feel free to do so if needed.
 	 */
-
-//    DoubleGeneralPath path =new DoubleGeneralPath(0,0);
 	public void grow( double growthAmount ){
-//		if (length == 0){
-//			assert (getShape() instanceof Ellipse2D);
-//
-//            length = growthAmount;
-//
-//			// The current length is 0, so add a line segment.
-//			path.lineTo(length, 0);
-//			setShape(path.getGeneralPath());
-//			// Save the new length.
-//
-//		}
-//		else{
-//            path.lineToRelative(growthAmount,0);
 		
 		if (length == 0){
 			assert points.size() == 0; // Shouldn't have any points yet if length is zero.
@@ -160,50 +142,6 @@ public class MessengerRna extends SimpleModelElement {
 			}
 			return createPathFromPoints(linePoints).getGeneralPath();
 		}
-
-		/*
-		// Create the set of points that will define the curve.
-		ArrayList<Point2DFloat> curvePoints = new ArrayList<Point2DFloat>();
-		float curveHeight = 4;
-		curvePoints.add(new Point2DFloat(-DEFAULT_LENGTH/2, 0));
-		curvePoints.add(new Point2DFloat(-DEFAULT_LENGTH/3, curveHeight));
-		curvePoints.add(new Point2DFloat(-DEFAULT_LENGTH/6, -curveHeight));
-		curvePoints.add(new Point2DFloat(0, curveHeight/4));
-		curvePoints.add(new Point2DFloat(DEFAULT_LENGTH/6, curveHeight));
-		curvePoints.add(new Point2DFloat(DEFAULT_LENGTH/3, -curveHeight));
-		curvePoints.add(new Point2DFloat(DEFAULT_LENGTH/2, 0));
-		
-		// Create the path.  Note that in order to create a closed shape, the
-		// top line is drawn, then the line is essentially reversed but a
-		// little lower, and the two lines are connected.
-		GeneralPath path = new GeneralPath();
-		path.moveTo(curvePoints.get(0).getX(), curvePoints.get(0).getY());
-		path.curveTo(curvePoints.get(1).getX(), curvePoints.get(1).getY(), curvePoints.get(2).getX(),
-				curvePoints.get(2).getY(), curvePoints.get(3).getX(), curvePoints.get(3).getY());
-		path.curveTo(curvePoints.get(4).getX(), curvePoints.get(4).getY(), curvePoints.get(5).getX(),
-				curvePoints.get(5).getY(), curvePoints.get(6).getX(), curvePoints.get(6).getY());
-		
-		path.lineTo(curvePoints.get(6).getX(), curvePoints.get(6).getY() - THICKNESS);
-		path.curveTo(curvePoints.get(5).getX(), curvePoints.get(5).getY() - THICKNESS, curvePoints.get(4).getX(),
-				curvePoints.get(4).getY() - THICKNESS, curvePoints.get(3).getX(), 
-				curvePoints.get(3).getY() - THICKNESS);
-		path.curveTo(curvePoints.get(2).getX(), curvePoints.get(2).getY() - THICKNESS, curvePoints.get(1).getX(),
-				curvePoints.get(1).getY() - THICKNESS, curvePoints.get(0).getX(), 
-				curvePoints.get(0).getY() - THICKNESS);
-		path.closePath();
-		return path;
-		*/
-		
-		
-		
-		/*
-		GeneralPath squiggly = new GeneralPath();
-		squiggly.moveTo(-10, 0);
-		for (int i=0; i<10; i++){
-			squiggly.lineTo(i*5, 1);
-		}
-		return squiggly;
-		*/
 	}
 	
 	/**
