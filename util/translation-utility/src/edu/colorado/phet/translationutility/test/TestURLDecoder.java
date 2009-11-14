bbe91934ada2c288fb492d4693a9b079dd7c0566
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-11-14 04:01:26 +0000

    simplify

--- util/translation-utility/src/edu/colorado/phet/translationutility/test/TestURLDecoder.java
+++ util/translation-utility/src/edu/colorado/phet/translationutility/test/TestURLDecoder.java
@@ -6,26 +6,20 @@ import java.net.URL;
 import java.net.URLDecoder;
 
 /**
- * Test whether Class.getProtectionDomain returns a properly encoded URL.
- * It doesn't seem to handle encoding of '+' properly, which should be encoded as "%2B".
- * See Unfuddle #1817.
- * 
- * Correctly encoded/decoded output:
- * url=file:/User/cmalley/Downloads/foo%2Bbar/
- * file=/User/cmalley/Downloads/foo+bar/
- * 
- * Typical bad output:
- * url=file:/User/cmalley/Downloads/foo+bar/
- * file=/User/cmalley/Downloads/foo bar/
- * 
- * This tested broken on Mac OS 10.5, Vista.
- *
- * @author Chris Malley (cmalley@pixelzoom.com)
+ * Demonstrates a problem with encoding of '+' character.
  */
 public class TestURLDecoder {
     public static void main( String[] args ) throws IOException {
-        URL url = TestURLDecoder.class.getProtectionDomain().getCodeSource().getLocation();
+
+        // Filename contains a '+ character.
+        String filename = "/tmp/foo+bar";
+        System.out.println( "filename=" + filename );
+
+        // Encoding should be "/tmp/foo%2Bbar" but is "/tmp/foo+bar".
+        URL url = new URL( "file:" + filename );
         System.out.println( "url=" + url );
+
+        // Decoding is "/tmp/foo bar" instead of the original filename.
         File file = new File( URLDecoder.decode( url.getFile(), "UTF-8" ) );
         System.out.println( "file=" + file.getAbsolutePath() );
     }
