c35d5639a760fa23fc9005000c2ce45c24a33d45
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-11-17 02:14:05 +0000

    organize imports

--- trunk/simulations-java/simulations/reactants-products-and-leftovers/src/edu/colorado/phet/reactantsproductsandleftovers/controls/ReactionChoiceNode.java
+++ trunk/simulations-java/simulations/reactants-products-and-leftovers/src/edu/colorado/phet/reactantsproductsandleftovers/controls/ReactionChoiceNode.java
@@ -1,6 +1,5 @@
 package edu.colorado.phet.reactantsproductsandleftovers.controls;
 
-import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
