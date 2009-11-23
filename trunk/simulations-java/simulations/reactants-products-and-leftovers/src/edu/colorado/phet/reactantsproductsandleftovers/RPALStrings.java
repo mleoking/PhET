7f492b1e4fa5627d93dcec6ad190397594f1fe5c
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-11-23 23:35:19 +0000

    11/23/09 add a stopwatch icon for the timer feature 11/23/09 i18n of all strings in the Game design

--- trunk/simulations-java/simulations/reactants-products-and-leftovers/src/edu/colorado/phet/reactantsproductsandleftovers/RPALStrings.java
+++ trunk/simulations-java/simulations/reactants-products-and-leftovers/src/edu/colorado/phet/reactantsproductsandleftovers/RPALStrings.java
@@ -24,6 +24,16 @@ public class RPALStrings {
     public static final String LABEL_REACTANTS = RPALResources.getString( "label.reactants" );
     public static final String LABEL_PRODUCTS = RPALResources.getString( "label.products" );
     public static final String LABEL_LEFTOVERS = RPALResources.getString( "label.leftovers" );
+    public static final String LABEL_LEVEL = RPALResources.getString( "label.level" );
+    public static final String LABEL_TIMER = RPALResources.getString( "label.timer" );
+    public static final String LABEL_SCORE = RPALResources.getString( "label.score" );
+    
+    // buttons
+    public static final String BUTTON_CHECK = RPALResources.getString( "button.check" );
+    public static final String BUTTON_NEXT = RPALResources.getString( "button.next" );
+    public static final String BUTTON_SHOW_ANSWER = RPALResources.getString( "button.showAnswer" );
+    public static final String BUTTON_TRY_AGAIN = RPALResources.getString( "button.tryAgain" );
+    public static final String BUTTON_NEW_GAME = RPALResources.getString( "button.newGame" );
 
     // radio buttons
     public static final String RADIO_BUTTON_WATER = RPALResources.getString( "radioButton.water" );
@@ -33,10 +43,15 @@ public class RPALStrings {
     // messages 
     public static final String MESSAGE_VALUE_OUT_OF_RANGE = RPALResources.getString( "message.valueOutOfRange" );
     
+    // questions
+    public static final String QUESTION_HOW_MANY_PRODUCTS_AND_LEFTOVERS = RPALResources.getString( "question.howManyProductsAndLeftovers" );
+    public static final String QUESTION_HOW_MANY_REACTANTS = RPALResources.getString( "question.howManyReactants" );
+    
     // titles
     public static final String TITLE_SANDWICH_SHOP = RPALResources.getString( "title.sandwichShop" );
     public static final String TITLE_REAL_REACTION = RPALResources.getString( "title.realReaction" );
     public static final String TITLE_GAME = RPALResources.getString( "title.game" );
+    public static final String TITLE_GAME_SETTINGS = RPALResources.getString( "title.gameSettings" );
     
     // these strings are not visible in the sim, but needed by the general model
     public static final String BREAD = "Bread";
