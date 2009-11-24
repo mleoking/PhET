1fafacc65e91e77d05dd2e0d5982dbeac7730c7a
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-11-24 01:07:37 +0000

    Intermediate commit of code for putting a node into the tool box.

---
+++ simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/view/LacZGeneToolBoxNode.java
@@ -0,0 +1,23 @@
+package edu.colorado.phet.genenetwork.view;
+
+import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform2D;
+import edu.colorado.phet.genenetwork.model.IObtainGeneModelElements;
+import edu.colorado.phet.genenetwork.model.LacZGene;
+import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.nodes.PPath;
+
+/**
+ * Class the represents LacZ in the tool box, and that allows users to
+ * click on it to add it to the model.
+ * 
+ * @author John Blanco
+ */
+public class LacZGeneToolBoxNode extends PNode {
+
+	private PPath availableForInsertionNode;
+	
+	public LacZGeneToolBoxNode(IObtainGeneModelElements model, ModelViewTransform2D mvt) {
+		availableForInsertionNode = new SimpleModelElementNode(new LacZGene(model), mvt);
+		addChild(availableForInsertionNode);
+	}
+}
