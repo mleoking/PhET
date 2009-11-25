4774f9599bb536db0940d7b6f958668a115f56bd
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-11-25 21:21:35 +0000

    see #1908, rewrite and document ResourceAnnotation

--- util/licensing/src/edu/colorado/phet/licensing/ResourceAnnotation.java
+++ util/licensing/src/edu/colorado/phet/licensing/ResourceAnnotation.java
@@ -1,116 +1,97 @@
 package edu.colorado.phet.licensing;
 
+import java.util.HashMap;
+
 import edu.colorado.phet.common.phetcommon.util.AnnotationParser;
 
 /**
- * Created by: Sam
- * Aug 7, 2008 at 9:40:29 PM
+ * Object-oriented representation of one line in a resource annotation file. 
+ * 
+ * @author Sam Reid
+ * @author Chris Malley (cmalley@pixelzoom.com)
  */
 public class ResourceAnnotation implements ResourceAnnotationElement {
-    private String name;
-    private String source;
-    private String author;
-    private String license;
-    private String notes;
-    private String same;//if this resource is a copy of another, you can reference the original here
-    private String licensefile;
-
-    /*
-     * If you're thinking that you've found documentation on the format of resource files, guess again.
-     * - There is no documentation on the semantics of these fields.
-     * - This "keys" array isn't used anywhere, so this may not be the actual list of fields that's supported.
-     * - These String literals are copied throughout this file (and others?) instead of using symbolic constants.
+    
+    /**
+     * A resource annotation consists of key-value pairs.
+     * These are the supported keys.
+     * To add a new key, add a constants here, add the constant to KEYS, and create a get method.
      */
-    private static final String[] keys = new String[]{"source", "author", "license", "notes", "same", "licensefile"};
+    private static final String SOURCE_KEY = "source"; // where we found the resource (organization, URL, etc.)
+    private static final String AUTHOR_KEY = "author"; // person or organization that created the resource
+    private static final String LICENSE_KEY = "license"; // the type of license, see PhetRuleSet for a list of recognized licenses
+    private static final String NOTES_KEY = "notes"; // any misc notes that you want to include
+    private static final String SAME_KEY = "same";  //if this resource is a copy of another, you can reference the original here
+    private static final String LICENSEFILE_KEY = "licensefile";  // file that contains the actual license text
+    
+    private static final String[] KEYS = { SOURCE_KEY, AUTHOR_KEY, LICENSE_KEY, NOTES_KEY, SAME_KEY, LICENSEFILE_KEY };
+    
+    private String name;
+    private HashMap<String,String> values; // maps keys to values, for those keys that have values
 
-    public ResourceAnnotation( String name ) {
-        this.name = name;
+    public ResourceAnnotation( String line ) {
+        values = new HashMap<String,String>();
+        parse( line );
+    }
+    
+    public String getName() {
+        return name;
     }
 
     public String getSource() {
-        return source;
+        return values.get( SOURCE_KEY );
     }
-
-    public void setSource( String source ) {
-        this.source = source;
+    
+    /**
+     * @deprecated This is being misused in ResourceAnnotationList as a hack for documenting things that aren't annotated. This is a bad design.
+     */
+    public void setSource( String value ) {
+        values.put( SOURCE_KEY, value );
     }
 
     public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor( String author ) {
-        this.author = author;
+        return values.get( AUTHOR_KEY );
     }
 
     public String getLicense() {
-        return license;
-    }
-
-    public void setLicense( String license ) {
-        this.license = license;
+        return values.get( LICENSE_KEY );
     }
 
     public String getSame() {
-        return same;
-    }
-
-    public void setSame( String same ) {
-        this.same = same;
+        return values.get( SAME_KEY );
     }
 
     public String getNotes() {
-        return notes;
-    }
-
-    public void setNotes( String notes ) {
-        this.notes = notes;
+        return values.get( NOTES_KEY );
     }
 
     public String getLicensefile() {
-        return licensefile;
+        return values.get( LICENSEFILE_KEY );
     }
-
-    public void setLicensefile( String licensefile ) {
-        this.licensefile = licensefile;
-    }
-
-    public static ResourceAnnotation parseElement( String line ) {
-        try{
-        AnnotationParser.Annotation a = AnnotationParser.parse( line.trim() );
-
-        ResourceAnnotation annotation = new ResourceAnnotation( a.getId() );
-
-        annotation.source = a.get( "source" );
-        annotation.author = a.get( "author" );
-        annotation.license = a.get( "license" );
-        annotation.notes = a.get( "notes" );
-        annotation.same = a.get( "same" );
-        annotation.licensefile = a.get( "licensefile" );
-        return annotation;
-        }catch(Exception e){
-            System.out.println( "Error on line="+line );
-            e.printStackTrace(  );
-            AnnotationParser.Annotation a = AnnotationParser.parse( line.trim() );
-            throw new RuntimeException( e);
-        }
-
-    }
-
+    
     public String toText() {
-        return name + " " + getString( "source", source ) + getString( "author", author ) + getString( "license", license ) + getString( "notes", notes ) + getString( "same", same ) + getString( "licensefile", licensefile );
+        String s = name;
+        for ( String key : values.keySet() ) {
+            s += " " + key + "=" + values.get( key );
+        }
+        return s;
     }
 
-    private String getString( String key, String value ) {
-        if ( value == null ) {
-            return "";
+    private void parse( String line ) {
+        try {
+            AnnotationParser.Annotation a = AnnotationParser.parse( line.trim() );
+            name = a.getId();
+            for ( String key : KEYS ) {
+                String value = a.get( key );
+                if ( value != null ) {
+                    values.put( key, a.get( key ) );
+                }
+            }
         }
-        else {
-            return key + "=" + value + " ";
+        catch ( Exception e ) {
+            System.err.println( "Error on line=" + line );
+            e.printStackTrace();
+            throw new RuntimeException( e );
         }
-    }
-
-    public String getName() {
-        return name;
     }
 }
