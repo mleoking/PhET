71809eab14ac9f66f2aa7637d253d3f3e6106f3b
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-12-01 11:29:41 +0000

    Electric-hockey: title fix for credits and website compatibility

--- simulations-java/simulations/electric-hockey/src/edu/colorado/phet/electrichockey/ElectricHockeyApplication.java
+++ simulations-java/simulations/electric-hockey/src/edu/colorado/phet/electrichockey/ElectricHockeyApplication.java
@@ -2,6 +2,7 @@ package edu.colorado.phet.electrichockey;
 
 import edu.colorado.phet.common.phetcommon.application.*;
 import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
+import edu.colorado.phet.common.phetcommon.util.LocaleUtils;
 import edu.colorado.phet.common.phetcommon.view.PhetLookAndFeel;
 import edu.colorado.phet.common.phetcommon.view.util.FrameSetup;
 
@@ -19,6 +20,19 @@ public class ElectricHockeyApplication extends PhetApplication {
             super( commandLineArgs, "electric-hockey" );
             setFrameSetup( new FrameSetup.CenteredWithSize( 800, 750 ) );
             super.setLookAndFeel( new PhetLookAndFeel() );
+        }
+
+        /**
+         * manually change English title for credits. The localized version should read "Electric Field Hockey" to be
+         * read correctly by the website
+         */
+        public String getName() {
+            if ( getResourceLoader().getLocale().equals( LocaleUtils.stringToLocale( "en" ) ) ) {
+                return super.getName() + " - derived from work by Ruth Chabay";
+            }
+            else {
+                return super.getName();
+            }
         }
     }
 
