dc931d97cfd4e88a8a99f6c330b5b8330522052a
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-12-01 22:01:23 +0000

    Intermediate commit of code that adds dotted outlines to DNA strand where the genes should be placed.

--- trunk/simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/view/DnaStrandNode2.java
+++ trunk/simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/view/DnaStrandNode2.java
@@ -7,7 +7,6 @@ import java.awt.Color;
 import java.awt.Shape;
 import java.awt.Stroke;
 import java.awt.geom.AffineTransform;
-import java.awt.geom.GeneralPath;
 import java.awt.geom.Rectangle2D;
 
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform2D;
@@ -43,8 +42,8 @@ public class DnaStrandNode2 extends PNode {
 		strand2Node = new PhetPPath(STRAND_STROKE, new Color(214, 87, 107));
 		addChild(strand2Node);
 		
-		// Update our shape.
-		updateShape();
+		// Update the shape of the DNA strands.
+		updateStrandShape();
 		
 		// Set up the visible bounds, if turned on.
 		if (SHOW_BOUNDS){
@@ -53,11 +52,16 @@ public class DnaStrandNode2 extends PNode {
 			addChild(boundsRectNode);
 		}
 		
+		// Add the gene segment shapes.
+		for (DnaStrand.GeneSegmentShape geneSegmentShape : dnaStrand.getGeneSegmentShapeList()){
+			addChild(new GeneSegmentNode(geneSegmentShape, mvt));
+		}
+		
 		// Set our initial position.
 		updateOffset();
 	}
 	
-    private void updateShape() {
+    private void updateStrandShape() {
 
     	// We only want the shape, and not any translation associated with the
     	// shape, so we create our own transform that only does the scaling
@@ -76,5 +80,35 @@ public class DnaStrandNode2 extends PNode {
     
     private void updateOffset() {
         setOffset( mvt.modelToView( dnaStrand.getPositionRef() ));
+    }
+    
+    private static class GeneSegmentNode extends PPath {
+
+    	private static final Stroke OUTLINE_STROKE = new BasicStroke(2.0f, BasicStroke.CAP_BUTT,
+    			BasicStroke.JOIN_BEVEL, 0, new float[] {8, 5}, 0);
+    	
+		public GeneSegmentNode(DnaStrand.GeneSegmentShape geneSegmentShape, ModelViewTransform2D mvt) {
+			super();
+			setStroke(OUTLINE_STROKE);
+			setStrokePaint(Color.BLACK);
+			
+	    	// We only want the shape, and not any translation associated with the
+	    	// shape, so we create our own transform that only does the scaling
+	    	// that is indicated in the model-view transform.
+	    	
+	    	// Create transform that only scales, and does no translation.
+	    	AffineTransform scalingOnlyTransform = AffineTransform.getScaleInstance(mvt.getAffineTransform().getScaleX(),
+	    			mvt.getAffineTransform().getScaleY());
+	    	
+	    	// Create the transformed shape.
+			Shape transformedShape = scalingOnlyTransform.createTransformedShape(geneSegmentShape);
+			
+			// Set the shape.
+			setPathTo(transformedShape);
+			
+			// Set our offset, which is generally expected to be relative to
+			// the DNA strand node of which this is a part.
+			setOffset(scalingOnlyTransform.transform(geneSegmentShape.getOffsetFromDnaStrandPosRef(), null));
+		}
     }
 }
