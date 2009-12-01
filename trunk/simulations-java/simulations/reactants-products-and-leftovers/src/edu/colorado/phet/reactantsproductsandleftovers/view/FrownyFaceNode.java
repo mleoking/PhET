4efc971d6c63bf3115ef79487d77e39a8d765e07
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-12-01 18:21:04 +0000

    smiley and frowny faces for Game

---
+++ trunk/simulations-java/simulations/reactants-products-and-leftovers/src/edu/colorado/phet/reactantsproductsandleftovers/view/FrownyFaceNode.java
@@ -0,0 +1,13 @@
+package edu.colorado.phet.reactantsproductsandleftovers.view;
+
+import edu.colorado.phet.reactantsproductsandleftovers.RPALImages;
+import edu.umd.cs.piccolo.nodes.PImage;
+
+
+public class FrownyFaceNode extends PImage {
+    
+    public FrownyFaceNode() {
+        super( RPALImages.FROWNY_FACE );
+    }
+
+}
---
+++ trunk/simulations-java/simulations/reactants-products-and-leftovers/src/edu/colorado/phet/reactantsproductsandleftovers/view/SmileyFaceNode.java
@@ -0,0 +1,13 @@
+package edu.colorado.phet.reactantsproductsandleftovers.view;
+
+import edu.colorado.phet.reactantsproductsandleftovers.RPALImages;
+import edu.umd.cs.piccolo.nodes.PImage;
+
+
+public class SmileyFaceNode extends PImage {
+    
+    public SmileyFaceNode() {
+        super( RPALImages.SMILEY_FACE );
+    }
+
+}
