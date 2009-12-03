c999f8ff4f31328c210f8a69af812d61ab33e14e
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-12-03 18:22:41 +0000

    Added a tool box node for RNA Polymerase based on the new inheritance hierarchy.  Also added the ability to add RNA Polymerase to the model.

---
+++ trunk/simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/view/RnaPolymeraseToolBoxNode2.java
@@ -0,0 +1,27 @@
+package edu.colorado.phet.genenetwork.view;
+
+import java.awt.geom.Point2D;
+
+import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform2D;
+import edu.colorado.phet.common.piccolophet.PhetPCanvas;
+import edu.colorado.phet.genenetwork.GeneNetworkStrings;
+import edu.colorado.phet.genenetwork.model.IObtainGeneModelElements;
+import edu.colorado.phet.genenetwork.model.RnaPolymerase;
+
+public class RnaPolymeraseToolBoxNode2 extends ToolBoxItemNode {
+
+	public RnaPolymeraseToolBoxNode2(IObtainGeneModelElements model, ModelViewTransform2D mvt, PhetPCanvas canvas) {
+		super(model, mvt, canvas);
+	}
+
+	@Override
+	protected void addModelElement(Point2D position) {
+		setModelElement(getModel().createAndAddRnaPolymerase(position));
+	}
+
+	@Override
+	protected void initializeSelectionNode() {
+		setSelectionNode(new SimpleModelElementNode(new RnaPolymerase(getModel()), SCALING_MVT, true));
+		setCaption(GeneNetworkStrings.POLYMERASE_TOOL_BOX_CAPTION);
+	}
+}
