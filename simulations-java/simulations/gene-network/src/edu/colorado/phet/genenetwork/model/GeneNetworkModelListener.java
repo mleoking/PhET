85e78982562c47054d72a2e4cbabfe2d29986e80
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-12-04 18:58:17 +0000

    Pulled out the listener interface for the LacOperonModel into a top level class.  This is so that it can be used for other gene network models.

---
+++ simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/GeneNetworkModelListener.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package edu.colorado.phet.genenetwork.model;
+
+/**
+ * Interface for listening to events from a gene network model.
+ * 
+ * @author John Blanco
+ */
+public interface GeneNetworkModelListener {
+    void modelElementAdded(SimpleModelElement modelElement);
+}
