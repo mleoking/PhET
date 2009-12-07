a67595f2e60f3ed4200df793ca1cb4d5d7a41f0d
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-12-07 21:14:48 +0000

    see #89, extend PaintImmediateDialog in energy-skate-park

--- trunk/simulations-java/simulations/energy-skate-park/src/edu/colorado/phet/energyskatepark/plots/EnergyPositionPlotDialog.java
+++ trunk/simulations-java/simulations/energy-skate-park/src/edu/colorado/phet/energyskatepark/plots/EnergyPositionPlotDialog.java
@@ -1,15 +1,14 @@
 package edu.colorado.phet.energyskatepark.plots;
 
+import edu.colorado.phet.common.phetcommon.application.PaintImmediateDialog;
 import edu.colorado.phet.common.phetcommon.view.PhetFrame;
 import edu.colorado.phet.energyskatepark.EnergySkateParkModule;
-
-import javax.swing.*;
 
 /**
  * Author: Sam Reid
  * Jun 29, 2007, 11:17:25 PM
  */
-public class EnergyPositionPlotDialog extends JDialog {
+public class EnergyPositionPlotDialog extends PaintImmediateDialog {
     private EnergyPositionPlot energyPosition;
 
     public EnergyPositionPlotDialog( PhetFrame phetFrame, String title, boolean modal, EnergySkateParkModule energySkateParkModule ) {
