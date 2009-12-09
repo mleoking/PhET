0eb501eca59673fa92e95ceff288fab9d26dd228
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-12-09 21:10:50 +0000

    Added the button and the code to make is pressable.

--- simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/view/LactoseInjectorNode.java
+++ simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/view/LactoseInjectorNode.java
@@ -2,11 +2,17 @@
 
 package edu.colorado.phet.genenetwork.view;
 
+import java.awt.geom.Point2D;
+import java.awt.geom.Point2D.Double;
 import java.awt.image.BufferedImage;
 
+import edu.colorado.phet.common.piccolophet.event.CursorHandler;
 import edu.colorado.phet.genenetwork.GeneNetworkResources;
 import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
+import edu.umd.cs.piccolo.event.PInputEvent;
 import edu.umd.cs.piccolo.nodes.PImage;
+import edu.umd.cs.piccolo.util.PDimension;
 
 /**
  * Node that represents the thing that the user interacts with in order to
@@ -23,7 +29,15 @@ public class LactoseInjectorNode extends PNode {
 	// Height of the injector prior to rotation.  This is in world size, which
 	// is close to pixels (but not quite exactly due to all that transform
 	// craziness).
-	private static final double INJECTOR_HEIGHT = 30; 
+	private static final double INJECTOR_HEIGHT = 120;
+	
+	// Angle of rotation of this node.
+	private static final double ROTATION_ANGLE = Math.PI/4;
+//	private static final double ROTATION_ANGLE = 0;
+	
+	// Offset of button within this node.  This was determined by trial and
+	// error and will need to be tweaked if the images change.
+	private static final Point2D BUTTON_OFFSET = new Point2D.Double(45, 45);
 
     //------------------------------------------------------------------------
     // Instance Data
@@ -38,11 +52,41 @@ public class LactoseInjectorNode extends PNode {
     //------------------------------------------------------------------------
 
 	public LactoseInjectorNode() {
-        // Load the graphic images for this device.
+        // Load the graphic images for this device.  IMPORTANT: This code
+		// assumes that the place where the lactose comes out is at the
+		// bottom center of this image.
         BufferedImage bufferedImage = GeneNetworkResources.getImage( "squeezer_background.png" );
         bodyImage = new PImage( bufferedImage );
-        bodyImage.scale(0.5);
-        bodyImage.rotate(Math.PI / 4);
         addChild(bodyImage);
+        bufferedImage = GeneNetworkResources.getImage( "button_pressed.png" );
+        pressedButtonImage = new PImage (bufferedImage);
+        pressedButtonImage.setOffset(BUTTON_OFFSET);
+        addChild(pressedButtonImage);
+        bufferedImage = GeneNetworkResources.getImage( "button_unpressed.png" );
+        unpressedButtonImage = new PImage (bufferedImage);
+        unpressedButtonImage.setOffset(BUTTON_OFFSET);
+        addChild(unpressedButtonImage);
+        
+        // Rotate and scale the node as a whole.
+        double scale = INJECTOR_HEIGHT / bodyImage.getFullBoundsReference().height;
+        rotateAboutPoint( ROTATION_ANGLE, new Point2D.Double( bodyImage.getFullBoundsReference().width / 2, bodyImage.getFullBoundsReference().height / 2));
+        scale(scale);
+        
+        // Set up the button handling.
+        unpressedButtonImage.setPickable(true);
+        pressedButtonImage.setPickable(false);
+        bodyImage.setPickable(false);
+        unpressedButtonImage.addInputEventListener(new CursorHandler());
+        unpressedButtonImage.addInputEventListener(new PBasicInputEventHandler(){
+        	@Override
+            public void mousePressed( PInputEvent event ) {
+                unpressedButtonImage.setVisible(false);
+            }
+        	
+        	@Override
+            public void mouseReleased( PInputEvent event ) {
+                unpressedButtonImage.setVisible(true);
+            }
+        });
 	}
 }
