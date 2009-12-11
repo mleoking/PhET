c98cff866a8d964a01c4a5278e6e53e5d4e9b305
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-12-11 22:08:14 +0000

    New motion strategy for closing on another model element that is moving.

---
+++ trunk/simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/CloseOnMovingTargetMotionStrategy.java
@@ -0,0 +1,42 @@
+package edu.colorado.phet.genenetwork.model;
+
+import java.awt.geom.Dimension2D;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+
+import edu.umd.cs.piccolo.util.PDimension;
+
+/**
+ * This class represents a motion strategy for a simple model element that
+ * causes it to move in a somewhat random fashion but also move towards an
+ * attachment point on another simple model element that is also moving.
+ * 
+ * @author John Blanco
+ */
+public class CloseOnMovingTargetMotionStrategy extends
+		DirectedRandomWalkMotionStrategy {
+	
+	private PDimension offsetFromTarget;
+	private final IModelElement targetElement;
+	private ModelElementListenerAdapter targetListener = new ModelElementListenerAdapter(){
+		public void positionChanged(){
+			updateDestination();
+		};
+		public void removedFromModel() {
+			targetElement.removeListener(targetListener);
+		};
+	};
+
+	public CloseOnMovingTargetMotionStrategy(IModelElement modelElement, IModelElement targetElement, 
+			Dimension2D attachmentPointOffset,Rectangle2D bounds) {
+		super(modelElement, bounds);
+		this.targetElement = targetElement;
+		targetElement.addListener(targetListener);
+		updateDestination();
+	}
+	
+	private void updateDestination(){
+		setDestination(getModelElement().getPositionRef().getX() + offsetFromTarget.getHeight(),
+				getModelElement().getPositionRef().getX() + offsetFromTarget.getWidth());
+	}
+}
