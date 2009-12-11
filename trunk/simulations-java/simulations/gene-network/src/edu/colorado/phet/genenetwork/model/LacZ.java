26a1065f03026ae43d4caab58faeff66fa0c99a6
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-12-11 21:35:36 +0000

    Added ability to get offset for lactose attachment point.

--- trunk/simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/LacZ.java
+++ trunk/simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/LacZ.java
@@ -8,6 +8,7 @@ import java.awt.Paint;
 import java.awt.Shape;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Area;
+import java.awt.geom.Dimension2D;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Point2D;
 
@@ -26,6 +27,7 @@ public class LacZ extends SimpleModelElement {
 	private static final Paint ELEMENT_PAINT = new GradientPaint(new Point2D.Double(-SIZE, 0), 
 			new Color(185, 147, 187), new Point2D.Double(SIZE * 5, 0), Color.WHITE);
 	private static final double EXISTENCE_TIME = 15; // Seconds.
+	private static final Dimension2D LACTOSE_ATTACHMENT_POINT_OFFSET = new PDimension(0, -SIZE/2);
 	
 	public LacZ(IGeneNetworkModelControl model, Point2D initialPosition) {
 		super(model, createShape(), initialPosition, ELEMENT_PAINT, true, EXISTENCE_TIME);
@@ -61,4 +63,14 @@ public class LacZ extends SimpleModelElement {
 	protected void onTransitionToExistingState() {
 		setMotionStrategy(new RandomWalkMotionStrategy(this, LacOperonModel.getMotionBounds()));
 	}
+	
+	/**
+	 * Get the location in absolute space of the attachment point for this
+	 * type of model element.
+	 */
+	public Point2D getAttachmentPointLocation(LacI lacI){
+		return new Point2D.Double(getPositionRef().getX() + LACTOSE_ATTACHMENT_POINT_OFFSET.getWidth(),
+				getPositionRef().getY() + LACTOSE_ATTACHMENT_POINT_OFFSET.getHeight());
+	}
+
 }
