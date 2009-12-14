a2b9bb8ed0d45e01b7f73fdf0f439cbed3c92d32
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-12-14 21:09:32 +0000

    see #2018, cleanup

--- simulations-java/simulations/reactants-products-and-leftovers/src/edu/colorado/phet/reactantsproductsandleftovers/test/TestCompoundBorderMacOS.java
+++ simulations-java/simulations/reactants-products-and-leftovers/src/edu/colorado/phet/reactantsproductsandleftovers/test/TestCompoundBorderMacOS.java
@@ -14,6 +14,7 @@ import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
 
+import edu.colorado.phet.common.phetcommon.view.util.SwingUtils;
 import edu.colorado.phet.common.piccolophet.PhetPCanvas;
 import edu.umd.cs.piccolo.util.PDimension;
 import edu.umd.cs.piccolox.pswing.PSwing;
@@ -31,18 +32,21 @@ public class TestCompoundBorderMacOS extends JFrame {
         super( "TestCompoundBorderMacOS" );
         setPreferredSize( new Dimension( 600, 400 )  );
 
-        JPanel controlPanel = new JPanel();
-        controlPanel.add( new BorderedLabel( "Swing" ) );
-
+        // canvas
         PhetPCanvas canvas = new PhetPCanvas( new PDimension( 600, 400 ) );
         PSwing pswing = new PSwing( new BorderedLabel( "PSwing" ) );
         pswing.scale( 2 );
         pswing.setOffset( 100, 100 );
         canvas.addWorldChild( pswing );
+        
+        // control panel
+        JPanel controlPanel = new JPanel();
+        controlPanel.add( new BorderedLabel( "Swing" ) );
 
+        // layout similar to a PhET sim
         JPanel mainPanel = new JPanel( new BorderLayout() );
-        mainPanel.add( canvas, BorderLayout.CENTER );
         mainPanel.add( controlPanel, BorderLayout.EAST );
+        mainPanel.add( canvas, BorderLayout.CENTER );
         setContentPane( mainPanel );
 
         pack();
@@ -51,6 +55,7 @@ public class TestCompoundBorderMacOS extends JFrame {
     private static class BorderedLabel extends JLabel {
 
         private static final Border BORDER = new CompoundBorder( new LineBorder( Color.BLUE, 4 ), new CompoundBorder( new LineBorder( Color.BLACK, 2 ), new EmptyBorder( 5, 14, 5, 14 ) ) );
+        
         public BorderedLabel( String label ) {
             super( label );
             setBorder( BORDER );
@@ -60,6 +65,7 @@ public class TestCompoundBorderMacOS extends JFrame {
     public static void main( String[] args ) {
         JFrame frame = new TestCompoundBorderMacOS();
         frame.setDefaultCloseOperation( WindowConstants.EXIT_ON_CLOSE );
+        SwingUtils.centerWindowOnScreen( frame );
         frame.setVisible( true );
     }
 
