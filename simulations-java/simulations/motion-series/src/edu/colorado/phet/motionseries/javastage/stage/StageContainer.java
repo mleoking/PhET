bb86e9fa532650ba4b42b04e336ffe3585864555
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-12-16 00:25:18 +0000

    split some exceptionally-long comment lines

--- simulations-java/simulations/motion-series/src/edu/colorado/phet/motionseries/javastage/stage/StageContainer.java
+++ simulations-java/simulations/motion-series/src/edu/colorado/phet/motionseries/javastage/stage/StageContainer.java
@@ -5,14 +5,16 @@ import java.awt.geom.Rectangle2D;
 
 /**
  * This interface represents the bounds (in screen coordinates) of an object that contains a Stage.
- * Currently this is only implemented in PlayArea, but future versions may include PNodes that act as StageContainers themselves.
+ * Currently this is only implemented in PlayArea, but future versions may include PNodes that act 
+ * as StageContainers themselves.
  *
  * @see PlayArea
  * @author Sam Reid
  */
 public interface StageContainer {
     /**
-     * Returns the bounds of the StageContainer.  This must be given as a defensive copy, as clients should be able to mutate this as part of Piccolo's coordinate transform system.
+     * Returns the bounds of the StageContainer.  This must be given as a defensive copy, 
+     * as clients should be able to mutate this as part of Piccolo's coordinate transform system.
      *
      * @return the bounds of the StageContainer.
      */
