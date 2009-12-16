fe63a4475278a5202f92152f5d2f4104ff126961
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-12-16 01:35:18 +0000

    Wicket-website: enhanced phetrequestcycle with docs and general ripper detection

--- trunk/web/wicket-website/src/java/edu/colorado/phet/website/util/PhetRequestCycle.java
+++ trunk/web/wicket-website/src/java/edu/colorado/phet/website/util/PhetRequestCycle.java
@@ -1,13 +1,23 @@
 package edu.colorado.phet.website.util;
 
+import org.apache.log4j.Logger;
 import org.apache.wicket.Response;
 import org.apache.wicket.protocol.http.WebApplication;
 import org.apache.wicket.protocol.http.WebRequest;
 import org.apache.wicket.protocol.http.WebRequestCycle;
 import org.apache.wicket.protocol.http.request.WebClientInfo;
-import org.apache.log4j.Logger;
 import org.hibernate.Session;
 
+/**
+ * A request cycle is created for each HTTP request, and is available at any point from pages or components by calling
+ * (PhetRequestCycle) getRequestCycle(). Only PhetRequestCycles should be returned.
+ * <p/>
+ * The PhetRequestCycle is responsible for initializing a Hibernate session that will be used for all database
+ * interactions for the pages, components, and everything else.
+ * <p/>
+ * Additionally, support is provided to determine whether this request was made from a specialized ripper (for
+ * external installers)
+ */
 public class PhetRequestCycle extends WebRequestCycle {
 
     private Session session;
@@ -15,6 +25,7 @@ public class PhetRequestCycle extends WebRequestCycle {
 
     public static final String KSU_RIPPER_USER_AGENT = "httrack-web-mirror-ar";
     public static final String YOUNG_AND_FREEDMAN_USER_AGENT = "httrack-web-mirror-yf";
+    public static final String RIPPER_USER_AGENT_SUBSTRING = "httrack-web";
 
     private static Logger logger = Logger.getLogger( PhetRequestCycle.class.getName() );
 
@@ -45,6 +56,10 @@ public class PhetRequestCycle extends WebRequestCycle {
 
     public String getUserAgent() {
         return ( (WebClientInfo) getClientInfo() ).getUserAgent();
+    }
+
+    public boolean isInstaller() {
+        return getUserAgent().startsWith( RIPPER_USER_AGENT_SUBSTRING );
     }
 
     public boolean isKsuRipperRequest() {
