500b85a7ddf0e75e9299b4090fbfbfdd4a939c3c
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-12-16 22:45:14 +0000

    New motion strategy.  The intent is to use this to simulate bumping, but it may have other applications.

---
+++ trunk/simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/ThereAndBackMotionStrategy.java
@@ -0,0 +1,34 @@
+/* Copyright 2009, University of Colorado */
+
+package edu.colorado.phet.genenetwork.model;
+
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+
+/**
+ * This is a motion strategy that moves to the specified location and then
+ * comes back to the original location.
+ * 
+ * @author John Blanco
+ */
+public class ThereAndBackMotionStrategy extends LinearMotionStrategy {
+
+	private static final double DEFAULT_VELOCITY = 10;
+	
+	private Point2D startingPoint = new Point2D.Double();
+	private boolean outwardBound = true;
+	
+	
+	public ThereAndBackMotionStrategy(IModelElement modelElement, Point2D destination, Rectangle2D bounds) {
+		super(modelElement, bounds, destination, DEFAULT_VELOCITY);
+		startingPoint.setLocation(modelElement.getPositionRef());
+	}
+
+	@Override
+	public void updatePositionAndMotion(double dt) {
+		if (outwardBound && isDestinationReached()){
+			// We're reached the remote destination, time for the return trip.
+			setDestination(startingPoint);
+		}
+	}
+}
