e3b333caf37c33f1a4d0a10c549189e07f47a1d4
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-12-21 18:52:25 +0000

    Intermediate commit on the road to developing the legend.

--- trunk/simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/GeneNetworkStrings.java
+++ trunk/simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/GeneNetworkStrings.java
@@ -21,10 +21,13 @@ public class GeneNetworkStrings {
     public static final String MESSAGE_NOT_A_CONFIG = PhetCommonResources.getString( "XMLPersistenceManager.message.notAConfigFile" );
     
     // Captions for items in the DNA segment tool box.
-    public static final String POLYMERASE_TOOL_BOX_CAPTION = GeneNetworkResources.getString( "Toolbox.Polymerase" );
     public static final String POLYMERASE_BINDING_REGION_TOOL_BOX_CAPTION = GeneNetworkResources.getString( "Toolbox.PolymeraseBindingRegion" );
     public static final String LACI_BINDING_REGION_TOOL_BOX_CAPTION = GeneNetworkResources.getString( "Toolbox.LacIBindingRegion" );
     
+    // Captions for items in the legend.
+    public static final String POLYMERASE_LEGEND_CAPTION = GeneNetworkResources.getString( "Legend.Polymerase" );
+    public static final String LAC_Z_LEGEND_CAPTION = GeneNetworkResources.getString( "Legend.LacZ" );
+
     public static final String CLOCK_SPEED_CONTROL_CAPTION = GeneNetworkResources.getString("ClockSpeedControlCaption");
 
 }
