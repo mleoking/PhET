1675e7efb4b7ed023fd755a72fb152ac1b3ec704
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2009-12-21 23:47:27 +0000

    read-only values shown below Before box in Game

--- trunk/simulations-java/simulations/reactants-products-and-leftovers/src/edu/colorado/phet/reactantsproductsandleftovers/view/game/GameBeforeNode.java
+++ trunk/simulations-java/simulations/reactants-products-and-leftovers/src/edu/colorado/phet/reactantsproductsandleftovers/view/game/GameBeforeNode.java
@@ -1,17 +1,22 @@
 package edu.colorado.phet.reactantsproductsandleftovers.view.game;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.Stroke;
+import java.util.ArrayList;
 
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.PhetPNode;
 import edu.colorado.phet.reactantsproductsandleftovers.RPALConstants;
 import edu.colorado.phet.reactantsproductsandleftovers.RPALStrings;
+import edu.colorado.phet.reactantsproductsandleftovers.controls.QuantityValueNode;
 import edu.colorado.phet.reactantsproductsandleftovers.model.Reactant;
 import edu.colorado.phet.reactantsproductsandleftovers.module.game.GameModel;
 import edu.colorado.phet.reactantsproductsandleftovers.module.game.GameModel.GameAdapter;
 import edu.colorado.phet.reactantsproductsandleftovers.module.game.GameModel.GameListener;
 import edu.colorado.phet.reactantsproductsandleftovers.view.BoxNode;
+import edu.colorado.phet.reactantsproductsandleftovers.view.BracketedLabelNode;
 import edu.colorado.phet.reactantsproductsandleftovers.view.ImageLayoutNode;
 import edu.colorado.phet.reactantsproductsandleftovers.view.SubstanceImageNode;
 import edu.colorado.phet.reactantsproductsandleftovers.view.ImageLayoutNode.GridLayoutNode;
@@ -28,10 +33,19 @@ public class GameBeforeNode extends PhetPNode {
     private static final Font TITLE_FONT = new PhetFont( 24 );
     private static final double TITLE_Y_SPACING = 10;
     
+    private static final double CONTROLS_Y_SPACING = 15;
+    
+    private static final double BRACKET_Y_SPACING = 3;
+    private static final PhetFont BRACKET_FONT = new PhetFont( 16 );
+    private static final Color BRACKET_TEXT_COLOR = Color.BLACK;
+    private static final Color BRACKET_COLOR = RPALConstants.BEFORE_AFTER_BOX_COLOR;
+    private static final Stroke BRACKET_STROKE = new BasicStroke( 0.75f );
+    
     private final GameModel model;
     private final BoxNode boxNode;
     private final GameListener gameListener;
     private final ImageLayoutNode imageLayoutNode;
+    private final ArrayList<QuantityValueNode> quantityValueNodes; // quantity controls for reactants
     
     public GameBeforeNode( GameModel model ) {
         super();
@@ -46,6 +60,16 @@ public class GameBeforeNode extends PhetPNode {
         titleNode.setTextPaint( Color.BLACK );
         addChild( titleNode );
         
+        // one quantity control for each reactant
+        quantityValueNodes = new ArrayList<QuantityValueNode>();
+        Reactant[] reactants = model.getReaction().getReactants();
+        for ( Reactant reactant : reactants ) {
+            QuantityValueNode quantityNode = new QuantityValueNode( reactant, model.getQuantityRange(), RPALConstants.HISTOGRAM_IMAGE_SCALE, true /* showName */ );
+            quantityNode.setEditable( false );
+            addChild( quantityNode );
+            quantityValueNodes.add( quantityNode );
+        }
+        
         // layout, origin at upper-left corner of box
         double x = 0;
         double y = 0;
@@ -54,6 +78,28 @@ public class GameBeforeNode extends PhetPNode {
         x = boxNode.getFullBoundsReference().getCenterX() - ( titleNode.getFullBoundsReference().getWidth() / 2 );
         y = boxNode.getFullBoundsReference().getMinY() - titleNode.getFullBoundsReference().getHeight() - TITLE_Y_SPACING;
         titleNode.setOffset( x, y );
+        // reactant quantity controls, horizontally centered in "cells"
+        double margin = ( reactants.length > 2 ) ? 0 : ( 0.15 * BOX_SIZE.getWidth() ); // make 2 reactants case look nice
+        final double deltaX = ( boxNode.getFullBoundsReference().getWidth() - ( 2 * margin ) ) / ( reactants.length );
+        x = boxNode.getFullBoundsReference().getMinX() + margin + ( deltaX / 2 );
+        y = boxNode.getFullBoundsReference().getMaxY() + CONTROLS_Y_SPACING;
+        for ( int i = 0; i < reactants.length; i++ ) {
+            quantityValueNodes.get( i ).setOffset( x, y );
+            x += deltaX;
+        }
+        
+        // reactants bracket, after doing layout of leftover quantity displays
+        double startX = quantityValueNodes.get( 0 ).getFullBoundsReference().getMinX();
+        double endX = quantityValueNodes.get( quantityValueNodes.size() - 1 ).getFullBoundsReference().getMaxX();
+        double width = endX - startX;
+        PNode reactantsLabelNode = new BracketedLabelNode( RPALStrings.LABEL_REACTANTS, width, BRACKET_FONT, BRACKET_TEXT_COLOR, BRACKET_COLOR, BRACKET_STROKE );
+        addChild( reactantsLabelNode );
+        x = startX;
+        y = 0;
+        for ( QuantityValueNode node : quantityValueNodes ) {
+            y = Math.max( y, node.getFullBoundsReference().getMaxY() + BRACKET_Y_SPACING );
+        }
+        reactantsLabelNode.setOffset( x, y );
         
         // sync with model
         this.model = model;
@@ -63,7 +109,7 @@ public class GameBeforeNode extends PhetPNode {
         };
         model.addGameListener( gameListener );
         
-        // image layout strategy
+        // images
         imageLayoutNode = new GridLayoutNode( BOX_SIZE );
         addChild( imageLayoutNode );
         addReactantImages();
