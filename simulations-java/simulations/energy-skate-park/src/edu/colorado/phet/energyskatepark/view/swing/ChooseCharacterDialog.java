7f8fd1a026e2f9c015a52d96d4c2f7291b1cfb1a
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-01-06 01:05:58 +0000

    switch to use PaintImmediateDialog, see #2042

--- simulations-java/simulations/energy-skate-park/src/edu/colorado/phet/energyskatepark/view/swing/ChooseCharacterDialog.java
+++ simulations-java/simulations/energy-skate-park/src/edu/colorado/phet/energyskatepark/view/swing/ChooseCharacterDialog.java
@@ -15,6 +15,7 @@ import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.event.MouseInputAdapter;
 
+import edu.colorado.phet.common.phetcommon.application.PaintImmediateDialog;
 import edu.colorado.phet.common.phetcommon.resources.PhetCommonResources;
 import edu.colorado.phet.common.phetcommon.view.util.BufferedImageUtils;
 import edu.colorado.phet.common.phetcommon.view.util.ImageLoader;
@@ -22,14 +23,13 @@ import edu.colorado.phet.common.phetcommon.view.util.SwingUtils;
 import edu.colorado.phet.energyskatepark.EnergySkateParkModule;
 import edu.colorado.phet.energyskatepark.EnergySkateParkStrings;
 import edu.colorado.phet.energyskatepark.SkaterCharacter;
-import edu.colorado.phet.energyskatepark.util.ESPPaintImmediateDialog;
 import edu.colorado.phet.energyskatepark.util.EnergySkateParkLogging;
 
 /**
  * Author: Sam Reid
  * Mar 30, 2007, 1:16:29 PM
  */
-public class ChooseCharacterDialog extends ESPPaintImmediateDialog {
+public class ChooseCharacterDialog extends PaintImmediateDialog {
     private JPanel contentPanel = new JPanel( new GridBagLayout() );
     private GridBagConstraints gridBagConstraints = new GridBagConstraints();
     private ArrayList characterPanels = new ArrayList();
