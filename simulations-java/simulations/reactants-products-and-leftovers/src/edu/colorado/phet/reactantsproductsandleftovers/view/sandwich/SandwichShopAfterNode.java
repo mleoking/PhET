5907d1cb36033cd83f1ec1627b023cd42d13925b
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-01-07 18:16:45 +0000

    promote Before/After box classes to top-level types

---
+++ simulations-java/simulations/reactants-products-and-leftovers/src/edu/colorado/phet/reactantsproductsandleftovers/view/sandwich/SandwichShopAfterNode.java
@@ -0,0 +1,21 @@
+/* Copyright 2010, University of Colorado */
+
+package edu.colorado.phet.reactantsproductsandleftovers.view.sandwich;
+
+import edu.colorado.phet.reactantsproductsandleftovers.RPALConstants;
+import edu.colorado.phet.reactantsproductsandleftovers.RPALStrings;
+import edu.colorado.phet.reactantsproductsandleftovers.module.sandwichshop.SandwichShopModel;
+import edu.colorado.phet.reactantsproductsandleftovers.view.AbstractAfterNode;
+import edu.colorado.phet.reactantsproductsandleftovers.view.ImageLayoutNode.StackedLayoutNode;
+
+/**
+ * The "After" box in the "Sandwich Shop" module.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
+public class SandwichShopAfterNode extends AbstractAfterNode {
+
+    public SandwichShopAfterNode( SandwichShopModel model ) {
+        super( RPALStrings.LABEL_AFTER_SANDWICH, model.getReaction(), model.getQuantityRange(), false /* showSubstanceName */, new StackedLayoutNode( RPALConstants.BEFORE_AFTER_BOX_SIZE ) );
+    }
+}
