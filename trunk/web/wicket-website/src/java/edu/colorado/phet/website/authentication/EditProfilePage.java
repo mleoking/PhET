1ae4e149f350bea852070d902150253fbb4d9215
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-01-07 11:56:58 +0000

    Wicket-website: edit profile work

--- trunk/web/wicket-website/src/java/edu/colorado/phet/website/authentication/EditProfilePage.java
+++ trunk/web/wicket-website/src/java/edu/colorado/phet/website/authentication/EditProfilePage.java
@@ -1,8 +1,18 @@
 package edu.colorado.phet.website.authentication;
 
-import org.apache.wicket.PageParameters;
+import java.util.Arrays;
+import java.util.List;
 
-import edu.colorado.phet.website.templates.PhetMenuPage;
+import org.apache.log4j.Logger;
+import org.apache.wicket.PageParameters;
+import org.apache.wicket.markup.html.form.CheckBox;
+import org.apache.wicket.markup.html.form.DropDownChoice;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.util.value.ValueMap;
+
+import edu.colorado.phet.website.data.PhetUser;
 import edu.colorado.phet.website.templates.PhetPage;
 import edu.colorado.phet.website.util.PageContext;
 import edu.colorado.phet.website.util.PhetUrlMapper;
@@ -13,12 +23,34 @@ public class EditProfilePage extends PhetPage {
 
     // TODO: i18nize
 
+    private TextField name;
+    private TextField organization;
+    private DropDownChoice description;
+    private TextField jobTitle;
+
+    private TextField address1;
+    private TextField address2;
+    private TextField city;
+    private TextField state;
+    private TextField country;
+    private TextField zipcode;
+
+    private TextField phone1;
+    private TextField phone2;
+    private TextField fax;
+
+    private CheckBox receiveEmail;
+
+    private static Logger logger = Logger.getLogger( EditProfilePage.class.getName() );
+
     public EditProfilePage( PageParameters parameters ) {
         super( parameters );
 
         AuthenticatedPage.checkSignedIn();
 
         addTitle( "Edit Profile" );
+
+        add( new EditProfileForm( "edit-profile-form" ) );
     }
 
     public static void addToMapper( PhetUrlMapper mapper ) {
@@ -32,5 +64,67 @@ public class EditProfilePage extends PhetPage {
                 return "edit-profile";
             }
         };
+    }
+
+    private List<String> getDescriptionOptions() {
+        return Arrays.asList(
+                "I am a teacher who uses PhET in my classes",
+                "I am a teacher interested in using PhET in the future",
+                "I am a student who uses PhET",
+                "I am a student interested in using PhET in the future",
+                "I am just interested in physics",
+                "Other"
+        );
+    }
+
+    private final class EditProfileForm extends Form {
+
+        private final ValueMap properties = new ValueMap();
+
+        public EditProfileForm( String id ) {
+            super( id );
+
+            PhetUser user = PhetSession.get().getUser();
+
+            properties.add( "name", user.getName() );
+            properties.add( "organization", user.getOrganization() );
+            properties.add( "description", user.getDescription() );
+            properties.add( "jobTitle", user.getJobTitle() );
+            properties.add( "address1", user.getAddress1() );
+            properties.add( "address2", user.getAddress2() );
+            properties.add( "city", user.getCity() );
+            properties.add( "state", user.getState() );
+            properties.add( "country", user.getCountry() );
+            properties.add( "zipcode", user.getZipcode() );
+            properties.add( "phone1", user.getPhone1() );
+            properties.add( "phone2", user.getPhone2() );
+            properties.add( "fax", user.getFax() );
+            properties.add( "receiveEmail", Boolean.toString( user.isReceiveEmail() ) );
+
+            add( name = new TextField( "name", new PropertyModel( properties, "name" ) ) );
+            add( organization = new TextField( "organization", new PropertyModel( properties, "organization" ) ) );
+            add( description = new DropDownChoice( "description", new PropertyModel( properties, "description" ), getDescriptionOptions() ) );
+            add( jobTitle = new TextField( "jobTitle", new PropertyModel( properties, "jobTitle" ) ) );
+            add( address1 = new TextField( "address1", new PropertyModel( properties, "address1" ) ) );
+            add( address2 = new TextField( "address2", new PropertyModel( properties, "address2" ) ) );
+            add( city = new TextField( "city", new PropertyModel( properties, "city" ) ) );
+            add( state = new TextField( "state", new PropertyModel( properties, "state" ) ) );
+            add( country = new TextField( "country", new PropertyModel( properties, "country" ) ) );
+            add( zipcode = new TextField( "zipcode", new PropertyModel( properties, "zipcode" ) ) );
+            add( phone1 = new TextField( "phone1", new PropertyModel( properties, "phone1" ) ) );
+            add( phone2 = new TextField( "phone2", new PropertyModel( properties, "phone2" ) ) );
+            add( fax = new TextField( "fax", new PropertyModel( properties, "fax" ) ) );
+            add( receiveEmail = new CheckBox( "receiveEmail", new PropertyModel( properties, "receiveEmail" ) ) );
+
+
+        }
+
+        @Override
+        protected void onSubmit() {
+            // for some reason: very broken!
+            for ( Object o : properties.keySet() ) {
+                logger.debug( o.toString() + ": " + properties.get( o ) );
+            }
+        }
     }
 }
