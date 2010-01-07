7abe67c74a7a3ece2e4a423e9552fcd923f71b1d
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-01-07 09:35:45 +0000

    Wicket-website: log in/out links enhancement

--- web/wicket-website/src/java/edu/colorado/phet/website/authentication/EditProfilePage.java
+++ web/wicket-website/src/java/edu/colorado/phet/website/authentication/EditProfilePage.java
@@ -3,16 +3,22 @@ package edu.colorado.phet.website.authentication;
 import org.apache.wicket.PageParameters;
 
 import edu.colorado.phet.website.templates.PhetMenuPage;
+import edu.colorado.phet.website.templates.PhetPage;
 import edu.colorado.phet.website.util.PageContext;
 import edu.colorado.phet.website.util.PhetUrlMapper;
 import edu.colorado.phet.website.util.links.AbstractLinker;
 import edu.colorado.phet.website.util.links.Linkable;
 
-public class EditProfilePage extends PhetMenuPage {
+public class EditProfilePage extends PhetPage {
+
+    // TODO: i18nize
+
     public EditProfilePage( PageParameters parameters ) {
         super( parameters );
 
         AuthenticatedPage.checkSignedIn();
+
+        addTitle( "Edit Profile" );
     }
 
     public static void addToMapper( PhetUrlMapper mapper ) {
