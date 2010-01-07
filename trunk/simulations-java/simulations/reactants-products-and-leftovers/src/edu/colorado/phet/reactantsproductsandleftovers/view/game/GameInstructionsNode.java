2c7a6076466a565da55a08c6073ddb85fefa8adf
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-01-07 22:22:14 +0000

    remove arrow code from GameInstructionsNode, no longer needed

--- trunk/simulations-java/simulations/reactants-products-and-leftovers/src/edu/colorado/phet/reactantsproductsandleftovers/view/game/GameInstructionsNode.java
+++ trunk/simulations-java/simulations/reactants-products-and-leftovers/src/edu/colorado/phet/reactantsproductsandleftovers/view/game/GameInstructionsNode.java
@@ -7,45 +7,20 @@ import java.awt.Font;
 
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.nodes.HTMLNode;
-import edu.umd.cs.piccolox.nodes.PComposite;
 
 
-public class GameInstructionsNode extends PComposite {
-    
-    private static final boolean SHOW_ARROW = false;
+public class GameInstructionsNode extends HTMLNode {
     
     private static final PhetFont FONT = new PhetFont( Font.BOLD, 36 );
     private static final Color COLOR = Color.YELLOW;
     
-    private final HTMLNode htmlNode;
-    private final DownArrowNode arrowNode;
-    
     public GameInstructionsNode( String html ) {
-        super();
-        
-        htmlNode = new HTMLNode( html, FONT, COLOR );
-        addChild( htmlNode );
-        
-        arrowNode = new DownArrowNode( 80 );
-        if ( SHOW_ARROW ) {
-            addChild( arrowNode );
-        }
-        
-        updateLayout();
+        super( html );
+        setHTMLColor( COLOR );
+        setFont( FONT );
     }
     
     public void setText( String html ) {
-        htmlNode.setHTML( html );
-        updateLayout();
-    }
-    
-    private void updateLayout() {
-        // layout, origin at upper left of text
-        double x = 0;
-        double y = 0;
-        htmlNode.setOffset( 0, 0 );
-        x = htmlNode.getFullBounds().getCenterX();
-        y = htmlNode.getFullBounds().getMaxY() + 10;
-        arrowNode.setOffset( x, y );
+        setHTML( html );
     }
 }
