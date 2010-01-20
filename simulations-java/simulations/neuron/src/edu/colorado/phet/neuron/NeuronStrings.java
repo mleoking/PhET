8a635426c7343595a5964d2f39d27a030d865fbe
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-01-20 04:10:46 +0000

    Added initial version of the "Stimulus Pulse" button to the control panel.

--- simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/NeuronStrings.java
+++ simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/NeuronStrings.java
@@ -37,4 +37,5 @@ public class NeuronStrings {
     
     public static final String SHOW_VOLTMETER = getString( "ShowVoltmeter" );
     public static final String SHOW_POTENTIAL_CHART = getString( "ShowPotentialChart" );
+    public static final String STIMULUS_PULSE = getString( "StimulusPulse" );
 }
