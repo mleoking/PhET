5a42dfce89516dd03ef491e8e1c1567c115d3a1d
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-01-21 22:05:23 +0000

    Added options to the command line of the ripper that are intended to increase the amount of bandwidth that can be used when ripping the web site.  See #2117.

--- trunk/util/phet-installer/new-automated/custom-installers/bin/config.php
+++ trunk/util/phet-installer/new-automated/custom-installers/bin/config.php
@@ -183,7 +183,7 @@
     define("RIPPER_USER_AGENT_KSU",  '"httrack-web-mirror-ar"');
 
     // Command-line args of the ripper:
-    define("RIPPER_OPTIONS", " -j %q0 -%e0 -r10 -s0");
+    define("RIPPER_OPTIONS", " -j %q0 -%e0 -r10 -s0 -A100000000 --disable-security-limits ");
     define("RIPPER_ARGS_YF", '"'.PHET_ROOT_URL.'" -O "'.RIPPED_WEBSITE_ROOT.'" '.PHET_RIPPER_FILTER_YF." -F ".RIPPER_USER_AGENT_YF.RIPPER_OPTIONS);
     define("RIPPER_ARGS_KSU", '"'.PHET_ROOT_URL.'" -O "'.RIPPED_WEBSITE_ROOT.'" '.PHET_RIPPER_FILTER_KSU." -F ".RIPPER_USER_AGENT_KSU.RIPPER_OPTIONS);
     define("RIPPER_VERSION_ARGS", " --version");
