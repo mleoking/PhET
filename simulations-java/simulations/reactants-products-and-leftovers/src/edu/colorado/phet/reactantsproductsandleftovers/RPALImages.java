9cf460559dcbde7a0319b990b4a265869d3a5da2
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-01-22 21:18:53 +0000

    add images for all molecules, uniform height = 38 pixels

--- simulations-java/simulations/reactants-products-and-leftovers/src/edu/colorado/phet/reactantsproductsandleftovers/RPALImages.java
+++ simulations-java/simulations/reactants-products-and-leftovers/src/edu/colorado/phet/reactantsproductsandleftovers/RPALImages.java
@@ -13,18 +13,56 @@ import java.awt.image.BufferedImage;
 public class RPALImages {
 
     private RPALImages() {}
-    
-    public static final BufferedImage BREAD = RPALResources.getImage( "bread.png" );
-    public static final BufferedImage CHEESE = RPALResources.getImage( "cheese.png" );
-    public static final BufferedImage MEAT = RPALResources.getImage( "meat.png" );
-    
-    public static final BufferedImage CH4 = RPALResources.getImage( "CH4.png" );
-    public static final BufferedImage CO2 = RPALResources.getImage( "CO2.png" );
-    public static final BufferedImage H2 = RPALResources.getImage( "H2.png" );
-    public static final BufferedImage H2O = RPALResources.getImage( "H2O.png" );
-    public static final BufferedImage N2 = RPALResources.getImage( "N2.png" );
-    public static final BufferedImage NH3 = RPALResources.getImage( "NH3.png" );
-    public static final BufferedImage O2 = RPALResources.getImage( "O2.png" );
-    
+
+    // misc images
     public static final BufferedImage STOPWATCH = RPALResources.getImage( "blue-stopwatch.png" );
+    
+    // sandwich images
+    public static final BufferedImage BREAD = getSandwichImage( "bread.png" );
+    public static final BufferedImage CHEESE = getSandwichImage( "cheese.png" );
+    public static final BufferedImage MEAT = getSandwichImage( "meat.png" );
+    
+    // molecule images
+    public static final BufferedImage C = getMoleculeImage( "C.png" );
+    public static final BufferedImage C2H2 = getMoleculeImage( "C2H2.png" );
+    public static final BufferedImage C2H4 = getMoleculeImage( "C2H4.png" );
+    public static final BufferedImage C2H5Cl = getMoleculeImage( "C2H5Cl.png" );
+    public static final BufferedImage C2H5OH = getMoleculeImage( "C2H5OH.png" );
+    public static final BufferedImage C2H6 = getMoleculeImage( "C2H6.png" );
+    public static final BufferedImage CH2O = getMoleculeImage( "CH2O.png" );
+    public static final BufferedImage CH3OH = getMoleculeImage( "CH3OH.png" );
+    public static final BufferedImage CH4 = getMoleculeImage( "CH4.png" );
+    public static final BufferedImage CO = getMoleculeImage( "CO.png" );
+    public static final BufferedImage CO2 = getMoleculeImage( "CO2.png" );
+    public static final BufferedImage CS2 = getMoleculeImage( "CS2.png" );
+    public static final BufferedImage Cl2 = getMoleculeImage( "Cl2.png" );
+    public static final BufferedImage F2 = getMoleculeImage( "F2.png" );
+    public static final BufferedImage H2 = getMoleculeImage( "H2.png" );
+    public static final BufferedImage H2O = getMoleculeImage( "H2O.png" );
+    public static final BufferedImage H2S = getMoleculeImage( "H2S.png" );
+    public static final BufferedImage HCl = getMoleculeImage( "HCl.png" );
+    public static final BufferedImage HF = getMoleculeImage( "HF.png" );
+    public static final BufferedImage N2 = getMoleculeImage( "N2.png" );
+    public static final BufferedImage N2O = getMoleculeImage( "N2O.png" );
+    public static final BufferedImage NH3 = getMoleculeImage( "NH3.png" );
+    public static final BufferedImage NO = getMoleculeImage( "NO.png" );
+    public static final BufferedImage NO2 = getMoleculeImage( "NO2.png" );
+    public static final BufferedImage O2 = getMoleculeImage( "O2.png" );
+    public static final BufferedImage OF2 = getMoleculeImage( "OF2.png" );
+    public static final BufferedImage P4 = getMoleculeImage( "P4.png" );
+    public static final BufferedImage PCl3 = getMoleculeImage( "PCl3.png" );
+    public static final BufferedImage PCl5 = getMoleculeImage( "PCl5.png" );
+    public static final BufferedImage PF3 = getMoleculeImage( "PF3.png" );
+    public static final BufferedImage PH3 = getMoleculeImage( "PH3.png" );
+    public static final BufferedImage S = getMoleculeImage( "S.png" );
+    public static final BufferedImage SO2 = getMoleculeImage( "SO2.png" );
+    public static final BufferedImage SO3 = getMoleculeImage( "SO3.png" );
+    
+    private static final BufferedImage getSandwichImage( String name ) {
+        return RPALResources.getImage( "sandwich/" + name );
+    }
+    
+    private static final BufferedImage getMoleculeImage( String name ) {
+        return RPALResources.getImage( "molecules/" + name );
+    }
 }
