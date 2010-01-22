4de2f422dcad2a7801f950d41c6c38cec60cc7a5
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-01-22 22:19:02 +0000

    add symbols for all molecules, make the creation of HTML subscripts bullet proof

--- simulations-java/simulations/reactants-products-and-leftovers/src/edu/colorado/phet/reactantsproductsandleftovers/RPALSymbols.java
+++ simulations-java/simulations/reactants-products-and-leftovers/src/edu/colorado/phet/reactantsproductsandleftovers/RPALSymbols.java
@@ -13,11 +13,66 @@ package edu.colorado.phet.reactantsproductsandleftovers;
  */
 public class RPALSymbols {
     
-    public static final String CH4 = "CH<sub>4</sub>";
-    public static final String CO2 = "CO<sub>2</sub>";
-    public static final String H2 = "H<sub>2</sub>";
-    public static final String H2O = "H<sub>2</sub>O";
-    public static final String O2 = "O<sub>2</sub>"; 
-    public static final String N2 = "N<sub>2</sub>";
-    public static final String NH3 = "NH<sub>3</sub>";
+    public static final String C = toSubscript( "C" );
+    public static final String C2H2 = toSubscript( "C2H2" );
+    public static final String C2H4 = toSubscript( "C2H4" );
+    public static final String C2H5Cl = toSubscript( "C2H5Cl" );
+    public static final String C2H5OH = toSubscript( "C2H5OH" );
+    public static final String C2H6 = toSubscript( "C2H6" );
+    public static final String CH2O = toSubscript( "CH2O" );
+    public static final String CH3OH = toSubscript( "CH3OH" );
+    public static final String CH4 = toSubscript( "CH4" );
+    public static final String Cl2 = toSubscript( "Cl2" );
+    public static final String CO = toSubscript( "CO" );
+    public static final String CO2 = toSubscript( "CO2" );
+    public static final String CS2 = toSubscript( "CS2" );
+    public static final String F2 = toSubscript( "F2" );
+    public static final String H2 = toSubscript( "H2" );
+    public static final String H2O = toSubscript( "H2O" );
+    public static final String H2S = toSubscript( "H2S" );
+    public static final String HCl = toSubscript( "HCl" );
+    public static final String HF = toSubscript( "HF" );
+    public static final String N2 = toSubscript( "N2" );
+    public static final String N2O = toSubscript( "N2O" );
+    public static final String NH3 = toSubscript( "NH3" );
+    public static final String O2 = toSubscript( "O2" );
+    public static final String OF2 = toSubscript( "OF2" );
+    public static final String P4 = toSubscript( "P4" );
+    public static final String PCl3 = toSubscript( "PCl3" );
+    public static final String PCl5 = toSubscript( "PCl5" );
+    public static final String PF3 = toSubscript( "PF3" );
+    public static final String PH3 = toSubscript( "PH3" );
+    public static final String S = toSubscript( "S" );
+    public static final String SO2 = toSubscript( "SO2" );
+    public static final String SO3 = toSubscript( "SO3" );
+    
+    /*
+     * Handles HTML subscript formatting.
+     * All numbers in a string are assumed to be part of a subscript, and will be enclosed in a <sub> tag.
+     * For example, "C2H4" is converted to "C<sub>2</sub>H<sub>4</sub>".
+     */
+    private static final String toSubscript( String inString ) {
+        String outString = "";
+        boolean sub = false; // are we in a <sub> tag?
+        for ( int i = 0; i < inString.length(); i++ ) {
+            final char c = inString.charAt( i );
+            if ( !sub && Character.isDigit( c ) ) {
+                // start the subscript tag when a digit is found
+                outString += "<sub>";
+                sub = true;
+            }
+            else if ( sub && !Character.isDigit( c ) ) {
+                // end the subscript tag when a non-digit is found
+                outString += "</sub>";
+                sub = false;
+            }
+            outString += c;
+        }
+        // end the subscript tag if the string ends with a digit
+        if ( sub ) {
+            outString += "</sub>";
+            sub = false;
+        }
+        return outString;
+    }
 }
