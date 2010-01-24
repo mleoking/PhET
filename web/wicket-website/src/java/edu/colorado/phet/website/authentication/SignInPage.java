f0aa86dfe66378aa73de60507d477100aebb7cf5
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-01-24 10:01:59 +0000

    Wicket-website: converted sign in page to panel for translatability

--- web/wicket-website/src/java/edu/colorado/phet/website/authentication/SignInPage.java
+++ web/wicket-website/src/java/edu/colorado/phet/website/authentication/SignInPage.java
@@ -2,98 +2,36 @@ package edu.colorado.phet.website.authentication;
 
 import org.apache.log4j.Logger;
 import org.apache.wicket.PageParameters;
-import org.apache.wicket.markup.html.WebMarkupContainer;
-import org.apache.wicket.markup.html.form.CheckBox;
-import org.apache.wicket.markup.html.form.Form;
-import org.apache.wicket.markup.html.form.PasswordTextField;
-import org.apache.wicket.markup.html.form.TextField;
-import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.model.ResourceModel;
-import org.apache.wicket.request.target.basic.RedirectRequestTarget;
-import org.apache.wicket.util.value.ValueMap;
 
-import edu.colorado.phet.website.components.LocalizedText;
+import edu.colorado.phet.website.authentication.panels.SignInPanel;
 import edu.colorado.phet.website.templates.PhetPage;
 import edu.colorado.phet.website.util.PageContext;
-import edu.colorado.phet.website.util.PhetRequestCycle;
 import edu.colorado.phet.website.util.PhetUrlMapper;
 import edu.colorado.phet.website.util.links.AbstractLinker;
 import edu.colorado.phet.website.util.links.Linkable;
 
 public class SignInPage extends PhetPage {
 
-    // TODO: gracefully handle when user enters the wrong password
-
     // TODO: add translation links at the bottom?
-
-    private TextField username;
-    private PasswordTextField password;
-
-    private String destination = null;
 
     private static Logger logger = Logger.getLogger( SignInPage.class.getName() );
 
-    /**
-     * Whether to remember the user or not.
-     * NOTE: don't convert to local variable, PropertyModel uses reflection to change this value
-     */
-    private boolean remember = true;
-
     public SignInPage( PageParameters parameters ) {
         super( parameters, true );
+
+        String destination = null;
 
         if ( parameters != null && parameters.containsKey( "dest" ) ) {
             destination = parameters.getString( "dest" );
         }
 
-        // TODO: add register link to i18n
-
-        add( new SignInForm( "sign-in-form" ) );
-
         addTitle( new ResourceModel( "signIn.title" ) );
 
-        add( new LocalizedText( "to-register", "signIn.toRegister", new Object[]{RegisterPage.getLinker( destination == null ? "/" : destination ).getHref( getPageContext(), getPhetCycle() )} ) );
+        add( new SignInPanel( "sign-in-panel", getPageContext(), destination ) );
+
     }
 
-    public final class SignInForm extends Form {
-        private static final long serialVersionUID = 1L;
-
-        private final ValueMap properties = new ValueMap();
-
-        public SignInForm( final String id ) {
-            super( id );
-
-            add( username = new TextField( "username", new PropertyModel( properties, "username" ) ) );
-            add( password = new PasswordTextField( "password", new PropertyModel( properties, "password" ) ) );
-
-            final WebMarkupContainer rememberBox = new WebMarkupContainer( "remember" );
-            add( rememberBox );
-
-            rememberBox.add( new CheckBox( "remember-check", new PropertyModel( SignInPage.this, "remember" ) ) );
-
-            username.setPersistent( remember );
-        }
-
-        public final void onSubmit() {
-            if ( PhetSession.get().signIn( (PhetRequestCycle) getRequestCycle(), username.getModelObjectAsString(), password.getInput() ) ) {
-                if ( destination != null ) {
-                    getRequestCycle().setRequestTarget( new RedirectRequestTarget( destination ) );
-                }
-                else {
-                    if ( !SignInPage.this.continueToOriginalDestination() ) {
-                        getRequestCycle().setRequestTarget( new RedirectRequestTarget( "/" ) );
-                    }
-                }
-            }
-            else {
-                error( "Signing in failed" );
-            }
-        }
-    }
-
-    public final void forgetMe() {
-        getPage().removePersistedFormData( SignInPage.SignInForm.class, true );
-    }
 
     public static Linkable getLinker( final String destination ) {
         return new AbstractLinker() {
---
+++ web/wicket-website/src/java/edu/colorado/phet/website/authentication/panels/SignInPanel.java
@@ -0,0 +1,84 @@
+package edu.colorado.phet.website.authentication.panels;
+
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.form.CheckBox;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.PasswordTextField;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.request.target.basic.RedirectRequestTarget;
+import org.apache.wicket.util.value.ValueMap;
+import org.apache.log4j.Logger;
+
+import edu.colorado.phet.website.authentication.PhetSession;
+import edu.colorado.phet.website.authentication.RegisterPage;
+import edu.colorado.phet.website.components.LocalizedText;
+import edu.colorado.phet.website.panels.PhetPanel;
+import edu.colorado.phet.website.util.PageContext;
+import edu.colorado.phet.website.util.PhetRequestCycle;
+
+public class SignInPanel extends PhetPanel {
+
+    private TextField username;
+    private PasswordTextField password;
+
+    private String destination = null;
+
+    /**
+     * Whether to remember the user or not.
+     * NOTE: don't convert to local variable, PropertyModel uses reflection to change this value
+     */
+    private boolean remember = true;
+
+    private static Logger logger = Logger.getLogger( SignInPanel.class.getName() );
+
+    public SignInPanel( String id, PageContext context, String destination ) {
+        super( id, context );
+
+        this.destination = destination;
+
+        add( new SignInForm( "sign-in-form" ) );
+
+        add( new LocalizedText( "to-register", "signIn.toRegister", new Object[]{RegisterPage.getLinker( destination == null ? "/" : destination ).getHref( context, getPhetCycle() )} ) );
+    }
+
+    public final class SignInForm extends Form {
+        private static final long serialVersionUID = 1L;
+
+        private final ValueMap properties = new ValueMap();
+
+        public SignInForm( final String id ) {
+            super( id );
+
+            add( username = new TextField( "username", new PropertyModel( properties, "username" ) ) );
+            add( password = new PasswordTextField( "password", new PropertyModel( properties, "password" ) ) );
+
+            final WebMarkupContainer rememberBox = new WebMarkupContainer( "remember" );
+            add( rememberBox );
+
+            rememberBox.add( new CheckBox( "remember-check", new PropertyModel( SignInPanel.this, "remember" ) ) );
+
+            username.setPersistent( remember );
+        }
+
+        public final void onSubmit() {
+            if ( PhetSession.get().signIn( (PhetRequestCycle) getRequestCycle(), username.getModelObjectAsString(), password.getInput() ) ) {
+                if ( destination != null ) {
+                    getRequestCycle().setRequestTarget( new RedirectRequestTarget( destination ) );
+                }
+                else {
+                    if ( !SignInPanel.this.continueToOriginalDestination() ) {
+                        getRequestCycle().setRequestTarget( new RedirectRequestTarget( "/" ) );
+                    }
+                }
+            }
+            else {
+                error( "Signing in failed" );
+            }
+        }
+    }
+
+    public final void forgetMe() {
+        getPage().removePersistedFormData( SignInForm.class, true );
+    }
+}
