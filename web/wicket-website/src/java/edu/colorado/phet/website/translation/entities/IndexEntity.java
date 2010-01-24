a3fa99bf9fc459dda4f7d0bbe500fd547399528e
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-01-24 00:09:21 +0000

    Wicket-website: added i18n of login logout links, location of links handled correctly for RTL

--- web/wicket-website/src/java/edu/colorado/phet/website/translation/entities/IndexEntity.java
+++ web/wicket-website/src/java/edu/colorado/phet/website/translation/entities/IndexEntity.java
@@ -22,6 +22,11 @@ public class IndexEntity extends TranslationEntity {
         addString( "home.translateSimulations" );
         addString( "home.browseSims" );
         addString( "home.simulations" );
+
+        addString( "session.loginRegister" );
+        addString( "session.editProfile" );
+        addString( "session.logout" );
+
         addPreview( new PhetPanelFactory() {
             public PhetPanel getNewPanel( String id, PageContext context, PhetRequestCycle requestCycle ) {
                 return new IndexPanel( id, context );
