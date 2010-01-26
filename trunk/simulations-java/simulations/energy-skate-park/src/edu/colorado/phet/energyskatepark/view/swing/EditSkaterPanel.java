81024b1e8b04b7d7a10f15b1fcf75e6201408467
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-01-26 22:12:01 +0000

    Removed the controls for Bounciness and Stickiness, see #2129.

--- trunk/simulations-java/simulations/energy-skate-park/src/edu/colorado/phet/energyskatepark/view/swing/EditSkaterPanel.java
+++ trunk/simulations-java/simulations/energy-skate-park/src/edu/colorado/phet/energyskatepark/view/swing/EditSkaterPanel.java
@@ -23,26 +23,12 @@ public class EditSkaterPanel extends AdvancedPanel {
     public EditSkaterPanel( final EnergySkateParkModule module ) {
         super( EnergySkateParkStrings.getString( "controls.edit-skater" )+" >>", EnergySkateParkStrings.getString( "controls.hide-skater-properties" ) +" <<");
         this.module = module;
-        final EnergySkateParkSlider restitution = new EnergySkateParkSlider( EnergySkateParkStrings.getString( "controls.bounciness" ), "", 0, 1.0, 1.0 );
-        restitution.setModelTicks( new double[]{0, 0.5, 1} );
-        restitution.addChangeListener( new ChangeListener() {
-            public void stateChanged( ChangeEvent e ) {
-                module.setBounciness( restitution.getValue() );
-            }
-        } );
         final EnergySkateParkSlider mass = new EnergySkateParkSlider(
                 EnergySkateParkStrings.getString( "controls.mass" ), EnergySkateParkStrings.getString( "units.kg" ), 0.2, 200, 75 );
         mass.setModelTicks( new double[]{0.2, 75, 200} );
         mass.addChangeListener( new ChangeListener() {
             public void stateChanged( ChangeEvent e ) {
                 setMass( mass.getValue() );
-            }
-        } );
-
-        final EnergySkateParkSlider stickiness = new EnergySkateParkSlider( EnergySkateParkStrings.getString( "controls.skater.stickiness" ), "", 0.01, 5, Body.DEFAULT_STICKINESS );
-        stickiness.addChangeListener( new ChangeListener() {
-            public void stateChanged( ChangeEvent e ) {
-                setStickiness( stickiness.getValue() );
             }
         } );
 
@@ -65,16 +51,12 @@ public class EditSkaterPanel extends AdvancedPanel {
         module.getEnergySkateParkModel().addEnergyModelListener( new EnergySkateParkModel.EnergyModelListenerAdapter() {
             public void primaryBodyChanged() {
                 if( module.getEnergySkateParkModel().getNumBodies() > 0 ) {
-                    restitution.setValue( module.getEnergySkateParkModel().getBody( 0 ).getBounciness() );
                     mass.setValue( module.getEnergySkateParkModel().getBody( 0 ).getMass() );
-                    stickiness.setValue( module.getEnergySkateParkModel().getBody( 0 ).getStickiness() );
                 }
             }
         } );
 
-        addControl( restitution );
         addControl( mass );
-        addControl( stickiness );
         addControl( revertToDefaults );
     }
 
