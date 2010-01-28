74bb466667b4953323a62fa8a7407a3adcdd78ec
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-01-28 23:38:50 +0000

    see #2144, cleanup this file so I can work with it. - convert unnecessary data members to local variables - make constants static final - make unmutable data members final - add some comments for the next guy

--- simulations-java/simulations/eating-and-exercise/src/edu/colorado/phet/eatingandexercise/view/ScaleNode.java
+++ simulations-java/simulations/eating-and-exercise/src/edu/colorado/phet/eatingandexercise/view/ScaleNode.java
@@ -1,10 +1,14 @@
 package edu.colorado.phet.eatingandexercise.view;
 
-import java.awt.*;
+import java.awt.BasicStroke;
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.*;
+import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.border.BevelBorder;
 
 import edu.colorado.phet.common.phetcommon.view.VerticalLayoutPanel;
@@ -19,43 +23,57 @@ import edu.umd.cs.piccolo.nodes.PText;
 import edu.umd.cs.piccolox.pswing.PSwing;
 
 /**
+ * Scale that the human is standing on, used to show his/her weight.
+ * The scale has a pseudo-3D look, the weight is displayed on the front face.
+ * Radio buttons are provides for switching between English and Metric units.
+ * While these radio buttons are part of the scale, they don't affect just the
+ * scale, they change units for the entire sim.
+ * 
  * Created by: Sam
  * Apr 9, 2008 at 8:35:03 PM
  */
 public class ScaleNode extends PNode {
-    private EatingAndExerciseModel model;
-    private Human human;
-    private PText weightReadout;
-    private double faceWidth = 0.9;
-    private double faceHeight = 0.13;
-    private double faceY = 0.05;
-    private double depthDX = 0.06;
-    private double depthDY = 0.1;
-    private float strokeWidth = 0.02f;
-    private PSwing unitsPSwing;
-    private final Color scaleColor = Color.lightGray;
-    private PhetPPath faceNode;
+    
+    private static final double FACE_WIDTH = 0.9;
+    private static final double FACE_HEIGHT = 0.13;
+    private static final double FACE_Y = 0.05;
+    private static final double DEPTH_DX = 0.06;
+    private static final double DEPTH_DY = 0.1;
+    private static final float STROKE_WIDTH = 0.02f;
+    private static final Color SCALE_COLOR = Color.LIGHT_GRAY;
+    private static final String BMI_LABEL = EatingAndExerciseResources.getString( "bmi" );
+    private static final String BMI_UNITS = EatingAndExerciseResources.getString( "units.bmi" );
+    private static final double TEXT_SCALE = 1.0 / 175.0;
+    
+    private final EatingAndExerciseModel model;
+    private final Human human;
+    private final PText weightReadout;
 
     public ScaleNode( final EatingAndExerciseModel model, Human human ) {
+        
         this.model = model;
         this.human = human;
+        
+        // top face of the scale
         DoubleGeneralPath topPath = new DoubleGeneralPath();
+        topPath.moveTo( -FACE_WIDTH / 2, FACE_Y );
+        topPath.lineTo( -FACE_WIDTH / 2 + DEPTH_DX, FACE_Y - DEPTH_DY );
+        topPath.lineTo( FACE_WIDTH / 2 - DEPTH_DX, FACE_Y - DEPTH_DY );
+        topPath.lineTo( FACE_WIDTH / 2, FACE_Y );
+        topPath.lineTo( -FACE_WIDTH / 2, FACE_Y );
+        addChild( new PhetPPath( topPath.getGeneralPath(), SCALE_COLOR, new BasicStroke( STROKE_WIDTH ), Color.black ) );
 
-        topPath.moveTo( -faceWidth / 2, faceY );
-        topPath.lineTo( -faceWidth / 2 + depthDX, faceY - depthDY );
-        topPath.lineTo( faceWidth / 2 - depthDX, faceY - depthDY );
-        topPath.lineTo( faceWidth / 2, faceY );
-        topPath.lineTo( -faceWidth / 2, faceY );
-        addChild( new PhetPPath( topPath.getGeneralPath(), scaleColor, new BasicStroke( strokeWidth ), Color.black ) );
-
+        // front face
         DoubleGeneralPath facePath = new DoubleGeneralPath();
-        facePath.moveTo( -faceWidth / 2, faceY );
-        facePath.lineTo( -faceWidth / 2, faceY + faceHeight );
-        facePath.lineTo( faceWidth / 2, faceY + faceHeight );
-        facePath.lineTo( faceWidth / 2, faceY );
-        facePath.lineTo( -faceWidth / 2, faceY );
-        faceNode = new PhetPPath( facePath.getGeneralPath(), scaleColor, new BasicStroke( strokeWidth ), Color.black );
+        facePath.moveTo( -FACE_WIDTH / 2, FACE_Y );
+        facePath.lineTo( -FACE_WIDTH / 2, FACE_Y + FACE_HEIGHT );
+        facePath.lineTo( FACE_WIDTH / 2, FACE_Y + FACE_HEIGHT );
+        facePath.lineTo( FACE_WIDTH / 2, FACE_Y );
+        facePath.lineTo( -FACE_WIDTH / 2, FACE_Y );
+        PNode faceNode = new PhetPPath( facePath.getGeneralPath(), SCALE_COLOR, new BasicStroke( STROKE_WIDTH ), Color.black );
         addChild( faceNode );
+        
+        // monitor changes in the human's weight
         human.addListener( new Human.Adapter() {
             public void weightChanged() {
                 updateReadout();
@@ -65,13 +83,14 @@ public class ScaleNode extends PNode {
                 updateReadout();
             }
         } );
+        
+        // weight read-out appears on the front of the scale
         weightReadout = new EatingAndExercisePText( "??" );
-        double TEXT_SCALE = 1.0 / 175.0;
         weightReadout.scale( TEXT_SCALE );
         addChild( weightReadout );
-
         updateReadout();
 
+        // radio buttons for switching between English and Metric units
         JPanel units = new VerticalLayoutPanel();
         units.setBorder( BorderFactory.createBevelBorder( BevelBorder.LOWERED ) );
         ButtonGroup buttonGroup = new ButtonGroup();
@@ -92,8 +111,8 @@ public class ScaleNode extends PNode {
             units.add( jRadioButton );
         }
 
-        unitsPSwing = new PSwing( units );
-        unitsPSwing.setOffset( faceWidth / 2 + strokeWidth / 2, 0 );
+        PSwing unitsPSwing = new PSwing( units );
+        unitsPSwing.setOffset( FACE_WIDTH / 2 + STROKE_WIDTH / 2, 0 );
         unitsPSwing.scale( TEXT_SCALE * 0.75 );
         addChild( unitsPSwing );
         model.addListener( new EatingAndExerciseModel.Adapter() {
@@ -104,13 +123,11 @@ public class ScaleNode extends PNode {
     }
 
     private void updateReadout() {
-        String BMI = EatingAndExerciseResources.getString( "bmi" );
-        String BMI_UNITS = EatingAndExerciseResources.getString( "units.bmi" );
-        weightReadout.setText( "" + EatingAndExerciseStrings.WEIGHT_FORMAT.format( model.getUnits().modelToViewMass( human.getMass() ) ) + " " + model.getUnits().getMassUnit() + ", " + BMI + ": " + EatingAndExerciseStrings.BMI_FORMAT.format( human.getBMI() ) + " " + BMI_UNITS );
+        weightReadout.setText( "" + EatingAndExerciseStrings.WEIGHT_FORMAT.format( model.getUnits().modelToViewMass( human.getMass() ) ) + " " + model.getUnits().getMassUnit() + ", " + BMI_LABEL + ": " + EatingAndExerciseStrings.BMI_FORMAT.format( human.getBMI() ) + " " + BMI_UNITS );
         updateTextLayout();
     }
 
     private void updateTextLayout() {
-        weightReadout.setOffset( 0 - weightReadout.getFullBounds().getWidth() / 2, faceY + faceHeight - weightReadout.getFullBounds().getHeight() - 0.01 );
+        weightReadout.setOffset( 0 - weightReadout.getFullBounds().getWidth() / 2, FACE_Y + FACE_HEIGHT - weightReadout.getFullBounds().getHeight() - 0.01 );
     }
 }
