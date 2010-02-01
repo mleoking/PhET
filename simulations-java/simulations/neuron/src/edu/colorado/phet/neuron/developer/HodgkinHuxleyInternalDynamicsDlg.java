ab7105cb10b91b5ae30d3b2b70e52590b3c5bc59
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-02-01 21:37:12 +0000

    Added a button for capturing data.  Doesn't work yet.

--- simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/developer/HodgkinHuxleyInternalDynamicsDlg.java
+++ simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/developer/HodgkinHuxleyInternalDynamicsDlg.java
@@ -4,7 +4,10 @@ package edu.colorado.phet.neuron.developer;
 
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
@@ -34,12 +37,14 @@ public class HodgkinHuxleyInternalDynamicsDlg extends PaintImmediateDialog{
 	private JTextField potassiumChannelCurrentValue = new JTextField("00.0 mA");
 	private JLabel leakChannelCurrentLabel = new JLabel("Leak Channel: ");
 	private JTextField leakChannelCurrentValue = new JTextField("00.0 mA");
+	private JButton captureButton = new JButton();
+	private boolean capturing = false;
 	
 	public HodgkinHuxleyInternalDynamicsDlg(IClock clock, HodgkinHuxleyModel hhModel) {
 		hodgkinHuxleyModel = hhModel;
 		
 		setTitle("Hodgkin-Huxley Model Internal Dynamics");
-		setLayout(new GridLayout(4,2));
+		setLayout(new GridLayout(5,2));
 		
 		// Update ourself on every clock tick that we are visible.
 		clock.addClockListener(new ClockAdapter(){
@@ -73,6 +78,17 @@ public class HodgkinHuxleyInternalDynamicsDlg extends PaintImmediateDialog{
 		leakChannelCurrentValue.setFont(DISPLAY_FONT);
 		leakChannelCurrentValue.setEditable(false);
 		add(leakChannelCurrentValue);
+		
+		// Add a button that allows the user to capture the values.
+		add(captureButton);
+		captureButton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				capturing = !capturing;
+				updateCaptureButtonState();
+			}
+		});
+		updateCaptureButtonState();
+		
 
 		pack();
 	}
@@ -82,5 +98,14 @@ public class HodgkinHuxleyInternalDynamicsDlg extends PaintImmediateDialog{
 		sodiumChannelCurrentValue.setText(String.format("%.2f mA", hodgkinHuxleyModel.get_na_current()));
 		potassiumChannelCurrentValue.setText(String.format("%.2f mA", hodgkinHuxleyModel.get_k_current()));
 		leakChannelCurrentValue.setText(String.format("%.2f mA", hodgkinHuxleyModel.get_l_current()));
+	}
+	
+	private void updateCaptureButtonState(){
+		if (!capturing){
+			captureButton.setText("Start Capturing");
+		}
+		else{
+			captureButton.setText("Stop Capturing");
+		}
 	}
 }
