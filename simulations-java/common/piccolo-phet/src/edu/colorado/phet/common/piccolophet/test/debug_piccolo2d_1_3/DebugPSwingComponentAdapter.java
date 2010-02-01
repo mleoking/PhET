f28579bf9ee34e4853e44d52ffb27c98e8049bbf
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-02-01 22:05:23 +0000

    ee #2140, finally reproduced the problem in an example.  The example is still sort of complicated. But the problem only occurs when the example is executed in the AWT dispatch thread, like PhET sims.

--- simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/test/debug_piccolo2d_1_3/DebugPSwingComponentAdapter.java
+++ simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/test/debug_piccolo2d_1_3/DebugPSwingComponentAdapter.java
@@ -1,82 +1,271 @@
 package edu.colorado.phet.common.piccolophet.test.debug_piccolo2d_1_3;
 
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Observable;
+import java.util.Observer;
 
 import javax.swing.*;
-import javax.swing.border.LineBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
-import edu.umd.cs.piccolo.PCanvas;
+import edu.colorado.phet.common.piccolophet.PhetPCanvas;
+import edu.colorado.phet.common.piccolophet.PhetPNode;
+import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolox.pswing.PSwing;
-import edu.umd.cs.piccolox.pswing.PSwingCanvas;
 
 /**
- * See #2140
+ * See PhET Unfuddle #2140
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
 public class DebugPSwingComponentAdapter extends JFrame {
     
     public DebugPSwingComponentAdapter() {
-        setResizable( false );
-        setSize( new Dimension( 400, 400 ) );
+        setSize( new Dimension( 1024, 768 ) );
+        
+        // model
+        Light model = new Light();
         
         // canvas
-        final PCanvas canvas = new PSwingCanvas();
-        canvas.setBorder( new LineBorder( Color.BLACK ) );
+        final PhetPCanvas canvas = new PhetPCanvas( new Dimension( 750, 750 ) );
         canvas.removeInputEventListener( canvas.getZoomEventHandler() );
         canvas.removeInputEventListener( canvas.getPanEventHandler() );
         setContentPane( canvas );
         
-        // choice panel, with two radio buttons
-        JRadioButton radioButton1 = new JRadioButton( "choice1 " );
-        final JRadioButton radioButton2 = new JRadioButton( "choice2 " );
-        ButtonGroup group = new ButtonGroup();
-        group.add( radioButton1 );
-        group.add( radioButton2 );
-        JPanel choicePanel = new JPanel();
-        choicePanel.add( radioButton1 );
-        choicePanel.add( radioButton2 );
+        // root node
+        PNode rootNode = new PNode();
+        canvas.addWorldChild( rootNode );
+        rootNode.setOffset( 100, 100 );
         
-        // PSwing wrapper for choice panel
-        PSwing choicePanelNode = new PSwing( choicePanel );
-        canvas.getLayer().addChild( choicePanelNode );
+        // control panel node
+        LightControlPanel gunControlPanel = new LightControlPanel( model );
+        rootNode.addChild( gunControlPanel );
         
-        // a check box related to choice2
-        JCheckBox checkBox = new JCheckBox( "option related to choice2" );
-        
-        // PSwing wrapper for check box
-        final PSwing checkBoxNode = new PSwing( checkBox ) {
-            @Override
-            public void setVisible( boolean b ) {
-                System.out.println( "checkBoxNode.setVisible " + b );
-                super.setVisible( b );
-            }
-        };
-        canvas.getLayer().addChild( checkBoxNode );
-        
-        // event handling
-        ActionListener buttonListener = new ActionListener() {
-            public void actionPerformed( ActionEvent e ) {
-                checkBoxNode.setVisible( radioButton2.isSelected() );
-            }
-        };
-        radioButton1.addActionListener( buttonListener );
-        radioButton2.addActionListener( buttonListener );
-        
-        // layout
-        choicePanelNode.setOffset( 50, 50 );
-        checkBoxNode.setOffset( 50, 100 );
-        
-        // initial state
-        radioButton1.setSelected( true );
-        checkBoxNode.setVisible( radioButton2.isSelected() );
+        // ...called at startup, as in hydrogen-atom sim
+        gunControlPanel.setTransitionWavelengths( null );
     }
     
-    public static void main( String[] args ) {
+    private enum LightType { WHITE, MONOCHROMATIC };
+    
+    /*
+     * Model, notifies observers when light type changes.
+     */
+    private static class Light extends Observable {
+        
+        public static final String PROPERTY_LIGHT_TYPE = "lightType";
+        
+        private LightType lightType;
+        
+        public Light() {
+            this.lightType = LightType.MONOCHROMATIC;
+        }
+        
+        public LightType getLightType() {
+            return lightType;
+        }
+        
+        public void setLightType( LightType lightType ) {
+            if ( lightType != this.lightType ) {
+                this.lightType = lightType;
+                notifyObservers( PROPERTY_LIGHT_TYPE );
+            }
+        }
+        
+        public boolean isWhiteLightType() {
+            return ( lightType == LightType.WHITE );
+        }
+        
+        public boolean isMonochromaticLightType() {
+            return ( lightType == LightType.MONOCHROMATIC );   
+        }
+        
+        @Override
+        public void notifyObservers() {
+            setChanged();
+            super.notifyObservers();
+            clearChanged();
+        }
+
+        @Override
+        public void notifyObservers( Object arg ) {
+            setChanged();
+            super.notifyObservers( arg );
+            clearChanged();
+        }
+    }
+    
+    /*
+     * Panel for selecting the light type via radio buttons.
+     */
+    private static class LightTypePanel extends JPanel {
+        
+        private final JRadioButton whiteButton, monochromaticButton;
+        private final ArrayList<ChangeListener> listeners;
+        
+        public LightTypePanel() {
+            
+            // buttons
+            whiteButton = new JRadioButton( "white" );
+            monochromaticButton = new JRadioButton( "monochromatic" );
+            ButtonGroup buttonGroup = new ButtonGroup();
+            buttonGroup.add( whiteButton );
+            buttonGroup.add( monochromaticButton );
+            add( whiteButton );
+            add( monochromaticButton );
+            
+            // event handling
+            listeners = new ArrayList<ChangeListener>();
+            whiteButton.addChangeListener( new ChangeListener() {
+                public void stateChanged( ChangeEvent e ) {
+                    if ( whiteButton.isSelected() ) {
+                        fireStateChanged();
+                    }
+                }      
+            });
+            monochromaticButton.addChangeListener( new ChangeListener() {
+                public void stateChanged( ChangeEvent e ) {
+                    if ( monochromaticButton.isSelected() ) {
+                        fireStateChanged();
+                    }
+                }
+            } );
+            
+            // Default state
+            setWhiteSelected( true );
+        }
+        
+        public void setWhiteSelected( boolean selected ) {
+            whiteButton.setSelected( selected );
+            monochromaticButton.setSelected( !selected );
+        }
+        
+        public boolean isWhiteSelected() {
+            return whiteButton.isSelected();
+        }
+        
+        public void setMonochromaticSelected( boolean selected ) {
+            setWhiteSelected( !selected );
+        }
+        
+        public boolean isMonochromaticSelected() {
+            return !isWhiteSelected();
+        }
+        
+        public void addChangeListener( ChangeListener listener ) {
+            listeners.add( listener );
+        }
+
+        public void removeChangeListener( ChangeListener listener ) {
+            listeners.remove( listener );
+        }
+
+        private void fireStateChanged() {
+            ChangeEvent event = new ChangeEvent( this ); 
+            for ( ChangeListener listener : listeners ) {
+                listener.stateChanged( event );
+            }
+        }
+    }
+    
+    /*
+     * Node that provides controls for the model.
+     * Select the light type and whether a monochromatic feature is enabled.
+     */
+    public class LightControlPanel extends PhetPNode implements Observer {
+
+        private final Light model;
+        private final LightTypePanel lightTypePanel;
+        private final JCheckBox checkBox;
+        private final PSwing checkBoxNode;
+        private boolean checkBoxFeatureEnabled;
+        
+        public LightControlPanel( Light model ) {
+            super();
+            
+            this.model = model;
+            checkBoxFeatureEnabled = false;
+            
+            lightTypePanel = new LightTypePanel();
+            checkBox = new JCheckBox( "some monochromatic feature" ) {
+                @Override
+                public void setVisible( boolean b ) {
+                    System.out.println( "transitionMarksControl.setVisible " + b );
+                    super.setVisible( b );
+                }
+            };
+
+            // Wrappers for Swing components
+            PSwing lightTypeControlWrapper = new PSwing( lightTypePanel );
+            checkBoxNode = new PSwing( checkBox );
+
+            // Layering, back to front
+            addChild( lightTypeControlWrapper );
+            addChild( checkBoxNode );
+
+            // Positioning
+            lightTypeControlWrapper.setOffset( 0, 0 );
+            checkBoxNode.setOffset( 0, lightTypeControlWrapper.getFullBoundsReference().getMaxY() + 5 );
+
+            // Event handling
+            lightTypePanel.addChangeListener( new ChangeListener() {
+                public void stateChanged( ChangeEvent event ) {
+                    handleLightTypeChange();
+                }
+            } );
+
+            // Sync with model
+            updateAll();
+        }
+        
+        public void setTransitionWavelengths( double[] transitionWavelengths ) {
+            System.out.println( "GunControlPanel.setTransitionWavelengths" );
+            if ( lightTypePanel.isMonochromaticSelected() ) {
+                checkBoxNode.setVisible( true );
+            }
+            else {
+                checkBoxNode.setVisible( false );
+            }
+        }
+        
+        private void handleLightTypeChange() {
+            System.out.println( "GunControlPanel.handleLightTypeChanged" );
+            if ( lightTypePanel.isMonochromaticSelected() ) {
+                checkBoxNode.setVisible( checkBoxFeatureEnabled );
+                model.setLightType( LightType.MONOCHROMATIC );
+            }
+            else {
+                checkBoxNode.setVisible( false );
+                model.setLightType( LightType.WHITE );
+            }
+        }
+        
+        public void update( Observable o, Object arg ) {
+            System.out.println( "GunControlPanel.update" );
+            if ( o == model ) {
+                if ( arg == Light.PROPERTY_LIGHT_TYPE ) {
+                    lightTypePanel.setMonochromaticSelected( model.isMonochromaticLightType() );
+                }
+            }
+        }
+        
+        private void updateAll() {
+            System.out.println( "GunControlPanel.updateAll" );
+            if ( model.isMonochromaticLightType() ) {
+                lightTypePanel.setMonochromaticSelected( true );
+            }
+            else {
+                lightTypePanel.setWhiteSelected( true );
+            }
+        }
+    }
+    
+    /*
+     * Starts the application.
+     */
+    private static void startApplication() {
         JFrame frame = new DebugPSwingComponentAdapter();
         frame.setDefaultCloseOperation( WindowConstants.EXIT_ON_CLOSE );
         // center on the screen
@@ -85,5 +274,25 @@ public class DebugPSwingComponentAdapter extends JFrame {
         int y = (int) ( tk.getScreenSize().getHeight() / 2 - frame.getHeight() / 2 );
         frame.setLocation( x, y );
         frame.setVisible( true );
+    }
+    
+    /**
+     * Executed synchronously on the AWT event dispatching thread, just like PhET applications.
+     * @param args
+     */
+    public static void main( String[] args ) {
+        try {
+            SwingUtilities.invokeAndWait( new Runnable() {
+                public void run() {
+                    startApplication();
+                }
+            } );
+        }
+        catch ( InterruptedException e ) {
+            e.printStackTrace();
+        }
+        catch ( InvocationTargetException e ) {
+            e.printStackTrace();
+        }
     }
 }
