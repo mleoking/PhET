0f740d8e733bc7590df68a6b2e3379ab29de84c2
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-02-01 22:47:37 +0000

    Added n4 and m3h to the parameters displayed by this control, finished adding the ability to capture and display the internal state data.

--- simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/developer/HodgkinHuxleyInternalDynamicsDlg.java
+++ simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/developer/HodgkinHuxleyInternalDynamicsDlg.java
@@ -2,13 +2,18 @@
 
 package edu.colorado.phet.neuron.developer;
 
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 import edu.colorado.phet.common.phetcommon.application.PaintImmediateDialog;
@@ -37,14 +42,20 @@ public class HodgkinHuxleyInternalDynamicsDlg extends PaintImmediateDialog{
 	private JTextField potassiumChannelCurrentValue = new JTextField("00.0 mA");
 	private JLabel leakChannelCurrentLabel = new JLabel("Leak Channel: ");
 	private JTextField leakChannelCurrentValue = new JTextField("00.0 mA");
+	private JLabel n4Label = new JLabel("n4: ");
+	private JTextField n4Value = new JTextField("0.00");
+	private JLabel m3hLabel = new JLabel("m3h: ");
+	private JTextField m3hValue = new JTextField("0.00");
 	private JButton captureButton = new JButton();
 	private boolean capturing = false;
+	
+	private ArrayList<HhDataCaptureEntry> captureData = new ArrayList<HhDataCaptureEntry>();
 	
 	public HodgkinHuxleyInternalDynamicsDlg(IClock clock, HodgkinHuxleyModel hhModel) {
 		hodgkinHuxleyModel = hhModel;
 		
 		setTitle("Hodgkin-Huxley Model Internal Dynamics");
-		setLayout(new GridLayout(5,2));
+		setLayout(new GridLayout(7,2));
 		
 		// Update ourself on every clock tick that we are visible.
 		clock.addClockListener(new ClockAdapter(){
@@ -79,10 +90,26 @@ public class HodgkinHuxleyInternalDynamicsDlg extends PaintImmediateDialog{
 		leakChannelCurrentValue.setEditable(false);
 		add(leakChannelCurrentValue);
 		
+		n4Label.setFont(DISPLAY_FONT);
+		add(n4Label);
+		n4Value.setFont(DISPLAY_FONT);
+		n4Value.setEditable(false);
+		add(n4Value);
+		
+		m3hLabel.setFont(DISPLAY_FONT);
+		add(m3hLabel);
+		m3hValue.setFont(DISPLAY_FONT);
+		m3hValue.setEditable(false);
+		add(m3hValue);
+		
 		// Add a button that allows the user to capture the values.
 		add(captureButton);
 		captureButton.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e) {
+				if (capturing){
+					displayCapturedData();
+					captureData.clear();
+				}
 				capturing = !capturing;
 				updateCaptureButtonState();
 			}
@@ -94,10 +121,22 @@ public class HodgkinHuxleyInternalDynamicsDlg extends PaintImmediateDialog{
 	}
 	
 	private void update(){
-		voltageText.setText(String.format("%.2f mV", hodgkinHuxleyModel.getMembraneVoltage() * 1000));
-		sodiumChannelCurrentValue.setText(String.format("%.2f mA", hodgkinHuxleyModel.get_na_current()));
-		potassiumChannelCurrentValue.setText(String.format("%.2f mA", hodgkinHuxleyModel.get_k_current()));
-		leakChannelCurrentValue.setText(String.format("%.2f mA", hodgkinHuxleyModel.get_l_current()));
+		double membraneVoltage = hodgkinHuxleyModel.getMembraneVoltage() * 1000;
+		double currentNa = hodgkinHuxleyModel.get_na_current();
+		double currentK = hodgkinHuxleyModel.get_k_current();
+		double currentLeak = hodgkinHuxleyModel.get_l_current();
+		double n4 = hodgkinHuxleyModel.get_n4();
+		double m3h = hodgkinHuxleyModel.get_m3h();
+		voltageText.setText(String.format("%.2f mV", membraneVoltage));
+		sodiumChannelCurrentValue.setText(String.format("%.2f mA", currentNa));
+		potassiumChannelCurrentValue.setText(String.format("%.2f mA", currentK));
+		leakChannelCurrentValue.setText(String.format("%.2f mA", currentLeak));
+		n4Value.setText(String.format("%.5f", n4));
+		m3hValue.setText(String.format("%.5f", m3h));
+		
+		if (capturing){
+			captureData.add(new HhDataCaptureEntry(membraneVoltage, currentNa, currentK, currentLeak, n4, m3h));
+		}
 	}
 	
 	private void updateCaptureButtonState(){
@@ -106,6 +145,63 @@ public class HodgkinHuxleyInternalDynamicsDlg extends PaintImmediateDialog{
 		}
 		else{
 			captureButton.setText("Stop Capturing");
+		}
+	}
+	
+	private void displayCapturedData(){
+		
+		JDialog dialog = new JDialog(this, true);
+		JTextArea textDisplay = new JTextArea(captureData.size() + 1, 80);
+		textDisplay.setEditable(false);
+		textDisplay.append(HhDataCaptureEntry.getHeader());
+		for (HhDataCaptureEntry dataEntry : captureData){
+			textDisplay.append(dataEntry.toString());
+		}
+		textDisplay.setCaretPosition(0);
+		JScrollPane scrollableTextDisplay = new JScrollPane(textDisplay);
+		dialog.add(scrollableTextDisplay);
+		dialog.setSize(new Dimension(500, 300));
+		dialog.setLocationRelativeTo(this); 
+		dialog.setVisible(true);
+		
+	}
+	
+	private static class HhDataCaptureEntry {
+		
+		private final double membraneVoltage;
+		private final double currentNa;
+		private final double currentK;
+		private final double currentLeak;
+		private final double n4;
+		private final double m3h;
+		
+		public HhDataCaptureEntry(double membraneVoltage, double currentNa,
+				double currentK, double currentLeak, double n4, double m3h) {
+			super();
+			this.membraneVoltage = membraneVoltage;
+			this.currentNa = currentNa;
+			this.currentK = currentK;
+			this.currentLeak = currentLeak;
+			this.n4 = n4;
+			this.m3h = m3h;
+		}
+		
+		protected static String getHeader(){
+			return "membraneVoltage, currentNa, currentK, currentLeak, n4, m3h\n";
+		}
+		
+		@Override
+		public String toString(){
+			String retVal = new String();
+			retVal += String.format("%.2f, ", membraneVoltage);
+			retVal += String.format("%.2f, ", currentNa);
+			retVal += String.format("%.2f, ", currentK);
+			retVal += String.format("%.2f, ", currentLeak);
+			retVal += String.format("%.5f, ", n4);
+			retVal += String.format("%.5f, ", m3h);
+			retVal += "\n";
+			
+			return retVal;
 		}
 	}
 }
