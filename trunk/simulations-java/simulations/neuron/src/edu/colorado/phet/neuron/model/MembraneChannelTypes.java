080eed1a17cf75412667afd5096b4b6ba6d33723
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-02-10 23:42:42 +0000

    Added gated channels to the list of possible types.

--- trunk/simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/model/MembraneChannelTypes.java
+++ trunk/simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/model/MembraneChannelTypes.java
@@ -9,5 +9,7 @@ package edu.colorado.phet.neuron.model;
  */
 public enum MembraneChannelTypes {
 	SODIUM_LEAKAGE_CHANNEL,
-	POTASSIUM_LEAKAGE_CHANNEL
+	SODIUM_GATED_CHANNEL,
+	POTASSIUM_LEAKAGE_CHANNEL,
+	POTASSIUM_GATED_CHANNEL
 }
