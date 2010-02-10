e059fe3d65b81038f727ad5b279db02ed064e140
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-02-10 22:54:34 +0000

    Initial submission of the gated channels for sodium and potassium.  Some refactoring is needed to clean up the location of some functionality within the class hierarchy.

---
+++ simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/model/SodiumGatedChannel.java
@@ -0,0 +1,48 @@
+/* Copyright 2009, University of Colorado */
+
+package edu.colorado.phet.neuron.model;
+
+import java.awt.Color;
+
+import edu.colorado.phet.common.phetcommon.view.util.ColorUtils;
+import edu.colorado.phet.neuron.NeuronConstants;
+
+public class SodiumGatedChannel extends AbstractGatedChannel {
+
+    //----------------------------------------------------------------------------
+    // Class Data
+    //----------------------------------------------------------------------------
+	
+	private static final double CHANNEL_HEIGHT = AxonMembrane.MEMBRANE_THICKNESS * 1.4; // In nanometers.
+	private static final double CHANNEL_WIDTH = AxonMembrane.MEMBRANE_THICKNESS * 0.70; // In nanometers.
+	
+    //----------------------------------------------------------------------------
+    // Instance Data
+    //----------------------------------------------------------------------------
+	
+    //----------------------------------------------------------------------------
+    // Constructor
+    //----------------------------------------------------------------------------
+	public SodiumGatedChannel() {
+		super(CHANNEL_WIDTH, CHANNEL_HEIGHT, ParticleType.SODIUM_ION);
+	}
+
+    //----------------------------------------------------------------------------
+    // Methods
+    //----------------------------------------------------------------------------
+	
+	@Override
+	public Color getChannelColor() {
+		return ColorUtils.darkerColor(NeuronConstants.SODIUM_COLOR, 0.2);
+	}
+
+	@Override
+	public Color getEdgeColor() {
+		return NeuronConstants.SODIUM_COLOR;
+	}
+
+	@Override
+	public MembraneChannelTypes getChannelType() {
+		return MembraneChannelTypes.SODIUM_LEAKAGE_CHANNEL;
+	}
+}
