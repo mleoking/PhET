918457ffdd82973fa528162049c745418740c72e
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-02-11 01:06:49 +0000

    Wicket-website: basic contribution browsing enabled

--- web/wicket-website/src/java/edu/colorado/phet/website/panels/ContributionBrowsePanel.java
+++ web/wicket-website/src/java/edu/colorado/phet/website/panels/ContributionBrowsePanel.java
@@ -1,26 +1,59 @@
 package edu.colorado.phet.website.panels;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
+import org.hibernate.Session;
 
 import edu.colorado.phet.website.content.ContributionPage;
+import edu.colorado.phet.website.content.SimulationPage;
+import edu.colorado.phet.website.data.LocalizedSimulation;
+import edu.colorado.phet.website.data.Simulation;
 import edu.colorado.phet.website.data.contribution.Contribution;
+import edu.colorado.phet.website.data.contribution.ContributionLevel;
+import edu.colorado.phet.website.data.contribution.ContributionType;
+import edu.colorado.phet.website.translation.PhetLocalizer;
+import edu.colorado.phet.website.util.HibernateTask;
+import edu.colorado.phet.website.util.HibernateUtils;
 import edu.colorado.phet.website.util.PageContext;
 
 public class ContributionBrowsePanel extends PhetPanel {
 
-    private final List<Contribution> contributions;
+    private final List<Contribution> newContributions;
 
-    public ContributionBrowsePanel( String id, final PageContext context, List<Contribution> contributions ) {
+    public ContributionBrowsePanel( String id, final PageContext context, final List<Contribution> contributions ) {
         super( id, context );
 
-        this.contributions = contributions;
+        newContributions = new LinkedList<Contribution>();
 
-        // TODO: is localization necessary?
+        // fill our new contributions list, and make sure to load all of the data that is lazy that we need
+        HibernateUtils.wrapTransaction( getHibernateSession(), new HibernateTask() {
+            public boolean run( Session session ) {
+                for ( Contribution oldContrib : contributions ) {
+                    Contribution contribution = (Contribution) session.load( Contribution.class, oldContrib.getId() );
+
+                    // we need to read levels
+                    contribution.getLevels();
+
+                    // we also need to read the types
+                    contribution.getTypes();
+
+                    for ( Object o : contribution.getSimulations() ) {
+                        Simulation sim = (Simulation) o;
+
+                        // we need to be able to read these to determine the localized simulation title later
+                        sim.getLocalizedSimulations();
+                    }
+                }
+                return true;
+            }
+        } );
+
+        // TODO: localize
 
         add( new ListView( "contributions", contributions ) {
             protected void populateItem( ListItem item ) {
@@ -29,83 +62,90 @@ public class ContributionBrowsePanel extends PhetPanel {
                 link.add( new Label( "contribution-title", contribution.getTitle() ) );
                 item.add( link );
                 item.add( new Label( "contribution-authors", contribution.getAuthors() ) );
+
+                Label levelLabel = new Label( "contribution-level", getLevelString( contribution ) );
+                levelLabel.setEscapeModelStrings( false );
+                item.add( levelLabel );
+
+                Label typeLabel = new Label( "contribution-type", getTypeString( contribution ) );
+                typeLabel.setEscapeModelStrings( false );
+                item.add( typeLabel );
+
+                // get a sorted (for the locale) list of simulations
+                List<LocalizedSimulation> lsims = new LinkedList<LocalizedSimulation>();
+                for ( Object o : contribution.getSimulations() ) {
+                    lsims.add( ( (Simulation) o ).getBestLocalizedSimulation( getLocale() ) );
+                }
+                HibernateUtils.orderSimulations( lsims, getLocale() );
+
+                // add the list in
+                item.add( new ListView( "contribution-simulations", lsims ) {
+                    protected void populateItem( ListItem item ) {
+                        LocalizedSimulation lsim = (LocalizedSimulation) item.getModel().getObject();
+                        Link link = SimulationPage.createLink( "simulation-link", context, lsim );
+                        link.add( new Label( "simulation-title", lsim.getTitle() ) );
+                        item.add( link );
+                    }
+                } );
+
+                // TODO: localize
+                item.add( new Label( "contribution-updated", contribution.getDateUpdated().toString() ) );
             }
         } );
 
-//        IDataProvider simData = new SimulationDataProvider( simulations );
-//        GridView gridView = new GridView( "rows", simData ) {
-//            @Override
-//            protected void populateEmptyItem( Item item ) {
-//                item.setVisible( false );
-//            }
-//
-//            @Override
-//            protected void populateItem( Item item ) {
-//                LocalizedSimulation simulation = (LocalizedSimulation) item.getModelObject();
-//                PhetLink link = SimulationPage.createLink( "simulation-link", context, simulation );
-//                link.add( new Label( "title", simulation.getTitle() ) );
-//                if ( !simulation.getLocale().equals( context.getLocale() ) ) {
-//                    // sim isn't translated
-//                    link.add( new AttributeAppender( "class", new Model( "untranslated-sim" ), " " ) );
-//                }
-//                String alt;
-//                try {
-//                    alt = MessageFormat.format( "Screenshot of the simulation {0}", encode( simulation.getTitle() ) );
-//                }
-//                catch( RuntimeException e ) {
-//                    e.printStackTrace();
-//                    alt = "Screenshot of the simulation";
-//                }
-//                link.add( new StaticImage( "thumbnail", simulation.getSimulation().getThumbnailUrl(), alt ) );
-//                item.add( link );
-//            }
-//        };
-//        gridView.setColumns( 3 );
-//        add( gridView );
-//
-//        add( HeaderContributor.forCss( "/css/simulation-display-v1.css" ) );
-
     }
 
-//    private static class SimulationDataProvider implements IDataProvider {
-//        private List<LocalizedSimulation> simulations;
-//
-//        private SimulationDataProvider( List<LocalizedSimulation> simulations ) {
-//            this.simulations = simulations;
-//        }
-//
-//
-//        public Iterator iterator( int first, int count ) {
-//            int endIndex = first + count;
-//            if ( endIndex > simulations.size() ) {
-//                endIndex = simulations.size();
-//            }
-//            return simulations.subList( first, endIndex ).iterator();
-//        }
-//
-//        public int size() {
-//            return simulations.size();
-//        }
-//
-//        public IModel model( final Object o ) {
-//            return new IModel() {
-//                public Object getObject() {
-//                    return o;
-//                }
-//
-//                public void setObject( Object o ) {
-//
-//                }
-//
-//                public void detach() {
-//
-//                }
-//            };
-//        }
-//
-//        public void detach() {
-//
-//        }
-//    }
+    public String getLevelString( Contribution contribution ) {
+        PhetLocalizer localizer = getPhetLocalizer();
+        String ret = "";
+        boolean started = false;
+        for ( Object o : contribution.getLevels() ) {
+            if ( started ) {
+                ret += "<br/>";
+            }
+            started = true;
+
+            ContributionLevel level = (ContributionLevel) o;
+            String key = level.getLevel().getAbbreviatedTranslationKey();
+            ret += localizer.getString( key, this );
+        }
+        return ret;
+    }
+
+    public String getTypeString( Contribution contribution ) {
+        PhetLocalizer localizer = getPhetLocalizer();
+        String ret = "";
+        boolean started = false;
+        for ( Object o : contribution.getTypes() ) {
+            if ( started ) {
+                ret += "<br/>";
+            }
+            started = true;
+
+            ContributionType type = (ContributionType) o;
+            String key = type.getType().getAbbreviatedTranslationKey();
+            ret += localizer.getString( key, this );
+        }
+        return ret;
+    }
+
+    public String getSimulationString( Contribution contribution ) {
+        PhetLocalizer localizer = getPhetLocalizer();
+        String ret = "";
+        boolean started = false;
+        for ( Object o : contribution.getSimulations() ) {
+            if ( started ) {
+                ret += "<br/>";
+            }
+            started = true;
+
+            Simulation sim = (Simulation) o;
+            LocalizedSimulation lsim = sim.getBestLocalizedSimulation( getLocale() );
+
+            // translated title
+            ret += lsim.getTitle();
+        }
+        return ret;
+    }
 
 }
