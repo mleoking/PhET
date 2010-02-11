36a60af61a54bd7d911bf5f6cb986a933515ddc5
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-02-11 20:31:56 +0000

    Added a node for representing the capture zones that are generally associated with channels.

---
+++ simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/view/CaptureZoneNode.java
@@ -0,0 +1,28 @@
+package edu.colorado.phet.neuron.view;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Stroke;
+
+import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform2D;
+import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
+import edu.colorado.phet.neuron.model.AbstractCaptureZone;
+import edu.umd.cs.piccolo.PNode;
+
+/**
+ * Class that represents a capture zone in the view, which is essentially the
+ * area from which a channel can draw in a particle.  Note that this exists
+ * primarily for debug, and will not be visible in the finished product.
+ */
+public class CaptureZoneNode extends PNode {
+	
+	private static final float STROKE_WIDTH = 3;
+	private static final Color STROKE_COLOR = Color.PINK;
+	private static final Stroke STROKE = new BasicStroke(STROKE_WIDTH);
+	
+    public CaptureZoneNode( AbstractCaptureZone captureZone, ModelViewTransform2D mvt ) {
+        // Create the shape that represents this capture zone.
+        PhetPPath representation = new PhetPPath(mvt.createTransformedShape(captureZone.getShape()), STROKE, STROKE_COLOR);
+		addChild( representation );
+	}
+}
