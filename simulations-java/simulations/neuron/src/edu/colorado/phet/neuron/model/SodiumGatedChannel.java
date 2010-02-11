369c781fbf231634ab091429ff8a1d5a7379c959
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-02-11 17:34:27 +0000

    Limited the resolution of the updates for "openness", since the HH model was oscillating so much that it was causing continuous updates.

--- simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/model/SodiumGatedChannel.java
+++ simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/model/SodiumGatedChannel.java
@@ -6,6 +6,7 @@ import java.awt.Color;
 
 import edu.colorado.phet.common.phetcommon.view.util.ColorUtils;
 import edu.colorado.phet.neuron.NeuronConstants;
+import edu.colorado.phet.neuron.utils.MathUtils;
 
 public class SodiumGatedChannel extends AbstractGatedChannel {
 
@@ -53,6 +54,16 @@ public class SodiumGatedChannel extends AbstractGatedChannel {
 		super.stepInTime(dt);
 		// Update the openness factor based on the state of the HH model.
 		// This is very specific to the model and the type of channel.
-		setOpenness(Math.min(Math.abs(hodgekinHodgkinHuxleyModel.get_na_current())/100, 1));
+		double openness = Math.min(Math.abs(hodgekinHodgkinHuxleyModel.get_na_current())/290, 1);
+		if (openness > 0 && openness < 1){
+			// Trim off some digits, otherwise we are continuously making
+			// tiny changes to this value due to internal gyrations of the
+			// HH model.
+			openness = MathUtils.round(openness, 2);
+		}
+		if (openness != getOpenness()){
+			System.out.println("Current = " + hodgekinHodgkinHuxleyModel.get_na_current() + ", openness = " + openness);
+			setOpenness(openness);
+		}
 	}
 }
