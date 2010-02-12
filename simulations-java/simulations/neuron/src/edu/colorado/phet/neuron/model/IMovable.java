0f968e906bbe55efa9f72231e5dad8aae00f11fc
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-02-12 21:45:05 +0000

    Initial submission of an interface for model elements that can be moved.

---
+++ simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/model/IMovable.java
@@ -0,0 +1,22 @@
+/* Copyright 2010, University of Colorado */
+
+package edu.colorado.phet.neuron.model;
+
+import java.awt.geom.Point2D;
+
+/**
+ * Interface implemented by model elements that can be moved.  This was
+ * created originally for use with motion strategies, but may have other
+ * uses.
+ * 
+ * @author John Blanco
+ */
+public interface IMovable {
+
+	/**
+	 * Set the position of the model element.
+	 *  
+	 * @param newPosition
+	 */
+	void setPosition(Point2D newPosition);
+}
