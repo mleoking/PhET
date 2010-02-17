79cb2d69abce2b48ad0bdd2de2bca63baac7b1a2
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-02-17 19:59:13 +0000

    refinements

--- simulations-java/simulations/reactants-products-and-leftovers/src/edu/colorado/phet/reactantsproductsandleftovers/view/game/GameRewardNode.java
+++ simulations-java/simulations/reactants-products-and-leftovers/src/edu/colorado/phet/reactantsproductsandleftovers/view/game/GameRewardNode.java
@@ -1,12 +1,17 @@
+/* Copyright 2010, University of Colorado */
+
 package edu.colorado.phet.reactantsproductsandleftovers.view.game;
 
 import java.awt.BorderLayout;
-import java.awt.Dimension;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.geom.Dimension2D;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
 
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.WindowConstants;
@@ -15,14 +20,13 @@ import javax.swing.border.TitledBorder;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
+import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
 import edu.colorado.phet.common.phetcommon.model.clock.ClockAdapter;
 import edu.colorado.phet.common.phetcommon.model.clock.ClockEvent;
 import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
-import edu.colorado.phet.common.phetcommon.model.clock.IClock;
 import edu.colorado.phet.common.phetcommon.util.IntegerRange;
 import edu.colorado.phet.common.phetcommon.view.controls.valuecontrol.LinearValueControl;
 import edu.colorado.phet.common.phetcommon.view.util.EasyGridBagLayout;
-import edu.colorado.phet.common.phetcommon.view.util.SwingUtils;
 import edu.colorado.phet.common.piccolophet.PhetPCanvas;
 import edu.colorado.phet.common.piccolophet.PhetPNode;
 import edu.colorado.phet.reactantsproductsandleftovers.RPALConstants;
@@ -33,58 +37,102 @@ import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.nodes.PImage;
 import edu.umd.cs.piccolo.util.PBounds;
 
-/**
- * The reward that is displayed when the game is completed with a perfect score.
- *
+/*
+ * * The reward that is displayed when the game is completed with a perfect
+ * score.
+ * 
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
 public class GameRewardNode extends PhetPNode {
     
-    private final Image[] images;
+    private static final int DEFAULT_CLOCK_DELAY = 60; // ms
+
+    private final ConstantDtClock clock;
+    private final ArrayList<Image> images;
+    private final Image faceImage;
+    private final Image sandwichImage;
+
     private int population;
     private int motionDelta;
-    private boolean playing;
-    
-    public GameRewardNode( PBounds bounds, IClock clock, Image[] images, int population, int motionDelta ) {
+
+    public GameRewardNode( PBounds bounds, int population, int motionDelta ) {
         super();
-        
-        this.images = images;
+
         this.population = population;
         this.motionDelta = motionDelta;
-        
-        // update the animation when the clock ticks
+
+        this.clock = new ConstantDtClock( DEFAULT_CLOCK_DELAY, 1 );
+        this.clock.pause();
+
+        // images list, includes everything by default
+        {
+            this.images = new ArrayList<Image>();
+
+            // molecules
+            for ( Image image : RPALImages.ALL_MOLECULES ) {
+                images.add( image );
+            }
+
+            // smiley face image
+            FaceNode faceNode = new FaceNode();
+            faceNode.scale( 0.25 );
+            faceImage = faceNode.toImage();
+            images.add( faceImage );
+
+            // sandwiches
+            sandwichImage = SandwichImageFactory.createImage( new SandwichShopModel( 2, 1, 1 /* bread, meat, cheese */ ) );
+            images.add( sandwichImage );
+            for ( Image image : RPALImages.ALL_SANDWICHES ) {
+                images.add( image );
+            }
+        }
+
         clock.addClockListener( new ClockAdapter() {
             @Override
+            // update the animation when the clock ticks
             public void clockTicked( ClockEvent clockEvent ) {
                 step();
             }
         } );
-        
+
         // initial state
-        playing = false;
         setBounds( bounds );
     }
-    
+
     public boolean setBounds( Rectangle2D bounds ) {
         if ( bounds.isEmpty() ) {
             throw new IllegalArgumentException( "bounds are empty" );
         }
-        boolean b = super.setBounds( bounds );
-        removeAllChildren();
-        updateNodes();
-        return b;
+        boolean boundsChanged = false;
+        if ( !bounds.equals( getBounds() ) ) {
+            boundsChanged = super.setBounds( bounds );
+            updateImages( true /* removeImages */);
+        }
+        return boundsChanged;
     }
-    
+
+    public void setClockDelay( int delay ) {
+        clock.setDelay( delay );
+    }
+
+    public int getClockDelay() {
+        return clock.getDelay();
+    }
+
     public void setPopulation( int population ) {
         if ( !( population > 0 ) ) {
             throw new IllegalArgumentException( "population must be > 0: " + population );
         }
         if ( population != this.population ) {
             this.population = population;
-            updateNodes();
+            updateImages( false /* removeImages */);
         }
     }
-    
+
+    public int getPopulation() {
+        return population;
+    }
+
     public void setMotionDelta( int motionDelta ) {
         if ( !( motionDelta > 0 ) ) {
             throw new IllegalArgumentException( "motionDelta must be > 0: " + motionDelta );
@@ -93,8 +141,53 @@ public class GameRewardNode extends PhetPNode {
             this.motionDelta = motionDelta;
         }
     }
-    
-    private void updateNodes() {
+
+    public int getMotionDelta() {
+        return motionDelta;
+    }
+
+    public void setSmileyFacesVisible( boolean visible ) {
+        if ( visible != isSmileyFacesVisible() ) {
+            if ( visible ) {
+                images.add( faceImage );
+            }
+            else {
+                images.remove( faceImage );
+            }
+            updateImages( true /* removeImages */);
+        }
+    }
+
+    public boolean isSmileyFacesVisible() {
+        return images.contains( faceImage );
+    }
+
+    public void setSandwichesVisible( boolean visible ) {
+        if ( visible != isSandwichesVisible() ) {
+            if ( visible ) {
+                images.add( sandwichImage );
+                for ( Image image : RPALImages.ALL_SANDWICHES ) {
+                    images.add( image );
+                }
+            }
+            else {
+                images.remove( sandwichImage );
+                for ( Image image : RPALImages.ALL_SANDWICHES ) {
+                    images.remove( image );
+                }
+            }
+            updateImages( true /* removeImages */);
+        }
+    }
+
+    public boolean isSandwichesVisible() {
+        return images.contains( sandwichImage );
+    }
+
+    private void updateImages( boolean removeImages ) {
+        if ( removeImages ) {
+            removeAllChildren(); // assume that this node has only images as children
+        }
         if ( getChildrenCount() > population ) {
             // remove some nodes
             while ( getChildrenCount() > population ) {
@@ -107,43 +200,43 @@ public class GameRewardNode extends PhetPNode {
                 addRandomNode();
             }
         }
-        for ( int i = 0; i < population; i++ ) {
-            addRandomNode();
-        }
     }
-    
+
     private void addRandomNode() {
-        PBounds bounds = getBoundsReference();
-        PImage imageNode = new PImage( images[ (int)( Math.random() * images.length ) ] );
+
+        // choose a random image
+        int index = (int) ( Math.random() * images.size() );
+        PImage imageNode = new PImage( images.get( index ) );
         addChild( imageNode );
+
+        // set a random location within the bounds
+        PBounds bounds = getBoundsReference();
         Point2D location = getRandomPoint( bounds );
         double x = location.getX() - ( imageNode.getFullBoundsReference().getWidth() / 2 );
         double y = location.getY() - ( imageNode.getFullBoundsReference().getHeight() / 2 );
         imageNode.setOffset( x, y );
     }
-    
+
     private Point2D getRandomPoint( PBounds bounds ) {
         double x = bounds.getX() + ( Math.random() * bounds.getWidth() );
         double y = bounds.getY() + ( Math.random() * bounds.getHeight() );
         return new Point2D.Double( x, y );
     }
-    
+
     public void play() {
-        playing = true;
+        clock.start();
     }
 
     public void stop() {
-        playing = false;
+        clock.pause();
     }
-    
+
     private void step() {
-        if ( playing ) {
-            for ( int i = 0; i < getChildrenCount(); i++ ) {
-                step( getChild( i ) );
-            }
+        for ( int i = 0; i < getChildrenCount(); i++ ) {
+            step( getChild( i ) );
         }
     }
-    
+
     private void step( PNode node ) {
         PBounds bounds = getBoundsReference();
         // walk a distance in a random direction
@@ -154,38 +247,26 @@ public class GameRewardNode extends PhetPNode {
         y = Math.max( bounds.getMinY(), Math.min( y, bounds.getMaxY() ) );
         node.setOffset( x, y );
     }
-    
+
     private int getRandomDirection() {
         return ( Math.random() > 0.5 ) ? 1 : -1;
     }
 
     // test harness
     public static void main( String[] args ) {
-        
-        // smiley face
-        FaceNode faceNode = new FaceNode();
-        faceNode.scale( 0.25 );
-        final Image faceImage = faceNode.toImage();
-        
-        // sandwich
-        Image sandwichImage = SandwichImageFactory.createImage( new SandwichShopModel( 2, 1, 1 ) );
-        
+
         // parameters that control the animation
-        final Image[] images = { sandwichImage, faceImage, RPALImages.H2O, RPALImages.H2, RPALImages.O2, RPALImages.N2, RPALImages.SO3, RPALImages.PCl3, RPALImages.CS2, RPALImages.BREAD, RPALImages.MEAT, RPALImages.CHEESE };
         final IntegerRange clockDelayRange = new IntegerRange( 10, 200, 60 );
-        final IntegerRange populationRange = new IntegerRange( 50, 500, 200 );
+        final IntegerRange populationRange = new IntegerRange( 50, 1000, 500 );
         final IntegerRange motionDeltaRange = new IntegerRange( 1, 20, 5 );
 
-        final Dimension frameSize = new Dimension( 1024, 768 );
-        
-        final ConstantDtClock clock = new ConstantDtClock( clockDelayRange.getDefault(), 1 );
-        clock.start();
         PBounds bounds = new PBounds( 0, 0, 100, 100 );
-        final GameRewardNode rewardNode = new GameRewardNode( bounds, clock, images, populationRange.getDefault(), motionDeltaRange.getDefault() );
+        final GameRewardNode rewardNode = new GameRewardNode( bounds, populationRange.getDefault(), motionDeltaRange.getDefault() );
         rewardNode.play();
-        
-        final PhetPCanvas canvas = new PhetPCanvas( frameSize ) {
+
+        final PhetPCanvas canvas = new PhetPCanvas( RPALConstants.CANVAS_RENDERING_SIZE ) {
             @Override
+            // when the canvas size changes, update the reward node
             protected void updateLayout() {
                 Dimension2D worldSize = getWorldSize();
                 if ( worldSize.getWidth() > 0 && worldSize.getHeight() > 0 ) {
@@ -196,34 +277,51 @@ public class GameRewardNode extends PhetPNode {
         };
         canvas.setBackground( RPALConstants.CANVAS_BACKGROUND );
         canvas.addWorldChild( rewardNode );
-        
+
         final LinearValueControl clockDelayControl = new LinearValueControl( clockDelayRange.getMin(), clockDelayRange.getMax(), "clock speed:", "##0", "ms" );
         clockDelayControl.setToolTipText( "how frequently the simulation clock ticks" );
-        clockDelayControl.setValue( clock.getDelay() );
+        clockDelayControl.setValue( rewardNode.getClockDelay() );
         clockDelayControl.addChangeListener( new ChangeListener() {
+
             public void stateChanged( ChangeEvent e ) {
-                 clock.setDelay( (int) clockDelayControl.getValue() );
+                rewardNode.setClockDelay( (int) clockDelayControl.getValue() );
             }
-        });
-        
+        } );
+
         final LinearValueControl populationControl = new LinearValueControl( populationRange.getMin(), populationRange.getMax(), "population:", "##0", "" );
         populationControl.setValue( populationRange.getDefault() );
         populationControl.setToolTipText( "how many images are in the play area" );
         populationControl.addChangeListener( new ChangeListener() {
             public void stateChanged( ChangeEvent e ) {
-                 rewardNode.setPopulation( (int) populationControl.getValue() );
+                rewardNode.setPopulation( (int) populationControl.getValue() );
             }
-        });
-        
+        } );
+
         final LinearValueControl motionDeltaControl = new LinearValueControl( motionDeltaRange.getMin(), motionDeltaRange.getMax(), "motion delta:", "##0", "pixels" );
         motionDeltaControl.setValue( motionDeltaRange.getDefault() );
         motionDeltaControl.setToolTipText( "how far images move each time the clock ticks" );
         motionDeltaControl.addChangeListener( new ChangeListener() {
             public void stateChanged( ChangeEvent e ) {
-                 rewardNode.setMotionDelta( (int) motionDeltaControl.getValue() );
+                rewardNode.setMotionDelta( (int) motionDeltaControl.getValue() );
             }
-        });
-        
+        } );
+
+        final JCheckBox smileyFacesCheckBox = new JCheckBox( "show smiley faces" );
+        smileyFacesCheckBox.setSelected( rewardNode.isSmileyFacesVisible() );
+        smileyFacesCheckBox.addActionListener( new ActionListener() {
+            public void actionPerformed( ActionEvent e ) {
+                rewardNode.setSmileyFacesVisible( smileyFacesCheckBox.isSelected() );
+            }
+        } );
+
+        final JCheckBox sandwichesCheckBox = new JCheckBox( "show sandwiches" );
+        sandwichesCheckBox.setSelected( rewardNode.isSandwichesVisible() );
+        sandwichesCheckBox.addActionListener( new ActionListener() {
+            public void actionPerformed( ActionEvent e ) {
+                rewardNode.setSandwichesVisible( sandwichesCheckBox.isSelected() );
+            }
+        } );
+
         JPanel devControlPanel = new JPanel();
         devControlPanel.setBorder( new TitledBorder( "Developer Controls" ) );
         EasyGridBagLayout devControlPanelLayout = new EasyGridBagLayout( devControlPanel );
@@ -233,22 +331,21 @@ public class GameRewardNode extends PhetPNode {
         devControlPanelLayout.addComponent( clockDelayControl, row++, column );
         devControlPanelLayout.addComponent( populationControl, row++, column );
         devControlPanelLayout.addComponent( motionDeltaControl, row++, column );
-        
+        devControlPanelLayout.addComponent( smileyFacesCheckBox, row++, column );
+        devControlPanelLayout.addComponent( sandwichesCheckBox, row++, column );
+
         JPanel controlPanel = new JPanel();
-        controlPanel.setBorder( new EmptyBorder( 10, 10, 10, 10) );
-        EasyGridBagLayout controlPanelLayout = new EasyGridBagLayout( controlPanel );
-        controlPanel.setLayout( controlPanelLayout );
-        controlPanelLayout.addComponent( devControlPanel, 0, 0 );
-        
+        controlPanel.setBorder( new EmptyBorder( 10, 10, 10, 10 ) );
+        controlPanel.add( devControlPanel );
+
         JPanel panel = new JPanel( new BorderLayout() );
         panel.add( canvas, BorderLayout.CENTER );
         panel.add( controlPanel, BorderLayout.EAST );
-        
-        JFrame frame = new JFrame();
+
+        JFrame frame = new JFrame( "Game Reward test" );
         frame.setContentPane( panel );
-        frame.setSize( frameSize );
         frame.setDefaultCloseOperation( WindowConstants.EXIT_ON_CLOSE );
-        SwingUtils.centerWindowOnScreen( frame );
+        PhetApplicationConfig.DEFAULT_FRAME_SETUP.initialize( frame ); // ala PhET sims
         frame.setVisible( true );
     }
 }
