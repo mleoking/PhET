15a20a417fe76cb705b951a17b3b5f58f1f0bb95
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-02-18 16:27:30 +0000

    Turned off visibility of capture zones.

--- trunk/simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/view/MembraneChannelNode.java
+++ trunk/simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/view/MembraneChannelNode.java
@@ -25,7 +25,7 @@ public class MembraneChannelNode extends PNode{
     //----------------------------------------------------------------------------
     // Class Data
     //----------------------------------------------------------------------------
-	private static final boolean SHOW_CAPTURE_ZONE = true;
+	private static final boolean SHOW_CAPTURE_ZONE = false;
 	
     //----------------------------------------------------------------------------
     // Instance Data
