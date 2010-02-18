a1a7f2c66b4c22a0f79599a07ce4784cd69f28c9
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-02-18 11:18:38 +0000

    Wicket-website: adding fields to contribution editing, CSS styling, etc.

--- trunk/web/wicket-website/src/java/edu/colorado/phet/website/panels/ContributionEditPanel.java
+++ trunk/web/wicket-website/src/java/edu/colorado/phet/website/panels/ContributionEditPanel.java
@@ -1,6 +1,7 @@
 package edu.colorado.phet.website.panels;
 
 import org.apache.log4j.Logger;
+import org.apache.wicket.behavior.HeaderContributor;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.RequiredTextField;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
@@ -19,6 +20,8 @@ public class ContributionEditPanel extends PhetPanel {
     public ContributionEditPanel( String id, PageContext context, Contribution preContribution ) {
         super( id, context );
 
+        add( HeaderContributor.forCss( "/css/contribution-main-v1.css" ) );
+
         contribution = preContribution;
 
         creating = contribution.getId() == 0;
@@ -31,12 +34,28 @@ public class ContributionEditPanel extends PhetPanel {
     public final class ContributionForm extends Form {
 
         private RequiredTextField authorsText;
+        private RequiredTextField organizationText;
+        private RequiredTextField emailText;
+        private RequiredTextField titleText;
+        private RequiredTextField keywordsText;
 
         public ContributionForm( String id ) {
             super( id );
 
             authorsText = new RequiredTextField( "contribution.edit.authors", new Model( creating ? "" : contribution.getAuthors() ) );
             add( authorsText );
+
+            organizationText = new RequiredTextField( "contribution.edit.organization", new Model( creating ? "" : contribution.getAuthorOrganization() ) );
+            add( organizationText );
+
+            emailText = new RequiredTextField( "contribution.edit.email", new Model( creating ? "" : contribution.getContactEmail() ) );
+            add( emailText );
+
+            titleText = new RequiredTextField( "contribution.edit.title", new Model( creating ? "" : contribution.getTitle() ) );
+            add( titleText );
+
+            keywordsText = new RequiredTextField( "contribution.edit.keywords", new Model( creating ? "" : contribution.getKeywords() ) );
+            add( keywordsText );
         }
 
         @Override
