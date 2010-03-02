948d941e67431e8c7b2efe68e83aa5f51731a361
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-03-02 19:48:23 +0000

    Made the zoom go slightly farther in.

--- trunk/simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/controlpanel/MembraneDiffusionControlPanel.java
+++ trunk/simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/controlpanel/MembraneDiffusionControlPanel.java
@@ -242,7 +242,7 @@ public class MembraneDiffusionControlPanel extends ControlPanel {
     	
         private static final Font LABEL_FONT = new PhetFont(12);
         private static final double MIN_VAL = 0.5;
-        private static final double MAX_VAL = 5;
+        private static final double MAX_VAL = 7;
         
 		public ZoomSlider(String title, final NeuronCanvas neuronCanvas) {
             super( MIN_VAL, MAX_VAL, title, "0", "");
