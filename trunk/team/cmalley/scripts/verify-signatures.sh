78a35c877d20a208be6b482f9cff069a5967ddca
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-03-03 19:50:00 +0000

    see #2185, bash script to identify jars with invalid digital signatures

---
+++ trunk/team/cmalley/scripts/verify-signatures.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+##############################################################################
+#
+#  Verifies the digital signature of all jars below a given directory.
+#  Use this to, for example, verify all jars on the PhET website.
+#
+##############################################################################
+
+# command line args
+if [ "${#}" -ne "1" ] ; then
+    echo "usage: ${0} directory";
+    exit 1;
+fi
+DIR=${1}
+
+for jarfile in `find ${DIR} -name *.jar -print`
+do
+  echo -n "${jarfile}: "
+  jarsigner -verify ${jarfile}
+done | grep SecurityException
+
+##############################################################################
+# end of file
