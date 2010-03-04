6199793e3cbd1bb796e6862a54d46b402eedbadf
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-03-04 19:06:32 +0000

    misc size and layout tweaks: 3/4/10 make Before and After box labels smaller 3/4/10 make "Reaction N of M" label smaller and black 3/4/10 make New Game button in scoreboard less prominent (smaller and very light gray) 3/4/10 simplify questions in Game

--- trunk/simulations-java/simulations/reactants-products-and-leftovers/src/edu/colorado/phet/reactantsproductsandleftovers/view/game/ReactionNumberLabelNode.java
+++ trunk/simulations-java/simulations/reactants-products-and-leftovers/src/edu/colorado/phet/reactantsproductsandleftovers/view/game/ReactionNumberLabelNode.java
@@ -6,7 +6,6 @@ import java.awt.Color;
 import java.text.MessageFormat;
 
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
-import edu.colorado.phet.reactantsproductsandleftovers.RPALConstants;
 import edu.colorado.phet.reactantsproductsandleftovers.RPALStrings;
 import edu.colorado.phet.reactantsproductsandleftovers.module.game.GameModel;
 import edu.colorado.phet.reactantsproductsandleftovers.module.game.GameModel.GameAdapter;
@@ -20,8 +19,8 @@ import edu.umd.cs.piccolo.nodes.PText;
 public class ReactionNumberLabelNode extends PText {
     
     private static final String PATTERN = RPALStrings.LABEL_REACTION_NUMBER;
-    private static final PhetFont FONT = new PhetFont( 28 );
-    private static final Color COLOR = RPALConstants.BEFORE_AFTER_BOX_COLOR;
+    private static final PhetFont FONT = new PhetFont( 20 );
+    private static final Color COLOR = Color.BLACK;
     
     public ReactionNumberLabelNode( final GameModel model ) {
         super();
