d3a7c59a120a5aabd4cfc6f15d5bda12f48fde99
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-03-08 18:09:29 +0000

    Changed to use millivolts internally, added comments, got rid of unused methods.

--- trunk/simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/view/MembranePotentialChart.java
+++ trunk/simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/view/MembranePotentialChart.java
@@ -3,30 +3,22 @@
 package edu.colorado.phet.neuron.view;
 
 import java.awt.BasicStroke;
-import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.geom.Dimension2D;
-import java.awt.geom.Point2D;
-import java.awt.geom.Rectangle2D;
-import java.text.MessageFormat;
-import java.text.SimpleDateFormat;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
-import org.jfree.chart.axis.DateAxis;
 import org.jfree.chart.axis.NumberAxis;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.renderer.xy.XYItemRenderer;
-import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
 import org.jfree.data.xy.XYDataset;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
-import org.jfree.ui.RectangleInsets;
 
 import edu.colorado.phet.common.jfreechartphet.piccolo.JFreeChartNode;
 import edu.colorado.phet.common.phetcommon.model.clock.ClockAdapter;
@@ -47,22 +39,28 @@ import edu.umd.cs.piccolox.pswing.PSwing;
  * the fill the chart half way, but this turned out to be too CPU intensive,
  * so it was changed to draw one line of data across the screen, then clear
  * and draw the next line.
- * 
- * Author: John Blanco, Sam Reid
+
+ * Author: John Blanco
  */
 
 public class MembranePotentialChart extends PNode {
 	
-	private static final Color STROKE_COLOR = Color.red;
+	//----------------------------------------------------------------------------
+	// Class Data
+	//----------------------------------------------------------------------------
+	
 	private static final double TIME_SPAN = 100; // In milliseconds.
 	
 	// This value sets the frequency of chart updates, which helps to reduce
 	// the processor consumption.
 	private static final double UPDATE_PERIOD = 4 * NeuronDefaults.CLOCK_DT; // In seconds
 	
+    //----------------------------------------------------------------------------
+    // Instance Data
+    //----------------------------------------------------------------------------
+	
     private JFreeChart chart;
     private JFreeChartNode jFreeChartNode;
-    private int crossSectionY;
     private AxonModel axonModel;
 	private XYSeries dataSeries = new XYSeries("0");
 
@@ -73,6 +71,10 @@ public class MembranePotentialChart extends PNode {
 	
 	private double endTimeOfChart = TIME_SPAN;
 
+    //----------------------------------------------------------------------------
+    // Constructor(s)
+    //----------------------------------------------------------------------------
+	
     public MembranePotentialChart( Dimension2D size, String title, AxonModel axonModel, String distanceUnits ) {
     	
         this.axonModel = axonModel;
@@ -85,89 +87,59 @@ public class MembranePotentialChart extends PNode {
         	    	updateChart(clockEvent);
         	    }
         	    public void simulationTimeReset( ClockEvent clockEvent ) {
-        	    	clearChart();
+        	    	clearAndSetStartTime(0);
         	    }
         	});
         }
         
         XYDataset dataset = new XYSeriesCollection( dataSeries );
-        // TODO: Internationalize.
+        // TODO: i18n.
         chart = createXYLineChart( title, "Time (ms)", "Membrane Potential (mv)", dataset, PlotOrientation.VERTICAL);
         chart.getXYPlot().getRangeAxis().setTickLabelsVisible( true );
-        chart.getXYPlot().getRangeAxis().setRange( -0.1, 0.1 );
+        chart.getXYPlot().getRangeAxis().setRange( -100, 100 );
         jFreeChartNode = new JFreeChartNode( chart, false );
         jFreeChartNode.setBounds( 0, 0, size.getWidth(), size.getHeight() );
 
-        setHorizontalLabel( MessageFormat.format( "Time (ms)", new Object[]{distanceUnits} ) );
+        // TODO: i18n
+        chart.getXYPlot().getDomainAxis().setLabel( "Time (ms)" );
         chart.getXYPlot().getDomainAxis().setRange( 0, TIME_SPAN );
 
         jFreeChartNode.updateChartRenderingInfo();
 
         addChild( jFreeChartNode );
-
-//        updateLocation();
-
     }
     
+    //----------------------------------------------------------------------------
+    // Methods
+    //----------------------------------------------------------------------------
+
     /**
      * Add a data point to the graph.
      * 
      * @param time - Time in milliseconds.
      * @param voltage - Voltage in volts.
      */
-    public void addDataPoint(double time, double voltage, boolean update){
+    private void addDataPoint(double time, double voltage, boolean update){
+
     	if (time > endTimeOfChart){
     		clearAndSetStartTime(time);
     	}
-    	dataSeries.add(time, voltage, update);
-    	
-//    	if (time > TIME_SPAN / 2){
-//    		// Slide the chart to the left to keep the data to keep the data
-//    		// in the middle.  This essentially creates a strip chart.
-//            chart.getXYPlot().getDomainAxis().setRange( time - TIME_SPAN / 2, time + TIME_SPAN / 2);
-//    	}
-    	
+    	// Add the date point to the data series.  Note that internally we
+    	// work in millivolts, not volts.
+    	dataSeries.add(time, voltage * 1000, update);
     }
     
-    public void clearChart(){
-    	dataSeries.clear();
-    	chart.getXYPlot().getDomainAxis().setRange( 0, TIME_SPAN );
-    	updateCountdownTimer = 0;
-    }
-
-    public void setHorizontalLabel( String horizontalUnits ) {
-        chart.getXYPlot().getDomainAxis().setLabel( horizontalUnits );
-    }
-
-    public Rectangle2D getChartBounds() {
-        Rectangle2D bounds = jFreeChartNode.getFullBounds();
-        localToParent( bounds );
-        return bounds;
-    }
-
-    /*
-    protected void updateLocation() {
-        synchronizeWidth();
-        synchronizeWidth();//in case the first one changed the insets of the graph
-
-        Rectangle2D r2 = jFreeChartNode.getBounds();
-        Rectangle2D data = jFreeChartNode.getDataArea();
-//        double dataX = latticeScreenCoordinates.toScreenCoordinates( 0, 0 ).getX();
-//        double dataY = latticeScreenCoordinates.toScreenCoordinates( 0, waveModel.getHeight() ).getY() + getChartOffset();
-        double dataInsetX = data.getX() - r2.getX();
-        jFreeChartNode.setBounds( 0, 0, r2.getWidth(), r2.getHeight() );
-        // TODO: Sam was setting this to correspond with the location of the
-        // model.  I'm not sure that I need to do this.  Set to 0,0 for now,
-        // and get rid of the whole thing later if possible.
-//        jFreeChartNode.setOffset( dataX - dataInsetX, dataY );
-        jFreeChartNode.setOffset( 0, 0 );
-    }
-    */
-
-    protected double getChartOffset() {
-        return 0;
-    }
-    
+    /**
+     * Create the JFreeChart chart that will show the data and that will be
+     * contained by this node.
+     * 
+     * @param title
+     * @param xAxisLabel
+     * @param yAxisLabel
+     * @param dataset
+     * @param orientation
+     * @return
+     */
     private static JFreeChart createXYLineChart(String title, String xAxisLabel, String yAxisLabel,
     		XYDataset dataset, PlotOrientation orientation) {
 
@@ -191,56 +163,20 @@ public class MembranePotentialChart extends PNode {
             false  // urls
         );
 
-        // Set the stroke to be larger than the default.
+        // Set the stroke for the data line to be larger than the default.
         XYPlot plot = chart.getXYPlot();
         XYItemRenderer renderer = plot.getRenderer();
         renderer.setStroke(new BasicStroke(3f, BasicStroke.JOIN_ROUND, BasicStroke.JOIN_BEVEL));
 
     	return chart;
     }
-    /**
-     * Creates a chart.
-     * 
-     * @param dataset  a dataset.
-     * 
-     * @return A chart.
-     */
-    private static JFreeChart createChart(XYDataset dataset) {
-
-        JFreeChart chart = ChartFactory.createTimeSeriesChart(
-            "Legal & General Unit Trust Prices",  // title
-            "Date",             // x-axis label
-            "Price Per Unit",   // y-axis label
-            dataset,            // data
-            true,               // create legend?
-            true,               // generate tooltips?
-            false               // generate URLs?
-        );
-
-        chart.setBackgroundPaint(Color.white);
-
-        XYPlot plot = (XYPlot) chart.getPlot();
-        plot.setBackgroundPaint(Color.lightGray);
-        plot.setDomainGridlinePaint(Color.white);
-        plot.setRangeGridlinePaint(Color.white);
-        plot.setAxisOffset(new RectangleInsets(5.0, 5.0, 5.0, 5.0));
-        plot.setDomainCrosshairVisible(true);
-        plot.setRangeCrosshairVisible(true);
-        
-        XYItemRenderer r = plot.getRenderer();
-        if (r instanceof XYLineAndShapeRenderer) {
-            XYLineAndShapeRenderer renderer = (XYLineAndShapeRenderer) r;
-            renderer.setBaseShapesVisible(true);
-            renderer.setBaseShapesFilled(true);
-        }
-        
-        DateAxis axis = (DateAxis) plot.getDomainAxis();
-        axis.setDateFormatOverride(new SimpleDateFormat("MMM-yyyy"));
-        
-        return chart;
-
-    }
     
+    /**
+     * Update the chart based on the current time and the model that is being
+     * monitored.
+     * 
+     * @param clockEvent
+     */
     private void updateChart(ClockEvent clockEvent){
     	updateCountdownTimer -= clockEvent.getSimulationTimeChange();
     	
@@ -253,54 +189,6 @@ public class MembranePotentialChart extends PNode {
     	else{
     		addDataPoint(timeInMilliseconds, axonModel.getMembranePotential(), false);
     	}
-    }
-
-    /*
-    private void synchronizeWidth() {
-        double diff = getDesiredDataWidth() - getActualDataWidth();
-        changeDataWidth( (int) diff );
-    }
-    */
-
-    private void changeDataWidth( int increase ) {
-        Rectangle2D r2 = jFreeChartNode.getBounds();
-        jFreeChartNode.setBounds( r2.getX(), r2.getY(), r2.getWidth() + increase, r2.getHeight() );
-        jFreeChartNode.updateChartRenderingInfo();
-    }
-
-    private double getActualDataWidth() {
-        return jFreeChartNode.getDataArea().getWidth();
-    }
-
-    public JFreeChartNode getjFreeChartNode() {
-        return jFreeChartNode;
-    }
-
-    /*
-    private double getDesiredDataWidth() {
-        Point2D min = latticeScreenCoordinates.toScreenCoordinates( 0, 0 );
-        Point2D max = latticeScreenCoordinates.toScreenCoordinates( waveModel.getWidth() - 1, waveModel.getHeight() - 1 );
-        return max.getX() - min.getX();
-    }
-    */
-
-    protected Point2D getPathLocation() {
-        Point2D nodeLoc = jFreeChartNode.plotToNode( new Point2D.Double( 0, 0 ) );
-        jFreeChartNode.localToParent( nodeLoc );
-        return nodeLoc;
-    }
-
-    protected Point2D[] readValues() {
-//        return new WaveSampler( waveModel, -60, latticeScreenCoordinates.getCellWidth() ).readValues(crossSectionY);
-    	// TODO: Make up bogus data for now.
-    	Point2D[] retval = new Point2D[2];
-    	retval[0] = new Point2D.Double(0, 0);
-    	retval[1] = new Point2D.Double(10, 0);
-    	return retval;
-    }
-
-    public double getChartHeight() {
-        return jFreeChartNode.getFullBounds().getHeight();
     }
 
     /**
@@ -337,6 +225,12 @@ public class MembranePotentialChart extends PNode {
 		return (double)tmp / factor;
 	}
 
+	/**
+	 * Test framework, do whatever is needed here to test the behavior of
+	 * this node.
+	 * 
+	 * @param args
+	 */
     public static void main(String[] args) {
     	
     	// Set up the main frame for the application.
@@ -365,18 +259,6 @@ public class MembranePotentialChart extends PNode {
 		});
         PSwing buttonPSwing = new PSwing(button);
         phetPCanvas.addScreenChild(buttonPSwing);
-        
-        // Create and add a button that will clear the chart.
-        JButton clearButton = new JButton("Clear Chart");
-        clearButton.addActionListener(new ActionListener() {
-			
-			public void actionPerformed(ActionEvent e) {
-				membranePotentialChart.clearChart();
-			}
-		});
-        PSwing clearButtonPSwing = new PSwing(clearButton);
-        clearButtonPSwing.setOffset(100, 0);
-        phetPCanvas.addScreenChild(clearButtonPSwing);
         
         // Associate the canvas and the frame and display it.
         frame.setContentPane(phetPCanvas);
