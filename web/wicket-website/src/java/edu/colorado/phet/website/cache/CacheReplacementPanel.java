f824e04bc0236d7bd601c8d4585d583a528c3508
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-03-16 05:15:00 +0000

    Wicket-website: docs

--- web/wicket-website/src/java/edu/colorado/phet/website/cache/CacheReplacementPanel.java
+++ web/wicket-website/src/java/edu/colorado/phet/website/cache/CacheReplacementPanel.java
@@ -9,6 +9,10 @@ import org.apache.wicket.markup.html.internal.HtmlHeaderContainer;
 import edu.colorado.phet.website.panels.PhetPanel;
 import edu.colorado.phet.website.util.PageContext;
 
+/**
+ * A lightweight PhetPanel that can have an arbitrary inserted header and body text. Useful for injecting cached
+ * versions of other panels
+ */
 public class CacheReplacementPanel extends PhetPanel {
 
     private CharSequence header;
@@ -16,14 +20,17 @@ public class CacheReplacementPanel extends PhetPanel {
 
     private static Logger logger = Logger.getLogger( CacheReplacementPanel.class.getName() );
 
+    /**
+     * @param id      The Wicket id for the panel to use
+     * @param context The page context
+     * @param header  The text to use for the header part (inserted into the HTML head element)
+     * @param body    The text to use for the main part (inserted where the original panel is placed)
+     */
     public CacheReplacementPanel( String id, PageContext context, CharSequence header, CharSequence body ) {
         super( id, context );
 
         this.header = header;
         this.body = body;
-
-        //logger.debug( "init header " + header );
-        //logger.debug( "init body " + body );
     }
 
     @Override
@@ -35,7 +42,9 @@ public class CacheReplacementPanel extends PhetPanel {
 
     @Override
     protected void onRender( MarkupStream markupStream ) {
+        // skip the component area in the markup, so it appears to have been rendered
         markupStream.skipComponent();
+
         RequestCycle cycle = getRequestCycle();
         Response response = cycle.getResponse();
         response.write( body );
