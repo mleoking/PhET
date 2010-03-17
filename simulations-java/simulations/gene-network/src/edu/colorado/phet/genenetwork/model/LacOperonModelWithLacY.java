5e85202b69bba20bee9a988d50956348a94544e5
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-03-17 14:50:11 +0000

    Added and cleaned up comments.

--- simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/LacOperonModelWithLacY.java
+++ simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/LacOperonModelWithLacY.java
@@ -4,16 +4,36 @@ import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 
 
+/**
+ * This class adds functionality to the basic lac operon model.  The additions
+ * primarily involve the inclusion of LacY, which is an enzyme (or protein or
+ * some such thing) that is involved in transporting lactose across the cell
+ * membrane.  At the time of this writing (March 2010), the tab that is most
+ * closely associated with this model is the Lactose Transport tab.
+ * 
+ * @author John Blanco
+ */
 public class LacOperonModelWithLacY extends LacOperonModel {
+	
+	//----------------------------------------------------------------------------
+	// Class Data
+	//----------------------------------------------------------------------------
 	
 	// Constants that define the size and position of the cell membrane.
 	private static final double CELL_MEMBRANE_THICKNESS = 4; // In nanometers.
 	private static final Point2D CELL_MEMBRANE_CENTER_POS = new Point2D.Double(0, 40);
 	
-    // The cell membrane.
+	//----------------------------------------------------------------------------
+	// Instance Data
+	//----------------------------------------------------------------------------
+	
+    // The rectangle that describes the position of the cell membrane.
     private Rectangle2D cellMembraneRect;
     
-
+	//----------------------------------------------------------------------------
+	// Constructor(s)
+	//----------------------------------------------------------------------------
+	
 	public LacOperonModelWithLacY(GeneNetworkClock clock, boolean simulateLacY) {
 		super(clock, simulateLacY);
 		
@@ -26,6 +46,10 @@ public class LacOperonModelWithLacY extends LacOperonModel {
 				CELL_MEMBRANE_THICKNESS);
 	}
 
+	//----------------------------------------------------------------------------
+	// Methods
+	//----------------------------------------------------------------------------
+	
 	/**
 	 * Get the cell membrane.  This variation of the lac operon model has one,
 	 * hence the override.
