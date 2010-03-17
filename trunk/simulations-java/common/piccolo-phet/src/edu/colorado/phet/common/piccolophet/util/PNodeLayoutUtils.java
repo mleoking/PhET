9e67113315900dc34f6273b677cc14960e8ae523
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-03-17 19:15:31 +0000

    add PNode layout utilities for computing max width/height, and summing widths/heights

--- trunk/simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/util/PNodeLayoutUtils.java
+++ trunk/simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/util/PNodeLayoutUtils.java
@@ -1,11 +1,14 @@
+/* Copyright 2010, University of Colorado */
+
 package edu.colorado.phet.common.piccolophet.util;
 
 import java.awt.geom.Point2D;
+import java.util.List;
 
 import edu.umd.cs.piccolo.PNode;
 
 /**
- * A collection of utilites that are useful for laying out PNodes.
+ * A collection of utilities that are useful for laying out PNodes.
  * 
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
@@ -31,6 +34,100 @@ public class PNodeLayoutUtils {
     
     public static double getOriginYOffset( PNode node ) {
         return node.getFullBoundsReference().getY() - node.getYOffset();
+    }
+    
+    /**
+     * Gets the max full width of a node's children.
+     * @param node
+     * @return
+     */
+    public static double getMaxFullWidthChildren( PNode node ) {
+        return getMaxFullWidth( node.getChildrenReference() );
+    }
+    
+    /**
+     * Get the max full width in a list of nodes.
+     * @param nodeList
+     * @return
+     */
+    public static double getMaxFullWidth( List<PNode> nodeList ) {
+        double max = 0;
+        for ( PNode node : nodeList ) {
+            double width = node.getFullBoundsReference().getWidth();
+            if ( width > max ) {
+                max = width;
+            }
+        }
+        return max;
+    }
+    
+    /**
+     * Gets the max full height of a node's children.
+     * @param node
+     * @return
+     */
+    public static double getMaxFullHeightChildren( PNode node ) {
+        return getMaxFullHeight( node.getChildrenReference() );
+    }
+
+    /**
+     * Get the max full height in a list of nodes.
+     * @param nodeList
+     * @return
+     */
+    public static double getMaxFullHeight( List<PNode> nodeList ) {
+        double max = 0;
+        for ( PNode node : nodeList ) {
+            double height = node.getFullBoundsReference().getHeight();
+            if ( height > max ) {
+                max = height;
+            }
+        }
+        return max;
+    }
+    
+    /**
+     * Sums the full widths of a node's children.
+     * @param node
+     * @return
+     */
+    public static double sumFullWidthsChildren( PNode node ) {
+        return sumFullWidths( node.getChildrenReference() );
+    }
+    
+    /**
+     * Sums the full widths of a list of nodes.
+     * @param nodeList
+     * @return
+     */
+    public static double sumFullWidths( List<PNode> nodeList ) {
+        double sum = 0;
+        for ( PNode node : nodeList ) {
+            sum += node.getFullBoundsReference().getWidth();
+        }
+        return sum;
+    }
+    
+    /**
+     * Sums the full heights of a node's children.
+     * @param node
+     * @return
+     */
+    public static double sumFullHeightsChildren( PNode node ) {
+        return sumFullHeights( node.getChildrenReference() );
+    }
+    
+    /**
+     * Sums the full heights of a list of nodes.
+     * @param nodeList
+     * @return
+     */
+    public static double sumFullHeights( List<PNode> nodeList ) {
+        double sum = 0;
+        for ( PNode node : nodeList ) {
+            sum += node.getFullBoundsReference().getHeight();
+        }
+        return sum;
     }
 
 }
