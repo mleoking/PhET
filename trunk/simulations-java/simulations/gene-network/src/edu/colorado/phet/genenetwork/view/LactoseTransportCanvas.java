1316e78bcc788bab656957ac8e079e248de90142
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-03-17 18:22:26 +0000

    Added the representation of the cell membrane to the canvas.

--- trunk/simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/view/LactoseTransportCanvas.java
+++ trunk/simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/view/LactoseTransportCanvas.java
@@ -2,7 +2,14 @@
 
 package edu.colorado.phet.genenetwork.view;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.GradientPaint;
+import java.awt.geom.Rectangle2D;
+
+import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.colorado.phet.genenetwork.model.LacOperonModel;
+import edu.umd.cs.piccolo.PNode;
 
 /**
  * Canvas for the Lactose Regulation tab.
@@ -10,11 +17,25 @@ import edu.colorado.phet.genenetwork.model.LacOperonModel;
  * @author John Blanco
  */
 public class LactoseTransportCanvas extends GeneNetworkCanvas {
+	
+	private PNode cellMembraneLayer; 
 
 	public LactoseTransportCanvas(LacOperonModel model) {
 		
 		super(model);
 		
+        // Add the cell membrane to the canvas.
+		cellMembraneLayer = new PNode();
+		addWorldChild(cellMembraneLayer);
+        Rectangle2D cellMembraneRect = model.getCellMembraneRect();
+        if (cellMembraneRect != null){
+        	Rectangle2D transformedCellMembraneRect = getMvt().createTransformedShape(cellMembraneRect).getBounds2D();
+        	GradientPaint paint = new GradientPaint(0f, (float)transformedCellMembraneRect.getCenterY(), Color.WHITE,
+        			0f, (float)transformedCellMembraneRect.getBounds2D().getMaxY(), new Color(255, 100, 100), true);
+        	cellMembraneLayer.addChild(new PhetPPath(transformedCellMembraneRect, paint, new BasicStroke(2f),
+        			Color.BLACK));
+        }
+        
         // Add the DNA strand to the canvas.
         setDnaStrand(new DnaStrandNode(model.getDnaStrand(), getMvt(), getBackground()));
 
