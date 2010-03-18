7a9fadd8e00cb506f7a048b868a3c74aa69d7fb3
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-03-18 17:29:29 +0000

    Fast prototype of the new Membrane Diffusion tab - just put the picture from the spec up on the tab.

---
+++ trunk/simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/module/MembraneDiffusionModule.java
@@ -0,0 +1,89 @@
+/* Copyright 2009, University of Colorado */
+
+package edu.colorado.phet.neuron.module;
+
+import java.awt.Frame;
+
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
+import edu.colorado.phet.common.phetcommon.view.clock.TimeSpeedSlider;
+import edu.colorado.phet.common.piccolophet.PiccoloModule;
+import edu.colorado.phet.common.piccolophet.nodes.mediabuttons.PiccoloClockControlPanel;
+import edu.colorado.phet.neuron.NeuronStrings;
+import edu.colorado.phet.neuron.controlpanel.MembraneDiffusionControlPanel;
+import edu.colorado.phet.neuron.model.NeuronClock;
+import edu.colorado.phet.neuron.view.MembraneDiffusionCanvas;
+
+/**
+ * Membrane Diffusion module.
+ *
+ * @author John Blanco
+ */
+public class MembraneDiffusionModule extends PiccoloModule {
+
+    //----------------------------------------------------------------------------
+    // Instance data
+    //----------------------------------------------------------------------------
+
+    private MembraneDiffusionCanvas canvas;
+    private MembraneDiffusionControlPanel controlPanel;
+    private PiccoloClockControlPanel clockControlPanel;
+
+    //----------------------------------------------------------------------------
+    // Constructors
+    //----------------------------------------------------------------------------
+
+    public MembraneDiffusionModule( Frame parentFrame ) {
+        super( NeuronStrings.TITLE_MEMBRANE_DIFFUSION_MODULE, new NeuronClock( NeuronDefaults.CLOCK_FRAME_RATE,
+        		NeuronDefaults.CLOCK_DT ) );
+
+        // Model
+        NeuronClock clock = (NeuronClock) getClock();
+        // TODO: TBD
+        
+        // Canvas
+        canvas = new MembraneDiffusionCanvas();
+        setSimulationPanel( canvas );
+
+        // Control Panel
+//        controlPanel = new MembraneDiffusionControlPanel( this, parentFrame, model, canvas );
+//        setControlPanel( controlPanel );
+        
+        // Clock controls
+        clockControlPanel = new PiccoloClockControlPanel( getClock() );
+    	final TimeSpeedSlider timeSpeedSlider = new TimeSpeedSlider(NeuronDefaults.CLOCK_DT / 10, 
+    			NeuronDefaults.CLOCK_DT, "0.00", (ConstantDtClock)getClock(), null);
+        timeSpeedSlider.addChangeListener( new ChangeListener() {
+            public void stateChanged( ChangeEvent e ) {
+                ((ConstantDtClock)getClock()).setDt( timeSpeedSlider.getValue() );
+            }
+        } );
+    	clockControlPanel.addBetweenTimeDisplayAndButtons(timeSpeedSlider);
+        setClockControlPanel( clockControlPanel );
+
+        // Help
+        if ( hasHelp() ) {
+            //XXX add help items
+        }
+
+        // Set initial state
+        reset();
+    }
+
+    //----------------------------------------------------------------------------
+    // Accessors
+    //----------------------------------------------------------------------------
+    
+    //----------------------------------------------------------------------------
+    // Module overrides
+    //----------------------------------------------------------------------------
+
+    /**
+     * Resets the module.
+     */
+    public void reset() {
+    	// TODO
+    }    
+}
