5021087794f361a99788df5c78b9d6c2ef1785c7
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-03-19 22:46:31 +0000

    Cleaned up imports.

--- simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/MotionBoundsTrimmer.java
+++ simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/MotionBoundsTrimmer.java
@@ -1,7 +1,6 @@
 package edu.colorado.phet.genenetwork.model;
 
 import java.awt.geom.Rectangle2D;
-import java.awt.geom.Rectangle2D.Double;
 
 /**
  * Utility class used to trim down motion bounds so that the edges of an
