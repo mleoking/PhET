9dd230d31e6e50b1ece91dece076d989c42c169e
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-03-22 20:40:10 +0000

    skeleton for new acid-base-solutions project

---
+++ simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/ABSConstants.java
@@ -0,0 +1,38 @@
+/* Copyright 2009, University of Colorado */
+
+package edu.colorado.phet.acidbasesolutions;
+
+import java.awt.Dimension;
+
+
+/**
+ * A collection of constants that configure global properties.
+ * If you change something here, it will change *everywhere* in this simulation.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
+public class ABSConstants {
+
+    /* Not intended for instantiation. */
+    private ABSConstants() {}
+    
+    //----------------------------------------------------------------------------
+    // Debugging
+    //----------------------------------------------------------------------------
+    
+    // enable debug output for canvas layout updates
+    public static final boolean DEBUG_CANVAS_UPDATE_LAYOUT = false;
+    
+    //----------------------------------------------------------------------------
+    // Application
+    //----------------------------------------------------------------------------
+    
+    public static final String PROJECT_NAME = "acid-base-solutions";
+
+    //----------------------------------------------------------------------------
+    // View
+    //----------------------------------------------------------------------------
+    
+    // reference coordinate frame size for world nodes
+    public static final Dimension CANVAS_RENDERING_SIZE = new Dimension( 1024, 768 );
+}
