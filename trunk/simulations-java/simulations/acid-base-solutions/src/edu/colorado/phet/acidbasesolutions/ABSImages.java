132138f6bfdfbc14c2af920e55dfd433b584255b
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-03-22 20:48:53 +0000

    skeleton for new acid-base-solutions project

---
+++ trunk/simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/ABSImages.java
@@ -0,0 +1,19 @@
+package edu.colorado.phet.acidbasesolutions;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * Images used throughout the simulation.
+ * Loaded statically to make it easy to debug missing images.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
+public class ABSImages {
+
+    /* not intended for instantiation */
+    private ABSImages() {}
+    
+    private static final BufferedImage getBufferedImage( String resourceName ) {
+        return ABSResources.getBufferedImage( resourceName );
+    }
+}
