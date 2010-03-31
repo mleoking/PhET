e100b9f13948c75f5186c3fda97bd3a20d4d3f27
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-03-31 19:46:09 +0000

    Added the ability to set a fade in target that is not 1 (i.e. fully faded in).

--- trunk/simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/model/TimedFadeInStrategy.java
+++ trunk/simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/model/TimedFadeInStrategy.java
@@ -3,10 +3,11 @@ package edu.colorado.phet.neuron.model;
 public class TimedFadeInStrategy extends FadeStrategy {
 
 	public final double fadeTime;
+	public double opaquenessTarget = 1;
 	public double fadeCountdownTimer;
 	
 	/**
-	 * Constructor
+	 * Constructor that assumes full fade in.
 	 * 
 	 * @param fadeTime - time, in seconds of sim time, for this to fade in.
 	 */
@@ -15,9 +16,24 @@ public class TimedFadeInStrategy extends FadeStrategy {
 		this.fadeCountdownTimer = fadeTime;
 	}
 	
+	/**
+	 * Constructor where the final opaqueness value is explicitly specified.
+	 * 
+	 * @param fadeTime - time, in seconds of sim time, for this to fade in.
+	 * @param 
+	 */
+	public TimedFadeInStrategy(double fadeTime, double opaquenessTarget){
+		this.fadeTime = fadeTime;
+		this.opaquenessTarget = opaquenessTarget;
+		this.fadeCountdownTimer = fadeTime;
+	}
+	
 	@Override
 	public void updateOpaqueness(IFadable fadableModelElement, double dt) {
-		fadableModelElement.setOpaqueness( Math.min( 1 - fadeCountdownTimer / fadeTime, 1) );
+		fadableModelElement.setOpaqueness( Math.min( (1 - fadeCountdownTimer / fadeTime) * opaquenessTarget, 1) );
 		fadeCountdownTimer -= dt;
+		if (fadeCountdownTimer < 0){
+			fadeCountdownTimer = 0;
+		}
 	}
 }
