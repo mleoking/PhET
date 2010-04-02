05aa27cd17771057d5cba051d9ebc1bf2b86dbe5
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-04-02 19:48:09 +0000

    more control panels and model elements for Magnifying Glass View prototype

--- simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/prototype/ProtoConstants.java
+++ simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/prototype/ProtoConstants.java
@@ -17,15 +17,20 @@ public class ProtoConstants {
     
     private ProtoConstants() {}
 
+    // canvas
     public static final Color CANVAS_COLOR = Color.WHITE;
     
+    // beaker
     public static final IntegerRange BEAKER_WIDTH_RANGE = new IntegerRange( 50, 500, 250 );
     public static final IntegerRange BEAKER_HEIGHT_RANGE = new IntegerRange( 50, 500, 300 );
     public static final Point2D BEAKER_CENTER = new Point2D.Double( 400, 400 );
+    public static final Color BEAKER_SOLUTION_COLOR = new Color( 193, 222, 227 ); // light blue
     
+    // magnifying glass
     public static final IntegerRange MAGNIFYING_GLASS_DIAMETER_RANGE = new IntegerRange( 100, 700, 400 );
     public static final Point2D MAGNIFYING_GLASS_CENTER = BEAKER_CENTER;
     
+    // weak acid
     public static final DoubleRange WEAK_ACID_CONCENTRATION_RANGE = new DoubleRange( 1E-3, 1 );
     public static final DoubleRange WEAK_ACID_STRENGTH_RANGE = new DoubleRange( 1E-10, 1 );
 }
