c761b49f299f416974bc3a0a24c2bb5ae5770a58
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-04-08 19:11:49 +0000

    prototype: add "show H2O" check box

--- simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/prototype/ImageControls.java
+++ simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/prototype/ImageControls.java
@@ -3,8 +3,11 @@
 package edu.colorado.phet.acidbasesolutions.prototype;
 
 import java.awt.GridBagConstraints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Hashtable;
 
+import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.TitledBorder;
@@ -26,7 +29,8 @@ import edu.colorado.phet.common.phetcommon.view.util.HTMLUtils;
 class ImageControls extends JPanel {
     
     private final ImagesNode imagesNode;
-    private final LinearValueControl maxMoleculesControl, maxH2OControl, scaleControl, transparencyControl, h2oTransparencyControl;
+    private final LinearValueControl maxMoleculesControl, scaleControl, transparencyControl, maxH2OControl, h2oTransparencyControl;
+    private final JCheckBox showH2OCheckBox;
     
     public ImageControls( final ImagesNode imagesNode ) {
         setBorder( new TitledBorder( "Images" ) );
@@ -48,17 +52,6 @@ class ImageControls extends JPanel {
             }
         });
         
-        // H2O images
-        IntegerRange h2OImagesRange = MGPConstants.MAX_H2O_IMAGES_RANGE;
-        String h2oImagesLabel = HTMLUtils.toHTMLString( MGPConstants.H2O_FRAGMENT + " images:" );
-        maxH2OControl = new LinearValueControl( h2OImagesRange.getMin(), h2OImagesRange.getMax(), h2oImagesLabel, "####0", "", new HorizontalLayoutStrategy() );
-        maxH2OControl.setUpDownArrowDelta( 1 );
-        maxH2OControl.addChangeListener( new ChangeListener() {
-            public void stateChanged( ChangeEvent e ) {
-                imagesNode.setMaxH2O( (int) maxH2OControl.getValue() );
-            }
-        });
-
         // scale
         DoubleRange scaleRange = MGPConstants.IMAGE_SCALE_RANGE;
         scaleControl = new LinearValueControl( scaleRange.getMin(), scaleRange.getMax(), "scale:", "0.00", "", new HorizontalLayoutStrategy() );
@@ -83,6 +76,17 @@ class ImageControls extends JPanel {
         transparencyLabelTable.put( new Double( transparencyControl.getMaximum() ), new JLabel( "opaque" ) );
         transparencyControl.setTickLabels( transparencyLabelTable );
         
+        // H2O images
+        IntegerRange h2OImagesRange = MGPConstants.MAX_H2O_IMAGES_RANGE;
+        String h2oImagesLabel = HTMLUtils.toHTMLString( MGPConstants.H2O_FRAGMENT + " images:" );
+        maxH2OControl = new LinearValueControl( h2OImagesRange.getMin(), h2OImagesRange.getMax(), h2oImagesLabel, "####0", "", new HorizontalLayoutStrategy() );
+        maxH2OControl.setUpDownArrowDelta( 1 );
+        maxH2OControl.addChangeListener( new ChangeListener() {
+            public void stateChanged( ChangeEvent e ) {
+                imagesNode.setMaxH2O( (int) maxH2OControl.getValue() );
+            }
+        });
+        
         // H2O transparency
         DoubleRange h2oTransparencyRange = MGPConstants.DOT_TRANSPARENCY_RANGE;
         String h2oTransparencyLabel = HTMLUtils.toHTMLString( MGPConstants.H2O_FRAGMENT + " transparency:" );
@@ -98,6 +102,14 @@ class ImageControls extends JPanel {
         h2oTransparencyLabelTable.put( new Double( h2oTransparencyControl.getMaximum() ), new JLabel( "opaque" ) );
         h2oTransparencyControl.setTickLabels( h2oTransparencyLabelTable );
         
+        // show H2O checkbox
+        showH2OCheckBox = new JCheckBox( "show H2O" );
+        showH2OCheckBox.addActionListener( new ActionListener() {
+            public void actionPerformed( ActionEvent e ) {
+                imagesNode.setH2OVisible( showH2OCheckBox.isSelected() );
+            }
+        } );
+        
         // layout
         EasyGridBagLayout layout = new EasyGridBagLayout( this );
         setLayout( layout );
@@ -107,16 +119,19 @@ class ImageControls extends JPanel {
         layout.addComponent( maxMoleculesControl, row, column++ );
         row++;
         column = 0;
-        layout.addComponent( maxH2OControl, row, column++ );
-        row++;
-        column = 0;
         layout.addComponent( scaleControl, row, column++ );
         row++;
         column = 0;
         layout.addComponent( transparencyControl, row, column++ );
         row++;
         column = 0;
+        layout.addComponent( maxH2OControl, row, column++ );
+        row++;
+        column = 0;
         layout.addComponent( h2oTransparencyControl, row, column++ );
+        row++;
+        column = 0;
+        layout.addComponent( showH2OCheckBox, row, column++ );
         
         updateControls();
     }
@@ -127,5 +142,6 @@ class ImageControls extends JPanel {
         scaleControl.setValue( imagesNode.getImageScale() );
         transparencyControl.setValue( imagesNode.getMoleculeTransparency() ); 
         h2oTransparencyControl.setValue( imagesNode.getH2OTransparency() );
+        showH2OCheckBox.setSelected( imagesNode.isH2OVisible() );
     }
 }
