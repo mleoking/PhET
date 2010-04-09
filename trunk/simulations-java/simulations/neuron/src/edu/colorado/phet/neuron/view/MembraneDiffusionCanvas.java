b2d9e4acdcd33737dd750928c100843a196bc356
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-04-09 16:58:24 +0000

    Removed the static image from this tab, cleaned up to get rid of unused items left over from copying another canvas.

--- trunk/simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/view/MembraneDiffusionCanvas.java
+++ trunk/simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/view/MembraneDiffusionCanvas.java
@@ -2,30 +2,21 @@
 
 package edu.colorado.phet.neuron.view;
 
-import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Point;
 import java.awt.Rectangle;
-import java.awt.Stroke;
 import java.awt.geom.Dimension2D;
-import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
-import java.awt.image.BufferedImage;
 
 import javax.swing.event.EventListenerList;
 
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform2D;
 import edu.colorado.phet.common.piccolophet.PhetPCanvas;
-import edu.colorado.phet.common.piccolophet.nodes.GradientButtonNode;
-import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
-import edu.colorado.phet.neuron.NeuronResources;
 import edu.colorado.phet.neuron.model.AxonModel;
 import edu.colorado.phet.neuron.model.MembraneChannel;
 import edu.colorado.phet.neuron.model.Particle;
 import edu.umd.cs.piccolo.PNode;
-import edu.umd.cs.piccolo.nodes.PImage;
-import edu.umd.cs.piccolo.util.PDimension;
 
 /**
  * Canvas on which the neuron simulation is depicted.
@@ -46,24 +37,6 @@ public class MembraneDiffusionCanvas extends PhetPCanvas {
     private static final Dimension INITIAL_INTERMEDIATE_DIMENSION = new Dimension( INITIAL_INTERMEDIATE_COORD_WIDTH,
     		INITIAL_INTERMEDIATE_COORD_HEIGHT );
     
-    // Size of the chart the depicts the membrane potential.
-    private static final Dimension2D POTENTIAL_CHART_SIZE = new PDimension(INITIAL_INTERMEDIATE_COORD_WIDTH,
-    		INITIAL_INTERMEDIATE_COORD_HEIGHT * 0.3);
-    
-    // Color of button for stimulating the neuron.
-    private static final Color CANVAS_BUTTON_COLOR = Color.GREEN;
-    
-    // For debug: Enable and disable nodes that can help with debug of layout.
-    private static final boolean SHOW_PARTICLE_BOUNDS = false;
-    private static final boolean SHOW_CENTER_CROSS_HAIR = false;
-    private static final boolean SHOW_CHANNEL_LOCATIONS = false;
-
-    // List of registered listeners for canvas events.
-    private EventListenerList listeners = new EventListenerList();
-    
-    // Amount of zooming applied to the root world node.
-    private double zoomFactor = 1;
-    
     //----------------------------------------------------------------------------
     // Instance Data
     //----------------------------------------------------------------------------
@@ -75,24 +48,14 @@ public class MembraneDiffusionCanvas extends PhetPCanvas {
     private ModelViewTransform2D mvt;
     
     // Layers for the canvas.
+    private PNode myWorldNode;
     private PNode particleLayer;
-    private PNode axonBodyLayer;
-    private PNode axonCrossSectionLayer;
+    private PNode chamberLayer;
+    private PNode membraneLayer;
     private PNode channelLayer;
     private PNode channelEdgeLayer;
     private PNode chartLayer;
     
-    // Chart for showing membrane potential.
-    private MembranePotentialChart membranePotentialChart;
-    
-    // Button for stimulating the neuron.
-    GradientButtonNode stimulateNeuronButton;
-    GradientButtonNode grayedStimulateNeuronButton;
-    
-    // For debug: Shows center of zoom.
-    private CrossHairNode crossHairNode;
-    private PNode myWorldNode=new PNode();
-
     //----------------------------------------------------------------------------
     // Constructors
     //----------------------------------------------------------------------------
@@ -119,14 +82,14 @@ public class MembraneDiffusionCanvas extends PhetPCanvas {
         addWorldChild(myWorldNode);
 
         // Create the layers in the desired order.
-        axonBodyLayer = new PNode();
-        axonCrossSectionLayer = new PNode();
+        chamberLayer = new PNode();
+        membraneLayer = new PNode();
         particleLayer = new PNode();
         channelLayer = new PNode();
         channelEdgeLayer = new PNode();
 
-        myWorldNode.addChild(axonBodyLayer);
-        myWorldNode.addChild(axonCrossSectionLayer);
+        myWorldNode.addChild(chamberLayer);
+        myWorldNode.addChild(membraneLayer);
         myWorldNode.addChild(channelLayer);
         myWorldNode.addChild(particleLayer);
         myWorldNode.addChild(channelEdgeLayer);
@@ -134,23 +97,12 @@ public class MembraneDiffusionCanvas extends PhetPCanvas {
         chartLayer = new PNode();
         addScreenChild(chartLayer);
 
-        // TODO: temp
-		BufferedImage tabImage = NeuronResources.getImage( "membrane-diffusion-spec-image.png" );
-        PNode tabImageNode = new PImage( tabImage );
-        chartLayer.addChild(tabImageNode);
-
-        
         // Update the layout.
         updateLayout();
-        
     }
     
     //----------------------------------------------------------------------------
-    // Accessors
-    //----------------------------------------------------------------------------
-    
-    //----------------------------------------------------------------------------
-    // Canvas layout
+    // Methods
     //----------------------------------------------------------------------------
     
     /**
@@ -170,19 +122,6 @@ public class MembraneDiffusionCanvas extends PhetPCanvas {
             // TODO
         }
     }
-    
-    private void updateStimButtonVisibility(){
-    	stimulateNeuronButton.setVisible(!model.isStimulusInitiationLockedOut());
-    	grayedStimulateNeuronButton.setVisible(model.isStimulusInitiationLockedOut());
-    }
-    
-	public void addZoomListener(ZoomListener neuronCanvasZoomListener){
-		listeners.add(ZoomListener.class, neuronCanvasZoomListener);
-	}
-	
-	public void removeZoomListener(ZoomListener neuronCanvasZoomListener){
-		listeners.remove(ZoomListener.class, neuronCanvasZoomListener);
-	}
     
     private void addParticle(Particle particleToBeAdded){
     	final ParticleNode particleNode = new ParticleNode(particleToBeAdded, mvt); 
@@ -205,32 +144,5 @@ public class MembraneDiffusionCanvas extends PhetPCanvas {
 				channelNode.removeFromCanvas(channelLayer, channelEdgeLayer);
 			}
 		});
-    	
-    	/* TODO: The code below adds nodes that make it clear exactly where
-    	 * the channel is for the various membrane channels.  This is useful
-    	 * for debugging, but should be removed once channel traversal is
-    	 * fully worked out.
-    	 */
-    	if (SHOW_CHANNEL_LOCATIONS){
-    		PhetPPath channelTestShape = new PhetPPath(mvt.createTransformedShape(channelToBeAdded.getChannelTestShape()), Color.ORANGE);
-    		channelEdgeLayer.addChild(channelTestShape);
-    	}
     }
-    
-    private static class CrossHairNode extends PNode {
-
-    	private static final double LINE_LENGTH = 10;
-    	private static final Stroke CROSS_HAIR_STROKE = new BasicStroke(3);
-    	private static final Color CROSS_HAIR_COLOR = Color.RED;
-    	
-		public CrossHairNode() {
-			PhetPPath verticalLine = new PhetPPath(new Line2D.Double(0, -LINE_LENGTH, 0, LINE_LENGTH),
-					CROSS_HAIR_STROKE, CROSS_HAIR_COLOR);
-			addChild(verticalLine);
-			PhetPPath horizontalLine = new PhetPPath(new Line2D.Double(-LINE_LENGTH, 0, LINE_LENGTH, 0),
-					CROSS_HAIR_STROKE, CROSS_HAIR_COLOR);
-			addChild(horizontalLine);
-		}
-    }
-    
 }
