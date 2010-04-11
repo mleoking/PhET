f6504e7cc8ad13a10fa46ecf93fdfaf8aa70298d
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-04-11 18:28:27 +0000

    cck: factor out test for TimestepSubdivisions, see #2242

--- trunk/simulations-java/simulations/circuit-construction-kit/scala-src/edu/colorado/phet/cckscala/tests/TimestepSubdivisions.scala
+++ trunk/simulations-java/simulations/circuit-construction-kit/scala-src/edu/colorado/phet/cckscala/tests/TimestepSubdivisions.scala
@@ -38,21 +38,3 @@ class TimestepSubdivisions(errorThreshold: Double) {
     }
   }
 }
-
-object TestTimestepSubdivisions {
-  case class TestState(time: Double = 0.0)
-  def main(args: Array[String]) {
-    val originalState = new TestState
-    val steppable = new Steppable[TestState] {
-      def update(a: TestState, dt: Double) = new TestState(a.time + dt)
-
-      def distance(a: TestState, b: TestState) = (a.time - b.time).abs
-    }
-    val dt = 1.0
-    var state = originalState
-    for (i <- 0 until 10) {
-      println("state = " + state)
-      state = new TimestepSubdivisions(1E-7).update(state, steppable, dt)
-    }
-  }
-}
