4cfc71809cb1e4a6beae8971e0051343c7e3aff9
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-04-12 22:09:02 +0000

    Added bounds to the InjectionMotionStrategy so that particles will stay inside the particle chambers.

--- trunk/simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/model/InjectionMotionStrategy.java
+++ trunk/simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/model/InjectionMotionStrategy.java
@@ -1,6 +1,7 @@
 package edu.colorado.phet.neuron.model;
 
 import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 import java.util.Random;
 
 import edu.colorado.phet.common.phetcommon.math.Vector2D;
@@ -26,17 +27,44 @@ public class InjectionMotionStrategy extends MotionStrategy {
 	private double timeUntilNextJump; // In seconds of sim time.
 	private Vector2D velocityVector = new Vector2D.Double();
 	private Point2D currentLocation = new Point2D.Double();
+	private Rectangle2D motionBounds = new Rectangle2D.Double();
+	private MembraneDiffusionModel model;
 	
-	public InjectionMotionStrategy(Point2D initialLocation, double angle) {
+	/**
+	 * Constructor.
+	 * 
+	 * @param initialLocation
+	 * @param model - Model in the moveable element is being injected.  TODO:
+	 * consider creating an interface with just the elements of the model that
+	 * are needed here, which is primarily the bounds of the chambers.
+	 * @param angle
+	 */
+	public InjectionMotionStrategy(Point2D initialLocation, MembraneDiffusionModel model, double angle) {
 		this.initialLocation.setLocation(initialLocation);
+		this.model = model;
 		timeUntilNextJump = generateNewJumpTime();
 		velocityVector.setComponents(MIN_INITIAL_VELOCITY, 0);
 		currentLocation.setLocation(initialLocation);
+		
+		// Get the motion bounds set up for this strategy.
+		updateMotionBounds();
 	}
 
 	@Override
 	public void move(IMovable movableModelElement, IFadable fadableModelElement, double dt) {
 		
+		// Bounce back toward the inside if we are outside of the motion bounds.
+		if ((currentLocation.getX() > motionBounds.getMaxX() && velocityVector.getX() > 0) ||
+			(currentLocation.getX() < motionBounds.getMinX() && velocityVector.getX() < 0))	{
+			// Reverse direction in the X direction.
+			velocityVector.setComponents(-velocityVector.getX(), velocityVector.getY());
+		}
+		if ((currentLocation.getY() > motionBounds.getMaxY() && velocityVector.getY() > 0) ||
+    		(currentLocation.getY() < motionBounds.getMinY() && velocityVector.getY() < 0))	{
+    		// Reverse direction in the Y direction.
+			velocityVector.setComponents(velocityVector.getX(), -velocityVector.getY());
+    	}
+
 		currentLocation.setLocation(currentLocation.getX() + velocityVector.getX() * dt,
 				currentLocation.getY() + velocityVector.getY() * dt);
 		movableModelElement.setPosition(currentLocation);
@@ -62,6 +90,28 @@ public class InjectionMotionStrategy extends MotionStrategy {
 			timeUntilNextJump = generateNewJumpTime();
 		}
 		*/
+	}
+	
+	private void updateMotionBounds(){
+		if (currentLocation.getY() > model.getMembraneRect().getMaxY()){
+			// Current location is above the membrane, so use the upper
+			// portion of the particle chamber as the bounds.
+			motionBounds.setFrame(model.getParticleChamberRect().getMinX(), model.getMembraneRect().getMaxY(),
+					model.getParticleChamberRect().getWidth(), model.getParticleChamberRect().getMaxY() - model.getMembraneRect().getMaxY());
+		}
+		else if (currentLocation.getY() < model.getMembraneRect().getMinY()){
+			// Current location is above the membrane, so use the upper
+			// portion of the particle chamber as the bounds.
+			motionBounds.setFrame(model.getParticleChamberRect().getMinX(), model.getParticleChamberRect().getMinY(),
+					model.getParticleChamberRect().getWidth(), model.getMembraneRect().getMinY() - model.getParticleChamberRect().getMinY());
+		}
+		else {
+			// The most likely case for encountering this clause is if the
+			// particle is passing through the membrane, and the bounds should
+			// not be undergoing an update at that time, so just assert.
+			System.out.println(getClass().getName() + "Error: Particle is not in either sub-chamber.");
+			assert false;
+		}
 	}
 	
 	private double generateNewJumpTime(){
