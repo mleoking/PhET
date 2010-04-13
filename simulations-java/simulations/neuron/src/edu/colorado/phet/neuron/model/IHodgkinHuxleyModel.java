07b6a164b1bbb7602704efcdd32907a48cf39860
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-04-13 18:35:15 +0000

    Cleanup in preparation for using with membrane diffusion tab.

--- simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/model/IHodgkinHuxleyModel.java
+++ simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/model/IHodgkinHuxleyModel.java
@@ -9,13 +9,36 @@ package edu.colorado.phet.neuron.model;
  */
 public interface IHodgkinHuxleyModel {
 
+	/**  
+	 * Advances the model by dt (delta time).
+	 * 
+	 * @param dt - Delta time in seconds.
+	 */
+	public abstract void stepInTime(double dt);
+
+	/**
+	 * Get the membrane potential in volts.
+	 * 
+	 * @return
+	 */
+	public abstract double getMembraneVoltage();
+
+	/**
+	 * Stimulate the neuron in a way that simulates a depolarization signal
+	 * coming to this neuron.  If the neuron is in the correct state, this
+	 * will trigger an action potential.
+	 */
+	public abstract void stimulate();
+
+	/**
+	 * Reset the model back to nominal conditions.
+	 */
 	public abstract void reset();
 
 	public abstract double get_n4();
 
 	public abstract double get_m3h();
 
-	//The -1 is to correct for the fact that in the H & H paper, the currents are reversed.	
 	public abstract double get_na_current();
 
 	public abstract double get_k_current();
@@ -39,27 +62,5 @@ public interface IHodgkinHuxleyModel {
 	public abstract void set_gk(double gk);
 	
 	public abstract void set_gl(double gl);
-
-	/**  
-	 * Advances the model by dt (delta time).
-	 * 
-	 * @param dt - Delta time in seconds.
-	 */
-	public abstract void stepInTime(double dt);
-
-	/**
-	 * Get the membrane potential in volts (Note: not millivolts, which are
-	 * used inside the model).
-	 * 
-	 * @return
-	 */
-	public abstract double getMembraneVoltage();
-
-	/**
-	 * Stimulate the neuron in a way that simulates a depolarization signal
-	 * coming to this neuron.  If the neuron is in the correct state, this
-	 * will trigger an action potential.
-	 */
-	public abstract void stimulate();
 
 }
