b5b7839892fdcf3cab4c5760a03d66eae1019b6d
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-04-13 22:58:52 +0000

    Improved how the channel is rotated when first created.

--- trunk/simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/view/PotassiumLeakChannelToolBoxNode.java
+++ trunk/simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/view/PotassiumLeakChannelToolBoxNode.java
@@ -1,15 +1,13 @@
 package edu.colorado.phet.neuron.view;
 
 import java.awt.geom.Point2D;
-import java.awt.geom.Point2D.Double;
 
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform2D;
 import edu.colorado.phet.common.piccolophet.PhetPCanvas;
-import edu.colorado.phet.common.piccolophet.nodes.HTMLNode;
+import edu.colorado.phet.neuron.model.MembraneChannel;
 import edu.colorado.phet.neuron.model.MembraneDiffusionModel;
 import edu.colorado.phet.neuron.model.PotassiumLeakageChannel;
 import edu.umd.cs.piccolo.PNode;
-import edu.umd.cs.piccolo.nodes.PText;
 
 public class PotassiumLeakChannelToolBoxNode extends ToolBoxItem {
 
@@ -19,17 +17,15 @@ public class PotassiumLeakChannelToolBoxNode extends ToolBoxItem {
 
 	@Override
 	protected void handleAddRequest(Point2D position) {
-		// TODO Fill this in - it should add a channel to the model.
-		System.out.println(getClass().getName() + "handleAddRequest called.");
+		setMembraneChannel(new PotassiumLeakageChannel(getModel(), null));
+		getModel().addUserControlledMembraneChannel(getMembraneChannel(), getMvt().viewToModel(getOffset()));
 	}
 
 	@Override
 	protected void initializeSelectionNode() {
-		PNode representation = new MembraneChannelNode(new PotassiumLeakageChannel(), SCALING_MVT);
-		representation.rotate(-Math.PI / 2);
-		representation.setOffset(
-				-representation.getFullBoundsReference().width / 2 - representation.getFullBoundsReference().getMinX(),
-				-representation.getFullBoundsReference().height / 2 - representation.getFullBoundsReference().getMinY());
+		MembraneChannel channel = new PotassiumLeakageChannel();
+		channel.setRotationalAngle(-Math.PI / 2);
+		PNode representation = new MembraneChannelNode(channel, SCALING_MVT);
 		setSelectionNode(representation);
 		// TODO i18n
 		setCaption("<html><center>Potassium Leak<br>Channel</center></html>");
