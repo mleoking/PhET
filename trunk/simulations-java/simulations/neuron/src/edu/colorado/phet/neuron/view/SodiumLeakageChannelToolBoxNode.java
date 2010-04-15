4992bfa8cae0bcc50d25ce6c6ac19705c46c8aa6
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-04-15 15:06:30 +0000

    Added the sodium leak channel to the membrane channel tool box.

---
+++ trunk/simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/view/SodiumLeakageChannelToolBoxNode.java
@@ -0,0 +1,35 @@
+package edu.colorado.phet.neuron.view;
+
+import java.awt.geom.Point2D;
+
+import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform2D;
+import edu.colorado.phet.common.piccolophet.PhetPCanvas;
+import edu.colorado.phet.neuron.model.MembraneChannel;
+import edu.colorado.phet.neuron.model.MembraneDiffusionModel;
+import edu.colorado.phet.neuron.model.SodiumLeakageChannel;
+import edu.umd.cs.piccolo.PNode;
+
+public class SodiumLeakageChannelToolBoxNode extends ToolBoxItem {
+
+	public SodiumLeakageChannelToolBoxNode(MembraneDiffusionModel model, ModelViewTransform2D mvt, PhetPCanvas canvas) {
+		super(model, mvt, canvas);
+	}
+
+	@Override
+	protected void handleAddRequest(Point2D position) {
+		setMembraneChannel(new SodiumLeakageChannel(getModel()));
+		getMembraneChannel().setRotationalAngle(-Math.PI / 2);
+		getMembraneChannel().setCenterLocation(getMvt().viewToModel(position));
+		getModel().addUserControlledMembraneChannel(getMembraneChannel());
+	}
+
+	@Override
+	protected void initializeSelectionNode() {
+		MembraneChannel channel = new SodiumLeakageChannel();
+		channel.setRotationalAngle(-Math.PI / 2);
+		PNode representation = new MembraneChannelNode(channel, SCALING_MVT);
+		setSelectionNode(representation);
+		// TODO i18n
+		setCaption("<html><center>Sodium Leak<br>Channel</center></html>");
+	}
+}
