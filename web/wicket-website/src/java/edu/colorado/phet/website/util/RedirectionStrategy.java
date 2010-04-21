75e332ddc9c2f540b8f8b79e20fa3dcdca23eb8b
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-04-21 23:13:36 +0000

    Wicket-website: added categories to redirection strategy

--- web/wicket-website/src/java/edu/colorado/phet/website/util/RedirectionStrategy.java
+++ web/wicket-website/src/java/edu/colorado/phet/website/util/RedirectionStrategy.java
@@ -13,27 +13,64 @@ import org.hibernate.Session;
 import edu.colorado.phet.website.content.contribution.ContributionPage;
 import edu.colorado.phet.website.data.contribution.Contribution;
 
+/**
+ * Handles redirecting all of the old-site URLs to the new URLs. They will then be sent out with 301 (permanent)
+ * redirections.
+ */
 public class RedirectionStrategy implements IRequestTargetUrlCodingStrategy {
 
     private static Logger logger = Logger.getLogger( RedirectionStrategy.class.getName() );
 
     private static Map<String, String> map = new HashMap<String, String>();
 
+    private static Map<String, String> categoryMap = new HashMap<String, String>();
+
+    private static final String VIEW_CONTRIBUTION = "/teacher_ideas/view-contribution.php";
+    private static final String VIEW_CATEGORY = "/simulations/index.php";
+
     static {
         // initialize redirection mapping
+        map.put( "/about/index.php", "/en/about" );
+        map.put( "/contribute/index.php", "/en/contribute" );
         map.put( "/index.php", "/" );
-        map.put( "/simulations/index.php", "/en/simulations/category/featured" );
+        map.put( "/research/index.php", "/en/research" );
+        map.put( "/simulations/translations.php", "/en/simulations/translated" );
         map.put( "/tech_support/index.php", "/en/troubleshooting" );
         map.put( "/tech_support/support-flash.php", "/en/troubleshooting/flash" );
         map.put( "/tech_support/support-java.php", "/en/troubleshooting/java" );
         map.put( "/tech_support/support-javascript.php", "/en/troubleshooting/javascript" );
-        map.put( "/contribute/index.php", "/en/contribute" );
-        map.put( "/research/index.php", "/en/research" );
-        map.put( "/about/index.php", "/en/about" );
 
-        map.put( "/simulations/sims.php?sim=Circuit_Construction_Kit_DC_Only", "/en/simulation/circuit-construction-kit/circuit-construction-kit-dc" );
+//        map.put( "/simulations/sims.php?sim=Circuit_Construction_Kit_DC_Only", "/en/simulation/circuit-construction-kit/circuit-construction-kit-dc" );
 
-        map.put( "/teacher_ideas/view-contribution.php", null );
+        map.put( VIEW_CONTRIBUTION, null );
+        map.put( VIEW_CATEGORY, null );
+
+
+        categoryMap.put( "Featured_Sims", "featured" );
+        categoryMap.put( "New_Sims", "new" );
+        categoryMap.put( "Physics", "physics" );
+        categoryMap.put( "Motion", "physics/motion" );
+        categoryMap.put( "Sound_and_Waves", "physics/sound-and-waves" );
+        categoryMap.put( "Work_Energy_and_Power", "physics/work-energy-and-power" );
+        categoryMap.put( "Heat_and_Thermo", "physics/heat-and-thermodynamics" );
+        categoryMap.put( "Quantum_Phenomena", "physics/quantum-phenomena" );
+        categoryMap.put( "Light_and_Radiation", "physics/light-and-radiation" );
+        categoryMap.put( "Electricity_Magnets_and_Circuits", "physics/electricity-magnets-and-circuits" );
+        categoryMap.put( "Biology", "biology" );
+        categoryMap.put( "Chemistry", "chemistry" );
+        categoryMap.put( "General_Chemistry", "chemistry/general" );
+        categoryMap.put( "Quantum_Chemistry", "chemistry/quantum" );
+        categoryMap.put( "Earth_Science", "earth-science" );
+        categoryMap.put( "Math_", "math" );
+        categoryMap.put( "Tools", "math/tools" );
+        categoryMap.put( "Applications", "math/applications" );
+        categoryMap.put( "All_Sims_by_Grade_Level_", "by-level" );
+        categoryMap.put( "Elementary_School", "by-level/elementary-school" );
+        categoryMap.put( "Middle_School_", "by-level/middle-school" );
+        categoryMap.put( "High_School_", "by-level/high-school" );
+        categoryMap.put( "University_", "by-level/university" );
+        categoryMap.put( "Cutting_Edge_Research", "cutting-edge-research" );
+        
 
         // TODO: add all URLs
     }
@@ -56,8 +93,7 @@ public class RedirectionStrategy implements IRequestTargetUrlCodingStrategy {
             // TODO: add support for this for locales w/ country code
             return path + "/";
         }
-        String cviewstr = "/teacher_ideas/view-contribution.php";
-        if ( path.startsWith( cviewstr ) ) {
+        if ( path.startsWith( VIEW_CONTRIBUTION ) ) {
             try {
                 String idstr = ( (String[]) parameters.get( "contribution_id" ) )[0];
                 final int id = Integer.parseInt( idstr );
@@ -80,6 +116,20 @@ public class RedirectionStrategy implements IRequestTargetUrlCodingStrategy {
             catch( RuntimeException e ) {
                 logger.warn( "bad number X", e );
             }
+        }
+        else if ( path.startsWith( VIEW_CATEGORY ) ) {
+            String prefix = "/en/simulations/category/";
+            if ( parameters.get( "cat" ) == null ) {
+                return prefix + "featured";
+            }
+            String cat = ( (String[]) parameters.get( "cat" ) )[0];
+            String newcat = categoryMap.get( cat );
+            if ( newcat != null ) {
+                return prefix + newcat;
+            }
+
+            // ignore old categories?
+            return prefix + "featured";
         }
         return null;
     }
