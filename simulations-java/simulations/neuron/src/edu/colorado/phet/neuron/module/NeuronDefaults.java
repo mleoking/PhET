c724559e197ece3574f127e45d3046c115cc6bd8
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-04-23 22:51:48 +0000

    Modified the way that clock defaults and ranges are handled by creating explicit min, max, and default values for each module.  This is in support of the feature request to stagger the opening/closing of the gated channels, since the min clock DT will be needed.

--- simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/module/NeuronDefaults.java
+++ simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/module/NeuronDefaults.java
@@ -19,8 +19,21 @@ public class NeuronDefaults {
     // Clock
     public static final boolean CLOCK_RUNNING = true;
     public static final int CLOCK_FRAME_RATE = 25; // fps, frames per second (wall time)
-    public static final double ACTION_POTENTIAL_CLOCK_DT = (1 / (double)CLOCK_FRAME_RATE) / 2000; // Sim defaults to 1/2000th of real time.
-    public static final double MEMBRANE_DIFFUSION_CLOCK_DT = (1 / (double)CLOCK_FRAME_RATE) / 2000; // Sim defaults to 1/2000th of real time.
+    
+    // Set up the clock ranges for the various modules.  Note that for this
+    // sim the clock rates are often several orders of magnitude slower than
+    // real time.
+    
+    public static final double MIN_ACTION_POTENTIAL_CLOCK_DT = (1 / (double)CLOCK_FRAME_RATE) / 3000;
+    public static final double MAX_ACTION_POTENTIAL_CLOCK_DT = (1 / (double)CLOCK_FRAME_RATE) / 1000;
+    public static final double DEFAULT_ACTION_POTENTIAL_CLOCK_DT = 
+    	(MIN_ACTION_POTENTIAL_CLOCK_DT + MAX_ACTION_POTENTIAL_CLOCK_DT) / 2;
+    
+    public static final double MIN_MEMBRANE_DIFFUSION_CLOCK_DT = (1 / (double)CLOCK_FRAME_RATE) / 3000;
+    public static final double MAX_MEMBRANE_DIFFUSION_CLOCK_DT = (1 / (double)CLOCK_FRAME_RATE) / 1000;
+    public static final double DEFAULT_MEMBRANE_DIFFUSION_CLOCK_DT = 
+    	(MIN_ACTION_POTENTIAL_CLOCK_DT + MAX_MEMBRANE_DIFFUSION_CLOCK_DT) / 2; 
+    
     public static final int CLOCK_TIME_COLUMNS = 10;
 
     // Model-view transform
