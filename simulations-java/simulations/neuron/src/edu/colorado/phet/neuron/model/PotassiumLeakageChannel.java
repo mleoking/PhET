6606f139fe8060d0cc8269053f86418408d1eac4
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-04-23 14:42:49 +0000

    Implemented bi-directional leakage for this channel and removed any relationship with the HH model.  This was based on feedback from the IPHY folks.

--- simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/model/PotassiumLeakageChannel.java
+++ simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/model/PotassiumLeakageChannel.java
@@ -6,7 +6,6 @@ import java.awt.Color;
 
 import edu.colorado.phet.common.phetcommon.view.util.ColorUtils;
 import edu.colorado.phet.neuron.NeuronConstants;
-import edu.colorado.phet.neuron.utils.MathUtils;
 
 public class PotassiumLeakageChannel extends AbstractLeakChannel {
 
@@ -21,17 +20,13 @@ public class PotassiumLeakageChannel extends AbstractLeakChannel {
 	
 	private static final double DEFAULT_PARTICLE_VELOCITY = 10000; // In nanometers per sec of sim time.
 	
-	// A scaling factor that is used to normalize the amount of leak channel
-	// current to a value between 0 and 1.  This value was determined by
-	// testing the Hodgkin-Huxley model.
-	private static final double PEAK_NEGATIVE_CURRENT = 3.44;
+	// Constants that define the rate and variability of particle capture.
+	private static final double MIN_INTER_PARTICLE_CAPTURE_TIME = 0.0005; // In seconds of sim time.
+	private static final double MAX_INTER_PARTICLE_CAPTURE_TIME = 0.005;  // In seconds of sim time.
 	
     //----------------------------------------------------------------------------
     // Instance Data
     //----------------------------------------------------------------------------
-	
-	private IHodgkinHuxleyModel hodgkinHuxleyModel;
-	private double previousNormalizedLeakCurrent = 0;
 	
     //----------------------------------------------------------------------------
     // Constructor(s)
@@ -39,14 +34,17 @@ public class PotassiumLeakageChannel extends AbstractLeakChannel {
 	
 	public PotassiumLeakageChannel(IParticleCapture modelContainingParticles, IHodgkinHuxleyModel hodgkinHuxleyModel) {
 		super(CHANNEL_WIDTH, CHANNEL_HEIGHT, modelContainingParticles);
-		this.hodgkinHuxleyModel = hodgkinHuxleyModel;
 
 		// Set the speed at which particles will move through the channel.
 		setParticleVelocity(DEFAULT_PARTICLE_VELOCITY);
 		
-		// Set up the capture zone for this channel.
+		// Set up the capture zones for this channel.
 		setInteriorCaptureZone(new PieSliceShapedCaptureZone(getCenterLocation(), CHANNEL_WIDTH * 5, Math.PI, Math.PI * 0.8));
 		setExteriorCaptureZone(new PieSliceShapedCaptureZone(getCenterLocation(), CHANNEL_WIDTH * 5, 0, Math.PI * 0.8));
+		
+		// Set the rate of particle capture for leakage.
+		setMinInterCaptureTime(MIN_INTER_PARTICLE_CAPTURE_TIME);
+		setMaxInterCaptureTime(MAX_INTER_PARTICLE_CAPTURE_TIME);
 		
 		// Start the capture timer now, since leak channels are always
 		// capturing particles.
@@ -82,62 +80,16 @@ public class PotassiumLeakageChannel extends AbstractLeakChannel {
 	}
 
 	@Override
-	public void stepInTime(double dt) {
-		super.stepInTime(dt);
-		
-		// Since this is a leak channel, it is always open, so the openness
-		// is not updated as it is for the gated channels.  However, we DO
-		// want more potassium to flow through when the leak current in the
-		// HH model goes up, so the following code accomplishes that goal.
-
-		double normalizedLeakCurrent = MathUtils.round(hodgkinHuxleyModel.get_l_current() / PEAK_NEGATIVE_CURRENT, 2);
-		if (normalizedLeakCurrent <= 0){
-			// Only pay attention to negative values for the current, which
-			// we will map to potassium flow back into the cell.  This is a
-			// bit of hollywooding.
-			normalizedLeakCurrent = Math.max(normalizedLeakCurrent, -1);
-			if (normalizedLeakCurrent != previousNormalizedLeakCurrent){
-				previousNormalizedLeakCurrent = normalizedLeakCurrent;
-				updateParticleCaptureRate(Math.abs(normalizedLeakCurrent));
-			}
-		}
-	}
-	
-	/**
-	 * Update the rate of particle capture based on the supplied normalized
-	 * value.
-	 * 
-	 * @param normalizedRate - A value between 0 and 1 where 0 represents the
-	 * minimum capture rate for particles and 1 represents the max.
-	 */
-	private void updateParticleCaptureRate(double normalizedRate){
-		if (normalizedRate <= 0.01){
-			// No captures at this rate.
-			setMinInterCaptureTime(Double.POSITIVE_INFINITY);
-			setMaxInterCaptureTime(Double.POSITIVE_INFINITY);
-			restartCaptureCountdownTimer();
-		}
-		else{
-			// Tweak the following values for different behavior.  Lower
-			// values will yeild more captures per unit time.
-			double smallestAllowableMin = 0.0002;
-			double largestAllowableMin = 0.002;
-			double smallestAllowableMax = smallestAllowableMin * 3;
-			double largestAllowableMax = largestAllowableMin * 3;
-			
-			setMinInterCaptureTime(smallestAllowableMin + (1 - normalizedRate) * (largestAllowableMin - smallestAllowableMin));
-			setMaxInterCaptureTime(smallestAllowableMax + (1 - normalizedRate) * (largestAllowableMax - smallestAllowableMax));
-			
-			if (getCaptureCountdownTimer() > getMaxInterCaptureTime()){
-				// Only restart the capture countdown if the current values is
-				// higher than the max.
-				restartCaptureCountdownTimer();
-			}
-		}
-	}
-
-	@Override
 	protected MembraneCrossingDirection chooseCrossingDirection() {
-		return MembraneCrossingDirection.IN_TO_OUT;
+		// Generally, this channel leaks from in to out, since the
+		// concentration of potassium is greater on the inside of the cell.
+		// However, the IPHY people requested that there should occasionally
+		// be some leakage in the other direction for greater realism, hence
+		// the random choice below.
+		MembraneCrossingDirection direction = MembraneCrossingDirection.IN_TO_OUT;
+		if (RAND.nextDouble() < 0.2){
+			direction = MembraneCrossingDirection.OUT_TO_IN;
+		}
+		return direction;
 	}
 }
