435428ba77ceeeeb117d09f01b31b51bb8b13926
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-04-26 17:51:07 +0000

    start modeling size attributes

---
+++ simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/model/Battery.java
@@ -0,0 +1,57 @@
+/* Copyright 2010, University of Colorado */
+
+package edu.colorado.phet.capacitorlab.model;
+
+import java.util.EventListener;
+
+import javax.swing.event.EventListenerList;
+
+/**
+ * Simple model of a DC battery.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
+public class Battery {
+
+    private double voltage;
+    private final EventListenerList listeners;
+    
+    public Battery() {
+        this( 0 );
+    }
+    
+    public Battery( double voltage ) {
+        this.voltage = voltage;
+        listeners = new EventListenerList();
+    }
+    
+    public void setVoltage( double voltage ) {
+        if ( voltage != this.voltage ) {
+            double oldVoltage = this.voltage;
+            this.voltage = voltage;
+            fireVoltageChanged( oldVoltage, voltage );
+        }
+    }
+    
+    public double getVoltage() {
+        return voltage;
+    }
+    
+    public interface BatteryChangeListener extends EventListener {
+        public void voltageChanged( double oldVoltage, double newVoltage );
+    }
+    
+    public void addBatteryChangeListener( BatteryChangeListener listener ) {
+        listeners.add( BatteryChangeListener.class, listener );
+    }
+    
+    public void removeBatteryChangeListener( BatteryChangeListener listener ) {
+        listeners.remove( BatteryChangeListener.class, listener );
+    }
+    
+    private void fireVoltageChanged( double oldVoltage, double newVoltage ) {
+        for ( BatteryChangeListener listener : listeners.getListeners( BatteryChangeListener.class ) ) {
+            listener.voltageChanged( oldVoltage, newVoltage );
+        }
+    }
+}
