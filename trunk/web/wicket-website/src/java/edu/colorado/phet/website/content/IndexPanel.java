89f000050f3a48fa5513c2cb08bafb727ea89d6c
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-04-27 20:54:07 +0000

    Wicket-website: refactored rotator panel out

--- trunk/web/wicket-website/src/java/edu/colorado/phet/website/content/IndexPanel.java
+++ trunk/web/wicket-website/src/java/edu/colorado/phet/website/content/IndexPanel.java
@@ -1,16 +1,7 @@
 package edu.colorado.phet.website.content;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.util.LinkedList;
-import java.util.List;
-
-import org.apache.wicket.behavior.AttributeAppender;
 import org.apache.wicket.behavior.HeaderContributor;
-import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.Link;
-import org.apache.wicket.model.Model;
-import org.hibernate.Session;
 
 import edu.colorado.phet.website.DistributionHandler;
 import edu.colorado.phet.website.PhetWicketApplication;
@@ -23,12 +14,12 @@ import edu.colorado.phet.website.content.getphet.FullInstallPanel;
 import edu.colorado.phet.website.content.getphet.OneAtATimePanel;
 import edu.colorado.phet.website.content.getphet.RunOurSimulationsPanel;
 import edu.colorado.phet.website.content.simulations.SimulationDisplay;
-import edu.colorado.phet.website.content.simulations.SimulationPage;
-import edu.colorado.phet.website.data.LocalizedSimulation;
 import edu.colorado.phet.website.panels.PhetPanel;
+import edu.colorado.phet.website.panels.RotatorPanel;
 import edu.colorado.phet.website.panels.TranslationLinksPanel;
 import edu.colorado.phet.website.translation.TranslationMainPage;
-import edu.colorado.phet.website.util.*;
+import edu.colorado.phet.website.util.PageContext;
+import edu.colorado.phet.website.util.PhetRequestCycle;
 
 /**
  * The panel which represents the main content portion of the home (index) page
@@ -97,49 +88,7 @@ public class IndexPanel extends PhetPanel {
         add( miniLink );
         miniLink.add( new StaticImage( "mini-screenshot", "/images/geometric-optics-screenshot.png", null ) );
 
-        //String flashvars = "dir=ltr&quantity=2&project1=mass-spring-lab&sim1=mass-spring-lab&title1=Masses+%26+Springs&url1=%2Fen%2Fsimulation%2Fmass-spring-lab&project2=circuit-construction-kit&sim2=circuit-construction-kit-dc&title2=Circuit+Construction+Kit+%28DC+Only%29&url2=%2Fen%2Fsimulation%2Fcircuit-construction-kit%2Fcircuit-construction-kit-dc";
-        final List<LocalizedSimulation> featured = new LinkedList<LocalizedSimulation>();
-
-        HibernateUtils.wrapTransaction( getHibernateSession(), new HibernateTask() {
-            public boolean run( Session session ) {
-                featured.add( HibernateUtils.getBestSimulation( session, getMyLocale(), "mass-spring-lab", "mass-spring-lab" ) );
-                featured.add( HibernateUtils.getBestSimulation( session, getMyLocale(), "circuit-construction-kit", "circuit-construction-kit-dc" ) );
-                featured.add( HibernateUtils.getBestSimulation( session, getMyLocale(), "soluble-salts", "soluble-salts" ) );
-                return true;
-            }
-        } );
-
-        StringBuffer buf = new StringBuffer();
-        buf.append( "dir=" + StringUtils.getStringDirect( getHibernateSession(), "language.dir", getMyLocale() ) + "&" );
-        buf.append( "quantity=" + Integer.toString( featured.size() ) + "&" );
-        try {
-            buf.append( "next=" + URLEncoder.encode( getPhetLocalizer().getString( "home.rotator.next", this ), "UTF-8" ) + "&" );
-            buf.append( "previous=" + URLEncoder.encode( getPhetLocalizer().getString( "home.rotator.previous", this ), "UTF-8" ) + "&" );
-
-            int idx = 1;
-            for ( LocalizedSimulation lsim : featured ) {
-                String ids = Integer.toString( idx++ );
-                buf.append( "sim" + ids + "=" + URLEncoder.encode( lsim.getSimulation().getName(), "UTF-8" ) + "&" );
-                buf.append( "title" + ids + "=" + URLEncoder.encode( lsim.getTitle(), "UTF-8" ) + "&" );
-                buf.append( "url" + ids + "=" + URLEncoder.encode( SimulationPage.getLinker( lsim ).getRawUrl( context, getPhetCycle() ), "UTF-8" ) + "&" );
-            }
-        }
-        catch( UnsupportedEncodingException e ) {
-            e.printStackTrace();
-        }
-
-        // TODO: separate out rotator panel
-
-        Model flashvarsModel = new Model( buf.toString() );
-
-        Label paramLabel = new Label( "flash-param" );
-        paramLabel.add( new AttributeAppender( "value", flashvarsModel, " " ) );
-        add( paramLabel );
-
-        Label embedLabel = new Label( "flash-embed" );
-        embedLabel.add( new AttributeAppender( "FlashVars", flashvarsModel, " " ) );
-        add( embedLabel );
-
+        add( new RotatorPanel( "rotator-panel", context ) );
     }
 
 }
---
+++ trunk/web/wicket-website/src/java/edu/colorado/phet/website/panels/RotatorPanel.java
@@ -0,0 +1,67 @@
+package edu.colorado.phet.website.panels;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.wicket.behavior.AttributeAppender;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.Model;
+import org.hibernate.Session;
+
+import edu.colorado.phet.website.content.simulations.SimulationPage;
+import edu.colorado.phet.website.data.LocalizedSimulation;
+import edu.colorado.phet.website.util.HibernateTask;
+import edu.colorado.phet.website.util.HibernateUtils;
+import edu.colorado.phet.website.util.PageContext;
+import edu.colorado.phet.website.util.StringUtils;
+
+public class RotatorPanel extends PhetPanel {
+    public RotatorPanel( String id, PageContext context ) {
+        super( id, context );
+
+        //String flashvars = "dir=ltr&quantity=2&project1=mass-spring-lab&sim1=mass-spring-lab&title1=Masses+%26+Springs&url1=%2Fen%2Fsimulation%2Fmass-spring-lab&project2=circuit-construction-kit&sim2=circuit-construction-kit-dc&title2=Circuit+Construction+Kit+%28DC+Only%29&url2=%2Fen%2Fsimulation%2Fcircuit-construction-kit%2Fcircuit-construction-kit-dc";
+        final List<LocalizedSimulation> featured = new LinkedList<LocalizedSimulation>();
+
+        HibernateUtils.wrapTransaction( getHibernateSession(), new HibernateTask() {
+            public boolean run( Session session ) {
+                featured.add( HibernateUtils.getBestSimulation( session, getMyLocale(), "mass-spring-lab", "mass-spring-lab" ) );
+                featured.add( HibernateUtils.getBestSimulation( session, getMyLocale(), "circuit-construction-kit", "circuit-construction-kit-dc" ) );
+                featured.add( HibernateUtils.getBestSimulation( session, getMyLocale(), "soluble-salts", "soluble-salts" ) );
+                return true;
+            }
+        } );
+
+        StringBuffer buf = new StringBuffer();
+        buf.append( "dir=" + StringUtils.getStringDirect( getHibernateSession(), "language.dir", getMyLocale() ) + "&" );
+        buf.append( "quantity=" + Integer.toString( featured.size() ) + "&" );
+        try {
+            buf.append( "next=" + URLEncoder.encode( getPhetLocalizer().getString( "home.rotator.next", this ), "UTF-8" ) + "&" );
+            buf.append( "previous=" + URLEncoder.encode( getPhetLocalizer().getString( "home.rotator.previous", this ), "UTF-8" ) + "&" );
+
+            int idx = 1;
+            for ( LocalizedSimulation lsim : featured ) {
+                String ids = Integer.toString( idx++ );
+                buf.append( "sim" + ids + "=" + URLEncoder.encode( lsim.getSimulation().getName(), "UTF-8" ) + "&" );
+                buf.append( "title" + ids + "=" + URLEncoder.encode( lsim.getTitle(), "UTF-8" ) + "&" );
+                buf.append( "url" + ids + "=" + URLEncoder.encode( SimulationPage.getLinker( lsim ).getRawUrl( context, getPhetCycle() ), "UTF-8" ) + "&" );
+            }
+        }
+        catch( UnsupportedEncodingException e ) {
+            e.printStackTrace();
+        }
+
+        // TODO: separate out rotator panel
+
+        Model flashvarsModel = new Model( buf.toString() );
+
+        Label paramLabel = new Label( "flash-param" );
+        paramLabel.add( new AttributeAppender( "value", flashvarsModel, " " ) );
+        add( paramLabel );
+
+        Label embedLabel = new Label( "flash-embed" );
+        embedLabel.add( new AttributeAppender( "FlashVars", flashvarsModel, " " ) );
+        add( embedLabel );
+    }
+}
