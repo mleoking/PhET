f82e813d3d476bf49f05f7b56363c8afced061a0
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-04-28 22:30:59 +0000

    Removed unused code based on dead code analysis using Eclipse Core Tools.

--- trunk/simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/model/SodiumIon.java
+++ trunk/simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/model/SodiumIon.java
@@ -3,18 +3,12 @@ package edu.colorado.phet.neuron.model;
 import java.awt.Color;
 
 import edu.colorado.phet.neuron.NeuronConstants;
-import edu.colorado.phet.neuron.NeuronStrings;
 
 public class SodiumIon extends Particle {
 	
 	@Override
 	public ParticleType getType() {
 		return ParticleType.SODIUM_ION;
-	}
-
-	@Override
-	public String getLabelText() {
-		return NeuronStrings.SODIUM_CHEMICAL_SYMBOL;
 	}
 
 	@Override
