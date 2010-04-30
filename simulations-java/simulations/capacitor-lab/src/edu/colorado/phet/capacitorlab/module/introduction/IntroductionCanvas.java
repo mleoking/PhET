12fd2d85ff4ab13f8a37f8f5d97bbeb87c4a2c5d
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-04-30 17:33:27 +0000

    make the Diectric tab the default for now, I'll work  on it first since Intro is a subset

--- simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/module/introduction/IntroductionCanvas.java
+++ simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/module/introduction/IntroductionCanvas.java
@@ -2,11 +2,8 @@
 
 package edu.colorado.phet.capacitorlab.module.introduction;
 
-import edu.colorado.phet.capacitorlab.CLConstants;
 import edu.colorado.phet.capacitorlab.model.CLModel;
 import edu.colorado.phet.capacitorlab.module.CLCanvas;
-import edu.colorado.phet.capacitorlab.view.BatteryNode;
-import edu.colorado.phet.capacitorlab.view.CapacitorNode;
 
 /**
  * Canvas for the "Introduction" module.
@@ -16,13 +13,7 @@ import edu.colorado.phet.capacitorlab.view.CapacitorNode;
 public class IntroductionCanvas extends CLCanvas {
 
     public IntroductionCanvas( CLModel model ) {
-        
-        BatteryNode batteryNode = new BatteryNode( model.getBattery(), CLConstants.BATTERY_VOLTAGE_RANGE );
-        addChild( batteryNode );
-        
-        CapacitorNode capacitorNode = new CapacitorNode( model.getCapacitor() );
-        addChild( capacitorNode );
-        capacitorNode.setOffset( 300, 0 );//XXX
+        //XXX
     }
     
     public void reset() {
