faf92b31615cc34dd1836e4bddb82fd0432ee0f2
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-05-02 07:07:08 +0000

    cck: remove extraneous debug output, see #2241

--- trunk/simulations-java/simulations/circuit-construction-kit/src/edu/colorado/phet/circuitconstructionkit/CCKDeveloperDialog.java
+++ trunk/simulations-java/simulations/circuit-construction-kit/src/edu/colorado/phet/circuitconstructionkit/CCKDeveloperDialog.java
@@ -14,16 +14,17 @@ import javax.swing.event.ChangeListener;
  * Developer dialog for CCK Settings.
  */
 public class CCKDeveloperDialog extends JDialog {
+    private final MNAAdapter solver;
 
     public CCKDeveloperDialog(PhetFrame phetFrame, CCKModule cckModule) {
         super(phetFrame, "CCK Developer Dialog", false);
+        solver = cckModule.getCCKModel().getCircuitSolver();
         JPanel contentPane = new VerticalLayoutPanel();
         JLabel textArea = new JLabel("<html>These developer controls govern the behavior of the timestep subdivision algorithm, <br>" +
                 "and subsequently the tradeoff between accuracy and speed in the CCK model.  Their effects will only be visible in <br>" +
                 "a circuit which requires timestep subdivisions. Model parameters are are 10^ slider values.  See #2241</html>");
         contentPane.add(textArea);
         contentPane.add(new JSeparator());
-        final MNAAdapter solver = cckModule.getCCKModel().getCircuitSolver();
 
         contentPane.add(new JTextArea("threshold for determining whether 2 states are similar enough;\n" +
                 "any error less than errorThreshold will be tolerated"));
@@ -31,6 +32,7 @@ public class CCKDeveloperDialog extends JDialog {
         errorThresholdControl.addChangeListener(new ChangeListener() {
             public void stateChanged(ChangeEvent e) {
                 solver.setErrorThreshold(Math.pow(10, errorThresholdControl.getValue()));
+                displayValues();
             }
         });
         contentPane.add(errorThresholdControl);
@@ -48,5 +50,9 @@ public class CCKDeveloperDialog extends JDialog {
 
         pack();
         SwingUtils.centerDialogInParent(this);
+    }
+
+    private void displayValues() {
+        System.out.println("errorThreshold = " + solver.getErrorThreshold() + ", minDT = " + solver.getMinDT());
     }
 }
