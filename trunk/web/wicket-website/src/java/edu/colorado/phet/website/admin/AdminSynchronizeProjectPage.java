ac7c645145dc9fd89559ceec5b050721bd0a9f72
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-05-03 20:33:06 +0000

    Wicket-website: synchronization URL set up for local use

--- trunk/web/wicket-website/src/java/edu/colorado/phet/website/admin/AdminSynchronizeProjectPage.java
+++ trunk/web/wicket-website/src/java/edu/colorado/phet/website/admin/AdminSynchronizeProjectPage.java
@@ -4,92 +4,56 @@ import org.apache.log4j.Logger;
 import org.apache.wicket.PageParameters;
 import org.apache.wicket.markup.html.pages.RedirectPage;
 
+import edu.colorado.phet.website.PhetWicketApplication;
 import edu.colorado.phet.website.data.Project;
-import edu.colorado.phet.website.data.contribution.Contribution;
 import edu.colorado.phet.website.templates.PhetRegularPage;
 import edu.colorado.phet.website.util.PageContext;
 import edu.colorado.phet.website.util.links.AbstractLinker;
 import edu.colorado.phet.website.util.links.RawLinkable;
-import edu.colorado.phet.website.content.IndexPage;
 
 /**
- * Page meant solely for handling submitted forms for adding comments. If the user isn't logged in, they are redirected
- * to the sign in page with the correct destination specified to return (so it will save their comment)
+ * Called during the deployment process from within the server, meant to synchronize a particular project
  */
 public class AdminSynchronizeProjectPage extends PhetRegularPage {
 
     private static Logger logger = Logger.getLogger( AdminSynchronizeProjectPage.class.getName() );
 
-    private int contributionId;
-    private Contribution contribution;
 
     public AdminSynchronizeProjectPage( PageParameters parameters ) {
         super( parameters );
 
         String addr = getPhetCycle().getWebRequest().getHttpServletRequest().getRemoteAddr();
         String host = getPhetCycle().getWebRequest().getHttpServletRequest().getRemoteHost();
+        String localhost = getPhetCycle().getWebRequest().getHttpServletRequest().getServerName();
 
         logger.debug( "addr: " + addr );
         logger.debug( "host: " + host );
+        logger.debug( "localhost: " + localhost );
 
-//        contributionId = parameters.getInt( "contribution_id" );
-//        final String text = parameters.getString( "text" );
-//
-//        if ( !PhetSession.get().isSignedIn() ) {
-//            setResponsePage( new RedirectPage( SignInPage.getLinker( getLinker( contributionId, text ).getRawUrl( getPageContext(), getPhetCycle() ) ).getRawUrl( getPageContext(), getPhetCycle() ) ) );
-//            return;
-//        }
-//
-//        final PhetUser user = PhetSession.get().getUser();
-//
-//        boolean success = HibernateUtils.wrapTransaction( getHibernateSession(), new HibernateTask() {
-//            public boolean run( Session session ) {
-//                contribution = (Contribution) session.createQuery( "select c from Contribution as c where c.id = :id" ).setInteger( "id", contributionId ).uniqueResult();
-//                return true;
-//            }
-//        } );
-//
-//        if ( !success ) {
-//            logger.info( "unknown contribution of id " + contributionId );
-//            throw new RestartResponseAtInterceptPageException( NotFoundPage.class );
-//        }
-//
-//        logger.debug( "adding comment to contribution " + contribution.getId() + " with user " + user.getEmail() + " and text " + text );
-//        HibernateUtils.wrapTransaction( getHibernateSession(), new HibernateTask() {
-//            public boolean run( Session session ) {
-//                Contribution contrib = (Contribution) session.load( Contribution.class, contribution.getId() );
-//                PhetUser phetuser = (PhetUser) session.load( PhetUser.class, user.getId() );
-//                ContributionComment comment = new ContributionComment();
-//                comment.setText( text );
-//                comment.setDateCreated( new Date() );
-//                comment.setDateUpdated( new Date() );
-//                comment.setContribution( contrib );
-//                comment.setPhetUser( phetuser );
-//                session.save( comment );
-//                return true;
-//            }
-//        } );
+        if ( localhost.equals( host ) ) {
+            String projectName = parameters.getString( "project" );
+
+            logger.info( "synchronizing project: " + projectName );
+
+            Project.synchronizeProject( PhetWicketApplication.get().getPhetDocumentRoot(), getHibernateSession(), projectName );
+        }
 
         // redirect back to contribution page
         setResponsePage( new RedirectPage( "/" ) );
 
     }
 
-//    public static void addToMapper( PhetUrlMapper mapper ) {
-//        mapper.addMap( "^admin/submit-comment$", AdminSynchronizeProjectPage.class, new String[]{} );
-//    }
-
-    public static RawLinkable getLinker( final int projectId ) {
+    public static RawLinkable getLinker( final String projectName ) {
         return new AbstractLinker() {
             @Override
             public String getSubUrl( PageContext context ) {
-                return "admin/synchronize?project=" + projectId;
+                return "admin/synchronize?project=" + projectName;
             }
         };
     }
 
     public static RawLinkable getLinker( Project project ) {
-        return getLinker( project.getId() );
+        return getLinker( project.getName() );
     }
 
 }
