68668905f88c40a48e755ce21dca1ee499d4b8ea
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-05-03 16:06:46 +0000

    add developer controls for some direct-manipulation things that I won't get to for awhile

--- trunk/simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/control/CLTitledControlPanel.java
+++ trunk/simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/control/CLTitledControlPanel.java
@@ -2,6 +2,7 @@
 
 package edu.colorado.phet.capacitorlab.control;
 
+import java.awt.Color;
 import java.awt.Font;
 
 import javax.swing.JPanel;
@@ -19,8 +20,13 @@ public abstract class CLTitledControlPanel extends JPanel {
     public static final Font TITLE_FONT = new PhetFont( Font.BOLD, 18 );
 
     public CLTitledControlPanel( String title ) {
+        this( title, Color.BLACK );
+    }
+    
+    public CLTitledControlPanel( String title, Color titleColor ) {
         TitledBorder border = new TitledBorder( title );
         border.setTitleFont( TITLE_FONT );
+        border.setTitleColor( titleColor );
         setBorder( border );
     }
 }
