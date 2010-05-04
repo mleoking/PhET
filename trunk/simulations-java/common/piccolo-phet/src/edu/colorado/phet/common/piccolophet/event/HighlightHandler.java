51e4e55de3db6c37ac2e8ef5463bdb5c755435c3
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-05-04 17:05:47 +0000

    migrate HighlightHandler to piccolo-phet

--- trunk/simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/event/HighlightHandler.java
+++ trunk/simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/event/HighlightHandler.java
@@ -1,6 +1,6 @@
 /* Copyright 2010, University of Colorado */
 
-package edu.colorado.phet.capacitorlab.util;
+package edu.colorado.phet.common.piccolophet.event;
 
 import java.awt.Image;
 import java.awt.Paint;
