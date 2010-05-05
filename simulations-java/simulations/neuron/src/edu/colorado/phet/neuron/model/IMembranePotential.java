7604809ebcf356c65bfd0120f60b1387833c4ce9
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-05-05 21:56:22 +0000

    Extracted an interface for getting the membrane potential.

---
+++ simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/model/IMembranePotential.java
@@ -0,0 +1,12 @@
+package edu.colorado.phet.neuron.model;
+
+public interface IMembranePotential {
+
+	/**
+	 * Get the membrane potential in volts.
+	 * 
+	 * @return
+	 */
+	public abstract double getMembranePotential();
+
+}
