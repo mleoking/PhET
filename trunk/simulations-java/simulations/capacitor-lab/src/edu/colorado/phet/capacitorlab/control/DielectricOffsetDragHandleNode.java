a716edd26fcacd5e25aef3b4064edd2106955519
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-05-05 23:36:35 +0000

    5/5/10 remove developer control panel from Dielectric module control panel 5/5/10 implement handlers for drag handles (still has some coordinate frame conversion issues)

--- trunk/simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/control/DielectricOffsetDragHandleNode.java
+++ trunk/simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/control/DielectricOffsetDragHandleNode.java
@@ -7,8 +7,12 @@ import java.awt.geom.Point2D;
 import edu.colorado.phet.capacitorlab.CLConstants;
 import edu.colorado.phet.capacitorlab.CLStrings;
 import edu.colorado.phet.capacitorlab.model.Capacitor;
+import edu.colorado.phet.capacitorlab.model.ModelViewTransform;
 import edu.colorado.phet.capacitorlab.model.Capacitor.CapacitorChangeAdapter;
+import edu.colorado.phet.common.phetcommon.util.DoubleRange;
 import edu.colorado.phet.common.piccolophet.PhetPNode;
+import edu.umd.cs.piccolo.event.PDragEventHandler;
+import edu.umd.cs.piccolo.event.PInputEvent;
 
 /**
  * Drag handle for changing the dielectric offset.
@@ -25,10 +29,11 @@ public class DielectricOffsetDragHandleNode extends PhetPNode {
     private static final Point2D LINE_START_LOCATION = new Point2D.Double( 0, 0 );
     private static final Point2D LINE_END_LOCATION = new Point2D.Double( LINE_LENGTH, 0 );
     
-    public DielectricOffsetDragHandleNode( final Capacitor capacitor ) {
+    public DielectricOffsetDragHandleNode( final Capacitor capacitor, ModelViewTransform mvt, DoubleRange valueRange ) {
         
         // arrow
         DragHandleArrowNode arrowNode = new DragHandleArrowNode( ARROW_TIP_LOCATION, ARROW_TAIL_LOCATION );
+        arrowNode.addInputEventListener( new DielectricOffsetDragHandler( capacitor, mvt, valueRange ) );
         
         // line
         DragHandleLineNode lineNode = new DragHandleLineNode( LINE_START_LOCATION, LINE_END_LOCATION );
@@ -59,5 +64,56 @@ public class DielectricOffsetDragHandleNode extends PhetPNode {
         x = arrowNode.getXOffset();
         y = arrowNode.getFullBoundsReference().getMaxY();
         valueNode.setOffset( x, y );
+    }
+    
+    private static class DielectricOffsetDragHandler extends PDragEventHandler {
+        
+        private final Capacitor capacitor;
+        private final ModelViewTransform mvt;
+        private final DoubleRange valueRange;
+        
+        private double startDragValue; // the model value when the drag started
+        private final Point2D startDragPosition; // mouse location when the drag started, in global coordinates
+        
+        public DielectricOffsetDragHandler( Capacitor capacitor, ModelViewTransform mvt, DoubleRange valueRange ) {
+            this.capacitor = capacitor;
+            this.mvt = mvt;
+            this.valueRange = new DoubleRange( valueRange );
+            startDragPosition = new Point2D.Double();
+        }
+        
+        @Override
+        protected void startDrag(PInputEvent event) {
+            super.startDrag( event );
+            startDragPosition.setLocation( getGlobalMousePosition( event ) );
+            startDragValue = capacitor.getDielectricOffset();
+        }
+        
+        @Override
+        protected void drag( PInputEvent event ) {
+            
+            // calculate the new model value, clamped to the range
+            Point2D mousePosition = getGlobalMousePosition( event );
+            double deltaX = mousePosition.getX() - startDragPosition.getX();
+            double deltaValue = mvt.viewToModel( deltaX );
+            double newValue =  startDragValue + deltaValue;
+            if ( newValue > valueRange.getMax() ) {
+                newValue = valueRange.getMax();
+            }
+            else if ( newValue < valueRange.getMin() ) {
+                newValue = valueRange.getMin();
+            }
+            
+            // update the model
+            capacitor.setDielectricOffset( newValue );
+        }
+        
+        /*
+         * Gets the mouse position in the global coordinate frame.
+         */
+        private Point2D getGlobalMousePosition( PInputEvent event ) {
+            Point2D pLocal = event.getPositionRelativeTo( event.getPickedNode() );
+            return event.getPickedNode().localToGlobal( pLocal );
+        }
     }
 }
--- trunk/simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/control/PlateAreaDragHandleNode.java
+++ trunk/simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/control/PlateAreaDragHandleNode.java
@@ -7,8 +7,12 @@ import java.awt.geom.Point2D;
 import edu.colorado.phet.capacitorlab.CLConstants;
 import edu.colorado.phet.capacitorlab.CLStrings;
 import edu.colorado.phet.capacitorlab.model.Capacitor;
+import edu.colorado.phet.capacitorlab.model.ModelViewTransform;
 import edu.colorado.phet.capacitorlab.model.Capacitor.CapacitorChangeAdapter;
+import edu.colorado.phet.common.phetcommon.util.DoubleRange;
 import edu.colorado.phet.common.piccolophet.PhetPNode;
+import edu.umd.cs.piccolo.event.PDragEventHandler;
+import edu.umd.cs.piccolo.event.PInputEvent;
 
 /**
  * Drag handle for changing the plate area.
@@ -28,11 +32,14 @@ public class PlateAreaDragHandleNode extends PhetPNode {
     private final DragHandleArrowNode arrowNode;
     private final DragHandleLineNode lineNode;
     private final DragHandleValueNode valueNode;
+    private final PlateAreaDragHandler dragHandler;
     
-    public PlateAreaDragHandleNode( final Capacitor capacitor ) {
+    public PlateAreaDragHandleNode( final Capacitor capacitor, ModelViewTransform mvt, DoubleRange valueRange ) {
         
         // arrow
         arrowNode = new DragHandleArrowNode( ARROW_TIP_LOCATION, ARROW_TAIL_LOCATION );
+        dragHandler = new PlateAreaDragHandler( capacitor, mvt, valueRange );
+        arrowNode.addInputEventListener( dragHandler );
         
         // line
         lineNode = new DragHandleLineNode( LINE_START_LOCATION, LINE_END_LOCATION );
@@ -59,7 +66,10 @@ public class PlateAreaDragHandleNode extends PhetPNode {
     }
     
     private void updateLayout() {
+        
         double angle = ( ( Math.PI / 2) - CLConstants.VIEWING_ANGLE ) + ( CLConstants.VIEWING_ANGLE / 2 ); // aligned with diagonal of plate surface
+        dragHandler.setAngle( angle );
+        
         double x = 0;
         double y = 0;
         lineNode.setOffset( x, y );
@@ -71,5 +81,75 @@ public class PlateAreaDragHandleNode extends PhetPNode {
         x = lineNode.getFullBoundsReference().getMaxX() - valueNode.getFullBoundsReference().getWidth();
         y = lineNode.getFullBoundsReference().getMinY() - valueNode.getFullBoundsReference().getHeight();
         valueNode.setOffset( x, y );
+    }
+    
+    private static class PlateAreaDragHandler extends PDragEventHandler {
+        
+        private final Capacitor capacitor;
+        private final ModelViewTransform mvt;
+        private final DoubleRange valueRange;
+        private double angle;
+        
+        private double startDragValue; // the model value when the drag started
+        private final Point2D startDragPosition; // mouse location when the drag started, in global coordinates
+        
+        public PlateAreaDragHandler( Capacitor capacitor, ModelViewTransform mvt, DoubleRange valueRange ) {
+            this.capacitor = capacitor;
+            this.mvt = mvt;
+            this.valueRange = new DoubleRange( valueRange );
+            startDragPosition = new Point2D.Double();
+        }
+        
+        public void setAngle( double angle ) {
+            this.angle = angle;
+        }
+        
+        @Override
+        protected void startDrag(PInputEvent event) {
+            super.startDrag( event );
+            startDragPosition.setLocation( getGlobalMousePosition( event ) );
+            startDragValue = capacitor.getPlateSize();
+        }
+        
+        @Override
+        protected void drag( PInputEvent event ) {
+            
+            // calculate the new model value, clamped to the range
+            Point2D mousePosition = getGlobalMousePosition( event );
+            double deltaX = mousePosition.getX() - startDragPosition.getX();
+            double deltaY = mousePosition.getY() - startDragPosition.getY();
+            
+            // only allow dragging down to the left, or up to the right
+            if ( ( deltaX < 0 && deltaY > 0 ) || ( deltaX > 0 && deltaY < 0 ) ) {
+                
+                double deltaView = 0;
+                if ( Math.abs( deltaX ) > Math.abs( deltaY ) ) {
+                    deltaView = -deltaX / Math.cos( angle );
+                }
+                else {
+                    deltaView = deltaY / Math.sin( angle );
+                }
+                
+                double deltaValue = mvt.viewToModel( deltaView );
+                double newValue =  startDragValue + deltaValue;
+                if ( newValue > valueRange.getMax() ) {
+                    newValue = valueRange.getMax();
+                }
+                else if ( newValue < valueRange.getMin() ) {
+                    newValue = valueRange.getMin();
+                }
+                
+                // update the model
+                capacitor.setPlateSize( newValue );
+            }
+        }
+        
+        /*
+         * Gets the mouse position in the global coordinate frame.
+         */
+        private Point2D getGlobalMousePosition( PInputEvent event ) {
+            Point2D pLocal = event.getPositionRelativeTo( event.getPickedNode() );
+            return event.getPickedNode().localToGlobal( pLocal );
+        }
     }
 }
