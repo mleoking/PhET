9de8628448522f9b851f4231e915dfe84466936d
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-05-07 18:37:22 +0000

    Trimmed the motion bounds for glucose when injected into the cell such that it doesn't overlap the cell membrane.

--- trunk/simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/LacY.java
+++ trunk/simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/model/LacY.java
@@ -130,7 +130,9 @@ public class LacY extends SimpleModelElement {
 					// The lactose has fully traversed the membrane.  Release
 					// it - it should now be inside the cell.
 					glucoseAttachmentPartner.detach(this);
-					glucoseAttachmentPartner.setMotionStrategy( new InjectionMotionStrategy(getModel().getInteriorMotionBounds(), new Vector2D.Double(0, -15)));
+					glucoseAttachmentPartner.setMotionStrategy( new InjectionMotionStrategy(
+							MotionBoundsTrimmer.trim(getModel().getInteriorMotionBounds(), glucoseAttachmentPartner),
+							new Vector2D.Double(0, -15)));
 					glucoseAttachmentPartner.setUpDraggableBounds(PositionWrtCell.INSIDE_CELL);
 					glucoseAttachmentPartner = null;
 					glucoseAttachmentState = AttachmentState.UNATTACHED_BUT_UNAVALABLE;
