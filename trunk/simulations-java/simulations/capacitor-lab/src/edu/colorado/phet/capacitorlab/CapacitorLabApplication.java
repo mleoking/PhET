acf180c3d5c6f62561b99d69da9f88c5aac38d12
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-05-10 19:18:24 +0000

    reduce width of Dielectric Constant text field

--- trunk/simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/CapacitorLabApplication.java
+++ trunk/simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/CapacitorLabApplication.java
@@ -57,6 +57,7 @@ public class CapacitorLabApplication extends PiccoloPhetApplication {
         for ( Module module : getModules() ) {
             maxWidth = Math.max( maxWidth, module.getControlPanel().getPreferredSize().width );
         }
+        System.out.println( "control panel width = " + maxWidth );//XXX
         for ( Module module : getModules() ) {
             module.getControlPanel().addControlFullWidth( Box.createHorizontalStrut( maxWidth ) );
         }
--- trunk/simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/control/DielectricConstantControl.java
+++ trunk/simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/control/DielectricConstantControl.java
@@ -14,13 +14,14 @@ public class DielectricConstantControl extends LinearValueControl {
     private static final double min = CLConstants.DIELECTRIC_CONSTANT_RANGE.getMin();
     private static final  double max = CLConstants.DIELECTRIC_CONSTANT_RANGE.getMax();
     private static final String label = CLStrings.LABEL_DIELECTRIC_CONSTANT;
-    private static final String textFieldPattern = "#0.000";
+    private static final String textFieldPattern = "0.000";
     private static final String units = "";
     
     public DielectricConstantControl( double value ) {
         super( min, max, label, textFieldPattern, units );
         setValue( value );
         setTickPattern( "0" );
+        setTextFieldColumns( 4 );
         setBorder( new EtchedBorder() );
     }
 }
