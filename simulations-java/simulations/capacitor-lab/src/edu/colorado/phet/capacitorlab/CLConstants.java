856260a1bebf0a14813eb4f2a7a61e73d233d474
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-05-10 20:21:29 +0000

    use ranges specified by Noah P, recalibrate locations and model-view transform

--- simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/CLConstants.java
+++ simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/CLConstants.java
@@ -35,28 +35,28 @@ public class CLConstants {
     // Model
     //----------------------------------------------------------------------------
     
-    public static final Point2D BATTERY_LOCATION = new Point2D.Double( -.130, 0 ); // meters
+    public static final Point2D BATTERY_LOCATION = new Point2D.Double( -.025, 0 ); // meters
     public static final DoubleRange BATTERY_VOLTAGE_RANGE = new DoubleRange( -10, 10, 0 ); // volts
     public static final boolean BATTERY_CONNECTED = true;
     
     public static final Point2D CAPACITOR_LOCATION = new Point2D.Double( 0, 0 ); // meters
-    public static final DoubleRange PLATE_SIZE_RANGE = new DoubleRange( .050, .100, .075 ); // meters
-    public static final DoubleRange PLATE_SEPARATION_RANGE = new DoubleRange( .010, .050, .020 ); // meters
-    public static final double PLATE_THICKNESS = .0015; // meters
+    public static final DoubleRange PLATE_SIZE_RANGE = new DoubleRange( .01, .02, .015 ); // meters
+    public static final DoubleRange PLATE_SEPARATION_RANGE = new DoubleRange( .001, .002, .0015 ); // meters
+    public static final double PLATE_THICKNESS = .0005; // meters
     
-    public static final DoubleRange DIELECTRIC_CONSTANT_RANGE = new DoubleRange( 1, 5, 1 ); // dimensionless
+    public static final DoubleRange DIELECTRIC_CONSTANT_RANGE = new DoubleRange( 1, 10, 1 ); // dimensionless
     public static final DoubleRange DIELECTRIC_OFFSET_RANGE = new DoubleRange( 0, PLATE_SIZE_RANGE.getMax(), 0 ); // meters
-    public static final double DIELECTRIC_GAP = 0.001; // meters
+    public static final double DIELECTRIC_GAP = 0.0002; // meters
     
-    public static final double WIRE_THICKNESS = .003; // meters
-    public static final double TOP_WIRE_EXTENT = Math.abs( .085 ); // how far the top wire extends above the capactor's origin, absolute value (meters)
+    public static final double WIRE_THICKNESS = .0005; // meters
+    public static final double TOP_WIRE_EXTENT = Math.abs( .016 ); // how far the top wire extends above the capactor's origin, absolute value (meters)
     public static final double BOTTOM_WIRE_EXTENT = TOP_WIRE_EXTENT; // how far the bottom wire extends below the capactor's origin, absolute value (meters)
     
     public static final double AIR_DIELECTRIC_CONSTANT = 1.00058986;
     
     // model-view transform
-    public static final double MVT_SCALE = 3000;
-    public static final Point2D MVT_OFFSET = new Point2D.Double( Math.abs( BATTERY_LOCATION.getX() ) + .030, .125 ); // meters
+    public static final double MVT_SCALE = 15000;
+    public static final Point2D MVT_OFFSET = new Point2D.Double( Math.abs( BATTERY_LOCATION.getX() ) + .005, .0275 ); // meters
     
     //----------------------------------------------------------------------------
     // View
