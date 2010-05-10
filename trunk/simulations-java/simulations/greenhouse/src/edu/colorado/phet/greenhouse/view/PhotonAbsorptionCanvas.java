11d358b5119874b86d2b7fd0053be96b9b2ea3a1
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-05-10 22:34:49 +0000

    Added an early sketch of this tab's specification to the canvas.

--- trunk/simulations-java/simulations/greenhouse/src/edu/colorado/phet/greenhouse/view/PhotonAbsorptionCanvas.java
+++ trunk/simulations-java/simulations/greenhouse/src/edu/colorado/phet/greenhouse/view/PhotonAbsorptionCanvas.java
@@ -5,13 +5,17 @@ package edu.colorado.phet.greenhouse.view;
 import java.awt.Color;
 import java.awt.Point;
 import java.awt.geom.Dimension2D;
+import java.awt.geom.Ellipse2D;
 import java.awt.geom.Point2D;
 
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform2D;
 import edu.colorado.phet.common.piccolophet.PhetPCanvas;
+import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.colorado.phet.greenhouse.GreenhouseDefaults;
+import edu.colorado.phet.greenhouse.GreenhouseResources;
 import edu.colorado.phet.neuron.module.NeuronDefaults;
 import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.nodes.PImage;
 
 /**
  * Canvas on which the neuron simulation is depicted.
@@ -54,14 +58,21 @@ public class PhotonAbsorptionCanvas extends PhetPCanvas {
         				4,  // Scale factor - smaller numbers "zoom out", bigger ones "zoom in".
         				true);
 
-        setBackground( Color.BLACK );
+        setBackground( new Color(153, 204, 255) );
 
         // Create the node that will be the root for all the world children on
         // this canvas.  This is done to make it easier to zoom in and out on
         // the world without affecting screen children.
         myWorldNode = new PNode();
         addWorldChild(myWorldNode);
-
+        
+        // TODO: This is temporary.  Add a node that is a sketch of what this
+        // will really look like at some point.
+        PImage sketchNode = new PImage(GreenhouseResources.getImage("photon-absorption-sketch-01.png"));
+        sketchNode.setScale(1300 / sketchNode.getFullBoundsReference().width);
+        sketchNode.setOffset(-200, 0);
+        myWorldNode.addChild(sketchNode);
+        
         // Update the layout.
         updateLayout();
         
