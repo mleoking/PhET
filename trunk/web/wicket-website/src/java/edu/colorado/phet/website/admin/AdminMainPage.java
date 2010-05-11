78acd9ff8e92d1d58c233ba73ea160e1d1e573ab
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-05-11 20:18:04 +0000

    Wicket-website: added debugging option to reverse double single-quotes

--- trunk/web/wicket-website/src/java/edu/colorado/phet/website/admin/AdminMainPage.java
+++ trunk/web/wicket-website/src/java/edu/colorado/phet/website/admin/AdminMainPage.java
@@ -2,6 +2,7 @@ package edu.colorado.phet.website.admin;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.apache.wicket.PageParameters;
@@ -10,15 +11,19 @@ import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.util.value.ValueMap;
+import org.hibernate.Session;
 
 import edu.colorado.phet.buildtools.BuildLocalProperties;
 import edu.colorado.phet.buildtools.util.FileUtils;
 import edu.colorado.phet.buildtools.util.PhetJarSigner;
 import edu.colorado.phet.website.PhetWicketApplication;
 import edu.colorado.phet.website.components.StringTextField;
+import edu.colorado.phet.website.data.TranslatedString;
 import edu.colorado.phet.website.data.transfer.TransferData;
 import edu.colorado.phet.website.notification.NotificationHandler;
 import edu.colorado.phet.website.translation.PhetLocalizer;
+import edu.colorado.phet.website.util.HibernateTask;
+import edu.colorado.phet.website.util.HibernateUtils;
 import edu.colorado.phet.website.util.SearchUtils;
 import edu.colorado.phet.website.util.StringUtils;
 
@@ -81,6 +86,27 @@ public class AdminMainPage extends AdminPage {
                 catch( IOException e ) {
                     e.printStackTrace();
                 }
+            }
+        } );
+
+        add( new Link( "debug-ticks" ) {
+            public void onClick() {
+                HibernateUtils.wrapTransaction( getHibernateSession(), new HibernateTask() {
+                    public boolean run( Session session ) {
+                        List strings = session.createQuery( "select ts from TranslatedString as ts" ).list();
+                        for ( Object o : strings ) {
+                            TranslatedString string = (TranslatedString) o;
+                            if ( string.getValue().contains( "''" ) ) {
+                                String newVal = string.getValue().replace( "''", "'" );
+                                logger.info( "String " + string.getKey() + " in translation " + string.getTranslation().getId() + " removing double single-quotes from \"" + string.getValue() + "\" to \"" + newVal + "\"" );
+                                string.setValue( newVal );
+                                session.update( string );
+                                PhetLocalizer.get().updateCachedString( string.getTranslation(), string.getKey(), string.getValue() );
+                            }
+                        }
+                        return true;
+                    }
+                } );
             }
         } );
     }
