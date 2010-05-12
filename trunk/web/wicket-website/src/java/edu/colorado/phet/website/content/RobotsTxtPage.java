fb8c5eb5cba45111421f9a6c71aa7874795a8143
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-05-12 10:17:38 +0000

    Wicket-website: docs

--- trunk/web/wicket-website/src/java/edu/colorado/phet/website/content/RobotsTxtPage.java
+++ trunk/web/wicket-website/src/java/edu/colorado/phet/website/content/RobotsTxtPage.java
@@ -6,6 +6,12 @@ import org.apache.wicket.markup.html.basic.Label;
 import edu.colorado.phet.website.PhetWicketApplication;
 import edu.colorado.phet.website.util.PhetRequestCycle;
 
+/**
+ * Displays the robots.txt response. Content depends on whether we are running on the production server (allow indexing)
+ * or any other server (disallow indexing).
+ * <p/>
+ * See http://en.wikipedia.org/wiki/Robots_exclusion_standard for more information on robots.txt
+ */
 public class RobotsTxtPage extends WebPage {
     public RobotsTxtPage() {
         String server = PhetRequestCycle.get().getWebRequest().getHttpServletRequest().getServerName();
