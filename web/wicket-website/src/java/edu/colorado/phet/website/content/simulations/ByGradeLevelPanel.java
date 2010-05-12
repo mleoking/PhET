30fbe6063d0b279b056a7174def6ecf11674ef03
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-05-12 08:17:03 +0000

    Wicket-website: added by-grade-level thumbnailed links

--- web/wicket-website/src/java/edu/colorado/phet/website/content/simulations/ByGradeLevelPanel.java
+++ web/wicket-website/src/java/edu/colorado/phet/website/content/simulations/ByGradeLevelPanel.java
@@ -8,6 +8,13 @@ public class ByGradeLevelPanel extends PhetPanel {
     public ByGradeLevelPanel( String id, final PageContext context ) {
         super( id, context );
 
+        // TODO: localize (alt attributes)
+
+        add( getNavMenu().getLocationByKey( "elementary-school" ).getLink( "elementary-school-link", context, getPhetCycle() ) );
+        add( getNavMenu().getLocationByKey( "middle-school" ).getLink( "middle-school-link", context, getPhetCycle() ) );
+        add( getNavMenu().getLocationByKey( "high-school" ).getLink( "high-school-link", context, getPhetCycle() ) );
+        add( getNavMenu().getLocationByKey( "university" ).getLink( "university-link", context, getPhetCycle() ) );
+
     }
 
 }
