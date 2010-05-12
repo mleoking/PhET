b68299440b11a57bbf6e6c98bc44d74edb4dec94
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-05-12 02:08:31 +0000

    Wicket-website: docs

--- web/wicket-website/src/java/edu/colorado/phet/website/util/links/RawLinkable.java
+++ web/wicket-website/src/java/edu/colorado/phet/website/util/links/RawLinkable.java
@@ -6,6 +6,16 @@ import edu.colorado.phet.website.util.PhetRequestCycle;
 public interface RawLinkable extends Linkable {
     public String getRawUrl( PageContext context, PhetRequestCycle cycle );
 
+    /**
+     * Returns a string that can be inserted into MessageFormat style strings like:
+     * <code><pre>
+     * <a {0}>link</a>
+     * </pre></code>
+     *
+     * @param context The page context
+     * @param cycle   The request cycle
+     * @return A string that looks like href="...."
+     */
     public String getHref( PageContext context, PhetRequestCycle cycle );
 
     public String getDefaultRawUrl();
