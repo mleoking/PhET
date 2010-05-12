38186d9a707b65ff34f59c7d1e902b629fb7a629
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-05-12 02:46:57 +0000

    Wicket-website: re-work of search results. i18n. added # of results. separated out into a panel

--- web/wicket-website/src/java/edu/colorado/phet/website/content/search/SearchResultsPage.java
+++ web/wicket-website/src/java/edu/colorado/phet/website/content/search/SearchResultsPage.java
@@ -2,27 +2,13 @@ package edu.colorado.phet.website.content.search;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
-import java.util.LinkedList;
-import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.apache.wicket.PageParameters;
-import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.link.Link;
-import org.apache.wicket.markup.html.list.ListItem;
-import org.apache.wicket.markup.html.list.ListView;
 
-import edu.colorado.phet.website.components.InvisibleComponent;
-import edu.colorado.phet.website.components.LocalizedText;
-import edu.colorado.phet.website.content.contribution.ContributionPage;
-import edu.colorado.phet.website.content.simulations.SimulationPage;
-import edu.colorado.phet.website.data.LocalizedSimulation;
-import edu.colorado.phet.website.data.contribution.Contribution;
 import edu.colorado.phet.website.templates.PhetRegularPage;
-import edu.colorado.phet.website.translation.PhetLocalizer;
 import edu.colorado.phet.website.util.PageContext;
 import edu.colorado.phet.website.util.PhetUrlMapper;
-import edu.colorado.phet.website.util.SearchUtils;
 import edu.colorado.phet.website.util.StringUtils;
 import edu.colorado.phet.website.util.links.AbstractLinker;
 import edu.colorado.phet.website.util.links.RawLinkable;
@@ -34,50 +20,18 @@ public class SearchResultsPage extends PhetRegularPage {
     public SearchResultsPage( PageParameters parameters ) {
         super( parameters );
 
-        // TODO: redo localization
-
         String query = parameters.getString( "q" );
 
         initializeLocation( getNavMenu().getLocationByKey( "search.results" ) );
 
-        List<LocalizedSimulation> lsims = new LinkedList<LocalizedSimulation>();
-        List<Contribution> contributions = new LinkedList<Contribution>();
-
-        PhetLocalizer localizer = (PhetLocalizer) getLocalizer();
-        // TODO: display query
-
         if ( query != null ) {
-            lsims = SearchUtils.simulationSearch( getHibernateSession(), query, getPageContext().getLocale() );
-            contributions = SearchUtils.contributionSearch( getHibernateSession(), query, getPageContext().getLocale() );
-        }
-
-        //add( new SimulationDisplayPanel( "search-results-panel", getPageContext(), lsims ) );
-        add( new ListView( "sims", lsims ) {
-            protected void populateItem( ListItem item ) {
-                LocalizedSimulation lsim = (LocalizedSimulation) item.getModel().getObject();
-                Link link = SimulationPage.getLinker( lsim ).getLink( "sim-link", getPageContext(), getPhetCycle() );
-                item.add( link );
-                link.add( new Label( "sim-title", lsim.getTitle() ) );
-            }
-        } );
-        add( new ListView( "contribs", contributions ) {
-            protected void populateItem( ListItem item ) {
-                Contribution contribution = (Contribution) item.getModel().getObject();
-                Link link = ContributionPage.getLinker( contribution ).getLink( "contrib-link", getPageContext(), getPhetCycle() );
-                item.add( link );
-                link.add( new Label( "contrib-title", contribution.getTitle() ) );
-            }
-        } );
-
-
-        if ( query != null ) {
-            addTitle( StringUtils.messageFormat( localizer.getString( "search.title", this ), query ) );
-            add( new LocalizedText( "search-query", "search.query", new Object[]{query} ) );
+            addTitle( StringUtils.messageFormat( getPhetLocalizer().getString( "search.title", this ), query ) );
         }
         else {
-            addTitle( StringUtils.messageFormat( localizer.getString( "search.title", this ), "-" ) );
-            add( new InvisibleComponent( "search-query" ) );
+            addTitle( StringUtils.messageFormat( getPhetLocalizer().getString( "search.title", this ), "-" ) );
         }
+
+        add( new SearchResultsPanel( "search-results-panel", getPageContext(), query ) );
 
     }
 
