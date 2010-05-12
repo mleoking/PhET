49a420464a1429aaf930d2fac09c34e0bdd0f23d
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-05-12 11:25:09 +0000

    Wicket-website: docs

--- web/wicket-website/src/java/edu/colorado/phet/website/components/StaticImage.java
+++ web/wicket-website/src/java/edu/colorado/phet/website/components/StaticImage.java
@@ -4,24 +4,47 @@ import org.apache.wicket.markup.ComponentTag;
 import org.apache.wicket.markup.html.WebComponent;
 import org.apache.wicket.model.IModel;
 
+/**
+ * Handles an image with src and alt attributes. wicket:id should be placed upon the img tag itself, and then
+ * the existing (or nonexisting) attributes will be overwritten.
+ */
 public class StaticImage extends WebComponent {
 
     private String url = null;
     private String alt = null;
     private IModel altModel = null;
 
-    public StaticImage( String id, String url, String alt ) {
+    /**
+     * Create an image with a standard URL for a src attribute.
+     *
+     * @param id  The Wicket ID for the image
+     * @param src The src attribute (URL)
+     * @param alt The alt text for accessibility
+     */
+    public StaticImage( String id, String src, String alt ) {
         super( id );
-        this.url = url;
+        this.url = src;
         this.alt = alt;
     }
 
-    public StaticImage( String id, String url, String alt, IModel altModel ) {
+    /**
+     * Create an image with a model for an alt attribute.
+     *
+     * @param id       The Wicket ID for the image
+     * @param src      The src attribute (URL)
+     * @param alt      Should be null. Exists to differentiate between overloaded constructors
+     * @param altModel The alt model. Will be converted to a string for accessibility
+     */
+    public StaticImage( String id, String src, String alt, IModel altModel ) {
         super( id );
-        this.url = url;
+        this.url = src;
         this.alt = alt;
         this.altModel = altModel;
     }
+
+    /*---------------------------------------------------------------------------*
+    * implementation
+    *----------------------------------------------------------------------------*/
 
     protected void onComponentTag( ComponentTag tag ) {
         checkComponentTag( tag, "img" );
