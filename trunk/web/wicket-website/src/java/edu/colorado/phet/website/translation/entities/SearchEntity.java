d90b0c4c4c9685cbe9037581045c2850eb2832ef
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-05-12 02:48:24 +0000

    Wicket-website: added search preview to website translation

--- trunk/web/wicket-website/src/java/edu/colorado/phet/website/translation/entities/SearchEntity.java
+++ trunk/web/wicket-website/src/java/edu/colorado/phet/website/translation/entities/SearchEntity.java
@@ -1,5 +1,11 @@
 package edu.colorado.phet.website.translation.entities;
 
+import edu.colorado.phet.website.content.search.SearchResultsPanel;
+import edu.colorado.phet.website.panels.PhetPanel;
+import edu.colorado.phet.website.translation.PhetPanelFactory;
+import edu.colorado.phet.website.util.PageContext;
+import edu.colorado.phet.website.util.PhetRequestCycle;
+
 public class SearchEntity extends TranslationEntity {
     public SearchEntity() {
         addString( "nav.search.results" );
@@ -11,6 +17,12 @@ public class SearchEntity extends TranslationEntity {
         addString( "search.clickSearch" );
         addString( "search.simulations" );
         addString( "search.activities" );
+
+        addPreview( new PhetPanelFactory() {
+            public PhetPanel getNewPanel( String id, PageContext context, PhetRequestCycle requestCycle ) {
+                return new SearchResultsPanel( id, context, "electricity" );
+            }
+        }, "Search Results" );
     }
 
     public String getDisplayName() {
