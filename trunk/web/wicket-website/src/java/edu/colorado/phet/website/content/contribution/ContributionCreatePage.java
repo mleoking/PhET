459330e2c9d6abf2bebe5625e0d21e7666a5cabb
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-05-12 03:13:55 +0000

    Wicket-website: added a message to simulation panel about adding ideas. added a message to activity submission saying to check the guidelines

--- trunk/web/wicket-website/src/java/edu/colorado/phet/website/content/contribution/ContributionCreatePage.java
+++ trunk/web/wicket-website/src/java/edu/colorado/phet/website/content/contribution/ContributionCreatePage.java
@@ -5,6 +5,7 @@ import org.apache.wicket.PageParameters;
 import org.apache.wicket.model.ResourceModel;
 
 import edu.colorado.phet.website.authentication.AuthenticatedPage;
+import edu.colorado.phet.website.components.LocalizedText;
 import edu.colorado.phet.website.data.contribution.Contribution;
 import edu.colorado.phet.website.panels.contribution.ContributionEditPanel;
 import edu.colorado.phet.website.templates.PhetRegularPage;
@@ -27,6 +28,11 @@ public class ContributionCreatePage extends PhetRegularPage {
         initializeLocation( getNavMenu().getLocationByKey( "teacherIdeas.submit" ) );
 
         add( new ContributionEditPanel( "contribution-edit-panel", getPageContext(), new Contribution() ) );
+
+        add( new LocalizedText( "check-guidelines", "contribution.create.checkGuidelines", new Object[]{
+                ContributionGuidelinesPanel.getLinker().getHref( getPageContext(), getPhetCycle() ),
+                "href=\"/publications/activities-guide/contribution-guidelines.pdf\""
+        } ) );
     }
 
     public static void addToMapper( PhetUrlMapper mapper ) {
