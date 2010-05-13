bc11b1a7b8bd5e0eaeb8d3d5598944af4ea04717
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-05-13 20:38:29 +0000

    add a dummy second module (tab) to take up vertical space

--- simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/prototype/MagnifyingGlassPrototypeApplication.java
+++ simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/prototype/MagnifyingGlassPrototypeApplication.java
@@ -3,9 +3,12 @@
 package edu.colorado.phet.acidbasesolutions.prototype;
 
 import edu.colorado.phet.acidbasesolutions.ABSConstants;
+import edu.colorado.phet.common.phetcommon.application.Module;
 import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
 import edu.colorado.phet.common.phetcommon.application.PhetApplicationLauncher;
+import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
 import edu.colorado.phet.common.piccolophet.PiccoloPhetApplication;
+import edu.umd.cs.piccolo.PCanvas;
 
 /**
  * Main application for the Magnifying Glass prototype.
@@ -17,6 +20,15 @@ public class MagnifyingGlassPrototypeApplication extends PiccoloPhetApplication 
     public MagnifyingGlassPrototypeApplication( PhetApplicationConfig config ) {
         super( config );
         addModule( new MGPModule( getPhetFrame(), isDeveloperControlsEnabled() ) );
+        addModule( new EmptyModule() );
+    }
+    
+    public static class EmptyModule extends Module {
+        public EmptyModule() {
+            super( "Some Other Tab", new ConstantDtClock( 1000, 1 ) );
+            setSimulationPanel( new PCanvas() );
+            setClockControlPanel( null );
+        }
     }
 
     public static void main( final String[] args ) throws ClassNotFoundException {
