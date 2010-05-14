ab2923613a7675e2066700e8cc65b3f070fc2940
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-05-14 07:37:22 +0000

    ported timeline to java, see #2092

---
+++ trunk/simulations-java/common/record-and-playback/src/edu/colorado/phet/recordandplayback/gui/RecordAndPlaybackControlPanel.java
@@ -0,0 +1,11 @@
+package edu.colorado.phet.recordandplayback.gui;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Sam
+ * Date: May 14, 2010
+ * Time: 12:47:34 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class RecordAndPlaybackControlPanel {
+}
