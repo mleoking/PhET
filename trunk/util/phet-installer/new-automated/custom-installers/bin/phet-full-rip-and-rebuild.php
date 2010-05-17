006d421dec19094269eb6bf892d6aab5a273b677
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-05-17 18:47:13 +0000

    Initial submission of this file, leveraged highly from similar scripts for KSU and YF installers.

---
+++ trunk/util/phet-installer/new-automated/custom-installers/bin/phet-full-rip-and-rebuild.php
@@ -0,0 +1,77 @@
+<?php
+
+    //=========================================================================
+    // This file contains a script that rips (i.e. creates a local copy or
+    // "mirror" of) the of the PhET web site, makes modifications to the files,
+    // and then rebuilds the PhET installers that enable the installation of a
+    // local mirror of the site on a user's machine.
+    //=========================================================================
+
+    //--------------------------------------------------------------------------
+    // Inclusions of other PHP files.
+    //--------------------------------------------------------------------------
+
+    require_once("config.php");
+    require_once("global.php");
+    require_once("file-util.php");
+    require_once("installer-util.php");
+    require_once("ripper-util.php");
+
+    //-------------------------------------------------------------------------
+    // Rip the web site and rebuild the installer.
+    //-------------------------------------------------------------------------
+    function full_rip_and_rebuild() {
+
+        // Grab a file lock to prevent multiple simultaneous executions.
+        if ( !file_lock( LOCK_FILE_STEM_NAME ) ){
+            flushing_echo("ERROR: The PhET installer builder appears to be completing another build.");
+            flushing_echo("If you believe this to be incorrect, use the appropriate script to force");
+            flushing_echo("an unlock (something like \"force-unlock.sh\") and try again.");
+            return;
+        }
+
+        // Log the start time of this operation.
+        $start_time = exec("date");
+        flushing_echo("Starting full rip and rebuild of PhET installers at time $start_time");
+
+        // Remove previous copy of web site.
+        ripper_remove_website_copy();
+
+        // Rip the web site.
+        ripper_rip_website("PHET");
+        //ripper_rip_website_subset();
+        ripper_download_sims();
+
+        // Log the time at which the rip completed.
+        $rip_finish_time = exec("date");
+        flushing_echo("Rip completed at time $start_time");
+
+        // Make sure permissions of the ripped website are correct.
+        file_chmod_recursive( RIPPED_WEBSITE_ROOT, 0775, 0775 );
+
+        // Incorporate the timestamp information that indicates when this
+        // version of the installer was created.
+        installer_create_marker_file();
+
+        // Insert the creation time into the marker file.  Note that there is
+        // no distribution tag for the standard PhET installers.
+        installer_insert_installer_creation_time( null );
+
+        // Build the local installers, meaning installers that can be used to
+        // install a local mirror of the PhET web site.
+        installer_build_local_mirror_installers(BITROCK_PHET_LOCAL_MIRROR_BUILDFILE);
+
+        // Output the time of completion.
+        $end_time = exec("date");
+        flushing_echo("\nCompleted rebuild at time $end_time");
+
+        // Release the lock.
+        file_unlock( LOCK_FILE_STEM_NAME );
+    }
+
+    //--------------------------------------------------------------------------
+    // Entry point for this PHP file.
+    //--------------------------------------------------------------------------
+    full_rip_and_rebuild();
+
+?>
