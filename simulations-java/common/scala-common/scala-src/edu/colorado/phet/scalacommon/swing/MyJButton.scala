d2892474a370e53c4b4384bda61cd4a0006d8c19
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-05-19 21:55:18 +0000

    motion-series: improve documentation

--- simulations-java/common/scala-common/scala-src/edu/colorado/phet/scalacommon/swing/MyJButton.scala
+++ simulations-java/common/scala-common/scala-src/edu/colorado/phet/scalacommon/swing/MyJButton.scala
@@ -1,9 +1,9 @@
 package edu.colorado.phet.scalacommon.swing
 
-
 import java.awt.event.{ActionEvent, ActionListener}
 import javax.swing.JButton
 
+//Todo: how about using Button instead; it looks like it has good support for this
 class MyJButton(text:String,actionListener:()=>Unit) extends JButton(text){
   addActionListener(new ActionListener(){
     def actionPerformed(e: ActionEvent) = actionListener()
