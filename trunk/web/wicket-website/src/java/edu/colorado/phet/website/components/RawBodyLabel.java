e4c51520d58a1ddd9ca691ad2c26068281afe52f
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-05-21 19:41:38 +0000

    Wicket-website: raw body label docs

--- trunk/web/wicket-website/src/java/edu/colorado/phet/website/components/RawBodyLabel.java
+++ trunk/web/wicket-website/src/java/edu/colorado/phet/website/components/RawBodyLabel.java
@@ -2,6 +2,14 @@ package edu.colorado.phet.website.components;
 
 import org.apache.wicket.model.IModel;
 
+/**
+ * Inserts raw text without escaping or the original surrounding tags.
+ * <p/>
+ * Example HTML: <span wicket:id="test">Test Label</span>
+ * Example Java: add( new RawBodyLabel( "test", "Test 1 &amp; 2" ) );
+ * <p/>
+ * Example HTML result: Test 1 &amp; 2
+ */
 public class RawBodyLabel extends RawLabel {
     public RawBodyLabel( String id ) {
         super( id );
