599cabd7728af0c15ee7d9c363c43773d9c38af3
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-05-21 19:20:23 +0000

    Wicket-website: added documentation template

---
+++ trunk/web/wicket-website/src/java/edu/colorado/phet/website/admin/doc/TechnicalDocPage.java
@@ -0,0 +1,21 @@
+package edu.colorado.phet.website.admin.doc;
+
+import org.apache.log4j.Logger;
+import org.apache.wicket.PageParameters;
+import org.apache.wicket.markup.html.form.TextField;
+
+import edu.colorado.phet.website.admin.AdminPage;
+
+public class TechnicalDocPage extends AdminPage {
+
+    private TextField keyText;
+    private TextField valueText;
+
+    private static Logger logger = Logger.getLogger( TechnicalDocPage.class.getName() );
+
+    public TechnicalDocPage( PageParameters parameters ) {
+        super( parameters );
+
+    }
+
+}
