d94b9b858a80eba1df4e90e255ff1d807a7d4e25
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-05-23 21:07:38 +0000

    Wicket-website: added more changelog tests

--- trunk/web/wicket-website/src/test/edu/colorado/phet/website/tests/ChangelogTests.java
+++ trunk/web/wicket-website/src/test/edu/colorado/phet/website/tests/ChangelogTests.java
@@ -3,7 +3,6 @@ package edu.colorado.phet.website.tests;
 import org.junit.Test;
 
 import edu.colorado.phet.website.data.Changelog;
-import edu.colorado.phet.website.util.StringUtils;
 
 import static org.junit.Assert.assertTrue;
 
@@ -25,8 +24,43 @@ public class ChangelogTests {
 
         System.out.println( "*** non-dev log: \n" + log + "\n\n" );
 
+        /*---------------------------------------------------------------------------*
+        * development log tests
+        *----------------------------------------------------------------------------*/
+
         assertTrue( devLog.getEntries().size() == 3 );
+        Changelog.Entry dev1 = devLog.getEntries().get( 0 );
+        assertTrue( dev1.getMajorVersion() == 3 );
+        assertTrue( dev1.getDevVersion() == 0 );
+        assertTrue( dev1.getRevision() == 38666 );
+        assertTrue( dev1.getDate() != null );
+        assertTrue( dev1.getLines().size() == 1 );
+        assertTrue( !dev1.getLines().get( 0 ).isVisible() );
+        assertTrue( dev1.getLines().get( 0 ).getMessage().equals( "Deployment to production server." ) );
+        Changelog.Entry dev2 = devLog.getEntries().get( 1 );
+        assertTrue( dev2.getLines().size() == 1 );
+        Changelog.Line devE = dev2.getLines().get( 0 );
+        assertTrue( devE.isVisible() );
+        assertTrue( devE.getDate() != null );
+        assertTrue( devE.getMessage().equals( "Fixed an issue where U-238 was shown decaying into Pb-207 instead of Pb-206." ) );
+
+        /*---------------------------------------------------------------------------*
+        * visible log tests
+        *----------------------------------------------------------------------------*/
+
         assertTrue( log.getEntries().size() == 2 );
+        Changelog.Entry e1 = devLog.getEntries().get( 0 );
+        assertTrue( e1.getMajorVersion() == 3 );
+        assertTrue( e1.getDevVersion() == 0 );
+        assertTrue( e1.getRevision() == 38666 );
+        assertTrue( e1.getDate() != null );
+        assertTrue( e1.getLines().size() == 1 );
+        for ( Changelog.Entry entry : log.getEntries() ) {
+            assertTrue( entry.getDevVersion() == 0 );
+            for ( Changelog.Line line : entry.getLines() ) {
+                assertTrue( line.isVisible() );
+            }
+        }
 
     }
 }
