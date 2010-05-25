56675749ba729f41d2943b9e1ca627b999f802d7
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-05-25 21:24:00 +0000

    Initial submission of this class, to which the existing model code will eventually be ported.

---
+++ simulations-java/simulations/greenhouse/src/edu/colorado/phet/greenhouse/model/GreenhouseEffectModel.java
@@ -0,0 +1,23 @@
+/* Copyright 2009, University of Colorado */
+
+package edu.colorado.phet.greenhouse.model;
+
+/**
+ * Main model for the Greenhouse Effect.  This class is the central point
+ * that the view and control use when interacting with the model, and as such
+ * it contains and controls all other model components, such as photons,
+ * clouds, etc.
+ * 
+ * @author Ron LeMaster, John Blanco
+ *
+ */
+public class GreenhouseEffectModel {
+
+	public GreenhouseEffectModel(GreenhouseClock clock){
+		
+	}
+	
+	private void stepInTime(double dt){
+		
+	}
+}
