88ba94985604a17cce2f5f652b56b2118eb645d0
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-05-25 06:12:17 +0000

    Wicket-website: adding divs around insides of necessary forms for validation

--- trunk/web/wicket-website/src/java/edu/colorado/phet/website/panels/SearchPanel.java
+++ trunk/web/wicket-website/src/java/edu/colorado/phet/website/panels/SearchPanel.java
@@ -10,8 +10,6 @@ public class SearchPanel extends PhetPanel {
     public SearchPanel( String id, PageContext context ) {
         super( id, context );
 
-        //add( AboutSponsorsPanel.getLinker().getLink( "sponsors-link", context, getPhetCycle() ) );
-
         WebMarkupContainerWithAssociatedMarkup form = new WebMarkupContainerWithAssociatedMarkup( "search-form" );
         form.add( new AttributeAppender( "action", true, new Model( context.getPrefix() + "search" ), " " ) );
         add( form );
