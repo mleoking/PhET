76906a70b1ea92dbcbec585c0ee728d9a6764475
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-05-25 19:54:57 +0000

    Wicket-website: hide nav list if empty for validation

--- trunk/web/wicket-website/src/java/edu/colorado/phet/website/menu/NavMenuList.java
+++ trunk/web/wicket-website/src/java/edu/colorado/phet/website/menu/NavMenuList.java
@@ -13,6 +13,7 @@ import org.apache.wicket.model.Model;
 import org.apache.wicket.model.ResourceModel;
 
 import edu.colorado.phet.website.DistributionHandler;
+import edu.colorado.phet.website.components.InvisibleComponent;
 import edu.colorado.phet.website.components.RawLabel;
 import edu.colorado.phet.website.constants.CSS;
 import edu.colorado.phet.website.panels.PhetPanel;
@@ -24,62 +25,66 @@ public class NavMenuList extends PhetPanel {
     public NavMenuList( String id, final PageContext context, List<NavLocation> locations, final Set<NavLocation> currentLocations, final int level ) {
         super( id, context );
 
-        ListView listView = new ListView<NavLocation>( "items", locations ) {
-            protected void populateItem( ListItem item ) {
-                NavLocation location = (NavLocation) item.getModel().getObject();
-                Link link = location.getLink( "link", context, (PhetRequestCycle) getRequestCycle() );
+        if ( locations.isEmpty() ) {
+            add( new InvisibleComponent( "items" ) );
+        }
+        else {
+            add( new ListView<NavLocation>( "items", locations ) {
+                protected void populateItem( ListItem item ) {
+                    NavLocation location = (NavLocation) item.getModel().getObject();
+                    Link link = location.getLink( "link", context, (PhetRequestCycle) getRequestCycle() );
 
-                RawLabel label = new RawLabel( "link-label", new ResourceModel( location.getLocalizationKey() ) );
+                    RawLabel label = new RawLabel( "link-label", new ResourceModel( location.getLocalizationKey() ) );
 
-                boolean open = false;
+                    boolean open = false;
 
-                if ( currentLocations != null ) {
-                    for ( NavLocation currentLocation : currentLocations ) {
-                        if ( !open ) {
-                            open = currentLocation.isUnderLocation( location );
+                    if ( currentLocations != null ) {
+                        for ( NavLocation currentLocation : currentLocations ) {
+                            if ( !open ) {
+                                open = currentLocation.isUnderLocation( location );
+                            }
+                            if ( currentLocation.getBaseKey().equals( location.getKey() ) || currentLocation.getKey().equals( location.getKey() ) ) {
+                                label.add( new AttributeAppender( "class", new Model<String>( "selected" ), " " ) );
+                            }
                         }
-                        if ( currentLocation.getBaseKey().equals( location.getKey() ) || currentLocation.getKey().equals( location.getKey() ) ) {
-                            label.add( new AttributeAppender( "class", new Model<String>( "selected" ), " " ) );
+                    }
+
+                    // adds class so we can remove these for things like the installer and whatnot in CSS
+                    if ( location.isUnderLocationKey( "get-phet" ) ) {
+                        label.add( new AttributeAppender( "class", new Model<String>( "get-phet-item" ), " " ) );
+                    }
+                    if ( location.isUnderLocationKey( "teacherIdeas" ) ) {
+                        label.add( new AttributeAppender( "class", new Model<String>( "teacher-ideas-item" ), " " ) );
+                        if ( !DistributionHandler.displayContributions( getPhetCycle() ) ) {
+                            item.setVisible( false );
                         }
                     }
-                }
-
-                // adds class so we can remove these for things like the installer and whatnot in CSS
-                if ( location.isUnderLocationKey( "get-phet" ) ) {
-                    label.add( new AttributeAppender( "class", new Model<String>( "get-phet-item" ), " " ) );
-                }
-                if ( location.isUnderLocationKey( "teacherIdeas" ) ) {
-                    label.add( new AttributeAppender( "class", new Model<String>( "teacher-ideas-item" ), " " ) );
-                    if ( !DistributionHandler.displayContributions( getPhetCycle() ) ) {
-                        item.setVisible( false );
+                    if ( location.isUnderLocationKey( "forTranslators.website" ) ) {
+                        if ( DistributionHandler.hideWebsiteTranslations( getPhetCycle() ) ) {
+                            item.setVisible( false );
+                        }
                     }
-                }
-                if ( location.isUnderLocationKey( "forTranslators.website" ) ) {
-                    if ( DistributionHandler.hideWebsiteTranslations( getPhetCycle() ) ) {
-                        item.setVisible( false );
+
+                    link.add( label );
+
+                    item.add( link );
+
+                    if ( location.getChildren().isEmpty() || !open ) {
+                        Label placeholder = new Label( "children", "BOO" );
+                        placeholder.setVisible( false );
+                        item.add( placeholder );
                     }
+                    else {
+                        NavMenuList children = new NavMenuList( "children", context, location.getVisibleChildren( currentLocations ), currentLocations, level + 1 );
+                        children.setRenderBodyOnly( true );
+                        item.add( children );
+                    }
+
+                    link.add( new AttributeAppender( "class", new Model<String>( "nav" + level ), " " ) );
+
                 }
-
-                link.add( label );
-
-                item.add( link );
-
-                if ( location.getChildren().isEmpty() || !open ) {
-                    Label placeholder = new Label( "children", "BOO" );
-                    placeholder.setVisible( false );
-                    item.add( placeholder );
-                }
-                else {
-                    NavMenuList children = new NavMenuList( "children", context, location.getVisibleChildren( currentLocations ), currentLocations, level + 1 );
-                    children.setRenderBodyOnly( true );
-                    item.add( children );
-                }
-
-                link.add( new AttributeAppender( "class", new Model<String>( "nav" + level ), " " ) );
-
-            }
-        };
-        add( listView );
+            } );
+        }
 
         add( HeaderContributor.forCss( CSS.NAV_MENU ) );
     }
