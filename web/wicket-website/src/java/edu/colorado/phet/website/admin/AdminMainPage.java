9ace371e6e21222243bd771c0b29e4584cd2a266
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-05-29 07:33:19 +0000

    Wicket-website: proper jul-slf4j-log4j routing and testing

--- web/wicket-website/src/java/edu/colorado/phet/website/admin/AdminMainPage.java
+++ web/wicket-website/src/java/edu/colorado/phet/website/admin/AdminMainPage.java
@@ -2,13 +2,17 @@ package edu.colorado.phet.website.admin;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.StringWriter;
 import java.util.List;
 
 import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+import org.apache.log4j.WriterAppender;
 import org.apache.wicket.PageParameters;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.util.value.ValueMap;
 import org.hibernate.Session;
@@ -17,20 +21,20 @@ import edu.colorado.phet.buildtools.BuildLocalProperties;
 import edu.colorado.phet.buildtools.util.FileUtils;
 import edu.colorado.phet.buildtools.util.PhetJarSigner;
 import edu.colorado.phet.website.PhetWicketApplication;
+import edu.colorado.phet.website.components.RawLabel;
 import edu.colorado.phet.website.components.StringTextField;
 import edu.colorado.phet.website.data.TranslatedString;
 import edu.colorado.phet.website.data.transfer.TransferData;
 import edu.colorado.phet.website.notification.NotificationHandler;
 import edu.colorado.phet.website.translation.PhetLocalizer;
-import edu.colorado.phet.website.util.HibernateTask;
-import edu.colorado.phet.website.util.HibernateUtils;
-import edu.colorado.phet.website.util.SearchUtils;
-import edu.colorado.phet.website.util.StringUtils;
+import edu.colorado.phet.website.util.*;
 
 public class AdminMainPage extends AdminPage {
 
     private TextField keyText;
     private TextField valueText;
+
+    private String logTest = "";
 
     private static final Logger logger = Logger.getLogger( AdminMainPage.class.getName() );
 
@@ -109,6 +113,28 @@ public class AdminMainPage extends AdminPage {
                 } );
             }
         } );
+
+        add( new Link( "debug-logging" ) {
+            @Override
+            public void onClick() {
+                StringWriter writer = new StringWriter();
+                WriterAppender appender = new WriterAppender( new PatternLayout( "%d{DATE} %5p %25c{1} - %m%n" ), writer );
+                Logger log4jLogger = Logger.getLogger( AdminMainPage.class );
+                log4jLogger.addAppender( appender );
+
+                java.util.logging.Logger julLogger = java.util.logging.Logger.getLogger( AdminMainPage.class.getName() );
+                logger.info( "log4j info" );
+                julLogger.info( "jul info" );
+                logger.debug( "log4j debug" );
+                julLogger.fine( "jul fine" );
+
+                log4jLogger.removeAppender( appender );
+
+                System.out.println( "Logged:\n" + writer.toString() );
+            }
+        } );
+
+        add( new RawLabel( "logTest", new Model<String>( logTest ) ) );
     }
 
     public final class SetStringForm extends Form {
