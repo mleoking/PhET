70d09ecea2359a723703533c08381813c1a00d3b
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-05-31 16:53:35 +0000

    phetcommon: isolated usages of logging configuration changes that require access to LoggingPermission("control"), see #2386

--- trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/util/logging/LoggingUtils.java
+++ trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/util/logging/LoggingUtils.java
@@ -3,6 +3,7 @@ package edu.colorado.phet.common.phetcommon.util.logging;
 import java.util.logging.*;
 
 public class LoggingUtils {
+    private static boolean attemptChangesToLoggingControlProperties = false;//see #2386
 
     /**
      * Enable logging of all levels for the particular package name
@@ -16,21 +17,35 @@ public class LoggingUtils {
         Logger logger = Logger.getLogger( packageName );
 
         // the logger should receive all messages
-        logger.setLevel( Level.ALL );
+        setLevel(logger,Level.ALL);
 
         ConsoleHandler handler = new ConsoleHandler();
 
         // the handler should output all messages
-        handler.setLevel( Level.ALL );
+        setLevel(handler,Level.ALL);
+        if (attemptChangesToLoggingControlProperties){
+    
+            // that even is not enough, we need to set a filter that won't double the log messages
+            handler.setFilter( new Filter() {
+                public boolean isLoggable( LogRecord logRecord ) {
+                    return logRecord.getLevel() == Level.FINE || logRecord.getLevel() == Level.FINER || logRecord.getLevel() == Level.FINEST;
+                }
+            } );
 
-        // that even is not enough, we need to set a filter that won't double the log messages
-        handler.setFilter( new Filter() {
-            public boolean isLoggable( LogRecord logRecord ) {
-                return logRecord.getLevel() == Level.FINE || logRecord.getLevel() == Level.FINER || logRecord.getLevel() == Level.FINEST;
-            }
-        } );
+            // add the handler
+            logger.addHandler( handler );
+        }
+    }
 
-        // add the handler
-        logger.addHandler( handler );
+    private static void setLevel(Handler handler, Level level) {
+        if (attemptChangesToLoggingControlProperties){
+            handler.setLevel(level);
+        }
+    }
+
+    public static void setLevel(Logger logger, Level level) {
+        if (attemptChangesToLoggingControlProperties){
+            logger.setLevel(level);
+        }
     }
 }
