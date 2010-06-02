01073231037b22173fe4e191164f2ab3943239b5
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-06-02 06:53:22 +0000

    motion-series: Fixed: F&M game mode buggy, when moving an object to the left of the leftmost ramp.

--- simulations-java/simulations/motion-series/scala-src/edu/colorado/phet/motionseries/sims/forcesandmotion/ForcesAndMotionApplication.scala
+++ simulations-java/simulations/motion-series/scala-src/edu/colorado/phet/motionseries/sims/forcesandmotion/ForcesAndMotionApplication.scala
@@ -80,7 +80,7 @@ class ForcesAndMotionApplication(config: PhetApplicationConfig) extends PiccoloP
   addModule(new IntroModule(getPhetFrame))
   addModule(new FrictionModule(getPhetFrame))
   addModule(new GraphingModule(getPhetFrame))
-  addModule(new RobotMovingCompanyModule(getPhetFrame, 1E-8.toRadians, MotionSeriesDefaults.forcesAndMotionRobotForce, MotionSeriesDefaults.objectsForForce1DGame)) //todo: this 1E-8 workaround seems necessary to avoid problems, we should find out why
+  addModule(new RobotMovingCompanyModule(getPhetFrame, 1E-8, MotionSeriesDefaults.forcesAndMotionRobotForce, MotionSeriesDefaults.objectsForForce1DGame)) //todo: this 1E-8 workaround seems necessary to avoid problems, we should find out why
 }
 
 object ForcesAndMotionApplication{
