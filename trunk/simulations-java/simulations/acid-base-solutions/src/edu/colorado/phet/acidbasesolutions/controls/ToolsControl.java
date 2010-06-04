2068e635119a7af85ed5c93056c3b2c7399fc3c9
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-06-04 13:55:52 +0000

    6/4/10 javadoc controls 6/4/10 convenience subclass for radio buttons 6/4/10 add tick marks to concentration slider, doc oddities with this

--- trunk/simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/controls/ToolsControl.java
+++ trunk/simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/controls/ToolsControl.java
@@ -1,4 +1,9 @@
+/* Copyright 2010, University of Colorado */
+
 package edu.colorado.phet.acidbasesolutions.controls;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.ButtonGroup;
 import javax.swing.JCheckBox;
@@ -8,17 +13,24 @@ import javax.swing.border.TitledBorder;
 
 import edu.colorado.phet.acidbasesolutions.constants.ABSConstants;
 import edu.colorado.phet.acidbasesolutions.constants.ABSStrings;
+import edu.colorado.phet.acidbasesolutions.view.ABSRadioButton;
 import edu.colorado.phet.common.phetcommon.view.util.EasyGridBagLayout;
 
-
+/**
+ * Control panel that provides access to various "tools".
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
 public class ToolsControl extends JPanel {
 
-    private final JRadioButton pHPaperRadioButton;
-    private final JRadioButton pHMeterRadioButton;
+    private final JRadioButton pHPaperRadioButton, pHMeterRadioButton;
     private final JRadioButton conductivityTesterRadioButton;
     private final JRadioButton magnifyingGlassRadioButton, barGraphRadioButton;
     private final JCheckBox showWaterCheckBox;
     
+    /**
+     * Subclass that hides some of the tools.
+     */
     public static class FewerToolsControlPanel extends ToolsControl {
         public FewerToolsControlPanel() {
             setPHPaperControlVisible( false );
@@ -27,23 +39,32 @@ public class ToolsControl extends JPanel {
     }
     
     public ToolsControl() {
+        
+        // border
         TitledBorder titledBorder = new TitledBorder( ABSStrings.TOOLS );
         titledBorder.setTitleFont( ABSConstants.TITLED_BORDER_FONT );
         setBorder( titledBorder );
         
-        pHPaperRadioButton = new JRadioButton( ABSStrings.PH_PAPER );
-        pHMeterRadioButton = new JRadioButton( ABSStrings.PH_METER );
-        conductivityTesterRadioButton = new JRadioButton( ABSStrings.CONDUCTIVITY_TESTER );
-        magnifyingGlassRadioButton = new JRadioButton( ABSStrings.MAGNIFYING_GLASS );
-        barGraphRadioButton = new JRadioButton( ABSStrings.BAR_GRAPH );
-        showWaterCheckBox = new JCheckBox( ABSStrings.SHOW_WATER );
+        // radio buttons
+        ButtonGroup group = new ButtonGroup();
+        ActionListener listener = new ActionListener() {
+            public void actionPerformed( ActionEvent e ) {
+                //XXX update model
+            }
+        };
+        pHPaperRadioButton = new ABSRadioButton( ABSStrings.PH_PAPER, group, listener );
+        pHMeterRadioButton = new ABSRadioButton( ABSStrings.PH_METER, group, listener );
+        conductivityTesterRadioButton = new ABSRadioButton( ABSStrings.CONDUCTIVITY_TESTER, group, listener );
+        magnifyingGlassRadioButton = new ABSRadioButton( ABSStrings.MAGNIFYING_GLASS, group, listener );
+        barGraphRadioButton = new ABSRadioButton( ABSStrings.BAR_GRAPH, group, listener );
         
-        ButtonGroup buttonGroup = new ButtonGroup();
-        buttonGroup.add( pHPaperRadioButton );
-        buttonGroup.add( pHMeterRadioButton );
-        buttonGroup.add( conductivityTesterRadioButton );
-        buttonGroup.add( magnifyingGlassRadioButton );
-        buttonGroup.add( barGraphRadioButton );
+        // "Show Water" check box
+        showWaterCheckBox = new JCheckBox( ABSStrings.SHOW_WATER );
+        showWaterCheckBox.addActionListener( new ActionListener() {
+            public void actionPerformed( ActionEvent e ) {
+                //XXX update
+            }
+        });
         
         // layout
         EasyGridBagLayout layout = new EasyGridBagLayout( this );
