d04f09961c73bcac22699d474ebf9f4cfd6d053a
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-06-04 17:55:46 +0000

    removed additional unused code, see #2389

--- simulations-java/simulations/sound/src/edu/colorado/phet/sound/view/SoundControlPanel.java
+++ simulations-java/simulations/sound/src/edu/colorado/phet/sound/view/SoundControlPanel.java
@@ -1,18 +1,7 @@
-/**
- * Class: SoundControlPanel
- * Package: edu.colorado.phet.sound.view
- * Author: Another Guy
- * Date: Aug 5, 2004
- */
 package edu.colorado.phet.sound.view;
 
 import java.awt.*;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 
-import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
@@ -30,17 +19,13 @@ import edu.colorado.phet.sound.SoundModule;
 import edu.colorado.phet.sound.SoundResources;
 import edu.colorado.phet.sound.model.SoundModel;
 
-//public class SoundControlPanel extends JPanel {
-
 public class SoundControlPanel extends ControlPanel {
     private MyControlPanel soundControlPanel;
     private int rowIdx = 0;
     private AmplitudeControlPanel amplitudeControlPanel;
-    private final SoundModule module;
-    
+
     public SoundControlPanel( SoundModule module ) {
         super( module );
-        this.module = module;
         soundControlPanel = new MyControlPanel( module );
         addControl( soundControlPanel );
     }
@@ -56,22 +41,11 @@ public class SoundControlPanel extends ControlPanel {
         amplitudeControlPanel.setAmplitude( amplitude );
     }
 
-    public void setAudioSource( int listenerSource ) {
-    }
-
     //
     // Inner classes for the component panels
     //
     private class MyControlPanel extends JPanel {
-
         MyControlPanel( SoundModule module ) {
-            //            final JCheckBox drawTestCB = new JCheckBox( "Wave drawing test" );
-            //            drawTestCB.addActionListener( new ActionListener() {
-            //                public void actionPerformed( ActionEvent e ) {
-            //                    WaveMediumGraphic.drawTest = drawTestCB.isSelected();
-            //                }
-            //            } );
-
             this.setLayout( new GridBagLayout() );
             GridBagConstraints gbc = new GridBagConstraints( 0, rowIdx++, 1, 1, 1, 1,
                                                              GridBagConstraints.CENTER,
@@ -127,14 +101,10 @@ public class SoundControlPanel extends ControlPanel {
             this.setBorder( frequencyBorder );
         }
 
-        /**
-         *
-         */
         private void updateFrequency( SoundModel model, int sliderValue ) {
             frequencyLabel.setText( Integer.toString( sliderValue ) + " " + Hertz );
             model.setFrequency( sliderValue );
         }
-
     }
 
     private static class AmplitudeControlPanel extends JPanel {
@@ -156,11 +126,6 @@ public class SoundControlPanel extends ControlPanel {
             amplitudeSlider.setPaintTicks( true );
             amplitudeSlider.setMajorTickSpacing( 5 );
             amplitudeSlider.setMinorTickSpacing( 1 );
-            amplitudeSlider.addMouseListener( new MouseAdapter() {
-                public void mouseReleased( MouseEvent e ) {
-                    //                updateAmplitude( amplitudeSlider.getValue() );
-                }
-            } );
             amplitudeSlider.addChangeListener( new ChangeListener() {
                 public void stateChanged( ChangeEvent e ) {
                     setModelAmplitude( amplitudeSlider.getValue() );
@@ -181,57 +146,6 @@ public class SoundControlPanel extends ControlPanel {
             int sliderValue = (int)( (double)( sliderMax - sliderMin ) * amplitude + (double)sliderMin );
             amplitudeSlider.setValue( sliderValue );
             setModelAmplitude( sliderValue );
-        }
-    }
-
-    private static class OctaveControlPanel extends JPanel {
-        private int sliderMax = 10;
-        private int sliderMin = 0;
-        private int sliderDefault = 5;
-        private SoundModel model;
-        private JSlider octaveAmplitudeSlider;
-
-        OctaveControlPanel( final SoundModule module ) {
-            this.model = module.getSoundModel();
-            this.setLayout( new GridLayout( 2, 1 ) );
-            this.setPreferredSize( new Dimension( 125, 80 ) );
-
-            octaveAmplitudeSlider = new JSlider( JSlider.HORIZONTAL,
-                                                 sliderMin,
-                                                 sliderMax,
-                                                 sliderDefault );
-            octaveAmplitudeSlider.setPreferredSize( new Dimension( 25, 60 ) );
-            octaveAmplitudeSlider.setPaintTicks( true );
-            octaveAmplitudeSlider.setMajorTickSpacing( 5 );
-            octaveAmplitudeSlider.setMinorTickSpacing( 1 );
-            octaveAmplitudeSlider.addChangeListener( new ChangeListener() {
-                public void stateChanged( ChangeEvent e ) {
-                    setModelAmplitude( octaveAmplitudeSlider.getValue() );
-                }
-            } );
-
-            Border amplitudeBorder = new TitledBorder( SoundResources.getString( "SoundControlPanel.Octave" ) );
-            this.setBorder( amplitudeBorder );
-
-            final JCheckBox enabledCB = new JCheckBox( SoundResources.getString( "SoundControlPanel.Enabled" ) );
-            enabledCB.addItemListener( new ItemListener() {
-                public void itemStateChanged( ItemEvent e ) {
-                    model.setOctaveEnabled( enabledCB.isSelected() );
-                    // Do this to make the module update the oscillators
-                    module.setAudioEnabled( module.getAudioEnabled() );
-                    if( enabledCB.isSelected() ) {
-                        setModelAmplitude( octaveAmplitudeSlider.getValue() );
-                    }
-                }
-            } );
-            setModelAmplitude( octaveAmplitudeSlider.getValue() );
-            this.add( enabledCB );
-            this.add( octaveAmplitudeSlider );
-        }
-
-        private void setModelAmplitude( int sliderValue ) {
-            double amplitude = ( (double)sliderValue ) * ( SoundConfig.s_maxAmplitude ) / ( sliderMax - sliderMin );
-            model.setOctaveAmplitude( amplitude );
         }
     }
 }
