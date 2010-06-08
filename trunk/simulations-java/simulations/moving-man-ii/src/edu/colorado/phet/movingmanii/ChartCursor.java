fbb285f8da1dc989f160e24285de0565b116e81e
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-06-08 11:39:09 +0000

    moving-man-ii: improvements for in-chart cursor

--- trunk/simulations-java/simulations/moving-man-ii/src/edu/colorado/phet/movingmanii/ChartCursor.java
+++ trunk/simulations-java/simulations/moving-man-ii/src/edu/colorado/phet/movingmanii/ChartCursor.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 public class ChartCursor {
     private double time;
     private ArrayList<Listener> listeners = new ArrayList<Listener>();
+    private boolean visible;
 
     public void addListener(Listener listener) {
         listeners.add(listener);
@@ -15,6 +16,8 @@ public class ChartCursor {
 
     public static interface Listener {
         void positionChanged();
+
+        void visibilityChanged();
     }
 
     public ChartCursor() {
@@ -25,9 +28,31 @@ public class ChartCursor {
     }
 
     public void setTime(double time) {
-        this.time = time;
+        if (this.time != time) {
+            this.time = time;
+            for (Listener listener : listeners) {
+                listener.positionChanged();
+            }
+        }
+    }
+
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
         for (Listener listener : listeners) {
-            listener.positionChanged();
+            listener.visibilityChanged();
+        }
+    }
+
+    public static class Adapter implements Listener {
+        public void positionChanged() {
+        }
+
+        public void visibilityChanged() {
+
         }
     }
 }
--- trunk/simulations-java/simulations/moving-man-ii/src/edu/colorado/phet/movingmanii/MovingManApplication.java
+++ trunk/simulations-java/simulations/moving-man-ii/src/edu/colorado/phet/movingmanii/MovingManApplication.java
@@ -7,6 +7,7 @@ import edu.colorado.phet.common.phetcommon.application.PhetApplicationLauncher;
 import edu.colorado.phet.common.phetcommon.model.clock.ClockAdapter;
 import edu.colorado.phet.common.phetcommon.model.clock.ClockEvent;
 import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
+import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 import edu.colorado.phet.recordandplayback.gui.RecordAndPlaybackControlPanel;
 import edu.colorado.phet.recordandplayback.model.RecordAndPlaybackModel;
 
@@ -21,6 +22,8 @@ public class MovingManApplication extends PhetApplication {
         public MyModule() {
             super("test", new ConstantDtClock(30, 1.0 / 30.0));
             final MovingManModel model = new MovingManModel();
+
+            //TODO: make MovingManModel extend RecordAndPlaybackModel<MovingManState>? 
             final RecordAndPlaybackModel<MovingManState> recordAndPlaybackModel = new RecordAndPlaybackModel<MovingManState>(1000) {
                 public MovingManState stepRecording(double simulationTimeChange) {
                     model.simulationTimeChanged(simulationTimeChange);
@@ -41,10 +44,26 @@ public class MovingManApplication extends PhetApplication {
                     model.clear();
                 }
             });
-            setControlPanel(null);
+            model.getChartCursor().addListener(new ChartCursor.Adapter() {
+                public void positionChanged() {
+                    //TODO: this is buggy
+//                    recordAndPlaybackModel.setPlaybackTime(model.getChartCursor().getTime());
+                }
+            });
+            recordAndPlaybackModel.addObserver(new SimpleObserver() {
+                public void update() {
+                    updateCursorVisibility(model, recordAndPlaybackModel);
+                }
+            });
+            updateCursorVisibility(model, recordAndPlaybackModel);
+
             setSimulationPanel(new MovingManSimulationPanel(model, recordAndPlaybackModel));
             setClockControlPanel(new RecordAndPlaybackControlPanel<MovingManState>(recordAndPlaybackModel, getSimulationPanel(), 20.0));
             setLogoPanelVisible(false);
+        }
+
+        private void updateCursorVisibility(MovingManModel model, RecordAndPlaybackModel<MovingManState> recordAndPlaybackModel) {
+            model.getChartCursor().setVisible(recordAndPlaybackModel.isPlayback() && recordAndPlaybackModel.getNumRecordedPoints() > 0);
         }
     }
 
