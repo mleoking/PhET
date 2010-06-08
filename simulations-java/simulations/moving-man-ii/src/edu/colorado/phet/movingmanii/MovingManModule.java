5539ca431d20ff4d4173ba1a2edcc3a7762e4b25
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-06-08 20:02:25 +0000

    moving-man: added introduction tab

---
+++ simulations-java/simulations/moving-man-ii/src/edu/colorado/phet/movingmanii/MovingManModule.java
@@ -0,0 +1,68 @@
+package edu.colorado.phet.movingmanii;
+
+import edu.colorado.phet.common.phetcommon.application.Module;
+import edu.colorado.phet.common.phetcommon.model.clock.ClockAdapter;
+import edu.colorado.phet.common.phetcommon.model.clock.ClockEvent;
+import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
+import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
+import edu.colorado.phet.movingmanii.charts.ChartCursor;
+import edu.colorado.phet.movingmanii.model.MovingManModel;
+import edu.colorado.phet.movingmanii.model.MovingManState;
+import edu.colorado.phet.recordandplayback.gui.RecordAndPlaybackControlPanel;
+import edu.colorado.phet.recordandplayback.model.RecordAndPlaybackModel;
+
+import javax.swing.*;
+
+/**
+ * @author Sam Reid
+ */
+public abstract class MovingManModule extends Module {
+    public MovingManModule(String name) {
+        super(name, new ConstantDtClock(30, 1.0 / 30.0));
+        final MovingManModel model = new MovingManModel();
+
+        //TODO: make MovingManModel extend RecordAndPlaybackModel<MovingManState>?
+        final RecordAndPlaybackModel<MovingManState> recordAndPlaybackModel = new RecordAndPlaybackModel<MovingManState>(1000) {
+            public MovingManState stepRecording(double simulationTimeChange) {
+                model.simulationTimeChanged(simulationTimeChange);
+                return model.getRecordingState();
+            }
+
+            public void setPlaybackState(MovingManState state) {
+                model.setPlaybackState(state);
+            }
+        };
+        getClock().addClockListener(new ClockAdapter() {
+            public void simulationTimeChanged(ClockEvent clockEvent) {
+                recordAndPlaybackModel.stepInTime(clockEvent.getSimulationTimeChange());
+            }
+        });
+        recordAndPlaybackModel.addHistoryClearListener(new RecordAndPlaybackModel.HistoryClearListener() {
+            public void historyCleared() {
+                model.clear();
+            }
+        });
+        model.getChartCursor().addListener(new ChartCursor.Adapter() {
+            public void positionChanged() {
+                //TODO: this is buggy
+//                    recordAndPlaybackModel.setPlaybackTime(model.getChartCursor().getTime());
+            }
+        });
+        recordAndPlaybackModel.addObserver(new SimpleObserver() {
+            public void update() {
+                updateCursorVisibility(model, recordAndPlaybackModel);
+            }
+        });
+        updateCursorVisibility(model, recordAndPlaybackModel);
+
+        setSimulationPanel(createSimulationPanel(model, recordAndPlaybackModel));
+        setClockControlPanel(new RecordAndPlaybackControlPanel<MovingManState>(recordAndPlaybackModel, getSimulationPanel(), 20.0));
+        setLogoPanelVisible(false);
+    }
+
+    protected abstract JComponent createSimulationPanel(MovingManModel model, RecordAndPlaybackModel<MovingManState> recordAndPlaybackModel);
+
+    private void updateCursorVisibility(MovingManModel model, RecordAndPlaybackModel<MovingManState> recordAndPlaybackModel) {
+        model.getChartCursor().setVisible(recordAndPlaybackModel.isPlayback() && recordAndPlaybackModel.getNumRecordedPoints() > 0);
+    }
+}
