a29eb9d204ca8ca73285e588ff3b95bc0a81300f
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-06-08 20:46:46 +0000

    moving-man: improvements for play area sliders

--- trunk/simulations-java/simulations/moving-man-ii/src/edu/colorado/phet/movingmanii/MovingManSimulationPanelWithPlayAreaSliders.java
+++ trunk/simulations-java/simulations/moving-man-ii/src/edu/colorado/phet/movingmanii/MovingManSimulationPanelWithPlayAreaSliders.java
@@ -1,5 +1,6 @@
 package edu.colorado.phet.movingmanii;
 
+import edu.colorado.phet.movingmanii.model.MovingMan;
 import edu.colorado.phet.movingmanii.model.MovingManModel;
 import edu.colorado.phet.movingmanii.model.MovingManState;
 import edu.colorado.phet.movingmanii.view.MovingManSimulationPanel;
@@ -15,22 +16,58 @@ import javax.swing.event.ChangeListener;
 public class MovingManSimulationPanelWithPlayAreaSliders extends MovingManSimulationPanel {
     public MovingManSimulationPanelWithPlayAreaSliders(final MovingManModel model, final RecordAndPlaybackModel<MovingManState> recordAndPlaybackModel) {
         super(model, recordAndPlaybackModel);
-        final PlayAreaSliderControl playAreaSliderControl = new PlayAreaSliderControl();
-        model.addListener(new MovingManModel.Listener() {
-            public void mousePositionChanged() {
-//                System.out.println("model changed: "+model.getMousePosition());
-                playAreaSliderControl.setValue(model.getMousePosition());
-            }
-        });
-        playAreaSliderControl.addListener(new ChangeListener() {
-            public void stateChanged(ChangeEvent e) {
-                model.getMovingMan().setPositionDriven();
-                double value = playAreaSliderControl.getValue();
-//                System.out.println("Slider dragged: "+value );
-                model.setMousePosition(value);
-            }
-        });
-        playAreaSliderControl.setOffset(400, 200);
-        addScreenChild(playAreaSliderControl);
+        {
+            final PlayAreaSliderControl positionSlider = new PlayAreaSliderControl(-10, 10, "Position", "m");
+            model.addListener(new MovingManModel.Listener() {
+                public void mousePositionChanged() {
+                    positionSlider.setValue(model.getMousePosition());
+                }
+            });
+            positionSlider.addListener(new ChangeListener() {
+                public void stateChanged(ChangeEvent e) {
+                    double value = positionSlider.getValue();//store and re-use since setPositionDriven fires an event, which changesthe value on the slider
+                    model.getMovingMan().setPositionDriven();
+                    model.setMousePosition(value);
+                }
+            });
+            positionSlider.setOffset(400, 200);
+            addScreenChild(positionSlider);
+        }
+
+        {
+            final PlayAreaSliderControl velocitySlider = new PlayAreaSliderControl(-50, 50, "Velocity", "m/s");
+            model.getMovingMan().addListener(new MovingMan.Listener() {
+                public void changed() {
+                    velocitySlider.setValue(model.getMovingMan().getVelocity());
+                }
+            });
+            velocitySlider.addListener(new ChangeListener() {
+                public void stateChanged(ChangeEvent e) {
+                    double value = velocitySlider.getValue();
+                    model.getMovingMan().setVelocityDriven();//todo: only if user caused the change, not if model caused the change
+                    model.getMovingMan().setVelocity(value);
+                }
+            });
+            velocitySlider.setOffset(400, 300);
+            addScreenChild(velocitySlider);
+        }
+
+        {
+            final PlayAreaSliderControl accelerationSlider = new PlayAreaSliderControl(-50, 50, "Acceleration", "m/s/s");
+            model.getMovingMan().addListener(new MovingMan.Listener() {
+                public void changed() {
+                    accelerationSlider.setValue(model.getMovingMan().getAcceleration());
+                }
+            });
+            accelerationSlider.addListener(new ChangeListener() {
+                public void stateChanged(ChangeEvent e) {
+                    double value = accelerationSlider.getValue();
+                    model.getMovingMan().setAccelerationDriven();
+                    model.getMovingMan().setAcceleration(value);
+                }
+            });
+            accelerationSlider.setOffset(400, 400);
+            addScreenChild(accelerationSlider);
+        }
     }
 }
