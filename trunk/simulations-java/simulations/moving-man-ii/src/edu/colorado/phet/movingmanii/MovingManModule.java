c74fd51f2d1dc0424d3c82881cffd8eb02e3e370
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-06-10 08:48:19 +0000

    moving-man: Fixed: If the user sets acceleration and velocity then presses go, should switch to acceleration mode so the acceleration is not overwritten (and same for velocity)

--- trunk/simulations-java/simulations/moving-man-ii/src/edu/colorado/phet/movingmanii/MovingManModule.java
+++ trunk/simulations-java/simulations/moving-man-ii/src/edu/colorado/phet/movingmanii/MovingManModule.java
@@ -6,18 +6,27 @@ import edu.colorado.phet.common.phetcommon.model.clock.ClockEvent;
 import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 import edu.colorado.phet.movingmanii.charts.ChartCursor;
+import edu.colorado.phet.movingmanii.model.MovingMan;
 import edu.colorado.phet.movingmanii.model.MovingManModel;
 import edu.colorado.phet.movingmanii.model.MovingManState;
 import edu.colorado.phet.recordandplayback.gui.RecordAndPlaybackControlPanel;
 import edu.colorado.phet.recordandplayback.model.RecordAndPlaybackModel;
 
 import javax.swing.*;
+import java.util.ArrayList;
 
 /**
  * @author Sam Reid
  */
 public abstract class MovingManModule extends Module {
     protected RecordAndPlaybackModel<MovingManState> recordAndPlaybackModel;
+
+    /*
+    If the user sets acceleration and velocity then presses go, should switch to acceleration mode so the acceleration is not overwritten (and same for velocity)
+    Try this rule: on pause, clear list.  When user enters a value, add to list.  When unpausing, choose highest modified property to be "driving".  This seems safer.
+     */
+    private ArrayList<MovingMan.MotionStrategy> actionList = new ArrayList<MovingMan.MotionStrategy>();
+    private boolean lastPauseState;
 
     public MovingManModule(String name) {
         super(name, new ConstantDtClock(30, 1.0 / 30.0));
@@ -28,7 +37,6 @@ public abstract class MovingManModule extends Module {
                 return recordAndPlaybackModel.isPaused();
             }
         });
-
         //TODO: make MovingManModel extend RecordAndPlaybackModel<MovingManState>?
         recordAndPlaybackModel = new RecordAndPlaybackModel<MovingManState>(1000) {
             public MovingManState stepRecording(double simulationTimeChange) {
@@ -41,12 +49,38 @@ public abstract class MovingManModule extends Module {
             }
         };
 
-        //TODO: When unpausing, if user set velocity or acceleration to be nonzero, need to switch to vel or acc-driven
+        //When unpausing, if user set velocity or acceleration to be nonzero, need to switch to vel or acc-driven
         recordAndPlaybackModel.addObserver(new SimpleObserver() {
             public void update() {
-                //To change body of implemented methods use File | Settings | File Templates.
+                if (recordAndPlaybackModel.isPaused() != lastPauseState) {
+                    if (recordAndPlaybackModel.isPaused()) {//Just paused
+                        actionList.clear();
+                    } else {
+//                        System.out.println("using actionList = " + actionList);
+                        if (actionList.contains(MovingMan.POSITION_DRIVEN))
+                            MovingMan.POSITION_DRIVEN.apply(model.getMovingMan());
+                        if (actionList.contains(MovingMan.VELOCITY_DRIVEN))
+                            MovingMan.VELOCITY_DRIVEN.apply(model.getMovingMan());
+                        if (actionList.contains(MovingMan.ACCELERATION_DRIVEN))
+                            MovingMan.ACCELERATION_DRIVEN.apply(model.getMovingMan());//highest precedence
+                    }
+                    lastPauseState = recordAndPlaybackModel.isPaused();
+                }
             }
         });
+        model.getMovingMan().addListener(new MovingMan.Listener() {
+            public void changed() {
+                if (recordAndPlaybackModel.isPaused()) {
+                    if (!actionList.contains(model.getMovingMan().getMotionStrategy())) {
+                        actionList.add(model.getMovingMan().getMotionStrategy());
+//                        System.out.println("updated actionList = " + actionList);
+                    }
+                }
+            }
+        });
+        lastPauseState = recordAndPlaybackModel.isPaused();
+
+        //Wire up the clock to the record and playback model.
         getClock().addClockListener(new ClockAdapter() {
             public void simulationTimeChanged(ClockEvent clockEvent) {
                 recordAndPlaybackModel.stepInTime(clockEvent.getSimulationTimeChange());
