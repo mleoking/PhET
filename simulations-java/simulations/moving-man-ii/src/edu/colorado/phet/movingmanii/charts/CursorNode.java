8095b8ae837f2755c9ef1d9dadc2119e771c020f
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-06-11 04:27:35 +0000

    moving-man: Fixed: Chart cursor drag + display should respect dynamic chart size, Fixed: Chart cursor node should disappear when chart minimized

--- simulations-java/simulations/moving-man-ii/src/edu/colorado/phet/movingmanii/charts/CursorNode.java
+++ simulations-java/simulations/moving-man-ii/src/edu/colorado/phet/movingmanii/charts/CursorNode.java
@@ -3,8 +3,6 @@ package edu.colorado.phet.movingmanii.charts;
 import edu.colorado.phet.common.motion.model.TimeData;
 import edu.colorado.phet.common.piccolophet.event.CursorHandler;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
-import edu.colorado.phet.movingmanii.charts.ChartCursor;
-import edu.colorado.phet.movingmanii.charts.MovingManChart;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
 import edu.umd.cs.piccolo.event.PInputEvent;
@@ -23,41 +21,55 @@ public class CursorNode extends PNode {
     public CursorNode(final ChartCursor cursor, final MovingManChart chart) {
         this.cursor = cursor;
         this.chart = chart;
-        cursor.addListener(new ChartCursor.Adapter() {
-            public void positionChanged() {
-                update();
-            }
-        });
-        cursor.addListener(new ChartCursor.Adapter() {
-            public void visibilityChanged() {
-                updateVisibility();
-            }
-        });
-        updateVisibility();
         path = new PhetPPath(new Color(50, 50, 200, 45), new BasicStroke(1, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 1, new float[]{4, 2}, 0), new Color(20, 20, 120, 100));
         addChild(path);
 
-        update();
-
+        final ChartCursor.Adapter pathUpdater = new ChartCursor.Adapter() {
+            public void positionChanged() {
+                double time = cursor.getTime();
+                double chartX = chart.modelToView(new TimeData(0, time)).getX();//todo: provide mapValue and mapTime
+                double width = 5.0;
+                path.setPathTo(new Rectangle2D.Double(chartX - width / 2, 0, width, chart.getViewDimension().getHeight()));
+            }
+        };
+        pathUpdater.positionChanged();
+        cursor.addListener(pathUpdater);
+        final ChartCursor.Adapter visibilityUpdater = new ChartCursor.Adapter() {
+            public void visibilityChanged() {
+                setVisible(cursor.isVisible());
+            }
+        };
+        visibilityUpdater.visibilityChanged();
+        cursor.addListener(visibilityUpdater);
         addInputEventListener(new CursorHandler());
         addInputEventListener(new PBasicInputEventHandler() {
-            @Override
+            double relativeGrabPoint = Double.NaN;
+
+            public void mousePressed(PInputEvent event) {
+                super.mousePressed(event);
+                updateRelativeGrabPoint(event);
+            }
+
             public void mouseDragged(PInputEvent event) {
-                double dx = event.getCanvasDelta().getWidth();
-                double w = chart.viewToModelDeltaX(dx);
-                cursor.setTime(cursor.getTime() + w);
+                super.mouseDragged(event);
+                if (Double.isNaN(relativeGrabPoint)) {
+                    updateRelativeGrabPoint(event);
+                }
+                cursor.setTime(getModelPoint(event) - relativeGrabPoint);
+            }
+
+            public void mouseReleased(PInputEvent event) {
+                super.mouseReleased(event);
+                relativeGrabPoint = Double.NaN;
+            }
+
+            private void updateRelativeGrabPoint(PInputEvent event) {
+                relativeGrabPoint = getModelPoint(event) - cursor.getTime();
+            }
+
+            private double getModelPoint(PInputEvent event) {
+                return chart.viewToModel(event.getPositionRelativeTo(chart).getX());
             }
         });
-    }
-
-    private void updateVisibility() {
-        setVisible(cursor.isVisible());
-    }
-
-    private void update() {
-        double time = cursor.getTime();
-        double chartX = chart.modelToView(new TimeData(0, time)).getX();//todo: provide mapValue and mapTime
-        double width = 5.0;
-        path.setPathTo(new Rectangle2D.Double(chartX - width / 2, 0, width, chart.getViewDimension().getHeight()));
     }
 }
