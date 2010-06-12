06865a8280cb2ce4e788492b95342e7effcf1a09
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-06-12 22:47:50 +0000

    6/12/10 make pH meter "on" by default 6/12/10 remove magnifying-glass-prototype from build properties

--- trunk/simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/constants/ABSConstants.java
+++ trunk/simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/constants/ABSConstants.java
@@ -52,7 +52,7 @@ public class ABSConstants {
     public static final double MAGNIFYING_GLASS_DIAMETER = 500;
     
     public static final Point2D PH_METER_LOCATION = new Point2D.Double( BEAKER_LOCATION.getX() + ( BEAKER_SIZE.getWidth() / 4 ), BEAKER_LOCATION.getY() - BEAKER_SIZE.getHeight() - 15 );
-    public static final boolean PH_METER_VISIBLE = false;
+    public static final boolean PH_METER_VISIBLE = true; //XXX
     public static final double PH_METER_SHAFT_LENGTH = 125;
     
     public static final double WATER_CONCENTRATION = 55.6; // water concentration when it's used as a solvent, mol/L
