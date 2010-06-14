e8ddb640a576fbbffe3677497a22679a6605774b
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-06-14 13:37:02 +0000

    moving-man: Fixed: Play area arrows look incorrect at small magnitudes

--- simulations-java/simulations/moving-man-ii/src/edu/colorado/phet/movingmanii/view/PlayAreaVector.java
+++ simulations-java/simulations/moving-man-ii/src/edu/colorado/phet/movingmanii/view/PlayAreaVector.java
@@ -21,10 +21,12 @@ public class PlayAreaVector extends PNode {
     }
 
     public void setArrow(double x, double y, double x2, double y2) {
-        if (Math.abs(x - x2) < 1) {
+        if (Math.abs(x - x2) < 1) {//Don't try to show arrows less than one pixel long
             path.setPathTo(new Rectangle(0, 0, 0, 0));
         } else {
-            path.setPathTo(new Arrow(new Point2D.Double(x, y), new Point2D.Double(x2, y2), 15, 15, tailWidth, 4.0, true).getShape());
+            path.setPathTo(new Arrow(new Point2D.Double(x, y), new Point2D.Double(x2, y2), 15, 15, tailWidth,
+                    1 / 2.0, //so that the arrow shrinks proportionately
+                    true).getShape());
         }
     }
 }
