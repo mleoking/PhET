29914c1aa49e0cce155539aee48e41ea84beda5b
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-06-14 04:08:29 +0000

    record-and-playback: 6/13/2010 Layout improvements for RecordAndPlaybackControlPanel

--- trunk/simulations-java/common/record-and-playback/src/edu/colorado/phet/recordandplayback/gui/RecordAndPlaybackControlPanel.java
+++ trunk/simulations-java/common/record-and-playback/src/edu/colorado/phet/recordandplayback/gui/RecordAndPlaybackControlPanel.java
@@ -2,13 +2,11 @@ package edu.colorado.phet.recordandplayback.gui;
 
 import edu.colorado.phet.common.phetcommon.resources.PhetCommonResources;
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
-import edu.colorado.phet.common.phetcommon.view.util.RectangleUtils;
 import edu.colorado.phet.common.piccolophet.PhetPCanvas;
 import edu.colorado.phet.common.piccolophet.event.ToolTipHandler;
 import edu.colorado.phet.common.piccolophet.nodes.mediabuttons.*;
 import edu.colorado.phet.recordandplayback.model.RecordAndPlaybackModel;
 import edu.umd.cs.piccolo.PNode;
-import edu.umd.cs.piccolo.util.PBounds;
 import edu.umd.cs.piccolox.pswing.PSwing;
 
 import javax.swing.*;
@@ -17,8 +15,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
-import java.awt.geom.Rectangle2D;
-import java.util.ArrayList;
 
 /**
  * The RecordAndPlaybackControlPanel is a replacement for the time control panel in phet simulations that
@@ -31,35 +27,38 @@ public class RecordAndPlaybackControlPanel<T> extends PhetPCanvas {
     private RecordAndPlaybackModel<T> model;
     private JComponent simPanel;//used to determine the size of this component
     private PiccoloTimeControlPanel.BackgroundNode backgroundNode = new PiccoloTimeControlPanel.BackgroundNode();
-    private ArrayList<PNode> nodes = new ArrayList<PNode>();
-    private Dimension preferredSize = new Dimension(800, 100);
     private JButton clearButton = new JButton(PhetCommonResources.getString("Common.clear"));
-    private PNode rightmostControl;
     private RewindButton rewind = new RewindButton(50);
     private PSwing clearButtonNode = new PSwing(clearButton);
     private PSwing modePanelNode;
     private PlayPauseButton playPause = new PlayPauseButton(75);
     private ToolTipHandler playPauseTooltipHandler = new ToolTipHandler(PhetCommonResources.getString("Common.ClockControlPanel.Pause"), this);
     private StepButton stepButton = new StepButton(50);
-    private double stepTimeChange;//todo: add this as a parameter
+    private double stepTimeChange = 0.0;//todo: add this as a parameter
+    private PNode controlLayer;
+    protected TimelineNode timelineNode;
 
     public RecordAndPlaybackControlPanel(final RecordAndPlaybackModel<T> model, JComponent simPanel, double maxTime) {
         this(model, simPanel, maxTime, Color.blue);
     }
 
+    /**
+     * Creates a record and playback control panel that also contains a playback speed slider.
+     *
+     * @param model
+     * @param simPanel
+     * @param maxTime
+     * @param timelineColor
+     */
     public RecordAndPlaybackControlPanel(final RecordAndPlaybackModel<T> model, JComponent simPanel, double maxTime, Color timelineColor) {
-        this(model, simPanel, new Function() {
-            public PNode createControl() {
-                return new PlaybackSpeedSlider<T>(model);
-            }
-        }, timelineColor, maxTime);
+        this(model, simPanel, timelineColor, maxTime);
+        addControl(new PlaybackSpeedSlider<T>(model));
     }
 
-    public RecordAndPlaybackControlPanel(final RecordAndPlaybackModel<T> model, JComponent simPanel, Function createRightControl, Color timelineColor, double maxTime) {
+    public RecordAndPlaybackControlPanel(final RecordAndPlaybackModel<T> model, JComponent simPanel, Color timelineColor, double maxTime) {
         this.model = model;
         this.simPanel = simPanel;
-        TimelineNode timeline = new TimelineNode<T>(model, this, timelineColor, maxTime);
-        rightmostControl = createRightControl.createControl();
+        timelineNode = new TimelineNode<T>(model, this, timelineColor, maxTime);
         ModePanel<T> modePanel = new ModePanel<T>(model);
         modePanelNode = new PSwing(modePanel);
 
@@ -75,8 +74,6 @@ public class RecordAndPlaybackControlPanel<T> extends PhetPCanvas {
             }
         });
 
-        rightmostControl.setOffset(0, preferredSize.getHeight() / 2 - rightmostControl.getFullBounds().getHeight() / 2);
-
         rewind.addListener(new DefaultIconButton.Listener() {
             public void buttonPressed() {
                 model.setRecord(false);
@@ -84,16 +81,16 @@ public class RecordAndPlaybackControlPanel<T> extends PhetPCanvas {
                 model.setPaused(true);
             }
         });
-        model.addObserver(new SimpleObserver() {
+        SimpleObserver updateRewindEnabled = new SimpleObserver() {
             public void update() {
-                updateRewindEnabled();
+                boolean enabled = model.isPlayback() && model.getNumRecordedPoints() > 0 && model.getTime() != model.getMinRecordedTime();
+                rewind.setEnabled(enabled);
             }
-        });
-        updateRewindEnabled();
-
+        };
+        model.addObserver(updateRewindEnabled);
+        updateRewindEnabled.update();
 
         rewind.addInputEventListener(new ToolTipHandler(PhetCommonResources.getString("Common.rewind"), this));
-        rewind.setOffset(0, 12);
 
         playPause.addListener(new PlayPauseButton.Listener() {
             public void playbackStateChanged() {
@@ -102,13 +99,15 @@ public class RecordAndPlaybackControlPanel<T> extends PhetPCanvas {
         });
         playPause.addInputEventListener(playPauseTooltipHandler);
 
-
-        model.addObserver(new SimpleObserver() {
+        SimpleObserver updatePlayPauseButton = new SimpleObserver() {
             public void update() {
-                updatePlayPauseButton();
+                playPause.setPlaying(!model.isPaused());
+                playPauseTooltipHandler.setText(model.isPaused() ? PhetCommonResources.getString("Common.ClockControlPanel.Play") : PhetCommonResources.getString("Common.ClockControlPanel.Pause"));
             }
-        });
-        updatePlayPauseButton();
+        };
+        model.addObserver(updatePlayPauseButton);
+        updatePlayPauseButton.update();
+
         stepButton.setEnabled(false);
         stepButton.addInputEventListener(new ToolTipHandler(PhetCommonResources.getString("Common.ClockControlPanel.Step"), this));
         model.addObserver(new SimpleObserver() {
@@ -123,82 +122,62 @@ public class RecordAndPlaybackControlPanel<T> extends PhetPCanvas {
                 else if (model.isRecord()) model.stepRecording(stepTimeChange);
             }
         });
-        stepButton.setOffset(0, 12);
+        addScreenChild(timelineNode);
+        controlLayer = new PNode();
+        addScreenChild(controlLayer);
 
         addControl(clearButtonNode);
         addControl(modePanelNode);
         addControl(rewind);
         addControl(playPause);
         addControl(stepButton);
-        addControl(rightmostControl);
 
-        addScreenChild(timeline);
-
-        setPreferredSize(preferredSize);
+        setPreferredSize(new Dimension((int) getLayer().getFullBounds().getWidth(), (int) getLayer().getFullBounds().getHeight()));
 
         //todo: this seems like it could suffer from the problem CM & I discovered that Component resize events are neither synchronous nor immediate
         simPanel.addComponentListener(new ComponentAdapter() {
             public void componentResized(ComponentEvent e) {
-                updateSize();
+                relayout();
             }
         });
 
-        updateSize();
         addComponentListener(new ComponentAdapter() {
             public void componentResized(ComponentEvent e) {
-                updateControlPanelLayout();
+                relayout();
             }
         });
-        updateControlPanelLayout();
+        relayout();
     }
 
-    private void updatePlayPauseButton() {
-        playPause.setPlaying(!model.isPaused());
-        playPauseTooltipHandler.setText(model.isPaused() ? PhetCommonResources.getString("Common.ClockControlPanel.Play") : PhetCommonResources.getString("Common.ClockControlPanel.Pause"));
+    public void addControl(PNode node) {
+        controlLayer.addChild(node);
     }
 
-    public static interface Function {
-        PNode createControl();
-    }
-
-    protected void addControl(PNode node) {
-        addScreenChild(node);
-        double offsetX = nodes.size() == 0 ? 0 : nodes.get(nodes.size() - 1).getFullBounds().getMaxX() + 5;
-        node.setOffset(offsetX, node.getOffset().getY() + 10);
-        nodes.add(node);
-    }
-
-    private void updateRewindEnabled() {
-        boolean enabled = model.isPlayback() && model.getNumRecordedPoints() > 0 && model.getTime() != model.getMinRecordedTime();
-        rewind.setEnabled(enabled);
-    }
-
-    private void updateControlPanelLayout() {
-        double buttonDX = 2;
-        playPause.setOffset(getPreferredSize().width / 2 - playPause.getFullBounds().getWidth() / 2, playPause.getOffset().getY());
-        rewind.setOffset(playPause.getFullBounds().getX() - rewind.getFullBounds().getWidth() - buttonDX, rewind.getOffset().getY());
-        stepButton.setOffset(playPause.getFullBounds().getMaxX() + buttonDX, stepButton.getOffset().getY());
-        rightmostControl.setOffset(stepButton.getFullBounds().getMaxX(), rightmostControl.getOffset().getY());
-
-        modePanelNode.setOffset(rewind.getFullBounds().getX() - modePanelNode.getFullBounds().width, playPause.getFullBounds().getCenterY() - modePanelNode.getFullBounds().getHeight() / 2);
-        clearButtonNode.setOffset(modePanelNode.getFullBounds().getX() - clearButtonNode.getFullBounds().width, playPause.getFullBounds().getCenterY() - clearButtonNode.getFullBounds().getHeight() / 2);
-
-        double halfWidth = playPause.getFullBounds().getCenterX() - rightmostControl.getFullBounds().getMaxX();
-        ArrayList<PBounds> bounds = new ArrayList<PBounds>();
-        for (PNode n : nodes) bounds.add(n.getFullBounds());
-
-        Rectangle2D union = bounds.get(0);
-        for (int i = 1; i < bounds.size(); i++) {
-            union = union.createUnion(bounds.get(i));
+    private double getControlLayerHeight() {
+        double h = controlLayer.getChild(0).getFullBounds().getHeight();
+        for (int i = 1; i < controlLayer.getChildrenCount(); i++) {
+            h = Math.max(h, controlLayer.getChild(i).getFullBounds().getHeight());
         }
-        Rectangle2D expanded = RectangleUtils.expand(union, 0, 0);
-        backgroundNode.setSize((int) (halfWidth * 2), (int) expanded.getHeight());
-        backgroundNode.setOffset(playPause.getFullBounds().getCenterX() - halfWidth, expanded.getY());
+        return h;
     }
 
-    private void updateSize() {
+    private void relayout() {
+        double x = 0;
+
+        for (int i = 0; i < controlLayer.getChildrenCount(); i++) {
+            PNode child = controlLayer.getChild(i);
+            child.setOffset(x, getControlLayerHeight() / 2 - child.getFullBounds().getHeight() / 2);  //assumes controls are origined at 0,0
+            double insetDX = 2.0;//distance between controls
+            x = x + child.getFullBounds().getWidth() + insetDX;
+        }//TODO: center so that play button is in the very middle
+        double playButtonCenter = playPause.getFullBounds().getCenterX();
+        controlLayer.setOffset(simPanel.getWidth() / 2 - playButtonCenter, timelineNode.getFullBounds().getHeight());
+
+        backgroundNode.setSize((int) controlLayer.getFullBounds().getWidth() + 4, (int) controlLayer.getFullBounds().getHeight() + 2);
+        backgroundNode.setOffset(controlLayer.getOffset().getX() - 2, controlLayer.getOffset().getY() - 1);
+
         if (simPanel.getWidth() > 0) {
-            Dimension pref = new Dimension(simPanel.getWidth(), preferredSize.height);
+            Dimension pref = new Dimension(simPanel.getWidth(), (int) getLayer().getFullBounds().getHeight() + 1);
             setPreferredSize(pref);
             if (getParent() != null) {
                 getParent().doLayout(); //This is necessary to solve this problem: 6/10/2010 Fixed: Record and playback timeline doesn't synchronize size at the right time (seems one behind); When switching back and forth tabs in moving man, the playback timeline changes size.
