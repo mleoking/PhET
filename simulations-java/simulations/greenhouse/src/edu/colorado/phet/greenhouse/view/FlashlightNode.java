2bc31c9702665ed6d4e40aab85a26925245b5a29
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-06-14 22:57:06 +0000

    Revised flashlight node to have a width parameter, changed location on canvas.

--- simulations-java/simulations/greenhouse/src/edu/colorado/phet/greenhouse/view/FlashlightNode.java
+++ simulations-java/simulations/greenhouse/src/edu/colorado/phet/greenhouse/view/FlashlightNode.java
@@ -2,15 +2,17 @@
 
 package edu.colorado.phet.greenhouse.view;
 
+
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform2D;
 import edu.colorado.phet.greenhouse.GreenhouseResources;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.nodes.PImage;
 
 /**
- * PNode that represents a flashlight in the view.  The flashlight is oriented
- * such that the point of origin and the expected direction line up with the
- * image.
+ * PNode that represents a flashlight in the view.  This node is set up such
+ * that setting its offset based on the photon emission point in the model
+ * should position it correctly.  This makes some assumptions about the
+ * direction of photon emission.
  * 
  * @author John Blanco
  */
@@ -19,11 +21,25 @@ public class FlashlightNode extends PNode {
 	private PImage flashlightImage;
 	private ModelViewTransform2D mvt;
 	
-	public FlashlightNode(ModelViewTransform2D mvt) {
+	/**
+	 * Constructor.
+	 * 
+	 * @param flashlightWidth - Width of the flashlight in screen coords.  The
+	 * height will be based on the aspect ratio of the image.  The size of
+	 * the control box is independent of this - its size is based on the
+	 * strings that define the user selections.
+	 * @param mvt - Model-view transform for translating between model and
+	 * view coordinate systems.
+	 */
+	public FlashlightNode(double flashlightWidth, ModelViewTransform2D mvt) {
 		
 		this.mvt = mvt;
 		
 		flashlightImage = new PImage(GreenhouseResources.getImage("flashlight.png"));
+		flashlightImage.scale(flashlightWidth / flashlightImage.getFullBoundsReference().width);
+		// Set the offset such that the center right side of the image is the
+		// origin.  This assumes that photons will be emitted to the right.
+		flashlightImage.setOffset(-flashlightWidth, -flashlightImage.getFullBoundsReference().height / 2);
 		addChild(flashlightImage);
 	}
 }
