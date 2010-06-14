cfebbfc158593a1fd995fee8be53dc38575bff74
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-06-14 16:37:48 +0000

    Wicket-website: added delete to the manage activities panel

--- trunk/web/wicket-website/src/java/edu/colorado/phet/website/panels/contribution/ContributionManageListPanel.java
+++ trunk/web/wicket-website/src/java/edu/colorado/phet/website/panels/contribution/ContributionManageListPanel.java
@@ -6,11 +6,17 @@ import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.markup.html.pages.RedirectPage;
+import org.hibernate.Session;
 
 import edu.colorado.phet.website.content.contribution.ContributionEditPage;
+import edu.colorado.phet.website.content.contribution.ContributionManagePage;
 import edu.colorado.phet.website.content.contribution.ContributionPage;
+import edu.colorado.phet.website.content.contribution.ContributionSuccessPage;
 import edu.colorado.phet.website.data.contribution.Contribution;
 import edu.colorado.phet.website.panels.PhetPanel;
+import edu.colorado.phet.website.util.HibernateTask;
+import edu.colorado.phet.website.util.HibernateUtils;
 import edu.colorado.phet.website.util.PageContext;
 
 /**
@@ -22,18 +28,35 @@ public class ContributionManageListPanel extends PhetPanel {
     public ContributionManageListPanel( String id, final PageContext context, List<Contribution> contributions ) {
         super( id, context );
 
+        // TODO: localize
+
         // don't show whatever this was contained in, so that the table rows are nested directly under the table tags in
         // the parent
         setRenderBodyOnly( true );
 
         add( new ListView<Contribution>( "contributions", contributions ) {
             protected void populateItem( ListItem<Contribution> item ) {
-                Contribution contribution = item.getModelObject();
+                final Contribution contribution = item.getModelObject();
                 Link link = ContributionPage.getLinker( contribution ).getLink( "contribution-link", context, getPhetCycle() );
                 link.add( new Label( "contribution-title", contribution.getTitle() ) );
                 item.add( link );
                 item.add( new Label( "contribution-authors", contribution.getAuthors() ) );
                 item.add( ContributionEditPage.getLinker( contribution ).getLink( "edit-link", context, getPhetCycle() ) );
+                item.add( new Link( "delete-link" ) {
+                    @Override
+                    public void onClick() {
+                        boolean success = HibernateUtils.wrapTransaction( getHibernateSession(), new HibernateTask() {
+                            public boolean run( Session session ) {
+                                Contribution contrib = (Contribution) session.load( Contribution.class, contribution.getId() );
+                                contrib.deleteMe( session );
+                                return true;
+                            }
+                        } );
+                        if ( success ) {
+                            setResponsePage( new RedirectPage( ContributionManagePage.getLinker().getRawUrl( context, getPhetCycle() ) ) );
+                        }
+                    }
+                } );
             }
         } );
     }
