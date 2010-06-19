f722b8dae3b46ffe4f19e21952eb8348efa76b43
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-06-19 06:54:31 +0000

    moving-man: finished adding support for flipped coordinate frame

--- simulations-java/simulations/moving-man/src/edu/colorado/phet/movingmanii/view/PlayAreaObjectNode.java
+++ simulations-java/simulations/moving-man/src/edu/colorado/phet/movingmanii/view/PlayAreaObjectNode.java
@@ -2,6 +2,7 @@ package edu.colorado.phet.movingmanii.view;
 
 import edu.colorado.phet.common.phetcommon.math.Function;
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
+import edu.colorado.phet.movingmanii.model.MutableBoolean;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.nodes.PImage;
 
@@ -16,25 +17,30 @@ public class PlayAreaObjectNode extends PNode {
     protected final Function.LinearFunction linearFunction;
     protected final double x;
     protected final double offsetX;
+    private final MutableBoolean positiveToTheRight;
     protected PImage wallNode;
 
-    public PlayAreaObjectNode(BufferedImage image, Range modelRange, final Range viewRange, double x, double offsetX) {
+    public PlayAreaObjectNode(BufferedImage image, Range modelRange, final Range viewRange, double x, double offsetX, MutableBoolean positiveToTheRight) {
         this.offsetX = offsetX;
+        this.positiveToTheRight = positiveToTheRight;
         linearFunction = new Function.LinearFunction(modelRange.getMin(), modelRange.getMax(), viewRange.getMin(), viewRange.getMax());
         wallNode = new PImage(image);
         this.x = x;
 
         addChild(wallNode);
-        viewRange.addObserver(new SimpleObserver() {
+        SimpleObserver locationUpdate = new SimpleObserver() {
             public void update() {
                 linearFunction.setOutput(viewRange.getMin(), viewRange.getMax());
                 updateLocation();
             }
-        });
-        updateLocation();
+        };
+        viewRange.addObserver(locationUpdate);
+        locationUpdate.update();
+
+        positiveToTheRight.addObserver(locationUpdate);
     }
 
     protected void updateLocation() {
-        setOffset(linearFunction.evaluate(x) - getFullBounds().getWidth() / 2 + offsetX, 0);
+        setOffset(linearFunction.evaluate(x) - getFullBounds().getWidth() / 2 + offsetX * (positiveToTheRight.getValue() ? 1 : -1), 0);
     }
 }
