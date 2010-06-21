163a440f460c2730cd80191ef8fbd53b4d6e5ad8
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-06-21 19:30:31 +0000

    moving-man: Fixed: Axis rotation fails for charts tab

--- simulations-java/simulations/moving-man/src/edu/colorado/phet/movingman/SpecialFeaturesMenu.java
+++ simulations-java/simulations/moving-man/src/edu/colorado/phet/movingman/SpecialFeaturesMenu.java
@@ -3,47 +3,63 @@ package edu.colorado.phet.movingman;
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 
 import javax.swing.*;
+
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * @author Sam Reid
  */
 public class SpecialFeaturesMenu extends JMenu {
-    public SpecialFeaturesMenu(final MovingManApplication movingManApplication) {//Pass the movingManApplication so we can access the active module
-        super("Special Features");//TODO: IL8N should re-use as many strings from the previous version as possible
-        final JRadioButtonMenuItem positiveToTheRightMenuItem = new JRadioButtonMenuItem(new AbstractAction("Positive to the Right") {
-            public void actionPerformed(ActionEvent e) {
-                movingManApplication.getPositiveToTheRight().setValue(true);
-            }
-        });
-        add(positiveToTheRightMenuItem);
-        final JRadioButtonMenuItem positiveToTheLeftMenuItem = new JRadioButtonMenuItem(new AbstractAction("Positive to the Left") {
-            public void actionPerformed(ActionEvent e) {
-                movingManApplication.getPositiveToTheRight().setValue(false);
-            }
-        });
-        add(positiveToTheLeftMenuItem);
+    public SpecialFeaturesMenu( final MovingManApplication movingManApplication ) {//Pass the movingManApplication so we can access the active module
+        super( "Special Features" );//TODO: IL8N should re-use as many strings from the previous version as possible
 
-        SimpleObserver updateButtonStates = new SimpleObserver() {
-            public void update() {
-                positiveToTheRightMenuItem.setSelected(movingManApplication.getPositiveToTheRight().getValue());
-                positiveToTheLeftMenuItem.setSelected(!movingManApplication.getPositiveToTheRight().getValue());
-            }
-        };
-        movingManApplication.getPositiveToTheRight().addObserver(updateButtonStates);
-        updateButtonStates.update();
-
-        addSeparator();
-
-        add(new JMenuItem(new AbstractAction("Evaluate Expression") {
-            public void actionPerformed(ActionEvent e) {
+        add( new JMenuItem( new AbstractAction( "Expression Evaluator" ) {
+            public void actionPerformed( ActionEvent e ) {
                 MovingManModule module = (MovingManModule) movingManApplication.getActiveModule();
-                if (!module.getEvaluateExpressionDialogVisible()) {
-                    module.setEvaluateExpressionDialogVisible(true);
+                if ( !module.getEvaluateExpressionDialogVisible() ) {
+                    module.setEvaluateExpressionDialogVisible( true );
                     //First time showing the dialog should pause the system instead of switching to expression mode immediately while playing
-                    module.setPaused(true);
+                    module.setPaused( true );
                 }
             }
-        }));
+        } ) );
+
+        final JCheckBoxMenuItem reverseXAxis = new JCheckBoxMenuItem( "Reverse x-axis", !movingManApplication.getPositiveToTheRight().getValue() );
+        reverseXAxis.addActionListener( new ActionListener() {
+            public void actionPerformed( ActionEvent e ) {
+                movingManApplication.getPositiveToTheRight().setValue( !reverseXAxis.isSelected() );
+            }
+        } );
+        movingManApplication.getPositiveToTheRight().addObserver( new SimpleObserver() {
+            public void update() {
+                reverseXAxis.setSelected( !movingManApplication.getPositiveToTheRight().getValue() );
+            }
+        } );
+        add( reverseXAxis );
+
+//        final JRadioButtonMenuItem positiveToTheRightMenuItem = new JRadioButtonMenuItem( new AbstractAction( "Positive to the Right" ) {
+//            public void actionPerformed( ActionEvent e ) {
+//                movingManApplication.getPositiveToTheRight().setValue( true );
+//            }
+//        } );
+//        add( positiveToTheRightMenuItem );
+//        final JRadioButtonMenuItem positiveToTheLeftMenuItem = new JRadioButtonMenuItem( new AbstractAction( "Positive to the Left" ) {
+//            public void actionPerformed( ActionEvent e ) {
+//                movingManApplication.getPositiveToTheRight().setValue( false );
+//            }
+//        } );
+//        add( positiveToTheLeftMenuItem );
+//
+//        SimpleObserver updateButtonStates = new SimpleObserver() {
+//            public void update() {
+//                positiveToTheRightMenuItem.setSelected( movingManApplication.getPositiveToTheRight().getValue() );
+//                positiveToTheLeftMenuItem.setSelected( !movingManApplication.getPositiveToTheRight().getValue() );
+//            }
+//        };
+//        movingManApplication.getPositiveToTheRight().addObserver( updateButtonStates );
+//        updateButtonStates.update();
+//
+//        addSeparator();
     }
 }
