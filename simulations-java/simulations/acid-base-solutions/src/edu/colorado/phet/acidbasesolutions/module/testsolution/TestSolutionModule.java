c76bd2b171d02a252521f3778c6a241938fd8cf6
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-06-22 04:01:05 +0000

    implement Reset All for existing functionality

--- simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/module/testsolution/TestSolutionModule.java
+++ simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/module/testsolution/TestSolutionModule.java
@@ -2,8 +2,10 @@
 
 package edu.colorado.phet.acidbasesolutions.module.testsolution;
 
+import edu.colorado.phet.acidbasesolutions.constants.ABSConstants;
 import edu.colorado.phet.acidbasesolutions.constants.ABSStrings;
 import edu.colorado.phet.acidbasesolutions.model.ABSModel;
+import edu.colorado.phet.acidbasesolutions.model.AqueousSolution;
 import edu.colorado.phet.acidbasesolutions.model.PureWaterSolution;
 import edu.colorado.phet.acidbasesolutions.module.ABSModule;
 
@@ -14,6 +16,8 @@ import edu.colorado.phet.acidbasesolutions.module.ABSModule;
  */
 public class TestSolutionModule extends ABSModule {
     
+    private static final AqueousSolution DEFAULT_SOLUTION = new PureWaterSolution();
+    
     private final ABSModel model;
     private final TestSolutionCanvas canvas;
     private final TestSolutionControlPanel controlPanel;
@@ -21,17 +25,24 @@ public class TestSolutionModule extends ABSModule {
     public TestSolutionModule() {
         super( ABSStrings.TEST_SOLUTION );
         
-        model = new ABSModel( new PureWaterSolution() );
+        model = new ABSModel( DEFAULT_SOLUTION );
         
         canvas = new TestSolutionCanvas( model );
         setSimulationPanel( canvas );
         
         controlPanel = new TestSolutionControlPanel( this, model );
         setControlPanel( controlPanel );
+        
+        reset();
     }
     
     @Override
     public void reset() {
-        //XXX
+        model.setSolution( DEFAULT_SOLUTION );
+        model.getMagnifyingGlass().setVisible( ABSConstants.MAGNIFYING_GLASS_VISIBLE );
+        model.getConcentrationGraph().setVisible( ABSConstants.CONCENTRATION_GRAPH_VISIBLE );
+        model.getPHMeter().setLocation( ABSConstants.PH_METER_LOCATION );
+        model.getPHMeter().setVisible( ABSConstants.PH_METER_VISIBLE );
+        model.setWaterVisible( ABSConstants.WATER_VISIBLE );
     }
 }
