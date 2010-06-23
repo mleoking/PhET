bc6ee677784ab94b12dd466e74fe6db93079bce7
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-06-23 14:42:01 +0000

    moving-man: Factored out creation of chart slider node thumbnails Factored out cursor usage

--- simulations-java/common/motion/src/edu/colorado/phet/common/motion/charts/TemporalChartSliderNode.java
+++ simulations-java/common/motion/src/edu/colorado/phet/common/motion/charts/TemporalChartSliderNode.java
@@ -1,7 +1,10 @@
 package edu.colorado.phet.common.motion.charts;
 
+import edu.colorado.phet.common.motion.tests.ColorArrows;
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
+import edu.colorado.phet.movingman.MovingManColorScheme;
 import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.nodes.PImage;
 
 import java.awt.*;
 import java.beans.PropertyChangeEvent;
@@ -12,6 +15,10 @@ import java.beans.PropertyChangeListener;
  */
 public class TemporalChartSliderNode extends MotionSliderNode.Vertical {
     private final TemporalChart temporalChart;
+
+    public TemporalChartSliderNode(final TemporalChart temporalChart, Color highlightColor) {
+        this(temporalChart, new PImage(ColorArrows.createArrow(MovingManColorScheme.POSITION_COLOR)), highlightColor);
+    }
 
     public TemporalChartSliderNode(final TemporalChart temporalChart, final PNode sliderThumb, Color highlightColor) {
         super(new Range(temporalChart.getMinRangeValue(), temporalChart.getMaxRangeValue()), 0.0, new Range(0, 100), sliderThumb, highlightColor);
