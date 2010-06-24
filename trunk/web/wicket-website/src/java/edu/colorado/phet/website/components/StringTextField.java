be0d61c3e218abdea3203c0c3863adc25f3d7f89
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-06-24 00:16:05 +0000

    website: Removed unused constructors and added docs to StringTextField

--- trunk/web/wicket-website/src/java/edu/colorado/phet/website/components/StringTextField.java
+++ trunk/web/wicket-website/src/java/edu/colorado/phet/website/components/StringTextField.java
@@ -3,24 +3,10 @@ package edu.colorado.phet.website.components;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.model.IModel;
 
+//TODO: Get rid of this class and use generics instead
 public class StringTextField extends TextField {
-    public StringTextField( String id ) {
-        super( id );
-        setType( String.class );
-    }
-
-    public StringTextField( String id, Class type ) {
-        super( id, type );
-        setType( String.class );
-    }
-
     public StringTextField( String id, IModel object ) {
         super( id, object );
-        setType( String.class );
-    }
-
-    public StringTextField( String id, IModel model, Class type ) {
-        super( id, model, type );
         setType( String.class );
     }
 }
