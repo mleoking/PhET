19174e24d5ac752e2c9623d937458d9cf390f651
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-06-29 18:30:57 +0000

    move reset functionality into model, factory for creating default solution for each module

--- trunk/simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/module/customsolution/CustomSolutionModule.java
+++ trunk/simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/module/customsolution/CustomSolutionModule.java
@@ -2,13 +2,10 @@
 
 package edu.colorado.phet.acidbasesolutions.module.customsolution;
 
-import edu.colorado.phet.acidbasesolutions.constants.ABSConstants;
 import edu.colorado.phet.acidbasesolutions.constants.ABSStrings;
 import edu.colorado.phet.acidbasesolutions.model.ABSModel;
 import edu.colorado.phet.acidbasesolutions.model.AqueousSolution;
-import edu.colorado.phet.acidbasesolutions.model.WeakAcidSolution;
-import edu.colorado.phet.acidbasesolutions.model.WeakBaseSolution;
-import edu.colorado.phet.acidbasesolutions.model.AqueousSolution.ICustomSolution;
+import edu.colorado.phet.acidbasesolutions.model.ABSModel.DefaultSolutionFactory;
 import edu.colorado.phet.acidbasesolutions.model.WeakAcidSolution.CustomWeakAcidSolution;
 import edu.colorado.phet.acidbasesolutions.module.ABSModule;
 
@@ -18,8 +15,6 @@ import edu.colorado.phet.acidbasesolutions.module.ABSModule;
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
 public class CustomSolutionModule extends ABSModule {
-
-    private static final AqueousSolution DEFAULT_SOLUTION = new CustomWeakAcidSolution();
     
     private final ABSModel model;
     private final CustomSolutionCanvas canvas;
@@ -28,7 +23,11 @@ public class CustomSolutionModule extends ABSModule {
     public CustomSolutionModule() {
         super( ABSStrings.CUSTOM_SOLUTION );
         
-        model = new ABSModel( DEFAULT_SOLUTION );
+        model = new ABSModel( new DefaultSolutionFactory() {
+            public AqueousSolution getDefaultSolution() {
+                return new CustomWeakAcidSolution();
+            }
+        });
         
         canvas = new CustomSolutionCanvas( model );
         setSimulationPanel( canvas );
@@ -41,19 +40,6 @@ public class CustomSolutionModule extends ABSModule {
     
     @Override
     public void reset() {
-        model.setSolution( DEFAULT_SOLUTION );
-        AqueousSolution solution = model.getSolution();
-        if ( solution instanceof ICustomSolution ) {
-            ICustomSolution customSolution = (ICustomSolution) solution;
-            customSolution.setConcentration( ABSConstants.CONCENTRATION_RANGE.getDefault() );
-            if ( customSolution instanceof WeakAcidSolution || customSolution instanceof WeakBaseSolution ) {
-                customSolution.setStrength( ABSConstants.WEAK_STRENGTH_RANGE.getDefault() );
-            }
-        }
-        model.getMagnifyingGlass().setVisible( ABSConstants.MAGNIFYING_GLASS_VISIBLE );
-        model.getConcentrationGraph().setVisible( ABSConstants.CONCENTRATION_GRAPH_VISIBLE );
-        model.getPHMeter().setLocation( ABSConstants.PH_METER_LOCATION );
-        model.getPHMeter().setVisible( ABSConstants.PH_METER_VISIBLE );
-        model.setWaterVisible( ABSConstants.WATER_VISIBLE );
+        model.reset();
     }
 }
