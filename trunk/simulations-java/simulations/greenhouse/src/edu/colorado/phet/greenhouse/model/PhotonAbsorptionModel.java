fcb3d8e79164ec18e2688eb432eb0f45a8da8f6f
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-07-01 21:12:55 +0000

    Initial submission of this class.

---
+++ trunk/simulations-java/simulations/greenhouse/src/edu/colorado/phet/greenhouse/model/PhotonAbsorptionModel.java
@@ -0,0 +1,43 @@
+/* Copyright 2010, University of Colorado */
+
+package edu.colorado.phet.greenhouse.model;
+
+import java.awt.geom.Dimension2D;
+import java.awt.geom.Point2D;
+
+/**
+ * Primary model for the Photon Absorption tab.  This models photons being
+ * absorbed (or often NOT absorbed) by various molecules.
+ * 
+ * @author John Blanco
+ */
+public class PhotonAbsorptionModel {
+
+    //----------------------------------------------------------------------------
+    // Class Data
+    //----------------------------------------------------------------------------
+    
+    // Constant that controls the point in model space where photons are
+    // emitted.
+    private static final Point2D PHOTON_EMISSION_LOCATION = new Point2D.Double(0, 0);
+
+    //----------------------------------------------------------------------------
+    // Instance Data
+    //----------------------------------------------------------------------------
+    
+    //----------------------------------------------------------------------------
+    // Constructor(s)
+    //----------------------------------------------------------------------------
+    
+    //----------------------------------------------------------------------------
+    // Methods
+    //----------------------------------------------------------------------------
+    
+    public Point2D getPhotonEmissionLocation(){
+        return PHOTON_EMISSION_LOCATION;
+    }
+
+    //----------------------------------------------------------------------------
+    // Inner Classes and Interfaces
+    //----------------------------------------------------------------------------
+}
