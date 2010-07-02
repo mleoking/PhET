a9d882bd29be8d5d4624af026b91bd4e95ca7f4e
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-07-02 06:25:23 +0000

    website: refactor to use ChangePasswordPanel as a single instance.

--- trunk/web/wicket-website/src/java/edu/colorado/phet/website/authentication/ResetPasswordCallbackPage.java
+++ trunk/web/wicket-website/src/java/edu/colorado/phet/website/authentication/ResetPasswordCallbackPage.java
@@ -1,23 +1,69 @@
 package edu.colorado.phet.website.authentication;
 
-import org.apache.wicket.PageParameters;
-import org.apache.wicket.model.ResourceModel;
+import java.util.List;
 
-import edu.colorado.phet.website.authentication.panels.ResetPasswordCallbackPanel;
+import org.apache.log4j.Logger;
+import org.apache.wicket.PageParameters;
+import org.apache.wicket.RestartResponseAtInterceptPageException;
+import org.apache.wicket.model.ResourceModel;
+import org.hibernate.Session;
+
+import edu.colorado.phet.website.authentication.panels.ChangePasswordPanel;
+import edu.colorado.phet.website.content.ErrorPage;
+import edu.colorado.phet.website.data.PhetUser;
+import edu.colorado.phet.website.data.ResetPasswordRequest;
 import edu.colorado.phet.website.templates.PhetMenuPage;
+import edu.colorado.phet.website.util.HibernateTask;
+import edu.colorado.phet.website.util.HibernateUtils;
 import edu.colorado.phet.website.util.PageContext;
 import edu.colorado.phet.website.util.PhetUrlMapper;
 import edu.colorado.phet.website.util.links.AbstractLinker;
 import edu.colorado.phet.website.util.links.RawLinkable;
 
 /**
+ * The landing page for when a user clicks on a forgot-my-password link in their email.
+ *
  * @author Sam Reid
  */
 public class ResetPasswordCallbackPage extends PhetMenuPage {
+
+    private static Logger logger = Logger.getLogger( ResetPasswordCallbackPage.class );
+
     public ResetPasswordCallbackPage( PageParameters parameters ) {
         super( parameters );
         addTitle( new ResourceModel( "resetPasswordCallback.title" ) );
-        add( new ResetPasswordCallbackPanel( "reset-password-callback-panel", getPageContext(), parameters.getString( "key" ) ) );
+
+        // for now, if a step here fails we will just go to the error page
+        PhetUser user = lookupUserForResetPasswordKey( parameters.getString( "key" ) );
+        if ( user == null ) {
+            throw new RestartResponseAtInterceptPageException( ErrorPage.class );
+        }
+
+        add( new ChangePasswordPanel( "reset-password-callback-panel", getPageContext(), user, false ) );
+    }
+
+    private PhetUser lookupUserForResetPasswordKey( final String key ) {
+        final PhetUser[] savedUser = {null};
+        boolean success = HibernateUtils.wrapTransaction( getHibernateSession(), new HibernateTask() {
+            public boolean run( Session session ) {
+                List matches = session.createQuery( "select ts from ResetPasswordRequest as ts where ts.key = :key" ).setString( "key", key ).list();
+                if ( matches.size() == 0 ) {
+                    logger.info( "No matches in database for key: " + key );
+                    return false;
+                }
+                else if ( matches.size() > 1 ) {
+                    logger.warn( "Multiple database matches for key: " + key );
+                    return false;
+                }
+                else {
+                    ResetPasswordRequest request = (ResetPasswordRequest) matches.get( 0 );
+                    PhetUser user = request.getPhetUser();
+                    savedUser[0] = user;
+                    return true;
+                }
+            }
+        } );
+        return savedUser[0];//if success==false, this still returns null
     }
 
     public static void addToMapper( PhetUrlMapper mapper ) {
--- trunk/web/wicket-website/src/java/edu/colorado/phet/website/authentication/panels/ChangePasswordPanel.java
+++ trunk/web/wicket-website/src/java/edu/colorado/phet/website/authentication/panels/ChangePasswordPanel.java
@@ -12,6 +12,7 @@ import org.hibernate.Session;
 
 import edu.colorado.phet.website.authentication.AuthenticatedPage;
 import edu.colorado.phet.website.authentication.PhetSession;
+import edu.colorado.phet.website.components.InvisibleComponent;
 import edu.colorado.phet.website.data.PhetUser;
 import edu.colorado.phet.website.panels.PhetPanel;
 import edu.colorado.phet.website.util.HibernateTask;
@@ -22,12 +23,32 @@ import edu.colorado.phet.website.util.PageContext;
  * @author Sam Reid
  */
 public class ChangePasswordPanel extends PhetPanel {
-    private static Logger logger = Logger.getLogger( ChangePasswordPanel.class );
+
+    private final boolean shouldConfirmCurrentPassword; // whether the
+    private final PhetUser userToChange; // not required to be persistent with hibernate. don't rely on this in onsubmit for anything other than the id
+
     private FeedbackPanel feedback;
 
-    public ChangePasswordPanel( String id, PageContext context ) {
+    private static Logger logger = Logger.getLogger( ChangePasswordPanel.class );
+
+    /**
+     * Add a change password panel that will allow changing the password of the passed in user. It can optionally
+     * contain a text box to confirm the current user's password before changing.
+     *
+     * @param id                           Wicket id
+     * @param context                      Page context
+     * @param user                         A PhetUser if we do not need to check the current password
+     * @param shouldConfirmCurrentPassword Whether to prompt for the current user's password
+     */
+    public ChangePasswordPanel( String id, PageContext context, PhetUser user, boolean shouldConfirmCurrentPassword ) {
         super( id, context );
-        AuthenticatedPage.checkSignedIn();
+
+        // don't check here, because if someone forgot their password they will not be logged in
+        //AuthenticatedPage.checkSignedIn();
+
+        this.shouldConfirmCurrentPassword = shouldConfirmCurrentPassword;
+        this.userToChange = user;
+
         feedback = new FeedbackPanel( "feedback" );
         add( feedback );
         add( new SetPasswordForm( "set-password-form", context ) );
@@ -43,14 +64,34 @@ public class ChangePasswordPanel extends PhetPanel {
             super( id );
             this.context = context;
 
-            currentPasswordTextField = new PasswordTextField( "current-password", new Model<String>( "" ) );
-            currentPasswordTextField.setRequired( false );//Since some users may still have password = ""
-            add( currentPasswordTextField );
+            if ( shouldConfirmCurrentPassword ) {
+                currentPasswordTextField = new PasswordTextField( "current-password", new Model<String>( "" ) );
+                currentPasswordTextField.setRequired( false );//Since some users may still have password = ""
+                add( currentPasswordTextField );
+
+                // verify the user's current password
+                add( new AbstractFormValidator() {
+                    public FormComponent[] getDependentFormComponents() {
+                        return new FormComponent[]{currentPasswordTextField};
+                    }
+
+                    public void validate( Form<?> form ) {
+                        if ( !PhetSession.passwordEquals( PhetSession.get().getUser().getHashedPassword(), currentPasswordTextField.getInput() ) ) {
+                            error( currentPasswordTextField, "changePassword.validation.incorrectPassword" );
+                        }
+                    }
+                } );
+            }
+            else {
+                add( new InvisibleComponent( "current-password" ) ); // hide the current password control. wicket:enclosure hides the HTML content surrounding it
+            }
             newPasswordTextField = new PasswordTextField( "new-password", new Model<String>( "" ) );
             add( newPasswordTextField );
             confirmNewPasswordTextField = new PasswordTextField( "confirm-new-password", new Model<String>( "" ) );
             confirmNewPasswordTextField.setRequired( false ); //If this is required, then the feedback can appear twice; the requirement is handled in the newPassword textfield
             add( confirmNewPasswordTextField );
+
+            // make sure the user's new password matches
             add( new AbstractFormValidator() {
                 public FormComponent[] getDependentFormComponents() {
                     return new FormComponent[]{newPasswordTextField, confirmNewPasswordTextField};
@@ -62,34 +103,21 @@ public class ChangePasswordPanel extends PhetPanel {
                     }
                 }
             } );
-
-            add( new AbstractFormValidator() {
-                public FormComponent[] getDependentFormComponents() {
-                    return new FormComponent[]{currentPasswordTextField};
-                }
-
-                public void validate( Form<?> form ) {
-                    if ( !PhetSession.passwordEquals( PhetSession.get().getUser().getHashedPassword(), currentPasswordTextField.getInput() ) ) {
-                        error( currentPasswordTextField, "changePassword.validation.incorrectPassword" );
-                    }
-                }
-            } );
         }
 
         @Override
         protected void onSubmit() {
-            final PhetUser currentUser = PhetSession.get().getUser();
             boolean success = HibernateUtils.wrapTransaction( getHibernateSession(), new HibernateTask() {
                 public boolean run( Session session ) {
-                    PhetUser user = (PhetUser) session.load( PhetUser.class, currentUser.getId() );
+                    PhetUser user = (PhetUser) session.load( PhetUser.class, userToChange.getId() );
                     user.setPassword( newPasswordTextField.getModelObject() );
                     session.update( user );
                     return true;
                 }
             } );
-            logger.info( "Finished hibernate: success = " + success );
+            logger.debug( "Finished hibernate: success = " + success );
             if ( success ) {
-                currentUser.setPassword( newPasswordTextField.getModelObject() );
+                userToChange.setPassword( newPasswordTextField.getModelObject() );
 
                 //redirect to the success page  
                 getRequestCycle().setRequestTarget( new RedirectRequestTarget( ChangePasswordSuccessPanel.getLinker().getRawUrl( context, getPhetCycle() ) ) );
