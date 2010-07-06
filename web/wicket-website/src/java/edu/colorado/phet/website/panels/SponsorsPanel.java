e097f50db2571a6b163ed1f550ba88190853c8f2
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-07-06 10:14:27 +0000

    website: fixed improper caching of sponsors panel due to string translations

--- web/wicket-website/src/java/edu/colorado/phet/website/panels/SponsorsPanel.java
+++ web/wicket-website/src/java/edu/colorado/phet/website/panels/SponsorsPanel.java
@@ -2,14 +2,18 @@ package edu.colorado.phet.website.panels;
 
 import org.apache.wicket.behavior.HeaderContributor;
 
+import edu.colorado.phet.website.cache.EventDependency;
 import edu.colorado.phet.website.components.StaticImage;
 import edu.colorado.phet.website.constants.CSS;
 import edu.colorado.phet.website.constants.Images;
 import edu.colorado.phet.website.content.about.AboutSponsorsPanel;
+import edu.colorado.phet.website.data.*;
+import edu.colorado.phet.website.data.util.HibernateEventListener;
+import edu.colorado.phet.website.data.util.IChangeListener;
 import edu.colorado.phet.website.util.PageContext;
 
 public class SponsorsPanel extends PhetPanel {
-    public SponsorsPanel( String id, PageContext context ) {
+    public SponsorsPanel( String id, final PageContext context ) {
         super( id, context );
 
         add( HeaderContributor.forCss( CSS.SPONSORS ) );
@@ -20,5 +24,21 @@ public class SponsorsPanel extends PhetPanel {
         add( new StaticImage( "hewlett-logo", Images.LOGO_HEWLETT, "The Hewlett Logo" ) );
         add( new StaticImage( "nsf-logo", Images.LOGO_NSF, "The NSF Logo" ) );
         add( new StaticImage( "ecsme-logo", Images.LOGO_ECSME, "The King Saud (ESCME) Logo" ) );
+
+        addDependency( new EventDependency() {
+
+            private IChangeListener stringListener;
+
+            @Override
+            protected void addListeners() {
+                stringListener = createTranslationChangeInvalidator( context.getLocale() );
+                HibernateEventListener.addListener( TranslatedString.class, stringListener );
+            }
+
+            @Override
+            protected void removeListeners() {
+                HibernateEventListener.removeListener( TranslatedString.class, stringListener );
+            }
+        } );
     }
 }
