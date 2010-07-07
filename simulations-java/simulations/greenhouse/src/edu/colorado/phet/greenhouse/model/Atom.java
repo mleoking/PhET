25f3c4e1f5a373c3f4250833f1a1e0a869b05417
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-07-07 18:05:40 +0000

    Initial submission of this class.

---
+++ simulations-java/simulations/greenhouse/src/edu/colorado/phet/greenhouse/model/Atom.java
@@ -0,0 +1,76 @@
+/* Copyright 2010, University of Colorado */
+
+package edu.colorado.phet.greenhouse.model;
+
+import java.awt.Color;
+import java.awt.geom.Point2D;
+
+import edu.colorado.phet.common.phetcommon.util.SimpleObservable;
+
+/**
+ * Class that represents an atom in the model.  This is used in the
+ * microscopic view of photon abosorption.  This is an abstract class, and
+ * it is expected that it be extended by specific atoms.
+ * 
+ * @author John Blanco
+ */
+public abstract class Atom extends SimpleObservable {
+    
+    //----------------------------------------------------------------------------
+    // Class Data
+    //----------------------------------------------------------------------------
+
+    //----------------------------------------------------------------------------
+    // Instance Data
+    //----------------------------------------------------------------------------
+
+    private Point2D position;
+    private final Color representationColor;
+    private final double radius;
+    private final double mass;
+
+    //----------------------------------------------------------------------------
+    // Constructor(s)
+    //----------------------------------------------------------------------------
+    
+    public Atom( Color representationColor, double radius, double mass, Point2D position ) {
+        super();
+        this.representationColor = representationColor;
+        this.radius = radius;
+        this.mass = mass;
+        this.position = position;
+    }
+    
+    public Atom( Color representationColor, double radius, double mass ) {
+        this (representationColor, radius, mass, new Point2D.Double(0, 0));
+    }
+
+    //------------------------------------------------------------------------
+    // Methods
+    //------------------------------------------------------------------------
+    
+    public Point2D getPosition() {
+        return position;
+    }
+    
+    public void setPosition( Point2D position ) {
+        this.position = position;
+    }
+    
+    public Color getRepresentationColor() {
+        return representationColor;
+    }
+    
+    public double getRadius() {
+        return radius;
+    }
+    
+    public double getMass() {
+        return mass;
+    }
+    
+    //------------------------------------------------------------------------
+    // Inner Classes and Interfaces
+    //------------------------------------------------------------------------
+    
+}
