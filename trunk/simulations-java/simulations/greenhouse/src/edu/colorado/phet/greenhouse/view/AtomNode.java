55b5181921b373f50ae081a743d1da20351d877d
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-07-07 23:18:40 +0000

    Added a gradient to the atom node for better appearance.

--- trunk/simulations-java/simulations/greenhouse/src/edu/colorado/phet/greenhouse/view/AtomNode.java
+++ trunk/simulations-java/simulations/greenhouse/src/edu/colorado/phet/greenhouse/view/AtomNode.java
@@ -2,11 +2,16 @@
 
 package edu.colorado.phet.greenhouse.view;
 
+import java.awt.Color;
+import java.awt.Paint;
 import java.awt.geom.Ellipse2D;
+import java.awt.geom.Point2D;
 
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform2D;
+import edu.colorado.phet.common.phetcommon.view.util.ColorUtils;
 import edu.colorado.phet.greenhouse.model.Atom;
+import edu.colorado.phet.mri.util.RoundGradientPaint;
 import edu.umd.cs.piccolo.nodes.PPath;
 
 
@@ -35,10 +40,14 @@ public class AtomNode extends PPath {
     public AtomNode( Atom atom, ModelViewTransform2D mvt ){
         this.atom = atom;
         this.mvt = mvt;
-        setPaint( atom.getRepresentationColor() );
         double transformedRadius = mvt.modelToViewDifferentialXDouble( atom.getRadius() );
+        Paint roundGradienPaint = new RoundGradientPaint( -transformedRadius/2, -transformedRadius/2, Color.WHITE, 
+                new Point2D.Double(transformedRadius/2, transformedRadius/2), 
+                ColorUtils.darkerColor( atom.getRepresentationColor(), 0.1 ));
+        setPaint( roundGradienPaint );
         setPathTo( new Ellipse2D.Double( -transformedRadius, -transformedRadius,
                 transformedRadius * 2, transformedRadius * 2 ) );
+        setStroke( null );
         atom.addObserver( new SimpleObserver() {
             public void update() {
                 updatePosition();
