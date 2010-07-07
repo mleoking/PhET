5ae3c6108c4c87cb8055ffec14e47de0cf1767f8
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-07-07 18:06:03 +0000

    Adding carbon and oxygen atoms to the model.

---
+++ simulations-java/simulations/greenhouse/src/edu/colorado/phet/greenhouse/model/CarbonAtom.java
@@ -0,0 +1,35 @@
+/* Copyright 2010, University of Colorado */
+
+package edu.colorado.phet.greenhouse.model;
+
+import java.awt.Color;
+import java.awt.geom.Point2D;
+
+
+/**
+ * Class that represents an atom of Carbon in the model.
+ * 
+ * @author John Blanco
+ */
+public class CarbonAtom extends Atom {
+    
+    //------------------------------------------------------------------------
+    // Class Data
+    //------------------------------------------------------------------------
+    
+    private static final Color REPRESENTATION_COLOR = Color.GRAY;
+    private static final double MASS = 12.011;   // In atomic mass units (AMU).
+    private static final double RADIUS = 70;     // In picometers.
+
+    //------------------------------------------------------------------------
+    // Constructor(s)
+    //------------------------------------------------------------------------
+
+    public CarbonAtom( Point2D position ) {
+        super( REPRESENTATION_COLOR, RADIUS, MASS, position );
+    }
+    
+    public CarbonAtom() {
+        this( new Point2D.Double(0, 0) );
+    }    
+}
