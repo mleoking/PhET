c917f03f9f94ce482d80b939a8bfc7386b0b0dc3
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-07-08 17:02:37 +0000

    Added initial support for double bonds.

--- trunk/simulations-java/simulations/greenhouse/src/edu/colorado/phet/greenhouse/model/AtomicBond.java
+++ trunk/simulations-java/simulations/greenhouse/src/edu/colorado/phet/greenhouse/model/AtomicBond.java
@@ -21,15 +21,21 @@ public class AtomicBond extends SimpleObservable {
     // Instance Data
     // ------------------------------------------------------------------------
     
-    private final Atom atom1, atom2;
+    private final Atom atom1, atom2;  // Atoms involved in the bond.
+    private final int bondCount;      // Indicates whether this is a single, 
+                                      // double, triple, etc. bond.
 
     // ------------------------------------------------------------------------
     // Constructor(s)
     // ------------------------------------------------------------------------
 
-    public AtomicBond( Atom atom1, Atom atom2 ) {
+    /**
+     * Constructor.
+     */
+    public AtomicBond( Atom atom1, Atom atom2, int bondCount ) {
         this.atom1 = atom1;
         this.atom2 = atom2;
+        this.bondCount = bondCount;
         
         // Listen to the atoms that are involved in this bond and send an
         // update notification whenever they move.
@@ -44,6 +50,13 @@ public class AtomicBond extends SimpleObservable {
             }
         });
     }
+    
+    /**
+     * Constructor that assumes a single bond.
+     */
+    public AtomicBond(Atom atom1, Atom atom2){
+        this(atom1, atom2, 1);
+    }
 
     // ------------------------------------------------------------------------
     // Methods
@@ -56,6 +69,10 @@ public class AtomicBond extends SimpleObservable {
     
     public Atom getAtom2() {
         return atom2;
+    }
+
+    public int getBondCount() {
+        return bondCount;
     }
     
     // ------------------------------------------------------------------------
--- trunk/simulations-java/simulations/greenhouse/src/edu/colorado/phet/greenhouse/view/AtomicBondNode.java
+++ trunk/simulations-java/simulations/greenhouse/src/edu/colorado/phet/greenhouse/view/AtomicBondNode.java
@@ -3,12 +3,15 @@
 package edu.colorado.phet.greenhouse.view;
 
 import java.awt.BasicStroke;
+import java.awt.Color;
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform2D;
+import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.colorado.phet.greenhouse.model.AtomicBond;
+import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.nodes.PPath;
 
 
@@ -17,13 +20,17 @@ import edu.umd.cs.piccolo.nodes.PPath;
  * 
  * @author John Blanco
  */
-public class AtomicBondNode extends PPath {
+public class AtomicBondNode extends PNode {
     
     // ------------------------------------------------------------------------
     // Class Data
     // ------------------------------------------------------------------------
     
-    private static double BOND_WIDTH = 10; // In model coords, which are essentially pixels.
+    private static double BOND_WIDTH_SINGLE = 10; // In model coords, which are essentially pixels.
+    private static double BOND_WIDTH_DOUBLE = 8;  // In model coords, which are essentially pixels.
+    private static double BOND_WIDTH_TRIPLE = 5;  // In model coords, which are essentially pixels.
+    
+    private static Color BOND_COLOR = Color.ORANGE;
 
     // ------------------------------------------------------------------------
     // Instance Data
@@ -37,26 +44,59 @@ public class AtomicBondNode extends PPath {
     // ------------------------------------------------------------------------
     
     public AtomicBondNode( AtomicBond atomicBond, ModelViewTransform2D mvt ){
+        assert atomicBond.getBondCount() > 0 && atomicBond.getBondCount() <=3;  // Only single through triple bonds currently supported.
         this.atomicBond = atomicBond;
         this.mvt = mvt;
-        setStrokePaint( atomicBond.getAtom1().getRepresentationColor() );
-        setStroke( new BasicStroke( (float)BOND_WIDTH ) );
         atomicBond.addObserver( new SimpleObserver() {
             public void update() {
-                updateShape();
+                updateRepresentation();
             }
         });
-        updateShape();
+        updateRepresentation();
     }
 
     // ------------------------------------------------------------------------
     // Methods
     // ------------------------------------------------------------------------
 
-    private void updateShape() {
-        Point2D transformedPt1 = mvt.modelToViewDouble( atomicBond.getAtom1().getPosition() );
-        Point2D transformedPt2 = mvt.modelToViewDouble( atomicBond.getAtom2().getPosition() );
-        setPathTo( new Line2D.Double( transformedPt1, transformedPt2 ) );
+    private void updateRepresentation() {
+        removeAllChildren();  // Clear out any previous representations.
+        switch (atomicBond.getBondCount()){
+        case 1:
+            // Single bond, so connect it from the center of one atom to the
+            // center of the other.
+            Point2D transformedPt1 = mvt.modelToViewDouble( atomicBond.getAtom1().getPosition() );
+            Point2D transformedPt2 = mvt.modelToViewDouble( atomicBond.getAtom2().getPosition() );
+            PPath bond = new PhetPPath(new BasicStroke((float)BOND_WIDTH_SINGLE), BOND_COLOR);
+            bond.setPathTo( new Line2D.Double( transformedPt1, transformedPt2 ) );
+            addChild(bond);
+            break;
+            
+        case 2:
+            // Double bond.
+            // TODO: This is a temporary version to support horizontal molecules,
+            // needs to be generalized.
+            double transformedRadius = mvt.modelToViewDifferentialXDouble( Math.min( atomicBond.getAtom1().getRadius(),
+                    atomicBond.getAtom2().getRadius() ) );
+            Point2D p1 = mvt.modelToViewDouble( atomicBond.getAtom1().getPosition() );
+            Point2D p2 = mvt.modelToViewDouble( atomicBond.getAtom2().getPosition() );
+            PPath bond1 = new PhetPPath(new BasicStroke((float)BOND_WIDTH_DOUBLE), BOND_COLOR);
+            bond1.setPathTo( new Line2D.Double( p1.getX(), p1.getY() + transformedRadius / 3, p2.getX(), p2.getY() + transformedRadius / 3) );
+            PPath bond2 = new PhetPPath(new BasicStroke((float)BOND_WIDTH_DOUBLE), BOND_COLOR);
+            bond2.setPathTo( new Line2D.Double( p1.getX(), p1.getY() - transformedRadius / 3, p2.getX(), p2.getY() - transformedRadius / 3) );
+            addChild(bond1);
+            addChild(bond2);
+            break;
+            
+        case 3:
+            // TODO
+            break;
+            
+        default:
+            System.err.println(getClass().getName() + " - Error: Can't represent bond number, value = " + atomicBond.getBondCount());
+            assert false;
+            break;
+        }
     }
 
     // ------------------------------------------------------------------------
