58bf83dd80630ece152177a1df314988840f9109
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-07-09 07:32:54 +0000

    moving-man: Only show the domain axis labels for the bottom chart

--- simulations-java/common/motion/src/edu/colorado/phet/common/motion/charts/MultiControlChart.java
+++ simulations-java/common/motion/src/edu/colorado/phet/common/motion/charts/MultiControlChart.java
@@ -14,9 +14,19 @@ public class MultiControlChart extends PNode {
     private double height;
 
     public MultiControlChart(final MinimizableControlChart[] charts) {
+        for (MinimizableControlChart chart : charts) {
+            children.add(chart);
+            addChild(chart);
+            chart.getMaximized().addObserver(new SimpleObserver() {
+                public void update() {
+                    updateLayout();
+                }
+            });
+        }
+
+        //Only show the topmost horizontal zoom control because it controls all axes and cleans up the screen to omit the extraneous controls
         final SimpleObserver updateHorizontalZoomButtonVisibility = new SimpleObserver() {
             public void update() {
-                //Only show the topmost horizontal zoom control because it controls all axes and cleans up the screen to omit the extraneous controls
                 for (int i = 0; i < charts.length; i++) {
                     if (charts[i].getMaximized().getValue()) {
                         setHorizontalZoomButtonVisible(i);
@@ -26,25 +36,46 @@ public class MultiControlChart extends PNode {
             }
         };
         for (MinimizableControlChart chart : charts) {
-            children.add(chart);
-            addChild(chart);
             chart.getMaximized().addObserver(updateHorizontalZoomButtonVisibility);
-            chart.getMaximized().addObserver(new SimpleObserver() {
-                public void update() {
-                    updateLayout();
-                }
-            } );
         }
         updateHorizontalZoomButtonVisibility.update();
 
+        //Only show the domain axis labels for the bottom chart
+        final SimpleObserver updateRangeLabelsVisible = new SimpleObserver() {
+            public void update() {
+                for (int i = charts.length - 1; i >= 0; i--) {
+                    if (charts[i].getMaximized().getValue()) {
+                        setDomainAxisLabelsVisible(i);
+                        break;
+                    }
+                }
+            }
+        };
+        for (MinimizableControlChart chart : charts) {
+            chart.getMaximized().addObserver(updateRangeLabelsVisible);
+        }
+        updateRangeLabelsVisible.update();
+
+        //Synchronize the chart bounds
         for (final MinimizableControlChart chart : charts) {
             chart.getChartNode().getDataModelBounds().addObserver(new SimpleObserver() {
                 public void update() {
                     for (MinimizableControlChart chart2 : charts) {
-                        chart2.getChartNode().getDataModelBounds().setHorizontalRange(chart.getChartNode().getDataModelBounds().getMinX(),chart.getChartNode().getDataModelBounds().getMaxX() );
+                        chart2.getChartNode().getDataModelBounds().setHorizontalRange(chart.getChartNode().getDataModelBounds().getMinX(), chart.getChartNode().getDataModelBounds().getMaxX());
                     }
                 }
             });
+        }
+    }
+
+    /**
+     * Show the domain axis labels for the specified chart, hiding them from all other charts.
+     *
+     * @param index the index of the chart that should have the visible range axis labels
+     */
+    private void setDomainAxisLabelsVisible(int index) {
+        for (int i = 0; i < children.size(); i++) {
+            children.get(i).setDomainAxisLabelsVisible(i == index);
         }
     }
 
@@ -59,14 +90,14 @@ public class MultiControlChart extends PNode {
     }
 
     public void setSize(double width, double height) {
-        this.width=width;
-        this.height=height;
+        this.width = width;
+        this.height = height;
         updateLayout();
     }
 
     public boolean setBounds(double x, double y, double width, double height) {
-        setSize(width,height);
-        setOffset(x,y);
+        setSize(width, height);
+        setOffset(x, y);
         return true;
     }
 }
