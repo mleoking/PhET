26342491e7df94f2730066013b759667b5a84efd
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-07-09 17:41:35 +0000

    Fixed reset all for charts minimize/maximize

--- simulations-java/common/motion/src/edu/colorado/phet/common/motion/charts/MinimizeMaximizeButton.java
+++ simulations-java/common/motion/src/edu/colorado/phet/common/motion/charts/MinimizeMaximizeButton.java
@@ -1,30 +1,51 @@
 package edu.colorado.phet.common.motion.charts;
 
-import edu.colorado.phet.common.piccolophet.nodes.MinimizeMaximizeNode;
-import edu.umd.cs.piccolo.PNode;
-
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+
+import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
+import edu.colorado.phet.common.piccolophet.nodes.MinimizeMaximizeNode;
+import edu.umd.cs.piccolo.PNode;
 
 /**
  * @author Sam Reid
  */
 public class MinimizeMaximizeButton extends PNode {
     private MinimizeMaximizeNode node;
-    private MutableBoolean maximized = new MutableBoolean(true);
+    private MutableBoolean maximized;
+    private final boolean defaultMaximizedValue;
 
     public MinimizeMaximizeButton(String title) {
+       this( title, true );
+    }
+    public MinimizeMaximizeButton(String title, boolean maximizedValue ) {
+       
         node = new MinimizeMaximizeNode(title, MinimizeMaximizeNode.BUTTON_RIGHT);
-        node.setMaximized(maximized.getValue());
+        addChild(node);
+        
+        SimpleObserver maximizedObserver = new SimpleObserver() {
+            public void update() {
+                node.setMaximized( maximized.getValue() );
+            }
+        };
+        
+        this.maximized = new MutableBoolean(maximizedValue);
+        this.maximized.addObserver( maximizedObserver );
+        maximizedObserver.update();
+        
+        this.defaultMaximizedValue = maximizedValue;
         node.addChangeListener(new ChangeListener() {
             public void stateChanged(ChangeEvent e) {
                 maximized.setValue(node.isMaximized());
             }
         });
-        addChild(node);
     }
 
     public MutableBoolean getMaximized() {
         return maximized;
+    }
+    
+    public void reset() {
+        maximized.setValue( defaultMaximizedValue );
     }
 }
