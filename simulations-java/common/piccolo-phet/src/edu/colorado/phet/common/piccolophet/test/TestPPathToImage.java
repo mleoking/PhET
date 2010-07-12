006b47d837f15f4529e632b1b60a5ac1adc16d1a
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-07-12 20:49:45 +0000

    updated TestPPathToImage: removed dependency on phet SwingUtils, updated docs, reformatted

--- simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/test/TestPPathToImage.java
+++ simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/test/TestPPathToImage.java
@@ -1,41 +1,38 @@
 package edu.colorado.phet.common.piccolophet.test;
 
-import java.awt.BasicStroke;
-import java.awt.Color;
+import java.awt.*;
 import java.awt.geom.Ellipse2D;
 import java.awt.image.RenderedImage;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
-import javax.swing.JFrame;
+import javax.swing.*;
 
-import edu.colorado.phet.common.phetcommon.view.util.SwingUtils;
 import edu.umd.cs.piccolo.PCanvas;
 import edu.umd.cs.piccolo.nodes.PImage;
 import edu.umd.cs.piccolo.nodes.PPath;
 
 /**
- * Tests PPath.toImage.  
- * On Macintosh, the right and bottom edges of the PPath are clipped by a few pixels.
+ * Tests PPath.toImage.
+ * The right and bottom edges of the PPath are clipped by a few pixels.
  */
 public class TestPPathToImage {
-    
+
     private static final float STROKE_WIDTH = 5f;
 
     public static void main( String[] args ) throws IOException {
-        
+
         // Create a circle node
         PPath pathNode = new PPath( new Ellipse2D.Double( 0, 0, 100, 100 ) );
         pathNode.setPaint( Color.RED );
         pathNode.setStroke( new BasicStroke( STROKE_WIDTH ) );
-        
+
         // Convert it to an image
         PImage imageNode = new PImage( pathNode.toImage() );
         imageNode.setOffset( 10, 10 );
-        
+
         // Canvas
         PCanvas canvas = new PCanvas();
         canvas.getLayer().addChild( imageNode );
@@ -44,15 +41,18 @@ public class TestPPathToImage {
         JFrame frame = new JFrame();
         frame.setContentPane( canvas );
         frame.setSize( 150, 150 );
-        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE ); 
-        SwingUtils.centerWindowOnScreen( frame );
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        Dimension screenSize = tk.getScreenSize();
+        frame.setLocation( (int) ( screenSize.getWidth() / 2 - frame.getWidth() / 2 ),
+                           (int) ( screenSize.getHeight() / 2 - frame.getHeight() / 2 ) );
         frame.setVisible( true );
 
         //Save to a file for inspection
-        final File file = new File("TestPPathToImage.png");
-        ImageIO.write((RenderedImage) pathNode.toImage(),"PNG",new FileOutputStream(file));
-        
+        final File file = new File( "TestPPathToImage.png" );
+        ImageIO.write( (RenderedImage) pathNode.toImage(), "PNG", new FileOutputStream( file ) );
+
         //Indicate where it was saved, since we used the working directory, which may be unknown
-        System.out.println("Saved file to " + file.getAbsolutePath());
+        System.out.println( "Saved file to " + file.getAbsolutePath() );
     }
 }
