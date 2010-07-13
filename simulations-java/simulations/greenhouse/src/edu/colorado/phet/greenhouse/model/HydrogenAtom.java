c976da2eb1b6ca031af499ab651363b7dc36b778
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-07-13 21:14:59 +0000

    Initial submission of this class.

---
+++ simulations-java/simulations/greenhouse/src/edu/colorado/phet/greenhouse/model/HydrogenAtom.java
@@ -0,0 +1,35 @@
+/* Copyright 2010, University of Colorado */
+
+package edu.colorado.phet.greenhouse.model;
+
+import java.awt.Color;
+import java.awt.geom.Point2D;
+
+
+/**
+ * Class that represents an atom of hydrogen in the model.
+ * 
+ * @author John Blanco
+ */
+public class HydrogenAtom extends Atom {
+    
+    //------------------------------------------------------------------------
+    // Class Data
+    //------------------------------------------------------------------------
+    
+    private static final Color REPRESENTATION_COLOR = Color.LIGHT_GRAY;
+    private static final double MASS = 1;   // In atomic mass units (AMU).
+    private static final double RADIUS = 37;     // In picometers.
+
+    //------------------------------------------------------------------------
+    // Constructor(s)
+    //------------------------------------------------------------------------
+
+    public HydrogenAtom( Point2D position ) {
+        super( REPRESENTATION_COLOR, RADIUS, MASS, position );
+    }
+    
+    public HydrogenAtom() {
+        this( new Point2D.Double(0, 0) );
+    }    
+}
