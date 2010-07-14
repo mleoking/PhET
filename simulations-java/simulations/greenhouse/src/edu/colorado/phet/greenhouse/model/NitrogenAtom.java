2f344c7b10860e9a0ff19afaa46a54ae07abffd4
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-07-14 17:34:51 +0000

    Initial submission of this class.

---
+++ simulations-java/simulations/greenhouse/src/edu/colorado/phet/greenhouse/model/NitrogenAtom.java
@@ -0,0 +1,35 @@
+/* Copyright 2010, University of Colorado */
+
+package edu.colorado.phet.greenhouse.model;
+
+import java.awt.Color;
+import java.awt.geom.Point2D;
+
+
+/**
+ * Class that represents an atom of nitrogen in the model.
+ * 
+ * @author John Blanco
+ */
+public class NitrogenAtom extends Atom {
+    
+    //------------------------------------------------------------------------
+    // Class Data
+    //------------------------------------------------------------------------
+    
+    private static final Color REPRESENTATION_COLOR = Color.BLUE;
+    private static final double MASS = 14.00674;   // In atomic mass units (AMU).
+    private static final double RADIUS = 75;       // In picometers.
+
+    //------------------------------------------------------------------------
+    // Constructor(s)
+    //------------------------------------------------------------------------
+
+    public NitrogenAtom( Point2D position ) {
+        super( REPRESENTATION_COLOR, RADIUS, MASS, position );
+    }
+    
+    public NitrogenAtom() {
+        this( new Point2D.Double(0, 0) );
+    }    
+}
