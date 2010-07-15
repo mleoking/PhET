a469ddf8bd33ef04c656c7d650f5116e9191af99
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-07-15 04:18:46 +0000

    docs, Darkened the color of acceleration so that it stands out against the grass

--- trunk/simulations-java/simulations/moving-man/src/edu/colorado/phet/movingman/MovingManColorScheme.java
+++ trunk/simulations-java/simulations/moving-man/src/edu/colorado/phet/movingman/MovingManColorScheme.java
@@ -8,7 +8,8 @@ import java.awt.*;
 public class MovingManColorScheme {
     public static final Color POSITION_COLOR = Color.blue;
     public static final Color VELOCITY_COLOR = Color.red;
-    public static final Color ACCELERATION_COLOR = new Color(0,235,0);//same green as original moving man
+    //Original moving man used acceleration color = new Color(0,235,0);, but that was too similar to the grass color, so we made it darker.
+    public static final Color ACCELERATION_COLOR = new Color(34,139,34);//Forest Green from http://www.tayloredmktg.com/rgb/
 
     public static Color semitransparent(Color color, int alpha) {
         return new Color(color.getRed(), color.getGreen(), color.getBlue(), alpha);
