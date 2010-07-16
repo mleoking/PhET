047d380f811a7fa3ea56bab66d3b2839ec4ed291
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-07-16 19:47:38 +0000

    greenhouse: removed an errant dependency on scala

--- simulations-java/simulations/greenhouse/src/edu/colorado/phet/greenhouse/model/Molecule.java
+++ simulations-java/simulations/greenhouse/src/edu/colorado/phet/greenhouse/model/Molecule.java
@@ -6,9 +6,7 @@ import java.awt.geom.Dimension2D;
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
 import java.util.HashMap;
-
-import scala.util.Random;
-
+import java.util.Random;
 
 /**
  * Class that represents a molecule in the model.  This, by its nature, is
