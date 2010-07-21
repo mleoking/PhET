8221dc4691559ef2980b743435f0ebf18c6e94e3
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-07-21 19:14:59 +0000

    see #2446, adjust compass behavior to match electromagnet current source (DC=kinematic, AC=incremental)

--- simulations-java/simulations/faraday/src/edu/colorado/phet/faraday/module/ElectromagnetModule.java
+++ simulations-java/simulations/faraday/src/edu/colorado/phet/faraday/module/ElectromagnetModule.java
@@ -129,7 +129,6 @@ public class ElectromagnetModule extends FaradayModule {
         
         // Compass model
         _compassModel = new Compass( _electromagnetModel, getClock() );
-        _compassModel.setBehavior( Compass.INCREMENTAL_BEHAVIOR );
         _compassModel.setLocation( COMPASS_LOCATION );
         model.addModelElement( _compassModel );
         
@@ -245,6 +244,7 @@ public class ElectromagnetModule extends FaradayModule {
         _electromagnetModel.update();
         
         // Compass model
+        _compassModel.setBehavior( _acPowerSupplyModel.isEnabled() ? Compass.INCREMENTAL_BEHAVIOR : Compass.KINEMATIC_BEHAVIOR );
         _compassModel.setLocation( COMPASS_LOCATION );
         _compassModel.setEnabled( true );
         
