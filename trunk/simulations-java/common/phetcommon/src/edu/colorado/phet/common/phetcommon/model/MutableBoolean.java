ef6b1679f81dcfa7b66c7be363505c02dd39c1db
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-07-21 22:51:51 +0000

    phetcommon: Introduced Observable<T> class with MutableBoolean convenience subclass.

--- trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/model/MutableBoolean.java
+++ trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/model/MutableBoolean.java
@@ -1,33 +1,12 @@
 package edu.colorado.phet.common.phetcommon.model;
 
-import edu.colorado.phet.common.phetcommon.util.SimpleObservable;
-
 /**
  * This can be used to represent a Boolean value in a MVC style pattern.  It remembers its default value and can be reset.
  *
  * @author Sam Reid
  */
-public class MutableBoolean extends SimpleObservable {
-    private boolean value;
-    private final boolean defaultValue;
-
-    public MutableBoolean(boolean value) {
-        this.defaultValue = value;
-        this.value = value;
-    }
-
-    public void reset() {
-        setValue(defaultValue);
-    }
-
-    public boolean getValue() {
-        return value;
-    }
-
-    public void setValue(boolean value) {
-        if (this.value != value) {
-            this.value = value;
-            notifyObservers();
-        }
+public class MutableBoolean extends Observable<Boolean> {
+    public MutableBoolean( Boolean value ) {
+        super( value );
     }
 }
