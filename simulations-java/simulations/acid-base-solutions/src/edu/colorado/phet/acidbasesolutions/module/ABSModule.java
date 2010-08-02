435032b544d6cd0310978e06e43f5669e902f3fd
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-08-02 18:41:48 +0000

    delete ABSClock, document why dummy clock in ABSModule

--- simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/module/ABSModule.java
+++ simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/module/ABSModule.java
@@ -2,22 +2,20 @@
 
 package edu.colorado.phet.acidbasesolutions.module;
 
-import edu.colorado.phet.acidbasesolutions.model.ABSClock;
+import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
 import edu.colorado.phet.common.piccolophet.PiccoloModule;
 
 /**
  * Base class for all modules.
+ * The clock is irrelevant, not used in this simulation.
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
 public abstract class ABSModule extends PiccoloModule {
 
     public ABSModule( String name ) {
-        super( name, new ABSClock() );
-        
-        setLogoPanelVisible( false );
-        
-        // No clock controls
+        super( name, new ConstantDtClock( 1000, 1 ), true /* startsPaused */ );
         setClockControlPanel( null );
+        setLogoPanelVisible( false );
     }
 }
