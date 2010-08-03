e7af72af46828c2941bc178db26ebb9be32fc975
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-08-03 08:10:24 +0000

    website: error page improvements and session expired page created

--- web/wicket-website/src/java/edu/colorado/phet/website/NewStrings.java
+++ web/wicket-website/src/java/edu/colorado/phet/website/NewStrings.java
@@ -16,30 +16,33 @@ public class NewStrings {
 
     public static void checkNewStrings() {
         Session session = HibernateUtils.getInstance().openSession();
-        checkString( session, "profile.edit.changePassword", "Change password" );
-        checkString( session, "nav.changePasswordSuccess", "Success" );
-        checkString( session, "changePasswordSuccess.title", "Password Changed" );
-        checkString( session, "changePassword.validation.newPasswordBlank", "The new password must not be blank." );
-        checkString( session, "changePassword.submit", "Submit" );
-        checkString( session, "changePassword.currentPassword", "Current Password:" );
-        checkString( session, "changePassword.newPassword", "New Password:" );
-        checkString( session, "changePassword.confirmNewPassword", "Confirm New Password:" );
-        checkString( session, "changePassword.validation.mismatch", "Confirmed password did not match the new password." );
-        checkString( session, "changePassword.validation.incorrectPassword", "The current password was incorrect." );
-        checkString( session, "changePassword.title", "Change Your PhET Password" );
-        checkString( session, "changePassword.header", "Change your password:" );
-        checkString( session, "signIn.resetYourPassword", "Forgot your password?" );
-        checkString( session, "resetPasswordRequest.title", "Reset Your Password" );
-        checkString( session, "resetPasswordRequest.submit", "Submit" );
-        checkString( session, "resetPasswordRequest.emailAddress", "Email Address:" );
-        checkString( session, "resetPasswordRequest.description", "Please enter your email address and press sumbit. An email will be sent to you with a link to reset your password." );
-        checkString( session, "resetPasswordRequest.header", "Reset your password." );
-        checkString( session, "resetPasswordRequest.validation.noAccountFound", "No PhET account was found for the specified email address." );
-        checkString( session, "resetPasswordRequest.emailBody", "<html><head></head><body><p>PhET Interactive Simulations received your password reset request.</p><p>To reset your password, please click </p><a href=\"{0}\">{0}</a> <p>or copy and paste the address into a browser.</p>Best Regards,<br>PhET Interactive Simulations</body></html>" );
-        checkString( session, "resetPasswordRequest.emailSubject", "Password reset requested" );
-        checkString( session, "resetPasswordRequestSuccess.message", "An email with instructions on how to reset your password has been sent to the specified email address." );
-        checkString( session, "resetPasswordRequestSuccess.title", "Password Reset Email Sent");
-        checkString( session, "resetPasswordCallback.title", "Reset Password" );
+//        checkString( session, "profile.edit.changePassword", "Change password" );
+//        checkString( session, "nav.changePasswordSuccess", "Success" );
+//        checkString( session, "changePasswordSuccess.title", "Password Changed" );
+//        checkString( session, "changePassword.validation.newPasswordBlank", "The new password must not be blank." );
+//        checkString( session, "changePassword.submit", "Submit" );
+//        checkString( session, "changePassword.currentPassword", "Current Password:" );
+//        checkString( session, "changePassword.newPassword", "New Password:" );
+//        checkString( session, "changePassword.confirmNewPassword", "Confirm New Password:" );
+//        checkString( session, "changePassword.validation.mismatch", "Confirmed password did not match the new password." );
+//        checkString( session, "changePassword.validation.incorrectPassword", "The current password was incorrect." );
+//        checkString( session, "changePassword.title", "Change Your PhET Password" );
+//        checkString( session, "changePassword.header", "Change your password:" );
+//        checkString( session, "signIn.resetYourPassword", "Forgot your password?" );
+//        checkString( session, "resetPasswordRequest.title", "Reset Your Password" );
+//        checkString( session, "resetPasswordRequest.submit", "Submit" );
+//        checkString( session, "resetPasswordRequest.emailAddress", "Email Address:" );
+//        checkString( session, "resetPasswordRequest.description", "Please enter your email address and press sumbit. An email will be sent to you with a link to reset your password." );
+//        checkString( session, "resetPasswordRequest.header", "Reset your password." );
+//        checkString( session, "resetPasswordRequest.validation.noAccountFound", "No PhET account was found for the specified email address." );
+//        checkString( session, "resetPasswordRequest.emailBody", "<html><head></head><body><p>PhET Interactive Simulations received your password reset request.</p><p>To reset your password, please click </p><a href=\"{0}\">{0}</a> <p>or copy and paste the address into a browser.</p>Best Regards,<br>PhET Interactive Simulations</body></html>" );
+//        checkString( session, "resetPasswordRequest.emailSubject", "Password reset requested" );
+//        checkString( session, "resetPasswordRequestSuccess.message", "An email with instructions on how to reset your password has been sent to the specified email address." );
+//        checkString( session, "resetPasswordRequestSuccess.title", "Password Reset Email Sent");
+//        checkString( session, "resetPasswordCallback.title", "Reset Password" );
+        checkString( session, "error.internalError.message", "Please contact us at {0} for assistance or to report the error." );
+        checkString( session, "error.sessionExpired", "Your session has expired" );
+        checkString( session, "error.sessionExpired.message", "If you are submitting information, please hit 'Back', copy the information, and log in again." );
         session.close();
     }
 
