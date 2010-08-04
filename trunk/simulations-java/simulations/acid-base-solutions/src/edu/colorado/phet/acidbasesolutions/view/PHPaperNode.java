29233a757d7304e4f44a493c67e49edcb2ce6142
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-08-04 18:11:57 +0000

    bug fix, pH paper didn't change to correct color if solution was changed while paper is not dipped

--- trunk/simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/view/PHPaperNode.java
+++ trunk/simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/view/PHPaperNode.java
@@ -64,6 +64,7 @@ public class PHPaperNode extends PhetPNode {
             }
             public void dippedHeightChanged() {
                 updateGeomerty();
+                updateColor();
             }
         });
 
