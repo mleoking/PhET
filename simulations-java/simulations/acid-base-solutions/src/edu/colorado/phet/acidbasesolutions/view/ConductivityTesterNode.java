7c4d03f71928e8e6bfb9ded0101073ce545aa60b
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-08-10 00:43:04 +0000

    add simple wires connecting probes to connectivity tester

--- simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/view/ConductivityTesterNode.java
+++ simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/view/ConductivityTesterNode.java
@@ -2,10 +2,8 @@
 
 package edu.colorado.phet.acidbasesolutions.view;
 
-import java.awt.BasicStroke;
-import java.awt.Color;
-import java.awt.Cursor;
-import java.awt.Stroke;
+import java.awt.*;
+import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 
@@ -13,24 +11,37 @@ import edu.colorado.phet.acidbasesolutions.constants.ABSResources;
 import edu.colorado.phet.acidbasesolutions.model.ConductivityTester;
 import edu.colorado.phet.acidbasesolutions.model.ConductivityTester.ConductivityTesterChangeListener;
 import edu.colorado.phet.acidbasesolutions.model.SolutionRepresentation.SolutionRepresentationChangeAdapter;
+import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.PhetPNode;
 import edu.colorado.phet.common.piccolophet.event.CursorHandler;
 import edu.umd.cs.piccolo.event.PDragSequenceEventHandler;
 import edu.umd.cs.piccolo.event.PInputEvent;
 import edu.umd.cs.piccolo.nodes.PImage;
 import edu.umd.cs.piccolo.nodes.PPath;
+import edu.umd.cs.piccolo.nodes.PText;
 import edu.umd.cs.piccolo.util.PDimension;
 
 
 public class ConductivityTesterNode extends PhetPNode {
     
+    // probe properties
     private static final Color POSITIVE_PROBE_FILL_COLOR = Color.RED;
     private static final Color NEGATIVE_PROBE_FILL_COLOR = Color.BLACK;
     private static final Color PROBE_STROKE_COLOR = Color.BLACK;
     private static final Stroke PROBE_STROKE = new BasicStroke( 1f );
+    private static final String POSITIVE_PROBE_LABEL = "+"; //XXX i18n
+    private static final String NEGATIVE_PROBE_LABEL = "-"; //XXX i18n
+    private static final Color POSITIVE_PROBE_LABEL_COLOR = Color.WHITE;
+    private static final Color NEGATIVE_PROBE_LABEL_COLOR = Color.WHITE;
+    private static final Font PROBE_LABEL_FONT = new PhetFont( Font.BOLD, 24 );
+    
+    // wire properties
+    private static final Color WIRE_COLOR = Color.BLACK;
+    private static final Stroke WIRE_STROKE = new BasicStroke( 2f );
 
     private final ConductivityTester tester;
     private final ProbeNode positiveProbeNode, negativeProbeNode;
+    private final WireNode positiveWireNode, negativeWireNode;
     
     public ConductivityTesterNode( final ConductivityTester tester ) {
         
@@ -52,12 +63,8 @@ public class ConductivityTesterNode extends PhetPNode {
             }
         });
         
-        PImage imageNode = new PImage( ABSResources.getBufferedImage( "uncleMalley.png" ) ); //XXX
-        addChild( imageNode );
-        imageNode.setOffset( -imageNode.getFullBoundsReference().getWidth()/2, 0 );
-        
         // positive probe
-        positiveProbeNode = new ProbeNode( tester.getProbeSizeReference(), POSITIVE_PROBE_FILL_COLOR );
+        positiveProbeNode = new ProbeNode( tester.getProbeSizeReference(), POSITIVE_PROBE_FILL_COLOR, POSITIVE_PROBE_LABEL, POSITIVE_PROBE_LABEL_COLOR );
         addChild( positiveProbeNode );
         positiveProbeNode.addInputEventListener( new CursorHandler( Cursor.N_RESIZE_CURSOR ) );
         positiveProbeNode.addInputEventListener( new PDragSequenceEventHandler() {
@@ -81,7 +88,7 @@ public class ConductivityTesterNode extends PhetPNode {
         } );
 
         // negative probe
-        negativeProbeNode = new ProbeNode( tester.getProbeSizeReference(), NEGATIVE_PROBE_FILL_COLOR );
+        negativeProbeNode = new ProbeNode( tester.getProbeSizeReference(), NEGATIVE_PROBE_FILL_COLOR, NEGATIVE_PROBE_LABEL, NEGATIVE_PROBE_LABEL_COLOR );
         addChild( negativeProbeNode );
         negativeProbeNode.addInputEventListener( new CursorHandler( Cursor.N_RESIZE_CURSOR ) );
         negativeProbeNode.addInputEventListener( new PDragSequenceEventHandler() {
@@ -103,7 +110,18 @@ public class ConductivityTesterNode extends PhetPNode {
             }
         } );
         
+        // positive wire
+        positiveWireNode = new WireNode();
+        addChild( positiveWireNode );
 
+        // negative wire
+        negativeWireNode = new WireNode();
+        addChild( negativeWireNode );
+        
+        PImage imageNode = new PImage( ABSResources.getBufferedImage( "uncleMalley.png" ) ); //XXX
+        addChild( imageNode );
+        imageNode.setOffset( -imageNode.getFullBoundsReference().getWidth() / 2, 0 );
+        
         // layout 
         //XXX mvt needed here?
         setOffset( tester.getLocationReference() );
@@ -114,29 +132,68 @@ public class ConductivityTesterNode extends PhetPNode {
     }
     
     private void updatePositiveProbeLocation() {
-        //XXX mvt
+        
+        // probe
         double x = tester.getPositiveProbeLocationReference().getX() - tester.getLocationReference().getX();
         double y = tester.getPositiveProbeLocationReference().getY() - tester.getLocationReference().getY();
         positiveProbeNode.setOffset( x, y );
+        
+        // wire
+        Point2D p1 = new Point2D.Double( 0, 0 );
+        Point2D p2 = new Point2D.Double( x, y - tester.getProbeSizeReference().getHeight() );
+        positiveWireNode.setEndPoints( p1, p2 );
     }
     
     private void updateNegativeProbeLocation() {
-        //XXX mvt
+        
+        // probe
         double x = tester.getNegativeProbeLocationReference().getX() - tester.getLocationReference().getX();
         double y = tester.getNegativeProbeLocationReference().getY() - tester.getLocationReference().getY();
         negativeProbeNode.setOffset( x, y );
+        
+        // wire
+        Point2D p1 = new Point2D.Double( 0, 0 );
+        Point2D p2 = new Point2D.Double( x, y - tester.getProbeSizeReference().getHeight() );
+        negativeWireNode.setEndPoints( p1, p2 );
     }
     
     /*
      * Probes, origin at bottom center.
      */
-    private static class ProbeNode extends PPath {
+    private static class ProbeNode extends PhetPNode {
         
-        public ProbeNode( PDimension size, Color color ) {
-            setPathTo( new Rectangle2D.Double( -size.getWidth() / 2, -size.getHeight(), size.getWidth(), size.getHeight() ) );
-            setStroke( PROBE_STROKE );
-            setStrokePaint( PROBE_STROKE_COLOR );
-            setPaint( color );
+        public ProbeNode( PDimension size, Color color, String label, Color labelColor ) {
+            
+            PPath pathNode = new PPath( new Rectangle2D.Double( -size.getWidth() / 2, -size.getHeight(), size.getWidth(), size.getHeight() ) );
+            pathNode.setStroke( PROBE_STROKE );
+            pathNode.setStrokePaint( PROBE_STROKE_COLOR );
+            pathNode.setPaint( color );
+            addChild( pathNode );
+            
+            PText labelNode = new PText( label );
+            labelNode.setTextPaint( labelColor );
+            labelNode.setFont( PROBE_LABEL_FONT );
+            addChild( labelNode );
+            
+            double x = pathNode.getFullBoundsReference().getCenterX() - ( labelNode.getFullBoundsReference().getWidth() / 2 );
+            double y = pathNode.getFullBoundsReference().getMaxY() - labelNode.getFullBoundsReference().getHeight() - 3;
+            labelNode.setOffset( x, y );
+        }
+    }
+    
+    private static class WireNode extends PPath {
+        
+        private final Line2D line;
+        
+        public WireNode() {
+            this.line = new Line2D.Double();
+            setStroke( WIRE_STROKE );
+            setStrokePaint( WIRE_COLOR );
+        }
+        
+        public void setEndPoints( Point2D p1, Point2D p2 ) {
+            line.setLine( p1, p2 );
+            setPathTo( line );
         }
     }
 }
