e3e1c0b5d6897904ca30953462e2d24856669d35
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-08-11 18:24:17 +0000

    Fixed: Applied force slider should snap back to zero when the user drops the slider button

--- trunk/simulations-java/simulations/motion-series/scala-src/edu/colorado/phet/motionseries/graphics/AppliedForceSliderNode.scala
+++ trunk/simulations-java/simulations/motion-series/scala-src/edu/colorado/phet/motionseries/graphics/AppliedForceSliderNode.scala
@@ -8,26 +8,30 @@ import edu.colorado.phet.common.phetcommon.view.util.PhetFont
 import edu.colorado.phet.common.motion.charts.{PlayAreaSliderControl, TextBox, MotionSliderNode}
 
 class AppliedForceSliderNode(motionSeriesObject: ForceMotionSeriesObject, dragHandler: () => Unit) extends PNode {
-  val positionSlider = new PlayAreaSliderControl(-MotionSeriesDefaults.MAX_APPLIED_FORCE, MotionSeriesDefaults.MAX_APPLIED_FORCE,
+  val slider = new PlayAreaSliderControl(-MotionSeriesDefaults.MAX_APPLIED_FORCE, MotionSeriesDefaults.MAX_APPLIED_FORCE,
     motionSeriesObject.parallelAppliedForce, "controls.applied-force-x".translate, "units.abbr.newtons".translate, MotionSeriesDefaults.appliedForceColor, new TextBox(new PhetFont(18, true), 6) {
       addListener(new TextBox.Listener() {
         def changed = {
           //have to replace comma with decimal otherwise we obtain problems like:
           //Exception in thread "AWT-EventQueue-0" java.lang.NumberFormatException: For input string: "0,00"
-          motionSeriesObject.parallelAppliedForce = java.lang.Double.parseDouble(getText.replace(',','.'))
+          motionSeriesObject.parallelAppliedForce = java.lang.Double.parseDouble(getText.replace(',', '.'))
         }
       })
     })
-  addChild(positionSlider)
+  addChild(slider)
 
   motionSeriesObject.parallelAppliedForceListeners += (() => {
-    positionSlider.setValue(motionSeriesObject.parallelAppliedForce)
+    slider.setValue(motionSeriesObject.parallelAppliedForce)
   })
 
-  positionSlider.addListener(new MotionSliderNode.Adapter {
+  slider.addListener(new MotionSliderNode.Adapter {
     override def sliderDragged(value: java.lang.Double) = {
       motionSeriesObject.parallelAppliedForce = value.doubleValue
       dragHandler()
+    }
+
+    override def sliderThumbReleased() = {
+      motionSeriesObject.parallelAppliedForce = 0.0
     }
   })
 }
