37b9a5b62d418591a17609c53df7e58d1263eb23
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-08-17 16:55:07 +0000

    Cleaned up imports.

--- trunk/simulations-java/simulations/membrane-diffusion/src/edu/colorado/phet/membranediffusion/model/MembraneChannel.java
+++ trunk/simulations-java/simulations/membrane-diffusion/src/edu/colorado/phet/membranediffusion/model/MembraneChannel.java
@@ -4,7 +4,6 @@ package edu.colorado.phet.membranediffusion.model;
 
 import java.awt.Color;
 import java.awt.Shape;
-import java.awt.geom.AffineTransform;
 import java.awt.geom.Dimension2D;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
