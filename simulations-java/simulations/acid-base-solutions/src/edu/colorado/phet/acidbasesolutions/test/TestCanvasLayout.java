65e788585dd849e3b213c82bb8c3d0c5ac3da84b
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-08-17 01:22:15 +0000

    generalize and simplify, no dependencies on sim model elements or nodes

--- simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/test/TestCanvasLayout.java
+++ simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/test/TestCanvasLayout.java
@@ -1,35 +1,30 @@
 package edu.colorado.phet.acidbasesolutions.test;
 
+import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
 
-import edu.colorado.phet.acidbasesolutions.model.AqueousSolution;
-import edu.colorado.phet.acidbasesolutions.model.Beaker;
-import edu.colorado.phet.acidbasesolutions.model.PureWaterSolution;
-import edu.colorado.phet.acidbasesolutions.view.BeakerNode;
 import edu.colorado.phet.common.piccolophet.PhetPCanvas;
 import edu.umd.cs.piccolo.PNode;
-import edu.umd.cs.piccolo.util.PDimension;
+import edu.umd.cs.piccolo.nodes.PPath;
 
 /**
- * Tests a layout problem I'm having with the sim.
- * But, unfortunately, this doesn't reproduce it.
- * <p>
- * The problem:
- * As the main frame is made less tall, the play area initially doesn't rescale,
- * and rescaling doesn't start until the bottom edge of the frame is level with
- * the bottom of the beaker.  This causes the reaction equation to be clipped.
+ * Tests a general layout problem I'm having with the sim.
+ * As the main frame is made less tall, the play area initially doesn't rescale.
+ * This causes the reaction equation to be clipped.
+ * This happens when FRAME_SIZE is less than CANVAS_RENDERING_SIZE,
+ * and appears to be a problem with PhetPCanvas' RenderingSizeStrategy.
+ * The problem goes away if CANVAS_RENDERING_SIZE is square.
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
 public class TestCanvasLayout extends JFrame {
 
-    public static final Dimension CANVAS_RENDERING_SIZE = new Dimension( 1024, 768 );
-    public static final Dimension FRAME_SIZE = new Dimension( 700, 600 );
-    public static final PDimension BEAKER_SIZE = new PDimension( 600, 450 );
+    private static final Dimension CANVAS_RENDERING_SIZE = new Dimension( 1024, 768 );
+    private static final Dimension FRAME_SIZE = new Dimension( 700, 600 );
     
     public TestCanvasLayout() {
         super( TestCanvasLayout.class.getName() );
@@ -47,12 +42,17 @@ public class TestCanvasLayout extends JFrame {
             PNode rootNode = new PNode();
             addWorldChild( rootNode );
             
-            AqueousSolution solution = new PureWaterSolution();
-            Point2D beakerLocation = new Point2D.Double( ( BEAKER_SIZE.getWidth() / 2 ) + 150, BEAKER_SIZE.getHeight() + 250 ); // bottom center
-            Beaker beaker = new Beaker( solution, beakerLocation, true /* visible */, BEAKER_SIZE );
+            PPath redBoxNode = new PPath( new Rectangle2D.Double( 0, 0, 700, 650 ) );
+            redBoxNode.setPaint( Color.RED );
+            rootNode.addChild( redBoxNode );
             
-            BeakerNode beakerNode = new BeakerNode( beaker );
-            rootNode.addChild( beakerNode );
+            PPath blueBoxNode = new PPath( new Rectangle2D.Double( 0, 0, 700, 100 ) );
+            blueBoxNode.setPaint( Color.blue );
+            rootNode.addChild( blueBoxNode );
+            
+            // layout
+            redBoxNode.setOffset( 100, 50 );
+            blueBoxNode.setOffset( 100, redBoxNode.getFullBoundsReference().getMaxY() + 20 );
         }
     }
     
