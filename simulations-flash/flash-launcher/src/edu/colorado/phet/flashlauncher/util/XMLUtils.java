debb0693cfa11f9c3ab2707ef8991d726b57117a
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-08-17 16:57:32 +0000

    Reformatted flash launcher, see #2463

--- simulations-flash/flash-launcher/src/edu/colorado/phet/flashlauncher/util/XMLUtils.java
+++ simulations-flash/flash-launcher/src/edu/colorado/phet/flashlauncher/util/XMLUtils.java
@@ -1,26 +1,30 @@
 package edu.colorado.phet.flashlauncher.util;
 
-import org.w3c.dom.Document;
+import java.io.StringReader;
+import java.io.StringWriter;
 
-import javax.xml.transform.*;
-import javax.xml.transform.stream.StreamResult;
-import javax.xml.transform.stream.StreamSource;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.dom.DOMResult;
-import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import java.io.StringWriter;
-import java.io.StringReader;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMResult;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+
+import org.w3c.dom.Document;
 
 public class XMLUtils {
-    private XMLUtils() {}
+    private XMLUtils() {
+    }
 
     /**
      * Converts a Document to an XML String.
+     *
      * @param document
      * @return
      * @throws javax.xml.transform.TransformerException
+     *
      */
     public static String toString( Document document ) throws TransformerException {
 
@@ -40,10 +44,12 @@ public class XMLUtils {
 
     /**
      * Converts an XML String to a Document.
+     *
      * @param string
      * @return
      * @throws TransformerException
      * @throws javax.xml.parsers.ParserConfigurationException
+     *
      */
     public static Document toDocument( String string ) throws TransformerException, ParserConfigurationException {
 
@@ -62,7 +68,7 @@ public class XMLUtils {
         // Document result
         DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
         Document document = builder.newDocument();
-        Result result = new DOMResult(document);
+        Result result = new DOMResult( document );
 
         transformer.transform( source, result );
         return document;
@@ -70,14 +76,15 @@ public class XMLUtils {
 
     public static String HTMLEntityEncode( String s ) {
         StringBuffer buf = new StringBuffer();
-        int len = (s == null ? -1 : s.length());
+        int len = ( s == null ? -1 : s.length() );
 
-        for( int i = 0; i < len; i++ ) {
+        for ( int i = 0; i < len; i++ ) {
             char c = s.charAt( i );
-            if( c>='a' && c<='z' || c>='A' && c<='Z' || c>='0' && c<='9' ) {
+            if ( c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c >= '0' && c <= '9' ) {
                 buf.append( c );
-            } else {
-                buf.append( "&#" + (int)c + ";" );
+            }
+            else {
+                buf.append( "&#" + (int) c + ";" );
             }
         }
         return buf.toString();
