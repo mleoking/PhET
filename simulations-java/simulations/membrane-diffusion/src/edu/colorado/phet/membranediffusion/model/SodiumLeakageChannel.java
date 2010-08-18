c7cf30533c42626c22807933ed4ee7a59450eb95
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-08-18 17:16:44 +0000

    Eliminated the interaction with the HH model that involves leak current, since it doesn't make sense for this sim (left over from Neuron sim).

--- simulations-java/simulations/membrane-diffusion/src/edu/colorado/phet/membranediffusion/model/SodiumLeakageChannel.java
+++ simulations-java/simulations/membrane-diffusion/src/edu/colorado/phet/membranediffusion/model/SodiumLeakageChannel.java
@@ -18,30 +18,21 @@ public class SodiumLeakageChannel extends AbstractLeakChannel {
 	
 	private static final Color BASE_COLOR = ColorUtils.interpolateRBGA(MembraneDiffusionConstants.SODIUM_COLOR, Color.YELLOW, 0.5);
 	
-	// Controls the rate of leakage when no action potential is occurring.
-	// Higher values mean more leakage, with 1 as the max.
-	private static final double NOMINAL_LEAK_LEVEL = 0.005;
-	
 	private static final double DEFAULT_PARTICLE_VELOCITY = 10000; // In nanometers per sec of sim time.
 	
-	// A scaling factor that is used to normalize the amount of leak channel
-	// current to a value between 0 and 1.  This value was determined by
-	// testing the Hodgkin-Huxley model.
-	private static final double PEAK_NEGATIVE_CURRENT = 3.44;
-
+    // Constants that define the rate and variability of particle capture.
+    private static final double MIN_INTER_PARTICLE_CAPTURE_TIME = 0.002; // In seconds of sim time.
+    private static final double MAX_INTER_PARTICLE_CAPTURE_TIME = 0.004; // In seconds of sim time.
+	
     //----------------------------------------------------------------------------
     // Instance Data
     //----------------------------------------------------------------------------
-	
-	private IHodgkinHuxleyModel hodgkinHuxleyModel;
-	private double previousNormalizedLeakCurrent = 0;
 	
     //----------------------------------------------------------------------------
     // Constructor
     //----------------------------------------------------------------------------
 	public SodiumLeakageChannel(IParticleCapture modelContainingParticles, IHodgkinHuxleyModel hodgkinHuxleyModel) {
 		super(CHANNEL_WIDTH, CHANNEL_HEIGHT, modelContainingParticles);
-		this.hodgkinHuxleyModel = hodgkinHuxleyModel;
 		
 		// Set the speed at which particles will move through the channel.
 		setParticleVelocity(DEFAULT_PARTICLE_VELOCITY);
@@ -49,10 +40,11 @@ public class SodiumLeakageChannel extends AbstractLeakChannel {
 		// Set up the capture zones for this channel.
 		setLowerCaptureZone(new PieSliceShapedCaptureZone(getCenterLocation(), CHANNEL_WIDTH * 3.5, Math.PI / 2, Math.PI * 0.3));
 		setUpperCaptureZone(new PieSliceShapedCaptureZone(getCenterLocation(), CHANNEL_WIDTH * 3.5, -Math.PI / 2, Math.PI * 0.3));
-
-		// Update the capture times.
-		updateParticleCaptureRate(NOMINAL_LEAK_LEVEL);
 		
+        // Set the rate of particle capture for leakage.
+        setMinInterCaptureTime(MIN_INTER_PARTICLE_CAPTURE_TIME);
+        setMaxInterCaptureTime(MAX_INTER_PARTICLE_CAPTURE_TIME);
+
 		// Start the capture timer now, since leak channels are always
 		// capturing particles.
 		restartCaptureCountdownTimer();
@@ -84,58 +76,5 @@ public class SodiumLeakageChannel extends AbstractLeakChannel {
 	@Override
 	protected ParticleType getParticleTypeToCapture() {
 		return ParticleType.SODIUM_ION;
-	}
-
-	@Override
-	public void stepInTime(double dt) {
-		super.stepInTime(dt);
-		
-		// Since this is a leak channel, it is always open, so the openness
-		// is not updated as it is for the gated channels.  However, we DO
-		// want more sodium to flow through when the leak current in the
-		// HH model goes up, so the following code accomplishes that goal.
-
-		double normalizedLeakCurrent = MathUtils.round(hodgkinHuxleyModel.get_l_current() / PEAK_NEGATIVE_CURRENT, 2);
-		if (normalizedLeakCurrent <= 0.01){
-			// Only pay attention to negative values for the current, which
-			// we will map to sodium flow back into the cell.  This is a
-			// bit of hollywooding.
-			normalizedLeakCurrent = Math.max(normalizedLeakCurrent, -1);
-			if (normalizedLeakCurrent != previousNormalizedLeakCurrent){
-				previousNormalizedLeakCurrent = normalizedLeakCurrent;
-				updateParticleCaptureRate(Math.max(Math.abs(normalizedLeakCurrent), NOMINAL_LEAK_LEVEL));
-			}
-		}
-	}
-	
-	/**
-	 * Update the rate of particle capture based on the supplied normalized
-	 * value.
-	 * 
-	 * @param normalizedRate - A value between 0 and 1 where 0 represents the
-	 * minimum capture rate for particles and 1 represents the max.
-	 */
-	private void updateParticleCaptureRate(double normalizedRate){
-		if (normalizedRate <= 0.001){
-			// No captures at this rate.
-			setMinInterCaptureTime(Double.POSITIVE_INFINITY);
-			setMaxInterCaptureTime(Double.POSITIVE_INFINITY);
-			restartCaptureCountdownTimer();
-		}
-		else{
-			// Tweak the following values for different behavior.
-			double absoluteMinInterCaptureTime = 0.0002;
-			double variableMinInterCaptureTime = 0.002;
-			double captureTimeRange = 0.005;
-			double minInterCaptureTime = absoluteMinInterCaptureTime + (1 - normalizedRate) * (variableMinInterCaptureTime);
-			setMinInterCaptureTime(minInterCaptureTime);
-			setMaxInterCaptureTime(minInterCaptureTime + (1 - normalizedRate) * captureTimeRange);
-			
-			if (getCaptureCountdownTimer() > getMaxInterCaptureTime()){
-				// Only restart the capture countdown if the current values is
-				// higher than the max.
-				restartCaptureCountdownTimer();
-			}
-		}
 	}
 }
