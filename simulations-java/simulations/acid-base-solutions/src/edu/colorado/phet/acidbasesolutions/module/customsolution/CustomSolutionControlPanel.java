4fcf4d2ae0c4ef6e685cf8633b30f5a9a124414c
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-08-19 18:10:59 +0000

    tweaks to spacing and justification in control panels

--- simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/module/customsolution/CustomSolutionControlPanel.java
+++ simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/module/customsolution/CustomSolutionControlPanel.java
@@ -2,6 +2,8 @@
 
 package edu.colorado.phet.acidbasesolutions.module.customsolution;
 
+import java.awt.Insets;
+
 import edu.colorado.phet.acidbasesolutions.controls.CustomSolutionControls;
 import edu.colorado.phet.acidbasesolutions.controls.TestControls;
 import edu.colorado.phet.acidbasesolutions.controls.ViewControls;
@@ -17,6 +19,7 @@ import edu.colorado.phet.common.phetcommon.view.ControlPanel;
 public class CustomSolutionControlPanel extends ControlPanel {
 
     public CustomSolutionControlPanel( Resettable resettable, ABSModel model ) {
+        setInsets( new Insets( 2, 5, 2, 5 ) );
         addControlFullWidth( new CustomSolutionControls( model ) );
         addControlFullWidth( new TestControls( model ) );
         addControlFullWidth( new ViewControls( model ) );
