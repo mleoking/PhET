dfaf46572475b9d1f2b05656307c8105ee27ec15
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-08-20 22:04:46 +0000

    Replaced the representation node with one generated from the generic membrane channel.  This is part of a simplification effort.

--- trunk/simulations-java/simulations/membrane-diffusion/src/edu/colorado/phet/membranediffusion/view/SodiumGatedChannelToolBoxNode.java
+++ trunk/simulations-java/simulations/membrane-diffusion/src/edu/colorado/phet/membranediffusion/view/SodiumGatedChannelToolBoxNode.java
@@ -2,13 +2,18 @@
 
 package edu.colorado.phet.membranediffusion.view;
 
+import java.awt.Color;
 import java.awt.geom.Point2D;
 
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform2D;
+import edu.colorado.phet.common.phetcommon.view.util.ColorUtils;
 import edu.colorado.phet.common.piccolophet.PhetPCanvas;
+import edu.colorado.phet.membranediffusion.MembraneDiffusionConstants;
+import edu.colorado.phet.membranediffusion.model.ChannelAlwaysClosedStrategy;
+import edu.colorado.phet.membranediffusion.model.GenericMembraneChannel;
 import edu.colorado.phet.membranediffusion.model.MembraneChannel;
 import edu.colorado.phet.membranediffusion.model.MembraneDiffusionModel;
-import edu.colorado.phet.membranediffusion.model.PotassiumGatedChannel;
+import edu.colorado.phet.membranediffusion.model.ParticleType;
 import edu.colorado.phet.membranediffusion.model.SodiumGatedChannel;
 import edu.umd.cs.piccolo.PNode;
 
@@ -19,6 +24,8 @@ import edu.umd.cs.piccolo.PNode;
  * @author John Blanco
  */
 public class SodiumGatedChannelToolBoxNode extends ToolBoxItem {
+    
+    private static final Color EDGE_COLOR = MembraneDiffusionConstants.SODIUM_COLOR;
 
 	public SodiumGatedChannelToolBoxNode(MembraneDiffusionModel model, ModelViewTransform2D mvt, PhetPCanvas canvas) {
 		super(model, mvt, canvas);
@@ -31,8 +38,9 @@ public class SodiumGatedChannelToolBoxNode extends ToolBoxItem {
 
 	@Override
 	protected void initializeSelectionNode() {
-		MembraneChannel channel = new SodiumGatedChannel();
-		PNode representation = new MembraneChannelNode(channel, SCALING_MVT);
-		setSelectionNode(representation);
+        MembraneChannel channel = new GenericMembraneChannel( getModel(), ParticleType.SODIUM_ION,
+                ColorUtils.darkerColor(EDGE_COLOR, 0.15), EDGE_COLOR, new ChannelAlwaysClosedStrategy() );
+        PNode representation = new MembraneChannelNode(channel, SCALING_MVT);
+        setSelectionNode(representation);
 	}
 }
