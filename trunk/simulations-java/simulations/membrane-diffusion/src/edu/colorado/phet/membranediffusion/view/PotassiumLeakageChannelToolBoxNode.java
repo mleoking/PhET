419f5ab09311e7a504fc92031ca13b9e91c74322
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-08-20 21:55:27 +0000

    Replaced the potassium leakage channel with the new generic channel.

--- trunk/simulations-java/simulations/membrane-diffusion/src/edu/colorado/phet/membranediffusion/view/PotassiumLeakageChannelToolBoxNode.java
+++ trunk/simulations-java/simulations/membrane-diffusion/src/edu/colorado/phet/membranediffusion/view/PotassiumLeakageChannelToolBoxNode.java
@@ -2,12 +2,18 @@
 
 package edu.colorado.phet.membranediffusion.view;
 
+import java.awt.Color;
 import java.awt.geom.Point2D;
 
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform2D;
+import edu.colorado.phet.common.phetcommon.view.util.ColorUtils;
 import edu.colorado.phet.common.piccolophet.PhetPCanvas;
+import edu.colorado.phet.membranediffusion.MembraneDiffusionConstants;
+import edu.colorado.phet.membranediffusion.model.ChannelAlwaysOpenStrategy;
+import edu.colorado.phet.membranediffusion.model.GenericMembraneChannel;
 import edu.colorado.phet.membranediffusion.model.MembraneChannel;
 import edu.colorado.phet.membranediffusion.model.MembraneDiffusionModel;
+import edu.colorado.phet.membranediffusion.model.ParticleType;
 import edu.colorado.phet.membranediffusion.model.PotassiumLeakageChannel;
 import edu.colorado.phet.membranediffusion.model.SodiumLeakageChannel;
 import edu.umd.cs.piccolo.PNode;
@@ -20,19 +26,24 @@ import edu.umd.cs.piccolo.PNode;
  */
 public class PotassiumLeakageChannelToolBoxNode extends ToolBoxItem {
 
+    private static final Color EDGE_COLOR = ColorUtils.interpolateRBGA(MembraneDiffusionConstants.POTASSIUM_COLOR,
+            new Color(00, 200, 255), 0.6);
+
 	public PotassiumLeakageChannelToolBoxNode(MembraneDiffusionModel model, ModelViewTransform2D mvt, PhetPCanvas canvas) {
 		super(model, mvt, canvas);
 	}
 
 	@Override
 	protected MembraneChannel createModelElement( Point2D position ) {
-	    return new PotassiumLeakageChannel(getModel(), getModel().getHodgkinHuxleyModel());
+        return new GenericMembraneChannel( getModel(), ParticleType.POTASSIUM_ION,
+                ColorUtils.darkerColor(EDGE_COLOR, 0.15), EDGE_COLOR, new ChannelAlwaysOpenStrategy() );
 	}
 
 	@Override
 	protected void initializeSelectionNode() {
-		MembraneChannel channel = new PotassiumLeakageChannel();
-		PNode representation = new MembraneChannelNode(channel, SCALING_MVT);
-		setSelectionNode(representation);
+        MembraneChannel channel = new GenericMembraneChannel( getModel(), ParticleType.POTASSIUM_ION,
+                ColorUtils.darkerColor(EDGE_COLOR, 0.15), EDGE_COLOR, new ChannelAlwaysOpenStrategy() );
+        PNode representation = new MembraneChannelNode(channel, SCALING_MVT);
+        setSelectionNode(representation);
 	}
 }
