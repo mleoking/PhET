37ef1ebf2ab1c4369fe3748399119897064f953f
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-08-22 18:02:43 +0000

    updated docs and build files

--- team/reids/sbt/RampProject.scala
+++ team/reids/sbt/RampProject.scala
@@ -15,14 +15,17 @@ class RampProject(info: ProjectInfo) extends ParentProject(info) {
   lazy val phetcommon = project("common" / "phetcommon", "phetcommon", new PhetProject(_), junit, javaws)
   lazy val piccolo2dcore = project("contrib" / "piccolo2d" / "core", "piccolo2d-core", new DefaultProject(_))
   lazy val piccolo2dextras = project("contrib" / "piccolo2d" / "extras", "piccolo2d-extras", new DefaultProject(_), piccolo2dcore)
-  lazy val piccolo2d = project("contrib" / "piccolo2d", "piccolo2d", new DefaultProject(_), piccolo2dextras)
+  lazy val piccolo2dexamples = project("contrib" / "piccolo2d" / "examples", "piccolo2d-examples", new DefaultProject(_), piccolo2dcore, piccolo2dextras)
+  lazy val piccolo2d = project("contrib" / "piccolo2d", "piccolo2d", new DefaultProject(_), piccolo2dextras, piccolo2dexamples)
   lazy val piccolophet = project("common" / "piccolo-phet", "piccolo-phet", new PhetProject(_), phetcommon, piccolo2d)
   lazy val jfreechartphet = project("common" / "jfreechart-phet", "jfreechart-phet", new PhetProject(_), jfreechart, piccolophet)
   lazy val scalacommon = project("common" / "scala-common", "scalacommon", new PhetProject(_), phetcommon, piccolo2d, piccolophet, scala)
   lazy val timeseries = project("common" / "timeseries", "timeseries", new PhetProject(_), piccolo2d, phetcommon)
-  lazy val recordandplayback = project("common" / "record-and-playback", "recordandplayback", new PhetProject(_), piccolo2d, phetcommon, piccolophet)
-  lazy val motion = project("common" / "motion", "motion", new PhetProject(_), timeseries, piccolo2d, phetcommon, jsci, jfreechartphet)
-  lazy val motionseries = project("simulations" / "motion-series", "motion-series", new PhetProject(_), scalacommon, phetcommon, motion, recordandplayback, timeseries)
+  lazy val recordandplayback = project("common" / "record-and-playback", "recordandplayback", new PhetProject(_), phetcommon, piccolophet)
+  lazy val motion = project("common" / "motion", "motion", new PhetProject(_), timeseries, piccolo2d, phetcommon, jsci, jfreechartphet,recordandplayback)
+  lazy val motionseries = project("simulations" / "motion-series", "motion-series", new PhetProject(_){
+    override def mainClass = Some("edu.colorado.phet.motionseries.sims.forcesandmotion.ForcesAndMotionApplication")
+  }, scalacommon, phetcommon, motion, recordandplayback, timeseries)
 
   class PhetProject(info: ProjectInfo) extends DefaultProject(info) {
     override def mainScalaSourcePath = "scala-src"
