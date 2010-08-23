1a5df72dc56bf0b61ba0e48cc0deac0b93985e6f
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-08-23 20:15:41 +0000

    simplify AbstractConcentrationGraphNode by removing unnecessary inner classes and unused features

--- trunk/simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/view/graph/ConcentrationYAxisNode.java
+++ trunk/simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/view/graph/ConcentrationYAxisNode.java
@@ -2,6 +2,7 @@
 
 package edu.colorado.phet.acidbasesolutions.view.graph;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Stroke;
@@ -22,22 +23,38 @@ import edu.umd.cs.piccolox.nodes.PComposite;
  */
 class ConcentrationYAxisNode extends PComposite {
     
+    // axes
     private static final Font AXIS_LABEL_FONT = new PhetFont( 16 );
     private static final Color AXIS_LABEL_COLOR = Color.BLACK;
     private static final double AXIS_LABEL_X_SPACING = 20;
     
-    private final double tickSpacing;
+    // ticks
+    private static final double TICK_LENGTH = 6;
+    private static final Stroke TICK_STROKE = new BasicStroke( 1f );
+    private static final Color TICK_COLOR = Color.BLACK;
+    private static final Font TICK_LABEL_FONT = new PhetFont( 14 );
+    private static final Color TICK_LABEL_COLOR = Color.BLACK;
     
-    public ConcentrationYAxisNode( PDimension graphOutlineSize, int numberOfTicks, 
-            double topMargin, int maxExponent, int exponentSpacing, double tickLength, 
-            Stroke tickStroke, Color tickColor, Font labelFont, Color labelColor,
-            Stroke gridlineStroke, Color gridlineColor ) {
+    // horizontal gridlines
+    private static final Stroke GRIDLINE_STROKE = new BasicStroke( 1f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[] { 3, 3 }, 0 ); // dashed
+    private static final Color GRIDLINE_COLOR = new Color( 192, 192, 192 ); // gray
+    
+    /**
+     * Constructor.
+     * 
+     * @param graphOutlineSize size of the outline around the graph's data area
+     * @param numberOfTicks number of tick marks
+     * @param topMargin vertical distance between the top tick and the top of the graph's data area
+     * @param maxExponent exponent of the maximum tick mark
+     * @param exponentDelta delta between tick mark exponents
+     */
+    public ConcentrationYAxisNode( PDimension graphOutlineSize, int numberOfTicks, double topMargin, int maxExponent, int exponentDelta ) {
         
         setPickable( false );
         setChildrenPickable( false );
         
         final double usableHeight = graphOutlineSize.getHeight() - topMargin;
-        tickSpacing = usableHeight / ( numberOfTicks - 1 );
+        final double tickYSpacing = usableHeight / ( numberOfTicks - 1 );
         
         double y = topMargin;
         int exponent = maxExponent;
@@ -45,29 +62,29 @@ class ConcentrationYAxisNode extends PComposite {
         addChild( axisNode );
         for ( int i = 0; i < numberOfTicks; i++ ) {
             
-            if ( i % exponentSpacing == 0 ) {
+            if ( i % exponentDelta == 0 ) {
                 
-                PPath leftTickNode = new PPath( new Line2D.Double( -( tickLength / 2 ), y, +( tickLength / 2 ), y ) );
-                leftTickNode.setStroke( tickStroke );
-                leftTickNode.setStrokePaint( tickColor );
+                PPath leftTickNode = new PPath( new Line2D.Double( -( TICK_LENGTH / 2 ), y, +( TICK_LENGTH / 2 ), y ) );
+                leftTickNode.setStroke( TICK_STROKE );
+                leftTickNode.setStrokePaint( TICK_COLOR );
                 addChild( leftTickNode );
                 
                 String s = "<html>10<sup>" + String.valueOf( exponent ) + "</sup></html>";
                 HTMLNode labelNode = new HTMLNode( s );
-                labelNode.setFont( labelFont );
-                labelNode.setHTMLColor( labelColor );
+                labelNode.setFont( TICK_LABEL_FONT );
+                labelNode.setHTMLColor( TICK_LABEL_COLOR );
                 double xOffset = leftTickNode.getFullBoundsReference().getMinX() - labelNode.getFullBoundsReference().getWidth() - 5;
                 double yOffset = leftTickNode.getFullBoundsReference().getCenterY() - ( labelNode.getFullBoundsReference().getHeight() / 2 );
                 labelNode.setOffset( xOffset, yOffset );
                 axisNode.addChild( labelNode );
             }
 
-            PPath gridlineNode = new PPath( new Line2D.Double( +( tickLength / 2 ), y, graphOutlineSize.getWidth() - ( tickLength / 2 ), y ) );
-            gridlineNode.setStroke( gridlineStroke );
-            gridlineNode.setStrokePaint( gridlineColor );
+            PPath gridlineNode = new PPath( new Line2D.Double( +( TICK_LENGTH / 2 ), y, graphOutlineSize.getWidth() - ( TICK_LENGTH / 2 ), y ) );
+            gridlineNode.setStroke( GRIDLINE_STROKE );
+            gridlineNode.setStrokePaint( GRIDLINE_COLOR );
             axisNode.addChild( gridlineNode );
 
-            y += tickSpacing;
+            y += tickYSpacing;
             exponent--;
         }
         
@@ -83,9 +100,5 @@ class ConcentrationYAxisNode extends PComposite {
         double xOffset = axisNode.getFullBoundsReference().getX() - labelNode.getFullBoundsReference().getWidth() - AXIS_LABEL_X_SPACING;
         double yOffset = axisNode.getFullBoundsReference().getCenterY() + ( labelNode.getFullBoundsReference().getHeight() / 2 );
         labelNode.setOffset( xOffset, yOffset );
-    }
-    
-    public double getTickSpacing() {
-        return tickSpacing;
     }
 }
