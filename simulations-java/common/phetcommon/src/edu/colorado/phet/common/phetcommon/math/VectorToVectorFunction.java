456eedcd11458d74c9fc205ddb43410b5b343fb1
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-08-24 17:17:28 +0000

    see #2474, delete unused classes from phetcommon.math

--- simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/math/VectorToVectorFunction.java
+++
@@ -1,7 +0,0 @@
-package edu.colorado.phet.common.phetcommon.math;
-
-public interface VectorToVectorFunction {
-    double[] evaluate(double[] args);
-
-    void evaluateInPlace(double[] args, double[] output);
-}
