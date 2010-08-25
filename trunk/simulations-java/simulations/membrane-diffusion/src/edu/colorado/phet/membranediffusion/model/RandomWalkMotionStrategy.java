f570a67ac28664531cda4a4bbd96209b636f965f
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-08-25 16:28:47 +0000

    Cleaned up imports.

--- trunk/simulations-java/simulations/membrane-diffusion/src/edu/colorado/phet/membranediffusion/model/RandomWalkMotionStrategy.java
+++ trunk/simulations-java/simulations/membrane-diffusion/src/edu/colorado/phet/membranediffusion/model/RandomWalkMotionStrategy.java
@@ -5,7 +5,6 @@ import java.awt.geom.Rectangle2D;
 import java.util.Random;
 
 import edu.colorado.phet.common.phetcommon.math.Vector2D;
-import edu.colorado.phet.common.phetcommon.math.Vector2D;
 
 /**
  * A motion strategy for making a particle (or whatever) perform a random
