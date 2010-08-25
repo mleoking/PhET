ae6076801b958c18507fb72dca64e6bc83dd0809
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-08-25 23:24:13 +0000

    simplify implementation of MoleculesNode, there was a lot of stuff in here to support unneeded dev controls

--- simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/view/MoleculesNode.java
+++ simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/view/MoleculesNode.java
@@ -26,51 +26,33 @@ public class MoleculesNode extends PComposite {
     private final MoleculeImageParentNode parentReactant, parentProduct, parentH3O, parentOH, parentH2O;
     private final IMoleculeCountStrategy moleculeCountStrategy, h2oCountStrategy;
     
-    private int maxMolecules, maxH2O;
-    private int countReactant, countProduct, countH3O, countOH, countH2O;
-    private double imageScale;
-    
-    /*
-     * Molecule image node.
-     * Also servers as a marker class.
-     */
-    private static class MoleculeImageNode extends PImage {
-        public MoleculeImageNode( Molecule molecule, double scale ) {
-            super( molecule.getIcon() );
-            setScale( scale );
-        }
-    }
-    
-    // marker class for parents of MoleculeImageNode
+    // marker class for parents of molecule image nodes
     protected static class MoleculeImageParentNode extends PComposite {}
     
     public MoleculesNode( final MagnifyingGlass magnifyingGlass ) {
         super();
         setPickable( false );
         
-        this.maxMolecules = ABSConstants.MAGNIFYING_GLASS_MAX_MOLECULES;
-        this.maxH2O = ABSConstants.MAGNIFYING_GLASS_MAX_H2O;
         this.moleculeCountStrategy = new ConcentrationMoleculeCountStrategy();
         this.h2oCountStrategy = new ConstantMoleculeCountStrategy();
-        this.imageScale = ABSConstants.MAGNIFYING_GLASS_IMAGE_SCALE;
         
         this.magnifyingGlass = magnifyingGlass;
         magnifyingGlass.addSolutionRepresentationChangeListener( new SolutionRepresentationChangeAdapter() {
             @Override
             public void solutionChanged() {
                 deleteAllMolecules();
-                updateMoleculeCounts();
+                updateMolecules();
                 updateMinoritySpeciesVisibility();
             }
             
             @Override
             public void strengthChanged() {
-                updateMoleculeCounts();
+                updateMolecules();
             }
             
             @Override
             public void concentrationChanged() {
-                updateMoleculeCounts();
+                updateMolecules();
             }
         });
         magnifyingGlass.addMagnifyingGlassListener( new MagnifyingGlassChangeListener() {
@@ -94,7 +76,7 @@ public class MoleculesNode extends PComposite {
         
         // default state
         parentH2O.setVisible( magnifyingGlass.isWaterVisible() );
-        updateMoleculeCounts();
+        updateMolecules();
         updateMinoritySpeciesVisibility();
     }
     
@@ -108,54 +90,6 @@ public class MoleculesNode extends PComposite {
         return parentH2O.getVisible();
     }
     
-    public int getMaxMolecules() {
-        return maxMolecules;
-    }
-    
-    public void setMaxMolecules( int maxMolecules ) {
-        if ( maxMolecules != this.maxMolecules ) {
-            this.maxMolecules = maxMolecules;
-            updateMoleculeCounts();
-        }
-    }
-    
-    public int getMaxH2O() {
-        return maxH2O;
-    }
-    
-    public void setMaxH2O( int maxH2O ) {
-        if ( maxH2O != this.maxH2O ) {
-            this.maxH2O = maxH2O;
-            updateMoleculeCounts();
-        }
-    }
-    
-    public double getImageScale() {
-        return imageScale;
-    }
-    
-    public void setImageScale( double imageScale ) {
-        if ( imageScale != this.imageScale ) {
-            this.imageScale = imageScale;
-            for ( int i = 0; i < getChildrenCount(); i++ ) {
-                PNode parent = getChild( i );
-                if ( parent instanceof MoleculeImageParentNode ) {
-                    updateScale( parent, imageScale );
-                }
-            }
-        }
-    }
-    
-    // Updates the scale of existing ImageNodes that are children of parent.
-    private static void updateScale( PNode parent, double scale ) {
-        for ( int i = 0; i < parent.getChildrenCount(); i++ ) {
-            PNode child = parent.getChild( i );
-            if ( child instanceof MoleculeImageNode ) {
-                ( (MoleculeImageNode) child ).setScale( scale );
-            }
-        }
-    }
-    
     /*
      * Our implementation (borrowed from advanced-acid-base-solutions) will always show
      * at least 1 of the minor species of molecules.  But in this sim, we never want to 
@@ -167,75 +101,34 @@ public class MoleculesNode extends PComposite {
         parentH3O.setVisible( !( solution instanceof BaseSolution ) ); // hide H3O for bases
     }
     
-    public int getCountReactant() {
-        return countReactant;
-    }
-    
-    public int getCountProduct() {
-        return countProduct;
-    }
-    
-    public int getCountH3O() {
-        return countH3O;
-    }
-    
-    public int getCountOH() {
-        return countOH;
-    }
-    
-    public int getCountH2O() {
-        return countH2O;
-    }
-    
-    protected PNode getParentReactant() {
-        return parentReactant;
-    }
-    
-    protected PNode getParentProduct() {
-        return parentProduct;
-    }
-    
-    protected PNode getParentH3O() {
-        return parentH3O;
-    }
-    
-    protected PNode getParentOH() {
-        return parentOH;
-    }
-    
-    protected PNode getParentH2O() {
-        return parentH2O;
-    }
-    
-    protected void updateMoleculeCounts() {
-        AqueousSolution solution = magnifyingGlass.getSolution();
-        countReactant = moleculeCountStrategy.getNumberOfMolecules( solution.getSoluteConcentration(), maxMolecules );
-        countProduct = moleculeCountStrategy.getNumberOfMolecules( solution.getProductConcentration(), maxMolecules );
-        countH3O = moleculeCountStrategy.getNumberOfMolecules( solution.getH3OConcentration(), maxMolecules );
-        countOH = moleculeCountStrategy.getNumberOfMolecules( solution.getOHConcentration(), maxMolecules );
-        countH2O = h2oCountStrategy.getNumberOfMolecules( solution.getH2OConcentration(), maxH2O );
-        updateMoleculeNodes();
-    }
-    
     /*
-     * Creates images based on molecule count strategies.
-     * Images are distributed at random locations throughout the container.
+     * Updates the number of molecule nodes for all molecules.
      */
-    protected void updateMoleculeNodes() {
+    private void updateMolecules() {
+        
         AqueousSolution solution = magnifyingGlass.getSolution();
-        if ( !( solution instanceof PureWaterSolution ) ) {
-            updateMoleculeNodes( getParentReactant(), getCountReactant(), getImageScale(), solution.getSolute() );
-            updateMoleculeNodes( getParentProduct(), getCountProduct(), getImageScale(), solution.getProduct() );
-        }
-        updateMoleculeNodes( getParentH3O(), getCountH3O(), getImageScale(), solution.getH3OMolecule() );
-        updateMoleculeNodes( getParentOH(), getCountOH(), getImageScale(), solution.getOHMolecule() );
-        updateMoleculeNodes( getParentH2O(), getCountH2O(), getImageScale(), solution.getWaterMolecule() );
+        
+        // compute molecule counts
+        int countReactant = moleculeCountStrategy.getNumberOfMolecules( solution.getSoluteConcentration(), ABSConstants.MAGNIFYING_GLASS_MAX_MOLECULES );
+        int countProduct = moleculeCountStrategy.getNumberOfMolecules( solution.getProductConcentration(), ABSConstants.MAGNIFYING_GLASS_MAX_MOLECULES );
+        int countH3O = moleculeCountStrategy.getNumberOfMolecules( solution.getH3OConcentration(), ABSConstants.MAGNIFYING_GLASS_MAX_MOLECULES );
+        int countOH = moleculeCountStrategy.getNumberOfMolecules( solution.getOHConcentration(), ABSConstants.MAGNIFYING_GLASS_MAX_MOLECULES );
+        
+        // use a different strategy for H2O molecule counts
+        int countH2O = h2oCountStrategy.getNumberOfMolecules( solution.getH2OConcentration(), ABSConstants.MAGNIFYING_GLASS_MAX_H2O );
+        
+        // update number of molecule image nodes
+        updateMoleculeNodes( parentReactant, solution.getSolute(), countReactant );
+        updateMoleculeNodes( parentProduct, solution.getProduct(), countProduct );
+        updateMoleculeNodes( parentH3O, solution.getH3OMolecule(), countH3O );
+        updateMoleculeNodes( parentOH, solution.getOHMolecule(), countOH  );
+        updateMoleculeNodes( parentH2O, solution.getWaterMolecule(), countH2O );
     }
     
     /*
      * Updates the number of molecule nodes for one type of molecule.
      */
-    private void updateMoleculeNodes( PNode parent, int count, double scale, Molecule molecule ) {
+    private void updateMoleculeNodes( PNode parent, Molecule molecule, int count ) {
 
         // remove nodes
         while ( count < parent.getChildrenCount() && count >= 0 ) {
@@ -244,17 +137,23 @@ public class MoleculesNode extends PComposite {
 
         // add nodes
         while ( count > parent.getChildrenCount() ) {
-            MoleculeImageNode node = new MoleculeImageNode( molecule, scale );
+            // create node
+            PImage node = new PImage( molecule.getIcon() );
+            node.setScale( ABSConstants.MAGNIFYING_GLASS_IMAGE_SCALE );
+            parent.addChild( node );
+            // move to a random location
             Point2D p = getRandomPoint();
             double x = p.getX() - ( node.getFullBoundsReference().getWidth() / 2 );
             double y = p.getY() - ( node.getFullBoundsReference().getHeight() / 2 );
             node.setOffset( x, y );
-            parent.addChild( node );
         }
 
         assert( count == parent.getChildrenCount() );
     }
     
+    /*
+     * Deletes all molecule image nodes, leaving other child nodes untouched.
+     */
     private void deleteAllMolecules() {
         for ( int i = 0; i < getChildrenCount(); i++ ) {
             PNode node = getChild( i );
@@ -262,7 +161,6 @@ public class MoleculesNode extends PComposite {
                 node.removeAllChildren();
             }
         }
-        countReactant = countProduct = countH3O = countOH = countH2O = 0;
     }
     
     /* 
