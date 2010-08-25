fcd9d93df1e73ded0cdeb4526d5e3aa6448b9962
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-08-25 19:45:02 +0000

    Cleaned up imports.

--- simulations-java/simulations/membrane-diffusion/src/edu/colorado/phet/membranediffusion/view/ParticleInjectorNode.java
+++ simulations-java/simulations/membrane-diffusion/src/edu/colorado/phet/membranediffusion/view/ParticleInjectorNode.java
@@ -11,7 +11,6 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.geom.Dimension2D;
-import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 import java.awt.geom.RoundRectangle2D;
