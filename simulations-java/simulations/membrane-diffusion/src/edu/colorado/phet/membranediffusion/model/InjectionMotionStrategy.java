1727aaee3e6921a67c820c1ea4ab500bcc36631e
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-08-25 16:26:09 +0000

    Cleaned up and simplified InjectionMotionStrategy.

--- simulations-java/simulations/membrane-diffusion/src/edu/colorado/phet/membranediffusion/model/InjectionMotionStrategy.java
+++ simulations-java/simulations/membrane-diffusion/src/edu/colorado/phet/membranediffusion/model/InjectionMotionStrategy.java
@@ -5,7 +5,6 @@ import java.awt.geom.Rectangle2D;
 import java.util.Random;
 
 import edu.colorado.phet.common.phetcommon.math.Vector2D;
-import edu.colorado.phet.common.phetcommon.math.Vector2D;
 
 /**
  * A motion strategy for injecting particles into the particle chambers.  They
@@ -33,29 +32,18 @@ public class InjectionMotionStrategy extends MotionStrategy {
 	private Vector2D velocityVector = new Vector2D();
 	private Point2D currentLocation = new Point2D.Double();
 	private Rectangle2D motionBounds = new Rectangle2D.Double();
-	private MembraneDiffusionModel model;
 	private int velocityChangeCounter = 0;
 	
-	/**
-	 * Constructor.
-	 * 
-	 * @param initialLocation
-	 * @param model - Model into which the movable element is being injected.
-	 * @param angle - Initial injection angle, in radians.
-	 * TODO: Consider creating an interface with just the elements of the
-	 * model that are needed here, which is primarily the bounds of the
-	 * chambers.
-	 */
-	public InjectionMotionStrategy(Point2D initialLocation, MembraneDiffusionModel model, double angle) {
-		this.initialLocation.setLocation(initialLocation);
-		this.model = model;
-		timeUntilNextVelocityChange = generateNewVelocityChangeTime();
-		velocityVector.setComponents(MAX_VELOCITY, 0);
-		currentLocation.setLocation(initialLocation);
-		
-		// Get the motion bounds set up for this strategy.
-		updateMotionBounds();
-	}
+    /**
+     * Constructor.
+     */
+    public InjectionMotionStrategy(Point2D initialLocation, Rectangle2D bounds, double angle) {
+        this.initialLocation.setLocation(initialLocation);
+        this.motionBounds.setFrame( bounds );
+        timeUntilNextVelocityChange = generateNewVelocityChangeTime();
+        velocityVector.setMagnitudeAndAngle( MAX_VELOCITY, angle );
+        currentLocation.setLocation(initialLocation);
+    }
 
 	@Override
 	public void move(IMovable movableModelElement, double dt) {
@@ -95,39 +83,6 @@ public class InjectionMotionStrategy extends MotionStrategy {
         return new Vector2D(velocityVector.getX(), velocityVector.getY());
     }
 
-    /**
-	 * Update the motion bounds for this strategy based on whether the model
-	 * element is above or below the membrane that separates the two sub-
-	 * chambers.
-	 */
-	private void updateMotionBounds(){
-		if (currentLocation.getY() > model.getMembraneRect().getMaxY()){
-			// Current location is above the membrane, so use the upper
-			// portion of the particle chamber as the bounds.
-			motionBounds.setFrame( 
-					model.getOverallParticleChamberRect().getMinX(),
-					model.getMembraneRect().getMaxY(),
-					model.getOverallParticleChamberRect().getWidth(),
-					model.getOverallParticleChamberRect().getMaxY() - model.getMembraneRect().getMaxY());
-		}
-		else if (currentLocation.getY() < model.getMembraneRect().getMinY()){
-			// Current location is below the membrane, so use the lower
-			// portion of the particle chamber as the bounds.
-			motionBounds.setFrame(
-					model.getOverallParticleChamberRect().getMinX(),
-					model.getOverallParticleChamberRect().getMinY(),
-					model.getOverallParticleChamberRect().getWidth(),
-					model.getMembraneRect().getMinY() - model.getOverallParticleChamberRect().getMinY());
-		}
-		else {
-			// The most likely case for encountering this clause is if the
-			// particle is passing through the membrane, and the bounds should
-			// not be undergoing an update at that time, so just assert.
-			System.out.println(getClass().getName() + "Error: Particle is not in either sub-chamber.");
-			assert false;
-		}
-	}
-	
 	private double generateNewVelocityChangeTime(){
 		if (velocityChangeCounter == 0){
 			return MIN_TIME_TO_FIRST_VELOCITY_UPDATE + RAND.nextDouble() * (MAX_TIME_TO_FIRST_VELOCITY_UPDATE - MIN_TIME_TO_FIRST_VELOCITY_UPDATE);
