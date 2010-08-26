6ee94de1e43fb77b4d3e0ca3617bf9a6516cf3be
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-08-26 00:06:20 +0000

    delete stubs for Save/Load feature, not needed in this sim

--- trunk/simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/AcidBaseSolutionsApplication.java
+++ trunk/simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/AcidBaseSolutionsApplication.java
@@ -11,8 +11,6 @@ import edu.colorado.phet.acidbasesolutions.module.IntroductionModule;
 import edu.colorado.phet.common.phetcommon.application.Module;
 import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
 import edu.colorado.phet.common.phetcommon.application.PhetApplicationLauncher;
-import edu.colorado.phet.common.phetcommon.util.persistence.XMLPersistenceManager;
-import edu.colorado.phet.common.phetcommon.view.PhetFrame;
 import edu.colorado.phet.common.piccolophet.PiccoloPhetApplication;
 
 /**
@@ -23,36 +21,11 @@ import edu.colorado.phet.common.piccolophet.PiccoloPhetApplication;
  */
 public class AcidBaseSolutionsApplication extends PiccoloPhetApplication {
 
-    //----------------------------------------------------------------------------
-    // Instance data
-    //----------------------------------------------------------------------------
-
-    // PersistanceManager is used to save/load simulation configurations.
-    private XMLPersistenceManager persistenceManager;
-
-    //----------------------------------------------------------------------------
-    // Constructors
-    //----------------------------------------------------------------------------
-
-    /**
-     * Sole constructor.
-     *
-     * @param config the configuration for this application
-     */
-    public AcidBaseSolutionsApplication( PhetApplicationConfig config )
-    {
+    public AcidBaseSolutionsApplication( PhetApplicationConfig config ) {
         super( config );
         initModules();
-        initMenubar( config.getCommandLineArgs() );
     }
 
-    //----------------------------------------------------------------------------
-    // Initialization
-    //----------------------------------------------------------------------------
-
-    /*
-     * Initializes the modules.
-     */
     private void initModules() {
         
         boolean dev = isDeveloperControlsEnabled();
@@ -71,43 +44,6 @@ public class AcidBaseSolutionsApplication extends PiccoloPhetApplication {
             module.getControlPanel().addControlFullWidth( Box.createHorizontalStrut( maxWidth ) );
         }
     }
-
-    /*
-     * Initializes the menubar.
-     */
-    private void initMenubar( String[] args ) {
-
-        // File->Save/Load
-        final PhetFrame frame = getPhetFrame();
-//        frame.addFileSaveLoadMenuItems();  //XXX does this sim have Save/Load?
-        if ( persistenceManager == null ) {
-            persistenceManager = new XMLPersistenceManager( frame );
-        }
-    }
-
-    //----------------------------------------------------------------------------
-    // Persistence
-    //----------------------------------------------------------------------------
-
-    /**
-     * Saves the simulation's configuration.
-     */
-    @Override
-    public void save() {
-       //TODO implement
-    }
-
-    /**
-     * Loads the simulation's configuration.
-     */
-    @Override
-    public void load() {
-        //TODO implement
-    }
-
-    //----------------------------------------------------------------------------
-    // main
-    //----------------------------------------------------------------------------
 
     public static void main( final String[] args ) {
         new PhetApplicationLauncher().launchSim( args, ABSConstants.PROJECT, AcidBaseSolutionsApplication.class );
