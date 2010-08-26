b8b3107e0a6bb48a68779931ec76cd8f816b6300
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-08-26 17:28:57 +0000

    doc fix

--- trunk/simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/module/multiplecapacitors/MultipleCapacitorsControlPanel.java
+++ trunk/simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/module/multiplecapacitors/MultipleCapacitorsControlPanel.java
@@ -5,7 +5,7 @@ package edu.colorado.phet.capacitorlab.module.multiplecapacitors;
 import edu.colorado.phet.capacitorlab.module.CLControlPanel;
 
 /**
- * Control panel for the "Introduction" module.
+ * Control panel for the "Multiple Capacitors" module.
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
