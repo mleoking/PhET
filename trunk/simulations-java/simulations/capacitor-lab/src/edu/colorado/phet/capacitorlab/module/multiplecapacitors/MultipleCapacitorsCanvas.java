1928dfc2d0a483c6c8f36f7bf9510228cef9b5d1
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-08-26 17:27:43 +0000

    add stubs for Multiple Capacitors tab

---
+++ trunk/simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/module/multiplecapacitors/MultipleCapacitorsCanvas.java
@@ -0,0 +1,22 @@
+/* Copyright 2010, University of Colorado */
+
+package edu.colorado.phet.capacitorlab.module.multiplecapacitors;
+
+import edu.colorado.phet.capacitorlab.model.CLModel;
+import edu.colorado.phet.capacitorlab.module.CLCanvas;
+
+/**
+ * Canvas for the "Multiple Capacitors" module.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
+public class MultipleCapacitorsCanvas extends CLCanvas {
+
+    public MultipleCapacitorsCanvas( CLModel model, boolean dev ) {
+        //XXX
+    }
+    
+    public void reset() {
+        //XXX
+    }
+}
