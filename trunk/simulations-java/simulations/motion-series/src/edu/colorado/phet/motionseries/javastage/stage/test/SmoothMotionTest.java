f56092284c287542faff1275db934e688c6369e7
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-08-29 18:08:36 +0000

    Added an isolated test for smooth animation.

---
+++ trunk/simulations-java/simulations/motion-series/src/edu/colorado/phet/motionseries/javastage/stage/test/SmoothMotionTest.java
@@ -0,0 +1,63 @@
+package edu.colorado.phet.motionseries.javastage.stage.test;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.geom.Rectangle2D;
+import java.lang.reflect.InvocationTargetException;
+
+import javax.swing.*;
+
+import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
+import edu.umd.cs.piccolo.PCanvas;
+
+/**
+ * It was pointed out that the motion of the block sliding on ice in the motion series sims wasn't
+ * very smooth; this test is to try to provide a SSCCE (short self contained correct example) of the problems.
+ *
+ * @author Sam Reid
+ */
+public class SmoothMotionTest {
+    private final JFrame frame;
+
+    public SmoothMotionTest() {
+        frame = new JFrame( getClass().getName() );
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        frame.setSize( 800, 600 );
+        final PCanvas contentPane = new PCanvas();
+        final PhetPPath node = new PhetPPath( new Rectangle2D.Double( 0, 0, 100, 100 ), Color.yellow, new BasicStroke( 2 ), Color.black );
+        contentPane.getLayer().addChild( node );
+
+        int delay = 5;
+        final double VELOCITY = 5/30.0*delay;//try to keep a nice velocity independent of the delay
+        Timer timer = new Timer( delay, new ActionListener() {
+            double velocity = VELOCITY;
+
+            public void actionPerformed( ActionEvent e ) {
+                if ( node.getFullBounds().getMaxX() > contentPane.getWidth() ) {
+                    velocity = -VELOCITY;
+                }
+                else if ( node.getFullBounds().getMinX() < 0 ) {
+                    velocity = VELOCITY;
+                }
+                node.setX( node.getX() + velocity );
+                node.setY( contentPane.getHeight() / 2 - node.getFullBounds().getHeight() / 2 );
+//                contentPane.paintImmediately( 0,0,contentPane.getWidth(), contentPane.getHeight() );
+            }
+        } );
+        timer.start();
+        frame.setContentPane( contentPane );
+    }
+
+    public static void main( String[] args ) throws InvocationTargetException, InterruptedException {
+        SwingUtilities.invokeAndWait( new Runnable() {
+            public void run() {
+                new SmoothMotionTest().start();
+            }
+        } );
+    }
+
+    private void start() {
+        frame.setVisible( true );
+    }
+}
