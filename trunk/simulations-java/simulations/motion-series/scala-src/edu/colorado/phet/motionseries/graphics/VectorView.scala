615fa8b9bbd59deb1e2d520cee1cf41d2b4f10c0
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-09-04 06:18:23 +0000

    updated documentation and reorganized methods

--- trunk/simulations-java/simulations/motion-series/scala-src/edu/colorado/phet/motionseries/graphics/VectorView.scala
+++ trunk/simulations-java/simulations/motion-series/scala-src/edu/colorado/phet/motionseries/graphics/VectorView.scala
@@ -7,16 +7,23 @@ import edu.colorado.phet.scalacommon.math.Vector2D
 import edu.umd.cs.piccolo.PNode
 import edu.colorado.phet.motionseries.sims.coordinateframes.{YComponent, XComponent, PerpendicularComponent, ParallelComponent}
 
+/**Class VectorView can inspect a MotionSeriesObject and add graphical representations of vectors onto a specified VectorDisplay
+ */
 class VectorView(motionSeriesObject: MotionSeriesObject,
                  vectorViewModel: VectorViewModel,
                  coordinateFrameModel: CoordinateFrameModel,
                  fbdWidth: Int) {
-  def addVectorAllComponents(motionSeriesObject: MotionSeriesObject,
-                             vector: MotionSeriesObjectVector,
-                             freeBodyDiagramOffset: Vector2DModel,
-                             playAreaOffset: Double,
-                             selectedVectorVisible: () => Boolean,
-                             vectorDisplay: VectorDisplay) = {
+  def addVectorAllComponents(motionSeriesObject: MotionSeriesObject, 
+                             vector: MotionSeriesObjectVector, 
+                             vectorDisplay: VectorDisplay): Unit =
+    addAllVectorsAllComponents(motionSeriesObject, vector, new Vector2DModel, 0, () => true, vectorDisplay)
+
+  def addAllVectorsAllComponents(motionSeriesObject: MotionSeriesObject,
+                                 vector: MotionSeriesObjectVector,
+                                 freeBodyDiagramOffset: Vector2DModel,
+                                 playAreaOffset: Double,
+                                 selectedVectorVisible: () => Boolean,
+                                 vectorDisplay: VectorDisplay) = {
 
     //TODO: This looks like it will be very performance intensive
     val parallelComponent = new ParallelComponent(vector, motionSeriesObject)
@@ -40,12 +47,10 @@ class VectorView(motionSeriesObject: MotionSeriesObject,
     addVector(motionSeriesObject, perpComponent, freeBodyDiagramOffset, playAreaOffset, vectorDisplay)
   }
 
-  def addVector(motionSeriesObject: MotionSeriesObject, vector: Vector , freeBodyDiagramOffset: Vector2DModel, offsetPlayArea: Double, vectorDisplay: VectorDisplay) = {
+  def addVector(motionSeriesObject: MotionSeriesObject, vector: Vector, freeBodyDiagramOffset: Vector2DModel, offsetPlayArea: Double, vectorDisplay: VectorDisplay) = {
     vectorDisplay.addVector(vector, freeBodyDiagramOffset, MotionSeriesDefaults.FBD_LABEL_MAX_OFFSET, offsetPlayArea)
     motionSeriesObject.removalListeners += (() => vectorDisplay.removeVector(vector))
   }
-
-  def addVectorAllComponents(motionSeriesObject: MotionSeriesObject, a: MotionSeriesObjectVector, vectorDisplay: VectorDisplay):Unit = addVectorAllComponents(motionSeriesObject, a, new Vector2DModel, 0, () => true, vectorDisplay)
 
   def addAllVectors(motionSeriesObject: MotionSeriesObject, vectorDisplay: VectorDisplay) = {
     addVectorAllComponents(motionSeriesObject, motionSeriesObject.appliedForceVector, vectorDisplay)
@@ -53,8 +58,8 @@ class VectorView(motionSeriesObject: MotionSeriesObject,
     addVectorAllComponents(motionSeriesObject, motionSeriesObject.normalForceVector, vectorDisplay)
     addVectorAllComponents(motionSeriesObject, motionSeriesObject.frictionForceVector, vectorDisplay)
     addVectorAllComponents(motionSeriesObject, motionSeriesObject.wallForceVector, vectorDisplay)
-    addVectorAllComponents(motionSeriesObject, motionSeriesObject.totalForceVector,
-      new Vector2DModel(new Vector2D(0, fbdWidth / 4)), 2,//Needs a separate offset since it should be shown above other force arrows
+    addAllVectorsAllComponents(motionSeriesObject, motionSeriesObject.totalForceVector,
+      new Vector2DModel(new Vector2D(0, fbdWidth / 4)), 2, //Needs a separate offset since it should be shown above other force arrows
       () => vectorViewModel.sumOfForcesVector, vectorDisplay) //no need to add a separate listener, since it is already contained in vectorviewmodel
   }
 }
@@ -68,7 +73,7 @@ trait VectorDisplay {
 class PlayAreaVectorDisplay(transform: ModelViewTransform2D, motionSeriesObject: MotionSeriesObject, vectorViewModel: VectorViewModel) extends PNode with VectorDisplay {
   def addVector(vector: Vector, offset2D: Vector2DModel, maxOffset: Int, offset: Double): Unit = {
     val defaultCenter = motionSeriesObject.height / 2.0
-    def getValue = motionSeriesObject.position2D + new Vector2D(motionSeriesObject.getAngle + java.lang.Math.PI / 2) * (offset + defaultCenter )
+    def getValue = motionSeriesObject.position2D + new Vector2D(motionSeriesObject.getAngle + java.lang.Math.PI / 2) * (offset + defaultCenter)
     val myoffset = new Vector2DModel(getValue)
     motionSeriesObject.addListener(() => myoffset.value = getValue)
     addChild(new BodyVectorNode(transform, vector, myoffset, motionSeriesObject, MotionSeriesDefaults.PLAY_AREA_FORCE_VECTOR_SCALE))
