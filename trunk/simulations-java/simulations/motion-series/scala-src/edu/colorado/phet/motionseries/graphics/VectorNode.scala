a069bc7ff46a256ebc4eeff45c405b1d7153c8b2
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-09-06 16:31:41 +0000

    performance improvements for VectorNode

--- trunk/simulations-java/simulations/motion-series/scala-src/edu/colorado/phet/motionseries/graphics/VectorNode.scala
+++ trunk/simulations-java/simulations/motion-series/scala-src/edu/colorado/phet/motionseries/graphics/VectorNode.scala
@@ -4,15 +4,13 @@ import edu.colorado.phet.common.phetcommon.view.util.PhetFont
 import edu.colorado.phet.common.piccolophet.nodes._
 import java.awt.geom.Point2D
 import edu.colorado.phet.scalacommon.math.Vector2D
-import edu.colorado.phet.motionseries.MotionSeriesDefaults
+import edu.colorado.phet.motionseries.MotionSeriesDefaults._
 
 import edu.umd.cs.piccolo.nodes.PImage
 import edu.umd.cs.piccolo.PNode
 import edu.colorado.phet.scalacommon.Predef._
-import edu.colorado.phet.motionseries.MotionSeriesConfig
 import java.awt.Color
 import edu.colorado.phet.motionseries.model._
-import MotionSeriesConfig._
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.{TransformListener, ModelViewTransform2D}
 
 /**
@@ -21,18 +19,16 @@ import edu.colorado.phet.common.phetcommon.view.graphics.transforms.{TransformLi
  * todo: could improve performance by passing isContainerVisible:()=>Boolean and addContainerVisibleListener:(()=>Unit)=>Unit
  * @author Sam Reid
  */
-class VectorNode(val transform: ModelViewTransform2D, val vector: Vector, val tailLocation: Vector2DModel, maxLabelDistance: Double, vectorLengthScale: Double) extends PNode {
-  val arrowNode = new ArrowNode(new Point2D.Double(0, 0), new Point2D.Double(0, 1), VectorHeadWidth(), VectorHeadWidth(), VectorTailWidth(), 0.5, true) {
+class VectorNode(val transform: ModelViewTransform2D,
+                 val vector: Vector,
+                 val tailLocation: Vector2DModel,
+                 maxLabelDistance: Double,
+                 vectorLengthScale: Double) extends PNode {
+  val arrowNode = new ArrowNode(new Point2D.Double(0, 0), new Point2D.Double(0, 1), VECTOR_ARROW_HEAD_WIDTH, VECTOR_ARROW_HEAD_WIDTH, VECTOR_ARROW_TAIL_WIDTH, 0.5, true) {
     setPaint(vector.getPaint)
   }
-  MotionSeriesConfig.VectorTailWidth.addListener(() => {arrowNode.setTailWidth(MotionSeriesConfig.VectorTailWidth.value)})
-  MotionSeriesConfig.VectorHeadWidth.addListener(() => {
-    arrowNode.setHeadWidth(MotionSeriesConfig.VectorHeadWidth.value)
-    arrowNode.setHeadHeight(MotionSeriesConfig.VectorHeadWidth.value)
-  })
-
   addChild(arrowNode)
-  private val abbreviatonTextNode = {
+  private val labelNode = {
     val html = new ShadowHTMLNode(vector.html) {
       setShadowColor(vector.color)
       setColor(Color.black)
@@ -42,7 +38,7 @@ class VectorNode(val transform: ModelViewTransform2D, val vector: Vector, val ta
     //for performance, buffer these outlines; htmlnodes are very processor intensive, each outline is 5 htmlnodes and there are many per sim
     new PImage(html.toImage)
   }
-  addChild(abbreviatonTextNode)
+  addChild(labelNode)
 
   /**
    * Method object that updates the graphics for the arrow node and the corresponding textual label.
@@ -56,21 +52,27 @@ class VectorNode(val transform: ModelViewTransform2D, val vector: Vector, val ta
   case class UpdateState(visible: Boolean, tip: Vector2D, tail: Vector2D)
   private var lastUpdateState = new UpdateState(true, new Vector2D, new Vector2D(123, 456)) //create with dummy data to ensure changed on first update()
 
+  //Allocate these temporary variables here for performance reasons, this update call is called a lot and is expensive
+  //from allocation of Vector2D 
+  val labelDistance = 0.6*vectorLengthScale
+  val minLabelDistance = maxLabelDistance / 2.0 //todo: improve heuristics for min label distance, or make it settable in the constructor
+  val labelScale = labelNode.getFullBounds.getHeight * 0.56
+  val labelBounds = labelNode.getFullBounds
+  
   val update = () => {
     val viewTip = transform.modelToViewDouble(vector.vector2DModel() * vectorLengthScale + tailLocation.value)
     val viewTail = transform.modelToViewDouble(tailLocation.value)
     val updateState = new UpdateState(vector.visible.booleanValue, viewTail, viewTip)
     val stayedInvisible = !updateState.visible && !lastUpdateState.visible
     if (updateState != lastUpdateState && !stayedInvisible) { //skip expensive updates if no change
-      //      println("Updating "+vector.abbreviation)
+//      println("Updating " + vector.abbreviation)
       setVisible(vector.visible.booleanValue)
       //Update the arrow node itself
       arrowNode.setTipAndTailLocations(viewTip, viewTail)
 
       //Update the location of the text label
       val textLocation = {
-        val proposedLabelLocation = vector.vector2DModel() * 0.6 * vectorLengthScale
-        val minLabelDistance = maxLabelDistance / 2.0 //todo: improve heuristics for min label distance, or make it settable in the constructor
+        val proposedLabelLocation = vector.vector2DModel() * labelDistance
         val labelVector = if (proposedLabelLocation.magnitude > maxLabelDistance)
           new Vector2D(vector.angle) * maxLabelDistance
         else if (proposedLabelLocation.magnitude < minLabelDistance && proposedLabelLocation.magnitude > 1E-2)
@@ -81,15 +83,15 @@ class VectorNode(val transform: ModelViewTransform2D, val vector: Vector, val ta
         val viewPt = transform.modelToViewDouble(labelVector + tailLocation.value)
 
         //vector.angle is negative since the coordinate frame is flipped going from model to view
-        val deltaArrow = new Vector2D(-vector.angle - vector.labelAngle) * abbreviatonTextNode.getFullBounds.getHeight * 0.56 //move orthogonal to the vector itself
+        val deltaArrow = new Vector2D(-vector.angle - vector.labelAngle) * labelScale //move orthogonal to the vector itself
         deltaArrow + viewPt
       }
-      abbreviatonTextNode.setOffset(textLocation.x - abbreviatonTextNode.getFullBounds.getWidth / 2, textLocation.y - abbreviatonTextNode.getFullBounds.getHeight / 2 +
-              (if (vector.labelAngle == 0) -abbreviatonTextNode.getFullBounds.getHeight / 2 else 0)) //Net force vector label should always be above, see MotionSeriesObject
-      abbreviatonTextNode.setVisible(viewTail.distance(viewTip) > 1)
+      labelNode.setOffset(textLocation.x - labelBounds.width / 2, textLocation.y - labelBounds.height / 2 +
+              (if (vector.labelAngle == 0) -labelBounds.height / 2 else 0)) //Net force vector label should always be above, see MotionSeriesObject
+      labelNode.setVisible(viewTail.distance(viewTip) > 1)
       lastUpdateState = updateState
     } else {
-      //      println("Skipping "+vector.abbreviation)
+//      println("Skipping " + vector.abbreviation)
     }
   }
   update()
@@ -115,7 +117,7 @@ class BodyVectorNode(transform: ModelViewTransform2D,
                      offset: Vector2DModel,
                      motionSeriesObject: MotionSeriesObject,
                      vectorLengthScale: Double)
-        extends VectorNode(transform, vector, offset, MotionSeriesDefaults.BODY_LABEL_MAX_OFFSET, vectorLengthScale) {
+        extends VectorNode(transform, vector, offset, BODY_LABEL_MAX_OFFSET, vectorLengthScale) {
   def doUpdate() = setOffset(motionSeriesObject.position2D)
   //TODO: only listen to position of motion series object
   motionSeriesObject.addListener(doUpdate)
