b1775f9645b99cfa99edff8f23aa4e51427eba84
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-09-08 05:24:41 +0000

    added documentation

--- trunk/simulations-java/simulations/motion-series/scala-src/edu/colorado/phet/motionseries/graphics/RampHeightIndicator.scala
+++ trunk/simulations-java/simulations/motion-series/scala-src/edu/colorado/phet/motionseries/graphics/RampHeightIndicator.scala
@@ -13,20 +13,29 @@ import java.text.DecimalFormat
 import edu.colorado.phet.scalacommon.math.Vector2D
 import edu.colorado.phet.scalacommon.Predef._
 
+/**
+ * This PNode indicates the height of the topmost part of the rotatable ramp segment.
+ * @author Sam Reid
+ */
 class RampHeightIndicator(rampSegment: Rotatable, transform: ModelViewTransform2D) extends PNode {
   val line = new PhetPPath(new BasicStroke(2f), Color.black)
   addChild(line)
 
-  val readout = new PText
-  readout.setFont(MotionSeriesDefaults.rampIndicatorFont)
-  addChild(readout)
-  def getLine = new Line2D.Double(new Vector2D(rampSegment.endPoint.x, 0), rampSegment.endPoint)
-  defineInvokeAndPass(rampSegment.addListenerByName) {
-    line.setPathTo(transform.createTransformedShape(getLine))
-    readout.setOffset(line.getFullBounds.getMaxX + 10, line.getFullBounds.getCenterY)
-    val heightValue = new DecimalFormat("0.0".literal).format(rampSegment.endPoint.y)
-    readout.setText("ramp.height-indicator".messageformat(heightValue))
+  val readout = new PText {
+    setFont(MotionSeriesDefaults.rampIndicatorFont)
   }
+  addChild(readout)
+  rampSegment.addListener(update)
+  update()
+
   setPickable(false)
   setChildrenPickable(false)
+
+  def update() = {
+    //Draw a vertical line from the endpoint of the ramp to the ground
+    line.setPathTo(transform.createTransformedShape(new Line2D.Double(new Vector2D(rampSegment.endPoint.x, 0), rampSegment.endPoint)))
+    readout.setOffset(line.getFullBounds.getMaxX + 10, line.getFullBounds.getCenterY)
+    val rampHeight = new DecimalFormat("0.0".literal).format(rampSegment.endPoint.y)
+    readout.setText("ramp.height-indicator".messageformat(rampHeight))
+  }
 }
