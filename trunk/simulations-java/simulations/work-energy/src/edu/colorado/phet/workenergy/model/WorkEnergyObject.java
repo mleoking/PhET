eac16081940f9049faf0260ddefb85a266677f7b
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-09-10 05:31:22 +0000

    added model properties

--- trunk/simulations-java/simulations/work-energy/src/edu/colorado/phet/workenergy/model/WorkEnergyObject.java
+++ trunk/simulations-java/simulations/work-energy/src/edu/colorado/phet/workenergy/model/WorkEnergyObject.java
@@ -6,9 +6,23 @@ package edu.colorado.phet.workenergy.model;
 public class WorkEnergyObject {
     private MutableDouble mass = new MutableDouble(20.0);//kg
     private MutableDouble gravity = new MutableDouble(0.0);//kg/m/m; in space to start with
+
     private MutableVector2D position = new MutableVector2D(0, 0);
     private MutableVector2D velocity = new MutableVector2D(0, 0);
     private MutableVector2D acceleration = new MutableVector2D(0, 0);
-    private MutableDouble kineticEnergy = new MutableDouble(0);
+
     private MutableVector2D appliedForce = new MutableVector2D(0, 0);
+    private MutableVector2D frictionForce = new MutableVector2D(0, 0);
+    private MutableVector2D gravityForce = new MutableVector2D(0, 0);
+    private MutableVector2D netForce = new MutableVector2D(0, 0);
+
+    private MutableDouble kineticEnergy = new MutableDouble(0);
+    private MutableDouble thermalEnergy = new MutableDouble(0);
+    private MutableDouble potentialEnergy = new MutableDouble(0);
+    private MutableDouble totalEnergy = new MutableDouble(0);
+
+    private MutableDouble netWork = new MutableDouble(0);
+    private MutableDouble gravityWork = new MutableDouble(0);
+    private MutableDouble frictionWork = new MutableDouble(0);
+    private MutableDouble appliedWork = new MutableDouble(0);
 }
