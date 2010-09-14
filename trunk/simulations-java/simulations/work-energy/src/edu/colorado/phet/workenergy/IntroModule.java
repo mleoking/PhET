485082a90d8dcd6b6bd22d9ef1e53505a9ab03e8
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-09-14 16:43:03 +0000

    preliminary work on the work-energy model

--- trunk/simulations-java/simulations/work-energy/src/edu/colorado/phet/workenergy/IntroModule.java
+++ trunk/simulations-java/simulations/work-energy/src/edu/colorado/phet/workenergy/IntroModule.java
@@ -7,6 +7,7 @@ import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
 import edu.colorado.phet.common.phetcommon.view.PhetFrame;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform2D;
 import edu.colorado.phet.common.piccolophet.PhetPCanvas;
+import edu.colorado.phet.workenergy.model.WorkEnergyModel;
 import edu.colorado.phet.workenergy.model.WorkEnergyObject;
 import edu.colorado.phet.workenergy.view.WorkEnergyObjectNode;
 
@@ -18,9 +19,9 @@ import java.awt.geom.Rectangle2D;
 public class IntroModule extends Module {
     public IntroModule(PhetFrame phetFrame) {
         super("Work Energy", new ConstantDtClock(30, 1.0));
-        final WorkEnergyObject workEnergyObject = new WorkEnergyObject();
+        final WorkEnergyModel model =new WorkEnergyModel();
         ModelViewTransform2D transform = new ModelViewTransform2D(new Rectangle2D.Double(0, 0, 1, 1), new Rectangle2D.Double(0, 0, 1, 1));
-        WorkEnergyObjectNode node = new WorkEnergyObjectNode(workEnergyObject, transform);
+        WorkEnergyObjectNode node = new WorkEnergyObjectNode(model.getObject(), transform);
         final PhetPCanvas panel = new PhetPCanvas();
         panel.addScreenChild(node);
         setSimulationPanel(panel);
@@ -30,7 +31,7 @@ public class IntroModule extends Module {
             @Override
             public void simulationTimeChanged(ClockEvent clockEvent) {
                 super.simulationTimeChanged(clockEvent);
-                workEnergyObject.stepInTime(clockEvent.getSimulationTimeChange());
+                model.stepInTime(clockEvent.getSimulationTimeChange());
             }
         });
     }
--- trunk/simulations-java/simulations/work-energy/src/edu/colorado/phet/workenergy/view/WorkEnergyObjectNode.java
+++ trunk/simulations-java/simulations/work-energy/src/edu/colorado/phet/workenergy/view/WorkEnergyObjectNode.java
@@ -8,10 +8,12 @@ import edu.colorado.phet.workenergy.model.WorkEnergyObject;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
 import edu.umd.cs.piccolo.event.PInputEvent;
+import edu.umd.cs.piccolo.nodes.PText;
 import edu.umd.cs.piccolo.util.PDimension;
 
 import java.awt.*;
 import java.awt.geom.Ellipse2D;
+import java.text.DecimalFormat;
 
 /**
  * @author Sam Reid
@@ -20,35 +22,48 @@ public class WorkEnergyObjectNode extends PNode {
     private WorkEnergyObject workEnergyObject;
     private ModelViewTransform2D transform;
 
-    public WorkEnergyObjectNode(final WorkEnergyObject workEnergyObject, final ModelViewTransform2D transform) {
+    public WorkEnergyObjectNode( final WorkEnergyObject workEnergyObject, final ModelViewTransform2D transform ) {
         this.workEnergyObject = workEnergyObject;
         this.transform = transform;
-        final PhetPPath path = new PhetPPath(new Ellipse2D.Double(0, 0, 100, 100), Color.blue, new BasicStroke(2), Color.black);
-        addChild(path);
-        addInputEventListener(new CursorHandler());
-        addInputEventListener(new PBasicInputEventHandler() {
+        final PhetPPath path = new PhetPPath( new Ellipse2D.Double( 0, 0, 100, 100 ), Color.blue, new BasicStroke( 2 ), Color.black );
+        addChild( path );
+
+        final PText keReadoutText = new PText();
+        addChild( keReadoutText );
+        addInputEventListener( new CursorHandler() );
+        addInputEventListener( new PBasicInputEventHandler() {
             @Override
-            public void mouseDragged(PInputEvent event) {
-                final PDimension delta = event.getDeltaRelativeTo(path);
+            public void mouseDragged( PInputEvent event ) {
+                final PDimension delta = event.getDeltaRelativeTo( path );
 //                System.out.println("delta = " + delta);
-                if (delta.width > 0) {
-                    workEnergyObject.setAppliedForce(1, 0);
-                } else if (delta.width < 0) {
-                    workEnergyObject.setAppliedForce(-1, 0);
+                if ( delta.width > 0 ) {
+                    workEnergyObject.setAppliedForce( 1, 0 );
+                }
+                else if ( delta.width < 0 ) {
+                    workEnergyObject.setAppliedForce( -1, 0 );
                 }
             }
 
             @Override
-            public void mouseReleased(PInputEvent event) {
-                workEnergyObject.setAppliedForce(0, 0);
+            public void mouseReleased( PInputEvent event ) {
+                workEnergyObject.setAppliedForce( 0, 0 );
             }
-        });
+        } );
+
         final SimpleObserver updatePosition = new SimpleObserver() {
             public void update() {
-                setOffset(transform.modelToView(workEnergyObject.getPositionProperty().getValue()));
+                setOffset( transform.modelToView( workEnergyObject.getPositionProperty().getValue() ) );
             }
         };
         updatePosition.update();
-        workEnergyObject.getPositionProperty().addObserver(updatePosition);
+        workEnergyObject.getPositionProperty().addObserver( updatePosition );
+
+        SimpleObserver updateKineticEnergy = new SimpleObserver() {
+            public void update() {
+                keReadoutText.setText( "KE = " + new DecimalFormat( "0.00" ).format( workEnergyObject.getKineticEnergyProperty().getValue() ) );
+            }
+        };
+        updateKineticEnergy.update();
+        workEnergyObject.getKineticEnergyProperty().addObserver( updateKineticEnergy );
     }
 }
