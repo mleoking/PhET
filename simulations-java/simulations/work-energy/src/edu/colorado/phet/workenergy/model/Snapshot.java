4a086d9199b0b4fec35dd3510f98449fdc395e99
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-09-14 16:43:03 +0000

    preliminary work on the work-energy model

---
+++ simulations-java/simulations/work-energy/src/edu/colorado/phet/workenergy/model/Snapshot.java
@@ -0,0 +1,11 @@
+package edu.colorado.phet.workenergy.model;
+
+/**
+ * @author Sam Reid
+ */
+public class Snapshot {
+    private WorkEnergyObject state;
+    public Snapshot( WorkEnergyObject state ) {
+        this.state = state;
+    }
+}
