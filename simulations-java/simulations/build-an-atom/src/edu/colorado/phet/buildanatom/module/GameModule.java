f1f3de4946f1cb4a6029cc112475c09c0de1984a
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-09-22 17:49:30 +0000

    Added a new module and canvas for the game tab.  Not filled in yet, just put in place to accommodate upcoming development.

---
+++ simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/module/GameModule.java
@@ -0,0 +1,63 @@
+/* Copyright 2007-2008, University of Colorado */
+
+package edu.colorado.phet.buildanatom.module;
+
+import java.awt.Frame;
+
+import edu.colorado.phet.buildanatom.BuildAnAtomStrings;
+import edu.colorado.phet.buildanatom.model.BuildAnAtomClock;
+import edu.colorado.phet.buildanatom.model.BuildAnAtomModel;
+import edu.colorado.phet.buildanatom.view.GameCanvas;
+import edu.colorado.phet.common.piccolophet.PiccoloModule;
+
+/**
+ * Module for the game tab.
+ */
+public class GameModule extends PiccoloModule {
+
+    //----------------------------------------------------------------------------
+    // Instance data
+    //----------------------------------------------------------------------------
+
+    private BuildAnAtomModel model;
+    private GameCanvas canvas;
+
+    //----------------------------------------------------------------------------
+    // Constructors
+    //----------------------------------------------------------------------------
+
+    public GameModule( Frame parentFrame ) {
+        super( BuildAnAtomStrings.TITLE_BUILD_ATOM_MODULE, new BuildAnAtomClock( BuildAnAtomDefaults.CLOCK_FRAME_RATE, BuildAnAtomDefaults.CLOCK_DT ) );
+        setClockControlPanel( null );
+
+        // Model
+        BuildAnAtomClock clock = (BuildAnAtomClock) getClock();
+        model = new BuildAnAtomModel( clock );
+
+        // Canvas
+        canvas = new GameCanvas( model );
+        setSimulationPanel( canvas );
+
+        // Help
+        if ( hasHelp() ) {
+            //XXX add help items
+        }
+
+        // Set initial state
+        reset();
+    }
+
+    //----------------------------------------------------------------------------
+    // Module overrides
+    //----------------------------------------------------------------------------
+
+    /**
+     * Resets the module.
+     */
+    public void reset() {
+
+        // reset the clock
+        BuildAnAtomClock clock = model.getClock();
+        clock.resetSimulationTime();
+    }    
+}
