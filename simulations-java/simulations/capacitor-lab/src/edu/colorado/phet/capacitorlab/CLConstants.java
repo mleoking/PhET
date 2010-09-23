f901b1d9646af8f56a25ce759d5073717bbf7546
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-09-23 00:10:06 +0000

    use the same exponent for value and label, with max label format 1x10^N

--- simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/CLConstants.java
+++ simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/CLConstants.java
@@ -78,9 +78,9 @@ public class CLConstants {
     public static final double DRAG_HANDLE_ARROW_LENGTH = 35; // pixels
     
     // default exponents for the meters
-    public static final int CAPACITANCE_METER_VALUE_EXPONENT = -14;
-    public static final int PLATE_CHARGE_METER_VALUE_EXPONENT = -11;
-    public static final int STORED_ENERGY_METER_VALUE_EXPONENT = -11;
+    public static final int CAPACITANCE_METER_VALUE_EXPONENT = -13;
+    public static final int PLATE_CHARGE_METER_VALUE_EXPONENT = -10;
+    public static final int STORED_ENERGY_METER_VALUE_EXPONENT = -10;
     
     // visibility of meters
     public static final boolean CAPACITANCE_METER_VISIBLE = true;
