13e73ccfbd79468ed970839c66924869a372c82d
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-09-27 16:16:51 +0000

    Added initial setup of model view transform.

--- simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/view/BuildAnAtomCanvas.java
+++ simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/view/BuildAnAtomCanvas.java
@@ -2,13 +2,17 @@
 
 package edu.colorado.phet.buildanatom.view;
 
+import java.awt.Point;
 import java.awt.geom.Dimension2D;
+import java.awt.geom.Point2D;
 
 import edu.colorado.phet.buildanatom.BuildAnAtomConstants;
 import edu.colorado.phet.buildanatom.BuildAnAtomResources;
 import edu.colorado.phet.buildanatom.model.BuildAnAtomModel;
 import edu.colorado.phet.buildanatom.module.BuildAnAtomDefaults;
+import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform2D;
 import edu.colorado.phet.common.piccolophet.PhetPCanvas;
+import edu.colorado.phet.neuron.module.NeuronDefaults;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.nodes.PImage;
 
@@ -22,45 +26,57 @@ public class BuildAnAtomCanvas extends PhetPCanvas {
     //----------------------------------------------------------------------------
 
     // Model
-    private BuildAnAtomModel model;
-    
-    // View 
-    private PNode rootNode;
-    
+    private final BuildAnAtomModel model;
+
+    // View
+    private final PNode rootNode;
+
+    // Transform.
+    private final ModelViewTransform2D mvt;
+
     //----------------------------------------------------------------------------
     // Constructors
     //----------------------------------------------------------------------------
-    
+
     public BuildAnAtomCanvas( BuildAnAtomModel model ) {
-        
+
         this.model = model;
-        
+
+        // Set up the canvas-screen transform.
+        setWorldTransformStrategy(new PhetPCanvas.CenteringBoxStrategy(this, BuildAnAtomDefaults.INTERMEDIATE_RENDERING_SIZE));
+
+        // Set up the model-canvas transform.
+        mvt = new ModelViewTransform2D(
+                new Point2D.Double( 0, 0 ),
+                new Point( (int) Math.round( BuildAnAtomDefaults.INTERMEDIATE_RENDERING_SIZE.width * 0.5 ),
+                (int) Math.round( BuildAnAtomDefaults.INTERMEDIATE_RENDERING_SIZE.height * 0.5 ) ),
+                3.9, // Scale factor - smaller numbers "zoom out", bigger ones "zoom in".
+        true );
+
         setBackground( BuildAnAtomConstants.CANVAS_BACKGROUND );
-        
+
         // Root of our scene graph
         rootNode = new PNode();
         addWorldChild( rootNode );
-        
-        
-        
+
         // TODO: Temp - put a sketch of the tab up as a very early prototype.
-//        PImage image = new PImage( BuildAnAtomResources.getImage( "tab-1-sketch-01.png" ));
-//        image.scale( 2.05 );
-//        PImage image = new PImage( BuildAnAtomResources.getImage( "first-tab-of-build-an-atom-sim-with-color.png" ));
-//        image.scale( 1.2 );
-//        image.setOffset( 50, 0 );
-//        rootNode.addChild(image);
+        //        PImage image = new PImage( BuildAnAtomResources.getImage( "tab-1-sketch-01.png" ));
+        //        image.scale( 2.05 );
+        //        PImage image = new PImage( BuildAnAtomResources.getImage( "first-tab-of-build-an-atom-sim-with-color.png" ));
+        //        image.scale( 1.2 );
+        //        image.setOffset( 50, 0 );
+        //        rootNode.addChild(image);
     }
 
 
     //----------------------------------------------------------------------------
     // Accessors
     //----------------------------------------------------------------------------
-    
+
     //----------------------------------------------------------------------------
     // Canvas layout
     //----------------------------------------------------------------------------
-    
+
     /*
      * Updates the layout of stuff on the canvas.
      */
@@ -75,7 +91,7 @@ public class BuildAnAtomCanvas extends PhetPCanvas {
         else if ( BuildAnAtomConstants.DEBUG_CANVAS_UPDATE_LAYOUT ) {
             System.out.println( "ExampleCanvas.updateLayout worldSize=" + worldSize );//XXX
         }
-        
+
         //XXX lay out nodes
     }
 }
