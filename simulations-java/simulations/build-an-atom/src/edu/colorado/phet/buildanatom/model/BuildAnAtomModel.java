16ef6d9aaf53b6dd6055b3d7e9f4f9f45453467a
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-09-27 20:25:52 +0000

    Added initial version of the atom inner class to the model.

--- simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/model/BuildAnAtomModel.java
+++ simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/model/BuildAnAtomModel.java
@@ -2,7 +2,9 @@
 
 package edu.colorado.phet.buildanatom.model;
 
+import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
 
 
 /**
@@ -14,13 +16,15 @@ public class BuildAnAtomModel {
     // Class Data
     //----------------------------------------------------------------------------
 
-    private static final Rectangle2D MODEL_BOUNDS = new Rectangle2D.Double(-150, -150, 300, 300);
+    private static final Rectangle2D MODEL_BOUNDS = new Rectangle2D.Double(-200, -150, 400, 300);
 
     //----------------------------------------------------------------------------
-    // Instance data
+    // Instance Data
     //----------------------------------------------------------------------------
 
     private final BuildAnAtomClock clock;
+
+    private final Atom atom;
 
     //----------------------------------------------------------------------------
     // Constructors
@@ -30,13 +34,18 @@ public class BuildAnAtomModel {
         super();
 
         this.clock = clock;
+
+        atom = new Atom(new Point2D.Double(0, 0));
     }
 
     //----------------------------------------------------------------------------
     // Methods
     //----------------------------------------------------------------------------
 
-    // TODO: This is for testing, let's
+    public Atom getAtom() {
+        return atom;
+    }
+
     public Rectangle2D getBounds(){
         return MODEL_BOUNDS;
     }
@@ -44,4 +53,42 @@ public class BuildAnAtomModel {
     public BuildAnAtomClock getClock() {
         return clock;
     }
+
+    //----------------------------------------------------------------------------
+    // Inner Classes and Interfaces
+    //----------------------------------------------------------------------------
+
+    /**
+     * This class represents that atom in the model.  It supplies static
+     * information such as the position of the atom, as well as dynamic
+     * information such as the number of protons present.
+     */
+    public static class Atom {
+
+        private static final double NUCLEUS_RADIUS = 20; // In picometers.
+
+        private final Point2D position = new Point2D.Double();
+
+        private final ArrayList<Double> electronShellRadii = new ArrayList<Double>(){{
+            add(new Double(50));
+            add( new Double(100));
+        }};
+
+        public Atom( Point2D position ) {
+            this.position.setLocation( position );
+        }
+
+        public ArrayList<Double> getElectronShellRadii() {
+            return electronShellRadii;
+        }
+
+        public static double getNucleusRadius() {
+            return NUCLEUS_RADIUS;
+        }
+
+        public Point2D getPosition() {
+            return position;
+        }
+    }
+
 }
