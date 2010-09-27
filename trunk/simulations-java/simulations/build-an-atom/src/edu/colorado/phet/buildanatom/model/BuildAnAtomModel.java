681c56f9b3f1fc5e1ecab891759d8a68441736a5
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-09-27 16:35:25 +0000

    Set up MVT and added a model bounds to the canvas.  This is for setting up of the overall layout.

--- trunk/simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/model/BuildAnAtomModel.java
+++ trunk/simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/model/BuildAnAtomModel.java
@@ -2,33 +2,46 @@
 
 package edu.colorado.phet.buildanatom.model;
 
+import java.awt.geom.Rectangle2D;
+
 
 /**
- * Model template.
+ * Main model class.  Units are picometers (1E-12).
  */
 public class BuildAnAtomModel {
-    
+
+    //----------------------------------------------------------------------------
+    // Class Data
+    //----------------------------------------------------------------------------
+
+    private static final Rectangle2D MODEL_BOUNDS = new Rectangle2D.Double(-150, -150, 300, 300);
+
     //----------------------------------------------------------------------------
     // Instance data
     //----------------------------------------------------------------------------
-    
+
     private final BuildAnAtomClock clock;
 
     //----------------------------------------------------------------------------
     // Constructors
     //----------------------------------------------------------------------------
-    
+
     public BuildAnAtomModel( BuildAnAtomClock clock ) {
         super();
-        
-        this.clock = clock;        
+
+        this.clock = clock;
     }
 
     //----------------------------------------------------------------------------
-    // Accessors
+    // Methods
     //----------------------------------------------------------------------------
-    
+
+    // TODO: This is for testing, let's
+    public Rectangle2D getBounds(){
+        return MODEL_BOUNDS;
+    }
+
     public BuildAnAtomClock getClock() {
         return clock;
-    }    
+    }
 }
