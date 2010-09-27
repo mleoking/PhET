112bd97c6120ca8fd255f8909e668b5bbe693aff
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-09-27 16:35:25 +0000

    Set up MVT and added a model bounds to the canvas.  This is for setting up of the overall layout.

--- simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/view/BuildAnAtomCanvas.java
+++ simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/view/BuildAnAtomCanvas.java
@@ -2,19 +2,19 @@
 
 package edu.colorado.phet.buildanatom.view;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
 import java.awt.Point;
 import java.awt.geom.Dimension2D;
 import java.awt.geom.Point2D;
 
 import edu.colorado.phet.buildanatom.BuildAnAtomConstants;
-import edu.colorado.phet.buildanatom.BuildAnAtomResources;
 import edu.colorado.phet.buildanatom.model.BuildAnAtomModel;
 import edu.colorado.phet.buildanatom.module.BuildAnAtomDefaults;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform2D;
 import edu.colorado.phet.common.piccolophet.PhetPCanvas;
-import edu.colorado.phet.neuron.module.NeuronDefaults;
+import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.umd.cs.piccolo.PNode;
-import edu.umd.cs.piccolo.nodes.PImage;
 
 /**
  * Canvas for the tab where the user builds an atom.
@@ -43,21 +43,24 @@ public class BuildAnAtomCanvas extends PhetPCanvas {
         this.model = model;
 
         // Set up the canvas-screen transform.
-        setWorldTransformStrategy(new PhetPCanvas.CenteringBoxStrategy(this, BuildAnAtomDefaults.INTERMEDIATE_RENDERING_SIZE));
+        setWorldTransformStrategy( new PhetPCanvas.CenteringBoxStrategy( this, BuildAnAtomDefaults.INTERMEDIATE_RENDERING_SIZE ) );
 
         // Set up the model-canvas transform.
         mvt = new ModelViewTransform2D(
                 new Point2D.Double( 0, 0 ),
                 new Point( (int) Math.round( BuildAnAtomDefaults.INTERMEDIATE_RENDERING_SIZE.width * 0.5 ),
                 (int) Math.round( BuildAnAtomDefaults.INTERMEDIATE_RENDERING_SIZE.height * 0.5 ) ),
-                3.9, // Scale factor - smaller numbers "zoom out", bigger ones "zoom in".
-        true );
+                2.5, // Scale factor - smaller numbers "zoom out", bigger ones "zoom in".
+                true );
 
         setBackground( BuildAnAtomConstants.CANVAS_BACKGROUND );
 
         // Root of our scene graph
         rootNode = new PNode();
         addWorldChild( rootNode );
+
+        // Put up the bounds of the model.
+        rootNode.addChild( new PhetPPath( mvt.createTransformedShape( model.getBounds() ), Color.PINK, new BasicStroke( 3f ), Color.BLACK ) );
 
         // TODO: Temp - put a sketch of the tab up as a very early prototype.
         //        PImage image = new PImage( BuildAnAtomResources.getImage( "tab-1-sketch-01.png" ));
