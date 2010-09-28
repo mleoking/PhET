3bb304d571151a4c2596a3ca9da2cb98190fb902
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-09-28 18:44:32 +0000

    added neutrons and protons

---
+++ trunk/simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/model/Neutron.java
@@ -0,0 +1,8 @@
+package edu.colorado.phet.buildanatom.model;
+
+public class Neutron extends SubatomicParticle {
+    public static final double RADIUS = 8;
+    public Neutron( double x, double y ) {
+        super( RADIUS, x, y );
+    }
+}
---
+++ trunk/simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/model/Proton.java
@@ -0,0 +1,8 @@
+package edu.colorado.phet.buildanatom.model;
+
+public class Proton extends SubatomicParticle {
+    public static final double RADIUS = 8;
+    public Proton(double x,double y ) {
+        super( RADIUS, x,y);
+    }
+}
