b45fb34f315f03963f630bd44e605f26de1423cf
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-09-28 21:53:18 +0000

    Added ability to set a destination.

--- simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/model/SubatomicParticle.java
+++ simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/model/SubatomicParticle.java
@@ -1,22 +1,28 @@
 package edu.colorado.phet.buildanatom.model;
 
 import java.awt.geom.Point2D;
+import java.awt.geom.Point2D.Double;
 import java.util.ArrayList;
 
 import edu.colorado.phet.common.phetcommon.model.Observable;
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 
 /**
+ * Abstract base class from which subatomic particles (e.g. electrons) extend.
+ *
  * @author Sam Reid
  * @author John Blanco
  */
 public abstract class SubatomicParticle {
+
     private final Observable<Point2D.Double> position;
+    private final Point2D destination = new Point2D.Double();
     private final double radius;
 
     public SubatomicParticle( double radius, double x, double y ) {
         this.radius = radius;
-        position = new Observable<Point2D.Double>( new Point2D.Double( x,y) );
+        position = new Observable<Point2D.Double>( new Point2D.Double( x, y ) );
+        this.destination.setLocation( x, y );
     }
 
     public Point2D.Double getPosition() {
@@ -28,25 +34,34 @@ public abstract class SubatomicParticle {
     }
 
     public void setPosition( double x, double y ) {
-        position.setValue( new Point2D.Double( x,y) );
+        position.setValue( new Point2D.Double( x, y ) );
     }
+
+    public void setDestination( Point2D.Double position ) {
+        setDestination( position.x, position.y );
+    }
+
+    public void setDestination( double x, double y ) {
+        destination.setLocation( x, y );
+    }
+
     public double getDiameter() {
         return getRadius() * 2;
     }
-    
-    
+
+
     public double getRadius() {
         return radius;
     }
     public void translate( double dx, double dy ) {
         setPosition( position.getValue().getX() + dx, position.getValue().getY() + dy );
     }
-    
-    public void reset(){
+
+    public void reset() {
         position.reset();
     }
 
     public void addPositionListener( SimpleObserver listener ) {
-        position.addObserver(  listener );
+        position.addObserver( listener );
     }
 }
