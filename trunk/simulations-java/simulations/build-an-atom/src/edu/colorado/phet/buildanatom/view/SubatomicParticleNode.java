b1f20fcc5649f3b17c3037394d3dd5b73586c0a8
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-09-28 18:49:37 +0000

    added support for mouse drag on particles

--- trunk/simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/view/SubatomicParticleNode.java
+++ trunk/simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/view/SubatomicParticleNode.java
@@ -5,21 +5,41 @@ import java.awt.geom.Point2D;
 
 import edu.colorado.phet.buildanatom.model.SubatomicParticle;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform2D;
+import edu.colorado.phet.common.piccolophet.event.CursorHandler;
 import edu.colorado.phet.common.piccolophet.nodes.SphericalNode;
 import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
+import edu.umd.cs.piccolo.event.PInputEvent;
+import edu.umd.cs.piccolo.util.PDimension;
 
 public class SubatomicParticleNode extends PNode {
     private ModelViewTransform2D mvt;
     private final SphericalNode sphericalNode;
     private SubatomicParticle subatomicParticle;
     
-    public SubatomicParticleNode( ModelViewTransform2D mvt, SubatomicParticle subatomicParticle, final Color baseColor ) {
+    public SubatomicParticleNode( final ModelViewTransform2D mvt, final SubatomicParticle subatomicParticle, final Color baseColor ) {
         this.mvt = mvt;
         this.subatomicParticle = subatomicParticle;
         sphericalNode = new SphericalNode( mvt.modelToViewDifferentialX( subatomicParticle.getDiameter() ), new SubatomicParticleGradient( subatomicParticle.getRadius(), baseColor ), false );
         addChild( sphericalNode );
 
         updatePosition();
+        
+        addInputEventListener( new CursorHandler( ) );
+        
+        subatomicParticle.addListener( new SubatomicParticle.Listener() {
+            public void positionChanged() {
+                updatePosition();
+            }
+        } );
+        addInputEventListener( new PBasicInputEventHandler(){
+            @Override
+            public void mouseDragged( PInputEvent event ) {
+                PDimension delta=event.getDeltaRelativeTo( getParent() );
+                Point2D modelDelta= mvt.viewToModelDifferential( delta.width,delta.height );
+                subatomicParticle.translate(modelDelta.getX(),modelDelta.getY());
+            }
+        } );
     }
     private void updatePosition() {
         Point2D location = mvt.modelToViewDouble( subatomicParticle.getPosition() );
