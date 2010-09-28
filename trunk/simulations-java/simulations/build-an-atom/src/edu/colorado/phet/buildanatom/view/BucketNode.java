64b5574916f770b76619ed6eea56a860c4c3ee96
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-09-28 16:33:20 +0000

    Added labels to the buckets.

--- trunk/simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/view/BucketNode.java
+++ trunk/simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/view/BucketNode.java
@@ -4,6 +4,7 @@ package edu.colorado.phet.buildanatom.view;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.GradientPaint;
 import java.awt.Paint;
 import java.awt.Shape;
@@ -13,8 +14,10 @@ import java.awt.geom.Point2D;
 import edu.colorado.phet.buildanatom.model.BuildAnAtomModel.Bucket;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform2D;
 import edu.colorado.phet.common.phetcommon.view.util.ColorUtils;
+import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.nodes.PText;
 
 /**
  *
@@ -25,6 +28,8 @@ public class BucketNode extends PNode {
     // ------------------------------------------------------------------------
     // Class Data
     // ------------------------------------------------------------------------
+
+    private static final Font LABEL_FONT = new PhetFont( 18, true );
 
     // ------------------------------------------------------------------------
     // Instance Data
@@ -39,20 +44,35 @@ public class BucketNode extends PNode {
         // want the scaling portion and we want to avoid any translation.
         AffineTransform scaleTransform = AffineTransform.getScaleInstance( mvt.getAffineTransform().getScaleX(),
                 mvt.getAffineTransform().getScaleY() );
+
+        // Create the scaled shapes.
         Shape scaledHoleShape = scaleTransform.createTransformedShape( bucket.getHoleShape() );
         Shape scaledContainerShape = scaleTransform.createTransformedShape( bucket.getContainerShape() );
+
+        // Create and add the container node.
         Paint containerPaint = new GradientPaint(
                 new Point2D.Double( scaledHoleShape.getBounds2D().getMinX(), scaledHoleShape.getBounds2D().getCenterY() ),
                 ColorUtils.brighterColor( bucket.getBaseColor(), 0.5 ),
                 new Point2D.Double( scaledHoleShape.getBounds2D().getMaxX(), scaledHoleShape.getBounds2D().getCenterY() ),
                 ColorUtils.darkerColor( bucket.getBaseColor(), 0.5 ) );
         addChild( new PhetPPath( scaledContainerShape, containerPaint ) );
+
+        // Create and add the hole node.
         Paint holePaint = new GradientPaint(
                 new Point2D.Double( scaledHoleShape.getBounds2D().getMinX(), scaledHoleShape.getBounds2D().getCenterY() ),
                 Color.BLACK,
                 new Point2D.Double( scaledHoleShape.getBounds2D().getMaxX(), scaledHoleShape.getBounds2D().getCenterY() ),
                 Color.LIGHT_GRAY );
         addChild( new PhetPPath( scaledHoleShape, holePaint, new BasicStroke( 1f ), Color.GRAY ) );
+
+        // Create and add the caption (if provided).
+        PText caption = new PText( bucket.getCaptionText() );
+        caption.setFont( LABEL_FONT );
+        caption.setTextPaint( Color.WHITE );
+        caption.setOffset(
+                scaledContainerShape.getBounds2D().getCenterX() - caption.getFullBoundsReference().getWidth() / 2,
+                scaledContainerShape.getBounds2D().getCenterY() - caption.getFullBoundsReference().getHeight() / 2 );
+        addChild( caption );
     }
 
     // ------------------------------------------------------------------------
