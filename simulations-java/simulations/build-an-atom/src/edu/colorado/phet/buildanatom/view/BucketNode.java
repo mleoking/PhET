e4b437bd61db57213fc6c573f0d969242fa4a6a2
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-09-28 16:13:24 +0000

    Added gradients for painting the buckets for a more faux 3D sort of look.

--- simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/view/BucketNode.java
+++ simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/view/BucketNode.java
@@ -2,12 +2,17 @@
 
 package edu.colorado.phet.buildanatom.view;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.GradientPaint;
+import java.awt.Paint;
+import java.awt.Shape;
 import java.awt.geom.AffineTransform;
+import java.awt.geom.Point2D;
 
-import edu.colorado.phet.buildanatom.model.BuildAnAtomModel;
 import edu.colorado.phet.buildanatom.model.BuildAnAtomModel.Bucket;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform2D;
+import edu.colorado.phet.common.phetcommon.view.util.ColorUtils;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.umd.cs.piccolo.PNode;
 
@@ -34,8 +39,20 @@ public class BucketNode extends PNode {
         // want the scaling portion and we want to avoid any translation.
         AffineTransform scaleTransform = AffineTransform.getScaleInstance( mvt.getAffineTransform().getScaleX(),
                 mvt.getAffineTransform().getScaleY() );
-        addChild( new PhetPPath( scaleTransform.createTransformedShape( bucket.getContainerShape() ), bucket.getBaseColor() ) );
-        addChild( new PhetPPath( scaleTransform.createTransformedShape( bucket.getHoleShape() ), Color.black ) );
+        Shape scaledHoleShape = scaleTransform.createTransformedShape( bucket.getHoleShape() );
+        Shape scaledContainerShape = scaleTransform.createTransformedShape( bucket.getContainerShape() );
+        Paint containerPaint = new GradientPaint(
+                new Point2D.Double( scaledHoleShape.getBounds2D().getMinX(), scaledHoleShape.getBounds2D().getCenterY() ),
+                ColorUtils.brighterColor( bucket.getBaseColor(), 0.5 ),
+                new Point2D.Double( scaledHoleShape.getBounds2D().getMaxX(), scaledHoleShape.getBounds2D().getCenterY() ),
+                ColorUtils.darkerColor( bucket.getBaseColor(), 0.5 ) );
+        addChild( new PhetPPath( scaledContainerShape, containerPaint ) );
+        Paint holePaint = new GradientPaint(
+                new Point2D.Double( scaledHoleShape.getBounds2D().getMinX(), scaledHoleShape.getBounds2D().getCenterY() ),
+                Color.BLACK,
+                new Point2D.Double( scaledHoleShape.getBounds2D().getMaxX(), scaledHoleShape.getBounds2D().getCenterY() ),
+                Color.LIGHT_GRAY );
+        addChild( new PhetPPath( scaledHoleShape, holePaint, new BasicStroke( 1f ), Color.GRAY ) );
     }
 
     // ------------------------------------------------------------------------
