3a19c42907fca15c2405f29ac2ea1b7d7f5720d8
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-09-28 16:01:31 +0000

    Got the bottom of the buckets to be rounded, adjusted electron shell sizes.

--- trunk/simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/model/BuildAnAtomModel.java
+++ trunk/simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/model/BuildAnAtomModel.java
@@ -6,7 +6,6 @@ import java.awt.Color;
 import java.awt.Shape;
 import java.awt.geom.Dimension2D;
 import java.awt.geom.Ellipse2D;
-import java.awt.geom.GeneralPath;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
@@ -26,10 +25,10 @@ public class BuildAnAtomModel {
 
     private static final Rectangle2D MODEL_BOUNDS = new Rectangle2D.Double( -200, -150, 400, 300 );
 
-    private static final Dimension2D BUCKET_SIZE = new PDimension( 40, 20 );
-    private static final Point2D PROTON_BUCKET_POSITION = new Point2D.Double( -60, -80 );
-    private static final Point2D NEUTRON_BUCKET_POSITION = new Point2D.Double( 0, -80 );
-    private static final Point2D ELECTRON_BUCKET_POSITION = new Point2D.Double( 60, -80 );
+    private static final Dimension2D BUCKET_SIZE = new PDimension( 60, 30 );
+    private static final Point2D PROTON_BUCKET_POSITION = new Point2D.Double( -100, -120 );
+    private static final Point2D NEUTRON_BUCKET_POSITION = new Point2D.Double( 0, -120 );
+    private static final Point2D ELECTRON_BUCKET_POSITION = new Point2D.Double( 100, -120 );
 
     //----------------------------------------------------------------------------
     // Instance Data
@@ -102,8 +101,8 @@ public class BuildAnAtomModel {
         // the covalent radius values found in Wikipedia.
         private final ArrayList<Double> electronShellRadii = new ArrayList<Double>() {
             {
-                add( new Double( 30 ) );
-                add( new Double( 58 ) );
+                add( new Double( 34 ) );
+                add( new Double( 110 ) );
             }
         };
 
@@ -159,11 +158,16 @@ public class BuildAnAtomModel {
                     size.getHeight() * HOLE_ELLIPSE_HEIGHT_PROPORTION );
             double containerHeight = size.getHeight() * ( 1 - ( HOLE_ELLIPSE_HEIGHT_PROPORTION / 2 ) );
             containerShape = new DoubleGeneralPath();
-            containerShape.moveTo( -size.getWidth() / 2, 0 );
-            containerShape.lineTo( -size.getWidth() *0.4, -containerHeight * 0.8 );
-            containerShape.lineTo( 0, -containerHeight );
-            containerShape.lineTo( size.getWidth() * 0.4, -containerHeight * 0.8 );
-            containerShape.lineTo( size.getWidth() / 2, 0 );
+            containerShape.moveTo( -size.getWidth() * 0.5, 0 );
+            containerShape.lineTo( -size.getWidth() * 0.4, -containerHeight * 0.8 );
+            containerShape.curveTo(
+                    -size.getWidth() * 0.3,
+                    -containerHeight * 0.8 - size.getHeight() * HOLE_ELLIPSE_HEIGHT_PROPORTION * 0.6,
+                    size.getWidth() * 0.3,
+                    -containerHeight * 0.8 - size.getHeight() * HOLE_ELLIPSE_HEIGHT_PROPORTION * 0.6,
+                    size.getWidth() * 0.4,
+                    -containerHeight * 0.8 );
+            containerShape.lineTo( size.getWidth() * 0.5, 0 );
             containerShape.closePath();
         }
 
