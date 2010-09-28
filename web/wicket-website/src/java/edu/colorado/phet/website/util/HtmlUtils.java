3d6f876ece8e3fee2f87bde737caed85a9f9be24
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-09-28 02:27:20 +0000

    website: adding encoding to specific urls

--- web/wicket-website/src/java/edu/colorado/phet/website/util/HtmlUtils.java
+++ web/wicket-website/src/java/edu/colorado/phet/website/util/HtmlUtils.java
@@ -39,4 +39,41 @@ public class HtmlUtils {
         }
         return buf.toString();
     }
+
+    /**
+     * Encodes a string into an HTML-escaped version (attribute version, skips ampersands).
+     *
+     * Escaping other things is just to appease the Nessus monster. This will cause jibberish for <>'s.
+     *
+     * @param s String to encode
+     * @return HTML encoded response
+     */
+    public static String encodeForAttribute( String s ) {
+        if ( s == null ) {
+            return null;
+        }
+        StringBuffer buf = new StringBuffer();
+        int len = s.length();
+
+        for ( int i = 0; i < len; i++ ) {
+            char c = s.charAt( i );
+            switch( c ) {
+                case '<':
+                    buf.append( "&lt;" );
+                    break;
+                case '>':
+                    buf.append( "&gt;" );
+                    break;
+                case '"':
+                    buf.append( "&quot;" );
+                    break;
+                case '\'':
+                    buf.append( "&apos;" );
+                    break;
+                default:
+                    buf.append( c );
+            }
+        }
+        return buf.toString();
+    }
 }
