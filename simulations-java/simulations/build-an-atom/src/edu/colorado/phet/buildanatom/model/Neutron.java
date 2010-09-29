b6405e0f93cccf5d5349d3e6857fba9d9a7ae79f
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-09-29 18:59:44 +0000

    Added default contructor, header comments, and reformatted.

--- simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/model/Neutron.java
+++ simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/model/Neutron.java
@@ -1,8 +1,20 @@
+/* Copyright 2010, University of Colorado */
+
 package edu.colorado.phet.buildanatom.model;
 
+/**
+ * Class that represents a neutron in the model.
+ *
+ * @author John Blanco
+ */
 public class Neutron extends SubatomicParticle {
     public static final double RADIUS = 8;
+
     public Neutron( double x, double y ) {
         super( RADIUS, x, y );
+    }
+
+    public Neutron() {
+        this( 0, 0 );
     }
 }
