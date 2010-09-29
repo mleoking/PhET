611a178d3b7d31ba6c0154d6eb6a8a40a57495e8
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-09-29 03:33:28 +0000

    center debug nodes on front face of dielectric

--- simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/view/DielectricTotalChargeNode.java
+++ simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/view/DielectricTotalChargeNode.java
@@ -2,14 +2,55 @@
 
 package edu.colorado.phet.capacitorlab.view;
 
+import java.awt.geom.Point2D;
+
+import edu.colorado.phet.capacitorlab.model.BatteryCapacitorCircuit;
+import edu.colorado.phet.capacitorlab.model.ModelViewTransform;
+import edu.colorado.phet.capacitorlab.model.Capacitor.CapacitorChangeAdapter;
+import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.PhetPNode;
 import edu.umd.cs.piccolo.nodes.PText;
 
 
 public class DielectricTotalChargeNode extends PhetPNode {
+    
+    private final BatteryCapacitorCircuit circuit;
+    private final ModelViewTransform mvt;
+    private final PText debugNode;
 
-    public DielectricTotalChargeNode() {
-        //XXX
-        addChild( new PText( "total charge" ) );//XXX
+    public DielectricTotalChargeNode( BatteryCapacitorCircuit circuit, ModelViewTransform mvt, boolean dev ) {
+        
+        this.circuit = circuit;
+        this.mvt = mvt;
+        
+        debugNode = new PText( "total charge" );
+        debugNode.setFont( new PhetFont( 16 ) );
+        if ( dev ) {
+            addChild( debugNode );
+        }
+        
+        circuit.getCapacitor().addCapacitorChangeListener( new CapacitorChangeAdapter() {
+
+            @Override
+            public void plateSizeChanged() {
+                update();
+            }
+
+            @Override
+            public void plateSeparationChanged() {
+                update();
+            }
+        } );
+        
+        update();
+    }
+    
+    private void update() {
+        // center the debug node on the front face of the dielectric
+        double x = 0;
+        double y = circuit.getCapacitor().getPlateSeparation() / 2;
+        double z = -circuit.getCapacitor().getPlateSideLength() / 2;
+        Point2D pView = mvt.modelToView( x, y, z );
+        debugNode.setOffset( pView.getX() - ( debugNode.getFullBoundsReference().getWidth() / 2 ), pView.getY() - ( debugNode.getFullBoundsReference().getHeight() / 2 ) );
     }
 }
