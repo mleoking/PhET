46ad0fc62e7e8b43a14bcf43eaa7e5aeda7a1047
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-09-29 18:59:44 +0000

    Added default contructor, header comments, and reformatted.

--- trunk/simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/model/Proton.java
+++ trunk/simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/model/Proton.java
@@ -1,8 +1,20 @@
+/* Copyright 2010, University of Colorado */
+
 package edu.colorado.phet.buildanatom.model;
 
+/**
+ * Class that represents a proton in the model.
+ *
+ * @author John Blanco
+ */
 public class Proton extends SubatomicParticle {
     public static final double RADIUS = 8;
-    public Proton(double x,double y ) {
-        super( RADIUS, x,y);
+
+    public Proton( double x, double y ) {
+        super( RADIUS, x, y );
+    }
+
+    public Proton() {
+        this( 0, 0 );
     }
 }
