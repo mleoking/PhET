bdfa2cc7a56cfd603f63167bd77dc25221e3d31a
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-09-29 18:40:20 +0000

    Fixed formatting and added comments.

--- simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/model/Electron.java
+++ simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/model/Electron.java
@@ -1,8 +1,14 @@
 package edu.colorado.phet.buildanatom.model;
 
+/**
+ * Class the represents an electron in the model.
+ *
+ * @author John Blanco
+ */
 public class Electron extends SubatomicParticle {
     public static final double ELECTRON_RADIUS = 5;
-    public Electron(double x,double y ) {
-        super( ELECTRON_RADIUS ,x,y);
+
+    public Electron( double x, double y ) {
+        super( ELECTRON_RADIUS, x, y );
     }
 }
