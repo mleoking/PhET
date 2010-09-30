efc54f61ca37f879153025a307d19b733f4a5c9a
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-09-30 15:00:59 +0000

    Reformatted to match PhET standards.

--- simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/BuildAnAtomConstants.java
+++ simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/BuildAnAtomConstants.java
@@ -18,18 +18,18 @@ public class BuildAnAtomConstants {
 
     /* Not intended for instantiation. */
     private BuildAnAtomConstants() {}
-    
+
     //----------------------------------------------------------------------------
     // Debugging
     //----------------------------------------------------------------------------
-    
+
     // enable debug output for canvas layout updates
     public static final boolean DEBUG_CANVAS_UPDATE_LAYOUT = false;
-    
+
     //----------------------------------------------------------------------------
     // Application
     //----------------------------------------------------------------------------
-    
+
     public static final String PROJECT_NAME = "build-an-atom";
 
     //----------------------------------------------------------------------------
@@ -39,37 +39,37 @@ public class BuildAnAtomConstants {
     // Default font properties
     public static final int DEFAULT_FONT_STYLE = Font.PLAIN;
     public static final int DEFAULT_FONT_SIZE = 16;
-    
+
     public static final Font CONTROL_PANEL_TITLE_FONT = new PhetFont( Font.BOLD, 12 );
     public static final Font CONTROL_PANEL_CONTROL_FONT = new PhetFont( Font.PLAIN, 12 );
-    
+
     public static final Font PLAY_AREA_TITLE_FONT = new PhetFont( Font.BOLD, 16 );
     public static final Font PLAY_AREA_CONTROL_FONT = new PhetFont( Font.PLAIN, 16 );
-    
+
     //----------------------------------------------------------------------------
     // Strokes
     //----------------------------------------------------------------------------
 
-    public static final Stroke DASHED_STROKE = 
+    public static final Stroke DASHED_STROKE =
         new BasicStroke( 1f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[] {3,3}, 0 );
 
     //----------------------------------------------------------------------------
     // Paints
     //----------------------------------------------------------------------------
-    
+
     // Color of the "play area"
-    public static final Color CANVAS_BACKGROUND = new Color(250, 250, 161);
-    
+    public static final Color CANVAS_BACKGROUND = new Color(255, 255, 153);
+
     // Color of labels placed directly on the play area
     public static final Color CANVAS_LABELS_COLOR = Color.BLACK;
-    
+
     // Generic transparent color
     public static final Color COLOR_TRANSPARENT = new Color( 0f, 0f, 0f, 0f );
-    
+
     //----------------------------------------------------------------------------
     // Images
     //----------------------------------------------------------------------------
-    
+
     //----------------------------------------------------------------------------
     // Cursors
     //----------------------------------------------------------------------------
