309268bd9e616298f4227757f6d832caf280115b
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-09-30 03:27:43 +0000

    Changed the frame rate to be closer to real time.

--- trunk/simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/module/BuildAnAtomDefaults.java
+++ trunk/simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/module/BuildAnAtomDefaults.java
@@ -7,10 +7,9 @@ import java.awt.Dimension;
 import edu.umd.cs.piccolo.util.PDimension;
 
 
-
 /**
  * Default settings that are common to 2 or more modules.
- * 
+ *
  * NOTE! This class is package private, and values herein should only be referenced
  * by the "defaults" classes for each module.  Classes that are module-specific should
  * use the class that corresponds to their module.
@@ -18,14 +17,15 @@ import edu.umd.cs.piccolo.util.PDimension;
 public class BuildAnAtomDefaults {
 
     /* Not intended for instantiation */
-    private BuildAnAtomDefaults() {}
-    
+    private BuildAnAtomDefaults() {
+    }
+
     // Clock
     public static final boolean CLOCK_RUNNING = true;
     public static final int CLOCK_FRAME_RATE = 25; // fps, frames per second (wall time)
-    public static final double CLOCK_DT = 1;
+    public static final double CLOCK_DT = 1 / (double) CLOCK_FRAME_RATE;
     public static final int CLOCK_TIME_COLUMNS = 10;
 
     // Model-view transform
-    public static final PDimension STAGE_SIZE= new PDimension( 1008,679 );
+    public static final PDimension STAGE_SIZE = new PDimension( 1008, 679 );
 }
