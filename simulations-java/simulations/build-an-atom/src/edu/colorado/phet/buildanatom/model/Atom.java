d6644cf739bb01e8256283844dfbb2b5c73abb38
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-10-01 23:19:12 +0000

    Factored out an ElectronShell class - makes the code much cleaner.

--- simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/model/Atom.java
+++ simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/model/Atom.java
@@ -2,8 +2,10 @@ package edu.colorado.phet.buildanatom.model;
 
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Random;
 
+import edu.colorado.phet.common.phetcommon.util.SimpleObservable;
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 
 /**
@@ -20,73 +22,60 @@ public class Atom {
     private static final double NUCLEUS_RADIUS = 10;
 
     // Electron shell radii.
-    private static final double INNER_SHELL_RADIUS = 34;
-    public static final double OUTER_SHELL_RADIUS = 102;
+    private static final double ELECTRON_SHELL_1_RADIUS = 34;
+    public static final double ELECTRON_SHELL_2_RADIUS = 102;
 
     // Position in model space.
     private final Point2D position = new Point2D.Double();
 
-    // List of the subatomic particles that are currently a part of this
-    // atom.
+    // List of the subatomic particles that are currently in the nucleus.
+    // Note that the electrons are maintained in the shells.
     private final ArrayList<Proton> protons = new ArrayList<Proton>();
     private final ArrayList<Neutron> neutrons = new ArrayList<Neutron>();
-    private final ArrayList<Electron> electronsInShell1 = new ArrayList<Electron>();
-    private final ArrayList<Electron> electronsInShell2 = new ArrayList<Electron>();
 
-    // Radii of the electron shells.  The values used for these distances
-    // are remotely related to reality (based on covalent bond radii of
-    // various molecules) but they have been tweaked significantly in
-    // order to be at a scale that works visually in the sim.
-    private final ArrayList<Double> electronShellRadii = new ArrayList<Double>() {
-        {
-            add( new Double( 34 ) );
-            add( new Double( 102 ) );
+    // Shells for containing electrons.
+    private final ElectronShell electronShell1 = new ElectronShell( ELECTRON_SHELL_1_RADIUS, 2 );
+    private final ElectronShell electronShell2 = new ElectronShell( ELECTRON_SHELL_2_RADIUS, 8 );
+
+    // Observer for electron shells.
+    private final SimpleObserver electronShellChangeObserver = new SimpleObserver() {
+        public void update() {
+            checkAndReconfigureShells();
         }
     };
-
-    // Electron shell positions
-    private final ArrayList<Point2D> openShell1Positions = new ArrayList<Point2D>() {
-        {
-            add( new Point2D.Double( INNER_SHELL_RADIUS, 0 ) );
-            add( new Point2D.Double( -INNER_SHELL_RADIUS, 0 ) );
-        }
-    };
-
-    private final ArrayList<Point2D> openShell2Positions = new ArrayList<Point2D>() {
-        {
-            add( new Point2D.Double( OUTER_SHELL_RADIUS * Math.cos( 0 * Math.PI / 4 ), OUTER_SHELL_RADIUS * Math.sin( 0 * Math.PI / 4 ) ) );
-            add( new Point2D.Double( OUTER_SHELL_RADIUS * Math.cos( 1 * Math.PI / 4 ), OUTER_SHELL_RADIUS * Math.sin( 1 * Math.PI / 4 ) ) );
-            add( new Point2D.Double( OUTER_SHELL_RADIUS * Math.cos( 2 * Math.PI / 4 ), OUTER_SHELL_RADIUS * Math.sin( 2 * Math.PI / 4 ) ) );
-            add( new Point2D.Double( OUTER_SHELL_RADIUS * Math.cos( 3 * Math.PI / 4 ), OUTER_SHELL_RADIUS * Math.sin( 3 * Math.PI / 4 ) ) );
-            add( new Point2D.Double( OUTER_SHELL_RADIUS * Math.cos( 4 * Math.PI / 4 ), OUTER_SHELL_RADIUS * Math.sin( 4 * Math.PI / 4 ) ) );
-            add( new Point2D.Double( OUTER_SHELL_RADIUS * Math.cos( 5 * Math.PI / 4 ), OUTER_SHELL_RADIUS * Math.sin( 5 * Math.PI / 4 ) ) );
-            add( new Point2D.Double( OUTER_SHELL_RADIUS * Math.cos( 6 * Math.PI / 4 ), OUTER_SHELL_RADIUS * Math.sin( 6 * Math.PI / 4 ) ) );
-            add( new Point2D.Double( OUTER_SHELL_RADIUS * Math.cos( 7 * Math.PI / 4 ), OUTER_SHELL_RADIUS * Math.sin( 7 * Math.PI / 4 ) ) );
-        }
-    };
-
-    private final ArrayList<Point2D> occupiedShell1Positions = new ArrayList<Point2D>();
-    private final ArrayList<Point2D> occupiedShell2Positions = new ArrayList<Point2D>();
 
     /**
      * Constructor.
      */
     public Atom( Point2D position ) {
         this.position.setLocation( position );
+        electronShell1.addObserver( electronShellChangeObserver );
+        electronShell2.addObserver( electronShellChangeObserver );
+    }
+
+    /**
+     * Check if the shells need to be reconfigured.  This can be necessary
+     * if an electron was removed from shell 1 while there were electrons
+     * present in shell 2.
+     */
+    protected void checkAndReconfigureShells() {
+        if ( !electronShell1.isFull() && !electronShell2.isEmpty() ) {
+            // Need to move an electron from shell 2 to shell 1.
+            System.err.println( getClass().getName() + "Reconfigure not yet implemented." );
+        }
     }
 
     public void reset() {
         protons.clear();
         neutrons.clear();
-        electronsInShell1.clear();
-        electronsInShell2.clear();
-        openShell1Positions.addAll( occupiedShell1Positions );
-        occupiedShell1Positions.clear();
-        openShell2Positions.addAll( occupiedShell2Positions );
-        occupiedShell2Positions.clear();
+        electronShell1.reset();
+        electronShell2.reset();
     }
 
     public ArrayList<Double> getElectronShellRadii() {
+        ArrayList<Double> electronShellRadii = new ArrayList<Double>();
+        electronShellRadii.add( new Double( electronShell1.getRadius() ) );
+        electronShellRadii.add( new Double( electronShell2.getRadius() ) );
         return electronShellRadii;
     }
 
@@ -96,20 +85,6 @@ public class Atom {
 
     public Point2D getPosition() {
         return position;
-    }
-
-    private Electron findClosestOuterShellElectron( Point2D location ) {
-
-        Electron closestElectron = null;
-        if ( electronsInShell2.size() > 0 ) {
-            for ( Electron candidateElectron : electronsInShell2 ) {
-                if ( closestElectron == null || closestElectron.getPosition().distance( location ) > candidateElectron.getPosition().distance( location ) ) {
-                    // The candidate is a better choice.
-                    closestElectron = candidateElectron;
-                }
-            }
-        }
-        return closestElectron;
     }
 
     public void addProton( final Proton proton ) {
@@ -161,83 +136,109 @@ public class Atom {
     }
 
     public void addElectron( final Electron electron ) {
-        assert !electronsInShell1.contains( electron ) && !electronsInShell2.contains( electron );
-
-        if ( electronsInShell1.size() < 2 ) {
-            // The inner shell isn't full yet, so put this electron there.
-            electronsInShell1.add( electron );
-            Point2D locationInShell = findClosestPointInList( electron.getPosition(), openShell1Positions );
-            electron.setDestination( locationInShell );
-            openShell1Positions.remove( locationInShell );
-            occupiedShell1Positions.add( locationInShell );
+        if ( !electronShell1.isFull() ) {
+            electronShell1.addElectron( electron );
         }
-        else if ( electronsInShell2.size() < 8 ) {
-            // Put the electron in the outer shell.
-            electronsInShell2.add( electron );
-            Point2D locationInShell = findClosestPointInList( electron.getPosition(), openShell2Positions );
-            electron.setDestination( locationInShell );
-            openShell2Positions.remove( locationInShell );
-            occupiedShell2Positions.add( locationInShell );
+        else if ( !electronShell2.isFull() ) {
+            electronShell2.addElectron( electron );
         }
         else {
-            assert false; // Should never get here, debug if it does.
+            // Too many electrons.  The sim should be designed such that this
+            // does not occur.  If it does, it should be debugged.
+            assert false;
         }
 
-        // Add a listener for when this electron is grabbed by the user.
         electron.addUserControlListener( new SimpleObserver() {
             public void update() {
                 if ( electron.isUserControlled() ) {
-                    // The user has picked up this particle, so we assume
-                    // that it is essentially removed from the atom.
-                    electron.removeUserControlListener( this );
-                    if ( electronsInShell1.contains( electron ) ) {
-                        // This electron was in the innermost shell.
-                        electronsInShell1.remove( electron );
-                        Point2D innerShellLocation = findClosestPointInList( electron.getPosition(), occupiedShell1Positions );
-                        // If there are one or more electrons in the outer
-                        // shell, the closest one must move down into the
-                        // newly opened spot.
-                        if (electronsInShell2.size() > 0){
-                            Electron electronToMove = findClosestOuterShellElectron( innerShellLocation );
-                            assert electronToMove != null;
-                            Point2D outerShellLocation = findClosestPointInList( electronToMove.getPosition(), occupiedShell2Positions );
-                            electronsInShell2.remove( electronToMove );
-                            electronsInShell1.add( electronToMove );
-                            electronToMove.setDestination( innerShellLocation );
-                            occupiedShell2Positions.remove( outerShellLocation );
-                            openShell2Positions.add(  outerShellLocation );
-                        }
-                        else{
-                            occupiedShell1Positions.remove( innerShellLocation );
-                            openShell1Positions.add( innerShellLocation );
-                        }
-                    }
-                    else if ( electronsInShell2.contains( electron ) ){
-                        electronsInShell2.remove( electron );
-                        Point2D locationInShell = findClosestPointInList( electron.getPosition(), occupiedShell2Positions );
-                        occupiedShell2Positions.remove( locationInShell );
-                        openShell2Positions.add( locationInShell );
-                    }
-                    else{
-                        assert false; // Should never get here, debug if it does.
+                    // This electron is being removed.  Do we need to reconfigure?
+                    if ( !electronShell1.isFull() && !electronShell2.isEmpty() ) {
+                        // Yes we do.  Move an electron from the shell 2 to
+                        // shell 1.
                     }
                 }
             }
         } );
     }
 
-    private Point2D findClosestPointInList( Point2D comparisonPt, ArrayList<Point2D> pointList ) {
-        if ( pointList.size() == 0 ) {
-            return null;
-        }
+    private static class ElectronShell extends SimpleObservable {
+        private final double radius;
+        private final int electronCapacity;
+        private final ArrayList<Point2D> openShellLocations = new ArrayList<Point2D>();
+        private final HashMap<Electron, Point2D> occupiedShellLocations = new HashMap<Electron, Point2D>();
 
-        Point2D closestPoint = null;
-        for ( Point2D candidatePoint : pointList ) {
-            if ( closestPoint == null || closestPoint.distance( comparisonPt ) > candidatePoint.distance( comparisonPt ) ) {
-                // Candidate point is better.
-                closestPoint = candidatePoint;
+        protected ElectronShell( double radius, int electronCapacity ) {
+            this.radius = radius;
+            this.electronCapacity = electronCapacity;
+            // Initialize the open shell locations.
+            double angleBetweenElectrons = 2 * Math.PI / electronCapacity;
+            for ( int i = 0; i < electronCapacity; i++ ) {
+                double angle = i * angleBetweenElectrons;
+                openShellLocations.add( new Point2D.Double( this.radius * Math.cos( angle ), this.radius * Math.sin( angle ) ) );
             }
         }
-        return closestPoint;
+
+        protected double getRadius() {
+            return radius;
+        }
+
+        protected boolean isFull() {
+            return occupiedShellLocations.size() == electronCapacity;
+        }
+
+        protected boolean isEmpty() {
+            return occupiedShellLocations.size() == 0;
+        }
+
+        protected void reset() {
+            for ( Point2D occupiedLocation : occupiedShellLocations.values() ) {
+                assert !openShellLocations.contains( occupiedLocation );
+                openShellLocations.add( occupiedLocation );
+            }
+            occupiedShellLocations.clear();
+        }
+
+        protected void addElectron( final Electron electronToAdd ) {
+            Point2D shellLocation = findClosestOpenLocation( electronToAdd.getPosition() );
+            assert shellLocation != null;
+            electronToAdd.setDestination( shellLocation );
+            openShellLocations.remove( shellLocation );
+            occupiedShellLocations.put( electronToAdd, shellLocation );
+            electronToAdd.addUserControlListener( new SimpleObserver() {
+                public void update() {
+                    if ( electronToAdd.isUserControlled() ) {
+                        // The user has picked up this electron, so consider
+                        // it to be removed from the shell.
+                        removeElectron( electronToAdd );
+                        electronToAdd.removeUserControlListener( this );
+                        notifyObservers();
+                    }
+                }
+            } );
+            notifyObservers();
+
+        }
+
+        private void removeElectron( Electron electronToRemove ) {
+            assert occupiedShellLocations.containsKey( electronToRemove );
+            Point2D newlyFreedShellLocation = occupiedShellLocations.get( electronToRemove );
+            openShellLocations.add( newlyFreedShellLocation );
+            occupiedShellLocations.remove( electronToRemove );
+        }
+
+        private Point2D findClosestOpenLocation( Point2D comparisonPt ) {
+            if ( openShellLocations.size() == 0 ) {
+                return null;
+            }
+
+            Point2D closestPoint = null;
+            for ( Point2D candidatePoint : openShellLocations ) {
+                if ( closestPoint == null || closestPoint.distance( comparisonPt ) > candidatePoint.distance( comparisonPt ) ) {
+                    // Candidate point is better.
+                    closestPoint = candidatePoint;
+                }
+            }
+            return closestPoint;
+        }
     }
 }
