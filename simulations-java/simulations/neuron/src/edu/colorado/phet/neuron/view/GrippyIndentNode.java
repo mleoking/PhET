84f9bab45ff8924304e0e01d03e74a8e32e1b6f6
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-10-01 19:27:43 +0000

    Added intents to the chart cursor in an effort to convey "grippability".  This version has 6 indents, but doesn't look that good, so I am archiving but plan to change it shortly.

---
+++ simulations-java/simulations/neuron/src/edu/colorado/phet/neuron/view/GrippyIndentNode.java
@@ -0,0 +1,40 @@
+/* Copyright 2010, University of Colorado */
+
+package edu.colorado.phet.neuron.view;
+
+import java.awt.Color;
+import java.awt.geom.Ellipse2D;
+
+import edu.colorado.phet.common.phetcommon.view.util.ColorUtils;
+import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
+import edu.umd.cs.piccolo.PNode;
+
+/**
+ * This node is meant to portray a small round indentation on a surface.  This
+ * is a modern user interface paradigm that is intended to convey the concept
+ * of "gripability" (sp?), i.e. something that the user can click on and
+ * subsequently grab.  This is meant to look somewhat 3D, much like etched
+ * borders do.
+ *
+ * @author John Blanco
+ */
+public class GrippyIndentNode extends PNode {
+
+    /**
+     * Constructor.  The base color should be the color of the item on
+     * which this indent is being placed, and it will
+     * Constructor.
+     */
+    public GrippyIndentNode( double diameter, Color baseColor ) {
+        Color baseDarkerColor = ColorUtils.darkerColor( baseColor, 0.5 );
+        Color translucentDarkerColor = new Color( baseDarkerColor.getRed(), baseDarkerColor.getGreen(),
+                baseDarkerColor.getBlue(), baseColor.getAlpha() );
+        Color baseLighterColor = ColorUtils.brighterColor( baseColor, 0.5 );
+        Color translucentBrighterColor = new Color( baseLighterColor.getRed(), baseLighterColor.getGreen(),
+                baseLighterColor.getBlue(), baseColor.getAlpha() );
+        addChild( new PhetPPath( new Ellipse2D.Double( -diameter / 2, -diameter / 2, diameter, diameter ),
+                translucentBrighterColor ) );
+        addChild( new PhetPPath( new Ellipse2D.Double( -diameter / 2, -diameter / 2, diameter * 0.8, diameter * 0.8 ),
+                translucentDarkerColor ) );
+    }
+}
