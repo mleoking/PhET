50b321ad6fa9225f1633fef9cf7e6841c70c11fa
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-10-04 19:53:37 +0000

    increase max field lines to 900, per Noah P

--- trunk/simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/CLConstants.java
+++ trunk/simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/CLConstants.java
@@ -100,5 +100,5 @@ public class CLConstants {
     // E-field
     public static final boolean EFIELD_VISIBLE = false;
     public static final DoubleRange EFIELD_SPACING_RANGE = new DoubleRange( PLATE_SIZE_RANGE.getMin() / 15, PLATE_SIZE_RANGE.getMin() / 2 ); // meters
-    public static final IntegerRange NUMBER_OF_EFIELD_LINES = new IntegerRange( 4, 625 ); // number of lines on smallest plate
+    public static final IntegerRange NUMBER_OF_EFIELD_LINES = new IntegerRange( 4, 900 ); // number of lines on smallest plate
 }
