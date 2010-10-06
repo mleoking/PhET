faeeac9c6744a9e6c69aa4127726cf1b11e90617
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-10-06 23:16:14 +0000

    Added layout & font for symbol.

--- trunk/simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/view/SymbolIndicatorNode.java
+++ trunk/simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/view/SymbolIndicatorNode.java
@@ -1,10 +1,13 @@
 package edu.colorado.phet.buildanatom.view;
 
-import java.awt.*;
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.geom.Rectangle2D;
 
 import edu.colorado.phet.buildanatom.model.Atom;
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
+import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.nodes.PText;
@@ -14,17 +17,37 @@ import edu.umd.cs.piccolo.nodes.PText;
  * @author Sam Reid
  */
 public class SymbolIndicatorNode extends PNode {
+
+    public static final Font SYMBOL_FONT = new PhetFont( 28, true );
+    public static final Font NUMBER_FONT = new PhetFont( 14, true );
+
     private final Atom atom;
+    private final PText symbol;
+    private PNode boundingBox;
 
     public SymbolIndicatorNode( final Atom atom, double width, double height ) {
         this.atom = atom;
-        addChild( new PhetPPath( new Rectangle2D.Double( 0, 0, width, height ), Color.white, new BasicStroke( 1 ), Color.black ) );
-        addChild( new PText( atom.getSymbol() ) {{
-            atom.addObserver( new SimpleObserver() {
-                public void update() {
-                    setText( atom.getSymbol() );
-                }
-            } );
-        }} );
+        atom.addObserver( new SimpleObserver() {
+            public void update() {
+                updateSymbol();
+            }
+        } );
+
+        boundingBox = new PhetPPath( new Rectangle2D.Double( 0, 0, width, height ), Color.white,
+                new BasicStroke( 1 ), Color.black );
+        addChild( boundingBox );
+
+        // Textual symbol.
+        symbol = new PText( atom.getSymbol() );
+        symbol.setFont( SYMBOL_FONT );
+        addChild( symbol );
+
+        updateSymbol();
+    }
+
+    private void updateSymbol(){
+        symbol.setText( atom.getSymbol() );
+        symbol.setOffset( boundingBox.getFullBoundsReference().getCenterX() - symbol.getFullBoundsReference().width / 2,
+                boundingBox.getFullBoundsReference().getCenterY() - symbol.getFullBoundsReference().height / 2 );
     }
 }
