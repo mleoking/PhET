7f687e52189fcfa3fb403211a9569a39af59efaf
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-10-06 23:41:27 +0000

    Added initial cut at the proton number.

--- trunk/simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/view/SymbolIndicatorNode.java
+++ trunk/simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/view/SymbolIndicatorNode.java
@@ -19,11 +19,12 @@ import edu.umd.cs.piccolo.nodes.PText;
 public class SymbolIndicatorNode extends PNode {
 
     public static final Font SYMBOL_FONT = new PhetFont( 28, true );
-    public static final Font NUMBER_FONT = new PhetFont( 14, true );
+    public static final Font NUMBER_FONT = new PhetFont( 20, true );
 
     private final Atom atom;
     private final PText symbol;
-    private PNode boundingBox;
+    private final PText protonCount;
+    private final PNode boundingBox;
 
     public SymbolIndicatorNode( final Atom atom, double width, double height ) {
         this.atom = atom;
@@ -38,9 +39,15 @@ public class SymbolIndicatorNode extends PNode {
         addChild( boundingBox );
 
         // Textual symbol.
-        symbol = new PText( atom.getSymbol() );
+        symbol = new PText();
         symbol.setFont( SYMBOL_FONT );
         addChild( symbol );
+
+        // Proton number.
+        protonCount = new PText();
+        protonCount.setFont( NUMBER_FONT );
+        protonCount.setTextPaint( Color.RED );
+        addChild( protonCount );
 
         updateSymbol();
     }
@@ -49,5 +56,8 @@ public class SymbolIndicatorNode extends PNode {
         symbol.setText( atom.getSymbol() );
         symbol.setOffset( boundingBox.getFullBoundsReference().getCenterX() - symbol.getFullBoundsReference().width / 2,
                 boundingBox.getFullBoundsReference().getCenterY() - symbol.getFullBoundsReference().height / 2 );
+        protonCount.setText( "" + atom.getNumProtons() );
+        protonCount.setOffset( symbol.getFullBoundsReference().getMinX() - protonCount.getFullBoundsReference().width,
+                symbol.getFullBoundsReference().getMaxY() );
     }
 }
