3bb008b1c47e0db7bdcf25999039515d3eb2debc
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-10-07 16:55:33 +0000

    delete unused box coloring strategies, since we seem to have settled on this

--- simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/view/IBoxColorStrategy.java
+++ simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/view/IBoxColorStrategy.java
@@ -18,43 +18,12 @@ public interface IBoxColorStrategy {
 
     public Color getSideColor( Color baseColor );
     
-    abstract static class AbstractColorStrategy implements IBoxColorStrategy {
-        /*
-         * Color.darker doesn't preserve alpha, so we need our own method.
-         * Notes that this results in the allocation of 2 Colors,
-         * but that's not an issue for this sim.
-         */
-        protected Color darker( Color color ) {
-            Color c = color.darker();
-            return new Color( c.getRed(), c.getGreen(), c.getBlue(), color.getAlpha() );
-        }
-    }
-
-    /**
-     * Top and front use the base color.
-     * Side uses a darker shade of the front color.
-     */
-    public static class TwoColorStrategy extends AbstractColorStrategy {
-
-        public Color getTopColor( Color baseColor ) {
-            return baseColor;
-        }
-
-        public Color getFrontColor( Color baseColor ) {
-            return getTopColor( baseColor );
-        }
-
-        public Color getSideColor( Color baseColor ) {
-            return darker( getFrontColor( baseColor ) );
-        }
-    }
-
     /**
      * Top uses the base color.
      * Front uses a darker shade of the top color.
      * Side uses a darker shade of the front color.
      */
-    public static class ThreeColorStrategy extends AbstractColorStrategy {
+    public static class ThreeColorStrategy implements IBoxColorStrategy {
         
         public Color getTopColor( Color baseColor ) {
             return baseColor;
@@ -66,6 +35,16 @@ public interface IBoxColorStrategy {
 
         public Color getSideColor( Color baseColor ) {
             return darker( getFrontColor( baseColor ) );
+        }
+        
+        /*
+         * Color.darker doesn't preserve alpha, so we need our own method.
+         * Notes that this results in the allocation of 2 Colors,
+         * but that's not an issue for this sim.
+         */
+        protected Color darker( Color color ) {
+            Color c = color.darker();
+            return new Color( c.getRed(), c.getGreen(), c.getBlue(), color.getAlpha() );
         }
     }
 }
