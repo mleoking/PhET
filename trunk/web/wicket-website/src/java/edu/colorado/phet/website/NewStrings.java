71c43c05861ad45fbc8c8322509c77b189fb0025
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-10-07 05:08:26 +0000

    website: i18n of front page changes

--- trunk/web/wicket-website/src/java/edu/colorado/phet/website/NewStrings.java
+++ trunk/web/wicket-website/src/java/edu/colorado/phet/website/NewStrings.java
@@ -22,6 +22,9 @@ public class NewStrings {
 //        checkString( session, "error.pageNotFound.message", "Please contact us at {0} for assistance or to report the error." );
         checkString( session, "troubleshooting.main.q14.title", "Why can’t I completely uninstall the PhET sims that I downloaded from my Windows computer?" );
         checkString( session, "troubleshooting.main.q14.answer", "<p>When a Java Web Start simulation is run in Windows, it is added to the list of programs in Control Panel -> Add or Remove Programs.  Due to a problem in Java Web Start, sometimes the item may remain in the list even after the simulation has been removed, and Windows may report \"Unable to completely uninstall application\".  More information about this issue can be found at {0}, and the producers of Java have acknowledged the problem and reported that they plan to fix it in an upcoming version of Java.</p>" );
+        checkString( session, "home.facebookText", "Join us on {0}" );
+        checkString( session, "home.twitterText", "Follow us on  {0}" );
+        checkString( session, "home.blogText", "Read our blog" );
         session.close();
     }
 
