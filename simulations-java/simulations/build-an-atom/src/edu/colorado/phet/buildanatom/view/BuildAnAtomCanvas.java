0619130f613913c4da48d696f7d88f2ec6ec4a60
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-10-07 15:51:38 +0000

    Format and comment cleanup.

--- simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/view/BuildAnAtomCanvas.java
+++ simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/view/BuildAnAtomCanvas.java
@@ -53,7 +53,7 @@ public class BuildAnAtomCanvas extends PhetPCanvas {
 
     // Stroke for drawing the electron shells.
     public static final Stroke ELECTRON_SHELL_STROKE = new BasicStroke( 2f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0,
-            new float[] {3,3}, 0 );
+            new float[] { 3, 3 }, 0 );
 
     // Reset button.
     private final GradientButtonNode resetButtonNode;
@@ -69,9 +69,9 @@ public class BuildAnAtomCanvas extends PhetPCanvas {
         // Set up the canvas-screen transform.
         setWorldTransformStrategy( new CenteredStage( this, BuildAnAtomDefaults.STAGE_SIZE ) );
 
-//        mvt = new ModelViewTransform2D( model.getModelViewport(),
-//                new Rectangle2D.Double( 0, BuildAnAtomDefaults.STAGE_SIZE.getHeight() * ( 1 - 0.8 ),
-//                BuildAnAtomDefaults.STAGE_SIZE.getWidth() * 0.7, BuildAnAtomDefaults.STAGE_SIZE.getHeight() * 0.7 ) );
+        //        mvt = new ModelViewTransform2D( model.getModelViewport(),
+        //                new Rectangle2D.Double( 0, BuildAnAtomDefaults.STAGE_SIZE.getHeight() * ( 1 - 0.8 ),
+        //                BuildAnAtomDefaults.STAGE_SIZE.getWidth() * 0.7, BuildAnAtomDefaults.STAGE_SIZE.getHeight() * 0.7 ) );
 
         // Set up the model-canvas transform.  IMPORTANT NOTES: The multiplier
         // factors for the point in the view can be adjusted to shift the
@@ -95,16 +95,16 @@ public class BuildAnAtomCanvas extends PhetPCanvas {
         rootNode.addChild( frontLayer );
 
         // Put up the bounds of the model.
-//        rootNode.addChild( new PhetPPath( mvt.createTransformedShape( model.getModelViewport() ), Color.PINK, new BasicStroke( 3f ), Color.BLACK ) );
+        //        rootNode.addChild( new PhetPPath( mvt.createTransformedShape( model.getModelViewport() ), Color.PINK, new BasicStroke( 3f ), Color.BLACK ) );
 
         // Add the atom's nucleus location to the canvas.
-//        Shape nucleusOutlineShape = mvt.createTransformedShape( new Ellipse2D.Double(
-//                -model.getAtom().getNucleusRadius(),
-//                -model.getAtom().getNucleusRadius(),
-//                model.getAtom().getNucleusRadius() * 2,
-//                model.getAtom().getNucleusRadius() * 2 ) );
-//        PNode nucleusOutlineNode = new PhetPPath( nucleusOutlineShape, new BasicStroke(1f), Color.RED );
-//        backLayer.addChild( nucleusOutlineNode );
+        //        Shape nucleusOutlineShape = mvt.createTransformedShape( new Ellipse2D.Double(
+        //                -model.getAtom().getNucleusRadius(),
+        //                -model.getAtom().getNucleusRadius(),
+        //                model.getAtom().getNucleusRadius() * 2,
+        //                model.getAtom().getNucleusRadius() * 2 ) );
+        //        PNode nucleusOutlineNode = new PhetPPath( nucleusOutlineShape, new BasicStroke(1f), Color.RED );
+        //        backLayer.addChild( nucleusOutlineNode );
         DoubleGeneralPath nucleusXMarkerModelCoords = new DoubleGeneralPath();
         double xMarkerSize = Proton.RADIUS; // Arbitrary, adjust as desired.
         nucleusXMarkerModelCoords.moveTo( model.getAtom().getPosition().getX() - xMarkerSize / 2,
@@ -116,16 +116,16 @@ public class BuildAnAtomCanvas extends PhetPCanvas {
         nucleusXMarkerModelCoords.lineTo( model.getAtom().getPosition().getX() + xMarkerSize / 2,
                 model.getAtom().getPosition().getY() - xMarkerSize / 2 );
         Shape nucleusXMarkerShape = mvt.createTransformedShape( nucleusXMarkerModelCoords.getGeneralPath() );
-        PNode nucleusXMarkerNode = new PhetPPath( nucleusXMarkerShape, new BasicStroke(4f), new Color(255, 0, 0, 75) );
+        PNode nucleusXMarkerNode = new PhetPPath( nucleusXMarkerShape, new BasicStroke( 4f ), new Color( 255, 0, 0, 75 ) );
         backLayer.addChild( nucleusXMarkerNode );
 
         // Add the atom's electron shells to the canvas.
-        for (Double shellRadius : model.getAtom().getElectronShellRadii()){
+        for ( Double shellRadius : model.getAtom().getElectronShellRadii() ) {
             Shape electronShellShape = mvt.createTransformedShape( new Ellipse2D.Double(
                     -shellRadius,
                     -shellRadius,
                     shellRadius * 2,
-                    shellRadius * 2));
+                    shellRadius * 2 ) );
             PNode electronShellNode = new PhetPPath( electronShellShape, ELECTRON_SHELL_STROKE, Color.BLUE );
             electronShellNode.setOffset( model.getAtom().getPosition() );
             backLayer.addChild( electronShellNode );
@@ -159,66 +159,66 @@ public class BuildAnAtomCanvas extends PhetPCanvas {
             }
         }
 
-        // Add the button for resetting the nucleus to the canvas.
-        // TODO: i18n
-        resetButtonNode = new GradientButtonNode("Reset All", 16, new Color(255, 153, 0));
-        double desiredResetButtonWidth = 100;
-        resetButtonNode.setScale(desiredResetButtonWidth / resetButtonNode.getFullBoundsReference().width);
-        rootNode.addChild(resetButtonNode);
-        resetButtonNode.addActionListener( new ActionListener() {
-            public void actionPerformed( ActionEvent e ) {
-                BuildAnAtomCanvas.this.model.reset();
-            }
-        });
-
-        //Show whether the nucleus is stable
+        // Show whether the nucleus is stable.
         StabilityIndicator stabilityIndicator = new StabilityIndicator( model.getAtom() );
-        //Position the stability indicator under the nucleus
+        // Position the stability indicator under the nucleus
         stabilityIndicator.setOffset( mvt.modelToViewX( 0 ) - stabilityIndicator.getFullBounds().getWidth() / 2, mvt.modelToViewY( -Atom.ELECTRON_SHELL_1_RADIUS * 3.0 / 4.0 ) - stabilityIndicator.getFullBounds().getHeight() );
         rootNode.addChild( stabilityIndicator );
 
-        //Show the legend / particle count indicater in the top left
-        ParticleCountLegend particleCountLegend= new ParticleCountLegend(model.getAtom());
-        particleCountLegend.setOffset(20,20);//top left corner, but with some padding
+        // Show the legend/particle count indicator in the top left.
+        ParticleCountLegend particleCountLegend = new ParticleCountLegend( model.getAtom() );
+        particleCountLegend.setOffset( 20, 20 );//top left corner, but with some padding
         rootNode.addChild( particleCountLegend );
 
         PDimension windowSize = new PDimension( 300, 100 );//for the 3 lower windows
         double verticalSpacingBetweenWindows = 20;
         int WINDOW_X = 700;
 
-        //Element indicator
-        PDimension elementIndicatorNodeWindowSize = new PDimension( 400,250-verticalSpacingBetweenWindows*2);
-        ElementIndicatorNode elementIndicatorNode= new ElementIndicatorNode( model.getAtom() );
+        // Element indicator
+        PDimension elementIndicatorNodeWindowSize = new PDimension( 400, 250 - verticalSpacingBetweenWindows * 2 );
+        ElementIndicatorNode elementIndicatorNode = new ElementIndicatorNode( model.getAtom() );
         PNode elementIndicatorWindow = new MaximizeControlNode( "element", elementIndicatorNodeWindowSize, elementIndicatorNode, true );
-        elementIndicatorNode.setOffset( elementIndicatorNodeWindowSize.width/2 - elementIndicatorNode.getFullBounds().getWidth()/2, elementIndicatorNodeWindowSize.getHeight()/2-elementIndicatorNode.getFullBounds().getHeight()/2);
-        elementIndicatorWindow.setOffset( WINDOW_X-100, verticalSpacingBetweenWindows);
-        elementIndicatorNode.translate( 0,10 );//fudge factor since centering wasn't quite right
+        elementIndicatorNode.setOffset( elementIndicatorNodeWindowSize.width / 2 - elementIndicatorNode.getFullBounds().getWidth() / 2, elementIndicatorNodeWindowSize.getHeight() / 2 - elementIndicatorNode.getFullBounds().getHeight() / 2 );
+        elementIndicatorWindow.setOffset( WINDOW_X - 100, verticalSpacingBetweenWindows );
+        elementIndicatorNode.translate( 0, 10 );//fudge factor since centering wasn't quite right
         rootNode.addChild( elementIndicatorWindow );
 
-        // Add the symbol indicator contained within a min/max node.
-        SymbolIndicatorNode symbolNode = new SymbolIndicatorNode( model.getAtom(), 83,83);//has to be big enough to hold Ne with 2 digit numbers on both sides
+        // Symbol indicator
+        SymbolIndicatorNode symbolNode = new SymbolIndicatorNode( model.getAtom(), 83, 83 );//has to be big enough to hold Ne with 2 digit numbers on both sides
         PNode symbolWindow = new MaximizeControlNode( "symbol", windowSize, symbolNode, true );
         //PDebug.debugBounds = true;//helps get the layout and bounds correct
         double insetX = 20;
         symbolNode.setOffset( windowSize.width - symbolNode.getFullBounds().getWidth() - insetX, windowSize.height / 2 - symbolNode.getFullBounds().getHeight() / 2 );
-        symbolWindow.setOffset( WINDOW_X, 250);
+        symbolWindow.setOffset( WINDOW_X, 250 );
         rootNode.addChild( symbolWindow );
 
-        //Mass indicator
-        MassIndicatorNode massIndicatorNode = new MassIndicatorNode(model.getAtom() );
+        // Mass indicator
+        MassIndicatorNode massIndicatorNode = new MassIndicatorNode( model.getAtom() );
         PNode massWindow = new MaximizeControlNode( "mass", windowSize, massIndicatorNode, true );
-        massIndicatorNode.setOffset( windowSize.width-massIndicatorNode.getFullBounds().getWidth()-insetX,windowSize.height/2 );//todo: the layout centers the scale node since the scale's origin is it's top left
-        massWindow.setOffset( WINDOW_X, symbolWindow.getFullBounds().getMaxY()+verticalSpacingBetweenWindows);
-        rootNode.addChild( massWindow);
+        massIndicatorNode.setOffset( windowSize.width - massIndicatorNode.getFullBounds().getWidth() - insetX, windowSize.height / 2 );//todo: the layout centers the scale node since the scale's origin is its top left
+        massWindow.setOffset( WINDOW_X, symbolWindow.getFullBounds().getMaxY() + verticalSpacingBetweenWindows );
+        rootNode.addChild( massWindow );
 
-        //Charge indicator
+        // Charge indicator
         ChargeIndicatorNode chargeIndicatorNode = new ChargeIndicatorNode( model.getAtom() );
         PNode chargeWindow = new MaximizeControlNode( "charge", windowSize, chargeIndicatorNode, true );
         chargeIndicatorNode.setOffset( windowSize.width - chargeIndicatorNode.getBoxWidth() - insetX, windowSize.height / 2 - chargeIndicatorNode.getFullBounds().getHeight() / 2 );
-        chargeWindow.setOffset( WINDOW_X, massWindow.getFullBounds().getMaxY()+verticalSpacingBetweenWindows);
+        chargeWindow.setOffset( WINDOW_X, massWindow.getFullBounds().getMaxY() + verticalSpacingBetweenWindows );
         rootNode.addChild( chargeWindow );
 
-        resetButtonNode.setOffset(chargeWindow.getFullBounds().getCenterX()-resetButtonNode.getFullBounds().getWidth()/2,chargeWindow.getFullBounds().getMaxY()+verticalSpacingBetweenWindows);
+        // Add the "Reset All" button.
+        resetButtonNode = new GradientButtonNode( "Reset All", 16, new Color( 255, 153, 0 ) );
+        double desiredResetButtonWidth = 100;
+        resetButtonNode.setScale( desiredResetButtonWidth / resetButtonNode.getFullBoundsReference().width );
+        rootNode.addChild( resetButtonNode );
+        resetButtonNode.addActionListener( new ActionListener() {
+            public void actionPerformed( ActionEvent e ) {
+                BuildAnAtomCanvas.this.model.reset();
+            }
+        } );
+        resetButtonNode.setOffset(
+                chargeWindow.getFullBounds().getCenterX() - resetButtonNode.getFullBounds().getWidth() / 2,
+                chargeWindow.getFullBounds().getMaxY() + verticalSpacingBetweenWindows );
     }
 
 
