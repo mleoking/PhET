2843560693531a0470af0567c8893fb4d8b712ba
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-10-08 21:23:21 +0000

    Cleaned up gradient stuff, added some initial gradients that look somewhat reasonable.

--- simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/view/ElectronShellNode.java
+++ simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/view/ElectronShellNode.java
@@ -13,65 +13,71 @@ import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.umd.cs.piccolo.PNode;
 
 /**
+ * Node that represents an electron shell, aka "orbit", in the view.  This
+ * node is able to switch between different representations of a shell.
+ *
  * @author Sam Reid
+ * @author John Blanco
  */
 public class ElectronShellNode extends PNode {
 
     // Color Test Set 03 - Blue background with gray atoms
-    private static final Color BACKGROUND_COLOR = new Color( 185, 205, 229 );
-    private static final Color INNER_SHELL_BASE_COLOR = Color.DARK_GRAY;
-    private static final Color INNER_SHELL_FADE_TO_COLOR = BACKGROUND_COLOR;
-    private static final Color OUTER_SHELL_BASE_COLOR = Color.DARK_GRAY;
-    private static final Color OUTER_SHELL_FADE_TO_COLOR = BACKGROUND_COLOR;
+//    private static final Color CENTER_COLOR = Color.DARK_GRAY;
+//    private static final Color OUTER_COLOR = new Color( 185, 205, 100 );
+    private static final Color CENTER_COLOR = new Color(0, 0, 255, 100);
+    private static final Color OUTER_COLOR = new Color(0, 0, 255, 0);
 
-    private static final Color OUTER_SHELL_BASE_COLOR_4_ELECTRONS = new Color(
-            OUTER_SHELL_BASE_COLOR.getRed(),
-            OUTER_SHELL_BASE_COLOR.getGreen(),
-            OUTER_SHELL_BASE_COLOR.getBlue(),
-            200);
-    private static final Color OUTER_SHELL_FADE_TO_COLOR_4_ELECTRONS = new Color(
-            OUTER_SHELL_FADE_TO_COLOR.getRed(),
-            OUTER_SHELL_FADE_TO_COLOR.getGreen(),
-            OUTER_SHELL_FADE_TO_COLOR.getBlue(),
-            100);
+    // Stroke for drawing the electron orbits.
+    private static final Stroke ELECTRON_SHELL_STROKE = new BasicStroke( 2f, BasicStroke.CAP_BUTT,
+            BasicStroke.JOIN_BEVEL, 0, new float[] { 3, 3 }, 0 );
 
-    // Stroke for drawing the electron shells.
-    private static final Stroke ELECTRON_SHELL_STROKE = new BasicStroke( 2f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0,
-                                                                         new float[] { 3, 3 }, 0 );
+    // Paint for the electron orbitals.
     private static final Paint ELECTRON_SHELL_STROKE_PAINT = new Color( 0, 0, 255, 100 );
 
+    /**
+     * Constructor.
+     */
     public ElectronShellNode( final ModelViewTransform2D mvt, final MutableBoolean viewOrbitals, final Atom atom, Double shellRadius ) {
+
         Shape electronShellShape = mvt.createTransformedShape( new Ellipse2D.Double(
                 -shellRadius,
                 -shellRadius,
                 shellRadius * 2,
                 shellRadius * 2 ) );
-        final PNode electronShellNode = new PhetPPath( electronShellShape, ELECTRON_SHELL_STROKE, ELECTRON_SHELL_STROKE_PAINT ) {{
-            setOffset( atom.getPosition() );
-            final SimpleObserver updateVisibility = new SimpleObserver() {
-                public void update() {
-                    setVisible( viewOrbitals.getValue() );
-                }
-            };
-            viewOrbitals.addObserver( updateVisibility );
-        }};
-        addChild( electronShellNode );
 
-//        Paint outerShell4ElectronsPaint = new RoundGradientPaint( 0, 0, OUTER_SHELL_BASE_COLOR_4_ELECTRONS,
-//                new Point2D.Double( LARGE_SHELL_RADIUS * 0.75, LARGE_SHELL_RADIUS * 0.75), OUTER_SHELL_FADE_TO_COLOR_4_ELECTRONS );
-        Paint outerShell4ElectronsPaint = new RoundGradientPaint( electronShellShape.getBounds2D().getCenterX(), electronShellShape.getBounds2D().getCenterY(),
-                                                                  OUTER_SHELL_BASE_COLOR_4_ELECTRONS,
-                                                                  new Point2D.Double( electronShellShape.getBounds2D().getWidth()/2, electronShellShape.getBounds2D().getHeight()/2 ),
-                                                                  OUTER_SHELL_FADE_TO_COLOR_4_ELECTRONS);
-        PhetPPath fuzzyView = new PhetPPath( electronShellShape,outerShell4ElectronsPaint){{
-            final SimpleObserver updateVisibility = new SimpleObserver() {
-                public void update() {
-                    setVisible( !viewOrbitals.getValue() );
-                }
-            };
-            viewOrbitals.addObserver( updateVisibility );
-        }};
+        // Create and add the node that will depict the shell as a circular
+        // orbit.
+        final PNode electronOrbitNode = new PhetPPath( electronShellShape, ELECTRON_SHELL_STROKE, ELECTRON_SHELL_STROKE_PAINT ) { {
+                setOffset( atom.getPosition() );
+                final SimpleObserver updateVisibility = new SimpleObserver() {
+                    public void update() {
+                        setVisible( viewOrbitals.getValue() );
+                    }
+                };
+                viewOrbitals.addObserver( updateVisibility );
+            } };
+        addChild( electronOrbitNode );
+
+        // Create and add the nodes that will be used when depicting the
+        // electrons as a fuzzy cloud.
+        Paint outerShell4ElectronsPaint = new RoundGradientPaint(
+                electronShellShape.getBounds2D().getCenterX(),
+                electronShellShape.getBounds2D().getCenterY(),
+                CENTER_COLOR,
+//                new Point2D.Double( electronShellShape.getBounds2D().getWidth() / 2, electronShellShape.getBounds2D().getHeight() / 2 ),
+                new Point2D.Double( electronShellShape.getBounds2D().getWidth() / 3, electronShellShape.getBounds2D().getHeight() / 3 ),
+                OUTER_COLOR );
+        PhetPPath electronCloudNode = new PhetPPath( electronShellShape, outerShell4ElectronsPaint ) {
+            {
+                final SimpleObserver updateVisibility = new SimpleObserver() {
+                    public void update() {
+                        setVisible( !viewOrbitals.getValue() );
+                    }
+                };
+                viewOrbitals.addObserver( updateVisibility );
+            }
+        };
         viewOrbitals.setValue( false );//XXX
-        addChild( fuzzyView );
+        addChild( electronCloudNode );
     }
 }
