395900514f20c38153b0d062a639e770b7a8c0a7
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-10-08 17:26:41 +0000

    Integrated new graphic for the mass scale.

--- trunk/simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/view/MassIndicatorNode.java
+++ trunk/simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/view/MassIndicatorNode.java
@@ -5,21 +5,25 @@ import java.awt.geom.Ellipse2D;
 import java.awt.geom.Rectangle2D;
 
 import edu.colorado.phet.buildanatom.BuildAnAtomConstants;
+import edu.colorado.phet.buildanatom.BuildAnAtomResources;
 import edu.colorado.phet.buildanatom.model.Atom;
 import edu.colorado.phet.common.phetcommon.math.Function;
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform2D;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.nodes.PImage;
 import edu.umd.cs.piccolo.nodes.PText;
 
 /**
  * Shows a scale with a numeric readout of the atom weight.  Origin is the top left of the scale body (not the platform).
  *
  * @author Sam Reid
+ * @author John Blanco
  */
 public class MassIndicatorNode extends PNode {
-    private Atom atom;
+    private static final double WIDTH = 90; // The image will define the aspect ratio and therefore the height.
+    private final Atom atom;
 
     public MassIndicatorNode( final Atom atom ) {
         this.atom=atom;
@@ -27,14 +31,18 @@ public class MassIndicatorNode extends PNode {
         final double stemWidth = 4;
         final double platformHeight = 3;
 
-        final PhetPPath base = new PhetPPath( baseShape, BuildAnAtomConstants.READOUT_BACKGROUND_COLOR, new BasicStroke( 1 ), Color.black );
-        addChild( base );
+//        final PhetPPath base = new PhetPPath( baseShape, BuildAnAtomConstants.READOUT_BACKGROUND_COLOR, new BasicStroke( 1 ), Color.black );
+//        addChild( base );
+//
+//        final PhetPPath stem = new PhetPPath( BuildAnAtomConstants.READOUT_BACKGROUND_COLOR, new BasicStroke( 1 ), Color.black );
+//        addChild( stem );
+//
+//        final PhetPPath platformNode = new PhetPPath( BuildAnAtomConstants.READOUT_BACKGROUND_COLOR, new BasicStroke( 1 ), Color.black );
+//        addChild( platformNode );
 
-        final PhetPPath stem = new PhetPPath( BuildAnAtomConstants.READOUT_BACKGROUND_COLOR, new BasicStroke( 1 ), Color.black );
-        addChild( stem );
-
-        final PhetPPath platformNode = new PhetPPath( BuildAnAtomConstants.READOUT_BACKGROUND_COLOR, new BasicStroke( 1 ), Color.black );
-        addChild( platformNode );
+        final PImage weighScaleImageNode = new PImage( BuildAnAtomResources.getImage( "atom_builder_scale.png" ) );
+        weighScaleImageNode.setScale( WIDTH / weighScaleImageNode.getFullBoundsReference().width );
+        addChild ( weighScaleImageNode );
 
         final PText readoutPText = new PText() {{
             setFont( BuildAnAtomConstants.READOUT_FONT );
@@ -45,7 +53,9 @@ public class MassIndicatorNode extends PNode {
         SimpleObserver updateText = new SimpleObserver() {
             public void update() {
                 readoutPText.setText( atom.getAtomicMassNumber() + "" );
-                readoutPText.setOffset( base.getFullBounds().getCenterX() - readoutPText.getFullBounds().getWidth() / 2, base.getFullBounds().getCenterY() - readoutPText.getFullBounds().getHeight() / 2 );
+//                readoutPText.setOffset( base.getFullBounds().getCenterX() - readoutPText.getFullBounds().getWidth() / 2, base.getFullBounds().getCenterY() - readoutPText.getFullBounds().getHeight() / 2 );
+                readoutPText.setOffset( weighScaleImageNode.getFullBounds().getCenterX() - readoutPText.getFullBounds().getWidth() / 2, weighScaleImageNode.getFullBounds().getMaxY() - readoutPText.getFullBounds().getHeight() );
+//                readoutPText.setOffset( weighScaleImageNode.getFullBounds().getCenterX() - readoutPText.getFullBounds().getWidth() / 2, weighScaleImageNode.getFullBounds().getMaxY() );
             }
         };
         atom.addObserver( updateText );
@@ -89,19 +99,24 @@ public class MassIndicatorNode extends PNode {
         }} );
         addChild( atomNode );
 
+        // Position the atom.  This has a "tweak factor" in it for adjusting
+        // the vertical position.
+        atomNode.setOffset( weighScaleImageNode.getFullBoundsReference().getCenterX(),
+                weighScaleImageNode.getFullBoundsReference().getMinY() - 5);
+
         //have the scale compress with increased weight
-        final SimpleObserver updateCompression = new SimpleObserver() {
-            public void update() {
-                Rectangle2D.Double stemShape = new Rectangle2D.Double( baseShape.getCenterX() - stemWidth / 2, baseShape.getMinY() - getStemHeight(), stemWidth, getStemHeight() );
-                stem.setPathTo( stemShape );
-                Rectangle.Double platform = new Rectangle2D.Double( baseShape.getX(), stemShape.getY() - platformHeight, baseShape.getWidth(), platformHeight );
-                platformNode.setPathTo( platform );
-                atomNode.setOffset( platform.getCenterX(), platform.getCenterY()-atomNode.getFullBounds().getHeight()/2 //set the atom on the scale
-                                                   - 4 );//looks weird if shell sits on the scale, so have it float a little
-            }
-        };
-        atom.addObserver( updateCompression );
-        updateCompression.update();
+//        final SimpleObserver updateCompression = new SimpleObserver() {
+//            public void update() {
+//                Rectangle2D.Double stemShape = new Rectangle2D.Double( baseShape.getCenterX() - stemWidth / 2, baseShape.getMinY() - getStemHeight(), stemWidth, getStemHeight() );
+//                stem.setPathTo( stemShape );
+//                Rectangle.Double platform = new Rectangle2D.Double( baseShape.getX(), stemShape.getY() - platformHeight, baseShape.getWidth(), platformHeight );
+//                platformNode.setPathTo( platform );
+//                atomNode.setOffset( platform.getCenterX(), platform.getCenterY()-atomNode.getFullBounds().getHeight()/2 //set the atom on the scale
+//                                                   - 4 );//looks weird if shell sits on the scale, so have it float a little
+//            }
+//        };
+//        atom.addObserver( updateCompression );
+//        updateCompression.update();
     }
 
     public double getStemHeight(){
