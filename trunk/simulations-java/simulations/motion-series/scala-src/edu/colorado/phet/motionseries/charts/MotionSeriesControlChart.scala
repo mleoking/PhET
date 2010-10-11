cf46c6b9a16b1b7a2b5e4110a6447bbd96ec04a4
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-10-11 18:44:31 +0000

    Fixed: "Reset All" does not remove the small play/pause button to the left of graph.

--- trunk/simulations-java/simulations/motion-series/scala-src/edu/colorado/phet/motionseries/charts/MotionSeriesControlChart.scala
+++ trunk/simulations-java/simulations/motion-series/scala-src/edu/colorado/phet/motionseries/charts/MotionSeriesControlChart.scala
@@ -235,6 +235,7 @@ abstract class MotionSeriesControlChart(motionSeriesModel: MotionSeriesModel, fo
   val sliderNode = createSliderNode(temporalChart)
   val controlPanel = new PNode {
     val goButtonVisible = new MutableBoolean(false) //go button should become visible when user specifies a force by dragging the slider or typing in the text field
+    motionSeriesModel.addResetListener(()=>goButtonVisible.reset())
     addChild(new PSwing(new SeriesSelectionControl("forces.parallel-title-with-units".translate, gridSize) {
       val editableLabel = new EditableLabel(appliedForceSeries) {
         override def setValueFromText() = {
