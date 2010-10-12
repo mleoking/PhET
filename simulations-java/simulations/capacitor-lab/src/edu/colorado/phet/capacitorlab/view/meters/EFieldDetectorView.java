349c44955f382acdda6401c85932e1839e862b7e
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-10-12 20:09:33 +0000

    add vector values to field detector, wire up to controls

--- simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/view/meters/EFieldDetectorView.java
+++ simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/view/meters/EFieldDetectorView.java
@@ -12,10 +12,13 @@ import java.awt.geom.Rectangle2D;
 import java.awt.geom.RoundRectangle2D;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.text.MessageFormat;
 
 import javax.swing.Box;
 import javax.swing.JCheckBox;
 import javax.swing.JLabel;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import edu.colorado.phet.capacitorlab.CLImages;
 import edu.colorado.phet.capacitorlab.CLPaints;
@@ -28,12 +31,15 @@ import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.PhetPNode;
 import edu.colorado.phet.common.piccolophet.event.BoundedDragHandler;
 import edu.colorado.phet.common.piccolophet.event.CursorHandler;
+import edu.colorado.phet.common.piccolophet.nodes.Vector2DNode;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
 import edu.umd.cs.piccolo.event.PInputEvent;
 import edu.umd.cs.piccolo.nodes.PImage;
 import edu.umd.cs.piccolo.nodes.PPath;
 import edu.umd.cs.piccolo.nodes.PText;
+import edu.umd.cs.piccolo.util.PDimension;
+import edu.umd.cs.piccolox.nodes.PComposite;
 import edu.umd.cs.piccolox.pswing.PSwing;
 
 /**
@@ -57,8 +63,10 @@ public class EFieldDetectorView {
     private static final Color WIRE_COLOR = Color.BLACK;
     private static final Stroke WIRE_STROKE = new BasicStroke( 3f );
         
-    private static final double VECTOR_DISPLAY_WIDTH = 200;
+    private static final PDimension VECTOR_DISPLAY_SIZE = new PDimension( 200, 175 );
     private static final Color VECTOR_DISPLAY_BACKGROUND = Color.WHITE;
+    
+    private static final Font VALUE_FONT = new PhetFont( 14 );
     
     private static final Font CONTROL_FONT = new PhetFont( Font.BOLD, 16 );
     private static final Color CONTROL_COLOR = Color.WHITE;
@@ -79,6 +87,7 @@ public class EFieldDetectorView {
             public void propertyChange( PropertyChangeEvent event ) {
                 if ( event.getPropertyName().equals( PNode.PROPERTY_VISIBLE ) ) {
                     probeNode.setVisible( bodyNode.getVisible() );
+                    wireNode.setVisible( bodyNode.getVisible() );
                 }
                 else if ( event.getPropertyName().equals( PNode.PROPERTY_FULL_BOUNDS ) ) {
                     updateWire();
@@ -97,6 +106,7 @@ public class EFieldDetectorView {
         });
         
         wireNode = new CubicWireNode( WIRE_COLOR, -25, 100 ); //XXX constants
+        wireNode.setPickable( false );
         
         // interactivity
         bodyNode.addInputEventListener( new CursorHandler() );
@@ -136,20 +146,33 @@ public class EFieldDetectorView {
      */
     private static class BodyNode extends PhetPNode {
         
+        private final VectorDisplayNode vectorDisplayNode;
+        
         public BodyNode() {
             
+            // title that appears at the top
             PText titleNode = new PText( CLStrings.TITLE_ELECTRIC_FIELD );
             titleNode.setTextPaint( TITLE_COLOR );
             titleNode.setFont( TITLE_FONT );
             
+            // close button
             PImage closeButtonNode = new PImage( CLImages.CLOSE_BUTTON );
-            addChild( closeButtonNode );
+            closeButtonNode.addInputEventListener( new CursorHandler() );
+            closeButtonNode.addInputEventListener( new PBasicInputEventHandler() {
+                @Override
+                public void mouseReleased( PInputEvent event ) {
+                    BodyNode.this.setVisible( false );
+                }
+            });
             
-            ControlPanel controlPanel = new ControlPanel();
+            // display area for vectors and values
+            vectorDisplayNode = new VectorDisplayNode( VECTOR_DISPLAY_SIZE );
+
+            // controls
+            ControlPanel controlPanel = new ControlPanel( vectorDisplayNode );
             PSwing controlPanelNode = new PSwing( controlPanel );
             
-            VectorDisplayNode vectorDisplayNode = new VectorDisplayNode( VECTOR_DISPLAY_WIDTH, controlPanelNode.getFullBoundsReference().getHeight() );
-
+            // background
             double width = controlPanelNode.getFullBoundsReference().getWidth() + vectorDisplayNode.getFullBoundsReference().getWidth() + ( 2 * BODY_X_MARGIN ) + BODY_X_SPACING;
             double height = titleNode.getFullBoundsReference().getHeight() + BODY_Y_SPACING + Math.max( controlPanelNode.getFullBoundsReference().getHeight(), vectorDisplayNode.getFullBoundsReference().getHeight() ) + ( 2 * BODY_Y_MARGIN );
             PPath backgroundNode = new PPath( new RoundRectangle2D.Double( 0, 0, width, height, BODY_CORNER_RADIUS, BODY_CORNER_RADIUS ) );
@@ -179,16 +202,6 @@ public class EFieldDetectorView {
             x = controlPanelNode.getFullBoundsReference().getMaxX() + BODY_X_SPACING;
             y = controlPanelNode.getYOffset();
             vectorDisplayNode.setOffset( x, y );
-            
-            // interactivity
-            closeButtonNode.addInputEventListener( new CursorHandler() );
-            
-            closeButtonNode.addInputEventListener( new PBasicInputEventHandler() {
-                @Override
-                public void mouseReleased( PInputEvent event ) {
-                    BodyNode.this.setVisible( false );
-                }
-            });
         }
     }
     
@@ -199,7 +212,7 @@ public class EFieldDetectorView {
         
         private final JCheckBox plateCheckBox, dielectricCheckBox, sumCheckBox, showValuesCheckBox;
         
-        public ControlPanel() {
+        public ControlPanel( final VectorDisplayNode vectorDisplayNode ) {
             
             setBackground( BODY_COLOR );
             
@@ -210,18 +223,41 @@ public class EFieldDetectorView {
             plateCheckBox = new JCheckBox( CLStrings.CHECKBOX_PLATE );
             plateCheckBox.setFont( CONTROL_FONT );
             plateCheckBox.setForeground( CLPaints.PLATE_EFIELD_VECTOR );
+            plateCheckBox.setSelected( vectorDisplayNode.isPlateVisible() );
+            plateCheckBox.addChangeListener( new ChangeListener() {
+                public void stateChanged( ChangeEvent event ) {
+                    vectorDisplayNode.setPlateVisible( plateCheckBox.isSelected() );
+                }
+            });
             
             dielectricCheckBox = new JCheckBox( CLStrings.CHECKBOX_DIELECTRIC );
             dielectricCheckBox.setFont( CONTROL_FONT );
             dielectricCheckBox.setForeground( CLPaints.DIELECTRIC_EFIELD_VECTOR );
+            dielectricCheckBox.setSelected( vectorDisplayNode.isDielectricVisible() );
+            dielectricCheckBox.addChangeListener( new ChangeListener() {
+                public void stateChanged( ChangeEvent event ) {
+                    vectorDisplayNode.setDielectricVisible( dielectricCheckBox.isSelected() );
+                }
+            });
             
             sumCheckBox = new JCheckBox( CLStrings.CHECKBOX_SUM );
             sumCheckBox.setFont( CONTROL_FONT );
             sumCheckBox.setForeground( CLPaints.SUM_EFIELD_VECTOR );
+            sumCheckBox.setSelected( vectorDisplayNode.isSumVisible() );
+            sumCheckBox.addChangeListener( new ChangeListener() {
+                public void stateChanged( ChangeEvent event ) {
+                    vectorDisplayNode.setSumVisible( sumCheckBox.isSelected() );
+                }
+            });
             
             showValuesCheckBox = new JCheckBox( CLStrings.CHECKBOX_SHOW_VALUES );
             showValuesCheckBox.setFont( CONTROL_FONT );
             showValuesCheckBox.setForeground( CONTROL_COLOR );
+            showValuesCheckBox.addChangeListener( new ChangeListener() {
+                public void stateChanged( ChangeEvent event ) {
+                    vectorDisplayNode.setValuesVisible( showValuesCheckBox.isSelected() );
+                }
+            });
             
             // layout
             setAnchor( Anchor.WEST );
@@ -231,7 +267,7 @@ public class EFieldDetectorView {
             add( plateCheckBox, row++, column );
             add( dielectricCheckBox, row++, column );
             add( sumCheckBox, row++, column );
-            add( Box.createVerticalStrut( 20 ), row++, column );
+            add( Box.createVerticalStrut( 40 ), row++, column );
             add( showValuesCheckBox, row++, column );
         }
     }
@@ -240,17 +276,129 @@ public class EFieldDetectorView {
      * Rectangular area where the vectors are displayed.
      * Vectors are clipped to this area.
      */
-    
-    private static final class VectorDisplayNode extends PPath {
+    private static final class VectorDisplayNode extends PComposite {
         
-        public VectorDisplayNode( double width, double height ) {
+        private final FieldVectorNode plateVectorNode, dielectricVectorNode, sumVectorNode;
+        private final FieldValueNode plateValueNode, dielectricValueNode, sumValueNode;
+        private boolean valuesVisible;
+        
+        public VectorDisplayNode( PDimension size ) {
             
-            PPath backgroundNode = new PPath( new Rectangle2D.Double( 0, 0, width, height ) );
+            // background
+            PPath backgroundNode = new PPath( new Rectangle2D.Double( 0, 0, size.getWidth(), size.getHeight() ) );
             backgroundNode.setPaint( VECTOR_DISPLAY_BACKGROUND );
             backgroundNode.setStroke( null );
             addChild( backgroundNode );
             
-            //XXX add vector nodes
+            // vectors
+            plateVectorNode = new FieldVectorNode();
+            dielectricVectorNode = new FieldVectorNode();
+            sumVectorNode = new FieldVectorNode();
+            
+            // values
+            plateValueNode = new FieldValueNode();
+            dielectricValueNode = new FieldValueNode();
+            sumValueNode = new FieldValueNode();
+            
+            // rendering order
+//            addChild( plateVectorNode );
+//            addChild( dielectricVectorNode );
+//            addChild( sumVectorNode );
+            addChild( plateValueNode );
+            addChild( dielectricValueNode );
+            addChild( sumValueNode );
+            
+            // layout
+            double x = 0;
+            double y = 0;
+            double xMargin = 5;
+            double yMargin = 5;
+            double ySpacing = 3;
+            backgroundNode.setOffset( x, y );
+            x = backgroundNode.getFullBoundsReference().getMinX() + xMargin;
+            y = backgroundNode.getFullBoundsReference().getMinY() + yMargin;
+            plateValueNode.setOffset( x, y );
+            y = plateValueNode.getFullBoundsReference().getMaxY() + ySpacing;
+            dielectricValueNode.setOffset( x, y );
+            y = dielectricValueNode.getFullBoundsReference().getMaxY() + ySpacing;
+            sumValueNode.setOffset( x, y );
+        }
+        
+        public void setPlateValue( double value ) {
+            plateValueNode.setValue( value );
+        }
+        
+        public void setDielectricValue( double value ) {
+            dielectricValueNode.setValue( value );
+        }
+        
+        public void setSumValue( double value ) {
+            sumValueNode.setValue( value );
+        }
+        
+        public void setPlateVisible( boolean visible ) {
+            plateVectorNode.setVisible( visible );
+            plateValueNode.setVisible( visible && valuesVisible );
+        }
+        
+        public boolean isPlateVisible() {
+            return plateVectorNode.isVisible();
+        }
+        
+        public void setDielectricVisible( boolean visible ) {
+            dielectricVectorNode.setVisible( visible );
+            dielectricValueNode.setVisible( visible && valuesVisible );
+        }
+        
+        public boolean isDielectricVisible() {
+            return dielectricVectorNode.isVisible();
+        }
+        
+        public void setSumVisible( boolean visible ) {
+            sumVectorNode.setVisible( visible );
+            sumValueNode.setVisible( visible && valuesVisible );
+        }
+        
+        public boolean isSumVisible() {
+            return sumVectorNode.isVisible();
+        }
+        
+        public void setValuesVisible( boolean valuesVisible ) {
+            if ( valuesVisible != this.valuesVisible ) {
+                this.valuesVisible = valuesVisible;
+                plateValueNode.setVisible( valuesVisible && plateVectorNode.isVisible() );
+                dielectricValueNode.setVisible( valuesVisible && dielectricVectorNode.isVisible() );
+                sumValueNode.setVisible( valuesVisible && sumVectorNode.isVisible() );
+            }
+        }
+        
+        public boolean isValuesVisible() {
+            return valuesVisible;
+        }
+    }
+    
+    /*
+     * Field vector.
+     */
+    private static class FieldVectorNode extends Vector2DNode {
+        
+        public FieldVectorNode() {
+            super( 0, 0, 1, 1 );//XXX
+        }
+    }
+    
+    /*
+     * Displays a numeric field value.
+     */
+    private static class FieldValueNode extends PText {
+        
+        public FieldValueNode() {
+            setFont( VALUE_FONT );
+            setValue( 0 );
+        }
+        
+        public void setValue( double value ) {
+            setText( MessageFormat.format( CLStrings.PATTERN_VALUE_UNITS, (int)value, CLStrings.UNITS_VOLTS_PER_METER ) );
         }
     }
     
