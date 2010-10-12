6bfb09f9a16e28126685524bf7c89412e6bf9996
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-10-12 15:45:39 +0000

    tweaks

--- simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/util/FadeOutActivity.java
+++ simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/util/FadeOutActivity.java
@@ -13,13 +13,13 @@ import edu.umd.cs.piccolo.activities.PActivity;
 public class FadeOutActivity extends PActivity {
     
     private final PNode node;
-    private float deltaTransparency;
+    private final float deltaTransparency;
 
     /**
      * Constructor.
      * @param node the node whose visibility will be toggled
      * @param duration duration of this activity, in milliseconds
-     * @param stepRate  amount of time that this activity should delay between steps, in milliseconds
+     * @param stepRate amount of time that this activity should delay between steps, in milliseconds
      */
     public FadeOutActivity( PNode node, long duration, long stepRate ) {
         super( duration );
