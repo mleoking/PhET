9a9ed157d5df62e8f29f0d3f88daef0bf5ecb7bf
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-10-14 21:13:29 +0000

    Added an "ion" readout near to the atom.

---
+++ trunk/simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/view/IonIndicatorNode.java
@@ -0,0 +1,30 @@
+package edu.colorado.phet.buildanatom.view;
+
+import java.awt.*;
+
+import edu.colorado.phet.buildanatom.model.Atom;
+import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
+import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
+import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.nodes.PText;
+
+/**
+ * @author Sam Reid
+ */
+public class IonIndicatorNode extends PNode {
+    private static final Font ATOM_ION_FONT = new PhetFont( 28, true );
+
+    public IonIndicatorNode( final Atom atom ) {
+        addChild( new PText( "Ion" ) {{       //todo: i18n
+            setFont( ATOM_ION_FONT );
+            setTextPaint( Color.blue );
+            atom.addObserver( new SimpleObserver() {
+                public void update() {
+                    setVisible( atom.getCharge() != 0 );
+                    setText( atom.getCharge()>0?"+ Ion":"- Ion" );//todo: i18n
+                    setTextPaint( atom.getCharge()>0?Color.red:Color.blue );
+                }
+            } );
+        }} );
+    }
+}
