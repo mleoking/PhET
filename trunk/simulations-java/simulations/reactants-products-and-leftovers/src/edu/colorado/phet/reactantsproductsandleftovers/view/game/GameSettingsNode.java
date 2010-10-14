280ed731989db0659274e2904036168f87b82733
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-10-14 23:04:01 +0000

    #2547, generalized Game Settings panel (needs to be migrated to phetcommon, along with associated icons and strings)

--- trunk/simulations-java/simulations/reactants-products-and-leftovers/src/edu/colorado/phet/reactantsproductsandleftovers/view/game/GameSettingsNode.java
+++ trunk/simulations-java/simulations/reactants-products-and-leftovers/src/edu/colorado/phet/reactantsproductsandleftovers/view/game/GameSettingsNode.java
@@ -2,23 +2,19 @@
 
 package edu.colorado.phet.reactantsproductsandleftovers.view.game;
 
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.Color;
+import java.awt.FlowLayout;
 
-import javax.swing.*;
-import javax.swing.border.Border;
-import javax.swing.border.CompoundBorder;
-import javax.swing.border.EmptyBorder;
-import javax.swing.border.LineBorder;
+import javax.swing.ButtonGroup;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 
-import edu.colorado.phet.common.phetcommon.view.util.EasyGridBagLayout;
-import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.PhetPNode;
-import edu.colorado.phet.reactantsproductsandleftovers.RPALImages;
 import edu.colorado.phet.reactantsproductsandleftovers.RPALStrings;
 import edu.colorado.phet.reactantsproductsandleftovers.module.game.GameModel;
 import edu.colorado.phet.reactantsproductsandleftovers.module.game.GameChallenge.ChallengeVisibility;
+import edu.colorado.phet.reactantsproductsandleftovers.view.game.GameSettingsPanel.GameSettingsPanelAdapater;
 import edu.umd.cs.piccolox.pswing.PSwing;
 
 /**
@@ -29,70 +25,25 @@ import edu.umd.cs.piccolox.pswing.PSwing;
  */
 public class GameSettingsNode extends PhetPNode {
     
-    private static final Font TITLE_FONT = new PhetFont( 24 );
-    private static final Border BORDER = new CompoundBorder( new LineBorder( Color.BLACK, 1 ),  new EmptyBorder( 5, 14, 5, 14 ) );
-    private static final Color BACKGROUND = new Color( 180, 205, 255 );
-
-    private final JRadioButton[] levelRadioButtons;
-    private final JRadioButton timerOnRadioButton, timerOffRadioButton;
-    private final JRadioButton soundOnRadioButton, soundOffRadioButton;
+    private static final Color BACKGROUND = new Color( 180, 205, 255 ); // light blue
+    
+    private final GameSettingsPanel panel;
     private final JRadioButton hideNothingRadioButton, hideMoleculesRadioButton, hideNumbersRadioButton;
     
     public GameSettingsNode( final GameModel model ) {
         super();
         
-        // Title
-        JLabel titleLabel = new JLabel( RPALStrings.TITLE_GAME_SETTINGS );
-        titleLabel.setFont( TITLE_FONT );
+        // Game settings panel
+        panel = new GameSettingsPanel( GameModel.getLevelRange() );
+        panel.setBackground( BACKGROUND );
+        panel.addGameSettingsPanelListener( new GameSettingsPanelAdapater() {
+            @Override
+            public void startButtonPressed() {
+                model.startGame( panel.getLevel(), panel.isTimerOn(), panel.isSoundOn(), getChallengeVisibility() );
+            }
+        });
         
-        // separator
-        JSeparator titleSeparator = new JSeparator();
-        titleSeparator.setForeground( Color.BLACK );
-        
-        // Level control
-        levelRadioButtons = new JRadioButton[ GameModel.getLevelRange().getLength() + 1 ];
-        JLabel levelLabel = new JLabel( RPALStrings.LABEL_LEVEL_CONTROL );
-        ButtonGroup levelButtonGroup = new ButtonGroup();
-        JPanel levelPanel = new JPanel( new FlowLayout( FlowLayout.LEFT ) );
-        levelPanel.setOpaque( false );
-        for ( int i = 0; i < levelRadioButtons.length; i++ ) {
-            int level = GameModel.getLevelRange().getMin() + i;
-            JRadioButton button = new JRadioButton( String.valueOf( level ) );
-            button.setOpaque( false );
-            levelRadioButtons[i] = button;
-            levelPanel.add( button );
-            levelButtonGroup.add( button );
-        }
-        
-        // Timer control
-        JLabel timerLabel = new JLabel( new ImageIcon( RPALImages.STOPWATCH ) );
-        timerOnRadioButton = new JRadioButton( RPALStrings.RADIO_BUTTON_ON );
-        timerOnRadioButton.setOpaque( false );
-        timerOffRadioButton = new JRadioButton( RPALStrings.RADIO_BUTTON_OFF );
-        timerOffRadioButton.setOpaque( false );
-        ButtonGroup timerButtonGroup = new ButtonGroup();
-        timerButtonGroup.add( timerOnRadioButton );
-        timerButtonGroup.add( timerOffRadioButton );
-        JPanel timerPanel = new JPanel( new FlowLayout( FlowLayout.LEFT ) );
-        timerPanel.setOpaque( false );
-        timerPanel.add( timerOnRadioButton );
-        timerPanel.add( timerOffRadioButton );
-        
-        // Sound control
-        JLabel soundLabel = new JLabel( new ImageIcon( RPALImages.SOUND_ICON ) );
-        soundOnRadioButton = new JRadioButton( RPALStrings.RADIO_BUTTON_ON );
-        soundOnRadioButton.setOpaque( false );
-        soundOffRadioButton = new JRadioButton( RPALStrings.RADIO_BUTTON_OFF );
-        soundOffRadioButton.setOpaque( false );
-        ButtonGroup soundButtonGroup = new ButtonGroup();
-        soundButtonGroup.add( soundOnRadioButton );
-        soundButtonGroup.add( soundOffRadioButton );
-        JPanel soundPanel = new JPanel( new FlowLayout( FlowLayout.LEFT ) );
-        soundPanel.setOpaque( false );
-        soundPanel.add( soundOnRadioButton );
-        soundPanel.add( soundOffRadioButton );
-        
-        // Visibility control
+        // customize the game settings by adding a "hide" control
         JLabel hideLabel = new JLabel( RPALStrings.LABEL_HIDE );
         hideNothingRadioButton = new JRadioButton( RPALStrings.RADIO_BUTTON_NOTHING );
         hideNothingRadioButton.setOpaque( false );
@@ -100,99 +51,26 @@ public class GameSettingsNode extends PhetPNode {
         hideMoleculesRadioButton.setOpaque( false );
         hideNumbersRadioButton = new JRadioButton( RPALStrings.RADIO_BUTTON_NUMBERS );
         hideNumbersRadioButton.setOpaque( false );
-        ButtonGroup imageButtonGroup = new ButtonGroup();
-        imageButtonGroup.add( hideNothingRadioButton );
-        imageButtonGroup.add( hideMoleculesRadioButton );
-        imageButtonGroup.add( hideNumbersRadioButton );
-        JPanel imagesPanel = new JPanel( new FlowLayout( FlowLayout.LEFT ) );
-        imagesPanel.setOpaque( false );
-        imagesPanel.add( hideNothingRadioButton );
-        imagesPanel.add( hideMoleculesRadioButton );
-        imagesPanel.add( hideNumbersRadioButton );
-        
-        // separator
-        JSeparator buttonSeparator = new JSeparator();
-        buttonSeparator.setForeground( Color.BLACK );
-        
-        // Start! button
-        JButton startButton = new JButton( RPALStrings.BUTTON_START );
-        startButton.setOpaque( false );
-        startButton.addActionListener( new ActionListener() {
-            public void actionPerformed( ActionEvent e ) {
-                model.startGame( getLevel(), isTimerOnSelected(), isSoundOnSelected(), getChallengeVisibility() );
-            }
-        });
-        
-        // panel
-        JPanel panel = new JPanel();
-        panel.setBackground( BACKGROUND );
-        panel.setBorder( BORDER );
-        EasyGridBagLayout layout = new EasyGridBagLayout( panel );
-        layout.setInsets( new Insets( 5, 5, 5, 5 ) );
-        panel.setLayout( layout );
-        int row = 0;
-        int column = 0;
-        layout.addComponent( titleLabel, row++, column, 2, 1, GridBagConstraints.CENTER );
-        layout.addFilledComponent( titleSeparator, row++, column, 2, 1, GridBagConstraints.HORIZONTAL );
-        layout.addAnchoredComponent( levelLabel, row, column++, GridBagConstraints.EAST );
-        layout.addComponent( levelPanel, row++, column );
-        column = 0;
-        layout.addAnchoredComponent( timerLabel, row, column++, GridBagConstraints.EAST );
-        layout.addComponent( timerPanel, row++, column );
-        column = 0;
-        layout.addAnchoredComponent( soundLabel, row, column++, GridBagConstraints.EAST );
-        layout.addComponent( soundPanel, row++, column );
-        column = 0;
-        layout.addAnchoredComponent( hideLabel, row, column++, GridBagConstraints.EAST );
-        layout.addComponent( imagesPanel, row++, column );
-        column = 0;
-        layout.addFilledComponent( buttonSeparator, row++, column, 2, 1, GridBagConstraints.HORIZONTAL );
-        layout.addComponent( startButton, row++, column, 2, 1, GridBagConstraints.CENTER );
+        ButtonGroup hideButtonGroup = new ButtonGroup();
+        hideButtonGroup.add( hideNothingRadioButton );
+        hideButtonGroup.add( hideMoleculesRadioButton );
+        hideButtonGroup.add( hideNumbersRadioButton );
+        JPanel hidePanel = new JPanel( new FlowLayout( FlowLayout.LEFT ) );
+        hidePanel.setOpaque( false );
+        hidePanel.add( hideNothingRadioButton );
+        hidePanel.add( hideMoleculesRadioButton );
+        hidePanel.add( hideNumbersRadioButton );
+        panel.addControl( hideLabel, hidePanel );
         
         // PSwing wrapper
         PSwing pswing = new PSwing( panel );
         addChild( pswing );
         
         // initial state
-        setLevel( model.getLevel() );
-        setTimerOnSelected( model.isTimerVisible() );
-        setSoundOnSelected( model.isSoundEnabled() );
+        panel.setLevel( model.getLevel() );
+        panel.setTimerOn( model.isTimerVisible() );
+        panel.setSoundOn( model.isSoundEnabled() );
         setChallengeVisibility( model.getChallengeVisibility() );
-    }
-    
-    private void setLevel( int level ) {
-        int index = level - GameModel.getLevelRange().getMin();
-        levelRadioButtons[index].setSelected( true );
-    }
-    
-    private int getLevel() {
-        int level = GameModel.getLevelRange().getMin() - 1; // invalid value, outside range
-        for ( int i = 0; i < levelRadioButtons.length; i++ ) {
-            if ( levelRadioButtons[i].isSelected() ) {
-                level = GameModel.getLevelRange().getMin() + i;
-                break;
-            }
-        }
-        assert( GameModel.getLevelRange().contains( level ) );
-        return level;
-    }
-    
-    private void setTimerOnSelected( boolean selected ) {
-        timerOnRadioButton.setSelected( selected );
-        timerOffRadioButton.setSelected( !selected );
-    }
-    
-    private boolean isTimerOnSelected() {
-        return timerOnRadioButton.isSelected();
-    }
-    
-    private void setSoundOnSelected( boolean selected ) {
-        soundOnRadioButton.setSelected( selected );
-        soundOffRadioButton.setSelected( !selected );
-    }
-    
-    private boolean isSoundOnSelected() {
-        return soundOnRadioButton.isSelected();
     }
     
     private void setChallengeVisibility( ChallengeVisibility challengeVisibility ) {
