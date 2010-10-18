f6a893988b3354492018e250def228a800e67ddf
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-10-18 18:51:30 +0000

    Borrowed some code from the Neuron sim to draw the plus and minus symbols instead of use fonts and shadow text.  This just looks better.  Also lightened background for positive charge icon.

--- simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/view/ChargePairingGraphNode.java
+++ simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/view/ChargePairingGraphNode.java
@@ -5,15 +5,16 @@ package edu.colorado.phet.buildanatom.view;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.Shape;
 import java.awt.geom.Dimension2D;
 import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 
 import edu.colorado.phet.buildanatom.model.Atom;
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
+import edu.colorado.phet.common.phetcommon.view.util.DoubleGeneralPath;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
-import edu.colorado.phet.common.piccolophet.nodes.ShadowPText;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.util.PDimension;
 
@@ -28,6 +29,7 @@ public class ChargePairingGraphNode extends PNode {
 
     private static final double VERTICAL_INTER_ICON_SPACING = 5;
     private static final double HORIZONTAL_INTER_ICON_SPACING = 5;
+    private static final double THICKNESS_FACTOR = 0.3;
 
     private final ArrayList<PositiveChargeIconNode> positiveChargeIconList = new ArrayList<PositiveChargeIconNode>();
     private final ArrayList<NegativeChargeIconNode> negativeChargeIconList = new ArrayList<NegativeChargeIconNode>();
@@ -35,58 +37,58 @@ public class ChargePairingGraphNode extends PNode {
     /**
      * Constructor.
      */
-    public ChargePairingGraphNode(final Atom atom) {
+    public ChargePairingGraphNode( final Atom atom ) {
 
         atom.addObserver( new SimpleObserver() {
 
             public void update() {
                 int negChargeDiff = atom.getNumElectrons() - negativeChargeIconList.size();
-                if (negChargeDiff > 0){
+                if ( negChargeDiff > 0 ) {
                     addNegativeIcons( negChargeDiff );
                 }
-                else if (negChargeDiff < 0){
+                else if ( negChargeDiff < 0 ) {
                     removeNegativeIcons( -negChargeDiff );
                 }
 
                 int posChargeDiff = atom.getNumProtons() - positiveChargeIconList.size();
-                if (posChargeDiff > 0){
+                if ( posChargeDiff > 0 ) {
                     addPositiveIcons( posChargeDiff );
                 }
-                else if (posChargeDiff < 0){
+                else if ( posChargeDiff < 0 ) {
                     removePositiveIcons( -posChargeDiff );
                 }
             }
-        });
+        } );
     }
 
     private void addPositiveIcons( int numToAdd ) {
         assert numToAdd >= 0;
-        for (int i = 0; i < numToAdd; i++){
+        for ( int i = 0; i < numToAdd; i++ ) {
             PositiveChargeIconNode icon = new PositiveChargeIconNode();
             positiveChargeIconList.add( icon );
             icon.setOffset(
-                    positiveChargeIconList.size() * (ChargeIconNode.CHARGE_ICON_SIZE.getWidth() + HORIZONTAL_INTER_ICON_SPACING),
+                    positiveChargeIconList.size() * ( ChargeIconNode.CHARGE_ICON_SIZE.getWidth() + HORIZONTAL_INTER_ICON_SPACING ),
                     ChargeIconNode.CHARGE_ICON_SIZE.getHeight() + VERTICAL_INTER_ICON_SPACING );
-            addChild(icon);
+            addChild( icon );
         }
     }
 
     private void addNegativeIcons( int numToAdd ) {
         assert numToAdd >= 0;
-        for (int i = 0; i < numToAdd; i++){
+        for ( int i = 0; i < numToAdd; i++ ) {
             NegativeChargeIconNode icon = new NegativeChargeIconNode();
             negativeChargeIconList.add( icon );
             icon.setOffset(
-                    negativeChargeIconList.size() * (ChargeIconNode.CHARGE_ICON_SIZE.getWidth() + HORIZONTAL_INTER_ICON_SPACING),
+                    negativeChargeIconList.size() * ( ChargeIconNode.CHARGE_ICON_SIZE.getWidth() + HORIZONTAL_INTER_ICON_SPACING ),
                     0 );
-            addChild(icon);
+            addChild( icon );
         }
     }
 
     private void removePositiveIcons( int numToRemove ) {
         assert numToRemove >= 0;
         assert positiveChargeIconList.size() >= numToRemove;
-        for (int i = 0; i < numToRemove; i++){
+        for ( int i = 0; i < numToRemove; i++ ) {
             PositiveChargeIconNode icon = positiveChargeIconList.get( positiveChargeIconList.size() - 1 );
             positiveChargeIconList.remove( icon );
             removeChild( icon );
@@ -96,7 +98,7 @@ public class ChargePairingGraphNode extends PNode {
     private void removeNegativeIcons( int numToRemove ) {
         assert numToRemove >= 0;
         assert negativeChargeIconList.size() >= numToRemove;
-        for (int i = 0; i < numToRemove; i++){
+        for ( int i = 0; i < numToRemove; i++ ) {
             NegativeChargeIconNode icon = negativeChargeIconList.get( negativeChargeIconList.size() - 1 );
             negativeChargeIconList.remove( icon );
             removeChild( icon );
@@ -105,43 +107,28 @@ public class ChargePairingGraphNode extends PNode {
 
     static class ChargeIconNode extends PNode {
 
-        private static final Dimension2D CHARGE_ICON_SIZE = new PDimension(20, 20);
-        private static final Font CHARGE_ICON_FONT = new PhetFont(24, true);
+        protected static final Dimension2D CHARGE_ICON_SIZE = new PDimension( 20, 20 );
 
-        // Tweak factor for making the label appear a bit more centered.
-        // Value should be between -1 and 1.  This is needed because the
-        // charge characters are often not very centered within the font.
-        private static final double HORIZ_LABEL_POS_TWEAK_FACTOR = 0;    // Negative shift left, positive shifts right.
-        private static final double VERT_LABEL_POS_TWEAK_FACTOR = -0.1;  // Negative shifts up, positive shifts down.
-
-        /**
-         * Constructor.
-         */
-        public ChargeIconNode(String symbol, Color foregroundColor, Color backgroundColor) {
-            ShadowPText label = new ShadowPText(symbol);
-            label.setFont(CHARGE_ICON_FONT);
-            label.setTextPaint( foregroundColor );
-            label.setShadowColor( Color.BLACK );
-            label.setOffset(
-                    CHARGE_ICON_SIZE.getWidth() * (0.5 + HORIZ_LABEL_POS_TWEAK_FACTOR) - label.getFullBoundsReference().getWidth() / 2,
-                    CHARGE_ICON_SIZE.getHeight() * (0.5 + VERT_LABEL_POS_TWEAK_FACTOR) - label.getFullBoundsReference().getHeight() / 2 );
-            Rectangle2D backgroundRect = new Rectangle2D.Double(0, 0, CHARGE_ICON_SIZE.getWidth(),
-                    CHARGE_ICON_SIZE.getHeight());
-            addChild(new PhetPPath(backgroundRect, backgroundColor, new BasicStroke(1), Color.BLACK));
-            addChild( label );
+        public ChargeIconNode( Shape symbol, Color foregroundColor, Color backgroundColor ) {
+            PhetPPath symbolNode = new PhetPPath( symbol, foregroundColor, new BasicStroke( 1f ), Color.BLACK );
+            symbolNode.setOffset( CHARGE_ICON_SIZE.getWidth() / 2, CHARGE_ICON_SIZE.getHeight() / 2 );
+            Rectangle2D backgroundRect = new Rectangle2D.Double( 0, 0, CHARGE_ICON_SIZE.getWidth(),
+                    CHARGE_ICON_SIZE.getHeight() );
+            addChild( new PhetPPath( backgroundRect, backgroundColor, new BasicStroke( 1 ), Color.BLACK ) );
+            addChild( symbolNode );
         }
     }
 
     static class PositiveChargeIconNode extends ChargeIconNode {
 
-        private static Color BACKGROUND_COLOR = new Color( 228, 183, 183 );
+        private static Color BACKGROUND_COLOR = new Color( 248, 203, 203 );
         private static Color SYMBOL_COLOR = Color.RED;
 
         /**
          * Constructor.
          */
         public PositiveChargeIconNode() {
-            super("+", SYMBOL_COLOR, BACKGROUND_COLOR);
+            super( drawPlusSign(CHARGE_ICON_SIZE.getWidth() * 0.6), SYMBOL_COLOR, BACKGROUND_COLOR );
         }
     }
 
@@ -154,7 +141,34 @@ public class ChargePairingGraphNode extends PNode {
          * Constructor.
          */
         public NegativeChargeIconNode() {
-            super("-", SYMBOL_COLOR, BACKGROUND_COLOR);
+            super( drawMinusSign(CHARGE_ICON_SIZE.getWidth() * 0.5), SYMBOL_COLOR, BACKGROUND_COLOR );
         }
     }
+
+    private static Shape drawPlusSign( double width ) {
+        DoubleGeneralPath path = new DoubleGeneralPath();
+        double thickness = width * THICKNESS_FACTOR;
+        double halfThickness = thickness / 2;
+        double halfWidth = width / 2;
+        path.moveTo( -halfWidth, -halfThickness );
+        path.lineTo( -halfWidth, halfThickness );
+        path.lineTo( -halfThickness, halfThickness );
+        path.lineTo( -halfThickness, halfWidth );
+        path.lineTo( halfThickness, halfWidth );
+        path.lineTo( halfThickness, halfThickness );
+        path.lineTo( halfWidth, halfThickness );
+        path.lineTo( halfWidth, -halfThickness );
+        path.lineTo( halfThickness, -halfThickness );
+        path.lineTo( halfThickness, -halfWidth );
+        path.lineTo( -halfThickness, -halfWidth );
+        path.lineTo( -halfThickness, -halfThickness );
+        path.closePath();
+        return path.getGeneralPath();
+    }
+
+    private static Shape drawMinusSign( double width ) {
+        double height = width * THICKNESS_FACTOR;
+        return new Rectangle2D.Double( -width / 2, -height / 2, width, height );
+    }
+
 }
