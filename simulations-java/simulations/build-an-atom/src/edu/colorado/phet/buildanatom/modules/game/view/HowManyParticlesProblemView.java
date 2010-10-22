83365a150cf444a52b4105ff07b4f5729124d872
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-10-22 19:33:07 +0000

    Added the symbol indicator node.

--- simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/modules/game/view/HowManyParticlesProblemView.java
+++ simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/modules/game/view/HowManyParticlesProblemView.java
@@ -3,6 +3,7 @@ package edu.colorado.phet.buildanatom.modules.game.view;
 import edu.colorado.phet.buildanatom.BuildAnAtomDefaults;
 import edu.colorado.phet.buildanatom.modules.game.model.BuildAnAtomGameModel;
 import edu.colorado.phet.buildanatom.modules.game.model.HowManyParticlesProblem;
+import edu.colorado.phet.buildanatom.view.SymbolIndicatorNode;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.umd.cs.piccolo.nodes.PText;
 
@@ -10,26 +11,31 @@ import edu.umd.cs.piccolo.nodes.PText;
 * @author Sam Reid
 */
 public class HowManyParticlesProblemView extends ProblemView {
-    private PText description = new PText( "How many particles?" ) {{
+
+    private final PText description = new PText( "How many particles?" ) {{
         setFont( new PhetFont( 20, true ) );
         setOffset( BuildAnAtomDefaults.STAGE_SIZE.width - getFullBounds().getWidth() - 200, 30 );
     }};
 
+    private final SymbolIndicatorNode symbolIndicatorNode;
+
     public HowManyParticlesProblemView( BuildAnAtomGameModel model, GameCanvas canvas, HowManyParticlesProblem howManyParticlesProblem, int problemIndex, int totalNumProblems ) {
         super( model, canvas, howManyParticlesProblem, problemIndex, totalNumProblems );
-
+        symbolIndicatorNode = new SymbolIndicatorNode( problem.getAtom() );
     }
 
     @Override
     public void init() {
         super.init();
         addChild( description );
+        addChild( symbolIndicatorNode );
     }
 
     @Override
     public void teardown() {
         super.teardown();
         removeChild( description );
+        removeChild( symbolIndicatorNode );
     }
 
 }
