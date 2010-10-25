991884ad589cbf695fb1e2644e7343f68b0d6e32
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-10-25 20:25:47 +0000

    Added the step where the child nodes are removed when this problem is complete.

--- simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/modules/game/view/CompleteTheSymbolProblemView.java
+++ simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/modules/game/view/CompleteTheSymbolProblemView.java
@@ -17,35 +17,39 @@ public class CompleteTheSymbolProblemView extends ProblemView {
         setFont( new PhetFont( 20, true ) );
         setOffset( BuildAnAtomDefaults.STAGE_SIZE.width - getFullBounds().getWidth() - 200, 30 );
     }};
+    private final AtomConfigText atomConfigurationText;
+    private final InteractiveSymbolNode interactiveSymbolNode;
 
     /**
      * Constructor.
      */
     public CompleteTheSymbolProblemView( BuildAnAtomGameModel model, GameCanvas canvas, CompleteTheSymbolProblem problem) {
         super( model, canvas, problem);
-        AtomConfigText atomConfigurationText = new AtomConfigText( problem.getAtom().getNumProtons(),
+        atomConfigurationText = new AtomConfigText( problem.getAtom().getNumProtons(),
                 problem.getAtom().getNumNeutrons(), problem.getAtom().getNumElectrons() );
         atomConfigurationText.setOffset(
                 BuildAnAtomDefaults.STAGE_SIZE.width /4 - atomConfigurationText.getFullBounds().getWidth() / 2,
                 BuildAnAtomDefaults.STAGE_SIZE.height / 2 - atomConfigurationText.getFullBounds().getHeight() / 2 );
-        addChild( atomConfigurationText );
-        InteractiveSymbolNode interactiveSymbolNode=new InteractiveSymbolNode( problem.getGuessedProtonsProperty(),
+        interactiveSymbolNode = new InteractiveSymbolNode( problem.getGuessedProtonsProperty(),
                 problem.getGuessedNeutronsProperty(), problem.getGuessedElectronsProperty(), true);
         interactiveSymbolNode.setOffset(
                 BuildAnAtomDefaults.STAGE_SIZE.width * 0.75 - interactiveSymbolNode.getFullBounds().getWidth() / 2,
                 BuildAnAtomDefaults.STAGE_SIZE.height / 2 - interactiveSymbolNode.getFullBounds().getHeight() / 2 );
-        addChild(interactiveSymbolNode);
     }
 
     @Override
     public void init() {
         super.init();
         addChild( description );
+        addChild( atomConfigurationText );
+        addChild( interactiveSymbolNode );
     }
 
     @Override
     public void teardown() {
         super.teardown();
         removeChild( description );
+        removeChild( atomConfigurationText );
+        removeChild( interactiveSymbolNode );
     }
 }
