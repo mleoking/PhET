87405c4248ba987bf1d91b86379ae2555daf8510
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-10-25 22:05:15 +0000

    Added some code, but then commented it out, that is intended to update when the properties change (so that we can show the user the answer if need be).  So far, this code is problematic.

--- trunk/simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/view/InteractiveSymbolNode.java
+++ trunk/simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/view/InteractiveSymbolNode.java
@@ -11,6 +11,7 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
 import edu.colorado.phet.common.phetcommon.model.Property;
+import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.umd.cs.piccolo.PNode;
@@ -38,15 +39,50 @@ public class InteractiveSymbolNode extends PNode {
     private final JSpinner massSpinner = new JSpinner( new SpinnerNumberModel( 0, 0, 30, 1 ) );
     private final JSpinner chargeSpinner = new JSpinner( new SpinnerNumberModel( 0, -15, 15, 1 ) );
 
+    private final Property<Integer> guessedProtonsProperty;
+    private final Property<Integer> guessedNeutronsProperty;
+    private final Property<Integer> guessedElectronsProperty;
+
     public InteractiveSymbolNode( final Property<Integer> guessedProtonsProperty,
             final Property<Integer> guessedNeutronsProperty, final Property<Integer> guessedElectronsProperty,
             final boolean showCharge ) {
+
+        this.guessedProtonsProperty = guessedProtonsProperty;
+        this.guessedNeutronsProperty = guessedNeutronsProperty;
+        this.guessedElectronsProperty = guessedElectronsProperty;
+
+        /*
+        guessedProtonsProperty.addObserver( new SimpleObserver() {
+            public void update() {
+                if ((Integer)protonSpinner.getValue() != guessedProtonsProperty.getValue()){
+                    protonSpinner.setValue( guessedProtonsProperty.getValue() );
+                }
+            }
+        });
+        guessedNeutronsProperty.addObserver( new SimpleObserver() {
+            public void update() {
+                if ((Integer)massSpinner.getValue() != guessedNeutronsProperty.getValue() + guessedProtonsProperty.getValue() ){
+                    massSpinner.setValue( guessedNeutronsProperty.getValue() + guessedProtonsProperty.getValue() );
+                }
+            }
+        });
+        guessedElectronsProperty.addObserver( new SimpleObserver() {
+            public void update() {
+                System.out.println("----------------");
+                System.out.println("electron number update, charge = " + (guessedProtonsProperty.getValue() - guessedElectronsProperty.getValue()));
+                System.out.println("electrons = " + guessedElectronsProperty.getValue());
+                System.out.println("protons = " + guessedProtonsProperty.getValue());
+                if ((Integer)chargeSpinner.getValue() != guessedProtonsProperty.getValue() - guessedElectronsProperty.getValue() ){
+                    chargeSpinner.setValue( guessedProtonsProperty.getValue() - guessedElectronsProperty.getValue() );
+                }
+            }
+        });
+        */
 
         PNode boundingBox = new PhetPPath( new Rectangle2D.Double( 0, 0, WIDTH, HEIGHT ), Color.white,
                 new BasicStroke( 3 ), Color.black );
         addChild( boundingBox );
 
-        // Textual symbol.
         PText symbol = new PText("X"){{
             setFont( SYMBOL_FONT );
             setOffset( WIDTH / 2 - getFullBoundsReference().width / 2, HEIGHT / 2 - getFullBoundsReference().height / 2 );
