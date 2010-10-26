4d3dda8124df59652d9153a27d0be1df2f7f4e29
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-10-26 16:33:59 +0000

    fixed: charge indicator number should have a "+" for positive numbers and not for zero

---
+++ trunk/simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/view/SignedIntegerFormat.java
@@ -0,0 +1,57 @@
+package edu.colorado.phet.buildanatom.view;
+
+import java.text.DecimalFormat;
+import java.text.FieldPosition;
+import java.text.NumberFormat;
+import java.text.ParsePosition;
+
+import edu.colorado.phet.common.phetcommon.resources.PhetResources;
+
+/**
+ * In Build an Atom, we need to always show the sign for positive and negative numbers (for the charge indication)
+ * But never for zero, some examples: -2, -1, 0, +1, +2, etc.
+ *
+ * @author Sam Reid
+ * @author John Blanco
+ */
+public class SignedIntegerFormat extends DecimalFormat {
+    private NumberFormat decimalFormat;
+
+    public SignedIntegerFormat() {
+        this.decimalFormat = ( DecimalFormat.getNumberInstance( PhetResources.readLocale() ) );
+        if ( decimalFormat instanceof DecimalFormat ) {
+            ( (DecimalFormat) decimalFormat ).applyPattern( "0" );
+        }
+    }
+
+    public StringBuffer format( long number, StringBuffer result, FieldPosition fieldPosition ) {
+        fieldPosition.setBeginIndex( 0 );
+        fieldPosition.setEndIndex( 0 );
+
+        //Add a '+' for positive numbers
+        if ( number > 0 ) {
+            return new StringBuffer( "+" + number );
+        }
+        else {
+            return new StringBuffer( "" + number );
+        }
+    }
+
+    @Override
+    public Number parse( String text, ParsePosition pos ) {
+        //Strip out '+' if the user had typed it in
+        if ( text.startsWith( "+" ) ) {
+            return new DecimalFormat( "0" ).parse( text.substring( 1 ), pos );
+        }
+        else {
+            return new DecimalFormat( "0" ).parse( text, pos );
+        }
+    }
+
+    public static void main( String[] args ) {
+        for ( int i = -4; i < 10; i++ ) {
+            System.out.println( "new SignedIntegerFormat().format(" + i + ") = " + new SignedIntegerFormat().format( i ) );
+        }
+    }
+
+}
