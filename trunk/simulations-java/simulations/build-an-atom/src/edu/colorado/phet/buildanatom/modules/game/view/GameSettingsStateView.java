b7e145d97f6f6bf2a7a77b522f0c965f8b02ebb6
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-10-27 16:32:45 +0000

    Made the sound and game level settings persistent.

--- trunk/simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/modules/game/view/GameSettingsStateView.java
+++ trunk/simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/modules/game/view/GameSettingsStateView.java
@@ -4,7 +4,6 @@ import edu.colorado.phet.buildanatom.BuildAnAtomDefaults;
 import edu.colorado.phet.buildanatom.modules.game.model.BuildAnAtomGameModel;
 import edu.colorado.phet.common.games.GameSettingsPanel;
 import edu.colorado.phet.common.phetcommon.util.IntegerRange;
-import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolox.pswing.PSwing;
 
@@ -17,15 +16,18 @@ public class GameSettingsStateView extends StateView {
 
     public GameSettingsStateView( GameCanvas gameCanvas, final BuildAnAtomGameModel model ) {
         super( model, model.getGameSettingsState(), gameCanvas );
-        panel = new GameSettingsPanel( new IntegerRange( 1, 3 ) );
-        panel.setTimerOn( model.getTimerEnabledProperty().getValue() );
+        panel = new GameSettingsPanel( new IntegerRange( 1, 3 ) ){{
+            setTimerOn( model.getTimerEnabledProperty().getValue() );
+            setSoundOn( model.getSoundEnabledProperty().getValue() );
+            setLevel( model.getLevelProperty().getValue() );
+            addGameSettingsPanelListener( new GameSettingsPanel.GameSettingsPanelAdapater() {
+                @Override
+                public void startButtonPressed() {
+                    model.startGame( panel.getLevel(), panel.isTimerOn(), panel.isSoundOn() );
+                }
+            } );
+        }};
         gameSettingsNode = new PSwing( panel );
-        panel.addGameSettingsPanelListener( new GameSettingsPanel.GameSettingsPanelAdapater() {
-            @Override
-            public void startButtonPressed() {
-                model.startGame( panel.getLevel(), panel.isTimerOn(), panel.isSoundOn() );
-            }
-        } );
     }
 
     @Override
