e5663323ed974504a4e4e9b3c63a4d8ade9f4b1c
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-10-29 21:57:12 +0000

    Added all problem types to the dialog and the ability to get the enabled/disabled values.  Also hooked up the Check All and Clear All buttons.

--- trunk/simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/developer/ProblemTypeSelectionDialog.java
+++ trunk/simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/developer/ProblemTypeSelectionDialog.java
@@ -6,6 +6,9 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Frame;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
@@ -14,29 +17,89 @@ import javax.swing.JPanel;
 import edu.colorado.phet.common.phetcommon.application.PaintImmediateDialog;
 
 /**
- * Dialog for use in the developer controls that allows the user to select a
- * single problem type for display to the user on the game tab.  This was
- * requested to make it easier to test users' responses to the various types
- * of problems presented in the game.
+ * Dialog for use in the developer controls that allows the user to select the
+ * problem types that are allowed on the game tab.  This was requested to make
+ * it easier to test users' responses to the various types of problems
+ * presented in the game.
  *
  * @author John Blanco
  */
 public class ProblemTypeSelectionDialog extends PaintImmediateDialog {
 
+    // Checkboxes that control the problem types allowed.
+    private final JCheckBox symbolToSchematicProblemAllowed = new JCheckBox( "Symbol To Schematic Problem Allowed", true );
+    private final JCheckBox schematicToSymbolProblemAllowed = new JCheckBox( "Schematic To Symbol Problem Allowed", true );
+    private final JCheckBox symbolToCountsProblemAllowed = new JCheckBox( "Symbol To Counts Problem Allowed", true );
+    private final JCheckBox countsToSymbolProblemAllowed = new JCheckBox( "Counts To Symbol Problem Allowed", true );
+    private final JCheckBox schematicToElementProblemAllowed = new JCheckBox( "Schematic To Element Problem Allowed", true );
+    private final JCheckBox countsToElementProblemAllowed = new JCheckBox( "Counts To Element Problem Allowed", true );
+
+    private final ArrayList<JCheckBox> checkBoxList = new ArrayList<JCheckBox>() {{
+        add( symbolToSchematicProblemAllowed );
+        add( schematicToSymbolProblemAllowed );
+        add( symbolToCountsProblemAllowed );
+        add( countsToSymbolProblemAllowed );
+        add( schematicToElementProblemAllowed );
+        add( countsToElementProblemAllowed );
+    }};
+
+    /**
+     * Constructor.
+     */
     public ProblemTypeSelectionDialog( Frame frame ) {
         super( frame, "Select Problem Types Allowed" );
-        setPreferredSize( new Dimension(300, 400) );
-        setLayout( new GridLayout(7, 1) );
-        add( new JCheckBox( "Prob type 1" ) );
-        add( new JCheckBox( "Prob type 2" ) );
-        add( new JCheckBox( "Prob type 3" ) );
-        add( new JCheckBox( "Prob type 4" ) );
-        add( new JCheckBox( "Prob type 5" ) );
-        add( new JCheckBox( "Prob type 6" ) );
-        JPanel buttonPanel = new JPanel(new FlowLayout());
-        buttonPanel.add( new JButton("Check All") );
-        buttonPanel.add( new JButton("Clear All") );
+        setPreferredSize( new Dimension( 300, 400 ) );
+        setLayout( new GridLayout( 7, 1 ) );
+        add( symbolToCountsProblemAllowed );
+        add( schematicToSymbolProblemAllowed );
+        add( symbolToCountsProblemAllowed );
+        add( countsToSymbolProblemAllowed );
+        add( schematicToElementProblemAllowed );
+        add( countsToElementProblemAllowed );
+        JPanel buttonPanel = new JPanel( new FlowLayout() );
+        buttonPanel.add( new JButton( "Check All" ){{
+            addActionListener( new ActionListener() {
+                public void actionPerformed( ActionEvent e ) {
+                    for ( JCheckBox checkBox : checkBoxList){
+                        checkBox.setSelected( true );
+                    }
+                }
+            });
+        }});
+        buttonPanel.add( new JButton( "Clear All" ){{
+            addActionListener( new ActionListener() {
+                public void actionPerformed( ActionEvent e ) {
+                    for ( JCheckBox checkBox : checkBoxList){
+                        checkBox.setSelected( false );
+                    }
+                }
+            });
+        }});
         add( buttonPanel );
         pack();
+    }
+
+    public boolean isSymbolToSchematicProblemAllowed() {
+        return symbolToSchematicProblemAllowed.isSelected();
+    }
+
+    public boolean isSchematicToSymbolProblemAllowed() {
+        return schematicToSymbolProblemAllowed.isSelected();
+    }
+
+    public boolean isSymbolToCountsProblemAllowed() {
+        return symbolToCountsProblemAllowed.isSelected();
+    }
+
+    public boolean isCountsToSymbolProblemAllowed() {
+        return countsToSymbolProblemAllowed.isSelected();
+    }
+
+    public boolean isSchematicToElementProblemAllowed() {
+        return schematicToElementProblemAllowed.isSelected();
+    }
+
+    public boolean isCountsToElementProblemAllowed() {
+        return countsToElementProblemAllowed.isSelected();
     }
 }
