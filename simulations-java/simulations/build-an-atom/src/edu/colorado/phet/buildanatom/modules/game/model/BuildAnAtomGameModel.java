a7abc9447d0494912dbe08ad4cdacaf7e470631b
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-11-01 21:27:34 +0000

    Modified the game levels based on a meeting with Kelly L.

--- simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/modules/game/model/BuildAnAtomGameModel.java
+++ simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/modules/game/model/BuildAnAtomGameModel.java
@@ -71,6 +71,24 @@ public class BuildAnAtomGameModel {
     // Level pools from the design doc.  These define the pools of problems for a given level.
     private final HashMap<Integer, ArrayList<AtomValue>> levelPools = new HashMap<Integer, ArrayList<AtomValue>>() {{
         put( 1, new ArrayList<AtomValue>() {{
+            add( new AtomValue( 1, 0, 0 ) );
+            add( new AtomValue( 1, 0, 1 ) );
+            add( new AtomValue( 1, 0, 2 ) );
+            add( new AtomValue( 2, 2, 2 ) );
+            add( new AtomValue( 3, 4, 2 ) );
+            add( new AtomValue( 3, 4, 3 ) );
+            add( new AtomValue( 4, 5, 4 ) );
+            add( new AtomValue( 5, 5, 5 ) );
+            add( new AtomValue( 6, 6, 6 ) );
+            add( new AtomValue( 7, 7, 7 ) );
+            add( new AtomValue( 7, 7, 10 ) );
+            add( new AtomValue( 8, 8, 8 ) );
+            add( new AtomValue( 8, 8, 10 ) );
+            add( new AtomValue( 9, 9, 9 ) );
+            add( new AtomValue( 9, 9, 10 ) );
+            add( new AtomValue( 10, 10, 10 ) );
+        }} );
+        put( 2, new ArrayList<AtomValue>() {{
             add( new AtomValue( 1, 0, 1 ) );
             add( new AtomValue( 2, 2, 2 ) );
             add( new AtomValue( 3, 4, 3 ) );
@@ -81,28 +99,28 @@ public class BuildAnAtomGameModel {
             add( new AtomValue( 8, 8, 8 ) );
             add( new AtomValue( 9, 9, 9 ) );
             add( new AtomValue( 10, 10, 10 ) );
+            add( new AtomValue( 11, 12, 11 ) );
+            add( new AtomValue( 12, 12, 12 ) );
+            add( new AtomValue( 13, 14, 13 ) );
+            add( new AtomValue( 14, 14, 14 ) );
+            add( new AtomValue( 15, 16, 15 ) );
+            add( new AtomValue( 16, 16, 16 ) );
+            add( new AtomValue( 17, 18, 17 ) );
+            add( new AtomValue( 18, 22, 18 ) );
         }} );
-        put( 2, new ArrayList<AtomValue>() {{
+        //before these can work, sim will need to support another shell for e-
+        put( 3, new ArrayList<AtomValue>() {{
             add( new AtomValue( 1, 0, 0 ) );
             add( new AtomValue( 1, 0, 2 ) );
             add( new AtomValue( 3, 4, 2 ) );
             add( new AtomValue( 7, 7, 10 ) );
             add( new AtomValue( 8, 8, 10 ) );
             add( new AtomValue( 9, 9, 10 ) );
-        }} );
-        //before these can work, sim will need to support another shell for e-
-        put( 3, new ArrayList<AtomValue>() {{
-            add( new AtomValue( 11, 12, 11 ) );
             add( new AtomValue( 11, 12, 10 ) );
-            add( new AtomValue( 12, 12, 12 ) );
+            add( new AtomValue( 13, 14, 10 ) );
             add( new AtomValue( 12, 12, 10 ) );
-            add( new AtomValue( 14, 14, 14 ) );
-            add( new AtomValue( 15, 16, 15 ) );
-            add( new AtomValue( 16, 16, 16 ) );
             add( new AtomValue( 16, 16, 18 ) );
-            add( new AtomValue( 17, 18, 17 ) );
             add( new AtomValue( 17, 18, 18 ) );
-            add( new AtomValue( 18, 22, 18 ) );
         }} );
     }};
     private ProblemSet problemSet;
