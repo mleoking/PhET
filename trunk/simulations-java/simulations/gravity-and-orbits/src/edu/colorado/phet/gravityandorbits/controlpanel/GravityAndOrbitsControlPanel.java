49b20eb75e3870ff45019b0e2b461639fac5f9df
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-11-02 17:02:45 +0000

    added ability to change diameter

--- trunk/simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/controlpanel/GravityAndOrbitsControlPanel.java
+++ trunk/simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/controlpanel/GravityAndOrbitsControlPanel.java
@@ -39,6 +39,8 @@ public class GravityAndOrbitsControlPanel extends ControlPanel {
         {
             addResetAllButton( module );
         }
+
+        addControl( new BodyDiameterControl( model.getSun() ) );
     }
 
     //----------------------------------------------------------------------------
