3c224d80443a096b8a0138332803af84f5b42ad4
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-11-02 22:46:34 +0000

    Added better handling for case where developer dialog disabled all problem types for a given level.

--- simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/modules/game/model/ProblemSet.java
+++ simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/modules/game/model/ProblemSet.java
@@ -7,12 +7,13 @@ import java.util.Random;
 import edu.colorado.phet.buildanatom.developer.ProblemTypeSelectionDialog;
 
 /**
- * Represents an ordered list of Problems corresponding to a particular difficulty level
+ * Represents an ordered list of Problems corresponding to a particular difficulty level.
  *
  * @author John Blanco
  * @author Sam Reid
  */
 public class ProblemSet {
+
     private static final int MIN_NUM_SCHEMATIC_PROBS_PER_SET = 2;
     private static final int MAX_PROTON_NUMBER_FOR_SCHEMATIC_PROBS = 3; // Disallow schematic (Bohr model) probs above this size.
     public static final Random RAND = new Random();
@@ -44,13 +45,17 @@ public class ProblemSet {
         assert numProblems >= MIN_NUM_SCHEMATIC_PROBS_PER_SET;
         for ( int i = 0; i < MIN_NUM_SCHEMATIC_PROBS_PER_SET; i++){
             Problem problem = generateSchematicProblem( model, atomValueList );
-            addProblem( problem );
+            if ( problem != null ){
+                addProblem( problem );
+            }
         }
 
         // Now add problems of any type to the problem set.
         for ( int i = problems.size() - 1; i < numProblems; i++ ) {
             Problem problem = generateProblem( model, atomValueList );
-            addProblem( problem );
+            if ( problem != null ){
+                addProblem( problem );
+            }
         }
 
         // Radomize the order of the problems.
@@ -135,88 +140,7 @@ public class ProblemSet {
         return problems.get( RAND.nextInt( problems.size() ) );
     }
 
-    private Problem getSchematicProblem( BuildAnAtomGameModel model, AtomValue atomValue ) {
-        ArrayList<Problem> problems = getPossibleSchematicProblems( model, atomValue );
-        return problems.get( RAND.nextInt( problems.size() ) );
-    }
-
-    /**
-     * Get all possible problems that involve an atom schematic, a.k.a. a
-     * "Bohr model" representation.
-     */
-    private ArrayList<Problem> getPossibleSchematicProblems( BuildAnAtomGameModel model, AtomValue atomValue ) {
-
-        assert atomValue.getProtons() < MAX_PROTON_NUMBER_FOR_SCHEMATIC_PROBS;
-
-        // Get the developer dialog that contains the information regarding
-        // which problem types are allowed.
-        // TODO: We may wish to remove this once interviews are complete.
-        ProblemTypeSelectionDialog allowedProbsDlg = ProblemTypeSelectionDialog.getInstance();
-
-        // Make sure that at least one schematic problem type is allowed based
-        // on the current setting of the developer dialog.  If not, turn 'em
-        // all back on.
-        if ( !allowedProbsDlg.isSymbolToSchematicProblemAllowed() &&
-             !allowedProbsDlg.isSchematicToElementProblemAllowed() &&
-             !allowedProbsDlg.isSchematicToSymbolProblemAllowed() ){
-            // No problem types are selected.  Warn the user via console and
-            // turn on all problem types.
-            System.err.println( getClass().getName() + " - Error: No problem types selected, re-selecting them all." );
-            allowedProbsDlg.setAllSelected();
-        }
-
-        ArrayList<Problem> problems = new ArrayList<Problem>();
-        if ( allowedProbsDlg.isSymbolToSchematicProblemAllowed() ) {
-            problems.add( new SymbolToSchematicProblem( model, atomValue ) );
-        }
-        if ( allowedProbsDlg.isSchematicToSymbolProblemAllowed() ) {
-            problems.add( new SchematicToSymbolProblem( model, atomValue ) );
-        }
-        if ( allowedProbsDlg.isSchematicToElementProblemAllowed() ) {
-            problems.add( new SchematicToElementProblem( model, atomValue ) );
-        }
-        return problems;
-    }
-
-    /**
-     * Get all possible problems that involve the symbol representation, i.e.
-     * the white box with the element symbol in the middle and the various
-     * numbers that qualify it in the corners of the box.
-     */
-    private ArrayList<Problem> getPossibleSymbolProblems( BuildAnAtomGameModel model, AtomValue atomValue ) {
-
-        // Get the developer dialog that contains the information regarding
-        // which problem types are allowed.
-        // TODO: We may wish to remove this once interviews are complete.
-        ProblemTypeSelectionDialog allowedProbsDlg = ProblemTypeSelectionDialog.getInstance();
-
-        // Make sure that at least one symbol problem type is allowed based
-        // on the current setting of the developer dialog.  If not, turn 'em
-        // all back on.
-        if ( !allowedProbsDlg.isSymbolToSchematicProblemAllowed() &&
-                !allowedProbsDlg.isSchematicToSymbolProblemAllowed() &&
-                !allowedProbsDlg.isSymbolToCountsProblemAllowed() &&
-                !allowedProbsDlg.isCountsToSymbolProblemAllowed() ) {
-            // No problem types are selected.  Warn the user via console and
-            // turn on all problem types.
-            System.err.println( getClass().getName() + " - Error: No problem types selected, re-selecting them all." );
-            allowedProbsDlg.setAllSelected();
-        }
-
-        ArrayList<Problem> problems = new ArrayList<Problem>();
-        if ( allowedProbsDlg.isSymbolToSchematicProblemAllowed() ) {
-            problems.add( new SymbolToSchematicProblem( model, atomValue ) );
-        }
-        if ( allowedProbsDlg.isSchematicToSymbolProblemAllowed() ) {
-            problems.add( new SchematicToSymbolProblem( model, atomValue ) );
-        }
-        if ( allowedProbsDlg.isSchematicToElementProblemAllowed() ) {
-            problems.add( new SchematicToElementProblem( model, atomValue ) );
-        }
-        return problems;
-    }
-
-    public void addProblem( Problem problem) {
+    private void addProblem( Problem problem) {
         problems.add( problem );
     }
 
@@ -252,57 +176,6 @@ public class ProblemSet {
         assert !isLastProblem();
         currentProblemIndex++;
         return getCurrentProblem();
-    }
-
-    private static class AtomValuePool {
-        private static final Random AVP_RAND = new Random();
-        private final ArrayList<AtomValue> remainingAtomValues;
-        private final ArrayList<AtomValue> usedAtomValues = new ArrayList<AtomValue>();
-
-        public AtomValuePool(BuildAnAtomGameModel model) {
-            remainingAtomValues = new ArrayList<AtomValue>( model.getLevelPool() );
-        }
-
-        /**
-         * Get a random atom value from the pool of available ones, and then
-         * remove it from the list so that it won't be returned again.
-         * @return
-         */
-        public AtomValue getRandomAtomValue(){
-            assert remainingAtomValues.size() > 0;
-            int index = AVP_RAND.nextInt( remainingAtomValues.size() );
-            AtomValue atomValue = remainingAtomValues.get( index );
-            usedAtomValues.add( atomValue );
-            remainingAtomValues.remove( index );
-            return atomValue;
-        }
-
-        /**
-         * Get an atom value from the pool that is at or below the specified
-         * proton count, and then remove it from the list of remaining atoms.
-         * If there are none that match the constraint, try the list of used
-         * atoms.
-         */
-        public AtomValue getRandomAtomValueMaxSize( int maxProtons ){
-
-            ArrayList<AtomValue> allowableAtomValues = new ArrayList<AtomValue>();
-            for ( AtomValue av : remainingAtomValues ){
-                if (av.getProtons() <= maxProtons){
-                    allowableAtomValues.add( av );
-                }
-            }
-            AtomValue atomValue;
-            if ( allowableAtomValues.size() > 0){
-                atomValue = allowableAtomValues.get( AVP_RAND.nextInt( allowableAtomValues.size() ) );
-                remainingAtomValues.remove( atomValue );
-                usedAtomValues.add( atomValue );
-            }
-            else{
-                System.err.println( getClass().getName() + " - Warning: No remaining atoms values below specified threshold, returning previously used atom value." );
-                atomValue = usedAtomValues.get( AVP_RAND.nextInt( usedAtomValues.size() ) );
-            }
-            return atomValue;
-        }
     }
 
     private static final ArrayList<ProblemType> LEVEL_1_SCHEMATIC_PROB_TYPES = new ArrayList<ProblemType>() {
@@ -497,5 +370,56 @@ public class ProblemSet {
      */
     private boolean isSchematicProbType( ProblemType problemType ){
         return ( problemType == ProblemType.SCHEMATIC_TO_ELEMENT || problemType == ProblemType.SCHEMATIC_TO_SYMBOL || problemType == ProblemType.SYMBOL_TO_SCHEMATIC);
+    }
+
+    private static class AtomValuePool {
+        private static final Random AVP_RAND = new Random();
+        private final ArrayList<AtomValue> remainingAtomValues;
+        private final ArrayList<AtomValue> usedAtomValues = new ArrayList<AtomValue>();
+
+        public AtomValuePool(BuildAnAtomGameModel model) {
+            remainingAtomValues = new ArrayList<AtomValue>( model.getLevelPool() );
+        }
+
+        /**
+         * Get a random atom value from the pool of available ones, and then
+         * remove it from the list so that it won't be returned again.
+         * @return
+         */
+        public AtomValue getRandomAtomValue(){
+            assert remainingAtomValues.size() > 0;
+            int index = AVP_RAND.nextInt( remainingAtomValues.size() );
+            AtomValue atomValue = remainingAtomValues.get( index );
+            usedAtomValues.add( atomValue );
+            remainingAtomValues.remove( index );
+            return atomValue;
+        }
+
+        /**
+         * Get an atom value from the pool that is at or below the specified
+         * proton count, and then remove it from the list of remaining atoms.
+         * If there are none that match the constraint, try the list of used
+         * atoms.
+         */
+        public AtomValue getRandomAtomValueMaxSize( int maxProtons ){
+
+            ArrayList<AtomValue> allowableAtomValues = new ArrayList<AtomValue>();
+            for ( AtomValue av : remainingAtomValues ){
+                if (av.getProtons() <= maxProtons){
+                    allowableAtomValues.add( av );
+                }
+            }
+            AtomValue atomValue;
+            if ( allowableAtomValues.size() > 0){
+                atomValue = allowableAtomValues.get( AVP_RAND.nextInt( allowableAtomValues.size() ) );
+                remainingAtomValues.remove( atomValue );
+                usedAtomValues.add( atomValue );
+            }
+            else{
+                System.err.println( getClass().getName() + " - Warning: No remaining atoms values below specified threshold, returning previously used atom value." );
+                atomValue = usedAtomValues.get( AVP_RAND.nextInt( usedAtomValues.size() ) );
+            }
+            return atomValue;
+        }
     }
 }
