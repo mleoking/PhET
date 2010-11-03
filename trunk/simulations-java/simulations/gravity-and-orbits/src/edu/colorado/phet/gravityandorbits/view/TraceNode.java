a7257c2c35498c0eae5478df119a67653669636c
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-11-03 05:39:34 +0000

    gravity-and-orbits: changed tracing behavior to match my-solar-system

--- trunk/simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/view/TraceNode.java
+++ trunk/simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/view/TraceNode.java
@@ -1,7 +1,6 @@
 package edu.colorado.phet.gravityandorbits.view;
 
 import java.awt.*;
-import java.awt.geom.Ellipse2D;
 import java.awt.geom.Point2D;
 
 import edu.colorado.phet.common.phetcommon.model.Property;
@@ -16,38 +15,42 @@ import edu.umd.cs.piccolo.PNode;
  */
 public class TraceNode extends PNode {
     private final PhetPPath phetPPath;
-    private Body body;
+    private boolean hasTraces = false;
 
     public TraceNode( final Body body, final ModelViewTransform2D transform, final Property<Boolean> visible ) {
-        this.body = body;
         phetPPath = new PhetPPath( new BasicStroke( 2 ), body.getColor() );
         addChild( phetPPath );
         visible.addObserver( new SimpleObserver() {
             public void update() {
                 setVisible( visible.getValue() );
+                if ( visible.getValue() ) {
+                    reset();
+                }
             }
         } );
-        body.getPositionProperty().addObserver( new SimpleObserver() {
-            public void update() {
-                final Body.TracePoint[] trace = body.getTrace();
-                if ( trace.length == 0 ) {
-                    clearPath();
+        body.addTraceListener( new Body.TraceListener() {
+            public void traceAdded( Body.TracePoint trace ) {
+                if ( !visible.getValue() ) {
+                    return;
+                }
+                Point2D viewPoint = transform.modelToViewDouble( trace.point.toPoint2D() );
+                if ( trace.userControlled || !hasTraces ) {
+                    phetPPath.moveTo( (float) viewPoint.getX(), (float) viewPoint.getY() );
                 }
                 else {
-                    Body.TracePoint lastTrace = trace[trace.length - 1];
-                    Point2D viewPoint = transform.modelToViewDouble( lastTrace.point.toPoint2D() );
-                    if ( lastTrace.userControlled || trace.length == 1 ) {
-                        phetPPath.moveTo( (float) viewPoint.getX(), (float) viewPoint.getY() );
-                    }
-                    else {
-                        phetPPath.lineTo( (float) viewPoint.getX(), (float) viewPoint.getY() );
-                    }
+                    phetPPath.lineTo( (float) viewPoint.getX(), (float) viewPoint.getY() );
                 }
+                hasTraces = true;
+            }
+
+            public void traceCleared() {
+                reset();
             }
         } );
     }
 
-    private void clearPath() {
+    public void reset() {
         phetPPath.reset();
+        hasTraces = false;
     }
 }
