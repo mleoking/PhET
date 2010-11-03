9ad73b2a23e76d77827432c8b764b2621d6ae3cf
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-11-03 03:47:31 +0000

    added arrow and label indicators for "to scale" mode

--- trunk/simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/view/BodyNode.java
+++ trunk/simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/view/BodyNode.java
@@ -8,13 +8,16 @@ import edu.colorado.phet.common.phetcommon.model.Property;
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 import edu.colorado.phet.common.phetcommon.view.graphics.RoundGradientPaint;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform2D;
+import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.event.CursorHandler;
+import edu.colorado.phet.common.piccolophet.nodes.ArrowNode;
 import edu.colorado.phet.common.piccolophet.nodes.SphericalNode;
 import edu.colorado.phet.gravityandorbits.model.Body;
 import edu.umd.cs.piccolo.PComponent;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
 import edu.umd.cs.piccolo.event.PInputEvent;
+import edu.umd.cs.piccolo.nodes.PText;
 
 /**
  * @author Sam Reid
@@ -23,12 +26,13 @@ public class BodyNode extends PNode {
     private ModelViewTransform2D modelViewTransform2D;
     private Body body;
     private final Property<Boolean> toScaleProperty;
+    private PNode arrowIndicator;
 
     public BodyNode( final Body body, final ModelViewTransform2D modelViewTransform2D ) {
         this( body, modelViewTransform2D, new Property<Boolean>( false ), new Property<ImmutableVector2D>( new ImmutableVector2D() ), null );
     }
 
-    public BodyNode( final Body body, final ModelViewTransform2D modelViewTransform2D, Property<Boolean> toScaleProperty, final Property<ImmutableVector2D> mousePositionProperty, final PComponent parentComponent ) {
+    public BodyNode( final Body body, final ModelViewTransform2D modelViewTransform2D, final Property<Boolean> toScaleProperty, final Property<ImmutableVector2D> mousePositionProperty, final PComponent parentComponent ) {
         this.modelViewTransform2D = modelViewTransform2D;
         this.body = body;
         this.toScaleProperty = toScaleProperty;
@@ -38,7 +42,7 @@ public class BodyNode extends PNode {
 
         final CursorHandler cursorHandler = new CursorHandler();
         addInputEventListener( cursorHandler );
-        
+
         addInputEventListener( new PBasicInputEventHandler() {
             public void mousePressed( PInputEvent event ) {
                 body.setUserControlled( true );
@@ -90,6 +94,29 @@ public class BodyNode extends PNode {
         };
         body.getDiameterProperty().addObserver( updateDiameter );
         toScaleProperty.addObserver( updateDiameter );
+
+
+        arrowIndicator = new PNode() {{
+            Point2D viewCenter = new Point2D.Double( 0, 0 );
+            ImmutableVector2D northEastVector = ImmutableVector2D.parseAngleAndMagnitude( 1, -Math.PI / 4 );
+            Point2D tip = northEastVector.getScaledInstance( 10 ).getDestination( viewCenter );
+            final Point2D tail = northEastVector.getScaledInstance( 50 ).getDestination( viewCenter );
+
+            addChild( new ArrowNode( tail, tip, 0, 0, 3 ) {{
+                setPaint( Color.yellow );
+            }} );
+            addChild( new PText( body.getName() ) {{
+                setOffset( tail.getX() - getFullBounds().getWidth() / 2 - 5, tail.getY() - getFullBounds().getHeight() - 10 );
+                setTextPaint( Color.white );
+                setFont( new PhetFont( 18, true ) );
+            }} );
+        }};
+        addChild( arrowIndicator );
+        toScaleProperty.addObserver( new SimpleObserver() {
+            public void update() {
+                arrowIndicator.setVisible( toScaleProperty.getValue() );
+            }
+        } );
     }
 
     private double getViewDiameter() {
