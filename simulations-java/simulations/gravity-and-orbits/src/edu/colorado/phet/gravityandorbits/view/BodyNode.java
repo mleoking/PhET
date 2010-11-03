b63a0c8207b44dfe7a6bafbb83d3029c5ac0e5a9
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-11-03 03:25:06 +0000

    gravity-and-orbits: changed cursor into a hand when the planet moves underneath a stationary cursor

--- simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/view/BodyNode.java
+++ simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/view/BodyNode.java
@@ -3,6 +3,7 @@ package edu.colorado.phet.gravityandorbits.view;
 import java.awt.*;
 import java.awt.geom.Point2D;
 
+import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
 import edu.colorado.phet.common.phetcommon.model.Property;
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 import edu.colorado.phet.common.phetcommon.view.graphics.RoundGradientPaint;
@@ -10,6 +11,7 @@ import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTra
 import edu.colorado.phet.common.piccolophet.event.CursorHandler;
 import edu.colorado.phet.common.piccolophet.nodes.SphericalNode;
 import edu.colorado.phet.gravityandorbits.model.Body;
+import edu.umd.cs.piccolo.PComponent;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
 import edu.umd.cs.piccolo.event.PInputEvent;
@@ -22,7 +24,11 @@ public class BodyNode extends PNode {
     private Body body;
     private final Property<Boolean> toScaleProperty;
 
-    public BodyNode( final Body body, final ModelViewTransform2D modelViewTransform2D, Property<Boolean> toScaleProperty ) {
+    public BodyNode( final Body body, final ModelViewTransform2D modelViewTransform2D ) {
+        this( body, modelViewTransform2D, new Property<Boolean>( false ), new Property<ImmutableVector2D>( new ImmutableVector2D() ), null );
+    }
+
+    public BodyNode( final Body body, final ModelViewTransform2D modelViewTransform2D, Property<Boolean> toScaleProperty, final Property<ImmutableVector2D> mousePositionProperty, final PComponent parentComponent ) {
         this.modelViewTransform2D = modelViewTransform2D;
         this.body = body;
         this.toScaleProperty = toScaleProperty;
@@ -30,7 +36,9 @@ public class BodyNode extends PNode {
         final SphericalNode sphere = new SphericalNode( getViewDiameter(), createPaint( getViewDiameter() ), false );
         addChild( sphere );
 
-        addInputEventListener( new CursorHandler() );
+        final CursorHandler cursorHandler = new CursorHandler();
+        addInputEventListener( cursorHandler );
+        
         addInputEventListener( new PBasicInputEventHandler() {
             public void mousePressed( PInputEvent event ) {
                 body.setUserControlled( true );
@@ -46,7 +54,32 @@ public class BodyNode extends PNode {
         } );
         body.getPositionProperty().addObserver( new SimpleObserver() {
             public void update() {
+                /* we need to determine whether the mouse is over the body both before and after the model change so
+                 * that we can toggle the hand pointer over the body.
+                 *
+                 * otherwise the body can move over the mouse and be dragged without ever seeing the hand pointer
+                 */
+                boolean isMouseOverBefore = BodyNode.this.getGlobalFullBounds().contains( mousePositionProperty.getValue().toPoint2D() );
                 setOffset( modelViewTransform2D.modelToView( body.getPosition() ) );
+                boolean isMouseOverAfter = BodyNode.this.getGlobalFullBounds().contains( mousePositionProperty.getValue().toPoint2D() );
+                if ( parentComponent != null ) {
+                    if ( isMouseOverBefore && !isMouseOverAfter ) {
+                        cursorHandler.mouseExited( new PInputEvent( null, null ) {
+                            @Override
+                            public PComponent getComponent() {
+                                return parentComponent;
+                            }
+                        } );
+                    }
+                    if ( !isMouseOverBefore && isMouseOverAfter ) {
+                        cursorHandler.mouseEntered( new PInputEvent( null, null ) {
+                            @Override
+                            public PComponent getComponent() {
+                                return parentComponent;
+                            }
+                        } );
+                    }
+                }
             }
         } );
         final SimpleObserver updateDiameter = new SimpleObserver() {
