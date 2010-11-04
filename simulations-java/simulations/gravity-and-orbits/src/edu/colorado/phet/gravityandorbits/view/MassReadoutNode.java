b611c0e9aec80bcf1f5a3fd0825258936646fb73
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-11-04 06:03:27 +0000

    improved text for planet mass readouts

--- simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/view/MassReadoutNode.java
+++ simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/view/MassReadoutNode.java
@@ -19,24 +19,27 @@ import edu.umd.cs.piccolo.nodes.PText;
  */
 public class MassReadoutNode extends PNode {
     public MassReadoutNode( final Body body, final ModelViewTransform2D modelViewTransform2D, final Property<Boolean> visible ) {
-        addChild( new PText( "1 million Earth masses" ) {{
+        addChild( new PText( "1 million Planet masses" ) {{
             setFont( new PhetFont( 18, true ) );
             setTextPaint( Color.white );
             body.getMassProperty().addObserver( new SimpleObserver() {
                 public void update() {
                     double massKG = body.getMass();
-                    double earthMasses = massKG / GravityAndOrbitsModel.PLANET_MASS;
-//                    System.out.println( "earthMasses = " + earthMasses );
+                    double planetMasses = massKG / GravityAndOrbitsModel.PLANET_MASS;
+//                    System.out.println( "planetMasses = " + planetMasses );
                     String text;
                     DecimalFormat decimalFormat = new DecimalFormat( "0" );
-                    if ( earthMasses > 1E3 ) {
-                        text = decimalFormat.format( earthMasses / 1E3 ) + " thousand earth masses";
+                    if ( planetMasses > 1E3 ) {
+                        text = decimalFormat.format( planetMasses / 1E3 ) + " thousand planet masses";
                     }
-                    else if ( Math.abs( earthMasses - 1 ) < 1E-2 ) {
-                        text = 1 + " earth mass";
+                    else if ( Math.abs( planetMasses - 1 ) < 1E-2 ) {
+                        text = 1 + " planet mass";
+                    }
+                    else if (planetMasses < 1){
+                        text = new DecimalFormat( "0.000").format( planetMasses )+" planet masses";
                     }
                     else {
-                        text = decimalFormat.format( earthMasses ) + " earth masses";
+                        text = decimalFormat.format( planetMasses ) + " planet masses";
                     }
                     setText( text );
                 }
