a1e8b0a55e67e0160624f27f2a9f568803707a48
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-11-05 05:39:31 +0000

    updated prototype

--- trunk/simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/simsharing/SimSharingServer.java
+++ trunk/simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/simsharing/SimSharingServer.java
@@ -1,8 +1,6 @@
 package edu.colorado.phet.gravityandorbits.simsharing;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -10,16 +8,18 @@ import java.net.Socket;
  * @author Sam Reid
  */
 public class SimSharingServer {
-    private final ServerSocket incomingSocket;
-    private final ServerSocket outgoingSocket;
+    private final ServerSocket studentServerSocket;
+    private final ServerSocket teacherServerSocket;
 
-    public static String host = "localhost";
+    public static String host = "phet-server.colorado.edu";
     public static final int STUDENT_PORT = 3752;
     public static final int TEACHER_PORT = 3753;
+    private Socket teacherSocket;
+    private BufferedWriter teacherBufferedWriter;
 
     public SimSharingServer() throws IOException {
-        incomingSocket = new ServerSocket( STUDENT_PORT );
-        outgoingSocket = new ServerSocket( TEACHER_PORT );
+        studentServerSocket = new ServerSocket( STUDENT_PORT );
+        teacherServerSocket = new ServerSocket( TEACHER_PORT );
     }
 
     public static void main( String[] args ) throws IOException {
@@ -31,7 +31,8 @@ public class SimSharingServer {
             public void run() {
                 while ( true ) {
                     try {
-                        Socket socket = incomingSocket.accept();
+                        Socket socket = studentServerSocket.accept();
+                        System.out.println( "connected to student" );
                         final BufferedReader bufferedReader = new BufferedReader( new InputStreamReader( socket.getInputStream() ) );
                         new Thread( new Runnable() {
                             public void run() {
@@ -42,6 +43,8 @@ public class SimSharingServer {
                                         if ( line == null ) {
                                             break;
                                         }
+                                        teacherBufferedWriter.write( line );
+                                        teacherBufferedWriter.flush();
                                     }
                                 }
                                 catch ( IOException e ) {
@@ -60,24 +63,25 @@ public class SimSharingServer {
             public void run() {
                 while ( true ) {
                     try {
-                        Socket socket = outgoingSocket.accept();
-                        final BufferedReader bufferedReader = new BufferedReader( new InputStreamReader( socket.getInputStream() ) );
-                        new Thread( new Runnable() {
-                            public void run() {
-                                try {
-                                    while ( true ) {
-                                        String line = bufferedReader.readLine();
-                                        System.out.println( "line = " + line );
-                                        if ( line == null ) {
-                                            break;
-                                        }
-                                    }
-                                }
-                                catch ( IOException e ) {
-                                    e.printStackTrace();
-                                }
-                            }
-                        } ).start();
+                        teacherSocket = teacherServerSocket.accept();
+                        System.out.println( "connected to teacher" );
+                        teacherBufferedWriter = new BufferedWriter( new OutputStreamWriter( teacherSocket.getOutputStream() ) );
+//                        new Thread( new Runnable() {
+//                            public void run() {
+//                                try {
+//                                    while ( true ) {
+//                                        teacherBufferedWriter.write( "hello at " + System.currentTimeMillis() );
+//                                        Thread.sleep( 1000 );
+//                                    }
+//                                }
+//                                catch ( IOException e ) {
+//                                    e.printStackTrace();
+//                                }
+//                                catch ( InterruptedException e ) {
+//                                    e.printStackTrace();
+//                                }
+//                            }
+//                        } ).start();
                     }
                     catch ( IOException e ) {
                         e.printStackTrace();
