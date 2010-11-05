b398ce3094f8cb440a445a50e9b5af4ef72ea3d3
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-11-05 19:37:43 +0000

    Created layers for the nucleons so that the nucleus looks better and, hopefully, the nucleons are easier to count.

--- simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/modules/game/view/InteractiveSchematicAtomNode.java
+++ simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/modules/game/view/InteractiveSchematicAtomNode.java
@@ -1,11 +1,23 @@
 package edu.colorado.phet.buildanatom.modules.game.view;
 
-import java.awt.*;
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Shape;
+import java.util.ArrayList;
+import java.util.Collections;
+
 import edu.colorado.phet.buildanatom.model.BuildAnAtomModel;
 import edu.colorado.phet.buildanatom.model.ElectronShell;
+import edu.colorado.phet.buildanatom.model.Neutron;
 import edu.colorado.phet.buildanatom.model.Proton;
+import edu.colorado.phet.buildanatom.model.SubatomicParticle;
 import edu.colorado.phet.buildanatom.modules.game.model.AtomValue;
-import edu.colorado.phet.buildanatom.view.*;
+import edu.colorado.phet.buildanatom.view.BucketNode;
+import edu.colorado.phet.buildanatom.view.ElectronNode;
+import edu.colorado.phet.buildanatom.view.ElectronShellNode;
+import edu.colorado.phet.buildanatom.view.NeutronNode;
+import edu.colorado.phet.buildanatom.view.ProtonNode;
+import edu.colorado.phet.buildanatom.view.SubatomicParticleNode;
 import edu.colorado.phet.common.phetcommon.model.BooleanProperty;
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform2D;
@@ -18,30 +30,38 @@ import edu.umd.cs.piccolo.PNode;
  * @author Sam Reid
  */
 public class InteractiveSchematicAtomNode extends PNode {
+    // The number of layers upon which particles can be placed.  The number
+    // was determined empirically and can be adjusted if needed.
+    private final static int NUM_NUCLEUS_LAYERS = 8;
+
+    // Reference to the model.
     private final BuildAnAtomModel model;
 
+    // Particle layers.  These exist so that we can give the nucleus a faux
+    // 3D sort of look, with the particles toward the center of the nucleus
+    // on top and those at the edges on the bottom.
+    private final ArrayList<PNode> nucleusLayers = new ArrayList<PNode>( NUM_NUCLEUS_LAYERS );
+
+    /**
+     * Constructor.
+     */
     public InteractiveSchematicAtomNode( final BuildAnAtomModel model, ModelViewTransform2D mvt, final BooleanProperty viewOrbitals ) {
         this.model=model;
 
         PNode backLayer = new PNode( );
-        PNode particleLayer = new PNode( );
-        PNode frontLayer= new PNode( );
-        // Layers on the canvas.
         addChild( backLayer );
-        addChild( particleLayer );
+        PNode electronLayer = new PNode( );
+        addChild( electronLayer );
+        for (int i = 0; i < NUM_NUCLEUS_LAYERS; i++){
+            PNode particleLayer = new PNode();
+            addChild( particleLayer );
+            nucleusLayers.add( particleLayer );
+        }
+        Collections.reverse( nucleusLayers ); // So that lower index values are higher layers.
+        PNode frontLayer= new PNode( );
         addChild( frontLayer );
 
-        // Put up the bounds of the model.
-        //        rootNode.addChild( new PhetPPath( mvt.createTransformedShape( model.getModelViewport() ), Color.PINK, new BasicStroke( 3f ), Color.BLACK ) );
-
         // Add the atom's nucleus location to the canvas.
-        //        Shape nucleusOutlineShape = mvt.createTransformedShape( new Ellipse2D.Double(
-        //                -model.getAtom().getNucleusRadius(),
-        //                -model.getAtom().getNucleusRadius(),
-        //                model.getAtom().getNucleusRadius() * 2,
-        //                model.getAtom().getNucleusRadius() * 2 ) );
-        //        PNode nucleusOutlineNode = new PhetPPath( nucleusOutlineShape, new BasicStroke(1f), Color.RED );
-        //        backLayer.addChild( nucleusOutlineNode );
         DoubleGeneralPath nucleusXMarkerModelCoords = new DoubleGeneralPath();
         double xMarkerSize = Proton.RADIUS; // Arbitrary, adjust as desired.
         nucleusXMarkerModelCoords.moveTo( model.getAtom().getPosition().getX() - xMarkerSize / 2,
@@ -75,10 +95,10 @@ public class InteractiveSchematicAtomNode extends PNode {
         backLayer.addChild( neutronBucketNode.getHoleLayer() );
         frontLayer.addChild( neutronBucketNode.getContainerLayer() );
 
-        // Add the subatomic particles.
+        // Add the electrons.
         for ( int i = 0; i < model.numElectrons(); i++ ) {
             final int finalI = i;
-            particleLayer.addChild( new ElectronNode( mvt, model.getElectron( i ) ){{
+            electronLayer.addChild( new ElectronNode( mvt, model.getElectron( i ) ){{
                 final SimpleObserver updateVisibility = new SimpleObserver() {
                     public void update() {
                         setVisible( viewOrbitals.getValue() || !model.getAtom().containsElectron( model.getElectron( finalI ) ) );
@@ -91,13 +111,31 @@ public class InteractiveSchematicAtomNode extends PNode {
             }} );
         }
 
-        for ( int i = 0; i < Math.max( model.numProtons(), model.numNeutrons() ); i++ ) {
-            if ( i < model.numProtons() ) {
-                particleLayer.addChild( new ProtonNode( mvt, model.getProton( i ) ) );
-            }
-            if ( i < model.numNeutrons() ) {
-                particleLayer.addChild( new NeutronNode( mvt, model.getNeutron( i ) ) );
-            }
+        // Add the nucleons.
+        for ( int i = 0; i < model.numProtons(); i++ ) {
+            final Proton proton = model.getProton( i );
+            final ProtonNode protonNode = new ProtonNode( mvt, proton );
+            nucleusLayers.get( mapNucleonToLayerNumber( proton ) ).addChild( protonNode );
+            proton.addPositionListener( new SimpleObserver() {
+                public void update() {
+                    if ( !proton.isUserControlled() ){
+                        updateNucleonLayer( proton, protonNode );
+                    }
+                }
+            });
+        }
+        for ( int i = 0; i < model.numNeutrons(); i++ ) {
+            final Neutron neutron = model.getNeutron( i );
+            nucleusLayers.get( mapNucleonToLayerNumber( neutron ) ).addChild( new NeutronNode( mvt, neutron ) );
+            final NeutronNode neutronNode = new NeutronNode( mvt, neutron );
+            nucleusLayers.get( mapNucleonToLayerNumber( neutron ) ).addChild( neutronNode );
+            neutron.addPositionListener( new SimpleObserver() {
+                public void update() {
+                    if ( !neutron.isUserControlled() ){
+                        updateNucleonLayer( neutron, neutronNode );
+                    }
+                }
+            });
         }
     }
 
@@ -107,5 +145,65 @@ public class InteractiveSchematicAtomNode extends PNode {
 
     public void displayAnswer( AtomValue answer ) {
         model.setState(answer);
+    }
+
+    /**
+     * Decide which of the layers this nucleon should be based on its
+     * location.  Atoms close to the center of the nucleus are on the more
+     * forward layers, further out are further back.
+     *
+     * @param nucleon
+     * @return
+     */
+    private int mapNucleonToLayerNumber( SubatomicParticle nucleon ){
+        // Note: This algorithm for layering was made up to look reasonable,
+        // and can be modified as needed to produce better looking nuclei.
+        double maxNucleusRadius = Neutron.RADIUS * 6;
+        double distanceFromCenter = nucleon.getPosition().distance( model.getAtom().getPosition() );
+        return Math.min( (int)Math.floor( distanceFromCenter / (maxNucleusRadius / NUM_NUCLEUS_LAYERS )), NUM_NUCLEUS_LAYERS - 1 );
+    }
+
+    /**
+     * Update the layer upon which this nucleon's representation resides.  If
+     * the nucleon is already on the correct layer, this will have no effect.
+     *
+     * @param nucleon
+     * @param nucleonNode
+     */
+    private void updateNucleonLayer( SubatomicParticle nucleon, SubatomicParticleNode nucleonNode ){
+        int currentLayerNumber = getNucleusLayerIndex( nucleonNode );
+        int targetLayerNumber = mapNucleonToLayerNumber( nucleon );
+        if (currentLayerNumber != targetLayerNumber){
+            System.out.println("Moving node from layer " + currentLayerNumber + " to layer " + targetLayerNumber);
+            removeNucleonNodeFromLayers( nucleonNode );
+            nucleusLayers.get( targetLayerNumber ).addChild( nucleonNode );
+        }
+    }
+
+    /**
+     * Get the index of the nucleus layer on which the given node resides.
+     *
+     * @param nucleonNode
+     * @return
+     */
+    private int getNucleusLayerIndex( SubatomicParticleNode nucleonNode ){
+        int index;
+        for (index = 0; index < NUM_NUCLEUS_LAYERS; index++){
+            if ( nucleusLayers.get( index ).isAncestorOf( nucleonNode )){
+                break;
+            }
+        }
+        return index;
+    }
+
+    private boolean removeNucleonNodeFromLayers( SubatomicParticleNode nucleonNode ){
+        boolean found = false;
+        for (PNode nucleusLayer : nucleusLayers){
+            if ( nucleusLayer.removeChild( nucleonNode ) != null){
+                found = true;
+                break;
+            }
+        }
+        return found;
     }
 }
