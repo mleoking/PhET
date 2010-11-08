451a63e7cbe9eba01f9103a5248ec65dad987be4
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-11-08 17:27:31 +0000

    updated work module constructor, was broken

--- trunk/simulations-java/simulations/work-energy/src/edu/colorado/phet/workenergy/module/WorkModule.java
+++ trunk/simulations-java/simulations/work-energy/src/edu/colorado/phet/workenergy/module/WorkModule.java
@@ -1,6 +1,5 @@
 package edu.colorado.phet.workenergy.module;
 
-import edu.colorado.phet.common.phetcommon.model.clock.IClock;
 import edu.colorado.phet.common.phetcommon.view.PhetFrame;
 import edu.colorado.phet.workenergy.model.WorkEnergyModel;
 
@@ -8,7 +7,7 @@ import edu.colorado.phet.workenergy.model.WorkEnergyModel;
  * @author Sam Reid
  */
 public class WorkModule extends WorkEnergyModule<WorkEnergyModel> {
-    public WorkModule( PhetFrame phetFrame, IClock clock ) {
-        super( phetFrame, "Work", new WorkEnergyModel( clock ) );
+    public WorkModule( PhetFrame phetFrame ) {
+        super( phetFrame, "Work", new WorkEnergyModel() );
     }
 }
