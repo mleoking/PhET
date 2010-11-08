caf46191acb8530b0efcf90cee5e7ee709faeb8f
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-11-08 07:54:45 +0000

    added zoom buttons to bar chart

--- trunk/simulations-java/simulations/work-energy/src/edu/colorado/phet/workenergy/view/WorkEnergyBarGraphNode.java
+++ trunk/simulations-java/simulations/work-energy/src/edu/colorado/phet/workenergy/view/WorkEnergyBarGraphNode.java
@@ -12,22 +12,27 @@ import edu.umd.cs.piccolo.PNode;
 /**
  * @author Sam Reid
  */
-public class BarGraphNode extends PNode {
-    public BarGraphNode( final Property<Boolean> visibleProperty, final WorkEnergyObject object ) {
+public class WorkEnergyBarGraphNode extends PNode {
+    public WorkEnergyBarGraphNode( final Property<Boolean> visibleProperty, final WorkEnergyObject object ) {
         visibleProperty.addObserver( new SimpleObserver() {
             public void update() {
                 setVisible( visibleProperty.getValue() );
             }
         } );
 
-        BarChartNode barGraph = new BarChartNode( "Energy (J)", 0.3, Color.white );
+        BarChartNode barGraph = new BarChartNode( "Energy (J)", 0.3, new Color( 0, 0, 0, 0 ) );
         barGraph.init( new BarChartNode.Variable[] {
                 new WorkEnergyBarValue( "Kinetic", object.getKineticEnergyProperty(), PhetColorScheme.KINETIC_ENERGY, barGraph ),
                 new WorkEnergyBarValue( "Potential", object.getPotentialEnergyProperty(), PhetColorScheme.POTENTIAL_ENERGY, barGraph ),
                 new WorkEnergyBarValue( "Thermal", object.getThermalEnergyProperty(), PhetColorScheme.HEAT_THERMAL_ENERGY, barGraph ),
                 new WorkEnergyBarValue( "Total", object.getTotalEnergyProperty(), PhetColorScheme.TOTAL_ENERGY, barGraph )
         } );
+        barGraph.scale( 1.2 );
         addChild( barGraph );
+        final VerticalZoomButtons verticalZoomButtons = new VerticalZoomButtons();
+        addChild( verticalZoomButtons );
+        barGraph.translate( verticalZoomButtons.getFullBounds().getWidth() + 2, 0 );
+        verticalZoomButtons.setOffset( 0, barGraph.getFullBounds().getCenterY() - verticalZoomButtons.getFullBounds().getHeight() / 2 );
     }
 
     public static class WorkEnergyBarValue extends BarChartNode.Variable {
