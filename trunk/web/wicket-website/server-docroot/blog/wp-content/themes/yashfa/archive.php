95ccf2ba3b6887329e0501a323ea813bbc96ea47
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-11-09 00:04:26 +0000

    blog: refactored social icons code into one place

--- trunk/web/wicket-website/server-docroot/blog/wp-content/themes/yashfa/archive.php
+++ trunk/web/wicket-website/server-docroot/blog/wp-content/themes/yashfa/archive.php
@@ -1,4 +1,4 @@
-<?php get_header( ); ?>
+<?php get_header( ); include 'socialicons.php'; ?>
 
 <div id="content-wrap">
     <div id="content">
@@ -57,57 +57,7 @@
                         <?php the_tags( __( 'Tags: ', 'yashfa' ), ', ', '<br />' ); ?>
                         </div>
 
-                        <div class="social-sub-div">
-                            <a href="http://www.facebook.com/sharer.php?u=<?php echo urlencode( get_permalink( ) ); ?>&amp;t=<?php echo urlencode( get_the_title( ) ); ?>"
-                               style="padding: 0; margin: 0; text-decoration: none;" target="_blank"
-                               title="Share this on Facebook">
-
-                                <img src="/files/icons/social/16/facebook.png" alt="Social Media Icon"
-                                     style="border: none;"/>
-                            </a>
-                        </div>
-                        <div class="social-sub-div">
-                            <a href="https://twitter.com/share?url=<?php echo urlencode( get_permalink( ) ); ?>&amp;text=<?php echo urlencode( get_the_title( ) ); ?>"
-                               style="padding: 0; margin: 0; text-decoration: none;" target="_blank"
-                               title="Share this on Twitter">
-                                <img src="/files/icons/social/16/twitter.png" alt="Social Media Icon"
-                                     style="border: none;"/>
-                            </a>
-                        </div>
-                        <div class="social-sub-div">
-                            <a href="http://www.stumbleupon.com/submit?url=<?php echo urlencode( get_permalink( ) ); ?>&amp;title=<?php echo urlencode( get_the_title( ) ); ?>"
-                               style="padding: 0; margin: 0; text-decoration: none;" target="_blank"
-                               title="Share this on Stumble Upon">
-                                <img src="/files/icons/social/16/stumbleupon.png"
-                                     alt="Social Media Icon" style="border: none;"/>
-
-                            </a>
-                        </div>
-                        <div class="social-sub-div">
-                            <a href="http://digg.com/submit?phase=2&amp;url=<?php echo urlencode( get_permalink( ) ); ?>&amp;title=<?php echo urlencode( get_the_title( ) ); ?>"
-                               style="padding: 0; margin: 0; text-decoration: none;" target="_blank"
-                               title="Share this on Digg">
-                                <img src="/files/icons/social/16/digg.png" alt="Social Media Icon"
-                                     style="border: none;"/>
-                            </a>
-                        </div>
-                        <div class="social-sub-div">
-                            <a href="http://reddit.com/submit?url=<?php echo urlencode( get_permalink( ) ); ?>&amp;title=<?php echo urlencode( get_the_title( ) ); ?>"
-                               style="padding: 0; margin: 0; text-decoration: none;" target="_blank"
-                               title="Share this on Reddit">
-                                <img src="/files/icons/social/16/reddit.png" alt="Social Media Icon"
-                                     style="border: none;"/>
-                            </a>
-
-                        </div>
-                        <div class="social-sub-div">
-                            <a href="http://del.icio.us/post?url=<?php echo urlencode( get_permalink( ) ); ?>&amp;title=<?php echo urlencode( get_the_title( ) ); ?>"
-                               style="padding: 0; margin: 0; text-decoration: none;" target="_blank"
-                               title="Share this on Delicious">
-                                <img src="/files/icons/social/16/delicious.png" alt="Social Media Icon"
-                                     style="border: none;"/>
-                            </a>
-                        </div>
+                        <?php echo show_social_media_icons( ); ?>
 
                         <div class="new-comments-area">
                         <?php comments_popup_link( __( '0 Comments', 'yashfa' ), __( '1 Comment', 'yashfa' ), __( '% Comments', 'yashfa' ) ); ?>
