9a8160591c2c6b446e4f3fc8faed821da4118783
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-11-09 05:23:18 +0000

    fixed defaults to spec values

--- trunk/simulations-java/simulations/work-energy/src/edu/colorado/phet/workenergy/module/WorkEnergyModule.java
+++ trunk/simulations-java/simulations/work-energy/src/edu/colorado/phet/workenergy/module/WorkEnergyModule.java
@@ -15,8 +15,8 @@ import edu.colorado.phet.workenergy.view.WorkEnergyCanvas;
 public class WorkEnergyModule<ModelType extends WorkEnergyModel> extends Module {
     private ModelType model;
     private Property<Boolean> showPieChartProperty = new Property<Boolean>( true );
-    private Property<Boolean> showEnergyBarChartProperty = new Property<Boolean>( true);
-    private Property<Boolean> showRulerProperty = new Property<Boolean>( true);
+    private Property<Boolean> showEnergyBarChartProperty = new Property<Boolean>( false );
+    private Property<Boolean> showRulerProperty = new Property<Boolean>( false );
 
     static final int delay = 30;//milliseconds between clock ticks
     static final double delaySec = delay / 1000.0;
