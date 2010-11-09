eff8392f9457fab12e228f3965783bbde213003a
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-11-09 05:23:37 +0000

    reformate; optimize imports

--- trunk/simulations-java/simulations/work-energy/src/edu/colorado/phet/workenergy/module/EnergyModule.java
+++ trunk/simulations-java/simulations/work-energy/src/edu/colorado/phet/workenergy/module/EnergyModule.java
@@ -1,6 +1,5 @@
 package edu.colorado.phet.workenergy.module;
 
-import edu.colorado.phet.common.phetcommon.model.clock.IClock;
 import edu.colorado.phet.common.phetcommon.view.PhetFrame;
 import edu.colorado.phet.workenergy.model.WorkEnergyModel;
 
@@ -8,7 +7,7 @@ import edu.colorado.phet.workenergy.model.WorkEnergyModel;
  * @author Sam Reid
  */
 public class EnergyModule extends WorkEnergyModule<WorkEnergyModel> {
-    public EnergyModule( PhetFrame phetFrame) {
-        super( phetFrame, "Energy", new WorkEnergyModel(  ) );
+    public EnergyModule( PhetFrame phetFrame ) {
+        super( phetFrame, "Energy", new WorkEnergyModel() );
     }
 }
