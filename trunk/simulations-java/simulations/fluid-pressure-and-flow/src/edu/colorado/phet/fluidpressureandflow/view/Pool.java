8ecf0d0821b332c71e43f6894194381d17697141
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-11-12 08:30:51 +0000

    fixed dragging to ensure the probe stays within range

--- trunk/simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/view/Pool.java
+++ trunk/simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/view/Pool.java
@@ -8,7 +8,7 @@ import java.awt.geom.Rectangle2D;
  */
 public class Pool {
     int height = 5;
-    int width = 2;
+    int width = 4;
 
     public Pool() {
     }
@@ -22,10 +22,18 @@ public class Pool {
     }
 
     public Shape getShape() {
-        return new Rectangle2D.Double( -width, -height, width * 2, height );
+        return new Rectangle2D.Double( -width / 2, -height, width, height );
     }
 
     public double getLiquidDensity() {
         return 1000; // kg/m^3
+    }
+
+    public double getMinX() {
+        return getShape().getBounds2D().getMinX();
+    }
+
+    public double getMaxX() {
+        return getShape().getBounds2D().getMaxX();
     }
 }
