e7f7751f552d39fa0f02295b9788cbe6213aaa71
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-11-12 01:09:34 +0000

    format

--- trunk/simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/model/Wire.java
+++ trunk/simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/model/Wire.java
@@ -26,22 +26,22 @@ import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
 public class Wire {
-    
+
     private final ArrayList<WireSegment> wireSegments;
     private final double thickness;
-    
+
     private final Property<Shape> shapeProperty;
     private final Property<Double> voltageProperty;
 
     public Wire( ArrayList<WireSegment> wireSegments, double thickness ) {
-        assert( wireSegments != null && wireSegments.size() > 0 );
-        assert( thickness > 0 );
-        
+        assert ( wireSegments != null && wireSegments.size() > 0 );
+        assert ( thickness > 0 );
+
         this.thickness = thickness;
         this.wireSegments = new ArrayList<WireSegment>( wireSegments );
         this.shapeProperty = new Property<Shape>( createShape() );
         this.voltageProperty = new Property<Double>( 0.0 );
-        
+
         // when any segment changes, update the shape property
         {
             SimpleObserver o = new SimpleObserver() {
@@ -55,27 +55,27 @@ public class Wire {
             }
         }
     }
-    
+
     public double getThickness() {
         return thickness;
     }
-    
+
     public double getVoltage() {
         return voltageProperty.getValue();
     }
-    
+
     public void setVoltage( double voltage ) {
         voltageProperty.setValue( voltage );
     }
-    
+
     public void addShapeObserver( SimpleObserver o ) {
         shapeProperty.addObserver( o );
     }
-    
+
     public Shape getShape() {
         return shapeProperty.getValue();
     }
-    
+
     private Shape createShape() {
         Area area = new Area();
         for ( WireSegment wireSegment : wireSegments ) {
@@ -84,18 +84,19 @@ public class Wire {
         return area;
     }
 
-    public boolean containsPoint(Point3D pt){
-        return shapeProperty.getValue().contains( pt.getX(),pt.getY() );
+    public boolean containsPoint( Point3D pt ) {
+        return shapeProperty.getValue().contains( pt.getX(), pt.getY() );
     }
-    
+
     /**
      * Wire that connects the tops of a battery and capacitor.
      */
     public static class TopWire extends Wire {
+
         public TopWire( final Battery battery, final Capacitor capacitor, double thickness ) {
             super( createSegments( battery, capacitor ), thickness );
         }
-        
+
         private static ArrayList<WireSegment> createSegments( final Battery battery, final Capacitor capacitor ) {
             final Point2D.Double leftCorner = new Point2D.Double( battery.getX(), battery.getY() - CLConstants.WIRE_EXTENT );
             final Point2D.Double rightCorner = new Point2D.Double( capacitor.getX(), leftCorner.getY() );
@@ -107,15 +108,16 @@ public class Wire {
             return segments;
         }
     }
-    
+
     /**
      * Wire that connects the bottoms of a battery and capacitor.
      */
     public static class BottomWire extends Wire {
+
         public BottomWire( final Battery battery, final Capacitor capacitor, double thickness ) {
             super( createSegments( battery, capacitor ), thickness );
         }
-        
+
         private static ArrayList<WireSegment> createSegments( final Battery battery, final Capacitor capacitor ) {
             final Point2D.Double leftCorner = new Point2D.Double( battery.getX(), battery.getY() + CLConstants.WIRE_EXTENT );
             final Point2D.Double rightCorner = new Point2D.Double( capacitor.getX(), leftCorner.getY() );
