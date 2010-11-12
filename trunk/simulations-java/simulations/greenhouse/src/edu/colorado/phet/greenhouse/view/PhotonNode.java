698d988d52254667b725ca4849d9840e39164afc
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-11-12 23:50:41 +0000

    Incorporated the PhotonImageFactory to generate the photon images.

--- trunk/simulations-java/simulations/greenhouse/src/edu/colorado/phet/greenhouse/view/PhotonNode.java
+++ trunk/simulations-java/simulations/greenhouse/src/edu/colorado/phet/greenhouse/view/PhotonNode.java
@@ -2,20 +2,20 @@
 
 package edu.colorado.phet.greenhouse.view;
 
+import java.util.HashMap;
 import java.util.Observable;
 import java.util.Observer;
-import java.util.Random;
 
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform2D;
+import edu.colorado.phet.common.piccolophet.util.PhotonImageFactory;
 import edu.colorado.phet.greenhouse.GreenhouseConfig;
-import edu.colorado.phet.greenhouse.GreenhouseResources;
 import edu.colorado.phet.greenhouse.model.Photon;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.nodes.PImage;
 
 /**
  * PNode that represents a photon in the view.
- * 
+ *
  * @author John Blanco
  */
 public class PhotonNode extends PNode implements Observer {
@@ -24,30 +24,36 @@ public class PhotonNode extends PNode implements Observer {
     // Class Data
     // ------------------------------------------------------------------------
 
+    // Map of photon wavelengths to visual images used for representing them.
+    private static final HashMap<Double, String> mapWavelengthToImageName = new HashMap<Double, String>(){{
+        put( GreenhouseConfig.microWavelength, "microwave-photon.png");
+        put( GreenhouseConfig.irWavelength, "photon-660.png");
+        put( GreenhouseConfig.sunlightWavelength, "photon-575.png");
+        put( GreenhouseConfig.uvWavelength, "photon-100.png");
+    }};
+
 	// ------------------------------------------------------------------------
 	// Instance Data
 	// ------------------------------------------------------------------------
-	
-	private PImage photonImage;
-	private Photon photon;  // Model element represented by this node.
-	private ModelViewTransform2D mvt;
-	
+
+	private final PImage photonImage;
+	private final Photon photon;  // Model element represented by this node.
+	private final ModelViewTransform2D mvt;
+
 	// ------------------------------------------------------------------------
 	// Constructor(s)
 	// ------------------------------------------------------------------------
-	
+
 	public PhotonNode(Photon photon, ModelViewTransform2D mvt) {
-		
+
 		this.photon = photon;
 		this.photon.addObserver(this);
 		this.mvt = mvt;
-		
-		if (photon.getWavelength() == GreenhouseConfig.irWavelength){
-			photonImage = new PImage(GreenhouseResources.getImage("photon-660.png"));
-		}
-		else{
-			photonImage = new PImage(GreenhouseResources.getImage("thin2.png"));
-		}
+
+		assert mapWavelengthToImageName.containsKey( photon.getWavelength() );
+		// TODO: Remove permanently if accepted.
+//        photonImage = new PImage( GreenhouseResources.getImage( mapWavelengthToImageName.get( photon.getWavelength() ) ) );
+        photonImage = new PImage( PhotonImageFactory.lookupPhotonImage( photon.getWavelength() * 1E9, 35 ) );
 		photonImage.setOffset( -photonImage.getFullBoundsReference().width / 2,
 		        -photonImage.getFullBoundsReference().height / 2 );
 		addChild(photonImage);
@@ -57,15 +63,15 @@ public class PhotonNode extends PNode implements Observer {
 	// ------------------------------------------------------------------------
 	// Methods
 	// ------------------------------------------------------------------------
-	
+
 	public void update(Observable o, Object arg) {
 		updatePosition();
 	}
-	
+
 	private void updatePosition(){
 		setOffset(mvt.modelToViewDouble(photon.getLocation()));
 	}
-	
+
 	// ------------------------------------------------------------------------
 	// Inner Classes and Interfaces
 	//------------------------------------------------------------------------
