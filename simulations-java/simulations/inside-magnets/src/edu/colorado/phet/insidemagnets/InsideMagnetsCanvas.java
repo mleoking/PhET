a792c89535303258d8ada9d5b30c882e86815b6f
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-11-15 23:57:03 +0000

    started adding application and modules

---
+++ simulations-java/simulations/inside-magnets/src/edu/colorado/phet/insidemagnets/InsideMagnetsCanvas.java
@@ -0,0 +1,11 @@
+package edu.colorado.phet.insidemagnets;
+
+import javax.swing.*;
+
+import edu.colorado.phet.common.piccolophet.PhetPCanvas;
+
+/**
+ * @author Sam Reid
+ */
+public class InsideMagnetsCanvas extends PhetPCanvas {
+}
