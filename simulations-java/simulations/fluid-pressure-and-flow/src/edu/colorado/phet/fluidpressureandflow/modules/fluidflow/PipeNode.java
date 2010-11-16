2e52a2454aa90b9901496decda791cf08d58a1b1
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-11-16 06:50:56 +0000

    Hid pipe entry points to left and right for when sim is shortened vertically

--- simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/modules/fluidflow/PipeNode.java
+++ simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/modules/fluidflow/PipeNode.java
@@ -2,6 +2,7 @@ package edu.colorado.phet.fluidpressureandflow.modules.fluidflow;
 
 import java.awt.*;
 import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform2D;
@@ -20,13 +21,28 @@ import edu.umd.cs.piccolo.util.PDimension;
  */
 public class PipeNode extends PNode {
     public PipeNode( final ModelViewTransform2D transform, final Pipe pipe ) {
+        //Hide the leftmost and rightmost parts as if water is coming from a gray pipe and leaving through a gray pipe
+        final PhetPPath leftCoupling = new PhetPPath( Color.gray );
+        final PhetPPath rightCoupling = new PhetPPath( Color.gray );
         addChild( new PhetPPath( Color.blue, new BasicStroke( 1 ), Color.black ) {{
             pipe.addShapeChangeListener( new SimpleObserver() {
                 public void update() {
                     setPathTo( transform.createTransformedShape( pipe.getShape() ) );
+
+                    double length = 10000;
+                    final Point2D topLeft = transform.modelToViewDouble( pipe.getTopLeft() );
+                    final Point2D bottomLeft = transform.modelToViewDouble( pipe.getBottomLeft() );
+                    leftCoupling.setPathTo( new Rectangle2D.Double( topLeft.getX() - length, topLeft.getY(), length, bottomLeft.getY() - topLeft.getY() ) );
+
+                    final Point2D topRight = transform.modelToViewDouble( pipe.getTopRight() );
+                    final Point2D bottomRight = transform.modelToViewDouble( pipe.getBottomRight() );
+                    rightCoupling.setPathTo( new Rectangle2D.Double( topRight.getX(), topRight.getY(), length, bottomRight.getY() - topRight.getY() ) );
                 }
             } );
         }} );
+        //Left coupling
+        addChild( leftCoupling );
+        addChild( rightCoupling );
         for ( PipePosition pipePosition : pipe.getPipePositions() ) {
             addChild( new PipePositionControl( transform, pipePosition ) );
         }
@@ -37,7 +53,7 @@ public class PipeNode extends PNode {
 //            double grabHandleRadius = 10;
 //            addChild( new PhetPPath( new Ellipse2D.Double( -grabHandleRadius, -grabHandleRadius, grabHandleRadius * 2, grabHandleRadius * 2 ), Color.green ) {{
             double arrowLength = 20;
-            addChild( new DoubleArrowNode( new Point2D.Double( 0, -arrowLength ), new Point2D.Double( 0, arrowLength ), 16, 16, 8) {{
+            addChild( new DoubleArrowNode( new Point2D.Double( 0, -arrowLength ), new Point2D.Double( 0, arrowLength ), 16, 16, 8 ) {{
                 setPaint( Color.green );
                 setStroke( new BasicStroke( 1 ) );
                 setStrokePaint( Color.black );
