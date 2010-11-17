a85d86aec5fd4ebe77ba27eb4575b06f2e848f38
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-11-17 16:35:33 +0000

    factored out ruler implementation

--- trunk/simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/view/EnglishRuler.java
+++ trunk/simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/view/EnglishRuler.java
@@ -1,44 +1,15 @@
 package edu.colorado.phet.fluidpressureandflow.view;
 
-import java.awt.geom.Point2D;
-
 import edu.colorado.phet.common.phetcommon.model.Property;
-import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform2D;
-import edu.colorado.phet.common.piccolophet.event.CursorHandler;
-import edu.colorado.phet.common.piccolophet.nodes.RulerNode;
 import edu.colorado.phet.fluidpressureandflow.model.Pool;
 import edu.colorado.phet.fluidpressureandflow.model.Units;
-import edu.umd.cs.piccolo.PNode;
-import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
-import edu.umd.cs.piccolo.event.PInputEvent;
-import edu.umd.cs.piccolo.util.PDimension;
 
 /**
  * @author Sam Reid
  */
-public class EnglishRuler extends PNode {
+public class EnglishRuler extends FluidPressureAndFlowRuler {
     public EnglishRuler( ModelViewTransform2D transform, Pool pool, final Property<Boolean> visible ) {
-        visible.addObserver( new SimpleObserver() {
-            public void update() {
-                setVisible( visible.getValue() );
-            }
-        } );
-        Point2D rulerModelOrigin = new Point2D.Double( pool.getMinX(), pool.getMinY() );
-        final RulerNode rulerNode = new RulerNode( Math.abs( transform.modelToViewDifferentialYDouble( Units.FEET.toSI(10 )) ),
-                                                   50, new String[] { "0", "1", "2", "3","4","5","6","7","8","9","10"}, "ft", 4, 18 );
-        rulerNode.rotate( -Math.PI / 2 );
-        rulerNode.setOffset( transform.modelToViewXDouble( rulerModelOrigin.getX() ),
-                             transform.modelToViewYDouble( rulerModelOrigin.getY() ) + rulerNode.getInsetWidth() );
-        addChild( rulerNode );
-
-        addInputEventListener( new CursorHandler() );
-        addInputEventListener( new PBasicInputEventHandler() {
-            @Override
-            public void mouseDragged( PInputEvent event ) {
-                PDimension delta = event.getDeltaRelativeTo( getParent() );
-                translate( delta.width, delta.height );
-            }
-        } );
+        super( transform, pool, visible, Math.abs( transform.modelToViewDifferentialYDouble( Units.FEET.toSI( 10 ) ) ), new String[] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" }, "ft" );
     }
 }
---
+++ trunk/simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/view/FluidPressureAndFlowRuler.java
@@ -0,0 +1,43 @@
+package edu.colorado.phet.fluidpressureandflow.view;
+
+import java.awt.geom.Point2D;
+
+import edu.colorado.phet.common.phetcommon.model.Property;
+import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
+import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform2D;
+import edu.colorado.phet.common.piccolophet.event.CursorHandler;
+import edu.colorado.phet.common.piccolophet.nodes.RulerNode;
+import edu.colorado.phet.fluidpressureandflow.model.Pool;
+import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
+import edu.umd.cs.piccolo.event.PInputEvent;
+import edu.umd.cs.piccolo.util.PDimension;
+
+/**
+ * @author Sam Reid
+ */
+public class FluidPressureAndFlowRuler extends PNode {
+    public FluidPressureAndFlowRuler( ModelViewTransform2D transform, Pool pool, final Property<Boolean> visible,double length,String[]majorTicks,String units) {
+        visible.addObserver( new SimpleObserver() {
+            public void update() {
+                setVisible( visible.getValue() );
+            }
+        } );
+        Point2D rulerModelOrigin = new Point2D.Double( pool.getMinX(), pool.getMinY() );
+        final RulerNode rulerNode = new RulerNode( length,
+                                                   50, majorTicks, units, 4, 18 );
+        rulerNode.rotate( -Math.PI / 2 );
+        rulerNode.setOffset( transform.modelToViewXDouble( rulerModelOrigin.getX() ),
+                             transform.modelToViewYDouble( rulerModelOrigin.getY() ) + rulerNode.getInsetWidth() );
+        addChild( rulerNode );
+
+        addInputEventListener( new CursorHandler() );
+        addInputEventListener( new PBasicInputEventHandler() {
+            @Override
+            public void mouseDragged( PInputEvent event ) {
+                PDimension delta = event.getDeltaRelativeTo( getParent() );
+                translate( delta.width, delta.height );
+            }
+        } );
+    }
+}
