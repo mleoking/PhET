05215993c455c64e901297f2ed507bfffbbd0b80
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-11-17 18:55:01 +0000

    Fluid-flow: Added a ruler

--- simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/view/EnglishRuler.java
+++ simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/view/EnglishRuler.java
@@ -1,15 +1,16 @@
 package edu.colorado.phet.fluidpressureandflow.view;
 
+import java.awt.geom.Point2D;
+
 import edu.colorado.phet.common.phetcommon.model.Property;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform2D;
-import edu.colorado.phet.fluidpressureandflow.model.Pool;
 import edu.colorado.phet.fluidpressureandflow.model.Units;
 
 /**
  * @author Sam Reid
  */
 public class EnglishRuler extends FluidPressureAndFlowRuler {
-    public EnglishRuler( ModelViewTransform2D transform, Pool pool, final Property<Boolean> visible ) {
-        super( transform, pool, visible, Math.abs( transform.modelToViewDifferentialYDouble( Units.FEET.toSI( 10 ) ) ), new String[] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" }, "ft" );
+    public  EnglishRuler( ModelViewTransform2D transform, final Property<Boolean> visible, Point2D.Double rulerModelOrigin ) {
+        super( transform, visible, Math.abs( transform.modelToViewDifferentialYDouble( Units.FEET.toSI( 10 ) ) ), new String[] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" }, "ft", rulerModelOrigin );
     }
 }
--- simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/view/MeterStick.java
+++ simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/view/MeterStick.java
@@ -1,14 +1,15 @@
 package edu.colorado.phet.fluidpressureandflow.view;
 
+import java.awt.geom.Point2D;
+
 import edu.colorado.phet.common.phetcommon.model.Property;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform2D;
-import edu.colorado.phet.fluidpressureandflow.model.Pool;
 
 /**
  * @author Sam Reid
  */
 public class MeterStick extends FluidPressureAndFlowRuler {
-    public MeterStick( ModelViewTransform2D transform, Pool pool, final Property<Boolean> visible ) {
-        super( transform, pool, visible, Math.abs( transform.modelToViewDifferentialYDouble( 5 ) ), new String[] { "0", "1", "2", "3", "4", "5" }, "m" );
+    public MeterStick( ModelViewTransform2D transform, final Property<Boolean> visible, Point2D.Double rulerModelOrigin ) {
+        super( transform, visible, Math.abs( transform.modelToViewDifferentialYDouble( 5 ) ), new String[] { "0", "1", "2", "3", "4", "5" }, "m", rulerModelOrigin );
     }
 }
