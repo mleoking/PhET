f37931af7958464cef9d93486a26861a145ca187
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-11-17 15:54:00 +0000

    Centered pipe on flow tab

--- trunk/simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/modules/fluidflow/FluidFlowCanvas.java
+++ trunk/simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/modules/fluidflow/FluidFlowCanvas.java
@@ -3,12 +3,15 @@ package edu.colorado.phet.fluidpressureandflow.modules.fluidflow;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 
 import javax.swing.*;
 
 import edu.colorado.phet.common.phetcommon.util.Function0;
 import edu.colorado.phet.common.phetcommon.util.Function1;
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
+import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform2D;
+import edu.colorado.phet.fluidpressureandflow.model.Pool;
 import edu.colorado.phet.fluidpressureandflow.model.PressureSensor;
 import edu.colorado.phet.fluidpressureandflow.view.*;
 import edu.umd.cs.piccolo.PNode;
@@ -21,8 +24,12 @@ public class FluidFlowCanvas extends FluidPressureAndFlowCanvas {
     private PNode particleLayer;
     private PNode foodColoringLayer;
 
+    private static final double modelHeight = Pool.DEFAULT_HEIGHT * 2.2;
+    private static final double pipeCenterY = -2;
+    private static final double modelWidth = modelHeight / STAGE_SIZE.getHeight() * STAGE_SIZE.getWidth();
+
     public FluidFlowCanvas( final FluidFlowModule module ) {
-        super( module );
+        super( module, new ModelViewTransform2D( new Rectangle2D.Double( -modelWidth / 2, -modelHeight / 2+pipeCenterY, modelWidth, modelHeight ), new Rectangle2D.Double( 0, 0, STAGE_SIZE.width, STAGE_SIZE.height ), true ) );
 
         addChild( new GroundNode( transform ) );
         addChild( new SkyNode( transform ) );
