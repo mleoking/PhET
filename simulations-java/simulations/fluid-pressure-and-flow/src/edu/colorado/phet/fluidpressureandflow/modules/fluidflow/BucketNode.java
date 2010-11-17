87a8202f680c00d5a4b54994f1ac30525a298276
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-11-17 19:43:17 +0000

    Fluid-flow: Added images for dropper and bucket

---
+++ simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/modules/fluidflow/BucketNode.java
@@ -0,0 +1,31 @@
+package edu.colorado.phet.fluidpressureandflow.modules.fluidflow;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.nodes.PImage;
+import edu.umd.cs.piccolox.pswing.PSwing;
+
+import static edu.colorado.phet.fluidpressureandflow.FluidPressureAndFlowResources.RESOURCES;
+
+/**
+ * @author Sam Reid
+ */
+public class BucketNode extends PNode {
+    public BucketNode( final FluidFlowModule module ) {
+        final PImage image = new PImage( RESOURCES.getImage( "bucket.png" ) );
+        addChild( image );
+        final PSwing pswing = new PSwing( new JButton( "Pour" ) {{
+            addActionListener( new ActionListener() {
+                public void actionPerformed( ActionEvent e ) {
+                    module.getFluidFlowModel().pourFoodColoring();
+                }
+            } );
+        }} );
+        pswing.setOffset( image.getFullBounds().getWidth(), image.getFullBounds().getHeight()-pswing.getFullBounds().getHeight());
+        addChild( pswing);
+    }
+}
