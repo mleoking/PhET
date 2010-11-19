3aa25e1c1698d646bdfc33d3e2e6a50510b2a088
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-11-19 06:33:28 +0000

    added control panel, fluid density control, clock controls, reset button, etc.

--- trunk/simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/modules/watertower/WaterTowerCanvas.java
+++ trunk/simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/modules/watertower/WaterTowerCanvas.java
@@ -1,8 +1,15 @@
 package edu.colorado.phet.fluidpressureandflow.modules.watertower;
 
+import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 
+import edu.colorado.phet.common.phetcommon.application.Module;
+import edu.colorado.phet.common.phetcommon.model.AndProperty;
+import edu.colorado.phet.common.phetcommon.model.Property;
+import edu.colorado.phet.common.phetcommon.util.Function1;
+import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform2D;
+import edu.colorado.phet.common.piccolophet.nodes.mediabuttons.FloatingClockControlNode;
 import edu.colorado.phet.fluidpressureandflow.model.Pool;
 import edu.colorado.phet.fluidpressureandflow.model.PressureSensor;
 import edu.colorado.phet.fluidpressureandflow.model.VelocitySensor;
@@ -30,5 +37,52 @@ public class WaterTowerCanvas extends FluidPressureAndFlowCanvas {
             addChild( new VelocitySensorNode( transform, velocitySensor ) );
         }
 
+        //TODO: this is duplicated in FluidFlowCanvas
+        // Control Panel
+        final ControlPanel controlPanelNode = new ControlPanel( new FluidFlowControlPanel<WaterTowerModel>( module ) ) {{
+            setOffset( STAGE_SIZE.getWidth() - getFullBounds().getWidth() - 2, 2 );
+        }};
+        addChild( controlPanelNode );
+        addChild( new ResetButton( module ) {{
+            setOffset( STAGE_SIZE.getWidth() - getFullBounds().getWidth() - 2, STAGE_SIZE.getHeight() - getFullBounds().getHeight() - 2 );
+        }} );
+
+        addChild( new FluidDensityControl<WaterTowerModel>( module ) {{
+            setOffset( 0, STAGE_SIZE.getHeight() - getFullBounds().getHeight() );
+        }} );
+
+        //Some nodes go behind the pool so that it looks like they submerge
+        final Point2D.Double rulerModelOrigin = new Point2D.Double( 0, 0 );
+        addChild( new MeterStick( transform, module.getMeterStickVisibleProperty(), rulerModelOrigin ) );
+        addChild( new EnglishRuler( transform, module.getYardStickVisibleProperty(), rulerModelOrigin ) );
+
+        Property<Boolean> moduleActive = new Property<Boolean>( false ) {{
+            module.addListener( new Module.Listener() {
+                public void activated() {
+                    setValue( true );
+                }
+
+                public void deactivated() {
+                    setValue( false );
+                }
+            } );
+        }};
+
+        Property<Boolean> clockRunning = new Property<Boolean>( true );
+        //wire up the clock to be running if the module is active and if the clock control button has been pressed
+        new AndProperty( clockRunning, moduleActive ) {{
+            addObserver( new SimpleObserver() {
+                public void update() {
+                    module.getFluidPressureAndFlowModel().getClock().setRunning( getValue() );
+                }
+            } );
+        }};
+        addChild( new FloatingClockControlNode( clockRunning, new Function1<Double, String>() {
+            public String apply( Double time ) {
+                return (int) ( time / 1.00 ) + " sec";
+            }
+        }, module.getClock() ) {{
+            setOffset( STAGE_SIZE.getWidth() / 2 - getFullBounds().getWidth() / 2, STAGE_SIZE.getHeight() - getFullBounds().getHeight() );
+        }} );
     }
 }
