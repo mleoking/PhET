696d3173460e54e4b3e6f2aa32d788cea323ed3c
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-11-20 04:16:05 +0000

    fluid-pressure-and-flow: water tower graphics start

--- simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/modules/watertower/WaterTower.java
+++ simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/modules/watertower/WaterTower.java
@@ -2,13 +2,22 @@ package edu.colorado.phet.fluidpressureandflow.modules.watertower;
 
 import java.awt.geom.Rectangle2D;
 
+import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
+import edu.colorado.phet.common.phetcommon.model.Property;
+
 /**
  * @author Sam Reid
  */
 public class WaterTower {
-    private Rectangle2D.Double tank = new Rectangle2D.Double( 0, 0, 1, 1 );
+    private Property<ImmutableVector2D> tankBottomCenter = new Property<ImmutableVector2D>( new ImmutableVector2D() );
+    private static double TANK_RADIUS = 5;
+    private static double TANK_HEIGHT = 15;
 
     public Rectangle2D.Double getTank() {
-        return tank;
+        return new Rectangle2D.Double( tankBottomCenter.getValue().getX() - TANK_RADIUS, tankBottomCenter.getValue().getY(), TANK_RADIUS * 2, TANK_HEIGHT );
+    }
+
+    public Property<ImmutableVector2D> getTankBottomCenter() {
+        return tankBottomCenter;
     }
 }
