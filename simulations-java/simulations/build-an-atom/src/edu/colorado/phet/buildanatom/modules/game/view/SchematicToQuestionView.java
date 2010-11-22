aa0d9157faf3a5e03742692d3bb0a42ffa63d5a1
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-11-22 23:18:06 +0000

    Added the "Schematic to Charge Question" problem and the associated view.

--- simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/modules/game/view/SchematicToQuestionView.java
+++ simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/modules/game/view/SchematicToQuestionView.java
@@ -2,14 +2,12 @@
 
 package edu.colorado.phet.buildanatom.modules.game.view;
 
-import java.awt.Color;
 import java.text.NumberFormat;
 
 import edu.colorado.phet.buildanatom.model.BuildAnAtomModel;
 import edu.colorado.phet.buildanatom.modules.game.model.BuildAnAtomGameModel;
 import edu.colorado.phet.buildanatom.modules.game.model.Problem;
 import edu.colorado.phet.common.phetcommon.model.BooleanProperty;
-import edu.colorado.phet.common.phetcommon.util.Function0;
 
 /**
  * Base class for views of problems that present a schematic view of an atom
@@ -25,9 +23,8 @@ public abstract class SchematicToQuestionView extends ToQuestionView {
      * Constructor.
      */
     public SchematicToQuestionView( final BuildAnAtomGameModel model, GameCanvas gameCanvas, final Problem problem,
-            String questionText, int minValue, int maxValue, NumberFormat numberFormat,
-            Function0<Color> colorFunction ) {
-        super( model, gameCanvas, problem, questionText, minValue, maxValue );
+            String questionText, int minValue, int maxValue, NumberFormat numberFormat ) {
+        super( model, gameCanvas, problem, questionText, minValue, maxValue, numberFormat );
         final BuildAnAtomModel buildAnAtomModel = new BuildAnAtomModel( getClock(), problem.getAnswer(), true ) ;
 
         gameAtomModelNode = new SchematicAtomNode( buildAnAtomModel, ProblemView.SCHEMATIC_PROBLEM_MVT, new BooleanProperty( true ) ){{
