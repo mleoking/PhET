db29a5f86b8e951e22dd823e86f08670a2c0e548
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-11-23 16:47:04 +0000

    Created problem types for presenting counts and asking about atomic mass or overall charge.

---
+++ simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/modules/game/view/CountsToQuestionView.java
@@ -0,0 +1,46 @@
+/* Copyright 2010, University of Colorado */
+
+package edu.colorado.phet.buildanatom.modules.game.view;
+
+import java.text.NumberFormat;
+
+import edu.colorado.phet.buildanatom.BuildAnAtomDefaults;
+import edu.colorado.phet.buildanatom.modules.game.model.BuildAnAtomGameModel;
+import edu.colorado.phet.buildanatom.modules.game.model.Problem;
+
+/**
+ * Base class for views of problems that present a set of counts of the
+ * number of protons, neutrons, and electrons, of an atom and asks the user
+ * a single question about some aspect of it, such as the overall charge.
+ *
+ * @author John Blanco
+ */
+public abstract class CountsToQuestionView extends ToQuestionView {
+
+    private final ParticleCountNode particleCountNode;
+
+    /**
+     * Constructor.
+     */
+    public CountsToQuestionView( final BuildAnAtomGameModel model, GameCanvas gameCanvas, final Problem problem,
+            String questionText, int minValue, int maxValue, NumberFormat numberFormat ) {
+        super( model, gameCanvas, problem, questionText, minValue, maxValue, numberFormat );
+        particleCountNode = new ParticleCountNode( problem.getAnswer().getProtons(),
+                problem.getAnswer().getNeutrons(), problem.getAnswer().getElectrons() );
+        particleCountNode.setOffset(
+                BuildAnAtomDefaults.STAGE_SIZE.width /4 - particleCountNode.getFullBounds().getWidth() / 2,
+                BuildAnAtomDefaults.STAGE_SIZE.height / 2 - particleCountNode.getFullBounds().getHeight() / 2 );
+    }
+
+    @Override
+    public void init() {
+        super.init();
+        addChild( particleCountNode );
+    }
+
+    @Override
+    public void teardown() {
+        super.teardown();
+        removeChild( particleCountNode );
+    }
+}
