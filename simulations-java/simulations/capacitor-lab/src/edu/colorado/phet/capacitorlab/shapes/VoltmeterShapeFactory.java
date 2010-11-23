e323c782820777c4bbd58684ce76708371dc83be
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-11-23 23:15:50 +0000

    move constants from VoltmeterShapeFactory to Voltmeter model element

--- simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/shapes/VoltmeterShapeFactory.java
+++ simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/shapes/VoltmeterShapeFactory.java
@@ -9,21 +9,14 @@ import java.awt.geom.Rectangle2D;
 import edu.colorado.phet.capacitorlab.model.CLModelViewTransform3D;
 import edu.colorado.phet.capacitorlab.model.Voltmeter;
 import edu.colorado.phet.common.phetcommon.math.Point3D;
-import edu.umd.cs.piccolo.util.PDimension;
 
 /**
  * Creates 2D projections of shapes that are related to the 3D voltmeter model.
- * Shapes are in the global view coordinate frame, everything else is in model coordinates.
+ * Shapes are in the global view coordinate frame.
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
 public class VoltmeterShapeFactory {
-    
-    /*
-     * Sizes determined by visual inspection of the associated image files.
-     * To see the corresponding bounds, run the sim with -dev.
-     */
-    private final PDimension PROBE_TIP_SIZE = new PDimension( 0.0005, 0.0015 );
     
     private final Voltmeter voltmeter;
     private final CLModelViewTransform3D mvt;
@@ -53,9 +46,11 @@ public class VoltmeterShapeFactory {
      * Gets the shape of a probe tip relative to some specified origin.
      */
     private Shape getProbeTipShape( Point3D origin, double theta ) {
-        double x = origin.getX() - ( PROBE_TIP_SIZE.getWidth() / 2 );
-        double y = origin.getY();
-        Rectangle2D r = new Rectangle2D.Double( x, y, PROBE_TIP_SIZE.getWidth(), PROBE_TIP_SIZE.getHeight() );
+        final double width = voltmeter.getProbeTipSizeReference().getWidth();
+        final double height = voltmeter.getProbeTipSizeReference().getHeight();
+        final double x = origin.getX() - ( width / 2 );
+        final double y = origin.getY();
+        Rectangle2D r = new Rectangle2D.Double( x, y, width, height );
         AffineTransform t = AffineTransform.getRotateInstance( theta, origin.getX(), origin.getY() );
         Shape s = t.createTransformedShape( r );
         return mvt.modelToView( s );
