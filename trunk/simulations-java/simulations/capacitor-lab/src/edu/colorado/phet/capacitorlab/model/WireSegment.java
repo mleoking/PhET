f9a1d1d0349aff72fffe7e083a7bafd01b23f3d6
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-11-24 00:04:59 +0000

    move constants from BatteryShapeFactory to Battery model element, improve definition of positive terminal

--- trunk/simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/model/WireSegment.java
+++ trunk/simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/model/WireSegment.java
@@ -56,10 +56,10 @@ public class WireSegment {
     public static class BatteryTopWireSegment extends WireSegment {
         
         public BatteryTopWireSegment( final Battery battery, Point2D endPoint ) {
-            super( new Point2D.Double( battery.getX(), battery.getY() + battery.getShapeFactory().getTopTerminalYOffset() ), endPoint );
+            super( new Point2D.Double( battery.getX(), battery.getY() + battery.getTopTerminalYOffset() ), endPoint );
             battery.addPolarityObserver( new SimpleObserver() {
                 public void update() {
-                    setStartPoint( new Point2D.Double( battery.getX(), battery.getY() + battery.getShapeFactory().getTopTerminalYOffset() ) );
+                    setStartPoint( new Point2D.Double( battery.getX(), battery.getY() + battery.getTopTerminalYOffset() ) );
                 }
             } );
         }
@@ -71,10 +71,10 @@ public class WireSegment {
     public static class BatteryBottomWireSegment extends WireSegment {
         
         public BatteryBottomWireSegment( final Battery battery, Point2D endPoint ) {
-            super( new Point2D.Double( battery.getX(), battery.getY() + battery.getShapeFactory().getBottomTerminalYOffset() ), endPoint );
+            super( new Point2D.Double( battery.getX(), battery.getY() + battery.getBottomTerminalYOffset() ), endPoint );
             battery.addPolarityObserver( new SimpleObserver() {
                 public void update() {
-                    setStartPoint( new Point2D.Double( battery.getX(), battery.getY() + battery.getShapeFactory().getBottomTerminalYOffset() ) );
+                    setStartPoint( new Point2D.Double( battery.getX(), battery.getY() + battery.getBottomTerminalYOffset() ) );
                 }
             });
         }
