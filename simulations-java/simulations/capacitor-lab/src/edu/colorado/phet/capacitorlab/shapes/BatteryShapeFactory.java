f9c1ba00b4a4c4577208e217a1dcf8d8941ae90b
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-11-24 00:04:59 +0000

    move constants from BatteryShapeFactory to Battery model element, improve definition of positive terminal

--- simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/shapes/BatteryShapeFactory.java
+++ simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/shapes/BatteryShapeFactory.java
@@ -5,13 +5,12 @@ package edu.colorado.phet.capacitorlab.shapes;
 import java.awt.Shape;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Rectangle2D;
-import java.awt.geom.RoundRectangle2D;
 
 import edu.colorado.phet.capacitorlab.model.Battery;
 import edu.colorado.phet.capacitorlab.model.CLModelViewTransform3D;
 import edu.colorado.phet.capacitorlab.model.Polarity;
+import edu.colorado.phet.capacitorlab.util.ShapeUtils;
 import edu.colorado.phet.common.phetcommon.math.Point3D;
-import edu.umd.cs.piccolo.util.PDimension;
 
 /**
  * Creates 2D projections of shapes that are related to the 3D battery model.
@@ -20,18 +19,6 @@ import edu.umd.cs.piccolo.util.PDimension;
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
 public class BatteryShapeFactory {
-    
-    /*
-     * Sizes determined by visual inspection of the associated image files.
-     * All units in meters.
-     * To see the corresponding bounds, run the sim with -dev.
-     */
-    private static final PDimension BODY_SIZE = new PDimension( 0.0065, 0.01425 );
-    private static final PDimension POSITIVE_TERMINAL_SIZE = new PDimension( 0.0025, 0.0014 );
-    private static final double POSITIVE_TERMINAL_CORNER_RADIUS = 0.0005;
-    private static final double POSITIVE_TERMINAL_Y_OFFSET = -( BODY_SIZE.getHeight() / 2 ) + 0.000505;
-    private static final PDimension NEGATIVE_TERMINAL_SIZE = new PDimension( 0.0035, 0.0009 );
-    private static final double NEGATIVE_TERMINAL_Y_OFFSET = -( BODY_SIZE.getHeight() / 2 ) + 0.00105;
     
     private final Battery battery;
     private final CLModelViewTransform3D mvt;
@@ -42,37 +29,16 @@ public class BatteryShapeFactory {
     }
     
     /**
-     * Gets the offset of the top terminal from the battery's origin, in model coordinates (meters).
-     * This offset depends on the polarity.
-     * @return
-     */
-    public double getTopTerminalYOffset() {
-        if ( battery.getPolarity() == Polarity.POSITIVE ) {
-            return POSITIVE_TERMINAL_Y_OFFSET;
-        }
-        else {
-            return NEGATIVE_TERMINAL_Y_OFFSET;
-        }
-    }
-    
-    /**
-     * Gets the offset of the bottom terminal from the battery's origin, in model coordinates (meters).
-     * We don't need to account for the polarity since the bottom terminal is never visible.
-     * @return
-     */
-    public double getBottomTerminalYOffset() {
-        return BODY_SIZE.getHeight() / 2;
-    }
-    
-    /**
      * Gets the shape of the battery's body in the world coordinate frame.
      * Origin at the center.
      * @return
      */
     public Shape createBodyShape() {
-        double x = battery.getLocationReference().getX() - ( BODY_SIZE.getWidth() / 2 );
-        double y = battery.getLocationReference().getY() - ( BODY_SIZE.getHeight() / 2 );
-        Shape s = new Rectangle2D.Double( x, y, BODY_SIZE.getWidth(), BODY_SIZE.getHeight() );
+        final double width = battery.getBodySizeReference().getWidth();
+        final double height = battery.getBodySizeReference().getHeight();
+        final double x = battery.getLocationReference().getX() - ( width / 2 );
+        final double y = battery.getLocationReference().getY() - ( height / 2 );
+        Shape s = new Rectangle2D.Double( x, y, width, height );
         return mvt.modelToView( s );
     }
 
@@ -91,25 +57,53 @@ public class BatteryShapeFactory {
     
     /*
      * Creates the shape of the positive terminal relative to some specified origin.
+     * The positive terminal is a cylinder, created using constructive area geometry.
      */
     private Shape createPositiveTerminalShape( Point3D origin ) {
-        final double terminalWidth = POSITIVE_TERMINAL_SIZE.getWidth();
-        final double terminalHeight = POSITIVE_TERMINAL_SIZE.getHeight();
-        double x = origin.getX() - ( terminalWidth / 2 );
-        double y = origin.getY() - ( BODY_SIZE.getHeight() / 2 ) + 0.0002;
-        Shape s = new RoundRectangle2D.Double( x, y, terminalWidth, terminalHeight, POSITIVE_TERMINAL_CORNER_RADIUS, POSITIVE_TERMINAL_CORNER_RADIUS );
-        return mvt.modelToView( s );
+        
+        // top of the cylinder
+        Shape topEllipse = null;
+        {
+            final double width = battery.getPositiveTerminalEllipseSize().getWidth();
+            final double height = battery.getPositiveTerminalEllipseSize().getHeight();
+            final double x = origin.getX() - ( width / 2 );
+            final double y = origin.getY() + battery.getTopTerminalYOffset() - ( height / 2 );
+            topEllipse = new Ellipse2D.Double( x, y, width, height );
+        }
+        
+        // bottom of the cylinder
+        Shape bottomEllipse = null;
+        {
+            final double width = battery.getPositiveTerminalEllipseSize().getWidth();
+            final double height = battery.getPositiveTerminalEllipseSize().getHeight();
+            final double x = origin.getX() - ( width / 2 );
+            final double y = origin.getY() + battery.getTopTerminalYOffset() - ( height / 2 ) + battery.getPositiveTerminalCylinderHeight();
+            bottomEllipse = new Ellipse2D.Double( x, y, width, height );
+        }
+        
+        // wall of the cylinder
+        Shape wallShape = null;
+        {
+            final double width = battery.getPositiveTerminalEllipseSize().getWidth();
+            final double height = battery.getPositiveTerminalCylinderHeight();
+            final double x = origin.getX() - ( width / 2 );
+            final double y = origin.getY() + battery.getTopTerminalYOffset();
+            wallShape = new Rectangle2D.Double( x, y, width, height );
+        }
+        
+        Shape composite = ShapeUtils.add( topEllipse, wallShape, bottomEllipse );
+        return mvt.modelToView( composite );
     }
     
     /*
      * Creates the shape of the negative terminal relative to some specified origin.
      */
     private Shape createNegativeTerminalShape( Point3D origin ) {
-        final double terminalWidth = NEGATIVE_TERMINAL_SIZE.getWidth();
-        final double terminalHeight = NEGATIVE_TERMINAL_SIZE.getHeight();
-        double x = origin.getX() - ( terminalWidth / 2 );
-        double y = origin.getY() - ( BODY_SIZE.getHeight() / 2 ) + 0.0006;
-        Shape s = new Ellipse2D.Double( x, y, terminalWidth, terminalHeight );
+        final double width = battery.getNegativeTerminalSizeReference().getWidth();
+        final double height = battery.getNegativeTerminalSizeReference().getHeight();
+        final double x = origin.getX() - ( width / 2 );
+        final double y = origin.getY() + battery.getTopTerminalYOffset() - ( height / 2 );
+        Shape s = new Ellipse2D.Double( x, y, width, height );
         return mvt.modelToView( s );
     }
 }
