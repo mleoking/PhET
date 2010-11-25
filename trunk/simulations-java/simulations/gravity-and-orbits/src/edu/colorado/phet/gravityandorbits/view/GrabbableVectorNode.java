083cfe22519c897b3d34c84531c7249f244a95b1
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-11-25 02:10:45 +0000

    Made velocity circles more like "my solar system" -added a 'v' to the circle -made the circle gray -made the vectors go behind planets -made the 'v' circle go behind vectors

--- trunk/simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/view/GrabbableVectorNode.java
+++ trunk/simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/view/GrabbableVectorNode.java
@@ -8,11 +8,14 @@ import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
 import edu.colorado.phet.common.phetcommon.model.Property;
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform2D;
+import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.event.CursorHandler;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.colorado.phet.gravityandorbits.model.Body;
+import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
 import edu.umd.cs.piccolo.event.PInputEvent;
+import edu.umd.cs.piccolo.nodes.PText;
 import edu.umd.cs.piccolo.util.PDimension;
 
 /**
@@ -25,7 +28,14 @@ public class GrabbableVectorNode extends VectorNode {
                                 final double scale, Color fill, Color outline ) {
         super( body, modelViewTransform2D, visible, property, scale, fill, outline );
         final Point2D tip = getTip();
-        grabArea = new PhetPPath( new Ellipse2D.Double( 0, 0, 40, 40 ), new Color( 0, 0, 0, 0 ), new BasicStroke( 1 ), Color.yellow ) {{
+        grabArea = new PhetPPath( new Ellipse2D.Double( 0, 0, 40, 40 ), new Color( 0, 0, 0, 0 ), new BasicStroke( 3 ), Color.lightGray ) {{
+            final PNode parent = this;
+            addChild( new PText( "V" ) {{
+                PText v = this;
+                setFont( new PhetFont( 28, true ) );
+                setTextPaint( Color.gray );
+                setOffset( parent.getFullBounds().getWidth() / 2 - v.getFullBounds().getWidth() / 2, parent.getFullBounds().getHeight() / 2 - v.getFullBounds().getHeight() / 2 );
+            }} );
             setOffset( tip.getX() - getFullBounds().getWidth() / 2, tip.getY() - getFullBounds().getHeight() / 2 );
         }};
         final SimpleObserver updateGrabArea = new SimpleObserver() {
@@ -45,5 +55,6 @@ public class GrabbableVectorNode extends VectorNode {
             }
         } );
         grabArea.addInputEventListener( new CursorHandler() );//todo: use same pattern as in body node so that mouse turns into cursor when arrow moves under stationary mouse?
+        grabArea.moveToBack();
     }
 }
