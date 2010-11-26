4f71ad79d61365c6108a083276f5cc8d1e38c620
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-11-26 07:55:03 +0000

    minor cleanup

--- simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/view/PathNode.java
+++ simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/view/PathNode.java
@@ -22,7 +22,7 @@ public class PathNode extends PNode {
     private final ModelViewTransform2D transform;
     private final Property<Boolean> visible;
     private ArrayList<Point> points = new ArrayList<Point>();//points in view space
-    public static int MAX_TRACE_LENGTH = (int) ( 365 / GravityAndOrbitsDefaults.NUMBER_DAYS_PER_TICK * 2 );//enough for 2 earth years
+    public static final int MAX_TRACE_LENGTH = (int) ( 365 / GravityAndOrbitsDefaults.NUMBER_DAYS_PER_TICK * 2 );//enough for 2 earth years
     private int[] xPrimitive = new int[MAX_TRACE_LENGTH];
     private int[] yPrimitive = new int[MAX_TRACE_LENGTH];
 
