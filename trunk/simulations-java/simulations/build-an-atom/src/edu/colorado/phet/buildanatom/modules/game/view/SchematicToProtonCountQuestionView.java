84089f10cc3d1912cc67fe8609a81af5fecde352
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-11-29 23:20:39 +0000

    Marked some "TODOs" for classes that may be removed.

--- trunk/simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/modules/game/view/SchematicToProtonCountQuestionView.java
+++ trunk/simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/modules/game/view/SchematicToProtonCountQuestionView.java
@@ -14,6 +14,10 @@ import edu.colorado.phet.common.phetcommon.util.Function0;
  * View for the problem that presents a schematic view of an atom and asks
  * the user about the proton count.
  *
+ * TODO: This problem type may be too simple, and as such we are considering
+ * getting rid of it.  If it is unused by the time we publish this sim with
+ * the game, we should get rid of this class.
+ *
  * @author John Blanco
  */
 public class SchematicToProtonCountQuestionView extends SchematicToQuestionView {
