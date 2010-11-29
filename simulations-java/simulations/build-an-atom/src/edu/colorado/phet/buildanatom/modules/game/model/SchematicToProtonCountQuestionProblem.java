3b6bb671ce754179113cd4e67d7ac13da84cf9c5
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-11-29 23:20:39 +0000

    Marked some "TODOs" for classes that may be removed.

--- simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/modules/game/model/SchematicToProtonCountQuestionProblem.java
+++ simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/modules/game/model/SchematicToProtonCountQuestionProblem.java
@@ -10,6 +10,10 @@ import edu.colorado.phet.buildanatom.modules.game.view.StateView;
  * Problem that presents a schematic (a.k.a. a "Bohr model") and asks the user
  * a question about the proton count.
  *
+ * TODO: This problem type may be too simple, and as such we are considering
+ * getting rid of it.  If it is unused by the time we publish this sim with
+ * the game, we should get rid of this class.
+ *
  * @author John Blanco
  */
 public class SchematicToProtonCountQuestionProblem extends Problem {
