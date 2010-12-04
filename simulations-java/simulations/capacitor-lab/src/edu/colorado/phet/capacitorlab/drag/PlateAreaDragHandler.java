d5a85419286f00294cb55db3a96a8af20432295e
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-12-04 03:26:50 +0000

    clean up

--- simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/drag/PlateAreaDragHandler.java
+++ simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/drag/PlateAreaDragHandler.java
@@ -11,7 +11,6 @@ import edu.colorado.phet.common.phetcommon.util.DoubleRange;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.event.PDragSequenceEventHandler;
 import edu.umd.cs.piccolo.event.PInputEvent;
-import edu.umd.cs.piccolo.util.PDimension;
 
 /**
  * Drag handler for capacitor plate area property.
@@ -20,102 +19,72 @@ import edu.umd.cs.piccolo.util.PDimension;
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
-/* package private */  class PlateAreaDragHandler extends PDragSequenceEventHandler {
-    
-    private final boolean DO_TRIG = false;
-    
+/* package private */class PlateAreaDragHandler extends PDragSequenceEventHandler {
+
     private final PNode dragNode;
     private final Capacitor capacitor;
     private final CLModelViewTransform3D mvt;
     private final DoubleRange valueRange;
-    
-    private double clickXOffset, clickYOffset; // y offset of mouse click from dragNode's origin, in parent node's coordinate frame
-    
+
+    private double clickXOffset; // x offset of mouse click from dragNode's origin, in parent node's coordinate frame
+
     public PlateAreaDragHandler( PNode dragNode, Capacitor capacitor, CLModelViewTransform3D mvt, DoubleRange valueRange ) {
         this.dragNode = dragNode;
         this.capacitor = capacitor;
         this.mvt = mvt;
         this.valueRange = new DoubleRange( valueRange );
     }
-    
+
     @Override
-    protected void startDrag(PInputEvent event) {
+    protected void startDrag( PInputEvent event ) {
         super.startDrag( event );
         Point2D pMouse = event.getPositionRelativeTo( dragNode.getParent() );
         Point2D pOrigin = mvt.modelToViewDelta( -capacitor.getPlateWidth() / 2, 0, -capacitor.getPlateWidth() / 2 );
         clickXOffset = pMouse.getX() - pOrigin.getX();
-        clickYOffset = pMouse.getY() - pOrigin.getY();
     }
-    
+
     @Override
     protected void drag( PInputEvent event ) {
-        if ( DO_TRIG ) {
-            dragTrig( event );
-        }
-        else {
-            dragFunc( event );
-        }
-    }
-    
-    protected void dragTrig( PInputEvent event ) {
         super.drag( event );
-        PDimension delta = event.getDeltaRelativeTo( dragNode.getParent() );
-        double dx = delta.getWidth();
-        double dy = delta.getHeight();
-        // only allow dragging down to the left, or up to the right
-        if ( ( dx < 0 && dy >= 0 ) || ( dx > 0 && dy <= 0 ) ) {
-            Point2D pMouse = event.getPositionRelativeTo( dragNode.getParent() );
-            double yView = pMouse.getY() - clickYOffset;
-            double zModel = getZModel( yView );
-            double plateWidth = Math.abs( 2 * zModel  );
-            if ( plateWidth > valueRange.getMax() ) {
-                plateWidth = valueRange.getMax();
-            }
-            else if ( plateWidth < valueRange.getMin() ) {
-                plateWidth = valueRange.getMin();
-            }
-            capacitor.setPlateWidth( plateWidth );
-        }
-    }
-    
-    private double getZModel( double yView ) {
-        return Math.cos( Math.PI/2 - mvt.getYaw() ) / mvt.viewToModel( 0, yView ).getY();
-    }
-    
-    protected void dragFunc( PInputEvent event ) {
-        super.drag( event );
-        PDimension delta = event.getDeltaRelativeTo( dragNode.getParent() );
-        double dx = delta.getWidth();
-        double dy = delta.getHeight();
-        // only allow dragging down to the left, or up to the right
-        if ( ( dx < 0 && dy >= 0 ) || ( dx > 0 && dy <= 0 ) ) {
-            Point2D pMouse = event.getPositionRelativeTo( dragNode.getParent() );
-            double xView = pMouse.getX() - clickXOffset;
-            double plateWidth = 2 * mvt.viewToModelDelta( -xView, 0 ).getX();
-            double adjustedPlateWidth = getAdjustedPlateWidth( pMouse, plateWidth );
-            capacitor.setPlateWidth( adjustedPlateWidth );
-        }
-    }
-    
-    private double getAdjustedPlateWidth( Point2D pMouse, double plateWidth ) {
-        double dx1 = getDX( pMouse, plateWidth );
-
-        final double deltaX = 1E-6;
-        double dx2 = getDX( pMouse, plateWidth + deltaX );
-
-        Function.LinearFunction function = new Function.LinearFunction( plateWidth,plateWidth+ deltaX,dx1,dx2 );
-        double adjustedPlateWidth = function.createInverse().evaluate( 0 );
-        if ( adjustedPlateWidth > valueRange.getMax() ) {
-            adjustedPlateWidth = valueRange.getMax();
-        }
-        else if ( adjustedPlateWidth < valueRange.getMin() ) {
-            adjustedPlateWidth = valueRange.getMin();
-        }
-        return adjustedPlateWidth;
+        Point2D pMouse = event.getPositionRelativeTo( dragNode.getParent() );
+        double plateWidth = getPlateWidth( pMouse );
+        capacitor.setPlateWidth( plateWidth );
     }
 
-    private double getDX( Point2D pMouse, double plateWidth ) {
-        Point2D pFront = mvt.modelToView( -plateWidth/2,0,-plateWidth/2 );
-        return pMouse.getX() - pFront.getX()-clickXOffset;
+    /*
+     * Determines the plateWidth for a specific mouse position.
+     * This effectively accounts for the z-axis dimension.
+     */
+    private double getPlateWidth( Point2D pMouse ) {
+        
+        // pick any 2 view values
+        final double xView1 = 0; 
+        final double xView2 = 1;
+        
+        // compute corresponding model values
+        final double xModel1 = getModelX( pMouse, xView1 );
+        final double xModel2 = getModelX( pMouse, xView2 );
+        
+        Function.LinearFunction function = new Function.LinearFunction( xView1, xView2, xModel1, xModel2 );
+        double plateWidth = function.createInverse().evaluate( 0 );
+        
+        // constrain to the plate width bounds
+        if ( plateWidth > valueRange.getMax() ) {
+            plateWidth = valueRange.getMax();
+        }
+        else if ( plateWidth < valueRange.getMin() ) {
+            plateWidth = valueRange.getMin();
+        }
+        
+        return plateWidth;
+    }
+
+    /*
+     * Determines how far the mouse is from where we grabbed the arrow, 
+     * for a hypothetical capacitor plate width.
+     */
+    private double getModelX( Point2D pMouse, double samplePlateWidth ) {
+        Point2D pFront = mvt.modelToView( -samplePlateWidth / 2, 0, -samplePlateWidth / 2 );
+        return pMouse.getX() - pFront.getX() - clickXOffset;
     }
 }
