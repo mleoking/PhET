a92530e5a1fc8ff01d24bbc6dde0708b567b2af9
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-12-07 03:49:04 +0000

    website: renaming string changes and terminology

--- trunk/web/wicket-website/src/java/edu/colorado/phet/website/StringChanges.java
+++ trunk/web/wicket-website/src/java/edu/colorado/phet/website/StringChanges.java
@@ -9,41 +9,41 @@ import edu.colorado.phet.website.util.hibernate.HibernateTask;
 import edu.colorado.phet.website.util.hibernate.HibernateUtils;
 
 /**
- * Contains strings that have been added since the last production deployment. If strings by those key names don't exist
- * they will be created.
+ * Contains strings that have been addedor modified since the last production deployment. If strings by those key names
+ * don't exist they will be created.
  */
-public class NewStrings {
+public class StringChanges {
 
-    private static Logger logger = Logger.getLogger( NewStrings.class );
+    private static Logger logger = Logger.getLogger( StringChanges.class );
 
     public static void checkNewStrings() {
         Session session = HibernateUtils.getInstance().openSession();
-        checkString( session, "home.subscribeToNewsletter", "Subscribe to our newsletter" );
-        checkString( session, "newsletter.subscribe.email", "Email Address:" );
-        checkString( session, "newsletter.subscribe.submit", "Subscribe" );
-        checkString( session, "newsletter.validation.email.Required", "An email address is required" );
-        checkString( session, "newsletter.validation.email", "A valid email address is required" );
-        checkString( session, "newsletter.validation.attempts", "Too many newsletter attempts have been made. Please try again later" );
-        checkString( session, "newsletter.nowSubscribed", "{0} is now subscribed to the PhET newsletter." );
-        checkString( session, "newsletter.nowUnsubscribed", "{0} is now unsubscribed from the PhET newsletter." );
-        checkString( session, "newsletter.nowRegistered", "Thank you for registering! In a few seconds you will be redirected to your original page." );
-        checkString( session, "newsletter.toFinishRegistering", "To finish creating your account, we've sent a confirmation email to {0}. In the Email, please click on the link or copy and paste it into a browser in order to confirm your email." );
-        checkString( session, "newsletter.toFinishSubscribing", "To finish subscribing, we've sent a confirmation email to {0}. In the Email, please click on the link or copy and paste it into a browser in order to confirm your email." );
-        checkString( session, "newsletter.troubleshooting", "If you don't receive the email momentarily, please make sure your email filter allows emails sent from {0}. For additional help, please email {1}." );
-        checkString( session, "newsletter.awaitingConfirmation", "Awaiting Email Confirmation" );
-        checkString( session, "newsletter.confirmEmailSent.title", "Awaiting Email Confirmation" );
+        addString( session, "home.subscribeToNewsletter", "Subscribe to our newsletter" );
+        addString( session, "newsletter.subscribe.email", "Email Address:" );
+        addString( session, "newsletter.subscribe.submit", "Subscribe" );
+        addString( session, "newsletter.validation.email.Required", "An email address is required" );
+        addString( session, "newsletter.validation.email", "A valid email address is required" );
+        addString( session, "newsletter.validation.attempts", "Too many newsletter attempts have been made. Please try again later" );
+        addString( session, "newsletter.nowSubscribed", "{0} is now subscribed to the PhET newsletter." );
+        addString( session, "newsletter.nowUnsubscribed", "{0} is now unsubscribed from the PhET newsletter." );
+        addString( session, "newsletter.nowRegistered", "Thank you for registering! In a few seconds you will be redirected to your original page." );
+        addString( session, "newsletter.toFinishRegistering", "To finish creating your account, we've sent a confirmation email to {0}. In the Email, please click on the link or copy and paste it into a browser in order to confirm your email." );
+        addString( session, "newsletter.toFinishSubscribing", "To finish subscribing, we've sent a confirmation email to {0}. In the Email, please click on the link or copy and paste it into a browser in order to confirm your email." );
+        addString( session, "newsletter.troubleshooting", "If you don't receive the email momentarily, please make sure your email filter allows emails sent from {0}. For additional help, please email {1}." );
+        addString( session, "newsletter.awaitingConfirmation", "Awaiting Email Confirmation" );
+        addString( session, "newsletter.confirmEmailSent.title", "Awaiting Email Confirmation" );
         //checkString( session, "newsletter.pastEditions", "<a {0}>Here are some past editions</a> of the PhET Newsletter to give you an idea of what you will receive." );
-        checkString( session, "newsletter.pleaseSignUp", "Please sign up if you would like to receive the PhET Newsletter. Once you have submitted your email address, you should receive a confirmation of your subscription via email." );
-        checkString( session, "newsletter.subscribeTo", "Subscribe to the Newsletter" );
-        checkString( session, "newsletter.subscribe.title", "Subscribe to PhET" );
-        checkString( session, "newsletter.confirmedEmail.title", "Confirmed PhET Email" );
-        checkString( session, "newsletter.confirmedEmail", "Email Confirmed" );
-        checkString( session, "newsletter.unsubscribed.title", "Unsubscribed to PhET Newsletters" );
-        checkString( session, "newsletter.unsubscribed", "Unsubscribed from PhET Newsletter" );
+        addString( session, "newsletter.pleaseSignUp", "Please sign up if you would like to receive the PhET Newsletter. Once you have submitted your email address, you should receive a confirmation of your subscription via email." );
+        addString( session, "newsletter.subscribeTo", "Subscribe to the Newsletter" );
+        addString( session, "newsletter.subscribe.title", "Subscribe to PhET" );
+        addString( session, "newsletter.confirmedEmail.title", "Confirmed PhET Email" );
+        addString( session, "newsletter.confirmedEmail", "Email Confirmed" );
+        addString( session, "newsletter.unsubscribed.title", "Unsubscribed to PhET Newsletters" );
+        addString( session, "newsletter.unsubscribed", "Unsubscribed from PhET Newsletter" );
 
-        checkString( session, "newsletter.pleaseCheckSubscribing", "Please check your e-mail and click on the link to complete your newsletter subscription." );
-        checkString( session, "newsletter.pleaseCheckRegistering", "Please check your e-mail and click on the link to complete your account registration." );
-        checkString( session, "newsletter.redirection", "In a few seconds you will be redirected to your original page." );
+        addString( session, "newsletter.pleaseCheckSubscribing", "Please check your e-mail and click on the link to complete your newsletter subscription." );
+        addString( session, "newsletter.pleaseCheckRegistering", "Please check your e-mail and click on the link to complete your account registration." );
+        addString( session, "newsletter.redirection", "In a few seconds you will be redirected to your original page." );
 
         overwriteString( session, "newsletter.toFinishSubscribing", "To finish subscribing, we've sent a confirmation email to {0}. In the Email, please click on the link or copy and paste it into a browser in order to confirm your email.", "Thank you for subscribing. An e-mail has been sent to {0}." );
         overwriteString( session, "newsletter.toFinishRegistering", "To finish creating your account, we've sent a confirmation email to {0}. In the Email, please click on the link or copy and paste it into a browser in order to confirm your email.", "Thank you for registering. An e-mail has been sent to {0}." );
@@ -70,7 +70,7 @@ public class NewStrings {
     * newsletter-instructions
     *----------------------------------------------------------------------------*/
 
-    private static void checkString( Session session, String key, String value ) {
+    private static void addString( Session session, String key, String value ) {
         String result = StringUtils.getStringDirect( session, key, PhetWicketApplication.getDefaultLocale() );
         if ( result == null ) {
             logger.warn( "Auto-setting English string with key=" + key + " value=" + value );
