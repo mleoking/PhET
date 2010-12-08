d1c77074597f72cd051a942247d37e915cf8b8ac
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-12-08 20:56:39 +0000

    Added a marker that shows center of gravity.  This is for debug purposes.

--- simulations-java/simulations/greenhouse/src/edu/colorado/phet/greenhouse/view/MoleculeNode.java
+++ simulations-java/simulations/greenhouse/src/edu/colorado/phet/greenhouse/view/MoleculeNode.java
@@ -2,10 +2,15 @@
 
 package edu.colorado.phet.greenhouse.view;
 
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Image;
+import java.awt.Shape;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
 
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform2D;
+import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.colorado.phet.greenhouse.model.Atom;
 import edu.colorado.phet.greenhouse.model.AtomicBond;
 import edu.colorado.phet.greenhouse.model.Molecule;
@@ -17,32 +22,36 @@ import edu.umd.cs.piccolo.PNode;
  * @author John Blanco
  */
 public class MoleculeNode extends PNode {
-    
+
     //------------------------------------------------------------------------
     // Class Data
     //------------------------------------------------------------------------
 
+    // This flag is used to turn on/off the appearance of the center of
+    // gravity, which is useful for debugging.
+    private static final boolean SHOW_COG = true;
+
     //------------------------------------------------------------------------
     // Instance Data
     //------------------------------------------------------------------------
-    
+
     private final PNode atomLayer;
     private final PNode bondLayer;
 
     //------------------------------------------------------------------------
     // Constructor(s)
     //------------------------------------------------------------------------
-    
+
     public MoleculeNode ( final Molecule molecule, ModelViewTransform2D mvt){
         bondLayer = new PNode();
         addChild(bondLayer);
         atomLayer = new PNode();
         addChild(atomLayer);
-        
+
         for (Atom atom : molecule.getAtoms()){
             atomLayer.addChild( new AtomNode( atom, mvt ) );
         }
-        
+
         for (AtomicBond atomicBond : molecule.getAtomicBonds()){
             bondLayer.addChild( new AtomicBondNode( atomicBond, mvt ) );
         }
@@ -58,12 +67,21 @@ public class MoleculeNode extends PNode {
         };
         molecule.addListener( listener );
         listener.electronicEnergyStateChanged( molecule );
+
+        // If enabled, show the center of gravity of the molecule.
+        if ( SHOW_COG ){
+            double cogMarkerRadius = 5;
+            Shape cogMarkerShape = new Ellipse2D.Double(-cogMarkerRadius, -cogMarkerRadius, cogMarkerRadius * 2, cogMarkerRadius * 2);
+            PNode cogMarkerNode = new PhetPPath( cogMarkerShape, Color.pink );
+            cogMarkerNode.setOffset( mvt.modelToViewDouble( molecule.getCenterOfGravityPos() ) );
+            atomLayer.addChild( cogMarkerNode );
+        }
     }
 
     //------------------------------------------------------------------------
     // Methods
     //------------------------------------------------------------------------
-    
+
     /**
      * Retrieve an image representation of this node.  This was created in
      * order to support putting molecule images on control panels, but may
