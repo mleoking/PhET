ac38ec89a0b9bf7d9a99ca505c0bba2dc0f40d2b
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-12-08 21:00:23 +0000

    Add a background to sensor nodes to make them look more sensory

--- simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/view/SensorNode.java
+++ simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/view/SensorNode.java
@@ -1,7 +1,10 @@
 package edu.colorado.phet.fluidpressureandflow.view;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Shape;
 import java.awt.geom.Ellipse2D;
+import java.awt.geom.RoundRectangle2D;
 import java.text.MessageFormat;
 
 import edu.colorado.phet.common.phetcommon.model.Property;
@@ -13,15 +16,13 @@ import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.colorado.phet.fluidpressureandflow.model.Sensor;
 import edu.colorado.phet.fluidpressureandflow.model.Units;
 import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.nodes.PPath;
 import edu.umd.cs.piccolo.nodes.PText;
 
 /**
  * @author Sam Reid
  */
 public abstract class SensorNode<T> extends PNode {
-    
-    private static final double HOT_SPOT_RADIUS = 3;
-    private static final Color HOT_SPOT_COLOR = Color.RED;
     
     /**
      * @param transform
@@ -31,13 +32,22 @@ public abstract class SensorNode<T> extends PNode {
     public SensorNode( final ModelViewTransform transform, final Sensor<T> sensor, final Property<Units.Unit> unitsProperty ) {
         
         // hot spot
-        addChild( new PhetPPath( new Ellipse2D.Double( -HOT_SPOT_RADIUS, -HOT_SPOT_RADIUS, HOT_SPOT_RADIUS * 2, HOT_SPOT_RADIUS * 2 ), HOT_SPOT_COLOR ) );
+        final double hotSpotRadius = 3;
+        final Color hotSpotColor = Color.RED;
+        PNode hotSpotNode = new PhetPPath( new Ellipse2D.Double( -hotSpotRadius, -hotSpotRadius, hotSpotRadius * 2, hotSpotRadius * 2 ), hotSpotColor );
         
         // value display
         final PText textNode = new PText( getText( sensor, unitsProperty ) ) {{
             setFont( new PhetFont( 18, true ) );
         }};
+        
+        // background box
+        final PPath backgroundNode = new PhetPPath( Color.white, new BasicStroke( 1f ), Color.gray );
+        
+        // rendering order
+        addChild( backgroundNode );
         addChild( textNode );
+        addChild( hotSpotNode );
         
         addInputEventListener( new CursorHandler() );
         
@@ -48,8 +58,19 @@ public abstract class SensorNode<T> extends PNode {
         } );
         final SimpleObserver updateTextObserver = new SimpleObserver() {
             public void update() {
+                
+                // update the text and center it
                 textNode.setText( getText( sensor, unitsProperty ) );
-                textNode.setOffset( -textNode.getFullBounds().getWidth() / 2, -textNode.getFullBounds().getHeight() );
+                final double textYSpacing = -10;
+                textNode.setOffset( -textNode.getFullBoundsReference().getWidth() / 2, -textNode.getFullBoundsReference().getHeight() + textYSpacing );
+                
+                // update the background to enclose the textNode
+                final double cornerRadius = 10;
+                final double margin = 3;
+                final double width = textNode.getFullBoundsReference().getWidth() + ( 2 * margin );
+                final double height = textNode.getFullBoundsReference().getHeight() + ( 2 * margin );
+                Shape backgroundShape = new RoundRectangle2D.Double( textNode.getFullBoundsReference().getMinX() - margin, textNode.getFullBoundsReference().getMinY() - margin, width, height, cornerRadius, cornerRadius );
+                backgroundNode.setPathTo( backgroundShape );
             }
         };
         sensor.addValueObserver( updateTextObserver );
