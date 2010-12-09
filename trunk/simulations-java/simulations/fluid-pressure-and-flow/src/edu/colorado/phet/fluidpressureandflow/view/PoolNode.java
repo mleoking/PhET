35f9f3619531940cf45102d4943000b9ca130344
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-12-09 00:31:33 +0000

    Fixed: fluid color should change with density - fade to amber for honey, gray for gasoline.

--- trunk/simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/view/PoolNode.java
+++ trunk/simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/view/PoolNode.java
@@ -1,9 +1,18 @@
 package edu.colorado.phet.fluidpressureandflow.view;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import edu.colorado.phet.common.phetcommon.math.Function;
+import edu.colorado.phet.common.phetcommon.math.MathUtil;
 import edu.colorado.phet.common.phetcommon.model.Property;
+import edu.colorado.phet.common.phetcommon.util.Function1;
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
@@ -18,7 +27,13 @@ public class PoolNode extends PNode {
 
     public PoolNode( final ModelViewTransform transform2D, final Pool pool, Property<Double> liquidDensity ) {
         this.liquidDensity = liquidDensity;
-        final PhetPPath path = new PhetPPath( transform2D.modelToView( pool.getShape() ), createPaint( transform2D, pool ) );
+        final PhetPPath path = new PhetPPath( transform2D.modelToView( pool.getShape() ), createPaint( transform2D, pool ) ) {{
+            waterColor.addObserver( new SimpleObserver() {
+                public void update() {
+                    setPaint( createPaint( transform2D, pool ) );
+                }
+            } );
+        }};
         addChild( path );
         setPickable( false );
         setChildrenPickable( false );
@@ -39,19 +54,144 @@ public class PoolNode extends PNode {
         return new GradientPaint( 0, (float) yTop, topColor, 0, (float) yBottom, bottomColor );
     }
 
-    public static Color getBottomColor( double d ) {
-        Function.LinearFunction r = new Function.LinearFunction( 1000, 1500, 35, 15 );
-        Function.LinearFunction g = new Function.LinearFunction( 1000, 1500, 136, 120 );
-        Function.LinearFunction b = new Function.LinearFunction( 1000, 1500, 158, 140 );
-        Function.LinearFunction a = new Function.LinearFunction( 1000, 1500, 173, 174 );
-        return new Color( (int) r.evaluate( d ), (int) g.evaluate( d ), (int) b.evaluate( d ), (int) a.evaluate( d ) );
+    /*
+     * The rest of this file uses piecewise linear functions to interpolate between gas, water and honey colors, with darker and more opaque near the bottom.
+     */
+    private static final double GAS_DENSITY = 719;//si
+    private static final double WATER_DENSITY = 1000;
+    private static final double HONEY_DENSITY = 1360;
+    private static final Color honeyColor = new Color( 255, 191, 0 );
+    private static final Color gasColor = Color.gray;
+    private static final Property<Color> waterColor = new Property<Color>( new Color( 100, 214, 247 ) );
+
+    {
+        if ( false ) {//debug color choice
+            new JFrame() {{
+                final JColorChooser contentPane = new JColorChooser() {{
+                    getSelectionModel().addChangeListener( new ChangeListener() {
+                        public void stateChanged( ChangeEvent e ) {
+                            waterColor.setValue( getColor() );
+                        }
+                    } );
+                }};
+                setContentPane( contentPane );
+                pack();
+            }}.setVisible( true );
+        }
     }
 
-    public static Color getTopColor( double d ) {
-        Function.LinearFunction rTop = new Function.LinearFunction( 1000, 1500, 90, 91 );
-        Function.LinearFunction gTop = new Function.LinearFunction( 1000, 1500, 212, 213 );
-        Function.LinearFunction bTop = new Function.LinearFunction( 1000, 1500, 238, 241 );
-        Function.LinearFunction aTop = new Function.LinearFunction( 1000, 1500, 158, 172 );
-        return new Color( (int) rTop.evaluate( d ), (int) gTop.evaluate( d ), (int) bTop.evaluate( d ), (int) aTop.evaluate( d ) );
+    public static class PiecewiseFunction {
+        private List<Piece> pieces = new ArrayList<Piece>();
+
+        static class Piece {
+            Function1<Double, Boolean> condition;
+            Function1<Double, Double> function;
+
+            Piece( Function1<Double, Boolean> condition, Function1<Double, Double> function ) {
+                this.condition = condition;
+                this.function = function;
+            }
+        }
+
+        public PiecewiseFunction( Piece[] pieces ) {
+            this.pieces = Arrays.asList( pieces );
+        }
+
+        public int evaluate( double x ) {
+            for ( Piece entry : pieces ) {
+                if ( entry.condition.apply( x ) ) {
+                    return (int) ( entry.function.apply( x ).doubleValue() );
+                }
+            }
+            throw new RuntimeException( "value out of domain: " + x );
+        }
     }
+
+    public static Function1<Double, Boolean> LessThan( final double x ) {
+        return new Function1<Double, Boolean>() {
+            public Boolean apply( Double aDouble ) {
+                return aDouble < x;
+            }
+        };
+    }
+
+    public static Function1<Double, Boolean> GreaterEqual( final double x ) {
+        return new Function1<Double, Boolean>() {
+            public Boolean apply( Double aDouble ) {
+                return aDouble >= x;
+            }
+        };
+    }
+
+    public static Function1<Double, Double> Linear( final Function.LinearFunction linearFunction ) {
+        return new Function1<Double, Double>() {
+            public Double apply( Double aDouble ) {
+                return linearFunction.evaluate( aDouble );
+            }
+        };
+    }
+
+    public static PiecewiseFunction getColorComponent( Function1<Color, Integer> component ) {
+        final Function.LinearFunction rLow = new Function.LinearFunction( GAS_DENSITY, WATER_DENSITY, component.apply( gasColor ), component.apply( waterColor.getValue() ) );
+        final Function.LinearFunction rHigh = new Function.LinearFunction( WATER_DENSITY, HONEY_DENSITY, component.apply( waterColor.getValue() ), component.apply( honeyColor ) );
+
+        PiecewiseFunction colorComponent = new PiecewiseFunction( new PiecewiseFunction.Piece[] {
+                new PiecewiseFunction.Piece( LessThan( WATER_DENSITY ), Linear( rLow ) ),
+                new PiecewiseFunction.Piece( GreaterEqual( WATER_DENSITY ), Linear( rHigh ) )
+        } );
+        return colorComponent;
+    }
+
+    public static Color getTopColor( double density ) {
+        return newColor( getColorComponent( new Function1<Color, Integer>() {
+            public Integer apply( Color color ) {
+                return color.getRed();
+            }
+        } ).evaluate( density ) + 20, getColorComponent( new Function1<Color, Integer>() {
+            public Integer apply( Color color ) {
+                return color.getGreen();
+            }
+        } ).evaluate( density ) + 20, getColorComponent( new Function1<Color, Integer>() {
+            public Integer apply( Color color ) {
+                return color.getBlue();
+            }
+        } ).evaluate( density ) + 20, getColorComponent( new Function1<Color, Integer>() {
+            public Integer apply( Color color ) {
+                return 120;
+            }
+        } ).evaluate( density ) );
+    }
+
+    public static Color getBottomColor( double density ) {
+        return newColor( getColorComponent( new Function1<Color, Integer>() {
+            public Integer apply( Color color ) {
+                return color.getRed();
+            }
+        } ).evaluate( density ) - 20, getColorComponent( new Function1<Color, Integer>() {
+            public Integer apply( Color color ) {
+                return color.getGreen();
+            }
+        } ).evaluate( density ) - 20, getColorComponent( new Function1<Color, Integer>() {
+            public Integer apply( Color color ) {
+                return color.getBlue();
+            }
+        } ).evaluate( density ) - 20, getColorComponent( new Function1<Color, Integer>() {
+            public Integer apply( Color color ) {
+                return 200;
+            }
+        } ).evaluate( density ) );
+    }
+
+    private static Color newColor( int r, int g, int b, int a ) {
+        return new Color( clamp( r ), clamp( g ), clamp( b ), clamp( a ) );
+    }
+
+    private static int clamp( int r ) {
+        return clamp( 0, r, 255 );
+    }
+
+    private static int clamp( int min, int value, int max ) {
+        return (int) MathUtil.clamp( min, value, max );
+    }
+
 }
