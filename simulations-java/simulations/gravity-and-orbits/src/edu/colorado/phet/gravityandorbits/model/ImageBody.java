eca93423467a8799f397a23ca7fba35b54d95e2b
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-12-10 21:27:35 +0000

    Started adding support for cartoon scales

--- simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/model/ImageBody.java
+++ simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/model/ImageBody.java
@@ -9,12 +9,12 @@ import edu.colorado.phet.gravityandorbits.view.BodyRenderer;
  * @author Sam Reid
  */
 public class ImageBody extends Body {
-    public ImageBody( String name, double x, double y, double diameter, double vx, double vy, double mass, Color color, Color highlight, Function.LinearFunction sizer ) {
-        this( name, x, y, diameter, vx, vy, mass, color, highlight, sizer, sizer );
+    public ImageBody( String name, double x, double y, double diameter, double vx, double vy, double mass, Color color, Color highlight, Function.LinearFunction sizer, double cartoonDiameterScaleFactor ) {
+        this( name, x, y, diameter, vx, vy, mass, color, highlight, sizer, sizer, cartoonDiameterScaleFactor );
     }
 
-    public ImageBody( String name, double x, double y, double diameter, double vx, double vy, double mass, Color color, Color highlight, Function.LinearFunction sizer, Function.LinearFunction iconSizer ) {
-        super( name, x, y, diameter, vx, vy, mass, color, highlight, sizer, iconSizer );
+    public ImageBody( String name, double x, double y, double diameter, double vx, double vy, double mass, Color color, Color highlight, Function.LinearFunction sizer, Function.LinearFunction iconSizer, double cartoonDiameterScaleFactor ) {
+        super( name, x, y, diameter, vx, vy, mass, color, highlight, sizer, iconSizer, cartoonDiameterScaleFactor );
     }
 
     @Override
--- simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/view/MassReadoutNode.java
+++ simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/view/MassReadoutNode.java
@@ -7,7 +7,6 @@ import java.text.DecimalFormat;
 
 import edu.colorado.phet.common.phetcommon.model.Property;
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
-import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.gravityandorbits.model.Body;
 import edu.colorado.phet.gravityandorbits.module.GravityAndOrbitsModule;
@@ -18,8 +17,8 @@ import edu.umd.cs.piccolo.nodes.PText;
  * @author Sam Reid
  */
 public class MassReadoutNode extends PNode {
-    public MassReadoutNode( final Body body, final BodyNode bodyNode, final Property<ModelViewTransform> modelViewTransform, final Property<Boolean> visible ) {
-        addChild( new PText( "1 million Planet masses" ) {{
+    public MassReadoutNode( final Body body, final BodyNode bodyNode, final Property<Boolean> visible ) {
+        addChild( new PText( "1 million Earth masses" ) {{
             setPickable( false );
             setChildrenPickable( false );
             setFont( new PhetFont( 18, true ) );
@@ -27,20 +26,20 @@ public class MassReadoutNode extends PNode {
             body.getMassProperty().addObserver( new SimpleObserver() {
                 public void update() {
                     double massKG = body.getMass();
-                    double planetMasses = massKG / GravityAndOrbitsModule.PLANET_MASS;
+                    double earthMasses = massKG / GravityAndOrbitsModule.EARTH_MASS;
                     String text;
                     DecimalFormat decimalFormat = new DecimalFormat( "0" );
-                    if ( planetMasses > 1E3 ) {
-                        text = decimalFormat.format( planetMasses / 1E3 ) + " thousand planet masses";
+                    if ( earthMasses > 1E3 ) {
+                        text = decimalFormat.format( earthMasses / 1E3 ) + " thousand Earth masses";
                     }
-                    else if ( Math.abs( planetMasses - 1 ) < 1E-2 ) {
+                    else if ( Math.abs( earthMasses - 1 ) < 1E-2 ) {
                         text = 1 + " planet mass";
                     }
-                    else if ( planetMasses < 1 ) {
-                        text = new DecimalFormat( "0.000" ).format( planetMasses ) + " planet masses";
+                    else if ( earthMasses < 1 ) {
+                        text = new DecimalFormat( "0.000" ).format( earthMasses ) + " Earth masses";
                     }
                     else {
-                        text = decimalFormat.format( planetMasses ) + " planet masses";
+                        text = decimalFormat.format( earthMasses ) + " Earth masses";
                     }
                     setText( text );
                 }
