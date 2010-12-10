76f0a64971b730c3734d54e72a37ec7f0a316579
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-12-10 21:05:53 +0000

    Added PropertyRadioButton, which wires up a JRadioButton to a property of type T in an enum-style Property<T>.

---
+++ simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/view/controls/PropertyRadioButton.java
@@ -0,0 +1,34 @@
+package edu.colorado.phet.common.phetcommon.view.controls;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+import edu.colorado.phet.common.phetcommon.model.Property;
+import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
+
+/**
+ * PropertyRadioButton wires up a JRadioButton to a property of type T in an enum-style Property<T>.
+ *
+ * @param <T> the type of object to be selected from
+ * @author Sam Reid
+ * @author Chris Malley
+ */
+public class PropertyRadioButton<T> extends JRadioButton {
+    public PropertyRadioButton( String title, final Property<T> property, final T value ) {
+        super( title );
+        final SimpleObserver updateSelected = new SimpleObserver() {
+            public void update() {
+                setSelected( property.getValue() == value );
+            }
+        };
+        addActionListener( new ActionListener() {
+            public void actionPerformed( ActionEvent e ) {
+                property.setValue( value );
+                updateSelected.update();//make sure radio buttons don't toggle off, in case they're not in a button group
+            }
+        } );
+        property.addObserver( updateSelected );
+    }
+}
