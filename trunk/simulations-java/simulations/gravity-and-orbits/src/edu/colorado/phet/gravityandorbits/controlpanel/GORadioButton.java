80a3f4956ea1e1b31f1bb5cf4b22e5623173c1ea
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-12-10 21:05:53 +0000

    Added PropertyRadioButton, which wires up a JRadioButton to a property of type T in an enum-style Property<T>.

--- trunk/simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/controlpanel/GORadioButton.java
+++ trunk/simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/controlpanel/GORadioButton.java
@@ -1,33 +1,17 @@
 package edu.colorado.phet.gravityandorbits.controlpanel;
 
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.*;
 
 import edu.colorado.phet.common.phetcommon.model.Property;
-import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
+import edu.colorado.phet.common.phetcommon.view.controls.PropertyRadioButton;
 
 /**
  * @author Sam Reid
  */
-public class GORadioButton<T> extends JRadioButton {
+public class GORadioButton<T> extends PropertyRadioButton<T> {
     public GORadioButton( String title, final Property<T> property, final T value ) {
-        super( title );
-        final SimpleObserver updateSelected = new SimpleObserver() {
-            public void update() {
-                setSelected( property.getValue() == value );
-            }
-        };
-        addActionListener( new ActionListener() {
-            public void actionPerformed( ActionEvent e ) {
-                property.setValue( value );
-                updateSelected.update();//make sure radio buttons don't toggle off
-            }
-        } );
-        property.addObserver( updateSelected );
-        setOpaque( false );
+        super( title, property, value );
+        setOpaque( false );//TODO: is this a mac problem?
         setFont( GravityAndOrbitsControlPanel.CONTROL_FONT );
         setBackground( GravityAndOrbitsControlPanel.BACKGROUND );
         setForeground( GravityAndOrbitsControlPanel.FOREGROUND );
