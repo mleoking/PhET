841ec99d64109ea12b787a58b7ebd72ec4ca9911
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-12-13 06:48:18 +0000

    converted graphics getter from override to strategy pattern

---
+++ simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/util/Function2.java
@@ -0,0 +1,10 @@
+package edu.colorado.phet.common.phetcommon.util;
+
+/**
+ * A one-parameter function that takes an argument of type U and returns a value of type T.
+ *
+ * @author Sam Reid
+ */
+public interface Function2<V, U, T> {
+    T apply( V v, U u );
+}
