ff436ef433cb9a00273fa951580beb12de1e15a5
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-12-14 18:30:44 +0000

    Added body-specific cartoon scale factors for forces so that all forces are visible in cartoon mode

--- trunk/simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/view/GravityAndOrbitsCanvas.java
+++ trunk/simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/view/GravityAndOrbitsCanvas.java
@@ -76,11 +76,13 @@ public class GravityAndOrbitsCanvas extends PhetPCanvas {
             addChild( new MassReadoutNode( body, bodyNode, module.getShowMassProperty() ) );
         }
 
+        //Add force vector nodes
         for ( Body body : model.getBodies() ) {
-            addChild( new VectorNode( body, modelViewTransformProperty, module.getShowGravityForceProperty(), body.getForceProperty(), forceScale, FORCE_VECTOR_COLOR_FILL, FORCE_VECTOR_COLOR_OUTLINE ) );
+            addChild( new VectorNode( body, modelViewTransformProperty, module.getShowGravityForceProperty(), body.getForceProperty(), forceScale, body.getCartoonForceScale(), FORCE_VECTOR_COLOR_FILL, FORCE_VECTOR_COLOR_OUTLINE ) );
         }
+        //Add velocity vector nodes
         for ( Body body : model.getBodies() ) {
-            addChild( new GrabbableVectorNode( body, modelViewTransformProperty, module.getShowVelocityProperty(), body.getVelocityProperty(), VectorNode.VELOCITY_SCALE, VELOCITY_VECTOR_COLOR_FILL, VELOCITY_VECTOR_COLOR_OUTLINE ) );
+            addChild( new GrabbableVectorNode( body, modelViewTransformProperty, module.getShowVelocityProperty(), body.getVelocityProperty(), VectorNode.VELOCITY_SCALE, 1.0, VELOCITY_VECTOR_COLOR_FILL, VELOCITY_VECTOR_COLOR_OUTLINE ) );
         }
 
         // Control Panel
