9131f66d3b2731942911cd8173be43c5952d93e2
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-12-20 19:23:34 +0000

    Fixed an issue where dual and triple bonds were not being depicted correctly when the molecules were not on a horizontal line.

--- trunk/simulations-java/simulations/greenhouse/src/edu/colorado/phet/greenhouse/view/AtomicBondNode.java
+++ trunk/simulations-java/simulations/greenhouse/src/edu/colorado/phet/greenhouse/view/AtomicBondNode.java
@@ -4,9 +4,11 @@ package edu.colorado.phet.greenhouse.view;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Stroke;
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 
+import edu.colorado.phet.common.phetcommon.math.Vector2D;
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform2D;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
@@ -17,21 +19,21 @@ import edu.umd.cs.piccolo.nodes.PPath;
 
 /**
  * Class that represents an atomic bond in the view.
- * 
+ *
  * @author John Blanco
  */
 public class AtomicBondNode extends PNode {
-    
+
     // ------------------------------------------------------------------------
     // Class Data
     // ------------------------------------------------------------------------
-    
+
     // Constants that control the width of the bond representation with
     // with respect to the average atom radius.
-    private static double BOND_WIDTH_PROPORTION_SINGLE = 0.56;
+    private static double BOND_WIDTH_PROPORTION_SINGLE = 0.45;
     private static double BOND_WIDTH_PROPORTION_DOUBLE = 0.28;
-    private static double BOND_WIDTH_PROPORTION_TRIPLE = 0.14;
-    
+    private static double BOND_WIDTH_PROPORTION_TRIPLE = 0.24;
+
     private static Color BOND_COLOR = new Color(0, 200, 0);
 
     // ------------------------------------------------------------------------
@@ -41,16 +43,16 @@ public class AtomicBondNode extends PNode {
     private final AtomicBond atomicBond;
     private final ModelViewTransform2D mvt;
     private final double averageAtomRadius;
-    
+
     // ------------------------------------------------------------------------
     // Constructor(s)
     // ------------------------------------------------------------------------
-    
+
     public AtomicBondNode( AtomicBond atomicBond, ModelViewTransform2D mvt ){
         assert atomicBond.getBondCount() > 0 && atomicBond.getBondCount() <=3;  // Only single through triple bonds currently supported.
         this.atomicBond = atomicBond;
         this.mvt = mvt;
-        
+
         // Listen to the bond for changes that may require an update of the
         // representation.
         atomicBond.addObserver( new SimpleObserver() {
@@ -58,10 +60,10 @@ public class AtomicBondNode extends PNode {
                 updateRepresentation();
             }
         });
-        
+
         // Calculate the width to use for the bond representation(s).
         averageAtomRadius=mvt.modelToViewDifferentialXDouble((atomicBond.getAtom1().getRadius() + atomicBond.getAtom2().getRadius()) / 2);
-        
+
         // Create the initial representation.
         updateRepresentation();
     }
@@ -86,47 +88,65 @@ public class AtomicBondNode extends PNode {
             addChild(bond);
             break;
         }
-            
-        case 2:{
+
+        case 2:
+        {
             // Double bond.
-            // TODO: This is a temporary version to support horizontal molecules,
-            // needs to be generalized.
-            double transformedRadius = mvt.modelToViewDifferentialXDouble( Math.min( atomicBond.getAtom1().getRadius(),
+            final double transformedRadius = mvt.modelToViewDifferentialXDouble( Math.min( atomicBond.getAtom1().getRadius(),
                     atomicBond.getAtom2().getRadius() ) );
+            // Get the center points of the two atoms.
             Point2D p1 = mvt.modelToViewDouble( atomicBond.getAtom1().getPositionRef() );
             Point2D p2 = mvt.modelToViewDouble( atomicBond.getAtom2().getPositionRef() );
-            bondWidth = (float)(BOND_WIDTH_PROPORTION_DOUBLE * averageAtomRadius);
-            PPath bond1 = new PhetPPath(new BasicStroke(bondWidth), BOND_COLOR);
-            bond1.setPathTo( new Line2D.Double( p1.getX(), p1.getY() + transformedRadius / 3, p2.getX(), p2.getY() + transformedRadius / 3) );
-            PPath bond2 = new PhetPPath(new BasicStroke(bondWidth), BOND_COLOR);
-            bond2.setPathTo( new Line2D.Double( p1.getX(), p1.getY() - transformedRadius / 3, p2.getX(), p2.getY() - transformedRadius / 3) );
+            final double angle = Math.atan2( p1.getX() - p2.getX(), p1.getY() - p2.getY() );
+            // Create a vector that will act as the offset from the center
+            // point to the origin of the bond line.
+            Vector2D offsetVector = new Vector2D(){{
+                setMagnitude( transformedRadius / 3 );
+                setAngle( angle  );
+            }};
+            // Draw the bonds.
+            Stroke bondLineStroke = new BasicStroke( (float) ( BOND_WIDTH_PROPORTION_DOUBLE * averageAtomRadius ) );
+            PPath bond1 = new PhetPPath( bondLineStroke, BOND_COLOR );
+            bond1.setPathTo( new Line2D.Double( p1.getX() + offsetVector.getX(), p1.getY() - offsetVector.getY(), p2.getX() + offsetVector.getX(), p2.getY() - offsetVector.getY() ) );
+            offsetVector.rotate( Math.PI );
+            PPath bond2 = new PhetPPath(bondLineStroke, BOND_COLOR);
+            bond2.setPathTo( new Line2D.Double( p1.getX() + offsetVector.getX(), p1.getY() - offsetVector.getY(), p2.getX() + offsetVector.getX(), p2.getY() - offsetVector.getY() ) );
             addChild(bond1);
             addChild(bond2);
             break;
         }
-            
+
         case 3:
         {
             // Triple bond.
-            // TODO: This is a temporary version to support horizontal molecules,
-            // needs to be generalized.
-            double transformedRadius = mvt.modelToViewDifferentialXDouble( Math.min( atomicBond.getAtom1().getRadius(),
+            // Double bond.
+            final double transformedRadius = mvt.modelToViewDifferentialXDouble( Math.min( atomicBond.getAtom1().getRadius(),
                     atomicBond.getAtom2().getRadius() ) );
+            // Get the center points of the two atoms.
             Point2D p1 = mvt.modelToViewDouble( atomicBond.getAtom1().getPositionRef() );
             Point2D p2 = mvt.modelToViewDouble( atomicBond.getAtom2().getPositionRef() );
-            bondWidth = (float)(BOND_WIDTH_PROPORTION_TRIPLE * averageAtomRadius);
-            PPath bond1 = new PhetPPath(new BasicStroke(bondWidth), BOND_COLOR);
-            bond1.setPathTo( new Line2D.Double( p1.getX(), p1.getY() + transformedRadius / 2, p2.getX(), p2.getY() + transformedRadius / 3) );
-            PPath bond2 = new PhetPPath(new BasicStroke(bondWidth), BOND_COLOR);
-            bond2.setPathTo( new Line2D.Double( p1, p2 ) );
-            PPath bond3 = new PhetPPath(new BasicStroke(bondWidth), BOND_COLOR);
-            bond3.setPathTo( new Line2D.Double( p1.getX(), p1.getY() - transformedRadius / 2, p2.getX(), p2.getY() - transformedRadius / 3) );
+            final double angle = Math.atan2( p1.getX() - p2.getX(), p1.getY() - p2.getY() );
+            // Create a vector that will act as the offset from the center
+            // point to the origin of the bond line.
+            Vector2D offsetVector = new Vector2D(){{
+                setMagnitude( transformedRadius * 0.6 );
+                setAngle( angle  );
+            }};
+            // Draw the bonds.
+            Stroke bondLineStroke = new BasicStroke( (float) ( BOND_WIDTH_PROPORTION_TRIPLE * averageAtomRadius ) );
+            PPath bond1 = new PhetPPath( bondLineStroke, BOND_COLOR );
+            bond1.setPathTo( new Line2D.Double( p1, p2 ) );
+            PPath bond2 = new PhetPPath( bondLineStroke, BOND_COLOR );
+            bond2.setPathTo( new Line2D.Double( p1.getX() + offsetVector.getX(), p1.getY() - offsetVector.getY(), p2.getX() + offsetVector.getX(), p2.getY() - offsetVector.getY() ) );
+            offsetVector.rotate( Math.PI );
+            PPath bond3 = new PhetPPath(bondLineStroke, BOND_COLOR);
+            bond3.setPathTo( new Line2D.Double( p1.getX() + offsetVector.getX(), p1.getY() - offsetVector.getY(), p2.getX() + offsetVector.getX(), p2.getY() - offsetVector.getY() ) );
             addChild(bond1);
             addChild(bond2);
             addChild(bond3);
             break;
         }
-            
+
         default:
             System.err.println(getClass().getName() + " - Error: Can't represent bond number, value = " + atomicBond.getBondCount());
             assert false;
