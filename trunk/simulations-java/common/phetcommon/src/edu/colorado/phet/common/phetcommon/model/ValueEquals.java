0ad81784cb20725cb572ca159a1924cc64246e67
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-12-21 20:23:03 +0000

    rename IsSelected to ValueEquals, to remove association with radio buttons, replace some uses in fluid-pressure-and-flow with PropertyRadioButton.

--- trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/model/ValueEquals.java
+++ trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/model/ValueEquals.java
@@ -4,16 +4,16 @@ import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 
 /**
  * This adapter class converts an enumeration property to a boolean property indicating
- * true if the specified property has the specified value.
+ * true if the specified property's value equals the specified value.
  * <p>
  * Note that this class is not recommend for radio button handlers; use PropertyRadioButton.
  *
  * @param <T> the property value type
  * @author Sam Reid
  */
-public class IsSelected<T> extends BooleanProperty {
+public class ValueEquals<T> extends BooleanProperty {
     
-    public IsSelected( final Property<T> property, final T value ) {
+    public ValueEquals( final Property<T> property, final T value ) {
         super( property.getValue() == value );
         property.addObserver( new SimpleObserver() {
             public void update() {
