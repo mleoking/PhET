52adc5d118890c607e7562a15ded573b79471c2a
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-12-24 00:33:12 +0000

    Added basic infrastructure for the new "Isotopes" simulation (a.k.a. flavor) that will derive from this project.

--- trunk/simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/IsotopesApplication.java
+++ trunk/simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/IsotopesApplication.java
@@ -2,14 +2,6 @@
 
 package edu.colorado.phet.buildanatom;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-
-import javax.swing.JCheckBoxMenuItem;
-import javax.swing.JMenu;
-
 import edu.colorado.phet.buildanatom.developer.ProblemTypeSelectionDialog;
 import edu.colorado.phet.buildanatom.modules.buildatom.BuildAnAtomModule;
 import edu.colorado.phet.buildanatom.modules.game.GameModule;
@@ -23,13 +15,11 @@ import edu.colorado.phet.common.piccolophet.PiccoloPhetApplication;
 /**
  * The main application for this simulation.
  */
-public class BuildAnAtomApplication extends PiccoloPhetApplication {
+public class IsotopesApplication extends PiccoloPhetApplication {
 
     //----------------------------------------------------------------------------
     // Instance data
     //----------------------------------------------------------------------------
-
-    private JCheckBoxMenuItem problemDialogVisibleControl;
 
     public static final Property<Boolean> animateUnstableNucleusProperty = new Property<Boolean>( true );
 
@@ -44,7 +34,7 @@ public class BuildAnAtomApplication extends PiccoloPhetApplication {
      *
      * @param config the configuration for this application
      */
-    public BuildAnAtomApplication( PhetApplicationConfig config )
+    public IsotopesApplication( PhetApplicationConfig config )
     {
         super( config );
         initModules();
@@ -78,35 +68,7 @@ public class BuildAnAtomApplication extends PiccoloPhetApplication {
         }
 
         // Developer menu
-        JMenu developerMenu = frame.getDeveloperMenu();
-
-        problemDialogVisibleControl = new JCheckBoxMenuItem( "Show Problem Type Dialog" );
-        developerMenu.add( problemDialogVisibleControl );
-        problemDialogVisibleControl.addActionListener( new ActionListener() {
-            public void actionPerformed( ActionEvent e ) {
-                setProblemTypeDialogVisible( problemDialogVisibleControl.isSelected());
-            }
-        } );
-
-        // Add a listener that will clear the check mark if the user closes
-        // the problem selection dialog directly.
-        problemTypeSelectionDialog.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e){
-                problemDialogVisibleControl.setSelected(false);
-            }
-        });
-
-        final JCheckBoxMenuItem animateNucleusCheckBox = new JCheckBoxMenuItem( "Animate Unstable Nucleus" ) {{
-            setSelected( animateUnstableNucleusProperty.getValue() );
-            addActionListener( new ActionListener() {
-                public void actionPerformed( ActionEvent e ) {
-                    animateUnstableNucleusProperty.setValue( isSelected() );
-                }
-            } );
-        }};
-
-        developerMenu.add( animateNucleusCheckBox );
+        // Add developer menu items here.
     }
 
     /**
@@ -126,6 +88,6 @@ public class BuildAnAtomApplication extends PiccoloPhetApplication {
          * create your own PhetApplicationConfig and use one of the other launchSim methods
          */
         new PhetApplicationLauncher().launchSim( args, BuildAnAtomConstants.PROJECT_NAME,
-                BuildAnAtomConstants.FLAVOR_NAME_BUILD_AN_ATOM, BuildAnAtomApplication.class );
+                BuildAnAtomConstants.FLAVOR_NAME_ISOTOPES, IsotopesApplication.class );
     }
 }
