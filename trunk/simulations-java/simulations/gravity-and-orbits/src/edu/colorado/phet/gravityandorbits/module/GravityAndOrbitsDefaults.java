b776f702fbe0e71c99a61be65e32dc8cd76bcd10
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-12-24 01:21:54 +0000

    remove totally wrong/misleading javadoc

--- trunk/simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/module/GravityAndOrbitsDefaults.java
+++ trunk/simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/module/GravityAndOrbitsDefaults.java
@@ -5,24 +5,18 @@ package edu.colorado.phet.gravityandorbits.module;
 import java.awt.*;
 
 
-/**
- * Default settings that are common to 2 or more modules.
- * <p/>
- * NOTE! This class is package private, and values herein should only be referenced
- * by the "defaults" classes for each module.  Classes that are module-specific should
- * use the class that corresponds to their module.
- */
 public class GravityAndOrbitsDefaults {
+    
     public static final double SECONDS_PER_MINUTE = 60;
 
-    private GravityAndOrbitsDefaults() {/* Not intended for instantiation */
-    }
+    /* Not intended for instantiation */
+    private GravityAndOrbitsDefaults() {}
 
     // Clock
     public static final int CLOCK_FRAME_RATE = 25; // fps, frames per second (wall time)
-    public static final double NUMBER_DAYS_PER_TICK = 1;
+    public static final double DAYS_PER_TICK = 1;
     public static final int SECONDS_PER_DAY = 86400;
-    public static final double DEFAULT_DT = NUMBER_DAYS_PER_TICK * SECONDS_PER_DAY;
+    public static final double DEFAULT_DT = DAYS_PER_TICK * SECONDS_PER_DAY;
 
     public static final Dimension VIEW_SIZE = new Dimension( 1500, 1500 );
 }
