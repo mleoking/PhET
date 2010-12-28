dbaa36ee3934c8ae3b9ee3e71b04f0ab05f53ec4
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-12-28 22:37:03 +0000

    Added the package and class for the Isotopes and Atomc Mass sim.

---
+++ trunk/simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/modules/buildisotope/view/BuildIsotopeCanvas.java
@@ -0,0 +1,108 @@
+/* Copyright 2007, University of Colorado */
+
+package edu.colorado.phet.buildanatom.modules.buildisotope.view;
+
+import java.awt.Point;
+import java.awt.geom.Dimension2D;
+import java.awt.geom.Point2D;
+
+import edu.colorado.phet.buildanatom.BuildAnAtomConstants;
+import edu.colorado.phet.buildanatom.BuildAnAtomDefaults;
+import edu.colorado.phet.buildanatom.model.Atom;
+import edu.colorado.phet.buildanatom.model.BuildAnAtomModel;
+import edu.colorado.phet.buildanatom.modules.game.view.InteractiveSchematicAtomNode;
+import edu.colorado.phet.buildanatom.view.ElementNameIndicator;
+import edu.colorado.phet.buildanatom.view.ParticleCountLegend;
+import edu.colorado.phet.buildanatom.view.StabilityIndicator;
+import edu.colorado.phet.common.phetcommon.model.BooleanProperty;
+import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
+import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform2D;
+import edu.colorado.phet.common.piccolophet.PhetPCanvas;
+import edu.umd.cs.piccolo.PNode;
+
+/**
+ * Canvas for the tab where the user builds an atom.
+ */
+public class BuildIsotopeCanvas extends PhetPCanvas {
+
+    //----------------------------------------------------------------------------
+    // Instance data
+    //----------------------------------------------------------------------------
+
+    // View
+    private final PNode rootNode;
+
+    // Transform.
+    private final ModelViewTransform2D mvt;
+
+    //----------------------------------------------------------------------------
+    // Constructor(s)
+    //----------------------------------------------------------------------------
+
+    public BuildIsotopeCanvas( final BuildAnAtomModel model ) {
+
+        // Set up the canvas-screen transform.
+        setWorldTransformStrategy( new PhetPCanvas.CenteredStage( this, BuildAnAtomDefaults.STAGE_SIZE ) );
+
+        // Set up the model-canvas transform.  IMPORTANT NOTES: The multiplier
+        // factors for the point in the view can be adjusted to shift the
+        // center right or left, and the scale factor can be adjusted to zoom
+        // in or out (smaller numbers zoom out, larger ones zoom in).
+        mvt = new ModelViewTransform2D(
+                new Point2D.Double( 0, 0 ),
+                new Point( (int) Math.round( BuildAnAtomDefaults.STAGE_SIZE.width * 0.30 ), (int) Math.round( BuildAnAtomDefaults.STAGE_SIZE.height * 0.45 ) ),
+                2.0,
+                true );
+
+        setBackground( BuildAnAtomConstants.CANVAS_BACKGROUND );
+
+        // Root of our scene graph
+        rootNode = new PNode();
+        addWorldChild( rootNode );
+
+        rootNode.addChild( new InteractiveSchematicAtomNode(model, mvt, new BooleanProperty( true ) ));
+
+        // Show the name of the element.
+        ElementNameIndicator elementNameIndicator = new ElementNameIndicator( model.getAtom(), new BooleanProperty( true ) );
+        // Position the name indicator above the nucleus
+        elementNameIndicator.setOffset( mvt.modelToViewX( 0 ), mvt.modelToViewY( Atom.ELECTRON_SHELL_1_RADIUS * 3.0 / 4.0 ) + elementNameIndicator.getFullBounds().getHeight() / 2 );
+        rootNode.addChild( elementNameIndicator );
+
+        // Show whether the nucleus is stable.
+        final StabilityIndicator stabilityIndicator = new StabilityIndicator( model.getAtom(), new BooleanProperty( true ) );
+        // Position the stability indicator under the nucleus
+        model.getAtom().addObserver( new SimpleObserver() {
+            public void update() {
+                stabilityIndicator.setOffset( mvt.modelToViewX( 0 ) - stabilityIndicator.getFullBounds().getWidth() / 2, mvt.modelToViewY( -Atom.ELECTRON_SHELL_1_RADIUS * 3.0 / 4.0 ) - stabilityIndicator.getFullBounds().getHeight() );
+            }
+        } );
+        rootNode.addChild( stabilityIndicator );
+
+        // Show the legend/particle count indicator in the top left.
+        ParticleCountLegend particleCountLegend = new ParticleCountLegend( model.getAtom() );
+        particleCountLegend.setOffset( 20, 10 );//top left corner, but with some padding
+        rootNode.addChild( particleCountLegend );
+    }
+
+    //----------------------------------------------------------------------------
+    // Methods
+    //----------------------------------------------------------------------------
+
+    /*
+     * Updates the layout of stuff on the canvas.
+     */
+    @Override
+    protected void updateLayout() {
+
+        Dimension2D worldSize = getWorldSize();
+        if ( worldSize.getWidth() <= 0 || worldSize.getHeight() <= 0 ) {
+            // canvas hasn't been sized, blow off layout
+            return;
+        }
+        else if ( BuildAnAtomConstants.DEBUG_CANVAS_UPDATE_LAYOUT ) {
+            System.out.println( "ExampleCanvas.updateLayout worldSize=" + worldSize );//XXX
+        }
+
+        //XXX lay out nodes
+    }
+}
