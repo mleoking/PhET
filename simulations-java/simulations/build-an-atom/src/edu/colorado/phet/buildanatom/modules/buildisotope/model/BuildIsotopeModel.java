fd0b56c6328a228a5f1a1ed5ff1d9d73d63c645c
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-12-28 22:38:59 +0000

    Removed the electron and proton portions of this model, since they were left over from the BuildAnAtomModel from which this was derived.

--- simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/modules/buildisotope/model/BuildIsotopeModel.java
+++ simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/modules/buildisotope/model/BuildIsotopeModel.java
@@ -7,25 +7,27 @@ import java.awt.geom.Dimension2D;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
-import java.util.Collections;
 
 import edu.colorado.phet.buildanatom.BuildAnAtomDefaults;
 import edu.colorado.phet.buildanatom.BuildAnAtomStrings;
 import edu.colorado.phet.buildanatom.model.Atom;
 import edu.colorado.phet.buildanatom.model.BuildAnAtomClock;
-import edu.colorado.phet.buildanatom.model.Electron;
 import edu.colorado.phet.buildanatom.model.Neutron;
-import edu.colorado.phet.buildanatom.model.Proton;
 import edu.colorado.phet.buildanatom.model.SubatomicParticle;
 import edu.colorado.phet.buildanatom.model.SubatomicParticleBucket;
 import edu.colorado.phet.buildanatom.modules.game.model.AtomValue;
-import edu.colorado.phet.common.phetcommon.model.Resettable;
 import edu.umd.cs.piccolo.util.PDimension;
 
 /**
- * Main model class for the Build an Isotope module.  Units are picometers (1E-12).
+ * This is the primary model class for the Build an Isotope module.  This
+ * class acts as the main interface for model actions, and contains the
+ * constituent model elements.  It watches all neutrons and, based on where
+ * they are placed by the user, moves them between the neutron bucket and the
+ * atom.
+ *
+ * In this model, units are picometers (1E-12).
  */
-public class BuildIsotopeModel implements Resettable {
+public class BuildIsotopeModel {
 
     //----------------------------------------------------------------------------
     // Class Data
@@ -36,21 +38,18 @@ public class BuildIsotopeModel implements Resettable {
             400,
             400 * BuildAnAtomDefaults.STAGE_SIZE.getHeight() / BuildAnAtomDefaults.STAGE_SIZE.getWidth() );//use the same aspect ratio so circles don't become elliptical
 
-    // Constants that define the number of sub-atomic particles that exist
-    // within the sim.
-    private static final int DEFAULT_NUM_ELECTRONS = 10;
-    private static final int DEFAULT_NUM_PROTONS = 10;
-    private static final int DEFAULT_NUM_NEUTRONS = 13;
+    // Constant that defines the default number of neutrons in the bucket.
+    private static final int DEFAULT_NUM_NEUTRONS_IN_BUCKET = 13;
 
-    // Constants that define the size, position, and appearance of the buckets.
+    // Constants that define the size, position, and appearance of the neutron bucket.
     private static final Dimension2D BUCKET_SIZE = new PDimension( 60, 30 );
-    private static final Point2D PROTON_BUCKET_POSITION = new Point2D.Double( -80, -150 );
     private static final Point2D NEUTRON_BUCKET_POSITION = new Point2D.Double( 0, -150 );
-    private static final Point2D ELECTRON_BUCKET_POSITION = new Point2D.Double( 80, -150 );
 
+    // Distance at which nucleons are captured by the nucleus.
     protected static final double NUCLEUS_CAPTURE_DISTANCE = Atom.ELECTRON_SHELL_1_RADIUS;
 
-    protected static final double ELECTRON_CAPTURE_DISTANCE = Atom.ELECTRON_SHELL_2_RADIUS + 20;
+    // Default atom configuration.
+    private static final AtomValue DEFAULT_ATOM_CONFIG = new AtomValue( 1, 0, 1 ); // Hydrogen.
 
     //----------------------------------------------------------------------------
     // Instance Data
@@ -60,16 +59,11 @@ public class BuildIsotopeModel implements Resettable {
 
     private final Atom atom;
 
-    // The subatomic particles.
-    private final ArrayList<Electron> electrons = new ArrayList<Electron>();
-    private final ArrayList<Proton> protons = new ArrayList<Proton>();
+    // Array that tracks the neutrons, whether they are in the bucket or in
+    // the atom.
     private final ArrayList<Neutron> neutrons = new ArrayList<Neutron>();
 
-    // The buckets which can hold the subatomic particles.
-    private final SubatomicParticleBucket electronBucket = new SubatomicParticleBucket( ELECTRON_BUCKET_POSITION,
-            BUCKET_SIZE, Color.blue, BuildAnAtomStrings.ELECTRONS_NAME, Electron.RADIUS, 0.6, -Electron.RADIUS / 2 );
-    private final SubatomicParticleBucket protonBucket = new SubatomicParticleBucket( PROTON_BUCKET_POSITION,
-            BUCKET_SIZE, Color.red, BuildAnAtomStrings.PROTONS_NAME, Proton.RADIUS );
+    // The buckets that holds the neutrons that are not in the atom.
     private final SubatomicParticleBucket neutronBucket = new SubatomicParticleBucket( NEUTRON_BUCKET_POSITION,
             BUCKET_SIZE, Color.gray, BuildAnAtomStrings.NEUTRONS_NAME, Neutron.RADIUS );
 
@@ -77,60 +71,19 @@ public class BuildIsotopeModel implements Resettable {
     // Constructor(s)
     //----------------------------------------------------------------------------
 
-    public BuildIsotopeModel( BuildAnAtomClock clock ) {
-        this( clock, new AtomValue( DEFAULT_NUM_PROTONS, DEFAULT_NUM_NEUTRONS, DEFAULT_NUM_ELECTRONS ) );
-    }
-
     /**
      * Construct the model with the atoms initially in the bucket.
      */
-    public BuildIsotopeModel( BuildAnAtomClock clock, AtomValue atomValue ) {
+    public BuildIsotopeModel( BuildAnAtomClock clock ) {
         super();
 
         this.clock = clock;
 
         // Create the atom.
-        atom = new Atom( new Point2D.Double( 0, 0 ), clock );
+        atom = new Atom( new Point2D.Double( 0, 0 ), clock, DEFAULT_ATOM_CONFIG );
 
-        for ( int i = 0; i < atomValue.getNumElectrons(); i++ ) {
-            final Electron electron = new Electron( clock );
-            electrons.add( electron );
-            electron.addListener( new SubatomicParticle.Adapter() {
-                @Override
-                public void droppedByUser( SubatomicParticle particle ) {
-                    // The user just released this electron.  If it is close
-                    // enough to the shell, and there is room, send it there.
-                    // Otherwise send it to its bucket.
-                    if ( atom.getRemainingElectronCapacity() > 0 && electron.getPosition().distance( atom.getPosition() ) < ELECTRON_CAPTURE_DISTANCE ) {
-                        atom.addElectron( electron , false );
-                    }
-                    else {
-                        electronBucket.addParticle( electron, false );
-                    }
-                }
-            } );
-        }
 
-        for ( int i = 0; i < atomValue.getNumProtons(); i++ ) {
-            final Proton proton = new Proton( clock );
-            protons.add( proton );
-            proton.addListener( new SubatomicParticle.Adapter() {
-                @Override
-                public void droppedByUser( SubatomicParticle particle ) {
-                    // The user just released this proton.  If it is close
-                    // enough to the nucleus, send it there, otherwise
-                    // send it to its bucket.
-                    if ( proton.getPosition().distance( atom.getPosition() ) < NUCLEUS_CAPTURE_DISTANCE ) {
-                        atom.addProton( proton, false );
-                    }
-                    else {
-                        protonBucket.addParticle( proton, false );
-                    }
-                }
-            } );
-        }
-
-        for ( int i = 0; i < atomValue.getNumNeutrons(); i++ ) {
+        for ( int i = 0; i < DEFAULT_NUM_NEUTRONS_IN_BUCKET; i++ ) {
             final Neutron neutron = new Neutron( clock );
             neutrons.add( neutron );
             neutron.addListener( new SubatomicParticle.Adapter() {
@@ -154,116 +107,11 @@ public class BuildIsotopeModel implements Resettable {
     // Methods
     //----------------------------------------------------------------------------
 
-    public Electron getElectron( int i ) {
-        assert i >= 0 && i < numElectrons();
-        return electrons.get( i );
-    }
-
-    public int numElectrons() {
-        return electrons.size();
-    }
-
-    public Proton getProton( int i ) {
-        assert i >= 0 && i < numProtons();
-        return protons.get( i );
-    }
-
-    public int numProtons() {
-        return protons.size();
-    }
-
-    public Neutron getNeutron( int i ) {
-        assert i >= 0 && i < numNeutrons();
-        return neutrons.get( i );
-    }
-
-    public int numNeutrons() {
-        return neutrons.size();
-    }
-
-    public void reset() {
-
-        // Reset the constituent model elements.
-        atom.reset();
-        electronBucket.reset();
-        neutronBucket.reset();
-        protonBucket.reset();
-
-        // Put all the particles back in the bucket.
-        for ( Electron electron : electrons ) {
-            electronBucket.addParticle( electron, true );
-        }
-        for ( Proton proton : protons ) {
-            protonBucket.addParticle( proton, true );
-        }
-        for ( Neutron neutron : neutrons ) {
-            neutronBucket.addParticle( neutron, true );
-        }
-    }
-
-    public Atom getAtom() {
-        return atom;
-    }
-
     public Rectangle2D getModelViewport() {
         return MODEL_VIEWPORT;
     }
 
     public BuildAnAtomClock getClock() {
         return clock;
-    }
-
-    public SubatomicParticleBucket getElectronBucket() {
-        return electronBucket;
-    }
-
-    public SubatomicParticleBucket getProtonBucket() {
-        return protonBucket;
-    }
-
-    public SubatomicParticleBucket getNeutronBucket() {
-        return neutronBucket;
-    }
-
-    public ArrayList<Proton> getProtons() {
-        return protons;
-    }
-
-    public ArrayList<Neutron> getNeutrons() {
-        return neutrons;
-    }
-
-    public ArrayList<Electron> getElectrons() {
-        return electrons;
-    }
-    public static<T> ArrayList<T> reverse(ArrayList<T>list){
-        ArrayList<T> t = new ArrayList<T>( list );
-        Collections.reverse( t );
-        return t;
-    }
-
-    public Proton getFreeProton() {
-        for ( Proton proton : reverse(protons )) {
-            if (!getAtom().containsProton(proton )){
-                return proton;
-            }
-        }
-        return null;
-    }
-    public Neutron getFreeNeutron() {
-        for ( Neutron neutron : reverse(neutrons ) ) {
-            if (!getAtom().containsNeutron(neutron )){
-                return neutron;
-            }
-        }
-        return null;
-    }
-    public Electron getFreeElectron() {
-        for ( Electron electron : reverse(electrons )) {
-            if (!getAtom().containsElectron(electron )){
-                return electron;
-            }
-        }
-        return null;
     }
 }
