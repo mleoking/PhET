2c09ccc10d595942088019578d37e1823ee9639e
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-12-29 18:33:00 +0000

    Corrected a misleading variable name.

--- simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/controlpanel/GAOCheckBox.java
+++ simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/controlpanel/GAOCheckBox.java
@@ -23,9 +23,9 @@ public class GAOCheckBox extends PropertyCheckBox {
     @Override
     protected void paintComponent( Graphics g ) {
         Graphics2D g2 = (Graphics2D) g;
-        Object originalKey = g2.getRenderingHint( RenderingHints.KEY_ANTIALIASING );
+        Object originalValue = g2.getRenderingHint( RenderingHints.KEY_ANTIALIASING );
         g2.setRenderingHint( RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON );
         super.paintComponent( g );
-        g2.setRenderingHint( RenderingHints.KEY_ANTIALIASING, originalKey );
+        g2.setRenderingHint( RenderingHints.KEY_ANTIALIASING, originalValue );
     }
 }
