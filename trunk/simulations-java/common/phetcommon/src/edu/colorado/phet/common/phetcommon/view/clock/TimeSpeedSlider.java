0c4d1e7485c8eb977e2706948ae1b0e741ac0b4f
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-12-29 18:39:09 +0000

    Added a constructor that specifies the text color.  This enables it to work better on dark backgrounds.

--- trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/view/clock/TimeSpeedSlider.java
+++ trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/view/clock/TimeSpeedSlider.java
@@ -1,6 +1,10 @@
 package edu.colorado.phet.common.phetcommon.view.clock;
 
+import java.awt.Color;
 import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.util.Hashtable;
 
 import javax.swing.JFrame;
@@ -18,24 +22,30 @@ import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
  * Jun 1, 2007, 2:27:44 PM
  */
 public class TimeSpeedSlider extends VerticalLayoutPanel {
-    private LinearValueControl linearSlider;
+    private final LinearValueControl linearSlider;
 
     public TimeSpeedSlider( double min, double max, String textFieldPattern, final ConstantDtClock defaultClock ) {
     	this( min, max, textFieldPattern, defaultClock, PhetCommonResources.getString( "Common.sim.speed" ) );
     }
 
     public TimeSpeedSlider( double min, double max, String textFieldPattern, final ConstantDtClock defaultClock,
-    		String title) {
+            String title) {
+        this( min, max, textFieldPattern, defaultClock, title, Color.BLACK );
+    }
+
+    public TimeSpeedSlider( double min, double max, String textFieldPattern, final ConstantDtClock defaultClock,
+    		String title, final Color textColor ) {
         linearSlider = new LinearValueControl( min, max, "", textFieldPattern, "" );
         linearSlider.setTextFieldVisible( false );
         Hashtable table = new Hashtable();
-        table.put( new Double( min ), new JLabel( PhetCommonResources.getString( "Common.time.slow" ) ) );
-        table.put( new Double( max ), new JLabel( PhetCommonResources.getString( "Common.time.fast" ) ) );
-        final JLabel value = new JLabel(  title );
+        table.put( new Double( min ), new TimeSpeederLabel( PhetCommonResources.getString( "Common.time.slow" ), textColor ) );
+        table.put( new Double( max ), new TimeSpeederLabel( PhetCommonResources.getString( "Common.time.fast" ), textColor ) );
+        final JLabel value = new TimeSpeederLabel( title, textColor );
         value.setFont( new PhetFont( Font.ITALIC, PhetFont.getDefaultFontSize()) );
         table.put( new Double( ( max + min ) / 2 ), value );
         linearSlider.setTickLabels( table );
         defaultClock.addConstantDtClockListener( new ConstantDtClock.ConstantDtClockAdapter() {
+            @Override
             public void dtChanged( ConstantDtClock.ConstantDtClockEvent event ) {
                 update( defaultClock );
             }
@@ -59,11 +69,18 @@ public class TimeSpeedSlider extends VerticalLayoutPanel {
     public void setValue( double dt ) {
         linearSlider.setValue( dt );
     }
-    
+
     public static void main(String[] args) {
 		JFrame testFrame = new JFrame();
 		testFrame.add(new TimeSpeedSlider(0, 100, "Test Slider", new ConstantDtClock(10, 10)));
 		testFrame.pack();
 		testFrame.setVisible(true);
 	}
+
+    private static class TimeSpeederLabel extends JLabel{
+        public TimeSpeederLabel( String text, Color textColor ) {
+            super( text );
+            setForeground( textColor );
+        }
+    }
 }
