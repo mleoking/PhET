8f7b60f6c7fa15659f7ef2d9cee708b6f067caa4
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-12-30 20:11:21 +0000

    App for testing check box differences.

---
+++ trunk/simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/CheckBoxTestApp.java
@@ -0,0 +1,59 @@
+/* Copyright 2010, University of Colorado */
+
+package edu.colorado.phet.gravityandorbits;
+
+import java.awt.Dimension;
+import java.awt.GridLayout;
+
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import edu.colorado.phet.common.piccolophet.PhetPCanvas;
+import edu.umd.cs.piccolox.pswing.PSwing;
+
+/**
+ * Little app for testing check box behavior on and off of Piccolo canvases.
+ */
+public class CheckBoxTestApp {
+
+    public static void main( String[] args ) {
+
+        final JCheckBox playAreaCheckBox = new JCheckBox("Play area check box");
+        final JCheckBox controlAreaCheckBox = new JCheckBox("Control area check box");
+        JCheckBox enableCheckBoxes = new JCheckBox("CheckBoxesEnabled"){{
+            setSelected( true );
+            addChangeListener( new ChangeListener() {
+                public void stateChanged( ChangeEvent e ) {
+                    playAreaCheckBox.setEnabled( isSelected() );
+                    controlAreaCheckBox.setEnabled( isSelected() );
+                }
+            });
+        }};
+
+        // Control panel
+        JPanel controlPanel = new JPanel(new GridLayout(2, 1));
+        controlPanel.add( enableCheckBoxes );
+        controlPanel.add( controlAreaCheckBox );
+
+        // Canvas
+        PhetPCanvas canvas = new PhetPCanvas();
+        canvas.setPreferredSize( new Dimension( 400, 400 ) );
+        canvas.addWorldChild( new PSwing( playAreaCheckBox ) );
+
+        // App panel
+        JPanel appPanel = new JPanel();
+        appPanel.add( canvas );
+        appPanel.add( controlPanel );
+
+        // Frame
+        JFrame frame = new JFrame();
+        frame.setContentPane( appPanel );
+        frame.pack();
+        frame.setDefaultCloseOperation( WindowConstants.EXIT_ON_CLOSE );
+        frame.setVisible( true );
+    }
+}
