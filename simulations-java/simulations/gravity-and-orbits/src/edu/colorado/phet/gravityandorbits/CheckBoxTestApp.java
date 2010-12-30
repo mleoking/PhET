07161228b93590b0a3f05abd6bcb87eb50b5b913
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-12-30 21:21:02 +0000

    moved into swing thread and set system UI

--- simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/CheckBoxTestApp.java
+++ simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/CheckBoxTestApp.java
@@ -4,10 +4,7 @@ package edu.colorado.phet.gravityandorbits;
 
 import java.awt.*;
 
-import javax.swing.JCheckBox;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.WindowConstants;
+import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
@@ -20,43 +17,61 @@ import edu.umd.cs.piccolox.pswing.PSwing;
 public class CheckBoxTestApp {
 
     public static void main( String[] args ) {
-
-        final JCheckBox playAreaCheckBox = new JCheckBox("Play area check box"){{
-            setBackground( Color.black );//make the background black to demonstrate that the checkbox still looks checkable, even when disabled
-        }};
-        final JCheckBox controlAreaCheckBox = new JCheckBox("Control area check box"){{
-            setBackground( Color.black );//make the background black to demonstrate that the checkbox still looks checkable, even when disabled
-        }};
-        JCheckBox enableCheckBoxes = new JCheckBox("CheckBoxesEnabled"){{
-            setSelected( true );
-            addChangeListener( new ChangeListener() {
-                public void stateChanged( ChangeEvent e ) {
-                    playAreaCheckBox.setEnabled( isSelected() );
-                    controlAreaCheckBox.setEnabled( isSelected() );
+        SwingUtilities.invokeLater( new Runnable() {
+            public void run() {
+                try {
+                    UIManager.setLookAndFeel( UIManager.getSystemLookAndFeelClassName() );
                 }
-            });
-        }};
+                catch ( ClassNotFoundException e ) {
+                    e.printStackTrace();
+                }
+                catch ( InstantiationException e ) {
+                    e.printStackTrace();
+                }
+                catch ( IllegalAccessException e ) {
+                    e.printStackTrace();
+                }
+                catch ( UnsupportedLookAndFeelException e ) {
+                    e.printStackTrace();
+                }
+                final JCheckBox playAreaCheckBox = new JCheckBox( "Play area check box" ) {{
+                    setBackground( Color.black );//make the background black to demonstrate that the checkbox still looks checkable, even when disabled
+                }};
+                final JCheckBox controlAreaCheckBox = new JCheckBox( "Control area check box" ) {{
+                    setBackground( Color.black );//make the background black to demonstrate that the checkbox still looks checkable, even when disabled
+                }};
+                JCheckBox enableCheckBoxes = new JCheckBox( "CheckBoxesEnabled" ) {{
+                    setSelected( true );
+                    addChangeListener( new ChangeListener() {
+                        public void stateChanged( ChangeEvent e ) {
+                            playAreaCheckBox.setEnabled( isSelected() );
+                            controlAreaCheckBox.setEnabled( isSelected() );
+                        }
+                    } );
+                }};
 
-        // Control panel
-        JPanel controlPanel = new JPanel(new GridLayout(2, 1));
-        controlPanel.add( enableCheckBoxes );
-        controlPanel.add( controlAreaCheckBox );
+                // Control panel
+                JPanel controlPanel = new JPanel( new GridLayout( 2, 1 ) );
+                controlPanel.add( enableCheckBoxes );
+                controlPanel.add( controlAreaCheckBox );
 
-        // Canvas
-        PhetPCanvas canvas = new PhetPCanvas();
-        canvas.setPreferredSize( new Dimension( 400, 400 ) );
-        canvas.addWorldChild( new PSwing( playAreaCheckBox ) );
+                // Canvas
+                PhetPCanvas canvas = new PhetPCanvas();
+                canvas.setPreferredSize( new Dimension( 400, 400 ) );
+                canvas.addWorldChild( new PSwing( playAreaCheckBox ) );
 
-        // App panel
-        JPanel appPanel = new JPanel();
-        appPanel.add( canvas );
-        appPanel.add( controlPanel );
+                // App panel
+                JPanel appPanel = new JPanel();
+                appPanel.add( canvas );
+                appPanel.add( controlPanel );
 
-        // Frame
-        JFrame frame = new JFrame();
-        frame.setContentPane( appPanel );
-        frame.pack();
-        frame.setDefaultCloseOperation( WindowConstants.EXIT_ON_CLOSE );
-        frame.setVisible( true );
+                // Frame
+                JFrame frame = new JFrame();
+                frame.setContentPane( appPanel );
+                frame.pack();
+                frame.setDefaultCloseOperation( WindowConstants.EXIT_ON_CLOSE );
+                frame.setVisible( true );
+            }
+        } );
     }
 }
