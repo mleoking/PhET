1d9fcc5090e3a000c83e843f089c43361a250cf5
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2010-12-30 00:05:10 +0000

    Cleaned up warnings.

--- simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/modules/interactiveisotope/view/InteractiveIsotopeNode.java
+++ simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/modules/interactiveisotope/view/InteractiveIsotopeNode.java
@@ -2,25 +2,13 @@
 
 package edu.colorado.phet.buildanatom.modules.interactiveisotope.view;
 
-import java.awt.BasicStroke;
-import java.awt.Color;
-import java.awt.Shape;
-
-import edu.colorado.phet.buildanatom.model.BuildAnAtomModel;
-import edu.colorado.phet.buildanatom.model.Electron;
 import edu.colorado.phet.buildanatom.model.Neutron;
-import edu.colorado.phet.buildanatom.model.Proton;
 import edu.colorado.phet.buildanatom.model.SubatomicParticle;
-import edu.colorado.phet.buildanatom.modules.game.model.AtomValue;
 import edu.colorado.phet.buildanatom.modules.game.view.SchematicAtomNode;
 import edu.colorado.phet.buildanatom.modules.interactiveisotope.model.InteractiveIsotopeModel;
 import edu.colorado.phet.buildanatom.view.BucketNode;
 import edu.colorado.phet.common.phetcommon.model.BooleanProperty;
-import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform2D;
-import edu.colorado.phet.common.phetcommon.view.util.DoubleGeneralPath;
-import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
-import edu.umd.cs.piccolo.PNode;
 
 /**
  * This class defines a Piccolo Node that represents an atom in "schematic"
@@ -31,15 +19,11 @@ import edu.umd.cs.piccolo.PNode;
  */
 public class InteractiveIsotopeNode extends SchematicAtomNode {
 
-    private final InteractiveIsotopeModel model;
-
     /**
      * Constructor.
      */
     public InteractiveIsotopeNode( final InteractiveIsotopeModel model, ModelViewTransform2D mvt, final BooleanProperty viewOrbitals ) {
         super( model.getAtom(), mvt, viewOrbitals );
-
-        this.model = model;
 
         // Add the bucket that holds the neutrons.
         BucketNode neutronBucketNode = new BucketNode( model.getNeutronBucket(), mvt );
