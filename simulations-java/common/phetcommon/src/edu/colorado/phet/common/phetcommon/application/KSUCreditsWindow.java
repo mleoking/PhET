f341069300d31c34295c82a14eaf49a4eb5c1dae
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-01-03 22:30:57 +0000

    #2624, "splash" window for KSU translation credits.  Approved logo image and assets.

---
+++ simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/application/KSUCreditsWindow.java
@@ -0,0 +1,59 @@
+/* Copyright 2011, University of Colorado */
+
+package edu.colorado.phet.common.phetcommon.application;
+
+import java.awt.Frame;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JWindow;
+import javax.swing.border.EmptyBorder;
+
+import edu.colorado.phet.common.phetcommon.resources.PhetCommonResources;
+import edu.colorado.phet.common.phetcommon.view.VerticalLayoutPanel;
+import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
+import edu.colorado.phet.common.phetcommon.view.util.SwingUtils;
+
+/**
+ * Window that displays translation credits for KSU ERCSME.
+ * See #2624.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
+public class KSUCreditsWindow extends JWindow {
+
+    private static final String TRANSLATED_BY = PhetCommonResources.getString( "Common.About.CreditsDialog.TranslationCreditsTitle" );
+
+    public KSUCreditsWindow( Frame parent ) {
+        super( parent );
+
+        JLabel label = new JLabel( TRANSLATED_BY );
+        label.setFont( new PhetFont( 24 ) );
+
+        JLabel logo =  new JLabel( new ImageIcon( PhetCommonResources.getImage( ( "logos/ERCSME-logo.png" ) ) ) );
+
+        VerticalLayoutPanel panel = new VerticalLayoutPanel();
+        int margin = 18;
+        panel.setBorder( new EmptyBorder( margin, margin, margin, margin ) );
+        panel.add( label );
+        panel.add( logo );
+
+        setContentPane( panel );
+        pack();
+
+        // click on the window to make it go away
+        addMouseListener( new MouseAdapter() {
+            public void mousePressed( MouseEvent event ) {
+                dispose();
+            }
+        });
+    }
+
+    public static void main( String[] args ) {
+        JWindow window = new KSUCreditsWindow( null );
+        SwingUtils.centerWindowOnScreen( window );
+        window.setVisible(  true );
+    }
+}
