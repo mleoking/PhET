8694f2b212a0b058fd178525fd4bffa0d762ee4f
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-01-03 23:27:38 +0000

    Added test buttons for setting the atom configuration.  These will eventually be removed.

--- trunk/simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/modules/interactiveisotope/view/InteractiveIsotopeCanvas.java
+++ trunk/simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/modules/interactiveisotope/view/InteractiveIsotopeCanvas.java
@@ -3,17 +3,20 @@
 package edu.colorado.phet.buildanatom.modules.interactiveisotope.view;
 
 import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.geom.Dimension2D;
 import java.awt.geom.Point2D;
 
 import edu.colorado.phet.buildanatom.BuildAnAtomConstants;
 import edu.colorado.phet.buildanatom.BuildAnAtomDefaults;
-import edu.colorado.phet.buildanatom.model.SubatomicParticle;
+import edu.colorado.phet.buildanatom.modules.game.model.AtomValue;
 import edu.colorado.phet.buildanatom.modules.interactiveisotope.model.InteractiveIsotopeModel;
 import edu.colorado.phet.buildanatom.view.ParticleCountLegend;
 import edu.colorado.phet.common.phetcommon.model.BooleanProperty;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform2D;
 import edu.colorado.phet.common.piccolophet.PhetPCanvas;
+import edu.colorado.phet.common.piccolophet.nodes.ButtonNode;
 import edu.umd.cs.piccolo.PNode;
 
 /**
@@ -56,7 +59,35 @@ public class InteractiveIsotopeCanvas extends PhetPCanvas {
         rootNode = new PNode();
         addWorldChild( rootNode );
 
+        // Add the node that contains both the atom and the neutron bucket.
         rootNode.addChild( new InteractiveIsotopeNode(model, mvt, new BooleanProperty( true ) ));
+
+        // TODO: These buttons are for unit test purposes and should
+        // eventually be removed.
+        ButtonNode testButton1 = new ButtonNode("Helium");
+        testButton1.addActionListener( new ActionListener() {
+            public void actionPerformed( ActionEvent e ) {
+                model.setAtomConfiguration( new AtomValue(2, 2, 2) );
+            }
+        });
+        testButton1.setOffset( 700, 50 );
+        addWorldChild( testButton1 );
+        ButtonNode testButton2 = new ButtonNode("Lithium");
+        testButton2.addActionListener( new ActionListener() {
+            public void actionPerformed( ActionEvent e ) {
+                model.setAtomConfiguration( new AtomValue(3, 4, 3) );
+            }
+        });
+        testButton2.setOffset( 700, 100 );
+        addWorldChild( testButton2 );
+        ButtonNode testButton3 = new ButtonNode("Clear");
+        testButton3.addActionListener( new ActionListener() {
+            public void actionPerformed( ActionEvent e ) {
+                model.setAtomConfiguration( new AtomValue(0, 0, 0) );
+            }
+        });
+        testButton3.setOffset( 700, 150 );
+        addWorldChild( testButton3 );
 
         /*
          * TODO: Commented out while details of atom presentation are sorted through.
