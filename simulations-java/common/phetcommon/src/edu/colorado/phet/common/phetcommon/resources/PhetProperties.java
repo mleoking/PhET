5a2613f0b19b7876fc80d13aded55500a5a9bad1
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-01-03 22:43:09 +0000

    #2624, add ability to suppress warning when optional localized strings are missing

--- simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/resources/PhetProperties.java
+++ simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/resources/PhetProperties.java
@@ -2,13 +2,17 @@
 package edu.colorado.phet.common.phetcommon.resources;
 
 import java.util.Properties;
+import java.util.logging.Logger;
 
+import edu.colorado.phet.common.phetcommon.util.logging.LoggingUtils;
 import edu.colorado.phet.common.phetcommon.view.util.StringUtil;
 
 /**
  * An extension of Properties that provides some convenience methods.
  */
 public class PhetProperties extends Properties {
+
+    private static final Logger LOGGER = LoggingUtils.getLogger( PhetProperties.class.getCanonicalName() );
 
     public PhetProperties() {
         super();
@@ -19,9 +23,15 @@ public class PhetProperties extends Properties {
     }
 
     public String getString( String propertyName ) {
+        return getString( propertyName, true );
+    }
+
+    public String getString( String propertyName, boolean warnIfMissing ) {
         String s = super.getProperty( propertyName );
         if ( s == null ) {
-            System.err.println( "WARNING: requested property not found: " + propertyName );
+            if ( warnIfMissing ) {
+                LOGGER.warning( "requested property not found: " + propertyName );
+            }
             s = propertyName;
         }
         return s;
@@ -34,7 +44,7 @@ public class PhetProperties extends Properties {
     public double getDouble( String key, double defaultValue ) {
         return StringUtil.asDouble( getString( key ), defaultValue );
     }
-    
+
     public long getLong( String key, long defaultValue ) {
         return StringUtil.asLong( getString( key ), defaultValue );
     }
@@ -42,7 +52,7 @@ public class PhetProperties extends Properties {
     public char getChar( String key, char defaultValue ) {
         return StringUtil.asChar( getString( key ), defaultValue );
     }
-    
+
     public boolean getBoolean( String key, boolean defaultValue ) {
         boolean value = defaultValue;
         String s = getString( key );
