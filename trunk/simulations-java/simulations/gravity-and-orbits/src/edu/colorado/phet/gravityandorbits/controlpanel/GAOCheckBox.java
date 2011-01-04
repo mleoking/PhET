a4c261fcde48683d2a2e71804eb31f50c1a0b503
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-01-04 23:17:22 +0000

    Removed application specific overrides for adding antialiasing

--- trunk/simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/controlpanel/GAOCheckBox.java
+++ trunk/simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/controlpanel/GAOCheckBox.java
@@ -2,8 +2,6 @@
 
 package edu.colorado.phet.gravityandorbits.controlpanel;
 
-import java.awt.*;
-
 import edu.colorado.phet.common.phetcommon.model.Property;
 import edu.colorado.phet.common.phetcommon.view.controls.PropertyCheckBox;
 
@@ -19,17 +17,5 @@ public class GAOCheckBox extends PropertyCheckBox {
         setFont( GravityAndOrbitsControlPanel.CONTROL_FONT );
         setBackground( GravityAndOrbitsControlPanel.BACKGROUND );
         setForeground( GravityAndOrbitsControlPanel.FOREGROUND );
-    }
-
-    /*
-     * Needed because aliasing looks problematic for these fonts + colors on Windows.
-     */
-    @Override
-    protected void paintComponent( Graphics g ) {
-        Graphics2D g2 = (Graphics2D) g;
-        Object originalValue = g2.getRenderingHint( RenderingHints.KEY_ANTIALIASING );
-        g2.setRenderingHint( RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON );
-        super.paintComponent( g );
-        g2.setRenderingHint( RenderingHints.KEY_ANTIALIASING, originalValue );
     }
 }
