06c697145f9bfa7887744ca095aad28adf017257
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-01-04 03:36:20 +0000

    started 3 patch image node for pressure sensor

---
+++ simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/ThreePatchImageNode.java
@@ -0,0 +1,33 @@
+// Copyright 2010-2011, University of Colorado
+
+package edu.colorado.phet.fluidpressureandflow;
+
+import javax.swing.*;
+
+import edu.umd.cs.piccolo.PCanvas;
+import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.nodes.PImage;
+
+/**
+ * @author Sam Reid
+ */
+public class ThreePatchImageNode extends PNode {
+    public ThreePatchImageNode() {
+        addChild( new PImage( FluidPressureAndFlowResources.RESOURCES.getImage( "pressure_meter_left.png" ) ) );
+        addChild( new PImage( FluidPressureAndFlowResources.RESOURCES.getImage( "pressure_meter_center.png" ) ) );
+        addChild( new PImage( FluidPressureAndFlowResources.RESOURCES.getImage( "pressure_meter_right.png" ) ) );
+    }
+
+    public static void main( String[] args ) {
+        SwingUtilities.invokeLater( new Runnable() {
+            public void run() {
+                new JFrame() {{
+                    setContentPane( new PCanvas() {{
+                        getLayer().addChild( new ThreePatchImageNode() );
+                    }} );
+                    setDefaultCloseOperation( EXIT_ON_CLOSE );
+                }}.setVisible( true );
+            }
+        } );
+    }
+}
