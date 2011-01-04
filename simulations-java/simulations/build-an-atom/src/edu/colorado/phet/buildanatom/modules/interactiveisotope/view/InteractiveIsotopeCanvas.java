c1670724aae6c202e32483b95ca9f18721fbcf08
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-01-04 16:53:00 +0000

    Added first cut at interactive periodic table control and did some changes to the layout to make it look decent.  Still much to be done here.

--- simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/modules/interactiveisotope/view/InteractiveIsotopeCanvas.java
+++ simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/modules/interactiveisotope/view/InteractiveIsotopeCanvas.java
@@ -14,13 +14,17 @@ import edu.colorado.phet.buildanatom.model.Atom;
 import edu.colorado.phet.buildanatom.modules.game.model.AtomValue;
 import edu.colorado.phet.buildanatom.modules.interactiveisotope.model.InteractiveIsotopeModel;
 import edu.colorado.phet.buildanatom.view.ParticleCountLegend;
+import edu.colorado.phet.buildanatom.view.PeriodicTableNode;
 import edu.colorado.phet.buildanatom.view.StabilityIndicator;
 import edu.colorado.phet.common.phetcommon.model.BooleanProperty;
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform2D;
 import edu.colorado.phet.common.piccolophet.PhetPCanvas;
+import edu.colorado.phet.common.piccolophet.event.CursorHandler;
 import edu.colorado.phet.common.piccolophet.nodes.ButtonNode;
 import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
+import edu.umd.cs.piccolo.event.PInputEvent;
 
 /**
  * Canvas for the tab where the user builds an atom.
@@ -52,8 +56,8 @@ public class InteractiveIsotopeCanvas extends PhetPCanvas {
         // in or out (smaller numbers zoom out, larger ones zoom in).
         mvt = new ModelViewTransform2D(
                 new Point2D.Double( 0, 0 ),
-                new Point( (int) Math.round( BuildAnAtomDefaults.STAGE_SIZE.width * 0.50 ), (int) Math.round( BuildAnAtomDefaults.STAGE_SIZE.height * 0.55 ) ),
-                2.0,
+                new Point( (int) Math.round( BuildAnAtomDefaults.STAGE_SIZE.width * 0.50 ), (int) Math.round( BuildAnAtomDefaults.STAGE_SIZE.height * 0.60 ) ),
+                1.75,
                 true );
 
         setBackground( BuildAnAtomConstants.CANVAS_BACKGROUND );
@@ -65,7 +69,7 @@ public class InteractiveIsotopeCanvas extends PhetPCanvas {
         // Add the node that contains both the atom and the neutron bucket.
         rootNode.addChild( new InteractiveIsotopeNode(model, mvt, new BooleanProperty( true ) ));
 
-        // Show whether the nucleus is stable.
+        // Add indicator that shows whether the nucleus is stable.
         final StabilityIndicator stabilityIndicator = new StabilityIndicator( model.getAtom(), new BooleanProperty( true ) );
         // Position the stability indicator under the nucleus
         model.getAtom().addObserver( new SimpleObserver() {
@@ -75,7 +79,45 @@ public class InteractiveIsotopeCanvas extends PhetPCanvas {
         } );
         rootNode.addChild( stabilityIndicator );
 
+        // Add the interactive periodic table that allows the user to select
+        // the initial element.
+        final PeriodicTableNode periodicTableNode = new PeriodicTableNode( model.getAtom() ){
+            @Override
+            protected void elementCellCreated( final PeriodicTableNode.ElementCell elementCell ) {
+                elementCell.addInputEventListener( new CursorHandler() );
+                elementCell.addInputEventListener( new PBasicInputEventHandler() {
+                    @Override
+                    public void mousePressed( PInputEvent event ) {
+                        AtomValue atomConfig;
+                        switch ( elementCell.getAtomicNumber() ){
+                        case 1:
+                            atomConfig = new AtomValue(1, 0, 1);
+                            break;
+                        case 2:
+                            atomConfig = new AtomValue(2, 2, 2);
+                            break;
+                        case 3:
+                            atomConfig = new AtomValue(3, 4, 3);
+                            break;
+                        case 10:
+                            atomConfig = new AtomValue(10, 10, 10);
+                            break;
+                        default:
+                            atomConfig = new AtomValue(5, 5, 5);
+                            break;
+                        }
+
+                        model.setAtomConfiguration( atomConfig );
+                    }
+                } );
+            }
+        };
+        periodicTableNode.setScale( 1.4 );
+        periodicTableNode.setOffset( 20, 20 );
+        addScreenChild( periodicTableNode );
+
         // TODO: These buttons are for unit test purposes and should
+        /*
         // eventually be removed.
         ButtonNode testButton1 = new ButtonNode("Helium");
         testButton1.addActionListener( new ActionListener() {
@@ -101,34 +143,13 @@ public class InteractiveIsotopeCanvas extends PhetPCanvas {
         });
         testButton3.setOffset( 100, 150 );
         addWorldChild( testButton3 );
-
-        /*
-         * TODO: Commented out while details of atom presentation are sorted through.
-
-        rootNode.addChild( new InteractiveSchematicAtomNode(model, mvt, new BooleanProperty( true ) ));
-
-        // Show the name of the element.
-        ElementNameIndicator elementNameIndicator = new ElementNameIndicator( model.getAtom(), new BooleanProperty( true ) );
-        // Position the name indicator above the nucleus
-        elementNameIndicator.setOffset( mvt.modelToViewX( 0 ), mvt.modelToViewY( Atom.ELECTRON_SHELL_1_RADIUS * 3.0 / 4.0 ) + elementNameIndicator.getFullBounds().getHeight() / 2 );
-        rootNode.addChild( elementNameIndicator );
-
-        // Show whether the nucleus is stable.
-        final StabilityIndicator stabilityIndicator = new StabilityIndicator( model.getAtom(), new BooleanProperty( true ) );
-        // Position the stability indicator under the nucleus
-        model.getAtom().addObserver( new SimpleObserver() {
-            public void update() {
-                stabilityIndicator.setOffset( mvt.modelToViewX( 0 ) - stabilityIndicator.getFullBounds().getWidth() / 2, mvt.modelToViewY( -Atom.ELECTRON_SHELL_1_RADIUS * 3.0 / 4.0 ) - stabilityIndicator.getFullBounds().getHeight() );
-            }
-        } );
-        rootNode.addChild( stabilityIndicator );
-                 */
-
+        */
 
         // Add the legend/particle count indicator.
         ParticleCountLegend particleCountLegend = new ParticleCountLegend( model.getAtom() );
-        particleCountLegend.setOffset( 700, 10 );
-        rootNode.addChild( particleCountLegend );
+        particleCountLegend.setScale( 1.25 );
+        particleCountLegend.setOffset( 575, 20 );
+        addScreenChild( particleCountLegend );
     }
 
     //----------------------------------------------------------------------------
