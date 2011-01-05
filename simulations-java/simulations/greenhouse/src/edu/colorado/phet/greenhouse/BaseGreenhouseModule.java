19080328591d404fabfd75ba6f76121cc0d43d63
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-01-05 21:21:28 +0000

    Cleaned up EarthGraphic - removed some unused code that had been there for a while as well as some code that was an aborted attempt at a fix for a bug, see #2641.

--- simulations-java/simulations/greenhouse/src/edu/colorado/phet/greenhouse/BaseGreenhouseModule.java
+++ simulations-java/simulations/greenhouse/src/edu/colorado/phet/greenhouse/BaseGreenhouseModule.java
@@ -120,7 +120,7 @@ public abstract class BaseGreenhouseModule extends Module {
         earth.addPhotonAbsorberListener( new PhotonAbsorberListener() );
         earth.getPhotonSource().addListener( model );
         // EarthGraphic adds itself to the apparatus panel. Manages its own transform, too
-        earthGraphic = new EarthGraphic( getApparatusPanel(), earth, modelBounds ,getPhotonToGraphicsMap() );
+        earthGraphic = new EarthGraphic( getApparatusPanel(), earth, modelBounds );
         earth.setReflectivityAssessor( earthGraphic );
 
         // Create the atmosphere
