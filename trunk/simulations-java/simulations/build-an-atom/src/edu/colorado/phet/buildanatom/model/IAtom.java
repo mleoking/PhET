6945084aa3eb3df8ad68086bb890b37a9c9def1f
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-01-06 19:37:27 +0000

    add TODO about eliminating IAtom interface

--- trunk/simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/model/IAtom.java
+++ trunk/simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/model/IAtom.java
@@ -2,9 +2,12 @@ package edu.colorado.phet.buildanatom.model;
 
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 
+//TODO This is only needed by PeriodicTableNode.  Consider replacing this entire interface with a Property<Integer>.
 /**
-* @author Sam Reid
-*/
+ * Interface implemented by all atoms.
+ *
+ * @author Sam Reid
+ */
 public interface IAtom {
     int getNumProtons();
     void addObserver( SimpleObserver simpleObserver );
