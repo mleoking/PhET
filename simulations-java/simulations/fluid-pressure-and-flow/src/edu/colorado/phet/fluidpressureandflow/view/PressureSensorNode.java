97088f1646d2a58c684c1ae9259e1a63efe8db6e
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-01-10 23:38:20 +0000

    refactored SensorNode and subclasses

--- simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/view/PressureSensorNode.java
+++ simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/view/PressureSensorNode.java
@@ -6,7 +6,6 @@ import java.awt.geom.Point2D;
 import edu.colorado.phet.common.phetcommon.math.MathUtil;
 import edu.colorado.phet.common.phetcommon.model.Property;
 import edu.colorado.phet.common.phetcommon.util.Function1;
-import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
 import edu.colorado.phet.fluidpressureandflow.ThreePatchImageNode;
 import edu.colorado.phet.fluidpressureandflow.model.Pool;
@@ -31,17 +30,9 @@ public class PressureSensorNode extends SensorNode<Double> {
     public PressureSensorNode( final ModelViewTransform transform, final PressureSensor sensor, final Property<Units.Unit> unitsProperty, final Pool pool ) {
         super( transform, sensor, unitsProperty );
 
-        final Property<String> text = new Property<String>( getText.apply() );
-        addChild( new ThreePatchImageNode( text ) {{
+        addChild( new ThreePatchImageNode( textProperty ) {{
             translate( 0, -getFullBounds().getHeight() / 2 );//make its hot spot be its opening which is on its center left
         }} );
-        final SimpleObserver updateText = new SimpleObserver() {
-            public void update() {
-                text.setValue( getText.apply() );
-            }
-        };
-        sensor.addValueObserver( updateText );
-        unitsProperty.addObserver( updateText );
         addInputEventListener( new RelativeDragHandler( this, transform, sensor.getLocationProperty(), new Function1<Point2D, Point2D>() {
             //TODO: Factor pool to subclass or general constraint method
             public Point2D apply( Point2D point2D ) {
