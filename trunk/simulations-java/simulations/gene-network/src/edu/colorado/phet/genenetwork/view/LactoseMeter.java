f7625f9871282cfa01e73418ed52f73f50d94dea
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-01-11 18:54:42 +0000

    Fixed two strings that were not translatable, see #2659.

--- trunk/simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/view/LactoseMeter.java
+++ trunk/simulations-java/simulations/gene-network/src/edu/colorado/phet/genenetwork/view/LactoseMeter.java
@@ -23,6 +23,7 @@ import edu.colorado.phet.common.piccolophet.PhetPNode;
 import edu.colorado.phet.common.piccolophet.event.CursorHandler;
 import edu.colorado.phet.common.piccolophet.nodes.HTMLNode;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
+import edu.colorado.phet.genenetwork.GeneNetworkResources;
 import edu.colorado.phet.genenetwork.GeneNetworkStrings;
 import edu.colorado.phet.genenetwork.model.Galactose;
 import edu.colorado.phet.genenetwork.model.GeneNetworkModelAdapter;
@@ -35,7 +36,7 @@ import edu.umd.cs.piccolo.util.PDimension;
 import edu.umd.cs.piccolox.pswing.PSwing;
 
 /**
- * 
+ *
  * @author John Blanco
  */
 public class LactoseMeter extends PhetPNode {
@@ -56,47 +57,50 @@ public class LactoseMeter extends PhetPNode {
 	private static final double BAR_WIDTH_PROPORTION = 0.85;
 	private static final double BAR_HEIGHT_PROPORTION = 0.50;
 	private static final double MAX_VALUE = 50;
-	
+
     //------------------------------------------------------------------------
     // Instance Data
     //------------------------------------------------------------------------
 
-	private IGeneNetworkModelControl model;
-	private PNode background;
-	private PNode barBackground;
-	private PhetPPath bar;
-	private Rectangle2D barShape = new Rectangle2D.Double();
-	private HTMLNode label;
-	private double barWidth;
-	private double maxBarHeight;
-    private JButton closeButton;
-    private PSwing closePSwing;
-    private HTMLNode overflowText;
-	
-	private IModelElementListener glucoseListener = new ModelElementListenerAdapter(){
-		public void removedFromModel(){
+	private final IGeneNetworkModelControl model;
+	private final PNode background;
+	private final PNode barBackground;
+	private final PhetPPath bar;
+	private final Rectangle2D barShape = new Rectangle2D.Double();
+	private final HTMLNode label;
+	private final double barWidth;
+	private final double maxBarHeight;
+    private final JButton closeButton;
+    private final PSwing closePSwing;
+    private final HTMLNode overflowText;
+
+	private final IModelElementListener glucoseListener = new ModelElementListenerAdapter(){
+		@Override
+        public void removedFromModel(){
 			updateBarSize();
 		}
 	};
-	
+
     //------------------------------------------------------------------------
     // Constructor
     //------------------------------------------------------------------------
 
 	public LactoseMeter(final IGeneNetworkModelControl model) {
-		
+
 		this.model = model;
-		
+
 		// Listen to the model for events that may matter to us.
 		model.addListener(new GeneNetworkModelAdapter(){
-			public void lactoseLevelChanged() {
+			@Override
+            public void lactoseLevelChanged() {
 				updateBarSize();
 			}
-			public void lactoseMeterVisibilityStateChange() {
+			@Override
+            public void lactoseMeterVisibilityStateChange() {
 				updateVisibility();
 			}
 		});
-		
+
 		// Create the overall background.
 		background = new PhetPPath(new RoundRectangle2D.Double(0, 0, size.getWidth(), size.getHeight(), 8, 8),
 				MAIN_BACKGROUND_COLOR, OUTLINE_STROKE, OUTLINE_STROKE_COLOR);
@@ -107,9 +111,9 @@ public class LactoseMeter extends PhetPNode {
 		barWidth = size.getWidth() * BAR_WIDTH_PROPORTION;
 		maxBarHeight = size.getHeight() * BAR_HEIGHT_PROPORTION;
 		double edgeOffset = (size.getWidth() - barWidth) / 2 + OUTLINE_STROKE_WIDTH / 2;
-		
+
 		// Add the button that will allow the user to close (actually hide) the meter.
-		ImageIcon imageIcon = new ImageIcon( 
+		ImageIcon imageIcon = new ImageIcon(
 				PhetCommonResources.getInstance().getImage(PhetCommonResources.IMAGE_CLOSE_BUTTON) );
 		closeButton = new JButton( imageIcon );
 		closeButton.setPreferredSize(new Dimension(imageIcon.getIconWidth(), imageIcon.getIconHeight()));
@@ -118,26 +122,26 @@ public class LactoseMeter extends PhetPNode {
 				model.setLactoseMeterVisible(false);
 			}
 		} );
-		
+
 		closePSwing = new PSwing( closeButton );
 		closePSwing.setOffset(size.getWidth() - closeButton.getBounds().width - edgeOffset, edgeOffset / 2);
 		closePSwing.addInputEventListener( new CursorHandler(Cursor.HAND_CURSOR) );
 		addChild(closePSwing);
-		
+
 		// Create the background for the bar.
 		barBackground = new PhetPPath(new Rectangle2D.Double(0, 0, barWidth, maxBarHeight), BAR_BACKGROUND_COLOR);
 		barBackground.setOffset(edgeOffset, closePSwing.getFullBoundsReference().getMaxY() + edgeOffset / 2);
 		addChild(barBackground);
-		
+
 		// Create a molecule of lactose to show near the label.
 		double mvtSizeFactor = size.getHeight() / 40; // Empirically determined, tweak as needed.
 		ModelViewTransform2D mvt = new ModelViewTransform2D(new Rectangle2D.Double(-1, -1, 2, 2),
 				new Rectangle2D.Double(-mvtSizeFactor, -mvtSizeFactor, mvtSizeFactor * 2, mvtSizeFactor * 2), true );
 		PNode lactoseNode = createLactoseNode(mvt);
-		lactoseNode.setOffset(size.getWidth() / 2, 
+		lactoseNode.setOffset(size.getWidth() / 2,
 				barBackground.getFullBoundsReference().getMaxY() + lactoseNode.getFullBoundsReference().height / 2 + edgeOffset);
 		addChild(lactoseNode);
-		
+
 		// Create the label.
 		label = new HTMLNode(GeneNetworkStrings.LACTOSE_METER_CAPTION, Color.BLACK, LABEL_FONT);
 		addChild(label);
@@ -149,42 +153,42 @@ public class LactoseMeter extends PhetPNode {
 				lactoseNode.getFullBoundsReference().getMaxY(),
 				size.getWidth() - 2 * edgeOffset,
 				size.getHeight() - lactoseNode.getFullBoundsReference().getMaxY() - edgeOffset);
-		
+
 		// Scale the label to fit.
 		double scale = Math.min(labelAreaRect.getWidth() / label.getWidth(),
 				labelAreaRect.getHeight() / label.getHeight());
 		label.setScale(scale);
-		label.setOffset(labelAreaRect.getCenterX() - label.getFullBoundsReference().width / 2, 
+		label.setOffset(labelAreaRect.getCenterX() - label.getFullBoundsReference().width / 2,
 			labelAreaRect.getCenterY() - label.getFullBoundsReference().height / 2);
-		
+
 		// Add the bar itself.  The shape will be set when updates occur.
 		bar = new PhetPPath( BAR_COLOR );
 		bar.setOffset((size.getWidth() - barWidth) / 2 + OUTLINE_STROKE_WIDTH / 2, 0);
 		addChild(bar);
-		
+
 		// Add the text that will be shown if a value is set that exceeds the
 		// supported range.
-		overflowText = new HTMLNode("<html><center>Off<br>Chart</center></html>");
+		overflowText = new HTMLNode( GeneNetworkStrings.OFF_CHART );
 		overflowText.setFont(LABEL_FONT);
 		overflowText.setHTMLColor(Color.BLUE);
 		overflowText.setScale((barWidth - edgeOffset * 2) / overflowText.getFullBoundsReference().getWidth());
-		overflowText.setOffset(size.getWidth() / 2 - overflowText.getFullBoundsReference().getWidth() / 2, 
+		overflowText.setOffset(size.getWidth() / 2 - overflowText.getFullBoundsReference().getWidth() / 2,
 				barBackground.getOffset().getY() + edgeOffset);
 		overflowText.setVisible(false);
 		addChild(overflowText);
-		
+
 		// Register to listen for updates from each existing glucose.  We are
 		// generally only interested in knowing when it gets removed from the
 		// model.
 		for (Glucose glucose : model.getGlucoseList()){
 			glucose.addListener(glucoseListener);
 		}
-		
+
 		// Do the initial updates.
 		updateBarSize();
 		updateVisibility();
 	}
-	
+
     //------------------------------------------------------------------------
     // Methods
     //------------------------------------------------------------------------
@@ -197,7 +201,7 @@ public class LactoseMeter extends PhetPNode {
 			overflowText.setVisible(true);
 		}
 		else{
-			barHeight = (double)model.getLactoseLevel() / MAX_VALUE * maxBarHeight;
+			barHeight = model.getLactoseLevel() / MAX_VALUE * maxBarHeight;
 			if (overflowText.getVisible()){
 				overflowText.setVisible(false);
 			}
@@ -207,11 +211,11 @@ public class LactoseMeter extends PhetPNode {
 		bar.setOffset(bar.getOffset().getX(),
 			barBackground.getBoundsReference().getMaxY() - barHeight + barBackground.getOffset().getY());
 	}
-	
+
 	private void updateVisibility(){
 		setVisible(model.isLactoseMeterVisible());
 	}
-	
+
     /**
      * Generate an image of Lactose.  This is necessary because Lactose exists
      * as a combination of two simple model elements in the model, so there is
