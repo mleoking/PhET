48e7c56a32e89e0aa4496895969724be463b13e6
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-01-11 22:03:52 +0000

    fix RPAL string dependency that crept into phetcommon when migrating IntegerSpinner

--- trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/view/controls/IntegerSpinner.java
+++ trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/view/controls/IntegerSpinner.java
@@ -9,32 +9,32 @@ import java.text.ParseException;
 
 import javax.swing.*;
 
+import edu.colorado.phet.common.phetcommon.resources.PhetCommonResources;
 import edu.colorado.phet.common.phetcommon.util.IntegerRange;
 import edu.colorado.phet.common.phetcommon.view.util.PhetOptionPane;
-import edu.colorado.phet.reactantsproductsandleftovers.RPALStrings;
 
 /**
  * JSpinner that uses integer values.
- * 
+ *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
 public class IntegerSpinner extends JSpinner {
-    
+
     private final IntegerRange range;
     private final JFormattedTextField textField;
-    
+
     public IntegerSpinner( IntegerRange range ) {
         super();
-        
+
         this.range = range;
 
         // number model
         setModel( new SpinnerNumberModel( range.getDefault(), range.getMin(), range.getMax(), 1 ) );
-        
+
         // editor
         NumberEditor editor = new NumberEditor( this );
         setEditor( editor );
-        
+
         // text field, commits when Enter is pressed or focus is lost
         textField = editor.getTextField();
         textField.setColumns( String.valueOf( range.getMax() ).length() );
@@ -46,11 +46,11 @@ public class IntegerSpinner extends JSpinner {
             }
         });
         textField.addFocusListener( new FocusAdapter() {
-            
+
             public void focusLost( FocusEvent e ) {
                 commitEdit();
             }
-            
+
             /*
              * Workaround to select contents when textfield get focus.
              * See bug ID 4699955 at bugs.sun.com
@@ -64,7 +64,7 @@ public class IntegerSpinner extends JSpinner {
             }
         } );
     }
-    
+
     public void setIntValue( int value ) {
        setValue( new Integer( value ) );
     }
@@ -83,7 +83,7 @@ public class IntegerSpinner extends JSpinner {
             handleInvalidValueDelayed();
         }
     }
-    
+
     // Workaround for #2218.
     private void handleInvalidValueDelayed() {
         Timer t = new Timer( 500, new ActionListener() {
@@ -94,16 +94,16 @@ public class IntegerSpinner extends JSpinner {
         t.setRepeats( false );
         t.start();
     }
-    
+
     private void handleInvalidValue() {
         textField.setValue( getValue() ); // revert, sync textfield to value
         Toolkit.getDefaultToolkit().beep();
         showInvalidValueDialog();
     }
-    
+
     private void showInvalidValueDialog() {
         Object[] args = { new Integer( range.getMin() ), new Integer( range.getMax() ) };
-        String message = MessageFormat.format( RPALStrings.MESSAGE_VALUE_OUT_OF_RANGE, args );
+        String message = MessageFormat.format( PhetCommonResources.getString( "message.valueOutOfRange" ), args );
         PhetOptionPane.showErrorDialog( this, message );
     }
 }
