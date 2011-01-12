2abfec93f3f438a801f27bfca40226a11e4fcfaf
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-01-12 17:26:45 +0000

    refactored Student client

--- util/simsharing/src/edu/colorado/phet/simsharing/Config.java
+++ util/simsharing/src/edu/colorado/phet/simsharing/Config.java
@@ -4,6 +4,9 @@ package edu.colorado.phet.simsharing;
 public class Config {
     public static int SERVER_PORT = 2552;
     public static int TEACHER_PORT = 2553;
-    public static String serverIP = "128.138.145.107";
-    public static String teacherIP = "75.32.245.168";
+
+    //    public static String serverIP = "128.138.145.107";
+//    public static String teacherIP = "75.32.245.168";
+    public static String serverIP = "localhost";
+    public static String teacherIP = "localhost";
 }
--- util/simsharing/src/edu/colorado/phet/simsharing/Student.java
+++ util/simsharing/src/edu/colorado/phet/simsharing/Student.java
@@ -1,28 +1,41 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.simsharing;
 
+import akka.actor.ActorRef;
+import akka.actor.Actors;
+
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
+
+import javax.swing.*;
 
 import edu.colorado.phet.common.phetcommon.application.ApplicationConstructor;
 import edu.colorado.phet.common.phetcommon.application.PhetApplication;
 import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
 import edu.colorado.phet.common.phetcommon.application.PhetApplicationLauncher;
-import edu.colorado.phet.common.phetcommon.view.PhetFrame;
+import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
+import edu.colorado.phet.common.phetcommon.util.VoidFunction0;
 import edu.colorado.phet.gravityandorbits.GravityAndOrbitsApplication;
-import edu.colorado.phet.simsharing.test1.SimSharingStudentClient;
+import edu.colorado.phet.gravityandorbits.simsharing.gravityandorbits.GravityAndOrbitsApplicationState;
 
 /**
  * @author Sam Reid
  */
 public class Student {
-    public static void main( GravityAndOrbitsApplication application ) throws IOException, AWTException {
-        PhetFrame frame = application.getPhetFrame();
-        frame.setTitle( frame.getTitle() + ": Student Edition" );
-        new SimSharingStudentClient( application ).start();
+    private final String[] args;
+    private int count = 0;
+
+    public Student( String[] args ) {
+        this.args = args;
     }
 
     public static void main( final String[] args ) throws IOException, AWTException {
+        new Student( args ).start();
+    }
+
+    private void start() {
         final GravityAndOrbitsApplication[] launchedApp = new GravityAndOrbitsApplication[1];
         new PhetApplicationLauncher().launchSim( args, GravityAndOrbitsApplication.PROJECT_NAME, new ApplicationConstructor() {
             public PhetApplication getApplication( PhetApplicationConfig config ) {
@@ -30,6 +43,31 @@ public class Student {
                 return launchedApp[0];
             }
         } );
-        main( launchedApp[0] );
+        final GravityAndOrbitsApplication application = launchedApp[0];
+        application.getPhetFrame().setTitle( application.getPhetFrame().getTitle() + ": Student Edition" );
+        final int N = 1;
+        final ActorRef server = Actors.remote().actorFor( "server", Config.serverIP, Config.SERVER_PORT );
+
+        final VoidFunction0 updateSharing = new VoidFunction0() {
+            public void apply() {
+                if ( count % N == 0 ) {
+                    GravityAndOrbitsApplicationState state = new GravityAndOrbitsApplicationState( application );
+                    server.sendOneWay( state );
+                }
+                count++;
+            }
+        };
+        application.getGravityAndOrbitsModule().addModelSteppedListener( new SimpleObserver() {
+            public void update() {
+                updateSharing.apply();
+            }
+        } );
+        new Timer( 30, new ActionListener() {
+            public void actionPerformed( ActionEvent e ) {
+                if ( application.getGravityAndOrbitsModule().getModeProperty().getValue().getModel().getClock().isPaused() ) {
+                    updateSharing.apply();
+                }
+            }
+        } ).start();
     }
 }
