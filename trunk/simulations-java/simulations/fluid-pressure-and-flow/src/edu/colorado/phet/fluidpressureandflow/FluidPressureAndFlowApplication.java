f58979b42c3e5e62888d7136addb900b89561960
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-01-17 20:44:36 +0000

    moved RESOURCES to FluidPressureAndFlowApplication

--- trunk/simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/FluidPressureAndFlowApplication.java
+++ trunk/simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/FluidPressureAndFlowApplication.java
@@ -3,16 +3,20 @@ package edu.colorado.phet.fluidpressureandflow;
 
 import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
 import edu.colorado.phet.common.phetcommon.application.PhetApplicationLauncher;
+import edu.colorado.phet.common.phetcommon.resources.PhetResources;
 import edu.colorado.phet.common.piccolophet.PiccoloPhetApplication;
 import edu.colorado.phet.fluidpressureandflow.modules.fluidflow.FluidFlowModule;
 import edu.colorado.phet.fluidpressureandflow.modules.fluidpressure.FluidPressureModule;
 import edu.colorado.phet.fluidpressureandflow.modules.watertower.WaterTowerModule;
 
 /**
+ * Main application class for "Fluid Pressure and Flow"
+ *
  * @author Sam Reid
  */
 public class FluidPressureAndFlowApplication extends PiccoloPhetApplication {
     private static final String NAME = "fluid-pressure-and-flow";
+    public static final PhetResources RESOURCES = new PhetResources( NAME );
 
     public FluidPressureAndFlowApplication( PhetApplicationConfig config ) {
         super( config );
