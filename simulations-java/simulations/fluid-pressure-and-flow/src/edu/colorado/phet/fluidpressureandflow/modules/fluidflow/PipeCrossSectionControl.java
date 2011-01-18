bbab793e2fc93423bce5c23f7fc029032439a105
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-01-18 22:22:40 +0000

    Made it so that the pipe control point can't be dragged offscreen vertically

--- simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/modules/fluidflow/PipeCrossSectionControl.java
+++ simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/modules/fluidflow/PipeCrossSectionControl.java
@@ -3,6 +3,7 @@ package edu.colorado.phet.fluidpressureandflow.modules.fluidflow;
 
 import java.awt.geom.Point2D;
 
+import edu.colorado.phet.common.phetcommon.math.MathUtil;
 import edu.colorado.phet.common.phetcommon.util.Function1;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
 import edu.colorado.phet.fluidpressureandflow.model.CrossSection;
@@ -20,23 +21,18 @@ public class PipeCrossSectionControl extends PNode {
 
         Function1<Point2D, Point2D> bottomConstraint = new Function1<Point2D, Point2D>() {
             public Point2D apply( Point2D bottomLocation ) {
-                final double limit = top.getPoint().getY() - DISTANCE_THRESHOLD;
-                final boolean tooClose = bottomLocation.getY() > limit;
-                double bottomY = tooClose ?
-                                 limit :
-                                 bottomLocation.getY();
-                return new Point2D.Double( bottom.getPoint().getX(), bottomY );
+                final double max = top.getPoint().getY() - DISTANCE_THRESHOLD;
+                final double min = -5;//make it so that the control point can't be dragged offscreen in the down direction
+                return new Point2D.Double( bottom.getPoint().getX(), MathUtil.clamp( min, bottomLocation.getY(), max ) );
             }
         };
 
         Function1<Point2D, Point2D> topConstraint = new Function1<Point2D, Point2D>() {
             public Point2D apply( Point2D topLocation ) {
-                final double limit = bottom.getPoint().getY() + DISTANCE_THRESHOLD;
-                final boolean tooClose = topLocation.getY() < limit;
-                double topY = tooClose ?
-                              limit :
-                              topLocation.getY();
-                return new Point2D.Double( top.getPoint().getX(), topY );
+                final double min = bottom.getPoint().getY() + DISTANCE_THRESHOLD;
+                final double max = 3;//make it so that the control point can't be dragged offscreen in the up direction
+                final double clamp = MathUtil.clamp( min, topLocation.getY(), max );
+                return new Point2D.Double( top.getPoint().getX(), clamp );
             }
         };
 
