3fb85b98da0538f31b650ee08a0a4a226f4c41f8
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-01-19 21:26:46 +0000

    Removed a class that was only used in one place.  This was based on feedback from code review, see #2650.

--- trunk/simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/view/SubatomicParticleNode.java
+++ trunk/simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/view/SubatomicParticleNode.java
@@ -3,11 +3,14 @@
 package edu.colorado.phet.buildanatom.view;
 
 import java.awt.Color;
+import java.awt.Paint;
 import java.awt.geom.Point2D;
 
 import edu.colorado.phet.buildanatom.model.SubatomicParticle;
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
+import edu.colorado.phet.common.phetcommon.view.graphics.RoundGradientPaint;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform2D;
+import edu.colorado.phet.common.phetcommon.view.util.ColorUtils;
 import edu.colorado.phet.common.piccolophet.event.CursorHandler;
 import edu.colorado.phet.common.piccolophet.nodes.SphericalNode;
 import edu.umd.cs.piccolo.PNode;
@@ -26,10 +29,20 @@ public class SubatomicParticleNode extends PNode {
     private final SphericalNode sphericalNode;
     private final SubatomicParticle subatomicParticle;
 
+    /**
+     * Constructor.
+     */
     public SubatomicParticleNode( final ModelViewTransform2D mvt, final SubatomicParticle subatomicParticle, final Color baseColor ) {
         this.mvt = mvt;
         this.subatomicParticle = subatomicParticle;
-        sphericalNode = new SphericalNode( mvt.modelToViewDifferentialX( subatomicParticle.getDiameter() ), new SubatomicParticleGradient( subatomicParticle.getRadius(), baseColor ), false );
+        double radius = subatomicParticle.getRadius();
+        // Create a gradient that gives the particles a 3D look.  The numbers
+        // used were empirically determined.
+        Paint particlePaint = new RoundGradientPaint( -radius / 1.5, -radius / 1.5,
+                ColorUtils.brighterColor( baseColor, 0.8 ), new Point2D.Double( radius, radius ),
+                baseColor );
+        sphericalNode = new SphericalNode( mvt.modelToViewDifferentialX( subatomicParticle.getDiameter() ),
+                particlePaint, false );
         addChild( sphericalNode );
 
         updatePosition();
