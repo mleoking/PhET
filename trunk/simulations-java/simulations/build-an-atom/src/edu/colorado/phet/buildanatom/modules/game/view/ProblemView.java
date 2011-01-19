ccb74d063ef45f021771c5addae5af9bb0bc4a76
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-01-19 19:42:50 +0000

    Code and comment cleanup, see #2650.

--- trunk/simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/modules/game/view/ProblemView.java
+++ trunk/simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/modules/game/view/ProblemView.java
@@ -23,7 +23,12 @@ import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.nodes.PText;
 
 /**
- * Abstract base class for Piccolo node that depicts a problem for a specific game Problem.
+ * Abstract base class for Piccolo node that depicts a problem for a specific
+ * game Problem.  Each ProblemView instance is expected to be associated with
+ * a Problem instance.  This contains the logic that controls the flow of the
+ * game, e.g. what happens when the user enters a correct answer, or an
+ * incorrect answer, etc.
+ *
  * @author Sam Reid
  */
 public abstract class ProblemView extends StateView {
@@ -33,10 +38,10 @@ public abstract class ProblemView extends StateView {
     // Transform used by all problem views that present a schematic on the
     // left side.
     public static final ModelViewTransform2D SCHEMATIC_PROBLEM_MVT = new ModelViewTransform2D(
-                    new Point2D.Double( 0, 0 ),
-                    new Point( (int) Math.round( BuildAnAtomDefaults.STAGE_SIZE.width * 0.27 ), (int) Math.round( BuildAnAtomDefaults.STAGE_SIZE.height * 0.45 ) ),
-                    2.0,
-                    true );
+            new Point2D.Double( 0, 0 ),
+            new Point( (int) Math.round( BuildAnAtomDefaults.STAGE_SIZE.width * 0.27 ), (int) Math.round( BuildAnAtomDefaults.STAGE_SIZE.height * 0.45 ) ),
+            2.0,
+            true );
 
     private final GameButtonNode checkButton;
     private final PText problemNumberDisplay;
@@ -45,101 +50,116 @@ public abstract class ProblemView extends StateView {
     private final Problem problem;
     private final BuildAnAtomClock clock = new BuildAnAtomClock();
 
-    public BuildAnAtomClock getClock() {
-        return clock;
-    }
-
+    /**
+     * Constructor.  Sets up the user interface for the portions that are
+     * common to all problem views.
+     */
     ProblemView( final BuildAnAtomGameModel model, BuildAnAtomGameCanvas gameCanvas, final Problem problem ) {
         super( model, problem, gameCanvas );
         this.problem = problem;
         gameAudioPlayer = new GameAudioPlayer( model.getSoundEnabledProperty().getValue() );
         final int problemIndex = model.getProblemIndex( problem ) + 1;
         final int maxProblems = model.getNumberProblems();
-        problemNumberDisplay = new PText( MessageFormat.format( BuildAnAtomStrings.GAME_PROBLEM_INDEX_READOUT_PATTERN, problemIndex, maxProblems ) ) {{
+        problemNumberDisplay = new PText( MessageFormat.format( BuildAnAtomStrings.GAME_PROBLEM_INDEX_READOUT_PATTERN, problemIndex, maxProblems ) ) {
+            {
                 setFont( new PhetFont( 20, true ) );
-            }};
+            }
+        };
         problemNumberDisplay.setOffset( 30, 30 );
         checkButton = new GameButtonNode( BuildAnAtomStrings.GAME_CHECK, BUTTON_OFFSET, new ActionListener() {
-            public void actionPerformed( ActionEvent e ) {
+                public void actionPerformed( ActionEvent e ) {
                 getModel().processGuess( getGuess() );
-                resultNode.addChild( new FaceNode( 400, FACE_COLOR, new Color( 180, 180, 180, 120 ), new Color( 180, 180, 180, 120 ) ) {
-                    {
-                        final FaceNode faceNode = this;
-                        if ( problem.isSolvedCorrectly() ) {
-                            setGuessEditable( false );
-                            smile();
-                            addChild( new PText( "+" + problem.getScore() ) {
-                                {
-                                    setOffset( faceNode.getFullBounds().getWidth() / 2, faceNode.getFullBounds().getHeight() );
-                                    setFont( new PhetFont( 24, true ) );
-                                }
+                final FaceNode faceNode = new FaceNode( 400, FACE_COLOR, new Color( 180, 180, 180, 120 ), new Color( 180, 180, 180, 120 ) );
+                resultNode.addChild( faceNode );
+
+                if ( problem.isSolvedCorrectly() ) {
+                    setGuessEditable( false );
+                    faceNode.smile();
+                    addChild( new PText( "+" + problem.getScore() ) {
+                        {
+                            setOffset( faceNode.getFullBounds().getWidth() / 2, faceNode.getFullBounds().getHeight() );
+                            setFont( new PhetFont( 24, true ) );
+                        }
+                    } );
+                    gameAudioPlayer.correctAnswer();
+                    GameButtonNode nextProblemButton = new GameButtonNode( BuildAnAtomStrings.GAME_NEXT, BUTTON_OFFSET, new ActionListener() {
+                            public void actionPerformed( ActionEvent e ) {
+                            getModel().next();
+                            }
                             } );
-                            gameAudioPlayer.correctAnswer();
-                            GameButtonNode nextProblemButton = new GameButtonNode( BuildAnAtomStrings.GAME_NEXT, BUTTON_OFFSET, new ActionListener() {
+                    checkButton.setVisible( false );
+                    resultNode.addChild( nextProblemButton );
+                }
+                else {
+                    faceNode.frown();
+                    gameAudioPlayer.wrongAnswer();
+                    setGuessEditable( false );
+                    if ( problem.getNumGuesses() == 1 ) {
+                        GameButtonNode tryAgainButton = new GameButtonNode( BuildAnAtomStrings.GAME_TRY_AGAIN, BUTTON_OFFSET, new ActionListener() {
                                 public void actionPerformed( ActionEvent e ) {
-                                    getModel().next();
-                                    }
-                                    } );
-                            checkButton.setVisible( false );
-                            resultNode.addChild( nextProblemButton );
-                        }
-                        else {
-                            frown();
-                            gameAudioPlayer.wrongAnswer();
-                            setGuessEditable( false );
-                            if ( problem.getNumGuesses() == 1 ) {
-                                GameButtonNode tryAgainButton = new GameButtonNode( BuildAnAtomStrings.GAME_TRY_AGAIN, BUTTON_OFFSET, new ActionListener() {
-                                    public void actionPerformed( ActionEvent e ) {
-                                        resultNode.removeAllChildren();
-                                        checkButton.setVisible( true );
-                                        setGuessEditable( true );
+                                resultNode.removeAllChildren();
+                                checkButton.setVisible( true );
+                                setGuessEditable( true );
+                                }
+                                } );
+                        resultNode.addChild( tryAgainButton );
+                        checkButton.setVisible( false );
+                    }
+                    else if ( problem.getNumGuesses() == 2 ) {
+                        GameButtonNode showAnswerButton = new GameButtonNode( BuildAnAtomStrings.GAME_SHOW_ANSWER, BUTTON_OFFSET, new ActionListener() {
+                                public void actionPerformed( ActionEvent e ) {
+                                displayAnswer( problem.getAnswer() );
+                                setGuessEditable( false );
+                                resultNode.removeAllChildren();
+                                GameButtonNode nextProblemButton = new GameButtonNode( BuildAnAtomStrings.GAME_NEXT, BUTTON_OFFSET, new ActionListener() {
+                                        public void actionPerformed( ActionEvent e ) {
+                                        getModel().next();
                                         }
                                         } );
-                                resultNode.addChild( tryAgainButton );
-                                checkButton.setVisible( false );
-                            }
-                            else if ( problem.getNumGuesses() == 2 ) {
-                                GameButtonNode showAnswerButton = new GameButtonNode( BuildAnAtomStrings.GAME_SHOW_ANSWER, BUTTON_OFFSET, new ActionListener() {
-                                    public void actionPerformed( ActionEvent e ) {
-                                        displayAnswer( problem.getAnswer() );
-                                        setGuessEditable( false );
-                                        resultNode.removeAllChildren();
-                                        GameButtonNode nextProblemButton = new GameButtonNode( BuildAnAtomStrings.GAME_NEXT, BUTTON_OFFSET, new ActionListener() {
-                                            public void actionPerformed( ActionEvent e ) {
-                                                getModel().next();
-                                                }
-                                                } );
-                                        resultNode.addChild( nextProblemButton );
-                                        }
-                                        } );
-                                resultNode.addChild( showAnswerButton );
-                                checkButton.setVisible( false );
-                            }
-                        }
-                        setOffset( BuildAnAtomDefaults.STAGE_SIZE.getWidth() / 2 - getFullBounds().getWidth() / 2, BuildAnAtomDefaults.STAGE_SIZE.getHeight() / 2 - getFullBounds().getHeight() / 2 );
-                        resultNode.moveToFront();
-                        }
-                } );
+                                resultNode.addChild( nextProblemButton );
+                                }
+                                } );
+                        resultNode.addChild( showAnswerButton );
+                        checkButton.setVisible( false );
+                    }
+                }
+                faceNode.setOffset( BuildAnAtomDefaults.STAGE_SIZE.getWidth() / 2 - faceNode.getFullBounds().getWidth() / 2,
+                        BuildAnAtomDefaults.STAGE_SIZE.getHeight() / 2 - faceNode.getFullBounds().getHeight() / 2 );
+                resultNode.moveToFront();
                 }
                 } );
         checkButton.setEnabled( false );
     }
 
-    public void enableCheckButton(){
+    public BuildAnAtomClock getClock() {
+        return clock;
+    }
+
+    public void enableCheckButton() {
         checkButton.setEnabled( true );
     }
 
     /**
-     * Applies the user's guess from the view/controller to the model (if it has not done so already).
+     * Get the guess that the user has submitted.
      *
-     * @return the gussed value
+     * @return the guessed value
      */
     protected abstract ImmutableAtom getGuess();
 
+    /**
+     * Display the answer to the user, generally called when the user has
+     * exhausted their guesses and not gotten the correct answer.
+     *
+     * @param answer
+     */
     protected abstract void displayAnswer( ImmutableAtom answer );
 
     //disable controls during feedback stages
     abstract protected void setGuessEditable( boolean guessEditable );
+
+    public Problem getProblem() {
+        return problem;
+    }
 
     @Override
     public void init() {
@@ -162,6 +182,10 @@ public abstract class ProblemView extends StateView {
         clock.stop();
     }
 
+    /**
+     * Convenience class for centering the game button and hooking up its
+     * action listener.
+     */
     private static class GameButtonNode extends ButtonNode {
         /**
          * Constructor.
@@ -174,9 +198,5 @@ public abstract class ProblemView extends StateView {
             addActionListener( listener );
             centerFullBoundsOnPoint( centerLocation.getX(), centerLocation.getY() );
         }
-    }
-
-    public Problem getProblem() {
-        return problem;
     }
 }
