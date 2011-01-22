f6f8e69a087e14d72d9f70c0576856b3cb72a79f
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-01-22 00:12:58 +0000

    Created infrastructure for 2nd tab, put a temporary sketch on the tab.

---
+++ simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/modules/isotopemixture/view/IsotopeMixtureCanvas.java
@@ -0,0 +1,93 @@
+// Copyright 2002-2011, University of Colorado
+
+package edu.colorado.phet.buildanatom.modules.isotopemixture.view;
+
+import java.awt.Font;
+import java.awt.Point;
+import java.awt.geom.Dimension2D;
+import java.awt.geom.Point2D;
+
+import edu.colorado.phet.buildanatom.BuildAnAtomConstants;
+import edu.colorado.phet.buildanatom.BuildAnAtomDefaults;
+import edu.colorado.phet.buildanatom.BuildAnAtomResources;
+import edu.colorado.phet.buildanatom.modules.interactiveisotope.model.InteractiveIsotopeModel;
+import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform2D;
+import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
+import edu.colorado.phet.common.piccolophet.PhetPCanvas;
+import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.nodes.PImage;
+
+/**
+ * Canvas for the tab where the user builds an atom.
+ */
+public class IsotopeMixtureCanvas extends PhetPCanvas {
+
+    //----------------------------------------------------------------------------
+    // Class Data
+    //----------------------------------------------------------------------------
+
+    private final static Font LABEL_FONT = new PhetFont( 20 );
+
+    //----------------------------------------------------------------------------
+    // Instance Data
+    //----------------------------------------------------------------------------
+
+    // View
+    private final PNode rootNode;
+
+    // Transform.
+    private final ModelViewTransform2D mvt;
+
+    //----------------------------------------------------------------------------
+    // Constructor(s)
+    //----------------------------------------------------------------------------
+
+    public IsotopeMixtureCanvas( final InteractiveIsotopeModel model ) {
+
+        // Set up the canvas-screen transform.
+        setWorldTransformStrategy( new PhetPCanvas.CenteredStage( this, BuildAnAtomDefaults.STAGE_SIZE ) );
+
+        // Set up the model-canvas transform.  IMPORTANT NOTES: The multiplier
+        // factors for the point in the view can be adjusted to shift the
+        // center right or left, and the scale factor can be adjusted to zoom
+        // in or out (smaller numbers zoom out, larger ones zoom in).
+        mvt = new ModelViewTransform2D(
+                new Point2D.Double( 0, 0 ),
+                new Point( (int) Math.round( BuildAnAtomDefaults.STAGE_SIZE.width * 0.50 ), (int) Math.round( BuildAnAtomDefaults.STAGE_SIZE.height * 0.55 ) ),
+                1.6, // "Zoom factor" - smaller zooms out, larger zooms in.
+                true );
+
+        setBackground( BuildAnAtomConstants.CANVAS_BACKGROUND );
+
+        // Root of our scene graph
+        rootNode = new PNode();
+        addWorldChild( rootNode );
+
+        // TODO: Temp - load a picture of what the tab will eventually look like.
+        PNode tabImageNode = new PImage( BuildAnAtomResources.getImage( "isotopes-mixture-sketch.png" ));
+        tabImageNode.setOffset( 0, 20 );
+        rootNode.addChild( tabImageNode );
+    }
+
+    //----------------------------------------------------------------------------
+    // Methods
+    //----------------------------------------------------------------------------
+
+    /*
+     * Updates the layout of stuff on the canvas.
+     */
+    @Override
+    protected void updateLayout() {
+
+        Dimension2D worldSize = getWorldSize();
+        if ( worldSize.getWidth() <= 0 || worldSize.getHeight() <= 0 ) {
+            // canvas hasn't been sized, blow off layout
+            return;
+        }
+        else if ( BuildAnAtomConstants.DEBUG_CANVAS_UPDATE_LAYOUT ) {
+            System.out.println( "ExampleCanvas.updateLayout worldSize=" + worldSize );//XXX
+        }
+
+        //XXX lay out nodes
+    }
+}
