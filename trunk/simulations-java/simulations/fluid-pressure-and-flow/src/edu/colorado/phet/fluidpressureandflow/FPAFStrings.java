f3c740f7b89f50181031e5454585ac1ef471e952
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-01-28 18:12:19 +0000

    removed bogus usage of string from Gravity and Orbits

---
+++ trunk/simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/FPAFStrings.java
@@ -0,0 +1,9 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.fluidpressureandflow;
+
+/**
+ * @author Sam Reid
+ */
+public class FPAFStrings {
+    public static final String RESET = FluidPressureAndFlowApplication.RESOURCES.getLocalizedString( "reset" );
+}
