1c66b4a0d36c097ffe1fcfae18bcf20e38c08098
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-01-30 15:13:45 +0000

    Fixed: decrease in volume should be proportional to water velocity

--- simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/modules/watertower/WaterTower.java
+++ simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/modules/watertower/WaterTower.java
@@ -17,7 +17,7 @@ public class WaterTower {
     public static final int MAX_Y = 18;
     public final Property<ImmutableVector2D> tankBottomCenter = new Property<ImmutableVector2D>( new ImmutableVector2D( 0, MAX_Y ) );
     private static double TANK_RADIUS = 5;
-    private static double TANK_HEIGHT = 10;
+    public static double TANK_HEIGHT = 10;
     private static final int LEG_EXTENSION = 3;
     public static double tankVolume = Math.PI * TANK_RADIUS * TANK_RADIUS * TANK_HEIGHT;
     public final Property<Double> fluidVolume = new Property<Double>( tankVolume );//meters cubed
@@ -66,10 +66,10 @@ public class WaterTower {
     }
 
     public Shape getWaterShape() {
-        return new Rectangle2D.Double( tankBottomCenter.getValue().getX() - TANK_RADIUS, tankBottomCenter.getValue().getY(), TANK_RADIUS * 2, getWaterHeight() );
+        return new Rectangle2D.Double( tankBottomCenter.getValue().getX() - TANK_RADIUS, tankBottomCenter.getValue().getY(), TANK_RADIUS * 2, getWaterLevel() );
     }
 
-    public double getWaterHeight() {
+    public double getWaterLevel() {
         return fluidVolume.getValue() / Math.PI / TANK_RADIUS / TANK_RADIUS;
     }
 
