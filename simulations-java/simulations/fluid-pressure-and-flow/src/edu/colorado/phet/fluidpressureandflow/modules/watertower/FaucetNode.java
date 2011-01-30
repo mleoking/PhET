fea7996a8264498f6fe1bdcc2fe551bd019dc448
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-01-30 14:06:29 +0000

    Made water velocity vary as v=sqrt(2*g*h)

--- simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/modules/watertower/FaucetNode.java
+++ simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/modules/watertower/FaucetNode.java
@@ -5,6 +5,8 @@ import java.awt.*;
 
 import javax.swing.*;
 
+import edu.colorado.phet.common.phetcommon.model.SettableProperty;
+import edu.colorado.phet.common.phetcommon.view.controls.PropertyRadioButton;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.fluidpressureandflow.FluidPressureAndFlowApplication;
@@ -12,15 +14,26 @@ import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.nodes.PImage;
 import edu.umd.cs.piccolox.pswing.PSwing;
 
+import static edu.colorado.phet.common.phetcommon.model.Not.not;
+
 /**
  * @author Sam Reid
  */
 public class FaucetNode extends PNode {
-    public FaucetNode( ModelViewTransform transform, FaucetFlowLevel faucetFlowLevel ) {
+    final static Color TRANSPARENT = new Color( 0, 0, 0, 0 );
+
+    public static class RadioButton extends PropertyRadioButton<Boolean> {
+        public RadioButton( String name, SettableProperty<Boolean> selected ) {
+            super( name, selected, true );
+            setBackground( TRANSPARENT );
+            setFont( new PhetFont( 14, true ) );
+        }
+    }
+
+    public FaucetNode( ModelViewTransform transform, final FaucetFlowLevel faucetFlowLevel ) {
         addChild( new PImage( FluidPressureAndFlowApplication.RESOURCES.getImage( "faucet.png" ) ) {{
             setScale( 0.75 );
             setOffset( -27, 0 );
-            final Color TRANSPARENT = new Color( 0, 0, 0, 0 );
             addChild( new PSwing( new JSlider() {{
                 setBackground( TRANSPARENT );
                 setPaintTicks( true );
@@ -29,14 +42,8 @@ public class FaucetNode extends PNode {
                 translate( 186, 0 );
             }} );
             addChild( new PSwing( new JPanel() {{
-                add( new JRadioButton( "Manual", true ) {{
-                    setBackground( TRANSPARENT );
-                    setFont( new PhetFont( 14, true ) );
-                }} );
-                add( new JRadioButton( "Auto", false ) {{
-                    setBackground( TRANSPARENT );
-                    setFont( new PhetFont( 14, true ) );
-                }} );
+                add( new RadioButton( "Manual", not( faucetFlowLevel.automatic ) ) );
+                add( new RadioButton( "Auto", faucetFlowLevel.automatic ) );
                 setBackground( TRANSPARENT );
             }} ) {{
                 setScale( 1.4 );
