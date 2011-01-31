e3c0c78642d0753ee133f8d2edbf92fd0306b50e
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-01-31 17:39:01 +0000

    Replaced usage of EasyGridBagLayout with BoixLayout in order to avoid deprecated code.

--- trunk/simulations-java/simulations/greenhouse/src/edu/colorado/phet/greenhouse/view/SpectrumDialog.java
+++ trunk/simulations-java/simulations/greenhouse/src/edu/colorado/phet/greenhouse/view/SpectrumDialog.java
@@ -3,21 +3,19 @@
 package edu.colorado.phet.greenhouse.view;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Frame;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
-import edu.colorado.phet.buildanatom.BuildAnAtomConstants;
 import edu.colorado.phet.common.phetcommon.application.PaintImmediateDialog;
-import edu.colorado.phet.common.phetcommon.view.util.EasyGridBagLayout;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.umd.cs.piccolo.PCanvas;
 
@@ -52,17 +50,24 @@ public class SpectrumDialog extends PaintImmediateDialog {
         // Create the panel that will contain the canvas and close button.
         JPanel mainPanel = new JPanel();
         mainPanel.setBorder( BorderFactory.createEmptyBorder( 15, 15, 15, 15 ) ); // top, left, bottom, right
-        EasyGridBagLayout layout = new EasyGridBagLayout( mainPanel );
-        layout.setInsets( new Insets( 10, 0, 10, 0 ) ); // top, left, bottom, right
-        layout.setAnchor( GridBagConstraints.CENTER );
-        mainPanel.setLayout( layout );
+//        EasyGridBagLayout layout = new EasyGridBagLayout( mainPanel );
+//        layout.setInsets( new Insets( 10, 0, 10, 0 ) ); // top, left, bottom, right
+//        layout.setAnchor( GridBagConstraints.CENTER );
+//        mainPanel.setLayout( layout );
+        mainPanel.setLayout( new BoxLayout( mainPanel, BoxLayout.Y_AXIS ) );
 
-        // Create the canvas.
+        // Create the canvas and add it to the panel.
         PCanvas canvas = new PCanvas();
         canvas.setBackground( Color.BLACK );
         canvas.setPreferredSize( new Dimension( 600, 400 ) );
         canvas.setBorder( BorderFactory.createEtchedBorder() ); // top, left, bottom, right
         mainPanel.add( canvas );
+
+        // Add an invisible panel that will create space between the diagram
+        // and the close button.
+        JPanel spacerPanel = new JPanel();
+        spacerPanel.setPreferredSize( new Dimension( 1, 15) );
+        mainPanel.add( spacerPanel );
 
         // Add the close button.
         // TODO: i18n
@@ -72,7 +77,8 @@ public class SpectrumDialog extends PaintImmediateDialog {
                 SpectrumDialog.this.dispose();
             }
         });
-        layout.addComponent( closeButton, 2, 0 );
+        closeButton.setAlignmentX( Component.CENTER_ALIGNMENT );
+        mainPanel.add( closeButton );
 
         // Add to the dialog
         getContentPane().add( mainPanel );
