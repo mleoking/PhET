dd4fb1feda70c0a5e4245c24e776d096c4eee08b
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-02-01 03:53:41 +0000

    partial implementation of balance scales view (fulcrums only)

--- trunk/simulations-java/simulations/balancing-chemical-equations/src/edu/colorado/phet/balancingchemicalequations/view/BalanceScaleNode.java
+++ trunk/simulations-java/simulations/balancing-chemical-equations/src/edu/colorado/phet/balancingchemicalequations/view/BalanceScaleNode.java
@@ -2,7 +2,15 @@
 
 package edu.colorado.phet.balancingchemicalequations.view;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.geom.GeneralPath;
+
 import edu.colorado.phet.balancingchemicalequations.model.Atom;
+import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
+import edu.umd.cs.piccolo.nodes.PPath;
+import edu.umd.cs.piccolo.nodes.PText;
+import edu.umd.cs.piccolo.util.PDimension;
 import edu.umd.cs.piccolox.nodes.PComposite;
 
 /**
@@ -15,10 +23,13 @@ public class BalanceScaleNode extends PComposite {
 
     private final static int DIFFERENCE_FOR_MAX_TILT = 4;
 
+    private final Atom atom;
     private int leftNumberOfAtoms, rightNumberOfAtoms;
 
-    public BalanceScaleNode( Atom atom ) {
-        this.leftNumberOfAtoms = this.rightNumberOfAtoms = 0;
+    public BalanceScaleNode( Atom atom, int leftNumberOfAtoms, int rightNumberOfAtoms ) {
+        this.atom = atom;
+        this.leftNumberOfAtoms = leftNumberOfAtoms;
+        this.rightNumberOfAtoms = rightNumberOfAtoms;
         update();
     }
 
@@ -31,6 +42,48 @@ public class BalanceScaleNode extends PComposite {
     }
 
     private void update() {
+        removeAllChildren();
+
+        FulcrumNode fulcrumNode = new FulcrumNode( atom );
+        addChild( fulcrumNode );
+
         // update to match leftNumberOfAtoms and rightNumberOfAtoms
+    }
+
+    /*
+     * Fulcrum on which the scale balances.
+     * Labeled with the atom symbol.
+     * Origin is at the tip of the fulcrum.
+     */
+    private static class FulcrumNode extends PComposite {
+
+        private static final PDimension SIZE = new PDimension( 40, 25 );
+
+        public FulcrumNode( Atom atom ) {
+
+            GeneralPath path = new GeneralPath();
+            path.moveTo( 0f, 0f );
+            path.lineTo( (float)(SIZE.getWidth()/2), (float)SIZE.getHeight() );
+            path.lineTo( (float)(-SIZE.getWidth()/2), (float)SIZE.getHeight() );
+            path.closePath();
+            PPath pathNode = new PPath( path );
+            pathNode.setPaint( Color.WHITE );
+            pathNode.setStroke( new BasicStroke( 1f ) );
+            pathNode.setStrokePaint( Color.BLACK );
+            addChild( pathNode );
+
+            PText symbolNode = new PText( atom.getSymbol() );
+            symbolNode.setFont( new PhetFont( 14 ) );
+            symbolNode.setTextPaint( Color.BLACK );
+            addChild( symbolNode );
+
+            // layout
+            double x = 0;
+            double y = 0;
+            pathNode.setOffset( x, y );
+            x = -symbolNode.getFullBoundsReference().getWidth() / 2;
+            y = pathNode.getFullBoundsReference().getMaxY() - symbolNode.getFullBoundsReference().getHeight() - 2;
+            symbolNode.setOffset( x, y );
+        }
     }
 }
