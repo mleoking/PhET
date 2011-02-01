7b03b39d0933832767536c1541fdcd829b14414c
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-02-01 04:38:14 +0000

    add dev node that shows the balanced equation

---
+++ trunk/simulations-java/simulations/balancing-chemical-equations/src/edu/colorado/phet/balancingchemicalequations/view/BalancedEquationNode.java
@@ -0,0 +1,55 @@
+// Copyright 2002-2011, University of Colorado
+
+package edu.colorado.phet.balancingchemicalequations.view;
+
+import edu.colorado.phet.balancingchemicalequations.model.Equation;
+import edu.colorado.phet.balancingchemicalequations.model.EquationTerm;
+import edu.colorado.phet.common.phetcommon.model.Property;
+import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
+import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
+import edu.colorado.phet.common.piccolophet.nodes.HTMLNode;
+
+/**
+ * Shows the balanced equation, used for dev debugging.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
+public class BalancedEquationNode extends HTMLNode {
+
+    public BalancedEquationNode( final Property<Equation> equationProperty ) {
+        setFont( new PhetFont( 8 ) );
+
+        equationProperty.addObserver( new SimpleObserver() {
+            public void update() {
+                StringBuffer b = new StringBuffer( "Answer: ");
+
+                // reactants
+                EquationTerm[] reactantTerms = equationProperty.getValue().getReactants();
+                for ( int i = 0; i < reactantTerms.length; i++ ) {
+                    b.append( reactantTerms[i].getBalancedCoefficient() );
+                    b.append( reactantTerms[i].getMolecule().getSymbol() );
+                    b.append( " " );
+                    if ( i < reactantTerms.length - 1 ) {
+                        b.append( "+ " );
+                    }
+                }
+
+                // right arrow
+                b.append( "\u2192 " );
+
+                // products
+                EquationTerm[] productTerms = equationProperty.getValue().getProducts();
+                for ( int i = 0; i < productTerms.length; i++ ) {
+                    b.append( productTerms[i].getBalancedCoefficient() );
+                    b.append( productTerms[i].getMolecule().getSymbol() );
+                    b.append( " " );
+                    if ( i < productTerms.length - 1 ) {
+                        b.append( "+ " );
+                    }
+                }
+
+                setHTML( b.toString() );
+            }
+        } );
+    }
+}
