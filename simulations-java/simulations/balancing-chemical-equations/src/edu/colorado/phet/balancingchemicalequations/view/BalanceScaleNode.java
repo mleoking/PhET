bce4e66968f73d5c9cd1f32822c4435ea645b0bb
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-02-01 04:18:12 +0000

    add beam to balance scales

--- simulations-java/simulations/balancing-chemical-equations/src/edu/colorado/phet/balancingchemicalequations/view/BalanceScaleNode.java
+++ simulations-java/simulations/balancing-chemical-equations/src/edu/colorado/phet/balancingchemicalequations/view/BalanceScaleNode.java
@@ -4,10 +4,16 @@ package edu.colorado.phet.balancingchemicalequations.view;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.GradientPaint;
+import java.awt.Paint;
 import java.awt.geom.GeneralPath;
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
 
 import edu.colorado.phet.balancingchemicalequations.model.Atom;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
+import edu.umd.cs.piccolo.nodes.PImage;
 import edu.umd.cs.piccolo.nodes.PPath;
 import edu.umd.cs.piccolo.nodes.PText;
 import edu.umd.cs.piccolo.util.PDimension;
@@ -21,15 +27,22 @@ import edu.umd.cs.piccolox.nodes.PComposite;
  */
 public class BalanceScaleNode extends PComposite {
 
-    private final static int DIFFERENCE_FOR_MAX_TILT = 4;
-
     private final Atom atom;
+    private final ArrayList<AtomNode> atomNodes;
+    private BeamNode beamNode;
     private int leftNumberOfAtoms, rightNumberOfAtoms;
 
     public BalanceScaleNode( Atom atom, int leftNumberOfAtoms, int rightNumberOfAtoms ) {
+
         this.atom = atom;
         this.leftNumberOfAtoms = leftNumberOfAtoms;
         this.rightNumberOfAtoms = rightNumberOfAtoms;
+
+        this.atomNodes = new ArrayList<AtomNode>();
+
+        FulcrumNode fulcrumNode = new FulcrumNode( atom );
+        addChild( fulcrumNode );
+
         update();
     }
 
@@ -42,12 +55,42 @@ public class BalanceScaleNode extends PComposite {
     }
 
     private void update() {
-        removeAllChildren();
 
-        FulcrumNode fulcrumNode = new FulcrumNode( atom );
-        addChild( fulcrumNode );
+        // beam, with appropriate tilt
+        if ( beamNode != null ) {
+            removeChild( beamNode );
+        }
+        beamNode = new BeamNode( leftNumberOfAtoms - rightNumberOfAtoms, FulcrumNode.SIZE.getHeight() );
+        addChild( beamNode );
 
         // update to match leftNumberOfAtoms and rightNumberOfAtoms
+    }
+
+    /*
+     * Atoms that appear on the scale.
+     * Origin at upper left of bounding rectangle.
+     */
+    private static class AtomNode extends PImage {
+        public AtomNode( Atom atom ) {
+            setImage( atom.getImage() );
+        }
+    }
+
+    /*
+     * The beam is a horizontal lever, centered on the fulcrum, and pivoted
+     * to represent the relationship between quantities on either side of the fulcrum.
+     */
+    private static class BeamNode extends PPath {
+
+        private static final int LENGTH = 200;
+        private static final int DIFFERENCE_FOR_MAX_TILT = 4;
+
+        public BeamNode( int difference, double fulcrumHeight ) {
+            Line2D line = new Line2D.Double( -LENGTH / 2, 0, LENGTH / 2, 0 );
+            setPathTo( line );
+            setStroke( new BasicStroke( 1f ) );
+            setStrokePaint( Color.BLACK );
+        }
     }
 
     /*
@@ -57,7 +100,8 @@ public class BalanceScaleNode extends PComposite {
      */
     private static class FulcrumNode extends PComposite {
 
-        private static final PDimension SIZE = new PDimension( 40, 25 );
+        private static final PDimension SIZE = new PDimension( 40, 30 );
+        private static final Paint FILL_PAINT = new GradientPaint( new Point2D.Double( 0, 0 ), Color.WHITE, new Point2D.Double( 0, SIZE.getHeight() ), Color.LIGHT_GRAY );
 
         public FulcrumNode( Atom atom ) {
 
@@ -67,7 +111,7 @@ public class BalanceScaleNode extends PComposite {
             path.lineTo( (float)(-SIZE.getWidth()/2), (float)SIZE.getHeight() );
             path.closePath();
             PPath pathNode = new PPath( path );
-            pathNode.setPaint( Color.WHITE );
+            pathNode.setPaint( FILL_PAINT );
             pathNode.setStroke( new BasicStroke( 1f ) );
             pathNode.setStrokePaint( Color.BLACK );
             addChild( pathNode );
