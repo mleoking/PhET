c39f0b3f4022ceebdafed19fb6b10112e2019785
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-02-02 20:20:13 +0000

    Made slider door act binary instead of continuous

--- trunk/simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/modules/watertower/WaterTowerNode.java
+++ trunk/simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/modules/watertower/WaterTowerNode.java
@@ -16,6 +16,8 @@ import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.colorado.phet.fluidpressureandflow.view.PoolNode;
 import edu.colorado.phet.fluidpressureandflow.view.RelativeDragHandler;
 import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
+import edu.umd.cs.piccolo.event.PInputEvent;
 import edu.umd.cs.piccolo.nodes.PImage;
 
 import static edu.colorado.phet.fluidpressureandflow.FluidPressureAndFlowApplication.RESOURCES;
@@ -96,11 +98,17 @@ public class WaterTowerNode extends PNode {
             };
             waterTower.panelOffset.addObserver( updatePanelLocation );
             waterTower.tankBottomCenter.addObserver( updatePanelLocation );
-            addInputEventListener( new RelativeDragHandler( this, transform, waterTower.panelOffset, new Function1<Point2D, Point2D>() {
-                public Point2D apply( Point2D point2D ) {
-                    return new Point2D.Double( waterTower.getTankShape().getWidth() / 2, MathUtil.clamp( 0, point2D.getY(), waterTower.getTankShape().getHeight() ) );
+
+            addInputEventListener( new PBasicInputEventHandler() {
+                public void mouseDragged( PInputEvent event ) {
+                    if ( event.getDeltaRelativeTo( getParent() ).getHeight() > 0 ) {
+                        waterTower.panelOffset.setValue( new ImmutableVector2D( WaterTower.TANK_RADIUS, 0 ) );
+                    }
+                    else if ( event.getDeltaRelativeTo( getParent() ).getHeight() < 0 ) {
+                        waterTower.panelOffset.setValue( new ImmutableVector2D( WaterTower.TANK_RADIUS, 2 ) );
+                    }
                 }
-            } ) );
+            } );
             addInputEventListener( new CursorHandler( Cursor.getPredefinedCursor( Cursor.N_RESIZE_CURSOR ) ) );
         }} );
     }
