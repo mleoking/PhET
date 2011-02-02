b2ef8b9fa311f72a854baed87e969d0ce23fe60a
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-02-02 21:15:51 +0000

    Added a hole in the water tank

--- simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/modules/watertower/WaterTower.java
+++ simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/modules/watertower/WaterTower.java
@@ -16,12 +16,13 @@ import edu.colorado.phet.common.phetcommon.view.util.DoubleGeneralPath;
 public class WaterTower {
     public static final int MAX_Y = 18;
     public static double TANK_RADIUS = 5;
+    public static double PANEL_OFFSET = TANK_RADIUS + 0.25;
     public static double TANK_HEIGHT = 10;
     private static final int LEG_EXTENSION = 3;
     public static double tankVolume = Math.PI * TANK_RADIUS * TANK_RADIUS * TANK_HEIGHT;
     public final Property<ImmutableVector2D> tankBottomCenter = new Property<ImmutableVector2D>( new ImmutableVector2D( 0, MAX_Y ) );
     public final Property<Double> fluidVolume = new Property<Double>( tankVolume );//meters cubed
-    public final Property<ImmutableVector2D> panelOffset = new Property<ImmutableVector2D>( new ImmutableVector2D( TANK_RADIUS, 0 ) );//The movable panel that can cover the hole.
+    public final Property<ImmutableVector2D> panelOffset = new Property<ImmutableVector2D>( new ImmutableVector2D( PANEL_OFFSET, 0 ) );//The movable panel that can cover the hole.
 
     public Rectangle2D.Double getTankShape() {
         return new Rectangle2D.Double( tankBottomCenter.getValue().getX() - TANK_RADIUS, tankBottomCenter.getValue().getY(), TANK_RADIUS * 2, TANK_HEIGHT );
@@ -74,7 +75,7 @@ public class WaterTower {
     }
 
     public Point2D getHoleLocation() {
-        return new Point2D.Double( tankBottomCenter.getValue().getX() + TANK_RADIUS, tankBottomCenter.getValue().getY() );
+        return new Point2D.Double( tankBottomCenter.getValue().getX() + TANK_RADIUS + 0.55 / 2, tankBottomCenter.getValue().getY() - 0.15 );
     }
 
     public boolean isHoleOpen() {
