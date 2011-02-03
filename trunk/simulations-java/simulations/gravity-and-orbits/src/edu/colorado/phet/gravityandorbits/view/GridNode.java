e1dfd1c52ed7e946ba1ddbf1e20d034041f72ce0
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-02-03 06:26:33 +0000

    Made zoom controls smaller Made the grid zoom in and out Fixed the text on the zoom out button

--- trunk/simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/view/GridNode.java
+++ trunk/simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/view/GridNode.java
@@ -6,6 +6,7 @@ import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 
 import edu.colorado.phet.common.phetcommon.model.Property;
+import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.umd.cs.piccolo.PNode;
@@ -16,24 +17,30 @@ import edu.umd.cs.piccolo.PNode;
 public class GridNode extends PNode {
     private static final int NUM_GRID_LINES = 10;
 
-    public GridNode( Property<ModelViewTransform> transform, double spacing, Point2D.Double center ) {
-        //horizontal lines
-        for ( int i = -NUM_GRID_LINES; i <= NUM_GRID_LINES; i++ ) {
-            double y = i * spacing + center.y;
-            double x1 = NUM_GRID_LINES * spacing + center.x;
-            double x2 = -NUM_GRID_LINES * spacing + center.x;
-            addGridLine( new Line2D.Double( x1, y, x2, y ), transform );
-        }
-
-        //vertical lines
-        for ( int i = -NUM_GRID_LINES; i <= NUM_GRID_LINES; i++ ) {
-            double x = i * spacing + center.x;
-            double y1 = NUM_GRID_LINES * spacing + center.y;
-            double y2 = -NUM_GRID_LINES * spacing + center.y;
-            addGridLine( new Line2D.Double( x, y1, x, y2 ), transform );
-        }
+    public GridNode( final Property<ModelViewTransform> transform, final double spacing, final Point2D.Double center ) {
         setPickable( false );
         setChildrenPickable( false );
+
+        transform.addObserver( new SimpleObserver() {
+            public void update() {
+                removeAllChildren();
+                //horizontal lines
+                for ( int i = -NUM_GRID_LINES; i <= NUM_GRID_LINES; i++ ) {
+                    double y = i * spacing + center.y;
+                    double x1 = NUM_GRID_LINES * spacing + center.x;
+                    double x2 = -NUM_GRID_LINES * spacing + center.x;
+                    addGridLine( new Line2D.Double( x1, y, x2, y ), transform );
+                }
+
+                //vertical lines
+                for ( int i = -NUM_GRID_LINES; i <= NUM_GRID_LINES; i++ ) {
+                    double x = i * spacing + center.x;
+                    double y1 = NUM_GRID_LINES * spacing + center.y;
+                    double y2 = -NUM_GRID_LINES * spacing + center.y;
+                    addGridLine( new Line2D.Double( x, y1, x, y2 ), transform );
+                }
+            }
+        } );
     }
 
     private void addGridLine( Line2D.Double line, Property<ModelViewTransform> transform ) {
