b2ec8dd02dd32700f2d6604838f64d45b2fd2797
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-02-04 01:04:03 +0000

    add "Under Construction" notice to Game tab

--- trunk/simulations-java/simulations/balancing-chemical-equations/src/edu/colorado/phet/balancingchemicalequations/module/game/GameCanvas.java
+++ trunk/simulations-java/simulations/balancing-chemical-equations/src/edu/colorado/phet/balancingchemicalequations/module/game/GameCanvas.java
@@ -3,12 +3,15 @@
 package edu.colorado.phet.balancingchemicalequations.module.game;
 
 import java.awt.Color;
+import java.awt.Font;
 
 import edu.colorado.phet.balancingchemicalequations.BCEGlobalProperties;
 import edu.colorado.phet.balancingchemicalequations.view.BCECanvas;
 import edu.colorado.phet.common.phetcommon.model.Resettable;
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
+import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.nodes.ResetAllButtonNode;
+import edu.umd.cs.piccolo.nodes.PText;
 
 /**
  * Canvas for the "Game" module.
@@ -20,6 +23,11 @@ public class GameCanvas extends BCECanvas {
     public GameCanvas( GameModel model, BCEGlobalProperties globalProperties, Resettable resettable ) {
         super( globalProperties.getCanvasColorProperty() );
 
+        // Under Construction notice
+        PText underConstructionNode = new PText( "Under Construction" );
+        underConstructionNode.setFont( new PhetFont( Font.BOLD, 20 ) );
+        addChild( underConstructionNode );
+
         // Reset All button
         ResetAllButtonNode resetAllButtonNode = new ResetAllButtonNode( resettable, globalProperties.getFrame(), 12, Color.BLACK, Color.WHITE );
         resetAllButtonNode.addResettable( globalProperties );
@@ -28,6 +36,9 @@ public class GameCanvas extends BCECanvas {
         // layout
         double x = 0;
         double y = 0;
+        underConstructionNode.setOffset( x, y );
+        x = underConstructionNode.getFullBoundsReference().getCenterX() - ( resetAllButtonNode.getFullBoundsReference().getWidth() / 2 );
+        y = underConstructionNode.getFullBoundsReference().getMaxY() + 100;
         resetAllButtonNode.setOffset( x, y );
 
         // Observers
