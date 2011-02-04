25a2cb7f6cdfd609eeea868d84282b2b8935147f
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-02-04 22:52:44 +0000

    refactored clock creation into model

--- trunk/simulations-java/simulations/light-reflection-and-refraction/src/edu/colorado/phet/lightreflectionandrefraction/model/LRRModel.java
+++ trunk/simulations-java/simulations/light-reflection-and-refraction/src/edu/colorado/phet/lightreflectionandrefraction/model/LRRModel.java
@@ -11,6 +11,7 @@ import edu.colorado.phet.common.phetcommon.model.Property;
 import edu.colorado.phet.common.phetcommon.model.clock.ClockAdapter;
 import edu.colorado.phet.common.phetcommon.model.clock.ClockEvent;
 import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
+import edu.colorado.phet.common.phetcommon.model.clock.IClock;
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 import edu.colorado.phet.common.phetcommon.util.VoidFunction1;
 import edu.umd.cs.piccolo.util.PDimension;
@@ -31,8 +32,8 @@ public class LRRModel {
     public Point2D lastEmissionPoint = new Point2D.Double();
     private double lightSpeedScaleFactor = 1.0;
 
-    public LRRModel( final ConstantDtClock clock ) {
-        this.clock = clock;
+    public LRRModel() {
+        this.clock = new ConstantDtClock( 20, 1e-15 );
         final SimpleObserver rayMaker = new SimpleObserver() {
             public void update() {
                 if ( laserEmissionRay != null ) {
@@ -91,5 +92,9 @@ public class LRRModel {
 
     public void setLightSpeedScaleFactor( double scaleFactor ) {
         this.lightSpeedScaleFactor = scaleFactor;
+    }
+
+    public IClock getClock() {
+        return clock;
     }
 }
--- trunk/simulations-java/simulations/light-reflection-and-refraction/src/edu/colorado/phet/lightreflectionandrefraction/modules/intro/IntroModule.java
+++ trunk/simulations-java/simulations/light-reflection-and-refraction/src/edu/colorado/phet/lightreflectionandrefraction/modules/intro/IntroModule.java
@@ -1,19 +1,15 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.lightreflectionandrefraction.modules.intro;
 
-import edu.colorado.phet.common.phetcommon.application.Module;
-import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
 import edu.colorado.phet.lightreflectionandrefraction.model.LRRModel;
+import edu.colorado.phet.lightreflectionandrefraction.modules.LRRModule;
 
 /**
  * @author Sam Reid
  */
-public class IntroModule extends Module {
+public class IntroModule extends LRRModule<LRRModel> {
     public IntroModule() {
-        super( "Intro", new ConstantDtClock( 30.0 ) );
-        LRRModel model = new LRRModel( new ConstantDtClock( 20, 1e-15 ) );
-        setSimulationPanel( new LightReflectionAndRefractionCanvas( model ) );
-        getModulePanel().setLogoPanel( null );
-        setClockControlPanel( null );
+        super( "Intro", new LRRModel() );
+        setSimulationPanel( new LightReflectionAndRefractionCanvas( getLRRModel() ) );
     }
 }
