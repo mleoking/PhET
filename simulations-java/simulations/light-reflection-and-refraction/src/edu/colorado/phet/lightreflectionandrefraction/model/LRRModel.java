4eda79a8082d0e3e4d4725f455dbc9babfb7ddd8
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-02-04 19:14:12 +0000

    Added on/off button for laser

--- simulations-java/simulations/light-reflection-and-refraction/src/edu/colorado/phet/lightreflectionandrefraction/model/LRRModel.java
+++ simulations-java/simulations/light-reflection-and-refraction/src/edu/colorado/phet/lightreflectionandrefraction/model/LRRModel.java
@@ -1,23 +1,55 @@
+// Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.lightreflectionandrefraction.model;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
+import edu.colorado.phet.common.phetcommon.model.Property;
 import edu.colorado.phet.common.phetcommon.model.clock.ClockAdapter;
 import edu.colorado.phet.common.phetcommon.model.clock.ClockEvent;
 import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
+import edu.colorado.phet.common.phetcommon.util.VoidFunction1;
+import edu.umd.cs.piccolo.util.PDimension;
 
 public class LRRModel {
     private List<LightRay> rays = new LinkedList<LightRay>();
     private ConstantDtClock clock;
 
     public static final double C = 2.99792458e8;
+    public Property<Boolean> laserOn = new Property<Boolean>( false );
+    final double redWavelength = 650E-9;
+    final double modelWidth = redWavelength * 50;
+    public static final PDimension STAGE_SIZE = new PDimension( 1008, 680 );
+    final double modelHeight = STAGE_SIZE.getHeight() / STAGE_SIZE.getWidth() * modelWidth;
+    private ArrayList<VoidFunction1<LightRay>> rayAddedListeners = new ArrayList<VoidFunction1<LightRay>>();
+    private LightRay emittingRay;
 
     public LRRModel( final ConstantDtClock clock ) {
         this.clock = clock;
         clock.addClockListener( new ClockAdapter() {
             @Override
             public void simulationTimeChanged( ClockEvent clockEvent ) {
+                if ( laserOn.getValue() ) {
+                    if ( emittingRay == null ) {
+                        final ImmutableVector2D tail = new ImmutableVector2D( -modelWidth / 2, modelHeight / 2 );
+                        final ImmutableVector2D origin = new ImmutableVector2D( 0, 0 );
+                        ImmutableVector2D diff = origin.getSubtractedInstance( tail ).getInstanceOfMagnitude( 1.0 ).getScaledInstance( 1E-12 );//point in the right direction, but keep it small
+
+                        emittingRay = new LightRay( new Property<ImmutableVector2D>( tail ), new Property<ImmutableVector2D>( tail.getAddedInstance( diff ) ), 1.0, redWavelength );
+                        for ( VoidFunction1<LightRay> rayAddedListener : rayAddedListeners ) {
+                            rayAddedListener.apply( emittingRay );
+                        }
+                    }
+                }
+                else {
+                    if ( emittingRay != null ) {
+                        rays.add( emittingRay );
+                        emittingRay = null;
+                    }
+                }
+                if ( emittingRay != null ) { emittingRay.propagateTip( clock.getSimulationTimeChange() ); }
                 for ( LightRay ray : rays ) {
                     ray.propagate( clock.getSimulationTimeChange() );
                 }
@@ -26,9 +58,15 @@ public class LRRModel {
         clock.start();
     }
 
-    public void addRay( LightRay ray ) {
-        rays.add( ray );
+    public double getWidth() {
+        return modelWidth;
     }
 
+    public double getHeight() {
+        return modelHeight;
+    }
 
+    public void addRayAddedListener( VoidFunction1<LightRay> listener ) {
+        rayAddedListeners.add( listener );
+    }
 }
--- simulations-java/simulations/light-reflection-and-refraction/src/edu/colorado/phet/lightreflectionandrefraction/modules/intro/LightReflectionAndRefractionCanvas.java
+++ simulations-java/simulations/light-reflection-and-refraction/src/edu/colorado/phet/lightreflectionandrefraction/modules/intro/LightReflectionAndRefractionCanvas.java
@@ -4,45 +4,54 @@ package edu.colorado.phet.lightreflectionandrefraction.modules.intro;
 import java.awt.*;
 import java.awt.geom.Rectangle2D;
 
-import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
-import edu.colorado.phet.common.phetcommon.model.Property;
+import edu.colorado.phet.common.phetcommon.util.VoidFunction1;
+import edu.colorado.phet.common.phetcommon.view.VerticalLayoutPanel;
+import edu.colorado.phet.common.phetcommon.view.controls.PropertyRadioButton;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
+import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
+import edu.colorado.phet.common.phetcommon.view.util.SwingUtils;
 import edu.colorado.phet.common.piccolophet.PhetPCanvas;
 import edu.colorado.phet.lightreflectionandrefraction.model.LRRModel;
 import edu.colorado.phet.lightreflectionandrefraction.model.LightRay;
 import edu.colorado.phet.lightreflectionandrefraction.view.LightRayNode;
 import edu.umd.cs.piccolo.PNode;
-import edu.umd.cs.piccolo.util.PDimension;
+import edu.umd.cs.piccolox.pswing.PSwing;
 
 /**
  * @author Sam Reid
  */
 public class LightReflectionAndRefractionCanvas extends PhetPCanvas {
-    public static final PDimension STAGE_SIZE = new PDimension( 1008, 680 );
     private PNode rootNode;
 
-    public LightReflectionAndRefractionCanvas( LRRModel model ) {
+    public LightReflectionAndRefractionCanvas( final LRRModel model ) {
         // Root of our scene graph
         rootNode = new PNode();
         addWorldChild( rootNode );
 
-        setWorldTransformStrategy( new PhetPCanvas.CenteredStage( this, STAGE_SIZE ) );
+        setWorldTransformStrategy( new PhetPCanvas.CenteredStage( this, LRRModel.STAGE_SIZE ) );
 
         setBackground( Color.black );
-        double redWavelength = 650E-9;
 
-        double modelWidth = redWavelength * 50;
-        double modelHeight = STAGE_SIZE.getHeight() / STAGE_SIZE.getWidth() * modelWidth;
-        ModelViewTransform transform = ModelViewTransform.createRectangleInvertedYMapping(
-                new Rectangle2D.Double( -modelWidth / 2, -modelHeight / 2, modelWidth, modelHeight ),
-                new Rectangle2D.Double( 0, 0, STAGE_SIZE.width, STAGE_SIZE.height ) );
+        final ModelViewTransform transform = ModelViewTransform.createRectangleInvertedYMapping(
+                new Rectangle2D.Double( -model.getWidth() / 2, -model.getHeight() / 2, model.getWidth(), model.getHeight() ),
+                new Rectangle2D.Double( 0, 0, LRRModel.STAGE_SIZE.width, LRRModel.STAGE_SIZE.height ) );
 
-        Property<ImmutableVector2D> start = new Property<ImmutableVector2D>( new ImmutableVector2D() );
-        Property<ImmutableVector2D> tail = new Property<ImmutableVector2D>( new ImmutableVector2D( modelWidth / 4, modelHeight / 4 ) );
-        LightRay ray = new LightRay( start, tail, 1.0, redWavelength );
-        model.addRay( ray );
-        LightRayNode rayNode = new LightRayNode( transform, ray );
-        addChild( rayNode );
+        addChild( new PSwing( new VerticalLayoutPanel() {{
+            add( new PropertyRadioButton<Boolean>( "On", model.laserOn, true ) {{
+                setForeground( Color.white );
+                setFont( new PhetFont( 18, true ) );
+            }} );
+            add( new PropertyRadioButton<Boolean>( "Off", model.laserOn, false ) {{
+                setForeground( Color.white );
+                setFont( new PhetFont( 18, true ) );
+            }} );
+            SwingUtils.setBackgroundDeep( this, Color.black );
+        }} ) );
+        model.addRayAddedListener( new VoidFunction1<LightRay>() {
+            public void apply( LightRay lightRay ) {
+                addChild( new LightRayNode( transform, lightRay ) );
+            }
+        } );
     }
 
     protected void addChild( PNode node ) {
