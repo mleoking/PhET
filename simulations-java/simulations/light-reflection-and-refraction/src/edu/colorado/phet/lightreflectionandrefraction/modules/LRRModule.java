160a9953da3cd4a5a5222945b7fe1fc18a60b87d
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-02-04 22:52:44 +0000

    refactored clock creation into model

---
+++ simulations-java/simulations/light-reflection-and-refraction/src/edu/colorado/phet/lightreflectionandrefraction/modules/LRRModule.java
@@ -0,0 +1,21 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.lightreflectionandrefraction.modules;
+
+import edu.colorado.phet.common.phetcommon.application.Module;
+import edu.colorado.phet.lightreflectionandrefraction.model.LRRModel;
+
+/**
+ * @author Sam Reid
+ */
+public class LRRModule<T extends LRRModel> extends Module {
+    private final T model;
+
+    public LRRModule( String name, T model ) {
+        super( name, model.getClock() );
+        this.model = model;
+    }
+
+    public T getLRRModel() {
+        return model;
+    }
+}
