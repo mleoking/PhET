729cc3c6eb7f44ebd80e85566599aae1d7950499
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-02-05 05:54:29 +0000

    fix #2715, JSpinners cause ClassCastException, rewrote parts of EquationNode

--- trunk/simulations-java/simulations/balancing-chemical-equations/src/edu/colorado/phet/balancingchemicalequations/view/EquationNode.java
+++ trunk/simulations-java/simulations/balancing-chemical-equations/src/edu/colorado/phet/balancingchemicalequations/view/EquationNode.java
@@ -4,6 +4,8 @@ package edu.colorado.phet.balancingchemicalequations.view;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 
 import javax.swing.event.ChangeEvent;
@@ -15,7 +17,6 @@ import edu.colorado.phet.common.phetcommon.model.Property;
 import edu.colorado.phet.common.phetcommon.util.IntegerRange;
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 import edu.colorado.phet.common.phetcommon.view.controls.IntegerSpinner;
-import edu.colorado.phet.common.phetcommon.view.util.HTMLUtils;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.PhetPNode;
 import edu.colorado.phet.common.piccolophet.nodes.HTMLNode;
@@ -34,14 +35,14 @@ import edu.umd.cs.piccolox.pswing.PSwing;
 public class EquationNode extends PhetPNode  {
 
     private static final Font FONT = new PhetFont( 20 );
-    private static final Color NAME_COLOR = Color.BLACK;
+    private static final Color SYMBOL_COLOR = Color.BLACK;
 
     private static final Color COEFFICIENT_COLOR = Color.BLACK;
 
     private final IntegerRange coefficientRange;
     private boolean editable;
     private final HorizontalAligner aligner;
-    private ArrayList<ActualCoefficientNode> actualCoefficientNodes;
+    private ArrayList<TermNode> termNodes;
     private Equation equation;
     private final SimpleObserver coefficientsObserver;
     private final RightArrowNode arrowNode;
@@ -61,10 +62,13 @@ public class EquationNode extends PhetPNode  {
         this.coefficientRange = coefficientRange;
         this.editable = editable;
         this.aligner = aligner;
-        this.actualCoefficientNodes = new ArrayList<ActualCoefficientNode>();
+        this.termNodes = new ArrayList<TermNode>();
 
         arrowNode = new RightArrowNode( equationProperty.getValue().isBalanced() );
         addChild( arrowNode );
+        double x = aligner.getCenterXOffset() - ( arrowNode.getFullBoundsReference().getWidth() / 2 );
+        double y = ( SymbolNode.getCapHeight() / 2 );
+        arrowNode.setOffset( x, y );
 
         termsParent = new PhetPNode();
         addChild( termsParent );
@@ -72,15 +76,17 @@ public class EquationNode extends PhetPNode  {
         // coefficient changes
         coefficientsObserver = new SimpleObserver() {
             public void update() {
-                updateNode();
+                arrowNode.setHighlighted( equation.isBalanced() );
             }
         };
+        // equation changes
         this.equation = equationProperty.getValue();
         equationProperty.addObserver( new SimpleObserver() {
             public void update() {
                 EquationNode.this.equation.removeCoefficientsObserver( coefficientsObserver );
                 EquationNode.this.equation = equationProperty.getValue();
                 EquationNode.this.equation.addCoefficientsObserver( coefficientsObserver );
+                updateNode();
             }
         } );
     }
@@ -88,7 +94,9 @@ public class EquationNode extends PhetPNode  {
     public void setEditable( boolean editable ) {
         if ( editable != this.editable ) {
             this.editable = editable;
-            updateNode();
+            for ( TermNode termNode : termNodes ) {
+                termNode.setEditable( editable );
+            }
         }
     }
 
@@ -97,40 +105,33 @@ public class EquationNode extends PhetPNode  {
     }
 
     /*
-     * Note:
-     * This layout algorithm depends on the fact that all terms contain at least 1 uppercase letter.
-     * This allows us to align the baselines of HTML-formatted text.
+     * Rebuilds the left and right sides of the equation.
      */
     private void updateNode() {
 
         termsParent.removeAllChildren();
-        for ( ActualCoefficientNode node : actualCoefficientNodes ) {
-            node.removeObserver();
+
+        for ( TermNode node : termNodes ) {
+            node.cleanup();
         }
-        actualCoefficientNodes.clear();
+        termNodes.clear();
 
-        // determine cap height of the font, using a char that has no descender
-        final double capHeight = new SymbolNode( "T" ).getFullBounds().getHeight();
-
-        updateSideOfEquation( equation.getReactants(), aligner.getReactantXOffsets( equation ), capHeight );
-        updateSideOfEquation( equation.getProducts(), aligner.getProductXOffsets( equation ), capHeight );
-
-        // right-pointing arrow
-        double x = aligner.getCenterXOffset() - ( arrowNode.getFullBoundsReference().getWidth() / 2 );
-        double y = ( capHeight / 2 );
-        arrowNode.setOffset( x, y );
-        arrowNode.setHighlighted( equation.isBalanced() );
+        updateSideOfEquation( equation.getReactants(), aligner.getReactantXOffsets( equation ) );
+        updateSideOfEquation( equation.getProducts(), aligner.getProductXOffsets( equation ) );
     }
 
     /*
      * Updates one side of the equation.
+     * This layout algorithm depends on the fact that all terms contain at least 1 capital letter.
+     * This allows us to align the baselines of HTML-formatted text.
      */
-    private void updateSideOfEquation( EquationTerm[] terms, double[] xOffsets, double capHeight ) {
+    private void updateSideOfEquation( EquationTerm[] terms, double[] xOffsets ) {
         assert( terms.length == xOffsets.length );
         for ( int i = 0; i < terms.length; i++ ) {
 
             // term
             TermNode termNode = new TermNode( coefficientRange, terms[i], editable );
+            termNodes.add( termNode );
             termsParent.addChild( termNode );
             termNode.setOffset( xOffsets[i] - ( termNode.getFullBoundsReference().getWidth() / 2 ), 0 );
 
@@ -139,7 +140,7 @@ public class EquationNode extends PhetPNode  {
                 PlusNode plusNode = new PlusNode();
                 termsParent.addChild( plusNode );
                 double x =  xOffsets[i] - ( ( xOffsets[i] - xOffsets[i-1] ) / 2 ) - ( plusNode.getFullBoundsReference().getWidth() / 2 ); // centered between 2 offsets
-                double y = ( capHeight / 2 ) - ( plusNode.getFullBoundsReference().getHeight() / 2 );
+                double y = ( SymbolNode.getCapHeight() / 2 ) - ( plusNode.getFullBoundsReference().getHeight() / 2 );
                 plusNode.setOffset( x, y );
             }
         }
@@ -151,17 +152,12 @@ public class EquationNode extends PhetPNode  {
      */
     private static class TermNode extends PhetPNode {
 
+        private final CoefficientNode coefficientNode;
+
         public TermNode( IntegerRange coefficientRange, EquationTerm term, boolean editable ) {
 
             // coefficient
-            PNode coefficientNode = null;
-            if ( editable ) {
-                coefficientNode = new ActualCoefficientNode( coefficientRange, term.getActualCoefficientProperty() );
-            }
-            else {
-                coefficientNode = new BalancedCoefficientNode( term.getBalancedCoefficient() );
-
-            }
+            coefficientNode = new CoefficientNode( coefficientRange, term.getActualCoefficientProperty(), editable );
             addChild( coefficientNode );
 
             // molecule symbol
@@ -169,37 +165,39 @@ public class EquationNode extends PhetPNode  {
             addChild( symbolNode );
 
             // layout
-            double x = 0;
-            double y = 0;
-            coefficientNode.setOffset( x, y );
-            x = coefficientNode.getFullBoundsReference().getMaxX() + 5;
-            y = 0;
-            symbolNode.setOffset( x, y );
+            coefficientNode.setOffset( 0, 0 );
+            symbolNode.setOffset( coefficientNode.getFullBoundsReference().getMaxX() + 5, 0 );
+        }
+
+        public void setEditable( boolean editable ) {
+            coefficientNode.setEditable( editable );
+        }
+
+        public void cleanup() {
+            coefficientNode.removeCoefficientObserver();
         }
     }
 
     /*
-     * Balanced coefficient, a constant that cannot be edited.
+     * Coefficient, can be read-only or editable
      */
-    private static class BalancedCoefficientNode extends PText {
-
-        public BalancedCoefficientNode( int coefficient ) {
-            super( String.valueOf( coefficient ) );
-            setFont( FONT );
-            setTextPaint( COEFFICIENT_COLOR );
-        }
-    }
-
-    /*
-     * Actual coefficient, editable spinner.
-     */
-    private static class ActualCoefficientNode extends PhetPNode {
+    private static class CoefficientNode extends PhetPNode {
 
         private final Property<Integer> coefficientProperty;
-        private final SimpleObserver observer;
+        private final SimpleObserver coefficientObserver;
+        private final PText textNode;
+        private final PSwing spinnerNode;
 
-        public ActualCoefficientNode( IntegerRange range, final Property<Integer> coefficientProperty ) {
-            this.coefficientProperty = coefficientProperty;
+        public CoefficientNode( IntegerRange range, final Property<Integer> coefficientProperty, boolean editable ) {
+
+            // read-only text
+            textNode = new PText();
+            textNode.setFont( FONT );
+            textNode.setTextPaint( COEFFICIENT_COLOR );
+            addChild( textNode );
+            textNode.setVisible( !editable );
+
+            // editable spinner
             final IntegerSpinner spinner = new IntegerSpinner( range );
             spinner.setFont( FONT );
             spinner.setForeground( COEFFICIENT_COLOR );
@@ -209,17 +207,36 @@ public class EquationNode extends PhetPNode  {
                     coefficientProperty.setValue( spinner.getIntValue() );
                 }
             } );
-            observer = new SimpleObserver() {
+            spinnerNode = new PSwing( spinner );
+            addChild( spinnerNode );
+            spinnerNode.setVisible( editable );
+
+            // layout
+            spinnerNode.setOffset( 0, 0 );
+            textNode.addPropertyChangeListener( PROPERTY_FULL_BOUNDS, new PropertyChangeListener() {
+                public void propertyChange( PropertyChangeEvent evt ) {
+                    textNode.setOffset( spinnerNode.getFullBoundsReference().getMaxX() - textNode.getFullBoundsReference().getWidth(), 0 );
+                }
+            } );
+
+            // coefficient observer
+            this.coefficientProperty = coefficientProperty;
+            coefficientObserver = new SimpleObserver() {
                 public void update() {
+                    textNode.setText( String.valueOf( coefficientProperty.getValue() ) );
                     spinner.setIntValue( coefficientProperty.getValue() );
                 }
             };
-            coefficientProperty.addObserver( observer );
-            addChild( new PSwing( spinner ) );
+            coefficientProperty.addObserver( coefficientObserver );
         }
 
-        public void removeObserver() {
-            coefficientProperty.removeObserver( observer );
+        public void setEditable( boolean editable ) {
+            textNode.setVisible( !editable );
+            spinnerNode.setVisible( editable );
+        }
+
+        public void removeCoefficientObserver() {
+            coefficientProperty.removeObserver( coefficientObserver );
         }
     }
 
@@ -229,7 +246,14 @@ public class EquationNode extends PhetPNode  {
     private static class SymbolNode extends HTMLNode {
 
         public SymbolNode( String html ) {
-            super( HTMLUtils.toHTMLString( html ), NAME_COLOR, FONT );
+            super( html, SYMBOL_COLOR, FONT );
+        }
+
+        /**
+         * Gets the height of a capital letter.
+         */
+        public static double getCapHeight() {
+            return new SymbolNode( "T" ).getFullBounds().getHeight();
         }
     }
 }
