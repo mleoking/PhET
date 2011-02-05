11746da5b4ae90980e402ef3b8bd1f5828912a81
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-02-05 06:33:11 +0000

    Added model for transmitted and reflected power

--- simulations-java/simulations/light-reflection-and-refraction/src/edu/colorado/phet/lightreflectionandrefraction/view/LightRayNode.java
+++ simulations-java/simulations/light-reflection-and-refraction/src/edu/colorado/phet/lightreflectionandrefraction/view/LightRayNode.java
@@ -15,7 +15,7 @@ import edu.umd.cs.piccolo.PNode;
  */
 public class LightRayNode extends PNode {
     public LightRayNode( final ModelViewTransform transform, final LightRay lightRay ) {
-        addChild( new PhetPPath( new BasicStroke( 2 ), Color.red ) {{
+        addChild( new PhetPPath( new BasicStroke( 2 ), new Color( 1f, 0, 0, (float) lightRay.getPowerFraction() ) ) {{
             lightRay.addObserver( new SimpleObserver() {
                 public void update() {
                     setPathTo( transform.modelToView( new Line2D.Double( lightRay.tip.getValue().toPoint2D(), lightRay.tail.getValue().toPoint2D() ) ) );
