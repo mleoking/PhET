8e7f10d5c71ffde679e477f8d4a11b82c8dbc323
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-02-05 03:59:57 +0000

    Added control panel for indices of refraction

--- simulations-java/simulations/light-reflection-and-refraction/src/edu/colorado/phet/lightreflectionandrefraction/modules/intro/MediumNode.java
+++ simulations-java/simulations/light-reflection-and-refraction/src/edu/colorado/phet/lightreflectionandrefraction/modules/intro/MediumNode.java
@@ -1,8 +1,8 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.lightreflectionandrefraction.modules.intro;
 
-import java.awt.*;
-
+import edu.colorado.phet.common.phetcommon.model.Property;
+import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.umd.cs.piccolo.PNode;
@@ -11,9 +11,15 @@ import edu.umd.cs.piccolo.PNode;
  * @author Sam Reid
  */
 public class MediumNode extends PNode {
-    public MediumNode( final ModelViewTransform transform, final Medium medium ) {
-        addChild( new PhetPPath( Color.green ) {{
-            setPathTo( transform.modelToView( medium.getShape() ) );
+    public MediumNode( final ModelViewTransform transform, final Property<Medium> medium ) {
+        addChild( new PhetPPath( medium.getValue().getColor() ) {{
+            medium.addObserver( new SimpleObserver() {
+                public void update() {
+                    setPathTo( transform.modelToView( medium.getValue().getShape() ) );
+                    setPaint( medium.getValue().getColor() );
+                }
+            } );
         }} );
+
     }
 }
