aca67bb6aa4941a8165c136f424435c010dec3c3
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-02-06 19:36:07 +0000

    Start the intensity meter in the bottom right

--- simulations-java/simulations/light-reflection-and-refraction/src/edu/colorado/phet/lightreflectionandrefraction/modules/intro/IntensityMeter.java
+++ simulations-java/simulations/light-reflection-and-refraction/src/edu/colorado/phet/lightreflectionandrefraction/modules/intro/IntensityMeter.java
@@ -12,11 +12,16 @@ import edu.colorado.phet.common.phetcommon.model.Property;
  * @author Sam Reid
  */
 public class IntensityMeter {
-    public final Property<ImmutableVector2D> sensorPosition = new Property<ImmutableVector2D>( new ImmutableVector2D( 0, 0 ) );
-    public final Property<ImmutableVector2D> bodyPosition = new Property<ImmutableVector2D>( new ImmutableVector2D( 0, 0 ) );
+    public final Property<ImmutableVector2D> sensorPosition;
+    public final Property<ImmutableVector2D> bodyPosition;
     public final Property<Boolean> enabled = new Property<Boolean>( false );
     public final Property<Reading> reading = new Property<Reading>( Reading.MISS );
-    private ArrayList<Reading> rayReadings = new ArrayList<Reading>();
+    private final ArrayList<Reading> rayReadings = new ArrayList<Reading>();
+
+    public IntensityMeter( double sensorX, double sensorY, double bodyX, double bodyY ) {
+        sensorPosition = new Property<ImmutableVector2D>( new ImmutableVector2D( sensorX, sensorY ) );
+        bodyPosition = new Property<ImmutableVector2D>( new ImmutableVector2D( bodyX, bodyY ) );
+    }
 
     public void translateSensor( Dimension2D delta ) {
         sensorPosition.setValue( sensorPosition.getValue().getAddedInstance( delta.getWidth(), delta.getHeight() ) );
