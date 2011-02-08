d666dfcf8c1c39cc300792172b4449d4d23b9e53
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-02-08 19:19:34 +0000

    2/8/2100 Generalized to parent types in BooleanProperty combinators and Or

--- trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/model/Or.java
+++ trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/model/Or.java
@@ -12,7 +12,7 @@ import edu.colorado.phet.common.phetcommon.util.Function2;
  * @author Sam Reid
  */
 public class Or extends BinaryBooleanProperty {
-    public Or( Property<Boolean> a, Property<Boolean> b ) {
+    public Or( ObservableProperty<Boolean> a, ObservableProperty<Boolean> b ) {
         super( a, b, new Function2<Boolean, Boolean, Boolean>() {
             public Boolean apply( Boolean x, Boolean y ) {
                 return x || y;
