83af9bee5ec0535c04c1065e86d8ec47339d0bee
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-02-10 00:13:22 +0000

    wired up combo box

--- trunk/simulations-java/simulations/light-reflection-and-refraction/src/edu/colorado/phet/lightreflectionandrefraction/modules/intro/IndexOfRefractionSlider.java
+++ trunk/simulations-java/simulations/light-reflection-and-refraction/src/edu/colorado/phet/lightreflectionandrefraction/modules/intro/IndexOfRefractionSlider.java
@@ -9,6 +9,7 @@ import javax.swing.event.ChangeListener;
 
 import edu.colorado.phet.common.phetcommon.model.Property;
 import edu.colorado.phet.common.phetcommon.util.Function1;
+import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 import edu.colorado.phet.common.phetcommon.view.controls.valuecontrol.HorizontalLayoutStrategy;
 import edu.colorado.phet.common.phetcommon.view.controls.valuecontrol.LinearValueControl;
 import edu.colorado.phet.lightreflectionandrefraction.model.LRRModel;
@@ -30,6 +31,11 @@ public class IndexOfRefractionSlider extends LinearValueControl {
         addChangeListener( new ChangeListener() {
             public void stateChanged( ChangeEvent e ) {
                 medium.setValue( new Medium( medium.getValue().getShape(), getValue(), colorMappingFunction.getValue().apply( getValue() ) ) );
+            }
+        } );
+        medium.addObserver( new SimpleObserver() {
+            public void update() {
+                setValue( medium.getValue().getIndexOfRefraction() );
             }
         } );
     }
