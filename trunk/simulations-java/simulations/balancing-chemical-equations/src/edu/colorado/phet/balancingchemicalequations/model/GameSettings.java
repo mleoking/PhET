b8fca3077008a1afbadd05ef5d5ea2b974ad9678
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-02-10 19:13:21 +0000

    add Game Settings panel

--- trunk/simulations-java/simulations/balancing-chemical-equations/src/edu/colorado/phet/balancingchemicalequations/model/GameSettings.java
+++ trunk/simulations-java/simulations/balancing-chemical-equations/src/edu/colorado/phet/balancingchemicalequations/model/GameSettings.java
@@ -12,55 +12,19 @@ import edu.colorado.phet.common.phetcommon.util.IntegerRange;
  */
 public class GameSettings {
 
-    private final ConstrainedIntegerProperty levelProperty;
-    private final Property<Boolean> soundEnabledProperty;
-    private final Property<Boolean> timerEnabledProperty;
+    public final ConstrainedIntegerProperty level;
+    public final Property<Boolean> soundEnabled;
+    public final Property<Boolean> timerEnabled;
 
     public GameSettings( IntegerRange levelsRange, boolean soundEnabled, boolean timerEnabled ) {
-        this.levelProperty = new ConstrainedIntegerProperty( levelsRange );
-        this.soundEnabledProperty = new Property<Boolean>( soundEnabled );
-        this.timerEnabledProperty = new Property<Boolean>( timerEnabled );
+        this.level = new ConstrainedIntegerProperty( levelsRange );
+        this.soundEnabled = new Property<Boolean>( soundEnabled );
+        this.timerEnabled = new Property<Boolean>( timerEnabled );
     }
 
     public void reset() {
-        levelProperty.reset();
-        soundEnabledProperty.reset();
-        timerEnabledProperty.reset();
-    }
-
-    public void setLevel( int level ) {
-        levelProperty.setValue( level );
-    }
-
-    public int getLevel() {
-        return levelProperty.getValue();
-    }
-
-    public ConstrainedIntegerProperty getLevelProperty() {
-        return levelProperty;
-    }
-
-    public void setSoundEnabled( boolean enabled ) {
-        soundEnabledProperty.setValue( enabled );
-    }
-
-    public boolean isSoundEnabled() {
-        return soundEnabledProperty.getValue();
-    }
-
-    public Property<Boolean> getSoundEnabledProperty() {
-        return soundEnabledProperty;
-    }
-
-    public void setTimerEnabled( boolean enabled ) {
-        timerEnabledProperty.setValue( enabled );
-    }
-
-    public boolean isTimerEnabled() {
-        return timerEnabledProperty.getValue();
-    }
-
-    public Property<Boolean> getTimerEnabledProperty() {
-        return timerEnabledProperty;
+        level.reset();
+        soundEnabled.reset();
+        timerEnabled.reset();
     }
 }
--- trunk/simulations-java/simulations/balancing-chemical-equations/src/edu/colorado/phet/balancingchemicalequations/module/game/GameModel.java
+++ trunk/simulations-java/simulations/balancing-chemical-equations/src/edu/colorado/phet/balancingchemicalequations/module/game/GameModel.java
@@ -18,6 +18,15 @@ public class GameModel {
         gameSettings = new GameSettings( new IntegerRange( 1, 3, 1 ) /* level */, true /* sound */, true /* timer */ );
     }
 
+    public GameSettings getGameSettings() {
+        return gameSettings;
+    }
+
+    public void startGame() {
+        //TODO
+        System.out.println( "GameModel.startGame" );//XXX
+    }
+
     public void reset() {
         gameSettings.reset();
     }
