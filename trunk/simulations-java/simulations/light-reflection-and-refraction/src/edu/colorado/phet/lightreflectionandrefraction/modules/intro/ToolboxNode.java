e684f2b05fbfc4f0c8e82d10438273dc14a98457
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-02-10 03:41:36 +0000

    Fixed: Intensity meter should go back to the toolbox when the close button is pressed

--- trunk/simulations-java/simulations/light-reflection-and-refraction/src/edu/colorado/phet/lightreflectionandrefraction/modules/intro/ToolboxNode.java
+++ trunk/simulations-java/simulations/light-reflection-and-refraction/src/edu/colorado/phet/lightreflectionandrefraction/modules/intro/ToolboxNode.java
@@ -4,20 +4,15 @@ package edu.colorado.phet.lightreflectionandrefraction.modules.intro;
 import java.awt.*;
 import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
-import java.awt.image.RenderedImage;
-import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
 
 import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
 import edu.colorado.phet.common.phetcommon.model.BooleanProperty;
+import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 import edu.colorado.phet.common.phetcommon.view.controls.PropertyCheckBox;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
 import edu.colorado.phet.common.phetcommon.view.util.BufferedImageUtils;
 import edu.colorado.phet.common.piccolophet.event.CursorHandler;
 import edu.colorado.phet.lightreflectionandrefraction.LightReflectionAndRefractionApplication;
-import edu.colorado.phet.lightreflectionandrefraction.model.LRRModel;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
 import edu.umd.cs.piccolo.event.PInputEvent;
@@ -25,6 +20,8 @@ import edu.umd.cs.piccolo.nodes.PImage;
 import edu.umd.cs.piccolo.nodes.PText;
 import edu.umd.cs.piccolo.util.PDimension;
 import edu.umd.cs.piccolox.pswing.PSwing;
+
+import static edu.colorado.phet.lightreflectionandrefraction.model.LRRModel.redWavelength;
 
 /**
  * @author Sam Reid
@@ -60,30 +57,26 @@ public class ToolboxNode extends PNode {
         }};
         addChild( protractor );
 
-
-        final double modelWidth = LRRModel.redWavelength * 62;
+        //TODO: some constants copied from LRRModel
+        final double modelWidth = redWavelength * 62;
         final double modelHeight = modelWidth * 0.7;
-
-        IntensityMeter intensityMeterIcon = new IntensityMeter( modelWidth * 0.3, -modelHeight * 0.3, modelWidth * 0.4, -modelHeight * 0.3 );
-        intensityMeterIcon.enabled.setValue( true );
-        final IntensityMeterNode iconNode = new IntensityMeterNode( transform, intensityMeterIcon );
-        final Image myIcon = iconNode.toImage( (int) ( 100.0 * iconNode.getFullBounds().getWidth() / iconNode.getFullBounds().getHeight() ), 100, new Color( 0, 0, 0, 0 ) );
-        try {
-            ImageIO.write( (RenderedImage) myIcon, "PNG", new File( "C:/Users/Sam/Desktop/test-" + System.currentTimeMillis() ) );
-        }
-        catch ( IOException e ) {
-            e.printStackTrace();
-        }
-        System.out.println( "myIcon = " + myIcon );
-        final PImage sensor = new PImage( myIcon ) {{
+        final IntensityMeterNode iconNode = new IntensityMeterNode( transform, new IntensityMeter( modelWidth * 0.3, -modelHeight * 0.3, modelWidth * 0.4, -modelHeight * 0.3 ) {{
+            enabled.setValue( true );
+        }} );
+        final PImage sensorThumbnail = new PImage( iconNode.toImage( (int) ( 100.0 * iconNode.getFullBounds().getWidth() / iconNode.getFullBounds().getHeight() ), 100, new Color( 0, 0, 0, 0 ) ) ) {{
+            final PImage sensorThumbnailRef = this;
             addInputEventListener( new PBasicInputEventHandler() {
                 IntensityMeterNode node = null;
 
                 public void mouseDragged( PInputEvent event ) {
                     intensityMeter.enabled.setValue( true );
-                    setVisible( false );
                     if ( node == null ) {
                         node = new IntensityMeterNode( transform, intensityMeter );
+                        intensityMeter.enabled.addObserver( new SimpleObserver() {
+                            public void update() {
+                                sensorThumbnailRef.setVisible( !intensityMeter.enabled.getValue() );
+                            }
+                        } );
                         final ImmutableVector2D modelPt = new ImmutableVector2D( transform.viewToModel( event.getPositionRelativeTo( getParent().getParent().getParent() ) ) );
                         final ImmutableVector2D delta = modelPt.getSubtractedInstance( intensityMeter.sensorPosition.getValue() );
                         intensityMeter.translateAll( new PDimension( delta.getX(), delta.getY() ) );
@@ -91,14 +84,18 @@ public class ToolboxNode extends PNode {
                     }
                     node.doDrag( event );
                 }
+
+                public void mouseReleased( PInputEvent event ) {
+                    node = null;
+                }
             } );
             addInputEventListener( new CursorHandler() );
             setOffset( protractor.getFullBounds().getMaxX() + 10, protractor.getFullBounds().getCenterY() - getFullBounds().getHeight() / 2 );
         }};
-        addChild( sensor );
+        addChild( sensorThumbnail );
 
         addChild( new PSwing( new PropertyCheckBox( "Show Normal", showNormal ) {{setBackground( new Color( 0, 0, 0, 0 ) );}} ) {{
-            setOffset( sensor.getFullBounds().getMaxX() + 10, sensor.getFullBounds().getCenterY() - getFullBounds().getHeight() / 2 );
+            setOffset( sensorThumbnail.getFullBounds().getMaxX() + 10, sensorThumbnail.getFullBounds().getCenterY() - getFullBounds().getHeight() / 2 );
         }} );
         titleLabel.setOffset( getFullBounds().getWidth() / 2 - titleLabel.getFullBounds().getWidth() / 2, 0 );
     }
