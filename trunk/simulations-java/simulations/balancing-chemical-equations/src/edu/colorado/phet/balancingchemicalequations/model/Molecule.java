70ac2a651bc571355b9fbd01a5f98db25744e442
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-02-14 20:47:19 +0000

    create molecule symbols based on atom list

--- trunk/simulations-java/simulations/balancing-chemical-equations/src/edu/colorado/phet/balancingchemicalequations/model/Molecule.java
+++ trunk/simulations-java/simulations/balancing-chemical-equations/src/edu/colorado/phet/balancingchemicalequations/model/Molecule.java
@@ -21,18 +21,28 @@ import edu.umd.cs.piccolo.PNode;
  */
 public abstract class Molecule {
 
-    private final String symbol;
-    private final Image image;
-    private final Atom[] atoms; // order of the atoms determines their display order
 
-    public Molecule( String symbol, Image image, Atom[] atoms ) {
-        this.symbol = toSubscript( symbol );
+    private final Atom[] atoms;
+    private final Image image;
+    private final String symbol;
+
+    /**
+     * Constructor.
+     * Order of atoms determines their display order and format of symbol.
+     * Image must be provided because there is no general method for
+     * creating a visual representation based on a list of atoms.
+     *
+     * @param atoms
+     * @param image
+     */
+    public Molecule( Atom[] atoms, Image image ) {
+        this.symbol = createSymbol( atoms );
         this.image = image;
         this.atoms = atoms;
     }
 
-    protected Molecule( String symbol, PNode node, Atom[] atoms ) {
-        this( symbol, node.toImage(), atoms );
+    protected Molecule( Atom[] atoms, PNode node ) {
+        this( atoms, node.toImage() );
     }
 
     public String getSymbol() {
@@ -45,6 +55,35 @@ public abstract class Molecule {
 
     public Atom[] getAtoms() {
         return atoms;
+    }
+
+    /*
+     * Creates an HTML symbol based on the list of atoms in the molecule.
+     * The atoms must be specified in order of appearance in the symbol.
+     * For example [C,C,H,H,H] becomes C2H4.
+     */
+    private static final String createSymbol( Atom[] atoms ) {
+        StringBuffer b = new StringBuffer();
+        int atomCount = 1;
+        for ( int i = 0; i < atoms.length; i++ ) {
+            if ( i == 0 ) {
+                b.append( atoms[i].getSymbol() );
+            }
+            else if ( atoms[i].getClass().equals( atoms[i-1].getClass() ) ) {
+                atomCount++;
+            }
+            else {
+                if ( atomCount > 1 ) {
+                    b.append( String.valueOf( atomCount ) );
+                }
+                atomCount = 1;
+                b.append( atoms[i].getSymbol() );
+            }
+        }
+        if ( atomCount > 1 ) {
+            b.append( String.valueOf( atomCount ) );
+        }
+        return toSubscript( b.toString() );
     }
 
     /*
@@ -80,77 +119,77 @@ public abstract class Molecule {
     public static class CH4 extends Molecule {
 
         public CH4() {
-            super( "CH4", new CH4Node(), new Atom[] { new C(), new H(), new H(), new H(), new H() } );
+            super( new Atom[] { new C(), new H(), new H(), new H(), new H() }, new CH4Node() );
         }
     }
 
     public static class Cl2 extends Molecule {
 
         public Cl2() {
-            super( "Cl2", new Cl2Node(), new Atom[] { new Cl(), new Cl() } );
+            super( new Atom[] { new Cl(), new Cl() }, new Cl2Node() );
         }
     }
 
     public static class CO2 extends Molecule {
 
         public CO2() {
-            super( "CO2", new CO2Node(), new Atom[] { new C(), new O(), new O() } );
+            super( new Atom[] { new C(), new O(), new O() }, new CO2Node() );
         }
     }
 
     public static class F2 extends Molecule {
 
         public F2() {
-            super( "F2", new F2Node(), new Atom[] { new F(), new F() } );
+            super( new Atom[] { new F(), new F() }, new F2Node() );
         }
     }
 
     public static class H2 extends Molecule {
 
         public H2() {
-            super( "H2", new H2Node(), new Atom[] { new H(), new H() } );
+            super( new Atom[] { new H(), new H() }, new H2Node() );
         }
     }
 
     public static class H2O extends Molecule {
 
         public H2O() {
-            super( "H2O", new H2ONode(), new Atom[] { new H(), new H(), new O() } );
+            super( new Atom[] { new H(), new H(), new O() }, new H2ONode() );
         }
     }
 
     public static class HCl extends Molecule {
 
         public HCl() {
-            super( "HCl", new HClNode(), new Atom[] { new H(), new Cl() } );
+            super( new Atom[] { new H(), new Cl() }, new HClNode() );
         }
     }
 
     public static class HF extends Molecule {
 
         public HF() {
-            super( "HF", new HFNode(), new Atom[] { new H(), new F() } );
+            super( new Atom[] { new H(), new F() }, new HFNode() );
         }
     }
 
     public static class N2 extends Molecule {
 
         public N2() {
-            super( "N2", new N2Node(), new Atom[] { new N(), new N() } );
+            super( new Atom[] { new N(), new N() }, new N2Node() );
         }
     }
 
     public static class NH3 extends Molecule {
 
         public NH3() {
-            super( "NH3", new NH3Node(), new Atom[] { new N(), new H(), new H(), new H() } );
+            super( new Atom[] { new N(), new H(), new H(), new H() }, new NH3Node() );
         }
     }
 
     public static class O2 extends Molecule {
 
         public O2() {
-            super( "O2", new O2Node(), new Atom[] { new O(), new O() } );
+            super( new Atom[] { new O(), new O() }, new O2Node() );
         }
     }
 }
