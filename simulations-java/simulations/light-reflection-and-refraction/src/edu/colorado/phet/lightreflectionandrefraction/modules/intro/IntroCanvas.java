05a478166eee97f4ed5b37856f372a97dd890310
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-02-14 17:48:21 +0000

    refactored canvas to support multiple modules

--- simulations-java/simulations/light-reflection-and-refraction/src/edu/colorado/phet/lightreflectionandrefraction/modules/intro/IntroCanvas.java
+++ simulations-java/simulations/light-reflection-and-refraction/src/edu/colorado/phet/lightreflectionandrefraction/modules/intro/IntroCanvas.java
@@ -1,6 +1,11 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.lightreflectionandrefraction.modules.intro;
 
+import java.awt.*;
+import java.awt.geom.Line2D;
+
+import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
+import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.colorado.phet.lightreflectionandrefraction.view.LightReflectionAndRefractionCanvas;
 import edu.colorado.phet.lightreflectionandrefraction.view.MediumNode;
 
@@ -18,6 +23,19 @@ public class IntroCanvas extends LightReflectionAndRefractionCanvas<IntroModel> 
         }} );
         addChild( new ControlPanelNode( new MediumControlPanel( this, model.bottomMedium, model.colorMappingFunction ) ) {{
             setOffset( stageSize.width - getFullBounds().getWidth() - 10, transform.modelToViewY( 0 ) + 10 );
+        }} );
+
+        //add a line that will show the border between the mediums even when both n's are the same... Just a thin line will be fine.
+        addChild( new PhetPPath( transform.modelToView( new Line2D.Double( -1, 0, 1, 0 ) ), new BasicStroke( 0.5f ), Color.gray ) {{
+            setPickable( false );
+        }} );
+
+        addChild( new NormalLine( transform, model.getHeight() ) {{
+            showNormal.addObserver( new SimpleObserver() {
+                public void update() {
+                    setVisible( showNormal.getValue() );
+                }
+            } );
         }} );
     }
 }
