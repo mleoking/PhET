d932a48e09195bfb653adc9ff8a4094332bc3eef
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-02-16 00:19:14 +0000

    Added support for reconfiguring the set of buckets needed to hold the various isotopes when a different element is selected.

--- simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/modules/isotopemixture/model/IsotopeMixturesModel.java
+++ simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/modules/isotopemixture/model/IsotopeMixturesModel.java
@@ -2,11 +2,14 @@
 
 package edu.colorado.phet.buildanatom.modules.isotopemixture.model;
 
+import java.awt.Color;
 import java.awt.geom.Dimension2D;
+import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 
 import edu.colorado.phet.buildanatom.model.AtomIdentifier;
+import edu.colorado.phet.buildanatom.model.Bucket;
 import edu.colorado.phet.buildanatom.model.BuildAnAtomClock;
 import edu.colorado.phet.buildanatom.model.IAtom;
 import edu.colorado.phet.buildanatom.model.IConfigurableAtomModel;
@@ -40,6 +43,8 @@ public class IsotopeMixturesModel implements Resettable, IConfigurableAtomModel 
                 ISOTOPE_TEST_CHAMBER_SIZE.getWidth(),
                 ISOTOPE_TEST_CHAMBER_SIZE.getHeight() );
 
+    private static final Dimension2D BUCKET_SIZE = new PDimension( 50, 30 ); // In picometers.
+
     // -----------------------------------------------------------------------
     // Instance Data
     // -----------------------------------------------------------------------
@@ -56,12 +61,20 @@ public class IsotopeMixturesModel implements Resettable, IConfigurableAtomModel 
     // contains a list of all stable isotopes that match the atomic weight
     // of the currently configured isotope.  There should be only one of each
     // possible isotope.
-    private final Property< ArrayList<ImmutableAtom> > possibleIsotopesProperty =
-        new Property< ArrayList<ImmutableAtom> >(new ArrayList<ImmutableAtom>());
+    private final Property<ArrayList<ImmutableAtom>> possibleIsotopesProperty =
+            new Property<ArrayList<ImmutableAtom>>( new ArrayList<ImmutableAtom>() );
+
+    // This property contains the list of the buckets where isotopes that are
+    // not in the test chamber reside.  This changes whenever a new element is
+    // selected, since there is one bucket for each stable isotope
+    // configuration for a given element.
+    private final Property<ArrayList<Bucket>> bucketListProperty =
+            new Property<ArrayList<Bucket>>( new ArrayList<Bucket>() );
 
     // -----------------------------------------------------------------------
     // Constructor(s)
     // -----------------------------------------------------------------------
+
     public IsotopeMixturesModel( BuildAnAtomClock clock ) {
         this.clock = clock;
     }
@@ -87,10 +100,22 @@ public class IsotopeMixturesModel implements Resettable, IConfigurableAtomModel 
      * already existed at the time this class was created.
      */
     public void setAtomConfiguration( IAtom atom ) {
-        prototypeIsotope.setNumProtons( atom.getNumProtons() );
-        prototypeIsotope.setNumElectrons( atom.getNumElectrons() );
-        prototypeIsotope.setNumNeutrons( atom.getNumNeutrons() );
-        possibleIsotopesProperty.setValue( AtomIdentifier.getAllIsotopes( atom.getNumProtons() ) );
+
+        if ( prototypeIsotope.getNumProtons() != atom.getNumProtons() ||
+             prototypeIsotope.getNumNeutrons() != atom.getNumNeutrons() ||
+             prototypeIsotope.getNumElectrons() != atom.getNumElectrons()){
+
+            // Update the prototype atom (a.k.a. isotope) configuration.
+            prototypeIsotope.setNumProtons( atom.getNumProtons() );
+            prototypeIsotope.setNumElectrons( atom.getNumElectrons() );
+            prototypeIsotope.setNumNeutrons( atom.getNumNeutrons() );
+
+            // Update the list of possible isotopes for this atomic configuration.
+            possibleIsotopesProperty.setValue( AtomIdentifier.getAllIsotopes( atom.getNumProtons() ) );
+
+            // Update the buckets for this atomic configuration.
+            updateBuckets();
+        }
     }
 
     public Rectangle2D getIsotopeTestChamberRect(){
@@ -98,7 +123,37 @@ public class IsotopeMixturesModel implements Resettable, IConfigurableAtomModel 
     }
 
     public void reset() {
-        // TODO Auto-generated method stub
+        // Set the default element to be hydrogen.
+        setAtomConfiguration( new ImmutableAtom( 1, 0, 1 ) );
+    }
+
+    /**
+     * Update the set of buckets based on the current atom configuration.
+     * This always removes and creates buckets, so for the sake of efficiency
+     * it shouldn't be called unless a change has occurred to the isotope
+     * configuration.
+     */
+    private void updateBuckets(){
+        // Make a copy of the current bucket list so that we can send a
+        // notification of their demise.
+        ArrayList< Bucket > oldBuckets = new ArrayList< Bucket >( bucketListProperty.getValue() );
+
+        // Create a new list of buckets based on the stable isotopes of the
+        // currently configured element.
+        ArrayList< ImmutableAtom > isotopeList = AtomIdentifier.getAllIsotopes( prototypeIsotope.getNumProtons() );
+        double bucketYOffset = ISOTOPE_TEST_CHAMBER_RECT.getMaxY() + 20;
+        double interBucketDistanceX = ISOTOPE_TEST_CHAMBER_RECT.getWidth() / (isotopeList.size() + 1);
+        ArrayList<Bucket> newBucketList = new ArrayList<Bucket>();
+        for ( int i = 0; i < isotopeList.size(); i++ ){
+            newBucketList.add( new Bucket(new Point2D.Double(interBucketDistanceX * (i + 1), bucketYOffset),
+                    BUCKET_SIZE, Color.BLUE, AtomIdentifier.getName( isotopeList.get( i ) )) );
+        }
+        bucketListProperty.setValue( newBucketList );
+
+        // Send notifications of removal for the old buckets.
+        for ( Bucket bucket : oldBuckets ) {
+            bucket.getPartOfModelProperty().setValue( false );
+        }
     }
 
     // -----------------------------------------------------------------------
