259f52a2b5c6a7763ec0d3df5be76efc15991da3
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-02-16 01:26:41 +0000

    change HF to look like HCl

---
+++ trunk/simulations-java/simulations/balancing-chemical-equations/src/edu/colorado/phet/balancingchemicalequations/view/molecules/HFNode.java
@@ -0,0 +1,42 @@
+// Copyright 2002-2011, University of Colorado
+
+package edu.colorado.phet.balancingchemicalequations.view.molecules;
+
+import edu.colorado.phet.balancingchemicalequations.model.Atom.F;
+import edu.colorado.phet.balancingchemicalequations.model.Atom.H;
+import edu.colorado.phet.balancingchemicalequations.view.molecules.AtomNode.BigAtomNode;
+import edu.colorado.phet.balancingchemicalequations.view.molecules.AtomNode.SmallAtomNode;
+import edu.colorado.phet.common.piccolophet.util.PNodeLayoutUtils;
+import edu.umd.cs.piccolox.nodes.PComposite;
+
+/**
+ * HF molecule.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
+public class HFNode extends PComposite {
+
+    public HFNode() {
+
+        // atom nodes
+        AtomNode leftNode = new SmallAtomNode( new H() );
+        AtomNode rightNode = new BigAtomNode( new F() );
+
+        // rendering order
+        PComposite parentNode = new PComposite();
+        addChild( parentNode );
+        parentNode.addChild( rightNode );
+        parentNode.addChild( leftNode );
+
+        // layout
+        double x = 0;
+        double y = 0;
+        leftNode.setOffset( x, y );
+        x = leftNode.getXOffset() + ( 0.5 * rightNode.getFullBoundsReference().getWidth() );
+        y = leftNode.getYOffset();
+        rightNode.setOffset( x, y );
+
+        // move origin to geometric center
+        parentNode.setOffset( -PNodeLayoutUtils.getOriginXOffset( parentNode ), -PNodeLayoutUtils.getOriginYOffset( parentNode ) );
+    }
+}
