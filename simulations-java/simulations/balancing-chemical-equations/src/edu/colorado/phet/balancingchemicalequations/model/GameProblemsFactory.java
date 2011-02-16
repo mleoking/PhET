96d7498fd8443b7e58035e34d8fa752a9963a3bf
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-02-16 20:50:18 +0000

    factory for creating problem sets

---
+++ simulations-java/simulations/balancing-chemical-equations/src/edu/colorado/phet/balancingchemicalequations/model/GameProblemsFactory.java
@@ -0,0 +1,241 @@
+// Copyright 2002-2011, University of Colorado
+
+package edu.colorado.phet.balancingchemicalequations.model;
+
+import java.util.ArrayList;
+
+import edu.colorado.phet.balancingchemicalequations.model.OneProductEquation.AmmoniaEquation;
+import edu.colorado.phet.balancingchemicalequations.model.OneProductEquation.Equation_2CO_O2_2CO2;
+import edu.colorado.phet.balancingchemicalequations.model.OneProductEquation.Equation_2C_O2_2CO;
+import edu.colorado.phet.balancingchemicalequations.model.OneProductEquation.Equation_2N2_O2_2N2O;
+import edu.colorado.phet.balancingchemicalequations.model.OneProductEquation.Equation_2NO_O2_2NO2;
+import edu.colorado.phet.balancingchemicalequations.model.OneProductEquation.Equation_2SO2_O2_2SO3;
+import edu.colorado.phet.balancingchemicalequations.model.OneProductEquation.Equation_C2H2_2H2_C2H6;
+import edu.colorado.phet.balancingchemicalequations.model.OneProductEquation.Equation_C2H4_H2_C2H6;
+import edu.colorado.phet.balancingchemicalequations.model.OneProductEquation.Equation_CH2O_H2_CH3OH;
+import edu.colorado.phet.balancingchemicalequations.model.OneProductEquation.Equation_CO_2H2_CH3OH;
+import edu.colorado.phet.balancingchemicalequations.model.OneProductEquation.Equation_C_2S_CS2;
+import edu.colorado.phet.balancingchemicalequations.model.OneProductEquation.Equation_C_CO2_2CO;
+import edu.colorado.phet.balancingchemicalequations.model.OneProductEquation.Equation_C_O2_CO2;
+import edu.colorado.phet.balancingchemicalequations.model.OneProductEquation.Equation_H2_Cl2_2HCl;
+import edu.colorado.phet.balancingchemicalequations.model.OneProductEquation.Equation_H2_F2_2HF;
+import edu.colorado.phet.balancingchemicalequations.model.OneProductEquation.Equation_N2_3H2_2NH3;
+import edu.colorado.phet.balancingchemicalequations.model.OneProductEquation.Equation_N2_O2_2NO;
+import edu.colorado.phet.balancingchemicalequations.model.OneProductEquation.Equation_P4_10Cl2_4PCl5;
+import edu.colorado.phet.balancingchemicalequations.model.OneProductEquation.Equation_P4_6Cl2_4PCl3;
+import edu.colorado.phet.balancingchemicalequations.model.OneProductEquation.Equation_P4_6F2_4PF3;
+import edu.colorado.phet.balancingchemicalequations.model.OneProductEquation.Equation_P4_6H2_4PH3;
+import edu.colorado.phet.balancingchemicalequations.model.OneProductEquation.Equation_PCl3_Cl2_PCl5;
+import edu.colorado.phet.balancingchemicalequations.model.OneProductEquation.WaterEquation;
+import edu.colorado.phet.balancingchemicalequations.model.TwoProductEquation.Equation_2C2H2_5O2_4CO2_2H2O;
+import edu.colorado.phet.balancingchemicalequations.model.TwoProductEquation.Equation_2C2H6_7O2_4CO2_6H2O;
+import edu.colorado.phet.balancingchemicalequations.model.TwoProductEquation.Equation_2C_2H2O_CH4_CO2;
+import edu.colorado.phet.balancingchemicalequations.model.TwoProductEquation.Equation_2F2_H2O_OF2_2HF;
+import edu.colorado.phet.balancingchemicalequations.model.TwoProductEquation.Equation_4NH3_3O2_2N2_6H2O;
+import edu.colorado.phet.balancingchemicalequations.model.TwoProductEquation.Equation_4NH3_5O2_4NO_6H2O;
+import edu.colorado.phet.balancingchemicalequations.model.TwoProductEquation.Equation_4NH3_6NO_5N2_6H2O;
+import edu.colorado.phet.balancingchemicalequations.model.TwoProductEquation.Equation_4NH3_7O2_4NO2_6H2O;
+import edu.colorado.phet.balancingchemicalequations.model.TwoProductEquation.Equation_C2H4_3O2_2CO2_2H2O;
+import edu.colorado.phet.balancingchemicalequations.model.TwoProductEquation.Equation_C2H5OH_3O2_2CO2_3H2O;
+import edu.colorado.phet.balancingchemicalequations.model.TwoProductEquation.Equation_C2H6_Cl2_C2H5Cl_HCl;
+import edu.colorado.phet.balancingchemicalequations.model.TwoProductEquation.Equation_CH4_4S_CS2_2H2S;
+import edu.colorado.phet.balancingchemicalequations.model.TwoProductEquation.Equation_CH4_H2O_3H2_CO;
+import edu.colorado.phet.balancingchemicalequations.model.TwoProductEquation.Equation_CS2_3O2_CO2_2SO2;
+import edu.colorado.phet.balancingchemicalequations.model.TwoProductEquation.Equation_OF2_H2O_O2_2HF;
+import edu.colorado.phet.balancingchemicalequations.model.TwoProductEquation.Equation_SO2_2H2_S_2H2O;
+import edu.colorado.phet.balancingchemicalequations.model.TwoProductEquation.Equation_SO2_3H2_H2S_2H2O;
+import edu.colorado.phet.balancingchemicalequations.model.TwoProductEquation.MethaneEquation;
+
+/**
+ * Factory that creates problem sets, based on game level.
+ * The design document specifies which equations correspond to which game levels.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
+public class GameProblemsFactory {
+
+    // Level 1
+    private static final ArrayList<Class <? extends Equation>> LEVEL1_LIST = new ArrayList<Class<? extends Equation>>() {{
+        add( AmmoniaEquation.class );
+        add( WaterEquation.class );
+        add( Equation_H2_F2_2HF.class );
+        add( Equation_H2_Cl2_2HCl.class );
+        add( Equation_CO_2H2_CH3OH.class );
+        add( Equation_CH2O_H2_CH3OH.class );
+        add( Equation_C2H4_H2_C2H6.class );
+        add( Equation_C2H2_2H2_C2H6.class );
+        add( Equation_C_O2_CO2.class );
+        add( Equation_2C_O2_2CO.class );
+        add( Equation_2CO_O2_2CO2.class );
+        add( Equation_C_CO2_2CO.class );
+        add( Equation_C_2S_CS2.class );
+        add( Equation_N2_3H2_2NH3.class );
+        add( Equation_N2_O2_2NO.class );
+        add( Equation_2NO_O2_2NO2.class );
+        add( Equation_2N2_O2_2N2O.class );
+        add( Equation_P4_6H2_4PH3.class );
+        add( Equation_P4_6F2_4PF3.class );
+        add( Equation_P4_6Cl2_4PCl3.class );
+        add( Equation_P4_10Cl2_4PCl5.class );
+        add( Equation_PCl3_Cl2_PCl5.class );
+        add( Equation_2SO2_O2_2SO3.class );
+    }};
+
+    // Level 2
+    private static final ArrayList<Class <? extends Equation>> LEVEL2_LIST = new ArrayList<Class<? extends Equation>>() {{
+        add( MethaneEquation.class );
+        add( Equation_2C_2H2O_CH4_CO2.class );
+        add( Equation_CH4_H2O_3H2_CO.class );
+        add( Equation_C2H4_3O2_2CO2_2H2O.class );
+        add( Equation_C2H6_Cl2_C2H5Cl_HCl.class );
+        add( Equation_CH4_4S_CS2_2H2S.class );
+        add( Equation_CS2_3O2_CO2_2SO2.class );
+        add( Equation_SO2_2H2_S_2H2O.class );
+        add( Equation_SO2_3H2_H2S_2H2O.class );
+        add( Equation_2F2_H2O_OF2_2HF.class );
+        add( Equation_OF2_H2O_O2_2HF.class );
+    }};
+
+    // Level 3
+    private static final ArrayList<Class <? extends Equation>>LEVEL3_LIST = new ArrayList<Class<? extends Equation>>() {{
+        add( Equation_2C2H6_7O2_4CO2_6H2O.class );
+        add( Equation_2C2H2_5O2_4CO2_2H2O.class );
+        add( Equation_C2H5OH_3O2_2CO2_3H2O.class );
+        add( Equation_4NH3_3O2_2N2_6H2O.class );
+        add( Equation_4NH3_5O2_4NO_6H2O.class );
+        add( Equation_4NH3_7O2_4NO2_6H2O.class );
+        add( Equation_4NH3_6NO_5N2_6H2O.class );
+    }};
+
+    // list of lists, so we can use level as an index to the proper list
+    private static ArrayList< ArrayList<Class <? extends Equation>>> LEVEL_LISTS = new ArrayList<ArrayList<Class<? extends Equation>>>() {{
+        add( LEVEL1_LIST );
+        add( LEVEL2_LIST );
+        add( LEVEL3_LIST );
+    }};
+
+    /**
+     * Default constructor.
+     */
+    public GameProblemsFactory() {}
+
+    /**
+     * Creates a problem set.
+     * The set will contain no duplicates if numberOfProblems <= the number of equations for the level.
+     *
+     * @param numberOfProblems
+     * @param level 1-N
+     */
+    public Equation[] createProblemSet( int numberOfProblems, int level ) {
+
+        if ( level < 1 || level > LEVEL_LISTS.size() ) {
+            throw new IllegalArgumentException( "unsupported level: " + level );
+        }
+
+        ArrayList<Class<? extends Equation>> equationClasses = new ArrayList<Class<? extends Equation>>();
+        for ( int i = 0; i < numberOfProblems; i++ ) {
+            equationClasses.add( getRandomEquationClass( level, equationClasses ) );
+        }
+        assert( equationClasses.size() == numberOfProblems );
+
+        Equation[] equations = new Equation[equationClasses.size()];
+        for ( int i = 0; i < equations.length; i++ ) {
+            equations[i] = instantiateEquation( equationClasses.get( i ) );
+        }
+        return equations;
+    }
+
+    /*
+     * Creates a random equation class for a specified level.
+     * Will not select a class that is already in the problem set, unless the problem set
+     * already contains all possible problems.
+     */
+    private Class<? extends Equation> getRandomEquationClass( int level, ArrayList<Class<? extends Equation>> equationClasses ) {
+
+        // Select a random equation class from the array for the specified level.
+        int equationIndex = getRandomEquationIndex( level );
+        Class<? extends Equation> equationClass = getEquationClass( level, equationIndex );
+
+        // If this is a duplicate, find the next one that's not in the problem set.
+        final int originalIndex = equationIndex;
+        while ( equationClasses.contains( equationClass ) ) {
+            equationIndex = getNextEquationIndex( level, equationIndex );
+            equationClass = getEquationClass( level, equationIndex );
+            if ( equationIndex == originalIndex ) {
+                // we're back where we started, bail
+                break;
+            }
+        }
+
+        return equationClass;
+    }
+
+    /*
+     * Gets an Equation class for a specified level and index.
+     */
+    private Class<? extends Equation> getEquationClass( int level, int reactionIndex ) {
+        return getEquationList( level ).get( reactionIndex );
+    }
+
+    /*
+     * Gets the number of equations for a specified level.
+     */
+    private int getNumberOfEquations( int level ) {
+        return getEquationList( level ).size();
+    }
+
+    /*
+     * Gets a random equation index for a specified level.
+     */
+    private int getRandomEquationIndex( int level ) {
+        return (int) ( Math.random() * getNumberOfEquations( level ) );
+    }
+
+    /*
+     * Gets the next equation index for a specified level and current equation index.
+     */
+    private int getNextEquationIndex( int level, int currentIndex ) {
+        int nextIndex = currentIndex + 1;
+        if ( nextIndex > getNumberOfEquations( level ) - 1 ) {
+            nextIndex = 0;
+        }
+        return nextIndex;
+    }
+
+    /*
+     * Gets the list of equations for a level.
+     * Levels are numbered from 1-N, as in the model.
+     */
+    private ArrayList<Class <? extends Equation>> getEquationList( int level ) {
+        return LEVEL_LISTS.get( level - 1 );
+    }
+
+    /*
+     * Uses reflection to instantiate an Equation by class.
+     */
+    private static Equation instantiateEquation( Class<? extends Equation> c ) {
+        Equation equation = null;
+        try {
+            equation = c.newInstance();
+        }
+        catch ( InstantiationException e ) {
+            e.printStackTrace();
+        }
+        catch ( IllegalAccessException e ) {
+            e.printStackTrace();
+        }
+        return equation;
+    }
+
+    // test
+    public static void main( String[] args ) {
+        GameProblemsFactory factory = new GameProblemsFactory();
+        for ( int level = 1; level < 4; level++ ) {
+            System.out.println( "LEVEL " + level );
+            Equation[] equations = factory.createProblemSet( 5, level );
+            for ( Equation equation : equations ) {
+                System.out.println( equation.getName() );
+            }
+        }
+    }
+}
