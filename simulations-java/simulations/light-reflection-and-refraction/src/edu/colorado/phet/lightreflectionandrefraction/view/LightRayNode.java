f0c387ac5fd8593c9d0403d403ed9de05391e00b
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-02-18 20:57:37 +0000

    Added bresenham tests

--- simulations-java/simulations/light-reflection-and-refraction/src/edu/colorado/phet/lightreflectionandrefraction/view/LightRayNode.java
+++ simulations-java/simulations/light-reflection-and-refraction/src/edu/colorado/phet/lightreflectionandrefraction/view/LightRayNode.java
@@ -3,6 +3,7 @@ package edu.colorado.phet.lightreflectionandrefraction.view;
 
 import java.awt.*;
 import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
 
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
@@ -17,15 +18,24 @@ public class LightRayNode extends PNode {
     public final PhetPPath ppath;
     public Shape shape;
     private Shape ss;
+    public Line2D.Double myline;
+    public Point2D mystart;
+    public Point2D myend;
+    private final LightRay lightRay;
 
     public LightRayNode( final ModelViewTransform transform, final LightRay lightRay ) {
+        this.lightRay = lightRay;
         float powerFraction = (float) lightRay.getPowerFraction();
         Color color = lightRay.getColor();
         ppath = new PhetPPath( new BasicStroke( 4 ), new Color( color.getRed() / 255f, color.getGreen() / 255f, color.getBlue() / 255f, (float) Math.sqrt( powerFraction ) ) ) {{
             lightRay.addObserver( new SimpleObserver() {
-
                 public void update() {
-                    shape = transform.modelToView( new Line2D.Double( lightRay.tip.getValue().toPoint2D(), lightRay.tail.getValue().toPoint2D() ) );
+                    final Point2D.Double start = lightRay.tip.getValue().toPoint2D();
+                    final Point2D.Double finish = lightRay.tail.getValue().toPoint2D();
+                    mystart = transform.modelToView( start );
+                    myend = transform.modelToView( finish );
+                    myline = new Line2D.Double( start, finish );
+                    shape = transform.modelToView( myline );
                     ss = getStroke().createStrokedShape( shape );
                     setPathTo( shape );
                 }
@@ -38,5 +48,13 @@ public class LightRayNode extends PNode {
 
     public boolean shapeContains( int x, int y ) {
         return ss.contains( x, y );
+    }
+
+    public Line2D.Double getLine() {
+        return new Line2D.Double( mystart, myend );
+    }
+
+    public Color getColor() {
+        return lightRay.getColor();
     }
 }
