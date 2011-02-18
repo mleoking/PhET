3bf04a515f5df1f089973f0f058f2240ee946d20
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-02-18 22:30:43 +0000

    removed failed attempts at white light, and factored out bresenham implementation to RayLayer

--- simulations-java/simulations/light-reflection-and-refraction/src/edu/colorado/phet/lightreflectionandrefraction/view/LightReflectionAndRefractionCanvas.java
+++ simulations-java/simulations/light-reflection-and-refraction/src/edu/colorado/phet/lightreflectionandrefraction/view/LightReflectionAndRefractionCanvas.java
@@ -2,11 +2,7 @@
 package edu.colorado.phet.lightreflectionandrefraction.view;
 
 import java.awt.*;
-import java.awt.geom.AffineTransform;
 import java.awt.geom.Point2D;
-import java.awt.geom.Rectangle2D;
-import java.awt.image.*;
-import java.util.HashMap;
 
 import edu.colorado.phet.common.phetcommon.model.BooleanProperty;
 import edu.colorado.phet.common.phetcommon.model.Property;
@@ -22,7 +18,6 @@ import edu.colorado.phet.lightreflectionandrefraction.model.LRRModel;
 import edu.colorado.phet.lightreflectionandrefraction.model.LightRay;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.util.PDimension;
-import edu.umd.cs.piccolo.util.PPaintContext;
 
 /**
  * @author Sam Reid
@@ -37,142 +32,10 @@ public class LightReflectionAndRefractionCanvas<T extends LRRModel> extends Phet
     protected final T model;
     protected final ModelViewTransform transform;
     protected final PDimension stageSize;
-    BufferedImage bufferedImage = new BufferedImage( 1000, 1000, BufferedImage.TYPE_4BYTE_ABGR_PRE );
-    protected final PNode rayLayer = new PNode() {
-        @Override
-        public void fullPaint( PPaintContext paintContext ) {
-            super.fullPaint( paintContext );
-
-//            Composite c = paintContext.getGraphics().getComposite();
-//            paintContext.getGraphics().setComposite( new Composite() {
-//                public CompositeContext createContext( ColorModel srcColorModel, ColorModel dstColorModel, RenderingHints hints ) {
-//                    return new CompositeContext() {
-//                        public void dispose() {
-//                        }
-//
-//                        //See http://www.java-gaming.org/index.php?topic=22467.0
-//                        public void compose( Raster src, Raster dstIn, WritableRaster dstOut ) {
-//                            int chan1 = src.getNumBands();
-//                            int chan2 = dstIn.getNumBands();
-//
-//                            int minCh = Math.min( chan1, chan2 );
-//
-//                            int[] pxSrc = null;
-//                            int[] pxDst = null;
-//
-////                            System.out.println( "dstIn.getWidth() = " + dstIn.getWidth() + ", h = " + dstIn.getHeight() );
-//                            //This bit is horribly inefficient,
-//                            //getting individual pixels rather than all at once.
-//                            for ( int x = 0; x < dstIn.getWidth(); x++ ) {
-//                                for ( int y = 0; y < dstIn.getHeight(); y++ ) {
-//
-//                                    pxSrc = src.getPixel( x, y, pxSrc );
-//                                    pxDst = dstIn.getPixel( x, y, pxDst );
-//
-//                                    for ( int i = 0; i < 3 && i < minCh; i++ ) {
-//                                        pxDst[i] = Math.min( 255, pxSrc[i] + pxDst[i] );
-//                                    }
-//                                    dstOut.setPixel( x, y, pxDst );
-//                                }
-//                            }
-//                        }
-//                    };
-//                }
-//            } );
-//            super.fullPaint( paintContext );
-//            paintContext.getGraphics().setComposite( c );
-        }
-    };
-
-//    BufferedImage tmpImage = new BufferedImage( 1000, 1000, BufferedImage.TYPE_4BYTE_ABGR_PRE );
-
-    @Override
-    public void paintComponent( Graphics canvasGraphics1D ) {
-        super.paintComponent( canvasGraphics1D );
-        Graphics2D canvasGraphics = (Graphics2D) canvasGraphics1D;
-
-        final Graphics2D mainBufferGraphics = bufferedImage.createGraphics();
-        mainBufferGraphics.setBackground( new Color( 0, 0, 0, 0 ) );
-        mainBufferGraphics.clearRect( 0, 0, 1000, 1000 );
-        mainBufferGraphics.setPaint( Color.blue );
-        final HashMap<Point, float[]> map = new HashMap<Point, float[]>();
-        for ( int i = 0; i < rayLayer.getChildrenCount(); i++ ) {
-            final LightRayNode child = (LightRayNode) rayLayer.getChild( i );
-            final TestBresenham testBresenham = new TestBresenham() {
-                public void setPixel( int x0, int y0 ) {
-                    Color color = child.getColor();
-                    final Point point = new Point( x0, y0 );
-                    if ( map.containsKey( point ) ) {
-                        float[] current = map.get( point );
-                        float[] newOne = color.getComponents( null );
-                        for ( int a = 0; a <= 3; a++ ) {
-                            current[a] = Math.min( 1, current[a] + newOne[a] );
-                        }
-                    }
-                    else {
-                        map.put( point, color.getComponents( null ) );
-                    }
-                }
-
-                @Override
-                public boolean isOutOfBounds( int x0, int y0 ) {
-                    return x0 < 0 || y0 < 0 || x0 > bufferedImage.getWidth() || y0 > bufferedImage.getHeight();
-                }
-            };
-            final int x1 = (int) child.getLine().x1;
-            final int y1 = (int) child.getLine().y1;
-            final int x2 = (int) child.getLine().x2;
-            final int y2 = (int) child.getLine().y2;
-            if ( testBresenham.isOutOfBounds( x1, y1 ) ) {
-                testBresenham.draw( x2, y2, x1, y1 );
-            }
-            else {
-                testBresenham.draw( x1, y1, x2, y2 );
-            }
-        }
-        for ( Point point : map.keySet() ) {
-            final float[] doubles = map.get( point );
-            mainBufferGraphics.setPaint( new Color( doubles[0], doubles[1], doubles[2], doubles[3] ) );
-            mainBufferGraphics.fillRect( point.x, point.y, 1, 1 );
-        }
-
-//        for ( int i = 0; i < rayLayer.getChildrenCount(); i++ ) {
-//            PNode child = rayLayer.getChild( i );
-//            final Graphics2D tmpGraphics = tmpImage.createGraphics();
-//            tmpGraphics.setBackground( new Color( 0, 0, 0, 0 ) );
-//            tmpGraphics.clearRect( 0, 0, 1000, 1000 );
-//            child.fullPaint( new PPaintContext( tmpGraphics ) );
-//
-//            BufferedImageOp imageOp = new AdditiveOp( bufferedImage );
-//            bufferedImage = imageOp.filter( tmpImage, bufferedImage );
-//            tmpGraphics.dispose();
-////            images.add(im);
-//        }
-//        mainBufferGraphics.setPaint( Color.red );
-//        float[][] r = new float[bufferedImage.getWidth()][bufferedImage.getHeight()];
-//        float[][] g = new float[bufferedImage.getWidth()][bufferedImage.getHeight()];
-//        float[][] b = new float[bufferedImage.getWidth()][bufferedImage.getHeight()];
-//        float[][] a = new float[bufferedImage.getWidth()][bufferedImage.getHeight()];
-//        for ( int i = 0; i < bufferedImage.getWidth(); i++ ) {
-//            for ( int k = 0; k < bufferedImage.getHeight(); k++ ) {
-//                Color color = new Color( r[i][k], g[i][k], b[i][k], a[i][k] );
-//                mainBufferGraphics.setPaint( color );
-//                mainBufferGraphics.fillRect( i, k, 1, 1 );
-//            }
-//        }
-//        System.out.println( "rayLayer.getChildrenCount() = " + rayLayer.getChildrenCount() );
-        mainBufferGraphics.dispose();
-
-        canvasGraphics.drawRenderedImage( bufferedImage, new AffineTransform() );
-    }
+    protected final PNode rayLayer = new PNode();
 
     public LightReflectionAndRefractionCanvas( final T model, final Function1<Double, Double> clampDragAngle, final Function1<Double, Boolean> clockwiseArrowNotAtMax, final Function1<Double, Boolean> ccwArrowNotAtMax ) {
         this.model = model;
-        model.addRayAddedListener( new VoidFunction1<LightRay>() {
-            public void apply( LightRay lightRay ) {
-                repaint();//redraw the rays with bresenham
-            }
-        } );
         // Root of our scene graph
         rootNode = new PNode();
         addWorldChild( rootNode );
@@ -242,8 +105,17 @@ public class LightReflectionAndRefractionCanvas<T extends LRRModel> extends Phet
                 }
             } );
         }} );
-//        addChild( rayLayer );
-
+        addChild( rayLayer );
+//        final RayLayer node = new RayLayer( rayLayer );
+//        addChild( node );
+//
+//        //TODO: fix this workaround
+//        Timer timer = new Timer( 1000,new ActionListener() {
+//            public void actionPerformed( ActionEvent e ) {
+//                node.updateImage();
+//            }
+//        } );
+//        timer.start();
         //Debug for showing stage
 //        addChild( new PhetPPath( new Rectangle2D.Double( 0, 0, STAGE_SIZE.getWidth(), STAGE_SIZE.getHeight() ), new BasicStroke( 2 ), Color.red ) );
     }
@@ -254,58 +126,5 @@ public class LightReflectionAndRefractionCanvas<T extends LRRModel> extends Phet
 
     public void removeChild( PNode node ) {
         rootNode.removeChild( node );
-    }
-
-    private class AdditiveOp implements BufferedImageOp {
-        private BufferedImage other;
-
-        public AdditiveOp( BufferedImage other ) {
-            this.other = other;
-        }
-
-        public BufferedImage filter( BufferedImage src, BufferedImage dest ) {
-            Raster a = src.getData();
-            Raster b = other.getData();
-            int[] aPixels = a.getPixels( 0, 0, a.getWidth(), a.getHeight(), (int[]) null );
-            int[] bPixels = b.getPixels( 0, 0, a.getWidth(), a.getHeight(), (int[]) null );
-            WritableRaster c = dest.getRaster();
-            int[] cPixels = new int[aPixels.length];
-            for ( int i = 0; i < aPixels.length; i++ ) {
-                cPixels[i] = aPixels[i] + bPixels[i];
-            }
-            c.setPixels( 0, 0, c.getWidth(), c.getHeight(), cPixels );
-
-//            for ( int i = 0; i < src.getWidth(); i++ ) {
-//                for ( int k = 0; k < src.getHeight(); k++ ) {
-//                    int srcRGB = src.getRGB( i, k );
-//                    int otherRGB = other.getRGB( i, k );
-//                    Color x = new Color( srcRGB );
-//                    Color y = new Color( otherRGB );
-//                    Color z = new Color( Math.min( 255, x.getRed() + y.getRed() ), Math.min( 255, x.getGreen() + y.getGreen() ), Math.min( 255, x.getBlue() + y.getBlue() ), Math.min( 255, x.getAlpha() + y.getAlpha() ) );
-//                    dest.setRGB( i, k, z.getRGB() );
-//                }
-//            }
-            return dest;
-        }
-
-        public Rectangle2D getBounds2D( BufferedImage src ) {
-            return new Rectangle2D.Double( 0, 0, src.getWidth(), src.getHeight() );
-        }
-
-        public BufferedImage createCompatibleDestImage( BufferedImage src, ColorModel destCM ) {
-            return new AffineTransformOp( new AffineTransform(), AffineTransformOp.TYPE_NEAREST_NEIGHBOR ).createCompatibleDestImage( src, destCM );
-        }
-
-        public Point2D getPoint2D( Point2D srcPt, Point2D dstPt ) {
-            if ( dstPt == null ) {
-                dstPt = new Point2D.Double();
-            }
-            dstPt.setLocation( srcPt.getX(), srcPt.getY() );
-            return dstPt;
-        }
-
-        public RenderingHints getRenderingHints() {
-            return null;
-        }
     }
 }
