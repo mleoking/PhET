324bebf37215e961e1169b6bcc78511ccf33892b
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-02-18 01:31:56 +0000

    #2744, delete empty comment headers

--- trunk/simulations-java/common/photon-absorption/src/edu/colorado/phet/common/photonabsorption/model/atoms/Atom.java
+++ trunk/simulations-java/common/photon-absorption/src/edu/colorado/phet/common/photonabsorption/model/atoms/Atom.java
@@ -12,14 +12,10 @@ import edu.colorado.phet.common.phetcommon.util.SimpleObservable;
  * Class that represents an atom in the model.  This is used in the
  * microscopic view of photon abosorption.  This is an abstract class, and
  * it is expected that it be extended by specific atoms.
- * 
+ *
  * @author John Blanco
  */
 public abstract class Atom extends SimpleObservable {
-    
-    //----------------------------------------------------------------------------
-    // Class Data
-    //----------------------------------------------------------------------------
 
     //----------------------------------------------------------------------------
     // Instance Data
@@ -33,7 +29,7 @@ public abstract class Atom extends SimpleObservable {
     //----------------------------------------------------------------------------
     // Constructor(s)
     //----------------------------------------------------------------------------
-    
+
     public Atom( Color representationColor, double radius, double mass, Point2D position ) {
         super();
         this.representationColor = representationColor;
@@ -41,7 +37,7 @@ public abstract class Atom extends SimpleObservable {
         this.mass = mass;
         this.position = position;
     }
-    
+
     public Atom( Color representationColor, double radius, double mass ) {
         this (representationColor, radius, mass, new Point2D.Double(0, 0));
     }
@@ -49,43 +45,38 @@ public abstract class Atom extends SimpleObservable {
     //------------------------------------------------------------------------
     // Methods
     //------------------------------------------------------------------------
-    
+
     public Point2D getPositionRef() {
         return position;
     }
-    
+
     public void setPosition( Point2D position ) {
         if (this.position != position){
             this.position.setLocation( position );
             notifyObservers();
         }
     }
-    
+
     public void setPosition( double x, double y ) {
         if (this.position.getX() != x || this.position.getY() != y){
             this.position.setLocation( x, y );
             notifyObservers();
         }
     }
-    
+
     public Color getRepresentationColor() {
         return representationColor;
     }
-    
+
     public double getRadius() {
         return radius;
     }
-    
+
     public double getMass() {
         return mass;
     }
-    
+
     public Rectangle2D getBoundingRect (){
         return new Rectangle2D.Double(position.getX() - radius, position.getY() - radius, radius * 2, radius * 2);
     }
-    
-    //------------------------------------------------------------------------
-    // Inner Classes and Interfaces
-    //------------------------------------------------------------------------
-    
 }
