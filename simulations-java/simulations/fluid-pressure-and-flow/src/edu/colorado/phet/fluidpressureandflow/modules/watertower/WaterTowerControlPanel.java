257bb7c1ec368d9ba021cc428278984349830c02
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-02-23 19:13:19 +0000

    Cleaned up WaterTowerControlPanel

--- simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/modules/watertower/WaterTowerControlPanel.java
+++ simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/modules/watertower/WaterTowerControlPanel.java
@@ -6,11 +6,12 @@ import java.awt.*;
 import javax.swing.*;
 
 import edu.colorado.phet.common.phetcommon.view.VerticalLayoutPanel;
-import edu.colorado.phet.fluidpressureandflow.FPAFStrings;
 import edu.colorado.phet.fluidpressureandflow.FluidPressureAndFlowModule;
 import edu.colorado.phet.fluidpressureandflow.model.FluidPressureAndFlowModel;
 import edu.colorado.phet.fluidpressureandflow.view.CheckBox;
 import edu.colorado.phet.fluidpressureandflow.view.UnitsControlPanel;
+
+import static edu.colorado.phet.fluidpressureandflow.FPAFStrings.*;
 
 /**
  * @author Sam Reid
@@ -20,20 +21,11 @@ public class WaterTowerControlPanel<T extends FluidPressureAndFlowModel> extends
     public WaterTowerControlPanel( final FluidPressureAndFlowModule<T> module ) {
         super();
 
-        addControlFullWidth( new CheckBox( FPAFStrings.RULER, module.rulerVisible ) );
-        addControlFullWidth( new CheckBox( FPAFStrings.MEASURING_TAPE, module.measuringTapeVisible ) );
-        addControlFullWidth( new UnitsControlPanel<T>( module ) );
-        addControlFullWidth( (JComponent) Box.createRigidArea( new Dimension( 40, 40 ) ) );
-        addControlFullWidth( new JSeparator() );
-        addControlFullWidth( new CheckBox( FPAFStrings.HOSE, module.hoseVisible ) );
+        add( new CheckBox( RULER, module.rulerVisible ) );
+        add( new CheckBox( MEASURING_TAPE, module.measuringTapeVisible ) );
+        add( new UnitsControlPanel<T>( module ) );
+        add( Box.createRigidArea( new Dimension( 5, 5 ) ) );//separate the "hose" control a bit from the other controls so it is easier to parse visually
+        add( new JSeparator() );
+        add( new CheckBox( HOSE, module.hoseVisible ) );
     }
-
-    private void addControlFullWidth( JComponent component ) {
-        add( component );
-    }
-
-    private void addControl( JComponent panel ) {
-        add( panel );
-    }
-
 }
