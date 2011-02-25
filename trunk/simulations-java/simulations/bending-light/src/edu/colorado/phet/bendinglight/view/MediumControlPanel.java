2eb035b2d8c7acc7dc749c1cee356f7a045b05d9
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-02-25 20:28:43 +0000

    Improved design for index of refraction control

--- trunk/simulations-java/simulations/bending-light/src/edu/colorado/phet/bendinglight/view/MediumControlPanel.java
+++ trunk/simulations-java/simulations/bending-light/src/edu/colorado/phet/bendinglight/view/MediumControlPanel.java
@@ -4,10 +4,17 @@ package edu.colorado.phet.bendinglight.view;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
+import java.util.Hashtable;
+
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import edu.colorado.phet.bendinglight.model.BendingLightModel;
 import edu.colorado.phet.bendinglight.model.Medium;
 import edu.colorado.phet.bendinglight.model.MediumState;
+import edu.colorado.phet.common.phetcommon.math.Function;
 import edu.colorado.phet.common.phetcommon.model.Property;
 import edu.colorado.phet.common.phetcommon.util.Function1;
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
@@ -30,73 +37,135 @@ public class MediumControlPanel extends PNode {
             return true;
         }
     };
+    private final Property<Medium> medium;
+    private final Property<Function1<Double, Color>> colorMappingFunction;
+    private static final int MIN = 1;
+    private static final double MAX = 1.6;
 
     public MediumControlPanel( final PhetPCanvas phetPCanvas, final Property<Medium> medium, final Property<Function1<Double, Color>> colorMappingFunction, final String name, boolean textFieldVisible ) {
+        this.medium = medium;
+        this.colorMappingFunction = colorMappingFunction;
         final MediumState initialMediumState = medium.getValue().getMediumState();
-        final PText materialLabel = new PText( name ) {{
-            setFont( labelFont );
+        final PNode topLabel = new PNode() {{
+            final PText materialLabel = new PText( name ) {{
+                setFont( new PhetFont( labelFont.getSize(), true ) );
+            }};
+            final Object[] mediumStates = new Object[] {
+                    BendingLightModel.AIR,
+                    BendingLightModel.WATER,
+                    BendingLightModel.GLASS,
+                    BendingLightModel.MYSTERY_A,
+                    BendingLightModel.MYSTERY_B,
+                    CUSTOM,
+            };
+            final PComboBox comboBox = new PComboBox( mediumStates ) {
+                {
+                    addActionListener( new ActionListener() {
+                        public void actionPerformed( ActionEvent e ) {
+                            MediumState selected = (MediumState) getSelectedItem();
+                            if ( !selected.isCustom() ) {
+                                setMediumState( selected, medium, colorMappingFunction );
+                            }
+                        }
+                    } );
+                    updateComboBox();
+                    medium.addObserver( new SimpleObserver() {
+                        public void update() {
+                            updateComboBox();
+                        }
+                    } );
+                    setFont( labelFont );
+                    setMediumState( initialMediumState, medium, colorMappingFunction );
+                }
+
+                private void updateComboBox() {
+                    int selected = -1;
+                    for ( int i = 0; i < mediumStates.length; i++ ) {
+                        MediumState mediumState = (MediumState) mediumStates[i];
+                        if ( mediumState.index == medium.getValue().getIndexOfRefraction() ) {
+                            selected = i;
+                        }
+                    }
+                    if ( selected != -1 ) {
+                        setSelectedIndex( selected );
+                    }
+                    else {
+                        setSelectedItem( CUSTOM );
+                    }
+                }
+            };
+            final PSwing comboBoxPSwing = new PSwing( comboBox ) {{
+                comboBox.setEnvironment( this, phetPCanvas );
+                setOffset( materialLabel.getFullBounds().getMaxX() + 10, materialLabel.getFullBounds().getCenterY() - getFullBounds().getHeight() / 2 + 1 );
+            }};
+            addChild( materialLabel );
+            addChild( comboBoxPSwing );
         }};
-        addChild( materialLabel );
-        final Object[] mediumStates = new Object[] {
-                BendingLightModel.AIR,
-                BendingLightModel.WATER,
-                BendingLightModel.GLASS,
-                BendingLightModel.MYSTERY_A,
-                BendingLightModel.MYSTERY_B,
-                CUSTOM,
-        };
-        final PComboBox comboBox = new PComboBox( mediumStates ) {
-            {
-                addActionListener( new ActionListener() {
-                    public void actionPerformed( ActionEvent e ) {
-                        MediumState selected = (MediumState) getSelectedItem();
-                        if ( !selected.isCustom() ) {
-                            setMediumState( selected, medium, colorMappingFunction );
+        addChild( topLabel );
+
+        //Many efforts were made to make this control work with LinearValueControl, including writing custom layouts.
+        //However, for unknown reasons, some text was always clipped off, and we decided to proceed by doing the layout in Piccolo, which resolved the problem.
+        final PNode slider = new PNode() {{
+            final PNode topComponent = new PNode() {{
+                final PText label = new PText( "Index of Refraction (n):" ) {{setFont( BendingLightCanvas.labelFont );}};
+                addChild( label );
+                addChild( new PSwing( new JTextField( new DecimalFormat( "0.00" ).format( medium.getValue().getIndexOfRefraction() ), 4 ) {{
+                    setFont( BendingLightCanvas.labelFont );
+                    addActionListener( new ActionListener() {
+                        public void actionPerformed( ActionEvent e ) {
+                            double value = Double.parseDouble( getText() );
+                            if ( value > MIN && value < MAX ) {
+                                setIndexOfRefraction( value );
+                            }
+                        }
+                    } );
+                    medium.addObserver( new SimpleObserver() {
+                        public void update() {
+                            setText( new DecimalFormat( "0.00" ).format( medium.getValue().getIndexOfRefraction() ) );
+                        }
+                    } );
+                }} ) {{
+                    setOffset( label.getFullBounds().getMaxX() + 10, label.getFullBounds().getCenterY() - getFullBounds().getHeight() / 2 );
+                }} );
+            }};
+            addChild( topComponent );
+
+            addChild( new PSwing( new JSlider( 0, 10000 ) {{
+                final Function.LinearFunction mapping = new Function.LinearFunction( getMinimum(), getMaximum(), MIN, MAX );
+                addChangeListener( new ChangeListener() {
+                    public void stateChanged( ChangeEvent e ) {
+                        if ( isFocusOwner() ) {//Only send events if caused by user, otherwise selecting "mystery b" causes buggy behavior
+                            final double indexOfRefraction = mapping.evaluate( getValue() );
+                            setIndexOfRefraction( indexOfRefraction );
                         }
                     }
                 } );
-                updateComboBox();
                 medium.addObserver( new SimpleObserver() {
                     public void update() {
-                        updateComboBox();
+                        setValue( (int) mapping.createInverse().evaluate( medium.getValue().getIndexOfRefraction() ) );
                     }
                 } );
-                setFont( labelFont );
-                setMediumState( initialMediumState, medium, colorMappingFunction );
-            }
-
-            private void updateComboBox() {
-                int selected = -1;
-                for ( int i = 0; i < mediumStates.length; i++ ) {
-                    MediumState mediumState = (MediumState) mediumStates[i];
-                    if ( mediumState.index == medium.getValue().getIndexOfRefraction() ) {
-                        selected = i;
-                    }
-                }
-                if ( selected != -1 ) {
-                    setSelectedIndex( selected );
-                }
-                else {
-                    setSelectedItem( CUSTOM );
-                }
-            }
-        };
-        final PSwing comboBoxPSwing = new PSwing( comboBox ) {{
-            comboBox.setEnvironment( this, phetPCanvas );
-            setOffset( materialLabel.getFullBounds().getMaxX() + 10, materialLabel.getFullBounds().getCenterY() - getFullBounds().getHeight() / 2 + 1 );
+                setPaintTicks( true );
+                setPaintLabels( true );
+                setLabelTable( new Hashtable<Object, Object>() {{
+                    put( (int) mapping.createInverse().evaluate( BendingLightModel.AIR.index ), new TickLabel( "Air" ) );
+                    put( (int) mapping.createInverse().evaluate( BendingLightModel.WATER.index ), new TickLabel( "Water" ) );
+                    put( (int) mapping.createInverse().evaluate( BendingLightModel.GLASS.index ), new TickLabel( "Glass" ) );
+//            put( LRRModel.N_DIAMOND, new TickLabel( "Diamond" ) );//commented out while we determine how to handle overlapping labels
+                }} );
+                setPreferredSize( new Dimension( (int) topComponent.getFullBounds().getWidth(), getPreferredSize().height ) );
+            }} ) {{
+                setOffset( 0, topComponent.getFullBounds().getMaxY() );
+            }} );
+            setOffset( 0, topLabel.getFullBounds().getMaxY() + 10 );
         }};
-        addChild( comboBoxPSwing );
 
-        final PSwing slider = new PSwing( new IndexOfRefractionSlider( medium, colorMappingFunction, "", textFieldVisible ) {{
-            getTextField().setFont( labelFont );
-        }} ) {{
-            medium.addObserver( new SimpleObserver() {
-                public void update() {
-                    setVisible( !medium.getValue().isMystery() );
-                }
-            } );
-            setOffset( 0, materialLabel.getFullBounds().getMaxY() );
-        }};
+        medium.addObserver( new SimpleObserver() {
+            public void update() {
+                slider.setVisible( !medium.getValue().isMystery() );
+            }
+        } );
+
         addChild( slider );
 
         final PText unknown = new PText( "n=?" ) {{
@@ -109,12 +178,11 @@ public class MediumControlPanel extends PNode {
             } );
         }};
         addChild( unknown );
+        topLabel.setOffset( getFullBounds().getCenterX() - topLabel.getFullBounds().getWidth() / 2, 0 );
+    }
 
-        final PText indexOfRefractionLabel = new PText( "Index of Refraction (n)" ) {{
-            setFont( new PhetFont( 16, true ) );
-            setOffset( 0, slider.getFullBounds().getMaxY() );
-        }};
-        addChild( indexOfRefractionLabel );
+    private void setIndexOfRefraction( double indexOfRefraction ) {
+        medium.setValue( new Medium( medium.getValue().getShape(), new MediumState( "Custom", indexOfRefraction ), colorMappingFunction.getValue().apply( indexOfRefraction ) ) );
     }
 
     private void setMediumState( MediumState mediumState, Property<Medium> medium, Property<Function1<Double, Color>> colorMappingFunction ) {
