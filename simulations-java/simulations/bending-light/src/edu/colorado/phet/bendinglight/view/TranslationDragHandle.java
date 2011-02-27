b011c896e39baea0fdc2d19c3ceb3a66cfdb61e4
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-02-27 05:05:41 +0000

    Added translation drag handles to the laser

---
+++ simulations-java/simulations/bending-light/src/edu/colorado/phet/bendinglight/view/TranslationDragHandle.java
@@ -0,0 +1,51 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.bendinglight.view;
+
+import java.awt.*;
+import java.awt.geom.Point2D;
+import java.awt.image.BufferedImage;
+
+import edu.colorado.phet.bendinglight.BendingLightApplication;
+import edu.colorado.phet.bendinglight.model.Laser;
+import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
+import edu.colorado.phet.common.phetcommon.model.BooleanProperty;
+import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
+import edu.colorado.phet.common.phetcommon.view.graphics.Arrow;
+import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
+import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
+import edu.umd.cs.piccolo.PNode;
+
+import static edu.colorado.phet.common.phetcommon.view.util.BufferedImageUtils.flipX;
+import static edu.colorado.phet.common.phetcommon.view.util.BufferedImageUtils.flipY;
+
+/**
+ * @author Sam Reid
+ */
+public class TranslationDragHandle extends PNode {
+
+    public TranslationDragHandle( final ModelViewTransform transform, final Laser laser, final double dx, final double dy, final BooleanProperty showDragHandles ) {
+        showDragHandles.addObserver( new SimpleObserver() {
+            public void update() {
+                setVisible( showDragHandles.getValue() );
+            }
+        } );
+        final BufferedImage image = flipY( flipX( BendingLightApplication.RESOURCES.getImage( "laser.png" ) ) );
+
+        final SimpleObserver update = new SimpleObserver() {
+            public void update() {
+                removeAllChildren();
+                final PNode counterClockwiseDragArrow = new PNode() {{
+                    Point2D pt = transform.modelToView( laser.getEmissionPoint().toPoint2D() );
+                    ImmutableVector2D vec = ImmutableVector2D.parseAngleAndMagnitude( image.getWidth() / 2, -laser.angle.getValue() );
+                    addChild( new PhetPPath( new Arrow( new Point2D.Double( pt.getX() + vec.getX(), pt.getY() + vec.getY() ), new ImmutableVector2D( dx, dy ), 10, 10, 5, 2, true ).getShape(), Color.green, new BasicStroke( 1 ), Color.black ) );
+                    setPickable( false );
+                    setChildrenPickable( false );
+                }};
+                addChild( counterClockwiseDragArrow );
+            }
+        };
+        laser.distanceFromOrigin.addObserver( update );
+        laser.angle.addObserver( update );
+    }
+
+}
