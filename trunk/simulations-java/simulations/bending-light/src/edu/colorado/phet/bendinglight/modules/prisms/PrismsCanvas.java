8fb0920e352abd31f4a02799f3f752df5772eab9
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-02-28 23:51:13 +0000

    reformatted

--- trunk/simulations-java/simulations/bending-light/src/edu/colorado/phet/bendinglight/modules/prisms/PrismsCanvas.java
+++ trunk/simulations-java/simulations/bending-light/src/edu/colorado/phet/bendinglight/modules/prisms/PrismsCanvas.java
@@ -19,14 +19,14 @@ public class PrismsCanvas extends BendingLightCanvas<PrismsModel> {
     public PrismsCanvas( final PrismsModel model, BooleanProperty moduleActive, Resettable resetAll ) {
         super( model, moduleActive, new Function1.Identity<Double>(), new Function1.Constant<Double, Boolean>( true ), new Function1.Constant<Double, Boolean>( true ), false,
                resetAll, new Function2<Shape, Shape, Shape>() {
-            public Shape apply( Shape full, Shape front ) {
-                return front;
-            }
-        }, new Function2<Shape, Shape, Shape>() {
-            public Shape apply( Shape full, Shape back ) {
-                return back;
-            }
-        }, "laser_knob.png" );
+                    public Shape apply( Shape full, Shape front ) {
+                        return front;
+                    }
+                }, new Function2<Shape, Shape, Shape>() {
+                    public Shape apply( Shape full, Shape back ) {
+                        return back;
+                    }
+                }, "laser_knob.png" );
         for ( Prism prism : model.getPrisms() ) {
             addChild( new PrismNode( transform, prism, model.prismMedium ) );
         }
