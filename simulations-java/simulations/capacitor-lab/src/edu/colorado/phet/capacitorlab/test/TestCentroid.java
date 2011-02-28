822951d8ff0f1cdc0b758c634aa625ea2fed54a4
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-02-28 16:57:27 +0000

    #2758, remove Piccolo dependencies from TestCentroid

--- simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/test/TestCentroid.java
+++ simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/test/TestCentroid.java
@@ -2,8 +2,10 @@
 
 package edu.colorado.phet.capacitorlab.test;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
-import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.GeneralPath;
 import java.awt.geom.Line2D;
@@ -14,9 +16,6 @@ import javax.swing.WindowConstants;
 
 import edu.colorado.phet.common.phetcommon.math.PolygonUtils;
 import edu.colorado.phet.common.phetcommon.view.util.SwingUtils;
-import edu.umd.cs.piccolo.PCanvas;
-import edu.umd.cs.piccolo.nodes.PPath;
-import edu.umd.cs.piccolox.nodes.PComposite;
 
 /**
  * Tests utilities for finding the centroid (geometric center) of a closed polygon.
@@ -26,21 +25,23 @@ import edu.umd.cs.piccolox.nodes.PComposite;
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
 public class TestCentroid extends JFrame {
-    
-    public TestCentroid() {
-        super( "TestCentroid" );
-        
-        PCanvas canvas = new PCanvas();
-        canvas.setPreferredSize( new Dimension( 800, 600 ) );
-        setContentPane( canvas );
-        
+
+    @Override
+    public void paint( Graphics g ) {
+
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setStroke( new BasicStroke( 2f ) );
+
+        double xOffset = 50;
+        double yOffset = 50;
+
         // points that define a polygon (in this case, a parallelogram)
-        Point2D p0 = new Point2D.Double( 0, 0 );
-        Point2D p1 = new Point2D.Double( 20, 40 );
-        Point2D p2 = new Point2D.Double( 80, 40 );
-        Point2D p3 = new Point2D.Double( 60, 0 );
+        Point2D p0 = new Point2D.Double( xOffset, yOffset );
+        Point2D p1 = new Point2D.Double( xOffset + 80, yOffset + 160 );
+        Point2D p2 = new Point2D.Double( xOffset + 320, yOffset + 160 );
+        Point2D p3 = new Point2D.Double( xOffset + 240, yOffset );
         Point2D[] points = new Point2D[] { p0, p1, p2, p3 };
-        
+
         // draw the polygon
         GeneralPath polygonPath = new GeneralPath();
         polygonPath.moveTo( (float) p0.getX(), (float) p0.getY() );
@@ -48,44 +49,31 @@ public class TestCentroid extends JFrame {
         polygonPath.lineTo( (float) p2.getX(), (float) p2.getY() );
         polygonPath.lineTo( (float) p3.getX(), (float) p3.getY() );
         polygonPath.closePath();
-        PPath polygonNode = new PPath( polygonPath );
-        polygonNode.setStrokePaint( Color.BLACK );
-        
+        g2.setPaint( Color.BLACK );
+        g2.draw( polygonPath );
+
         // draw the diagonals of the parallelogram
         Line2D diagonalPath1 = new Line2D.Double( p0, p2 );
-        PPath diagonalNode1 = new PPath( diagonalPath1 );
-        diagonalNode1.setStrokePaint( Color.RED );
         Line2D diagonalPath2 = new Line2D.Double( p1, p3 );
-        PPath diagonalNode2 = new PPath( diagonalPath2 );
-        diagonalNode2.setStrokePaint( Color.RED );
-        
+        g2.setPaint( Color.RED );
+        g2.draw( diagonalPath1 );
+        g2.draw( diagonalPath2 );
+
         // draw a circle at the centroid (geometric center)
-        double diameter = 4;
+        double diameter = 12;
         Point2D centroid = PolygonUtils.getCentroid( points );
         double x = centroid.getX() - ( diameter / 2 );
         double y = centroid.getY() - ( diameter / 2 );
-        Ellipse2D centroidPath = new Ellipse2D.Double( x, y, diameter, diameter );
-        PPath centroidNode = new PPath( centroidPath );
-        centroidNode.setStrokePaint( Color.GREEN );
-        
-        PComposite parentNode = new PComposite();
-        parentNode.addChild( polygonNode );
-        parentNode.addChild( diagonalNode1 );
-        parentNode.addChild( diagonalNode2 );
-        parentNode.addChild( centroidNode );
-        
-        canvas.getLayer().addChild( parentNode );
-        parentNode.scale( 5 );
-        parentNode.setOffset( 100, 100 );
-        
-        pack();
+        Ellipse2D circlePath = new Ellipse2D.Double( x, y, diameter, diameter );
+        g2.setPaint( Color.GREEN );
+        g2.draw( circlePath );
     }
-    
+
     public static void main( String[] args ) {
         JFrame frame = new TestCentroid();
         frame.setDefaultCloseOperation( WindowConstants.EXIT_ON_CLOSE );
+        frame.setSize( 500, 300 );
         SwingUtils.centerWindowOnScreen( frame );
         frame.setVisible( true );
     }
-
 }
