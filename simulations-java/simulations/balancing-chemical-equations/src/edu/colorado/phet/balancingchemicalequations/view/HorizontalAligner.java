570fe862231999d2cb057a460e7d359b643c2ed2
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-03-01 22:56:34 +0000

    change request: add layout support for reverse reactions (1 reactant, 2 products)

--- simulations-java/simulations/balancing-chemical-equations/src/edu/colorado/phet/balancingchemicalequations/view/HorizontalAligner.java
+++ simulations-java/simulations/balancing-chemical-equations/src/edu/colorado/phet/balancingchemicalequations/view/HorizontalAligner.java
@@ -4,12 +4,14 @@ package edu.colorado.phet.balancingchemicalequations.view;
 
 import java.awt.Dimension;
 
+import javax.swing.SwingConstants;
+
 import edu.colorado.phet.balancingchemicalequations.model.Equation;
 import edu.colorado.phet.balancingchemicalequations.model.EquationTerm;
 
 /**
- * Encapsulates the strategy used horizontally aligning terms in an equation
- * with columns of molecules in the "box" view.
+ * Encapsulates the strategy used to horizontally aligning terms in an equation
+ * with columns of molecules in the "boxes" view.
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
@@ -20,7 +22,7 @@ public class HorizontalAligner {
 
     /**
      * Constructor.
-     * @param boxWidth width of the box
+     * @param boxWidth width of one of the 2 boxes
      * @param boxSeparation horizontal separation between the left and right boxes
      */
     public HorizontalAligner( Dimension boxSize, double boxSeparation ) {
@@ -37,7 +39,7 @@ public class HorizontalAligner {
     }
 
     /**
-     * Gets the x offset of the center between left and right hand sides.
+     * Gets the x offset of the center between left- and right-hand sides.
      * @return
      */
     public double getCenterXOffset() {
@@ -46,34 +48,49 @@ public class HorizontalAligner {
 
     /**
      * Gets the offsets for an equation's reactant terms.
+     * Reactants are on the left-hand side of the equation.
      * @param equation
      * @return
      */
     public double[] getReactantXOffsets( Equation equation ) {
-        return getXOffsets( equation.getReactants(), 0 /* xAdjustment */ );
+        return getXOffsets( equation.getReactants(), SwingConstants.RIGHT, 0 /* left edge of left box */ );
     }
 
     /**
      * Gets the offsets for an equation's product terms.
+     * Products are on the right-hand side of the equation.
      * @param equation
      * @return
      */
     public double[] getProductXOffsets( Equation equation ) {
-        return getXOffsets( equation.getProducts(), boxSize.getWidth() + boxSeparation /* xAdjustment */ );
+        return getXOffsets( equation.getProducts(), SwingConstants.LEFT, boxSize.getWidth() + boxSeparation /* left edge of right box */ );
     }
 
     /*
      * Gets the x offsets for a set of terms.
      * The box is divided up into columns and terms are centered in the columns.
+     * In the general case, N terms will divide the box into N columns.
+     * In the special case of 1 term, the box will be divided into 2 columns,
+     * and the single term will be placed in the column that corresponds to alignment.
+     *
+     * @param terms
+     * @param alignment
+     * @param xAdjustment
      */
-    private double[] getXOffsets( EquationTerm[] terms, double xAdjustment ) {
+    private double[] getXOffsets( EquationTerm[] terms, int alignment, double xAdjustment ) {
+        assert( alignment == SwingConstants.LEFT || alignment == SwingConstants.RIGHT );
         final int numberOfTerms = terms.length;
         final double columnWidth = boxSize.getWidth() / Math.max( 2, numberOfTerms );
         double x = xAdjustment + columnWidth / 2;
         double[] xOffsets = new double[ numberOfTerms ];
-        for ( int i = 0; i < numberOfTerms; i++ ) {
-            xOffsets[i] = x;
-            x += columnWidth;
+        if ( numberOfTerms == 1 && alignment == SwingConstants.RIGHT ) {
+            xOffsets[0] = x + columnWidth;
+        }
+        else {
+            for ( int i = 0; i < numberOfTerms; i++ ) {
+                xOffsets[i] = x;
+                x += columnWidth;
+            }
         }
         return xOffsets;
     }
