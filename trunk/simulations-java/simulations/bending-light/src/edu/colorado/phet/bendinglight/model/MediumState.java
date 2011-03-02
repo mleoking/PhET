df1b070a0106141134f4cfe33be4d38390439b06
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-03-02 23:00:20 +0000

    improved MediumState and formatting

--- trunk/simulations-java/simulations/bending-light/src/edu/colorado/phet/bendinglight/model/MediumState.java
+++ trunk/simulations-java/simulations/bending-light/src/edu/colorado/phet/bendinglight/model/MediumState.java
@@ -8,23 +8,25 @@ public class MediumState {
     public final String name;
     public final double index;
     public final boolean mystery;
+    public final boolean custom;
 
     public MediumState( String name, double index ) {
         this( name, index, false );
     }
 
     public MediumState( String name, double index, boolean mystery ) {
+        this( name, index, mystery, false );
+    }
+
+    public MediumState( String name, double index, boolean mystery, boolean custom ) {
         this.name = name;
         this.index = index;
         this.mystery = mystery;
+        this.custom = custom;
     }
 
     @Override
     public String toString() {
         return name;
-    }
-
-    public boolean isCustom() {
-        return false;
     }
 }
