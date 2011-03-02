7e91139989f1c106ea6f2a654273c1dda9d1e1c1
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-03-02 18:17:40 +0000

    simplify game model and timer by making properties "public static final" and deleting getters

--- simulations-java/simulations/balancing-chemical-equations/src/edu/colorado/phet/balancingchemicalequations/module/game/GameTimer.java
+++ simulations-java/simulations/balancing-chemical-equations/src/edu/colorado/phet/balancingchemicalequations/module/game/GameTimer.java
@@ -6,7 +6,6 @@ import edu.colorado.phet.common.phetcommon.model.Property;
 import edu.colorado.phet.common.phetcommon.model.clock.ClockAdapter;
 import edu.colorado.phet.common.phetcommon.model.clock.ClockEvent;
 import edu.colorado.phet.common.phetcommon.model.clock.IClock;
-import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 
 /**
  * Game timer, keeps track of the elapsed time in the game.
@@ -15,35 +14,21 @@ import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
  */
 /* package private */ class GameTimer {
 
+    // properties directly accessible by clients
+    public final Property<Long> time; // ms
+
     private final IClock clock;
-    private final Property<Long> timeProperty; // ms
     private long startTime; // System time in ms when the game started
 
     public GameTimer( IClock clock ) {
         this.clock = clock;
-        this.timeProperty = new Property<Long>( 0L );
+        this.time = new Property<Long>( 0L );
         clock.addClockListener( new ClockAdapter() {
             @Override
             public void clockTicked( ClockEvent clockEvent ) {
-                timeProperty.setValue( clockEvent.getWallTime() - startTime );
+                time.setValue( clockEvent.getWallTime() - startTime );
             }
         } );
-    }
-
-    /**
-     * Gets the time, in ms.
-     * @return
-     */
-    public long getTime() {
-        return timeProperty.getValue();
-    }
-
-    public void addTimeObserver( SimpleObserver o ) {
-        timeProperty.addObserver( o );
-    }
-
-    public void removeTimeObserver( SimpleObserver o ) {
-        timeProperty.removeObserver( o );
     }
 
     /**
@@ -51,7 +36,7 @@ import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
      * When the timer is started, it restarts from zero.
      */
     public void start() {
-        timeProperty.setValue( 0L );
+        time.setValue( 0L );
         startTime = System.currentTimeMillis(); // don't use clock.getWallTime, it's not valid until the clock ticks
         clock.start();
     }
