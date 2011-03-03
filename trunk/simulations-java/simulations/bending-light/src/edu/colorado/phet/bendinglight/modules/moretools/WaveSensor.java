4278ed6bb22b45b619a8bb408b04f071feb48633
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-03-03 04:57:35 +0000

    improved charting

--- trunk/simulations-java/simulations/bending-light/src/edu/colorado/phet/bendinglight/modules/moretools/WaveSensor.java
+++ trunk/simulations-java/simulations/bending-light/src/edu/colorado/phet/bendinglight/modules/moretools/WaveSensor.java
@@ -3,20 +3,37 @@ package edu.colorado.phet.bendinglight.modules.moretools;
 
 import java.awt.geom.Dimension2D;
 import java.awt.geom.Point2D;
+import java.util.ArrayList;
 
 import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
 import edu.colorado.phet.common.phetcommon.model.Property;
+import edu.colorado.phet.common.phetcommon.util.Option;
 
 /**
  * @author Sam Reid
  */
 public class WaveSensor {
-    public final Probe probe1 = new Probe( -4.173076923076922E-7, 9.180769230769231E-7 );
-    public final Probe probe2 = new Probe( -1.5440384615384618E-6, -1.2936538461538458E-6 );
+    public final Probe probe1 = new Probe( -4.173076923076922E-7, 9.180769230769231E-7 ) {{
+        series.setValue( new ArrayList<Option<ImmutableVector2D>>() {{
+            for ( int i = 0; i < 200; i++ ) {
+                add( new Option.Some<ImmutableVector2D>( new ImmutableVector2D( i / 100.0, Math.sin( i / 100.0 * 10 ) ) ) );
+            }
+        }} );
+    }};
+    public final Probe probe2 = new Probe( -1.5440384615384618E-6, -1.2936538461538458E-6 ) {{
+        series.setValue( new ArrayList<Option<ImmutableVector2D>>() {{
+            for ( int i = 0; i < 200; i++ ) {
+                add( new Option.Some<ImmutableVector2D>( new ImmutableVector2D( i / 100.0, Math.cos( i / 100.0 * 10 ) ) ) );
+            }
+        }} );
+    }};
     public final Property<ImmutableVector2D> bodyPosition = new Property<ImmutableVector2D>( new ImmutableVector2D( 4.882500000000015E-6, -3.1298076923077013E-6 ) );
 
     public WaveSensor() {
         //This code helps to come up with a good set of defaults for the values.
+//        public static String toCode( ImmutableVector2D v ) {
+//        return "new ImmutableVector2D(" + v.getX() + "," + v.getY() + ")";
+//    }
 //        final SimpleObserver simpleObserver = new SimpleObserver() {
 //            public void update() {
 //                System.out.println( "bodyPosition.setValue( " + toCode( bodyPosition.getValue() ) + ");" );
@@ -28,10 +45,6 @@ public class WaveSensor {
 //        probe2.position.addObserver( simpleObserver );
     }
 
-    public static String toCode( ImmutableVector2D v ) {
-        return "new ImmutableVector2D(" + v.getX() + "," + v.getY() + ")";
-    }
-
     public void translateBody( Dimension2D dimension2D ) {
         bodyPosition.setValue( bodyPosition.getValue().plus( dimension2D ) );
     }
@@ -40,8 +53,7 @@ public class WaveSensor {
      * Moves the sensor body and probes until the hot spot (center of one probe) is on the specified position.
      */
     public void translateToHotSpot( Point2D position ) {
-        ImmutableVector2D delta = new ImmutableVector2D( position ).minus( probe1.position.getValue() );
-        translateAll( delta );
+        translateAll( new ImmutableVector2D( position ).minus( probe1.position.getValue() ) );
     }
 
     public void translateAll( ImmutableVector2D delta ) {
@@ -52,7 +64,7 @@ public class WaveSensor {
 
     public static class Probe {
         public final Property<ImmutableVector2D> position;
-        public final Property<Double> value = new Property<Double>( 0.0 );
+        public final Property<ArrayList<Option<ImmutableVector2D>>> series = new Property<ArrayList<Option<ImmutableVector2D>>>( new ArrayList<Option<ImmutableVector2D>>() );
 
         public Probe( double x, double y ) {
             position = new Property<ImmutableVector2D>( new ImmutableVector2D( x, y ) );
