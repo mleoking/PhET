3714426379b66a6835c63fddaeb9dbe634b7f72a
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-03-03 02:00:02 +0000

    Fixed: Velocity sensor shouldn’t read 0.0 c in the toolbox

--- trunk/simulations-java/simulations/bending-light/src/edu/colorado/phet/bendinglight/model/VelocitySensor.java
+++ trunk/simulations-java/simulations/bending-light/src/edu/colorado/phet/bendinglight/model/VelocitySensor.java
@@ -11,7 +11,7 @@ import edu.colorado.phet.common.phetcommon.model.Property;
  */
 public class VelocitySensor {
     public final Property<ImmutableVector2D> position = new Property<ImmutableVector2D>( new ImmutableVector2D( 0, 0 ) );
-    public final Property<Double> value = new Property<Double>( 0.0 );
+    public final Property<Double> value = new Property<Double>( Double.NaN );
 
     public void translate( Dimension2D delta ) {
         position.setValue( position.getValue().getAddedInstance( delta.getWidth(), delta.getHeight() ) );
