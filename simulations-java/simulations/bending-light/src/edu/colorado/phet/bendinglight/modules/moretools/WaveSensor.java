de9e59da16c35da9246930a9dd33f98d9cbbf3ea
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-03-03 01:27:17 +0000

    Fixed layout of wave sensor in toolbox

--- simulations-java/simulations/bending-light/src/edu/colorado/phet/bendinglight/modules/moretools/WaveSensor.java
+++ simulations-java/simulations/bending-light/src/edu/colorado/phet/bendinglight/modules/moretools/WaveSensor.java
@@ -10,17 +10,38 @@ import edu.colorado.phet.common.phetcommon.model.Property;
  * @author Sam Reid
  */
 public class WaveSensor {
-    public final Probe probe1 = new Probe();
-    public final Probe probe2 = new Probe();
-    public final Property<ImmutableVector2D> bodyPosition = new Property<ImmutableVector2D>( new ImmutableVector2D( 0, 0 ) );
+    public final Probe probe1 = new Probe( -4.173076923076922E-7, 9.180769230769231E-7 );
+    public final Probe probe2 = new Probe( -1.5440384615384618E-6, -1.2936538461538458E-6 );
+    public final Property<ImmutableVector2D> bodyPosition = new Property<ImmutableVector2D>( new ImmutableVector2D( 4.882500000000015E-6, -3.1298076923077013E-6 ) );
+
+    public WaveSensor() {
+        //This code helps to come up with a good set of defaults for the values.
+//        final SimpleObserver simpleObserver = new SimpleObserver() {
+//            public void update() {
+//                System.out.println( "bodyPosition.setValue( " + toCode( bodyPosition.getValue() ) + ");" );
+//                System.out.println( "probe1.position.setValue( " + toCode( probe1.position.getValue() ) + ");" );
+//                System.out.println( "probe2.position.setValue( " + toCode( probe2.position.getValue() ) + ");" );
+//            }
+//        };
+//        probe1.position.addObserver( simpleObserver );
+//        probe2.position.addObserver( simpleObserver );
+    }
+
+    public static String toCode( ImmutableVector2D v ) {
+        return "new ImmutableVector2D(" + v.getX() + "," + v.getY() + ")";
+    }
 
     public void translateBody( Dimension2D dimension2D ) {
         bodyPosition.setValue( bodyPosition.getValue().plus( dimension2D ) );
     }
 
     public static class Probe {
-        public final Property<ImmutableVector2D> position = new Property<ImmutableVector2D>( new ImmutableVector2D( 0, 0 ) );
+        public final Property<ImmutableVector2D> position;
         public final Property<Double> value = new Property<Double>( 0.0 );
+
+        public Probe( double x, double y ) {
+            position = new Property<ImmutableVector2D>( new ImmutableVector2D( x, y ) );
+        }
 
         public void translate( Dimension2D delta ) {
             position.setValue( position.getValue().plus( delta ) );
