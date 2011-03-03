40ae91346f0db90421f75c4a893f9a05c582bd27
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-03-03 19:40:32 +0000

    Changed the background color of the red slider to be more color-blind friendly, see #2769.

--- trunk/simulations-java/common/photon-absorption/src/edu/colorado/phet/common/photonabsorption/view/PhotonEmitterNode.java
+++ trunk/simulations-java/common/photon-absorption/src/edu/colorado/phet/common/photonabsorption/view/PhotonEmitterNode.java
@@ -88,7 +88,7 @@ public class PhotonEmitterNode extends PNode {
 
         // Add the slider that will control the rate of photon emission.
         Dimension emissionControlSliderSize = new Dimension( 100, 30 ); // This may be adjusted as needed for best look.
-        emissionRateControlSlider = new IntensitySlider( Color.RED, IntensitySlider.HORIZONTAL, emissionControlSliderSize );
+        emissionRateControlSlider = new IntensitySlider( new Color( 255, 85, 0 ), IntensitySlider.HORIZONTAL, emissionControlSliderSize );
         emissionRateControlSlider.setMinimum( 0 );
         emissionRateControlSlider.setMaximum( SLIDER_RANGE );
         emissionRateControlSlider.addChangeListener( new ChangeListener() {
@@ -231,7 +231,7 @@ public class PhotonEmitterNode extends PNode {
 
             // Update the color of the slider.
             if ( model.getEmittedPhotonWavelength() == WavelengthConstants.IR_WAVELENGTH ) {
-                emissionRateControlSlider.setColor( Color.RED );
+                emissionRateControlSlider.setColor( new Color( 255, 85, 0 ) );
             }
             else if ( model.getEmittedPhotonWavelength() == WavelengthConstants.VISIBLE_WAVELENGTH ) {
                 emissionRateControlSlider.setColor( Color.YELLOW );
