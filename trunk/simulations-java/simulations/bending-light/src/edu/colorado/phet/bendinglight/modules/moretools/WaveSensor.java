861b29d8cec6c83eda9ec9ef8e2acb2346364997
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-03-03 01:14:53 +0000

    renamed AmplitudeSensor to WaveSensor

--- trunk/simulations-java/simulations/bending-light/src/edu/colorado/phet/bendinglight/modules/moretools/WaveSensor.java
+++ trunk/simulations-java/simulations/bending-light/src/edu/colorado/phet/bendinglight/modules/moretools/WaveSensor.java
@@ -9,7 +9,7 @@ import edu.colorado.phet.common.phetcommon.model.Property;
 /**
  * @author Sam Reid
  */
-public class AmplitudeSensor {
+public class WaveSensor {
     public final Probe probe1 = new Probe();
     public final Probe probe2 = new Probe();
     public final Property<ImmutableVector2D> bodyPosition = new Property<ImmutableVector2D>( new ImmutableVector2D( 0, 0 ) );
