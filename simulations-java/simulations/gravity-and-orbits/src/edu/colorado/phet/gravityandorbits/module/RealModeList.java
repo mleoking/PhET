df84338f075623c8ed38cdc1cc2a4a3387f9f88c
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-03-04 21:29:46 +0000

    Use real masses and positions as much as possible, this ensures reasonable looking orbits and minimizes the differences between tabs

--- simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/module/RealModeList.java
+++ simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/module/RealModeList.java
@@ -3,6 +3,7 @@ package edu.colorado.phet.gravityandorbits.module;
 
 import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
 import edu.colorado.phet.common.phetcommon.model.Property;
+import edu.colorado.phet.gravityandorbits.model.GravityAndOrbitsClock;
 import edu.colorado.phet.gravityandorbits.view.Scale;
 
 /**
@@ -15,33 +16,27 @@ public class RealModeList extends ModeList {
     public static final double EARTH_RADIUS = 6.371E6;
     public static final double EARTH_MASS = 5.9736E24;
     public static final double EARTH_PERIHELION = 147098290E3;
-    private static final double EARTH_ORBITAL_SPEED_AT_PERIHELION = 30300;
+    public static final double EARTH_ORBITAL_SPEED_AT_PERIHELION = 30300;
 
-    private static final double MOON_MASS = 7.3477E22;
-    private static final double MOON_RADIUS = 1737.1E3;
-    private static final double MOON_EARTH_SPEED = -1.01E3;
-    private static final double MOON_SPEED = MOON_EARTH_SPEED;
+    public static final double MOON_MASS = 7.3477E22;
+    public static final double MOON_RADIUS = 1737.1E3;
+    public static final double MOON_EARTH_SPEED = -1.01E3;
+    public static final double MOON_SPEED = MOON_EARTH_SPEED;
     public static final double MOON_PERIGEE = 391370E3;
-    private static final double MOON_X = EARTH_PERIHELION;
-    private static final double MOON_Y = MOON_PERIGEE;
+    public static final double MOON_X = EARTH_PERIHELION;
+    public static final double MOON_Y = MOON_PERIGEE;
 
     //see http://en.wikipedia.org/wiki/International_Space_Station
-    private static final double SPACE_STATION_RADIUS = 109;
+    public static final double SPACE_STATION_RADIUS = 109;
     public static final double SPACE_STATION_MASS = 369914;
-    private static final double SPACE_STATION_SPEED = 7706;
-    private static final double SPACE_STATION_PERIGEE = 347000;
+    public static final double SPACE_STATION_SPEED = 7706;
+    public static final double SPACE_STATION_PERIGEE = 347000;
 
     // Add in some initial -x velocity to offset the earth-moon barycenter drift
     //This value was computed by sampling the total momentum in GravityAndOrbitsModel for this mode
     static ImmutableVector2D sampledSystemMomentum = new ImmutableVector2D( 7.421397422188586E25, -1.080211713202125E22 );
     static ImmutableVector2D velocityOffset = sampledSystemMomentum.getScaledInstance( -1 / ( EARTH_MASS + MOON_MASS ) );
 
-    /*
-    sun => , 0, 0
-     earth => 0, EARTH_ORBITAL_SPEED_AT_PERIHELION
-     moon => , MOON_SPEED, 0
-     ss => , 0, SPACE_STATION_SPEED
-     */
     public RealModeList( Property<Boolean> clockPausedProperty, Property<Boolean> gravityEnabledProperty, Property<Scale> scaleProperty, Property<Boolean> stepping, Property<Boolean> rewinding, Property<Double> timeSpeedScaleProperty ) {
         super( new ModeListParameter( clockPausedProperty, gravityEnabledProperty, scaleProperty, stepping, rewinding, timeSpeedScaleProperty ),
                new BodyPrototype( SUN_RADIUS, SUN_MASS, 0, 0 ),
@@ -52,6 +47,7 @@ public class RealModeList extends ModeList {
                0, SPACE_STATION_SPEED,
                0, EARTH_ORBITAL_SPEED_AT_PERIHELION,
                MOON_SPEED, EARTH_ORBITAL_SPEED_AT_PERIHELION,
-               MOON_SPEED, 0 );
+               MOON_SPEED, 0,
+               GravityAndOrbitsClock.DEFAULT_DT );
     }
 }
