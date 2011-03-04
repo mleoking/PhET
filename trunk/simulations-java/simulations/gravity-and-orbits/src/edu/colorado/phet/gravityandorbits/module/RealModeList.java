4568b046fec5bd31b52e5d8034e2e81172905eda
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-03-04 22:31:50 +0000

    more unification of cartoon and real parameters

--- trunk/simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/module/RealModeList.java
+++ trunk/simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/module/RealModeList.java
@@ -10,28 +10,6 @@ import edu.colorado.phet.gravityandorbits.view.Scale;
  * @author Sam Reid
  */
 public class RealModeList extends ModeList {
-    public static final double SUN_RADIUS = 6.955E8;
-    public static final double SUN_MASS = 1.989E30;
-
-    public static final double EARTH_RADIUS = 6.371E6;
-    public static final double EARTH_MASS = 5.9736E24;
-    public static final double EARTH_PERIHELION = 147098290E3;
-    public static final double EARTH_ORBITAL_SPEED_AT_PERIHELION = 30300;
-
-    public static final double MOON_MASS = 7.3477E22;
-    public static final double MOON_RADIUS = 1737.1E3;
-    public static final double MOON_EARTH_SPEED = -1.01E3;
-    public static final double MOON_SPEED = MOON_EARTH_SPEED;
-    public static final double MOON_PERIGEE = 391370E3;
-    public static final double MOON_X = EARTH_PERIHELION;
-    public static final double MOON_Y = MOON_PERIGEE;
-
-    //see http://en.wikipedia.org/wiki/International_Space_Station
-    public static final double SPACE_STATION_RADIUS = 109;
-    public static final double SPACE_STATION_MASS = 369914;
-    public static final double SPACE_STATION_SPEED = 7706;
-    public static final double SPACE_STATION_PERIGEE = 347000;
-
     // Add in some initial -x velocity to offset the earth-moon barycenter drift
     //This value was computed by sampling the total momentum in GravityAndOrbitsModel for this mode
     static ImmutableVector2D sampledSystemMomentum = new ImmutableVector2D( 7.421397422188586E25, -1.080211713202125E22 );
@@ -39,15 +17,18 @@ public class RealModeList extends ModeList {
 
     public RealModeList( Property<Boolean> clockPausedProperty, Property<Boolean> gravityEnabledProperty, Property<Scale> scaleProperty, Property<Boolean> stepping, Property<Boolean> rewinding, Property<Double> timeSpeedScaleProperty ) {
         super( new ModeListParameter( clockPausedProperty, gravityEnabledProperty, scaleProperty, stepping, rewinding, timeSpeedScaleProperty ),
-               new BodyPrototype( SUN_RADIUS, SUN_MASS, 0, 0 ),
-               new BodyPrototype( EARTH_RADIUS, EARTH_MASS, EARTH_PERIHELION, 0 ),
-               new BodyPrototype( MOON_RADIUS, MOON_MASS, MOON_X, MOON_Y ),
-               new BodyPrototype( SPACE_STATION_RADIUS, SPACE_STATION_MASS, EARTH_PERIHELION + SPACE_STATION_PERIGEE + EARTH_RADIUS, 0 ),
+               SUN,
+               EARTH,
+               MOON,
+               SPACE_STATION,
                1.25,
                0, SPACE_STATION_SPEED,
                0, EARTH_ORBITAL_SPEED_AT_PERIHELION,
                MOON_SPEED, EARTH_ORBITAL_SPEED_AT_PERIHELION,
                MOON_SPEED, 0,
-               GravityAndOrbitsClock.DEFAULT_DT );
+               GravityAndOrbitsClock.DEFAULT_DT,
+               400,
+               21600,
+               SUN_RADIUS, EARTH_RADIUS, SUN_RADIUS, EARTH_RADIUS, MOON_RADIUS, EARTH_RADIUS, MOON_RADIUS, EARTH_RADIUS, SPACE_STATION_RADIUS );
     }
 }
