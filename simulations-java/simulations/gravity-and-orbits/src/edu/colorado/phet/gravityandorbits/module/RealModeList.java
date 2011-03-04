a7119aadc01c39c1d5af4f2189c8db9e7e81ee14
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-03-04 20:02:04 +0000

    replaced position and velocity with primitives instead of ImmutableVector2D for readability

--- simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/module/RealModeList.java
+++ simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/module/RealModeList.java
@@ -38,9 +38,9 @@ public class RealModeList extends ModeList {
 
     public RealModeList( Property<Boolean> clockPausedProperty, Property<Boolean> gravityEnabledProperty, Property<Scale> scaleProperty, Property<Boolean> stepping, Property<Boolean> rewinding, Property<Double> timeSpeedScaleProperty ) {
         super( new ModeListParameter( clockPausedProperty, gravityEnabledProperty, scaleProperty, stepping, rewinding, timeSpeedScaleProperty ),
-               new BodyPrototype( SUN_RADIUS, SUN_MASS, new ImmutableVector2D( 0, 0 ), new ImmutableVector2D( 0, 0 ) ),
-               new BodyPrototype( EARTH_RADIUS, EARTH_MASS, new ImmutableVector2D( EARTH_PERIHELION, 0 ), new ImmutableVector2D( 0, EARTH_ORBITAL_SPEED_AT_PERIHELION ) ),
-               new BodyPrototype( MOON_RADIUS, MOON_MASS, new ImmutableVector2D( MOON_X, MOON_Y ), new ImmutableVector2D( MOON_SPEED, 0 ) ),
-               new BodyPrototype( SPACE_STATION_RADIUS, SPACE_STATION_MASS, new ImmutableVector2D( EARTH_PERIHELION + SPACE_STATION_PERIGEE + EARTH_RADIUS, 0 ), new ImmutableVector2D( 0, SPACE_STATION_SPEED ) ), 1.25 );
+               new BodyPrototype( SUN_RADIUS, SUN_MASS, 0, 0, 0, 0 ),
+               new BodyPrototype( EARTH_RADIUS, EARTH_MASS, EARTH_PERIHELION, 0, 0, EARTH_ORBITAL_SPEED_AT_PERIHELION ),
+               new BodyPrototype( MOON_RADIUS, MOON_MASS, MOON_X, MOON_Y, MOON_SPEED, 0 ),
+               new BodyPrototype( SPACE_STATION_RADIUS, SPACE_STATION_MASS, EARTH_PERIHELION + SPACE_STATION_PERIGEE + EARTH_RADIUS, 0, 0, SPACE_STATION_SPEED ), 1.25 );
     }
 }
