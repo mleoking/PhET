49295ec7018f55c368656d9e38be9d244f0069ed
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-03-04 22:09:25 +0000

    feature request: add close button to Game popups

--- simulations-java/simulations/balancing-chemical-equations/src/edu/colorado/phet/balancingchemicalequations/view/game/GameResultNode.java
+++ simulations-java/simulations/balancing-chemical-equations/src/edu/colorado/phet/balancingchemicalequations/view/game/GameResultNode.java
@@ -2,16 +2,20 @@
 
 package edu.colorado.phet.balancingchemicalequations.view.game;
 
-import java.awt.*;
+import java.awt.Color;
 import java.awt.geom.Rectangle2D;
 
+import edu.colorado.phet.common.phetcommon.resources.PhetCommonResources;
 import edu.colorado.phet.common.phetcommon.util.Function1;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
+import edu.colorado.phet.common.piccolophet.PhetPNode;
 import edu.colorado.phet.common.piccolophet.event.CursorHandler;
 import edu.colorado.phet.common.piccolophet.nodes.FaceNode;
 import edu.colorado.phet.common.piccolophet.util.PNodeLayoutUtils;
 import edu.umd.cs.piccolo.PNode;
-import edu.umd.cs.piccolo.event.PDragEventHandler;
+import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
+import edu.umd.cs.piccolo.event.PInputEvent;
+import edu.umd.cs.piccolo.nodes.PImage;
 import edu.umd.cs.piccolo.nodes.PPath;
 import edu.umd.cs.piccolox.nodes.PComposite;
 
@@ -21,7 +25,7 @@ import edu.umd.cs.piccolox.nodes.PComposite;
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
-public abstract class GameResultNode extends PComposite {
+public abstract class GameResultNode extends PhetPNode {
 
     private static final PhetFont FONT = new PhetFont( 24 );
     private static final Color BACKGROUND = new Color( 180, 205, 255 );
@@ -35,43 +39,60 @@ public abstract class GameResultNode extends PComposite {
     public GameResultNode( boolean smile, Function1<PhetFont, PNode> createContentFunction ) {
 
         // make draggable
-        addInputEventListener( new PDragEventHandler() );
         addInputEventListener( new CursorHandler() );
-
-        removeAllChildren();
+        addInputEventListener( new PBasicInputEventHandler() {
+            @Override
+            public void mouseDragged( PInputEvent event ) {
+                translate( event.getDelta().getWidth(), event.getDelta().getHeight() );
+            }
+        });
 
         PNode parentNode = new PComposite();
         addChild( parentNode );
 
+        // face
         FaceNode faceNode = new FaceNode( FACE_DIAMETER );
         if ( !smile ) {
             faceNode.frown();
         }
         parentNode.addChild( faceNode );
 
-        PNode iconsAndTextNode = createContentFunction.apply( FONT );
-        parentNode.addChild( iconsAndTextNode );
+        // content
+        PNode contentNode = createContentFunction.apply( FONT );
+        parentNode.addChild( contentNode );
 
-        // layout
-        double maxWidth = Math.max( faceNode.getFullBoundsReference().getWidth(), iconsAndTextNode.getFullBoundsReference().getWidth() );
+        // layout: content centered below face
+        double maxWidth = Math.max( faceNode.getFullBoundsReference().getWidth(), contentNode.getFullBoundsReference().getWidth() );
         double x = ( maxWidth / 2 ) - ( faceNode.getFullBoundsReference().getWidth() / 2 );
         double y = 0;
         faceNode.setOffset( x, y );
-        x = ( maxWidth / 2 ) - ( iconsAndTextNode.getFullBoundsReference().getWidth() / 2 );
+        x = ( maxWidth / 2 ) - ( contentNode.getFullBoundsReference().getWidth() / 2 );
         y = faceNode.getFullBoundsReference().getMaxY() + 20;
-        iconsAndTextNode.setOffset( x - PNodeLayoutUtils.getOriginXOffset( iconsAndTextNode ), y - PNodeLayoutUtils.getOriginYOffset( iconsAndTextNode ) );
+        contentNode.setOffset( x - PNodeLayoutUtils.getOriginXOffset( contentNode ), y - PNodeLayoutUtils.getOriginYOffset( contentNode ) );
         parentNode.setOffset( -PNodeLayoutUtils.getOriginXOffset( parentNode ), -PNodeLayoutUtils.getOriginYOffset( parentNode ) );
 
-        // now add a transparent background
+        // add a transparent background
         double w = getFullBoundsReference().getWidth() + ( 2 * MARGIN );
         double h = getFullBoundsReference().getHeight() + ( 2 * MARGIN );
         PPath backgroundNode = new PPath( new Rectangle2D.Double( 0, 0, w, h ) );
         backgroundNode.setPaint( BACKGROUND );
         addChild( backgroundNode );
         backgroundNode.moveToBack();
-
-        // layout the top-level nodes
         backgroundNode.setOffset( 0, 0 );
         parentNode.translate( MARGIN, MARGIN );
+
+        // close button at upper-right
+        PImage closeButtonNode = new PImage( PhetCommonResources.getImage( PhetCommonResources.IMAGE_CLOSE_BUTTON ) );
+        closeButtonNode.scale( 1.5 );
+        addChild( closeButtonNode );
+        closeButtonNode.addInputEventListener( new PBasicInputEventHandler() {
+            @Override
+            public void mouseReleased( PInputEvent event ) {
+                setVisible( false );
+            }
+        });
+        x = backgroundNode.getFullBoundsReference().getMaxX() - closeButtonNode.getFullBoundsReference().getWidth() - 5;
+        y = backgroundNode.getFullBoundsReference().getMinY() + 5;
+        closeButtonNode.setOffset( x, y );
     }
 }
