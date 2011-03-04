680f84dabe183b26a7d2113988829cb46221f3a1
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-03-04 17:13:31 +0000

    refactoring to simplify ModeList implementation

---
+++ trunk/simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/module/BodySpec.java
@@ -0,0 +1,21 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.gravityandorbits.module;
+
+import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
+
+/**
+ * @author Sam Reid
+ */
+public class BodySpec {
+    public final double radius;
+    public final double mass;
+    public final ImmutableVector2D position;
+    public final ImmutableVector2D velocity;
+
+    public BodySpec( double radius, double mass, ImmutableVector2D position, ImmutableVector2D velocity ) {
+        this.radius = radius;
+        this.mass = mass;
+        this.position = position;
+        this.velocity = velocity;
+    }
+}
