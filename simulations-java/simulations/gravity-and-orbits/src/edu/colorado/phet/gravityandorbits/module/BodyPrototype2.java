c32e95c81fe226eaf21d6814a777775aeb83703a
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-03-05 02:42:36 +0000

    new classes Mode and BodyPrototype2 for specifying initial conditions

---
+++ simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/module/BodyPrototype2.java
@@ -0,0 +1,23 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.gravityandorbits.module;
+
+/**
+ * @author Sam Reid
+ */
+public class BodyPrototype2 {
+    public double mass;
+    public double radius;
+    public double x;
+    public double y;
+    public double vx;
+    public double vy;
+
+    public BodyPrototype2( double mass, double radius, double x, double y, double vx, double vy ) {
+        this.mass = mass;
+        this.radius = radius;
+        this.x = x;
+        this.y = y;
+        this.vx = vx;
+        this.vy = vy;
+    }
+}
--- simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/module/CartoonModeList.java
+++ simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/module/CartoonModeList.java
@@ -2,7 +2,6 @@
 package edu.colorado.phet.gravityandorbits.module;
 
 import edu.colorado.phet.common.phetcommon.model.Property;
-import edu.colorado.phet.gravityandorbits.model.GravityAndOrbitsClock;
 import edu.colorado.phet.gravityandorbits.view.Scale;
 
 /**
@@ -11,21 +10,23 @@ import edu.colorado.phet.gravityandorbits.view.Scale;
 public class CartoonModeList extends ModeList {
     public CartoonModeList( Property<Boolean> clockPausedProperty, Property<Boolean> gravityEnabledProperty, Property<Scale> scaleProperty, Property<Boolean> stepping, Property<Boolean> rewinding, Property<Double> timeSpeedScaleProperty ) {
         super( new ModeListParameter( clockPausedProperty, gravityEnabledProperty, scaleProperty, stepping, rewinding, timeSpeedScaleProperty ),
-               //Use real masses and positions as much as possible, this ensures reasonable looking orbits and minimizes the differences between tabs
-               SUN,
-               EARTH,
-               MOON,
-               SPACE_STATION,
-               1.25,
-               400, 21600, 0, SPACE_STATION_SPEED,
-               0, EARTH_ORBITAL_SPEED_AT_PERIHELION,
-               MOON_SPEED, EARTH_ORBITAL_SPEED_AT_PERIHELION,
-               MOON_SPEED, 0,
-               GravityAndOrbitsClock.DEFAULT_DT,
-               //Keeping the scale factors similar keeps both objects closer to the same scale
-               SUN_RADIUS * 50, EARTH_RADIUS * 1100,
-               SUN_RADIUS * 50, EARTH_RADIUS * 50, MOON_RADIUS * 20,
-               EARTH_RADIUS * 15, MOON_RADIUS * 15,
-               EARTH_RADIUS * 0.8, SPACE_STATION_RADIUS * 8 );
+               new SunEarth() {{
+                   sun.radius *= 50;
+                   earth.radius *= 1100;
+               }}, new SunEarthMoon() {{
+                    sun.radius *= 50;
+                    earth.radius *= 300;
+                    moon.radius *= 100;
+
+                    earth.mass *= 200;
+                    moon.vx *= 4;
+                    moon.y *= 10;
+                }}, new EarthMoon() {{
+                    earth.radius *= 15;
+                    moon.radius *= 15;
+                }}, new EarthSpaceStation() {{
+                    earth.radius *= 0.8;
+                    spaceStation.radius *= 8;
+                }} );
     }
 }
