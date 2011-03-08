f46873e656cad0da982e6bd7249451782cbce0eb
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-03-08 21:53:04 +0000

    Made velocity sensor node resize with text

--- trunk/simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/nodes/ThreeImageNode.java
+++ trunk/simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/nodes/ThreeImageNode.java
@@ -5,6 +5,8 @@ package edu.colorado.phet.common.piccolophet.nodes;
 import java.awt.*;
 import java.awt.geom.AffineTransform;
 
+import edu.colorado.phet.common.phetcommon.model.property.Property;
+import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.nodes.PImage;
 
@@ -18,6 +20,7 @@ public class ThreeImageNode extends PNode {
     public final PImage leftPatch;
     public final PImage centerPatch;
     public final PImage rightPatch;
+    private final Property<Double> centerWidth;
 
     /*
      * Constructor that uses the leaves the center image size unchanged.
@@ -29,7 +32,8 @@ public class ThreeImageNode extends PNode {
     /*
      * Constructs a ThreeImageNode with the specified images, and the specified width for the center component.
      */
-    public ThreeImageNode( Image left, Image center, Image right, double centerWidth ) {
+    public ThreeImageNode( Image left, Image center, Image right, double initialCenterWidth ) {
+        this.centerWidth = new Property<Double>( initialCenterWidth );
         leftPatch = new PImage( left );
         addChild( leftPatch );
 
@@ -39,17 +43,25 @@ public class ThreeImageNode extends PNode {
         rightPatch = new PImage( right );
         addChild( rightPatch );
 
-        setCenterWidth( centerWidth );
+        setCenterWidth( initialCenterWidth );
     }
 
     public void setCenterWidth( double centerWidth ) {
-        //Stretch center piece to fit the text, it is always an exact fit and there is no minimum.
-        centerPatch.setTransform( new AffineTransform() );//reset the centerPatch so that its bounds can be used to compute the right scale sx
-        double sx = centerWidth / centerPatch.getFullBounds().getWidth();//how much to scale the centerPatch
-        centerPatch.setTransform( AffineTransform.getScaleInstance( sx, 1 ) );
-        centerPatch.translate( leftPatch.getFullBounds().getMaxX() / sx, 0 );
+        //Handling internal resizing before sending out notifications prevents an order dependency in listeners
+        if ( centerWidth != this.centerWidth.getValue() ) {
+            //Stretch center piece to fit the text, it is always an exact fit and there is no minimum.
+            centerPatch.setTransform( new AffineTransform() );//reset the centerPatch so that its bounds can be used to compute the right scale sx
+            double sx = centerWidth / centerPatch.getFullBounds().getWidth();//how much to scale the centerPatch
+            centerPatch.setTransform( AffineTransform.getScaleInstance( sx, 1 ) );
+            centerPatch.translate( leftPatch.getFullBounds().getMaxX() / sx, 0 );
 
-        //Position the right patch to the side of the stretched center patch so they don't overlap
-        rightPatch.setOffset( leftPatch.getFullBounds().getWidth() + centerWidth, 0 );
+            //Position the right patch to the side of the stretched center patch so they don't overlap
+            rightPatch.setOffset( leftPatch.getFullBounds().getWidth() + centerWidth, 0 );
+            this.centerWidth.setValue( centerWidth );
+        }
+    }
+
+    public void addCenterWidthObserver( SimpleObserver simpleObserver ) {
+        centerWidth.addObserver( simpleObserver );
     }
 }
--- trunk/simulations-java/simulations/bending-light/src/edu/colorado/phet/bendinglight/modules/moretools/VelocitySensorNode.java
+++ trunk/simulations-java/simulations/bending-light/src/edu/colorado/phet/bendinglight/modules/moretools/VelocitySensorNode.java
@@ -3,6 +3,9 @@ package edu.colorado.phet.bendinglight.modules.moretools;
 
 import java.awt.*;
 import java.awt.geom.Point2D;
+import java.awt.image.BufferedImage;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.text.DecimalFormat;
 
 import edu.colorado.phet.bendinglight.model.BendingLightModel;
@@ -15,6 +18,8 @@ import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTra
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.event.CursorHandler;
 import edu.colorado.phet.common.piccolophet.nodes.ArrowNode;
+import edu.colorado.phet.common.piccolophet.nodes.ThreeImageNode;
+import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
 import edu.umd.cs.piccolo.event.PInputEvent;
 import edu.umd.cs.piccolo.nodes.PImage;
@@ -32,45 +37,73 @@ public class VelocitySensorNode extends ToolboxNode.DoDragNode {
     public VelocitySensorNode( final ModelViewTransform transform, final VelocitySensor velocitySensor ) {
         this.transform = transform;
         this.velocitySensor = velocitySensor;
-        final Rectangle readoutBounds = new Rectangle( 18, 38, 96 - 18, 70 - 38 );
-        final Rectangle titleBounds = new Rectangle( 18, 10, 96 - 18, 32 - 10 );
-        final PImage imageNode = new PImage( RESOURCES.getImage( "velocity_detector.png" ) );
+        final int titleOffsetY = 7;
+        final int readoutOffsetY = 38;
+
+        final ThreeImageNode imageNode = new ThreeImageNode( RESOURCES.getImage( "velocity_left.png" ), RESOURCES.getImage( "velocity_center.png" ), RESOURCES.getImage( "velocity_right.png" ) );
         addChild( imageNode );
 
-        addChild( new PText( "Speed" ) {{
+        final PText titleNode = new PText( "Speed" ) {{
             setFont( new PhetFont( 22 ) );
-            setOffset( titleBounds.getCenterX() - getFullBounds().getWidth() / 2, titleBounds.getCenterY() - getFullBounds().getHeight() / 2 );
-        }} );
+            imageNode.addCenterWidthObserver( new SimpleObserver() {
+                public void update() {
+                    setOffset( imageNode.getFullBounds().getCenterX() - getFullBounds().getWidth() / 2, titleOffsetY );
+                }
+            } );
+        }};
+        addChild( titleNode );
         addChild( new PText() {{
             setFont( new PhetFont( 26 ) );
+            final SimpleObserver updateTextLocation = new SimpleObserver() {
+                public void update() {
+                    setOffset( imageNode.getFullBounds().getCenterX() - getFullBounds().getWidth() / 2, readoutOffsetY );
+                }
+            };
+            imageNode.addCenterWidthObserver( updateTextLocation );
             velocitySensor.value.addObserver( new SimpleObserver() {
                 public void update() {
                     final Option<ImmutableVector2D> value = velocitySensor.value.getValue();
                     setText( ( value.isNone() ) ? "?" :
                              new DecimalFormat( "0.00" ).format( value.get().getMagnitude() / BendingLightModel.SPEED_OF_LIGHT ) + " c" );
-                    setOffset( readoutBounds.getCenterX() - getFullBounds().getWidth() / 2, readoutBounds.getCenterY() - getFullBounds().getHeight() / 2 );
+                    imageNode.setCenterWidth( Math.max( titleNode.getFullBounds().getWidth(), getFullBounds().getWidth() ) );
+                    updateTextLocation.update();
                 }
             } );
         }} );
+        final BufferedImage velocityPoint = RESOURCES.getImage( "velocity_point.png" );
+        final PImage velocityPointNode = new PImage( velocityPoint ) {{
+            final PropertyChangeListener updatePosition = new PropertyChangeListener() {
+                public void propertyChange( PropertyChangeEvent evt ) {
+                    setOffset( imageNode.getFullBounds().getCenterX() - getFullBounds().getWidth() / 2, imageNode.getFullBounds().getMaxY() );
+                }
+            };
+            imageNode.addPropertyChangeListener( PROPERTY_FULL_BOUNDS, updatePosition );
+            updatePosition.propertyChange( null );
+        }};
+        addChild( velocityPointNode );
 
         addChild( new ArrowNode( new Point2D.Double(), new Point2D.Double( 100, 100 ), 20, 20, 10 ) {{
             setPaint( Color.blue );
             setStrokePaint( Color.black );
             setStroke( new BasicStroke( 1 ) );
-            velocitySensor.value.addObserver( new SimpleObserver() {
-                public void update() {
+            final PropertyChangeListener updateArrow = new PropertyChangeListener() {
+                public void propertyChange( PropertyChangeEvent evt ) {
                     final Option<ImmutableVector2D> value = velocitySensor.value.getValue();
                     if ( value.isNone() ) {
                         setVisible( false );
                     }
                     else {
                         ImmutableVector2D v = transform.modelToViewDelta( value.get() ).times( 1.5E-14 );
-                        setTipAndTailLocations( imageNode.getFullBounds().getCenterX() + v.getX() / 2, imageNode.getFullBounds().getMaxY() + v.getY() / 2,
-                                                imageNode.getFullBounds().getCenterX() - v.getX() / 2, imageNode.getFullBounds().getMaxY() - v.getY() / 2 );
+                        setTipAndTailLocations( velocityPointNode.getFullBounds().getCenterX() + v.getX() / 2, velocityPointNode.getFullBounds().getMaxY() + v.getY() / 2,
+                                                velocityPointNode.getFullBounds().getCenterX() - v.getX() / 2, velocityPointNode.getFullBounds().getMaxY() - v.getY() / 2 );
                         setVisible( true );
                     }
-//                    setText( Double.isNaN( value ) ? "?" : new DecimalFormat( "0.0" ).format( value / BendingLightModel.SPEED_OF_LIGHT ) + " c" );
-//                    setOffset( readoutBounds.getCenterX() - getFullBounds().getWidth() / 2, readoutBounds.getCenterY() - getFullBounds().getHeight() / 2 );
+                }
+            };
+            velocityPointNode.addPropertyChangeListener( PNode.PROPERTY_FULL_BOUNDS, updateArrow );
+            velocitySensor.value.addObserver( new SimpleObserver() {
+                public void update() {
+                    updateArrow.propertyChange( null );
                 }
             } );
         }} );
@@ -84,7 +117,7 @@ public class VelocitySensorNode extends ToolboxNode.DoDragNode {
         velocitySensor.position.addObserver( new SimpleObserver() {
             public void update() {
                 final Point2D.Double viewPoint = transform.modelToView( velocitySensor.position.getValue() ).toPoint2D();
-                setOffset( viewPoint.getX() - imageNode.getFullBounds().getWidth() / 2, viewPoint.getY() - imageNode.getFullBounds().getHeight() );
+                setOffset( viewPoint.getX() - imageNode.getFullBounds().getWidth() / 2, viewPoint.getY() - imageNode.getFullBounds().getHeight() - velocityPoint.getHeight() );
             }
         } );
     }
