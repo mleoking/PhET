6ad31b449b3ebd27bc18c8ee5cef53bb112851bb
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-03-09 23:36:15 +0000

    #2775, add notification mechanism that returns new and old value.  Add VoidFunction2 in support of this (with same backward parameter naming as Function2.)

--- trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/model/property/Property.java
+++ trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/model/property/Property.java
@@ -3,7 +3,9 @@ package edu.colorado.phet.common.phetcommon.model.property;
 
 import java.util.ArrayList;
 
+import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
+import edu.colorado.phet.common.phetcommon.util.function.VoidFunction2;
 
 /**
  * Property<T> can be used to represent a value of type T in a MVC style pattern.  It remembers its default value and can be reset.
@@ -17,7 +19,8 @@ import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
 public class Property<T> extends SettableProperty<T> {
     private T value;
     private final T initialValue;
-    private ArrayList<VoidFunction1<T>> valueObservers = new ArrayList<VoidFunction1<T>>();//Listeners that also receive the new value in the callback
+    private ArrayList<VoidFunction1<T>> newValueObservers = new ArrayList<VoidFunction1<T>>();//Listeners that receive the new value in the callback
+    private ArrayList<VoidFunction2<T,T>> newAndOldValueObservers = new ArrayList<VoidFunction2<T,T>>();//Listeners that receive the new and old values in the callback
 
     public Property( T value ) {
         this.initialValue = value;
@@ -35,27 +38,58 @@ public class Property<T> extends SettableProperty<T> {
 
     public void setValue( T value ) {
         if ( !this.value.equals( value ) ) {
+            T oldValue = this.value;
             this.value = value;
             notifyObservers();//Notify SimpleObservers
-            notifyValueObservers();//Notify typed callback listeners
+            notifyNewValueObservers( value );//Notify typed callback listeners
+            notifyNewAndOldValueObservers( value, oldValue );
+        }
+    }
+
+    /*
+     * Notify observers that receive new value in the callback.
+     */
+    private void notifyNewValueObservers( T newValue ) {
+        for ( VoidFunction1<T> observer : new ArrayList<VoidFunction1<T>>( newValueObservers ) ) {//Iterate on a copy of the observer list to avoid ConcurrentModificationException, see #2741
+            observer.apply( newValue );
         }
     }
 
     /**
-     * Additionally notify observers that receive the value itself in the callback.
+     * Adds an observer that will receive the new value in the callback.
+     * Unlike SimpleObservers, this observer does not receive immediate notification when it's added,
+     * because there is no notion of "new value".
+     * @param observer
      */
-    private void notifyValueObservers() {
-        for ( VoidFunction1<T> observer : new ArrayList<VoidFunction1<T>>( valueObservers ) ) {//Iterate on a copy of the observer list to avoid ConcurrentModificationException, see #2741
-            observer.apply( getValue() );
-        }
-    }
-
     public void addObserver( VoidFunction1<T> observer ) {
-        valueObservers.add( observer );
+        newValueObservers.add( observer );
     }
 
     public void removeObserver( VoidFunction1<T> observer ) {
-        valueObservers.remove( observer );
+        newValueObservers.remove( observer );
+    }
+
+    /*
+     * Notify observers that receive the new and old values in the callback.
+     */
+    private void notifyNewAndOldValueObservers( T newValue, T oldValue ) {
+        for ( VoidFunction2<T,T> observer : new ArrayList<VoidFunction2<T,T>>( newAndOldValueObservers ) ) {//Iterate on a copy of the observer list to avoid ConcurrentModificationException, see #2741
+            observer.apply( newValue, oldValue );
+        }
+    }
+
+    /**
+     * Adds an observer that will receive the new and old value in the callback.
+     * Unlike SimpleObservers, this observer does not receive immediate notification when it's added,
+     * because there is no notion of "new value" or "old value".
+     * @param observer
+     */
+    public void addObserver( VoidFunction2<T,T> observer ) {
+        newAndOldValueObservers.add( observer );
+    }
+
+    public void removeObserver( VoidFunction2<T,T> observer ) {
+        newAndOldValueObservers.remove( observer );
     }
 
     /**
@@ -66,5 +100,26 @@ public class Property<T> extends SettableProperty<T> {
      */
     protected T getInitialValue() {
         return initialValue;
+    }
+
+    // test
+    public static void main( String[] args ) {
+        final Property<Boolean> enabled = new Property<Boolean>( true );
+        enabled.addObserver( new SimpleObserver() {
+            public void update() {
+                System.out.println( "SimpleObserver enabled=" + enabled.getValue() );
+            }
+        } );
+        enabled.addObserver( new VoidFunction1<Boolean>() {
+            public void apply( Boolean newValue ) {
+                System.out.println( "VoidFunction1 enabled=" + newValue );
+            }
+        } );
+        enabled.addObserver( new VoidFunction2<Boolean,Boolean>() {
+            public void apply( Boolean newValue, Boolean oldValue ) {
+                System.out.println( "VoidFunction1 new=" + newValue + " old=" + oldValue );
+            }
+        } );
+        enabled.setValue( !enabled.getValue() );
     }
 }
