a4beba7470a9ad45ae61c335570f8ae6d12613fb
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-03-10 00:25:55 +0000

    Added initial prototype of slider implementation.

--- trunk/simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/modules/interactiveisotope/view/IsotopeSliderNode.java
+++ trunk/simulations-java/simulations/build-an-atom/src/edu/colorado/phet/buildanatom/modules/interactiveisotope/view/IsotopeSliderNode.java
@@ -4,15 +4,24 @@ package edu.colorado.phet.buildanatom.modules.interactiveisotope.view;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.geom.Dimension2D;
 import java.awt.geom.Rectangle2D;
 
+import javax.swing.BorderFactory;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
 import edu.colorado.phet.buildanatom.modules.isotopemixture.model.IsotopeMixturesModel;
+import edu.colorado.phet.buildanatom.modules.isotopemixture.view.IsotopeMixturesCanvas;
+import edu.colorado.phet.common.phetcommon.view.controls.valuecontrol.LinearValueControl;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
+import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.nodes.PText;
 import edu.umd.cs.piccolo.util.PDimension;
+import edu.umd.cs.piccolox.pswing.PSwing;
 
 /**
  * PNode that displays a slider, a numerical value, and a "key" isotope, and
@@ -33,7 +42,42 @@ public class IsotopeSliderNode extends PNode {
                 Color.BLACK);
         enclosure.setOffset( mvt.modelToView( modelControl.getCenterPositionRef() ) );
         addChild( enclosure );
-        // TODO: temp - add a label.
-        enclosure.addChild( new PText(modelControl.getAtomConfig().getName() + "-" + modelControl.getAtomConfig().getMassNumber()) );
+
+        // Add the slider that controls the quantity of this isotope.
+        LinearValueControl isotopeQuantityControl;
+        isotopeQuantityControl = new LinearValueControl(
+                0, 100, modelControl.getAtomConfig().getName() + "-" + modelControl.getAtomConfig().getMassNumber(),
+                "###",
+                null);
+        isotopeQuantityControl.setUpDownArrowDelta( 1 );
+        isotopeQuantityControl.setTextFieldEditable( true );
+        isotopeQuantityControl.setFont( new PhetFont( Font.PLAIN, 14 ) );
+        isotopeQuantityControl.setTickPattern( "0" );
+        isotopeQuantityControl.setMajorTickSpacing( 25 );
+        isotopeQuantityControl.setMinorTickSpacing( 12.5 );
+        isotopeQuantityControl.setValue( 0 );
+        isotopeQuantityControl.setBackground( IsotopeMixturesCanvas.BACKGROUND_COLOR );
+        isotopeQuantityControl.getSlider().setBackground( IsotopeMixturesCanvas.BACKGROUND_COLOR );
+        isotopeQuantityControl.addChangeListener( new ChangeListener() {
+            public void stateChanged( ChangeEvent e ) {
+                // TODO: TBD.
+            }
+        });
+
+        PNode linearSliderNode = new PSwing( isotopeQuantityControl ){{
+            centerFullBoundsOnPoint( 0, 0 );
+        }};
+        enclosure.addChild( linearSliderNode );
+
+        /*
+        PNode labeledU235Nucleus = new LabeledNucleusImageNode("uranium-nucleus-small.png",
+                NuclearPhysicsStrings.URANIUM_235_ISOTOPE_NUMBER,
+                NuclearPhysicsStrings.URANIUM_235_CHEMICAL_SYMBOL,
+                NuclearPhysicsConstants.URANIUM_235_LABEL_COLOR );
+        Icon icon = new ImageIcon( labeledU235Nucleus.toImage(25, 25, null) );
+        _u235AmountControl.setValueLabelIcon( icon );
+        */
+
+
     }
 }
