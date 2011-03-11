5b7012e3766b177f15d517079557168d6d588dda
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-03-11 06:38:41 +0000

    3/10/11 Fixed boolean properties to notify new observer types

--- trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/model/property/Not.java
+++ trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/model/property/Not.java
@@ -2,6 +2,7 @@
 package edu.colorado.phet.common.phetcommon.model.property;
 
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
+import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
 
 /**
  * Provides the negation of a Property<Boolean>
@@ -10,14 +11,21 @@ import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
  */
 public class Not extends SettableProperty<Boolean> {
     private Property<Boolean> parent;
+    private Boolean lastValue;
 
     public Not( final Property<Boolean> parent ) {
         this.parent = parent;
+        lastValue = getValue();
         parent.addObserver( new SimpleObserver() {
             public void update() {
-                notifyObservers();
+                notifyObservers( getValue(), lastValue );
+                lastValue = getValue();
             }
         } );
+    }
+
+    public And and( SettableProperty<Boolean> p ) {
+        return new And( this, p );
     }
 
     @Override
@@ -29,7 +37,28 @@ public class Not extends SettableProperty<Boolean> {
         parent.setValue( !value );//we'll observe the change in the constructor listener, and fire notifications.
     }
 
-    public static SettableProperty<Boolean> not( Property<Boolean> p ) {
+    public static Not not( Property<Boolean> p ) {
         return new Not( p );
+    }
+
+    public static void main( String[] args ) {
+        BooleanProperty a = new BooleanProperty( true );
+        a.addObserver( new VoidFunction1<Boolean>() {
+            public void apply( Boolean aBoolean ) {
+                System.out.println( "a = " + aBoolean );
+            }
+        } );
+        Not b = new Not( a );
+        b.addObserver( new VoidFunction1<Boolean>() {
+            public void apply( Boolean aBoolean ) {
+                System.out.println( "b = " + aBoolean );
+            }
+        } );
+        System.out.println( "Setting a to false:" );
+        a.setValue( false );
+
+
+        System.out.println( "Setting a to true" );
+        a.setValue( true );
     }
 }
