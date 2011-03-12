087f137be86095c73dad03c68361159b03715542
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-03-12 17:37:21 +0000

    Provide single data points instead of batches for recordings

--- trunk/util/simsharing/src/edu/colorado/phet/simsharing/teacher/GetRecordingSample.java
+++ trunk/util/simsharing/src/edu/colorado/phet/simsharing/teacher/GetRecordingSample.java
@@ -6,14 +6,20 @@ import java.io.Serializable;
 /**
  * @author Sam Reid
  */
-public class GetRecording implements Serializable {
+public class GetRecordingSample implements Serializable {
     private final String filename;
+    private final int index;
 
-    public GetRecording( String filename ) {
+    public GetRecordingSample( String filename, int index ) {
         this.filename = filename;
+        this.index = index;
     }
 
     public String getFilename() {
         return filename;
+    }
+
+    public int getIndex() {
+        return index;
     }
 }
