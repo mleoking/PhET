34113e2c6e434718bdcabf71649f2c62998336e3
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-03-14 02:46:56 +0000

    Minor refactoring

--- trunk/util/simsharing/src/edu/colorado/phet/simsharing/teacher/Table.java
+++ trunk/util/simsharing/src/edu/colorado/phet/simsharing/teacher/Table.java
@@ -14,64 +14,12 @@ import javax.swing.table.*;
 public class Table extends JPanel {
     public JTable table;
 
-    public static class Row {
-        public final String classID;
-        public final String userID;
-        public final Date startTime;
-        public final long upTime;
-        public final boolean online;
-        public final String simName;
-        public final long latency;
-        public final int sessionID;
-        public final boolean earthCrashed;
-        public final double sunMass;
-
-        public Row( String classID, String userID, Date startTime, long upTime, boolean online, String simName, long latency, int sessionID, boolean earthCrashed, double sunMass ) {
-            this.classID = classID;
-            this.userID = userID;
-            this.startTime = startTime;
-            this.upTime = upTime;
-            this.online = online;
-            this.simName = simName;
-            this.latency = latency;
-            this.sessionID = sessionID;
-            this.earthCrashed = earthCrashed;
-            this.sunMass = sunMass;
-        }
-
-        public Object getValue( int columnIndex ) {
-            switch( columnIndex ) {
-                case 0:
-                    return classID;
-                case 1:
-                    return userID;
-                case 2:
-                    return startTime;
-                case 3:
-                    return upTime;
-                case 4:
-                    return online;
-                case 5:
-                    return simName;
-                case 6:
-                    return latency;
-                case 7:
-                    return sessionID;
-                case 8:
-                    return earthCrashed;
-                case 9:
-                    return sunMass;
-            }
-            return null;
-        }
-    }
-
     public Table() {
         setLayout( new BorderLayout() );
-        final ArrayList<Row> rows = new ArrayList<Row>();
-        rows.add( new Row( "Hector-123", "alpha", new Date(), 1000, true, "Gravity and Orbits", 23, 78, true, 100E24 ) );
-        rows.add( new Row( "Hector-123", "beta", new Date(), 200, true, "Gravity and Orbits", 22, 79, true, 95E24 ) );
-        rows.add( new Row( "Hector-123", "17", new Date(), 3000, false, "Gravity and Orbits", 30, 80, false, 98E23 ) );
+        final ArrayList<StudentInfo> rows = new ArrayList<StudentInfo>();
+        rows.add( new StudentInfo( "Hector-123", "alpha", new Date(), 1000, true, "Gravity and Orbits", 23, 78, true, 100E24 ) );
+        rows.add( new StudentInfo( "Hector-123", "beta", new Date(), 200, true, "Gravity and Orbits", 22, 79, true, 95E24 ) );
+        rows.add( new StudentInfo( "Hector-123", "17", new Date(), 3000, false, "Gravity and Orbits", 30, 80, false, 98E23 ) );
         table = new JTable( new AbstractTableModel() {
             public String[] columns = new String[] { "Class ID", "User ID", "Start Time", "Uptime (minutes)", "Online", "Sim", "Latency (ms)", "Session ID", "Earth Crashed", "Sun Mass (kg)" };
 
