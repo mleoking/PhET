b8c39cce1a20844a06431ee9858da4abbcb431ee
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-03-16 19:38:52 +0000

    refactored ToolboxNode

---
+++ trunk/simulations-java/simulations/bending-light/src/edu/colorado/phet/bendinglight/modules/intro/Tool.java
@@ -0,0 +1,98 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.bendinglight.modules.intro;
+
+import java.awt.*;
+import java.awt.geom.Point2D;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+import edu.colorado.phet.bendinglight.view.BendingLightCanvas;
+import edu.colorado.phet.common.phetcommon.model.property.Property;
+import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
+import edu.colorado.phet.common.phetcommon.util.function.Function3;
+import edu.colorado.phet.common.phetcommon.util.function.VoidFunction0;
+import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
+import edu.colorado.phet.common.piccolophet.event.CursorHandler;
+import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
+import edu.umd.cs.piccolo.event.PInputEvent;
+import edu.umd.cs.piccolo.nodes.PImage;
+
+/**
+ * @author Sam Reid
+ */
+public class Tool extends PNode {
+    Tool( Image thumbnail, final Property<Boolean> showTool, final ModelViewTransform transform, final ToolboxNode toolbox, final BendingLightCanvas canvas,
+          final Function3<ModelViewTransform, Property<Boolean>, Point2D, DraggableNode> nodeMaker, final ResetModel resetModel ) {
+        final PImage thumbnailIcon = new PImage( thumbnail ) {{
+            showTool.addObserver( new SimpleObserver() {
+                public void update() {
+                    setVisible( !showTool.getValue() );
+                }
+            } );
+            final PImage thumbRef = this;
+            addInputEventListener( new PBasicInputEventHandler() {
+                {
+                    resetModel.addResetListener( new VoidFunction0() {
+                        public void apply() {
+                            reset();
+                        }
+                    } );
+                }
+
+                DraggableNode node = null;
+                boolean intersect = false;
+
+                @Override
+                public void mousePressed( PInputEvent event ) {
+                    showTool.setValue( true );
+                    setVisible( false );
+                    if ( node == null ) {
+                        node = nodeMaker.apply( transform, showTool, transform.viewToModel( event.getPositionRelativeTo( canvas.getRootNode() ) ) );
+                        final PropertyChangeListener pcl = new PropertyChangeListener() {
+                            public void propertyChange( PropertyChangeEvent evt ) {
+                                intersect = toolbox.getGlobalFullBounds().contains( node.getGlobalFullBounds().getCenter2D() );
+                            }
+                        };
+                        node.addPropertyChangeListener( PROPERTY_FULL_BOUNDS, pcl );
+                        node.addInputEventListener( new PBasicInputEventHandler() {
+                            public void mouseReleased( PInputEvent event ) {
+                                if ( intersect ) {
+                                    showTool.setValue( false );
+                                    thumbRef.setVisible( true );
+                                    node.removePropertyChangeListener( pcl );
+                                    reset();
+                                }
+                            }
+                        } );
+
+                        canvas.addChild( node );
+                    }
+                }
+
+                public void mouseDragged( PInputEvent event ) {
+                    node.doDrag( event );
+                }
+
+                //This is when the user drags the object out of the toolbox then drops it right back in the toolbox.
+                public void mouseReleased( PInputEvent event ) {
+                    if ( intersect ) {
+                        showTool.setValue( false );
+                        thumbRef.setVisible( true );
+                        reset();
+                        //TODO: how to remove pcl?
+                    }
+                }
+
+                private void reset() {
+                    canvas.removeChild( node );
+                    node = null;
+                }
+            } );
+            addInputEventListener( new CursorHandler() );
+        }};
+
+        addChild( thumbnailIcon );
+    }
+
+}
