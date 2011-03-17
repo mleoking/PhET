c6322f60f559897744244a71c53e9161d316ccd4
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-03-17 22:14:25 +0000

    Added application and module classes

---
+++ simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/MicroscopicModule.java
@@ -0,0 +1,11 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.sugarandsaltsolutions;
+
+/**
+ * @author Sam Reid
+ */
+public class MicroscopicModule extends SugarAndSaltSolutionsModule {
+    public MicroscopicModule() {
+        super( "Microscopic" );
+    }
+}
---
+++ simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/SugarAndSaltSolutionsApplication.java
@@ -0,0 +1,25 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.sugarandsaltsolutions;
+
+import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
+import edu.colorado.phet.common.phetcommon.application.PhetApplicationLauncher;
+import edu.colorado.phet.common.phetcommon.resources.PhetResources;
+import edu.colorado.phet.common.piccolophet.PiccoloPhetApplication;
+
+/**
+ * @author Sam Reid
+ */
+public class SugarAndSaltSolutionsApplication extends PiccoloPhetApplication {
+    private static final String NAME = "bending-light";
+    public static final PhetResources RESOURCES = new PhetResources( NAME );
+
+    public SugarAndSaltSolutionsApplication( PhetApplicationConfig config ) {
+        super( config );
+        addModule( new IntroModule() );
+        addModule( new MicroscopicModule() );
+    }
+
+    public static void main( String[] args ) {
+        new PhetApplicationLauncher().launchSim( args, NAME, SugarAndSaltSolutionsApplication.class );
+    }
+}
