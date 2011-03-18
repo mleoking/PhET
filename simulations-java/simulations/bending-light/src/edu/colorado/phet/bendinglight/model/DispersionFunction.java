cbb03f77b8b43a4a70583a065b12c4184abe668a
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-03-18 21:29:05 +0000

    renamed parameter

--- simulations-java/simulations/bending-light/src/edu/colorado/phet/bendinglight/model/DispersionFunction.java
+++ simulations-java/simulations/bending-light/src/edu/colorado/phet/bendinglight/model/DispersionFunction.java
@@ -5,5 +5,5 @@ package edu.colorado.phet.bendinglight.model;
  * @author Sam Reid
  */
 public interface DispersionFunction {
-    double getIndexOfRefraction( double wavelength, double indexOfRefractionForBlue );//VisibleColor.MIN_WAVELENGTH / 1E9
+    double getIndexOfRefraction( double wavelength, double baseIndexOfRefraction );//VisibleColor.MIN_WAVELENGTH / 1E9
 }
