d1d489794017e8180dc9725e2965a0db6c59a460
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-03-19 00:58:58 +0000

    removed unused interface

--- trunk/simulations-java/simulations/bending-light/src/edu/colorado/phet/bendinglight/model/DispersionFunction.java
+++
@@ -1,9 +0,0 @@
-// Copyright 2002-2011, University of Colorado
-package edu.colorado.phet.bendinglight.model;
-
-/**
- * @author Sam Reid
- */
-public interface DispersionFunction {
-    double getIndexOfRefraction( double wavelength, double baseIndexOfRefraction );//VisibleColor.MIN_WAVELENGTH / 1E9
-}
