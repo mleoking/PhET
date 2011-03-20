da9168857dd8554d02e4aa2a9de5eb9a9b098e78
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-03-20 19:19:02 +0000

    removed unused code

--- simulations-java/simulations/bending-light/src/edu/colorado/phet/bendinglight/modules/moretools/WaveSensor.java
+++ simulations-java/simulations/bending-light/src/edu/colorado/phet/bendinglight/modules/moretools/WaveSensor.java
@@ -18,40 +18,14 @@ import edu.colorado.phet.common.phetcommon.util.function.Function1;
  * @author Sam Reid
  */
 public class WaveSensor {
-    public final Probe probe1 = new Probe( -4.173076923076922E-7, 9.180769230769231E-7 ) {{
-//        series.setValue( new ArrayList<Option<ImmutableVector2D>>() {{
-//            for ( int i = 0; i < 200; i++ ) {
-//                add( new Option.Some<ImmutableVector2D>( new ImmutableVector2D( i / 100.0, Math.sin( i / 100.0 * 10 ) ) ) );
-//            }
-//        }} );
-    }};
-    public final Probe probe2 = new Probe( -1.5440384615384618E-6, -1.2936538461538458E-6 ) {{
-//        series.setValue( new ArrayList<Option<ImmutableVector2D>>() {{
-//            for ( int i = 0; i < 200; i++ ) {
-//                add( new Option.Some<ImmutableVector2D>( new ImmutableVector2D( i / 100.0, Math.cos( i / 100.0 * 10 ) ) ) );
-//            }
-//        }} );
-    }};
+    public final Probe probe1 = new Probe( -4.173076923076922E-7, 9.180769230769231E-7 );
+    public final Probe probe2 = new Probe( -1.5440384615384618E-6, -1.2936538461538458E-6 );
     public final Property<ImmutableVector2D> bodyPosition = new Property<ImmutableVector2D>( new ImmutableVector2D( 4.882500000000015E-6, -3.1298076923077013E-6 ) );
     public final Clock clock;
     public final BooleanProperty visible = new BooleanProperty( false );//in the play area
 
     public WaveSensor( final Clock clock, final Function1<ImmutableVector2D, Option<Double>> probe1Value, final Function1<ImmutableVector2D, Option<Double>> probe2Value ) {
         this.clock = clock;
-        //This code helps to come up with a good set of defaults for the values.
-//        public static String toCode( ImmutableVector2D v ) {
-//        return "new ImmutableVector2D(" + v.getX() + "," + v.getY() + ")";
-//    }
-//        final SimpleObserver simpleObserver = new SimpleObserver() {
-//            public void update() {
-//                System.out.println( "bodyPosition.setValue( " + toCode( bodyPosition.getValue() ) + ");" );
-//                System.out.println( "probe1.position.setValue( " + toCode( probe1.position.getValue() ) + ");" );
-//                System.out.println( "probe2.position.setValue( " + toCode( probe2.position.getValue() ) + ");" );
-//            }
-//        };
-//        probe1.position.addObserver( simpleObserver );
-//        probe2.position.addObserver( simpleObserver );
-
         clock.addClockListener( new ClockAdapter() {
             public void simulationTimeChanged( ClockEvent clockEvent ) {
                 updateProbeSample( probe1, probe1Value, clock );
