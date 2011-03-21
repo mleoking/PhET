99a2fb5e822a16efe64bd75effe7e6d06f34ae21
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-03-21 05:09:00 +0000

    cleanup fields in BendingLightModel

--- simulations-java/simulations/bending-light/src/edu/colorado/phet/bendinglight/model/MediumColorFactory.java
+++ simulations-java/simulations/bending-light/src/edu/colorado/phet/bendinglight/model/MediumColorFactory.java
@@ -9,22 +9,29 @@ import edu.colorado.phet.common.phetcommon.math.Function;
  * @author Sam Reid
  */
 public class MediumColorFactory {
+
+    public static final Color AIR_COLOR = Color.white;
+    public static final Color WATER_COLOR = new Color( 198, 226, 246 );
+    public static final Color GLASS_COLOR = new Color( 171, 169, 212 );
+    public static final Color DIAMOND_COLOR = new Color( 78, 79, 164 );
+
+
     //Maps index of refraction to color
     public static Color getColor( double indexOfRefractionForRed ) {
         if ( indexOfRefractionForRed < BendingLightModel.WATER.index() ) {
             double ratio = new Function.LinearFunction( 1.0, BendingLightModel.WATER.index(), 0, 1 ).evaluate( indexOfRefractionForRed );
-            return colorBlend( BendingLightModel.AIR_COLOR, BendingLightModel.WATER_COLOR, ratio );
+            return colorBlend( AIR_COLOR, WATER_COLOR, ratio );
         }
         else if ( indexOfRefractionForRed < BendingLightModel.GLASS.index() ) {
             double ratio = new Function.LinearFunction( BendingLightModel.WATER.index(), BendingLightModel.GLASS.index(), 0, 1 ).evaluate( indexOfRefractionForRed );
-            return colorBlend( BendingLightModel.WATER_COLOR, BendingLightModel.GLASS_COLOR, ratio );
+            return colorBlend( WATER_COLOR, GLASS_COLOR, ratio );
         }
         else if ( indexOfRefractionForRed < BendingLightModel.DIAMOND.index() ) {
             double ratio = new Function.LinearFunction( BendingLightModel.GLASS.index(), BendingLightModel.DIAMOND.index(), 0, 1 ).evaluate( indexOfRefractionForRed );
-            return colorBlend( BendingLightModel.GLASS_COLOR, BendingLightModel.DIAMOND_COLOR, ratio );
+            return colorBlend( GLASS_COLOR, DIAMOND_COLOR, ratio );
         }
         else {
-            return BendingLightModel.DIAMOND_COLOR;
+            return DIAMOND_COLOR;
         }
     }
 
