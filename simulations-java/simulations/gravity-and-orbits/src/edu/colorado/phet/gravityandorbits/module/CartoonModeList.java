c1a171adaf70546d6f7e0f1cbb0dc0136682111e
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-03-22 05:25:51 +0000

    Tuned cartoon initial conditions so that there are 12 cartoon lunar orbits in one cartoon earth orbit

--- simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/module/CartoonModeList.java
+++ simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/module/CartoonModeList.java
@@ -7,33 +7,27 @@ import edu.colorado.phet.common.phetcommon.model.property.Property;
  * @author Sam Reid
  */
 public class CartoonModeList extends ModeList {
+
     public CartoonModeList( Property<Boolean> clockPausedProperty, Property<Boolean> gravityEnabledProperty, Property<Boolean> stepping, Property<Boolean> rewinding, Property<Double> timeSpeedScaleProperty, final double alpha ) {
         super( new ModeListParameter( clockPausedProperty, gravityEnabledProperty, stepping, rewinding, timeSpeedScaleProperty ),
                new SunEarth() {{
+                   //This state copied from SunEarthMoon, should be kept in sync so the modes are similar
                    sun.radius *= 50;
-                   earth.radius *= 1100;
-                   //Make the sun move as it does in the SunEarthMoon mode
-                   final int earthMassScaleFactor = 20000;
+                   earth.radius *= 800;
+                   final int earthMassScaleFactor = 10200; //Tuned by hand so there are 12 cartoon lunar orbits in one cartoon earth orbit
                    earth.mass *= earthMassScaleFactor;
-                   forceScale *= 0.8 / earthMassScaleFactor * 0.75;//to balance increased mass
+                   forceScale *= 0.8 / earthMassScaleFactor * 0.75;//to balance increased mass and so that forces are 1/2 grid cell in default conditions
                }}, new SunEarthMoon() {{
                     sun.radius *= 50;
-                    earth.radius *= 1100;
+                    earth.radius *= 800;
                     moon.radius *= 800;
 
-                    final int earthMassScaleFactor = 20000;
+                    final int earthMassScaleFactor = 10200; //Tuned by hand so there are 12 cartoon lunar orbits in one cartoon earth orbit
                     earth.mass *= earthMassScaleFactor;
-                    moon.vx *= alpha;
-                    moon.y = earth.radius * 2;
-                    moon.mass *= earthMassScaleFactor;
-                    forceScale *= 0.8 / earthMassScaleFactor * 0.75;//to balance increased mass
+                    moon.vx *= 21;
+                    moon.y = earth.radius * 1.7;
 
-                    double distBetweenEarthAndMoon = earth.getPosition().minus( moon.getPosition() ).getMagnitude();
-                    System.out.println( "distBetweenEarthAndMoon = " + distBetweenEarthAndMoon );
-
-                    double distBetweenEarthAndSun = earth.getPosition().minus( sun.getPosition() ).getMagnitude();
-                    System.out.println( "distBetweenEarthAndSun = " + distBetweenEarthAndSun );
-
+                    forceScale *= 0.8 / earthMassScaleFactor * 0.75;//to balance increased mass and so that forces are 1/2 grid cell in default conditions
                 }}, new EarthMoon() {{
                     earth.radius *= 15;
                     moon.radius *= 15;
