293a357a4353aa7ac333c133476c8f2e99b0baa8
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-03-30 19:07:07 +0000

    Removed persistence support.

--- simulations-java/simulations/build-a-molecule/src/edu/colorado/phet/buildamolecule/BuildAMoleculeApplication.java
+++ simulations-java/simulations/build-a-molecule/src/edu/colorado/phet/buildamolecule/BuildAMoleculeApplication.java
@@ -2,15 +2,14 @@
 
 package edu.colorado.phet.buildamolecule;
 
-import java.awt.*;
+import java.awt.Frame;
 
-import javax.swing.*;
+import javax.swing.JMenu;
 
 import edu.colorado.phet.buildamolecule.module.MakeMoleculeModule;
 import edu.colorado.phet.common.phetcommon.application.Module;
 import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
 import edu.colorado.phet.common.phetcommon.application.PhetApplicationLauncher;
-import edu.colorado.phet.common.phetcommon.util.persistence.XMLPersistenceManager;
 import edu.colorado.phet.common.phetcommon.view.PhetFrame;
 import edu.colorado.phet.common.phetcommon.view.menu.OptionsMenu;
 import edu.colorado.phet.common.piccolophet.PiccoloPhetApplication;
@@ -23,9 +22,6 @@ public class BuildAMoleculeApplication extends PiccoloPhetApplication {
     //----------------------------------------------------------------------------
     // Instance data
     //----------------------------------------------------------------------------
-
-    // PersistanceManager is used to save/load simulation configurations.
-    private XMLPersistenceManager persistenceManager;
 
     //----------------------------------------------------------------------------
     // Constructors
@@ -66,12 +62,8 @@ public class BuildAMoleculeApplication extends PiccoloPhetApplication {
      */
     private void initMenubar() {
 
-        // File->Save/Load
+        // Create main frame.
         final PhetFrame frame = getPhetFrame();
-        frame.addFileSaveLoadMenuItems();
-        if ( persistenceManager == null ) {
-            persistenceManager = new XMLPersistenceManager( frame );
-        }
 
         // Options menu
         OptionsMenu optionsMenu = new OptionsMenu();
@@ -90,8 +82,8 @@ public class BuildAMoleculeApplication extends PiccoloPhetApplication {
     //----------------------------------------------------------------------------
 
     public static void main( final String[] args ) throws ClassNotFoundException {
-        /* 
-         * If you want to customize your application (look-&-feel, window size, etc) 
+        /*
+         * If you want to customize your application (look-&-feel, window size, etc)
          * create your own PhetApplicationConfig and use one of the other launchSim methods
          */
         new PhetApplicationLauncher().launchSim( args, BuildAMoleculeConstants.PROJECT_NAME, BuildAMoleculeApplication.class );
