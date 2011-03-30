64ae75370ae0301eecbddccb97589d1092b05a75
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-03-30 19:58:00 +0000

    Added canvases for the 2nd and 3rd tabs and put temporary images on them as a fast prototype sort of thing.

--- trunk/simulations-java/simulations/build-a-molecule/src/edu/colorado/phet/buildamolecule/module/LargerMoleculesModule.java
+++ trunk/simulations-java/simulations/build-a-molecule/src/edu/colorado/phet/buildamolecule/module/LargerMoleculesModule.java
@@ -2,10 +2,10 @@
 
 package edu.colorado.phet.buildamolecule.module;
 
-import java.awt.*;
+import java.awt.Frame;
 
 import edu.colorado.phet.buildamolecule.BuildAMoleculeStrings;
-import edu.colorado.phet.buildamolecule.view.BuildAMoleculeCanvas;
+import edu.colorado.phet.buildamolecule.view.LargerMoleculesCanvas;
 import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
 import edu.colorado.phet.common.piccolophet.PiccoloModule;
 
@@ -15,7 +15,7 @@ public class LargerMoleculesModule extends PiccoloModule {
     // Instance data
     //----------------------------------------------------------------------------
 
-    private final BuildAMoleculeCanvas canvas;
+    private final LargerMoleculesCanvas canvas;
 
     //----------------------------------------------------------------------------
     // Constructors
@@ -29,7 +29,7 @@ public class LargerMoleculesModule extends PiccoloModule {
         setClockControlPanel( null );
 
         // Canvas
-        canvas = new BuildAMoleculeCanvas();
+        canvas = new LargerMoleculesCanvas();
         setSimulationPanel( canvas );
 
         // Set initial state
