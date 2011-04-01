9fefd443042b86421495184963d863ef9a2987c3
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-04-01 21:40:25 +0000

    fix TestMoleculeNodes, it wouldn't launch from JNLP because the class was package private

--- trunk/simulations-java/simulations/balancing-chemical-equations/src/edu/colorado/phet/balancingchemicalequations/test/TestMoleculeNodes.java
+++ trunk/simulations-java/simulations/balancing-chemical-equations/src/edu/colorado/phet/balancingchemicalequations/test/TestMoleculeNodes.java
@@ -66,7 +66,7 @@ import edu.umd.cs.piccolox.pswing.PSwing;
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
-/* package private */ class TestMoleculeNodes extends JFrame {
+public class TestMoleculeNodes extends JFrame {
 
     public TestMoleculeNodes() {
         super( TestMoleculeNodes.class.getName() );
