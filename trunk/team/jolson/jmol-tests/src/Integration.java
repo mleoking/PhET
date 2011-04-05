809e309189c2b9d6085cfda8e93c503f03c05df8
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-04-05 21:45:48 +0000

    more jmol tests

--- trunk/team/jolson/jmol-tests/src/Integration.java
+++ trunk/team/jolson/jmol-tests/src/Integration.java
@@ -25,25 +25,22 @@ package org.jmol;
  *  02111-1307  USA.
  */
 
-import java.awt.BorderLayout;
-import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Rectangle;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.applet.Applet;
+import java.awt.*;
+import java.util.Arrays;
+import java.util.Map;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 import org.jmol.adapter.smarter.SmarterJmolAdapter;
+import org.jmol.api.JmolCallbackListener;
+import org.jmol.api.JmolStatusListener;
 import org.jmol.api.JmolViewer;
-import org.jmol.util.Logger;
 import org.openscience.jmol.app.jmolpanel.AppConsole;
 
 /**
  * A example of integrating the Jmol viewer into a java application, with optional console.
- *
+ * <p/>
  * <p>I compiled/ran this code directly in the examples directory by doing:
  * <pre>
  * javac -classpath ../Jmol.jar Integration.java
@@ -55,85 +52,132 @@ import org.openscience.jmol.app.jmolpanel.AppConsole;
 
 public class Integration {
 
-  /*
-   * Demonstrates a simple way to include an optional console along with the applet.
-   * 
-   */
-  public static void main(String[] argv) {
-    JFrame frame = new JFrame("Hello");
-    frame.addWindowListener(new ApplicationCloser());
-    frame.setSize(410, 700);
-    Container contentPane = frame.getContentPane();
-    JmolPanel jmolPanel = new JmolPanel();
-    jmolPanel.setPreferredSize(new Dimension(400, 400));
+    /*
+    * Demonstrates a simple way to include an optional console along with the applet.
+    *
+    */
+    public static void main( String[] argv ) {
+        JFrame frame = new JFrame( "Hello" );
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        frame.setSize( 410, 700 );
+        Container contentPane = frame.getContentPane();
+        JmolPanel jmolPanel = new JmolPanel();
+        jmolPanel.setPreferredSize( new Dimension( 400, 400 ) );
 
-    // main panel -- Jmol panel on top
+        // main panel -- Jmol panel on top
 
-    JPanel panel = new JPanel();
-    panel.setLayout(new BorderLayout());
-    panel.add(jmolPanel);
-    
-    // main panel -- console panel on bottom
-    
-    JPanel panel2 = new JPanel();
-    panel2.setLayout(new BorderLayout());
-    panel2.setPreferredSize(new Dimension(400, 300));
-    AppConsole console = new AppConsole(jmolPanel.viewer, panel2,
-        "History State Clear");
-    
-    // You can use a different JmolStatusListener or JmolCallbackListener interface
-    // if you want to, but AppConsole itself should take care of any console-related callbacks
-    jmolPanel.viewer.setJmolCallbackListener(console);
-    
-    panel.add("South", panel2);
-    
-    contentPane.add(panel);
-    frame.setVisible(true);
+        JPanel panel = new JPanel();
+        panel.setLayout( new BorderLayout() );
+        panel.add( jmolPanel );
 
-    // sample start-up script
-    
-    String strError = jmolPanel.viewer
-        .openFile("http://chemapps.stolaf.edu/jmol/docs/examples-11/data/caffeine.xyz");
-    //viewer.openStringInline(strXyzHOH);
-    if (strError == null)
-      jmolPanel.viewer.evalString(strScript);
-    else
-      Logger.error(strError);
-  }
+        // main panel -- console panel on bottom
 
-  final static String strXyzHOH = "3\n" 
-    + "water\n" 
-    + "O  0.0 0.0 0.0\n"
-    + "H  0.76923955 -0.59357141 0.0\n" 
-    + "H -0.76923955 -0.59357141 0.0\n";
+        JPanel panel2 = new JPanel();
+        panel2.setLayout( new BorderLayout() );
+        panel2.setPreferredSize( new Dimension( 400, 300 ) );
+        AppConsole console = new AppConsole( jmolPanel.viewer, panel2,
+                                             "History State Clear" );
 
-  final static String strScript = "delay; move 360 0 0 0 0 0 0 0 4;";
+        // You can use a different JmolStatusListener or JmolCallbackListener interface
+        // if you want to, but AppConsole itself should take care of any console-related callbacks
+        //jmolPanel.viewer.setJmolCallbackListener( console );
+        jmolPanel.viewer.setJmolCallbackListener( new JmolCallbackListener() {
+            public void setCallbackFunction( String s, String s1 ) {
+                System.out.println( "callback: setCallbackFunction: " + s + ", " + s1 );
+            }
 
-  static class ApplicationCloser extends WindowAdapter {
-    @Override
-    public void windowClosing(WindowEvent e) {
-      System.exit(0);
-    }
-  }
+            public void notifyCallback( int i, Object[] objects ) {
+                System.out.println( "callback: notifyCallback: " + i + ", " + Arrays.toString( objects ) );
+            }
 
-  static class JmolPanel extends JPanel {
+            public boolean notifyEnabled( int i ) {
+                System.out.println( "callback: notifyEnabled: " + i );
+                return true;
+            }
+        } );
 
-    JmolViewer viewer;
-    
-    private final Dimension currentSize = new Dimension();
-    private final Rectangle rectClip = new Rectangle(); // ignored by Jmol
-    
-    JmolPanel() {
-      viewer = JmolViewer.allocateViewer(this, new SmarterJmolAdapter(), 
-          null, null, null, null, null);
+        panel.add( "South", panel2 );
+
+        contentPane.add( panel );
+        frame.setVisible( true );
+
+        // sample start-up script
+
+        //String strError = jmolPanel.viewer.openFile( "http://chemapps.stolaf.edu/jmol/docs/examples-11/data/caffeine.xyz" );
+        String strError = jmolPanel.viewer.openStringInline( strXyzHOH );
+        //viewer.openStringInline(strXyzHOH);
+        //if ( strError == null ) { jmolPanel.viewer.evalString( strScript ); }
+        //else { Logger.error( strError ); }
+
+        //jmolPanel.viewer.script( "wireframe off; spacefill on;" ); // space fill
+        //jmolPanel.viewer.script( "wireframe 0.2; spacefill 25%" ); // ball and stick
+        //jmolPanel.viewer.script( "wireframe off; spacefill 25%" ); // no bonds
+        jmolPanel.viewer.script( "wireframe off; spacefill 50%" ); // no bonds
+        // set bonds on
+        // also available: spacefill ionic
+
+
     }
 
-    @Override
-    public void paint(Graphics g) {
-      getSize(currentSize);
-      g.getClipBounds(rectClip);
-      viewer.renderScreenImage(g, currentSize, rectClip);
+    final static String strXyzHOH = "3\n"
+                                    + "water\n"
+                                    + "O  0.0 0.0 0.0\n"
+                                    + "H  0.76923955 -0.59357141 0.0\n"
+                                    + "H -0.76923955 -0.59357141 0.0\n";
+
+    final static String strScript = "delay; move 360 0 0 0 0 0 0 0 4;";
+
+    static class JmolPanel extends JPanel {
+
+        JmolViewer viewer;
+
+        private final Dimension currentSize = new Dimension();
+        private final Rectangle rectClip = new Rectangle(); // ignored by Jmol
+
+        JmolPanel() {
+            viewer = JmolViewer.allocateViewer( this, new SmarterJmolAdapter(),
+                                                null, null, null, null, new JmolStatusListener() {
+                        public String eval( String s ) {
+                            return null;
+                        }
+
+                        public float[][] functionXY( String s, int i, int i1 ) {
+                            return new float[0][];
+                        }
+
+                        public float[][][] functionXYZ( String s, int i, int i1, int i2 ) {
+                            return new float[0][][];
+                        }
+
+                        public String createImage( String s, String s1, Object o, int i ) {
+                            return null;
+                        }
+
+                        public Map<String, Applet> getRegistryInfo() {
+                            return null;
+                        }
+
+                        public void showUrl( String s ) {
+                        }
+
+                        public void setCallbackFunction( String s, String s1 ) {
+                        }
+
+                        public void notifyCallback( int i, Object[] objects ) {
+                        }
+
+                        public boolean notifyEnabled( int i ) {
+                            return false;
+                        }
+                    } );
+        }
+
+        @Override
+        public void paint( Graphics g ) {
+            getSize( currentSize );
+            g.getClipBounds( rectClip );
+            viewer.renderScreenImage( g, currentSize, rectClip );
+        }
     }
-  }
 
 }
