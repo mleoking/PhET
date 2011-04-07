f81d9aeb1914dda0a9664ca370bb19a64e253124
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-04-07 21:54:45 +0000

    Renamed "sun" to "star"

--- simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/GAOStrings.java
+++ simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/GAOStrings.java
@@ -44,7 +44,7 @@ public class GAOStrings {
     public static final String SATELLITE = getString( "satellite" );
     public static final String SHOW = getString( "show" );
     public static final String RESET = getString( "reset" );
-    public static final String SUN = getString( "sun" );
+    public static final String STAR = getString( "star" );
     public static final String THOUSAND_EARTH_MASSES = getString( "thousandEarthMasses" );
     public static final String THOUSAND_MILES = getString( "thousandMiles" );
     public static final String VELOCITY = getString( "velocity" );
