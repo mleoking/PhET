4c6367fae955178007ffbdc0f84930116fa6d72d
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-04-10 01:29:33 +0000

    #2796, keeping removeChild for symmetry with addChild. I typically create add/remove methods (for nodes, listeners, etc) in pairs.

--- simulations-java/simulations/balancing-chemical-equations/src/edu/colorado/phet/balancingchemicalequations/view/BCECanvas.java
+++ simulations-java/simulations/balancing-chemical-equations/src/edu/colorado/phet/balancingchemicalequations/view/BCECanvas.java
@@ -41,7 +41,6 @@ public abstract class BCECanvas extends PhetPCanvas {
         rootNode.addChild( node );
     }
 
-    //REVIEW: remove if unused
     protected void removeChild( PNode node ) {
         if ( node != null && rootNode.indexOfChild( node ) != -1 ) {
             rootNode.removeChild( node );
