ff69dc341d6656bc9914f94d4cfa34672028dad9
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-04-10 01:03:47 +0000

    #2796, remove unused parameters from NotBalancedTerseNode constructors

--- trunk/simulations-java/simulations/balancing-chemical-equations/src/edu/colorado/phet/balancingchemicalequations/view/game/NotBalancedTerseNode.java
+++ trunk/simulations-java/simulations/balancing-chemical-equations/src/edu/colorado/phet/balancingchemicalequations/view/game/NotBalancedTerseNode.java
@@ -14,10 +14,6 @@ import edu.colorado.phet.balancingchemicalequations.BCEConstants;
 import edu.colorado.phet.balancingchemicalequations.BCEGlobalProperties;
 import edu.colorado.phet.balancingchemicalequations.BCEResources;
 import edu.colorado.phet.balancingchemicalequations.BCEStrings;
-import edu.colorado.phet.balancingchemicalequations.model.DisplacementEquation.Displacement_CH4_2O2_CO2_2H2O;
-import edu.colorado.phet.balancingchemicalequations.model.Equation;
-import edu.colorado.phet.balancingchemicalequations.model.EquationTerm;
-import edu.colorado.phet.balancingchemicalequations.view.HorizontalAligner;
 import edu.colorado.phet.common.phetcommon.util.function.Function1;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.PhetPCanvas;
@@ -47,20 +43,17 @@ public class NotBalancedTerseNode extends GamePopupNode {
      * @param whyButtonListener
      * @param aligner
      */
-    public NotBalancedTerseNode( final Equation equation, BCEGlobalProperties globalProperties, final ActionListener whyButtonListener, final HorizontalAligner aligner ) {
-        this( equation, globalProperties.popupsCloseButtonVisible.getValue(), globalProperties.popupsTitleBarVisible.getValue(),
-                globalProperties.popupsWhyButtonVisible.getValue(), whyButtonListener, aligner );
+    public NotBalancedTerseNode( BCEGlobalProperties globalProperties, final ActionListener whyButtonListener ) {
+        this( globalProperties.popupsCloseButtonVisible.getValue(), globalProperties.popupsTitleBarVisible.getValue(),
+              globalProperties.popupsWhyButtonVisible.getValue(), whyButtonListener );
     }
 
     /*
-     * @param equation the equation
      * @param closeButtonVisible
      * @param titleBarVisible
      * @param whyButtonVisible
-     * @param aligner specifies horizontal layout, for aligning with other user-interface components
      */
-    private NotBalancedTerseNode( final Equation equation, boolean closeButtonVisible, boolean titleBarVisible, final boolean whyButtonVisible,//REVIEW: why is argument equation unused?
-            final ActionListener whyButtonListener, final HorizontalAligner aligner ) {//REVIEW: why is argument aligner unused?
+    private NotBalancedTerseNode( boolean closeButtonVisible, boolean titleBarVisible, final boolean whyButtonVisible, final ActionListener whyButtonListener ) {
         super( false /* smile */, closeButtonVisible, titleBarVisible, new Function1<PhetFont, PNode>() {
             public PNode apply( PhetFont font ) {
                 PNode parentNode = new PNode();
@@ -107,22 +100,12 @@ public class NotBalancedTerseNode extends GamePopupNode {
         PhetPCanvas canvas = new PhetPCanvas( BCEConstants.CANVAS_RENDERING_SIZE );
         canvas.setPreferredSize( new Dimension( 1024, 768 ) );
 
-        Equation equation = new Displacement_CH4_2O2_CO2_2H2O();
-        EquationTerm[] reactants = equation.getReactants();
-        for ( EquationTerm term : reactants ) {
-            term.setUserCoefficient( 15 );
-        }
-        EquationTerm[] products = equation.getProducts();
-        for ( EquationTerm term : products ) {
-            term.setUserCoefficient( 15 );
-        }
-
         ActionListener whyButtonListener = new ActionListener() {
             public void actionPerformed( ActionEvent e ) {
                 System.out.println( "Show Why button pressed" );
             }
         };
-        NotBalancedTerseNode node = new NotBalancedTerseNode( equation, false, false, true, whyButtonListener, new HorizontalAligner( new Dimension( 475, 400 ), 90 ) );
+        NotBalancedTerseNode node = new NotBalancedTerseNode( false, false, true, whyButtonListener );
         node.setOffset( 20, 200 );
         canvas.addWorldChild( node );
 
