7f4308a60c559997277f04d37a68f7aff48e7c2e
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-04-11 19:14:48 +0000

    add meters to Multi-Cap canvas

--- simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/module/multiplecapacitors/MultipleCapacitorsCanvas.java
+++ simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/module/multiplecapacitors/MultipleCapacitorsCanvas.java
@@ -2,8 +2,12 @@
 
 package edu.colorado.phet.capacitorlab.module.multiplecapacitors;
 
+import java.awt.geom.Dimension2D;
+
 import edu.colorado.phet.capacitorlab.model.CLModelViewTransform3D;
 import edu.colorado.phet.capacitorlab.module.CLCanvas;
+import edu.colorado.phet.capacitorlab.view.meters.*;
+import edu.colorado.phet.common.phetcommon.math.Point3D;
 
 /**
  * Canvas for the "Multiple Capacitors" module.
@@ -12,11 +16,60 @@ import edu.colorado.phet.capacitorlab.module.CLCanvas;
  */
 public class MultipleCapacitorsCanvas extends CLCanvas {
 
+    private final MultipleCapacitorsModel model;
+    private final CLModelViewTransform3D mvt;
+
+    // meters
+    private final CapacitanceMeterNode capacitanceMeterNode;
+    private final PlateChargeMeterNode plateChargeMeterNode;
+    private final StoredEnergyMeterNode storedEnergyMeterNode;
+    private final VoltmeterView voltmeter;
+    private final EFieldDetectorView eFieldDetector;
+
     public MultipleCapacitorsCanvas( MultipleCapacitorsModel model, CLModelViewTransform3D mvt, boolean dev ) {
-        //XXX
+
+        this.model = model;
+        this.mvt = mvt;
+
+        // maximums
+        final double eFieldVectorReferenceMagnitude = 1; //XXX
+
+        capacitanceMeterNode = new CapacitanceMeterNode( model.getCapacitanceMeter(), mvt );
+        plateChargeMeterNode = new PlateChargeMeterNode( model.getPlateChargeMeter(), mvt );
+        storedEnergyMeterNode = new StoredEnergyMeterNode( model.getStoredEnergyMeter(), mvt );
+        voltmeter = new VoltmeterView( model.getVoltmeter(), mvt );
+        eFieldDetector = new EFieldDetectorView( model.getEFieldDetector(), mvt, eFieldVectorReferenceMagnitude, dev );
+
+        // rendering order
+        addChild( capacitanceMeterNode );
+        addChild( plateChargeMeterNode );
+        addChild( storedEnergyMeterNode );
+        addChild( eFieldDetector.getBodyNode() );
+        addChild( eFieldDetector.getWireNode() );
+        addChild( eFieldDetector.getProbeNode() );
+        addChild( voltmeter.getBodyNode() );
+        addChild( voltmeter.getPositiveProbeNode() );
+        addChild( voltmeter.getPositiveWireNode() );
+        addChild( voltmeter.getNegativeProbeNode() );
+        addChild( voltmeter.getNegativeWireNode() );
     }
 
     public void reset() {
-        //XXX
+        //TODO anything to reset?
+    }
+
+    @Override
+    protected void updateLayout() {
+        super.updateLayout();
+
+        Dimension2D worldSize = getWorldSize();
+        if ( worldSize.getWidth() <= 0 || worldSize.getHeight() <= 0 ) {
+            // canvas hasn't been sized, blow off layout
+            return;
+        }
+
+        // adjust the model bounds
+        Point3D p = mvt.viewToModelDelta( worldSize.getWidth(), worldSize.getHeight() );
+        model.getWorld().setBounds( 0, 0, p.getX(), p.getY() );
     }
 }
