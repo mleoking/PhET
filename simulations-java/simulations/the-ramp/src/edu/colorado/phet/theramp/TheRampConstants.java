da7df833f0dafc20218abc8b0866342843870b2c
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-04-11 21:17:12 +0000

    > Set initial frame size to be 1024x768 instead of MAX_EXTENT

--- simulations-java/simulations/the-ramp/src/edu/colorado/phet/theramp/TheRampConstants.java
+++ simulations-java/simulations/the-ramp/src/edu/colorado/phet/theramp/TheRampConstants.java
@@ -7,6 +7,6 @@ import edu.colorado.phet.common.phetcommon.view.util.FrameSetup;
 public class TheRampConstants {
 
     public static final String PROJECT_NAME = "the-ramp";
-    
-    public static final FrameSetup FRAME_SETUP = new FrameSetup.MaxExtent( new FrameSetup.CenteredWithSize( 800, 600 ) );
+
+    public static final FrameSetup FRAME_SETUP = new FrameSetup.CenteredWithSize( 1024, 768 );
 }
