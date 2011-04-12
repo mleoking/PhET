1b24ab1cb4fb1add59ac3dae3c349e8fee44e553
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-04-12 21:50:58 +0000

    bam: bucket work

--- simulations-java/simulations/build-a-molecule/src/edu/colorado/phet/buildamolecule/model/Kit.java
+++ simulations-java/simulations/build-a-molecule/src/edu/colorado/phet/buildamolecule/model/Kit.java
@@ -1,5 +1,6 @@
 package edu.colorado.phet.buildamolecule.model;
 
+import java.awt.geom.Point2D;
 import java.util.List;
 
 import edu.colorado.phet.buildamolecule.model.buckets.Bucket;
@@ -10,8 +11,31 @@ import edu.colorado.phet.buildamolecule.model.buckets.Bucket;
 public class Kit {
     private final List<Bucket> buckets;
 
+    public static final double KIT_Y = -250;
+    public static final double KIT_X_CENTER = -100;
+    public static final double BUCKET_PADDING = 50;
+
     public Kit( List<Bucket> buckets ) {
         this.buckets = buckets;
+
+        double usedWidth = 0;
+
+        // lays out all of the buckets from the left to right
+        for ( int i = 0; i < buckets.size(); i++ ) {
+            Bucket bucket = buckets.get( i );
+            if ( i != 0 ) {
+                usedWidth += BUCKET_PADDING;
+            }
+            bucket.setPosition( new Point2D.Double( usedWidth, KIT_Y ) );
+            usedWidth += bucket.getWidth();
+        }
+
+        // centers the buckets horizontally within the kit
+        for ( Bucket bucket : buckets ) {
+            Point2D.Double bucketX = new Point2D.Double( bucket.getPosition().getX() - usedWidth / 2 + KIT_X_CENTER + bucket.getWidth() / 2, KIT_Y );
+            System.out.println( "bucketX = " + bucketX );
+            bucket.setPosition( bucketX );
+        }
     }
 
     public List<Bucket> getBuckets() {
