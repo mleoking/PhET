217e48815196e15b3850296935bf735eb888e7bc
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-04-13 02:43:53 +0000

    phetcommon: adding in rectangle transformations into ModelViewTransform

--- trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/view/graphics/transforms/ModelViewTransform.java
+++ trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/view/graphics/transforms/ModelViewTransform.java
@@ -130,6 +130,23 @@ public class ModelViewTransform {
         return new Dimension2DDouble( pt.getX(), pt.getY() );
     }
 
+    /**
+     * Transform a rectangle from model to view coordinates, with ranges [x, x + width] and [y, y + height] with width>=0, height>=0
+     *
+     * @param r Input rectangle
+     * @return Transformed rectangle
+     */
+    public Rectangle2D modelToViewRectangle( Rectangle2D r ) {
+        // we must compensate for negative scales. we need positive width and height
+        double height = Math.abs( modelToViewDeltaY( r.getHeight() ) );
+        double width = Math.abs( modelToViewDeltaX( r.getWidth() ) );
+
+        // if scale is negative, we actually need to flip which "corner" x or y points to. we do this using the transformed width or height
+        double x = modelToViewX( r.getX() ) - ( getTransform().getScaleX() < 0 ? width : 0 );
+        double y = modelToViewY( r.getY() ) - ( getTransform().getScaleY() < 0 ? height : 0 );
+        return new Rectangle2D.Double( x, y, width, height );
+    }
+
     /*---------------------------------------------------------------------------*
     * View to Model transforms
     *----------------------------------------------------------------------------*/
@@ -171,7 +188,7 @@ public class ModelViewTransform {
         try {
             return transform.createInverse();
         }
-        catch ( NoninvertibleTransformException e ) {
+        catch( NoninvertibleTransformException e ) {
             throw new RuntimeException( e );
         }
     }
@@ -180,9 +197,34 @@ public class ModelViewTransform {
         return getInverseTransform().createTransformedShape( shape );
     }
 
+    public double viewToModelDeltaX( double x ) {
+        return viewToModelDelta( new Point2D.Double( x, 0 ) ).getX();
+    }
+
+    public double viewToModelDeltaY( double y ) {
+        return viewToModelDelta( new Point2D.Double( 0, y ) ).getY();
+    }
+
     public Dimension2DDouble viewToModel( Dimension2D delta ) {
         final Point2D point2D = viewToModel( delta.getWidth(), delta.getHeight() );
         return new Dimension2DDouble( point2D.getX(), point2D.getY() );
+    }
+
+    /**
+     * Transform a rectangle from view to model coordinates, with ranges [x, x + width] and [y, y + height] with width>=0, height>=0
+     *
+     * @param r Input rectangle
+     * @return Transformed rectangle
+     */
+    public Rectangle2D viewToModelRectangle( Rectangle2D r ) {
+        // we must compensate for negative scales. we need positive width and height
+        double height = Math.abs( viewToModelDeltaY( r.getHeight() ) );
+        double width = Math.abs( viewToModelDeltaX( r.getWidth() ) );
+
+        // if scale is negative, we actually need to flip which "corner" x or y points to. we do this using the transformed width or height
+        double x = viewToModelX( r.getX() ) - ( getTransform().getScaleX() < 0 ? width : 0 );
+        double y = viewToModelY( r.getY() ) - ( getTransform().getScaleY() < 0 ? height : 0 );
+        return new Rectangle2D.Double( x, y, width, height );
     }
 
     //Machine generated (and fine-tuned) equality test
