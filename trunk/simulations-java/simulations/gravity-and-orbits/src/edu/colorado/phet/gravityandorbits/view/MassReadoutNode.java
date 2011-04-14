d1098b31641184c32c4e278a659b2e14942241d0
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-04-14 03:15:59 +0000

    removed unused string, fixed PText initialization and made BodyNode final, see #2801

--- trunk/simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/view/MassReadoutNode.java
+++ trunk/simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/view/MassReadoutNode.java
@@ -5,14 +5,12 @@ package edu.colorado.phet.gravityandorbits.view;
 import java.awt.*;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
-import java.text.MessageFormat;
 
 import edu.colorado.phet.common.phetcommon.model.property.And;
 import edu.colorado.phet.common.phetcommon.model.property.Not;
 import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
-import edu.colorado.phet.gravityandorbits.GAOStrings;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.nodes.PText;
 import edu.umd.cs.piccolo.util.PBounds;
@@ -25,12 +23,11 @@ import edu.umd.cs.piccolo.util.PBounds;
 public abstract class MassReadoutNode extends PNode {
 
     //REVIEW all subclasses use bodyNode.getBody().getMass(), so why not store the body?
-    //REVIEW final?
-    protected BodyNode bodyNode;
+    protected final BodyNode bodyNode;
 
     public MassReadoutNode( final BodyNode bodyNode, final Property<Boolean> visible ) {
         this.bodyNode = bodyNode;
-        addChild( new PText( MessageFormat.format( GAOStrings.PATTERN_VALUE_UNITS, 1, GAOStrings.MILLION_EARTH_MASSES ) ) {{
+        addChild( new PText( createText() ) {{
             setPickable( false );
             setChildrenPickable( false );
             setFont( new PhetFont( 18, true ) );
