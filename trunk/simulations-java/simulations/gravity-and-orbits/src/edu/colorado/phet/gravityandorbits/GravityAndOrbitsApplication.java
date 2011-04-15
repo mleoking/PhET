8518c9b65c94644b09f6d2c124788635a4395b68
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-04-15 15:31:23 +0000

    factored out subclasses for modules, see #2801

--- trunk/simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/GravityAndOrbitsApplication.java
+++ trunk/simulations-java/simulations/gravity-and-orbits/src/edu/colorado/phet/gravityandorbits/GravityAndOrbitsApplication.java
@@ -8,6 +8,7 @@ import edu.colorado.phet.common.phetcommon.application.PhetApplicationLauncher;
 import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.phetcommon.resources.PhetResources;
 import edu.colorado.phet.common.phetcommon.util.function.Function1;
+import edu.colorado.phet.common.phetcommon.view.PhetFrame;
 import edu.colorado.phet.common.phetcommon.view.menu.OptionsMenu;
 import edu.colorado.phet.common.piccolophet.PiccoloPhetApplication;
 import edu.colorado.phet.gravityandorbits.module.*;
@@ -18,6 +19,7 @@ import static edu.colorado.phet.gravityandorbits.GAOStrings.TO_SCALE;
 /**
  * The main application for Gravity and Orbits.
  *
+ * @author Sam Reid
  * @see GravityAndOrbitsModule
  */
 public class GravityAndOrbitsApplication extends PiccoloPhetApplication {
@@ -28,40 +30,38 @@ public class GravityAndOrbitsApplication extends PiccoloPhetApplication {
     private final GravityAndOrbitsModule toScale;
     private final Property<Boolean> whiteBackgroundProperty = new Property<Boolean>( false );
 
-    //REVIEW This constructor is difficult to read than it needs to be. Why not encapsulate in 2 subclasses of GravityAndOrbitsModule? Let's discuss...
     public GravityAndOrbitsApplication( PhetApplicationConfig config ) {
         super( config );
         //Modules are stored so the data can be read and set for simsharing
-        intro = new GravityAndOrbitsModule( getPhetFrame(), whiteBackgroundProperty, CARTOON, false, new Function1<ModeListParameter, ArrayList<GravityAndOrbitsMode>>() {
-            public ArrayList<GravityAndOrbitsMode> apply( ModeListParameter p ) {
-                return new CartoonModeList( p.clockPausedProperty, p.gravityEnabledProperty, p.stepping, p.rewinding, p.timeSpeedScaleProperty, 1 );
-            }
-        }, 0, false );
+        intro = new IntroModule( getPhetFrame(), whiteBackgroundProperty );
         addModule( intro );
 
-        //@REVIEW: Like this? Does it solve the problem you described?
-//        intro = new IntroModule( getPhetFrame(), whiteBackgroundProperty );
-//        addModule( intro );
-
-        toScale = new GravityAndOrbitsModule( getPhetFrame(), whiteBackgroundProperty, TO_SCALE, true, new Function1<ModeListParameter, ArrayList<GravityAndOrbitsMode>>() {
-            public ArrayList<GravityAndOrbitsMode> apply( ModeListParameter p ) {
-                return new RealModeList( p.clockPausedProperty, p.gravityEnabledProperty, p.stepping, p.rewinding, p.timeSpeedScaleProperty );
-            }
-        }, 3, true );//Start Real tab in earth/satellite mode because it is more playful
+        toScale = new CartoonModule( getPhetFrame(), whiteBackgroundProperty );
         addModule( toScale );
+
         getPhetFrame().addMenu( new OptionsMenu() {{addWhiteBackgroundCheckBoxMenuItem( whiteBackgroundProperty );}} );
     }
 
-    //@REVIEW: Like this? Does it solve the problem you described?
-//    public static class IntroModule extends GravityAndOrbitsModule {
-//        public IntroModule( final PhetFrame phetFrame, Property<Boolean> whiteBackgroundProperty ) {
-//            super( phetFrame, whiteBackgroundProperty, CARTOON, false, new Function1<ModeListParameter, ArrayList<GravityAndOrbitsMode>>() {
-//                public ArrayList<GravityAndOrbitsMode> apply( ModeListParameter p ) {
-//                    return new CartoonModeList( p.clockPausedProperty, p.gravityEnabledProperty, p.stepping, p.rewinding, p.timeSpeedScaleProperty, 1 );
-//                }
-//            }, 0, false );
-//        }
-//    }
+    public static class IntroModule extends GravityAndOrbitsModule {
+        public IntroModule( final PhetFrame phetFrame, Property<Boolean> whiteBackgroundProperty ) {
+            super( phetFrame, whiteBackgroundProperty, CARTOON, false, new Function1<ModeListParameter, ArrayList<GravityAndOrbitsMode>>() {
+                public ArrayList<GravityAndOrbitsMode> apply( ModeListParameter p ) {
+                    return new CartoonModeList( p.clockPausedProperty, p.gravityEnabledProperty, p.stepping, p.rewinding, p.timeSpeedScaleProperty, 1 );
+                }
+            }, 0, false );
+        }
+    }
+
+    public static class CartoonModule extends GravityAndOrbitsModule {
+        public CartoonModule( final PhetFrame phetFrame, Property<Boolean> whiteBackgroundProperty ) {
+            super( phetFrame, whiteBackgroundProperty, TO_SCALE, true, new Function1<ModeListParameter, ArrayList<GravityAndOrbitsMode>>() {
+                public ArrayList<GravityAndOrbitsMode> apply( ModeListParameter p ) {
+                    return new RealModeList( p.clockPausedProperty, p.gravityEnabledProperty, p.stepping, p.rewinding, p.timeSpeedScaleProperty );
+                }
+            }, 3,//Start Real tab in earth/satellite mode because it is more playful
+                   true );
+        }
+    }
 
     public GravityAndOrbitsModule getIntro() {
         return intro;
