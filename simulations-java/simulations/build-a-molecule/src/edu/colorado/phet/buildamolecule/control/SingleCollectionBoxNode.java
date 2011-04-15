4b9692ae559198e1e9dbd9caf00e487fa70058c5
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-04-15 23:34:13 +0000

    bam: show BCE-style molecules in the collection area

--- simulations-java/simulations/build-a-molecule/src/edu/colorado/phet/buildamolecule/control/SingleCollectionBoxNode.java
+++ simulations-java/simulations/build-a-molecule/src/edu/colorado/phet/buildamolecule/control/SingleCollectionBoxNode.java
@@ -8,9 +8,12 @@ import java.awt.geom.Rectangle2D;
 import edu.colorado.phet.buildamolecule.model.CollectionBox;
 import edu.colorado.phet.buildamolecule.view.BuildAMoleculeCanvas;
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
+import edu.colorado.phet.common.phetcommon.util.function.VoidFunction2;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.nodes.HTMLNode;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
+import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.util.PBounds;
 import edu.umd.cs.piccolo.util.PDimension;
 import edu.umd.cs.piccolox.swing.SwingLayoutNode;
 
@@ -31,7 +34,7 @@ public class SingleCollectionBoxNode extends SwingLayoutNode {
 
         c.gridy = 1;
         c.insets = new Insets( 3, 0, 0, 0 ); // some padding between the two
-        addChild( new PhetPPath( new Rectangle2D.Double( 0, 0, 160, 50 ), Color.BLACK ) {{
+        final PhetPPath blackBox = new PhetPPath( new Rectangle2D.Double( 0, 0, 160, 50 ), Color.BLACK ) {{
             canvas.addFullyLayedOutObserver( new SimpleObserver() {
                 public void update() {
                     // we need to pass the collection box model coordinates, but here we have relative piccolo coordinates
@@ -55,11 +58,19 @@ public class SingleCollectionBoxNode extends SwingLayoutNode {
                     box.setDropBounds( canvas.getModelViewTransform().viewToModel( viewBounds ).getBounds2D() );
                 }
             } );
-        }}, c );
+        }};
+        addChild( blackBox, c );
 
-        box.quantity.addObserver( new SimpleObserver() {
-            public void update() {
-                // TODO: add in updating of view here (show the molecule!!!)
+        box.quantity.addObserver( new VoidFunction2<Integer, Integer>() {
+            public void apply( Integer newValue, Integer oldValue ) {
+                if ( newValue > oldValue ) {
+                    PNode pseudo3DNode = box.getMoleculeType().createPseudo3DNode();
+                    PBounds blackBoxFullBounds = blackBox.getFullBounds();
+                    pseudo3DNode.centerFullBoundsOnPoint(
+                            blackBoxFullBounds.getCenterX() - blackBoxFullBounds.getX(),
+                            blackBoxFullBounds.getCenterY() - blackBoxFullBounds.getY() );
+                    blackBox.addChild( pseudo3DNode );
+                }
             }
         } );
     }
