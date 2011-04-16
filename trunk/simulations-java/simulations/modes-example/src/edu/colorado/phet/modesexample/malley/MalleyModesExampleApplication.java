a7ae5c49abfaaac27d5b0f474f73c25e187b81ce
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-04-16 19:40:54 +0000

    add high-level architecture description to main class javadoc

--- trunk/simulations-java/simulations/modes-example/src/edu/colorado/phet/modesexample/malley/MalleyModesExampleApplication.java
+++ trunk/simulations-java/simulations/modes-example/src/edu/colorado/phet/modesexample/malley/MalleyModesExampleApplication.java
@@ -8,6 +8,20 @@ import edu.colorado.phet.common.piccolophet.PiccoloPhetApplication;
 
 /**
  * Top-level class to demonstrate my mode architecture.
+ * <p/>
+ * In this architecture, a mode is a lightweight container for the things that change when the mode changes.
+ * A mode has no responsibility for creating or interacting with these things, it is solely a container.
+ * As such, it introduces no new coupling, and allows us to use the MVC paradigm typical of mode-less PhET sims.
+ * <p/>
+ * As in mode-less sims, the Module has responsibility for instantiating the model, view and control components.
+ * The Module also has responsibility for creating the modes, and watching for mode changes.
+ * Watching for mode changes is done via a Property<T> called currentMode.
+ * When currentMode changes, the Module is responsible for swapping the things that need to change
+ * (eg, model, canvas, control panel, part of a control panel,... whatever) and it gets these things
+ * from the Mode container.
+ * <p/>
+ * In the user interface, mode is changed via a control, in this case a set of radio buttons.
+ * Those radio buttons control the value of the Module's currentMode property, which is Module is observing.
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
