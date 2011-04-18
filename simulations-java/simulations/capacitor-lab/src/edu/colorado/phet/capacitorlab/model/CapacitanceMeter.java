d84bec5e8ff7f6177f29b142cb3496d952d6d3c7
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-04-18 21:20:15 +0000

    replace abstract method with function parameter in BarMeter base class

--- simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/model/CapacitanceMeter.java
+++ simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/model/CapacitanceMeter.java
@@ -3,6 +3,7 @@
 package edu.colorado.phet.capacitorlab.model;
 
 import edu.colorado.phet.common.phetcommon.math.Point3D;
+import edu.colorado.phet.common.phetcommon.util.function.Function1;
 
 /**
  * Model element for the Capacitance meter.
@@ -12,11 +13,10 @@ import edu.colorado.phet.common.phetcommon.math.Point3D;
 public class CapacitanceMeter extends BarMeter {
 
     public CapacitanceMeter( final ICircuit circuit, World world, Point3D location, boolean visible ) {
-        super( circuit, world, location, visible );
-    }
-
-    @Override
-    protected double getCircuitValue() {
-        return getCircuit().getTotalCapacitance();
+        super( circuit, world, location, visible, new Function1<ICircuit, Double>() {
+            public Double apply( ICircuit iCircuit ) {
+                return circuit.getTotalCapacitance();
+            }
+        } );
     }
 }
--- simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/model/PlateChargeMeter.java
+++ simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/model/PlateChargeMeter.java
@@ -3,6 +3,7 @@
 package edu.colorado.phet.capacitorlab.model;
 
 import edu.colorado.phet.common.phetcommon.math.Point3D;
+import edu.colorado.phet.common.phetcommon.util.function.Function1;
 
 /**
  * Model element for the Plate Charge meter.
@@ -12,11 +13,10 @@ import edu.colorado.phet.common.phetcommon.math.Point3D;
 public class PlateChargeMeter extends BarMeter {
 
     public PlateChargeMeter( final ICircuit circuit, World world, Point3D location, boolean visible ) {
-        super( circuit, world, location, visible );
-    }
-
-    @Override
-    protected double getCircuitValue() {
-        return getCircuit().getTotalCharge();
+        super( circuit, world, location, visible, new Function1<ICircuit, Double>() {
+            public Double apply( ICircuit iCircuit ) {
+                return circuit.getTotalCharge();
+            }
+        } );
     }
 }
