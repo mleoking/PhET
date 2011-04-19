c6c0b3b161f8273e9ed78f188bc49992e46319ae
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-04-19 13:39:02 +0000

    added docs for moretools package

--- simulations-java/simulations/bending-light/src/edu/colorado/phet/bendinglight/modules/moretools/ChartNode.java
+++ simulations-java/simulations/bending-light/src/edu/colorado/phet/bendinglight/modules/moretools/ChartNode.java
@@ -11,10 +11,8 @@ import edu.colorado.phet.common.phetcommon.model.clock.Clock;
 import edu.colorado.phet.common.phetcommon.model.clock.ClockAdapter;
 import edu.colorado.phet.common.phetcommon.model.clock.ClockEvent;
 import edu.colorado.phet.common.phetcommon.model.property.Property;
-import edu.colorado.phet.common.phetcommon.util.Option;
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
-import edu.colorado.phet.common.phetcommon.view.util.DoubleGeneralPath;
 import edu.colorado.phet.common.phetcommon.view.util.RectangleUtils;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.umd.cs.piccolo.PNode;
@@ -30,17 +28,21 @@ import static edu.colorado.phet.common.phetcommon.view.graphics.transforms.Model
  * @author Sam Reid
  */
 public class ChartNode extends PClip {
-    private final Property<ModelViewTransform> transform;
+    private final Property<ModelViewTransform> transform;//Mapping from model (SI) to chart coordinates
     private PNode gridLines = new PNode();
+
+    //stroke dash parameters
     public static int DASH_ON = 10;
     public static int DASH_OFF = 5;
 
     public ChartNode( final Clock clock, final Rectangle chartArea, final ArrayList<Series> series ) {
         setPathTo( chartArea );
         setStroke( null );
-        final double timeWidth = 100 * MAX_DT / TIME_SPEEDUP_SCALE;
+        final double timeWidth = 100 * MAX_DT / TIME_SPEEDUP_SCALE;//Amount of time to show on the horizontal axis of the chart
         final int maxSampleCount = (int) Math.ceil( timeWidth / BendingLightModel.MIN_DT );
         transform = new Property<ModelViewTransform>( createRectangleMapping( new Rectangle2D.Double( 0, -1, timeWidth, 2 ), chartArea ) );
+
+        //Add nodes for the grid lines and serieses
         addChild( gridLines );
         for ( Series s : series ) {
             addChild( new SeriesNode( s ) );
@@ -49,18 +51,22 @@ public class ChartNode extends PClip {
         //Move over the view port as time passes
         clock.addClockListener( new ClockAdapter() {
             public void simulationTimeChanged( ClockEvent clockEvent ) {
+
+                //Update the mapping from model to chart
                 final double minTime = clock.getSimulationTime() - timeWidth;
                 transform.setValue( createRectangleMapping( new Rectangle2D.Double( minTime, -1, timeWidth, 2 ), chartArea ) );
 
-                //Update grid lines
+                //Clear grid lines and add them back in the new positions
                 gridLines.removeAllChildren();
 
+                //Add vertical grid lines
                 double verticalGridLineSpacing = timeWidth / 4;//distance between vertical grid lines
                 double verticalGridLineSpacingDelta = getDelta( verticalGridLineSpacing, clock );
                 for ( double x = minTime - verticalGridLineSpacingDelta; x <= minTime + timeWidth; x += timeWidth / 4 ) {
                     addVerticalLine( x );
                 }
 
+                //Add one horizontal grid line
                 double horizontalGridLineDelta = getDelta( DASH_ON + DASH_OFF, clock );
                 gridLines.addChild( new GridLine( minTime - horizontalGridLineDelta, 0, minTime + timeWidth, 0, 0 ) );//horizontal axis
 
@@ -81,51 +87,10 @@ public class ChartNode extends PClip {
     }
 
     private void addVerticalLine( double x ) {
-        gridLines.addChild( new GridLine( x, -1, x, 1, 0 ) );
+        gridLines.addChild( new GridLine( x, -1, x, 1, 0 ) );//-1 to +1 is far enough since in model coordinates
     }
 
-    public static class Series {
-        public final Property<ArrayList<Option<DataPoint>>> path;
-        private final Color color;
-
-        public Series( Property<ArrayList<Option<DataPoint>>> path, Color color ) {
-            this.path = path;
-            this.color = color;
-        }
-
-        public Paint getColor() {
-            return color;
-        }
-
-        public void addPoint( final double time, final double value ) {
-            path.setValue( new ArrayList<Option<DataPoint>>( path.getValue() ) {{
-                add( new Option.Some<DataPoint>( new DataPoint( time, value ) ) );
-            }} );
-        }
-
-        public Shape toShape() {
-            DoubleGeneralPath generalPath = new DoubleGeneralPath();
-            boolean moved = false;
-            for ( Option<DataPoint> value : path.getValue() ) {
-                if ( value.isSome() ) {
-                    final DataPoint dataPoint = value.get();
-                    if ( !moved ) {
-                        generalPath.moveTo( dataPoint.time, dataPoint.value );
-                        moved = true;
-                    }
-                    else {
-                        generalPath.lineTo( dataPoint.time, dataPoint.value );
-                    }
-                }
-            }
-            return generalPath.getGeneralPath();
-        }
-
-        public void keepLastSamples( final int maxSampleCount ) {
-            path.setValue( new ArrayList<Option<DataPoint>>( path.getValue().subList( Math.max( 0, path.getValue().size() - maxSampleCount ), path.getValue().size() ) ) );
-        }
-    }
-
+    //Node for drawing the series of points.
     private class SeriesNode extends PNode {
         public SeriesNode( final Series s ) {
             addChild( new PhetPPath( new BasicStroke( 2 ), s.getColor() ) {{
@@ -138,8 +103,11 @@ public class ChartNode extends PClip {
         }
     }
 
+    //Draw a horizontal or vertical grid line
     public class GridLine extends PNode {
-        public GridLine( final double x1, final double y1, final double x2, final double y2, double phase ) {
+        public GridLine( final double x1, final double y1, final double x2, final double y2,
+                         double phase//Have to model the phase to make it look like the grid line is moving
+        ) {
             final float strokeWidth = 2;
             addChild( new PhetPPath( new BasicStroke( strokeWidth, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 1f, new float[] { DASH_ON, DASH_OFF }, (float) phase ), Color.lightGray ) {
                 {
