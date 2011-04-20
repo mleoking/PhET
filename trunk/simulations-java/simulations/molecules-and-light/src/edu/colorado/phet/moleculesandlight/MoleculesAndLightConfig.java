51f81602718f5446ace8f09c1a8c38a70ea82a8a
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-04-20 19:21:18 +0000

    Removed constants that were redundant with the constants in the photonabsorption common code.  This change was suggested as a result of a code review, see #2744.

--- trunk/simulations-java/simulations/molecules-and-light/src/edu/colorado/phet/moleculesandlight/MoleculesAndLightConfig.java
+++ trunk/simulations-java/simulations/molecules-and-light/src/edu/colorado/phet/moleculesandlight/MoleculesAndLightConfig.java
@@ -9,13 +9,6 @@ public class MoleculesAndLightConfig {
     public static final String PROJECT_NAME = "molecules-and-light";
     public static final String FLAVOR_NAME_MOLECULES_AND_LIGHT = "molecules-and-light";
 
-    public static final double SUNLIGHT_WAVELENGTH = 400E-9;
-    public static final double MICRO_WAVELENGTH = 20;
-    public static final double IR_WAVELENGTH = 850E-9;
-    public static final double VISIBLE_WAVELENGTH = 580E-9;
-    public static final double UV_WAVELENGTH = 100E-9;
-    public static final double DEBUG_WAVELENGTH = 1;
-
     // Graphics-related parameters
     public static final Color PANEL_BACKGROUND_COLOR = new Color( 206, 206, 206 );
 }
