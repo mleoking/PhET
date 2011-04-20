c4cf3313ff464305dc03507f007f700fb0613e80
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-04-20 22:01:32 +0000

    bam: refactoring out AllFilledDialogNode

--- trunk/simulations-java/simulations/build-a-molecule/src/edu/colorado/phet/buildamolecule/view/MoleculeCollectingCanvas.java
+++ trunk/simulations-java/simulations/build-a-molecule/src/edu/colorado/phet/buildamolecule/view/MoleculeCollectingCanvas.java
@@ -1,23 +1,13 @@
 package edu.colorado.phet.buildamolecule.view;
 
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.geom.Rectangle2D;
-
-import javax.swing.*;
 
 import edu.colorado.phet.buildamolecule.BuildAMoleculeConstants;
+import edu.colorado.phet.buildamolecule.control.AllFilledDialogNode;
 import edu.colorado.phet.buildamolecule.control.CollectionAreaNode;
 import edu.colorado.phet.buildamolecule.model.KitCollectionModel;
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 import edu.colorado.phet.common.phetcommon.util.function.VoidFunction0;
-import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
-import edu.colorado.phet.common.piccolophet.nodes.HTMLNode;
-import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
-import edu.umd.cs.piccolo.PNode;
-import edu.umd.cs.piccolox.pswing.PSwing;
-import edu.umd.cs.piccolox.swing.SwingLayoutNode;
 
 /**
  * Canvas that shows multiple kits AND a collection area to the right
@@ -36,7 +26,7 @@ public class MoleculeCollectingCanvas extends BuildAMoleculeCanvas {
                     // not filled
                     return;
                 }
-                addWorldChild( new CollectionBoxesFullNode( regenerateCallback, initialModel ) );
+                addWorldChild( new AllFilledDialogNode( initialModel, getModelViewTransform(), regenerateCallback ) );
             }
         } );
     }
@@ -50,40 +40,4 @@ public class MoleculeCollectingCanvas extends BuildAMoleculeCanvas {
         addWorldChild( collectionAreaNode );
     }
 
-    private class CollectionBoxesFullNode extends PNode {
-        public CollectionBoxesFullNode( final VoidFunction0 regenerateCallback, KitCollectionModel initialModel ) {
-            PNode background = new PNode();
-            addChild( background );
-            addChild( new SwingLayoutNode( new GridBagLayout() ) {{
-                // TODO: i18n
-                addChild( new HTMLNode( "You have filled all of your collection boxes.<br>Would you like to fill a different set of collection boxes?" ) {{
-                    setFont( new PhetFont( 16 ) );
-                }},
-                          new GridBagConstraints() {{
-                              gridx = 0;
-                              gridy = 0;
-                          }} );
-                // TODO: i18n
-                addChild( new PSwing( new JButton( "Yes" ) {{ // or how about just "I guess, since I have no other option"
-                    addActionListener( new ActionListener() {
-                        public void actionPerformed( ActionEvent e ) {
-                            regenerateCallback.apply();
-                        }
-                    } );
-                }} ),
-                          new GridBagConstraints() {{
-                              gridx = 0;
-                              gridy = 1;
-                              insets = new Insets( 10, 0, 0, 0 );
-                          }} );
-            }} );
-
-            float padding = 10;
-
-            background.addChild( PhetPPath.createRectangle( (float) getFullBounds().getX() - padding, (float) getFullBounds().getY() - padding, (float) getFullBounds().getWidth() + 2 * padding, (float) getFullBounds().getHeight() + 2 * padding ) );
-
-            Rectangle2D playAreaViewBounds = getModelViewTransform().modelToView( initialModel.getAvailablePlayAreaBounds() ).getBounds2D();
-            centerFullBoundsOnPoint( playAreaViewBounds.getCenterX(), playAreaViewBounds.getCenterY() );
-        }
-    }
 }
