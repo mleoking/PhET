3b9b2e5d5b08982c54f17f95afe841f745324ac8
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-04-21 09:17:00 +0000

    bam: updated equivalency tests

---
+++ trunk/simulations-java/simulations/build-a-molecule/src/edu/colorado/phet/buildamolecule/model/tests/MoleculeEquivalencyTests.java
@@ -0,0 +1,18 @@
+package edu.colorado.phet.buildamolecule.model.tests;
+
+import java.util.List;
+
+import edu.colorado.phet.buildamolecule.model.CompleteMolecule;
+
+public class MoleculeEquivalencyTests {
+    public static void main( String[] args ) {
+        List<CompleteMolecule> molecules = CompleteMolecule.getAllCompleteMolecules();
+        for ( CompleteMolecule a : molecules ) {
+            for ( CompleteMolecule b : molecules ) {
+                if ( a != b && a.getMoleculeStructure().isEquivalent( b.getMoleculeStructure() ) ) {
+                    System.out.println( a.getCommonName() + " ==? " + b.getCommonName() );
+                }
+            }
+        }
+    }
+}
