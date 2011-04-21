2a4520bb34dd2ba75b123b91902fd258df397434
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-04-21 08:04:15 +0000

    bam: debugging stuff

--- trunk/simulations-java/simulations/build-a-molecule/src/edu/colorado/phet/buildamolecule/model/tests/MoleculeEquivalenceTests.java
+++ trunk/simulations-java/simulations/build-a-molecule/src/edu/colorado/phet/buildamolecule/model/tests/MoleculeEquivalenceTests.java
@@ -1,19 +1,21 @@
 package edu.colorado.phet.buildamolecule.model.tests;
 
-import java.util.List;
-
-import edu.colorado.phet.buildamolecule.model.CompleteMolecule;
+import edu.colorado.phet.buildamolecule.model.MoleculeStructure;
 
 public class MoleculeEquivalenceTests {
     // TODO: is Ethylene sub-position failing?
     public static void main( String[] args ) {
-        List<CompleteMolecule> molecules = CompleteMolecule.getAllCompleteMolecules();
-        for ( CompleteMolecule a : molecules ) {
-            for ( CompleteMolecule b : molecules ) {
-                if ( a != b && a.getMoleculeStructure().isEquivalent( b.getMoleculeStructure() ) ) {
-                    System.out.println( a.getCommonName() + " ==? " + b.getCommonName() );
-                }
-            }
-        }
+//        List<CompleteMolecule> molecules = CompleteMolecule.getAllCompleteMolecules();
+//        for ( CompleteMolecule a : molecules ) {
+//            for ( CompleteMolecule b : molecules ) {
+//                if ( a != b && a.getMoleculeStructure().isEquivalent( b.getMoleculeStructure() ) ) {
+//                    System.out.println( a.getCommonName() + " ==? " + b.getCommonName() );
+//                }
+//            }
+//        }
+        assert ( !MoleculeStructure.fromSerial( "6|5|N|H|H|O|C|C|5|2|0|4|5|1|3|5|3|0" ).isEquivalent( MoleculeStructure.fromSerial( "6|5|H|O|N|C|H|C|2|0|1|5|1|4|3|2|4|0" ) ) );
+
+
+        assert ( !MoleculeStructure.fromSerial( "7|6|O|O|O|H|H|N|H|0|5|2|5|1|5|2|3|0|4|1|6" ).isEquivalent( MoleculeStructure.fromSerial( "7|6|H|O|N|H|H|O|O|5|3|0|6|2|0|1|4|5|2|2|1" ) ) );
     }
 }
