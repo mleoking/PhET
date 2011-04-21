a494d04d97f9413c7ae40099162e02f7e5a76ced
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-04-21 02:09:43 +0000

    started moleculetabledialog

---
+++ simulations-java/simulations/build-a-molecule/src/edu/colorado/phet/buildamolecule/MoleculeTableDialog.java
@@ -0,0 +1,16 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.buildamolecule;
+
+import java.awt.*;
+
+import javax.swing.*;
+
+/**
+ * @author Sam Reid
+ */
+public class MoleculeTableDialog extends JDialog {
+    public MoleculeTableDialog( Frame owner ) {
+        super( owner, false );
+
+    }
+}
