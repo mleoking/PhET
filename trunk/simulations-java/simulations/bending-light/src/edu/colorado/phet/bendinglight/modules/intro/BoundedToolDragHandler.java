9aac6dcf1eb87cd309c5cafc620c456198f39437
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-04-22 18:52:39 +0000

    refactored BoundedDragHandler

--- trunk/simulations-java/simulations/bending-light/src/edu/colorado/phet/bendinglight/modules/intro/BoundedToolDragHandler.java
+++ trunk/simulations-java/simulations/bending-light/src/edu/colorado/phet/bendinglight/modules/intro/BoundedToolDragHandler.java
@@ -1,69 +1,30 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.bendinglight.modules.intro;
 
-import java.awt.geom.Dimension2D;
-import java.awt.geom.Point2D;
-
-import edu.colorado.phet.bendinglight.model.ImmutableRectangle2D;
 import edu.colorado.phet.common.piccolophet.nodes.ToolNode;
-import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
 import edu.umd.cs.piccolo.event.PInputEvent;
 import edu.umd.cs.piccolo.util.PDimension;
 
 /**
- * The BoundedToolDragHandler is used by Tool instances so that when they are dragged out of the toolbox, they translate around the screen but without leaving the visible bounds.
+ * Convenience subclass so that the ToolNode.dragAll is called on dragNode.
  *
  * @author Sam Reid
  */
-public class BoundedToolDragHandler extends PBasicInputEventHandler {
-    private Point2D lastLocation;
+public class BoundedToolDragHandler extends BoundedDragHandler {
     private ToolNode node;
 
-    //Create a handler, using the PInputEvent that created the ToolNode
     public BoundedToolDragHandler( ToolNode node ) {
+        super( node );
         this.node = node;
     }
 
     public BoundedToolDragHandler( ToolNode node, PInputEvent event ) {
-        this( node );
-        mousePressed( event );
+        super( node, event );
+        this.node = node;
     }
 
-    public void mousePressed( PInputEvent event ) {
-        //Compute the initial state so that the node can be moved to locations through deltas only (since that is its interface)
-        Point2D viewStartingPoint = event.getPositionRelativeTo( node.getParent().getParent() );//Not sure why getParent.getParent is the best coordinate frame, but it works and is consistent (just using node causes failures from node not being on pickpath)
-        viewStartingPoint = node.getParent().getParent().localToGlobal( viewStartingPoint );
-        lastLocation = new Point2D.Double( viewStartingPoint.getX(), viewStartingPoint.getY() );
-    }
-
-    //Drag the node to the constrained location
-    public void mouseDragged( final PInputEvent event ) {
-        //Compute the global coordinate for where the drag is supposed to take the node
-        Point2D newDragPosition = event.getPositionRelativeTo( node.getParent().getParent() );//see note in constructor
-        newDragPosition = node.getParent().getParent().localToGlobal( newDragPosition );
-
-        //Bound the desired point within the canvas, accounting for some insets (so some part will always be visible)
-        final int inset = 10;
-        final ImmutableRectangle2D immutableRectangle2D = new ImmutableRectangle2D( inset, inset, event.getSourceSwingEvent().getComponent().getWidth() - inset * 2, event.getSourceSwingEvent().getComponent().getHeight() - inset * 2 );
-        Point2D constrained = immutableRectangle2D.getClosestPoint( newDragPosition );
-        Dimension2D delta = new PDimension( constrained.getX() - lastLocation.getX(), constrained.getY() - lastLocation.getY() );
-
-        //Convert from global to the node parent frame
-        delta = node.globalToLocal( delta );
-        delta = node.localToParent( delta );
-
-        //Translate the node and get ready for next event
-        dragNode( new PDimension( delta.getWidth(), delta.getHeight() ) );
-        lastLocation = constrained;
-    }
-
-    //Handles dragging the node.  This method is overrideable since some usages may need to set the position of a model element instead of just translating the pnode
-    //Delta is in the coordinate frame of the node itself
-    protected void dragNode( PDimension delta ) {
-        node.dragAll( new PDimension( delta.getWidth(), delta.getHeight() ) );
-    }
-
-    public void mouseReleased( PInputEvent event ) {
-        lastLocation = null;
+    //Drags the node according to the definition in ToolNode.dragAll
+    @Override protected void dragNode( PDimension delta ) {
+        node.dragAll( delta );
     }
 }
