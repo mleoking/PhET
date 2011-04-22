a045b3cbda21fac8599da16f94cc44f5dd0076bb
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-04-22 18:06:11 +0000

    refactored prism toolbox to use ToolBoxNode

--- simulations-java/simulations/bending-light/src/edu/colorado/phet/bendinglight/modules/prisms/PrismToolboxNode.java
+++ simulations-java/simulations/bending-light/src/edu/colorado/phet/bendinglight/modules/prisms/PrismToolboxNode.java
@@ -4,20 +4,24 @@ package edu.colorado.phet.bendinglight.modules.prisms;
 import java.awt.*;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import edu.colorado.phet.bendinglight.BendingLightStrings;
 import edu.colorado.phet.bendinglight.modules.intro.HBox;
+import edu.colorado.phet.bendinglight.modules.intro.Tool;
 import edu.colorado.phet.bendinglight.view.BendingLightCanvas;
 import edu.colorado.phet.bendinglight.view.MediumControlPanel;
+import edu.colorado.phet.common.phetcommon.model.property.Property;
+import edu.colorado.phet.common.phetcommon.util.function.Function0;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
-import edu.colorado.phet.common.piccolophet.event.CursorHandler;
+import edu.colorado.phet.common.phetcommon.view.util.BufferedImageUtils;
+import edu.colorado.phet.common.piccolophet.nodes.ToolNode;
 import edu.umd.cs.piccolo.PNode;
-import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
-import edu.umd.cs.piccolo.event.PInputEvent;
-import edu.umd.cs.piccolo.nodes.PImage;
 import edu.umd.cs.piccolo.nodes.PText;
+import edu.umd.cs.piccolo.util.PDimension;
 
 import static edu.colorado.phet.bendinglight.modules.prisms.PrismsModel.getPrismPrototypes;
 
@@ -39,77 +43,71 @@ public class PrismToolboxNode extends PNode {
 
         //Iterate over the prism prototypes in the model and create a draggable icon for each one
         for ( final Prism prism : getPrismPrototypes() ) {
-            //Create a node for the prism, just used to create the thumbnail
-            PrismNode prismNode = new PrismNode( transform, prism, model.prismMedium );
-            final int thumbnailHeight = 70;
-            Image image = prismNode.toImage( (int) ( prismNode.getFullBounds().getWidth() * thumbnailHeight / prismNode.getFullBounds().getHeight() ), thumbnailHeight, null );
-            final PImage thumbnail = new PImage( image ) {{
-                final PImage thumbnailRef = this;
-
-                //Add user interaction
-                addInputEventListener( new PBasicInputEventHandler() {
-                    PrismNode createdNode = null;//Last created node that events should be forwarded to
-                    boolean intersect = false;//Ready to drop the prism back in the toolbox
-
-                    //When the mouse is pressed, create a new prism model and node
-                    @Override public void mousePressed( PInputEvent event ) {
-                        if ( createdNode == null ) {
-                            //Create a new prism model, and add it to the model
-                            final Point2D positionRelativeTo = event.getPositionRelativeTo( getParent().getParent().getParent() );//why?
-                            Point2D modelPoint = transform.viewToModel( positionRelativeTo );
-                            final Prism prismCopy = prism.copy();
-                            final Rectangle2D bounds = prismCopy.getBounds();
-                            Point2D copyCenter = new Point2D.Double( bounds.getX(), bounds.getY() );
-                            prismCopy.translate( modelPoint.getX() - copyCenter.getX() - bounds.getWidth() / 2, modelPoint.getY() - copyCenter.getY() - bounds.getHeight() / 2 );
-                            model.addPrism( prismCopy );
-
-                            //there is no callback for node creation here, so we create the node ourselves.
-                            //In a normal MVC scheme though, addPrism would create the node, and that would cause problems for this technique
-
-                            //Create the node and keep a reference to it so it can be removed later
-                            final PrismNode closureNode = new PrismNode( transform, prismCopy, model.prismMedium );
-                            this.createdNode = closureNode;
-
-                            //Check to see if the prism can be dropped back in the toolbox
-                            final PropertyChangeListener pcl = new PropertyChangeListener() {
-                                public void propertyChange( PropertyChangeEvent evt ) {
-                                    intersect = PrismToolboxNode.this.getGlobalFullBounds().contains( closureNode.getGlobalFullBounds().getCenter2D() );
-                                }
-                            };
-                            closureNode.addPropertyChangeListener( PROPERTY_FULL_BOUNDS, pcl );
-                            closureNode.addInputEventListener( new PBasicInputEventHandler() {
-                                //When the mouse is released, put the prism back in the toolbox if it was over the toolbox
-                                public void mouseReleased( PInputEvent event ) {
-                                    if ( intersect ) {
-                                        thumbnailRef.setVisible( true );//make sure it can be grabbed again
-                                        closureNode.removePropertyChangeListener( pcl );//prevent memory leaks
-                                        canvas.removePrismNode( closureNode );//remove the node from the view
-                                        createdNode = null;//Signal that a new node needs to be created next time
-                                        model.removePrism( prismCopy );//remove the prism model from the model
-                                    }
-                                }
-                            } );
-
-                            canvas.addPrismNode( createdNode );
-                        }
-                    }
-
-                    //Forward drag events to the created prism node
-                    public void mouseDragged( PInputEvent event ) {
-                        createdNode.prism.translate( transform.viewToModelDelta( event.getDeltaRelativeTo( getParent() ) ) );
-                    }
-
-                    //Signify that another node should be created when dragged out of the toolbox
-                    public void mouseReleased( PInputEvent event ) {
-                        createdNode = null;
-                    }
-                } );
-                addInputEventListener( new CursorHandler() );
-            }};
-            content.addChild( thumbnail );
+            content.addChild( new PrismTool( prism, model, transform, canvas, new Function0<Rectangle2D>() {
+                public Rectangle2D apply() {
+                    return getGlobalFullBounds();
+                }
+            } ) );
         }
 
         //Allow the user to control the type of material in the prisms
         content.addChild( new MediumControlPanel( canvas, model.prismMedium, BendingLightStrings.OBJECTS, false, model.wavelengthProperty, "0.0000000", 8 ) );
+    }
+
+    //TODO: rename Tool and ToolNode, they are confusing.  Rename to ToolIconNode and ToolNode
+    static class PrismTool extends Tool {
+        public PrismTool( final Prism prism, final PrismsModel model, ModelViewTransform transform, PrismsCanvas canvas, final Function0<Rectangle2D> globalToolboxBounds ) {
+            super( toThumbnail( prism, model, transform ), new Property<Boolean>( false ) {
+                @Override public void setValue( Boolean value ) {
+                    super.setValue( false );
+                }
+            }, transform, canvas, new NodeFactory() {
+                public ToolNode createNode( ModelViewTransform transform, Property<Boolean> visible, Point2D location ) {
+                    return new PrismToolNode( transform, prism.copy(), model, location );
+                }
+            }, model, globalToolboxBounds );
+            dragMultiple = true;
+        }
+
+        @Override protected void addChild( BendingLightCanvas canvas, ToolNode node ) {
+            canvas.addChildBehindLight( node );
+        }
+
+        @Override protected void doRemoveChild( BendingLightCanvas canvas, ToolNode node ) {
+            canvas.removeChildBehindLight( node );
+        }
+
+        private static Image toThumbnail( Prism prism, PrismsModel model, ModelViewTransform transform ) {
+            PrismNode prismNode = new PrismNode( transform, prism, model.prismMedium );
+            final int thumbnailHeight = 70;
+            final Image image = prismNode.toImage( (int) ( prismNode.getFullBounds().getWidth() * thumbnailHeight / prismNode.getFullBounds().getHeight() ), thumbnailHeight, null );
+            try {
+                ImageIO.write( BufferedImageUtils.toBufferedImage( image ), "PNG", new File( "C:/Users/Sam/Desktop/testimage_" + System.currentTimeMillis() + ".png" ) );
+            }
+            catch ( IOException e ) {
+                e.printStackTrace();
+            }
+            return image;
+        }
+    }
+
+    static class PrismToolNode extends ToolNode {
+        private final ModelViewTransform transform;
+        private final Prism prism;
+
+        PrismToolNode( final ModelViewTransform transform, final Prism prism, final PrismsModel model, Point2D modelPoint ) {
+            this.transform = transform;
+            this.prism = prism;
+            //Create a new prism model, and add it to the model
+            final Rectangle2D bounds = prism.getBounds();
+            Point2D copyCenter = new Point2D.Double( bounds.getX(), bounds.getY() );
+            prism.translate( modelPoint.getX() - copyCenter.getX() - bounds.getWidth() / 2, modelPoint.getY() - copyCenter.getY() - bounds.getHeight() / 2 );
+            model.addPrism( prism );
+            addChild( new PrismNode( transform, prism, model.prismMedium ) );
+        }
+
+        @Override public void dragAll( PDimension viewDelta ) {
+            prism.translate( transform.viewToModelDelta( viewDelta ) );
+        }
     }
 }
