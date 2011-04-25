92f6027d57cc3b5a47e5421a6f01c64bccb045bf
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-04-25 14:03:20 +0000

    Removed balloon model and view

--- simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/fluidpressure/FluidPressureModel.java
+++ simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/fluidpressure/FluidPressureModel.java
@@ -1,7 +1,6 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.fluidpressureandflow.fluidpressure;
 
-import edu.colorado.phet.fluidpressureandflow.common.model.Balloon;
 import edu.colorado.phet.fluidpressureandflow.common.model.FluidPressureAndFlowModel;
 import edu.colorado.phet.fluidpressureandflow.common.model.PressureSensor;
 import edu.colorado.phet.fluidpressureandflow.common.model.units.UnitSet;
@@ -16,7 +15,6 @@ public class FluidPressureModel extends FluidPressureAndFlowModel {
         super( UnitSet.ATMOSPHERES );
         addPressureSensor( new PressureSensor( this, 1, 0 ) );
         addPressureSensor( new PressureSensor( this, -4, 2 ) );//Show second pressure sensor at a y location that yields a different pressure value
-        addBalloon( new Balloon( this, 3, 2 ) );
     }
 
     @Override
