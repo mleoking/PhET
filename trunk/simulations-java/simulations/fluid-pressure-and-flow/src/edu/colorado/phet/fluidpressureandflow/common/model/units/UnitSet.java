a10ebf8582b9887d288947396d6f28d2d1e36fc2
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-04-25 14:41:46 +0000

    Fixed: Fluid Density is expressed in metric (kg/m3) even when units are set to feet

--- trunk/simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/common/model/units/UnitSet.java
+++ trunk/simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/common/model/units/UnitSet.java
@@ -1,6 +1,10 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.fluidpressureandflow.common.model.units;
 
+import java.text.DecimalFormat;
+
+import static edu.colorado.phet.fluidpressureandflow.common.FPAFStrings.KG_PER_M_3;
+import static edu.colorado.phet.fluidpressureandflow.common.FPAFStrings.LB_PER_FT_3;
 import static edu.colorado.phet.fluidpressureandflow.common.model.units.Units.*;
 
 /**
@@ -12,13 +16,17 @@ public class UnitSet {
     public final Unit pressure;
     public final Unit velocity;
     public final Unit distance;
-    public static final UnitSet ATMOSPHERES = new UnitSet( ATMOSPHERE, FEET_PER_SECOND, FEET );//English units but with atmospheres for pressure instead of psi
-    public static final UnitSet ENGLISH = new UnitSet( PSI, FEET_PER_SECOND, FEET );
-    public static final UnitSet METRIC = new UnitSet( PASCAL, METERS_PER_SECOND, METERS );
+    public final Unit density;
+    private static final Unit FLUID_DENSITY_METRIC = new LinearUnit( "Fluid Density(metric)", KG_PER_M_3, 1, new DecimalFormat( "0.00" ) );
+    private static final Unit FLUID_DENSITY_ENGLISH = new LinearUnit( "Fluid Density(english)", LB_PER_FT_3, 16.0184634, new DecimalFormat( "0.00" ) );
+    public static final UnitSet ATMOSPHERES = new UnitSet( ATMOSPHERE, FEET_PER_SECOND, FEET, FLUID_DENSITY_ENGLISH );//English units but with atmospheres for pressure instead of psi
+    public static final UnitSet ENGLISH = new UnitSet( PSI, FEET_PER_SECOND, FEET, FLUID_DENSITY_ENGLISH );
+    public static final UnitSet METRIC = new UnitSet( PASCAL, METERS_PER_SECOND, METERS, FLUID_DENSITY_METRIC );
 
-    public UnitSet( Unit pressure, Unit velocity, Unit distance ) {
+    public UnitSet( Unit pressure, Unit velocity, Unit distance, Unit density ) {
         this.pressure = pressure;
         this.velocity = velocity;
         this.distance = distance;
+        this.density = density;
     }
 }
