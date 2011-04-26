16ab735f33b36294f99e73b465e362c33a59fd39
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-04-26 22:56:38 +0000

    bam: Jmol launching and 1-3D-view changes

--- simulations-java/simulations/build-a-molecule/src/edu/colorado/phet/buildamolecule/control/JmolPanel.java
+++ simulations-java/simulations/build-a-molecule/src/edu/colorado/phet/buildamolecule/control/JmolPanel.java
@@ -8,25 +8,43 @@ import org.jmol.adapter.smarter.SmarterJmolAdapter;
 import org.jmol.api.JmolViewer;
 
 import edu.colorado.phet.buildamolecule.model.CompleteMolecule;
+import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
+import edu.umd.cs.piccolo.nodes.PText;
+import edu.umd.cs.piccolo.util.PPaintContext;
 
 /**
  * Displays a 3D molecule structure that can be rotated
  */
 public class JmolPanel extends JPanel {
-    private JmolViewer viewer;
+    private JmolViewer viewer = null;
 
-    public JmolPanel( CompleteMolecule molecule ) {
-        viewer = JmolViewer.allocateViewer( this, new SmarterJmolAdapter(), null, null, null, "-applet", null );
+    public JmolPanel( final CompleteMolecule molecule ) {
+
+        // create the 3D view after we have shown the "loading" text and dialog
+        SwingUtilities.invokeLater( new Runnable() {
+            public void run() {
+                // create the 3D view
+                JmolViewer viewer = JmolViewer.allocateViewer( JmolPanel.this, new SmarterJmolAdapter(), null, null, null, "-applet", null );
+
+                String errorString = viewer.openStringInline( molecule.getCmlData() );
+                if ( errorString != null ) {
+                    throw new RuntimeException( "Jmol problem: " + errorString );
+                }
+
+                // store reference to this AFTER we have processed the molecule data, so that the "Loading" text shows up until the molecule is loaded
+                JmolPanel.this.viewer = viewer;
+
+                // set default options, and start the spinning. We need the viewer instance to be set BEFORE these are called
+                setSpaceFill();
+                viewer.script( "spin on;" );
+
+                repaint();
+            }
+        } );
+
+        setBackground( Color.BLACK );
 
         setPreferredSize( new Dimension( 400, 400 ) );
-
-        String errorString = viewer.openStringInline( molecule.getCmlData() );
-        if ( errorString != null ) {
-            throw new RuntimeException( "Jmol problem: " + errorString );
-        }
-
-        setSpaceFill();
-        viewer.script( "spin on;" );
     }
 
     public void setSpaceFill() {
@@ -39,11 +57,32 @@ public class JmolPanel extends JPanel {
 
     @Override
     public void paint( Graphics g ) {
-        Dimension currentSize = new Dimension();
-        getSize( currentSize );
-        Rectangle clipBounds = new Rectangle();
-        g.getClipBounds( clipBounds );
-        viewer.renderScreenImage( g, currentSize, clipBounds );
+        // Jmol's canonical example of embedding in other Java is to override the paint method, so we do it here
+
+        if ( viewer == null ) {
+            // if we have no viewer yet, we show the loading text on a black background
+            super.paint( g );
+
+            // create a piccolo node (helpful for centering and styling)
+            PText text = new PText( "Loading 3D..." ) {{ // TODO: i18n
+                setTextPaint( Color.WHITE );
+                setFont( new PhetFont( 20 ) );
+
+                // center in the panel
+                setOffset( ( JmolPanel.this.getWidth() - getFullBounds().getWidth() ) / 2, ( JmolPanel.this.getHeight() - getFullBounds().getHeight() ) / 2 );
+            }};
+
+            // paint the piccolo node onto the panel
+            text.fullPaint( new PPaintContext( (Graphics2D) g ) );
+        }
+        else {
+            // copied from Jmol's Integration.java
+            Dimension currentSize = new Dimension();
+            getSize( currentSize );
+            Rectangle clipBounds = new Rectangle();
+            g.getClipBounds( clipBounds );
+            viewer.renderScreenImage( g, currentSize, clipBounds );
+        }
     }
 
     public static void main( String[] args ) {
--- simulations-java/simulations/build-a-molecule/src/edu/colorado/phet/buildamolecule/view/MoleculeNode.java
+++ simulations-java/simulations/build-a-molecule/src/edu/colorado/phet/buildamolecule/view/MoleculeNode.java
@@ -1,6 +1,8 @@
 package edu.colorado.phet.buildamolecule.view;
 
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.awt.geom.Rectangle2D;
 import java.util.HashMap;
 import java.util.Map;
@@ -13,11 +15,13 @@ import edu.colorado.phet.buildamolecule.model.buckets.AtomModel;
 import edu.colorado.phet.chemistry.model.Atom;
 import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.phetcommon.resources.PhetCommonResources;
+import edu.colorado.phet.common.phetcommon.util.Option;
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.event.CursorHandler;
 import edu.colorado.phet.common.piccolophet.nodes.HTMLNode;
+import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.event.PInputEvent;
 import edu.umd.cs.piccolo.nodes.PImage;
@@ -30,6 +34,7 @@ public class MoleculeNode extends PNode {
     private Kit kit;
     private MoleculeStructure moleculeStructure;
     private ModelViewTransform mvt;
+    private Property<Option<JmolDialog>> dialog = new Property<Option<JmolDialog>>( new Option.None<JmolDialog>() );
 
     private Map<AtomModel, SimpleObserver> observerMap = new HashMap<AtomModel, SimpleObserver>();
 
@@ -63,15 +68,50 @@ public class MoleculeNode extends PNode {
             * show 3d button
             *----------------------------------------------------------------------------*/
             addChild( new PNode() {{
-                addChild( new PImage( PhetCommonResources.getMaximizeButtonImage() ) );
+                PImage image = new PImage( PhetCommonResources.getMaximizeButtonImage() );
+                addChild( image );
                 addInputEventListener( new CursorHandler() {
                     @Override
                     public void mouseClicked( PInputEvent event ) {
-                        JmolDialog.displayMolecule3D( parentFrame, completeMolecule );
+                        // if the 3D dialog is not shown, show it
+                        if ( dialog.getValue().isNone() ) {
+                            // set our reference to it ("disables" this button)
+                            dialog.setValue( new Option.Some<JmolDialog>( JmolDialog.displayMolecule3D( parentFrame, completeMolecule ) ) );
+
+                            // listen to when it closes so we can re-enable the button
+                            dialog.getValue().get().addWindowListener( new WindowAdapter() {
+                                @Override public void windowClosed( WindowEvent e ) {
+                                    dialog.setValue( new Option.None<JmolDialog>() );
+                                }
+                            } );
+                        }
+                        else {
+                            dialog.getValue().get().requestFocus();
+                        }
                     }
                 } );
                 setOffset( currentX.getValue(), 0 );
                 currentX.setValue( currentX.getValue() + getFullBounds().getWidth() + 5 );
+
+                // change overall transparency based on dialog existence
+                dialog.addObserver( new SimpleObserver() {
+                    public void update() {
+                        setTransparency( dialog.getValue().isSome() ? 0.5f : 1f );
+                    }
+                } );
+
+                /*---------------------------------------------------------------------------*
+                * gray "disabled" overlay
+                *----------------------------------------------------------------------------*/
+                addChild( new PhetPPath( new Rectangle2D.Double( 0, 0, image.getWidth(), image.getHeight() ) ) {{
+                    setPaint( new Color( 128, 128, 128, 64 ) );
+                    setStroke( null );
+                    dialog.addObserver( new SimpleObserver() {
+                        public void update() {
+                            setVisible( dialog.getValue().isSome() );
+                        }
+                    } );
+                }} );
             }} );
         }
 
@@ -103,11 +143,28 @@ public class MoleculeNode extends PNode {
         }
 
         updatePosition(); // sanity check. should update (unfortunately) a number of times above
+
+        // hide 3D dialogs when the kit is hidden
+        kit.visible.addObserver( new SimpleObserver() {
+            public void update() {
+                if ( !kit.visible.getValue() ) {
+                    hideDialogIfShown();
+                }
+            }
+        } );
     }
 
     public void destruct() {
         for ( AtomModel atomModel : observerMap.keySet() ) {
             atomModel.removePositionListener( observerMap.get( atomModel ) );
+        }
+        hideDialogIfShown();
+    }
+
+    public void hideDialogIfShown() {
+        if ( dialog.getValue().isSome() ) {
+            dialog.getValue().get().dispose();
+            dialog.setValue( new Option.None<JmolDialog>() );
         }
     }
 
