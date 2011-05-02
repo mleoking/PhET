0f2318b9202af769ee13d1c63f79575b71aa4bcc
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-05-02 03:21:05 +0000

    added faucet graphics

--- simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/common/model/Beaker.java
+++ simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/common/model/Beaker.java
@@ -3,6 +3,7 @@ package edu.colorado.phet.sugarandsaltsolutions.common.model;
 
 import java.awt.*;
 import java.awt.geom.Area;
+import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 
 import edu.colorado.phet.common.phetcommon.view.util.DoubleGeneralPath;
@@ -46,5 +47,10 @@ public class Beaker {
         double area = width * width;//assumes a square "cylinder"
         double height = volume / area;
         return new Rectangle2D.Double( x, y, width, height );
+    }
+
+    //Gets the bottom right corner for attaching the output faucet
+    public Point2D getOutputFaucetAttachmentPoint() {
+        return new Point2D.Double( x + width, y );
     }
 }
