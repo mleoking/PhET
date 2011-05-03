66a0c5413a7c01df6f9ccc7bee527ecd45cfb9db
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-05-03 14:52:17 +0000

    added reset method to Property, see #2775

--- trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/model/property3/Notifier.java
+++ trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/model/property3/Notifier.java
@@ -3,17 +3,21 @@ package edu.colorado.phet.common.phetcommon.model.property3;
 
 /**
  * Keeps track of when values have changed, used in Observable instances to make sure that notifications are only sent out when necessary.
+ * This class is intended for us internally by the property3 package, but left public for reuse in other packages where necessary.
  *
  * @author Sam Reid
  */
 public class Notifier<T> implements Gettable<T> {
     private T value;
+    private T initialValue;
 
     public Notifier( T value ) {
         this.value = value;
+        this.initialValue = value;
     }
 
-    public boolean change( T value ) {
+    //Sets the specified value and retuns true if the value changed
+    public boolean set( T value ) {
         boolean changed = !value.equals( this.value );
         this.value = value;
         return changed;
@@ -21,5 +25,10 @@ public class Notifier<T> implements Gettable<T> {
 
     public T get() {
         return value;
+    }
+
+    //Determine the initial value
+    public T getInitialValue() {
+        return initialValue;
     }
 }
