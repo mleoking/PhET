2bea73f6cfb49d3a8a2caaaf99b063af70f5b0a3
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-05-03 01:31:45 +0000

    fixed background color of the canvas and salt crystals, and improved rule for when salt shakes out

--- simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/common/view/SaltShakerNode.java
+++ simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/common/view/SaltShakerNode.java
@@ -1,6 +1,8 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.sugarandsaltsolutions.common.view;
 
+import java.util.ArrayList;
+
 import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
 import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
@@ -18,6 +20,8 @@ import static edu.colorado.phet.sugarandsaltsolutions.SugarAndSaltSolutionsAppli
  * @author Sam Reid
  */
 public class SaltShakerNode extends PNode {
+    private ArrayList<Double> historyDY = new ArrayList<Double>();
+
     public SaltShakerNode( final ModelViewTransform transform, final VoidFunction1<ImmutableVector2D> addSalt ) {
         //Show the image of the shaker
         addChild( new PImage( RESOURCES.getImage( "shaker.png" ) ) );
@@ -27,11 +31,21 @@ public class SaltShakerNode extends PNode {
         addInputEventListener( new PBasicInputEventHandler() {
             @Override public void mouseDragged( PInputEvent event ) {
                 //Move the shaker
-                translate( event.getCanvasDelta().getWidth(), event.getCanvasDelta().getHeight() );
+                double dx = event.getCanvasDelta().getWidth();
+                double dy = event.getCanvasDelta().getHeight();
+                translate( dx, dy );
 
-                //If it was a proper shake, emit salt crystals
-                if ( event.getCanvasDelta().getHeight() < 0 ) {
-                    addSalt.apply( new ImmutableVector2D( transform.viewToModel( event.getCanvasPosition() ) ) );
+                //Keep track of the deltas in the y direction since they are used to determine if any salt should shake out
+                if ( dy != 0 ) {
+                    historyDY.add( dy );
+                }
+
+                //If it was a proper shake, emit salt crystals.  This means the salt shaker was moving down, then up
+                if ( historyDY.size() > 2 ) {
+                    double previousDY = historyDY.get( historyDY.size() - 2 );
+                    if ( previousDY >= 0 && dy < 0 ) {
+                        addSalt.apply( new ImmutableVector2D( transform.viewToModel( event.getCanvasPosition() ) ) );
+                    }
                 }
             }
         } );
