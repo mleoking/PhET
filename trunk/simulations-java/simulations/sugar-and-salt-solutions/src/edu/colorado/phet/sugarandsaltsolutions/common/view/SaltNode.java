26c8db4afba1f376e735e4672a5e662cc915cf9c
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-05-06 19:26:21 +0000

    Made background black and crystals white

--- trunk/simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/common/view/SaltNode.java
+++ trunk/simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/common/view/SaltNode.java
@@ -10,7 +10,7 @@ import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.colorado.phet.sugarandsaltsolutions.common.model.Salt;
 import edu.umd.cs.piccolo.PNode;
 
-import static java.awt.Color.lightGray;
+import static java.awt.Color.white;
 
 /**
  * Graphical representation of a salt crystal
@@ -20,7 +20,7 @@ import static java.awt.Color.lightGray;
 public class SaltNode extends PNode {
     public SaltNode( final ModelViewTransform transform, final Salt salt ) {
         //Draw the shape of the salt crystal at its location
-        addChild( new PhetPPath( lightGray ) {{
+        addChild( new PhetPPath( white ) {{
             salt.position.addObserver( new VoidFunction1<ImmutableVector2D>() {
                 public void apply( ImmutableVector2D value ) {
                     ImmutableVector2D position = transform.modelToView( value );
--- trunk/simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/common/view/SugarNode.java
+++ trunk/simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/common/view/SugarNode.java
@@ -10,7 +10,7 @@ import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.colorado.phet.sugarandsaltsolutions.common.model.Sugar;
 import edu.umd.cs.piccolo.PNode;
 
-import static java.awt.Color.lightGray;
+import static java.awt.Color.white;
 
 /**
  * Graphical representation of a sugar crystal
@@ -20,7 +20,7 @@ import static java.awt.Color.lightGray;
 public class SugarNode extends PNode {
     public SugarNode( final ModelViewTransform transform, final Sugar sugar ) {
         //Draw the shape of the sugar crystal at its location
-        addChild( new PhetPPath( lightGray ) {{
+        addChild( new PhetPPath( white ) {{
             sugar.position.addObserver( new VoidFunction1<ImmutableVector2D>() {
                 public void apply( ImmutableVector2D modelPosition ) {
                     ImmutableVector2D viewPosition = transform.modelToView( modelPosition );
