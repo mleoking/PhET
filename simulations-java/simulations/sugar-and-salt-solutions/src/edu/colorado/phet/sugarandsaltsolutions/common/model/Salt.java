f3f0089b3cc24abfe3fc8d7b9135e1740914ffe3
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-05-06 18:32:57 +0000

    made it so each grain of sugar and salt as 1E-6 moles (no randomness)

--- simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/common/model/Salt.java
+++ simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/common/model/Salt.java
@@ -9,6 +9,11 @@ import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
  * @author Sam Reid
  */
 public class Salt extends Crystal {
+    //Create a salt crystal with 1E-6 moles of salt
+    public Salt( ImmutableVector2D position ) {
+        this( position, 1E-6 );
+    }
+
     public Salt( ImmutableVector2D position, double moles ) {
         super( position, moles );
     }
