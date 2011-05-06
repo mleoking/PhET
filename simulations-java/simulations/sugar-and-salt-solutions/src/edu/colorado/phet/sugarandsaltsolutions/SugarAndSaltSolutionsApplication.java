0ce7874bb2e2258f246ac468d724afdd84fe994d
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-05-06 23:53:03 +0000

    Made water a lighter blue Made the bar chart background the same color as the water

--- simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/SugarAndSaltSolutionsApplication.java
+++ simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/SugarAndSaltSolutionsApplication.java
@@ -1,6 +1,8 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.sugarandsaltsolutions;
 
+import java.awt.*;
+
 import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
 import edu.colorado.phet.common.phetcommon.application.PhetApplicationLauncher;
 import edu.colorado.phet.common.phetcommon.resources.PhetResources;
@@ -16,6 +18,7 @@ import edu.colorado.phet.sugarandsaltsolutions.micro.MicroscopicModule;
 public class SugarAndSaltSolutionsApplication extends PiccoloPhetApplication {
     private static final String NAME = "sugar-and-salt-solutions";
     public static final PhetResources RESOURCES = new PhetResources( NAME );
+    public static final Color WATER_COLOR = new Color( 179, 239, 243 );
 
     public SugarAndSaltSolutionsApplication( PhetApplicationConfig config ) {
         super( config );
--- simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/common/view/WaterNode.java
+++ simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/common/view/WaterNode.java
@@ -1,11 +1,10 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.sugarandsaltsolutions.common.view;
 
-import java.awt.*;
-
 import edu.colorado.phet.common.phetcommon.util.function.VoidFunction0;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
+import edu.colorado.phet.sugarandsaltsolutions.SugarAndSaltSolutionsApplication;
 import edu.colorado.phet.sugarandsaltsolutions.common.model.Water;
 import edu.umd.cs.piccolo.PNode;
 
@@ -14,7 +13,7 @@ import edu.umd.cs.piccolo.PNode;
  */
 public class WaterNode extends PNode {
     public WaterNode( final ModelViewTransform transform, final Water water ) {
-        addChild( new PhetPPath( Color.blue ) {{
+        addChild( new PhetPPath( SugarAndSaltSolutionsApplication.WATER_COLOR ) {{
             water.volume.addObserver( new VoidFunction0() {
                 public void apply() {
                     setPathTo( transform.modelToView( water.getShape() ) );
