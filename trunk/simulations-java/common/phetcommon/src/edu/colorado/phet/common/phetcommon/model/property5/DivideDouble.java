fef7a07e5c3c59cfc352b74e7b80824b968419cb
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-05-07 18:12:51 +0000

    Fixed: If you empty the solution, the bar charts go off-scale and stay that way when you add more water

--- trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/model/property5/DivideDouble.java
+++ trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/model/property5/DivideDouble.java
@@ -4,7 +4,8 @@ package edu.colorado.phet.common.phetcommon.model.property5;
 import edu.colorado.phet.common.phetcommon.util.function.Function0;
 
 /**
- * Observable property that computes the quotient of its arguments.
+ * Observable property that computes the quotient of its arguments, but redefines 0/0 to be 0 instead of infinity which is
+ * usually the appropriate behavior in physical scenarios.
  *
  * @author Sam Reid
  */
@@ -12,7 +13,12 @@ public class DivideDouble extends CompositeProperty<Double> {
     public DivideDouble( final ObservableProperty<Double> numerator, final ObservableProperty<Double> denominator ) {
         super( new Function0<Double>() {
                    public Double apply() {
-                       return numerator.getValue() / denominator.getValue();
+                       if ( numerator.getValue() == 0 ) {
+                           return 0.0;
+                       }
+                       else {
+                           return numerator.getValue() / denominator.getValue();
+                       }
                    }
                }, numerator, denominator );
     }
