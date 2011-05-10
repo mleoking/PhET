e13e961863326c8d526a52c3e681a8e85260c5be
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-05-10 02:57:16 +0000

    organize imports

--- trunk/simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/view/ConductivityTesterNode.java
+++ trunk/simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/view/ConductivityTesterNode.java
@@ -11,7 +11,6 @@ import java.text.DecimalFormat;
 
 import edu.colorado.phet.acidbasesolutions.constants.ABSImages;
 import edu.colorado.phet.acidbasesolutions.constants.ABSSymbols;
-import edu.colorado.phet.acidbasesolutions.model.ConductivityTester;
 import edu.colorado.phet.acidbasesolutions.model.ConductivityTester.ConductivityTesterChangeListener;
 import edu.colorado.phet.acidbasesolutions.model.SolutionRepresentation.SolutionRepresentationChangeAdapter;
 import edu.colorado.phet.common.phetcommon.math.Function.LinearFunction;
@@ -30,7 +29,7 @@ import edu.umd.cs.piccolox.nodes.PComposite;
 /**
  * Visual representation of the conductivity tester.
  * A simple circuit with a battery and a light bulb, and a probe at each end of the circuit.
- * When the probes are inserted in the solution, the circuit is completed, and the light bulb 
+ * When the probes are inserted in the solution, the circuit is completed, and the light bulb
  * lights up based on the tester's brightness value.
  * Origin is at the bottom-center of the light bulb.
  *
@@ -42,39 +41,39 @@ public class ConductivityTesterNode extends PhetPNode {
     private static final double PERCENT_LIGHT_BULB_ATTACHMENT = 0.12; // percent of light bulb's full height, from bottom of bulb, determines where to attach the probe wire
     private static final LinearFunction BRIGHTNESS_TO_ALPHA_FUNCTION = new LinearFunction( 0, 1, 0.85, 1 ); // alpha of the bulb
     private static final LinearFunction BRIGHTNESS_TO_INTENSITY_FUNCTION = new LinearFunction( 0, 1, 0, 1 ); // intensity of the light rays
-    
+
     // connector wire, connects bulb and battery
     private static final Color CONNECTOR_WIRE_COLOR = Color.BLACK;
     private static final double CONNECTOR_WIRE_LENGTH = 50;
-    
+
     // general probe properties
     private static final Color PROBE_STROKE_COLOR = Color.BLACK;
     private static final Stroke PROBE_STROKE = new BasicStroke( 1f );
     private static final Font PROBE_LABEL_FONT = new PhetFont( Font.BOLD, 24 );
-    
+
     // positive probe properties
     private static final Color POSITIVE_PROBE_FILL_COLOR = Color.RED;
     private static final String POSITIVE_PROBE_LABEL = ABSSymbols.PLUS;
     private static final Color POSITIVE_PROBE_LABEL_COLOR = Color.WHITE;
-    
+
     // negative probe properties
     private static final Color NEGATIVE_PROBE_FILL_COLOR = Color.BLACK;
     private static final String NEGATIVE_PROBE_LABEL = ABSSymbols.MINUS;
     private static final Color NEGATIVE_PROBE_LABEL_COLOR = Color.WHITE;
-    
+
     // general wire properties
     private static final Stroke WIRE_STROKE = new BasicStroke( 3f );
-    
+
     // positive wire properties
     private static final Color POSITIVE_WIRE_COLOR = Color.BLACK;
     private static final int POSITIVE_WIRE_CONTROL_POINT_DX = 25;
     private static final int POSITIVE_WIRE_CONTROL_POINT_DY = -100;
-    
+
     // negative wire properties
     private static final Color NEGATIVE_WIRE_COLOR = Color.BLACK;
     private static final int NEGATIVE_WIRE_CONTROL_POINT_DX = -POSITIVE_WIRE_CONTROL_POINT_DX;
     private static final int NEGATIVE_WIRE_CONTROL_POINT_DY = POSITIVE_WIRE_CONTROL_POINT_DY;
-    
+
     private final IConductivityTester tester;
 
     private final LightBulbNode lightBulbNode;
@@ -83,27 +82,28 @@ public class ConductivityTesterNode extends PhetPNode {
     private final ProbeNode positiveProbeNode, negativeProbeNode;
     private final CubicWireNode positiveWireNode, negativeWireNode;
     private final ValueNode valueNode;
-    
+
     /**
      * Constructor.
+     *
      * @param tester model element
-     * @param dev whether to enable developer features
+     * @param dev    whether to enable developer features
      */
     public ConductivityTesterNode( final IConductivityTester tester, boolean dev ) {
-        
+
         this.tester = tester;
         tester.addSolutionRepresentationChangeListener( new SolutionRepresentationChangeAdapter() {
             public void visibilityChanged() {
                 setVisible( tester.isVisible() );
             }
-        });
-        
+        } );
+
         tester.addConductivityTesterChangeListener( new ConductivityTesterChangeListener() {
 
             public void positiveProbeLocationChanged() {
                 updatePositiveProbeLocation();
             }
-            
+
             public void negativeProbeLocationChanged() {
                 updateNegativeProbeLocation();
             }
@@ -111,25 +111,25 @@ public class ConductivityTesterNode extends PhetPNode {
             public void brightnessChanged() {
                 updateBrightness();
             }
-        });
-        
+        } );
+
         // light bulb
         lightBulbNode = new LightBulbNode();
         lightBulbNode.setScale( 0.6 );
-        
+
         // light rays
         double lightBulbRadius = lightBulbNode.getFullBoundsReference().getWidth() / 2;
         lightRaysNode = new LightRaysNode( lightBulbRadius );
-        
+
         // battery
         batteryNode = new BatteryNode();
-        
+
         // wire that connects the light bulb to the battery
         StraightWireNode connectorWireNode = new StraightWireNode( CONNECTOR_WIRE_COLOR );
         Point2D lightBulbConnectionPoint = new Point2D.Double( 0, 0 );
         Point2D batteryConnectionPoint = new Point2D.Double( 0, CONNECTOR_WIRE_LENGTH );
         connectorWireNode.setEndPoints( lightBulbConnectionPoint, batteryConnectionPoint );
-        
+
         // positive probe
         positiveProbeNode = new ProbeNode( tester.getProbeSizeReference(), POSITIVE_PROBE_FILL_COLOR, POSITIVE_PROBE_LABEL, POSITIVE_PROBE_LABEL_COLOR );
         positiveProbeNode.addInputEventListener( new CursorHandler( Cursor.N_RESIZE_CURSOR ) );
@@ -162,7 +162,7 @@ public class ConductivityTesterNode extends PhetPNode {
             protected void startDrag( PInputEvent event ) {
                 super.startDrag( event );
                 Point2D pMouse = event.getPositionRelativeTo( getParent() );
-                clickYOffset = pMouse.getY() - tester.getNegativeProbeLocationReference().getY()  + tester.getLocationReference().getY();
+                clickYOffset = pMouse.getY() - tester.getNegativeProbeLocationReference().getY() + tester.getLocationReference().getY();
             }
 
             protected void drag( final PInputEvent event ) {
@@ -172,16 +172,16 @@ public class ConductivityTesterNode extends PhetPNode {
                 tester.setNegativeProbeLocation( tester.getNegativeProbeLocationReference().getX(), y );
             }
         } );
-        
+
         // positive wire
         positiveWireNode = new CubicWireNode( POSITIVE_WIRE_COLOR, POSITIVE_WIRE_CONTROL_POINT_DX, POSITIVE_WIRE_CONTROL_POINT_DY );
 
         // negative wire
         negativeWireNode = new CubicWireNode( NEGATIVE_WIRE_COLOR, NEGATIVE_WIRE_CONTROL_POINT_DX, NEGATIVE_WIRE_CONTROL_POINT_DY );
-        
+
         // brightness value, for debugging
         valueNode = new ValueNode();
-        
+
         // rendering order
         addChild( lightRaysNode );
         addChild( positiveWireNode );
@@ -194,7 +194,7 @@ public class ConductivityTesterNode extends PhetPNode {
         if ( dev ) {
             addChild( valueNode );
         }
-        
+
         // layout 
         double x = 0;
         double y = 0;
@@ -211,7 +211,7 @@ public class ConductivityTesterNode extends PhetPNode {
         valueNode.setOffset( x, y );
         updatePositiveProbeLocation();
         updateNegativeProbeLocation();
-        
+
         // location & visibility
         setOffset( tester.getLocationReference() );
         setVisible( tester.isVisible() );
@@ -233,9 +233,9 @@ public class ConductivityTesterNode extends PhetPNode {
         Point2D probeConnectionPoint = new Point2D.Double( x, y );
         positiveWireNode.setEndPoints( batteryConnectionPoint, probeConnectionPoint );
     }
-    
+
     private void updateNegativeProbeLocation() {
-        
+
         // probe
         double x = tester.getNegativeProbeLocationReference().getX() - tester.getLocationReference().getX();
         double y = tester.getNegativeProbeLocationReference().getY() - tester.getLocationReference().getY();
@@ -250,33 +250,33 @@ public class ConductivityTesterNode extends PhetPNode {
         Point2D probeConnectionPoint = new Point2D.Double( x, y );
         negativeWireNode.setEndPoints( componentConnectionPoint, probeConnectionPoint );
     }
-    
+
     private void updateBrightness() {
         lightBulbNode.setGlassTransparency( (float) BRIGHTNESS_TO_ALPHA_FUNCTION.evaluate( tester.getBrightness() ) );
         lightRaysNode.setIntensity( BRIGHTNESS_TO_INTENSITY_FUNCTION.evaluate( tester.getBrightness() ) );
         valueNode.setValue( tester.getBrightness() );
     }
-    
+
     /*
-     * Light bulb, origin at bottom center.
-     */
+    * Light bulb, origin at bottom center.
+    */
     private static class LightBulbNode extends PComposite {
-        
+
         private final PImage glassNode;
-        
+
         public LightBulbNode() {
-            
+
             PNode baseNode = new PImage( ABSImages.LIGHT_BULB_BASE );
-            
+
             glassNode = new PImage( ABSImages.LIGHT_BULB_GLASS );
-            
+
             PNode maskNode = new PImage( ABSImages.LIGHT_BULB_GLASS_MASK );
-            
+
             // rendering order
             addChild( maskNode );
             addChild( glassNode );
             addChild( baseNode );
-            
+
             // layout
             double x = -baseNode.getFullBoundsReference().getWidth() / 2;
             double y = -baseNode.getFullBoundsReference().getHeight();
@@ -286,15 +286,15 @@ public class ConductivityTesterNode extends PhetPNode {
             glassNode.setOffset( x, y );
             maskNode.setOffset( glassNode.getOffset() );
         }
-        
+
         public void setGlassTransparency( float transparency ) {
             glassNode.setTransparency( transparency );
         }
     }
-    
+
     /*
-     * Battery, origin at left center.
-     */
+    * Battery, origin at left center.
+    */
     private static class BatteryNode extends PComposite {
         public BatteryNode() {
             PImage imageNode = new PImage( ABSImages.BATTERY );
@@ -304,47 +304,47 @@ public class ConductivityTesterNode extends PhetPNode {
             imageNode.setOffset( x, y );
         }
     }
-    
+
     /*
-     * Probe, origin at bottom center.
-     */
+    * Probe, origin at bottom center.
+    */
     private static class ProbeNode extends PhetPNode {
-        
+
         public ProbeNode( PDimension size, Color color, String label, Color labelColor ) {
-            
+
             PPath pathNode = new PPath( new Rectangle2D.Double( -size.getWidth() / 2, -size.getHeight(), size.getWidth(), size.getHeight() ) );
             pathNode.setStroke( PROBE_STROKE );
             pathNode.setStrokePaint( PROBE_STROKE_COLOR );
             pathNode.setPaint( color );
             addChild( pathNode );
-            
+
             PText labelNode = new PText( label );
             labelNode.setTextPaint( labelColor );
             labelNode.setFont( PROBE_LABEL_FONT );
             addChild( labelNode );
-            
+
             double x = pathNode.getFullBoundsReference().getCenterX() - ( labelNode.getFullBoundsReference().getWidth() / 2 );
             double y = pathNode.getFullBoundsReference().getMaxY() - labelNode.getFullBoundsReference().getHeight() - 3;
             labelNode.setOffset( x, y );
         }
     }
-    
+
     /* Interface implemented by all wires. */
     private interface IWire {
         public void setEndPoints( Point2D startPoint, Point2D endPoint );
     }
-    
+
     /*
-     * Wire that is drawn as a straight line.
-     */
+    * Wire that is drawn as a straight line.
+    */
     private static class StraightWireNode extends PPath implements IWire {
-        
+
         public StraightWireNode( Color color ) {
             super();
             setStroke( WIRE_STROKE );
             setStrokePaint( color );
         }
-        
+
         public void setEndPoints( Point2D startPoint, Point2D endPoint ) {
             setPathTo( new Line2D.Double( 0, 0, CONNECTOR_WIRE_LENGTH, 0 ) );
         }
@@ -370,24 +370,24 @@ public class ConductivityTesterNode extends PhetPNode {
             setPathTo( new CubicCurve2D.Double( startPoint.getX(), startPoint.getY(), ctrl1.getX(), ctrl1.getY(), ctrl2.getX(), ctrl2.getY(), endPoint.getX(), endPoint.getY() ) );
         }
     }
-    
+
     /*
-     * Displays the model's brightness value, for debugging.
-     */
+    * Displays the model's brightness value, for debugging.
+    */
     private static class ValueNode extends PText {
-        
+
         private static final DecimalFormat FORMAT = new DecimalFormat( "0.000" );
-        
+
         public ValueNode() {
             this( 0 );
         }
-        
+
         public ValueNode( double brightness ) {
             setTextPaint( Color.RED );
             setFont( new PhetFont( 16 ) );
             setValue( brightness );
         }
-        
+
         public void setValue( double brightness ) {
             setText( "brightness=" + FORMAT.format( brightness ) ); // no i18n needed, this is a dev feature
         }
