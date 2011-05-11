df19885370d1417f880cdf8523dd1704b308685b
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-05-11 01:17:47 +0000

    bam: updated molecule table to use JTable

--- simulations-java/simulations/build-a-molecule/src/edu/colorado/phet/buildamolecule/tests/MoleculeTableDialog.java
+++ simulations-java/simulations/build-a-molecule/src/edu/colorado/phet/buildamolecule/tests/MoleculeTableDialog.java
@@ -2,17 +2,17 @@
 package edu.colorado.phet.buildamolecule.tests;
 
 import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 import javax.swing.*;
+import javax.swing.event.TableModelListener;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableModel;
 
-import edu.colorado.phet.buildamolecule.control.JmolDialog;
 import edu.colorado.phet.buildamolecule.model.CompleteMolecule;
-import edu.colorado.phet.common.phetcommon.resources.PhetCommonResources;
 import edu.colorado.phet.common.phetcommon.view.util.SwingUtils;
 import edu.colorado.phet.common.piccolophet.PhetPCanvas;
 import edu.umd.cs.piccolo.PNode;
@@ -24,7 +24,7 @@ import edu.umd.cs.piccolo.PNode;
 public class MoleculeTableDialog extends JDialog {
     public MoleculeTableDialog( final Frame owner ) {
         super( owner, false );
-        List<CompleteMolecule> list = CompleteMolecule.getAllCompleteMolecules();
+        final List<CompleteMolecule> list = new ArrayList<CompleteMolecule>( CompleteMolecule.getAllCompleteMolecules() );
         Collections.sort( list, new Comparator<CompleteMolecule>() {
             public int compare( CompleteMolecule o1, CompleteMolecule o2 ) {
                 int sizeCompare = Double.compare( o1.getMoleculeStructure().getAtoms().size(), o2.getMoleculeStructure().getAtoms().size() );
@@ -35,41 +35,90 @@ public class MoleculeTableDialog extends JDialog {
             }
         } );
 
-        JPanel content = new JPanel( new GridBagLayout() );
-        int count = 0;
-        for ( final CompleteMolecule completeMolecule : list ) {
-            final int finalCount = count;
-            final GridBagConstraints constraints = new GridBagConstraints() {{
-                gridx = RELATIVE;
-                gridy = finalCount;
-                anchor = WEST;
-                insets = new Insets( 4, 4, 4, 4 );
-            }};
-            content.add( new JTextField( completeMolecule.getDisplayName() ) {{
-                setEditable( false );
-                setBorder( null );
-            }}, constraints );
-            content.add( new JLabel( "<html>" + completeMolecule.getMoleculeStructure().getGeneralFormulaFragment() + "</html>" ), constraints );
-            content.add( new PhetPCanvas() {{
-                setBorder( null );
-                setBackground( new JPanel().getBackground() );
-                final PNode node = completeMolecule.createPseudo3DNode();
-                final Dimension preferredSize = new Dimension( (int) node.getFullBounds().getWidth() + 1, (int) node.getFullBounds().getHeight() + 1 );
-                setPreferredSize( preferredSize );
-                node.centerFullBoundsOnPoint( preferredSize.getWidth() / 2, preferredSize.getHeight() / 2 );
-                addScreenChild( node );
-            }}, constraints );
-            content.add( new JLabel( new ImageIcon( PhetCommonResources.getMaximizeButtonImage() ) ) {{
-                setCursor( Cursor.getPredefinedCursor( Cursor.HAND_CURSOR ) );
-                addMouseListener( new MouseAdapter() {
-                    @Override public void mousePressed( MouseEvent e ) {
-                        JmolDialog.displayMolecule3D( owner, completeMolecule );
-                    }
-                } );
-            }}, constraints );
-            count++;
-        }
-        setContentPane( new JScrollPane( content ) );
+//        final int[] rowHeights = new int[list.size()];
+//        for ( int row = 0; row < list.size(); row++ ) {
+//            CompleteMolecule molecule = list.get( row );
+//            PNode node = molecule.createPseudo3DNode();
+//            rowHeights[row] = (int) Math.max( 20, node.getHeight() + 10 );
+//        }
+
+        JTable table = new JTable( new TableModel() {
+            public int getRowCount() {
+                return list.size();
+            }
+
+            public int getColumnCount() {
+                return 4;
+            }
+
+            public String getColumnName( int columnIndex ) {
+                switch( columnIndex ) {
+                    case 0:
+                        return "Name";
+                    case 1:
+                        return "Formula";
+                    case 2:
+                        return "Pseudo-3D";
+                    case 3:
+                        return "PubChem CID";
+                    default:
+                        throw new RuntimeException( "unknown column: " + columnIndex );
+                }
+            }
+
+            public Class<?> getColumnClass( int columnIndex ) {
+                if ( columnIndex == 2 ) {
+                    return MoleculeCellRenderer.class;
+                }
+                return JLabel.class;
+//                if ( columnIndex == 0 ) {
+//                    return JLabel.class;
+//                }
+//                else {
+//                    throw new RuntimeException( "unknown column: " + columnIndex );
+//                }
+            }
+
+            public boolean isCellEditable( int rowIndex, int columnIndex ) {
+                return false;
+            }
+
+            public Object getValueAt( int rowIndex, int columnIndex ) {
+                switch( columnIndex ) {
+                    case 0:
+                        return list.get( rowIndex ).getDisplayName();
+                    case 1:
+                        return "<html>" + list.get( rowIndex ).getMoleculeStructure().getGeneralFormulaFragment() + "</html>";
+                    case 2:
+                        return list.get( rowIndex );
+                    case 3:
+                        return String.valueOf( list.get( rowIndex ).cid );
+                    default:
+                        throw new RuntimeException( "unknown column: " + columnIndex );
+                }
+            }
+
+            public void setValueAt( Object aValue, int rowIndex, int columnIndex ) {
+            }
+
+            public void addTableModelListener( TableModelListener l ) {
+            }
+
+            public void removeTableModelListener( TableModelListener l ) {
+            }
+        } ) {
+            {
+                setRowHeight( 75 );
+            }
+//            @Override
+//            public int getRowHeight( int row ) {
+//                return rowHeights[row];
+//            }
+        };
+
+        table.getColumnModel().getColumn( 2 ).setCellRenderer( new MoleculeCellRenderer() );
+
+        setContentPane( new JScrollPane( table ) );
         pack();
         setSize( new Dimension( 800, 600 ) );
         SwingUtils.centerInParent( this );
@@ -80,4 +129,30 @@ public class MoleculeTableDialog extends JDialog {
             setDefaultCloseOperation( DISPOSE_ON_CLOSE );
         }}.setVisible( true );
     }
+
+    private static class MoleculeCellRenderer extends PhetPCanvas implements TableCellRenderer {
+        private PNode node = null;
+
+        private MoleculeCellRenderer() {
+            setBorder( null );
+            setBackground( new JPanel().getBackground() );
+        }
+
+        public Component getTableCellRendererComponent( JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column ) {
+            if ( node != null ) {
+                removeScreenChild( node );
+            }
+
+            node = ( (CompleteMolecule) value ).createPseudo3DNode();
+            final Dimension preferredSize = new Dimension( (int) node.getFullBounds().getWidth() + 1, (int) node.getFullBounds().getHeight() + 1 );
+            setPreferredSize( preferredSize );
+            node.centerFullBoundsOnPoint( preferredSize.getWidth() / 2, preferredSize.getHeight() / 2 );
+            addScreenChild( node );
+
+            this.paintImmediately();
+
+            return this;
+        }
+    }
 }
+
