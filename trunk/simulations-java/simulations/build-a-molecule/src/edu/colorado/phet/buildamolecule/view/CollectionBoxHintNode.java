a1ebb05a098f19158bdd3ac2b00263c43762c571
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-05-11 19:30:13 +0000

    bam: animated blue collection box hint arrow

--- trunk/simulations-java/simulations/build-a-molecule/src/edu/colorado/phet/buildamolecule/view/CollectionBoxHintNode.java
+++ trunk/simulations-java/simulations/build-a-molecule/src/edu/colorado/phet/buildamolecule/view/CollectionBoxHintNode.java
@@ -3,6 +3,8 @@ package edu.colorado.phet.buildamolecule.view;
 
 import java.awt.*;
 import java.awt.geom.Rectangle2D;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import edu.colorado.phet.buildamolecule.model.CollectionBox;
 import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
@@ -13,21 +15,51 @@ import edu.umd.cs.piccolo.util.PBounds;
 
 public class CollectionBoxHintNode extends PNode {
     public CollectionBoxHintNode( ModelViewTransform mvt, PBounds moleculeDestinationBounds, CollectionBox box ) {
-        final ImmutableVector2D tip = mvt.modelToView( new ImmutableVector2D( box.getDropBounds().getMinX() - 20, box.getDropBounds().getCenterY() ) );
-        final ImmutableVector2D tail;
+
+        // at the end, where our tip and tail should be
+        final ImmutableVector2D tipTarget = mvt.modelToView( new ImmutableVector2D( box.getDropBounds().getMinX() - 20, box.getDropBounds().getCenterY() ) );
+        final ImmutableVector2D tailTarget;
+
+        // molecule view bounds on creation
         final Rectangle2D moleculeViewBounds = mvt.modelToViewRectangle( moleculeDestinationBounds );
 
         double maxArrowLength = 100;
-        ImmutableVector2D farTailStart = mvt.modelToView( new ImmutableVector2D( moleculeDestinationBounds.getMaxX() + 20, moleculeDestinationBounds.getCenterY() ) );
-        if ( tip.getDistance( farTailStart ) > maxArrowLength ) {
-            tail = farTailStart.getSubtractedInstance( tip ).getNormalizedInstance().getScaledInstance( maxArrowLength ).getAddedInstance( tip );
+        final ImmutableVector2D tailStart = mvt.modelToView( new ImmutableVector2D( moleculeDestinationBounds.getMaxX() + 20, moleculeDestinationBounds.getCenterY() ) );
+        if ( tipTarget.getDistance( tailStart ) > maxArrowLength ) {
+            tailTarget = tailStart.getSubtractedInstance( tipTarget ).getNormalizedInstance().getScaledInstance( maxArrowLength ).getAddedInstance( tipTarget );
         }
         else {
-            tail = farTailStart;
+            tailTarget = tailStart;
         }
-        addChild( new ArrowNode( tail.toPoint2D(), tip.toPoint2D(), 30, 40, 20 ) {{
+
+        final ImmutableVector2D tailOffset = tipTarget.getSubtractedInstance( tailTarget );
+        final ImmutableVector2D tipStart = tailStart.getAddedInstance( tailOffset );
+
+        final ArrowNode blueArrow = new ArrowNode( tailStart.toPoint2D(), tipStart.toPoint2D(), 30, 40, 20 ) {{
             setPaint( Color.BLUE );
-        }} );
+        }};
+        addChild( blueArrow );
+
+        // animate the blue arrow, using position = target * a + start * (1-a)
+        new Timer() {
+            double alpha = 0;
+            {
+                scheduleAtFixedRate( new TimerTask() {
+                                         @Override public void run() {
+                                             alpha += 0.05;
+                                             if ( alpha > 1 ) {
+                                                 alpha = 1;
+                                                 cancel(); // prevent timer from calling in the future
+                                             }
+                                             double heuristicAlpha = Math.pow( alpha, 0.8 );
+                                             ImmutableVector2D tip = tipStart.times( 1 - heuristicAlpha ).getAddedInstance( tipTarget.times( heuristicAlpha ) );
+                                             ImmutableVector2D tail = tailStart.times( 1 - heuristicAlpha ).getAddedInstance( tailTarget.times( heuristicAlpha ) );
+                                             blueArrow.setTipAndTailLocations( tip.toPoint2D(), tail.toPoint2D() );
+                                             blueArrow.repaint();
+                                         }
+                                     }, 30, 30 );
+            }
+        };
     }
 
     public void disperse() {
