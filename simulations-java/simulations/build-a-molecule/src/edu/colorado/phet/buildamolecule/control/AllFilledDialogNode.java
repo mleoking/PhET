dcb689fb72aae234eced880f3cf9de83437959e9
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-05-11 18:50:13 +0000

    bam: "try with different molecules" buttonNode as orange

--- simulations-java/simulations/build-a-molecule/src/edu/colorado/phet/buildamolecule/control/AllFilledDialogNode.java
+++ simulations-java/simulations/build-a-molecule/src/edu/colorado/phet/buildamolecule/control/AllFilledDialogNode.java
@@ -12,6 +12,7 @@ import edu.colorado.phet.buildamolecule.model.KitCollectionModel;
 import edu.colorado.phet.common.phetcommon.util.function.VoidFunction0;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
+import edu.colorado.phet.common.piccolophet.nodes.ButtonNode;
 import edu.colorado.phet.common.piccolophet.nodes.HTMLNode;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.umd.cs.piccolo.PNode;
@@ -26,27 +27,30 @@ public class AllFilledDialogNode extends PNode {
         PNode background = new PNode();
         addChild( background );
         addChild( new SwingLayoutNode( new GridBagLayout() ) {{
+            // text explaining all collection boxes are filled
             addChild( new HTMLNode( BuildAMoleculeStrings.COLLECTION_ALL_FILLED ) {{
-                setFont( new PhetFont( 20, true ) );
-            }},
+                          setFont( new PhetFont( 20, true ) );
+                      }},
                       new GridBagConstraints() {{
                           gridx = 0;
                           gridy = 0;
-                      }} );
-            addChild( new PSwing( new JButton( BuildAMoleculeStrings.COLLECTION_TRY_WITH_DIFFERENT_MOLECULES ) {{ // or how about just "I guess, since I have no other option"
-                addActionListener( new ActionListener() {
-                    public void actionPerformed( ActionEvent e ) {
-                        regenerateCallback.apply();
-                    }
-                } );
-            }} ) {{
-                setScale( 1.5 );
-            }},
+                      }}
+            );
+
+            // button to generate a new kit/collection
+            addChild( new ButtonNode( BuildAMoleculeStrings.COLLECTION_TRY_WITH_DIFFERENT_MOLECULES, 18, Color.ORANGE ) {{
+                          addActionListener( new ActionListener() {
+                              public void actionPerformed( ActionEvent e ) {
+                                  regenerateCallback.apply();
+                              }
+                          } );
+                      }},
                       new GridBagConstraints() {{
                           gridx = 0;
                           gridy = 1;
                           insets = new Insets( 10, 0, 0, 0 );
-                      }} );
+                      }}
+            );
         }} );
 
         float padding = 10;
