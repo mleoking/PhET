5771f283ce534fe6af2b72a8d7171de84be931c7
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-05-11 23:06:02 +0000

    #2876, doc IConductivityTester

--- simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/nodes/conductivitytester/IConductivityTester.java
+++ simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/nodes/conductivitytester/IConductivityTester.java
@@ -7,30 +7,65 @@ import java.util.EventListener;
 import edu.umd.cs.piccolo.util.PDimension;
 
 /**
- * Interface used by ConductivityTesterNode
+ * Interface implemented by all conductivity testers, used by ConductivityTesterNode.
  *
  * @author Sam Reid
  */
 public interface IConductivityTester {
+
     boolean isVisible();
 
-    void addConductivityTesterChangeListener( ConductivityTesterChangeListener conductivityTesterChangeListener );
+    /**
+     * Gets a reference to the location of the tester.
+     * Clients should take care not to modify this point, or pass it to methods
+     * that might modify the point.
+     *
+     * @return
+     */
+    Point2D getLocationReference();
 
-    PDimension getProbeSizeReference();
+    /**
+     * Sets the location of the positive probe.
+     * This is in the same coordinate frame as (not relative to) getLocation.
+     *
+     * @param x
+     * @param y
+     */
+    void setPositiveProbeLocation( double x, double y );
 
     Point2D getPositiveProbeLocationReference();
 
-    Point2D getLocationReference();
-
-    void setPositiveProbeLocation( double x, double y );
+    /**
+     * Sets the location of the negative probe.
+     * This is in the same coordinate frame as (not relative to) getLocation.
+     *
+     * @param x
+     * @param y
+     */
+    void setNegativeProbeLocation( double x, double y );
 
     Point2D getNegativeProbeLocationReference();
 
-    void setNegativeProbeLocation( double x, double y );
+    /**
+     * Gets the dimensions of the probe, in model coordinates.
+     *
+     * @return
+     */
+    PDimension getProbeSizeReference();
 
+    /**
+     * Gets the brightness of the tester, a number between 0 and 1 inclusive.
+     * 0 indicates no conductivity (open circuit)
+     * 1 indicates maximum conductivity that the device can display
+     *
+     * @return 0-1
+     */
     double getBrightness();
 
+    void addConductivityTesterChangeListener( ConductivityTesterChangeListener conductivityTesterChangeListener );
+
     public interface ConductivityTesterChangeListener extends EventListener {
+
         public void brightnessChanged();
 
         public void positiveProbeLocationChanged();
