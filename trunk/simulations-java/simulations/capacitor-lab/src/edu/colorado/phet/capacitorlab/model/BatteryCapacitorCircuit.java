ead9f4eb2f36abe31bac7b5f398a84ca1cd37be0
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-05-12 03:42:20 +0000

    fix: add missing notification in circuit.setBatteryConnected

--- trunk/simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/model/BatteryCapacitorCircuit.java
+++ trunk/simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/model/BatteryCapacitorCircuit.java
@@ -145,6 +145,7 @@ public class BatteryCapacitorCircuit extends AbstractCircuit {
             }
             this.batteryConnected = batteryConnected;
             updateVoltages();
+            fireCircuitChanged();
         }
     }
 
