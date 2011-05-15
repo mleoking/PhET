717aa588c9d447166f746e14616da51f2e8f1c7d
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-05-15 16:23:06 +0000

    Made it so the conductivity tester can be dropped back in the toolbox

--- simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/common/view/ConductivityTesterToolNode.java
+++ simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/common/view/ConductivityTesterToolNode.java
@@ -1,44 +1,32 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.sugarandsaltsolutions.common.view;
 
-import java.awt.geom.Dimension2D;
-
-import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
-import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
 import edu.colorado.phet.common.piccolophet.nodes.ToolNode;
-import edu.colorado.phet.common.piccolophet.nodes.conductivitytester.ConductivityTesterNode;
-import edu.colorado.phet.sugarandsaltsolutions.common.model.ConductivityTester;
+import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.util.PDimension;
 
 /**
  * Tool node that is created when the user drags the conductivity tester from its toolbox.
+ * This is modeled using composition since we must inherit from both ToolNode and ConductivityTesterNode
  *
  * @author Sam Reid
  */
 public class ConductivityTesterToolNode extends ToolNode {
-    private final ModelViewTransform transform;
-    private final ConductivityTester tester;
+    private final SugarAndSaltSolutionsConductivityTesterNode node;
 
     //Reuse the same ConductivityTesterNode instead of creating new ones each time we drag out of the toolbox.
-    public ConductivityTesterToolNode( ModelViewTransform transform, ConductivityTesterNode node, ConductivityTester tester ) {
-        this.transform = transform;
-        this.tester = tester;
+    public ConductivityTesterToolNode( SugarAndSaltSolutionsConductivityTesterNode node ) {
+        this.node = node;
         addChild( node );
     }
 
+    //Drag all parts of the conductivity tester when dragged out of the toolbox
     @Override public void dragAll( PDimension viewDelta ) {
-        //Drag the unit (battery + bulb) in view coordinates
-        ImmutableVector2D newLocation = new ImmutableVector2D( tester.getLocationReference() ).plus( viewDelta );
-        tester.setLocation( newLocation.getX(), newLocation.getY() );
+        node.dragAll( viewDelta );
+    }
 
-        //How far it was dragged in model coordinates
-        Dimension2D modelDelta = transform.viewToModelDelta( viewDelta );
-
-        //Drag the probes in model coordinates
-        ImmutableVector2D newPositiveProbeLocation = new ImmutableVector2D( tester.getPositiveProbeLocationReference() ).plus( modelDelta );
-        tester.setPositiveProbeLocation( newPositiveProbeLocation.getX(), newPositiveProbeLocation.getY() );
-
-        ImmutableVector2D newNegativeProbeLocation = new ImmutableVector2D( tester.getNegativeProbeLocationReference() ).plus( modelDelta );
-        tester.setNegativeProbeLocation( newNegativeProbeLocation.getX(), newNegativeProbeLocation.getY() );
+    //Identify parts that can be dropped back in the toolbox, in this case, the light bulb
+    @Override public PNode[] getDroppableComponents() {
+        return node.getDroppableComponents();
     }
 }
---
+++ simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/common/view/SugarAndSaltSolutionsConductivityTesterNode.java
@@ -0,0 +1,86 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.sugarandsaltsolutions.common.view;
+
+import java.awt.*;
+import java.awt.geom.Dimension2D;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+
+import edu.colorado.phet.common.phetcommon.util.function.Function0;
+import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
+import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
+import edu.colorado.phet.common.piccolophet.event.CursorHandler;
+import edu.colorado.phet.common.piccolophet.nodes.conductivitytester.ConductivityTesterNode;
+import edu.colorado.phet.sugarandsaltsolutions.common.model.ConductivityTester;
+import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
+import edu.umd.cs.piccolo.event.PInputEvent;
+import edu.umd.cs.piccolo.util.PDimension;
+
+/**
+ * Conductivity Tester Node specialized for Sugar and Salt Solutions.  This makes the light bulb draggable, and makes it possible to
+ * drag into and out of the toolbox.
+ *
+ * @author Sam Reid
+ */
+public class SugarAndSaltSolutionsConductivityTesterNode extends ConductivityTesterNode {
+    private final ConductivityTester conductivityTester;
+    private final ModelViewTransform transform;
+
+    public SugarAndSaltSolutionsConductivityTesterNode( final ConductivityTester conductivityTester, final ModelViewTransform transform, final PNode rootNode, final Function0<Rectangle2D> getToolboxBounds, Point2D location ) {
+        super( conductivityTester, transform, Color.lightGray, Color.red, Color.green, false );
+        this.conductivityTester = conductivityTester;
+        this.transform = transform;
+        conductivityTester.visible.addObserver( new VoidFunction1<Boolean>() {
+            public void apply( Boolean visible ) {
+                setVisible( visible );
+            }
+        } );
+        //Make it possible to drag the light bulb, which translates all parts of the conductivity tester (including probes)
+        lightBulbNode.addInputEventListener( new CursorHandler() );
+        lightBulbNode.addInputEventListener( new PBasicInputEventHandler() {
+            @Override public void mouseDragged( PInputEvent event ) {
+
+                //The bulb and battery drag in view coordinates
+                dragAll( event.getDeltaRelativeTo( rootNode ) );
+            }
+        } );
+        //initialize the probe locations so that relative locations will be equivalent to those when it is dragged out of the toolbox (same code used)
+
+        //Update the location of the body and probes for the conductivity tester, called on initialization
+        // (to make sure icon looks consistent) and when dragged out of the toolbox
+        Point2D viewLocation = transform.modelToView( location );
+        conductivityTester.setLocation( viewLocation.getX(), viewLocation.getY() );
+        conductivityTester.setNegativeProbeLocation( location.getX() - 0.03, location.getY() );
+        conductivityTester.setPositiveProbeLocation( location.getX() + 0.07, location.getY() );
+    }
+
+    //Used to create a thumbnail icon for use in the toolbox.
+    public Image createImage() {
+        //Generate a thumbnail of the conductivity tester node.  This is done by making it visible, calling toImage() and then making it invisible
+        boolean visible = conductivityTester.visible.get();
+        conductivityTester.visible.set( true );
+        Image image = toImage();
+        conductivityTester.visible.set( visible );//Restore default value
+        return image;
+    }
+
+    public void dragAll( PDimension viewDelta ) {
+
+        //Drag the conductivity tester in view coordinates
+        conductivityTester.setLocation( conductivityTester.getLocationReference().getX() + viewDelta.getWidth(),
+                                        conductivityTester.getLocationReference().getY() + viewDelta.getHeight() );
+
+        //The probes drag in model coordinates
+        Dimension2D modelDelta = transform.viewToModelDelta( viewDelta );
+        conductivityTester.setNegativeProbeLocation( conductivityTester.getNegativeProbeLocationReference().getX() + modelDelta.getWidth(),
+                                                     conductivityTester.getNegativeProbeLocationReference().getY() + modelDelta.getHeight() );
+        conductivityTester.setPositiveProbeLocation( conductivityTester.getPositiveProbeLocationReference().getX() + modelDelta.getWidth(),
+                                                     conductivityTester.getPositiveProbeLocationReference().getY() + modelDelta.getHeight() );
+    }
+
+    //Only the bulb can be dropped back in the toolbox since it is the only part that translates the unit
+    public PNode[] getDroppableComponents() {
+        return new PNode[] { lightBulbNode };
+    }
+}
