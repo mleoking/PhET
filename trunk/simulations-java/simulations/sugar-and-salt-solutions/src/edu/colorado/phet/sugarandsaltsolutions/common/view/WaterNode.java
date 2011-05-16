bf0ac954e815da75a7d5d3caaee0b2c9714d98fc
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-05-16 05:04:58 +0000

    Show the conductivity tester probes as being submerged underwater

--- trunk/simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/common/view/WaterNode.java
+++ trunk/simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/common/view/WaterNode.java
@@ -1,10 +1,11 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.sugarandsaltsolutions.common.view;
 
+import java.awt.*;
+
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
-import edu.colorado.phet.sugarandsaltsolutions.SugarAndSaltSolutionsApplication;
 import edu.colorado.phet.sugarandsaltsolutions.common.model.Water;
 import edu.umd.cs.piccolo.PNode;
 
@@ -12,8 +13,8 @@ import edu.umd.cs.piccolo.PNode;
  * @author Sam Reid
  */
 public class WaterNode extends PNode {
-    public WaterNode( final ModelViewTransform transform, final Water water ) {
-        addChild( new PhetPPath( SugarAndSaltSolutionsApplication.WATER_COLOR ) {{
+    public WaterNode( final ModelViewTransform transform, final Water water, Color color ) {
+        addChild( new PhetPPath( color ) {{
             water.volume.addObserver( new SimpleObserver() {
                 public void update() {
                     setPathTo( transform.modelToView( water.getShape() ) );
