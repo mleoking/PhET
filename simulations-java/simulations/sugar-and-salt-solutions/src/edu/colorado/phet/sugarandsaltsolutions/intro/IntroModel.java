3881c0d4728e28d699dbaabe5e2af706d99b6e68
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-05-17 03:17:52 +0000

    Fixed: precipitate dissolves when draining liquid

--- simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/intro/IntroModel.java
+++ simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/intro/IntroModel.java
@@ -3,14 +3,12 @@ package edu.colorado.phet.sugarandsaltsolutions.intro;
 
 import edu.colorado.phet.common.phetcommon.model.property.ObservableProperty;
 import edu.colorado.phet.common.phetcommon.model.property.Property;
+import edu.colorado.phet.common.phetcommon.model.property.SettableProperty;
 import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
 import edu.colorado.phet.sugarandsaltsolutions.common.model.SugarAndSaltSolutionModel;
 import edu.colorado.phet.sugarandsaltsolutions.intro.model.MacroCrystal;
 import edu.colorado.phet.sugarandsaltsolutions.intro.model.MacroSalt;
 import edu.colorado.phet.sugarandsaltsolutions.intro.model.MacroSugar;
-
-import static java.lang.Double.isInfinite;
-import static java.lang.Double.isNaN;
 
 /**
  * Introductory model that keeps track of moles of solute dissolved in the liquid.
@@ -20,8 +18,8 @@ import static java.lang.Double.isNaN;
 public class IntroModel extends SugarAndSaltSolutionModel {
 
     //Saturation points for salt and sugar assume 25 degrees C
-    private static final double saltSaturationPoint = 6.14;//moles per liter
-    private static final double sugarSaturationPoint = 5.85;//moles per liter
+    private static final double saltSaturationPoint = 6.14 * 1000;//6.14 moles per liter, converted to SI
+    private static final double sugarSaturationPoint = 5.85 * 1000;//5.85 moles per liter, converted to SI
 
     //Model moles, concentration, amount dissolved, amount precipitated, etc. for salt and sugar
     public final SoluteModel salt = new SoluteModel( water.volume, saltSaturationPoint );
@@ -30,6 +28,8 @@ public class IntroModel extends SugarAndSaltSolutionModel {
     //Determine if there are any solutes (i.e., if moles of salt or moles of sugar is greater than zero).  This is used to show/hide the "remove solutes" button
     public final ObservableProperty<Boolean> anySolutes = salt.moles.greaterThan( 0 ).or( sugar.moles.greaterThan( 0 ) );
     public final Property<Boolean> showConcentrationBarChart = new Property<Boolean>( true );
+    private double initialSaltConcentration;
+    private double initialSugarConcentration;
 
     public IntroModel() {
         salt.concentration.addObserver( new VoidFunction1<Double>() {
@@ -40,6 +40,13 @@ public class IntroModel extends SugarAndSaltSolutionModel {
                 updateConductivityTesterBrightness();
             }
         } );
+    }
+
+    @Override protected void updateModel( double dt ) {
+        //Have to record the concentrations before the model updates since the concentrations change if water is added or removed.
+        initialSaltConcentration = salt.concentration.get();
+        initialSugarConcentration = sugar.concentration.get();
+        super.updateModel( dt );
     }
 
     @Override public double getSaltConcentration() {
@@ -68,25 +75,14 @@ public class IntroModel extends SugarAndSaltSolutionModel {
     @Override protected void waterDrained( double outVolume ) {
         super.waterDrained( outVolume );
 
-        //Make sure to keep the concentration the same when water flowing out
-        updateConcentration( outVolume, salt.moles );
-        updateConcentration( outVolume, sugar.moles );
+        //Make sure to keep the concentration the same when water flowing out.  Use the values recorded before the model stepped to ensure conservation of solute moles
+        updateConcentration( outVolume, initialSaltConcentration, salt.moles );
+        updateConcentration( outVolume, initialSugarConcentration, sugar.moles );
     }
 
     //Make sure to keep the concentration the same when water flowing out
-    private void updateConcentration( double outVolume, Property<Double> property ) {
-        //Find what the concentration was before water exited
-        double initConcentration = property.get() / ( water.volume.get() + outVolume );
-
-        //If the water volume was zero, it could cause the initConcentration to be NaN, so handle error cases and just have all crystal moles exit in those cases.
-        if ( initConcentration <= 0 || isInfinite( initConcentration ) || isNaN( initConcentration ) ) {
-            initConcentration = 0;
-        }
-
-        //Find the new number of moles that keeps the concentration the same
-        double finalMoles = initConcentration * water.volume.get();
-
-        //Set the number of moles
-        property.set( finalMoles );
+    private void updateConcentration( double outVolume, double concentration, SettableProperty<Double> moles ) {
+        double molesOfSoluteLeaving = concentration * outVolume;
+        moles.set( moles.get() - molesOfSoluteLeaving );
     }
 }
