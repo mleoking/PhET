8da5096a594bf0ce53e1877fd3b4c6b8dd8bd003
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-05-17 18:55:21 +0000

    move battery and listeners to AbstractCircuit

--- simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/model/AbstractCircuit.java
+++ simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/model/AbstractCircuit.java
@@ -2,6 +2,10 @@
 
 package edu.colorado.phet.capacitorlab.model;
 
+import javax.swing.event.EventListenerList;
+
+import edu.colorado.phet.capacitorlab.CLConstants;
+
 /**
  * Base class for all circuits.
  *
@@ -10,12 +14,40 @@ package edu.colorado.phet.capacitorlab.model;
 public abstract class AbstractCircuit implements ICircuit {
 
     private final String displayName;
+    private final Battery battery;
+    private final EventListenerList listeners;
 
-    protected AbstractCircuit( String displayName ) {
+    protected AbstractCircuit( String displayName, CLModelViewTransform3D mvt ) {
         this.displayName = displayName;
+        this.battery = new Battery( CLConstants.BATTERY_LOCATION, CLConstants.BATTERY_VOLTAGE_RANGE.getDefault(), mvt );
+        this.listeners = new EventListenerList();
     }
 
     public String getDisplayName() {
         return displayName;
+    }
+
+    public Battery getBattery() {
+        return battery;
+    }
+
+    public void reset() {
+        battery.reset();
+    }
+
+    // @see ICircuit.addCircuitChangeListener
+    public void addCircuitChangeListener( CircuitChangeListener listener ) {
+        listeners.add( CircuitChangeListener.class, listener );
+    }
+
+    // @see ICircuit.removeCircuitChangeListener
+    public void removeCircuitChangeListener( CircuitChangeListener listener ) {
+        listeners.remove( CircuitChangeListener.class, listener );
+    }
+
+    protected void fireCircuitChanged() {
+        for ( CircuitChangeListener listener : listeners.getListeners( CircuitChangeListener.class ) ) {
+            listener.circuitChanged();
+        }
     }
 }
