28462f4b824a2ee98bb2b5164dae626ebf33e8db
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-05-18 05:55:47 +0000

    bam: Collection hint arrow: changed tip/tail locations and text position. Invisible on movement

--- trunk/simulations-java/simulations/build-a-molecule/src/edu/colorado/phet/buildamolecule/view/CollectionBoxHintNode.java
+++ trunk/simulations-java/simulations/build-a-molecule/src/edu/colorado/phet/buildamolecule/view/CollectionBoxHintNode.java
@@ -3,99 +3,99 @@ package edu.colorado.phet.buildamolecule.view;
 
 import java.awt.*;
 import java.awt.geom.Rectangle2D;
-import java.awt.geom.RoundRectangle2D;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
-import java.util.Timer;
-import java.util.TimerTask;
 
+import edu.colorado.phet.buildamolecule.BuildAMoleculeStrings;
+import edu.colorado.phet.buildamolecule.model.Atom2D;
+import edu.colorado.phet.buildamolecule.model.Atom2D.Adapter;
 import edu.colorado.phet.buildamolecule.model.CollectionBox;
+import edu.colorado.phet.buildamolecule.model.Molecule;
 import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.nodes.ArrowNode;
-import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.nodes.PText;
 import edu.umd.cs.piccolo.util.PBounds;
 
 public class CollectionBoxHintNode extends PNode {
-    public CollectionBoxHintNode( final ModelViewTransform mvt, PBounds moleculeDestinationBounds, final CollectionBox box ) {
+    public CollectionBoxHintNode( final ModelViewTransform mvt, final Molecule molecule, final CollectionBox box ) {
+
+        PBounds moleculeDestinationBounds = molecule.getDestinationBounds();
 
         // at the end, where our tip and tail should be
         final ImmutableVector2D tipTarget = mvt.modelToView( new ImmutableVector2D( box.getDropBounds().getMinX() - 20, box.getDropBounds().getCenterY() ) );
-        final ImmutableVector2D tailTarget;
 
         final Rectangle2D moleculeViewBounds = mvt.modelToViewRectangle( moleculeDestinationBounds );
         final Rectangle2D boxViewBounds = mvt.modelToView( box.getDropBounds() ).getBounds2D();
 
-        double maxArrowLength = 100;
-        final ImmutableVector2D tailStart = mvt.modelToView( new ImmutableVector2D( moleculeDestinationBounds.getMaxX() + 20, moleculeDestinationBounds.getCenterY() ) );
-        if ( tipTarget.getDistance( tailStart ) > maxArrowLength ) {
-            tailTarget = tailStart.getSubtractedInstance( tipTarget ).getNormalizedInstance().getScaledInstance( maxArrowLength ).getAddedInstance( tipTarget );
-        }
-        else {
-            tailTarget = tailStart;
-        }
+        PNode labelNode = new PNode() {{
+            addChild( new PText( BuildAMoleculeStrings.COLLECTION_HINT ) {{
+                setFont( new PhetFont( 16, true ) );
+                setTextPaint( Color.BLACK );
+            }} );
+            centerFullBoundsOnPoint( moleculeViewBounds.getCenterX(), moleculeViewBounds.getMaxY() + 5 + getFullBounds().getHeight() / 2 );
+        }};
+        addChild( labelNode );
 
-        final ImmutableVector2D tailOffset = tipTarget.getSubtractedInstance( tailTarget );
-        final ImmutableVector2D tipStart = tailStart.getAddedInstance( tailOffset );
+        ImmutableVector2D textEnd = new ImmutableVector2D( labelNode.getFullBounds().getMaxX() - 5, labelNode.getFullBounds().getCenterY() );
+        ImmutableVector2D direction = tipTarget.minus( textEnd ).getNormalizedInstance();
 
-        final ArrowNode blueArrow = new ArrowNode( tailStart.toPoint2D(), tipStart.toPoint2D(), 30, 40, 20 ) {{
+        final ArrowNode blueArrow = new ArrowNode( textEnd.plus( direction.times( 15 ) ).toPoint2D(), tipTarget.toPoint2D(), 30, 40, 20 ) {{
             setPaint( Color.BLUE );
         }};
         addChild( blueArrow );
 
-        addChild( new PNode() {
-            {
-                PText textNode = new PText( "Drag your molecule here" ) {{
-                    setFont( new PhetFont( 14, true ) );
-                    setTextPaint( Color.BLACK );
-                }};
-                double verticalPad = 5;
-                double horizontalPad = 5;
-                PBounds textBounds = textNode.getFullBounds();
-                addChild( new PhetPPath( new RoundRectangle2D.Double( textBounds.x - horizontalPad, textBounds.y - verticalPad, textBounds.getWidth() + horizontalPad * 2, textBounds.getHeight() + verticalPad * 2, 15, 15 ) ) {{
-                    setPaint( Color.WHITE );
-                    setStrokePaint( Color.BLACK );
-                }} );
-                addChild( textNode );
-                blueArrow.addPropertyChangeListener( new PropertyChangeListener() {
-                    public void propertyChange( PropertyChangeEvent evt ) {
-                        updatePosition();
-                    }
-                } );
-                updatePosition();
-            }
-
-            public void updatePosition() {
-                centerFullBoundsOnPoint( blueArrow.getTailLocation().getX(), blueArrow.getTailLocation().getY() );
-//                centerFullBoundsOnPoint( boxViewBounds.getCenterX(), boxViewBounds.getMaxY() );
-                repaint();
-            }
-        } );
-
-        // animate the blue arrow, using position = target * a + start * (1-a)
-        new Timer() {
-            double alpha = 0;
-
-            {
-                scheduleAtFixedRate( new TimerTask() {
-                                         @Override public void run() {
-                                             alpha += 0.05;
-                                             if ( alpha > 1 ) {
-                                                 alpha = 1;
-                                                 cancel(); // prevent timer from calling in the future
-                                             }
-                                             double heuristicAlpha = Math.pow( alpha, 0.8 );
-                                             ImmutableVector2D tip = tipStart.times( 1 - heuristicAlpha ).getAddedInstance( tipTarget.times( heuristicAlpha ) );
-                                             ImmutableVector2D tail = tailStart.times( 1 - heuristicAlpha ).getAddedInstance( tailTarget.times( heuristicAlpha ) );
-                                             blueArrow.setTipAndTailLocations( tip.toPoint2D(), tail.toPoint2D() );
-                                             blueArrow.repaint();
-                                         }
-                                     }, 30, 30 );
+        // hide this when we move the molecule
+        Adapter grabListener = new Adapter() {
+            @Override public void grabbedByUser( Atom2D particle ) {
+                disperse();
+                for ( Atom2D atom : molecule.getAtoms() ) {
+                    atom.removeListener( this );
+                }
             }
         };
+        for ( Atom2D atom : molecule.getAtoms() ) {
+            atom.addListener( grabListener );
+        }
+    }
+
+    /**
+     * Figures out a nice point around the rectangle (padding-distance from it) that the tip of an arrow should end at to
+     * point from outsidePoint
+     *
+     * @param bounds  Rectangle bounds
+     * @param padding Padding
+     * @param tail    Arrow tail
+     * @return Arrow tip
+     */
+    private ImmutableVector2D getInterceptPoint( Rectangle2D bounds, double padding, ImmutableVector2D tail ) {
+        ImmutableVector2D center = new ImmutableVector2D( bounds.getCenterX(), bounds.getCenterY() );
+        ImmutableVector2D unitDirection = center.minus( tail ).getNormalizedInstance();
+
+        double minX = bounds.getMinX() - padding;
+        double maxX = bounds.getMaxX() + padding;
+
+        double minY = bounds.getMinY() - padding;
+        double maxY = bounds.getMaxY() + padding;
+
+        ImmutableVector2D hitLocation = null;
+
+        // check for left/right hits
+        if ( tail.getX() < minX || tail.getX() > maxX ) {
+            double borderX = tail.getX() > bounds.getCenterX() ? maxX : minX;
+            double t = ( borderX - tail.getX() ) / unitDirection.getX();
+            hitLocation = tail.plus( unitDirection.times( t ) );
+        }
+        if ( tail.getY() < minY || tail.getY() > maxY ) {
+            double borderY = tail.getY() > bounds.getCenterY() ? maxY : minY;
+            double t = ( borderY - tail.getY() ) / unitDirection.getY();
+
+            ImmutableVector2D otherHitLocation = tail.plus( unitDirection.times( t ) );
+            if ( hitLocation == null || hitLocation.getDistance( center ) > otherHitLocation.getDistance( center ) ) {
+                hitLocation = otherHitLocation;
+            }
+        }
+        return hitLocation;
     }
 
     public void disperse() {
