8f7916dcc26e4fc1a0fb46be96e3796dc82c72d2
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-05-19 03:02:00 +0000

    add wires to parallel circuits (model and view)

---
+++ trunk/simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/model/wire/WireBatteryTopToNCapacitorTops.java
@@ -0,0 +1,49 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.capacitorlab.model.wire;
+
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+
+import edu.colorado.phet.capacitorlab.CLConstants;
+import edu.colorado.phet.capacitorlab.model.Battery;
+import edu.colorado.phet.capacitorlab.model.CLModelViewTransform3D;
+import edu.colorado.phet.capacitorlab.model.Capacitor;
+import edu.colorado.phet.capacitorlab.model.wire.WireSegment.BatteryTopWireSegment;
+import edu.colorado.phet.capacitorlab.model.wire.WireSegment.CapacitorTopWireSegment;
+import edu.colorado.phet.common.phetcommon.util.function.Function0;
+
+/**
+ * Wire that connects the top of a battery to the tops of N parallel capacitors.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
+public class WireBatteryTopToNCapacitorTops extends Wire {
+
+    public WireBatteryTopToNCapacitorTops( final CLModelViewTransform3D mvt, final double thickness, final Battery battery, final ArrayList<Capacitor> capacitors ) {
+        super( mvt, thickness, new Function0<ArrayList<WireSegment>>() {
+            public ArrayList<WireSegment> apply() {
+
+                // connect battery to the rightmost capacitor
+                final Capacitor rightmostCapacitor = capacitors.get( capacitors.size() - 1 );
+                final double y = Math.min( battery.getY() - CLConstants.WIRE_EXTENT, rightmostCapacitor.getLocation().getY() - 0.01 );//TODO clean this up
+                final Point2D.Double leftCorner = new Point2D.Double( battery.getX(), y );
+                final Point2D.Double rightCorner = new Point2D.Double( rightmostCapacitor.getX(), leftCorner.getY() );
+                final double t = ( thickness / 2 ); // for proper connection at corners with CAP_BUTT wire stroke
+                ArrayList<WireSegment> segments = new ArrayList<WireSegment>() {{
+                    add( new BatteryTopWireSegment( battery, leftCorner ) );
+                    add( new WireSegment( leftCorner.getX() - t, leftCorner.getY() + t, rightCorner.getX() + t, rightCorner.getY() + t ) );
+                    add( new CapacitorTopWireSegment( rightCorner, rightmostCapacitor ) );
+                }};
+
+                // add segments for all capacitors in between the battery and rightmost capacitor
+                for ( int i = 0; i < capacitors.size() - 1; i++ ) {
+                    Capacitor capacitor = capacitors.get( i );
+                    Point2D.Double startPoint = new Point2D.Double( capacitor.getX(), y );
+                    segments.add( new CapacitorTopWireSegment( startPoint, capacitor ) );
+                }
+
+                return segments;
+            }
+        } );
+    }
+}
--- trunk/simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/view/multicaps/ParallelCircuitNode.java
+++ trunk/simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/view/multicaps/ParallelCircuitNode.java
@@ -2,16 +2,20 @@
 package edu.colorado.phet.capacitorlab.view.multicaps;
 
 import java.awt.geom.Point2D;
+import java.util.ArrayList;
 
 import edu.colorado.phet.capacitorlab.control.CapacitanceControlNode;
 import edu.colorado.phet.capacitorlab.model.CLModelViewTransform3D;
 import edu.colorado.phet.capacitorlab.model.Capacitor;
 import edu.colorado.phet.capacitorlab.model.DielectricChargeView;
 import edu.colorado.phet.capacitorlab.model.circuit.ParallelCircuit;
+import edu.colorado.phet.capacitorlab.model.wire.Wire;
 import edu.colorado.phet.capacitorlab.module.multiplecapacitors.MultipleCapacitorsModel;
 import edu.colorado.phet.capacitorlab.view.CapacitorNode;
+import edu.colorado.phet.capacitorlab.view.WireNode;
 import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.piccolophet.util.PNodeLayoutUtils;
+import edu.umd.cs.piccolo.PNode;
 
 /**
  * Node for a circuit with capacitors in parallel.
@@ -25,6 +29,7 @@ public class ParallelCircuitNode extends AbstractCircuitNode {
                                 double maxPlateCharge, double maxExcessDielectricPlateCharge, double maxEffectiveEField, double maxDielectricEField ) {
         super( circuit, mvt );
 
+        // capacitors
         for ( Capacitor capacitor : circuit.getCapacitors() ) {
 
             // capacitor node, at its model location
@@ -45,6 +50,37 @@ public class ParallelCircuitNode extends AbstractCircuitNode {
             capacitanceControlNode.setOffset( x, y );
         }
 
-        //TODO add wires
+        // wires
+        ArrayList<Wire> wires = circuit.getWires();
+        assert ( wires.size() == 2 );
+
+        PNode topWireNode = new WireNode( wires.get( 0 ), mvt );
+        addChild( topWireNode );
+        topWireNode.moveToFront();
+
+        PNode bottomWireNode = new WireNode( wires.get( 1 ), mvt );
+        addChild( bottomWireNode );
+        bottomWireNode.moveToBack();
+
+        //TODO this code is similar in all circuit nodes
+        // Center the current indicators on the wires that are connected to the battery.
+        {
+            double x, y;
+
+            // top current indicator centered on top wire, between battery and first capacitor to the right of the battery
+            double topWireThickness = mvt.modelToViewDelta( wires.get( 0 ).getThickness(), 0, 0 ).getX();
+            double xModel = circuit.getBattery().getX() + ( ( circuit.getCapacitors().get( 0 ).getX() - circuit.getBattery().getX() ) / 2 );
+            x = mvt.modelToViewDelta( xModel, 0, 0 ).getX();
+            y = topWireNode.getFullBoundsReference().getMinY() + ( topWireThickness / 2 );
+            getTopCurrentIndicatorNode().setOffset( x, y );
+            getTopCurrentIndicatorNode().moveToFront();
+
+            // bottom current indicator centered on bottom wire
+            double bottomWireThickness = mvt.modelToViewDelta( wires.get( wires.size() - 1 ).getThickness(), 0, 0 ).getX();
+            x = getTopCurrentIndicatorNode().getXOffset();
+            y = bottomWireNode.getFullBoundsReference().getMaxY() - ( bottomWireThickness / 2 );
+            getBottomCurrentIndicatorNode().setOffset( x, y );
+            getBottomCurrentIndicatorNode().moveToFront();
+        }
     }
 }
