e4b5a32676901964064b61e24c13c7ac55b0987d
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-05-19 03:37:51 +0000

    add wires to combo circuits 1 (model and view)

--- trunk/simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/view/multicaps/Combination1CircuitNode.java
+++ trunk/simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/view/multicaps/Combination1CircuitNode.java
@@ -2,16 +2,20 @@
 package edu.colorado.phet.capacitorlab.view.multicaps;
 
 import java.awt.geom.Point2D;
+import java.util.ArrayList;
 
 import edu.colorado.phet.capacitorlab.control.CapacitanceControlNode;
 import edu.colorado.phet.capacitorlab.model.CLModelViewTransform3D;
 import edu.colorado.phet.capacitorlab.model.Capacitor;
 import edu.colorado.phet.capacitorlab.model.DielectricChargeView;
 import edu.colorado.phet.capacitorlab.model.circuit.Combination1Circuit;
+import edu.colorado.phet.capacitorlab.model.wire.Wire;
 import edu.colorado.phet.capacitorlab.module.multiplecapacitors.MultipleCapacitorsModel;
 import edu.colorado.phet.capacitorlab.view.CapacitorNode;
+import edu.colorado.phet.capacitorlab.view.WireNode;
 import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.piccolophet.util.PNodeLayoutUtils;
+import edu.umd.cs.piccolo.PNode;
 
 /**
  * Node for a circuit with a parallel and series combination of capacitors.
@@ -45,6 +49,40 @@ public class Combination1CircuitNode extends AbstractCircuitNode {
             capacitanceControlNode.setOffset( x, y );
         }
 
-        //TODO add wires
+        ArrayList<Wire> wires = circuit.getWires();
+        PNode topWireNode = null;
+        PNode bottomWireNode = null;
+        for ( int i = 0; i < wires.size(); i++ ) {
+            WireNode wireNode = new WireNode( wires.get( i ), mvt );
+            addChild( wireNode );
+
+            if ( i == 0 ) {
+                topWireNode = wireNode;
+            }
+            else if ( i == wires.size() - 1 ) {
+                bottomWireNode = wireNode;
+            }
+        }
+
+        //TODO this code is similar in all circuit nodes
+        // Center the current indicators on the wires that are connected to the battery.
+        {
+            double x, y;
+
+            // top current indicator centered on top wire, between battery and first capacitor to the right of the battery
+            double topWireThickness = mvt.modelToViewDelta( wires.get( 0 ).getThickness(), 0, 0 ).getX();
+            double xModel = circuit.getBattery().getX() + ( ( circuit.getCapacitors().get( 0 ).getX() - circuit.getBattery().getX() ) / 2 );
+            x = mvt.modelToViewDelta( xModel, 0, 0 ).getX();
+            y = topWireNode.getFullBoundsReference().getMinY() + ( topWireThickness / 2 );
+            getTopCurrentIndicatorNode().setOffset( x, y );
+            getTopCurrentIndicatorNode().moveToFront();
+
+            // bottom current indicator centered on bottom wire
+            double bottomWireThickness = mvt.modelToViewDelta( wires.get( wires.size() - 1 ).getThickness(), 0, 0 ).getX();
+            x = getTopCurrentIndicatorNode().getXOffset();
+            y = bottomWireNode.getFullBoundsReference().getMaxY() - ( bottomWireThickness / 2 );
+            getBottomCurrentIndicatorNode().setOffset( x, y );
+            getBottomCurrentIndicatorNode().moveToFront();
+        }
     }
 }
