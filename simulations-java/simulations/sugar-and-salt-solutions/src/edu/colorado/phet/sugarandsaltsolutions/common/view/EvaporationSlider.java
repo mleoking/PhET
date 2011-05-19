75f4f5b73b7d190e231ba4a38c05a67a97a34855
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-05-19 19:49:57 +0000

    Removed background from evaporation slider

--- simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/common/view/EvaporationSlider.java
+++ simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/common/view/EvaporationSlider.java
@@ -1,6 +1,7 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.sugarandsaltsolutions.common.view;
 
+import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.Hashtable;
@@ -8,7 +9,6 @@ import java.util.Hashtable;
 import javax.swing.*;
 
 import edu.colorado.phet.common.phetcommon.model.property.SettableProperty;
-import edu.colorado.phet.common.piccolophet.nodes.ControlPanelNode;
 import edu.colorado.phet.common.piccolophet.nodes.layout.HBox;
 import edu.umd.cs.piccolo.nodes.PText;
 import edu.umd.cs.piccolox.pswing.PSwing;
@@ -16,36 +16,46 @@ import edu.umd.cs.piccolox.pswing.PSwing;
 import static edu.colorado.phet.sugarandsaltsolutions.common.view.SugarAndSaltSolutionsCanvas.CONTROL_FONT;
 
 /**
+ * Modified copy of Evaporation slider, while we test different versions.  In the end, there should be only one.
+ *
  * @author Sam Reid
  */
-public class EvaporationSlider extends ControlPanelNode {
+public class EvaporationSlider extends HBox {
     public EvaporationSlider( final SettableProperty<Integer> evaporationRate ) {
-        super( new HBox() {{
-            //Add a label
-            addChild( new PText( "Evaporation" ) {{setFont( CONTROL_FONT );}} );
-
-            //Add the slider
-            addChild( new PSwing( new PropertySlider( 0, 100, evaporationRate ) {{
-                //Showing ticks makes the knob larger and pointy (even though there are no ticks
-                setPaintTicks( true );
-
-                //Show none and lots labels at the extrema
-                setPaintLabels( true );
-                setLabelTable( new Hashtable() {{
-                    put( 0, new JLabel( "none" ) );
-                    put( 100, new JLabel( "lots" ) );
-                }} );
-                addMouseListener( new MouseAdapter() {
-                    @Override public void mouseReleased( MouseEvent e ) {
-                        evaporationRate.set( 0 );
-
-                        //To make sure the slider goes back to zero, it is essential to set the value to something other than zero first
-                        //Just calling setValue(0) here or waiting for the callback from the model doesn't work if the user was dragging the knob
-                        setValue( 1 );
-                        setValue( 0 );
-                    }
-                } );
-            }} ) );
+//        super( new HBox() {{
+        //Add a label
+        addChild( new PText( "Evaporation" ) {{
+            setFont( CONTROL_FONT );
+            setTextPaint( Color.white );
         }} );
+
+        //Add the slider
+        addChild( new PSwing( new PropertySlider( 0, 100, evaporationRate ) {{
+            //Showing ticks makes the knob larger and pointy (even though there are no ticks
+            setPaintTicks( true );
+
+            //Show none and lots labels at the extrema
+            setPaintLabels( true );
+            setLabelTable( new Hashtable() {{
+                put( 0, new JLabel( "none" ) {{
+                    setForeground( Color.white );
+                }} );
+                put( 100, new JLabel( "lots" ) {{
+                    setForeground( Color.white );
+                }} );
+            }} );
+            addMouseListener( new MouseAdapter() {
+                @Override public void mouseReleased( MouseEvent e ) {
+                    evaporationRate.set( 0 );
+
+                    //To make sure the slider goes back to zero, it is essential to set the value to something other than zero first
+                    //Just calling setValue(0) here or waiting for the callback from the model doesn't work if the user was dragging the knob
+                    setValue( 1 );
+                    setValue( 0 );
+                }
+            } );
+            setBackground( new Color( 0, 0, 0, 0 ) );
+        }} ) );
+//        }} );
     }
 }
