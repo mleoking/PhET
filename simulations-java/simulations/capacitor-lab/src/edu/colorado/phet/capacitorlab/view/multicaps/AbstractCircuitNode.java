43e8fd6877cf1511700405b9c8298f9f3c8a959d
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-05-19 00:49:42 +0000

    add current indicators to circuit view base class

--- simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/view/multicaps/AbstractCircuitNode.java
+++ simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/view/multicaps/AbstractCircuitNode.java
@@ -8,6 +8,7 @@ import edu.colorado.phet.capacitorlab.model.Battery;
 import edu.colorado.phet.capacitorlab.model.CLModelViewTransform3D;
 import edu.colorado.phet.capacitorlab.model.circuit.ICircuit;
 import edu.colorado.phet.capacitorlab.view.BatteryNode;
+import edu.colorado.phet.capacitorlab.view.CurrentIndicatorNode;
 import edu.colorado.phet.common.piccolophet.PhetPNode;
 
 /**
@@ -18,18 +19,39 @@ import edu.colorado.phet.common.piccolophet.PhetPNode;
 public class AbstractCircuitNode extends PhetPNode {
 
     private final BatteryNode batteryNode;
+    private final CurrentIndicatorNode topCurrentIndicatorNode, bottomCurrentIndicatorNode;
 
     public AbstractCircuitNode( ICircuit circuit, CLModelViewTransform3D mvt ) {
 
+        // battery, at model location
         Battery battery = circuit.getBattery();
         batteryNode = new BatteryNode( battery, CLConstants.BATTERY_VOLTAGE_RANGE );
         addChild( batteryNode );
-        // battery at model location
         Point2D pView = mvt.modelToView( battery.getLocationReference() );
         batteryNode.setOffset( pView );
+
+        // current indicators
+        topCurrentIndicatorNode = new CurrentIndicatorNode( circuit, 0 );
+        topCurrentIndicatorNode.scale( 0.75 );
+        addChild( topCurrentIndicatorNode );
+        bottomCurrentIndicatorNode = new CurrentIndicatorNode( circuit, Math.PI );
+        bottomCurrentIndicatorNode.scale( 0.75 );
+        addChild( bottomCurrentIndicatorNode );
+
+        //TODO temporary locations, until we have wires
+        topCurrentIndicatorNode.setOffset( batteryNode.getFullBoundsReference().getMaxX(), batteryNode.getFullBounds().getMinY() - 50 );
+        bottomCurrentIndicatorNode.setOffset( topCurrentIndicatorNode.getXOffset(), batteryNode.getFullBounds().getMaxY() + 50 );
     }
 
     protected BatteryNode getBatteryNode() {
         return batteryNode;
+    }
+
+    protected CurrentIndicatorNode getTopCurrentIndicatorNode() {
+        return topCurrentIndicatorNode;
+    }
+
+    protected CurrentIndicatorNode getBottomCurrentIndicatorNode() {
+        return bottomCurrentIndicatorNode;
     }
 }
