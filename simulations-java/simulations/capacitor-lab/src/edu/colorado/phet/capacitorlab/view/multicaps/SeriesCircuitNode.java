ec52e98155b0ee4fe90169f8090fa26dd2135df7
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-05-19 16:57:59 +0000

    simplify implementation of SeriesCircuitNode, rendering order is not important because wire shapes account for occlusion

--- simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/view/multicaps/SeriesCircuitNode.java
+++ simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/view/multicaps/SeriesCircuitNode.java
@@ -29,11 +29,29 @@ public class SeriesCircuitNode extends AbstractCircuitNode {
                               double maxPlateCharge, double maxExcessDielectricPlateCharge, double maxEffectiveEField, double maxDielectricEField ) {
         super( circuit, mvt );
 
-        ArrayList<Wire> wires = circuit.getWires();
-        ArrayList<Capacitor> capacitors = circuit.getCapacitors();
-        assert ( wires.size() == capacitors.size() + 1 );
+        // capacitors
+        for ( Capacitor capacitor : circuit.getCapacitors() ) {
 
-        // add wires counterclockwise from the bottom terminal of the battery, so that rendering order is correct
+            // capacitor node, at its model location
+            CapacitorNode capacitorNode = new CapacitorNode( capacitor, mvt,
+                                                             plateChargeVisible, eFieldVisible, dielectricChargeView,
+                                                             maxPlateCharge, maxExcessDielectricPlateCharge, maxEffectiveEField, maxDielectricEField );
+            capacitorNode.getDielectricNode().setVisible( false );
+            addChild( capacitorNode );
+            Point2D pView = mvt.modelToView( capacitor.getLocation() );
+            capacitorNode.setOffset( pView );
+
+            // capacitance control, to the left of the capacitor
+            CapacitanceControlNode capacitanceControlNode = new CapacitanceControlNode( capacitor,
+                                                                                        MultipleCapacitorsModel.CAPACITANCE_RANGE, MultipleCapacitorsModel.CAPACITANCE_DISPLAY_EXPONENT );
+            addChild( capacitanceControlNode );
+            double x = capacitorNode.getFullBoundsReference().getMinX() - capacitanceControlNode.getFullBoundsReference().getWidth() - PNodeLayoutUtils.getOriginXOffset( capacitanceControlNode ) - 5;
+            double y = capacitorNode.getYOffset() - ( capacitanceControlNode.getFullBoundsReference().getHeight() / 2 );
+            capacitanceControlNode.setOffset( x, y );
+        }
+
+        // wires
+        ArrayList<Wire> wires = circuit.getWires();
         PNode topWireNode = null;
         PNode bottomWireNode = null;
         for ( int i = wires.size() - 1; i >= 0; i-- ) {
@@ -47,35 +65,10 @@ public class SeriesCircuitNode extends AbstractCircuitNode {
             else if ( i == 0 ) {
                 topWireNode = wireNode;
             }
-
-            // move battery in front of first wire
-            if ( i == wires.size() - 1 ) {
-                getBatteryNode().moveToFront();
-            }
-
-            if ( i > 0 ) {
-                Capacitor capacitor = capacitors.get( i - 1 );
-                // capacitor node, at its model location
-                CapacitorNode capacitorNode = new CapacitorNode( capacitor, mvt,
-                                                                 plateChargeVisible, eFieldVisible, dielectricChargeView,
-                                                                 maxPlateCharge, maxExcessDielectricPlateCharge, maxEffectiveEField, maxDielectricEField );
-                capacitorNode.getDielectricNode().setVisible( false );
-                addChild( capacitorNode );
-                Point2D pView = mvt.modelToView( capacitor.getLocation() );
-                capacitorNode.setOffset( pView );
-
-                // capacitance control, to the left of the capacitor
-                CapacitanceControlNode capacitanceControlNode = new CapacitanceControlNode( capacitor,
-                                                                                            MultipleCapacitorsModel.CAPACITANCE_RANGE, MultipleCapacitorsModel.CAPACITANCE_DISPLAY_EXPONENT );
-                addChild( capacitanceControlNode );
-                double x = capacitorNode.getFullBoundsReference().getMinX() - capacitanceControlNode.getFullBoundsReference().getWidth() - PNodeLayoutUtils.getOriginXOffset( capacitanceControlNode ) - 5;
-                double y = capacitorNode.getYOffset() - ( capacitanceControlNode.getFullBoundsReference().getHeight() / 2 );
-                capacitanceControlNode.setOffset( x, y );
-            }
         }
 
         //TODO this code is similar in all circuit nodes
-        // Center the current indicators on the wires that are connected to the battery.
+        // current indicators
         {
             double x, y;
 
