88f3dd8177992853cb32ee47365784a5cf82d6ad
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-05-19 17:07:07 +0000

    move creation of capacitor nodes to circuit node base class

--- trunk/simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/view/multicaps/AbstractCircuitNode.java
+++ trunk/simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/view/multicaps/AbstractCircuitNode.java
@@ -1,15 +1,20 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.capacitorlab.view.multicaps;
 
-import java.awt.geom.Point2D;
-
 import edu.colorado.phet.capacitorlab.CLConstants;
+import edu.colorado.phet.capacitorlab.control.CapacitanceControlNode;
 import edu.colorado.phet.capacitorlab.model.Battery;
 import edu.colorado.phet.capacitorlab.model.CLModelViewTransform3D;
+import edu.colorado.phet.capacitorlab.model.Capacitor;
+import edu.colorado.phet.capacitorlab.model.DielectricChargeView;
 import edu.colorado.phet.capacitorlab.model.circuit.ICircuit;
+import edu.colorado.phet.capacitorlab.module.multiplecapacitors.MultipleCapacitorsModel;
 import edu.colorado.phet.capacitorlab.view.BatteryNode;
+import edu.colorado.phet.capacitorlab.view.CapacitorNode;
 import edu.colorado.phet.capacitorlab.view.CurrentIndicatorNode;
+import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.piccolophet.PhetPNode;
+import edu.colorado.phet.common.piccolophet.util.PNodeLayoutUtils;
 
 /**
  * Base class for all circuit nodes.
@@ -21,14 +26,35 @@ public class AbstractCircuitNode extends PhetPNode {
     private final BatteryNode batteryNode;
     private final CurrentIndicatorNode topCurrentIndicatorNode, bottomCurrentIndicatorNode;
 
-    public AbstractCircuitNode( ICircuit circuit, CLModelViewTransform3D mvt ) {
+    public AbstractCircuitNode( ICircuit circuit, CLModelViewTransform3D mvt,
+                                Property<Boolean> plateChargeVisible, final Property<Boolean> eFieldVisible, Property<DielectricChargeView> dielectricChargeView,
+                                double maxPlateCharge, double maxExcessDielectricPlateCharge, double maxEffectiveEField, double maxDielectricEField ) {
 
         // battery, at model location
         Battery battery = circuit.getBattery();
         batteryNode = new BatteryNode( battery, CLConstants.BATTERY_VOLTAGE_RANGE );
         addChild( batteryNode );
-        Point2D pView = mvt.modelToView( battery.getLocationReference() );
-        batteryNode.setOffset( pView );
+        batteryNode.setOffset( mvt.modelToView( battery.getLocationReference() ) );
+
+        // capacitors
+        for ( Capacitor capacitor : circuit.getCapacitors() ) {
+
+            // capacitor node, at its model location
+            CapacitorNode capacitorNode = new CapacitorNode( capacitor, mvt,
+                                                             plateChargeVisible, eFieldVisible, dielectricChargeView,
+                                                             maxPlateCharge, maxExcessDielectricPlateCharge, maxEffectiveEField, maxDielectricEField );
+            capacitorNode.getDielectricNode().setVisible( false );
+            addChild( capacitorNode );
+            capacitorNode.setOffset( mvt.modelToView( capacitor.getLocation() ) );
+
+            // capacitance control, to the left of the capacitor
+            CapacitanceControlNode capacitanceControlNode = new CapacitanceControlNode( capacitor,
+                                                                                        MultipleCapacitorsModel.CAPACITANCE_RANGE, MultipleCapacitorsModel.CAPACITANCE_DISPLAY_EXPONENT );
+            addChild( capacitanceControlNode );
+            double x = capacitorNode.getFullBoundsReference().getMinX() - capacitanceControlNode.getFullBoundsReference().getWidth() - PNodeLayoutUtils.getOriginXOffset( capacitanceControlNode ) - 5;
+            double y = capacitorNode.getYOffset() - ( capacitanceControlNode.getFullBoundsReference().getHeight() / 2 );
+            capacitanceControlNode.setOffset( x, y );
+        }
 
         // current indicators
         topCurrentIndicatorNode = new CurrentIndicatorNode( circuit, 0 );
--- trunk/simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/view/multicaps/Combination1CircuitNode.java
+++ trunk/simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/view/multicaps/Combination1CircuitNode.java
@@ -1,20 +1,14 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.capacitorlab.view.multicaps;
 
-import java.awt.geom.Point2D;
 import java.util.ArrayList;
 
-import edu.colorado.phet.capacitorlab.control.CapacitanceControlNode;
 import edu.colorado.phet.capacitorlab.model.CLModelViewTransform3D;
-import edu.colorado.phet.capacitorlab.model.Capacitor;
 import edu.colorado.phet.capacitorlab.model.DielectricChargeView;
 import edu.colorado.phet.capacitorlab.model.circuit.Combination1Circuit;
 import edu.colorado.phet.capacitorlab.model.wire.Wire;
-import edu.colorado.phet.capacitorlab.module.multiplecapacitors.MultipleCapacitorsModel;
-import edu.colorado.phet.capacitorlab.view.CapacitorNode;
 import edu.colorado.phet.capacitorlab.view.WireNode;
 import edu.colorado.phet.common.phetcommon.model.property.Property;
-import edu.colorado.phet.common.piccolophet.util.PNodeLayoutUtils;
 import edu.umd.cs.piccolo.PNode;
 
 /**
@@ -27,28 +21,7 @@ public class Combination1CircuitNode extends AbstractCircuitNode {
     public Combination1CircuitNode( Combination1Circuit circuit, CLModelViewTransform3D mvt,
                                     Property<Boolean> plateChargeVisible, final Property<Boolean> eFieldVisible, Property<DielectricChargeView> dielectricChargeView,
                                     double maxPlateCharge, double maxExcessDielectricPlateCharge, double maxEffectiveEField, double maxDielectricEField ) {
-        super( circuit, mvt );
-
-        // capacitors
-        for ( Capacitor capacitor : circuit.getCapacitors() ) {
-
-            // capacitor node, at its model location
-            CapacitorNode capacitorNode = new CapacitorNode( capacitor, mvt,
-                                                             plateChargeVisible, eFieldVisible, dielectricChargeView,
-                                                             maxPlateCharge, maxExcessDielectricPlateCharge, maxEffectiveEField, maxDielectricEField );
-            capacitorNode.getDielectricNode().setVisible( false );
-            addChild( capacitorNode );
-            Point2D pView = mvt.modelToView( capacitor.getLocation() );
-            capacitorNode.setOffset( pView );
-
-            // capacitance control, to the left of the capacitor
-            CapacitanceControlNode capacitanceControlNode = new CapacitanceControlNode( capacitor,
-                                                                                        MultipleCapacitorsModel.CAPACITANCE_RANGE, MultipleCapacitorsModel.CAPACITANCE_DISPLAY_EXPONENT );
-            addChild( capacitanceControlNode );
-            double x = capacitorNode.getFullBoundsReference().getMinX() - capacitanceControlNode.getFullBoundsReference().getWidth() - PNodeLayoutUtils.getOriginXOffset( capacitanceControlNode ) - 5;
-            double y = capacitorNode.getYOffset() - ( capacitanceControlNode.getFullBoundsReference().getHeight() / 2 );
-            capacitanceControlNode.setOffset( x, y );
-        }
+        super( circuit, mvt, plateChargeVisible, eFieldVisible, dielectricChargeView, maxPlateCharge, maxExcessDielectricPlateCharge, maxEffectiveEField, maxDielectricEField );
 
         // wires
         ArrayList<Wire> wires = circuit.getWires();
@@ -67,7 +40,7 @@ public class Combination1CircuitNode extends AbstractCircuitNode {
         }
 
         //TODO this code is similar in all circuit nodes
-        // current indicators
+        // current indicators layout
         {
             double x, y;
 
