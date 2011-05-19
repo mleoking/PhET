19373c5efe80f37d28293582fe68422aab54cd3d
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-05-19 01:20:03 +0000

    5/18/11 display wires in "single" circuit 5/18/11 add Wire constructor that has a Function0 arg for creating wire segments

--- simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/model/Wire.java
+++ simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/model/Wire.java
@@ -3,17 +3,12 @@
 package edu.colorado.phet.capacitorlab.model;
 
 import java.awt.*;
-import java.awt.geom.Point2D;
 import java.util.ArrayList;
 
-import edu.colorado.phet.capacitorlab.CLConstants;
-import edu.colorado.phet.capacitorlab.model.WireSegment.BatteryBottomWireSegment;
-import edu.colorado.phet.capacitorlab.model.WireSegment.BatteryTopWireSegment;
-import edu.colorado.phet.capacitorlab.model.WireSegment.CapacitorBottomWireSegment;
-import edu.colorado.phet.capacitorlab.model.WireSegment.CapacitorTopWireSegment;
 import edu.colorado.phet.capacitorlab.shapes.WireShapeFactory;
 import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
+import edu.colorado.phet.common.phetcommon.util.function.Function0;
 import edu.colorado.phet.common.phetcommon.view.util.ShapeUtils;
 
 /**
@@ -35,6 +30,10 @@ public class Wire {
     // observable properties
     private final Property<Shape> shapeProperty; // Shape in view coordinates!
     private final Property<Double> voltageProperty;
+
+    public Wire( CLModelViewTransform3D mvt, double thickness, Function0<ArrayList<WireSegment>> createWireSegments ) {
+        this( mvt, thickness, createWireSegments.apply() );
+    }
 
     public Wire( CLModelViewTransform3D mvt, double thickness, ArrayList<WireSegment> segments ) {
         assert ( segments != null && segments.size() > 0 );
@@ -111,77 +110,5 @@ public class Wire {
 
     protected Shape createShape() {
         return shapeFactory.createWireShape();
-    }
-
-    /**
-     * Wire that connects the tops of a battery and capacitor.
-     */
-    public static class TopWire extends Wire {
-
-        public TopWire( final Battery battery, final ICapacitor capacitor, double thickness, CLModelViewTransform3D mvt ) {
-            super( mvt, thickness, createSegments( battery, capacitor, thickness ) );
-        }
-
-        private static ArrayList<WireSegment> createSegments( final Battery battery, final ICapacitor capacitor, double thickness ) {
-            final Point2D.Double leftCorner = new Point2D.Double( battery.getX(), battery.getY() - CLConstants.WIRE_EXTENT );
-            final Point2D.Double rightCorner = new Point2D.Double( capacitor.getX(), leftCorner.getY() );
-            final double t = ( thickness / 2 ); // for proper connection at corners with CAP_BUTT wire stroke
-            ArrayList<WireSegment> segments = new ArrayList<WireSegment>() {{
-                add( new BatteryTopWireSegment( battery, leftCorner ) );
-                add( new WireSegment( leftCorner.getX() - t, leftCorner.getY() + t, rightCorner.getX() + t, rightCorner.getY() + t ) );
-                add( new CapacitorTopWireSegment( rightCorner, capacitor ) );
-            }};
-            return segments;
-        }
-    }
-
-    /**
-     * Wire that connects the bottoms of a battery and capacitor.
-     */
-    public static class BottomWire extends Wire {
-
-        private final Battery battery;
-        private final ICapacitor capacitor;
-
-        public BottomWire( final Battery battery, final ICapacitor capacitor, double thickness, CLModelViewTransform3D mvt ) {
-            super( mvt, thickness, createSegments( battery, capacitor, thickness ) );
-
-            this.battery = battery;
-            this.capacitor = capacitor;
-
-            // adjust when dimensions of capacitor change
-            SimpleObserver o = new SimpleObserver() {
-                public void update() {
-                    setShape( createShape() );
-                }
-            };
-            capacitor.addPlateSizeObserver( o );
-            capacitor.addPlateSeparationObserver( o );
-        }
-
-        private static ArrayList<WireSegment> createSegments( final Battery battery, final ICapacitor capacitor, double thickness ) {
-            final Point2D.Double leftCorner = new Point2D.Double( battery.getX(), battery.getY() + CLConstants.WIRE_EXTENT );
-            final Point2D.Double rightCorner = new Point2D.Double( capacitor.getX(), leftCorner.getY() );
-            final double t = ( thickness / 2 ); // for proper connection at corners with CAP_BUTT wire stroke
-            ArrayList<WireSegment> segments = new ArrayList<WireSegment>() {{
-                add( new BatteryBottomWireSegment( battery, leftCorner ) );
-                add( new WireSegment( leftCorner.getX() - t, leftCorner.getY() + t, rightCorner.getX() + t, rightCorner.getY() + t ) );
-                add( new CapacitorBottomWireSegment( rightCorner, capacitor ) );
-            }};
-            return segments;
-        }
-
-        /*
-         * Subtract any part of the wire that is occluded by the battery or bottom plate.
-         */
-        @Override
-        protected Shape createShape() {
-            Shape wireShape = super.createShape();
-            // HACK: null check required because createShape is called in the superclass constructor.
-            if ( battery != null && capacitor != null ) {
-                wireShape = ShapeUtils.subtract( wireShape, battery.getShapeFactory().createBodyShape(), capacitor.getShapeFactory().createBottomPlateShape() );
-            }
-            return wireShape;
-        }
     }
 }
--- simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/view/multicaps/SingleCircuitNode.java
+++ simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/view/multicaps/SingleCircuitNode.java
@@ -1,8 +1,6 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.capacitorlab.view.multicaps;
 
-import java.awt.geom.Point2D;
-
 import edu.colorado.phet.capacitorlab.control.CapacitanceControlNode;
 import edu.colorado.phet.capacitorlab.model.CLModelViewTransform3D;
 import edu.colorado.phet.capacitorlab.model.Capacitor;
@@ -10,6 +8,7 @@ import edu.colorado.phet.capacitorlab.model.DielectricChargeView;
 import edu.colorado.phet.capacitorlab.model.circuit.SingleCircuit;
 import edu.colorado.phet.capacitorlab.module.multiplecapacitors.MultipleCapacitorsModel;
 import edu.colorado.phet.capacitorlab.view.CapacitorNode;
+import edu.colorado.phet.capacitorlab.view.WireNode;
 import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.piccolophet.util.PNodeLayoutUtils;
 
@@ -27,21 +26,39 @@ public class SingleCircuitNode extends AbstractCircuitNode {
                               double maxPlateCharge, double maxExcessDielectricPlateCharge, double maxEffectiveEField, double maxDielectricEField ) {
         super( circuit, mvt );
 
-        // capacitor node, at its model location
+        // capacitor
         Capacitor capacitor = circuit.getCapacitor();
         CapacitorNode capacitorNode = new CapacitorNode( capacitor, mvt, plateChargeVisible, eFieldVisible, dielectricChargeView,
                                                          maxPlateCharge, maxExcessDielectricPlateCharge, maxEffectiveEField, maxDielectricEField );
         capacitorNode.getDielectricNode().setVisible( false );
-        addChild( capacitorNode );
-        Point2D pView = mvt.modelToView( capacitor.getLocation() );
-        capacitorNode.setOffset( pView );
 
-        // capacitance control, to the left of the capacitor
+        // capacitance control
         CapacitanceControlNode capacitanceControlNode = new CapacitanceControlNode( capacitor,
                                                                                     MultipleCapacitorsModel.CAPACITANCE_RANGE, MultipleCapacitorsModel.CAPACITANCE_DISPLAY_EXPONENT );
+
+        // top and bottom wires
+        WireNode topWireNode = new WireNode( circuit.getTopWire(), mvt );
+        WireNode bottomWireNode = new WireNode( circuit.getBottomWire(), mvt );
+
+        // rendering order
+        addChild( bottomWireNode );
+        addChild( capacitorNode );
         addChild( capacitanceControlNode );
-        double x = capacitorNode.getFullBoundsReference().getMinX() - capacitanceControlNode.getFullBoundsReference().getWidth() - PNodeLayoutUtils.getOriginXOffset( capacitanceControlNode ) - 5;
-        double y = capacitorNode.getYOffset() - ( capacitanceControlNode.getFullBoundsReference().getHeight() / 2 );
-        capacitanceControlNode.setOffset( x, y );
+        addChild( topWireNode );
+
+        // layout
+        {
+            // capacitor node at its model location
+            capacitorNode.setOffset( mvt.modelToView( capacitor.getLocation() ) );
+
+            // capacitance control to left of capacitor
+            double x = capacitorNode.getFullBoundsReference().getMinX() - capacitanceControlNode.getFullBoundsReference().getWidth() - PNodeLayoutUtils.getOriginXOffset( capacitanceControlNode ) - 5;
+            double y = capacitorNode.getYOffset() - ( capacitanceControlNode.getFullBoundsReference().getHeight() / 2 );
+            capacitanceControlNode.setOffset( x, y );
+
+            // wires shapes are in model coordinate frame, so the nodes live at (0,0)
+            topWireNode.setOffset( 0, 0 );
+            bottomWireNode.setOffset( 0, 0 );
+        }
     }
 }
