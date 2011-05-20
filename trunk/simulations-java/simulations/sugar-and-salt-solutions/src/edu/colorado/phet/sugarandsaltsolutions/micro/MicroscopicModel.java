a154ccd62da71035d8c57b055da1d023255b139d
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-05-20 14:57:57 +0000

    Added a reset button Added random motion so water molecules continue moving

--- trunk/simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/micro/MicroscopicModel.java
+++ trunk/simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/micro/MicroscopicModel.java
@@ -13,6 +13,7 @@ import org.jbox2d.dynamics.BodyDef;
 import org.jbox2d.dynamics.World;
 
 import edu.colorado.phet.common.phetcommon.util.function.VoidFunction0;
+import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
 import edu.colorado.phet.sugarandsaltsolutions.common.model.SugarAndSaltSolutionModel;
 
 /**
@@ -23,13 +24,18 @@ import edu.colorado.phet.sugarandsaltsolutions.common.model.SugarAndSaltSolution
 public class MicroscopicModel extends SugarAndSaltSolutionModel {
 
     //List of all model objects objects
-    private ArrayList<WaterMolecule> bodyList = new ArrayList<WaterMolecule>();
+    private ArrayList<WaterMolecule> waterList = new ArrayList<WaterMolecule>();
 
     //Listeners who are called back when the physics updates
     private ArrayList<VoidFunction0> frameListeners = new ArrayList<VoidFunction0>();
 
     //Box2d world which updates the physics
     private World world;
+
+    //Listeners that are notified when a water molecule enters the model.  Removal listeners are added to the water molecule
+    private ArrayList<VoidFunction1<WaterMolecule>> waterAddedListeners = new ArrayList<VoidFunction1<WaterMolecule>>();
+
+    private Random random = new Random();
 
     public MicroscopicModel() {
         //Set the bounds of the physics engine
@@ -41,10 +47,7 @@ public class MicroscopicModel extends SugarAndSaltSolutionModel {
         world = new World( worldAABB, new Vec2( 0, 1 ), true );
 
         //Add water particles
-        Random random = new Random();
-        for ( int i = 0; i < 200; i++ ) {
-            addBody( random.nextFloat() * 200 - 100, random.nextFloat() * 200 - 100, 0, 0, (float) ( random.nextFloat() * Math.PI * 2 ) );
-        }
+        addWaterParticles( System.currentTimeMillis() );
 
         //Create beaker floor
         createBarrier( 0, 99, 100, 2 );
@@ -52,6 +55,25 @@ public class MicroscopicModel extends SugarAndSaltSolutionModel {
         //Create sides
         createBarrier( 99, 0, 2, 100 );
         createBarrier( -99, 0, 2, 100 );
+
+        //Move to a stable state on startup
+        //Commented out because it takes too long
+//        long startTime = System.currentTimeMillis();
+//        for ( int i = 0; i < 10; i++ ) {
+//            world.step( (float) ( clock.getDt() * 10 ), 1 );
+//        }
+//        System.out.println( "stable start time: " + ( System.currentTimeMillis() - startTime ) );
+    }
+
+    private void addWaterParticles( long seed ) {
+        Random random = new Random( seed );
+        for ( int i = 0; i < 200; i++ ) {
+            WaterMolecule water = createWater( random.nextFloat() * 200 - 100, random.nextFloat() * 200 - 100, 0, 0, (float) ( random.nextFloat() * Math.PI * 2 ) );
+            waterList.add( water );
+            for ( VoidFunction1<WaterMolecule> waterAddedListener : waterAddedListeners ) {
+                waterAddedListener.apply( water );
+            }
+        }
     }
 
     //Creates a rectangular barrier
@@ -67,12 +89,17 @@ public class MicroscopicModel extends SugarAndSaltSolutionModel {
         body.setMassFromShapes();
     }
 
+    public void addWaterAddedListener( VoidFunction1<WaterMolecule> waterAddedListener ) {
+        waterAddedListeners.add( waterAddedListener );
+    }
+
     //Wrapper class which contains Body and shape
     public static class WaterMolecule {
         public Body body;
         public CircleDef oxygen;
         public CircleDef h1;
         public CircleDef h2;
+        private ArrayList<VoidFunction0> removalListeners = new ArrayList<VoidFunction0>();
 
         WaterMolecule( Body body, CircleDef oxygen, CircleDef h1, CircleDef h2 ) {
             this.body = body;
@@ -80,10 +107,22 @@ public class MicroscopicModel extends SugarAndSaltSolutionModel {
             this.h1 = h1;
             this.h2 = h2;
         }
+
+        //Add a listener that will be notified when this water leaves the model
+        public void addRemovalListener( VoidFunction0 removalListener ) {
+            removalListeners.add( removalListener );
+        }
+
+        //Notify listeners that this water molecule has left the model
+        public void notifyRemoved() {
+            for ( VoidFunction0 removalListener : removalListeners ) {
+                removalListener.apply();
+            }
+        }
     }
 
     //Adds a circular body at the specified location with the given velocity
-    private void addBody( float x, float y, float vx, float vy, float angle ) {
+    private WaterMolecule createWater( float x, float y, float vx, float vy, float angle ) {
 
         //First create the body def at the right location
         BodyDef bodyDef = new BodyDef();
@@ -122,12 +161,24 @@ public class MicroscopicModel extends SugarAndSaltSolutionModel {
         body.setLinearVelocity( new Vec2( vx, vy ) );
 
         //Add the body to the list
-        bodyList.add( new WaterMolecule( body, oxygen, h1, h2 ) );
+        return new WaterMolecule( body, oxygen, h1, h2 );
     }
 
     @Override protected void updateModel( double dt ) {
         //Ignore super update for now
 //        super.updateModel( dt );
+
+        for ( WaterMolecule waterMolecule : waterList ) {
+            //Apply a random force so the system doesn't settle down
+            float rand1 = ( random.nextFloat() - 0.5f ) * 2;
+            waterMolecule.body.applyForce( new Vec2( rand1 * 1000, random.nextFloat() ), waterMolecule.body.getPosition() );
+
+            //Setting random velocity looks funny
+//            double randomAngle = random.nextDouble() * Math.PI * 2;
+//            ImmutableVector2D v = ImmutableVector2D.parseAngleAndMagnitude( rand1 * 1, randomAngle );
+//            Vec2 linearVelocity = waterMolecule.body.getLinearVelocity();
+//            waterMolecule.body.setLinearVelocity( new Vec2( linearVelocity.x + (float) v.getX(), linearVelocity.y + (float) v.getY() ) );
+        }
         world.step( (float) dt * 5, 10 );
 
         //Notify listeners that the model changed
@@ -137,12 +188,27 @@ public class MicroscopicModel extends SugarAndSaltSolutionModel {
     }
 
     //Get all bodies in the model
-    public ArrayList<WaterMolecule> getBodyList() {
-        return bodyList;
+    public ArrayList<WaterMolecule> getWaterList() {
+        return waterList;
     }
 
     //Register for a callback when the model steps
     public void addFrameListener( VoidFunction0 listener ) {
         frameListeners.add( listener );
+    }
+
+    //Resets the model, clearing water molecules and starting over
+    @Override public void reset() {
+        clearWater();
+        addWaterParticles( System.currentTimeMillis() );
+    }
+
+    //Removes all water from the model
+    private void clearWater() {
+        for ( WaterMolecule waterMolecule : waterList ) {
+            world.destroyBody( waterMolecule.body );
+            waterMolecule.notifyRemoved();
+        }
+        waterList.clear();
     }
 }
