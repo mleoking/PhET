495055945061fe4f08efe1c253edc33a97f71998
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-05-23 20:26:12 +0000

    Version of the test class that creates a simple button where text offset is fairly apparent, see #2780.

--- trunk/simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/test/TestButtonTextCentering.java
+++ trunk/simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/test/TestButtonTextCentering.java
@@ -4,13 +4,18 @@ package edu.colorado.phet.common.piccolophet.test;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.geom.Dimension2D;
 
 import javax.swing.*;
 
+import edu.colorado.phet.buildanatom.BuildAnAtomDefaults;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 
 import edu.colorado.phet.common.piccolophet.PhetPCanvas;
 import edu.colorado.phet.common.piccolophet.nodes.ButtonNode;
+import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.util.PDebug;
+import edu.umd.cs.piccolo.util.PDimension;
 
 /**
  * Class for testing the centering of text in the ButtonNode class, see #2780.
@@ -19,31 +24,28 @@ import edu.colorado.phet.common.piccolophet.nodes.ButtonNode;
  */
 public class TestButtonTextCentering {
 
-    public static final String TEST_TEXT_STRING = "This is the Test Caption";
+    private static final Dimension2D STAGE_SIZE = new PDimension( 1008, 679 );
 
     public static void main( String[] args ) {
+
+        PDebug.debugBounds = false;
         ActionListener listener = new ActionListener() {
             public void actionPerformed( ActionEvent event ) {
                 System.out.println( "actionPerformed event= " + event );
             }
         };
 
-        ButtonNode button1 = new ButtonNode( TEST_TEXT_STRING ) {{
-            setCornerRadius( 20 );
-            setImageTextGap( 20 );
-            setMargin( 20, 10, 20, 10 );
-            setFont( new PhetFont( 16 ) );
-            setBackground( Color.GREEN );
-        }};
-        button1.setOffset( 5, 5 );
+        ButtonNode button1 = new ButtonNode( "xxx", new PhetFont( Font.BOLD, 18 ), Color.ORANGE );
+        button1.setOffset( 500, 500 );
         button1.addActionListener( listener );
 
         PhetPCanvas canvas = new PhetPCanvas();
-        canvas.addScreenChild( button1 );
+        canvas.setWorldTransformStrategy( new PhetPCanvas.CenteredStage( canvas, STAGE_SIZE ) );
+        canvas.addWorldChild( button1 );
 
         JFrame frame = new JFrame( ButtonNode.class.getName() );
         frame.setContentPane( canvas );
-        frame.setSize( 525, 600 );
+        frame.setSize( (int)STAGE_SIZE.getWidth(), (int)STAGE_SIZE.getHeight() );
         frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
         frame.setVisible( true );
     }
