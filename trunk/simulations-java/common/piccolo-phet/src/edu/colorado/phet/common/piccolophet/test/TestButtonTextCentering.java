f79bcf207cf03c6170a93105b8c3df76e2b61589
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-05-23 23:02:57 +0000

    Added some different tests - one that looks at font sized without piccolo and one that allows zooming in on the piccolo buttons.

--- trunk/simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/test/TestButtonTextCentering.java
+++ trunk/simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/test/TestButtonTextCentering.java
@@ -5,12 +5,17 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.geom.Dimension2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.Rectangle2D.*;
 
 import javax.swing.*;
 
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.PhetPCanvas;
 import edu.colorado.phet.common.piccolophet.nodes.ButtonNode;
+import edu.colorado.phet.common.piccolophet.nodes.HTMLNode;
+import edu.umd.cs.piccolo.PCanvas;
+import edu.umd.cs.piccolo.nodes.PText;
 import edu.umd.cs.piccolo.util.PDebug;
 import edu.umd.cs.piccolo.util.PDimension;
 
@@ -23,7 +28,7 @@ public class TestButtonTextCentering {
 
     private static final Dimension2D STAGE_SIZE = new PDimension( 1008, 679 );
 
-    public static void main( String[] args ) {
+    public static void test1() {
 
         PDebug.debugBounds = false;
         ActionListener listener = new ActionListener() {
@@ -45,5 +50,58 @@ public class TestButtonTextCentering {
         frame.setSize( (int) STAGE_SIZE.getWidth(), (int) STAGE_SIZE.getHeight() );
         frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
         frame.setVisible( true );
+    }
+
+    public static void test2() {
+        JFrame frame = new JFrame( ButtonNode.class.getName() );
+        frame.setContentPane( new JPanel(){
+            @Override protected void paintComponent( Graphics g ) {
+                super.paintComponent( g );
+                Graphics2D g2 = (Graphics2D)g;
+                g2.setFont( new PhetFont(Font.BOLD, 18 ) );
+                g2.setRenderingHint( RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON );
+                String text = "Zorg Foury";
+                Rectangle2D stringBounds = g2.getFontMetrics().getStringBounds( text, g2 );
+                g2.draw( new Rectangle2D.Double(100, 100 - stringBounds.getHeight() + g2.getFontMetrics().getDescent(), stringBounds.getWidth(), stringBounds.getHeight()));
+                g2.drawString( text, 100, 100 );
+            }
+        });
+        frame.setSize( 250, 200 );
+        frame.setVisible( true );
+    }
+
+    public static void test3() {
+        PDebug.debugBounds = true;
+
+        PCanvas canvas = new PCanvas();
+        String text = "This is a <br>test.";
+        final Font font = new PhetFont( 18, true );
+        HTMLNode htmlNode = new HTMLNode( text ){{
+            setFont( font );
+            setOffset(20, 20);
+        }};
+        canvas.getLayer().addChild( htmlNode );
+        PText textNode = new PText( text ){{
+            setFont( font );
+            setOffset(20, 100);
+        }};
+        canvas.getLayer().addChild( textNode );
+
+        ButtonNode button1 = new ButtonNode( "xxx", new PhetFont( Font.BOLD, 18 ), Color.ORANGE );
+        button1.setOffset( 100, 200 );
+        canvas.getLayer().addChild( button1 );
+
+
+        JFrame frame = new JFrame( ButtonNode.class.getName() );
+        frame.setContentPane( canvas );
+        frame.setSize( (int) STAGE_SIZE.getWidth(), (int) STAGE_SIZE.getHeight() );
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        frame.setVisible( true );
+
+
+    }
+
+    public static void main( String[] args ) {
+        test3();
     }
 }
