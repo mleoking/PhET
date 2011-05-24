23d03a3b62921a30cb2e65462a96c1b21884f953
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-05-24 03:05:46 +0000

    Changed background to blue like geometric optics which reportedly worked well on Trish's projector

--- simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/common/SugarAndSaltSolutionsColorScheme.java
+++ simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/common/SugarAndSaltSolutionsColorScheme.java
@@ -14,7 +14,9 @@ public class SugarAndSaltSolutionsColorScheme {
     //Color recommendations from KL
     private static final Color bakersChocolate = new Color( 92, 51, 23 );
     private static final Color semiSweetChocolate = new Color( 107, 66, 38 );
+    private static final Color buildAnAtom = new Color( 255, 255, 153 );//Nothing white shows up on this
+    private static final Color geometricOptics = new Color( 0, 51, 153 );//Nothing white shows up on this
 
-    public final Property<Color> backgroundColor = new Property<Color>( semiSweetChocolate );
+    public final Property<Color> backgroundColor = new Property<Color>( geometricOptics );
     public final Property<Color> saltColor = new Property<Color>( Color.white );
 }
