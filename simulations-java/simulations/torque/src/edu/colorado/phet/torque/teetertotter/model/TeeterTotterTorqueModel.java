6b541ef1676fe226b75fc1b2bfd64d9dad3979c1
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-05-25 20:57:53 +0000

    Changed to use more compact constructor for the background node.

--- simulations-java/simulations/torque/src/edu/colorado/phet/torque/teetertotter/model/TeeterTotterTorqueModel.java
+++ simulations-java/simulations/torque/src/edu/colorado/phet/torque/teetertotter/model/TeeterTotterTorqueModel.java
@@ -10,9 +10,15 @@ import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
  */
 public class TeeterTotterTorqueModel {
 
+    private static final double GROUND_LEVEL_Y = 0; // Position of the ground in the Y direction, assumed to be flat and infinitely wide.
+
     private final ConstantDtClock clock = new ConstantDtClock( 30.0 );
 
     public ConstantDtClock getClock() {
         return clock;
+    }
+
+    public double getGroundLevelY() {
+        return GROUND_LEVEL_Y;
     }
 }
