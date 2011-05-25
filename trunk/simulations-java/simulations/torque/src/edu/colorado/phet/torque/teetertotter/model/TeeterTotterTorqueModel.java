68b04c72b2eb157510614b45927bb2143eb91f2a
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-05-25 21:44:19 +0000

    Added fulcrum and plank

--- trunk/simulations-java/simulations/torque/src/edu/colorado/phet/torque/teetertotter/model/TeeterTotterTorqueModel.java
+++ trunk/simulations-java/simulations/torque/src/edu/colorado/phet/torque/teetertotter/model/TeeterTotterTorqueModel.java
@@ -1,7 +1,11 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.torque.teetertotter.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
+import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
 
 /**
  * Primary model class for the tab that depicts torque on a plank, a.k.a. a teeter totter.
@@ -9,16 +13,47 @@ import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
  * @author John Blanco
  */
 public class TeeterTotterTorqueModel {
-
-    private static final double GROUND_LEVEL_Y = 0; // Position of the ground in the Y direction, assumed to be flat and infinitely wide.
-
     private final ConstantDtClock clock = new ConstantDtClock( 30.0 );
+
+    //A list of all the weights in the model
+    private final List<Weight> weights = new ArrayList<Weight>();
+
+    //Listeners that are notified when a weight is added to the model
+    private final ArrayList<VoidFunction1<Weight>> weightAddedListeners = new ArrayList<VoidFunction1<Weight>>();
+
+    //Fulcrum that the plank pivots on
+    private final Fulcrum fulcrum = new Fulcrum();
+
+    //Plank that objects can be placed on that is (optionally) supported by pillars
+    private final Plank plank = new Plank( Fulcrum.HEIGHT );
 
     public ConstantDtClock getClock() {
         return clock;
     }
 
-    public double getGroundLevelY() {
-        return GROUND_LEVEL_Y;
+    //Returns a list of the weights in the model
+    public List<Weight> getWeights() {
+        return new ArrayList<Weight>( weights );
+    }
+
+    //Adds a listener that is notified when a weight is added
+    public void addWeightAddedListener( VoidFunction1<Weight> listener ) {
+        weightAddedListeners.add( listener );
+    }
+
+    //Adds a weight to the model and notifies registered listeners
+    public void addWeight( Weight weight ) {
+        weights.add( weight );
+        for ( VoidFunction1<Weight> weightAddedListener : weightAddedListeners ) {
+            weightAddedListener.apply( weight );
+        }
+    }
+
+    public Fulcrum getFulcrum() {
+        return fulcrum;
+    }
+
+    public Plank getPlank() {
+        return plank;
     }
 }
---
+++ trunk/simulations-java/simulations/torque/src/edu/colorado/phet/torque/teetertotter/view/ModelObjectNode.java
@@ -0,0 +1,28 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.torque.teetertotter.view;
+
+import java.awt.*;
+
+import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
+import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
+import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
+import edu.colorado.phet.torque.teetertotter.model.ModelObject;
+import edu.umd.cs.piccolo.PNode;
+
+/**
+ * Base class for displaying and interacting with any model object in the torque sim.
+ * Uses the shape of the object, which will change as the object moves.
+ *
+ * @author Sam Reid
+ */
+public class ModelObjectNode extends PNode {
+    public ModelObjectNode( final ModelViewTransform mvt, final ModelObject modelObject, Color color ) {
+        addChild( new PhetPPath( color ) {{
+            modelObject.shape.addObserver( new VoidFunction1<Shape>() {
+                public void apply( Shape shape ) {
+                    setPathTo( mvt.modelToView( shape ) );
+                }
+            } );
+        }} );
+    }
+}
