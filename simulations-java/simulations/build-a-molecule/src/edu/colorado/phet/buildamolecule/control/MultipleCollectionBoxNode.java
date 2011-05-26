f806b6402ef2d7091545f298124a5aa5f545cc2a
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-05-26 22:16:15 +0000

    bam: enforcing consistent sizing of collection box nodes in collection areas

--- simulations-java/simulations/build-a-molecule/src/edu/colorado/phet/buildamolecule/control/MultipleCollectionBoxNode.java
+++ simulations-java/simulations/build-a-molecule/src/edu/colorado/phet/buildamolecule/control/MultipleCollectionBoxNode.java
@@ -5,17 +5,48 @@ import java.text.MessageFormat;
 
 import edu.colorado.phet.buildamolecule.BuildAMoleculeStrings;
 import edu.colorado.phet.buildamolecule.model.CollectionBox;
+import edu.colorado.phet.buildamolecule.model.CompleteMolecule;
+import edu.colorado.phet.buildamolecule.model.MoleculeList;
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 import edu.colorado.phet.common.phetcommon.util.function.Function1;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.nodes.HTMLNode;
 import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.util.PBounds;
 
 /**
  * Displays a collection box that can collect multiple molecules with two text labels above. One shows the "goal", and the other shows the current
  * quantity present in the box.
  */
 public class MultipleCollectionBoxNode extends CollectionBoxNode {
+    private static double maxWidth;
+    private static double maxHeight;
+
+    static {
+        maxWidth = 0;
+        maxHeight = 0;
+
+        // compute maximum width and height for all different molecules
+        for ( CompleteMolecule molecule : MoleculeList.COLLECTION_BOX_MOLECULES ) {
+            PBounds boxBounds = new MultipleCollectionBoxNode( new CollectionBox( molecule, 1 ), new Function1<PNode, Rectangle2D>() {
+                public Rectangle2D apply( PNode pNode ) {
+                    return null;
+                }
+            } ).getFullBounds();
+
+            maxWidth = Math.max( maxWidth, boxBounds.getWidth() );
+            maxHeight = Math.max( maxHeight, boxBounds.getHeight() );
+        }
+    }
+
+    public static double getMaxWidth() {
+        return maxWidth;
+    }
+
+    public static double getMaxHeight() {
+        return maxHeight;
+    }
+
     public MultipleCollectionBoxNode( final CollectionBox box, Function1<PNode, Rectangle2D> toModelBounds ) {
         super( box, 2, toModelBounds );
 
--- simulations-java/simulations/build-a-molecule/src/edu/colorado/phet/buildamolecule/control/SingleCollectionBoxNode.java
+++ simulations-java/simulations/build-a-molecule/src/edu/colorado/phet/buildamolecule/control/SingleCollectionBoxNode.java
@@ -6,15 +6,46 @@ import java.text.MessageFormat;
 
 import edu.colorado.phet.buildamolecule.BuildAMoleculeStrings;
 import edu.colorado.phet.buildamolecule.model.CollectionBox;
+import edu.colorado.phet.buildamolecule.model.CompleteMolecule;
+import edu.colorado.phet.buildamolecule.model.MoleculeList;
 import edu.colorado.phet.common.phetcommon.util.function.Function1;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.nodes.HTMLNode;
 import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.util.PBounds;
 
 /**
  * Allows the collection of a single molecule
  */
 public class SingleCollectionBoxNode extends CollectionBoxNode {
+    private static double maxWidth;
+    private static double maxHeight;
+
+    static {
+        maxWidth = 0;
+        maxHeight = 0;
+
+        // compute maximum width and height for all different molecules
+        for ( CompleteMolecule molecule : MoleculeList.COLLECTION_BOX_MOLECULES ) {
+            PBounds boxBounds = new SingleCollectionBoxNode( new CollectionBox( molecule, 1 ), new Function1<PNode, Rectangle2D>() {
+                public Rectangle2D apply( PNode pNode ) {
+                    return null;
+                }
+            } ).getFullBounds();
+
+            maxWidth = Math.max( maxWidth, boxBounds.getWidth() );
+            maxHeight = Math.max( maxHeight, boxBounds.getHeight() );
+        }
+    }
+
+    public static double getMaxWidth() {
+        return maxWidth;
+    }
+
+    public static double getMaxHeight() {
+        return maxHeight;
+    }
+
     public SingleCollectionBoxNode( final CollectionBox box, Function1<PNode, Rectangle2D> toModelBounds ) {
         super( box, 1, toModelBounds );
         assert ( box.getCapacity() == 1 );
