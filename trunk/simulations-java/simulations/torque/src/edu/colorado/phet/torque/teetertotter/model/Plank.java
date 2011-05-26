91456c1bfb8d9f776f4a8c5515cbf0dff2416137
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-05-26 21:10:48 +0000

    Added ability to tilt the plank, also started adding the "snap to" markers.

--- trunk/simulations-java/simulations/torque/src/edu/colorado/phet/torque/teetertotter/model/Plank.java
+++ trunk/simulations-java/simulations/torque/src/edu/colorado/phet/torque/teetertotter/model/Plank.java
@@ -1,7 +1,9 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.torque.teetertotter.model;
 
-import java.awt.geom.Rectangle2D;
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.GeneralPath;
 
 /**
  * This is the pivot point where the teeter-totter is balanced.
@@ -10,14 +12,35 @@ import java.awt.geom.Rectangle2D;
  */
 public class Plank extends ModelObject {
 
-    public static final double LENGTH = 4;//meters
-    public static final double HEIGHT = 0.05; //meters
+    public static final double LENGTH = 4;// meters
+    public static final double THICKNESS = 0.05; // meters
+    public static final int NUM_SNAP_TO_MARKERS = 19;
 
-    public Plank( double y ) {
-        super( new Rectangle2D.Double( -LENGTH / 2, y, LENGTH, HEIGHT ) );
+    public Plank( double centerHeight ) {
+        super( generateShape( centerHeight, 0 ) );
     }
 
     public double getLength() {
         return LENGTH;
+    }
+
+    private static Shape generateShape( final double centerHeight, double tiltAngle ) {
+        GeneralPath path = new GeneralPath();
+        path.moveTo( 0, 0 );
+        path.lineTo( LENGTH / 2, 0 );
+        path.lineTo( LENGTH / 2, THICKNESS );
+        path.lineTo( 0, THICKNESS );
+        path.lineTo( -LENGTH / 2, THICKNESS );
+        path.lineTo( -LENGTH / 2, 0 );
+        path.lineTo( 0, 0 );
+        // Add the "snap to" markers to the plank.
+        path.lineTo( 0, THICKNESS );
+        path.moveTo( LENGTH / 4, 0 );
+        path.lineTo( LENGTH / 4, THICKNESS );
+        // Rotate the appropriate amount.
+        Shape shape = AffineTransform.getRotateInstance( tiltAngle ).createTransformedShape( path );
+        // Translate to the appropriate height.
+        shape = AffineTransform.getTranslateInstance( 0, centerHeight ).createTransformedShape( shape );
+        return shape;
     }
 }
