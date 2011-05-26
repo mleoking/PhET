984fa35df094b27fe4ca86e2f3506578357f1fdf
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-05-26 22:26:15 +0000

    Intermediate checkin of code for adding and removing weights.

---
+++ trunk/simulations-java/simulations/torque/src/edu/colorado/phet/torque/teetertotter/model/weights/Brick.java
@@ -0,0 +1,29 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.torque.teetertotter.model.weights;
+
+import java.awt.*;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+
+/**
+ * @author John Blanco
+ */
+public class Brick extends Weight {
+    private static final double WIDTH = 0.2;
+    private static final double HEIGHT = WIDTH / 3;
+    private static final double MASS = 20; // In kg.  Yeah, it's one heavy brick.
+
+    // Position of this brick.  The position is defined as the center in the
+    // X direction and the bottom in the Y direction.  This makes it easy to
+    // set it on top of things.
+    private final Point2D centerBottomPos = new Point2D.Double();
+
+    public Brick( Point2D initialCenterBottom ) {
+        super( generateShape( initialCenterBottom ), MASS );
+        this.centerBottomPos.setLocation( initialCenterBottom );
+    }
+
+    private static Shape generateShape( Point2D centerBottom ) {
+        return new Rectangle2D.Double( centerBottom.getX() - WIDTH / 2, centerBottom.getY(), WIDTH, HEIGHT );
+    }
+}
