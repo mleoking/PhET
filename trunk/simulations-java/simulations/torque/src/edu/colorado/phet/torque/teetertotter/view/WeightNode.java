dcdd53494b9973a52ad0ccdb7167d561d0aa2503
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-06-01 15:16:23 +0000

    Added setting/clearing of the userControlled property when the user moves the weight.

--- trunk/simulations-java/simulations/torque/src/edu/colorado/phet/torque/teetertotter/view/WeightNode.java
+++ trunk/simulations-java/simulations/torque/src/edu/colorado/phet/torque/teetertotter/view/WeightNode.java
@@ -12,18 +12,31 @@ import edu.umd.cs.piccolo.event.PInputEvent;
 import edu.umd.cs.piccolo.util.PDimension;
 
 /**
+ * A node that represents a weight in the view.
  *
+ * @author John Blanco
  */
 public class WeightNode extends ModelObjectNode {
     public WeightNode( final ModelViewTransform mvt, final Weight weight ) {
+        // TODO: Temp - at this point, all weights are bricks, but this obviously won't work long term.
         super( mvt, weight, Color.RED );
         addInputEventListener( new CursorHandler() );
         addInputEventListener( new PDragEventHandler() {
+            @Override protected void startDrag( PInputEvent event ) {
+                // The user is moving this, so they have control.
+                weight.userControlled.set( true );
+            }
+
             @Override
             public void mouseDragged( PInputEvent event ) {
                 PDimension viewDelta = event.getDeltaRelativeTo( getParent() );
                 ImmutableVector2D modelDelta = mvt.viewToModelDelta( new ImmutableVector2D( viewDelta ) );
                 weight.translate( modelDelta );
+            }
+
+            @Override protected void endDrag( PInputEvent event ) {
+                // The user is no longer moving this, so they have relinquished control.
+                weight.userControlled.set( false );
             }
         } );
     }
