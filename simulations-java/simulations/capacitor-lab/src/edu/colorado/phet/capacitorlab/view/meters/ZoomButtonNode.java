a41e13735c6b1c5ee06f694ee8c2b3973aee9b93
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-06-02 23:33:32 +0000

    PSwing zoom buttons are causing problems, change to ButtonNode

--- simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/view/meters/ZoomButtonNode.java
+++ simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/view/meters/ZoomButtonNode.java
@@ -6,10 +6,9 @@ import java.awt.*;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Rectangle2D;
 
-import javax.swing.*;
-
 import edu.colorado.phet.capacitorlab.view.MinusNode;
 import edu.colorado.phet.capacitorlab.view.PlusNode;
+import edu.colorado.phet.common.piccolophet.nodes.ButtonNode;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.nodes.PPath;
 import edu.umd.cs.piccolox.nodes.PComposite;
@@ -17,36 +16,38 @@ import edu.umd.cs.piccolox.nodes.PComposite;
 /**
  * "Zoom" button, used for changing the scale on meters.
  * </p>
- * This evolved from a single button that dynamically changed its icon,
- * to specific buttons for zooming in and out. The concrete classes were
+ * This evolved several times, from a single JButton that dynamically changed its icon,
+ * to specific ButtonNodes for zooming in and out. The concrete classes were
  * a convenient way of using the original implementation with minimal changes.
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
-public abstract class ZoomButton extends JButton {
+public abstract class ZoomButtonNode extends ButtonNode {
 
-    public static class ZoomInButton extends ZoomButton {
-        public ZoomInButton() {
-            super( true );
+    private static final Color ENABLED_COLOR = Color.BLACK;
+    private static final Color DISABLED_COLOR = Color.GRAY;
+
+    public static class ZoomInButtonNode extends ZoomButtonNode {
+        public ZoomInButtonNode() {
+            super( "zoomIn", true );
         }
     }
 
-    public static class ZoomOutButton extends ZoomButton {
-        public ZoomOutButton() {
-            super( false );
+    public static class ZoomOutButtonNode extends ZoomButtonNode {
+        public ZoomOutButtonNode() {
+            super( "zoomOut", false );
         }
     }
 
-    public ZoomButton( boolean zoomIn ) {
-        setIcon( new ImageIcon( new ZoomImageNode( zoomIn ).toImage() ) );
+    public ZoomButtonNode( String actionCommand, boolean zoomIn ) {
+        super( actionCommand, new ZoomIconNode( zoomIn, ENABLED_COLOR ), new ZoomIconNode( zoomIn, DISABLED_COLOR ) );
+        setBackground( Color.WHITE );
     }
 
-    private static class ZoomImageNode extends PComposite {
+    private static class ZoomIconNode extends PComposite {
 
         // all other dimensions are derived from the glass diameter
         private static final double GLASS_DIAMETER = 13;
-
-        private static final Color COLOR = Color.BLACK;
 
         private static final float GLASS_STROKE_WIDTH = (float) ( GLASS_DIAMETER / 7f );
         private static final Stroke GLASS_STROKE = new BasicStroke( GLASS_STROKE_WIDTH );
@@ -57,31 +58,26 @@ public abstract class ZoomButton extends JButton {
         private static final double PLUS_MINUS_WIDTH = 0.6 * GLASS_DIAMETER;
         private static final double PLUS_MINUS_HEIGHT = 0.1 * GLASS_DIAMETER;
 
-        /**
-         * Constructor
-         *
-         * @param zoomIn true if this is a "zoom in" button, false if it's a "zoom out" button
-         */
-        public ZoomImageNode( boolean zoomIn ) {
+        public ZoomIconNode( boolean zoomIn, Color color ) {
 
             PPath glassNode = new PPath( new Ellipse2D.Double( 0, 0, GLASS_DIAMETER, GLASS_DIAMETER ) );
             glassNode.setPaint( new Color( 0, 0, 0, 0 ) ); // transparent, so that clicking in the center of the glass works
             glassNode.setStroke( GLASS_STROKE );
-            glassNode.setStrokePaint( COLOR );
+            glassNode.setStrokePaint( color );
             addChild( glassNode );
 
             PPath handleNode = new PPath( new Rectangle2D.Double( 0, 0, HANDLE_WIDTH, HANDLE_HEIGHT ) );
             handleNode.setStroke( null );
-            handleNode.setPaint( COLOR );
+            handleNode.setPaint( color );
             addChild( handleNode );
 
             // plus or minus sign
             PNode signNode = null;
             if ( zoomIn ) {
-                signNode = new PlusNode( PLUS_MINUS_WIDTH, PLUS_MINUS_HEIGHT, COLOR );
+                signNode = new PlusNode( PLUS_MINUS_WIDTH, PLUS_MINUS_HEIGHT, color );
             }
             else {
-                signNode = new MinusNode( PLUS_MINUS_WIDTH, PLUS_MINUS_HEIGHT, COLOR );
+                signNode = new MinusNode( PLUS_MINUS_WIDTH, PLUS_MINUS_HEIGHT, color );
             }
             addChild( signNode );
 
