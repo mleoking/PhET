9dfc7678bb541ade118d26a54c1fcda2967b093c
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-06-02 17:19:15 +0000

    6/2/11 reduce dielectric constant control to 2 decimal places precision

--- simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/control/DielectricConstantControl.java
+++ simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/control/DielectricConstantControl.java
@@ -16,9 +16,9 @@ import edu.colorado.phet.common.phetcommon.view.controls.valuecontrol.LinearValu
 public class DielectricConstantControl extends LinearValueControl {
 
     private static final double MIN = CLConstants.DIELECTRIC_CONSTANT_RANGE.getMin();
-    private static final  double MAX = CLConstants.DIELECTRIC_CONSTANT_RANGE.getMax();
+    private static final double MAX = CLConstants.DIELECTRIC_CONSTANT_RANGE.getMax();
     private static final String LABEL = MessageFormat.format( CLStrings.PATTERN_LABEL, CLStrings.DIELECTRIC_CONSTANT );
-    private static final String TEXTFIELD_PATTERN = "0.000";
+    private static final String TEXTFIELD_PATTERN = "0.00";
     private static final String UNITS = "";
 
     public DielectricConstantControl( double value ) {
