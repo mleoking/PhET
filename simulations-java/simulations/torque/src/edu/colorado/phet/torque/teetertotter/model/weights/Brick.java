c2cbd463475768e0cf49b9c3bcce883d638b8d8e
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-06-03 23:17:18 +0000

    Added the ability for the weights to rotate, and a specific implementation to rotate the brick.

--- simulations-java/simulations/torque/src/edu/colorado/phet/torque/teetertotter/model/weights/Brick.java
+++ simulations-java/simulations/torque/src/edu/colorado/phet/torque/teetertotter/model/weights/Brick.java
@@ -2,6 +2,7 @@
 package edu.colorado.phet.torque.teetertotter.model.weights;
 
 import java.awt.*;
+import java.awt.geom.AffineTransform;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 
@@ -16,13 +17,16 @@ public class Brick extends Weight {
     private static final double MASS = 20; // In kg.  Yeah, it's one heavy brick.
 
     public Brick( Point2D initialCenterBottom ) {
-        super( generateShape( initialCenterBottom ), MASS );
+        super( generateShape( initialCenterBottom, 0 ), MASS );
     }
 
     // Generate the shape for this object.  This is static so that it can be
     // used in the constructor.
-    private static Shape generateShape( Point2D centerBottom ) {
-        return new Rectangle2D.Double( centerBottom.getX() - WIDTH / 2, centerBottom.getY(), WIDTH, HEIGHT );
+    private static Shape generateShape( Point2D centerBottom, double rotationAngle ) {
+        Rectangle2D rect = new Rectangle2D.Double( -WIDTH / 2, -HEIGHT / 2, WIDTH, HEIGHT );
+        Shape rotatedShape = AffineTransform.getRotateInstance( rotationAngle ).createTransformedShape( rect );
+        Shape translatedShape = AffineTransform.getTranslateInstance( centerBottom.getX(), centerBottom.getY() + HEIGHT / 2 ).createTransformedShape( rotatedShape );
+        return translatedShape;
     }
 
     @Override public void translate( ImmutableVector2D modelDelta ) {
@@ -36,6 +40,11 @@ public class Brick extends Weight {
     }
 
     private void updateShape() {
-        setShapeProperty( generateShape( positionHandle ) );
+        setShapeProperty( generateShape( positionHandle, rotationAngle ) );
+    }
+
+    @Override public void setRotationAngle( double angle ) {
+        super.setRotationAngle( angle );
+        updateShape();
     }
 }
--- simulations-java/simulations/torque/src/edu/colorado/phet/torque/teetertotter/model/weights/Weight.java
+++ simulations-java/simulations/torque/src/edu/colorado/phet/torque/teetertotter/model/weights/Weight.java
@@ -12,6 +12,7 @@ import edu.colorado.phet.torque.teetertotter.model.ModelObject;
  */
 public abstract class Weight extends ModelObject {
     private final double mass;
+    protected double rotationAngle;
 
     public Weight( Shape shape, double mass ) {
         super( shape );
@@ -20,5 +21,18 @@ public abstract class Weight extends ModelObject {
 
     public double getMass() {
         return mass;
+    }
+
+
+    /**
+     * Set the angle of rotation.  The point of rotation is the position
+     * handle.  For a weight, that means that this method can be used to make
+     * it appear to sit will on plank.
+     *
+     * @param angle rotational angle in radians.
+     */
+    public void setRotationAngle( double angle ) {
+        rotationAngle = angle;
+        // Override to implement the updates to the shape.
     }
 }
