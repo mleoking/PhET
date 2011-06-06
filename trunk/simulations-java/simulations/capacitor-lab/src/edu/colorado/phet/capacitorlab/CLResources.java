2b5f2c62c4da20d45cbf815b399309f0e2672e9f
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-06-06 18:21:49 +0000

    delete unused methods

--- trunk/simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/CLResources.java
+++ trunk/simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/CLResources.java
@@ -4,9 +4,7 @@ package edu.colorado.phet.capacitorlab;
 
 import java.awt.image.BufferedImage;
 
-import edu.colorado.phet.common.phetcommon.resources.PhetCommonResources;
 import edu.colorado.phet.common.phetcommon.resources.PhetResources;
-import edu.umd.cs.piccolo.nodes.PImage;
 
 /**
  * Wrapper around the PhET resource loader.
@@ -20,37 +18,14 @@ public class CLResources {
     private static final PhetResources RESOURCES = new PhetResources( CLConstants.PROJECT_NAME );
 
     /* not intended for instantiation */
-    private CLResources() {}
-
-    public static final PhetResources getResourceLoader() {
-        return RESOURCES;
+    private CLResources() {
     }
 
     public static final String getString( String name ) {
-        return RESOURCES.getLocalizedString( name  );
-    }
-
-    public static final char getChar( String name, char defaultValue ) {
-        return RESOURCES.getLocalizedChar( name, defaultValue );
-    }
-
-    public static final int getInt( String name, int defaultValue ) {
-        return RESOURCES.getLocalizedInt( name, defaultValue );
+        return RESOURCES.getLocalizedString( name );
     }
 
     public static final BufferedImage getBufferedImage( String name ) {
         return RESOURCES.getImage( name );
-    }
-
-    public static final PImage getImageNode( String name ) {
-        return new PImage( RESOURCES.getImage( name ) );
-    }
-
-    public static final String getCommonString( String name ) {
-        return PhetCommonResources.getInstance().getLocalizedString( name );
-    }
-
-    public static final BufferedImage getCommonImage( String name ) {
-        return PhetCommonResources.getInstance().getImage( name );
     }
 }
