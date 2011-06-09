1b04072854caeffd3abc37e21941b3ed2dd21319
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-06-09 23:58:07 +0000

    #2938, changed implementation of "Mx10^N units" number display, because HTMLNode looked lousy on Windows

--- simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/view/meters/TimesTenValueNode.java
+++ simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/view/meters/TimesTenValueNode.java
@@ -5,30 +5,55 @@ import java.awt.*;
 import java.text.MessageFormat;
 import java.text.NumberFormat;
 
-import edu.colorado.phet.capacitorlab.CLStrings;
-import edu.colorado.phet.common.piccolophet.nodes.HTMLNode;
+import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
+import edu.umd.cs.piccolo.nodes.PText;
+import edu.umd.cs.piccolox.nodes.PComposite;
 
 /**
- * Displays a value of the form "4x10^3".
+ * Displays a value of the form "4.01x10^3".
+ * Origin is at upper-left corner of bounding rectangle.
+ * </p>
+ * Note that this node was originally an extension of HTMLNode, since HTML is a natural for doing superscripts.
+ * But HTML rendering of superscripts was so poor on Windows (see #2938) that I decided to implement my own superscripts using PText.
+ * The one drawback with this approach is that we can't align baselines of the number and units.
+ * Since all of our units are uppercase letters, that's no a problem in Capacitor Lab.
+ * But it prevents this from being a good general solution that could be migrated to phetcommon.
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
-public class TimesTenValueNode extends HTMLNode {
-
-    private static final String PATTERN_VALUE = "<html>{0}x10<sup>{1}</sup></html>";
+public class TimesTenValueNode extends PComposite {
 
     private final NumberFormat mantissaFormat;
     private int exponent;
     private final String units;
     private double value;
+    private final PText baseNode, exponentNode, unitsNode;
 
-    public TimesTenValueNode( NumberFormat mantissaFormat, int exponent, String units, double value, Font font, Color color ) {
-        setFont( font );
-        setHTMLColor( color );
+    public TimesTenValueNode( NumberFormat mantissaFormat, int exponent, String units, double value, final PhetFont font, final Color color ) {
+
         this.mantissaFormat = mantissaFormat;
         this.exponent = exponent;
         this.units = units;
         this.value = value;
+
+        baseNode = new PText() {{
+            setFont( font );
+            setTextPaint( color );
+        }};
+        addChild( baseNode );
+
+        exponentNode = new PText() {{
+            setFont( new PhetFont( font.getStyle(), (int) ( 0.9 * font.getSize() ) ) );
+            setTextPaint( color );
+        }};
+        addChild( exponentNode );
+
+        unitsNode = new PText( units ) {{
+            setFont( font );
+            setTextPaint( color );
+        }};
+        addChild( unitsNode );
+
         update();
     }
 
@@ -46,12 +71,33 @@ public class TimesTenValueNode extends HTMLNode {
         }
     }
 
+    /*
+     * Zero is displayed as "0 units".
+     * Non-zero is displayed as "4.01x10^13 units" (for example).
+     */
     private void update() {
-        String mantissaString = "0";
+        final int unitsXSpacing = 3;
         if ( value != 0 ) {
+            // update text
             double mantissa = value / Math.pow( 10, exponent );
-            mantissaString = MessageFormat.format( PATTERN_VALUE, mantissaFormat.format( mantissa ), exponent );
+            baseNode.setText( MessageFormat.format( "{0}x10", mantissaFormat.format( mantissa ) ) );
+            exponentNode.setText( String.valueOf( exponent ) );
+            // adjust layout
+            final double exponentOffset = 0.4 * exponentNode.getFullBoundsReference().getHeight();
+            baseNode.setOffset( 0, exponentOffset );
+            exponentNode.setOffset( baseNode.getFullBoundsReference().getMaxX(),
+                                    baseNode.getFullBoundsReference().getMinY() - exponentOffset );
+            unitsNode.setOffset( exponentNode.getFullBoundsReference().getMaxX() + unitsXSpacing,
+                                 baseNode.getFullBoundsReference().getMaxY() - unitsNode.getFullBoundsReference().getHeight() );
         }
-        setHTML( MessageFormat.format( CLStrings.PATTERN_VALUE_UNITS, mantissaString, units ) );
+        else {
+            baseNode.setText( "0" );
+            exponentNode.setText( "" );
+            // adjust layout
+            baseNode.setOffset( 0, 0 );
+            exponentNode.setOffset( 0, 0 );
+            unitsNode.setOffset( baseNode.getFullBoundsReference().getMaxX() + unitsXSpacing,
+                                 baseNode.getFullBoundsReference().getMaxY() - unitsNode.getFullBoundsReference().getHeight() );
+        }
     }
 }
