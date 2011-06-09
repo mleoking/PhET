5909017471e94c55f70a5e710bf787ccdc0a159c
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-06-09 22:22:14 +0000

    #2936 (code review followup) doc

--- simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/model/DielectricChargeView.java
+++ simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/model/DielectricChargeView.java
@@ -4,9 +4,9 @@ package edu.colorado.phet.capacitorlab.model;
 
 /**
  * Visual representations for dielectric charge.
+ * This lives in the model because the model knows about it.
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
- * //REVIEW: consider moving DielectricChargeView to a view package
  */
 public enum DielectricChargeView {
     NONE, TOTAL, EXCESS
