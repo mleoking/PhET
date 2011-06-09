5e75a0a711c74a65f06f4cf39595c6e979220a86
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-06-09 17:15:22 +0000

    added review comments, see #2936

--- trunk/simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/view/meters/BarMeterNode.java
+++ trunk/simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/view/meters/BarMeterNode.java
@@ -186,6 +186,7 @@ public abstract class BarMeterNode extends PhetPNode {
         closeButton = new PImage( CLImages.CLOSE_BUTTON );
         addChild( closeButton );
 
+        //REVIEW: Pushing scope in BarMeterNode constructor (around //zoom buttons and //observers) seems awkward
         // zoom buttons
         {
             zoomInButtonNode = new ZoomInButtonNode();
@@ -218,7 +219,7 @@ public abstract class BarMeterNode extends PhetPNode {
         // layout
         updateLayout();
 
-
+        //REVIEW: Pushing scope in BarMeterNode constructor (around //zoom buttons and //observers) seems awkward
         // observers
         {
             // value
