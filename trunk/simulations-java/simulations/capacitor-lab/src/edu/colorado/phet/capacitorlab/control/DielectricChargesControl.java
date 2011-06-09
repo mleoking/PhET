b3a43a973aad0601ac4a2b1b898531625727817b
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-06-09 15:04:29 +0000

    added review comment, see #2936

--- trunk/simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/control/DielectricChargesControl.java
+++ trunk/simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/control/DielectricChargesControl.java
@@ -29,6 +29,7 @@ public class DielectricChargesControl extends JPanel {
         JRadioButton showAllRadioButton = new PropertyRadioButton<DielectricChargeView>( CLStrings.SHOW_ALL_CHARGES, dielectricChargeView, DielectricChargeView.TOTAL );
         JRadioButton showExcessRadioButton = new PropertyRadioButton<DielectricChargeView>( CLStrings.SHOW_EXCESS_CHARGES, dielectricChargeView, DielectricChargeView.EXCESS );
 
+        //REVIEW: PropertyRadioButton makes ButtonGroup unnecessary, this code should be removed.
         ButtonGroup group = new ButtonGroup();
         group.add( hideAllRadioButton );
         group.add( showAllRadioButton );
