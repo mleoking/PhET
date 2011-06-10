fc5f80089b40f4b58cb2f6a1eebc7c26b1dcd04a
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-06-10 23:41:56 +0000

    change request: adjust initial locations of bar maters in MC tab so that text doesn't intersect wires of any circuits

--- simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/module/multiplecapacitors/MultipleCapacitorsModel.java
+++ simulations-java/simulations/capacitor-lab/src/edu/colorado/phet/capacitorlab/module/multiplecapacitors/MultipleCapacitorsModel.java
@@ -48,15 +48,15 @@ public class MultipleCapacitorsModel extends CLModel {
     private static final double WIRE_EXTENT = 0.01; // how far a wire extends above or below topmost capacitor's origin, in meters
 
     // Capacitance meter
-    public static final Point3D CAPACITANCE_METER_LOCATION = new Point3D.Double( 0.038, 0.0017, 0 );
+    public static final Point3D CAPACITANCE_METER_LOCATION = new Point3D.Double( 0.033, 0.0017, 0 );
     public static final boolean CAPACITANCE_METER_VISIBLE = false;
 
     // Plate Charge meter
-    public static final Point3D PLATE_CHARGE_METER_LOCATION = new Point3D.Double( 0.049, 0.0017, 0 );
+    public static final Point3D PLATE_CHARGE_METER_LOCATION = new Point3D.Double( 0.046, 0.0017, 0 );
     public static final boolean PLATE_CHARGE_METER_VISIBLE = false;
 
     // Stored Energy meter
-    public static final Point3D STORED_ENERGY_METER_LOCATION = new Point3D.Double( 0.06, 0.0017, 0 );
+    public static final Point3D STORED_ENERGY_METER_LOCATION = new Point3D.Double( 0.059, 0.0017, 0 );
     public static final boolean STORED_ENERGY_METER_VISIBLE = false;
 
     // E-Field Detector
