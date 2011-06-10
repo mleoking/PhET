f6f8c361981ea1d204218d9b71f8f0df105e060e
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-06-10 01:03:09 +0000

    Water tab: Added sucrose graphic

--- simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/water/MoleculeNode.java
+++ simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/water/MoleculeNode.java
@@ -1,16 +1,28 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.sugarandsaltsolutions.water;
 
+import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
 import edu.colorado.phet.common.phetcommon.util.function.VoidFunction0;
 import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
+import edu.colorado.phet.sugarandsaltsolutions.SugarAndSaltSolutionsApplication;
 import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.nodes.PImage;
 
 /**
+ * Shows a graphical representation of the molecule.
+ *
  * @author Sam Reid
  */
 public class MoleculeNode extends PNode {
-    public MoleculeNode( ModelViewTransform transform, Molecule sodiumIon, VoidFunction1<VoidFunction0> addFrameListener, S3Element naIon ) {
-
+    public MoleculeNode( final ModelViewTransform transform, Molecule sodiumIon, VoidFunction1<VoidFunction0> addFrameListener, S3Element naIon ) {
+        addChild( new PImage( SugarAndSaltSolutionsApplication.RESOURCES.getImage( "sucrose.png" ) ) );
+        scale( 0.6 );
+        sodiumIon.position.addObserver( new VoidFunction1<ImmutableVector2D>() {
+            public void apply( ImmutableVector2D immutableVector2D ) {
+                setOffset( transform.modelToView( immutableVector2D ).getX() - getFullBounds().getWidth() / 2,
+                           transform.modelToView( immutableVector2D ).getY() - getFullBounds().getHeight() / 2 );
+            }
+        } );
     }
 }
