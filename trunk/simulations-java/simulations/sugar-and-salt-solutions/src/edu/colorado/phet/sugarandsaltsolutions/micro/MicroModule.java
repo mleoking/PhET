b65fbd0ff9d5fd89bcd8b5ea33d88468cd8e6018
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-06-11 18:09:40 +0000

    Micro tab: Added a solute salt/sugar control

--- trunk/simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/micro/MicroModule.java
+++ trunk/simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/micro/MicroModule.java
@@ -1,12 +1,17 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.sugarandsaltsolutions.micro;
 
+import edu.colorado.phet.common.phetcommon.model.property.Property;
+import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 import edu.colorado.phet.solublesalts.SolubleSaltsApplication.SolubleSaltsClock;
 import edu.colorado.phet.solublesalts.SolubleSaltsConfig;
 import edu.colorado.phet.solublesalts.model.SolubleSaltsModel;
+import edu.colorado.phet.solublesalts.model.salt.MercuryBromide;
 import edu.colorado.phet.solublesalts.model.salt.SodiumChloride;
 import edu.colorado.phet.solublesalts.module.SolubleSaltsModule;
 import edu.colorado.phet.sugarandsaltsolutions.common.SugarAndSaltSolutionsColorScheme;
+import edu.colorado.phet.sugarandsaltsolutions.common.model.DispenserType;
+import edu.colorado.phet.sugarandsaltsolutions.common.view.SoluteControlPanelNode;
 
 /**
  * Micro tab that shows the NaCl ions and Sucrose molecules
@@ -23,8 +28,21 @@ public class MicroModule extends SolubleSaltsModule {
                                                    0.5E-23 ) );
 
         // Use NaCl by default
-        ( (SolubleSaltsModel) getModel() ).setCurrentSalt( new SodiumChloride() );
 
-//        getSimulationPanel()
+        final Property<DispenserType> dispenserType = new Property<DispenserType>( DispenserType.SALT );
+        dispenserType.addObserver( new SimpleObserver() {
+            public void update() {
+                if ( dispenserType.get() == DispenserType.SALT ) {
+                    ( (SolubleSaltsModel) getModel() ).setCurrentSalt( new SodiumChloride() );
+                }
+                else {
+                    ( (SolubleSaltsModel) getModel() ).setCurrentSalt( new MercuryBromide() );
+                }
+            }
+        } );
+        getFullScaleCanvas().addChild( new SoluteControlPanelNode( dispenserType ) {{
+            scale( 1.5 );
+            setOffset( 1300 - getFullBounds().getWidth(), 768 / 2 - getFullBounds().getHeight() / 2 );
+        }} );
     }
 }
