032d51aaefd7ceb7f22e93e24485d8a56cdfe97b
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-06-11 21:49:05 +0000

    Micro tab: Improved sugar graphics

--- simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/micro/MicroModule.java
+++ simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/micro/MicroModule.java
@@ -2,10 +2,20 @@
 package edu.colorado.phet.sugarandsaltsolutions.micro;
 
 import java.awt.*;
+import java.awt.geom.Point2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
+import javax.imageio.ImageIO;
+
+import edu.colorado.phet.chemistry.model.Element;
 import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
+import edu.colorado.phet.common.phetcommon.util.function.VoidFunction0;
+import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
+import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
 import edu.colorado.phet.solublesalts.SolubleSaltsApplication.SolubleSaltsClock;
 import edu.colorado.phet.solublesalts.SolubleSaltsConfig;
 import edu.colorado.phet.solublesalts.model.SolubleSaltsModel;
@@ -20,6 +30,8 @@ import edu.colorado.phet.solublesalts.view.IonGraphicManager;
 import edu.colorado.phet.sugarandsaltsolutions.common.SugarAndSaltSolutionsColorScheme;
 import edu.colorado.phet.sugarandsaltsolutions.common.model.DispenserType;
 import edu.colorado.phet.sugarandsaltsolutions.common.view.SoluteControlPanelNode;
+import edu.colorado.phet.sugarandsaltsolutions.water.model.WaterModel;
+import edu.colorado.phet.sugarandsaltsolutions.water.view.SucroseNode;
 
 /**
  * Micro tab that shows the NaCl ions and Sucrose molecules
@@ -28,8 +40,8 @@ import edu.colorado.phet.sugarandsaltsolutions.common.view.SoluteControlPanelNod
  */
 public class MicroModule extends SolubleSaltsModule {
     static {
-        IonGraphicManager.putImage( new SugarMoleculePlus(), Color.yellow );
-        IonGraphicManager.putImage( new SugarMoleculeMinus(), Color.yellow );
+        IonGraphicManager.putImage( SugarMoleculePlus.class, getSucroseImage() );
+        IonGraphicManager.putImage( SugarMoleculeMinus.class, getSucroseImage() );
     }
 
     public MicroModule( SugarAndSaltSolutionsColorScheme configuration ) {
@@ -91,5 +103,23 @@ public class MicroModule extends SolubleSaltsModule {
         public SugarMoleculeMinus() {
             super( ionProperties );
         }
+    }
+
+    private static BufferedImage getSucroseImage() {
+        final SucroseNode sucroseNode = new SucroseNode( ModelViewTransform.createSinglePointScaleMapping( new Point2D.Double(), new Point2D.Double(), 3150 / 6.3E-7 * 400 ), new WaterModel().newSugar( 0, 0 ), new VoidFunction1<VoidFunction0>() {
+            public void apply( VoidFunction0 voidFunction0 ) {
+                voidFunction0.apply();
+            }
+        }, Element.O.getColor(), Element.H.getColor(), Color.gray ) {{
+            double width = getFullBounds().getWidth();
+            scale( SugarMoleculeMinus.RADIUS * 3 / width );
+            rotate( Math.random() * Math.PI * 2 );
+        }};
+        return (BufferedImage) sucroseNode.toImage();
+    }
+
+    //Sample main writes a sucrose image to file for inspection
+    public static void main( String[] args ) throws IOException {
+        ImageIO.write( getSucroseImage(), "PNG", new File( args[0], System.currentTimeMillis() + ".PNG" ) );
     }
 }
