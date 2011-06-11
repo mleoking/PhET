590e37c9f58a42b215fe84a1e4093ef5df7b5a5f
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-06-11 21:58:43 +0000

    added docs

--- trunk/simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/micro/MicroModule.java
+++ trunk/simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/micro/MicroModule.java
@@ -3,6 +3,7 @@ package edu.colorado.phet.sugarandsaltsolutions.micro;
 
 import java.awt.*;
 import java.awt.geom.Point2D;
+import java.awt.geom.Point2D.Double;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -34,11 +35,17 @@ import edu.colorado.phet.sugarandsaltsolutions.water.model.WaterModel;
 import edu.colorado.phet.sugarandsaltsolutions.water.view.SucroseNode;
 
 /**
- * Micro tab that shows the NaCl ions and Sucrose molecules
+ * Micro tab that shows the NaCl ions and Sucrose molecules.
+ * <p/>
+ * In order to efficiently re-use pre existing code from the Soluble Salts (AKA Salts and Solubility) project, we make the following inaccurate encodings:
+ * 1. Sugar is a subclass of Salt
+ * 2. Sugar has two constituents, a "positive" sugar molecule and a "negative" sugar molecule
  *
  * @author Sam Reid
  */
 public class MicroModule extends SolubleSaltsModule {
+
+    //Register sucrose images with the sugar constituents
     static {
         IonGraphicManager.putImage( SugarMoleculePlus.class, getSucroseImage() );
         IonGraphicManager.putImage( SugarMoleculeMinus.class, getSucroseImage() );
@@ -53,8 +60,9 @@ public class MicroModule extends SolubleSaltsModule {
                                                    0.5E-23 ) );
 
         // Use NaCl by default
-
         final Property<DispenserType> dispenserType = new Property<DispenserType>( DispenserType.SALT );
+
+        //When the user selects a different solute, update the dispenser type
         dispenserType.addObserver( new SimpleObserver() {
             public void update() {
                 if ( dispenserType.get() == DispenserType.SALT ) {
@@ -65,12 +73,15 @@ public class MicroModule extends SolubleSaltsModule {
                 }
             }
         } );
+
+        //Show a control that lets the user choose different solutes (salt/sugar)
         getFullScaleCanvas().addChild( new SoluteControlPanelNode( dispenserType ) {{
             scale( 1.5 );
             setOffset( 1300 - getFullBounds().getWidth(), 768 / 2 - getFullBounds().getHeight() / 2 );
         }} );
     }
 
+    //Model sugar as a kind of salt with positive and negative sucrose molecules (that look identical)
     public static class SugarCrystal extends Salt {
         static private Lattice lattice = new OneToOneLattice( SugarMoleculePlus.RADIUS + SugarMoleculeMinus.RADIUS );
         static private ArrayList<Component> components = new ArrayList<Component>();
@@ -105,14 +116,24 @@ public class MicroModule extends SolubleSaltsModule {
         }
     }
 
+    //Create an image for sucrose using the same code as in the water tab to keep representations consistent
     private static BufferedImage getSucroseImage() {
-        final SucroseNode sucroseNode = new SucroseNode( ModelViewTransform.createSinglePointScaleMapping( new Point2D.Double(), new Point2D.Double(), 3150 / 6.3E-7 * 400 ), new WaterModel().newSugar( 0, 0 ), new VoidFunction1<VoidFunction0>() {
+        //Create a transform that will make the constituent particles big enough since they are rasterized.  I obtained the values by running the Water tab and printing out the box2d transform used in SucroseNode
+        final ModelViewTransform transform = ModelViewTransform.createSinglePointScaleMapping( new Point2D.Double(), new Double(), 3150 / 6.3E-7 * 400 );
+
+        //Create the graphic
+        final SucroseNode sucroseNode = new SucroseNode( transform, new WaterModel().newSugar( 0, 0 ), new VoidFunction1<VoidFunction0>() {
             public void apply( VoidFunction0 voidFunction0 ) {
                 voidFunction0.apply();
             }
         }, Element.O.getColor(), Element.H.getColor(), Color.gray ) {{
+
+            //Scale the graphic so it will be a good size for putting into a crystal lattice, with sizes
+            //Just using RADIUS * 2 leaves too much space between particles in the lattice
             double width = getFullBounds().getWidth();
             scale( SugarMoleculeMinus.RADIUS * 3 / width );
+
+            //Put it a random angle
             rotate( Math.random() * Math.PI * 2 );
         }};
         return (BufferedImage) sucroseNode.toImage();
