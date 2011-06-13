ce81c7dda959a70cb44fef8f51b0b2ae9aab6a33
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-06-13 06:49:52 +0000

    bam: removed TODO and unused 3d.png icon

--- trunk/simulations-java/simulations/build-a-molecule/src/edu/colorado/phet/buildamolecule/BuildAMoleculeConstants.java
+++ trunk/simulations-java/simulations/build-a-molecule/src/edu/colorado/phet/buildamolecule/BuildAMoleculeConstants.java
@@ -68,7 +68,6 @@ public class BuildAMoleculeConstants {
     *----------------------------------------------------------------------------*/
 
     public static final String IMAGE_SPLIT_ICON = "split-blue.png";
-    public static final String IMAGE_3D_ICON = "3d.png";
     public static final String IMAGE_GREEN_LEFT = "green-left.png";
     public static final String IMAGE_GREEN_RIGHT = "green-right.png";
     public static final String IMAGE_GREEN_MIDDLE = "green-middle.png";
