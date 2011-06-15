be51889c573ab938ab9abd47c0db8a30c3431d78
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-06-15 22:44:50 +0000

    Added developer controls for charges

--- simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/water/model/Atom.java
+++ simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/water/model/Atom.java
@@ -6,6 +6,7 @@ import org.jbox2d.common.Vec2;
 import org.jbox2d.dynamics.Body;
 
 import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
+import edu.colorado.phet.common.phetcommon.model.property.ObservableProperty;
 import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
 
@@ -18,7 +19,7 @@ public class Atom {
     public final Particle particle;
     public final ImmutableVector2D modelOffset;
 
-    public Atom( double x, double y, final ModelViewTransform transform, final double r, final Body body, final double localOffsetX, final double localOffsetY, final double charge, final boolean origin ) {
+    public Atom( double x, double y, final ModelViewTransform transform, final double r, final Body body, final double localOffsetX, final double localOffsetY, final ObservableProperty<Double> charge, final boolean origin ) {
         modelOffset = new ImmutableVector2D( localOffsetX, localOffsetY );
         position = new Property<ImmutableVector2D>( new ImmutableVector2D( x, y ) );
 
@@ -45,7 +46,7 @@ public class Atom {
             }
 
             public double getCharge() {
-                return charge;
+                return charge.get();
             }
 
             public ImmutableVector2D getModelPosition() {
--- simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/water/model/DefaultParticle.java
+++ simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/water/model/DefaultParticle.java
@@ -4,6 +4,7 @@ package edu.colorado.phet.sugarandsaltsolutions.water.model;
 import org.jbox2d.dynamics.World;
 
 import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
+import edu.colorado.phet.common.phetcommon.model.property.ObservableProperty;
 import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.phetcommon.util.function.VoidFunction0;
 import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
@@ -18,7 +19,7 @@ public class DefaultParticle extends Molecule {
     public final double radius;
     public final Property<ImmutableVector2D> position;
 
-    public DefaultParticle( World world, final ModelViewTransform transform, final double x, final double y, double vx, double vy, final double theta, VoidFunction1<VoidFunction0> addUpdateListener, double charge, double radius ) {
+    public DefaultParticle( World world, final ModelViewTransform transform, final double x, final double y, double vx, double vy, final double theta, VoidFunction1<VoidFunction0> addUpdateListener, ObservableProperty<Double> charge, double radius ) {
         super( world, transform, x, y, vx, vy, theta, addUpdateListener );
         final Atom atom = new Atom( x, y, transform, radius, body, 0, 0, charge, true );
         initAtoms( atom );
