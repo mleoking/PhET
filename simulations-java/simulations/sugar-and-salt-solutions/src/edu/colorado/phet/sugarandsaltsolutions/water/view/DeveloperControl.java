abbf5e73f6e2f3b52db0db07c4c6dbca71dfa96b
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-06-16 03:05:09 +0000

    Water tab: Added a developer control that restricts forces to apply to waters only

--- simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/water/view/DeveloperControl.java
+++ simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/water/view/DeveloperControl.java
@@ -1,14 +1,17 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.sugarandsaltsolutions.water.view;
 
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 
 import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
 import edu.colorado.phet.common.phetcommon.view.VerticalLayoutPanel;
+import edu.colorado.phet.common.phetcommon.view.controls.PropertyRadioButton;
 import edu.colorado.phet.sugarandsaltsolutions.common.view.PropertySlider;
 import edu.colorado.phet.sugarandsaltsolutions.water.model.WaterModel;
 
@@ -17,68 +20,79 @@ import edu.colorado.phet.sugarandsaltsolutions.water.model.WaterModel;
  *
  * @author Sam Reid
  */
-public class DeveloperControl extends VerticalLayoutPanel {
+public class DeveloperControl extends JPanel {
 
     public DeveloperControl( final WaterModel waterModel ) {
-        //Developer controls for physics settings,
-        add( new JPanel() {{
-            add( new JButton( "Add Water" ) {{
-                addActionListener( new ActionListener() {
-                    public void actionPerformed( ActionEvent e ) {
-                        waterModel.addWater( waterModel.getRandomX(), waterModel.getRandomY(), 0 );
-                    }
-                } );
+
+        add( new VerticalLayoutPanel() {{
+            //Developer controls for physics settings,
+            add( new JPanel() {{
+                add( new JButton( "Add Water" ) {{
+                    addActionListener( new ActionListener() {
+                        public void actionPerformed( ActionEvent e ) {
+                            waterModel.addWater( waterModel.getRandomX(), waterModel.getRandomY(), 0 );
+                        }
+                    } );
+                }} );
+                add( new JLabel( "num waters:" ) );
+                add( new DoubleLabel( waterModel.waterList.count ) );
             }} );
-            add( new JLabel( "num waters:" ) );
-            add( new DoubleLabel( waterModel.waterList.count ) );
-        }} );
-        add( new JLabel( "coulomb strength (k)" ) );
-        add( new JPanel() {{
-            add( new PropertySlider( 0, 1000, waterModel.k ) );
-            add( new IntLabel( waterModel.k ) );
-        }} );
-        add( new JLabel( "coulomb power" ) );
-        add( new JPanel() {{
-            add( new PropertySlider( 0, 4, waterModel.pow ) );
-            add( new IntLabel( waterModel.pow ) );
-        }} );
+            add( new JLabel( "coulomb strength (k)" ) );
+            add( new JPanel() {{
+                add( new PropertySlider( 0, 1000, waterModel.k ) );
+                add( new IntLabel( waterModel.k ) );
+            }} );
+            add( new JLabel( "coulomb power" ) );
+            add( new JPanel() {{
+                add( new PropertySlider( 0, 4, waterModel.pow ) );
+                add( new IntLabel( waterModel.pow ) );
+            }} );
 
-        add( new JLabel( "model randomness" ) );
-        add( new JPanel() {{
-            add( new PropertySlider( 0, 100, waterModel.randomness ) );
-            add( new IntLabel( waterModel.randomness ) );
-        }} );
+            add( new JLabel( "model randomness" ) );
+            add( new JPanel() {{
+                add( new PropertySlider( 0, 100, waterModel.randomness ) );
+                add( new IntLabel( waterModel.randomness ) );
+            }} );
 
-        add( new JPanel() {{
-            add( new DoublePropertySlider( "min interaction dist", 0, 8, waterModel.minInteractionDistance ) );
-        }} );
+            add( new JPanel() {{
+                add( new DoublePropertySlider( "min interaction dist", 0, 8, waterModel.minInteractionDistance ) );
+            }} );
 
-        add( new JPanel() {{
-            add( new DoublePropertySlider( "max interaction dist", 0, 8, waterModel.maxInteractionDistance ) );
-        }} );
+            add( new JPanel() {{
+                add( new DoublePropertySlider( "max interaction dist", 0, 8, waterModel.maxInteractionDistance ) );
+            }} );
 
-        add( new JPanel() {{
-            add( new DoublePropertySlider( "prob. of interaction", 0, 1, waterModel.probabilityOfInteraction ) );
-        }} );
+            add( new JPanel() {{
+                add( new DoublePropertySlider( "prob. of interaction", 0, 1, waterModel.probabilityOfInteraction ) );
+            }} );
 
-        add( new JPanel() {{
-            add( new DoublePropertySlider( "time scale", 0, 1, waterModel.timeScale ) );
+            add( new JPanel() {{
+                add( new DoublePropertySlider( "time scale", 0, 1, waterModel.timeScale ) );
+            }} );
+            setBorder( new LineBorder( Color.black ) );
         }} );
+        add( new JSeparator( SwingConstants.VERTICAL ) );
 
-        add( new JPanel() {{
-            add( new DoublePropertySlider( "oxygen charge", -2, 0, waterModel.oxygenCharge ) );
-        }} );
-        add( new JPanel() {{
-            add( new DoublePropertySlider( "hydrogen charge", 0, 1, waterModel.hydrogenCharge ) );
-        }} );
-        add( new JPanel() {{
-            add( new DoublePropertySlider( "ion charge", 0, 3, waterModel.ionCharge ) );
-        }} );
+        add( new VerticalLayoutPanel() {{
+            add( new JPanel() {{
+                add( new DoublePropertySlider( "oxygen charge", -2, 0, waterModel.oxygenCharge ) );
+            }} );
+            add( new JPanel() {{
+                add( new DoublePropertySlider( "hydrogen charge", 0, 1, waterModel.hydrogenCharge ) );
+            }} );
+            add( new JPanel() {{
+                add( new DoublePropertySlider( "ion charge", 0, 3, waterModel.ionCharge ) );
+            }} );
 
-        add( new JLabel( "iterations per time step" ) );
-        add( new JPanel() {{
-            add( new PropertySlider( 0, 500, waterModel.iterations ) );
-            add( new IntLabel( waterModel.iterations ) );
+            add( new JLabel( "iterations per time step" ) );
+            add( new JPanel() {{
+                add( new PropertySlider( 0, 500, waterModel.iterations ) );
+                add( new IntLabel( waterModel.iterations ) );
+            }} );
+
+            add( new PropertyRadioButton<Boolean>( "Coulomb forces between all particles", waterModel.coulombForceOnAllMolecules, true ) );
+            add( new PropertyRadioButton<Boolean>( "Coulomb forces only on water", waterModel.coulombForceOnAllMolecules, false ) );
+            setBorder( new LineBorder( Color.black ) );
         }} );
     }
 
