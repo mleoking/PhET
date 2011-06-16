e15d727849e86c9d132736a14c82bd2276f52dc8
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-06-16 18:09:16 +0000

    Water tab: Added a sugar bucket

---
+++ simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/water/view/DraggableSugarCrystalNode.java
@@ -0,0 +1,61 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.sugarandsaltsolutions.water.view;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+import edu.colorado.phet.common.phetcommon.util.function.VoidFunction0;
+import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
+import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
+import edu.colorado.phet.common.piccolophet.event.CursorHandler;
+import edu.colorado.phet.sugarandsaltsolutions.water.model.Sucrose;
+import edu.colorado.phet.sugarandsaltsolutions.water.model.WaterModel;
+import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
+import edu.umd.cs.piccolo.event.PInputEvent;
+
+/**
+ * Draggable graphic for sugar that appears in the salt bucket and can be dragged and dropped into the play area.
+ * TODO: duplicate code with DraggableSaltCrystalNode
+ *
+ * @author Sam Reid
+ */
+public class DraggableSugarCrystalNode extends PNode {
+    public DraggableSugarCrystalNode( final WaterModel waterModel, ModelViewTransform transform,
+                                      //Region where dropping the crystal is allowed, in the particle box
+                                      final PNode target ) {
+        //Ask the model to create a salt crystal so it will have the correct dimensions and will work with our graphics classes
+        ArrayList<Sucrose> saltCrystal = waterModel.createSugarCrystal();
+
+        //Disable collisions between salt crystal and waters while user is dragging it.  Couldn't get collision filtering to work, so this is our workaround
+        for ( Sucrose sucrose : saltCrystal ) {
+            waterModel.unhook( sucrose );
+        }
+
+        //Add graphics for the sucrose molecules in the crystal
+        for ( Sucrose sucrose : saltCrystal ) {
+            addChild( new SucroseNode( transform, sucrose, new VoidFunction1<VoidFunction0>() {
+                public void apply( VoidFunction0 voidFunction0 ) {
+                    voidFunction0.apply();
+                }
+            }, Color.yellow, Color.yellow, Color.yellow ) );
+        }
+
+        //Overrides the cursor handler in DefaultParticleNode instances
+        addInputEventListener( new CursorHandler() );
+        addInputEventListener( new PBasicInputEventHandler() {
+            @Override public void mouseDragged( PInputEvent event ) {
+                translate( event.getDeltaRelativeTo( getParent() ).getWidth() / getScale(), event.getDeltaRelativeTo( getParent() ).getHeight() / getScale() );
+            }
+
+            //When the user drops the crystal in the particle box, remove the dragged salt node and add salt directly to the model, which will create a new graphic
+            @Override public void mouseReleased( PInputEvent event ) {
+                if ( target.getGlobalFullBounds().contains( DraggableSugarCrystalNode.this.getGlobalFullBounds().getCenter2D() ) ) {
+                    waterModel.addSugar();
+                    getParent().removeChild( DraggableSugarCrystalNode.this );
+                }
+            }
+        } );
+        scale( 0.6 );
+    }
+}
