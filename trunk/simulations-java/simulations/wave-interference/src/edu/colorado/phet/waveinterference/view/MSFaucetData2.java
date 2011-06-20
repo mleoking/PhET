60d54d14a7b84de03d2512afdae0ce6a25bd043d
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-06-20 15:43:58 +0000

    renamed faucet image and related class

--- trunk/simulations-java/simulations/wave-interference/src/edu/colorado/phet/waveinterference/view/MSFaucetData2.java
+++
@@ -1,8 +0,0 @@
-// Copyright 2002-2011, University of Colorado
-package edu.colorado.phet.waveinterference.view;
-
-public class MSFaucetData2 extends FaucetData {
-    public MSFaucetData2() {
-        super( "wave-interference/images/faucet3.png", 0.85, 0.6 );//These values move the image location, not just the location of the water drops within the fixed image
-    }
-}
