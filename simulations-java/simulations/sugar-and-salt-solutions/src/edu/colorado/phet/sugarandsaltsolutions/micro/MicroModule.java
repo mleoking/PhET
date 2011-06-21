9f1a18ac7d32335849b6aa6feb0ad779ed7e7716
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-06-21 14:09:39 +0000

    moved graphics code

--- simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/micro/MicroModule.java
+++ simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/micro/MicroModule.java
@@ -1,24 +1,9 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.sugarandsaltsolutions.micro;
 
-import java.awt.*;
-import java.awt.geom.Point2D;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
-
-import edu.colorado.phet.chemistry.model.Element;
 import edu.colorado.phet.common.phetcommon.application.Module;
-import edu.colorado.phet.common.phetcommon.util.function.VoidFunction0;
-import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
-import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
 import edu.colorado.phet.sugarandsaltsolutions.SugarAndSaltSolutionsColorScheme;
 import edu.colorado.phet.sugarandsaltsolutions.micro.model.MicroModel;
-import edu.colorado.phet.sugarandsaltsolutions.micro.model.SugarIon;
-import edu.colorado.phet.sugarandsaltsolutions.water.model.WaterModel;
-import edu.colorado.phet.sugarandsaltsolutions.water.view.SucroseNode;
 
 /**
  * Micro tab that shows the NaCl ions and Sucrose molecules.
@@ -48,37 +33,8 @@ public class MicroModule extends Module {
         setSimulationPanel( new MicroCanvas( model, configuration ) );
     }
 
-    //Create an image for sucrose using the same code as in the water tab to keep representations consistent
-    private static BufferedImage getSucroseImage() {
-        //Create a transform that will make the constituent particles big enough since they are rasterized.  I obtained the values by running the Water tab and printing out the box2d transform used in SucroseNode
-        final ModelViewTransform transform = ModelViewTransform.createSinglePointScaleMapping( new Point2D.Double(), new Point2D.Double(), 3150 / 6.3E-7 * 400 );
-
-        //Create the graphic
-        final SucroseNode sucroseNode = new SucroseNode( transform, new WaterModel().newSugar( 0, 0 ), new VoidFunction1<VoidFunction0>() {
-            public void apply( VoidFunction0 voidFunction0 ) {
-                voidFunction0.apply();
-            }
-        }, Element.O.getColor(), Element.H.getColor(), Color.gray ) {{
-//        }, Color.yellow, Color.yellow, Color.yellow ) {{
-
-            //Scale the graphic so it will be a good size for putting into a crystal lattice, with sizes
-            //Just using RADIUS * 2 leaves too much space between particles in the lattice
-            double width = getFullBounds().getWidth();
-            scale( SugarIon.RADIUS * 3 / width );
-
-            //Put it a random angle
-            rotate( Math.random() * Math.PI * 2 );
-        }};
-        return (BufferedImage) sucroseNode.toImage();
-    }
-
     @Override public void reset() {
         super.reset();
         model.reset();
-    }
-
-    //Sample main writes a sucrose image to file for inspection
-    public static void main( String[] args ) throws IOException {
-        ImageIO.write( getSucroseImage(), "PNG", new File( args[0], System.currentTimeMillis() + ".PNG" ) );
     }
 }
