da87fdf3a3fd1a7a50a3b85a36ae546305790959
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-06-21 00:26:07 +0000

    > Fixed: the play button in the expression evaluator only shows sometimes, resolves #2892

--- trunk/simulations-java/simulations/moving-man/src/edu/colorado/phet/movingman/ExpressionDialog.java
+++ trunk/simulations-java/simulations/moving-man/src/edu/colorado/phet/movingman/ExpressionDialog.java
@@ -2,6 +2,8 @@
 package edu.colorado.phet.movingman;
 
 import bsh.EvalError;
+
+import edu.colorado.phet.common.phetcommon.model.property.BooleanProperty;
 import edu.colorado.phet.common.phetcommon.view.PhetFrame;
 import edu.colorado.phet.common.phetcommon.view.VerticalLayoutPanel;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
@@ -54,12 +56,12 @@ public class ExpressionDialog extends JDialog {
             contentPane.add(expressionPanel);
             PhetPCanvas goButtonCanvas = new PhetPCanvas();//The go button is a PNode, so we must be embedded in a phetpcanvas 
             {
-                GoButton goButton = new GoButton(module.recordAndPlaybackModel, module.getMovingManModel().getPositionMode());
+                GoButton goButton = new GoButton(module.recordAndPlaybackModel, new BooleanProperty( true ) );
                 goButtonCanvas.addScreenChild(goButton);
                 Dimension buttonDim = new Dimension((int) goButton.getFullBounds().getWidth(), (int) goButton.getFullBounds().getHeight());
                 goButtonCanvas.setPreferredSize(buttonDim);
-                goButtonCanvas.setMaximumSize(buttonDim);
-                goButtonCanvas.setBorder(null);
+                goButtonCanvas.setMaximumSize( buttonDim );
+                goButtonCanvas.setBorder( null );
                 goButtonCanvas.setBackground(contentPane.getBackground());
             }
             contentPane.setFillNone();//Center the go button
