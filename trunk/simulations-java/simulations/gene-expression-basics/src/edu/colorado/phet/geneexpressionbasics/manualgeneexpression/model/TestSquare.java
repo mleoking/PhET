6576350aec26f5e7c5bacc0725e56efd2c4d0f7a
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-06-22 20:16:15 +0000

    Added some basic model classes to allow prototyping of movement along the DNA strand.

---
+++ trunk/simulations-java/simulations/gene-expression-basics/src/edu/colorado/phet/geneexpressionbasics/manualgeneexpression/model/TestSquare.java
@@ -0,0 +1,19 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.geneexpressionbasics.manualgeneexpression.model;
+
+import java.awt.geom.Rectangle2D;
+
+/**
+ * For testing and prototyping.
+ * TODO: This class should be removed when no longer used.
+ *
+ * @author John Blanco
+ */
+public class TestSquare extends ModelObject {
+    /**
+     * Constructor.
+     */
+    public TestSquare() {
+        super( new Rectangle2D.Double( -10, -10, 20, 20 ) );
+    }
+}
