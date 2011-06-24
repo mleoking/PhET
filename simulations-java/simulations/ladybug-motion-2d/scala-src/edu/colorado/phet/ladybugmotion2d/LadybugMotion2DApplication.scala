9d5e8a3ff79d80b4cb52d8f88a31ebb535f76342
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-06-24 15:14:16 +0000

    added static imports for main

--- simulations-java/simulations/ladybug-motion-2d/scala-src/edu/colorado/phet/ladybugmotion2d/LadybugMotion2DApplication.scala
+++ simulations-java/simulations/ladybug-motion-2d/scala-src/edu/colorado/phet/ladybugmotion2d/LadybugMotion2DApplication.scala
@@ -1,11 +1,12 @@
 package edu.colorado.phet.ladybugmotion2d
 
 import model.LadybugModel
-import edu.colorado.phet.scalacommon.{ScalaClock, ScalaApplicationLauncher}
+import edu.colorado.phet.scalacommon.ScalaClock
+import edu.colorado.phet.ladybugmotion2d.LadybugDefaults.defaultDT
+import edu.colorado.phet.scalacommon.ScalaApplicationLauncher.launchApplication
 
 object LadybugMotion2DApplication {
   def main(args: Array[String]) {
-    ScalaApplicationLauncher.launchApplication(args, "ladybug-motion-2d", "ladybug-motion-2d",
-                                               () => new LadybugModule[LadybugModel](new ScalaClock(30, LadybugDefaults.defaultDT)))
+    launchApplication(args, "ladybug-motion-2d", "ladybug-motion-2d", () => new LadybugModule[LadybugModel](new ScalaClock(30, defaultDT)))
   }
 }
