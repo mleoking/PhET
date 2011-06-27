b6137161ca98a6b5a6f1fb7ba197539aecc0a8dd
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-06-27 15:49:46 +0000

    repackaged conductivity tester nodes

--- trunk/simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/macro/view/ConductivityTesterToolNode.java
+++ trunk/simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/macro/view/ConductivityTesterToolNode.java
@@ -1,5 +1,5 @@
 // Copyright 2002-2011, University of Colorado
-package edu.colorado.phet.sugarandsaltsolutions.common.view;
+package edu.colorado.phet.sugarandsaltsolutions.macro.view;
 
 import edu.colorado.phet.common.piccolophet.nodes.ToolNode;
 import edu.umd.cs.piccolo.PNode;
