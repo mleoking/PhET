7b1d49c75a2c9ed2709e2ef3f849471801c17e54
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-06-28 15:48:59 +0000

    Refactored color properties

--- simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/SugarAndSaltSolutionsColorScheme.java
+++ simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/SugarAndSaltSolutionsColorScheme.java
@@ -3,10 +3,7 @@ package edu.colorado.phet.sugarandsaltsolutions;
 
 import java.awt.*;
 
-import edu.colorado.phet.common.phetcommon.model.property.CompositeProperty;
-import edu.colorado.phet.common.phetcommon.model.property.ObservableProperty;
 import edu.colorado.phet.common.phetcommon.model.property.Property;
-import edu.colorado.phet.common.phetcommon.util.function.Function0;
 
 import static java.awt.Color.white;
 
@@ -18,33 +15,9 @@ import static java.awt.Color.white;
 public class SugarAndSaltSolutionsColorScheme {
     private static final Color background = new Color( 0, 51, 153 );//Color recommendation from KL so that white sugar and salt will show against it.  Same color as geometric optics background
 
-    private final Property<Color> userSelectedBackgroundColor = new Property<Color>( background );
-
     public final Property<Color> saltColor = new Property<Color>( white );
 
     public final Property<Boolean> whiteBackground = new Property<Boolean>( false );
 
     public final ColorSet backgroundColorSet = new ColorSet( background, whiteBackground, Color.white );
-
-    public static class ColorSet {
-        public final Property<Boolean> whiteBackground;
-        public final Color colorForWhiteBackground;
-
-        //The background color selected by the user, may not be the displayed color based on whether 'white background' is selected
-        public final Property<Color> selectedColor;
-
-        //The background color to show, which accounts for the whiteBackground flag
-        public final ObservableProperty<Color> color;
-
-        public ColorSet( Color color, final Property<Boolean> whiteBackground, final Color colorForWhiteBackground ) {
-            this.whiteBackground = whiteBackground;
-            this.colorForWhiteBackground = colorForWhiteBackground;
-            this.selectedColor = new Property<Color>( color );
-            this.color = new CompositeProperty<Color>( new Function0<Color>() {
-                public Color apply() {
-                    return whiteBackground.get() ? colorForWhiteBackground : selectedColor.get();
-                }
-            }, whiteBackground, selectedColor );
-        }
-    }
 }
