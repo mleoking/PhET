aaa886939147d463dde9f2efc961be46da6e5042
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-06-28 03:14:08 +0000

    ms: adding Java structure

---
+++ trunk/simulations-java/simulations/molecule-shapes/src/edu/colorado/phet/moleculeshapes/MoleculeShapesConstants.java
@@ -0,0 +1,13 @@
+// Copyright 2002-2011, University of Colorado
+
+package edu.colorado.phet.moleculeshapes;
+
+public class MoleculeShapesConstants {
+
+    /* Not intended for instantiation. */
+    private MoleculeShapesConstants() {
+    }
+
+    public static final String PROJECT_NAME = "molecule-shapes";
+
+}
