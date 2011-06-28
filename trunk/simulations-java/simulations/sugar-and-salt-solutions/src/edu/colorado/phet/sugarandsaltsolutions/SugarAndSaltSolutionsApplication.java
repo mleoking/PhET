44310027e5a46615fc55de0de971967fd2ab9069
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-06-28 18:43:42 +0000

    switched to micro tab for development

--- trunk/simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/SugarAndSaltSolutionsApplication.java
+++ trunk/simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/SugarAndSaltSolutionsApplication.java
@@ -34,7 +34,7 @@ public class SugarAndSaltSolutionsApplication extends PiccoloPhetApplication {
         addModule( new WaterModule( globalState ) );
 
         if ( config.isDev() ) {
-            setStartModule( moduleAt( 2 ) );
+            setStartModule( moduleAt( 1 ) );
         }
 
         //Add developer menus for changing the color of background and salt
