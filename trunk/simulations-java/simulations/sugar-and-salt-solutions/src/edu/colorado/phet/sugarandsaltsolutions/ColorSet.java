f28fa09f07b751fc7175872659a6cd60ac84acae
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-06-28 15:48:59 +0000

    Refactored color properties

---
+++ trunk/simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/ColorSet.java
@@ -0,0 +1,36 @@
+package edu.colorado.phet.sugarandsaltsolutions;
+
+import java.awt.*;
+
+import edu.colorado.phet.common.phetcommon.model.property.CompositeProperty;
+import edu.colorado.phet.common.phetcommon.model.property.ObservableProperty;
+import edu.colorado.phet.common.phetcommon.model.property.Property;
+import edu.colorado.phet.common.phetcommon.util.function.Function0;
+
+/**
+ * Gives colors for objects (including background) in the sugar and salt sim.  This allows the user to configure colors to use against a dark background
+ * and models the color to switch to when the background is white.
+ *
+ * @author Sam Reid
+ */
+public class ColorSet {
+    public final Property<Boolean> whiteBackground;
+    public final Color colorForWhiteBackground;
+
+    //The background color selected by the user, may not be the displayed color based on whether 'white background' is selected
+    public final Property<Color> selectedColor;
+
+    //The background color to show, which accounts for the whiteBackground flag
+    public final ObservableProperty<Color> color;
+
+    public ColorSet( Color color, final Property<Boolean> whiteBackground, final Color colorForWhiteBackground ) {
+        this.whiteBackground = whiteBackground;
+        this.colorForWhiteBackground = colorForWhiteBackground;
+        this.selectedColor = new Property<Color>( color );
+        this.color = new CompositeProperty<Color>( new Function0<Color>() {
+            public Color apply() {
+                return whiteBackground.get() ? colorForWhiteBackground : selectedColor.get();
+            }
+        }, whiteBackground, selectedColor );
+    }
+}
