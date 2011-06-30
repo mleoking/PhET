b192aa528f6f2a1509cbbceafb9b6564f18fe24d
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-06-30 18:14:57 +0000

    Added a test for good code red, see #2986

---
+++ trunk/team/reids/idea-tests/src/ideatest/VerticalPanel.java
@@ -0,0 +1,31 @@
+package ideatest;
+
+/**
+ * In IntelliJ IDEA 10.5 #IU-107.105 the "addChild" in VerticalPanel's super call is underlined in red,
+ * with the mouseover message "Cannot reference "Component.addChild" before supertype constructor has been called"
+ */
+//Parent class with an overrideable method
+class Component {
+    void addChild( Component b ) {
+    }
+}
+
+class Panel extends Component {
+    public Panel( Component pNode ) {
+    }
+
+    Panel() {
+    }
+}
+
+public class VerticalPanel extends Panel {
+    public VerticalPanel() {
+        super( new Component() {{
+            addChild( new Panel() );
+        }} );
+    }
+
+    public static void main( String[] args ) {
+        System.out.println( "new SelectedItemNode() = " + new VerticalPanel() );
+    }
+}
