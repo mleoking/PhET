f76b6dcd95c533ada0fdf1555dae93a2750b0f6b
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-07-01 17:28:40 +0000

    started on periodic table

---
+++ simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/micro/view/PeriodicTableDialog.java
@@ -0,0 +1,12 @@
+package edu.colorado.phet.sugarandsaltsolutions.micro.view;
+
+import javax.swing.*;
+
+/**
+ * @author Sam Reid
+ */
+public class PeriodicTableDialog extends JDialog {
+    public PeriodicTableDialog() {
+
+    }
+}
