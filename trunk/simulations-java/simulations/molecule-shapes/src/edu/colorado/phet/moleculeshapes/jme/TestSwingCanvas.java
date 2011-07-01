654076173a0f18ec2a98f8263683c96c27dd8a19
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-07-01 04:56:31 +0000

    Launch the jme canvas within a tabbed PiccoloPhetApplication

--- trunk/simulations-java/simulations/molecule-shapes/src/edu/colorado/phet/moleculeshapes/jme/TestSwingCanvas.java
+++ trunk/simulations-java/simulations/molecule-shapes/src/edu/colorado/phet/moleculeshapes/jme/TestSwingCanvas.java
@@ -1,15 +1,15 @@
 package edu.colorado.phet.moleculeshapes.jme;
 
 import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.util.concurrent.Callable;
 
 import javax.swing.*;
 
-import edu.colorado.phet.common.phetcommon.application.PhetApplication;
+import edu.colorado.phet.common.phetcommon.application.Module;
 import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
-import edu.colorado.phet.common.phetcommon.view.PhetFrame;
+import edu.colorado.phet.common.phetcommon.application.PhetApplicationLauncher;
+import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
+import edu.colorado.phet.common.piccolophet.PiccoloPhetApplication;
 
 import com.jme3.app.Application;
 import com.jme3.app.SimpleApplication;
@@ -17,52 +17,60 @@ import com.jme3.system.AppSettings;
 import com.jme3.system.JmeCanvasContext;
 
 //Copied from http://code.google.com/p/jmonkeyengine/source/browse/trunk/engine/src/test/jme3test/awt/TestCanvas.java
-public class TestSwingCanvas {
+public class TestSwingCanvas extends PiccoloPhetApplication {
+
+    public TestSwingCanvas( PhetApplicationConfig config ) {
+        super( config );
+        addModule( new JMEModule( "Module 1" ) );
+        addModule( new JMEModule( "Module 2" ) );
+    }
 
     public static void main( String[] args ) {
+        new PhetApplicationLauncher().launchSim( args, "molecule-shapes", TestSwingCanvas.class );
+    }
+
+    public static void main2() {
         SwingUtilities.invokeLater( new Runnable() {
             public void run() {
-
                 JPopupMenu.setDefaultLightWeightPopupEnabled( false );
-
-                PhetFrame frame = new PhetFrame( new PhetApplication( new PhetApplicationConfig( new String[0], "molecule-shapes" ) ) );
-
-                AppSettings settings = new AppSettings( true );
-                settings.setWidth( Math.max( 640, frame.getContentPane().getWidth() ) );
-                settings.setHeight( Math.max( 480, frame.getContentPane().getHeight() ) );
-
-                final Application app = new ShowMolecule();
-
-                frame.addWindowListener( new WindowAdapter() {
-                    @Override
-                    public void windowClosed( WindowEvent e ) {
-                        app.stop();
-                    }
-                } );
-
-                //Improve default camera angle and mouse behavior
-                app.enqueue( new Callable<Void>() {
-                    public Void call() {
-                        SimpleApplication simpleApp = (SimpleApplication) app;
-                        simpleApp.getFlyByCamera().setDragToRotate( true );
-                        return null;
-                    }
-                } );
-
-                app.setPauseOnLostFocus( false );
-                app.setSettings( settings );
-                app.createCanvas();
-
-                JmeCanvasContext context = (JmeCanvasContext) app.getContext();
-                final Canvas canvas = context.getCanvas();
-                canvas.setSize( settings.getWidth(), settings.getHeight() );
-
-                frame.getContentPane().add( canvas );
-                frame.pack();
-                app.startCanvas();
-                frame.setLocationRelativeTo( null );
-                frame.setVisible( true );
             }
         } );
+    }
+
+    private class JMEModule extends Module {
+        public JMEModule( String name ) {
+            super( name, new ConstantDtClock( 30.0 ) );
+            AppSettings settings = new AppSettings( true );
+
+            final Application app = new MoleculeApplication();
+
+            //Improve default camera angle and mouse behavior
+            app.enqueue( new Callable<Void>() {
+                public Void call() {
+                    SimpleApplication simpleApp = (SimpleApplication) app;
+                    simpleApp.getFlyByCamera().setDragToRotate( true );
+                    return null;
+                }
+            } );
+
+            app.setPauseOnLostFocus( false );
+            app.setSettings( settings );
+            app.createCanvas();
+
+            JmeCanvasContext context = (JmeCanvasContext) app.getContext();
+            final Canvas canvas = context.getCanvas();
+            canvas.setSize( settings.getWidth(), settings.getHeight() );
+            addListener( new Listener() {
+                public void activated() {
+                    app.startCanvas();
+                }
+
+                public void deactivated() {
+//                    app.stop(  );
+                }
+            } );
+
+            setSimulationPanel( new JPanel( new BorderLayout() ) {{add( canvas, BorderLayout.CENTER );}} );
+        }
     }
 }
