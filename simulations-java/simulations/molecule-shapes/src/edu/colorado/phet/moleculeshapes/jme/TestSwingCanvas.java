a15931996507d65276313e30f75ac2178c408210
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-07-01 04:02:19 +0000

    Launch the jme application within a PhetFrame, adding the file and help-> about menus

--- simulations-java/simulations/molecule-shapes/src/edu/colorado/phet/moleculeshapes/jme/TestSwingCanvas.java
+++ simulations-java/simulations/molecule-shapes/src/edu/colorado/phet/moleculeshapes/jme/TestSwingCanvas.java
@@ -1,22 +1,20 @@
 package edu.colorado.phet.moleculeshapes.jme;
 
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.util.concurrent.Callable;
-import java.util.logging.ConsoleHandler;
-import java.util.logging.Handler;
-import java.util.logging.Logger;
 
 import javax.swing.*;
+
+import edu.colorado.phet.common.phetcommon.application.PhetApplication;
+import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
+import edu.colorado.phet.common.phetcommon.view.PhetFrame;
 
 import com.jme3.app.Application;
 import com.jme3.app.SimpleApplication;
 import com.jme3.system.AppSettings;
 import com.jme3.system.JmeCanvasContext;
-import com.jme3.util.JmeFormatter;
 
 //Copied from http://code.google.com/p/jmonkeyengine/source/browse/trunk/engine/src/test/jme3test/awt/TestCanvas.java
 public class TestSwingCanvas {
@@ -27,79 +25,13 @@ public class TestSwingCanvas {
     private static JFrame frame;
 
     private static void createFrame() {
-        frame = new JFrame( "Test" );
-        frame.setDefaultCloseOperation( JFrame.DISPOSE_ON_CLOSE );
+        frame = new PhetFrame( new PhetApplication( new PhetApplicationConfig( new String[0], "molecule-shapes" ) ) );
         frame.addWindowListener( new WindowAdapter() {
             @Override
             public void windowClosed( WindowEvent e ) {
                 app.stop();
             }
         } );
-
-        JMenuBar menuBar = new JMenuBar();
-        frame.setJMenuBar( menuBar );
-
-        JMenu menuFile = new JMenu( "File" );
-        menuBar.add( menuFile );
-
-        final JMenuItem itemRemoveCanvas = new JMenuItem( "Remove Canvas" );
-        menuFile.add( itemRemoveCanvas );
-        itemRemoveCanvas.addActionListener( new ActionListener() {
-            public void actionPerformed( ActionEvent e ) {
-                if ( itemRemoveCanvas.getText().equals( "Remove Canvas" ) ) {
-                    frame.getContentPane().remove( canvas );
-
-                    // force OS to repaint over canvas ..
-                    // this is needed since AWT does not handle
-                    // that when a heavy-weight component is removed.
-                    frame.setVisible( false );
-                    frame.setVisible( true );
-                    frame.requestFocus();
-
-                    itemRemoveCanvas.setText( "Add Canvas" );
-                }
-                else if ( itemRemoveCanvas.getText().equals( "Add Canvas" ) ) {
-                    frame.getContentPane().add( canvas );
-                    itemRemoveCanvas.setText( "Remove Canvas" );
-                }
-            }
-        } );
-
-        JMenuItem itemKillCanvas = new JMenuItem( "Stop/Start Canvas" );
-        menuFile.add( itemKillCanvas );
-        itemKillCanvas.addActionListener( new ActionListener() {
-            public void actionPerformed( ActionEvent e ) {
-                frame.getContentPane().remove( canvas );
-                app.stop( true );
-
-                createCanvas();
-                frame.getContentPane().add( canvas );
-                frame.pack();
-                startApp();
-            }
-        } );
-
-        JMenuItem itemExit = new JMenuItem( "Exit" );
-        menuFile.add( itemExit );
-        itemExit.addActionListener( new ActionListener() {
-            public void actionPerformed( ActionEvent ae ) {
-                frame.dispose();
-                app.stop();
-            }
-        } );
-
-        JMenu menuEdit = new JMenu( "Edit" );
-        menuBar.add( menuEdit );
-        JMenuItem itemDelete = new JMenuItem( "Delete" );
-        menuEdit.add( itemDelete );
-
-        JMenu menuView = new JMenu( "View" );
-        menuBar.add( menuView );
-        JMenuItem itemSetting = new JMenuItem( "Settings" );
-        menuView.add( itemSetting );
-
-        JMenu menuHelp = new JMenu( "Help" );
-        menuBar.add( menuHelp );
     }
 
     public static void createCanvas() {
@@ -133,14 +65,6 @@ public class TestSwingCanvas {
     }
 
     public static void main( String[] args ) {
-        JmeFormatter formatter = new JmeFormatter();
-
-        Handler consoleHandler = new ConsoleHandler();
-        consoleHandler.setFormatter( formatter );
-
-        Logger.getLogger( "" ).removeHandler( Logger.getLogger( "" ).getHandlers()[0] );
-        Logger.getLogger( "" ).addHandler( consoleHandler );
-
         SwingUtilities.invokeLater( new Runnable() {
             public void run() {
                 JPopupMenu.setDefaultLightWeightPopupEnabled( false );
@@ -156,5 +80,4 @@ public class TestSwingCanvas {
             }
         } );
     }
-
 }
