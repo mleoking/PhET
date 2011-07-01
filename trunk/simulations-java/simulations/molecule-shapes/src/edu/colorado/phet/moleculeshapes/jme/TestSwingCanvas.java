57367140ae3fdaa67b5041e8ba7b3bebee96cc6c
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-07-01 04:09:21 +0000

    removed unused code

--- trunk/simulations-java/simulations/molecule-shapes/src/edu/colorado/phet/moleculeshapes/jme/TestSwingCanvas.java
+++ trunk/simulations-java/simulations/molecule-shapes/src/edu/colorado/phet/moleculeshapes/jme/TestSwingCanvas.java
@@ -19,62 +19,47 @@ import com.jme3.system.JmeCanvasContext;
 //Copied from http://code.google.com/p/jmonkeyengine/source/browse/trunk/engine/src/test/jme3test/awt/TestCanvas.java
 public class TestSwingCanvas {
 
-    private static JmeCanvasContext context;
-    private static Canvas canvas;
-    private static Application app;
-    private static JFrame frame;
-
-    private static void createFrame() {
-        frame = new PhetFrame( new PhetApplication( new PhetApplicationConfig( new String[0], "molecule-shapes" ) ) );
-        frame.addWindowListener( new WindowAdapter() {
-            @Override
-            public void windowClosed( WindowEvent e ) {
-                app.stop();
-            }
-        } );
-    }
-
-    public static void createCanvas() {
-        AppSettings settings = new AppSettings( true );
-        settings.setWidth( Math.max( 640, frame.getContentPane().getWidth() ) );
-        settings.setHeight( Math.max( 480, frame.getContentPane().getHeight() ) );
-
-        app = new ShowMolecule();
-
-        app.setPauseOnLostFocus( false );
-        app.setSettings( settings );
-        app.createCanvas();
-
-        context = (JmeCanvasContext) app.getContext();
-        canvas = context.getCanvas();
-        canvas.setSize( settings.getWidth(), settings.getHeight() );
-    }
-
-    public static void startApp() {
-        app.startCanvas();
-        app.enqueue( new Callable<Void>() {
-            public Void call() {
-                if ( app instanceof SimpleApplication ) {
-                    SimpleApplication simpleApp = (SimpleApplication) app;
-                    simpleApp.getFlyByCamera().setDragToRotate( true );
-                }
-                return null;
-            }
-        } );
-
-    }
-
     public static void main( String[] args ) {
         SwingUtilities.invokeLater( new Runnable() {
             public void run() {
+
                 JPopupMenu.setDefaultLightWeightPopupEnabled( false );
 
-                createFrame();
-                createCanvas();
+                PhetFrame frame = new PhetFrame( new PhetApplication( new PhetApplicationConfig( new String[0], "molecule-shapes" ) ) );
+
+                AppSettings settings = new AppSettings( true );
+                settings.setWidth( Math.max( 640, frame.getContentPane().getWidth() ) );
+                settings.setHeight( Math.max( 480, frame.getContentPane().getHeight() ) );
+
+                final Application app = new ShowMolecule();
+
+                frame.addWindowListener( new WindowAdapter() {
+                    @Override
+                    public void windowClosed( WindowEvent e ) {
+                        app.stop();
+                    }
+                } );
+
+                //Improve default camera angle and mouse behavior
+                app.enqueue( new Callable<Void>() {
+                    public Void call() {
+                        SimpleApplication simpleApp = (SimpleApplication) app;
+                        simpleApp.getFlyByCamera().setDragToRotate( true );
+                        return null;
+                    }
+                } );
+
+                app.setPauseOnLostFocus( false );
+                app.setSettings( settings );
+                app.createCanvas();
+
+                JmeCanvasContext context = (JmeCanvasContext) app.getContext();
+                final Canvas canvas = context.getCanvas();
+                canvas.setSize( settings.getWidth(), settings.getHeight() );
 
                 frame.getContentPane().add( canvas );
                 frame.pack();
-                startApp();
+                app.startCanvas();
                 frame.setLocationRelativeTo( null );
                 frame.setVisible( true );
             }
