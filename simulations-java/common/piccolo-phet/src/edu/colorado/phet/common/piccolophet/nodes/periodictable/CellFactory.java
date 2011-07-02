5bdc721748556dd92d9a0cc075fd06528b7d0ff2
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-07-02 05:51:19 +0000

    7/1/11 Fixed: highlighted cells are clipped on 2 sides, see #2944

--- simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/nodes/periodictable/CellFactory.java
+++ simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/nodes/periodictable/CellFactory.java
@@ -1,9 +1,12 @@
 package edu.colorado.phet.common.piccolophet.nodes.periodictable;
 
 import java.awt.*;
+import java.util.Arrays;
 
+import edu.colorado.phet.common.phetcommon.util.function.Function1;
 import edu.colorado.phet.common.piccolophet.nodes.periodictable.PeriodicTableNode.BasicElementCell;
 import edu.colorado.phet.common.piccolophet.nodes.periodictable.PeriodicTableNode.ElementCell;
+import edu.colorado.phet.common.piccolophet.nodes.periodictable.PeriodicTableNode.HighlightedElementCell;
 
 /**
  * Determines how to create PNodes for cells in the periodic table.
@@ -17,6 +20,28 @@ public interface CellFactory {
     public static class Default implements CellFactory {
         public ElementCell createCellForElement( int atomicNumberOfCell, Color backgroundColor ) {
             return new BasicElementCell( atomicNumberOfCell, backgroundColor );
+        }
+    }
+
+    //Cell factory that highlights the specified elements
+    public static class HighlightElements implements CellFactory {
+        private final Function1<Integer, Boolean> highlightRule;
+
+        public HighlightElements( final Integer... highlighted ) {
+            this( new Function1<Integer, Boolean>() {
+                public Boolean apply( Integer integer ) {
+                    return Arrays.asList( highlighted ).contains( integer );
+                }
+            } );
+        }
+
+        public HighlightElements( final Function1<Integer, Boolean> highlightRule ) {
+            this.highlightRule = highlightRule;
+        }
+
+        //If the cell should be highlighted, return a highlighted node instead of a regular one
+        public ElementCell createCellForElement( int atomicNumberOfCell, Color backgroundColor ) {
+            return highlightRule.apply( atomicNumberOfCell ) ? new HighlightedElementCell( atomicNumberOfCell, backgroundColor ) : new BasicElementCell( atomicNumberOfCell, backgroundColor );
         }
     }
 }
