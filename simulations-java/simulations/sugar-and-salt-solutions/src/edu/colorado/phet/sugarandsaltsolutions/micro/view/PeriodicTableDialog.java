1bf1fef0383dee9e53edf11a88edef0d488ad52c
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-07-02 06:20:35 +0000

    Micro tab: Synchronize periodic table with the selected solute

--- simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/micro/view/PeriodicTableDialog.java
+++ simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/micro/view/PeriodicTableDialog.java
@@ -12,6 +12,7 @@ import edu.colorado.phet.common.piccolophet.nodes.periodictable.CellFactory.High
 import edu.colorado.phet.common.piccolophet.nodes.periodictable.PeriodicTableNode;
 import edu.colorado.phet.sugarandsaltsolutions.SugarAndSaltSolutionsColorScheme;
 import edu.colorado.phet.sugarandsaltsolutions.common.model.DispenserType;
+import edu.umd.cs.piccolo.PNode;
 
 /**
  * Dialog that shows the periodic table and updates when the selected solute type changes
@@ -21,18 +22,32 @@ import edu.colorado.phet.sugarandsaltsolutions.common.model.DispenserType;
 public class PeriodicTableDialog extends JDialog {
     public PeriodicTableDialog( final Property<DispenserType> dispenser, final SugarAndSaltSolutionsColorScheme colorScheme, PhetFrame parentFrame ) {
         super( parentFrame );
-        setContentPane( new PhetPCanvas() {{
-            colorScheme.backgroundColorSet.color.addObserver( new VoidFunction1<Color>() {
-                public void apply( Color color ) {
-                    setBackground( color );
-                }
-            } );
-            final PeriodicTableNode node = new PeriodicTableNode( Color.white, new HighlightElements( dispenser.get().getElementAtomicMasses() ) ) {{
-                scale( 1.5 );
-            }};
-            addScreenChild( node );
-            setPreferredSize( new Dimension( (int) node.getFullBounds().getWidth(), (int) node.getFullBounds().getHeight() ) );
-        }} );
+        setContentPane( new PhetPCanvas() {
+            PNode root = new PNode();
+
+            {
+                addScreenChild( root );
+
+                //Match the background with the rest of the sim
+                colorScheme.backgroundColorSet.color.addObserver( new VoidFunction1<Color>() {
+                    public void apply( Color color ) {
+                        setBackground( color );
+                    }
+                } );
+
+                //On init, and when the dispenser type changes, show the periodic table for the specified DispenserType
+                dispenser.addObserver( new VoidFunction1<DispenserType>() {
+                    public void apply( DispenserType dispenserType ) {
+                        final PeriodicTableNode node = new PeriodicTableNode( Color.white, new HighlightElements( dispenser.get().getElementAtomicMasses() ) ) {{
+                            scale( 1.5 );
+                        }};
+                        root.removeAllChildren();
+                        root.addChild( node );
+                        setPreferredSize( new Dimension( (int) node.getFullBounds().getWidth(), (int) node.getFullBounds().getHeight() ) );
+                    }
+                } );
+            }
+        } );
         pack();
     }
 }
