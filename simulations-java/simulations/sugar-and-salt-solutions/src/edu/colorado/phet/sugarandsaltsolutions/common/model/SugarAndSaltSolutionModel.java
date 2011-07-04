579e50c8e1472f378afad340eaa8d83c498de776
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-07-04 01:43:35 +0000

    moved conductivity tester to MacroModel subclass

--- simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/common/model/SugarAndSaltSolutionModel.java
+++ simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/common/model/SugarAndSaltSolutionModel.java
@@ -1,14 +1,11 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.sugarandsaltsolutions.common.model;
 
-import java.awt.*;
 import java.awt.geom.Line2D;
-import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 
 import edu.colorado.phet.common.phetcommon.math.ImmutableRectangle2D;
 import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
-import edu.colorado.phet.common.phetcommon.math.MathUtil;
 import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
 import edu.colorado.phet.common.phetcommon.model.event.Notifier;
 import edu.colorado.phet.common.phetcommon.model.property.ObservableProperty;
@@ -16,10 +13,8 @@ import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.phetcommon.model.property.SettableProperty;
 import edu.colorado.phet.common.phetcommon.model.property.doubleproperty.CompositeDoubleProperty;
 import edu.colorado.phet.common.phetcommon.model.property.doubleproperty.DoubleProperty;
-import edu.colorado.phet.common.phetcommon.util.RichSimpleObserver;
 import edu.colorado.phet.common.phetcommon.util.function.Function0;
 import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
-import edu.colorado.phet.common.piccolophet.nodes.conductivitytester.IConductivityTester.ConductivityTesterChangeListener;
 import edu.colorado.phet.sugarandsaltsolutions.SugarAndSaltSolutionsResources;
 import edu.colorado.phet.sugarandsaltsolutions.common.view.VerticalRangeContains;
 import edu.colorado.phet.sugarandsaltsolutions.macro.model.MacroCrystal;
@@ -96,9 +91,6 @@ public class SugarAndSaltSolutionModel extends AbstractSugarAndSaltSolutionsMode
     public final CompositeDoubleProperty saltConcentration;
     public final CompositeDoubleProperty sugarConcentration;
 
-    //Shape of the water draining out the output faucet, needed for purposes of determining whether there is an electrical connection for the conductivity tester
-    private Rectangle2D outFlowShape;
-
     //Model for the salt shaker
     public final SaltShaker saltShaker;
 
@@ -111,9 +103,6 @@ public class SugarAndSaltSolutionModel extends AbstractSugarAndSaltSolutionsMode
     //Amounts of sugar and salt in crystal form falling from the dispenser
     private CompositeDoubleProperty airborneSaltGrams;
     private CompositeDoubleProperty airborneSugarGrams;
-
-    //Model for the conductivity tester
-    public final ConductivityTester conductivityTester;
 
     //User setting: whether the concentration bar chart should be shown
     public final Property<Boolean> showConcentrationBarChart;
@@ -200,9 +189,6 @@ public class SugarAndSaltSolutionModel extends AbstractSugarAndSaltSolutionsMode
         //This logic is used in the model update step to determine if water can flow out, as well as in the user interface to determine if the user can turn on the output faucet
         lowerFaucetCanDrain = new VerticalRangeContains( solution.shape, 0.011746031746031754, 0.026349206349206344 );
 
-        //Shape of the water draining out the output faucet, needed for purposes of determining whether there is an electrical connection for the conductivity tester
-        outFlowShape = new Rectangle();
-
         //Model for the salt shaker
         saltShaker = new SaltShaker( beaker.getCenterX(), beaker.getTopY() + beaker.getHeight() * 0.5, beaker, moreSaltAllowed, getSaltShakerName() ) {{
             //Wire up the SugarDispenser so it is enabled when the model has the SALT type dispenser selected
@@ -213,9 +199,6 @@ public class SugarAndSaltSolutionModel extends AbstractSugarAndSaltSolutionsMode
             } );
         }};
 
-        //Model for the conductivity tester
-        conductivityTester = new ConductivityTester( beakerDimension.width, beakerDimension.height );
-
         sugarDispenser = new SugarDispenser( beaker.getCenterX(), beaker.getTopY() + beaker.getHeight() * 0.5, beaker, moreSugarAllowed, getSugarDispenserName() ) {{
             //Wire up the SugarDispenser so it is enabled when the model has the SUGAR type dispenser selected
             dispenserType.addObserver( new VoidFunction1<DispenserType>() {
@@ -224,32 +207,6 @@ public class SugarAndSaltSolutionModel extends AbstractSugarAndSaltSolutionsMode
                 }
             } );
         }};
-
-        //Update the conductivity tester when the water level changes, since it might move up to touch a probe (or move out from underneath a submerged probe)
-        new RichSimpleObserver() {
-            @Override public void update() {
-                updateConductivityTesterBrightness();
-            }
-        }.observe( saltConcentration, solution.shape );
-
-        //When the conductivity tester probe locations change, also update the conductivity tester brightness since they may come into contact (or leave contact) with the fluid
-        conductivityTester.addConductivityTesterChangeListener( new ConductivityTesterChangeListener() {
-            public void brightnessChanged() {
-            }
-
-            public void positiveProbeLocationChanged() {
-                updateConductivityTesterBrightness();
-            }
-
-            public void negativeProbeLocationChanged() {
-                updateConductivityTesterBrightness();
-            }
-
-            public void locationChanged() {
-                //Have to callback here too since the battery or bulb could get sumberged and short the circuit
-                updateConductivityTesterBrightness();
-            }
-        } );
     }
 
     //When a crystal is absorbed by the water, increase the number of moles in solution
@@ -293,37 +250,6 @@ public class SugarAndSaltSolutionModel extends AbstractSugarAndSaltSolutionsMode
     private void updateConcentration( double outVolume, double concentration, SettableProperty<Double> moles ) {
         double molesOfSoluteLeaving = concentration * outVolume;
         moles.set( moles.get() - molesOfSoluteLeaving );
-    }
-
-    //Determine if a conductivity tester probe is touching water in the beaker, or water flowing out of the beaker (which would have the same concentration as the water in the beaker)
-    private boolean isProbeTouchingWaterThatMightHaveSalt( ImmutableRectangle2D region ) {
-        Rectangle2D waterBounds = solution.shape.get().getBounds2D();
-
-        final Rectangle2D regionBounds = region.toRectangle2D();
-        return waterBounds.intersects( region.toRectangle2D() ) || outFlowShape.intersects( regionBounds );
-    }
-
-    //Update the conductivity tester brightness when the probes come into contact with (or stop contacting) the fluid
-    protected void updateConductivityTesterBrightness() {
-
-        //Check for a collision with the probe, using the full region of each probe (so if any part intersects, there is still an electrical connection).
-        Rectangle2D waterBounds = solution.shape.get().getBounds2D();
-
-        //See if both probes are touching water that might have salt in it
-        boolean bothProbesTouching = isProbeTouchingWaterThatMightHaveSalt( conductivityTester.getPositiveProbeRegion() ) && isProbeTouchingWaterThatMightHaveSalt( conductivityTester.getNegativeProbeRegion() );
-
-        //Check to see if the circuit is shorted out (if light bulb or battery is submerged).
-        //Null checks are necessary since those regions are computed from view components and may not have been computed yet (but will be non-null if the user dragged out the conductivity tester from the toolbox)
-        boolean batterySubmerged = conductivityTester.getBatteryRegion() != null && waterBounds.intersects( conductivityTester.getBatteryRegion().getBounds2D() );
-        boolean bulbSubmerged = conductivityTester.getBulbRegion() != null && waterBounds.intersects( conductivityTester.getBulbRegion().getBounds2D() );
-
-        //The circuit should short out if the battery or bulb is submerged
-        boolean shortCircuited = batterySubmerged || bulbSubmerged;
-
-        //Set the brightness to be a linear function of the salt concentration (but keeping it bounded between 0 and 1 which are the limits of the conductivity tester brightness
-        //Use a scale factor that matches up with the limits on saturation (manually sampled at runtime)
-        conductivityTester.brightness.set( bothProbesTouching && !shortCircuited ? MathUtil.clamp( 0, saltConcentration.get() * 1.62E-4, 1 ) : 0.0 );
-        conductivityTester.shortCircuited.set( shortCircuited );
     }
 
     //Adds the specified Sugar crystal to the model
@@ -456,18 +382,11 @@ public class SugarAndSaltSolutionModel extends AbstractSugarAndSaltSolutionsMode
         outputFlowRate.reset();
         sugarDispenser.reset();
         saltShaker.reset();
-        conductivityTester.reset();
         dispenserType.reset();
         showConcentrationValues.reset();
         showConcentrationBarChart.reset();
 
         notifyReset();
-    }
-
-    //Sets the shape of the water flowing out of the beaker, for purpose of determining conductivity (since outflow water can conduct)
-    public void setOutflowShape( Rectangle2D outFlowShape ) {
-        this.outFlowShape = outFlowShape;
-        updateConductivityTesterBrightness();
     }
 
     //Determine if any salt can be removed for purposes of displaying a "remove salt" button
--- simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/macro/view/MacroCanvas.java
+++ simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/macro/view/MacroCanvas.java
@@ -1,11 +1,16 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.sugarandsaltsolutions.macro.view;
 
+import java.awt.geom.Rectangle2D;
+
+import edu.colorado.phet.common.phetcommon.math.ImmutableRectangle2D;
+import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
 import edu.colorado.phet.sugarandsaltsolutions.GlobalState;
 import edu.colorado.phet.sugarandsaltsolutions.common.model.SugarAndSaltSolutionModel;
 import edu.colorado.phet.sugarandsaltsolutions.common.view.SoluteControlPanelNode;
 import edu.colorado.phet.sugarandsaltsolutions.common.view.SugarAndSaltSolutionsCanvas;
 import edu.colorado.phet.sugarandsaltsolutions.macro.model.MacroModel;
+import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.util.PDimension;
 import edu.umd.cs.piccolox.pswing.PSwingCanvas;
 
@@ -15,11 +20,30 @@ import edu.umd.cs.piccolox.pswing.PSwingCanvas;
  * @author Sam Reid
  */
 public class MacroCanvas extends SugarAndSaltSolutionsCanvas {
+
+    //Separate layer for the conductivity toolbox to make sure the conductivity node shows as submerged in the water, but still goes behind the shaker
+    protected final PNode conductivityToolboxLayer = new PNode();
+
     public MacroCanvas( final MacroModel model, GlobalState globalState ) {
         super( model, globalState );
 
         //This tab uses the conductivity tester
         submergedInWaterNode.addChild( conductivityToolboxLayer );
+
+        //Toolbox from which the conductivity tester can be dragged
+        conductivityToolboxLayer.addChild( new ConductivityTesterToolboxNode( model, this ) {{
+            //Set the location of the control panel
+            setOffset( stageSize.getWidth() - getFullBounds().getWidth() - INSET, soluteControlPanelNode.getFullBounds().getMaxY() + INSET );
+        }} );
+
+        //When the shape of the flowing-out water changes, update the model so we can account for conductivity of the water while it is draining
+        drainFaucetNode.addListener( new VoidFunction1<Rectangle2D>() {
+            public void apply( Rectangle2D outFlowShape ) {
+                ImmutableRectangle2D r = new ImmutableRectangle2D( outFlowShape );
+                Rectangle2D transformed = drainFaucetNode.localToGlobal( r.toRectangle2D() );
+                model.setOutflowShape( transform.viewToModel( transformed ).getBounds2D() );
+            }
+        } );
     }
 
     //Create a radio-button-based selector for solutes
