73ab59ed28bd4a1a27ba8af0a30241770feae26d
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-07-11 20:19:53 +0000

    stubs for properties and control panels

---
+++ trunk/simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/common/view/ViewProperties.java
@@ -0,0 +1,24 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.moleculepolarity.common.view;
+
+import edu.colorado.phet.common.phetcommon.model.property.Property;
+
+/**
+ * Properties that control things in the view.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
+public class ViewProperties {
+
+    // Choices for visual representations of the model.
+    public enum ModelRepresentation {
+        BALL_AND_STICK,
+        ELECTROSTATIC_POTENTIAL,
+        ELECTRON_DENSITY
+    }
+
+    public final Property<ModelRepresentation> modelRepresentation = new Property<ModelRepresentation>( ModelRepresentation.BALL_AND_STICK );
+    public final Property<Boolean> bondDipolesVisible = new Property<Boolean>( false );
+    public final Property<Boolean> moleculeDipoleVisible = new Property<Boolean>( false );
+    public final Property<Boolean> partialChargesVisible = new Property<Boolean>( false );
+}
---
+++ trunk/simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/control/TestPanel.java
@@ -0,0 +1,10 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.moleculepolarity.control;
+
+/**
+ * "Test" control panel.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
+public class TestPanel {
+}
