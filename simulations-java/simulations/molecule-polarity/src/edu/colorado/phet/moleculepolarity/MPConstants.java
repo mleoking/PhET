62b13c5e250302b0e5479f832a81bd6a1a1aaec9
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-07-11 17:34:04 +0000

    add new molecule-polarity simulation

---
+++ simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/MPConstants.java
@@ -0,0 +1,18 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.moleculepolarity;
+
+/**
+ * A collection of constants that configure global properties.
+ * If you change something here, it will change *everywhere* in this simulation.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
+public class MPConstants {
+
+     /* Not intended for instantiation. */
+    private MPConstants() {
+    }
+
+    public static final String PROJECT_NAME = "molecule-polarity";
+
+}
