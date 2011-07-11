a7f774304e7c80ee986e084146216963f9e039f3
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-07-11 20:19:53 +0000

    stubs for properties and control panels

---
+++ simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/control/ModelPanel.java
@@ -0,0 +1,11 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.moleculepolarity.control;
+
+/**
+ * "Model" control panel.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
+public class ModelPanel {
+
+}
---
+++ simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/control/ViewPanel.java
@@ -0,0 +1,10 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.moleculepolarity.control;
+
+/**
+ * "View" control panel.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
+public class ViewPanel {
+}
