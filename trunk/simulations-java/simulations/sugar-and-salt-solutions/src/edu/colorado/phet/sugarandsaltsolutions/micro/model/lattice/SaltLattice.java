510a5521a53f664844fbe00661b5199ed8c9ed01
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-07-11 17:28:41 +0000

    factored out square lattice

--- trunk/simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/micro/model/lattice/SaltLattice.java
+++ trunk/simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/micro/model/lattice/SaltLattice.java
@@ -5,14 +5,12 @@ import java.util.ArrayList;
 import edu.colorado.phet.sugarandsaltsolutions.micro.model.ImmutableList;
 import edu.colorado.phet.sugarandsaltsolutions.micro.model.lattice.Component.SodiumIon;
 
-import static edu.colorado.phet.sugarandsaltsolutions.micro.model.lattice.BondType.*;
-
 /**
  * Data structures and algorithms for creating and modeling a salt crystal lattice.  Instances are immutable.
  *
  * @author Sam Reid
  */
-public class SaltLattice extends Lattice {
+public class SaltLattice extends SquareLattice {
 
     public SaltLattice() {
         super( new ImmutableList<Component>( new SodiumIon() ), new ImmutableList<Bond>() );
@@ -20,17 +18,6 @@ public class SaltLattice extends Lattice {
 
     public SaltLattice( ImmutableList<Component> components, ImmutableList<Bond> bonds ) {
         super( components, bonds );
-    }
-
-    //Find the available sites where a new component might be added
-    @Override protected ArrayList<OpenSite> getOpenSites() {
-        ArrayList<OpenSite> openSites = new ArrayList<OpenSite>();
-        for ( Component component : components ) {
-            for ( BondType bondType : new BondType[] { UP, DOWN, LEFT, RIGHT } ) {
-                testAddSite( openSites, component, getBonds( component ), bondType );
-            }
-        }
-        return openSites;
     }
 
     @Override protected void testAddSite( ArrayList<OpenSite> openSites, Component component, ArrayList<Bond> bonds, BondType type ) {
