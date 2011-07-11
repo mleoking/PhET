c5383a3b5239e53349062b8e72b3bad89afa047c
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-07-11 17:34:04 +0000

    add new molecule-polarity simulation

---
+++ trunk/simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/MoleculePolarityApplication.java
@@ -0,0 +1,28 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.moleculepolarity;
+
+import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
+import edu.colorado.phet.common.phetcommon.application.PhetApplicationLauncher;
+import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
+import edu.colorado.phet.common.piccolophet.PhetPCanvas;
+import edu.colorado.phet.common.piccolophet.PiccoloModule;
+import edu.colorado.phet.common.piccolophet.PiccoloPhetApplication;
+
+/**
+ * Mian class for the "Molecule Polarity" application.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
+public class MoleculePolarityApplication extends PiccoloPhetApplication  {
+
+    public MoleculePolarityApplication( PhetApplicationConfig config ) {
+        super( config );
+        addModule( new PiccoloModule( "dummy", new ConstantDtClock( 25 ) ) {{
+            setSimulationPanel( new PhetPCanvas() );
+        }});
+    }
+
+    public static void main( final String[] args ) {
+        new PhetApplicationLauncher().launchSim( args, MPConstants.PROJECT_NAME, MoleculePolarityApplication.class );
+    }
+}
