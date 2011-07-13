383d6e52aab47f52590f46a031151b3ae0413f89
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-07-13 19:00:18 +0000

    create common.control package

--- trunk/simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/common/control/ViewControlPanel.java
+++ trunk/simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/common/control/ViewControlPanel.java
@@ -1,5 +1,5 @@
 // Copyright 2002-2011, University of Colorado
-package edu.colorado.phet.moleculepolarity.control;
+package edu.colorado.phet.moleculepolarity.common.control;
 
 import edu.colorado.phet.common.phetcommon.view.controls.PropertyCheckBox;
 import edu.colorado.phet.common.phetcommon.view.util.GridPanel;
