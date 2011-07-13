5c01767238e0c05ee6e45de7fa07ccb3a71d3953
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-07-13 17:29:15 +0000

    rename package twoatoms to threeatoms

--- trunk/simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/threeatoms/ThreeAtomsCanvas.java
+++ trunk/simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/threeatoms/ThreeAtomsCanvas.java
@@ -1,5 +1,5 @@
 // Copyright 2002-2011, University of Colorado
-package edu.colorado.phet.moleculepolarity.twoatoms;
+package edu.colorado.phet.moleculepolarity.threeatoms;
 
 import java.awt.*;
 
@@ -14,6 +14,6 @@ import edu.colorado.phet.moleculepolarity.common.view.MPCanvas;
 public class ThreeAtomsCanvas extends MPCanvas {
 
     public ThreeAtomsCanvas( Frame parentFrame, Resettable... resettable ) {
-        super( );
+        super();
     }
 }
