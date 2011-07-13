c8499516c1c8269816607bd75a86415fa6642ad0
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-07-13 00:16:50 +0000

    add E-Field model

---
+++ trunk/simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/common/view/NegativeEFieldPlateNode.java
@@ -0,0 +1,10 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.moleculepolarity.common.view;
+
+/**
+ * XXX
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
+public class NegativeEFieldPlateNode {
+}
