d0ecee8f82fad6bc899209d862ae955974430a38
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-07-15 20:48:21 +0000

    Added setup of transforms for the canvas.

--- simulations-java/simulations/balance-and-torque/src/edu/colorado/phet/balanceandtorque/game/view/GameCanvas.java
+++ simulations-java/simulations/balance-and-torque/src/edu/colorado/phet/balanceandtorque/game/view/GameCanvas.java
@@ -2,18 +2,39 @@
 package edu.colorado.phet.balanceandtorque.game.view;
 
 import java.awt.*;
+import java.awt.geom.Dimension2D;
+import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 
 import edu.colorado.phet.balanceandtorque.BalanceAndTorqueResources.Images;
+import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
 import edu.colorado.phet.common.piccolophet.PhetPCanvas;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.umd.cs.piccolo.nodes.PImage;
+import edu.umd.cs.piccolo.util.PDimension;
 
 /**
  * @author John Blanco
  */
 public class GameCanvas extends PhetPCanvas {
+
+    private static Dimension2D STAGE_SIZE = new PDimension( 1008, 679 );
+
     public GameCanvas() {
+        // Set up the canvas-screen transform.
+        setWorldTransformStrategy( new PhetPCanvas.CenteredStage( this, STAGE_SIZE ) );
+
+        // Set up the model-canvas transform.
+        //
+        // IMPORTANT NOTES: The multiplier factors for the 2nd point can be
+        // adjusted to shift the center right or left, and the scale factor
+        // can be adjusted to zoom in or out (smaller numbers zoom out, larger
+        // ones zoom in).
+        ModelViewTransform mvt = ModelViewTransform.createSinglePointScaleInvertedYMapping(
+                new Point2D.Double( 0, 0 ),
+                new Point( (int) Math.round( STAGE_SIZE.getWidth() * 0.5 ), (int) Math.round( STAGE_SIZE.getHeight() * 0.5 ) ),
+                150 ); // "Zoom factor" - smaller zooms out, larger zooms in.
+
         setBackground( Color.BLACK );
         addWorldChild( new PhetPPath( new Rectangle2D.Double( 0, 0, 100, 100 ), Color.PINK ) );
         addWorldChild( new PImage( Images.LEVER_INTO_WHEELBARROW ) );
