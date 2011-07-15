261a76c084cff7b29505a2e1406ae827a5b435cf
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-07-15 18:04:41 +0000

    Micro tab: Nitrate group remains together when sodium nitrate dissolves

---
+++ simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/micro/model/NitrateCrystal.java
@@ -0,0 +1,15 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.sugarandsaltsolutions.micro.model;
+
+import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
+
+/**
+ * This code helps keep the nitrate together when the NaNO3 crystal dissolves
+ *
+ * @author Sam Reid
+ */
+public class NitrateCrystal extends Crystal {
+    public NitrateCrystal( ImmutableVector2D position, double sizeScale ) {
+        super( position, sizeScale );
+    }
+}
