100a05b1fd514d7eb2d60fe904eed551ba15ea92
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-07-18 23:50:41 +0000

    add some doc to jmol-phet Molecule

--- simulations-java/common/jmol-phet/src/edu/colorado/phet/common/jmolphet/Molecule.java
+++ simulations-java/common/jmol-phet/src/edu/colorado/phet/common/jmolphet/Molecule.java
@@ -3,22 +3,47 @@ package edu.colorado.phet.common.jmolphet;
 
 import org.jmol.api.JmolViewer;
 
-//TODO javadoc
+//TODO why is this here? Either rename to JmolMolecule, or move to common/chemistry/src/.../model
 
 /**
+ * Interface for a molecule that is viewable with Jmol, intended for use with JmolPanel and JmolDialog.
+ *
  * @author Sam Reid
  */
 public interface Molecule {
 
-    //TODO javadoc
+    /**
+     * Gets the user-visible name of the molecule.
+     *
+     * @return String
+     */
     String getDisplayName();
 
-    //TODO what is this, and what is it used for?
+    /**
+     * Gets the PubChem Compound ID (CID).
+     * See http://en.wikipedia.org/wiki/PubChem
+     *
+     * @return CID
+     */
     int getCID();
 
-    //TODO what formats can this be in?
+    /**
+     * Gets the molecule description.
+     * Jmol supports a large number of formats, and auto detects the format of the String.
+     * For a list of supported formats, see http://jmol.svn.sourceforge.net/viewvc/jmol/trunk/Jmol-datafiles/
+     *
+     * @return molecule description
+     */
     String getData();
 
-    // TODO what does this do? how is it used?
+    /**
+     * This is intended to be a hook for substituting custom colors for things related
+     * to the molecule. Since you're given a handle to the viewer, you could be abusive
+     * and use this to execute any script.  But you wouldn't do that, would you?
+     * <p/>
+     * TODO provide an example of how to change atom colors
+     *
+     * @param viewer
+     */
     void fixJmolColors( JmolViewer viewer );
 }
