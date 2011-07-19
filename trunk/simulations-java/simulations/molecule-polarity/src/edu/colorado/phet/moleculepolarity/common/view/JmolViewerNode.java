32602eb76390423bfa02b1bb096526d3fd6c6dc4
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-07-19 17:12:12 +0000

    simplify viewer node, remove multi threading, add setters for things that need to be turned on/off

--- trunk/simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/common/view/JmolViewerNode.java
+++ trunk/simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/common/view/JmolViewerNode.java
@@ -4,24 +4,21 @@ package edu.colorado.phet.moleculepolarity.common.view;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.Rectangle;
+import java.text.MessageFormat;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
 import javax.swing.WindowConstants;
 
 import org.jmol.adapter.smarter.SmarterJmolAdapter;
 import org.jmol.api.JmolViewer;
 import org.jmol.util.Logger;
 
-import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.PhetPCanvas;
+import edu.colorado.phet.common.piccolophet.PhetPNode;
 import edu.colorado.phet.moleculepolarity.common.model.Molecule3D;
 import edu.colorado.phet.moleculepolarity.common.model.Molecule3D.Ammonia;
-import edu.umd.cs.piccolo.nodes.PText;
-import edu.umd.cs.piccolo.util.PPaintContext;
 import edu.umd.cs.piccolox.pswing.PSwing;
 
 /**
@@ -29,98 +26,107 @@ import edu.umd.cs.piccolox.pswing.PSwing;
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
-public class JmolViewerNode extends PSwing {
+public class JmolViewerNode extends PhetPNode {
 
-    public JmolViewerNode( Molecule3D molecule ) {
-        super( new ViewerPanel( molecule ) );
+    public static enum MoleculeRepresentation {
+        BALL_AND_STICK, SPACE_FILLED
+    }
+
+    private final ViewerPanel viewerPanel;
+
+    public JmolViewerNode( Molecule3D molecule, Color background, Dimension size ) {
+        viewerPanel = new ViewerPanel( molecule, background, size );
+        addChild( new PSwing( viewerPanel ) );
+        setMoleculeRepresentation( MoleculeRepresentation.BALL_AND_STICK );
+        setBondDipolesVisible( true );
+        setMolecularDipoleVisible( true );
+        setElectrostaticPotentialVisible( true );
     }
 
     //TODO consider merging this with jmol-phet JmolPanel
     // Container for Jmol viewer
     private static class ViewerPanel extends JPanel {
 
-        private JmolViewer viewer = null;
-        private final String loadingString;
+        private final JmolViewer viewer;
 
-        public ViewerPanel(
-                final Molecule3D molecule ) {
+        public ViewerPanel( final Molecule3D molecule, Color background, Dimension size ) {
+            setPreferredSize( size );
 
-            this.loadingString = "Loading Jmol..."; //XXX i18n, and I don't see this rendered
+            // configure Jmol's logging so we don't dump lots of stuff to the console
+            Logger.setLogLevel( Logger.LEVEL_WARN );
 
-            // create the 3D view after we have shown the "loading" text and dialog
-            SwingUtilities.invokeLater( new Runnable() {
-                public void run() {
-                    // don't dump everything out into the console
-                    Logger.setLogLevel( Logger.LEVEL_WARN );
+            // create the 3D viewer
+            viewer = JmolViewer.allocateViewer( ViewerPanel.this, new SmarterJmolAdapter(), null, null, null, "-applet", null );
 
-                    // create the 3D view
-                    JmolViewer viewer = JmolViewer.allocateViewer( ViewerPanel.this, new SmarterJmolAdapter(), null, null, null, "-applet", null );
-                    viewer.setBooleanProperty( "antialiasDisplay", true );
-                    viewer.setBooleanProperty( "autoBond", false );
+            // default settings
+            viewer.setBooleanProperty( "antialiasDisplay", true );
+            viewer.setBooleanProperty( "autoBond", false );
+            doScript( "unbind \"_popupMenu\"" ); // hide the right-click popup menu
+            doScript( "frank off" ); // hide the "Jmol" watermark in the lower-right corner
+            doScript( MessageFormat.format( "background [{0},{1},{2}]", background.getRed(), background.getGreen(), background.getBlue() ) ); //TODO how to make background transparent?
 
-                    String errorString = viewer.openStringInline( molecule.getData() );
-                    if ( errorString != null ) {
-                        throw new RuntimeException( "Jmol problem: " + errorString );
-                    }
-
-                    // set the visible colors to our model colors
-                    molecule.adjustColors( viewer );
-
-                    // store reference to this AFTER we have processed the molecule data, so that the "Loading" text shows up until the molecule is loaded
-                    ViewerPanel.this.viewer = viewer;
-
-                    //TODO reverse engineer page source at http://www.chemtube3d.com/ElectrostaticSurfacesPolar.html
-                    // working...
-                    viewer.script( "wireframe 0.2; spacefill 25%" ); // ball-and-stick view
-                    viewer.script( "unbind \"_popupMenu\"" ); // hide the right-click popup menu
-                    viewer.script( "frank off" ); // hide the "Jmol" watermark in the lower-right corner
-                    viewer.script( "label" ); // label the atoms
-                    viewer.script( "dipole bonds on" ); // show bond dipoles
-                    viewer.script( "dipole molecular on" ); // show molecular dipole
-                    viewer.script( "isosurface resolution 6 solvent map mep translucent" ); // molecular electrostatic potential (MEP)
-//                    viewer.script( "color partialCharge" ); // color atoms based on partial charge
-
-                    // not working...
-                    viewer.script( "background white" );//XXX how to make this transparent?
-
-                    repaint();
-                }
-            } );
-
-            setOpaque( false );
-            setBackground( new Color( 0, 0, 0, 0 ) );
-            setPreferredSize( new Dimension( 400, 400 ) );//XXX how to determine this dynamically?
+            setMolecule( molecule );
         }
 
-        @Override public void paint
-                ( Graphics
-                          g ) {
-            // Jmol's canonical example of embedding in other Java is to override the paint method, so we do it here
+        // Jmol's canonical example of embedding in other Java is to override the paint method, so we do that here.
+        @Override public void paint( Graphics g ) {
+            // copied from Jmol's Integration.java
+            Dimension currentSize = new Dimension();
+            getSize( currentSize ); // stores size in currentSize
+            Rectangle clipBounds = new Rectangle();
+            g.getClipBounds( clipBounds );
+            viewer.renderScreenImage( g, currentSize, clipBounds );
+        }
 
-            if ( viewer == null ) {
-                // if we have no viewer yet, we show the loading text on a black background
-                super.paint( g );
+        public void doScript( String script ) {
+            viewer.script( script );
+        }
 
-                // create a piccolo node (helpful for centering and styling)
-                PText text = new PText( loadingString ) {{
-                    setTextPaint( Color.RED ); //XXX this should depend on the background color
-                    setFont( new PhetFont( 20 ) );
-
-                    // center in the panel
-                    setOffset( ( getWidth() - getFullBounds().getWidth() ) / 2, ( getHeight() - getFullBounds().getHeight() ) / 2 );
-                }};
-
-                // paint the piccolo node onto the panel
-                text.fullPaint( new PPaintContext( (Graphics2D) g ) );
+        public void setMolecule( Molecule3D molecule ) {
+            // load the molecule data
+            String errorString = viewer.openStringInline( molecule.getData() );
+            if ( errorString != null ) {
+                throw new RuntimeException( "Jmol problem: " + errorString ); //TODO improve exception handling
             }
-            else {
-                // copied from Jmol's Integration.java
-                Dimension currentSize = new Dimension();
-                getSize( currentSize ); // stores size in currentSize
-                Rectangle clipBounds = new Rectangle();
-                g.getClipBounds( clipBounds );
-                viewer.renderScreenImage( g, currentSize, clipBounds );
-            }
+            doScript( "label" ); // label the atoms
+            // adjust colors
+            molecule.adjustColors( viewer );
+        }
+    }
+
+    public void setMoleculeRepresentation( MoleculeRepresentation representation ) {
+        if ( representation == MoleculeRepresentation.BALL_AND_STICK ) {
+            viewerPanel.doScript( "wireframe 0.2; spacefill 25%" );
+        }
+        else {
+            viewerPanel.doScript( "wireframe off; spacefill 60%" );
+        }
+    }
+
+    public void setBondDipolesVisible( boolean visible ) {
+        if ( visible ) {
+            viewerPanel.doScript( "dipole bonds on" );
+        }
+        else {
+            viewerPanel.doScript( "dipole bonds off" );
+        }
+    }
+
+    public void setMolecularDipoleVisible( boolean visible ) {
+        if ( visible ) {
+            viewerPanel.doScript( "dipole molecular on" );
+        }
+        else {
+            viewerPanel.doScript( "dipole molecular off" );
+        }
+    }
+
+    public void setElectrostaticPotentialVisible( boolean visible ) {
+        if ( visible ) {
+            viewerPanel.doScript( "isosurface resolution 6 solvent map mep translucent" );
+        }
+        else {
+            viewerPanel.doScript( "isosurface off" );
         }
     }
 
@@ -129,7 +135,7 @@ public class JmolViewerNode extends PSwing {
         final PhetPCanvas canvas = new PhetPCanvas() {{
             setPreferredSize( new Dimension( 1024, 768 ) );
             setBackground( Color.LIGHT_GRAY );
-            JmolViewerNode viewerNode = new JmolViewerNode( new Ammonia() );
+            JmolViewerNode viewerNode = new JmolViewerNode( new Ammonia(), getBackground(), new Dimension( 400, 400 ) );
             getLayer().addChild( viewerNode );
             viewerNode.setOffset( 100, 100 );
         }};
