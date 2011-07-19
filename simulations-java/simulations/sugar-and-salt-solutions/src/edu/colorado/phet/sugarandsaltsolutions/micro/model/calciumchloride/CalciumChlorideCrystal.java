177e9eda1dffedc19f3b52a3b19696b06dee0372
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-07-19 16:44:38 +0000

    Micro tab: Moved oxygen in NO3 closer to the central nitrogen so they will look more bonded

--- simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/micro/model/calciumchloride/CalciumChlorideCrystal.java
+++ simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/micro/model/calciumchloride/CalciumChlorideCrystal.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 
 import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
 import edu.colorado.phet.sugarandsaltsolutions.micro.model.Bond;
+import edu.colorado.phet.sugarandsaltsolutions.micro.model.Component;
 import edu.colorado.phet.sugarandsaltsolutions.micro.model.Crystal;
 import edu.colorado.phet.sugarandsaltsolutions.micro.model.LatticeConstituent;
 import edu.colorado.phet.sugarandsaltsolutions.micro.model.SphericalParticle.CalciumIonParticle;
@@ -20,14 +21,14 @@ public class CalciumChlorideCrystal extends Crystal {
         super( position );
 
         //Recursive method to traverse the graph and create particles
-        fill( lattice, lattice.components.getFirst(), new ArrayList<edu.colorado.phet.sugarandsaltsolutions.micro.model.Component>(), new ImmutableVector2D() );
+        fill( lattice, lattice.components.getFirst(), new ArrayList<Component>(), new ImmutableVector2D() );
 
         //Update positions so the lattice position overwrites constituent particle positions
         stepInTime( new ImmutableVector2D(), 0.0 );
     }
 
     //Recursive method to traverse the graph and create particles
-    private void fill( CalciumChlorideLattice lattice, edu.colorado.phet.sugarandsaltsolutions.micro.model.Component component, ArrayList<edu.colorado.phet.sugarandsaltsolutions.micro.model.Component> handled, ImmutableVector2D relativePosition ) {
+    private void fill( CalciumChlorideLattice lattice, Component component, ArrayList<edu.colorado.phet.sugarandsaltsolutions.micro.model.Component> handled, ImmutableVector2D relativePosition ) {
         //Calcium
         if ( component instanceof edu.colorado.phet.sugarandsaltsolutions.micro.model.Component.CalciumIon ) {
             latticeConstituents.add( new LatticeConstituent( new CalciumIonParticle(), relativePosition ) );
