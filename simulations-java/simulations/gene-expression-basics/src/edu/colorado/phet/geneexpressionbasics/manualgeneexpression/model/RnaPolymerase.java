0aec8a5c92a33c11b2ed935e3ab416d24ed04870
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-07-20 18:59:13 +0000

    Improved the shape - looks a bit more like the spec now, though is not done yet.

--- simulations-java/simulations/gene-expression-basics/src/edu/colorado/phet/geneexpressionbasics/manualgeneexpression/model/RnaPolymerase.java
+++ simulations-java/simulations/gene-expression-basics/src/edu/colorado/phet/geneexpressionbasics/manualgeneexpression/model/RnaPolymerase.java
@@ -3,7 +3,8 @@ package edu.colorado.phet.geneexpressionbasics.manualgeneexpression.model;
 
 import java.awt.Color;
 import java.awt.Paint;
-import java.awt.geom.Ellipse2D;
+import java.awt.Shape;
+import java.awt.geom.GeneralPath;
 import java.awt.geom.Point2D;
 
 import edu.colorado.phet.geneexpressionbasics.common.model.MobileBiomolecule;
@@ -17,11 +18,25 @@ public class RnaPolymerase extends MobileBiomolecule {
     private static final double HEIGHT = 150;
 
     public RnaPolymerase( Point2D position ) {
-        super( new Ellipse2D.Double( position.getX() - WIDTH / 2, position.getY() - HEIGHT / 2, WIDTH, HEIGHT ), Color.GREEN );
+        super( createShape(), new Color( 34, 139, 34 ) );
         setPosition( position );
     }
 
     public Paint getPaint() {
         return new Color( 0, 200, 40 );
+    }
+
+    private static Shape createShape() {
+        GeneralPath path = new GeneralPath();
+        // Start from left side, vertical center.
+        path.moveTo( -WIDTH / 2, 0 );
+        path.lineTo( -WIDTH * 0.2, HEIGHT * 0.5 );
+        path.lineTo( WIDTH * 0.4, HEIGHT * 0.4 );
+        path.lineTo( WIDTH / 2, 0 );
+        path.lineTo( WIDTH * 0.4, -HEIGHT * 0.5 );
+        path.lineTo( -WIDTH * 0.3, -HEIGHT * 0.5 );
+        path.lineTo( -WIDTH * 0.4, -HEIGHT * 0.2 );
+        path.closePath();
+        return path;
     }
 }
