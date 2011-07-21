a823ccd5a56b368855db20808b67183bd91bc9d2
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-07-21 04:11:43 +0000

    Micro tab: Made oxygen atoms in sugar molecule charge color neutral Micro tab: Changed color for neutral from gray to yellow

--- simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/micro/model/SphericalParticle.java
+++ simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/micro/model/SphericalParticle.java
@@ -20,8 +20,13 @@ public class SphericalParticle extends Particle {
     //Color corresponding to the identity of the atom
     public final Color color;
 
-    //Color for the charge of the atom, red = positive, gray = neutral, blue = negative
+    //Color for the charge of the atom, red = positive, yellow = neutral, blue = negative
     public Color chargeColor;
+
+    //Color to use for neutrally charged objects
+    private static final Color NEUTRAL_COLOR = Color.yellow;
+    private static final Color POSITIVE_COLOR = Color.red;
+    private static final Color NEGATIVE_COLOR = Color.blue;
 
     //This constructor matches the table given in the design doc and to-do doc,
     public SphericalParticle( double radiusInPM, Color chargeColor, Color atomColor ) {
@@ -42,44 +47,60 @@ public class SphericalParticle extends Particle {
     //These classes contains state information for particulars particles and ions and permit matching in MicroModel for particle counting.
     public static class HydrogenIonParticle extends SphericalParticle {
         public HydrogenIonParticle() {
-            super( 37, gray, white );
+            super( 37, SphericalParticle.NEUTRAL_COLOR, white );
         }
     }
 
     public static class CarbonIonParticle extends SphericalParticle {
         public CarbonIonParticle() {
-            super( 77, gray, gray );
+            super( 77, SphericalParticle.NEUTRAL_COLOR, gray );
         }
     }
 
     public static class NitrogenIonParticle extends SphericalParticle {
         public NitrogenIonParticle() {
-            super( 75, blue, blue );
+            super( 75, SphericalParticle.NEGATIVE_COLOR, blue );
         }
     }
 
-    public static class OxygenIonParticle extends SphericalParticle {
-        public OxygenIonParticle() {
-            super( 73, blue, red );
+    //Abstract since oxygen ions and oxygen in sucrose must have different colors
+    public abstract static class OxygenIonParticle extends SphericalParticle {
+        public OxygenIonParticle( Color chargeColor ) {
+            super( 73, chargeColor, red );
         }
     }
+
+    //Free oxygen atoms have a negative charge
+    public static class FreeOxygenIonParticle extends OxygenIonParticle {
+        public FreeOxygenIonParticle() {
+            super( Color.blue );
+        }
+    }
+
+    //When participating in sucrose, oxygen atoms are neutral
+    public static class SucroseOxygenParticle extends OxygenIonParticle {
+        public SucroseOxygenParticle() {
+            super( SphericalParticle.NEUTRAL_COLOR );
+        }
+    }
+
 
     public static class SodiumIonParticle extends SphericalParticle {
         public SodiumIonParticle() {
-            super( 102, red, magenta );
+            super( 102, SphericalParticle.POSITIVE_COLOR, magenta );
         }
     }
 
     public static class ChlorideIonParticle extends SphericalParticle {
         public ChlorideIonParticle() {
-            super( 181, blue, green );
+            super( 181, SphericalParticle.NEGATIVE_COLOR, green );
         }
     }
 
     public static class CalciumIonParticle extends SphericalParticle {
         public CalciumIonParticle() {
             //Calcium should be a dark green
-            super( 100, red, new Color( 6, 98, 23 ) );
+            super( 100, SphericalParticle.POSITIVE_COLOR, new Color( 6, 98, 23 ) );
         }
     }
 }
