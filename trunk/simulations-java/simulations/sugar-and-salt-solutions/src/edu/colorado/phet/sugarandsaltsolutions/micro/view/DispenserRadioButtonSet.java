e72ced5f4006859b0de2a37eb686ac66b223f6c5
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-07-22 00:49:23 +0000

    Micro tab: When changing kits, switch to a new dispenser (unless dispenser exists in new kit)

--- trunk/simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/micro/view/DispenserRadioButtonSet.java
+++ trunk/simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/micro/view/DispenserRadioButtonSet.java
@@ -15,11 +15,34 @@ import static edu.colorado.phet.sugarandsaltsolutions.common.view.SugarAndSaltSo
  * @author Sam Reid
  */
 public class DispenserRadioButtonSet extends PSwing {
+    private Property<DispenserType> dispenserType;
+    private Item[] items;
+
     public DispenserRadioButtonSet( final Property<DispenserType> dispenserType, final Item... items ) {
         super( new VerticalLayoutPanel() {{
             for ( Item item : items ) {
                 add( new PropertyRadioButton<DispenserType>( item.name, dispenserType, item.dispenserType ) {{setFont( CONTROL_FONT );}} );
             }
         }} );
+        this.dispenserType = dispenserType;
+        this.items = items;
+    }
+
+    //When switching to a new kit, switch to a dispenser that is in the set (if not already selecting it).  If switching from a set that contains NaCl to a new set that also contains NaCl, then keep the selection
+    public void setSelected() {
+        if ( !containsDispenser() ) {
+            dispenserType.set( items[0].dispenserType );
+        }
+    }
+
+    //Determine whether the currently selected dispenser is one of the choices in this kit
+    private boolean containsDispenser() {
+        boolean isDispenserAvailable = false;
+        for ( Item item : items ) {
+            if ( item.dispenserType == dispenserType.get() ) {
+                isDispenserAvailable = true;
+            }
+        }
+        return isDispenserAvailable;
     }
 }
