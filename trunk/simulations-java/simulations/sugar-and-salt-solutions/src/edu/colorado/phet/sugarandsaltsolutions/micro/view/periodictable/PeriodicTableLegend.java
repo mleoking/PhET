3b8a1d4561d403626160d37f62796735fa97be21
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-07-23 00:43:55 +0000

    Micro tab: In the periodic table, use color squares that are the same size as the element squares

--- trunk/simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/micro/view/periodictable/PeriodicTableLegend.java
+++ trunk/simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/micro/view/periodictable/PeriodicTableLegend.java
@@ -1,5 +1,5 @@
 // Copyright 2002-2011, University of Colorado
-package edu.colorado.phet.sugarandsaltsolutions.micro.view;
+package edu.colorado.phet.sugarandsaltsolutions.micro.view.periodictable;
 
 import java.awt.BasicStroke;
 import java.awt.geom.Rectangle2D;
@@ -9,6 +9,7 @@ import edu.colorado.phet.common.piccolophet.nodes.layout.HBox;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.nodes.PText;
 
+import static edu.colorado.phet.common.piccolophet.nodes.periodictable.PeriodicTableNode.CELL_DIMENSION;
 import static edu.colorado.phet.sugarandsaltsolutions.common.view.SugarAndSaltSolutionsCanvas.CONTROL_FONT;
 import static java.awt.Color.*;
 
@@ -20,8 +21,10 @@ import static java.awt.Color.*;
 public class PeriodicTableLegend extends PNode {
 
     //Create a table legend based on the width of the periodic table
-    public PeriodicTableLegend( final double periodicTableWidth ) {
-        Rectangle2D.Double box = new Rectangle2D.Double( 0, 0, 100, 50 );
+    public PeriodicTableLegend( final double periodicTableWidth, double scale ) {
+
+        //use color squares that are the same size as the element squares
+        Rectangle2D.Double box = new Rectangle2D.Double( 0, 0, CELL_DIMENSION * scale, CELL_DIMENSION * scale );
 
         //Create a box in the bottom left for gray metals
         final HBox metalBox = new HBox( new PhetPPath( box, lightGray, new BasicStroke( 1 ), black ), new PText( "Metal" ) {{
