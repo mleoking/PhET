92d2d419f7af32e705d780d6caad66a7b513b664
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-07-24 15:13:01 +0000

    Refactored MicroModel to dynamically dispatch crystal dissolve calls instead of instanceof checking

--- trunk/simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/micro/model/Crystal.java
+++ trunk/simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/micro/model/Crystal.java
@@ -1,6 +1,11 @@
 package edu.colorado.phet.sugarandsaltsolutions.micro.model;
 
+import java.util.ArrayList;
+
 import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
+
+import static java.lang.Math.PI;
+import static java.lang.Math.random;
 
 /**
  * A crystal represents 0 or more (usually 1 or more) constituents which can be put into solution.  It is constructed from a lattice.
@@ -10,5 +15,15 @@ import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
 public class Crystal extends Compound {
     public Crystal( ImmutableVector2D position ) {
         super( position );
+    }
+
+    //Splits up all constituents from the crystal lattice, returning the components (particles or molecules) that should move about freely
+    public ArrayList<? extends Particle> dissolve() {
+        ArrayList<Particle> freeParticles = new ArrayList<Particle>();
+        for ( Constituent constituent : this ) {
+            constituent.particle.velocity.set( velocity.get().getRotatedInstance( random() * PI * 2 ) );
+            freeParticles.add( constituent.particle );
+        }
+        return freeParticles;
     }
 }
