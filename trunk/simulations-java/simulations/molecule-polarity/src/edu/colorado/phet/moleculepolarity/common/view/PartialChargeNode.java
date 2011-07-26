8c23f2365a3c579a502d7f081152a5205040516a
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-07-26 19:15:30 +0000

    initial step toward positioning partial charges

--- trunk/simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/common/view/PartialChargeNode.java
+++ trunk/simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/common/view/PartialChargeNode.java
@@ -3,9 +3,10 @@ package edu.colorado.phet.moleculepolarity.common.view;
 
 import java.awt.Color;
 
-import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
+import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.moleculepolarity.MPConstants;
+import edu.colorado.phet.moleculepolarity.common.model.Atom;
 import edu.colorado.phet.moleculepolarity.common.model.Bond;
 import edu.umd.cs.piccolo.nodes.PText;
 import edu.umd.cs.piccolox.nodes.PComposite;
@@ -22,7 +23,7 @@ public class PartialChargeNode extends PComposite {
     private static final double REF_MAGNITUDE = MPConstants.ELECTRONEGATIVITY_RANGE.getLength();
     private static final double REF_SCALE = 1;
 
-    public PartialChargeNode( Bond bond, final boolean positivePolarity ) {
+    public PartialChargeNode( final Bond bond, final Atom atom, final Atom relatedAtom, final boolean positivePolarity ) {
 
         final PText textNode = new PText() {{
             setFont( new PhetFont( 40 ) );
@@ -30,8 +31,10 @@ public class PartialChargeNode extends PComposite {
         }};
         addChild( textNode );
 
-        bond.dipoleMagnitude.addObserver( new VoidFunction1<Double>() {
-            public void apply( Double magnitude ) {
+        SimpleObserver updater = new SimpleObserver() {
+            public void update() {
+
+                final double magnitude = bond.dipoleMagnitude.get();
 
                 textNode.setVisible( magnitude != 0 ); // invisible if dipole is zero
 
@@ -52,8 +55,15 @@ public class PartialChargeNode extends PComposite {
                         textNode.setScale( scale );
                         textNode.setOffset( -textNode.getFullBoundsReference().getWidth() / 2, -textNode.getFullBoundsReference().getHeight() / 2 ); // origin at center
                     }
+
+                    //TODO set offset so that symbol is on bond axis, just past atom
                 }
             }
-        } );
+        };
+        bond.dipoleMagnitude.addObserver( updater );
+        atom.location.addObserver( updater );
+        relatedAtom.location.addObserver( updater );
+
+
     }
 }
