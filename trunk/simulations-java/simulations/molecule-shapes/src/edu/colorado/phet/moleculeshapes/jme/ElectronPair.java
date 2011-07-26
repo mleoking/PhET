dfabc66a6563c8fd7eeffd048f1d69828568c70a
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-07-26 05:13:54 +0000

    ms: model experiments progress

--- trunk/simulations-java/simulations/molecule-shapes/src/edu/colorado/phet/moleculeshapes/jme/ElectronPair.java
+++ trunk/simulations-java/simulations/molecule-shapes/src/edu/colorado/phet/moleculeshapes/jme/ElectronPair.java
@@ -37,10 +37,21 @@ public class ElectronPair {
         position.set( position.get().plus( directionToCenter.times( 0.02 * offset ) ) );
     }
 
+    public double getRepulsionFactor() {
+        return isLonePair ? 1.0 : 1.0; // change first number to add in higher lone-pair repulsion
+    }
+
     public void repulseFrom( ElectronPair other, double timeElapsed ) {
         // only handle the force on this object for now
+
+        // from other => this
         ImmutableVector3D delta = position.get().minus( other.position.get() );
-        ImmutableVector3D velocityDelta = delta.normalized().times( timeElapsed * ELECTRON_PAIR_REPULSION_SCALE / ( delta.magnitude() * delta.magnitude() ) );
+
+        // a factor that causes lone pairs to have more repulsion
+        double repulsionFactor = getRepulsionFactor() * other.getRepulsionFactor();
+
+        // mimic Coulomb's Law
+        ImmutableVector3D velocityDelta = delta.normalized().times( timeElapsed * ELECTRON_PAIR_REPULSION_SCALE * repulsionFactor / ( delta.magnitude() * delta.magnitude() ) );
         velocity.set( velocity.get().plus( velocityDelta ) );
     }
 
