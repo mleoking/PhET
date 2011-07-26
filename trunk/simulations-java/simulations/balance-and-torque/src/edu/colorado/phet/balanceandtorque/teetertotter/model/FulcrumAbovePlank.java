64512cf75126b0cac009a6b92d081fa0ec4d538e
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-07-26 18:23:03 +0000

    Improved the V support shape for fulcrum above plank shape

--- trunk/simulations-java/simulations/balance-and-torque/src/edu/colorado/phet/balanceandtorque/teetertotter/model/FulcrumAbovePlank.java
+++ trunk/simulations-java/simulations/balance-and-torque/src/edu/colorado/phet/balanceandtorque/teetertotter/model/FulcrumAbovePlank.java
@@ -1,7 +1,10 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.balanceandtorque.teetertotter.model;
 
+import java.awt.BasicStroke;
 import java.awt.Shape;
+import java.awt.geom.Area;
+import java.awt.geom.Rectangle2D;
 
 import edu.colorado.phet.common.phetcommon.view.util.DoubleGeneralPath;
 
@@ -14,7 +17,7 @@ import edu.colorado.phet.common.phetcommon.view.util.DoubleGeneralPath;
  */
 public class FulcrumAbovePlank extends ShapeModelElement {
     //Distance from the base of the fulcrum to its top
-    private static final double LEG_THICKNESS_FACTOR = 0.1;
+    private static final double LEG_THICKNESS_FACTOR = 0.08;
 
     private final double height;
 
@@ -27,16 +30,20 @@ public class FulcrumAbovePlank extends ShapeModelElement {
         return height;
     }
 
-    private static final Shape generateShape( double width, double height ) {
-        double legThickness = LEG_THICKNESS_FACTOR * width;
-        DoubleGeneralPath path = new DoubleGeneralPath();
-        path.moveTo( -width / 2, 0 );
-        path.lineTo( 0, height );
-        path.lineTo( width / 2, 0 );
-        path.lineTo( width / 2 - legThickness, 0 );
-        path.lineTo( 0, ( width - 2 * legThickness ) / width * height );
-        path.lineTo( -width / 2 + legThickness, 0 );
-        path.closePath();
-        return path.getGeneralPath();
+    private static Shape generateShape( final double width, final double height ) {
+        final double legThickness = LEG_THICKNESS_FACTOR * width;
+
+        //Create a V shape with a stroke of the legThickness
+        Shape shape = new BasicStroke( (float) legThickness ).createStrokedShape( new DoubleGeneralPath( -width / 2, 0 ) {{
+            lineTo( 0, height + legThickness );
+            lineTo( width / 2, 0 );
+        }}.getGeneralPath() );
+
+        //Subtract out the part where the earth meets the base of the support, so that it looks flush with the flat earth surface
+        //Also, subtract a clip from the top so the V isn't pointy
+        return new Area( shape ) {{
+            subtract( new Area( new Rectangle2D.Double( -width, -legThickness, width * 2, legThickness ) ) );
+            subtract( new Area( new Rectangle2D.Double( -width, height + legThickness / 2, width * 2, height ) ) );
+        }};
     }
 }
