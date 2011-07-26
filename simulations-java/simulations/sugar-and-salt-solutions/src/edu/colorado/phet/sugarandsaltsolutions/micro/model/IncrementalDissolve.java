0ca42a1475fd785681bebe02c050da9a859b688a
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-07-26 14:57:00 +0000

    Micro tab: Improvements for crystal dissolving

--- simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/micro/model/IncrementalDissolve.java
+++ simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/micro/model/IncrementalDissolve.java
@@ -10,17 +10,24 @@ import edu.colorado.phet.common.phetcommon.model.property.ObservableProperty;
  */
 public class IncrementalDissolve implements DissolveStrategy {
 
+    //Debugging tool to visualize the dissolving process
+    long lastDissolve = System.currentTimeMillis();
+
     //Dissolve the lattice incrementally so that we get as close as possible to the saturation point
     public void dissolve( ItemList<? extends Crystal> crystals, final Crystal crystal, ItemList<Particle> freeParticles, ObservableProperty<Boolean> unsaturated ) {
 
-        while ( unsaturated.get() && crystal.numberConstituents() > 0 ) {
+        while ( unsaturated.get() && crystal.numberConstituents() > 0
+//                && System.currentTimeMillis() - lastDissolve > 1000
+                ) {
 
-            Constituent constituent = crystal.getConstituent( 0 );
+            Constituent constituent = crystal.getConstituentToDissolve();
             crystal.dissolve( constituent );
 
             freeParticles.add( constituent.particle );
 
             crystal.removeConstituent( constituent );
+
+            lastDissolve = System.currentTimeMillis();
         }
 
         //Remove the crystal from the list so it will no longer keep its constituents together
