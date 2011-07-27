ca98b35026433216930328f8676c634798bdb559
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-07-27 23:37:19 +0000

    Jmol: order molecules in combo box by increasing size/complexity

--- simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/realmolecules/RealMoleculesModel.java
+++ simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/realmolecules/RealMoleculesModel.java
@@ -20,17 +20,17 @@ public class RealMoleculesModel extends MPModel {
     //TODO i18n of molecule names
     public RealMoleculesModel() {
         molecules = new ArrayList<Molecule3D>() {{
+            add( new Molecule3D( "H2", "hydrogen", "jmol/X-2D-hydrogen.sdf" ) );
+            add( new Molecule3D( "HF", "hydrogen fluoride", "jmol/hydrogen-fluoride.sdf" ) );
+            add( new Molecule3D( "H2O", "water", "jmol/water.sdf" ) );
+            add( new Molecule3D( "CO2", "carbon dioxide", "jmol/carbon-dioxide.sdf" ) );
+            add( new Molecule3D( "HCN", "hydrogen cyanide", "jmol/hydrogen-cyanide.sdf" ) );
             add( new Molecule3D( "NH3", "ammonia", "jmol/ammonia.sdf" ) );
             add( new Molecule3D( "BF3", "boron trifluoride", "jmol/X-2D-boron-trifluoride.sdf" ) );
-            add( new Molecule3D( "CO2", "carbon dioxide", "jmol/carbon-dioxide.sdf" ) );
-            add( new Molecule3D( "CHCl3", "chloroform", "jmol/chloroform.sdf" ) );
-            add( new Molecule3D( "CH3F", "fluoromethane", "jmol/fluoromethane.sdf" ) );
             add( new Molecule3D( "CH2O", "formaldehyde", "jmol/formaldehyde.sdf" ) );
-            add( new Molecule3D( "H2", "hydrogen", "jmol/X-2D-hydrogen.sdf" ) );
-            add( new Molecule3D( "HCN", "hydrogen cyanide", "jmol/hydrogen-cyanide.sdf" ) );
-            add( new Molecule3D( "HF", "hydrogen fluoride", "jmol/hydrogen-fluoride.sdf" ) );
             add( new Molecule3D( "CH4", "methane", "jmol/X-3D-methane.sdf" ) );
-            add( new Molecule3D( "H2O", "water", "jmol/water.sdf" ) );
+            add( new Molecule3D( "CH3F", "fluoromethane", "jmol/fluoromethane.sdf" ) );
+            add( new Molecule3D( "CHCl3", "chloroform", "jmol/chloroform.sdf" ) );
             if ( PhetApplication.getInstance().isDeveloperControlsEnabled() ) {
                 add( new ImportMolecule() );
             }
