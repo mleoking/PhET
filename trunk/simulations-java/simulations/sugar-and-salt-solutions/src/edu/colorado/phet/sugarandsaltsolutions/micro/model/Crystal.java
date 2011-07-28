92abfc273259505f26e379b8efd6ead438ddf05b
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-07-28 04:17:40 +0000

    Micro tab: Improved search for lattice binding sites

--- trunk/simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/micro/model/Crystal.java
+++ trunk/simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/micro/model/Crystal.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 
 import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
 
+import static edu.colorado.phet.common.phetcommon.math.ImmutableVector2D.ZERO;
+
 /**
  * Marker class to signify which compounds are crystals vs noncrystals.
  *
@@ -11,8 +13,11 @@ import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
  */
 public class Crystal<T extends Particle> extends Compound<T> {
 
-    private double spacing;
-    public Lattice<T> lattice;
+    //The spacing between components on the lattice
+    private final double spacing;
+
+    //The lattice, which determines the graph layout (topology) of the crystal
+    public final Lattice<T> lattice;
 
     //Construct the compound from the specified lattice
     public Crystal( ImmutableVector2D position, double spacing, Lattice<T> lattice ) {
@@ -20,18 +25,15 @@ public class Crystal<T extends Particle> extends Compound<T> {
         this.spacing = spacing;
         this.lattice = lattice;
 
-//        for ( Component c : lattice.components ) {
-//            constituents.add( new Constituent<T>( c.constructLatticeConstituent( angle ), ) )
-//        }
-
         //Recursive method to traverse the graph and create particles
-        fill( lattice.components.getFirst(), new ArrayList<T>(), new ImmutableVector2D(), spacing );
+        fill( lattice.components.getFirst(), new ArrayList<T>(), ZERO, spacing );
 
         //Update positions so the lattice position overwrites constituent particle positions
-        stepInTime( new ImmutableVector2D(), 0.0 );
+        stepInTime( ZERO, 0.0 );
     }
 
     //Recursive method to traverse the graph and create particles at the right locations
+    //TODO: Get rid of spacing argument, it is a field now
     private void fill( T component, ArrayList<T> handled, ImmutableVector2D relativePosition, double spacing ) {
 
         //Create and add sucrose molecules in the right relative locations
@@ -46,6 +48,24 @@ public class Crystal<T extends Particle> extends Compound<T> {
                 fill( bond.destination, handled, relativePosition.plus( getDelta( spacing, bond ).getRotatedInstance( angle ) ), spacing );
             }
         }
+    }
+
+    //Find the location of the specified component by recursively searching over the graph
+    private ImmutableVector2D findLocation( T component, ArrayList<T> visited, ImmutableVector2D relativePosition, T seekComponent ) {
+        if ( component == seekComponent ) {
+            return relativePosition;
+        }
+        visited.add( component );
+        ArrayList<Bond<T>> bonds = lattice.getBonds( component );
+        for ( Bond<T> bond : bonds ) {
+            if ( !visited.contains( bond.destination ) ) {
+                ImmutableVector2D location = findLocation( bond.destination, visited, relativePosition.plus( getDelta( spacing, bond ).getRotatedInstance( angle ) ), seekComponent );
+                if ( location != null ) {
+                    return location;
+                }
+            }
+        }
+        return null;
     }
 
     //Determine a direction to move based on the bond type
@@ -69,8 +89,7 @@ public class Crystal<T extends Particle> extends Compound<T> {
 
     //Convert a lattice site to a crystal site so a real particle can connect to the crystal
     private CrystalSite toCrystalSite( LatticeSite<T> site ) {
-
-        //TODO: implement
-        return new CrystalSite( null, null );
+        ImmutableVector2D location = findLocation( lattice.components.getFirst(), new ArrayList<T>(), ZERO, site.component );
+        return new CrystalSite( location, site.component.getClass() );
     }
 }
