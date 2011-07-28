add2219af2243953a05ba403de81e0e89874d80f
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-07-28 23:07:57 +0000

    Added separate rectangles for the transcribed and regulatory regions of the gene, separated out a GeneNode class, added tables to the gene's regions.

--- trunk/simulations-java/simulations/gene-expression-basics/src/edu/colorado/phet/geneexpressionbasics/manualgeneexpression/model/Gene.java
+++ trunk/simulations-java/simulations/gene-expression-basics/src/edu/colorado/phet/geneexpressionbasics/manualgeneexpression/model/Gene.java
@@ -1,13 +1,18 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.geneexpressionbasics.manualgeneexpression.model;
 
-import java.awt.*;
+import java.awt.Paint;
 import java.awt.geom.Rectangle2D;
 
 /**
+ * This class is the model representation of a gene on a DNA molecule.  Each
+ * gene consists of a regulatory region and a transcribed region.
+ *
+ * @author John Blanco
  * @author Sam Reid
  */
 public class Gene {
+    private static final double REG_REGION_PROPORTION = 0.25;
     private Rectangle2D.Double rect;
     private Paint color;
 
@@ -17,7 +22,13 @@ public class Gene {
     }
 
     public Rectangle2D.Double getRect() {
-        return rect;
+        double width = ( 1 - REG_REGION_PROPORTION ) * rect.getWidth();
+        return new Rectangle2D.Double( rect.getMaxX() - width, rect.getY(), width, rect.getHeight() );
+    }
+
+    public Rectangle2D.Double getRegulatoryRegionRect() {
+        double width = REG_REGION_PROPORTION * rect.getWidth();
+        return new Rectangle2D.Double( rect.getX(), rect.getY(), width, rect.getHeight() );
     }
 
     public Paint getColor() {
---
+++ trunk/simulations-java/simulations/gene-expression-basics/src/edu/colorado/phet/geneexpressionbasics/manualgeneexpression/view/GeneNode.java
@@ -0,0 +1,36 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.geneexpressionbasics.manualgeneexpression.view;
+
+import java.awt.Color;
+import java.awt.Font;
+
+import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
+import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
+import edu.colorado.phet.common.piccolophet.nodes.HTMLNode;
+import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
+import edu.colorado.phet.geneexpressionbasics.manualgeneexpression.model.Gene;
+import edu.umd.cs.piccolo.PNode;
+
+/**
+ * Base class for displaying and interacting with mobile biomolecules.  In
+ * essence, this observes the shape of the biomolecule, which changes as it
+ * moves.
+ */
+public class GeneNode extends PNode {
+    private static final Font LABEL_FONT = new PhetFont( 11 );
+
+    public GeneNode( final ModelViewTransform mvt, final Gene gene ) {
+        addChild( new PhetPPath( mvt.modelToView( gene.getRect() ), gene.getColor() ) );
+        addChild( new PhetPPath( mvt.modelToView( gene.getRegulatoryRegionRect() ), new Color( 30, 144, 255 ) ) );
+        addChild( new HTMLNode( "<center>Regulatory<br>Region</center>" ) {{
+            setFont( LABEL_FONT );
+            setOffset( mvt.modelToViewX( gene.getRegulatoryRegionRect().getCenterX() ) - getFullBoundsReference().width / 2,
+                       mvt.modelToViewY( gene.getRegulatoryRegionRect().getMinY() ) );
+        }} );
+        addChild( new HTMLNode( "<center>Transcribed<br>Region</center>" ) {{
+            setFont( LABEL_FONT );
+            setOffset( mvt.modelToViewX( gene.getRect().getCenterX() ) - getFullBoundsReference().width / 2,
+                       mvt.modelToViewY( gene.getRect().getMinY() ) );
+        }} );
+    }
+}
