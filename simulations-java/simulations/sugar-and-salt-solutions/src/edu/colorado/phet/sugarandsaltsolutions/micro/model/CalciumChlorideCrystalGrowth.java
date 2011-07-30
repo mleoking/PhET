853fbea9feb73bdbd86fab1eac4bb7318dbc5bee
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-07-30 18:23:04 +0000

    Added incremental crystallization for calcium chloride

---
+++ simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/micro/model/CalciumChlorideCrystalGrowth.java
@@ -0,0 +1,33 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.sugarandsaltsolutions.micro.model;
+
+import edu.colorado.phet.common.phetcommon.util.Option;
+import edu.colorado.phet.common.phetcommon.util.Option.None;
+import edu.colorado.phet.common.phetcommon.util.Option.Some;
+import edu.colorado.phet.sugarandsaltsolutions.micro.model.SphericalParticle.Calcium;
+import edu.colorado.phet.sugarandsaltsolutions.micro.model.SphericalParticle.Chloride;
+import edu.colorado.phet.sugarandsaltsolutions.micro.model.calciumchloride.CalciumChlorideCrystal;
+
+import static edu.colorado.phet.sugarandsaltsolutions.micro.model.RandomUtil.randomAngle;
+
+/**
+ * Provides growth for calcium chloride crystals.  Works with IncrementalGrowth by giving it specific information about seeding and creating calcium chloride crystals
+ *
+ * @author Sam Reid
+ */
+public class CalciumChlorideCrystalGrowth extends IncrementalGrowth<SphericalParticle, CalciumChlorideCrystal> {
+    public CalciumChlorideCrystalGrowth( MicroModel model, ItemList<CalciumChlorideCrystal> crystals ) {
+        super( model, crystals );
+    }
+
+    @Override protected CalciumChlorideCrystal toCrystal( SphericalParticle particle ) {
+        return new CalciumChlorideCrystal( particle.getPosition(), randomAngle() );
+    }
+
+    //Randomly choose any free sodium or chloride ion to seed the crystal
+    protected Option<SphericalParticle> selectSeed() {
+        Option<Particle> particleOption = model.freeParticles.selectRandom( Calcium.class, Chloride.class );
+        if ( particleOption.isNone() ) { return new None<SphericalParticle>(); }
+        else { return new Some<SphericalParticle>( (SphericalParticle) particleOption.get() ); }
+    }
+}
