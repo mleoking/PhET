4baaf3e57f527b5202ff46c7e0230b7562cfaf19
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-07-30 18:02:34 +0000

    Micro tab: Generalized crystal growth algorithm to make it easier to add support for other solutes

--- trunk/simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/micro/model/IncrementalGrowth.java
+++ trunk/simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/micro/model/IncrementalGrowth.java
@@ -8,53 +8,52 @@ import java.util.Comparator;
 import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
 import edu.colorado.phet.common.phetcommon.model.property.ObservableProperty;
 import edu.colorado.phet.common.phetcommon.util.Option;
-import edu.colorado.phet.sugarandsaltsolutions.micro.model.SphericalParticle.Chloride;
-import edu.colorado.phet.sugarandsaltsolutions.micro.model.SphericalParticle.Sodium;
-import edu.colorado.phet.sugarandsaltsolutions.micro.model.sodiumchloride.SodiumChlorideCrystal;
 
 import static edu.colorado.phet.sugarandsaltsolutions.micro.model.MicroModel.FREE_PARTICLE_SPEED;
-import static edu.colorado.phet.sugarandsaltsolutions.micro.model.RandomUtil.randomAngle;
 
 /**
  * This class handles incremental crystallization of particles when the concentration surpasses the saturation point.
+ * I originally tried just specifying the "T extends particle" generic type and using Crystal
  *
  * @author Sam Reid
  */
-public class IncrementalGrowth {
+public abstract class IncrementalGrowth<T extends Particle, U extends Crystal<T>> {
 
     //Keep track of the last time a crystal was formed so that they can be created gradually instead of all at once
-    private double lastNaClCrystallizationTime;
-    private final MicroModel model;
+    private double lastNewCrystalFormationTime;
+    protected final MicroModel model;
+    private final ItemList<U> crystals;
 
-    public IncrementalGrowth( MicroModel model ) {
+    public IncrementalGrowth( MicroModel model, ItemList<U> crystals ) {
         this.model = model;
+        this.crystals = crystals;
     }
 
     //Check to see whether it is time to create or add to existing crystals, if the solution is over saturated
-    public void formNaClCrystals( double dt, ObservableProperty<Boolean> sodiumChlorideUnsaturated ) {
-        double timeSinceLast = model.getTime() - lastNaClCrystallizationTime;
+    public void formNaClCrystals( double dt, ObservableProperty<Boolean> unsaturated ) {
+        double timeSinceLast = model.getTime() - lastNewCrystalFormationTime;
 
         //Make sure at least 1 second has passed, then convert to crystals
-        if ( !sodiumChlorideUnsaturated.get() && timeSinceLast > 1 && model.sodiumChlorideCrystals.size() == 0 ) {
+        if ( !unsaturated.get() && timeSinceLast > 1 && crystals.size() == 0 ) {
 
             //Create a crystal if there weren't any
             System.out.println( "No crystals, starting a new one" );
-            startSodiumChlorideCrystal();
-            lastNaClCrystallizationTime = model.getTime();
+            formNewCrystal();
+            lastNewCrystalFormationTime = model.getTime();
         }
 
         //If the solution is saturated, try adding on to an existing crystal
-        else if ( !sodiumChlorideUnsaturated.get() ) {
+        else if ( !unsaturated.get() ) {
 
             //Find existing crystal(s)
-            SodiumChlorideCrystal crystal = model.sodiumChlorideCrystals.get( 0 );
+            Crystal<T> crystal = crystals.get( 0 );
 
             //Enumerate all particles and distances from crystal sites, but only look for sites that are underwater, otherwise particles would try to fly out of the solution (and get stuck at the boundary)
-            ArrayList<CrystallizationMatch> matches = getAllCrystallizationMatches( crystal );
+            ArrayList<CrystallizationMatch<T>> matches = getAllCrystallizationMatches( crystal );
             if ( matches.size() > 0 ) {
 
                 //Find a matching particle nearby one of the sites and join it together
-                CrystallizationMatch match = matches.get( 0 );
+                CrystallizationMatch<T> match = matches.get( 0 );
 
                 //If close enough, join the lattice
                 if ( match.distance <= FREE_PARTICLE_SPEED * dt ) {
@@ -63,7 +62,7 @@ public class IncrementalGrowth {
                     model.freeParticles.remove( match.particle );
 
                     //Add it as a constituent of the crystal
-                    crystal.addConstituent( new Constituent<SphericalParticle>( (SphericalParticle) match.particle, match.site.relativePosition ) );
+                    crystal.addConstituent( new Constituent<T>( match.particle, match.site.relativePosition ) );
                 }
 
                 //Otherwise, move closest particle toward the lattice
@@ -75,18 +74,18 @@ public class IncrementalGrowth {
             //No matches, so start a new crystal
             else {
                 System.out.println( "No matches, starting a new crystal" );
-                startSodiumChlorideCrystal();
+                formNewCrystal();
             }
         }
     }
 
     //Look for all open site on its lattice and sort by distance to available participants
-    public ArrayList<CrystallizationMatch> getAllCrystallizationMatches( SodiumChlorideCrystal crystal ) {
-        ArrayList<CrystallizationMatch> matches = new ArrayList<CrystallizationMatch>();
+    public ArrayList<CrystallizationMatch<T>> getAllCrystallizationMatches( Crystal<T> crystal ) {
+        ArrayList<CrystallizationMatch<T>> matches = new ArrayList<CrystallizationMatch<T>>();
         for ( Particle freeParticle : model.freeParticles ) {
-            for ( OpenSite<SphericalParticle> openSite : crystal.getOpenSites() ) {
+            for ( OpenSite<T> openSite : crystal.getOpenSites() ) {
                 if ( model.solution.shape.get().contains( openSite.shape.getBounds2D() ) && openSite.matches( freeParticle ) ) {
-                    matches.add( new CrystallizationMatch( freeParticle, openSite ) );
+                    matches.add( new CrystallizationMatch<T>( (T) freeParticle, openSite ) );
                 }
             }
         }
@@ -101,20 +100,24 @@ public class IncrementalGrowth {
     }
 
     //Convert a particle to a crystal, or add to existing crystals to decrease the concentration below the saturation point
-    private void startSodiumChlorideCrystal() {
-        Option<Particle> selected = model.freeParticles.selectRandom( Sodium.class, Chloride.class );
+    private void formNewCrystal() {
+        Option<?> selected = selectSeed();
         if ( selected.isSome() ) {
             //If there is no crystal, create one with one particle
-            convertToCrystal( selected.get() );
+            convertToCrystal( (T) selected.get() );
         }
     }
 
+    protected abstract Option<?> selectSeed();
+
     //Convert the specified particle to a crystal
-    private void convertToCrystal( Particle particle ) {
-        SodiumChlorideCrystal crystal = new SodiumChlorideCrystal( particle.getPosition(), randomAngle() );
-        crystal.addConstituent( new Constituent<SphericalParticle>( (SphericalParticle) particle, ImmutableVector2D.ZERO ) );
+    private void convertToCrystal( T particle ) {
+        U crystal = toCrystal( particle );
+        crystal.addConstituent( new Constituent<T>( particle, ImmutableVector2D.ZERO ) );
 
         model.freeParticles.remove( particle );
-        model.sodiumChlorideCrystals.add( crystal );
+        crystals.add( crystal );
     }
+
+    protected abstract U toCrystal( T particle );
 }
