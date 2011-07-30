b4eb766234c0f3ad716f1a87205223e267fa26ef
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-07-30 01:57:05 +0000

    factored out RemoveSoluteButtonNode

---
+++ trunk/simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/macro/view/RemoveSoluteButtonNode.java
@@ -0,0 +1,39 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.sugarandsaltsolutions.macro.view;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import edu.colorado.phet.common.phetcommon.model.property.ObservableProperty;
+import edu.colorado.phet.common.phetcommon.util.function.VoidFunction0;
+import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
+import edu.colorado.phet.common.piccolophet.nodes.TextButtonNode;
+
+import static edu.colorado.phet.sugarandsaltsolutions.common.view.SugarAndSaltSolutionsCanvas.BUTTON_COLOR;
+import static edu.colorado.phet.sugarandsaltsolutions.common.view.SugarAndSaltSolutionsCanvas.CONTROL_FONT;
+
+/**
+ * Button that allows the user to remove a particular type of solute, only shown if the sim contains the solute.
+ *
+ * @author Sam Reid
+ */
+public class RemoveSoluteButtonNode extends TextButtonNode {
+    public RemoveSoluteButtonNode( String text, ObservableProperty<Boolean> anyToRemove, final VoidFunction0 remove ) {
+        super( text, CONTROL_FONT );
+        setBackground( BUTTON_COLOR );
+
+        //Only show the button if there is solute to be removed
+        anyToRemove.addObserver( new VoidFunction1<Boolean>() {
+            public void apply( Boolean anyToRemove ) {
+                setVisible( anyToRemove );
+            }
+        } );
+
+        //When the user presses the button ,clear the solute
+        addActionListener( new ActionListener() {
+            public void actionPerformed( ActionEvent e ) {
+                remove.apply();
+            }
+        } );
+    }
+}
--- trunk/simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/macro/view/RemoveSoluteControlNode.java
+++ trunk/simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/macro/view/RemoveSoluteControlNode.java
@@ -1,16 +1,10 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.sugarandsaltsolutions.macro.view;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
-import edu.colorado.phet.common.piccolophet.nodes.TextButtonNode;
+import edu.colorado.phet.common.phetcommon.util.function.VoidFunction0;
 import edu.colorado.phet.sugarandsaltsolutions.common.model.ISugarAndSaltModel;
 import edu.colorado.phet.sugarandsaltsolutions.common.view.SugarAndSaltSolutionsCanvas;
 import edu.umd.cs.piccolo.PNode;
-
-import static edu.colorado.phet.sugarandsaltsolutions.common.view.SugarAndSaltSolutionsCanvas.CONTROL_FONT;
 
 /**
  * Node that contains one button to remove salt (if there is any salt) and another button for sugar.
@@ -18,38 +12,23 @@ import static edu.colorado.phet.sugarandsaltsolutions.common.view.SugarAndSaltSo
  * @author Sam Reid
  */
 public class RemoveSoluteControlNode extends PNode {
+
     public RemoveSoluteControlNode( final ISugarAndSaltModel model ) {
 
         //Button to remove salt, only shown if there is any salt
-        TextButtonNode saltButton = new TextButtonNode( "Remove salt", CONTROL_FONT ) {{
-            setBackground( SugarAndSaltSolutionsCanvas.BUTTON_COLOR );
-            model.isAnySaltToRemove().addObserver( new VoidFunction1<Boolean>() {
-                public void apply( Boolean visible ) {
-                    setVisible( visible );
-                }
-            } );
-            addActionListener( new ActionListener() {
-                public void actionPerformed( ActionEvent e ) {
-                    model.removeSalt();
-                }
-            } );
-        }};
+        RemoveSoluteButtonNode saltButton = new RemoveSoluteButtonNode( "Remove salt", model.isAnySaltToRemove(), new VoidFunction0() {
+            public void apply() {
+                model.removeSalt();
+            }
+        } );
         addChild( saltButton );
 
         //Button to remove sugar, only shown if there is any sugar
-        TextButtonNode sugarButton = new TextButtonNode( "Remove sugar", CONTROL_FONT ) {{
-            setBackground( SugarAndSaltSolutionsCanvas.BUTTON_COLOR );
-            model.isAnySugarToRemove().addObserver( new VoidFunction1<Boolean>() {
-                public void apply( Boolean visible ) {
-                    setVisible( visible );
-                }
-            } );
-            addActionListener( new ActionListener() {
-                public void actionPerformed( ActionEvent e ) {
-                    model.removeSugar();
-                }
-            } );
-        }};
+        RemoveSoluteButtonNode sugarButton = new RemoveSoluteButtonNode( "Remove sugar", model.isAnySugarToRemove(), new VoidFunction0() {
+            public void apply() {
+                model.removeSugar();
+            }
+        } );
         addChild( sugarButton );
 
         //Put the buttons next to each other, leaving the origin at (0,0) so it can be positioned easily by the client
