9dd04417ae2455e13b6774219776737111d88101
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-07-31 03:22:16 +0000

    Micro tab: Fixed: When toggling back and forth between kits, sometimes bar chart labels appear under the kit box, not in the bar chart background

--- trunk/simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/common/view/barchart/BarItem.java
+++ trunk/simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/common/view/barchart/BarItem.java
@@ -5,6 +5,7 @@ import java.awt.Color;
 
 import edu.colorado.phet.common.phetcommon.model.property.ObservableProperty;
 import edu.colorado.phet.common.phetcommon.util.Option;
+import edu.colorado.phet.common.phetcommon.util.function.Function0;
 import edu.umd.cs.piccolo.PNode;
 
 /**
@@ -16,9 +17,11 @@ public class BarItem {
     public final ObservableProperty<Double> concentration;
     public final ObservableProperty<Color> color;
     public final String caption;
-    public final Option<PNode> icon;
 
-    public BarItem( ObservableProperty<Double> concentration, ObservableProperty<Color> color, String caption, Option<PNode> icon ) {
+    //Icons to be shown beneath the bar.  Functions are used to create new icons for each kit since giving the same PNode multiple parents caused layout problems
+    public final Function0<Option<PNode>> icon;
+
+    public BarItem( ObservableProperty<Double> concentration, ObservableProperty<Color> color, String caption, Function0<Option<PNode>> icon ) {
         this.concentration = concentration;
         this.color = color;
         this.caption = caption;
