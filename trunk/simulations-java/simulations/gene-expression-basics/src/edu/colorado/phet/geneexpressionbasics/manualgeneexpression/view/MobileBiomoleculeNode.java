8f163a48048cb8ec987c1127f24dd57b5ba68ae7
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-08-05 18:54:09 +0000

    Added gradient for drawing biomolecules.

--- trunk/simulations-java/simulations/gene-expression-basics/src/edu/colorado/phet/geneexpressionbasics/manualgeneexpression/view/MobileBiomoleculeNode.java
+++ trunk/simulations-java/simulations/gene-expression-basics/src/edu/colorado/phet/geneexpressionbasics/manualgeneexpression/view/MobileBiomoleculeNode.java
@@ -3,10 +3,14 @@ package edu.colorado.phet.geneexpressionbasics.manualgeneexpression.view;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.GradientPaint;
+import java.awt.Paint;
 import java.awt.Shape;
+import java.awt.geom.Point2D;
 
 import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
+import edu.colorado.phet.common.phetcommon.view.util.ColorUtils;
 import edu.colorado.phet.common.piccolophet.event.CursorHandler;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.colorado.phet.geneexpressionbasics.common.model.MobileBiomolecule;
@@ -19,11 +23,19 @@ import edu.umd.cs.piccolo.PNode;
  */
 public class MobileBiomoleculeNode extends PNode {
     public MobileBiomoleculeNode( final ModelViewTransform mvt, final MobileBiomolecule mobileBiomolecule ) {
-        addChild( new PhetPPath( mobileBiomolecule.getBaseColor(), new BasicStroke( 1 ), Color.BLACK ) {{
+
+        Paint gradientPaint = new GradientPaint( mvt.modelToView( new Point2D.Double( mobileBiomolecule.getShape().getBounds2D().getMinX(),
+                                                                                      mobileBiomolecule.getShape().getBounds2D().getCenterY() ) ),
+                                                 ColorUtils.brighterColor( mobileBiomolecule.getBaseColor(), 0.5 ),
+                                                 new Point2D.Double( mobileBiomolecule.getShape().getBounds2D().getMaxX(),
+                                                                     mobileBiomolecule.getShape().getBounds2D().getCenterY() ),
+                                                 ColorUtils.darkerColor( mobileBiomolecule.getBaseColor(), 0.5 ) );
+        addChild( new PhetPPath( new BasicStroke( 1 ), Color.BLACK ) {{
             // Update the shape whenever it changes.
             mobileBiomolecule.addShapeChangeObserver( new VoidFunction1<Shape>() {
                 public void apply( Shape shape ) {
                     setPathTo( mvt.modelToView( shape ) );
+                    setPaint( createGradientPaint( mvt, mobileBiomolecule ) );
                 }
             } );
             // Cursor handling.
@@ -31,5 +43,14 @@ public class MobileBiomoleculeNode extends PNode {
             // Drag handling.
             addInputEventListener( new BiomoleculeDragHandler( mobileBiomolecule, this, mvt ) );
         }} );
+    }
+
+    Paint createGradientPaint( ModelViewTransform mvt, MobileBiomolecule mobileBiomolecule ) {
+        return new GradientPaint( mvt.modelToView( new Point2D.Double( mobileBiomolecule.getShape().getBounds2D().getMinX(),
+                                                                       mobileBiomolecule.getShape().getBounds2D().getCenterY() ) ),
+                                  ColorUtils.brighterColor( mobileBiomolecule.getBaseColor(), 0.8 ),
+                                  mvt.modelToView( new Point2D.Double( mobileBiomolecule.getShape().getBounds2D().getMaxX(),
+                                                                       mobileBiomolecule.getShape().getBounds2D().getCenterY() ) ),
+                                  ColorUtils.darkerColor( mobileBiomolecule.getBaseColor(), 0.5 ) );
     }
 }
