1373d3dbb576f88d2c606d4df280f92081f4a2f3
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-08-06 03:08:14 +0000

    created dev package and moved existing dev classes there

--- trunk/simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/water/dev/DeveloperControlDialog.java
+++ trunk/simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/water/dev/DeveloperControlDialog.java
@@ -1,5 +1,5 @@
 // Copyright 2002-2011, University of Colorado
-package edu.colorado.phet.sugarandsaltsolutions.water.view;
+package edu.colorado.phet.sugarandsaltsolutions.water.dev;
 
 import java.awt.Window;
 
