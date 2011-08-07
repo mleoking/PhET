ada6eecb391a6cf75a0f8aed0440249be1ce45e0
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-08-07 10:16:30 +0000

    ms: added detection of least-squares optimized closest correct VSEPR configuration (and thus accurate error bounds)

--- trunk/simulations-java/simulations/molecule-shapes/src/edu/colorado/phet/moleculeshapes/model/MoleculeModel.java
+++ trunk/simulations-java/simulations/molecule-shapes/src/edu/colorado/phet/moleculeshapes/model/MoleculeModel.java
@@ -2,6 +2,8 @@
 package edu.colorado.phet.moleculeshapes.model;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class MoleculeModel {
@@ -25,12 +27,13 @@ public class MoleculeModel {
             pair.attractToDistance( tpf );
         }
         if ( counter++ % 50 == 0 ) {
-            VseprConfiguration config = new VseprConfiguration( getBondedAtoms().size(), getLonePairs().size() );
+            VseprConfiguration config = new VseprConfiguration( getBondedPairs().size(), getLonePairs().size() );
             System.out.println( "Testing " + config.name + "(" + config.geometry.name + "): " + config.matchesElectronPairs( pairs, 0.15 ) );
+            AttractorModel.applyAttractorForces( this, tpf );
         }
     }
 
-    public ArrayList<ElectronPair> getBondedAtoms() {
+    public ArrayList<ElectronPair> getBondedPairs() {
         return getPairs( false );
     }
 
@@ -50,6 +53,21 @@ public class MoleculeModel {
 
     public void addPair( ElectronPair pair ) {
         pairs.add( pair );
+
+        // sort so that the lone pairs come first
+        Collections.sort( pairs, new Comparator<ElectronPair>() {
+            public int compare( ElectronPair a, ElectronPair b ) {
+                if ( a.isLonePair == b.isLonePair ) {
+                    return 0;
+                }
+                if ( a.isLonePair ) {
+                    return -1;
+                }
+                else {
+                    return 1;
+                }
+            }
+        } );
 
         // notify
         for ( Listener listener : listeners ) {
