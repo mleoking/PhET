19fc486ff27c4219a47c16da70c106148d6742f3
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-08-08 23:00:14 +0000

    Micro tab: Use the same scale factor for ParticleCountTable

--- simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/micro/model/ParticleCountTable.java
+++ simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/micro/model/ParticleCountTable.java
@@ -6,16 +6,14 @@ package edu.colorado.phet.sugarandsaltsolutions.micro.model;
  * @author Sam Reid
  */
 public class ParticleCountTable {
+
+    //Increase the requested amount because otherwise it is too difficult to get it to crystallize before the water is almost completely evaporated
+    public static final int scaleFactor = 2;
+
     //Amounts are given for 1E-23L of water, then multiplied by 2 since the max fluid volume is 2E-23L
-    public static final int MAX_SODIUM_CHLORIDE = (int) ( 10.30 * 2 );
-    public static final int MAX_ETHANOL = (int) ( 13.07 * 2 );
-
-    //Increase the requested amount of CaCl2 because otherwise it is too difficult to get it to crystallize before the water is almost completely evaporated
-    public static final int MAX_CALCIUM_CHLORIDE = (int) ( 5.43 * 2 ) * 3;
-
-    //Increase the requested amount of CaCl2 because otherwise it is too difficult to get it to crystallize before the water is almost completely evaporated
-    public static final int MAX_SODIUM_NITRATE = (int) ( 7.00 * 2 ) * 3;
-
-    //Increase the requested amount of sucrose because otherwise it is too difficult to get it to crystallize before the water is almost completely evaporated
-    public static final int MAX_SUCROSE = (int) ( 1.76 * 2 ) * 3;
+    public static final int MAX_SODIUM_CHLORIDE = (int) ( 10.30 * 2 ) * scaleFactor;
+    public static final int MAX_ETHANOL = (int) ( 13.07 * 2 ) * scaleFactor;
+    public static final int MAX_CALCIUM_CHLORIDE = (int) ( 5.43 * 2 ) * scaleFactor;
+    public static final int MAX_SODIUM_NITRATE = (int) ( 7.00 * 2 ) * scaleFactor;
+    public static final int MAX_SUCROSE = (int) ( 1.76 * 2 ) * scaleFactor;
 }
