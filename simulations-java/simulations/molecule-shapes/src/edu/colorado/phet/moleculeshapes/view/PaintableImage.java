d1e3f48f3ea2fd2f8b5a2812ddeb6951a1074de6
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-08-09 01:15:09 +0000

    ms: HUD testing

---
+++ simulations-java/simulations/molecule-shapes/src/edu/colorado/phet/moleculeshapes/view/PaintableImage.java
@@ -0,0 +1,36 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.moleculeshapes.view;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.nio.ByteBuffer;
+
+import com.jme3.texture.Image;
+
+public abstract class PaintableImage extends Image {
+    private BufferedImage backImg;
+    private ByteBuffer scratch;
+
+    public PaintableImage( int width, int height, boolean hasAlpha ) {
+        super( hasAlpha ? Format.ABGR8 : Format.BGR8, width, height, ByteBuffer.allocateDirect( 4 * width * height ) );
+        scratch = data.get( 0 );
+        backImg = new BufferedImage( width, height, hasAlpha ? BufferedImage.TYPE_4BYTE_ABGR : BufferedImage.TYPE_3BYTE_BGR );
+
+    }
+
+    public void refreshImage() {
+        Graphics2D g = backImg.createGraphics();
+        paint( g );
+        g.dispose();
+
+        /* get the image data */
+        byte data[] = (byte[]) backImg.getRaster().getDataElements( 0, 0, backImg.getWidth(), backImg.getHeight(), null );
+        scratch.clear();
+        scratch.put( data, 0, data.length );
+        scratch.rewind();
+        setData( scratch );
+    }
+
+    public abstract void paint( Graphics2D graphicsContext );
+
+}
