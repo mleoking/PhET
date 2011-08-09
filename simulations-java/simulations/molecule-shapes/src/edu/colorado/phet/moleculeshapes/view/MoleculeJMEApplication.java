2fb0984627f43b44c149b93272cb4504114a3324
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-08-09 06:34:17 +0000

    ms: refactor out HUDNode

--- simulations-java/simulations/molecule-shapes/src/edu/colorado/phet/moleculeshapes/view/MoleculeJMEApplication.java
+++ simulations-java/simulations/molecule-shapes/src/edu/colorado/phet/moleculeshapes/view/MoleculeJMEApplication.java
@@ -1,16 +1,12 @@
 package edu.colorado.phet.moleculeshapes.view;
 
-import java.awt.*;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.Callable;
 
-import javax.swing.*;
-
 import edu.colorado.phet.common.phetcommon.model.property.Property;
-import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.moleculeshapes.MoleculeShapesConstants;
 import edu.colorado.phet.moleculeshapes.model.ElectronPair;
 import edu.colorado.phet.moleculeshapes.model.ImmutableVector3D;
@@ -23,16 +19,10 @@ import com.jme3.input.controls.AnalogListener;
 import com.jme3.input.controls.MouseAxisTrigger;
 import com.jme3.input.controls.MouseButtonTrigger;
 import com.jme3.light.DirectionalLight;
-import com.jme3.material.Material;
-import com.jme3.material.RenderState.BlendMode;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
-import com.jme3.renderer.queue.RenderQueue.Bucket;
-import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
-import com.jme3.scene.shape.Quad;
-import com.jme3.texture.Texture2D;
 
 /**
  * Use jme3 to show a rotating molecule
@@ -187,59 +177,7 @@ public class MoleculeJMEApplication extends BaseJMEApplication {
         setDisplayFps( false );
         setDisplayStatView( false );
 
-        guiNode.attachChild( new Geometry( "HUD", new Quad( 512, 64, true ) ) {{
-            setMaterial( new Material( assetManager, "Common/MatDefs/Misc/Unshaded.j3md" ) {{
-                setTexture( "ColorMap", new Texture2D() {{
-                    setImage( new PaintableImage( 512, 64, true ) {
-                        JPanel panel = new JPanel( new FlowLayout( FlowLayout.LEFT ) );
-
-                        {
-                            panel.add( new JLabel( "This is a test 图片" ) {{
-                                setForeground( Color.BLACK );
-                                setFont( new PhetFont( 20 ) );
-                            }} );
-                            panel.add( new JLabel( "Test" ) );
-                            panel.add( new JButton( "Test" ) );
-                            panel.setPreferredSize( new Dimension( 256, 64 ) );
-                            panel.setSize( panel.getPreferredSize() );
-                            layoutComponent( panel );
-                            panel.setDoubleBuffered( false ); // avoids having the RepaintManager attempt to get the containing window (and throw a NPE)
-
-                            System.out.println( panel.getBounds() );
-                            System.out.println( panel.isDisplayable() );
-
-                            refreshImage();
-                        }
-
-                        @Override public void paint( Graphics2D g ) {
-                            g.setBackground( new Color( 0f, 0f, 0f, 0f ) );
-                            g.clearRect( 0, 0, getWidth(), getHeight() );
-                            if ( panel != null ) {
-                                panel.paint( g );
-                            }
-                        }
-                    } );
-                }} );
-
-                getAdditionalRenderState().setBlendMode( BlendMode.Alpha );
-                setTransparent( true );
-            }} );
-//            setQueueBucket( Bucket.Transparent );
-
-            setLocalTranslation( new Vector3f( 10, 10, 0 ) );
-        }} );
-    }
-
-    private static void layoutComponent( Component component ) {
-        synchronized ( component.getTreeLock() ) {
-            component.doLayout();
-
-            if ( component instanceof Container ) {
-                for ( Component child : ( (Container) component ).getComponents() ) {
-                    layoutComponent( child );
-                }
-            }
-        }
+        guiNode.attachChild( new HUDNode( assetManager ) );
     }
 
     public void testAddAtom( boolean isLonePair ) {
@@ -306,4 +244,5 @@ public class MoleculeJMEApplication extends BaseJMEApplication {
             }
         } );
     }
+
 }
