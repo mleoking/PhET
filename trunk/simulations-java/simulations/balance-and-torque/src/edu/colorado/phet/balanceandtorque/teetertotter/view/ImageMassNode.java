118c86cc79d7c32bf8b97f77d15100257b3c7f84
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-08-11 22:43:18 +0000

    Partial implementation of mass indicators for the image nodes.  Doesn't quite work yet - seems to mess up the bounds or rotation or something.

--- trunk/simulations-java/simulations/balance-and-torque/src/edu/colorado/phet/balanceandtorque/teetertotter/view/ImageMassNode.java
+++ trunk/simulations-java/simulations/balance-and-torque/src/edu/colorado/phet/balanceandtorque/teetertotter/view/ImageMassNode.java
@@ -3,15 +3,18 @@ package edu.colorado.phet.balanceandtorque.teetertotter.view;
 
 import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
+import java.text.DecimalFormat;
 
 import edu.colorado.phet.balanceandtorque.teetertotter.model.masses.ImageMass;
+import edu.colorado.phet.common.phetcommon.model.property.BooleanProperty;
 import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
+import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.PhetPCanvas;
 import edu.colorado.phet.common.piccolophet.event.CursorHandler;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.nodes.PImage;
-import edu.umd.cs.piccolo.util.PBounds;
+import edu.umd.cs.piccolo.nodes.PText;
 
 /**
  * This class defines a Piccolo node that represents a model element in the
@@ -23,14 +26,13 @@ import edu.umd.cs.piccolo.util.PBounds;
 public class ImageMassNode extends PNode {
     private final ImageMass mass;
     private final ModelViewTransform mvt;
-    private PBounds unrotatedBounds = new PBounds();
+    final PImage imageNode = new PImage();
 
-    public ImageMassNode( final ModelViewTransform mvt, final ImageMass mass, PhetPCanvas canvas ) {
+    public ImageMassNode( final ModelViewTransform mvt, final ImageMass mass, PhetPCanvas canvas, BooleanProperty massLabelVisibilityProperty ) {
         this.mass = mass;
         this.mvt = mvt;
 
-        final PImage imageNode = new PImage();
-        unrotatedBounds.setRect( imageNode.getFullBoundsReference() );
+        // Add the image node.
         addChild( imageNode );
 
         // Observe image changes.
@@ -43,7 +45,6 @@ public class ImageMassNode extends PNode {
                     System.out.println( getClass().getName() + " - Warning: Scaling factor is too large or small, drawing size should be adjusted.  Scaling factor = " + scalingFactor );
                 }
                 imageNode.setScale( scalingFactor );
-                unrotatedBounds.setRect( imageNode.getFullBoundsReference() );
                 updatePositionAndAngle();
             }
         } );
@@ -54,22 +55,38 @@ public class ImageMassNode extends PNode {
                 imageNode.setScale( 1 );
                 double scalingFactor = Math.abs( mvt.modelToViewDeltaY( newHeight ) ) / imageNode.getFullBoundsReference().height;
                 imageNode.setScale( scalingFactor );
-                unrotatedBounds.setRect( imageNode.getFullBoundsReference() );
                 updatePositionAndAngle();
             }
         } );
 
-        // Register for notification of position changes.
+        // Observe position changes.
         mass.addPositionChangeObserver( new VoidFunction1<Point2D>() {
             public void apply( Point2D newPosition ) {
                 updatePositionAndAngle();
             }
         } );
 
-        // Register for notifications of rotational angle changes
+        // Observe rotational angle changes.
         mass.addRotationalAngleChangeObserver( new VoidFunction1<Double>() {
             public void apply( Double newAngle ) {
                 updatePositionAndAngle();
+            }
+        } );
+
+        // Add the mass indication and observe changes to its visibility.
+        DecimalFormat formatter = new DecimalFormat( "##" );
+        // TODO: i18n, including order and units!
+        final PText massIndicator = new PText( formatter.format( mass.getMass() ) + " kg" ) {{
+            setFont( new PhetFont( 18 ) );
+        }};
+        massIndicator.setOffset( imageNode.getFullBoundsReference().getCenterX() - massIndicator.getFullBoundsReference().width / 2,
+                                 imageNode.getFullBoundsReference().getMinY() - massIndicator.getFullBoundsReference().height );
+        addChild( massIndicator );
+
+        // Observe changes to mass indicator visibility.
+        massLabelVisibilityProperty.addObserver( new VoidFunction1<Boolean>() {
+            public void apply( Boolean showMassIndicator ) {
+                massIndicator.setVisible( showMassIndicator );
             }
         } );
 
@@ -82,8 +99,8 @@ public class ImageMassNode extends PNode {
 
     private void updatePositionAndAngle() {
         setRotation( 0 );
-        setOffset( mvt.modelToViewX( mass.getPosition().getX() - mass.getCenterOfMassXOffset() ) - getFullBoundsReference().width / 2,
-                   mvt.modelToViewY( mass.getPosition().getY() ) - getFullBoundsReference().height );
+        setOffset( mvt.modelToViewX( mass.getPosition().getX() - mass.getCenterOfMassXOffset() ) - imageNode.getFullBoundsReference().width / 2,
+                   mvt.modelToViewY( mass.getPosition().getY() ) - imageNode.getFullBoundsReference().height );
         rotateAboutPoint( -mass.getRotationAngle(), getFullBoundsReference().getWidth() / 2 + mvt.modelToViewDeltaX( mass.getCenterOfMassXOffset() ),
                           getFullBoundsReference().getHeight() );
     }
