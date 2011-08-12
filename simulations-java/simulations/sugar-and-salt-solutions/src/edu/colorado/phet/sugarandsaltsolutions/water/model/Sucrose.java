aae01719892608b0f53b095ca01935cc388de826
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-08-12 18:40:20 +0000

    Fixed z-ordering for sucrose

--- simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/water/model/Sucrose.java
+++ simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/water/model/Sucrose.java
@@ -5,12 +5,12 @@ import java.util.ArrayList;
 
 import org.jbox2d.dynamics.World;
 
-import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
 import edu.colorado.phet.common.phetcommon.model.property.ObservableProperty;
 import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.phetcommon.util.function.VoidFunction0;
 import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
+import edu.colorado.phet.sugarandsaltsolutions.common.model.ProjectedPositions.AtomPosition;
 import edu.colorado.phet.sugarandsaltsolutions.common.model.SucrosePositions;
 
 /**
@@ -21,31 +21,35 @@ public class Sucrose extends Molecule {
     private ArrayList<Atom> hydrogens = new ArrayList<Atom>();
     private ArrayList<Atom> oxygens = new ArrayList<Atom>();
     private ArrayList<Atom> carbons = new ArrayList<Atom>();
+    private ObservableProperty<Double> oxygenCharge;
+    private ObservableProperty<Double> hydrogenCharge;
 
     public Sucrose( World world, final ModelViewTransform transform, double x, double y, double vx, double vy, final double theta, VoidFunction1<VoidFunction0> addUpdateListener, ObservableProperty<Double> oxygenCharge, ObservableProperty<Double> hydrogenCharge ) {
         super( world, transform, x, y, vx, vy, theta, addUpdateListener );
+        this.oxygenCharge = oxygenCharge;
+        this.hydrogenCharge = hydrogenCharge;
         SucrosePositions sucrosePositions = new SucrosePositions();
         ArrayList<Atom> allAtoms = new ArrayList<Atom>();
 
         //Flag the first particle as being the coordinate frame origin for the molecule
         boolean origin = true;
-        for ( ImmutableVector2D offset : sucrosePositions.getCarbonPositions() ) {
-            final Atom carbon = new Atom( x + offset.getX(), y + offset.getY(), transform, WaterMolecule.carbonRadius, body, offset.getX(), offset.getY(), new Property<Double>( 0.0 ), origin );
-            carbons.add( carbon );
-            allAtoms.add( carbon );
+
+        for ( AtomPosition atomPosition : sucrosePositions.getAtoms() ) {
+            final Atom a = new Atom( x + atomPosition.position.getX(), y + atomPosition.position.getY(), transform, WaterMolecule.carbonRadius, body, atomPosition.position.getX(), atomPosition.position.getY(), getCharge( atomPosition ), origin );
+            if ( atomPosition.type.equals( "C" ) ) { carbons.add( a ); }
+            if ( atomPosition.type.equals( "H" ) ) { hydrogens.add( a ); }
+            if ( atomPosition.type.equals( "O" ) ) { oxygens.add( a ); }
+            allAtoms.add( a );
             origin = false;
         }
-        for ( ImmutableVector2D position : sucrosePositions.getOxygenPositions() ) {
-            final Atom oxygen = new Atom( x + position.getX(), y + position.getY(), transform, WaterMolecule.oxygenRadius, body, position.getX(), position.getY(), oxygenCharge, origin );
-            oxygens.add( oxygen );
-            allAtoms.add( oxygen );
-        }
-        for ( ImmutableVector2D position : sucrosePositions.getHydrogenPositions() ) {
-            final Atom hydrogen = new Atom( x + position.getX(), y + position.getY(), transform, WaterMolecule.hydrogenRadius, body, position.getX(), position.getY(), hydrogenCharge, origin );
-            hydrogens.add( hydrogen );
-            allAtoms.add( hydrogen );
-        }
         initAtoms( allAtoms.toArray( new Atom[allAtoms.size()] ) );
+    }
+
+    private ObservableProperty<Double> getCharge( AtomPosition atomPosition ) {
+        if ( atomPosition.type.equals( "C" ) ) { return new Property<Double>( 0.0 ); }
+        if ( atomPosition.type.equals( "H" ) ) { return hydrogenCharge; }
+        if ( atomPosition.type.equals( "O" ) ) { return oxygenCharge; }
+        throw new RuntimeException( "Unknown type: " + atomPosition.type );
     }
 
     public ArrayList<Atom> getHydrogens() {
