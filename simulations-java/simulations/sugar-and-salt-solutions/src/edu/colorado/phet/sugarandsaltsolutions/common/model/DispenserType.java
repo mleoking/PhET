d061e2dc66ff6e5df0d7c84ee9d7fb075d598695
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-08-12 19:11:11 +0000

    Finished removing ethanol

--- simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/common/model/DispenserType.java
+++ simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/common/model/DispenserType.java
@@ -20,9 +20,6 @@ public class DispenserType {
     public static final DispenserType SODIUM_NITRATE = new DispenserType( "Sodium Nitrate", 11, 7, 8 );
     public static final DispenserType CALCIUM_CHLORIDE = new DispenserType( "Calcium Chloride", 20, 17 );
 
-    //TODO: Remove ethanol
-    public static final DispenserType ETHANOL = new DispenserType( "Ethanol", 6, 8, 1 );
-
     //Enum pattern, so no other instances should be created
     private DispenserType( String name, Integer... elements ) {
         this.name = name;
