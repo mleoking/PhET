e6fd9c8c80aa10dd60121215f52e8e71e7ecabba
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-08-12 02:52:47 +0000

    cannibalize Vector2DNode for use in rewrite of DipoleNode

--- trunk/simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/common/view/DipoleNode.java
+++ trunk/simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/common/view/DipoleNode.java
@@ -1,39 +1,73 @@
 // Copyright 2002-2011, University of Colorado
+
 package edu.colorado.phet.moleculepolarity.common.view;
 
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 
 import edu.colorado.phet.common.phetcommon.math.PolarCartesianConverter;
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
-import edu.colorado.phet.common.piccolophet.nodes.Vector2DNode;
+import edu.colorado.phet.common.phetcommon.view.graphics.Arrow;
+import edu.colorado.phet.common.piccolophet.PhetPNode;
 import edu.colorado.phet.moleculepolarity.MPConstants;
 import edu.colorado.phet.moleculepolarity.common.model.Bond;
-import edu.umd.cs.piccolox.nodes.PComposite;
+import edu.umd.cs.piccolo.nodes.PPath;
 
 /**
  * Base class for visual representation of dipoles.
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
-public abstract class DipoleNode extends PComposite {
+public class DipoleNode extends PhetPNode {
 
-    private static final double REF_MAGNITUDE = MPConstants.ELECTRONEGATIVITY_RANGE.getLength();
-    private static final double REF_LENGTH = 150;
-
+    private static final double REFERENCE_MAGNITUDE = MPConstants.ELECTRONEGATIVITY_RANGE.getLength(); // model value
+    private static final double REFERENCE_LENGTH = 150; // view size
     private static final double PERPENDICULAR_OFFSET = 75; // offset perpendicular to the axis of the endpoints
+    private static final Dimension HEAD_SIZE = new Dimension( 12, 25 ); // similar to Jmol
+    private static final double TAIL_WIDTH = 4; // similar to Jmol
+    private static final double FRACTIONAL_HEAD_HEIGHT = 0.5; // when the head size is less than fractionalHeadHeight * arrow length, the head will be scaled.
 
-    private final Vector2DNode vectorNode;
+    private double x;
+    private final PPath arrowNode;
 
-    protected DipoleNode( Color color ) {
-        vectorNode = new Vector2DNode( 1, 0, REF_MAGNITUDE, REF_LENGTH ); // origin is at tail of VectorNode
-        vectorNode.setArrowFillPaint( color );
-        vectorNode.setHeadSize( 12, 25 ); // similar to Jmol
-        vectorNode.setTailWidth( 4 ); // similar to Jmol
-        addChild( vectorNode );
+    private Point2D somePoint; // reusable point
+
+    public DipoleNode( Color color ) {
+        super();
+
+        setPickable( false );
+        setChildrenPickable( false );
+
+        arrowNode = new PPath();
+        arrowNode.setPaint( color );
+        addChild( arrowNode );
+
+        somePoint = new Point2D.Double();
+
+        update();
     }
 
     protected void setComponentX( double x ) {
-        vectorNode.setXY( x, 0 );
+        if ( x != this.x ) {
+            this.x = x;
+            update();
+        }
+    }
+
+    // Updates the arrow to match the node's state.
+    private void update() {
+        final double y = 0;
+        final double magnitude = PolarCartesianConverter.getRadius( x, y );
+        if ( magnitude == 0 ) {
+            arrowNode.setPathTo( new Rectangle2D.Double() ); // because Arrow doesn't handle zero-length arrows
+        }
+        else {
+            somePoint.setLocation( x * ( REFERENCE_LENGTH / REFERENCE_MAGNITUDE ), y );
+            Arrow arrow = new Arrow( new Point2D.Double( 0, 0 ), somePoint, HEAD_SIZE.height, HEAD_SIZE.width, TAIL_WIDTH, FRACTIONAL_HEAD_HEIGHT, true /* scaleTailToo */ );
+            arrowNode.setPathTo( arrow.getShape() );
+        }
     }
 
     // Visual representation of a bond dipole.
