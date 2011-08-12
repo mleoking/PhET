17ed6674ee0b447d8aa9e4cdd8db99cc707cf14b
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-08-12 18:06:00 +0000

    Added correct model positions for glucose

--- simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/micro/model/glucose/Glucose.java
+++ simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/micro/model/glucose/Glucose.java
@@ -2,13 +2,11 @@
 package edu.colorado.phet.sugarandsaltsolutions.micro.model.glucose;
 
 import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
-import edu.colorado.phet.sugarandsaltsolutions.common.model.SucrosePositions;
+import edu.colorado.phet.sugarandsaltsolutions.common.model.GlucosePositions;
+import edu.colorado.phet.sugarandsaltsolutions.common.model.GlucosePositions.Atom;
 import edu.colorado.phet.sugarandsaltsolutions.micro.model.Compound;
 import edu.colorado.phet.sugarandsaltsolutions.micro.model.Constituent;
 import edu.colorado.phet.sugarandsaltsolutions.micro.model.SphericalParticle;
-import edu.colorado.phet.sugarandsaltsolutions.micro.model.SphericalParticle.Carbon;
-import edu.colorado.phet.sugarandsaltsolutions.micro.model.SphericalParticle.Hydrogen;
-import edu.colorado.phet.sugarandsaltsolutions.micro.model.SphericalParticle.NeutralOxygen;
 
 import static edu.colorado.phet.common.phetcommon.math.ImmutableVector2D.ZERO;
 import static edu.colorado.phet.sugarandsaltsolutions.micro.model.MicroModel.sizeScale;
@@ -23,23 +21,12 @@ public class Glucose extends Compound<SphericalParticle> {
         this( ZERO, Math.random() * 2 * Math.PI );
     }
 
-    public Glucose( ImmutableVector2D relativePosition ) {
-        this( relativePosition, Math.random() * 2 * Math.PI );
-    }
-
     public Glucose( ImmutableVector2D relativePosition, double angle ) {
         super( relativePosition, angle );
 
-        //Add the glucose molecule atoms in the right locations
-        SucrosePositions sucrosePositions = new SucrosePositions();
-        for ( ImmutableVector2D offset : sucrosePositions.getHydrogenPositions() ) {
-            constituents.add( new Constituent<SphericalParticle>( new Hydrogen(), relativePosition.plus( offset.times( sizeScale ) ) ) );
-        }
-        for ( ImmutableVector2D offset : sucrosePositions.getCarbonPositions() ) {
-            constituents.add( new Constituent<SphericalParticle>( new Carbon(), relativePosition.plus( offset.times( sizeScale ) ) ) );
-        }
-        for ( ImmutableVector2D offset : sucrosePositions.getOxygenPositions() ) {
-            constituents.add( new Constituent<SphericalParticle>( new NeutralOxygen(), relativePosition.plus( offset.times( sizeScale ) ) ) );
+        //Add the glucose molecule atoms in the right locations, and in the right z-ordering
+        for ( Atom atom : new GlucosePositions().getAtoms() ) {
+            constituents.add( new Constituent<SphericalParticle>( atom.createConstituent(), relativePosition.plus( atom.position.times( sizeScale ) ) ) );
         }
 
         //Update positions so the lattice position overwrites constituent particle positions
