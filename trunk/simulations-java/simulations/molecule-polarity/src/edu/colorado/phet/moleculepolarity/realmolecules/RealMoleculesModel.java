9ac25cfe3cf2e751bac2c6b24ced43dbc1af0728
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-08-13 02:45:08 +0000

    8/12/11 i18n of molecule names

--- trunk/simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/realmolecules/RealMoleculesModel.java
+++ trunk/simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/realmolecules/RealMoleculesModel.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 
 import edu.colorado.phet.common.phetcommon.application.PhetApplication;
 import edu.colorado.phet.common.phetcommon.model.property.Property;
+import edu.colorado.phet.moleculepolarity.MPStrings;
 import edu.colorado.phet.moleculepolarity.common.model.MPModel;
 import edu.colorado.phet.moleculepolarity.common.model.Molecule3D;
 import edu.colorado.phet.moleculepolarity.common.model.Molecule3D.ImportMolecule;
@@ -20,33 +21,33 @@ public class RealMoleculesModel extends MPModel {
 
     private final ArrayList<Molecule3D> molecules;
 
-    //TODO i18n of molecule names
     public RealMoleculesModel() {
+
         molecules = new ArrayList<Molecule3D>() {{
 
-            add( new Molecule3D( "H2", "hydrogen", "mol2/h2.mol2" ) );
-            add( new Molecule3D( "N2", "nitrogen", "mol2/n2.mol2" ) );
-            add( new Molecule3D( "O2", "oxygen", "mol2/o2.mol2" ) );
-            add( new Molecule3D( "F2", "fluorine", "mol2/f2.mol2" ) );
-            add( new Molecule3D( "HF", "hydrogen fluoride", "mol2/hf.mol2" ) );
-            add( new Molecule3D( "CO", "carbon monoxide", "mol2/co.mol2" ) );
+            add( new Molecule3D( "H2", MPStrings.HYDROGEN, "mol2/h2.mol2" ) );
+            add( new Molecule3D( "N2", MPStrings.NITROGEN, "mol2/n2.mol2" ) );
+            add( new Molecule3D( "O2", MPStrings.OXYGEN, "mol2/o2.mol2" ) );
+            add( new Molecule3D( "F2", MPStrings.FLUORINE, "mol2/f2.mol2" ) );
+            add( new Molecule3D( "HF", MPStrings.HYDROGEN_FLUORIDE, "mol2/hf.mol2" ) );
+            add( new Molecule3D( "CO", MPStrings.CARBON_DIOXIDE, "mol2/co.mol2" ) );
 
-            add( new Molecule3D( "H2O", "water", "mol2/h2o.mol2" ) );
-            add( new Molecule3D( "CO2", "carbon dioxide", "mol2/co2.mol2" ) );
-            add( new Molecule3D( "HCN", "hydrogen cyanide", "mol2/hcn.mol2" ) );
-            add( new Molecule3D( "O3", "ozone", "mol2/o3.mol2" ) );
+            add( new Molecule3D( "H2O", MPStrings.WATER, "mol2/h2o.mol2" ) );
+            add( new Molecule3D( "CO2", MPStrings.CARBON_DIOXIDE, "mol2/co2.mol2" ) );
+            add( new Molecule3D( "HCN", MPStrings.HYDROGEN_CYANIDE, "mol2/hcn.mol2" ) );
+            add( new Molecule3D( "O3", MPStrings.OZONE, "mol2/o3.mol2" ) );
 
-            add( new Molecule3D( "NH3", "ammonia", "mol2/nh3.mol2" ) );
-            add( new Molecule3D( "BH3", "borane", "mol2/bh3.mol2" ) );
-            add( new Molecule3D( "BF3", "boron trifluoride", "mol2/bf3.mol2" ) );
-            add( new Molecule3D( "CH2O", "formaldehyde", "mol2/ch2o.mol2" ) );
+            add( new Molecule3D( "NH3", MPStrings.NITROGEN, "mol2/nh3.mol2" ) );
+            add( new Molecule3D( "BH3", MPStrings.BORANE, "mol2/bh3.mol2" ) );
+            add( new Molecule3D( "BF3", MPStrings.BORON_TRIFLUORIDE, "mol2/bf3.mol2" ) );
+            add( new Molecule3D( "CH2O", MPStrings.FORMALDEHYDE, "mol2/ch2o.mol2" ) );
 
-            add( new Molecule3D( "CH4", "methane", "mol2/ch4.mol2" ) );
-            add( new Molecule3D( "CH3F", "fluoromethane", "mol2/ch3f.mol2" ) );
-            add( new Molecule3D( "CH2F2", "difluoromethane", "mol2/ch2f2.mol2" ) );
-            add( new Molecule3D( "CHF3", "trifluoromethane", "mol2/chf3.mol2" ) );
-            add( new Molecule3D( "CF4", "tetrafluoromethane", "mol2/cf4.mol2" ) );
-            add( new Molecule3D( "CHCl3", "chloroform", "mol2/chcl3.mol2" ) );
+            add( new Molecule3D( "CH4", MPStrings.METHANE, "mol2/ch4.mol2" ) );
+            add( new Molecule3D( "CH3F", MPStrings.FLUOROMETHANE, "mol2/ch3f.mol2" ) );
+            add( new Molecule3D( "CH2F2", MPStrings.DIFLUOROMETHANE, "mol2/ch2f2.mol2" ) );
+            add( new Molecule3D( "CHF3", MPStrings.TRIFLUOROMETHANE, "mol2/chf3.mol2" ) );
+            add( new Molecule3D( "CF4", MPStrings.TETRAFLUOROMETHANE, "mol2/cf4.mol2" ) );
+            add( new Molecule3D( "CHCl3", MPStrings.CHLOROFORM, "mol2/chcl3.mol2" ) );
 
             if ( PhetApplication.getInstance().isDeveloperControlsEnabled() ) {
                 add( new ImportMolecule() );
