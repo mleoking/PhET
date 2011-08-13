4d4b8191d7a59b94420d043542cf39f6cec97930
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-08-13 03:46:09 +0000

    ms: added piccolo JME support and changed "Show Geometry ... " button to use TextButtonNode

---
+++ trunk/simulations-java/simulations/molecule-shapes/src/edu/colorado/phet/moleculeshapes/view/PiccoloJMENode.java
@@ -0,0 +1,51 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.moleculeshapes.view;
+
+import java.awt.*;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+import javax.swing.*;
+
+import edu.umd.cs.piccolo.PCanvas;
+import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.util.PBounds;
+
+import com.jme3.asset.AssetManager;
+import com.jme3.input.InputManager;
+
+public class PiccoloJMENode extends SwingJMENode {
+
+    public PiccoloJMENode( final PNode node, AssetManager assetManager, InputManager inputManager ) {
+        super( new JPanel( new FlowLayout( FlowLayout.LEFT, 0, 0 ) ) {{
+                   add( new PCanvas() {
+                       {
+                           setOpaque( false );
+                           removeInputEventListener( getZoomEventHandler() );
+                           removeInputEventListener( getPanEventHandler() );
+
+                           getLayer().addChild( node );
+
+                           node.addPropertyChangeListener( PNode.PROPERTY_FULL_BOUNDS, new PropertyChangeListener() {
+                               public void propertyChange( PropertyChangeEvent evt ) {
+                                   updateSize();
+                               }
+                           } );
+
+                           updateSize();
+                       }
+
+                       public void updateSize() {
+                           PBounds bounds = getRoot().getFullBounds();
+
+                           setPreferredSize( new Dimension( (int) bounds.width, (int) bounds.height ) );
+                           setSize( getPreferredSize() );
+                       }
+                   } );
+               }}, assetManager, inputManager );
+    }
+
+    public PCanvas getCanvas() {
+        return (PCanvas) getComponent();
+    }
+}
