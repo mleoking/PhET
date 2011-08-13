57893479ac6e23b297e72970e9ef4390a65b3a6d
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-08-13 03:11:55 +0000

    reorganize

--- trunk/simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/MPConstants.java
+++ trunk/simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/MPConstants.java
@@ -25,10 +25,9 @@ public class MPConstants {
     public static final DoubleRange ELECTRONEGATIVITY_RANGE = new DoubleRange( 0.7, 4, 2 );
     public static final double ELECTRONEGATIVITY_SNAP_INTERVAL = 0.1;
 
-    // View
+    // Canvas
     public static final Dimension CANVAS_RENDERING_SIZE = new Dimension( 1024, 600 );
     public static final Color CANVAS_COLOR = Color.WHITE;
-    public static final Color PLATE_DISABLED_COLOR = Color.LIGHT_GRAY;
 
     // E-field plates
     public static final double PLATE_WIDTH = 50; // width when the plate is flattened to 2D
@@ -37,5 +36,5 @@ public class MPConstants {
     public static final double PLATE_PERSPECTIVE_Y_OFFSET = 35; // y difference between foreground and background edges of the plate
     public static final Color PLATE_NEGATIVE_COLOR = Color.BLUE;
     public static final Color PLATE_POSITIVE_COLOR = PhetColorScheme.RED_COLORBLIND;
-
+    public static final Color PLATE_DISABLED_COLOR = Color.LIGHT_GRAY;
 }
