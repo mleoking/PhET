3bbf3cc6afabb7f8b51f6b4e891b5df1ac35ba22
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-08-15 22:21:21 +0000

    update atom locations when molecule angle or bond angles change

--- simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/common/model/TriatomicMolecule.java
+++ simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/common/model/TriatomicMolecule.java
@@ -4,8 +4,9 @@ package edu.colorado.phet.moleculepolarity.common.model;
 import java.awt.Color;
 
 import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
+import edu.colorado.phet.common.phetcommon.math.PolarCartesianConverter;
 import edu.colorado.phet.common.phetcommon.model.property.Property;
-import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
+import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 import edu.colorado.phet.moleculepolarity.MPConstants;
 import edu.colorado.phet.moleculepolarity.MPStrings;
 
@@ -18,37 +19,46 @@ public class TriatomicMolecule {
 
     private static final double BOND_LENGTH = 150;
 
-    private final Property<ImmutableVector2D> location; // center of atom B
-    public final Property<Double> angleAB, angleBC;
-    public final Atom atomA, atomB, atomC;
-    public final Bond bondAB, bondBC;
+    public final Atom atomA, atomB, atomC; // the atoms labeled A, B, C
+    public final Bond bondAB; // the bond connecting atoms A and B
+    public final Bond bondBC; // the bond connecting atoms B and C
+    private final Property<ImmutableVector2D> location; // location is at the center of atom B
+    public final Property<Double> angle; // angle of rotation of the entire molecule about the location
+    public final Property<Double> bondAngleA; // the bond angle of atom A relative to atom B, before applying molecule rotation
+    public final Property<Double> bondAngleC; // the bond angle of atom C relative to atom B, before applying molecule rotation
+
     private boolean dragging; // true when the user is dragging the molecule
 
     public TriatomicMolecule( ImmutableVector2D location ) {
+
         this.location = new Property<ImmutableVector2D>( location );
-        angleAB = new Property<Double>( 0d );
-        angleBC = new Property<Double>( 0d );
-        atomA = new Atom( MPStrings.A, 100, Color.YELLOW, MPConstants.ELECTRONEGATIVITY_RANGE.getMin(), location.minus( BOND_LENGTH / 2, 0 ) );
-        atomB = new Atom( MPStrings.B, 100, Color.ORANGE, MPConstants.ELECTRONEGATIVITY_RANGE.getMax(), location.plus( BOND_LENGTH / 2, 0 ) );
-        atomC = new Atom( MPStrings.C, 100, Color.GREEN, MPConstants.ELECTRONEGATIVITY_RANGE.getMin(), location.plus( BOND_LENGTH / 2, 0 ) );
+        atomA = new Atom( MPStrings.A, 100, Color.YELLOW, MPConstants.ELECTRONEGATIVITY_RANGE.getMin() );
+        atomB = new Atom( MPStrings.B, 100, Color.ORANGE, MPConstants.ELECTRONEGATIVITY_RANGE.getMax() );
+        atomC = new Atom( MPStrings.C, 100, Color.GREEN, MPConstants.ELECTRONEGATIVITY_RANGE.getMin() );
         bondAB = new Bond( atomA, atomB );
         bondBC = new Bond( atomB, atomC );
-        VoidFunction1<Double> observer = new VoidFunction1<Double>() {
-            public void apply( Double angle ) {
-                updateAtomLocations( angle );
+        angle = new Property<Double>( 0d );
+        bondAngleA = new Property<Double>( Math.PI );
+        bondAngleC = new Property<Double>( 0d );
+
+        SimpleObserver observer = new SimpleObserver() {
+            public void update() {
+                updateAtomLocations();
             }
         };
-        angleAB.addObserver( observer );
-        angleBC.addObserver( observer );
+        angle.addObserver( observer );
+        bondAngleA.addObserver( observer );
+        bondAngleC.addObserver( observer );
     }
 
     public void reset() {
         location.reset();
-        angleAB.reset();
-        angleBC.reset();
         atomA.reset();
         atomB.reset();
         atomC.reset();
+        angle.reset();
+        bondAngleA.reset();
+        bondAngleC.reset();
     }
 
     public boolean isDragging() {
@@ -60,17 +70,19 @@ public class TriatomicMolecule {
     }
 
     // repositions the atoms
-    private void updateAtomLocations( double angle ) {
-        final double radius = BOND_LENGTH / 2;
-        // atom A
-        double xA = location.get().getX() - BOND_LENGTH;
-        double yA = location.get().getY();
-        atomA.location.set( new ImmutableVector2D( xA, yA ) );
-        // atom B
+    private void updateAtomLocations() {
+        final double radius = BOND_LENGTH;
+        // atom B remains at a fixed location
         atomB.location.set( location.get() );
+        // atom A
+        double thetaA = angle.get() + bondAngleA.get();
+        double xA = PolarCartesianConverter.getX( radius, thetaA ) + location.get().getX();
+        double yA = PolarCartesianConverter.getY( radius, thetaA ) + location.get().getY();
+        atomA.location.set( new ImmutableVector2D( xA, yA ) );
         // atom C
-        double xC = location.get().getX() + BOND_LENGTH;
-        double yC = location.get().getY();
+        double thetaC = angle.get() + bondAngleC.get();
+        double xC = PolarCartesianConverter.getX( radius, thetaC ) + location.get().getX();
+        double yC = PolarCartesianConverter.getY( radius, thetaC ) + location.get().getY();
         atomC.location.set( new ImmutableVector2D( xC, yC ) );
     }
 }
