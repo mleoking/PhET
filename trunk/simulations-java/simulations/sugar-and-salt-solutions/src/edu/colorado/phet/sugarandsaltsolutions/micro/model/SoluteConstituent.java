1bdd66d10df129a8d4f48b0263a66f1e078b84ec
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-08-15 15:53:04 +0000

    factored out nitrate

--- trunk/simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/micro/model/SoluteConstituent.java
+++ trunk/simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/micro/model/SoluteConstituent.java
@@ -15,7 +15,11 @@ public class SoluteConstituent {
     //Particle concentrations for the dissolved components
     public final CompositeDoubleProperty concentration;
 
+    //Color to display in the bar chart, depends on whether "show charges" has been selected by the user
     public final ObservableProperty<Color> color;
+
+    //DrainData helps to maintain a constant concentration as particles flow out the drain by tracking flow rate and timing
+    //There is one DrainData for each type since they may flow at different rates and have different schedules
     public final DrainData drainData;
 
     public SoluteConstituent( MicroModel model, ObservableProperty<Color> color, Class<? extends Particle> type ) {
