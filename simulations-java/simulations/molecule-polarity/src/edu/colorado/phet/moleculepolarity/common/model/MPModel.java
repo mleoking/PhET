ad8fabf7a5cea37445705bae30b8c6699f754f1f
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-08-16 22:26:28 +0000

    general alignment of molecular dipole with E-field so that it's handled in the base class model

--- simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/common/model/MPModel.java
+++ simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/common/model/MPModel.java
@@ -1,7 +1,9 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.moleculepolarity.common.model;
 
+import edu.colorado.phet.common.phetcommon.math.Function.LinearFunction;
 import edu.colorado.phet.common.phetcommon.model.Resettable;
+import edu.colorado.phet.moleculepolarity.MPConstants;
 
 /**
  * Base class for models in this sim.
@@ -14,6 +16,46 @@ public abstract class MPModel implements Resettable {
 
     public void reset() {
         eField.reset();
+    }
+
+    /*
+     * Rotate the molecule one step towards alignment of the molecular dipole with the E-field.
+     * Angular velocity is proportional to the dipole's magnitude.
+     */
+    protected void updateMoleculeOrientation( IMolecule molecule ) {
+
+        // magnitude of angular velocity is proportional to molecular dipole magnitude
+        LinearFunction angularVelocityFunction = new LinearFunction( 0, MPConstants.ELECTRONEGATIVITY_RANGE.getLength(), 0, Math.toRadians( 10 ) );
+        final double deltaDipoleAngle = Math.abs( angularVelocityFunction.evaluate( molecule.getDipole().getMagnitude() ) );
+
+        // convert angle to range [0,2*PI)
+        final double dipoleAngle = normalizeAngle( molecule.getDipole().getAngle() );
+
+        // move the molecular dipole one step towards alignment with the E-field
+        double newDipoleAngle = dipoleAngle;
+        if ( dipoleAngle == 0 ) {
+            // do nothing, molecule is aligned with E-field
+        }
+        else if ( dipoleAngle > 0 && dipoleAngle < Math.PI ) {
+            // rotate counterclockwise
+            newDipoleAngle = dipoleAngle - deltaDipoleAngle;
+            if ( newDipoleAngle < 0 ) {
+                // new angle would overshoot, set to zero
+                newDipoleAngle = 0;
+            }
+        }
+        else {
+            // rotate clockwise
+            newDipoleAngle = dipoleAngle + deltaDipoleAngle;
+            if ( newDipoleAngle > 2 * Math.PI ) {
+                // new angle would overshoot, set to zero
+                newDipoleAngle = 0;
+            }
+        }
+
+        // convert dipole rotation to molecule rotation
+        double deltaMoleculeAngle = newDipoleAngle - dipoleAngle;
+        molecule.setAngle( molecule.getAngle() + deltaMoleculeAngle );
     }
 
     // converts an angle to range [0,2*PI) radians
--- simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/threeatoms/ThreeAtomsModel.java
+++ simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/threeatoms/ThreeAtomsModel.java
@@ -1,12 +1,10 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.moleculepolarity.threeatoms;
 
-import edu.colorado.phet.common.phetcommon.math.Function.LinearFunction;
 import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
 import edu.colorado.phet.common.phetcommon.model.clock.ClockAdapter;
 import edu.colorado.phet.common.phetcommon.model.clock.ClockEvent;
 import edu.colorado.phet.common.phetcommon.model.clock.IClock;
-import edu.colorado.phet.moleculepolarity.MPConstants;
 import edu.colorado.phet.moleculepolarity.common.model.MPModel;
 import edu.colorado.phet.moleculepolarity.common.model.TriatomicMolecule;
 
@@ -27,7 +25,7 @@ public class ThreeAtomsModel extends MPModel {
             public void clockTicked( ClockEvent clockEvent ) {
                 if ( eField.enabled.get() && !molecule.isDragging() ) {
                     // if the E-field is on and the user isn't controlling the molecule's orientation...
-                    updateMoleculeOrientation();
+                    updateMoleculeOrientation( molecule );
                 }
             }
         } );
@@ -36,45 +34,5 @@ public class ThreeAtomsModel extends MPModel {
     @Override public void reset() {
         super.reset();
         molecule.reset();
-    }
-
-    /*
-     * Rotate the molecule one step towards alignment with the E-field.
-     * Angular velocity proportional to the different in electronegativity between the 2 atoms.
-     */
-    private void updateMoleculeOrientation() {
-
-        // magnitude of angular velocity is proportional to molecular dipole magnitude
-        LinearFunction angularVelocityFunction = new LinearFunction( 0, MPConstants.ELECTRONEGATIVITY_RANGE.getLength(), 0, Math.toRadians( 10 ) );
-        final double deltaDipoleAngle = Math.abs( angularVelocityFunction.evaluate( molecule.dipole.get().getMagnitude() ) );
-
-        // convert angle to range [0,2*PI)
-        final double dipoleAngle = normalizeAngle( molecule.dipole.get().getAngle() );
-
-        // move the molecular dipole one step towards alignment with the E-field
-        double newDipoleAngle = dipoleAngle;
-        if ( dipoleAngle == 0 ) {
-            // do nothing, molecule is aligned with E-field
-        }
-        else if ( dipoleAngle > 0 && dipoleAngle < Math.PI ) {
-            // rotate counterclockwise
-            newDipoleAngle = dipoleAngle - deltaDipoleAngle;
-            if ( newDipoleAngle < 0 ) {
-                // new angle would overshoot, set to zero
-                newDipoleAngle = 0;
-            }
-        }
-        else {
-            // rotate clockwise
-            newDipoleAngle = dipoleAngle + deltaDipoleAngle;
-            if ( newDipoleAngle > 2 * Math.PI ) {
-                // new angle would overshoot, set to zero
-                newDipoleAngle = 0;
-            }
-        }
-
-        // convert dipole rotation to molecule rotation
-        double deltaMoleculeAngle = newDipoleAngle - dipoleAngle;
-        molecule.setAngle( molecule.getAngle() + deltaMoleculeAngle );
     }
 }
