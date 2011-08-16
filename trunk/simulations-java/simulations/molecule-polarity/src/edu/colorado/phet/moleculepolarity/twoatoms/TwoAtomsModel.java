5116ef74198712be3981bcb03dd9b7cf58683a52
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-08-16 22:26:28 +0000

    general alignment of molecular dipole with E-field so that it's handled in the base class model

--- trunk/simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/twoatoms/TwoAtomsModel.java
+++ trunk/simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/twoatoms/TwoAtomsModel.java
@@ -1,12 +1,10 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.moleculepolarity.twoatoms;
 
-import edu.colorado.phet.common.phetcommon.math.Function.LinearFunction;
 import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
 import edu.colorado.phet.common.phetcommon.model.clock.ClockAdapter;
 import edu.colorado.phet.common.phetcommon.model.clock.ClockEvent;
 import edu.colorado.phet.common.phetcommon.model.clock.IClock;
-import edu.colorado.phet.moleculepolarity.MPConstants;
 import edu.colorado.phet.moleculepolarity.common.model.DiatomicMolecule;
 import edu.colorado.phet.moleculepolarity.common.model.MPModel;
 
@@ -27,7 +25,7 @@ public class TwoAtomsModel extends MPModel {
             public void clockTicked( ClockEvent clockEvent ) {
                 if ( eField.enabled.get() && !molecule.isDragging() ) {
                     // if the E-field is on and the user isn't controlling the molecule's orientation...
-                    updateMoleculeOrientation();
+                    updateMoleculeOrientation( molecule );
                 }
             }
         } );
@@ -36,61 +34,5 @@ public class TwoAtomsModel extends MPModel {
     @Override public void reset() {
         super.reset();
         molecule.reset();
-    }
-
-    /*
-     * Rotate the molecule one step towards alignment with the E-field.
-     * Angular velocity proportional to the different in electronegativity between the 2 atoms.
-     */
-    private void updateMoleculeOrientation() {
-
-        // magnitude of angular velocity is proportional to molecular dipole magnitude
-        final double deltaEN = molecule.bond.deltaElectronegativity.get();
-        LinearFunction enToAngle = new LinearFunction( 0, MPConstants.ELECTRONEGATIVITY_RANGE.getLength(), 0, Math.toRadians( 10 ) );
-        final double deltaAngle = Math.abs( enToAngle.evaluate( deltaEN ) );
-
-        // convert angle to range [0,2*PI)
-        final double angle = normalizeAngle( molecule.getAngle() );
-
-        //TODO simplify, this seems unnecessarily complicated
-        // move the molecule one step towards alignment with the E-field
-        double newAngle = angle;
-        if ( deltaEN > 0 ) {
-            // dipole points from atomA to atomB, rotate toward angle=0
-            if ( angle == 0 ) {
-                // do nothing, molecule is aligned with E-field
-            }
-            else if ( angle > 0 && angle < Math.PI ) {
-                newAngle = angle - deltaAngle;
-                if ( newAngle < 0 ) {
-                    newAngle = 0;
-                }
-            }
-            else {
-                newAngle = angle + deltaAngle;
-                if ( newAngle > 2 * Math.PI ) {
-                    newAngle = 0;
-                }
-            }
-        }
-        else {
-            // dipole points from atomB to atomA, rotate toward angle=PI
-            if ( angle == Math.PI ) {
-                // do nothing, molecule is aligned with E-field
-            }
-            else if ( angle > 0 && angle < Math.PI ) {
-                newAngle = angle + deltaAngle;
-                if ( newAngle > Math.PI ) {
-                    newAngle = Math.PI;
-                }
-            }
-            else {
-                newAngle = angle - deltaAngle;
-                if ( newAngle < Math.PI && newAngle > 0 ) {
-                    newAngle = Math.PI;
-                }
-            }
-        }
-        molecule.setAngle( newAngle );
     }
 }
