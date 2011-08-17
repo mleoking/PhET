c928b157ef5f975c1c2aca7e43c1ba9ecebf6219
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-08-17 03:12:27 +0000

    factored out model updater

--- team/reids/scenic/src/org/reid/scenic/TestScenicPanel.java
+++ team/reids/scenic/src/org/reid/scenic/TestScenicPanel.java
@@ -51,20 +51,7 @@ public class TestScenicPanel {
                     pack();
                     new Thread( new Runnable() {
                         public void run() {
-                            recurse( scenicPanel, new Function1<Model, Model>() {
-                                         public Model apply( final Model model ) {
-                                             final double dt = 0.05;
-                                             final ImmutableVector2D force = new ImmutableVector2D( 0, 9.8 );
-
-                                             return new Model( model.atoms.map( new Function1<Atom, Atom>() {
-                                                 public Atom apply( Atom atom ) {
-                                                     //v = v0 + at, a = f/m, v = v0+ft/m
-                                                     ImmutableVector2D velocity = atom.velocity.plus( force.times( dt / atom.mass ) );
-                                                     return new Atom( atom.position.plus( atom.velocity.times( dt ) ), atom.position.getY() < MAX_Y ? velocity : new ImmutableVector2D( velocity.getX(), -Math.abs( velocity.getY() ) ), atom.mass );
-                                                 }
-                                             } ) );
-                                         }
-                                     }, new Model( new ImmutableList<Atom>( createAtoms() ) ) );
+                            recurse( scenicPanel, new ModelUpdater(), new Model( new ImmutableList<Atom>( createAtoms() ) ) );
                         }
                     } ).start();
                 }}.setVisible( true );
