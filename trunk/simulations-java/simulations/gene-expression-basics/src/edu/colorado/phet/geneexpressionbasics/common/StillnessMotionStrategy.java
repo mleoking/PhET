aa640721a8b8bb92eb1a9fb33a104a2ccff91392
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-08-18 17:10:21 +0000

    Added motion strategy interface and "stillness" strategy.

---
+++ trunk/simulations-java/simulations/gene-expression-basics/src/edu/colorado/phet/geneexpressionbasics/common/StillnessMotionStrategy.java
@@ -0,0 +1,16 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.geneexpressionbasics.common;
+
+import java.awt.geom.Point2D;
+
+/**
+ * Motion strategy that has no motion, i.e. causes the user to be still.
+ *
+ * @author John Blanco
+ */
+public class StillnessMotionStrategy implements IMotionStrategy {
+
+    public Point2D getNextLocation( double dt, Point2D currentLocation ) {
+        return new Point2D.Double( currentLocation.getX(), currentLocation.getY() );
+    }
+}
