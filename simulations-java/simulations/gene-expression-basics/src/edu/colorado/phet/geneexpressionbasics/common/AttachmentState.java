a6b48de1cf40866b372ea5aec7c7a1db8bfd69ff
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-08-18 19:15:03 +0000

    Added this enum for tracking attachment states for biomolecules.

---
+++ simulations-java/simulations/gene-expression-basics/src/edu/colorado/phet/geneexpressionbasics/common/AttachmentState.java
@@ -0,0 +1,15 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.geneexpressionbasics.common;
+
+/**
+ * This enum defines the possible states of an attachment that exists between
+ * two model elements.
+ *
+ * @author John Blanco
+ */
+public enum AttachmentState {
+    UNATTACHED_AND_AVAILABLE,
+    MOVING_TOWARDS_ATTACHMENT,
+    ATTACHED,
+    UNATTACHED_BUT_UNAVAILABLE
+}
