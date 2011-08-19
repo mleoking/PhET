302595378c5a7ff7a56a57f32d4c1582111e029e
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-08-19 17:30:25 +0000

    8/19/11 add titles to surface color keys, fix black outline

--- simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/common/view/SurfaceColorKeyNode.java
+++ simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/common/view/SurfaceColorKeyNode.java
@@ -6,6 +6,7 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GradientPaint;
 import java.awt.Paint;
+import java.awt.Shape;
 import java.awt.geom.Rectangle2D;
 
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
@@ -22,16 +23,17 @@ import edu.umd.cs.piccolox.nodes.PComposite;
  */
 public abstract class SurfaceColorKeyNode extends PComposite {
 
-    private static final Dimension SIZE = new Dimension( 400, 25 );
-    private static final Font FONT = new PhetFont( 16 );
-    private static final double Y_SPACING = 5;
-    private static final double Y_INSET = 5;
+    private static final Dimension SIZE = new Dimension( 400, 20 );
+    private static final Font TITLE_FONT = new PhetFont( Font.BOLD, 14 );
+    private static final Font RANGE_FONT = new PhetFont( 12 );
+    private static final double Y_SPACING = 3;
+    private static final double X_INSET = 5;
 
     // Color key for "electron density" surface representation.
     public static class ElectronDensityColorKeyNode extends SurfaceColorKeyNode {
         public ElectronDensityColorKeyNode() {
             super( new Color[] { Color.WHITE, Color.BLACK },
-                   MPStrings.LESS, MPStrings.MORE );
+                   MPStrings.ELECTRON_DENSITY, MPStrings.LESS, MPStrings.MORE );
         }
     }
 
@@ -39,7 +41,7 @@ public abstract class SurfaceColorKeyNode extends PComposite {
     public static class ElectrostaticPotentialColorKeyNode extends SurfaceColorKeyNode {
         public ElectrostaticPotentialColorKeyNode() {
             super( new Color[] { Color.BLUE, Color.WHITE, Color.RED },
-                   MPStrings.POSITIVE, MPStrings.NEGATIVE );
+                   MPStrings.ELECTROSTATIC_POTENTIAL, MPStrings.POSITIVE, MPStrings.NEGATIVE );
         }
     }
 
@@ -48,7 +50,7 @@ public abstract class SurfaceColorKeyNode extends PComposite {
     public static class RainbowElectrostaticPotentialColorKeyNode extends SurfaceColorKeyNode {
         public RainbowElectrostaticPotentialColorKeyNode() {
             super( new Color[] { Color.RED, Color.ORANGE, Color.YELLOW, Color.GREEN, Color.BLUE, VisibleColor.INDIGO, VisibleColor.VIOLET },
-                   "Positive", "Negative" );//TODO i18n
+                   MPStrings.ELECTROSTATIC_POTENTIAL, MPStrings.POSITIVE, MPStrings.NEGATIVE );
         }
     }
 
@@ -59,12 +61,15 @@ public abstract class SurfaceColorKeyNode extends PComposite {
      * @param leftLabel
      * @param rightLabel
      */
-    public SurfaceColorKeyNode( Color[] colors, String leftLabel, String rightLabel ) {
+    public SurfaceColorKeyNode( Color[] colors, String title, String leftLabel, String rightLabel ) {
 
         final double segmentWidth = SIZE.width / (double) ( colors.length - 1 );
 
         // spectrum, composed of multiple PPath's because Java 1.5 doesn't include LinearGradientPaint
-        PPath spectrumNode = new PPath( new Rectangle2D.Double( 0, 0, SIZE.width, SIZE.height ) );
+        final Shape spectrumShape = new Rectangle2D.Double( 0, 0, SIZE.width, SIZE.height );
+        PPath spectrumNode = new PPath( spectrumShape ) {{
+            setStroke( null );
+        }};
         for ( int i = 0; i < colors.length - 1; i++ ) {
             Color leftColor = colors[i];
             Color rightColor = colors[i + 1];
@@ -76,23 +81,32 @@ public abstract class SurfaceColorKeyNode extends PComposite {
             }} );
         }
 
+        // put an outline on top, because outlining spectrumNode looks incorrect
+        spectrumNode.addChild( new PPath( spectrumShape ) );
+
         // labels
+        PText titleNode = new PText( title ) {{
+            setFont( TITLE_FONT );
+        }};
         PText leftLabelNode = new PText( leftLabel ) {{
-            setFont( FONT );
+            setFont( RANGE_FONT );
         }};
         PText rightLabelNode = new PText( rightLabel ) {{
-            setFont( FONT );
+            setFont( RANGE_FONT );
         }};
 
         // rendering order
         addChild( spectrumNode );
+        addChild( titleNode );
         addChild( leftLabelNode );
         addChild( rightLabelNode );
 
         // layout
-        leftLabelNode.setOffset( spectrumNode.getFullBoundsReference().getMinX() + Y_INSET,
+        spectrumNode.setOffset( 0, titleNode.getFullBoundsReference().getMaxY() + Y_SPACING );
+        titleNode.setOffset( spectrumNode.getFullBoundsReference().getCenterX() - ( titleNode.getFullBoundsReference().getWidth() / 2 ), 0 );
+        leftLabelNode.setOffset( spectrumNode.getFullBoundsReference().getMinX() + X_INSET,
                                  spectrumNode.getFullBoundsReference().getMaxY() + Y_SPACING );
-        rightLabelNode.setOffset( spectrumNode.getFullBoundsReference().getMaxX() - rightLabelNode.getFullBoundsReference().getWidth() - Y_INSET,
+        rightLabelNode.setOffset( spectrumNode.getFullBoundsReference().getMaxX() - rightLabelNode.getFullBoundsReference().getWidth() - X_INSET,
                                   spectrumNode.getFullBoundsReference().getMaxY() + Y_SPACING );
     }
 }
