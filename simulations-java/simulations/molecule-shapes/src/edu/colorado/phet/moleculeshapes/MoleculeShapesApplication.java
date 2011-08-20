e8119688d51da84f083b796ffb307a52e167dc09
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-08-20 04:12:36 +0000

    ms: potential fix for KP's error

--- simulations-java/simulations/molecule-shapes/src/edu/colorado/phet/moleculeshapes/MoleculeShapesApplication.java
+++ simulations-java/simulations/molecule-shapes/src/edu/colorado/phet/moleculeshapes/MoleculeShapesApplication.java
@@ -5,6 +5,7 @@ package edu.colorado.phet.moleculeshapes;
 
 import java.awt.*;
 import java.io.File;
+import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
