17bba38bfb2b18cc22c0c8865d37043975ed25b4
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-08-22 16:17:16 +0000

    delete dead code

--- simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/MPColors.java
+++ simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/MPColors.java
@@ -16,5 +16,5 @@ public class MPColors {
 
     public static final Color PLATE_NEGATIVE_COLOR = new Color( 255, 255, 153 ); // pale yellow
     public static final Color PLATE_POSITIVE_COLOR = PLATE_NEGATIVE_COLOR;
-    public static final Color PLATE_DISABLED_COLOR = Color.LIGHT_GRAY;//new Color( 120, 120, 120 );
+    public static final Color PLATE_DISABLED_COLOR = Color.LIGHT_GRAY;
 }
