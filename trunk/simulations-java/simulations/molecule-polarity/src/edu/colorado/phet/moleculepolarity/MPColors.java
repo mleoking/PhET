ac103b1cfc5030658de85a24ee22683f4b55dc12
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-08-22 16:56:04 +0000

    8/21/11 change request: try no color change on the E-field plates when turned on

--- trunk/simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/MPColors.java
+++ trunk/simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/MPColors.java
@@ -14,7 +14,7 @@ public class MPColors {
     public static final Color ATOM_B = Color.GREEN;
     public static final Color ATOM_C = Color.PINK;
 
-    public static final Color PLATE_NEGATIVE_COLOR = new Color( 255, 255, 153 ); // pale yellow
-    public static final Color PLATE_POSITIVE_COLOR = PLATE_NEGATIVE_COLOR;
     public static final Color PLATE_DISABLED_COLOR = Color.LIGHT_GRAY;
+    public static final Color PLATE_NEGATIVE_COLOR = PLATE_DISABLED_COLOR;//new Color( 255, 255, 153 ); // pale yellow
+    public static final Color PLATE_POSITIVE_COLOR = PLATE_NEGATIVE_COLOR;
 }
