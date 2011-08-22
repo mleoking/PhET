286acf6c213ef3247fd35ee41ff1989f4f2313d8
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-08-22 04:15:46 +0000

    8/21/11 restore titles with smaller non-bold font

--- trunk/simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/common/view/ElectronegativityTableNode.java
+++ trunk/simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/common/view/ElectronegativityTableNode.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
+import edu.colorado.phet.moleculepolarity.MPStrings;
 import edu.colorado.phet.moleculepolarity.common.view.JmolViewerNode.ElementColor;
 import edu.umd.cs.piccolo.nodes.PPath;
 import edu.umd.cs.piccolo.nodes.PText;
@@ -28,6 +29,11 @@ public class ElectronegativityTableNode extends PComposite {
 
     public ElectronegativityTableNode( final JmolViewerNode viewerNode ) {
 
+        PText titleNode = new PText( MPStrings.ATOM_ELECTRONEGATIVITIES ) {{
+            setFont( new PhetFont( 12 ) );
+        }};
+        addChild( titleNode );
+
         cells = new ArrayList<Cell>() {{
             add( new Cell( "H", 1, 2.1 ) );
             add( new Cell( "B", 5, 2.0 ) );
@@ -41,7 +47,7 @@ public class ElectronegativityTableNode extends PComposite {
         // layout cells, first and last cells are horizontally separated from others
         final double xGap = 12;
         double x = 0;
-        final double y = 0;
+        final double y = titleNode.getFullBoundsReference().getHeight() + 3;
         Cell firstCell = cells.get( 0 );
         addChild( firstCell );
         firstCell.setOffset( x, y );
@@ -56,6 +62,9 @@ public class ElectronegativityTableNode extends PComposite {
         Cell lastCell = cells.get( cells.size() - 1 );
         addChild( lastCell );
         lastCell.setOffset( x, y );
+
+        // center title above cells
+        titleNode.setOffset( ( lastCell.getFullBoundsReference().getMaxX() - firstCell.getFullBoundsReference().getMinX() ) / 2 - ( titleNode.getFullBoundsReference().getWidth() / 2 ), 0 );
 
         // when the current molecule changes, ask Jmol for the molecule's elements and colors
         viewerNode.molecule.addObserver( new SimpleObserver() {
