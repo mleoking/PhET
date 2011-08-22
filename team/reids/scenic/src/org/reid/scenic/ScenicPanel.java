de8cd875a9a2bfba73a5067bda03e453b03dd9e1
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-08-22 14:17:41 +0000

    started support for interaction

--- team/reids/scenic/src/org/reid/scenic/ScenicPanel.java
+++ team/reids/scenic/src/org/reid/scenic/ScenicPanel.java
@@ -4,9 +4,12 @@ package org.reid.scenic;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JPanel;
 
+import edu.colorado.phet.common.phetcommon.util.function.Function2;
 import edu.colorado.phet.common.phetcommon.util.function.VoidFunction2;
 
 /**
@@ -18,16 +21,25 @@ public class ScenicPanel<T> extends JPanel {
 
     private T model;
     private VoidFunction2<T, Graphics2D> painter;
+    private Function2<T, MouseEvent, T> mousePressHandler;
 
-    public ScenicPanel( VoidFunction2<T, Graphics2D> painter ) {
+    public ScenicPanel( VoidFunction2<T, Graphics2D> painter, Function2<T, MouseEvent, T> mousePressHandler ) {
         this.painter = painter;
+        this.mousePressHandler = mousePressHandler;
+        addMouseListener( new MouseAdapter() {
+            @Override public void mousePressed( MouseEvent e ) {
+                model = ScenicPanel.this.mousePressHandler.apply( model, e );
+            }
+        } );
     }
 
     @Override protected void paintComponent( Graphics g ) {
         super.paintComponent( g );    //To change body of overridden methods use File | Settings | File Templates.
         Graphics2D g2 = (Graphics2D) g;
         g2.setRenderingHint( RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON );
-        painter.apply( model, g2 );
+        if ( model != null ) {
+            painter.apply( model, g2 );
+        }
     }
 
     public void setModel( T model ) {
--- team/reids/scenic/src/org/reid/scenic/view/ButtonView.java
+++ team/reids/scenic/src/org/reid/scenic/view/ButtonView.java
@@ -1,8 +1,11 @@
 // Copyright 2002-2011, University of Colorado
 package org.reid.scenic.view;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.RoundRectangle2D;
 
 import org.reid.scenic.model.ButtonModel;
 
@@ -17,8 +20,14 @@ public class ButtonView {
     }
 
     public void paint( Graphics2D graphics2D ) {
-        graphics2D.setFont( buttonModel.getFont() );
+        graphics2D.setFont( buttonModel.font );
+        Rectangle2D bounds = graphics2D.getFont().getStringBounds( buttonModel.text, graphics2D.getFontRenderContext() );
+        graphics2D.setStroke( new BasicStroke( 1 ) );
+        graphics2D.setPaint( Color.yellow );
+        double sizeIncrease = 5;
+        graphics2D.fill( new RoundRectangle2D.Double( buttonModel.x - sizeIncrease, buttonModel.y - bounds.getHeight() - sizeIncrease, bounds.getWidth() + sizeIncrease * 2, bounds.getHeight() + sizeIncrease * 2, 10, 10 ) );
+
         graphics2D.setPaint( Color.black );
-        graphics2D.drawString( buttonModel.getText(), 50, 50 );
+        graphics2D.drawString( buttonModel.text, (float) buttonModel.x, (float) buttonModel.y );
     }
 }
