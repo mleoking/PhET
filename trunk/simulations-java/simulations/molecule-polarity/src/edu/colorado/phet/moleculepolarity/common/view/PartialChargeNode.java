9973d0b0b0244f8df4f83daa6d86beab6f3873ed
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-08-23 01:05:33 +0000

    position partial charge opposite molecular dipole for atom B in triatomic molecule

--- trunk/simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/common/view/PartialChargeNode.java
+++ trunk/simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/common/view/PartialChargeNode.java
@@ -5,32 +5,35 @@ import java.awt.Color;
 
 import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
 import edu.colorado.phet.common.phetcommon.util.RichSimpleObserver;
+import edu.colorado.phet.common.phetcommon.util.function.Function0;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.moleculepolarity.MPConstants;
 import edu.colorado.phet.moleculepolarity.MPStrings;
 import edu.colorado.phet.moleculepolarity.common.model.Atom;
 import edu.colorado.phet.moleculepolarity.common.model.Bond;
+import edu.colorado.phet.moleculepolarity.common.model.IMolecule;
 import edu.umd.cs.piccolo.nodes.PText;
 import edu.umd.cs.piccolox.nodes.PComposite;
 
 /**
- * Visual representation of partial charge, a delta symbol followed by either + or -.
- * The symbol is positioned outside the atom, along the bond axis.
+ * Base class for the visual representations of partial charge, a delta symbol followed by either + or -.
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
-public class PartialChargeNode extends PComposite {
+public abstract class PartialChargeNode extends PComposite {
 
     private static final double REF_MAGNITUDE = MPConstants.ELECTRONEGATIVITY_RANGE.getLength();
     private static final double REF_SCALE = 1;
 
+    protected final RichSimpleObserver observer;
+
     /**
-     * Constructor
+     * Constructor.
      *
-     * @param atom atom contains the partial charge info
-     * @param bond bond is needed for layout
+     * @param atom               the atom whose partial charge is displayed
+     * @param unitVectorFunction a function that determines where the charge is placed along the outside edge of the atom
      */
-    public PartialChargeNode( final Atom atom, final Bond bond ) {
+    public PartialChargeNode( final Atom atom, final Function0<ImmutableVector2D> unitVectorFunction ) {
 
         final PText textNode = new PText() {{
             setFont( new PhetFont( 32 ) );
@@ -38,7 +41,7 @@ public class PartialChargeNode extends PComposite {
         }};
         addChild( textNode );
 
-        RichSimpleObserver observer = new RichSimpleObserver() {
+        observer = new RichSimpleObserver() {
             public void update() {
 
                 final double partialCharge = atom.partialCharge.get();
@@ -62,16 +65,56 @@ public class PartialChargeNode extends PComposite {
                         textNode.setOffset( -textNode.getFullBoundsReference().getWidth() / 2, -textNode.getFullBoundsReference().getHeight() / 2 ); // origin at center
                     }
 
-                    // A vector that points in the direction we will need to move the charge node (away from the associated atom)
-                    ImmutableVector2D unitVectorFromBond = new ImmutableVector2D( bond.getCenter(), atom.location.get() ).getNormalizedInstance();
+                    // A vector that points in the direction we will need to move the charge node.
+                    ImmutableVector2D unitVector = unitVectorFunction.apply();
 
                     // Compute the amount to move the partial charge node
                     double multiplier = ( atom.getDiameter() / 2 ) + ( Math.max( getFullBoundsReference().getWidth(), getFullBoundsReference().getHeight() ) / 2 ) + 3;
-                    ImmutableVector2D relativeOffset = unitVectorFromBond.times( multiplier );
+                    ImmutableVector2D relativeOffset = unitVector.times( multiplier );
                     setOffset( atom.location.get().plus( relativeOffset ).toPoint2D() );
                 }
             }
         };
         observer.observe( atom.partialCharge, atom.location );
+    }
+
+    /*
+     * Partial charge for an atom that participates in a single bond.
+     * It's partial charge is the opposite of the charge of the other atom in the bond.
+     * The charge is placed along the axis of the bond, away from the atom.
+     */
+    public static class OppositePartialChargeNode extends PartialChargeNode {
+        public OppositePartialChargeNode( final Atom atom, final Bond bond ) {
+            super( atom, new Function0<ImmutableVector2D>() {
+                public ImmutableVector2D apply() {
+                    // along the bond axis, in the direction of the atom
+                    return new ImmutableVector2D( bond.getCenter(), atom.location.get() ).getNormalizedInstance();
+                }
+            } );
+        }
+    }
+
+    /*
+     * Partial charge for an atom that participates in more than one bond.
+     * It's partial charge is the composite of charges contributed by other atoms in the bonds.
+     * The charge is placed along the axis of the molecular dipole, on the opposite side of the atom from the dipole.
+     */
+    public static class CompositePartialChargeNode extends PartialChargeNode {
+        public CompositePartialChargeNode( final Atom atom, final IMolecule molecule ) {
+            super( atom, new Function0<ImmutableVector2D>() {
+                public ImmutableVector2D apply() {
+                    ImmutableVector2D normalVector;
+                    if ( molecule.getDipole().getMagnitude() > 0 ) {
+                        normalVector = molecule.getDipole().getRotatedInstance( Math.PI ).getNormalizedInstance();
+                    }
+                    else {
+                        // can't normalize a zero-magnitude vector, so create our own with the proper angle
+                        normalVector = new ImmutableVector2D( 1, molecule.getDipole().getAngle() );
+                    }
+                    return normalVector;
+                }
+            } );
+            molecule.addDipoleObserver( observer );
+        }
     }
 }
