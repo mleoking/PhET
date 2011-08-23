867dddaf75bd1b28e52d5c1052c0f783ba47f252
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-08-23 00:07:15 +0000

    improved interaction

--- team/reids/scenic/src/org/reid/scenic/view/ButtonView.java
+++ team/reids/scenic/src/org/reid/scenic/view/ButtonView.java
@@ -4,6 +4,7 @@ package org.reid.scenic.view;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.font.FontRenderContext;
 import java.awt.geom.Rectangle2D;
 import java.awt.geom.RoundRectangle2D;
 
@@ -21,13 +22,26 @@ public class ButtonView {
 
     public void paint( Graphics2D graphics2D ) {
         graphics2D.setFont( buttonModel.font );
-        Rectangle2D bounds = graphics2D.getFont().getStringBounds( buttonModel.text, graphics2D.getFontRenderContext() );
         graphics2D.setStroke( new BasicStroke( 1 ) );
-        graphics2D.setPaint( Color.yellow );
-        double sizeIncrease = 5;
-        graphics2D.fill( new RoundRectangle2D.Double( buttonModel.x - sizeIncrease, buttonModel.y - bounds.getHeight() - sizeIncrease, bounds.getWidth() + sizeIncrease * 2, bounds.getHeight() + sizeIncrease * 2, 10, 10 ) );
+        graphics2D.setPaint( buttonModel.pressed ? Color.red : Color.yellow );
+        final RoundRectangle2D.Double outerRoundRect = getOuterRoundRect();
+        graphics2D.fill( outerRoundRect );
 
         graphics2D.setPaint( Color.black );
         graphics2D.drawString( buttonModel.text, (float) buttonModel.x, (float) buttonModel.y );
+    }
+
+    class MyFontRenderContext extends FontRenderContext {
+    }
+
+    private RoundRectangle2D.Double getOuterRoundRect() {
+        double sizeIncrease = 5;
+        FontRenderContext fontRenderContext = new MyFontRenderContext();
+        Rectangle2D bounds = buttonModel.font.getStringBounds( buttonModel.text, fontRenderContext );
+        return new RoundRectangle2D.Double( buttonModel.x - sizeIncrease, buttonModel.y - bounds.getHeight() - sizeIncrease, bounds.getWidth() + sizeIncrease * 2, bounds.getHeight() + sizeIncrease * 2, 10, 10 );
+    }
+
+    public boolean contains( int x, int y ) {
+        return getOuterRoundRect().contains( x, y );
     }
 }
