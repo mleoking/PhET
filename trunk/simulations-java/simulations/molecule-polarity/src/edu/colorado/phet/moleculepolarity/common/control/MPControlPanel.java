c6955d4c72b4eea4934fb3a46c3e0114fc2f864b
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-08-23 20:11:34 +0000

    update

--- trunk/simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/common/control/MPControlPanel.java
+++ trunk/simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/common/control/MPControlPanel.java
@@ -1,9 +1,10 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.moleculepolarity.common.control;
 
-import java.awt.*;
+import java.awt.Font;
 
-import javax.swing.*;
+import javax.swing.Box;
+import javax.swing.JLabel;
 
 import edu.colorado.phet.common.phetcommon.view.util.GridPanel;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
@@ -18,9 +19,13 @@ public abstract class MPControlPanel extends GridPanel {
     public MPControlPanel( String title ) {
         setGridX( 0 ); // vertical
         setAnchor( Anchor.WEST ); // left justified
+
+        // title
         add( new JLabel( title ) {{
             setFont( new PhetFont( Font.BOLD, 16 ) );
         }} );
+
+        // space between title and controls below it
         add( Box.createVerticalStrut( 5 ) );
     }
 }
