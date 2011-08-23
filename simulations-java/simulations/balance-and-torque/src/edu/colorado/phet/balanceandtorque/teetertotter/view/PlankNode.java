9bbbd702205e3884696754610b7fa4aaff0dc3aa
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-08-23 17:32:20 +0000

    Added handle nodes, which will eventually be invisible, for allowing the user to grab and move the plank.  This is just the handle, and the functionality doesn't work yet.

--- simulations-java/simulations/balance-and-torque/src/edu/colorado/phet/balanceandtorque/teetertotter/view/PlankNode.java
+++ simulations-java/simulations/balance-and-torque/src/edu/colorado/phet/balanceandtorque/teetertotter/view/PlankNode.java
@@ -3,19 +3,23 @@ package edu.colorado.phet.balanceandtorque.teetertotter.view;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.Shape;
 import java.awt.Stroke;
+import java.awt.geom.Rectangle2D;
 
 import edu.colorado.phet.balanceandtorque.teetertotter.model.Plank;
 import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
+import edu.colorado.phet.common.piccolophet.event.CursorHandler;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.umd.cs.piccolo.PNode;
 
 /**
- * Graphic for the fulcrum, a triangle that the plank pivots about.
+ * Graphic for the plank.  Draws tick marks and provides handler for grabbing
+ * the plank and setting its position.
  *
- * @author Sam Reid
+ * @author John Blanco
  */
 public class PlankNode extends ModelObjectNode {
     private static final Stroke NORMAL_TICK_MARK_STROKE = new BasicStroke( 1, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER );
@@ -23,11 +27,14 @@ public class PlankNode extends ModelObjectNode {
 
     public PlankNode( final ModelViewTransform mvt, final Plank plank ) {
         super( mvt, plank, new Color( 243, 203, 127 ) );
+
+        // Create a layer for the tick marks and add the code to create and
+        // update them.
         final PNode tickMarkLayer = new PNode();
         addChild( tickMarkLayer );
         plank.addShapeObserver( new VoidFunction1<Shape>() {
             public void apply( Shape rotatedPlankShape ) {
-                // Remove existing tick marks.
+                // Update the tick marks by removing them and redrawing them.
                 tickMarkLayer.removeAllChildren();
                 // Add the tick marks.  The spacing should match that of the
                 // plank's "snap to" locations.  The marks are created based
@@ -45,6 +52,41 @@ public class PlankNode extends ModelObjectNode {
                         tickMarkLayer.addChild( new PhetPPath( mvt.modelToView( plank.getTickMarks().get( i ) ), NORMAL_TICK_MARK_STROKE, Color.BLACK ) );
                     }
                 }
+            }
+        } );
+
+        // Create a layer for some grabbable "handles" that will allow the
+        // user to manually change the angle of the plank.
+        assert plank.getTiltAngle() == 0; // Tilt angle be zero for this to work.
+        final PNode handleLayer = new PNode();
+        addChild( handleLayer );
+        // Only put the handles on the ends of the plank, otherwise things get
+        // weird.  Note that the handles are invisible.
+        Color handleColor = Color.pink;
+        Rectangle2D plankBounds = plank.getShape().getBounds2D();
+        final PNode rightHandle = new PhetPPath( new Rectangle2D.Double( mvt.modelToViewDeltaX( plankBounds.getWidth() / 4 ),
+                                                                         -mvt.modelToViewDeltaY( plankBounds.getHeight() ),
+                                                                         mvt.modelToViewDeltaX( plankBounds.getWidth() / 4 ),
+                                                                         -mvt.modelToViewDeltaY( plankBounds.getHeight() ) ),
+                                                 handleColor );
+        rightHandle.setOffset( mvt.modelToView( plank.getPivotPoint() ) );
+        rightHandle.addInputEventListener( new CursorHandler( Cursor.N_RESIZE_CURSOR ) );
+        handleLayer.addChild( rightHandle );
+
+        final PNode leftHandle = new PhetPPath( new Rectangle2D.Double( -mvt.modelToViewDeltaX( plankBounds.getWidth() / 2 ),
+                                                                        -mvt.modelToViewDeltaY( plankBounds.getHeight() ),
+                                                                        mvt.modelToViewDeltaX( plankBounds.getWidth() / 4 ),
+                                                                        -mvt.modelToViewDeltaY( plankBounds.getHeight() ) ),
+                                                handleColor );
+        leftHandle.setOffset( mvt.modelToView( plank.getPivotPoint() ) );
+        leftHandle.addInputEventListener( new CursorHandler( Cursor.N_RESIZE_CURSOR ) );
+        handleLayer.addChild( leftHandle );
+
+        plank.addShapeObserver( new VoidFunction1<Shape>() {
+            public void apply( Shape rotatedPlankShape ) {
+                // Rotate the handles to match the plank's angle.
+                rightHandle.setRotation( -plank.getTiltAngle() );
+                leftHandle.setRotation( -plank.getTiltAngle() );
             }
         } );
     }
