ea686f1899fd794cd3825ecd0eaa23ec09931aac
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-08-23 20:12:12 +0000

    doc

--- simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/common/control/RotateCursorHandler.java
+++ simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/common/control/RotateCursorHandler.java
@@ -15,7 +15,10 @@ import edu.colorado.phet.moleculepolarity.MPImages;
  */
 public class RotateCursorHandler extends CursorHandler {
 
+    // hot spot is in the center of the image
     private static final Point HOT_SPOT = new Point( MPImages.ROTATE_CURSOR.getWidth() / 2, MPImages.ROTATE_CURSOR.getHeight() / 2 );
+
+    // the cursor, must be 32x32 for Windows
     public static final Cursor CURSOR = Toolkit.getDefaultToolkit().createCustomCursor( MPImages.ROTATE_CURSOR, HOT_SPOT, "rotate" );
 
     public RotateCursorHandler() {
