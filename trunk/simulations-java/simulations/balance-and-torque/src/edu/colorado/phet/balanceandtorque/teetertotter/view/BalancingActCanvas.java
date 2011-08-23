67a12f93ca97b89845cb3bc34dc602ff4781fdfd
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-08-23 18:24:47 +0000

    First working version of the feature where the user can drag the edges of the plank in order to change the tilt angle.

--- trunk/simulations-java/simulations/balance-and-torque/src/edu/colorado/phet/balanceandtorque/teetertotter/view/BalancingActCanvas.java
+++ trunk/simulations-java/simulations/balance-and-torque/src/edu/colorado/phet/balanceandtorque/teetertotter/view/BalancingActCanvas.java
@@ -114,7 +114,7 @@ public class BalancingActCanvas extends PhetPCanvas {
         // Add graphics for the plank, the fulcrum, the attachment bar, and the columns.
         rootNode.addChild( new FulcrumAbovePlankNode( mvt, model.getFulcrum() ) );
         rootNode.addChild( new AttachmentBarNode( mvt, model.getAttachmentBar() ) );
-        rootNode.addChild( new PlankNode( mvt, model.getPlank() ) );
+        rootNode.addChild( new PlankNode( mvt, model.getPlank(), this ) );
         for ( SupportColumn supportColumn : model.getSupportColumns() ) {
             rootNode.addChild( new SupportColumnNode( mvt, supportColumn, model.supportColumnsActive ) );
         }
--- trunk/simulations-java/simulations/balance-and-torque/src/edu/colorado/phet/balanceandtorque/teetertotter/view/PlankNode.java
+++ trunk/simulations-java/simulations/balance-and-torque/src/edu/colorado/phet/balanceandtorque/teetertotter/view/PlankNode.java
@@ -6,14 +6,18 @@ import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.Shape;
 import java.awt.Stroke;
+import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 
 import edu.colorado.phet.balanceandtorque.teetertotter.model.Plank;
 import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
+import edu.colorado.phet.common.piccolophet.PhetPCanvas;
 import edu.colorado.phet.common.piccolophet.event.CursorHandler;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.event.PDragEventHandler;
+import edu.umd.cs.piccolo.event.PInputEvent;
 
 /**
  * Graphic for the plank.  Draws tick marks and provides handler for grabbing
@@ -25,7 +29,7 @@ public class PlankNode extends ModelObjectNode {
     private static final Stroke NORMAL_TICK_MARK_STROKE = new BasicStroke( 1, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER );
     private static final Stroke BOLD_TICK_MARK_STROKE = new BasicStroke( 3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER );
 
-    public PlankNode( final ModelViewTransform mvt, final Plank plank ) {
+    public PlankNode( final ModelViewTransform mvt, final Plank plank, PhetPCanvas canvas ) {
         super( mvt, plank, new Color( 243, 203, 127 ) );
 
         // Create a layer for the tick marks and add the code to create and
@@ -89,5 +93,54 @@ public class PlankNode extends ModelObjectNode {
                 leftHandle.setRotation( -plank.getTiltAngle() );
             }
         } );
+
+        rightHandle.addInputEventListener( new GrabHandleDragHandler( plank, canvas, mvt ) );
+    }
+
+    private static class GrabHandleDragHandler extends PDragEventHandler {
+        private final Plank plank;
+        private final ModelViewTransform mvt;
+        private final PhetPCanvas canvas;
+        private double dragAngleDelta;
+
+        public GrabHandleDragHandler( Plank plank, PhetPCanvas canvas, ModelViewTransform mvt ) {
+            this.plank = plank;
+            this.canvas = canvas;
+            this.mvt = mvt;
+        }
+
+        @Override protected void startDrag( PInputEvent event ) {
+            super.startDrag( event );
+            // The user wants to move this.
+            plank.userControlled.set( true );
+            Point2D modelStartDragPosition = getModelPosition( event.getCanvasPosition() );
+            dragAngleDelta = plank.getTiltAngle() - Math.atan2( modelStartDragPosition.getY() - plank.getCenterSurfacePoint().getY(),
+                                                                modelStartDragPosition.getX() - plank.getCenterSurfacePoint().getX() );
+        }
+
+        @Override
+        public void mouseDragged( PInputEvent event ) {
+            Point2D modelDragPosition = getModelPosition( event.getCanvasPosition() );
+            double dragAngle = Math.atan2( modelDragPosition.getY() - plank.getCenterSurfacePoint().getY(),
+                                           modelDragPosition.getX() - plank.getCenterSurfacePoint().getX() );
+            plank.setTiltAngle( dragAngle + dragAngleDelta );
+        }
+
+        @Override protected void endDrag( PInputEvent event ) {
+            super.endDrag( event );
+            // The user is no longer controlling this.
+            plank.userControlled.set( false );
+        }
+
+        /**
+         * Convert the canvas position to the corresponding location in the
+         * model.
+         */
+        private Point2D getModelPosition( Point2D canvasPos ) {
+            Point2D worldPos = new Point2D.Double( canvasPos.getX(), canvasPos.getY() );
+            canvas.getPhetRootNode().screenToWorld( worldPos );
+            Point2D modelPos = mvt.viewToModel( worldPos );
+            return modelPos;
+        }
     }
 }
