f04408f8e107d6d8b27e60789fa3dcb089816d46
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-08-25 00:44:03 +0000

    8/24/11 remove bad code from MPCanvas.updateLayout that was interfering with CenteredStage transform strategy 8/24/11 add stage bounds node, for layout debugging

--- trunk/simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/common/view/MPCanvas.java
+++ trunk/simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/common/view/MPCanvas.java
@@ -1,15 +1,13 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.moleculepolarity.common.view;
 
-import java.awt.geom.Dimension2D;
 import java.util.logging.Logger;
 
+import edu.colorado.phet.common.phetcommon.application.PhetApplication;
 import edu.colorado.phet.common.phetcommon.util.logging.LoggingUtils;
 import edu.colorado.phet.common.piccolophet.PhetPCanvas;
-import edu.colorado.phet.common.piccolophet.util.PNodeLayoutUtils;
 import edu.colorado.phet.moleculepolarity.MPConstants;
 import edu.umd.cs.piccolo.PNode;
-import edu.umd.cs.piccolo.util.PBounds;
 
 /**
  * Base class for all canvases.
@@ -19,12 +17,18 @@ import edu.umd.cs.piccolo.util.PBounds;
 public class MPCanvas extends PhetPCanvas {
 
     private static final Logger LOGGER = LoggingUtils.getLogger( MPCanvas.class.getCanonicalName() );
+    private static final boolean SHOW_STAGE_BOUNDS = true && PhetApplication.getInstance().isDeveloperControlsEnabled();
 
     private final PNode rootNode;
 
     protected MPCanvas() {
-        setWorldTransformStrategy( new PhetPCanvas.CenteredStage( this, MPConstants.STAGE_SIZE ) );
+
         setBackground( MPConstants.CANVAS_COLOR );
+        setWorldTransformStrategy( new PhetPCanvas.CenteredStage( this, MPConstants.STAGE_SIZE ) );
+        if ( SHOW_STAGE_BOUNDS ) {
+            addBoundsNode( MPConstants.STAGE_SIZE );
+        }
+
         rootNode = new PNode();
         addWorldChild( rootNode );
     }
@@ -38,29 +42,6 @@ public class MPCanvas extends PhetPCanvas {
     protected void removeChild( PNode node ) {
         if ( node != null && rootNode.indexOfChild( node ) != -1 ) {
             rootNode.removeChild( node );
-        }
-    }
-
-    @Override protected void updateLayout() {
-        super.updateLayout();
-
-        if ( getWidth() > 0 && getHeight() > 0 ) {
-            topCenterRootNode();
-        }
-//        LOGGER.info( "size=" + getSize() + " worldSize=" + getWorldSize() );
-    }
-
-    protected void topCenterRootNode() {
-        topCenterNode( rootNode );
-    }
-
-    protected void topCenterNode( PNode node ) {
-        if ( node != null ) {
-            Dimension2D worldSize = getWorldSize();
-            PBounds b = node.getFullBoundsReference();
-            double xOffset = ( worldSize.getWidth() - b.getWidth() - PNodeLayoutUtils.getOriginXOffset( node ) ) / 2;
-            double yOffset = 0;
-            node.setOffset( xOffset, yOffset );
         }
     }
 }
