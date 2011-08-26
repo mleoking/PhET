3567fbc1fa88c011ed9f424859fa738b0272e384
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-08-26 15:47:46 +0000

    reformatted

--- trunk/simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/micro/model/calciumchloride/CalciumChlorideCrystal.java
+++ trunk/simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/micro/model/calciumchloride/CalciumChlorideCrystal.java
@@ -1,8 +1,6 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.sugarandsaltsolutions.micro.model.calciumchloride;
 
-import java.util.ArrayList;
-
 import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
 import edu.colorado.phet.sugarandsaltsolutions.micro.model.Constituent;
 import edu.colorado.phet.sugarandsaltsolutions.micro.model.Crystal;
