044127b18c2fd30cf13fd2f3d7f8f1e15514f894
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-08-26 21:58:45 +0000

    ms: added experimental Jmol support

--- simulations-java/simulations/molecule-shapes/src/edu/colorado/phet/moleculeshapes/control/MoleculeShapesControlPanel.java
+++ simulations-java/simulations/molecule-shapes/src/edu/colorado/phet/moleculeshapes/control/MoleculeShapesControlPanel.java
@@ -6,12 +6,13 @@ import java.awt.event.MouseEvent;
 import java.awt.geom.Rectangle2D.Double;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 
-import edu.colorado.phet.common.phetcommon.view.controls.PropertyCheckBox;
-import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
-import edu.colorado.phet.common.piccolophet.nodes.ControlPanelNode;
+import org.jmol.api.JmolViewer;
+
+import edu.colorado.phet.common.jmolphet.JmolPanel;
+import edu.colorado.phet.common.jmolphet.Molecule;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
-import edu.colorado.phet.moleculeshapes.MoleculeShapesApplication;
 import edu.colorado.phet.moleculeshapes.MoleculeShapesConstants;
 import edu.colorado.phet.moleculeshapes.MoleculeShapesResources.Images;
 import edu.colorado.phet.moleculeshapes.model.MoleculeModel.Adapter;
@@ -105,6 +106,81 @@ public class MoleculeShapesControlPanel extends PNode {
             setOffset( 0, bondingPanel.getFullBounds().getMaxY() + PANEL_SPACER );
         }};
         addChild( nonBondingPanel );
+
+        /*---------------------------------------------------------------------------*
+        * experimental Jmol panel
+        *----------------------------------------------------------------------------*/
+        final TitledControlPanelNode jmolPanel = new TitledControlPanelNode( new PNode() {{
+            // padding, and make sure we have the width
+            addChild( new PhetPPath( new Double( 0, 0, MoleculeShapesConstants.CONTROL_PANEL_INNER_WIDTH, 10 ), new Color( 0, 0, 0, 0 ) ) );
+
+            addChild( new PSwing( new JTabbedPane() {{
+                setBorder( new EmptyBorder( new Insets( 0, 0, 0, 0 ) ) );
+                setBackground( Color.BLACK );
+                addTab( "Water", new JmolPanel( new Molecule() {
+                    public String getDisplayName() {
+                        return "Water";
+                    }
+
+                    public String getData() {
+                        return "<?xml version=\"1.0\"?>\n" +
+                               "<molecule id=\"id962\" xmlns=\"http://www.xml-cml.org/schema\">\n" +
+                               " <name>962</name>\n" +
+                               " <atomArray>\n" +
+                               "  <atom id=\"a1\" elementType=\"O\" x3=\"0.000000\" y3=\"0.000000\" z3=\"0.000000\"/>\n" +
+                               "  <atom id=\"a2\" elementType=\"H\" x3=\"0.277400\" y3=\"0.892900\" z3=\"0.254400\"/>\n" +
+                               "  <atom id=\"a3\" elementType=\"H\" x3=\"0.606800\" y3=\"-0.238300\" z3=\"-0.716900\"/>\n" +
+                               " </atomArray>\n" +
+                               " <bondArray>\n" +
+                               "  <bond atomRefs2=\"a1 a2\" order=\"1\"/>\n" +
+                               "  <bond atomRefs2=\"a1 a3\" order=\"1\"/>\n" +
+                               " </bondArray>\n" +
+                               "</molecule>";
+                    }
+
+                    public void fixJmolColors( JmolViewer viewer ) {
+                    }
+                }, "Loading..." ) );
+                addTab( "Ethane", new JmolPanel( new Molecule() {
+                    public String getDisplayName() {
+                        return "Ethane";
+                    }
+
+                    public String getData() {
+                        return "<?xml version=\"1.0\"?>\n" +
+                               "<molecule id=\"id6324\" xmlns=\"http://www.xml-cml.org/schema\">\n" +
+                               " <name>6324</name>\n" +
+                               " <atomArray>\n" +
+                               "  <atom id=\"a1\" elementType=\"C\" x3=\"-0.756000\" y3=\"0.000000\" z3=\"0.000000\"/>\n" +
+                               "  <atom id=\"a2\" elementType=\"C\" x3=\"0.756000\" y3=\"0.000000\" z3=\"0.000000\"/>\n" +
+                               "  <atom id=\"a3\" elementType=\"H\" x3=\"-1.140400\" y3=\"0.658600\" z3=\"0.784500\"/>\n" +
+                               "  <atom id=\"a4\" elementType=\"H\" x3=\"-1.140400\" y3=\"0.350100\" z3=\"-0.962600\"/>\n" +
+                               "  <atom id=\"a5\" elementType=\"H\" x3=\"-1.140500\" y3=\"-1.008700\" z3=\"0.178100\"/>\n" +
+                               "  <atom id=\"a6\" elementType=\"H\" x3=\"1.140400\" y3=\"-0.350100\" z3=\"0.962600\"/>\n" +
+                               "  <atom id=\"a7\" elementType=\"H\" x3=\"1.140500\" y3=\"1.008700\" z3=\"-0.178100\"/>\n" +
+                               "  <atom id=\"a8\" elementType=\"H\" x3=\"1.140400\" y3=\"-0.658600\" z3=\"-0.784500\"/>\n" +
+                               " </atomArray>\n" +
+                               " <bondArray>\n" +
+                               "  <bond atomRefs2=\"a1 a2\" order=\"1\"/>\n" +
+                               "  <bond atomRefs2=\"a1 a3\" order=\"1\"/>\n" +
+                               "  <bond atomRefs2=\"a1 a4\" order=\"1\"/>\n" +
+                               "  <bond atomRefs2=\"a1 a5\" order=\"1\"/>\n" +
+                               "  <bond atomRefs2=\"a2 a6\" order=\"1\"/>\n" +
+                               "  <bond atomRefs2=\"a2 a7\" order=\"1\"/>\n" +
+                               "  <bond atomRefs2=\"a2 a8\" order=\"1\"/>\n" +
+                               " </bondArray>\n" +
+                               "</molecule>";
+                    }
+
+                    public void fixJmolColors( JmolViewer viewer ) {
+                    }
+                }, "Loading..." ) );
+                setPreferredSize( new Dimension( (int) MoleculeShapesConstants.CONTROL_PANEL_INNER_WIDTH, (int) MoleculeShapesConstants.CONTROL_PANEL_INNER_WIDTH ) );
+            }} ) );
+        }}, "Real Molecules", Color.BLACK, new BasicStroke( MoleculeShapesConstants.CONTROL_PANEL_BORDER_WIDTH ), MoleculeShapesConstants.CONTROL_PANEL_BORDER_COLOR ) {{
+            setOffset( 0, nonBondingPanel.getFullBounds().getMaxY() + PANEL_SPACER );
+        }};
+        addChild( jmolPanel );
     }
 
     private class BondTypeNode extends PNode {
