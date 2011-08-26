2520b46ee35acc2a6324f3cb56d1029d0fa4235e
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-08-26 15:51:37 +0000

    Changed to get the behavioral state from the biomolecule when the attachment site is reached.

--- simulations-java/simulations/gene-expression-basics/src/edu/colorado/phet/geneexpressionbasics/common/model/MovingTowardsAttachmentState.java
+++ simulations-java/simulations/gene-expression-basics/src/edu/colorado/phet/geneexpressionbasics/common/model/MovingTowardsAttachmentState.java
@@ -4,11 +4,16 @@ package edu.colorado.phet.geneexpressionbasics.common.model;
 import java.util.List;
 
 /**
+ * This state controls the behavior of a biomolecule that is moving towards an
+ * attachment.  In general, this means that the molecule wanders towards the
+ * site and, when it arrives, initiates a state change to the appropriate
+ * "attached" state.
+ *
  * @author John Blanco
  */
 public class MovingTowardsAttachmentState extends BiomoleculeBehaviorState {
 
-    private static final double CAPTURE_DISTANCE = 20; // In picometers.
+    private static final double CAPTURE_DISTANCE = 20; // In picometers, empirically determined.
 
     private IMotionStrategy motionStrategy;
     private final AttachmentSite attachmentSite;
@@ -21,9 +26,10 @@ public class MovingTowardsAttachmentState extends BiomoleculeBehaviorState {
     @Override public BiomoleculeBehaviorState stepInTime( double dt, MobileBiomolecule biomolecule ) {
         biomolecule.setPosition( motionStrategy.getNextLocation( dt, biomolecule.getPosition() ) );
         if ( biomolecule.getPosition().distance( attachmentSite.locationProperty.get() ) < CAPTURE_DISTANCE ) {
-            // The molecule has reached the attachment site.  Time to
-            // transition to the Attached state.
-            return new AttachedState( attachmentSite );
+            // The molecule has reached the attachment site.  Since the
+            // different biomolecules exhibit different behavior once attached,
+            // we have the biomolecule generate its own attached state.
+            return biomolecule.getAttachmentPointReachedState( attachmentSite );
         }
         else {
             return this;
