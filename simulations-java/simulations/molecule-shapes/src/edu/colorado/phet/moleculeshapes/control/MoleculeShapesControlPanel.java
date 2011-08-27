2f06828f4db4addae3f494ca27268c5a2a9b6f1c
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-08-27 03:18:39 +0000

    ms: moved lone-pair toggle button into the main control panel

--- simulations-java/simulations/molecule-shapes/src/edu/colorado/phet/moleculeshapes/control/MoleculeShapesControlPanel.java
+++ simulations-java/simulations/molecule-shapes/src/edu/colorado/phet/moleculeshapes/control/MoleculeShapesControlPanel.java
@@ -2,6 +2,8 @@
 package edu.colorado.phet.moleculeshapes.control;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.geom.Rectangle2D.Double;
 
@@ -12,9 +14,14 @@ import org.jmol.api.JmolViewer;
 
 import edu.colorado.phet.common.jmolphet.JmolPanel;
 import edu.colorado.phet.common.jmolphet.Molecule;
+import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
+import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
+import edu.colorado.phet.common.piccolophet.nodes.TextButtonNode;
 import edu.colorado.phet.moleculeshapes.MoleculeShapesConstants;
 import edu.colorado.phet.moleculeshapes.MoleculeShapesResources.Images;
+import edu.colorado.phet.moleculeshapes.model.MoleculeModel.Adapter;
+import edu.colorado.phet.moleculeshapes.model.PairGroup;
 import edu.colorado.phet.moleculeshapes.view.MoleculeJMEApplication;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
@@ -25,10 +32,7 @@ import edu.umd.cs.piccolox.pswing.PSwing;
 public class MoleculeShapesControlPanel extends PNode {
     private static final double PANEL_SPACER = 20; // space between text and bond lines
 
-    private final MoleculeJMEApplication app;
-
     public MoleculeShapesControlPanel( final MoleculeJMEApplication app ) {
-        this.app = app;
 
         /*---------------------------------------------------------------------------*
         * bonding panel
@@ -91,18 +95,72 @@ public class MoleculeShapesControlPanel extends PNode {
             // padding, and make sure we have the width
             addChild( new PhetPPath( new Double( 0, 0, MoleculeShapesConstants.CONTROL_PANEL_INNER_WIDTH, 10 ), new Color( 0, 0, 0, 0 ) ) );
 
-            addChild( new BondTypeControlNode(
+            final BondTypeControlNode lonePairNode = new BondTypeControlNode(
                     app,
-                    new PImage( Images.LONE_PAIR_SMALL ), 0 ) {{
-                setOffset( 0, 10 );
-                // TODO: refactor the input listener into something more common (code duplication)
-                addDragListener( new PBasicInputEventHandler() {
-                    @Override public void mousePressed( PInputEvent event ) {
-                        if ( enabled && event.getButton() == MouseEvent.BUTTON1 ) {
-                            app.startNewInstanceDrag( 0 );
+                    new PImage( Images.LONE_PAIR_SMALL ), 0 ) {
+                {
+                    setOffset( 0, 10 );
+                    // TODO: refactor the input listener into something more common (code duplication)
+                    addDragListener( new PBasicInputEventHandler() {
+                        @Override public void mousePressed( PInputEvent event ) {
+                            if ( enabled && event.getButton() == MouseEvent.BUTTON1 ) {
+                                app.startNewInstanceDrag( 0 );
+                            }
                         }
-                    }
-                } );
+                    } );
+                    MoleculeJMEApplication.showLonePairs.addObserver( new SimpleObserver() {
+                        public void update() {
+                            updateState();
+                        }
+                    } );
+                }
+
+                @Override protected boolean isEnabled() {
+                    // TODO: note that it looks weird adding a pair when it is invisible
+                    return super.isEnabled();
+//                    return super.isEnabled() && MoleculeJMEApplication.showLonePairs.get();
+                }
+            };
+            addChild( lonePairNode );
+
+            final TextButtonNode toggleLonePairsButton = new TextButtonNode( "<will be replaced>", new PhetFont( 14 ), Color.ORANGE ) {
+                {
+                    addActionListener( new ActionListener() {
+                        public void actionPerformed( ActionEvent e ) {
+                            MoleculeJMEApplication.showLonePairs.set( !MoleculeJMEApplication.showLonePairs.get() );
+                            updateState();
+                        }
+                    } );
+                    app.getMolecule().addListener( new Adapter() {
+                        @Override public void onGroupAdded( PairGroup group ) {
+                            updateState();
+                        }
+
+                        @Override public void onGroupRemoved( PairGroup group ) {
+                            updateState();
+                        }
+                    } );
+                    updateState();
+                }
+
+                public void updateState() {
+                    // update the text state. TODO i18n
+                    setText( MoleculeJMEApplication.showLonePairs.get() ? "Hide Lone Pairs" : "Show Lone Pairs" );
+
+                    // center horizontally, and place below the lone pair image
+                    setOffset(
+                            ( MoleculeShapesConstants.CONTROL_PANEL_INNER_WIDTH - getFullBounds().getWidth() ) / 2,
+                            lonePairNode.getFullBounds().getMaxY() + 10 );
+
+                    // enabled if there are lone pairs to hide
+                    setEnabled( !app.getMolecule().getLonePairs().isEmpty() );
+                }
+            };
+            addChild( toggleLonePairsButton );
+
+            addChild( new PNode() {{
+                setOffset( 0, toggleLonePairsButton.getFullBounds().getMaxY() + 3 );
+                addChild( new PhetPPath( new java.awt.geom.Rectangle2D.Float( 0, 0, 10, 0.1f ), new Color( 0, 0, 0, 0 ) ) );
             }} );
         }}, "Lone Pair", Color.BLACK, new BasicStroke( MoleculeShapesConstants.CONTROL_PANEL_BORDER_WIDTH ), MoleculeShapesConstants.CONTROL_PANEL_BORDER_COLOR ) {{
             setOffset( 0, bondingPanel.getFullBounds().getMaxY() + PANEL_SPACER );
