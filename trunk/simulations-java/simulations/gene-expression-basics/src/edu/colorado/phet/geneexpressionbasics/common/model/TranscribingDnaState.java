019d8d6db75d59a1a0c0934bbd76eb6e86216e8d
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-08-29 19:45:15 +0000

    Added the ability for the polymerase to determine the length of the gene and transcribe it.

--- trunk/simulations-java/simulations/gene-expression-basics/src/edu/colorado/phet/geneexpressionbasics/common/model/TranscribingDnaState.java
+++ trunk/simulations-java/simulations/gene-expression-basics/src/edu/colorado/phet/geneexpressionbasics/common/model/TranscribingDnaState.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
 import edu.colorado.phet.common.phetcommon.util.Option;
+import edu.colorado.phet.geneexpressionbasics.manualgeneexpression.model.Gene;
 
 /**
  * State the controls a biomolecule's behavior when it is transcribing DNA.
@@ -15,14 +16,14 @@ public class TranscribingDnaState extends BiomoleculeBehaviorState {
 
     private static final double VELOCITY = 500; // In picometers per second.
 
+    private final double transcribedRegionLength;
     private final AttachmentSite attachmentSite;
 
     private double distanceTraveled = 0;
 
-    public TranscribingDnaState( AttachmentSite attachmentSite ) {
+    public TranscribingDnaState( AttachmentSite attachmentSite, Gene geneBeingTranscribed ) {
         this.attachmentSite = attachmentSite;
-        // TODO: The half life is currently fixed, but we may want to change it based on the affinity of the attachment.
-        // If so, it would be calculated here.
+        transcribedRegionLength = geneBeingTranscribed.getTranscribedRegionLength();
     }
 
     @Override public BiomoleculeBehaviorState stepInTime( double dt, MobileBiomolecule biomolecule ) {
@@ -30,8 +31,7 @@ public class TranscribingDnaState extends BiomoleculeBehaviorState {
         biomolecule.setPosition( biomolecule.getPosition().getX() + dt * VELOCITY, biomolecule.getPosition().getY() );
         distanceTraveled += dt * VELOCITY;
 
-        // TODO: Needs to find the end of the gene and detach.  For now it just travels an arbitrary distance.
-        if ( distanceTraveled > 2000 ) {
+        if ( distanceTraveled > transcribedRegionLength ) {
             // Release the attachment site.
             attachmentSite.attachedMolecule.set( new Option.None<MobileBiomolecule>() );
             // Detach and drift upwards.
