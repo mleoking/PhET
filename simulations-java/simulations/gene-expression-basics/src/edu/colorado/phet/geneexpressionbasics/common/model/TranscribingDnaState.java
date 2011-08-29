6f06c0d391124e7a8ed264a94292f487d533a328
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-08-29 22:39:57 +0000

    Added animated conformation changing.

--- simulations-java/simulations/gene-expression-basics/src/edu/colorado/phet/geneexpressionbasics/common/model/TranscribingDnaState.java
+++ simulations-java/simulations/gene-expression-basics/src/edu/colorado/phet/geneexpressionbasics/common/model/TranscribingDnaState.java
@@ -14,29 +14,63 @@ import edu.colorado.phet.geneexpressionbasics.manualgeneexpression.model.Gene;
  */
 public class TranscribingDnaState extends BiomoleculeBehaviorState {
 
+    //-------------------------------------------------------------------------
+    // Class Data
+    //-------------------------------------------------------------------------
+
     private static final double VELOCITY = 500; // In picometers per second.
+
+    // Amount of time required to change from one conformation to another.
+    private static final double CONFORMATIONAL_CHANGE_TIME = 0.5; // In seconds.
+
+    //-------------------------------------------------------------------------
+    // Instance Data
+    //-------------------------------------------------------------------------
 
     private final double transcribedRegionLength;
     private final AttachmentSite attachmentSite;
 
     private double distanceTraveled = 0;
 
+    private double degreeOfConformationalChange = 0;
+
+    //-------------------------------------------------------------------------
+    // Constructor(s)
+    //-------------------------------------------------------------------------
+
     public TranscribingDnaState( MobileBiomolecule biomolecule, AttachmentSite attachmentSite, Gene geneBeingTranscribed ) {
         super( biomolecule );
         this.attachmentSite = attachmentSite;
         transcribedRegionLength = geneBeingTranscribed.getTranscribedRegionLength();
-        biomolecule.changeConformation( 1 );
     }
+
+    //-------------------------------------------------------------------------
+    // Methods
+    //-------------------------------------------------------------------------
 
     @Override public BiomoleculeBehaviorState stepInTime( double dt ) {
 
-        biomolecule.setPosition( biomolecule.getPosition().getX() + dt * VELOCITY, biomolecule.getPosition().getY() );
-        distanceTraveled += dt * VELOCITY;
-
-        if ( distanceTraveled > transcribedRegionLength ) {
+        if ( degreeOfConformationalChange < 1 && distanceTraveled == 0 ) {
+            // The molecule is in the process of changing to the "transcribing"
+            // conformation.
+            degreeOfConformationalChange = Math.min( degreeOfConformationalChange + dt / CONFORMATIONAL_CHANGE_TIME, 1 );
+            biomolecule.changeConformation( degreeOfConformationalChange );
+        }
+        else if ( distanceTraveled < transcribedRegionLength ) {
+            // The molecule is transcribing the gene.
+            biomolecule.setPosition( biomolecule.getPosition().getX() + dt * VELOCITY, biomolecule.getPosition().getY() );
+            distanceTraveled += dt * VELOCITY;
+        }
+        else if ( degreeOfConformationalChange > 0 ) {
+            // The molecule is changing back to the non-transcribing conformation.
+            degreeOfConformationalChange = Math.max( degreeOfConformationalChange - dt / CONFORMATIONAL_CHANGE_TIME, 0 );
+            biomolecule.changeConformation( degreeOfConformationalChange );
+        }
+        else {
+            // The molecule has completed the entire transcription process.
             // Release the attachment site.
             attachmentSite.attachedMolecule.set( new Option.None<MobileBiomolecule>() );
-            // Return to normal shape.
+            // Make sure it is back to the nominal conformation.
             biomolecule.changeConformation( 0 );
             // Detach and drift upwards.
             return new DetachingState( biomolecule, new ImmutableVector2D( 0, 1 ) );
