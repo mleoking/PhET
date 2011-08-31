5669eda351a12812f3afe996052e573445b55936
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-08-31 19:47:31 +0000

    Fixed: Update the value in the velocity sensor on reset, without this call it would reset to "?" even if 0.00 was the original value

--- simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/watertower/model/FPAFVelocitySensor.java
+++ simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/watertower/model/FPAFVelocitySensor.java
@@ -8,17 +8,29 @@ import edu.colorado.phet.common.piccolophet.nodes.VelocitySensor;
 import edu.colorado.phet.fluidpressureandflow.common.model.VelocitySensorContext;
 
 /**
+ * Velocity sensor customized for Fluid Pressure and Flow sim
+ *
  * @author Sam Reid
  */
 public class FPAFVelocitySensor extends VelocitySensor {
+
+    public final SimpleObserver updateValue;
+
     public FPAFVelocitySensor( final VelocitySensorContext context, double x, double y ) {
         super( x, y );
-        final SimpleObserver updateValue = new SimpleObserver() {
+        updateValue = new SimpleObserver() {
             public void update() {
                 value.set( new Option.Some<ImmutableVector2D>( context.getVelocity( position.get().getX(), position.get().getY() ) ) );
             }
         };
         position.addObserver( updateValue );
         context.addVelocityUpdateListener( updateValue );
+    }
+
+    @Override public void reset() {
+        super.reset();
+
+        //Update the value in the sensor, without this call it would reset to "?" even if 0.00 was the original value
+        updateValue.update();
     }
 }
