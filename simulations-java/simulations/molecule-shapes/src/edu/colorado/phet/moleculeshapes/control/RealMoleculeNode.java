08dbc3ab4ab69359d45f6772218c7092feaf0bfa
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-08-31 01:58:10 +0000

    ms: added real-molecule responses on changes

--- simulations-java/simulations/molecule-shapes/src/edu/colorado/phet/moleculeshapes/control/RealMoleculeNode.java
+++ simulations-java/simulations/molecule-shapes/src/edu/colorado/phet/moleculeshapes/control/RealMoleculeNode.java
@@ -2,13 +2,20 @@
 package edu.colorado.phet.moleculeshapes.control;
 
 import java.awt.*;
+import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
+import java.util.List;
 
 import edu.colorado.phet.common.phetcommon.model.property.Property;
+import edu.colorado.phet.common.phetcommon.util.function.Function1;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.colorado.phet.common.piccolophet.nodes.kit.Kit;
 import edu.colorado.phet.common.piccolophet.nodes.kit.KitSelectionNode;
 import edu.colorado.phet.moleculeshapes.MoleculeShapesConstants;
+import edu.colorado.phet.moleculeshapes.model.MoleculeModel;
+import edu.colorado.phet.moleculeshapes.model.MoleculeModel.AnyChangeAdapter;
+import edu.colorado.phet.moleculeshapes.model.PairGroup;
 import edu.colorado.phet.moleculeshapes.model.RealMolecule;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.nodes.PText;
@@ -19,19 +26,67 @@ import edu.umd.cs.piccolox.pswing.PSwing;
  */
 public class RealMoleculeNode extends PNode {
 
+    private PNode child = null;
+    private final MoleculeModel molecule;
+    private final double SIZE = MoleculeShapesConstants.CONTROL_PANEL_INNER_WIDTH;
+
+    public RealMoleculeNode( MoleculeModel molecule ) {
+        this.molecule = molecule;
+        // padding, and make sure we have the width
+        addChild( new PhetPPath( new Rectangle2D.Double( 0, 0, SIZE, 10 ), new Color( 0, 0, 0, 0 ) ) );
+
+        updateChild();
+
+        molecule.addListener( new AnyChangeAdapter() {
+            @Override public void onGroupChange( PairGroup group ) {
+                updateChild();
+            }
+        } );
+    }
+
+    private void updateChild() {
+        if ( child != null ) {
+            removeChild( child );
+        }
+
+        // get the list of real molecules that correspond to our VSEPR model
+        List<RealMolecule> molecules = RealMolecule.getMatchingMolecules( molecule );
+
+        if ( molecules.isEmpty() ) {
+            // blank node
+            // TODO i18n
+            child = new PText( "(none)" );
+
+            // center the text
+            child.setOffset( ( SIZE - child.getWidth() ) / 2, ( SIZE - child.getHeight() ) / 2 );
+        }
+        else {
+            Property<Integer> indexProperty = new Property<Integer>( 0 );
+            List<Kit<PSwing>> kits = map( molecules, new Function1<RealMolecule, Kit<PSwing>>() {
+                public Kit<PSwing> apply( RealMolecule realMolecule ) {
+                    return new Kit<PSwing>( labelText( realMolecule.getDisplayName() ), new PSwing( new RealMoleculePanel( realMolecule ) ) );
+                }
+            } );
+            child = new KitSelectionNode<PSwing>( indexProperty, kits.toArray( new Kit[molecules.size()] ) );
+        }
+        addChild( child );
+
+        repaint();
+    }
+
+    private static <A, B> List<B> map( List<A> list, Function1<A, B> map ) {
+        // TODO: move to somewhere more convenient
+        List<B> result = new ArrayList<B>();
+        for ( A element : list ) {
+            result.add( map.apply( element ) );
+        }
+        return result;
+    }
+
     private PText labelText( String str ) {
         return new PText( str ) {{
             setFont( new PhetFont( 14 ) );
             setTextPaint( MoleculeShapesConstants.CONTROL_PANEL_BORDER_COLOR );
         }};
-    }
-
-    public RealMoleculeNode() {
-        // padding, and make sure we have the width
-        addChild( new PhetPPath( new java.awt.geom.Rectangle2D.Double( 0, 0, MoleculeShapesConstants.CONTROL_PANEL_INNER_WIDTH, 10 ), new Color( 0, 0, 0, 0 ) ) );
-
-        addChild( new KitSelectionNode<PSwing>( new Property<Integer>( 0 ),
-                                                new Kit<PSwing>( labelText( "Water" ), new PSwing( new RealMoleculePanel( RealMolecule.WATER ) ) ),
-                                                new Kit<PSwing>( labelText( "Ethane" ), new PSwing( new RealMoleculePanel( RealMolecule.ETHANE ) ) ) ) );
     }
 }
