03cd648da719390f3a8ccf8609a65849d0c3a878
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-08-31 01:34:01 +0000

    renamed methods and fields

--- trunk/simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/watertower/view/HoseNode.java
+++ trunk/simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/watertower/view/HoseNode.java
@@ -21,7 +21,7 @@ public class HoseNode extends PNode {
     public HoseNode( final ModelViewTransform transform, final Hose hose ) {
         addChild( new PhetPPath() );
 
-        final double height = Math.abs( transform.modelToViewDeltaY( hose.attachmentWidth ) );
+        final double height = Math.abs( transform.modelToViewDeltaY( hose.holeSize ) );
         addChild( new PhetPPath( new Rectangle2D.Double( 0, 0, 10, height ), Color.green ) {{
             hose.attachmentPoint.addObserver( new VoidFunction1<ImmutableVector2D>() {
                 public void apply( ImmutableVector2D point ) {
