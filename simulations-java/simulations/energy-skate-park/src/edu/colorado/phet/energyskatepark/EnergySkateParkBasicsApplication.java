647da78af4b530baab5e4a2ecb9f3ddf74d8af0c
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-09-01 21:02:52 +0000

    Started new energy-skate-park-basics flavor

---
+++ simulations-java/simulations/energy-skate-park/src/edu/colorado/phet/energyskatepark/EnergySkateParkBasicsApplication.java
@@ -0,0 +1,22 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.energyskatepark;
+
+import edu.colorado.phet.common.phetcommon.application.PhetApplication;
+import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
+import edu.colorado.phet.common.phetcommon.application.PhetApplicationLauncher;
+import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
+import edu.colorado.phet.energyskatepark.model.EnergySkateParkOptions;
+
+/**
+ * @author Sam Reid
+ */
+public class EnergySkateParkBasicsApplication extends PhetApplication {
+    public EnergySkateParkBasicsApplication( PhetApplicationConfig config ) {
+        super( config );
+        addModule( new EnergySkateParkModule( "Module", new ConstantDtClock( 30, EnergySkateParkApplication.SIMULATION_TIME_DT ), getPhetFrame(), new EnergySkateParkOptions() ) );
+    }
+
+    public static void main( String[] args ) {
+        new PhetApplicationLauncher().launchSim( args, "energy-skate-park", "energy-skate-park-basics", EnergySkateParkBasicsApplication.class );
+    }
+}
