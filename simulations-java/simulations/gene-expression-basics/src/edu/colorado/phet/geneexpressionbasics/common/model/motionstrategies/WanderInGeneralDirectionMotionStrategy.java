c6b957deb0317c7583367e281c9ac69dd257c992
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-09-01 19:29:33 +0000

    Added motion bounds to WanderInGeneralDirectionMotionStrategy.

--- simulations-java/simulations/gene-expression-basics/src/edu/colorado/phet/geneexpressionbasics/common/model/motionstrategies/WanderInGeneralDirectionMotionStrategy.java
+++ simulations-java/simulations/gene-expression-basics/src/edu/colorado/phet/geneexpressionbasics/common/model/motionstrategies/WanderInGeneralDirectionMotionStrategy.java
@@ -3,10 +3,13 @@ package edu.colorado.phet.geneexpressionbasics.common.model.motionstrategies;
 
 import java.awt.Shape;
 import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 import java.util.Random;
 
 import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
 import edu.colorado.phet.common.phetcommon.math.Vector2D;
+import edu.colorado.phet.common.phetcommon.model.property.Property;
+import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
 
 /**
  * @author John Blanco
@@ -17,34 +20,53 @@ public class WanderInGeneralDirectionMotionStrategy extends MotionStrategy {
     private static final double MAX_VELOCITY = 500; // In picometers/s
     private static final double MIN_TIME_IN_ONE_DIRECTION = 0.25; // In seconds.
     private static final double MAX_TIME_IN_ONE_DIRECTION = 1.25; // In seconds.
+    private static final Random RAND = new Random();
 
     private final ImmutableVector2D generalDirection;
-    private final Random rand = new Random();
     private double directionChangeCountdown = 0;
     private ImmutableVector2D currentMotionVector = new Vector2D( 0, 0 );
 
-    public WanderInGeneralDirectionMotionStrategy( ImmutableVector2D generalDirection ) {
+    public WanderInGeneralDirectionMotionStrategy( ImmutableVector2D generalDirection, Property<MotionBounds> motionBoundsProperty ) {
+        motionBoundsProperty.addObserver( new VoidFunction1<MotionBounds>() {
+            public void apply( MotionBounds motionBounds ) {
+                WanderInGeneralDirectionMotionStrategy.this.motionBounds = motionBounds;
+            }
+        } );
         this.generalDirection = generalDirection;
     }
 
-    public Point2D getNextLocation( Point2D currentLocation, double dt ) {
+    @Override public Point2D getNextLocation( Point2D currentLocation, Shape shape, double dt ) {
         directionChangeCountdown -= dt;
         if ( directionChangeCountdown <= 0 ) {
-            // Time to change direction.
-            double newVelocity = MIN_VELOCITY + rand.nextDouble() * ( MAX_VELOCITY - MIN_VELOCITY );
-            double varianceAngle = ( rand.nextDouble() - 0.5 ) * Math.PI / 3;
+            // Time to change the direction.
+            double newVelocity = MIN_VELOCITY + RAND.nextDouble() * ( MAX_VELOCITY - MIN_VELOCITY );
+            double varianceAngle = ( RAND.nextDouble() - 0.5 ) * Math.PI / 3;
             currentMotionVector = generalDirection.getInstanceOfMagnitude( newVelocity ).getRotatedInstance( varianceAngle );
             // Reset the countdown timer.
-            directionChangeCountdown = MIN_TIME_IN_ONE_DIRECTION + rand.nextDouble() * ( MAX_TIME_IN_ONE_DIRECTION - MIN_TIME_IN_ONE_DIRECTION );
+            directionChangeCountdown = MIN_TIME_IN_ONE_DIRECTION + RAND.nextDouble() * ( MAX_TIME_IN_ONE_DIRECTION - MIN_TIME_IN_ONE_DIRECTION );
         }
+
+        // Make sure that current motion will not cause the model element to
+        // move outside of the motion bounds.
+        if ( !motionBounds.testMotionAgainstBounds( shape, currentMotionVector, dt ) ) {
+            // The current motion vector would take this element out of bounds,
+            // so it needs to "bounce".
+            currentMotionVector = getMotionVectorForBounce( shape, currentMotionVector, dt, MAX_VELOCITY );
+            // Reset the timer.
+            directionChangeCountdown = generateDirectionChangeCountdownValue();
+        }
+
+        // Calculate the next location based on the motion vector.
         Point2D nextLocation = new Point2D.Double( currentLocation.getX() + currentMotionVector.getX() * dt,
                                                    currentLocation.getY() + currentMotionVector.getY() * dt );
-        // TODO: Need to check next location against bounds and "bounce" once we have bounds.
         return nextLocation;
     }
 
-    @Override public Point2D getNextLocation( Point2D currentLocation, Shape shape, double dt ) {
-        // TODO: Disregards shape for now, needs to be implemented.
-        return getNextLocation( currentLocation, dt );
+    @Override public Point2D getNextLocation( Point2D currentLocation, double dt ) {
+        return getNextLocation( currentLocation, new Rectangle2D.Double( currentLocation.getX(), currentLocation.getY(), Double.MIN_VALUE, Double.MIN_VALUE ), dt );
+    }
+
+    private double generateDirectionChangeCountdownValue() {
+        return MIN_TIME_IN_ONE_DIRECTION + RAND.nextDouble() * ( MAX_TIME_IN_ONE_DIRECTION - MIN_TIME_IN_ONE_DIRECTION );
     }
 }
