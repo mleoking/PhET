1690dc82d761e969c389e7bd1b301933acb7c45c
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-09-02 22:12:34 +0000

    improved control panel

--- simulations-java/simulations/energy-skate-park/src/edu/colorado/phet/energyskatepark/basics/EnergySkateParkBasicsModule.java
+++ simulations-java/simulations/energy-skate-park/src/edu/colorado/phet/energyskatepark/basics/EnergySkateParkBasicsModule.java
@@ -1,6 +1,12 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.energyskatepark.basics;
 
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JCheckBox;
+
 import edu.colorado.phet.common.phetcommon.util.function.VoidFunction0;
 import edu.colorado.phet.common.phetcommon.view.PhetFrame;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
@@ -14,6 +20,7 @@ import edu.colorado.phet.energyskatepark.serialization.EnergySkateParkIO;
 import edu.colorado.phet.energyskatepark.view.EnergySkateParkLookAndFeel;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.nodes.PText;
+import edu.umd.cs.piccolox.pswing.PSwing;
 
 /**
  * Module for Energy Skate Park Basics.
@@ -24,6 +31,7 @@ public class EnergySkateParkBasicsModule extends AbstractEnergySkateParkModule {
     public static final double INSET = 5;
     public final ControlPanelNode energyGraphControlPanel;
     public final String PARABOLA = "energy-skate-park/tracks/basics/parabola.esp";
+    private static final Font CONTROL_FONT = new PhetFont( 16 );
 
     public EnergySkateParkBasicsModule( String name, PhetFrame phetFrame ) {
         super( name, phetFrame, new EnergySkateParkOptions() );
@@ -37,8 +45,20 @@ public class EnergySkateParkBasicsModule extends AbstractEnergySkateParkModule {
         //Add the energy graph control panel
         energyGraphControlPanel = new ControlPanelNode( new VBox(
                 new PText( "Energy Graphs" ) {{setFont( new PhetFont( 16, true ) );}},
-                new TextButtonNode( "Pie Chart" ),
-                new TextButtonNode( "Bar Chart" ) ), EnergySkateParkLookAndFeel.backgroundColor ) {{
+
+                //Checkbox to show/hide the pie chart
+                new PSwing( new JCheckBox( "Show Pie Chart", isPieChartVisible() ) {{
+                    setFont( CONTROL_FONT );
+                    addActionListener( new ActionListener() {
+                        public void actionPerformed( ActionEvent e ) {
+                            setPieChartVisible( isSelected() );
+                        }
+                    } );
+                }} ),
+                new TextButtonNode( "Bar Chart" ) {{
+                    setFont( CONTROL_FONT );
+                }}
+        ), EnergySkateParkLookAndFeel.backgroundColor ) {{
 
             //Set its location when the layout changes in the piccolo node, since this sim isn't using stage coordinates
             energySkateParkSimulationPanel.getRootNode().addLayoutListener( new VoidFunction0() {
