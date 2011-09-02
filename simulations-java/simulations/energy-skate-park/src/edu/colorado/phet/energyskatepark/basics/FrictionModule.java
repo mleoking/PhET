370aa3c02efd4cc56f6c1de9fce018556a3cbcbb
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-09-02 20:49:45 +0000

    Added controls to the friction tab

--- simulations-java/simulations/energy-skate-park/src/edu/colorado/phet/energyskatepark/basics/FrictionModule.java
+++ simulations-java/simulations/energy-skate-park/src/edu/colorado/phet/energyskatepark/basics/FrictionModule.java
@@ -1,15 +1,44 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.energyskatepark.basics;
 
+import edu.colorado.phet.common.phetcommon.util.function.VoidFunction0;
 import edu.colorado.phet.common.phetcommon.view.PhetFrame;
+import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
+import edu.colorado.phet.common.piccolophet.nodes.ControlPanelNode;
+import edu.colorado.phet.common.piccolophet.nodes.TextButtonNode;
+import edu.colorado.phet.common.piccolophet.nodes.layout.VBox;
+import edu.colorado.phet.energyskatepark.view.EnergySkateParkLookAndFeel;
+import edu.umd.cs.piccolo.nodes.PText;
 
 /**
- * Module for the "Energy Skate Park Basics" Friction
+ * Module for the "Energy Skate Park Basics" Friction tab
  *
  * @author Sam Reid
  */
 public class FrictionModule extends EnergySkateParkBasicsModule {
+    private ControlPanelNode trackFrictionControlPanel;
+
     public FrictionModule( PhetFrame phetFrame ) {
         super( "Friction", phetFrame );
+
+        //Add a track friction control panel
+        trackFrictionControlPanel = new ControlPanelNode( new VBox(
+                new PText( "Track" ) {{setFont( new PhetFont( 16, true ) );}},
+                new TextButtonNode( "Track Friction" ),
+                new TextButtonNode( "Stick to Track" ) ), EnergySkateParkLookAndFeel.backgroundColor ) {{
+
+            //Set its location when the layout changes in the piccolo node, since this sim isn't using stage coordinates
+            energySkateParkSimulationPanel.getRootNode().addLayoutListener( new VoidFunction0() {
+                public void apply() {
+                    setOffset( energySkateParkSimulationPanel.getWidth() - getFullBounds().getWidth() - INSET, energyGraphControlPanel.getFullBounds().getMaxY() + INSET );
+                }
+            } );
+        }};
+        energySkateParkSimulationPanel.getRootNode().addChild( trackFrictionControlPanel );
+
+        addTrackSelectionControlPanel();
+        addResetAllButton( trackFrictionControlPanel );
+
+        load( PARABOLA, true );
     }
 }
--- simulations-java/simulations/energy-skate-park/src/edu/colorado/phet/energyskatepark/basics/IntroModule.java
+++ simulations-java/simulations/energy-skate-park/src/edu/colorado/phet/energyskatepark/basics/IntroModule.java
@@ -1,14 +1,7 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.energyskatepark.basics;
 
-import edu.colorado.phet.common.phetcommon.util.function.VoidFunction0;
 import edu.colorado.phet.common.phetcommon.view.PhetFrame;
-import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
-import edu.colorado.phet.common.piccolophet.nodes.ControlPanelNode;
-import edu.colorado.phet.common.piccolophet.nodes.TextButtonNode;
-import edu.colorado.phet.common.piccolophet.nodes.layout.VBox;
-import edu.colorado.phet.energyskatepark.view.EnergySkateParkLookAndFeel;
-import edu.umd.cs.piccolo.nodes.PText;
 
 /**
  * Module for the "Energy Skate Park Basics" Introduction
@@ -16,53 +9,14 @@ import edu.umd.cs.piccolo.nodes.PText;
  * @author Sam Reid
  */
 public class IntroModule extends EnergySkateParkBasicsModule {
-    private static final double INSET = 5;
-
     public IntroModule( PhetFrame phetFrame ) {
         super( "Introduction", phetFrame );
 
-        //Add the control panel
-        final ControlPanelNode controlPanelNode = new ControlPanelNode( new VBox(
-                new PText( "Energy Graphs" ) {{setFont( new PhetFont( 16, true ) );}},
-                new TextButtonNode( "Pie Chart" ),
-                new TextButtonNode( "Bar Chart" ) ), EnergySkateParkLookAndFeel.backgroundColor ) {{
+        //Show the reset all button below the bottom control panel
+        addResetAllButton( energyGraphControlPanel );
 
-            //Set its location when the layout changes in the piccolo node, since this sim isn't using stage coordinates
-            energySkateParkSimulationPanel.getRootNode().addLayoutListener( new VoidFunction0() {
-                public void apply() {
-                    setOffset( energySkateParkSimulationPanel.getWidth() - getFullBounds().getWidth() - INSET, INSET );
-                }
-            } );
-        }};
-        energySkateParkSimulationPanel.getRootNode().addChild( controlPanelNode );
-
-        //Add the "Reset all" button
-        energySkateParkSimulationPanel.getRootNode().addChild( new TextButtonNode( "Reset All" ) {{
-
-            //Set its location when the layout changes in the piccolo node, since this sim isn't using stage coordinates
-            energySkateParkSimulationPanel.getRootNode().addLayoutListener( new VoidFunction0() {
-                public void apply() {
-                    setOffset( controlPanelNode.getFullBounds().getCenterX() - getFullBounds().getWidth() / 2, controlPanelNode.getFullBounds().getMaxY() + INSET * 2 );
-                }
-            } );
-        }} );
-
-        //Add the control panel that is used to change to a different track
-        final String PARABOLA = "energy-skate-park/tracks/basics/parabola.esp";
-        final ControlPanelNode trackSelectionNode = new ControlPanelNode( new VBox(
-                new TrackButton( this, PARABOLA, true ),
-                new TrackButton( this, "energy-skate-park/tracks/basics/to-the-floor.esp", true ),
-                new TrackButton( this, "energy-skate-park/tracks/basics/double-well.esp", true )
-        ), EnergySkateParkLookAndFeel.backgroundColor ) {{
-
-            //Set its location when the layout changes in the piccolo node, since this sim isn't using stage coordinates
-            energySkateParkSimulationPanel.getRootNode().addLayoutListener( new VoidFunction0() {
-                public void apply() {
-                    setOffset( INSET, INSET );
-                }
-            } );
-        }};
-        energySkateParkSimulationPanel.getRootNode().addChild( trackSelectionNode );
+        //Show buttons that allows the user to choose different tracks
+        addTrackSelectionControlPanel();
 
         //Load the initial track
         load( PARABOLA, true );
