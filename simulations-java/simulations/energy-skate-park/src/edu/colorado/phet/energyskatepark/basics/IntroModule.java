afd513e082e91ec8a3620ebd75484890a607498e
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-09-02 02:55:11 +0000

    started adding a control panel

--- simulations-java/simulations/energy-skate-park/src/edu/colorado/phet/energyskatepark/basics/IntroModule.java
+++ simulations-java/simulations/energy-skate-park/src/edu/colorado/phet/energyskatepark/basics/IntroModule.java
@@ -2,6 +2,11 @@
 package edu.colorado.phet.energyskatepark.basics;
 
 import edu.colorado.phet.common.phetcommon.view.PhetFrame;
+import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
+import edu.colorado.phet.common.piccolophet.nodes.ControlPanelNode;
+import edu.colorado.phet.common.piccolophet.nodes.TextButtonNode;
+import edu.colorado.phet.common.piccolophet.nodes.layout.VBox;
+import edu.umd.cs.piccolo.nodes.PText;
 
 /**
  * Module for the "Energy Skate Park Basics" Introduction
@@ -11,5 +16,15 @@ import edu.colorado.phet.common.phetcommon.view.PhetFrame;
 public class IntroModule extends EnergySkateParkBasicsModule {
     public IntroModule( PhetFrame phetFrame ) {
         super( "Introduction", phetFrame );
+
+        ControlPanelNode controlPanelNode = new ControlPanelNode( new VBox(
+                new PText( "Energy Graphs" ) {{setFont( new PhetFont( 16, true ) );}},
+                new TextButtonNode( "Pie Chart" ),
+                new TextButtonNode( "Bar Chart" )
+        ) ) {{
+//            setOffset( energySkateParkSimulationPanel. );
+        }};
+
+        energySkateParkSimulationPanel.getRootNode().addChild( controlPanelNode );
     }
 }
