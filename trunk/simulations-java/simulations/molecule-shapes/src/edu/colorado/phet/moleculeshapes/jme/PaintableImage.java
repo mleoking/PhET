10150973749373a6731ca3588ed86e8254585347
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-09-03 05:40:53 +0000

    ms: HUDNode and PaintableImage cleanup

--- trunk/simulations-java/simulations/molecule-shapes/src/edu/colorado/phet/moleculeshapes/jme/PaintableImage.java
+++ trunk/simulations-java/simulations/molecule-shapes/src/edu/colorado/phet/moleculeshapes/jme/PaintableImage.java
@@ -7,12 +7,17 @@ import java.nio.ByteBuffer;
 
 import com.jme3.texture.Image;
 
+/**
+ * An image (JME3 texture) that can be drawn on with a Graphics2D context.
+ * <p/>
+ * When needed (or on construction), refreshImage() should be called to update
+ * the texture.
+ */
 public abstract class PaintableImage extends Image {
     private BufferedImage backImg;
     private ByteBuffer scratch;
 
     public PaintableImage( int width, int height, boolean hasAlpha ) {
-        // TODO: fix alpha support. broken currently
         super( hasAlpha ? Format.RGBA8 : Format.BGR8, width, height, ByteBuffer.allocateDirect( 4 * width * height ) );
         scratch = data.get( 0 );
         backImg = new BufferedImage( width, height, hasAlpha ? BufferedImage.TYPE_4BYTE_ABGR : BufferedImage.TYPE_3BYTE_BGR );
@@ -32,6 +37,7 @@ public abstract class PaintableImage extends Image {
         setData( scratch );
     }
 
+    // override this to define how to paint the image
     public abstract void paint( Graphics2D graphicsContext );
 
 }
