bae05d215bd2cebd8b08820b2c2816b90a52c455
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-09-03 08:15:07 +0000

    ms: extremely significant bond performance gains, better docs and JME handling.

--- simulations-java/simulations/molecule-shapes/src/edu/colorado/phet/moleculeshapes/view/BondNode.java
+++ simulations-java/simulations/molecule-shapes/src/edu/colorado/phet/moleculeshapes/view/BondNode.java
@@ -1,9 +1,6 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.moleculeshapes.view;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.phetcommon.util.Option;
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
@@ -27,17 +24,17 @@ import com.jme3.scene.shape.Cylinder;
  */
 public class BondNode extends Node {
 
+    // position properties
     private final Property<ImmutableVector3D> a;
     private final Property<ImmutableVector3D> b;
+
     private final int bondOrder;
     private final float bondRadius;
     private final Option<Float> maxLength;
-    private final MoleculeJMEApplication app;
     private final Camera camera;
-    private final ColorRGBA aColor;
-    private final ColorRGBA bColor;
 
-    private final List<SingleBondNode> children = new ArrayList<SingleBondNode>();
+    private final SingleBondNode[] aBonds;
+    private final SingleBondNode[] bBonds;
 
     public BondNode( final Property<ImmutableVector3D> a, final Property<ImmutableVector3D> b, int bondOrder, float bondRadius,
                      Option<Float> maxLength, MoleculeJMEApplication app, Camera camera ) {
@@ -52,48 +49,65 @@ public class BondNode extends Node {
         this.bondOrder = bondOrder;
         this.bondRadius = bondRadius;
         this.maxLength = maxLength;
-        this.app = app;
         this.camera = camera;
-        this.aColor = aColor;
-        this.bColor = bColor;
+
+        // initialize the single bond nodes, for use later
+        aBonds = new SingleBondNode[bondOrder];
+        bBonds = new SingleBondNode[bondOrder];
+        for ( int i = 0; i < bondOrder; i++ ) {
+            // they will have unit height and unit radius! we will scale, rotate and translate them later
+            aBonds[i] = new SingleBondNode( 1, 1, app.getAssetManager(), aColor );
+            bBonds[i] = new SingleBondNode( 1, 1, app.getAssetManager(), bColor );
+
+            // attach them
+            attachChild( aBonds[i] );
+            attachChild( bBonds[i] );
+        }
 
         updateView();
     }
 
+    /**
+     * Reposition all of the SingleBondNodes so that they form to make a bond with the correct physical alignment and position
+     */
     public void updateView() {
-        for ( SingleBondNode child : children ) {
-            detachChild( child );
-        }
-        children.clear();
-
-
         // transform our position and direction into the local coordinate frame. we will do our computations there
         Vector3f click3d = camera.getWorldCoordinates( new Vector2f( 0, 0 ), 0f ).clone();
         Vector3f cameraPosition = getWorldTransform().transformInverseVector( click3d, new Vector3f() );
 
-
-        Vector3f start = JmeUtils.convertVector( a.get() );
+        // extract our start and end
+        final Vector3f start = JmeUtils.convertVector( a.get() );
         final Vector3f end = JmeUtils.convertVector( b.get() );
 
+        // unit vector point in the direction of the end from the start
         final Vector3f towardsEnd = end.subtract( start ).normalize();
 
+        // calculate the length of the bond. sometimes it can be length-limited, and we push the bond towards B
         float distance = start.distance( end );
         final float length;
         float overLength = 0;
         if ( maxLength.isSome() && distance > maxLength.get() ) {
+            // our bond would be too long
             length = maxLength.get();
             overLength = distance - maxLength.get();
         }
         else {
             length = distance;
         }
+
+        // find the center of our bond. we add in the "over" length if necessary to offset the bond from between A and B
         final Vector3f bondCenter = FastMath.interpolateLinear( .5f, start, end ).add( towardsEnd.mult( overLength / 2 ) );
 
         // get a unit vector perpendicular to the bond direction and camera direction
         final Vector3f perpendicular = bondCenter.subtract( end ).normalize().cross( bondCenter.subtract( cameraPosition ).normalize() ).normalize();
 
-        // compute offsets from the "central" bond position, for showing double and triple bonds
+        /*
+         * Compute offsets from the "central" bond position, for showing double and triple bonds.
+         * The offsets are basically the relative positions of the 1/2/3 cylinders that are displayed as a bond.
+         */
         final Vector3f[] offsets;
+
+        // how far bonds are apart. constant refined for visual appearance. triple-bonds aren't wider than atoms, most notably
         float bondSeparation = bondRadius * ( 12.0f / 5.0f );
         switch( bondOrder ) {
             case 1:
@@ -109,38 +123,31 @@ public class BondNode extends Node {
                 throw new RuntimeException( "bad bond order: " + bondOrder );
         }
 
-        // add single bond nodes at each offset position
-        if ( aColor.equals( bColor ) || !MoleculeShapesProperties.useColoredBonds.get() ) {
-            for ( final Vector3f offset : offsets ) {
-                SingleBondNode child = new SingleBondNode( length, bondRadius, app.getAssetManager(), ColorRGBA.White ) {{
-                    setLocalTranslation( bondCenter.add( offset ) );
-                    lookAt( end.add( offset ), Vector3f.UNIT_Y );
-                }};
-                attachChild( child );
-                children.add( child );
-            }
-        }
-        else {
-            for ( final Vector3f offset : offsets ) {
-                final Vector3f colorOffset = towardsEnd.mult( length / 4 );
+        // since we need to support two different colors (A-colored and B-colored), we need to compute the offsets from the bond center for each
+        final Vector3f colorOffset = towardsEnd.mult( length / 4 );
 
-                SingleBondNode aBond = new SingleBondNode( length / 2, bondRadius, app.getAssetManager(), aColor ) {{
-                    setLocalTranslation( bondCenter.add( offset ).subtract( colorOffset ) );
-                    lookAt( end.add( offset ), Vector3f.UNIT_Y );
-                }};
-                attachChild( aBond );
-                children.add( aBond );
+        for ( int i = 0; i < bondOrder; i++ ) {
+            // luckily, JME3's transform applies in the convenient "scale then rotation then translate", so our code is compact
 
-                SingleBondNode bBond = new SingleBondNode( length / 2, bondRadius, app.getAssetManager(), bColor ) {{
-                    setLocalTranslation( bondCenter.add( offset ).add( colorOffset ) );
-                    lookAt( end.add( offset ), Vector3f.UNIT_Y );
-                }};
-                attachChild( bBond );
-                children.add( bBond );
-            }
+            // since our cylinders point along the Z direction and have unit-length and unit-radius, we scale it out to the desired radius and half-length
+            aBonds[i].setLocalScale( bondRadius, bondRadius, length / 2 );
+
+            // point the cylinder in the direction of the bond. Cylinder is symmetric, so sign doesn't matter
+            aBonds[i].setLocalRotation( JmeUtils.getRotationQuaternion( Vector3f.UNIT_Z, towardsEnd ) );
+
+            // add in the necessary offsets
+            aBonds[i].setLocalTranslation( bondCenter.add( offsets[i] ).subtract( colorOffset ) );
+
+            // similarly as above
+            bBonds[i].setLocalScale( bondRadius, bondRadius, length / 2 );
+            bBonds[i].setLocalRotation( JmeUtils.getRotationQuaternion( Vector3f.UNIT_Z, towardsEnd ) );
+            bBonds[i].setLocalTranslation( bondCenter.add( offsets[i] ).add( colorOffset ) );
         }
     }
 
+    /**
+     * Basically, a glorified cylinder.
+     */
     public static class SingleBondNode extends Geometry {
         public SingleBondNode( final float length, final float bondRadius, AssetManager assetManager, final ColorRGBA color ) {
             super( "Bond" );
@@ -154,7 +161,12 @@ public class BondNode extends Node {
             setMaterial( new Material( assetManager, "Common/MatDefs/Light/Lighting.j3md" ) {{
                 setBoolean( "UseMaterialColors", true );
 
-                setColor( "Diffuse", color );
+                // keep the color up-to-date (and initialize it)
+                MoleculeShapesProperties.useColoredBonds.addObserver( new SimpleObserver() {
+                    public void update() {
+                        setColor( "Diffuse", MoleculeShapesProperties.useColoredBonds.get() ? color : ColorRGBA.White );
+                    }
+                } );
             }} );
         }
     }
