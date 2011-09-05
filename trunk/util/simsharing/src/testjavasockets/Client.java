915d858e80f9b80e7c79d8bfd196e210af329fd0
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-09-05 02:18:28 +0000

    Added docs and tests

--- trunk/util/simsharing/src/testjavasockets/Client.java
+++ trunk/util/simsharing/src/testjavasockets/Client.java
@@ -6,26 +6,43 @@ import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 public class Client {
-    private void start() throws IOException, ClassNotFoundException {
-        Socket server = new Socket( "localhost", 1234 );
+    private int numQuestions;
+
+    public Client( int numQuestions ) {
+        this.numQuestions = numQuestions;
+    }
+
+    private void start() throws IOException, ClassNotFoundException, InterruptedException {
+
+        //Connect to the server
+        Socket server = Server.connect();
+
+        //Create streams for communicating with the server
         ObjectOutputStream writeToServer = new ObjectOutputStream( server.getOutputStream() );
         ObjectInputStream readFromServer = new ObjectInputStream( server.getInputStream() );
+
+        //Read the initial message from the server to verify communication is working properly
         Object fromServer = readFromServer.readObject();
-
         System.out.println( "Server: " + fromServer );
-        writeToServer.writeObject( "Add these numbers: 3,7" );
-        fromServer = readFromServer.readObject();
 
-        System.out.println( "fromServer = " + fromServer );
+        //Send a command to the server and process the result
+        for ( int i = 0; i < numQuestions; i++ ) {
+            writeToServer.writeObject( "Add these numbers: 3,6" );
+            fromServer = readFromServer.readObject();
+            System.out.println( "fromServer = " + fromServer );
+        }
 
+        //Done with commands, so issue a logout command to the server thread.  Doesn't kill all connections, just this one.
         writeToServer.writeObject( "logout" );
+        writeToServer.flush();
 
+        //All done with the server, so close our connections
         writeToServer.close();
         readFromServer.close();
         server.close();
     }
 
-    public static void main( String[] args ) throws IOException, ClassNotFoundException {
-        new Client().start();
+    public static void main( String[] args ) throws IOException, ClassNotFoundException, InterruptedException {
+        new Client( 10 ).start();
     }
 }
--- trunk/util/simsharing/src/testjavasockets/Server.java
+++ trunk/util/simsharing/src/testjavasockets/Server.java
@@ -9,31 +9,57 @@ import java.util.StringTokenizer;
 
 public class Server {
 
+    //Default host the server will run on, must be a publicly accessible IP address so clients can connect to it
+    public static String DEFAULT_HOST = "localhost";
+
+    //Default port the server will use for listening
+    public static int DEFAULT_PORT = 1234;
+
     //Flag to indicate the server is ready to accept new connections
     private boolean listening = true;
 
-    public static void main( String[] args ) throws IOException {
-        new Server().start();
+    //The port the server will use for listening
+    public int port;
+
+    //Create a server that will listen on the specified port.  Does not start listening until start() is called
+    public Server( int port ) {
+        this.port = port;
     }
 
+    //Starts the server listening on its port
     private void start() throws IOException {
-        final ServerSocket serverSocket = new ServerSocket( 1234 );
+
+        //Listen for connections on the specified port
+        final ServerSocket serverSocket = new ServerSocket( port );
+
+        //Accept as many incoming connections as detected while listening, and create a thread to handle each one
         while ( listening ) {
             System.out.println( "Server listening on port: " + serverSocket.getLocalPort() );
             final Socket socket = serverSocket.accept();
+            System.out.println( "Server accepted socket" );
+
+            //Create a new thread to handle connection
             new Thread( new Runnable() {
                 boolean threadAlive = true;
 
                 public void run() {
                     try {
+
+                        //Streams to communicate with the client, could be zipped with jzlib and could be buffered
                         final ObjectOutputStream writeToClient = new ObjectOutputStream( socket.getOutputStream() );
                         final ObjectInputStream readFromClient = new ObjectInputStream( socket.getInputStream() );
+
+                        //Send an initial message to test the connection
                         writeToClient.writeObject( "Greetings from the server" );
-                        System.out.println( "Server accepted socket" );
+
+                        //Loop as long as no 'logout' command was given, and process the commands
                         while ( threadAlive ) {
+
+                            //Read the object from the client
                             Object fromClient = readFromClient.readObject();
                             System.out.println( "fromClient = " + fromClient );
 
+                            //Process the command and respond
                             if ( fromClient instanceof String && fromClient.toString().startsWith( "Add" ) ) {
                                 StringTokenizer st = new StringTokenizer( fromClient.toString().substring( fromClient.toString().indexOf( ":" ) + 1 ), ", " );
                                 int x = Integer.parseInt( st.nextToken() );
@@ -41,6 +67,7 @@ public class Server {
                                 writeToClient.writeObject( "added your numbers, " + x + "+" + y + " = " + ( x + y ) );
                             }
 
+                            //Handle logout commands.
                             if ( fromClient.equals( "logout" ) ) {
                                 System.out.println( "Received logout command, exiting thread" );
                                 threadAlive = false;
@@ -48,6 +75,7 @@ public class Server {
 
                         }
 
+                        //The thread has been killed
                         writeToClient.close();
                         readFromClient.close();
                         socket.close();
@@ -59,6 +87,15 @@ public class Server {
             } ).start();
         }
 
+        //No longer listening for any connections, so close the socket
         serverSocket.close();
+    }
+
+    public static Socket connect() throws IOException {
+        return new Socket( DEFAULT_HOST, DEFAULT_PORT );
+    }
+
+    public static void main( String[] args ) throws IOException {
+        new Server( DEFAULT_PORT ).start();
     }
 }
