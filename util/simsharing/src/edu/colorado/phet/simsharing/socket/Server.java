0b292ac6d9cbabbdf6860fc4e56e49e740df1acd
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-09-05 16:57:51 +0000

    updated settings

--- util/simsharing/src/edu/colorado/phet/simsharing/socket/Server.java
+++ util/simsharing/src/edu/colorado/phet/simsharing/socket/Server.java
@@ -48,12 +48,12 @@ import com.mongodb.Mongo;
  */
 public class Server {
     //Remote settings
-//    public static int PORT = 44101;
-//    public static String HOST_IP_ADDRESS = "128.138.145.107";//phet-server, but can be mutated to specify a different host
+    public static int PORT = 44101;
+    public static String HOST_IP_ADDRESS = "128.138.145.107";//phet-server, but can be mutated to specify a different host
 
     //Settings for running locally
-    public static String HOST_IP_ADDRESS = "localhost";
-    public static int PORT = 1234;
+//    public static String HOST_IP_ADDRESS = "localhost";
+//    public static int PORT = 1234;
 
     public static String[] names = new String[] { "Alice", "Bob", "Charlie", "Danielle", "Earl", "Frankie", "Gail", "Hank", "Isabelle", "Joe", "Kim", "Lucy", "Mikey", "Nathan", "Ophelia", "Parker", "Quinn", "Rusty", "Shirley", "Tina", "Uther Pendragon", "Vivian", "Walt", "Xander", "Yolanda", "Zed" };
     private ArrayList<SessionID> students = new ArrayList<SessionID>();
