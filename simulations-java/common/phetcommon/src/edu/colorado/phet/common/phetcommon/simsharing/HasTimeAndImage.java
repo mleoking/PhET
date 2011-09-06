afd68b57943ecaf4e2e46aa9c0864a9dd4085e0e
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-09-06 17:49:33 +0000

    9/6/11 Added classes to support simsharing

--- simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/simsharing/HasTimeAndImage.java
+++ simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/simsharing/HasTimeAndImage.java
@@ -1,5 +1,5 @@
 // Copyright 2002-2011, University of Colorado
-package edu.colorado.phet.gravityandorbits.simsharing;
+package edu.colorado.phet.common.phetcommon.simsharing;
 
 /**
  * @author Sam Reid
