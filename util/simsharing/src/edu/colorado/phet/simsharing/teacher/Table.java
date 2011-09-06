f543b2a6799110e39e47c1c60462a8079238db24
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-09-06 15:31:44 +0000

    removed akka version

--- util/simsharing/src/edu/colorado/phet/simsharing/teacher/Table.java
+++ util/simsharing/src/edu/colorado/phet/simsharing/teacher/Table.java
@@ -1,12 +1,21 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.simsharing.teacher;
 
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.Component;
 import java.util.ArrayList;
 import java.util.Date;
 
-import javax.swing.*;
-import javax.swing.table.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.JTableHeader;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableColumnModel;
+import javax.swing.table.TableModel;
 
 /**
  * @author Sam Reid
