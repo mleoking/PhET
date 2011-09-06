232173ddbc0e9c71e3de20b40377add706d23c9d
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-09-06 23:10:05 +0000

    ms: Adding CH3Cl, HCl

--- trunk/simulations-java/simulations/molecule-shapes/src/edu/colorado/phet/moleculeshapes/view/MoleculeJMEApplication.java
+++ trunk/simulations-java/simulations/molecule-shapes/src/edu/colorado/phet/moleculeshapes/view/MoleculeJMEApplication.java
@@ -59,7 +59,6 @@ import static edu.colorado.phet.moleculeshapes.MoleculeShapesConstants.OUTSIDE_P
  * TODO: More molecules
  * TODO: make a version where I can change the toolbar background, and the font color?
  * TODO: tweak angle visibility for different numbers of bonds
- * TODO: molecules A few more examples we could use include PF5 (trigonal bipyramid), CH3Cl or CH3Br (tetrahedral), HCl (diatomic??)
  * <p/>
  * TODO: better MVC (especially C) to deal with all of these threading issues
  * TODO: clearer separation of JME / Swing thread code (basically, a lot of cleanup and documentation)
