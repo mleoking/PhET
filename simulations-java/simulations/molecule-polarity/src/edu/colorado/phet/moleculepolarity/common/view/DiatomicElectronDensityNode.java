4c3ffff154dacbb69cacd9409712500b5d943c03
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-09-08 20:59:17 +0000

    9/8/11 set correct endpoints for gradient in 2D electrostatic potential

--- simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/common/view/DiatomicElectronDensityNode.java
+++ simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/common/view/DiatomicElectronDensityNode.java
@@ -111,9 +111,8 @@ public class DiatomicElectronDensityNode extends PComposite {
             final double gradientWidth = f.evaluate( scale );
 
             // gradient endpoints prior to accounting for molecule transform
-            final double xOffset = gradientWidth / 2;
-            Point2D pointA = new Point2D.Double( -xOffset, 0 );
-            Point2D pointB = new Point2D.Double( xOffset, 0 );
+            Point2D pointA = new Point2D.Double( -gradientWidth / 2, 0 );
+            Point2D pointB = new Point2D.Double( gradientWidth / 2, 0 );
 
             // transform gradient endpoints to account for molecule transform
             AffineTransform transform = new AffineTransform();
