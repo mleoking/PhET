4368eb24dae422edf7a432d0e47256158c746218
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-09-09 19:52:51 +0000

    Hooked the (currently stubbed) model for the game into the module.

--- simulations-java/simulations/balance-and-torque/src/edu/colorado/phet/balanceandtorque/game/BalanceGameModule.java
+++ simulations-java/simulations/balance-and-torque/src/edu/colorado/phet/balanceandtorque/game/BalanceGameModule.java
@@ -1,8 +1,8 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.balanceandtorque.game;
 
+import edu.colorado.phet.balanceandtorque.game.model.BalanceGameModel;
 import edu.colorado.phet.balanceandtorque.game.view.BalanceGameCanvas;
-import edu.colorado.phet.balanceandtorque.teetertotter.model.BalancingActModel;
 import edu.colorado.phet.common.phetcommon.application.Module;
 
 /**
@@ -13,11 +13,11 @@ import edu.colorado.phet.common.phetcommon.application.Module;
 public class BalanceGameModule extends Module {
 
     public BalanceGameModule() {
-        this( new BalancingActModel() );
+        this( new BalanceGameModel() );
         setClockControlPanel( null );
     }
 
-    private BalanceGameModule( BalancingActModel model ) {
+    private BalanceGameModule( BalanceGameModel model ) {
         // TODO: i18n
         super( "Game", model.getClock() );
         setSimulationPanel( new BalanceGameCanvas() );
