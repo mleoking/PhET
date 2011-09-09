3f18fe8f6dac137ca20ef097fc7f6c0b96628cad
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-09-09 19:52:51 +0000

    Hooked the (currently stubbed) model for the game into the module.

--- trunk/simulations-java/simulations/balance-and-torque/src/edu/colorado/phet/balanceandtorque/game/model/BalanceGameModel.java
+++ trunk/simulations-java/simulations/balance-and-torque/src/edu/colorado/phet/balanceandtorque/game/model/BalanceGameModel.java
@@ -1,10 +1,174 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.balanceandtorque.game.model;
 
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.colorado.phet.balanceandtorque.teetertotter.BalanceAndTorqueSharedConstants;
+import edu.colorado.phet.balanceandtorque.teetertotter.model.AttachmentBar;
+import edu.colorado.phet.balanceandtorque.teetertotter.model.FulcrumAbovePlank;
+import edu.colorado.phet.balanceandtorque.teetertotter.model.Plank;
+import edu.colorado.phet.balanceandtorque.teetertotter.model.SupportColumn;
+import edu.colorado.phet.balanceandtorque.teetertotter.model.UserMovableModelElement;
+import edu.colorado.phet.balanceandtorque.teetertotter.model.masses.Mass;
+import edu.colorado.phet.common.phetcommon.model.clock.ClockAdapter;
+import edu.colorado.phet.common.phetcommon.model.clock.ClockEvent;
+import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
+import edu.colorado.phet.common.phetcommon.model.property.BooleanProperty;
+import edu.colorado.phet.common.phetcommon.model.property.ChangeObserver;
+import edu.colorado.phet.common.phetcommon.util.ObservableList;
+import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
+
 /**
  * Main model class for the balance game.
  *
  * @author John Blanco
  */
 public class BalanceGameModel {
+    //------------------------------------------------------------------------
+    // Class Data
+    //------------------------------------------------------------------------
+
+    private static final double FULCRUM_HEIGHT = 0.85; // In meters.
+    private static final double PLANK_HEIGHT = 0.75; // In meters.
+
+    //------------------------------------------------------------------------
+    // Instance Data
+    //------------------------------------------------------------------------
+
+    // Clock that drives all time-dependent behavior in this model.
+    private final ConstantDtClock clock = new ConstantDtClock( BalanceAndTorqueSharedConstants.FRAME_RATE );
+
+    // A list of all the masses in the model
+    public final ObservableList<Mass> massList = new ObservableList<Mass>();
+
+    // Fulcrum on which the plank pivots
+    private final FulcrumAbovePlank fulcrum = new FulcrumAbovePlank( 1, FULCRUM_HEIGHT );
+
+    // Support columns
+    private final List<SupportColumn> supportColumns = new ArrayList<SupportColumn>() {{
+        // Note: These are positioned so that the closing window that is
+        // placed on them (the red x) is between two snap-to points on the
+        // plank that the they don't get blocked by force vectors.
+        add( new SupportColumn( PLANK_HEIGHT, -1.625 ) );
+        add( new SupportColumn( PLANK_HEIGHT, 1.625 ) );
+    }};
+
+    // Property that controls whether the columns are supporting the plank.
+    public final BooleanProperty supportColumnsActive = new BooleanProperty( true );
+
+    // Plank upon which the various masses can be placed.
+    private final Plank plank = new Plank( clock,
+                                           new Point2D.Double( 0, PLANK_HEIGHT ),
+                                           new Point2D.Double( 0, FULCRUM_HEIGHT ),
+                                           supportColumnsActive );
+
+    // Bar that attaches the fulcrum to the pivot point.
+    private final AttachmentBar attachmentBar = new AttachmentBar( plank );
+
+    //------------------------------------------------------------------------
+    // Constructor(s)
+    //------------------------------------------------------------------------
+
+    public BalanceGameModel() {
+        clock.addClockListener( new ClockAdapter() {
+            @Override public void clockTicked( ClockEvent clockEvent ) {
+                stepInTime( clock.getDt() );
+            }
+        } );
+    }
+
+    //------------------------------------------------------------------------
+    // Methods
+    //------------------------------------------------------------------------
+
+    public ConstantDtClock getClock() {
+        return clock;
+    }
+
+    private void stepInTime( double dt ) {
+        for ( Mass mass : new ArrayList<Mass>( massList ) ) {
+            mass.stepInTime( dt );
+        }
+    }
+
+    // Adds a mass to the model.
+    public UserMovableModelElement addMass( final Mass mass ) {
+        mass.userControlled.addObserver( new VoidFunction1<Boolean>() {
+            public void apply( Boolean userControlled ) {
+                if ( !userControlled ) {
+                    // The user has dropped this mass.
+                    if ( !plank.addMassToSurface( mass ) ) {
+                        // The attempt to add mass to surface of plank failed,
+                        // probably because the area below the mass is full,
+                        // or because the mass wasn't over the plank.
+                        removeMassAnimated( mass );
+                    }
+                }
+            }
+        } );
+        massList.add( mass );
+        return mass;
+    }
+
+    /**
+     * Remove the mass from the model and animate its removal.
+     *
+     * @param mass
+     */
+    private void removeMassAnimated( final Mass mass ) {
+        // Register a listener for the completion of the removal animation sequence.
+        mass.addAnimationStateObserver( new ChangeObserver<Boolean>() {
+            public void update( Boolean isAnimating, Boolean wasAnimating ) {
+                if ( wasAnimating && !isAnimating ) {
+                    // Animation sequence has completed.
+                    mass.removeAnimationStateObserver( this );
+                    massList.remove( mass );
+                }
+            }
+        } );
+        // Kick off the animation back to the tool box.
+        mass.initiateAnimation();
+    }
+
+    /**
+     * Removes a mass right away, with no animation.
+     *
+     * @param mass
+     */
+    private void removeMass( final Mass mass ) {
+        massList.remove( mass );
+    }
+
+    public FulcrumAbovePlank getFulcrum() {
+        return fulcrum;
+    }
+
+    public Plank getPlank() {
+        return plank;
+    }
+
+    public AttachmentBar getAttachmentBar() {
+        return attachmentBar;
+    }
+
+    public List<SupportColumn> getSupportColumns() {
+        return supportColumns;
+    }
+
+    public void reset() {
+        getClock().resetSimulationTime();
+
+        // Remove masses from the plank.
+        plank.removeAllMasses();
+
+        // Remove this model's references to the masses.
+        for ( Mass mass : new ArrayList<Mass>( massList ) ) {
+            removeMass( mass );
+        }
+
+        // Set the support columns to their initial state.
+        supportColumnsActive.reset();
+    }
 }
