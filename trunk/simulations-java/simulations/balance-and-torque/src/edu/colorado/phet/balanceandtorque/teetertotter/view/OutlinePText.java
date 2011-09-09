c1f2472a4476072a57adda172b4ffa81fc4bd19f
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-09-09 17:56:15 +0000

    Simplified, made text, colors, & font immutable, added a bunch of things to the test harness.

--- trunk/simulations-java/simulations/balance-and-torque/src/edu/colorado/phet/balanceandtorque/teetertotter/view/OutlinePText.java
+++ trunk/simulations-java/simulations/balance-and-torque/src/edu/colorado/phet/balanceandtorque/teetertotter/view/OutlinePText.java
@@ -4,67 +4,51 @@ package edu.colorado.phet.balanceandtorque.teetertotter.view;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.Point;
 import java.awt.font.FontRenderContext;
 import java.awt.font.TextLayout;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Dimension2D;
-import java.awt.geom.Point2D;
-import java.awt.geom.Rectangle2D;
 
 import javax.swing.JFrame;
 
-import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.PhetPCanvas;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.nodes.PPath;
 import edu.umd.cs.piccolo.util.PDimension;
 
 /**
+ * PNode that represents outlined text.
+ *
+ * @author John Blanco
  * @author Sam Reid
  */
 public class OutlinePText extends PNode {
 
-    private String text;
-    private Font font;
-    private FontRenderContext fontRenderContext;
-    private PhetPPath render;
+    // Font render context used for outline text.  Honestly, I (jblanco) don't
+    // know much about font render contexts, but I found a Piccolo node called
+    // PStyledText that did this, so I tried it, and it seems to work okay.
+    // This should be tested in some other environments (e.g. Mac) too.
+    private static FontRenderContext SWING_FRC = new FontRenderContext( null, true, false );
 
-    public OutlinePText( String text, Color fillColor, Color outlineColor, Font font, FontRenderContext fontRenderContext ) {
-        this.text = text;
-        this.font = font;
-        this.fontRenderContext = fontRenderContext;
-        this.render = new PhetPPath( fillColor, new BasicStroke( 1 ), outlineColor );
-        addChild( render );
-        update();
-    }
-
-    void update() {
-        TextLayout textLayout = new TextLayout( text, font, fontRenderContext );
-        render.setPathTo( textLayout.getOutline( new AffineTransform() ) );
-    }
-
-    public void setFont( Font font ) {
-        this.font = font;
-        update();
-    }
-
-    public void setTextPaint( Color color ) {
-        render.setPaint( color );
-    }
-
-    public void setFillColor( Color color ) {
-        render.setStrokePaint( color );
-    }
-
-    public void setText( String valueText ) {
-        this.text = valueText;
-        update();
+    /**
+     * Constructor.
+     *
+     * @param text
+     * @param font
+     * @param fillColor
+     * @param outlineColor
+     */
+    public OutlinePText( String text, Font font, Color fillColor, Color outlineColor, double outlineStrokeWidth ) {
+        PPath textPPath = new PhetPPath( fillColor, new BasicStroke( (float) outlineStrokeWidth ), outlineColor );
+        TextLayout textLayout = new TextLayout( text, font, SWING_FRC );
+        textPPath.setPathTo( textLayout.getOutline( new AffineTransform() ) );
+        addChild( textPPath );
     }
 
     /**
-     * Main routine that constructs a PhET Piccolo canvas in a window.
+     * Test harness.
      *
      * @param args
      */
@@ -75,19 +59,22 @@ public class OutlinePText extends PNode {
         // Set up the canvas-screen transform.
         canvas.setWorldTransformStrategy( new PhetPCanvas.CenteredStage( canvas, STAGE_SIZE ) );
 
-        ModelViewTransform mvt = ModelViewTransform.createSinglePointScaleInvertedYMapping(
-                new Point2D.Double( 0, 0 ),
-                new Point( (int) Math.round( STAGE_SIZE.getWidth() * 0.50 ), (int) Math.round( STAGE_SIZE.getHeight() * 0.50 ) ),
-                1 ); // "Zoom factor" - smaller zooms out, larger zooms in.
+        // Add the text.
+        OutlinePText outlineTextNode1 = new OutlinePText( "36 point plain", new PhetFont( 36 ), Color.PINK, Color.BLACK, 1 );
+        outlineTextNode1.setOffset( 50, 50 );
+        canvas.addWorldChild( outlineTextNode1 );
+        OutlinePText outlineTextNode2 = new OutlinePText( "48 point bold", new PhetFont( 48, true ), Color.YELLOW, Color.BLACK, 1 );
+        outlineTextNode2.setOffset( 50, 100 );
+        canvas.addWorldChild( outlineTextNode2 );
+        OutlinePText outlineTextNode3 = new OutlinePText( "24 point bold", new PhetFont( 24, true ), Color.GREEN, Color.BLACK, 1 );
+        outlineTextNode3.setOffset( 50, 150 );
+        canvas.addWorldChild( outlineTextNode3 );
+        OutlinePText outlineTextNode4 = new OutlinePText( "72 point bold", new PhetFont( 72, true ), Color.MAGENTA, Color.BLACK, 2 );
+        outlineTextNode4.setOffset( 50, 250 );
+        canvas.addWorldChild( outlineTextNode4 );
 
-        OutlinePText outlineTextNode = new OutlinePText( "Hey", Color.PINK, Color.BLACK, new PhetFont( 24 ), new FontRenderContext( new AffineTransform(), true, true ) );
-        canvas.addWorldChild( outlineTextNode );
-        outlineTextNode.setOffset( 100, 100 );
-
-        canvas.getLayer().addChild( new PhetPPath( new Rectangle2D.Double( -5, -5, 10, 10 ), Color.PINK ) );
-
-        // Boiler plate app stuff.
-        JFrame frame = new JFrame( "RNA Polymerase Shape Testing" );
+        // Boiler plate Piccolo app stuff.
+        JFrame frame = new JFrame( "Outline Text Test" );
         frame.setContentPane( canvas );
         frame.setSize( (int) STAGE_SIZE.getWidth(), (int) STAGE_SIZE.getHeight() );
         frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
