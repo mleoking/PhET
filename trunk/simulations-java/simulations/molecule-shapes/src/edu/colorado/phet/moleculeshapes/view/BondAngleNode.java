306baf692f269338ce10c26d2ac8b170d0caf43e
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-09-10 22:27:32 +0000

    ms: minor refactoring of names

--- trunk/simulations-java/simulations/molecule-shapes/src/edu/colorado/phet/moleculeshapes/view/BondAngleNode.java
+++ trunk/simulations-java/simulations/molecule-shapes/src/edu/colorado/phet/moleculeshapes/view/BondAngleNode.java
@@ -22,10 +22,10 @@ public class BondAngleNode extends Node {
     private PointArc arc;
 
     // TODO: docs and cleanup, and move out if kept
-    public BondAngleNode( final MoleculeJMEApplication app, MoleculeModel molecule, ImmutableVector3D aDir, ImmutableVector3D bDir, Vector3f transformedDirection, Vector3f lastMidpoint ) {
+    public BondAngleNode( final MoleculeJMEApplication app, MoleculeModel molecule, ImmutableVector3D aDir, ImmutableVector3D bDir, Vector3f localCameraPosition, Vector3f lastMidpoint ) {
         super( "Bond Angle" );
         float radius = 5;
-        final float alpha = calculateBrightness( aDir, bDir, transformedDirection, molecule.getBondedGroups().size() );
+        final float alpha = calculateBrightness( aDir, bDir, localCameraPosition, molecule.getBondedGroups().size() );
 
         Vector3f a = JmeUtils.convertVector( aDir );
         Vector3f b = JmeUtils.convertVector( bDir );
@@ -65,14 +65,14 @@ public class BondAngleNode extends Node {
     private static final float[] lowThresholds = new float[] { 0, 0, 0, 0, 0.35f, 0.45f, 0.5f };
     private static final float[] highThresholds = new float[] { 0, 0, 0, 0.5f, 0.55f, 0.65f, 0.75f };
 
-    public static float calculateBrightness( ImmutableVector3D aDir, ImmutableVector3D bDir, Vector3f transformedDirection, int bondQuantity ) {
+    public static float calculateBrightness( ImmutableVector3D aDir, ImmutableVector3D bDir, Vector3f localCameraPosition, int bondQuantity ) {
         // if there are less than 3 bonds, always show the bond angle. (experimental)
         if ( bondQuantity <= 2 ) {
             return 1;
         }
 
         // a combined measure of how close the angles are AND how orthogonal they are to the camera
-        float brightness = (float) Math.abs( aDir.cross( bDir ).dot( JmeUtils.convertVector( transformedDirection ) ) );
+        float brightness = (float) Math.abs( aDir.cross( bDir ).dot( JmeUtils.convertVector( localCameraPosition ) ) );
 
         // DEV version for finding good-looking constants
 //        float lowThreshold = MoleculeShapesProperties.minimumBrightnessFade.get().floatValue();
