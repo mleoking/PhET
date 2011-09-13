19e0bd930accf54b2b57de7b5df231363648fc5c
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-09-13 22:41:56 +0000

    9/13/11 first pass at bond angle drag indicators (visibility behavior is correct, shape is wrong)

---
+++ simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/common/view/BondAngleDragIndicatorNode.java
@@ -0,0 +1,45 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.moleculepolarity.common.view;
+
+import java.awt.BasicStroke;
+import java.awt.geom.Line2D;
+
+import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
+import edu.colorado.phet.moleculepolarity.common.model.Atom;
+import edu.umd.cs.piccolo.nodes.PPath;
+import edu.umd.cs.piccolox.nodes.PComposite;
+
+/**
+ * Indicator that dragging an atom will change the bond angle.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
+public class BondAngleDragIndicatorNode extends PComposite {
+
+    private final Atom atom;
+    private final PPath barNode;
+
+    public BondAngleDragIndicatorNode( final Atom atom ) {
+
+        this.atom = atom;
+
+        this.barNode = new PPath() {{
+            setStroke( new BasicStroke( 12f ) );
+            setStrokePaint( atom.getColor() );
+        }};
+        addChild( barNode );
+
+        atom.location.addObserver( new SimpleObserver() {
+            public void update() {
+                updateNode();
+            }
+        } );
+    }
+
+    private void updateNode() {
+        double length = atom.getDiameter() + 40;
+        double x = atom.location.get().getX() - ( length / 2 );
+        double y = atom.location.get().getY();
+        barNode.setPathTo( new Line2D.Double( x, y, x + length, y ) );
+    }
+}
