c2b3593ac9530d69a62632dae5d152fc8ed0bc47
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-09-13 22:41:56 +0000

    9/13/11 first pass at bond angle drag indicators (visibility behavior is correct, shape is wrong)

--- trunk/simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/common/view/TriatomicMoleculeNode.java
+++ trunk/simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/common/view/TriatomicMoleculeNode.java
@@ -5,6 +5,7 @@ import edu.colorado.phet.common.piccolophet.PhetPNode;
 import edu.colorado.phet.common.piccolophet.event.CursorHandler;
 import edu.colorado.phet.moleculepolarity.common.control.BondAngleHandler;
 import edu.colorado.phet.moleculepolarity.common.control.MoleculeRotationHandler;
+import edu.colorado.phet.moleculepolarity.common.control.MouseOverVisibilityHandler;
 import edu.colorado.phet.moleculepolarity.common.control.MoveToFrontHandler;
 import edu.colorado.phet.moleculepolarity.common.control.RotateCursorHandler;
 import edu.colorado.phet.moleculepolarity.common.model.TriatomicMolecule;
@@ -25,10 +26,18 @@ public class TriatomicMoleculeNode extends PhetPNode {
         AtomNode atomANode = new AtomNode( molecule.atomA );
         AtomNode atomBNode = new AtomNode( molecule.atomB );
         AtomNode atomCNode = new AtomNode( molecule.atomC );
+        final BondAngleDragIndicatorNode indicatorANode = new BondAngleDragIndicatorNode( molecule.atomA ) {{
+            setVisible( false );
+        }};
+        final BondAngleDragIndicatorNode indicatorCNode = new BondAngleDragIndicatorNode( molecule.atomC ) {{
+            setVisible( false );
+        }};
 
         // rendering order, bonds behind atoms
         addChild( bondABNode );
         addChild( bondBCNode );
+        addChild( indicatorANode );
+        addChild( indicatorCNode );
         addChild( atomANode );
         addChild( atomBNode );
         addChild( atomCNode );
@@ -49,5 +58,9 @@ public class TriatomicMoleculeNode extends PhetPNode {
         // move the atom being dragged to the front
         atomANode.addInputEventListener( new MoveToFrontHandler( atomANode ) );
         atomCNode.addInputEventListener( new MoveToFrontHandler( atomCNode ) );
+
+        // make bond angle indicators visible only on mouseOver
+        atomANode.addInputEventListener( new MouseOverVisibilityHandler( indicatorANode ) );
+        atomCNode.addInputEventListener( new MouseOverVisibilityHandler( indicatorCNode ) );
     }
 }
