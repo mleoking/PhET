53164baf11300d2666cb3fd88f4b2bc5ad54d2a9
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-09-13 17:14:29 +0000

    9/13/11 permanently remove E-field features from "Real Molecules" tab

--- simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/realmolecules/RealMoleculesCanvas.java
+++ simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/realmolecules/RealMoleculesCanvas.java
@@ -9,9 +9,7 @@ import edu.colorado.phet.common.phetcommon.model.Resettable;
 import edu.colorado.phet.common.phetcommon.util.RichSimpleObserver;
 import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
 import edu.colorado.phet.common.piccolophet.nodes.ControlPanelNode;
-import edu.colorado.phet.common.piccolophet.util.PNodeLayoutUtils;
 import edu.colorado.phet.moleculepolarity.MPStrings;
-import edu.colorado.phet.moleculepolarity.common.control.EFieldControlPanel;
 import edu.colorado.phet.moleculepolarity.common.control.MPResetAllButtonNode;
 import edu.colorado.phet.moleculepolarity.common.control.MoleculeControlNode;
 import edu.colorado.phet.moleculepolarity.common.control.SurfaceControlPanel;
@@ -19,8 +17,6 @@ import edu.colorado.phet.moleculepolarity.common.control.ViewControlPanel;
 import edu.colorado.phet.moleculepolarity.common.view.ElectronegativityTableNode;
 import edu.colorado.phet.moleculepolarity.common.view.JmolViewerNode;
 import edu.colorado.phet.moleculepolarity.common.view.MPCanvas;
-import edu.colorado.phet.moleculepolarity.common.view.NegativePlateNode;
-import edu.colorado.phet.moleculepolarity.common.view.PositivePlateNode;
 import edu.colorado.phet.moleculepolarity.common.view.SurfaceColorKeyNode.ElectronDensityColorKeyNode;
 import edu.colorado.phet.moleculepolarity.common.view.SurfaceColorKeyNode.ElectrostaticPotentialColorKeyNode;
 import edu.colorado.phet.moleculepolarity.common.view.SurfaceColorKeyNode.RainbowElectrostaticPotentialColorKeyNode;
@@ -42,12 +38,9 @@ public class RealMoleculesCanvas extends MPCanvas {
         super();
 
         // nodes
-        PNode negativePlateNode = new NegativePlateNode( model.eField );
-        PNode positivePlateNode = new PositivePlateNode( model.eField );
         final JmolViewerNode viewerNode = new JmolViewerNode( model.currentMolecule, getBackground(), JMOL_VIEWER_SIZE );
         PNode viewControlsNode = new ControlPanelNode( new ViewControlPanel( viewProperties, true, false, true, true, MPStrings.BOND_DIPOLES ) );
         PNode isosurfaceControlsNode = new ControlPanelNode( new SurfaceControlPanel( viewProperties.isosurfaceType ) );
-        PNode eFieldControlsNode = new ControlPanelNode( new EFieldControlPanel( model.eField.enabled ) );
         PNode resetAllButtonNode = new MPResetAllButtonNode( new Resettable[] { model, viewProperties }, parentFrame );
         PNode scriptNode = new JmolScriptNode( viewerNode );
         final ElectronegativityTableNode electronegativityTableNode = new ElectronegativityTableNode( viewerNode );
@@ -58,14 +51,9 @@ public class RealMoleculesCanvas extends MPCanvas {
 
         // rendering order
         {
-            // plates
-//            addChild( negativePlateNode );
-//            addChild( positivePlateNode );
-
             // controls
             addChild( viewControlsNode );
             addChild( isosurfaceControlsNode );
-//            addChild( eFieldControlsNode );
             addChild( resetAllButtonNode );
             if ( PhetApplication.getInstance().isDeveloperControlsEnabled() ) {
                 addChild( scriptNode );
@@ -88,9 +76,7 @@ public class RealMoleculesCanvas extends MPCanvas {
         {
             final double xSpacing = 50;
             final double ySpacing = 10;
-            negativePlateNode.setOffset( 30, 30 - PNodeLayoutUtils.getOriginYOffset( negativePlateNode ) );
-            viewerNode.setOffset( negativePlateNode.getFullBoundsReference().getMaxX() + xSpacing, negativePlateNode.getYOffset() );
-            positivePlateNode.setOffset( viewerNode.getFullBoundsReference().getMaxX() + xSpacing, negativePlateNode.getYOffset() );
+            viewerNode.setOffset( 0, 30 );
             electrostaticPotentialColorKeyNode.setOffset( viewerNode.getFullBoundsReference().getCenterX() - ( electrostaticPotentialColorKeyNode.getFullBoundsReference().getWidth() / 2 ),
                                                           viewerNode.getFullBoundsReference().getMaxY() + ySpacing );
             rainbowColorKeyNode.setOffset( electrostaticPotentialColorKeyNode.getOffset() );
@@ -99,10 +85,9 @@ public class RealMoleculesCanvas extends MPCanvas {
                                                   electrostaticPotentialColorKeyNode.getFullBoundsReference().getMaxY() + 20 );
             moleculeComboBox.setOffset( viewerNode.getFullBoundsReference().getCenterX() - ( moleculeComboBox.getFullBoundsReference().getWidth() / 2 ),
                                         viewerNode.getFullBoundsReference().getMinY() - moleculeComboBox.getFullBoundsReference().getHeight() - 30 );
-            viewControlsNode.setOffset( positivePlateNode.getFullBoundsReference().getMaxX() + xSpacing, positivePlateNode.getYOffset() );
+            viewControlsNode.setOffset( viewerNode.getFullBoundsReference().getMaxX() + xSpacing, viewerNode.getYOffset() );
             isosurfaceControlsNode.setOffset( viewControlsNode.getXOffset(), viewControlsNode.getFullBoundsReference().getMaxY() + ySpacing );
-            eFieldControlsNode.setOffset( isosurfaceControlsNode.getXOffset(), isosurfaceControlsNode.getFullBoundsReference().getMaxY() + ySpacing );
-            resetAllButtonNode.setOffset( isosurfaceControlsNode.getXOffset(), eFieldControlsNode.getFullBoundsReference().getMaxY() + ySpacing );
+            resetAllButtonNode.setOffset( isosurfaceControlsNode.getXOffset(), isosurfaceControlsNode.getFullBoundsReference().getMaxY() + ySpacing );
             scriptNode.setOffset( resetAllButtonNode.getXOffset(), resetAllButtonNode.getFullBoundsReference().getMaxY() + ySpacing );
 
         }
