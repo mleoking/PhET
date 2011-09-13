18bb756d9265ad963862b7af00e19ecdc02006c2
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-09-13 23:21:48 +0000

    9/13/11 move bond angle indicator to front on mouse over

--- simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/common/control/BondAngleHandler.java
+++ simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/common/control/BondAngleHandler.java
@@ -4,7 +4,8 @@ package edu.colorado.phet.moleculepolarity.common.control;
 import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
 import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.moleculepolarity.common.model.IMolecule;
-import edu.umd.cs.piccolo.PNode;
+import edu.colorado.phet.moleculepolarity.common.view.AtomNode;
+import edu.colorado.phet.moleculepolarity.common.view.BondAngleDragIndicatorNode;
 import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
 import edu.umd.cs.piccolo.event.PInputEvent;
 
@@ -17,7 +18,8 @@ public class BondAngleHandler extends PBasicInputEventHandler {
 
     private final IMolecule molecule;
     private final Property<Double> bondAngle;
-    private final PNode dragNode;
+    private final AtomNode atomNode;
+    private final BondAngleDragIndicatorNode indicatorNode;
     double previousAngle;
 
     /**
@@ -25,18 +27,20 @@ public class BondAngleHandler extends PBasicInputEventHandler {
      *
      * @param molecule  angle is relative to this molecule's location, and we pause any animation of this molecule while dragging
      * @param bondAngle property that this handler modifies
-     * @param dragNode  node that is being dragged
+     * @param atomNode  node that is being dragged
      */
-    public BondAngleHandler( IMolecule molecule, Property<Double> bondAngle, PNode dragNode ) {
+    public BondAngleHandler( IMolecule molecule, Property<Double> bondAngle, AtomNode atomNode, BondAngleDragIndicatorNode indicatorNode ) {
         this.molecule = molecule;
         this.bondAngle = bondAngle;
-        this.dragNode = dragNode;
+        this.atomNode = atomNode;
+        this.indicatorNode = indicatorNode;
     }
 
     @Override public void mousePressed( PInputEvent event ) {
         molecule.setDragging( true );
         previousAngle = getAngle( event ); //Store the original angle since rotations are computed as deltas between each event
-        dragNode.moveToFront();
+        atomNode.moveToFront();
+        indicatorNode.moveInBackOf( atomNode );
     }
 
     @Override public void mouseReleased( PInputEvent event ) {
@@ -52,6 +56,6 @@ public class BondAngleHandler extends PBasicInputEventHandler {
 
     // Find the angle about the molecule's location.
     private double getAngle( PInputEvent event ) {
-        return new ImmutableVector2D( molecule.getLocation().toPoint2D(), event.getPositionRelativeTo( dragNode.getParent() ) ).getAngle();
+        return new ImmutableVector2D( molecule.getLocation().toPoint2D(), event.getPositionRelativeTo( atomNode.getParent() ) ).getAngle();
     }
 }
