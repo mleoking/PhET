753922ec2711c645271caad1822dcf7c76942c59
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-09-14 18:09:12 +0000

    9/14/2011 Copied ArrowButtonNode to piccolo-phet from molecule-shapes, added border to show up against white background and made default button for kits

--- trunk/simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/nodes/kit/BackButton.java
+++ trunk/simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/nodes/kit/BackButton.java
@@ -1,15 +1,15 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.common.piccolophet.nodes.kit;
 
-import static edu.colorado.phet.common.phetcommon.view.util.BufferedImageUtils.flipX;
+import edu.colorado.phet.common.piccolophet.nodes.ArrowButtonNode;
 
 /**
  * Button for moving backwards through the kits
  *
  * @author Sam Reid
  */
-public class BackButton extends ArrowButton {
+public class BackButton extends ArrowButtonNode {
     public BackButton() {
-        super( flipX( RESOURCES.getImage( "gray-arrow.png" ) ) );
+        super( Orientation.LEFT );
     }
 }
