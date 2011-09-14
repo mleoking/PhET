5c6761f22d7434008fb7db5e4b29927269383ad7
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-09-14 04:10:29 +0000

    9/13/11 use double arrows to indicate dragging direction for changing bond angle

--- simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/common/view/BondAngleDragIndicatorNode.java
+++ simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/common/view/BondAngleDragIndicatorNode.java
@@ -4,10 +4,11 @@ package edu.colorado.phet.moleculepolarity.common.view;
 import java.awt.Color;
 import java.awt.Shape;
 import java.awt.geom.AffineTransform;
-import java.awt.geom.Rectangle2D;
+import java.awt.geom.Point2D;
 
 import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
+import edu.colorado.phet.common.phetcommon.view.graphics.Arrow;
 import edu.colorado.phet.moleculepolarity.common.model.Atom;
 import edu.colorado.phet.moleculepolarity.common.model.IMolecule;
 import edu.umd.cs.piccolo.nodes.PPath;
@@ -15,14 +16,18 @@ import edu.umd.cs.piccolox.nodes.PComposite;
 
 /**
  * Indicator that dragging an atom will change the bond angle.
+ * This consists of a pair of arrows on either side of the atom,
+ * rotated to indicate the drag direction that will result in a change to bond angle.
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
 public class BondAngleDragIndicatorNode extends PComposite {
 
+    private static final int ARROW_LENGTH = 25; // relatively short, so we don't need curved arrows
+
     private final IMolecule molecule;
     private final Atom atom;
-    private final PPath pathNode;
+    private final PPath leftNode, rightNode;
 
     public BondAngleDragIndicatorNode( final IMolecule molecule, final Atom atom ) {
 
@@ -33,11 +38,17 @@ public class BondAngleDragIndicatorNode extends PComposite {
         this.molecule = molecule;
         this.atom = atom;
 
-        pathNode = new PPath() {{
+        leftNode = new PPath() {{
             setPaint( atom.getColor() );
             setStrokePaint( Color.GRAY );
         }};
-        addChild( pathNode );
+        addChild( leftNode );
+
+        rightNode = new PPath() {{
+            setPaint( atom.getColor() );
+            setStrokePaint( Color.GRAY );
+        }};
+        addChild( rightNode );
 
         atom.location.addObserver( new SimpleObserver() {
             public void update() {
@@ -48,19 +59,29 @@ public class BondAngleDragIndicatorNode extends PComposite {
 
     private void updateNode() {
 
-        // create the "normalized" shape at (0,0)
-        double length = atom.getDiameter() + 60;
-        double thickness = 20;
-        Shape normalShape = new Rectangle2D.Double( -length / 2, -thickness / 2, length, thickness );
+        // create "normalized" shapes at (0,0) with no rotation
+        final double radius = atom.getDiameter() / 2;
+        final double spacing = 2;
+        IndicatorArrow leftArrow = new IndicatorArrow( new Point2D.Double( -( radius + spacing ), 0 ), new Point2D.Double( -( radius + spacing + ARROW_LENGTH ), 0 ) );
+        IndicatorArrow rightArrow = new IndicatorArrow( new Point2D.Double( ( radius + spacing ), 0 ), new Point2D.Double( ( radius + spacing + ARROW_LENGTH ), 0 ) );
 
-        // transform the shape to account for atom location and relationship to molecule location
+        // transform the shapes to account for atom location and relationship to molecule location
         ImmutableVector2D v = new ImmutableVector2D( molecule.getLocation(), atom.location.get() );
         double angle = v.getAngle() - ( Math.PI / 2 );
         AffineTransform transform = new AffineTransform();
         transform.translate( atom.location.get().getX(), atom.location.get().getY() );
         transform.rotate( angle );
-        Shape shape = transform.createTransformedShape( normalShape );
+        Shape leftShape = transform.createTransformedShape( leftArrow.getShape() );
+        Shape rightShape = transform.createTransformedShape( rightArrow.getShape() );
 
-        pathNode.setPathTo( shape );
+        leftNode.setPathTo( leftShape );
+        rightNode.setPathTo( rightShape );
+    }
+
+    // Encapsulates the "look" of the arrows.
+    private static class IndicatorArrow extends Arrow {
+        public IndicatorArrow( Point2D pTail, Point2D pTip ) {
+            super( pTail, pTip, 20, 20, 10 );
+        }
     }
 }
