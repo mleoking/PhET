118b46ef66a1c981b8c0838e4183f1ba455e68fa
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-09-15 20:27:40 +0000

    Consolidated the code for managing people who can be moved on and off the plank (i.e. used as masses).

--- simulations-java/simulations/balance-and-torque/src/edu/colorado/phet/balanceandtorque/teetertotter/model/masses/AdolescentBoy.java
+++ simulations-java/simulations/balance-and-torque/src/edu/colorado/phet/balanceandtorque/teetertotter/model/masses/AdolescentBoy.java
@@ -4,15 +4,14 @@ package edu.colorado.phet.balanceandtorque.teetertotter.model.masses;
 import java.awt.geom.Point2D;
 
 import edu.colorado.phet.balanceandtorque.BalanceAndTorqueResources.Images;
-import edu.colorado.phet.common.phetcommon.view.util.BufferedImageUtils;
 
 
 /**
- * Model class that represents a boy who is roughly 10 years old.
+ * Model class that represents a boy who is roughly 9 years old.
  *
  * @author John Blanco
  */
-public class AdolescentBoy extends ImageMass {
+public class AdolescentBoy extends HumanMass {
 
     private static final double MASS = 20; // in kg
     private static final double STANDING_HEIGHT = 1.1; // In meters.
@@ -20,24 +19,6 @@ public class AdolescentBoy extends ImageMass {
     private static final double SITTING_CENTER_OF_MASS_X_OFFSET = 0.07; // In meters, determined visually.  Update if image changes.
 
     public AdolescentBoy() {
-        super( MASS, Images.ADOLESCENT_STANDING, STANDING_HEIGHT, new Point2D.Double( 0, 0 ) );
-    }
-
-    @Override public void setOnPlank( boolean onPlank ) {
-        if ( onPlank ) {
-            heightProperty.set( SITTING_HEIGHT );
-            if ( getPosition().getX() > 0 ) {
-                imageProperty.set( Images.COMPACT_ADOLESCENT_SITTING );
-                setCenterOfMassXOffset( SITTING_CENTER_OF_MASS_X_OFFSET );
-            }
-            else {
-                imageProperty.set( BufferedImageUtils.flipX( Images.COMPACT_ADOLESCENT_SITTING ) );
-                setCenterOfMassXOffset( -SITTING_CENTER_OF_MASS_X_OFFSET );
-            }
-        }
-        else {
-            heightProperty.set( STANDING_HEIGHT );
-            imageProperty.set( Images.ADOLESCENT_STANDING );
-        }
+        super( MASS, Images.ADOLESCENT_STANDING, STANDING_HEIGHT, Images.ADOLESCENT_SITTING, SITTING_HEIGHT, new Point2D.Double( 0, 0 ), SITTING_CENTER_OF_MASS_X_OFFSET, false );
     }
 }
