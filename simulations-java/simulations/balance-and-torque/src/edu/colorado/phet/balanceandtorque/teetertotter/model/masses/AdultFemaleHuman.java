b2f8ce34c0c35a7b774fd301e1a9efa6e45cfb3a
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-09-15 20:23:59 +0000

    Added subclass to consolidate the code for the people that can be put on the balance.  Also started adding ability to clone the various masses.

--- simulations-java/simulations/balance-and-torque/src/edu/colorado/phet/balanceandtorque/teetertotter/model/masses/AdultFemaleHuman.java
+++ simulations-java/simulations/balance-and-torque/src/edu/colorado/phet/balanceandtorque/teetertotter/model/masses/AdultFemaleHuman.java
@@ -4,15 +4,15 @@ package edu.colorado.phet.balanceandtorque.teetertotter.model.masses;
 import java.awt.geom.Point2D;
 
 import edu.colorado.phet.balanceandtorque.BalanceAndTorqueResources.Images;
-import edu.colorado.phet.common.phetcommon.view.util.BufferedImageUtils;
 
 
 /**
- * Model class that represents a woman.
+ * Model class that represents a woman that can be moved on and off of the
+ * balance.
  *
  * @author John Blanco
  */
-public class AdultFemaleHuman extends ImageMass {
+public class AdultFemaleHuman extends HumanMass {
 
     private static final double MASS = 50; // in kg
     private static final double STANDING_HEIGHT = 1.5; // In meters.
@@ -20,26 +20,6 @@ public class AdultFemaleHuman extends ImageMass {
     private static final double SITTING_CENTER_OF_MASS_X_OFFSET = 0.1; // In meters, determined visually.  Update if image changes.
 
     public AdultFemaleHuman() {
-        super( MASS, Images.ADULT_WOMAN_STANDING, STANDING_HEIGHT, new Point2D.Double( 0, 0 ) );
-    }
-
-    @Override public void setOnPlank( boolean onPlank ) {
-        if ( onPlank ) {
-            heightProperty.set( SITTING_HEIGHT );
-            if ( getPosition().getX() > 0 ) {
-                imageProperty.set( Images.ADULT_WOMAN_SITTING );
-                setCenterOfMassXOffset( SITTING_CENTER_OF_MASS_X_OFFSET );
-            }
-            else {
-                // Reverse image if on other side of balance.
-                imageProperty.set( BufferedImageUtils.flipX( Images.ADULT_WOMAN_SITTING ) );
-                setCenterOfMassXOffset( -SITTING_CENTER_OF_MASS_X_OFFSET );
-            }
-        }
-        else {
-            heightProperty.set( STANDING_HEIGHT );
-            imageProperty.set( Images.ADULT_WOMAN_STANDING );
-            setCenterOfMassXOffset( 0 );
-        }
+        super( MASS, Images.ADULT_WOMAN_STANDING, STANDING_HEIGHT, Images.ADULT_WOMAN_SITTING, SITTING_HEIGHT, new Point2D.Double( 0, 0 ), SITTING_CENTER_OF_MASS_X_OFFSET, false );
     }
 }
