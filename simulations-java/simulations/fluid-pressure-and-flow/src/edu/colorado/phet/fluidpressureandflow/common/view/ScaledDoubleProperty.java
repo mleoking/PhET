bc498fcc881643da4bdfeeda0c690c92e2026d51
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-09-19 21:26:12 +0000

    Fluid flow: Fixed: Exception when dragging the fluid flow rate slider on mac

--- simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/common/view/ScaledDoubleProperty.java
+++ simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/common/view/ScaledDoubleProperty.java
@@ -1,6 +1,8 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.fluidpressureandflow.common.view;
 
+import java.math.BigDecimal;
+
 import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
 
@@ -11,17 +13,62 @@ import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
  * @author Sam Reid
  */
 public class ScaledDoubleProperty extends Property<Double> {
+
+    //Round off the value to prevent notification cycles
+    public static double round( double value ) {
+
+        //Round to 8 decimal places
+        return new BigDecimal( value ).setScale( 8, BigDecimal.ROUND_HALF_UP ).doubleValue();
+    }
+
     public ScaledDoubleProperty( final Property<Double> property, final double scale ) {
         super( property.get() * scale );
+
+        //Wire up for 2-way notifications, but make sure the value is rounded to prevent notification cycles caused by floating point error
+        //For example, setting x = 0.22286099728778608, then scaling by scale=0.3642612329277407 then scaling back by the same number does not produce x, it produces x'=0.22286099728778605
+        //This can cause looping notification cycles.  So we round the number to try to avoid this problem
         property.addObserver( new VoidFunction1<Double>() {
             public void apply( Double value ) {
-                set( value * scale );
+                set( round( value ) );
             }
         } );
         addObserver( new VoidFunction1<Double>() {
             public void apply( Double value ) {
-                property.set( value / scale );
+                property.set( round( value / scale ) );
             }
         } );
+    }
+
+    static class Test2 {
+        public static void main( String[] args ) {
+            double x = 0.22286099728778608;
+            double scale = 0.3642612329277407;
+            double z = x * scale / scale;
+            System.out.println( "z = " + z );
+
+            double a = 0.22286099728778608 * 0.3642612329277407 / 0.3642612329277407 / 0.22286099728778608;
+            System.out.println( "a = " + a );
+
+            final long precision = 1000000;
+
+            double thisValue = Math.round( x * scale * precision ) / (double) precision;
+            System.out.println( "thisValue = " + thisValue );
+            System.out.println( "x = " + x * scale );
+
+            System.out.println( "round( x ) = " + round( x ) );
+        }
+    }
+
+    public static void main( String[] args ) {
+        double x = Math.random();
+        double scale = Math.random();
+        double y = x * scale;
+        double z = y / scale;
+        if ( x != z ) {
+            System.out.println( "x = " + x + ", z=" + z + ", scale = " + scale );
+        }
+        else {
+            main( args );
+        }
     }
 }
