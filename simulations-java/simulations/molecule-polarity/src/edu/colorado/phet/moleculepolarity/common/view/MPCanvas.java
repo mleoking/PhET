cf740d91041a67d52744a77fe85971a5d2dfc537
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-09-20 21:43:26 +0000

    move bkg color constant to MPCanvas

--- simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/common/view/MPCanvas.java
+++ simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/common/view/MPCanvas.java
@@ -1,11 +1,11 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.moleculepolarity.common.view;
 
+import java.awt.Color;
 import java.awt.geom.Dimension2D;
 
 import edu.colorado.phet.common.phetcommon.application.PhetApplication;
 import edu.colorado.phet.common.piccolophet.PhetPCanvas;
-import edu.colorado.phet.moleculepolarity.MPConstants;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.util.PDimension;
 
@@ -16,14 +16,15 @@ import edu.umd.cs.piccolo.util.PDimension;
  */
 public class MPCanvas extends PhetPCanvas {
 
-    private static Dimension2D STAGE_SIZE = new PDimension( 1008, 679 );
+    private static final Dimension2D STAGE_SIZE = new PDimension( 1008, 679 );
+    private static final Color CANVAS_COLOR = new Color( 180, 205, 255 ); // light blue
     private static final boolean SHOW_STAGE_BOUNDS = true && PhetApplication.getInstance().isDeveloperControlsEnabled();
 
     private final PNode rootNode;
 
     protected MPCanvas() {
 
-        setBackground( MPConstants.CANVAS_COLOR );
+        setBackground( CANVAS_COLOR );
         setWorldTransformStrategy( new PhetPCanvas.CenteredStage( this, STAGE_SIZE ) );
         if ( SHOW_STAGE_BOUNDS ) {
             addBoundsNode( STAGE_SIZE );
