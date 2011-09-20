233cbafbe3441af9e0580c056a0b7d28a473edc5
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-09-20 00:01:54 +0000

    move duplicate ClockListener in 2D models to the base class

--- simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/twoatoms/TwoAtomsModel.java
+++ simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/twoatoms/TwoAtomsModel.java
@@ -2,8 +2,6 @@
 package edu.colorado.phet.moleculepolarity.twoatoms;
 
 import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
-import edu.colorado.phet.common.phetcommon.model.clock.ClockAdapter;
-import edu.colorado.phet.common.phetcommon.model.clock.ClockEvent;
 import edu.colorado.phet.common.phetcommon.model.clock.IClock;
 import edu.colorado.phet.moleculepolarity.common.model.DiatomicMolecule;
 import edu.colorado.phet.moleculepolarity.common.model.MPModel2D;
@@ -18,21 +16,7 @@ public class TwoAtomsModel extends MPModel2D {
     public final DiatomicMolecule molecule;
 
     public TwoAtomsModel( IClock clock ) {
-
-        molecule = new DiatomicMolecule( new ImmutableVector2D( 350, 390 ), 0 );
-
-        clock.addClockListener( new ClockAdapter() {
-            public void clockTicked( ClockEvent clockEvent ) {
-                // if the E-field is on and the user isn't controlling the molecule's orientation...
-                if ( eField.enabled.get() && !molecule.isDragging() ) {
-                    updateMoleculeOrientation( molecule );
-                }
-            }
-        } );
-    }
-
-    @Override public void reset() {
-        super.reset();
-        molecule.reset();
+        super( clock, new DiatomicMolecule( new ImmutableVector2D( 350, 390 ), 0 ) );
+        molecule = (DiatomicMolecule) getMolecule(); // hate to cast, but it facilitates moving shared code to base class
     }
 }
