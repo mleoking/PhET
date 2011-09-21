dd6497b248ac2d19e987eaa7ea404f73ab8af945
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-09-21 22:09:48 +0000

    Generalized DialogCheckBoxMenuItem and moved to phetcommon. This is a piece of code I've used many times in sims, a check box menu item that opens a dialog, then needs to manage the dialog during its lifetime.

--- trunk/simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/developer/JmolConsoleMenuItem.java
+++ trunk/simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/developer/JmolConsoleMenuItem.java
@@ -1,80 +1,26 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.moleculepolarity.developer;
 
+import java.awt.Container;
 import java.awt.Frame;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-
-import javax.swing.JCheckBoxMenuItem;
-import javax.swing.JDialog;
 
 import org.jmol.api.JmolViewer;
 
-import edu.colorado.phet.common.phetcommon.view.util.SwingUtils;
+import edu.colorado.phet.common.phetcommon.util.function.Function0;
+import edu.colorado.phet.common.phetcommon.view.menu.DialogCheckBoxMenuItem;
 
 /**
- * Menu item that opens a Jmol console in a dialog.
+ * Menu item that opens a Jmol Console in a dialog.
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
-public class JmolConsoleMenuItem extends JCheckBoxMenuItem {
-
-    private final Frame owner;
-    private final JmolViewer viewer;
-    private JDialog dialog;
+public class JmolConsoleMenuItem extends DialogCheckBoxMenuItem {
 
     public JmolConsoleMenuItem( final Frame owner, final JmolViewer viewer ) {
-        super( "Jmol Console..." );
-        this.owner = owner;
-        this.viewer = viewer;
-        addActionListener( new ActionListener() {
-            public void actionPerformed( ActionEvent e ) {
-                if ( isSelected() ) {
-                    openDialog();
-                }
-                else {
-                    closeDialog();
-                }
+        super( "Jmol Console...", "Jmol Console", owner, new Function0<Container>() {
+            public Container apply() {
+                return new JmolConsole( viewer );
             }
         } );
-    }
-
-    private void openDialog() {
-        if ( dialog != null ) {
-            closeDialog();
-        }
-        dialog = new JDialog( owner ) {{
-            setTitle( "Jmol Console" );
-            setContentPane( new JmolConsole( viewer ) );
-            pack();
-            addWindowListener( new WindowAdapter() {
-
-                // called when the close button in the dialog's window dressing is clicked
-                @Override
-                public void windowClosing( WindowEvent e ) {
-                    closeDialog();
-                }
-
-                // called by JDialog.dispose
-                @Override
-                public void windowClosed( WindowEvent e ) {
-                    closeDialog();
-                    if ( isSelected() ) {
-                        setSelected( false );
-                    }
-                }
-            } );
-        }};
-        SwingUtils.centerInParent( dialog );
-        dialog.setVisible( true );
-    }
-
-    private void closeDialog() {
-        if ( dialog != null ) {
-            dialog.dispose();
-            dialog = null;
-        }
     }
 }
