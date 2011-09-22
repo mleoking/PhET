f56afa9ca877cdc349c4d342c4aa2cbeb7ca35f6
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-09-22 01:12:39 +0000

    9/21/11 move JmolConsole to jmol-phet

--- simulations-java/common/jmol-phet/src/edu/colorado/phet/common/jmolphet/JmolConsole.java
+++ simulations-java/common/jmol-phet/src/edu/colorado/phet/common/jmolphet/JmolConsole.java
@@ -1,5 +1,5 @@
 // Copyright 2002-2011, University of Colorado
-package edu.colorado.phet.moleculepolarity.developer;
+package edu.colorado.phet.common.jmolphet;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
@@ -83,6 +83,7 @@ public class JmolConsole extends JPanel {
         setBorder( new EmptyBorder( 5, 5, 5, 5 ) );
     }
 
+    // test
     public static void main( String[] args ) {
 
         class ViewerPanel extends JPanel {
--- simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/MoleculePolarityApplication.java
+++ simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/MoleculePolarityApplication.java
@@ -1,18 +1,21 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.moleculepolarity;
 
+import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JCheckBoxMenuItem;
 
+import edu.colorado.phet.common.jmolphet.JmolConsole;
 import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
 import edu.colorado.phet.common.phetcommon.application.PhetApplicationLauncher;
+import edu.colorado.phet.common.phetcommon.util.function.Function0;
 import edu.colorado.phet.common.phetcommon.view.PhetFrame;
+import edu.colorado.phet.common.phetcommon.view.menu.DialogCheckBoxMenuItem;
 import edu.colorado.phet.common.phetcommon.view.menu.OptionsMenu;
 import edu.colorado.phet.common.piccolophet.PiccoloPhetApplication;
 import edu.colorado.phet.moleculepolarity.common.view.JmolViewerNode;
-import edu.colorado.phet.moleculepolarity.developer.JmolConsoleMenuItem;
 import edu.colorado.phet.moleculepolarity.realmolecules.RealMoleculesModule;
 import edu.colorado.phet.moleculepolarity.threeatoms.ThreeAtomsModule;
 import edu.colorado.phet.moleculepolarity.twoatoms.TwoAtomsModule;
@@ -46,8 +49,12 @@ public class MoleculePolarityApplication extends PiccoloPhetApplication {
             }} );
         }} );
 
-        // Developer menu
-        parentFrame.getDeveloperMenu().add( new JmolConsoleMenuItem( getPhetFrame(), realMoleculesModule.getJmolViewer() ) );
+        // Developer menu item for Jmol Console
+        parentFrame.getDeveloperMenu().add( new DialogCheckBoxMenuItem( "Jmol Console...", "Jmol Console", getPhetFrame(), new Function0<Container>() {
+            public Container apply() {
+                return new JmolConsole( realMoleculesModule.getJmolViewer() );
+            }
+        } ) );
     }
 
     public static void main( final String[] args ) {
