244a253c197caf484dc9b2e1678122a7002527b3
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-09-22 08:02:47 +0000

    tectonics: added basic structure

---
+++ trunk/simulations-java/simulations/plate-tectonics/src/edu/colorado/phet/platetectonics/PlateTectonicsConstants.java
@@ -0,0 +1,16 @@
+// Copyright 2002-2011, University of Colorado
+
+package edu.colorado.phet.platetectonics;
+
+/**
+ * Contains global constants and some dynamic global variables (like colors)
+ */
+public class PlateTectonicsConstants {
+
+    /* Not intended for instantiation. */
+    private PlateTectonicsConstants() {
+    }
+
+    public static final String PROJECT_NAME = "plate-tectonics";
+
+}
