76f96c77b7dc57a7610204ca5907e1680ee1201e
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-09-23 05:45:27 +0000

    doc

--- simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/common/view/ViewProperties.java
+++ simulations-java/simulations/molecule-polarity/src/edu/colorado/phet/moleculepolarity/common/view/ViewProperties.java
@@ -6,7 +6,7 @@ import edu.colorado.phet.common.phetcommon.model.property.Property;
 
 /**
  * Properties that control things in the view.
- * This is a union of all such properties in all modules.
+ * Since this sim has a small number of controls, this is a union of all such properties in all modules.
  * Properties that are irrelevant for a module are simply ignored by that module.
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
