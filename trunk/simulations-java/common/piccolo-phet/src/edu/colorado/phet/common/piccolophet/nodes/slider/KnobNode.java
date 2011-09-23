bd19315ccaa8702ad8f713ad6e4bfc029e7bddc2
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-09-23 01:37:50 +0000

    Repackaged piccolo slider

--- trunk/simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/nodes/slider/KnobNode.java
+++ trunk/simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/nodes/slider/KnobNode.java
@@ -1,5 +1,5 @@
 // Copyright 2002-2011, University of Colorado
-package edu.colorado.phet.common.piccolophet.nodes.faucet;
+package edu.colorado.phet.common.piccolophet.nodes.slider;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
