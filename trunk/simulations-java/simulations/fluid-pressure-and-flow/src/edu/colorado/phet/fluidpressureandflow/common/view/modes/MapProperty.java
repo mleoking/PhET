a7aaadd9dddcc09362c3e55cdc6519d247fcae46
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-09-24 06:16:37 +0000

    feasibility test for modes

---
+++ trunk/simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/common/view/modes/MapProperty.java
@@ -0,0 +1,39 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.fluidpressureandflow.common.view.modes;
+
+import edu.colorado.phet.common.phetcommon.model.property.CompositeProperty;
+import edu.colorado.phet.common.phetcommon.model.property.Property;
+import edu.colorado.phet.common.phetcommon.util.Pair;
+import edu.colorado.phet.common.phetcommon.util.function.Function0;
+import edu.colorado.phet.common.phetcommon.util.function.Function1;
+
+/**
+ * @author Sam Reid
+ */
+public class MapProperty<T, U> extends CompositeProperty<U> {
+
+    public MapProperty( final Property<T> parent, final Pair<T, U>... map ) {
+        this( parent, new RuntimeExceptionFunction<T, U>(), map );
+    }
+
+    public MapProperty( final Property<T> parent, final U defaultValue, final Pair<T, U>... map ) {
+        this( parent, new Function1.Constant<T, U>( defaultValue ), map );
+    }
+
+    public MapProperty( final Property<T> parent, final Function1<T, U> defaultValue, final Pair<T, U>... map ) {
+        super( new Function0<U>() {
+            public U apply() {
+                for ( Pair<T, U> pair : map ) {
+                    if ( parent.get() == pair._1 ) { return pair._2; }
+                }
+                return defaultValue.apply( parent.get() );
+            }
+        }, parent );
+    }
+
+    private static class RuntimeExceptionFunction<T, U> implements Function1<T, U> {
+        public U apply( T t ) {
+            throw new RuntimeException( "No match found for value: " + t );
+        }
+    }
+}
---
+++ trunk/simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/common/view/modes/Mode.java
@@ -0,0 +1,9 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.fluidpressureandflow.common.view.modes;
+
+/**
+ * @author Sam Reid
+ */
+public enum Mode {
+    MONITOR, PROJECTOR, HANDOUT
+}
