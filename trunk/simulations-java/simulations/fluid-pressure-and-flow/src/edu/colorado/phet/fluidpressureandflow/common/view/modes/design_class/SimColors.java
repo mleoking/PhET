b83bb79e92f086fd4a1ff7619e86eab8a28bd209
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-09-25 21:04:25 +0000

    minor refactoring for SimColors

--- trunk/simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/common/view/modes/design_class/SimColors.java
+++ trunk/simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/common/view/modes/design_class/SimColors.java
@@ -17,26 +17,27 @@ import edu.colorado.phet.common.phetcommon.util.function.Function0;
  */
 public class SimColors {
     public final Property<IColorScheme> colorScheme = new Property<IColorScheme>( new MonitorColorScheme() );
-    public final ObservableProperty<Color> background = new CompositeProperty<Color>( new Function0<Color>() {
+    public final ObservableProperty<Color> background = wrap( new Function0<Color>() {
         public Color apply() {
             return colorScheme.get().getBackground();
         }
-    }, colorScheme );
-    public final ObservableProperty<Color> saltColor = new CompositeProperty<Color>( new Function0<Color>() {
+    } );
+    public final ObservableProperty<Color> saltColor = wrap( new Function0<Color>() {
         public Color apply() {
             return colorScheme.get().getSaltColor();
         }
-    }, colorScheme );
-    public final ObservableProperty<Font> titleFont = new CompositeProperty<Font>( new Function0<Font>() {
-        public Font apply() {
-            return colorScheme.get().getTitleFont();
-        }
-    }, colorScheme );
+    } );
     public final ObservableProperty<Color> sugarColor = wrap( new Function0<Color>() {
         public Color apply() {
             return colorScheme.get().getSugarColor();
         }
     } );
+
+    public final ObservableProperty<Font> titleFont = new CompositeProperty<Font>( new Function0<Font>() {
+        public Font apply() {
+            return colorScheme.get().getTitleFont();
+        }
+    }, colorScheme );
 
     private ObservableProperty<Color> wrap( Function0<Color> function0 ) {
         return new CompositeProperty<Color>( function0, colorScheme );
