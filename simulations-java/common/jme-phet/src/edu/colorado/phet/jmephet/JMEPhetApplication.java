c1d80896b909ffe945c12f5475c1c3b70cceecb7
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-09-27 18:18:07 +0000

    - resurrect contrib/apple and OSXAdapter, #920 - add handler for Mac Quit (Cmd-Q) menu item, so the JME application quit properly on Mac

--- simulations-java/common/jme-phet/src/edu/colorado/phet/jmephet/JMEPhetApplication.java
+++ simulations-java/common/jme-phet/src/edu/colorado/phet/jmephet/JMEPhetApplication.java
@@ -1,9 +1,12 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.jmephet;
 
-import java.awt.*;
+import apple.dts.samplecode.osxadapter.OSXAdapter;
+
+import java.awt.Component;
 
 import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
+import edu.colorado.phet.common.phetcommon.util.PhetUtilities;
 import edu.colorado.phet.common.phetcommon.view.ModulePanel;
 import edu.colorado.phet.common.piccolophet.PiccoloPhetApplication;
 import edu.colorado.phet.common.piccolophet.TabbedModulePanePiccolo;
@@ -37,9 +40,35 @@ public class JMEPhetApplication extends PiccoloPhetApplication {
                 // don't allow removal of our primary module panel
             }
         } );
+        registerForMacOSXEvents();
     }
 
     public JMEPhetApplication( PhetApplicationConfig config, TabbedModulePanePiccolo tabbedModulePane ) {
         super( config, tabbedModulePane );
+    }
+
+    // Generic registration with the Mac OS X application menu
+    // Checks the platform, then attempts to register with the Apple EAWT
+    // See OSXAdapter.java to see how this is done without directly referencing any Apple APIs
+    public void registerForMacOSXEvents() {
+        if ( PhetUtilities.isMacintosh() ) {
+            try {
+                // Generate and register the OSXAdapter, passing it a hash of all the methods we wish to
+                // use as delegates for various com.apple.eawt.ApplicationListener methods
+                OSXAdapter.setQuitHandler( this, JMEPhetApplication.class.getDeclaredMethod( "macQuit", (Class[]) null ) );
+            }
+            catch ( Exception e ) {
+                System.err.println( "Error while loading the OSXAdapter:" );
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // General quit handler; fed to the OSXAdapter as the method to call when a system quit event occurs
+    // A quit event is triggered by Cmd-Q, selecting Quit from the application or Dock menu, or logging out.
+    // This will be called via reflection by OSXAdapter, so IDEs may identify this as an unused method.
+    public boolean macQuit() {
+        exit();
+        return true; // yes, we really want to quit
     }
 }
