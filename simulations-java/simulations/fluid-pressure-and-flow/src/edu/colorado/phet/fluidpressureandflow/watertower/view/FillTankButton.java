49083617823b1963cd369195fd09e0ac622a9026
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-09-30 17:21:39 +0000

    Water tower: Fixed: Sometimes repeated clicks on the "Fill" button don't work when the door is open

--- simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/watertower/view/FillTankButton.java
+++ simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/watertower/view/FillTankButton.java
@@ -4,7 +4,9 @@ package edu.colorado.phet.fluidpressureandflow.watertower.view;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
 import edu.colorado.phet.common.phetcommon.model.property.ObservableProperty;
+import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.phetcommon.util.function.VoidFunction0;
 import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
@@ -20,16 +22,25 @@ import static edu.colorado.phet.fluidpressureandflow.watertower.view.WaterTowerC
  * @author Sam Reid
  */
 public class FillTankButton extends TextButtonNode {
-    public FillTankButton( ObservableProperty<Boolean> full, final VoidFunction0 fill ) {
+    public FillTankButton( ObservableProperty<Boolean> full, final VoidFunction0 fill, final Property<ImmutableVector2D> panelOffset ) {
         super( FILL, new PhetFont( FLOATING_BUTTON_FONT_SIZE ), FluidPressureControlPanel.BACKGROUND );
         addActionListener( new ActionListener() {
             public void actionPerformed( ActionEvent e ) {
                 fill.apply();
             }
         } );
+
+        //Disable the button if the tank is full
+        //But only if it won't immediately become non-full, which would also disarm the button and make repeated clicks not work
         full.addObserver( new VoidFunction1<Boolean>() {
             public void apply( Boolean full ) {
-                setEnabled( !full );
+                boolean doorOpen = panelOffset.get().getY() > 0;
+                if ( isEnabled() && full && !doorOpen ) {
+                    setEnabled( false );
+                }
+                else if ( !isEnabled() && !full ) {
+                    setEnabled( true );
+                }
             }
         } );
     }
