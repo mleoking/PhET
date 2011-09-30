d7fb8bd0dc2fd8fedd6d5122444f4b779c9ef03e
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-09-30 05:49:23 +0000

    Added new "Sugar and Salt Solutions: Macroscopic" sim

---
+++ trunk/simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/wetlab/MacroscopicApplication.java
@@ -0,0 +1,38 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.sugarandsaltsolutions.wetlab;
+
+import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
+import edu.colorado.phet.common.phetcommon.application.PhetApplicationLauncher;
+import edu.colorado.phet.common.phetcommon.view.menu.TeacherMenu;
+import edu.colorado.phet.common.piccolophet.PiccoloPhetApplication;
+import edu.colorado.phet.sugarandsaltsolutions.GlobalState;
+import edu.colorado.phet.sugarandsaltsolutions.SugarAndSaltSolutionsColorScheme;
+import edu.colorado.phet.sugarandsaltsolutions.SugarAndSaltSolutionsResources;
+import edu.colorado.phet.sugarandsaltsolutions.macro.MacroModule;
+
+/**
+ * This sim was written to accommodate a chemistry wet lab in Fall 2011, this sim just shows the macro tab.
+ * <p/>
+ * No need to internationalize this version--it is just used for one experiment.
+ *
+ * @author Sam Reid
+ */
+public class MacroscopicApplication extends PiccoloPhetApplication {
+
+    private static final String NAME = "sugar-and-salt-solutions-macroscopic";
+
+    public MacroscopicApplication( PhetApplicationConfig config ) {
+        super( config );
+
+        //Create the modules
+        final GlobalState globalState = new GlobalState( new SugarAndSaltSolutionsColorScheme(), config, getPhetFrame() );
+        addModule( new MacroModule( globalState ) );
+
+        //Add an options menu with the option to change the background to white for use on projectors in bright classrooms
+        getPhetFrame().addMenu( new TeacherMenu() {{addWhiteBackgroundMenuItem( globalState.colorScheme.projectorMode );}} );
+    }
+
+    public static void main( String[] args ) {
+        new PhetApplicationLauncher().launchSim( args, SugarAndSaltSolutionsResources.NAME, NAME, MacroscopicApplication.class );
+    }
+}
