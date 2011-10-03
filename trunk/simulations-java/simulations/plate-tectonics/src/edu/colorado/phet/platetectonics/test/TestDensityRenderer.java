216a6bbf600afbeb8a4b74b8535ac68cf36c0b48
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-10-03 07:55:44 +0000

    tectonics: moved class

--- trunk/simulations-java/simulations/plate-tectonics/src/edu/colorado/phet/platetectonics/test/TestDensityRenderer.java
+++ trunk/simulations-java/simulations/plate-tectonics/src/edu/colorado/phet/platetectonics/test/TestDensityRenderer.java
@@ -1,5 +1,5 @@
 // Copyright 2002-2011, University of Colorado
-package edu.colorado.phet.platetectonics.java2d;
+package edu.colorado.phet.platetectonics.test;
 
 import java.awt.*;
 import java.awt.geom.Rectangle2D;
