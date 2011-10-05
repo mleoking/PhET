ed4a4efbb1e01a3b2bb8a4f05d0ce17ebd007bb2
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-10-05 05:17:15 +0000

    Made "Reset all" button non-bold to re-use common code for reset all buttons, see #3090

--- simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/common/view/SugarAndSaltSolutionsResetAllButtonNode.java
+++ simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/common/view/SugarAndSaltSolutionsResetAllButtonNode.java
@@ -1,36 +1,28 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.sugarandsaltsolutions.common.view;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.Color;
 
+import edu.colorado.phet.common.phetcommon.model.Resettable;
 import edu.colorado.phet.common.phetcommon.util.function.VoidFunction0;
-import edu.colorado.phet.common.piccolophet.nodes.HTMLImageButtonNode;
-import edu.umd.cs.piccolo.PNode;
+import edu.colorado.phet.common.piccolophet.nodes.ResetAllButtonNode;
 
-import static edu.colorado.phet.common.phetcommon.resources.PhetCommonResources.STRING_RESET_ALL;
-import static edu.colorado.phet.common.phetcommon.resources.PhetCommonResources.getInstance;
 import static edu.colorado.phet.sugarandsaltsolutions.common.view.BeakerAndShakerCanvas.*;
-
-//REVIEW why aren't you using piccolo-phet's ResetAllButtonNode?
 
 /**
  * Reset all button used in all tabs, shown in the bottom right corner
  *
  * @author Sam Reid
  */
-public class SugarAndSaltSolutionsResetAllButtonNode extends PNode {
+public class SugarAndSaltSolutionsResetAllButtonNode extends ResetAllButtonNode {
     public SugarAndSaltSolutionsResetAllButtonNode( final double stageWidth, final double stageHeight, final VoidFunction0 reset ) {
-        addChild( new HTMLImageButtonNode( getInstance().getLocalizedString( STRING_RESET_ALL ), BUTTON_COLOR ) {{
-            setFont( CONTROL_FONT );
+        super( new Resettable() {
+            public void reset() {
+                reset.apply();
+            }
+        }, null, CONTROL_FONT.getSize(), Color.black, BUTTON_COLOR );
 
-            //Have to set the offset after changing the font since it changes the size of the node
-            setOffset( stageWidth - getFullBounds().getWidth() - INSET, stageHeight - getFullBounds().getHeight() - INSET );
-            addActionListener( new ActionListener() {
-                public void actionPerformed( ActionEvent e ) {
-                    reset.apply();
-                }
-            } );
-        }} );
+        //Have to set the offset after changing the font since it changes the size of the node
+        setOffset( stageWidth - getFullBounds().getWidth() - INSET, stageHeight - getFullBounds().getHeight() - INSET );
     }
 }
