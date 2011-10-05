8af842291906960df9fa638191e05aa21b7e9e93
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-10-05 03:27:54 +0000

    updated docs for Hydrogen and Oxygen fields, see #3090

--- trunk/simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/common/model/SphericalParticle.java
+++ trunk/simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/common/model/SphericalParticle.java
@@ -63,7 +63,7 @@ public class SphericalParticle extends Particle {
 
     //These classes contains state information for particulars particles and ions and permit matching in MicroModel for particle counting.
     public static class Hydrogen extends SphericalParticle {
-        public static final double RADIUS_PICOMETERS = 37; //REVIEW why can't clients use radius field?
+        public static final double RADIUS_PICOMETERS = 37;//Field provided for reuse in the water tab since WaterMolecule needs to have the same oxygen radius
 
         public Hydrogen() {
             super( RADIUS_PICOMETERS, SphericalParticle.NEUTRAL_COLOR, white, +1 );
@@ -84,7 +84,7 @@ public class SphericalParticle extends Particle {
 
     //Abstract since oxygen ions and oxygen in sucrose/glucose must have different colors
     public abstract static class Oxygen extends SphericalParticle {
-        public static final double RADIUS_PICOMETERS = 73;  //REVIEW why can't clients use radius field?
+        public static final double RADIUS_PICOMETERS = 73;//Field provided for reuse in the water tab since WaterMolecule needs to have the same oxygen radius
 
         public Oxygen( Color chargeColor ) {
             super( RADIUS_PICOMETERS, chargeColor, RED_COLORBLIND, -2 );
