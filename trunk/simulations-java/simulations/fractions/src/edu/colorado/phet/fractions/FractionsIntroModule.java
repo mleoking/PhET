4cb80f7571f87b0fa7688225f59e5653c64df722
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-10-07 02:58:52 +0000

    started prototype for fractions sim

---
+++ trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/FractionsIntroModule.java
@@ -0,0 +1,18 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.fractions;
+
+import edu.colorado.phet.common.phetcommon.application.Module;
+import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
+
+/**
+ * @author Sam Reid
+ */
+public class FractionsIntroModule extends Module {
+    public FractionsIntroModule() {
+        super( "Fractions Intro", new ConstantDtClock( 30 ) );
+        setSimulationPanel( new FractionsIntroCanvas() );
+
+        getModulePanel().setLogoPanel( null );
+        setClockControlPanel( null );
+    }
+}
