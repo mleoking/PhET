7218e3d982632214f9ca02b8e2b55d4ca0519d90
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-10-07 04:25:21 +0000

    Water tab: Made control panel controls bold Made "Reset all" button bold

--- simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/water/view/WaterControlPanel.java
+++ simulations-java/simulations/sugar-and-salt-solutions/src/edu/colorado/phet/sugarandsaltsolutions/water/view/WaterControlPanel.java
@@ -10,7 +10,6 @@ import javax.swing.SwingUtilities;
 import edu.colorado.phet.common.phetcommon.model.property.SettableNot;
 import edu.colorado.phet.common.phetcommon.view.VerticalLayoutPanel;
 import edu.colorado.phet.common.phetcommon.view.controls.PropertyCheckBox;
-import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.phetcommon.view.util.SwingUtils;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPText;
 import edu.colorado.phet.common.piccolophet.nodes.TextButtonNode;
@@ -23,6 +22,7 @@ import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolox.pswing.PSwing;
 
 import static edu.colorado.phet.sugarandsaltsolutions.SugarAndSaltSolutionsResources.Strings.*;
+import static edu.colorado.phet.sugarandsaltsolutions.common.view.BeakerAndShakerCanvas.CONTROL_FONT;
 import static edu.colorado.phet.sugarandsaltsolutions.common.view.BeakerAndShakerCanvas.TITLE_FONT;
 
 /**
@@ -31,8 +31,6 @@ import static edu.colorado.phet.sugarandsaltsolutions.common.view.BeakerAndShake
  * @author Sam Reid
  */
 public class WaterControlPanel extends WhiteControlPanelNode {
-
-    public static final PhetFont BUTTON_FONT = new PhetFont( 16 );
 
     public WaterControlPanel( final WaterModel waterModel, final GlobalState state, final WaterCanvas waterCanvas, final Sucrose3DDialog sucrose3DDialog ) {
         super( new VBox(
@@ -46,13 +44,13 @@ public class WaterControlPanel extends WhiteControlPanelNode {
 
                     //Checkbox to show/hide water charges (showing partial charges)
                     add( new PropertyCheckBox( WATER_PARTIAL_CHARGES, waterModel.showWaterCharges ) {{
-                        setFont( BUTTON_FONT );
+                        setFont( CONTROL_FONT );
                     }} );
 
                     //Allow the user to show individual atoms within the sugar molecule, but only if a sugar molecule is in the scene
                     //Works for both the sugar in the bucket and any in the model
                     add( new PropertyCheckBox( SUGAR_HIGHLIGHT, new SettableNot( waterModel.showSugarAtoms ) ) {{
-                        setFont( BUTTON_FONT );
+                        setFont( CONTROL_FONT );
                     }} );
                 }} ),
 
@@ -72,7 +70,7 @@ public class WaterControlPanel extends WhiteControlPanelNode {
                 }} : new PNode(),
 
                 //Add a button that allows the user to show the 3D water molecule
-                new TextButtonNode( SUGAR_IN_3_D, BUTTON_FONT, Color.yellow ) {{
+                new TextButtonNode( SUGAR_IN_3_D, CONTROL_FONT, Color.yellow ) {{
                     addActionListener( new ActionListener() {
                         public void actionPerformed( ActionEvent e ) {
                             sucrose3DDialog.showDialog();
