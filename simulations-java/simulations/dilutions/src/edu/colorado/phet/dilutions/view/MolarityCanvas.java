5ddba9f5fb7d5cbf040c14878c28ef585f822564
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-10-11 00:23:04 +0000

    10/10/11 sliders for molarity tab

--- simulations-java/simulations/dilutions/src/edu/colorado/phet/dilutions/view/MolarityCanvas.java
+++ simulations-java/simulations/dilutions/src/edu/colorado/phet/dilutions/view/MolarityCanvas.java
@@ -7,6 +7,8 @@ import java.awt.Frame;
 import edu.colorado.phet.common.phetcommon.model.Resettable;
 import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.piccolophet.nodes.ResetAllButtonNode;
+import edu.colorado.phet.dilutions.control.DilutionsSliderNode.SoluteAmountSliderNode;
+import edu.colorado.phet.dilutions.control.DilutionsSliderNode.SolutionVolumeSliderNode;
 import edu.colorado.phet.dilutions.control.ShowValuesNode;
 import edu.colorado.phet.dilutions.control.SoluteControlNode;
 import edu.colorado.phet.dilutions.model.MolarityModel;
@@ -32,10 +34,14 @@ public class MolarityCanvas extends AbstractDilutionsCanvas implements Resettabl
             setConfirmationEnabled( false );
         }};
         ConcentrationDisplayNode concentrationDisplayNode = new ConcentrationDisplayNode( new PDimension( 40, BEAKER_HEIGHT ), model.solution, model.getConcentrationRange() );
+        SoluteAmountSliderNode soluteAmountSliderNode = new SoluteAmountSliderNode( new PDimension( 5, BEAKER_HEIGHT ), model.solution.soluteAmount, model.getSoluteAmountRange() );
+        SolutionVolumeSliderNode solutionVolumeSliderNode = new SolutionVolumeSliderNode( new PDimension( 5, BEAKER_HEIGHT ), model.solution.volume, model.getVolumeRange() );
 
         // rendering order
         {
             addChild( concentrationDisplayNode );
+            addChild( soluteAmountSliderNode );
+            addChild( solutionVolumeSliderNode );
             addChild( showValuesNode );
             addChild( resetAllButtonNode );
             addChild( soluteControlNode ); // combo box on top
@@ -43,10 +49,14 @@ public class MolarityCanvas extends AbstractDilutionsCanvas implements Resettabl
 
         // layout
         {
-            soluteControlNode.setOffset( 100, 100 );
-            concentrationDisplayNode.setOffset( 600, 100 );
-            showValuesNode.setOffset( concentrationDisplayNode.getFullBoundsReference().getMaxX() - 10,
-                                      concentrationDisplayNode.getFullBoundsReference().getMaxY() + 10 );
+            soluteControlNode.setOffset( 50, 50 );
+            soluteAmountSliderNode.setOffset( 100, 200 );
+            solutionVolumeSliderNode.setOffset( soluteAmountSliderNode.getFullBoundsReference().getMaxX() + 75,
+                                                soluteAmountSliderNode.getYOffset() );
+            concentrationDisplayNode.setOffset( 650,
+                                                soluteAmountSliderNode.getYOffset() );
+            showValuesNode.setOffset( concentrationDisplayNode.getFullBoundsReference().getMaxX() + 10,
+                                      concentrationDisplayNode.getFullBoundsReference().getMaxY() - 40 );
             resetAllButtonNode.setOffset( showValuesNode.getXOffset(), showValuesNode.getFullBoundsReference().getMaxY() + 10 );
         }
     }
