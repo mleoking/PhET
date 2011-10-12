0718e126bc5767e47ecbc9e3d760e1c48385f1a4
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-10-12 18:43:56 +0000

    10/12/11 create custom formatter with special handling of zero and ints

---
+++ trunk/simulations-java/simulations/dilutions/src/edu/colorado/phet/dilutions/util/SmartDoubleFormat.java
@@ -0,0 +1,46 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.dilutions.util;
+
+import java.text.DecimalFormat;
+
+import edu.colorado.phet.common.phetcommon.util.DefaultDecimalFormat;
+
+/**
+ * A double formatter that provides special treatment of zero and integers.
+ * Implemented via composition because NumberFormat.format is final.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
+public class SmartDoubleFormat {
+
+    private DecimalFormat defaultFormat;
+    private final boolean formatIntegersAsIntegers;
+    private final boolean formatZeroAsInteger;
+
+    /**
+     * Constructor
+     *
+     * @param pattern                  the NumberFormat pattern used to format values
+     * @param formatIntegersAsIntegers if true, show 0 as "0"; otherwise format using pattern
+     * @param showZeroAsInteger        if true, format integers with no decimal places; otherwise format using pattern
+     */
+    public SmartDoubleFormat( String pattern, boolean formatIntegersAsIntegers, boolean showZeroAsInteger ) {
+        this.defaultFormat = new DefaultDecimalFormat( pattern );
+        this.formatIntegersAsIntegers = formatIntegersAsIntegers;
+        this.formatZeroAsInteger = showZeroAsInteger;
+    }
+
+    public String format( double value ) {
+        String s;
+        if ( value == 0 && formatZeroAsInteger ) {
+            s = "0";
+        }
+        else if ( ( value % 1 == 0 ) && formatIntegersAsIntegers ) {
+            s = String.valueOf( (int) value );
+        }
+        else {
+            s = defaultFormat.format( value );
+        }
+        return s;
+    }
+}
