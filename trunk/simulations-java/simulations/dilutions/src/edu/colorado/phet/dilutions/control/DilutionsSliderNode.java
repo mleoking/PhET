8fdfbb3566591c20a0e2653d19dc3b3f7063a393
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-10-12 20:03:46 +0000

    10/12/11 add tick marks for min/max range labels on sliders 10/12/11 fix units on concentration display

--- trunk/simulations-java/simulations/dilutions/src/edu/colorado/phet/dilutions/control/DilutionsSliderNode.java
+++ trunk/simulations-java/simulations/dilutions/src/edu/colorado/phet/dilutions/control/DilutionsSliderNode.java
@@ -12,9 +12,6 @@ import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 import java.awt.geom.Rectangle2D;
 import java.awt.geom.RoundRectangle2D;
-import java.text.DecimalFormat;
-import java.text.MessageFormat;
-import java.text.NumberFormat;
 
 import javax.swing.JTextField;
 import javax.swing.border.CompoundBorder;
@@ -33,11 +30,12 @@ import edu.colorado.phet.common.piccolophet.event.SliderThumbDragHandler;
 import edu.colorado.phet.common.piccolophet.event.SliderThumbDragHandler.Orientation;
 import edu.colorado.phet.common.piccolophet.nodes.HTMLNode;
 import edu.colorado.phet.dilutions.DilutionsResources.Strings;
+import edu.colorado.phet.dilutions.util.SmartDoubleFormat;
+import edu.colorado.phet.dilutions.view.TickMarkNode;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.nodes.PPath;
 import edu.umd.cs.piccolo.nodes.PText;
 import edu.umd.cs.piccolo.util.PDimension;
-import edu.umd.cs.piccolox.nodes.PComposite;
 import edu.umd.cs.piccolox.pswing.PSwing;
 
 /**
@@ -50,40 +48,48 @@ public abstract class DilutionsSliderNode extends PhetPNode {
     // Slider for controlling amount of solute
     public static class SoluteAmountSliderNode extends DilutionsSliderNode {
         public SoluteAmountSliderNode( PDimension size, final Property<Double> soluteAmount, DoubleRange range, Property<Boolean> valuesVisible ) {
-            super( Strings.SOLUTE_AMOUNT, size, soluteAmount, new DecimalFormat( "0.00" ), Strings.UNITS_MOLES,
-                   range, Strings.LOTS, Strings.NONE, valuesVisible );
+            super( Strings.SOLUTE_AMOUNT, size, soluteAmount,
+                   Strings.UNITS_MOLES, range, new SmartDoubleFormat( "0.00" ),
+                   new SmartDoubleFormat( "0.00" ),
+                   Strings.LOTS, Strings.NONE, valuesVisible );
         }
     }
 
     // Slider for controlling volume of solution
     public static class SolutionVolumeSliderNode extends DilutionsSliderNode {
         public SolutionVolumeSliderNode( PDimension size, final Property<Double> solutionVolume, DoubleRange range, Property<Boolean> valuesVisible ) {
-            super( Strings.SOLUTION_VOLUME, size, solutionVolume, new DecimalFormat( "0.00" ), Strings.UNITS_LITERS,
-                   range, Strings.FULL, Strings.LOW, valuesVisible );
+            super( Strings.SOLUTION_VOLUME, size, solutionVolume,
+                   Strings.UNITS_LITERS, range, new SmartDoubleFormat( "0.00" ),
+                   new SmartDoubleFormat( "0.00" ),
+                   Strings.FULL, Strings.LOW, valuesVisible );
         }
     }
 
     private static final PhetFont TITLE_FONT = new PhetFont( Font.BOLD, 16 );
     private static final PhetFont VALUE_FONT = new PhetFont( 16 );
-    private static final PhetFont LABEL_FONT = new PhetFont( 14 );
 
     // track
     private static final Color TRACK_FILL_COLOR = Color.BLACK;
 
     // knob
-    private static final PDimension KNOB_SIZE = new PDimension( 35, 10 );
+    private static final PDimension KNOB_SIZE = new PDimension( 30, 10 );
     private static final Stroke KNOB_STROKE = new BasicStroke( 1f );
     private static final Color KNOB_NORMAL_COLOR = new Color( 89, 156, 212 );
     private static final Color KNOB_HIGHLIGHT_COLOR = new Color( 214, 255, 255 );
     private static final Color KNOB_STROKE_COLOR = Color.BLACK;
+
+    // tick marks
+    private static final double TICK_LENGTH = ( KNOB_SIZE.getWidth() / 2 ) + 3;
+    private static final PhetFont TICK_FONT = new PhetFont( 14 );
 
     private final LinearFunction function;
     private final ValueNode valueNode;
     private final TrackNode trackNode;
     private final ThumbNode thumbNode;
 
-    public DilutionsSliderNode( String title, PDimension trackSize, final Property<Double> modelValue, NumberFormat valueFormat, String units,
-                                DoubleRange range, String maxQualityText, String minQualityText, Property<Boolean> valuesVisible ) {
+    public DilutionsSliderNode( String title, PDimension trackSize, final Property<Double> modelValue,
+                                String units, DoubleRange range, SmartDoubleFormat valueFormat, SmartDoubleFormat tickFormat,
+                                String maxQualityText, String minQualityText, Property<Boolean> valuesVisible ) {
 
         this.function = new LinearFunction( range.getMin(), range.getMax(), trackSize.getHeight(), 0 );
 
@@ -96,41 +102,31 @@ public abstract class DilutionsSliderNode extends PhetPNode {
                 modelValue.set( value );
             }
         } );
-        final RangeValueNode maxValueNode = new RangeValueNode( range.getMax(), valueFormat, units );
-        final RangeValueNode minValueNode = new RangeValueNode( range.getMin(), valueFormat, units );
-        final QualityNode maxQualityNode = new QualityNode( maxQualityText );
-        final QualityNode minQualityNode = new QualityNode( minQualityText );
+        final TickMarkNode maxNode = new TickMarkNode( range.getMax(), units, maxQualityText, TICK_FONT, TICK_LENGTH, tickFormat );
+        final TickMarkNode minNode = new TickMarkNode( range.getMin(), units, minQualityText, TICK_FONT, TICK_LENGTH, tickFormat );
 
         // rendering order
         {
             addChild( titleNode );
             addChild( valueNode );
+            addChild( maxNode );
+            addChild( minNode );
             addChild( trackNode );
             addChild( thumbNode );
-            addChild( maxValueNode );
-            addChild( minValueNode );
-            addChild( maxQualityNode );
-            addChild( minQualityNode );
         }
 
         // layout
         {
-            // max labels centered above track
-            maxValueNode.setOffset( trackNode.getFullBoundsReference().getCenterX() - ( maxValueNode.getFullBoundsReference().getWidth() / 2 ),
-                                    trackNode.getFullBoundsReference().getMinY() - maxValueNode.getFullBoundsReference().getHeight() - ( thumbNode.getFullBoundsReference().getHeight() / 2 ) - 2 );
-            maxQualityNode.setOffset( trackNode.getFullBoundsReference().getCenterX() - ( maxQualityNode.getFullBoundsReference().getWidth() / 2 ),
-                                      trackNode.getFullBoundsReference().getMinY() - maxQualityNode.getFullBoundsReference().getHeight() - ( thumbNode.getFullBoundsReference().getHeight() / 2 ) - 2 );
-            // min labels centered below track
-            minValueNode.setOffset( trackNode.getFullBoundsReference().getCenterX() - ( minValueNode.getFullBoundsReference().getWidth() / 2 ),
-                                    trackNode.getFullBoundsReference().getMaxY() + ( thumbNode.getFullBoundsReference().getHeight() / 2 ) + 2 );
-            minQualityNode.setOffset( trackNode.getFullBoundsReference().getCenterX() - ( minQualityNode.getFullBoundsReference().getWidth() / 2 ),
-                                      trackNode.getFullBoundsReference().getMaxY() + ( thumbNode.getFullBoundsReference().getHeight() / 2 ) + 2 );
-            // value centered above max label
+            // max label at top of track
+            maxNode.setOffset( 0, 0 );
+            // min label at bottom of track
+            minNode.setOffset( 0, trackSize.getHeight() );
+            // value centered above track
             valueNode.setOffset( trackNode.getFullBoundsReference().getCenterX() - ( valueNode.getFullBoundsReference().getWidth() / 2 ),
-                                 Math.min( maxValueNode.getFullBoundsReference().getMinY(), maxQualityNode.getFullBoundsReference().getMinY() ) - valueNode.getFullBoundsReference().getHeight() - 2 );
+                                 trackNode.getFullBoundsReference().getMinY() - ( maxNode.getFullBoundsReference().getHeight() / 2 ) - 4 - valueNode.getFullBoundsReference().getHeight() );
             // title centered above value
             titleNode.setOffset( trackNode.getFullBoundsReference().getCenterX() - ( titleNode.getFullBoundsReference().getWidth() / 2 ),
-                                 valueNode.getFullBoundsReference().getMinY() - titleNode.getFullBoundsReference().getHeight() - 2 );
+                                 valueNode.getFullBoundsReference().getMinY() - titleNode.getFullBoundsReference().getHeight() - 3 );
             // thumb centered in track
             thumbNode.setOffset( trackNode.getFullBoundsReference().getCenterX(),
                                  trackNode.getFullBoundsReference().getCenterY() );
@@ -146,10 +142,8 @@ public abstract class DilutionsSliderNode extends PhetPNode {
         valuesVisible.addObserver( new VoidFunction1<Boolean>() {
             public void apply( Boolean visible ) {
                 valueNode.setVisible( visible );
-                maxValueNode.setVisible( visible );
-                minValueNode.setVisible( visible );
-                maxQualityNode.setVisible( !visible );
-                minQualityNode.setVisible( !visible );
+                maxNode.setValueVisible( visible );
+                minNode.setValueVisible( visible );
             }
         } );
     }
@@ -172,10 +166,10 @@ public abstract class DilutionsSliderNode extends PhetPNode {
 
         private final Property<Double> modelValue;
         private final DoubleRange range;
-        private final NumberFormat format;
+        private final SmartDoubleFormat format;
         private final JTextField textField;
 
-        public ValueNode( final Property<Double> modelValue, final DoubleRange range, final NumberFormat format, final String units ) {
+        public ValueNode( final Property<Double> modelValue, final DoubleRange range, final SmartDoubleFormat format, final String units ) {
 
             this.modelValue = modelValue;
             this.range = range;
@@ -261,14 +255,6 @@ public abstract class DilutionsSliderNode extends PhetPNode {
         }
     }
 
-    // A qualitative label
-    private static class QualityNode extends PText {
-        public QualityNode( String text ) {
-            super( text );
-            setFont( LABEL_FONT );
-        }
-    }
-
     // The track that the thumb moves in. Origin is at upper-left corner.
     private static class TrackNode extends PPath {
         public TrackNode( PDimension size, Color fillColor ) {
@@ -293,30 +279,6 @@ public abstract class DilutionsSliderNode extends PhetPNode {
             addInputEventListener( new CursorHandler() );
             addInputEventListener( new PaintHighlightHandler( this, KNOB_NORMAL_COLOR, KNOB_HIGHLIGHT_COLOR ) );
             addInputEventListener( new SliderThumbDragHandler( Orientation.VERTICAL, relativeNode, trackNode, this, range, updateFunction ) );
-        }
-    }
-
-    // Range values
-    private static class RangeValueNode extends PComposite {
-
-        private final NumberFormat format;
-        private final String units;
-        private final PText textNode;
-
-        public RangeValueNode( double initialValue, NumberFormat format, String units ) {
-            this.format = format;
-            this.units = units;
-            textNode = new PText() {{
-                setFont( LABEL_FONT );
-            }};
-            addChild( textNode );
-            setValue( initialValue );
-        }
-
-        // Converts concentration value to a string with units, with special treatment of zero.
-        public void setValue( double value ) {
-            String valueString = ( value == 0 ) ? "0" : format.format( value );
-            textNode.setText( MessageFormat.format( Strings.PATTERN_0VALUE_1UNITS, valueString, units ) );
         }
     }
 }
---
+++ trunk/simulations-java/simulations/dilutions/src/edu/colorado/phet/dilutions/view/TickMarkNode.java
@@ -0,0 +1,56 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.dilutions.view;
+
+import java.awt.geom.Line2D;
+import java.text.MessageFormat;
+
+import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
+import edu.colorado.phet.dilutions.DilutionsResources.Strings;
+import edu.colorado.phet.dilutions.util.SmartDoubleFormat;
+import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.nodes.PPath;
+import edu.umd.cs.piccolo.nodes.PText;
+import edu.umd.cs.piccolox.nodes.PComposite;
+
+/**
+ * Tick mark is a horizontal line with a label to the left of the line.
+ * The label can be switched between showing a value and qualitative text.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
+public class TickMarkNode extends PComposite {
+
+    private final PNode valueNode, qualityNode;
+
+    public TickMarkNode( double value, String units, String qualityLabel, final PhetFont labelFont, double tickLength, SmartDoubleFormat format ) {
+
+        // nodes
+        PNode tickNode = new PPath( new Line2D.Double( -tickLength, 0, 0, 0 ) );
+        String valueString = MessageFormat.format( Strings.PATTERN_0VALUE_1UNITS, format.format( value ), units );
+        valueNode = new PText( valueString ) {{
+            setFont( labelFont );
+        }};
+        qualityNode = new PText( qualityLabel ) {{
+            setFont( labelFont );
+        }};
+
+        // rendering order
+        addChild( tickNode );
+        addChild( valueNode );
+        addChild( qualityNode );
+
+        // layout
+        valueNode.setOffset( tickNode.getFullBoundsReference().getMinX() - valueNode.getFullBoundsReference().getWidth() - 3,
+                             -( valueNode.getFullBoundsReference().getHeight() / 2 ) );
+        qualityNode.setOffset( tickNode.getFullBoundsReference().getMinX() - qualityNode.getFullBoundsReference().getWidth() - 3,
+                               -( qualityNode.getFullBoundsReference().getHeight() / 2 ) );
+
+        // default state
+        setValueVisible( true );
+    }
+
+    public void setValueVisible( boolean visible ) {
+        valueNode.setVisible( visible );
+        qualityNode.setVisible( !visible );
+    }
+}
