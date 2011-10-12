98d9041efece6e89b1745b0dd40a3a2e76d75efb
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-10-12 17:31:10 +0000

    10/12/11 use water color from ph-scale

---
+++ trunk/simulations-java/simulations/dilutions/src/edu/colorado/phet/dilutions/DilutionsColors.java
@@ -0,0 +1,14 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.dilutions;
+
+import java.awt.Color;
+
+/**
+ * Colors used throughout the simulation.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
+public class DilutionsColors {
+
+    public static final Color WATER_COLOR = new Color( 193, 222, 227 );
+}
