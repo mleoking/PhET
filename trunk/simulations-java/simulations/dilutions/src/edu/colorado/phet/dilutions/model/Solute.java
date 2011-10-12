1a76e9254185a5e56ec915e1d19bc171238eb091
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-10-12 18:49:09 +0000

    add moles-to-particles multiplier for precipitates

--- trunk/simulations-java/simulations/dilutions/src/edu/colorado/phet/dilutions/model/Solute.java
+++ trunk/simulations-java/simulations/dilutions/src/edu/colorado/phet/dilutions/model/Solute.java
@@ -20,72 +20,74 @@ public abstract class Solute {
     public final Color solutionColor;
     public final Color precipitateColor;
     public final double precipitateScale; // how much to scale the precipitate particles in the view
+    public final int precipitateParticlesMultiplier; // multiply number of moles of precipitate by this number to arrive at number of particles to render
 
     // For most solutes, the color of the precipitate is the same as the color in solution.
-    public Solute( String name, String formula, double maxConcentration, Color solutionColor, double precipitateScale ) {
-        this( name, formula, maxConcentration, solutionColor, solutionColor, precipitateScale );
+    public Solute( String name, String formula, double maxConcentration, Color solutionColor, double precipitateScale, int precipitateParticlesMultiplier ) {
+        this( name, formula, maxConcentration, solutionColor, solutionColor, precipitateScale, precipitateParticlesMultiplier );
     }
 
-    public Solute( String name, String formula, double maxConcentration, Color solutionColor, Color precipitateColor, double precipitateScale ) {
+    public Solute( String name, String formula, double maxConcentration, Color solutionColor, Color precipitateColor, double precipitateScale, int precipitateParticlesMultiplier ) {
         this.name = name;
         this.formula = formula;
         this.maxConcentration = maxConcentration;
         this.solutionColor = solutionColor;
         this.precipitateColor = precipitateColor;
         this.precipitateScale = precipitateScale;
+        this.precipitateParticlesMultiplier = precipitateParticlesMultiplier;
     }
 
     public static class KoolAid extends Solute {
         public KoolAid() {
-            super( Strings.KOOL_AID, Symbols.KOOL_AID, 5.0, PhetColorScheme.RED_COLORBLIND, 1 );
+            super( Strings.KOOL_AID, Symbols.KOOL_AID, 5.0, PhetColorScheme.RED_COLORBLIND, 1, 200 );
         }
     }
 
     public static class CobaltIINitrate extends Solute {
         public CobaltIINitrate() {
-            super( Strings.COBALT_II_NITRATE, Symbols.COBALT_II_NITRATE, 5.0, PhetColorScheme.RED_COLORBLIND, 1 );
+            super( Strings.COBALT_II_NITRATE, Symbols.COBALT_II_NITRATE, 5.0, PhetColorScheme.RED_COLORBLIND, 1, 200 );
         }
     }
 
     public static class NickelChloride extends Solute {
         public NickelChloride() {
-            super( Strings.NICKEL_II_CHLORIDE, Symbols.NICKEL_II_CHLORIDE, 5.0, Color.GREEN, 1 );
+            super( Strings.NICKEL_II_CHLORIDE, Symbols.NICKEL_II_CHLORIDE, 5.0, Color.GREEN, 1, 200 );
         }
     }
 
     public static class CobaltChloride extends Solute {
         public CobaltChloride() {
-            super( Strings.COBALT_CHLORIDE, Symbols.COBALT_CHLORIDE, 4.35, new Color( 255, 147, 158 ) /* rose pink */, 1 );
+            super( Strings.COBALT_CHLORIDE, Symbols.COBALT_CHLORIDE, 4.35, new Color( 255, 147, 158 ) /* rose pink */, 1, 200 );
         }
     }
 
     public static class PotassiumChromate extends Solute {
         public PotassiumChromate() {
-            super( Strings.POTASSIUM_CHROMATE, Symbols.POTASSIUM_CHROMATE, 3.35, Color.YELLOW, 1 );
+            super( Strings.POTASSIUM_CHROMATE, Symbols.POTASSIUM_CHROMATE, 3.35, Color.YELLOW, 1, 200 );
         }
     }
 
     public static class GoldIIIChloride extends Solute {
         public GoldIIIChloride() {
-            super( Strings.GOLD_III_CHLORIDE, Symbols.GOLD_III_CHLORIDE, 2.25, Color.YELLOW, 1 );
+            super( Strings.GOLD_III_CHLORIDE, Symbols.GOLD_III_CHLORIDE, 2.25, Color.YELLOW, 1, 200 );
         }
     }
 
     public static class CopperSulfate extends Solute {
         public CopperSulfate() {
-            super( Strings.COPPER_SULFATE, Symbols.COPPER_SULFATE, 1.40, Color.BLUE, 1 );
+            super( Strings.COPPER_SULFATE, Symbols.COPPER_SULFATE, 1.40, Color.BLUE, 1, 200 );
         }
     }
 
     public static class PotassiumDichromate extends Solute {
         public PotassiumDichromate() {
-            super( Strings.POTASSIUM_DICHROMATE, Symbols.POTASSIUM_DICHROMATE, 0.50, Color.ORANGE, 1 );
+            super( Strings.POTASSIUM_DICHROMATE, Symbols.POTASSIUM_DICHROMATE, 0.50, Color.ORANGE, 1, 200 );
         }
     }
 
     public static class PotassiumPermanganate extends Solute {
         public PotassiumPermanganate() {
-            super( Strings.POTASSIUM_PERMANGANATE, Symbols.POTASSIUM_PERMANGANATE, 0.50, new Color( 117, 57, 255 ) /* purple */, Color.BLACK, 1 );
+            super( Strings.POTASSIUM_PERMANGANATE, Symbols.POTASSIUM_PERMANGANATE, 0.50, new Color( 117, 57, 255 ) /* purple */, Color.BLACK, 1, 200 );
         }
     }
 }
