f4ef6b97024ef0a6ef3bde5dbec0be489c8a6ea0
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-10-14 21:07:29 +0000

    Consolidated duplicated code in the app classes for SOM and SOM basics.

--- simulations-java/simulations/states-of-matter/src/edu/colorado/phet/statesofmatter/StatesOfMatterBasicsApplication.java
+++ simulations-java/simulations/states-of-matter/src/edu/colorado/phet/statesofmatter/StatesOfMatterBasicsApplication.java
@@ -3,26 +3,16 @@
 package edu.colorado.phet.statesofmatter;
 
 import java.awt.Frame;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JMenu;
-import javax.swing.JRadioButtonMenuItem;
 
 import edu.colorado.phet.common.phetcommon.application.ApplicationConstructor;
 import edu.colorado.phet.common.phetcommon.application.PhetApplication;
 import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
 import edu.colorado.phet.common.phetcommon.application.PhetApplicationLauncher;
 import edu.colorado.phet.common.phetcommon.util.IProguardKeepClass;
-import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
 import edu.colorado.phet.common.phetcommon.view.PhetLookAndFeel;
-import edu.colorado.phet.common.phetcommon.view.menu.OptionsMenu;
-import edu.colorado.phet.common.phetcommon.view.menu.TeacherMenu;
-import edu.colorado.phet.common.piccolophet.PiccoloPhetApplication;
-import edu.colorado.phet.statesofmatter.developer.DeveloperControlsMenuItem;
 import edu.colorado.phet.statesofmatter.module.phasechanges.PhaseChangesModule;
 import edu.colorado.phet.statesofmatter.module.solidliquidgas.SolidLiquidGasModule;
-import edu.colorado.phet.statesofmatter.view.TemperatureUnits;
+import edu.colorado.phet.statesofmatter.view.AbstractStatesOfMatterApplication;
 
 /**
  * Main application class for the States of Matter - Basics simulation.  This
@@ -31,73 +21,16 @@ import edu.colorado.phet.statesofmatter.view.TemperatureUnits;
  *
  * @author John Blanco
  */
-public class StatesOfMatterBasicsApplication extends PiccoloPhetApplication implements IProguardKeepClass {
+public class StatesOfMatterBasicsApplication extends AbstractStatesOfMatterApplication implements IProguardKeepClass {
 
-    //----------------------------------------------------------------------------
-    // Class Data
-    //----------------------------------------------------------------------------
-
-    //----------------------------------------------------------------------------
-    // Instance Data
-    //----------------------------------------------------------------------------
-
-    private SolidLiquidGasModule m_solidLiquidGasModule;
-    private PhaseChangesModule m_phaseChangesModule;
-
-    //----------------------------------------------------------------------------
-    // Sole Constructor
-    //----------------------------------------------------------------------------
-
+    /**
+     * Constructor.
+     *
+     * @param config
+     */
     public StatesOfMatterBasicsApplication( PhetApplicationConfig config ) {
         super( config );
         initModules();
-        initMenubar();
-    }
-
-    //----------------------------------------------------------------------------
-    // Initialization
-    //----------------------------------------------------------------------------
-
-    /**
-     * Initializes the menu bar.
-     */
-    protected void initMenubar() {
-        // Options menu
-        OptionsMenu optionsMenu = new OptionsMenu();
-        final JRadioButtonMenuItem kelvinRadioButton = new JRadioButtonMenuItem( StatesOfMatterStrings.KELVIN ) {{
-            addActionListener( new ActionListener() {
-                public void actionPerformed( ActionEvent e ) {
-                    StatesOfMatterGlobalState.temperatureUnitsProperty.set( TemperatureUnits.KELVIN );
-                }
-            } );
-        }};
-        optionsMenu.add( kelvinRadioButton );
-
-        final JRadioButtonMenuItem celsiusRadioButton = new JRadioButtonMenuItem( StatesOfMatterStrings.CELSIUS ) {{
-            addActionListener( new ActionListener() {
-                public void actionPerformed( ActionEvent e ) {
-                    StatesOfMatterGlobalState.temperatureUnitsProperty.set( TemperatureUnits.CELSIUS );
-                }
-            } );
-        }};
-        optionsMenu.add( celsiusRadioButton );
-        StatesOfMatterGlobalState.temperatureUnitsProperty.addObserver( new VoidFunction1<TemperatureUnits>() {
-            public void apply( TemperatureUnits temperatureUnitsValue ) {
-                kelvinRadioButton.setSelected( temperatureUnitsValue == TemperatureUnits.KELVIN );
-                celsiusRadioButton.setSelected( temperatureUnitsValue == TemperatureUnits.CELSIUS );
-            }
-        } );
-        getPhetFrame().addMenu( optionsMenu );
-
-        // Add a Teacher menu with an item to change the background to white
-        // for use in making handouts, on projectors, etc.
-        getPhetFrame().addMenu( new TeacherMenu() {{
-            addWhiteBackgroundMenuItem( StatesOfMatterGlobalState.whiteBackground );
-        }} );
-
-        // Developer menu
-        JMenu developerMenu = getPhetFrame().getDeveloperMenu();
-        developerMenu.add( new DeveloperControlsMenuItem( this ) );
     }
 
     /**
@@ -106,16 +39,9 @@ public class StatesOfMatterBasicsApplication extends PiccoloPhetApplication impl
     private void initModules() {
         Frame parentFrame = getPhetFrame();
 
-        m_solidLiquidGasModule = new SolidLiquidGasModule( parentFrame );
-        addModule( m_solidLiquidGasModule );
-
-        m_phaseChangesModule = new PhaseChangesModule( false );
-        addModule( m_phaseChangesModule );
+        addModule( new SolidLiquidGasModule( parentFrame ) );
+        addModule( new PhaseChangesModule( false ) );
     }
-
-    //----------------------------------------------------------------------------
-    // main
-    //----------------------------------------------------------------------------
 
     /**
      * Main entry point.
