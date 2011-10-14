6e08055b0504fe11d725e185b9f4eff4bca8babb
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-10-14 17:54:52 +0000

    repackaged classes

--- simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/intro/FractionsApplication.java
+++ simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/intro/FractionsApplication.java
@@ -1,5 +1,5 @@
 // Copyright 2002-2011, University of Colorado
-package edu.colorado.phet.fractions;
+package edu.colorado.phet.fractions.intro;
 
 import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
 import edu.colorado.phet.common.phetcommon.application.PhetApplicationLauncher;
