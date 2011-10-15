da6d0228c76f9581a3057cf609352612799d61aa
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-10-15 20:14:39 +0000

    added new modules

---
+++ trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/intro/FractionsLabModule.java
@@ -0,0 +1,14 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.fractions.intro;
+
+import edu.colorado.phet.common.phetcommon.application.Module;
+import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
+
+/**
+ * @author Sam Reid
+ */
+public class FractionsLabModule extends Module {
+    public FractionsLabModule() {
+        super( "Fractions Lab", new ConstantDtClock() );
+    }
+}
