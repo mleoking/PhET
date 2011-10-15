01573c76dc5030daf805b75b32fc66ae445eea0b
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-10-15 22:09:26 +0000

    Matching Game tab: Fixed centering of fractions

--- trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/intro/matchinggame/view/FractionRepresentationNode.java
+++ trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/intro/matchinggame/view/FractionRepresentationNode.java
@@ -5,10 +5,9 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.geom.Line2D;
 
-import edu.colorado.phet.common.phetcommon.model.property.CompositeProperty;
-import edu.colorado.phet.common.phetcommon.util.function.Function0;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
+import edu.colorado.phet.common.piccolophet.nodes.PhetPText;
 import edu.colorado.phet.fractions.intro.intro.view.ObservableFractionNumberNode;
 import edu.colorado.phet.fractions.intro.matchinggame.model.FractionRepresentation;
 
@@ -20,19 +19,12 @@ public class FractionRepresentationNode extends RepresentationNode {
         super( transform, fractionRepresentation );
         final PhetPPath line = new PhetPPath( new Line2D.Double( 0, 0, 67, 0 ), new BasicStroke( 4 ), Color.black );
         addChild( line );
-        addChild( new ObservableFractionNumberNode( new CompositeProperty<Integer>( new Function0<Integer>() {
-            public Integer apply() {
-                return fractionRepresentation.fraction.numerator;
-            }
-        } ) ) {{
-            setOffset( line.getFullBounds().getCenterX() - getFullBounds().getWidth() / 2 - 20, line.getFullBounds().getY() - getFullBounds().getHeight() );
+        addChild( new PhetPText( fractionRepresentation.fraction.numerator + "", ObservableFractionNumberNode.FONT ) {{
+            setOffset( line.getFullBounds().getCenterX() - getFullBounds().getWidth() / 2, line.getFullBounds().getY() - getFullBounds().getHeight() );
         }} );
-        addChild( new ObservableFractionNumberNode( new CompositeProperty<Integer>( new Function0<Integer>() {
-            public Integer apply() {
-                return fractionRepresentation.fraction.denominator;
-            }
-        } ) ) {{
-            setOffset( line.getFullBounds().getCenterX() - getFullBounds().getWidth() / 2 - 20, line.getFullBounds().getY() );
+
+        addChild( new PhetPText( fractionRepresentation.fraction.denominator + "", ObservableFractionNumberNode.FONT ) {{
+            setOffset( line.getFullBounds().getCenterX() - getFullBounds().getWidth() / 2, line.getFullBounds().getY() );
         }} );
     }
 }
