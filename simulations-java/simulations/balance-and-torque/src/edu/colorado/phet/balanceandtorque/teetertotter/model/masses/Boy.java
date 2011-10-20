bb90987c7d6eb64294670c8f4a44301cbce7200f
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-10-20 22:19:07 +0000

    Adjusted the mass of the boy to make it work in more challenges.

--- simulations-java/simulations/balance-and-torque/src/edu/colorado/phet/balanceandtorque/teetertotter/model/masses/Boy.java
+++ simulations-java/simulations/balance-and-torque/src/edu/colorado/phet/balanceandtorque/teetertotter/model/masses/Boy.java
@@ -15,7 +15,7 @@ import edu.colorado.phet.balanceandtorque.BalanceAndTorqueResources.Images;
  */
 public class Boy extends HumanMass {
 
-    public static final double MASS = 25; // in kg
+    public static final double MASS = 20; // in kg
     private static final double STANDING_HEIGHT = 1.1; // In meters.
     private static final double SITTING_HEIGHT = 0.7; // In meters.
     private static final double SITTING_CENTER_OF_MASS_X_OFFSET = 0.07; // In meters, determined visually.  Update if image changes.
