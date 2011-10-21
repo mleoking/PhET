837070ad73aaa6aa55e84c156a192013adbb1136
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-10-21 15:33:50 +0000

    10/21/11 put a thin line around solution surface

--- simulations-java/simulations/dilutions/src/edu/colorado/phet/dilutions/view/SolutionNode.java
+++ simulations-java/simulations/dilutions/src/edu/colorado/phet/dilutions/view/SolutionNode.java
@@ -1,6 +1,8 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.dilutions.view;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
 import java.awt.Shape;
 import java.awt.geom.Area;
 import java.awt.geom.Ellipse2D;
@@ -14,19 +16,21 @@ import edu.colorado.phet.dilutions.DilutionsColors;
 import edu.colorado.phet.dilutions.model.Solution;
 import edu.umd.cs.piccolo.nodes.PPath;
 import edu.umd.cs.piccolo.util.PDimension;
+import edu.umd.cs.piccolox.nodes.PComposite;
 
 /**
  * Soution shown in the beaker in the "Molarity" tab.
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
-public class SolutionNode extends PPath {
+public class SolutionNode extends PComposite {
 
-    private static final double CYLINDER_END_HEIGHT = BeakerNode.CYLINDER_END_HEIGHT; //XXX
+    private static final double CYLINDER_END_HEIGHT = BeakerNode.CYLINDER_END_HEIGHT; //TODO remove this dependency
 
     private final PDimension beakerSize;
     private final Solution solution;
     private final LinearFunction volumeFunction;
+    private final PPath cylinderNode, surfaceNode;
 
     public SolutionNode( PDimension beakerSize, Solution solution, DoubleRange volumeRange ) {
 
@@ -34,12 +38,23 @@ public class SolutionNode extends PPath {
         setPickable( false );
         setChildrenPickable( false );
 
-        setStroke( null );
-
         this.beakerSize = beakerSize;
         this.solution = solution;
         this.volumeFunction = new LinearFunction( volumeRange.getMin(), volumeRange.getMax(),
                                                   ( volumeRange.getMin() / volumeRange.getMax() ) * beakerSize.getHeight(), beakerSize.getHeight() );
+
+        // nodes
+        cylinderNode = new PPath() {{
+            setStroke( null );
+        }};
+        surfaceNode = new PPath() {{
+            setStroke( new BasicStroke( 0.5f ) );
+            setStrokePaint( new Color( 0, 0, 0, 100 ) );
+        }};
+
+        // rendering order
+        addChild( cylinderNode );
+        addChild( surfaceNode );
 
         SimpleObserver observer = new SimpleObserver() {
             public void update() {
@@ -57,11 +72,14 @@ public class SolutionNode extends PPath {
         // update the color of the solution, accounting for saturation
         LinearFunction f = new LinearFunction( 0, solution.getSaturatedConcentration(), 0, 1 );
         double colorScale = f.evaluate( solution.getConcentration() );
-        setPaint( ColorUtils.interpolateRBGA( DilutionsColors.WATER_COLOR, solution.solute.get().solutionColor, colorScale ) );
+        Color color = ColorUtils.interpolateRBGA( DilutionsColors.WATER_COLOR, solution.solute.get().solutionColor, colorScale );
+        cylinderNode.setPaint( color );
+        surfaceNode.setPaint( color );
 
         // update amount of stuff in the beaker, based on solution volume
         double height = volumeFunction.evaluate( solution.volume.get() );
-        setPathTo( createCylinderShape( height ) );
+        cylinderNode.setPathTo( createCylinderShape( height ) );
+        surfaceNode.setPathTo( createSurfaceShape( height ) );
     }
 
     private Shape createCylinderShape( double height ) {
@@ -69,5 +87,9 @@ public class SolutionNode extends PPath {
         area.add( new Area( new Ellipse2D.Double( 0, beakerSize.getHeight() - height - ( CYLINDER_END_HEIGHT / 2 ), beakerSize.getWidth(), CYLINDER_END_HEIGHT ) ) );
         area.add( new Area( new Ellipse2D.Double( 0, beakerSize.getHeight() - ( CYLINDER_END_HEIGHT / 2 ), beakerSize.getWidth(), CYLINDER_END_HEIGHT ) ) );
         return area;
+    }
+
+    private Shape createSurfaceShape( double height ) {
+        return new Ellipse2D.Double( 0, beakerSize.getHeight() - height - ( CYLINDER_END_HEIGHT / 2 ), beakerSize.getWidth(), CYLINDER_END_HEIGHT );
     }
 }
