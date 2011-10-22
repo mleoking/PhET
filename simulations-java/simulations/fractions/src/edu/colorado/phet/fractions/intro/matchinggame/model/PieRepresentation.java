26ca4d10c8fc04cb01c0487987a428300f07e931
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-10-22 03:55:35 +0000

    added pie representation

--- simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/intro/matchinggame/model/PieRepresentation.java
+++ simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/intro/matchinggame/model/PieRepresentation.java
@@ -1,6 +1,7 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.fractions.intro.matchinggame.model;
 
+import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
 import edu.colorado.phet.fractions.intro.intro.model.Fraction;
 import edu.colorado.phet.fractions.intro.matchinggame.view.PieNode;
@@ -10,8 +11,8 @@ import edu.umd.cs.piccolo.PNode;
  * @author Sam Reid
  */
 public class PieRepresentation extends Representation {
-    protected PieRepresentation( ModelViewTransform transform, Fraction fraction, double x, double y ) {
-        super( transform, fraction, x, y );
+    protected PieRepresentation( ModelViewTransform transform, Fraction fraction, ImmutableVector2D position ) {
+        super( transform, fraction, position.getX(), position.getY() );
     }
 
     @Override protected PNode toNode( ModelViewTransform transform ) {
