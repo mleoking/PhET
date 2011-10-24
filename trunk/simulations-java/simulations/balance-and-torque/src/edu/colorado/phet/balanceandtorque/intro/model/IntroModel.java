2bdfd5931154a53b5e5341fd9fbd28be0371935b
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-10-24 23:05:23 +0000

    Refactoring - Made this a subclass of the new balance model.

--- trunk/simulations-java/simulations/balance-and-torque/src/edu/colorado/phet/balanceandtorque/intro/model/IntroModel.java
+++ trunk/simulations-java/simulations/balance-and-torque/src/edu/colorado/phet/balanceandtorque/intro/model/IntroModel.java
@@ -4,26 +4,14 @@ package edu.colorado.phet.balanceandtorque.intro.model;
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import edu.colorado.phet.balanceandtorque.common.BalanceAndTorqueSharedConstants;
-import edu.colorado.phet.balanceandtorque.common.model.AttachmentBar;
-import edu.colorado.phet.balanceandtorque.common.model.ColumnState;
-import edu.colorado.phet.balanceandtorque.common.model.FulcrumAbovePlank;
-import edu.colorado.phet.balanceandtorque.common.model.Plank;
-import edu.colorado.phet.balanceandtorque.common.model.SupportColumn;
+import edu.colorado.phet.balanceandtorque.balancelab.model.BalanceModel;
 import edu.colorado.phet.balanceandtorque.common.model.UserMovableModelElement;
 import edu.colorado.phet.balanceandtorque.common.model.masses.FireExtinguisher;
 import edu.colorado.phet.balanceandtorque.common.model.masses.Mass;
 import edu.colorado.phet.balanceandtorque.common.model.masses.SmallTrashCan;
-import edu.colorado.phet.common.phetcommon.model.Resettable;
-import edu.colorado.phet.common.phetcommon.model.clock.ClockAdapter;
-import edu.colorado.phet.common.phetcommon.model.clock.ClockEvent;
-import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
 import edu.colorado.phet.common.phetcommon.model.property.ChangeObserver;
-import edu.colorado.phet.common.phetcommon.model.property.Property;
-import edu.colorado.phet.common.phetcommon.util.ObservableList;
 
 /**
  * Primary model class for the intro tab in the balancing act simulation.
@@ -32,47 +20,11 @@ import edu.colorado.phet.common.phetcommon.util.ObservableList;
  *
  * @author John Blanco
  */
-public class IntroModel implements Resettable {
-    //------------------------------------------------------------------------
-    // Class Data
-    //------------------------------------------------------------------------
-
-    private static final double FULCRUM_HEIGHT = 0.85; // In meters.
-    private static final double PLANK_HEIGHT = 0.75; // In meters.
+public class IntroModel extends BalanceModel {
 
     //------------------------------------------------------------------------
     // Instance Data
     //------------------------------------------------------------------------
-
-    // Clock that drives all time-dependent behavior in this model.
-    private final ConstantDtClock clock = new ConstantDtClock( BalanceAndTorqueSharedConstants.FRAME_RATE );
-
-    // A list of all the masses in the model
-    public final ObservableList<Mass> massList = new ObservableList<Mass>();
-
-    // Fulcrum on which the plank pivots
-    private final FulcrumAbovePlank fulcrum = new FulcrumAbovePlank( 1, FULCRUM_HEIGHT );
-
-    // Support columns
-    private final List<SupportColumn> supportColumns = new ArrayList<SupportColumn>() {{
-        // Note: These are positioned so that the closing window that is
-        // placed on them (the red x) is between two snap-to points on the
-        // plank that the they don't get blocked by force vectors.
-        add( new SupportColumn( PLANK_HEIGHT, -1.625 ) );
-        add( new SupportColumn( PLANK_HEIGHT, 1.625 ) );
-    }};
-
-    // Property that controls whether two, one or zero columns are supporting the plank.
-    public final Property<ColumnState> columnState = new Property<ColumnState>( ColumnState.DOUBLE_COLUMNS );
-
-    // Plank upon which the various masses can be placed.
-    private final Plank plank = new Plank( clock,
-                                           new Point2D.Double( 0, PLANK_HEIGHT ),
-                                           new Point2D.Double( 0, FULCRUM_HEIGHT ),
-                                           columnState );
-
-    // Bar that attaches the fulcrum to the pivot point.
-    private final AttachmentBar attachmentBar = new AttachmentBar( plank );
 
     // Map of the initial positions for the masses, used during resets.
     Map<Mass, Point2D> mapMassesToInitialPositions = new HashMap<Mass, Point2D>();
@@ -82,12 +34,6 @@ public class IntroModel implements Resettable {
     //------------------------------------------------------------------------
 
     public IntroModel() {
-        clock.addClockListener( new ClockAdapter() {
-            @Override public void clockTicked( ClockEvent clockEvent ) {
-                stepInTime( clock.getDt() );
-            }
-        } );
-
         // Add the initial masses and save their initial positions.
         Mass mass = new FireExtinguisher( false ) {{
             setPosition( 2.7, 0 );
@@ -111,18 +57,8 @@ public class IntroModel implements Resettable {
     // Methods
     //------------------------------------------------------------------------
 
-    public ConstantDtClock getClock() {
-        return clock;
-    }
-
-    private void stepInTime( double dt ) {
-        for ( Mass mass : new ArrayList<Mass>( massList ) ) {
-            mass.stepInTime( dt );
-        }
-    }
-
     // Adds a mass to the model.
-    public UserMovableModelElement addMass( final Mass mass ) {
+    @Override public UserMovableModelElement addMass( final Mass mass ) {
         mass.userControlled.addObserver( new ChangeObserver<Boolean>() {
             public void update( Boolean newValue, Boolean oldValue ) {
                 if ( newValue == false && oldValue == true ) {
@@ -140,44 +76,13 @@ public class IntroModel implements Resettable {
         return mass;
     }
 
-    /**
-     * Removes a mass right away, with no animation.
-     *
-     * @param mass
-     */
-    private void removeMass( final Mass mass ) {
-        massList.remove( mass );
-    }
-
-    public FulcrumAbovePlank getFulcrum() {
-        return fulcrum;
-    }
-
-    public Plank getPlank() {
-        return plank;
-    }
-
-    public AttachmentBar getAttachmentBar() {
-        return attachmentBar;
-    }
-
-    public List<SupportColumn> getSupportColumns() {
-        return supportColumns;
-    }
-
     public void reset() {
-        getClock().resetSimulationTime();
-
-        // Remove masses from the plank.
-        plank.removeAllMasses();
+        super.reset();
 
         // Move each mass back to its original position.
         for ( Mass mass : new ArrayList<Mass>( massList ) ) {
             assert mapMassesToInitialPositions.containsKey( mass );  // Should have initial positions for all masses.
             mass.setPosition( mapMassesToInitialPositions.get( mass ) );
         }
-
-        // Set the support columns to their initial state.
-        columnState.reset();
     }
 }
