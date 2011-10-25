2d906e1b7fd4f89902e5e8fccf0ed559c36dd163
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-10-25 22:10:23 +0000

    Rewrote model

--- simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/intro/matchinggame/model/MatchingGameModel.java
+++ simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/intro/matchinggame/model/MatchingGameModel.java
@@ -1,46 +1,48 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.fractions.intro.matchinggame.model;
 
-import fj.F2;
-
-import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
-import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
+import edu.colorado.phet.fractions.intro.common.view.Pattern;
 import edu.colorado.phet.fractions.intro.intro.model.Fraction;
-import edu.umd.cs.piccolo.util.PBounds;
-
-import static edu.colorado.phet.common.phetcommon.view.util.RectangleUtils.expand;
+import edu.colorado.phet.fractions.intro.matchinggame.view.DecimalFractionNode;
+import edu.colorado.phet.fractions.intro.matchinggame.view.FractionRepresentationNode;
+import edu.colorado.phet.fractions.intro.matchinggame.view.PatternNode;
+import edu.colorado.phet.fractions.intro.matchinggame.view.PieNode;
+import edu.colorado.phet.fractions.intro.matchinggame.view.RepresentationNode;
 
 /**
  * @author Sam Reid
  */
 public class MatchingGameModel {
-    public final ArrayList<Representation> fractionRepresentations = new ArrayList<Representation>();
+    public final ArrayList<RepresentationNode> nodes = new ArrayList<RepresentationNode>();
     private final Random random = new Random();
 
     //Allow fractions to get closer together so it won't go into an infinite loop if they have to overlap
     private final double padding = 20;
 
     public MatchingGameModel( ModelViewTransform transform ) {
-
-        final ArrayList<F2<ModelViewTransform, Fraction, Representation>> representations = new ArrayList<F2<ModelViewTransform, Fraction, Representation>>() {{
-            add( new F2<ModelViewTransform, Fraction, Representation>() {
-                public Representation f( ModelViewTransform transform, Fraction fraction ) {
-                    return createDecimalRepresentation( transform, fraction );
+        ArrayList<Representation> representations = new ArrayList<Representation>() {{
+            add( new Representation() {
+                public RepresentationNode createNode( ModelViewTransform transform, Fraction fraction ) {
+                    return new DecimalFractionNode( transform, fraction );
                 }
             } );
-            add( new F2<ModelViewTransform, Fraction, Representation>() {
-                public Representation f( ModelViewTransform transform, Fraction fraction ) {
-                    return createFractionRepresentation( transform, fraction );
+            add( new Representation() {
+                public RepresentationNode createNode( ModelViewTransform transform, Fraction fraction ) {
+                    return new FractionRepresentationNode( transform, fraction );
                 }
             } );
-            add( new F2<ModelViewTransform, Fraction, Representation>() {
-                public Representation f( ModelViewTransform transform, Fraction fraction ) {
-                    return createPieRepresentation( transform, fraction );
+            add( new Representation() {
+                public RepresentationNode createNode( ModelViewTransform transform, Fraction fraction ) {
+                    return new PieNode( transform, fraction );
+                }
+            } );
+            add( new Representation() {
+                public RepresentationNode createNode( ModelViewTransform transform, Fraction fraction ) {
+                    return new PatternNode( transform, new Pattern.NineGrid(), fraction );
                 }
             } );
         }};
@@ -48,49 +50,39 @@ public class MatchingGameModel {
         for ( int i = 0; i < 4; i++ ) {
             Fraction fraction = new Fraction( random.nextInt( 11 ) + 1, random.nextInt( 11 ) + 1 );
 
-            ArrayList<F2<ModelViewTransform, Fraction, Representation>> remainingRepresentations = new ArrayList<F2<ModelViewTransform, Fraction, Representation>>( representations );
+            ArrayList<Representation> remainingRepresentations = new ArrayList<Representation>( representations );
 
             for ( int k = 0; k < 2; k++ ) {
                 int selected = random.nextInt( remainingRepresentations.size() );
-                F2<ModelViewTransform, Fraction, Representation> selectedRepresentation = remainingRepresentations.get( selected );
-                fractionRepresentations.add( selectedRepresentation.f( transform, fraction ) );
+                Representation selectedRepresentation = remainingRepresentations.get( selected );
+                final RepresentationNode node = selectedRepresentation.createNode( transform, fraction );
+                node.setOffset( random.nextInt( 1000 ), random.nextInt( 600 ) );
+                nodes.add( node );
                 remainingRepresentations.remove( selectedRepresentation );
             }
         }
     }
 
-    private PieRepresentation createPieRepresentation( ModelViewTransform transform, Fraction fraction ) {
-        return new PieRepresentation( transform, fraction, getPosition( new FractionRepresentation( transform, fraction ), padding ) );
-    }
-
-    private FractionRepresentation createFractionRepresentation( ModelViewTransform transform, Fraction fraction ) {
-        return new FractionRepresentation( transform, fraction, getPosition( new FractionRepresentation( transform, fraction ), padding ) );
-    }
-
-    private DecimalFraction createDecimalRepresentation( ModelViewTransform transform, Fraction fraction ) {
-        return new DecimalFraction( transform, fraction, getPosition( new DecimalFraction( transform, fraction ), padding ) );
-    }
-
-    private ImmutableVector2D getPosition( Representation decimalFraction, double padding ) {
-        while ( hits( decimalFraction, fractionRepresentations, padding ) && padding > 0 ) {
-            decimalFraction.setOffset( new ImmutableVector2D( random.nextInt( 1000 ), random.nextInt( 600 ) ) );
-            padding = padding - 0.3;
-        }
-        return decimalFraction.getOffset();
-    }
+//    private ImmutableVector2D getPosition( Representation decimalFraction, double padding ) {
+//        while ( hits( decimalFraction, fractionRepresentations, padding ) && padding > 0 ) {
+//            decimalFraction.setOffset( new ImmutableVector2D( random.nextInt( 1000 ), random.nextInt( 600 ) ) );
+//            padding = padding - 0.3;
+//        }
+//        return decimalFraction.getOffset();
+//    }
 
     //Check to see if one node overlaps a pre-existing node, so it can be placed in an open area
-    private boolean hits( Representation representation, List<Representation> representations, double padding ) {
-        for ( Representation a : representations ) {
-            final PBounds aBounds = a.node.getGlobalFullBounds();
-            final PBounds bBounds = representation.node.getGlobalFullBounds();
-            if ( expand( aBounds, padding / 2, padding / 2 ).intersects( expand( bBounds, padding / 2, padding / 2 ) ) ) {
-                return true;
-            }
-            if ( !new Rectangle2D.Double( 0, 0, 1000, 600 ).contains( expand( bBounds, padding, padding ) ) ) {
-                return true;
-            }
-        }
-        return false;
-    }
+//    private boolean hits( Representation representation, List<Representation> representations, double padding ) {
+//        for ( Representation a : representations ) {
+//            final PBounds aBounds = a.node.getGlobalFullBounds();
+//            final PBounds bBounds = representation.node.getGlobalFullBounds();
+//            if ( expand( aBounds, padding / 2, padding / 2 ).intersects( expand( bBounds, padding / 2, padding / 2 ) ) ) {
+//                return true;
+//            }
+//            if ( !new Rectangle2D.Double( 0, 0, 1000, 600 ).contains( expand( bBounds, padding, padding ) ) ) {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
 }
--- simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/intro/matchinggame/view/PieNode.java
+++ simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/intro/matchinggame/view/PieNode.java
@@ -4,17 +4,19 @@ package edu.colorado.phet.fractions.intro.matchinggame.view;
 import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
 import edu.colorado.phet.common.piccolophet.nodes.kit.ZeroOffsetNode;
+import edu.colorado.phet.fractions.intro.intro.model.Fraction;
 import edu.colorado.phet.fractions.intro.intro.view.PieSetFractionNode;
-import edu.colorado.phet.fractions.intro.matchinggame.model.PieRepresentation;
 
 /**
  * @author Sam Reid
  */
 public class PieNode extends RepresentationNode {
-    public PieNode( ModelViewTransform transform, final PieRepresentation representation ) {
-        super( transform, representation );
+    public PieNode( ModelViewTransform transform, final Fraction fraction ) {
+        super( transform, fraction );
 
-        PieSetFractionNode pieSetFractionNode = new PieSetFractionNode( new Property<Integer>( representation.fraction.numerator ), new Property<Integer>( representation.fraction.denominator ), new Property<Boolean>( true ) );
+        PieSetFractionNode pieSetFractionNode = new PieSetFractionNode( new Property<Integer>( fraction.numerator ), new Property<Integer>( fraction.denominator ), new Property<Boolean>( true ) );
         addChild( new ZeroOffsetNode( pieSetFractionNode ) );
+
+        scale( 0.5 );
     }
 }
