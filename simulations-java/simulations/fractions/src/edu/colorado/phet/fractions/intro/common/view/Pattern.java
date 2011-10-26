a2f39c71f81d4d55622b265689062fbb45e0364f
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-10-26 01:59:39 +0000

    Matching game: Added "six plus signs pattern"

--- simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/intro/common/view/Pattern.java
+++ simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/intro/common/view/Pattern.java
@@ -4,8 +4,13 @@ package edu.colorado.phet.fractions.intro.common.view;
 import fj.data.List;
 
 import java.awt.Shape;
+import java.awt.geom.GeneralPath;
 import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
+
+import edu.colorado.phet.common.phetcommon.view.util.DoubleGeneralPath;
+
+import static fj.data.List.iterableList;
 
 /**
  * @author Sam Reid
@@ -21,9 +26,44 @@ public class Pattern {
         return new Rectangle2D.Double( x, y, length, length );
     }
 
+    public static GeneralPath plusSign( double gridX, double gridY, final double edgeLength ) {
+        return new DoubleGeneralPath( gridX * edgeLength + edgeLength, gridY * edgeLength ) {
+            {
+                right();
+                down();
+                right();
+                down();
+                left();
+                down();
+                left();
+                up();
+                left();
+                up();
+                right();
+                up();
+            }
+
+            private void right() {
+                lineToRelative( edgeLength, 0 );
+            }
+
+            private void left() {
+                lineToRelative( -edgeLength, 0 );
+            }
+
+            private void up() {
+                lineToRelative( 0, -edgeLength );
+            }
+
+            private void down() {
+                lineToRelative( 0, edgeLength );
+            }
+        }.getGeneralPath();
+    }
+
     public static class NineGrid extends Pattern {
         public NineGrid() {
-            super( List.iterableList( new ArrayList<Shape>() {{
+            super( iterableList( new ArrayList<Shape>() {{
                 double length = 20;
                 for ( int i = 0; i < 3; i++ ) {
                     for ( int j = 0; j < 3; j++ ) {
@@ -34,7 +74,17 @@ public class Pattern {
         }
     }
 
-//    public static class SixPlusses extends Pattern {
-//
-//    }
+    public static class SixPlusSigns extends Pattern {
+        public SixPlusSigns() {
+            super( iterableList( new ArrayList<Shape>() {{
+                final double edgeLength = 20 / 2;
+                add( plusSign( 1, 0, edgeLength ) );
+                add( plusSign( 3, 1, edgeLength ) );
+                add( plusSign( 5, 2, edgeLength ) );
+                add( plusSign( 0, 2, edgeLength ) );
+                add( plusSign( 2, 3, edgeLength ) );
+                add( plusSign( 4, 4, edgeLength ) );
+            }} ) );
+        }
+    }
 }
