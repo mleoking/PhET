5c8a7d6c71e4412a7a05b1364afbe1648390ac5d
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-10-27 15:37:19 +0000

    commented out hibernate for deploy

--- trunk/util/simsharing/src/edu/colorado/phet/simsharing/server/hibernate/HibernateSimState.java
+++ trunk/util/simsharing/src/edu/colorado/phet/simsharing/server/hibernate/HibernateSimState.java
@@ -1,8 +1,8 @@
-// Copyright 2002-2011, University of Colorado
-package edu.colorado.phet.simsharing.server.hibernate;
-
-/**
- * @author Sam Reid
- */
-public class HibernateSimState {
-}
+//// Copyright 2002-2011, University of Colorado
+//package edu.colorado.phet.simsharing.server.hibernate;
+//
+///**
+// * @author Sam Reid
+// */
+//public class HibernateSimState {
+//}
