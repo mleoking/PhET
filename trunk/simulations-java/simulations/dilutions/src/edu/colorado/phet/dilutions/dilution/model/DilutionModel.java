c23bdae33384126bf019eeed1b38dcf9f284cf79
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-10-27 15:41:35 +0000

    10/27/11 Dilution: use (0,1) for range of M1 and M2

--- trunk/simulations-java/simulations/dilutions/src/edu/colorado/phet/dilutions/dilution/model/DilutionModel.java
+++ trunk/simulations-java/simulations/dilutions/src/edu/colorado/phet/dilutions/dilution/model/DilutionModel.java
@@ -17,22 +17,25 @@ import edu.colorado.phet.dilutions.common.model.Solution;
 public class DilutionModel implements Resettable {
 
     private static final double MAX_BEAKER_VOLUME = 1; // liter
-    private static final DoubleRange SOLUTE_AMOUNT_RANGE = new DoubleRange( 0, 1, 0.5 ); // moles
+    private static final DoubleRange SOLUTE_AMOUNT_RANGE = new DoubleRange( 0, 0.2, 0.1 ); // moles
     private static final DoubleRange SOLUTION_VOLUME_RANGE = new DoubleRange( 0.0, 0.2, 0.1 ); // liters
     private static final DoubleRange DILUTION_VOLUME_RANGE = new DoubleRange( 0.2, 1, 0.5 ); // liters
+    private static final DoubleRange CONCENTRATION_RANGE = new DoubleRange( SOLUTE_AMOUNT_RANGE.getMin() / DILUTION_VOLUME_RANGE.getMax(),
+                                                                            SOLUTE_AMOUNT_RANGE.getMax() / DILUTION_VOLUME_RANGE.getMin() ); // M
 
     static {
         assert ( SOLUTION_VOLUME_RANGE.getMax() <= MAX_BEAKER_VOLUME );
         assert ( DILUTION_VOLUME_RANGE.getMax() <= MAX_BEAKER_VOLUME );
+        assert ( DILUTION_VOLUME_RANGE.getMin() > 0 );
     }
 
     public final Solute solute;
     public final Solution solution, dilution, water;
 
     public DilutionModel() {
-        this.solute = new Solute( Strings.SOLUTE, "?", 5.0, Color.BLUE, 1, 200 ); // hypothetical solute with unknown formula
-        this.solution = new Solution( solute, 1, SOLUTION_VOLUME_RANGE.getDefault() );
-        this.dilution = new Solution( solute, 1, DILUTION_VOLUME_RANGE.getDefault() );
+        this.solute = new Solute( Strings.SOLUTE, "?", CONCENTRATION_RANGE.getMax(), Color.BLUE, 1, 200 ); // hypothetical solute with unknown formula
+        this.solution = new Solution( solute, SOLUTE_AMOUNT_RANGE.getDefault(), SOLUTION_VOLUME_RANGE.getDefault() );
+        this.dilution = new Solution( solute, SOLUTE_AMOUNT_RANGE.getDefault(), DILUTION_VOLUME_RANGE.getDefault() );
         this.water = new Solution( solute, 0, DILUTION_VOLUME_RANGE.getDefault() );
     }
 
@@ -49,8 +52,7 @@ public class DilutionModel implements Resettable {
     }
 
     public DoubleRange getConcentrationRange() {
-        assert ( DILUTION_VOLUME_RANGE.getMin() != 0 && DILUTION_VOLUME_RANGE.getMax() != 0 );
-        return new DoubleRange( SOLUTE_AMOUNT_RANGE.getMin() / DILUTION_VOLUME_RANGE.getMax(), SOLUTE_AMOUNT_RANGE.getMax() / DILUTION_VOLUME_RANGE.getMin() );
+        return CONCENTRATION_RANGE;
     }
 
     public void reset() {
