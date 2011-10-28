32499e63f1329a44a64a6bf6aa75a1da9817ce05
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-10-28 18:07:39 +0000

    added docs

--- simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/simsharing/IActor.java
+++ simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/simsharing/IActor.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 
 /**
  * Primary abstraction for sending one-way and reply-oriented messages.
+ * This interface and related classes allow clients to connect to the server for data sharing.
+ * Note, the classes named *actors are not full-blown-actors in the Erlang, Scala or Akka sense, but minimal representations for sending messages.
  *
  * @author Sam Reid
  */
