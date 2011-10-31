88c1ca80ebe3fda9ebcd01969bf6f2d716ccc1b4
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-10-31 16:45:59 +0000

    Added file i/o for message storing, see #3136

--- trunk/util/simsharing/src/edu/colorado/phet/simsharing/eventserver/EventServer.java
+++ trunk/util/simsharing/src/edu/colorado/phet/simsharing/eventserver/EventServer.java
@@ -1,11 +1,18 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.simsharing.eventserver;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
 
 import edu.colorado.phet.common.phetcommon.simsharing.MessageHandler;
 import edu.colorado.phet.common.phetcommon.simsharing.MessageServer;
@@ -22,12 +29,56 @@ public class EventServer implements MessageHandler {
     //On phet-server, port must be in a specific range of allowed ports, see Unfuddle ticket
     public static int PORT = 44101;
 
+    private final Map<String, BufferedWriter> map = Collections.synchronizedMap( new HashMap<String, BufferedWriter>() );
+
     private void start() throws IOException {
         new MessageServer( PORT, this ).start();
     }
 
     public void handle( Object message, ObjectOutputStream writeToClient, ObjectInputStream readFromClient ) throws IOException {
-        System.out.println( message );
+        if ( message != null ) {
+            String m = message.toString();
+            System.out.println( message );
+            StringTokenizer st = new StringTokenizer( m, "\t" );
+            String machineID = st.nextToken();
+            String sessionID = st.nextToken();
+
+            String object = st.nextToken();
+            String action = st.nextToken();
+
+            BufferedWriter bufferedWriter = getBufferedWriter( machineID, sessionID );
+            bufferedWriter.write( m );
+            bufferedWriter.newLine();
+            bufferedWriter.flush();
+
+            if ( object.equalsIgnoreCase( "Sim" ) && action.equalsIgnoreCase( "Exited" ) ) {
+                bufferedWriter.close();
+
+                String key = getKey( machineID, sessionID );
+                map.remove( key );
+
+                System.out.println( "Session exited: " + m );
+            }
+        }
+    }
+
+    private BufferedWriter getBufferedWriter( String machineID, String sessionID ) throws IOException {
+        String key = getKey( machineID, sessionID );
+
+        if ( map.containsKey( key ) ) {
+            return map.get( key );
+        }
+        else {
+            File file = new File( "/home/phet/eventserver/data/" + key + ".txt" );
+            file.getParentFile().mkdirs();
+            BufferedWriter bufferedWriter = new BufferedWriter( new FileWriter( file, true ) );
+            map.put( key, bufferedWriter );
+            return bufferedWriter;
+        }
+    }
+
+    private String getKey( String machineID, String sessionID ) {
+        return machineID + "_" + sessionID;
     }
 
     //Use phet-server for deployments, but localhost for local testing.
