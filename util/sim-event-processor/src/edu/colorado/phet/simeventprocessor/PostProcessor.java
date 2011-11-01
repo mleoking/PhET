2397b7be09c8fee156bc55e0a8782b817e4c7cb6
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-11-01 20:57:38 +0000

    Added multi-plots for post-processing, see #3136

--- util/sim-event-processor/src/edu/colorado/phet/simeventprocessor/PostProcessor.java
+++ util/sim-event-processor/src/edu/colorado/phet/simeventprocessor/PostProcessor.java
@@ -49,21 +49,7 @@ public class PostProcessor extends Processor {
         }} );
 
         //Events to search for in Molecule Polarity Tab 1
-        final EntryList eventsOfInterest = new EntryList() {{
-            add( "check box", "pressed", param( "text", "Bond Dipole" ) );
-            add( "check box", "pressed", param( "text", "Partial Charges" ) );
-            add( "check box", "pressed", param( "text", "Bond Character" ) );
-
-            add( "radio button", "pressed", param( "description", "Surface type" ), param( "value", "ELECTROSTATIC_POTENTIAL" ) );
-            add( "radio button", "pressed", param( "description", "Surface type" ), param( "value", "ELECTROSTATIC_POTENTIAL" ) );
-            add( "radio button", "pressed", param( "description", "Surface type" ), param( "value", "NONE" ) );
-
-            add( "radio button", "pressed", param( "description", "Electric field on" ), param( "value", "true" ) );
-            add( "radio button", "pressed", param( "description", "Electric field on" ), param( "value", "false" ) );
-
-            add( "mouse", "dragged", param( "atom", "A" ) );
-            add( "mouse", "dragged", param( "atom", "B" ) );
-        }};
+        final EntryList eventsOfInterest = getMoleculePolarityEventsOfInterest();
 
         plot( "Events of interest", "Time (sec)", "Events", new XYSeries( "Alice" ) {{
             for ( long time = 0; time < eventLog.getLastTime(); time += 1000 ) {
@@ -81,7 +67,50 @@ public class PostProcessor extends Processor {
         System.out.println( "Things the user didn't do: " + copy );
     }
 
+    private EntryList getMoleculePolarityEventsOfInterest() {
+        return new EntryList() {{
+            add( "check box", "pressed", param( "text", "Bond Dipole" ) );
+            add( "check box", "pressed", param( "text", "Partial Charges" ) );
+            add( "check box", "pressed", param( "text", "Bond Character" ) );
+
+            add( "radio button", "pressed", param( "description", "Surface type" ), param( "value", "ELECTROSTATIC_POTENTIAL" ) );
+            add( "radio button", "pressed", param( "description", "Surface type" ), param( "value", "ELECTROSTATIC_POTENTIAL" ) );
+            add( "radio button", "pressed", param( "description", "Surface type" ), param( "value", "NONE" ) );
+
+            add( "radio button", "pressed", param( "description", "Electric field on" ), param( "value", "true" ) );
+            add( "radio button", "pressed", param( "description", "Electric field on" ), param( "value", "false" ) );
+
+            add( "mouse", "dragged", param( "atom", "A" ) );
+            add( "mouse", "dragged", param( "atom", "B" ) );
+        }};
+    }
+
+    @Override public void process( final ArrayList<EventLog> all ) {
+        //Events to search for in Molecule Polarity Tab 1
+        final EntryList eventsOfInterest = getMoleculePolarityEventsOfInterest();
+
+        final int[] id = { 0 };
+        ArrayList<XYSeries> serieses = new ArrayList<XYSeries>() {{
+            for ( final EventLog eventLog : all ) {
+
+                final XYSeries series = new XYSeries( "Student " + ( id[0]++ ) ) {{
+                    for ( long time = 0; time < eventLog.getLastTime(); time += 1000 ) {
+                        int events = eventLog.getNumberOfEvents( time, eventsOfInterest );
+                        System.out.println( time + "\t" + events );
+                        add( time / 1000.0, events );
+                    }
+                }};
+                add( series );
+            }
+        }};
+
+        plot( "Events of interest", "Time (sec)", "Events", serieses.toArray( new XYSeries[serieses.size()] ) );
+    }
+
     public static void main( String[] args ) throws IOException {
-        new PostProcessor().process( new File( "C:\\Users\\Sam\\Desktop\\biglog3.txt" ) );
+        new PostProcessor().process(
+                new File( "C:\\Users\\Sam\\Desktop\\biglog4.txt" ),
+                new File( "C:\\Users\\Sam\\Desktop\\biglog5.txt" ),
+                new File( "C:\\Users\\Sam\\Desktop\\biglog6.txt" ) );
     }
 }
