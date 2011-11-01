b67ece05b9b5e0ce0a7c171d1ada62fec0c2857f
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-11-01 19:12:50 +0000

    11/01/11 Dilution: update moles of solute when M1 or V1 changes 11/01/11 Dilution: update water volume when V1 or V2 changes

--- simulations-java/simulations/dilutions/src/edu/colorado/phet/dilutions/dilution/model/DilutionModel.java
+++ simulations-java/simulations/dilutions/src/edu/colorado/phet/dilutions/dilution/model/DilutionModel.java
@@ -4,7 +4,9 @@ package edu.colorado.phet.dilutions.dilution.model;
 import java.awt.Color;
 
 import edu.colorado.phet.common.phetcommon.model.Resettable;
+import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.phetcommon.util.DoubleRange;
+import edu.colorado.phet.common.phetcommon.util.RichSimpleObserver;
 import edu.colorado.phet.dilutions.DilutionsResources.Strings;
 import edu.colorado.phet.dilutions.common.model.Solute;
 import edu.colorado.phet.dilutions.common.model.Solution;
@@ -25,18 +27,42 @@ public class DilutionModel implements Resettable {
 
     static {
         assert ( SOLUTION_VOLUME_RANGE.getMax() <= MAX_BEAKER_VOLUME );
+        assert ( SOLUTION_VOLUME_RANGE.getMax() <= DILUTION_VOLUME_RANGE.getMin() );
         assert ( DILUTION_VOLUME_RANGE.getMax() <= MAX_BEAKER_VOLUME );
         assert ( DILUTION_VOLUME_RANGE.getMin() > 0 );
     }
 
     public final Solute solute;
     public final Solution solution, dilution, water;
+    public final Property<Double> solutionConcentration; // use a separate property so that we can control this independently of Solution.concentration
 
     public DilutionModel() {
+
         this.solute = new Solute( Strings.SOLUTE, "?", CONCENTRATION_RANGE.getMax(), Color.BLUE, 5, 200 ); // hypothetical solute with unknown formula
         this.solution = new Solution( solute, SOLUTE_AMOUNT_RANGE.getDefault(), SOLUTION_VOLUME_RANGE.getDefault() );
         this.dilution = new Solution( solute, SOLUTE_AMOUNT_RANGE.getDefault(), DILUTION_VOLUME_RANGE.getDefault() );
         this.water = new Solution( solute, 0, DILUTION_VOLUME_RANGE.getDefault() );
+
+        double initialConcentration = ( solution.volume.get() == 0 ) ? 0 : solution.soluteAmount.get() / solution.volume.get();
+        this.solutionConcentration = new Property<Double>( initialConcentration );
+
+        RichSimpleObserver waterVolumeUpdater = new RichSimpleObserver() {
+            @Override public void update() {
+                // compute the amount of water used to dilute the solution
+                water.volume.set( dilution.volume.get() - solution.volume.get() );
+            }
+        };
+        waterVolumeUpdater.observe( solution.volume, dilution.volume );
+
+        RichSimpleObserver soluteAmountUpdater = new RichSimpleObserver() {
+            public void update() {
+                // number of moles is the same for the solution and dilution
+                double soluteAmount = solutionConcentration.get() * solution.volume.get(); // moles=M*Liters
+                solution.soluteAmount.set( soluteAmount );
+                dilution.soluteAmount.set( soluteAmount );
+            }
+        };
+        soluteAmountUpdater.observe( solutionConcentration, solution.volume );
     }
 
     public double getMaxBeakerVolume() {
@@ -59,5 +85,6 @@ public class DilutionModel implements Resettable {
         solution.reset();
         dilution.reset();
         water.reset();
+        solutionConcentration.reset();
     }
 }
