a62caa6f1303fb82c024a9f2c7148f657f574b4d
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-11-01 17:28:10 +0000

    moved main to PostProcessor, see #3136

--- util/sim-event-processor/src/edu/colorado/phet/simeventprocessor/Processor.java
+++ util/sim-event-processor/src/edu/colorado/phet/simeventprocessor/Processor.java
@@ -31,9 +31,4 @@ public class Processor {
 
         new PostProcessor().process( eventLog );
     }
-
-    public static void main( String[] args ) throws IOException {
-//        new Processor().process( new File( "C:\\Users\\Sam\\Desktop\\biglog1.txt" ) );
-        new Processor().process( new File( "C:\\Users\\Sam\\Desktop\\biglog2.txt" ) );
-    }
 }
