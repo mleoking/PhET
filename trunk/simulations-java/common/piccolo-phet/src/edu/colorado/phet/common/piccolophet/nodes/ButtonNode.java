4a04cbdfd7d082b479ecb0255c8bb0872c7951da
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-11-03 00:05:11 +0000

    #3136, camelcase for object

--- trunk/simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/nodes/ButtonNode.java
+++ trunk/simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/nodes/ButtonNode.java
@@ -373,7 +373,7 @@ public class ButtonNode extends PhetPNode {
     private void notifyActionPerformed() {
 
         //Notify about the event first so that listener callbacks were clearly caused by this action
-        SimSharingEvents.actionPerformed( "button node", "pressed", Parameter.param( "actionCommand", actionCommand ) );
+        SimSharingEvents.actionPerformed( "buttonNode", "pressed", Parameter.param( "actionCommand", actionCommand ) );
 
         ActionEvent event = new ActionEvent( this, ActionEvent.ACTION_PERFORMED, actionCommand ); // use Swing convention from AbstractButton.fireActionPerformed
         for ( ActionListener actionListener : new ArrayList<ActionListener>( actionListeners ) ) {
