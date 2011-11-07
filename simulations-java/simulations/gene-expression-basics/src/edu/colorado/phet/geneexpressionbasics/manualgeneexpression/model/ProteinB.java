1f665917e445d5b78ca6a712db53283fb2536dba
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-11-07 22:54:15 +0000

    Added two more proteins.

--- simulations-java/simulations/gene-expression-basics/src/edu/colorado/phet/geneexpressionbasics/manualgeneexpression/model/ProteinB.java
+++ simulations-java/simulations/gene-expression-basics/src/edu/colorado/phet/geneexpressionbasics/manualgeneexpression/model/ProteinB.java
@@ -5,17 +5,18 @@ import java.awt.Color;
 import java.awt.Shape;
 
 import edu.colorado.phet.common.phetcommon.math.MathUtil;
+import edu.colorado.phet.common.phetcommon.math.Vector2D;
 import edu.colorado.phet.common.phetcommon.view.util.DoubleGeneralPath;
 
 /**
  * @author John Blanco
  */
-public class ProteinA extends Protein {
+public class ProteinB extends Protein {
 
     private static final Color BASE_COLOR = Color.YELLOW;
     private static final double FULL_GROWN_WIDTH = 300;
 
-    protected ProteinA( GeneExpressionModel model ) {
+    protected ProteinB( GeneExpressionModel model ) {
         super( model, createInitialShape(), BASE_COLOR );
     }
 
@@ -30,11 +31,12 @@ public class ProteinA extends Protein {
     private static Shape createShape( double growthFactor ) {
         final double currentWidth = MathUtil.clamp( 0.01, growthFactor, 1 ) * FULL_GROWN_WIDTH;
         DoubleGeneralPath path = new DoubleGeneralPath( 0, 0 ) {{
-            moveTo( -currentWidth / 2, 0 );
-            lineTo( 0, -currentWidth / 2 );
-            lineTo( currentWidth / 2, 0 );
-            lineTo( 0, currentWidth / 2 );
-            lineTo( -currentWidth / 2, 0 );
+            Vector2D vector = new Vector2D( -currentWidth / 2, 0 );
+            moveTo( vector.getX(), vector.getY() );
+            for ( int i = 0; i < 6; i++ ) {
+                vector.rotate( Math.PI / 3 );
+                lineTo( vector.getX(), vector.getY() );
+            }
             closePath();
         }};
 
