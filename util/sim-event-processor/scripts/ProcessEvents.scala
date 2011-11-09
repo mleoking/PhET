b26138f4691ba7542c8eac7f81cdc9f152455aa8
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-11-09 05:04:16 +0000

    simplified plots

--- util/sim-event-processor/scripts/ProcessEvents.scala
+++ util/sim-event-processor/scripts/ProcessEvents.scala
@@ -1,15 +1,19 @@
 import edu.colorado.phet.simeventprocessor.scala._
 import phet._
 
-val all = phet load "C:\\Users\\Sam\\Desktop\\all-11-8-2011-iv"
+val all = phet load "C:\\Users\\Sam\\Desktop\\0all-11-8-2011-v"
 
 //def jcNov8Morning(log: EventLog) = log.day == "11-08-2011" && log.study == "colorado" && log.user != "null" && log.user != "samreid"
 //def emNov7Filter(log: EventLog) = log.day == "11-07-2011" && log.study == "utah" && log.user != "null" && log.user != "samreid"
 
 val selected = all.filter(log =>
-                            log.day == "11-08-2011" &&
-                            log.study == "colorado" &&
-                            log.epoch > 1320783060834L).sortBy(_.epoch)
+                          //                            log.day == "11-08-2011" &&
+                                                      log.study == "colorado" &&
+//                            log.study == "utah" &&
+                            //                            log.epoch > 1320792019874L &&
+                            log.user != "samreid" &&
+                            !log.machine.startsWith("samreid") &&
+                            !log.machine.startsWith("chrismalley")).sortBy(_.epoch)
 println("found: " + selected.length + " logs")
 selected.print
 
@@ -22,36 +26,45 @@ println("Number of unique machines: " + machines.size)
 val users = selected.map(_.user).distinct.sortBy(numerical)
 println("Unique userIDs: " + users.size + ": " + users)
 
-val eventCountData = selected.map(log => timeSeries(log, log.countEvents(_)))
+val eventCountData = selected.map(_.eventCountData)
 xyplot("Events vs time", "Time (minutes)", "Events", eventCountData)
 
 for ( log <- selected ) {
   println(log.histogramByObject)
 }
 
-val simToUse = simEvents.balancingChemicalEquations
+val simEventMap = Map("Molecule Shapes" -> simEvents.moleculeShapes,
+                      "Molecule Polarity" -> simEvents.moleculePolarity,
+                      "Balancing Chemical Equations" -> simEvents.balancingChemicalEquations)
 
-val importantEvents = selected.map(log => timeSeries(log, log.countMatches(simToUse, _)))
-xyplot("Filtered events vs time", "Time (minutes)", "Events", importantEvents)
+for ( sim <- simEventMap.keys; simLogs = selected.filter(_.simName == sim); if simLogs.size > 0 ) {
 
-//Print how many features each user used
-val sorted = selected.sortBy(_.findMatches(simToUse).size)
-for ( log <- sorted ) {
-  val userEvents = log findMatches simToUse
-  println("user " + log.user + " matched " + userEvents.size + "/" + simToUse.size)
+  //Find which events are important in this sim
+  val simEvents = simEventMap(sim)
+
+  val filteredDataSets = simLogs.map(_.countEvents(simEvents))
+  xyplot("Filtered events vs time for " + sim, "Time (minutes)", "Events", filteredDataSets)
+
+  //Print how many features each user used
+  val sorted = selected.sortBy(_.findMatches(simEvents).size)
+  for ( log <- sorted ) {
+    val userEvents = log findMatches simEvents
+    println("user " + log.user + " matched " + userEvents.size + "/" + simEvents.size)
+  }
+
+  println("unused features in "+sim)
+  //val unusedFeatures = for (event <- simToUse if selected.filter(log => (log find (event :: Nil))).size==0) yield event
+  val unusedFeatures = simEvents.filter(event => selected.filter(_.matches(event)).size == 0)
+  println(unusedFeatures mkString "\n")
+
+  println("who used what in "+sim)
+  val whoUsedWhat = for ( event <- simEvents ) yield {
+    val used = selected.filter(_.matches(event)).map(_.user)
+    used -> event
+  }
+  println(whoUsedWhat mkString "\n")
+
 }
-
-println("unused features")
-//val unusedFeatures = for (event <- simToUse if selected.filter(log => (log find (event :: Nil))).size==0) yield event
-val unusedFeatures = simToUse.filter(event => selected.filter(_.matches(event)).size == 0)
-println(unusedFeatures mkString "\n")
-
-println("who used what")
-val whoUsedWhat = for ( event <- simToUse ) yield {
-  val used = selected.filter(_.matches(event)).map(_.user)
-  used -> event
-}
-println(whoUsedWhat mkString "\n")
 
 val allEvents = selected.flatMap(_.entries)
 
--- util/sim-event-processor/src/edu/colorado/phet/simeventprocessor/scala/phet.scala
+++ util/sim-event-processor/src/edu/colorado/phet/simeventprocessor/scala/phet.scala
@@ -3,6 +3,7 @@ package edu.colorado.phet.simeventprocessor.scala
 
 import org.jfree.data.xy.XYSeries
 import edu.colorado.phet.simeventprocessor.{JavaPredef, Processor}
+import edu.colorado.phet.simeventprocessor.scala.phet._
 
 /**
  * Functions and implicits to make the REPL easier to use
@@ -19,7 +20,7 @@ object phet {
   }
 
   //Use this method for printing user id's in numerical instead of alphabetical order
-  def numerical(value:String) = {
+  def numerical(value: String) = {
     try {
       value.toInt
     }
@@ -30,7 +31,8 @@ object phet {
 
   implicit def wrapLogSeq(i: Seq[EventLog]) = new LogSeqWrapper(i)
 
-  def timeSeries(log: EventLog, value: Int => Double): XYSeries = seqSeries("ID " + log.user, 0 to log.lastTime by 1000, value)
+  //Turning this number too high can cause it to take too long.  1000 was a good granularity, but took a bit too long for large data sets
+  def timeSeries(log: EventLog, value: Int => Double): XYSeries = seqSeries("ID " + log.user, 0 to log.lastTime by 10000, value)
 
   def seqSeries(name: String, time: Seq[Int], value: Int => Double): XYSeries =
     new XYSeries(name) {
@@ -39,8 +41,8 @@ object phet {
       }
     }
 
-  def xyplot(title: String, domainAxis: String, rangeAxis: String, xySeries: Seq[XYSeries]) {
-    JavaPredef.plot(title, domainAxis, rangeAxis, xySeries.toArray)
+  def xyplot(title: String="", domainAxis: String="", rangeAxis: String="", dataSets: Seq[XYSeries]) {
+    JavaPredef.plot(title, domainAxis, rangeAxis, dataSets.toArray)
   }
 
   def barchart(title: String, domainAxis: String, rangeAxis: String, xySeries: Seq[XYSeries]) {
@@ -48,8 +50,8 @@ object phet {
   }
 }
 
-class LogSeqWrapper(log: Seq[EventLog]) {
+class LogSeqWrapper(selectedLogs: Seq[EventLog]) {
   def print() {
-    println(log mkString "\n")
+    println(selectedLogs mkString "\n")
   }
 }
