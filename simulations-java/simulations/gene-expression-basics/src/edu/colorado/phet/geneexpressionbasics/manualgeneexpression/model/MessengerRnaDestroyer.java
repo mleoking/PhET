018efd508aa8dc7ec4d9e90388ccbc1d7fceb683
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-11-10 23:32:29 +0000

    Changed shape based on feedback.

--- simulations-java/simulations/gene-expression-basics/src/edu/colorado/phet/geneexpressionbasics/manualgeneexpression/model/MessengerRnaDestroyer.java
+++ simulations-java/simulations/gene-expression-basics/src/edu/colorado/phet/geneexpressionbasics/manualgeneexpression/model/MessengerRnaDestroyer.java
@@ -3,6 +3,8 @@ package edu.colorado.phet.geneexpressionbasics.manualgeneexpression.model;
 
 import java.awt.Color;
 import java.awt.Shape;
+import java.awt.geom.Area;
+import java.awt.geom.Ellipse2D;
 import java.awt.geom.Point2D;
 
 import edu.colorado.phet.common.phetcommon.view.util.DoubleGeneralPath;
@@ -16,7 +18,6 @@ import edu.colorado.phet.geneexpressionbasics.common.model.MobileBiomolecule;
 public class MessengerRnaDestroyer extends MobileBiomolecule {
 
     private static final double WIDTH = 250;   // In nanometers.
-    private static final double HEIGHT = 190;  // In nanometers.
 
     public MessengerRnaDestroyer( GeneExpressionModel model ) {
         this( model, new Point2D.Double( 0, 0 ) );
@@ -28,12 +29,14 @@ public class MessengerRnaDestroyer extends MobileBiomolecule {
     }
 
     private static Shape createShape() {
-        DoubleGeneralPath path = new DoubleGeneralPath();
-        // Start from top center.  This is a triangle.
-        path.moveTo( 0, HEIGHT * 0.5 );
-        path.lineTo( WIDTH * 0.5, -HEIGHT * 0.5 );
-        path.lineTo( -WIDTH * 0.5, -HEIGHT * 0.5 );
-        path.closePath();
-        return path.getGeneralPath();
+        Shape circle = new Ellipse2D.Double( -WIDTH / 2, -WIDTH / 2, WIDTH, WIDTH );
+        DoubleGeneralPath mouthPath = new DoubleGeneralPath( 0, 0 ) {{
+            lineTo( WIDTH, WIDTH * 0.8 );
+            lineTo( WIDTH, -WIDTH * 0.8 );
+            closePath();
+        }};
+        Area overallShape = new Area( circle );
+        overallShape.subtract( new Area( mouthPath.getGeneralPath() ) );
+        return overallShape;
     }
 }
