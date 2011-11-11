711a8b3e0522c9a21012e2e739cf8624cd4dd418
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-11-11 07:30:37 +0000

    lwjgl: generation of platform-specific native libs

---
+++ simulations-java/contrib/lwjgl/native/build_natives.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Reads in platform-specific libraries and creates the JARs that we need.
+# by Jonathan Olson
+
+for platform in linux macosx solaris windows
+do
+	jarName=native_${platform}.jar
+	
+	# get a list of the libraries that we need to pack in the JAR
+	cd $platform
+	libList=`ls -1 * | xargs`
+	cd ..
+	
+	# create a temporary directory and fill it with copies of those libraries
+	mkdir build
+	for lib in $libList
+	do
+		cp "$platform/$lib" build/
+	done
+	
+	# build the JAR from our temporary directory
+	jar -cfm "$jarName" MANIFEST.MF -C build .
+	
+	# delete the temporary directory
+	rm -Rf build
+	
+	# list files in the JAR for verification
+	echo "Files in ${jarName}:"
+	jar -tf "$jarName"
+done
