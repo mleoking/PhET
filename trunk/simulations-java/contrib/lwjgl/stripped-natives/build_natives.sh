640bfc5dcc5fdb0eb748f5d9872ac593759a0c15
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-11-11 10:49:15 +0000

    lwjgl: test project work

--- trunk/simulations-java/contrib/lwjgl/stripped-natives/build_natives.sh
+++ trunk/simulations-java/contrib/lwjgl/stripped-natives/build_natives.sh
@@ -7,15 +7,15 @@ do
 	jarName=native_${platform}.jar
 	
 	# get a list of the libraries that we need to pack in the JAR
-	cd $platform
+	cd ../native/$platform
 	libList=`ls -1 * | xargs`
-	cd ..
+	cd ../../stripped-natives
 	
 	# create a temporary directory and fill it with copies of those libraries
 	mkdir build
 	for lib in $libList
 	do
-		cp "$platform/$lib" build/
+		cp "../native/$platform/$lib" build/
 	done
 	
 	# build the JAR from our temporary directory
---
+++ trunk/simulations-java/simulations/test-lwjgl-project/src/edu/colorado/phet/testlwjglproject/TestLWJGLApplication.java
@@ -0,0 +1,29 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.testlwjglproject;
+
+import java.io.IOException;
+
+import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
+import edu.colorado.phet.common.phetcommon.application.PhetApplicationLauncher;
+import edu.colorado.phet.common.piccolophet.PiccoloPhetApplication;
+import edu.colorado.phet.testlwjglproject.lwjgl.StartupUtils;
+
+public class TestLWJGLApplication extends PiccoloPhetApplication {
+
+    public TestLWJGLApplication( PhetApplicationConfig config ) {
+        super( config );
+        addModule( new TestModule( TestProjectResources.getString( "sim1.module1" ) ) );
+//        addModule( new EmptyModule( TestProjectResources.getString( "sim1.module2" ) ) );
+    }
+
+    public static void main( String[] args ) {
+        try {
+            StartupUtils.setupLibraries();
+        }
+        catch ( IOException e ) {
+            e.printStackTrace();
+        }
+
+        new PhetApplicationLauncher().launchSim( new PhetApplicationConfig( args, "test-lwjgl-project", "sim1" ), TestLWJGLApplication.class );
+    }
+}
