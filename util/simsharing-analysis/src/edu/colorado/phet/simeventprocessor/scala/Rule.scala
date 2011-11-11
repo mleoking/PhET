a4b5560ac08180dabd627e8b2a442a6d84837c34
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-11-11 07:07:23 +0000

    minor refactoring, see #3147

--- util/simsharing-analysis/src/edu/colorado/phet/simeventprocessor/scala/Rule.scala
+++ util/simsharing-analysis/src/edu/colorado/phet/simeventprocessor/scala/Rule.scala
@@ -8,13 +8,9 @@ package edu.colorado.phet.simeventprocessor.scala
 
 case class Rule(actor: String, event: String, params: Map[String, String]) extends Match {
 
-  def matches(entry: Entry) = entry.matches(actor, event, params)
+  def apply(entry: Entry) = entry.matches(actor, event, params)
 
-  override def toString = actor + "\t" + event + "\t" + {
-    ( for ( elm <- params ) yield {
-      elm._1 + " = " + elm._2
-    } ).mkString("\t")
-  }
+  override def toString() = actor + "\t" + event + "\t" + ( for ( elm <- params ) yield {elm._1 + " = " + elm._2} ).mkString("\t")
 }
 
 object Rule {
@@ -22,16 +18,9 @@ object Rule {
 }
 
 case class ActorRule(actor: String, params: Map[String, String]) extends Match {
-  def matches(entry: Entry) = entry.matches(actor, params)
+  def apply(entry: Entry) = entry.matches(actor, params)
 
-  override def toString = actor + "\t" + "<any>" + "\t" + {
-    ( for ( elm <- params ) yield {
-      elm._1 + " = " + elm._2
-    } ).mkString("\t")
-  }
+  override def toString() = actor + "\t" + "<any>" + "\t" + ( for ( elm <- params ) yield {elm._1 + " = " + elm._2} ).mkString("\t")
 }
 
-
-trait Match {
-  def matches(entry: Entry): Boolean
-}
+trait Match extends ( Entry => Boolean )
