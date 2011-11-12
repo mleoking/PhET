a7dcd00d1a6c846f37140ae3b5f2a9f29d08a72c
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-11-12 04:39:56 +0000

    fixed end time for date range

--- util/simsharing-analysis/src/edu/colorado/phet/simeventprocessor/scala/scripts/VisualizeLogTimes.scala
+++ util/simsharing-analysis/src/edu/colorado/phet/simeventprocessor/scala/scripts/VisualizeLogTimes.scala
@@ -16,11 +16,11 @@ object VisualizeLogTimes extends App {
   //create an xy data set that shows the number of logs as a function time within each time window
 
   val firstLog = logs.minBy(_.epoch)
-  val lastLog = logs.maxBy(_.epoch)
+  val lastLog = logs.maxBy(_.endEpoch)
 
   println("first log:\n" + firstLog.date + "\nlast log:\n" + lastLog.date)
 
-  val range = firstLog.epoch to lastLog.epoch by 10000
+  val range = firstLog.epoch to lastLog.endEpoch by 10000
   val countCO = for ( time <- range; count = logs.count(log => log.running(time) && log.study == "colorado"); if count > 0 ) yield {time -> count}
   val countUT = for ( time <- range; count = logs.count(log => log.running(time) && log.study == "utah"); if count > 0 ) yield {time -> count}
 
