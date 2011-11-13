3021c08a68042fffb413a39ee25d6d7e8dc70b5b
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-11-13 04:07:55 +0000

    Group tab time together in the same plot

--- util/simsharing-analysis/src/edu/colorado/phet/simeventprocessor/scala/scripts/HowMuchTimeSpentInTabs.scala
+++ util/simsharing-analysis/src/edu/colorado/phet/simeventprocessor/scala/scripts/HowMuchTimeSpentInTabs.scala
@@ -1,7 +1,7 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.simeventprocessor.scala.scripts
 
-import edu.colorado.phet.simeventprocessor.scala.{studySessionsNov2011, phet}
+import edu.colorado.phet.simeventprocessor.scala.{Session, studySessionsNov2011, phet}
 import phet._
 
 /**
@@ -15,15 +15,26 @@ object HowMuchTimeSpentInTabs extends App {
                     "Molecule Polarity" -> List("Two Atoms", "Three Atoms", "Real Molecules"),
                     "Molecule Shapes" -> Nil)
 
-  for ( s <- studySessionsNov2011.all ) {
-    val logs = all.filter(s)
-    val sims = logs.map(_.simName).distinct
-    for ( sim <- sims ) {
-      val tabs = simTabs(sim)
-      if ( tabs.length > 1 ) {
-        val timeSpentInEachTab = tabs.map(tab => tab -> logs.map(_.selectTab(tabs, tab).elapsedTime / 1000 / 60)).toMap
-        barChart("Class session: " + s + " using " + sim, "time (minutes)", timeSpentInEachTab)
-      }
-    }
+  val sims = all.map(_.simName).distinct
+
+  //Group together in the same plot
+  for ( sim <- sims ) {
+    val tabs = simTabs(sim)
+    def summary(s: Session) = s.study.substring(0, 2).toUpperCase + " " + s.day + " " + s.start
+    val result = studySessionsNov2011.all.map(s => tabs.map(tab => (tab, summary(s)) -> all.filter(s).map(_.selectTab(tabs, tab).elapsedTime / 1000 / 60)).toMap).flatten.toMap
+    barChart("Tab usage for " + sim, "time (minutes)", result)
   }
+
+  //Plot each in a separate plot
+  //  for ( s <- studySessionsNov2011.all ) {
+  //    val logs = all.filter(s)
+  //    val sims = logs.map(_.simName).distinct
+  //    for ( sim <- sims ) {
+  //      val tabs = simTabs(sim)
+  //      if ( tabs.length > 1 ) {
+  //        val timeSpentInEachTab = tabs.map(tab => (tab,"column") -> logs.map(_.selectTab(tabs, tab).elapsedTime / 1000 / 60)).toMap
+  //        barChart("Class session: " + s + " using " + sim, "time (minutes)", timeSpentInEachTab)
+  //      }
+  //    }
+  //  }
 }
