3cf4c92b979182beb481447a8109d7d59b59d90d
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-11-16 23:32:59 +0000

    Made it so the dye button can't be pressed repeatedly

--- trunk/simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/flow/view/GridInjectorNode.java
+++ trunk/simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/flow/view/GridInjectorNode.java
@@ -1,36 +1,33 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.fluidpressureandflow.flow.view;
 
+import java.awt.Cursor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.geom.Point2D;
+
+import javax.swing.Timer;
 
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
-import edu.colorado.phet.common.piccolophet.nodes.InjectorNode;
+import edu.colorado.phet.common.piccolophet.nodes.NonInteractiveInjectorNode;
 import edu.colorado.phet.fluidpressureandflow.flow.model.Pipe;
+import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
+import edu.umd.cs.piccolo.event.PInputEvent;
 
 /**
  * Injects a grid of particles.
  *
  * @author Sam Reid
  */
-public class GridInjectorNode extends InjectorNode {
+public class GridInjectorNode extends NonInteractiveInjectorNode {
+
     public GridInjectorNode( final ModelViewTransform mvt, double rotationAngle, final SimpleObserver squirt, final Pipe pipe ) {
         super( rotationAngle, new SimpleObserver() {
             public void update() {
                 squirt.update();
             }
         } );
-
-//        observers.addListener( new VoidFunction1<Void>() {
-//            public void apply( Void myVoid ) {
-//                setEnabled( false );
-//                new javax.swing.Timer( 5000, new ActionListener() {
-//                    public void actionPerformed( ActionEvent e ) {
-//                        setEnabled( true );
-//                    }
-//                } ) {{setRepeats( false );}}.start();
-//            }
-//        } );
 
         final SimpleObserver updateLocation = new SimpleObserver() {
             public void update() {
@@ -42,5 +39,63 @@ public class GridInjectorNode extends InjectorNode {
         };
         pipe.addShapeChangeListener( updateLocation );
         updateLocation.update();
+
+        //Our own mouse listener and cursor handler is necessary so the user can't press the button repeatedly
+        buttonImageNode.addInputEventListener( new PBasicInputEventHandler() {
+            public boolean pushed = false;
+            public boolean entered = false;
+            public boolean disabled = false;
+
+            @Override public void mouseEntered( PInputEvent event ) {
+                if ( !disabled ) {
+                    checkPush( event );
+                }
+                entered = true;
+            }
+
+            private void checkPush( PInputEvent event ) {
+                if ( !pushed ) {
+                    event.getComponent().pushCursor( Cursor.getPredefinedCursor( Cursor.HAND_CURSOR ) );
+                    pushed = true;
+                }
+            }
+
+            @Override public void mouseExited( PInputEvent event ) {
+                checkPop( event );
+                entered = false;
+            }
+
+            private void checkPop( PInputEvent event ) {
+                if ( pushed ) {
+                    event.getComponent().popCursor();
+                    pushed = false;
+                }
+            }
+
+            @Override public void mousePressed( PInputEvent event ) {
+                buttonImageNode.setImage( pressedButtonImage );
+            }
+
+            @Override public void mouseReleased( final PInputEvent event ) {
+                if ( !disabled ) {
+
+                    inject.update();
+                    checkPop( event );
+
+                    disabled = true;
+
+                    //After 5 sec, re-enable the button and update the cursor
+                    new Timer( 5000, new ActionListener() {
+                        public void actionPerformed( ActionEvent e ) {
+                            buttonImageNode.setImage( unpressedButtonImage );
+                            if ( entered ) {
+                                checkPush( event );
+                            }
+                            disabled = false;
+                        }
+                    } ) {{setRepeats( false );}}.start();
+                }
+            }
+        } );
     }
 }
