ed8c02409862295b6de7b27f9065418ea6e8884d
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-11-17 17:58:54 +0000

    added cursor hand

--- simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/intro/intro/view/TogglePiece.java
+++ simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/intro/intro/view/TogglePiece.java
@@ -7,6 +7,7 @@ import java.awt.Shape;
 import java.awt.Stroke;
 
 import edu.colorado.phet.common.phetcommon.util.function.VoidFunction0;
+import edu.colorado.phet.common.piccolophet.event.CursorHandler;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
 import edu.umd.cs.piccolo.event.PInputEvent;
@@ -17,7 +18,7 @@ import edu.umd.cs.piccolo.event.PInputEvent;
 public class TogglePiece extends PhetPPath {
     public TogglePiece( Shape shape, Paint fill, Stroke stroke, Paint strokePaint, final VoidFunction0 toggleOff, final VoidFunction0 toggleOn ) {
         super( shape, fill, stroke, strokePaint );
-
+        addInputEventListener( new CursorHandler() );
         addInputEventListener( new PBasicInputEventHandler() {
             @Override public void mousePressed( PInputEvent event ) {
                 if ( getPaint() == Color.white ) {
