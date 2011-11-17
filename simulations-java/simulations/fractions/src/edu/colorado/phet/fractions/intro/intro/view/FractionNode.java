949fde588f6cfd760e2060f6ba430fb48d3382e8
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-11-17 04:57:02 +0000

    Hide zero values in mixed fractions, and don't show divided by 1 in reduced fractions

--- simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/intro/intro/view/FractionNode.java
+++ simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/intro/intro/view/FractionNode.java
@@ -6,6 +6,7 @@ import java.awt.Color;
 import java.awt.geom.Line2D;
 
 import edu.colorado.phet.common.phetcommon.model.property.Property;
+import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.colorado.phet.common.piccolophet.nodes.kit.ZeroOffsetNode;
 import edu.umd.cs.piccolo.PNode;
@@ -20,11 +21,28 @@ public class FractionNode extends PNode {
     public FractionNode( final Property<Integer> numerator, final Property<Integer> denominator ) {
         final PhetPPath line = new PhetPPath( new Line2D.Double( 0, 0, 150, 0 ), new BasicStroke( 12 ), Color.black );
         addChild( line );
-        addChild( new ZeroOffsetNode( new FractionNumberNode( numerator ) ) {{
+        final ZeroOffsetNode num = new ZeroOffsetNode( new FractionNumberNode( numerator ) ) {{
             setOffset( line.getFullBounds().getCenterX() - getFullBounds().getWidth() / 2, line.getFullBounds().getY() - getFullBounds().getHeight() );
-        }} );
-        addChild( new ZeroOffsetNode( new FractionNumberNode( denominator ) ) {{
+        }};
+        addChild( num );
+        final ZeroOffsetNode den = new ZeroOffsetNode( new FractionNumberNode( denominator ) ) {{
             setOffset( line.getFullBounds().getCenterX() - getFullBounds().getWidth() / 2, line.getFullBounds().getY() );
-        }} );
+        }};
+        addChild( den );
+
+        final ZeroOffsetNode numberNode = new ZeroOffsetNode( new FractionNumberNode( numerator ) ) {{
+            setOffset( line.getFullBounds().getCenterX() - getFullBounds().getWidth() / 2, line.getFullBounds().getCenterY() - getFullBounds().getHeight() / 2 );
+        }};
+        addChild( numberNode );
+
+        denominator.addObserver( new VoidFunction1<Integer>() {
+            public void apply( Integer integer ) {
+                line.setVisible( integer != 1 );
+                den.setVisible( integer != 1 );
+                num.setVisible( integer != 1 );
+
+                numberNode.setVisible( integer == 1 );
+            }
+        } );
     }
 }
