7eaa6df9ac8d6aefd728bbf2900b0f4c57ac92bb
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-11-24 10:24:12 +0000

    molecule-shapes: attractor progress

--- trunk/simulations-java/simulations/molecule-shapes/src/edu/colorado/phet/moleculeshapes/model/RealMoleculeModel.java
+++ trunk/simulations-java/simulations/molecule-shapes/src/edu/colorado/phet/moleculeshapes/model/RealMoleculeModel.java
@@ -1,14 +1,17 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.moleculeshapes.model;
 
-import Jama.Matrix;
-
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import edu.colorado.phet.chemistry.model.Element;
 import edu.colorado.phet.common.phetcommon.math.ImmutableVector3D;
 import edu.colorado.phet.common.phetcommon.math.Permutation;
 import edu.colorado.phet.moleculeshapes.model.AttractorModel.ResultMapping;
+
+import static edu.colorado.phet.common.phetcommon.util.FunctionalUtils.rangeInclusive;
 
 /**
  * Represents a physically malleable version of a real molecule, with lone pairs if necessary. Different from the
@@ -21,8 +24,14 @@ public class RealMoleculeModel extends MoleculeModel {
     // stores the ideal position vectors for both the bonds and lone pairs
     private List<ImmutableVector3D> idealPositionVectors;
 
+    private Set<Element> elementsUsed = new HashSet<Element>();
+
+    private List<Permutation> cachedPermutations;
+
     public RealMoleculeModel( final RealMolecule realMolecule ) {
         this.realMolecule = realMolecule;
+
+        setSortingEnabled( false );
 
         final int numLonePairs = realMolecule.getLonePairCount();
         final int numBonds = realMolecule.getBonds().size();
@@ -35,6 +44,7 @@ public class RealMoleculeModel extends MoleculeModel {
             ImmutableVector3D normalizedPosition = atom.position.get().normalized();
             idealPositionVectors.add( normalizedPosition );
             addPair( new RealPairGroup( normalizedPosition.times( PairGroup.BONDED_PAIR_DISTANCE ), bond.order, atom.getElement() ) );
+            elementsUsed.add( atom.getElement() );
         }
 
         // all of the ideal vectors (including for lone pairs)
@@ -49,12 +59,14 @@ public class RealMoleculeModel extends MoleculeModel {
 
         ResultMapping mapping = AttractorModel.findClosestMatchingConfiguration( this, idealModelBondVectors, Permutation.permutations( idealModelBondVectors.size() ) );
 
-        Matrix rotation = mapping.rotation;
-
         // add in lone pairs in their correct "initial" positions
         for ( int i = 0; i < numLonePairs; i++ ) {
-            addPair( new PairGroup( mapping.rotateVector( idealModelVectors.get( i ) ).times( PairGroup.LONE_PAIR_DISTANCE ), 0, false ) );
+            ImmutableVector3D normalizedPosition = mapping.rotateVector( idealModelVectors.get( i ) );
+            idealPositionVectors.add( normalizedPosition );
+            addPair( new PairGroup( normalizedPosition.times( PairGroup.LONE_PAIR_DISTANCE ), 0, false ) );
         }
+
+        cachedPermutations = computePermutations();
     }
 
     @Override public List<ImmutableVector3D> getIdealPositionVectors() {
@@ -62,8 +74,42 @@ public class RealMoleculeModel extends MoleculeModel {
     }
 
     @Override public List<Permutation> getAllowablePositionPermutations() {
-        return new ArrayList<Permutation>() {{
-            add( Permutation.identity( idealPositionVectors.size() ) );
-        }};
+        return cachedPermutations;
+    }
+
+    private List<Permutation> computePermutations() {
+        List<Permutation> permutations = new ArrayList<Permutation>();
+        permutations.add( Permutation.identity( getGroups().size() ) );
+
+        // permute away the lone pairs
+        if ( getLonePairs().size() > 1 ) {
+            List<Permutation> result = new ArrayList<Permutation>();
+            for ( Permutation permutation : permutations ) {
+                // lone pairs are stored at the back, so adjust accordingly
+                result.addAll( permutation.withIndicesPermuted( rangeInclusive( getBondedGroups().size(), getGroups().size() - 1 ) ) );
+            }
+            permutations = result;
+        }
+
+        // for each element, add permutations if necessary
+        for ( Element element : elementsUsed ) {
+            List<Integer> indicesWithElement = new ArrayList<Integer>();
+            for ( int i = 0; i < getBondedGroups().size(); i++ ) {
+                if ( ( (RealPairGroup) ( getGroups().get( i ) ) ).getElement() == element ) {
+                    indicesWithElement.add( i );
+                }
+            }
+
+            // permutations only necessary if we have more than two of that type
+            if ( indicesWithElement.size() > 1 ) {
+                List<Permutation> result = new ArrayList<Permutation>();
+                for ( Permutation permutation : permutations ) {
+                    result.addAll( permutation.withIndicesPermuted( indicesWithElement ) );
+                }
+                permutations = result;
+            }
+        }
+
+        return permutations;
     }
 }
