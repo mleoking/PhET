a8c43019667504fb3486184eda39e0e3c6fcff44
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-12-01 22:10:43 +0000

    12/1/2011 Created integerproperty package by copying doubleproperty package

---
+++ trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/model/property/integerproperty/LessThan.java
@@ -0,0 +1,21 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.common.phetcommon.model.property.integerproperty;
+
+import edu.colorado.phet.common.phetcommon.model.property.CompositeBooleanProperty;
+import edu.colorado.phet.common.phetcommon.model.property.ObservableProperty;
+import edu.colorado.phet.common.phetcommon.util.function.Function0;
+
+/**
+ * ObservableProperty that is true if the specified ObservableProperty is greater than another specified value.
+ *
+ * @author Sam Reid
+ */
+public class LessThan extends CompositeBooleanProperty {
+    public LessThan( final ObservableProperty<Integer> a, final int b ) {
+        super( new Function0<Boolean>() {
+            public Boolean apply() {
+                return a.get() < b;
+            }
+        }, a );
+    }
+}
