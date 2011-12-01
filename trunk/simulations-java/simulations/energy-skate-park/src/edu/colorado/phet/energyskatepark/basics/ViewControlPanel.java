ffd01a03f9a951d73f1c96447d5ad6bf67e4f00e
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-12-01 22:01:48 +0000

    Integrated new version of controls for bar chart, pie chart, and grid visibility that allow the user to click the images.  These work at this point, but layout needs improvement.

--- trunk/simulations-java/simulations/energy-skate-park/src/edu/colorado/phet/energyskatepark/basics/ViewControlPanel.java
+++ trunk/simulations-java/simulations/energy-skate-park/src/edu/colorado/phet/energyskatepark/basics/ViewControlPanel.java
@@ -1,12 +1,6 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.energyskatepark.basics;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-
-import javax.swing.JCheckBox;
 import javax.swing.JDialog;
 
 import edu.colorado.phet.common.phetcommon.util.function.VoidFunction0;
@@ -17,8 +11,7 @@ import edu.colorado.phet.energyskatepark.EnergySkateParkResources;
 import edu.colorado.phet.energyskatepark.view.EnergySkateParkLookAndFeel;
 import edu.colorado.phet.energyskatepark.view.EnergySkateParkSimulationPanel;
 import edu.colorado.phet.energyskatepark.view.swing.PropertyCheckBoxNode;
-import edu.umd.cs.piccolo.nodes.PImage;
-import edu.umd.cs.piccolox.pswing.PSwing;
+import edu.colorado.phet.energyskatepark.view.swing.PropertyTogglingImageNode;
 
 /**
  * Misc controls for visibility of things in the view (charts, grid).
@@ -31,53 +24,20 @@ public class ViewControlPanel extends ControlPanelNode {
         super( new VBox( 10, VBox.LEFT_ALIGNED,
                          new HBox(
                                  //Checkbox to show/hide bar chart
-                                 new PSwing( new JCheckBox( EnergySkateParkResources.getString( "plots.bar-graph" ), module.barChartVisible.get() ) {{
-                                     setFont( EnergySkateParkBasicsModule.CONTROL_FONT );
-                                     addActionListener( new ActionListener() {
-                                         public void actionPerformed( ActionEvent e ) {
-                                             module.barChartVisible.set( isSelected() );
-                                         }
-                                     } );
-                                     module.addResetListener( new VoidFunction0() {
-                                         public void apply() {
-                                             setSelected( module.pieChartVisible.get() );
-                                         }
-                                     } );
-                                     // set the check box state when the dialog is closed via its window dressing
-                                     barChartDialog.addWindowListener( new WindowAdapter() {
-                                         // called when the close button in the dialog's window dressing is clicked
-                                         public void windowClosing( WindowEvent e ) {
-                                             setSelected( module.pieChartVisible.get() );
-                                         }
-
-                                         // called by JDialog.dispose
-                                         public void windowClosed( WindowEvent e ) {
-                                             setSelected( module.pieChartVisible.get() );
-                                         }
-                                     } );
-                                 }} ),
-                                 new PImage( EnergySkateParkResources.getImage( "icons/bar_icon.png" ) ) ),
+                                 new PropertyCheckBoxNode( EnergySkateParkResources.getString( "plots.bar-graph" ), module.barChartVisible ),
+                                 new PropertyTogglingImageNode( EnergySkateParkResources.getImage( "icons/bar_icon.png" ), module.barChartVisible )
+                         ),
 
                          new HBox(
                                  //Checkbox to show/hide the pie chart
-                                 new PSwing( new JCheckBox( EnergySkateParkResources.getString( "pieChart" ), module.pieChartVisible.get() ) {{
-                                     setFont( EnergySkateParkBasicsModule.CONTROL_FONT );
-                                     addActionListener( new ActionListener() {
-                                         public void actionPerformed( ActionEvent e ) {
-                                             module.pieChartVisible.set( isSelected() );
-                                         }
-                                     } );
-                                     module.addResetListener( new VoidFunction0() {
-                                         public void apply() {
-                                             setSelected( module.pieChartVisible.get() );
-                                         }
-                                     } );
-                                 }} ),
-                                 new PImage( EnergySkateParkResources.getImage( "icons/pie_icon.png" ) ) ),
+                                 new PropertyCheckBoxNode( EnergySkateParkResources.getString( "pieChart" ), module.pieChartVisible ),
+                                 new PropertyTogglingImageNode( EnergySkateParkResources.getImage( "icons/pie_icon.png" ), module.pieChartVisible )
+                         ),
 
                          new HBox(
                                  //Checkbox to show/hide the grid lines
-                                 new PropertyCheckBoxNode( EnergySkateParkResources.getString( "controls.show-grid" ), module.gridVisible )
+                                 new PropertyCheckBoxNode( EnergySkateParkResources.getString( "controls.show-grid" ), module.gridVisible ),
+                                 new PropertyTogglingImageNode( EnergySkateParkResources.getImage( "icons/grid_icon.png" ), module.gridVisible )
                          )
 
         ), EnergySkateParkLookAndFeel.backgroundColor );
