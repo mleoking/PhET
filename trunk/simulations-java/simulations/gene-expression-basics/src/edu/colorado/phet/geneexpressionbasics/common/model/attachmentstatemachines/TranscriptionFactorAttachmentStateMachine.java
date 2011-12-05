715322bc5134fdb4c081a40ee8ccaeab751c2d71
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-12-05 20:15:54 +0000

    Fixed some issues found during testing of the generic attachment state machine.

---
+++ trunk/simulations-java/simulations/gene-expression-basics/src/edu/colorado/phet/geneexpressionbasics/common/model/attachmentstatemachines/TranscriptionFactorAttachmentStateMachine.java
@@ -0,0 +1,8 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.geneexpressionbasics.common.model.attachmentstatemachines;
+
+/**
+ * @author John Blanco
+ */
+public class TranscriptionFactorAttachmentStateMachine {
+}
