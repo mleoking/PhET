ed4f5c243d01d259a2b9a01f8f8004b221284562
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-12-05 18:36:46 +0000

    fix doc typo

--- trunk/simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/controls/CustomSolutionControls.java
+++ trunk/simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/controls/CustomSolutionControls.java
@@ -9,22 +9,30 @@ import java.awt.event.ActionListener;
 import java.text.DecimalFormat;
 import java.util.Hashtable;
 
-import javax.swing.*;
+import javax.swing.Box;
+import javax.swing.ButtonGroup;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
 import edu.colorado.phet.acidbasesolutions.constants.ABSConstants;
 import edu.colorado.phet.acidbasesolutions.constants.ABSStrings;
-import edu.colorado.phet.acidbasesolutions.model.*;
+import edu.colorado.phet.acidbasesolutions.model.ABSModel;
 import edu.colorado.phet.acidbasesolutions.model.ABSModel.ModelChangeListener;
+import edu.colorado.phet.acidbasesolutions.model.AcidSolution;
+import edu.colorado.phet.acidbasesolutions.model.AqueousSolution;
 import edu.colorado.phet.acidbasesolutions.model.AqueousSolution.AqueousSolutionChangeListener;
 import edu.colorado.phet.acidbasesolutions.model.AqueousSolution.ICustomSolution;
 import edu.colorado.phet.acidbasesolutions.model.StrongAcidSolution.CustomStrongAcidSolution;
 import edu.colorado.phet.acidbasesolutions.model.StrongBaseSolution.CustomStrongBaseSolution;
+import edu.colorado.phet.acidbasesolutions.model.WeakAcidSolution;
 import edu.colorado.phet.acidbasesolutions.model.WeakAcidSolution.CustomWeakAcidSolution;
+import edu.colorado.phet.acidbasesolutions.model.WeakBaseSolution;
 import edu.colorado.phet.acidbasesolutions.model.WeakBaseSolution.CustomWeakBaseSolution;
-import edu.colorado.phet.common.phetcommon.view.PhetTitledPanel;
 import edu.colorado.phet.common.phetcommon.view.ColoredSeparator.BlackSeparator;
+import edu.colorado.phet.common.phetcommon.view.PhetTitledPanel;
 import edu.colorado.phet.common.phetcommon.view.controls.valuecontrol.AbstractValueControl;
 import edu.colorado.phet.common.phetcommon.view.controls.valuecontrol.ILayoutStrategy;
 import edu.colorado.phet.common.phetcommon.view.controls.valuecontrol.LogarithmicValueControl;
@@ -33,14 +41,14 @@ import edu.colorado.phet.common.phetcommon.view.util.EasyGridBagLayout;
 /**
  * Controls used to set the properties of a custom solution.
  * Mutable properties include the type of solute (acid or base),
- * the concentration of the solution in solution, and the 
+ * the concentration of the solution in solution, and the
  * strength (weak or strong) of the solute.  For weak solutes,
  * the strength can be specified.
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
 public class CustomSolutionControls extends PhetTitledPanel {
-    
+
     private final ABSModel model;
     private AqueousSolution solution;
     private final TypePanel typePanel;
@@ -48,21 +56,21 @@ public class CustomSolutionControls extends PhetTitledPanel {
     private final StrengthPanel strengthPanel;
     private final AqueousSolutionChangeListener solutionChangeListener;
     private boolean isSyncingWithModel;
-    
+
     public CustomSolutionControls( ABSModel model ) {
         super( ABSStrings.SOLUTION );
-        
+
         // model
         {
             this.model = model;
             isSyncingWithModel = false;
-            
+
             this.model.addModelChangeListener( new ModelChangeListener() {
                 public void solutionChanged() {
                     updateControls();
                 }
             } );
-            
+
             this.solution = model.getSolution();
             solutionChangeListener = new AqueousSolutionChangeListener() {
 
@@ -76,7 +84,7 @@ public class CustomSolutionControls extends PhetTitledPanel {
             };
             solution.addAqueousSolutionChangeListener( solutionChangeListener );
         }
-        
+
         // subpanels
         {
             ChangeListener changeListener = new ChangeListener() {
@@ -88,7 +96,7 @@ public class CustomSolutionControls extends PhetTitledPanel {
             concentrationPanel = new ConcentrationPanel( changeListener );
             strengthPanel = new StrengthPanel( changeListener );
         }
-        
+
         // main panel layout
         {
             int row = 0;
@@ -102,39 +110,39 @@ public class CustomSolutionControls extends PhetTitledPanel {
             layout.addFilledComponent( new BlackSeparator(), row++, column, GridBagConstraints.HORIZONTAL );
             layout.addFilledComponent( strengthPanel, row++, column, GridBagConstraints.HORIZONTAL );
         }
-        
+
         // default state
         {
             updateControls();
         }
     }
-    
+
     /*
-     * Updates this control to match the model.
-     */
+    * Updates this control to match the model.
+    */
     private void updateControls() {
-        
+
         isSyncingWithModel = true;
-        
+
         // move the solution listener
         solution.removeAqueousSolutionChangeListener( solutionChangeListener );
         solution = model.getSolution();
         solution.addAqueousSolutionChangeListener( solutionChangeListener );
-        
+
         // adjust control settings
         typePanel.setAcidSelected( solution instanceof AcidSolution );
         concentrationPanel.setConcentration( model.getSolution().getConcentration() );
         strengthPanel.setWeakSelected( ( solution instanceof WeakAcidSolution ) || ( solution instanceof WeakBaseSolution ) );
         strengthPanel.setStrength( model.getSolution().getStrength() );
-        
+
         isSyncingWithModel = false;
     }
-    
+
     /*
-     * Updates the model to match this control.
-     */
+    * Updates the model to match this control.
+    */
     private void updateModel() {
-        
+
         if ( !isSyncingWithModel ) {
 
             AqueousSolution currentSolution = model.getSolution();
@@ -193,17 +201,17 @@ public class CustomSolutionControls extends PhetTitledPanel {
             }
         }
     }
-    
+
     /*
-     * A subpanel of this control that notifies one ChangeListener
-     * when the value mangaged by the panel is changed.
-     * This is a convenient way to prevent subpanels from having to know about the model,
-     * and keep all knowledge of the model in the main panel.
-     */
+    * A subpanel of this control that notifies one ChangeListener
+    * when the value managed by the panel is changed.
+    * This is a convenient way to prevent subpanels from having to know about the model,
+    * and keep all knowledge of the model in the main panel.
+    */
     private static class ChangeablePanel extends JPanel {
-        
+
         private final ChangeListener listener;
-        
+
         public ChangeablePanel( ChangeListener listener ) {
             this.listener = listener;
         }
@@ -212,36 +220,36 @@ public class CustomSolutionControls extends PhetTitledPanel {
             listener.stateChanged( new ChangeEvent( this ) );
         }
     }
-    
+
     /*
-     * Panel for setting the solute type.
-     */
+    * Panel for setting the solute type.
+    */
     private static class TypePanel extends ChangeablePanel {
-        
+
         private final JRadioButton acidRadioButton, baseRadioButton;
-        
-        public TypePanel( ChangeListener changeListener ) { 
+
+        public TypePanel( ChangeListener changeListener ) {
             super( changeListener );
-            
+
             // radio buttons
             {
                 ButtonGroup group = new ButtonGroup();
-                
+
                 ActionListener actionListener = new ActionListener() {
                     public void actionPerformed( ActionEvent e ) {
                         fireStateChanged();
                     }
                 };
-                
+
                 acidRadioButton = new JRadioButton( ABSStrings.ACID );
                 group.add( acidRadioButton );
                 acidRadioButton.addActionListener( actionListener );
-                
+
                 baseRadioButton = new JRadioButton( ABSStrings.BASE );
                 group.add( baseRadioButton );
                 baseRadioButton.addActionListener( actionListener );
             }
-            
+
             // layout
             {
                 EasyGridBagLayout layout = new EasyGridBagLayout( this );
@@ -252,23 +260,23 @@ public class CustomSolutionControls extends PhetTitledPanel {
                 layout.addComponent( baseRadioButton, row, column++ );
             }
         }
-        
+
         public void setAcidSelected( boolean selected ) {
             acidRadioButton.setSelected( selected );
         }
-        
+
         public boolean isAcidSelected() {
             return acidRadioButton.isSelected();
         }
     }
-    
+
     /* 
-     * Panel for setting the concentration.
-     */
+    * Panel for setting the concentration.
+    */
     private static class ConcentrationPanel extends ChangeablePanel {
-        
+
         private final LogarithmicValueControl concentrationControl;
-        
+
         public ConcentrationPanel( ChangeListener changeListener ) {
             super( changeListener );
 
@@ -288,7 +296,7 @@ public class CustomSolutionControls extends PhetTitledPanel {
                     }
                 } );
             }
-            
+
             // labels on the slider
             {
                 // labels at each 10x interval
@@ -296,22 +304,22 @@ public class CustomSolutionControls extends PhetTitledPanel {
                 DecimalFormat format = new DecimalFormat( "#.###" );
                 int numberOfLabels = 0;
                 for ( double c = concentrationControl.getMinimum(); c <= concentrationControl.getMaximum(); c *= 10 ) {
-                    concentrationLabelTable.put( new Double( c ), new JLabel( format.format( c )) );
+                    concentrationLabelTable.put( new Double( c ), new JLabel( format.format( c ) ) );
                     numberOfLabels++;
                 }
                 concentrationControl.setTickLabels( concentrationLabelTable );
-                
+
                 /*
-                 * Setting tick marks for a LogarithmicValueControl is unfortunately a bit of a hack.
-                 * The underlying JSlider is linear and has an integer range, and we need to set the
-                 * tick spacing based on this range, not our logarithmic "model" range.
-                 * Here we assume that the ticks are evenly spaced, and compute the tick spacing
-                 * based on the integer range of the underlying JSlider.
-                 */
+                * Setting tick marks for a LogarithmicValueControl is unfortunately a bit of a hack.
+                * The underlying JSlider is linear and has an integer range, and we need to set the
+                * tick spacing based on this range, not our logarithmic "model" range.
+                * Here we assume that the ticks are evenly spaced, and compute the tick spacing
+                * based on the integer range of the underlying JSlider.
+                */
                 int tickSpacing = ( concentrationControl.getSlider().getMaximum() - concentrationControl.getSlider().getMinimum() ) / ( numberOfLabels - 1 );
                 concentrationControl.getSlider().setMajorTickSpacing( tickSpacing );
             }
-            
+
             // layout
             {
                 EasyGridBagLayout layout = new EasyGridBagLayout( this );
@@ -322,51 +330,51 @@ public class CustomSolutionControls extends PhetTitledPanel {
                 layout.addComponent( concentrationControl, row, column );
             }
         }
-        
+
         public void setConcentration( double concentration ) {
             concentrationControl.setValue( concentration );
         }
-        
+
         public double getConcentration() {
             return concentrationControl.getValue();
         }
     }
-    
+
     /*
-     * Panel for setting the strength.
-     * For strong solutes, the strength is fixed.
-     * For weak solutes, the strength can be adjusted via a slider.
-     */
+    * Panel for setting the strength.
+    * For strong solutes, the strength is fixed.
+    * For weak solutes, the strength can be adjusted via a slider.
+    */
     private static class StrengthPanel extends ChangeablePanel {
-        
+
         private final JRadioButton weakRadioButton, strongRadioButton;
         private final LogarithmicValueControl weakStrengthControl;
-        
+
         public StrengthPanel( ChangeListener changeListener ) {
             super( changeListener );
-            
+
             // dynamic strength label
             JLabel strengthLabel = new JLabel( ABSStrings.STRENGTH );
-            
+
             // radio buttons
             {
                 ButtonGroup group = new ButtonGroup();
-                
+
                 ActionListener actionListener = new ActionListener() {
                     public void actionPerformed( ActionEvent e ) {
                         fireStateChanged();
                     }
                 };
-                
+
                 weakRadioButton = new JRadioButton( ABSStrings.WEAK );
                 group.add( weakRadioButton );
                 weakRadioButton.addActionListener( actionListener );
-                
+
                 strongRadioButton = new JRadioButton( ABSStrings.STRONG );
                 group.add( strongRadioButton );
                 strongRadioButton.addActionListener( actionListener );
             }
-            
+
             // strength control
             {
                 double min = ABSConstants.WEAK_STRENGTH_RANGE.getMin();
@@ -387,13 +395,13 @@ public class CustomSolutionControls extends PhetTitledPanel {
                     }
                 } );
             }
-            
+
             // layout with inner panel
             {
                 JPanel radioButtonsPanel = new JPanel();
                 radioButtonsPanel.add( weakRadioButton );
                 radioButtonsPanel.add( strongRadioButton );
-                
+
                 EasyGridBagLayout layout = new EasyGridBagLayout( this );
                 layout.setInsets( new Insets( 0, 0, 0, 0 ) );
                 setLayout( layout );
@@ -409,42 +417,43 @@ public class CustomSolutionControls extends PhetTitledPanel {
                 layout.addComponent( Box.createHorizontalStrut( weakStrengthControl.getPreferredSize().width ), row, column );
             }
         }
-        
+
         public void setWeakSelected( boolean selected ) {
             weakRadioButton.setSelected( selected );
             weakStrengthControl.setVisible( selected );
         }
-        
+
         public boolean isWeakSelected() {
             return weakRadioButton.isSelected();
         }
-        
+
         /*
-         * HACK ALERT:
-         * It's a little odd to just ignore strength values that are out of range.
-         * But the strength slider only applies to weak acids/bases, and will only
-         * be visible for weak acids/bases. And its convenient to be able to call
-         * this for any solution, and simply have it be ignored for strong acids/bases.
-         */
+        * HACK ALERT:
+        * It's a little odd to just ignore strength values that are out of range.
+        * But the strength slider only applies to weak acids/bases, and will only
+        * be visible for weak acids/bases. And its convenient to be able to call
+        * this for any solution, and simply have it be ignored for strong acids/bases.
+        */
         public void setStrength( double strength ) {
             if ( ABSConstants.WEAK_STRENGTH_RANGE.contains( strength ) ) {
                 weakStrengthControl.setValue( strength );
             }
         }
-        
+
         public double getStrength() {
             return weakStrengthControl.getValue();
         }
     }
-    
+
     /*
-     * Value control layout that has only the slider.
-     * The label, text field and units are omitted.
-     */
+    * Value control layout that has only the slider.
+    * The label, text field and units are omitted.
+    */
     private static class SliderLayoutStrategy implements ILayoutStrategy {
 
-        public SliderLayoutStrategy() {}
-        
+        public SliderLayoutStrategy() {
+        }
+
         public void doLayout( AbstractValueControl valueControl ) {
             valueControl.add( valueControl.getSlider() );
         }
