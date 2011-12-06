1e22ffafded60caed715b3849b620d51a70809c3
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-12-06 20:16:59 +0000

    12/6/11 #3170, add sim-sharing event for when user tries to drag pH color key, which is not interactive

--- trunk/simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/constants/ABSSimSharing.java
+++ trunk/simulations-java/simulations/acid-base-solutions/src/edu/colorado/phet/acidbasesolutions/constants/ABSSimSharing.java
@@ -15,6 +15,7 @@ public class ABSSimSharing {
     public static final String IS_DIPPED = "isDipped";
     public static final String IS_IN_SOLUTION = "isInSolution";
     public static final String MAGNIFYING_GLASS = "magnifyingGlass";
+    public static final String PH_COLOR_KEY = "phColorKey";
     public static final String PH_METER = "phMeter";
     public static final String PH_PAPER = "phPaper";
     public static final String WEAK_STRENGTH_CONTROL = "weakStrengthControl";
