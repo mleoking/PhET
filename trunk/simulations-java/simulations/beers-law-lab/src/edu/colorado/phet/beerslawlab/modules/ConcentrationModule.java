e30a2841b9386c6ae5114d313c5bb9dbc337ad8f
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-12-08 00:38:08 +0000

    imports

--- trunk/simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/modules/ConcentrationModule.java
+++ trunk/simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/modules/ConcentrationModule.java
@@ -1,7 +1,7 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.beerslawlab.modules;
 
-import edu.colorado.phet.beerslawlab.BLLResources;
+import edu.colorado.phet.beerslawlab.BLLResources.Strings;
 import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
 import edu.colorado.phet.common.piccolophet.PhetPCanvas;
 import edu.colorado.phet.common.piccolophet.PiccoloModule;
@@ -14,7 +14,7 @@ import edu.colorado.phet.common.piccolophet.PiccoloModule;
 public class ConcentrationModule extends PiccoloModule {
 
     public ConcentrationModule() {
-        super( BLLResources.Strings.CONCENTRATION, new ConstantDtClock( 25 ) );
+        super( Strings.CONCENTRATION, new ConstantDtClock( 25 ) );
         setLogoPanel( null );
         setControlPanel( null );
         setClockControlPanel( null );
