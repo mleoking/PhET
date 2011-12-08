9bcf5ee54c96eb5c2467835535f984a9eaa46981
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-12-08 00:38:08 +0000

    imports

--- simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/modules/BeersLawModule.java
+++ simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/modules/BeersLawModule.java
@@ -1,7 +1,7 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.beerslawlab.modules;
 
-import edu.colorado.phet.beerslawlab.BLLResources;
+import edu.colorado.phet.beerslawlab.BLLResources.Strings;
 import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
 import edu.colorado.phet.common.piccolophet.PhetPCanvas;
 import edu.colorado.phet.common.piccolophet.PiccoloModule;
@@ -14,7 +14,7 @@ import edu.colorado.phet.common.piccolophet.PiccoloModule;
 public class BeersLawModule extends PiccoloModule {
 
     public BeersLawModule() {
-        super( BLLResources.Strings.BEERS_LAW, new ConstantDtClock( 25 ) );
+        super( Strings.BEERS_LAW, new ConstantDtClock( 25 ) );
         setLogoPanel( null );
         setControlPanel( null );
         setClockControlPanel( null );
