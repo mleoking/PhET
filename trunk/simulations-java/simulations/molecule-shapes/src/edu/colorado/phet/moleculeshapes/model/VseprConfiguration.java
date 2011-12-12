d344d9a4a3718579579f1ea31ed8274cf37d7691
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-12-12 05:11:24 +0000

    ms: some clean-up

--- trunk/simulations-java/simulations/molecule-shapes/src/edu/colorado/phet/moleculeshapes/model/VseprConfiguration.java
+++ trunk/simulations-java/simulations/molecule-shapes/src/edu/colorado/phet/moleculeshapes/model/VseprConfiguration.java
@@ -119,11 +119,11 @@ public class VseprConfiguration {
         }};
     }
 
-    public ResultMapping getIdealRotationToPositions( Molecule molecule ) { // TODO change this to be more general, and use the LocalShape method
+    public ResultMapping getIdealRotationToPositions( List<PairGroup> groups ) {
         // ideal vectors excluding lone pairs (just for the bonds)
         List<ImmutableVector3D> idealModelBondVectors = getIdealBondUnitVectors();
 
-        return AttractorModel.findClosestMatchingConfiguration( AttractorModel.getOrientations( molecule.getAllNonCentralAtoms() ), idealModelBondVectors, Permutation.permutations( idealModelBondVectors.size() ) );
+        return AttractorModel.findClosestMatchingConfiguration( AttractorModel.getOrientations( groups ), idealModelBondVectors, Permutation.permutations( idealModelBondVectors.size() ) );
     }
 
     /*---------------------------------------------------------------------------*
