e444d25386c21f76034308981e8098a7b98850e3
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-12-13 04:25:26 +0000

    Improved toggle buttons

--- trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/intro/intro/view/RepresentationControlPanel.java
+++ trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/intro/intro/view/RepresentationControlPanel.java
@@ -3,18 +3,20 @@ package edu.colorado.phet.fractions.intro.intro.view;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Point;
+import java.awt.geom.Point2D;
 import java.awt.geom.RoundRectangle2D;
 
 import edu.colorado.phet.common.phetcommon.model.property.Property;
+import edu.colorado.phet.common.phetcommon.model.property.ValueEquals;
+import edu.colorado.phet.common.phetcommon.util.function.VoidFunction0;
 import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
-import edu.colorado.phet.common.piccolophet.event.CursorHandler;
+import edu.colorado.phet.common.phetcommon.view.util.RectangleUtils;
 import edu.colorado.phet.common.piccolophet.nodes.ControlPanelNode;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.colorado.phet.common.piccolophet.nodes.kit.ZeroOffsetNode;
 import edu.colorado.phet.common.piccolophet.nodes.layout.HBox;
 import edu.umd.cs.piccolo.PNode;
-import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
-import edu.umd.cs.piccolo.event.PInputEvent;
 
 /**
  * @author Sam Reid
@@ -47,33 +49,39 @@ public class RepresentationControlPanel extends ControlPanelNode {
             final double finalMaxWidth = maxWidth;
             final HBox representationLayer = new HBox( 10 ) {{
                 for ( final RepIcon element : elements ) {
-                    PNode highlighter = new PhetPPath( new RoundRectangle2D.Double( -2, -2, finalMaxWidth + 4, finalMaxHeight + 4, 20, 20 ), new Color( 242, 242, 242 ), new BasicStroke( 4, BasicStroke.CAP_ROUND, BasicStroke.JOIN_MITER ), null ) {{
 
-                        PNode node = new ZeroOffsetNode( element.getNode() );
+                    PNode button = new PhetPPath( new RoundRectangle2D.Double( -2, -2, finalMaxWidth + 4, finalMaxHeight + 4, 20, 20 ), null ) {{
 
-                        addChild( node );
-                        node.setOffset( finalMaxWidth / 2 - node.getFullBounds().getWidth() / 2, finalMaxHeight / 2 - node.getFullBounds().getHeight() / 2 );
-
-                        selected.valueEquals( element.getRepresentation() ).addObserver( new VoidFunction1<Boolean>() {
-                            public void apply( Boolean selected ) {
-                                setStrokePaint( selected ? Color.yellow : new Color( 178, 178, 178 ) );
-                                setStroke( selected ? new BasicStroke( 6 ) : new BasicStroke( 2 ) );
-                            }
-                        } );
-
-                        addInputEventListener( new CursorHandler() );
-                        addInputEventListener( new PBasicInputEventHandler() {
-                            @Override public void mousePressed( PInputEvent event ) {
-                                selected.set( element.getRepresentation() );
-                            }
-                        } );
+                        final ZeroOffsetNode theNode = new ZeroOffsetNode( element.getNode() ) {{
+                            final Point2D.Double origOffset = new Point2D.Double( finalMaxWidth / 2 - getFullWidth() / 2, finalMaxHeight / 2 - getFullHeight() / 2 );
+                            setOffset( origOffset );
+                        }};
+                        addChild( theNode );
                     }};
 
-                    addChild( highlighter );
+                    addChild( new ToggleButtonNode( button, selected.valueEquals( element.getRepresentation() ), new VoidFunction0() {
+                        public void apply() {
+                            selected.set( element.getRepresentation() );
+                        }
+                    } ) );
                 }
 //                setOffset( 30, title.getFullBounds().getMaxY() );
             }};
             addChild( representationLayer );
+        }
+
+        //Add padding around the node so it doesn't disturb the HBox layout when it moves within the padded node
+        private class PaddedNode extends PNode {
+            public PaddedNode( final PNode node, double dx, double dy, ValueEquals<ChosenRepresentation> isSelected ) {
+                addChild( new PhetPPath( RectangleUtils.expand( node.getFullBoundsReference(), dx, dy ), null ) );
+                addChild( node );
+
+                isSelected.addObserver( new VoidFunction1<Boolean>() {
+                    public void apply( Boolean selected ) {
+                        node.setOffset( selected ? new Point( 5, 5 ) : new Point( 0, 0 ) );
+                    }
+                } );
+            }
         }
     }
 }
