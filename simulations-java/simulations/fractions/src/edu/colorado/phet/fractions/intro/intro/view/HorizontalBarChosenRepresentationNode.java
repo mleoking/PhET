4c5cf34864922c8f424c0b194e9ccfa0751eec0b
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-12-13 16:19:40 +0000

    Constrain the horizontal bar representation to fill the screen when there are 6 and to take up 2 rows

--- simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/intro/intro/view/HorizontalBarChosenRepresentationNode.java
+++ simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/intro/intro/view/HorizontalBarChosenRepresentationNode.java
@@ -10,15 +10,27 @@ import edu.colorado.phet.common.phetcommon.util.RichSimpleObserver;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 
 /**
+ * Shows the fraction as a set of bars that lie horizontally, taking up two rows if more than 3
+ *
  * @author Sam Reid
  */
 public class HorizontalBarChosenRepresentationNode extends ChosenRepresentationNode {
+    private static final double NUM_BARS_PER_LINE = 3;
+    private static final double DISTANCE_BETWEEN_BARS_Y = 20;
+
     public HorizontalBarChosenRepresentationNode( Property<ChosenRepresentation> chosenRepresentation, final Property<Integer> numerator, final Property<Integer> denominator ) {
         super( chosenRepresentation, ChosenRepresentation.HORIZONTAL_BAR );
 
         new RichSimpleObserver() {
-            @Override public void update() {
+            public void update() {
                 removeAllChildren();
+
+                final int distanceBetweenBars = 55;
+
+                //Find how much space we can use for 3 bars horizontally
+                double spaceForBars = FractionsIntroCanvas.STAGE_SIZE.getWidth() - FractionsIntroCanvas.INSET * 2 - distanceBetweenBars * 2;
+                double totalBarWidth = spaceForBars / NUM_BARS_PER_LINE;
+                final double cellWidth = totalBarWidth / denominator.get();
 
                 int numBars = numerator.get() / denominator.get();
                 if ( numerator.get() % denominator.get() != 0 ) {
@@ -26,14 +38,24 @@ public class HorizontalBarChosenRepresentationNode extends ChosenRepresentationN
                 }
                 int numElementsAdded = 0;
                 double x = 0;
+                double y = 0;
                 for ( int i = 0; i < numBars; i++ ) {
+                    double barHeight = 50;
                     for ( int k = 0; k < denominator.get(); k++ ) {
                         Color color = numElementsAdded < numerator.get() ? FractionsIntroCanvas.FILL_COLOR : Color.white;
-                        addChild( new PhetPPath( new Rectangle2D.Double( x, 0, 50, 50 ), color, new BasicStroke( 2 ), Color.black ) );
-                        x = x + 50;
+                        addChild( new PhetPPath( new Rectangle2D.Double( x, y, cellWidth, barHeight ), color, new BasicStroke( 2 ), Color.black ) );
+                        x = x + cellWidth;
                         numElementsAdded++;
                     }
-                    x = x + 55;
+
+                    //go to next bar
+                    x = x + distanceBetweenBars;
+
+                    //Go to next row
+                    if ( i == 2 ) {
+                        y = y + barHeight + DISTANCE_BETWEEN_BARS_Y;
+                        x = 0;
+                    }
                 }
             }
         }.observe( numerator, denominator );
