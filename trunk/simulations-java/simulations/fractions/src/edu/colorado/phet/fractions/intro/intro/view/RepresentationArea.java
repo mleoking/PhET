5cd129c402ccf6b36c1872fe9886241cbd6df9a3
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-12-13 17:21:42 +0000

    Centered representations vertically on Intro tab

--- trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/intro/intro/view/RepresentationArea.java
+++ trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/intro/intro/view/RepresentationArea.java
@@ -11,18 +11,32 @@ import edu.umd.cs.piccolo.PNode;
  */
 public class RepresentationArea extends PNode {
     public RepresentationArea( Property<ChosenRepresentation> chosenRepresentation, Property<Integer> numerator, Property<Integer> denominator ) {
-        addChild( new HorizontalBarChosenRepresentationNode( chosenRepresentation, numerator, denominator ) );
+
+        //Y offset values sampled with this inner class listener:
+//        addInputEventListener( new PBasicInputEventHandler() {
+//                        @Override public void mouseDragged( PInputEvent event ) {
+//                            translate( 0, event.getDeltaRelativeTo( RepresentationArea.this.getParent() ).getHeight() );
+//                            System.out.println( "offset y = " + getOffset().getY() );
+//                        }
+//                    } );
+
+        addChild( new HorizontalBarChosenRepresentationNode( chosenRepresentation, numerator, denominator ) {{
+            setOffset( 0, -29 );
+        }} );
         addChild( new VerticalBarChosenRepresentationNode( chosenRepresentation, numerator, denominator ) {{
-            setOffset( 0, -80 );
+            setOffset( 0, -73 );
         }} );
 
         //Since it is unclear how to subdivide a single grid while keeping it the same size, we have discarded this representation for now.
 //        addChild( new GridFractionNode( chosenRepresentation, numerator, denominator ) {{
 //            setOffset( 0, -50 );
 //        }} );
-        addChild( new PieSetFractionNode( numerator, denominator, chosenRepresentation.valueEquals( ChosenRepresentation.PIE ) ) );
+        addChild( new PieSetFractionNode( numerator, denominator, chosenRepresentation.valueEquals( ChosenRepresentation.PIE ) ) {{
+            setOffset( 0, -48 );//Sampled with
+        }} );
         addChild( new NumberLineNode( numerator, denominator, chosenRepresentation.valueEquals( ChosenRepresentation.NUMBER_LINE ) ) {{
-            setOffset( 10, 20 );
+            setOffset( 10, 15 );
+
         }} );
     }
 }
