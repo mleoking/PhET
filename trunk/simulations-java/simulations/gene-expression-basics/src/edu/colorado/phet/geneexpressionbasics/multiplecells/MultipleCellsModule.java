5163e6076f7a2b6cd8496b3a5a4a387d3bde13e9
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-12-14 21:32:20 +0000

    Added basic model infrastructure for the "Multiple Cells" tab.

--- trunk/simulations-java/simulations/gene-expression-basics/src/edu/colorado/phet/geneexpressionbasics/multiplecells/MultipleCellsModule.java
+++ trunk/simulations-java/simulations/gene-expression-basics/src/edu/colorado/phet/geneexpressionbasics/multiplecells/MultipleCellsModule.java
@@ -2,19 +2,21 @@
 package edu.colorado.phet.geneexpressionbasics.multiplecells;
 
 import edu.colorado.phet.common.phetcommon.application.Module;
-import edu.colorado.phet.geneexpressionbasics.manualgeneexpression.model.ManualGeneExpressionModel;
+import edu.colorado.phet.geneexpressionbasics.multiplecells.model.MultipleCellsModel;
 import edu.colorado.phet.geneexpressionbasics.multiplecells.view.MultipleCellsCanvas;
 
 /**
+ * Main module file for the Multiple Cells tab.
+ *
  * @author John Blanco
  */
 public class MultipleCellsModule extends Module {
     public MultipleCellsModule( String name ) {
-        this( name, new ManualGeneExpressionModel() );
+        this( name, new MultipleCellsModel() );
         setClockControlPanel( null );
     }
 
-    private MultipleCellsModule( String name, ManualGeneExpressionModel model ) {
+    private MultipleCellsModule( String name, MultipleCellsModel model ) {
         // TODO: i18n
         super( name, model.getClock() );
         setSimulationPanel( new MultipleCellsCanvas( model ) );
