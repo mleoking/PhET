e3d82fe65c3db188ea9ff9b9c808304727be6834
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-12-17 00:15:27 +0000

    Stubbed class for cell parameter controller.

---
+++ simulations-java/simulations/gene-expression-basics/src/edu/colorado/phet/geneexpressionbasics/multiplecells/view/CellParameterController.java
@@ -0,0 +1,16 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.geneexpressionbasics.multiplecells.view;
+
+import edu.umd.cs.piccolo.PNode;
+
+/**
+ * PNode that contains a slider and a textual title and allows the user to
+ * control the level of some parameter within a population of cells.
+ *
+ * @author John Blanco
+ */
+public class CellParameterController extends PNode {
+    public CellParameterController() {
+
+    }
+}
