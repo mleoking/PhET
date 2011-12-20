955daf654a919c97ce0ea4a5cfc8427e8e354b11
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-12-20 18:17:43 +0000

    12/20/11 specify solutes as in design document

--- trunk/simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/concentration/ConcentrationModel.java
+++ trunk/simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/concentration/ConcentrationModel.java
@@ -19,12 +19,11 @@ import edu.colorado.phet.common.phetcommon.util.DoubleRange;
 public class ConcentrationModel implements Resettable {
 
     private static final DoubleRange SOLUTE_AMOUNT_RANGE = new DoubleRange( 0, 1, 0.5 ); // moles
-    private static final DoubleRange SOLUTION_VOLUME_RANGE = new DoubleRange( 0.2, 1, 0.5 ); // liters
-    private static final DoubleRange CONCENTRATION_RANGE = new DoubleRange( SOLUTE_AMOUNT_RANGE.getMin() / SOLUTION_VOLUME_RANGE.getMax(),
-                                                                            SOLUTE_AMOUNT_RANGE.getMax() / SOLUTION_VOLUME_RANGE.getMin() );
+    private static final DoubleRange SOLUTION_VOLUME_RANGE = new DoubleRange( 0, 1, 0.5 ); // liters
 
     static {
-        assert ( SOLUTION_VOLUME_RANGE.getMin() > 0 );
+        assert ( SOLUTE_AMOUNT_RANGE.getMin() >= 0 );
+        assert ( SOLUTION_VOLUME_RANGE.getMin() >= 0 );
     }
 
     private final ArrayList<Solute> solutes; // the supported set of solutes
@@ -32,20 +31,18 @@ public class ConcentrationModel implements Resettable {
 
     public ConcentrationModel() {
 
-        // solutes, in rainbow (ROYBIV) order
+        // Solutes, in rainbow (ROYBIV) order. Values are specified in the design document.
         this.solutes = new ArrayList<Solute>() {{
-            add( new Solute( Strings.KOOL_AID, Symbols.KOOL_AID, 5.0, Color.RED, 5, 200 ) );
-            add( new Solute( Strings.COBALT_II_NITRATE, Symbols.COBALT_II_NITRATE, 5.0, Color.RED, 5, 200 ) );
-            add( new Solute( Strings.COBALT_CHLORIDE, Symbols.COBALT_CHLORIDE, 4.35, new Color( 0xFF6A6A ) /* rose pink */, 5, 200 ) );
-            add( new Solute( Strings.POTASSIUM_DICHROMATE, Symbols.POTASSIUM_DICHROMATE, 0.50, new Color( 0xFF7F00 ) /* orange */, 5, 200 ) );
-            add( new Solute( Strings.POTASSIUM_CHROMATE, Symbols.POTASSIUM_CHROMATE, 3.35, Color.YELLOW, 5, 200 ) );
-            add( new Solute( Strings.NICKEL_II_CHLORIDE, Symbols.NICKEL_II_CHLORIDE, 5.0, new Color( 0x008000 ) /* green */, 5, 200 ) );
-            add( new Solute( Strings.COPPER_SULFATE, Symbols.COPPER_SULFATE, 1.40, new Color( 0x1E90FF ) /* blue */, 5, 200 ) );
-            add( new Solute( Strings.POTASSIUM_PERMANGANATE, Symbols.POTASSIUM_PERMANGANATE, 0.50, new Color( 0x8B008B ) /* purple */, Color.BLACK, 5, 200 ) );
+            final int precipitateParticlesPerMole = 200;
+            add( new Solute( Strings.KOOL_AID, Symbols.KOOL_AID, 342.296, 5.96, 5.50, Color.RED, 5, precipitateParticlesPerMole ) );
+            add( new Solute( Strings.COBALT_II_NITRATE, Symbols.COBALT_II_NITRATE, 182.942, 5.64, 5.0, Color.RED, 5, precipitateParticlesPerMole ) );
+            add( new Solute( Strings.COBALT_CHLORIDE, Symbols.COBALT_CHLORIDE, 129.839, 4.33, 4.0, new Color( 0xFF6A6A ) /* rose pink */, 5, precipitateParticlesPerMole ) );
+            add( new Solute( Strings.POTASSIUM_DICHROMATE, Symbols.POTASSIUM_DICHROMATE, 294.185, 0.51, 0.50, new Color( 0xFF7F00 ) /* orange */, 5, precipitateParticlesPerMole ) );
+            add( new Solute( Strings.POTASSIUM_CHROMATE, Symbols.POTASSIUM_CHROMATE, 194.191, 3.35, 3.0, Color.YELLOW, 5, precipitateParticlesPerMole ) );
+            add( new Solute( Strings.NICKEL_II_CHLORIDE, Symbols.NICKEL_II_CHLORIDE, 129.599, 5.21, 5.0, new Color( 0x008000 ) /* green */, 5, precipitateParticlesPerMole ) );
+            add( new Solute( Strings.COPPER_SULFATE, Symbols.COPPER_SULFATE, 159.609, 1.38, 1.0, new Color( 0x1E90FF ) /* blue */, 5, precipitateParticlesPerMole ) );
+            add( new Solute( Strings.POTASSIUM_PERMANGANATE, Symbols.POTASSIUM_PERMANGANATE, 158.034, 0.48, 0.4, new Color( 0x8B008B ) /* purple */, Color.BLACK, 5, precipitateParticlesPerMole ) );
         }};
-        for ( Solute solute : solutes ) {
-            assert ( CONCENTRATION_RANGE.contains( solute.saturatedConcentration ) );
-        }
 
         this.solution = new Solution( solutes.get( 0 ), SOLUTE_AMOUNT_RANGE.getDefault(), SOLUTION_VOLUME_RANGE.getDefault() );
     }
@@ -60,10 +57,6 @@ public class ConcentrationModel implements Resettable {
 
     public DoubleRange getSolutionVolumeRange() {
         return SOLUTION_VOLUME_RANGE;
-    }
-
-    public DoubleRange getConcentrationRange() {
-        return CONCENTRATION_RANGE;
     }
 
     public void reset() {
