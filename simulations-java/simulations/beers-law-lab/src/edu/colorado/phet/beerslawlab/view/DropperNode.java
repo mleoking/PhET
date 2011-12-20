5cbfa6b5b822616b456470c13400b740e22049ef
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-12-20 21:01:31 +0000

    center label in dropper

--- simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/view/DropperNode.java
+++ simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/view/DropperNode.java
@@ -11,6 +11,7 @@ import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.PhetPNode;
+import edu.colorado.phet.common.piccolophet.event.CursorHandler;
 import edu.colorado.phet.common.piccolophet.nodes.HTMLNode;
 import edu.colorado.phet.common.piccolophet.nodes.kit.ZeroOffsetNode;
 import edu.umd.cs.piccolo.PNode;
@@ -23,12 +24,11 @@ import edu.umd.cs.piccolo.nodes.PImage;
  */
 public class DropperNode extends PhetPNode {
 
-    private static final double IMAGE_SCALE = 0.75;
-    private static final double LABEL_Y_OFFSET = 40 * IMAGE_SCALE; // y offset of the label's center from the image's center
+    private static final double IMAGE_SCALE = 0.65;
+    private static final double LABEL_X_OFFSET = -50 * IMAGE_SCALE; // y offset of the label's center from the image's center
 
     public DropperNode( final Property<Solute> solute, final Property<SoluteForm> soluteFormProperty ) {
 
-        // Image file is assumed to be oriented with dropper hole pointing down.
         final PImage imageNode = new PImage( Images.DROPPER ) {{
             scale( IMAGE_SCALE );
         }};
@@ -38,7 +38,8 @@ public class DropperNode extends PhetPNode {
         PNode parentNode = new PNode();
         parentNode.addChild( imageNode );
         parentNode.addChild( labelNode );
-        imageNode.setOffset( -imageNode.getFullBoundsReference().getWidth() / 2, -imageNode.getFullBoundsReference().getHeight() / 2 );
+        imageNode.setOffset( -imageNode.getFullBoundsReference().getWidth() / 2, -( imageNode.getFullBoundsReference().getHeight() / 2 ) );
+        parentNode.rotate( 0.5 * -Math.PI ); // Image file is assumed to be oriented with dropper hole pointing left.
 
         // Apply a wrapper node to move the origin to (0,0). Do this after transforming parentNode and its children.
         addChild( new ZeroOffsetNode( parentNode ) );
@@ -47,8 +48,8 @@ public class DropperNode extends PhetPNode {
         solute.addObserver( new SimpleObserver() {
             public void update() {
                 labelNode.setHTML( solute.get().formula );
-                labelNode.setOffset( -( labelNode.getFullBoundsReference().getWidth() / 2 ),
-                                     -( labelNode.getFullBoundsReference().getHeight() / 2 ) + LABEL_Y_OFFSET );
+                labelNode.setOffset( -( labelNode.getFullBoundsReference().getWidth() / 2 ) + LABEL_X_OFFSET,
+                                     -( labelNode.getFullBoundsReference().getHeight() / 2 ) );
             }
         } );
 
@@ -58,5 +59,7 @@ public class DropperNode extends PhetPNode {
                 setVisible( soluteFormProperty.get().equals( SoluteForm.SOLUTION ) );
             }
         } );
+
+        addInputEventListener( new CursorHandler() );
     }
 }
