4bb29afe72939fb34ff52392a74e2a9896ef3e3d
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-12-20 22:26:14 +0000

    Moved the bulk of the functionality into the main.js file from the HTML file.

--- trunk/team/jblanco/html5/simple-drag-and-drop/main.js
+++ trunk/team/jblanco/html5/simple-drag-and-drop/main.js
@@ -1,17 +1,53 @@
 // Copyright 2002-2011, University of Colorado
+
 var canvas;
 var mouseX = 0;
 var mouseY = 0;
+var mouseDown = false;
 var context;
 
-init();
 
+//Hook up the initialization function.
+$( document ).ready( function() {
+    init();
+} );
+
+// Hook up event handler for window resize.
+$( window ).resize( resizer );
+
+// Handler for window resize events.
+function resizer() {
+    canvas.width = $( window ).width();
+    canvas.height = $( window ).height();
+    draw();
+}
+;
+
+// Initialize the canvas, context,
 function init() {
+    function Node() {
+        this.x = 5;
+        this.y = 10;
+
+        this.funcName = function() {
+            this.y = 20;
+        }
+    }
+
+    var testNode = new Node();
+    console.log( testNode.y );
+    testNode.funcName();
+    console.log( testNode.y );
+
+    canvas = $( '#canvas' )[0];
+
+    if ( canvas.getContext ) {
+        context = canvas.getContext( '2d' );
+    }
 
     document.onmousedown = onDocumentMouseDown;
     document.onmouseup = onDocumentMouseUp;
     document.onmousemove = onDocumentMouseMove;
-    document.ondblclick = onDocumentDoubleClick;
 
     document.addEventListener( 'touchstart', onDocumentTouchStart, false );
     document.addEventListener( 'touchmove', onDocumentTouchMove, false );
@@ -30,15 +66,10 @@ function init() {
     );
 
     // Do the initial drawing, events will cause subsequent updates.
-//    draw();
+    resizer();
 }
 
-function draw() {
-    clearBackground();
-    drawParticle( mouseX, mouseY, 10, "red" )
-}
-
-var drawParticle = function ( xPos, yPos, radius, color ) {
+function drawParticle( xPos, yPos, radius, color ) {
     context.strokeStyle = '#000'; // black
     context.lineWidth = 4;
 
@@ -54,42 +85,81 @@ var drawParticle = function ( xPos, yPos, radius, color ) {
     context.fill();
 }
 
-var clearBackground = function() {
+function clearBackground() {
     context.save();
     context.globalCompositeOperation = "source-over";
     context.fillStyle = "rgb(255, 255, 153)";
     context.fillRect( 0, 0, canvas.width, canvas.height );
     context.restore();
-};
+}
+;
+
+function drawCircle( xPos, yPos, radius, color ) {
+    context.strokeStyle = '#000'; // black
+    context.lineWidth = 4;
+    context.fillStyle = color;
+    context.beginPath();
+    context.arc( xPos, yPos, radius, 0, Math.PI * 2, true );
+    context.closePath();
+    context.fill();
+}
+
+// Main drawing function.
+function draw() {
+    clearBackground();
+    drawCircle( mouseX, mouseY, 20, "red" );
+}
 
 // Event handlers.
 
 function onDocumentMouseDown() {
     console.log( "onDocumentMouseDown" );
+    mouseDown = true;
+    mouseX = event.clientX;
+    mouseY = event.clientY;
+    draw();
 }
 
 function onDocumentMouseUp() {
     console.log( "onDocumentMouseDown" );
+    mouseDown = false;
+    mouseX = event.clientX;
+    mouseY = event.clientY;
+    draw();
 }
 
 function onDocumentMouseMove( event ) {
     console.log( "onDocumentMouseMove" );
-}
-
-function onDocumentDoubleClick() {
-    console.log( "onDocumentDoubleClick" );
+    if ( mouseDown ) {
+        mouseX = event.clientX;
+        mouseY = event.clientY;
+        draw();
+    }
 }
 
 function onDocumentTouchStart( event ) {
     console.log( "onDocumentTouchStart" );
+    if ( event.touches.length == 1 ) {
+        event.preventDefault();
+        mouseX = event.touches[ 0 ].pageX;
+        mouseY = event.touches[ 0 ].pageY;
+        draw();
+    }
 }
 
 function onDocumentTouchMove( event ) {
     console.log( "onDocumentTouchMove" );
+    if ( event.touches.length == 1 ) {
+        event.preventDefault();
+        mouseX = event.touches[ 0 ].pageX;
+        mouseY = event.touches[ 0 ].pageY;
+        draw();
+    }
 }
 
 function onDocumentTouchEnd( event ) {
     console.log( "onDocumentTouchEnd" );
+    mouseDown = false;
 }
 
 function onWindowDeviceOrientation( event ) {
