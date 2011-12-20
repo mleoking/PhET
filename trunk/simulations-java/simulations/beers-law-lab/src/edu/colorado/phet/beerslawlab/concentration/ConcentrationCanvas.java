0db4cb68e4ac287b725519dc636c0bdef484f2f7
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-12-20 18:30:37 +0000

    12/20/11 grab solute control from dilutions

--- trunk/simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/concentration/ConcentrationCanvas.java
+++ trunk/simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/concentration/ConcentrationCanvas.java
@@ -1,9 +1,12 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.beerslawlab.concentration;
 
+import java.awt.Color;
 import java.awt.Frame;
 
+import edu.colorado.phet.beerslawlab.control.SoluteControlNode;
 import edu.colorado.phet.beerslawlab.view.BLLCanvas;
+import edu.colorado.phet.common.piccolophet.nodes.ResetAllButtonNode;
 
 /**
  * Canvas for the "Concentration" module.
@@ -13,6 +16,33 @@ import edu.colorado.phet.beerslawlab.view.BLLCanvas;
 public class ConcentrationCanvas extends BLLCanvas {
 
     public ConcentrationCanvas( final ConcentrationModel model, Frame parentFrame ) {
-        //TODO
+
+        // Solute control
+        SoluteControlNode soluteControlNode = new SoluteControlNode( model.getSolutes(), model.solution.solute );
+
+        // Reset All button
+        ResetAllButtonNode resetAllButtonNode = new ResetAllButtonNode( model, parentFrame, 18, Color.BLACK, Color.ORANGE ) {{
+            setConfirmationEnabled( false );
+        }};
+
+        // rendering order
+        {
+            addChild( soluteControlNode );
+            addChild( resetAllButtonNode );
+        }
+
+        // layout
+        {
+            final double xMargin = 20;
+            final double yMargin = 20;
+            // upper right
+            soluteControlNode.setOffset( getStageSize().getWidth() - soluteControlNode.getFullBoundsReference().getWidth() - xMargin, yMargin );
+            // lower right
+            resetAllButtonNode.setOffset( getStageSize().getWidth() - resetAllButtonNode.getFullBoundsReference().getWidth() - xMargin,
+                                          getStageSize().getHeight() - resetAllButtonNode.getFullBoundsReference().getHeight() - yMargin );
+        }
+        //TODO enable this after more layout is fleshed out
+//        scaleRootNodeToFitStage();
+//        centerRootNodeOnStage();
     }
 }
