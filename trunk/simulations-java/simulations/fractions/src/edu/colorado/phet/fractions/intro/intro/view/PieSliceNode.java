3b3f7a743088ca07ad46346458f1dd4d2cd79bb3
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-12-20 20:22:33 +0000

    Fixed: Whole pie node has a line at theta=0

--- trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/intro/intro/view/PieSliceNode.java
+++ trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/intro/intro/view/PieSliceNode.java
@@ -4,8 +4,9 @@ package edu.colorado.phet.fractions.intro.intro.view;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Paint;
-import java.awt.Rectangle;
 import java.awt.geom.Arc2D;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
 
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.umd.cs.piccolo.PNode;
@@ -14,17 +15,16 @@ import edu.umd.cs.piccolo.PNode;
  * Single slice for a pie.
  */
 public class PieSliceNode extends PNode {
-    private final Paint color;
-
     public PieSliceNode( double startDegrees, double extentDegrees,
 
                          //The area which the entire pie should take up
-                         Rectangle area, Paint color ) {
-        this.color = color;
-        removeAllChildren();
+                         Rectangle2D area, Paint color ) {
 
-        PhetPPath path = new PhetPPath( new Arc2D.Double( area.x, area.y, area.width, area.height, startDegrees, extentDegrees, Arc2D.Double.PIE ),
-                                        this.color, new BasicStroke( 2 ), Color.black );
+        final boolean fullCircle = extentDegrees >= 360 - 1E-6;
+
+        final Arc2D.Double arc = new Arc2D.Double( area.getX(), area.getY(), area.getWidth(), area.getHeight(), startDegrees, extentDegrees, Arc2D.Double.PIE );
+        final Ellipse2D.Double ellipse = new Ellipse2D.Double( area.getX(), area.getY(), area.getWidth(), area.getHeight() );
+        PhetPPath path = new PhetPPath( fullCircle ? ellipse : arc, color, new BasicStroke( 2 ), Color.black );
         addChild( path );
     }
 }
