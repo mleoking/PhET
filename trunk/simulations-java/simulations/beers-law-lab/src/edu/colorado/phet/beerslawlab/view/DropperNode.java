dddd32c6bd7ab34252b7fafc80f366f0cee293bb
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-12-21 02:26:55 +0000

    12/20/11 add drag constraint to MovableDragHandler

--- trunk/simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/view/DropperNode.java
+++ trunk/simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/view/DropperNode.java
@@ -8,8 +8,10 @@ import edu.colorado.phet.beerslawlab.BLLResources.Images;
 import edu.colorado.phet.beerslawlab.BLLSimSharing.Objects;
 import edu.colorado.phet.beerslawlab.model.Dropper;
 import edu.colorado.phet.beerslawlab.model.SoluteForm;
+import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
 import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
+import edu.colorado.phet.common.phetcommon.util.function.Function1;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.PhetPNode;
 import edu.colorado.phet.common.piccolophet.event.CursorHandler;
@@ -78,6 +80,12 @@ public class DropperNode extends PhetPNode {
         } );
 
         addInputEventListener( new CursorHandler() );
-        addInputEventListener( new MovableDragHandler( Objects.SHAKER, dropper, this ) );
+        addInputEventListener( new MovableDragHandler( Objects.SHAKER, dropper, this,
+                                                       new Function1<ImmutableVector2D, ImmutableVector2D>() {
+                                                           public ImmutableVector2D apply( ImmutableVector2D v ) {
+                                                               //TODO constrain drag
+                                                               return v;
+                                                           }
+                                                       } ) );
     }
 }
