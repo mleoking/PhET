8bd04d390e297e4ed6b5ae6193eb95af9b4f2af9
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-12-21 03:32:34 +0000

    12/20/11 add "Remove Solute" button

--- trunk/simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/concentration/ConcentrationCanvas.java
+++ trunk/simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/concentration/ConcentrationCanvas.java
@@ -3,12 +3,17 @@ package edu.colorado.phet.beerslawlab.concentration;
 
 import java.awt.Color;
 import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import edu.colorado.phet.beerslawlab.BLLResources.Strings;
 import edu.colorado.phet.beerslawlab.control.SoluteControlNode;
 import edu.colorado.phet.beerslawlab.view.BLLCanvas;
 import edu.colorado.phet.beerslawlab.view.DropperNode;
 import edu.colorado.phet.beerslawlab.view.ShakerNode;
+import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.nodes.ResetAllButtonNode;
+import edu.colorado.phet.common.piccolophet.nodes.TextButtonNode;
 
 /**
  * Canvas for the "Concentration" module.
@@ -25,6 +30,14 @@ public class ConcentrationCanvas extends BLLCanvas {
         ShakerNode shakerNode = new ShakerNode( model.shaker, model.soluteForm );
         // Dropper
         DropperNode dropperNode = new DropperNode( model.dropper, model.soluteForm );
+        // Remove Solute button
+        TextButtonNode removeSoluteButtonNode = new TextButtonNode( Strings.REMOVE_SOLUTE, new PhetFont( 18 ), Color.ORANGE ) {{
+            addActionListener( new ActionListener() {
+                public void actionPerformed( ActionEvent e ) {
+                    //TODO
+                }
+            } );
+        }};
         // Reset All button
         ResetAllButtonNode resetAllButtonNode = new ResetAllButtonNode( model, parentFrame, 18, Color.BLACK, Color.ORANGE ) {{
             setConfirmationEnabled( false );
@@ -34,6 +47,7 @@ public class ConcentrationCanvas extends BLLCanvas {
         {
             addChild( shakerNode );
             addChild( dropperNode );
+            addChild( removeSoluteButtonNode );
             addChild( resetAllButtonNode );
             addChild( soluteControlNode ); // on top, because it has a combo box popup
         }
@@ -44,6 +58,9 @@ public class ConcentrationCanvas extends BLLCanvas {
             final double yMargin = 20;
             // upper right
             soluteControlNode.setOffset( getStageSize().getWidth() - soluteControlNode.getFullBoundsReference().getWidth() - xMargin, yMargin );
+            // bottom center
+            removeSoluteButtonNode.setOffset( ( getStageSize().getWidth() - removeSoluteButtonNode.getFullBoundsReference().getWidth() ) / 2,
+                                              getStageSize().getHeight() - removeSoluteButtonNode.getFullBoundsReference().getHeight() - yMargin );
             // lower right
             resetAllButtonNode.setOffset( getStageSize().getWidth() - resetAllButtonNode.getFullBoundsReference().getWidth() - xMargin,
                                           getStageSize().getHeight() - resetAllButtonNode.getFullBoundsReference().getHeight() - yMargin );
