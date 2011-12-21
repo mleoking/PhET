2bdcb3536ea7fdba3939cb37ad18c6de4c29e883
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-12-21 03:20:11 +0000

    12/20/11 add PropertyLabel, for interactive labels that are associated with Swing controls (eg, icon next to radio button or check box)

---
+++ simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/view/controls/PropertyLabel.java
@@ -0,0 +1,31 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.common.phetcommon.view.controls;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.Icon;
+import javax.swing.JLabel;
+
+import edu.colorado.phet.common.phetcommon.model.property.Property;
+import edu.colorado.phet.common.phetcommon.simsharing.SimSharingManager;
+import edu.colorado.phet.common.phetcommon.simsharing.SimSharingStrings.Actions;
+
+/**
+ * Pressing this label sets a property value.
+ * This is useful for icons that are associated with Swing controls.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
+public class PropertyLabel<T> extends JLabel {
+
+    public PropertyLabel( final String simSharingObject, Icon icon, final Property<T> property, final T value ) {
+        super( icon );
+        addMouseListener( new MouseAdapter() {
+            @Override public void mousePressed( MouseEvent e ) {
+                SimSharingManager.sendEvent( simSharingObject, Actions.PRESSED );
+                property.set( value );
+            }
+        } );
+    }
+}
