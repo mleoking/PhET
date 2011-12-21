02532efeba412b7ad07479b28f08681b174ae947
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-12-21 18:16:43 +0000

    12/21/11 refine some controls

--- simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/concentration/ConcentrationCanvas.java
+++ simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/concentration/ConcentrationCanvas.java
@@ -3,20 +3,15 @@ package edu.colorado.phet.beerslawlab.concentration;
 
 import java.awt.Color;
 import java.awt.Frame;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-import edu.colorado.phet.beerslawlab.BLLResources.Strings;
 import edu.colorado.phet.beerslawlab.control.EvaporationControlNode;
+import edu.colorado.phet.beerslawlab.control.RemoveSoluteButtonNode;
 import edu.colorado.phet.beerslawlab.control.SoluteControlNode;
 import edu.colorado.phet.beerslawlab.view.BLLCanvas;
 import edu.colorado.phet.beerslawlab.view.DropperNode;
 import edu.colorado.phet.beerslawlab.view.ShakerNode;
-import edu.colorado.phet.common.phetcommon.model.property.Property;
-import edu.colorado.phet.common.phetcommon.model.property.doubleproperty.DoubleProperty;
-import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.nodes.ResetAllButtonNode;
-import edu.colorado.phet.common.piccolophet.nodes.TextButtonNode;
+import edu.umd.cs.piccolo.PNode;
 
 /**
  * Canvas for the "Concentration" module.
@@ -27,28 +22,13 @@ public class ConcentrationCanvas extends BLLCanvas {
 
     public ConcentrationCanvas( final ConcentrationModel model, Frame parentFrame ) {
 
-        // Solute controls
-        SoluteControlNode soluteControlNode = new SoluteControlNode( model.getSolutes(), model.solute, model.soluteForm );
-        // Shaker
-        ShakerNode shakerNode = new ShakerNode( model.shaker, model.soluteForm );
-        // Dropper
-        DropperNode dropperNode = new DropperNode( model.dropper, model.soluteForm );
-
-        // Evaporation control
-        final DoubleProperty evaporationRate = new DoubleProperty( 0d ); //TODO move to model
-        final DoubleProperty waterVolume = new DoubleProperty( 1d ); //TODO move to model
-        final Property<Boolean> clockRunning = new Property<Boolean>( true ); //TODO move to model
-        EvaporationControlNode evaporationControlNode = new EvaporationControlNode( evaporationRate, waterVolume, clockRunning );
-        // Remove Solute button
-        TextButtonNode removeSoluteButtonNode = new TextButtonNode( Strings.REMOVE_SOLUTE, new PhetFont( 18 ), Color.ORANGE ) {{
-            addActionListener( new ActionListener() {
-                public void actionPerformed( ActionEvent e ) {
-                    //TODO
-                }
-            } );
-        }};
-        // Reset All button
-        ResetAllButtonNode resetAllButtonNode = new ResetAllButtonNode( model, parentFrame, 18, Color.BLACK, Color.ORANGE ) {{
+        // Nodes
+        PNode soluteControlNode = new SoluteControlNode( model.getSolutes(), model.solute, model.soluteForm );
+        PNode shakerNode = new ShakerNode( model.shaker, model.soluteForm );
+        PNode dropperNode = new DropperNode( model.dropper, model.soluteForm );
+        PNode evaporationControlNode = new EvaporationControlNode( model.evaporationRate, model.getEvaporationRateRange() );
+        PNode removeSoluteButtonNode = new RemoveSoluteButtonNode( model.solution );
+        PNode resetAllButtonNode = new ResetAllButtonNode( model, parentFrame, 18, Color.BLACK, Color.ORANGE ) {{
             setConfirmationEnabled( false );
         }};
 
---
+++ simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/control/RemoveSoluteButtonNode.java
@@ -0,0 +1,29 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.beerslawlab.control;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import edu.colorado.phet.beerslawlab.BLLResources.Strings;
+import edu.colorado.phet.beerslawlab.model.Solution;
+import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
+import edu.colorado.phet.common.piccolophet.nodes.TextButtonNode;
+
+/**
+ * Button that removes all solute from a solution.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
+public class RemoveSoluteButtonNode extends TextButtonNode {
+
+    public RemoveSoluteButtonNode( final Solution solution ) {
+        super( Strings.REMOVE_SOLUTE, new PhetFont( 18 ), Color.ORANGE );
+        addActionListener( new ActionListener() {
+            public void actionPerformed( ActionEvent e ) {
+                solution.setSoluteAmount( 0 );
+            }
+        } );
+    }
+}
+
