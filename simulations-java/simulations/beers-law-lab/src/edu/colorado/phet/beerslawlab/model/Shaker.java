aef04f1711dc6f569203be5737840be72a6aa0a9
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-12-21 19:52:45 +0000

    push drag bounds into superclass

--- simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/model/Shaker.java
+++ simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/model/Shaker.java
@@ -1,10 +1,9 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.beerslawlab.model;
 
-import java.awt.geom.Rectangle2D;
-
 import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
 import edu.colorado.phet.common.phetcommon.model.property.Property;
+import edu.umd.cs.piccolo.util.PBounds;
 
 /**
  * Model of the dropper, contains solute in solid form.
@@ -14,12 +13,10 @@ import edu.colorado.phet.common.phetcommon.model.property.Property;
 public class Shaker extends Movable {
 
     public final Property<Solute> solute;
-    public final Rectangle2D dragBounds;
 
-    public Shaker( ImmutableVector2D location, Rectangle2D dragBounds, Property<Solute> solute ) {
-        super( location );
-        assert ( dragBounds.contains( location.toPoint2D() ) );
+    public Shaker( ImmutableVector2D location, PBounds bounds, Property<Solute> solute ) {
+        super( location, bounds );
+        assert ( bounds.contains( location.toPoint2D() ) );
         this.solute = solute;
-        this.dragBounds = dragBounds;
     }
 }
