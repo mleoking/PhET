0d763d189e8f08c34e94a3375bb861c56c2d89ac
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-12-21 00:54:26 +0000

    add momentary button to dropper

---
+++ simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/view/MomentaryButtonNode.java
@@ -0,0 +1,55 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.beerslawlab.view;
+
+import java.awt.Image;
+
+import edu.colorado.phet.common.phetcommon.model.property.Property;
+import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
+import edu.colorado.phet.common.piccolophet.PiccoloPhetResources;
+import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
+import edu.umd.cs.piccolo.event.PInputEvent;
+import edu.umd.cs.piccolo.nodes.PImage;
+
+/**
+ * A momentary button is "on" while pressed.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
+public class MomentaryButtonNode extends PNode {
+
+    // Constructor that uses default images (round red buttons with 3D look)
+    public MomentaryButtonNode( Property<Boolean> onProperty ) {
+        this( onProperty, PiccoloPhetResources.getImage( "button_pressed.png" ), PiccoloPhetResources.getImage( "button_unpressed.png" ) );
+    }
+
+    public MomentaryButtonNode( final Property<Boolean> onProperty, Image onImage, Image offImage ) {
+        assert ( onImage != offImage ); // different images are required
+
+        final PImage onNode = new PImage( onImage );
+        addChild( onNode );
+
+        final PImage offNode = new PImage( offImage );
+        addChild( offNode );
+
+        // Both images must have the same size.
+        assert ( onNode.getFullBoundsReference().equals( offNode.getFullBoundsReference() ) );
+
+        addInputEventListener( new PBasicInputEventHandler() {
+            @Override public void mousePressed( PInputEvent event ) {
+                onProperty.set( true );
+            }
+
+            @Override public void mouseReleased( PInputEvent event ) {
+                onProperty.set( false );
+            }
+        } );
+
+        onProperty.addObserver( new SimpleObserver() {
+            public void update() {
+                onNode.setVisible( onProperty.get() );
+                offNode.setVisible( !onProperty.get() );
+            }
+        } );
+    }
+}
