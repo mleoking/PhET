7f53fb883809035be8b178232bb40af6150041c8
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-12-21 03:48:33 +0000

    12/20/11 add evaporation control

--- simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/concentration/ConcentrationCanvas.java
+++ simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/concentration/ConcentrationCanvas.java
@@ -7,10 +7,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import edu.colorado.phet.beerslawlab.BLLResources.Strings;
+import edu.colorado.phet.beerslawlab.control.EvaporationControlNode;
 import edu.colorado.phet.beerslawlab.control.SoluteControlNode;
 import edu.colorado.phet.beerslawlab.view.BLLCanvas;
 import edu.colorado.phet.beerslawlab.view.DropperNode;
 import edu.colorado.phet.beerslawlab.view.ShakerNode;
+import edu.colorado.phet.common.phetcommon.model.property.Property;
+import edu.colorado.phet.common.phetcommon.model.property.doubleproperty.DoubleProperty;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.nodes.ResetAllButtonNode;
 import edu.colorado.phet.common.piccolophet.nodes.TextButtonNode;
@@ -30,6 +33,12 @@ public class ConcentrationCanvas extends BLLCanvas {
         ShakerNode shakerNode = new ShakerNode( model.shaker, model.soluteForm );
         // Dropper
         DropperNode dropperNode = new DropperNode( model.dropper, model.soluteForm );
+
+        // Evaporation control
+        final DoubleProperty evaporationRate = new DoubleProperty( 0d ); //TODO move to model
+        final DoubleProperty waterVolume = new DoubleProperty( 1d ); //TODO move to model
+        final Property<Boolean> clockRunning = new Property<Boolean>( true ); //TODO move to model
+        EvaporationControlNode evaporationControlNode = new EvaporationControlNode( evaporationRate, waterVolume, clockRunning );
         // Remove Solute button
         TextButtonNode removeSoluteButtonNode = new TextButtonNode( Strings.REMOVE_SOLUTE, new PhetFont( 18 ), Color.ORANGE ) {{
             addActionListener( new ActionListener() {
@@ -47,6 +56,7 @@ public class ConcentrationCanvas extends BLLCanvas {
         {
             addChild( shakerNode );
             addChild( dropperNode );
+            addChild( evaporationControlNode );
             addChild( removeSoluteButtonNode );
             addChild( resetAllButtonNode );
             addChild( soluteControlNode ); // on top, because it has a combo box popup
@@ -58,9 +68,12 @@ public class ConcentrationCanvas extends BLLCanvas {
             final double yMargin = 20;
             // upper right
             soluteControlNode.setOffset( getStageSize().getWidth() - soluteControlNode.getFullBoundsReference().getWidth() - xMargin, yMargin );
-            // bottom center
-            removeSoluteButtonNode.setOffset( ( getStageSize().getWidth() - removeSoluteButtonNode.getFullBoundsReference().getWidth() ) / 2,
-                                              getStageSize().getHeight() - removeSoluteButtonNode.getFullBoundsReference().getHeight() - yMargin );
+            // bottom left
+            evaporationControlNode.setOffset( xMargin,
+                                              getStageSize().getHeight() - evaporationControlNode.getFullBoundsReference().getHeight() - yMargin );
+            // left of evaporation control
+            removeSoluteButtonNode.setOffset( evaporationControlNode.getFullBoundsReference().getMaxX() + 10,
+                                              evaporationControlNode.getFullBoundsReference().getCenterY() - ( removeSoluteButtonNode.getFullBoundsReference().getHeight() / 2 ) );
             // lower right
             resetAllButtonNode.setOffset( getStageSize().getWidth() - resetAllButtonNode.getFullBoundsReference().getWidth() - xMargin,
                                           getStageSize().getHeight() - resetAllButtonNode.getFullBoundsReference().getHeight() - yMargin );
---
+++ simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/control/EvaporationControlNode.java
@@ -0,0 +1,47 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.beerslawlab.control;
+
+import edu.colorado.phet.beerslawlab.BLLResources.Strings;
+import edu.colorado.phet.common.phetcommon.model.property.Property;
+import edu.colorado.phet.common.phetcommon.model.property.doubleproperty.DoubleProperty;
+import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
+import edu.colorado.phet.common.piccolophet.nodes.ControlPanelNode;
+import edu.colorado.phet.common.piccolophet.nodes.PhetPText;
+import edu.colorado.phet.common.piccolophet.nodes.layout.HBox;
+import edu.colorado.phet.common.piccolophet.nodes.slider.HSliderNode;
+import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
+import edu.umd.cs.piccolo.event.PInputEvent;
+import edu.umd.cs.piccolo.nodes.PText;
+
+/**
+ * Evaporation control.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
+public class EvaporationControlNode extends ControlPanelNode {
+
+    public EvaporationControlNode( final DoubleProperty evaporationRate, final DoubleProperty waterVolume, Property<Boolean> clockRunning ) {
+        super( new HBox(
+
+                // Label
+                new PText( Strings.EVAPORATION ) {{
+                    setFont( new PhetFont( 18 ) );
+                }},
+
+                // Slider
+                new HSliderNode( 0, 100, evaporationRate, waterVolume.greaterThan( 0.0 ).and( clockRunning ) ) {{
+
+                    // Tick labels
+                    addLabel( 0.0, new PhetPText( Strings.NONE, new PhetFont( 14 ) ) );
+                    addLabel( 100.0, new PhetPText( Strings.LOTS, new PhetFont( 14 ) ) );
+
+                    // Set rate to zero when slider is released.
+                    this.addInputEventListener( new PBasicInputEventHandler() {
+                        @Override public void mouseReleased( PInputEvent event ) {
+                            evaporationRate.set( 0.0 );
+                        }
+                    } );
+                }}
+        ) );
+    }
+}
