2c2b75d2085071bf004667006ab22a8037584839
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-12-21 16:09:28 +0000

    Show 6 empty cups on startup for cup mode

--- trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/intro/intro/view/WaterGlassSetFractionNode.java
+++ trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/intro/intro/view/WaterGlassSetFractionNode.java
@@ -4,6 +4,7 @@ package edu.colorado.phet.fractions.intro.intro.view;
 import edu.colorado.phet.common.phetcommon.model.property.ObservableProperty;
 import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.phetcommon.util.RichSimpleObserver;
+import edu.colorado.phet.fractions.intro.intro.model.ContainerState;
 
 /**
  * Shows a fraction as a set of glasses of water.
@@ -12,33 +13,25 @@ import edu.colorado.phet.common.phetcommon.util.RichSimpleObserver;
  */
 public class WaterGlassSetFractionNode extends VisibilityNode {
 
-    public WaterGlassSetFractionNode( final Property<Integer> numerator, final Property<Integer> denominator, ObservableProperty<Boolean> enabled ) {
+    public WaterGlassSetFractionNode( final Property<ContainerState> containerState, ObservableProperty<Boolean> enabled ) {
         super( enabled );
         new RichSimpleObserver() {
             public void update() {
 
-                int d = denominator.get();
-
-                //6 pies fit on the screen
+                //6 fit on the screen
                 int distanceBetweenPies = 10;
                 double spaceForPies = FractionsIntroCanvas.WIDTH_FOR_REPRESENTATION - distanceBetweenPies * 5;
                 final double DIAMETER = spaceForPies / 6;
 
                 removeAllChildren();
-                int numFullCakes = numerator.get() / d;
-                int lastCup = numerator.get() % d;
                 SpacedHBox box = new SpacedHBox( DIAMETER + distanceBetweenPies );
 
-                for ( int i = 0; i < numFullCakes; i++ ) {
-                    box.addChild( new WaterGlassNode( denominator.get(), denominator.get() ) );
-                }
-
-                if ( lastCup > 0 ) {
-                    box.addChild( new WaterGlassNode( numerator.get() % denominator.get(), denominator.get() ) );
+                for ( int i = 0; i < containerState.get().numContainers; i++ ) {
+                    box.addChild( new WaterGlassNode( containerState.get().getContainer( i ).numFilledCells, containerState.get().getContainer( i ).numCells ) );
                 }
 
                 addChild( box );
             }
-        }.observe( numerator, denominator );
+        }.observe( containerState );
     }
 }
