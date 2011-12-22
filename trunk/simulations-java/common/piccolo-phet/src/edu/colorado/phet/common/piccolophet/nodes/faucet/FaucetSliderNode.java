13abaf174b83c522c0e714c9e0f27e6ddb8dc0cd
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-12-22 22:56:39 +0000

    #3198, simplify FaucetSliderNode, behavior unchanged for existing clients

--- trunk/simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/nodes/faucet/FaucetSliderNode.java
+++ trunk/simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/nodes/faucet/FaucetSliderNode.java
@@ -6,95 +6,50 @@ import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.phetcommon.util.function.VoidFunction0;
 import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
 import edu.colorado.phet.common.piccolophet.nodes.slider.HSliderNode;
-import edu.colorado.phet.common.piccolophet.nodes.slider.SliderNode;
-import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
 import edu.umd.cs.piccolo.event.PInputEvent;
 
 /**
- * Slider control shown at the top of the faucet to control the flow rate.
- * Positioned within the image and scaled up/down with the image to ensure good correspondence
+ * Slider control shown at the top of the faucet to control the flow.
+ * When the slider is disabled, the flow is set to zero.
  *
  * @author Sam Reid
  */
-public class FaucetSliderNode extends PNode {
-
-    public final SliderNode sliderNode;
+public class FaucetSliderNode extends HSliderNode {
 
     /**
-     * Creates a slider control to be shown on the faucet to control the flow rate
+     * Creates a slider control to be shown on the faucet to control the flow.
      *
-     * @param enabled                flag to indicate if more water can be added with this control, used to zero the control
+     * @param enabled                property to indicate if the slider is enabled
      * @param flowRatePercentage     the percentage of max flow rate, between 0 and 1
      * @param snapToZeroWhenReleased does the knob snap back to zero when the user releases it?
      */
     public FaucetSliderNode( final ObservableProperty<Boolean> enabled, final Property<Double> flowRatePercentage, final boolean snapToZeroWhenReleased ) {
+        super( 0, 1, flowRatePercentage, enabled );
 
-        //Wire up 2-way communication with the Property
-        final Property<Double> sliderProperty = new Property<Double>( flowRatePercentage.get() ) {
-            @Override public void set( Double value ) {
-                if ( enabled.get() ) {
-                    super.set( value );
-                }
-                else {
-                    super.set( 0.0 );
-                }
-            }
-        };
-        sliderProperty.addObserver( new VoidFunction1<Double>() {
-            public void apply( Double value ) {
-                if ( enabled.get() ) {
-                    flowRatePercentage.set( value );
-                }
-                else {
-                    flowRatePercentage.set( 0.0 );
-                }
-            }
-        } );
-        flowRatePercentage.addObserver( new VoidFunction1<Double>() {
-            public void apply( Double value ) {
-                sliderProperty.set( value );
-            }
-        } );
-        sliderNode = new HSliderNode( 0, 1, sliderProperty, enabled );
-
-        //The two styles for user input are
-        // a) the user has to hold the slider knob or it will snap back to zero
-        // b) the user can put the slider at a value, then let go and it will retain that value (until no more water is enabled)
-
+        // Sets the flow to zero.
         final VoidFunction0 snapToZero = new VoidFunction0() {
             public void apply() {
-                //Turn off the flow level
                 flowRatePercentage.set( 0.0 );
             }
         };
 
+        // Sets the flow to zero when the user releases the slider thumb.
         if ( snapToZeroWhenReleased ) {
-            //Set the flow back to zero when the user lets go, the user has to hold the slider to keep the faucet on
-            sliderNode.addInputEventListener( new PBasicInputEventHandler() {
+            addInputEventListener( new PBasicInputEventHandler() {
                 @Override public void mouseReleased( PInputEvent event ) {
                     snapToZero.apply();
                 }
             } );
         }
-        else {
-            //When no more water is enabled, snap the slider back to zero
-            final VoidFunction1<Boolean> moveSliderToZero = new VoidFunction1<Boolean>() {
-                public void apply( Boolean allowed ) {
-                    if ( !allowed ) {
-                        snapToZero.apply();
-                    }
-                }
-            };
-            enabled.addObserver( moveSliderToZero );
 
-            //If no more fluid is enabled when the user lets go of the slider, snap it back to zero
-            sliderNode.addInputEventListener( new PBasicInputEventHandler() {
-                @Override public void mouseReleased( PInputEvent event ) {
-                    moveSliderToZero.apply( enabled.get() );
+        // When the faucet is disabled, snap the slider back to zero.
+        enabled.addObserver( new VoidFunction1<Boolean>() {
+            public void apply( Boolean enabled ) {
+                if ( !enabled ) {
+                    snapToZero.apply();
                 }
-            } );
-        }
-        addChild( sliderNode );
+            }
+        } );
     }
 }
