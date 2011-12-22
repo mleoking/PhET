9b54256dd4445d88364c643f9e8b3acab26e19e2
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-12-22 20:06:53 +0000

    #3196, move duplicated code to superclass

--- trunk/simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/nodes/slider/SliderNode.java
+++ trunk/simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/nodes/slider/SliderNode.java
@@ -2,6 +2,7 @@
 package edu.colorado.phet.common.piccolophet.nodes.slider;
 
 import edu.colorado.phet.common.phetcommon.model.property.SettableProperty;
+import edu.colorado.phet.common.piccolophet.simsharing.SimSharingDragSequenceEventHandler;
 import edu.umd.cs.piccolo.PNode;
 
 /**
@@ -11,13 +12,20 @@ import edu.umd.cs.piccolo.PNode;
  * @author Sam Reid
  */
 public abstract class SliderNode extends PNode {
+
     public final double min;
     public final double max;
     public final SettableProperty<Double> value;
+    public SimSharingDragSequenceEventHandler dragHandler; // set by subclasses
 
     public SliderNode( final double min, final double max, final SettableProperty<Double> value ) {
         this.min = min;
         this.max = max;
         this.value = value;
+    }
+
+    // For sim-sharing, see #3196
+    public SimSharingDragSequenceEventHandler getDragHandler() {
+        return dragHandler;
     }
 }
