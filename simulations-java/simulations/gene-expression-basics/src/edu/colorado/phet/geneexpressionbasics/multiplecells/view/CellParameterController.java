a49231198ef00864ab1e7d377feb9a1616ce9f77
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-12-22 18:47:49 +0000

    Added slider and title to previously stubbed cell parameter controller.

--- simulations-java/simulations/gene-expression-basics/src/edu/colorado/phet/geneexpressionbasics/multiplecells/view/CellParameterController.java
+++ simulations-java/simulations/gene-expression-basics/src/edu/colorado/phet/geneexpressionbasics/multiplecells/view/CellParameterController.java
@@ -1,16 +1,35 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.geneexpressionbasics.multiplecells.view;
 
+import java.awt.Color;
+import java.awt.Font;
+
+import edu.colorado.phet.common.phetcommon.model.property.Property;
+import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
+import edu.colorado.phet.common.piccolophet.nodes.ControlPanelNode;
+import edu.colorado.phet.common.piccolophet.nodes.layout.VBox;
+import edu.colorado.phet.common.piccolophet.nodes.slider.HSliderNode;
 import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.nodes.PText;
 
 /**
  * PNode that contains a slider and a textual title and allows the user to
- * control the level of some parameter within a population of cells.
+ * control the level of some parameter.
  *
  * @author John Blanco
  */
 public class CellParameterController extends PNode {
-    public CellParameterController() {
+    private static final Font LABEL_FONT = new PhetFont( 14 );
+
+    public CellParameterController( String labelText, Color color ) {
+        PNode content = new VBox(
+                new PText( labelText ) {{
+                    setFont( LABEL_FONT );
+                }},
+                new HSliderNode( 0, 100, new Property<Double>( 0.0 ) )
+        );
+
+        addChild( new ControlPanelNode( content, color ) );
 
     }
 }
