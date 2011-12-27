76ad880c937eff4336782aa84ce46faf330cb2f1
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-12-27 01:06:58 +0000

    move origin of dropper to hole that dispenses stock solution

--- simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/view/DropperNode.java
+++ simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/view/DropperNode.java
@@ -13,8 +13,6 @@ import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.PhetPNode;
 import edu.colorado.phet.common.piccolophet.nodes.HTMLNode;
-import edu.colorado.phet.common.piccolophet.nodes.kit.ZeroOffsetNode;
-import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.nodes.PImage;
 
 /**
@@ -25,42 +23,35 @@ import edu.umd.cs.piccolo.nodes.PImage;
  */
 public class DropperNode extends PhetPNode {
 
-    private static final double IMAGE_SCALE = 0.65;
-    private static final double LABEL_X_OFFSET = -50 * IMAGE_SCALE; // x offset of the label's center from the dropper image's center
-    private static final double BUTTON_Y_OFFSET = 10 * IMAGE_SCALE; // y offset of button from the dropper image's center
+    private static final double BUTTON_Y_OFFSET = 60; // y offset of button center in dropper image file
+    private static final double LABEL_Y_OFFSET = 255; // y offset of the label's center in dropper image file
 
     public DropperNode( final Dropper dropper, final Property<SoluteForm> soluteFormProperty ) {
-        scale( 0.75 ); //TODO eliminate need for scaling
+        scale( 0.5 ); //TODO eliminate need for scaling
 
-        final PImage imageNode = new PImage( Images.DROPPER ) {{
-            scale( IMAGE_SCALE );
-        }};
-        final HTMLNode labelNode = new HTMLNode( "", Color.BLACK, new PhetFont( Font.BOLD, 22 ) );
+        final PImage imageNode = new PImage( Images.DROPPER );
+        addChild( imageNode );
+        imageNode.setOffset( -imageNode.getFullBoundsReference().getWidth() / 2, -imageNode.getFullBoundsReference().getHeight() );
 
-        // Combine image and label into a parent, to simplify rotation and label alignment.
-        PNode parentNode = new PNode();
-        parentNode.addChild( imageNode );
-        parentNode.addChild( labelNode );
-        imageNode.setOffset( -imageNode.getFullBoundsReference().getWidth() / 2, -( imageNode.getFullBoundsReference().getHeight() / 2 ) );
-        parentNode.rotate( 0.5 * -Math.PI ); // Image file is assumed to be oriented with dropper hole pointing left.
-
-        // Apply a wrapper node to move the origin to (0,0). Do this after transforming parentNode and its children.
-        addChild( new ZeroOffsetNode( parentNode ) );
+        final HTMLNode labelNode = new HTMLNode( "", Color.BLACK, new PhetFont( Font.BOLD, 30 ) );
+        addChild( labelNode );
 
         // On/off button
         MomentaryButtonNode buttonNode = new MomentaryButtonNode( dropper.on, dropper.enabled ) {{
             setSimSharingObject( Objects.DROPPER_BUTTON );
-            scale( 0.45 );
+            scale( 0.65 );
         }};
         addChild( buttonNode );
-        buttonNode.setOffset( ( parentNode.getFullBoundsReference().getWidth() - buttonNode.getFullBoundsReference().getWidth() ) / 2, BUTTON_Y_OFFSET );
+        buttonNode.setOffset( imageNode.getFullBoundsReference().getCenterX() - ( buttonNode.getFullBoundsReference().getWidth() / 2 ),
+                              imageNode.getFullBoundsReference().getMaxY() - ( imageNode.getFullBoundsReference().getHeight() - BUTTON_Y_OFFSET ) - ( buttonNode.getFullBoundsReference().getHeight() / 2 ) );
 
         // Change the label when the solute changes.
         dropper.solute.addObserver( new SimpleObserver() {
             public void update() {
                 labelNode.setHTML( dropper.solute.get().formula );
-                labelNode.setOffset( -( labelNode.getFullBoundsReference().getWidth() / 2 ) + LABEL_X_OFFSET,
-                                     -( labelNode.getFullBoundsReference().getHeight() / 2 ) );
+                labelNode.setRotation( -Math.PI / 2 );
+                labelNode.setOffset( -( labelNode.getFullBoundsReference().getWidth() / 2 ),
+                                     imageNode.getFullBoundsReference().getMaxY() - ( imageNode.getFullBoundsReference().getHeight() - LABEL_Y_OFFSET ) + ( labelNode.getFullBoundsReference().getHeight() / 2 ) );
             }
         } );
 
