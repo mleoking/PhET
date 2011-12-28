6c1e19f6af78d55956ecdc52a148d3afc8ec9050
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-12-28 04:57:39 +0000

    Added cakes for 1-8 denominator Removed cakes for 1/12 divisions

--- simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/intro/intro/view/CakeSetFractionNode.java
+++ simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/intro/intro/view/CakeSetFractionNode.java
@@ -22,18 +22,32 @@ public class CakeSetFractionNode extends VisibilityNode {
 
     //Order that slices appear in the cake as the user adds them
     HashMap<Integer, int[]> orderToAdd = new HashMap<Integer, int[]>() {{
+        put( 1, new int[] { 1 } );
         put( 2, new int[] { 2, 1 } );
         put( 3, new int[] { 1, 2, 3 } );
         put( 4, new int[] { 1, 2, 3, 4 } );
-        put( 12, new int[] { 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3 } );
+        put( 5, new int[] { 1, 2, 3, 4, 5 } );
+        put( 6, new int[] { 1, 2, 3, 4, 5, 6 } );
+        put( 7, new int[] { 1, 2, 3, 4, 5, 6, 7 } );
+        put( 8, new int[] { 1, 2, 3, 4, 5, 6, 7, 8 } );
+
+
+//        put( 12, new int[] { 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3 } );
     }};
 
     //Find which should appear before/after others in z-ordering.  Must be back to front.
     HashMap<Integer, int[]> renderOrder = new HashMap<Integer, int[]>() {{
+        put( 1, new int[] { 1 } );
         put( 2, new int[] { 2, 1 } );
         put( 3, new int[] { 1, 2, 3 } );
         put( 4, new int[] { 1, 2, 3, 4 } );
-        put( 12, new int[] { 3, 4, 5, 2, 6, 1, 7, 12, 8, 11, 9, 10 } );
+        put( 5, new int[] { 2, 1, 3, 5, 4 } );
+        put( 6, new int[] { 2, 1, 3, 6, 4, 5 } );
+        put( 7, new int[] { 2, 3, 1, 4, 7, 5, 6 } );
+        put( 8, new int[] { 2, 3, 1, 4, 5, 8, 6, 7 } );
+
+
+//        put( 12, new int[] { 3, 4, 5, 2, 6, 1, 7, 12, 8, 11, 9, 10 } );
     }};
 
     public CakeSetFractionNode( final Property<ContainerState> state, ObservableProperty<Boolean> enabled ) {
@@ -83,35 +97,6 @@ public class CakeSetFractionNode extends VisibilityNode {
             if ( !c.isEmpty( new CellPointer( container, i ) ) ) {
                 values.add( orderToAddThem[i] );
             }
-        }
-
-        Collections.sort( values, new Comparator<Integer>() {
-            public int compare( Integer o1, Integer o2 ) {
-                return Double.compare( renderOrder.indexOf( o1 ), renderOrder.indexOf( o2 ) );
-            }
-        } );
-
-        int[] x = new int[values.size()];
-        for ( int i = 0; i < x.length; i++ ) {
-            x[i] = values.get( i );
-        }
-        return x;
-    }
-
-    private int[] getSliceArray( int numSlices, Integer denominator ) {
-
-        final int[] orderToAddThem = orderToAdd.get( denominator );
-        final int[] theRenderOrder = renderOrder.get( denominator );
-
-        final ArrayList<Integer> renderOrder = new ArrayList<Integer>();
-        for ( int aTheRenderOrder : theRenderOrder ) {
-            renderOrder.add( aTheRenderOrder );
-        }
-
-        //Find which should appear before/after others.
-        ArrayList<Integer> values = new ArrayList<Integer>();
-        for ( int i = 0; i < numSlices; i++ ) {
-            values.add( orderToAddThem[i] );
         }
 
         Collections.sort( values, new Comparator<Integer>() {
