bf5cfeab4d9c22d350d2c002014ad6a27fa168e9
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-12-28 01:04:35 +0000

    add sim-sharing parameter "isInSolution" for dragging of meter probe

--- trunk/simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/view/MovableDragHandler.java
+++ trunk/simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/view/MovableDragHandler.java
@@ -9,6 +9,7 @@ import edu.colorado.phet.beerslawlab.model.Movable;
 import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
 import edu.colorado.phet.common.phetcommon.simsharing.Parameter;
 import edu.colorado.phet.common.phetcommon.simsharing.SimSharingManager;
+import edu.colorado.phet.common.phetcommon.util.function.Function0;
 import edu.colorado.phet.common.piccolophet.simsharing.SimSharingDragSequenceEventHandler;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.event.PInputEvent;
@@ -25,14 +26,24 @@ public class MovableDragHandler extends SimSharingDragSequenceEventHandler {
     private double clickXOffset, clickYOffset; // offset of mouse click from dragNode's origin, in parent's coordinate frame
 
     public MovableDragHandler( final String simSharingObject, final Movable movable, PNode dragNode ) {
+        this( simSharingObject, movable, dragNode,
+              new Function0<Parameter[]>() {
+                  public Parameter[] apply() {
+                      return new Parameter[0];
+                  }
+              } );
+    }
+
+    public MovableDragHandler( final String simSharingObject, final Movable movable, PNode dragNode, final Function0<Parameter[]> simSharingParametersFunction ) {
         this.movable = movable;
         this.dragNode = dragNode;
         // sim-sharing
         setStartEndDragFunction( new DragFunction() {
             public void apply( String action, Parameter xParameter, Parameter yParameter, PInputEvent event ) {
                 SimSharingManager.sendEvent( simSharingObject, action,
-                                             new Parameter( Parameters.LOCATION_X, (int) movable.location.get().getX() ),
-                                             new Parameter( Parameters.LOCATION_Y, (int) movable.location.get().getY() ) );
+                                             Parameter.appendParameters( simSharingParametersFunction.apply(),
+                                                                         new Parameter( Parameters.LOCATION_X, (int) movable.location.get().getX() ),
+                                                                         new Parameter( Parameters.LOCATION_Y, (int) movable.location.get().getY() ) ) );
             }
         } );
     }
