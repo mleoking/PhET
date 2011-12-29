e6366ed662820815fec6370a344ddff6ef1defff
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-12-29 08:22:19 +0000

    pt: variable density materials fixed

---
+++ simulations-java/simulations/plate-tectonics/src/edu/colorado/phet/platetectonics/view/DensityMaterial.java
@@ -0,0 +1,77 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.platetectonics.view;
+
+import java.nio.ByteBuffer;
+
+import org.lwjgl.BufferUtils;
+
+import edu.colorado.phet.common.phetcommon.math.MathUtil;
+import edu.colorado.phet.lwjglphet.GLMaterial;
+import edu.colorado.phet.lwjglphet.GLOptions;
+import edu.colorado.phet.lwjglphet.math.ImmutableVector2F;
+import edu.colorado.phet.platetectonics.model.CrustModel;
+
+import static org.lwjgl.opengl.GL11.*;
+
+public class DensityMaterial extends GLMaterial {
+    private static final int width = 256;
+    private static final int height = 256;
+
+    // TODO: check byte orders?
+    private static final ByteBuffer buffer = BufferUtils.createByteBuffer( 4 * width * height );
+
+    static {
+        buffer.rewind();
+        for ( int row = 0; row < height; row++ ) {
+            for ( int col = 0; col < width; col++ ) {
+                byte densityIndex = (byte) col;
+                buffer.put( new byte[] { densityIndex, densityIndex, densityIndex, (byte) 255 } );
+//                    buffer.put( new byte[] { (byte)row, (byte)col, 0, (byte) 255 } );
+//                buffer.put( new byte[] { (byte) 255, (byte) 255, (byte) 255, (byte) 255 } );
+            }
+        }
+    }
+
+    public static ImmutableVector2F densityMap( float density ) {
+        float minDensityToShow = 2500;
+        float maxDensityToShow = 3500;
+        float maxMaxDensityToShow = CrustModel.CENTER_DENSITY;
+
+        float densityRatio = ( density - minDensityToShow ) / ( maxDensityToShow - minDensityToShow );
+        float x;
+        if ( density <= 3300 ) {
+            x = 100f + ( 1f - densityRatio ) * 155f;
+        }
+        else {
+            float start = 100f + ( 1f - ( 3300 - minDensityToShow ) / ( maxDensityToShow - minDensityToShow ) ) * 155f;
+            float end = 50f;
+            float ratio = ( density - 3300 ) / ( maxMaxDensityToShow - 3300 );
+            x = start + ( end - start ) * ratio;
+        }
+        x = (float) MathUtil.clamp( 0.0, x / 255.0, 1.0 ); // clamp it in the normal range
+        return new ImmutableVector2F( x, 0.5f );
+
+//        float minDensityToShow = 2500;
+//        float maxDensityToShow = CrustModel.CENTER_DENSITY;
+//        float v = 1 - ( density - minDensityToShow ) / ( maxDensityToShow - minDensityToShow );
+//        return new ImmutableVector2F( v, 0.5f );
+    }
+
+    @Override public void before( GLOptions options ) {
+        // TODO: somehow we need the "white" color, since it's probably blending with our texture. investigate
+        glColor4f( 1, 1, 1, 1 );
+        glEnable( GL_TEXTURE_2D );
+        glShadeModel( GL_FLAT );
+        buffer.rewind();
+        glTexImage2D( GL_TEXTURE_2D, 0, 4, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, buffer );
+        glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP );
+        glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP );
+        glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
+        glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
+    }
+
+    @Override public void after( GLOptions options ) {
+        glShadeModel( GL_SMOOTH );
+        glDisable( GL_TEXTURE_2D );
+    }
+}
--- simulations-java/simulations/plate-tectonics/src/edu/colorado/phet/platetectonics/view/RegionNode.java
+++ simulations-java/simulations/plate-tectonics/src/edu/colorado/phet/platetectonics/view/RegionNode.java
@@ -4,22 +4,15 @@ package edu.colorado.phet.platetectonics.view;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.lwjgl.opengl.GL11;
-
-import edu.colorado.phet.common.phetcommon.math.MathUtil;
 import edu.colorado.phet.common.phetcommon.model.event.UpdateListener;
-import edu.colorado.phet.common.phetcommon.util.function.Function0;
 import edu.colorado.phet.lwjglphet.GLMaterial;
-import edu.colorado.phet.lwjglphet.GLOptions;
 import edu.colorado.phet.lwjglphet.math.ImmutableVector2F;
 import edu.colorado.phet.lwjglphet.math.ImmutableVector3F;
 import edu.colorado.phet.lwjglphet.shapes.PlanarPolygon;
-import edu.colorado.phet.platetectonics.model.CrustModel;
 import edu.colorado.phet.platetectonics.model.PlateModel;
 import edu.colorado.phet.platetectonics.model.Region;
 import edu.colorado.phet.platetectonics.model.Region.Type;
 import edu.colorado.phet.platetectonics.modules.PlateTectonicsTab;
-import edu.colorado.phet.platetectonics.util.ColorMaterial;
 
 /**
  * Visual display of a Region (polygonal area in the cross-section of the earth)
@@ -58,7 +51,7 @@ public class RegionNode extends PlanarPolygon {
                             float y = boundary[i].y;
                             ImmutableVector3F position3d = module.getModelViewTransform().transformPosition( PlateModel.convertToRadial( new ImmutableVector3F( x, y, z ) ) );
                             vertices[i] = new ImmutableVector2F( position3d.x, position3d.y );
-                            textureCoordinates[i] = new ImmutableVector2F( region.getDensity( boundary[i] ), 0 ); // TODO: handle texture coordinates depending on region type
+                            textureCoordinates[i] = DensityMaterial.densityMap( region.getDensity( boundary[i] ) );
                         }
 
                         setVertices( vertices, textureCoordinates );
@@ -71,12 +64,7 @@ public class RegionNode extends PlanarPolygon {
             setMaterial( material );
         }
         else {
-            setMaterial( new DensityMaterial( new Function0<Float>() {
-                public Float apply() {
-                    // TODO: replace with continuous-based texture version that doesn't depend on the particular region
-                    return region.getDensity( new ImmutableVector2F() );
-                }
-            } ) );
+            setMaterial( new DensityMaterial() );
         }
     }
 
@@ -91,32 +79,5 @@ public class RegionNode extends PlanarPolygon {
         }};
     }
 
-    public static class DensityMaterial extends GLMaterial {
 
-        private final Function0<Float> densityFunc;
-
-        public DensityMaterial( Function0<Float> density ) {
-            this.densityFunc = density;
-        }
-
-        @Override public void before( GLOptions options ) {
-            float density = densityFunc.apply();
-            float minDensityToShow = 2500;
-            float maxDensityToShow = 3500;
-            float maxMaxDensityToShow = CrustModel.CENTER_DENSITY;
-
-            float densityRatio = ( density - minDensityToShow ) / ( maxDensityToShow - minDensityToShow );
-            float x;
-            if( density <= 3300 ) {
-            x = 100f + ( 1f - densityRatio ) * 155f;
-            } else {
-                float start = 100f + (1f - (3300 - minDensityToShow)/(maxDensityToShow - minDensityToShow) ) * 155f;
-                float end = 50f;
-                float ratio = (density - 3300) / (maxMaxDensityToShow - 3300 );
-                x = start + (end-start) * ratio;
-            }
-            x = (float) MathUtil.clamp( 0.0, x / 255.0, 1.0 ); // clamp it in the normal range
-            GL11.glColor4f( x, x, x, 1f );
-        }
-    }
 }
