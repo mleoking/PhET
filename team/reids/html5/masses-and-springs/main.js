c8f0d19f7fd24c112c484db6f80a22a296ff5597
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-12-30 20:36:24 +0000

    improved checkbox

--- team/reids/html5/masses-and-springs/main.js
+++ team/reids/html5/masses-and-springs/main.js
@@ -437,29 +437,21 @@ function CheckBox( x, y ) {
 }
 
 CheckBox.prototype.draw = function ( context ) {
-    context.beginPath();
-    context.fillStyle = '#00f';
-    context.strokeStyle = '#77b';
+    context.fillStyle = '#fff';
+    context.strokeStyle = '#88e';
     context.lineWidth = 2;
-    context.beginPath();
-    context.moveTo( this.x, this.y );
-    context.lineTo( this.x + this.width, this.y );
-    context.lineTo( this.x + this.width, this.y + this.height );
-    context.lineTo( this.x, this.y + this.height );
-    context.lineTo( this.x, this.y );
-    context.stroke();
-    context.closePath();
+    roundRect( context, this.x, this.y, this.width, this.height, 7, true, true )
 
+    context.strokeStyle = '#000';
+    context.lineWidth = 4;
     if ( this.selected ) {
         context.beginPath();
-        context.fillStyle = '#00f';
-        context.strokeStyle = '#88e';
-        context.lineWidth = 2;
         context.beginPath();
-        context.moveTo( this.x, this.y );
-        context.lineTo( this.x + this.width, this.y + this.width );
-        context.moveTo( this.x + this.width, this.y );
-        context.lineTo( this.x, this.y + this.height );
+        context.strokeStyle = '#222';
+        context.moveTo( this.x + 5, this.y + 5 );
+        context.lineTo( this.x + this.width - 5, this.y + this.width - 5 );
+        context.moveTo( this.x + this.width - 5, this.y + 5 );
+        context.lineTo( this.x + 5, this.y + this.height - 5 );
         context.stroke();
         context.closePath();
     }
@@ -527,4 +519,45 @@ Slider.prototype.containsPoint = function ( location ) {
 
 Slider.prototype.getReferencePoint = function () {
     return new Point2D( this.knobX, this.y );
+}
+
+/**
+ * Draws a rounded rectangle using the current state of the canvas.
+ * If you omit the last three params, it will draw a rectangle
+ * outline with a 5 pixel border radius
+ * @param {CanvasRenderingContext2D} ctx
+ * @param {Number} x The top left x coordinate
+ * @param {Number} y The top left y coordinate
+ * @param {Number} width The width of the rectangle
+ * @param {Number} height The height of the rectangle
+ * @param {Number} radius The corner radius. Defaults to 5;
+ * @param {Boolean} fill Whether to fill the rectangle. Defaults to false.
+ * @param {Boolean} stroke Whether to stroke the rectangle. Defaults to true.
+ *
+ * @author http://js-bits.blogspot.com/2010/07/canvas-rounded-corner-rectangles.html
+ */
+function roundRect( ctx, x, y, width, height, radius, fill, stroke ) {
+    if ( typeof stroke == "undefined" ) {
+        stroke = true;
+    }
+    if ( typeof radius === "undefined" ) {
+        radius = 5;
+    }
+    ctx.beginPath();
+    ctx.moveTo( x + radius, y );
+    ctx.lineTo( x + width - radius, y );
+    ctx.quadraticCurveTo( x + width, y, x + width, y + radius );
+    ctx.lineTo( x + width, y + height - radius );
+    ctx.quadraticCurveTo( x + width, y + height, x + width - radius, y + height );
+    ctx.lineTo( x + radius, y + height );
+    ctx.quadraticCurveTo( x, y + height, x, y + height - radius );
+    ctx.lineTo( x, y + radius );
+    ctx.quadraticCurveTo( x, y, x + radius, y );
+    ctx.closePath();
+    if ( stroke ) {
+        ctx.stroke();
+    }
+    if ( fill ) {
+        ctx.fill();
+    }
 }
