5d13c58c3178d769bcf191b3970207dbfef03700
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2011-12-30 06:43:26 +0000

    pt: draggable crust pieces

--- simulations-java/simulations/plate-tectonics/src/edu/colorado/phet/platetectonics/modules/PlateMotionTab.java
+++ simulations-java/simulations/plate-tectonics/src/edu/colorado/phet/platetectonics/modules/PlateMotionTab.java
@@ -1,31 +1,24 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.platetectonics.modules;
 
-import java.awt.BasicStroke;
 import java.awt.Color;
-import java.awt.geom.Rectangle2D;
 
 import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
 import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
-import edu.colorado.phet.common.phetcommon.view.util.DoubleGeneralPath;
 import edu.colorado.phet.common.piccolophet.nodes.ControlPanelNode;
-import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.colorado.phet.lwjglphet.LWJGLCanvas;
-import edu.colorado.phet.lwjglphet.LWJGLCursorHandler;
-import edu.colorado.phet.lwjglphet.math.ImmutableMatrix4F;
 import edu.colorado.phet.lwjglphet.math.ImmutableVector2F;
 import edu.colorado.phet.lwjglphet.nodes.GuiNode;
 import edu.colorado.phet.lwjglphet.nodes.OrthoPiccoloNode;
-import edu.colorado.phet.lwjglphet.shapes.UnitMarker;
 import edu.colorado.phet.platetectonics.PlateTectonicsResources.Strings;
 import edu.colorado.phet.platetectonics.control.CrustChooserPanel;
+import edu.colorado.phet.platetectonics.control.CrustPiece;
 import edu.colorado.phet.platetectonics.control.PlayModePanel;
 import edu.colorado.phet.platetectonics.model.PlateMotionModel;
 import edu.colorado.phet.platetectonics.util.Bounds3D;
 import edu.colorado.phet.platetectonics.util.Grid3D;
 import edu.colorado.phet.platetectonics.view.PlateView;
-import edu.umd.cs.piccolo.PNode;
 
 /**
  * Displays two main plates that the user can direct to move towards, away from, or along each other.
@@ -86,15 +79,6 @@ public class PlateMotionTab extends PlateTectonicsTab {
         }};
         addGuiNode( crustChooserNode );
 
-        crustPieceLayer.addChild( new UnitMarker() {{
-            // TODO: change to float version
-            ImmutableVector2D nodeOffset = crustChooserNode.position.get();
-            ImmutableVector2F pieceOffset = crustChooserPanel.getYoungOceanicCenter();
-            transform.append( ImmutableMatrix4F.translation( (float) nodeOffset.getX(), (float) nodeOffset.getY(), 0 ) );
-            transform.append( ImmutableMatrix4F.translation( pieceOffset.x, pieceOffset.y, 0 ) );
-            transform.append( ImmutableMatrix4F.scaling( 10 ) );
-        }} );
-
         // continental piece
         OrthoPiccoloNode continentalPiece = new OrthoPiccoloNode( new CrustPiece( CrustChooserPanel.CRUST_AREA_MAX_HEIGHT, 0.8f ), this, getCanvasTransform(),
                                                                   new Property<ImmutableVector2D>(
@@ -105,7 +89,7 @@ public class PlateMotionTab extends PlateTectonicsTab {
                     getContinentalOffset().y - getNode().getFullBounds().getHeight() / 2 ) );
         }};
         crustPieceLayer.addChild( continentalPiece );
-        guiNodes.add( continentalPiece );
+        guiNodes.add( 0, continentalPiece );
 
         // young oceanic piece
         OrthoPiccoloNode youngOceanicPiece = new OrthoPiccoloNode( new CrustPiece( 35, 0.5f ), this, getCanvasTransform(),
@@ -117,7 +101,7 @@ public class PlateMotionTab extends PlateTectonicsTab {
                     getYoungOceanicOffset().y - getNode().getFullBounds().getHeight() / 2 ) );
         }};
         crustPieceLayer.addChild( youngOceanicPiece );
-        guiNodes.add( youngOceanicPiece );
+        guiNodes.add( 0, youngOceanicPiece );
 
         // old oceanic piece
         OrthoPiccoloNode oldOceanicPiece = new OrthoPiccoloNode( new CrustPiece( 35, 0.4f ), this, getCanvasTransform(),
@@ -129,46 +113,7 @@ public class PlateMotionTab extends PlateTectonicsTab {
                     getOldOceanicOffset().y - getNode().getFullBounds().getHeight() / 2 ) );
         }};
         crustPieceLayer.addChild( oldOceanicPiece );
-        guiNodes.add( oldOceanicPiece );
-    }
-
-    private static class CrustPiece extends PNode {
-        public CrustPiece( final float height, float intensity ) {
-            final float topHeight = 20;
-            final float rightWidth = 30;
-
-            float topIntensity = 1 - ( 1 - intensity ) * 0.5f;
-            float rightIntensity = 1 - ( 1 - intensity ) * 0.3f;
-
-            Color mainColor = new Color( intensity, intensity, intensity, 1f );
-            Color topColor = new Color( topIntensity, topIntensity, topIntensity, 1f );
-            Color rightColor = new Color( rightIntensity, rightIntensity, rightIntensity, 1f );
-            Color strokePaint = new Color( 0.2f, 0.2f, 0.2f, 1f );
-            BasicStroke stroke = new BasicStroke( 1 );
-
-            // front
-            addChild( new PhetPPath(
-                    new Rectangle2D.Double( 0, topHeight, CrustChooserPanel.CRUST_AREA_MAX_WIDTH - rightWidth, height - topHeight ),
-                    mainColor, stroke, strokePaint ) );
-
-            // top
-            addChild( new PhetPPath( new DoubleGeneralPath() {{
-                moveTo( 0, topHeight );
-                lineTo( rightWidth, 0 );
-                lineTo( CrustChooserPanel.CRUST_AREA_MAX_WIDTH, 0 );
-                lineTo( CrustChooserPanel.CRUST_AREA_MAX_WIDTH - rightWidth, topHeight );
-            }}.getGeneralPath(), topColor, stroke, strokePaint ) );
-
-            // right
-            addChild( new PhetPPath( new DoubleGeneralPath() {{
-                moveTo( CrustChooserPanel.CRUST_AREA_MAX_WIDTH - rightWidth, topHeight );
-                lineTo( CrustChooserPanel.CRUST_AREA_MAX_WIDTH, 0 );
-                lineTo( CrustChooserPanel.CRUST_AREA_MAX_WIDTH, height - topHeight );
-                lineTo( CrustChooserPanel.CRUST_AREA_MAX_WIDTH - rightWidth, height );
-            }}.getGeneralPath(), rightColor, stroke, strokePaint ) );
-
-            addInputEventListener( new LWJGLCursorHandler() );
-        }
+        guiNodes.add( 0, oldOceanicPiece );
     }
 
     private ImmutableVector2F getCrustOffset( ImmutableVector2F pieceOffset ) {
