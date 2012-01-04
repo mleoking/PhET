685a41769497025277fcda2165cbdc7f8734486b
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-01-04 05:05:55 +0000

    simplify the creation/deletion of precipitate particles

--- trunk/simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/model/PrecipitateParticle.java
+++ trunk/simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/model/PrecipitateParticle.java
@@ -3,7 +3,6 @@ package edu.colorado.phet.beerslawlab.model;
 
 import java.awt.Color;
 import java.awt.geom.Point2D;
-import java.util.ArrayList;
 
 /**
  * One particle that makes up the precipitate that forms on the bottom of the beaker.
@@ -12,21 +11,14 @@ import java.util.ArrayList;
  */
 public class PrecipitateParticle {
 
-    public interface PrecipitateParticleListener {
-        // Instructs listeners to remove the specified particle from the model.
-        public void particleDeleted( PrecipitateParticle particle );
-    }
-
     private final Point2D location; // location of the particle in the coordinate frame of the beaker
     private final Color color;
     private final double size;
-    private final ArrayList<PrecipitateParticleListener> listeners;
 
     public PrecipitateParticle( Point2D location, Color color, double size ) {
         this.location = location;
         this.color = color;
         this.size = size;
-        this.listeners = new ArrayList<PrecipitateParticleListener>();
     }
 
     public Point2D getLocation() {
@@ -39,24 +31,5 @@ public class PrecipitateParticle {
 
     public double getSize() {
         return size;
-    }
-
-    public void delete() {
-        fireParticleDeleted();
-    }
-
-    public void addListener( PrecipitateParticleListener listener ) {
-        listeners.add( listener );
-    }
-
-    private void removeListener( PrecipitateParticleListener listener ) {
-        listeners.remove( listener );
-    }
-
-    private void fireParticleDeleted() {
-        for ( PrecipitateParticleListener listener : new ArrayList<PrecipitateParticleListener>( listeners ) ) {
-            listener.particleDeleted( this );
-            removeListener( listener ); // automatically remove the listener, since the particle is being deleted
-        }
     }
 }
--- trunk/simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/view/PrecipitateNode.java
+++ trunk/simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/view/PrecipitateNode.java
@@ -1,14 +1,16 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.beerslawlab.view;
 
+import java.util.HashMap;
+
 import edu.colorado.phet.beerslawlab.model.Precipitate;
 import edu.colorado.phet.beerslawlab.model.Precipitate.PrecipitateListener;
 import edu.colorado.phet.beerslawlab.model.PrecipitateParticle;
-import edu.colorado.phet.beerslawlab.view.PrecipitateParticleNode.PrecipitateParticleNodeListener;
 import edu.umd.cs.piccolox.nodes.PComposite;
 
 /**
- * This node manages the precipitate that forms on the bottom of the beaker when the solution is saturated.
+ * Visual representation of the precipitate that forms on the bottom of the beaker when the solution is saturated.
+ * Manages the creation and deletion of precipitate particle nodes.
  * Origin is at bottom center of the beaker.
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
@@ -17,17 +19,23 @@ public class PrecipitateNode extends PComposite {
 
     public PrecipitateNode( Precipitate precipitate ) {
 
-        final PrecipitateParticleNodeListener nodeListener = new PrecipitateParticleNodeListener() {
-            public void removeNode( PrecipitateParticleNode node ) {
-                removeChild( node );
-            }
-        };
+        // Maps model elements to their corresponding nodes.
+        final HashMap<PrecipitateParticle, PrecipitateParticleNode> map = new HashMap<PrecipitateParticle, PrecipitateParticleNode>();
 
         precipitate.addListener( new PrecipitateListener() {
+
+            // A particle was added.
             public void particleAdded( PrecipitateParticle particle ) {
                 PrecipitateParticleNode particleNode = new PrecipitateParticleNode( particle );
-                particleNode.addListener( nodeListener );
+                map.put( particle, particleNode );
                 addChild( particleNode );
+            }
+
+            // A particle was removed.
+            public void particleRemoved( PrecipitateParticle particle ) {
+                PrecipitateParticleNode particleNode = map.get( particle );
+                map.remove( particle );
+                removeChild( particleNode );
             }
         } );
     }
