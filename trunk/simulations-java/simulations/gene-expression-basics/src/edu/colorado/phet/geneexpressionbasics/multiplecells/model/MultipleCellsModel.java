9bca9bc829ddf6f64b1dc20ff4f0ac7c15b77a2b
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-01-09 19:21:46 +0000

    Added "Reset All" to multiple cells tab.

--- trunk/simulations-java/simulations/gene-expression-basics/src/edu/colorado/phet/geneexpressionbasics/multiplecells/model/MultipleCellsModel.java
+++ trunk/simulations-java/simulations/gene-expression-basics/src/edu/colorado/phet/geneexpressionbasics/multiplecells/model/MultipleCellsModel.java
@@ -11,6 +11,7 @@ import java.util.Random;
 
 import edu.colorado.phet.common.phetcommon.math.MathUtil;
 import edu.colorado.phet.common.phetcommon.math.Vector2D;
+import edu.colorado.phet.common.phetcommon.model.Resettable;
 import edu.colorado.phet.common.phetcommon.model.clock.ClockAdapter;
 import edu.colorado.phet.common.phetcommon.model.clock.ClockEvent;
 import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
@@ -24,7 +25,7 @@ import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
  *
  * @author John Blanco
  */
-public class MultipleCellsModel {
+public class MultipleCellsModel implements Resettable {
 
     public static final int MAX_CELLS = 130;
     private static final Random RAND = new Random();
@@ -142,6 +143,12 @@ public class MultipleCellsModel {
         // Clear out all existing cells.
         visibleCellList.clear();
         cellList.clear();
+
+        // Reset all the cell control parameters.
+        transcriptionFactorLevel.reset();
+        proteinDegradationRate.reset();
+        transcriptionFactorAssociationProbability.reset();
+        polymeraseAssociationProbability.reset();
 
         // Add the max number of cells to the list of invisible cells.
         while ( cellList.size() < MAX_CELLS ) {
