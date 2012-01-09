6dd3bfec64bbbe6dde61bc82d0abf97a79e5bb7f
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-01-09 23:39:19 +0000

    Created a new module for sim-event-data-collection-server

--- trunk/util/sim-event-data-collection-server/src/edu/colorado/phet/simeventdatacollectionserver/StringServer.java
+++ trunk/util/sim-event-data-collection-server/src/edu/colorado/phet/simeventdatacollectionserver/StringServer.java
@@ -1,5 +1,5 @@
 // Copyright 2002-2011, University of Colorado
-package edu.colorado.phet.simsharing.eventserver;
+package edu.colorado.phet.simeventdatacollectionserver;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
