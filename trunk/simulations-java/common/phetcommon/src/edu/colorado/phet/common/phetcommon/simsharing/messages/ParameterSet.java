758a9ec938cc1bb5a6808c85708217a488dd101c
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-01-09 15:40:52 +0000

    Added ParameterSet, see #3209

---
+++ trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/simsharing/messages/ParameterSet.java
@@ -0,0 +1,50 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.common.phetcommon.simsharing.messages;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import edu.colorado.phet.common.phetcommon.simsharing.Parameter;
+
+/**
+ * Set of parameters used in sim sharing.  Key ordering is maintained.  Duplicate keys are not allowed,
+ * but setting the same key/value pair is allowed to permit flexibility in tricky sims.
+ * <p/>
+ * A ParameterSet is immutable, this makes it easier to use a more concise "builder"-like pattern in client code.
+ *
+ * @author Sam Reid
+ */
+public class ParameterSet {
+    private final ArrayList<ParameterKey> keys;
+    private final HashMap<ParameterKey, String> map;
+
+    public ParameterSet() {
+        this( new ArrayList<ParameterKey>(), new HashMap<ParameterKey, String>() );
+    }
+
+    public ParameterSet( ArrayList<ParameterKey> keys, HashMap<ParameterKey, String> map ) {
+        this.keys = keys;
+        this.map = map;
+    }
+
+    public ParameterSet add( final Parameter parameter ) {
+        if ( map.containsKey( parameter.name ) ) {
+            if ( !map.get( parameter.name ).equals( parameter.value ) ) {
+                throw new RuntimeException( "Parameter name already contained with different value: " + map.get( parameter.name ) + ", newValue = " + parameter.value );
+            }
+            else {
+                //Nothing to do, key and value already stored.  Do not re-add key to list or it will alter the original ordering.
+                return this;
+            }
+        }
+        else {
+            ArrayList<ParameterKey> newKeys = new ArrayList<ParameterKey>( keys ) {{
+                add( parameter.name );
+            }};
+            HashMap<ParameterKey, String> newMap = new HashMap<ParameterKey, String>( map ) {{
+                put( parameter.name, parameter.value );
+            }};
+            return new ParameterSet( newKeys, newMap );
+        }
+    }
+}
