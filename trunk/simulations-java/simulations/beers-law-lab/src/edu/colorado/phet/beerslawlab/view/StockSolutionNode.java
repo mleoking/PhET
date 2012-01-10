c4ba549208c96a3066aedb6fba758278c5c23339
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-01-10 01:54:25 +0000

    use an unlicensed placeholder for dropper that will have proper shape and prospective

--- trunk/simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/view/StockSolutionNode.java
+++ trunk/simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/view/StockSolutionNode.java
@@ -1,6 +1,7 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.beerslawlab.view;
 
+import java.awt.geom.Area;
 import java.awt.geom.Rectangle2D;
 
 import edu.colorado.phet.beerslawlab.model.Beaker;
@@ -10,6 +11,8 @@ import edu.colorado.phet.beerslawlab.model.Solution;
 import edu.colorado.phet.beerslawlab.model.Solvent;
 import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.phetcommon.util.RichSimpleObserver;
+import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
+import edu.colorado.phet.common.phetcommon.view.util.DoubleGeneralPath;
 import edu.umd.cs.piccolo.nodes.PPath;
 
 /**
@@ -18,9 +21,6 @@ import edu.umd.cs.piccolo.nodes.PPath;
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
 public class StockSolutionNode extends PPath {
-
-    // height of the glass portion of the dropper image, determines how far the solution extends up into the dropper.
-    private static final double GLASS_HEIGHT = 150;
 
     private final Solvent solvent;
     private final Property<Solute> solute;
@@ -45,21 +45,54 @@ public class StockSolutionNode extends PPath {
             }
         };
         observer.observe( solute, dropper.location, dropper.on );
+
+        dropper.visible.addObserver( new VoidFunction1<Boolean>() {
+            public void apply( Boolean visible ) {
+                setVisible( visible );
+            }
+        } );
     }
 
     private void updateNode() {
+
         // color
         setPaint( Solution.createColor( solvent, solute.get(), solute.get().stockSolutionConcentration ) );
-        // shape
+
+        // solution inside the dropper
+        DoubleGeneralPath solutionInsideDropper = new DoubleGeneralPath() {{
+            // These constants are specific to the dropper image file.
+            final double tipWidth = 16;
+            final double tipHeight = 10;
+            final double glassWidth = 50;
+            final double glassHeight = 150;
+            final double glassYOffset = tipHeight + 10;
+            moveTo( dropper.getX() - tipWidth / 2, dropper.getY() );
+            lineTo( dropper.getX() - tipWidth / 2, dropper.getY() - tipHeight );
+            lineTo( dropper.getX() - glassWidth / 2, dropper.getY() - glassYOffset );
+            lineTo( dropper.getX() - glassWidth / 2, dropper.getY() - glassHeight );
+            lineTo( dropper.getX() + glassWidth / 2, dropper.getY() - glassHeight );
+            lineTo( dropper.getX() + glassWidth / 2, dropper.getY() - glassYOffset );
+            lineTo( dropper.getX() + tipWidth / 2, dropper.getY() - tipHeight );
+            lineTo( dropper.getX() + tipWidth / 2, dropper.getY() );
+            closePath();
+        }};
+
+        // solution outside the dropper
+        Rectangle2D solutionOutsideDropper = null;
         if ( dropper.on.get() ) {
+
             double x = dropper.getX() - ( dropperHoleWidth / 2 );
-            double y = dropper.getY() - GLASS_HEIGHT;
+            double y = dropper.getY();
             double width = dropperHoleWidth;
-            double height = beaker.getY() - dropper.getY() + GLASS_HEIGHT;
-            setPathTo( new Rectangle2D.Double( x, y, width, height ) );
+            double height = beaker.getY() - dropper.getY();
+            solutionOutsideDropper = new Rectangle2D.Double( x, y, width, height );
         }
-        else {
-            setPathTo( new Rectangle2D.Double() ); // empty rectangle
+
+        // union of inside + outside
+        Area area = new Area( solutionInsideDropper.getGeneralPath() );
+        if ( solutionOutsideDropper != null ) {
+            area.add( new Area( solutionOutsideDropper ) );
         }
+        setPathTo( area );
     }
 }
