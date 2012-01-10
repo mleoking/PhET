a77850e164796f422988eea259db0a66d94e19d0
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-01-10 02:30:58 +0000

    Factored out FileStorage and added mongo-2.7.2

---
+++ util/sim-event-data-collection-server/src/edu/colorado/phet/simeventdatacollectionserver/MongoDBStorage.java
@@ -0,0 +1,10 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.simeventdatacollectionserver;
+
+/**
+ * Uses a MongoDB database for storing and retrieving the messages.
+ *
+ * @author Sam Reid
+ */
+public class MongoDBStorage {
+}
