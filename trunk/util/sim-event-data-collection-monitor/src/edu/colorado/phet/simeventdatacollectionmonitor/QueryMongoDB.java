c8ff899526e41c2379aae8748fb39abd8620ea23
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-01-10 16:33:14 +0000

    added sim-event-data-collection-monitor

--- trunk/util/sim-event-data-collection-monitor/src/edu/colorado/phet/simeventdatacollectionmonitor/QueryMongoDB.java
+++ trunk/util/sim-event-data-collection-monitor/src/edu/colorado/phet/simeventdatacollectionmonitor/QueryMongoDB.java
@@ -1,9 +1,17 @@
 // Copyright 2002-2011, University of Colorado
-package edu.colorado.phet.simeventdatacollectionserver;
+package edu.colorado.phet.simeventdatacollectionmonitor;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.UnknownHostException;
 import java.util.List;
 import java.util.Set;
+
+import javax.swing.SwingUtilities;
+import javax.swing.Timer;
+
+import edu.colorado.phet.simsharinganalysis.Entry;
+import edu.colorado.phet.simsharinganalysis.EntryJavaUtil;
 
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
@@ -17,15 +25,35 @@ import com.mongodb.Mongo;
  * @author Sam Reid
  */
 public class QueryMongoDB {
-    public QueryMongoDB() {
+    Mongo m;
+
+    public QueryMongoDB() throws UnknownHostException {
+        m = new Mongo();
     }
 
     public static void main( String[] args ) throws UnknownHostException {
-        new QueryMongoDB().start();
+        SwingUtilities.invokeLater( new Runnable() {
+            public void run() {
+                try {
+                    new QueryMongoDB().start();
+                }
+                catch ( UnknownHostException e ) {
+                    e.printStackTrace();
+                }
+            }
+        } );
     }
 
     private void start() throws UnknownHostException {
-        Mongo m = new Mongo();
+        //Run timer in swing thread for ease of integration with swing components.
+        new Timer( 1000, new ActionListener() {
+            public void actionPerformed( ActionEvent e ) {
+                process();
+            }
+        } ).start();
+    }
+
+    private void process() {
         List<String> names = m.getDatabaseNames();
         System.out.println( "names = " + names );
         for ( String machineID : names ) {
@@ -57,6 +85,8 @@ public class QueryMongoDB {
         String object = obj.get( "object" ).toString();
         String action = obj.get( "action" ).toString();
         DBObject parameters = (DBObject) obj.get( "parameters" );
-        System.out.println( "parameters = " + parameters );
+//        System.out.println( "parameters = " + parameters );
+        Entry e = EntryJavaUtil.EntryJavaUtil( Long.parseLong( time ), messageType, object, action, null );
+        System.out.println( e );
     }
 }
