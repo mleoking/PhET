36783bbc456a3c927e99b7e0725053e72f2be3da
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-01-11 21:44:02 +0000

    convert to SimSharingDragHandler

--- trunk/simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/view/MovableDragHandler.java
+++ trunk/simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/view/MovableDragHandler.java
@@ -7,13 +7,9 @@ import java.awt.geom.Rectangle2D;
 import edu.colorado.phet.beerslawlab.BLLSimSharing.Parameters;
 import edu.colorado.phet.beerslawlab.model.Movable;
 import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
-import edu.colorado.phet.common.phetcommon.simsharing.Parameter;
-import edu.colorado.phet.common.phetcommon.simsharing.SimSharingManager;
-import edu.colorado.phet.common.phetcommon.simsharing.messages.IUserAction;
 import edu.colorado.phet.common.phetcommon.simsharing.messages.IUserComponent;
 import edu.colorado.phet.common.phetcommon.simsharing.messages.ParameterSet;
-import edu.colorado.phet.common.phetcommon.util.function.Function0;
-import edu.colorado.phet.common.piccolophet.simsharing.SimSharingDragHandlerOld;
+import edu.colorado.phet.common.piccolophet.simsharing.SimSharingDragHandler;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.event.PInputEvent;
 
@@ -22,32 +18,16 @@ import edu.umd.cs.piccolo.event.PInputEvent;
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
-public class MovableDragHandler extends SimSharingDragHandlerOld {
+public class MovableDragHandler extends SimSharingDragHandler {
 
     private final Movable movable;
     private final PNode dragNode;
     private double clickXOffset, clickYOffset; // offset of mouse click from dragNode's origin, in parent's coordinate frame
 
     public MovableDragHandler( final IUserComponent userComponent, final Movable movable, PNode dragNode ) {
-        this( userComponent, movable, dragNode,
-              new Function0<Parameter[]>() {
-                  public Parameter[] apply() {
-                      return new Parameter[0];
-                  }
-              } );
-    }
-
-    public MovableDragHandler( final IUserComponent userComponent, final Movable movable, PNode dragNode, final Function0<Parameter[]> simSharingParametersFunction ) {
+        super( userComponent );
         this.movable = movable;
         this.dragNode = dragNode;
-        // sim-sharing
-        setStartEndDragFunction( new DragFunction() {
-            public void apply( IUserAction action, Parameter xParameter, Parameter yParameter, PInputEvent event ) {
-                SimSharingManager.sendUserMessage( userComponent, action,
-                                                   new ParameterSet( simSharingParametersFunction.apply() ).param( Parameters.locationX, (int) movable.location.get().getX() ).
-                                                           param( Parameters.locationY, (int) movable.location.get().getY() ) );
-            }
-        } );
     }
 
     @Override protected void startDrag( PInputEvent event ) {
@@ -62,8 +42,11 @@ public class MovableDragHandler extends SimSharingDragHandlerOld {
         Point2D pMouse = event.getPositionRelativeTo( dragNode.getParent() );
         double x = pMouse.getX() - clickXOffset;
         double y = pMouse.getY() - clickYOffset;
-        //TODO assumes a 1:1 model-view transform
-        movable.location.set( constrainToBounds( x, y, movable.getDragBounds() ) );
+        movable.location.set( constrainToBounds( x, y, movable.getDragBounds() ) ); // assumes a 1:1 model-view transform
+    }
+
+    @Override public ParameterSet getParametersForAllEvents() {
+        return super.getParametersForAllEvents().param( Parameters.locationX, (int) movable.location.get().getX() ).param( Parameters.locationY, (int) movable.location.get().getY() );
     }
 
     // Constrains xy coordinates to be within the specified bounds.
