66b216072c362b2451c68a8dec52284c00aaa008
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-01-12 22:06:37 +0000

    #3126, example of providing both mutators and subclassing solutions to customizing sim-sharing message parameters (for discussion)

--- trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/simsharing/components/SimSharingJCheckBox.java
+++ trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/simsharing/components/SimSharingJCheckBox.java
@@ -2,15 +2,24 @@
 package edu.colorado.phet.common.phetcommon.simsharing.components;
 
 import java.awt.event.ActionEvent;
+import java.util.ArrayList;
 
 import javax.swing.Action;
 import javax.swing.Icon;
 import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
 
+import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
+import edu.colorado.phet.common.phetcommon.simsharing.Parameter;
 import edu.colorado.phet.common.phetcommon.simsharing.SimSharingManager;
 import edu.colorado.phet.common.phetcommon.simsharing.messages.ComponentTypes;
 import edu.colorado.phet.common.phetcommon.simsharing.messages.IUserComponent;
 import edu.colorado.phet.common.phetcommon.simsharing.messages.ParameterKeys;
+import edu.colorado.phet.common.phetcommon.simsharing.messages.ParameterSet;
+import edu.colorado.phet.common.phetcommon.simsharing.messages.UserComponentId;
+import edu.colorado.phet.common.phetcommon.util.function.Function0;
 
 import static edu.colorado.phet.common.phetcommon.simsharing.Parameter.componentType;
 import static edu.colorado.phet.common.phetcommon.simsharing.messages.UserActions.pressed;
@@ -23,6 +32,9 @@ import static edu.colorado.phet.common.phetcommon.simsharing.messages.UserAction
 public class SimSharingJCheckBox extends JCheckBox {
 
     private final IUserComponent userComponent;
+
+    // By default there are no custom parameters
+    private ArrayList<Function0<ParameterSet>> customParameterFunctions = new ArrayList<Function0<ParameterSet>>();
 
     public SimSharingJCheckBox( IUserComponent userComponent ) {
         this.userComponent = userComponent;
@@ -64,9 +76,59 @@ public class SimSharingJCheckBox extends JCheckBox {
     }
 
     @Override protected void fireActionPerformed( ActionEvent event ) {
-        SimSharingManager.sendUserMessage( userComponent,
-                                           pressed,
-                                           componentType( ComponentTypes.checkBox ).param( ParameterKeys.isSelected, isSelected() ) );
+        SimSharingManager.sendUserMessage( userComponent, pressed, getParameters() );
         super.fireActionPerformed( event );
+    }
+
+    // Gets parameters. Custom parameters are added following standard parameters.
+    private ParameterSet getParameters() {
+        return componentType( ComponentTypes.checkBox ).param( ParameterKeys.isSelected, isSelected() ).addAll( getCustomParameters() );
+    }
+
+    // Override this is you want to add custom parameters via subclassing.
+    protected ParameterSet getCustomParameters() {
+        ParameterSet parameterSet = new ParameterSet();
+        for ( Function0<ParameterSet> function : customParameterFunctions ) {
+            parameterSet.addAll( function.apply() );
+        }
+        return parameterSet;
+    }
+
+    // Use this if you want to add custom parameters, but can't use subclassing.
+    public void addCustomParametersFunction( Function0<ParameterSet> function ) {
+        customParameterFunctions.add( function );
+    }
+
+    // demonstrate usage
+    public static void main( String[] args ) {
+
+        String[] myArgs = { "-study" };
+        SimSharingManager.init( new PhetApplicationConfig( myArgs, "myProject" ) );
+
+        // check box that uses subclassing to provide custom parameters
+        final JCheckBox checkBox1 = new SimSharingJCheckBox( new UserComponentId( "checkBox1" ), "subclassing" ) {
+            @Override protected ParameterSet getCustomParameters() {
+                return Parameter.param( ParameterKeys.text, "I use subclassing." );
+            }
+        };
+
+        // check box that uses mutation to provide custom parameters
+        final JCheckBox checkBox2 = new SimSharingJCheckBox( new UserComponentId( "checkBox2" ), "mutation" ) {{
+            addCustomParametersFunction( new Function0<ParameterSet>() {
+                public ParameterSet apply() {
+                    return Parameter.param( ParameterKeys.text, "I use mutation." );
+                }
+            } );
+        }};
+
+        JFrame frame = new JFrame() {{
+            setContentPane( new JPanel() {{
+                add( checkBox1 );
+                add( checkBox2 );
+            }} );
+            pack();
+            setDefaultCloseOperation( WindowConstants.EXIT_ON_CLOSE );
+        }};
+        frame.setVisible( true );
     }
 }
