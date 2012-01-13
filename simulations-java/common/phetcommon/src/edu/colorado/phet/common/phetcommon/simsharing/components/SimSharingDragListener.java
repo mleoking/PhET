4bc60b10ee879fd646e080ca12c632aea0d01382
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-01-13 18:36:08 +0000

    Added componentType constructor arg to SimSharingDragListener, so that it has similar interface to SimSharingDragHandler

--- simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/simsharing/components/SimSharingDragListener.java
+++ simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/simsharing/components/SimSharingDragListener.java
@@ -7,6 +7,7 @@ import java.awt.geom.Point2D;
 
 import edu.colorado.phet.common.phetcommon.simsharing.Parameter;
 import edu.colorado.phet.common.phetcommon.simsharing.SimSharingManager;
+import edu.colorado.phet.common.phetcommon.simsharing.messages.IComponentType;
 import edu.colorado.phet.common.phetcommon.simsharing.messages.IUserAction;
 import edu.colorado.phet.common.phetcommon.simsharing.messages.IUserComponent;
 import edu.colorado.phet.common.phetcommon.simsharing.messages.ParameterKeys;
@@ -31,18 +32,20 @@ public class SimSharingDragListener extends MouseAdapter {
     }
 
     protected final IUserComponent userComponent;
+    private final IComponentType componentType;
     private final SimSharingDragPoints dragPoints; // mouse coordinates, accumulated during a drag sequence
     private DragFunction startDragFunction, dragFunction, endDragFunction; // functions called for various events
 
     // Sends a message on startDrag and endDrag, but not drag
-    public SimSharingDragListener( IUserComponent userComponent ) {
-        this( userComponent, false );
+    public SimSharingDragListener( IUserComponent userComponent, IComponentType componentType ) {
+        this( userComponent, componentType, false );
     }
 
     // Sends a message on drag if reportDrag=true
-    public SimSharingDragListener( IUserComponent userComponent, final boolean sendDragMessages ) {
+    public SimSharingDragListener( IUserComponent userComponent, IComponentType componentType, final boolean sendDragMessages ) {
 
         this.userComponent = userComponent;
+        this.componentType = componentType;
         this.dragPoints = new SimSharingDragPoints();
 
         // default functions
@@ -117,7 +120,7 @@ public class SimSharingDragListener extends MouseAdapter {
 
     // Return parameters that are used by default for all events
     protected ParameterSet getParametersForAllEvents( MouseEvent event ) {
-        return new ParameterSet().add( getXParameter( event ) ).add( getYParameter( event ) );
+        return Parameter.componentType( componentType ).add( getXParameter( event ) ).add( getYParameter( event ) );
     }
 
     private void addDragPoint( MouseEvent event ) {
