2858b5fea1180ff21d9de095c3c0f76207a06a84
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-01-14 03:55:31 +0000

    added to report

--- trunk/util/sim-event-data-collection-analysis/src/edu/colorado/phet/simsharinganalysis/scripts/AcidBaseSolutionSpring2012Analysis.scala
+++ trunk/util/sim-event-data-collection-analysis/src/edu/colorado/phet/simsharinganalysis/scripts/AcidBaseSolutionSpring2012Analysis.scala
@@ -14,7 +14,14 @@ object AcidBaseSolutionSpring2012Analysis extends App {
     println("How many times pressed tabs: " + log.filter(_.componentType == "tab").length)
     val tabs = List("introductionTab", "customSolutionTab")
     println("How many events in each tab: " + tabs.map(t => t + "=" + log.selectTab(tabs, t).length))
-    println("Number of tabs visited: " + log.entries.map(e => log.getTabComponent(e, tabs(0))).distinct.length)
-    log.entries.map(entry => log.getTabComponent(entry, "introductionTab") + " \t " + entry).foreach(println)
+    println("Number of tabs visited: " + log.entries.map(log.getTabComponent(_, tabs(0))).distinct.length)
+    val nonInteractiveEvents = log.entries.filter(entry => entry.messageType == "user" && entry.interactive == "false")
+    println("Number of events on non-interactive components: " + nonInteractiveEvents.length)
+    println("Number of distinct non-interacive components that the user tried to interact with: " + nonInteractiveEvents.map(_.component).distinct.length)
+    println("Entries for non-interactive components:")
+    nonInteractiveEvents.foreach(println)
+
+    //Print the log augmented with tab annotations
+    //log.entries.map(entry => log.getTabComponent(entry, "introductionTab") + " \t " + entry).foreach(println)
   }
 }
