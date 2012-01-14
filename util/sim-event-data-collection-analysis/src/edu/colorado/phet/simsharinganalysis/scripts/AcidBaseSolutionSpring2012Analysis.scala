d8ce59261e9b0dcdbba0faf25bbf2414a463fa2f
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-01-14 03:58:02 +0000

    improved console output

--- util/sim-event-data-collection-analysis/src/edu/colorado/phet/simsharinganalysis/scripts/AcidBaseSolutionSpring2012Analysis.scala
+++ util/sim-event-data-collection-analysis/src/edu/colorado/phet/simsharinganalysis/scripts/AcidBaseSolutionSpring2012Analysis.scala
@@ -7,7 +7,8 @@ object AcidBaseSolutionSpring2012Analysis extends App {
   val logs = phet.load("C:\\Users\\Sam\\Desktop\\friday-13th-logs").sortBy(_.startTime)
   println("found: " + logs.length + " logs")
   for ( log <- logs ) {
-    println("session: " + log.session + ", minutes of interaction=" + log.minutesUsed + ", numUserEvents=" + log.userEntries.size)
+    println("session: " + log.session)
+    println("minutes of interaction=" + log.minutesUsed + ", numUserEvents=" + log.userEntries.size)
     println("num user events per minute: " + log.userEntries.size / log.minutesUsed)
     println("How many times pressed the showSolventCheckBox: " + log.filter(_.component == "showSolventCheckBox").length)
     println("How many times dunked the phMeter: " + log.filter(_.component == "phMeter").filter(_.hasParameter("isInSolution", "true")).filter(_.action == "drag").length)
