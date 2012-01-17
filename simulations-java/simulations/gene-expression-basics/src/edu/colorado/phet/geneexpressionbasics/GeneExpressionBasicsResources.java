6ed58c86f2f1f1ee73b1c5c70537bfb64e6ef80e
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-01-17 17:53:37 +0000

    Added a new image to the resources.

--- simulations-java/simulations/gene-expression-basics/src/edu/colorado/phet/geneexpressionbasics/GeneExpressionBasicsResources.java
+++ simulations-java/simulations/gene-expression-basics/src/edu/colorado/phet/geneexpressionbasics/GeneExpressionBasicsResources.java
@@ -20,6 +20,7 @@ public class GeneExpressionBasicsResources {
 
     //Images
     public static class Images {
+        public static final BufferedImage ECOLI = RESOURCES.getImage( "ecoli.jpg" );
         public static final BufferedImage ECOLI_IMAGE = RESOURCES.getImage( "ecoli_image.gif" );
         public static final BufferedImage GRAY_ARROW = RESOURCES.getImage( "gray-arrow.png" );
         public static final BufferedImage SECOND_TAB_STATIC_PICTURE = RESOURCES.getImage( "second-tab-static-picture.png" );
