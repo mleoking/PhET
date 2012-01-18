2a42c0c0ee41f11a66e4d66f8065652775eab59e
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-01-18 04:17:19 +0000

    added SimSharingLinearValueControl (implementation identical to SimSharingLogarithmicValueControl! bad!)

---
+++ trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/view/controls/valuecontrol/SimSharingLinearValueControl.java
@@ -0,0 +1,56 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.common.phetcommon.view.controls.valuecontrol;
+
+import edu.colorado.phet.common.phetcommon.simsharing.messages.IParameterValue;
+import edu.colorado.phet.common.phetcommon.simsharing.messages.IUserComponent;
+import edu.colorado.phet.common.phetcommon.simsharing.messages.ParameterKeys;
+
+import static edu.colorado.phet.common.phetcommon.simsharing.Parameter.param;
+import static edu.colorado.phet.common.phetcommon.simsharing.SimSharingManager.sendUserMessage;
+import static edu.colorado.phet.common.phetcommon.simsharing.messages.ParameterKeys.value;
+import static edu.colorado.phet.common.phetcommon.simsharing.messages.UserActions.*;
+
+/**
+ * Linear value control that sends sim-sharing messages.
+ *
+ * @author Sam Reid
+ */
+public class SimSharingLinearValueControl extends LinearValueControl {
+    private final IUserComponent userComponent;
+
+    public SimSharingLinearValueControl( IUserComponent userComponent, double min, double max, String label, String textFieldPattern, String units ) {
+        super( min, max, label, textFieldPattern, units );
+        this.userComponent = userComponent;
+    }
+
+    public SimSharingLinearValueControl( IUserComponent userComponent, double min, double max, String label, String textFieldPattern, String units, ILayoutStrategy layoutStrategy ) {
+        super( min, max, label, textFieldPattern, units, layoutStrategy );
+        this.userComponent = userComponent;
+    }
+
+    @Override protected void sliderStartDrag( double modelValue ) {
+        sendUserMessage( userComponent, startDrag, param( value, modelValue ) );
+        super.sliderStartDrag( modelValue );
+    }
+
+    @Override protected void sliderEndDrag( double modelValue ) {
+        sendUserMessage( userComponent, endDrag, param( value, modelValue ) );
+        super.sliderEndDrag( modelValue );
+    }
+
+    @Override protected void sliderDrag( double modelValue ) {
+        sendUserMessage( userComponent, drag, param( value, modelValue ) );
+        super.sliderDrag( modelValue );
+    }
+
+    @Override protected void textFieldCommitted( IParameterValue commitAction, double value ) {
+        sendUserMessage( userComponent, textFieldCommitted, param( ParameterKeys.commitAction, commitAction ).param( ParameterKeys.value, value ) );
+        super.textFieldCommitted( commitAction, value );
+    }
+
+    //TODO this should probably be a system or model message
+    @Override protected void textFieldCorrected( IParameterValue errorType, String value, double correctedValue ) {
+        sendUserMessage( userComponent, textFieldCorrected, param( ParameterKeys.errorType, errorType ).param( ParameterKeys.value, value ).param( ParameterKeys.correctedValue, correctedValue ) );
+        super.textFieldCorrected( errorType, value, correctedValue );
+    }
+}
