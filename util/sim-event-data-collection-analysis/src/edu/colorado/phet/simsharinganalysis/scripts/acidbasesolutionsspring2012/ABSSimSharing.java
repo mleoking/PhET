38bce1416f5cef531145ea853ded01510b2d34ce
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-01-18 23:56:24 +0000

    improved session output

---
+++ util/sim-event-data-collection-analysis/src/edu/colorado/phet/simsharinganalysis/scripts/acidbasesolutionsspring2012/ABSSimSharing.java
@@ -0,0 +1,30 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.simsharinganalysis.scripts.acidbasesolutionsspring2012;
+
+import edu.colorado.phet.common.phetcommon.simsharing.messages.IParameterKey;
+import edu.colorado.phet.common.phetcommon.simsharing.messages.IUserComponent;
+
+/**
+ * Sim-sharing enums that are specific to this sim.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
+public class ABSSimSharing {
+
+    public static enum UserComponents implements IUserComponent {
+        battery, concentrationControl, concentrationGraph,
+        lightBulb, magnifyingGlass, phMeter, phPaper,
+        reactionEquation, weakStrengthControl, phMeterRadioButton, phPaperRadioButton,
+        phMeterIcon, phPaperIcon, conductivityTesterIcon, magnifyingGlassRadioButton,
+        concentrationGraphRadioButton, liquidRadioButton, showSolventCheckBox, magnifyingGlassIcon,
+        waterIcon, concentrationGraphIcon, liquidIcon, phColorKey, acidRadioButton, baseRadioButton,
+        weakRadioButton, strongRadioButton, waterRadioButton, strongAcidRadioButton, weakAcidRadioButton,
+        strongBaseRadioButton, weakBaseRadioButton, strongAcidIcon, weakAcidIcon, strongBaseIcon,
+        weakBaseIcon, conductivityTesterRadioButton,
+        customSolutionTab, introductionTab
+    }
+
+    public static enum ParameterKeys implements IParameterKey {
+        handle, isCircuitCompleted, isInSolution, isPaperAlignedWithColorKey, lens, molecule
+    }
+}
--- util/sim-event-data-collection-analysis/src/edu/colorado/phet/simsharinganalysis/util/GrowingTable.scala
+++ util/sim-event-data-collection-analysis/src/edu/colorado/phet/simsharinganalysis/util/GrowingTable.scala
@@ -2,6 +2,7 @@
 package edu.colorado.phet.simsharinganalysis.util
 
 import collection.mutable.HashMap
+import java.text.DecimalFormat
 
 /**
  * This table returns 0 for nonexistent keys and can add values for nonexistent keys
@@ -17,4 +18,9 @@ class GrowingTable {
   def apply(s: String) = if ( t.contains(s) ) t(s) else 0L
 
   override def toString = t.toString
+
+  def toMinuteMap = {
+    val format = new DecimalFormat("0.00")
+    t.map(elm => elm._1 -> format.format(elm._2 / 1000.0 / 60.0))
+  }
 }
