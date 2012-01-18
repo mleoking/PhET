bc5acffa73085e533e7537dbe3f209f809052b4d
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-01-18 20:47:40 +0000

    created a text area for output

--- trunk/util/sim-event-data-collection-analysis/src/edu/colorado/phet/simsharinganalysis/gui/StudentActivityPanel.scala
+++ trunk/util/sim-event-data-collection-analysis/src/edu/colorado/phet/simsharinganalysis/gui/StudentActivityPanel.scala
@@ -5,6 +5,7 @@ import javax.swing.{JTextArea, JScrollPane, JPanel}
 import java.io.File
 import java.awt.{Dimension, BorderLayout}
 import edu.colorado.phet.simsharinganalysis.scripts.AcidBaseSolutionSpring2012AnalysisReport
+import edu.colorado.phet.simsharinganalysis.util.MyStringBuffer
 
 
 /**
@@ -14,19 +15,11 @@ import edu.colorado.phet.simsharinganalysis.scripts.AcidBaseSolutionSpring2012An
 class StudentActivityPanel(dir: File) extends JPanel(new BorderLayout) {
   add(new StudentActivityCanvas(dir.getAbsolutePath), BorderLayout.CENTER)
 
-  class MyStringBuffer {
-    var buffer = ""
-
-    def println(s: String) {
-      buffer = buffer + s + "\n"
-    }
-  }
-
   val myBuffer = new MyStringBuffer
   val abs = AcidBaseSolutionSpring2012AnalysisReport.report(dir, myBuffer.println)
 
-  println("my buffer = " + myBuffer.buffer)
-  add(new JScrollPane(new JTextArea(myBuffer.buffer) {
+  println("my buffer = " + myBuffer)
+  add(new JScrollPane(new JTextArea(myBuffer.toString) {
     setEditable(false)
   }) {
     setPreferredSize(new Dimension(800, 300))
