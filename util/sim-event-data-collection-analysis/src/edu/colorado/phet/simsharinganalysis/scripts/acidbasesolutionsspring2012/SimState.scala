f442ccce8ca7ab1dbfc55210e7a0a07ca9638260
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-01-21 02:28:22 +0000

    improved parser and state machine

--- util/sim-event-data-collection-analysis/src/edu/colorado/phet/simsharinganalysis/scripts/acidbasesolutionsspring2012/SimState.scala
+++ util/sim-event-data-collection-analysis/src/edu/colorado/phet/simsharinganalysis/scripts/acidbasesolutionsspring2012/SimState.scala
@@ -5,7 +5,7 @@ import edu.colorado.phet.simsharinganalysis.Entry
 import Globals._
 
 object SimState {
-  def apply() = SimState(0, initialTab0, initialTab1)
+  def apply(): SimState = SimState(0, initialTab0, initialTab1)
 }
 
 case class SimState(selectedTab: Int, tab0: Tab0, tab1: Tab1) {
@@ -19,49 +19,10 @@ case class SimState(selectedTab: Int, tab0: Tab0, tab1: Tab1) {
   def displayedTest: String = getSelectedTab.test
 }
 
-abstract class AcidBaseSolutionsTab {
+trait AcidBaseSolutionsTab {
+  def viewAndTestState: ViewAndTestState
+
   def solution: String
-
-  def view: String
-
-  def test: String
-}
-
-//ShowSolvent indicates that the check box is checked, but solvent only showing if view is also "molecules"
-case class Tab0(solution: String, viewAndTestState: ViewAndTestState) extends AcidBaseSolutionsTab {
-
-  def next(e: Entry): Tab0 = {
-
-    e match {
-
-      //If clicking on something disabled, then do not change the state, see #3218
-      case x: Entry if x.enabled == false => this
-
-      //Watch which solution the user selects
-      case Entry(_, "user", c, _, "pressed", _) if List("waterRadioButton", "waterIcon").contains(c) => copy(solution = water)
-      case Entry(_, "user", c, _, "pressed", _) if List("strongAcidRadioButton", "strongAcidIcon").contains(c) => copy(solution = strongAcid)
-      case Entry(_, "user", c, _, "pressed", _) if List("weakAcidRadioButton", "weakAcidIcon").contains(c) => copy(solution = weakAcid)
-      case Entry(_, "user", c, _, "pressed", _) if List("strongBaseRadioButton", "strongBaseIcon").contains(c) => copy(solution = strongBase)
-      case Entry(_, "user", c, _, "pressed", _) if List("weakBaseRadioButton", "weakBaseIcon").contains(c) => copy(solution = weakBase)
-
-      //Watch which view the user selects
-      case Entry(_, "user", c, _, "pressed", _) if List("magnifyingGlassRadioButton", "magnifyingGlassIcon").contains(c) => copy(view = molecules)
-      case Entry(_, "user", c, _, "pressed", _) if List("concentrationGraphRadioButton", "concentrationGraphIcon").contains(c) => copy(view = barGraph)
-      case Entry(_, "user", c, _, "pressed", _) if List("liquidRadioButton", "liquidIcon").contains(c) => copy(view = liquid)
-
-      //See if the user changed tests
-      case Entry(_, "user", c, _, "pressed", _) if List("phMeterRadioButton", "phMeterIcon").contains(c) => copy(test = phMeter)
-      case Entry(_, "user", c, _, "pressed", _) if List("phPaperRadioButton", "phPaperIcon").contains(c) => copy(test = phPaper)
-      case Entry(_, "user", c, _, "pressed", _) if List("conductivityTesterRadioButton", "conductivityTesterIcon").contains(c) => copy(test = conductivityTester)
-
-      //Handle reset all presses
-      case Entry(_, "user", "resetAllConfirmationDialogYesButton", _, "pressed", _) => initialTab0
-
-      //Nothing happened to change the state
-      case _ => this
-
-    }
-  }
 
   def view = viewAndTestState.view
 
@@ -70,6 +31,9 @@ case class Tab0(solution: String, viewAndTestState: ViewAndTestState) extends Ac
 
 //Factored out state that is shared between all tabs, since case classes do not inherit well.
 case class ViewAndTestState(view: String, test: String, somethingEnabled: Boolean) {
+  assert(view != null)
+  assert(test != null)
+
   def next(e: Entry) = {
 
     e match {
@@ -93,13 +57,40 @@ case class ViewAndTestState(view: String, test: String, somethingEnabled: Boolea
   }
 }
 
+//ShowSolvent indicates that the check box is checked, but solvent only showing if view is also "molecules"
+case class Tab0(solution: String, viewAndTestState: ViewAndTestState) extends AcidBaseSolutionsTab {
+
+  def next(e: Entry): Tab0 = {
+
+    val updated = e match {
+
+      //If clicking on something disabled, then do not change the state, see #3218
+      case x: Entry if x.enabled == false => this
+
+      //Watch which solution the user selects
+      case Entry(_, "user", c, _, "pressed", _) if List("waterRadioButton", "waterIcon").contains(c) => copy(solution = water)
+      case Entry(_, "user", c, _, "pressed", _) if List("strongAcidRadioButton", "strongAcidIcon").contains(c) => copy(solution = strongAcid)
+      case Entry(_, "user", c, _, "pressed", _) if List("weakAcidRadioButton", "weakAcidIcon").contains(c) => copy(solution = weakAcid)
+      case Entry(_, "user", c, _, "pressed", _) if List("strongBaseRadioButton", "strongBaseIcon").contains(c) => copy(solution = strongBase)
+      case Entry(_, "user", c, _, "pressed", _) if List("weakBaseRadioButton", "weakBaseIcon").contains(c) => copy(solution = weakBase)
+
+      //Handle reset all presses
+      case Entry(_, "user", "resetAllConfirmationDialogYesButton", _, "pressed", _) => initialTab0
+
+      //Nothing happened to change the state
+      case _ => this
+
+    }
+
+    updated.copy(viewAndTestState = viewAndTestState.next(e))
+  }
+}
+
 case class Tab1(viewAndTestState: ViewAndTestState, acid: Boolean, weak: Boolean) extends AcidBaseSolutionsTab {
 
   def next(e: Entry): Tab1 = {
 
-    copy(viewAndTestState = viewAndTestState.next(e))
-
-    e match {
+    val updated = e match {
 
       //If clicking on something disabled, then do not change the state, see #3218
       case x: Entry if x.enabled == false => this
@@ -116,6 +107,8 @@ case class Tab1(viewAndTestState: ViewAndTestState, acid: Boolean, weak: Boolean
       //Nothing happened to change the state
       case _ => this
     }
+
+    updated.copy(viewAndTestState = viewAndTestState.next(e))
   }
 
   def solution = {
@@ -124,8 +117,4 @@ case class Tab1(viewAndTestState: ViewAndTestState, acid: Boolean, weak: Boolean
     else if ( !weak && acid ) strongAcid
     else strongBase
   }
-
-  def view = viewAndTestState.view
-
-  def test = viewAndTestState.test
 }
