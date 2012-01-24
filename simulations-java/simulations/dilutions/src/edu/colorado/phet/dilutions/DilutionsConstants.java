244f962ed6c6edb7bdd2b4b099b967a0f9a5852f
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-01-24 22:28:53 +0000

    factor out constants for various font sizes and styles (labels, units, ranges, values)

---
+++ simulations-java/simulations/dilutions/src/edu/colorado/phet/dilutions/DilutionsConstants.java
@@ -0,0 +1,19 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.dilutions;
+
+import java.awt.Font;
+
+import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
+
+/**
+ * Constants used throughout this sim.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
+public class DilutionsConstants {
+
+    public static final PhetFont SLIDER_LABEL_FONT = new PhetFont( Font.BOLD, 16 );
+    public static final PhetFont SLIDER_UNITS_FONT = new PhetFont( Font.PLAIN, 14 );
+    public static final PhetFont SLIDER_RANGE_FONT = new PhetFont( Font.PLAIN, 14 );
+    public static final PhetFont SLIDER_VALUE_FONT = new PhetFont( Font.PLAIN, 16 );
+}
