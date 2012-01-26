452e0f8c2842ba570bddf1f69d06c5a19504fd97
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-01-26 00:43:33 +0000

    pt: earth texturing experiment

--- trunk/simulations-java/simulations/plate-tectonics/src/edu/colorado/phet/platetectonics/view/materials/EarthTexture.java
+++ trunk/simulations-java/simulations/plate-tectonics/src/edu/colorado/phet/platetectonics/view/materials/EarthTexture.java
@@ -1,40 +1,52 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.platetectonics.view.materials;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.Random;
+
+import javax.imageio.ImageIO;
 
 import org.lwjgl.BufferUtils;
 import org.lwjgl.util.glu.GLU;
 
 import edu.colorado.phet.lwjglphet.utils.GLDisplayList;
 
+import static edu.colorado.phet.platetectonics.PlateTectonicsResources.RESOURCES;
 import static org.lwjgl.opengl.GL11.*;
 
 public class EarthTexture {
     private static final int WIDTH = 512;
     private static final int HEIGHT = 512;
 
-    private static final Random random = new Random( 2501 );
+    private static final Random random = new Random( 250134503L );
 
     private static final GLDisplayList textureAction;
 
     static {
         textureAction = new GLDisplayList( new Runnable() {
             public void run() {
-                ByteBuffer buffer = BufferUtils.createByteBuffer( WIDTH * HEIGHT * 3 );
-                for ( int i = 0; i < buffer.capacity(); i += 3 ) {
-                    final byte b = (byte) ( random.nextInt() % 256 );
-                    buffer.put( new byte[] { b, b, b } );
+                ByteBuffer buffer = BufferUtils.createByteBuffer( WIDTH * HEIGHT * 4 );
+                try {
+                    BufferedImage image = ImageIO.read( RESOURCES.getResourceAsStream( "images/textures/noise.png" ) );
+                    byte data[] = (byte[]) image.getRaster().getDataElements( 0, 0, image.getWidth(), image.getHeight(), null );
+                    for ( int i = 0; i < data.length; i += 4 ) {
+                        data[i] = (byte) ( data[i] & 0x3F | 0xC0 );
+                        data[i + 1] = (byte) ( data[i + 1] & 0x3F | 0xC0 );
+                        data[i + 2] = (byte) ( data[i + 2] & 0x3F | 0xC0 );
+                        data[i + 3] = (byte) ( 0xFF );
+                    }
+                    buffer.clear();
+                    buffer.put( data, 0, data.length );
+                }
+                catch ( IOException e ) {
+                    throw new RuntimeException( "failure to read noise file", e );
                 }
                 buffer.rewind();
 
                 glTexEnvf( GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE );
-
-                glTexEnvf( GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE );
-
                 glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
-//                         GL_NEAREST );
                                  GL_LINEAR_MIPMAP_NEAREST );
                 glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
 
@@ -42,7 +54,7 @@ public class EarthTexture {
                 glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT );
                 glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT );
 
-                GLU.gluBuild2DMipmaps( GL_TEXTURE_2D, 3, WIDTH, HEIGHT, GL_RGB, GL_UNSIGNED_BYTE, buffer );
+                GLU.gluBuild2DMipmaps( GL_TEXTURE_2D, 4, WIDTH, HEIGHT, GL_RGBA, GL_UNSIGNED_BYTE, buffer );
             }
         } );
 
