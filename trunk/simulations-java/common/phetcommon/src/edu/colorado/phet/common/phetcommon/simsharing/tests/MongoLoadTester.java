82bd2a5ffef8f05d0d5d00c40248fcfcd5378eb9
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-01-28 02:43:23 +0000

    Added a load tester

--- trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/simsharing/tests/MongoLoadTester.java
+++ trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/simsharing/tests/MongoLoadTester.java
@@ -1,14 +1,7 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.common.phetcommon.simsharing.tests;
 
-import java.net.UnknownHostException;
-
-import edu.colorado.phet.common.phetcommon.simsharing.logs.MongoLog;
-
-import com.mongodb.BasicDBObject;
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
-import com.mongodb.Mongo;
+import java.io.IOException;
 
 /**
  * Runs lots of threads which all send some data to the mongo server--helps us to see what kind of load MongoDB can support on our hardware/network configuration.
@@ -18,33 +11,40 @@ import com.mongodb.Mongo;
 public class MongoLoadTester {
 
     private static final int TIME_BETWEEN_MESSAGES_MILLIS = 1000;
-    private static final int NUM_CLIENTS = 100;
+    private static final int NUM_CLIENTS = 30;
 
     public static void main( String[] args ) {
         for ( int i = 0; i < NUM_CLIENTS; i++ ) {
             final int finalI = i;
             new Thread( new Runnable() {
                 public void run() {
-                    Mongo m = null;
+
                     try {
-                        m = new Mongo( MongoLog.HOST_IP_ADDRESS, MongoLog.PORT );
+                        Process p = Runtime.getRuntime().exec( new String[] { "java", "-jar", "C:\\workingcopy\\phet\\svn\\trunk\\simulations-java\\simulations\\acid-base-solutions\\deploy\\acid-base-solutions_all.jar" } );
                     }
-                    catch ( UnknownHostException e ) {
+                    catch ( IOException e ) {
                         e.printStackTrace();
                     }
-                    DB db = m.getDB( "loadtester" );
-                    DBCollection collection = db.getCollection( "loadtesterCollection" );
-                    while ( true ) {
-                        try {
-                            collection.insert( new BasicDBObject() {{
-                                put( "index", finalI );
-                            }} );
-                            Thread.sleep( TIME_BETWEEN_MESSAGES_MILLIS );
-                        }
-                        catch ( InterruptedException e ) {
-                            e.printStackTrace();
-                        }
-                    }
+//                    Mongo m = null;
+//                    try {
+//                        m = new Mongo( MongoLog.HOST_IP_ADDRESS, MongoLog.PORT );
+//                    }
+//                    catch ( UnknownHostException e ) {
+//                        e.printStackTrace();
+//                    }
+//                    DB db = m.getDB( "loadtester" );
+//                    DBCollection collection = db.getCollection( "loadtesterCollection" );
+//                    while ( true ) {
+//                        try {
+//                            collection.insert( new BasicDBObject() {{
+//                                put( "index", finalI );
+//                            }} );
+//                            Thread.sleep( TIME_BETWEEN_MESSAGES_MILLIS );
+//                        }
+//                        catch ( InterruptedException e ) {
+//                            e.printStackTrace();
+//                        }
+//                    }
                 }
             } ).start();
         }
---
+++ trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/simsharing/tests/MongoLoadTesterSimLauncher.java
@@ -0,0 +1,30 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.common.phetcommon.simsharing.tests;
+
+import java.io.IOException;
+
+/**
+ * Launch a sim several times to make sure the server can accommodate lots of sessions.
+ *
+ * @author Sam Reid
+ */
+public class MongoLoadTesterSimLauncher {
+
+    private static final int NUM_CLIENTS = 30;
+
+    public static void main( String[] args ) {
+        for ( int i = 0; i < NUM_CLIENTS; i++ ) {
+            new Thread( new Runnable() {
+                public void run() {
+                    try {
+                        //I tested with a sim I built that has only one flavor, which is a study flavor
+                        Runtime.getRuntime().exec( new String[] { "java", "-jar", "C:\\workingcopy\\phet\\svn\\trunk\\simulations-java\\simulations\\acid-base-solutions\\deploy\\acid-base-solutions_all.jar" } );
+                    }
+                    catch ( IOException e ) {
+                        e.printStackTrace();
+                    }
+                }
+            } ).start();
+        }
+    }
+}
