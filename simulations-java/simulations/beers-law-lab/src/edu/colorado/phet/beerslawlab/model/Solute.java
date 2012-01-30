2438dc9107175648b6f23dc824e01813e22a60b7
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-01-30 17:49:53 +0000

    subclasses for specific solutes

--- simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/model/Solute.java
+++ simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/model/Solute.java
@@ -3,6 +3,8 @@ package edu.colorado.phet.beerslawlab.model;
 
 import java.awt.Color;
 
+import edu.colorado.phet.beerslawlab.BLLResources.Strings;
+import edu.colorado.phet.beerslawlab.BLLResources.Symbols;
 import edu.colorado.phet.common.phetcommon.util.ColorRange;
 
 /**
@@ -11,6 +13,8 @@ import edu.colorado.phet.common.phetcommon.util.ColorRange;
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
 public class Solute {
+
+    private static final int PARTICLES_PER_MOLE = 200; // default
 
     // The form that the solute is delivered in, either a solid or in a stock solution.
     public static enum SoluteForm {
@@ -43,5 +47,53 @@ public class Solute {
         this.particleColor = particleColor;
         this.particleSize = particleSize;
         this.particlesPerMole = particlesPerMole;
+    }
+
+    public static class KoolAid extends Solute {
+        public KoolAid() {
+            super( Strings.KOOL_AID, Symbols.KOOL_AID, 342.296, 5.96, 5.50, new ColorRange( new Color( 255, 225, 225 ), Color.RED ), 5, PARTICLES_PER_MOLE );
+        }
+    }
+
+    public static class ColbaltIINitrate extends Solute {
+        public ColbaltIINitrate() {
+            super( Strings.COBALT_II_NITRATE, Symbols.COBALT_II_NITRATE, 182.942, 5.64, 5.0, new ColorRange( new Color( 255, 225, 225 ), Color.RED ), 5, PARTICLES_PER_MOLE );
+        }
+    }
+
+    public static class CobaltChloride extends Solute {
+        public CobaltChloride() {
+            super( Strings.COBALT_CHLORIDE, Symbols.COBALT_CHLORIDE, 129.839, 4.33, 4.0, new ColorRange( new Color( 255, 242, 242 ), new Color( 0xFF6A6A ) /* rose pink */ ), 5, PARTICLES_PER_MOLE );
+        }
+    }
+
+    public static class PotassiumDichromate extends Solute {
+        public PotassiumDichromate() {
+            super( Strings.POTASSIUM_DICHROMATE, Symbols.POTASSIUM_DICHROMATE, 294.185, 0.51, 0.50, new ColorRange( new Color( 255, 232, 210 ), new Color( 0xFF7F00 ) /* orange */ ), 5, PARTICLES_PER_MOLE );
+        }
+    }
+
+    public static class PotassiumChromate extends Solute {
+        public PotassiumChromate() {
+            super( Strings.POTASSIUM_CHROMATE, Symbols.POTASSIUM_CHROMATE, 194.191, 3.35, 3.0, new ColorRange( new Color( 255, 255, 199 ), Color.YELLOW ), 5, PARTICLES_PER_MOLE );
+        }
+    }
+
+    public static class NickelIIChloride extends Solute {
+        public NickelIIChloride() {
+            super( Strings.NICKEL_II_CHLORIDE, Symbols.NICKEL_II_CHLORIDE, 129.599, 5.21, 5.0, new ColorRange( new Color( 234, 244, 234 ), new Color( 0x008000 ) /* green */ ), 5, PARTICLES_PER_MOLE );
+        }
+    }
+
+    public static class CopperSulfate extends Solute {
+        public CopperSulfate() {
+            super( Strings.COPPER_SULFATE, Symbols.COPPER_SULFATE, 159.609, 1.38, 1.0, new ColorRange( new Color( 222, 238, 255 ), new Color( 0x1E90FF ) /* blue */ ), 5, PARTICLES_PER_MOLE );
+        }
+    }
+
+    public static class PotassiumPermanganate extends Solute {
+        public PotassiumPermanganate() {
+            super( Strings.POTASSIUM_PERMANGANATE, Symbols.POTASSIUM_PERMANGANATE, 158.034, 0.48, 0.4, new ColorRange( new Color( 255, 0, 255 ), new Color( 0x8B008B ) /* purple */ ), Color.BLACK, 5, PARTICLES_PER_MOLE );
+        }
     }
 }
