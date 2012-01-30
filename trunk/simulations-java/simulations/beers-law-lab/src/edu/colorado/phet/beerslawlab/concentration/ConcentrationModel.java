5b6cdf547440cef1af28149e4eaed6fad504158f
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-01-30 17:49:53 +0000

    subclasses for specific solutes

--- trunk/simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/concentration/ConcentrationModel.java
+++ trunk/simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/concentration/ConcentrationModel.java
@@ -1,11 +1,8 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.beerslawlab.concentration;
 
-import java.awt.Color;
 import java.util.ArrayList;
 
-import edu.colorado.phet.beerslawlab.BLLResources.Strings;
-import edu.colorado.phet.beerslawlab.BLLResources.Symbols;
 import edu.colorado.phet.beerslawlab.model.Beaker;
 import edu.colorado.phet.beerslawlab.model.ConcentrationMeter;
 import edu.colorado.phet.beerslawlab.model.Dropper;
@@ -15,6 +12,14 @@ import edu.colorado.phet.beerslawlab.model.Precipitate;
 import edu.colorado.phet.beerslawlab.model.Shaker;
 import edu.colorado.phet.beerslawlab.model.ShakerParticles;
 import edu.colorado.phet.beerslawlab.model.Solute;
+import edu.colorado.phet.beerslawlab.model.Solute.CobaltChloride;
+import edu.colorado.phet.beerslawlab.model.Solute.ColbaltIINitrate;
+import edu.colorado.phet.beerslawlab.model.Solute.CopperSulfate;
+import edu.colorado.phet.beerslawlab.model.Solute.KoolAid;
+import edu.colorado.phet.beerslawlab.model.Solute.NickelIIChloride;
+import edu.colorado.phet.beerslawlab.model.Solute.PotassiumChromate;
+import edu.colorado.phet.beerslawlab.model.Solute.PotassiumDichromate;
+import edu.colorado.phet.beerslawlab.model.Solute.PotassiumPermanganate;
 import edu.colorado.phet.beerslawlab.model.Solute.SoluteForm;
 import edu.colorado.phet.beerslawlab.model.Solution;
 import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
@@ -23,7 +28,6 @@ import edu.colorado.phet.common.phetcommon.model.clock.ClockAdapter;
 import edu.colorado.phet.common.phetcommon.model.clock.ClockEvent;
 import edu.colorado.phet.common.phetcommon.model.clock.IClock;
 import edu.colorado.phet.common.phetcommon.model.property.Property;
-import edu.colorado.phet.common.phetcommon.util.ColorRange;
 import edu.colorado.phet.common.phetcommon.util.DoubleRange;
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
@@ -78,15 +82,14 @@ public class ConcentrationModel implements Resettable {
 
         // Solutes, in rainbow (ROYBIV) order. Values are specified in the design document.
         this.solutes = new ArrayList<Solute>() {{
-            final int precipitateParticlesPerMole = 200;
-            add( new Solute( Strings.KOOL_AID, Symbols.KOOL_AID, 342.296, 5.96, 5.50, new ColorRange( new Color( 255, 225, 225 ), Color.RED ), 5, precipitateParticlesPerMole ) );
-            add( new Solute( Strings.COBALT_II_NITRATE, Symbols.COBALT_II_NITRATE, 182.942, 5.64, 5.0, new ColorRange( new Color( 255, 225, 225 ), Color.RED ), 5, precipitateParticlesPerMole ) );
-            add( new Solute( Strings.COBALT_CHLORIDE, Symbols.COBALT_CHLORIDE, 129.839, 4.33, 4.0, new ColorRange( new Color( 255, 242, 242 ), new Color( 0xFF6A6A ) /* rose pink */ ), 5, precipitateParticlesPerMole ) );
-            add( new Solute( Strings.POTASSIUM_DICHROMATE, Symbols.POTASSIUM_DICHROMATE, 294.185, 0.51, 0.50, new ColorRange( new Color( 255, 232, 210 ), new Color( 0xFF7F00 ) /* orange */ ), 5, precipitateParticlesPerMole ) );
-            add( new Solute( Strings.POTASSIUM_CHROMATE, Symbols.POTASSIUM_CHROMATE, 194.191, 3.35, 3.0, new ColorRange( new Color( 255, 255, 199 ), Color.YELLOW ), 5, precipitateParticlesPerMole ) );
-            add( new Solute( Strings.NICKEL_II_CHLORIDE, Symbols.NICKEL_II_CHLORIDE, 129.599, 5.21, 5.0, new ColorRange( new Color( 234, 244, 234 ), new Color( 0x008000 ) /* green */ ), 5, precipitateParticlesPerMole ) );
-            add( new Solute( Strings.COPPER_SULFATE, Symbols.COPPER_SULFATE, 159.609, 1.38, 1.0, new ColorRange( new Color( 222, 238, 255 ), new Color( 0x1E90FF ) /* blue */ ), 5, precipitateParticlesPerMole ) );
-            add( new Solute( Strings.POTASSIUM_PERMANGANATE, Symbols.POTASSIUM_PERMANGANATE, 158.034, 0.48, 0.4, new ColorRange( new Color( 255, 0, 255 ), new Color( 0x8B008B ) /* purple */ ), Color.BLACK, 5, precipitateParticlesPerMole ) );
+            add( new KoolAid() );
+            add( new ColbaltIINitrate() );
+            add( new CobaltChloride() );
+            add( new PotassiumDichromate() );
+            add( new PotassiumChromate() );
+            add( new NickelIIChloride() );
+            add( new CopperSulfate() );
+            add( new PotassiumPermanganate() );
         }};
 
         this.solute = new Property<Solute>( solutes.get( 0 ) );
