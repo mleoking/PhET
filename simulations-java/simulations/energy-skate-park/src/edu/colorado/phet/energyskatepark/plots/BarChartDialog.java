ea2f3a5c5967ab9ed8461c31bc1687b0287c6b7a
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-01-30 16:12:12 +0000

    Energy Skate Park Basics: Hide the bar chart when the user switches tabs

--- simulations-java/simulations/energy-skate-park/src/edu/colorado/phet/energyskatepark/plots/BarChartDialog.java
+++ simulations-java/simulations/energy-skate-park/src/edu/colorado/phet/energyskatepark/plots/BarChartDialog.java
@@ -1,6 +1,7 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.energyskatepark.plots;
 
+import edu.colorado.phet.common.phetcommon.application.Module;
 import edu.colorado.phet.common.phetcommon.application.PaintImmediateDialog;
 import edu.colorado.phet.common.phetcommon.view.PhetFrame;
 import edu.colorado.phet.energyskatepark.AbstractEnergySkateParkModule;
@@ -11,14 +12,32 @@ import edu.colorado.phet.energyskatepark.AbstractEnergySkateParkModule;
  */
 public class BarChartDialog extends PaintImmediateDialog {
     private final BarGraphCanvas barGraphCanvas;
+    private boolean visibleOnActivation = false;
 
     public BarChartDialog( PhetFrame phetFrame, String title, boolean modal, AbstractEnergySkateParkModule module ) {
         super( phetFrame, title, modal );
         barGraphCanvas = new BarGraphCanvas( module );
         setContentPane( barGraphCanvas );
+
+        //Hide the bar chart when the user switches tabs
+        module.addListener( new Module.Listener() {
+            public void activated() {
+                setVisible( visibleOnActivation );
+            }
+
+            public void deactivated() {
+                visibleOnActivation = isVisible();
+                setVisible( false );
+            }
+        } );
+    }
+
+    @Override public void setVisible( boolean b ) {
+        super.setVisible( b );
     }
 
     public void reset() {
+        visibleOnActivation = false;
         setVisible( false );
         barGraphCanvas.reset();
     }
