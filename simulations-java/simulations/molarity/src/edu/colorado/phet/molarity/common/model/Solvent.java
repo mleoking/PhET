1b9abf7749f2320f701075fdaef62a84d429dd2e
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-01-31 00:47:30 +0000

    indicate why I chose not to use a single static instance of Water

--- simulations-java/simulations/molarity/src/edu/colorado/phet/molarity/common/model/Solvent.java
+++ simulations-java/simulations/molarity/src/edu/colorado/phet/molarity/common/model/Solvent.java
@@ -12,8 +12,11 @@ import edu.colorado.phet.molarity.MolarityResources.Symbols;
  */
 public class Solvent {
 
-    // pure water
-    //REVIEW: Why is water a class and not a field?
+    /*
+     * Pure water is a type of solvent.
+     * Since Solvents are immutable, we could use one static instance on Water.
+     * But that seems a bit precarious; if it's ever changed to mutable, it could make for some odd/difficult bugs.
+     */
     public static class Water extends Solvent {
         public Water() {
             super( Symbols.WATER, new Color( 0xE0FFFF ) );
