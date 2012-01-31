cd24662327df983367aeccf60214f3a5a968835f
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-01-31 21:19:34 +0000

    Move DnaSeparation to top level class, added functionality to it, added the ability to insert and remove separations to/from the DNA molecule.

---
+++ simulations-java/simulations/gene-expression-basics/src/edu/colorado/phet/geneexpressionbasics/manualgeneexpression/model/DnaSeparation.java
@@ -0,0 +1,52 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.geneexpressionbasics.manualgeneexpression.model;
+
+/**
+ * Class that defines a separation of the DNA strand.  This is used when
+ * forcing the DNA strand to separate in certain locations, which happens,
+ * for instance, when RNA polymerase is attached and transcribing the DNA.
+ *
+ * @author John Blanco
+ */
+public class DnaSeparation {
+    private final double targetAmount;
+    private double xPos; // X Position in model space.
+    private double amount = 0; // Actual amount of separation.  Starts at 0 and is generally grown over time toward target.
+
+    /**
+     * Constructor.
+     *
+     * @param xPos         - X Position in model space where this separation should
+     *                     exist.
+     * @param targetAmount
+     */
+    public DnaSeparation( double xPos, double targetAmount ) {
+        this.xPos = xPos;
+        this.targetAmount = targetAmount;
+    }
+
+    public double getXPos() {
+        return xPos;
+    }
+
+    public void setXPos( double xPos ) {
+        this.xPos = xPos;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount( double amount ) {
+        this.amount = Math.min( amount, targetAmount );
+    }
+
+    public boolean atTargetAmount() {
+        return amount == targetAmount;
+    }
+
+    public void setProportionOfTargetAmount( double proportion ) {
+        assert proportion >= 0 && proportion <= 1;
+        amount = targetAmount * proportion;
+    }
+}
