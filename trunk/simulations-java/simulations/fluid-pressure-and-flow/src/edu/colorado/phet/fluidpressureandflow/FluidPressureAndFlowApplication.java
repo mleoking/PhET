0f1fb0f0ec0bdd7d03eabbc023215f5d74dfc80c
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-01-31 03:33:53 +0000

    Fixed: use project name constant

--- trunk/simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/FluidPressureAndFlowApplication.java
+++ trunk/simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/FluidPressureAndFlowApplication.java
@@ -23,6 +23,6 @@ public class FluidPressureAndFlowApplication extends PiccoloPhetApplication {
     }
 
     public static void main( String[] args ) {
-        new PhetApplicationLauncher().launchSim( args, "fluid-pressure-and-flow", FluidPressureAndFlowApplication.class );
+        new PhetApplicationLauncher().launchSim( args, FluidPressureAndFlowResources.NAME, FluidPressureAndFlowApplication.class );
     }
 }
