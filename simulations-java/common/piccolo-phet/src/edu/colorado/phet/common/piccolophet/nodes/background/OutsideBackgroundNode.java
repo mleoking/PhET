2aaca222b29dba84adba140bb24c500f6f0152c4
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-02-01 16:34:19 +0000

    Made sky black when atmosphere is off

--- simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/nodes/background/OutsideBackgroundNode.java
+++ simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/nodes/background/OutsideBackgroundNode.java
@@ -1,8 +1,10 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.common.piccolophet.nodes.background;
 
+import java.awt.Color;
 import java.awt.geom.Rectangle2D;
 
+import edu.colorado.phet.common.phetcommon.math.ImmutableRectangle2D;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
 import edu.umd.cs.piccolo.PNode;
 
@@ -17,6 +19,9 @@ import edu.umd.cs.piccolo.PNode;
  */
 public class OutsideBackgroundNode extends PNode {
 
+    //Default rectangle to use for model bounds, made public so that clients can easily use the default (instead of making lots of aux constructors here)
+    public static final ImmutableRectangle2D DEFAULT_MODEL_BOUNDS = new ImmutableRectangle2D( -1000, -2000, 2000, 4000 );
+
     /**
      * Convenience constructor that assumes some typical values for the overall size of the earth and sky.
      *
@@ -25,7 +30,11 @@ public class OutsideBackgroundNode extends PNode {
      * @param groundGradientBottomY bottom of the gradient in model coordinates.
      */
     public OutsideBackgroundNode( ModelViewTransform mvt, double skyGradientTopY, double groundGradientBottomY ) {
-        this( mvt, skyGradientTopY, groundGradientBottomY, new Rectangle2D.Double( -1000, -2000, 2000, 4000 ) );
+        this( mvt, skyGradientTopY, groundGradientBottomY, DEFAULT_MODEL_BOUNDS.toRectangle2D() );
+    }
+
+    public OutsideBackgroundNode( ModelViewTransform mvt, double skyGradientTopY, double groundGradientBottomY, Rectangle2D modelBounds ) {
+        this( mvt, skyGradientTopY, groundGradientBottomY, modelBounds, SkyNode.DEFAULT_BOTTOM_COLOR, SkyNode.DEFAULT_TOP_COLOR );
     }
 
     /**
@@ -36,14 +45,14 @@ public class OutsideBackgroundNode extends PNode {
      * @param skyGradientTopY       top of the gradient in model coordinates.
      * @param groundGradientBottomY bottom of the gradient in model coordinates.
      */
-    public OutsideBackgroundNode( ModelViewTransform mvt, double skyGradientTopY, double groundGradientBottomY, Rectangle2D modelBounds ) {
+    public OutsideBackgroundNode( ModelViewTransform mvt, double skyGradientTopY, double groundGradientBottomY, Rectangle2D modelBounds, Color skyBottomColor, Color skyTopColor ) {
         if ( modelBounds.getMinY() < 0 ) {
             // Add the ground first, because we're earthy people.
             addChild( new GroundNode( mvt, new Rectangle2D.Double( modelBounds.getX(), modelBounds.getMinY(), modelBounds.getWidth(), -modelBounds.getMinY() ), groundGradientBottomY ) );
         }
         if ( modelBounds.getMaxY() > 0 ) {
             // Add the sky.
-            addChild( new SkyNode( mvt, new Rectangle2D.Double( modelBounds.getX(), 0, modelBounds.getWidth(), modelBounds.getMaxY() ), skyGradientTopY ) );
+            addChild( new SkyNode( mvt, new Rectangle2D.Double( modelBounds.getX(), 0, modelBounds.getWidth(), modelBounds.getMaxY() ), skyGradientTopY, skyBottomColor, skyTopColor ) );
         }
     }
 }
