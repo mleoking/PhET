2a0f63b9e84c42104aee02e414c2cb616bfc8454
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-02-01 23:10:20 +0000

    intermediate pass at light controls

--- simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/beerslaw/view/BeersLawCanvas.java
+++ simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/beerslaw/view/BeersLawCanvas.java
@@ -6,6 +6,7 @@ import java.awt.Frame;
 import edu.colorado.phet.beerslawlab.beerslaw.model.BeersLawModel;
 import edu.colorado.phet.beerslawlab.common.view.BLLCanvas;
 import edu.colorado.phet.beerslawlab.common.view.SoluteChoiceNode;
+import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.umd.cs.piccolo.PNode;
 
 /**
@@ -15,14 +16,22 @@ import edu.umd.cs.piccolo.PNode;
  */
 public class BeersLawCanvas extends BLLCanvas {
 
+    public enum LightRepresentation {BEAM, PHOTONS}
+    public enum WavelengthControlType {LAMBDA_MAX, MANUAL}
+
+    private final Property<LightRepresentation> lightRepresentation = new Property<LightRepresentation>( LightRepresentation.BEAM );
+    private final Property<WavelengthControlType> wavelengthControlType = new Property<WavelengthControlType>( WavelengthControlType.LAMBDA_MAX  );
+
     public BeersLawCanvas( final BeersLawModel model, Frame parentFrame ) {
 
         // Nodes
         PNode soluteChoiceNode = new SoluteChoiceNode( model.getSolutes(), model.solute );
+        PNode lightControlsNode = new LightControlsNode( lightRepresentation, wavelengthControlType );
 
         // Rendering order
         {
             addChild( soluteChoiceNode );
+            addChild( lightControlsNode );
         }
 
         // layout
@@ -32,6 +41,9 @@ public class BeersLawCanvas extends BLLCanvas {
             // solution combo box at top center
             soluteChoiceNode.setOffset( ( getStageSize().getWidth() - soluteChoiceNode.getFullBoundsReference().getWidth() ) / 2,
                                         yMargin );
+            // left center
+            lightControlsNode.setOffset( xMargin,
+                                         ( getStageSize().getHeight() - lightControlsNode.getFullBoundsReference().getWidth() ) / 2 );
         }
     }
 }
