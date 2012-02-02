06738da36bc87bb0a8a45da1a7bc12f1d9d3cc95
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-02-02 20:51:24 +0000

    add hooks for constraining drag to stage bounds

--- trunk/simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/beerslaw/view/BLLRulerNode.java
+++ trunk/simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/beerslaw/view/BLLRulerNode.java
@@ -1,6 +1,8 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.beerslawlab.beerslaw.view;
 
+import java.awt.geom.Dimension2D;
+
 import edu.colorado.phet.beerslawlab.common.BLLResources;
 import edu.colorado.phet.beerslawlab.common.BLLResources.Strings;
 import edu.colorado.phet.beerslawlab.common.BLLSimSharing.UserComponents;
@@ -11,20 +13,22 @@ import edu.colorado.phet.common.piccolophet.simsharing.SimSharingDragHandler;
 import edu.umd.cs.piccolo.event.PInputEvent;
 
 /**
- * A 2cm ruler.
+ * A 2cm ruler, constrained to drag within the bounds of the stage.
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
 public class BLLRulerNode extends RulerNode {
 
-    public BLLRulerNode( double length ) {
-        super( length, 40, new String[] { "0", "1", "2" }, Strings.UNITS_CENTIMETERS, 9, 12 );//TODO i18n
+    public BLLRulerNode( double length, final Dimension2D stageSize ) {
+        super( length, 40, new String[] { "0", "1", "2" }, Strings.UNITS_CENTIMETERS, 9, 12 );
         addInputEventListener( new CursorHandler() );
         addInputEventListener( new SimSharingDragHandler( UserComponents.ruler, UserComponentTypes.sprite ) {
             @Override protected void drag( PInputEvent event ) {
                 super.drag( event );
                 //TODO constrain to play area
-                setOffset( getXOffset() + event.getDelta().getWidth(), getYOffset() + event.getDelta().getHeight() );
+                double xOffset = getXOffset() + event.getDelta().getWidth();
+                double yOffset = getYOffset() + event.getDelta().getHeight();
+                setOffset( xOffset, yOffset );
             }
         } );
     }
