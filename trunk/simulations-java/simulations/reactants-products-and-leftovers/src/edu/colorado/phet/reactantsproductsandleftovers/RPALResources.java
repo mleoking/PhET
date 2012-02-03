298562b6818ab887a8cfb52ef337a21382e375bc
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-02-03 04:28:20 +0000

    delete unused methods

--- trunk/simulations-java/simulations/reactants-products-and-leftovers/src/edu/colorado/phet/reactantsproductsandleftovers/RPALResources.java
+++ trunk/simulations-java/simulations/reactants-products-and-leftovers/src/edu/colorado/phet/reactantsproductsandleftovers/RPALResources.java
@@ -22,35 +22,11 @@ public class RPALResources {
     /* not intended for instantiation */
     private RPALResources() {}
 
-    public static final PhetResources getResourceLoader() {
-        return RESOURCES;
-    }
-    
     public static final String getString( String name ) {
         return RESOURCES.getLocalizedString( name  );
     }
     
-    public static final char getChar( String name, char defaultValue ) {
-        return RESOURCES.getLocalizedChar( name, defaultValue );
-    }
-
-    public static final int getInt( String name, int defaultValue ) {
-        return RESOURCES.getLocalizedInt( name, defaultValue );
-    }
-    
     public static final BufferedImage getImage( String name ) {
         return RESOURCES.getImage( name );
-    }
-    
-    public static final PImage getImageNode( String name ) {
-        return new PImage( RESOURCES.getImage( name ) );
-    }
-    
-    public static final String getCommonString( String name ) {
-        return PhetCommonResources.getInstance().getLocalizedString( name );
-    }
-    
-    public static final BufferedImage getCommonImage( String name ) {
-        return PhetCommonResources.getInstance().getImage( name );
     }
 }
