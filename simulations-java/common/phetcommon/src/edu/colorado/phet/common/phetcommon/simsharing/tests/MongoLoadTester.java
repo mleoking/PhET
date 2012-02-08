0d98a0284d957e65e03a96e0658a87805dbef11b
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-02-08 17:52:42 +0000

    Cleaned up to make more compact and readable.

--- simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/simsharing/tests/MongoLoadTester.java
+++ simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/simsharing/tests/MongoLoadTester.java
@@ -34,7 +34,7 @@ public class MongoLoadTester {
 
     private static final int AVERAGE_TIME_BETWEEN_MESSAGES_MILLIS = 1000;
     private static final int MAX_MESSAGE_TIME_VARIATION = 500;
-    private static final int NUM_CLIENTS = 50;
+    private static final int NUM_CLIENTS = 10;
 
     //Part of the mongoDB password, see #3231
     public static final String MER = "meR".toLowerCase();
@@ -43,6 +43,7 @@ public class MongoLoadTester {
 
     public static void main( String[] args ) {
 
+        // Argument processing.
         double runTime = 10; // in seconds.
         if ( args.length > 1 ) {
             printUsage();
@@ -52,33 +53,38 @@ public class MongoLoadTester {
             runTime = Double.parseDouble( args[0] );
         }
 
-        // Look at the DB to establish the initial state.
-        long initialMessageCount = 0;
+        System.out.println( "Load test starting up, run time = " + runTime );
+
+        // Establish a connection to the database.
+        long initialMessageCount;
+        Mongo mongo;
+        DB db;
+        DBCollection collection;
         try {
-            Mongo mongo = new Mongo( MongoLog.HOST_IP_ADDRESS, MongoLog.PORT );
-            DB db = mongo.getDB( LOAD_TESTING_DB_NAME );
+            mongo = new Mongo( MongoLog.HOST_IP_ADDRESS, MongoLog.PORT );
+            db = mongo.getDB( LOAD_TESTING_DB_NAME );
             db.authenticate( DB_USER_NAME, ( MER + SimSharingManager.MONGO_PASSWORD + "" + ( 2 * 2 * 2 ) + "ss0O88723otbubaoue" ).toCharArray() );
-            DBCollection collection = db.getCollection( DB_COLLECTION );
+            collection = db.getCollection( DB_COLLECTION );
             initialMessageCount = collection.getCount();
         }
         catch ( UnknownHostException e ) {
+            System.out.println( "Exception thrown when trying to connect to database, " + e.getMessage() );
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            return;
         }
-
-        int sentMessageCount = 0;
 
         // Create the thread pool where the threads will be run.
         ExecutorService threadPool = Executors.newFixedThreadPool( NUM_CLIENTS );
 
         // Create a bunch of threads, each of which send data to the DB.
-        List<MessageSendingThread> threads = new ArrayList<MessageSendingThread>();
+        List<MessageSendingThread> messageSendingThreads = new ArrayList<MessageSendingThread>();
         for ( int i = 0; i < NUM_CLIENTS; i++ ) {
-            threads.add( new MessageSendingThread( runTime, i ) );
+            messageSendingThreads.add( new MessageSendingThread( runTime, i ) );
         }
 
         // Launch the threads.
-        for ( Runnable thread : threads ) {
-            threadPool.submit( thread );
+        for ( Runnable messageSendingThread : messageSendingThreads ) {
+            threadPool.submit( messageSendingThread );
         }
 
         // Wait until all threads are complete, then analyze the data in the
@@ -87,25 +93,19 @@ public class MongoLoadTester {
             threadPool.shutdown();
             threadPool.awaitTermination( (long) ( runTime * 10 ), TimeUnit.SECONDS );
             System.out.println( "All threads terminated." );
-            try {
-                Mongo mongo = new Mongo( MongoLog.HOST_IP_ADDRESS, MongoLog.PORT );
-                DB db = mongo.getDB( LOAD_TESTING_DB_NAME );
-                DBCollection collection = db.getCollection( DB_COLLECTION );
-                db.authenticate( DB_USER_NAME, ( MER + SimSharingManager.MONGO_PASSWORD + "" + ( 2 * 2 * 2 ) + "ss0O88723otbubaoue" ).toCharArray() );
-                long finalMessageCount = collection.getCount();
-                // Total up the sent messages.
-                int totalMessagesSent = 0;
-                for ( MessageSendingThread thread : threads ) {
-                    totalMessagesSent += thread.messagesSent;
-                }
-                System.out.println( "initialMessageCount = " + initialMessageCount );
-                System.out.println( "finalMessageCount = " + finalMessageCount );
-                System.out.println( "totalMessagesSent = " + totalMessagesSent );
-                System.out.println( "Test passed = " + ( initialMessageCount + totalMessagesSent == finalMessageCount ) );
+            long finalMessageCount = collection.getCount();
+            // Total up the sent messages.
+            int totalMessagesSent = 0;
+            for ( MessageSendingThread thread : messageSendingThreads ) {
+                totalMessagesSent += thread.messagesSent;
             }
-            catch ( UnknownHostException e ) {
-                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-            }
+            System.out.println( "initialMessageCount = " + initialMessageCount );
+            System.out.println( "finalMessageCount = " + finalMessageCount );
+            System.out.println( "totalMessagesSent = " + totalMessagesSent );
+
+            // Compare the expected and actual message count to decide whether
+            // the test was successful.
+            System.out.println( "Test passed = " + ( initialMessageCount + totalMessagesSent == finalMessageCount ) );
         }
         catch ( InterruptedException e ) {
             e.printStackTrace();
@@ -116,6 +116,7 @@ public class MongoLoadTester {
         System.out.println( "Usage: MongoLoadTester <run time in seconds>" );
     }
 
+    // Thread that sends messages to the database.
     private static class MessageSendingThread implements Runnable {
 
         private final double runTime;
