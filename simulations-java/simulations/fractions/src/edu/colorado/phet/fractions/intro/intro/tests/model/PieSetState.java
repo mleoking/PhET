6fc7ded05430ca8fde20d40c0de4c5e91b30ae6b
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-02-09 15:19:53 +0000

    Repackaged model classes

--- simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/intro/intro/tests/model/PieSetState.java
+++ simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/intro/intro/tests/model/PieSetState.java
@@ -1,5 +1,5 @@
 // Copyright 2002-2011, University of Colorado
-package edu.colorado.phet.fractions.intro.intro.tests;
+package edu.colorado.phet.fractions.intro.intro.tests.model;
 
 import java.util.ArrayList;
 import java.util.Collections;
