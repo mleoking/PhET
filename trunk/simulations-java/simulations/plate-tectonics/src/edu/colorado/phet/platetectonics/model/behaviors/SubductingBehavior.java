d38149b0c2c9067a8c88ffdab676d8628e71920e
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-02-09 12:40:21 +0000

    pt: testing subduction intial behavior

--- trunk/simulations-java/simulations/plate-tectonics/src/edu/colorado/phet/platetectonics/model/behaviors/SubductingBehavior.java
+++ trunk/simulations-java/simulations/plate-tectonics/src/edu/colorado/phet/platetectonics/model/behaviors/SubductingBehavior.java
@@ -1,6 +1,7 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.platetectonics.model.behaviors;
 
+import edu.colorado.phet.lwjglphet.math.ImmutableVector2F;
 import edu.colorado.phet.lwjglphet.math.ImmutableVector3F;
 import edu.colorado.phet.platetectonics.model.PlateMotionPlate;
 import edu.colorado.phet.platetectonics.model.Sample;
@@ -13,8 +14,101 @@ public class SubductingBehavior extends PlateBehavior {
 
     @Override public void stepInTime( float millionsOfYears ) {
         for ( Sample sample : plate.getCrust().getSamples() ) {
-            final ImmutableVector3F offsetVector = new ImmutableVector3F( millionsOfYears * 10000 * ( getPlate().isLeftPlate() ? 1 : -1 ), 0, 0 );
-            sample.setPosition( sample.getPosition().plus( offsetVector ) );
+            final int sign = getPlate().isLeftPlate() ? 1 : -1;
+            final float shearFactor = 1;
+
+            ImmutableVector3F preShearedPosition = sample.getPosition();
+            ImmutableVector3F postShearedPosition = new ImmutableVector3F( preShearedPosition.x + preShearedPosition.y * sign * shearFactor,
+                                                                           preShearedPosition.y,
+                                                                           preShearedPosition.z );
+            ImmutableVector3F transformedPosition = transformedSamplePosition( postShearedPosition, millionsOfYears );
+            ImmutableVector3F unshearedPosition = new ImmutableVector3F( transformedPosition.x - transformedPosition.y * sign * shearFactor,
+                                                                         transformedPosition.y,
+                                                                         transformedPosition.z );
+
+            sample.setPosition( unshearedPosition );
+
+
+//            final ImmutableVector3F offsetVector = new ImmutableVector3F( millionsOfYears * 10000 * sign, 0, 0 );
+//            final float pushFactor = sample.getPosition().x * sign;
+//            if ( ( getPlate().isLeftPlate() && offsetVector.x > 0 )
+//                 || ( !getPlate().isLeftPlate() && offsetVector.x < 0 ) ) {
+//                offsetVector = offsetVector.plus( new ImmutableVector3F( 0, 0, ) )
+//            }
+//            sample.setPosition( sample.getPosition().plus( offsetVector ) );
         }
+    }
+
+    public static ImmutableVector3F transformedSamplePosition( ImmutableVector3F position, float timeElapsed ) {
+        ImmutableVector2F origin = new ImmutableVector2F( 0, 5005 );
+        ImmutableVector2F toDir = ImmutableVector2F.Y_UNIT.negate();
+        ImmutableVector2F fromDir = ImmutableVector2F.X_UNIT;
+
+        ImmutableVector2F pos = new ImmutableVector2F( position.x, position.y ).minus( origin );
+
+        // flip the "from" direction if we are on the other side of the "to" direction
+        if ( fromDir.dot( toDir ) > fromDir.dot( pos.normalized() ) ) {
+            fromDir = ImmutableVector2F.X_UNIT.negate();
+        }
+
+        ImmutableVector2F medianDir = fromDir.plus( toDir ).normalized();
+        ImmutableVector2F motionDir = new ImmutableVector2F( Math.signum( medianDir.x ) * medianDir.y, -Math.abs( medianDir.x ) );
+
+        float value = toDir.dot( pos ) * fromDir.dot( pos );
+        float currentProgress = motionDir.dot( pos );
+        float newProgress = currentProgress + timeElapsed * 5000;
+
+        ImmutableVector2F highSolution;
+        ImmutableVector2F lowSolution;
+        {
+            // vector a == fromDir
+            float ax = fromDir.x;
+            float ay = fromDir.y;
+            // vector b == toDir
+            float bx = toDir.x;
+            float by = toDir.y;
+            // vector d = motionDir
+            float dx = motionDir.x;
+            float dy = motionDir.y;
+
+            float c = value;
+            float p = newProgress;
+
+            // we want to solve where
+            // p = dot( d, x ) and
+            // c = dot( a, x ) * dot( b, x )
+            // this was solved by hand into these quadratic coefficients, and we can solve for both "possible" solutions
+            // polyA * x^2 + polyB * x + polyC = 0
+            float polyA = ax * bx - ( ax * by + ay * bx ) * ( dx / dy ) + ay * by * ( dx / dy ) * ( dx / dy );
+            float polyB = ( ax * by + ay * bx ) * ( p / dy ) - ay * by * 2 * p * ( dx / ( dy * dy ) );
+            float polyC = ay * by * p * p / ( dy * dy ) - c;
+
+            // solve for x using the quadratic equation.
+            float discriminant = polyB * polyB - 4 * polyA * polyC;
+            if ( discriminant < 0 ) {
+                System.out.println( "toDir = " + toDir );
+                System.out.println( "fromDir = " + fromDir );
+                System.out.println( "motionDir = " + motionDir );
+                System.out.println( "sample.position = " + position );
+                System.out.println( "pos = " + pos );
+                System.out.println( "currentProgress = " + currentProgress );
+                System.out.println( "newProgress = " + newProgress );
+                System.out.println( "value = " + value );
+                throw new RuntimeException( "discriminant < 0" );
+            }
+            float largeX = (float) ( ( -polyB + Math.sqrt( discriminant ) ) / ( 2 * polyA ) );
+            float smallX = (float) ( ( -polyB - Math.sqrt( discriminant ) ) / ( 2 * polyA ) );
+
+            // solve for y based on the "progress" formula y=(p-x*dx)/dy
+            float largeY = ( p - largeX * dx ) / dy;
+            float smallY = ( p - smallX * dx ) / dy;
+
+            highSolution = new ImmutableVector2F( largeX, largeY );
+            lowSolution = new ImmutableVector2F( smallX, smallY );
+        }
+
+        // pick the solution that has a smaller y for now TODO: make sure this is right
+        final ImmutableVector2F p = ( lowSolution.y < highSolution.y ? lowSolution : highSolution ).plus( origin );
+        return new ImmutableVector3F( p.x, p.y, position.z );
     }
 }
