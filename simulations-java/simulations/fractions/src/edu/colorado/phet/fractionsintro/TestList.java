d6c23caca27975ff1cb3908e09327cab612d146f
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-02-11 06:43:35 +0000

    Added test for list equality

---
+++ simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/TestList.java
@@ -0,0 +1,35 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.fractionsintro;
+
+import fj.data.List;
+
+import java.util.Arrays;
+
+import static fj.data.List.iterableList;
+
+interface Predicate<T> {
+    public boolean apply( T t );
+}
+
+class Worker {
+    public boolean isEmployee() {
+        return false;
+    }
+}
+
+public class TestList {
+    public static void main( String[] args ) {
+        final java.util.List<Integer> myList = Arrays.asList( 1, 2, 3 );
+        final List<Integer> a = iterableList( myList );
+        final List<Integer> b = iterableList( myList );
+        boolean equal = a.equals( b );
+        System.out.println( "equal = " + equal );//prints false
+
+
+        Predicate<Worker> isEmployeePredicate = new Predicate<Worker>() {
+            public boolean apply( Worker w ) {
+                return w.isEmployee();
+            }
+        };
+    }
+}
