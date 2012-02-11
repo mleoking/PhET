b94b780508263c6159af697f66ebdffa115efb6b
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-02-11 22:45:58 +0000

    added list tests, see #3252

---
+++ trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/intro/tests/TestDouble.java
@@ -0,0 +1,42 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.fractionsintro.intro.tests;
+
+import fj.data.List;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Vector;
+
+/**
+ * @author Sam Reid
+ */
+public class TestDouble {
+    static final class X {
+        @Override public String toString() {
+            return "X";
+        }
+
+        @Override public boolean equals( Object obj ) {
+            return true;
+        }
+    }
+
+    static final class Y {
+        @Override public boolean equals( Object obj ) {
+            return true;
+        }
+    }
+
+    public static void main( String[] args ) {
+        boolean equal = new Vector<Double>().equals( new ArrayList<String>() );
+        System.out.println( "equal = " + equal );//prints true
+
+        List<X> a = List.single( new X() );
+        List<Y> b = List.single( new Y() );
+        System.out.println( "a.equals( b ) = " + a.equals( b ) );
+
+        System.out.println( "a = " + a );
+
+        System.out.println( List.iterableList( Arrays.asList( 1, 3, 4 ) ) );
+    }
+}
