2d70366ec15a786a944bd593b34f097b9bc41aae
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-02-13 22:37:37 +0000

    added test

---
+++ simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/intro/tests/TestCompositeModel.java
@@ -0,0 +1,53 @@
+// Copyright 2002-2012, University of Colorado
+package edu.colorado.phet.fractionsintro.intro.tests;
+
+import edu.colorado.phet.common.phetcommon.model.property.Property;
+import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
+
+/**
+ * model: numerator / denominator * 100 = percentage
+ * same as
+ * numerator = percentage /100 * denominator
+ *
+ * @author Sam Reid
+ */
+public class TestCompositeModel {
+    public static void main( String[] args ) {
+        final Property<Double> radius = new Property<Double>( 1.0 );
+        final Property<Double> angle = new Property<Double>( Math.toRadians( 45 ) );
+
+        final Property<Double> x = new Property<Double>( Math.cos( angle.get() ) * radius.get() );
+        final Property<Double> y = new Property<Double>( Math.sin( angle.get() ) * radius.get() );
+
+        //See when the values change
+//        radius.trace( "radius" );
+//        angle.trace( "angle" );
+//        x.trace( "x" );
+//        y.trace( "y" );
+
+        //When radius or angle changes, update x and y
+        final SimpleObserver updateXY = new SimpleObserver() {
+            public void update() {
+                x.set( Math.cos( angle.get() ) * radius.get() );
+                y.set( Math.sin( angle.get() ) * radius.get() );
+            }
+        };
+        radius.addObserver( updateXY );
+        angle.addObserver( updateXY );
+
+        //When x or y changes, update the radius and angle
+        SimpleObserver updateRA = new SimpleObserver() {
+            public void update() {
+                radius.set( Math.sqrt( x.get() * x.get() + y.get() * y.get() ) );
+                angle.set( Math.atan2( y.get(), x.get() ) );
+                System.out.println( "radius = " + radius );
+            }
+        };
+        x.addObserver( updateRA );
+        y.addObserver( updateRA );
+
+        radius.set( 3.0 );
+
+        System.out.println( "radius.get() = " + radius.get() );
+    }
+}
