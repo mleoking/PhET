e127201e0d5708e0b3d9aa73d875f99017cf549c
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-02-14 20:00:15 +0000

    2/14/12 flesh out concentration control a bit more

--- simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/beerslaw/view/ConcentrationControlNode.java
+++ simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/beerslaw/view/ConcentrationControlNode.java
@@ -4,18 +4,25 @@ package edu.colorado.phet.beerslawlab.beerslaw.view;
 import java.awt.Dimension;
 import java.awt.geom.Rectangle2D;
 import java.text.MessageFormat;
+import java.text.NumberFormat;
+
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 import edu.colorado.phet.beerslawlab.beerslaw.model.BeersLawSolution;
 import edu.colorado.phet.beerslawlab.beerslaw.model.BeersLawSolution.PureWater;
 import edu.colorado.phet.beerslawlab.common.BLLConstants;
 import edu.colorado.phet.beerslawlab.common.BLLResources.Strings;
+import edu.colorado.phet.common.phetcommon.model.property.ChangeObserver;
 import edu.colorado.phet.common.phetcommon.model.property.Property;
+import edu.colorado.phet.common.phetcommon.util.DefaultDecimalFormat;
 import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.nodes.kit.ZeroOffsetNode;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.nodes.PPath;
 import edu.umd.cs.piccolo.nodes.PText;
+import edu.umd.cs.piccolox.pswing.PSwing;
 
 /**
  * Control for changing solution's concentration.
@@ -25,16 +32,32 @@ import edu.umd.cs.piccolo.nodes.PText;
 public class ConcentrationControlNode extends PNode {
 
     private static final Dimension TRACK_SIZE = new Dimension( 250, 30 );
+    private static final PhetFont FONT = new PhetFont( BLLConstants.CONTROL_FONT_SIZE  );
+    private static final NumberFormat FORMAT = new DefaultDecimalFormat( "0" );
+
+    private final Property<BeersLawSolution> solution;
+    private final PPath sliderNode; //TODO use a slider
+    private final JTextField textField;
+    private final PText unitsNode;
 
     public ConcentrationControlNode( Property<BeersLawSolution> solution ) {
 
+        this.solution = solution;
+
         // nodes
-        PText labelNode = new PText( MessageFormat.format( Strings.PATTERN_0LABEL, Strings.CONCENTRATION ) );
-        labelNode.setFont( new PhetFont( BLLConstants.CONTROL_FONT_SIZE ) );
-        PPath sliderNode = new PPath( new Rectangle2D.Double( 0, 0, TRACK_SIZE.width, TRACK_SIZE.height ) ); //TODO use a slider
-        PPath textFieldNode = new PPath( new Rectangle2D.Double( 0, 0, 40, 40 ) ); //TODO use a JTextField
-        final PText unitsNode = new PText( "????" );
-        unitsNode.setFont( new PhetFont( BLLConstants.CONTROL_FONT_SIZE  ) );
+        PText labelNode = new PText( MessageFormat.format( Strings.PATTERN_0LABEL, Strings.CONCENTRATION ) ) {{
+            setFont( FONT );
+        }};
+        sliderNode = new PPath( new Rectangle2D.Double( 0, 0, TRACK_SIZE.width, TRACK_SIZE.height ) );
+        textField = new JTextField( "??????" ) {{
+            setColumns( 4 );
+            setFont( FONT );
+            setHorizontalAlignment( SwingConstants.RIGHT );
+        }};
+        PNode textFieldNode = new PSwing( textField );
+        unitsNode = new PText( "????" ) {{
+            setFont( FONT );
+        }};
 
         // rendering order
         PNode parentNode = new PNode();
@@ -52,13 +75,34 @@ public class ConcentrationControlNode extends PNode {
         unitsNode.setOffset( textFieldNode.getFullBoundsReference().getMaxX() + 5,
                               textFieldNode.getFullBoundsReference().getCenterY() - ( unitsNode.getFullBoundsReference().getHeight() / 2 ) );
 
-        // units are specific to the solution
-        solution.addObserver( new VoidFunction1<BeersLawSolution>() {
-            public void apply( BeersLawSolution solution ) {
-                setVisible( !( solution instanceof PureWater ) );
-                unitsNode.setText( solution.getDisplayUnits() );
+        // update the control when concentration changes
+        final VoidFunction1<Double> concentrationObserver = new VoidFunction1<Double>() {
+            public void apply( Double concentration ) {
+                updateControl();
+            }
+        };
 
+        // when solution selection changes, rewire the concentration observer and update the control
+        solution.addObserver( new ChangeObserver<BeersLawSolution> () {
+            public void update( BeersLawSolution newSolution, BeersLawSolution oldSolution ) {
+                if ( oldSolution != null ) {
+                    oldSolution.concentration.removeObserver( concentrationObserver );
+                }
+                newSolution.concentration.addObserver( concentrationObserver );
+                setVisible( !( newSolution instanceof PureWater ) );
+                updateControl();
             }
         });
+
+        //TODO add handlers for textField
+
+        updateControl(); // because adding a ChangeObserver doesn't result in immediate notification
+    }
+
+    private void updateControl() {
+        BeersLawSolution currentSolution = solution.get();
+        //TODO move slider to correct position
+        textField.setText( FORMAT.format( currentSolution.concentration.get() * ( 1 / Math.pow( 10, currentSolution.concentrationDisplayExponent ) ) ) );
+        unitsNode.setText( currentSolution.getDisplayUnits() );
     }
 }
