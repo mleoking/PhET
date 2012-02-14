d5277fc16d3bdb813c69d3abed40b72b0ed349b0
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-02-14 21:22:53 +0000

    Added docs

--- trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/intro/model/pieset/AnimationTarget.java
+++ trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/intro/model/pieset/AnimationTarget.java
@@ -6,6 +6,8 @@ import lombok.Data;
 import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
 
 /**
+ * Place where a slice is animating to (a cell or back to the bucket).
+ *
  * @author Sam Reid
  */
 @Data public class AnimationTarget {
