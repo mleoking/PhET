cac43d229438e7b182f64b0f854df858ab6c7e84
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-02-15 21:55:51 +0000

    refactored report

---
+++ util/sim-event-data-collection-analysis/src/edu/colorado/phet/simsharinganalysis/scripts/moleculeshapesfebruary2012/DebugSingleLog.scala
@@ -0,0 +1,24 @@
+// Copyright 2002-2012, University of Colorado
+package edu.colorado.phet.simsharinganalysis.scripts.moleculeshapesfebruary2012
+
+/**
+ * @author Sam Reid
+ */
+
+object DebugSingleLog {
+  def debugSingle() {
+    //    val log = phet parse new File("C:\\Users\\Sam\\Desktop\\em-interviews-iii\\2012_02_06_C1F_MolShap_V10202_ComboBox_EM.txt")
+    //    val states = this getStates log
+    //    for ( i <- 0 until log.size ) {
+    //      println(i + "\t" + log.entries(i).toString.replace('\t', ' ') + "\t" + states(i))
+    //    }
+    //
+    //    val report = toReport(log)
+    //    println(report)
+    //    val format = new DecimalFormat("0.00")
+    //    println("time in tab 0: " + format.format(report.minutesInTab0) + " minutes")
+    //    println("time in tab 1: " + format.format(report.minutesInTab1) + " minutes")
+    //    println("time in tab 1 and real: " + format.format(report.minutesOnRealInTab1) + " minutes")
+    //    println("time in tab 1 and model: " + format.format(report.minutesOnModelInTab1) + " minutes")
+  }
+}
