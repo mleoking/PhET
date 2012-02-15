22ea8e82dcb4c7a8855f0cdebf52ea64f7a24381
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-02-15 21:55:51 +0000

    refactored report

--- trunk/util/sim-event-data-collection-analysis/src/edu/colorado/phet/simsharinganalysis/scripts/moleculeshapesfebruary2012/MSAnalysis.scala
+++ trunk/util/sim-event-data-collection-analysis/src/edu/colorado/phet/simsharinganalysis/scripts/moleculeshapesfebruary2012/MSAnalysis.scala
@@ -10,7 +10,9 @@ import java.text.DecimalFormat
  * @author Sam Reid
  */
 object MSAnalysis extends StateMachine[SimState] {
-  def toReport(log: Log) = {
+  def toReport(log: Log) = new Report(log)
+
+  class Report(log: Log) {
     val states = getStates(log)
     "num states: " + states.length
 
@@ -18,28 +20,29 @@ object MSAnalysis extends StateMachine[SimState] {
     val b = states.reverse.tail.reverse
     val statePairs = b.zip(a)
 
-    val kitTransitions = log.entries.filter(entry => entry.component == "kitSelectionNode.previousButton" || entry.component == "kitSelectionNode.nextButton").length
+    val numKitTransitions = log.entries.filter(entry => entry.component == "kitSelectionNode.previousButton" || entry.component == "kitSelectionNode.nextButton").length
     val numTabTransitions = statePairs.filter(pair => pair._1.tab != pair._2.tab).length
     val numMoleculeTransitions = statePairs.filter(pair => pair._1.tab == pair._2.tab).filter(pair => pair._1.kit != pair._2.kit).length
     val numViewTransitions = statePairs.filter(pair => pair._1.tab == pair._2.tab).filter(pair => pair._1.kit != pair._2.kit).length
 
     def minutesInTab(tab: Int): Double = statePairs.filter(_._1.tab == tab).map(pair => pair._2.time - pair._1.time).sum / 1000.0 / 60.0
+
     def timeInTab1State(state: String): Double = statePairs.filter(_._1.tab == 1).filter(_._1.tab1.view == state).map(pair => pair._2.time - pair._1.time).sum / 1000.0 / 60.0
 
-    new Report(minutesInTab(0), minutesInTab(1), timeInTab1State("real"), timeInTab1State("model"), numTabTransitions, kitTransitions, numMoleculeTransitions, numViewTransitions)
-  }
-
-  case class Report(minutesInTab0: Double, minutesInTab1: Double,
-                    minutesOnRealInTab1: Double, minutesOnModelInTab1: Double,
-                    numTabTransitions: Int, numKitTransitions: Int, numMoleculeTransitions: Int, numViewTransitions: Int) {
-    override def toString = "minutes in tab 0: " + minutesInTab0 + "\n" +
-                            "minutes in tab 1: " + minutesInTab1 + "\n" +
-                            "minutes on real in tab 1: " + minutesOnRealInTab1 + "\n" +
-                            "minutes on model in tab 1: " + minutesOnModelInTab1 + "\n" +
+    override def toString = "minutes in tab 0: " + minutesInTab(0) + "\n" +
+                            "minutes in tab 1: " + minutesInTab(1) + "\n" +
+                            "minutes on real in tab 1: " + timeInTab1State("real") + "\n" +
+                            "minutes on model in tab 1: " + timeInTab1State("model") + "\n" +
                             "tab transitions: " + numTabTransitions + "\n" +
                             "kit transitions: " + numKitTransitions + "\n" +
                             "molecule transitions: " + numMoleculeTransitions + "\n" +
                             "view transitions: " + numViewTransitions
+  }
+
+  case class ReportX(minutesInTab0: Double, minutesInTab1: Double,
+                     minutesOnRealInTab1: Double, minutesOnModelInTab1: Double,
+                     numTabTransitions: Int, numKitTransitions: Int, numMoleculeTransitions: Int, numViewTransitions: Int) {
+
   }
 
   //Given the current state and an entry, compute the next state
@@ -56,18 +59,26 @@ object MSAnalysis extends StateMachine[SimState] {
   }
 
   def main(args: Array[String]) {
-    val log = phet parse new File("C:\\Users\\Sam\\Desktop\\em-interviews-iii\\2012_02_06_C1F_MolShap_V10202_ComboBox_EM.txt")
-    val states = this getStates log
-    for ( i <- 0 until log.size ) {
-      println(i + "\t" + log.entries(i).toString.replace('\t', ' ') + "\t" + states(i))
-    }
 
-    val report = toReport(log)
-    println(report)
-    val format = new DecimalFormat("0.00")
-    println("time in tab 0: " + format.format(report.minutesInTab0) + " minutes")
-    println("time in tab 1: " + format.format(report.minutesInTab1) + " minutes")
-    println("time in tab 1 and real: " + format.format(report.minutesOnRealInTab1) + " minutes")
-    println("time in tab 1 and model: " + format.format(report.minutesOnModelInTab1) + " minutes")
+    val logs = phet load new File("C:\\Users\\Sam\\Desktop\\ms-data")
+    val reports = logs.map(toReport(_))
+    println("time tab 1\t" +
+            "time tab 2\t" +
+            "time real\t" +
+            "time model\t" +
+            "tab transitions\t" +
+            "kit transitions\t" +
+            "molecule transitions\t" +
+            "view transitions")
+
+    def format(x: Double) = new DecimalFormat("0.00").format(x)
+    reports.foreach(r => println(format(r.minutesInTab(0)) + "\t" +
+                                 format(r.minutesInTab(1)) + "\t" +
+                                 format(r.timeInTab1State("real")) + "\t" +
+                                 format(r.timeInTab1State("model")) + "\t" +
+                                 r.numTabTransitions + "\t" +
+                                 r.numKitTransitions + "\t" +
+                                 r.numMoleculeTransitions + "\t" +
+                                 r.numViewTransitions))
   }
 }
