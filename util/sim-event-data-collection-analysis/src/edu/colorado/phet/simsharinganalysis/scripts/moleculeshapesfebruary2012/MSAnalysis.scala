2616d8b5bdd576221cdfb313dc8507ec2a8a5568
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-02-16 02:07:57 +0000

    Improved report output

--- util/sim-event-data-collection-analysis/src/edu/colorado/phet/simsharinganalysis/scripts/moleculeshapesfebruary2012/MSAnalysis.scala
+++ util/sim-event-data-collection-analysis/src/edu/colorado/phet/simsharinganalysis/scripts/moleculeshapesfebruary2012/MSAnalysis.scala
@@ -12,7 +12,7 @@ import java.text.DecimalFormat
 object MSAnalysis extends StateMachine[SimState] {
   def toReport(log: Log) = new Report(log)
 
-  class Report(log: Log) {
+  class Report(val log: Log) {
     val states = getStates(log)
     "num states: " + states.length
 
@@ -22,8 +22,8 @@ object MSAnalysis extends StateMachine[SimState] {
 
     val numKitTransitions = log.entries.filter(entry => entry.component == "kitSelectionNode.previousButton" || entry.component == "kitSelectionNode.nextButton").length
     val numTabTransitions = statePairs.filter(pair => pair._1.tab != pair._2.tab).length
-    val numMoleculeTransitions = statePairs.filter(pair => pair._1.tab == pair._2.tab).filter(pair => pair._1.kit != pair._2.kit).length
-    val numViewTransitions = statePairs.filter(pair => pair._1.tab == pair._2.tab).filter(pair => pair._1.kit != pair._2.kit).length
+    val numMoleculeTransitions = statePairs.filter(pair => pair._1.tab1.molecule != pair._2.tab1.molecule).length
+    val numViewTransitions = statePairs.filter(pair => pair._1.tab1.view != pair._2.tab1.view).length
 
     def minutesInTab(tab: Int): Double = statePairs.filter(_._1.tab == tab).map(pair => pair._2.time - pair._1.time).sum / 1000.0 / 60.0
 
@@ -39,12 +39,6 @@ object MSAnalysis extends StateMachine[SimState] {
                             "view transitions: " + numViewTransitions
   }
 
-  case class ReportX(minutesInTab0: Double, minutesInTab1: Double,
-                     minutesOnRealInTab1: Double, minutesOnModelInTab1: Double,
-                     numTabTransitions: Int, numKitTransitions: Int, numMoleculeTransitions: Int, numViewTransitions: Int) {
-
-  }
-
   //Given the current state and an entry, compute the next state
   def initialState(log: Log) = SimState(log.startTime)
 
@@ -55,6 +49,7 @@ object MSAnalysis extends StateMachine[SimState] {
     else if ( e.enabled == false ) state.copy(time = e.time)
     else if ( e.componentType == "tab" ) state.copy(tab = if ( e.component == "moleculeShapesTab" ) 0 else 1, time = e.time)
     else if ( state.tab == 0 ) state.copy(tab0 = state.tab0.next(e), time = e.time)
+    else if ( state.tab == 1 ) state.copy(tab1 = state.tab1.next(e), time = e.time)
     else state.copy(tab1 = state.tab1.next(e), time = e.time)
   }
 
@@ -62,7 +57,8 @@ object MSAnalysis extends StateMachine[SimState] {
 
     val logs = phet load new File("C:\\Users\\Sam\\Desktop\\ms-data")
     val reports = logs.map(toReport(_))
-    println("time tab 1\t" +
+    println("filename\t" + "" +
+            "time tab 1\t" +
             "time tab 2\t" +
             "time real\t" +
             "time model\t" +
@@ -72,7 +68,8 @@ object MSAnalysis extends StateMachine[SimState] {
             "view transitions")
 
     def format(x: Double) = new DecimalFormat("0.00").format(x)
-    reports.foreach(r => println(format(r.minutesInTab(0)) + "\t" +
+    reports.foreach(r => println(r.log.file.getName + "\t" +
+                                 format(r.minutesInTab(0)) + "\t" +
                                  format(r.minutesInTab(1)) + "\t" +
                                  format(r.timeInTab1State("real")) + "\t" +
                                  format(r.timeInTab1State("model")) + "\t" +
--- util/sim-event-data-collection-analysis/src/edu/colorado/phet/simsharinganalysis/scripts/moleculeshapesfebruary2012/SimState.scala
+++ util/sim-event-data-collection-analysis/src/edu/colorado/phet/simsharinganalysis/scripts/moleculeshapesfebruary2012/SimState.scala
@@ -10,15 +10,11 @@ object SimState {
 
 case class SimState(tab: Int, tab0: Tab0, tab1: Tab1, time: Long) {
   def getSelectedTab = if ( tab == 0 ) tab0 else tab1
-
-  //  def view = if ( tab == 1 ) tab1.view else "other"
-  def kit = tab1.kit
-
-  def molecule = getSelectedTab.molecule
 }
 
+case class Molecule(electronGeometry: String, moleculeGeometry: String)
+
 trait MSTab {
-  def molecule: String
 }
 
 //Factored out state that is shared between all tabs, since case classes do not inherit well.
@@ -50,8 +46,7 @@ case class ViewAndTestState(view: String, test: String, somethingEnabled: Boolea
 }
 
 //ShowSolvent indicates that the check box is checked, but solvent only showing if view is also "molecules"
-case class Tab0() extends MSTab {
-  def molecule = "???"
+case class Tab0(dummy: String) extends MSTab {
 
   def next(e: Entry): Tab0 = {
 
@@ -59,13 +54,6 @@ case class Tab0() extends MSTab {
 
       //If clicking on something disabled, then do not change the state, see #3218
       case x: Entry if x.enabled == false => this
-
-      //Watch which solution the user selects
-      //      case Entry(_, "user", c, _, "pressed", _) if List("waterRadioButton", "waterIcon").contains(c) => copy(solution = water)
-      //      case Entry(_, "user", c, _, "pressed", _) if List("strongAcidRadioButton", "strongAcidIcon").contains(c) => copy(solution = strongAcid)
-      //      case Entry(_, "user", c, _, "pressed", _) if List("weakAcidRadioButton", "weakAcidIcon").contains(c) => copy(solution = weakAcid)
-      //      case Entry(_, "user", c, _, "pressed", _) if List("strongBaseRadioButton", "strongBaseIcon").contains(c) => copy(solution = strongBase)
-      //      case Entry(_, "user", c, _, "pressed", _) if List("weakBaseRadioButton", "weakBaseIcon").contains(c) => copy(solution = weakBase)
 
       //Handle reset all presses
       case Entry(_, "user", "resetAllConfirmationDialogYesButton", _, "pressed", _) => initialTab0
@@ -75,13 +63,11 @@ case class Tab0() extends MSTab {
 
     }
 
-    //    updated.copy(viewAndTestState = viewAndTestState.next(e))
     updated
   }
 }
 
-case class Tab1(view: String, kit: String) extends MSTab {
-  def molecule = "???"
+case class Tab1(molecule: String, view: String, kit: String) extends MSTab {
 
   def next(e: Entry): Tab1 = {
 
@@ -96,6 +82,8 @@ case class Tab1(view: String, kit: String) extends MSTab {
 
       //Handle reset all presses
       case Entry(_, "user", "resetAllConfirmationDialogYesButton", _, "pressed", _) => initialTab1
+
+      case Entry(_, "model", "molecule", "moleculeModel", "realMoleculeChanged", _) => copy(molecule = e("realMolecule"))
 
       //Nothing happened to change the state
       case _ => this
