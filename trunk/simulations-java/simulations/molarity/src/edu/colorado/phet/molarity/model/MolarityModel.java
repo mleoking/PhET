4a6b1297f12a5c83a7c34f4dd91e95442d9e5a80
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-02-16 17:36:19 +0000

    change ranges and saturation points: solute=0-1 mol, volume=0.1-1L, concentration=0-6M, Kool-Aid=5.95M, Cobalt nitrate=5.65M, Nickel chloride=5.20M

--- trunk/simulations-java/simulations/molarity/src/edu/colorado/phet/molarity/model/MolarityModel.java
+++ trunk/simulations-java/simulations/molarity/src/edu/colorado/phet/molarity/model/MolarityModel.java
@@ -21,9 +21,10 @@ public class MolarityModel implements Resettable {
     private static final int PARTICLES_PER_MOLE = 200; // number of particles to show for each mole of precipitate
     private static final int PARTICLE_SIZE = 5; // particles are square, this is the length of one side
     private static final DoubleRange SOLUTE_AMOUNT_RANGE = new DoubleRange( 0, 1, 0.5 ); // moles
-    private static final DoubleRange SOLUTION_VOLUME_RANGE = new DoubleRange( 0.2, 1, 0.5 ); // liters
+    private static final DoubleRange SOLUTION_VOLUME_RANGE = new DoubleRange( 0.1, 1, 0.5 ); // liters
     private static final DoubleRange CONCENTRATION_RANGE = new DoubleRange( SOLUTE_AMOUNT_RANGE.getMin() / SOLUTION_VOLUME_RANGE.getMax(),
-                                                                            SOLUTE_AMOUNT_RANGE.getMax() / SOLUTION_VOLUME_RANGE.getMin() );
+                                                                            SOLUTE_AMOUNT_RANGE.getMax() / SOLUTION_VOLUME_RANGE.getMin() ); // M
+    private static final DoubleRange CONCENTRATION_DISPLAY_RANGE = new DoubleRange( CONCENTRATION_RANGE.getMin(), 6 ); // M
 
     // validate assumptions
     static {
@@ -37,13 +38,13 @@ public class MolarityModel implements Resettable {
 
         // solutes, in rainbow (ROYGBIV) order
         this.solutes = new ArrayList<Solute>() {{
-            add( new Solute( Strings.KOOL_AID, MolaritySymbols.KOOL_AID, 5.0, new ColorRange( new Color( 255, 225, 225 ), Color.RED ), PARTICLE_SIZE, PARTICLES_PER_MOLE ) );
-            add( new Solute( Strings.COBALT_II_NITRATE, MolaritySymbols.COBALT_II_NITRATE, 5.0, new ColorRange( new Color( 255, 225, 225 ), Color.RED ), PARTICLE_SIZE, PARTICLES_PER_MOLE ) );
+            add( new Solute( Strings.KOOL_AID, MolaritySymbols.KOOL_AID, 5.95, new ColorRange( new Color( 255, 225, 225 ), Color.RED ), PARTICLE_SIZE, PARTICLES_PER_MOLE ) );
+            add( new Solute( Strings.COBALT_II_NITRATE, MolaritySymbols.COBALT_II_NITRATE, 5.65, new ColorRange( new Color( 255, 225, 225 ), Color.RED ), PARTICLE_SIZE, PARTICLES_PER_MOLE ) );
             add( new Solute( Strings.COBALT_CHLORIDE, MolaritySymbols.COBALT_CHLORIDE, 4.35, new ColorRange( new Color( 255, 242, 242 ), new Color( 0xFF6A6A ) ), PARTICLE_SIZE, PARTICLES_PER_MOLE ) );
             add( new Solute( Strings.POTASSIUM_DICHROMATE, MolaritySymbols.POTASSIUM_DICHROMATE, 0.50, new ColorRange( new Color( 255, 232, 210 ), new Color( 0xFF7F00 ) ), PARTICLE_SIZE, PARTICLES_PER_MOLE ) );
             add( new Solute( Strings.GOLD_III_CHLORIDE, MolaritySymbols.GOLD_III_CHLORIDE, 2.25, new ColorRange( new Color( 255, 255, 199 ), new Color( 0xFFD700 ) ), PARTICLE_SIZE, PARTICLES_PER_MOLE ) );
             add( new Solute( Strings.POTASSIUM_CHROMATE, MolaritySymbols.POTASSIUM_CHROMATE, 3.35, new ColorRange( new Color( 255, 255, 199 ), Color.YELLOW ), PARTICLE_SIZE, PARTICLES_PER_MOLE ) );
-            add( new Solute( Strings.NICKEL_II_CHLORIDE, MolaritySymbols.NICKEL_II_CHLORIDE, 5.0, new ColorRange( new Color( 234, 244, 234 ), new Color( 0x008000 ) ), PARTICLE_SIZE, PARTICLES_PER_MOLE ) );
+            add( new Solute( Strings.NICKEL_II_CHLORIDE, MolaritySymbols.NICKEL_II_CHLORIDE, 5.2, new ColorRange( new Color( 234, 244, 234 ), new Color( 0x008000 ) ), PARTICLE_SIZE, PARTICLES_PER_MOLE ) );
             add( new Solute( Strings.COPPER_SULFATE, MolaritySymbols.COPPER_SULFATE, 1.40, new ColorRange( new Color( 222, 238, 255 ), new Color( 0x1E90FF ) ), PARTICLE_SIZE, PARTICLES_PER_MOLE ) );
             add( new Solute( Strings.POTASSIUM_PERMANGANATE, MolaritySymbols.POTASSIUM_PERMANGANATE, 0.50, new ColorRange( new Color( 255, 0, 255 ), new Color( 0x8B008B ) ), Color.BLACK, PARTICLE_SIZE, PARTICLES_PER_MOLE ) );
         }};
@@ -68,6 +69,11 @@ public class MolarityModel implements Resettable {
 
     public DoubleRange getConcentrationRange() {
         return CONCENTRATION_RANGE;
+    }
+
+    // Range to use for concentration display, possibly different than full range of model.
+    public DoubleRange getConcentrationDisplayRange() {
+        return CONCENTRATION_DISPLAY_RANGE;
     }
 
     public void reset() {
