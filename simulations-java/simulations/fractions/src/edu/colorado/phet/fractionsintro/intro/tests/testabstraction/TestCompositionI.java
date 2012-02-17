ac9946d7dacf3f750e15396f50a416fb661de0f5
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-02-17 16:17:28 +0000

    added abstraction tests

---
+++ simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/intro/tests/testabstraction/TestCompositionI.java
@@ -0,0 +1,47 @@
+// Copyright 2002-2012, University of Colorado
+package edu.colorado.phet.fractionsintro.intro.tests.testabstraction;
+
+/**
+ * @author Sam Reid
+ */
+public class TestCompositionI {
+    private static class Slice {
+        public final double x;
+
+        private Slice( double x ) {this.x = x;}
+
+        public Slice translate( double dx ) {return new Slice( x + dx );}
+    }
+
+    private static interface ISlice {
+    }
+
+    private static class RotatableSlice implements ISlice {
+        private final Slice slice;
+        private final double angle;
+
+        private RotatableSlice( double angle ) {
+            this.slice = new Slice( 0 );
+            this.angle = angle;
+        }
+
+        private RotatableSlice( Slice slice, double angle ) {
+            this.slice = slice;
+            this.angle = angle;
+        }
+
+        public RotatableSlice rotate( int i ) {
+            return new RotatableSlice( slice, i );
+        }
+
+        public RotatableSlice translate( int dx ) {
+            return new RotatableSlice( slice.translate( dx ), angle );
+        }
+    }
+
+    public static void main( String[] args ) {
+        RotatableSlice r = new RotatableSlice( 0.0 );
+        RotatableSlice r2 = r.rotate( 90 );
+        RotatableSlice r3 = r2.translate( 100 );
+    }
+}
---
+++ simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/intro/tests/testabstraction/TestInheritance.java
@@ -0,0 +1,29 @@
+// Copyright 2002-2012, University of Colorado
+package edu.colorado.phet.fractionsintro.intro.tests.testabstraction;
+
+/**
+ * @author Sam Reid
+ */
+public class TestInheritance {
+    private static class Slice {
+        public final double x;
+
+        private Slice( double x ) {this.x = x;}
+
+        public Slice translate( double dx ) {return new Slice( x + dx );}
+    }
+
+    private static class RotatableSlice {
+        public final double degrees;
+
+        private RotatableSlice( double degrees ) {this.degrees = degrees;}
+
+        public RotatableSlice rotate( double degrees ) { return new RotatableSlice( degrees ); }
+    }
+
+    public static void main( String[] args ) {
+        RotatableSlice r = new RotatableSlice( 0.0 );
+        RotatableSlice r2 = r.rotate( 90 );
+//        RotatableSlice r3 = r2.translate( 100 );//Compile error
+    }
+}
