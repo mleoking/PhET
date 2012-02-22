a92b5a7ef9b8781803b7e681edc464365481a133
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-02-22 06:48:18 +0000

    make trivial classes inner

--- trunk/simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/concentration/view/ParticleNode.java
+++ trunk/simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/concentration/view/ParticleNode.java
@@ -4,7 +4,11 @@ package edu.colorado.phet.beerslawlab.concentration.view;
 import java.awt.BasicStroke;
 import java.awt.geom.Rectangle2D;
 
+import edu.colorado.phet.beerslawlab.concentration.model.PrecipitateParticle;
+import edu.colorado.phet.beerslawlab.concentration.model.ShakerParticle;
 import edu.colorado.phet.beerslawlab.concentration.model.SoluteParticle;
+import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
+import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
 import edu.umd.cs.piccolo.nodes.PPath;
 
 /**
@@ -24,5 +28,42 @@ abstract class ParticleNode extends PPath {
         setPathTo( new Rectangle2D.Double( -particle.getSize() / 2, -particle.getSize() / 2, particle.getSize(), particle.getSize() ) ); // square
         setRotation( particle.getOrientation() );
         setOffset( particle.getLocation().getX(), particle.getLocation().getY() - STROKE_WIDTH ); // account for stroke width so there's no overlap with bottom of beaker
+    }
+
+    /**
+     * A precipitate particle. These particles don't move, they appear on the bottom of the beaker.
+     * The class exists solely to constrain the constructor to a specific type of particle.
+     */
+    static class PrecipitateParticleNode extends ParticleNode {
+        public PrecipitateParticleNode( PrecipitateParticle particle ) {
+            super( particle );
+        }
+    }
+
+    /**
+     * A solid solute particle exiting the shaker. These particles move.
+     */
+    static class ShakerParticleNode extends ParticleNode {
+
+        private final ShakerParticle particle;
+        private final VoidFunction1<ImmutableVector2D> locationObserver;
+
+        public ShakerParticleNode( ShakerParticle particle ) {
+            super( particle );
+
+            this.particle = particle;
+
+            // move to particle's location
+            locationObserver = new VoidFunction1<ImmutableVector2D>() {
+                public void apply( ImmutableVector2D location ) {
+                    setOffset( location.toPoint2D() );
+                }
+            };
+            particle.addLocationObserver( locationObserver );
+        }
+
+        public void cleanup() {
+            particle.removeLocationObserver( locationObserver );
+        }
     }
 }
--- trunk/simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/concentration/view/ShakerParticlesNode.java
+++ trunk/simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/concentration/view/ShakerParticlesNode.java
@@ -18,20 +18,20 @@ class ShakerParticlesNode extends PComposite {
     public ShakerParticlesNode( ShakerParticles shakerParticles ) {
 
         // Maps model elements to their corresponding nodes.
-        final HashMap<ShakerParticle, ShakerParticleNode> map = new HashMap<ShakerParticle, ShakerParticleNode>();
+        final HashMap<ShakerParticle, edu.colorado.phet.beerslawlab.concentration.view.ParticleNode.ShakerParticleNode> map = new HashMap<ShakerParticle, edu.colorado.phet.beerslawlab.concentration.view.ParticleNode.ShakerParticleNode>();
 
         shakerParticles.addParticlesChangeListener( new ParticlesChangeListener() {
 
             // A particle was added.
             public void particleAdded( ShakerParticle particle ) {
-                ShakerParticleNode particleNode = new ShakerParticleNode( particle );
+                edu.colorado.phet.beerslawlab.concentration.view.ParticleNode.ShakerParticleNode particleNode = new edu.colorado.phet.beerslawlab.concentration.view.ParticleNode.ShakerParticleNode( particle );
                 map.put( particle, particleNode );
                 addChild( particleNode );
             }
 
             // A particle was removed.
             public void particleRemoved( ShakerParticle particle ) {
-                ShakerParticleNode particleNode = map.get( particle );
+                edu.colorado.phet.beerslawlab.concentration.view.ParticleNode.ShakerParticleNode particleNode = map.get( particle );
                 map.remove( particle );
                 removeChild( particleNode );
                 particleNode.cleanup();
