49070f1259f457e05c4eb6715bed440a20f35caa
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-02-22 03:55:18 +0000

    wire up %transmission property (value not correctly computed yet)

--- simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/beerslaw/model/ATDetector.java
+++ simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/beerslaw/model/ATDetector.java
@@ -6,56 +6,46 @@ import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
 import edu.colorado.phet.common.phetcommon.model.property.CompositeProperty;
 import edu.colorado.phet.common.phetcommon.model.property.ObservableProperty;
 import edu.colorado.phet.common.phetcommon.model.property.Property;
-import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 import edu.colorado.phet.common.phetcommon.util.function.Function0;
-import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
 import edu.umd.cs.piccolo.util.PBounds;
 
 /**
  * Detector for absorbance (A) and transmittance (T).
  * If place in the path of the beam between the light and cuvette, measures 0 absorbance and 100% transmittance.
  * Inside the cuvette, there is no measurement.
- * <p>
- * Absorbance (A) is unitless. A = abC.
- * <p>
- * a = molar absorptivity.  Units = M-1cm-1  (Also called e or molar extinction coefficient)
- * This is a measure of the amount of light absorbed per unit concentration at a given wavelength.
- * It is different constant value for each chemical substance.  The relationship between absorbance
- * and concentration is linear (below the detection limit).
- * <p>
- * b = pathlength. Units = cm.
- * The container of solution used in spectrophotometers is called a cuvette.
- * The dimension or cross section of the cuvette that the light passes through is the pathlength.
- * <p>
- * C = concentration. Units = M = mol/L (moles of solute per Liter of solution)
- * <p>
- * A = 2 - log10 %T, where %T is percent transmittance.
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
 public class ATDetector {
 
-    public static enum ATDetectorMode {TRANSMITTANCE, ABSORBANCE}
+    public static enum ATDetectorMode {PERCENT_TRANSMITTANCE, ABSORBANCE}
 
-    public final CompositeProperty<Double> value; //TODO this should be a CompositeProperty, so it can't be set by clients
+    public final CompositeProperty<Double> value;
     public final Movable body;
     public final Movable probe;
     public final double probeDiameter; // diameter of the probe's sensor area, in cm
-    public Property<ATDetectorMode> mode = new Property<ATDetectorMode>( ATDetectorMode.TRANSMITTANCE );
+    public Property<ATDetectorMode> mode = new Property<ATDetectorMode>( ATDetectorMode.PERCENT_TRANSMITTANCE );
 
     public ATDetector( ImmutableVector2D bodyLocation, PBounds bodyDragBounds,
                        ImmutableVector2D probeLocation, PBounds probeDragBounds,
-                       final ObservableProperty<Double> absorbance ) {
+                       final ObservableProperty<Double> absorbance, final ObservableProperty<Double> percentTransmission ) {
 
         this.body = new Movable( bodyLocation, bodyDragBounds );
         this.probe = new Movable( probeLocation, probeDragBounds );
-        this.probeDiameter = 0.25;
+        this.probeDiameter = 0.25; // specific to the probe image file
 
+        //TODO this should also be a function of the detector's probe location
+        // update the value that is displayed by the detector
         this.value = new CompositeProperty<Double>( new Function0<Double>() {
             public Double apply() {
-                return absorbance.get();
+                if ( mode.get() == ATDetectorMode.PERCENT_TRANSMITTANCE ) {
+                    return percentTransmission.get();
+                }
+                else {
+                    return absorbance.get();
+                }
             }
-        }, absorbance, mode );
+        }, absorbance, percentTransmission, mode );
     }
 
     public void reset() {
