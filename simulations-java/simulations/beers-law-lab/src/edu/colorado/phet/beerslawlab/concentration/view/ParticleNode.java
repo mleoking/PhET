774d431bb5c85d8ee5269401168ad08ad12e4b29
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-02-22 06:36:57 +0000

    move duplicate code for particle nodes into a base class

---
+++ simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/concentration/view/ParticleNode.java
@@ -0,0 +1,28 @@
+// Copyright 2002-2012, University of Colorado
+package edu.colorado.phet.beerslawlab.concentration.view;
+
+import java.awt.BasicStroke;
+import java.awt.geom.Rectangle2D;
+
+import edu.colorado.phet.beerslawlab.concentration.model.SoluteParticle;
+import edu.umd.cs.piccolo.nodes.PPath;
+
+/**
+ * Base class for all particles.
+ * Origin is at the center of the particle.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
+public class ParticleNode extends PPath {
+
+    private static final float STROKE_WIDTH = 1f;
+
+    public ParticleNode( SoluteParticle particle ) {
+        setPaint( particle.getColor() );
+        setStrokePaint( particle.getColor().darker() );
+        setStroke( new BasicStroke( STROKE_WIDTH ) );
+        setPathTo( new Rectangle2D.Double( -particle.getSize() / 2, -particle.getSize() / 2, particle.getSize(), particle.getSize() ) ); // square
+        setRotation( particle.getOrientation() );
+        setOffset( particle.getLocation().getX(), particle.getLocation().getY() - STROKE_WIDTH ); // account for stroke width so there's no overlap with bottom of beaker
+    }
+}
--- simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/concentration/view/PrecipitateParticleNode.java
+++ simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/concentration/view/PrecipitateParticleNode.java
@@ -1,29 +1,16 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.beerslawlab.concentration.view;
 
-import java.awt.BasicStroke;
-import java.awt.geom.Rectangle2D;
-
 import edu.colorado.phet.beerslawlab.concentration.model.PrecipitateParticle;
-import edu.umd.cs.piccolo.nodes.PPath;
 
 /**
  * Visual representation of a precipitate particle.
- * We use the same representation for all solutes, but vary the size and orientation.
- * Origin is at the center of the particle.
+ * The class exists solely to constrain the constructor to a specific type of particle.
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
-class PrecipitateParticleNode extends PPath {
-
-    private static final float STROKE_WIDTH = 1f;
-
+class PrecipitateParticleNode extends ParticleNode {
     public PrecipitateParticleNode( PrecipitateParticle particle ) {
-        setPaint( particle.getColor() );
-        setStrokePaint( particle.getColor().darker() );
-        setStroke( new BasicStroke( STROKE_WIDTH ) );
-        setPathTo( new Rectangle2D.Double( -particle.getSize() / 2, -particle.getSize() / 2, particle.getSize(), particle.getSize() ) ); // square
-        setRotation( particle.getOrientation() );
-        setOffset( particle.getLocation().getX(), particle.getLocation().getY() - STROKE_WIDTH ); // account for stroke width so there's no overlap with bottom of beaker
+        super( particle );
     }
 }
