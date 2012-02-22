1a2cace13dd362c5bfaffe03ac0df458f7eee000
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-02-22 03:27:53 +0000

    first pass at integrating absorbance model (detector always shows absorbance, no matter where it's placed)

--- trunk/simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/beerslaw/model/BeersLawModel.java
+++ trunk/simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/beerslaw/model/BeersLawModel.java
@@ -15,8 +15,13 @@ import edu.colorado.phet.beerslawlab.beerslaw.model.BeersLawSolution.PotassiumPe
 import edu.colorado.phet.beerslawlab.beerslaw.model.Light.LightRepresentation;
 import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
 import edu.colorado.phet.common.phetcommon.model.Resettable;
+import edu.colorado.phet.common.phetcommon.model.property.ChangeObserver;
+import edu.colorado.phet.common.phetcommon.model.property.CompositeProperty;
+import edu.colorado.phet.common.phetcommon.model.property.ObservableProperty;
 import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.phetcommon.util.DoubleRange;
+import edu.colorado.phet.common.phetcommon.util.function.Function0;
+import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
 import edu.umd.cs.piccolo.util.PBounds;
 
@@ -34,6 +39,12 @@ public class BeersLawModel implements Resettable {
     public final Cuvette cuvette;
     public final ATDetector detector;
     public final Ruler ruler;
+
+    // Absorbance model
+    private final CompositeProperty<Double> absorbance; // A=abC
+    private final CompositeProperty<Double> molarAbsorptivity; // a
+    private final CompositeProperty<Double> pathLength; // b
+    private final Property<Double> concentration; // C
 
     public BeersLawModel() {
 
@@ -57,9 +68,59 @@ public class BeersLawModel implements Resettable {
 
         this.cuvette = new Cuvette( new ImmutableVector2D( light.location.getX() + 1.5, 1.25 ), 1.0, 2.75, new DoubleRange( 0.5, 2.0 ) );
 
+        //TODO this is too complicated
+        // absorbance model: A=abC
+        {
+            // a: molar absorptivity
+            this.molarAbsorptivity = new CompositeProperty<Double>( new Function0<Double>() {
+                public Double apply() {
+                    return solution.get().molarAbsorptionMax;
+                }
+            }, solution );
+
+            // b: path length, synonymous with cuvette width
+            this.pathLength = new CompositeProperty<Double>( new Function0<Double>() {
+                public Double apply() {
+                    return cuvette.width.get();
+                }
+            }, cuvette.width );
+
+            // C: concentration
+            {
+                this.concentration = new Property<Double>( solution.get().concentration.get() );
+
+                // This will be attached to the concentration property of the current solution.
+                final VoidFunction1<Double> concentrationObserver = new VoidFunction1<Double>() {
+                    public void apply( Double concentration ) {
+                        BeersLawModel.this.concentration.set( concentration );
+                    }
+                };
+
+                // Rewire the concentration observer when the solution changes.
+                ChangeObserver<BeersLawSolution> solutionObserver = new ChangeObserver<BeersLawSolution>() {
+                    public void update( BeersLawSolution newValue, BeersLawSolution oldValue ) {
+                        if ( oldValue != null ) {
+                            oldValue.concentration.removeObserver( concentrationObserver );
+                        }
+                        newValue.concentration.addObserver( concentrationObserver );
+                    }
+                };
+                solution.addObserver( solutionObserver );
+                solutionObserver.update( solution.get(), null ); // because ChangeObserver.update is not called on registration
+            }
+
+            // A=abC
+            this.absorbance = new CompositeProperty<Double>( new Function0<Double>() {
+                public Double apply() {
+                    return molarAbsorptivity.get() * pathLength.get() * concentration.get();
+                }
+            }, molarAbsorptivity, pathLength, concentration );
+        }
+
         //TODO compute drag bounds to match the stage size
         this.detector = new ATDetector( new ImmutableVector2D( 6, 3.75 ), new PBounds( 0, 0, 7.9, 5.25 ),
-                                        new ImmutableVector2D( 6, light.location.getY() ), new PBounds( 0, 0, 7.9, 5.25 ) );
+                                        new ImmutableVector2D( 6, light.location.getY() ), new PBounds( 0, 0, 7.9, 5.25 ),
+                                        absorbance );
 
         //TODO compute drag bounds to match the stage size
         this.ruler = new Ruler( 2, 0.3, new ImmutableVector2D( 3, 4.9 ), new PBounds( 0, 0, 8, 5.5 ) );
