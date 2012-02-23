b3933193b450d9f9743e41acb2b0c1bfd4f38d8c
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-02-23 06:56:49 +0000

    Removed unused code for water glass node, fixed: Water glasses always have a cursor

--- trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/intro/view/WaterGlassNode.java
+++ trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/intro/view/WaterGlassNode.java
@@ -5,18 +5,13 @@ import java.awt.Color;
 import java.awt.Font;
 
 import edu.colorado.phet.common.phetcommon.util.DoubleRange;
-import edu.colorado.phet.common.phetcommon.util.function.VoidFunction0;
-import edu.colorado.phet.common.phetcommon.util.function.VoidFunction0.Null;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.RichPNode;
-import edu.colorado.phet.common.piccolophet.event.CursorHandler;
 import edu.colorado.phet.fractions.FractionsResources;
 import edu.colorado.phet.fractionsintro.intro.view.beaker.BeakerNode;
 import edu.colorado.phet.fractionsintro.intro.view.beaker.Solute;
 import edu.colorado.phet.fractionsintro.intro.view.beaker.Solution;
 import edu.colorado.phet.fractionsintro.intro.view.beaker.SolutionNode;
-import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
-import edu.umd.cs.piccolo.event.PInputEvent;
 import edu.umd.cs.piccolo.util.PDimension;
 
 /**
@@ -38,10 +33,6 @@ public class WaterGlassNode extends RichPNode {
                                                                             SOLUTE_AMOUNT_RANGE.getMax() / DILUTION_VOLUME_RANGE.getMin() ); // M
 
     public WaterGlassNode( Integer numerator, Integer denominator ) {
-        this( numerator, denominator, new Null(), new Null() );
-    }
-
-    public WaterGlassNode( Integer numerator, Integer denominator, final VoidFunction0 addWater, final VoidFunction0 removeWater ) {
 
         final boolean full = numerator.equals( denominator );
         final boolean empty = numerator == 0;
@@ -76,27 +67,5 @@ public class WaterGlassNode extends RichPNode {
 
         waterBeakerBackgroundNode.setPickable( true );
         waterBeakerBackgroundNode.setChildrenPickable( true );
-
-        addInputEventListener( new CursorHandler() );
-
-        //if click is above water level and glass not full, add water
-        //if click is below water level and glass not empty, remove water
-        waterNode.addInputEventListener( new PBasicInputEventHandler() {
-            @Override public void mousePressed( PInputEvent event ) {
-                if ( !empty ) {
-                    removeWater.apply();
-                }
-            }
-        } );
-
-        waterBeakerNode.setPickable( false );
-        waterBeakerNode.setChildrenPickable( false );
-        waterBeakerBackgroundNode.addInputEventListener( new PBasicInputEventHandler() {
-            @Override public void mousePressed( PInputEvent event ) {
-                if ( !full ) {
-                    addWater.apply();
-                }
-            }
-        } );
     }
 }
