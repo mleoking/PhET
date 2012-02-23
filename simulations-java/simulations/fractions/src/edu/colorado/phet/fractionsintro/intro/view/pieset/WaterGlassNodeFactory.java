00add9d5114e90e4476689b2565146ee39cce4b1
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-02-23 03:57:06 +0000

    Improved performance by caching calls to WaterGlassNode constructor

--- simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/intro/view/pieset/WaterGlassNodeFactory.java
+++ simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/intro/view/pieset/WaterGlassNodeFactory.java
@@ -3,8 +3,14 @@ package edu.colorado.phet.fractionsintro.intro.view.pieset;
 
 import fj.F;
 
+import java.awt.image.BufferedImage;
+import java.util.Hashtable;
+
+import edu.colorado.phet.common.phetcommon.util.Pair;
+import edu.colorado.phet.common.phetcommon.view.util.BufferedImageUtils;
 import edu.colorado.phet.fractionsintro.intro.view.WaterGlassNode;
 import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.nodes.PImage;
 
 /**
  * Function that creates water glass nodes for PieSetNode.
@@ -12,9 +18,19 @@ import edu.umd.cs.piccolo.PNode;
  * @author Sam Reid
  */
 public class WaterGlassNodeFactory extends F<SliceNodeArgs, PNode> {
+
+    //Fix performance by cashing images to avoid calling the WaterGlassNode constructor too many times
+    private static final Hashtable<Pair<Integer, Integer>, BufferedImage> nodes = new Hashtable<Pair<Integer, Integer>, BufferedImage>();
+
     @Override public PNode f( final SliceNodeArgs args ) {
-        return new WaterGlassNode( 1, args.denominator ) {{
-            setOffset( args.slice.shape().getBounds2D().getCenterX() - getFullWidth() / 2, args.slice.shape().getBounds2D().getCenterY() - getFullHeight() / 2 );
+        final Pair<Integer, Integer> key = new Pair<Integer, Integer>( 1, args.denominator );
+        if ( !nodes.containsKey( key ) ) {
+            WaterGlassNode node = new WaterGlassNode( 1, args.denominator );
+            nodes.put( key, BufferedImageUtils.toBufferedImage( node.toImage() ) );
+        }
+
+        return new PImage( nodes.get( key ) ) {{
+            setOffset( args.slice.shape().getBounds2D().getCenterX() - getFullBounds().getWidth() / 2, args.slice.shape().getBounds2D().getCenterY() - getFullBounds().getHeight() / 2 );
             setVisible( !args.inContainer );
         }};
     }
