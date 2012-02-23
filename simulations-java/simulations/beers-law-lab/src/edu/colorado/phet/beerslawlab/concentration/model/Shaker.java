95d339a1189a141d8bd0ba65775d06051ba92d54
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-02-23 22:42:16 +0000

    fix for: make shaker empty, then drain out a little bit of solution, one grain of solute comes out of the shaker while activating the drain

--- simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/concentration/model/Shaker.java
+++ simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/concentration/model/Shaker.java
@@ -15,23 +15,11 @@ import edu.umd.cs.piccolo.util.PBounds;
  */
 public class Shaker extends Movable {
 
-    //TODO might be better if these points were in the coordinate system of the image file
-    // Locations of the shaker holes, relative to the shaker's local origin, and specific to the shaker image file.
-    private static final ImmutableVector2D[] RELATIVE_HOLE_LOCATIONS = {
-            new ImmutableVector2D( 0, 0 ),
-            new ImmutableVector2D( -10, -16 ),
-            new ImmutableVector2D( -16, -10 ),
-            new ImmutableVector2D( -8, 5 ),
-            new ImmutableVector2D( 8, -5 ),
-            new ImmutableVector2D( 10, 17 ),
-            new ImmutableVector2D( 21, 15 ),
-    };
-
     private final double orientation; // rotation angle, in radians
     public final Property<Solute> solute;
     public final Property<Boolean> visible;
     public final Property<Boolean> empty;
-    private final double maxDispensingRate;
+    private final double maxDispensingRate; // mol/sec
     private final Property<Double> dispensingRate; // mol/sec
     private ImmutableVector2D previousLocation;
 
@@ -66,31 +54,21 @@ public class Shaker extends Movable {
         return dispensingRate.get();
     }
 
-    private void setDispensingRate( double dispensingRate ) {
-        if ( !empty.get() ) {
-            this.dispensingRate.set( dispensingRate );
-        }
-    }
-
     public void addDispensingRateObserver( SimpleObserver observer ) {
         dispensingRate.addObserver( observer );
-    }
-
-    public ImmutableVector2D[] getRelativeHoleLocations() {
-        return RELATIVE_HOLE_LOCATIONS;
     }
 
     // Set the dispensing rate if the shaker is moving.
     public void stepInTime() {
         if ( visible.get() && !empty.get() ) {
             if ( previousLocation.equals( location.get() ) ) {
-                setDispensingRate( 0 ); // shaker is not moving, don't dispense anything
+                this.dispensingRate.set( 0d ); // shaker is not moving, don't dispense anything
             }
             else {
-                setDispensingRate( maxDispensingRate ); // this seems to work fine
-                previousLocation = new ImmutableVector2D( getX(), getY() );
+                this.dispensingRate.set( maxDispensingRate ); // this seems to work fine
             }
         }
+        previousLocation = new ImmutableVector2D( getX(), getY() );
     }
 
     @Override public void reset() {
