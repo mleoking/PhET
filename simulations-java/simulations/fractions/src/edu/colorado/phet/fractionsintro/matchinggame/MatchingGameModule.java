f7c51cc8765498a5d48332cc653274065be9e033
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-02-25 04:02:00 +0000

    renamed model class

--- simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/matchinggame/MatchingGameModule.java
+++ simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/matchinggame/MatchingGameModule.java
@@ -4,7 +4,7 @@ package edu.colorado.phet.fractionsintro.matchinggame;
 import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
 import edu.colorado.phet.fractionsintro.common.AbstractFractionsModule;
-import edu.colorado.phet.fractionsintro.matchinggame.model.MatchingGameModel;
+import edu.colorado.phet.fractionsintro.matchinggame.model.MatchingGameModelOld;
 import edu.colorado.phet.fractionsintro.matchinggame.view.MatchingGameCanvas;
 
 /**
@@ -13,7 +13,7 @@ import edu.colorado.phet.fractionsintro.matchinggame.view.MatchingGameCanvas;
 public class MatchingGameModule extends AbstractFractionsModule {
     public MatchingGameModule() {
         super( "Matching Game", new ConstantDtClock() );
-        MatchingGameModel model = new MatchingGameModel( ModelViewTransform.createIdentity() );
+        MatchingGameModelOld model = new MatchingGameModelOld( ModelViewTransform.createIdentity() );
         setSimulationPanel( new MatchingGameCanvas( model ) );
     }
 }
