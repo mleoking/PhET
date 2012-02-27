97f0ef3e2306b81eb43fc20b30080c002ea0e4ce
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-02-27 23:16:20 +0000

    added support for right scale jettison

--- trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/matchinggame/model/MatchingGameModel.java
+++ trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/matchinggame/model/MatchingGameModel.java
@@ -20,7 +20,7 @@ public class MatchingGameModel {
         addClockListener( new ClockAdapter() {
             @Override public void simulationTimeChanged( ClockEvent clockEvent ) {
                 state.set( state.get().stepInTime( clockEvent.getSimulationTimeChange() ) );
-                System.out.println( "Left scale value = " + state.get().getLeftScaleValue() );
+                System.out.println( "Left scale value = " + state.get().getScaleValue( state.get().leftScale ) );
             }
         } );
     }};
