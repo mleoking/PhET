b8d625b7b90e20a9317c5be869242631c720e9c1
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-02-28 18:20:29 +0000

    Minor layout fixes and improved level 1

--- trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/matchinggame/model/MatchingGameState.java
+++ trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/matchinggame/model/MatchingGameState.java
@@ -45,8 +45,8 @@ import static fj.data.List.range;
     //Number of times the user scored by getting a fraction into a score cell, used for iterating to the next score cell for animation
     public final int scored;
 
-    public final Scale leftScale = new Scale( new Vector2D( 150, 300 ) );
-    public final Scale rightScale = new Scale( new Vector2D( 500, 300 ) );
+    public final Scale leftScale = new Scale( new Vector2D( 220, 300 ) );
+    public final Scale rightScale = new Scale( new Vector2D( 570, 300 ) );
     public final Development development;
 
     //Time (in sim time) of when something was dropped on the scale, for purposes of animating the bar
@@ -54,23 +54,23 @@ import static fj.data.List.range;
     public double rightScaleDropTime;
 
     public static MatchingGameState initialState() {
-        final List<Cell> cells = createCells( 0, 500, 100, 100, 6, 2, 0, 0 );
+        final List<Cell> cells = createCells( 100, 430, 130, 120, 6, 2, 0, 0 );
 
         //Nodes for filling the cells.
 
-        final F<Fraction, PNode> numericFraction = new F<Fraction, PNode>() {
+        final F<Fraction, PNode> numeric = new F<Fraction, PNode>() {
             @Override public PNode f( Fraction f ) {
-                return new FractionNode( f, 0.2 );
+                return new FractionNode( f, 0.3 );
             }
         };
         final F<Fraction, PNode> horizontalBars = new F<Fraction, PNode>() {
             @Override public PNode f( Fraction f ) {
-                return new HorizontalBarsNode( new Fraction( f.numerator, f.denominator ), 0.85 );
+                return new HorizontalBarsNode( new Fraction( f.numerator, f.denominator ), 0.9 );
             }
         };
         final F<Fraction, PNode> verticalBars = new F<Fraction, PNode>() {
             @Override public PNode f( Fraction f ) {
-                return new VerticalBarsNode( new Fraction( f.numerator, f.denominator ), 0.85 );
+                return new VerticalBarsNode( new Fraction( f.numerator, f.denominator ), 0.9 );
             }
         };
 
@@ -82,21 +82,21 @@ import static fj.data.List.range;
 
         return new MatchingGameState( cells.map( new F<Cell, MovableFraction>() {
             @Override public MovableFraction f( Cell c ) {
-                return c.i == 0 && c.j == 0 ? fraction( 2, 3, c, numericFraction ) :
-                       c.i == 1 && c.j == 0 ? fraction( 4, 9, c, numericFraction ) :
-                       c.i == 2 && c.j == 0 ? fraction( 5, 9, c, numericFraction ) :
-                       c.i == 3 && c.j == 0 ? fraction( 5, 9, c, horizontalBars ) :
-                       c.i == 4 && c.j == 0 ? fraction( 2, 3, c, verticalBars ) :
-                       c.i == 5 && c.j == 0 ? fraction( 5, 9, c, verticalBars ) :
-                       c.i == 0 && c.j == 1 ? fraction( 5, 9, c, pies ) :
-                       c.i == 1 && c.j == 1 ? fraction( 2, 3, c, pies ) :
-                       c.i == 2 && c.j == 1 ? fraction( 1, 9, c, pies ) :
+                return c.i == 0 && c.j == 0 ? fraction( 2, 3, c, numeric ) :
+                       c.i == 1 && c.j == 0 ? fraction( 3, 4, c, pies ) :
+                       c.i == 2 && c.j == 0 ? fraction( 1, 4, c, numeric ) :
+                       c.i == 3 && c.j == 0 ? fraction( 2, 3, c, horizontalBars ) :
+                       c.i == 4 && c.j == 0 ? fraction( 1, 3, c, pies ) :
+                       c.i == 5 && c.j == 0 ? fraction( 1, 4, c, verticalBars ) :
+                       c.i == 0 && c.j == 1 ? fraction( 2, 3, c, pies ) :
+                       c.i == 1 && c.j == 1 ? fraction( 3, 4, c, numeric ) :
+                       c.i == 2 && c.j == 1 ? fraction( 2, 3, c, verticalBars ) :
                        c.i == 3 && c.j == 1 ? fraction( 1, 4, c, horizontalBars ) :
-                       c.i == 4 && c.j == 1 ? fraction( 5, 9, c, numericFraction ) :
-                       c.i == 5 && c.j == 1 ? fraction( 5, 9, c, pies ) :
-                       fraction( 2, 5, c, numericFraction );
+                       c.i == 4 && c.j == 1 ? fraction( 1, 3, c, horizontalBars ) :
+                       c.i == 5 && c.j == 1 ? fraction( 1, 4, c, pies ) :
+                       fraction( 2, 5, c, numeric );
             }
-        } ), cells, createCells( 100, 10, 100, 75, 6, 1, 50, 0 ), 0, new Development( true ) );
+        } ), cells, createCells( 80, 10, 100, 75, 6, 1, 50, 0 ), 0, new Development( true ) );
     }
 
     //Create a MovableFraction for the given fraction at the specified cell
