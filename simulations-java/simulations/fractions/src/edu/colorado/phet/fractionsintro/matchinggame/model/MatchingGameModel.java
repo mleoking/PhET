70a3992479b2d4a4d3a4a32be3ccd8836b1e8b45
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-02-28 05:40:25 +0000

    Added audio

--- simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/matchinggame/model/MatchingGameModel.java
+++ simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/matchinggame/model/MatchingGameModel.java
@@ -1,10 +1,23 @@
 // Copyright 2002-2012, University of Colorado
 package edu.colorado.phet.fractionsintro.matchinggame.model;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.Timer;
+
 import edu.colorado.phet.common.phetcommon.model.clock.Clock;
 import edu.colorado.phet.common.phetcommon.model.clock.ClockAdapter;
 import edu.colorado.phet.common.phetcommon.model.clock.ClockEvent;
 import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
+import edu.colorado.phet.common.phetcommon.model.property.ChangeObserver;
 import edu.colorado.phet.common.phetcommon.model.property.Property;
 
 import static edu.colorado.phet.fractionsintro.matchinggame.model.MatchingGameState.initialState;
@@ -15,7 +28,55 @@ import static edu.colorado.phet.fractionsintro.matchinggame.model.MatchingGameSt
  * @author Sam Reid
  */
 public class MatchingGameModel {
-    public final Property<MatchingGameState> state = new Property<MatchingGameState>( initialState() );
+    public final Property<MatchingGameState> state = new Property<MatchingGameState>( initialState() ) {{
+        addObserver( new ChangeObserver<MatchingGameState>() {
+            @Override public void update( final MatchingGameState newValue, final MatchingGameState oldValue ) {
+                if ( oldValue.getLeftScaleValue() == 0.0 && newValue.getLeftScaleValue() > 0 ) {
+                    playAudioClip( newValue.getLeftScaleValue() );
+                }
+                if ( oldValue.getRightScaleValue() == 0.0 && newValue.getRightScaleValue() > 0 ) {
+                    playAudioClip( newValue.getRightScaleValue() );
+                }
+            }
+
+
+            //I followed this example: http://www.java2s.com/Code/Java/Development-Class/AnexampleofloadingandplayingasoundusingaClip.htm
+            private void playAudioClip( final double value ) {//play a sound
+                try {
+                    AudioInputStream sound = AudioSystem.getAudioInputStream( Thread.currentThread().getContextClassLoader().getResourceAsStream( "fractions/audio/pitchup.wav" ) );
+                    // load the sound into memory (a Clip)
+                    DataLine.Info info = new DataLine.Info( Clip.class, sound.getFormat() );
+                    final Clip clip = (Clip) AudioSystem.getLine( info );
+                    clip.open( sound );
+
+                    // play the sound clip
+                    clip.start();
+
+                    //Halt prematurely based on the value so the sound will go higher pitched (and longer) for higher values
+                    new Timer( 1000, new ActionListener() {
+                        @Override public void actionPerformed( ActionEvent e ) {
+                            clip.stop();
+                        }
+                    } ) {{
+                        setRepeats( false );
+                        final int delay = (int) ( 1000 * value );
+//                            System.out.println( "delay = " + delay );
+                        setInitialDelay( delay );
+                    }}.start();
+//                        clip.stop();
+                }
+                catch ( UnsupportedAudioFileException e ) {
+                    e.printStackTrace();
+                }
+                catch ( IOException e ) {
+                    e.printStackTrace();
+                }
+                catch ( LineUnavailableException e ) {
+                    e.printStackTrace();
+                }
+            }
+        } );
+    }};
     public Clock clock = new ConstantDtClock( 60 ) {{
         addClockListener( new ClockAdapter() {
             @Override public void simulationTimeChanged( ClockEvent clockEvent ) {
