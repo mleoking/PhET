af2185ab13aa8dabc50160de72e4bc7228cd8cc6
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-02-28 20:49:30 +0000

    factored out Level1Pool

--- trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/matchinggame/model/MatchingGameState.java
+++ trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/matchinggame/model/MatchingGameState.java
@@ -7,20 +7,8 @@ import fj.data.Option;
 import lombok.Data;
 
 import edu.colorado.phet.common.phetcommon.math.ImmutableRectangle2D;
-import edu.colorado.phet.common.phetcommon.model.property.Property;
-import edu.colorado.phet.fractions.util.Cache;
 import edu.colorado.phet.fractions.util.immutable.Vector2D;
-import edu.colorado.phet.fractionsintro.intro.model.Container;
-import edu.colorado.phet.fractionsintro.intro.model.ContainerSet;
-import edu.colorado.phet.fractionsintro.intro.model.Fraction;
-import edu.colorado.phet.fractionsintro.intro.view.FractionNode;
-import edu.colorado.phet.fractionsintro.matchinggame.view.fractions.HorizontalBarsNode;
-import edu.colorado.phet.fractionsintro.matchinggame.view.fractions.PieNode;
-import edu.colorado.phet.fractionsintro.matchinggame.view.fractions.VerticalBarsNode;
-import edu.umd.cs.piccolo.PNode;
-import edu.umd.cs.piccolo.nodes.PImage;
 
-import static edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform.createIdentity;
 import static edu.colorado.phet.fractionsintro.matchinggame.model.Motions.*;
 import static edu.colorado.phet.fractionsintro.matchinggame.model.Motions.Scale;
 import static fj.data.List.list;
@@ -57,60 +45,7 @@ import static fj.data.List.range;
 
     public static MatchingGameState initialState() {
         final List<Cell> cells = createCells( 100, 430, 130, 120, 6, 2, 0, 0 );
-
-        //Nodes for filling the cells.
-        final F<Fraction, PNode> numeric = new F<Fraction, PNode>() {
-            @Override public PNode f( Fraction f ) {
-                return new FractionNode( f, 0.3 );
-            }
-        };
-        final F<Fraction, PNode> horizontalBars = new F<Fraction, PNode>() {
-            @Override public PNode f( Fraction f ) {
-                return new HorizontalBarsNode( new Fraction( f.numerator, f.denominator ), 0.9 );
-            }
-        };
-        final F<Fraction, PNode> verticalBars = new F<Fraction, PNode>() {
-            @Override public PNode f( Fraction f ) {
-                return new VerticalBarsNode( new Fraction( f.numerator, f.denominator ), 0.9 );
-            }
-        };
-
-        final F<Fraction, PNode> pies = new F<Fraction, PNode>() {
-            @Override public PNode f( Fraction fraction ) {
-                return new PieNode( createIdentity(), fraction, new Property<ContainerSet>( new ContainerSet( fraction.denominator, new Container[] { new Container( fraction.denominator, range( 0, fraction.numerator ) ) } ) ) );
-            }
-        };
-
-        return new MatchingGameState( cells.map( new F<Cell, MovableFraction>() {
-            @Override public MovableFraction f( Cell c ) {
-                return c.i == 0 && c.j == 0 ? fraction( 2, 3, c, numeric ) :
-                       c.i == 1 && c.j == 0 ? fraction( 3, 4, c, pies ) :
-                       c.i == 2 && c.j == 0 ? fraction( 1, 4, c, numeric ) :
-                       c.i == 3 && c.j == 0 ? fraction( 2, 3, c, horizontalBars ) :
-                       c.i == 4 && c.j == 0 ? fraction( 1, 3, c, pies ) :
-                       c.i == 5 && c.j == 0 ? fraction( 1, 4, c, verticalBars ) :
-                       c.i == 0 && c.j == 1 ? fraction( 2, 3, c, pies ) :
-                       c.i == 1 && c.j == 1 ? fraction( 3, 4, c, numeric ) :
-                       c.i == 2 && c.j == 1 ? fraction( 2, 3, c, verticalBars ) :
-                       c.i == 3 && c.j == 1 ? fraction( 1, 4, c, horizontalBars ) :
-                       c.i == 4 && c.j == 1 ? fraction( 1, 3, c, horizontalBars ) :
-                       c.i == 5 && c.j == 1 ? fraction( 1, 4, c, pies ) :
-                       fraction( 2, 5, c, numeric );
-            }
-        } ), cells, createCells( 80, 10, 100, 75, 6, 1, 50, 0 ), 0, new Development( true ) );
-    }
-
-    //Create a MovableFraction for the given fraction at the specified cell
-    private static MovableFraction fraction( int numerator, int denominator, Cell cell, final F<Fraction, PNode> node ) {
-
-        //Cache nodes as images to improve performance
-        return new MovableFraction( new Vector2D( cell.rectangle.getCenter() ), numerator, denominator, false, cell, 1.0,
-                                    new Cache<Fraction, PNode>( new F<Fraction, PNode>() {
-                                        @Override public PNode f( Fraction fraction ) {
-                                            return new PImage( node.f( fraction ).toImage() );
-                                        }
-                                    } ),
-                                    MoveToCell( cell ) );
+        return new MatchingGameState( new Level1Pool().create( cells ), cells, createCells( 80, 10, 100, 75, 6, 1, 50, 0 ), 0, new Development( true ) );
     }
 
     //Create adjacent cells from which fractions can be dragged
