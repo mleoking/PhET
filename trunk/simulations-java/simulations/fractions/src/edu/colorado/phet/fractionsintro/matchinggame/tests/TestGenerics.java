e871439741425b87774b466d10dae4b078763b88
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-02-28 17:04:14 +0000

    Added docs for generics test

--- trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/matchinggame/tests/TestGenerics.java
+++ trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/matchinggame/tests/TestGenerics.java
@@ -7,16 +7,20 @@ import lombok.Data;
 import edu.colorado.phet.fractions.util.immutable.Vector2D;
 
 /**
+ * Test for generics issues.
+ *
  * @author Sam Reid
  */
 public class TestGenerics {
 
+    //Interface we want our library to work with.  Immutable object with a position.
     public static interface Movable {
         public Vector2D getPosition();
 
         public Movable withPosition( Vector2D v );
     }
 
+    //Client side implementation of Movable
     public static @Data class MovableBlock implements Movable {
         public final Vector2D position;
 
@@ -25,6 +29,7 @@ public class TestGenerics {
         }
     }
 
+    //Static methods for moving the block, but doesn't work on the Movable abstraction, just the concrete MovableBlock
     public static class Motions {
         public static final F<MovableBlock, MovableBlock> move = new F<MovableBlock, MovableBlock>() {
             @Override public MovableBlock f( MovableBlock movableBlock ) {
@@ -38,15 +43,33 @@ public class TestGenerics {
         };
     }
 
+    //Strategy (as a class) that moves the Movable to the right one step.  How to get rid of the unchecked cast?
     public static class Move3<T extends Movable> extends F<T, T> {
         @Override public T f( T t ) {
             return (T) t.withPosition( t.getPosition().plus( 1, 0 ) );
         }
     }
 
+    //Strategy (as a method) for moving the Movable
+    public static <T extends Movable> F<T, T> Move4() {
+        return new F<T, T>() {
+            @Override public T f( T t ) {
+                return (T) t.withPosition( t.getPosition().plus( 1, 0 ) );
+            }
+        };
+    }
+
+    //Strategy (as a field) for moving the Movable
+    //TODO: how to do this as a field?
+//    public static F<T extends Movable, T extends Movable> Move5 = new F<T, T>() {
+//        @Override public T f( T t ) {
+//            return (T) t.withPosition( t.getPosition().plus( 1, 0 ) );
+//        }
+//    };
+
     public static void main( String[] args ) {
         MovableBlock m = new MovableBlock( new Vector2D( 0, 0 ) );
         MovableBlock m2 = new Move3<MovableBlock>().f( m );
-        System.out.println( "m2 = " + m2 );
+        System.out.println( "m2 = " + m2 );//prints m2 = TestGenerics.MovableBlock(position=Vector2D(x=1.0, y=0.0))
     }
 }
