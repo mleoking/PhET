25854f0ebf4376b63ca00c8becde3e85d172bc9d
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-02-29 20:36:52 +0000

    repackaged, moved app main to module class

--- simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/equalitylab/EqualityLabModule.java
+++ simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/equalitylab/EqualityLabModule.java
@@ -1,8 +1,14 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.fractionsintro.equalitylab;
 
+import edu.colorado.phet.common.phetcommon.application.ApplicationConstructor;
+import edu.colorado.phet.common.phetcommon.application.PhetApplication;
+import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
+import edu.colorado.phet.common.phetcommon.application.PhetApplicationLauncher;
 import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
 import edu.colorado.phet.fractionsintro.common.AbstractFractionsModule;
+import edu.colorado.phet.fractionsintro.equalitylab.model.EqualityLabModel;
+import edu.colorado.phet.fractionsintro.equalitylab.view.EqualityLabCanvas;
 
 /**
  * Equality lab module.
@@ -14,5 +20,14 @@ public class EqualityLabModule extends AbstractFractionsModule {
         super( "Equality Lab", new ConstantDtClock() );
         final EqualityLabModel model = new EqualityLabModel();
         setSimulationPanel( new EqualityLabCanvas( model ) );
+    }
+
+    public static void main( String[] args ) {
+        final ApplicationConstructor constructor = new ApplicationConstructor() {
+            @Override public PhetApplication getApplication( PhetApplicationConfig c ) {
+                return new PhetApplication( c ) {{addModule( new EqualityLabModule() );}};
+            }
+        };
+        new PhetApplicationLauncher().launchSim( args, "fractions", "fractions-intro", constructor );
     }
 }
