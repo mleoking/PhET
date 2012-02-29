c8d817f7dd8088bf33b4fd7e9013467cdebfbc7e
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-02-29 21:37:30 +0000

    parameterized model on initial max number of containers

--- trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/equalitylab/model/EqualityLabModel.java
+++ trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/equalitylab/model/EqualityLabModel.java
@@ -4,6 +4,7 @@ package edu.colorado.phet.fractionsintro.equalitylab.model;
 import edu.colorado.phet.common.phetcommon.model.clock.Clock;
 import edu.colorado.phet.common.phetcommon.model.property.SettableProperty;
 import edu.colorado.phet.fractionsintro.intro.model.FractionsIntroModel;
+import edu.colorado.phet.fractionsintro.intro.model.IntroState;
 import edu.colorado.phet.fractionsintro.intro.model.pieset.PieSet;
 import edu.colorado.phet.fractionsintro.intro.view.Representation;
 
@@ -13,7 +14,7 @@ import edu.colorado.phet.fractionsintro.intro.view.Representation;
  * @author Sam Reid
  */
 public class EqualityLabModel {
-    private final FractionsIntroModel model = new FractionsIntroModel();
+    private final FractionsIntroModel model = new FractionsIntroModel( IntroState.IntroState( 6 ) );
     public final SettableProperty<Representation> representation = model.representation;
     public final SettableProperty<PieSet> pieSet = model.pieSet;
     public final SettableProperty<PieSet> horizontalBarSet = model.horizontalBarSet;
