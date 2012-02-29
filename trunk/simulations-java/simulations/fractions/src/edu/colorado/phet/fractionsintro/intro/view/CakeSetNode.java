c43e882d4b4ad363432a6e776527f9746f8bb694
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-02-29 21:49:22 +0000

    factored out CakeSetNode

---
+++ trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/intro/view/CakeSetNode.java
@@ -0,0 +1,52 @@
+// Copyright 2002-2012, University of Colorado
+package edu.colorado.phet.fractionsintro.intro.view;
+
+import fj.F;
+import lombok.Data;
+
+import java.awt.image.BufferedImage;
+
+import edu.colorado.phet.common.phetcommon.model.property.SettableProperty;
+import edu.colorado.phet.fractions.util.Cache;
+import edu.colorado.phet.fractionsintro.intro.model.pieset.PieSet;
+import edu.colorado.phet.fractionsintro.intro.view.pieset.PieSetNode;
+import edu.colorado.phet.fractionsintro.intro.view.pieset.SliceNodeArgs;
+import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.nodes.PImage;
+
+import static edu.colorado.phet.fractions.FractionsResources.RESOURCES;
+import static edu.colorado.phet.fractions.util.Cache.cache;
+import static edu.colorado.phet.fractionsintro.intro.view.CakeNode.cropSides;
+
+/**
+ * Node for showing a set of draggable 3d cakes
+ *
+ * @author Sam Reid
+ */
+public class CakeSetNode extends PieSetNode {
+
+    static @Data class Arg {
+        final int cell;
+        final int denominator;
+    }
+
+    //Cache for performance, to avoid cropping each time
+    static final Cache<Arg, BufferedImage> cakeImages = cache( new F<Arg, BufferedImage>() {
+        @Override public BufferedImage f( Arg a ) {
+            return cropSides( RESOURCES.getImage( "cake/cake_" + a.denominator + "_" + ( a.cell + 1 ) + ".png" ) );
+        }
+    } );
+
+    public CakeSetNode( SettableProperty<PieSet> cakeSet, PNode rootNode ) {
+        super( cakeSet, rootNode, new F<SliceNodeArgs, PNode>() {
+            @Override public PNode f( final SliceNodeArgs a ) {
+                int cell = a.slice.cell( a.denominator );
+                return new PImage( cakeImages.f( new Arg( cell, a.denominator ) ) ) {{
+                    //Center on the slice tip because each image is padded to the amount of a full cake
+                    double fudgeY = getFullBounds().getHeight() / 4;
+                    setOffset( a.slice.position.getX() - getFullBounds().getWidth() / 2, a.slice.position.getY() - getFullBounds().getHeight() / 2 - fudgeY );
+                }};
+            }
+        } );
+    }
+}
--- trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/intro/view/FractionsIntroCanvas.java
+++ trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/intro/view/FractionsIntroCanvas.java
@@ -1,20 +1,14 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.fractionsintro.intro.view;
 
-import fj.F;
-import lombok.Data;
-
 import java.awt.Color;
-import java.awt.image.BufferedImage;
 
 import edu.colorado.phet.common.phetcommon.model.Resettable;
 import edu.colorado.phet.common.piccolophet.nodes.ResetAllButtonNode;
 import edu.colorado.phet.common.piccolophet.nodes.kit.ZeroOffsetNode;
-import edu.colorado.phet.fractions.util.Cache;
 import edu.colorado.phet.fractionsintro.common.view.AbstractFractionsCanvas;
 import edu.colorado.phet.fractionsintro.intro.model.FractionsIntroModel;
 import edu.colorado.phet.fractionsintro.intro.view.pieset.PieSetNode;
-import edu.colorado.phet.fractionsintro.intro.view.pieset.SliceNodeArgs;
 import edu.colorado.phet.fractionsintro.intro.view.representationcontrolpanel.CakeIcon;
 import edu.colorado.phet.fractionsintro.intro.view.representationcontrolpanel.HorizontalBarIcon;
 import edu.colorado.phet.fractionsintro.intro.view.representationcontrolpanel.NumberLineIcon;
@@ -23,12 +17,7 @@ import edu.colorado.phet.fractionsintro.intro.view.representationcontrolpanel.Re
 import edu.colorado.phet.fractionsintro.intro.view.representationcontrolpanel.RepresentationIcon;
 import edu.colorado.phet.fractionsintro.intro.view.representationcontrolpanel.VerticalBarIcon;
 import edu.colorado.phet.fractionsintro.intro.view.representationcontrolpanel.WaterGlassIcon;
-import edu.umd.cs.piccolo.PNode;
-import edu.umd.cs.piccolo.nodes.PImage;
 
-import static edu.colorado.phet.fractions.FractionsResources.RESOURCES;
-import static edu.colorado.phet.fractions.util.Cache.cache;
-import static edu.colorado.phet.fractionsintro.intro.view.CakeNode.cropSides;
 import static edu.colorado.phet.fractionsintro.intro.view.Representation.*;
 
 /**
@@ -75,29 +64,8 @@ public class FractionsIntroCanvas extends AbstractFractionsCanvas {
         //For debugging cakes
 //        addChild( new RepresentationNode( model.representation, CAKE, new PieSetNode( model.cakeSet, rootNode ) ) );
 
-        @Data class Arg {
-            final int cell;
-            final int denominator;
-        }
-
-        //Cache for performance, to avoid cropping each time
-        final Cache<Arg, BufferedImage> cakeImages = cache( new F<Arg, BufferedImage>() {
-            @Override public BufferedImage f( Arg a ) {
-                return cropSides( RESOURCES.getImage( "cake/cake_" + a.denominator + "_" + ( a.cell + 1 ) + ".png" ) );
-            }
-        } );
-
         //For draggable cakes
-        addChild( new RepresentationNode( model.representation, CAKE, new PieSetNode( model.cakeSet, rootNode, new F<SliceNodeArgs, PNode>() {
-            @Override public PNode f( final SliceNodeArgs a ) {
-                int cell = a.slice.cell( a.denominator );
-                return new PImage( cakeImages.f( new Arg( cell, a.denominator ) ) ) {{
-                    //Center on the slice tip because each image is padded to the amount of a full cake
-                    double fudgeY = getFullBounds().getHeight() / 4;
-                    setOffset( a.slice.position.getX() - getFullBounds().getWidth() / 2, a.slice.position.getY() - getFullBounds().getHeight() / 2 - fudgeY );
-                }};
-            }
-        } ) ) );
+        addChild( new RepresentationNode( model.representation, CAKE, new CakeSetNode( model.cakeSet, rootNode ) ) );
 
         //The fraction control node
         addChild( new ZeroOffsetNode( new FractionControlNode( model.numerator, model.denominator, model.maximum ) ) {{
