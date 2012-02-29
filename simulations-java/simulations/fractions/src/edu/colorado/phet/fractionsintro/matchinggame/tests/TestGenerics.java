06133ce0745521504508fae51b261c36c27f10df
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-02-29 06:24:09 +0000

    updated TestGenerics test

--- simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/matchinggame/tests/TestGenerics.java
+++ simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/matchinggame/tests/TestGenerics.java
@@ -51,7 +51,7 @@ public class TestGenerics {
     }
 
     //Strategy (as a method) for moving the Movable
-    public static <T extends Movable> F<T, T> Move4() {
+    @SuppressWarnings("unchecked") public static <T extends Movable> F<T, T> Move4() {
         return new F<T, T>() {
             @Override public T f( T t ) {
                 return (T) t.withPosition( t.getPosition().plus( 1, 0 ) );
@@ -59,17 +59,33 @@ public class TestGenerics {
         };
     }
 
+    @SuppressWarnings("unchecked") public static <T extends Movable> T Move5( T t ) {
+        return (T) t.withPosition( t.getPosition().plus( 1, 0 ) );
+    }
+
     //Strategy (as a field) for moving the Movable
     //TODO: how to do this as a field?
-//    public static F<T extends Movable, T extends Movable> Move5 = new F<T, T>() {
+//    public static <T extends Movable> F<? extends Movable, ? extends Movable> Move5 = new F<T, T>() {
 //        @Override public T f( T t ) {
 //            return (T) t.withPosition( t.getPosition().plus( 1, 0 ) );
 //        }
 //    };
 
+    public static class Wrapper<T extends Movable> {
+        //Type check could still fail at runtime, but only if withPosition() returns a type other than T (even if Movable)
+        @SuppressWarnings("unchecked") private F<T, T> Move6 = new F<T, T>() {
+            @Override public T f( T a ) {
+                return (T) a.withPosition( a.getPosition().plus( 10, 10 ) );
+            }
+        };
+    }
+
     public static void main( String[] args ) {
         MovableBlock m = new MovableBlock( new Vector2D( 0, 0 ) );
-        MovableBlock m2 = new Move3<MovableBlock>().f( m );
+        MovableBlock m2 = (MovableBlock) Move4().f( m );
+        MovableBlock m3 = Move5( m );
+        final F<MovableBlock, MovableBlock> function = new Wrapper<MovableBlock>().Move6;
+        MovableBlock m4 = function.f( m );
         System.out.println( "m2 = " + m2 );//prints m2 = TestGenerics.MovableBlock(position=Vector2D(x=1.0, y=0.0))
     }
 }
