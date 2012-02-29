17dd4cdf5d7abee27ed11d65ac2fe4f8ca4458b6
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-02-29 20:42:29 +0000

    equality lab: removed unused code

--- simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/equalitylab/model/EqualityLabModel.java
+++ simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/equalitylab/model/EqualityLabModel.java
@@ -1,10 +1,18 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.fractionsintro.equalitylab.model;
 
-import edu.colorado.phet.fractionsintro.common.model.SingleFractionModel;
+import edu.colorado.phet.common.phetcommon.model.property.Property;
+import edu.colorado.phet.common.phetcommon.model.property.SettableProperty;
+import edu.colorado.phet.fractionsintro.intro.view.Representation;
 
 /**
+ * Model for the Equality Lab tab
+ *
  * @author Sam Reid
  */
-public class EqualityLabModel extends SingleFractionModel {
+public class EqualityLabModel {
+    public SettableProperty<Representation> representation = new Property<Representation>( Representation.PIE );
+
+    public void resetAll() {
+    }
 }
