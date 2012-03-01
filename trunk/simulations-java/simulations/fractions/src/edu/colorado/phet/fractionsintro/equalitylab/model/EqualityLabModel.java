fd13cef7fa6d0d26c12ebc497270e95aa0786bb7
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-03-01 00:05:26 +0000

    added right representation

--- trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/equalitylab/model/EqualityLabModel.java
+++ trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/equalitylab/model/EqualityLabModel.java
@@ -2,6 +2,7 @@
 package edu.colorado.phet.fractionsintro.equalitylab.model;
 
 import edu.colorado.phet.common.phetcommon.model.clock.Clock;
+import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.phetcommon.model.property.SettableProperty;
 import edu.colorado.phet.common.phetcommon.model.property.integerproperty.IntegerProperty;
 import edu.colorado.phet.fractions.util.immutable.Vector2D;
@@ -20,7 +21,6 @@ import edu.colorado.phet.fractionsintro.intro.view.Representation;
 public class EqualityLabModel {
     private final FactorySet factorySet = new FactorySet( new Vector2D( 100, -AbstractSliceFactory.stageSize.height + 200 ), 3, 120, 100, 210 );
     private final FractionsIntroModel model = new FractionsIntroModel( IntroState.IntroState( 6, factorySet ), factorySet );
-    public final SettableProperty<Representation> representation = model.representation;
     public final SettableProperty<PieSet> pieSet = model.pieSet;
     public final SettableProperty<PieSet> horizontalBarSet = model.horizontalBarSet;
     public final Clock clock = model.clock;
@@ -28,6 +28,8 @@ public class EqualityLabModel {
     public final IntegerProperty denominator = model.denominator;
     public final IntegerProperty maximum = model.maximum;
     public final SettableProperty<PieSet> waterGlassSet = model.waterGlassSet;
+    public final SettableProperty<Representation> leftRepresentation = model.representation;
+    public final SettableProperty<Representation> rightRepresentation = new Property<Representation>( leftRepresentation.get() );
 
     public void resetAll() {
         model.resetAll();
