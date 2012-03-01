b1fbcc02946d8ae32b3395150fbdf1a224c730e9
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-03-01 05:56:07 +0000

    convert water glasses to images and cache them to improve performance (by about 20%)

--- trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/intro/view/WaterGlassSetNode.java
+++ trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/intro/view/WaterGlassSetNode.java
@@ -3,16 +3,20 @@ package edu.colorado.phet.fractionsintro.intro.view;
 
 import fj.F;
 import fj.data.List;
+import lombok.Data;
 
 import java.awt.Color;
+import java.awt.Image;
 
 import edu.colorado.phet.common.phetcommon.model.property.SettableProperty;
+import edu.colorado.phet.fractions.util.Cache;
 import edu.colorado.phet.fractionsintro.intro.model.pieset.Pie;
 import edu.colorado.phet.fractionsintro.intro.model.pieset.PieSet;
 import edu.colorado.phet.fractionsintro.intro.model.pieset.Slice;
 import edu.colorado.phet.fractionsintro.intro.view.pieset.PieSetNode;
 import edu.colorado.phet.fractionsintro.intro.view.pieset.WaterGlassNodeFactory;
 import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.nodes.PImage;
 
 import static fj.Ord.doubleOrd;
 
@@ -25,6 +29,21 @@ public class WaterGlassSetNode extends PieSetNode {
     public WaterGlassSetNode( SettableProperty<PieSet> model, PNode rootNode, Color color ) {
         super( model, rootNode, new WaterGlassNodeFactory( color ), createEmptyCellsNode( color ) );
     }
+
+    public static final @Data class Args {
+        public final int numFilled;
+        public final int denominator;
+        public final Color color;
+    }
+
+    public static F<Args, Image> nodeMaker = new F<Args, Image>() {
+        @Override public Image f( final Args args ) {
+            return new WaterGlassNode( args.numFilled, args.denominator, args.color ).toImage();
+        }
+    };
+
+    //Improve application runtime about 20% by caching these images
+    public static Cache<Args, Image> cache = new Cache<Args, Image>( nodeMaker );
 
     public static F<PieSet, PNode> createEmptyCellsNode( final Color color ) {
         return new F<PieSet, PNode>() {
@@ -39,7 +58,7 @@ public class WaterGlassSetNode extends PieSetNode {
                     } );
 
                     //TODO: Could read from cache like WaterGlassNodeFactory instead of creating new each time to improve performance
-                    node.addChild( new WaterGlassNode( state.countFilledCells( pie ), state.denominator, color ) {{
+                    node.addChild( new PImage( cache.f( new Args( state.countFilledCells( pie ), state.denominator, color ) ) ) {{
                         setOffset( pie.cells.index( 0 ).shape().getBounds2D().getX(), centers.minimum( doubleOrd ) );
                     }} );
                 }
