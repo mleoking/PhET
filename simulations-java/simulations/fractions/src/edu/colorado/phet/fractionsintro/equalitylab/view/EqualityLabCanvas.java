90e38b4c277e758af17086bb6574238e9a292bb4
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-03-01 00:05:26 +0000

    added right representation

--- simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/equalitylab/view/EqualityLabCanvas.java
+++ simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/equalitylab/view/EqualityLabCanvas.java
@@ -4,12 +4,14 @@ package edu.colorado.phet.fractionsintro.equalitylab.view;
 import java.awt.Color;
 
 import edu.colorado.phet.common.phetcommon.model.Resettable;
+import edu.colorado.phet.common.phetcommon.model.property.SettableProperty;
 import edu.colorado.phet.common.piccolophet.nodes.ResetAllButtonNode;
 import edu.colorado.phet.common.piccolophet.nodes.kit.ZeroOffsetNode;
 import edu.colorado.phet.fractionsintro.common.view.AbstractFractionsCanvas;
 import edu.colorado.phet.fractionsintro.equalitylab.model.EqualityLabModel;
 import edu.colorado.phet.fractionsintro.intro.view.FractionControlNode;
 import edu.colorado.phet.fractionsintro.intro.view.NumberLineNode;
+import edu.colorado.phet.fractionsintro.intro.view.Representation;
 import edu.colorado.phet.fractionsintro.intro.view.RepresentationNode;
 import edu.colorado.phet.fractionsintro.intro.view.WaterGlassSetNode;
 import edu.colorado.phet.fractionsintro.intro.view.pieset.PieSetNode;
@@ -32,15 +34,13 @@ public class EqualityLabCanvas extends AbstractFractionsCanvas {
     public EqualityLabCanvas( final EqualityLabModel model ) {
 
         //Control panel for choosing different representations, can be split into separate controls for each display
-        final RepresentationControlPanel representationControlPanel = new RepresentationControlPanel( model.representation, new RepresentationIcon[] {
-                new PieIcon( model.representation ),
-                new HorizontalBarIcon( model.representation ),
-                new WaterGlassIcon( model.representation ),
-                new NumberLineIcon( model.representation ),
-        } ) {{
-            setOffset( INSET, INSET );
+        final RepresentationControlPanel leftRepresentation = new RepresentationControlPanel( model.leftRepresentation, getRepresentations( model.leftRepresentation ) ) {{ setOffset( INSET, INSET ); }};
+        addChild( leftRepresentation );
+
+        final RepresentationControlPanel rightRepresentation = new RepresentationControlPanel( model.rightRepresentation, getRepresentations( model.rightRepresentation ) ) {{
+            setOffset( STAGE_SIZE.getWidth() - getFullWidth() - INSET, INSET );
         }};
-        addChild( representationControlPanel );
+        addChild( rightRepresentation );
 
         ResetAllButtonNode resetAllButtonNode = new ResetAllButtonNode( new Resettable() {
             public void reset() {
@@ -57,19 +57,28 @@ public class EqualityLabCanvas extends AbstractFractionsCanvas {
         }} );
 
         //Show the pie set node when pies are selected
-        addChild( new RepresentationNode( model.representation, PIE, new PieSetNode( model.pieSet, rootNode ) ) );
+        addChild( new RepresentationNode( model.leftRepresentation, PIE, new PieSetNode( model.pieSet, rootNode ) ) );
 
         //For horizontal bars
-        addChild( new RepresentationNode( model.representation, HORIZONTAL_BAR, new PieSetNode( model.horizontalBarSet, rootNode ) ) );
+        addChild( new RepresentationNode( model.leftRepresentation, HORIZONTAL_BAR, new PieSetNode( model.horizontalBarSet, rootNode ) ) );
 
         //For water glasses
-        addChild( new RepresentationNode( model.representation, WATER_GLASSES, new WaterGlassSetNode( model.waterGlassSet, rootNode ) ) );
+        addChild( new RepresentationNode( model.leftRepresentation, WATER_GLASSES, new WaterGlassSetNode( model.waterGlassSet, rootNode ) ) );
 
         //Number line
-        addChild( new NumberLineNode( model.numerator, model.denominator, model.representation.valueEquals( NUMBER_LINE ), model.maximum ) {{
-            setOffset( INSET + 10, representationControlPanel.getFullBounds().getMaxY() + 100 + 15 );
+        addChild( new NumberLineNode( model.numerator, model.denominator, model.leftRepresentation.valueEquals( NUMBER_LINE ), model.maximum ) {{
+            setOffset( INSET + 10, leftRepresentation.getFullBounds().getMaxY() + 100 + 15 );
         }} );
 
         resetAllButtonNode.setOffset( STAGE_SIZE.getWidth() - resetAllButtonNode.getFullBounds().getWidth(), STAGE_SIZE.getHeight() - resetAllButtonNode.getFullBounds().getHeight() );
+    }
+
+    private RepresentationIcon[] getRepresentations( SettableProperty<Representation> representation ) {
+        return new RepresentationIcon[] {
+                new PieIcon( representation ),
+                new HorizontalBarIcon( representation ),
+                new WaterGlassIcon( representation ),
+                new NumberLineIcon( representation ),
+        };
     }
 }
