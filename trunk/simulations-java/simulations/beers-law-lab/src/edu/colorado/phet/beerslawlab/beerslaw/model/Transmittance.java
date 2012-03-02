61aa2629445f489ac2c3161ddc55b3f0eac282c6
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-03-02 21:52:30 +0000

    organize import

--- trunk/simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/beerslaw/model/Transmittance.java
+++ trunk/simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/beerslaw/model/Transmittance.java
@@ -2,7 +2,6 @@
 package edu.colorado.phet.beerslawlab.beerslaw.model;
 
 import edu.colorado.phet.common.phetcommon.model.property.CompositeProperty;
-import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.phetcommon.util.function.Function0;
 
 /**
