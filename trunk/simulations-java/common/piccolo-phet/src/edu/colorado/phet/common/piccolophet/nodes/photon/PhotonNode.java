ac71aff7664183927d792be77d553835412279fc
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-03-02 15:11:32 +0000

    #2920: added a constructor for specifying custom sizes for orb/halo/sparkle; improved overridable colorscheme

--- trunk/simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/nodes/photon/PhotonNode.java
+++ trunk/simulations-java/common/piccolo-phet/src/edu/colorado/phet/common/piccolophet/nodes/photon/PhotonNode.java
@@ -9,6 +9,7 @@ import java.awt.geom.Ellipse2D;
 import java.awt.geom.Point2D;
 
 import edu.colorado.phet.common.phetcommon.view.graphics.RoundGradientPaint;
+import edu.colorado.phet.common.phetcommon.view.util.ColorUtils;
 import edu.colorado.phet.common.phetcommon.view.util.VisibleColor;
 import edu.colorado.phet.common.piccolophet.PhetPNode;
 import edu.umd.cs.piccolo.PNode;
@@ -29,42 +30,47 @@ import edu.umd.cs.piccolo.nodes.PPath;
  * <li>IR photos are rendered as gray orbs with red crosshairs.
  * </ul>
  * <p>
- * If a different color mapping is desired, then you have 2 choices:
+ * If a different colorscheme is desired, then you have 2 choices:
  * (1) map your wavelength to a different wavelength before calling this constructor, or
- * (2) override getPhotonColor and getSparkleColor.
+ * (2) subclass and override getOrbPaint, getHaloPaint and getSparklePaint
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
 public class PhotonNode extends PhetPNode {
 
-    // Orb
-    private static final int ORB_ALPHA = 130;
-    private static final Color ORB_CENTER_COLOR = new Color( 255, 255, 255, 180 );
-    private static final Color UV_ORB_COLOR = new Color( 160, 160, 160 ); // gray
-    private static final Color IR_ORB_COLOR = UV_ORB_COLOR;
+    // UV and IR colors
+    private static final Color UV_COLOR = new Color( 160, 160, 160 ); // gray
+    private static final Color IR_COLOR = UV_COLOR;
 
     // Sparkle
     private static final double SPARKLE_ANGLE = Math.toRadians( 18 );
     private static final Color SPARKLE_COLOR = new Color( 255, 255, 255, 100 ); // sparkle color for visible wavelengths
-    private static final Color UV_SPARKLE_COLOR = VisibleColor.wavelengthToColor( 400, UV_ORB_COLOR, UV_ORB_COLOR );
-    private static final Color IR_SPARKLE_COLOR = VisibleColor.wavelengthToColor( 715, UV_ORB_COLOR, UV_ORB_COLOR );
+    private static final Color UV_SPARKLE_COLOR = VisibleColor.wavelengthToColor( 400, UV_COLOR, UV_COLOR );
+    private static final Color IR_SPARKLE_COLOR = VisibleColor.wavelengthToColor( 715, UV_COLOR, UV_COLOR );
 
     /**
-     * Constructs a PhotoNode for the specified wavelength and diameter.
+     * Constructor, uses standard sizes for halo, orb and sparkle.
      * @param wavelength
      * @param diameter diameter of the outer halo
      */
     public PhotonNode( double wavelength, double diameter ) {
+        this( wavelength, diameter, 0.5 * diameter, 0.575 * diameter );
+    }
 
-        // map wavelength to colors
-        final Color photonColor = getPhotonColor( wavelength );
-        final Color sparkleColor = getSparkleColor( wavelength );
+    /**
+     * Use this constructor if you want to customize the sizes of the halo, orb and sparkle.
+     * @param wavelength
+     * @param haloDiameter
+     * @param orbDiameter
+     * @param sparkleDiameter
+     */
+    public PhotonNode( double wavelength, double haloDiameter, double orbDiameter, double sparkleDiameter ) {
 
         // assemble the components of the node
         PNode parentNode = new PNode();
-        parentNode.addChild( new HaloNode( photonColor, diameter ) );
-        parentNode.addChild( new OrbNode( photonColor, 0.5 * diameter ) );
-        parentNode.addChild( new SparkleNode( sparkleColor, 0.575 * diameter, SPARKLE_ANGLE ) );
+        parentNode.addChild( new CircleNode( getHaloPaint( wavelength, haloDiameter ), haloDiameter ) );
+        parentNode.addChild( new CircleNode( getOrbPaint( wavelength, orbDiameter ), orbDiameter ) );
+        parentNode.addChild( new SparkleNode( getSparkleColor( wavelength, sparkleDiameter ), sparkleDiameter, SPARKLE_ANGLE ) );
 
         // convert to image, center
         PImage imageNode = new PImage( parentNode.toImage() );
@@ -73,26 +79,11 @@ public class PhotonNode extends PhetPNode {
                              -imageNode.getFullBoundsReference().getHeight() / 2 );
     }
 
-    // The orb that is the main body of the photon. Origin at center.
-    private static class OrbNode extends PPath {
+    // Used for the orb and halo. Origin at center.
+    private static class CircleNode extends PPath {
 
-        public OrbNode( Color color, double diameter ) {
+        public CircleNode( Paint paint, double diameter ) {
             Shape shape = new Ellipse2D.Double( -diameter / 2, -diameter / 2, diameter, diameter );
-            Color outerColor = new Color( color.getRed(), color.getGreen(), color.getBlue(), ORB_ALPHA );
-            Paint paint = new RoundGradientPaint( 0, 0, ORB_CENTER_COLOR, new Point2D.Double( 0.25 * diameter, 0.25 * diameter ), outerColor );
-            setPathTo( shape );
-            setPaint( paint );
-            setStroke( null );
-        }
-    }
-
-    // The halo that surrounds the orb. Origin at center.
-    private static class HaloNode extends PPath {
-
-        public HaloNode( Color color, double diameter ) {
-            Shape shape = new Ellipse2D.Double( -diameter / 2, -diameter / 2, diameter, diameter );
-            Color outerColor = new Color( color.getRed(), color.getGreen(), color.getBlue(), 0 );
-            Paint paint = new RoundGradientPaint( 0, 0, color, new Point2D.Double( 0.4 * diameter, 0.4 * diameter ), outerColor );
             setPathTo( shape );
             setPaint( paint );
             setStroke( null );
@@ -102,9 +93,9 @@ public class PhotonNode extends PhetPNode {
     // Sparkle at the center of the photon. Origin at center.
     private static class SparkleNode extends PNode {
 
-        public SparkleNode( Color color, double diameter, double angle ) {
-            PNode bigCrosshairs = new CrosshairsNode( color, diameter );
-            PNode smallCrosshairs = new CrosshairsNode( color, 0.7 * diameter );
+        public SparkleNode( Paint paint, double diameter, double angle ) {
+            PNode bigCrosshairs = new CrosshairsNode( paint, diameter );
+            PNode smallCrosshairs = new CrosshairsNode( paint, 0.7 * diameter );
             smallCrosshairs.rotate( Math.toRadians( 45 ) );
             addChild( smallCrosshairs );
             addChild( bigCrosshairs );
@@ -115,18 +106,18 @@ public class PhotonNode extends PhetPNode {
     // Crosshairs used to create the sparkle. Origin at center.
     private static class CrosshairsNode extends PNode {
 
-        public CrosshairsNode( Color color, double diameter ) {
+        public CrosshairsNode( Paint paint, double diameter ) {
 
             final double crosshairWidth = diameter;
             final double crosshairHeight = 0.15 * crosshairWidth;
             Shape crosshairShape = new Ellipse2D.Double( -crosshairWidth / 2, -crosshairHeight / 2, crosshairWidth, crosshairHeight );
 
             PPath horizontalPart = new PPath( crosshairShape );
-            horizontalPart.setPaint( color );
+            horizontalPart.setPaint( paint );
             horizontalPart.setStroke( null );
 
             PPath verticalPart = new PPath( crosshairShape );
-            verticalPart.setPaint( color );
+            verticalPart.setPaint( paint );
             verticalPart.setStroke( null );
             verticalPart.rotate( Math.toRadians( 90 ) );
 
@@ -136,19 +127,32 @@ public class PhotonNode extends PhetPNode {
     }
 
     /*
-     * Override this if you want a different mapping of wavelength to photon color.
-     * Colors for the orb and halo are derived from this color.
+     * Override this if you want a different mapping of wavelength to orb paint.
+     * The default is a round gradient that is solid in the center and a bit transparent towards the outer edge.
      * Use caution when overriding: this method is called in the constructor.
      */
-    protected Color getPhotonColor( double wavelength ) {
-       return VisibleColor.wavelengthToColor( wavelength, UV_ORB_COLOR, IR_ORB_COLOR );
+    protected Paint getOrbPaint( double wavelength, double orbDiameter ) {
+        Color innerColor = new Color( 255, 255, 255, 180 );
+        Color outerColor = ColorUtils.createColor( VisibleColor.wavelengthToColor( wavelength, UV_COLOR, IR_COLOR ), 130 );
+        return new RoundGradientPaint( 0, 0, innerColor, new Point2D.Double( 0.25 * orbDiameter, 0.25 * orbDiameter ), outerColor );
     }
 
     /*
-     * Override this if you want a different mapping of wavelength to sparkle color.
+     * Override this if you want a different mapping of wavelength to halo paint.
+     * The default is a round gradient that is solid in the center and fully transparent a the outer edge.
      * Use caution when overriding: this method is called in the constructor.
      */
-    protected Color getSparkleColor( double wavelength ) {
+    protected Paint getHaloPaint( double wavelength, double haloDiameter ) {
+        Color innerColor = VisibleColor.wavelengthToColor( wavelength, UV_COLOR, IR_COLOR );
+        Color outerColor = ColorUtils.createColor( innerColor, 0 );
+        return new RoundGradientPaint( 0, 0, innerColor, new Point2D.Double( 0.4 * haloDiameter, 0.4 * haloDiameter ), outerColor );
+    }
+
+    /*
+     * Override this if you want a different mapping of wavelength to sparkle paint.
+     * Use caution when overriding: this method is called in the constructor.
+     */
+    protected Paint getSparkleColor( double wavelength, double sparkleDiameter ) {
         if ( wavelength < VisibleColor.MIN_WAVELENGTH ) {
             return UV_SPARKLE_COLOR;
         }
