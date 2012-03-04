f83c932c7cf449964064c21cb418831af4a8cc71
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-03-04 01:24:19 +0000

    removed unused code

--- simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/intro/model/pieset/Pie.java
+++ simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/intro/model/pieset/Pie.java
@@ -1,7 +1,6 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.fractionsintro.intro.model.pieset;
 
-import fj.F;
 import fj.data.List;
 import lombok.Data;
 
@@ -12,12 +11,4 @@ import lombok.Data;
  */
 @Data public class Pie {
     public final List<Slice> cells;
-
-    public Pie translate( final double dx, final double dy ) {
-        return new Pie( cells.map( new F<Slice, Slice>() {
-            @Override public Slice f( Slice slice ) {
-                return slice.translate( dx, dy );
-            }
-        } ) );
-    }
 }
