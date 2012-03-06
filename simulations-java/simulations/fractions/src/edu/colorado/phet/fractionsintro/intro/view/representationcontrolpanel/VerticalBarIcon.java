b851a2f226b21cb846a8306848ead2700478aeae
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-03-06 15:22:27 +0000

    Intro: Shrink the vertical bar "icon" in the vertical direction and give it an aspect ratio more similar to the representation

--- simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/intro/view/representationcontrolpanel/VerticalBarIcon.java
+++ simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/intro/view/representationcontrolpanel/VerticalBarIcon.java
@@ -1,26 +1,28 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.fractionsintro.intro.view.representationcontrolpanel;
 
-import java.awt.Color;
-import java.awt.Shape;
-import java.awt.geom.Rectangle2D;
-import java.util.ArrayList;
-
-import edu.colorado.phet.common.phetcommon.model.property.SettableProperty;
+import edu.colorado.phet.fractionsintro.intro.model.pieset.factories.SliceFactory;
 import edu.colorado.phet.fractionsintro.intro.view.Representation;
+import edu.colorado.phet.fractionsintro.intro.view.pieset.ShapeNode;
+import edu.umd.cs.piccolo.PNode;
 
 /**
  * Shows the icon on the representation control panel for the horizontal bars.
  *
  * @author Sam Reid
  */
-public class VerticalBarIcon extends ShapeIcon {
-    public VerticalBarIcon( SettableProperty<Representation> chosenRepresentation, Color color ) {
-        super(
-                new ArrayList<Shape>(),
-                new ArrayList<Shape>() {{
-                    add( new Rectangle2D.Double( 0, 0, DIM, DIM * 4 ) );
-                }}, chosenRepresentation, Representation.VERTICAL_BAR, color
-        );
+public class VerticalBarIcon implements RepresentationIcon {
+    private final PNode node;
+
+    public VerticalBarIcon( final SliceFactory sliceFactory ) {
+        node = new ShapeNode( sliceFactory.createBucketSlice( 1 ) ) {{ scale( 44 / getFullBounds().getWidth() ); }};
+    }
+
+    @Override public PNode getNode() {
+        return node;
+    }
+
+    @Override public Representation getRepresentation() {
+        return Representation.VERTICAL_BAR;
     }
 }
