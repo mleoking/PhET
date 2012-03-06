f4c88240ba25df0d3526d97de0212f5860b03747
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-03-06 05:57:26 +0000

    merged sample mains into module classes

--- simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/matchinggame/MatchingGameModule.java
+++ simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/matchinggame/MatchingGameModule.java
@@ -1,6 +1,10 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.fractionsintro.matchinggame;
 
+import edu.colorado.phet.common.phetcommon.application.ApplicationConstructor;
+import edu.colorado.phet.common.phetcommon.application.PhetApplication;
+import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
+import edu.colorado.phet.common.phetcommon.application.PhetApplicationLauncher;
 import edu.colorado.phet.fractionsintro.common.AbstractFractionsModule;
 import edu.colorado.phet.fractionsintro.matchinggame.model.MatchingGameModel;
 import edu.colorado.phet.fractionsintro.matchinggame.view.MatchingGameCanvas;
@@ -16,5 +20,15 @@ public class MatchingGameModule extends AbstractFractionsModule {
     public MatchingGameModule( MatchingGameModel model ) {
         super( "Matching Game", model.clock );
         setSimulationPanel( new MatchingGameCanvas( model ) );
+    }
+
+    //Test main for launching this module in an application by itself for testing
+    public static void main( String[] args ) {
+        final ApplicationConstructor constructor = new ApplicationConstructor() {
+            @Override public PhetApplication getApplication( PhetApplicationConfig c ) {
+                return new PhetApplication( c ) {{addModule( new MatchingGameModule() );}};
+            }
+        };
+        new PhetApplicationLauncher().launchSim( args, "fractions", "fractions-intro", constructor );
     }
 }
