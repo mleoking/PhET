e62a65e2049f25ec70f2575bc05b38db3876f156
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-03-06 05:57:26 +0000

    merged sample mains into module classes

--- trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/intro/FractionsIntroModule.java
+++ trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/intro/FractionsIntroModule.java
@@ -1,6 +1,10 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.fractionsintro.intro;
 
+import edu.colorado.phet.common.phetcommon.application.ApplicationConstructor;
+import edu.colorado.phet.common.phetcommon.application.PhetApplication;
+import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
+import edu.colorado.phet.common.phetcommon.application.PhetApplicationLauncher;
 import edu.colorado.phet.fractionsintro.common.AbstractFractionsModule;
 import edu.colorado.phet.fractionsintro.intro.model.FractionsIntroModel;
 import edu.colorado.phet.fractionsintro.intro.model.IntroState;
@@ -20,5 +24,15 @@ public class FractionsIntroModule extends AbstractFractionsModule {
     private FractionsIntroModule( FractionsIntroModel model ) {
         super( "Intro", model.getClock() );
         setSimulationPanel( new FractionsIntroCanvas( model ) );
+    }
+
+    //Test main for launching this module in an application by itself for testing
+    public static void main( String[] args ) {
+        final ApplicationConstructor constructor = new ApplicationConstructor() {
+            @Override public PhetApplication getApplication( PhetApplicationConfig c ) {
+                return new PhetApplication( c ) {{addModule( new FractionsIntroModule() );}};
+            }
+        };
+        new PhetApplicationLauncher().launchSim( args, "fractions", "fractions-intro", constructor );
     }
 }
