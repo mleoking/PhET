c83e9b28c44a19f109ca7b4c1cbab6e2c4ec0d44
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-03-07 18:54:15 +0000

    Matching Game & Intro: Fixed: Put the clipping region just below the bucket

--- trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/intro/view/pieset/MovableSliceLayer.java
+++ trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/intro/view/pieset/MovableSliceLayer.java
@@ -5,12 +5,15 @@ import fj.Effect;
 import fj.F;
 import fj.data.List;
 
+import java.awt.geom.Area;
 import java.awt.geom.Rectangle2D;
 
 import edu.colorado.phet.common.phetcommon.model.property.SettableProperty;
+import edu.colorado.phet.common.piccolophet.nodes.BucketView;
 import edu.colorado.phet.fractionsintro.intro.model.pieset.PieSet;
 import edu.colorado.phet.fractionsintro.intro.model.pieset.Slice;
 import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.util.PBounds;
 import edu.umd.cs.piccolox.nodes.PClip;
 
 /**
@@ -19,16 +22,24 @@ import edu.umd.cs.piccolox.nodes.PClip;
  * @author Sam Reid
  */
 public class MovableSliceLayer extends PNode {
-    public MovableSliceLayer( final PieSet state, final F<SliceNodeArgs, PNode> createSliceNode, final SettableProperty<PieSet> model, final PNode rootNode, final double clipY ) {
+    public MovableSliceLayer( final PieSet state, final F<SliceNodeArgs, PNode> createSliceNode, final SettableProperty<PieSet> model, final PNode rootNode, final BucketView bucketView ) {
 
         //Show graphics for the movable cells.  Put in a clip so that long bars will look like they sink through a "bottomless" bucket since they are too big at full size
         PClip movablePiecesLayer = new PClip() {{
             setStroke( null );
 
             //Create a clip that is far enough away that it will work for a variety of screen resolutions
-            double far = 10000;
+            final double far = 10000;
 
-            setPathTo( new Rectangle2D.Double( -far, -far, far * 2, far + clipY ) );
+            //For primary representations, cut out the part below the bucket.  This is null for the scaled representation on Equality Lab tab because there are no buckets there
+            if ( bucketView != null ) {
+                setPathTo( new Area() {{
+                    add( new Area( new Rectangle2D.Double( -far, -far, far * 3, far * 3 ) ) );
+                    final PBounds fullBounds = bucketView.getFrontNode().getFullBounds();
+                    Rectangle2D rect = new Rectangle2D.Double( fullBounds.getX() + 34, fullBounds.getMaxY() - 15, fullBounds.getWidth() - 34 * 2, fullBounds.getMaxY() + 1000 );
+                    subtract( new Area( rect ) );
+                }} );
+            }
             List<MovableSliceNode> nodes = state.slices.map( new F<Slice, MovableSliceNode>() {
                 @Override public MovableSliceNode f( Slice slice ) {
                     return new MovableSliceNode( createSliceNode.f( new SliceNodeArgs( slice, model.get().denominator, state.isInContainer( slice ) ) ), rootNode, model, slice );
