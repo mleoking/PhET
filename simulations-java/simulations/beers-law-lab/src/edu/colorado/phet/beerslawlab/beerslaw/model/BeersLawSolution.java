45edcf75c0bbd3791f4aec9e69f8438f3d2069b7
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-03-08 21:27:01 +0000

    add JC's experimental data to lookup tables for mapping wavelength to molar absorptivity

--- simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/beerslaw/model/BeersLawSolution.java
+++ simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/beerslaw/model/BeersLawSolution.java
@@ -4,6 +4,14 @@ package edu.colorado.phet.beerslawlab.beerslaw.model;
 import java.awt.Color;
 import java.text.MessageFormat;
 
+import edu.colorado.phet.beerslawlab.beerslaw.model.MolarAbsorptivityData.CobaltChlorideData;
+import edu.colorado.phet.beerslawlab.beerslaw.model.MolarAbsorptivityData.CobaltIINitrateData;
+import edu.colorado.phet.beerslawlab.beerslaw.model.MolarAbsorptivityData.CopperSulfateData;
+import edu.colorado.phet.beerslawlab.beerslaw.model.MolarAbsorptivityData.DrinkMixData;
+import edu.colorado.phet.beerslawlab.beerslaw.model.MolarAbsorptivityData.NickelIIChlorideData;
+import edu.colorado.phet.beerslawlab.beerslaw.model.MolarAbsorptivityData.PotassiumChromateData;
+import edu.colorado.phet.beerslawlab.beerslaw.model.MolarAbsorptivityData.PotassiumDichromateData;
+import edu.colorado.phet.beerslawlab.beerslaw.model.MolarAbsorptivityData.PotassiumPermanganateData;
 import edu.colorado.phet.beerslawlab.common.BLLResources.Strings;
 import edu.colorado.phet.beerslawlab.common.BLLSymbols;
 import edu.colorado.phet.beerslawlab.common.model.Solvent;
@@ -38,21 +46,22 @@ public class BeersLawSolution implements Resettable {
     public final ColorRange colorRange; // colors for the range of values that we're interested in. colorRange.maxColor is not necessarily the saturated color
     public final Color saturatedColor;
     public final CompositeProperty<Color> fluidColor; // derived
-    public final double lambdaMax; // wavelength for maximum absorbance, nm
-    public final double molarAbsorptionMax; // corresponds to lambdaMax, 1/(cm*M)
+    public final MolarAbsorptivityData molarAbsorptivityData; // experiement data that maps wavelength to molar absorptivity
+    public final double lambdaMax; // wavelength for maximum absorbance, nm  //TODO delete
+    public final double molarAbsorptionMax; // corresponds to lambdaMax, 1/(cm*M) //TODO delete
 
     // Constructor for solutions whose colorRange.maxColor is the same as the saturated color.
     public BeersLawSolution( String name, String formula,
                              DoubleRange concentrationRange, int concentrationViewExponent,
                              ColorRange colorRange,
-                             double lambdaMax, double molarAbsorptionMax ) {
-        this( name, formula, concentrationRange, concentrationViewExponent, colorRange, colorRange.getMax(), lambdaMax, molarAbsorptionMax );
+                             MolarAbsorptivityData molarAbsorptivityData ) {
+        this( name, formula, concentrationRange, concentrationViewExponent, colorRange, colorRange.getMax(), molarAbsorptivityData );
     }
 
     public BeersLawSolution( String name, String formula,
                              DoubleRange concentrationRange, int concentrationViewExponent,
                              ColorRange colorRange, Color saturatedColor,
-                             double lambdaMax, double molarAbsorptionMax ) {
+                             MolarAbsorptivityData molarAbsorptivityData ) {
 
         this.solvent = new Water();
         this.name = name;
@@ -62,8 +71,9 @@ public class BeersLawSolution implements Resettable {
         this.concentrationTransform = new ConcentrationTransform( concentrationViewExponent );
         this.colorRange = colorRange;
         this.saturatedColor = saturatedColor;
-        this.lambdaMax = lambdaMax;
-        this.molarAbsorptionMax = molarAbsorptionMax;
+        this.molarAbsorptivityData = molarAbsorptivityData;
+        this.lambdaMax = molarAbsorptivityData.getLambdaMax();
+        this.molarAbsorptionMax = molarAbsorptivityData.wavelengthToMolarAbsorptivity( lambdaMax );
 
         // derive the solution color
         this.fluidColor = new CompositeProperty<Color>( new Function0<Color>() {
@@ -101,7 +111,7 @@ public class BeersLawSolution implements Resettable {
             super( Strings.DRINK_MIX, BLLSymbols.DRINK_MIX,
                    new DoubleRange( 0, 0.400, 0.100 ), -3,
                    new ColorRange( new Color( 255, 225, 225 ), Color.RED ),
-                   511, 5.08 );
+                   new DrinkMixData() );
         }
 
         // This solution's solute doesn't have a formula, so use just its name.
@@ -115,7 +125,7 @@ public class BeersLawSolution implements Resettable {
             super( Strings.COBALT_II_NITRATE, BLLSymbols.COBALT_II_NITRATE,
                    new DoubleRange( 0, 0.400, 0.100 ), -3,
                    new ColorRange( new Color( 255, 225, 225 ), Color.RED ),
-                   550, 4.72 );
+                   new CobaltIINitrateData() );
         }
     }
 
@@ -124,7 +134,7 @@ public class BeersLawSolution implements Resettable {
             super( Strings.COBALT_CHLORIDE, BLLSymbols.COBALT_CHLORIDE,
                    new DoubleRange( 0, 0.250, 0.100 ), -3,
                    new ColorRange( new Color( 255, 242, 242 ), new Color( 255, 106, 106 ) ),
-                   549, 7.23 );
+                   new CobaltChlorideData() );
         }
     }
 
@@ -133,31 +143,34 @@ public class BeersLawSolution implements Resettable {
             super( Strings.POTASSIUM_DICHROMATE, BLLSymbols.POTASSIUM_DICHROMATE,
                    new DoubleRange( 0, 0.000500, 0.000100 ), -6,
                    new ColorRange( new Color( 255, 232, 210 ), new Color( 255, 127, 0 ) ),
-                   394, 3696.53 );
+                   new PotassiumDichromateData() );
         }
     }
 
     public static class PotassiumChromateSolution extends BeersLawSolution {
         public PotassiumChromateSolution() {
             super( Strings.POTASSIUM_CHROMATE, BLLSymbols.POTASSIUM_CHROMATE,
-                   new DoubleRange( 0, 0.000400, 0.000100 ), -6, new ColorRange( new Color( 255, 255, 199 ), Color.YELLOW ),
-                   413, 4770.07 );
+                   new DoubleRange( 0, 0.000400, 0.000100 ), -6,
+                   new ColorRange( new Color( 255, 255, 199 ), Color.YELLOW ),
+                   new PotassiumChromateData() );
         }
     }
 
     public static class NickelIIChlorideSolution extends BeersLawSolution {
         public NickelIIChlorideSolution() {
             super( Strings.NICKEL_II_CHLORIDE, BLLSymbols.NICKEL_II_CHLORIDE,
-                   new DoubleRange( 0, 0.350, 0.100 ), -3, new ColorRange( new Color( 234, 244, 234 ), new Color( 0, 128, 0 ) ),
-                   435, 5.31 );
+                   new DoubleRange( 0, 0.350, 0.100 ), -3,
+                   new ColorRange( new Color( 234, 244, 234 ), new Color( 0, 128, 0 ) ),
+                   new NickelIIChlorideData() );
         }
     }
 
     public static class CopperSulfateSolution extends BeersLawSolution {
         public CopperSulfateSolution() {
             super( Strings.COPPER_SULFATE, BLLSymbols.COPPER_SULFATE,
-                   new DoubleRange( 0, 0.200, 0.100 ), -3, new ColorRange( new Color( 222, 238, 255 ), new Color( 30, 144, 255 ) ),
-                   780, 9.67 );
+                   new DoubleRange( 0, 0.200, 0.100 ), -3,
+                   new ColorRange( new Color( 222, 238, 255 ), new Color( 30, 144, 255 ) ),
+                   new CopperSulfateData() );
         }
     }
 
@@ -167,7 +180,7 @@ public class BeersLawSolution implements Resettable {
             super( Strings.POTASSIUM_PERMANGANATE, BLLSymbols.POTASSIUM_PERMANGANATE,
                    new DoubleRange( 0, 0.000800, 0.000100 ), -6,
                    new ColorRange( new Color( 255, 235, 255 ), new Color( 255, 0, 255 ) ), new Color( 130, 0, 139 ),
-                   566, 2389.61 );
+                   new PotassiumPermanganateData() );
         }
     }
 }
