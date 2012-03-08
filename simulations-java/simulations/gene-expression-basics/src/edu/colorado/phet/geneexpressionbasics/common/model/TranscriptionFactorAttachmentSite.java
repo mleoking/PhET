e02f11c79e38f62ab099f4c731461b220aaf35ea
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-03-08 21:10:22 +0000

    Moved class that was in incorrect package.

--- simulations-java/simulations/gene-expression-basics/src/edu/colorado/phet/geneexpressionbasics/common/model/TranscriptionFactorAttachmentSite.java
+++ simulations-java/simulations/gene-expression-basics/src/edu/colorado/phet/geneexpressionbasics/common/model/TranscriptionFactorAttachmentSite.java
@@ -1,5 +1,5 @@
 // Copyright 2002-2012, University of Colorado
-package edu.colorado.phet.geneexpressionbasics.common.model.motionstrategies;
+package edu.colorado.phet.geneexpressionbasics.common.model;
 
 import java.awt.geom.Point2D;
 
