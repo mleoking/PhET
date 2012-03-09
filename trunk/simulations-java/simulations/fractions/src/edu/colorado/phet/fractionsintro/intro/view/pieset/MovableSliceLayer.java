256a2872f702412959cb71eacce0de56bcdc4c5e
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-03-09 23:55:42 +0000

    Equality Lab: Fixed: Pies don't fill on Equality lab

--- trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/intro/view/pieset/MovableSliceLayer.java
+++ trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/intro/view/pieset/MovableSliceLayer.java
@@ -25,21 +25,20 @@ public class MovableSliceLayer extends PNode {
     public MovableSliceLayer( final PieSet state, final F<SliceNodeArgs, PNode> createSliceNode, final SettableProperty<PieSet> model, final PNode rootNode, final BucketView bucketView ) {
 
         //Show graphics for the movable cells.  Put in a clip so that long bars will look like they sink through a "bottomless" bucket since they are too big at full size
-        PClip movablePiecesLayer = new PClip() {{
+        PNode movablePiecesLayer = new PClip() {{
             setStroke( null );
 
             //Create a clip that is far enough away that it will work for a variety of screen resolutions
             final double far = 10000;
 
             //For primary representations, cut out the part below the bucket.  This is null for the scaled representation on Equality Lab tab because there are no buckets there
-            if ( bucketView != null ) {
-                setPathTo( new Area() {{
-                    add( new Area( new Rectangle2D.Double( -far, -far, far * 3, far * 3 ) ) );
-                    final PBounds fullBounds = bucketView.getFrontNode().getFullBounds();
-                    Rectangle2D rect = new Rectangle2D.Double( fullBounds.getX() + 34, fullBounds.getMaxY() - 15, fullBounds.getWidth() - 34 * 2, fullBounds.getMaxY() + 1000 );
-                    subtract( new Area( rect ) );
-                }} );
-            }
+            setPathTo( bucketView == null ? new Rectangle2D.Double( -far, -far, far * 3, far * 3 ) :
+                       new Area() {{
+                           add( new Area( new Rectangle2D.Double( -far, -far, far * 3, far * 3 ) ) );
+                           final PBounds fullBounds = bucketView.getFrontNode().getFullBounds();
+                           Rectangle2D rect = new Rectangle2D.Double( fullBounds.getX() + 34, fullBounds.getMaxY() - 15, fullBounds.getWidth() - 34 * 2, fullBounds.getMaxY() + 1000 );
+                           subtract( new Area( rect ) );
+                       }} );
             List<MovableSliceNode> nodes = state.slices.map( new F<Slice, MovableSliceNode>() {
                 @Override public MovableSliceNode f( Slice slice ) {
                     return new MovableSliceNode( createSliceNode.f( new SliceNodeArgs( slice, model.get().denominator, state.isInContainer( slice ) ) ), rootNode, model, slice );
