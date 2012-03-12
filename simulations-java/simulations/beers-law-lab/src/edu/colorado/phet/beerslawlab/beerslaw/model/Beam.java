0b3f1579f3824134bfbc105d5a98cd6ef522c872
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-03-12 22:35:45 +0000

    Beers: simplify Beam model and view to a single rectangle, instead of 3 segments

--- simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/beerslaw/model/Beam.java
+++ simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/beerslaw/model/Beam.java
@@ -28,11 +28,10 @@ import edu.colorado.phet.common.phetcommon.view.util.VisibleColor;
  */
 public class Beam {
 
-    private static final double RIGHT_SEGMENT_WIDTH = 30; // cm, wide enough to be way off the right edge of the play area
-    private static final int MAX_LIGHT_ALPHA = 200; // transparency of light when transmittance is 1.0
-    private static final int MIN_LIGHT_ALPHA = 20; // min transparency of light when transmittance is non-zero
+    private static final double MAX_LIGHT_WIDTH = 50; // cm, wide enough to be way off the right edge of the play area
+    private static final int MAX_LIGHT_ALPHA = 200; // transparency of light when transmittance is 1.0 (tuned by eye)
+    private static final int MIN_LIGHT_ALPHA = 20; // min transparency of light when transmittance is non-zero (tuned by eye)
     private static final Function TRANSMITTANCE_TO_ALPHA = new LinearFunction( 0, 1, MIN_LIGHT_ALPHA, MAX_LIGHT_ALPHA ); // maps transmittance to transparency
-    private static final Color INVISIBLE_COLOR = new Color( 0, 0, 0, 0 );
 
     private final Light light;
     private final Cuvette cuvette;
@@ -40,8 +39,8 @@ public class Beam {
     private final Absorbance absorbance;
     private final ModelViewTransform mvt;
 
-    public final Property<ImmutableRectangle2D> leftShape, centerShape, rightShape;
-    public final Property<Paint> leftPaint, centerPaint, rightPaint;
+    public final Property<ImmutableRectangle2D> shape;
+    public final Property<Paint> paint;
     public final CompositeProperty<Boolean> visible;
 
     public Beam( final Light light, Cuvette cuvette, ATDetector detector, Absorbance absorbance, ModelViewTransform mvt ) {
@@ -53,14 +52,8 @@ public class Beam {
         this.mvt = mvt;
 
         // Proper values will be set when observers are registered
-        this.leftShape = new Property<ImmutableRectangle2D>( new ImmutableRectangle2D( 0, 0 ) );
-        this.centerShape = new Property<ImmutableRectangle2D>( new ImmutableRectangle2D( 0, 0 ) );
-        this.rightShape = new Property<ImmutableRectangle2D>( new ImmutableRectangle2D( 0, 0 ) );
-
-        // Proper values will be set when observers are registered
-        this.leftPaint = new Property<Paint>( Color.WHITE );
-        this.centerPaint = new Property<Paint>( Color.WHITE );
-        this.rightPaint = new Property<Paint>( Color.WHITE );
+        this.shape = new Property<ImmutableRectangle2D>( new ImmutableRectangle2D( 0, 0 ) );
+        this.paint = new Property<Paint>( Color.WHITE );
 
         // Make the beam visible when the light is on.
         visible = new CompositeProperty<Boolean>( new Function0<Boolean>() {
@@ -73,7 +66,7 @@ public class Beam {
         RichSimpleObserver shapeObserver = new RichSimpleObserver() {
             @Override public void update() {
                 if ( visible.get() ) {
-                    updateSegments();
+                    updateShape();
                 }
             }
         };
@@ -83,7 +76,7 @@ public class Beam {
         final RichSimpleObserver colorObserver = new RichSimpleObserver() {
             public void update() {
                 if ( visible.get() ) {
-                    updateColors();
+                    updateColor();
                 }
             }
         };
@@ -93,115 +86,30 @@ public class Beam {
         visible.addObserver( new VoidFunction1<Boolean>() {
             public void apply( Boolean visible ) {
                 if ( visible ) {
-                    updateColors();
-                    updateSegments();
+                    updateColor();
+                    updateShape();
                 }
             }
         } );
     }
 
     // Updates colors of the beam segments
-    private void updateColors() {
-
+    private void updateColor() {
         final double wavelength = light.wavelength.get();
-
-        // left uses color of light
-        leftPaint.set( ColorUtils.createColor( new VisibleColor( wavelength ), MAX_LIGHT_ALPHA ) );
-
-        // center is a gradient
         final Double transmittance = absorbance.getTransmittance();
-        if ( transmittance == 0 ) {
-            centerPaint.set( INVISIBLE_COLOR );
-        }
-        else {
-            // This gradient is in view coordinates.
-            Color leftColor = ColorUtils.createColor( new VisibleColor( wavelength ), MAX_LIGHT_ALPHA );
-            Color rightColor = ColorUtils.createColor( new VisibleColor( wavelength ), (int) TRANSMITTANCE_TO_ALPHA.evaluate( transmittance ) );
-            double x = mvt.modelToViewDeltaX( cuvette.location.getX() );
-            double w = mvt.modelToViewDeltaX( cuvette.width.get() );
-            centerPaint.set( new GradientPaint( (float) x, 0, leftColor, (float) ( x + w ), 0, rightColor ) );
-        }
-
-        // right is whatever color was the right edge of the center gradient
-        if ( transmittance == 0 ) {
-            rightPaint.set( INVISIBLE_COLOR );
-        }
-        else {
-            Color color = new VisibleColor( light.wavelength.get() );
-            rightPaint.set( ColorUtils.createColor( color, (int) TRANSMITTANCE_TO_ALPHA.evaluate( transmittance ) ) );
-        }
+        Color leftColor = ColorUtils.createColor( new VisibleColor( wavelength ), MAX_LIGHT_ALPHA );
+        Color rightColor = ColorUtils.createColor( new VisibleColor( wavelength ), (int) TRANSMITTANCE_TO_ALPHA.evaluate( transmittance ) );
+        double x = mvt.modelToViewDeltaX( cuvette.location.getX() );
+        double w = mvt.modelToViewDeltaX( cuvette.width.get() );
+        paint.set( new GradientPaint( (float) x, 0, leftColor, (float) ( x + w ), 0, rightColor ) );
     }
 
-    // Updates the geometry of the beam segments
-    private void updateSegments() {
-
-        // light
-        final double lightX = light.location.getX();
-        final double lightMinY = light.getMinY();
-        final double lightHeight = light.lensDiameter;
-
-        // cuvette
-        final double cuvetteMinX = cuvette.location.getX();
-        final double cuvetteMaxX = cuvetteMinX + cuvette.width.get();
-        final double cuvetteWidth = cuvette.width.get();
-
-        // probe
-        final double probeX = detector.probe.location.get().getX();
-
-        // left segment
-        if ( probeInLeftSegment() ) {
-            leftShape.set( new ImmutableRectangle2D( lightX, lightMinY, probeX - lightX, lightHeight ) );
-        }
-        else {
-            leftShape.set( new ImmutableRectangle2D( lightX, lightMinY, cuvetteMinX - lightX, lightHeight ) );
-        }
-
-        // center segment
-        if ( probeInLeftSegment() ) {
-            // this segment doesn't exist
-            centerShape.set( new ImmutableRectangle2D( 0, 0 ) );
-        }
-        else if ( probeInCenterSegment() ) {
-            // probe is interacting with this segment
-            centerShape.set( new ImmutableRectangle2D( cuvetteMinX, lightMinY, probeX - cuvetteMinX, lightHeight ) );
-        }
-        else {
-            // probe is not interacting with this segment
-            centerShape.set( new ImmutableRectangle2D( cuvetteMinX, lightMinY, cuvetteWidth, lightHeight ) );
-        }
-
-        // right segment
-        if ( probeInLeftSegment() || probeInCenterSegment() ) {
-            // this segment doesn't exist
-            rightShape.set( new ImmutableRectangle2D( 0, 0 ) );
-        }
-        else if ( probeInRightSegment() ) {
-            // probe is interacting with this segment
-            rightShape.set( new ImmutableRectangle2D( cuvetteMaxX, lightMinY, probeX - cuvetteMaxX, lightHeight ) );
-        }
-        else {
-            // probe is not interacting with this segment
-            rightShape.set( new ImmutableRectangle2D( cuvetteMaxX, lightMinY, RIGHT_SEGMENT_WIDTH, lightHeight ) );
-        }
-    }
-
-    // Is the probe in the left segment?
-    private boolean probeInLeftSegment() {
-        return detector.probeInBeam() &&
-               ( detector.probe.location.get().getX() > light.location.getX() ) &&
-               ( detector.probe.location.get().getX() < cuvette.location.getX() );
-    }
-
-    // Is the probe in the center segment?
-    private boolean probeInCenterSegment() {
-        return detector.probeInBeam() &&
-               ( detector.probe.location.get().getX() >= cuvette.location.getX() ) &&
-               ( detector.probe.location.get().getX() <= cuvette.location.getX() + cuvette.width.get() );
-
-    }
-
-    // Is the probe in the right segment?
-    private boolean probeInRightSegment() {
-        return detector.probeInBeam() && ( detector.probe.location.get().getX() > cuvette.location.getX() + cuvette.width.get() );
+    // Updates the shape of the beam
+    private void updateShape() {
+        final double x = light.location.getX();
+        final double y = light.getMinY();
+        final double width = detector.probeInBeam() ? detector.probe.getX() - x : MAX_LIGHT_WIDTH;
+        final double height = light.lensDiameter;
+        shape.set( new ImmutableRectangle2D( x, y, width, height ) );
     }
 }
