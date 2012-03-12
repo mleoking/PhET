9c9635a88a50de4a2484e8261caeca16326a35f9
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-03-12 22:35:45 +0000

    Beers: simplify Beam model and view to a single rectangle, instead of 3 segments

--- trunk/simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/beerslaw/view/BeamNode.java
+++ trunk/simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/beerslaw/view/BeamNode.java
@@ -4,38 +4,45 @@ package edu.colorado.phet.beerslawlab.beerslaw.view;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Paint;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
 
 import edu.colorado.phet.beerslawlab.beerslaw.model.Beam;
 import edu.colorado.phet.common.phetcommon.math.ImmutableRectangle2D;
-import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.phetcommon.util.RichSimpleObserver;
 import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
 import edu.colorado.phet.common.phetcommon.view.util.ColorUtils;
-import edu.colorado.phet.common.piccolophet.PhetPNode;
-import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.nodes.PPath;
-import edu.umd.cs.piccolox.nodes.PComposite;
 
 /**
  * Representation of light as a beam.
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
-class BeamNode extends PhetPNode {
+class BeamNode extends PPath {
 
-    public BeamNode( final Beam beam, ModelViewTransform mvt ) {
+    public BeamNode( final Beam beam, final ModelViewTransform mvt ) {
 
+        // not interactive, so that probe can be grabbed behind the beam
         setPickable( false );
         setChildrenPickable( false );
 
-        final PNode parentNode = new PComposite();
-        addChild( parentNode );
-        parentNode.addChild( new SegmentNode( beam.leftShape, beam.leftPaint, mvt ) );
-        parentNode.addChild( new SegmentNode( beam.centerShape, beam.centerPaint, mvt ) );
-        parentNode.addChild( new SegmentNode( beam.rightShape, beam.rightPaint, mvt ) );
+        // put a gray outline around the beam, so that lighter colors are still visible
+        setStroke( new BasicStroke( 0.25f ) );
+        setStrokePaint( ColorUtils.createColor( Color.LIGHT_GRAY, 210 ) );
+
+        // Shape
+        beam.shape.addObserver( new VoidFunction1<ImmutableRectangle2D>() {
+            public void apply( ImmutableRectangle2D r ) {
+                setPathTo( mvt.modelToView( r ).toRectangle2D() );
+            }
+        } );
+
+        // Paint
+        beam.paint.addObserver( new VoidFunction1<Paint>() {
+            public void apply( Paint paint ) {
+                setPaint( paint );
+            }
+        } );
 
         // Make this node visible when beam is visible.
         RichSimpleObserver visibilityObserver = new RichSimpleObserver() {
@@ -44,39 +51,5 @@ class BeamNode extends PhetPNode {
             }
         };
         visibilityObserver.observe( beam.visible );
-
-        // put a gray outline around the beam, so that lighter colors are still visible
-        PPath outlineNode = new PPath() {{
-            setStroke( new BasicStroke( 0.25f ) );
-            setStrokePaint( ColorUtils.createColor( Color.LIGHT_GRAY, 210 ) );
-            parentNode.addPropertyChangeListener( PNode.PROPERTY_FULL_BOUNDS, new PropertyChangeListener() {
-                public void propertyChange( PropertyChangeEvent evt ) {
-                    setPathTo( parentNode.getFullBoundsReference() );
-                }
-            } );
-        }};
-        addChild( outlineNode );
-    }
-
-    // A segment of the beam
-    private static class SegmentNode extends PPath {
-
-        public SegmentNode( Property<ImmutableRectangle2D> shape, Property<Paint> paint, final ModelViewTransform mvt ) {
-            setStroke( null );
-
-            // Shape
-            shape.addObserver( new VoidFunction1<ImmutableRectangle2D>() {
-                public void apply( ImmutableRectangle2D r ) {
-                    setPathTo( mvt.modelToView( r ).toRectangle2D() );
-                }
-            } );
-
-            // Paint
-            paint.addObserver( new VoidFunction1<Paint>() {
-                public void apply( Paint paint ) {
-                    setPaint( paint );
-                }
-            } );
-        }
     }
 }
