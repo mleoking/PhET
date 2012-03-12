0530915d8cfbacc730494ceed538d26878ee6167
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-03-12 21:06:27 +0000

    break long lines

--- simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/simsharing/messages/ParameterKeys.java
+++ simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/simsharing/messages/ParameterKeys.java
@@ -5,14 +5,16 @@ package edu.colorado.phet.common.phetcommon.simsharing.messages;
  * @author Sam Reid
  */
 public enum ParameterKeys implements IParameterKey {
-    canvasPositionX, canvasPositionY, componentType, description, height, interactive, item, isSelected, key, part, text, title, value, width, window, x, y,
+    canvasPositionX, canvasPositionY, componentType, description, height, interactive, item,
+    isSelected, key, part, text, title, value, width, window, x, y,
     enabled,
     minX, maxX, averageX, minY, maxY, averageY,
     isPlaying,
     selectedKit,
 
     //For system:
-    time, name, version, project, flavor, locale, distributionTag, javaVersion, osName, osVersion, parserVersion, study, id, machineCookie, messageCount, messageIndex, sessionId, commandLineArgs,
+    time, name, version, project, flavor, locale, distributionTag, javaVersion, osName, osVersion,
+    parserVersion, study, id, machineCookie, messageCount, messageIndex, sessionId, commandLineArgs,
 
     errorMessage,
     //For drag events
