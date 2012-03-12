3c9fd40759e2cdb9e73a3793cf93af3611150f7a
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-03-12 16:39:09 +0000

    Copy MS analysis as a template for RPAL analysis

---
+++ trunk/util/sim-event-data-collection-analysis/src/edu/colorado/phet/simsharinganalysis/scripts/rpal2012/RPALAnalysis.scala
@@ -0,0 +1,95 @@
+// Copyright 2002-2012, University of Colorado
+package edu.colorado.phet.simsharinganalysis.scripts.rpal2012
+
+import edu.colorado.phet.simsharinganalysis.scripts.StateMachine
+import edu.colorado.phet.simsharinganalysis.scripts.moleculeshapesfebruary2012.SimState
+import edu.colorado.phet.simsharinganalysis.{phet, Entry, Log}
+import java.io.File
+import java.text.DecimalFormat
+
+/**
+ * @author Sam Reid
+ */
+
+object RPALAnalysis extends StateMachine[SimState] {
+  def toReport(log: Log) = new Report(log)
+
+  //create a list of pairs (1,2) (2,3) ...
+  def pairs[T](x: List[T]) = {
+    val a = x.tail
+    val b = a.reverse.tail.reverse
+    ( b zip a ).toList
+  }
+
+  /**
+   * Consolidate the important information that was needed from a single log
+   * of entries.
+   */
+  class Report(val log: Log) {
+
+    //Uses a state machine to compute the states that the simulation went through.
+    val states = getStates(log)
+
+    //For timing, only keep things between first and last user event because sometimes the sim is left on with no activity for an hour or more after student is gone
+    val userStates = states.slice(states.indexWhere(_.entry.messageType == "user"), states.lastIndexWhere(_.entry.messageType == "user") + 1)
+
+    //Count the number of transitions that the user made.
+    val kitTransitions = log.entries.filter(entry => List("kitSelectionNode.previousButton", "kitSelectionNode.nextButton").contains(entry.component)).length
+    val tabTransitions = states.filter(entry => entry.start.tab != entry.end.tab).length
+    val moleculeTransitions = states.filter(entry => entry.start.tab1.molecule != entry.end.tab1.molecule).length
+    val viewTransitions = states.filter(entry => entry.start.tab1.view != entry.end.tab1.view).length
+
+    def minutesInTab(tab: Int): Double = userStates.filter(_.start.tab == tab).map(_.time).sum / 1000.0 / 60.0
+
+    def timeInTab1State(state: String): Double = userStates.filter(_.start.tab == 1).filter(_.start.tab1.view == state).map(_.time).sum / 1000.0 / 60.0
+
+    override def toString = "minutes in tab 0: " + minutesInTab(0) + "\n" +
+                            "minutes in tab 1: " + minutesInTab(1) + "\n" +
+                            "minutes on real in tab 1: " + timeInTab1State("real") + "\n" +
+                            "minutes on model in tab 1: " + timeInTab1State("model") + "\n" +
+                            "tab transitions: " + tabTransitions + "\n" +
+                            "kit transitions: " + kitTransitions + "\n" +
+                            "molecule transitions: " + moleculeTransitions + "\n" +
+                            "view transitions: " + viewTransitions
+  }
+
+  //Given the current state and an entry, compute the next state
+  def initialState(log: Log) = SimState(log.startTime)
+
+  def nextState(state: SimState, e: Entry) = {
+
+    //When the sim gets reset, go back to the first state
+    if ( e.messageType == "system" && e.component == "application" && e.action == "exited" ) SimState(e.time)
+    else if ( e.enabled == false ) state.copy(time = e.time)
+    else if ( e.componentType == "tab" ) state.copy(tab = if ( e.component == "moleculeShapesTab" ) 0 else 1, time = e.time)
+    else if ( state.tab == 0 ) state.copy(tab0 = state.tab0.next(e), time = e.time)
+    else if ( state.tab == 1 ) state.copy(tab1 = state.tab1.next(e), time = e.time)
+    else state.copy(tab1 = state.tab1.next(e), time = e.time)
+  }
+
+  def main(args: Array[String]) {
+
+    val logs = phet load new File("C:\\Users\\Sam\\Desktop\\ms-data-revised")
+    val reports = logs.map(toReport(_))
+    println("filename\t" + "" +
+            "time tab 1\t" +
+            "time tab 2\t" +
+            "time real\t" +
+            "time model\t" +
+            "tab transitions\t" +
+            "kit transitions\t" +
+            "molecule transitions\t" +
+            "view transitions")
+
+    def format(x: Double) = new DecimalFormat("0.00").format(x)
+    reports.foreach(r => println(r.log.file.getName + "\t" +
+                                 format(r.minutesInTab(0)) + "\t" +
+                                 format(r.minutesInTab(1)) + "\t" +
+                                 format(r.timeInTab1State("real")) + "\t" +
+                                 format(r.timeInTab1State("model")) + "\t" +
+                                 r.tabTransitions + "\t" +
+                                 r.kitTransitions + "\t" +
+                                 r.moleculeTransitions + "\t" +
+                                 r.viewTransitions))
+  }
+}
---
+++ trunk/util/sim-event-data-collection-analysis/src/edu/colorado/phet/simsharinganalysis/scripts/rpal2012/SimState.scala
@@ -0,0 +1,94 @@
+package edu.colorado.phet.simsharinganalysis.scripts.rpal2012
+
+// Copyright 2002-2012, University of Colorado
+
+import edu.colorado.phet.simsharinganalysis.Entry
+import Globals._
+
+object SimState {
+  def apply(time: Long): SimState = SimState(0, initialTab0, initialTab1, time)
+}
+
+case class SimState(tab: Int, tab0: Tab0, tab1: Tab1, time: Long) {
+  def getSelectedTab = if ( tab == 0 ) tab0 else tab1
+}
+
+case class Molecule(electronGeometry: String, moleculeGeometry: String)
+
+trait MSTab
+
+//Factored out state that is shared between all tabs, since case classes do not inherit well.
+case class ViewAndTestState(view: String, test: String, somethingEnabled: Boolean) {
+  assert(view != null)
+  assert(test != null)
+
+  def next(e: Entry) = {
+
+    e match {
+
+      //If clicking on something disabled, then do not change the state, see #3218
+      case x: Entry if x.enabled == false => this
+
+      //Watch which view the user selects
+      case Entry(_, "user", c, _, "pressed", _) if List("magnifyingGlassRadioButton", "magnifyingGlassIcon") contains c => copy(view = molecules)
+      case Entry(_, "user", c, _, "pressed", _) if List("concentrationGraphRadioButton", "concentrationGraphIcon") contains c => copy(view = barGraph)
+      case Entry(_, "user", c, _, "pressed", _) if List("liquidRadioButton", "liquidIcon") contains c => copy(view = liquid)
+
+      //See if the user changed tests
+      case Entry(_, "user", c, _, "pressed", _) if List("phMeterRadioButton", "phMeterIcon").contains(c) => copy(test = phMeter)
+      case Entry(_, "user", c, _, "pressed", _) if List("phPaperRadioButton", "phPaperIcon").contains(c) => copy(test = phPaper)
+      case Entry(_, "user", c, _, "pressed", _) if List("conductivityTesterRadioButton", "conductivityTesterIcon").contains(c) => copy(test = conductivityTester)
+
+      //Nothing happened to change the state
+      case _ => this
+    }
+  }
+}
+
+//ShowSolvent indicates that the check box is checked, but solvent only showing if view is also "molecules"
+case class Tab0(dummy: String) extends MSTab {
+
+  def next(e: Entry): Tab0 = {
+
+    val updated = e match {
+
+      //If clicking on something disabled, then do not change the state, see #3218
+      case x: Entry if x.enabled == false => this
+
+      //Handle reset all presses
+      case Entry(_, "user", "resetAllConfirmationDialogYesButton", _, "pressed", _) => initialTab0
+
+      //Nothing happened to change the state
+      case _ => this
+
+    }
+
+    updated
+  }
+}
+
+case class Tab1(molecule: String, view: String, kit: String) extends MSTab {
+
+  def next(e: Entry): Tab1 = {
+
+    val updated = e match {
+
+      //If clicking on something disabled, then do not change the state, see #3218
+      case x: Entry if x.enabled == false => this
+
+      //Watch which solution the user selects
+      case Entry(_, "user", "modelViewCheckBox", _, "pressed", _) => copy(view = "model")
+      case Entry(_, "user", "realViewCheckBox", _, "pressed", _) => copy(view = "real")
+
+      //Handle reset all presses
+      case Entry(_, "user", "resetAllConfirmationDialogYesButton", _, "pressed", _) => initialTab1
+
+      case Entry(_, "model", "molecule", "moleculeModel", "realMoleculeChanged", _) => copy(molecule = e("realMolecule"))
+
+      //Nothing happened to change the state
+      case _ => this
+    }
+    updated
+    //    updated.copy(viewAndTestState = viewAndTestState.next(e))
+  }
+}
