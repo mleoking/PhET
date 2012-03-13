55d33c36d0385a539e15a78481eb9006293d94c3
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-03-13 23:57:09 +0000

    minor readability updates

--- trunk/util/sim-event-data-collection-analysis/src/edu/colorado/phet/simsharinganalysis/scripts/rpal2012/Globals.scala
+++ trunk/util/sim-event-data-collection-analysis/src/edu/colorado/phet/simsharinganalysis/scripts/rpal2012/Globals.scala
@@ -2,11 +2,6 @@
 package edu.colorado.phet.simsharinganalysis.scripts.rpal2012
 
 object Globals {
-  val tabs = List("introductionTab", "customSolutionTab")
-
-  //TODO: Instead of hardcoding these strings, could make a new project that depends on ABS and use toString on those enums.
-  //Right now we are keeping this project integrated with the sim-event-data-collection-analysis-project for expedience, so haven't done that yet.
-
   val initialTab0 = Tab0("cheese")
   val initialTab1 = Tab1("ammonia", "real", "KITTY")
   val initialTab2 = Tab2(1, true, true, Hiding.nothing, -1, Nil)
