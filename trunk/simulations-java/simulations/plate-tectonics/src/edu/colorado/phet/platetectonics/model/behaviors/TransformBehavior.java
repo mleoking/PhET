02bc6cb039f63983a077a9b86314a6d4b959965a
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-03-13 01:13:11 +0000

    pt: Improved transform fault valley

--- trunk/simulations-java/simulations/plate-tectonics/src/edu/colorado/phet/platetectonics/model/behaviors/TransformBehavior.java
+++ trunk/simulations-java/simulations/plate-tectonics/src/edu/colorado/phet/platetectonics/model/behaviors/TransformBehavior.java
@@ -9,20 +9,38 @@ public class TransformBehavior extends PlateBehavior {
 
     private final boolean towardsFront;
 
+    private float timeElapsed = 0;
+    private static final float FAULT_VALLEY_TIME_FACTOR = 0.3f;
+    private static final float VALLEY_DIP_DISTANCE = 5000;
+
     public TransformBehavior( PlateMotionPlate plate, PlateMotionPlate otherPlate, boolean towardsFront ) {
         super( plate, otherPlate );
         this.towardsFront = towardsFront;
     }
 
     @Override public void stepInTime( float millionsOfYears ) {
+
+        float timeBefore = timeElapsed;
+        timeElapsed += millionsOfYears;
+        float timeAfter = timeElapsed;
+
         getPlate().shiftZ( 30000f / 2 * ( towardsFront ? millionsOfYears : -millionsOfYears ) );
 
+
+        float riftValleyAmount = (float) ( Math.exp( -timeBefore * FAULT_VALLEY_TIME_FACTOR ) - Math.exp( -timeAfter * FAULT_VALLEY_TIME_FACTOR ) );
+
         // add in the rift valley
-        final float delta = -millionsOfYears * 100;
-        getTerrain().shiftColumnElevation( getOppositeSide().getIndex( getTerrain().getNumColumns() ), delta );
-        final Sample edgeSample = getCrust().getTopBoundary().getEdgeSample( getOppositeSide() );
-        edgeSample.setPosition( edgeSample.getPosition().plus( ImmutableVector3F.Y_UNIT.times( delta ) ) );
+        final float delta = -riftValleyAmount * VALLEY_DIP_DISTANCE;
+        shiftIndexElevation( 0, delta );
+        shiftIndexElevation( 1, delta / 4 );
+        shiftIndexElevation( 2, -delta / 4 );
 
         getPlate().getTerrain().elevationChanged.updateListeners();
+    }
+
+    public void shiftIndexElevation( int index, float delta ) {
+        getTerrain().shiftColumnElevation( getOppositeSide().getFromIndex( getTerrain().getNumColumns(), index ), delta );
+        final Sample sample = getOppositeSide().getFromEnd( getCrust().getTopBoundary().samples, index );
+        sample.setPosition( sample.getPosition().plus( ImmutableVector3F.Y_UNIT.times( delta ) ) );
     }
 }
