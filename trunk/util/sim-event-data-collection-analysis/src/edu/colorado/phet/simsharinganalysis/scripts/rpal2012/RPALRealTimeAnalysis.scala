110caf722f47b6f43f520896bbf5f06fc8303dcd
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-03-14 02:37:11 +0000

    added timeline plot to show tab transitions

--- trunk/util/sim-event-data-collection-analysis/src/edu/colorado/phet/simsharinganalysis/scripts/rpal2012/RPALRealTimeAnalysis.scala
+++ trunk/util/sim-event-data-collection-analysis/src/edu/colorado/phet/simsharinganalysis/scripts/rpal2012/RPALRealTimeAnalysis.scala
@@ -2,15 +2,32 @@
 package edu.colorado.phet.simsharinganalysis.scripts.rpal2012
 
 import edu.colorado.phet.simsharinganalysis.RealTimeAnalysis
+import edu.colorado.phet.common.piccolophet.PhetPCanvas
+import swing.{Component, Frame}
+import java.awt.Dimension
 
 /**
  * @author Sam Reid
  */
 object RPALRealTimeAnalysis extends App {
+
+  val canvas = new PhetPCanvas()
+  val chartFrame = new Frame {
+    contents = new Component {
+
+      override lazy val peer = canvas
+    }
+    size = new Dimension(1024, 100)
+  }
+
+  chartFrame.visible = true
+
   new RealTimeAnalysis(log => {
     val states = RPALAnalysis getStates log
+    canvas.getLayer.removeAllChildren()
+    canvas.getLayer.addChild(new TimelineNode(states))
 
-    ( RPALAnalysis toReport log ).toString + "\n" +
+    RPALAnalysis.toReport(log).toString + "\n" +
     "Tab " + states.last.end.tab + "\n" +
     "Tab 1 View " + states.last.end.tab1.view + "\n" +
     "Tab 0 sandwich: " + states.last.end.tab0.sandwich + "\n" +
