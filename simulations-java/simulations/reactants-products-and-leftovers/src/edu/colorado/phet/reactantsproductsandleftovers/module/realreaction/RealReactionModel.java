5faf58da2d53de03462deb47aff986c5097f1f51
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-03-14 00:33:11 +0000

    #3285, add support for testing if sim-sharing is running with a specific configuration

--- simulations-java/simulations/reactants-products-and-leftovers/src/edu/colorado/phet/reactantsproductsandleftovers/module/realreaction/RealReactionModel.java
+++ simulations-java/simulations/reactants-products-and-leftovers/src/edu/colorado/phet/reactantsproductsandleftovers/module/realreaction/RealReactionModel.java
@@ -6,7 +6,7 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import javax.swing.event.EventListenerList;
 
-import edu.colorado.phet.common.phetcommon.simsharing.SimSharingConfig.RPALApril2012;
+import edu.colorado.phet.common.phetcommon.simsharing.SimSharingConfig;
 import edu.colorado.phet.common.phetcommon.simsharing.SimSharingManager;
 import edu.colorado.phet.reactantsproductsandleftovers.model.ChemicalReaction;
 import edu.colorado.phet.reactantsproductsandleftovers.model.OneProductReactions.AmmoniaReaction;
@@ -21,15 +21,15 @@ import edu.colorado.phet.reactantsproductsandleftovers.model.TwoProductReactions
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
 public class RealReactionModel extends RPALModel {
-    
+
     private final EventListenerList listeners;
     private final ChemicalReaction[] reactions;
     private ChemicalReaction reaction;
     private final ChemicalReaction defaultReaction;
-    
+
     public RealReactionModel() {
         reactions = new ChemicalReaction[] { new WaterReaction(), new AmmoniaReaction(), new MethaneReaction() };
-        defaultReaction = ( new RPALApril2012().studyName.equals( SimSharingManager.getInstance().getStudyName() ) ? reactions[1] : reactions[0] );
+        defaultReaction = ( SimSharingManager.usingConfig( SimSharingConfig.RPAL_APRIL_2012 ) ) ? reactions[1] : reactions[0];
         reaction = defaultReaction;
         listeners = new EventListenerList();
     }
@@ -42,28 +42,28 @@ public class RealReactionModel extends RPALModel {
         }
         setReaction( defaultReaction );
     }
-    
+
     public ChemicalReaction[] getReactions() {
         return reactions;
     }
-    
+
     public void setReaction( ChemicalReaction reaction ) {
         this.reaction = reaction;
         fireStateChanged();
     }
-    
+
     public ChemicalReaction getReaction() {
         return reaction;
     }
-    
+
     public void addChangeListener( ChangeListener listener ) {
         listeners.add( ChangeListener.class, listener );
     }
-    
+
     public void removeChangeListener( ChangeListener listener ) {
         listeners.remove( ChangeListener.class, listener );
     }
-    
+
     private void fireStateChanged() {
         ChangeEvent event = new ChangeEvent( this );
         for ( ChangeListener listener : listeners.getListeners( ChangeListener.class ) ) {
