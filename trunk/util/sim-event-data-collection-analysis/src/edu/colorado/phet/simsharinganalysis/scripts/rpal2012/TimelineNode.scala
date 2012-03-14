bb3834a032da10d13b03917ab9e1091fd3bf8aa9
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-03-14 02:49:58 +0000

    show number of tries for guesses

--- trunk/util/sim-event-data-collection-analysis/src/edu/colorado/phet/simsharinganalysis/scripts/rpal2012/TimelineNode.scala
+++ trunk/util/sim-event-data-collection-analysis/src/edu/colorado/phet/simsharinganalysis/scripts/rpal2012/TimelineNode.scala
@@ -3,10 +3,11 @@ package edu.colorado.phet.simsharinganalysis.scripts.rpal2012
 
 import edu.umd.cs.piccolo.PNode
 import edu.colorado.phet.simsharinganalysis.scripts.StateEntry
-import edu.colorado.phet.common.piccolophet.nodes.PhetPPath
 import java.awt.geom.Rectangle2D
 import java.awt.Color
 import edu.colorado.phet.common.phetcommon.math.Function.LinearFunction
+import edu.colorado.phet.common.piccolophet.nodes.{PhetPText, PhetPPath}
+import edu.colorado.phet.common.piccolophet.nodes.layout.VBox
 
 /**
  * @author Sam Reid
@@ -14,7 +15,7 @@ import edu.colorado.phet.common.phetcommon.math.Function.LinearFunction
 class TimelineNode(states: List[StateEntry[SimState]]) extends PNode {
   val startTime = states.head.entry.time
   val endTime = states.last.entry.time
-  val function = new LinearFunction(startTime, endTime + 1, 0, 1024)
+  val function = new LinearFunction(startTime, Math.max(endTime, startTime + 1000 * 60 * 3), 0, 1024)
   val lightRed = new Color(255, 147, 147)
   val lightBlue = new Color(96, 216, 255)
   val lightGreen = new Color(144, 255, 191)
@@ -29,6 +30,17 @@ class TimelineNode(states: List[StateEntry[SimState]]) extends PNode {
     val t1 = if ( i + 1 <= states.length - 1 ) function.evaluate(states(i + 1).entry.time) else t0
     val width = t1 - t0
     addChild(new PhetPPath(new Rectangle2D.Double(function.evaluate(state.entry.time), 0, width, 10), color))
-    addChild(new PhetPPath(new Rectangle2D.Double(function.evaluate(state.entry.time), 0, 1, 10), Color.black))
+    val tick = new Rectangle2D.Double(function.evaluate(state.entry.time), 0, 1, 10)
+    addChild(new PhetPPath(tick, Color.black))
+    if ( state.entry.matches("checkButton", "pressed") ) {
+      val text = new PhetPText(state.entry("attempts"))
+      val text2 = new PhetPText(state.entry("correct").toBoolean match {
+                                  case true => "+"
+                                  case false => "-"
+                                })
+      addChild(new VBox(text, text2) {
+        setOffset(tick.getCenterX - getFullBounds.getWidth / 2, tick.getMaxY)
+      })
+    }
   }
 }
