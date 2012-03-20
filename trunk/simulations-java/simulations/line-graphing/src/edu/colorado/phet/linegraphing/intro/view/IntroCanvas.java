0a722077e3246dcd10dd71ecb9dfa31e633d58bb
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-03-20 23:26:59 +0000

    control for chosing form of slope-intercept equation

--- trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/intro/view/IntroCanvas.java
+++ trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/intro/view/IntroCanvas.java
@@ -3,10 +3,12 @@ package edu.colorado.phet.linegraphing.intro.view;
 
 import java.awt.Color;
 
+import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.piccolophet.nodes.ResetAllButtonNode;
 import edu.colorado.phet.linegraphing.LGConstants;
 import edu.colorado.phet.linegraphing.common.view.LGCanvas;
 import edu.colorado.phet.linegraphing.intro.model.IntroModel;
+import edu.colorado.phet.linegraphing.intro.view.SlopeInterceptFormControl.SlopeInterceptForm;
 import edu.umd.cs.piccolo.PNode;
 
 /**
@@ -16,10 +18,12 @@ import edu.umd.cs.piccolo.PNode;
  */
 public class IntroCanvas extends LGCanvas {
 
+    private final Property<SlopeInterceptForm> slopeInterceptForm = new Property<SlopeInterceptForm>( SlopeInterceptForm.Y_FORM ); //TODO relocate
+
     public IntroCanvas( IntroModel model ) {
 
-        final PNode savedLinesControl = new SavedLinesControl();
-
+        PNode savedLinesControl = new SavedLinesControl();
+        PNode slopeInterceptFormControl = new SlopeInterceptFormControl( slopeInterceptForm );
         PNode resetAllButtonNode = new ResetAllButtonNode( model, null, LGConstants.CONTROL_FONT_SIZE, Color.BLACK, Color.ORANGE ) {{
             setConfirmationEnabled( false );
         }};
@@ -27,6 +31,7 @@ public class IntroCanvas extends LGCanvas {
         // rendering order
         {
             addChild( savedLinesControl );
+            addChild( slopeInterceptFormControl );
             addChild( resetAllButtonNode );
         }
 
@@ -37,6 +42,8 @@ public class IntroCanvas extends LGCanvas {
             final double yMargin = 20;
             savedLinesControl.setOffset( getStageSize().getWidth() - savedLinesControl.getFullBoundsReference().getWidth() - xMargin,
                                        100 );
+            slopeInterceptFormControl.setOffset( getStageSize().getWidth() - slopeInterceptFormControl.getFullBoundsReference().getWidth() - xMargin,
+                                                 savedLinesControl.getFullBoundsReference().getMaxY() + 20 );
             // lower right
             resetAllButtonNode.setOffset( getStageSize().getWidth() - resetAllButtonNode.getFullBoundsReference().getWidth() - xMargin,
                                           getStageSize().getHeight() - resetAllButtonNode.getFullBoundsReference().getHeight() - yMargin );
