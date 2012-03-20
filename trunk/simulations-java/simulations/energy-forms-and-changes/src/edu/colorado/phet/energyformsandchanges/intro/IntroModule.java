74b6a92ad517d78bbd636ea84964c0b7b3f44d53
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-03-20 17:58:55 +0000

    Added basic classes for initial setup (i.e. application, module, model, and canvas classes).

---
+++ trunk/simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/intro/IntroModule.java
@@ -0,0 +1,35 @@
+// Copyright 2002-2012, University of Colorado
+package edu.colorado.phet.energyformsandchanges.intro;
+
+import edu.colorado.phet.common.piccolophet.SimSharingPiccoloModule;
+import edu.colorado.phet.energyformsandchanges.EnergyFormsAndChangesResources;
+import edu.colorado.phet.energyformsandchanges.EnergyFormsAndChangesSimSharing;
+import edu.colorado.phet.energyformsandchanges.intro.model.IntroModel;
+import edu.colorado.phet.energyformsandchanges.intro.view.IntroCanvas;
+
+/**
+ * The "Intro" module for the Energy Forms and Changes simulation.  This is
+ * where the model and canvas are hooked together.
+ *
+ * @author John Blanco
+ */
+public class IntroModule extends SimSharingPiccoloModule {
+
+    private IntroModel model;
+
+    public IntroModule() {
+        this( new IntroModel() );
+    }
+
+    private IntroModule( IntroModel model ) {
+        super( EnergyFormsAndChangesSimSharing.UserComponents.introTab, EnergyFormsAndChangesResources.Strings.INTRO, model.getClock() );
+        this.model = model;
+        setClockControlPanel( null );
+        setSimulationPanel( new IntroCanvas( model ) );
+        reset();
+    }
+
+    @Override public void reset() {
+        model.reset();
+    }
+}
