c84ae65be25120c1d1fc4820775e5e6b84f9f298
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-03-20 07:22:29 +0000

    pt: Fixed: hard color boundary between above/below sea level terrain was causing "instant" changes of flashing terrain

--- trunk/simulations-java/simulations/plate-tectonics/src/edu/colorado/phet/platetectonics/view/TerrainNode.java
+++ trunk/simulations-java/simulations/plate-tectonics/src/edu/colorado/phet/platetectonics/view/TerrainNode.java
@@ -103,22 +103,40 @@ public class TerrainNode extends GridStripNode {
         /*---------------------------------------------------------------------------*
         * color buffer
         *----------------------------------------------------------------------------*/
+        float grassRed = (float) ( PlateTectonicsConstants.EARTH_GREEN.getRed() ) / 255f;
+        float grassGreen = (float) ( PlateTectonicsConstants.EARTH_GREEN.getGreen() ) / 255f;
+        float grassBlue = (float) ( PlateTectonicsConstants.EARTH_GREEN.getBlue() ) / 255f;
+
         for ( int zIndex = 0; zIndex < numZSamples; zIndex++ ) {
             for ( int xIndex = 0; xIndex < numXSamples; xIndex++ ) {
                 float elevation = terrainStrip.getSample( xIndex, zIndex ).getElevation();
 
-                if ( elevation < 0 ) {
+                // blend elevations below a certain window
+                if ( elevation < 1000 ) {
                     float value = (float) MathUtil.clamp( 0, ( elevation + 7000 ) / 10000, 1 );
-                    colorBuffer.put( new float[] { value, value, value, 1 } );
+                    if ( elevation < -500 ) {
+                        // below this, no blend, and just straight ocean floor coloring based on depth
+                        colorBuffer.put( new float[] { value, value, value, 1 } );
+                    }
+                    else {
+                        // between 1km and -0.5km, we blend together the land and ocean terrain colors
+                        // -0.5km is chosen since this is the elevation of the mid-oceanic ridge
+                        float blend = (float) MathUtil.clamp( 0, -( elevation - 1000 ) / 1500, 1 );
+                        colorBuffer.put( new float[] {
+                                value * blend + grassRed * ( 1 - blend ),
+                                value * blend + grassGreen * ( 1 - blend ),
+                                value * blend + grassBlue * ( 1 - blend ),
+                                1
+                        } );
+                    }
                 }
                 else {
-                    float grassRed = (float) ( PlateTectonicsConstants.EARTH_GREEN.getRed() ) / 255f;
-                    float grassGreen = (float) ( PlateTectonicsConstants.EARTH_GREEN.getGreen() ) / 255f;
-                    float grassBlue = (float) ( PlateTectonicsConstants.EARTH_GREEN.getBlue() ) / 255f;
                     if ( elevation < 3000 ) {
+                        // straight grass below 3k
                         colorBuffer.put( new float[] { grassRed, grassGreen, grassBlue, 1 } );
                     }
                     else {
+                        // otherwise, we need to blend to snow (white) at the top
                         float ratioToSnow = ( elevation - 3000 ) / 7000;
                         if ( ratioToSnow > 1 ) {
                             ratioToSnow = 1;
