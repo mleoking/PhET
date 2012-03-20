ec8e4d16f42270c8383f072930d6245ebd543d2f
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-03-20 21:46:20 +0000

    add line-graphing project

---
+++ trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/LGResources.java
@@ -0,0 +1,12 @@
+// Copyright 2002-2012, University of Colorado
+package edu.colorado.phet.linegraphing;
+
+/**
+ * Resources for the line-graphing project.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
+public class LGResources {
+
+    public static final String PROJECT_NAME = "line-graphing";
+}
