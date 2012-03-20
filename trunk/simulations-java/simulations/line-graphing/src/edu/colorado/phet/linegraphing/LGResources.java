fee2a3f300498d771ea81d802b6afa1a86294de5
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-03-20 22:40:47 +0000

    English strings for Intro tab

--- trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/LGResources.java
+++ trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/LGResources.java
@@ -15,6 +15,17 @@ public class LGResources {
 
     // Localized strings
     public static class Strings {
+
+        public static final String AXIS_PLUS_X = RESOURCES.getLocalizedString( "axis.+x" );
+        public static final String AXIS_MINUS_X = RESOURCES.getLocalizedString( "axis.-x" );
+        public static final String AXIS_PLUS_Y = RESOURCES.getLocalizedString( "axis.+y" );
+        public static final String AXIS_MINUS_Y = RESOURCES.getLocalizedString( "axis.-y" );
+
+        public static final String EQUATION_Y_EQUALS_MX_PLUS_B = RESOURCES.getLocalizedString( "equation.y=mx+b" );
+        public static final String EQUATION_X_EQUALS_MY_PLUS_B = RESOURCES.getLocalizedString( "equation.x=my+b" );
+        public static final String EQUATION_Y_EQUALS_X = RESOURCES.getLocalizedString( "equation.y=+x" );
+        public static final String EQUATION_Y_EQUALS__MINUS_X = RESOURCES.getLocalizedString( "equation.y=-x" );
+
         public static final String TAB_INTRO = RESOURCES.getLocalizedString( "tab.intro" );
         public static final String TAB_GAME = RESOURCES.getLocalizedString( "tab.game" );
     }
