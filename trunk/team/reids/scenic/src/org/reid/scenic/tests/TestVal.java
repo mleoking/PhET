d48a0976919194e10351cf7480222a95d3be4237
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-03-20 04:31:19 +0000

    fixed compilation

--- trunk/team/reids/scenic/src/org/reid/scenic/tests/TestVal.java
+++ trunk/team/reids/scenic/src/org/reid/scenic/tests/TestVal.java
@@ -1,25 +1,19 @@
 // Copyright 2002-2012, University of Colorado
 package org.reid.scenic.tests;
 
-import lombok.val;
-
-import java.util.ArrayList;
-
-import edu.colorado.phet.common.phetcommon.util.function.Function1;
-
 /**
  * @author Sam Reid
  */
 public class TestVal {
     public static void main( String[] args ) {
-        val functions = new ArrayList<Function1<Integer, String>>();
-        functions.add( new Function1<Integer, String>() {
-            @Override public String apply( final Integer integer ) {
-                return integer.toString();
-            }
-        } );
-        System.out.println( "functions = " + functions );
-        val y = new TestVal().x;
-        System.out.println( "x = " + y );
+//        val functions = new ArrayList<Function1<Integer, String>>();
+//        functions.add( new Function1<Integer, String>() {
+//            @Override public String apply( final Integer integer ) {
+//                return integer.toString();
+//            }
+//        } );
+//        System.out.println( "functions = " + functions );
+//        val y = new TestVal().x;
+//        System.out.println( "x = " + y );
     }
 }
