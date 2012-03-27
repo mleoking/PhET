ca3d14315d088d3ec571bfabf1bf29079dd8e17f
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-03-27 19:45:20 +0000

    migrate PropertyCheckBoxWithIcon to phetcommon

--- trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/intro/view/VisibilityControls.java
+++ trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/intro/view/VisibilityControls.java
@@ -2,21 +2,14 @@
 package edu.colorado.phet.linegraphing.intro.view;
 
 import java.awt.Font;
-import java.awt.Image;
 import java.awt.Insets;
 import java.text.MessageFormat;
 
-import javax.swing.ImageIcon;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 
 import edu.colorado.phet.common.phetcommon.model.property.Property;
-import edu.colorado.phet.common.phetcommon.model.property.SettableProperty;
-import edu.colorado.phet.common.phetcommon.simsharing.components.SimSharingIcon;
-import edu.colorado.phet.common.phetcommon.simsharing.messages.IUserComponent;
-import edu.colorado.phet.common.phetcommon.util.function.VoidFunction0;
-import edu.colorado.phet.common.phetcommon.view.controls.PropertyCheckBox;
+import edu.colorado.phet.common.phetcommon.view.controls.PropertyCheckBoxWithIcon;
 import edu.colorado.phet.common.phetcommon.view.util.GridPanel;
 import edu.colorado.phet.common.phetcommon.view.util.GridPanel.Anchor;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
@@ -46,19 +39,17 @@ class VisibilityControls extends PNode {
     public VisibilityControls( Property<Boolean> yEqualsXVisible, Property<Boolean> yEqualsNegativeXVisible,
                                Property<Boolean> pointToolVisible, Property<Boolean> riseOverRunVisible, Property<Boolean> graphLinesVisible ) {
 
-        //TODO add "Show" title
-
-        //TODO add icons
-
+        // components
         JComponent title = new JLabel( Strings.SHOW ) {{
             setFont( TITLE_FONT );
         }};
-        JComponent positiveCheckBox = new CheckBoxWithIcon( UserComponents.yEqualsXCheckBox, Y_EQUALS_X, Images.Y_EQUALS_X_ICON, yEqualsXVisible );
-        JComponent negativeCheckBox = new CheckBoxWithIcon( UserComponents.yEqualsNegativeXCheckBox, Y_EQUALS_NEGATIVE_X, Images.Y_EQUALS_NEGATIVE_X_ICON, yEqualsNegativeXVisible );
-        JComponent pointToolCheckBox = new CheckBoxWithIcon( UserComponents.pointToolCheckBox, Strings.POINT_TOOL, Images.POINT_TOOL_ICON, pointToolVisible );
-        JComponent riseOverRunCheckBox = new CheckBoxWithIcon( UserComponents.riseOverRunCheckBox, Strings.RISE_OVER_RUN, Images.RISE_OVER_RUN_ICON, riseOverRunVisible );
-        JComponent graphLinesCheckBox = new CheckBoxWithIcon( UserComponents.graphLinesCheckBox, Strings.GRAPH_LINES, Images.GRAPH_LINES_ICON, graphLinesVisible );
+        JComponent positiveCheckBox = new PropertyCheckBoxWithIcon( UserComponents.yEqualsXCheckBox, Y_EQUALS_X, CONTROL_FONT, Images.Y_EQUALS_X_ICON, yEqualsXVisible );
+        JComponent negativeCheckBox = new PropertyCheckBoxWithIcon( UserComponents.yEqualsNegativeXCheckBox, Y_EQUALS_NEGATIVE_X, CONTROL_FONT, Images.Y_EQUALS_NEGATIVE_X_ICON, yEqualsNegativeXVisible );
+        JComponent pointToolCheckBox = new PropertyCheckBoxWithIcon( UserComponents.pointToolCheckBox, Strings.POINT_TOOL, CONTROL_FONT, Images.POINT_TOOL_ICON, pointToolVisible );
+        JComponent riseOverRunCheckBox = new PropertyCheckBoxWithIcon( UserComponents.riseOverRunCheckBox, Strings.RISE_OVER_RUN, CONTROL_FONT, Images.RISE_OVER_RUN_ICON, riseOverRunVisible );
+        JComponent graphLinesCheckBox = new PropertyCheckBoxWithIcon( UserComponents.graphLinesCheckBox, Strings.GRAPH_LINES, CONTROL_FONT, Images.GRAPH_LINES_ICON, graphLinesVisible );
 
+        // layout
         GridPanel panel = new GridPanel();
         panel.setGridX( 0 ); // vertical
         panel.setInsets( new Insets( 2, 2, 2, 2 ) );
@@ -71,20 +62,7 @@ class VisibilityControls extends PNode {
         panel.add( riseOverRunCheckBox );
         panel.add( graphLinesCheckBox );
 
+        // wrap Swing in a Piccolo control panel
         addChild( new ControlPanelNode( panel ) );
-    }
-
-    // Check box with associated icon. Clicking either the check box or icon toggles the property.
-    private static class CheckBoxWithIcon extends JPanel {
-        public CheckBoxWithIcon( IUserComponent userComponent, final String text, Image image, final SettableProperty<Boolean> property ) {
-            add( new PropertyCheckBox( userComponent, text, property ) {{
-                setFont( CONTROL_FONT );
-            }} );
-            add( new SimSharingIcon( userComponent, new ImageIcon( image ), new VoidFunction0() {
-                public void apply() {
-                    property.set( !property.get() );
-                }
-            } ) );
-        }
     }
 }
