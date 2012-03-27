adf8d0892394eddb409f7c6cecedff4ccb7b1a15
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-03-27 19:20:22 +0000

    delete control for choosing from of slope-intercept equation, we're using only y=mx+b

--- trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/intro/view/SlopeInterceptEquationNode.java
+++ trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/intro/view/SlopeInterceptEquationNode.java
@@ -2,10 +2,13 @@
 package edu.colorado.phet.linegraphing.intro.view;
 
 import java.awt.geom.Rectangle2D;
+import java.text.MessageFormat;
 
 import edu.colorado.phet.common.piccolophet.PhetPNode;
+import edu.colorado.phet.linegraphing.LGResources.Strings;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.nodes.PPath;
+import edu.umd.cs.piccolo.nodes.PText;
 
 /**
  * Interface for manipulating the source-intercept equation.
@@ -14,7 +17,19 @@ import edu.umd.cs.piccolo.nodes.PPath;
  */
 class SlopeInterceptEquationNode extends PhetPNode {
 
+    private static final String PATTERN = "{0} = {1}{2} + {3}"; // eg, y = mx + b
+
+    // y = mx + b
+    private static String EQUATION = MessageFormat.format( PATTERN,
+                                                         Strings.SYMBOL_VERTICAL_AXIS,
+                                                         Strings.SYMBOL_SLOPE,
+                                                         Strings.SYMBOL_HORIZONTAL_AXIS,
+                                                         Strings.SYMBOL_INTERCEPT );
+
     public SlopeInterceptEquationNode() {
-        addChild( new PPath( new Rectangle2D.Double( 0, 0, 350, 200 )) ); //TODO placeholder for layout
+        addChild( new PPath( new Rectangle2D.Double( 0, 0, 350, 200 ) ) ); //TODO placeholder for layout
+        addChild( new PText( EQUATION ) {{
+            setOffset( 10, 10 );
+        }} );
     }
 }
