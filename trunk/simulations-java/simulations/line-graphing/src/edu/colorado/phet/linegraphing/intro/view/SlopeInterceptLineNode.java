69eb2120b85093da6b0432f5108fb8fbef7b13c7
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-03-28 23:06:03 +0000

    constrain lines to the range of the graph

--- trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/intro/view/SlopeInterceptLineNode.java
+++ trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/intro/view/SlopeInterceptLineNode.java
@@ -20,15 +20,32 @@ public class SlopeInterceptLineNode extends PComposite {
 
     private static final PDimension ARROW_HEAD_SIZE = new PDimension( 8, 8 );
     private static final double LINE_THICKNESS = 1;
-    private static final double LINE_EXTENT = 0.5; // how far line extends past edges of graph, in model coordinates
 
     public SlopeInterceptLineNode( SlopeInterceptLine line, LineGraph graph, ModelViewTransform mvt, Color color ) {
-        final double tipX = graph.minX - LINE_EXTENT;
-        final double tailX = graph.maxX + LINE_EXTENT;
+
+        // constrain the line to the range of the graph
+        double tipX = graph.minX;
+        double tipY = line.solveY( tipX );
+        if ( tipY < graph.minY ) {
+            tipX = line.solveX( graph.minY );
+        }
+        else if ( tipY > graph.maxY ) {
+            tipX = line.solveX( graph.maxY );
+        }
+
+        double tailX = graph.maxX;
+        double tailY = line.solveY( tailX );
+        if ( tailY < graph.minY ) {
+            tailX = line.solveX( graph.minY );
+        }
+        else if ( tailY > graph.maxY ) {
+            tailX = line.solveX( graph.maxY );
+        }
+
         Point2D tipLocation = new Point2D.Double( mvt.modelToViewDeltaX( tipX ),
-                                                  mvt.modelToViewDeltaY( line.solve( tipX ) ) );
+                                                  mvt.modelToViewDeltaY( line.solveY( tipX ) ) );
         Point2D tailLocation = new Point2D.Double( mvt.modelToViewDeltaX( tailX ),
-                                                  mvt.modelToViewDeltaY( line.solve( tailX ) ) );
+                                                   mvt.modelToViewDeltaY( line.solveY( tailX ) ) );
         DoubleArrowNode arrowNode = new DoubleArrowNode( tailLocation, tipLocation, ARROW_HEAD_SIZE.getHeight(), ARROW_HEAD_SIZE.getWidth(), LINE_THICKNESS );
         arrowNode.setPaint( color );
         arrowNode.setStroke( null ); // DoubleArrowNode is a shape that we fill, no need to stroke
