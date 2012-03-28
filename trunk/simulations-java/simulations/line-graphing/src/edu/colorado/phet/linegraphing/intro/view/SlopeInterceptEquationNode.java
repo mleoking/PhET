086192749986c4b12789ef8ae119585a0b163509
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-03-28 21:57:16 +0000

    quick-and-dirty implementation of equation using Swing spinners

--- trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/intro/view/SlopeInterceptEquationNode.java
+++ trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/intro/view/SlopeInterceptEquationNode.java
@@ -1,19 +1,22 @@
 // Copyright 2002-2012, University of Colorado
 package edu.colorado.phet.linegraphing.intro.view;
 
+import java.awt.Font;
+import java.awt.geom.Line2D;
 import java.awt.geom.Rectangle2D;
 import java.text.MessageFormat;
 
-import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
 import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.phetcommon.simsharing.messages.UserComponent;
 import edu.colorado.phet.common.phetcommon.util.IntegerRange;
+import edu.colorado.phet.common.phetcommon.util.function.VoidFunction0;
 import edu.colorado.phet.common.phetcommon.view.controls.IntegerSpinner;
+import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.PhetPNode;
+import edu.colorado.phet.common.piccolophet.nodes.ControlPanelNode;
 import edu.colorado.phet.linegraphing.LGResources.Strings;
 import edu.colorado.phet.linegraphing.intro.model.SlopeInterceptLine;
 import edu.umd.cs.piccolo.PNode;
@@ -39,26 +42,57 @@ class SlopeInterceptEquationNode extends PhetPNode {
 
     public SlopeInterceptEquationNode( final Property<SlopeInterceptLine> interactiveLine ) {
 
-        //TODO placeholder for layout
-        addChild( new PPath( new Rectangle2D.Double( 0, 0, 350, 200 ) ) );
-
-        //TODO test for changing line
+        // TODO quick-and-dirty implementation using JSpinners
         {
+            // Swing controls
+            final IntegerSpinner riseSpinner = new IntegerSpinner( new UserComponent( "riseSpinner" ), new IntegerRange( 0, 10, interactiveLine.get().rise ) );
+            final IntegerSpinner runSpinner = new IntegerSpinner( new UserComponent( "runSpinner" ), new IntegerRange( -10, 10, interactiveLine.get().run ) );
             final IntegerSpinner interceptSpinner = new IntegerSpinner( new UserComponent( "interceptSpinner" ), new IntegerRange( -10, 10, interactiveLine.get().intercept ) );
-            interceptSpinner.addChangeListener( new ChangeListener() {
-                public void stateChanged( ChangeEvent e ) {
-                    SlopeInterceptLine line = interactiveLine.get();
-                    interactiveLine.set( new SlopeInterceptLine( line.rise, line.run, interceptSpinner.getIntValue() ) );
+            final VoidFunction0 updateLine = new VoidFunction0() {
+                public void apply() {
+                    interactiveLine.set( new SlopeInterceptLine( riseSpinner.getIntValue(), runSpinner.getIntValue(), interceptSpinner.getIntValue() ) );
                 }
-            } );
-            JLabel label = new JLabel( "y = mx + " );
-            JPanel panel = new JPanel();
-            panel.add( label );
-            panel.add( interceptSpinner );
-            PSwing pswing = new PSwing( panel );
-            pswing.setOffset( 30, 30 );
-            pswing.scale( 2 );
-            addChild( pswing );
+            };
+            final ChangeListener changeListener = new ChangeListener() {
+                public void stateChanged( ChangeEvent e ) {
+                    updateLine.apply();
+                }
+            };
+            riseSpinner.addChangeListener( changeListener );
+            runSpinner.addChangeListener( changeListener );
+            interceptSpinner.addChangeListener( changeListener );
+
+            // nodes
+            PText yEquals = new PText( "y = " ) {{ setFont( new PhetFont( Font.BOLD, 14 )); }};
+            PSwing pswingRise = new PSwing( riseSpinner );
+            PSwing pswingRun = new PSwing( runSpinner );
+            PPath lineNode = new PPath( new Line2D.Double( 0, 0, pswingRise.getFullBoundsReference().getWidth(), 0 ) );
+            PText xPlus = new PText( "x +" ) {{ setFont( new PhetFont( Font.BOLD, 14 )); }};
+            PSwing pswingIntercept = new PSwing( interceptSpinner );
+
+            // rendering order
+            PNode parentNode = new PNode();
+            parentNode.addChild( yEquals );
+            parentNode.addChild( pswingRise );
+            parentNode.addChild( lineNode );
+            parentNode.addChild( pswingRun );
+            parentNode.addChild( xPlus );
+            parentNode.addChild( pswingIntercept );
+
+            // layout
+            pswingRise.setOffset( yEquals.getFullBoundsReference().getMaxX() + 2,
+                                  yEquals.getFullBoundsReference().getMaxY() - pswingRise.getFullBoundsReference().getHeight() );
+            lineNode.setOffset( pswingRise.getXOffset(),
+                                 pswingRise.getFullBoundsReference().getMaxY() + 2 );
+            pswingRun.setOffset( pswingRise.getXOffset(),
+                                 lineNode.getFullBoundsReference().getMaxY() + 2 );
+            xPlus.setOffset( pswingRise.getFullBoundsReference().getMaxX() + 4,
+                             yEquals.getYOffset() );
+            pswingIntercept.setOffset( xPlus.getFullBoundsReference().getMaxX() + 2,
+                                       xPlus.getFullBoundsReference().getCenterY() - ( pswingIntercept.getFullBoundsReference().getHeight() / 2 ) );
+
+            parentNode.scale( 1.8 );
+            addChild( new ControlPanelNode( parentNode ) );
         }
     }
 }
