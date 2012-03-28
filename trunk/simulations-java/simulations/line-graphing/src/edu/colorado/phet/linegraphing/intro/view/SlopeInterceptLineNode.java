4a681681ab8fecec478580578a109c5d8c01e8f9
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-03-28 23:23:44 +0000

    extend lines a bit beyond the bounds of the grid

--- trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/intro/view/SlopeInterceptLineNode.java
+++ trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/intro/view/SlopeInterceptLineNode.java
@@ -4,6 +4,8 @@ package edu.colorado.phet.linegraphing.intro.view;
 import java.awt.Color;
 import java.awt.geom.Point2D;
 
+import javax.sound.sampled.Line;
+
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
 import edu.colorado.phet.common.piccolophet.nodes.DoubleArrowNode;
 import edu.colorado.phet.linegraphing.intro.model.LineGraph;
@@ -20,27 +22,28 @@ public class SlopeInterceptLineNode extends PComposite {
 
     private static final PDimension ARROW_HEAD_SIZE = new PDimension( 8, 8 );
     private static final double LINE_THICKNESS = 1;
+    private static final double LINE_EXTENT = 0.5;
 
     public SlopeInterceptLineNode( SlopeInterceptLine line, LineGraph graph, ModelViewTransform mvt, Color color ) {
 
         // tail is the left-most end point. Compute x such that the point is inside the grid.
-        double tailX = graph.minX;
+        double tailX = graph.minX - LINE_EXTENT;
         double tailY = line.solveY( tailX );
-        if ( tailY < graph.minY ) {
-            tailX = line.solveX( graph.minY );
+        if ( tailY < graph.minY - LINE_EXTENT ) {
+            tailX = line.solveX( graph.minY - LINE_EXTENT );
         }
-        else if ( tailY > graph.maxY ) {
-            tailX = line.solveX( graph.maxY );
+        else if ( tailY > graph.maxY + LINE_EXTENT ) {
+            tailX = line.solveX( graph.maxY + LINE_EXTENT );
         }
 
         // tip is the right-most end point. Compute x such that the point is inside the grid.
-        double tipX = graph.maxX;
+        double tipX = graph.maxX + LINE_EXTENT;
         double tipY = line.solveY( tipX );
-        if ( tipY < graph.minY ) {
-            tipX = line.solveX( graph.minY );
+        if ( tipY < graph.minY - LINE_EXTENT ) {
+            tipX = line.solveX( graph.minY - LINE_EXTENT );
         }
-        else if ( tipY > graph.maxY ) {
-            tipX = line.solveX( graph.maxY );
+        else if ( tipY > graph.maxY + LINE_EXTENT ) {
+            tipX = line.solveX( graph.maxY + LINE_EXTENT );
         }
 
         // double-headed arrow
