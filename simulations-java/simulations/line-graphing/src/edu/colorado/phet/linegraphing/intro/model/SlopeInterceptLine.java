ea7c5e7f6fa98a51876ad45cd953644efd213e11
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-03-28 19:58:41 +0000

    make SlopeInterceptLine immutable

--- simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/intro/model/SlopeInterceptLine.java
+++ simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/intro/model/SlopeInterceptLine.java
@@ -1,36 +1,30 @@
 // Copyright 2002-2012, University of Colorado
 package edu.colorado.phet.linegraphing.intro.model;
 
-import edu.colorado.phet.common.phetcommon.model.property.CompositeProperty;
-import edu.colorado.phet.common.phetcommon.model.property.Property;
-import edu.colorado.phet.common.phetcommon.util.function.Function0;
-
 /**
- * Model of a line, using slope-intercept form, y=mx+b.
+ * Model of an immutable line, using slope-intercept form, y=mx+b.
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
 public class SlopeInterceptLine {
 
-    public final Property<Integer> rise;
-    public final Property<Integer> run;
-    public final Property<Integer> intercept;
-    public final CompositeProperty<Double> slope; // derived
+    public final int rise;
+    public final int run;
+    public final int intercept;
 
     public SlopeInterceptLine( int rise, int run, int intercept ) {
-        this.rise = new Property<Integer>( rise );
-        this.run = new Property<Integer>( run );
-        this.intercept = new Property<Integer>( intercept );
-        this.slope = new CompositeProperty<Double>( new Function0<Double>() {
-            public Double apply() {
-                return ( (double) SlopeInterceptLine.this.rise.get() ) / SlopeInterceptLine.this.run.get();
-            }
-        }, this.rise, this.run );
+        this.rise = rise;
+        this.run = run;
+        this.intercept = intercept;
+    }
+
+    public double getSlope() {
+        return ( (double) rise ) / run;
     }
 
     // y=mx+b
     public double solve( double x ) {
-        return solve( slope.get(), x, intercept.get() );
+        return solve( getSlope(), x, intercept );
     }
 
     // y=mx+b
