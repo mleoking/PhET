a82c9e2887d19ccf84587ab2adac36d677088bde
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-04-01 00:48:37 +0000

    factored out parent class SlowMotionNormalTimeControlPanel

--- trunk/simulations-java/simulations/energy-skate-park/src/edu/colorado/phet/energyskatepark/view/piccolo/EnergySkateParkTimeControlPanel.java
+++ trunk/simulations-java/simulations/energy-skate-park/src/edu/colorado/phet/energyskatepark/view/piccolo/EnergySkateParkTimeControlPanel.java
@@ -1,65 +1,37 @@
 package edu.colorado.phet.energyskatepark.view.piccolo;
 
-import java.awt.Color;
-
 import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
 import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
 import edu.colorado.phet.common.phetcommon.view.TimeControlListener;
-import edu.colorado.phet.common.phetcommon.view.controls.PropertyRadioButton;
-import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
-import edu.colorado.phet.common.piccolophet.RichPNode;
-import edu.colorado.phet.common.piccolophet.nodes.layout.HBox;
-import edu.colorado.phet.common.piccolophet.nodes.mediabuttons.PiccoloClockControlPanel;
 import edu.colorado.phet.energyskatepark.AbstractEnergySkateParkModule;
-import edu.umd.cs.piccolox.pswing.PSwing;
 
 import static edu.colorado.phet.energyskatepark.EnergySkateParkApplication.SIMULATION_TIME_DT;
-import static edu.colorado.phet.energyskatepark.EnergySkateParkResources.getString;
-import static edu.colorado.phet.energyskatepark.EnergySkateParkSimSharing.UserComponents.normalSpeedRadioButton;
-import static edu.colorado.phet.energyskatepark.EnergySkateParkSimSharing.UserComponents.slowMotionRadioButton;
 
 /**
- * Clock control panel that shows "slow motion" and "normal" as radio buttons with a play/pause and step button.
+ * Adapter that wires up energy skate park module + clock to the reusable slow motion normal time control panel.
  *
  * @author Sam Reid
  */
-public class EnergySkateParkTimeControlPanel extends RichPNode {
-
-    private final Color BLANK = new Color( 0, 0, 0, 0 );
-    private final PhetFont RADIO_BUTTON_FONT = new PhetFont( 16 );
-
+public class EnergySkateParkTimeControlPanel extends SlowMotionNormalTimeControlPanel {
     public EnergySkateParkTimeControlPanel( final AbstractEnergySkateParkModule module, final ConstantDtClock clock ) {
+        super( module.normalSpeed, clock );
         module.normalSpeed.addObserver( new VoidFunction1<Boolean>() {
             public void apply( Boolean normalSpeed ) {
                 clock.setDt( normalSpeed ? SIMULATION_TIME_DT : SIMULATION_TIME_DT / 4.0 );
             }
         } );
+        piccoloClockControlPanel.addTimeControlListener( new TimeControlListener.TimeControlAdapter() {
+            public void stepPressed() {
+                module.setRecordOrLiveMode();
+            }
 
-        PiccoloClockControlPanel controlPanel = new PiccoloClockControlPanel( clock ) {{
-            setBackground( BLANK );
-            getButtonCanvas().setBackground( BLANK );
-            getBackgroundNode().setVisible( false );
-            addTimeControlListener( new TimeControlListener.TimeControlAdapter() {
-                public void stepPressed() {
-                    module.setRecordOrLiveMode();
-                }
+            public void playPressed() {
+                module.setRecordOrLiveMode();
+            }
 
-                public void playPressed() {
-                    module.setRecordOrLiveMode();
-                }
-
-                public void pausePressed() {
-                    module.setRecordOrLiveMode();
-                }
-            } );
-        }};
-
-        addChild( new HBox( new PSwing( new PropertyRadioButton<Boolean>( slowMotionRadioButton, getString( "slow.motion" ), module.normalSpeed, false ) {{
-            setBackground( BLANK );
-            setFont( RADIO_BUTTON_FONT );
-        }} ), new PSwing( new PropertyRadioButton<Boolean>( normalSpeedRadioButton, getString( "normal" ), module.normalSpeed, true ) {{
-            setBackground( BLANK );
-            setFont( RADIO_BUTTON_FONT );
-        }} ), new PSwing( controlPanel ) ) );
+            public void pausePressed() {
+                module.setRecordOrLiveMode();
+            }
+        } );
     }
 }
---
+++ trunk/simulations-java/simulations/energy-skate-park/src/edu/colorado/phet/energyskatepark/view/piccolo/SlowMotionNormalTimeControlPanel.java
@@ -0,0 +1,45 @@
+// Copyright 2002-2012, University of Colorado
+package edu.colorado.phet.energyskatepark.view.piccolo;
+
+import java.awt.Color;
+
+import edu.colorado.phet.common.phetcommon.model.clock.IClock;
+import edu.colorado.phet.common.phetcommon.model.property.SettableProperty;
+import edu.colorado.phet.common.phetcommon.view.controls.PropertyRadioButton;
+import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
+import edu.colorado.phet.common.piccolophet.RichPNode;
+import edu.colorado.phet.common.piccolophet.nodes.layout.HBox;
+import edu.colorado.phet.common.piccolophet.nodes.mediabuttons.PiccoloClockControlPanel;
+import edu.umd.cs.piccolox.pswing.PSwing;
+
+import static edu.colorado.phet.energyskatepark.EnergySkateParkResources.getString;
+import static edu.colorado.phet.energyskatepark.EnergySkateParkSimSharing.UserComponents.normalSpeedRadioButton;
+import static edu.colorado.phet.energyskatepark.EnergySkateParkSimSharing.UserComponents.slowMotionRadioButton;
+
+/**
+ * Clock control panel that shows "slow motion" and "normal" as radio buttons with a play/pause and step button.
+ *
+ * @author Sam Reid
+ */
+public class SlowMotionNormalTimeControlPanel extends RichPNode {
+
+    private final Color BLANK = new Color( 0, 0, 0, 0 );
+    private final PhetFont RADIO_BUTTON_FONT = new PhetFont( 16 );
+    protected final PiccoloClockControlPanel piccoloClockControlPanel;
+
+    public SlowMotionNormalTimeControlPanel( SettableProperty<Boolean> normalSpeed, IClock clock ) {
+        piccoloClockControlPanel = new PiccoloClockControlPanel( clock ) {{
+            setBackground( BLANK );
+            getButtonCanvas().setBackground( BLANK );
+            getBackgroundNode().setVisible( false );
+        }};
+
+        addChild( new HBox( new PSwing( new PropertyRadioButton<Boolean>( slowMotionRadioButton, getString( "slow.motion" ), normalSpeed, false ) {{
+            setBackground( BLANK );
+            setFont( RADIO_BUTTON_FONT );
+        }} ), new PSwing( new PropertyRadioButton<Boolean>( normalSpeedRadioButton, getString( "normal" ), normalSpeed, true ) {{
+            setBackground( BLANK );
+            setFont( RADIO_BUTTON_FONT );
+        }} ), new PSwing( piccoloClockControlPanel ) ) );
+    }
+}
