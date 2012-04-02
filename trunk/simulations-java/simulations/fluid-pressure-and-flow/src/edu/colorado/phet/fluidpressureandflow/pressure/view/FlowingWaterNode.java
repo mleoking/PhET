2594f8bd6230d191ae52526a6bf9de93485db379
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-04-02 22:15:29 +0000

    Pressure tab: Update faucet fluid color based on the density of the fluid

--- trunk/simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/pressure/view/FlowingWaterNode.java
+++ trunk/simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/pressure/view/FlowingWaterNode.java
@@ -3,6 +3,7 @@ package edu.colorado.phet.fluidpressureandflow.pressure.view;
 
 import java.awt.geom.Rectangle2D;
 
+import edu.colorado.phet.common.phetcommon.model.property.ObservableProperty;
 import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.phetcommon.util.RichSimpleObserver;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
@@ -10,7 +11,6 @@ import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.colorado.phet.fluidpressureandflow.pressure.model.TrapezoidPool;
 import edu.umd.cs.piccolo.PNode;
 
-import static edu.colorado.phet.fluidpressureandflow.pressure.view.WaterColor.WATER_DENSITY;
 import static edu.colorado.phet.fluidpressureandflow.pressure.view.WaterColor.getBottomColor;
 
 /**
@@ -19,16 +19,17 @@ import static edu.colorado.phet.fluidpressureandflow.pressure.view.WaterColor.ge
  * @author Sam Reid
  */
 public class FlowingWaterNode extends PNode {
-    public FlowingWaterNode( final TrapezoidPool pool, final Property<Double> flowRatePercentage, final ModelViewTransform transform ) {
+    public FlowingWaterNode( final TrapezoidPool pool, final Property<Double> flowRatePercentage, final ModelViewTransform transform, final ObservableProperty<Double> liquidDensity ) {
         final double distanceToStickUpAboveTheGroundToGoBehindFaucet = 0.5;
-        addChild( new PhetPPath( getBottomColor( WATER_DENSITY ) ) {{
+        addChild( new PhetPPath( getBottomColor( liquidDensity.get() ) ) {{
             new RichSimpleObserver() {
                 @Override public void update() {
                     double width = 0.6 * flowRatePercentage.get();//meters wide of the fluid flow
                     final Rectangle2D.Double shape = new Rectangle2D.Double( pool.centerAtLeftChamberOpening - width / 2, -pool.height + pool.getWaterHeight(), width, pool.height + distanceToStickUpAboveTheGroundToGoBehindFaucet - pool.getWaterHeight() );
                     setPathTo( transform.modelToView( shape ) );
+                    setPaint( getBottomColor( liquidDensity.get() ) );
                 }
-            }.observe( flowRatePercentage, pool.waterVolume );
+            }.observe( flowRatePercentage, pool.waterVolume, liquidDensity );
         }} );
     }
 }
