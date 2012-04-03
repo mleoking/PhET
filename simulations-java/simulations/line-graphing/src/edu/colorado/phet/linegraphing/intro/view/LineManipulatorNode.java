b22c817fe938364ed659b8e9258e6a1f82f9816c
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-04-03 15:38:44 +0000

    3D look for line manipulators

--- simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/intro/view/LineManipulatorNode.java
+++ simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/intro/view/LineManipulatorNode.java
@@ -4,6 +4,7 @@ package edu.colorado.phet.linegraphing.intro.view;
 import java.awt.BasicStroke;
 import java.awt.Color;
 
+import edu.colorado.phet.common.piccolophet.nodes.ShadedSphereNode;
 import edu.colorado.phet.common.piccolophet.nodes.SphericalNode;
 
 /**
@@ -11,9 +12,10 @@ import edu.colorado.phet.common.piccolophet.nodes.SphericalNode;
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
-public class LineManipulatorNode extends SphericalNode {
+public class LineManipulatorNode extends ShadedSphereNode {
 
     public LineManipulatorNode( double diameter, Color fillColor ) {
-         super( diameter, fillColor, new BasicStroke( 1f ), Color.BLACK, false /* convertToImage */ );
+        super( diameter, fillColor, Color.WHITE, fillColor.darker().darker(), false /* convertToImage */ );
+        setStrokeAndPaint( new BasicStroke( 1f ), fillColor.darker().darker() );
     }
 }
