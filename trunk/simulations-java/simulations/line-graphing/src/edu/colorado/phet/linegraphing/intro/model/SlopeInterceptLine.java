828a1f755fb5c51e640e1b48581ef186c07c833f
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-04-03 01:38:14 +0000

    4/2/12 show reduced equations on lines

--- trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/intro/model/SlopeInterceptLine.java
+++ trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/intro/model/SlopeInterceptLine.java
@@ -1,6 +1,11 @@
 // Copyright 2002-2012, University of Colorado
 package edu.colorado.phet.linegraphing.intro.model;
 
+import java.text.MessageFormat;
+
+import edu.colorado.phet.linegraphing.LGResources;
+import edu.colorado.phet.linegraphing.LGResources.Strings;
+
 /**
  * Model of an immutable line, using slope-intercept form, y=mx+b.
  *
@@ -55,22 +60,74 @@ public class SlopeInterceptLine {
         return ( y - b ) / m;
     }
 
-    //TODO i18n
+    // Gets the equation as it was entered by the user.
     public String getEquation() {
+        final String x = Strings.SYMBOL_HORIZONTAL_AXIS;
+        final String y = Strings.SYMBOL_VERTICAL_AXIS;
         if ( run == 0 ) {
-            return "x = 0";
+            return x + " = 0";
         }
         else if ( rise == 0 ) {
-            return "y = " + (int)intercept;
+            return y + " = " + (int) intercept;
         }
         else if ( intercept == 0 ) {
-            return "y = (" + (int)rise + "/" + (int)run + ")x";
+            return y + " = (" + (int) rise + "/" + (int) run + ")" + x;
         }
         else if ( intercept < 0 ) {
-            return "y = (" + (int)rise + "/" + (int)run + ")x - " + (int)Math.abs( intercept );
+            return y + " = (" + (int) rise + "/" + (int) run + ")" + x + " - " + (int) Math.abs( intercept );
         }
         else {
-            return "y = (" + (int)rise + "/" + (int)run + ")x + " + (int)Math.abs( intercept );
+            return y + " = (" + (int) rise + "/" + (int) run + ")" + x + " + " + (int) Math.abs( intercept );
+        }
+    }
+
+    // Gets the equation with slope in reduced form.
+    public String getReducedEquation() {
+        final String x = Strings.SYMBOL_HORIZONTAL_AXIS;
+        final String y = Strings.SYMBOL_VERTICAL_AXIS;
+        if ( run == 0 ) {
+            return x + " = 0";
+        }
+        else if ( rise == 0 ) {
+            return y + " = " + (int) intercept;
+        }
+        else if ( intercept == 0 ) {
+            return y + " = " + getReducedSlope() + x;
+        }
+        else if ( intercept < 0 ) {
+            return y + " = " + getReducedSlope() + x + " - " + (int) Math.abs( intercept );
+        }
+        else {
+            return y + " = " + getReducedSlope() + x + " + " + (int) Math.abs( intercept );
+        }
+    }
+
+    // Gets the slope as an integer or reduced fraction.
+    private String getReducedSlope() {
+        if ( rise == run ) {
+            return "";
+        }
+        else if ( rise % run == 0 ) {
+            return String.valueOf( (int) ( rise / run ) );
+        }
+        else {
+            int gcd = getGreatestCommonDivisor( (int) Math.abs( rise ), (int) Math.abs( run ) );
+            if ( rise * run > 0 ) {
+                return MessageFormat.format( "({0}/{1})", (int) Math.abs( rise / gcd ), (int) Math.abs( run / gcd ) );
+            }
+            else {
+                return MessageFormat.format( "-({0}/{1})", (int) Math.abs( rise / gcd ), (int) Math.abs( run / gcd ) );
+            }
+        }
+    }
+
+    // Euclid's algorithm for computing the greatest common divisor (GCD) of two integers
+    private static int getGreatestCommonDivisor( int a, int b ) {
+        if ( b == 0 ) {
+            return a;
+        }
+        else {
+            return getGreatestCommonDivisor( b, a % b );
         }
     }
 }
