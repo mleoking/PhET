6afa7651b75bf7177de58fad1c9fd5d453160fd7
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-04-03 23:56:51 +0000

    rename bracket node

--- trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/intro/view/BracketValueNode.java
+++ trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/intro/view/BracketValueNode.java
@@ -7,7 +7,9 @@ import java.awt.Color;
 import java.awt.Paint;
 import java.awt.Stroke;
 import java.awt.geom.GeneralPath;
+import java.text.NumberFormat;
 
+import edu.colorado.phet.common.phetcommon.util.DefaultDecimalFormat;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPText;
 import edu.umd.cs.piccolo.PNode;
@@ -16,42 +18,44 @@ import edu.umd.cs.piccolo.nodes.PText;
 import edu.umd.cs.piccolox.nodes.PComposite;
 
 /**
- * A text label centered below a horizontal bracket.
+ * Bracket with a value next to it.
  * Origin is dependent on direction and sign of length.
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
-public class BracketLabelNode extends PComposite {
+public class BracketValueNode extends PComposite {
 
     public static enum Direction {LEFT, RIGHT, UP, DOWN} // direction that the open side of the bracket faces
 
+    // bracket
     private static final Stroke BRACKET_STROKE = new BasicStroke( 0.5f );
     private static final Color BRACKET_COLOR = Color.BLACK;
-    private static final PhetFont LABEL_FONT = new PhetFont( 12 );
-    private static final Color LABEL_COLOR = Color.BLACK;
-    private final double MINUS_SIGN_WIDTH = new PhetPText( "-", LABEL_FONT ).getFullBoundsReference().getWidth();
-
     private static final double BRACKET_END_HEIGHT = 10;
     private static final double BRACKET_CORNER_RADIUS = 0.5 * BRACKET_END_HEIGHT;
     private static final double BRACKET_TIP_WIDTH = 6;
     private static final double BRACKET_TIP_HEIGHT = 6;
 
-    private static final double TEXT_SPACING = 2;
+    // label
+    private static final PhetFont LABEL_FONT = new PhetFont( 12 );
+    private static final Color LABEL_COLOR = Color.BLACK;
+    private static final NumberFormat LABEL_FORMAT = new DefaultDecimalFormat( "0" );
+    private final double MINUS_SIGN_WIDTH = new PhetPText( "-", LABEL_FONT ).getFullBoundsReference().getWidth();
+    private static final double LABEL_SPACING = 2;
 
     // Constructor that uses default look
-    public BracketLabelNode( Direction direction, double length, String text ) {
-        this( direction, length, BRACKET_COLOR, BRACKET_STROKE, text, LABEL_FONT, LABEL_COLOR );
+    public BracketValueNode( Direction direction, double length, double value ) {
+        this( direction, length, BRACKET_COLOR, BRACKET_STROKE, value, LABEL_FONT, LABEL_COLOR );
     }
 
-    public BracketLabelNode( Direction direction, double length, Color bracketColor, Stroke bracketStroke, String text, PhetFont font, Color textColor ) {
+    public BracketValueNode( Direction direction, double length, Color bracketColor, Stroke bracketStroke, double value, PhetFont font, Color textColor ) {
 
         PNode bracketNode = new BracketNode( Math.abs( length ), bracketColor, bracketStroke );
         addChild( bracketNode );
 
-        PText textNode = new PText( text );
-        textNode.setFont( font );
-        textNode.setTextPaint( textColor );
-        addChild( textNode );
+        PText labelNode = new PText( LABEL_FORMAT.format( value ) );
+        labelNode.setFont( font );
+        labelNode.setTextPaint( textColor );
+        addChild( labelNode );
 
         // layout
         {
@@ -64,8 +68,8 @@ public class BracketLabelNode extends PComposite {
                     bracketNode.setOffset( -bracketNode.getFullBoundsReference().getWidth(), 0 );
                 }
                 // centered below bracket
-                textNode.setOffset( bracketNode.getFullBoundsReference().getCenterX() - ( textNode.getFullBoundsReference().getWidth() / 2 ) - signOffset,
-                                    bracketNode.getFullBoundsReference().getMaxY() + TEXT_SPACING );
+                labelNode.setOffset( bracketNode.getFullBoundsReference().getCenterX() - ( labelNode.getFullBoundsReference().getWidth() / 2 ) - signOffset,
+                                    bracketNode.getFullBoundsReference().getMaxY() + LABEL_SPACING );
             }
             else if ( direction == Direction.DOWN ) {
                 bracketNode.setRotation( Math.PI );
@@ -73,8 +77,8 @@ public class BracketLabelNode extends PComposite {
                     bracketNode.setOffset( bracketNode.getFullBoundsReference().getWidth(), 0 );
                 }
                 // centered above bracket
-                textNode.setOffset( bracketNode.getFullBoundsReference().getCenterX() - ( textNode.getFullBoundsReference().getWidth() / 2 ) - signOffset,
-                                    bracketNode.getFullBoundsReference().getMinY() - textNode.getFullBoundsReference().getHeight() - TEXT_SPACING );
+                labelNode.setOffset( bracketNode.getFullBoundsReference().getCenterX() - ( labelNode.getFullBoundsReference().getWidth() / 2 ) - signOffset,
+                                    bracketNode.getFullBoundsReference().getMinY() - labelNode.getFullBoundsReference().getHeight() - LABEL_SPACING );
             }
             else if ( direction == Direction.LEFT ) {
                 bracketNode.setRotation( -Math.PI / 2 );
@@ -82,8 +86,8 @@ public class BracketLabelNode extends PComposite {
                     bracketNode.setOffset( 0, bracketNode.getFullBoundsReference().getHeight() );
                 }
                 // centered to right of bracket
-                textNode.setOffset( bracketNode.getFullBoundsReference().getMaxX() + TEXT_SPACING,
-                                    bracketNode.getFullBoundsReference().getCenterY() - ( textNode.getFullBoundsReference().getHeight() / 2 ) );
+                labelNode.setOffset( bracketNode.getFullBoundsReference().getMaxX() + LABEL_SPACING,
+                                     bracketNode.getFullBoundsReference().getCenterY() - ( labelNode.getFullBoundsReference().getHeight() / 2 ) );
             }
             else if ( direction == Direction.RIGHT ) {
                 bracketNode.setRotation( Math.PI / 2 );
@@ -91,8 +95,8 @@ public class BracketLabelNode extends PComposite {
                     bracketNode.setOffset( 0, -bracketNode.getFullBoundsReference().getHeight() );
                 }
                 // centered to left of bracket
-                textNode.setOffset( bracketNode.getFullBoundsReference().getMinX() - textNode.getFullBoundsReference().getWidth() - TEXT_SPACING,
-                                    bracketNode.getFullBoundsReference().getCenterY() - ( textNode.getFullBoundsReference().getHeight() / 2 ) );
+                labelNode.setOffset( bracketNode.getFullBoundsReference().getMinX() - labelNode.getFullBoundsReference().getWidth() - LABEL_SPACING,
+                                    bracketNode.getFullBoundsReference().getCenterY() - ( labelNode.getFullBoundsReference().getHeight() / 2 ) );
             }
             else {
                 throw new UnsupportedOperationException( "unsupported direction: " + direction );
