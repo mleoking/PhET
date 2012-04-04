cf1c4720a4baa0843d6b99bad4ae9cc0d2459998
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-04-04 19:24:25 +0000

    Added a shelf.

--- trunk/simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/intro/model/IntroModel.java
+++ trunk/simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/intro/model/IntroModel.java
@@ -1,6 +1,10 @@
 // Copyright 2002-2012, University of Colorado
 package edu.colorado.phet.energyformsandchanges.intro.model;
 
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.List;
+
 import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
 import edu.colorado.phet.common.phetcommon.model.clock.IClock;
 
@@ -11,8 +15,20 @@ import edu.colorado.phet.common.phetcommon.model.clock.IClock;
  * @author John Blanco
  */
 public class IntroModel {
+
+    // Main model clock.
     protected final ConstantDtClock clock = new ConstantDtClock( 30.0 );
 
+    // List of all shelves in the model.
+    private final List<Shelf> shelfList = new ArrayList<Shelf>();
+
+    public IntroModel() {
+        shelfList.add( new Shelf( new Point2D.Double( 0, 0 ), 1, "blah", 0.02, 0.1, Math.PI / 2 ) );
+    }
+
+    public List<Shelf> getShelfList() {
+        return shelfList;
+    }
 
     public void reset() {
         // TODO.
