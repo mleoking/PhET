ad6bebe0f943a3f290e4bf8d2465fbb2a0814e69
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-04-04 21:20:36 +0000

    used colorblind-friendly version of red for interactive line

--- simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/LGColors.java
+++ simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/LGColors.java
@@ -3,6 +3,8 @@ package edu.colorado.phet.linegraphing;
 
 import java.awt.Color;
 
+import edu.colorado.phet.common.phetcommon.view.PhetColorScheme;
+
 /**
  * Colors used throughout this project.
  *
@@ -10,11 +12,11 @@ import java.awt.Color;
  */
 public class LGColors {
 
-    public static final Color INTERACTIVE_LINE = Color.RED;
+    public static final Color INTERACTIVE_LINE = PhetColorScheme.RED_COLORBLIND;
     public static final Color SAVED_LINE_NORMAL = Color.GREEN.darker().darker();
     public static final Color SAVED_LINE_HIGHLIGHT = Color.GREEN.darker();
     public static final Color Y_EQUALS_X = Color.BLUE;
-    public static final Color Y_EQUALS_NEGATIVE_X = Color.CYAN;
+    public static final Color Y_EQUALS_NEGATIVE_X = Color.CYAN.darker();
     public static final Color SLOPE_COLOR = new Color( 120, 215, 3 );
     public static final Color INTERCEPT_COLOR = Color.YELLOW;
     public static final Color SAVE_LINE_BUTTON = Color.WHITE;
