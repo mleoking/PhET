0f047f3e6af83758ba049c9c7d8b2b546c158ac3
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-04-04 18:52:29 +0000

    Added model representation of a shelf.

---
+++ simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/intro/model/Shelf.java
@@ -0,0 +1,48 @@
+// Copyright 2002-2012, University of Colorado
+package edu.colorado.phet.energyformsandchanges.intro.model;
+
+import java.awt.geom.Point2D;
+
+/**
+ * A horizontal surface on which objects within the model can be placed or
+ * dropped.  It is basically a horizontal line, but it also includes an image
+ * and additional information about how to represent it in the view.
+ *
+ * @author John Blanco
+ */
+public class Shelf {
+    // Position of the left edge of the shelf in model coordinates.
+    private final Point2D position;
+
+    // Width of the shelf in model units (meters).
+    private final double width;
+
+    // Name of the image file that should be used to represent the shelf.
+    private final String imageFileName;
+
+    // In the view, the shelf is portrayed as having a 3D appearance.  These
+    // variables are used to define how this is done.  Note that these must be
+    // coordinated with the image of the shelf that is used.
+    private final double thickness; // In meters.
+    private final double foreshortenedHeight; // In meters.
+    private final double perspectiveAngle; // In radians.
+
+    /**
+     * Constructor.
+     *
+     * @param position
+     * @param width
+     * @param imageFileName
+     * @param thickness
+     * @param foreshortenedHeight
+     * @param perspectiveAngle
+     */
+    public Shelf( Point2D position, double width, String imageFileName, double thickness, double foreshortenedHeight, double perspectiveAngle ) {
+        this.position = position;
+        this.width = width;
+        this.imageFileName = imageFileName;
+        this.thickness = thickness;
+        this.foreshortenedHeight = foreshortenedHeight;
+        this.perspectiveAngle = perspectiveAngle;
+    }
+}
