91a5bd69894bc217ab5e134490c530b74977fb82
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-04-04 14:19:35 +0000

    extracted class MassNode

---
+++ simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/pressure/view/MassNode.java
@@ -0,0 +1,82 @@
+// Copyright 2002-2012, University of Colorado
+package edu.colorado.phet.fluidpressureandflow.pressure.view;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Shape;
+import java.awt.geom.Dimension2D;
+
+import edu.colorado.phet.common.phetcommon.model.property.Property;
+import edu.colorado.phet.common.phetcommon.util.ObservableList;
+import edu.colorado.phet.common.phetcommon.util.function.Function1;
+import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
+import edu.colorado.phet.common.piccolophet.event.CursorHandler;
+import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
+import edu.colorado.phet.fluidpressureandflow.pressure.model.ChamberPool;
+import edu.colorado.phet.fluidpressureandflow.pressure.model.Mass;
+import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
+import edu.umd.cs.piccolo.event.PInputEvent;
+
+import static edu.colorado.phet.fluidpressureandflow.pressure.view.MassesLayer.getDottedLineShape;
+
+/**
+ * Node for showing draggable masses in the mass pool.
+ *
+ * @author Sam Reid
+ */
+public class MassNode extends PNode {
+    public MassNode( final ChamberPool pool, final Property<ObservableList<Mass>> masses, final Mass mass, final ModelViewTransform transform ) {
+        addChild( new PhetPPath( transform.modelToView( mass.shape ), Color.gray, new BasicStroke( 2 ), Color.black ) );
+        addInputEventListener( new CursorHandler() );
+        addInputEventListener( new PBasicInputEventHandler() {
+            @Override public void mousePressed( final PInputEvent event ) {
+                masses.set( masses.get().map( new Function1<Mass, Mass>() {
+                    @Override public Mass apply( final Mass m ) {
+                        return m == mass ? m.withDragging( true ) : m;
+                    }
+                } ) );
+            }
+
+            @Override public void mouseDragged( final PInputEvent event ) {
+                super.mouseDragged( event );
+                final Dimension2D modelDelta = transform.viewToModelDelta( event.getDelta() );
+                masses.set( masses.get().map( new Function1<Mass, Mass>() {
+                    @Override public Mass apply( final Mass mass ) {
+                        Mass translatedMass = mass.translate( modelDelta );
+                        return mass.dragging ? translatedMass : mass;
+                    }
+                } ) );
+            }
+
+            @Override public void mouseReleased( final PInputEvent event ) {
+
+                //Turn off the "dragging" flag
+                masses.set( masses.get().map( new Function1<Mass, Mass>() {
+                    @Override public Mass apply( Mass m ) {
+
+                        //Snap to the dotted line or above ground
+                        if ( m.dragging ) {
+                            boolean intersected = false;
+                            final Shape dottedLineShape = getDottedLineShape( pool, m );
+                            if ( m.shape.intersects( dottedLineShape.getBounds2D() ) ) {
+                                m = m.withMinY( dottedLineShape.getBounds2D().getMinY() ).withCenterX( dottedLineShape.getBounds2D().getCenterX() );
+                                intersected = true;
+                            }
+                            else if ( m.getMinY() < 0 ) {
+                                m = m.withMinY( 0.0 );
+                            }
+
+                            //if mass is over an opening, move it back to its initial position
+                            if ( pool.isOverOpening( m ) && !intersected ) {
+                                m = m.withShape( m.initialShape );
+                            }
+                        }
+
+                        return m.withDragging( false );
+                    }
+                } ) );
+            }
+        } );
+    }
+}
--- simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/pressure/view/MassesLayer.java
+++ simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/pressure/view/MassesLayer.java
@@ -4,22 +4,17 @@ package edu.colorado.phet.fluidpressureandflow.pressure.view;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Shape;
-import java.awt.geom.Dimension2D;
 import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 
 import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.phetcommon.util.ObservableList;
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
-import edu.colorado.phet.common.phetcommon.util.function.Function1;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
-import edu.colorado.phet.common.piccolophet.event.CursorHandler;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.colorado.phet.fluidpressureandflow.pressure.model.ChamberPool;
 import edu.colorado.phet.fluidpressureandflow.pressure.model.Mass;
 import edu.umd.cs.piccolo.PNode;
-import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
-import edu.umd.cs.piccolo.event.PInputEvent;
 
 /**
  * Node that shows a collection of masses for the chamber pool.
@@ -60,69 +55,13 @@ public class MassesLayer extends PNode {
         return null;
     }
 
-    private static class MassNode extends PNode {
-        public MassNode( final ChamberPool pool, final Property<ObservableList<Mass>> masses, final Mass mass, final ModelViewTransform transform ) {
-            addChild( new PhetPPath( transform.modelToView( mass.shape ), Color.gray, new BasicStroke( 2 ), Color.black ) );
-            addInputEventListener( new CursorHandler() );
-            addInputEventListener( new PBasicInputEventHandler() {
-                @Override public void mousePressed( final PInputEvent event ) {
-                    masses.set( masses.get().map( new Function1<Mass, Mass>() {
-                        @Override public Mass apply( final Mass m ) {
-                            return m == mass ? m.withDragging( true ) : m;
-                        }
-                    } ) );
-                }
-
-                @Override public void mouseDragged( final PInputEvent event ) {
-                    super.mouseDragged( event );
-                    final Dimension2D modelDelta = transform.viewToModelDelta( event.getDelta() );
-                    masses.set( masses.get().map( new Function1<Mass, Mass>() {
-                        @Override public Mass apply( final Mass mass ) {
-                            Mass translatedMass = mass.translate( modelDelta );
-                            return mass.dragging ? translatedMass : mass;
-                        }
-                    } ) );
-                }
-
-                @Override public void mouseReleased( final PInputEvent event ) {
-
-                    //Turn off the "dragging" flag
-                    masses.set( masses.get().map( new Function1<Mass, Mass>() {
-                        @Override public Mass apply( Mass m ) {
-
-                            //Snap to the dotted line or above ground
-                            if ( m.dragging ) {
-                                boolean intersected = false;
-                                final Shape dottedLineShape = getDottedLineShape( pool, m );
-                                if ( m.shape.intersects( dottedLineShape.getBounds2D() ) ) {
-                                    m = m.withMinY( dottedLineShape.getBounds2D().getMinY() ).withCenterX( dottedLineShape.getBounds2D().getCenterX() );
-                                    intersected = true;
-                                }
-                                else if ( m.getMinY() < 0 ) {
-                                    m = m.withMinY( 0.0 );
-                                }
-
-                                //if mass is over an opening, move it back to its initial position
-                                if ( pool.isOverOpening( m ) && !intersected ) {
-                                    m = m.withShape( m.initialShape );
-                                }
-                            }
-
-                            return m.withDragging( false );
-                        }
-                    } ) );
-                }
-            } );
-        }
-    }
-
     private class DottedLineDropRegion extends PNode {
         public DottedLineDropRegion( ChamberPool pool, final Mass dragging, ModelViewTransform transform ) {
             addChild( new PhetPPath( transform.modelToView( getDottedLineShape( pool, dragging ) ), new Color( 255, 220, 240 ), new BasicStroke( 2, BasicStroke.CAP_SQUARE, BasicStroke.JOIN_MITER, 1, new float[] { 8, 6 }, 0 ), Color.black ) );
         }
     }
 
-    private static Shape getDottedLineShape( final ChamberPool pool, final Mass dragging ) {
+    public static Shape getDottedLineShape( final ChamberPool pool, final Mass dragging ) {
         ArrayList<Mass> stackedMasses = pool.getStackedMasses();
         Rectangle2D bounds = pool.getLeftOpeningWaterShape().getBounds2D();
         double maxY = bounds.getMaxY();
