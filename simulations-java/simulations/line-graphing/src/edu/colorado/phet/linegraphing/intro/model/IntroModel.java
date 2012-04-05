4eac0df2f024235d4b02e11397e49b2af22429a3
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-04-05 21:05:31 +0000

    4/5/12 constants to make model easier to adjust

--- simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/intro/model/IntroModel.java
+++ simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/intro/model/IntroModel.java
@@ -16,8 +16,13 @@ import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTra
  */
 public class IntroModel implements Resettable {
 
-    private static final IntegerRange X_RANGE = new IntegerRange( -10, 10 );
+    private static final int GRID_VIEW_UNITS = 560; // max dimension of the grid in the view
+    private static final int GRID_MODEL_UNITS = 10; // dimensions of the grid in the model
+
+    private static final IntegerRange X_RANGE = new IntegerRange( -GRID_MODEL_UNITS, GRID_MODEL_UNITS );
     private static final IntegerRange Y_RANGE = X_RANGE;
+    private static final double MVT_SCALE = GRID_VIEW_UNITS / Math.max( X_RANGE.getLength(), Y_RANGE.getLength() ); // view units / model units
+
     public static final IntegerRange RISE_RANGE = new IntegerRange( -10, 10 );
     public static final IntegerRange RUN_RANGE = new IntegerRange( -10, 10 );
     public static final IntegerRange INTERCEPT_RANGE = new IntegerRange( -10, 10 );
@@ -29,7 +34,8 @@ public class IntroModel implements Resettable {
     public final Property<ImmutableVector2D> pointToolLocation;
 
     public IntroModel() {
-        mvt = ModelViewTransform.createOffsetScaleMapping( new Point2D.Double( 0, 0 ), 28, -28 ); // y is inverted
+
+        mvt = ModelViewTransform.createOffsetScaleMapping( new Point2D.Double( 0, 0 ), MVT_SCALE, -MVT_SCALE ); // y is inverted
         interactiveLine = new Property<SlopeInterceptLine>( new SlopeInterceptLine( 5, 5, 2 ) );
         yEqualsXLine = new SlopeInterceptLine( 1, 1, 0 );
         yEqualsNegativeXLine = new SlopeInterceptLine( 1, -1, 0 );
