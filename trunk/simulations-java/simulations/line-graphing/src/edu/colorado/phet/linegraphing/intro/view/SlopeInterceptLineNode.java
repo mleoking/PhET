d1338be19f3ddecf061a2e8ed5c7d2e91cc16046
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-04-05 23:21:47 +0000

    use larger font for equations in graph

--- trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/intro/view/SlopeInterceptLineNode.java
+++ trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/intro/view/SlopeInterceptLineNode.java
@@ -28,7 +28,7 @@ public class SlopeInterceptLineNode extends PComposite {
     private static final PDimension ARROW_HEAD_SIZE = new PDimension( 10, 10 );
     private static final double LINE_THICKNESS = 3;
     private static final double LINE_EXTENT = 25; // how far the line extends past the grid
-    private static final PhetFont EQUATION_FONT = new PhetFont( Font.BOLD, 14 );
+    private static final PhetFont EQUATION_FONT = new PhetFont( Font.BOLD, 20 );
 
     private final DoubleArrowNode arrowNode;
     private final PText equationNode;
