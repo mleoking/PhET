39ddb2b263eacabe9c5fd47d3a1036359bcccd95
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-04-06 22:34:37 +0000

    Added first cut of the burner to the model.

---
+++ trunk/simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/intro/model/Burner.java
@@ -0,0 +1,65 @@
+// Copyright 2002-2012, University of Colorado
+package edu.colorado.phet.energyformsandchanges.intro.model;
+
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+
+import edu.colorado.phet.common.phetcommon.model.property.Property;
+
+/**
+ * Model element that represents the burner in the simulation.  The burner can
+ * heat and also cool other model elements.
+ *
+ * @author John Blanco
+ */
+public class Burner {
+
+    //-------------------------------------------------------------------------
+    // Class Data
+    //-------------------------------------------------------------------------
+
+    private static final double WIDTH = 0.05; // In meters.
+    private static final double HEIGHT = WIDTH * 1.3;
+
+    //-------------------------------------------------------------------------
+    // Instance Data
+    //-------------------------------------------------------------------------
+
+    private final Point2D position = new Point2D.Double( 0, 0 );
+
+    // Property that is used to control the amount of heating or cooling that
+    // is being done.
+    public final Property<Double> heatCoolLevel = new Property<Double>( 0.0 );
+
+    //-------------------------------------------------------------------------
+    // Constructor(s)
+    //-------------------------------------------------------------------------
+
+    /**
+     * Constructor.
+     *
+     * @param position The position in model space where this burner exists.
+     *                 By convention for this simulation, the position is
+     *                 defined as the bottom center of the model element.
+     */
+    public Burner( Point2D position ) {
+        this.position.setLocation( position );
+    }
+
+    //-------------------------------------------------------------------------
+    // Methods
+    //-------------------------------------------------------------------------
+
+    /**
+     * Get a rectangle that defines the outline of the burner.  In the model,
+     * the burner is essentially a 2D rectangle.
+     *
+     * @return
+     */
+    public Rectangle2D getOutlineRect() {
+        return new Rectangle2D.Double( position.getX() - WIDTH / 2,
+                                       position.getY(),
+                                       WIDTH,
+                                       HEIGHT );
+    }
+}
