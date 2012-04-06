5734a89b60bb16b92133c99130e0620a2e72e64c
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-04-06 23:17:11 +0000

    Added beginnings of code for drawing the frame upon which heatable objects will sit.

--- simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/intro/view/BurnerNode.java
+++ simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/intro/view/BurnerNode.java
@@ -1,6 +1,12 @@
 // Copyright 2002-2012, University of Colorado
 package edu.colorado.phet.energyformsandchanges.intro.view;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
+
+import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.colorado.phet.energyformsandchanges.intro.model.Burner;
@@ -13,7 +19,22 @@ import edu.umd.cs.piccolo.PNode;
  */
 public class BurnerNode extends PNode {
 
+    private static final double perspectiveAngle = Math.PI / 4;
+    private static final double edgeLength = 20; // In screen units, which are close to pixels.
+
     public BurnerNode( Burner burner, ModelViewTransform mvt ) {
-        addChild( new PhetPPath( mvt.modelToViewRectangle( burner.getOutlineRect() ) ) );
+        addChild( new PhetPPath( mvt.modelToViewRectangle( burner.getOutlineRect() ), new BasicStroke( 1 ), Color.RED ) );
+
+        // Create the faux-3D frame for the burner.
+        Rectangle2D viewRect = mvt.modelToViewRectangle( burner.getOutlineRect() );
+        ImmutableVector2D offsetVector = new ImmutableVector2D( 0, -edgeLength / 2 ).getRotatedInstance( perspectiveAngle );
+        AffineTransform backTransform = AffineTransform.getTranslateInstance( offsetVector.getX(), offsetVector.getY() );
+        Rectangle2D backFrame = backTransform.createTransformedShape( viewRect ).getBounds2D();
+        addChild( new PhetPPath( backFrame, new BasicStroke( 1 ), Color.BLACK ) );
+        AffineTransform frontTransform = AffineTransform.getTranslateInstance( -offsetVector.getX(), -offsetVector.getY() );
+        Rectangle2D frontFrame = frontTransform.createTransformedShape( viewRect ).getBounds2D();
+        addChild( new PhetPPath( frontFrame, new BasicStroke( 1 ), Color.BLACK ) );
+
+
     }
 }
