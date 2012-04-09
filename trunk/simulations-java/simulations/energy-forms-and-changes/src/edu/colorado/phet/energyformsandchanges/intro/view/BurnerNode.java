e557107d9f35580162ce6575656e09f971ea3c27
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-04-09 20:28:21 +0000

    Added sides of the burner stand.

--- trunk/simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/intro/view/BurnerNode.java
+++ trunk/simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/intro/view/BurnerNode.java
@@ -3,15 +3,23 @@ package edu.colorado.phet.energyformsandchanges.intro.view;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
-import java.awt.geom.AffineTransform;
+import java.awt.Point;
+import java.awt.Stroke;
+import java.awt.geom.Dimension2D;
+import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
+
+import javax.swing.JFrame;
 
 import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
+import edu.colorado.phet.common.phetcommon.view.util.DoubleGeneralPath;
+import edu.colorado.phet.common.piccolophet.PhetPCanvas;
 import edu.colorado.phet.common.piccolophet.nodes.HeaterCoolerNode;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.colorado.phet.energyformsandchanges.intro.model.Burner;
 import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.util.PDimension;
 
 /**
  * Piccolo node that represents the burner in the view.
@@ -26,28 +34,142 @@ public class BurnerNode extends PNode {
     public BurnerNode( final Burner burner, final ModelViewTransform mvt ) {
         addChild( new PhetPPath( mvt.modelToViewRectangle( burner.getOutlineRect() ), new BasicStroke( 1 ), Color.RED ) );
 
-        // Add the heater-cooler node to the center bottom of the model
-        // representation.
+        // Get a version of the rectangle that defines the burner size and
+        // location in the view.
+        final Rectangle2D burnerViewRect = mvt.modelToView( burner.getOutlineRect() ).getBounds2D();
+
+        // Add the heater-cooler node to the center bottom.
         // TODO: i18n
         PNode heaterCoolerNode = new HeaterCoolerNode( burner.heatCoolLevel, "Heat", "Cool" ) {{
             setScale( mvt.modelToViewDeltaX( burner.getOutlineRect().getWidth() ) * 0.5 / getFullBoundsReference().width );
-            Rectangle2D burnerViewRect = mvt.modelToView( burner.getOutlineRect() ).getBounds2D();
             setOffset( burnerViewRect.getX() + burnerViewRect.getWidth() / 2 - getFullBoundsReference().width / 2,
                        burnerViewRect.getMaxY() - getFullBoundsReference().height );
         }};
         addChild( heaterCoolerNode );
 
+        // Add the left and right sides of the stand.
+        addChild( new BurnerSide( new Point2D.Double( burnerViewRect.getX(), burnerViewRect.getY() ), burnerViewRect.getHeight() ) );
+        addChild( new BurnerSide( new Point2D.Double( burnerViewRect.getMaxX(), burnerViewRect.getY() ), burnerViewRect.getHeight() ) );
 
+        //---------------------------------------------------------------------
         // Create the faux-3D frame for the burner.
+        //---------------------------------------------------------------------
+
+        ImmutableVector2D backOffsetVector = new ImmutableVector2D( -edgeLength / 2, -edgeLength / 2 ).getRotatedInstance( perspectiveAngle );
+        ImmutableVector2D frontOffsetVector = new ImmutableVector2D( edgeLength / 2, edgeLength / 2 ).getRotatedInstance( perspectiveAngle );
+
+        // Draw the left side rectangle.
+        DoubleGeneralPath leftSideRectangle = new DoubleGeneralPath();
+        leftSideRectangle.moveTo( new ImmutableVector2D( burnerViewRect.getMinX(), burnerViewRect.getMinY() ) );
+        leftSideRectangle.lineTo( 0, 0 );
+
+        // Create vectors for the 
+        // Draw the laf
         Rectangle2D viewRect = mvt.modelToViewRectangle( burner.getOutlineRect() );
-        ImmutableVector2D offsetVector = new ImmutableVector2D( 0, -edgeLength / 2 ).getRotatedInstance( perspectiveAngle );
-        AffineTransform backTransform = AffineTransform.getTranslateInstance( offsetVector.getX(), offsetVector.getY() );
-        Rectangle2D backFrame = backTransform.createTransformedShape( viewRect ).getBounds2D();
-        addChild( new PhetPPath( backFrame, new BasicStroke( 1 ), Color.BLACK ) );
-        AffineTransform frontTransform = AffineTransform.getTranslateInstance( -offsetVector.getX(), -offsetVector.getY() );
-        Rectangle2D frontFrame = frontTransform.createTransformedShape( viewRect ).getBounds2D();
-        addChild( new PhetPPath( frontFrame, new BasicStroke( 1 ), Color.BLACK ) );
+//        Rectangle2D backFrame = backOffsetTransform.createTransformedShape( viewRect ).getBounds2D();
+//        addChild( new PhetPPath( backFrame, new BasicStroke( 1 ), Color.BLACK ) );
+//        Rectangle2D frontFrame = frontOffsetTransform.createTransformedShape( viewRect ).getBounds2D();
+//        addChild( new PhetPPath( frontFrame, new BasicStroke( 1 ), Color.BLACK ) );
 
 
+    }
+
+    /**
+     * Node that represents a parallelogram with vertical sides.  The x and y
+     * coordinate are the upper left position of a rectan, and the angle is the angle of the upper left corner.
+     */
+//    private static class VerticalSideParallelogramNode extends PNode {
+//        private VerticalSideParallelogramNode( double x, double y, double bottomAndTopLength, double leftAndRightSideLength, double angle ) {
+//            if ( angle >= Math.PI || angle <= 0 ) {
+//                throw new IllegalArgumentException( "Invalid value for angle, value = " + angle );
+//            }
+//
+//            // Create the shape of the parallelogram.
+//            ImmutableVector2D upperLeftCorner;
+//            DoubleGeneralPath path = new DoubleGeneralPath();
+//            if ( angle < Math.PI ) {
+//                upperLeftCorner = new ImmutableVector2D( x, y );
+//            }
+//            else {
+//                upperLeftCorner = new ImmutableVector2D( x, y - Math.cos( angle ) * bottomAndTopLength );
+//            }
+//            ImmutableVector2D upperRightCorner =
+//
+//
+//        }
+//    }
+
+    private static final class VerticallySkewedRectangle extends PNode {
+        private VerticallySkewedRectangle( Rectangle2D rect, double skewAngle ) {
+            ImmutableVector2D upperLeftCorner;
+            if ( skewAngle <= 90 ) {
+                upperLeftCorner = new ImmutableVector2D( rect.getX(), rect.getY() );
+            }
+            else {
+                upperLeftCorner = new ImmutableVector2D( rect.getX(), rect.getY() - rect.getWidth() * Math.cos( skewAngle ) );
+            }
+            ImmutableVector2D upperRightCorner = upperLeftCorner.getAddedInstance( new ImmutableVector2D( 0, -rect.getWidth() ).getRotatedInstance( skewAngle ) );
+            ImmutableVector2D lowerRightCorner = upperRightCorner.getAddedInstance( 0, rect.getHeight() );
+            ImmutableVector2D lowerLeftCorner = lowerRightCorner.getAddedInstance( new ImmutableVector2D( 0, rect.getWidth() ).getRotatedInstance( skewAngle ) );
+            DoubleGeneralPath path = new DoubleGeneralPath( upperLeftCorner );
+            path.lineTo( upperRightCorner );
+//            path.lineTo( lowerRightCorner );
+//            path.lineTo( lowerLeftCorner );
+//            path.closePath();
+            addChild( new PhetPPath( path.getGeneralPath(), new BasicStroke( 2 ), Color.BLACK ) );
+        }
+    }
+
+    private static class BurnerSide extends PNode {
+        private static final Stroke STROKE = new BasicStroke( 3 );
+        private static final Color STROKE_COLOR = Color.BLACK;
+        private static final double PERSPECTIVE_ANGLE = Math.PI / 4; // Positive is counterclockwise, a value of 0 produces a non-skewed rectangle.
+        private static final double TOP_AND_BOTTOM_EDGE_LENGTH = 30;
+
+        private BurnerSide( Point2D topCenter, double height ) {
+            ImmutableVector2D topCenterVector = new ImmutableVector2D( topCenter );
+
+            ImmutableVector2D upperLeftCorner = topCenterVector.getAddedInstance( new ImmutableVector2D( -TOP_AND_BOTTOM_EDGE_LENGTH / 2, 0 ).getRotatedInstance( -PERSPECTIVE_ANGLE ) );
+            ImmutableVector2D lowerLeftCorner = upperLeftCorner.getAddedInstance( new ImmutableVector2D( 0, height ) );
+            ImmutableVector2D lowerRightCorner = lowerLeftCorner.getAddedInstance( new ImmutableVector2D( TOP_AND_BOTTOM_EDGE_LENGTH, 0 ).getRotatedInstance( -PERSPECTIVE_ANGLE ) );
+            ImmutableVector2D upperRightCorner = lowerRightCorner.getAddedInstance( new ImmutableVector2D( 0, -height ) );
+            DoubleGeneralPath path = new DoubleGeneralPath( topCenterVector );
+            path.lineTo( upperLeftCorner );
+            path.lineTo( lowerLeftCorner );
+            path.lineTo( lowerRightCorner );
+            path.lineTo( upperRightCorner );
+            path.closePath();
+            addChild( new PhetPPath( path.getGeneralPath(), new BasicStroke( 2 ), Color.BLACK ) );
+        }
+    }
+
+    /**
+     * Main routine that constructs a PhET Piccolo canvas in a window.
+     *
+     * @param args
+     */
+    public static void main( String[] args ) {
+
+        // Set up the stage and the canvas.
+        Dimension2D stageSize = new PDimension( 500, 400 );
+        PhetPCanvas canvas = new PhetPCanvas();
+
+        // Set up the canvas-screen transform.
+        canvas.setWorldTransformStrategy( new PhetPCanvas.CenteredStage( canvas, stageSize ) );
+
+        ModelViewTransform mvt = ModelViewTransform.createSinglePointScaleInvertedYMapping(
+                new Point2D.Double( 0, 0 ),
+                new Point( (int) Math.round( stageSize.getWidth() * 0.5 ), (int) Math.round( stageSize.getHeight() * 0.50 ) ),
+                1 ); // "Zoom factor" - smaller zooms out, larger zooms in.
+
+        canvas.getLayer().addChild( new BurnerSide( new Point2D.Double( 20, 20 ), 70 ) );
+
+        // Boiler plate app stuff.
+        JFrame frame = new JFrame();
+        frame.setContentPane( canvas );
+        frame.setSize( (int) stageSize.getWidth(), (int) stageSize.getHeight() );
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        frame.setLocationRelativeTo( null ); // Center.
+        frame.setVisible( true );
     }
 }
