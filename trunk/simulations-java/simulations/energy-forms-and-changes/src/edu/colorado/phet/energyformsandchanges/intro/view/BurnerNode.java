e027361e9384bcd017ac96198153e95984cadd9d
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-04-09 20:32:42 +0000

    Removed unused code that was created for prototyping.

--- trunk/simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/intro/view/BurnerNode.java
+++ trunk/simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/intro/view/BurnerNode.java
@@ -28,8 +28,9 @@ import edu.umd.cs.piccolo.util.PDimension;
  */
 public class BurnerNode extends PNode {
 
-    private static final double perspectiveAngle = Math.PI / 4;
-    private static final double edgeLength = 20; // In screen units, which are close to pixels.
+    private static final Stroke BURNER_STAND_STROKE = new BasicStroke( 3 );
+    private static final Color BURNER_STAND_STROKE_COLOR = Color.BLACK;
+    private static final double PERSPECTIVE_ANGLE = Math.PI / 4; // Positive is counterclockwise, a value of 0 produces a non-skewed rectangle.
 
     public BurnerNode( final Burner burner, final ModelViewTransform mvt ) {
         addChild( new PhetPPath( mvt.modelToViewRectangle( burner.getOutlineRect() ), new BasicStroke( 1 ), Color.RED ) );
@@ -48,87 +49,17 @@ public class BurnerNode extends PNode {
         addChild( heaterCoolerNode );
 
         // Add the left and right sides of the stand.
-        addChild( new BurnerSide( new Point2D.Double( burnerViewRect.getX(), burnerViewRect.getY() ), burnerViewRect.getHeight() ) );
-        addChild( new BurnerSide( new Point2D.Double( burnerViewRect.getMaxX(), burnerViewRect.getY() ), burnerViewRect.getHeight() ) );
-
-        //---------------------------------------------------------------------
-        // Create the faux-3D frame for the burner.
-        //---------------------------------------------------------------------
-
-        ImmutableVector2D backOffsetVector = new ImmutableVector2D( -edgeLength / 2, -edgeLength / 2 ).getRotatedInstance( perspectiveAngle );
-        ImmutableVector2D frontOffsetVector = new ImmutableVector2D( edgeLength / 2, edgeLength / 2 ).getRotatedInstance( perspectiveAngle );
-
-        // Draw the left side rectangle.
-        DoubleGeneralPath leftSideRectangle = new DoubleGeneralPath();
-        leftSideRectangle.moveTo( new ImmutableVector2D( burnerViewRect.getMinX(), burnerViewRect.getMinY() ) );
-        leftSideRectangle.lineTo( 0, 0 );
-
-        // Create vectors for the 
-        // Draw the laf
-        Rectangle2D viewRect = mvt.modelToViewRectangle( burner.getOutlineRect() );
-//        Rectangle2D backFrame = backOffsetTransform.createTransformedShape( viewRect ).getBounds2D();
-//        addChild( new PhetPPath( backFrame, new BasicStroke( 1 ), Color.BLACK ) );
-//        Rectangle2D frontFrame = frontOffsetTransform.createTransformedShape( viewRect ).getBounds2D();
-//        addChild( new PhetPPath( frontFrame, new BasicStroke( 1 ), Color.BLACK ) );
-
-
+        addChild( new BurnerStandSide( new Point2D.Double( burnerViewRect.getX(), burnerViewRect.getY() ), burnerViewRect.getHeight() ) );
+        addChild( new BurnerStandSide( new Point2D.Double( burnerViewRect.getMaxX(), burnerViewRect.getY() ), burnerViewRect.getHeight() ) );
     }
 
-    /**
-     * Node that represents a parallelogram with vertical sides.  The x and y
-     * coordinate are the upper left position of a rectan, and the angle is the angle of the upper left corner.
-     */
-//    private static class VerticalSideParallelogramNode extends PNode {
-//        private VerticalSideParallelogramNode( double x, double y, double bottomAndTopLength, double leftAndRightSideLength, double angle ) {
-//            if ( angle >= Math.PI || angle <= 0 ) {
-//                throw new IllegalArgumentException( "Invalid value for angle, value = " + angle );
-//            }
-//
-//            // Create the shape of the parallelogram.
-//            ImmutableVector2D upperLeftCorner;
-//            DoubleGeneralPath path = new DoubleGeneralPath();
-//            if ( angle < Math.PI ) {
-//                upperLeftCorner = new ImmutableVector2D( x, y );
-//            }
-//            else {
-//                upperLeftCorner = new ImmutableVector2D( x, y - Math.cos( angle ) * bottomAndTopLength );
-//            }
-//            ImmutableVector2D upperRightCorner =
-//
-//
-//        }
-//    }
-
-    private static final class VerticallySkewedRectangle extends PNode {
-        private VerticallySkewedRectangle( Rectangle2D rect, double skewAngle ) {
-            ImmutableVector2D upperLeftCorner;
-            if ( skewAngle <= 90 ) {
-                upperLeftCorner = new ImmutableVector2D( rect.getX(), rect.getY() );
-            }
-            else {
-                upperLeftCorner = new ImmutableVector2D( rect.getX(), rect.getY() - rect.getWidth() * Math.cos( skewAngle ) );
-            }
-            ImmutableVector2D upperRightCorner = upperLeftCorner.getAddedInstance( new ImmutableVector2D( 0, -rect.getWidth() ).getRotatedInstance( skewAngle ) );
-            ImmutableVector2D lowerRightCorner = upperRightCorner.getAddedInstance( 0, rect.getHeight() );
-            ImmutableVector2D lowerLeftCorner = lowerRightCorner.getAddedInstance( new ImmutableVector2D( 0, rect.getWidth() ).getRotatedInstance( skewAngle ) );
-            DoubleGeneralPath path = new DoubleGeneralPath( upperLeftCorner );
-            path.lineTo( upperRightCorner );
-//            path.lineTo( lowerRightCorner );
-//            path.lineTo( lowerLeftCorner );
-//            path.closePath();
-            addChild( new PhetPPath( path.getGeneralPath(), new BasicStroke( 2 ), Color.BLACK ) );
-        }
-    }
-
-    private static class BurnerSide extends PNode {
-        private static final Stroke STROKE = new BasicStroke( 3 );
-        private static final Color STROKE_COLOR = Color.BLACK;
+    private static class BurnerStandSide extends PNode {
         private static final double PERSPECTIVE_ANGLE = Math.PI / 4; // Positive is counterclockwise, a value of 0 produces a non-skewed rectangle.
         private static final double TOP_AND_BOTTOM_EDGE_LENGTH = 30;
 
-        private BurnerSide( Point2D topCenter, double height ) {
+        private BurnerStandSide( Point2D topCenter, double height ) {
+            // Draw the side as a parallelogram.
             ImmutableVector2D topCenterVector = new ImmutableVector2D( topCenter );
-
             ImmutableVector2D upperLeftCorner = topCenterVector.getAddedInstance( new ImmutableVector2D( -TOP_AND_BOTTOM_EDGE_LENGTH / 2, 0 ).getRotatedInstance( -PERSPECTIVE_ANGLE ) );
             ImmutableVector2D lowerLeftCorner = upperLeftCorner.getAddedInstance( new ImmutableVector2D( 0, height ) );
             ImmutableVector2D lowerRightCorner = lowerLeftCorner.getAddedInstance( new ImmutableVector2D( TOP_AND_BOTTOM_EDGE_LENGTH, 0 ).getRotatedInstance( -PERSPECTIVE_ANGLE ) );
@@ -139,7 +70,27 @@ public class BurnerNode extends PNode {
             path.lineTo( lowerRightCorner );
             path.lineTo( upperRightCorner );
             path.closePath();
-            addChild( new PhetPPath( path.getGeneralPath(), new BasicStroke( 2 ), Color.BLACK ) );
+            addChild( new PhetPPath( path.getGeneralPath(), BURNER_STAND_STROKE, BURNER_STAND_STROKE_COLOR ) );
+        }
+    }
+
+    private static class BurnerStandTop extends PNode {
+        private static final double TOP_AND_BOTTOM_EDGE_LENGTH = 30;
+
+        private BurnerStandTop( Point2D topCenter, double height ) {
+            // Draw the side as a parallelogram.
+            ImmutableVector2D topCenterVector = new ImmutableVector2D( topCenter );
+            ImmutableVector2D upperLeftCorner = topCenterVector.getAddedInstance( new ImmutableVector2D( -TOP_AND_BOTTOM_EDGE_LENGTH / 2, 0 ).getRotatedInstance( -PERSPECTIVE_ANGLE ) );
+            ImmutableVector2D lowerLeftCorner = upperLeftCorner.getAddedInstance( new ImmutableVector2D( 0, height ) );
+            ImmutableVector2D lowerRightCorner = lowerLeftCorner.getAddedInstance( new ImmutableVector2D( TOP_AND_BOTTOM_EDGE_LENGTH, 0 ).getRotatedInstance( -PERSPECTIVE_ANGLE ) );
+            ImmutableVector2D upperRightCorner = lowerRightCorner.getAddedInstance( new ImmutableVector2D( 0, -height ) );
+            DoubleGeneralPath path = new DoubleGeneralPath( topCenterVector );
+            path.lineTo( upperLeftCorner );
+            path.lineTo( lowerLeftCorner );
+            path.lineTo( lowerRightCorner );
+            path.lineTo( upperRightCorner );
+            path.closePath();
+            addChild( new PhetPPath( path.getGeneralPath(), BURNER_STAND_STROKE, BURNER_STAND_STROKE_COLOR ) );
         }
     }
 
@@ -162,7 +113,7 @@ public class BurnerNode extends PNode {
                 new Point( (int) Math.round( stageSize.getWidth() * 0.5 ), (int) Math.round( stageSize.getHeight() * 0.50 ) ),
                 1 ); // "Zoom factor" - smaller zooms out, larger zooms in.
 
-        canvas.getLayer().addChild( new BurnerSide( new Point2D.Double( 20, 20 ), 70 ) );
+        canvas.getLayer().addChild( new BurnerStandSide( new Point2D.Double( 20, 20 ), 70 ) );
 
         // Boiler plate app stuff.
         JFrame frame = new JFrame();
