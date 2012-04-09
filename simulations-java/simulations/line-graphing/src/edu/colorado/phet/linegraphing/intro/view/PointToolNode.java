18faad77b14b48506816b2ef61e0993a4456f005
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-04-09 16:05:51 +0000

    4/6/12 constrain point tool to stage bounds

--- simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/intro/view/PointToolNode.java
+++ simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/intro/view/PointToolNode.java
@@ -2,6 +2,7 @@
 package edu.colorado.phet.linegraphing.intro.view;
 
 import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 import java.text.MessageFormat;
 import java.text.NumberFormat;
 
@@ -20,7 +21,6 @@ import edu.colorado.phet.common.piccolophet.simsharing.SimSharingDragHandler;
 import edu.colorado.phet.linegraphing.LGResources.Images;
 import edu.colorado.phet.linegraphing.LGSimSharing.ParameterKeys;
 import edu.colorado.phet.linegraphing.LGSimSharing.UserComponents;
-import edu.colorado.phet.linegraphing.intro.model.LineGraph;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.event.PInputEvent;
 import edu.umd.cs.piccolo.nodes.PImage;
@@ -38,7 +38,7 @@ public class PointToolNode extends PhetPNode {
     private static final NumberFormat COORDINATES_FORMAT = new DefaultDecimalFormat( "0" );
     private static final double COORDINATES_Y_CENTER = 28; // center of the display area, measured from the top of the unscaled image file
 
-    public PointToolNode( Property<ImmutableVector2D> point, final ModelViewTransform mvt, LineGraph graph ) {
+    public PointToolNode( Property<ImmutableVector2D> point, final ModelViewTransform mvt, Rectangle2D dragBounds ) {
 
         // tool body
         final PNode bodyNode = new PImage( Images.POINT_TOOL );
@@ -66,7 +66,7 @@ public class PointToolNode extends PhetPNode {
 
         // dragging
         addInputEventListener( new CursorHandler() );
-        addInputEventListener( new PointToolDragHandler( this, point, mvt, graph ) );
+        addInputEventListener( new PointToolDragHandler( this, point, mvt, dragBounds ) );
     }
 
     // Drag handler for the point tool, constrained to the range of the graph, snaps to grid.
@@ -75,30 +75,31 @@ public class PointToolNode extends PhetPNode {
         private final PNode dragNode;
         private final Property<ImmutableVector2D> point;
         private final ModelViewTransform mvt;
-        private final LineGraph graph;
+        private final Rectangle2D dragBounds; // drag bounds, in view coordinate frame
         private double clickXOffset, clickYOffset; // offset of mouse click from dragNode's origin, in parent's coordinate frame
 
-        public PointToolDragHandler( PNode dragNode, Property<ImmutableVector2D> point, ModelViewTransform mvt, LineGraph graph ) {
+        public PointToolDragHandler( PNode dragNode, Property<ImmutableVector2D> point, ModelViewTransform mvt, Rectangle2D dragBounds ) {
             super( UserComponents.pointTool, UserComponentTypes.sprite, true );
             this.dragNode = dragNode;
             this.point = point;
             this.mvt = mvt;
-            this.graph = graph;
+            this.dragBounds = dragBounds;
         }
 
         @Override protected void startDrag( PInputEvent event ) {
             super.startDrag( event );
             Point2D pMouse = event.getPositionRelativeTo( dragNode.getParent() );
-            clickXOffset = pMouse.getX() - mvt.modelToViewDeltaX( point.get().getX() );
-            clickYOffset = pMouse.getY() - mvt.modelToViewDeltaY( point.get().getY() );
+            clickXOffset = pMouse.getX() - mvt.modelToViewX( point.get().getX() );
+            clickYOffset = pMouse.getY() - mvt.modelToViewY( point.get().getY() );
         }
 
         @Override protected void drag( final PInputEvent event ) {
             super.drag( event );
             Point2D pMouse = event.getPositionRelativeTo( dragNode.getParent() );
-            double xModel = mvt.viewToModelDeltaX( pMouse.getX() - clickXOffset );
-            double yModel = mvt.viewToModelDeltaY( pMouse.getY() - clickYOffset );
-            point.set( constrainToGraph( xModel, yModel, graph ) );
+            final double viewX = pMouse.getX() - clickXOffset;
+            final double viewY = pMouse.getY() - clickYOffset;
+            ImmutableVector2D pView = constrainToBounds( viewX, viewY, dragBounds );
+            point.set( mvt.viewToModel( pView ) );
         }
 
         @Override protected void endDrag( PInputEvent event ) {
@@ -114,10 +115,10 @@ public class PointToolNode extends PhetPNode {
                     with( super.getParametersForAllEvents( event ) );
         }
 
-        // Constrains xy coordinates to be within the range of the graph.
-        private static ImmutableVector2D constrainToGraph( double x, double y, LineGraph graph ) {
-            return new ImmutableVector2D( Math.max( graph.minX, Math.min( graph.maxX, x ) ),
-                                          Math.max( graph.minY, Math.min( graph.maxY, y ) ) );
+        // Constrains xy view coordinates to be within some view bounds.
+        private static ImmutableVector2D constrainToBounds( double x, double y, Rectangle2D dragBounds ) {
+            return new ImmutableVector2D( Math.max( dragBounds.getMinX(), Math.min( dragBounds.getMaxX(), x ) ),
+                                          Math.max( dragBounds.getMinY(), Math.min( dragBounds.getMaxY(), y ) ) );
         }
     }
 }
