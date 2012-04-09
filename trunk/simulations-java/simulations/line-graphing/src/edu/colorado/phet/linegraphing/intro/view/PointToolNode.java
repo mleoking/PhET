b744174924ed0c51924864b2fef6bd0df9569c29
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-04-09 17:24:27 +0000

    when point tool is outside grid, don't snap and don't display coordinates

--- trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/intro/view/PointToolNode.java
+++ trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/intro/view/PointToolNode.java
@@ -21,6 +21,7 @@ import edu.colorado.phet.common.piccolophet.simsharing.SimSharingDragHandler;
 import edu.colorado.phet.linegraphing.LGResources.Images;
 import edu.colorado.phet.linegraphing.LGSimSharing.ParameterKeys;
 import edu.colorado.phet.linegraphing.LGSimSharing.UserComponents;
+import edu.colorado.phet.linegraphing.intro.model.LineGraph;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.event.PInputEvent;
 import edu.umd.cs.piccolo.nodes.PImage;
@@ -38,7 +39,13 @@ public class PointToolNode extends PhetPNode {
     private static final NumberFormat COORDINATES_FORMAT = new DefaultDecimalFormat( "0" );
     private static final double COORDINATES_Y_CENTER = 28; // center of the display area, measured from the top of the unscaled image file
 
-    public PointToolNode( Property<ImmutableVector2D> point, final ModelViewTransform mvt, Rectangle2D dragBounds ) {
+    /**
+     * Constructor
+     * @param point the point that is the tool is displaying, determines this node's offset and what it displays
+     * @param mvt model-view transform
+     * @param dragBounds drag bounds, in view coordinate frame
+     */
+    public PointToolNode( Property<ImmutableVector2D> point, final ModelViewTransform mvt, final LineGraph graph, Rectangle2D dragBounds ) {
 
         // tool body
         final PNode bodyNode = new PImage( Images.POINT_TOOL );
@@ -58,7 +65,14 @@ public class PointToolNode extends PhetPNode {
         point.addObserver( new VoidFunction1<ImmutableVector2D>() {
             public void apply( ImmutableVector2D point ) {
                 setOffset( mvt.modelToView( point ).toPoint2D() );
-                coordinatesNode.setText( MessageFormat.format( COORDINATES_PATTERN, COORDINATES_FORMAT.format( point.getX() ), COORDINATES_FORMAT.format( point.getY() ) ) );
+                if ( graph.contains( point ) ) {
+                    coordinatesNode.setText( MessageFormat.format( COORDINATES_PATTERN, COORDINATES_FORMAT.format( point.getX() ), COORDINATES_FORMAT.format( point.getY() ) ) );
+
+                }
+                else {
+                   coordinatesNode.setText( "- -" );
+                }
+                // horizontally centered
                 coordinatesNode.setOffset( bodyNode.getFullBoundsReference().getCenterX() - ( coordinatesNode.getFullBoundsReference().getWidth() / 2 ),
                                            bodyNode.getFullBoundsReference().getMinY() + COORDINATES_Y_CENTER - ( coordinatesNode.getFullBoundsReference().getHeight() / 2 ) );
             }
@@ -66,7 +80,7 @@ public class PointToolNode extends PhetPNode {
 
         // dragging
         addInputEventListener( new CursorHandler() );
-        addInputEventListener( new PointToolDragHandler( this, point, mvt, dragBounds ) );
+        addInputEventListener( new PointToolDragHandler( this, point, mvt, graph, dragBounds ) );
     }
 
     // Drag handler for the point tool, constrained to the range of the graph, snaps to grid.
@@ -75,14 +89,16 @@ public class PointToolNode extends PhetPNode {
         private final PNode dragNode;
         private final Property<ImmutableVector2D> point;
         private final ModelViewTransform mvt;
+        private final LineGraph graph;
         private final Rectangle2D dragBounds; // drag bounds, in view coordinate frame
         private double clickXOffset, clickYOffset; // offset of mouse click from dragNode's origin, in parent's coordinate frame
 
-        public PointToolDragHandler( PNode dragNode, Property<ImmutableVector2D> point, ModelViewTransform mvt, Rectangle2D dragBounds ) {
+        public PointToolDragHandler( PNode dragNode, Property<ImmutableVector2D> point, ModelViewTransform mvt, LineGraph graph, Rectangle2D dragBounds ) {
             super( UserComponents.pointTool, UserComponentTypes.sprite, true );
             this.dragNode = dragNode;
             this.point = point;
             this.mvt = mvt;
+            this.graph = graph;
             this.dragBounds = dragBounds;
         }
 
@@ -98,14 +114,16 @@ public class PointToolNode extends PhetPNode {
             Point2D pMouse = event.getPositionRelativeTo( dragNode.getParent() );
             final double viewX = pMouse.getX() - clickXOffset;
             final double viewY = pMouse.getY() - clickYOffset;
-            ImmutableVector2D pView = constrainToBounds( viewX, viewY, dragBounds );
+            ImmutableVector2D pView = constrainToBounds( viewX, viewY );
             point.set( mvt.viewToModel( pView ) );
         }
 
         @Override protected void endDrag( PInputEvent event ) {
             super.endDrag( event );
-            // snap to the grid
-            point.set( new ImmutableVector2D( MathUtil.round( point.get().getX() ), MathUtil.round( point.get().getY() ) ) );
+            if ( graph.contains( point.get() ) ) {
+                // snap to the grid
+                point.set( new ImmutableVector2D( MathUtil.round( point.get().getX() ), MathUtil.round( point.get().getY() ) ) );
+            }
         }
 
         @Override public ParameterSet getParametersForAllEvents( PInputEvent event ) {
@@ -116,7 +134,7 @@ public class PointToolNode extends PhetPNode {
         }
 
         // Constrains xy view coordinates to be within some view bounds.
-        private static ImmutableVector2D constrainToBounds( double x, double y, Rectangle2D dragBounds ) {
+        private ImmutableVector2D constrainToBounds( double x, double y ) {
             return new ImmutableVector2D( Math.max( dragBounds.getMinX(), Math.min( dragBounds.getMaxX(), x ) ),
                                           Math.max( dragBounds.getMinY(), Math.min( dragBounds.getMaxY(), y ) ) );
         }
