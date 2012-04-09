8223fa2eea9565a92c31f4eca1b2261384a65caa
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-04-09 23:29:23 +0000

    Changed the blocks to be 2D only in model space, and projected by the view into faux-3d.

--- trunk/simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/intro/model/Block.java
+++ trunk/simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/intro/model/Block.java
@@ -4,13 +4,15 @@ package edu.colorado.phet.energyformsandchanges.intro.model;
 import java.awt.Color;
 import java.awt.Shape;
 import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 
-import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
 import edu.colorado.phet.common.phetcommon.simsharing.messages.IUserComponentType;
 import edu.colorado.phet.common.phetcommon.simsharing.messages.UserComponentTypes;
-import edu.colorado.phet.common.phetcommon.view.util.DoubleGeneralPath;
 
 /**
+ * Class that represents a block in the view.  In the model, a block is two-
+ * dimensional, so its shape is represented by a rectangle.
+ *
  * @author John Blanco
  */
 public abstract class Block extends UserMovableModelElement {
@@ -18,15 +20,26 @@ public abstract class Block extends UserMovableModelElement {
     // Height and width of the face, which is square.
     public static final double FACE_SIZE = 0.05; // In meters.
 
-    // Flattened size of the top and visible side.
-    private static final double EDGE_AND_TOP_SIZE = FACE_SIZE * 0.25;
-
     public abstract Color getColor();
 
     public abstract String getLabel();
 
     public void setOnSurface( Point2D surfaceLocation ) {
         // TODO
+    }
+
+    /**
+     * Get a rectangle the defines the current shape in model space.  By
+     * convention for this simulation, the position is the middle of the
+     * bottom of the block's defining rectangle.
+     *
+     * @return
+     */
+    public Rectangle2D getRect() {
+        return new Rectangle2D.Double( position.get().getX() - FACE_SIZE / 2,
+                                       position.get().getY(),
+                                       FACE_SIZE,
+                                       FACE_SIZE );
     }
 
     /**
@@ -38,29 +51,7 @@ public abstract class Block extends UserMovableModelElement {
      * @return
      */
     public static Shape getRawShape() {
-        DoubleGeneralPath path = new DoubleGeneralPath( new Point2D.Double( 0, 0 ) );
-        // Set up variables that will be used throughout the drawing process.
-        ImmutableVector2D upperLeftCornerOfFace = new ImmutableVector2D( 0, FACE_SIZE );
-        ImmutableVector2D upperRightCornerOfFace = new ImmutableVector2D( FACE_SIZE, FACE_SIZE );
-        ImmutableVector2D lowerRightCornerOfFace = new ImmutableVector2D( FACE_SIZE, 0 );
-        // Draw the face.
-        path.lineTo( upperLeftCornerOfFace );
-        path.lineTo( upperRightCornerOfFace );
-        path.lineTo( lowerRightCornerOfFace );
-        // Draw the top.
-        path.moveTo( upperLeftCornerOfFace );
-        path.lineTo( upperLeftCornerOfFace.getAddedInstance( EDGE_AND_TOP_SIZE, EDGE_AND_TOP_SIZE ) );
-        path.lineTo( upperRightCornerOfFace.getAddedInstance( EDGE_AND_TOP_SIZE, EDGE_AND_TOP_SIZE ) );
-        path.lineTo( upperRightCornerOfFace );
-        path.lineTo( upperLeftCornerOfFace );
-        // Draw the side.
-        path.moveTo( upperRightCornerOfFace );
-        path.lineTo( upperRightCornerOfFace.getAddedInstance( EDGE_AND_TOP_SIZE, EDGE_AND_TOP_SIZE ) );
-        path.lineTo( lowerRightCornerOfFace.getAddedInstance( EDGE_AND_TOP_SIZE, EDGE_AND_TOP_SIZE ) );
-        path.lineTo( lowerRightCornerOfFace );
-        path.lineTo( upperRightCornerOfFace );
-
-        return path.getGeneralPath();
+        return new Rectangle2D.Double( -FACE_SIZE / 2, 0, FACE_SIZE, FACE_SIZE );
     }
 
     @Override public IUserComponentType getUserComponentType() {
--- trunk/simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/intro/view/BlockNode.java
+++ trunk/simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/intro/view/BlockNode.java
@@ -4,11 +4,15 @@ package edu.colorado.phet.energyformsandchanges.intro.view;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.Stroke;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 
+import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
 import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
+import edu.colorado.phet.common.phetcommon.view.util.DoubleGeneralPath;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.event.CursorHandler;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
@@ -26,6 +30,12 @@ import edu.umd.cs.piccolo.nodes.PText;
 public class BlockNode extends PNode {
 
     private static final Font LABEL_FONT = new PhetFont( 32, false );
+    private static final Stroke OUTLINE_STROKE = new BasicStroke( 3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL );
+    private static final Color OUTLINE_STROKE_COLOR = Color.DARK_GRAY;
+    private static final double PERSPECTIVE_ANGLE = Math.PI / 4; // Positive is counterclockwise, a value of 0 produces a non-skewed rectangle.
+    private static final double PERSPECTIVE_EDGE_PROPORTION = 0.33;
+
+    private static final boolean SHOW_2D_REPRESENTATION = true;
 
     public BlockNode( Block block, final ModelViewTransform mvt ) {
 
@@ -33,8 +43,40 @@ public class BlockNode extends PNode {
         // shape from the position of the block.
         AffineTransform scaleTransform = AffineTransform.getScaleInstance( mvt.getTransform().getScaleX(), mvt.getTransform().getScaleY() );
 
-        // Add the representation of the block.
-        addChild( new PhetPPath( scaleTransform.createTransformedShape( Block.getRawShape() ), block.getColor(), new BasicStroke( 2 ), Color.LIGHT_GRAY ) );
+        // Add the representation of the block.  This is projected into a 3D
+        // appearance based on the size and position of the 2D block in model
+        // space.  It is projected and offset both forward and backward so that
+        // the model position is in the center of the 3D shape.
+        double perspectiveEdgeSize = mvt.modelToViewDeltaX( block.getRect().getWidth() * PERSPECTIVE_EDGE_PROPORTION );
+        ImmutableVector2D forwardPerspectiveOffset = new ImmutableVector2D( -perspectiveEdgeSize / 2, 0 ).getRotatedInstance( -PERSPECTIVE_ANGLE );
+        ImmutableVector2D backwardPerspectiveOffset = new ImmutableVector2D( perspectiveEdgeSize / 2, 0 ).getRotatedInstance( -PERSPECTIVE_ANGLE );
+        Rectangle2D blockRectInViewCoords = scaleTransform.createTransformedShape( Block.getRawShape() ).getBounds2D();
+        ImmutableVector2D lowerLeftCornerOfFace = new ImmutableVector2D( blockRectInViewCoords.getMinX(), blockRectInViewCoords.getMaxY() ).getAddedInstance( forwardPerspectiveOffset );
+        ImmutableVector2D lowerRightCornerOfFace = new ImmutableVector2D( blockRectInViewCoords.getMaxX(), blockRectInViewCoords.getMaxY() ).getAddedInstance( forwardPerspectiveOffset );
+        ImmutableVector2D upperRightCornerOfFace = new ImmutableVector2D( blockRectInViewCoords.getMaxX(), blockRectInViewCoords.getMinY() ).getAddedInstance( forwardPerspectiveOffset );
+        ImmutableVector2D upperLeftCornerOfFace = new ImmutableVector2D( blockRectInViewCoords.getMinX(), blockRectInViewCoords.getMinY() ).getAddedInstance( forwardPerspectiveOffset );
+        ImmutableVector2D upperBackLeftCorner = new ImmutableVector2D( blockRectInViewCoords.getMinX(), blockRectInViewCoords.getMinY() ).getAddedInstance( backwardPerspectiveOffset );
+        ImmutableVector2D upperBackRightCorner = new ImmutableVector2D( blockRectInViewCoords.getMaxX(), blockRectInViewCoords.getMinY() ).getAddedInstance( backwardPerspectiveOffset );
+        ImmutableVector2D lowerBackRightCorner = new ImmutableVector2D( blockRectInViewCoords.getMaxX(), blockRectInViewCoords.getMaxY() ).getAddedInstance( backwardPerspectiveOffset );
+        DoubleGeneralPath path = new DoubleGeneralPath();
+        path.moveTo( lowerLeftCornerOfFace );
+        path.lineTo( lowerRightCornerOfFace );
+        path.lineTo( upperRightCornerOfFace );
+        path.lineTo( upperLeftCornerOfFace );
+        path.lineTo( lowerLeftCornerOfFace );
+        path.moveTo( upperLeftCornerOfFace );
+        path.lineTo( upperBackLeftCorner );
+        path.lineTo( upperBackRightCorner );
+        path.lineTo( upperRightCornerOfFace );
+        path.moveTo( upperBackRightCorner );
+        path.lineTo( lowerBackRightCorner );
+        path.lineTo( lowerRightCornerOfFace );
+        path.lineTo( upperRightCornerOfFace );
+        addChild( new PhetPPath( path.getGeneralPath(), block.getColor(), OUTLINE_STROKE, OUTLINE_STROKE_COLOR ) );
+
+        if ( SHOW_2D_REPRESENTATION ) {
+            addChild( new PhetPPath( scaleTransform.createTransformedShape( Block.getRawShape() ), new BasicStroke( 1 ), Color.RED ) );
+        }
 
         // Update the offset if and when the model position changes.
         block.position.addObserver( new VoidFunction1<Point2D>() {
@@ -43,19 +85,18 @@ public class BlockNode extends PNode {
             }
         } );
 
-        // Add the label.
-        PNode label = new PText( block.getLabel() ) {{
-            setFont( LABEL_FONT );
-            if ( getFullBoundsReference().width >= mvt.modelToViewDeltaX( Block.FACE_SIZE * 0.9 ) ) {
-                // Scale the label to fit on the face of the block.
-                double scale = ( mvt.modelToViewDeltaX( Block.FACE_SIZE * 0.9 ) / getFullBoundsReference().width );
-                setScale( scale );
-            }
-            double centerX = mvt.modelToViewDeltaX( Block.FACE_SIZE / 2 );
-            double centerY = mvt.modelToViewDeltaY( Block.FACE_SIZE * 0.75 );
-            centerFullBoundsOnPoint( centerX, centerY );
+        // Add and position the label.
+        PText label = new PText( block.getLabel() );
+        label.setFont( LABEL_FONT );
+        if ( label.getFullBoundsReference().width >= mvt.modelToViewDeltaX( Block.FACE_SIZE * 0.9 ) ) {
+            // Scale the label to fit on the face of the block.
+            double scale = ( mvt.modelToViewDeltaX( Block.FACE_SIZE * 0.9 ) / getFullBoundsReference().width );
+            label.setScale( scale );
+        }
+        double labelCenterX = ( upperLeftCornerOfFace.getX() + upperRightCornerOfFace.getX() ) / 2;
+        double labelCenterY = ( upperLeftCornerOfFace.getY() + label.getFullBoundsReference().height );
+        label.centerFullBoundsOnPoint( labelCenterX, labelCenterY );
 
-        }};
         addChild( label );
 
 
