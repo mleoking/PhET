73e572ef6a1ef009ebf7dad249a106aa410a9e81
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-04-11 23:28:58 +0000

    Added label to water.

--- simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/intro/view/BeakerNode.java
+++ simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/intro/view/BeakerNode.java
@@ -3,15 +3,18 @@ package edu.colorado.phet.energyformsandchanges.intro.view;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Stroke;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
 import java.awt.geom.Rectangle2D;
 
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
+import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.colorado.phet.energyformsandchanges.intro.model.Beaker;
 import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.nodes.PText;
 
 /**
  * Piccolo node that represents a beaker in the view.
@@ -24,7 +27,7 @@ public class BeakerNode extends PNode {
     private static final Color OUTLINE_COLOR = Color.LIGHT_GRAY;
     private static final Color FLUID_COLOR = new Color( 220, 180, 180 );
     private static final double PERSPECTIVE_PROPORTION = 0.2;
-
+    private static final Font LABEL_FONT = new PhetFont( 32, false );
     private static final boolean SHOW_MODEL_RECT = true;
 
     public BeakerNode( final Beaker beaker, final ModelViewTransform mvt ) {
@@ -49,6 +52,12 @@ public class BeakerNode extends PNode {
         addChild( new PhetPPath( new Ellipse2D.Double( beakerViewRect.getMinX(), beakerViewRect.getMinY() - ellipseHeight / 2, beakerViewRect.getWidth(), ellipseHeight ),
                                  OUTLINE_STROKE,
                                  OUTLINE_COLOR ) );
+
+        // Add the label.
+        PText label = new PText( "Water" );
+        label.setFont( LABEL_FONT );
+        label.centerFullBoundsOnPoint( beakerViewRect.getCenterX(), beakerViewRect.getMinY() + label.getFullBoundsReference().height * 2 );
+        addChild( label );
 
         // If enabled, show the outline of the rectangle that represents the
         // beaker's position in the model.
