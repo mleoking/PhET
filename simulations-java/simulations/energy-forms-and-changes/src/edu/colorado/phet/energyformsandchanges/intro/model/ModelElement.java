96eb1f7819eb6814b6789539d453354c5886f700
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-04-11 18:55:00 +0000

    Added a base class for all model elements that can be moved by the user or that can support other movable elements.

---
+++ simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/intro/model/ModelElement.java
@@ -0,0 +1,11 @@
+// Copyright 2002-2012, University of Colorado
+package edu.colorado.phet.energyformsandchanges.intro.model;
+
+/**
+ * Base class for all model elements in this module.  At the time of this
+ * writing, this includes blocks, beakers, burners, and thermometers.
+ *
+ * @author John Blanco
+ */
+public class ModelElement {
+}
