4bb3cfdc7de2968e1141f99c7c0da47449c8f464
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-04-12 18:35:49 +0000

    Made the beaker movable.

--- trunk/simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/intro/model/Beaker.java
+++ trunk/simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/intro/model/Beaker.java
@@ -1,18 +1,21 @@
 // Copyright 2002-2012, University of Colorado
 package edu.colorado.phet.energyformsandchanges.intro.model;
 
-import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 
+import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
 import edu.colorado.phet.common.phetcommon.model.property.Property;
+import edu.colorado.phet.common.phetcommon.simsharing.messages.IUserComponent;
+import edu.colorado.phet.common.phetcommon.simsharing.messages.IUserComponentType;
 import edu.colorado.phet.common.phetcommon.util.DoubleRange;
+import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
 
 /**
  * Model element that represents a beaker in the simulation.
  *
  * @author John Blanco
  */
-public class Beaker extends ModelElement {
+public class Beaker extends UserMovableModelElement {
 
     //-------------------------------------------------------------------------
     // Class Data
@@ -25,13 +28,14 @@ public class Beaker extends ModelElement {
     // Instance Data
     //-------------------------------------------------------------------------
 
-    private final Point2D position = new Point2D.Double( 0, 0 );
-
     // Property that is used to control the amount of fluid in the beaker.
     public final Property<Double> fluidLevel = new Property<Double>( 0.3 );
 
     // Surface upon which any model elements will sit.
     private final Property<HorizontalSurface> topSurface;
+
+    // Bottom of this model element, used when setting it on something.
+    private Property<HorizontalSurface> bottomSurface;
 
     //-------------------------------------------------------------------------
     // Constructor(s)
@@ -40,13 +44,27 @@ public class Beaker extends ModelElement {
     /**
      * Constructor.
      *
-     * @param position The position in model space where this burner exists.
-     *                 By convention for this simulation, the position is
-     *                 defined as the bottom center of the model element.
+     * @param initialPosition The initial position in model space.  This is
+     *                        the center bottom of the model element.
      */
-    public Beaker( Point2D position ) {
-        this.position.setLocation( position );
+    public Beaker( ImmutableVector2D initialPosition ) {
+
+        this.position.set( initialPosition );
+
+        // Create the top surface.  Since the beaker has objects rest at the
+        // bottom inside the beaker, the top surface is the bottom of the
+        // defining rectangle.
         topSurface = new Property<HorizontalSurface>( new HorizontalSurface( new DoubleRange( getOutlineRect().getMinX(), getOutlineRect().getMaxX() ), getOutlineRect().getMinY(), this ) );
+
+        // For the beaker, the top and bottom surfaces are the same.
+        bottomSurface = topSurface;
+
+        // Update the top and bottom surfaces whenever the position changes.
+        position.addObserver( new VoidFunction1<ImmutableVector2D>() {
+            public void apply( final ImmutableVector2D immutableVector2D ) {
+                updateSurfaces();
+            }
+        } );
     }
 
     //-------------------------------------------------------------------------
@@ -60,13 +78,31 @@ public class Beaker extends ModelElement {
      * @return
      */
     public Rectangle2D getOutlineRect() {
-        return new Rectangle2D.Double( position.getX() - WIDTH / 2,
-                                       position.getY(),
+        return new Rectangle2D.Double( position.get().getX() - WIDTH / 2,
+                                       position.get().getY(),
                                        WIDTH,
                                        HEIGHT );
     }
 
+    private void updateSurfaces() {
+        topSurface.set( new HorizontalSurface( new DoubleRange( getOutlineRect().getMinX(), getOutlineRect().getMaxX() ),
+                                               getOutlineRect().getMaxY(),
+                                               this ) );
+    }
+
     @Override public Property<HorizontalSurface> getTopSurfaceProperty() {
         return topSurface;
+    }
+
+    @Override public Property<HorizontalSurface> getBottomSurfaceProperty() {
+        return bottomSurface;
+    }
+
+    @Override public IUserComponent getUserComponent() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override public IUserComponentType getUserComponentType() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
     }
 }
--- trunk/simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/intro/view/BeakerNode.java
+++ trunk/simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/intro/view/BeakerNode.java
@@ -5,12 +5,16 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Stroke;
+import java.awt.geom.AffineTransform;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
 import java.awt.geom.Rectangle2D;
 
+import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
+import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
+import edu.colorado.phet.common.piccolophet.event.CursorHandler;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.colorado.phet.energyformsandchanges.intro.model.Beaker;
 import edu.umd.cs.piccolo.PNode;
@@ -25,16 +29,19 @@ public class BeakerNode extends PNode {
 
     private static final Stroke OUTLINE_STROKE = new BasicStroke( 3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL );
     private static final Color OUTLINE_COLOR = Color.LIGHT_GRAY;
-    private static final Color FLUID_COLOR = new Color( 175, 238, 238, 200 );
     private static final double PERSPECTIVE_PROPORTION = 0.2;
     private static final Font LABEL_FONT = new PhetFont( 32, false );
     private static final boolean SHOW_MODEL_RECT = true;
 
     public BeakerNode( final Beaker beaker, final ModelViewTransform mvt ) {
 
+        // Extract the scale transform from the MVT so that we can separate the
+        // shape from the position.
+        AffineTransform scaleTransform = AffineTransform.getScaleInstance( mvt.getTransform().getScaleX(), mvt.getTransform().getScaleY() );
+
         // Get a version of the rectangle that defines the beaker size and
         // location in the view.
-        final Rectangle2D beakerViewRect = mvt.modelToView( beaker.getOutlineRect() ).getBounds2D();
+        final Rectangle2D beakerViewRect = scaleTransform.createTransformedShape( beaker.getOutlineRect() ).getBounds2D();
 
         // Add the left and right sides of the beaker.
         addChild( new PhetPPath( new Line2D.Double( beakerViewRect.getMinX(), beakerViewRect.getMinY(), beakerViewRect.getMinX(), beakerViewRect.getMaxY() ),
@@ -54,7 +61,7 @@ public class BeakerNode extends PNode {
                                  OUTLINE_COLOR ) );
 
         // Add the water TODO this is temp, since size will need to change.
-        addChild( new PhetPPath( beakerViewRect, FLUID_COLOR ) );
+        addChild( new PerspectiveWaterNode() );
 
         // Add the label.
         PText label = new PText( "Water" );
@@ -65,7 +72,30 @@ public class BeakerNode extends PNode {
         // If enabled, show the outline of the rectangle that represents the
         // beaker's position in the model.
         if ( SHOW_MODEL_RECT ) {
-            addChild( new PhetPPath( mvt.modelToViewRectangle( beaker.getOutlineRect() ), new BasicStroke( 1 ), Color.RED ) );
+            addChild( new PhetPPath( beakerViewRect, new BasicStroke( 1 ), Color.RED ) );
+        }
+
+        // Update the offset if and when the model position changes.
+        beaker.position.addObserver( new VoidFunction1<ImmutableVector2D>() {
+            public void apply( ImmutableVector2D position ) {
+                setOffset( mvt.modelToView( position ).toPoint2D() );
+            }
+        } );
+
+        // Add the cursor handler.
+        addInputEventListener( new CursorHandler( CursorHandler.HAND ) );
+
+        // Add the drag handler.
+        addInputEventListener( new MovableElementDragHandler( beaker, this, mvt ) );
+    }
+
+
+    private static class PerspectiveWaterNode extends PNode {
+        private static final Color WATER_COLOR = new Color( 175, 238, 238, 200 );
+
+        private PerspectiveWaterNode() {
+
         }
     }
+
 }
