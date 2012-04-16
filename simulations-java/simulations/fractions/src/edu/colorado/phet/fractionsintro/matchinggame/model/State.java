65944f1b6f4c177068d42c1206acecd8e124b271
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-04-16 19:57:12 +0000

    After getting it wrong, hide the "check answer" button until they remove something from the platform or put something else up there.

--- simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/matchinggame/model/State.java
+++ simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/matchinggame/model/State.java
@@ -5,4 +5,7 @@ package edu.colorado.phet.fractionsintro.matchinggame.model;
  */
 public enum State {
     WAITING_FOR_USER_TO_CHECK_ANSWER, RIGHT, SHOWING_WHY_ANSWER_WRONG, SHOWING_CORRECT_ANSWER,
+
+    //After getting it wrong, hide the "check answer" button until they remove something from the platform or put something else up there.
+    WAITING_FOR_USER_TO_CHANGE_ANSWER,
 }
