17a9fad8d26b0734b22c8dbed179d6b76e2c45de
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-04-18 06:31:46 +0000

    factored out test

--- simulations-java/simulations/fractions/functionalscenegraph-src/edu/colorado/phet/functionalscenegraph/SCanvas.java
+++ simulations-java/simulations/fractions/functionalscenegraph-src/edu/colorado/phet/functionalscenegraph/SCanvas.java
@@ -1,12 +1,8 @@
 package edu.colorado.phet.functionalscenegraph;
 
-import fj.Effect;
 import fj.data.Option;
-import lombok.Data;
 
-import java.awt.Color;
 import java.awt.Cursor;
-import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -14,20 +10,13 @@ import java.awt.RenderingHints;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
-import java.awt.geom.Ellipse2D;
 
 import javax.swing.JComponent;
-import javax.swing.JFrame;
-import javax.swing.WindowConstants;
 
-import edu.colorado.phet.common.phetcommon.math.ImmutableRectangle2D;
 import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
-import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.fractions.util.immutable.Vector2D;
-
-import static edu.colorado.phet.functionalscenegraph.DrawText.textNode;
 
 /**
  * @author Sam Reid
@@ -96,55 +85,5 @@ public class SCanvas extends JComponent {
         g2.setRenderingHint( RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON );
         this.child.get().render( new Graphics2DContext( g2 ) );
         g.setFont( orig );
-    }
-
-    public static void main( String[] args ) {
-        new JFrame( "Test" ) {{
-            final Property<State> model = new Property<State>( new State( new Vector2D( 50, 200 ) ) );
-            model.trace( "model changed" );
-            final SNode root = createRoot( model );
-            setContentPane( new SCanvas( root ) {{
-                setPreferredSize( new Dimension( 800, 600 ) );
-
-                //When the model updates, create a new node
-                model.addObserver( new VoidFunction1<State>() {
-                    @Override public void apply( final State state ) {
-                        child.set( createRoot( model ) );
-                        repaint();
-                    }
-                } );
-
-            }} );
-            pack();
-            setDefaultCloseOperation( WindowConstants.EXIT_ON_CLOSE );
-        }}.setVisible( true );
-    }
-
-    //look up corresponding model element by ID?
-    //Better to provide different methods to dispatch to.  Should be attached to the node.
-    public static @Data class State {
-        public final Vector2D circlePosition;
-
-        public State withCirclePosition( final Vector2D circlePosition ) { return new State( circlePosition ); }
-    }
-
-    private static SNode createRoot( final Property<State> model ) {
-        final SNode text = textNode( "Hello", new PhetFont( 30, true ), Color.blue ).translate( 100, 100 );
-        ImmutableRectangle2D bounds = text.getBounds();
-        System.out.println( "bounds = " + bounds );
-
-        DrawShape shape = new DrawShape( bounds );
-        SNode fillShape = new FillShape( bounds ).withPaint( Color.white );
-
-        final SNode ellipse = new FillShape( new Ellipse2D.Double( 0, 0, 100, 100 ) ).translate( model.get().circlePosition ).
-                withDragEvent( new Effect<Vector2D>() {
-                    @Override public void e( final Vector2D vector2D ) {
-                        model.set( model.get().withCirclePosition( model.get().circlePosition.plus( vector2D ) ) );
-                    }
-                } );
-        return new SList( fillShape, shape, text, ellipse );
-
-        //Deltas not getting transformed yet
-//        .scale( 2 );
     }
 }
