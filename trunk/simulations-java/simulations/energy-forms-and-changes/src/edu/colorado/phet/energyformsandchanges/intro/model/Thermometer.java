ea3ab65c722798db322196fb6ce7372aa78be303
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-04-27 21:24:35 +0000

    Changed thermometer model to only have a point position.  View will handle all other aspects of representation.

--- trunk/simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/intro/model/Thermometer.java
+++ trunk/simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/intro/model/Thermometer.java
@@ -1,9 +1,6 @@
 // Copyright 2002-2012, University of Colorado
 package edu.colorado.phet.energyformsandchanges.intro.model;
 
-import java.awt.Shape;
-import java.awt.geom.Rectangle2D;
-
 import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
 import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.phetcommon.simsharing.messages.IUserComponent;
@@ -11,44 +8,16 @@ import edu.colorado.phet.common.phetcommon.simsharing.messages.IUserComponentTyp
 import edu.colorado.phet.common.phetcommon.simsharing.messages.UserComponentTypes;
 
 /**
- * Class that represents a thermometer in the model.
+ * Class that represents a thermometer in the model.  The thermometer has
+ * only a point and a temperature in the model.  Its visual representation is
+ * left entirely to the view.
  *
  * @author John Blanco
  */
 public class Thermometer extends UserMovableModelElement {
 
-    // Height and width of the face, which is square.
-    public static final double WIDTH = 0.02; // In meters.
-    public static final double HEIGHT = WIDTH * 3;
-
     public Thermometer( ImmutableVector2D initialPosition ) {
         position.set( initialPosition );
-    }
-
-    /**
-     * Get a rectangle the defines the current shape in model space.  By
-     * convention for this simulation, the position is the middle of the
-     * bottom of the block's defining rectangle.
-     *
-     * @return
-     */
-    public Rectangle2D getRect() {
-        return new Rectangle2D.Double( position.get().getX() - WIDTH / 2,
-                                       position.get().getY(),
-                                       WIDTH,
-                                       HEIGHT );
-    }
-
-    /**
-     * Get the "raw shape" that should be used for depicting this block in the
-     * view.  In this context, "raw" means that it is untranslated.  By
-     * convention for this simulation, the point (0, 0) is the bottom center
-     * of the block.
-     *
-     * @return
-     */
-    public static Shape getRawShape() {
-        return new Rectangle2D.Double();
     }
 
     @Override public IUserComponent getUserComponent() {
