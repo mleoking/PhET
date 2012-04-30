9534b067bff119cfa66b1b6fa0f1edc5a026ff68
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-04-30 23:43:44 +0000

    Added developer menu items for the dirt color

--- simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/FluidPressureAndFlowApplication.java
+++ simulations-java/simulations/fluid-pressure-and-flow/src/edu/colorado/phet/fluidpressureandflow/FluidPressureAndFlowApplication.java
@@ -1,8 +1,17 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.fluidpressureandflow;
 
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JMenuItem;
+
 import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
 import edu.colorado.phet.common.phetcommon.application.PhetApplicationLauncher;
+import edu.colorado.phet.common.phetcommon.dialogs.ColorChooserFactory;
+import edu.colorado.phet.common.phetcommon.dialogs.ColorChooserFactory.Listener;
+import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.piccolophet.PiccoloPhetApplication;
 import edu.colorado.phet.fluidpressureandflow.flow.FluidFlowModule;
 import edu.colorado.phet.fluidpressureandflow.pressure.FluidPressureModule;
@@ -14,9 +23,49 @@ import edu.colorado.phet.fluidpressureandflow.watertower.WaterTowerModule;
  * @author Sam Reid
  */
 public class FluidPressureAndFlowApplication extends PiccoloPhetApplication {
+    public static final Property<Color> dirtTopColor = new Property<Color>( new Color( 157, 139, 97 ) );
+    public static final Property<Color> dirtBottomColor = new Property<Color>( new Color( 100, 90, 60 ) );
 
     public FluidPressureAndFlowApplication( PhetApplicationConfig config ) {
         super( config );
+        getPhetFrame().getDeveloperMenu().add( new JMenuItem( "Dirt top color" ) {{
+            addActionListener( new ActionListener() {
+                @Override public void actionPerformed( final ActionEvent e ) {
+                    ColorChooserFactory.showDialog( "Dirt top color", getPhetFrame(), dirtTopColor.get(), new Listener() {
+                        @Override public void colorChanged( final Color color ) {
+                            dirtTopColor.set( color );
+                        }
+
+                        @Override public void ok( final Color color ) {
+                            dirtTopColor.set( color );
+                        }
+
+                        @Override public void cancelled( final Color originalColor ) {
+                            dirtTopColor.set( originalColor );
+                        }
+                    }, true );
+                }
+            } );
+        }} );
+        getPhetFrame().getDeveloperMenu().add( new JMenuItem( "Dirt bottom color" ) {{
+            addActionListener( new ActionListener() {
+                @Override public void actionPerformed( final ActionEvent e ) {
+                    ColorChooserFactory.showDialog( "Dirt bottom color", getPhetFrame(), dirtBottomColor.get(), new Listener() {
+                        @Override public void colorChanged( final Color color ) {
+                            dirtBottomColor.set( color );
+                        }
+
+                        @Override public void ok( final Color color ) {
+                            dirtBottomColor.set( color );
+                        }
+
+                        @Override public void cancelled( final Color originalColor ) {
+                            dirtBottomColor.set( originalColor );
+                        }
+                    }, true );
+                }
+            } );
+        }} );
         addModule( new FluidPressureModule() );
         addModule( new FluidFlowModule() );
         addModule( new WaterTowerModule() );
