8c39a114e00ee0804a523623c3cae2de7c615afb
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-05-01 20:50:21 +0000

    Moved the drag handler that was in ThermometerNode to be a top-level class.

---
+++ simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/intro/view/DragHandler.java
@@ -0,0 +1,43 @@
+// Copyright 2002-2012, University of Colorado
+package edu.colorado.phet.energyformsandchanges.intro.view;
+
+import java.awt.geom.Point2D;
+
+import edu.colorado.phet.common.phetcommon.util.function.Function1;
+import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
+import edu.colorado.phet.common.piccolophet.event.RelativeDragHandler;
+import edu.colorado.phet.energyformsandchanges.intro.model.UserMovableModelElement;
+import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.event.PInputEvent;
+
+/**
+ * Drag handler for objects that can be moved by the user.  This is constructed
+ * with a constraint function that defines where the model object can go.
+ */
+class DragHandler extends RelativeDragHandler {
+
+    private final UserMovableModelElement modelElement;
+
+    /**
+     * Constructor.  The node must be property positioned before calling
+     * this, or it won't work correctly.
+     *
+     * @param modelElement
+     * @param node
+     * @param mvt
+     */
+    public DragHandler( UserMovableModelElement modelElement, PNode node, ModelViewTransform mvt, Function1<Point2D, Point2D> constraint ) {
+        super( node, mvt, modelElement.position, constraint );
+        this.modelElement = modelElement;
+    }
+
+    @Override public void mousePressed( PInputEvent event ) {
+        super.mousePressed( event );
+        modelElement.userControlled.set( true );
+    }
+
+    @Override public void mouseReleased( PInputEvent event ) {
+        super.mouseReleased( event );
+        modelElement.userControlled.set( false );
+    }
+}
