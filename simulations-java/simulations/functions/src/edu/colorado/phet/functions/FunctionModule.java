6e70f1316efd8b36c44b214b2c78276ea42c567d
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-05-02 00:50:53 +0000

    changed background color for mockups

--- simulations-java/simulations/functions/src/edu/colorado/phet/functions/FunctionModule.java
+++ simulations-java/simulations/functions/src/edu/colorado/phet/functions/FunctionModule.java
@@ -1,6 +1,8 @@
 // Copyright 2002-2012, University of Colorado
 package edu.colorado.phet.functions;
 
+import java.awt.Color;
+
 import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
 import edu.colorado.phet.common.piccolophet.PhetPCanvas;
 import edu.colorado.phet.common.piccolophet.PiccoloModule;
@@ -9,9 +11,14 @@ import edu.colorado.phet.common.piccolophet.PiccoloModule;
  * @author Sam Reid
  */
 public class FunctionModule extends PiccoloModule {
+
+    private static final Color CREAM = new Color( 247, 243, 183 );
+
     public FunctionModule( final String name ) {
         super( name, new ConstantDtClock() );
-        setSimulationPanel( new PhetPCanvas() );
+        setSimulationPanel( new PhetPCanvas() {{
+            setBackground( CREAM );
+        }} );
         setClockControlPanel( null );
     }
 }
