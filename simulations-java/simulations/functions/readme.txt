f312ee63817d47757d352190a5d4b28ef21e94d0
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-05-02 00:45:42 +0000

    Functions: Added stub project for generating templates and mockups

---
+++ simulations-java/simulations/functions/readme.txt
@@ -0,0 +1,6 @@
+Right now this is just a stub project to help generate some mock-ups.
+Design is being written here:
+https://docs.google.com/document/d/1rc_pvZYnUtQAhrWWgCEPKzIETzsx6VxJBCTZurj6n2k/edit
+
+Sam Reid
+5-1-2012
---
+++ simulations-java/simulations/functions/src/edu/colorado/phet/functions/FunctionsApplication.java
@@ -0,0 +1,28 @@
+// Copyright 2002-2011, University of Colorado
+package edu.colorado.phet.functions;
+
+import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
+import edu.colorado.phet.common.phetcommon.application.PhetApplicationLauncher;
+import edu.colorado.phet.common.piccolophet.PiccoloPhetApplication;
+
+/**
+ * "Functions" PhET Application
+ * <p/>
+ * Right now this is just a stub project to help generate some mock-ups
+ *
+ * @author Sam Reid
+ */
+public class FunctionsApplication extends PiccoloPhetApplication {
+
+    public FunctionsApplication( PhetApplicationConfig config ) {
+        super( config );
+        addModule( new FunctionModule( "Intro" ) );
+        addModule( new FunctionModule( "Numbers" ) );
+        addModule( new FunctionModule( "Function Maker" ) );
+        addModule( new FunctionModule( "Game Maker" ) );
+    }
+
+    public static void main( String[] args ) {
+        new PhetApplicationLauncher().launchSim( args, "functions", "functions", FunctionsApplication.class );
+    }
+}
