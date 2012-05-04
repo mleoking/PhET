9298bc44999479558c4ea17412f2a7734dc55fd4
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-05-04 18:16:25 +0000

    RPAL: added outputs for games

--- util/sim-event-data-collection-analysis/src/edu/colorado/phet/simsharinganalysis/scripts/rpal2012/RPALAnalysisGameSummary.scala
+++ util/sim-event-data-collection-analysis/src/edu/colorado/phet/simsharinganalysis/scripts/rpal2012/RPALAnalysisGameSummary.scala
@@ -12,27 +12,43 @@ import edu.colorado.phet.simsharinganalysis.{phet, Log}
 class GameReport(result: GameResult) {
   lazy val undefinedString = if ( result.finished ) "undefined (finished)" else "undefined (aborted)"
 
-  override def toString = result.level + "\t" + result.hiding + "\t" + ( 0 to 5 ).map(i => if ( result.checks.isDefinedAt(i) ) result.checks(i) else undefinedString).mkString("\t")
+  override def toString = {
+    result.level + "\t" + result.hiding + "\t" + ( 0 until 5 ).map(i => if ( result.points.isDefinedAt(i) ) result.points(i) else undefinedString).mkString("\t")
+  }
 }
 
 class IndividualReport(log: Log) {
   val report = RPALAnalysis toReport log
   val gameReports = report.gameResults.map(r => new GameReport(r))
 
-  override def toString = log.session + "\t" + gameReports.mkString("\t")
+  override def toString = log.file.getName + "\t" + gameReports.mkString("\t")
 }
 
 class GameSummaryReport(name: String, logs: List[Log]) {
   lazy val individualReports = logs.map(log => new IndividualReport(log))
 
-  override def toString =
-    "name\n" + individualReports.mkString("\n")
+  def columns(gameIndex: Int) = "Game " + gameIndex + ": Level\t" +
+                                "Game " + gameIndex + ": Hidden\t" +
+                                "Game " + gameIndex + ": Question 1\t" +
+                                "Game " + gameIndex + ": Question 2\t" +
+                                "Game " + gameIndex + ": Question 3\t" +
+                                "Game " + gameIndex + ": Question 4\t" +
+                                "Game " + gameIndex + ": Question 5"
+
+  override def toString = "File\t" + ( 1 to 7 ).map(i => columns(i)).mkString("\t") + "\n" + individualReports.mkString("\n")
 }
 
 object RPALAnalysisGameSummary {
 
   def main(args: Array[String]) {
     val a1Logs = phet load new File("C:\\Users\\Sam\\Desktop\\RPAL_logs\\A1 logs")
+    a1Logs.foreach(log => {
+      val report = new RPALAnalysis.Report(log)
+      val gr = report.gameResults
+      gr.foreach(g => {
+        println(g.points.mkString(","))
+      })
+    })
     println(new GameSummaryReport("A1", a1Logs))
   }
 }
