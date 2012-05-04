9bc13f21fcbdf8ed58455a86a11e7aa9918f3966
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-05-04 23:25:34 +0000

    Build a Fraction tab: Started canvas

--- trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/buildafraction/BuildAFractionModule.java
+++ trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/buildafraction/BuildAFractionModule.java
@@ -1,5 +1,9 @@
 package edu.colorado.phet.fractionsintro.buildafraction;
 
+import edu.colorado.phet.common.phetcommon.application.ApplicationConstructor;
+import edu.colorado.phet.common.phetcommon.application.PhetApplication;
+import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
+import edu.colorado.phet.common.phetcommon.application.PhetApplicationLauncher;
 import edu.colorado.phet.fractionsintro.FractionsIntroSimSharing.Components;
 import edu.colorado.phet.fractionsintro.buildafraction.model.BuildAFractionModel;
 import edu.colorado.phet.fractionsintro.buildafraction.view.BuildAFractionCanvas;
@@ -18,5 +22,15 @@ public class BuildAFractionModule extends AbstractFractionsModule {
     private BuildAFractionModule( BuildAFractionModel model ) {
         super( Components.buildAFractionTab, "Build a Fraction", model.clock );
         setSimulationPanel( new BuildAFractionCanvas( model ) );
+    }
+
+    //Test main for launching this module in an application by itself for testing
+    public static void main( String[] args ) {
+        final ApplicationConstructor constructor = new ApplicationConstructor() {
+            @Override public PhetApplication getApplication( PhetApplicationConfig c ) {
+                return new PhetApplication( c ) {{addModule( new BuildAFractionModule() );}};
+            }
+        };
+        new PhetApplicationLauncher().launchSim( args, "fractions", "fractions-intro", constructor );
     }
 }
