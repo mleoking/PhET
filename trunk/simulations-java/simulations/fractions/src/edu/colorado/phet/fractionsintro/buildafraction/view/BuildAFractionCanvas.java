f5ff8d974ef54db02c3893c2ea46d2faddead33c
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-05-05 17:01:18 +0000

    added bucket view and improved bar graphics

--- trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/buildafraction/view/BuildAFractionCanvas.java
+++ trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/buildafraction/view/BuildAFractionCanvas.java
@@ -6,13 +6,18 @@ import fj.data.List;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Stroke;
+import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 import java.awt.geom.RoundRectangle2D;
 import java.util.Collection;
 
 import javax.swing.JRadioButton;
 
+import edu.colorado.phet.common.phetcommon.model.Bucket;
+import edu.colorado.phet.common.phetcommon.view.Dimension2DDouble;
+import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
 import edu.colorado.phet.common.piccolophet.RichPNode;
+import edu.colorado.phet.common.piccolophet.nodes.BucketView;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPText;
 import edu.colorado.phet.common.piccolophet.nodes.Spacer;
@@ -24,6 +29,7 @@ import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolox.pswing.PSwing;
 
 import static edu.colorado.phet.fractions.FractionsResources.Strings.MY_FRACTIONS;
+import static fj.data.List.range;
 
 /**
  * Main simulation canvas for "build a fraction" tab
@@ -39,7 +45,7 @@ public class BuildAFractionCanvas extends AbstractFractionsCanvas {
 
         //IDEA: show the target in the box but grayed out and dotted line.  When the user has a match, it turns red dotted line.  When they drop it in, it fills in.
         //Would this have worked for build a molecule?
-        List<PNode> scoreBoxes = List.range( 0, 4 ).map( new F<Integer, PNode>() {
+        List<PNode> scoreBoxes = range( 0, 4 ).map( new F<Integer, PNode>() {
             @Override public PNode f( final Integer integer ) {
                 return new PhetPPath( new RoundRectangle2D.Double( 0, 0, 120, 120, 30, 30 ), stroke, Color.darkGray );
             }
@@ -61,7 +67,7 @@ public class BuildAFractionCanvas extends AbstractFractionsCanvas {
                     return bar( i + 1, i % 2, i / 2 );
                 }
             };
-            addChild( new FNode( List.range( 0, 8 ).map( toBar ) ) {{
+            addChild( new FNode( range( 0, 8 ).map( toBar ) ) {{
                 centerFullBoundsOnPoint( border.getCenterX(), border.getCenterY() );
             }} );
             setOffset( ( STAGE_SIZE.width - rightControlPanel.getFullWidth() ) / 2 - this.getFullWidth() / 2, INSET );
@@ -71,6 +77,10 @@ public class BuildAFractionCanvas extends AbstractFractionsCanvas {
             final PhetPPath border = new PhetPPath( new RoundRectangle2D.Double( 0, 0, 700, 150, 30, 30 ), stroke, Color.darkGray );
             addChild( border );
 
+            BucketView bucketView = new BucketView( new Bucket( 150, -50, new Dimension2DDouble( 200, 100 ), Color.blue, "pieces" ), ModelViewTransform.createOffsetScaleMapping( new Point2D.Double( 0, 0 ), 1, -1 ) );
+            addChild( bucketView.getHoleNode() );
+            addChild( bucketView.getFrontNode() );
+
             setOffset( ( STAGE_SIZE.width - rightControlPanel.getFullWidth() ) / 2 - this.getFullWidth() / 2, STAGE_SIZE.height - INSET - this.getFullHeight() );
         }} );
     }
@@ -79,12 +89,15 @@ public class BuildAFractionCanvas extends AbstractFractionsCanvas {
         return new PNode() {{
 
             double width = 120;
-            double height = 25;
+            final double height = 25;
 
-            double sliceWidth = width / numSegments;
-            for ( int i = 0; i < numSegments; i++ ) {
-                addChild( new PhetPPath( new Rectangle2D.Double( i * sliceWidth, 0, sliceWidth, height ), new BasicStroke( 1 ), Color.black ) );
-            }
+            final double sliceWidth = width / numSegments;
+            List<PNode> nodes = range( 0, numSegments ).map( new F<Integer, PNode>() {
+                @Override public PNode f( final Integer i ) {
+                    return new PhetPPath( new Rectangle2D.Double( i * sliceWidth, 0, sliceWidth, height ), new BasicStroke( 1 ), Color.black );
+                }
+            } );
+            addChild( new FNode( nodes ) );
 
             final int spacingX = 15;
             final int spacingY = 15;
