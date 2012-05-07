c347a0dc00b3ce4e67003e98aae91903d6a9c08e
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-05-07 14:50:32 +0000

    Simplified test main

--- trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/FractionsIntroApplication.java
+++ trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/FractionsIntroApplication.java
@@ -6,6 +6,8 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JCheckBoxMenuItem;
 
+import edu.colorado.phet.common.phetcommon.application.ApplicationConstructor;
+import edu.colorado.phet.common.phetcommon.application.PhetApplication;
 import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
 import edu.colorado.phet.common.phetcommon.application.PhetApplicationLauncher;
 import edu.colorado.phet.common.piccolophet.PiccoloPhetApplication;
@@ -74,5 +76,15 @@ public class FractionsIntroApplication extends PiccoloPhetApplication {
 
     public static void main( String[] args ) {
         new PhetApplicationLauncher().launchSim( args, "fractions", "fractions-intro", FractionsIntroApplication.class );
+    }
+
+    //Utility method for testing a single module
+    public static void runModule( String[] args, final MatchingGameModule module ) {
+        final ApplicationConstructor constructor = new ApplicationConstructor() {
+            @Override public PhetApplication getApplication( PhetApplicationConfig c ) {
+                return new PhetApplication( c ) {{addModule( module );}};
+            }
+        };
+        new PhetApplicationLauncher().launchSim( args, "fractions", "fractions-intro", constructor );
     }
 }
