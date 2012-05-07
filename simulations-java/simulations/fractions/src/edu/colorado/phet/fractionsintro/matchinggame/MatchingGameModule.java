3dc3a79ab792a63cc8b7c7cb2fb5328df65a061d
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-05-07 14:50:32 +0000

    Simplified test main

--- simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/matchinggame/MatchingGameModule.java
+++ simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/matchinggame/MatchingGameModule.java
@@ -3,17 +3,17 @@ package edu.colorado.phet.fractionsintro.matchinggame;
 
 import javax.swing.JComponent;
 
-import edu.colorado.phet.common.phetcommon.application.ApplicationConstructor;
-import edu.colorado.phet.common.phetcommon.application.PhetApplication;
-import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
-import edu.colorado.phet.common.phetcommon.application.PhetApplicationLauncher;
 import edu.colorado.phet.common.phetcommon.model.clock.IClock;
 import edu.colorado.phet.fractionsintro.FractionsIntroSimSharing.Components;
 import edu.colorado.phet.fractionsintro.common.AbstractFractionsModule;
 import edu.colorado.phet.fractionsintro.matchinggame.model.MatchingGameModel;
 import edu.colorado.phet.fractionsintro.matchinggame.view.MatchingGameCanvas;
 
+import static edu.colorado.phet.fractionsintro.FractionsIntroApplication.runModule;
+
 /**
+ * Module for the matching game tab.
+ *
  * @author Sam Reid
  */
 public class MatchingGameModule extends AbstractFractionsModule {
@@ -31,12 +31,5 @@ public class MatchingGameModule extends AbstractFractionsModule {
     }
 
     //Test main for launching this module in an application by itself for testing
-    public static void main( String[] args ) {
-        final ApplicationConstructor constructor = new ApplicationConstructor() {
-            @Override public PhetApplication getApplication( PhetApplicationConfig c ) {
-                return new PhetApplication( c ) {{addModule( new MatchingGameModule( true ) );}};
-            }
-        };
-        new PhetApplicationLauncher().launchSim( args, "fractions", "fractions-intro", constructor );
-    }
+    public static void main( String[] args ) { runModule( args, new MatchingGameModule( true ) ); }
 }
