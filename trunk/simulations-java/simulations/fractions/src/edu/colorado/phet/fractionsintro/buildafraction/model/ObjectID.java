2dfab2cf2816643a2bfda584df9235a3bb1b3d65
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-05-08 18:05:43 +0000

    Added docs

--- trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/buildafraction/model/ObjectID.java
+++ trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/buildafraction/model/ObjectID.java
@@ -3,6 +3,9 @@ package edu.colorado.phet.fractionsintro.buildafraction.model;
 import lombok.Data;
 
 /**
+ * For keeping track of objects throughout the application.  Cannot use reference identity because object instances change.
+ * Cannot use equality test since two objects might have the same state.  See implementation-notes.txt
+ *
  * @author Sam Reid
  */
 public @Data class ObjectID {
