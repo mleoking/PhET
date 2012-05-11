bac6b518927c683710ad16c492cbd04e362fb123
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-05-11 20:45:24 +0000

    #3322, code review: address comments in Solvent

--- simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/common/model/Solvent.java
+++ simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/common/model/Solvent.java
@@ -8,32 +8,12 @@ import edu.colorado.phet.beerslawlab.common.BLLSymbols;
 import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
 
-//REVIEW - There only appears to be one solvent used in the sim: Water.  Why
-// not just have a class that represents water? My guess is that other solvents
-// were envisioned or are planned for the future.  Should probable either not
-// this, or just make this be water.
-
 /**
  * A solvent (in this case a liquid) that dissolves another liquid (the solute) to create a solution.
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
 public class Solvent implements IFluid {
-
-    //REVIEW: Style comment - seems a bit odd to have Water here at the top of the class.
-    /*
-     * Pure water is a type of solvent.
-     * Since Solvents are immutable, we could use one static instance of Water.
-     * But that seems a bit precarious; if it's ever changed to mutable, it could make for some odd/difficult bugs.
-     */
-    public static class Water extends Solvent {
-
-        public static final Color COLOR = new Color( 224, 255, 255 );
-
-        public Water() {
-            super( Strings.WATER, BLLSymbols.WATER, COLOR );
-        }
-    }
 
     public final String name;
     public final String formula;
@@ -49,8 +29,21 @@ public class Solvent implements IFluid {
         return color.get();
     }
 
-    //REVIEW: The color property is public, so why have this method?  This isn't done elsewhere, such as for the location property in Movable.
     public void addFluidColorObserver( SimpleObserver observer ) {
         color.addObserver( observer );
+    }
+
+    /*
+     * Pure water is a type of solvent.
+     * Since Solvents are immutable, we could use one static instance of Water.
+     * But that seems a bit precarious; if it's ever changed to mutable, it could make for some odd/difficult bugs.
+     */
+    public static class Water extends Solvent {
+
+        public static final Color COLOR = new Color( 224, 255, 255 );
+
+        public Water() {
+            super( Strings.WATER, BLLSymbols.WATER, COLOR );
+        }
     }
 }
