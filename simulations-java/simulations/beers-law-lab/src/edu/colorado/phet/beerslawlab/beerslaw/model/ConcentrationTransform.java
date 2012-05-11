62456ff49c6795c8a9940748175a9a9ae1f269a8
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-05-11 20:30:29 +0000

    #3322, code review: improve javadoc for ConcentrationTransform

--- simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/beerslaw/model/ConcentrationTransform.java
+++ simulations-java/simulations/beers-law-lab/src/edu/colorado/phet/beerslawlab/beerslaw/model/ConcentrationTransform.java
@@ -4,17 +4,21 @@ package edu.colorado.phet.beerslawlab.beerslaw.model;
 import edu.colorado.phet.beerslawlab.common.BLLResources.Strings;
 
 /**
- * Concentration is stored the same for all solutions in the model, in moles per liter (M).
- * //REVIEW - Suggest improving following documentation.  Had to read several times to start to get it.
- * But each solution, this class specifies the units to be used for displaying the concentration in
+ * Manages the transformation between how concentration values are stored in
+ * the model, and how they are presented in the view.
+ * <p>
+ * In the model, concentration is stored in moles per liter (M) for all solutions.
+ * In the view, the concentration units will vary by solution, such that the
+ * concentration can be displayed as integral values (no decimals, no scientific notation.)
+ * This class specifies the units to be used for displaying the concentration in
  * the view (eg, mM, uM) and handles the conversion between model and view units.
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
 public class ConcentrationTransform {
 
-    private final double scale;
-    private final String units;
+    private final double scale; // scale factor used to convert model units (M) to view units
+    private final String units; // view units
 
     /*
      * Constructor.
