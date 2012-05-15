b087ebe37839cd34174a9061abfcbe598ab67ea3
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-05-15 19:45:17 +0000

    Cleanup: Using simpler constructors for labels, got rid of TODO related to a problem that has been solved.

--- simulations-java/simulations/gene-expression-basics/src/edu/colorado/phet/geneexpressionbasics/mrnaproduction/view/HorizontalSliderWithLabelsAtEnds.java
+++ simulations-java/simulations/gene-expression-basics/src/edu/colorado/phet/geneexpressionbasics/mrnaproduction/view/HorizontalSliderWithLabelsAtEnds.java
@@ -7,33 +7,26 @@ import edu.colorado.phet.common.phetcommon.model.property.BooleanProperty;
 import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.phetcommon.simsharing.messages.UserComponent;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
+import edu.colorado.phet.common.piccolophet.nodes.PhetPText;
 import edu.colorado.phet.common.piccolophet.nodes.layout.HBox;
 import edu.colorado.phet.common.piccolophet.nodes.slider.HSliderNode;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.nodes.PText;
 
 /**
- * Convenience class for a horizontal slider with labels at ends instead of
- * having tick marks with labels below them.
+ * Convenience class for a horizontal slider that has labels at each end
+ * rather than having tick marks with labels below them.
  *
  * @author John Blanco
  */
 class HorizontalSliderWithLabelsAtEnds extends PNode {
     private static final double OVERALL_WIDTH = 150;
     private static final Font LABEL_FONT = new PhetFont( 12 );
-
-    //TODO: There is an incorrect behavior based on which side the slider knob is on.
-    //This bug seems like it was introduced with changes to the HSliderNode, but I can't tell why it was working in 0.00.18
-    //To compensate, I have increased the inter element spacing, but it makes one label further from the slider than the other
     private static final double INTER_ELEMENT_SPACING = 5;
 
     HorizontalSliderWithLabelsAtEnds( UserComponent userComponent, Property<Double> doubleProperty, double min, double max, String leftLabel, String rightLabel ) {
-        PText leftLabelNode = new PText( leftLabel ) {{
-            setFont( LABEL_FONT );
-        }};
-        PText rightLabelNode = new PText( rightLabel ) {{
-            setFont( LABEL_FONT );
-        }};
+        PText leftLabelNode = new PhetPText( leftLabel, LABEL_FONT );
+        PText rightLabelNode = new PhetPText( rightLabel, LABEL_FONT );
         double sliderWidth = OVERALL_WIDTH - leftLabelNode.getFullBoundsReference().width -
                              rightLabelNode.getFullBoundsReference().width - ( 2 * INTER_ELEMENT_SPACING );
         PNode sliderNode = new HSliderNode( userComponent,
