f452cc69fd4cf75dfaec95c11f459f4cf9300b4a
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-05-15 17:22:10 +0000

    Added and integrated some new representations for the energy chunks.

--- trunk/simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/intro/view/EnergyChunkNode.java
+++ trunk/simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/intro/view/EnergyChunkNode.java
@@ -1,17 +1,12 @@
 // Copyright 2002-2012, University of Colorado
 package edu.colorado.phet.energyformsandchanges.intro.view;
 
-import java.awt.BasicStroke;
-import java.awt.Color;
-import java.awt.geom.RoundRectangle2D;
+import java.awt.Image;
 import java.util.Random;
 
 import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
 import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
-import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
-import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
-import edu.colorado.phet.common.piccolophet.nodes.PhetPText;
 import edu.colorado.phet.energyformsandchanges.EnergyFormsAndChangesResources;
 import edu.colorado.phet.energyformsandchanges.intro.model.EnergyChunk;
 import edu.umd.cs.piccolo.PNode;
@@ -24,23 +19,22 @@ import edu.umd.cs.piccolo.nodes.PImage;
  */
 public class EnergyChunkNode extends PNode {
 
+    private static final double WIDTH = 24; // In screen coords, which is close to pixels.
     private static final Random RAND = new Random( 1962 );
 
     public EnergyChunkNode( EnergyChunk energyChunk, final ModelViewTransform mvt ) {
 
+        Image imageSource;
         if ( RAND.nextBoolean() ) {
-            PNode image = new PImage( EnergyFormsAndChangesResources.Images.ENERGY_CHUNK_THERMAL );
-            image.setOffset( -image.getFullBoundsReference().width / 2, -image.getFullBoundsReference().height / 2 );
-            addChild( image );
+            imageSource = EnergyFormsAndChangesResources.Images.E_THERM_OUTLINE;
         }
         else {
-            double width = 17;
-            PNode outline = new PhetPPath( new RoundRectangle2D.Double( -width / 2, -width / 2, width, width, 3, 3 ), new BasicStroke( 2 ), new Color( 200, 0, 0 ) );
-            outline.addChild( new PhetPText( "E", new PhetFont( 12, true ) ) {{
-                centerFullBoundsOnPoint( 0, 0 );
-            }} );
-            addChild( outline );
+            imageSource = EnergyFormsAndChangesResources.Images.E_THERM_OUTLINE_BLACK;
         }
+        PImage image = new PImage( imageSource );
+        image.setScale( WIDTH / image.getFullBoundsReference().width );
+        image.setOffset( -image.getFullBoundsReference().width / 2, -image.getFullBoundsReference().height / 2 );
+        addChild( image );
 
         // Set this node's position when the corresponding model element moves.
         energyChunk.position.addObserver( new VoidFunction1<ImmutableVector2D>() {
