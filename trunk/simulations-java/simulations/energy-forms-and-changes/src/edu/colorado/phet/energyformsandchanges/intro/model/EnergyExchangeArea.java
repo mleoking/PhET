54dbe1be3f70135a3fc7c9fa860e3eaf8b3b57cf
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-05-16 20:08:47 +0000

    Created class for containing and managing energy in the various thermal elements in the model.

---
+++ trunk/simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/intro/model/EnergyExchangeArea.java
@@ -0,0 +1,29 @@
+// Copyright 2002-2012, University of Colorado
+package edu.colorado.phet.energyformsandchanges.intro.model;
+
+import java.awt.geom.Rectangle2D;
+
+import edu.colorado.phet.common.phetcommon.model.property.Property;
+import edu.colorado.phet.common.phetcommon.util.ObservableList;
+
+/**
+ * Class that represents a 2D space that contains energy.  Contact between
+ * two such areas that have different amounts of energy will result in a
+ * transfer of energy proportional to the amount of contact.
+ *
+ * @author John Blanco
+ */
+public class EnergyExchangeArea implements EnergyContainer {
+
+    private final Property<Double> energy;
+    private final Rectangle2D bounds = new Rectangle2D.Double();
+
+    public EnergyExchangeArea( Rectangle2D bounds, double initialEnergy ) {
+        this.bounds.setFrame( bounds );
+        energy = new Property<Double>( initialEnergy );
+    }
+
+    public ObservableList<EnergyChunk> getEnergyChunkList() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
