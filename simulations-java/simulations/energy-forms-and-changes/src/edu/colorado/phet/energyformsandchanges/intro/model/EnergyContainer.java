0c59afd509615891cf9be3845d48bc127ed719fd
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-05-16 20:16:55 +0000

    Added method definitions and stubbed methods for changing the amount of energy and for exchanging energy between two energy containers.

--- simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/intro/model/EnergyContainer.java
+++ simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/intro/model/EnergyContainer.java
@@ -4,8 +4,40 @@ package edu.colorado.phet.energyformsandchanges.intro.model;
 import edu.colorado.phet.common.phetcommon.util.ObservableList;
 
 /**
+ * Interface for model elements that contain energy.
+ *
  * @author John Blanco
  */
 public interface EnergyContainer {
+
+    /**
+     * Change the amount of energy contained.  This is used to both add and
+     * remove energy.
+     *
+     * @param deltaEnergy
+     */
+    void changeEnergy( double deltaEnergy );
+
+    /**
+     * Get the current amount of energy contained.
+     *
+     * @return
+     */
+    double getEnergy();
+
+    /**
+     * Reset to the initial amount of energy.
+     */
+    void reset();
+
+    /**
+     * Exchange energy with another energy container.  The implementation must
+     * determine the amount of contact or overlap as well as the energy
+     * gradient and do the exchange based on these conditions.
+     *
+     * @param energyContainer
+     */
+    void exchangeEnergyWith( EnergyContainer energyContainer );
+
     ObservableList<EnergyChunk> getEnergyChunkList();
 }
