897e35777cbc86765a9c2f23e74a67b363bddefd
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-05-17 21:06:23 +0000

    Added shared constants.

---
+++ trunk/simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/common/EFACConstants.java
@@ -0,0 +1,11 @@
+// Copyright 2002-2012, University of Colorado
+package edu.colorado.phet.energyformsandchanges.common;
+
+/**
+ * Shared constants used in multiple locations within the sim.
+ *
+ * @author John Blanco
+ */
+public class EFACConstants {
+    public static final double ROOM_TEMPERATURE = 296; // In Kelvin.
+}
