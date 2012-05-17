80c7b3f6debba1534726c8339f4448ec486cefe2
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-05-17 04:57:27 +0000

    reactions: added arrow, refactoring in constants with docs

--- simulations-java/simulations/chemical-reactions/src/edu/colorado/phet/chemicalreactions/ChemicalReactionsConstants.java
+++ simulations-java/simulations/chemical-reactions/src/edu/colorado/phet/chemicalreactions/ChemicalReactionsConstants.java
@@ -10,10 +10,41 @@ import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTra
 import edu.umd.cs.piccolo.util.PDimension;
 
 public class ChemicalReactionsConstants {
-    public static final Dimension2D STAGE_SIZE = new PDimension( 1008, 709 ); // change height to 679 once we have another tab
+
+    /*---------------------------------------------------------------------------*
+    * debugging
+    *----------------------------------------------------------------------------*/
+
+    public static final boolean ENABLE_BOX2D_DEBUG_DRAW = true;
+
+    /*---------------------------------------------------------------------------*
+    * colors
+    *----------------------------------------------------------------------------*/
+
     public static final Color CANVAS_BACKGROUND_COLOR = new Color( 198, 226, 246 );
+
+    public static final Color KIT_BACKGROUND = Color.WHITE; // kit area background
+    public static final Color KIT_BORDER = Color.BLACK; // border around the kit area
+
+    public static final Color KIT_ARROW_BACKGROUND_ENABLED = Color.YELLOW; // kit next/prev arrow background
+    public static final Color KIT_ARROW_BORDER_ENABLED = Color.BLACK; // kit next/prev arrow border
     public static final Color PLAY_AREA_BACKGROUND_COLOR = Color.WHITE;
 
+    public static final Color OPERATOR_COLOR = new Color( 160, 210, 255 );
+    public static final Color OPERATOR_BORDER_COLOR = new Color( 110, 160, 200 );
+
+    /*---------------------------------------------------------------------------*
+    * model / sizes
+    *----------------------------------------------------------------------------*/
+
+    // how many times per frame (clock-tick) should we run box2d calculations + body intraStep() calls
+    public static final int STEPS_PER_FRAME = 5;
+
+    // how many iterations should we tell the box2d world to calculate during each one of the above steps?
+    // number of iterations total per frame will be MODEL_ITERATIONS_PER_STEP * STEPS_PER_FRAME
+    public static final int MODEL_ITERATIONS_PER_STEP = 5;
+
+    public static final Dimension2D STAGE_SIZE = new PDimension( 1008, 709 ); // change height to 679 once we have another tab
     public static final ModelViewTransform MODEL_VIEW_TRANSFORM = ModelViewTransform.createSinglePointScaleInvertedYMapping(
             new Point2D.Double( 0, 0 ),
             new Point( (int) Math.round( ChemicalReactionsConstants.STAGE_SIZE.getWidth() * 0.5 ),
@@ -22,30 +53,28 @@ public class ChemicalReactionsConstants {
 
     public static final ModelViewTransform BOX2D_MODEL_TRANSFORM = ModelViewTransform.createOffsetScaleMapping( new Point2D.Double(), 200, 200 );
 
-    public static final Color KIT_BACKGROUND = Color.WHITE; // kit area background
-    public static final Color KIT_BORDER = Color.BLACK; // border around the kit area
-
-    public static final Color KIT_ARROW_BACKGROUND_ENABLED = Color.YELLOW; // kit next/prev arrow background
-    public static final Color KIT_ARROW_BORDER_ENABLED = Color.BLACK; // kit next/prev arrow border
-
     // size of our model area. model centered around 0 (and there are a few dependencies on this)
     public static final PDimension MODEL_SIZE = new PDimension( MODEL_VIEW_TRANSFORM.viewToModelDeltaX( STAGE_SIZE.getWidth() ), // don't simplify. working around MVT bug
                                                                 Math.abs( MODEL_VIEW_TRANSFORM.viewToModelDeltaY( STAGE_SIZE.getHeight() ) ) );
 
+    /*---------------------------------------------------------------------------*
+    * dimensions and padding
+    *----------------------------------------------------------------------------*/
+
     // padding between simulation edges and kit/collection areas
     public static final double VIEW_PADDING = 18;
     public static final double MODEL_PADDING = MODEL_VIEW_TRANSFORM.viewToModelDeltaX( VIEW_PADDING );
-    public static final int MODEL_ITERATIONS_PER_STEP = 5;
-    public static final int STEPS_PER_FRAME = 5;
-
-    public static final boolean ENABLE_BOX2D_DEBUG_DRAW = true;
 
     public static final float BOX2D_DENSITY = 1;
-    public static final float BOX2D_WALL_THICKNESS = 4;
+    public static final float BOX2D_WALL_THICKNESS = 4; // box2d coordinates
 
+    // "plus" dimensions in view coordinates
     public static final float PLUS_VIEW_THICKNESS = 15;
     public static final float PLUS_VIEW_LENGTH = 50;
 
-    public static final Color PLUS_COLOR = new Color( 160, 210, 255 );
-    public static final Color PLUS_BORDER_COLOR = new Color( 110, 160, 200 );
+    // "arrow" dimensions in view coordinates
+    public static final float ARROW_VIEW_THICKNESS = 15;
+    public static final float ARROW_VIEW_HEAD_LENGTH = 25;
+    public static final float ARROW_VIEW_HEAD_WIDTH = 35;
+    public static final float ARROW_VIEW_PADDING = 20;
 }
--- simulations-java/simulations/chemical-reactions/src/edu/colorado/phet/chemicalreactions/view/KitView.java
+++ simulations-java/simulations/chemical-reactions/src/edu/colorado/phet/chemicalreactions/view/KitView.java
@@ -3,7 +3,9 @@ package edu.colorado.phet.chemicalreactions.view;
 
 import java.awt.BasicStroke;
 import java.awt.geom.Area;
+import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,6 +15,7 @@ import edu.colorado.phet.chemicalreactions.model.Molecule;
 import edu.colorado.phet.chemicalreactions.model.MoleculeBucket;
 import edu.colorado.phet.chemicalreactions.module.ChemicalReactionsCanvas;
 import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
+import edu.colorado.phet.common.piccolophet.nodes.ArrowNode;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.umd.cs.piccolo.PNode;
 
@@ -51,25 +54,16 @@ public class KitView {
             }
         }
 
-        // pluses between reactants
-        final List<MoleculeBucket> reactantBuckets = kit.getReactantBuckets();
-        for ( int i = 1; i < reactantBuckets.size(); i++ ) {
-            // get the space in between our reactants
-            double leftX = bucketMap.get( reactantBuckets.get( i - 1 ) ).getFrontNode().getFullBounds().getMaxX();
-            double rightX = bucketMap.get( reactantBuckets.get( i ) ).getFrontNode().getFullBounds().getMinX();
-
-            // center of the plus
-            final double centerX = ( leftX + rightX ) / 2;
-            final double centerY = kit.getLayoutBounds().getAvailableKitViewBounds().getCenterY();
-
-            // construct it from two areas
-            bottomLayer.addChild( new PhetPPath( new Area() {{
-                add( new Area( new Rectangle2D.Double( centerX - PLUS_VIEW_THICKNESS / 2, centerY - PLUS_VIEW_LENGTH / 2,
-                                                       PLUS_VIEW_THICKNESS, PLUS_VIEW_LENGTH ) ) );
-                add( new Area( new Rectangle2D.Double( centerX - PLUS_VIEW_LENGTH / 2, centerY - PLUS_VIEW_THICKNESS / 2,
-                                                       PLUS_VIEW_LENGTH, PLUS_VIEW_THICKNESS ) ) );
-            }}, PLUS_COLOR, new BasicStroke( 1 ), PLUS_BORDER_COLOR ) );
+        // pluses between groups of reactant or product buckets
+        for ( List<MoleculeBucket> moleculeBuckets : Arrays.asList( kit.getReactantBuckets(), kit.getProductBuckets() ) ) {
+            for ( int i = 1; i < moleculeBuckets.size(); i++ ) {
+                addPlusBetween( moleculeBuckets.get( i - 1 ), moleculeBuckets.get( i ) );
+            }
         }
+
+        // add an arrow between last reactant bucket and first product bucket
+        addArrowBetween( kit.getReactantBuckets().get( kit.getReactantBuckets().size() - 1 ),
+                         kit.getProductBuckets().get( 0 ) );
 
         // update visibility based on the kit visibility
         kit.visible.addObserver( new SimpleObserver() {
@@ -81,6 +75,45 @@ public class KitView {
                 bottomLayer.setVisible( visible );
             }
         } );
+    }
+
+    public void addPlusBetween( MoleculeBucket leftBucket, MoleculeBucket rightBucket ) {
+        // get the space in between our buckets
+        double leftX = bucketMap.get( leftBucket ).getFrontNode().getFullBounds().getMaxX();
+        double rightX = bucketMap.get( rightBucket ).getFrontNode().getFullBounds().getMinX();
+
+        // center of the plus
+        final double centerX = ( leftX + rightX ) / 2;
+        final double centerY = kit.getLayoutBounds().getAvailableKitViewBounds().getCenterY();
+
+        // construct it from two areas
+        bottomLayer.addChild( new PhetPPath( new Area() {{
+            add( new Area( new Rectangle2D.Double( centerX - PLUS_VIEW_THICKNESS / 2, centerY - PLUS_VIEW_LENGTH / 2,
+                                                   PLUS_VIEW_THICKNESS, PLUS_VIEW_LENGTH ) ) );
+            add( new Area( new Rectangle2D.Double( centerX - PLUS_VIEW_LENGTH / 2, centerY - PLUS_VIEW_THICKNESS / 2,
+                                                   PLUS_VIEW_LENGTH, PLUS_VIEW_THICKNESS ) ) );
+        }}, OPERATOR_COLOR, new BasicStroke( 1 ), OPERATOR_BORDER_COLOR ) );
+    }
+
+    public void addArrowBetween( MoleculeBucket leftBucket, MoleculeBucket rightBucket ) {
+        // get the space in between our buckets (with padding, so the arrow bounds touch these)
+        double leftX = bucketMap.get( leftBucket ).getFrontNode().getFullBounds().getMaxX() + ARROW_VIEW_PADDING;
+        double rightX = bucketMap.get( rightBucket ).getFrontNode().getFullBounds().getMinX() - ARROW_VIEW_PADDING;
+
+        final double centerY = kit.getLayoutBounds().getAvailableKitViewBounds().getCenterY();
+
+
+        bottomLayer.addChild( new ArrowNode(
+                new Point2D.Double( leftX, centerY ),
+                new Point2D.Double( rightX, centerY ),
+                ARROW_VIEW_HEAD_LENGTH,
+                ARROW_VIEW_HEAD_WIDTH,
+                ARROW_VIEW_THICKNESS
+        ) {{
+            setPaint( OPERATOR_COLOR );
+            setStroke( new BasicStroke( 1 ) );
+            setStrokePaint( OPERATOR_BORDER_COLOR );
+        }} );
     }
 
     public PNode getTopLayer() {
