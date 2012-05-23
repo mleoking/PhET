bcd274616425c3df0114f0c9820cd88979d03670
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-05-23 19:00:23 +0000

    5/23/12 move PointTool to linegraphing.common.model

--- simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/common/model/PointTool.java
+++ simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/common/model/PointTool.java
@@ -1,5 +1,5 @@
 // Copyright 2002-2012, University of Colorado
-package edu.colorado.phet.linegraphing.slopeintercept.model;
+package edu.colorado.phet.linegraphing.common.model;
 
 import java.util.ArrayList;
 
