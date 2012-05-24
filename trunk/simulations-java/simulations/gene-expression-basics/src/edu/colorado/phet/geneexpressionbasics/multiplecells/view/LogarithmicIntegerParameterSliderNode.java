4ec078be7fce65565b2c832b2f2b167d7efb89db
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-05-24 22:38:09 +0000

    Replaced what was essentially duplicated code by wrapping the LogarithmicDoubleParameterSliderNode inside of this slider.

--- trunk/simulations-java/simulations/gene-expression-basics/src/edu/colorado/phet/geneexpressionbasics/multiplecells/view/LogarithmicIntegerParameterSliderNode.java
+++ trunk/simulations-java/simulations/gene-expression-basics/src/edu/colorado/phet/geneexpressionbasics/multiplecells/view/LogarithmicIntegerParameterSliderNode.java
@@ -1,21 +1,12 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.geneexpressionbasics.multiplecells.view;
 
-import java.awt.Color;
-import java.awt.Font;
-
-import edu.colorado.phet.common.phetcommon.model.property.BooleanProperty;
 import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.phetcommon.model.property.SettableProperty;
+import edu.colorado.phet.common.phetcommon.model.property.doubleproperty.DoubleProperty;
 import edu.colorado.phet.common.phetcommon.simsharing.messages.IUserComponent;
 import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
-import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
-import edu.colorado.phet.common.piccolophet.nodes.HTMLNode;
-import edu.colorado.phet.common.piccolophet.nodes.layout.VBox;
-import edu.colorado.phet.common.piccolophet.nodes.slider.HSliderNode;
-import edu.colorado.phet.geneexpressionbasics.GeneExpressionBasicsResources;
 import edu.umd.cs.piccolo.PNode;
-import edu.umd.cs.piccolo.nodes.PText;
 
 /**
  * Class that depicts a slider that controls a parameter of type Integer in a
@@ -25,64 +16,23 @@ import edu.umd.cs.piccolo.nodes.PText;
  */
 class LogarithmicIntegerParameterSliderNode extends PNode {
 
-    private static final Font TITLE_FONT = new PhetFont( 14 );
-    private static final double SLIDER_TRACK_LENGTH = 100;
-    private static final double SLIDER_TRACK_THICKNESS = 4;
-    private static final Font LABEL_FONT = new PhetFont( 12 );
+    LogarithmicIntegerParameterSliderNode( IUserComponent userComponent, final int min, final int max, final SettableProperty<Integer> settableProperty, String htmlLabelText ) {
 
-    LogarithmicIntegerParameterSliderNode( IUserComponent userComponent, final double min, final double max, final SettableProperty<Integer> settableProperty, String htmlLabelText ) {
+        // Connect the integer property to a double property.
+        final Property<Double> doubleProperty = new DoubleProperty( new Double( settableProperty.get() ) );
 
-        // Parameter checking.
-        if ( min <= 0 ) {
-            throw new IllegalArgumentException( "Can't have a value of 0 or less for a logarithmic slider." );
-        }
-        else if ( min > max ) {
-            throw new IllegalArgumentException( "min value must be smaller than max value." );
-        }
-
-        // Create the label.
-        PNode labelNode = new HTMLNode( htmlLabelText ) {{
-            setFont( TITLE_FONT );
-        }};
-
-        double sliderMin = Math.log10( min );
-        double sliderMax = Math.log10( max );
-
-        // Property that is controlled by slider and then converted to an
-        // exponential value.
-        final Property<Double> passThroughProperty = new Property<Double>( Math.log10( settableProperty.get() ) );
-
-        // Create the slider node.
-        HSliderNode sliderNode = new HSliderNode( userComponent, sliderMin, sliderMax, SLIDER_TRACK_THICKNESS,
-                                                  SLIDER_TRACK_LENGTH, passThroughProperty, new BooleanProperty( true ) ) {{
-            setTrackFillPaint( Color.black );
-        }};
-
-        // Conversion to exponential.
-        passThroughProperty.addObserver( new VoidFunction1<Double>() {
-            public void apply( Double sliderValue ) {
-                settableProperty.set( (int) Math.round( Math.pow( 10, sliderValue ) ) );
+        doubleProperty.addObserver( new VoidFunction1<Double>() {
+            public void apply( Double newValue ) {
+                settableProperty.set( (int) Math.round( newValue ) );
             }
         } );
 
-        // Hook up the data flow in the other direction, so that if the
-        // controlled value changes (which may occur, for example, when the
-        // property is reset) this changes too.
         settableProperty.addObserver( new VoidFunction1<Integer>() {
-            public void apply( Integer value ) {
-                passThroughProperty.set( Math.log10( value ) );
+            public void apply( Integer newValue ) {
+                doubleProperty.set( new Double( newValue ) );
             }
         } );
 
-        // Add the labels to the slider node.
-        sliderNode.addLabel( sliderMin, new PText( GeneExpressionBasicsResources.Strings.LOW ) {{
-            setFont( LABEL_FONT );
-        }} );
-        sliderNode.addLabel( sliderMax, new PText( GeneExpressionBasicsResources.Strings.HIGH ) {{
-            setFont( LABEL_FONT );
-        }} );
-
-        // Add the label and slider to a vertical box.
-        addChild( new VBox( 0, labelNode, sliderNode ) );
+        addChild( new LogarithmicParameterSliderNode( userComponent, min, max, doubleProperty, htmlLabelText ) );
     }
 }
