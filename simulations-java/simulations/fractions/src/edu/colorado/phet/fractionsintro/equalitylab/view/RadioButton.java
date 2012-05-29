95b0721983d564b64d0209a9900ca27e82e4cacd
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-05-29 16:15:52 +0000

    added docs

--- simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/equalitylab/view/RadioButton.java
+++ simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/equalitylab/view/RadioButton.java
@@ -8,15 +8,15 @@ import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolox.pswing.PSwing;
 
 /**
+ * Wrapper for PSwing radio buttons, with standard font and background.
+ *
  * @author Sam Reid
  */
 public class RadioButton extends PNode {
-
     public RadioButton( final IUserComponent component, final String text, final SettableProperty<Boolean> property, final boolean value ) {
         addChild( new PSwing( new PropertyRadioButton<Boolean>( component, text, property, value ) {{
             setOpaque( false );
             setFont( new PhetFont( 18, true ) );
         }} ) );
-
     }
 }
