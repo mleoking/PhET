0ab83127c0356028eabbad1a1881deb5a20bcd20
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-05-31 16:53:46 +0000

    pt / lwjgl: partial commit for raycasting of terrain (not hooked up yet)

--- trunk/simulations-java/simulations/plate-tectonics/src/edu/colorado/phet/platetectonics/model/Terrain.java
+++ trunk/simulations-java/simulations/plate-tectonics/src/edu/colorado/phet/platetectonics/model/Terrain.java
@@ -5,7 +5,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import edu.colorado.phet.common.phetcommon.model.event.ValueNotifier;
+import edu.colorado.phet.common.phetcommon.util.Option;
+import edu.colorado.phet.common.phetcommon.util.Option.None;
 import edu.colorado.phet.lwjglphet.math.ImmutableVector2F;
+import edu.colorado.phet.lwjglphet.math.ImmutableVector3F;
+import edu.colorado.phet.lwjglphet.math.LWJGLTransform;
+import edu.colorado.phet.lwjglphet.math.Ray3F;
 import edu.colorado.phet.platetectonics.util.Side;
 
 public class Terrain {
@@ -127,5 +132,57 @@ public class Terrain {
         for ( TerrainSample sample : getColumn( columnIndex ) ) {
             sample.setElevation( elevation );
         }
+    }
+
+    private ImmutableVector3F[] getViewSpaceRow( int zIndex, ImmutableVector3F[] xRadials, LWJGLTransform modelViewTransform ) {
+        ImmutableVector3F[] result = new ImmutableVector3F[xPositions.size()];
+        ImmutableVector3F zRadial = PlateModel.getZRadialVector( zPositions.get( zIndex ) );
+        for ( int xIndex = 0; xIndex < xPositions.size(); xIndex++ ) {
+            float elevation = getSample( xIndex, zIndex ).getElevation();
+            ImmutableVector3F cartesianModelVector = PlateModel.convertToRadial( xRadials[xIndex], zRadial, elevation );
+            ImmutableVector3F viewVector = modelViewTransform.transformPosition( cartesianModelVector );
+
+            result[xIndex] = viewVector;
+        }
+        return result;
+    }
+
+    // returns the point in "planar" form, not radial. ray is implied as view space
+    public Option<ImmutableVector3F> intersectWithRay( Ray3F ray, LWJGLTransform modelViewTransform ) {
+        // used for faster trig calculations for positioning
+        ImmutableVector3F[] xRadials = new ImmutableVector3F[xPositions.size()];
+
+        for ( int i = 0; i < xPositions.size(); i++ ) {
+            xRadials[i] = PlateModel.getXRadialVector( xPositions.get( i ) );
+        }
+
+        // try intersecting from front to back
+        // NOTE: this is reliant on the order of z indices decreasing
+        int zIndex = zSamples - 1;
+        ImmutableVector3F[] lastRow = getViewSpaceRow( zIndex, xRadials, modelViewTransform );
+        while ( zIndex >= 1 ) {
+            zIndex--;
+            ImmutableVector3F[] nextRow = getViewSpaceRow( zIndex, xRadials, modelViewTransform );
+            for ( int i = 1; i < xPositions.size(); i++ ) {
+                ImmutableVector3F leftLast = lastRow[i - 1];
+                ImmutableVector3F rightLast = lastRow[i];
+                ImmutableVector3F leftNext = nextRow[i - 1];
+                ImmutableVector3F rightNext = nextRow[i];
+
+                // check two "triangles" for each quad
+                Option<ImmutableVector3F> hit = ray.intersectWithTriangle( leftNext, rightNext, rightLast );
+                if ( hit.isSome() ) {
+                    return hit;
+                }
+
+                hit = ray.intersectWithTriangle( rightLast, leftLast, leftNext );
+                if ( hit.isSome() ) {
+                    return hit;
+                }
+            }
+        }
+
+        // no hit found
+        return new None<ImmutableVector3F>();
     }
 }
