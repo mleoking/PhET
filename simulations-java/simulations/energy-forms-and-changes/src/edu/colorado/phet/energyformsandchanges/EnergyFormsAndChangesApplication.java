03b03c763bef69b3853d52d88d6df4b36fa0cc79
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-06-01 21:50:20 +0000

    Added a developer control that hides/shows a button that will dump the energies of the thermal objects to the console.  Button is not yet hooked up.

--- simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/EnergyFormsAndChangesApplication.java
+++ simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/EnergyFormsAndChangesApplication.java
@@ -1,11 +1,19 @@
 // Copyright 2002-2012, University of Colorado
 package edu.colorado.phet.energyformsandchanges;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JMenu;
+
 import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
 import edu.colorado.phet.common.phetcommon.application.PhetApplicationLauncher;
+import edu.colorado.phet.common.phetcommon.view.PhetFrame;
 import edu.colorado.phet.common.piccolophet.PiccoloPhetApplication;
 import edu.colorado.phet.energyformsandchanges.energysystems.EnergySystemsModule;
 import edu.colorado.phet.energyformsandchanges.intro.EFACIntroModule;
+import edu.colorado.phet.energyformsandchanges.intro.view.EFACIntroCanvas;
 
 /**
  * Main application class for PhET's Energy Forms and Changes simulation.
@@ -21,6 +29,26 @@ public class EnergyFormsAndChangesApplication extends PiccoloPhetApplication {
         // Create the modules
         addModule( new EFACIntroModule() );
         addModule( new EnergySystemsModule() );
+        initMenuBar();
+    }
+
+    private void initMenuBar() {
+        final PhetFrame frame = getPhetFrame();
+
+        // Developer menu
+        JMenu developerMenu = frame.getDeveloperMenu();
+
+        // Add an item for controlling whether unstable nuclei are animated.
+        final JCheckBoxMenuItem animateNucleusCheckBox = new JCheckBoxMenuItem( "Show Dump Energies Button" ) {{
+            setSelected( EFACIntroCanvas.showDumpEnergiesButton.get() );
+            addActionListener( new ActionListener() {
+                public void actionPerformed( ActionEvent e ) {
+                    EFACIntroCanvas.showDumpEnergiesButton.set( isSelected() );
+                }
+            } );
+        }};
+
+        developerMenu.add( animateNucleusCheckBox );
     }
 
     public static void main( String[] args ) {
