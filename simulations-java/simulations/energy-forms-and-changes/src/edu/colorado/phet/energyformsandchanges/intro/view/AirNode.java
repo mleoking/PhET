f003ea32601673af79bde64bac29c57004b9e24c
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-06-05 21:15:52 +0000

    Added the ability to track energy chunks in the air and add/remove corresponding nodes in the view.

--- simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/intro/view/AirNode.java
+++ simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/intro/view/AirNode.java
@@ -4,9 +4,11 @@ package edu.colorado.phet.energyformsandchanges.intro.view;
 import java.awt.BasicStroke;
 import java.awt.Color;
 
+import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.colorado.phet.energyformsandchanges.intro.model.Air;
+import edu.colorado.phet.energyformsandchanges.intro.model.EnergyChunk;
 import edu.umd.cs.piccolo.PNode;
 
 /**
@@ -16,9 +18,29 @@ public class AirNode extends PNode {
 
     private static final boolean SHOW_BOUNDS = true;
 
-    public AirNode( Air air, ModelViewTransform mvt ) {
+    public AirNode( final Air air, final ModelViewTransform mvt ) {
         if ( SHOW_BOUNDS ) {
             addChild( new PhetPPath( mvt.modelToView( air.getThermalContactArea().getBounds() ), new BasicStroke( 1 ), Color.RED ) );
         }
+
+        // Create a layer where energy chunks will be placed.
+        final PNode energyChunkLayer = new PNode();
+        addChild( energyChunkLayer );
+
+        // Watch for energy chunks coming and going and add/remove nodes accordingly.
+        air.getEnergyChunkList().addElementAddedObserver( new VoidFunction1<EnergyChunk>() {
+            public void apply( final EnergyChunk addedEnergyChunk ) {
+                final PNode energyChunkNode = new EnergyChunkNode( addedEnergyChunk, mvt );
+                energyChunkLayer.addChild( energyChunkNode );
+                air.getEnergyChunkList().addElementRemovedObserver( new VoidFunction1<EnergyChunk>() {
+                    public void apply( EnergyChunk removedEnergyChunk ) {
+                        if ( removedEnergyChunk == addedEnergyChunk ) {
+                            energyChunkLayer.removeChild( energyChunkNode );
+                            air.getEnergyChunkList().removeElementRemovedObserver( this );
+                        }
+                    }
+                } );
+            }
+        } );
     }
 }
