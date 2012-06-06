40ec59ad9206152a357a8fc3dafca7cda381082d
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-06-06 14:50:23 +0000

    Simplified level descriptors

--- simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/buildafraction/view/Target.java
+++ simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/buildafraction/view/Target.java
@@ -1,10 +1,12 @@
 package edu.colorado.phet.fractionsintro.buildafraction.view;
 
+import fj.F;
 import fj.data.List;
 import lombok.Data;
 
 import java.awt.Color;
 
+import edu.colorado.phet.fractionsintro.buildafraction.model.BuildAFractionModel;
 import edu.colorado.phet.fractionsintro.intro.model.Fraction;
 import edu.colorado.phet.fractionsintro.matchinggame.view.fractions.FilledPattern;
 
@@ -17,5 +19,7 @@ public @Data class Target {
     public final List<FilledPattern> filledPattern;
 
     //Convenience for single pattern
-    public static Target newTarget( Fraction fraction, Color color, FilledPattern pattern ) { return new Target( fraction, color, List.single( pattern ) ); }
+    public static Target target( int numerator, int denominator, Color color, F<Fraction, FilledPattern> pattern ) {
+        return new Target( new Fraction( numerator, denominator ), color, BuildAFractionModel.composite( pattern ).f( new Fraction( numerator, denominator ) ) );
+    }
 }
