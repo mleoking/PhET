67e0fdb938d6ff0750fc20b5fbb02be506a04899
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-06-06 03:22:21 +0000

    started creating levels

---
+++ trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/buildafraction/model/Level.java
@@ -0,0 +1,28 @@
+package edu.colorado.phet.fractionsintro.buildafraction.model;
+
+import fj.data.List;
+
+import edu.colorado.phet.common.phetcommon.model.property.Property;
+import edu.colorado.phet.fractionsintro.buildafraction.view.Target;
+import edu.colorado.phet.fractionsintro.intro.model.Fraction;
+
+/**
+ * Level for the build a fraction game.
+ *
+ * @author Sam Reid
+ */
+public class Level {
+
+    //Fractions the user has created in the play area, which may match a target
+    public final Property<List<Fraction>> createdFractions = new Property<List<Fraction>>( List.<Fraction>nil() );
+
+    public final List<Target> targets;
+
+    public Level( final List<Target> targets ) {
+        this.targets = targets;
+    }
+
+    public Target getTarget( final int i ) {
+        return targets.index( i );
+    }
+}
