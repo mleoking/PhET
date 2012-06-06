e494ce66998e220b1b9de21a850b1365c9e65541
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-06-06 20:11:14 +0000

    started adding pictures scene

---
+++ simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/buildafraction/view/PictureSceneNode.java
@@ -0,0 +1,14 @@
+package edu.colorado.phet.fractionsintro.buildafraction.view;
+
+import edu.colorado.phet.fractionsintro.buildafraction.model.BuildAFractionModel;
+import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.util.PDimension;
+
+/**
+ * @author Sam Reid
+ */
+public class PictureSceneNode extends PNode {
+    public PictureSceneNode( final Integer level, final PNode rootNode, final BuildAFractionModel model, final PDimension stageSize, final BuildAFractionCanvas buildAFractionCanvas ) {
+
+    }
+}
