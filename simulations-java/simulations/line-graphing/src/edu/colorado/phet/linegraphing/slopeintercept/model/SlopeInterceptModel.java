0471fa1dbcc7b9cb924c9d2bcd6758ae919a2174
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-06-12 17:21:27 +0000

    more refactoring of model-container classes

--- simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/slopeintercept/model/SlopeInterceptModel.java
+++ simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/slopeintercept/model/SlopeInterceptModel.java
@@ -3,6 +3,7 @@ package edu.colorado.phet.linegraphing.slopeintercept.model;
 
 import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.phetcommon.util.DoubleRange;
+import edu.colorado.phet.common.phetcommon.util.IntegerRange;
 import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
 import edu.colorado.phet.linegraphing.common.LGColors;
 import edu.colorado.phet.linegraphing.common.model.LineFormsModel;
@@ -16,37 +17,38 @@ import edu.colorado.phet.linegraphing.common.model.WellDefinedLineProperty;
  */
 public class SlopeInterceptModel extends LineFormsModel {
 
-    private static final DoubleRange RISE_RANGE = new DoubleRange( -10, 10, 2 );
-    private static final DoubleRange RUN_RANGE = new DoubleRange( -10, 10, 3 );
-    private static final DoubleRange INTERCEPT_RANGE = new DoubleRange( -10, 10, 1 );
+    private static final IntegerRange X_RANGE = new IntegerRange( -10, 10 );
+    private static final IntegerRange Y_RANGE = X_RANGE;
 
     public final Property<DoubleRange> riseRange, runRange, interceptRange;
 
     public SlopeInterceptModel() {
-        this( RISE_RANGE, RUN_RANGE, INTERCEPT_RANGE );
+        this( X_RANGE, Y_RANGE, 2, 3, 1 );
     }
 
-    private SlopeInterceptModel( DoubleRange riseRange, DoubleRange runRange, DoubleRange interceptRange ) {
-        super( new WellDefinedLineProperty( new StraightLine( riseRange.getDefault(), runRange.getDefault(), interceptRange.getDefault(),
-                                                           LGColors.INTERACTIVE_LINE, LGColors.INTERACTIVE_LINE ) ) );
+    private SlopeInterceptModel( final IntegerRange xRange, final IntegerRange yRange, int rise, int run, int intercept ) {
+        super( xRange, yRange,
+               new WellDefinedLineProperty( new StraightLine( rise, run, intercept, LGColors.INTERACTIVE_LINE, LGColors.INTERACTIVE_LINE ) ) );
 
-        this.riseRange = new Property<DoubleRange>( riseRange );
-        this.runRange = new Property<DoubleRange>( runRange );
-        this.interceptRange = new Property<DoubleRange>( interceptRange );
+        assert( yRange.contains( intercept ) );
+
+        this.riseRange = new Property<DoubleRange>( new DoubleRange( yRange.getMin(), yRange.getMax(), rise ) );
+        this.runRange = new Property<DoubleRange>( new DoubleRange( xRange.getMin(), xRange.getMax(), run ) );
+        this.interceptRange = new Property<DoubleRange>( new DoubleRange( yRange.getMin(), yRange.getMax(), intercept ) );
 
         // Dynamically adjust ranges so that variables are constrained to the bounds of the graph.
         interactiveLine.addObserver( new VoidFunction1<StraightLine>() {
             public void apply( StraightLine line ) {
 
                 // rise
-                final double minRise = graph.yRange.getMin() - line.yIntercept;
-                final double maxRise = graph.yRange.getMax() - line.yIntercept;
-                SlopeInterceptModel.this.riseRange.set( new DoubleRange( minRise, maxRise ) );
+                final double minRise = yRange.getMin() - line.yIntercept;
+                final double maxRise = yRange.getMax() - line.yIntercept;
+                riseRange.set( new DoubleRange( minRise, maxRise ) );
 
                 // y intercept
-                final double minIntercept = ( line.rise >= 0 ) ? graph.yRange.getMin() : graph.yRange.getMin() - line.rise;
-                final double maxIntercept = ( line.rise <= 0 ) ? graph.yRange.getMax() : graph.yRange.getMax() - line.rise;
-                SlopeInterceptModel.this.interceptRange.set( new DoubleRange( minIntercept, maxIntercept ) );
+                final double minIntercept = ( line.rise >= 0 ) ? yRange.getMin() : yRange.getMin() - line.rise;
+                final double maxIntercept = ( line.rise <= 0 ) ? yRange.getMax() : yRange.getMax() - line.rise;
+                interceptRange.set( new DoubleRange( minIntercept, maxIntercept ) );
             }
         } );
     }
