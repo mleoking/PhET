38b7753fa2097f3adec5542c7217c4946983df5e
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-06-12 16:50:29 +0000

    refactor duplication in model-container classes for first 2 tabs

--- trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/pointslope/model/PointSlopeModel.java
+++ trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/pointslope/model/PointSlopeModel.java
@@ -1,120 +1,99 @@
 // Copyright 2002-2012, University of Colorado
 package edu.colorado.phet.linegraphing.pointslope.model;
 
-import java.awt.geom.Point2D;
-
-import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
-import edu.colorado.phet.common.phetcommon.model.Resettable;
 import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.phetcommon.util.DoubleRange;
-import edu.colorado.phet.common.phetcommon.util.IntegerRange;
-import edu.colorado.phet.common.phetcommon.util.ObservableList;
 import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
-import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
 import edu.colorado.phet.linegraphing.common.LGColors;
-import edu.colorado.phet.linegraphing.common.model.Graph;
-import edu.colorado.phet.linegraphing.common.model.PointTool;
+import edu.colorado.phet.linegraphing.common.model.LineFormsModel;
 import edu.colorado.phet.linegraphing.common.model.StraightLine;
 
-//TODO lots of duplication with SlopeInterceptModel
 /**
  * Model for the "Point Slope" module.
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
-public class PointSlopeModel implements Resettable {
+public class PointSlopeModel extends LineFormsModel {
 
-    private static final int GRID_VIEW_UNITS = 530; // max dimension of the grid in the view
-    private static final int GRID_MODEL_UNITS = 10; // dimensions of the grid in the model
+    private static final DoubleRange RISE_RANGE = new DoubleRange( -10, 10, 3 );
+    private static final DoubleRange RUN_RANGE = new DoubleRange( -10, 10, 4 );
+    private static final DoubleRange X1_RANGE = new DoubleRange( -10, 10, 1 );
+    private static final DoubleRange Y1_RANGE = new DoubleRange( -10, 10, 2 );
 
-    private static final IntegerRange X_RANGE = new IntegerRange( -GRID_MODEL_UNITS, GRID_MODEL_UNITS );
-    private static final IntegerRange Y_RANGE = X_RANGE;
-    private static final double MVT_SCALE = GRID_VIEW_UNITS / Math.max( X_RANGE.getLength(), Y_RANGE.getLength() ); // view units / model units
-
-    public final ModelViewTransform mvt;
     public final Property<DoubleRange> riseRange, runRange, x1Range, y1Range;
-    public final Property<StraightLine> interactiveLine;
-    public final ObservableList<StraightLine> savedLines;
-    public final ObservableList<StraightLine> standardLines;
-    public final Graph graph;
-    public final PointTool pointTool1, pointTool2;
 
     public PointSlopeModel() {
+        this( RISE_RANGE, RUN_RANGE, X1_RANGE, Y1_RANGE );
+    }
 
-        mvt = ModelViewTransform.createOffsetScaleMapping( new Point2D.Double( 1.2 * GRID_VIEW_UNITS / 2, 1.25 * GRID_VIEW_UNITS / 2 ), MVT_SCALE, -MVT_SCALE ); // y is inverted
+    private PointSlopeModel( DoubleRange riseRange, DoubleRange runRange, DoubleRange x1Range, DoubleRange y1Range ) {
+        super( new StraightLineProperty( new StraightLine( riseRange.getDefault(), runRange.getDefault(), x1Range.getDefault(), y1Range.getDefault(),
+                                                           LGColors.INTERACTIVE_LINE, LGColors.INTERACTIVE_LINE ) ) );
+        this.riseRange = new Property<DoubleRange>( riseRange );
+        this.runRange = new Property<DoubleRange>( runRange );
+        this.x1Range = new Property<DoubleRange>( x1Range );
+        this.y1Range = new Property<DoubleRange>( y1Range );
 
-        riseRange = new Property<DoubleRange>( new DoubleRange( -10, 10, 3 ) );
-        runRange = new Property<DoubleRange>( new DoubleRange( -10, 10, 4 ) );
-        x1Range = new Property<DoubleRange>( new DoubleRange( -10, 10, 1 ) );
-        y1Range = new Property<DoubleRange>( new DoubleRange( -10, 10, 2 ) );
-
-        interactiveLine = new Property<StraightLine>( new StraightLine( riseRange.get().getDefault(), runRange.get().getDefault(), x1Range.get().getDefault(), y1Range.get().getDefault(),
-                                                                        LGColors.INTERACTIVE_LINE, LGColors.INTERACTIVE_LINE ) ) {
-            @Override public void set( StraightLine line ) {
-                double newRise = line.rise;
-                double newRun = line.run;
-                // Skip over values that would result in slope=0/0
-                if ( line.rise == 0 && line.run == 0 ) {
-                    if ( get().run != 0 ) {
-                        // run changed, skip over run = 0
-                        newRun = ( get().run > 0 ) ? -1 : 1;
-                    }
-                    else if ( get().rise != 0 ) {
-                        // rise changed, skip over rise = 0
-                        newRise = ( get().rise > 0 ) ? -1 : 1;
-                    }
-                    else {
-                        // rise and run haven't changed, arbitrarily move rise toward origin
-                        newRise = ( get().y1 > 0 ) ? -1 : 1;
-                    }
-                }
-                super.set( new StraightLine( newRise, newRun, line.x1,  line.y1, line.color, line.highlightColor ) );
-            }
-        };
-        savedLines = new ObservableList<StraightLine>();
-        standardLines = new ObservableList<StraightLine>();
-
-        graph = new Graph( X_RANGE, Y_RANGE );
-
-        pointTool1 = new PointTool( new ImmutableVector2D( X_RANGE.getMin() + ( 0.75 * X_RANGE.getLength() ), Y_RANGE.getMin() - 3 ), interactiveLine, savedLines, standardLines );
-        pointTool2 = new PointTool( new ImmutableVector2D( X_RANGE.getMin() + ( 0.25 * X_RANGE.getLength() ), pointTool1.location.get().getY() ), interactiveLine, savedLines, standardLines );
-
-        // Dynamically set ranges so that variables are constrained to the bounds of the graph.
+        // Dynamically adjust ranges so that variables are constrained to the bounds of the graph.
         interactiveLine.addObserver( new VoidFunction1<StraightLine>() {
             public void apply( StraightLine line ) {
 
                 // rise
                 final double minRise = graph.yRange.getMin() - line.y1;
                 final double maxRise = graph.yRange.getMax() - line.y1;
-                riseRange.set( new DoubleRange( minRise, maxRise ) );
+                PointSlopeModel.this.riseRange.set( new DoubleRange( minRise, maxRise ) );
 
                 // run
                 final double minRun = graph.xRange.getMin() - line.x1;
                 final double maxRun = graph.xRange.getMax() - line.x1;
-                runRange.set( new DoubleRange( minRun, maxRun ) );
+                PointSlopeModel.this.runRange.set( new DoubleRange( minRun, maxRun ) );
 
                 // x1
                 final double minX1 = Math.max( graph.xRange.getMin(), graph.xRange.getMin() - line.run );
                 final double maxX1 = Math.min( graph.xRange.getMax(), graph.xRange.getMax() - line.run );
-                x1Range.set( new DoubleRange( minX1, maxX1 ) );
+                PointSlopeModel.this.x1Range.set( new DoubleRange( minX1, maxX1 ) );
 
                 // y1
                 final double minY1 = Math.max( graph.yRange.getMin(), graph.yRange.getMin() - line.rise );
                 final double maxY1 = Math.min( graph.yRange.getMax(), graph.yRange.getMax() - line.rise );
-                y1Range.set( new DoubleRange( minY1, maxY1 ) );
+                PointSlopeModel.this.y1Range.set( new DoubleRange( minY1, maxY1 ) );
             }
         } );
     }
 
-    public void reset() {
+    @Override public void reset() {
+        super.reset();
         riseRange.reset();
         runRange.reset();
         x1Range.reset();
         y1Range.reset();
-        interactiveLine.reset();
-        savedLines.clear();
-        standardLines.clear();
-        pointTool1.reset();
-        pointTool2.reset();
+    }
+
+    private static class StraightLineProperty extends Property<StraightLine> {
+
+        public StraightLineProperty( StraightLine line ) {
+            super( line );
+        }
+
+        @Override public void set( StraightLine line ) {
+            double newRise = line.rise;
+            double newRun = line.run;
+            // Skip over values that would result in slope=0/0
+            if ( line.rise == 0 && line.run == 0 ) {
+                if ( get().run != 0 ) {
+                    // run changed, skip over run = 0
+                    newRun = ( get().run > 0 ) ? -1 : 1;
+                }
+                else if ( get().rise != 0 ) {
+                    // rise changed, skip over rise = 0
+                    newRise = ( get().rise > 0 ) ? -1 : 1;
+                }
+                else {
+                    // rise and run haven't changed, arbitrarily move rise toward origin
+                    newRise = ( get().y1 > 0 ) ? -1 : 1;
+                }
+            }
+            super.set( new StraightLine( newRise, newRun, line.x1, line.y1, line.color, line.highlightColor ) );
+        }
     }
 }
--- trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/slopeintercept/model/SlopeInterceptModel.java
+++ trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/slopeintercept/model/SlopeInterceptModel.java
@@ -1,19 +1,11 @@
 // Copyright 2002-2012, University of Colorado
 package edu.colorado.phet.linegraphing.slopeintercept.model;
 
-import java.awt.geom.Point2D;
-
-import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
-import edu.colorado.phet.common.phetcommon.model.Resettable;
 import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.phetcommon.util.DoubleRange;
-import edu.colorado.phet.common.phetcommon.util.IntegerRange;
-import edu.colorado.phet.common.phetcommon.util.ObservableList;
 import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
-import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
 import edu.colorado.phet.linegraphing.common.LGColors;
-import edu.colorado.phet.linegraphing.common.model.Graph;
-import edu.colorado.phet.linegraphing.common.model.PointTool;
+import edu.colorado.phet.linegraphing.common.model.LineFormsModel;
 import edu.colorado.phet.linegraphing.common.model.StraightLine;
 
 /**
@@ -21,87 +13,75 @@ import edu.colorado.phet.linegraphing.common.model.StraightLine;
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
-public class SlopeInterceptModel implements Resettable {
+public class SlopeInterceptModel extends LineFormsModel {
 
-    private static final int GRID_VIEW_UNITS = 530; // max dimension of the grid in the view
-    private static final int GRID_MODEL_UNITS = 10; // dimensions of the grid in the model
+    private static final DoubleRange RISE_RANGE = new DoubleRange( -10, 10, 2 );
+    private static final DoubleRange RUN_RANGE = new DoubleRange( -10, 10, 3 );
+    private static final DoubleRange INTERCEPT_RANGE = new DoubleRange( -10, 10, 1 );
 
-    private static final IntegerRange X_RANGE = new IntegerRange( -GRID_MODEL_UNITS, GRID_MODEL_UNITS );
-    private static final IntegerRange Y_RANGE = X_RANGE;
-    private static final double MVT_SCALE = GRID_VIEW_UNITS / Math.max( X_RANGE.getLength(), Y_RANGE.getLength() ); // view units / model units
-
-    public final ModelViewTransform mvt;
     public final Property<DoubleRange> riseRange, runRange, interceptRange;
-    public final Property<StraightLine> interactiveLine;
-    public final ObservableList<StraightLine> savedLines;
-    public final ObservableList<StraightLine> standardLines;
-    public final Graph graph;
-    public final PointTool pointTool1, pointTool2;
 
     public SlopeInterceptModel() {
+        this( RISE_RANGE, RUN_RANGE, INTERCEPT_RANGE );
+    }
 
-        mvt = ModelViewTransform.createOffsetScaleMapping( new Point2D.Double( 1.2 * GRID_VIEW_UNITS / 2, 1.25 * GRID_VIEW_UNITS / 2 ), MVT_SCALE, -MVT_SCALE ); // y is inverted
+    private SlopeInterceptModel( DoubleRange riseRange, DoubleRange runRange, DoubleRange interceptRange ) {
+        super( new StraightLineProperty( new StraightLine( riseRange.getDefault(), runRange.getDefault(), interceptRange.getDefault(),
+                                                           LGColors.INTERACTIVE_LINE, LGColors.INTERACTIVE_LINE ) ) );
 
-        riseRange = new Property<DoubleRange>( new DoubleRange( -10, 10, 2 ) );
-        runRange = new Property<DoubleRange>( new DoubleRange( -10, 10, 3 ) );
-        interceptRange = new Property<DoubleRange>( new DoubleRange( -10, 10, 1 ) );
+        this.riseRange = new Property<DoubleRange>( riseRange );
+        this.runRange = new Property<DoubleRange>( runRange );
+        this.interceptRange = new Property<DoubleRange>( interceptRange );
 
-        interactiveLine = new Property<StraightLine>( new StraightLine( riseRange.get().getDefault(), runRange.get().getDefault(), interceptRange.get().getDefault(),
-                                                                        LGColors.INTERACTIVE_LINE, LGColors.INTERACTIVE_LINE ) ) {
-            @Override public void set( StraightLine line ) {
-                double newRise = line.rise;
-                double newRun = line.run;
-                // Skip over values that would result in slope=0/0
-                if ( line.rise == 0 && line.run == 0 ) {
-                    if ( get().run != 0 ) {
-                        // run changed, skip over run = 0
-                        newRun = ( get().run > 0 ) ? -1 : 1;
-                    }
-                    else if ( get().rise != 0 ) {
-                        // rise changed, skip over rise = 0
-                        newRise = ( get().rise > 0 ) ? -1 : 1;
-                    }
-                    else {
-                        // rise and run haven't changed, arbitrarily move rise toward origin
-                        newRise = ( get().yIntercept > 0 ) ? -1 : 1;
-                    }
-                }
-                super.set( new StraightLine( newRise, newRun, line.yIntercept, line.color, line.highlightColor ) );
-            }
-        };
-        savedLines = new ObservableList<StraightLine>();
-        standardLines = new ObservableList<StraightLine>();
-
-        graph = new Graph( X_RANGE, Y_RANGE );
-
-        pointTool1 = new PointTool( new ImmutableVector2D( X_RANGE.getMin() + ( 0.75 * X_RANGE.getLength() ), Y_RANGE.getMin() - 3 ), interactiveLine, savedLines, standardLines );
-        pointTool2 = new PointTool( new ImmutableVector2D( X_RANGE.getMin() + ( 0.25 * X_RANGE.getLength() ), pointTool1.location.get().getY() ), interactiveLine, savedLines, standardLines );
-
-        // Dynamically set ranges so that variables are constrained to the bounds of the graph.
+        // Dynamically adjust ranges so that variables are constrained to the bounds of the graph.
         interactiveLine.addObserver( new VoidFunction1<StraightLine>() {
             public void apply( StraightLine line ) {
 
                 // rise
                 final double minRise = graph.yRange.getMin() - line.yIntercept;
                 final double maxRise = graph.yRange.getMax() - line.yIntercept;
-                riseRange.set( new DoubleRange( minRise, maxRise ) );
+                SlopeInterceptModel.this.riseRange.set( new DoubleRange( minRise, maxRise ) );
 
                 // y intercept
                 final double minIntercept = ( line.rise >= 0 ) ? graph.yRange.getMin() : graph.yRange.getMin() - line.rise;
                 final double maxIntercept = ( line.rise <= 0 ) ? graph.yRange.getMax() : graph.yRange.getMax() - line.rise;
-                interceptRange.set( new DoubleRange( minIntercept, maxIntercept ) );
+                SlopeInterceptModel.this.interceptRange.set( new DoubleRange( minIntercept, maxIntercept ) );
             }
         } );
     }
 
-    public void reset() {
+    @Override public void reset() {
+        super.reset();
         riseRange.reset();
         runRange.reset();
         interceptRange.reset();
-        interactiveLine.reset();
-        savedLines.clear();
-        standardLines.clear();
-        pointTool1.reset();
-        pointTool2.reset();
+    }
+
+    private static class StraightLineProperty extends Property<StraightLine> {
+
+        public StraightLineProperty( StraightLine line ) {
+            super( line );
+        }
+
+        @Override public void set( StraightLine line ) {
+            double newRise = line.rise;
+            double newRun = line.run;
+            // Skip over values that would result in slope=0/0
+            if ( line.rise == 0 && line.run == 0 ) {
+                if ( get().run != 0 ) {
+                    // run changed, skip over run = 0
+                    newRun = ( get().run > 0 ) ? -1 : 1;
+                }
+                else if ( get().rise != 0 ) {
+                    // rise changed, skip over rise = 0
+                    newRise = ( get().rise > 0 ) ? -1 : 1;
+                }
+                else {
+                    // rise and run haven't changed, arbitrarily move rise toward origin
+                    newRise = ( get().yIntercept > 0 ) ? -1 : 1;
+                }
+            }
+            super.set( new StraightLine( newRise, newRun, line.yIntercept, line.color, line.highlightColor ) );
+        }
     }
 }
