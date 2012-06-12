1da3b54aa5638ff3203280d675f20cc856f560dd
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-06-12 18:12:32 +0000

    refactor duplication in canvas classes for first 2 tabs

--- simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/pointslope/view/PointSlopeCanvas.java
+++ simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/pointslope/view/PointSlopeCanvas.java
@@ -1,78 +1,43 @@
 // Copyright 2002-2012, University of Colorado
 package edu.colorado.phet.linegraphing.pointslope.view;
 
-import java.awt.Color;
-
-import edu.colorado.phet.common.phetcommon.model.Resettable;
 import edu.colorado.phet.common.phetcommon.model.property.Property;
-import edu.colorado.phet.common.piccolophet.nodes.ResetAllButtonNode;
-import edu.colorado.phet.linegraphing.common.LGColors;
-import edu.colorado.phet.linegraphing.common.LGConstants;
-import edu.colorado.phet.linegraphing.common.view.GraphControls;
-import edu.colorado.phet.linegraphing.common.view.LGCanvas;
-import edu.colorado.phet.linegraphing.common.view.PointToolNode;
+import edu.colorado.phet.linegraphing.common.view.LineFormsCanvas;
 import edu.colorado.phet.linegraphing.pointslope.model.PointSlopeModel;
-import edu.umd.cs.piccolo.PNode;
-
-//TODO lots of duplication with SlopeInterceptCanvas
 
 /**
  * Canvas for the "Point Slope" module.
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
-public class PointSlopeCanvas extends LGCanvas {
+public class PointSlopeCanvas extends LineFormsCanvas {
 
-    private final Property<Boolean> linesVisible = new Property<Boolean>( true );
-    private final Property<Boolean> interactiveLineVisible = new Property<Boolean>( true );
-    private final Property<Boolean> interactiveEquationVisible = new Property<Boolean>( true );
-    private final Property<Boolean> slopeVisible = new Property<Boolean>( true );
-
+    // Constructor with default properties
     public PointSlopeCanvas( final PointSlopeModel model ) {
-
-        // nodes
-        PNode graphNode = new PointSlopeGraphNode( model.graph, model.mvt, model.interactiveLine, model.savedLines, model.standardLines,
-                                                   model.riseRange, model.runRange, model.x1Range, model.y1Range,
-                                                   interactiveEquationVisible, linesVisible, interactiveLineVisible, slopeVisible );
-        PNode pointTool1 = new PointToolNode( model.pointTool1, model.mvt, model.graph, getStageBounds(), linesVisible );
-        PNode pointTool2 = new PointToolNode( model.pointTool2, model.mvt, model.graph, getStageBounds(), linesVisible );
-        PNode equationControls = new PointSlopeEquationControls( interactiveEquationVisible, model.interactiveLine,
-                                                                 model.savedLines, linesVisible, model.riseRange, model.runRange, model.x1Range, model.y1Range
-        );
-        PNode graphControls = new GraphControls( linesVisible, slopeVisible, model.standardLines );
-        PNode resetAllButtonNode = new ResetAllButtonNode( new Resettable[] { this, model }, null, LGConstants.CONTROL_FONT_SIZE, Color.BLACK, LGColors.RESET_ALL_BUTTON ) {{
-            setConfirmationEnabled( false );
-        }};
-
-        // rendering order
-        {
-            addChild( graphNode );
-            addChild( equationControls );
-            addChild( graphControls );
-            addChild( resetAllButtonNode );
-            addChild( pointTool1 );
-            addChild( pointTool2 );
-        }
-
-        // layout
-        {
-            // NOTE: Nodes that have corresponding model elements handle their own offsets.
-            final int ySpacing = 25;
-            // upper-right of graph
-            equationControls.setOffset( graphNode.getFullBoundsReference().getMaxX(), 50 );
-            // centered below equation controls
-            graphControls.setOffset( equationControls.getFullBoundsReference().getCenterX() - ( graphControls.getFullBoundsReference().getWidth() / 2 ),
-                                     equationControls.getFullBoundsReference().getMaxY() + ySpacing );
-            // centered below graph controls
-            resetAllButtonNode.setOffset( equationControls.getFullBoundsReference().getCenterX() - ( resetAllButtonNode.getFullBoundsReference().getWidth() / 2 ),
-                                          graphControls.getFullBoundsReference().getMaxY() + ySpacing );
-        }
-        centerRootNodeOnStage();
+        this( model,
+              new Property<Boolean>( true ),
+              new Property<Boolean>( true ),
+              new Property<Boolean>( true ),
+              new Property<Boolean>( true ) );
     }
 
-    public void reset() {
-        interactiveLineVisible.reset();
-        linesVisible.reset();
-        slopeVisible.reset();
+    /*
+     * Constructor
+     * @param model
+     * @param linesVisible are lines visible on the graph?
+     * @param interactiveLineVisible is the interactive line visible visible on the graph?
+     * @param interactiveEquationVisible is the interactive equation visible?
+     * @param slopeVisible are the slope (rise/run) brackets visible on the graphed line?
+     */
+    private PointSlopeCanvas( final PointSlopeModel model,
+                              Property<Boolean> linesVisible, Property<Boolean> interactiveLineVisible,
+                              Property<Boolean> interactiveEquationVisible, Property<Boolean> slopeVisible ) {
+        super( model,
+               linesVisible, interactiveLineVisible, interactiveEquationVisible, slopeVisible,
+               new PointSlopeGraphNode( model.graph, model.mvt, model.interactiveLine, model.savedLines, model.standardLines,
+                                        model.riseRange, model.runRange, model.x1Range, model.y1Range,
+                                        interactiveEquationVisible, linesVisible, interactiveLineVisible, slopeVisible ),
+               new PointSlopeEquationControls( interactiveEquationVisible, model.interactiveLine,
+                                               model.savedLines, linesVisible, model.riseRange, model.runRange, model.x1Range, model.y1Range ) );
     }
 }
