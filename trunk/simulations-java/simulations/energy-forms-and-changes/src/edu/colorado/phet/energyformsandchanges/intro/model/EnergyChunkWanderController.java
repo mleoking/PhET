ed2a65042235181cab15bc2a7434c1773eb8a487
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-06-19 17:38:34 +0000

    Separated the motion strategy for wandering energy chunks into its own class, added a destination to the constructor.  This is to make it available to other classes.

---
+++ trunk/simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/intro/model/EnergyChunkWanderController.java
@@ -0,0 +1,72 @@
+// Copyright 2002-2012, University of Colorado
+package edu.colorado.phet.energyformsandchanges.intro.model;
+
+import java.util.Random;
+
+import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
+import edu.colorado.phet.common.phetcommon.math.Vector2D;
+
+/**
+ * This class is used to make an energy chunk wander, i.e. to perform somewhat
+ * of a random walk while moving towards a destination.
+ *
+ * @author John Blanco
+ */
+public final class EnergyChunkWanderController {
+
+    //-------------------------------------------------------------------------
+    // Class Data
+    //-------------------------------------------------------------------------
+
+    private static final double MIN_VELOCITY = 0.03; // In m/s.
+    private static final double MAX_VELOCITY = 0.06; // In m/s.
+    private static final Random RAND = new Random();
+    private static final double MIN_TIME_IN_ONE_DIRECTION = 0.5;
+    private static final double MAX_TIME_IN_ONE_DIRECTION = 1;
+    private static final double DISTANCE_AT_WHICH_TO_STOP_WANDERING = 0.1; // In meters, empirically chosen.
+    private static final double MAX_ANGLE_VARIATION = Math.PI * 0.3; // Max deviation from angle to destination, in radians, empirically chosen.
+
+    //-------------------------------------------------------------------------
+    // Instance Data
+    //-------------------------------------------------------------------------
+
+    private final ImmutableVector2D destination;
+    private Vector2D velocity = new Vector2D( 0, MAX_VELOCITY );
+    private Vector2D currentPosition = new Vector2D();
+    private double countdownTimer = 0;
+
+    //-------------------------------------------------------------------------
+    // Constructor(s)
+    //-------------------------------------------------------------------------
+
+    public EnergyChunkWanderController( ImmutableVector2D startingPoint, ImmutableVector2D destination ) {
+        this.destination = destination;
+        currentPosition.setValue( startingPoint );
+        updateVelocity();
+    }
+
+    //-------------------------------------------------------------------------
+    // Methods
+    //-------------------------------------------------------------------------
+
+    public void setNextPosition( EnergyChunk energyChunk, double dt ) {
+        energyChunk.position.set( energyChunk.position.get().getAddedInstance( velocity.getScaledInstance( dt ) ) );
+        countdownTimer -= dt;
+        if ( countdownTimer <= 0 ) {
+            updateVelocity();
+            countdownTimer = MIN_TIME_IN_ONE_DIRECTION + ( MAX_TIME_IN_ONE_DIRECTION - MIN_TIME_IN_ONE_DIRECTION ) * RAND.nextDouble();
+        }
+    }
+
+    private void updateVelocity() {
+        ImmutableVector2D vectorToDestination = destination.getSubtractedInstance( currentPosition );
+        double angle = vectorToDestination.getAngle();
+        if ( vectorToDestination.getMagnitude() > DISTANCE_AT_WHICH_TO_STOP_WANDERING ) {
+            // Add some randomness to the direction of travel.
+            angle = angle + ( RAND.nextDouble() - 0.5 ) * 2 * MAX_ANGLE_VARIATION;
+
+        }
+        double scalarVelocity = MIN_VELOCITY + ( MAX_VELOCITY - MIN_VELOCITY ) * RAND.nextDouble();
+        velocity.setComponents( scalarVelocity * Math.cos( angle ), scalarVelocity * Math.sin( angle ) );
+    }
+}
