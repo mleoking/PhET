5287ff88e95daeb5c3c531d0f2bb33240975f590
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-06-20 02:40:56 +0000

    Added a level and fixed layout bug

--- trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/buildafraction/model/pictures/PictureLevel.java
+++ trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractionsintro/buildafraction/model/pictures/PictureLevel.java
@@ -1,5 +1,6 @@
 package edu.colorado.phet.fractionsintro.buildafraction.model.pictures;
 
+import fj.F;
 import fj.data.List;
 
 import edu.colorado.phet.common.phetcommon.model.property.Property;
@@ -18,6 +19,15 @@ public class PictureLevel {
     public final List<Integer> containers;
     public final List<PictureTarget> targets;
     public final List<Integer> pieces;
+
+    //Cannot be a constructor because has same erasure
+    public static PictureLevel pictureLevel( final List<Integer> containers, final List<Integer> pieces, final List<Fraction> targets ) {
+        return new PictureLevel( containers, pieces, targets.map( new F<Fraction, PictureTarget>() {
+            @Override public PictureTarget f( final Fraction fraction ) {
+                return new PictureTarget( fraction );
+            }
+        } ) );
+    }
 
     public PictureLevel( final List<Integer> containers, final List<Integer> pieces, final List<PictureTarget> targets ) {
         this.containers = containers;
