86a5f3c28832755c43381009f678f9eff291fdf3
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-06-25 03:42:21 +0000

    Finished prototype for function creation

--- simulations-java/simulations/functions/src/edu/colorado/phet/functions/buildafunction/FunctionMirrorNode.java
+++ simulations-java/simulations/functions/src/edu/colorado/phet/functions/buildafunction/FunctionMirrorNode.java
@@ -17,16 +17,18 @@ import edu.umd.cs.piccolo.PNode;
 public class FunctionMirrorNode extends PNode {
     private final ArrayList<Vector2D> points = new ArrayList<Vector2D>();
     private final PhetPPath linePath;
+    private final DraggableToken token;
 
-    public FunctionMirrorNode( final DraggableToken x ) {
+    public FunctionMirrorNode( final DraggableToken token ) {
+        this.token = token;
         linePath = new PhetPPath( new BasicStroke( 1 ), Color.black );
         addChild( linePath );
 
         //X is the input variable the user is dragging through.  This node is a smaller mirror of the same thing.
-        final DraggableToken copy = new DraggableToken( x.value, null );
-        x.addPropertyChangeListener( PROPERTY_FULL_BOUNDS, new PropertyChangeListener() {
+        final DraggableToken copy = new DraggableToken( token.value, null );
+        token.addPropertyChangeListener( PROPERTY_FULL_BOUNDS, new PropertyChangeListener() {
             public void propertyChange( final PropertyChangeEvent evt ) {
-                final Vector2D v = Vector2D.v( x.getXOffset() + x.getFullBounds().getWidth() / 2, x.getYOffset() + x.getFullBounds().getHeight() / 2 );
+                final Vector2D v = Vector2D.v( token.getXOffset() + token.getFullBounds().getWidth() / 2, token.getYOffset() + token.getFullBounds().getHeight() / 2 );
                 points.add( v );
                 if ( points.size() == 1 ) {
                     linePath.moveTo( (float) v.x, (float) v.y );
@@ -34,12 +36,12 @@ public class FunctionMirrorNode extends PNode {
                 else {
                     linePath.lineTo( (float) v.x, (float) v.y );
                 }
-                copy.setOffset( x.getXOffset(), x.getYOffset() );
+                copy.setOffset( token.getXOffset(), token.getYOffset() );
             }
         } );
         addChild( copy );
 
-        x.addListener( new Listener() {
+        token.addListener( new Listener() {
             public void functionApplied( final DraggableToken t, final FunctionBoxWithText function ) {
                 FunctionBoxWithText a = new FunctionBoxWithText( function.getText() ) {
                     {
@@ -53,5 +55,9 @@ public class FunctionMirrorNode extends PNode {
                 addChild( a );
             }
         } );
+    }
+
+    public Object evaluate( final Object v ) {
+        return token.evaluateAsFunction( v );
     }
 }
