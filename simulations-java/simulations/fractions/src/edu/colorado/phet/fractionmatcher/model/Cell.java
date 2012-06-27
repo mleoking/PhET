b006e4506a1596c852f6e71a8da88b42c790ed8f
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-06-27 16:38:22 +0000

    #3360, add REVIEW comment about problem with field name and comment

--- simulations-java/simulations/fractions/src/edu/colorado/phet/fractionmatcher/model/Cell.java
+++ simulations-java/simulations/fractions/src/edu/colorado/phet/fractionmatcher/model/Cell.java
@@ -16,6 +16,7 @@ import edu.colorado.phet.fractions.util.immutable.Vector2D;
 @Data public class Cell {
     public final ImmutableRectangle2D rectangle;
 
+    //REVIEW you removed field j, now this comment is wrong and we don't know if i is row or column index.
     //Index of the site in the grid, top left is (0,0)
     public final int i;
 
