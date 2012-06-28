613cb8f953f0aeb01e41cfd76616e8725b425893
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-06-28 04:23:12 +0000

    Parameterized MatchingGameModel to accept different level factories

--- trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractionmatcher/model/MatchingGameModel.java
+++ trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractionmatcher/model/MatchingGameModel.java
@@ -39,21 +39,10 @@ public class MatchingGameModel {
     //System that plays audio when the user makes a correct guess (and audio is enabled)
     private final GameAudioPlayer gameAudioPlayer = new GameAudioPlayer( true );
 
-    public final AbstractLevelFactory levelFactory = new IntroLevelFactory();
+    public final AbstractLevelFactory levelFactory;
 
     //State of the game, including position of all objects.
-    public final Property<MatchingGameState> state = new Property<MatchingGameState>( initialState( levelFactory ) ) {{
-        addObserver( new ChangeObserver<MatchingGameState>() {
-            public void update( final MatchingGameState newValue, final MatchingGameState oldValue ) {
-                if ( newValue.info.audio && oldValue.info.mode == Mode.USER_IS_MOVING_OBJECTS_TO_THE_SCALES && newValue.info.mode == Mode.USER_CHECKED_CORRECT_ANSWER ) {
-                    gameAudioPlayer.correctAnswer();
-                }
-                if ( newValue.info.audio && oldValue.info.mode == Mode.USER_IS_MOVING_OBJECTS_TO_THE_SCALES && newValue.info.mode == Mode.SHOWING_WHY_ANSWER_WRONG ) {
-                    gameAudioPlayer.wrongAnswer();
-                }
-            }
-        } );
-    }};
+    public final Property<MatchingGameState> state;
 
     //Clock that runs the sim.
     public final Clock clock = new ConstantDtClock( 60.0 ) {{
@@ -70,92 +59,30 @@ public class MatchingGameModel {
 
     //Public interface for observable properties on the sequence of immutable model states
     //This is meant to be read with IDEA's closure folding feature, so that each declaration takes one line
-    public final ObservableProperty<Double> leftScaleValue = doubleProperty( new F<MatchingGameState, Double>() {
-        @Override public Double f( final MatchingGameState s ) {
-            return s.getLeftScaleValue();
-        }
-    } );
-    public final ObservableProperty<Double> rightScaleValue = doubleProperty( new F<MatchingGameState, Double>() {
-        @Override public Double f( final MatchingGameState s ) {
-            return s.getRightScaleValue();
-        }
-    } );
-    public final ObservableProperty<Integer> checks = intProperty( new F<MatchingGameState, Integer>() {
-        @Override public Integer f( final MatchingGameState s ) {
-            return s.getChecks();
-        }
-    } );
-    public final ObservableProperty<Integer> scored = intProperty( new F<MatchingGameState, Integer>() {
-        @Override public Integer f( final MatchingGameState s ) {
-            return s.scored;
-        }
-    } );
-    public final ObservableProperty<Integer> level = intProperty( new F<MatchingGameState, Integer>() {
-        @Override public Integer f( final MatchingGameState s ) {
-            return s.info.level;
-        }
-    } );
-    public final ObservableProperty<Integer> score = intProperty( new F<MatchingGameState, Integer>() {
-        @Override public Integer f( final MatchingGameState s ) {
-            return s.info.score;
-        }
-    } );
-    public final ObservableProperty<Boolean> timerVisible = booleanProperty( new F<MatchingGameState, Boolean>() {
-        @Override public Boolean f( final MatchingGameState s ) {
-            return s.info.timerVisible;
-        }
-    } );
-    public final ObservableProperty<Double> barGraphAnimationTime = doubleProperty( new F<MatchingGameState, Double>() {
-        @Override public Double f( final MatchingGameState s ) {
-            return s.barGraphAnimationTime;
-        }
-    } );
-    public final ObservableProperty<Mode> mode = new CompositeProperty<Mode>( new Function0<Mode>() {
-        public Mode apply() {
-            return state.get().getMode();
-        }
-    }, state );
+    public final ObservableProperty<Double> leftScaleValue;
+    public final ObservableProperty<Double> rightScaleValue;
+    public final ObservableProperty<Integer> checks;
+    public final ObservableProperty<Integer> scored;
+    public final ObservableProperty<Integer> level;
+    public final ObservableProperty<Integer> score;
+    public final ObservableProperty<Boolean> timerVisible;
+    public final ObservableProperty<Double> barGraphAnimationTime;
+    public final ObservableProperty<Mode> mode;
 
     //Model property for whether the model should be revealing the clues (bar chart for fractions)
-    public final ObservableProperty<Boolean> revealClues = new CompositeBooleanProperty( new Function0<Boolean>() {
-        public Boolean apply() {
-            return state.get().getMode() == Mode.SHOWING_WHY_ANSWER_WRONG ||
-                   state.get().getMode() == Mode.USER_CHECKED_CORRECT_ANSWER ||
-                   state.get().getMode() == Mode.SHOWING_CORRECT_ANSWER_AFTER_INCORRECT_GUESS;
-        }
-    }, state );
+    public final ObservableProperty<Boolean> revealClues;
 
-    public final ObservableProperty<Long> timeInSec = new CompositeProperty<Long>( new Function0<Long>() {
-        public Long apply() {
-            return state.get().info.time / 1000L;
-        }
-    }, state );
+    //Elapsed time
+    public final ObservableProperty<Long> timeInSec;
 
-    public final ObservableProperty<List<Integer>> fractionIDs = new CompositeProperty<List<Integer>>( new Function0<List<Integer>>() {
-        public List<Integer> apply() {
-            return state.get().fractions.map( new F<MovableFraction, Integer>() {
-                @Override public Integer f( final MovableFraction m ) {
-                    return m.id.value;
-                }
-            } );
-        }
-    }, state );
+    //IDs of the fractions in play
+    public final ObservableProperty<List<Integer>> fractionIDs;
 
     //Property for the list of game results
-    public final Property<List<GameResult>> gameResults = new Property<List<GameResult>>( state.get().gameResults ) {{
-        state.addObserver( new VoidFunction1<MatchingGameState>() {
-            public void apply( final MatchingGameState matchingGameState ) {
-                set( state.get().gameResults );
-            }
-        } );
-    }};
+    public final Property<List<GameResult>> gameResults;
 
     //True if the user is choosing settings
-    public final CompositeBooleanProperty choosingSettings = new CompositeBooleanProperty( new Function0<Boolean>() {
-        public Boolean apply() {
-            return state.get().info.mode == Mode.CHOOSING_SETTINGS;
-        }
-    }, state );
+    public final CompositeBooleanProperty choosingSettings;
 
     private ObservableProperty<Double> doubleProperty( final F<MatchingGameState, Double> f ) {
         return new CompositeDoubleProperty( new Function0<Double>() {
@@ -181,7 +108,103 @@ public class MatchingGameModel {
         }, state );
     }
 
-    public MatchingGameModel() {
+    public MatchingGameModel( AbstractLevelFactory levelFactory ) {
+        this.levelFactory = levelFactory;
+        this.state = new Property<MatchingGameState>( initialState( levelFactory ) ) {{
+            addObserver( new ChangeObserver<MatchingGameState>() {
+                public void update( final MatchingGameState newValue, final MatchingGameState oldValue ) {
+                    if ( newValue.info.audio && oldValue.info.mode == Mode.USER_IS_MOVING_OBJECTS_TO_THE_SCALES && newValue.info.mode == Mode.USER_CHECKED_CORRECT_ANSWER ) {
+                        gameAudioPlayer.correctAnswer();
+                    }
+                    if ( newValue.info.audio && oldValue.info.mode == Mode.USER_IS_MOVING_OBJECTS_TO_THE_SCALES && newValue.info.mode == Mode.SHOWING_WHY_ANSWER_WRONG ) {
+                        gameAudioPlayer.wrongAnswer();
+                    }
+                }
+            } );
+        }};
+        this.mode = new CompositeProperty<Mode>( new Function0<Mode>() {
+            public Mode apply() {
+                return state.get().getMode();
+            }
+        }, state );
+        this.revealClues = new CompositeBooleanProperty( new Function0<Boolean>() {
+            public Boolean apply() {
+                return state.get().getMode() == Mode.SHOWING_WHY_ANSWER_WRONG ||
+                       state.get().getMode() == Mode.USER_CHECKED_CORRECT_ANSWER ||
+                       state.get().getMode() == Mode.SHOWING_CORRECT_ANSWER_AFTER_INCORRECT_GUESS;
+            }
+        }, state );
+
+        this.timeInSec = new CompositeProperty<Long>( new Function0<Long>() {
+            public Long apply() {
+                return state.get().info.time / 1000L;
+            }
+        }, state );
+
+        this.fractionIDs = new CompositeProperty<List<Integer>>( new Function0<List<Integer>>() {
+            public List<Integer> apply() {
+                return state.get().fractions.map( new F<MovableFraction, Integer>() {
+                    @Override public Integer f( final MovableFraction m ) {
+                        return m.id.value;
+                    }
+                } );
+            }
+        }, state );
+
+        gameResults = new Property<List<GameResult>>( state.get().gameResults ) {{
+            state.addObserver( new VoidFunction1<MatchingGameState>() {
+                public void apply( final MatchingGameState matchingGameState ) {
+                    set( state.get().gameResults );
+                }
+            } );
+        }};
+
+        choosingSettings = new CompositeBooleanProperty( new Function0<Boolean>() {
+            public Boolean apply() {
+                return state.get().info.mode == Mode.CHOOSING_SETTINGS;
+            }
+        }, state );
+
+        leftScaleValue = doubleProperty( new F<MatchingGameState, Double>() {
+            @Override public Double f( final MatchingGameState s ) {
+                return s.getLeftScaleValue();
+            }
+        } );
+        rightScaleValue = doubleProperty( new F<MatchingGameState, Double>() {
+            @Override public Double f( final MatchingGameState s ) {
+                return s.getRightScaleValue();
+            }
+        } );
+        checks = intProperty( new F<MatchingGameState, Integer>() {
+            @Override public Integer f( final MatchingGameState s ) {
+                return s.getChecks();
+            }
+        } );
+        scored = intProperty( new F<MatchingGameState, Integer>() {
+            @Override public Integer f( final MatchingGameState s ) {
+                return s.scored;
+            }
+        } );
+        level = intProperty( new F<MatchingGameState, Integer>() {
+            @Override public Integer f( final MatchingGameState s ) {
+                return s.info.level;
+            }
+        } );
+        score = intProperty( new F<MatchingGameState, Integer>() {
+            @Override public Integer f( final MatchingGameState s ) {
+                return s.info.score;
+            }
+        } );
+        timerVisible = booleanProperty( new F<MatchingGameState, Boolean>() {
+            @Override public Boolean f( final MatchingGameState s ) {
+                return s.info.timerVisible;
+            }
+        } );
+        barGraphAnimationTime = doubleProperty( new F<MatchingGameState, Double>() {
+            @Override public Double f( final MatchingGameState s ) {
+                return s.barGraphAnimationTime;
+            }
+        } );
 
         //Send model messages when the values on the scales change.
         leftScaleValue.addObserver( new VoidFunction1<Double>() {
