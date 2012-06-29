13d543af0e75de88203111a22dc1a0b1312523d9
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-06-29 13:38:55 +0000

    Build a Fraction: Added a "refresh" button, not wired up

---
+++ trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/buildafraction/view/pictures/SingleButton.java
@@ -0,0 +1,53 @@
+// Copyright 2002-2012, University of Colorado
+package edu.colorado.phet.buildafraction.view.pictures;
+
+import java.awt.image.BufferedImage;
+
+import edu.colorado.phet.common.phetcommon.util.function.VoidFunction0;
+import edu.colorado.phet.common.piccolophet.event.CursorHandler;
+import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
+import edu.umd.cs.piccolo.event.PInputEvent;
+import edu.umd.cs.piccolo.nodes.PImage;
+
+/**
+ * @author Sam Reid
+ */
+public class SingleButton extends PNode {
+    public SingleButton( final BufferedImage buttonImage, final BufferedImage pressedButtonImage, final VoidFunction0 effect ) {
+        final PImage node = new PImage( buttonImage );
+        addInputEventListener( new CursorHandler() );
+        addInputEventListener( new PBasicInputEventHandler() {
+            boolean pressed = false;
+
+            @Override public void mousePressed( final PInputEvent event ) {
+                node.setImage( pressedButtonImage );
+                pressed = true;
+            }
+
+            @Override public void mouseReleased( final PInputEvent event ) {
+                if ( node.getImage() == pressedButtonImage && pressed ) {
+                    effect.apply();
+                }
+                node.setImage( buttonImage );
+                pressed = false;
+            }
+
+            @Override public void mouseExited( final PInputEvent event ) {
+                node.setImage( buttonImage );
+            }
+
+            @Override public void mouseEntered( final PInputEvent event ) {
+                if ( pressed ) {
+                    node.setImage( pressedButtonImage );
+                }
+            }
+        } );
+        addChild( node );
+    }
+
+    public void setAllPickable( final boolean b ) {
+        setPickable( b );
+        setChildrenPickable( b );
+    }
+}
