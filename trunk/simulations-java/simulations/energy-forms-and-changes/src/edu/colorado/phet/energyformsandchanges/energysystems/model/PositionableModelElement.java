1c58b121cf589a664051c67576cba4a23ca9d540
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-07-11 21:49:18 +0000

    Added base class that will be used by the model elements that can be placed on the selection carousel.

---
+++ trunk/simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/energysystems/model/PositionableModelElement.java
@@ -0,0 +1,36 @@
+// Copyright 2002-2012, University of Colorado
+package edu.colorado.phet.energyformsandchanges.energysystems.model;
+
+import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
+import edu.colorado.phet.common.phetcommon.model.property.ObservableProperty;
+import edu.colorado.phet.common.phetcommon.model.property.Property;
+
+/**
+ * A model element that has a position (a.k.a. a location) which can be
+ * changed.
+ *
+ * @author John Blanco
+ */
+public class PositionableModelElement {
+    private final Property<ImmutableVector2D> position = new Property<ImmutableVector2D>( new ImmutableVector2D( 0, 0 ) );
+
+    public PositionableModelElement() {
+        // Default constructor - uses default initial position.
+    }
+
+    public PositionableModelElement( ImmutableVector2D initialPosition ) {
+        position.set( initialPosition );
+    }
+
+    public void setPosition( ImmutableVector2D newPosition ) {
+        position.set( newPosition );
+    }
+
+    public ImmutableVector2D getPosition() {
+        return new ImmutableVector2D( position.get() );
+    }
+
+    public ObservableProperty<ImmutableVector2D> getObservablePosition() {
+        return position;
+    }
+}
