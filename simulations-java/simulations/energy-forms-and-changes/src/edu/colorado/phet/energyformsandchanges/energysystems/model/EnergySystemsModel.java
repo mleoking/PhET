034d1dc660966d0dbc8767dc947065e1dafb15bc
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-07-11 23:01:00 +0000

    Added energy source/converter/user carousels to the model, added controllers to the view, and hooked 'em up.

--- simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/energysystems/model/EnergySystemsModel.java
+++ simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/energysystems/model/EnergySystemsModel.java
@@ -1,6 +1,7 @@
 // Copyright 2002-2012, University of Colorado
 package edu.colorado.phet.energyformsandchanges.energysystems.model;
 
+import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
 import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
 import edu.colorado.phet.common.phetcommon.model.clock.IClock;
 import edu.colorado.phet.common.phetcommon.model.property.BooleanProperty;
@@ -26,6 +27,12 @@ public class EnergySystemsModel {
 
     // Boolean property that controls whether the energy chunks are visible to the user.
     public final BooleanProperty energyChunksVisible = new BooleanProperty( false );
+
+    // Carousels that control the positions of the energy sources, converters,
+    // and users.
+    public final Carousel energySourcesCarousel = new Carousel( new ImmutableVector2D( 0, 0 ), new ImmutableVector2D( 0, 0 ) ); // TODO - Position and offset.
+    public final Carousel energyConvertersCarousel = new Carousel( new ImmutableVector2D( 0, 0 ), new ImmutableVector2D( 0, 0 ) ); // TODO - Position and offset.
+    public final Carousel energyUsersCarousel = new Carousel( new ImmutableVector2D( 0, 0 ), new ImmutableVector2D( 0, 0 ) ); // TODO - Position and offset.
 
     //-------------------------------------------------------------------------
     // Constructor(s)
