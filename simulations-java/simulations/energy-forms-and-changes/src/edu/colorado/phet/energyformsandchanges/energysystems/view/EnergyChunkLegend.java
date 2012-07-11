4e469498c1e63c5d62f2b576ef7f961f88792797
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-07-11 21:29:24 +0000

    Changed layout of legend so that chunks and captions would align better.

--- simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/energysystems/view/EnergyChunkLegend.java
+++ simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/energysystems/view/EnergyChunkLegend.java
@@ -3,15 +3,16 @@ package edu.colorado.phet.energyformsandchanges.energysystems.view;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.nodes.ControlPanelNode;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPText;
-import edu.colorado.phet.common.piccolophet.nodes.layout.HBox;
-import edu.colorado.phet.common.piccolophet.nodes.layout.VBox;
 import edu.colorado.phet.energyformsandchanges.EnergyFormsAndChangesResources;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.nodes.PImage;
+import edu.umd.cs.piccolox.swing.SwingLayoutNode;
 
 /**
  * Legend for the various different energy chunks, i.e. thermal, electrical,
@@ -23,12 +24,46 @@ public class EnergyChunkLegend extends PNode {
     private static final Font LEGEND_ENTRY_FONT = new PhetFont( 14 );
 
     public EnergyChunkLegend() {
-        PNode contents = new VBox(
-                new PhetPText( "Forms of Energy", new PhetFont( 14, true ) ),
-                new HBox( new PImage( EnergyFormsAndChangesResources.Images.E_MECH_OUTLINE ), new PhetPText( "Mechanical", LEGEND_ENTRY_FONT ) ),
-                new HBox( new PImage( EnergyFormsAndChangesResources.Images.E_ELECTRIC_OUTLINE ), new PhetPText( "Electrical", LEGEND_ENTRY_FONT ) ),
-                new HBox( new PImage( EnergyFormsAndChangesResources.Images.E_THERM_OUTLINE ), new PhetPText( "Thermal", LEGEND_ENTRY_FONT ) )
-        );
+        PNode contents = new SwingLayoutNode( new GridBagLayout() ) {{
+            // TODO: i18n of all text in this node.
+            GridBagConstraints constraints = new GridBagConstraints();
+            // Add the title.
+            constraints.gridx = 0;
+            constraints.gridy = 0;
+            constraints.gridwidth = 2;
+            addChild( new PhetPText( "Forms of Energy", new PhetFont( 14, true ) ), constraints );
+
+            // Add the legend entries.
+            constraints.gridy++;
+            constraints.gridwidth = 1;
+            constraints.gridx = 0;
+            constraints.anchor = GridBagConstraints.LINE_START;
+            constraints.insets.top = 10;
+            constraints.insets.left = 0;
+            addChild( new PImage( EnergyFormsAndChangesResources.Images.E_MECH_OUTLINE ), constraints );
+            constraints.gridx++;
+            constraints.insets.left = 5;
+            addChild( new PhetPText( "Mechanical", LEGEND_ENTRY_FONT ), constraints );
+
+            constraints.gridy++;
+            constraints.gridx = 0;
+            constraints.insets.top = 5;
+            constraints.insets.left = 0;
+            addChild( new PImage( EnergyFormsAndChangesResources.Images.E_ELECTRIC_OUTLINE ), constraints );
+            constraints.gridx++;
+            constraints.insets.left = 5;
+            addChild( new PhetPText( "Electrical", LEGEND_ENTRY_FONT ), constraints );
+
+            constraints.gridy++;
+            constraints.gridx = 0;
+            constraints.insets.left = 0;
+            addChild( new PImage( EnergyFormsAndChangesResources.Images.E_THERM_OUTLINE ), constraints );
+            constraints.gridx++;
+            constraints.insets.left = 5;
+            addChild( new PhetPText( "Thermal", LEGEND_ENTRY_FONT ), constraints );
+        }};
+
+        // Create the control panel.
         addChild( new ControlPanelNode( contents, new Color( 250, 250, 250 ) ) );
     }
 }
