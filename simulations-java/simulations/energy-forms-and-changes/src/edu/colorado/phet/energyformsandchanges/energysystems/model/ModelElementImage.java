c33f4115db7afe8eaa6c0c472d5be50aee82a02b
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-07-13 20:43:38 +0000

    Initial creation of this class.

---
+++ simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/energysystems/model/ModelElementImage.java
@@ -0,0 +1,39 @@
+// Copyright 2002-2012, University of Colorado
+package edu.colorado.phet.energyformsandchanges.energysystems.model;
+
+import java.awt.image.BufferedImage;
+
+import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
+
+/**
+ * Class that provides information about an image that is used in the view
+ * representation of a model element.
+ *
+ * @author John Blanco
+ */
+public class ModelElementImage {
+    private final BufferedImage image;
+    private final double width; // Width of the image in model units (meters).  Height is derived from aspect ratio of image.
+
+    // Offset in model units (meters) from the center of the posiion of the
+    // model element that owns this image to the center of the image.
+    private final ImmutableVector2D centerToCenterOffset;
+
+    public ModelElementImage( BufferedImage image, double width, ImmutableVector2D centerToCenterOffset ) {
+        this.image = image;
+        this.width = width;
+        this.centerToCenterOffset = centerToCenterOffset;
+    }
+
+    public BufferedImage getImage() {
+        return image;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public ImmutableVector2D getCenterToCenterOffset() {
+        return centerToCenterOffset;
+    }
+}
