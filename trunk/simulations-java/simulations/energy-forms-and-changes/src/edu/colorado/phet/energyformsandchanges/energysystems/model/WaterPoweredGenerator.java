0f617682e4bb1a0a399ab011a31f6ae293d8b0e0
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-07-13 22:26:39 +0000

    New class that represents water-powered generator in the model.

--- trunk/simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/energysystems/model/WaterPoweredGenerator.java
+++ trunk/simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/energysystems/model/WaterPoweredGenerator.java
@@ -1,6 +1,9 @@
 // Copyright 2002-2012, University of Colorado
 package edu.colorado.phet.energyformsandchanges.energysystems.model;
 
+import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
+import edu.colorado.phet.energyformsandchanges.EnergyFormsAndChangesResources;
+
 /**
  * This class represents an electrical generator that is powered by flowing
  * water or steam.
@@ -9,6 +12,9 @@ package edu.colorado.phet.energyformsandchanges.energysystems.model;
  */
 public class WaterPoweredGenerator extends EnergyConverter {
 
+    public WaterPoweredGenerator() {
+        super( new ModelElementImage( EnergyFormsAndChangesResources.Images.GENERATOR, 0.1, new ImmutableVector2D( 0, 0 ) ) );
+    }
 
     @Override public double stepInTime( double dt, double incomingEnergy ) {
         // TODO - Implement.
