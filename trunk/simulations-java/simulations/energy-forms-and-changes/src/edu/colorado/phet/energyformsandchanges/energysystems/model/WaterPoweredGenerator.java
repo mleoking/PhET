367daf3ea736e29273d76d7f2047d3c1fc0f57ff
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-07-13 21:54:34 +0000

    New class that represents water-powered generator in the model.

---
+++ trunk/simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/energysystems/model/WaterPoweredGenerator.java
@@ -0,0 +1,17 @@
+// Copyright 2002-2012, University of Colorado
+package edu.colorado.phet.energyformsandchanges.energysystems.model;
+
+/**
+ * This class represents an electrical generator that is powered by flowing
+ * water or steam.
+ *
+ * @author John Blanco
+ */
+public class WaterPoweredGenerator extends EnergyConverter {
+
+
+    @Override public double stepInTime( double dt, double incomingEnergy ) {
+        // TODO - Implement.
+        return 0;
+    }
+}
