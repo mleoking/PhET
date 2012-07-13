d00f694481956def18fbf8f121531951f4f98531
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-07-13 20:19:58 +0000

    7/13/12 set up basic "phases" of game, add dependency on common "games" module

--- trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/linegame/model/LineGameModel.java
+++ trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/linegame/model/LineGameModel.java
@@ -2,6 +2,7 @@
 package edu.colorado.phet.linegraphing.linegame.model;
 
 import edu.colorado.phet.common.phetcommon.model.Resettable;
+import edu.colorado.phet.common.phetcommon.model.property.Property;
 
 /**
  * Model for the "Line Game" module.
@@ -9,9 +10,22 @@ import edu.colorado.phet.common.phetcommon.model.Resettable;
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
 public class LineGameModel implements Resettable {
-    //TODO
+
+    // phases of a game
+    public enum GamePhase {
+        SETTINGS,  // user is choosing game settings
+        PLAY, // user is playing the game
+        RESULTS, // user is receiving results of playing the game
+        REWARD // user is receiving a reward for exceptional game play
+    }
+
+    public final Property<GamePhase> phase;
+
+    public LineGameModel() {
+        phase = new Property<GamePhase>( GamePhase.SETTINGS );
+    }
 
     public void reset() {
-        //TODO
+        phase.reset();
     }
 }
