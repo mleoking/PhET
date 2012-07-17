0c0a0ef24fdaa1d147b48ad86b70364102631e09
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-07-17 22:14:07 +0000

    minor refactoring

--- trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/buildafraction/view/pictures/Target.java
+++ trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/buildafraction/view/pictures/Target.java
@@ -9,7 +9,7 @@ import edu.umd.cs.piccolo.PNode;
 /**
  * @author Sam Reid
  */
-@Data class Target {
+public @Data class Target {
     public final PictureScoreBoxNode cell;
     public final PNode node;
     public final Fraction value;
