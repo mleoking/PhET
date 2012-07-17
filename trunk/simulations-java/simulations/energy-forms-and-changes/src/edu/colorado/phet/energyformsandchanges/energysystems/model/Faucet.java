78857fd28bde49d11d8af496937c4383652786a0
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-07-17 18:21:32 +0000

    Added initial version of faucet to model and view.

---
+++ trunk/simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/energysystems/model/Faucet.java
@@ -0,0 +1,33 @@
+// Copyright 2002-2012, University of Colorado
+package edu.colorado.phet.energyformsandchanges.energysystems.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
+import edu.colorado.phet.energyformsandchanges.EnergyFormsAndChangesResources;
+import edu.colorado.phet.energyformsandchanges.common.EFACConstants;
+
+import static edu.colorado.phet.energyformsandchanges.EnergyFormsAndChangesResources.Images.TEMP_FAUCET;
+
+/**
+ * Class that represents a faucet that can be turned on to provide mechanical
+ * energy to other energy system elements.
+ *
+ * @author John Blanco
+ */
+public class Faucet extends EnergySource {
+
+    private static final List<ModelElementImage> IMAGE_LIST = new ArrayList<ModelElementImage>() {{
+        add( new ModelElementImage( TEMP_FAUCET, TEMP_FAUCET.getWidth() / EFACConstants.ENERGY_SYSTEMS_MVT_SCALE_FACTOR, new ImmutableVector2D( -0.1, 0.1 ) ) );
+    }};
+
+    protected Faucet() {
+        super( EnergyFormsAndChangesResources.Images.FAUCET_ICON, IMAGE_LIST );
+    }
+
+    @Override public double stepInTime( double dt ) {
+        // TODO: Implement.
+        return 0;
+    }
+}
