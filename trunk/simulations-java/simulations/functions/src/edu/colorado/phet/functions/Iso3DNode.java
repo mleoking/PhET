1f6ec01fe2fdac681b4e3c425882d8d1baf6013f
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-07-19 21:58:28 +0000

    fixed broken build

--- trunk/simulations-java/simulations/functions/src/edu/colorado/phet/functions/Iso3DNode.java
+++ trunk/simulations-java/simulations/functions/src/edu/colorado/phet/functions/Iso3DNode.java
@@ -1,9 +1,5 @@
+// Copyright 2002-2012, University of Colorado
 package edu.colorado.phet.functions;
-
-import fj.data.List;
-import lombok.Data;
-
-import java.util.ArrayList;
 
 import edu.umd.cs.piccolo.PNode;
 
@@ -12,42 +8,42 @@ import edu.umd.cs.piccolo.PNode;
  *
  * @author Sam Reid
  */
-public class Iso3DNode {
-    public final ArrayList<ZFace> zNodes = new ArrayList<ZFace>();
-    public final ArrayList<YFace> yNodes = new ArrayList<YFace>();
-    public final ArrayList<XFace> xNodes = new ArrayList<XFace>();
+public class Iso3DNode extends PNode {
+//    public final ArrayList<ZFace> zNodes = new ArrayList<ZFace>();
+//    public final ArrayList<YFace> yNodes = new ArrayList<YFace>();
+//    public final ArrayList<XFace> xNodes = new ArrayList<XFace>();
+//
+//    public final List<Face> sort() {
+//        return null;
+//    }
 
-    public final List<Face> sort() {
-
-    }
-
-    public static @Data class Face {
-        public final PNode node;
-    }
-
-    private static @Data class ZFace extends Face {
-        public final double z;
-        public final Range xRange;
-        public final Range yRange;
-    }
-
-    private static @Data class YFace {
-        final double y;
-        final Range zRange;
-        final Range xRange;
-        final PNode node;
-    }
-
-    //Node that has a constant "x" value,
-    private static @Data class XFace {
-        final double x;
-        final Range zRange;
-        final Range yRange;
-        final PNode node;
-    }
-
-    private @Data static class Range {
-        public final double min;
-        public final double extent;
-    }
+//    public static @Data class Face {
+//        public final PNode node;
+//    }
+//
+//    private static @Data class ZFace extends Face {
+//        public final double z;
+//        public final Range xRange;
+//        public final Range yRange;
+//    }
+//
+//    private static @Data class YFace {
+//        final double y;
+//        final Range zRange;
+//        final Range xRange;
+//        final PNode node;
+//    }
+//
+//    //Node that has a constant "x" value,
+//    private static @Data class XFace {
+//        final double x;
+//        final Range zRange;
+//        final Range yRange;
+//        final PNode node;
+//    }
+//
+//    private @Data static class Range {
+//        public final double min;
+//        public final double extent;
+//    }
 }
