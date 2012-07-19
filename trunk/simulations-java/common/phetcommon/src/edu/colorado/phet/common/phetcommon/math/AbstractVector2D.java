f656084338b1fdb57a1115b0aaa33e8fb45d9f80
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-07-19 21:59:11 +0000

    7/19/12 Moved more methods from ImmutableVector2D to AbstractVector2D, see #3374

--- trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/math/AbstractVector2D.java
+++ trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/math/AbstractVector2D.java
@@ -1,8 +1,11 @@
 // Copyright 2002-2012, University of Colorado
 package edu.colorado.phet.common.phetcommon.math;
 
+import java.awt.geom.Dimension2D;
 import java.awt.geom.Point2D;
 import java.io.Serializable;
+
+import edu.colorado.phet.common.phetcommon.view.Dimension2DDouble;
 
 /**
  * Abstract base class for vector2d classes (mutable and immutable), see #3374
@@ -73,7 +76,26 @@ public abstract class AbstractVector2D implements Serializable {
         return Math.sqrt( dx * dx + dy * dy );
     }
 
+    public double distance( AbstractVector2D v ) {
+        double dx = this.getX() - v.getX();
+        double dy = this.getY() - v.getY();
+        return Math.sqrt( dx * dx + dy * dy );
+    }
+
     public double getDistance( Point2D point ) {
         return getDistance( new ImmutableVector2D( point ) );
+    }
+
+    //Transform this ImmutableVector2D into a Dimension2D
+    public Dimension2D toDimension() {
+        return new Dimension2DDouble( getX(), getY() );
+    }
+
+    public Point2D.Double getDestination( Point2D startPt ) {
+        return new Point2D.Double( startPt.getX() + getX(), startPt.getY() + getY() );
+    }
+
+    public double getCrossProductScalar( AbstractVector2D v ) {
+        return ( this.getMagnitude() * v.getMagnitude() * Math.sin( this.getAngle() - v.getAngle() ) );
     }
 }
