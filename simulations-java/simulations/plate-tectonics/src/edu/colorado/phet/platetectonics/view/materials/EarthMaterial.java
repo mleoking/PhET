3a9ac23256b872cc36f966dee00398e6316fbc81
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-07-20 22:12:57 +0000

    pt: materials / texture docs

--- simulations-java/simulations/plate-tectonics/src/edu/colorado/phet/platetectonics/view/materials/EarthMaterial.java
+++ simulations-java/simulations/plate-tectonics/src/edu/colorado/phet/platetectonics/view/materials/EarthMaterial.java
@@ -5,9 +5,14 @@ import java.awt.*;
 
 import edu.colorado.phet.lwjglphet.math.ImmutableVector2F;
 
+/**
+ * Base for a material (texture) for the cross-section of the Earth.
+ */
 public interface EarthMaterial {
+    // main color based on density, temperature, etc.
     public Color getColor( float density, float temperature, ImmutableVector2F position, float alpha );
 
+    // min/max colors used for the legend
     public Color getMinColor();
 
     public Color getMaxColor();
--- simulations-java/simulations/plate-tectonics/src/edu/colorado/phet/platetectonics/view/materials/TemperatureMaterial.java
+++ simulations-java/simulations/plate-tectonics/src/edu/colorado/phet/platetectonics/view/materials/TemperatureMaterial.java
@@ -7,40 +7,21 @@ import edu.colorado.phet.common.phetcommon.math.MathUtil;
 import edu.colorado.phet.lwjglphet.math.ImmutableVector2F;
 import edu.colorado.phet.platetectonics.model.CrustModel;
 
+/**
+ * Displays gray for "cool" and red for "warm", with a blend in-between
+ */
 public class TemperatureMaterial implements EarthMaterial {
-    private static final int width = 256;
-    private static final int height = 256;
-
     private static final Color min = new Color( 64, 64, 64 );
     private static final Color max = new Color( 255, 64, 64 );
 
     public static ImmutableVector2F temperatureMap( float temperature ) {
         float minTemp = CrustModel.ZERO_CELSIUS;
-        float maxTemp = 1500;
         float maxMaxTemp = CrustModel.ZERO_CELSIUS + 6100 + 300;
 
-//        float tempRatio = ( temperature - minTemp ) / ( maxTemp - minTemp );
-//        float x;
-//        float constant = 1300;
-//        if ( temperature <= 1300 ) {
-//            x = 100f + ( 1f - tempRatio ) * 155f;
-//        }
-//        else {
-//            float start = 100f + ( 1f - ( 1300 - minTemp ) / ( maxTemp - minTemp ) ) * 155f;
-//            float end = 50f;
-//            float ratio = ( temperature - 1300 ) / ( maxMaxTemp - 1300 );
-//            x = start + ( end - start ) * ratio;
-//        }
-//        x = (float) MathUtil.clamp( 0.0, x / 255.0, 1.0 ); // clamp it in the normal range
         float x = ( temperature - minTemp ) / ( maxMaxTemp - minTemp );
         x = (float) Math.pow( x, 0.4f );
         x = (float) MathUtil.clamp( 0.08, x, 0.95 );
         return new ImmutableVector2F( x, 0.5f );
-
-//        float minDensityToShow = 2500;
-//        float maxDensityToShow = CrustModel.CENTER_DENSITY;
-//        float v = 1 - ( density - minDensityToShow ) / ( maxDensityToShow - minDensityToShow );
-//        return new ImmutableVector2F( v, 0.5f );
     }
 
     public Color getColor( float density, float temperature, ImmutableVector2F position, float alpha ) {
