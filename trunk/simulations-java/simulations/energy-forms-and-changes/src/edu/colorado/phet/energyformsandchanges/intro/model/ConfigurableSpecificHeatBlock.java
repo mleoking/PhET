56b5a10edc6839f273207e29539a33945abd0dea
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-07-20 17:57:52 +0000

    Added settable/gettable properties for specific heat and color.  Also added comment blocks.

--- trunk/simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/intro/model/ConfigurableSpecificHeatBlock.java
+++ trunk/simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/intro/model/ConfigurableSpecificHeatBlock.java
@@ -2,13 +2,12 @@
 package edu.colorado.phet.energyformsandchanges.intro.model;
 
 import java.awt.Color;
-import java.awt.Image;
 
 import edu.colorado.phet.common.phetcommon.math.ImmutableVector2D;
 import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
 import edu.colorado.phet.common.phetcommon.model.property.BooleanProperty;
+import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.phetcommon.simsharing.messages.IUserComponent;
-import edu.colorado.phet.energyformsandchanges.EnergyFormsAndChangesResources;
 import edu.colorado.phet.energyformsandchanges.EnergyFormsAndChangesSimSharing;
 
 /**
@@ -18,39 +17,57 @@ import edu.colorado.phet.energyformsandchanges.EnergyFormsAndChangesSimSharing;
  */
 public class ConfigurableSpecificHeatBlock extends Block {
 
+    //-------------------------------------------------------------------------
+    // Class Data
+    //-------------------------------------------------------------------------
+
     private static final double INITIAL_SPECIFIC_HEAT = 840; // In J/kg-K, source = design document.
-    private static final double DENSITY = 2000; // In kg/m^3, source = design document.
+    private static final double DENSITY = 2000; // In kg/m^3.
+
+    public static final double MIN_SPECIFIC_HEAT = 100;
+    public static final double MAX_SPECIFIC_HEAT = 1000;
+
+    //-------------------------------------------------------------------------
+    // Instance Data
+    //-------------------------------------------------------------------------
+
+    // Specific heat, which is configurable in this block.
+    public final Property<Double> specificHeat = new Property<Double>( INITIAL_SPECIFIC_HEAT );
+
+    // Color changes as specific heat changes.
+    public final Property<Color> color = new Property<Color>( new Color( 200, 22, 11 ) );
+
+    //-------------------------------------------------------------------------
+    // Constructor(s)
+    //-------------------------------------------------------------------------
 
     protected ConfigurableSpecificHeatBlock( ConstantDtClock clock, ImmutableVector2D initialPosition, BooleanProperty energyChunksVisible ) {
         super( clock, initialPosition, DENSITY, INITIAL_SPECIFIC_HEAT, energyChunksVisible );
     }
 
-    @Override public Image getFrontTextureImage() {
-        return EnergyFormsAndChangesResources.Images.BRICK_TEXTURE_FRONT;
-    }
-
-    @Override public Image getTopTextureImage() {
-        return EnergyFormsAndChangesResources.Images.BRICK_TEXTURE_TOP;
-    }
-
-    @Override public Image getSideTextureImage() {
-        return EnergyFormsAndChangesResources.Images.BRICK_TEXTURE_RIGHT;
-    }
+    //-------------------------------------------------------------------------
+    // Methods
+    //-------------------------------------------------------------------------
 
     @Override public Color getColor() {
-        return new Color( 200, 22, 11 );
+        return color.get();
     }
 
     @Override public String getLabel() {
         // TODO: i18n
-        return "Brick";
+        return "Custom";
     }
 
     public EnergyContainerCategory getEnergyContainerCategory() {
-        return EnergyContainerCategory.BRICK;
+        return EnergyContainerCategory.CUSTOM;
     }
 
     @Override public IUserComponent getUserComponent() {
         return EnergyFormsAndChangesSimSharing.UserComponents.brick;
+    }
+
+    @Override public void reset() {
+        specificHeat.reset();
+        super.reset();
     }
 }
