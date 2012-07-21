38bb15272c3a81ea9909cff3c9d9edafdb54f524
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-07-21 04:29:01 +0000

    Improved pie shape containers in the toolbox

--- trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/buildafraction/view/pictures/PieceIconNode.java
+++ trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/buildafraction/view/pictures/PieceIconNode.java
@@ -3,13 +3,17 @@ package edu.colorado.phet.fractions.buildafraction.view.pictures;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Shape;
+import java.awt.geom.Ellipse2D;
 import java.awt.geom.Rectangle2D;
 
+import edu.colorado.phet.common.phetcommon.math.Vector2D;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
+import edu.colorado.phet.fractions.buildafraction.model.pictures.ShapeType;
+import edu.colorado.phet.fractions.fractionsintro.intro.model.pieset.factories.CircularShapeFunction;
 import edu.umd.cs.piccolo.PNode;
 
-import static edu.colorado.phet.fractions.buildafraction.view.pictures.SimpleContainerNode.rectangleHeight;
-import static edu.colorado.phet.fractions.buildafraction.view.pictures.SimpleContainerNode.rectangleWidth;
+import static edu.colorado.phet.fractions.buildafraction.view.pictures.SimpleContainerNode.*;
 
 /**
  * Shows an icon on the bucket with a single slice in the context of its container.
@@ -20,14 +24,21 @@ public class PieceIconNode extends PNode {
 
     public static final double TINY_SCALE = 0.43;
 
-    public PieceIconNode( int divisions ) {
+    public PieceIconNode( int divisions, ShapeType shapeType ) {
         for ( int i = 0; i < divisions; i++ ) {
-            final double pieceWidth = rectangleWidth / divisions;
-            addChild( new PhetPPath( new Rectangle2D.Double( pieceWidth * i, 0, pieceWidth, rectangleHeight ), Color.white, new BasicStroke( 1 ), Color.black ) );
+            addChild( new PhetPPath( shapeType == ShapeType.HORIZONTAL_BAR ? barShape( divisions, i ) : pieShape( divisions, i ), Color.white, new BasicStroke( 1 ), Color.black ) );
         }
         //Thicker outer stroke
-        addChild( new PhetPPath( new Rectangle2D.Double( 0, 0, rectangleWidth, rectangleHeight ), new BasicStroke( 2 ), Color.black ) );
+        addChild( new PhetPPath( shapeType == ShapeType.HORIZONTAL_BAR ? new Rectangle2D.Double( 0, 0, rectangleWidth, rectangleHeight ) :
+                                 new Ellipse2D.Double( 0, 0, circleDiameter, circleDiameter ), new BasicStroke( 2 ), Color.black ) );
 
         scale( TINY_SCALE );
+    }
+
+    private Shape pieShape( final int divisions, final int i ) {return new CircularShapeFunction( Math.PI * 2 / divisions, circleDiameter / 2 ).createShape( Vector2D.v( circleDiameter / 2, circleDiameter / 2 ), Math.PI * 2 / divisions * i );}
+
+    private Rectangle2D.Double barShape( final int divisions, final int i ) {
+        final double pieceWidth = rectangleWidth / divisions;
+        return new Rectangle2D.Double( pieceWidth * i, 0, pieceWidth, rectangleHeight );
     }
 }
