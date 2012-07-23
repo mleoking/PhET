247881ecb3c9e25f0e0b9b0e1bbb96090ca1dc51
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-07-23 15:00:47 +0000

    added a sample main

--- simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/fractionsintro/intro/model/pieset/factories/CircularShapeFunction.java
+++ simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/fractionsintro/intro/model/pieset/factories/CircularShapeFunction.java
@@ -4,11 +4,18 @@ package edu.colorado.phet.fractions.fractionsintro.intro.model.pieset.factories;
 import fj.F;
 import lombok.Data;
 
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Shape;
 import java.awt.geom.Arc2D;
 import java.awt.geom.Ellipse2D;
 
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
 import edu.colorado.phet.common.phetcommon.math.vector.Vector2D;
+import edu.colorado.phet.common.piccolophet.PhetPCanvas;
+import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.colorado.phet.fractions.fractionsintro.intro.model.pieset.Slice;
 
 /**
@@ -27,5 +34,21 @@ public @Data class CircularShapeFunction extends F<Slice, Shape> {
         return extent >= Math.PI * 2 - epsilon ?
                new Ellipse2D.Double( position.getX() - radius, position.getY() - radius, radius * 2, radius * 2 ) :
                new Arc2D.Double( position.getX() - radius, position.getY() - radius, radius * 2, radius * 2, angle * 180.0 / Math.PI, extent * 180.0 / Math.PI, Arc2D.PIE );
+    }
+
+    public static void main( String[] args ) {
+        SwingUtilities.invokeLater( new Runnable() {
+            public void run() {
+                new JFrame() {{
+                    setPreferredSize( new Dimension( 1024, 768 ) );
+                    setContentPane( new PhetPCanvas() {{
+                        addScreenChild( new PhetPPath( new CircularShapeFunction( Math.PI * 2, 100 ).createShape( Vector2D.v( 100, 100 ), 0 ), Color.red ) );
+                        addScreenChild( new PhetPPath( new CircularShapeFunction( Math.PI * 2 / 2, 100 ).createShape( Vector2D.v( 100, 100 ), 0 ), Color.green ) );
+                        addScreenChild( new PhetPPath( new CircularShapeFunction( Math.PI * 2 / 3, 100 ).createShape( Vector2D.v( 100, 100 ), 0 ), Color.blue ) );
+                        addScreenChild( new PhetPPath( new CircularShapeFunction( Math.PI * 2 / 4, 100 ).createShape( Vector2D.v( 100, 100 ), 0 ), Color.yellow ) );
+                    }} );
+                }}.setVisible( true );
+            }
+        } );
     }
 }
