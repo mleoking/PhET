d069eb87b91ec8abc7bf12193cced585650aeab7
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-07-25 20:51:48 +0000

    Added lockout timer to prevent the controls for heat/cool mode from changing too quickly.

--- simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/intro/view/BurnerNode.java
+++ simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/intro/view/BurnerNode.java
@@ -5,11 +5,14 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Point;
 import java.awt.Stroke;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.geom.Dimension2D;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 
 import javax.swing.JFrame;
+import javax.swing.Timer;
 
 import edu.colorado.phet.common.phetcommon.math.vector.Vector2D;
 import edu.colorado.phet.common.phetcommon.util.DoubleRange;
@@ -24,6 +27,8 @@ import edu.colorado.phet.energyformsandchanges.intro.view.stove.HeaterCoolerWith
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.util.PDimension;
 
+import static edu.colorado.phet.energyformsandchanges.intro.view.stove.HeaterCoolerWithLimitsNode.HeatCoolMode;
+
 /**
  * Piccolo node that represents a burner in the view.
  *
@@ -31,12 +36,28 @@ import edu.umd.cs.piccolo.util.PDimension;
  */
 public class BurnerNode extends PNode {
 
+    //-------------------------------------------------------------------------
+    // Class Data
+    //-------------------------------------------------------------------------
+
     private static final Stroke BURNER_STAND_STROKE = new BasicStroke( 4, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL );
     private static final Color BURNER_STAND_STROKE_COLOR = Color.BLACK;
     private static final double PERSPECTIVE_ANGLE = Math.PI / 4; // Positive is counterclockwise, a value of 0 produces a non-skewed rectangle.
     private static final double BURNER_EDGE_LENGTH = 30;
+    private static final double MODE_CHANGE_LOCKOUT_TIME = 3; // In seconds.
 
+    // For debug purposes.
     private static final boolean SHOW_2D_RECT = false;
+
+    //-------------------------------------------------------------------------
+    // Instance Data
+    //-------------------------------------------------------------------------
+
+    protected final HeaterCoolerWithLimitsNode heaterCoolerNode;
+
+    //-------------------------------------------------------------------------
+    // Constructor(s)
+    //-------------------------------------------------------------------------
 
     public BurnerNode( final Burner burner, final ModelViewTransform mvt ) {
 
@@ -50,25 +71,36 @@ public class BurnerNode extends PNode {
 
         // Add the heater-cooler node to the center bottom.
         // TODO: i18n
-        final HeaterCoolerWithLimitsNode heaterCoolerNode = new HeaterCoolerWithLimitsNode( burner.heatCoolLevel, "Heat", "Cool" ) {{
+        heaterCoolerNode = new HeaterCoolerWithLimitsNode( burner.heatCoolLevel, "Heat", "Cool" ) {{
             setScale( mvt.modelToViewDeltaX( burner.getOutlineRect().getWidth() ) * 0.7 / getFullBoundsReference().width );
             setOffset( burnerViewRect.getX() + burnerViewRect.getWidth() / 2 - getFullBoundsReference().width / 2,
                        burnerViewRect.getMaxY() - getFullBoundsReference().height * 0.9 );
         }};
         addChild( heaterCoolerNode );
 
-        // TODO: Comment
+        // Set up the lockout timer that prevents the heat/cool mode from
+        // changing too rapidly.
+        final Timer heatCoolModeChangeLockoutTimer = new Timer( (int) ( MODE_CHANGE_LOCKOUT_TIME * 1000 ), null );
+        heatCoolModeChangeLockoutTimer.addActionListener( new ActionListener() {
+            public void actionPerformed( ActionEvent e ) {
+                updateHeatCoolMode( burner.heatCoolLevel.getBoundsProperty().get() );
+                heatCoolModeChangeLockoutTimer.stop();
+            }
+        } );
+
+        // Update the mode of the heater cooler based on the limits on the burner.
         burner.heatCoolLevel.getBoundsProperty().addObserver( new VoidFunction1<DoubleRange>() {
             public void apply( DoubleRange allowedRange ) {
-                if ( allowedRange.getMin() == 0 && heaterCoolerNode.heatCoolMode.get() != HeaterCoolerWithLimitsNode.HeatCoolMode.HEAT_ONLY ) {
-                    heaterCoolerNode.heatCoolMode.set( HeaterCoolerWithLimitsNode.HeatCoolMode.HEAT_ONLY );
+                if ( !heatCoolModeChangeLockoutTimer.isRunning() ) {
+                    // Change is not locked out, so an update is allowed.
+                    HeatCoolMode modeBeforeUpdate = heaterCoolerNode.heatCoolMode.get();
+                    updateHeatCoolMode( allowedRange );
+                    if ( heaterCoolerNode.heatCoolMode.get() != modeBeforeUpdate ) {
+                        // A mode change occurred, so start the lockout timer.
+                        heatCoolModeChangeLockoutTimer.restart();
+                    }
                 }
-                else if ( allowedRange.getMax() == 0 && heaterCoolerNode.heatCoolMode.get() != HeaterCoolerWithLimitsNode.HeatCoolMode.COOL_ONLY ) {
-                    heaterCoolerNode.heatCoolMode.set( HeaterCoolerWithLimitsNode.HeatCoolMode.COOL_ONLY );
-                }
-                else if ( heaterCoolerNode.heatCoolMode.get() != HeaterCoolerWithLimitsNode.HeatCoolMode.HEAT_AND_COOL ) {
-                    heaterCoolerNode.heatCoolMode.set( HeaterCoolerWithLimitsNode.HeatCoolMode.HEAT_AND_COOL );
-                }
+                // else ignore the change and let the timer handle it.
             }
         } );
 
@@ -95,6 +127,26 @@ public class BurnerNode extends PNode {
             }
         } );
     }
+
+    //-------------------------------------------------------------------------
+    // Methods
+    //-------------------------------------------------------------------------
+
+    private void updateHeatCoolMode( DoubleRange allowedRange ) {
+        if ( allowedRange.getMin() == 0 && heaterCoolerNode.heatCoolMode.get() != HeatCoolMode.HEAT_ONLY ) {
+            heaterCoolerNode.heatCoolMode.set( HeatCoolMode.HEAT_ONLY );
+        }
+        else if ( allowedRange.getMax() == 0 && heaterCoolerNode.heatCoolMode.get() != HeatCoolMode.COOL_ONLY ) {
+            heaterCoolerNode.heatCoolMode.set( HeatCoolMode.COOL_ONLY );
+        }
+        else if ( allowedRange.getMin() < 0 && allowedRange.getMax() > 0 && heaterCoolerNode.heatCoolMode.get() != HeatCoolMode.HEAT_AND_COOL ) {
+            heaterCoolerNode.heatCoolMode.set( HeatCoolMode.HEAT_AND_COOL );
+        }
+    }
+
+    //-------------------------------------------------------------------------
+    // Inner Classes and Interfaces
+    //-------------------------------------------------------------------------
 
     private static class BurnerStandSide extends PNode {
 
@@ -154,10 +206,8 @@ public class BurnerNode extends PNode {
         }
     }
 
-    /**
-     * Main routine that constructs a PhET Piccolo canvas in a window.
-     *
-     * @param args
+    /*
+     * Test harness.
      */
     public static void main( String[] args ) {
 
