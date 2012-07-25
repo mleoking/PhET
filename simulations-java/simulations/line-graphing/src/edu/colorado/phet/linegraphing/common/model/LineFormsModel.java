7d16fd65050635e23b9f1511b12046da3f5478ae
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-07-25 18:13:36 +0000

    7/25/12 change PointTool to use a single list of lines, so it can be reused in the game

--- simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/common/model/LineFormsModel.java
+++ simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/common/model/LineFormsModel.java
@@ -5,8 +5,10 @@ import java.awt.geom.Point2D;
 
 import edu.colorado.phet.common.phetcommon.math.vector.Vector2D;
 import edu.colorado.phet.common.phetcommon.model.Resettable;
+import edu.colorado.phet.common.phetcommon.model.property.ChangeObserver;
 import edu.colorado.phet.common.phetcommon.util.IntegerRange;
 import edu.colorado.phet.common.phetcommon.util.ObservableList;
+import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
 
 /**
@@ -43,8 +45,35 @@ public class LineFormsModel implements Resettable {
 
         this.graph = new Graph( xRange, yRange );
 
-        this.pointTool1 = new PointTool( new Vector2D( xRange.getMin() + ( 0.75 * xRange.getLength() ), yRange.getMin() - 3 ), interactiveLine, savedLines, standardLines );
-        this.pointTool2 = new PointTool( new Vector2D( xRange.getMin() + ( 0.25 * xRange.getLength() ), pointTool1.location.get().getY() ), interactiveLine, savedLines, standardLines );
+        // Observable collection of all lines, required by point tool.
+        final ObservableList<StraightLine> allLines = new ObservableList<StraightLine>();
+        {
+            interactiveLine.addObserver( new ChangeObserver<StraightLine>() {
+                public void update( StraightLine newLine, StraightLine oldLine ) {
+                    allLines.add( newLine ); // add interactive line to end, so we find it last
+                    if ( oldLine != null ) {
+                        allLines.remove( oldLine );
+                    }
+                }
+            } );
+            final VoidFunction1<StraightLine> elementAddedObserver = new VoidFunction1<StraightLine>() {
+                public void apply( StraightLine line ) {
+                    allLines.add( 0, line ); // add saved and standard lines to from, so we find them first
+                }
+            };
+            final VoidFunction1<StraightLine> elementRemovedObserver = new VoidFunction1<StraightLine>() {
+                public void apply( StraightLine line ) {
+                    allLines.remove( line );
+                }
+            };
+            savedLines.addElementAddedObserver( elementAddedObserver );
+            savedLines.addElementRemovedObserver( elementRemovedObserver );
+            standardLines.addElementAddedObserver( elementAddedObserver );
+            standardLines.addElementRemovedObserver( elementRemovedObserver );
+        }
+
+        this.pointTool1 = new PointTool( new Vector2D( xRange.getMin() + ( 0.75 * xRange.getLength() ), yRange.getMin() - 3 ), allLines );
+        this.pointTool2 = new PointTool( new Vector2D( xRange.getMin() + ( 0.25 * xRange.getLength() ), pointTool1.location.get().getY() ), allLines );
     }
 
     public void reset() {
