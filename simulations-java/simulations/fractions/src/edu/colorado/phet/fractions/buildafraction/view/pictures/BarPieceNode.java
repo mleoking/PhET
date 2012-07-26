65f88f71e6c2901033c31203e163eebdceed1efa
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-07-26 15:34:48 +0000

    Build a Fraction: Factored out subclasses for PieceNode

---
+++ simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/buildafraction/view/pictures/BarPieceNode.java
@@ -0,0 +1,37 @@
+// Copyright 2002-2012, University of Colorado
+package edu.colorado.phet.fractions.buildafraction.view.pictures;
+
+import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
+import edu.colorado.phet.common.piccolophet.nodes.kit.ZeroOffsetNode;
+import edu.colorado.phet.fractions.buildafraction.model.pictures.ShapeType;
+import edu.colorado.phet.fractions.fractionsintro.intro.view.pieset.ShapeNode;
+
+/**
+ * @author Sam Reid
+ */
+public class BarPieceNode extends PieceNode {
+    private PhetPPath barShadow;
+    private ZeroOffsetNode barShadowNode;
+
+    public BarPieceNode( final int pieceDenominator, final PictureSceneNode pictureSceneNode, final PhetPPath shape ) {
+        super( pieceDenominator, pictureSceneNode, shape, ShapeType.HORIZONTAL_BAR );
+
+        barShadow = new PhetPPath( this.pathNode.getPathReference(), ShapeNode.SHADOW_PAINT );
+        barShadowNode = new ZeroOffsetNode( barShadow ) {{
+            setOffset( getShadowOffset().getTranslateX(), getShadowOffset().getTranslateY() );
+        }};
+        addChild( new ZeroOffsetNode( this.pathNode ) );
+
+        installInputListeners();
+    }
+
+    @Override protected void dragStarted() {
+        addChild( 0, barShadowNode );
+    }
+
+    @Override protected void dragEnded() {
+        while ( getChildrenReference().contains( barShadowNode ) ) {
+            removeChild( barShadowNode );
+        }
+    }
+}
