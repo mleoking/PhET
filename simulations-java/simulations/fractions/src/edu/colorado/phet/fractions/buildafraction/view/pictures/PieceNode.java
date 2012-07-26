4c913155f15c71e96a22af0df0391eb0158f5bea
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-07-26 15:34:22 +0000

    Build a Fraction: Added shadows for dragged pie pieces

--- simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/buildafraction/view/pictures/PieceNode.java
+++ simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/buildafraction/view/pictures/PieceNode.java
@@ -6,9 +6,9 @@ import fj.data.Option;
 
 import java.awt.BasicStroke;
 import java.awt.Shape;
+import java.awt.geom.AffineTransform;
 
 import edu.colorado.phet.common.phetcommon.math.Function.LinearFunction;
-import edu.colorado.phet.common.phetcommon.math.vector.Vector2D;
 import edu.colorado.phet.common.piccolophet.event.CursorHandler;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.colorado.phet.common.piccolophet.nodes.kit.ZeroOffsetNode;
@@ -18,10 +18,15 @@ import edu.colorado.phet.fractions.buildafraction.view.BuildAFractionCanvas;
 import edu.colorado.phet.fractions.buildafraction.view.Stackable;
 import edu.colorado.phet.fractions.fractionsintro.intro.model.Fraction;
 import edu.colorado.phet.fractions.fractionsintro.intro.model.pieset.factories.CircularShapeFunction;
+import edu.colorado.phet.fractions.fractionsintro.intro.view.pieset.ShapeNode;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.activities.PInterpolatingActivity;
 import edu.umd.cs.piccolo.event.PInputEvent;
 import edu.umd.cs.piccolo.util.PDimension;
+
+import static edu.colorado.phet.common.phetcommon.math.vector.Vector2D.ZERO;
+import static edu.colorado.phet.fractions.buildafraction.view.pictures.SimpleContainerNode.circleDiameter;
+import static java.awt.geom.AffineTransform.getTranslateInstance;
 
 /**
  * @author Sam Reid
@@ -33,32 +38,44 @@ public class PieceNode extends Stackable {
     private final ShapeType shapeType;
     public static final BasicStroke stroke = new BasicStroke( 2 );
     double pieceRotation = 0.0;
+    private PhetPPath pieShadow;
+    private PNode pieBackground;
 
     public PieceNode( final Integer pieceSize, final PieceContext context, PhetPPath shape, final ShapeType shapeType ) {
         this.pieceSize = pieceSize;
         pathNode = shape;
         this.shapeType = shapeType;
 
+//        shadowNode = new PhetPPath( pathNode.getPathReference(), ShapeNode.SHADOW_PAINT );
+//        final ZeroOffsetNode shadow = new ZeroOffsetNode( shadowNode );
+//        shadow.translate( 6, 6 );
+//        shadow.setVisible( false );
+
         if ( shapeType == ShapeType.HORIZONTAL_BAR ) {
+
+//            addChild( shadow );
 
             PNode piece = new ZeroOffsetNode( pathNode );
             addChild( piece );
         }
         else {
-            final PhetPPath b = new PhetPPath( SimpleContainerNode.createPieSlice( 1 ), BuildAFractionCanvas.TRANSPARENT );
-
-            PNode background = new PNode() {{
-                addChild( b );
+            pieBackground = new PNode() {{
+                addChild( new PhetPPath( SimpleContainerNode.createPieSlice( 1 ), BuildAFractionCanvas.TRANSPARENT ) );
             }};
-            addChild( new ZeroOffsetNode( background ) );
-            addChild( pathNode );
-            background.addChild( pathNode );
+            addChild( new ZeroOffsetNode( pieBackground ) );
+            pieShadow = new PhetPPath( getShadowOffset().createTransformedShape( pathNode.getPathReference() ),
+                                       ShapeNode.SHADOW_PAINT );
+            pieBackground.addChild( pathNode );
         }
 
         addInputEventListener( new CursorHandler() );
         addInputEventListener( new SimSharingDragHandler( null, true ) {
             @Override protected void startDrag( final PInputEvent event ) {
                 super.startDrag( event );
+
+                if ( shapeType == ShapeType.PIE ) {
+                    pieBackground.addChild( 0, pieShadow );
+                }
                 PieceNode.this.moveToFront();
                 setPositionInStack( Option.<Integer>none() );
                 final AnimateToScale activity = new AnimateToScale( PieceNode.this, 200 );
@@ -77,9 +94,18 @@ public class PieceNode extends Stackable {
             @Override protected void endDrag( final PInputEvent event ) {
                 super.endDrag( event );
                 context.endDrag( PieceNode.this, event );
+
+                //Protect against multiple copies accidentally being added
+                if ( shapeType == ShapeType.PIE ) {
+                    while ( pieBackground.getChildrenReference().contains( pieShadow ) ) {
+                        pieBackground.removeChild( pieShadow );
+                    }
+                }
             }
         } );
     }
+
+    private AffineTransform getShadowOffset() {return getTranslateInstance( 6, 6 );}
 
     public void setInitialScale( double s ) {
         this.initialScale = s;
@@ -127,9 +153,9 @@ public class PieceNode extends Stackable {
 
     public void setPieceRotation( final double angle ) {
         final double extent = Math.PI * 2.0 / pieceSize;
-        Shape shape = new CircularShapeFunction( extent, SimpleContainerNode.circleDiameter / 2 ).createShape( Vector2D.ZERO, angle );
+        Shape shape = new CircularShapeFunction( extent, circleDiameter / 2 ).createShape( ZERO, angle );
         pathNode.setPathTo( shape );
+        pieShadow.setPathTo( getShadowOffset().createTransformedShape( shape ) );
         this.pieceRotation = angle;
     }
-
 }
