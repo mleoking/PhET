f16ab592301db2bfece084df42781c904861d286
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-07-27 02:52:01 +0000

    #3386, remove REVIEW comment about param order in Arrow2F

--- trunk/simulations-java/common/lwjgl-phet/src/edu/colorado/phet/lwjglphet/math/Arrow2F.java
+++ trunk/simulations-java/common/lwjgl-phet/src/edu/colorado/phet/lwjglphet/math/Arrow2F.java
@@ -3,8 +3,6 @@ package edu.colorado.phet.lwjglphet.math;
 
 import edu.colorado.phet.common.phetcommon.math.vector.Vector2F;
 
-//REVIEW I hate to see this awful interface (headHeight before headWidth) copied into another common lib.
-
 /**
  * copied somewhat from the Arrow class TODO cleanup
  */
