f0370d72fd0dae88ba4aacc10ccb35a780b48b73
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-07-27 04:58:17 +0000

    #3386, added REVIEW tags for labels and materials subpackages

--- trunk/simulations-java/simulations/plate-tectonics/src/edu/colorado/phet/platetectonics/view/labels/TextLabelNode.java
+++ trunk/simulations-java/simulations/plate-tectonics/src/edu/colorado/phet/platetectonics/view/labels/TextLabelNode.java
@@ -61,6 +61,7 @@ public class TextLabelNode extends BaseLabelNode {
 
     @Override
     public void renderSelf( GLOptions options ) {
+        //REVIEW super.renderSelf is currently a no-op, but might be a good practice to call it. Ditto for other subclasses of BaseLabelNode.
         Vector3F labelLocation = modelViewTransform.transformPosition( PlateTectonicsModel.convertToRadial( textLabel.centerPosition.get() ) );
         labelNode.transform.set( ImmutableMatrix4F.translation( labelLocation.x, labelLocation.y, labelLocation.z ) );
         labelNode.transform.append( ImmutableMatrix4F.scaling( LABEL_SCALE * scale.get() ) );
