c475085703841cc20bd9fd371bf2abfb24fe9794
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-08-01 15:46:07 +0000

    factored out Function classes

--- trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/buildafraction/model/numbers/NumberLevel.java
+++ trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/buildafraction/model/numbers/NumberLevel.java
@@ -1,12 +1,14 @@
 // Copyright 2002-2012, University of Colorado
 package edu.colorado.phet.fractions.buildafraction.model.numbers;
 
-import fj.F;
-import fj.Ord;
 import fj.data.List;
 
 import edu.colorado.phet.fractions.buildafraction.model.Level;
 import edu.colorado.phet.fractions.fractionsintro.intro.model.Fraction;
+
+import static edu.colorado.phet.fractions.buildafraction.model.numbers.NumberTarget._fraction;
+import static edu.colorado.phet.fractions.fractionsintro.intro.model.Fraction.*;
+import static fj.Ord.intOrd;
 
 /**
  * Level for the build a fraction game.
@@ -15,36 +17,26 @@ import edu.colorado.phet.fractions.fractionsintro.intro.model.Fraction;
  */
 public class NumberLevel extends Level {
 
+    //The numbers that the user can use, sorted in increasing order
     public final List<Integer> numbers;
+
+    //The targets the user must match.
     public final List<NumberTarget> targets;
 
     public NumberLevel( final List<Integer> numbers, final List<NumberTarget> targets ) {
         super( targets.length() );
-        this.numbers = numbers.sort( Ord.intOrd );
+        this.numbers = numbers.sort( intOrd );
         this.targets = targets;
     }
 
     //Infer the number cards from the list of targets, using exactly what is necessary
     public NumberLevel( final List<NumberTarget> targets ) {
-        this( targets.map( new F<NumberTarget, Integer>() {
-            @Override public Integer f( final NumberTarget numberTarget ) {
-                return numberTarget.fraction.numerator;
-            }
-        } ).
-                append( targets.map( new F<NumberTarget, Integer>() {
-                    @Override public Integer f( final NumberTarget numberTarget ) {
-                        return numberTarget.fraction.denominator;
-                    }
-                } ) ), targets );
+        this( targets.map( _fraction ).map( _numerator ).append( targets.map( _fraction ).map( _denominator ) ), targets );
     }
 
     public static NumberLevel numberLevelReduced( final List<NumberTarget> targets ) {
-        List<Fraction> reduced = targets.map( new F<NumberTarget, Fraction>() {
-            @Override public Fraction f( final NumberTarget numberTarget ) {
-                return numberTarget.fraction.reduce();
-            }
-        } );
-        List<Integer> cards = reduced.map( Fraction._numerator ).append( reduced.map( Fraction._denominator ) );
+        List<Fraction> reduced = targets.map( _fraction ).map( _reduce );
+        List<Integer> cards = reduced.map( _numerator ).append( reduced.map( Fraction._denominator ) );
         return new NumberLevel( cards, targets );
     }
 }
