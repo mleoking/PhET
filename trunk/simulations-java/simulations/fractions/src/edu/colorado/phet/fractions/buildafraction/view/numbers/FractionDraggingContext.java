0f5608d3b11c53c76c3e5f24aa34f2260a4563f5
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-08-01 16:46:39 +0000

    added docs

--- trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/buildafraction/view/numbers/FractionDraggingContext.java
+++ trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/buildafraction/view/numbers/FractionDraggingContext.java
@@ -4,6 +4,8 @@ package edu.colorado.phet.fractions.buildafraction.view.numbers;
 import edu.umd.cs.piccolo.event.PInputEvent;
 
 /**
+ * Context in which a FractionNode can be dragged.
+ *
  * @author Sam Reid
  */
 public interface FractionDraggingContext {
