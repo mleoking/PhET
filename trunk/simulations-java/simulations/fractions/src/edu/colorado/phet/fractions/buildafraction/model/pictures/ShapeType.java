9e7157242598cbc3011bbdddbd40179e3f4e8b55
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-08-01 15:50:11 +0000

    added docs

--- trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/buildafraction/model/pictures/ShapeType.java
+++ trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/buildafraction/model/pictures/ShapeType.java
@@ -2,6 +2,8 @@
 package edu.colorado.phet.fractions.buildafraction.model.pictures;
 
 /**
+ * A shape type for Build a Fraction: Shapes levels, either horizontal bar or pie.
+ *
  * @author Sam Reid
  */
 public enum ShapeType {
