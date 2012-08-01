beba67133101c4b4a7288160ea01038f3f74805f
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-08-01 16:52:15 +0000

    style changes

--- simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/fractionsintro/common/util/DefaultP2.java
+++ simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/fractionsintro/common/util/DefaultP2.java
@@ -15,17 +15,13 @@ public @Data class DefaultP2<A, B> extends P2<A, B> {
 
     //Lombok would generate this, but the IDEA plugin doesn't handle generics, so we code it explicitly
     public DefaultP2( final A a, final B b ) {
-        _1 = a;
-        _2 = b;
+        this._1 = a;
+        this._2 = b;
     }
 
-    @Override public A _1() {
-        return _1;
-    }
+    @Override public A _1() { return _1; }
 
-    @Override public B _2() {
-        return _2;
-    }
+    @Override public B _2() { return _2; }
 
     public static <A, B> DefaultP2<A, B> p2( A a, B b ) { return new DefaultP2<A, B>( a, b ); }
 }
