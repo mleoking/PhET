b0d80fcbe187220a683936adb4effbe9d99ec8c4
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-08-02 04:38:22 +0000

    added next button when level complete

---
+++ trunk/simulations-java/simulations/functions/src/edu/colorado/phet/functions/intro/Scene.java
@@ -0,0 +1,10 @@
+package edu.colorado.phet.functions.intro;
+
+import edu.colorado.phet.functions.buildafunction2.ValueContext;
+import edu.umd.cs.piccolo.PNode;
+
+/**
+ * @author Sam Reid
+ */
+public abstract class Scene extends PNode implements ValueContext {
+}
