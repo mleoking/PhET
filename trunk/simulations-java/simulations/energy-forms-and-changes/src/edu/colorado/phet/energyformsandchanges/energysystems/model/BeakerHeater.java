0ec8ede9eb0f36130f20744c45cb2c2d47652b82
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-08-08 21:42:51 +0000

    Integrated some longer pieces of connecting wire for the light bulbs and the beaker heater.

--- trunk/simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/energysystems/model/BeakerHeater.java
+++ trunk/simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/energysystems/model/BeakerHeater.java
@@ -27,7 +27,7 @@ public class BeakerHeater extends EnergyUser {
     private static final Vector2D HEATER_ELEMENT_OFFSET = new Vector2D( -0.002, 0.022 );
 
     private static final List<ModelElementImage> IMAGE_LIST = new ArrayList<ModelElementImage>() {{
-        add( new ModelElementImage( WIRE_BLACK_MIDDLE, WIRE_BLACK_MIDDLE.getWidth() / EFACConstants.ENERGY_SYSTEMS_MVT_SCALE_FACTOR, new Vector2D( -0.0375, -0.04 ) ) );
+        add( new ModelElementImage( WIRE_BLACK_MIDDLE_62, WIRE_BLACK_MIDDLE_62.getWidth() / EFACConstants.ENERGY_SYSTEMS_MVT_SCALE_FACTOR, new Vector2D( -0.037, -0.04 ) ) );
         add( new ModelElementImage( WIRE_BLACK_RIGHT, WIRE_BLACK_RIGHT.getWidth() / EFACConstants.ENERGY_SYSTEMS_MVT_SCALE_FACTOR, new Vector2D( -0.009, -0.016 ) ) );
         add( new ModelElementImage( ELEMENT_BASE, ELEMENT_BASE.getWidth() / EFACConstants.ENERGY_SYSTEMS_MVT_SCALE_FACTOR, new Vector2D( 0, 0 ) ) );
         add( new ModelElementImage( HEATER_ELEMENT_OFF, HEATER_ELEMENT_OFF.getWidth() / EFACConstants.ENERGY_SYSTEMS_MVT_SCALE_FACTOR, HEATER_ELEMENT_OFFSET ) );
