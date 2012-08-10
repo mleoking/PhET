16caf3c4d5ee3ab1fa50a8eba656e3d69f31ab45
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-08-10 16:05:54 +0000

    improved carousel buttons

--- simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/buildafraction/view/CarouselDotComponent.java
+++ simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/buildafraction/view/CarouselDotComponent.java
@@ -15,38 +15,35 @@ import edu.colorado.phet.common.piccolophet.nodes.layout.VBox;
 import edu.umd.cs.piccolo.PNode;
 
 /**
+ * Shows the different dots, one for each page and the labels above each dot.  Dot is filled in if that is where the user is.  Like on an ipad homescreen or on angry birds.
+ *
  * @author Sam Reid
  */
 class CarouselDotComponent extends PNode {
     public CarouselDotComponent( final IntegerProperty selectedPage ) {
         final int circleDiameter = 16;
-        addChild( new HBox( new VBox( new PhetPText( "1-5", new PhetFont( 15 ) ) {{
+
+        addChild( new HBox( new VBox( text( "1-5", selectedPage, 0 ), circle( selectedPage, circleDiameter, 0 ) ),
+                            new VBox( text( "6-10", selectedPage, 1 ), circle( selectedPage, circleDiameter, 1 ) ) ) );
+    }
+
+    private PhetPPath circle( final IntegerProperty selectedPage, final int circleDiameter, final int index ) {
+        return new PhetPPath( new Ellipse2D.Double( 0, 0, circleDiameter, circleDiameter ), new BasicStroke( 2 ), Color.gray ) {{
             selectedPage.addObserver( new VoidFunction1<Integer>() {
                 public void apply( final Integer integer ) {
-                    setTextPaint( integer == 0 ? Color.black : Color.gray );
+                    setPaint( integer == index ? Color.black : BuildAFractionCanvas.TRANSPARENT );
                 }
             } );
-        }}, new PhetPPath( new Ellipse2D.Double( 0, 0, circleDiameter, circleDiameter ), new BasicStroke( 2 ), Color.gray ) {{
+        }};
+    }
+
+    private PhetPText text( final String s, final IntegerProperty selectedPage, final int index ) {
+        return new PhetPText( s, new PhetFont( 15 ) ) {{
             selectedPage.addObserver( new VoidFunction1<Integer>() {
                 public void apply( final Integer integer ) {
-                    setPaint( integer == 0 ? Color.black : BuildAFractionCanvas.TRANSPARENT );
+                    setTextPaint( integer == index ? Color.black : Color.gray );
                 }
             } );
-        }}
-        ),
-                            new VBox( new PhetPText( "6-10", new PhetFont( 15 ) ) {{
-                                selectedPage.addObserver( new VoidFunction1<Integer>() {
-                                    public void apply( final Integer integer ) {
-                                        setTextPaint( integer == 1 ? Color.black : Color.gray );
-                                    }
-                                } );
-                            }}, new PhetPPath( new Ellipse2D.Double( 0, 0, circleDiameter, circleDiameter ), new BasicStroke( 2 ), Color.gray ) {{
-                                selectedPage.addObserver( new VoidFunction1<Integer>() {
-                                    public void apply( final Integer integer ) {
-                                        setPaint( integer == 1 ? Color.black : BuildAFractionCanvas.TRANSPARENT );
-                                    }
-                                } );
-                            }}
-                            ) ) );
+        }};
     }
 }
