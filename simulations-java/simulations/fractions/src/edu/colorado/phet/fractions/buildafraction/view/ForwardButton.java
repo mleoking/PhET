af183bc8d831fc51e47f80bdcfe969bd190ed881
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-08-10 16:27:37 +0000

    improved simsharing

--- simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/buildafraction/view/ForwardButton.java
+++ simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/buildafraction/view/ForwardButton.java
@@ -3,6 +3,7 @@ package edu.colorado.phet.fractions.buildafraction.view;
 
 import edu.colorado.phet.common.phetcommon.model.property.ObservableProperty;
 import edu.colorado.phet.common.phetcommon.model.property.Property;
+import edu.colorado.phet.common.phetcommon.simsharing.SimSharingManager;
 import edu.colorado.phet.common.phetcommon.util.function.VoidFunction0;
 import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
 import edu.colorado.phet.fractions.common.view.SpinnerButtonNode;
@@ -10,6 +11,7 @@ import edu.umd.cs.piccolo.PNode;
 
 import static edu.colorado.phet.fractions.FractionsResources.Images.*;
 import static edu.colorado.phet.fractions.common.view.BackButton.rescale;
+import static edu.colorado.phet.fractions.fractionsintro.FractionsIntroSimSharing.Components.forwardButton;
 
 /**
  * Button that lets the user move forward (to the next set of divisions or the next level).
@@ -24,6 +26,7 @@ class ForwardButton extends PNode {
     public ForwardButton( final VoidFunction0 pressed, ObservableProperty<Boolean> enabled ) {
         addChild( new SpinnerButtonNode( rescale( RIGHT_BUTTON_UP ), rescale( RIGHT_BUTTON_PRESSED ), rescale( RIGHT_BUTTON_GRAY ), new VoidFunction1<Boolean>() {
             public void apply( final Boolean spinning ) {
+                SimSharingManager.sendButtonPressed( forwardButton );
                 pressed.apply();
             }
         }, enabled ) );
