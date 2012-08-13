62429071e0b5b1385c0e28f9d7f67a6e9bc1ee29
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-08-13 20:41:12 +0000

    Added MixedFractionNode

---
+++ simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/buildafraction/view/shapes/MixedFractionNode.java
@@ -0,0 +1,28 @@
+// Copyright 2002-2012, University of Colorado
+package edu.colorado.phet.fractions.buildafraction.view.shapes;
+
+import edu.colorado.phet.common.phetcommon.model.property.Property;
+import edu.colorado.phet.common.piccolophet.nodes.layout.HBox;
+import edu.colorado.phet.fractions.buildafraction.model.MixedFraction;
+import edu.colorado.phet.fractions.fractionsintro.intro.view.FractionNode;
+import edu.colorado.phet.fractions.fractionsintro.intro.view.FractionNumberNode;
+import edu.umd.cs.piccolo.PNode;
+
+/**
+ * Shows a fraction as a mixed fraction, such as 1 2/3.  See also MixedNumberNode
+ *
+ * @author Sam Reid
+ */
+class MixedFractionNode extends PNode {
+    public MixedFractionNode( final MixedFraction fraction ) {
+        final double fractionSizeScale = 0.3;
+
+        //AP: Usually mixed numbers are written with the "1" nearly as tall as the entire fraction
+        final double mixedNumberWholeScale = 2.4;
+        addChild( new HBox( 0,
+                            new FractionNumberNode( new Property<Integer>( fraction.whole ) ) {{
+                                setScale( fractionSizeScale * mixedNumberWholeScale );
+                            }},
+                            new FractionNode( fraction.fraction, fractionSizeScale ) ) );
+    }
+}
