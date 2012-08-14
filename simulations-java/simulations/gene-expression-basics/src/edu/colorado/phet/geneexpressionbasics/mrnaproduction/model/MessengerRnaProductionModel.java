22a575887ce6e7fb7262d0254fac17f0c68f8c53
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-08-14 23:41:45 +0000

    Various code cleanup, part of response to code review, see #3381.

--- simulations-java/simulations/gene-expression-basics/src/edu/colorado/phet/geneexpressionbasics/mrnaproduction/model/MessengerRnaProductionModel.java
+++ simulations-java/simulations/gene-expression-basics/src/edu/colorado/phet/geneexpressionbasics/mrnaproduction/model/MessengerRnaProductionModel.java
@@ -30,15 +30,7 @@ import edu.colorado.phet.geneexpressionbasics.common.model.TranscriptionFactor.T
 import edu.colorado.phet.geneexpressionbasics.common.model.motionstrategies.MotionBounds;
 
 /**
- * REVIEW: this is for the mRNA tab, not the manual gene expression tab?
- * Primary model for the manual gene expression tab.
- * <p/>
- * REVIEW: again, dimensions seem to be in picometers mostly?
- * Dimensions in this model (and all sub-elements of the model) are in nano-
- * meters, i.e. 10E-9 meters.
- * <p/>
- * The point (0,0) in model space is at the leftmost edge of the DNA strand, and
- * at the vertical center of the strand.
+ * Primary model for the Messenger RNA Production tab.
  *
  * @author John Blanco
  */
