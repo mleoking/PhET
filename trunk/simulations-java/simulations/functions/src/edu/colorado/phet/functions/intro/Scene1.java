7646a48db0a25d8915c900f9f2f1bc30ce8931b7
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-08-15 22:34:08 +0000

    Prevent value from dragging left through function

--- trunk/simulations-java/simulations/functions/src/edu/colorado/phet/functions/intro/Scene1.java
+++ trunk/simulations-java/simulations/functions/src/edu/colorado/phet/functions/intro/Scene1.java
@@ -60,21 +60,30 @@ class Scene1 extends Scene {
         boolean leftBefore = valueNode.getGlobalFullBounds().getCenterX() < functionNode.getGlobalFullBounds().getCenterX();
         double distanceFromOpening = functionBounds.getMinX() - valueBounds.getMaxX();
         PBounds functionBox = new PBounds( functionBounds.getMinX() - 50, functionBounds.getY(), functionBounds.getWidth() + 50, functionBounds.getHeight() );
-        if ( delta.width >= 0 && functionBox.intersects( valueBounds ) ) {
+
+        //Move through function
+        if ( functionBox.intersects( valueBounds ) ) {
+            System.out.println( "1" );
+            //Move toward opening
             if ( distanceFromOpening > 0 && distanceFromOpening < 50 ) {
+                System.out.println( "a" );
                 double valueCenterY = valueBounds.getCenterY();
                 double functionCenterY = functionBounds.getCenterY();
                 double deltaY = functionCenterY - valueCenterY;
                 LinearFunction f = new LinearFunction( 0, 50, 1, 0 );
-                valueNode.translate( delta.width, f.evaluate( distanceFromOpening ) * deltaY );
+                valueNode.translate( Math.max( delta.width, 0 ), f.evaluate( distanceFromOpening ) * deltaY );
             }
+
+            //Move through
             else if ( valueBounds.getMinX() < functionBounds.getMaxX() && valueBounds.getMaxX() > functionBounds.getMinX() ) {
+                System.out.println( "b" );
                 double valueCenterY = valueBounds.getCenterY();
                 double functionCenterY = functionBounds.getCenterY();
                 double deltaY = functionCenterY - valueCenterY;
-                valueNode.translate( delta.width, deltaY );
+                valueNode.translate( Math.max( 0, delta.width ), deltaY );
             }
             else {
+                System.out.println( "c" );
                 valueNode.translate( delta.width, delta.height );
             }
             boolean leftAfter = valueNode.getGlobalFullBounds().getCenterX() < functionNode.getGlobalFullBounds().getCenterX();
@@ -84,6 +93,7 @@ class Scene1 extends Scene {
         }
 
         else {
+            System.out.println( "2" );
             valueNode.translate( delta.width, delta.height );
         }
 
