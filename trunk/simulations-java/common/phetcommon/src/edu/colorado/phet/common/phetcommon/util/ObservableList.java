050e4c74ec5b200da551adf605a05f9ed639c09a
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-08-15 16:48:40 +0000

    After further thought, the methods for getting previous, next, insertAfter, insertBefore (added in the previous commit on this file) have been backed out.

--- trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/util/ObservableList.java
+++ trunk/simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/util/ObservableList.java
@@ -356,42 +356,4 @@ public class ObservableList<T> implements List<T> {
     public T max() {
         return (T) Collections.max( new ArrayList<Comparable>( (Collection<? extends Comparable>) this ) );
     }
-
-    public T getNextItem( T item ) {
-        int index = indexOf( item );
-        // REVIEW: maybe have an exception here, since if assertions are not enabled it will return the first element in the list
-        assert index != -1; // This function shouldn't be used for segments not on the list.
-        if ( index == size() - 1 ) {
-            // The given segment is the last element on the list, so null is returned.
-            return null;
-        }
-        else {
-            return get( index + 1 );
-        }
-    }
-
-    public T getPreviousItem( T item ) {
-        int index = indexOf( item );
-        // REVIEW: also recommend an exception here
-        assert index != -1; // This function shouldn't be used for segments not on the list.
-        if ( index == 0 ) {
-            // The given segment is the first element on the list, so null is returned.
-            return null;
-        }
-        else {
-            return get( index - 1 );
-        }
-    }
-
-    public void insertAfter( T existingItem, T itemToInsert ) {
-        // REVIEW: also recommend an exception here
-        assert contains( existingItem );
-        add( indexOf( existingItem ) + 1, itemToInsert );
-    }
-
-    public void insertBefore( T existingItem, T itemToInsert ) {
-        // REVIEW: also recommend an exception here
-        assert contains( existingItem );
-        add( indexOf( existingItem ), itemToInsert );
-    }
 }
