ba3cb017ad27764227b5b19edf53341c29e35abc
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-08-15 21:04:05 +0000

    Added a stack of input cards

--- trunk/simulations-java/simulations/functions/src/edu/colorado/phet/functions/buildafunction2/ValueNode.java
+++ trunk/simulations-java/simulations/functions/src/edu/colorado/phet/functions/buildafunction2/ValueNode.java
@@ -1,18 +1,25 @@
 package edu.colorado.phet.functions.buildafunction2;
 
+import fj.F;
+
 import java.awt.Color;
 import java.awt.Stroke;
 import java.awt.geom.Area;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
 
+import edu.colorado.phet.common.phetcommon.view.util.BufferedImageUtils;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.event.CursorHandler;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPText;
+import edu.colorado.phet.functions.FunctionsResources.Images;
+import edu.colorado.phet.functions.intro.Key;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
 import edu.umd.cs.piccolo.event.PInputEvent;
+import edu.umd.cs.piccolo.nodes.PImage;
 import edu.umd.cs.piccolo.util.PDimension;
 
 /**
@@ -23,20 +30,21 @@ public class ValueNode extends PNode {
     private PNode node;
     private Area a;
     private PhetPPath boundNode;
-    private int numRotations = 0;
+    private Object originalValue;
 
-    public ValueNode( ValueContext valueContext, final int number, Stroke stroke, Color paint, Color strokePaint, final Color textPaint ) {
-        this( valueContext, new PhetPText( number + "", new PhetFont( 42, true ) ) {{setTextPaint( textPaint );}}, stroke, paint, strokePaint );
-    }
+    private ArrayList<F<Object, Object>> functions = new ArrayList<F<Object, Object>>();
+    private final Color textPaint;
 
-    public ValueNode( final ValueContext valueContext, PNode node, Stroke stroke, Color paint, Color strokePaint ) {
+    public ValueNode( final ValueContext valueContext, Object originalValue, Stroke stroke, Color paint, Color strokePaint, Color textPaint ) {
+        this.originalValue = originalValue;
+        this.textPaint = textPaint;
         a = new Area( new Rectangle2D.Double( 0, 0, 50, Constants.ellipseWidth ) );
         double ellipseWidth = 50;
         a.add( new Area( new Ellipse2D.Double( a.getBounds2D().getMaxX() - ellipseWidth / 2, a.getBounds2D().getCenterY() - ellipseWidth / 2, ellipseWidth, ellipseWidth ) ) );
         a.add( new Area( new Ellipse2D.Double( a.getBounds2D().getMinX() - ellipseWidth / 2, a.getBounds2D().getCenterY() - ellipseWidth / 2, ellipseWidth, ellipseWidth ) ) );
         boundNode = new PhetPPath( a, paint, stroke, strokePaint );
         addChild( boundNode );
-        this.node = node;
+        this.node = toNode( getCurrentValue() );
         centerContent();
         addChild( node );
 
@@ -57,19 +65,40 @@ public class ValueNode extends PNode {
         addInputEventListener( new CursorHandler() );
     }
 
+    public Object getCurrentValue() {
+        Object x = originalValue;
+        for ( F<Object, Object> function : functions ) {
+            x = function.f( x );
+        }
+        return x;
+    }
+
+    private static PhetPText toTextNode( String text, final Color textPaint ) {return new PhetPText( text, new PhetFont( 42, true ) ) {{setTextPaint( textPaint );}};}
+
     public void centerContent() {
         this.node.setOffset( a.getBounds2D().getCenterX() - node.getFullBounds().getWidth() / 2, a.getBounds2D().getCenterY() - node.getFullBounds().getHeight() / 2 );
     }
 
-    public void setStrokePaint( final Color color ) {
-        boundNode.setStrokePaint( color );
+    public void setStrokePaint( final Color color ) { boundNode.setStrokePaint( color ); }
+
+    public void applyFunction( F<Object, Object> f ) {
+        System.out.println( getCurrentValue() );
+        functions.add( f );
+        System.out.println( getCurrentValue() );
+        removeChild( node );
+        node = toNode( getCurrentValue() );
+        addChild( node );
+        centerContent();
     }
 
-    public void rotateRight() {
-        numRotations++;
+    private PNode toNode( final Object currentValue ) {
+        if ( currentValue instanceof Key ) {
+            return new PImage( BufferedImageUtils.getRotatedImage( BufferedImageUtils.multiScaleToWidth( Images.KEY, 45 ), -Math.PI / 2 + ( (Key) currentValue ).getNumRotations() * Math.PI / 2 ) );
+        }
+        else {
+            return toTextNode( currentValue.toString(), textPaint );
+        }
     }
 
-    public int getNumberRotations() {
-        return numRotations;
-    }
+    public int getNumberRotations() { return ( (Key) getCurrentValue() ).numRotations; }
 }
--- trunk/simulations-java/simulations/functions/src/edu/colorado/phet/functions/intro/IntroCanvas.java
+++ trunk/simulations-java/simulations/functions/src/edu/colorado/phet/functions/intro/IntroCanvas.java
@@ -4,27 +4,20 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
 
-import edu.colorado.phet.common.phetcommon.math.Function.LinearFunction;
 import edu.colorado.phet.common.phetcommon.model.Resettable;
-import edu.colorado.phet.common.phetcommon.view.util.BufferedImageUtils;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.nodes.HTMLImageButtonNode;
 import edu.colorado.phet.common.piccolophet.nodes.ResetAllButtonNode;
 import edu.colorado.phet.common.piccolophet.nodes.layout.HBox;
-import edu.colorado.phet.functions.FunctionsResources.Images;
 import edu.colorado.phet.functions.buildafunction2.AbstractFunctionsCanvas;
 import edu.colorado.phet.functions.buildafunction2.BuildAFunctionPrototype2Canvas;
 import edu.colorado.phet.functions.buildafunction2.UnaryNumberFunctionNode;
 import edu.colorado.phet.functions.buildafunction2.ValueContext;
 import edu.colorado.phet.functions.buildafunction2.ValueNode;
-import edu.umd.cs.piccolo.nodes.PImage;
-import edu.umd.cs.piccolo.util.PBounds;
 import edu.umd.cs.piccolo.util.PDimension;
 
 import static edu.colorado.phet.functions.FunctionModule.radioButton;
-import static javax.swing.GrayFilter.createDisabledImage;
 
 /**
  * @author Sam Reid
@@ -48,7 +41,7 @@ public class IntroCanvas extends AbstractFunctionsCanvas implements ValueContext
         }};
         addScreenChild( resetAllButtonNode );
 
-        scene = new Scene1();
+        scene = new Scene1( this );
         addChild( scene );
     }
 
@@ -60,80 +53,7 @@ public class IntroCanvas extends AbstractFunctionsCanvas implements ValueContext
         scene.mouseReleased( valueNode );
     }
 
-    private class Scene1 extends Scene {
-
-        private final UnaryNumberFunctionNode functionNode;
-        private final ValueNode valueNode;
-        private final PImage valueNodeImage;
-        private final ValueNode targetNode;
-
-        private Scene1() {
-            final BufferedImage key = BufferedImageUtils.multiScaleToWidth( Images.KEY, 45 );
-            targetNode = new ValueNode( IntroCanvas.this, new PImage( createDisabledImage( createDisabledImage( key ) ) ), new BasicStroke( 1, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 1f, new float[] { 10, 10 }, 0 ), new Color( 0, 0, 0, 0 ), Color.gray ) {{
-                setOffset( 903.9881831610056, 318.4047267355978 );
-            }};
-            addChild( targetNode );
-
-            valueNodeImage = new PImage( BufferedImageUtils.getRotatedImage( key, -Math.PI / 2 ) );
-            valueNode = new ValueNode( IntroCanvas.this, valueNodeImage, new BasicStroke( 1 ), Color.white, Color.black ) {{
-                setOffset( 84.37223042836038, 315.3914327917278 );
-            }};
-            addChild( valueNode );
-
-            functionNode = new UnaryNumberFunctionNode( new PImage( BufferedImageUtils.multiScaleToWidth( Images.ROTATE_RIGHT, 50 ) ), false ) {{
-                setOffset( 390.72378138847836, 294.298375184638 );
-            }};
-            addChild( functionNode );
-        }
-
-        public void mouseDragged( final ValueNode valueNode, final PDimension delta ) {
-            PBounds valueBounds = valueNode.getGlobalFullBounds();
-            PBounds functionBounds = functionNode.getGlobalFullBounds();
-            boolean leftBefore = valueNode.getGlobalFullBounds().getCenterX() < functionNode.getGlobalFullBounds().getCenterX();
-            double distanceFromOpening = functionBounds.getMinX() - valueBounds.getMaxX();
-            PBounds functionBox = new PBounds( functionBounds.getMinX() - 50, functionBounds.getY(), functionBounds.getWidth() + 50, functionBounds.getHeight() );
-            if ( delta.width >= 0 && functionBox.intersects( valueBounds ) ) {
-                if ( distanceFromOpening > 0 && distanceFromOpening < 50 ) {
-                    double valueCenterY = valueBounds.getCenterY();
-                    double functionCenterY = functionBounds.getCenterY();
-                    double deltaY = functionCenterY - valueCenterY;
-                    LinearFunction f = new LinearFunction( 0, 50, 1, 0 );
-                    valueNode.translate( delta.width, f.evaluate( distanceFromOpening ) * deltaY );
-                }
-                else if ( valueBounds.getMinX() < functionBounds.getMaxX() && valueBounds.getMaxX() > functionBounds.getMinX() ) {
-                    double valueCenterY = valueBounds.getCenterY();
-                    double functionCenterY = functionBounds.getCenterY();
-                    double deltaY = functionCenterY - valueCenterY;
-                    valueNode.translate( delta.width, deltaY );
-                }
-                else {
-                    valueNode.translate( delta.width, delta.height );
-                }
-                boolean leftAfter = valueNode.getGlobalFullBounds().getCenterX() < functionNode.getGlobalFullBounds().getCenterX();
-                if ( leftBefore == true && leftAfter == false ) {
-                    valueNodeImage.setImage( BufferedImageUtils.getRotatedImage( BufferedImageUtils.toBufferedImage( valueNodeImage.getImage() ), Math.PI / 2 ) );
-                    valueNode.centerContent();
-                    valueNode.rotateRight();
-                }
-            }
-
-            else {
-                valueNode.translate( delta.width, delta.height );
-            }
-
-//            System.out.println( "LB = " + leftBefore + ", leftAfter = " + leftAfter );
-        }
-
-        public void mouseReleased( final ValueNode valueNode ) {
-            if ( valueNode.getGlobalFullBounds().intersects( targetNode.getGlobalFullBounds() ) && valueNode.getNumberRotations() % 4 == 1 ) {
-                valueNode.setStrokePaint( Color.red );
-                valueNode.centerFullBoundsOnPoint( targetNode.getFullBounds().getCenterX(), targetNode.getFullBounds().getCenterY() );
-                IntroCanvas.this.showNextButton();
-            }
-        }
-    }
-
-    private void showNextButton() {
+    public void showNextButton() {
         final HTMLImageButtonNode nextButton = new HTMLImageButtonNode( "Next" ) {{
             setFont( resetAllButtonNode.getFont() );
             setBackground( Color.green );
