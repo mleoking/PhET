05f19ee3f33a81c68ec717fe03a994eafc40388c
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-08-15 18:51:21 +0000

    Build a Fraction: Numbers: Changed from 4 targets to 3 in level 5

--- trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/buildafraction/model/numbers/RandomColors4.java
+++ trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/buildafraction/model/numbers/RandomColors4.java
@@ -13,7 +13,7 @@ import static fj.data.List.list;
  *
  * @author Sam Reid
  */
-class RandomColors4 {
+class RandomColors4 implements IRandomColors {
     private final List<Color> colors = NumberLevelList.shuffle( list( LIGHT_RED, LIGHT_GREEN, LIGHT_BLUE, Color.orange ) );
     private int index = 0;
 
