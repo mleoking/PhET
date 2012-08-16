a8fab46230e0618999f52b6946c315d48554ba29
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-08-16 05:03:12 +0000

    Build a Fraction: Numbers: Fixed: hitting "reset" when the sim starts up causes the fraction outline to move

--- trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/buildafraction/view/numbers/FractionDraggingContext.java
+++ trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/buildafraction/view/numbers/FractionDraggingContext.java
@@ -1,6 +1,8 @@
 // Copyright 2002-2012, University of Colorado
 package edu.colorado.phet.fractions.buildafraction.view.numbers;
 
+import edu.colorado.phet.common.phetcommon.math.vector.Vector2D;
+
 /**
  * Context in which a FractionNode can be dragged.
  *
@@ -10,4 +12,6 @@ public interface FractionDraggingContext {
     void endDrag( FractionNode fractionGraphic );
 
     void updateStacks();
+
+    Vector2D getCenterOfScreen();
 }
