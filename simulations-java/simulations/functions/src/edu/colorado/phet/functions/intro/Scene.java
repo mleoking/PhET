71ca7256daa526d09b2f0ea0cf7cf47997ef65a8
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-08-16 04:32:17 +0000

    removed debug output

--- simulations-java/simulations/functions/src/edu/colorado/phet/functions/intro/Scene.java
+++ simulations-java/simulations/functions/src/edu/colorado/phet/functions/intro/Scene.java
@@ -52,10 +52,8 @@ public abstract class Scene extends PNode implements ValueContext {
 
         //Move through function
         if ( functionBox.intersects( valueBounds ) ) {
-            System.out.println( "1" );
             //Move toward opening
             if ( distanceFromOpening > 0 && distanceFromOpening < 50 ) {
-                System.out.println( "a" );
                 double valueCenterY = valueBounds.getCenterY();
                 double functionCenterY = functionBounds.getCenterY();
                 double deltaY = functionCenterY - valueCenterY;
@@ -65,14 +63,12 @@ public abstract class Scene extends PNode implements ValueContext {
 
             //Move through
             else if ( valueBounds.getMinX() < functionBounds.getMaxX() && valueBounds.getMaxX() > functionBounds.getMinX() ) {
-                System.out.println( "b" );
                 double valueCenterY = valueBounds.getCenterY();
                 double functionCenterY = functionBounds.getCenterY();
                 double deltaY = functionCenterY - valueCenterY;
                 valueNode.translate( Math.max( 0, delta.width ), deltaY );
             }
             else {
-                System.out.println( "c" );
                 valueNode.translate( delta.width, delta.height );
             }
             boolean leftAfter = valueNode.getGlobalFullBounds().getCenterX() < functionNode.getGlobalFullBounds().getCenterX();
@@ -82,7 +78,6 @@ public abstract class Scene extends PNode implements ValueContext {
         }
 
         else {
-            System.out.println( "2" );
             valueNode.translate( delta.width, delta.height );
         }
     }
