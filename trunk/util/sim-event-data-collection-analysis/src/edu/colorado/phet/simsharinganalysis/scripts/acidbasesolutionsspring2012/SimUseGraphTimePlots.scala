998bf04b6d00549fe7d39124655bd7e8e76ad639
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-08-21 18:32:35 +0000

    started factoring out rules for plots

--- trunk/util/sim-event-data-collection-analysis/src/edu/colorado/phet/simsharinganalysis/scripts/acidbasesolutionsspring2012/SimUseGraphTimePlots.scala
+++ trunk/util/sim-event-data-collection-analysis/src/edu/colorado/phet/simsharinganalysis/scripts/acidbasesolutionsspring2012/SimUseGraphTimePlots.scala
@@ -8,7 +8,7 @@ object SimUseGraphTimePlots {
 
   def toMillis(minute: Int) = minute * 60000
 
-  def getCountsForMinute(minute: Int, report: AcidBaseReport, featureType: String) = {
+  def getCountsForMinute(minute: Int, report: AcidBaseReport, rule: StateTransition => Boolean) = {
     val startMillis = toMillis(minute - 1)
     val endMillis = toMillis(minute)
     val startTime = report.log.startTime
@@ -17,10 +17,11 @@ object SimUseGraphTimePlots {
     //TODO: Only count first time a feature used?
     report.statesWithTransitions.filter(p =>
                                           p.entry.time - startTime >= startMillis &&
-                                          p.entry.time - startTime < endMillis).length
+                                          p.entry.time - startTime < endMillis &&
+                                          rule(p)).length
   }
 
-  def getCounts(report: AcidBaseReport, featureType: String) = minutes.map(minute => getCountsForMinute(minute, report, featureType))
+  def getCounts(report: AcidBaseReport, rule: StateTransition => Boolean) = minutes.map(minute => getCountsForMinute(minute, report, rule))
 
   def main(args: Array[String]) {
     val groups = SimUseGraphSupport.groups
@@ -37,9 +38,19 @@ object SimUseGraphTimePlots {
       println("A" + ( groupIndex + 1 ) + " Sessions")
       println()
       for ( report <- groups(groupIndex).reports ) {
-        println(report.session + "\t" + SimUseGraph.Explore + "\t" + getCounts(report, SimUseGraph.Explore).mkString("\t"))
-        //        println(report.session + "\t" + SimUseGraph.Prompted + "\t" + getCounts(report, SimUseGraph.Prompted).mkString("\t"))
-        //        println(report.session + "\t" + SimUseGraph.NoCredit + "\t" + getCounts(report, SimUseGraph.NoCredit).mkString("\t"))
+        def countEverything(s: StateTransition) = true
+        def countClicks(s: StateTransition, featureType: String) = {
+          val filtered = table.filter(_._1.filter(s))
+          assert(filtered.length == 1)
+          val element = filtered.head
+          val classification = element._2(groupIndex)
+          classification == featureType
+        }
+        println(report.session + "\tTotal Clicks (E,P,N)\t" + getCounts(report, countEverything).mkString("\t"))
+        println(report.session + "\tExplore Clicks (E)\t" + getCounts(report, countClicks(_, SimUseGraph.Explore)).mkString("\t"))
+        println(report.session + "\tPrompted Clicks (P)\t" + getCounts(report, countClicks(_, SimUseGraph.Prompted)).mkString("\t"))
+        println(report.session + "\tNew Features (E)\t" + getCounts(report, countEverything).mkString("\t"))
+        println(report.session + "\tNew Features (P)\t" + getCounts(report, countEverything).mkString("\t"))
       }
     }
   }
