1f2b1893075826e1a9e9563215ae473d37fd7b84
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-08-21 16:41:12 +0000

    added static imports

--- simulations-java/simulations/functions/src/edu/colorado/phet/functions/intro/Scene2.java
+++ simulations-java/simulations/functions/src/edu/colorado/phet/functions/intro/Scene2.java
@@ -11,7 +11,9 @@ import java.awt.Color;
 import edu.colorado.phet.functions.buildafunction.UnaryFunctionNode;
 import edu.colorado.phet.functions.buildafunction.ValueContext;
 import edu.colorado.phet.functions.buildafunction.ValueNode;
-import edu.colorado.phet.functions.model.Functions;
+
+import static edu.colorado.phet.functions.model.Functions.INTEGER_PLUS_1;
+import static edu.colorado.phet.functions.model.Functions.INTEGER_TIMES_2;
 
 /**
  * @author Sam Reid
@@ -27,8 +29,8 @@ public class Scene2 extends Scene {
     }
 
     private static List<UnaryFunctionNode> unaryFunctionNodeList() {
-        return List.list( new UnaryFunctionNode( "\u27152", false, Functions.INTEGER_TIMES_2 ) {{ setOffset( 390.72378138847836, 294.298375184638 - 80 ); }},
-                          new UnaryFunctionNode( "+1", false, Functions.INTEGER_PLUS_1 ) {{ setOffset( 390.72378138847836, 444.298375184638 - 80 ); }}
+        return List.list( new UnaryFunctionNode( "\u27152", false, INTEGER_TIMES_2 ) {{ setOffset( 390.72378138847836, 294.298375184638 - 80 ); }},
+                          new UnaryFunctionNode( "+1", false, INTEGER_PLUS_1 ) {{ setOffset( 390.72378138847836, 444.298375184638 - 80 ); }}
         );
     }
 
