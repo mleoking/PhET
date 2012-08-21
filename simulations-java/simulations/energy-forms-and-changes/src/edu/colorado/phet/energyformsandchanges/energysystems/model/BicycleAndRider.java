a3e647dcf2998e6551d41fc5075ebb637ae6de49
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-08-21 17:03:54 +0000

    Added properties for crank rotation rate, rear wheel rotation rate, and target crank rotation rate.  Also filled in stepInTime to perform acceleration and deceleration of crank and rear wheel rotation.

--- simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/energysystems/model/BicycleAndRider.java
+++ simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/energysystems/model/BicycleAndRider.java
@@ -4,10 +4,12 @@ package edu.colorado.phet.energyformsandchanges.energysystems.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import edu.colorado.phet.common.phetcommon.math.MathUtil;
 import edu.colorado.phet.common.phetcommon.math.vector.Vector2D;
 import edu.colorado.phet.common.phetcommon.model.property.ObservableProperty;
 import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.phetcommon.simsharing.messages.IUserComponent;
+import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
 import edu.colorado.phet.energyformsandchanges.EnergyFormsAndChangesResources;
 import edu.colorado.phet.energyformsandchanges.EnergyFormsAndChangesSimSharing;
 
@@ -25,9 +27,18 @@ public class BicycleAndRider extends EnergySource {
     // Class Data
     //-------------------------------------------------------------------------
 
+    private static final double MAX_ANGULAR_VELOCITY_OF_CRANK = 2 * Math.PI; // In radians/sec.
+    private static final double ANGULAR_ACCELERATION = Math.PI / 2; // In radians/(sec^2).
+    // TODO: This is temp until we figure out how much it should really put out.
+    private static final double MAX_ENERGY_OUTPUT_RATE = 10; // In joules / (radians / sec)
+    private static final double CRANK_TO_REAR_WHEEL_RATIO = 2;
+
+    // Offset of the bike frame center.  Most other image offsets are relative
+    // to this one.
     private static final Vector2D FRAME_CENTER_OFFSET = new Vector2D( -0.01, 0.01 );
 
-    // Offsets, which are in meters, were empirically determined.  The values
+    // Images used when representing this model element in the view.  The
+    // offsets, which are in meters, were empirically determined.  The values
     // aren't really to scale, since there are so many things in this model
     // with very different scales.
     public static final ModelElementImage FRAME_IMAGE = new ModelElementImage( BICYCLE_FRAME, FRAME_CENTER_OFFSET );
@@ -44,15 +55,17 @@ public class BicycleAndRider extends EnergySource {
         add( FRONT_LEG );
     }};
 
-    private static final double ENERGY_OUTPUT_RATE = 10; // In joules / (radians / sec)
-
     //-------------------------------------------------------------------------
     // Instance Data
     //-------------------------------------------------------------------------
 
     private boolean active = false;
-    private Property<Double> rearWheelRotationalAngle = new Property<Double>( 0.0 ); // In radians.
-    private double rearWheelRotationalVelocity = 0; // In radians/s.
+    private Property<Double> crankAngle = new Property<Double>( 0.0 ); // In radians.
+    private Property<Double> rearWheelAngle = new Property<Double>( 0.0 ); // In radians.
+    private double crankAngularVelocity = 0; // In radians/s.
+
+    // Property through which the target pedaling rate is set.
+    public Property<Double> targetCrankAngularVelocity = new Property<Double>( 0.0 ); // In radians/sec
 
     //-------------------------------------------------------------------------
     // Constructor(s)
@@ -60,6 +73,13 @@ public class BicycleAndRider extends EnergySource {
 
     public BicycleAndRider() {
         super( EnergyFormsAndChangesResources.Images.BICYCLE_ICON, IMAGE_LIST );
+
+        // Monitor target rotation rate for validity.
+        targetCrankAngularVelocity.addObserver( new VoidFunction1<Double>() {
+            public void apply( Double targetAngularVelocity ) {
+                assert targetAngularVelocity >= 0 && targetAngularVelocity <= MAX_ANGULAR_VELOCITY_OF_CRANK;
+            }
+        } );
     }
 
     //-------------------------------------------------------------------------
@@ -68,18 +88,29 @@ public class BicycleAndRider extends EnergySource {
 
     @Override public Energy stepInTime( double dt ) {
         if ( active ) {
-            // TODO: Implement
+            double angularVelocityDiffFromTarget = targetCrankAngularVelocity.get() - crankAngularVelocity;
+            if ( angularVelocityDiffFromTarget != 0 ) {
+                // Accelerate or decelerate.
+                double change = ANGULAR_ACCELERATION * dt * ( angularVelocityDiffFromTarget > 0 ? 1 : -1 );
+                crankAngularVelocity = MathUtil.clamp( 0, crankAngularVelocity + change, targetCrankAngularVelocity.get() );
+            }
+            crankAngle.set( crankAngle.get() + crankAngularVelocity * dt );
+            rearWheelAngle.set( rearWheelAngle.get() + crankAngularVelocity * dt * CRANK_TO_REAR_WHEEL_RATIO );
         }
-        return new Energy( Energy.Type.ELECTRICAL, Math.abs( rearWheelRotationalVelocity * ENERGY_OUTPUT_RATE ) );
+        return new Energy( Energy.Type.ELECTRICAL, Math.abs( crankAngularVelocity / MAX_ANGULAR_VELOCITY_OF_CRANK * MAX_ENERGY_OUTPUT_RATE ) );
     }
 
     @Override public void deactivate() {
         super.deactivate();
-        rearWheelRotationalVelocity = 0;
+        crankAngularVelocity = 0;
     }
 
-    public ObservableProperty<Double> getRearWheelRotationalAngle() {
-        return rearWheelRotationalAngle;
+    public ObservableProperty<Double> getRearWheelAngle() {
+        return rearWheelAngle;
+    }
+
+    public Property<Double> getCrankAngle() {
+        return crankAngle;
     }
 
     @Override public IUserComponent getUserComponent() {
