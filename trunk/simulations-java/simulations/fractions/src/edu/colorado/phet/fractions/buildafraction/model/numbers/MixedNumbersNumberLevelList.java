055e26984d7e962e587ec5500536245aa02aa154
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-08-22 16:00:36 +0000

    Build a Mixed Fraction: Numbers: Added level 1

--- trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/buildafraction/model/numbers/MixedNumbersNumberLevelList.java
+++ trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/buildafraction/model/numbers/MixedNumbersNumberLevelList.java
@@ -6,6 +6,10 @@ import fj.data.List;
 import java.awt.Color;
 import java.util.ArrayList;
 
+import static edu.colorado.phet.fractions.buildafraction.model.numbers.NumberLevelList.pie;
+import static edu.colorado.phet.fractions.buildafraction.model.numbers.NumberLevelList.shuffle;
+import static fj.data.List.list;
+
 /**
  * List of levels used for the "mixed fractions" tab, for the levels in which number cards are used.
  *
@@ -13,10 +17,24 @@ import java.util.ArrayList;
  */
 public class MixedNumbersNumberLevelList extends ArrayList<NumberLevel> {
     public MixedNumbersNumberLevelList() {
-        for ( int i = 0; i < 10; i++ ) { add( level1() ); }
+        add( level1() );
+        while ( size() < 10 ) { add( levelX() ); }
     }
 
+    /*Level 1:
+    -- Circles as targets
+    -- {1:1/2, 2:1/2, 3:1/4} as the challenges
+    --just enough cards to complete targets
+    -- as before refreshing will randomly reorder, recolor
+    */
     private NumberLevel level1() {
-        return new NumberLevel( List.list( 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 5 ), List.replicate( 3, NumberTarget.target( 1, 2, 3, Color.red, NumberLevelList.pie.sequential() ) ) );
+        RandomColors3 colors = new RandomColors3();
+        return new NumberLevel( shuffle( list( NumberTarget.target( 1, 1, 2, colors.next(), pie.sequential() ),
+                                               NumberTarget.target( 2, 1, 2, colors.next(), pie.sequential() ),
+                                               NumberTarget.target( 3, 1, 4, colors.next(), pie.sequential() ) ) ) );
+    }
+
+    private NumberLevel levelX() {
+        return new NumberLevel( List.list( 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 5 ), List.replicate( 3, NumberTarget.target( 1, 2, 3, Color.red, pie.sequential() ) ) );
     }
 }
