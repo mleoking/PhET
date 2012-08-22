2b1ea33b8633642b3fab86e9a6aa3ba35b0d0f8e
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-08-22 02:25:39 +0000

    minor refactoring

--- simulations-java/simulations/functions/src/edu/colorado/phet/functions/intro/Graphic.java
+++ simulations-java/simulations/functions/src/edu/colorado/phet/functions/intro/Graphic.java
@@ -3,6 +3,11 @@ package edu.colorado.phet.functions.intro;
 
 import lombok.Data;
 
+import edu.colorado.phet.common.phetcommon.view.util.BufferedImageUtils;
+import edu.colorado.phet.functions.FunctionsResources.Images;
+import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.nodes.PImage;
+
 /**
  * @author Sam Reid
  */
@@ -10,4 +15,8 @@ public @Data class Graphic {
     public final int numRotations;
 
     public Graphic rotateRight() { return new Graphic( ( numRotations + 1 ) % 4 ); }
+
+    public PNode toNode() {
+        return new PImage( BufferedImageUtils.getRotatedImage( BufferedImageUtils.multiScaleToWidth( Images.KEY, 60 ), -Math.PI / 2 + getNumRotations() * Math.PI / 2 ) );
+    }
 }
