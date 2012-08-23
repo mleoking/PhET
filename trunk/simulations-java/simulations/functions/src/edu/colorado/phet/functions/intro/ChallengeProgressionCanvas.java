94fd839a9d9e4d31cba682d53efe06d92b6c299c
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-08-23 04:16:53 +0000

    factored out challenge progression ui

---
+++ trunk/simulations-java/simulations/functions/src/edu/colorado/phet/functions/intro/ChallengeProgressionCanvas.java
@@ -0,0 +1,71 @@
+// Copyright 2002-2012, University of Colorado
+package edu.colorado.phet.functions.intro;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import edu.colorado.phet.common.phetcommon.model.Resettable;
+import edu.colorado.phet.common.phetcommon.model.property.integerproperty.IntegerProperty;
+import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
+import edu.colorado.phet.common.piccolophet.nodes.HTMLImageButtonNode;
+import edu.colorado.phet.common.piccolophet.nodes.ResetAllButtonNode;
+import edu.colorado.phet.functions.buildafunction.AbstractFunctionsCanvas;
+import edu.colorado.phet.functions.buildafunction.BuildAFunctionCanvas;
+import edu.umd.cs.piccolo.PNode;
+
+/**
+ * @author Sam Reid
+ */
+public abstract class ChallengeProgressionCanvas extends AbstractFunctionsCanvas {
+    public static final long ANIMATION_DELAY = 200;
+    protected final ResetAllButtonNode resetAllButtonNode;
+    protected final HTMLImageButtonNode nextButton;
+    public final IntegerProperty level = new IntegerProperty( 0 );
+
+    public ChallengeProgressionCanvas() {
+        setBackground( BuildAFunctionCanvas.BACKGROUND_COLOR );
+        resetAllButtonNode = new ResetAllButtonNode( new Resettable() {
+            public void reset() {
+            }
+        }, this, new PhetFont( 18, true ), Color.black, Color.orange ) {{
+            setOffset( 1024 - this.getFullWidth() - INSET - INSET - INSET, 768 - this.getFullHeight() - INSET - 40 - 40 - 10 - INSET );
+        }};
+        addScreenChild( resetAllButtonNode );
+
+
+        nextButton = new HTMLImageButtonNode( "Next" ) {{
+            setFont( resetAllButtonNode.getFont() );
+            setBackground( Color.green );
+            setOffset( resetAllButtonNode.getFullBounds().getX(), resetAllButtonNode.getFullBounds().getMinY() - getFullBounds().getHeight() - 10 );
+        }};
+        nextButton.addActionListener( new ActionListener() {
+            public void actionPerformed( final ActionEvent e ) {
+                level.increment();
+                Integer level = ChallengeProgressionCanvas.this.level.get();
+                if ( level >= Scenes.scenes.length() ) {
+                    level = Scenes.scenes.length() - 1;
+                }
+                nextButtonPressed();
+            }
+        } );
+        addChild( nextButton );
+        nextButton.setVisible( false );
+    }
+
+    protected abstract void nextButtonPressed();
+
+    public void showNextButton() {
+        nextButton.setVisible( true );
+    }
+
+    protected void animateToNewScene( final PNode newScene ) {
+        nextButton.setVisible( false );
+        addChild( newScene );
+        newScene.setOffset( STAGE_SIZE.width, 0 );
+        newScene.animateToPositionScaleRotation( 0, 0, 1, 0, ANIMATION_DELAY );
+        finishAnimation( newScene );
+    }
+
+    protected abstract void finishAnimation( final PNode newScene );
+}
