8bd3656320398967aa515253d3daf7418632a625
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-08-23 22:58:44 +0000

    Had to manually add this for some reason.

---
+++ simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/energysystems/view/TeaPotNode.java
@@ -0,0 +1,35 @@
+// Copyright 2002-2012, University of Colorado
+package edu.colorado.phet.energyformsandchanges.energysystems.view;
+
+import edu.colorado.phet.common.phetcommon.model.property.Property;
+import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
+import edu.colorado.phet.common.piccolophet.nodes.HeaterCoolerNode;
+import edu.colorado.phet.energyformsandchanges.energysystems.model.TeaPot;
+import edu.umd.cs.piccolo.PNode;
+
+/**
+ * Piccolo node that represents a teapot with a burner beneath it in the view.
+ *
+ * @author John Blanco
+ */
+public class TeaPotNode extends PositionableFadableModelElementNode {
+
+    public TeaPotNode( TeaPot faucet, final ModelViewTransform mvt ) {
+        super( faucet, mvt );
+
+        // Add the burner.
+        // TODO: i18n
+        PNode heaterNode = new HeaterCoolerNode( new Property<Double>( 0.0 ), "Heat", "Cool" ) {{
+            setScale( 0.5 );
+        }};
+        addChild( heaterNode );
+
+        // Add the tea pot.
+        final PNode teaPotImageNode = new ModelElementImageNode( TeaPot.TEAPOT_IMAGE, mvt );
+        addChild( teaPotImageNode );
+
+        // Position the heater below the tea pot.
+        heaterNode.setOffset( teaPotImageNode.getFullBoundsReference().getCenterX() - heaterNode.getFullBoundsReference().width / 2,
+                              teaPotImageNode.getFullBoundsReference().getMaxY() + 30 );
+    }
+}
