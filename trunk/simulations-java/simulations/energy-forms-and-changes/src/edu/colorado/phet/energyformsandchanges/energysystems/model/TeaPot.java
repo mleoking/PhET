0bbbfb8e03cfb1c645103d1e0feb475b1ad26491
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-08-23 23:28:22 +0000

    Initial work on getting tea pot to output its energy more like a real tea pot.

--- trunk/simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/energysystems/model/TeaPot.java
+++ trunk/simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/energysystems/model/TeaPot.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import edu.colorado.phet.common.phetcommon.math.vector.Vector2D;
+import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.phetcommon.simsharing.messages.IUserComponent;
 import edu.colorado.phet.energyformsandchanges.EnergyFormsAndChangesResources;
 import edu.colorado.phet.energyformsandchanges.EnergyFormsAndChangesSimSharing;
@@ -18,7 +19,13 @@ import static edu.colorado.phet.energyformsandchanges.EnergyFormsAndChangesResou
  */
 public class TeaPot extends EnergySource {
 
-    private static final double ENERGY_PRODUCTION_RATE = 200; // In joules/second
+    //-------------------------------------------------------------------------
+    // Class Data
+    //-------------------------------------------------------------------------
+
+    private static final double MAX_ENERGY_PRODUCTION_RATE = 200; // In joules/second
+    private static final double MAX_ENERGY_CHANGE_RATE = 10; // In joules/second
+    private static final double COOLING_CONSTANT = 0.1; // Controls rate at which tea pot cools down, empirically determined.
 
     public static final ModelElementImage TEAPOT_IMAGE = new ModelElementImage( TEAPOT, new Vector2D( 0.02, 0.005 ) );
 
@@ -26,13 +33,41 @@ public class TeaPot extends EnergySource {
         add( TEAPOT_IMAGE );
     }};
 
+    //-------------------------------------------------------------------------
+    // Instance Data
+    //-------------------------------------------------------------------------
+
+    public final Property<Double> heatCoolAmount = new Property<Double>( 0.0 );
+
+    private double energyProductionRate = 0;
+
+    //-------------------------------------------------------------------------
+    // Constructor(s)
+    //-------------------------------------------------------------------------
+
     protected TeaPot() {
         super( EnergyFormsAndChangesResources.Images.TEAPOT_ICON, IMAGE_LIST );
     }
 
+    //-------------------------------------------------------------------------
+    // Methods
+    //-------------------------------------------------------------------------
+
     @Override public Energy stepInTime( double dt ) {
-        double producedEnergy = active ? ENERGY_PRODUCTION_RATE * dt : 0;
-        return new Energy( Energy.Type.MECHANICAL, producedEnergy, Math.PI / 2 );
+        if ( active ) {
+            double energyProductionIncreaseRate = heatCoolAmount.get() * MAX_ENERGY_CHANGE_RATE;
+            energyProductionRate = Math.min( energyProductionRate + energyProductionIncreaseRate * dt, MAX_ENERGY_PRODUCTION_RATE );
+            double energyProductionDecreaseRate = energyProductionRate * COOLING_CONSTANT;
+            energyProductionRate = energyProductionRate + energyProductionIncreaseRate * dt - energyProductionDecreaseRate * dt;
+        }
+        System.out.println( "energyProductionRate = " + energyProductionRate );
+        return new Energy( Energy.Type.MECHANICAL, energyProductionRate * dt, Math.PI / 2 );
+    }
+
+    @Override public void deactivate() {
+        super.deactivate();
+        heatCoolAmount.reset();
+        energyProductionRate = 0;
     }
 
     @Override public IUserComponent getUserComponent() {
