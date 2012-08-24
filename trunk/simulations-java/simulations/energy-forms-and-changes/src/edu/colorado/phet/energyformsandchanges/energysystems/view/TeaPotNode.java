b67e3eb027ff1b1a793e25be551ed5f6d867d411
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-08-24 18:23:53 +0000

    Added stand to the tea pot node, switched to the larger tea pot image, improved the layout.

--- trunk/simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/energysystems/view/TeaPotNode.java
+++ trunk/simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/energysystems/view/TeaPotNode.java
@@ -1,8 +1,11 @@
 // Copyright 2002-2012, University of Colorado
 package edu.colorado.phet.energyformsandchanges.energysystems.view;
 
+import java.awt.geom.Rectangle2D;
+
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
 import edu.colorado.phet.common.piccolophet.nodes.HeaterCoolerNode;
+import edu.colorado.phet.energyformsandchanges.common.view.BurnerStandNode;
 import edu.colorado.phet.energyformsandchanges.energysystems.model.TeaPot;
 import edu.umd.cs.piccolo.PNode;
 
@@ -16,19 +19,31 @@ public class TeaPotNode extends PositionableFadableModelElementNode {
     public TeaPotNode( TeaPot teaPot, final ModelViewTransform mvt ) {
         super( teaPot, mvt );
 
-        // Add the burner.
+        // Create the burner.
         // TODO: i18n
         PNode heaterNode = new HeaterCoolerNode( teaPot.heatCoolAmount, "Heat", "Cool" ) {{
             setScale( 0.5 );
         }};
-        addChild( heaterNode );
 
-        // Add the tea pot.
+        // Create the tea pot.
         final PNode teaPotImageNode = new ModelElementImageNode( TeaPot.TEAPOT_IMAGE, mvt );
-        addChild( teaPotImageNode );
 
-        // Position the heater below the tea pot.
-        heaterNode.setOffset( teaPotImageNode.getFullBoundsReference().getCenterX() - heaterNode.getFullBoundsReference().width / 2,
-                              teaPotImageNode.getFullBoundsReference().getMaxY() + 30 );
+        // Create the burner stand.
+        double burnerStandWidth = teaPotImageNode.getFullBoundsReference().getWidth() * 0.9;
+        double burnerStandHeight = burnerStandWidth * 0.8;
+        Rectangle2D burnerStandRect = new Rectangle2D.Double( teaPotImageNode.getFullBoundsReference().getCenterX() - burnerStandWidth / 2,
+                                                              teaPotImageNode.getFullBoundsReference().getMaxY() - 35,
+                                                              burnerStandWidth,
+                                                              burnerStandHeight );
+        PNode burnerStandNode = new BurnerStandNode( burnerStandRect, burnerStandWidth * 0.2 );
+
+        // Do the layout.
+        heaterNode.setOffset( burnerStandRect.getCenterX() - heaterNode.getFullBoundsReference().width / 2,
+                              burnerStandRect.getMaxY() - heaterNode.getFullBoundsReference().getHeight() + 15 );
+
+        // Add the nodes in the appropriate order for the desired layering affect.
+        addChild( heaterNode );
+        addChild( burnerStandNode );
+        addChild( teaPotImageNode );
     }
 }
