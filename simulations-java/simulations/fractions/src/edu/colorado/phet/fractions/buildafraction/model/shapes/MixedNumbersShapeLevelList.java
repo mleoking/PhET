97105e2655a4b2e4a55d05d48f0198e5eeae7e4c
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-08-27 16:53:56 +0000

    Added level 2

--- simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/buildafraction/model/shapes/MixedNumbersShapeLevelList.java
+++ simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/buildafraction/model/shapes/MixedNumbersShapeLevelList.java
@@ -1,7 +1,6 @@
 // Copyright 2002-2012, University of Colorado
 package edu.colorado.phet.fractions.buildafraction.model.shapes;
 
-import fj.Equal;
 import fj.F;
 import fj.data.List;
 
@@ -9,12 +8,16 @@ import java.awt.Color;
 import java.util.ArrayList;
 
 import edu.colorado.phet.fractions.buildafraction.model.MixedFraction;
-import edu.colorado.phet.fractions.buildafraction.model.numbers.NumberLevelList;
-import edu.colorado.phet.fractions.buildafraction.view.MixedNumbersLevelSelectionNode;
 import edu.colorado.phet.fractions.common.math.Fraction;
-import edu.colorado.phet.fractions.common.util.Sampling;
 
+import static edu.colorado.phet.fractions.buildafraction.model.numbers.MixedNumbersNumberLevelList.getMixedFractions;
+import static edu.colorado.phet.fractions.buildafraction.model.numbers.NumberLevelList.shuffle;
+import static edu.colorado.phet.fractions.buildafraction.model.shapes.ShapeLevel.shapeLevelMixed;
 import static edu.colorado.phet.fractions.buildafraction.model.shapes.ShapeLevelList.choosePiesOrBars;
+import static edu.colorado.phet.fractions.buildafraction.view.MixedNumbersLevelSelectionNode.colors;
+import static edu.colorado.phet.fractions.common.math.Fraction.fraction;
+import static edu.colorado.phet.fractions.common.util.Sampling.choose;
+import static edu.colorado.phet.fractions.common.util.Sampling.chooseOne;
 import static fj.data.List.list;
 import static fj.data.List.nil;
 
@@ -28,6 +31,7 @@ public class MixedNumbersShapeLevelList extends ArrayList<ShapeLevel> {
     //To avoid big piles, we probably want to do a check so that on any given draw, only a single "3" is drawn for a target whole number.  So there will never be a level where two targets appear with 3 as the whole number.
     public MixedNumbersShapeLevelList() {
         add( level1() );
+        add( level2() );
         while ( size() < 10 ) { add( testLevel() ); }
     }
 
@@ -37,28 +41,37 @@ public class MixedNumbersShapeLevelList extends ArrayList<ShapeLevel> {
     -- as before refreshing will randomly reorder targets, and choose between circles/rectangles
     -- a few extra pieces to allow multiple pathways to a solution (for instance, 2 halves that could form a whole)*/
     private ShapeLevel level1() {
-        List<MixedFraction> targets = List.list( new MixedFraction( 1, 1, 2 ), new MixedFraction( 2, 1, 2 ), new MixedFraction( 2, 1, 4 ) );
-        return ShapeLevel.shapeLevelMixed( subdivideOne( straightforwardCards( targets ) ), NumberLevelList.shuffle( targets ), MixedNumbersLevelSelectionNode.colors[0], choosePiesOrBars() );
+        List<MixedFraction> targets = shuffle( list( new MixedFraction( 1, 1, 2 ), new MixedFraction( 2, 1, 2 ), new MixedFraction( 2, 1, 4 ) ) );
+        return shapeLevelMixed( addSubdividedCards( straightforwardCards( targets ), 2 ), targets, colors[0], choosePiesOrBars() );
+    }
+
+    /*Level 2:
+    -- Targets with 1 or 2 as whole number, fractional portion from the set {1/2, 1/3, 2/3, 1/4, 3/4}
+    -- Wholes, 1/2's, 1/3's, and 1/4's
+    -- a few extra pieces to allow multiple pathways to a solution*/
+    private ShapeLevel level2() {
+        List<MixedFraction> targets = choose( 3, getMixedFractions( list( 1, 2 ), list( fraction( 1, 2 ), fraction( 1, 3 ), fraction( 2, 3 ), fraction( 1, 4 ), fraction( 3, 4 ) ) ) );
+        return shapeLevelMixed( addSubdividedCards( straightforwardCards( targets ), 2 ), shuffle( targets ), colors[1], choosePiesOrBars() );
     }
 
     //Take any of the cards, and subdivide it into smaller cards
-    private List<Integer> subdivideOne( final List<Integer> integers ) {
+    private List<Integer> addSubdividedCards( final List<Integer> integers, final int smallestPieceToSubdivide ) {
 
         //Find cards that can be divided into smaller cards
         List<Integer> allowed = integers.filter( new F<Integer, Boolean>() {
             @Override public Boolean f( final Integer integer ) {
-                return integer <= 4;
+                return integer <= smallestPieceToSubdivide;
             }
         } );
 
         //Choose one to subdivide
-        Integer oneToSubdivide = Sampling.chooseOne( allowed );
+        Integer oneToSubdivide = chooseOne( allowed );
 
         //Split into two smaller cards that add to the same thing
         List<Integer> subdivided = list( oneToSubdivide * 2, oneToSubdivide * 2 );
 
-        //Remove the old and add the new cards.
-        return integers.delete( oneToSubdivide, Equal.intEqual ).append( subdivided );
+        //Add the new cards.
+        return integers.append( subdivided );
     }
 
     //Just the exact cards necessary to fit the selected fractions
