a2866cdac59b9c8964bafb92c9a33324c6afd8ed
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-08-28 02:56:32 +0000

    added static imports

--- simulations-java/simulations/functions/src/edu/colorado/phet/functions/intro/Scenes.java
+++ simulations-java/simulations/functions/src/edu/colorado/phet/functions/intro/Scenes.java
@@ -19,13 +19,19 @@ import edu.colorado.phet.functions.FunctionsResources.Images;
 import edu.colorado.phet.functions.buildafunction.UnaryFunctionNode;
 import edu.colorado.phet.functions.buildafunction.ValueNode;
 import edu.colorado.phet.functions.intro.view.NavigationBar.ShapeIcon;
-import edu.colorado.phet.functions.model.Functions;
-import edu.colorado.phet.functions.model.Type;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.nodes.PImage;
 
+import static edu.colorado.phet.common.phetcommon.view.util.BufferedImageUtils.getRotatedImage;
+import static edu.colorado.phet.common.phetcommon.view.util.BufferedImageUtils.toBufferedImage;
+import static edu.colorado.phet.common.piccolophet.nodes.FaceNode.*;
+import static edu.colorado.phet.functions.FunctionsResources.Images.ROTATE_RIGHT;
+import static edu.colorado.phet.functions.intro.Scene.toStack;
 import static edu.colorado.phet.functions.model.Functions.*;
+import static edu.colorado.phet.functions.model.Type.*;
 import static fj.data.List.list;
+import static java.awt.Color.lightGray;
+import static java.lang.Math.PI;
 import static java.lang.Math.toRadians;
 
 /**
@@ -35,12 +41,12 @@ public class Scenes {
     public static F<ShapeValue, PNode> imageNode( final Image image ) {
         return new F<ShapeValue, PNode>() {
             @Override public PNode f( final ShapeValue graphic ) {
-                return new PImage( BufferedImageUtils.getRotatedImage( BufferedImageUtils.toBufferedImage( image ), -Math.PI / 2 + graphic.getNumRotations() * Math.PI / 2 ) );
+                return new PImage( getRotatedImage( toBufferedImage( image ), -PI / 2 + graphic.getNumRotations() * PI / 2 ) );
             }
         };
     }
 
-    private static final F<ShapeValue, PNode> smile = imageNode( new PadBoundsNode( new FaceNode( 60, FaceNode.HEAD_PAINT, new BasicStroke( 1 ), Color.lightGray, FaceNode.EYE_PAINT, FaceNode.MOUTH_PAINT ) ).toImage() );
+    private static final F<ShapeValue, PNode> smile = imageNode( new PadBoundsNode( new FaceNode( 60, HEAD_PAINT, new BasicStroke( 1 ), lightGray, EYE_PAINT, MOUTH_PAINT ) ).toImage() );
     private static final F<ShapeValue, PNode> triangle = imageNode( new ShapeIcon().toImage() );
 
     //copied from fractions
@@ -58,12 +64,12 @@ public class Scenes {
             //Level 1: Apply a single function
             new F<IntroCanvas, Scene>() {
                 @Override public Scene f( final IntroCanvas introCanvas ) {
-                    return new Scene( introCanvas, Scene.toStack( 3, new F<Unit, ValueNode>() {
+                    return new Scene( introCanvas, toStack( 3, new F<Unit, ValueNode>() {
                         @Override public ValueNode f( final Unit unit ) {
                             return new ValueNode( introCanvas, new ShapeValue( 0, smile ), new BasicStroke( 1 ), Color.white, Color.black, Color.black );
                         }
                     } ),
-                                      List.list( new UnaryFunctionNode( new PImage( BufferedImageUtils.multiScaleToWidth( Images.ROTATE_RIGHT, 60 ) ), false, Functions.ROTATE_GRAPHIC_RIGHT, Type.SHAPE, Type.SHAPE, 390.72378138847836, 294.298375184638 ) ),
+                                      List.list( new UnaryFunctionNode( new PImage( BufferedImageUtils.multiScaleToWidth( ROTATE_RIGHT, 60 ) ), false, ROTATE_GRAPHIC_RIGHT, SHAPE, SHAPE, 390.72378138847836, 294.298375184638 ) ),
                                       Scene.createTargetNodeList( introCanvas, list( new ShapeValue( 1, smile ) ) ) );
                 }
             },
@@ -71,12 +77,12 @@ public class Scenes {
             //Level 2: Apply same function twice
             new F<IntroCanvas, Scene>() {
                 @Override public Scene f( final IntroCanvas introCanvas ) {
-                    return new Scene( introCanvas, Scene.toStack( 3, new F<Unit, ValueNode>() {
+                    return new Scene( introCanvas, toStack( 3, new F<Unit, ValueNode>() {
                         @Override public ValueNode f( final Unit unit ) {
                             return new ValueNode( introCanvas, new ShapeValue( -1, smile ), new BasicStroke( 1 ), Color.white, Color.black, Color.black );
                         }
                     } ),
-                                      List.list( new UnaryFunctionNode( new PImage( BufferedImageUtils.multiScaleToWidth( Images.ROTATE_RIGHT, 60 ) ), false, Functions.ROTATE_GRAPHIC_RIGHT, Type.SHAPE, Type.SHAPE, 390.72378138847836, 294.298375184638 ) ),
+                                      List.list( new UnaryFunctionNode( new PImage( BufferedImageUtils.multiScaleToWidth( ROTATE_RIGHT, 60 ) ), false, ROTATE_GRAPHIC_RIGHT, SHAPE, SHAPE, 390.72378138847836, 294.298375184638 ) ),
                                       Scene.createTargetNodeList( introCanvas, list( new ShapeValue( 1, smile ) ) ) );
                 }
             },
@@ -84,12 +90,12 @@ public class Scenes {
             //Level 3: Apply different functions, one after the other
             new F<IntroCanvas, Scene>() {
                 @Override public Scene f( final IntroCanvas introCanvas ) {
-                    return new Scene( introCanvas, Scene.toStack( 3, new F<Unit, ValueNode>() {
+                    return new Scene( introCanvas, toStack( 3, new F<Unit, ValueNode>() {
                         @Override public ValueNode f( final Unit unit ) {
                             return new ValueNode( introCanvas, new ShapeValue( -1, triangle ), new BasicStroke( 1 ), Color.white, Color.black, Color.black );
                         }
                     } ),
-                                      List.list( new UnaryFunctionNode( new PImage( BufferedImageUtils.multiScaleToWidth( Images.ROTATE_RIGHT, 60 ) ), false, Functions.ROTATE_GRAPHIC_RIGHT, Type.SHAPE, Type.SHAPE, 390.72378138847836, 294.298375184638 ) ),
+                                      List.list( new UnaryFunctionNode( new PImage( BufferedImageUtils.multiScaleToWidth( ROTATE_RIGHT, 60 ) ), false, ROTATE_GRAPHIC_RIGHT, SHAPE, SHAPE, 390.72378138847836, 294.298375184638 ) ),
                                       Scene.createTargetNodeList( introCanvas, list( new ShapeValue( 1, triangle ) ) ) );
                 }
             },
@@ -98,13 +104,13 @@ public class Scenes {
             //First string level
             new F<IntroCanvas, Scene>() {
                 @Override public Scene f( final IntroCanvas canvas ) {
-                    return new Scene( canvas, Scene.toStack( 4, new F<Unit, ValueNode>() {
+                    return new Scene( canvas, toStack( 4, new F<Unit, ValueNode>() {
                         @Override public ValueNode f( final Unit unit ) {
                             return new ValueNode( canvas, "hello", new BasicStroke( 1 ), Color.white, Color.black, Color.black );
                         }
                     } ),
-                                      list( new UnaryFunctionNode( new PImage( BufferedImageUtils.multiScaleToWidth( Images.LEFT_RIGHT_ARROW, 80 ) ), false, STRING_REVERSE, Type.TEXT, Type.TEXT, 390.72378138847836, 294.298375184638 - 80 ),
-                                            new UnaryFunctionNode( new PImage( BufferedImageUtils.multiScaleToWidth( Images.COPY, 50 ) ), false, STRING_DOUBLE, Type.TEXT, Type.TEXT, 390.72378138847836, 444.298375184638 - 80 )
+                                      list( new UnaryFunctionNode( new PImage( BufferedImageUtils.multiScaleToWidth( Images.LEFT_RIGHT_ARROW, 80 ) ), false, STRING_REVERSE, TEXT, TEXT, 390.72378138847836, 294.298375184638 - 80 ),
+                                            new UnaryFunctionNode( new PImage( BufferedImageUtils.multiScaleToWidth( Images.COPY, 50 ) ), false, STRING_DOUBLE, TEXT, TEXT, 390.72378138847836, 444.298375184638 - 80 )
                                       ),
                                       Scene.createTargetNodeList( canvas, list( "olleh", "hellohello" ) )
                     );
@@ -116,13 +122,13 @@ public class Scenes {
 
             new F<IntroCanvas, Scene>() {
                 @Override public Scene f( final IntroCanvas canvas ) {
-                    return new Scene( canvas, Scene.toStack( 4, new F<Unit, ValueNode>() {
+                    return new Scene( canvas, toStack( 4, new F<Unit, ValueNode>() {
                         @Override public ValueNode f( final Unit unit ) {
                             return new ValueNode( canvas, 3, new BasicStroke( 1 ), Color.white, Color.black, Color.black );
                         }
                     } ),
-                                      list( new UnaryFunctionNode( "\u27152", false, INTEGER_TIMES_2, Type.NUMBER, Type.NUMBER, 390.72378138847836, 294.298375184638 - 80 ),
-                                            new UnaryFunctionNode( "+1", false, INTEGER_PLUS_1, Type.NUMBER, Type.NUMBER, 390.72378138847836, 444.298375184638 - 80 )
+                                      list( new UnaryFunctionNode( "\u27152", false, INTEGER_TIMES_2, NUMBER, NUMBER, 390.72378138847836, 294.298375184638 - 80 ),
+                                            new UnaryFunctionNode( "+1", false, INTEGER_PLUS_1, NUMBER, NUMBER, 390.72378138847836, 444.298375184638 - 80 )
                                       ),
                                       Scene.createTargetNodeList( canvas, list( 4, 6, 8 ) )
                     );
@@ -130,13 +136,13 @@ public class Scenes {
             },
             new F<IntroCanvas, Scene>() {
                 @Override public Scene f( final IntroCanvas canvas ) {
-                    return new Scene( canvas, Scene.toStack( 4, new F<Unit, ValueNode>() {
+                    return new Scene( canvas, toStack( 4, new F<Unit, ValueNode>() {
                         @Override public ValueNode f( final Unit unit ) {
                             return new ValueNode( canvas, 2, new BasicStroke( 1 ), Color.white, Color.black, Color.black );
                         }
                     } ),
-                                      list( new UnaryFunctionNode( "^2", false, INTEGER_POWER_2, Type.NUMBER, Type.NUMBER, 390.72378138847836, 294.298375184638 - 80 ),
-                                            new UnaryFunctionNode( "-1", false, INTEGER_MINUS_1, Type.NUMBER, Type.NUMBER, 390.72378138847836, 444.298375184638 - 80 )
+                                      list( new UnaryFunctionNode( "^2", false, INTEGER_POWER_2, NUMBER, NUMBER, 390.72378138847836, 294.298375184638 - 80 ),
+                                            new UnaryFunctionNode( "-1", false, INTEGER_MINUS_1, NUMBER, NUMBER, 390.72378138847836, 444.298375184638 - 80 )
                                       ),
                                       Scene.createTargetNodeList( canvas, list( 4, 9, 16 ) )
                     );
