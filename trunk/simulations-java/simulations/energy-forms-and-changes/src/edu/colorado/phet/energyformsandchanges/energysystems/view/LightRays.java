cd3623841914707b01ca001d343d8ab2981705d2
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-08-28 18:42:30 +0000

    Added lines that make up the rays.

--- trunk/simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/energysystems/view/LightRays.java
+++ trunk/simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/energysystems/view/LightRays.java
@@ -1,8 +1,10 @@
 // Copyright 2002-2012, University of Colorado
 package edu.colorado.phet.energyformsandchanges.energysystems.view;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
-import java.awt.geom.Ellipse2D;
+import java.awt.Stroke;
+import java.awt.geom.Line2D;
 
 import edu.colorado.phet.common.phetcommon.math.vector.Vector2D;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
@@ -15,8 +17,16 @@ import edu.umd.cs.piccolo.PNode;
  */
 public class LightRays extends PNode {
 
+    private static Stroke RAY_STROKE = new BasicStroke( 2 );
+
     public LightRays( Vector2D center, double innerRadius, double outerRadius, int numRays, Color color ) {
-        addChild( new PhetPPath( new Ellipse2D.Double( -outerRadius, -outerRadius, outerRadius * 2, outerRadius * 2 ), color ) );
+        assert numRays > 0 && outerRadius > innerRadius; // Parameter checking.
+        for ( int i = 0; i < numRays; i++ ) {
+            double angle = ( Math.PI * 2 / numRays ) * i;
+            Vector2D rayStartPoint = new Vector2D( outerRadius - innerRadius, 0 ).getRotatedInstance( angle );
+            Vector2D rayEndPoint = new Vector2D( outerRadius, 0 ).getRotatedInstance( angle );
+            addChild( new PhetPPath( new Line2D.Double( rayStartPoint.toPoint2D(), rayEndPoint.toPoint2D() ), RAY_STROKE, color ) );
+        }
         setOffset( center.toPoint2D() );
     }
 }
