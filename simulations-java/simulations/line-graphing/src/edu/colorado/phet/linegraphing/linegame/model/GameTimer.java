091a30b13d919a9503cb0c2156c5ee03dc0691a5
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-08-29 16:42:23 +0000

    generalizations and doc for GameTimer

--- simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/linegame/model/GameTimer.java
+++ simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/linegame/model/GameTimer.java
@@ -4,6 +4,7 @@ package edu.colorado.phet.linegraphing.linegame.model;
 
 import edu.colorado.phet.common.phetcommon.model.clock.ClockAdapter;
 import edu.colorado.phet.common.phetcommon.model.clock.ClockEvent;
+import edu.colorado.phet.common.phetcommon.model.clock.ClockListener;
 import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
 import edu.colorado.phet.common.phetcommon.model.clock.IClock;
 import edu.colorado.phet.common.phetcommon.model.property.Property;
@@ -21,6 +22,10 @@ public class GameTimer {
     private final IClock clock;
     private long startTime; // System time in ms when the game started
 
+    /**
+     * Constructor that uses a default clock.
+     * The frame rate of this clock is sufficient for displaying a timer, but not for driving smooth animation.
+     */
     public GameTimer() {
         this( new ConstantDtClock( 1000 / 5, 1 ) ); // don't need to tick too often to update the scoreboard time
     }
@@ -36,20 +41,45 @@ public class GameTimer {
         } );
     }
 
-    // Starts the timer. When the timer is started, it restarts from zero.
-    public void start() {
-        time.set( 0L );
-        startTime = System.currentTimeMillis(); // don't use clock.getWallTime, it's not valid until the clock ticks
-        clock.start();
-    }
-
-    // Stops the timer.
-    public void stop() {
-        clock.pause();
-    }
-
-    // Is the timer running?
+    /**
+     *  Is the timer running?
+     *  @returns true or false
+     */
     public boolean isRunning() {
         return clock.isRunning();
+    }
+
+    /**
+     * Starts the timer. When the timer is started, it restarts from zero.
+     * If the timer is already running, this is a no-op.
+     */
+    public void start() {
+        if ( !isRunning() ) {
+            time.set( 0L );
+            startTime = System.currentTimeMillis(); // don't use clock.getWallTime, it's not valid until the clock ticks
+            clock.start();
+        }
+    }
+
+    /**
+     * Stops the timer.
+     * If the timer is already stopped, this is a no-op.
+     */
+    public void stop() {
+        if ( isRunning() ) {
+            clock.pause();
+        }
+    }
+
+    /**
+     *  Convenience method for starting/stopping the timer.
+     */
+    public void setRunning( boolean running ) {
+        if ( running ) {
+            start();
+        }
+        else {
+            stop();
+        }
     }
 }
