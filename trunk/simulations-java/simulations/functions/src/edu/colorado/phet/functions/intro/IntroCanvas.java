9c5257343ebb7d3597ff7eb19932909fa2be1bbe
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-08-30 01:59:52 +0000

    improved navigation bar

--- trunk/simulations-java/simulations/functions/src/edu/colorado/phet/functions/intro/IntroCanvas.java
+++ trunk/simulations-java/simulations/functions/src/edu/colorado/phet/functions/intro/IntroCanvas.java
@@ -3,6 +3,7 @@ package edu.colorado.phet.functions.intro;
 import fj.F;
 import fj.data.List;
 
+import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
 import edu.colorado.phet.functions.buildafunction.ValueContext;
 import edu.colorado.phet.functions.buildafunction.ValueNode;
 import edu.colorado.phet.functions.intro.view.NavigationBar;
@@ -19,21 +20,25 @@ public class IntroCanvas extends ChallengeProgressionCanvas implements ValueCont
     private Scene scene;
     private List<F<IntroCanvas, Scene>> scenes;
 
-    public IntroCanvas( List<F<IntroCanvas, Scene>> scenes ) {
+    public IntroCanvas( final List<F<IntroCanvas, Scene>> scenes ) {
         this.scenes = scenes;
 
-        addChild( new NavigationBar() {{
-            setOffset( STAGE_SIZE.width / 2 - getFullBounds().getWidth() / 2, INSET );
+        addChild( new NavigationBar( level ) {{
+            setOffset( STAGE_SIZE.width / 2 - getFullBounds().getWidth() / 2, 4 );
         }} );
 
         scene = scenes.index( level.get() ).f( this );
         addChild( scene );
+
+        level.addObserver( new VoidFunction1<Integer>() {
+            public void apply( final Integer integer ) {
+                final Integer index = Math.min( scenes.length() - 1, integer );
+                animateToNewScene( scenes.index( index ).f( IntroCanvas.this ) );
+            }
+        } );
     }
 
-    @Override protected void nextButtonPressed() {
-        final Integer index = Math.min( scenes.length() - 1, level.get() );
-        animateToNewScene( scenes.index( index ).f( this ) );
-    }
+    @Override protected int getLevelCount() { return scenes.length();}
 
     @Override protected void finishAnimation( final PNode newScene ) {
         scene.animateToPositionScaleRotation( -STAGE_SIZE.width, 0, 1, 0, ANIMATION_DELAY ).setDelegate( new PActivityDelegate() {
