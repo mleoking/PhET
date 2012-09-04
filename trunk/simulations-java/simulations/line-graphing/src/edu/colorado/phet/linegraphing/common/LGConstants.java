2a668ab47d80550dfa6ab9b0a93b2045d27c1d4d
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-09-04 23:42:16 +0000

    extract constants for range of axes

--- trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/common/LGConstants.java
+++ trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/common/LGConstants.java
@@ -3,6 +3,7 @@ package edu.colorado.phet.linegraphing.common;
 
 import java.awt.Font;
 
+import edu.colorado.phet.common.phetcommon.util.IntegerRange;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 
 /**
@@ -11,6 +12,9 @@ import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
 public class LGConstants {
+
+    public static final IntegerRange X_AXIS_RANGE = new IntegerRange( -10, 10 );
+    public static final IntegerRange Y_AXIS_RANGE = X_AXIS_RANGE;
 
     public static final int CONTROL_FONT_SIZE = 18;
     public static final PhetFont CONTROL_FONT = new PhetFont( CONTROL_FONT_SIZE );
