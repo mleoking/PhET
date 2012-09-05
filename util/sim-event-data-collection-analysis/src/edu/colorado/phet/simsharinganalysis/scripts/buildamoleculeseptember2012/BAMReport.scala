c1c1ba4b66096c7fd1fc3aa7c684915412a473e1
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-09-05 20:06:34 +0000

    started adding support for BAM

---
+++ util/sim-event-data-collection-analysis/src/edu/colorado/phet/simsharinganalysis/scripts/buildamoleculeseptember2012/BAMReport.scala
@@ -0,0 +1,10 @@
+package edu.colorado.phet.simsharinganalysis.scripts.buildamoleculeseptember2012
+
+import edu.colorado.phet.simsharinganalysis.Log
+
+/**
+ * @author Sam Reid
+ */
+class BAMReport(log: Log) {
+  def reportText = "Hello"
+}
