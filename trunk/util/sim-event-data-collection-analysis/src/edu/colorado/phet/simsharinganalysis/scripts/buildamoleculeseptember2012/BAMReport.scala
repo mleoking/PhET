34920408a7bbb2aea7450f30239750828d117f5c
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-09-05 22:40:56 +0000

    keep track of molecules collected

--- trunk/util/sim-event-data-collection-analysis/src/edu/colorado/phet/simsharinganalysis/scripts/buildamoleculeseptember2012/BAMReport.scala
+++ trunk/util/sim-event-data-collection-analysis/src/edu/colorado/phet/simsharinganalysis/scripts/buildamoleculeseptember2012/BAMReport.scala
@@ -1,7 +1,7 @@
 package edu.colorado.phet.simsharinganalysis.scripts.buildamoleculeseptember2012
 
 import edu.colorado.phet.simsharinganalysis.{phet, Log}
-import BAMStateMachine.tabs
+import BAMStateMachine.tabNames
 import edu.colorado.phet.buildamolecule.BuildAMoleculeSimSharing
 import java.io.File
 
@@ -24,7 +24,7 @@ class BAMReport(log: Log) {
   val timeSimOpenInMinutes = millisecondsToMinutes(states.last.end.time - states.head.start.time)
 
   def timeOnTab(tab: String) = {
-    val tabIndex = tabs.indexOf(tab)
+    val tabIndex = tabNames.indexOf(tab)
     millisecondsToMinutes(states.filter(_.start.tab == tabIndex).map(_.time).sum)
   }
 
@@ -33,14 +33,18 @@ class BAMReport(log: Log) {
     "Number of times pressing " + component + ": " + count + "\n"
   }
 
-  def reportText = "Previous state: " + states.last.start + "\n" +
+  lazy val collectedMolecules = states.flatMap(state => state.end.tab0.collected ++ state.end.tab1.collected ++ state.end.tab2.collected).distinct
+
+  def reportText = "File: " + log.file + "\n" +
+                   "Previous state: " + states.last.start + "\n" +
                    "Entry: " + states.last.entry + "\n" +
                    "Current State: " + states.last.end + "\n" +
                    "Time sim open: " + timeSimOpenInMinutes + "\n" +
                    "start time: " + states.head.start.time + "\n" +
                    "end time: " + states.last.end.time + "\n" +
-                   tabs.map(tab => "Time on tab " + ( tabs.indexOf(tab) + 1 ) + " (" + tab + "): " + timeOnTab(tab)).mkString("\n") + "\n" +
+                   tabNames.map(tab => "Time on tab " + ( tabNames.indexOf(tab) + 1 ) + " (" + tab + "): " + timeOnTab(tab)).mkString("\n") + "\n" +
                    buttonReport(BuildAMoleculeSimSharing.UserComponent.breakApartButton.name()) +
                    buttonReport(BuildAMoleculeSimSharing.UserComponent.jmol3DButton.name()) +
-                   buttonReport(BuildAMoleculeSimSharing.UserComponent.scissorsButton.name())
+                   buttonReport(BuildAMoleculeSimSharing.UserComponent.scissorsButton.name()) +
+                   "Molecules collected: " + collectedMolecules.mkString(", ")
 }
