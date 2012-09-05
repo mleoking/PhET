8387ead89a410c03648be70d48ff87936fa2bb1c
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-09-05 23:06:42 +0000

    improved format for collected molecules output

--- util/sim-event-data-collection-analysis/src/edu/colorado/phet/simsharinganalysis/scripts/buildamoleculeseptember2012/BAMStateMachine.scala
+++ util/sim-event-data-collection-analysis/src/edu/colorado/phet/simsharinganalysis/scripts/buildamoleculeseptember2012/BAMStateMachine.scala
@@ -17,7 +17,7 @@ class BAMStateMachine extends StateMachine[SimState] {
   //Given the current state and an entry, compute the next state
   def initialState(log: Log) = init(log.startTime)
 
-  def init(time: Long) = SimState(time, 0, TabState(Nil), TabState(Nil), TabState(Nil))
+  def init(time: Long) = SimState(time, 0, TabState(Nil, 0, 0), TabState(Nil, 0, 0), TabState(Nil, 0, 0))
 
   def nextState(state: SimState, e: Entry) = {
 
@@ -27,9 +27,9 @@ class BAMStateMachine extends StateMachine[SimState] {
     else if ( e.componentType == "tab" ) state.copy(tab = BAMStateMachine.tabNames.indexOf(e.component), time = e.time)
 
     //1346883561948	model	collectionBox	modelElement	moleculePutInCollectionBox	collectionBoxMolecularFormula = H2O	collectionBoxCommonName = Water	collectionBoxCID = 962	collectionBoxQuantity = 1	collectionBoxCapacity = 1	moleculeId = 69120
-    else if ( e.messageType == "model" && e.component == "collectionBox" && state.tab == 0 ) state.copy(time = e.time, tab0 = state.tab0.collect(e("collectionBoxCommonName")))
-    else if ( e.messageType == "model" && e.component == "collectionBox" && state.tab == 1 ) state.copy(time = e.time, tab1 = state.tab1.collect(e("collectionBoxCommonName")))
-    else if ( e.messageType == "model" && e.component == "collectionBox" && state.tab == 2 ) state.copy(time = e.time, tab2 = state.tab2.collect(e("collectionBoxCommonName")))
+    else if ( e.messageType == "model" && e.component == "collectionBox" && state.tab == 0 ) state.copy(time = e.time, tab0 = state.tab0.collect(e("collectionBoxCommonName"), e.time))
+    else if ( e.messageType == "model" && e.component == "collectionBox" && state.tab == 1 ) state.copy(time = e.time, tab1 = state.tab1.collect(e("collectionBoxCommonName"), e.time))
+    else if ( e.messageType == "model" && e.component == "collectionBox" && state.tab == 2 ) state.copy(time = e.time, tab2 = state.tab2.collect(e("collectionBoxCommonName"), e.time))
     else state.copy(time = e.time)
   }
 }
