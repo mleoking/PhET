5ebc2d0c3d708dd78385d07f9aaf7e22824ea25e
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-09-05 19:12:39 +0000

    format

--- simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/linegame/model/MatchingChallenge.java
+++ simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/linegame/model/MatchingChallenge.java
@@ -22,8 +22,8 @@ public abstract class MatchingChallenge<T extends PointSlopeLine> {
 
     public final T answer; // the correct answer
     public final Property<T> guess; // the user's current guess
-    public final ModelViewTransform mvt; // transform between model and view coordinate frames
     public final LineFactory<T> lineFactory; // instantiates lines
+    public final ModelViewTransform mvt; // transform between model and view coordinate frames
 
     public MatchingChallenge( T answer, T guess, LineFactory<T> lineFactory, ModelViewTransform mvt ) {
         this.answer = answer;
