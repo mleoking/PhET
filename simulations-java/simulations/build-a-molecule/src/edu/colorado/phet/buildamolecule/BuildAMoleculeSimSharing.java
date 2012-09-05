2d5ff77e1c7d2f31f371ccc0c80a1f18c307c41d
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-09-05 20:05:24 +0000

    bam: molecule reactions included in sim-sharing

--- simulations-java/simulations/build-a-molecule/src/edu/colorado/phet/buildamolecule/BuildAMoleculeSimSharing.java
+++ simulations-java/simulations/build-a-molecule/src/edu/colorado/phet/buildamolecule/BuildAMoleculeSimSharing.java
@@ -1,6 +1,8 @@
 // Copyright 2002-2012, University of Colorado
 package edu.colorado.phet.buildamolecule;
 
+import edu.colorado.phet.common.phetcommon.simsharing.messages.IModelAction;
+import edu.colorado.phet.common.phetcommon.simsharing.messages.IModelComponent;
 import edu.colorado.phet.common.phetcommon.simsharing.messages.IParameterKey;
 import edu.colorado.phet.common.phetcommon.simsharing.messages.IUserComponent;
 
@@ -10,6 +12,34 @@ public class BuildAMoleculeSimSharing {
     }
 
     public enum ParameterKey implements IParameterKey {
-        atomSymbol, atomReference
+        atomId, atomElement,
+
+        atomWasInKitArea,
+        atomDroppedInKitArea,
+
+        bondOccurs,
+        bondAtomA,
+        bondAtomB,
+        bondDirection,
+
+        bondMoleculeDestroyedA,
+        bondMoleculeDestroyedB,
+        bondMoleculeCreated,
+
+        moleculeStructureDestroyedA,
+        moleculeStructureDestroyedB,
+        moleculeStructureCreated
     }
+
+    public enum ModelComponent implements IModelComponent {
+        atom,
+        molecule
+    }
+
+    public enum ModelAction implements IModelAction {
+        atomDropped,
+        bondAttempt,
+        bonding
+    }
+
 }
