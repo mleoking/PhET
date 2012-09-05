9309d48851027a3e4e6bcefc4a1e458317c67ac0
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-09-05 21:06:58 +0000

    improved time readout

--- util/sim-event-data-collection-analysis/src/edu/colorado/phet/simsharinganalysis/scripts/buildamoleculeseptember2012/BAMStateMachine.scala
+++ util/sim-event-data-collection-analysis/src/edu/colorado/phet/simsharinganalysis/scripts/buildamoleculeseptember2012/BAMStateMachine.scala
@@ -3,12 +3,14 @@ package edu.colorado.phet.simsharinganalysis.scripts.buildamoleculeseptember2012
 import edu.colorado.phet.simsharinganalysis.scripts.StateMachine
 import edu.colorado.phet.simsharinganalysis.{Log, Entry}
 
+object BAMStateMachine {
+  val tabs = List("makeMoleculesTab", "collectMultipleTab", "largerMoleculesTab")
+}
+
 /**
  * @author Sam Reid
  */
 class BAMStateMachine extends StateMachine[SimState] {
-
-  val tabs = List("makeMoleculesTab", "collectMultipleTab", "largerMoleculesTab")
 
   //Given the current state and an entry, compute the next state
   def initialState(log: Log) = SimState(log.startTime, 0)
@@ -18,7 +20,7 @@ class BAMStateMachine extends StateMachine[SimState] {
     //When the sim gets reset, go back to the first state
     if ( e.messageType == "system" && e.component == "application" && e.action == "exited" ) SimState(e.time, 0)
     else if ( e.enabled == false ) state.copy(time = e.time)
-    else if ( e.componentType == "tab" ) state.copy(tab = tabs.indexOf(e.component), time = e.time)
+    else if ( e.componentType == "tab" ) state.copy(tab = BAMStateMachine.tabs.indexOf(e.component), time = e.time)
     //    else if ( state.tab == 0 ) state.copy(tab0 = state.tab0.next(e), time = e.time)
     //    else if ( state.tab == 1 ) state.copy(tab1 = state.tab1.next(e), time = e.time)
     //    else state.copy(tab1 = state.tab1.next(e), time = e.time)
