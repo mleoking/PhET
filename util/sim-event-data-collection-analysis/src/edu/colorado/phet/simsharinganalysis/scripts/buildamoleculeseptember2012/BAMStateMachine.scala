54f5c4c8360fa0e97e15c536540e48b38ced4b2f
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-09-05 22:40:56 +0000

    keep track of molecules collected

--- util/sim-event-data-collection-analysis/src/edu/colorado/phet/simsharinganalysis/scripts/buildamoleculeseptember2012/BAMStateMachine.scala
+++ util/sim-event-data-collection-analysis/src/edu/colorado/phet/simsharinganalysis/scripts/buildamoleculeseptember2012/BAMStateMachine.scala
@@ -2,14 +2,11 @@ package edu.colorado.phet.simsharinganalysis.scripts.buildamoleculeseptember2012
 
 import edu.colorado.phet.simsharinganalysis.scripts.StateMachine
 import edu.colorado.phet.simsharinganalysis.{Log, Entry}
-import edu.colorado.phet.buildamolecule.BuildAMoleculeSimSharing
+import edu.colorado.phet.buildamolecule.BuildAMoleculeSimSharing.UserComponent._
 
 object BAMStateMachine {
-  val tabs = List(
-    BuildAMoleculeSimSharing.UserComponent.makeMoleculesTab,
-    BuildAMoleculeSimSharing.UserComponent.collectMultipleTab,
-    BuildAMoleculeSimSharing.UserComponent.largerMoleculesTab
-  ).map( e => e.name() )
+  private val tabs = List(makeMoleculesTab, collectMultipleTab, largerMoleculesTab)
+  val tabNames = tabs.map(_.name)
 }
 
 /**
@@ -18,17 +15,21 @@ object BAMStateMachine {
 class BAMStateMachine extends StateMachine[SimState] {
 
   //Given the current state and an entry, compute the next state
-  def initialState(log: Log) = SimState(log.startTime, 0)
+  def initialState(log: Log) = init(log.startTime)
+
+  def init(time: Long) = SimState(time, 0, TabState(Nil), TabState(Nil), TabState(Nil))
 
   def nextState(state: SimState, e: Entry) = {
 
     //When the sim gets reset, go back to the first state
-    if ( e.messageType == "system" && e.component == "application" && e.action == "exited" ) SimState(e.time, 0)
+    if ( e.messageType == "system" && e.component == "application" && e.action == "exited" ) init(e.time)
     else if ( e.enabled == false ) state.copy(time = e.time)
-    else if ( e.componentType == "tab" ) state.copy(tab = BAMStateMachine.tabs.indexOf( e.component ), time = e.time)
-    //    else if ( state.tab == 0 ) state.copy(tab0 = state.tab0.next(e), time = e.time)
-    //    else if ( state.tab == 1 ) state.copy(tab1 = state.tab1.next(e), time = e.time)
-    //    else state.copy(tab1 = state.tab1.next(e), time = e.time)
+    else if ( e.componentType == "tab" ) state.copy(tab = BAMStateMachine.tabNames.indexOf(e.component), time = e.time)
+
+    //1346883561948	model	collectionBox	modelElement	moleculePutInCollectionBox	collectionBoxMolecularFormula = H2O	collectionBoxCommonName = Water	collectionBoxCID = 962	collectionBoxQuantity = 1	collectionBoxCapacity = 1	moleculeId = 69120
+    else if ( e.messageType == "model" && e.component == "collectionBox" && state.tab == 0 ) state.copy(time = e.time, tab0 = state.tab0.collect(e("collectionBoxCommonName")))
+    else if ( e.messageType == "model" && e.component == "collectionBox" && state.tab == 1 ) state.copy(time = e.time, tab1 = state.tab1.collect(e("collectionBoxCommonName")))
+    else if ( e.messageType == "model" && e.component == "collectionBox" && state.tab == 2 ) state.copy(time = e.time, tab2 = state.tab2.collect(e("collectionBoxCommonName")))
     else state.copy(time = e.time)
   }
 }
--- util/sim-event-data-collection-analysis/src/edu/colorado/phet/simsharinganalysis/scripts/buildamoleculeseptember2012/SimState.scala
+++ util/sim-event-data-collection-analysis/src/edu/colorado/phet/simsharinganalysis/scripts/buildamoleculeseptember2012/SimState.scala
@@ -3,5 +3,15 @@ package edu.colorado.phet.simsharinganalysis.scripts.buildamoleculeseptember2012
 /**
  * @author Sam Reid
  */
-case class SimState(time: Long, tab: Int) {
+case class SimState(time: Long, tab: Int, tab0: TabState, tab1: TabState, tab2: TabState) {
+  def currentTab = tab match {
+    case 0 => tab0
+    case 1 => tab1
+    case 2 => tab2
+    case _ => throw new RuntimeException("Tab not found")
+  }
+}
+
+case class TabState(collected: List[String]) {
+  def collect(commonName: String) = copy(collected = collected ::: List(commonName))
 }
