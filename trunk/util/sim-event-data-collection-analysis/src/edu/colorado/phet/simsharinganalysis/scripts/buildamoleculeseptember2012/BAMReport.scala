748f224638040c56d5a0580d7304ba1c0491bbbc
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-09-05 21:06:58 +0000

    improved time readout

--- trunk/util/sim-event-data-collection-analysis/src/edu/colorado/phet/simsharinganalysis/scripts/buildamoleculeseptember2012/BAMReport.scala
+++ trunk/util/sim-event-data-collection-analysis/src/edu/colorado/phet/simsharinganalysis/scripts/buildamoleculeseptember2012/BAMReport.scala
@@ -1,20 +1,28 @@
 package edu.colorado.phet.simsharinganalysis.scripts.buildamoleculeseptember2012
 
 import edu.colorado.phet.simsharinganalysis.Log
+import BAMStateMachine.tabs
 
 /**
  * @author Sam Reid
  */
 class BAMReport(log: Log) {
-  def millisecondsToMinutes(milliseconds: Long) = milliseconds / 60000.0
+
+  def millisecondsToMinutes(millis: Long) = JavaTiming.formatMillis(millis)
 
   val states = new BAMStateMachine().getStates(log)
   val timeSimOpenInMinutes = millisecondsToMinutes(states.last.end.time - states.head.start.time)
+
+  def timeOnTab(tab: String) = {
+    val tabIndex = tabs.indexOf(tab)
+    millisecondsToMinutes(states.filter(_.start.tab == tabIndex).map(_.time).sum)
+  }
 
   def reportText = "Previous state: " + states.last.start + "\n" +
                    "Entry: " + states.last.entry + "\n" +
                    "Current State: " + states.last.end + "\n" +
                    "Time sim open: " + timeSimOpenInMinutes + "\n" +
                    "start time: " + states.head.start.time + "\n" +
-                   "end time: " + states.last.end.time
+                   "end time: " + states.last.end.time + "\n" +
+                   tabs.map(tab => "Time on tab " + ( tabs.indexOf(tab) + 1 ) + " (" + tab + "): " + timeOnTab(tab)).mkString("\n")
 }
