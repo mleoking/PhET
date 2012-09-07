d92c1b43e2590acba83399b0f12019fd732ccb65
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-09-07 16:18:27 +0000

    introduce PointPointLine as the base class for the line model hierarchy

--- simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/common/model/LineFactory.java
+++ simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/common/model/LineFactory.java
@@ -3,32 +3,44 @@ package edu.colorado.phet.linegraphing.common.model;
 
 import java.awt.Color;
 
+import edu.colorado.phet.common.phetcommon.math.MathUtil;
+
 /**
  * Factories for creating lines.
  * These factories are used to handle instantiation of lines in classes that are parameterized using generics.
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
-public interface LineFactory<T extends PointSlopeLine> {
-
-    // Creates a simplified version of the specified line. Simplification involves reducing the slope.
-    public T simplify( T line );
+public abstract class LineFactory<T extends PointSlopeLine> {
 
     // Creates a line with a different point
-    public T withPoint( T line, double x1, double y1 );
+    public abstract T withPoint( T line, double x1, double y1 );
 
     // Creates a line with a different slope
-    public T withSlope( T line, double rise, double run );
+    public abstract T withSlope( T line, double rise, double run );
 
     // Creates a line with a different color
-    public T withColor( T line, Color color );
+    public abstract T withColor( T line, Color color );
+
+    /*
+     * Creates a simplified instance of the line.
+     * For our purposes, this means simplifying (aka, reducing) the slope.
+     * Simplification uses Euclid's algorithm for computing the greatest common divisor (GCD) of two integers,
+     * so this is effective only if the rise and run are integer values. Otherwise 'this' is returned.
+     */
+    public T simplified( T line ) {
+        if ( ( line.rise == (int) line.rise ) && ( line.run == (int) line.run ) ) { // true if rise and run are integers
+            final int reducedRise = (int) ( line.rise / MathUtil.getGreatestCommonDivisor( (int) line.rise, (int) line.run ) );
+            final int reducedRun = (int) ( line.run / MathUtil.getGreatestCommonDivisor( (int) line.rise, (int) line.run ) );
+            return withSlope( line, reducedRise, reducedRun );
+        }
+        else {
+            return line;
+        }
+    }
 
     // Factory for creating lines in point-slope form.
-    public static class PointSlopeLineFactory implements LineFactory<PointSlopeLine> {
-
-        public PointSlopeLine simplify( PointSlopeLine line ) {
-            return line.simplified();
-        }
+    public static class PointSlopeLineFactory extends LineFactory<PointSlopeLine> {
 
         public PointSlopeLine withPoint( PointSlopeLine line, double x1, double y1 ) {
             return new PointSlopeLine( x1, y1, line.rise, line.run, line.color );
@@ -44,13 +56,7 @@ public interface LineFactory<T extends PointSlopeLine> {
     }
 
     // Factory for creating lines in slope-intercept form.
-    public static class SlopeInterceptLineFactory implements LineFactory<SlopeInterceptLine> {
-
-        public SlopeInterceptLine simplify( SlopeInterceptLine line ) {
-            PointSlopeLine psLine = line.simplified();
-            assert ( psLine.x1 == 0 ); //TODO would be nice if we could avoid this
-            return new SlopeInterceptLine( psLine.rise, psLine.run, psLine.y1, psLine.color );
-        }
+    public static class SlopeInterceptLineFactory extends LineFactory<SlopeInterceptLine> {
 
         public SlopeInterceptLine withPoint( SlopeInterceptLine line, double x1, double y1 ) {
             assert ( x1 == 0 ); //TODO would be nice if we could avoid this
--- simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/common/model/PointSlopeLine.java
+++ simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/common/model/PointSlopeLine.java
@@ -3,92 +3,14 @@ package edu.colorado.phet.linegraphing.common.model;
 
 import java.awt.Color;
 
-import edu.colorado.phet.common.phetcommon.math.MathUtil;
-
 /**
  * An immutable line, specified in point-slope form: (y - y1) = m(x - x1)
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
-public class PointSlopeLine {
-
-    public final double x1, y1; // x and y components of the point
-    public final double rise, run; // vertical and horizontal components of the slope
-    public final Color color; // color used for visualizing the line
+public class PointSlopeLine extends PointPointLine {
 
     public PointSlopeLine( double x1, double y1, double rise, double run, Color color ) {
-        assert( !( rise == 0 && run == 0 ) ); // a line with slope=0/0 is undefined
-        this.rise = rise;
-        this.run = run;
-        this.x1 = x1;
-        this.y1 = y1;
-        this.color = color;
-    }
-
-    /*
-     * Given x, solve y = m(x - x1) + y1
-     * Returns Double.NaN if the solution is not unique, or there is no solution (x can't possibly be on the line.)
-     * This occurs when we have a vertical line, with no rise.
-     */
-    public double solveY( double x ) {
-        if ( run == 0 ) {
-            return Double.NaN;
-        }
-        else {
-            return ( ( rise / run ) * ( x - x1 ) ) + y1;
-        }
-    }
-
-    /*
-     * Given y, solve x = ((y - y1)/m) + x1
-     * Returns Double.NaN if the solution is not unique, or there is no solution (y can't possibly be on the line.)
-     * This occurs when we have a horizontal line, with no run.
-     */
-    public double solveX( double y ) {
-        if ( rise == 0 ) {
-            return Double.NaN;
-        }
-        else {
-            return ( ( y - y1 ) / ( rise / run ) ) + x1;
-        }
-    }
-
-    /*
-     * Creates a simplified instance of the line.
-     * For our purposes, this means simplifying (aka, reducing) the slope.
-     * Simplification uses Euclid's algorithm for computing the greatest common divisor (GCD) of two integers,
-     * so this is effective only if the rise and run are integer values. Otherwise 'this' is returned.
-     */
-    public PointSlopeLine simplified() {
-        if ( ( rise == (int) rise ) && ( run == (int) run ) ) { // true if rise and run are integers
-            final int reducedRise = (int) ( rise / MathUtil.getGreatestCommonDivisor( (int) rise, (int) run ) );
-            final int reducedRun = (int) ( run / MathUtil.getGreatestCommonDivisor( (int) rise, (int) run ) );
-            return new PointSlopeLine( x1, y1, reducedRise, reducedRun, color );
-        }
-        else {
-            return this;
-        }
-    }
-
-    // Returns true if 2 points on the specified line are also on this line.
-    public boolean same( PointSlopeLine line ) {
-        return onLine( line.x1, line.y1 ) && onLine( line.x1 + run, line.y1 + rise );
-    }
-
-    // Returns true if point (x,y) is on this line.
-    private boolean onLine( double x, double y ) {
-        if ( rise == 0 ) {
-            return ( y == y1 );
-        }
-        else if ( run == 0 ) {
-            return ( x == x1 );
-        }
-        else {
-            return ( x == solveX( y ) );
-        }
-    }
-
-    @Override public String toString() {
-        return "rise=" + rise + ",run=" + run + ",x1=" + x1 + ",y1=" + y1 + ",color=" + color;
+        super( x1, y1, x1 + run, y1 + rise, color );
     }
 }
