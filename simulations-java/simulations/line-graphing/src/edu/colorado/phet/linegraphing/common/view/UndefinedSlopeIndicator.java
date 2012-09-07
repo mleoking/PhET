e588fbf057c602686d669cbc3222c057adef867e
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-09-07 16:06:00 +0000

    9/7/12 put a translucent red "X" over the interactive equation when run=0

---
+++ simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/common/view/UndefinedSlopeIndicator.java
@@ -0,0 +1,26 @@
+// Copyright 2002-2012, University of Colorado
+package edu.colorado.phet.linegraphing.common.view;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Paint;
+import java.awt.geom.Line2D;
+
+import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
+import edu.umd.cs.piccolox.nodes.PComposite;
+
+/**
+ * A translucent "X", to be placed over an equation whose slope is undefined (run == 0).
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
+public class UndefinedSlopeIndicator extends PComposite {
+
+    private final BasicStroke STROKE = new BasicStroke( 4f );
+    private final Paint STROKE_PAINT = new Color( 255, 0, 0, 80 );
+
+    public UndefinedSlopeIndicator( double width, double height ) {
+        addChild( new PhetPPath( new Line2D.Double( 0, 0, width, height ), STROKE, STROKE_PAINT ) );
+        addChild( new PhetPPath( new Line2D.Double( 0, height, width, 0 ), STROKE, STROKE_PAINT ) );
+    }
+}
