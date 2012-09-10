f286696ac93f022167fe4737d763c641caa8d844
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-09-10 17:21:22 +0000

    9/10/12 add cursor handler in LineManipulatorNode base class, all manipulators are interactive

--- simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/common/view/LineManipulatorNode.java
+++ simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/common/view/LineManipulatorNode.java
@@ -4,6 +4,7 @@ package edu.colorado.phet.linegraphing.common.view;
 import java.awt.BasicStroke;
 import java.awt.Color;
 
+import edu.colorado.phet.common.piccolophet.event.CursorHandler;
 import edu.colorado.phet.common.piccolophet.nodes.ShadedSphereNode;
 
 /**
@@ -16,5 +17,6 @@ public class LineManipulatorNode extends ShadedSphereNode {
     public LineManipulatorNode( double diameter, Color fillColor ) {
         super( diameter, fillColor, Color.WHITE, fillColor.darker().darker(), false /* convertToImage */ );
         setStrokeAndPaint( new BasicStroke( 1f ), fillColor.darker().darker() );
+        addInputEventListener( new CursorHandler() );
     }
 }
