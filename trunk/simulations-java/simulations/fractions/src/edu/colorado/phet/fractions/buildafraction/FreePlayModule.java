43be85f6cb948b9f84871cfb018e6020360386bc
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-09-11 18:37:07 +0000

    improved fading

--- trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/buildafraction/FreePlayModule.java
+++ trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/buildafraction/FreePlayModule.java
@@ -1,9 +1,13 @@
 // Copyright 2002-2012, University of Colorado
 package edu.colorado.phet.fractions.buildafraction;
 
+import java.awt.Rectangle;
+
 import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
 import edu.colorado.phet.common.piccolophet.activities.PActivityDelegateAdapter;
+import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.colorado.phet.fractions.FractionsResources.Strings;
+import edu.colorado.phet.fractions.buildafraction.view.BuildAFractionCanvas;
 import edu.colorado.phet.fractions.buildafraction.view.FreePlayCanvas;
 import edu.colorado.phet.fractions.fractionsintro.AbstractFractionsModule;
 import edu.colorado.phet.fractions.fractionsintro.FractionsIntroSimSharing.Components;
@@ -21,15 +25,24 @@ public class FreePlayModule extends AbstractFractionsModule implements FreePlayC
     }
 
     public void resetFreePlayCanvas() {
-
         //Fade to reset.  I am wondering if "no fade" or "diagonal wipe" would be better
-        FreePlayCanvas c = (FreePlayCanvas) getSimulationPanel();
-        c.getPhetRootNode().animateToTransparency( 0, 250 ).setDelegate( new PActivityDelegateAdapter() {
+        //Anyways, use a front layer to fade because just changing the transparency on the layer makes everything transparent (like card nodes) making it so you can see things underneath.
+        final FreePlayCanvas c = (FreePlayCanvas) getSimulationPanel();
+        final PhetPPath block = new PhetPPath( new Rectangle( c.getWidth(), c.getHeight() ), BuildAFractionCanvas.LIGHT_BLUE ) {{
+            setTransparency( 0 );
+        }};
+        c.getPhetRootNode().addScreenChild( block );
+        block.animateToTransparency( 1, 250 ).setDelegate( new PActivityDelegateAdapter() {
             @Override public void activityFinished( final PActivity activity ) {
                 final FreePlayCanvas newOne = new FreePlayCanvas( FreePlayModule.this );
-                newOne.getPhetRootNode().setTransparency( 0.0f );
+                final PhetPPath block = new PhetPPath( new Rectangle( c.getWidth(), c.getHeight() ), BuildAFractionCanvas.LIGHT_BLUE );
+                newOne.getPhetRootNode().addScreenChild( block );
                 setSimulationPanel( newOne );
-                newOne.getPhetRootNode().animateToTransparency( 1, 250 );
+                block.animateToTransparency( 0, 250 ).setDelegate( new PActivityDelegateAdapter() {
+                    @Override public void activityFinished( final PActivity activity ) {
+                        newOne.getPhetRootNode().removeScreenChild( block );
+                    }
+                } );
             }
         } );
     }
