8684d17c15ca44a748cde8344033409e0c2f643a
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-09-11 18:09:51 +0000

    Fade to reset

--- simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/buildafraction/FreePlayModule.java
+++ simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/buildafraction/FreePlayModule.java
@@ -2,10 +2,12 @@
 package edu.colorado.phet.fractions.buildafraction;
 
 import edu.colorado.phet.common.phetcommon.model.clock.ConstantDtClock;
+import edu.colorado.phet.common.piccolophet.activities.PActivityDelegateAdapter;
 import edu.colorado.phet.fractions.FractionsResources.Strings;
 import edu.colorado.phet.fractions.buildafraction.view.FreePlayCanvas;
 import edu.colorado.phet.fractions.fractionsintro.AbstractFractionsModule;
 import edu.colorado.phet.fractions.fractionsintro.FractionsIntroSimSharing.Components;
+import edu.umd.cs.piccolo.activities.PActivity;
 
 /**
  * Main module for "build a fraction with mixed numbers" tab, which is only visible in the standalone "Build a Fraction" sim.
@@ -19,6 +21,16 @@ public class FreePlayModule extends AbstractFractionsModule implements FreePlayC
     }
 
     public void resetFreePlayCanvas() {
-        setSimulationPanel( new FreePlayCanvas( this ) );
+
+        //Fade to reset.  I am wondering if "no fade" or "diagonal wipe" would be better
+        FreePlayCanvas c = (FreePlayCanvas) getSimulationPanel();
+        c.getPhetRootNode().animateToTransparency( 0, 250 ).setDelegate( new PActivityDelegateAdapter() {
+            @Override public void activityFinished( final PActivity activity ) {
+                final FreePlayCanvas newOne = new FreePlayCanvas( FreePlayModule.this );
+                newOne.getPhetRootNode().setTransparency( 0.0f );
+                setSimulationPanel( newOne );
+                newOne.getPhetRootNode().animateToTransparency( 1, 250 );
+            }
+        } );
     }
 }
