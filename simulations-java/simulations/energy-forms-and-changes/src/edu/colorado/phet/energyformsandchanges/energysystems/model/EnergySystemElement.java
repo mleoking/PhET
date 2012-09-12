c937ee11255c1134a09347b881972cfca78f720d
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-09-12 17:48:05 +0000

    Added an observable list for containing the energy chunks.

--- simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/energysystems/model/EnergySystemElement.java
+++ simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/energysystems/model/EnergySystemElement.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import edu.colorado.phet.common.phetcommon.simsharing.messages.IUserComponent;
+import edu.colorado.phet.common.phetcommon.util.ObservableList;
+import edu.colorado.phet.energyformsandchanges.common.model.EnergyChunk;
 
 /**
  * Base class for energy sources, converters, and users.
@@ -14,18 +16,18 @@ import edu.colorado.phet.common.phetcommon.simsharing.messages.IUserComponent;
  */
 public abstract class EnergySystemElement extends PositionableFadableModelElement {
 
-    // List of images, if any, that are used in the view to represent this element. 
+    // List of images, if any, that are used in the view to represent this element.
+    // TODO: I think that this can go once each element has its own node, and was basically done for prototyping.
     private final List<ModelElementImage> imageList = new ArrayList<ModelElementImage>();
+
+    // List of energy chunks contained and managed by this energy system element.
+    public final ObservableList<EnergyChunk> energyChunkList = new ObservableList<EnergyChunk>();
 
     // Image that is used as an icon in the view to represent this element.
     private final Image iconImage;
 
     // State variable that tracks whether this element is currently active.
     protected boolean active = false;
-
-    protected EnergySystemElement( Image iconImage ) {
-        this( iconImage, new ArrayList<ModelElementImage>() );
-    }
 
     protected EnergySystemElement( Image iconImage, List<ModelElementImage> images ) {
         this.iconImage = iconImage;
