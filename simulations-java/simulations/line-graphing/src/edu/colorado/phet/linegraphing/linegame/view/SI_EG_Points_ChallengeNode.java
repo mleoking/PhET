eaa8547a267fc8bed1277c592df0828ae9e63cdb
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-09-12 01:56:28 +0000

    9/11/12 add sim-sharing UserComponent values for (x1,y1) and (x2,y2) manipulators

--- simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/linegame/view/SI_EG_Points_ChallengeNode.java
+++ simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/linegame/view/SI_EG_Points_ChallengeNode.java
@@ -62,7 +62,7 @@ public class SI_EG_Points_ChallengeNode extends SI_ChallengeNode {
 
             // (x1,y1) manipulator
             x1y1ManipulatorNode = new LineManipulatorNode( manipulatorDiameter, LGColors.POINT_X1_Y1 );
-            x1y1ManipulatorNode.addInputEventListener( new X1Y1DragHandler( UserComponents.pointManipulator, UserComponentTypes.sprite,
+            x1y1ManipulatorNode.addInputEventListener( new X1Y1DragHandler( UserComponents.x1y1Manipulator, UserComponentTypes.sprite,
                                                                             x1y1ManipulatorNode, mvt, guessLine,
                                                                             new Property<DoubleRange>( new DoubleRange( graph.xRange ) ),
                                                                             new Property<DoubleRange>( new DoubleRange( graph.yRange ) ),
@@ -70,7 +70,7 @@ public class SI_EG_Points_ChallengeNode extends SI_ChallengeNode {
 
             // (x2,y2) manipulator
             x2y2ManipulatorNode = new LineManipulatorNode( manipulatorDiameter, LGColors.POINT_X2_Y2 );
-            x2y2ManipulatorNode.addInputEventListener( new X2Y2DragHandler( UserComponents.slopeManipulator, UserComponentTypes.sprite,
+            x2y2ManipulatorNode.addInputEventListener( new X2Y2DragHandler( UserComponents.x2y2Manipulator, UserComponentTypes.sprite,
                                                                             x2y2ManipulatorNode, mvt, guessLine,
                                                                             new Property<DoubleRange>( new DoubleRange( graph.xRange ) ),
                                                                             new Property<DoubleRange>( new DoubleRange( graph.yRange ) ) ) );
