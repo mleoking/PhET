dc4df6cd1a660bbf2edb2c316716809340e4ef45
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-09-13 19:43:00 +0000

    Working energy chunk layer.

--- simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/common/view/EnergyChunkLayer.java
+++ simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/common/view/EnergyChunkLayer.java
@@ -1,6 +1,10 @@
 // Copyright 2002-2012, University of Colorado
 package edu.colorado.phet.energyformsandchanges.common.view;
 
+import java.awt.geom.Point2D;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
 import edu.colorado.phet.common.phetcommon.util.ObservableList;
 import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
@@ -21,11 +25,28 @@ import edu.umd.cs.piccolo.PNode;
  */
 public class EnergyChunkLayer extends PNode {
 
-    public EnergyChunkLayer( final ObservableList<EnergyChunk> energyChunkList, final ModelViewTransform mvt ) {
+    public EnergyChunkLayer( final ObservableList<EnergyChunk> energyChunkList, PNode parentNode, final ModelViewTransform mvt ) {
+
+//        setGlobalTranslation( new Point2D.Double( 0, 0 ) );
+
+        parentNode.addPropertyChangeListener( "transform", new PropertyChangeListener() {
+            public void propertyChange( PropertyChangeEvent evt ) {
+                System.out.println( "=======================Transform changed=======================" );
+//                setOffset( globalToLocal( new Point2D.Double( 0, 0 ) ) );
+                setGlobalTranslation( new Point2D.Double( 0, 0 ) );
+                System.out.println( "getOffset() = " + getOffset() );
+                System.out.println( "localToGlobal( getOffset() ) = " + localToGlobal( getOffset() ) );
+                System.out.println( "getGlobalTranslation = " + getGlobalTranslation() );
+            }
+        } );
+
+        // Add energy chunk nodes as children as the energy chunks come in to
+        // existence in the model.
         energyChunkList.addElementAddedObserver( new VoidFunction1<EnergyChunk>() {
             public void apply( final EnergyChunk addedEnergyChunk ) {
                 final EnergyChunkNode energyChunkNode = new EnergyChunkNode( addedEnergyChunk, mvt );
                 addChild( energyChunkNode );
+                // Remove the energy chunk nodes as they are removed from the model.
                 energyChunkList.addElementRemovedObserver( new VoidFunction1<EnergyChunk>() {
                     public void apply( EnergyChunk removedEnergyChunk ) {
                         if ( removedEnergyChunk == addedEnergyChunk ) {
