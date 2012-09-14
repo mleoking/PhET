b46be411c9f26498e61219b4d74a94bf1a4992a8
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-09-14 15:54:20 +0000

    Build a Fraction: Shapes: Added star icon for level 10

--- trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/buildafraction/view/MixedNumbersLevelSelectionNode.java
+++ trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/buildafraction/view/MixedNumbersLevelSelectionNode.java
@@ -86,7 +86,7 @@ public class MixedNumbersLevelSelectionNode extends AbstractLevelSelectionNode {
                level == 7 ? Pattern.ringOfHexagons() :
                level == 8 ? Pattern.ninjaStar() :
                level == 9 ? Pattern.grid( 3 ) :
-               level == 10 ? Pattern.pie( 10 ) :
+               level == 10 ? Pattern.fivePointStarWithLeaves() :
                null;
     }
 
