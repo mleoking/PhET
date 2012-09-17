b8ace2f810febe7eeb3709bc234d30e6176a220e
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-09-17 02:53:25 +0000

    added puller images

--- simulations-java/simulations/forces-and-motion-basics/src/edu/colorado/phet/forcesandmotionbasics/tugofwar/TugOfWarCanvas.java
+++ simulations-java/simulations/forces-and-motion-basics/src/edu/colorado/phet/forcesandmotionbasics/tugofwar/TugOfWarCanvas.java
@@ -3,9 +3,11 @@ package edu.colorado.phet.forcesandmotionbasics.tugofwar;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
 
 import javax.swing.JCheckBox;
 
+import edu.colorado.phet.common.phetcommon.math.vector.Vector2D;
 import edu.colorado.phet.common.piccolophet.nodes.ControlPanelNode;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPText;
@@ -13,11 +15,15 @@ import edu.colorado.phet.common.piccolophet.nodes.background.SkyNode;
 import edu.colorado.phet.common.piccolophet.nodes.layout.HBox;
 import edu.colorado.phet.common.piccolophet.nodes.layout.VBox;
 import edu.colorado.phet.forcesandmotionbasics.AbstractForcesAndMotionBasicsCanvas;
+import edu.colorado.phet.forcesandmotionbasics.ForcesAndMotionBasicsResources;
 import edu.colorado.phet.forcesandmotionbasics.ForcesAndMotionBasicsResources.Images;
 import edu.umd.cs.piccolo.nodes.PImage;
 import edu.umd.cs.piccolox.pswing.PSwing;
 
 import static edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform.createIdentity;
+import static edu.colorado.phet.forcesandmotionbasics.tugofwar.TugOfWarCanvas.PColor.BLUE;
+import static edu.colorado.phet.forcesandmotionbasics.tugofwar.TugOfWarCanvas.PColor.RED;
+import static edu.colorado.phet.forcesandmotionbasics.tugofwar.TugOfWarCanvas.PSize.*;
 
 /**
  * @author Sam Reid
@@ -59,5 +65,54 @@ public class TugOfWarCanvas extends AbstractForcesAndMotionBasicsCanvas {
 
         addChild( rope );
         addChild( cart );
+
+        final double IMAGE_SCALE = 0.75;
+        Vector2D largePosition = Vector2D.v( 88.38995568685374, 488.15361890694203 );
+        Vector2D mediumPosition = Vector2D.v( 151.66912850812423, 513.264401772526 );
+        Vector2D smallPosition1 = Vector2D.v( 215.9527326440175, 558.463810930576 );
+        Vector2D smallPosition2 = Vector2D.v( 263.1610044313148, 559.4682422451999 );
+        final PImage largeRedPuller = puller( BLUE, LARGE, IMAGE_SCALE, largePosition );
+        addChild( largeRedPuller );
+        addChild( puller( BLUE, MEDIUM, IMAGE_SCALE, mediumPosition ) );
+        addChild( puller( BLUE, SMALL, IMAGE_SCALE, smallPosition1 ) );
+        addChild( puller( BLUE, SMALL, IMAGE_SCALE, smallPosition2 ) );
+
+        final double offset = largeRedPuller.getFullBounds().getWidth();
+        addChild( puller( RED, LARGE, IMAGE_SCALE, reflect( largePosition, offset ) ) );
+        addChild( puller( RED, MEDIUM, IMAGE_SCALE, reflect( mediumPosition, offset ) ) );
+        addChild( puller( RED, SMALL, IMAGE_SCALE, reflect( smallPosition1, offset ) ) );
+        addChild( puller( RED, SMALL, IMAGE_SCALE, reflect( smallPosition2, offset ) ) );
     }
+
+    private Vector2D reflect( final Vector2D position, final double width ) {
+        double distanceFromCenter = STAGE_SIZE.width / 2 - position.x;
+        double newX = STAGE_SIZE.width / 2 + distanceFromCenter - width;
+        return new Vector2D( newX, position.y );
+    }
+
+    public static PImage puller( PColor color, PSize size, final double scale ) {
+        return puller( color, size, scale, Vector2D.ZERO );
+    }
+
+    public static PImage puller( PColor color, PSize size, final double scale, final Vector2D v ) {
+        return new PImage( pullerImage( color, size, 0 ) ) {{
+            setScale( scale );
+            setOffset( v.x, v.y );
+            addInputEventListener( new DebugDragLocation() );
+        }};
+    }
+
+    private static BufferedImage pullerImage( final PColor color, final PSize size, int item ) {
+        return ForcesAndMotionBasicsResources.RESOURCES.getImage( "pull_figure_" + sizeText( size ) + color.name() + "_" + item + ".png" );
+    }
+
+    private static String sizeText( final PSize size ) {
+        return size == LARGE ? "lrg_" :
+               size == SMALL ? "small_" :
+               "";
+    }
+
+    static enum PColor {BLUE, RED}
+
+    static enum PSize {SMALL, MEDIUM, LARGE}
 }
