fe375f9651f7a5a6787fc4f73fa113c040b5c82c
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-09-17 04:47:50 +0000

    highlight knots when nearby

--- trunk/simulations-java/simulations/forces-and-motion-basics/src/edu/colorado/phet/forcesandmotionbasics/tugofwar/PullerNode.java
+++ trunk/simulations-java/simulations/forces-and-motion-basics/src/edu/colorado/phet/forcesandmotionbasics/tugofwar/PullerNode.java
@@ -1,9 +1,14 @@
 // Copyright 2002-2012, University of Colorado
 package edu.colorado.phet.forcesandmotionbasics.tugofwar;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
 
 import edu.colorado.phet.common.phetcommon.math.vector.Vector2D;
+import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.colorado.phet.forcesandmotionbasics.ForcesAndMotionBasicsResources;
 import edu.colorado.phet.forcesandmotionbasics.tugofwar.TugOfWarCanvas.PColor;
 import edu.colorado.phet.forcesandmotionbasics.tugofwar.TugOfWarCanvas.PSize;
@@ -18,9 +23,12 @@ import edu.umd.cs.piccolo.util.PDimension;
  */
 public class PullerNode extends PNode {
     private final Vector2D initialOffset;
+    public final PColor color;
+    private final PhetPPath attachmentNode;
 
-    public PullerNode( PColor color, PSize size, int item, final double scale, Vector2D offset, PullerContext context ) {
-        BufferedImage image = ForcesAndMotionBasicsResources.RESOURCES.getImage( "pull_figure_" + sizeText( size ) + color.name() + "_" + item + ".png" );
+    public PullerNode( final PColor color, final PSize size, int item, final double scale, Vector2D offset, final PullerContext context ) {
+        this.color = color;
+        final BufferedImage image = ForcesAndMotionBasicsResources.RESOURCES.getImage( "pull_figure_" + sizeText( size ) + color.name() + "_" + item + ".png" );
         addChild( new PImage( image ) );
         setScale( scale );
         setOffset( offset.x, offset.y );
@@ -32,6 +40,7 @@ public class PullerNode extends PNode {
                 final PDimension delta = event.getDeltaRelativeTo( PullerNode.this.getParent() );
                 PullerNode.this.translate( delta.width / PullerNode.this.getScale(), delta.height / PullerNode.this.getScale() );
 //                System.out.println( event.getPickedNode().getOffset() );
+                context.drag( PullerNode.this );
             }
 
             @Override public void mouseReleased( final PInputEvent event ) {
@@ -40,12 +49,24 @@ public class PullerNode extends PNode {
                 PullerNode.this.animateToPositionScaleRotation( initialOffset.x, initialOffset.y, scale, 0, TugOfWarCanvas.ANIMATION_DURATION );
             }
         } );
+
+        final double w = 10;
+        attachmentNode = new PhetPPath( new Ellipse2D.Double( -w / 2, -w / 2, w, w ), new BasicStroke( 2 ), Color.green ) {{
+            setOffset( color == PColor.BLUE ?
+                       image.getWidth() - w / 2 :
+                       w / 2, image.getHeight() - 100 );
+        }};
+        addChild( attachmentNode );
     }
 
     private static String sizeText( final PSize size ) {
         return size == PSize.LARGE ? "lrg_" :
                size == PSize.SMALL ? "small_" :
                "";
+    }
+
+    public Point2D getGlobalAttachmentPoint() {
+        return attachmentNode.getGlobalFullBounds().getCenter2D();
     }
 
 }
