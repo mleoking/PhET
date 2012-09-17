ef97703801287eede74be3b9cabf9b92eae5e986
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-09-17 01:58:33 +0000

    Added grass and sky

---
+++ simulations-java/simulations/forces-and-motion-basics/src/edu/colorado/phet/forcesandmotionbasics/ForcesAndMotionBasicsSimSharing.java
@@ -0,0 +1,12 @@
+package edu.colorado.phet.forcesandmotionbasics;
+
+import edu.colorado.phet.common.phetcommon.simsharing.messages.IUserComponent;
+
+/**
+ * @author Sam Reid
+ */
+public class ForcesAndMotionBasicsSimSharing {
+    public static enum UserComponents implements IUserComponent {
+        tugOfWarTab
+    }
+}
