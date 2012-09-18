27945669c7f6e3f5da909d18ab04d99f4d9715c2
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-09-18 23:58:26 +0000

    addressed review issue, see #3441

--- trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/buildafraction/model/shapes/ShapeType.java
+++ trunk/simulations-java/simulations/fractions/src/edu/colorado/phet/fractions/buildafraction/model/shapes/ShapeType.java
@@ -1,10 +1,8 @@
 // Copyright 2002-2012, University of Colorado
 package edu.colorado.phet.fractions.buildafraction.model.shapes;
 
-//REVIEW does this mean "for levels that involve shapes in the Build a Fraction sim"?
-
 /**
- * A shape type for Build a Fraction: Shapes levels, either horizontal bar or pie.
+ * A shape type for levels in which the user uses shapes to build fractions: either horizontal bar or pie.
  *
  * @author Sam Reid
  */
