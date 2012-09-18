68270326d48cfdf89a95e66e6c1ea6dc57254f28
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-09-18 23:16:58 +0000

    rework SlopeInterceptEquationFactory: render signs and operators using PPath, parameterize y-offset of slope sign

--- trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/common/view/EquationFactory.java
+++ trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/common/view/EquationFactory.java
@@ -1,6 +1,8 @@
 // Copyright 2002-2012, University of Colorado
 package edu.colorado.phet.linegraphing.common.view;
 
+import java.awt.Color;
+import java.awt.geom.Rectangle2D;
 import java.text.MessageFormat;
 
 import edu.colorado.phet.common.phetcommon.math.MathUtil;
@@ -8,6 +10,8 @@ import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPText;
 import edu.colorado.phet.linegraphing.common.LGResources.Strings;
 import edu.colorado.phet.linegraphing.common.model.Line;
+import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.nodes.PPath;
 
 /**
  * Base class for all factories that create equation nodes for lines.
@@ -15,6 +19,14 @@ import edu.colorado.phet.linegraphing.common.model.Line;
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
 public abstract class EquationFactory {
+
+    // vertical offset of the slope's sign. zero is vertically centered on the equals sign, positive values move it down, negative move it up.
+    protected static final int SLOPE_SIGN_Y_OFFSET = 0;
+
+    // fudge factors for horizontal lines, to vertically align them on equals sign
+    protected static final int SIGN_Y_FUDGE_FACTOR = 2;
+    protected static final int OPERATOR_Y_FUDGE_FACTOR = SIGN_Y_FUDGE_FACTOR;
+    protected static final int FRACTION_LINE_Y_FUDGE_FACTOR = SIGN_Y_FUDGE_FACTOR;
 
     // spacing between components of an equation
     protected static final double X_SPACING = 3;
@@ -43,5 +55,34 @@ public abstract class EquationFactory {
     // Converts a double to an integer string, using nearest-neighbor rounding.
     protected static String toIntString( double d ) {
         return String.valueOf( MathUtil.roundHalfUp( d ) );
+    }
+
+
+    // Creates a plus or minus sign. Sign is distinct from operator, so that they can have different looks.
+    protected static PNode createSignNode( double value, Color color ) {
+        if ( value >= 0 ) {
+            return new PlusNode( 10, 2, color );
+        }
+        else {
+            return new MinusNode( 10, 2, color );
+        }
+    }
+
+    // Creates a plus or minus operator. Operator is distinct from sign, so that they can have different looks.
+    protected static PNode createOperatorNode( double value, Color color ) {
+        if ( value >= 0 ) {
+            return new PlusNode( 10, 2, color );
+        }
+        else {
+            return new MinusNode( 10, 2, color );
+        }
+    }
+
+    // Creates the horizontal line that separates numerator and denominator in a fraction (slope, in our case.)
+    protected static PNode createFractionLineNode( double width, Color color ) {
+        PPath node = new PPath( new Rectangle2D.Double( 0, 0, width, 2 ) );
+        node.setStroke( null );
+        node.setPaint( color );
+        return node;
     }
 }
