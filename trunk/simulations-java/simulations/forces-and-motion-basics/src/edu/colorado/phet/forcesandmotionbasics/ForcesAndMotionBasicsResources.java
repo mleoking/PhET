716121888175bbfbf3651f4951d0b7e4da92f4e3
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-09-18 05:57:00 +0000

    Added new images for buttons

--- trunk/simulations-java/simulations/forces-and-motion-basics/src/edu/colorado/phet/forcesandmotionbasics/ForcesAndMotionBasicsResources.java
+++ trunk/simulations-java/simulations/forces-and-motion-basics/src/edu/colorado/phet/forcesandmotionbasics/ForcesAndMotionBasicsResources.java
@@ -25,6 +25,9 @@ public class ForcesAndMotionBasicsResources {
         public static final BufferedImage CLOUD1 = RESOURCES.getImage( "cloud1.png" );
         public static final BufferedImage DRAKOONSONNE = RESOURCES.getImage( "DrakoonSonne.png" );
         public static final BufferedImage GO_BUTTON = RESOURCES.getImage( "go_button.png" );
+        public static final BufferedImage GO_HOVER = RESOURCES.getImage( "go_hover.png" );
+        public static final BufferedImage GO_PRESSED = RESOURCES.getImage( "go_pressed.png" );
+        public static final BufferedImage GO_UP = RESOURCES.getImage( "go_up.png" );
         public static final BufferedImage GRASS = RESOURCES.getImage( "grass.png" );
         public static final BufferedImage GROUND_TEXTURE = RESOURCES.getImage( "ground-texture.png" );
         public static final BufferedImage MYSTERY_BOX = RESOURCES.getImage( "mystery-box.png" );
@@ -54,5 +57,8 @@ public class ForcesAndMotionBasicsResources {
         public static final BufferedImage PULL_FIGURE_SMALL_RED_3 = RESOURCES.getImage( "pull_figure_small_RED_3.png" );
         public static final BufferedImage ROPE = RESOURCES.getImage( "rope.png" );
         public static final BufferedImage STOP_BUTTON = RESOURCES.getImage( "stop_button.png" );
+        public static final BufferedImage STOP_HOVER = RESOURCES.getImage( "stop_hover.png" );
+        public static final BufferedImage STOP_PRESSED = RESOURCES.getImage( "stop_pressed.png" );
+        public static final BufferedImage STOP_UP = RESOURCES.getImage( "stop_up.png" );
     }
 }
--- trunk/simulations-java/simulations/forces-and-motion-basics/src/edu/colorado/phet/forcesandmotionbasics/tugofwar/ImageButtonNodeWithText.java
+++ trunk/simulations-java/simulations/forces-and-motion-basics/src/edu/colorado/phet/forcesandmotionbasics/tugofwar/ImageButtonNodeWithText.java
@@ -15,19 +15,37 @@ import edu.umd.cs.piccolo.nodes.PImage;
  * @author Sam Reid
  */
 public class ImageButtonNodeWithText extends PNode {
-    public ImageButtonNodeWithText( final BufferedImage image, final String text, final VoidFunction0 effect ) {
-        final PImage imageNode = new PImage( image );
+    public ImageButtonNodeWithText( final BufferedImage up, final BufferedImage hover, final BufferedImage pressed, final String text, final VoidFunction0 effect ) {
+        final PImage imageNode = new PImage( up );
         addChild( imageNode );
         final PhetPText textNode = new PhetPText( text );
-        textNode.scale( image.getWidth() / textNode.getFullWidth() * 0.8 );
+        textNode.scale( up.getWidth() / textNode.getFullWidth() * 0.65 );
         addChild( textNode );
 
         textNode.centerFullBoundsOnPoint( imageNode.getFullBounds().getCenter2D() );
+
+        //account for shadow
+        textNode.translate( -4 / textNode.getScale(), -4 / textNode.getScale() );
         addInputEventListener( new CursorHandler() );
         addInputEventListener( new PBasicInputEventHandler() {
+            @Override public void mouseEntered( final PInputEvent event ) {
+                super.mouseEntered( event );
+                imageNode.setImage( hover );
+            }
+
+            @Override public void mouseExited( final PInputEvent event ) {
+                imageNode.setImage( up );
+            }
+
             @Override public void mousePressed( final PInputEvent event ) {
                 super.mousePressed( event );
+                imageNode.setImage( pressed );
+            }
+
+            @Override public void mouseReleased( final PInputEvent event ) {
+                super.mouseReleased( event );
                 effect.apply();
+                imageNode.setImage( up );
             }
         } );
     }
