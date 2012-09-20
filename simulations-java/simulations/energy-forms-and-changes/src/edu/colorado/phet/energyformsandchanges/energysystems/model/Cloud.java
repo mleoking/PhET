4010dc54db3be93a5c6b7d75f2b1fc5af1b79b33
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-09-20 20:44:14 +0000

    Added bouncing of energy chunks off of the clouds.

--- simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/energysystems/model/Cloud.java
+++ simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/energysystems/model/Cloud.java
@@ -1,7 +1,11 @@
 // Copyright 2002-2012, University of Colorado
 package edu.colorado.phet.energyformsandchanges.energysystems.model;
 
+import java.awt.Shape;
+import java.awt.geom.Ellipse2D;
+
 import edu.colorado.phet.common.phetcommon.math.vector.Vector2D;
+import edu.colorado.phet.common.phetcommon.model.property.ObservableProperty;
 import edu.colorado.phet.common.phetcommon.model.property.Property;
 
 import static edu.colorado.phet.energyformsandchanges.EnergyFormsAndChangesResources.Images.CLOUD_1;
@@ -13,13 +17,23 @@ import static edu.colorado.phet.energyformsandchanges.EnergyFormsAndChangesResou
  */
 public class Cloud {
 
-    private static final double CLOUD_WIDTH = 0.07; // In meters, though obviously not to scale.
-
+    private static final double CLOUD_WIDTH = 0.035; // In meters, though obviously not to scale.
     public static final ModelElementImage CLOUD_IMAGE = new ModelElementImage( CLOUD_1, CLOUD_WIDTH, new Vector2D( 0, 0 ) );
-    public final Vector2D offset;
+    private static final double CLOUD_HEIGHT = CLOUD_IMAGE.getHeight(); // In meters, though obviously not to scale.
+
+    public final Vector2D offsetFromParent;
+    private final ObservableProperty<Vector2D> parentPosition;
     public final Property<Double> existenceStrength = new Property<Double>( 1.0 );
 
-    public Cloud( Vector2D initialPosition ) {
-        offset = initialPosition;
+    public Cloud( Vector2D offsetFromParent, ObservableProperty<Vector2D> parentPosition ) {
+        this.offsetFromParent = offsetFromParent;
+        this.parentPosition = parentPosition;
+    }
+
+    public Shape getCloudAbsorptionReflectionShape() {
+        return new Ellipse2D.Double( parentPosition.get().x + offsetFromParent.x - CLOUD_WIDTH / 2,
+                                     parentPosition.get().y + offsetFromParent.y - CLOUD_HEIGHT / 2,
+                                     CLOUD_WIDTH,
+                                     CLOUD_HEIGHT );
     }
 }
