10fcad095c43e64d666fca0a62ed34ab9b7db2ab
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-09-21 22:00:31 +0000

    Started adding energy chunks to the faucet.

--- trunk/simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/energysystems/model/FaucetAndWater.java
+++ trunk/simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/energysystems/model/FaucetAndWater.java
@@ -10,6 +10,7 @@ import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.phetcommon.simsharing.messages.IUserComponent;
 import edu.colorado.phet.energyformsandchanges.EnergyFormsAndChangesResources;
 import edu.colorado.phet.energyformsandchanges.EnergyFormsAndChangesSimSharing;
+import edu.colorado.phet.energyformsandchanges.common.model.EnergyChunk;
 import edu.colorado.phet.energyformsandchanges.common.model.EnergyType;
 
 import static edu.colorado.phet.energyformsandchanges.EnergyFormsAndChangesResources.Images.TEMP_FAUCET;
@@ -24,6 +25,7 @@ public class FaucetAndWater extends EnergySource {
 
     public static final Vector2D OFFSET_FROM_CENTER_TO_WATER_ORIGIN = new Vector2D( 0.065, 0.08 );
     private static final double MAX_ENERGY_PRODUCTION_RATE = 200; // In joules/second.
+    private static final double ENERGY_CHUNK_VELOCITY = 0.05; // In meters/second.
 
     public final Property<Double> flowProportion = new Property<Double>( 0.0 );
     public final BooleanProperty enabled = new BooleanProperty( true );
@@ -32,13 +34,31 @@ public class FaucetAndWater extends EnergySource {
         add( new ModelElementImage( TEMP_FAUCET, new Vector2D( -0.035, 0.075 ) ) );
     }};
 
-    private double energyChunkEmissionCountdownTimer = Double.POSITIVE_INFINITY;
+    private double energyChunkEmissionCountdownTimer = 0.5;
+    private final BooleanProperty energyChunksVisible;
 
-    protected FaucetAndWater() {
+    protected FaucetAndWater( BooleanProperty energyChunksVisible ) {
         super( EnergyFormsAndChangesResources.Images.FAUCET_ICON, IMAGE_LIST );
+        this.energyChunksVisible = energyChunksVisible;
     }
 
     @Override public Energy stepInTime( double dt ) {
+
+        // Check if time to emit an energy chunk and, if so, do it.
+        energyChunkEmissionCountdownTimer -= dt;
+        if ( energyChunkEmissionCountdownTimer <= 0 ) {
+            energyChunkList.add( new EnergyChunk( EnergyType.MECHANICAL,
+                                                  getPosition().plus( OFFSET_FROM_CENTER_TO_WATER_ORIGIN ),
+                                                  new Vector2D( 0, -ENERGY_CHUNK_VELOCITY ),
+                                                  energyChunksVisible ) );
+            energyChunkEmissionCountdownTimer = 0.5;
+        }
+
+        // Move the energy chunks.
+        for ( EnergyChunk energyChunk : energyChunkList ) {
+            energyChunk.translateBasedOnVelocity( dt );
+        }
+
         return new Energy( EnergyType.MECHANICAL, MAX_ENERGY_PRODUCTION_RATE * flowProportion.get() * dt, -Math.PI / 2 );
     }
 
