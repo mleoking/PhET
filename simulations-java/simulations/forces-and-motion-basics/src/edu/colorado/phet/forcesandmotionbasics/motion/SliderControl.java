7e0267c4b9a4fc70d71f22c3117b72c79bd88fa6
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-09-24 15:11:58 +0000

    Wired up slider to text box

--- simulations-java/simulations/forces-and-motion-basics/src/edu/colorado/phet/forcesandmotionbasics/motion/SliderControl.java
+++ simulations-java/simulations/forces-and-motion-basics/src/edu/colorado/phet/forcesandmotionbasics/motion/SliderControl.java
@@ -1,11 +1,13 @@
 package edu.colorado.phet.forcesandmotionbasics.motion;
 
 import java.awt.Color;
+import java.text.DecimalFormat;
 
 import javax.swing.JTextField;
 
 import edu.colorado.phet.common.phetcommon.model.property.BooleanProperty;
 import edu.colorado.phet.common.phetcommon.model.property.doubleproperty.DoubleProperty;
+import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPText;
 import edu.colorado.phet.common.piccolophet.nodes.layout.HBox;
 import edu.colorado.phet.common.piccolophet.nodes.layout.VBox;
@@ -20,9 +22,9 @@ import static edu.colorado.phet.forcesandmotionbasics.common.AbstractForcesAndMo
  * @author Sam Reid
  */
 public class SliderControl extends PNode {
-    public SliderControl() {
+    public SliderControl( final DoubleProperty appliedForce ) {
         VBox box = new VBox( 5, new PhetPText( "Applied Force", CONTROL_FONT ),
-                             new HSliderNode( null, -100, 100, DEFAULT_TRACK_THICKNESS, 200 * 1.75, new DoubleProperty( 0.0 ), new BooleanProperty( true ) ) {{
+                             new HSliderNode( null, -100, 100, DEFAULT_TRACK_THICKNESS, 200 * 1.75, appliedForce, new BooleanProperty( true ) ) {{
                                  addLabel( 0, new PhetPText( "0", CONTROL_FONT ) );
                                  addLabel( -50, new PhetPText( "a", CONTROL_FONT ) {{setTransparency( 0.0f );}} );
                                  addLabel( -100, new PhetPText( "a", CONTROL_FONT ) {{setTransparency( 0.0f );}} );
@@ -30,13 +32,16 @@ public class SliderControl extends PNode {
                                  addLabel( 100, new PhetPText( "100", CONTROL_FONT ) {{setTransparency( 0.0f );}} );
 
                                  setTrackFillPaint( Color.white );
-//                                 addLabel( 50, new PNode() );
-//                                 addLabel( 100, new PNode() );
                              }},
-                             new HBox( new PhetPText( "Newtons", CONTROL_FONT ) {{setTransparency( 0.0f );}}, new PSwing( new JTextField( 4 ) {{
+                             new HBox( new PhetPText( "Newtons", CONTROL_FONT ) {{setTransparency( 0.0f );}}, new PSwing( new JTextField( 3 ) {{
                                  setFont( CONTROL_FONT );
                                  setText( "0" );
                                  setHorizontalAlignment( JTextField.RIGHT );
+                                 appliedForce.addObserver( new VoidFunction1<Double>() {
+                                     public void apply( final Double value ) {
+                                         setText( new DecimalFormat( "0" ).format( value ) );
+                                     }
+                                 } );
                              }} ), new PhetPText( "Newtons", CONTROL_FONT ) ) );
 
         addChild( box );
