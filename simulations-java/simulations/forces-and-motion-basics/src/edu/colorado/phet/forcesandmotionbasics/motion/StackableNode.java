315e8dd441f230710f1349da06448dfd920c7226
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-09-24 05:20:56 +0000

    allow dropping objects on skateboard

---
+++ simulations-java/simulations/forces-and-motion-basics/src/edu/colorado/phet/forcesandmotionbasics/motion/StackableNode.java
@@ -0,0 +1,52 @@
+package edu.colorado.phet.forcesandmotionbasics.motion;
+
+import java.awt.image.BufferedImage;
+
+import edu.colorado.phet.common.phetcommon.math.vector.Vector2D;
+import edu.colorado.phet.common.piccolophet.activities.AnimateToScale;
+import edu.colorado.phet.common.piccolophet.event.CursorHandler;
+import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
+import edu.umd.cs.piccolo.event.PInputEvent;
+import edu.umd.cs.piccolo.nodes.PImage;
+import edu.umd.cs.piccolo.util.PDimension;
+
+/**
+ * @author Sam Reid
+ */
+public class StackableNode extends PNode {
+
+    private Vector2D initialOffset;
+    private final double initialScale;
+
+    public StackableNode( final StackableNodeContext context, final BufferedImage image ) {
+        addChild( new PImage( image ) );
+        setScale( 0.8 );
+        this.initialScale = getScale();
+        addInputEventListener( new CursorHandler() );
+        addInputEventListener( new PBasicInputEventHandler() {
+            @Override public void mousePressed( final PInputEvent event ) {
+                addActivity( new AnimateToScale( 1.0, StackableNode.this, 200 ) );
+            }
+
+            @Override public void mouseDragged( final PInputEvent event ) {
+                final PDimension delta = event.getDeltaRelativeTo( StackableNode.this.getParent() );
+                translate( delta.width, delta.height );
+            }
+
+            @Override public void mouseReleased( final PInputEvent event ) {
+                super.mouseReleased( event );
+                context.stackableNodeDropped( StackableNode.this );
+            }
+        } );
+    }
+
+    public void animateHome() {
+        animateToPositionScaleRotation( initialOffset.x, initialOffset.y, initialScale, 0, 200 );
+    }
+
+    public void setInitialOffset( final double x, final double y ) {
+        this.initialOffset = Vector2D.v( x, y );
+        setOffset( x, y );
+    }
+}
---
+++ simulations-java/simulations/forces-and-motion-basics/src/edu/colorado/phet/forcesandmotionbasics/motion/StackableNodeContext.java
@@ -0,0 +1,9 @@
+// Copyright 2002-2012, University of Colorado
+package edu.colorado.phet.forcesandmotionbasics.motion;
+
+/**
+ * @author Sam Reid
+ */
+public interface StackableNodeContext {
+    void stackableNodeDropped( StackableNode stackableNode );
+}
