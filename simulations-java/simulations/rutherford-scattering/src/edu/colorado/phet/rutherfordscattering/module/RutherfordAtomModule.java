8e0ee051fa9b5d304e92d69d76eb4c1c7f0534e6
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-09-24 16:52:23 +0000

    #3437, label length of boxes (set RSConstants.SHOW_BOX_LENGTH=false to disabled this)

--- simulations-java/simulations/rutherford-scattering/src/edu/colorado/phet/rutherfordscattering/module/RutherfordAtomModule.java
+++ simulations-java/simulations/rutherford-scattering/src/edu/colorado/phet/rutherfordscattering/module/RutherfordAtomModule.java
@@ -2,9 +2,11 @@
 
 package edu.colorado.phet.rutherfordscattering.module;
 
+import java.awt.Color;
 import java.awt.geom.Dimension2D;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
+import java.text.MessageFormat;
 
 import edu.colorado.phet.common.phetcommon.model.clock.IClock;
 import edu.colorado.phet.common.piccolophet.PhetPCanvas;
@@ -26,13 +28,13 @@ import edu.umd.cs.piccolo.event.PInputEvent;
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
 public class RutherfordAtomModule extends RSAbstractModule {
-    
+
     //----------------------------------------------------------------------------
     // Class data
     //----------------------------------------------------------------------------
-    
+
     private static final boolean HAS_WIGGLE_ME = true;
-    
+
     //----------------------------------------------------------------------------
     // Instance data
     //----------------------------------------------------------------------------
@@ -41,7 +43,7 @@ public class RutherfordAtomModule extends RSAbstractModule {
     private RSModel _model;
     private Gun _gun;
     private RutherfordAtom _atom;
-    
+
     // View
     private PhetPCanvas _canvas;
     private PNode _rootNode;
@@ -50,18 +52,19 @@ public class RutherfordAtomModule extends RSAbstractModule {
     private BeamNode _beamNode;
     private GunNode _gunNode;
     private AnimationBoxNode _animationBoxNode;
+    private PNode _lengthNode;
     private ZoomIndicatorNode _zoomIndicatorNode;
     private TracesNode _alphaParticleTracesNode;
     private RutherfordAtomNode _atomNode;
-    
+
     // Control panels
     private PiccoloClockControlPanel _clockControlPanel;
     private RutherfordAtomControlPanel _controlPanel;
-    
+
     // Help
     private DefaultWiggleMe _wiggleMe;
     private boolean _wiggleMeInitialized = false;
-    
+
     //----------------------------------------------------------------------------
     // Constructors
     //----------------------------------------------------------------------------
@@ -82,10 +85,10 @@ public class RutherfordAtomModule extends RSAbstractModule {
         Point2D position = new Point2D.Double( 0, 0 );
         double orientation = Math.toRadians( -90 ); // pointing straight up
         double nozzleWidth = RSConstants.ANIMATION_BOX_SIZE.width;
-        _gun = new Gun( position, orientation, nozzleWidth, 
-                RSConstants.INITIAL_SPEED_RANGE,
-                RSConstants.BEAM_OF_ALPHA_PARTICLES_COLOR, 
-                RSConstants.ANIMATION_BOX_SIZE );
+        _gun = new Gun( position, orientation, nozzleWidth,
+                        RSConstants.INITIAL_SPEED_RANGE,
+                        RSConstants.BEAM_OF_ALPHA_PARTICLES_COLOR,
+                        RSConstants.ANIMATION_BOX_SIZE );
 
         // Space
         double spaceWidth = _gun.getNozzleWidth();
@@ -97,8 +100,8 @@ public class RutherfordAtomModule extends RSAbstractModule {
         Point2D spaceCenter = space.getCenter();
         double radius = 0.95 * ( RSConstants.ANIMATION_BOX_SIZE.width / 2 );
         _atom = new RutherfordAtom( spaceCenter, radius, RSConstants.ELECTRON_ANGULAR_SPEED,
-                RSConstants.NUMBER_OF_PROTONS_RANGE, RSConstants.NUMBER_OF_NEUTRONS_RANGE, RSConstants.ANIMATION_BOX_SIZE );
-        
+                                    RSConstants.NUMBER_OF_PROTONS_RANGE, RSConstants.NUMBER_OF_NEUTRONS_RANGE, RSConstants.ANIMATION_BOX_SIZE );
+
         // Model
         _model = new RSModel( clock, _gun, space, _atom );
 
@@ -116,7 +119,7 @@ public class RutherfordAtomModule extends RSAbstractModule {
         // Root of our scene graph
         _rootNode = new PNode();
         _canvas.addWorldChild( _rootNode );
-        
+
         // Box + Beam + Gun
         {
             // Parent node, used for layout
@@ -130,7 +133,7 @@ public class RutherfordAtomModule extends RSAbstractModule {
             _boxBeamGunParent.addChild( _beamNode );
             _boxBeamGunParent.addChild( _boxOfAtomsNode );
             _boxBeamGunParent.addChild( _gunNode );
-            
+
             // Positioning
             final double gunCenterOffset = 20;
             final double boxWidth = _boxOfAtomsNode.getFullBounds().getWidth();
@@ -150,13 +153,18 @@ public class RutherfordAtomModule extends RSAbstractModule {
         // Animation box
         _animationBoxNode = new AnimationBoxNode( _model, RSConstants.ANIMATION_BOX_SIZE );
 
+        // Length indicator for the box
+        final double length = 150; // fm
+        final String pattern = "{0} fm (nuclear scale)";  //TODO i18n
+        _lengthNode = new BoxLengthNode( RSConstants.ANIMATION_BOX_SIZE.getWidth(), MessageFormat.format( pattern, length ), Color.WHITE );
+
         // Zoom indicator
         _zoomIndicatorNode = new ZoomIndicatorNode();
-        
+
         // Atom
         _atomNode = new RutherfordAtomNode( _atom );
-        _animationBoxNode.getAtomLayer().addChild(  _atomNode  );
-        
+        _animationBoxNode.getAtomLayer().addChild( _atomNode );
+
         // Alpha Particles tracer
         {
             _alphaParticleTracesNode = new TracesNode( _model );
@@ -169,8 +177,11 @@ public class RutherfordAtomModule extends RSAbstractModule {
             _rootNode.addChild( _boxBeamGunParent );
             _rootNode.addChild( _animationBoxNode );
             _rootNode.addChild( _zoomIndicatorNode );
+            if ( RSConstants.SHOW_BOX_LENGTH ) {
+                _rootNode.addChild( _lengthNode );
+            }
         }
-        
+
         //----------------------------------------------------------------------------
         // Control
         //----------------------------------------------------------------------------
@@ -178,7 +189,7 @@ public class RutherfordAtomModule extends RSAbstractModule {
         // Clock controls
         _clockControlPanel = new PiccoloClockControlPanel( (RSClock) getClock() );
         setClockControlPanel( _clockControlPanel );
-        
+
         // Control panel
         _controlPanel = new RutherfordAtomControlPanel( this );
         setControlPanel( _controlPanel );
@@ -191,7 +202,7 @@ public class RutherfordAtomModule extends RSAbstractModule {
             //XXX add help items to the help pane
 //            HelpPane helpPane = getDefaultHelpPane();
         }
-        
+
         // See initWiggleMe for Wiggle Me initialization.
 
         //----------------------------------------------------------------------------
@@ -205,27 +216,27 @@ public class RutherfordAtomModule extends RSAbstractModule {
     //----------------------------------------------------------------------------
     // Accessors
     //----------------------------------------------------------------------------
-    
+
     public Gun getGun() {
         return _model.getGun();
     }
-    
+
     public RutherfordAtom getAtom() {
         return _atom;
     }
-    
+
     public void removeAllAlphaParticles() {
         _model.removeAllAlphaParticles();
     }
-    
+
     public TracesNode getTracesNode() {
         return _alphaParticleTracesNode;
     }
-    
+
     public RutherfordAtomNode getAtomNode() {
         return _atomNode;
     }
-    
+
     //----------------------------------------------------------------------------
     // Superclass overrides
     //----------------------------------------------------------------------------
@@ -234,22 +245,22 @@ public class RutherfordAtomModule extends RSAbstractModule {
      * Resets the module to its default state.
      */
     public void reset() {
-        
+
         IClock clock = getClock();
 
         clock.pause();
-        
+
         _model.removeAllAlphaParticles();
-        
+
         _gun.setEnabled( RSConstants.GUN_ENABLED );
         _gun.setIntensity( RSConstants.GUN_INTENSITY );
         _gun.setSpeed( RSConstants.INITIAL_SPEED_RANGE.getDefault() );
-        
+
         _atom.setNumberOfProtons( RSConstants.NUMBER_OF_PROTONS_RANGE.getDefault() );
         _atom.setNumberOfNeutrons( RSConstants.NUMBER_OF_NEUTRONS_RANGE.getDefault() );
-        
+
         _controlPanel.setTracesEnabled( RSConstants.TRACES_ENABLED );
-        
+
         if ( isActive() ) {
             if ( RSConstants.CLOCK_PAUSED ) {
                 clock.pause();
@@ -259,14 +270,14 @@ public class RutherfordAtomModule extends RSAbstractModule {
             }
         }
     }
-    
+
     //----------------------------------------------------------------------------
     // AbstractModule implementation
     //----------------------------------------------------------------------------
-    
+
     /*
-     * Updates the layout of stuff on the canvas.
-     */
+    * Updates the layout of stuff on the canvas.
+    */
     protected void updateCanvasLayout() {
 
         Dimension2D worldSize = _canvas.getWorldSize();
@@ -275,7 +286,7 @@ public class RutherfordAtomModule extends RSAbstractModule {
             // canvas hasn't been sized, blow off layout
             return;
         }
-        
+
         // margins and spacing
         final double xMargin = 40;
         final double yMargin = 40;
@@ -291,7 +302,7 @@ public class RutherfordAtomModule extends RSAbstractModule {
             y = 250;
             _boxBeamGunParent.setOffset( x, y );
         }
-        
+
         // Animation box
         {
             // to the right of the box/beam/gun, below the "not to scale" label
@@ -300,33 +311,37 @@ public class RutherfordAtomModule extends RSAbstractModule {
             _animationBoxNode.setOffset( x, y );
         }
 
+        // Units
+        _lengthNode.setOffset( _animationBoxNode.getXOffset(),
+                               _animationBoxNode.getFullBoundsReference().getMaxY() + 10 );
+
         // Zoom Indicator
         {
             Rectangle2D tinyBoxBounds = _zoomIndicatorNode.globalToLocal( _boxOfAtomsNode.getTinyBoxGlobalFullBounds() );
             Rectangle2D bigBoxBounds = _zoomIndicatorNode.globalToLocal( _animationBoxNode.getGlobalFullBounds() );
             _zoomIndicatorNode.update( tinyBoxBounds, bigBoxBounds );
         }
-        
+
         initWiggleMe();
     }
-    
+
     //----------------------------------------------------------------------------
     // Wiggle Me
     //----------------------------------------------------------------------------
-    
+
     /*
-     * Initializes a wiggle me that points to the gun on/off button.
-     */
+    * Initializes a wiggle me that points to the gun on/off button.
+    */
     private void initWiggleMe() {
         if ( !_wiggleMeInitialized && HAS_WIGGLE_ME ) {
-            
+
             // Create wiggle me, add to root node.
             String wiggleMeString = RSResources.getString( "string.turnOnTheGun" );
             _wiggleMe = new DefaultWiggleMe( _canvas, wiggleMeString );
             _wiggleMe.setArrowTailPosition( MotionHelpBalloon.TOP_LEFT );
             _wiggleMe.setArrowLength( 60 );
             _rootNode.addChild( _wiggleMe );
-            
+
             // Animate from the upper- right to the gun button position
             PNode gunButtonNode = _gunNode.getButtonNode();
             Rectangle2D bounds = _rootNode.globalToLocal( gunButtonNode.getGlobalFullBounds() );
@@ -334,7 +349,7 @@ public class RutherfordAtomModule extends RSAbstractModule {
             final double y = bounds.getMaxY();
             _wiggleMe.setOffset( 400, -100 );
             _wiggleMe.animateTo( x, y );
-            
+
             // Clicking on the canvas makes the wiggle me go away.
             _canvas.addInputEventListener( new PBasicInputEventHandler() {
                 public void mousePressed( PInputEvent event ) {
@@ -344,7 +359,7 @@ public class RutherfordAtomModule extends RSAbstractModule {
                     _wiggleMe = null;
                 }
             } );
-            
+
             _wiggleMeInitialized = true;
         }
     }
