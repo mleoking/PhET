6171712e2817de58e64e8590c8d3d0b6e58374bb
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-09-25 20:53:36 +0000

    Separated the energy converter and supporting post for the solar panel and re-layered for better energy chunk behavior.

--- trunk/simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/energysystems/model/FaucetAndWater.java
+++ trunk/simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/energysystems/model/FaucetAndWater.java
@@ -32,7 +32,7 @@ public class FaucetAndWater extends EnergySource {
 
     public static final Vector2D OFFSET_FROM_CENTER_TO_WATER_ORIGIN = new Vector2D( 0.065, 0.08 );
     private static final double MAX_ENERGY_PRODUCTION_RATE = 200; // In joules/second.
-    private static final double WATER_FALLING_VELOCITY = 0.07; // In meters/second.
+    private static final double WATER_FALLING_VELOCITY = 0.09; // In meters/second.
     private static final double FLOW_PER_CHUNK = 0.4;  // Empirically determined to get desired energy chunk emission rate.
     private static final double MAX_WATER_WIDTH = 0.015; // In meters.
     private static final double MAX_DISTANCE_FROM_FAUCET_TO_BOTTOM_OF_WATER = 0.5; // In meters.
