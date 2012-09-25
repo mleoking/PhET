d98c3f80cffa3b796421e9ff6919c1a8a49786a6
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-09-25 22:42:13 +0000

    Added preliminary version of energy chunk handling to the beaker heater.

--- trunk/simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/energysystems/view/BeakerHeaterNode.java
+++ trunk/simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/energysystems/view/BeakerHeaterNode.java
@@ -3,6 +3,7 @@ package edu.colorado.phet.energyformsandchanges.energysystems.view;
 
 import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
+import edu.colorado.phet.energyformsandchanges.common.view.EnergyChunkLayer;
 import edu.colorado.phet.energyformsandchanges.energysystems.model.BeakerHeater;
 import edu.umd.cs.piccolo.nodes.PImage;
 
@@ -16,9 +17,11 @@ public class BeakerHeaterNode extends ImageBasedEnergySystemElementNode {
     public BeakerHeaterNode( final BeakerHeater beakerHeater, final ModelViewTransform mvt ) {
         super( beakerHeater, mvt );
 
-        // Add the images.  Assumes that the last image is the energized heating coil.
+        // Add the images that are used to depict this element along with the
+        // layer that will contain the energy chunks.
         addImageNode( BeakerHeater.WIRE_STRAIGHT_IMAGE );
         addImageNode( BeakerHeater.WIRE_CURVE_IMAGE );
+        addChild( new EnergyChunkLayer( beakerHeater.energyChunkList, beakerHeater.getObservablePosition(), mvt ) );
         addImageNode( BeakerHeater.ELEMENT_BASE_IMAGE );
         addImageNode( BeakerHeater.HEATER_ELEMENT_OFF_IMAGE );
         final PImage energizedCoil = addImageNode( BeakerHeater.HEATER_ELEMENT_ON_IMAGE );
