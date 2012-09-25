85cf526d66de0d5b7cf7115244d89bba2b7cff69
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-09-25 22:11:59 +0000

    Tab 1: Make flag wave

--- trunk/simulations-java/simulations/forces-and-motion-basics/src/edu/colorado/phet/forcesandmotionbasics/tugofwar/FlagNode.java
+++ trunk/simulations-java/simulations/forces-and-motion-basics/src/edu/colorado/phet/forcesandmotionbasics/tugofwar/FlagNode.java
@@ -2,7 +2,11 @@ package edu.colorado.phet.forcesandmotionbasics.tugofwar;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.geom.GeneralPath;
+
+import javax.swing.Timer;
 
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
@@ -13,21 +17,42 @@ import edu.umd.cs.piccolo.PNode;
  * @author Sam Reid
  */
 public class FlagNode extends PNode {
+
+    private final Timer timer;
+    private final PhetPText textNode;
+
     public FlagNode( Color color, final String text ) {
-        GeneralPath path = new GeneralPath();
-        final int maxX = 175;
-        final int maxY = 75;
-        path.moveTo( 0, 0 );
-        path.curveTo( maxX / 3, 25, 2 * maxX / 3, -25, maxX, 0 );
-        path.lineTo( maxX, maxY );
-        path.curveTo( 2 * maxX / 3, -25 + maxY, maxX / 3, 25 + maxY, 0, maxY );
-        path.lineTo( 0, 0 );
-        path.closePath();
-        final PhetPPath p = new PhetPPath( path, color, new BasicStroke( 2 ), Color.black );
+        final PhetPPath p = new PhetPPath( createPath(), color, new BasicStroke( 2 ), Color.black );
         addChild( p );
-        addChild( new PhetPText( text, new PhetFont( 32, true ) ) {{
+        textNode = new PhetPText( text, new PhetFont( 32, true ) ) {{
             setTextPaint( Color.white );
             centerFullBoundsOnPoint( p.getFullBounds().getCenterX(), p.getFullBounds().getCenterY() );
-        }} );
+        }};
+        addChild( textNode );
+
+        timer = new Timer( 20, new ActionListener() {
+            public void actionPerformed( final ActionEvent e ) {
+                p.setPathTo( createPath() );
+            }
+        } );
+        timer.start();
     }
+
+    private GeneralPath createPath() {
+        double time = System.currentTimeMillis() / 1000.0;
+        GeneralPath path = new GeneralPath();
+        final int maxX = 175;
+        final float maxY = (float) ( 75 );
+        final float dy = (float) ( 7 * Math.sin( time * 6 ) );
+        final float dx = (float) ( 2 * Math.sin( time * 5 ) ) + 10;
+        path.moveTo( 0, 0 );
+        path.curveTo( maxX / 3 + dx, 25 + dy, 2 * maxX / 3 + dx, -25 - dy, maxX + dx, dy / 2 );
+        path.lineTo( maxX + dx, maxY + dy / 2 );
+        path.curveTo( 2 * maxX / 3 + dx, -25 + maxY - dy, maxX / 3 + dx, 25 + maxY + dy, 0, maxY );
+        path.lineTo( 0, 0 );
+        path.closePath();
+        return path;
+    }
+
+    public void dispose() { timer.stop(); }
 }
