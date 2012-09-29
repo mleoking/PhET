2b8e8b1888158add306aa890dc2c72f87f7c84e5
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-09-29 00:51:55 +0000

    Tab 1: Fixed: Return button should disappear after being pressed

--- trunk/simulations-java/simulations/forces-and-motion-basics/src/edu/colorado/phet/forcesandmotionbasics/tugofwar/Cart.java
+++ trunk/simulations-java/simulations/forces-and-motion-basics/src/edu/colorado/phet/forcesandmotionbasics/tugofwar/Cart.java
@@ -1,6 +1,8 @@
 // Copyright 2002-2012, University of Colorado
 package edu.colorado.phet.forcesandmotionbasics.tugofwar;
 
+import edu.colorado.phet.common.phetcommon.model.property.doubleproperty.DoubleProperty;
+
 /**
  * @author Sam Reid
  */
@@ -9,23 +11,23 @@ public class Cart {
     public static final boolean dev = false;
     private static final double DEBUG_SPEED = dev ? 5 : 1;
     private double velocity = 0;
-    private double position = 0;
+    public DoubleProperty position = new DoubleProperty( 0.0 );
 
     public Cart() {
     }
 
     public double getPosition() {
-        return position;
+        return position.get();
     }
 
     public void stepInTime( double dt, double acceleration ) {
         dt = dt * 35 * DEBUG_SPEED;//speed up time because the masses of the characters give everything too much momentum
         velocity = velocity + acceleration * dt;
-        position = position + velocity * dt;
+        position.set( position.get() + velocity * dt );
     }
 
     public void restart() {
         velocity = 0.0;
-        position = 0.0;
+        position.reset();
     }
 }
