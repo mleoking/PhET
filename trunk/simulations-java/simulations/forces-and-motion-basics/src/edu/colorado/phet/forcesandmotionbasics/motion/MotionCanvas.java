bcd971249b86c816f11b4f3584a80311a45047ca
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-10-01 17:23:17 +0000

    Tab 2-3: Moved "Masses" check box above "Speed" checkbox

--- trunk/simulations-java/simulations/forces-and-motion-basics/src/edu/colorado/phet/forcesandmotionbasics/motion/MotionCanvas.java
+++ trunk/simulations-java/simulations/forces-and-motion-basics/src/edu/colorado/phet/forcesandmotionbasics/motion/MotionCanvas.java
@@ -235,12 +235,12 @@ public class MotionCanvas extends AbstractForcesAndMotionBasicsCanvas implements
         final PNode speedControlPanel = new HBox( 15, new PSwing( speedCheckBox ), new SpeedometerNode( "Speed", 125, model.speed, STROBE_SPEED ) {{scale( 0.25 );}} );
         final VBox vbox = friction ?
                           new VBox( 0, VBox.LEFT_ALIGNED, new PSwing( showForcesCheckBox ), indent( showValuesCheckBox ), indent( showSumOfForcesCheckBox ),
-                                    speedControlPanel,
                                     new PSwing( massCheckBox ),
+                                    speedControlPanel,
                                     new FrictionSliderControl( model.frictionValue ) ) :
                           new VBox( 0, VBox.LEFT_ALIGNED, new PSwing( showForcesCheckBox ), indent( showValuesCheckBox ),
-                                    speedControlPanel,
-                                    new PSwing( massCheckBox ) );
+                                    new PSwing( massCheckBox ),
+                                    speedControlPanel );
 
         final ControlPanelNode controlPanelNode = new ControlPanelNode( vbox, new Color( 227, 233, 128 ), new BasicStroke( 2 ), Color.black );
         controlPanelNode.setOffset( STAGE_SIZE.width - controlPanelNode.getFullWidth() - INSET, INSET );
