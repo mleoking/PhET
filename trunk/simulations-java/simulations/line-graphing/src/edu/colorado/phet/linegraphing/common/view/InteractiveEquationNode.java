49c8469bf3d48208c0012bbbd356f0e1e3a07afb
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-10-03 22:32:18 +0000

    10/3/12 consolidate equation layout metrics in a base class, used by all equation renderers

--- trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/common/view/InteractiveEquationNode.java
+++ trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/common/view/InteractiveEquationNode.java
@@ -4,75 +4,23 @@ package edu.colorado.phet.linegraphing.common.view;
 import java.text.NumberFormat;
 
 import edu.colorado.phet.common.phetcommon.model.property.Property;
-import edu.colorado.phet.common.phetcommon.util.DefaultDecimalFormat;
 import edu.colorado.phet.common.phetcommon.util.DoubleRange;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
-import edu.colorado.phet.common.piccolophet.PhetPNode;
 import edu.colorado.phet.linegraphing.common.LGSimSharing.UserComponents;
 import edu.colorado.phet.linegraphing.common.view.SlopeSpinnerNode.RiseSpinnerNode;
 import edu.colorado.phet.linegraphing.common.view.SlopeSpinnerNode.RunSpinnerNode;
 import edu.colorado.phet.linegraphing.common.view.SpinnerStateIndicator.SlopeColors;
 import edu.umd.cs.piccolo.PNode;
-import edu.umd.cs.piccolo.util.PDimension;
 
 /**
  * Base class for all interactive equations.
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
-public class InteractiveEquationNode extends PhetPNode {
-
-    protected static final NumberFormat FORMAT = new DefaultDecimalFormat( "0" );
-
-    protected final int pointSize; // point size of the static font used to render the equation
-
-    /*
-    * This controls the vertical offset of the slope's sign.
-    * Zero is vertically centered on the equals sign, positive values move it down, negative move it up.
-    * This was created because there was a great deal of discussion and disagreement about where the sign should be placed.
-    */
-    protected final double slopeSignYOffset;
-
-    // fudge factors for horizontal lines, to vertically center them with equals sign (set by visual inspection)
-    protected final double slopeSignYFudgeFactor;
-    protected final double operatorYFudgeFactor;
-    protected final double fractionLineYFudgeFactor;
-    protected final double undefinedSlopeYFudgeFactor;
-
-    protected final float fractionLineThickness; // thickness of the fraction divisor line
-    protected final PDimension operatorLineSize; // size of the lines used to create + and - operators
-    protected final PDimension signLineSize; // size of the lines used to create + and - signs
-
-    // spacing between components of an equation (set by visual inspection)
-    protected final double integerSignXSpacing; // spacing between a sign and the integer to the right of it
-    protected final double fractionSignXSpacing; // spacing between a sign and the fraction to the right of it
-    protected final double slopeXSpacing; // spacing between the slope and what's to the right of it
-    protected final double operatorXSpacing; // space around an operator (eg, +)
-    protected final double relationalOperatorXSpacing; // space around the relational operator (eg, =)
-    protected final double parenXSpacing; // space between a parenthesis and the thing it encloses
-    protected final double spinnersYSpacing; // y spacing between spinners and fraction line
-    protected final double ySpacing; // all y spacing
+public class InteractiveEquationNode extends EquationNode {
 
     protected InteractiveEquationNode( int pointSize ) {
-        this.pointSize = pointSize;
-
-        // Compute dimensions and layout offsets as percentages of the font's point size.
-        slopeSignYOffset = 0;
-        slopeSignYFudgeFactor = 0.07 * pointSize;
-        operatorYFudgeFactor = 0.07 * pointSize;
-        fractionLineYFudgeFactor = 0.07 * pointSize;
-        undefinedSlopeYFudgeFactor = 0.07 * pointSize;
-        fractionLineThickness = 0.06f * pointSize;
-        operatorLineSize = new PDimension( 0.54 * pointSize, 0.07 * pointSize );
-        signLineSize = new PDimension( 0.54 * pointSize, 0.11 * pointSize );
-        integerSignXSpacing = 0.18 * pointSize;
-        fractionSignXSpacing = 0.36 * pointSize;
-        slopeXSpacing = 0.15 * pointSize;
-        operatorXSpacing = 0.25 * pointSize;
-        relationalOperatorXSpacing = 0.35 * pointSize;
-        parenXSpacing = 0.07 * pointSize;
-        ySpacing = 0.1 * pointSize;
-        spinnersYSpacing = 0.2 * pointSize;
+        super( pointSize );
     }
 
     // Gets the max width for the rise and run spinners used in an interactive equation.
--- trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/common/view/StaticEquationNode.java
+++ trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/common/view/StaticEquationNode.java
@@ -7,63 +7,21 @@ import java.awt.Paint;
 import java.awt.geom.Rectangle2D;
 
 import edu.colorado.phet.common.phetcommon.math.MathUtil;
-import edu.colorado.phet.common.piccolophet.PhetPNode;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPText;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.nodes.PPath;
 import edu.umd.cs.piccolo.nodes.PText;
-import edu.umd.cs.piccolo.util.PDimension;
 
 /**
  * Base class for all static equation nodes.
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
-public abstract class StaticEquationNode extends PhetPNode {
-
-    /*
-    * This controls the vertical offset of the slope's sign.
-    * Zero is vertically centered on the equals sign, positive values move it down, negative move it up.
-    * This was created because there was a great deal of discussion and disagreement about where the sign should be placed.
-    */
-    protected final double slopeSignYOffset;
-
-    // fudge factors for horizontal lines, to vertically center them with equals sign (set by visual inspection)
-    protected final double slopeSignYFudgeFactor;
-    protected final double operatorYFudgeFactor;
-    protected final double fractionLineYFudgeFactor;
-
-    protected final PDimension operatorLineSize;  // size of the lines used to create + and - operators
-    protected final PDimension signLineSize; // size of the lines used to create + and - signs
-    protected final double fractionLineThickness; // thickness of the fraction divisor line
-
-    // spacing between components of an equation (set by visual inspection)
-    protected final double integerSignXSpacing; // spacing between a sign and the integer to the right of it
-    protected final double fractionSignXSpacing; // spacing between a sign and the fraction to the right of it
-    protected final double slopeXSpacing; // spacing between the slope and what's to the right of it
-    protected final double operatorXSpacing; // space around an operator (eg, +)
-    protected final double relationalOperatorXSpacing; // space around the relational operator (eg, =)
-    protected final double parenXSpacing; // space between a parenthesis and the thing it encloses
-    protected final double ySpacing; // all y spacing
+public abstract class StaticEquationNode extends EquationNode {
 
     public StaticEquationNode( int pointSize ) {
+        super( pointSize );
         setPickable( false );
-
-        // Compute dimensions and layout offsets as percentages of the font's point size.
-        slopeSignYOffset = 0;
-        slopeSignYFudgeFactor = 0.08 * pointSize;
-        operatorYFudgeFactor = 0.08 * pointSize;
-        fractionLineYFudgeFactor = 0.08 * pointSize;
-        operatorLineSize = new PDimension( 0.42 * pointSize, 0.08 * pointSize );
-        signLineSize = new PDimension( 0.54 * pointSize, 0.1 * pointSize );
-        fractionLineThickness = 0.06 * pointSize;
-        integerSignXSpacing = 0.0625 * pointSize;
-        fractionSignXSpacing = 0.36 * pointSize;
-        slopeXSpacing = 0.15 * pointSize; //TODO have separate spacing for fraction vs integer slope, this is a bit big for integer slopes
-        operatorXSpacing = 0.25 * pointSize;
-        relationalOperatorXSpacing = 0.35 * pointSize;
-        parenXSpacing = 0.08 * pointSize;
-        ySpacing = 0.1 * pointSize;
     }
 
     // Changes the color of the equation by doing a deep traversal of this node's descendants.
