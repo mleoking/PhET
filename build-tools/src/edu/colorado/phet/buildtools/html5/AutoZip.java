2947781c454aeb1041484ebc94cc03f146aae5fd
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-10-05 04:18:08 +0000

    Added autozip tool

---
+++ build-tools/src/edu/colorado/phet/buildtools/html5/AutoZip.java
@@ -0,0 +1,116 @@
+package edu.colorado.phet.buildtools.html5;
+
+import scala.actors.threadpool.Arrays;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.tools.ant.Location;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.Target;
+import org.apache.tools.ant.taskdefs.Zip;
+
+/**
+ * For testing with CocoonJS, scan a filesystem and automatically re-zip whenever anything changes.
+ * I would have preferred to do this with SBT but couldn't figure out an easy way to do so.
+ * <p/>
+ * Note: this deletes files from your filesystem, so please be sure you know what you are doing before launching this.
+ *
+ * @author Sam Reid
+ */
+public class AutoZip {
+    static long lastModified = 0;
+
+    public static void main( String[] args ) {
+        final File root = new File( args[0] );
+        System.out.println( "root = " + root );
+
+        new Thread( new Runnable() {
+            public void run() {
+                long count = 0;
+                while ( true ) {
+                    long modifiedTime = lastModified( root );
+                    if ( modifiedTime > lastModified ) {
+                        lastModified = modifiedTime;
+                        try {
+                            runTask( root );
+                        }
+                        catch ( IOException e1 ) {
+                            e1.printStackTrace();
+                        }
+                        System.out.println();
+                        System.out.println( "Finished task" );
+                    }
+                    else {
+                        if ( count % 30 == 0 ) {
+                            System.out.print( "." );
+                        }
+                    }
+                    count++;
+                    try {
+                        Thread.sleep( 30 );
+                    }
+                    catch ( InterruptedException e ) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        } ).start();
+    }
+
+    private static void runTask( final File root ) throws IOException {
+        final File autozip = new File( root.getParentFile(), "autozip.zip" );
+
+        if ( autozip.exists() ) {
+            boolean deleted = autozip.delete();
+            if ( !deleted ) {
+                throw new RuntimeException( "errored" );
+            }
+        }
+//        FileUtils.zip( getAllFiles( root ), autozip );
+
+        Zip zip = new Zip();
+        zip.setBasedir( root );
+        zip.setDestFile( autozip );
+        zip.setProject( new Project() );
+        zip.setLocation( Location.UNKNOWN_LOCATION );
+        zip.setOwningTarget( new Target() );
+        zip.init();
+        zip.execute();
+    }
+
+    private static File[] getAllFiles( final File root ) {
+        if ( root.isFile() ) {
+            return new File[] { root };
+        }
+        else {
+            ArrayList<File> files = new ArrayList<File>();
+            for ( File file : root.listFiles( new FilenameFilter() {
+                public boolean accept( final File dir, final String name ) {
+                    return !name.toLowerCase().equals( ".svn" );
+                }
+            } ) ) {
+                files.addAll( Arrays.asList( getAllFiles( file ) ) );
+            }
+            return files.toArray( new File[files.size()] );
+        }
+    }
+
+    private static long lastModified( final File root ) {
+        if ( root.isDirectory() ) {
+            long base = root.lastModified();
+            for ( File file : root.listFiles() ) {
+                final long last = file.lastModified();
+                if ( last > base ) {
+                    base = last;
+                }
+            }
+            return base;
+        }
+        else {
+            return root.lastModified();
+        }
+    }
+}
