e2a2380fd3b7e51929d077acad61fd28e48a25e8
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-10-06 03:46:52 +0000

    instantiate some colors in LGColors to take advantage of IDEA's color editor

--- trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/common/LGColors.java
+++ trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/common/LGColors.java
@@ -17,10 +17,10 @@ public class LGColors {
     public static final Color INTERACTIVE_LINE = PhetColorScheme.RED_COLORBLIND;
     public static final Color SAVED_LINE_NORMAL = Color.GRAY;
     public static final Color SAVED_LINE_HIGHLIGHT = new Color( 0, 190, 0 );
-    public static final Color Y_EQUALS_X = Color.BLUE;
+    public static final Color Y_EQUALS_X = new Color( 0, 0, 255 );
     public static final Color Y_EQUALS_NEGATIVE_X = new Color( 0, 178, 178 );
     public static final Color SLOPE = new Color( 120, 245, 3 );
-    public static final Color INTERCEPT = Color.YELLOW;
+    public static final Color INTERCEPT = new Color( 255, 255, 0 );
     public static final Color POINT_X1_Y1 = new Color( 255, 125, 255 );
     public static final Color POINT_X2_Y2 = POINT_X1_Y1;
     public static final Color SAVE_LINE_BUTTON = Color.WHITE;
