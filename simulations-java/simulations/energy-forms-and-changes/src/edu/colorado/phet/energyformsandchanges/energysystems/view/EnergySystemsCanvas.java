6df4a1731fce565d6a0ef54f51af6ba6d812fcb7
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-10-08 22:36:17 +0000

    Made the light rays from the sun be shown only when the energy chunks are NOT shown.

--- simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/energysystems/view/EnergySystemsCanvas.java
+++ simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/energysystems/view/EnergySystemsCanvas.java
@@ -110,7 +110,7 @@ public class EnergySystemsCanvas extends PhetPCanvas {
 
         // Add the various energy system elements.
         PNode faucetAndWaterNode = new FaucetAndWaterNode( model.faucet, mvt );
-        PNode sunNode = new SunNode( model.sun, mvt );
+        PNode sunNode = new SunNode( model.sun, mvt, model.energyChunksVisible );
         PNode teaPotNode = new TeaPotNode( model.teaPot, mvt );
         PNode bikerNode = new BikerNode( model.biker, mvt );
         PNode waterPoweredGeneratorNode = new WaterPoweredGeneratorNode( model.waterPoweredGenerator, mvt );
