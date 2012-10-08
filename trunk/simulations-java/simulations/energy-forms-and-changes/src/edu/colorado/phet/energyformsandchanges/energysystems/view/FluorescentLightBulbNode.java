7f9537c9a951e7dd0532fbda640123d1da6785df
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-10-08 19:23:35 +0000

    Split out the view for the different light bulb types, integrated the new images for the fluorescent light bulb.

---
+++ trunk/simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/energysystems/view/FluorescentLightBulbNode.java
@@ -0,0 +1,52 @@
+// Copyright 2002-2012, University of Colorado
+package edu.colorado.phet.energyformsandchanges.energysystems.view;
+
+import java.awt.Color;
+
+import edu.colorado.phet.common.phetcommon.math.vector.Vector2D;
+import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
+import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
+import edu.colorado.phet.energyformsandchanges.common.view.EnergyChunkLayer;
+import edu.colorado.phet.energyformsandchanges.energysystems.model.FluorescentLightBulb;
+import edu.colorado.phet.energyformsandchanges.energysystems.model.LightBulb;
+import edu.umd.cs.piccolo.PNode;
+
+/**
+ * Piccolo node that represents the fluorescent light bulb in the view.
+ *
+ * @author John Blanco
+ */
+public class FluorescentLightBulbNode extends ImageBasedEnergySystemElementNode {
+
+    public FluorescentLightBulbNode( final FluorescentLightBulb lightBulb, final ModelViewTransform mvt ) {
+        super( lightBulb, mvt );
+
+        // Add the light rays.
+        final LightRays lightRays = new LightRays( new Vector2D( 0, 0 ), 30, 400, 20, Math.PI / 4, Color.YELLOW );
+        addChild( lightRays );
+
+        // Add the images and the layer that will contain the energy chunks.
+        addImageNode( LightBulb.WIRE_FLAT_IMAGE );
+        addImageNode( LightBulb.WIRE_CURVE_IMAGE );
+        addImageNode( LightBulb.ELEMENT_BASE_BACK_IMAGE );
+        addImageNode( FluorescentLightBulb.BACK_OFF );
+        final PNode energizedBack = addImageNode( FluorescentLightBulb.BACK_ON );
+        addChild( new EnergyChunkLayer( lightBulb.energyChunkList, lightBulb.getObservablePosition(), mvt ) );
+        addImageNode( LightBulb.ELEMENT_BASE_FRONT_IMAGE );
+        addImageNode( FluorescentLightBulb.FRONT_OFF );
+        final PNode energizedFront = addImageNode( FluorescentLightBulb.FRONT_ON );
+
+        // Center the light rays on the bulb image.
+        lightRays.setOffset( energizedFront.getFullBoundsReference().getCenterX(),
+                             energizedFront.getFullBoundsReference().getCenterY() - energizedFront.getFullBoundsReference().getHeight() * 0.10 );
+
+        // Update the transparency of the lit bulb based on model element.
+        lightBulb.litProportion.addObserver( new VoidFunction1<Double>() {
+            public void apply( Double litProportion ) {
+                energizedFront.setTransparency( litProportion.floatValue() );
+                energizedBack.setTransparency( litProportion.floatValue() );
+                lightRays.setTransparency( litProportion.floatValue() );
+            }
+        } );
+    }
+}
