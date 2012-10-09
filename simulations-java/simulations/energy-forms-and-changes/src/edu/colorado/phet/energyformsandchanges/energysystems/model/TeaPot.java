35a65ce12de80f75c2da60df82b344256f2b7495
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-10-09 20:44:34 +0000

    Added initial version of energy chunk emission to the teapot.

--- simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/energysystems/model/TeaPot.java
+++ simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/energysystems/model/TeaPot.java
@@ -1,12 +1,16 @@
 // Copyright 2002-2012, University of Colorado
 package edu.colorado.phet.energyformsandchanges.energysystems.model;
 
+import java.util.ArrayList;
+
 import edu.colorado.phet.common.phetcommon.math.vector.Vector2D;
+import edu.colorado.phet.common.phetcommon.model.property.BooleanProperty;
 import edu.colorado.phet.common.phetcommon.model.property.ObservableProperty;
 import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.phetcommon.simsharing.messages.IUserComponent;
 import edu.colorado.phet.energyformsandchanges.EnergyFormsAndChangesResources;
 import edu.colorado.phet.energyformsandchanges.EnergyFormsAndChangesSimSharing;
+import edu.colorado.phet.energyformsandchanges.common.model.EnergyChunk;
 import edu.colorado.phet.energyformsandchanges.common.model.EnergyType;
 
 import static edu.colorado.phet.energyformsandchanges.EnergyFormsAndChangesResources.Images.TEAPOT_LARGE;
@@ -26,18 +30,17 @@ public class TeaPot extends EnergySource {
     private static final double MAX_ENERGY_CHANGE_RATE = 20; // In joules/second
     private static final double COOLING_CONSTANT = 0.1; // Controls rate at which tea pot cools down, empirically determined.
     private static final double COOL_DOWN_COMPLETE_THRESHOLD = 30; // In joules/second
-
     public static final ModelElementImage TEAPOT_IMAGE = new ModelElementImage( TEAPOT_LARGE, new Vector2D( 0.0, 0.015 ) );
-
     public static final double ENERGY_REQUIRED_FOR_CHUNK_TO_EMIT = 100; // In joules, but empirically determined.
+    public static final double MAX_ENERGY_CHUNK_DISTANCE = 0.5; // In meters.
 
     //-------------------------------------------------------------------------
     // Instance Data
     //-------------------------------------------------------------------------
 
     public final Property<Double> heatCoolAmount = new Property<Double>( 0.0 );
-
     private Property<Double> energyProductionRate = new Property<Double>( 0.0 );
+    private double energyProducedSinceLastChunk = 0;
 
     //-------------------------------------------------------------------------
     // Constructor(s)
@@ -65,6 +68,25 @@ public class TeaPot extends EnergySource {
                 // trickle on forever.
                 energyProductionRate.set( 0.0 );
             }
+
+            // See if it's time to emit a new energy chunk.
+            energyProducedSinceLastChunk += energyProductionRate.get() * dt;
+            if ( energyProducedSinceLastChunk > ENERGY_REQUIRED_FOR_CHUNK_TO_EMIT ) {
+                // It's time, so emit one.
+                EnergyChunk energyChunk = new EnergyChunk( EnergyType.MECHANICAL, getPosition(), new BooleanProperty( true ) );
+                energyChunk.setVelocity( new Vector2D( 0.1, 0.1 ) );
+                energyChunkList.add( energyChunk );
+                energyProducedSinceLastChunk -= ENERGY_REQUIRED_FOR_CHUNK_TO_EMIT;
+            }
+
+            // Move all energy chunks that are under this element's control.
+            for ( EnergyChunk energyChunk : new ArrayList<EnergyChunk>( energyChunkList ) ) {
+                energyChunk.translateBasedOnVelocity( dt );
+                if ( getPosition().distance( energyChunk.position.get() ) > MAX_ENERGY_CHUNK_DISTANCE ) {
+                    // Time to remove this chunk.
+                    energyChunkList.remove( energyChunk );
+                }
+            }
         }
         return new Energy( EnergyType.MECHANICAL, energyProductionRate.get() * dt, Math.PI / 2 );
     }
@@ -73,6 +95,7 @@ public class TeaPot extends EnergySource {
         super.deactivate();
         heatCoolAmount.reset();
         energyProductionRate.reset();
+        energyProducedSinceLastChunk = 0;
     }
 
     @Override public IUserComponent getUserComponent() {
