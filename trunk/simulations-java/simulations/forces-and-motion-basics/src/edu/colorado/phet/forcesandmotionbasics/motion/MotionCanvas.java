97cfb87bb54a44e1791d229b64c14d2114ebdaf3
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-10-12 23:44:36 +0000

    Tab 2-3: Changed max speed from 9.5 m/s to 10 m/s

--- trunk/simulations-java/simulations/forces-and-motion-basics/src/edu/colorado/phet/forcesandmotionbasics/motion/MotionCanvas.java
+++ trunk/simulations-java/simulations/forces-and-motion-basics/src/edu/colorado/phet/forcesandmotionbasics/motion/MotionCanvas.java
@@ -109,7 +109,7 @@ public class MotionCanvas extends AbstractForcesAndMotionBasicsCanvas implements
     private static final double typicalDT = 0.033333333333333215;
 
     //Speed at which the bricks start to look as if they are going backwards.  Meters per second.
-    public static final double STROBE_SPEED = 9.559393222711847;
+    public static final double STROBE_SPEED = 10.0;
 
     private final BooleanProperty playing = new BooleanProperty( true );
     private final PusherNode pusherNode;
