dffddf2b91cf15db57cfab64bf1593028e77ee5e
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-10-17 04:17:34 +0000

    created game loop

--- simulations-html5/simulations/energy-skate-park-html5/src/energy-skate-park-html5.js
+++ simulations-html5/simulations/energy-skate-park-html5/src/energy-skate-park-html5.js
@@ -1,3 +1,35 @@
 (function () {
     console.log( "started" );
+    var canvas = document.createElement( "canvas" );
+    var ctx = canvas.getContext( "2d" );
+    document.body.appendChild( canvas );
+
+    //or another game loop here: http://www.playmycode.com/blog/2011/08/building-a-game-mainloop-in-javascript/
+    //or here: http://jsfiddle.net/Y9uBv/5/
+    var requestAnimationFrame =
+            requestAnimationFrame ||
+            webkitRequestAnimationFrame ||
+            mozRequestAnimationFrame ||
+            msRequestAnimationFrame ||
+            oRequestAnimationFrame;
+
+    var x = 100;
+
+    function loop() {
+
+        //http://stackoverflow.com/questions/1664785/html5-canvas-resize-to-fit-window
+        ctx.canvas.width = window.innerWidth;
+        ctx.canvas.height = window.innerHeight;
+
+        ctx.fillStyle = 'blue';
+        ctx.fillRect( 0, 0, window.innerWidth / 2, window.innerHeight / 2 );
+
+        ctx.fillStyle = 'black';
+        ctx.fillRect( x, 100, 20, 20 );
+
+        ++x;
+        requestAnimationFrame( loop );
+    }
+
+    requestAnimationFrame( loop );
 })();
