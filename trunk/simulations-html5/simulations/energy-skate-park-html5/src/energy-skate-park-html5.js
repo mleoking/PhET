24eadeff1b952d2f02d4a33ac32803dfd9ecba07
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-10-17 04:36:43 +0000

    show frame rate

--- trunk/simulations-html5/simulations/energy-skate-park-html5/src/energy-skate-park-html5.js
+++ trunk/simulations-html5/simulations/energy-skate-park-html5/src/energy-skate-park-html5.js
@@ -15,6 +15,10 @@
 
     var x = 100;
 
+    var lastTime = new Date().getTime();
+
+    var deltas = [];
+
     function loop() {
 
         //http://stackoverflow.com/questions/1664785/html5-canvas-resize-to-fit-window
@@ -26,6 +30,24 @@
 
         ctx.fillStyle = 'black';
         ctx.fillRect( x, 100, 20, 20 );
+
+        var currentTime = new Date().getTime();
+        deltas.push( currentTime - lastTime );
+        if ( deltas.length > 60 ) {
+            deltas.splice( 0, 1 );
+        }
+        var sum = 0.0;
+        for ( var i = 0; i < deltas.length; i++ ) {
+            sum += deltas[i];
+        }
+        var delta = sum / deltas.length;
+        var deltaSeconds = delta / 1000;
+        var FPS = 1.0 / deltaSeconds;
+        ctx.fillStyle = 'black';
+
+        ctx.fillText( "FPS: " + FPS.toPrecision( 2 ), 100, 100 );
+
+        lastTime = currentTime;
 
         ++x;
         requestAnimationFrame( loop );
