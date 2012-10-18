31cbec4a584ff2f269fcaebb1ce87d070dade117
sbreed <sbreed@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-10-18 21:14:26 +0000

    completed conversion of initial demo to require.js modules

--- trunk/simulations-html5/simulations/build-an-atom/src/app/point2d.js
+++ trunk/simulations-html5/simulations/build-an-atom/src/app/point2d.js
@@ -5,27 +5,27 @@ define([], function(){
   //-----------------------------------------------------------------------------
 
   function Point2D( x, y ) {
-      // Instance Fields or Data Members
-      this.x = x;
-      this.y = y;
+    // Instance Fields or Data Members
+    this.x = x;
+    this.y = y;
   }
 
   Point2D.prototype.toString = function () {
-      return this.x + ", " + this.y;
-  }
+    return this.x + ", " + this.y;
+  };
 
   Point2D.prototype.setComponents = function ( x, y ) {
-      this.x = x;
-      this.y = y;
-  }
+    this.x = x;
+    this.y = y;
+  };
 
   Point2D.prototype.set = function ( point2D ) {
-      this.setComponents( point2D.x, point2D.y );
-  }
+    this.setComponents( point2D.x, point2D.y );
+  };
 
   Point2D.prototype.equals = function ( point2D ) {
-      return (point2D.x == this.x) && (point2D.y == this.y);
-  }
+    return (point2D.x == this.x) && (point2D.y == this.y);
+  };
 
   return Point2D;
 
--- trunk/simulations-html5/simulations/build-an-atom/src/app/proton.js
+++ trunk/simulations-html5/simulations/build-an-atom/src/app/proton.js
@@ -2,12 +2,12 @@ define([
   'nucleon'
 ], function( Nucleon ){
 
-function Proton() {
-  Nucleon.call( this, "gray" );
-}
+  function Proton() {
+    Nucleon.call( this, "red" );
+  }
 
-Proton.prototype = Nucleon.prototype;
+  Proton.prototype = Nucleon.prototype;
 
-return Proton;
+  return Proton;
 
 });
