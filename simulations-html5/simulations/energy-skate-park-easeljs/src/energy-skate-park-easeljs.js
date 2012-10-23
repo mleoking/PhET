e73a8ed8ed70cad892d4c93a19861ae9978981f8
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-10-23 07:40:48 +0000

    added jquery and improved buttons

--- simulations-html5/simulations/energy-skate-park-easeljs/src/energy-skate-park-easeljs.js
+++ simulations-html5/simulations/energy-skate-park-easeljs/src/energy-skate-park-easeljs.js
@@ -30,7 +30,26 @@
         }
     }
 
+    function WebSocketTest() {
+        if ( "WebSocket" in window ) {
+            // Let us open a web socket
+            var ws = new WebSocket( "ws://localhost:8887/echo" );
+            ws.onmessage = function ( evt ) { document.location.reload( true ); };
+            ws.onclose = function () { };
+        }
+        else {
+            // The browser doesn't support WebSocket
+            alert( "WebSocket NOT supported by your Browser!" );
+        }
+    }
+
     function run( images ) {
+
+        WebSocketTest();
+
+        $( "#myResetAllButton" ).click( function () {
+            document.location.reload( true );
+        } );
 //
 //        var canvas = document.createElement( "canvas" );
 //
