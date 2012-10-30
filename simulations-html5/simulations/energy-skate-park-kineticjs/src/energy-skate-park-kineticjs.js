5d0c723ac7480e73f14983d3b3147dbe99b0dcd5
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-10-30 18:40:02 +0000

    moved more math out of the main file

--- simulations-html5/simulations/energy-skate-park-kineticjs/src/energy-skate-park-kineticjs.js
+++ simulations-html5/simulations/energy-skate-park-kineticjs/src/energy-skate-park-kineticjs.js
@@ -460,19 +460,6 @@
         requestAnimationFrame( loop );
     }
 
-    var dot = function ( a, b ) { return a.x * b.x + a.y * b.y; };
-
-    var sub = function ( a, b ) { return {x:(a.x - b.x), y:(a.y - b.y)}; };
-
-    //http://wiki.processing.org/w/Find_which_side_of_a_line_a_point_is_on
-    var linePointPosition2DVector = function ( p1, p2, p3 ) {
-
-        var diff = sub( p2, p1 );
-        var perp = {x:-diff.y, y:diff.x};
-        var pp = sub( p3, p1 );
-        return dot( pp, perp );
-    };
-
     // Only executed our code once the DOM is ready.
     window.onload = function () {
         preloadImages( ["resources/skater.png", "resources/house.png", "resources/mountains.png"] ).done( run )
