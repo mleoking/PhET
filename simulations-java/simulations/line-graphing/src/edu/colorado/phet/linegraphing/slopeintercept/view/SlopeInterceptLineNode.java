75cd540db792748066116f4cc3b0a9f87f5f071e
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-10-30 01:42:43 +0000

    doc

--- simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/slopeintercept/view/SlopeInterceptLineNode.java
+++ simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/slopeintercept/view/SlopeInterceptLineNode.java
@@ -19,7 +19,7 @@ public class SlopeInterceptLineNode extends LineNode {
 
     public SlopeInterceptLineNode( Line line, Graph graph, ModelViewTransform mvt ) {
         super( line, graph, mvt );
-        assert ( line.x1 == 0 ); // line is in slope-intercept form
+        assert ( line.x1 == 0 ); // point is on the y-axis
     }
 
     // Creates the line's equation in slope-intercept form.
