0c3270d843314731fb40dc02b02c857687491a06
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-10-30 22:45:07 +0000

    show both unsimplified and simplified value on RHS of slope equation

--- simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/slope/view/SlopeEquationNode.java
+++ simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/slope/view/SlopeEquationNode.java
@@ -4,6 +4,7 @@ package edu.colorado.phet.linegraphing.slope.view;
 import java.awt.Color;
 import java.text.NumberFormat;
 
+import edu.colorado.phet.common.phetcommon.math.MathUtil;
 import edu.colorado.phet.common.phetcommon.model.property.Property;
 import edu.colorado.phet.common.phetcommon.util.DefaultDecimalFormat;
 import edu.colorado.phet.common.phetcommon.util.DoubleRange;
@@ -17,6 +18,7 @@ import edu.colorado.phet.linegraphing.common.LGResources.Strings;
 import edu.colorado.phet.linegraphing.common.LGSimSharing.UserComponents;
 import edu.colorado.phet.linegraphing.common.model.Line;
 import edu.colorado.phet.linegraphing.common.view.EquationNode;
+import edu.colorado.phet.linegraphing.common.view.MinusNode;
 import edu.colorado.phet.linegraphing.common.view.SpinnerNode;
 import edu.colorado.phet.linegraphing.common.view.SpinnerStateIndicator.PointColors;
 import edu.colorado.phet.linegraphing.common.view.UndefinedSlopeIndicator;
@@ -26,7 +28,7 @@ import edu.umd.cs.piccolo.nodes.PText;
 
 /**
  * Renderer for slope equations, with interactive points (x1, y1, x2, y2).
- * Form is: m = y2 - y1 / x2 - x1  = rise/run
+ * Form is: m = y2 - y1 / x2 - x1  = unsimplified value = simplified value
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
@@ -54,78 +56,172 @@ public class SlopeEquationNode extends EquationNode {
         // Determine the max width of the rise and run spinners.
         double maxSlopeSpinnerWidth = computeMaxSlopeSpinnerWidth( xRange, yRange, interactiveFont, FORMAT );
 
-        // Nodes
+        // Nodes that could appear is all possible ways to write the equation
         // m =
         PNode mNode = new PhetPText( Strings.SYMBOL_SLOPE, staticFont, staticColor );
-        PNode leftEqualsNode = new PhetPText( "=", staticFont, staticColor );
+        PNode interactiveEqualsNode = new PhetPText( "=", staticFont, staticColor );
         // y2 - y2
         PNode y2Node = new ZeroOffsetNode( new SpinnerNode( UserComponents.y2Spinner, y2, xRange, new PointColors(), interactiveFont, FORMAT ) );
         PNode numeratorOperatorNode = new PhetPText( "-", staticFont, staticColor );
         PNode y1Node = new ZeroOffsetNode( new SpinnerNode( UserComponents.y1Spinner, y1, xRange, new PointColors(), interactiveFont, FORMAT ) );
-        // left fraction line
-        PPath leftLineNode = new PhetPPath( createFractionLineShape( 10 ), staticColor, null, null ); // correct length will be set later
+        // fraction line
+        PPath interactiveFractionLineNode = new PhetPPath( createFractionLineShape( 10 ), staticColor, null, null ); // correct length will be set later
         // x2 - x1
         PNode x2Node = new ZeroOffsetNode( new SpinnerNode( UserComponents.x2Spinner, x2, xRange, new PointColors(), interactiveFont, FORMAT ) );
         PNode denominatorOperatorNode = new PhetPText( "-", staticFont, staticColor );
         PNode x1Node = new ZeroOffsetNode( new SpinnerNode( UserComponents.x1Spinner, x1, xRange, new PointColors(), interactiveFont, FORMAT ) );
-        // = rise / run
-        PNode rightEqualsNode = new PhetPText( "=", staticFont, staticColor );
-        final PText riseNode = new PhetPText( FORMAT.format( interactiveLine.get().rise ), staticFont, staticColor );
-        final PText runNode = new PhetPText( FORMAT.format( interactiveLine.get().run ), staticFont, staticColor );
-        final PPath rightLineNode = new PhetPPath( createFractionLineShape( 10 ), staticColor, null, null ); // correct length will be set later
+        // = unsimplified value
+        final PNode unsimplifiedEqualsNode = new PhetPText( "=", staticFont, staticColor );
+        final PText unsimplifiedRiseNode = new PhetPText( "?", staticFont, staticColor );
+        final PText unsimplifiedRunNode = new PhetPText( "?", staticFont, staticColor );
+        final PPath unsimplifiedFractionLineNode = new PhetPPath( createFractionLineShape( 10 ), staticColor, null, null ); // correct length will be set later
+        // = simplified value
+        final PNode simplifiedEqualsNode = new PhetPText( "=", staticFont, staticColor );
+        final PNode simplifiedMinusSign = new MinusNode( signLineSize, staticColor );
+        final PText simplifiedRiseNode = new PhetPText( "?", staticFont, staticColor );
+        final PText simplifiedRunNode = new PhetPText( "?", staticFont, staticColor );
+        final PPath simplifiedFractionLineNode = new PhetPPath( createFractionLineShape( 10 ), staticColor, null, null ); // correct length will be set later
+        final PText undefinedSlopeNode = new PhetPText( Strings.UNDEFINED, staticFont, staticColor );
 
         // rendering order
         {
+            // m =
             addChild( mNode );
-            addChild( leftEqualsNode );
+            addChild( interactiveEqualsNode );
+            // y2 - y1
             addChild( y2Node );
             addChild( numeratorOperatorNode );
             addChild( y1Node );
-            addChild( leftLineNode );
+            // fraction line
+            addChild( interactiveFractionLineNode );
+            // x2 - x1
             addChild( x2Node );
             addChild( denominatorOperatorNode );
             addChild( x1Node );
-            addChild( rightEqualsNode );
-            addChild( riseNode );
-            addChild( rightLineNode );
-            addChild( runNode );
+            // = unsimplified value
+            addChild( unsimplifiedEqualsNode );
+            addChild( unsimplifiedRiseNode );
+            addChild( unsimplifiedFractionLineNode );
+            addChild( unsimplifiedRunNode );
+            // = simplified value
+            addChild( simplifiedEqualsNode );
+            // other nodes are added as needed by dynamic layout
         }
 
-        // layout
+        // static layout
         {
             // m =
             mNode.setOffset( 0, 0 );
-            leftEqualsNode.setOffset( mNode.getFullBoundsReference().getMaxX() + relationalOperatorXSpacing, mNode.getYOffset() );
+            interactiveEqualsNode.setOffset( mNode.getFullBoundsReference().getMaxX() + relationalOperatorXSpacing, mNode.getYOffset() );
             // fraction line
-            leftLineNode.setOffset( leftEqualsNode.getFullBoundsReference().getMaxX() + relationalOperatorXSpacing,
-                                    leftEqualsNode.getFullBoundsReference().getCenterY() + fractionLineYFudgeFactor );
+            interactiveFractionLineNode.setOffset( interactiveEqualsNode.getFullBoundsReference().getMaxX() + relationalOperatorXSpacing,
+                                                   interactiveEqualsNode.getFullBoundsReference().getCenterY() + fractionLineYFudgeFactor );
             // y2 - y1
-            y2Node.setOffset( leftLineNode.getXOffset(),
-                              leftLineNode.getFullBoundsReference().getMinY() - y2Node.getFullBoundsReference().getHeight() - spinnersYSpacing );
+            y2Node.setOffset( interactiveFractionLineNode.getXOffset(),
+                              interactiveFractionLineNode.getFullBoundsReference().getMinY() - y2Node.getFullBoundsReference().getHeight() - spinnersYSpacing );
             numeratorOperatorNode.setOffset( y2Node.getFullBoundsReference().getMaxX() + operatorXSpacing,
                                              y2Node.getFullBoundsReference().getCenterY() - ( numeratorOperatorNode.getFullBoundsReference().getHeight() / 2 ) );
             y1Node.setOffset( numeratorOperatorNode.getFullBoundsReference().getMaxX() + operatorXSpacing,
                               y2Node.getYOffset() );
             // fix fraction line length
             final double leftLineLength = y1Node.getFullBoundsReference().getMaxX() - y2Node.getFullBoundsReference().getMinX();
-            leftLineNode.setPathTo( createFractionLineShape( leftLineLength ) );
+            interactiveFractionLineNode.setPathTo( createFractionLineShape( leftLineLength ) );
             // x2 - x1
             x2Node.setOffset( y2Node.getXOffset(),
-                              leftLineNode.getFullBoundsReference().getMaxY() + spinnersYSpacing );
+                              interactiveFractionLineNode.getFullBoundsReference().getMaxY() + spinnersYSpacing );
             denominatorOperatorNode.setOffset( x2Node.getFullBoundsReference().getMaxX() + operatorXSpacing,
                                                x2Node.getFullBoundsReference().getCenterY() - ( denominatorOperatorNode.getFullBoundsReference().getHeight() / 2 ) );
             x1Node.setOffset( denominatorOperatorNode.getFullBoundsReference().getMaxX() + operatorXSpacing,
                               x2Node.getYOffset() );
-            // = rise/run
-            rightEqualsNode.setOffset( leftLineNode.getFullBoundsReference().getMaxX() + relationalOperatorXSpacing,
-                                       leftEqualsNode.getYOffset() );
-            rightLineNode.setOffset( rightEqualsNode.getFullBoundsReference().getMaxX() + relationalOperatorXSpacing,
-                                     leftLineNode.getYOffset() );
-            riseNode.setOffset( rightLineNode.getXOffset(), // x offset will be set correctly later
-                                rightLineNode.getFullBoundsReference().getMinY() - riseNode.getFullBoundsReference().getHeight() - ySpacing );
-            runNode.setOffset( rightLineNode.getXOffset(), // x offset will be set correctly later
-                               rightLineNode.getFullBoundsReference().getMaxY() + ySpacing );
+            // = unsimplified value
+            unsimplifiedEqualsNode.setOffset( interactiveFractionLineNode.getFullBoundsReference().getMaxX() + relationalOperatorXSpacing,
+                                              interactiveEqualsNode.getYOffset() );
+            unsimplifiedFractionLineNode.setOffset( unsimplifiedEqualsNode.getFullBoundsReference().getMaxX() + relationalOperatorXSpacing,
+                                                    interactiveFractionLineNode.getYOffset() );
+            // all other layout is done dynamically, in updateLayout
         }
+
+        // dynamic layout
+        final VoidFunction1<Line> updateLayout = new VoidFunction1<Line>() {
+            public void apply( Line line ) {
+
+                // Unsimplified: adjust the fraction line length, center the rise and run values
+                {
+                    unsimplifiedFractionLineNode.setPathTo( createFractionLineShape( Math.max( unsimplifiedRiseNode.getFullBoundsReference().getWidth(), unsimplifiedRunNode.getFullBoundsReference().getWidth() ) ) );
+                    unsimplifiedRiseNode.setOffset( unsimplifiedFractionLineNode.getFullBoundsReference().getCenterX() - ( unsimplifiedRiseNode.getFullBoundsReference().getWidth() / 2 ),
+                                                    unsimplifiedFractionLineNode.getFullBoundsReference().getMinY() - unsimplifiedRiseNode.getFullBoundsReference().getHeight() - ySpacing );
+                    unsimplifiedRunNode.setOffset( unsimplifiedFractionLineNode.getFullBoundsReference().getCenterX() - ( unsimplifiedRunNode.getFullBoundsReference().getWidth() / 2 ),
+                                                   unsimplifiedFractionLineNode.getFullBoundsReference().getMaxY() + ySpacing );
+                }
+
+                // Simplified
+                {
+                    simplifiedEqualsNode.setOffset( unsimplifiedFractionLineNode.getFullBoundsReference().getMaxX() + relationalOperatorXSpacing,
+                                                    unsimplifiedEqualsNode.getYOffset() );
+
+                    // remove all related nodes, then we'll add the ones that are relevant
+                    removeChild( simplifiedMinusSign );
+                    removeChild( simplifiedRiseNode );
+                    removeChild( simplifiedRunNode );
+                    removeChild( simplifiedFractionLineNode );
+                    removeChild( undefinedSlopeNode );
+
+                    if ( line.undefinedSlope() ) {
+                        // "undefined"
+                        addChild( undefinedSlopeNode );
+                        undefinedSlopeNode.setOffset( simplifiedEqualsNode.getFullBoundsReference().getMaxX() + relationalOperatorXSpacing,
+                                                      simplifiedEqualsNode.getY() );
+                    }
+                    else if ( line.getSlope() == 0 ) {
+                        // 0
+                        addChild( simplifiedRiseNode );
+                        simplifiedRiseNode.setText( "0" );
+                        simplifiedRiseNode.setOffset( simplifiedEqualsNode.getFullBoundsReference().getMaxX() + relationalOperatorXSpacing,
+                                                      simplifiedEqualsNode.getY() );
+                    }
+                    else {
+                        final double nextXOffset;
+                        if ( line.getSlope() < 0 ) {
+                            // minus sign
+                            addChild( simplifiedMinusSign );
+                            simplifiedMinusSign.setOffset( simplifiedEqualsNode.getFullBoundsReference().getMaxX() + relationalOperatorXSpacing,
+                                                           simplifiedEqualsNode.getFullBoundsReference().getCenterY() - ( simplifiedMinusSign.getFullBoundsReference().getHeight() / 2 ) + slopeSignYFudgeFactor + slopeSignYOffset );
+                            nextXOffset = simplifiedMinusSign.getFullBoundsReference().getMaxX() + operatorXSpacing;
+                        }
+                        else {
+                            nextXOffset = simplifiedEqualsNode.getFullBoundsReference().getMaxX() + relationalOperatorXSpacing;
+                        }
+
+                        if ( MathUtil.isInteger( line.getSlope() ) ) {
+                            // integer
+                            addChild( simplifiedRiseNode );
+                            simplifiedRiseNode.setText( FORMAT.format( Math.abs( line.getSlope() ) ) );
+                            simplifiedRiseNode.setOffset( nextXOffset, simplifiedEqualsNode.getYOffset() );
+                        }
+                        else {
+                            // fraction
+                            addChild( simplifiedFractionLineNode );
+                            addChild( simplifiedRiseNode );
+                            addChild( simplifiedRunNode );
+
+                            // set absolute values
+                            simplifiedRiseNode.setText( FORMAT.format( Math.abs( line.getSimplifiedRise() ) ) );
+                            simplifiedRunNode.setText( FORMAT.format( Math.abs( line.getSimplifiedRun() ) ) );
+
+                            // adjust fraction line length
+                            simplifiedFractionLineNode.setPathTo( createFractionLineShape( Math.max( simplifiedRiseNode.getFullBoundsReference().getWidth(), simplifiedRunNode.getFullBoundsReference().getWidth() ) ) );
+
+                            // layout, values horizontally centered
+                            simplifiedFractionLineNode.setOffset( nextXOffset, unsimplifiedFractionLineNode.getYOffset() );
+                            simplifiedRiseNode.setOffset( simplifiedFractionLineNode.getFullBoundsReference().getCenterX() - ( simplifiedRiseNode.getFullBoundsReference().getWidth() / 2 ),
+                                                          simplifiedFractionLineNode.getFullBoundsReference().getMinY() - simplifiedRiseNode.getFullBoundsReference().getHeight() - ySpacing );
+                            simplifiedRunNode.setOffset( simplifiedFractionLineNode.getFullBoundsReference().getCenterX() - ( simplifiedRunNode.getFullBoundsReference().getWidth() / 2 ),
+                                                         simplifiedFractionLineNode.getFullBoundsReference().getMaxY() + ySpacing );
+                        }
+                    }
+                }
+            }
+        };
 
         // sync the model with the controls
         RichSimpleObserver lineUpdater = new RichSimpleObserver() {
@@ -153,15 +249,11 @@ public class SlopeEquationNode extends EquationNode {
                 updatingControls = false;
 
                 // Update the rise & run values
-                riseNode.setText( FORMAT.format( line.rise ) );
-                runNode.setText( FORMAT.format( line.run ) );
+                unsimplifiedRiseNode.setText( FORMAT.format( line.rise ) );
+                unsimplifiedRunNode.setText( FORMAT.format( line.run ) );
 
-                // Adjust the fraction line length and center the rise and run values
-                rightLineNode.setPathTo( createFractionLineShape( Math.max( riseNode.getFullBoundsReference().getWidth(), runNode.getFullBoundsReference().getWidth() ) ) );
-                riseNode.setOffset( rightLineNode.getFullBoundsReference().getCenterX() - ( riseNode.getFullBoundsReference().getWidth() / 2 ),
-                                    riseNode.getYOffset() );
-                runNode.setOffset( rightLineNode.getFullBoundsReference().getCenterX() - ( runNode.getFullBoundsReference().getWidth() / 2 ),
-                                   runNode.getYOffset() );
+                // do layout before adding undefined-slope indicator
+                updateLayout.apply( line );
 
                 // undefined-slope indicator
                 removeChild( undefinedSlopeIndicator );
--- simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/test/TestEquationNodes.java
+++ simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/test/TestEquationNodes.java
@@ -115,7 +115,7 @@ public class TestEquationNodes {
 
         // canvas
         PhetPCanvas canvas = new PhetPCanvas();
-        canvas.setPreferredSize( new Dimension( 1024, 768 ) );
+        canvas.setPreferredSize( new Dimension( 1200, 768 ) );
         canvas.getLayer().addChild( pointSlopeParent );
         canvas.getLayer().addChild( slopeInterceptParent );
         canvas.getLayer().addChild( slopeParent );
