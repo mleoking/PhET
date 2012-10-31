eb76322be8a6a35eed6cf27597ac1895231933d8
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-10-31 21:12:56 +0000

    10/31/12 make return type of LineNode.createEquationNode less specific

--- simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/slopeintercept/view/SlopeInterceptLineNode.java
+++ simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/slopeintercept/view/SlopeInterceptLineNode.java
@@ -7,8 +7,8 @@ import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTra
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
 import edu.colorado.phet.linegraphing.common.model.Graph;
 import edu.colorado.phet.linegraphing.common.model.Line;
-import edu.colorado.phet.linegraphing.common.view.EquationNode;
 import edu.colorado.phet.linegraphing.common.view.LineNode;
+import edu.umd.cs.piccolo.PNode;
 
 /**
  * Visual representation of a line in simplified slope-intercept form (y = mx + b).
@@ -23,7 +23,7 @@ public class SlopeInterceptLineNode extends LineNode {
     }
 
     // Creates the line's equation in slope-intercept form.
-    protected EquationNode createEquationNode( Line line, PhetFont font, Color color ) {
+    protected PNode createEquationNode( Line line, PhetFont font, Color color ) {
         return new SlopeInterceptEquationNode( line, font, color );
     }
 }
