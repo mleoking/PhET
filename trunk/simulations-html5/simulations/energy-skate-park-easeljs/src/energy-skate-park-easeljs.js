403df862a39c0724012d4c77f1bbaca3783a9959
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-11-02 20:18:12 +0000

    added smooth spline

--- trunk/simulations-html5/simulations/energy-skate-park-easeljs/src/energy-skate-park-easeljs.js
+++ trunk/simulations-html5/simulations/energy-skate-park-easeljs/src/energy-skate-park-easeljs.js
@@ -193,14 +193,54 @@ $( function () {
 
         var controlPoints = [a, b, c];
 
+//        var updateSplineTrack = function () {
+//            console.log( "drag" );
+//
+//            //use the same algorithm as in trunk\simulations-java\common\spline\src\edu\colorado\phet\common\spline\CubicSpline2D.java
+//
+//            track.setPoints( myArray );
+//        };
+
+        function getX( point ) {return point.x;}
+
+        function getY( point ) {return point.y;}
+
+
         function createLine() {
             var graphics = new createjs.Graphics();
             var line = new createjs.Shape( graphics );
             line.drawBetweenControlPoints = function () {
                 graphics.clear();
                 graphics.beginStroke( "#000000" ).setStrokeStyle( 20 );
+
+                var pointArray = [];
                 for ( var i = 0; i < controlPoints.length; i++ ) {
-                    var controlPoint = controlPoints[i];
+                    var circleElement = controlPoints[i];
+                    pointArray.push( circleElement.x, circleElement.y );
+                }
+//                track.setPoints( pointArray );
+
+
+                var x = controlPoints.map( getX );
+                var y = controlPoints.map( getY );
+                var s = numeric.linspace( 0, 1, controlPoints.length );
+                var splineX = numeric.spline( s, x );
+                var splineY = numeric.spline( s, y );
+
+                //Use 75 interpolating points because it is smooth enough even for a very large track (experimented with 3 control points only, with more control points may need more samples)
+                var sAll = numeric.linspace( 0, 1, 75 );
+
+                //http://stackoverflow.com/questions/1669190/javascript-min-max-array-values
+                var myArray = [];
+                for ( var i = 0; i < sAll.length; i++ ) {
+                    var b = splineX.at( sAll[i] );
+                    var a = splineY.at( sAll[i] );
+                    myArray.push( {x:b, y:a} );
+                }
+
+
+                for ( var i = 0; i < myArray.length; i++ ) {
+                    var controlPoint = myArray[i];
                     if ( i == 0 ) {
                         graphics.moveTo( controlPoint.x, controlPoint.y );
                     }
