72c505cc23a996549606c876772d1259827423fe
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-11-05 21:12:33 +0000

    factored out spline

--- trunk/simulations-html5/simulations/energy-skate-park-easeljs/src/app/main.js
+++ trunk/simulations-html5/simulations/energy-skate-park-easeljs/src/app/main.js
@@ -5,6 +5,7 @@ require( [
              'skater',
              'control-panel',
              'background',
+             'spline',
              'image!resources/barChartIcon.png',
              'image!resources/gridIcon.png',
              'image!resources/house.png',
@@ -12,7 +13,7 @@ require( [
              'image!resources/pieChartIcon.png',
              'image!resources/skater.png',
              'image!resources/speedIcon.png'
-         ], function ( Geometry, Vector2D, WebsocketRefresh, Skater, ControlPanel, Background, barChartIconImage, gridIconImage, houseImage, mountainsImage, pieChartIconImage, skaterImage, speedIconImage ) {
+         ], function ( Geometry, Vector2D, WebsocketRefresh, Skater, ControlPanel, Background, Spline, barChartIconImage, gridIconImage, houseImage, mountainsImage, pieChartIconImage, skaterImage, speedIconImage ) {
 
     WebsocketRefresh.listenForRefresh();
 
@@ -36,141 +37,10 @@ require( [
     var controlPanel = ControlPanel.createControlPanel( barChartIconImage, pieChartIconImage, gridIconImage, speedIconImage );
     var background = Background.create( houseImage, mountainsImage, groundHeight );
 
-    var splineLayer = new createjs.Container();
-    var line = null;
-
-    function controlPointPressHandler( e ) {
-
-        //Make dragging relative to touch point
-        var relativePressPoint = null;
-        e.onMouseMove = function ( event ) {
-            var transformed = event.target.parent.globalToLocal( event.stageX, event.stageY );
-            if ( relativePressPoint === null ) {
-                relativePressPoint = {x:e.target.x - transformed.x, y:e.target.y - transformed.y};
-            }
-            else {
-                e.target.x = transformed.x + relativePressPoint.x;
-                e.target.y = Math.min( transformed.y + relativePressPoint.y, 768 - groundHeight );
-
-                //add my own fields for layout
-                e.target.centerX = e.target.x;
-                e.target.centerY = e.target.y;
-                line.drawBetweenControlPoints();
-            }
-        }
-    }
-
-    function createControlPoint( x, y ) {
-        var circleGraphics = new createjs.Graphics();
-        circleGraphics.beginFill( createjs.Graphics.getRGB( 0, 0, 255 ) );
-        circleGraphics.drawCircle( 0, 0, 20 );
-        var controlPoint = new createjs.Shape( circleGraphics );
-        controlPoint.onPress = controlPointPressHandler;
-        setCursorHand( controlPoint );
-        //add my own fields for layout
-        controlPoint.x = x;
-        controlPoint.y = y;
-        return controlPoint;
-    }
-
-    var a = createControlPoint( 100, 200 );
-    var b = createControlPoint( 200, 300 );
-    var c = createControlPoint( 300, 250 );
-
-    var controlPoints = [a, b, c];
-
-    function getX( point ) {return point.x;}
-
-    function getY( point ) {return point.y;}
-
-    function createLine() {
-        var graphics = new createjs.Graphics();
-        var line = new createjs.Shape( graphics );
-        line.drawBetweenControlPoints = function () {
-            graphics.clear();
-            graphics.beginStroke( "#000000" ).setStrokeStyle( 20 );
-
-            var pointArray = [];
-            for ( var i = 0; i < controlPoints.length; i++ ) {
-                var circleElement = controlPoints[i];
-                pointArray.push( circleElement.x, circleElement.y );
-            }
-
-            var x = controlPoints.map( getX );
-            var y = controlPoints.map( getY );
-            var s = numeric.linspace( 0, 1, controlPoints.length );
-            var splineX = numeric.spline( s, x );
-            var splineY = numeric.spline( s, y );
-
-            //Use 75 interpolating points because it is smooth enough even for a very large track (experimented with 3 control points only, with more control points may need more samples)
-            var sAll = numeric.linspace( 0, 1, 75 );
-
-            //http://stackoverflow.com/questions/1669190/javascript-min-max-array-values
-            var myArray = [];
-            for ( var i = 0; i < sAll.length; i++ ) {
-                var b = splineX.at( sAll[i] );
-                var a = splineY.at( sAll[i] );
-                myArray.push( {x:b, y:a} );
-            }
-
-            for ( var i = 0; i < myArray.length; i++ ) {
-                var controlPoint = myArray[i];
-                if ( i == 0 ) {
-                    graphics.moveTo( controlPoint.x, controlPoint.y );
-                }
-                else {
-                    graphics.lineTo( controlPoint.x, controlPoint.y );
-                }
-            }
-        };
-        line.drawBetweenControlPoints();
-        setCursorHand( line );
-
-        //Allow dragging the entire line
-        function pressHandler( e ) {
-            line.dragging = true;
-            //Make dragging relative to touch point
-            var previousPoint = null;
-            e.onMouseMove = function ( event ) {
-                var transformed = event.target.parent.globalToLocal( event.stageX, event.stageY );
-                if ( previousPoint === null ) {
-                    previousPoint = {x:transformed.x, y:transformed.y};
-                }
-                else {
-                    var newPoint = {x:transformed.x, y:transformed.y};
-                    var deltaX = newPoint.x - previousPoint.x;
-                    var deltaY = newPoint.y - previousPoint.y;
-
-                    a.x = a.x + deltaX;
-                    a.y = a.y + deltaY;
-
-                    b.x = b.x + deltaX;
-                    b.y = b.y + deltaY;
-
-                    c.x = c.x + deltaX;
-                    c.y = c.y + deltaY;
-
-                    line.drawBetweenControlPoints();
-                    previousPoint = newPoint;
-                }
-            };
-        }
-
-        line.onPress = pressHandler;
-
-
-        return line;
-    }
-
-    line = createLine();
-    splineLayer.addChild( line );
-
-    splineLayer.addChild( a );
-    splineLayer.addChild( b );
-    splineLayer.addChild( c );
-
     //Cache the background into a single image
 //        background.cache( 0, 0, 1024, 768, 1 );
+
+    var splineLayer = Spline.createSplineLayer( groundHeight );
 
     group.addChild( background );
     group.addChild( controlPanel );
@@ -211,7 +81,6 @@ require( [
         }
     }
 
-
     // Event handling
 
     var onResize = function () {
@@ -247,9 +116,9 @@ require( [
 
 
             //Could avoid recomputing the splines in this step if they haven't changed.  But it doesn't show up as high in the profiler.
-            var s = numeric.linspace( 0, 1, controlPoints.length );
-            var splineX = numeric.spline( s, controlPoints.map( getX ) );
-            var splineY = numeric.spline( s, controlPoints.map( getY ) );
+            var s = numeric.linspace( 0, 1, splineLayer.controlPoints.length );
+            var splineX = numeric.spline( s, splineLayer.controlPoints.map( getX ) );
+            var splineY = numeric.spline( s, splineLayer.controlPoints.map( getY ) );
             skater.x = splineX.at( skater.attachmentPoint );
             skater.y = splineY.at( skater.attachmentPoint );
 
@@ -280,13 +149,18 @@ require( [
             }
 
             //don't let the skater cross the spline
-            if ( controlPoints.length > 2 ) {
-                var s = numeric.linspace( 0, 1, controlPoints.length );
+
+            function getX( point ) {return point.x;}
+
+            function getY( point ) {return point.y;}
+
+            if ( splineLayer.controlPoints.length > 2 ) {
+                var s = numeric.linspace( 0, 1, splineLayer.controlPoints.length );
                 var delta = 1E-6;
 
                 function getSides( xvalue, yvalue ) {
-                    var splineX = numeric.spline( s, controlPoints.map( getX ).map( function ( x ) {return x - xvalue } ) );
-                    var splineY = numeric.spline( s, controlPoints.map( getY ).map( function ( y ) {return y - yvalue } ) );
+                    var splineX = numeric.spline( s, splineLayer.controlPoints.map( getX ).map( function ( x ) {return x - xvalue } ) );
+                    var splineY = numeric.spline( s, splineLayer.controlPoints.map( getY ).map( function ( y ) {return y - yvalue } ) );
 
                     var xRoots = splineX.roots();
                     var sides = [];
