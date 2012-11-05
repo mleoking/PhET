29f0bc33f7c902e63381383f54a39079b471b56a
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-11-05 20:34:06 +0000

    factored out background

---
+++ simulations-html5/simulations/energy-skate-park-easeljs/src/app/background.js
@@ -0,0 +1,34 @@
+define( [], function () {
+    var result = {create:function ( images, groundHeight ) {
+        var groundGraphics = new createjs.Graphics();
+        groundGraphics.beginFill( "#64aa64" );
+
+        groundGraphics.rect( 0, 768 - groundHeight, 1024, groundHeight );
+        var ground = new createjs.Shape( groundGraphics );
+
+        var skyGraphics = new createjs.Graphics();
+        skyGraphics.beginLinearGradientFill( ["#7cc7fe", "#ffffff"], [0, 1], 0, 0, 0, 768 - groundHeight );
+        skyGraphics.rect( 0, 0, 1024, 768 - groundHeight );
+        var sky = new createjs.Shape( skyGraphics );
+
+
+        var background = new createjs.Container();
+        background.addChild( sky );
+        background.addChild( ground );
+        var houseImage = images[1];
+        var house = new createjs.Bitmap( houseImage );
+        house.y = 768 - groundHeight - houseImage.height;
+        house.x = 800;
+        var mountainImage = images[2];
+        var mountain = new createjs.Bitmap( mountainImage );
+        var mountainScale = 0.43;
+        mountain.x = -50;
+        mountain.y = 768 - groundHeight - mountainImage.height * mountainScale;
+        mountain.scaleX = mountainScale;
+        mountain.scaleY = mountainScale;
+        background.addChild( mountain );
+        background.addChild( house );
+        return background;
+    }};
+    return result;
+} );
