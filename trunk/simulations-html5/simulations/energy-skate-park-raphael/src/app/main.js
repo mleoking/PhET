478306b7edec440066120251d7b056aa315f9865
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-11-06 05:52:36 +0000

    update skater with animation

--- trunk/simulations-html5/simulations/energy-skate-park-raphael/src/app/main.js
+++ trunk/simulations-html5/simulations/energy-skate-park-raphael/src/app/main.js
@@ -29,6 +29,44 @@ require( [
 
     var ground = paper.rect( 0, 768 - groundHeight, 1024, 768 ).attr( {fill:"#64aa64"} );
 
+    //or another game loop here: http://www.playmycode.com/blog/2011/08/building-a-game-mainloop-in-javascript/
+    //or here: http://jsfiddle.net/Y9uBv/5/
+    //See http://stackoverflow.com/questions/5605588/how-to-use-requestanimationframe
+    var requestAnimationFrame = function () {
+        return (
+                window.requestAnimationFrame ||
+                window.webkitRequestAnimationFrame ||
+                window.mozRequestAnimationFrame ||
+                window.oRequestAnimationFrame ||
+                window.msRequestAnimationFrame ||
+                function ( /* function */ callback ) {
+                    window.setTimeout( callback, 1000 / 60 );
+                }
+                );
+    }();
+
+    var lastTime = Date.now();
+
+    var skaterY = 100;
+
+    function updatePhysics() {
+        var newTime = Date.now();
+        var dt = newTime - lastTime;
+        if ( dt > 0 ) {
+
+            skaterY = skaterY + 0.1 * dt;
+            skater.attr( {y:skaterY} );
+            lastTime = newTime;
+        }
+    }
+
+    function loop() {
+        updatePhysics();
+        requestAnimationFrame( loop );
+    }
+
+    requestAnimationFrame( loop );
+
     function resizePaper() {
         paper.changeSize( window.innerWidth, window.innerHeight, true, false );
     }
