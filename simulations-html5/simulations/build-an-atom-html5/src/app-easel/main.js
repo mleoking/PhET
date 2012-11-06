056f4a7db60fcd7cc28f14704ea0be46e6f656ce
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-11-06 00:32:09 +0000

    Basic drag and drop working.

--- simulations-html5/simulations/build-an-atom-html5/src/app-easel/main.js
+++ simulations-html5/simulations/build-an-atom-html5/src/app-easel/main.js
@@ -7,8 +7,32 @@ require( [
     var canvas = $( '#canvas' );
     var stage = new Easel.Stage( canvas[0] );
     var shape = new Easel.Shape();
-    shape.graphics.beginFill( 'rgba(255,0,0,1)' ).drawRoundRect( 0, 0, 120, 120, 10 );
+    shape.graphics.beginFill( 'rgba(255,0,0,1)' ).drawCircle( 200, 200, 150 );
     stage.addChild( shape );
     stage.update();
+
+    // Enable touch and mouse events.
+    stage.enableMouseOver( 10 );
+    stage.mouseMoveOutside = true;
+
+    // Handler for mouse events on the shape.
+    shape.onPress = function ( evt ) {
+        var offset = {
+            x:shape.x - evt.stageX,
+            y:shape.y - evt.stageY
+        };
+        evt.onMouseMove = function ( ev ) {
+            shape.x = ev.stageX + offset.x;
+            shape.y = ev.stageY + offset.y;
+            console.log( "Dragged!!!" );
+        };
+        console.log( "Pressed!!!" );
+    };
+
+    Easel.Ticker.addListener( {
+                                  tick:function () {
+                                      stage.update();
+                                  }
+                              } );
 
 } );
