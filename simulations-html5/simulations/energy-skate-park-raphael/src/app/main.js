aa1b6c06e308fed6a600c9ddac8b56f44ee9e101
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-11-06 04:51:04 +0000

    added ground and sky

--- simulations-html5/simulations/energy-skate-park-raphael/src/app/main.js
+++ simulations-html5/simulations/energy-skate-park-raphael/src/app/main.js
@@ -12,6 +12,9 @@ require( [
     //See sample code here: http://raphaeljs.com/touches.html
     var paper = new ScaleRaphael( "container", 1024, 768 );
     paper.overallScaleFactor = 1.0;
+    var groundHeight = 116;
+//    var sky = paper.rect( 0, 0, 1024, 768 - groundHeight ).attr( {fill:"#64aa64",stroke:null} );
+    var sky = paper.rect( 0, 0, 1024, 768 - groundHeight ).attr( {fill:"90-#ffffff:5-#7cc7fe:95", stroke:null} );
     var r = paper.circle( 100, 100, 50 ).attr( {fill:"hsb(0, 1, 1)", stroke:"none", opacity:1} ),
             g = paper.circle( 210, 100, 50 ).attr( {fill:"hsb(.3, 1, 1)", stroke:"none", opacity:1} ),
             b = paper.circle( 320, 100, 50 ).attr( {fill:"hsb(.6, 1, 1)", stroke:"none", opacity:1} ),
@@ -29,6 +32,7 @@ require( [
             };
     paper.set( r, g, b, p ).drag( move, start, up );
 
+    var ground = paper.rect( 0, 768 - groundHeight, 1024, 768 ).attr( {fill:"#64aa64"} );
 
     function resizePaper() {
         paper.changeSize( window.innerWidth, window.innerHeight, true, false );
