64b23e3a5e357ed00f6098d36b4ce62130897a61
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-11-08 20:36:02 +0000

    added "X"

--- simulations-html5/simulations/build-an-atom-html5/src/app-easel/main.js
+++ simulations-html5/simulations/build-an-atom-html5/src/app-easel/main.js
@@ -1,19 +1,24 @@
 // Copyright 2002-2012, University of Colorado
-require([
-  'underscore',
-  'easel',
-  'model/particle2',
-  'view/particleView'
-], function ( _, Easel, Particle2, ParticleView ) {
+require( [
+             'underscore',
+             'easel',
+             'model/particle2',
+             'view/particleView',
+             'view/atom-view'
+         ], function ( _, Easel, Particle2, ParticleView, AtomView ) {
 
     var atomConstructionCanvas = $( '#atom-construction-canvas' );
     var atomStage = new Easel.Stage( atomConstructionCanvas[0] );
 
-    atomStage.addChild( ParticleView.createParticleView( new Particle2(0, 0, "red", 20, "proton")));
-    atomStage.addChild( ParticleView.createParticleView( new Particle2(50, 50, "gray", 20, "neutron")));
+    atomStage.addChild( ParticleView.createParticleView( new Particle2( 0, 0, "red", 20, "proton" ) ) );
+    atomStage.addChild( ParticleView.createParticleView( new Particle2( 50, 50, "gray", 20, "neutron" ) ) );
+    var atomView = new AtomView();
+    atomView.x = 100;
+    atomView.y = 100;
+    atomStage.addChild( atomView );
 
     atomStage.update();
-    console.log("main init");
+    console.log( "main init" );
 
     //Get rid of text cursor when dragging on the canvas, see http://stackoverflow.com/questions/2659999/html5-canvas-hand-cursor-problems
     atomConstructionCanvas[0].onselectstart = function () { return false; }; // IE
---
+++ simulations-html5/simulations/build-an-atom-html5/src/app-easel/view/atom-view.js
@@ -0,0 +1,21 @@
+// Copyright 2002-2012, University of Colorado
+define( [
+            'easel'
+        ], function ( Easel ) {
+
+    var AtomView = function ( label, color ) {
+        this.initialize( label, color );
+    };
+    var p = AtomView.prototype = new createjs.Container(); // inherit from Container
+
+    p.Container_initialize = p.initialize;
+    p.initialize = function ( label, color ) {
+        this.Container_initialize();
+        console.log( "hello" );
+        var x = new Easel.Shape();
+        x.graphics.beginStroke( "orange" ).setStrokeStyle( 5 ).moveTo( -10, -10 ).lineTo( 10, 10 ).moveTo( -10, 10 ).lineTo( 10, -10 ).endStroke();
+        this.addChild( x );
+    };
+
+    return AtomView;
+} );
