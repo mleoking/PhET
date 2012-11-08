b4acdee50bffa7a7c69ddf3a7196a99aa318019f
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-11-08 19:24:51 +0000

    Added a new, alternative way of representing particles.

---
+++ simulations-html5/simulations/build-an-atom-html5/src/app-easel/model/particle2.js
@@ -0,0 +1,14 @@
+// Copyright 2002-2012, University of Colorado
+define([
+], function( ){
+
+  function Particle2( xPos, yPos, color, radius, type ){
+      this.xPos = xPos;
+      this.yPos = yPos;
+      this.color = color;
+      this.radius = radius;
+      this.type = type;
+  }
+
+  return Particle2;
+});
