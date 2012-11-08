8a8512a52c698c2965f4fb25e0bd4953a19ff590
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-11-08 19:32:12 +0000

    Added dragging.

--- trunk/simulations-html5/simulations/build-an-atom-html5/src/app-easel/view/particleView.js
+++ trunk/simulations-html5/simulations/build-an-atom-html5/src/app-easel/view/particleView.js
@@ -6,13 +6,31 @@ define( [
     function showPointer( mouseEvent ) { document.body.style.cursor = "pointer"; }
     function showDefault( mouseEvent ) { document.body.style.cursor = "default"; }
 
+    function pressHandler( e ) {
+        //Make dragging relative to touch point
+        var relativePressPoint = null;
+        e.onMouseMove = function ( event ) {
+            var transformed = event.target.parent.globalToLocal( event.stageX, event.stageY );
+            if ( relativePressPoint === null ) {
+                relativePressPoint = {x:e.target.x - transformed.x, y:e.target.y - transformed.y};
+            }
+            else {
+                e.target.x = transformed.x + relativePressPoint.x;
+                e.target.y = transformed.y + relativePressPoint.y;
+            }
+        };
+    }
+
     function createParticleView( particle ) {
         var particleView = new Easel.Shape();
         particleView.graphics.beginFill( "red" ).drawCircle( particle.xPos, particle.yPos, particle.radius ).endFill();
         particleView.onMouseOver = showPointer;
         particleView.onMouseOut = showDefault;
+        particleView.onPress = pressHandler;
         return particleView;
     }
+
+
 
     var ParticleView = {};
     ParticleView.createParticleView = createParticleView;
