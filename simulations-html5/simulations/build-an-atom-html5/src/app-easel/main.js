e148c64dc106e0d34b8e021a308d18e0cdae1b93
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-11-08 19:43:52 +0000

    Faster frame rate, better cursor handling.

--- simulations-html5/simulations/build-an-atom-html5/src/app-easel/main.js
+++ simulations-html5/simulations/build-an-atom-html5/src/app-easel/main.js
@@ -14,6 +14,13 @@ require([
     atomStage.update();
     console.log("main init");
 
+    //Get rid of text cursor when dragging on the canvas, see http://stackoverflow.com/questions/2659999/html5-canvas-hand-cursor-problems
+    atomConstructionCanvas[0].onselectstart = function () { return false; }; // IE
+    atomConstructionCanvas[0].onmousedown = function () { return false; }; // Mozilla
+
+    // Set the frame rate.
+    Easel.Ticker.setFPS( 60 );
+
     // Enable touch and mouse events.
     atomStage.enableMouseOver( 10 );
     atomStage.mouseMoveOutside = true;
