70ce51d600a91b92f6352900c4b002163c83bdfb
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-11-09 16:23:07 +0000

    fixed self reference

--- simulations-html5/simulations/energy-skate-park-easeljs-jquerymobile/src/app/view/pie-chart.js
+++ simulations-html5/simulations/energy-skate-park-easeljs-jquerymobile/src/app/view/pie-chart.js
@@ -7,16 +7,15 @@ define( ["skater"], function ( Skater ) {
     p.Container_initialize = p.initialize;
     p.initialize = function ( skater ) {
         this.Container_initialize();
+        this.skater = skater;
         var shape = new createjs.Shape();
         shape.graphics.beginStroke( "orange" ).setStrokeStyle( 5 ).moveTo( -10, -10 ).lineTo( 10, 10 ).moveTo( -10, 10 ).lineTo( 10, -10 ).endStroke();
         this.addChild( shape );
-        this.container = this;
-        this.skater = skater;
     };
 
     p.tick = function () {
-        this.container.x = this.skater.x;
-        this.container.y = this.skater.y;
+        this.x = this.skater.x;
+        this.y = this.skater.y;
     };
 
     return PieChart;
