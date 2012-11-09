d7b415537e7ae78be4e2da524e3725e46e5ca612
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-11-09 16:26:15 +0000

    fixed init

--- simulations-html5/simulations/energy-skate-park-easeljs-jquerymobile/src/app/view/pie-chart.js
+++ simulations-html5/simulations/energy-skate-park-easeljs-jquerymobile/src/app/view/pie-chart.js
@@ -5,12 +5,14 @@ define( ["skater"], function ( Skater ) {
     var p = PieChart.prototype = new createjs.Container(); // inherit from Container
 
     p.Container_initialize = p.initialize;
+
     p.initialize = function ( skater ) {
         this.Container_initialize();
         this.skater = skater;
         var shape = new createjs.Shape();
         shape.graphics.beginStroke( "orange" ).setStrokeStyle( 5 ).moveTo( -10, -10 ).lineTo( 10, 10 ).moveTo( -10, 10 ).lineTo( 10, -10 ).endStroke();
         this.addChild( shape );
+//        this.tick();
     };
 
     p.tick = function () {
