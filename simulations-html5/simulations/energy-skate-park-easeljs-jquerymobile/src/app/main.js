922f703a855b55fe7519bcab4787cb5bfda53a54
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-11-09 04:55:27 +0000

    fixed comment

--- simulations-html5/simulations/energy-skate-park-easeljs-jquerymobile/src/app/main.js
+++ simulations-html5/simulations/energy-skate-park-easeljs-jquerymobile/src/app/main.js
@@ -59,30 +59,30 @@ require( [
         }
     }
 
-    http://stackoverflow.com/questions/4009524/change-button-text-jquery-mobile
-            (function ( $ ) {
-                /*
-                 * Changes the displayed text for a jquery mobile button.
-                 * Encapsulates the idiosyncracies of how jquery re-arranges the DOM
-                 * to display a button for either an <a> link or <input type="button">
-                 */
-                $.fn.changeButtonText = function ( newText ) {
-                    return this.each( function () {
-                        $this = $( this );
-                        if ( $this.is( 'a' ) ) {
-                            $( 'span.ui-btn-text', $this ).text( newText );
-                            return;
-                        }
-                        if ( $this.is( 'input' ) ) {
-                            $this.val( newText );
-                            // go up the tree
-                            var ctx = $this.closest( '.ui-btn' );
-                            $( 'span.ui-btn-text', ctx ).text( newText );
-                            return;
-                        }
-                    } );
-                };
-            })( jQuery );
+    //http://stackoverflow.com/questions/4009524/change-button-text-jquery-mobile
+    (function ( $ ) {
+        /*
+         * Changes the displayed text for a jquery mobile button.
+         * Encapsulates the idiosyncracies of how jquery re-arranges the DOM
+         * to display a button for either an <a> link or <input type="button">
+         */
+        $.fn.changeButtonText = function ( newText ) {
+            return this.each( function () {
+                $this = $( this );
+                if ( $this.is( 'a' ) ) {
+                    $( 'span.ui-btn-text', $this ).text( newText );
+                    return;
+                }
+                if ( $this.is( 'input' ) ) {
+                    $this.val( newText );
+                    // go up the tree
+                    var ctx = $this.closest( '.ui-btn' );
+                    $( 'span.ui-btn-text', ctx ).text( newText );
+                    return;
+                }
+            } );
+        };
+    })( jQuery );
 
     $( '#barGraphLabel' ).find( '> .ui-btn-inner' ).append( '<img class="alignRightPlease" id="barChartIconImage" src="resources/barChartIcon.png" />' );
     $( '#pieChartLabel' ).find( '> .ui-btn-inner' ).append( '<img class="alignRightPlease" id="pieChartIconImage" src="resources/pieChartIcon.png" />' );
