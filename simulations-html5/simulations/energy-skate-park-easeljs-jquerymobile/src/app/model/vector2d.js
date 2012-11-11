261563fec91391b1022429d77ce65c3fd00a7281
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-11-11 03:59:09 +0000

    added speedometer

--- simulations-html5/simulations/energy-skate-park-easeljs-jquerymobile/src/app/model/vector2d.js
+++ simulations-html5/simulations/energy-skate-park-easeljs-jquerymobile/src/app/model/vector2d.js
@@ -1,13 +1,15 @@
 define( [], function () {
 
-    function Vector2D( _x, _y ) {
-        var vector = {x: _x || 0, y: _y || 0};
+    function Vector2D( x, y ) {
+        var vector = {x: x || 0, y: y || 0};
         vector.plus = function ( a, b ) { return Vector2D( vector.x + a, vector.y + b ); };
         vector.times = function ( scale ) {return Vector2D( vector.x * scale, vector.y * scale );};
         vector.magnitude = function () {return Math.sqrt( vector.magnitudeSquared() )};
         vector.magnitudeSquared = function () {return vector.x * vector.x + vector.y * vector.y };
         return vector;
     }
+
+    Vector2D.fromAngle = function ( angle ) { return new Vector2D( Math.cos( angle ), Math.sin( angle ) ); }
 
     return Vector2D;
 } );
