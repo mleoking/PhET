c0642f982dc5f8560fef9ed8d40527eca26bfbe1
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-11-12 22:20:04 +0000

    removed i18n directory

--- trunk/simulations-html5/simulations/energy-skate-park-easeljs-jquerymobile/src/app/nls/colors.js
+++
@@ -1,8 +0,0 @@
-define( {
-            "root": {
-                "red": "red",
-                "blue": "blue",
-                "green": "green"
-            },
-            "fr-fr": true
-        } );
