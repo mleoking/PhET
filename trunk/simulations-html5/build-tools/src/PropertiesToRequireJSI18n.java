4e77f5d2b745b96a9941d3a57af457afc7d34b4b
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-11-12 22:02:45 +0000

    Added energy skate park translations

--- trunk/simulations-html5/build-tools/src/PropertiesToRequireJSI18n.java
+++ trunk/simulations-html5/build-tools/src/PropertiesToRequireJSI18n.java
@@ -6,6 +6,8 @@ import java.io.FilenameFilter;
 import java.io.IOException;
 import java.util.Properties;
 
+import edu.colorado.phet.common.phetcommon.util.FileUtils;
+
 /**
  * Converts .properties files to the i18n format used by requirejs's i18n plugin.
  * See http://requirejs.org/docs/api.html#i18n
@@ -14,8 +16,14 @@ import java.util.Properties;
  */
 public class PropertiesToRequireJSI18n {
     public static void main( final String[] args ) throws IOException {
-        File root = new File( args[0] );
-        for ( final File file : root.listFiles( new FilenameFilter() {
+
+        //Source is the localization directory, such as "C:\workingcopy\phet\svn-1.7\trunk\simulations-java\simulations\energy-skate-park\data\energy-skate-park\localization"
+        File source = new File( args[0] );
+
+        //Destination is the target nls root, such as "C:\workingcopy\phet\svn-1.7\trunk\simulations-html5\simulations\energy-skate-park-easeljs-jquerymobile\src\app\nls"
+        File destination = new File( args[1] );
+
+        for ( final File file : source.listFiles( new FilenameFilter() {
             @Override public boolean accept( final File dir, final String name ) {
                 return name.endsWith( ".properties" );
             }
@@ -41,6 +49,22 @@ public class PropertiesToRequireJSI18n {
             System.out.println();
             System.out.println();
             System.out.println();
+
+            String a = file.getName().substring( 0, file.getName().indexOf( "-strings" ) );
+            String filename = a + "-strings.js";
+
+            final boolean english = file.getName().indexOf( '_' ) < 0;
+            if ( !english ) {
+                final String tail = file.getName().substring( file.getName().indexOf( "_" ) + 1 );
+                String localeAndCountry = tail.substring( 0, tail.indexOf( '.' ) );
+                File outputDir = new File( destination, localeAndCountry );
+                outputDir.mkdirs();
+                System.out.println( "outputDir = " + outputDir );
+                FileUtils.writeString( new File( outputDir, filename ), output );
+            }
+            else {
+                FileUtils.writeString( new File( destination, filename ), output );
+            }
         }
     }
 }
--- trunk/simulations-html5/simulations/energy-skate-park-easeljs-jquerymobile/src/app/nls/fr-fr/colors.js
+++
@@ -1,6 +0,0 @@
-//Contents of my/nls/fr-fr/colors.js
-define( {
-            "red": "rouge",
-            "blue": "bleu",
-            "green": "vert"
-        } );
