83342865cdd3768c19d7c755d247ae9232f57e1d
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-11-13 20:47:17 +0000

    fill background to fit screen horizontally

--- trunk/simulations-html5/simulations/energy-skate-park-easeljs-jquerymobile/src/app/view/background.js
+++ trunk/simulations-html5/simulations/energy-skate-park-easeljs-jquerymobile/src/app/view/background.js
@@ -7,14 +7,14 @@ define( [
         var groundGraphics = new createjs.Graphics();
         groundGraphics.beginFill( "#64aa64" );
 
-        groundGraphics.rect( 0, 768 - groundHeight, 1024, groundHeight );
+        var extentOutsideOfNominalBounds = 1000;
+        groundGraphics.rect( -extentOutsideOfNominalBounds, 768 - groundHeight, 1024 + 2 * extentOutsideOfNominalBounds, groundHeight );
         var ground = new createjs.Shape( groundGraphics );
 
         var skyGraphics = new createjs.Graphics();
         skyGraphics.beginLinearGradientFill( ["#7cc7fe", "#ffffff"], [0, 1], 0, 0, 0, 768 - groundHeight );
-        skyGraphics.rect( 0, 0, 1024, 768 - groundHeight );
+        skyGraphics.rect( -extentOutsideOfNominalBounds, 0, 1024 + 2 * extentOutsideOfNominalBounds, 768 - groundHeight );
         var sky = new createjs.Shape( skyGraphics );
-
 
         var background = new createjs.Container();
         background.addChild( sky );
