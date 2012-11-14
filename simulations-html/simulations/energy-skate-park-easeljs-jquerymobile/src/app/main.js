9674b0c52bf2d06da99e0aebcea9437d1a416145
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-11-14 21:49:46 +0000

    animate on tab press

--- simulations-html/simulations/energy-skate-park-easeljs-jquerymobile/src/app/main.js
+++ simulations-html/simulations/energy-skate-park-easeljs-jquerymobile/src/app/main.js
@@ -11,16 +11,20 @@ require( ['websocket-refresh',
 //    $( "#tab3" ).append( $( '<canvas id="c" width=400 height=300></canvas>' ) );
 
     $( '#tab1' ).css( 'position', 'absolute' ).css( 'width', '100%' ).css( 'height', '100%' );
+
+
     $( "#theMainBody" ).append( $( '<div data-role="navbar" id="navBar"><ul>' +
                                    '<li><a href="" id="introNavBarButton" class="ui-btn-active ui-state-persist">' + Strings["tab.introduction"] + '</a></li>' +
                                    '<li><a href="" id="frictionNavBarButton">' + Strings["tab.friction"] + '</a></li>' +
                                    '<li><a href="" id="playgroundNavBarButton">' + Strings["tab.trackPlayground"] + '</a></li></ul></div>' ) ).trigger( "create" );
 
     $( "#introNavBarButton" ).click( function () {
-        $( '#tab1' ).animate( {'left': '0px'}, 200, "swing", function () {} );
+        $( '#tab1' ).css( "-webkit-transition", "all 1s ease-in-out" );
+        $( '#tab1' ).css( 'webkit-transform', 'translate(0px,0px)' );
     } );
     $( "#frictionNavBarButton" ).click( function () {
-        $( '#tab1' ).animate( {'left': '-1000'}, 200, "swing", function () {} );
+        $( '#tab1' ).css( "-webkit-transition", "all 1s ease-in-out" );
+        $( '#tab1' ).css( 'webkit-transform', 'translate(-1000px,0px)' );
     } );
     $( "#playgroundNavBarButton" ).click( function () { console.log( "playground" ); } );
     WebsocketRefresh.listenForRefresh();
