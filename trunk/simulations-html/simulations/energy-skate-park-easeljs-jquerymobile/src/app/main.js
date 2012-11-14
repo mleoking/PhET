cd754a971a5be0ce4e11bb5a755fded8e0268220
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-11-14 19:28:43 +0000

    internationalized tabs

--- trunk/simulations-html/simulations/energy-skate-park-easeljs-jquerymobile/src/app/main.js
+++ trunk/simulations-html/simulations/energy-skate-park-easeljs-jquerymobile/src/app/main.js
@@ -6,6 +6,11 @@ require( ['websocket-refresh',
     console.log( Strings["plots.position.meters"] );
     console.log( Strings["plots.energy-vs-position"] );
     console.log( Strings["energy-skate-park.description"] );
+
+    $( "#theMainBody" ).append( $( '<div data-role="navbar" id="navBar"><ul>' +
+                                   '<li><a href="" class="ui-btn-active ui-state-persist">' + Strings["tab.introduction"] + '</a></li>' +
+                                   '<li><a href="" id="frictionNavBarButton">' + Strings["tab.friction"] + '</a></li>' +
+                                   '<li><a href="">' + Strings["tab.trackPlayground"] + '</a></li></ul></div>' ) ).trigger( "create" );
     WebsocketRefresh.listenForRefresh();
     var tab1 = new IntroductionTab();
 } );
