c97b08fe25522243aa9d21e01f26d50d60033e86
paulcantrell <paulcantrell@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-11-14 07:19:55 +0000

    Added experimental subclass support.

--- trunk/simulations-html5/simulations/energy-skate-park-easeljs-jquerymobile/src/app/view/grid.js
+++ trunk/simulations-html5/simulations/energy-skate-park-easeljs-jquerymobile/src/app/view/grid.js
@@ -1,27 +1,23 @@
-define( ["skater"], function ( Skater ) {
-    var Grid = function ( groundY ) {
-        this.initialize( groundY );
-    };
-    var p = Grid.prototype = new createjs.Container(); // inherit from Container
-
-    p.Container_initialize = p.initialize;
-
-    p.initialize = function ( groundY ) {
-        this.Container_initialize();
-        var shape = new createjs.Shape();
-        shape.graphics.beginStroke( 'black' ).setStrokeStyle( 1 );
-        var gridSpacing = 70;
-        for ( var i = 0; i < 10; i++ ) {
-            var y = groundY - gridSpacing * i;
-            shape.graphics.moveTo( 0, y ).lineTo( 1024, y );
+define( ["skater", 'util/extend'], function ( Skater ) {
+    
+    return createjs.Container.extend('Grid', {
+        initialize: function( superInitialize, groundY ) {
+            superInitialize();
+            var shape = new createjs.Shape();
+            shape.graphics.beginStroke( 'black' ).setStrokeStyle( 1 );
+            var gridSpacing = 70;
+            for ( var i = 0; i < 10; i++ ) {
+                var y = groundY - gridSpacing * i;
+                shape.graphics.moveTo( 0, y ).lineTo( 1024, y );
+            }
+            for ( i = 0; i < 15; i++ ) {
+                var x = 0 + i * gridSpacing;
+                shape.graphics.moveTo( x, 0 ).lineTo( x, groundY );
+            }
+            this.addChild( shape );
+    //        shape.cache( 0, 0, 1024, 768 );
         }
-        for ( i = 0; i < 15; i++ ) {
-            var x = 0 + i * gridSpacing;
-            shape.graphics.moveTo( x, 0 ).lineTo( x, groundY );
-        }
-        this.addChild( shape );
-//        shape.cache( 0, 0, 1024, 768 );
-    };
-
-    return Grid;
+    });
+    
 } );
+
