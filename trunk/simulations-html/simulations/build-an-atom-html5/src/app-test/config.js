b27fc8fd2da478877bd1d3724270cc2eec2a400d
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-11-16 17:44:27 +0000

    Set up JS space for testing.

---
+++ trunk/simulations-html/simulations/build-an-atom-html5/src/app-test/config.js
@@ -0,0 +1,26 @@
+console.log("Config file");
+require.config({
+                   deps: ["main"],
+
+                   paths: {
+                       vendor: "../js/vendor",
+                       plugins: "../js/plugins",
+
+                       underscore: "../js/vendor/underscore-min",
+                       kinetic: "../js/vendor/kinetic-v4.0.4"
+
+                   },
+
+                   shim: {
+
+                       underscore: {
+                           exports: "_"
+                       },
+
+                       kinetic: {
+                           exports: "Kinetic"
+                       }
+
+                   }
+               });
+
