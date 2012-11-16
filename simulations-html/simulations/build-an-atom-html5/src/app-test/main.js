50ad7adfcd068c417a16d4eb4f24791b5a55cec6
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-11-16 19:22:32 +0000

    Got next test item working.

--- simulations-html/simulations/build-an-atom-html5/src/app-test/main.js
+++ simulations-html/simulations/build-an-atom-html5/src/app-test/main.js
@@ -1,19 +1,53 @@
-// Copyright 2002-2012, University of Colorado
-
-/**
- * Simple drag and drop on an Easel canvas.
- */
 require( [
              'easel'
          ], function ( Easel ) {
 
-    console.log("Here!");
-    // Create the canvas where atoms will be constructed.
-    var canvas = $( '#canvas' );
+    console.log( "Starting simple easel drag and drop test." );
+
+    // Create the stage.
+    var canvas = $( '#test-canvas' );
     var stage = new Easel.Stage( canvas[0] );
 
     // Create and add a circle.
     var particle = new Easel.Shape();
-    particle.graphics.beginStroke( "black" ).beginFill( particle.color ).setStrokeStyle( 1 ).drawCircle( particle.xPos, particle.yPos, particle.radius ).endFill();
+    particle.graphics.beginStroke( "black" ).beginFill( "green" ).setStrokeStyle( 2 ).drawCircle( 100, 100, 30 ).endFill();
+    console.log( particle );
     stage.addChild( particle );
+
+    // Create and hook up the mouse event handlers.
+    function showPointer( mouseEvent ) {
+        document.body.style.cursor = "pointer";
+    }
+    particle.onMouseOver = showPointer;
+
+    function showDefault( mouseEvent ) {
+        document.body.style.cursor = "default";
+    }
+    particle.onMouseOut = showDefault;
+
+    function pressHandler( e ) {
+        console.log( "Pressed." )
+        //Make dragging relative to touch point
+        var relativePressPoint = null;
+        e.onMouseMove = function ( event ) {
+            var transformed = event.target.parent.globalToLocal( event.stageX, event.stageY );
+            if ( relativePressPoint === null ) {
+                relativePressPoint = {x:e.target.x - transformed.x, y:e.target.y - transformed.y};
+            }
+            else {
+                e.target.x = transformed.x + relativePressPoint.x;
+                e.target.y = transformed.y + relativePressPoint.y;
+            }
+        };
+    }
+    particle.onPress = pressHandler;
+
+    // Enable mouse events.
+    stage.enableMouseOver( 10 );
+    stage.mouseMoveOutside = true;
+
+    // Set the frame rate at which the stage is updated.
+    Easel.Ticker.setFPS( 60 );
+    Easel.Ticker.addListener( stage, true );
+
 } );
