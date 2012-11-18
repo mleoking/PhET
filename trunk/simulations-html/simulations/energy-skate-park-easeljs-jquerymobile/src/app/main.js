8e826576a83b462a29cb9500954d882fbaf45ab8
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-11-18 05:44:01 +0000

    removed unused code

--- trunk/simulations-html/simulations/energy-skate-park-easeljs-jquerymobile/src/app/main.js
+++ trunk/simulations-html/simulations/energy-skate-park-easeljs-jquerymobile/src/app/main.js
@@ -2,14 +2,6 @@ require( ['websocket-refresh',
              'tab',
              'i18n!nls/energy-skate-park-strings'
          ], function ( WebsocketRefresh, Tab, Strings ) {
-    console.log( Strings.large );
-    console.log( Strings["plots.position.meters"] );
-    console.log( Strings["plots.energy-vs-position"] );
-    console.log( Strings["energy-skate-park.description"] );
-    //$( "#tab2" ).append( $( '<canvas id="c2" width=400 height=300></canvas>' ) );
-//    $( "#tab3" ).append( $( '<canvas id="c" width=400 height=300></canvas>' ) );
-
-//    $( '#tab1' ).css( 'width', '100%' ).css( 'height', '100%' );
 
     $( "#theMainBody" ).append( $( '<div data-role="navbar" id="navBar"><ul>' +
                                    '<li><a href="#tab1" id="introNavBarButton" data-transition="none" class="ui-btn-active ui-state-persist">' + Strings["tab.introduction"] + '</a></li>' +
