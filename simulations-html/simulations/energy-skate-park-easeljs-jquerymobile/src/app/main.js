6b362e506d8a71ea0efa2ee5982dbb9b6cf78022
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-11-19 23:12:10 +0000

    removed unused code

--- simulations-html/simulations/energy-skate-park-easeljs-jquerymobile/src/app/main.js
+++ simulations-html/simulations/energy-skate-park-easeljs-jquerymobile/src/app/main.js
@@ -10,16 +10,4 @@ require( ['websocket-refresh',
     new Tab( "tab1", true, massSlider( {id: "tab1"} ) );
     new Tab( "tab2", false, frictionControls( {id: "tab2"} ) );
     new Tab( "tab3", false, frictionControls( {id: "tab3"} ) );
-//    new Tab( "tab4", false );
-//    new Tab( "tab5", false );
-//    new Tab( "tab6", false );
-//    new Tab( "tab7", false );
-//    new Tab( "tab8", false );
-//    new Tab( "tab9", false );
-//    new Tab( "tab10", false );
-//    new Tab( "tab11", false );
-//    new Tab( "tab12", false );
-//    new Tab( "tab13", false );
-//    new Tab( "tab14", false );
-//    new Tab( "tab15", false );
 } );
