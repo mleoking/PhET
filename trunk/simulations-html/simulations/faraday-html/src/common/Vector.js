5d1c980b9225e7ae93a6cb546a58c7161aa4eb35
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-11-19 18:56:02 +0000

    11/19/12 get rid of canvas text cursor by disabling text selection. 11/19/12 enable touch events

---
+++ trunk/simulations-html/simulations/faraday-html/src/common/Vector.js
@@ -0,0 +1,25 @@
+// Copyright 2002-2012, University of Colorado
+
+/**
+ * Immutable 2D vector.
+ *
+ * @author Chris Malley (PixelZoom, Inc.)
+ */
+define( [], function () {
+
+    /**
+     * @param {Number} x
+     * @param {Number} y
+     * @constructor
+     */
+    function Vector( x, y ) {
+
+        // Cartesian coordinates
+        this.getX = function() { return x; };
+        this.getY = function() { return y; };
+
+        // Polar coordinates
+        this.getAngle = function() { return Math.atan2( y, x ); };
+        this.getMagnitude = function() { return Math.sqrt( x * x + y * y ); };
+    }
+} );
