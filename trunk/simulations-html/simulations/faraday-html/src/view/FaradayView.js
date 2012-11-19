d70c4429d4cfc4e6e03c2a77977067cb0f2e3bec
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-11-19 16:20:39 +0000

    wire up control panel to view properties and stub display objects

--- trunk/simulations-html/simulations/faraday-html/src/view/FaradayView.js
+++ trunk/simulations-html/simulations/faraday-html/src/view/FaradayView.js
@@ -5,9 +5,15 @@
  *
  * @author Chris Malley (PixelZoom, Inc.)
  */
-define( [ 'easel', 'view/BarMagnetDisplay' ], function ( Easel, BarMagnetDisplay ) {
+define( [ 'easel', 'common/Property', 'view/BarMagnetDisplay' ], function ( Easel, Property, BarMagnetDisplay ) {
 
     function FaradayView( canvas, model, mvt ) {
+
+        // properties that are specific to the view (have no model counterpart.)
+        this.magnetTransparent = new Property( false );
+        this.fieldVisible = new Property( true );
+        this.compassVisible = new Property( true );
+        this.fieldMeterVisible = new Property( false );
 
         // Create the stage.
         this.stage = new Easel.Stage( canvas );
@@ -18,9 +24,30 @@ define( [ 'easel', 'view/BarMagnetDisplay' ], function ( Easel, BarMagnetDisplay
         background.graphics.rect( 0, 0, canvas.width, canvas.height );
         this.stage.addChild( background );
 
-        // Render a bar magnet
-        var barMagnetDisplay = new BarMagnetDisplay( model.barMagnet, mvt );
-        this.stage.addChild( barMagnetDisplay );
+        // TODO field
+        this.field = new Easel.Text( "field", "bold 36px Arial", 'white' );
+        this.field.x = 50;
+        this.field.y = 50;
+        this.field.visible = this.fieldVisible.get();
+        this.stage.addChild( this.field );
+
+        // bar magnet
+        this.barMagnet = new BarMagnetDisplay( model.barMagnet, mvt );
+        this.stage.addChild( this.barMagnet );
+
+        // TODO compass
+        this.compass = new Easel.Text( "compass", "bold 36px Arial", 'white' );
+        this.compass.x = 50;
+        this.compass.y = 100;
+        this.compass.visible = this.compassVisible.get();
+        this.stage.addChild( this.compass );
+
+        // TODO field meter
+        this.meter = new Easel.Text( "meter", "bold 36px Arial", 'white' );
+        this.meter.x = 50;
+        this.meter.y = 150;
+        this.meter.visible = this.fieldMeterVisible.get();
+        this.stage.addChild( this.meter );
 
         this.stage.enableMouseOver();
     }
