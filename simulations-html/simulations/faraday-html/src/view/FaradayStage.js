461ed6ff3e8b89ea3db79bb185cbd467d81caec6
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-11-20 20:49:13 +0000

    remove test code for compass needle

--- simulations-html/simulations/faraday-html/src/view/FaradayStage.js
+++ simulations-html/simulations/faraday-html/src/view/FaradayStage.js
@@ -10,18 +10,17 @@ define( [ 'easel',
           'common/Property',
           'view/BarMagnetDisplay',
           'view/CompassDisplay',
-          'view/CompassNeedleDisplay',
           'view/FieldDisplay',
           'view/FieldMeterDisplay'
         ],
-        function ( Easel, Property, BarMagnetDisplay, CompassDisplay, CompassNeedleDisplay, FieldDisplay, FieldMeterDisplay ) {
+        function ( Easel, Property, BarMagnetDisplay, CompassDisplay, FieldDisplay, FieldMeterDisplay ) {
 
     function FaradayStage( canvas, model ) {
 
         // view-specific properties (have no model counterpart.)
         this.magnetTransparent = new Property( false );
         this.magnetTransparent.addObserver( function() {
-             //TODO change transparency of barMagnet display object
+             //TODO make "inside field" display object visible
         });
 
         // stage
@@ -51,13 +50,6 @@ define( [ 'easel',
         this.stage.addChild( this.barMagnet );
         this.stage.addChild( this.compass );
         this.stage.addChild( this.meter );
-
-        //XXX test compass needle
-        var compassNeedle = new CompassNeedleDisplay( 0, 1 );
-        var origin = model.mvt.modelToView( new Easel.Point(0,0) );
-        compassNeedle.x = origin.x;
-        compassNeedle.y = origin.y;
-        this.stage.addChild( compassNeedle );
     }
 
     // Resets all view-specific properties
