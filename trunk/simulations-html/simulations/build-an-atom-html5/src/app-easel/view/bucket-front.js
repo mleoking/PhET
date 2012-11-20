a83deee003c59e926d9135c7e37e130b5cd3c40b
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-11-20 23:47:49 +0000

    Improved formatting and header comment.

--- trunk/simulations-html/simulations/build-an-atom-html5/src/app-easel/view/bucket-front.js
+++ trunk/simulations-html/simulations/build-an-atom-html5/src/app-easel/view/bucket-front.js
@@ -1,19 +1,15 @@
 // Copyright 2002-2012, University of Colorado
-define( [
-            'underscore',
-            'easel'
-        ], function ( _, Easel ) {
-
+define( [ 'underscore', 'easel' ], function ( _, Easel ) {
     /**
-     * Sets the stroke style for the current subpath. Like all drawing methods, this can be chained, so you can define the stroke style and color in a single line of code like so:
-     * myGraphics.setStrokeStyle(8,"round").beginStroke("#F00");
+     * @class BucketFront
      * @param centerX Center of the bucket front.
      * @param topY Top of the bucket front.
      * @param width Width of the bucket front.
      * @param height Height of the bucket front.
      * @param labelText Text shown on front of bucket.
-     * @return {BucketFront} The representation of the front of the bucket.
+     * @constructor
      **/
+
     var BucketFront = function ( centerX, topY, width, labelText ) {
         Easel.Container.prototype.initialize.call( this );
         this.initialize( centerX, topY, width, labelText );
@@ -29,12 +25,13 @@ define( [
         // Create the basic shape of the front of the bucket.
         var shape = new Easel.Shape();
         shape.graphics.beginStroke( "black" ).beginFill( "gray" ).setStrokeStyle( 2 );
-        shape.graphics.moveTo( 0, 0 );
-        shape.graphics.lineTo( width * 0.1, height * 0.8 );
-        shape.graphics.bezierCurveTo( width * 0.3, height, width * 0.7, height, width * 0.9, height * 0.8 );
-        shape.graphics.lineTo( width, 0 );
-        shape.graphics.bezierCurveTo( width * 0.8, height * 0.15, width * 0.2, height * 0.15, 0, 0 );
-        shape.graphics.closePath();
+        shape.graphics
+                .moveTo( 0, 0 )
+                .lineTo( width * 0.1, height * 0.8 )
+                .bezierCurveTo( width * 0.3, height, width * 0.7, height, width * 0.9, height * 0.8 )
+                .lineTo( width, 0 )
+                .bezierCurveTo( width * 0.8, height * 0.15, width * 0.2, height * 0.15, 0, 0 )
+                .closePath();
         shape.graphics.endStroke().endFill();
         this.addChild( shape );
 
