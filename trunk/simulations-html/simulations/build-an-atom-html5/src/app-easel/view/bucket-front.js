c3d517ed0b7439ec5a9b4000e2230609af2324c2
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-11-20 22:50:00 +0000

    Added labels to the subatomic particle buckets.

--- trunk/simulations-html/simulations/build-an-atom-html5/src/app-easel/view/bucket-front.js
+++ trunk/simulations-html/simulations/build-an-atom-html5/src/app-easel/view/bucket-front.js
@@ -26,12 +26,17 @@ define( [
     p.initialize = function ( centerX, topY, width, labelText ) {
         var height = width * 0.4; // Determined empirically for best look.
         var shape = new Easel.Shape();
-//        shape.graphics.beginStroke( "black" ).beginFill( "gray" ).setStrokeStyle( 2 ).drawRect( 0, 0, width, height ).endStroke().endFill();
         shape.graphics.beginStroke( "black" ).beginFill( "gray" ).setStrokeStyle( 2 );
         shape.graphics.moveTo( 0, 0 ).lineTo( width * 0.2, height ).lineTo( width * 0.8, height ).lineTo(width, 0 ).closePath();
         shape.graphics.endStroke().endFill();
         this.addChild( shape );
+        var label = new Easel.Text( labelText, "bold 24px Helvetica", "white" );
+        label.textBaseline = "middle";
+        label.x = width / 2 - label.getMeasuredWidth() / 2;
+        label.y = height / 2;
+        this.addChild( label );
 
+        // Set the position.
         this.x = centerX - width / 2;
         this.y = topY;
     };
