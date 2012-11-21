b7314d988f27ad052ea3b58398e2fcc8a3d1f422
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-11-21 21:52:41 +0000

    Put the stage into its own file (BuildAnAtomStage).

---
+++ simulations-html/simulations/build-an-atom-html/src/app-easel/model/BuildAnAtomModel.js
@@ -0,0 +1,11 @@
+// Copyright 2002-2012, University of Colorado
+define( [
+            'atom.js'
+        ], function ( Atom ) {
+
+    function BuildAnAtomModel() {
+        this.atom = new Atom();
+    }
+
+    return BuildAnAtomModel();
+} );
