2b14153cd45c474d744bc7aac6bdf917c88e8f05
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-11-21 23:34:16 +0000

    Made the BucketFront class based on the model representation of the bucket.

--- trunk/simulations-html/simulations/build-an-atom-html/src/app-easel/view/bucket-front.js
+++ trunk/simulations-html/simulations/build-an-atom-html/src/app-easel/view/bucket-front.js
@@ -2,24 +2,22 @@
 define( [ 'underscore', 'easel' ], function ( _, Easel ) {
     /**
      * @class BucketFront
-     * @param centerX Center of the bucket front.
-     * @param topY Top of the bucket front.
-     * @param width Width of the bucket front.
-     * @param height Height of the bucket front.
-     * @param labelText Text shown on front of bucket.
+     * @param bucket The model of the bucket.
      * @constructor
      **/
-
-    var BucketFront = function ( centerX, topY, width, labelText ) {
+    var BucketFront = function ( bucket ) {
         Easel.Container.prototype.initialize.call( this );
-        this.initialize( centerX, topY, width, labelText );
+        this.bucket = bucket;
+        this.initialize();
     };
 
     var p = BucketFront.prototype;
 
     _.extend( p, Easel.Container.prototype );
 
-    p.initialize = function ( centerX, topY, width, labelText ) {
+    p.initialize = function () {
+
+        var width = this.bucket.width;
         var height = width * 0.5; // Determined empirically for best look.
 
         // Create the basic shape of the front of the bucket.
@@ -31,20 +29,25 @@ define( [ 'underscore', 'easel' ], function ( _, Easel ) {
                 .bezierCurveTo( width * 0.3, height, width * 0.7, height, width * 0.9, height * 0.8 )
                 .lineTo( width, 0 )
                 .bezierCurveTo( width * 0.8, height * 0.15, width * 0.2, height * 0.15, 0, 0 )
-                .closePath();
-        shape.graphics.endStroke().endFill();
+                .closePath()
+                .endStroke()
+                .endFill();
         this.addChild( shape );
 
         // Create and add the label, centered on the front.
-        var label = new Easel.Text( labelText, "bold 24px Helvetica", "white" );
+        var label = new Easel.Text( this.bucket.labelText, "bold 24px Helvetica", "white" );
         label.textBaseline = "middle";
         label.x = width / 2 - label.getMeasuredWidth() / 2;
         label.y = height / 2;
         this.addChild( label );
 
+        // Set the registration point.
+        this.regX = width / 2;
+        this.regY = 0;
+
         // Set the position.
-        this.x = centerX - width / 2;
-        this.y = topY;
+        this.x = this.bucket.x;
+        this.y = this.bucket.y;
     };
 
     return BucketFront;
