4ff14dcf384b7c23924dc7416cc5ec5ffd974b6a
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-11-21 20:56:15 +0000

    switch to textfield for entering magnet strength, slider is brain damaged

--- trunk/simulations-html/simulations/faraday-html/src/view/ControlPanel.js
+++ trunk/simulations-html/simulations/faraday-html/src/view/ControlPanel.js
@@ -17,6 +17,24 @@ define( [ 'common/PropertyCheckBox' ],
              */
             ControlPanel.connect = function ( model, stage ) {
 
+                //TODO Would prefer a slider, but HTML slider is brain damaged, and doesn't work in FireFox.
+                // Strength text field
+                var strengthTextField = document.getElementById( "strengthTextField" );
+                strengthTextField.onkeydown = function ( event ) {
+                    // Interpretting keyCodes has all kinds of browser incompatibility problems.
+                    if ( event.keyCode === 13 ) {
+                        // keep the value in range
+                        if ( strengthTextField.value < model.barMagnet.strengthRange.min ) {
+                            strengthTextField.value = model.barMagnet.strengthRange.min;
+                        }
+                        else if ( strengthTextField.value > model.barMagnet.strengthRange.max ) {
+                            strengthTextField.value = model.barMagnet.strengthRange.max;
+                        }
+                        // update the model
+                        model.barMagnet.strength.set( strengthTextField.value );
+                    }
+                };
+
                 // Check boxes
                 PropertyCheckBox.connect( stage.magnetTransparent, "seeInsideMagnetCheckBox" );
                 PropertyCheckBox.connect( model.field.visible, "showFieldCheckBox" );
@@ -24,7 +42,7 @@ define( [ 'common/PropertyCheckBox' ],
                 PropertyCheckBox.connect( model.fieldMeter.visible, "showFieldMeterCheckBox" );
 
                 var flipPolarityButton = document.getElementById( "flipPolarityButton" );
-                flipPolarityButton.onclick = function () {
+                flipPolarityButton.onmousemove = function () {
                     model.barMagnet.orientation.set( model.barMagnet.orientation.get() + Math.PI );
                 };
 
