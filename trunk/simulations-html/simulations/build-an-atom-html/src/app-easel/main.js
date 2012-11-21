44f19921ab2ef8627cb5266c9d3fe2a8389f48df
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-11-21 21:52:41 +0000

    Put the stage into its own file (BuildAnAtomStage).

--- trunk/simulations-html/simulations/build-an-atom-html/src/app-easel/main.js
+++ trunk/simulations-html/simulations/build-an-atom-html/src/app-easel/main.js
@@ -9,114 +9,117 @@ require( [
              'view/bucket-front',
              'view/symbol-view',
              'view/mass-number-view',
+             'view/BuildAnAtomStage',
              'model/atom',
              'view/electron-shell-view',
              'tpl!templates/periodic-table.html'
-         ], function ( _, Easel, Particle2, ParticleView, AtomView, BucketHole, BucketFront, SymbolView, MassNumberView, Atom, ElectronShellView, periodicTable ) {
+         ], function ( _, Easel, Particle2, ParticleView, AtomView, BucketHole, BucketFront, SymbolView, MassNumberView, BuildAnAtomStage, Atom, ElectronShellView, periodicTable ) {
 
-    var $window = $( window );
+    var buildAnAtomStage = new BuildAnAtomStage( document.getElementById( 'atom-construction-canvas' ) );
 
-    // Create the canvas where atoms will be constructed.
-    var atomConstructionCanvas = $( '#atom-construction-canvas' );
-    var atomStage = new Easel.Stage( atomConstructionCanvas[0] );
-    var stageWidth = atomConstructionCanvas[0].width;
-    var stageHeight = atomConstructionCanvas[0].height;
-
-    // Create a root node for the scene graph.
-    var root = new Easel.Container();
-    atomStage.addChild( root );
-
-    // Create and add the place where the nucleus will be constructed.
-    var atomView = new AtomView();
-    atomView.x = stageWidth / 2;
-    atomView.y = stageHeight * 0.4;
-    root.addChild( atomView );
-
-    // Add the electron shell.
-    atomStage.addChild( new ElectronShellView( stageWidth / 2, stageHeight * 0.4 ) );
-
-    // Add the location where the atom will be constructed.
-
-    // Create and add the bucket holes where the idle particles will be kept.
-    var bucketWidth = 150;
-    var bucketHoleY = stageHeight * 0.75;
-    var neutronBucketHole = new BucketHole( stageWidth / 2, bucketHoleY, bucketWidth );
-    root.addChild( neutronBucketHole );
-    var protonBucketHole = new BucketHole( ( stageWidth - bucketWidth ) / 4, bucketHoleY, bucketWidth );
-    root.addChild( protonBucketHole );
-    var electronBucketHole = new BucketHole( stageWidth - (( stageWidth - bucketWidth ) / 4), bucketHoleY, bucketWidth );
-    root.addChild( electronBucketHole );
-
-    // Create and add the particles.
-    root.addChild( ParticleView.createParticleView( new Particle2( protonBucketHole.x + 30, protonBucketHole.y, "red", 15, "proton" ) ) );
-    root.addChild( ParticleView.createParticleView( new Particle2( protonBucketHole.x + 60, protonBucketHole.y, "red", 15, "proton" ) ) );
-    root.addChild( ParticleView.createParticleView( new Particle2( protonBucketHole.x + 90, protonBucketHole.y, "red", 15, "proton" ) ) );
-    root.addChild( ParticleView.createParticleView( new Particle2( protonBucketHole.x + 120, protonBucketHole.y, "red", 15, "proton" ) ) );
-    root.addChild( ParticleView.createParticleView( new Particle2( neutronBucketHole.x + 30, neutronBucketHole.y, "gray", 15, "neutron" ) ) );
-    root.addChild( ParticleView.createParticleView( new Particle2( neutronBucketHole.x + 60, neutronBucketHole.y, "gray", 15, "neutron" ) ) );
-    root.addChild( ParticleView.createParticleView( new Particle2( neutronBucketHole.x + 90, neutronBucketHole.y, "gray", 15, "neutron" ) ) );
-    root.addChild( ParticleView.createParticleView( new Particle2( neutronBucketHole.x + 120, neutronBucketHole.y, "gray", 15, "neutron" ) ) );
-    root.addChild( ParticleView.createParticleView( new Particle2( neutronBucketHole.x + 120, neutronBucketHole.y, "gray", 15, "neutron" ) ) );
-    root.addChild( ParticleView.createParticleView( new Particle2( electronBucketHole.x + 30, electronBucketHole.y, "blue", 8, "electron" ) ) );
-    root.addChild( ParticleView.createParticleView( new Particle2( electronBucketHole.x + 60, electronBucketHole.y, "blue", 8, "electron" ) ) );
-    root.addChild( ParticleView.createParticleView( new Particle2( electronBucketHole.x + 90, electronBucketHole.y, "blue", 8, "electron" ) ) );
-    root.addChild( ParticleView.createParticleView( new Particle2( electronBucketHole.x + 120, electronBucketHole.y, "blue", 8, "electron" ) ) );
-
-    // Add the bucket fronts.
-    root.addChild( new BucketFront( protonBucketHole.centerX, protonBucketHole.centerY, bucketWidth, "Protons" ) );
-    root.addChild( new BucketFront( neutronBucketHole.centerX, neutronBucketHole.centerY, bucketWidth, "Neutrons" ) );
-    root.addChild( new BucketFront( electronBucketHole.centerX, electronBucketHole.centerY, bucketWidth, "Electrons" ) );
-
-    atomStage.update();
-
-    //Get rid of text cursor when dragging on the canvas, see http://stackoverflow.com/questions/2659999/html5-canvas-hand-cursor-problems
-    atomConstructionCanvas[0].onselectstart = function () {
-        return false;
-    }; // IE
-    atomConstructionCanvas[0].onmousedown = function () {
-        return false;
-    }; // Mozilla
-
-    // Set the frame rate.
-    Easel.Ticker.setFPS( 60 );
-
-    // Enable and configure touch and mouse events.
-    Easel.Touch.enable( atomStage, false, false );
-    atomStage.enableMouseOver( 10 );
-    atomStage.mouseMoveOutside = true;
-
-    Easel.Ticker.addListener( atomStage, true );
-
-    //resize the canvas when the window is resized
-    //Copied from energy skate park easel prototype
-    $window.on( 'resize', function () {
-        var winW = $window.width();
-        var winH = $window.height();
-
-        var scale = Math.min( winW / 614, winH / 768 );
-
-        var canvasW = scale * 614;
-        var canvasH = scale * 768;
-
-        var container = atomConstructionCanvas.parent();
-
-        // set the canvas size
-        atomConstructionCanvas.attr( {
-                                         width:~~canvasW,
-                                         height:~~canvasH
-                                     } );
-
-        // center the canvas in its parent container
-        atomConstructionCanvas.css( {
-                                        marginLeft:(container.width() - ~~canvasW) / 2
-                                    } );
-
-        root.scaleX = root.scaleY = scale;
-        atomStage.update();
-    } );
-
-    $window.resize();
-
+//    var $window = $( window );
+//
+//    // Create the canvas where atoms will be constructed.
+//    var atomConstructionStage = $( '#atom-construction-canvas' );
+//    var atomStage = new Easel.Stage( atomConstructionStage[0] );
+//    var stageWidth = atomConstructionStage[0].width;
+//    var stageHeight = atomConstructionStage[0].height;
+//
+//    // Create a root node for the scene graph.
+//    var root = new Easel.Container();
+//    atomStage.addChild( root );
+//
+//    // Create and add the place where the nucleus will be constructed.
+//    var atomView = new AtomView();
+//    atomView.x = stageWidth / 2;
+//    atomView.y = stageHeight * 0.4;
+//    root.addChild( atomView );
+//
+//    // Add the electron shell.
+//    atomStage.addChild( new ElectronShellView( stageWidth / 2, stageHeight * 0.4 ) );
+//
+//    // Add the location where the atom will be constructed.
+//
+//    // Create and add the bucket holes where the idle particles will be kept.
+//    var bucketWidth = 150;
+//    var bucketHoleY = stageHeight * 0.75;
+//    var neutronBucketHole = new BucketHole( stageWidth / 2, bucketHoleY, bucketWidth );
+//    root.addChild( neutronBucketHole );
+//    var protonBucketHole = new BucketHole( ( stageWidth - bucketWidth ) / 4, bucketHoleY, bucketWidth );
+//    root.addChild( protonBucketHole );
+//    var electronBucketHole = new BucketHole( stageWidth - (( stageWidth - bucketWidth ) / 4), bucketHoleY, bucketWidth );
+//    root.addChild( electronBucketHole );
+//
+//    // Create and add the particles.
+//    root.addChild( ParticleView.createParticleView( new Particle2( protonBucketHole.x + 30, protonBucketHole.y, "red", 15, "proton" ) ) );
+//    root.addChild( ParticleView.createParticleView( new Particle2( protonBucketHole.x + 60, protonBucketHole.y, "red", 15, "proton" ) ) );
+//    root.addChild( ParticleView.createParticleView( new Particle2( protonBucketHole.x + 90, protonBucketHole.y, "red", 15, "proton" ) ) );
+//    root.addChild( ParticleView.createParticleView( new Particle2( protonBucketHole.x + 120, protonBucketHole.y, "red", 15, "proton" ) ) );
+//    root.addChild( ParticleView.createParticleView( new Particle2( neutronBucketHole.x + 30, neutronBucketHole.y, "gray", 15, "neutron" ) ) );
+//    root.addChild( ParticleView.createParticleView( new Particle2( neutronBucketHole.x + 60, neutronBucketHole.y, "gray", 15, "neutron" ) ) );
+//    root.addChild( ParticleView.createParticleView( new Particle2( neutronBucketHole.x + 90, neutronBucketHole.y, "gray", 15, "neutron" ) ) );
+//    root.addChild( ParticleView.createParticleView( new Particle2( neutronBucketHole.x + 120, neutronBucketHole.y, "gray", 15, "neutron" ) ) );
+//    root.addChild( ParticleView.createParticleView( new Particle2( neutronBucketHole.x + 120, neutronBucketHole.y, "gray", 15, "neutron" ) ) );
+//    root.addChild( ParticleView.createParticleView( new Particle2( electronBucketHole.x + 30, electronBucketHole.y, "blue", 8, "electron" ) ) );
+//    root.addChild( ParticleView.createParticleView( new Particle2( electronBucketHole.x + 60, electronBucketHole.y, "blue", 8, "electron" ) ) );
+//    root.addChild( ParticleView.createParticleView( new Particle2( electronBucketHole.x + 90, electronBucketHole.y, "blue", 8, "electron" ) ) );
+//    root.addChild( ParticleView.createParticleView( new Particle2( electronBucketHole.x + 120, electronBucketHole.y, "blue", 8, "electron" ) ) );
+//
+//    // Add the bucket fronts.
+//    root.addChild( new BucketFront( protonBucketHole.centerX, protonBucketHole.centerY, bucketWidth, "Protons" ) );
+//    root.addChild( new BucketFront( neutronBucketHole.centerX, neutronBucketHole.centerY, bucketWidth, "Neutrons" ) );
+//    root.addChild( new BucketFront( electronBucketHole.centerX, electronBucketHole.centerY, bucketWidth, "Electrons" ) );
+//
+//    atomStage.update();
+//
+//    //Get rid of text cursor when dragging on the canvas, see http://stackoverflow.com/questions/2659999/html5-canvas-hand-cursor-problems
+//    atomConstructionStage[0].onselectstart = function () {
+//        return false;
+//    }; // IE
+//    atomConstructionStage[0].onmousedown = function () {
+//        return false;
+//    }; // Mozilla
+//
+//    // Set the frame rate.
+//    Easel.Ticker.setFPS( 60 );
+//
+//    // Enable and configure touch and mouse events.
+//    Easel.Touch.enable( atomStage, false, false );
+//    atomStage.enableMouseOver( 10 );
+//    atomStage.mouseMoveOutside = true;
+//
+//    Easel.Ticker.addListener( atomStage, true );
+//
+//    //resize the canvas when the window is resized
+//    //Copied from energy skate park easel prototype
+//    $window.on( 'resize', function () {
+//        var winW = $window.width();
+//        var winH = $window.height();
+//
+//        var scale = Math.min( winW / 614, winH / 768 );
+//
+//        var canvasW = scale * 614;
+//        var canvasH = scale * 768;
+//
+//        var container = atomConstructionStage.parent();
+//
+//        // set the canvas size
+//        atomConstructionStage.attr( {
+//                                         width:~~canvasW,
+//                                         height:~~canvasH
+//                                     } );
+//
+//        // center the canvas in its parent container
+//        atomConstructionStage.css( {
+//                                        marginLeft:(container.width() - ~~canvasW) / 2
+//                                    } );
+//
+//        root.scaleX = root.scaleY = scale;
+//        atomStage.update();
+//    } );
+//
+//    $window.resize();
+//
     $( document ).ready( function () {
 
         var atom = new Atom();
@@ -129,6 +132,4 @@ require( [
 
         $( '#periodic-table' ).html( periodicTable() );
     } );
-
-
 } );
---
+++ trunk/simulations-html/simulations/build-an-atom-html/src/app-easel/view/BuildAnAtomStage.js
@@ -0,0 +1,125 @@
+define( [
+            'underscore',
+            'easel',
+            'model/particle2',
+            'view/particle-view',
+            'view/atom-view',
+            'view/bucket-hole',
+            'view/bucket-front',
+            'view/symbol-view',
+            'view/mass-number-view',
+            'model/atom',
+            'view/electron-shell-view'
+        ], function ( _, Easel, Particle2, ParticleView, AtomView, BucketHole, BucketFront, SymbolView, MassNumberView, Atom, ElectronShellView ) {
+
+    function BuildAnAtomStage( canvas, model ) {
+
+        var $window = $( window );
+
+        // Create the stage.
+        this.stage = new Easel.Stage( canvas );
+        var stageWidth = canvas.width;
+        var stageHeight = canvas.height;
+
+        // Create a root node for the scene graph.
+        var root = new Easel.Container();
+        this.stage.addChild( root );
+
+        // Create and add the place where the nucleus will be constructed.
+        var atomView = new AtomView();
+        atomView.x = stageWidth / 2;
+        atomView.y = stageHeight * 0.4;
+        root.addChild( atomView );
+
+        // Add the electron shell.
+        var electronShell = new ElectronShellView();
+        electronShell.x = stageWidth / 2;
+        electronShell.y = stageHeight * 0.4;
+        this.stage.addChild( electronShell );
+
+        // Create and add the bucket holes where the idle particles will be kept.
+        var bucketWidth = 150;
+        var bucketHoleY = stageHeight * 0.75;
+        var neutronBucketHole = new BucketHole( stageWidth / 2, bucketHoleY, bucketWidth );
+        root.addChild( neutronBucketHole );
+        var protonBucketHole = new BucketHole( ( stageWidth - bucketWidth ) / 4, bucketHoleY, bucketWidth );
+        root.addChild( protonBucketHole );
+        var electronBucketHole = new BucketHole( stageWidth - (( stageWidth - bucketWidth ) / 4), bucketHoleY, bucketWidth );
+        root.addChild( electronBucketHole );
+
+        // Create and add the particles.
+        root.addChild( ParticleView.createParticleView( new Particle2( protonBucketHole.x + 30, protonBucketHole.y, "red", 15, "proton" ) ) );
+        root.addChild( ParticleView.createParticleView( new Particle2( protonBucketHole.x + 60, protonBucketHole.y, "red", 15, "proton" ) ) );
+        root.addChild( ParticleView.createParticleView( new Particle2( protonBucketHole.x + 90, protonBucketHole.y, "red", 15, "proton" ) ) );
+        root.addChild( ParticleView.createParticleView( new Particle2( protonBucketHole.x + 120, protonBucketHole.y, "red", 15, "proton" ) ) );
+        root.addChild( ParticleView.createParticleView( new Particle2( neutronBucketHole.x + 30, neutronBucketHole.y, "gray", 15, "neutron" ) ) );
+        root.addChild( ParticleView.createParticleView( new Particle2( neutronBucketHole.x + 60, neutronBucketHole.y, "gray", 15, "neutron" ) ) );
+        root.addChild( ParticleView.createParticleView( new Particle2( neutronBucketHole.x + 90, neutronBucketHole.y, "gray", 15, "neutron" ) ) );
+        root.addChild( ParticleView.createParticleView( new Particle2( neutronBucketHole.x + 120, neutronBucketHole.y, "gray", 15, "neutron" ) ) );
+        root.addChild( ParticleView.createParticleView( new Particle2( neutronBucketHole.x + 120, neutronBucketHole.y, "gray", 15, "neutron" ) ) );
+        root.addChild( ParticleView.createParticleView( new Particle2( electronBucketHole.x + 30, electronBucketHole.y, "blue", 8, "electron" ) ) );
+        root.addChild( ParticleView.createParticleView( new Particle2( electronBucketHole.x + 60, electronBucketHole.y, "blue", 8, "electron" ) ) );
+        root.addChild( ParticleView.createParticleView( new Particle2( electronBucketHole.x + 90, electronBucketHole.y, "blue", 8, "electron" ) ) );
+        root.addChild( ParticleView.createParticleView( new Particle2( electronBucketHole.x + 120, electronBucketHole.y, "blue", 8, "electron" ) ) );
+
+        // Add the bucket fronts.
+        root.addChild( new BucketFront( protonBucketHole.centerX, protonBucketHole.centerY, bucketWidth, "Protons" ) );
+        root.addChild( new BucketFront( neutronBucketHole.centerX, neutronBucketHole.centerY, bucketWidth, "Neutrons" ) );
+        root.addChild( new BucketFront( electronBucketHole.centerX, electronBucketHole.centerY, bucketWidth, "Electrons" ) );
+
+        // Initial stage update.  TODO: Is this needed?
+        this.stage.update();
+
+        //Get rid of text cursor when dragging on the canvas, see http://stackoverflow.com/questions/2659999/html5-canvas-hand-cursor-problems
+        canvas.onselectstart = function () {
+            return false;
+        }; // IE
+        canvas.onmousedown = function () {
+            return false;
+        }; // Mozilla
+
+        // Set the frame rate.
+        Easel.Ticker.setFPS( 60 );
+
+        // Enable and configure touch and mouse events.
+        Easel.Touch.enable( this.stage, false, false );
+        this.stage.enableMouseOver( 10 );
+        this.stage.mouseMoveOutside = true;
+
+        Easel.Ticker.addListener( this.stage, true );
+
+        //resize the canvas when the window is resized.
+        var buildAnAtomCanvas = $( canvas );
+        var self = this;
+        $window.on( 'resize', function () {
+            var winW = $window.width();
+            var winH = $window.height();
+
+            var scale = Math.min( winW / 614, winH / 768 );
+
+            var canvasW = scale * 614;
+            var canvasH = scale * 768;
+
+            var container = buildAnAtomCanvas.parent();
+
+            // set the canvas size
+            buildAnAtomCanvas.attr( {
+                                 width:~~canvasW,
+                                 height:~~canvasH
+                             } );
+
+            // center the canvas in its parent container
+            buildAnAtomCanvas.css( {
+                                marginLeft:(container.width() - ~~canvasW) / 2
+                            } );
+
+            root.scaleX = root.scaleY = scale;
+            self.stage.update();
+        } );
+
+        $window.resize();
+    }
+
+    return BuildAnAtomStage;
+
+} );
