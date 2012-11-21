208e5a03de126d3a74b7b1a1df8145057a3c8650
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-11-21 03:19:12 +0000

    add visualization of field inside magnet

---
+++ simulations-html/simulations/faraday-html/src/view/FieldInsideDisplay.js
@@ -0,0 +1,90 @@
+// Copyright 2002-2012, University of Colorado
+
+/**
+ * Visualization of the E-field inside the bar magnet.
+ */
+define( [
+            'easel',
+            'common/Dimension',
+            'common/MathUtil',
+            'view/CompassNeedleDisplay'
+        ],
+        function ( Easel, Dimension, MathUtil, CompassNeedleDisplay ) {
+
+            /**
+             * @param {BarMagnet} magnet
+             * @param {ModelViewTransform} mvt
+             * @constructor
+             */
+            function FieldInsideDisplay( magnet, mvt ) {
+
+                // constructor stealing
+                Easel.Container.call( this );
+
+                // parent container for needles
+                var needlesContainer = new Easel.Container();
+                this.addChild( needlesContainer );
+
+                var NEEDLE_SIZE = new Dimension( 25, 7 );
+
+                // @param {Number} strength
+                var updateStrength = function ( strength ) {
+
+                    needlesContainer.removeAllChildren();
+
+                    var ROWS = 2;
+                    var COLUMNS = 7;
+
+                    var xDelta = magnet.size.width / ( COLUMNS + 1 );
+                    var xStart = -(magnet.size.width / 2) + xDelta; // left
+                    var xOffset = xStart;
+
+                    var yDelta = magnet.size.height / ROWS;
+                    var yStart = -(magnet.size.height / 2) + ( yDelta/2 ); // top
+                    var yOffset = yStart;
+
+                    // populate top-to-bottom, left-to-right
+                    for ( var row = 0; row < ROWS; row++ ) {
+                        xOffset = xStart;
+                        for ( var column = 0; column < COLUMNS; column++ ) {
+                            var alpha = 1;  //TODO alpha = strength/maxStrength
+                            var needle = new CompassNeedleDisplay( NEEDLE_SIZE, 0, alpha );
+                            needle.x = xOffset;
+                            needle.y = yOffset;
+                            needlesContainer.addChild( needle );
+                            xOffset += xDelta;
+                        }
+                        yOffset += yDelta;
+                    }
+                };
+                magnet.strength.addObserver( updateStrength );
+
+                // @param {Point} location
+                var updateLocation = function( location ) {
+                    var p = mvt.modelToView( location );
+                    this.x = p.x;
+                    this.y = p.y;
+                    //WORKAROUND: Easel nested Containers all appear to have their own independent transform!
+                    needlesContainer.x = this.x;
+                    needlesContainer.y = this.y;
+                };
+                magnet.location.addObserver( updateLocation );
+
+                // @param {Number} orientation in radians
+                var updateOrientation = function ( orientation ) {
+                    this.rotation = MathUtil.toDegrees( orientation );
+                    needlesContainer.rotation = this.rotation; //WORKAROUND: Easel nested Containers all appear to have their own independent transform!
+                };
+                magnet.orientation.addObserver( updateOrientation );
+
+                // update now
+                updateStrength( magnet.strength.get() );
+                updateLocation( magnet.location.get() );
+                updateOrientation( magnet.orientation.get() );
+            }
+
+            // prototype chaining
+            FieldInsideDisplay.prototype = new Easel.Container();
+
+            return FieldInsideDisplay;
+        } );
