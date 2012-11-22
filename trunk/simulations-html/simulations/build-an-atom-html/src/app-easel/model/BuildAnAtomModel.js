f3ae061e27227a836ee4243b2e93a08d16cc55dc
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-11-22 00:28:50 +0000

    Added particle to model, made view render from the particle list.

--- trunk/simulations-html/simulations/build-an-atom-html/src/app-easel/model/BuildAnAtomModel.js
+++ trunk/simulations-html/simulations/build-an-atom-html/src/app-easel/model/BuildAnAtomModel.js
@@ -1,18 +1,28 @@
 // Copyright 2002-2012, University of Colorado
 define( [
             'model/atom',
-            'model/Bucket'
-        ], function ( Atom, Bucket ) {
+            'model/Bucket',
+            'model/particle'
+        ], function ( Atom, Bucket, Particle ) {
 
     function BuildAnAtomModel() {
+
         this.atom = new Atom();
 
         this.buckets = {
-          protonBucket: new Bucket( 105, 600, 150, "Protons" ),
-          neutronBucket: new Bucket( 305, 600, 150, "Neutrons" ),
-          electronBucket: new Bucket( 505, 600, 150, "Electrons" )
+            protonBucket:new Bucket( 105, 600, 150, "Protons" ),
+            neutronBucket:new Bucket( 305, 600, 150, "Neutrons" ),
+            electronBucket:new Bucket( 505, 600, 150, "Electrons" )
         };
+
+        this.particles = [ ];
+
+        this.initializeParticles();
     }
+
+    BuildAnAtomModel.prototype.initializeParticles = function () {
+        this.particles.push( new Particle(  this.buckets.protonBucket.x, this.buckets.protonBucket.y, "red", 15, "proton" ));
+    };
 
     return BuildAnAtomModel;
 } );
