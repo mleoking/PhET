60ee22d5b45c5b3a49f5c9e51cd4d569543512b5
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-11-23 05:27:06 +0000

    added analytics for checkboxes

---
+++ trunk/simulations-html/common/common-html/src/app/analytics/Analytics.js
@@ -0,0 +1,43 @@
+//File for delivering and collecting events for sim event data analysis
+define( [], function () {
+    function Analytics() {
+
+    }
+
+    /**
+     * This function starts with required parameters
+     * @param {String} component
+     * @param {String} componentType
+     * @param {String} action
+     * @param {Array} parameters optional parameters
+     * @param {String} messageType optional
+     * */
+    Analytics.prototype.log = function ( component, componentType, action, parameters, messageType ) {
+        //args: time, messageType, component, componentType, action, parameters
+        var time = new Date().getTime();
+        var m = (messageType === undefined) ? "user" : messageType;
+
+        var img = new Image();
+        var message = "http://simian.colorado.edu/__utm.gif?" +
+                      "time=" + time +
+                      "&messageType=" + m +
+                      "&component=" + component +
+                      "&componentType=" + componentType +
+                      "&action=" + action;
+        if ( parameters !== undefined ) {
+            for ( var i = 0; i < parameters.length; i++ ) {
+                var obj = parameters[i];
+
+                //http://stackoverflow.com/questions/1078118/how-to-iterate-over-a-json-structure
+                for ( var key in obj ) {
+                    var attributeName = key;
+                    var attributeValue = obj[key];
+                    message = message + "&parameter_" + attributeName + "=" + attributeValue;
+                }
+            }
+        }
+        img.src = message;
+    };
+
+    return Analytics;
+} );
