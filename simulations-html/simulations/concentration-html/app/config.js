b8ced449be8e92612d02fd57ea43734ceb6795f1
paulcantrell <paulcantrell@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-11-26 20:06:05 +0000

    Moved libs out of concentration and into lib/.

--- simulations-html/simulations/concentration-html/app/config.js
+++ simulations-html/simulations/concentration-html/app/config.js
@@ -4,11 +4,11 @@ require.config({
     paths: {
         common: '../common',
         
-        easel:           '../lib/easel-0.5.0',
-        image:           '../lib/image-0.2.1',
-        jquery:          '../lib/jquery-1.8.3',
-        'jquery.mobile': '../lib/jquery.mobile-1.2.0',
-        underscore:      '../lib/underscore-1.4.2',
+        easel:           '../../../contrib/easel-0.5.0',
+        image:           '../../../contrib/image-0.2.1',
+        jquery:          '../../../contrib/jquery-1.8.3',
+        'jquery.mobile': '../../../contrib/jquery.mobile-1.2.0',
+        underscore:      '../../../contrib/underscore-1.4.2',
     },
     
     shim: {
