9aba164a88fe7219f73211eb22cb217c3b2be8ed
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-11-27 18:21:00 +0000

    Formatting cleanup, no functional change.

--- simulations-html/simulations/build-an-atom-html/src/app-easel/view/ParticleView.js
+++ simulations-html/simulations/build-an-atom-html/src/app-easel/view/ParticleView.js
@@ -9,50 +9,50 @@ define( [
     // Private Methods
 
     function showPointer( mouseEvent ) {
-      $('#atom-construction-canvas').css({ cursor: "pointer" });
+        $( '#atom-construction-canvas' ).css( { cursor:"pointer" } );
     }
 
     function showDefault( mouseEvent ) {
-      $('#atom-construction-canvas').css({ cursor: "default" });
+        $( '#atom-construction-canvas' ).css( { cursor:"default" } );
     }
 
     // Constructor
 
-    function ParticleView(){
-      this.initialize.apply(this, arguments);
+    function ParticleView() {
+        this.initialize.apply( this, arguments );
     }
 
-    _.extend(ParticleView.prototype, Easel.Shape.prototype);
+    _.extend( ParticleView.prototype, Easel.Shape.prototype );
 
-    ParticleView.prototype.initialize = function( particle, mvt ){
-      Easel.Shape.prototype.initialize.call(this);
+    ParticleView.prototype.initialize = function ( particle, mvt ) {
+        Easel.Shape.prototype.initialize.call( this );
 
-      this.particle = particle;
+        this.particle = particle;
 
-      this.graphics
-        .beginStroke( "black" )
-        .beginFill( particle.color )
-        .setStrokeStyle( 1 )
-        .drawCircle( 0, 0, particle.radius )
-        .endFill();
+        this.graphics
+                .beginStroke( "black" )
+                .beginFill( particle.color )
+                .setStrokeStyle( 1 )
+                .drawCircle( 0, 0, particle.radius )
+                .endFill();
 
-      var centerInViewSpace = mvt.modelToView( new Point2D( particle.x, particle.y ) );
-      this.x = centerInViewSpace.x;
-      this.y = centerInViewSpace.y;
+        var centerInViewSpace = mvt.modelToView( new Point2D( particle.x, particle.y ) );
+        this.x = centerInViewSpace.x;
+        this.y = centerInViewSpace.y;
 
-      var self = this;
+        var self = this;
 
-      DragHandler.register(this, function(point){
-        particle.setLocation( mvt.viewToModel( point ) );
-        // self.x = point.x;
-        // self.y = point.y;
-      });
+        DragHandler.register( this, function ( point ) {
+            particle.setLocation( mvt.viewToModel( point ) );
+            // self.x = point.x;
+            // self.y = point.y;
+        } );
 
-      particle.events.on('locationChange', function(){
-        var newLocation = mvt.modelToView( new Point2D( particle.x, particle.y ) );
-        self.x = newLocation.x;
-        self.y = newLocation.y;
-      });
+        particle.events.on( 'locationChange', function () {
+            var newLocation = mvt.modelToView( new Point2D( particle.x, particle.y ) );
+            self.x = newLocation.x;
+            self.y = newLocation.y;
+        } );
 
     };
 
@@ -69,7 +69,7 @@ define( [
                 e.target.y = transformed.y + relativePressPoint.y;
             }
         };
-     };
+    };
 
     return ParticleView;
 } );
