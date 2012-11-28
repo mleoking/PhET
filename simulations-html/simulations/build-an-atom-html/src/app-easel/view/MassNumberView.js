6b80d8314cba0706148b0a0bea29526100588186
sbreed <sbreed@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-11-28 22:59:23 +0000

    wiring up atom mass number widget

--- simulations-html/simulations/build-an-atom-html/src/app-easel/view/MassNumberView.js
+++ simulations-html/simulations/build-an-atom-html/src/app-easel/view/MassNumberView.js
@@ -13,13 +13,18 @@ define([
     this.$el = $('#mass-number-container');
     this.el = this.$el[0];
 
+    var self = this;
+    this.atom.events.on('configurationChanged', function(){
+      self.render();
+    });
+
     this.render();
   }
 
   MassNumberlView.prototype.render = function(){
 
     var template = massNumberTemplate({
-      mass: this.atom.protons + this.atom.neutrons
+      mass: this.atom.getWeight()
     });
 
     this.$el.html( template );
