974b00a733910fb8ce046b6bbf54a75de5af3113
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-11-28 18:55:03 +0000

    11/28/12 add fr strings for i18n testing

---
+++ trunk/simulations-html/simulations/faraday-html/src/resources/nls/fr/faraday-html-strings.js
@@ -0,0 +1,11 @@
+// Copyright 2002-2012, University of Colorado
+
+define( {
+            "flipPolarity":"Intervertir les p\u00F4les Nord (N) et Sud (S)",
+            "magnetStrength":"Intensit\u00E9\:",
+            "resetAll":"Tout remettre \u00E0 z\u00E9ro",
+            "seeInsideMagnet":"Montrer l'int\u00E9rieur de l'aimant",
+            "showCompass":"Montrer la boussole",
+            "showField":"Montrer l'ensemble des aiguilles aimant\u00E9es",
+            "showFieldMeter":"Montrer le teslam\u00E8tre"
+        } );
