446afccfeb694e907ba13146a95fdefb531fce9a
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-11-28 21:25:05 +0000

    Reformatted.

--- simulations-html/simulations/build-an-atom-html/src/app-easel/common/Utils.js
+++ simulations-html/simulations/build-an-atom-html/src/app-easel/common/Utils.js
@@ -1,15 +1,14 @@
 // Copyright 2002-2012, University of Colorado
 
-define( [],
-        function () {
+define( [], function () {
 
-            // Not meant to be instantiated.
-            var Utils = { };
+    // Not meant to be instantiated.
+    var Utils = { };
 
-            // @return {String}
-            Utils.distanceBetweenPoints = function ( x1, y1, x2, y2 ) {
-                return Math.sqrt( ( x1 - x2 ) * ( x1 - x2 ) + ( y1 - y2 ) * ( y1 - y2 ) );
-            };
+    // @return {String}
+    Utils.distanceBetweenPoints = function ( x1, y1, x2, y2 ) {
+        return Math.sqrt( ( x1 - x2 ) * ( x1 - x2 ) + ( y1 - y2 ) * ( y1 - y2 ) );
+    };
 
-            return Utils;
-        } );
+    return Utils;
+} );
