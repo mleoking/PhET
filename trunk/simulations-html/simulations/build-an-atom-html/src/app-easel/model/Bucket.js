d37af65090edc6ce8b11db81fa91361e45c50518
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-11-29 21:42:45 +0000

    Added particle radius as parameter of bucket, started using it in placing particles.

--- trunk/simulations-html/simulations/build-an-atom-html/src/app-easel/model/Bucket.js
+++ trunk/simulations-html/simulations/build-an-atom-html/src/app-easel/model/Bucket.js
@@ -1,25 +1,27 @@
 // Copyright 2002-2012, University of Colorado
 define( [ 'underscore' ], function ( _ ) {
 
-    function Bucket( xPos, yPos, width, labelText ) {
+    function Bucket( xPos, yPos, width, particleRadius, labelText ) {
         this.x = xPos;
         this.y = yPos;
         this.width = width;
+        this.particleRadius = particleRadius;
         this.labelText = labelText;
         this.particles = [];
     }
 
-    Bucket.prototype.addParticle = function ( particle ){
+    Bucket.prototype.addParticle = function ( particle ) {
 
-        var xPos = (this.x - this.width / 2) + Math.random() * this.width;
+        var maxAcross = this.width / ( this.particleRadius * 2 ) - 1;
+        var xPos = ( this.x - this.width / 2 ) + ( this.particleRadius ) + _.random( 0, maxAcross ) * this.particleRadius * 2;
 
-        particle.setLocation( {x: xPos, y: this.y });
+        particle.setLocation( {x:xPos, y:this.y } );
         this.particles.push( particle );
         var self = this;
-        particle.events.one('particleGrabbed', function(){
+        particle.events.one( 'particleGrabbed', function () {
             self.particles = _.without( self.particles, particle );
             console.log( "particle removed from bucket" );
-        })
+        } )
     }
 
     return Bucket;
