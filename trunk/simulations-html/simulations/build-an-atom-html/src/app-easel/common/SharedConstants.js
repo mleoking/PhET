6b1225424e53ea3ff2b011bb63aba6684a4e90db
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-11-30 17:13:30 +0000

    Shared constants.

---
+++ trunk/simulations-html/simulations/build-an-atom-html/src/app-easel/common/SharedConstants.js
@@ -0,0 +1,12 @@
+// Copyright 2002-2012, University of Colorado
+define( [ ], function ( ) {
+
+    // Not meant to be instantiated.
+    var SharedConstants = {};
+
+    SharedConstants.NUCLEON_RAIDUS = 15; // In pixels.
+    SharedConstants.ELECTRON_RADIUS = 15; // In pixels.
+
+    return SharedConstants;
+
+} );
