bb45e1b15f79e25d8c98408bd819adfdc7c663ad
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-12-06 16:25:17 +0000

    put conclusions in doc

--- team/cmalley/html5/textmetrics-test/textmetrics-test.js
+++ team/cmalley/html5/textmetrics-test/textmetrics-test.js
@@ -3,6 +3,13 @@
 /**
  * Rendering test to see how closely we can determine the bounds of Canvas text.
  *
+ * Conclusions: This approximation is probably adequate for PhET's needs, but:
+ *
+ * - the results are better with some fonts (eg, Arial) than others (eg Comic Sans)
+ * - results vary by browser (hugely with some fonts, eg Comic Sans
+ * - textBaseline property works differently in different browsers, especially when using "hanging" or "ideographic"
+ * - textBaseline is supported by IE9, Firefox, Opera, Chrome, and Safari
+ *
  * @author Chris Malley (PixelZoom, Inc)
  */
 var renderText = function ( string, font, context, x, y ) {
