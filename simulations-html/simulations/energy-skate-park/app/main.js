ca2ed57876d1389b1d2e5ea264ee4954751316a7
sbreed <sbreed@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-12-06 20:51:05 +0000

    adding common Property class for managing speedControl

--- simulations-html/simulations/energy-skate-park/app/main.js
+++ simulations-html/simulations/energy-skate-park/app/main.js
@@ -9,7 +9,7 @@ require( ['websocket-refresh',
     var analytics = new Analytics();
 
     //TODO: move page creation to here, so it reads like $("body").append($(createTab("tab1")))
-    new Tab( "tab1", true, massSlider( {id: "tab1"} ), analytics );
+    window.tab1 = new Tab( "tab1", true, massSlider( {id: "tab1"} ), analytics );
     new Tab( "tab2", false, frictionControls( {id: "tab2"} ), analytics );
     new Tab( "tab3", false, frictionControls( {id: "tab3"} ), analytics );
 } );
