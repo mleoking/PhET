ffd47bcfa8542e12c979971b46a4c55ffe2a1017
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-12-07 05:23:12 +0000

    improved relationship between tab and canvas

--- simulations-html/simulations/energy-skate-park/app/view/Tab.js
+++ simulations-html/simulations/energy-skate-park/app/view/Tab.js
@@ -1,8 +1,29 @@
-define( ['underscore', 'view/EnergySkateParkCanvas'], function ( _, EnergySkateParkCanvas ) {
-    function Tab( $tab, Strings, analytics ) {
+define( ['model/skater-model', 'underscore', 'view/EnergySkateParkCanvas', 'model/physics', 'Property'], function ( SkaterModel, _, EnergySkateParkCanvas, Physics, Property ) {
+    function Tab( $tab, Easel, Strings, analytics ) {
+        var self = this;
+        self.dt = new Property( 0.02 );
         var $canvas = $tab.find( 'canvas' );
 
-        new EnergySkateParkCanvas( $canvas, Strings, analytics );
+        var skaterModel = new SkaterModel();
+        var groundHeight = 116;
+        var groundY = 768 - groundHeight;
+
+        var energySkateParkCanvas = new EnergySkateParkCanvas( $canvas, Strings, analytics, skaterModel, groundHeight, groundY );
+
+        var paused = false;
+
+        Easel.Ticker.addListener( function () {
+            if ( !paused ) {
+                var subdivisions = 1;
+                for ( var i = 0; i < subdivisions; i++ ) {
+                    Physics.updatePhysics( skaterModel, groundHeight, energySkateParkCanvas.root.splineLayer, self.dt.get() / subdivisions );
+                }
+
+//                updateFrameRate();
+                energySkateParkCanvas.root.tick();
+            }
+            energySkateParkCanvas.render();
+        } );
     }
 
     Tab.prototype.$ = function ( selector ) {
