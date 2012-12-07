0019af9f7f8559ced55f2cd8c7f5c653b0a5c279
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-12-07 20:14:44 +0000

    populate all the tabs

--- trunk/simulations-html/simulations/energy-skate-park/app/main.js
+++ trunk/simulations-html/simulations/energy-skate-park/app/main.js
@@ -2,22 +2,23 @@ require( ['util/WebsocketRefresh',
              '../../../common/phet/app/analytics/Analytics',
              'i18n!../nls/energy-skate-park-strings',
              'view/Tab',
-             'easel', 'tpl!../tab.html'
+             'easel',
+             'tpl!../tab.html'
          ], function ( WebsocketRefresh, Analytics, Strings, Tab, Easel, tabTemplate ) {
 
     WebsocketRefresh.listenForRefresh();
     var analytics = new Analytics();
 
     var $container = $( '#container' );
-    $container.append( tabTemplate( {barGraph: Strings["plots.bar-graph"],
-                                        pieChart: Strings["pieChart"],
-                                        grid: Strings["controls.show-grid"],
-                                        speed: Strings["properties.speed"]} ) );
-
-    new Tab( $( "#tab1" ), Easel, Strings, analytics ).render();
-
-    //TODO: move page creation to here, so it reads like $("body").append($(createTab("tab1")))
-//    window.tab1 = new Tab( "tab1", true, massSlider( {id: "tab1"} ), analytics );
-//    new Tab( "tab2", false, frictionControls( {id: "tab2"} ), analytics );
-//    new Tab( "tab3", false, frictionControls( {id: "tab3"} ), analytics );
+    var tabs = ["tab1", "tab2", "tab3"];
+    for ( var i = 0; i < tabs.length; i++ ) {
+        var tab = tabs[i];
+        $container.append( tabTemplate( {id: tab,
+                                            barGraph: Strings["plots.bar-graph"],
+                                            pieChart: Strings["pieChart"],
+                                            grid: Strings["controls.show-grid"],
+                                            speed: Strings["properties.speed"]} ) );
+        var $tab = $( "#" + tab );
+        new Tab( $tab, Easel, Strings, analytics ).render();
+    }
 } );
