95fb664dd932a99270fabf05a9a0704a683bdd48
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-12-07 03:58:45 +0000

    added tabs

--- trunk/simulations-html/simulations/energy-skate-park/app/main-jquerymobile.js
+++ trunk/simulations-html/simulations/energy-skate-park/app/main-jquerymobile.js
@@ -8,9 +8,8 @@ require( ['websocket-refresh',
     WebsocketRefresh.listenForRefresh();
     var analytics = new Analytics();
 
-
     //TODO: move page creation to here, so it reads like $("body").append($(createTab("tab1")))
-//    window.tab1 = new Tab( "tab1", true, massSlider( {id: "tab1"} ), analytics );
-//    new Tab( "tab2", false, frictionControls( {id: "tab2"} ), analytics );
-//    new Tab( "tab3", false, frictionControls( {id: "tab3"} ), analytics );
+    window.tab1 = new Tab( "tab1", true, massSlider( {id: "tab1"} ), analytics );
+    new Tab( "tab2", false, frictionControls( {id: "tab2"} ), analytics );
+    new Tab( "tab3", false, frictionControls( {id: "tab3"} ), analytics );
 } );
