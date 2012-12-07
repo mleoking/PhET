b167286f6758fe00e4bef1e46ca6d6f9f92da0de
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-12-07 04:13:48 +0000

    minor changes

--- trunk/simulations-html/simulations/energy-skate-park/app/main.js
+++ trunk/simulations-html/simulations/energy-skate-park/app/main.js
@@ -2,12 +2,15 @@ require( ['websocket-refresh',
              'tab',
              "tpl!view/massSlider.html",
              "tpl!view/frictionControls.html",
-             '../../../common/phet/app/analytics/Analytics'
-         ], function ( WebsocketRefresh, Tab, massSlider, frictionControls, Analytics ) {
+             '../../../common/phet/app/analytics/Analytics',
+             'i18n!../nls/energy-skate-park-strings',
+             'view/Tab'
+         ], function ( WebsocketRefresh, Tabold, massSlider, frictionControls, Analytics, Strings, Tab ) {
 
     WebsocketRefresh.listenForRefresh();
     var analytics = new Analytics();
 
+    var tab1 = new Tab( Strings );
 
     //TODO: move page creation to here, so it reads like $("body").append($(createTab("tab1")))
 //    window.tab1 = new Tab( "tab1", true, massSlider( {id: "tab1"} ), analytics );
