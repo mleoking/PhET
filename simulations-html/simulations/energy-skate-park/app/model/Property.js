c2a13bd44c1756086c72a627be13e716d51adb0d
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-12-08 04:16:06 +0000

    changed property to use prototype

--- simulations-html/simulations/energy-skate-park/app/model/Property.js
+++ simulations-html/simulations/energy-skate-park/app/model/Property.js
@@ -21,94 +21,77 @@ define( [], function () {
     function Property( value ) {
 
         // Variables declared in the constructor are private.
-        var _value = value;
-        var _initialValue = value;
-        var _observers = [];
-
-
-        /**
-         * Gets the value.
-         * @return {*}
-         */
-        this.get = function () {
-            return _value;
-        };
-
-        /**
-         * Sets the value and notifies registered observers.
-         * If the value hasn't changed, this is a no-op.
-         *
-         * @param value
-         */
-
-            // TODO: this should be refactored to use jQuery custom events.
-        this.set = function ( value ) {
-            if ( value !== _value ) {
-                var oldValue = _value;
-                _value = value;
-                var observersCopy = _observers.slice(); // make a copy, in case notification results in removeObserver
-                for ( var i = 0; i < observersCopy.length; i++ ) {
-                    observersCopy[i]( value, oldValue );
-                }
-            }
-        };
-
-        /**
-         * Resets the value to the initial value.
-         */
-        this.reset = function () {
-            this.set( _initialValue );
-        };
-
-        /**
-         * Adds an observer.
-         * If observer is already registered, this is a no-op.
-         *
-         * @param observer a function of the form observer(newValue,oldValue)
-         */
-        this.addObserver = function ( observer ) {
-            if ( _observers.indexOf( observer ) === -1 ) {
-                _observers.push( observer );
-            }
-        };
-
-        /**
-         * Adds an observer and sends a notification message with the current value.
-         * TODO: Discuss with team whether this should be merged into addObserver
-         * @param observer
-         */
-        this.observe = function ( observer ) {
-            this.addObserver( observer );
-            observer( this.get(), this.get() );
-        };
-
-        /**
-         * Removes an observer.
-         * If observer is not registered, this is a no-op.
-         *
-         * @param observer
-         */
-        this.removeObserver = function ( observer ) {
-            var index = _observers.indexOf( observer );
-            if ( index !== -1 ) {
-                _observers.splice( index, index + 1 );
-            }
-        };
+        this._value = value;
+        this._initialValue = value;
+        this._observers = [];
     }
 
-    //TODO is this a good way of co-locating small tests?
-    // test
-    Property.prototype.test = function () {
-        var name = new Property( "Moe" );
-        var observer = function ( newName, oldName ) {
-            console.log( "newName=" + newName + ", oldName=" + oldName );
-        };
-        name.addObserver( observer );
-        name.set( "Larry" ); // observer should be notified
-        name.removeObserver( observer );
-        name.set( "Curly" ); // this should result in no notification
+    /**
+     * Gets the value.
+     * @return {*}
+     */
+    Property.prototype.get = function () {
+        return this._value;
+    };
+
+    /**
+     * Sets the value and notifies registered observers.
+     * If the value hasn't changed, this is a no-op.
+     *
+     * @param value
+     */
+
+        // TODO: this should be refactored to use jQuery custom events.
+    Property.prototype.set = function ( value ) {
+        if ( value !== _value ) {
+            var oldValue = this._value;
+            this._value = value;
+            var observersCopy = this._observers.slice(); // make a copy, in case notification results in removeObserver
+            for ( var i = 0; i < observersCopy.length; i++ ) {
+                observersCopy[i]( value, oldValue );
+            }
+        }
+    };
+
+    /**
+     * Resets the value to the initial value.
+     */
+    Property.prototype.reset = function () {this.set( this._initialValue );};
+
+    /**
+     * Adds an observer.
+     * If observer is already registered, this is a no-op.
+     *
+     * @param observer a function of the form observer(newValue,oldValue)
+     */
+    Property.prototype.addObserver = function ( observer ) {
+        if ( this._observers.indexOf( observer ) === -1 ) {
+            this._observers.push( observer );
+        }
+    };
+
+    /**
+     * Adds an observer and sends a notification message with the current value.
+     * TODO: Discuss with team whether this should be merged into addObserver
+     * @param observer
+     */
+    Property.prototype.observe = function ( observer ) {
+        this.addObserver( observer );
+        observer( this.get(), this.get() );
+    };
+
+    /**
+     * Removes an observer.
+     * If observer is not registered, this is a no-op.
+     *
+     * @param observer
+     */
+    Property.prototype.removeObserver = function ( observer ) {
+        var index = this._observers.indexOf( observer );
+        if ( index !== -1 ) {
+            this._observers.splice( index, index + 1 );
+        }
     };
 
     return Property;
 } );
-
