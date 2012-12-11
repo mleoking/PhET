bd6cdf36117eb122f15343dd01e6bce7b147549b
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-12-11 17:11:09 +0000

    simplify example

--- trunk/team/cmalley/html5/require-include-test/simulations/mysim/js/main.js
+++ trunk/team/cmalley/html5/require-include-test/simulations/mysim/js/main.js
@@ -1,18 +1,13 @@
 // Copyright 2002-2012, University of Colorado
 
-/*
- * This function is called when scripts/helper/util.js is loaded.
- * If util.js calls define(), then this function is not fired until util's dependencies have loaded,
- * and the util argument will hold the module value for "helper/util".
- */
 require( [
-             "../../../common/mycommon/js/model/Model",
-             "../../../common/mycommon/js/util/Person"
+             "model/Model",
+             "../js/model/Person"
          ],
          function ( Model, Person ) {
 
              var model = new Model( "joe", "plumber" );
-             console.log( "Do we still have a Person in main? : " + ( this.person instanceof Person ) );
+             console.log( "Do we still have a Person in main? : " + ( model.person instanceof Person ) );
 
              if ( model.person instanceof Person ) {
                  model.person.printName();
