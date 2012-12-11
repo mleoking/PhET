8212aa11b4d996a5abb289a334faaa9f02a88567
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-12-11 17:22:48 +0000

    simplify more

--- trunk/team/cmalley/html5/require-include-test/js/main.js
+++ trunk/team/cmalley/html5/require-include-test/js/main.js
@@ -1,18 +1,18 @@
 // Copyright 2002-2012, University of Colorado
 
 require( [
-             "model/Model",
+             "model/Contact",
              "../js/model/Person"
          ],
-         function ( Model, Person ) {
+         function ( Contact, Person ) {
 
-             var model = new Model( "joe", "plumber" );
-             console.log( "Do we still have a Person in main? : " + ( model.person instanceof Person ) );
+             var contact = new Contact( "joe", "plumber" );
+             console.log( "Do we still have a Person in main? : " + ( contact.person instanceof Person ) );
 
-             if ( model.person instanceof Person ) {
-                 model.person.printName();
+             if ( contact.person instanceof Person ) {
+                 contact.person.printName();
              }
              else {
-                 console.log( "ERROR: model.person has wrong type: " + typeof( model.person ) );
+                 console.log( "ERROR: contact.person has wrong type: " + typeof( contact.person ) );
              }
          } );
