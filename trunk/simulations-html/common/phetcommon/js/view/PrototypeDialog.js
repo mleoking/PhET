fb668cca90ddf3245e33978e7ad38b1571477746
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-12-11 04:30:20 +0000

    improved template loader

--- trunk/simulations-html/common/phetcommon/js/view/PrototypeDialog.js
+++ trunk/simulations-html/common/phetcommon/js/view/PrototypeDialog.js
@@ -1,5 +1,5 @@
 //Code for handling the "this is a prototype" dialog for the Dec 2012 newsletter.
-define( ['tpl!../../../phetcommon/html/prototype-dialog.html'], function ( template ) {
+define( ['tpl!phetcommon_html/prototype-dialog.html'], function ( template ) {
     return {init: function ( simName ) {
         $( template( {simName: simName} ) ).appendTo( $( "body" ) );
         $( '.prototype-dialog-ok-button' ).click( function () {
