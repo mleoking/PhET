1fbcf598395e078514a2fd929e4aecf56e5fb32e
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-12-11 18:04:33 +0000

    add a note about phetcommon_html path

--- simulations-html/common/phetcommon/js/view/PrototypeDialog.js
+++ simulations-html/common/phetcommon/js/view/PrototypeDialog.js
@@ -1,4 +1,5 @@
 //Code for handling the "this is a prototype" dialog for the Dec 2012 newsletter.
+//NOTE: path phetcommon_html must be defined by the client.
 define( ['tpl!phetcommon_html/prototype-dialog.html'], function ( template ) {
     return {init: function ( simName ) {
         $( template( {simName: simName} ) ).appendTo( $( "body" ) );
