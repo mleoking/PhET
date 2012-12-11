7fe1b19eeaf0a8ab4a45fd5ff9bbc0b75be1f3e2
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-12-11 04:24:32 +0000

    fixed signature

--- simulations-html/common/phetcommon/js/view/PrototypeDialog.js
+++ simulations-html/common/phetcommon/js/view/PrototypeDialog.js
@@ -1,7 +1,7 @@
 //Code for handling the "this is a prototype" dialog for the Dec 2012 newsletter.
 define( ['tpl!../../../phetcommon/html/prototype-dialog.html'], function ( template ) {
-    return {init: function ( simName, url ) {
-        $( template( {simName: simName, url: url} ) ).appendTo( $( "body" ) );
+    return {init: function ( simName ) {
+        $( template( {simName: simName} ) ).appendTo( $( "body" ) );
         $( '.prototype-dialog-ok-button' ).click( function () {
             $( '.prototype-overlay' ).hide();
         } );
