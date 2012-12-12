6925824641f66171277de03d55a494e88bdfc198
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-12-12 23:04:03 +0000

    Made the plus sign appear in charge indicator for positive charge values.

--- simulations-html/simulations/build-an-atom-html/js/view/NetChargeView.js
+++ simulations-html/simulations/build-an-atom-html/js/view/NetChargeView.js
@@ -1,36 +1,41 @@
 // Copyright 2002-2012, University of Colorado
-define([
-  'underscore',
-  'tpl!templates/net-charge.html'
-], function( _, netChargeTemplate ){
+define( [
+            'underscore',
+            'tpl!templates/net-charge.html'
+        ], function ( _, netChargeTemplate ) {
 
-  function NetChargeView( atom ){
-    this.atom = atom;
+    function NetChargeView( atom ) {
 
-    if( !this.atom ){
-      throw new Error('No atom supplied.');
+        // Parameter checking.
+        if ( !atom ) {
+            throw new Error( 'No atom supplied.' );
+        }
+        this.atom = atom;
+
+        // Grab the div out of the DOM.
+        this.$el = $( '#net-charge-container' );
+        this.el = this.$el[0];
+
+        // Set up to redraw when atom configuration changes.
+        var self = this;
+        this.atom.events.on( 'configurationChanged', function () {
+            self.render();
+        } );
+
+        this.render();
     }
 
-    this.$el = $('#net-charge-container');
-    this.el = this.$el[0];
+    NetChargeView.prototype.render = function () {
 
-    var self = this;
-    this.atom.events.on('configurationChanged', function(){
-        console.log( "self.atom.getCharge()" + self.atom.getCharge() );
-      self.render();
-    });
+        var charge = this.atom.getCharge();
+        var chargeIndicator = this.atom.getCharge() > 0 ? "+" : "";
 
-    this.render();
-  }
+        var template = netChargeTemplate( {
+                                              charge:chargeIndicator + this.atom.getCharge()
+                                          } );
 
-  NetChargeView.prototype.render = function(){
+        this.$el.html( template );
+    };
 
-    var template = netChargeTemplate({
-      charge: this.atom.getCharge()
-    });
-
-    this.$el.html( template );
-  };
-
-  return NetChargeView;
-});
+    return NetChargeView;
+} );
