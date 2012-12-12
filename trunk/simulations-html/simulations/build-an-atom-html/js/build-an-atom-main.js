06f2b678ac877217cceab1336b53b74bc56a2fbd
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-12-12 22:12:20 +0000

    Integrated prototype dialog.

--- trunk/simulations-html/simulations/build-an-atom-html/js/build-an-atom-main.js
+++ trunk/simulations-html/simulations/build-an-atom-html/js/build-an-atom-main.js
@@ -2,22 +2,29 @@
 require( [
              'underscore',
              'easel',
+             'phetcommon/view/PrototypeDialog',
              'view/BuildAnAtomStage',
              'model/BuildAnAtomModel',
              'view/SymbolView',
              'view/MassNumberView',
              'view/PeriodicTableView'
-         ], function ( _, Easel, BuildAnAtomStage, BuildAnAtomModel, SymbolView, MassNumberView, PeriodicTablelView ) {
+         ], function ( _, Easel, PrototypeDialog, BuildAnAtomStage, BuildAnAtomModel, SymbolView, MassNumberView, PeriodicTablelView ) {
 
+    // Create the model.
     var buildAnAtomModel = new BuildAnAtomModel();
+
+    // Create the canvas where the user will construct the atoms.
     window.buildAnAtomStage = new BuildAnAtomStage( document.getElementById( 'atom-construction-canvas' ), buildAnAtomModel );
 
+    // Display the "prototype" dialog.  TODO: This is temporary.
+    PrototypeDialog.init( "Build an Atom" );
+
+    // Create the widgets that will display various information about the constructed atom.
     $( document ).ready( function () {
-
         var atom = buildAnAtomModel.atom;
-
         var symbolWidget = new SymbolView( atom );
         var massNumberWidget = new MassNumberView( atom );
         var periodicTableWidget = new PeriodicTablelView( atom );
     } );
+
 } );
