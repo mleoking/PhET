b547a393cbb40de3413380b65913fe953aef6230
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-12-12 03:27:17 +0000

    add a view component that adjusts itself to fill the canvas as the browser window is resized

---
+++ team/cmalley/html5/canvas-resize/js/view/GridDisplay.js
@@ -0,0 +1,43 @@
+/**
+ * Displays a grid of red squares.
+ * Assumes that the origin is in the center of the canvas.
+ */
+define( [ 'easel' ],
+        function ( Easel ) {
+
+            function GridDisplay( mvt, canvasWidth, canvasHeight ) {
+
+                Easel.Container.call( this );
+
+                var SIDE_LENGTH = 10;
+                var X_SPACING = 3 * SIDE_LENGTH;
+                var Y_SPACING = X_SPACING;
+                this.update = function ( canvasWidth, canvasHeight ) {
+
+                    this.removeAllChildren();
+
+                    // create a grid of red rectangles
+                    var gridWidth = canvasWidth + X_SPACING;
+                    var gridHeight = canvasHeight + Y_SPACING;
+                    var y = -gridHeight/2;
+                    while ( y <= gridHeight/2 ) {
+                        var x = -gridWidth/2;
+                        while ( x <= gridWidth/2 ) {
+                            var needle = new Easel.Shape();
+                            needle.graphics.beginFill( 'red' );
+                            needle.graphics.rect( -SIDE_LENGTH/2, -SIDE_LENGTH/2, SIDE_LENGTH, SIDE_LENGTH );
+                            needle.x = x;
+                            needle.y = y;
+                            this.addChild( needle );
+                            x += X_SPACING;
+                        }
+                        y += Y_SPACING;
+                    }
+                }
+                this.update( canvasWidth, canvasHeight );
+            }
+
+            GridDisplay.prototype = new Easel.Container();
+
+            return GridDisplay;
+        } );
