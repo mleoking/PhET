91fea01008c2f306071d5a63609eb7faf9ff0e2c
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-12-13 16:09:33 +0000

    make OK button larger, click on anything to dismiss

--- simulations-html/common/phetcommon/js/view/PrototypeDialog.js
+++ simulations-html/common/phetcommon/js/view/PrototypeDialog.js
@@ -3,7 +3,7 @@
 define( ['tpl!phetcommon_html/prototype-dialog.html'], function ( template ) {
     return {init: function ( simName ) {
         $( template( {simName: simName} ) ).appendTo( $( "body" ) );
-        $( '#prototype-dialog-ok-button' ).click( function () {
+        $( '#prototype-dialog-overlay' ).click( function () {
             $( '#prototype-dialog-overlay' ).hide();
         } );
     }};
