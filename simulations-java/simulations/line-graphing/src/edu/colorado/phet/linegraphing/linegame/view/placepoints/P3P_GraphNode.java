115820e89cef5343754e79bc02688bf4eb26d8c8
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-12-21 21:26:44 +0000

    refactor P3P challenges, move functionality to base classes, add support for slope-intercept

--- simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/linegame/view/placepoints/P3P_GraphNode.java
+++ simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/linegame/view/placepoints/P3P_GraphNode.java
@@ -4,15 +4,23 @@ package edu.colorado.phet.linegraphing.linegame.view.placepoints;
 import java.awt.Color;
 
 import edu.colorado.phet.common.phetcommon.application.PhetApplication;
+import edu.colorado.phet.common.phetcommon.model.property.Property;
+import edu.colorado.phet.common.phetcommon.simsharing.messages.UserComponentTypes;
+import edu.colorado.phet.common.phetcommon.util.DoubleRange;
+import edu.colorado.phet.common.phetcommon.util.RichSimpleObserver;
 import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
+import edu.colorado.phet.linegraphing.common.LGColors;
+import edu.colorado.phet.linegraphing.common.LGSimSharing.UserComponents;
 import edu.colorado.phet.linegraphing.common.model.Graph;
 import edu.colorado.phet.linegraphing.common.model.Line;
 import edu.colorado.phet.linegraphing.common.view.GraphNode;
 import edu.colorado.phet.linegraphing.common.view.LineNode;
-import edu.colorado.phet.linegraphing.common.view.SlopeToolNode;
-import edu.colorado.phet.linegraphing.linegame.model.graphtheline.GTL_Challenge;
+import edu.colorado.phet.linegraphing.common.view.manipulator.LineManipulatorNode;
+import edu.colorado.phet.linegraphing.common.view.manipulator.PointDragHandler;
+import edu.colorado.phet.linegraphing.linegame.LineGameConstants;
 import edu.colorado.phet.linegraphing.linegame.model.placepoints.P3P_Challenge;
 import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolox.nodes.PComposite;
 
 /**
  * Base class for the graph node in all "Place 3 Points" (P3P) challenges.
@@ -21,9 +29,10 @@ import edu.umd.cs.piccolo.PNode;
  */
 public abstract class P3P_GraphNode extends GraphNode {
 
-    private final PNode linesParent, manipulatorsParent;
+    private final LineNode answerNode;
+    private final LineManipulatorNode p1ManipulatorNode, p2ManipulatorNode, p3ManipulatorNode;
 
-    public P3P_GraphNode( P3P_Challenge challenge ) {
+    public P3P_GraphNode( final P3P_Challenge challenge ) {
         super( challenge.graph, challenge.mvt );
 
         // To reduce brain damage during development, show the answer as a translucent gray line.
@@ -33,21 +42,71 @@ public abstract class P3P_GraphNode extends GraphNode {
             addChild( answerNode );
         }
 
-        // parent nodes, for maintaining rendering order
-        linesParent = new PNode();
-        manipulatorsParent = new PNode();
+        // parent for the guess node, to maintain rendering order
+        final PNode guessNodeParent = new PComposite();
 
-        // rendering order
-        addChild( linesParent );
-        addChild( manipulatorsParent );
+        // the correct answer, initially hidden
+        answerNode = createAnswerLineNode( challenge.answer, challenge.graph, challenge.mvt );
+        answerNode.setEquationVisible( false );
+        answerNode.setVisible( false );
+
+        final double manipulatorDiameter = challenge.mvt.modelToViewDeltaX( LineGameConstants.MANIPULATOR_DIAMETER );
+
+        // p1 manipulator
+        p1ManipulatorNode = new LineManipulatorNode( manipulatorDiameter, LGColors.POINT_1 );
+        p1ManipulatorNode.addInputEventListener( new PointDragHandler( UserComponents.p1Manipulator, UserComponentTypes.sprite,
+                                                                       p1ManipulatorNode, challenge.mvt,
+                                                                       challenge.p1, new Property[] { challenge.p2, challenge.p3 },
+                                                                       new Property<DoubleRange>( new DoubleRange( challenge.graph.xRange ) ),
+                                                                       new Property<DoubleRange>( new DoubleRange( challenge.graph.yRange ) ) ) );
+
+        // p1 manipulator
+        p2ManipulatorNode = new LineManipulatorNode( manipulatorDiameter, LGColors.POINT_2 );
+        p2ManipulatorNode.addInputEventListener( new PointDragHandler( UserComponents.p2Manipulator, UserComponentTypes.sprite,
+                                                                       p2ManipulatorNode, challenge.mvt,
+                                                                       challenge.p2, new Property[] { challenge.p1, challenge.p3 },
+                                                                       new Property<DoubleRange>( new DoubleRange( challenge.graph.xRange ) ),
+                                                                       new Property<DoubleRange>( new DoubleRange( challenge.graph.yRange ) ) ) );
+
+        // p3 manipulator
+        p3ManipulatorNode = new LineManipulatorNode( manipulatorDiameter, LGColors.POINT_3 );
+        p3ManipulatorNode.addInputEventListener( new PointDragHandler( UserComponents.p3Manipulator, UserComponentTypes.sprite,
+                                                                       p3ManipulatorNode, challenge.mvt,
+                                                                       challenge.p3, new Property[] { challenge.p1, challenge.p2 },
+                                                                       new Property<DoubleRange>( new DoubleRange( challenge.graph.xRange ) ),
+                                                                       new Property<DoubleRange>( new DoubleRange( challenge.graph.yRange ) ) ) );
+
+        // Rendering order
+        addChild( guessNodeParent );
+        addChild( answerNode );
+        addChild( p1ManipulatorNode );
+        addChild( p2ManipulatorNode );
+        addChild( p3ManipulatorNode );
+
+        // Show the user's current guess
+        final RichSimpleObserver pointsObserver = new RichSimpleObserver() {
+            public void update() {
+
+                // draw the line
+                guessNodeParent.removeAllChildren();
+                if ( challenge.guess.get() != null ) {
+                    LineNode guessNode = createGuessLineNode( challenge.guess.get(), challenge.graph, challenge.mvt );
+                    guessNode.setEquationVisible( false );
+                    guessNodeParent.addChild( guessNode );
+                }
+
+                // move the manipulators
+                p1ManipulatorNode.setOffset( challenge.mvt.modelToView( challenge.p1.get().x, challenge.p1.get().y ) );
+                p2ManipulatorNode.setOffset( challenge.mvt.modelToView( challenge.p2.get().x, challenge.p2.get().y ) );
+                p3ManipulatorNode.setOffset( challenge.mvt.modelToView( challenge.p3.get().x, challenge.p3.get().y ) );
+            }
+        };
+        pointsObserver.observe( challenge.p1, challenge.p2, challenge.p3 );
     }
 
-    protected void addLineNode( PNode node ) {
-        linesParent.addChild( node );
-    }
-
-    protected void addManipulatorNode( PNode node ) {
-        manipulatorsParent.addChild( node );
+    // Sets the visibility of the correct answer.
+    public void setAnswerVisible( boolean visible ) {
+        answerNode.setVisible( visible );
     }
 
     // Creates the node that corresponds to the "answer" line.
@@ -55,7 +114,4 @@ public abstract class P3P_GraphNode extends GraphNode {
 
     // Creates the node that corresponds to the "guess" line.
     protected abstract LineNode createGuessLineNode( Line line, Graph graph, ModelViewTransform mvt );
-
-    // Changes the visibility of the "answer" line.
-    public abstract void setAnswerVisible( boolean visible );
 }
--- simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/linegame/view/placepoints/P3P_PS_ChallengeNode.java
+++ simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/linegame/view/placepoints/P3P_PS_ChallengeNode.java
@@ -1,41 +1,19 @@
 // Copyright 2002-2012, University of Colorado
 package edu.colorado.phet.linegraphing.linegame.view.placepoints;
 
-import java.awt.BasicStroke;
 import java.awt.Color;
-import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.geom.Rectangle2D;
-import java.text.MessageFormat;
 
 import edu.colorado.phet.common.games.GameAudioPlayer;
-import edu.colorado.phet.common.phetcommon.model.property.BooleanProperty;
-import edu.colorado.phet.common.phetcommon.util.function.VoidFunction0;
-import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
-import edu.colorado.phet.common.piccolophet.nodes.FaceNode;
-import edu.colorado.phet.common.piccolophet.nodes.PhetPText;
-import edu.colorado.phet.common.piccolophet.nodes.TextButtonNode;
-import edu.colorado.phet.linegraphing.common.LGResources.Images;
-import edu.colorado.phet.linegraphing.common.LGResources.Strings;
 import edu.colorado.phet.linegraphing.common.model.Line;
-import edu.colorado.phet.linegraphing.common.view.EquationNode;
-import edu.colorado.phet.linegraphing.common.view.PointToolNode;
-import edu.colorado.phet.linegraphing.linegame.LineGameConstants;
 import edu.colorado.phet.linegraphing.linegame.model.LineGameModel;
-import edu.colorado.phet.linegraphing.linegame.model.LineGameModel.PlayState;
 import edu.colorado.phet.linegraphing.linegame.model.placepoints.P3P_Challenge;
-import edu.colorado.phet.linegraphing.linegame.view.EquationBoxNode;
 import edu.colorado.phet.linegraphing.pointslope.view.PointSlopeEquationNode;
 import edu.umd.cs.piccolo.PNode;
-import edu.umd.cs.piccolo.nodes.PImage;
-import edu.umd.cs.piccolo.nodes.PText;
 import edu.umd.cs.piccolo.util.PDimension;
 
 /**
- * View component for a "Place 3 Points" (P3P) challenge.
- * Given an equation in point-slope (PS) form, place 3 points on the line.
+ * View component for a "Place 3 Points" (P3P) challenge with equations in point-slope (PS) form.
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
@@ -43,224 +21,15 @@ public class P3P_PS_ChallengeNode extends P3P_ChallengeNode {
 
     public P3P_PS_ChallengeNode( final LineGameModel model, final P3P_Challenge challenge, final GameAudioPlayer audioPlayer, PDimension challengeSize ) {
         super( model, challenge, audioPlayer, challengeSize );
-
-        PNode titleNode = new PhetPText( challenge.title, LineGameConstants.TITLE_FONT, LineGameConstants.TITLE_COLOR );
-
-        final PDimension boxSize = new PDimension( 0.35 * challengeSize.getWidth(), 0.2 * challengeSize.getHeight() );
-
-        // The equation for the answer.
-        final PNode answerBoxNode = new EquationBoxNode( Strings.LINE_TO_GRAPH, challenge.answer.color, boxSize,
-                                                         createEquationNode( challenge.answer, LineGameConstants.STATIC_EQUATION_FONT, challenge.answer.color ) );
-
-        // The equation for the current guess will be a child of this node, to maintain rendering order.
-        final PNode guessEquationParent = new PNode();
-        guessEquationParent.setVisible( false );
-
-        // icons for indicating correct vs incorrect
-        final PNode answerCorrectNode = new PImage( Images.CHECK_MARK );
-        final PNode guessCorrectNode = new PImage( Images.CHECK_MARK );
-        final PNode guessIncorrectNode = new PImage( Images.X_MARK );
-
-        final P3P_GraphNode graphNode = createGraphNode( challenge );
-
-        final FaceNode faceNode = new FaceNode( LineGameConstants.FACE_DIAMETER, LineGameConstants.FACE_COLOR,
-                                                new BasicStroke( 1f ), LineGameConstants.FACE_COLOR.darker(), Color.BLACK, Color.BLACK );
-
-        final PText pointsAwardedNode = new PhetPText( "", LineGameConstants.POINTS_AWARDED_FONT, LineGameConstants.POINTS_AWARDED_COLOR );
-
-        // Buttons
-        final Font buttonFont = LineGameConstants.BUTTON_FONT;
-        final Color buttonForeground = LineGameConstants.BUTTON_COLOR;
-        final TextButtonNode checkButton = new TextButtonNode( Strings.CHECK, buttonFont, buttonForeground );
-        final TextButtonNode tryAgainButton = new TextButtonNode( Strings.TRY_AGAIN, buttonFont, buttonForeground );
-        final TextButtonNode showAnswerButton = new TextButtonNode( Strings.SHOW_ANSWER, buttonFont, buttonForeground );
-        final TextButtonNode nextButton = new TextButtonNode( Strings.NEXT, buttonFont, buttonForeground );
-
-        // Point tools
-        Rectangle2D pointToolDragBounds = new Rectangle2D.Double( 0, 0, challengeSize.getWidth(), challengeSize.getHeight() );
-        PointToolNode pointToolNode1 = new PointToolNode( challenge.pointTool1, challenge.mvt, challenge.graph, pointToolDragBounds, new BooleanProperty( true ) );
-        PointToolNode pointToolNode2 = new PointToolNode( challenge.pointTool2, challenge.mvt, challenge.graph, pointToolDragBounds, new BooleanProperty( true ) );
-        pointToolNode1.scale( LineGameConstants.POINT_TOOL_SCALE );
-        pointToolNode2.scale( LineGameConstants.POINT_TOOL_SCALE );
-
-        // Point tools moveToFront when dragged, so we give them a common parent to preserve rendering order of the reset of the scenegraph.
-        PNode pointToolParent = new PNode();
-        pointToolParent.addChild( pointToolNode1 );
-        pointToolParent.addChild( pointToolNode2 );
-
-        // rendering order
-        {
-            addChild( titleNode );
-            addChild( answerBoxNode );
-            addChild( answerCorrectNode );
-            addChild( guessEquationParent );
-            addChild( graphNode );
-            addChild( checkButton );
-            addChild( tryAgainButton );
-            addChild( showAnswerButton );
-            addChild( nextButton );
-            addChild( answerCorrectNode );
-            addChild( guessCorrectNode );
-            addChild( guessIncorrectNode );
-            addChild( pointToolParent );
-            addChild( faceNode );
-            addChild( pointsAwardedNode );
-        }
-
-        // layout
-        final int iconXMargin = 10;
-        final int iconYMargin = 5;
-        {
-            // title centered at top
-            titleNode.setOffset( ( challengeSize.getWidth() / 2 ) - ( titleNode.getFullBoundsReference().getWidth() / 2 ),
-                                 10 );
-
-            // equation in left half of challenge space
-            answerBoxNode.setOffset( ( challengeSize.getWidth() / 2 ) - answerBoxNode.getFullBoundsReference().getWidth() - 40,
-                                     graphNode.getFullBoundsReference().getMinY() + 70 );
-            // correct/incorrect icons in upper-right corner of equation boxes
-            answerCorrectNode.setOffset( answerBoxNode.getFullBoundsReference().getMaxX() - answerCorrectNode.getFullBoundsReference().getWidth() - iconXMargin,
-                                         answerBoxNode.getFullBoundsReference().getMinY() + iconYMargin );
-            // graphNode is positioned automatically based on mvt's origin offset.
-            // buttons centered at bottom of challenge space
-            final double ySpacing = 15;
-            final double buttonCenterX = ( challengeSize.getWidth() / 2 );
-            final double buttonCenterY = graphNode.getFullBoundsReference().getMaxY() + ySpacing;
-            checkButton.setOffset( buttonCenterX - ( checkButton.getFullBoundsReference().getWidth() / 2 ), buttonCenterY );
-            tryAgainButton.setOffset( buttonCenterX - ( tryAgainButton.getFullBoundsReference().getWidth() / 2 ), buttonCenterY );
-            showAnswerButton.setOffset( buttonCenterX - ( showAnswerButton.getFullBoundsReference().getWidth() / 2 ), buttonCenterY );
-            nextButton.setOffset( buttonCenterX - ( nextButton.getFullBoundsReference().getWidth() / 2 ), buttonCenterY );
-
-            // face centered below equation boxes
-            faceNode.setOffset( answerBoxNode.getFullBoundsReference().getCenterX() - ( faceNode.getFullBoundsReference().getWidth() / 2 ),
-                                checkButton.getFullBoundsReference().getMaxY() - faceNode.getFullBoundsReference().getHeight() );
-        }
-
-        // Update visibility of the correct/incorrect icons.
-        final VoidFunction0 updateIcons = new VoidFunction0() {
-            public void apply() {
-                answerCorrectNode.setVisible( model.state.get() == PlayState.NEXT );
-                guessCorrectNode.setVisible( answerCorrectNode.getVisible() && challenge.isCorrect() );
-                guessIncorrectNode.setVisible( answerCorrectNode.getVisible() && !challenge.isCorrect() );
-            }
-        };
-
-        // Function that keeps the guess equation updated as the user manipulates the points.
-        challenge.guess.addObserver( new VoidFunction1<Line>() {
-            public void apply( Line line ) {
-
-                // update the equation
-                guessEquationParent.removeAllChildren();
-                guessEquationParent.addChild( new EquationBoxNode( Strings.YOUR_LINE, LineGameConstants.GUESS_COLOR, boxSize,
-                                                                   createEquationNode( line, LineGameConstants.STATIC_EQUATION_FONT, LineGameConstants.GUESS_COLOR ) ) );
-                guessEquationParent.setOffset( answerBoxNode.getXOffset(), answerBoxNode.getFullBoundsReference().getMaxY() + 20 );
-
-                // keep icons in correct place on box
-                guessCorrectNode.setOffset( guessEquationParent.getFullBoundsReference().getMaxX() - guessCorrectNode.getFullBoundsReference().getWidth() - iconXMargin,
-                                            guessEquationParent.getFullBoundsReference().getMinY() + iconYMargin );
-                guessIncorrectNode.setOffset( guessEquationParent.getFullBoundsReference().getMaxX() - guessIncorrectNode.getFullBoundsReference().getWidth() - iconXMargin,
-                                              guessEquationParent.getFullBoundsReference().getMinY() + iconYMargin );
-
-                // make relevant icons visible
-                updateIcons.apply();
-            }
-        } );
-
-
-        // state changes
-        model.state.addObserver( new VoidFunction1<PlayState>() {
-            public void apply( PlayState state ) {
-
-                // visibility of face
-                faceNode.setVisible( state == PlayState.TRY_AGAIN ||
-                                     state == PlayState.SHOW_ANSWER ||
-                                     ( state == PlayState.NEXT && challenge.isCorrect() ) );
-
-                // visibility of points
-                pointsAwardedNode.setVisible( faceNode.getVisible() && challenge.isCorrect() );
-
-                // visibility of buttons
-                checkButton.setVisible( state == PlayState.FIRST_CHECK || state == PlayState.SECOND_CHECK );
-                tryAgainButton.setVisible( state == PlayState.TRY_AGAIN );
-                showAnswerButton.setVisible( state == PlayState.SHOW_ANSWER );
-                nextButton.setVisible( state == PlayState.NEXT );
-
-                // states in which the graph is interactive
-                graphNode.setPickable( state == PlayState.FIRST_CHECK || state == PlayState.SECOND_CHECK || ( state == PlayState.NEXT && !challenge.isCorrect() ) );
-                graphNode.setChildrenPickable( graphNode.getPickable() );
-
-                // Show the equation for the user's guess at the end of the challenge.
-                guessEquationParent.setVisible( state == PlayState.NEXT );
-
-                // visibility of correct/incorrect icons
-                updateIcons.apply();
-            }
-        } );
-
-        // "Check" button
-        checkButton.addActionListener( new ActionListener() {
-            public void actionPerformed( ActionEvent e ) {
-                if ( challenge.isCorrect() ) {
-                    faceNode.smile();
-                    graphNode.setAnswerVisible( true );
-                    audioPlayer.correctAnswer();
-                    final int points = model.computePoints( model.state.get() == PlayState.FIRST_CHECK ? 1 : 2 );  //TODO handle this better
-                    model.results.score.set( model.results.score.get() + points );
-                    pointsAwardedNode.setText( MessageFormat.format( Strings.POINTS_AWARDED, String.valueOf( points ) ) );
-                    // points to right of face
-                    pointsAwardedNode.setOffset( faceNode.getFullBoundsReference().getMaxX() + 10,
-                                                 faceNode.getFullBoundsReference().getCenterY() - ( pointsAwardedNode.getFullBoundsReference().getHeight() / 2 ) );
-                    model.state.set( PlayState.NEXT );
-                }
-                else {
-                    faceNode.frown();
-                    audioPlayer.wrongAnswer();
-                    pointsAwardedNode.setText( "" );
-                    if ( model.state.get() == PlayState.FIRST_CHECK ) {
-                        model.state.set( PlayState.TRY_AGAIN );
-                    }
-                    else {
-                        model.state.set( PlayState.SHOW_ANSWER );
-                    }
-                }
-            }
-        } );
-
-        // "Try Again" button
-        tryAgainButton.addActionListener( new ActionListener() {
-            public void actionPerformed( ActionEvent e ) {
-                model.state.set( PlayState.SECOND_CHECK );
-            }
-        } );
-
-        // "Show Answer" button
-        showAnswerButton.addActionListener( new ActionListener() {
-            public void actionPerformed( ActionEvent e ) {
-                graphNode.setAnswerVisible( true );
-                model.state.set( PlayState.NEXT );
-            }
-        } );
-
-        // "Next" button
-        nextButton.addActionListener( new ActionListener() {
-            public void actionPerformed( ActionEvent e ) {
-                model.state.set( PlayState.FIRST_CHECK );
-            }
-        } );
     }
 
     // Creates the equation portion of the view.
-    private PNode createEquationNode( Line line, PhetFont font, Color color ) {
-        if ( line == null ) {
-            return new PhetPText( Strings.NOT_A_LINE, new PhetFont( Font.BOLD, 24 ), color );
-        }
-        else {
-            return new PointSlopeEquationNode( line, font, color );
-        }
+    protected PNode createEquationNode( Line line, PhetFont font, Color color ) {
+        return new PointSlopeEquationNode( line, font, color );
     }
 
     // Creates the graph portion of the view.
-    private P3P_GraphNode createGraphNode( P3P_Challenge challenge ) {
+    protected P3P_GraphNode createGraphNode( P3P_Challenge challenge ) {
         return new P3P_PS_GraphNode( challenge );
     }
 }
