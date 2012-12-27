58497972d88b8805c3db2923b80e58aa98754855
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-12-27 18:20:07 +0000

    move duplication in GTL_Challenge and MTE_Challenge into base class MatchChallenge

--- trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/linegame/model/MTE_Challenge.java
+++ trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/linegame/model/MTE_Challenge.java
@@ -6,14 +6,7 @@ import java.awt.geom.Point2D;
 import edu.colorado.phet.common.games.GameAudioPlayer;
 import edu.colorado.phet.common.phetcommon.math.vector.Vector2D;
 import edu.colorado.phet.common.phetcommon.util.IntegerRange;
-import edu.colorado.phet.common.phetcommon.util.ObservableList;
-import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
-import edu.colorado.phet.common.phetcommon.view.graphics.transforms.ModelViewTransform;
-import edu.colorado.phet.linegraphing.common.LGResources.Strings;
-import edu.colorado.phet.linegraphing.common.model.Graph;
 import edu.colorado.phet.linegraphing.common.model.Line;
-import edu.colorado.phet.linegraphing.common.model.PointTool;
-import edu.colorado.phet.linegraphing.common.model.PointTool.Orientation;
 import edu.colorado.phet.linegraphing.linegame.view.ChallengeNode;
 import edu.colorado.phet.linegraphing.linegame.view.MTE_ChallengeNode;
 import edu.umd.cs.piccolo.util.PDimension;
@@ -26,71 +19,20 @@ import edu.umd.cs.piccolo.util.PDimension;
  */
 public class MTE_Challenge extends MatchChallenge {
 
-    private static final int GRAPH_WIDTH = 400; // graph width in view coordinates
-    private static final Point2D ORIGIN_OFFSET = new Point2D.Double( 275, 300 ); // offset of the origin (center of the graph) in view coordinates
-
-    public final String title;
-    public final ModelViewTransform mvt; // transform between model and view coordinate frames
-    public final Graph graph; // the graph that plots the lines
-    public final LineForm lineForm;
-    public final ManipulationMode manipulationMode;
-    public final PointTool pointTool1, pointTool2;
-    private final ObservableList<Line> allLines; // lines that are visible to the point tools
-    private boolean answerVisible;
-
     public MTE_Challenge( Line answer, LineForm lineForm, ManipulationMode manipulationMode, IntegerRange xRange, IntegerRange yRange ) {
-        super( answer, Line.Y_EQUALS_X_LINE );
+        super( answer, lineForm, manipulationMode, xRange, yRange,
+               new Point2D.Double( 275, 300 ), // origin offset
+               new Vector2D( xRange.getMin() + ( 0.05 * xRange.getLength() ), yRange.getMin() - 1.5 ), // point tool location 1
+               new Vector2D( xRange.getMin() + ( 0.35 * xRange.getLength() ), yRange.getMin() - 4.5 ) );  // point tool location 2
 
-        this.lineForm = lineForm;
-        this.manipulationMode = manipulationMode;
-
-        // Adjust the title and initial state of the guess.
-        if ( manipulationMode == ManipulationMode.SLOPE ) {
-            title = Strings.SET_THE_SLOPE;
-            guess.set( Line.createPointSlope( answer.x1, answer.y1, 1, 1 ) );
-        }
-        else if ( manipulationMode == ManipulationMode.INTERCEPT ) {
-            title = Strings.SET_THE_Y_INTERCEPT;
-            guess.set( Line.createSlopeIntercept( answer.rise, answer.run, 0 ) );
-        }
-        else if ( manipulationMode == ManipulationMode.POINT ) {
-            title = Strings.SET_THE_POINT;
-            guess.set( Line.createPointSlope( 0, 0, answer.rise, answer.run ) );
-        }
-        else {
-            title = Strings.MAKE_THE_EQUATION;
-            // default line is fine
-        }
-
-        final double mvtScale = GRAPH_WIDTH / xRange.getLength(); // view units / model units
-        mvt = ModelViewTransform.createOffsetScaleMapping( ORIGIN_OFFSET, mvtScale, -mvtScale ); // graph on right, y inverted
-
-        graph = new Graph( xRange, yRange );
-
-        allLines = new ObservableList<Line>();
-        allLines.add( answer );
-        this.pointTool1 = new PointTool( new Vector2D( xRange.getMin() + ( 0.05 * xRange.getLength() ), yRange.getMin() - 1.5 ), Orientation.UP, allLines );
-        this.pointTool2 = new PointTool( new Vector2D( xRange.getMin() + ( 0.35 * xRange.getLength() ), yRange.getMin() - 4.5 ), Orientation.DOWN, allLines );
-
-        this.guess.addObserver( new SimpleObserver() {
-            public void update() {
-                updateLines();
-            }
-        } );
     }
 
-    // Visibility of the answer affects whether the user's guess is "seen" by the point tools.
-    public void setAnswerVisible( boolean visible ) {
-        answerVisible = visible;
-        updateLines();
-    }
-
-    // Updates the list of lines that are visible to the point tools.
-    private void updateLines() {
-        allLines.clear();
-        allLines.add( answer );
+    // Updates the collection of lines that are "seen" by the point tools.
+    @Override protected void updatePointToolLines() {
+        pointToolLines.clear();
+        pointToolLines.add( answer );
         if ( answerVisible ) {
-            allLines.add( guess.get() );
+            pointToolLines.add( guess.get() );
         }
     }
 
