ecd75c9c28f56c373065fef3433862f02e415836
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2012-12-27 22:53:44 +0000

    move most (but not all) duplicate code into ChallengeNode base class

--- trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/linegame/view/ChallengeNode.java
+++ trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/linegame/view/ChallengeNode.java
@@ -1,25 +1,167 @@
 // Copyright 2002-2012, University of Colorado
 package edu.colorado.phet.linegraphing.linegame.view;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.geom.Rectangle2D;
+import java.text.MessageFormat;
+
+import edu.colorado.phet.common.games.GameAudioPlayer;
+import edu.colorado.phet.common.phetcommon.model.property.BooleanProperty;
+import edu.colorado.phet.common.phetcommon.util.function.VoidFunction1;
 import edu.colorado.phet.common.piccolophet.PhetPNode;
+import edu.colorado.phet.common.piccolophet.nodes.FaceNode;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPText;
+import edu.colorado.phet.common.piccolophet.nodes.TextButtonNode;
+import edu.colorado.phet.linegraphing.common.LGResources.Strings;
+import edu.colorado.phet.linegraphing.common.view.PointToolNode;
 import edu.colorado.phet.linegraphing.linegame.LineGameConstants;
+import edu.colorado.phet.linegraphing.linegame.model.LineGameModel;
 import edu.colorado.phet.linegraphing.linegame.model.MatchChallenge;
+import edu.colorado.phet.linegraphing.linegame.model.PlayState;
 import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.nodes.PText;
 import edu.umd.cs.piccolo.util.PDimension;
 
 /**
  * Base class view for all challenges.
+ * Provides the view components that are common to all challenges.
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
 public abstract class ChallengeNode extends PhetPNode {
 
-    public ChallengeNode( MatchChallenge challenge, PDimension challengeSize ) {
+    protected final FaceNode faceNode;
+    protected final PText pointsAwardedNode;
+    protected final TextButtonNode checkButton;
+    protected final PNode parentNode; //TODO rename
+
+    public ChallengeNode( final LineGameModel model, final MatchChallenge challenge, final GameAudioPlayer audioPlayer, PDimension challengeSize ) {
 
         // title
         PNode titleNode = new PhetPText( challenge.title, LineGameConstants.TITLE_FONT, LineGameConstants.TITLE_COLOR );
-        addChild( titleNode );
         titleNode.setOffset( ( challengeSize.getWidth() / 2 ) - ( titleNode.getFullBoundsReference().getWidth() / 2 ), 10 ); // top center
+
+        faceNode = new FaceNode( LineGameConstants.FACE_DIAMETER, LineGameConstants.FACE_COLOR,
+                                 new BasicStroke( 1f ), LineGameConstants.FACE_COLOR.darker(), Color.BLACK, Color.BLACK );
+
+        pointsAwardedNode = new PhetPText( "", LineGameConstants.POINTS_AWARDED_FONT, LineGameConstants.POINTS_AWARDED_COLOR );
+
+        // Buttons
+        final Font buttonFont = LineGameConstants.BUTTON_FONT;
+        final Color buttonForeground = LineGameConstants.BUTTON_COLOR;
+        checkButton = new TextButtonNode( Strings.CHECK, buttonFont, buttonForeground );
+        final TextButtonNode tryAgainButton = new TextButtonNode( Strings.TRY_AGAIN, buttonFont, buttonForeground );
+        final TextButtonNode showAnswerButton = new TextButtonNode( Strings.SHOW_ANSWER, buttonFont, buttonForeground );
+        final TextButtonNode nextButton = new TextButtonNode( Strings.NEXT, buttonFont, buttonForeground );
+
+        // Point tools
+        Rectangle2D pointToolDragBounds = new Rectangle2D.Double( 0, 0, challengeSize.getWidth(), challengeSize.getHeight() );
+        PointToolNode pointToolNode1 = new PointToolNode( challenge.pointTool1, challenge.mvt, challenge.graph, pointToolDragBounds, new BooleanProperty( true ) );
+        PointToolNode pointToolNode2 = new PointToolNode( challenge.pointTool2, challenge.mvt, challenge.graph, pointToolDragBounds, new BooleanProperty( true ) );
+        pointToolNode1.scale( LineGameConstants.POINT_TOOL_SCALE );
+        pointToolNode2.scale( LineGameConstants.POINT_TOOL_SCALE );
+
+        // Point tools moveToFront when dragged, so we give them a common parent to preserve rendering order of the reset of the scenegraph.
+        PNode pointToolParent = new PNode();
+        pointToolParent.addChild( pointToolNode1 );
+        pointToolParent.addChild( pointToolNode2 );
+
+        // Parent for subclass-specific nodes, to maintain rendering order.
+        parentNode = new PNode();
+
+        // Rendering order
+        addChild( parentNode );
+        addChild( titleNode );
+        addChild( checkButton );
+        addChild( tryAgainButton );
+        addChild( showAnswerButton );
+        addChild( nextButton );
+        addChild( pointToolParent );
+        addChild( faceNode );
+        addChild( pointsAwardedNode );
+
+        // layout
+        {
+            // buttons centered at bottom of challenge space
+            final double buttonCenterX = ( challengeSize.getWidth() / 2 );
+            final double buttonCenterY = challengeSize.getHeight() - checkButton.getFullBoundsReference().getHeight() - 30;
+            checkButton.setOffset( buttonCenterX - ( checkButton.getFullBoundsReference().getWidth() / 2 ), buttonCenterY );
+            tryAgainButton.setOffset( buttonCenterX - ( tryAgainButton.getFullBoundsReference().getWidth() / 2 ), buttonCenterY );
+            showAnswerButton.setOffset( buttonCenterX - ( showAnswerButton.getFullBoundsReference().getWidth() / 2 ), buttonCenterY );
+            nextButton.setOffset( buttonCenterX - ( nextButton.getFullBoundsReference().getWidth() / 2 ), buttonCenterY );
+        }
+
+        // "Check" button
+        checkButton.addActionListener( new ActionListener() {
+            public void actionPerformed( ActionEvent e ) {
+                if ( challenge.isCorrect() ) {
+                    faceNode.smile();
+                    audioPlayer.correctAnswer();
+                    final int points = model.computePoints( model.state.get() == PlayState.FIRST_CHECK ? 1 : 2 );  //TODO handle this better
+                    model.results.score.set( model.results.score.get() + points );
+                    pointsAwardedNode.setText( MessageFormat.format( Strings.POINTS_AWARDED, String.valueOf( points ) ) );
+                    // points to right of face
+                    pointsAwardedNode.setOffset( faceNode.getFullBoundsReference().getMaxX() + 10,
+                                                 faceNode.getFullBoundsReference().getCenterY() - ( pointsAwardedNode.getFullBoundsReference().getHeight() / 2 ) );
+                    model.state.set( PlayState.NEXT );
+                }
+                else {
+                    faceNode.frown();
+                    audioPlayer.wrongAnswer();
+                    pointsAwardedNode.setText( "" );
+                    if ( model.state.get() == PlayState.FIRST_CHECK ) {
+                        model.state.set( PlayState.TRY_AGAIN );
+                    }
+                    else {
+                        model.state.set( PlayState.SHOW_ANSWER );
+                    }
+                }
+            }
+        } );
+
+        // "Try Again" button
+        tryAgainButton.addActionListener( new ActionListener() {
+            public void actionPerformed( ActionEvent e ) {
+                model.state.set( PlayState.SECOND_CHECK );
+            }
+        } );
+
+        // "Show Answer" button
+        showAnswerButton.addActionListener( new ActionListener() {
+            public void actionPerformed( ActionEvent e ) {
+                model.state.set( PlayState.NEXT );
+            }
+        } );
+
+        // "Next" button
+        nextButton.addActionListener( new ActionListener() {
+            public void actionPerformed( ActionEvent e ) {
+                model.state.set( PlayState.FIRST_CHECK );
+            }
+        } );
+
+        // state changes
+        model.state.addObserver( new VoidFunction1<PlayState>() {
+            public void apply( PlayState state ) {
+
+                // visibility of face
+                faceNode.setVisible( state == PlayState.TRY_AGAIN ||
+                                     state == PlayState.SHOW_ANSWER ||
+                                     ( state == PlayState.NEXT && challenge.isCorrect() ) );
+
+                // visibility of points
+                pointsAwardedNode.setVisible( faceNode.getVisible() && challenge.isCorrect() );
+
+                // visibility of buttons
+                checkButton.setVisible( state == PlayState.FIRST_CHECK || state == PlayState.SECOND_CHECK );
+                tryAgainButton.setVisible( state == PlayState.TRY_AGAIN );
+                showAnswerButton.setVisible( state == PlayState.SHOW_ANSWER );
+                nextButton.setVisible( state == PlayState.NEXT );
+            }
+        } );
     }
 }
