05589ed2c31d1485d609425703a2aac80b25837d
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2013-01-10 22:27:40 +0000

    1/10/13 change: "Correct Answer" to "A Correct Answer"

--- simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/linegame/view/MTE_ChallengeNode.java
+++ simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/linegame/view/MTE_ChallengeNode.java
@@ -48,7 +48,7 @@ public class MTE_ChallengeNode extends ChallengeNode {
 
         // Answer
         final EquationBoxNode answerBoxNode =
-                new EquationBoxNode( Strings.CORRECT_EQUATION, challenge.answer.color, new PDimension( boxWidth, 0.2 * challengeSize.getHeight() ),
+                new EquationBoxNode( Strings.A_CORRECT_EQUATION, challenge.answer.color, new PDimension( boxWidth, 0.2 * challengeSize.getHeight() ),
                                      createEquationNode( challenge.equationForm, challenge.answer, LineGameConstants.STATIC_EQUATION_FONT, challenge.answer.color ) );
 
         // Guess
