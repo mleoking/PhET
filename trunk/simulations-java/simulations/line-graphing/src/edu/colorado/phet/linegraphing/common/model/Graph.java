503ec5cf784dc5c6a4f53b5f329138ecca96fab5
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2013-01-17 03:24:20 +0000

    delete bogus import

--- trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/common/model/Graph.java
+++ trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/common/model/Graph.java
@@ -1,8 +1,6 @@
 // Copyright 2002-2012, University of Colorado
 package edu.colorado.phet.linegraphing.common.model;
 
-import javafx.beans.value.ObservableListValue;
-
 import edu.colorado.phet.common.phetcommon.math.vector.Vector2D;
 import edu.colorado.phet.common.phetcommon.util.IntegerRange;
 import edu.colorado.phet.common.phetcommon.util.ObservableList;
