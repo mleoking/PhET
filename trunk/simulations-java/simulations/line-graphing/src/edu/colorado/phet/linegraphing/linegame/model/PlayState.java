36561f4b4285ead65674d3038517b6f1cca2fe4e
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2013-01-18 21:23:07 +0000

    doc

--- trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/linegame/model/PlayState.java
+++ trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/linegame/model/PlayState.java
@@ -5,7 +5,7 @@ package edu.colorado.phet.linegraphing.linegame.model;
  * States during the "play" phase of a game, mutually exclusive. (See GamePhase.)
  * For lack of better names, the state names correspond to the main action that
  * the user can take in that state.  For example. the FIRST_CHECK state is where the user
- * has their first opportunity to press the "Check" button to check their answer.
+ * has their first opportunity to press the "Check" button.
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  */
