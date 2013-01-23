943de6e6b0ffc8fc8dbfc97acf583d90e20aacf6
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2013-01-23 20:52:04 +0000

    #3487, revert inadvertent changes (bogus import, formatting) that were checked in with REVIEW comments in [71828]

--- trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/linegame/model/ChallengeFactory6.java
+++ trunk/simulations-java/simulations/line-graphing/src/edu/colorado/phet/linegraphing/linegame/model/ChallengeFactory6.java
@@ -3,7 +3,6 @@ package edu.colorado.phet.linegraphing.linegame.model;
 
 import java.awt.Color;
 import java.util.ArrayList;
-import java.util.Collections;
 
 import edu.colorado.phet.common.phetcommon.util.IntegerRange;
 import edu.colorado.phet.linegraphing.common.model.Line;
@@ -17,7 +16,6 @@ class ChallengeFactory6 extends ChallengeFactory {
 
     /**
      * Creates challenges for this game level.
-     *
      * @param xRange range of the graph's x axis
      * @param yRange range of the graph's y axis
      * @return list of challenges
