ea811dd3904cab1ea1d27cc1dfaceca290896422
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2013-01-30 18:37:29 +0000

    doc

--- trunk/simulations-html/simulations/beers-law-lab/js/common/view/StringUtils.js
+++ trunk/simulations-html/simulations/beers-law-lab/js/common/view/StringUtils.js
@@ -8,6 +8,9 @@ define( [], function() {
     /*
      * Similar to Java's MessageFormat.
      * Eg, messageFormat( "{0} + {1}", [2,3] ) -> "2 + 3"
+     *
+     * @param pattern pattern string, with {N} placeholders where N is an integer
+     * @param args values to be substituted for placeholders in pattern
      */
     StringUtils.messageFormat = function ( pattern, args ) {
         return pattern.replace( /\{(\d+)\}/g, function () {
