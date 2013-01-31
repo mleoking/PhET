fb5f7d4066a58ecd24ff83ea0baa4b244caeb959
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2013-01-31 01:43:10 +0000

    drag handler for shaker, with constrained bounds

--- trunk/simulations-html/simulations/beers-law-lab/js/common/model/Rectangle.js
+++ trunk/simulations-html/simulations/beers-law-lab/js/common/model/Rectangle.js
@@ -9,5 +9,25 @@ define( [], function () {
         this.height = height;
     }
 
+    Rectangle.prototype.getMaxX = function() {
+        return this.x + this.width;
+    }
+
+    Rectangle.prototype.getCenterX = function () {
+        return this.x + this.width / 2;
+    }
+
+    Rectangle.prototype.getMaxY = function () {
+        return this.y + this.height;
+    }
+
+    Rectangle.prototype.getCenterY = function () {
+        return this.y + this.height / 2;
+    }
+
+    Rectangle.prototype.contains = function( x, y ) {
+        return ( x >= this.x && x <= this.getMaxX() && y >= this.y && y <= this.getMaxY() );
+    }
+
     return Rectangle;
 } );
