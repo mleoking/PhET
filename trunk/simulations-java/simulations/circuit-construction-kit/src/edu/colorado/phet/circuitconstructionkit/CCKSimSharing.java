f3e3916759bae1acded9de2cffee30ed93453aa7
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2013-02-01 19:44:30 +0000

    Added some user components and user actions, see #3497.

--- trunk/simulations-java/simulations/circuit-construction-kit/src/edu/colorado/phet/circuitconstructionkit/CCKSimSharing.java
+++ trunk/simulations-java/simulations/circuit-construction-kit/src/edu/colorado/phet/circuitconstructionkit/CCKSimSharing.java
@@ -1,13 +1,23 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.circuitconstructionkit;
 
+import edu.colorado.phet.common.phetcommon.simsharing.messages.IUserAction;
 import edu.colorado.phet.common.phetcommon.simsharing.messages.IUserComponent;
 
 /**
+ * This class is where all the user components, actions, and such are defined
+ * for the sim, which are necessary for the "sim sharing" (a.k.a. data
+ * collection) feature.
+ *
+ * @author John Blanco
  * @author Sam Reid
  */
 public class CCKSimSharing {
     public static enum UserComponents implements IUserComponent {
-        wire
+        battery, wire, lightBulb
+    }
+
+    public static enum UserActions implements IUserAction {
+        createdComponent, removedComponent, connectionFormed
     }
 }
