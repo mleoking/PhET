55738fae69679436805e74d62b79d68222afb727
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2013-02-08 04:28:39 +0000

    Increased simulation speed from 1/4 to 1/2 of original

--- trunk/simulations-java/simulations/forces-and-motion-basics/src/edu/colorado/phet/forcesandmotionbasics/motion/MotionModule.java
+++ trunk/simulations-java/simulations/forces-and-motion-basics/src/edu/colorado/phet/forcesandmotionbasics/motion/MotionModule.java
@@ -16,7 +16,7 @@ public class MotionModule extends SimSharingPiccoloModule implements Resettable 
     private final boolean accelerometer;
 
     public MotionModule( UserComponents component, String title, boolean friction, boolean accelerometer ) {
-        super( component, title, new ConstantDtClock( (int) ( 1000.0 / ConstantDtClock.DEFAULT_FRAMES_PER_SECOND ), 1.0 / ConstantDtClock.DEFAULT_FRAMES_PER_SECOND / 4 ) );
+        super( component, title, new ConstantDtClock( (int) ( 1000.0 / ConstantDtClock.DEFAULT_FRAMES_PER_SECOND ), 1.0 / ConstantDtClock.DEFAULT_FRAMES_PER_SECOND / 2.0 ) );
         setSimulationPanel( new MotionCanvas( this, getClock(), friction, accelerometer ) );
         setClockControlPanel( null );
         this.friction = friction;
