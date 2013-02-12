d51e6302dda6ceee4e7403fd5d876b53205fce55
cmalley <cmalley@3dd8ee02-672a-0410-8165-a85d635670bd>
2013-02-12 20:03:49 +0000

    delete team/cmalley/html5, moved to github

--- team/cmalley/html5/require-include-test/js/model/Contact.js
+++
@@ -1,9 +0,0 @@
-define( [ "modelpath/Person" ],
-        function ( Person ) {
-
-            function Contact( name ) {
-                this.person = new Person( name );
-            }
-
-            return Contact;
-        } );
--- team/cmalley/html5/require-test/src/main.js
+++
@@ -1,8 +0,0 @@
-/*
- * This function is called when scripts/helper/util.js is loaded.
- * If util.js calls define(), then this function is not fired until util's dependencies have loaded,
- * and the util argument will hold the module value for "helper/util".
- */
-require( ["helper/util"], function ( util ) {
-    renderCenteredText( "Hello World", document.getElementById( 'canvas' ).getContext( '2d' ) );
-} );
