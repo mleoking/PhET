23353c3b99f36529d5784a657c9baa92f4374dfe
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2013-03-01 05:03:29 +0000

    improved home screen layout

--- branches/level-selection/simulations-java/simulations/forces-and-motion-basics/src/edu/colorado/phet/forcesandmotionbasics/touch/LevelSelectionScreen.java
+++ branches/level-selection/simulations-java/simulations/forces-and-motion-basics/src/edu/colorado/phet/forcesandmotionbasics/touch/LevelSelectionScreen.java
@@ -3,12 +3,15 @@ package edu.colorado.phet.forcesandmotionbasics.touch;
 import java.awt.*;
 import java.awt.geom.Rectangle2D;
 
+import edu.colorado.phet.common.phetcommon.view.util.BufferedImageUtils;
 import edu.colorado.phet.common.phetcommon.view.util.PhetFont;
+import edu.colorado.phet.common.piccolophet.nodes.HTMLImageButtonNode;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPPath;
 import edu.colorado.phet.common.piccolophet.nodes.PhetPText;
 import edu.colorado.phet.common.piccolophet.nodes.layout.HBox;
 import edu.colorado.phet.common.piccolophet.nodes.layout.VBox;
 import edu.colorado.phet.forcesandmotionbasics.ForcesAndMotionBasicsApplication;
+import edu.colorado.phet.forcesandmotionbasics.ForcesAndMotionBasicsResources;
 import edu.colorado.phet.forcesandmotionbasics.common.AbstractForcesAndMotionBasicsCanvas;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
@@ -22,10 +25,15 @@ public class LevelSelectionScreen extends AbstractForcesAndMotionBasicsCanvas {
         this.application = application;
         setBackground( Color.black );
         addWorldChild( new PNode() {{
-            addChild( new HBox( new LevelNode( new PhetPPath( new Rectangle2D.Double( 0, 0, 300, 300 ), Color.green ), "Tug of War", listener( 0 ) ),
-                                new LevelNode( new PhetPPath( new Rectangle2D.Double( 0, 0, 200, 200 ), Color.green ), "Motion", listener( 1 ) ),
-                                new LevelNode( new PhetPPath( new Rectangle2D.Double( 0, 0, 200, 200 ), Color.green ), "Friction", listener( 2 ) ),
-                                new LevelNode( new PhetPPath( new Rectangle2D.Double( 0, 0, 200, 200 ), Color.green ), "Acceleration Lab", listener( 3 ) ) ) );
+            addChild( new VBox( new PhetPText( "Forces and Motion: Basics", new PhetFont( 48, true ), Color.white ),
+                                new PhetPPath( new Rectangle2D.Double( 0, 0, 100, 50 ), null, null ),//spacing
+                                new HBox( new LevelNode( new PhetPPath( new Rectangle2D.Double( 0, 0, 300, 300 ), Color.green ), "Tug of War", listener( 0 ) ),
+                                          new LevelNode( new PhetPPath( new Rectangle2D.Double( 0, 0, 200, 200 ), Color.green ), "Motion", listener( 1 ) ),
+                                          new LevelNode( new PhetPPath( new Rectangle2D.Double( 0, 0, 200, 200 ), Color.green ), "Friction", listener( 2 ) ),
+                                          new LevelNode( new PhetPPath( new Rectangle2D.Double( 0, 0, 200, 200 ), Color.green ), "Acceleration Lab", listener( 3 ) ) ) ) {{
+                setOffset( 0, 50 );
+            }} );
+            addChild( new HBox( new PhetPText( "PhET", new PhetFont( 20, true ), Color.yellow ), new HTMLImageButtonNode( BufferedImageUtils.multiScaleToHeight( ForcesAndMotionBasicsResources.Images.MENU_ICON, 20 ) ) ) );
         }} );
     }
 
@@ -40,7 +48,7 @@ public class LevelSelectionScreen extends AbstractForcesAndMotionBasicsCanvas {
     private static class LevelNode extends PNode {
         private LevelNode( PNode icon, String text, PInputEventListener listener ) {
             addChild( new VBox( icon,
-                                new PhetPText( text, new PhetFont( 24 ), Color.white ) ) );
+                                new PhetPText( text, new PhetFont( 28 ), Color.white ) ) );
             addInputEventListener( listener );
         }
     }
