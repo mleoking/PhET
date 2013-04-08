c525d27f44b7d281bc0d77d53a9063e21a94700e
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2013-04-08 18:31:44 +0000

    Finalized (hopefully) the background colors for the tabs and control panels.

--- trunk/simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/common/EFACConstants.java
+++ trunk/simulations-java/simulations/energy-forms-and-changes/src/edu/colorado/phet/energyformsandchanges/common/EFACConstants.java
@@ -69,17 +69,9 @@ public class EFACConstants {
     public static final float NOMINAL_WATER_OPACITY = 0.75f;
     public static final Color WATER_COLOR_OPAQUE = new Color( 175, 238, 238 );
     public static final Color WATER_COLOR_IN_BEAKER = new Color( 175, 238, 238, (int) ( Math.round( NOMINAL_WATER_OPACITY * 255 ) ) );
-    // TODO: Remove unused colors when finalized.
-//    public static final Color FIRST_TAB_BACKGROUND_COLOR = new Color( 238, 232, 170 );
-//    public static final Color FIRST_TAB_BACKGROUND_COLOR = new Color( 242, 238, 191 );
-    public static final Color FIRST_TAB_BACKGROUND_COLOR = new Color( 246, 244, 213 );
+    public static final Color FIRST_TAB_BACKGROUND_COLOR = new Color( 245, 235, 175 );
     public static final Color SECOND_TAB_BACKGROUND_COLOR = FIRST_TAB_BACKGROUND_COLOR;
-//    public static final Color CONTROL_PANEL_BACKGROUND_COLOR = new Color( 197, 209, 221 );  // Blue gray. JB not so keen on this.
-//    public static final Color CONTROL_PANEL_BACKGROUND_COLOR = new Color( 194, 232, 141 ); // Somewhat pale green.  JB kind of likes.
-//    public static final Color CONTROL_PANEL_BACKGROUND_COLOR = new Color( 180, 211, 51 ); // Darker green. JB things so so.
     public static final Color CONTROL_PANEL_BACKGROUND_COLOR = new Color( 199, 229, 199 ); // Pale gray green.  JB, NP, and AP voted on this as a fave.  Maybe too close to water though.
-//    public static final Color CONTROL_PANEL_BACKGROUND_COLOR = new Color( 184, 199, 199 ); //
-//    public static final Color CONTROL_PANEL_BACKGROUND_COLOR = new Color( 198, 210, 201 ); // Lighter blue gray.  JB thinks not bad.
     public static final BasicStroke CONTROL_PANEL_OUTLINE_STROKE = new BasicStroke( 1.5f );
     public static final Color CONTROL_PANEL_OUTLINE_COLOR = Color.BLACK;
     public static final Color CLOCK_CONTROL_BACKGROUND_COLOR = new Color( 120, 120, 120 );
