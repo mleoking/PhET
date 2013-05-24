7a755e40208e47af7d7a0cd88d7d24cc7e7588e9
jblanco <jblanco@3dd8ee02-672a-0410-8165-a85d635670bd>
2013-05-24 20:10:26 +0000

    Set the timeout until the challege is available back to 60 seconds, see #3537.

--- trunk/simulations-java/simulations/balance-and-torque-study/src/edu/colorado/phet/balanceandtorquestudy/balancelab/view/BalanceLabCanvas.java
+++ trunk/simulations-java/simulations/balance-and-torque-study/src/edu/colorado/phet/balanceandtorquestudy/balancelab/view/BalanceLabCanvas.java
@@ -41,7 +41,7 @@ import static edu.colorado.phet.common.piccolophet.PhetPCanvas.CenteredStage.DEF
  */
 public class BalanceLabCanvas extends BasicBalanceCanvas {
 
-    private static final int CHALLENGE_UNAVAILABLE_TIME = 5; // In seconds.
+    private static final int CHALLENGE_UNAVAILABLE_TIME = 60; // In seconds.
 
     protected MassKitSelectionNode fullMassKitSelectionNode;
     protected SimpleMassKitSelectionNode simpleMassKitSelectionNode;
