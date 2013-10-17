b63aa60758e3ac70910a34d792ab9ff4964eeefb
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2013-10-17 21:07:15 +0000

    Switched to Java 1.4 features so it will work with the build, see #3574

--- trunk/simulations-flash/flash-launcher/src/edu/colorado/phet/flashlauncher/util/BareBonesBrowserLaunch.java
+++ trunk/simulations-flash/flash-launcher/src/edu/colorado/phet/flashlauncher/util/BareBonesBrowserLaunch.java
@@ -28,10 +28,10 @@ public class BareBonesBrowserLaunch {
     public static void openURL( String url ) {
         FlashLauncher.println( "Starting OpenURL..." );
         try {  //attempt to use Desktop library from JDK 1.6+
-            Class<?> d = Class.forName( "java.awt.Desktop" );
+            Class d = Class.forName( "java.awt.Desktop" );
             FlashLauncher.println( "found class " + d );
             d.getDeclaredMethod( "browse", new Class[]{java.net.URI.class} ).invoke(
-                    d.getDeclaredMethod( "getDesktop" ).invoke( null ),
+                    d.getDeclaredMethod( "getDesktop", null ).invoke( null,null ),
                     new Object[]{java.net.URI.create( url )} );
             //above code mimicks:  java.awt.Desktop.getDesktop().browse()
         }
@@ -53,7 +53,8 @@ public class BareBonesBrowserLaunch {
                 else { //assume Unix or Linux
                     FlashLauncher.println( "Starting browser on Unix/Linux" );
                     String browser = null;
-                    for ( String b : browsers ) {
+                    for ( int i = 0; i < browsers.length; i++ ) {
+                        String b = browsers[i];
                         if ( browser == null && Runtime.getRuntime().exec( new String[]
                                                                                    {"which", b} ).getInputStream().read() != -1 ) {
                             Runtime.getRuntime().exec( new String[]{browser = b, url} );
