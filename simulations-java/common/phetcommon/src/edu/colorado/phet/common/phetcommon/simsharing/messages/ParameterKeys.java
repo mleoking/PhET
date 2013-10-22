407e0c0df01a26b8043a88e279fa6ed0babaae98
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2013-10-22 01:00:27 +0000

    Added type parameter

--- simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/simsharing/messages/ParameterKeys.java
+++ simulations-java/common/phetcommon/src/edu/colorado/phet/common/phetcommon/simsharing/messages/ParameterKeys.java
@@ -6,7 +6,7 @@ package edu.colorado.phet.common.phetcommon.simsharing.messages;
  */
 public enum ParameterKeys implements IParameterKey {
     canvasPositionX, canvasPositionY, componentType, description, height, interactive, item,
-    isSelected, key, part, text, title, value, width, window, x, y, z,
+    isSelected, key, part, text, title, value, width, window, x, y, z, type,
     angle,
     enabled,
     minX, maxX, averageX, minY, maxY, averageY,
