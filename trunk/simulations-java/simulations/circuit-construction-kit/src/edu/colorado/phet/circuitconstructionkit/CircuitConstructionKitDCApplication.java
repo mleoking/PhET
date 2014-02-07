5a9b5453959353ea11048857860b1cb724f6cf30
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2014-02-07 19:31:16 +0000

    Added versions A and B for stanford study, see #3602

--- trunk/simulations-java/simulations/circuit-construction-kit/src/edu/colorado/phet/circuitconstructionkit/CircuitConstructionKitDCApplication.java
+++ trunk/simulations-java/simulations/circuit-construction-kit/src/edu/colorado/phet/circuitconstructionkit/CircuitConstructionKitDCApplication.java
@@ -1,6 +1,8 @@
 // Copyright 2002-2011, University of Colorado
 package edu.colorado.phet.circuitconstructionkit;
 
+import java.util.Arrays;
+
 import edu.colorado.phet.common.phetcommon.application.ApplicationConstructor;
 import edu.colorado.phet.common.phetcommon.application.PhetApplication;
 import edu.colorado.phet.common.phetcommon.application.PhetApplicationConfig;
@@ -11,15 +13,27 @@ import edu.colorado.phet.common.phetcommon.application.PhetApplicationLauncher;
  * Apr 12, 2007, 11:48:01 PM
  */
 public class CircuitConstructionKitDCApplication extends CircuitConstructionKitApplication {
+    private static String[] args;
+
     public CircuitConstructionKitDCApplication( PhetApplicationConfig config ) {
         super( config, false, false );
     }
 
     public static void main( String[] args ) {
+        CircuitConstructionKitDCApplication.args = args;
         new PhetApplicationLauncher().launchSim( new CircuitConstructionKitApplicationConfig( args, "circuit-construction-kit", "circuit-construction-kit-dc" ), new ApplicationConstructor() {
             public PhetApplication getApplication( PhetApplicationConfig config ) {
                 return new CircuitConstructionKitDCApplication( config );
             }
         } );
+    }
+
+    public static boolean isStanfordStudy() {
+        return Arrays.asList( CircuitConstructionKitDCApplication.args ).contains( "stanford-a" ) ||
+               Arrays.asList( CircuitConstructionKitDCApplication.args ).contains( "stanford-b" );
+    }
+
+    public static boolean isStanfordStudyA() {
+        return Arrays.asList( CircuitConstructionKitDCApplication.args ).contains( "stanford-a" );
     }
 }
