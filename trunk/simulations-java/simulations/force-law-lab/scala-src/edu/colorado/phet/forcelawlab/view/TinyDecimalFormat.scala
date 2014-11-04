4510f4745cdd546921df6243d481ee95064db794
olsonj <olsonj@3dd8ee02-672a-0410-8165-a85d635670bd>
2014-11-04 23:21:55 +0000

    fix number formatting, see #3651

--- trunk/simulations-java/simulations/force-law-lab/scala-src/edu/colorado/phet/forcelawlab/view/TinyDecimalFormat.scala
+++ trunk/simulations-java/simulations/force-law-lab/scala-src/edu/colorado/phet/forcelawlab/view/TinyDecimalFormat.scala
@@ -2,18 +2,23 @@
 package edu.colorado.phet.forcelawlab.view
 
 import java.text.{FieldPosition, DecimalFormat}
+import scala.math.max
 
-class TinyDecimalFormat extends DecimalFormat("0.00000000000") {
+class TinyDecimalFormat extends DecimalFormat("0.000000000000") {
   override def format(number: Double, result: StringBuffer, fieldPosition: FieldPosition) = {
     val s = super.format(number, result, fieldPosition).toString
     //start at the end, and insert spaces after every 3 elements, may not work for every locale
     var str = ""
     for ( ch <- s ) {
       str = str + ch
-      //4 instead of 3 because space adds another element
-      if ( str.length % 4 == 0 ) {
+      val lastBreakIndex = max( str.lastIndexOf( "." ), str.lastIndexOf( " " ) )
+      if ( str.length() - lastBreakIndex == 4 ) {
         str = str + " "
       }
+//      //4 instead of 3 because space adds another element
+//      if ( str.length % 4 == 0 ) {
+//        str = str + " "
+//      }
     }
     new StringBuffer(str)
   }
