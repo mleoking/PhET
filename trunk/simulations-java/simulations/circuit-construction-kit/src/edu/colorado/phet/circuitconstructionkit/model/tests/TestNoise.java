5cdd52157778d503e9d91eb3115d2c3b836833a6
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2015-04-06 16:15:06 +0000

    Added docs and max voltage clamp

--- trunk/simulations-java/simulations/circuit-construction-kit/src/edu/colorado/phet/circuitconstructionkit/model/tests/TestNoise.java
+++ trunk/simulations-java/simulations/circuit-construction-kit/src/edu/colorado/phet/circuitconstructionkit/model/tests/TestNoise.java
@@ -9,10 +9,27 @@ public class TestNoise {
     static final Random random = new Random();
 
     static double getReadout( double trueVoltage ) {
-        double maxNoise = Math.abs( trueVoltage / 10 );
-        double voltageNoise = random.nextGaussian() * 2.5 / 100.0 * trueVoltage;
-        if ( voltageNoise > maxNoise ) { voltageNoise = maxNoise; }
-        if ( voltageNoise < -maxNoise ) { voltageNoise = -maxNoise; }
+
+        double maxVoltage = 10.0; // This parameter is freely chosen.
+
+        // Cap the maximum possible noise to either 10% of the true voltage or 10% of the maximum voltage
+        double maxNoise = Math.min( Math.abs( trueVoltage ), maxVoltage ) / 10.0;
+
+        // Noise standard deviation is capped to 2.5% of the maximum voltage / amp
+        double voltageNoise;
+        if ( Math.abs( trueVoltage ) < maxVoltage ) {
+            voltageNoise = random.nextGaussian() * 2.5 / 100.0 * trueVoltage;
+        }
+        else {
+            voltageNoise = random.nextGaussian() * 2.5 / 100 * maxVoltage;
+        }
+
+        if ( voltageNoise > maxNoise ) {
+            voltageNoise = maxNoise;
+        }
+        if ( voltageNoise < -maxNoise ) {
+            voltageNoise = -maxNoise;
+        }
         double voltageDisplay = trueVoltage + voltageNoise;
         return voltageDisplay;
     }
@@ -20,15 +37,15 @@ public class TestNoise {
     public static void main( String[] args ) {
         System.out.println( "true voltage\tnoisy readout\tdifference\tpercentNoise" );
         double maxNoise = 0;
-        for ( double trueVoltage = -20; trueVoltage <= +20; trueVoltage += 0.01 ) {
+        for ( double trueVoltage = -40; trueVoltage <= +40; trueVoltage += 0.05 ) {
             double readout = getReadout( trueVoltage );
-            double difference = Math.abs(trueVoltage-readout);
+            double difference = Math.abs( trueVoltage - readout );
             double percentNoise = Math.abs( difference / trueVoltage * 100 );
-            System.out.println( trueVoltage + "\t" + readout + "\t" + difference+"\t"+percentNoise );
-            if (percentNoise>maxNoise){
-                maxNoise=percentNoise;
+            System.out.println( trueVoltage + "\t" + readout + "\t" + difference + "\t" + percentNoise );
+            if ( percentNoise > maxNoise ) {
+                maxNoise = percentNoise;
             }
         }
-        System.out.println("\t\t\t\t"+maxNoise);
+        System.out.println( "\t\t\t\t" + maxNoise );
     }
 }
