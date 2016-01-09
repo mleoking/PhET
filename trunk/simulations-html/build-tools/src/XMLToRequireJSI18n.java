a09d8852cfc99b401c3b794e1fdae5130a487b64
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2016-01-09 03:13:24 +0000

    Backported to jdk6

--- trunk/simulations-html/build-tools/src/XMLToRequireJSI18n.java
+++ trunk/simulations-html/build-tools/src/XMLToRequireJSI18n.java
@@ -1,21 +1,23 @@
 package edu.colorado.phet.buildtools.html5;
 
+import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+//import java.nio.file.Files;
+//import java.nio.file.Path;
 
 import edu.colorado.phet.translationutility.simulations.Simulation;
 
 //Convert Flash XML strings to JavaScript RequireJS i18n format, requires Java 7
 public class XMLToRequireJSI18n {
     public static void main( String[] args ) throws IOException, Simulation.SimulationException {
-        Path tempDir = Files.createTempDirectory( "phet-xml-to-requirejs-temp" );
+        File tempDir = new File( "/Users/samreid/temp-strings/" + System.currentTimeMillis() );
+        tempDir.mkdirs();
 
         System.out.println( "Converting XML to properties with temp path: " + tempDir );
-        XMLToProperties.main( new String[]{args[0], tempDir.toFile().getAbsolutePath()} );
+        XMLToProperties.main( new String[]{args[0], tempDir.getAbsolutePath()} );
 
         System.out.println( "Converting Properties to RequireJS" );
-        PropertiesToRequireJSI18n.main( new String[]{tempDir.toFile().getAbsolutePath(), args[1]} );
+        PropertiesToRequireJSI18n.main( new String[]{tempDir.getAbsolutePath(), args[1]} );
 
         System.out.println( "Done" );
     }
