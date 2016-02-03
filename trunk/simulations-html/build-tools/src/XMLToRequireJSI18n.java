48cad284004c71cc968d3a7f90dea7753ec139d6
samreid <samreid@3dd8ee02-672a-0410-8165-a85d635670bd>
2016-02-03 05:30:40 +0000

    Added the task for porting strings from XML, see https://github.com/phetsims/chipper/issues/131

--- trunk/simulations-html/build-tools/src/XMLToRequireJSI18n.java
+++ trunk/simulations-html/build-tools/src/XMLToRequireJSI18n.java
@@ -2,6 +2,7 @@ package edu.colorado.phet.buildtools.html5;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 //import java.nio.file.Files;
 //import java.nio.file.Path;
 
@@ -9,6 +10,45 @@ import edu.colorado.phet.translationutility.simulations.Simulation;
 
 //Convert Flash XML strings to JavaScript RequireJS i18n format, requires Java 7
 public class XMLToRequireJSI18n {
+    public static class All {
+        static final ArrayList<String> alreadyDone = new ArrayList<String>();
+
+        public static void main( String[] args ) throws IOException, Simulation.SimulationException {
+            visit( new File( "/Users/samreid/phet-svn-trunk/simulations-flash" ) );
+            visit( new File( "/Users/samreid/phet-svn-trunk/simulations-flex" ) );
+        }
+
+        private static void visit( File f ) throws IOException, Simulation.SimulationException {
+            if ( f.isDirectory() ) {
+                File[] files = f.listFiles();
+                for ( int i = 0; i < files.length; i++ ) {
+                    File file = files[i];
+                    visit( file );
+                }
+            }
+            else {
+                if ( f.getAbsolutePath().endsWith( ".xml" ) &&
+                     f.getParentFile().getName().equals( "localization" ) &&
+                     f.getAbsolutePath().contains( "-strings_" ) ) {
+                    System.out.println( "Found XML file: " + f.getAbsolutePath() );
+                    String projectName = f.getParentFile().getParentFile().getName();
+
+                    String localizationDir = f.getParentFile().getAbsolutePath();
+                    if ( !alreadyDone.contains( localizationDir ) ) {
+                        File file = new File( "/Users/samreid/github/babel/autoport/" + projectName );
+                        if ( file.exists() ) {
+                            System.err.println( "Danger!  Directory already existed, there may be a collision: " + file.getAbsolutePath() );
+                            System.exit( 0 );
+                        }
+                        file.mkdirs();
+
+                        XMLToRequireJSI18n.main( new String[]{localizationDir, file.getAbsolutePath()} );
+                        alreadyDone.add( localizationDir );
+                    }
+                }
+            }
+        }
+    }
     public static void main( String[] args ) throws IOException, Simulation.SimulationException {
         File tempDir = new File( "/Users/samreid/temp-strings/" + System.currentTimeMillis() );
         tempDir.mkdirs();
